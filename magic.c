0066e94092f47847b9fae51f167472a75cde7066
Pavel Stehule <stehule@kix.fsv.cvut.cz>
2006-08-28 15:21:54 +0000

    small correctures

--- magic.c
+++ magic.c
@@ -1,4 +1,5 @@
 #include "postgres.h"                                                                                                                             
+#include "fmgr.h"                                                                                                                                 
 
 #ifdef PG_MODULE_MAGIC                                                                                                                            
 PG_MODULE_MAGIC;                                                                                                                                  
