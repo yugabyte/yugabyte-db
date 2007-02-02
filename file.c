1787bad86c752f488d372e5ec60d2d543b5ff9c1
Pavel Stehule <stehule@kix.fsv.cvut.cz>
2007-02-02 12:32:03 +0000

    unsecure utl_file implementation

--- file.c
+++ file.c
@@ -6,6 +6,7 @@
 
 #define INVALID_OPERATION		"UTL_FILE_INVALID_OPERATION"
 #define WRITE_ERROR			"UTL_FILE_WRITE_ERROR"
+#define READ_ERROR			"UTL_FILE_READ_ERROR"
 #define INVALID_FILEHANDLE		"UTL_FILE_INVALID_FILEHANDLE"
 #define INVALID_MAXLINESIZE		"UTL_FILE_INVALID_MAXLINESIZE"
 #define INVALID_MODE			"UTL_FILE_INVALID_MODE"
@@ -15,6 +16,7 @@
 
 Datum utl_file_fopen(PG_FUNCTION_ARGS);
 Datum utl_file_get_line(PG_FUNCTION_ARGS);
+Datum utl_file_get_nextline(PG_FUNCTION_ARGS);
 Datum utl_file___put(PG_FUNCTION_ARGS);
 Datum utl_file_putf(PG_FUNCTION_ARGS);
 Datum utl_file_fflush(PG_FUNCTION_ARGS);
@@ -23,6 +25,7 @@ Datum utl_file_fclose_all(PG_FUNCTION_ARGS);
 
 PG_FUNCTION_INFO_V1(utl_file_fopen);
 PG_FUNCTION_INFO_V1(utl_file_get_line);
+PG_FUNCTION_INFO_V1(utl_file_get_nextline);
 PG_FUNCTION_INFO_V1(utl_file___put);
 PG_FUNCTION_INFO_V1(utl_file_putf);
 PG_FUNCTION_INFO_V1(utl_file_fflush);
@@ -73,6 +76,7 @@ typedef struct FileSlot
 } FileSlot;
 
 #define MAX_SLOTS		10
+#define TEXT_LEN(d)	(VARSIZE(d) - VARHDRSZ)
 
 static FileSlot slots[MAX_SLOTS] = {{NULL, 0},{NULL, 0},{NULL, 0},{NULL, 0},{NULL, 0},
 			    	    {NULL, 0},{NULL, 0},{NULL, 0},{NULL, 0},{NULL, 0}};
@@ -147,6 +151,9 @@ utl_file_fopen(PG_FUNCTION_ARGS)
 	int	max_linesize;
 	const char *mode = NULL;
 	FILE *file;
+	char *fullname;
+	int aux_pos, aux_len;
+	
 
 	NOT_NULL_ARG(0);
 	NOT_NULL_ARG(1);
@@ -189,17 +196,170 @@ utl_file_fopen(PG_FUNCTION_ARGS)
 			CUSTOM_EXCEPTION(INVALID_MODE, "open mode is different than [R,W,A]");
 	}
 		
-	file = fopen("/tmp/test.txt", mode);
+	aux_pos = TEXT_LEN(loc);
+	aux_len = TEXT_LEN(filename);
+
+	fullname = palloc0(aux_pos + 1 + aux_len + 1);
+	memcpy(fullname, VARDATA(loc), aux_pos);
+	fullname[aux_pos] = '/';
+	memcpy(fullname + aux_pos + 1, VARDATA(filename), aux_len);
+	fullname[aux_pos + aux_len + 1] = '\0';
+
+	file = fopen(fullname, mode);
+	if (!file)
+	{
+		switch (errno)
+		{
+			case EACCES:
+			case ENAMETOOLONG:
+			case ENOENT:
+			case ENOTDIR:
+				CUSTOM_EXCEPTION(INVALID_PATH, strerror(errno));
+				break;
+			
+			default:
+				CUSTOM_EXCEPTION(INVALID_OPERATION, strerror(errno));
+		}
+	}
 
 	PG_RETURN_INT32(get_descriptor(file, max_linesize));
 }
 
 
+#define CHECK_LENGTH(l) \
+	if (l > max_linesize) \
+		CUSTOM_EXCEPTION(VALUE_ERROR, "buffer is too long");
+
+/* read line from file. set eof if is EOF */
+
+static text *
+get_line(FILE *f, int max_linesize, bool *iseof)
+{
+	int c;
+	char *buffer = NULL;
+	char *bpt;
+	int csize = 0;
+	text *result = NULL;
+	
+	bool eof = true;
+
+	buffer = palloc(max_linesize + 2);
+	bpt = buffer;
+	
+	while ((c = fgetc(f)) != EOF)
+	{
+		eof = false; 	/* I was able read one char */
+    
+		if (c == '\r')  /* lookin ahead \n */
+		{
+			c = fgetc(f);
+			if (c == EOF)
+				break;  /* last char */
+				
+			if (c != '\n')
+				ungetc(c, f);
+			/* skip \r\n */
+			break;
+		}
+		else if (c == '\n')
+			break;
+
+		CHECK_LENGTH(++csize);
+		*bpt++ = c;
+	}
+
+	if (!eof)
+	{
+		result = palloc(csize + VARHDRSZ);
+		memcpy(VARDATA(result), buffer, csize);
+		VARATT_SIZEP(result) = csize + VARHDRSZ;
+
+		*iseof = false;
+	}
+	else
+	{
+		switch (errno)
+		{
+			case 0:
+				break;
+
+			case EBADF:
+				CUSTOM_EXCEPTION(INVALID_OPERATION, "file descriptor isn't valid for reading");
+				break;
+
+			default:
+				CUSTOM_EXCEPTION(READ_ERROR, strerror(errno));
+				break;
+		}
+	    
+		*iseof = true;
+	}
+
+	pfree(buffer);
+	return result;
+}
+
+
+/*
+ * FUNCTION UTL_FILE.GET_LINE(file UTL_TYPE.FILE_TYPE)
+ *          RETURNS text;
+ *
+ * Reads one line from file.
+ *
+ * Exceptions:
+ *  NO_DATA_FOUND, INVALID_FILEHANDLE, INVALID_OPERATION, READ_ERROR
+ */
 Datum
 utl_file_get_line(PG_FUNCTION_ARGS)
 {
-	PG_RETURN_NULL();
+	int max_linesize;
+	FILE *f;
+	text *result;
+	bool iseof;
+
+	CHECK_FILE_HANDLE();
+	f = get_stream(PG_GETARG_INT32(0), &max_linesize);
+
+	result = get_line(f, max_linesize, &iseof);
+
+	if (iseof)
+	    	ereport(ERROR,
+				(errcode(ERRCODE_NO_DATA_FOUND),
+		    		 errmsg("no data found")));
+
+	PG_RETURN_TEXT_P(result);
 }
+
+
+/*
+ * FUNCTION UTL_FILE.GET_NEXTLINE(file UTL_TYPE.FILE_TYPE)
+ *          RETURNS text;
+ *
+ * Reads one line from file or retutns NULL
+ * by Steven Feuerstein.
+ *
+ * Exceptions:
+ *  INVALID_FILEHANDLE, INVALID_OPERATION, READ_ERROR
+ */
+Datum
+utl_file_get_nextline(PG_FUNCTION_ARGS)
+{
+	int max_linesize;
+	FILE *f;
+	text *result;
+	bool iseof;
+
+	CHECK_FILE_HANDLE();
+	f = get_stream(PG_GETARG_INT32(0), &max_linesize);
+
+	result = get_line(f, max_linesize, &iseof);
+
+	if (iseof)
+		PG_RETURN_NULL();
+
+	PG_RETURN_TEXT_P(result);
+}
+
 
 /*
  * FUNCTION UTL_FILE.PUT(file UTL_FILE.FILE_TYPE, buffer text)
@@ -244,9 +404,8 @@ utl_file___put(PG_FUNCTION_ARGS)
 		new_line = PG_GETARG_BOOL(2);
 
 	/* I have to check buffer's size */
-	bfsize = VARSIZE(buffer) - VARHDRSZ;
-	if (bfsize > max_linesize)
-		CUSTOM_EXCEPTION(VALUE_ERROR, "buffer is too long");
+	bfsize = TEXT_LEN(buffer);
+	CHECK_LENGTH(bfsize);
 	
 	tbuf = (char *) palloc(bfsize + 1);
 	memcpy(tbuf, VARDATA(buffer), bfsize);
@@ -263,10 +422,92 @@ utl_file___put(PG_FUNCTION_ARGS)
 }
 
 
+/*
+ * FUNCTION UTL_FILE.PUTF(file UTL_FILE.FILE_TYPE,
+ *			format text,
+ *			arg1 text,
+ *			arg2 text,
+ *			arg3 text,
+ *			arg4 text,
+ *			arg5 text)
+ *	    RETURNS bool;
+ *
+ * Puts formated data to file. Allows %s like subst symbol.
+ *
+ * Exception:
+ *  INVALID_FILEHANDLE, INVALID_OPERATION, WRITE_ERROR
+ */
 Datum
 utl_file_putf(PG_FUNCTION_ARGS)
 {
-	PG_RETURN_VOID();
+	FILE *f;
+	text *format;
+	int max_linesize;
+	int format_length;
+	char *fpt;
+	int cur_par = 0;
+	int cur_len = 0;
+
+	CHECK_FILE_HANDLE();
+	f = get_stream(PG_GETARG_INT32(0), &max_linesize);
+
+	NOT_NULL_ARG(1);
+	format = PG_GETARG_TEXT_P(1);
+	format_length = TEXT_LEN(format);
+
+	for (fpt = VARDATA(format); format_length > 0; fpt++, format_length--)
+	{
+		if (format_length == 1)
+		{		
+			/* last char */
+			CHECK_LENGTH(++cur_len);
+			if (fputc(*fpt, f) == EOF)
+				CHECK_ERRNO_PUT();
+			continue;
+		}
+		/* ansi compatible string */
+		if (fpt[0] == '\\' && fpt[1] == 'n')
+		{
+			CHECK_LENGTH(++cur_len);
+			if (fputc('\n', f) == EOF)
+				CHECK_ERRNO_PUT();
+			fpt++; format_length--;
+			continue;
+		}
+		if (fpt[0] == '%')
+		{
+			if (fpt[1] == '%')
+			{
+				CHECK_LENGTH(++cur_len);
+				if (fputc('%', f) == EOF)
+					CHECK_ERRNO_PUT();
+			}
+			else if (fpt[1] == 's' && ++cur_par <= 5 && !PG_ARGISNULL(cur_par + 1))
+			{
+				text *buffer = PG_GETARG_TEXT_P(cur_par + 1);
+				int bfsize = TEXT_LEN(buffer);
+				char *tbuf;
+
+				cur_len += bfsize;
+				CHECK_LENGTH(cur_len);
+
+				tbuf = (char *) palloc(bfsize + 1);
+				memcpy(tbuf, VARDATA(buffer), bfsize);
+				tbuf[bfsize] = '\0';
+
+				if (fputs(tbuf, f) == EOF)
+					CHECK_ERRNO_PUT();
+				
+			}
+			fpt++; format_length--;
+			continue;
+		}
+		CHECK_LENGTH(++cur_len);
+		if (fputc(fpt[0], f) == EOF)	
+			CHECK_ERRNO_PUT();
+	}
+
+	PG_RETURN_BOOL(true);
 }
 
 
