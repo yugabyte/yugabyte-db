33a62677c3b4945e3c800cbb799f155fe2dcbfa7
David E. Wheeler <david@justatheory.com>
2008-09-23 18:45:59 +0000

    Converted `sql/check.sql` to use `check_test()`.

--- sql/check.sql
+++ sql/check.sql
@@ -3,7 +3,7 @@
 
 -- $Id$
 
-SELECT plan(26);
+SELECT plan(39);
 
 -- This will be rolled back. :-)
 CREATE TABLE public.sometab(
@@ -16,113 +16,122 @@ CREATE TABLE public.sometab(
 /****************************************************************************/
 -- Test has_check().
 
-\echo ok 1 - test has_check( schema, table, description )
-SELECT is(
+SELECT * FROM check_test(
     has_check( 'public', 'sometab', 'public.sometab should have a check constraint' ),
-    'ok 1 - public.sometab should have a check constraint',
-    'has_check( schema, table, description ) should work'
+    true,
+    'has_check( schema, table, desc )',
+    'public.sometab should have a check constraint',
+    ''
 );
 
-\echo ok 3 - test has_check( table, description )
-SELECT is(
+SELECT * FROM check_test(
     has_check( 'sometab', 'sometab should have a check constraint' ),
-    'ok 3 - sometab should have a check constraint',
-    'has_check( table, description ) should work'
+    true,
+    'has_check( table, desc )',
+    'sometab should have a check constraint',
+    ''
 );
 
-\echo ok 5 - test has_check( table )
-SELECT is(
+SELECT * FROM check_test(
     has_check( 'sometab' ),
-    'ok 5 - Table sometab should have a check constraint',
-    'has_check( table ) should work'
+    true,
+    'has_check( table )',
+    'Table sometab should have a check constraint',
+    ''
 );
 
-\echo ok 7 - test has_check( schema, table, description ) fail
-SELECT is(
+SELECT * FROM check_test(
     has_check( 'pg_catalog', 'pg_class', 'pg_catalog.pg_class should have a check constraint' ),
-    'not ok 7 - pg_catalog.pg_class should have a check constraint
-# Failed test 7: "pg_catalog.pg_class should have a check constraint"',
-    'has_check( schema, table, description ) should fail properly'
+    false,
+    'has_check( schema, table, descr ) fail',
+    'pg_catalog.pg_class should have a check constraint',
+    ''
 );
 
-\echo ok 9 - test has_check( table, description ) fail
-SELECT is(
+SELECT * FROM check_test(
     has_check( 'pg_class', 'pg_class should have a check constraint' ),
-    'not ok 9 - pg_class should have a check constraint
-# Failed test 9: "pg_class should have a check constraint"',
-    'has_check( table, description ) should fail properly'
+    false,
+    'has_check( table, desc ) fail',
+    'pg_class should have a check constraint',
+    ''
 );
-UPDATE __tresults__ SET ok = true, aok = true WHERE numb IN( 7, 9 );
 
 /****************************************************************************/
 -- Test col_has_check().
 
-\echo ok 11 - test col_has_check( schema, table, column, description )
-SELECT is(
+SELECT * FROM check_test(
     col_has_check( 'public', 'sometab', 'name', 'public.sometab.name should be a pk' ),
-    'ok 11 - public.sometab.name should be a pk',
-    'col_has_check( schema, table, column, description ) should work'
+    true,
+    'col_has_check( sch, tab, col, desc )',
+    'public.sometab.name should be a pk',
+    ''
 );
 
-\echo ok 13 - test col_has_check( table, column, description )
-SELECT is(
+SELECT * FROM check_test(
     col_has_check( 'sometab', 'name', 'sometab.name should be a pk' ),
-    'ok 13 - sometab.name should be a pk',
-    'col_has_check( table, column, description ) should work'
+    true,
+    'col_has_check( tab, col, desc )',
+    'sometab.name should be a pk',
+    ''
 );
 
-\echo ok 15 - test col_has_check( table, column )
-SELECT is(
+SELECT * FROM check_test(
     col_has_check( 'sometab', 'name' ),
-    'ok 15 - Column sometab(name) should have a check constraint',
-    'col_has_check( table, column ) should work'
+    true,
+    'col_has_check( table, column )',
+    'Column sometab(name) should have a check constraint',
+    ''
 );
 
-\echo ok 17 - test col_has_check( schema, table, column, description ) fail
-SELECT is(
+SELECT * FROM check_test(
     col_has_check( 'public', 'sometab', 'id', 'public.sometab.id should be a pk' ),
-    'not ok 17 - public.sometab.id should be a pk
-# Failed test 17: "public.sometab.id should be a pk"
-#         have: {name}
-#         want: {id}',
-    'col_has_check( schema, table, column, description ) should fail properly'
+    false,
+    'col_has_check( sch, tab, col, desc ) fail',
+    'public.sometab.id should be a pk',
+    '        have: {name}
+        want: {id}'
 );
 
-\echo ok 19 - test col_has_check( table, column, description ) fail
-SELECT is(
+SELECT * FROM check_test(
     col_has_check( 'sometab', 'id', 'sometab.id should be a pk' ),
-    'not ok 19 - sometab.id should be a pk
-# Failed test 19: "sometab.id should be a pk"
-#         have: {name}
-#         want: {id}',
-    'col_has_check( table, column, description ) should fail properly'
+    false,
+    'col_has_check( tab, col, desc ) fail',
+    'sometab.id should be a pk',
+    '        have: {name}
+        want: {id}'
 );
-UPDATE __tresults__ SET ok = true, aok = true WHERE numb IN( 17, 19 );
 
 /****************************************************************************/
 -- Test col_has_check() with an array of columns.
 
-CREATE TABLE public.argh (id int not null, name text not null, check ( id IN (1, 2) AND name IN ('foo', 'bar')));
+CREATE TABLE public.argh (
+    id   INT  NOT NULL,
+    name TEXT NOT NULL,
+    CHECK ( id IN (1, 2) AND name IN ('foo', 'bar'))
+);
 
-\echo ok 21 - test col_has_check( schema, table, column[], description )
-SELECT is(
+SELECT * FROM check_test(
     col_has_check( 'public', 'argh', ARRAY['id', 'name'], 'id + name should be a pk' ),
-    'ok 21 - id + name should be a pk',
-    'col_has_check( schema, table, column[], description ) should work'
+    true,
+    'col_has_check( sch, tab, col[], desc )',
+    'id + name should be a pk',
+    ''
 );
 
-\echo ok 23 - test col_has_check( table, column[], description )
-SELECT is(
+SELECT * FROM check_test(
     col_has_check( 'argh', ARRAY['id', 'name'], 'id + name should be a pk' ),
-    'ok 23 - id + name should be a pk',
-    'col_has_check( table, column[], description ) should work'
+    true,
+    'col_has_check( tab, col[], desc )',
+    'id + name should be a pk',
+    ''
 );
 
-\echo ok 25 - test col_has_check( table, column[], description )
-SELECT is(
+SELECT * FROM check_test(
     col_has_check( 'argh', ARRAY['id', 'name'] ),
-    'ok 25 - Columns argh(id, name) should have a check constraint',
-    'col_has_check( table, column[] ) should work'
+    true,
+    'col_has_check( tab, col[] )',
+    'Columns argh(id, name) should have a check constraint',
+    ''
 );
 
 /****************************************************************************/
