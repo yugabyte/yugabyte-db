25a4913ef2c9ef58aef0189e1ec82abe0d1f678c
Takahiro Itagaki <itagaki.takahiro@gmail.com>
2008-10-15 01:43:17 +0000

    Adjust platform dependent regression test.

--- sql/orafunc.sql
+++ sql/orafunc.sql
@@ -101,7 +101,7 @@ select dbms_pipe.receive_message('test_int');
 select dbms_pipe.next_item_type();
 select dbms_pipe.unpack_message_number();
 
-select * from dbms_pipe.db_pipes where name = 'bob';
+select name, items, "limit", private, owner from dbms_pipe.db_pipes where name = 'bob';
 
 select PLVstr.betwn('Harry and Sally are very happy', 7, 9);
 select PLVstr.betwn('Harry and Sally are very happy', 7, 9, FALSE);
