f92b25f265a3efcc600696909e71ea93c7dbb66c
David E. Wheeler <david@justatheory.com>
2008-10-17 18:19:21 +0000

    Added `index_is_unique()` and `index_is_primary()`.

--- sql/index.sql
+++ sql/index.sql
@@ -3,20 +3,20 @@
 
 -- $Id$
 
-SELECT plan(75);
+SELECT plan(153);
 --SELECT * FROM no_plan();
 
 -- This will be rolled back. :-)
 SET client_min_messages = warning;
 CREATE TABLE public.sometab(
     id    INT NOT NULL PRIMARY KEY,
-    name  TEXT DEFAULT '' UNIQUE,
+    name  TEXT DEFAULT '',
     numb  NUMERIC(10, 2),
     myint NUMERIC(8)
 );
 CREATE INDEX idx_foo ON public.sometab(name);
 CREATE INDEX idx_bar ON public.sometab(name, numb);
-CREATE INDEX idx_baz ON public.sometab(LOWER(name));
+CREATE UNIQUE INDEX idx_baz ON public.sometab(LOWER(name));
 RESET client_min_messages;
 
 /****************************************************************************/
@@ -196,6 +196,186 @@ SELECT * FROM check_test(
 );
 
 /****************************************************************************/
+-- Test index_is_unique().
+SELECT * FROM check_test(
+    index_is_unique( 'public', 'sometab', 'idx_baz', 'whatever' ),
+    true,
+    'index_is_unique()',
+    'whatever',
+    ''
+);
+
+SELECT * FROM check_test(
+    index_is_unique( 'public', 'sometab', 'idx_baz' ),
+    true,
+    'index_is_unique() no desc',
+    'Index "idx_baz" should be unique',
+    ''
+);
+
+SELECT * FROM check_test(
+    index_is_unique( 'sometab', 'idx_baz' ),
+    true,
+    'index_is_unique() no schema',
+    'Index "idx_baz" should be unique',
+    ''
+);
+
+SELECT * FROM check_test(
+    index_is_unique( 'idx_baz' ),
+    true,
+    'index_is_unique() index only',
+    'Index "idx_baz" should be unique',
+    ''
+);
+
+SELECT * FROM check_test(
+    index_is_unique( 'public', 'sometab', 'sometab_pkey', 'whatever' ),
+    true,
+    'index_is_unique() on pk',
+    'whatever',
+    ''
+);
+
+SELECT * FROM check_test(
+    index_is_unique( 'public', 'sometab', 'sometab_pkey' ),
+    true,
+    'index_is_unique() on pk no desc',
+    'Index "sometab_pkey" should be unique',
+    ''
+);
+
+SELECT * FROM check_test(
+    index_is_unique( 'sometab', 'sometab_pkey' ),
+    true,
+    'index_is_unique() on pk no schema',
+    'Index "sometab_pkey" should be unique',
+    ''
+);
+
+SELECT * FROM check_test(
+    index_is_unique( 'sometab_pkey' ),
+    true,
+    'index_is_unique() on pk index only',
+    'Index "sometab_pkey" should be unique',
+    ''
+);
+
+SELECT * FROM check_test(
+    index_is_unique( 'public', 'sometab', 'idx_bar', 'whatever' ),
+    false,
+    'index_is_unique() fail',
+    'whatever',
+    ''
+);
+
+SELECT * FROM check_test(
+    index_is_unique( 'public', 'sometab', 'idx_bar' ),
+    false,
+    'index_is_unique() fail no desc',
+    'Index "idx_bar" should be unique',
+    ''
+);
+
+SELECT * FROM check_test(
+    index_is_unique( 'sometab', 'idx_bar' ),
+    false,
+    'index_is_unique() fail no schema',
+    'Index "idx_bar" should be unique',
+    ''
+);
+
+SELECT * FROM check_test(
+    index_is_unique( 'idx_bar' ),
+    false,
+    'index_is_unique() fail index only',
+    'Index "idx_bar" should be unique',
+    ''
+);
+
+SELECT * FROM check_test(
+    index_is_unique( 'blahblah' ),
+    false,
+    'index_is_unique() no such index',
+    'Index "blahblah" should be unique',
+    ''
+);
+
+/****************************************************************************/
+-- Test index_is_primary().
+SELECT * FROM check_test(
+    index_is_primary( 'public', 'sometab', 'sometab_pkey', 'whatever' ),
+    true,
+    'index_is_primary()',
+    'whatever',
+    ''
+);
+
+SELECT * FROM check_test(
+    index_is_primary( 'public', 'sometab', 'sometab_pkey' ),
+    true,
+    'index_is_primary() no desc',
+    'Index "sometab_pkey" should be on a primary key',
+    ''
+);
+
+SELECT * FROM check_test(
+    index_is_primary( 'sometab', 'sometab_pkey' ),
+    true,
+    'index_is_primary() no schema',
+    'Index "sometab_pkey" should be on a primary key',
+    ''
+);
+
+SELECT * FROM check_test(
+    index_is_primary( 'sometab_pkey' ),
+    true,
+    'index_is_primary() index only',
+    'Index "sometab_pkey" should be on a primary key',
+    ''
+);
+
+SELECT * FROM check_test(
+    index_is_primary( 'public', 'sometab', 'idx_baz', 'whatever' ),
+    false,
+    'index_is_primary() fail',
+    'whatever',
+    ''
+);
+
+SELECT * FROM check_test(
+    index_is_primary( 'public', 'sometab', 'idx_baz' ),
+    false,
+    'index_is_primary() fail no desc',
+    'Index "idx_baz" should be on a primary key',
+    ''
+);
+
+SELECT * FROM check_test(
+    index_is_primary( 'sometab', 'idx_baz' ),
+    false,
+    'index_is_primary() fail no schema',
+    'Index "idx_baz" should be on a primary key',
+    ''
+);
+
+SELECT * FROM check_test(
+    index_is_primary( 'idx_baz' ),
+    false,
+    'index_is_primary() fail index only',
+    'Index "idx_baz" should be on a primary key',
+    ''
+);
+
+SELECT * FROM check_test(
+    index_is_primary( 'blahblah' ),
+    false,
+    'index_is_primary() no such index',
+    'Index "blahblah" should be on a primary key',
+    ''
+);
+
+/****************************************************************************/
 -- Test is_clustered().
 SELECT * FROM check_test(
     is_clustered( 'public', 'sometab', 'idx_bar', 'whatever' ),
@@ -225,6 +405,39 @@ SELECT * FROM check_test(
     is_clustered( 'idx_bar' ),
     false,
     'is_clustered() fail index only',
+    'Table should be clustered on index "idx_bar"',
+    ''
+);
+
+CLUSTER public.sometab using idx_bar;
+SELECT * FROM check_test(
+    is_clustered( 'public', 'sometab', 'idx_bar', 'whatever' ),
+    true,
+    'is_clustered()',
+    'whatever',
+    ''
+);
+
+SELECT * FROM check_test(
+    is_clustered( 'public', 'sometab', 'idx_bar' ),
+    true,
+    'is_clustered() no desc',
+    'Table public.sometab should be clustered on index "idx_bar"',
+    ''
+);
+
+SELECT * FROM check_test(
+    is_clustered( 'sometab', 'idx_bar' ),
+    true,
+    'is_clustered() no schema',
+    'Table sometab should be clustered on index "idx_bar"',
+    ''
+);
+
+SELECT * FROM check_test(
+    is_clustered( 'idx_bar' ),
+    true,
+    'is_clustered() index only',
     'Table should be clustered on index "idx_bar"',
     ''
 );
