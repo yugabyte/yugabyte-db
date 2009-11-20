54f5904860534090f7d96d56a9e0c6f541100f65
David E. Wheeler <david@justatheory.com>
2009-11-20 15:45:28 +0900

    Add `roles_are()`.

--- sql/roletap.sql
+++ sql/roletap.sql
@@ -1,7 +1,7 @@
 \unset ECHO
 \i test_setup.sql
 
-SELECT plan(24);
+SELECT plan(39);
 --SELECT * FROM no_plan();
 
 /****************************************************************************/
@@ -63,6 +63,58 @@ SELECT * FROM check_test(
     'hasnt_role(nonexistent role, desc)',
     'desc',
     ''
+);
+
+/****************************************************************************/
+-- Test roles_are().
+
+CREATE FUNCTION ___myroles(ex text) RETURNS NAME[] AS $$
+    SELECT COALESCE(ARRAY( SELECT rolname FROM pg_catalog.pg_roles WHERE rolname <> $1 ), '{}'::name[]);;
+$$ LANGUAGE SQL;
+
+SELECT * FROM check_test(
+    roles_are( ___myroles(''), 'whatever' ),
+    true,
+    'roles_are(roles, desc)',
+    'whatever',
+    ''
+);
+
+SELECT * FROM check_test(
+    roles_are( ___myroles('') ),
+    true,
+    'roles_are(roles)',
+    'There should be the correct roles',
+    ''
+);
+
+SELECT * FROM check_test(
+    roles_are( array_append(___myroles(''), '__howdy__'), 'whatever' ),
+    false,
+    'roles_are(roles, desc) missing',
+    'whatever',
+    '    Missing roles:
+        __howdy__'
+);
+
+SELECT * FROM check_test(
+    roles_are( ___myroles(current_role), 'whatever' ),
+    false,
+    'roles_are(roles, desc) extras',
+    'whatever',
+    '    Extra roles:
+        ' || current_role
+);
+
+SELECT * FROM check_test(
+    roles_are( array_append(___myroles(current_role), '__howdy__'), 'whatever' ),
+    false,
+    'roles_are(roles, desc) missing and extras',
+    'whatever',
+    '    Extra roles:
+        ' || current_role || '
+    Missing roles:
+        __howdy__'
 );
 
 /****************************************************************************/
