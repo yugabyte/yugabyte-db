7722b4f6a791a337c5531e2e6941547fc9e833e5
David E. Wheeler <david@justatheory.com>
2009-12-16 16:12:57 -0800

    Add backward-compatible `collect_tap()`.

--- sql/util.sql
+++ sql/util.sql
@@ -1,7 +1,7 @@
 \unset ECHO
 \i test_setup.sql
 
-SELECT plan(23);
+SELECT plan(24);
 --SELECT * FROM no_plan();
 
 SELECT is( pg_typeof(42), 'integer', 'pg_type(int) should work' );
@@ -60,21 +60,30 @@ SELECT matches(
 
 /****************************************************************************/
 -- Test collect_tap().
-
--- Create fake collect_tap() to prevent an error on 8.3 and lower.
-CREATE FUNCTION collect_tap(text, text, text ) RETURNS text AS $$
-    SELECT fail('Fake collect_tap(text, text, text) was called!')
-$$ LANGUAGE SQL;
-
 SELECT is(
-    CASE WHEN pg_version_num() >= 80400
-    THEN collect_tap('foo', 'bar', 'baz')
-    ELSE collect_tap('{foo, bar, baz}')
-    END,
+    collect_tap(ARRAY['foo', 'bar', 'baz']),
     'foo
 bar
 baz',
-    'collect_tap() should simply collect tap'
+    'collect_tap(text[]) should simply collect tap'
+);
+
+CREATE FUNCTION test_variadic() RETURNS TEXT AS $$
+BEGIN
+    IF pg_version_num() >= 80400 THEN
+        RETURN collect_tap('foo', 'bar', 'baz');
+    ELSE
+        RETURN collect_tap(ARRAY['foo', 'bar', 'baz']);
+    END IF;
+END;
+$$ LANGUAGE plpgsql;
+
+SELECT is(
+    test_variadic(),
+    'foo
+bar
+baz',
+    'variadic collect_tap() should simply collect tap'
 );
 
 /****************************************************************************/
