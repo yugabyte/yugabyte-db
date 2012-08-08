31b255318ded2547a9dbc0425768e9bb14427aaa
Mitsuru Hasegawa <hasegawa@metrosystems.co.jp>
2012-08-08 20:27:44 +0900

    Bug fix:EXECUTEコマンド中にエラー終了したあとにSELECTを実行すると 想定外の実行計画になる問題を修正した。
    EXECUTEコマンド実行時に、ProcessUtility_hookの初期処理で外部変数に
    ステートメント名を保存し、終了処理でNULLに戻すが、ProcessUtility_hook
    の実行中にエラー終了した場合は、外部変数にステートメント名が設定
    されたままだった。
    このため、その後SELECT等の通常のクエリを発行すると、外部変数に
    残っているステートメント名を使用して、プランキャッシュからクエリ
    文字列を取得し、その文字列に指定されているヒントを使用していた。

--- sql/prepare.sql
+++ sql/prepare.sql
@@ -127,3 +127,5 @@ EXPLAIN (COSTS false) EXECUTE p7 (10);
 -- error case
 /*+ NestLoop(t1 t2) */
 EXPLAIN (COSTS false) EXECUTE p8 (10);
+/*+ NestLoop(t1 t2) */
+EXPLAIN (COSTS false) SELECT count(*) FROM t1, t2 WHERE t1.id = t2.id AND t1.id > 10;
