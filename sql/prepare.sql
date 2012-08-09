ac1930b3afba16a04dc61b4dc27daee38c9ba731
Mitsuru Hasegawa <hasegawa@metrosystems.co.jp>
2012-08-09 17:02:56 +0900

    Revert "Bug fix:EXECUTEコマンド中にエラー終了したあとにSELECTを実行すると"
    This reverts commit 31b255318ded2547a9dbc0425768e9bb14427aaa.
    
    使用したコールバックは、エラーメッセージにCONTEXTを追加するためのもの
    だったため、エラー時のみでなく、INFOなどのほかのレベルでも呼び出される
    ため、planner_hook呼び出しまでの間にメッセージ出力がある場合は、stmt_name
    がNULLに初期化されてしまう。

--- sql/prepare.sql
+++ sql/prepare.sql
@@ -127,5 +127,3 @@ EXPLAIN (COSTS false) EXECUTE p7 (10);
 -- error case
 /*+ NestLoop(t1 t2) */
 EXPLAIN (COSTS false) EXECUTE p8 (10);
-/*+ NestLoop(t1 t2) */
-EXPLAIN (COSTS false) SELECT count(*) FROM t1, t2 WHERE t1.id = t2.id AND t1.id > 10;
