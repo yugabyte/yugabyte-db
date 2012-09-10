bab5ce52e50bcc378cc2c73f415d561316ca9001
David E. Wheeler <david@justatheory.com>
2012-09-10 16:52:04 -0700

    Support 9.2 and earlier in single function.
    Makes it much easier to maintain extension upgrade scripts.

--- sql/pgtap--0.90.0--0.91.0.sql
+++ sql/pgtap--0.90.0--0.91.0.sql
@@ -1,27 +1,25 @@
-DO $$
-BEGIN
-    IF pg_version_num() < 9.0 THEN
-        EXECUTE $E$
-            CREATE FUNCTION pg_tablespace_location(
-                OID
-            ) RETURNS TEXT LANGUAGE SQL AS $F$
-                SELECT spclocation
-                  FROM pg_catalog.pg_tablespace
-                 WHERE OID = $1;
-            $F$;
-        $E$;
-    END IF;
-END;
-$$;
-
 CREATE OR REPLACE FUNCTION has_tablespace( NAME, TEXT, TEXT )
 RETURNS TEXT AS $$
-    SELECT ok(
-        EXISTS(
-            SELECT true
-              FROM pg_catalog.pg_tablespace
-             WHERE spcname = $1
-               AND pg_tablespace_location(oid) = $2
-        ), $3
-    );
-$$ LANGUAGE sql;
+BEGIN
+    IF pg_version_num() >= 90200 THEN
+        RETURN ok(
+            EXISTS(
+                SELECT true
+                  FROM pg_catalog.pg_tablespace
+                 WHERE spcname = $1
+                   AND pg_tablespace_location(oid) = $2
+            ), $3
+        );
+    ELSE
+        RETURN ok(
+            EXISTS(
+                SELECT true
+                  FROM pg_catalog.pg_tablespace
+                 WHERE spcname = $1
+                   AND spclocation = $2
+            ), $3
+        );
+    END IF;
+END;
+$$ LANGUAGE plpgsql;
+
