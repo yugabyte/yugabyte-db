ea2ef1580b909c03beead57b7c80b9c2920f04d4
David E. Wheeler <david@justatheory.com>
2013-01-09 12:02:10 -0800

    Add `rel_owner_is()`.

--- sql/pgtap--0.91.0--0.92.0.sql
+++ sql/pgtap--0.91.0--0.92.0.sql
@@ -84,3 +84,73 @@ CREATE OR REPLACE FUNCTION hasnt_composite ( NAME )
 RETURNS TEXT AS $$
     SELECT hasnt_composite( $1, 'Composite type ' || quote_ident($1) || ' should not exist' );
 $$ LANGUAGE SQL;
+
+CREATE OR REPLACE FUNCTION _get_rel_owner ( NAME, NAME )
+RETURNS NAME AS $$
+    SELECT pg_catalog.pg_get_userbyid(c.relowner)
+      FROM pg_catalog.pg_class c
+      JOIN pg_catalog.pg_namespace n ON n.oid = c.relnamespace
+     WHERE n.nspname = $1
+       AND c.relname = $2
+$$ LANGUAGE SQL;
+
+CREATE OR REPLACE FUNCTION _get_rel_owner ( NAME )
+RETURNS NAME AS $$
+    SELECT pg_catalog.pg_get_userbyid(c.relowner)
+      FROM pg_catalog.pg_class c
+     WHERE c.relname = $1
+       AND pg_catalog.pg_table_is_visible(c.oid)
+$$ LANGUAGE SQL;
+
+-- rel_owner_is ( schema, relation, user, description )
+CREATE OR REPLACE FUNCTION rel_owner_is ( NAME, NAME, NAME, TEXT )
+RETURNS TEXT AS $$
+DECLARE
+    owner NAME := _get_rel_owner($1, $2);
+BEGIN
+    -- Make sure the relation exists.
+    IF owner IS NULL THEN
+        RETURN ok(FALSE, $4) || E'\n' || diag(
+            E'    Relation ' || quote_ident($1) || '.' || quote_ident($2) || ' does not exist'
+        );
+    END IF;
+
+    RETURN is(owner, $3, $4);
+END;
+$$ LANGUAGE plpgsql;
+
+-- rel_owner_is ( schema, relation, user )
+CREATE OR REPLACE FUNCTION rel_owner_is ( NAME, NAME, NAME )
+RETURNS TEXT AS $$
+    SELECT rel_owner_is(
+        $1, $2, $3,
+        'Relation ' || quote_ident($1) || '.' || quote_ident($2) || ' should be owned by ' || quote_ident($3)
+    );
+$$ LANGUAGE sql;
+
+-- rel_owner_is ( relation, user, description )
+CREATE OR REPLACE FUNCTION rel_owner_is ( NAME, NAME, TEXT )
+RETURNS TEXT AS $$
+DECLARE
+    owner NAME := _get_rel_owner($1);
+BEGIN
+    -- Make sure the relation exists.
+    IF owner IS NULL THEN
+        RETURN ok(FALSE, $3) || E'\n' || diag(
+            E'    Relation ' || quote_ident($1) || ' does not exist'
+        );
+    END IF;
+
+    RETURN is(owner, $2, $3);
+END;
+$$ LANGUAGE plpgsql;
+
+-- rel_owner_is ( relation, user )
+CREATE OR REPLACE FUNCTION rel_owner_is ( NAME, NAME )
+RETURNS TEXT AS $$
+    SELECT rel_owner_is(
+        $1, $2,
+        'Relation ' || quote_ident($1) || ' should be owned by ' || quote_ident($2)
+    );
+$$ LANGUAGE sql;
+
--- test/sql/ownership.sql
+++ test/sql/ownership.sql
@@ -1,11 +1,31 @@
 \unset ECHO
 \i test/setup.sql
 
-SELECT plan(12);
+SELECT plan(54);
 --SELECT * FROM no_plan();
 
+-- This will be rolled back. :-)
+SET client_min_messages = warning;
+CREATE TABLE public.sometab(
+    id      INT NOT NULL PRIMARY KEY,
+    name    TEXT DEFAULT '',
+    numb    NUMERIC(10, 2),
+    "myInt" NUMERIC(8)
+);
+
+CREATE TYPE public.sometype AS (
+    id    INT,
+    name  TEXT
+);
+
+CREATE SEQUENCE public.someseq;
+
+CREATE SCHEMA someschema;
+
+RESET client_min_messages;
+
 /****************************************************************************/
-SELECT * From check_test(
+SELECT * FROM check_test(
     db_owner_is(current_database(), current_user, 'mumble'),
 	true,
     'db_owner_is(db, user, desc)',
@@ -13,7 +33,7 @@ SELECT * From check_test(
     ''
 );
 
-SELECT * From check_test(
+SELECT * FROM check_test(
     db_owner_is(current_database(), current_user),
 	true,
     'db_owner_is(db, user)',
@@ -21,7 +41,7 @@ SELECT * From check_test(
     ''
 );
 
-SELECT * From check_test(
+SELECT * FROM check_test(
     db_owner_is('__not__' || current_database(), current_user, 'mumble'),
 	false,
     'db_owner_is(non-db, user)',
@@ -29,13 +49,129 @@ SELECT * From check_test(
     '    Database __not__' || current_database() || ' does not exist'
 );
 
-SELECT * From check_test(
+SELECT * FROM check_test(
     db_owner_is(current_database(), '__not__' || current_user, 'mumble'),
 	false,
     'db_owner_is(db, non-user)',
     'mumble',
     '        have: ' || current_user || '
         want: __not__' || current_user
+);
+
+/****************************************************************************/
+-- Test rel_owner_is() with a table.
+SELECT * FROM check_test(
+    rel_owner_is('public', 'sometab', current_user, 'mumble'),
+	true,
+    'rel_owner_is(sch, tab, user, desc)',
+    'mumble',
+    ''
+);
+
+SELECT * FROM check_test(
+    rel_owner_is('public', 'sometab', current_user),
+	true,
+    'rel_owner_is(sch, tab, user)',
+    'Relation public.sometab should be owned by ' || current_user,
+    ''
+);
+
+SELECT * FROM check_test(
+    rel_owner_is('__not__public', 'sometab', current_user, 'mumble'),
+	false,
+    'rel_owner_is(non-sch, tab, user)',
+    'mumble',
+    '    Relation __not__public.sometab does not exist'
+);
+
+SELECT * FROM check_test(
+    rel_owner_is('public', '__not__sometab', current_user, 'mumble'),
+	false,
+    'rel_owner_is(sch, non-tab, user)',
+    'mumble',
+    '    Relation public.__not__sometab does not exist'
+);
+
+SELECT * FROM check_test(
+    rel_owner_is('sometab', current_user, 'mumble'),
+	true,
+    'rel_owner_is(tab, user, desc)',
+    'mumble',
+    ''
+);
+
+SELECT * FROM check_test(
+    rel_owner_is('sometab', current_user),
+	true,
+    'rel_owner_is(tab, user)',
+    'Relation sometab should be owned by ' || current_user,
+    ''
+);
+
+SELECT * FROM check_test(
+    rel_owner_is('__not__sometab', current_user, 'mumble'),
+	false,
+    'rel_owner_is(non-tab, user)',
+    'mumble',
+    '    Relation __not__sometab does not exist'
+);
+
+/****************************************************************************/
+-- Test rel_owner_is() with a schema.
+SELECT * FROM check_test(
+    rel_owner_is('public', 'someseq', current_user, 'mumble'),
+	true,
+    'rel_owner_is(sch, seq, user, desc)',
+    'mumble',
+    ''
+);
+
+SELECT * FROM check_test(
+    rel_owner_is('public', 'someseq', current_user),
+	true,
+    'rel_owner_is(sch, seq, user)',
+    'Relation public.someseq should be owned by ' || current_user,
+    ''
+);
+
+SELECT * FROM check_test(
+    rel_owner_is('__not__public', 'someseq', current_user, 'mumble'),
+	false,
+    'rel_owner_is(non-sch, seq, user)',
+    'mumble',
+    '    Relation __not__public.someseq does not exist'
+);
+
+SELECT * FROM check_test(
+    rel_owner_is('public', '__not__someseq', current_user, 'mumble'),
+	false,
+    'rel_owner_is(sch, non-seq, user)',
+    'mumble',
+    '    Relation public.__not__someseq does not exist'
+);
+
+SELECT * FROM check_test(
+    rel_owner_is('someseq', current_user, 'mumble'),
+	true,
+    'rel_owner_is(seq, user, desc)',
+    'mumble',
+    ''
+);
+
+SELECT * FROM check_test(
+    rel_owner_is('someseq', current_user),
+	true,
+    'rel_owner_is(seq, user)',
+    'Relation someseq should be owned by ' || current_user,
+    ''
+);
+
+SELECT * FROM check_test(
+    rel_owner_is('__not__someseq', current_user, 'mumble'),
+	false,
+    'rel_owner_is(non-seq, user)',
+    'mumble',
+    '    Relation __not__someseq does not exist'
 );
 
 /****************************************************************************/
