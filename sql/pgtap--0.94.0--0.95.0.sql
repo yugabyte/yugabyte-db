83a839b506fb5b06c77872405fedb31541b98834
Jim Nasby <JNasby@enova.com>
2014-07-31 19:07:32 -0500

    Add upgrade script

---
+++ sql/pgtap--0.94.0--0.95.0.sql
@@ -0,0 +1,28 @@
+CREATE OR REPLACE FUNCTION is_member_of( NAME, NAME[], TEXT )
+RETURNS TEXT AS $$
+DECLARE
+    missing text[];
+BEGIN
+    IF NOT _has_role($1) THEN
+        RETURN fail( $3 ) || E'\n' || diag (
+            '    Role ' || quote_ident($1) || ' does not exist'
+        );
+    END IF;
+
+    SELECT ARRAY(
+        SELECT quote_ident($2[i])
+          FROM generate_series(1, array_upper($2, 1)) s(i)
+          LEFT JOIN pg_catalog.pg_roles r ON rolname = $2[i]
+         WHERE r.oid IS NULL
+            OR NOT r.oid = ANY ( _grolist($1) )
+         ORDER BY s.i
+    ) INTO missing;
+    IF missing[1] IS NULL THEN
+        RETURN ok( true, $3 );
+    END IF;
+    RETURN ok( false, $3 ) || E'\n' || diag(
+        '    Users missing from the ' || quote_ident($1) || E' group:\n        ' ||
+        array_to_string( missing, E'\n        ')
+    );
+END;
+$$ LANGUAGE plpgsql;
