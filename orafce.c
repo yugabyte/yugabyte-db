5f45d4841c4752ee7ef17533ece95a5ed3398399
Pavel Stehule <pavel.stehule@gooddata.com>
2015-10-10 19:09:50 +0200

    little bit better extension initialization

--- orafce.c
+++ orafce.c
@@ -1,7 +1,11 @@
 #include "postgres.h"
+#include "storage/lwlock.h"
+#include "storage/shmem.h"
 #include "utils/guc.h"
+
 #include "orafce.h"
 #include "builtins.h"
+#include "pipe.h"
 
 /*  default value */
 char  *nls_date_format = NULL;
@@ -9,6 +13,11 @@ char  *nls_date_format = NULL;
 void
 _PG_init(void)
 {
+	EmitWarningsOnPlaceholders("orafce");
+
+	RequestAddinShmemSpace(SHMEMMSGSZ);
+	RequestAddinLWLocks(1);
+
 	/* Define custom GUC variables. */
 	DefineCustomStringVariable("orafce.nls_date_format",
 									"Emulate oracle's date output behaviour.",
