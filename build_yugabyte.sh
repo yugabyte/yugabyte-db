bed11ec06ae889f8b3ca5f230000713af1be8aba
Mikhail Bautin <mbautin@users.noreply.github.com>
2016-02-09 12:13:58 -0800

    Automatically setting a flag for rebuilding dependencies in build_yugabyte.sh

--- build_yugabyte.sh
+++ build_yugabyte.sh
@@ -8,5 +8,18 @@ build_dir="$project_dir/build/debug"
 mkdir -p "$build_dir"
 cd "$build_dir"
 
-cmake "$project_dir"
-make
+# Even though thirdparty/build-if-necessary.sh has its own build stamp file,
+# the logic here is simplified: we only build third-party dependencies once and
+# never rebuild it.
+
+thirdparty_built_flag_file="$PWD/built_thirdparty"
+
+if [ -f "$thirdparty_built_flag_file" ]; then
+  export NO_REBUILT_THIRDPARTY=1
+  cmake "${cmake_args[@]}"
+fi
+
+cmake -DKUDU_LINK=dynamic "$project_dir"
+
+touch "$thirdparty_built_flag_file"
+
