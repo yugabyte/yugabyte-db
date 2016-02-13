9367dfe74e8a19e3003adfefe0994c096c4addbf
Mikhail Bautin <mbautin@users.noreply.github.com>
2016-02-13 10:01:41 -0800

    A really simple load test (key/value write only) is now working

--- src/kudu/benchmarks/yb_load_test_tool.cc
+++ src/kudu/benchmarks/yb_load_test_tool.cc
@@ -27,8 +27,20 @@
 #include "kudu/util/thread.h"
 
 DEFINE_string(
-    yb_load_test_master_addresses, "localhost",
-    "Addresses of masters for the cluster to operate on");
+  yb_load_test_master_addresses, "localhost",
+  "Addresses of masters for the cluster to operate on");
+
+DEFINE_string(
+  yb_load_test_table_name, "yb_load_test",
+  "Table name to use for YugaByte load testing");
+
+DEFINE_int64(
+  yb_load_test_num_rows, 1000000,
+  "Number of rows to insert");
+
+DEFINE_int64(
+  yb_load_test_progress_report_frequency, 10000,
+  "Report progress once per this number of rows");
 
 using namespace kudu::client;
 using kudu::client::sp::shared_ptr;
@@ -47,6 +59,21 @@ int main(int argc, char* argv[]) {
      .Build(&client));
   shared_ptr<KuduSession> session(client->NewSession());
   session->SetFlushMode(KuduSession::FlushMode::MANUAL_FLUSH);
+  session->SetTimeoutMillis(60000);
+
+  const string table_name(FLAGS_yb_load_test_table_name);
+
+  LOG(INFO) << "Checking if table '" << table_name << "' already exists";
+  {
+    KuduSchema existing_schema;
+    if (client->GetTableSchema(table_name, &existing_schema).ok()) {
+      LOG(INFO) << "Table '" << table_name << "' already exists, deleting";
+      // Table with the same name already exists, drop it.
+      CHECK_OK(client->DeleteTable(table_name));
+    } else {
+      LOG(INFO) << "Table '" << table_name << "' does not exist yet";
+    }
+  }
 
   LOG(INFO) << "Building schema";
   KuduSchemaBuilder schemaBuilder;
@@ -57,26 +84,33 @@ int main(int argc, char* argv[]) {
 
   LOG(INFO) << "Creating table";
   gscoped_ptr<KuduTableCreator> table_creator(client->NewTableCreator());
-  const string kTableName = "yb_test";
-  Status table_creation_status = table_creator->table_name(kTableName).schema(&schema).Create();
-  LOG(INFO) << "Table creation status message: " << table_creation_status.message().ToString();
+  Status table_creation_status = table_creator->table_name(table_name).schema(&schema).Create();
+  if (!table_creation_status.ok()) {
+    LOG(INFO) << "Table creation status message: " << table_creation_status.message().ToString();
+  }
 
-  // "Table already exists" is OK.
-  // TODO: drop and re-create table.
   if (table_creation_status.message().ToString().find("Table already exists") ==
       std::string::npos) {
     CHECK_OK(table_creation_status);
   }
 
   shared_ptr<KuduTable> table;
-  CHECK_OK(client->OpenTable(kTableName, &table));
+  CHECK_OK(client->OpenTable(table_name, &table));
 
-  for (int i = 0; i < 1000; ++i) {
+  LOG(INFO) << "Starting load test";
+  for (int64 i = 0; i < FLAGS_yb_load_test_num_rows; ++i) {
     gscoped_ptr<KuduInsert> insert(table->NewInsert());
-    insert->mutable_row()->SetString("k", "key" + std::to_string(i));
-    insert->mutable_row()->SetString("v", "value" + std::to_string(i));
+    string key(strings::Substitute("key$0", i));
+    string value(strings::Substitute("value$0", i));
+    insert->mutable_row()->SetString("k", key.c_str());
+    insert->mutable_row()->SetString("v", value.c_str());
+    VLOG(1) << "Insertion: " + insert->ToString();
     CHECK_OK(session->Apply(insert.release()));
+    VLOG(1) << "Flushing pending writes at i = " << i;
     CHECK_OK(session->Flush());
+    if (i % FLAGS_yb_load_test_progress_report_frequency == 0) {
+      LOG(INFO) << "i = " << i;
+    }
   }
 
   CHECK_OK(session->Close());
