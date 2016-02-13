dee01df148fbc2713abd7dee767272b6692be556
Mikhail Bautin <mbautin@users.noreply.github.com>
2016-02-12 16:01:01 -0800

    Write some data to Kudu

--- src/kudu/benchmarks/yb_load_test_tool.cc
+++ src/kudu/benchmarks/yb_load_test_tool.cc
@@ -26,13 +26,13 @@
 #include "kudu/util/subprocess.h"
 #include "kudu/util/thread.h"
 
-
 DEFINE_string(
     yb_load_test_master_addresses, "localhost",
     "Addresses of masters for the cluster to operate on");
 
 using namespace kudu::client;
-using namespace kudu::client::sp;
+using kudu::client::sp::shared_ptr;
+using kudu::Status;
 
 int main(int argc, char* argv[]) {
   gflags::SetUsageMessage(
@@ -46,15 +46,38 @@ int main(int argc, char* argv[]) {
      .add_master_server_addr(FLAGS_yb_load_test_master_addresses)
      .Build(&client));
   shared_ptr<KuduSession> session(client->NewSession());
+  session->SetFlushMode(KuduSession::FlushMode::MANUAL_FLUSH);
 
+  LOG(INFO) << "Building schema";
   KuduSchemaBuilder schemaBuilder;
-  schemaBuilder.AddColumn("k")->PrimaryKey()->Type(KuduColumnSchema::STRING);
-  schemaBuilder.AddColumn("v")->Type(KuduColumnSchema::STRING);
+  schemaBuilder.AddColumn("k")->PrimaryKey()->Type(KuduColumnSchema::STRING)->NotNull();
+  schemaBuilder.AddColumn("v")->Type(KuduColumnSchema::STRING)->NotNull();
   KuduSchema schema;
   CHECK_OK(schemaBuilder.Build(&schema));
 
+  LOG(INFO) << "Creating table";
   gscoped_ptr<KuduTableCreator> table_creator(client->NewTableCreator());
-  CHECK_OK(table_creator->table_name("yb_test").schema(&schema).Create());
+  const string kTableName = "yb_test";
+  Status table_creation_status = table_creator->table_name(kTableName).schema(&schema).Create();
+  LOG(INFO) << "Table creation status message: " << table_creation_status.message().ToString();
+
+  // "Table already exists" is OK.
+  // TODO: drop and re-create table.
+  if (table_creation_status.message().ToString().find("Table already exists") ==
+      std::string::npos) {
+    CHECK_OK(table_creation_status);
+  }
+
+  shared_ptr<KuduTable> table;
+  CHECK_OK(client->OpenTable(kTableName, &table));
+
+  for (int i = 0; i < 1000; ++i) {
+    gscoped_ptr<KuduInsert> insert(table->NewInsert());
+    insert->mutable_row()->SetString("k", "key" + std::to_string(i));
+    insert->mutable_row()->SetString("v", "value" + std::to_string(i));
+    CHECK_OK(session->Apply(insert.release()));
+    CHECK_OK(session->Flush());
+  }
 
   CHECK_OK(session->Close());
   LOG(INFO) << "Test completed";
