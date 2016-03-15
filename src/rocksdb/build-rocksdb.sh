4a3bb6599d9534402ef0819aae302698dc6bbb78
Mikhail Bautin <mbautin@users.noreply.github.com>
2016-03-14 17:05:47 -0700

    Integrating RocksDB build into the YB CMake

---
+++ src/rocksdb/build-rocksdb.sh
@@ -0,0 +1,121 @@
+#!/bin/bash
+
+set -euo pipefail
+
+print_help() {
+  echo >&2 <<-EOT
+Usage: ${0##*/} <options>
+
+Links RocksDB sources into the build directory and builds it.
+
+Options:
+  --build-dir <build_dir>
+    The base build directory, e.g. yugabyte/build/debug or yugabyte/build/release.
+    This is required.
+  --build-type <build_type>
+    The build type (e.g. "debug" or "release"). This must match the build directory.
+    This is optional. If specified, this is expected to match the final component of the build
+    directory.
+  --make-parallelism
+    Specify the parallelism of the make command.
+EOT
+}
+
+build_dir=""
+build_type=""
+make_parallelism=""
+debug_level=""
+
+while [ $# -ne 0 ]; do
+  case "$1" in
+    --build-type)
+      build_type=$2
+      shift
+    ;;
+    --build-dir)
+      build_dir=$2
+      shift
+    ;;
+    --make-parallelism)
+      make_parallelism=$2
+      shift
+    ;;
+    --debug-level)
+      debug_level=$2
+      shift
+    ;;
+    *)
+      echo "Invalid option: $1" >&2
+      exit 1
+  esac
+  shift
+done
+
+if [ -z "$build_dir" ]; then
+  echo "--build-dir is not specified"
+fi
+
+if [ ! -d "$build_dir" ]; then
+  echo "Directory specified by --build-dir ('$build_dir') does not exist or is not a directory" >&2
+  exit 1
+fi
+
+# Normalize the directory in case it contains relative components ("..").
+build_dir=$( cd "$build_dir" && pwd )
+
+if [ -z "$build_type" ]; then
+  build_type=${build_dir##*/}
+fi
+
+make_opts=""
+if [ -n "$make_parallelism" ]; then
+  if [[ ! "$make_parallelism" =~ ^[0-9]+$ ]]; then
+    echo "Invalid value for --make-parallelism: '$make_parallelism'" >&2
+    exit 1
+  fi
+  make_opts="-j$make_parallelism"
+fi
+
+if [ -n "$debug_level" ]; then
+  if [[ ! "$debug_level" =~ ^[0-2] ]]; then
+    echo "Invalid value for --debug-level: must be 0, 1, or 2" >&2
+    exit 1
+  fi
+  make_opts+=" DEBUG_LEVEL=$debug_level"
+fi
+
+build_type_lowercase=$( echo "$build_type" | tr '[:upper:]' '[:lower:]' )
+if [ "${build_dir##*/}" != "$build_type_lowercase" ]; then
+  echo "Build directory '$build_dir' does not end with build type ('$build_type') as its last" \
+    "path component: '$build_dir'" >&2
+  exit 1
+fi
+
+rocksdb_dir=$( cd "`dirname $0`" && pwd )
+
+if [ ! -f "$rocksdb_dir/Makefile" ]; then
+  echo "Makefile not found in RocksDB source directory '$rocksdb_dir'" >&2
+  exit 1
+fi
+
+link_dir="$build_dir/rocksdb-symlinks-only"
+build_dir="$build_dir/rocksdb-build"
+
+set -x
+
+rm -rf "$link_dir"
+
+cd "$rocksdb_dir"
+make clean  # ensure there are no build artifacts in the RocksDB source directory itself
+
+# Create a "link-only" directory inside of our build directory that mirrors the RocksDB source tree.
+cp -Rs "$rocksdb_dir" "$link_dir"
+
+# Sync any new links that may have been created into our RocksDB build directory that may already
+# contain build artifacts from an earlier build.
+rsync -al "$link_dir/" "$build_dir"
+
+cd "$build_dir"
+
+set -x
+make $make_opts
--- yb_build.sh
+++ yb_build.sh
@@ -2,8 +2,40 @@
 
 set -euo pipefail
 
+show_help() {
+  cat >&2 <<-EOT
+Usage: ${0##*/} [<options>] [<build_type>]
+Options:
+  -h, --help
+    Show help
+Build types:
+  debug, fastdebug, release, profile_gen, profile_build
+EOT
+}
+
+cmake_opts=""
+cmake_build_type="debug"
+
+while [ $# -gt 0 ]; do
+  case "$1" in
+    -h|--help)
+      show_help >&2
+      exit 1
+    ;;
+    debug|fastdebug|release|profile_gen|profile_build)
+      cmake_build_type="$1"
+    ;;
+    *)
+      echo "Invalid option: '$1'" >&2
+      exit 1
+  esac
+  shift
+done
+
+cmake_opts+=" -DCMAKE_BUILD_TYPE=$cmake_build_type"
+
 project_dir=$( cd `dirname $0` && pwd )
-build_dir="$project_dir/build/debug"
+build_dir="$project_dir/build/$cmake_build_type"
 
 mkdir -p "$build_dir"
 cd "$build_dir"
@@ -24,7 +56,7 @@ if [ ! -f Makefile ] || [ ! -f "$thirdparty_built_flag_file" ]; then
     export NO_REBUILD_THIRDPARTY=1
   fi
   echo "Running cmake in $PWD"
-  ( set -x; cmake -DYB_LINK=dynamic "$project_dir" )
+  ( set -x; cmake -DYB_LINK=dynamic $cmake_opts "$project_dir" )
 fi
 
 echo Running make in $PWD
