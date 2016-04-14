f76a8a34d8865d6bbda6faf7d47ae17819020c1c
Bogdan Matican <bmatican@users.noreply.github.com>
2016-04-14 14:34:52 -0700

    Adding export into local_load_test.sh to work on mac bash 3.x

--- bin/local_load_test.sh
+++ bin/local_load_test.sh
@@ -2,5 +2,6 @@
 
 set -euxo pipefail
 cd `dirname $0`/..
+export DYLD_FALLBACK_LIBRARY_PATH=$HOME/code/yugabyte/build/latest/rocksdb-build
 build/latest/bin/yb_load_test_tool \
   --load_test_master_addresses localhost:7101,localhost:7102,localhost:7103 "$@"
