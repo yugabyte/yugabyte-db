b8bff60196f9c0e1c072f5b76f17976cf4f12ddf
Ramkumar Vaidyanathan <ramkumarvs@users.noreply.github.com>
2016-05-31 11:15:12 -0700

    Initial Commit

---
+++ managed/app/controllers/HomeController.java
@@ -0,0 +1,16 @@
+package controllers;
+
+import play.mvc.*;
+
+import views.html.*;
+
+public class HomeController extends Controller {
+    /**
+     * Renders the Index Page
+     *
+     * @return
+     */
+    public Result index() {
+        return ok(index.render("Yugabyte Middleware APIs."));
+    }
+}
---
+++ managed/build.sbt
@@ -0,0 +1,13 @@
+name := """yugaware"""
+
+version := "1.0-SNAPSHOT"
+
+lazy val root = (project in file(".")).enablePlugins(PlayJava)
+
+scalaVersion := "2.11.7"
+
+libraryDependencies ++= Seq(
+  javaJdbc,
+  cache,
+  javaWs
+)
