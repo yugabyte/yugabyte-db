43d4c88017355cbf330430fcd4864af3ed569d45
Ramkumar Vaidyanathan <ramkumarvs@users.noreply.github.com>
2016-06-09 01:22:15 -0700

    Basic UI
    Summary:
    Basic UI
       - Login Page
       - Logout
       - Initial Dashboard
       - api endpoints have been changed to /api/XXX
    
    Test Plan:
    $ sbt run
    
    hit http://localhost:9000/register
    will let you register a customer
    
    hit http://localhost:9000
    you should see the login page
    
    Reviewers: karthik
    
    Reviewed By: karthik
    
    Subscribers: jenkins-bot, eng
    
    Differential Revision: https://phabricator.dev.yugabyte.com/D136

--- managed/test/controllers/SessionControllerTest.java
+++ managed/test/controllers/SessionControllerTest.java
@@ -21,94 +21,95 @@ import static play.test.Helpers.fakeRequest;
 
 public class SessionControllerTest extends WithApplication {
 
-    @Override
-    protected Application provideApplication() {
-        return new GuiceApplicationBuilder()
-                .configure((Map) Helpers.inMemoryDatabase())
-                .build();
-    }
+  @Override
+  protected Application provideApplication() {
+      return new GuiceApplicationBuilder()
+	        .configure((Map) Helpers.inMemoryDatabase())
+	        .build();
+  }
 
-    @Before
-    public void setUp() {
-        Customer customer = Customer.create("Valid Customer", "foo@bar.com", "password");
-        customer.save();
-    }
+  @Before
+  public void setUp() {
+    Customer customer = Customer.create("Valid Customer", "foo@bar.com", "password");
+    customer.save();
+  }
 
-    @Test
-    public void testValidLogin() {
-        ObjectNode loginJson = Json.newObject();
-        loginJson.put("email", "Foo@bar.com");
-        loginJson.put("password", "password");
-        Result result = route(fakeRequest(controllers.routes.SessionController.login()).bodyJson(loginJson));
-        JsonNode json = Json.parse(contentAsString(result));
+  @Test
+  public void testValidLogin() {
+    ObjectNode loginJson = Json.newObject();
+    loginJson.put("email", "Foo@bar.com");
+    loginJson.put("password", "password");
+    Result result = route(fakeRequest(controllers.routes.SessionController.login()).bodyJson(loginJson));
+    JsonNode json = Json.parse(contentAsString(result));
 
-        assertEquals(OK, result.status());
-        assertNotNull(json.get("authToken"));
-    }
+    assertEquals(OK, result.status());
+    assertNotNull(json.get("authToken"));
+  }
 
-    @Test
-    public void testLoginWithInvalidPassword() {
-        ObjectNode loginJson = Json.newObject();
-        loginJson.put("email", "foo@bar.com");
-        loginJson.put("password", "password1");
-        Result result = route(fakeRequest(controllers.routes.SessionController.login()).bodyJson(loginJson));
+  @Test
+  public void testLoginWithInvalidPassword() {
+    ObjectNode loginJson = Json.newObject();
+    loginJson.put("email", "foo@bar.com");
+    loginJson.put("password", "password1");
+    Result result = route(fakeRequest(controllers.routes.SessionController.login()).bodyJson(loginJson));
 
-        assertEquals(UNAUTHORIZED, result.status());
-    }
+    assertEquals(UNAUTHORIZED, result.status());
+  }
 
-    @Test
-    public void testLoginWithNullPassword() {
-        ObjectNode loginJson = Json.newObject();
-        loginJson.put("email", "foo@bar.com");
-        Result result = route(fakeRequest(controllers.routes.SessionController.login()).bodyJson(loginJson));
+  @Test
+  public void testLoginWithNullPassword() {
+    ObjectNode loginJson = Json.newObject();
+    loginJson.put("email", "foo@bar.com");
+    Result result = route(fakeRequest(controllers.routes.SessionController.login()).bodyJson(loginJson));
 
-        assertEquals(BAD_REQUEST, result.status());
-    }
+    assertEquals(BAD_REQUEST, result.status());
+  }
 
-    @Test
-    public void testRegisterCustomer() {
-        ObjectNode registerJson = Json.newObject();
-        registerJson.put("email", "foo2@bar.com");
-        registerJson.put("password", "password");
-        registerJson.put("name", "Foo");
+  @Test
+  public void testRegisterCustomer() {
+    ObjectNode registerJson = Json.newObject();
+    registerJson.put("email", "foo2@bar.com");
+    registerJson.put("password", "password");
+    registerJson.put("name", "Foo");
 
-        Result result = route(fakeRequest(controllers.routes.SessionController.register()).bodyJson(registerJson));
-        JsonNode json = Json.parse(contentAsString(result));
+    Result result = route(fakeRequest(controllers.routes.SessionController.register()).bodyJson(registerJson));
+    JsonNode json = Json.parse(contentAsString(result));
 
-        assertEquals(OK, result.status());
-        assertNotNull(json.get("authToken"));
+    assertEquals(OK, result.status());
+    assertNotNull(json.get("authToken"));
 
-        ObjectNode loginJson = Json.newObject();
-        loginJson.put("email", "foo2@bar.com");
-        loginJson.put("password", "password");
-        result = route(fakeRequest(controllers.routes.SessionController.login()).bodyJson(loginJson));
-        json = Json.parse(contentAsString(result));
+    ObjectNode loginJson = Json.newObject();
+    loginJson.put("email", "foo2@bar.com");
+    loginJson.put("password", "password");
+    result = route(fakeRequest(controllers.routes.SessionController.login()).bodyJson(loginJson));
+    json = Json.parse(contentAsString(result));
 
-        assertEquals(OK, result.status());
-        assertNotNull(json.get("authToken"));
-    }
+    assertEquals(OK, result.status());
+    assertNotNull(json.get("authToken"));
+  }
 
-    @Test
-    public void testRegisterCustomerWithoutEmail() {
-        ObjectNode registerJson = Json.newObject();
-        registerJson.put("email", "foo@bar.com");
-        Result result = route(fakeRequest(controllers.routes.SessionController.login()).bodyJson(registerJson));
-        JsonNode json = Json.parse(contentAsString(result));
+  @Test
+  public void testRegisterCustomerWithoutEmail() {
+    ObjectNode registerJson = Json.newObject();
+    registerJson.put("email", "foo@bar.com");
+    Result result = route(fakeRequest(controllers.routes.SessionController.login()).bodyJson(registerJson));
 
-        assertEquals(BAD_REQUEST, result.status());
-    }
+    JsonNode json = Json.parse(contentAsString(result));
 
-    @Test
-    public void testLogout() {
-        ObjectNode loginJson = Json.newObject();
-        loginJson.put("email", "Foo@bar.com");
-        loginJson.put("password", "password");
-        Result result = route(fakeRequest(controllers.routes.SessionController.login()).bodyJson(loginJson));
-        JsonNode json = Json.parse(contentAsString(result));
+    assertEquals(BAD_REQUEST, result.status());
+  }
 
-        assertEquals(OK, result.status());
-        String authToken = json.get("authToken").asText();
-        result = route(fakeRequest(controllers.routes.SessionController.logout()).header("X-AUTH-TOKEN", authToken));
-        assertTrue(result.redirectLocation().isPresent());
-    }
+  @Test
+  public void testLogout() {
+    ObjectNode loginJson = Json.newObject();
+    loginJson.put("email", "Foo@bar.com");
+    loginJson.put("password", "password");
+    Result result = route(fakeRequest(controllers.routes.SessionController.login()).bodyJson(loginJson));
+    JsonNode json = Json.parse(contentAsString(result));
+
+    assertEquals(OK, result.status());
+    String authToken = json.get("authToken").asText();
+    result = route(fakeRequest(controllers.routes.SessionController.logout()).header("X-AUTH-TOKEN", authToken));
+	  assertEquals(OK, result.status());
+  }
 }
