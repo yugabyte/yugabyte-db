d62016bce39e6015c076aff2cc0c227edaefb559
Ramkumar Vaidyanathan <ramkumarvs@users.noreply.github.com>
2016-06-29 10:12:40 -0700

    Instance details with Grafana Integration
    Summary:
    Added Show Instance Page
    Added GrafanaPanel to Instance Details Page
    
    Test Plan: went to http://localhost:9000 and validated
    
    Reviewers: bharat, karthik, bogdan
    
    Reviewed By: bogdan
    
    Subscribers: bogdan, jenkins-bot, eng
    
    Differential Revision: https://phabricator.dev.yugabyte.com/D184

--- managed/app/helpers/ApiHelper.java
+++ managed/app/helpers/ApiHelper.java
@@ -5,9 +5,12 @@ package helpers;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.google.inject.Inject;
 import com.google.inject.Singleton;
+import play.libs.Json;
 import play.libs.ws.WSClient;
+import play.libs.ws.WSRequest;
 import play.libs.ws.WSResponse;
 
+import java.util.Map;
 import java.util.concurrent.CompletionStage;
 import java.util.concurrent.ExecutionException;
 
@@ -18,21 +21,44 @@ import java.util.concurrent.ExecutionException;
 @Singleton
 public class ApiHelper {
 
-	@Inject
-	WSClient wsClient;
+  @Inject
+  WSClient wsClient;
 
-	public JsonNode postRequest(String url, JsonNode data)  {
-		CompletionStage<JsonNode> jsonPromise = wsClient.url(url)
-			.post(data)
-			.thenApply(WSResponse::asJson);
+  public JsonNode postRequest(String url, JsonNode data)  {
+    CompletionStage<JsonNode> jsonPromise = wsClient.url(url)
+      .post(data)
+      .thenApply(WSResponse::asJson);
 
-		try {
-			return jsonPromise.toCompletableFuture().get();
-		} catch (InterruptedException e) {
-			return ApiResponse.errorJSON(e.getMessage());
-		} catch (ExecutionException e) {
-			return ApiResponse.errorJSON(e.getMessage());
-		}
-	}
+    return handleJSONPromise(jsonPromise);
+  }
 
+  public JsonNode getRequest(String url, Map<String, String> headers) {
+    WSRequest request = requestWithHeaders(url, headers);
+
+    CompletionStage<JsonNode> jsonPromise = request
+      .get()
+      .thenApply(WSResponse::asJson);
+    return handleJSONPromise(jsonPromise);
+  }
+
+  private JsonNode handleJSONPromise(CompletionStage<JsonNode> jsonPromise) {
+    try {
+      return jsonPromise.toCompletableFuture().get();
+    } catch (InterruptedException e) {
+      return ApiResponse.errorJSON(e.getMessage());
+    } catch (ExecutionException e) {
+      return ApiResponse.errorJSON(e.getMessage());
+    }
+  }
+
+  private WSRequest requestWithHeaders(String url, Map<String, String> headers) {
+    WSRequest request = wsClient.url(url);
+
+    if (!headers.isEmpty()) {
+      for(Map.Entry<String, String> entry : headers.entrySet()) {
+        request.setHeader(entry.getKey(), entry.getValue());
+      }
+    }
+    return request;
+  }
 }
