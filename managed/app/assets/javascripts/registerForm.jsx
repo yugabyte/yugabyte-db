bcf20c9f4ac981bed6f90f7ec43a28c3cb121d91
Pritam Roy <jordanblink@users.noreply.github.com>
2016-07-14 11:12:43 -0700

    Fixed indentation

--- managed/app/assets/javascripts/registerForm.jsx
+++ managed/app/assets/javascripts/registerForm.jsx
@@ -1,56 +1,51 @@
 // Copyright (c) YugaByte, Inc.
 
 var RegisterForm = React.createClass({
-    handleSubmit: function(e) {
-        e.preventDefault();
-        $.ajax( {
-            url: "/api/register",
-            type: 'POST',
-            data: $('#registerForm').serialize(),
-            success: function(response) {
-                sessionStorage.setItem("authToken", response.authToken);
-                window.location.href = "/";
-            },
-            error: function(response) {
-                if(response.status == 401) {
-                    var errorJson = $.parseJSON(response.responseText);
-
-                } else {
-                    $("#registerFormAlert").hide();
-                    parseFormErrorResponse(response);
-                }
-            }
-        });
-
-    },
-
-    render: function() {
-        return (
-            <div className="row">
-                <div className="col-sm-4 col-sm-offset-4">
-                    <div className="panel panel-default login-panel">
-                        <div className="panel-heading">
-                            <h3 className="panel-title">Register Customer</h3>
-                        </div>
-                        <div className="panel-body">
-                            <form id="registerForm" onSubmit={this.handleSubmit}>
-                                <fieldset>
-                                    <YBInputBox name="name" type="text"
-                                                placeholder="Name" label="Name" />
-                                    <YBInputBox name="email" type="text"
-                                                placeholder="E-mail" label="E-mail" />
-                                    <YBInputBox name="password" type="password"
-                                                placeholder="Password" label="Password" />
-                                    <YBSubmitButton name="Register" />
-                                </fieldset>
-                            </form>
-                            <YBErrorBox name="registerFormAlert" />
-                        </div>
-                    </div>
-                </div>
+  handleSubmit: function(e) {
+    e.preventDefault();
+    $.ajax( {
+      url: "/api/register",
+      type: 'POST',
+      data: $('#registerForm').serialize(),
+      success: function(response) {
+        sessionStorage.setItem("authToken", response.authToken);
+        window.location.href = "/";
+      },
+      error: function(response) {
+        if(response.status == 401) {
+          var errorJson = $.parseJSON(response.responseText);
+        }
+        else {
+          $("#registerFormAlert").hide();
+          parseFormErrorResponse(response);
+        }
+      }
+    });
+  },
+  render: function() {
+    return (
+      <div className="row">
+        <div className="col-sm-4 col-sm-offset-4">
+          <div className="panel panel-default login-panel">
+            <div className="panel-heading">
+              <h3 className="panel-title">Register Customer</h3>
             </div>
-        );
-    }
+            <div className="panel-body">
+              <form id="registerForm" onSubmit={this.handleSubmit}>
+                <fieldset>
+                  <YBInputBox name="name" type="text" placeholder="Name" label="Name" />
+                  <YBInputBox name="email" type="text" placeholder="E-mail" label="E-mail" />
+                  <YBInputBox name="password" type="password" placeholder="Password" label="Password" />
+                  <YBSubmitButton name="Register" />
+                </fieldset>
+              </form>
+              <YBErrorBox name="registerFormAlert" />
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
 });
 
 ReactDOM.render(<RegisterForm id="registerForm"/>, document.getElementById('content'));
