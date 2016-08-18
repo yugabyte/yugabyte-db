9bf3c83200dae691c9435853c55c79a25000125d
Ramkumar Vaidyanathan <ramkumarvs@users.noreply.github.com>
2016-08-18 15:28:24 -0700

    UI Tweaks
    Test Plan:
    start middleware using sbt run
    run npm start inside of UI folder
    
    hit http://localhost:3000/
    
    Reviewers: pritam
    
    Reviewed By: pritam
    
    Subscribers: eng
    
    Differential Revision: https://phabricator.dev.yugabyte.com/D386

--- managed/ui/src/components/Dashboard.js
+++ managed/ui/src/components/Dashboard.js
@@ -2,41 +2,33 @@
 
 import React, { Component, PropTypes } from 'react';
 import DashboardRightPane from './DashboardRightPane';
-export default class Dashboard extends Component {
+import TopNavBar from './TopNavBar';
+import SideNavBar from './SideNavBar';
 
-    static contextTypes = {
-        router: PropTypes.object
-    };
-    
-    componentWillMount() {
-        if(typeof this.props.customer === "undefined" || this.props.customer.status !== "authenticated"){
-            this.context.router.push('/login');
-        }
-    }
-    
-    render() {
-        return (
-        <div className="container-fluid">
-            <nav className="navbar navbar-default ybNavBar">
-                <div className="container-fluid">
-                    <div className="navbar-header">
-                        <a className="navbar-brand" href="#">
-                            YugaByte Customer Portal
-                        </a>
-                    </div>
-                </div>
-            </nav>
-            <div className="ybLeftPane">
-                <ul className="ybLeftPaneItem">
-                    <span className="ybLeftNavTop">
-                      <li>Dashboard</li>
-                    </span>
-                </ul>
+export default class Dashboard extends Component {
+  static contextTypes = {
+    router: PropTypes.object
+  };
+
+  componentWillMount() {
+    if(typeof this.props.customer === "undefined" || this.props.customer.status !== "authenticated"){
+			this.context.router.push('/login');
+	  }
+	}
+
+  render() {
+    return (
+      <div className="dashboard">
+        <TopNavBar />
+        <SideNavBar />
+        <div className="page-wrapper">
+          <div className="row header-row">
+            <div className="col-lg-10">
+              <h1>Dashboard</h1>
             </div>
-            <div className="ybRightPane">
-                <DashboardRightPane />
-            </div>
+            <DashboardRightPane />
+          </div>
         </div>
-    );
+      </div>);
   }
 }
