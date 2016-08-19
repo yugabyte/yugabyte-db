43abe9802c9782e725712ee0cfcbd64886e198bb
Ramkumar Vaidyanathan <ramkumarvs@users.noreply.github.com>
2016-08-19 00:16:50 -0700

    UI fixes
    Summary:
    - Fix bunch of issues caused D385
    - Code formatting/spacing issues
    - Form Error message handling
    
    Test Plan:
    $ sbt run
    inside of ui folder
    $ npm start
    and hit http://localhost:3000/
    and validate login/register flow works
    
    Reviewers: pritam, karthik
    
    Reviewed By: karthik
    
    Subscribers: eng
    
    Differential Revision: https://phabricator.dev.yugabyte.com/D387

---
+++ managed/ui/src/components/NavBar.js
@@ -0,0 +1,25 @@
+// Copyright (c) YugaByte, Inc.
+
+import React, { Component } from 'react';
+
+import TopNavBar from './TopNavBar';
+import SideNavBar from './SideNavBar';
+
+export default class NavBar extends Component {
+	render() {
+		return (
+			<nav className="navbar navbar-default navbar-static-top" role="navigation" >
+				<div className="navbar-header">
+					<button type="button" className="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+						<span className="sr-only">Toggle navigation</span>
+						<span className="icon-bar"></span>
+						<span className="icon-bar"></span>
+						<span className="icon-bar"></span>
+					</button>
+					<a className="navbar-brand" href="/">YugaByte Customer Portal</a>
+				</div>
+				<TopNavBar />
+				<SideNavBar />
+			</nav>);
+	}
+}
--- managed/ui/src/components/SideNavBar.js
+++ managed/ui/src/components/SideNavBar.js
@@ -6,13 +6,19 @@ import '../stylesheets/SideNavBar.css'
 export default class SideNavBar extends Component {
 	render() {
     return (
-  		<div className="navbar-default sidebar">
-	  	  <div className="sidebar-nav navbar-collapse">
-					<ul className="nav in">
-					  <li><a href="/" class="active"><i class="fa fa-dashboard fa-fw"></i> Dashboard</a></li>
-					</ul>
-		  	</div>
-			</div>
-	  );
+      <div className="navbar-default sidebar" role="navigation">
+        <div className="sidebar-nav navbar-collapse">
+          <ul className="nav in" id="side-menu">
+            <li>
+              <a href="/" className="active"><i className="fa fa-dashboard fa-fw"></i> Dashboard</a>
+            </li>
+            <li className="">
+              <a href="#"><i className="fa fa-database fa-fw"></i> Universes<span className="fa arrow"></span></a>
+              <ul className="nav nav-second-level collapse instance-list" aria-expanded="false"></ul>
+            </li>
+          </ul>
+        </div>
+      </div>
+  	);
   }
 }
