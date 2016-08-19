3ac020e5724698491e1f5f1a8723cce6dfa25240
Ramkumar Vaidyanathan <ramkumarvs@users.noreply.github.com>
2016-08-19 00:52:07 -0700

    Fix create universe modal
    Summary: Switch create universe modal to use react-bootstrap
    
    Test Plan:
    run
    $ sbt run
    inside of ui folder, run
    $ npm start
    hit http://localhost:3000
    
    and login and try launching the create universe modal
    
    Reviewers: pritam, karthik
    
    Reviewed By: karthik
    
    Subscribers: eng
    
    Differential Revision: https://phabricator.dev.yugabyte.com/D388

--- managed/ui/src/components/CreateUniverse.js
+++ managed/ui/src/components/CreateUniverse.js
@@ -1,85 +1,66 @@
 // Copyright (c) YugaByte, Inc.
 
 import React, { Component } from 'react';
-import Modal from 'react-modal';
+import { Modal } from 'react-bootstrap';
 
 export default class Dashboard extends Component {
   constructor(props) {
     super(props);
-    this.showCreateInstanceModal = this.showCreateInstanceModal.bind(this);
-    this.closeModal=this.closeModal.bind(this);
+    this.showModal = this.showModal.bind(this);
+    this.closeModal = this.closeModal.bind(this);
     this.state = {
-      isModalOpen: false
+      showModal: false
     };
   }
 
-  showCreateInstanceModal(){
-    this.setState({isModalOpen:true});
+  showModal(){
+    this.setState({showModal:true});
   }
   closeModal(){
-    this.setState({isModalOpen: false});
+    this.setState({showModal: false});
   }
   render() {
     return (
       <div>
         <div>
-          <div className="createUniverseButton btn btn-default btn-success" onClick={this.showCreateInstanceModal}>
+          <div className="createUniverseButton btn btn-default btn-success" onClick={this.showModal}>
             Create Universe
           </div>
         </div>
-        <Modal isOpen={this.state.isModalOpen} onRequestClose={this.closeModal}>
-          <div className="modal-heading">
-            <button type="button" className="close" data-dismiss="modal" aria-label="Close" onClick={this.closeModal}>
-              <span aria-hidden="true">&times;</span>
-            </button>
-            <div className="text-center h4">Create A New Universe</div>
-          </div>
-          <div className="modal-body">
-            <div className="modal-body-left">
-              <form>
-                <label className="form-item-label">
-                  Universe Name
-                  <input className="form-control" placeholder="Universe Name"/>
-                </label>
-                <label className="form-item-label">
-                  Cloud Provider
-                  <select className="form-control"></select>
-                </label>
-                <label className="form-item-label">
-                  Region
-                  <select className="form-control"></select>
-                </label>
-                <div className="createUniverseFormSplit">
-                  Advanced
-                </div>
-                <label className="form-item-label">
-                  Multi AZ Capable &nbsp;
-                  <input type="checkbox"/>
-                </label>
-                <label className="form-item-label">
-                  Instance Type
-                  <select className="form-control"></select>
-                </label>
-              </form>
-            </div>
-            <div className="modal-body-right">
-              <div className="body-right-content">
-                <div className="content-head h4">
-                  Universe Overview
-                </div>
-                <div className="content-item">
-                  Universe Name :
-                </div>
-                <div className="content-item">
-                  Universe Map:
-                </div>
-                <div className="content-item">
-                  Cost Estimate:
-                </div>
+        <Modal show={this.state.showModal} onHide={this.closeModal}>
+          <Modal.Header closeButton>
+            <Modal.Title>Create A New Universe</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <form>
+              <label className="form-item-label">
+                Universe Name
+                <input className="form-control" placeholder="Universe Name"/>
+              </label>
+              <label className="form-item-label">
+                Cloud Provider
+                <select className="form-control"></select>
+              </label>
+              <label className="form-item-label">
+                Region
+                <select className="form-control"></select>
+              </label>
+              <div className="createUniverseFormSplit">
+                Advanced
               </div>
-              <div className="createButton btn btn-default btn-success">Create Universe</div>
-            </div>
-          </div>
+              <label className="form-item-label">
+                Multi AZ Capable &nbsp;
+                <input type="checkbox"/>
+              </label>
+              <label className="form-item-label">
+                Instance Type
+                <select className="form-control"></select>
+              </label>
+            </form>
+          </Modal.Body>
+          <Modal.Footer>
+            <div className="btn btn-default btn-success">Create Universe</div>
+          </Modal.Footer>
         </Modal>
       </div>
     )
