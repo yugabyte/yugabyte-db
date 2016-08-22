e3d4ae4de0d30913fd10eeca7089693eae2c6135
Ramkumar Vaidyanathan <ramkumarvs@users.noreply.github.com>
2016-08-22 11:23:43 -0700

    Cleanup routes, and handle authenticated/unauthenticated flows
    Summary:
    - Clean up the App container
    - Handle authenicated/un authenticated flows inside of routes
    
    Test Plan:
    start api server
    $ sbt run
    
    start ui server
    $ cd ui
    $ npm start
    
    hit http://localhost:3000/
    
    and make sure
    /login
    /register
    renders without authentication
    
    /home
    renders after authentication
    
    Reviewers: karthik, pritam
    
    Reviewed By: pritam
    
    Subscribers: eng
    
    Differential Revision: https://phabricator.dev.yugabyte.com/D393

--- managed/ui/src/index.js
+++ managed/ui/src/index.js
@@ -4,13 +4,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { Router, browserHistory } from 'react-router';
-import routes from './routes';
+import fetchRoutes from './routes';
 import configureStore from './store/configureStore.js';
 const store = configureStore();
 import 'bootstrap/dist/css/bootstrap.css';
 
 ReactDOM.render(
   <Provider store={store}>
-    <Router history={browserHistory} routes={routes} />
+    <Router history={browserHistory}>
+      {fetchRoutes(store)}
+    </Router>
   </Provider>
   , document.getElementById('root'));
