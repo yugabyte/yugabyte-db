aa9597dc20b76af84588f83abc92f21de73fd803
Pritam Roy <jordanblink@users.noreply.github.com>
2016-08-31 21:18:24 -0700

    ENG-333 Side Navbar matched to Wireframes
    Summary:
    Created Side NavBar matching to Dashboard wireframes
    
    {F636}
    
    Test Plan:
    open browser type localhost:3000
    The navbar as shown in screenshot should be visible in browser
    
    Reviewers: karthik
    
    Reviewed By: karthik
    
    Subscribers: eng
    
    Differential Revision: https://phabricator.dev.yugabyte.com/D435

--- managed/ui/src/index.js
+++ managed/ui/src/index.js
@@ -8,7 +8,7 @@ import fetchRoutes from './routes';
 import configureStore from './store/configureStore.js';
 const store = configureStore();
 import 'bootstrap/dist/css/bootstrap.css';
-
+import '../src/stylesheets/custom.css';
 ReactDOM.render(
   <Provider store={store}>
     <Router history={browserHistory}>
