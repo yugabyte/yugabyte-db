2265f811f73fb8a083ce94c90a273d2fb339cf44
Pritam Roy <jordanblink@users.noreply.github.com>
2016-08-31 21:18:24 -0700

    ENG-333 Side Navbar matched to Wireframes
    Summary:
    Created Side NavBar matching to Dashboard wireframes
    
    {F636}
    
    Test Plan:
    open browser type localhost:3000
    The navbar as shown in screenshot should be visible in browser
    
    Reviewers: karthik
    
    Reviewed By: karthik
    
    Subscribers: eng
    
    Differential Revision: https://phabricator.dev.yugabyte.com/D435

--- managed/ui/src/components/NavBar.js
+++ managed/ui/src/components/NavBar.js
@@ -7,19 +7,14 @@ import SideNavBar from './SideNavBar';
 export default class NavBar extends Component {
 	render() {
 		return (
-			<nav className="navbar navbar-default navbar-static-top" role="navigation" >
-				<div className="navbar-header">
-					<button type="button" className="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
-						<span className="sr-only">Toggle navigation</span>
-						<span className="icon-bar"></span>
-						<span className="icon-bar"></span>
-						<span className="icon-bar"></span>
-					</button>
-					<a className="navbar-brand" href="/">YugaByte Customer Portal</a>
-				</div>
-				<TopNavBarContainer />
-				<SideNavBar />
-			</nav>
+			<div className="nav-sm">
+			  <div className="container body">
+				  <div className="main_container">
+				    <TopNavBarContainer />
+				    <SideNavBar />
+			    </div>
+		    </div>
+			</div>
 		);
 	}
 }
--- managed/ui/src/components/SideNavBar.js
+++ managed/ui/src/components/SideNavBar.js
@@ -4,19 +4,63 @@ import React, { Component } from 'react';
 import '../stylesheets/SideNavBar.css'
 
 export default class SideNavBar extends Component {
+  constructor (props) {
+    super(props);
+    this.setFilter = this.setFilter.bind(this);
+    this.isActive = this.isActive.bind(this);
+  }
+
+  componentWillMount () {
+    this.setState({selected: ''});
+  }
+
+  setFilter (filter) {
+    this.setState({selected : filter});
+  }
+
+  isActive(value) {
+    if (value === this.state.selected) {
+      return 'active';
+    } else {
+      return 'default';
+    }
+  }
+
 	render() {
     return (
-      <div className="navbar-default sidebar" role="navigation">
-        <div className="sidebar-nav navbar-collapse">
-          <ul className="nav in" id="side-menu">
-            <li>
-              <a href="/" className="active"><i className="fa fa-dashboard fa-fw"></i> Dashboard</a>
-            </li>
-            <li className="">
-              <a href="#"><i className="fa fa-database fa-fw"></i> Universes<span className="fa arrow"></span></a>
-              <ul className="nav nav-second-level collapse instance-list" aria-expanded="false"></ul>
-            </li>
-          </ul>
+      <div className="col-md-3 left_col">
+        <div className="left_col scroll-view">
+          <div className="navbar nav_title">
+            <a href="index.html" className="site_title"><i className="fa fa-database"></i> <span>YugaByte</span></a>
+          </div>
+          <div className="clearfix"></div>
+          <br />
+          <div id="sidebar-menu" className="main_menu_side hidden-print main_menu">
+            <div className="menu_section">
+              <ul className="nav side-menu">
+                <li className={this.isActive('')} onClick={this.setFilter.bind(this, '')}>
+                  <a href="#" ><i className="fa fa-home"></i> Home
+                    <span className="label label-success"></span>
+                  </a>
+                </li>
+                <li className={this.isActive('universe')} onClick={this.setFilter.bind(this, 'universe')}>
+                  <a href="#"><i className="fa fa-globe"></i> Universes <span className="label label-success"></span></a>
+                </li>
+                <li className={this.isActive('alerts')} onClick={this.setFilter.bind(this, 'alerts')}>
+                  <a href="#"><i className="fa fa-bell-o"></i> Alerts <span className="label label-success"></span></a>
+                </li>
+                <li className={this.isActive('metrics')} onClick={this.setFilter.bind(this, 'metrics')}><a href="#">
+                  <i className="fa fa-line-chart"></i> Metrics <span className="label label-success"></span></a>
+                </li>
+                <li className={this.isActive('docs')} onClick={this.setFilter.bind(this, 'docs')}>
+                  <a href="#"><i className="fa fa-file-text-o"></i> Docs <span className="label label-success"></span></a>
+                </li>
+                <li className={this.isActive('support')} onClick={this.setFilter.bind(this, 'support')}>
+                  <a href="#"><i className="fa fa-phone"></i> Support <span className="label label-success"></span></a>
+                </li>
+              </ul>
+            </div>
+          </div>
         </div>
       </div>
   	);
