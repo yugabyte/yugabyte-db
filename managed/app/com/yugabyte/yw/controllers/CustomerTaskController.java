4a69094c8ca103e8228d95e575ed283ea1fe9756
Karthik Ranganathan <rkarthik007@users.noreply.github.com>
2016-09-01 22:05:06 -0700

    ENG-236: Undo changes to list tasks API
    Summary: The list tasks API was calling a scala render method and is no longer used - so I had removed it. Its causing a unit test failure, so reverting back to the way it was before.
    
    Test Plan: Tested by running `sbt test` and making sure it succeeds.
    
    Reviewers: bharat
    
    Reviewed By: bharat
    
    Subscribers: eng
    
    Differential Revision: https://phabricator.dev.yugabyte.com/D443

--- managed/app/com/yugabyte/yw/controllers/CustomerTaskController.java
+++ managed/app/com/yugabyte/yw/controllers/CustomerTaskController.java
@@ -2,21 +2,32 @@
 
 package com.yugabyte.yw.controllers;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
 import java.util.UUID;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.node.ObjectNode;
+import com.google.common.collect.ImmutableMap;
 import com.google.inject.Inject;
 import com.yugabyte.yw.commissioner.Commissioner;
 import com.yugabyte.yw.common.ApiHelper;
 import com.yugabyte.yw.common.ApiResponse;
+import com.yugabyte.yw.forms.CustomerTaskFormData;
 import com.yugabyte.yw.models.Customer;
 import com.yugabyte.yw.models.CustomerTask;
 import com.yugabyte.yw.ui.controllers.AuthenticatedController;
 
+import play.libs.Json;
+import play.mvc.Http;
 import play.mvc.Result;
+
+import com.yugabyte.yw.ui.views.html.*;
+import static com.yugabyte.yw.ui.controllers.TokenAuthenticator.COOKIE_AUTH_TOKEN;
 
 public class CustomerTaskController extends AuthenticatedController {
   @Inject
@@ -29,7 +40,52 @@ public class CustomerTaskController extends AuthenticatedController {
   public static final Logger LOG = LoggerFactory.getLogger(CustomerTaskController.class);
 
   public Result list(UUID customerUUID) {
-    return badRequest();
+    Customer customer = Customer.find.byId(customerUUID);
+
+    if (customer == null) {
+      ObjectNode responseJson = Json.newObject();
+      responseJson.put("error", "Invalid Customer UUID: " + customerUUID);
+      return badRequest(responseJson);
+    }
+
+    Set<CustomerTask> pendingTasks = CustomerTask.find.where()
+      .eq("customer_uuid", customer.uuid)
+      .orderBy("create_time desc")
+      .setMaxRows(TASK_HISTORY_LIMIT)
+      .findSet();
+
+    List<CustomerTaskFormData> taskList = new ArrayList<>();
+    String customerTaskBaseUrl =
+      "http://" + ctx().request().host() + "/api/customers/" + customer.uuid + "/tasks/";
+    for (CustomerTask task : pendingTasks) {
+      CustomerTaskFormData taskData = new CustomerTaskFormData();
+
+      JsonNode taskProgress = apiHelper.getRequest(customerTaskBaseUrl + task.getTaskUUID(),
+        ImmutableMap.of("X-AUTH-TOKEN", ctx().request().cookie(COOKIE_AUTH_TOKEN).value()));
+
+      // If the task progress API returns error, we will log it and not add that task to
+      // to the task list for UI rendering.
+      if (taskProgress.has("error")) {
+        LOG.error("Error fetching Task Progress for " + task.getTaskUUID());
+      } else {
+        taskData.percentComplete = taskProgress.get("percent").asInt();
+        if (taskData.percentComplete == 100) {
+          task.markAsCompleted();
+        }
+        taskData.success = taskProgress.get("status").asText().equals("Success") ? true : false;
+        taskData.id = task.getTaskUUID();
+        taskData.title = task.getFriendlyDescription();
+        taskList.add(taskData);
+      }
+    }
+
+    if (request().accepts(Http.MimeTypes.HTML)) {
+      return ok(customerTask.render(taskList));
+    } else if (request().accepts(Http.MimeTypes.JSON)) {
+      return ok(Json.toJson(taskList));
+    } else {
+      return badRequest();
+    }
   }
 
   public Result status(UUID customerUUID, UUID taskUUID) {
