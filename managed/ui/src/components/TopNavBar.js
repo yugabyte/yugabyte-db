03ce353a46ff41b901db7f9a773473e182208f82
Pritam Roy <jordanblink@users.noreply.github.com>
2016-09-05 23:53:59 -0700

    Top Nav Bar Links
    Summary:
    Fixed Top Nav Bar links
    Profile component is not implemented yet.
    Logout will take the user to login page.
    
    {F683}
    
    Test Plan:
    Click on dropdown in top-left manu
    Click on logout, should be redirected to login page and localStorage should be cleared.
    
    Reviewers: karthik, ram
    
    Reviewed By: ram
    
    Subscribers: eng
    
    Differential Revision: https://phabricator.dev.yugabyte.com/D454

--- managed/ui/src/components/TopNavBar.js
+++ managed/ui/src/components/TopNavBar.js
@@ -2,7 +2,8 @@
 
 import React, { Component } from 'react';
 import '../stylesheets/TopNavBar.css';
-import 'react-fa'
+import 'react-fa';
+import { MenuItem , NavDropdown } from 'react-bootstrap';
 
 export default class TopNavBar extends Component {
 	constructor(props) {
@@ -17,17 +18,18 @@ export default class TopNavBar extends Component {
 	render() {
 		return (
 			<ul className="nav navbar-top-links navbar-right">
-				<li className="dropdown">
-					<a className="dropdown-toggle" data-toggle="dropdown" href="#">
-						<i className="fa fa-user fa-fw"></i>  <i className="fa fa-caret-down"></i>
-					</a>
-					<ul className="dropdown-menu dropdown-user">
-						<li><a href="/profile"><i className="fa fa-user fa-fw"></i> Profile</a></li>
-						<li className="divider"></li>
-						<li><a href="/login" id="logoutLink" onClick={this.handleLogout}><i className="fa fa-sign-out fa-fw"></i> Logout</a></li>
-					</ul>
-				</li>
-			</ul>
+				<NavDropdown eventKey="4" title=
+					{<div className='fa fa-user top-nav-menu-button'></div>} id="nav-dropdown">
+				  <MenuItem eventKey="4.1" href="/profile"><i className="fa fa-user fa-fw"></i>
+						&nbsp;Profile
+					</MenuItem>
+				  <MenuItem divider />
+				  <MenuItem eventKey="4.2" href="/login" id="logoutLink" onClick={this.handleLogout}>
+						<i className="fa fa-sign-out fa-fw"></i>
+						Logout
+					</MenuItem>
+			  </NavDropdown>
+      </ul>
 		);
 	}
 }
