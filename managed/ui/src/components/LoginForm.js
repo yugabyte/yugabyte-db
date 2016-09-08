842a01d875ea20a607f589e21f865edfd3f899e6
Pritam Roy <jordanblink@users.noreply.github.com>
2016-09-08 14:59:03 -0700

    Fixed Login Link
    Summary: When the user logs in they should be redirected to dashboard route.
    
    Test Plan:
    go to localhost:3000/login
    login using valid credentials, you should be redirected to dashboard route
    
    Reviewers: karthik, ram
    
    Reviewed By: ram
    
    Subscribers: eng
    
    Differential Revision: https://phabricator.dev.yugabyte.com/D480

--- managed/ui/src/components/LoginForm.js
+++ managed/ui/src/components/LoginForm.js
@@ -13,7 +13,7 @@ class LoginForm extends Component {
     if (nextProps.customer.status === 'authenticated' &&
         nextProps.customer.customer &&
         !nextProps.customer.error) {
-        this.context.router.push('/home');
+        this.context.router.push('/dashboard');
     }
   }
 
