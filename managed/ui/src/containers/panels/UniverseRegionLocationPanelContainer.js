039278b68a5828f355a29c5853f6a78847d4891f
Pritam Roy <jordanblink@users.noreply.github.com>
2016-09-12 14:17:09 -0700

    Created Panel Layout for Home Page
    Summary:
    Created Components and Panels for Home Page layout
    
    {F723}
    
    Test Plan:
    Go to http://locahost:3000 and http://localhost:3000/dashboard on the browser
    should show the dashboard page.
    
    Reviewers: ram, karthik
    
    Reviewed By: ram, karthik
    
    Subscribers: eng
    
    Differential Revision: https://phabricator.dev.yugabyte.com/D464

---
+++ managed/ui/src/containers/panels/UniverseRegionLocationPanelContainer.js
@@ -0,0 +1,6 @@
+// Copyright (c) YugaByte, Inc.
+
+import { connect } from 'react-redux';
+import UniverseRegionLocationPanel from '../../components/panels/UniverseRegionLocationPanel';
+
+export default connect()(UniverseRegionLocationPanel);
