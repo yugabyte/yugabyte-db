029ebef0eb4e31008a262e46a3dd157d6f7e6a43
Bogdan Matican <bmatican@users.noreply.github.com>
2016-09-27 19:04:41 -0700

    Adding links to master/tserver webviews
    Summary: Wanted to be able to go to the Master/Tserver webview directly from the universe page.
    
    Test Plan: https://phabricator.dev.yugabyte.com/F965
    
    Reviewers: pritam, karthik, kannan, ram
    
    Reviewed By: ram
    
    Subscribers: eng
    
    Differential Revision: https://phabricator.dev.yugabyte.com/D582

--- managed/ui/src/components/NodeDetails.js
+++ managed/ui/src/components/NodeDetails.js
@@ -12,25 +12,53 @@ export default class NodeDetails extends Component {
 
   render() {
     const { nodeDetails } = this.props;
-	  const nodeDetailRows = Object.keys(nodeDetails).map(function(key) {
+    const nodeDetailRows = Object.keys(nodeDetails).map(function(key) {
       var nodeDetail = nodeDetails[key];
+      var privateIP = nodeDetail.cloudInfo.private_ip;
+
       return {
         name: nodeDetail.nodeName,
         regionAz: `${nodeDetail.cloudInfo.region}/${nodeDetail.cloudInfo.az}`,
         isMaster: nodeDetail.isMaster ? "Yes" : "No",
+        masterPort: nodeDetail.masterHttpPort,
+        tserverPort: nodeDetail.tserverHttpPort,
         isTServer: nodeDetail.isTserver ? "Yes" : "No",
-        privateIP: nodeDetail.cloudInfo.private_ip,
+        privateIP: privateIP,
         nodeStatus: nodeDetail.state,
       };
     });
+
+    var formatIpPort = function(cell, row, type) {
+      if (cell === "No") {
+        return <span>{cell}</span>;
+      }
+      var href;
+      if (type === "master") {
+        href = "http://" + row.privateIP + ":" + row.masterPort
+      } else {
+        href = "http://" + row.privateIP + ":" + row.tserverPort
+      }
+      return (<a href={href} target="_blank">{cell}</a>);
+    }
 
     return (
       <YBPanelItem name="Node Details">
         <BootstrapTable data={nodeDetailRows}>
           <TableHeaderColumn dataField="name" isKey={true}>Instance Name</TableHeaderColumn>
           <TableHeaderColumn dataField="regionAz">Region/Zone</TableHeaderColumn>
-          <TableHeaderColumn dataField="isMaster">Master</TableHeaderColumn>
-          <TableHeaderColumn dataField="isTServer">TServer</TableHeaderColumn>
+          <TableHeaderColumn dataField="region">Region</TableHeaderColumn>
+          <TableHeaderColumn
+              dataField="isMaster"
+              dataFormat={ formatIpPort }
+              formatExtraData="master" >
+            Master
+          </TableHeaderColumn>
+          <TableHeaderColumn
+              dataField="isTServer"
+              dataFormat={ formatIpPort }
+              formatExtraData="tserver" >
+            TServer
+          </TableHeaderColumn>
           <TableHeaderColumn dataField="privateIP">Private IP</TableHeaderColumn>
           <TableHeaderColumn dataField="nodeStatus">Node Status</TableHeaderColumn>
         </BootstrapTable>
