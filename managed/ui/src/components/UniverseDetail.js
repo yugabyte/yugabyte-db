17af70c68f4fcf3517ea75b665a65178e6946ff1
Pritam Roy <jordanblink@users.noreply.github.com>
2016-10-10 14:16:19 -0700

    Universe Detail Header Section
    Summary:
    Implemented Doug's recommendation for Universe Detail Header section.
    
    WireFrame
    {F1025}
    
    Actual Implementation
    {F1024}
    {F1026}
    
    Delete Universe Functionality will break as part of this diff.
    Will port Delete Universe as part of a follow up diff.
    
    Test Plan: Go to UniverseDetail page, The header should be as in the screenshot.
    
    Reviewers: karthik, ram
    
    Reviewed By: ram
    
    Subscribers: eng
    
    Differential Revision: https://phabricator.dev.yugabyte.com/D645

--- managed/ui/src/components/UniverseDetail.js
+++ managed/ui/src/components/UniverseDetail.js
@@ -1,17 +1,20 @@
 // Copyright (c) YugaByte, Inc.
 
 import React, { Component, PropTypes } from 'react';
-import { Grid, Row, Col, ButtonGroup } from 'react-bootstrap';
+import { Grid, Row, Col, ButtonGroup, Image,
+          DropdownButton, MenuItem } from 'react-bootstrap';
 import RegionMap from './RegionMap';
 import NodeDetails from './NodeDetails';
 import UniverseInfoPanel from './UniverseInfoPanel';
 import ConnectStringPanel from './ConnectStringPanel';
 import GraphPanelContainer from '../containers/GraphPanelContainer';
-import DeleteUniverseContainer from '../containers/DeleteUniverseContainer';
 import TaskProgressContainer from '../containers/TaskProgressContainer';
 import YBButton from './fields/YBButton';
 import UniverseFormContainer from '../containers/forms/UniverseFormContainer';
 import GFlagsFormContainer from '../containers/forms/GFlagsFormContainer';
+import {Link} from 'react-router';
+import universelogo from '../images/universe_icon.png';
+import YBLabelWithIcon from './fields/YBLabelWithIcon';
 
 export default class UniverseDetail extends Component {
 
@@ -54,30 +57,46 @@ export default class UniverseDetail extends Component {
     return (
       <Grid id="page-wrapper">
         <Row className="header-row">
-          <Col lg={9} >
-            <h3>Universe { currentUniverse.name }</h3>
+          <Col lg={9}>
+            <div className="detail-label-small">
+               <Link to="/universes">
+                 <YBLabelWithIcon icon="fa fa-chevron-left fa-fw">
+                   Universes
+                 </YBLabelWithIcon>
+               </Link>
+            </div>
+            <div>
+              <h2><Image src={universelogo}/> Universe { currentUniverse.name }</h2>
+            </div>
           </Col>
-          <ButtonGroup>
-            <YBButton btnClass="universe-button btn btn-xs btn-info"
+          <Col lg={3}>
+            <div className="detail-label-small">
+              Test And Verify
+            </div>
+            <ButtonGroup className="universe-detail-btn-group">
+              <YBButton btnClass=" btn btn-default btn-sm bg-orange"
+                        btnText="Edit" btnIcon="fa fa-database" onClick={this.props.showUniverseModal} />
+              <DropdownButton bsStyle="btn btn-default btn-sm " title="More" id="bg-nested-dropdown" >
+                <MenuItem eventKey="1">
+                  <YBLabelWithIcon icon="fa fa-refresh fa-fw">
+                    s/w Upgrades
+                  </YBLabelWithIcon>
+                </MenuItem>
+                <MenuItem eventKey="2" onClick={this.props.showGFlagsModal} >
+                  <YBLabelWithIcon icon="fa fa-flag fa-fw">
+                    Flags
+                  </YBLabelWithIcon>
+                </MenuItem>
+              </DropdownButton>
+              <YBButton btnIcon="fa fa-trash-o" btnClass="btn btn-default btn-sm" />
+            </ButtonGroup>
+          </Col>
+          <UniverseFormContainer type="Edit"
+                                 visible={showModal===true && visibleModal==="universeModal"}
+                                 onHide={this.props.closeModal} title="Edit Universe" />
+          <GFlagsFormContainer modalVisible={showModal === true && visibleModal === "gFlagsModal"}
+                               onHide={this.props.closeModal} title="Set GFlags"/>
 
-                           btnText="Edit" btnIcon="fa fa-database" onClick={this.props.showUniverseModal} />
-
-
-            <UniverseFormContainer type="Edit"
-                                   visible={showModal===true && visibleModal==="universeModal"}
-                                   onHide={this.props.closeModal} title="Edit Universe" />
-
-
-            <YBButton btnClass="universe-button btn btn-xs btn-default"
-                      btnText="Flags" btnIcon="fa fa-flag fa-fw"
-                      onClick={this.props.showGFlagsModal}/>
-
-            <GFlagsFormContainer modalVisible={showModal === true && visibleModal === "gFlagsModal"}
-                                 onHide={this.props.closeModal} title="Set GFlags"/>
-
-            <DeleteUniverseContainer uuid={currentUniverse.universeUUID} />
-
-          </ButtonGroup>
         </Row>
         <Row>
           <Col lg={11}>
