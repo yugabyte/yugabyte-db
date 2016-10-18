2c5b296b52ae8d1b06a91a72817b7b92f42ab40a
Pritam Roy <jordanblink@users.noreply.github.com>
2016-10-18 16:10:18 -0700

    Universe Detail Page layout fixes
    Summary:
    No horizontal scroll bar on the page.
    Fixing layout to remove bugs and match designer's layout recommendation.
    
    {F1135}
    {F1136}
    
    Test Plan:
    go to localhost:3000/universes/:uuid
    Layout and display should match as given in the screenshot.
    
    Reviewers: karthik, ram
    
    Reviewed By: ram
    
    Subscribers: eng
    
    Differential Revision: https://phabricator.dev.yugabyte.com/D650

--- managed/ui/src/components/MapMarker.js
+++ managed/ui/src/components/MapMarker.js
@@ -3,35 +3,51 @@
 import React, { Component, PropTypes } from 'react';
 import { Marker, Popup } from 'react-leaflet';
 import { Icon }  from 'leaflet';
-import CustomMarker from '../stylesheets/images/marker.png';
+import DefaultMarker from '../stylesheets/images/marker.png';
 import ShadowMarker from '../stylesheets/images/marker-shadow.png';
+import RootMarker from '../stylesheets/images/rootmarker.png';
+
 export default class MapMarker extends Component {
   static propTypes = {
     latitude: PropTypes.number.isRequired,
     longitude: PropTypes.number.isRequired,
     label: PropTypes.string,
-    customIcon: PropTypes.bool
+    type: PropTypes.oneOf(['Default', 'Root', 'Async', 'Cache'])
   };
 
   static defaultProps = {
-    customIcon: false
+    type: "Default"
   }
 
   render() {
-    const { latitude, longitude, label, customIcon } = this.props;
+    const { latitude, longitude, label, type } = this.props;
     var popup;
     if (label) {
       popup = <Popup><span>{label}</span></Popup>
     }
 
     var opts = {};
-    if (customIcon) {
+    if( type === "Default" ) {
       opts['icon'] = new Icon({
-        iconUrl: CustomMarker,
+        iconUrl: DefaultMarker,
         shadowUrl: ShadowMarker,
-        iconSize:     [20, 27],
-        popupAnchor:  [10, 10],
-        iconAnchor:   [10,30],
+        iconSize: [20, 27],
+        popupAnchor: [10, 10],
+        iconAnchor: [10, 30],
+        shadowAnchor: [12, 46]
+      });
+    }
+    else {
+      var markerData = "";
+      if (type === "Root") {
+        markerData = RootMarker;
+      }
+      opts['icon'] = new Icon({
+        iconUrl: markerData,
+        shadowUrl: "",
+        iconSize: [27, 27],
+        popupAnchor: [10, 10],
+        iconAnchor: [10, 30],
         shadowAnchor: [12, 46]
       });
     }
