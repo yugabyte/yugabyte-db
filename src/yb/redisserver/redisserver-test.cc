dcaa423a6548736945b509ae3ef231297f16404b
Amitanand Aiyer <amitanandaiyer@users.noreply.github.com>
2016-10-18 14:20:43 -0700

    Integrate cpp_redis client into our codebase and use it to test the redis proxy layer.
    Summary:
    Integrate cpp_redis into our build system.
    Add a redis test that uses the open source redis client
    
    Test Plan: unit tests. added a unit test to redisserver-test
    
    Reviewers: kannan, mikhail, karthik
    
    Reviewed By: karthik
    
    Subscribers: eng
    
    Differential Revision: https://phabricator.dev.yugabyte.com/D674

--- src/yb/redisserver/redisserver-test.cc
+++ src/yb/redisserver/redisserver-test.cc
@@ -4,6 +4,9 @@
 #include <string>
 #include <vector>
 
+#include "cpp_redis/redis_client.hpp"
+#include "cpp_redis/reply.hpp"
+
 #include "yb/gutil/strings/substitute.h"
 #include "yb/integration-tests/redis_table-test.h"
 #include "yb/redisserver/redis_server.h"
@@ -23,15 +26,20 @@ using yb::rpc::EncodeAsArrays;
 using yb::rpc::EncodeAsBulkString;
 using yb::rpc::EncodeAsSimpleString;
 
+typedef cpp_redis::redis_client RedisClient;
+typedef cpp_redis::reply RedisReply;
+
 class TestRedisService : public RedisTableTest {
  public:
   void SetUp() override;
   void TearDown() override;
 
+ protected:
   void SendCommandAndExpectTimeout(const string& cmd);
 
   void SendCommandAndExpectResponse(const string& cmd, const string& resp);
 
+  int server_port() { return redis_server_port_; }
  private:
   Status SendCommandAndGetResponse(
       const string& cmd, int expected_resp_length, int timeout_in_millis = 1000);
@@ -49,7 +57,7 @@ void TestRedisService::SetUp() {
 
   redis_server_port_ = GetFreePort(&redis_port_lock_);
   RedisServerOptions opts;
-  opts.rpc_opts.rpc_bind_addresses = strings::Substitute("0.0.0.0:$0", redis_server_port_);
+  opts.rpc_opts.rpc_bind_addresses = strings::Substitute("0.0.0.0:$0", server_port());
 
   auto master_rpc_addrs = master_rpc_addresses_as_strings();
   opts.master_addresses_flag = JoinStrings(master_rpc_addrs, ",");
@@ -63,7 +71,7 @@ void TestRedisService::SetUp() {
   LOG(INFO) << "Redis server successfully started.";
 
   Sockaddr remote;
-  remote.ParseString("0.0.0.0", redis_server_port_);
+  remote.ParseString("0.0.0.0", server_port());
   CHECK_OK(client_sock_.Init(0));
   CHECK_OK(client_sock_.SetNoDelay(false));
   LOG(INFO) << "Connecting to " << remote.ToString();
@@ -156,5 +164,44 @@ TEST_F(TestRedisService, DISABLED_TestSetThenGet) {
   SendCommandAndExpectResponse("*3\r\n$3\r\nset\r\n$3\r\nfoo\r\n$4\r\nTEST\r\n", "+OK\r\n");
   SendCommandAndExpectResponse("*2\r\n$3\r\nget\r\n$3\r\nfoo\r\n", "+TEST\r\n");
 }
+
+TEST_F(TestRedisService, TestUsingOpenSourceClient) {
+  RedisClient client;
+  std::atomic_int callbacks_called(0);
+
+  client.connect("127.0.0.1", server_port(), [] (RedisClient&) {
+    LOG(ERROR) << "client disconnected (disconnection handler)" << std::endl;
+  });
+
+  // Same as client.send({ "SET", "hello", "42" }, ...)
+  client.set("hello", "42", [&callbacks_called] (RedisReply& reply) {
+    LOG(INFO) << "Received response : " << reply.as_string() << std::endl;
+    callbacks_called++;
+    ASSERT_EQ("OK", reply.as_string());
+  });
+  // Same as client.send({ "DECRBY", "hello", 12 }, ...)
+  client.decrby("hello", 12, [&callbacks_called] (RedisReply& reply) {
+    LOG(INFO) << "Received response : " << reply.as_integer() << std::endl;
+    callbacks_called++;
+    // TBD: ASSERT_EQ(30, reply.as_integer());
+  });
+  // Same as client.send({ "GET", "hello" }, ...)
+  client.get("hello", [&callbacks_called] (RedisReply& reply) {
+    LOG(INFO) << "Received response : " << reply.as_integer() << std::endl;
+    callbacks_called++;
+    // TBD: ASSERT_EQ(30, reply.as_integer());
+  });
+  // Same as client.send({ "SET", "world", "72" }, ...)
+  client.set("world", "72", [&callbacks_called] (RedisReply& reply) {
+    LOG(INFO) << "Received response : " << reply.as_string() << std::endl;
+    callbacks_called++;
+    ASSERT_EQ("OK", reply.as_string());
+  });
+  // Commands are pipelined and only sent when client.commit() is called.
+  // sync_commit() waits until all responses are received.
+  client.sync_commit();
+  ASSERT_EQ(4, callbacks_called);
+}
+
 }  // namespace redisserver
 }  // namespace yb
