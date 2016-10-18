1427b609fe4487819e0c1f9497d953b7885fcf58
Pritam Roy <jordanblink@users.noreply.github.com>
2016-10-18 11:48:46 -0700

    Create Form for Entering DC Config Values
    Summary:
    Created OnlineCodePad for creating DC Config JSON.
    CodePad supports syntax highlighting, code folding and basic autocomplete.
    
    {F1116}
    {F1117}
    {F1118}
    
    Test Plan:
    Go to locahost:3000/setup_dc
    You should be able to see the form as in the screenshot
    
    Reviewers: karthik, ram
    
    Reviewed By: ram
    
    Subscribers: eng
    
    Differential Revision: https://phabricator.dev.yugabyte.com/D694

---
+++ managed/ui/src/pages/SetupDataCenter.js
@@ -0,0 +1,12 @@
+// Copyright (c) YugaByte, Inc.
+
+import React, { Component } from 'react';
+import SetupDataCenterFormContainer from '../containers/forms/SetupDataCenterFormContainer';
+
+export default class SetupDataCenter extends Component {
+  render() {
+    return (
+      <SetupDataCenterFormContainer />
+    );
+  }
+}
