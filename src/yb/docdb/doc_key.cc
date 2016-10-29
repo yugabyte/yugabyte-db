c4b2fa91199ad5f614b9ff62f6f599ed5c60eeaf
Hector Cuellar <hectorgcr@users.noreply.github.com>
2016-10-29 00:29:10 -0700

    cast raw_key of data type INT32 as an int32_t
    Test Plan: tablet_peer-test doesn't cause an issue in asan build
    
    Reviewers: mikhail, kannan
    
    Reviewed By: kannan
    
    Subscribers: eng
    
    Differential Revision: https://phabricator.dev.yugabyte.com/D783

--- src/yb/docdb/doc_key.cc
+++ src/yb/docdb/doc_key.cc
@@ -151,9 +151,11 @@ DocKey DocKey::FromKuduEncodedKey(const EncodedKey &encoded_key, const Schema &s
     const auto& type_info = *schema.column(i).type_info();
     const void* const raw_key = encoded_key.raw_keys()[i];
     switch (type_info.type()) {
-      case DataType::INT64: FALLTHROUGH_INTENDED;
-      case DataType::INT32:
+      case DataType::INT64:
         new_doc_key.range_group_.emplace_back(*reinterpret_cast<const int64_t*>(raw_key));
+        break;
+      case DataType::INT32:
+        new_doc_key.range_group_.emplace_back(*reinterpret_cast<const int32_t*>(raw_key));
         break;
       case DataType::STRING: FALLTHROUGH_INTENDED;
       case DataType::BINARY:
