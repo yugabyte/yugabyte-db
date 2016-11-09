097bb7da2780eaf7e75e314bcb010e47f1726cbc
Jim Nasby <Jim.Nasby@BlueTreble.com>
2016-11-09 12:55:10 -0600

    Merge additional version support and travis testing
    Fixes is_indexed() for older versions
    Fix unit tests on older versions
    Add 8.2-8.4 and 9.6 to Travis
    
    Squashed commit of the following:
    
    commit 9a8c9db6d8e36cf6d99b29aedfe629a589b4c872
    Author: Jim Nasby <Jim.Nasby@BlueTreble.com>
    Date:   Mon Nov 7 21:40:40 2016 -0600
    
        Enable 9.6 in travis
    
    commit a41a0821f0194995137abe66427a7f49327d1daf
    Author: Jim Nasby <Jim.Nasby@BlueTreble.com>
    Date:   Mon Nov 7 21:40:18 2016 -0600
    
        Add alternative output for 9.6
    
    commit ebfc1ee343a473b0b2cb31bfbd1e22a061be9ea3
    Merge: c79f048 9ad1685
    Author: Jim Nasby <Jim.Nasby@BlueTreble.com>
    Date:   Mon Nov 7 21:37:39 2016 -0600
    
        Merge branch 'master' into more_travis
    
    commit c79f048d64a48e7dacf6900cdd7d9d0695508771
    Author: Jim Nasby <Jim.Nasby@BlueTreble.com>
    Date:   Mon Nov 7 15:42:47 2016 -0600
    
        Add fix for 9.6
    
    commit 46921c479d00f92415b8e9ffe96ccc9a44281f2d
    Author: Jim Nasby <Jim.Nasby@BlueTreble.com>
    Date:   Mon Nov 7 15:32:48 2016 -0600
    
        8.1 doesn't work on travis
    
    commit e337147df2073c8cb50c4f7b4052bb84d7f82adb
    Author: Jim Nasby <Jim.Nasby@BlueTreble.com>
    Date:   Mon Nov 7 15:32:30 2016 -0600
    
        Fix 9.5
    
    commit a32a8be03431f9af3a8e4fbd56f26bb12e351ad3
    Author: Jim Nasby <Jim.Nasby@BlueTreble.com>
    Date:   Mon Nov 7 15:12:04 2016 -0600
    
        There's a chance this will work on 8.1
    
    commit 3ca77ad9086014e5bec4f4dd3c387be285742341
    Author: Jim Nasby <Jim.Nasby@BlueTreble.com>
    Date:   Mon Nov 7 15:11:48 2016 -0600
    
        Fixes for 8.2
    
    commit 0923abf2797fb236dbd3d393fae92ebac948f9ce
    Author: Jim Nasby <Jim.Nasby@BlueTreble.com>
    Date:   Mon Nov 7 14:48:57 2016 -0600
    
        Fix unit test for older versions
    
    commit b350483c191ea7bd3ce8457286574d44ce15d07b
    Author: Jim Nasby <Jim.Nasby@BlueTreble.com>
    Date:   Mon Nov 7 14:40:48 2016 -0600
    
        Fix upgrade script
    
    commit 57ab54ad4a30ad51184fa78514e192035a9c68a9
    Author: Jim Nasby <Jim.Nasby@BlueTreble.com>
    Date:   Fri Nov 4 16:23:40 2016 -0500
    
        Fix 8.3 error
    
    commit 97072f4a3a3bdbfbac23c254a66cd46c6f3d24d6
    Merge: c373d0c 9a0f5bd
    Author: Jim Nasby <Jim.Nasby@BlueTreble.com>
    Date:   Fri Nov 4 15:54:37 2016 -0500
    
        Merge branch 'master' into more_travis
    
    commit c373d0ceb516bd5e2c1eb0c4b2f512eed405b455
    Merge: 8854c00 367f66e
    Author: Jim Nasby <Jim.Nasby@BlueTreble.com>
    Date:   Sun Oct 23 10:43:43 2016 -0500
    
        Merge branch 'master' into more_travis
    
    commit 367f66e283a3c00b19c9fbc7375f89d4b59db4b7
    Author: Jim Nasby <Jim.Nasby@BlueTreble.com>
    Date:   Sun Oct 23 10:08:10 2016 -0500
    
        Add 8.2-8.4 to travis
        This requires changing _is_indexed() not not use WITH
    
        There's still some version-related cleanup to do, like updating the
        minimum-version test in the Makefile, updating META.json, and maybe the
        docs. There's also a questionable pg73 test. But merging this in now
        will make it easier to detect problems via automated travis testing.
    
        Squashed commit of the following:
    
        commit 8854c0036589ae28fbda3d5b7a680357297a5f5b
        Author: Jim Nasby <Jim.Nasby@BlueTreble.com>
        Date:   Sat Oct 15 19:10:06 2016 -0500
    
            Remove 8.0 and 8.1 from travis
    
        commit b1f5c2bf51e9b9edfd842b89615362f04c485d71
        Author: Jim Nasby <Jim.Nasby@BlueTreble.com>
        Date:   Sat Oct 15 19:01:08 2016 -0500
    
            Remove use of WITH
    
        commit d74d0adbb4cb78d37d89bd70b2f3abfb8e28986f
        Author: Jim Nasby <Jim.Nasby@BlueTreble.com>
        Date:   Sat Oct 15 18:38:53 2016 -0500
    
            Get rid of WITH
    
    commit 8854c0036589ae28fbda3d5b7a680357297a5f5b
    Author: Jim Nasby <Jim.Nasby@BlueTreble.com>
    Date:   Sat Oct 15 19:10:06 2016 -0500
    
        Remove 8.0 and 8.1 from travis
    
    commit b1f5c2bf51e9b9edfd842b89615362f04c485d71
    Author: Jim Nasby <Jim.Nasby@BlueTreble.com>
    Date:   Sat Oct 15 19:01:08 2016 -0500
    
        Remove use of WITH
    
    commit d74d0adbb4cb78d37d89bd70b2f3abfb8e28986f
    Author: Jim Nasby <Jim.Nasby@BlueTreble.com>
    Date:   Sat Oct 15 18:38:53 2016 -0500
    
        Get rid of WITH

--- test/sql/throwtap.sql
+++ test/sql/throwtap.sql
@@ -124,7 +124,10 @@ SELECT * FROM check_test(
     '',
     '    died: P0001: todo_end() called without todo_start()'
     || CASE WHEN pg_version_num() < 90200 THEN '' ELSE '
-        CONTEXT:
+        CONTEXT:'
+    || CASE WHEN pg_version_num() < 90600 THEN '' ELSE '
+            PL/pgSQL function todo_end() line 7 at RAISE' END
+    || '
             SQL statement "SELECT * FROM todo_end()"
             PL/pgSQL function lives_ok(text,text) line 14 at EXECUTE'
     || CASE WHEN pg_version_num() >= 90500 THEN '' ELSE ' statement' END END
@@ -306,7 +309,7 @@ SELECT lives_ok(
     CASE WHEN pg_version_num() < 90500 THEN $exec$
 CREATE FUNCTION check_assert(b boolean) RETURNS void LANGUAGE plpgsql AS $body$
 BEGIN
-    RAISE 'this code should never be called!';
+    RAISE EXCEPTION 'this code should never be called!';
 END
 $body$;
 $exec$
@@ -322,35 +325,43 @@ $exec$
 );
 
 CREATE FUNCTION test_assert() RETURNS SETOF text LANGUAGE plpgsql AS $body$
+DECLARE
+  t text;
 BEGIN
 IF pg_version_num() >= 90500 THEN
-RETURN QUERY SELECT * FROM check_test(
+FOR t IN SELECT * FROM check_test(
     throws_ok( 'SELECT check_assert(false)', 'P0004', 'assert description' ),
     true,
     'throws_ok catches assert',
     'threw P0004: assert description',
     ''
-);
+) LOOP
+    RETURN NEXT t;
+END LOOP;
 
-RETURN QUERY SELECT * FROM check_test(
+FOR t IN SELECT * FROM check_test(
     throws_ok( 'SELECT check_assert(true)', 'P0004' ),
     false,
     'throws_ok does not accept passing assert',
     'threw P0004',
     '      caught: no exception
       wanted: P0004'
-);
+) LOOP
+    RETURN NEXT t;
+END LOOP;
 
-RETURN QUERY SELECT * FROM check_test(
+FOR t IN SELECT * FROM check_test(
     lives_ok( 'SELECT check_assert(true)' ),
     true,
     'lives_ok calling check_assert(true)',
     '',
     ''
-);
+) LOOP
+    RETURN NEXT t;
+END LOOP;
 
 -- Check its diagnostics when there is an exception.
-RETURN QUERY SELECT * FROM check_test(
+FOR t IN SELECT * FROM check_test(
     lives_ok( 'SELECT check_assert(false)' ),
     false,
     'lives_ok with check_assert(false)',
@@ -360,46 +371,56 @@ RETURN QUERY SELECT * FROM check_test(
             PL/pgSQL function check_assert(boolean) line 3 at ASSERT
             SQL statement "SELECT check_assert(false)"
             PL/pgSQL function lives_ok(text,text) line 14 at EXECUTE
-            PL/pgSQL function test_assert() line 30 at RETURN QUERY'
-);
+            PL/pgSQL function test_assert() line 38 at FOR over SELECT rows'
+) LOOP
+    RETURN NEXT t;
+END LOOP;
 ELSE
-RETURN QUERY SELECT * FROM check_test(
+FOR t IN SELECT * FROM check_test(
     pass(''),
     true,
     'throws_ok catches assert',
     '',
     ''
-);
+) LOOP
+    RETURN NEXT t;
+END LOOP;
 
-RETURN QUERY SELECT * FROM check_test(
+FOR t IN SELECT * FROM check_test(
     fail(''),
     false,
     'throws_ok does not accept passing assert',
     '',
     ''
-);
+) LOOP
+    RETURN NEXT t;
+END LOOP;
 
-RETURN QUERY SELECT * FROM check_test(
+FOR t IN SELECT * FROM check_test(
     pass(''),
     true,
     'lives_ok calling check_assert(true)',
     '',
     ''
-);
+) LOOP
+    RETURN NEXT t;
+END LOOP;
 
 -- Check its diagnostics when there is an exception.
-RETURN QUERY SELECT * FROM check_test(
+FOR t IN SELECT * FROM check_test(
     fail(''),
     false,
     'lives_ok with check_assert(false)',
     '',
     ''
-);
+) LOOP
+    RETURN NEXT t;
+END LOOP;
 END IF;
 END
 $body$;
 
-SELECT test_assert();
+SELECT * FROM test_assert();
 
 /****************************************************************************/
 -- Finish the tests and clean up.
