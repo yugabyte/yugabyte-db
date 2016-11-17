0a81b21927ee2e71f00e545885d0f8c35cb811c7
Pritam Roy <jordanblink@users.noreply.github.com>
2016-11-17 13:30:22 -0800

    Rolling Upgrade and Set GFlags Client to Middleware Integration
    Summary:
    Integration of Client to Middleware for RollingRestart and SetGflags
    
    {F1303}
    
    {F1304}
    
    Test Plan: Go to UI , Set up a rolling restart or upgrade using the form options.
    
    Reviewers: karthik, ram
    
    Reviewed By: ram
    
    Subscribers: eng
    
    Differential Revision: https://phabricator.dev.yugabyte.com/D775

--- managed/ui/src/components/fields/YBCheckBox.js
+++ managed/ui/src/components/fields/YBCheckBox.js
@@ -2,18 +2,25 @@
 
 import React, { Component } from 'react';
 import YBLabel from './YBLabel';
+import {isValidObject} from '../../utils/ObjectUtils';
 
 export default class YBCheckBox extends Component {
 
   render() {
     const { input, label, meta, checkState,
-      onClick } = this.props;
+            onClick } = this.props;
+    var onCheckClick = function(event) {
+      input.onChange(event);
+      if (isValidObject(onClick)) {
+        onClick(event);
+      }
+    }
     return (
-       <YBLabel label={label} meta={meta} >
-            <input {...input} type="checkbox"
-                   name={this.props.name} defaultChecked={checkState}
-                   id={this.props.id} onClick={onClick}
-            />
+       <YBLabel label={label} meta={meta}>
+         <input {...input} type="checkbox"
+                           name={this.props.name} defaultChecked={checkState}
+                           id={this.props.id} onClick={onCheckClick}
+         />
        </YBLabel>
       )
     }
--- managed/ui/src/config.js
+++ managed/ui/src/config.js
@@ -1,3 +1,5 @@
 // Copyright (c) YugaByte, Inc.
 
 export const ROOT_URL = location.href.indexOf('localhost') > 0 ? 'http://localhost:9000/api' : '/api';
+
+export const SOFTWARE_PACKAGE = "yb-server-0.0.1-SNAPSHOT.66a01f21a89450af4bfa4bf159811fd2191b83d0.tar.gz";
