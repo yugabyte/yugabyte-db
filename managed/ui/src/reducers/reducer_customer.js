9736c87146cec5426bb369ffa2d3ad4609580c90
Pritam Roy <jordanblink@users.noreply.github.com>
2016-11-21 15:41:50 -0800

    ENG-578 Fixed Error handling when customer is logged out by the server
    Summary:
    Fixed error handling in case call to authenticateCustomer() endpoint fails.
    If server logs the client out, then a generic error message "Unable to Authenticate Customer" is displayed.
    
    Test Plan:
    Go to localhost:3000.
    Come back to sbt console and shut down the server process.
    On changing tabs in ui you should be redirected to login page, with proper error message being displayed.
    
    Reviewers: karthik, ram
    
    Reviewed By: ram
    
    Subscribers: eng
    
    Differential Revision: https://phabricator.dev.yugabyte.com/D853

--- managed/ui/src/reducers/reducer_customer.js
+++ managed/ui/src/reducers/reducer_customer.js
@@ -7,19 +7,21 @@ import {
   FETCH_CUSTOMER_TASKS, FETCH_CUSTOMER_TASKS_SUCCESS, FETCH_CUSTOMER_TASKS_FAILURE,
   RESET_CUSTOMER_TASKS
 } from '../actions/customers';
+import {isValidObject} from '../utils/ObjectUtils';
 
-const INITIAL_STATE = {customer: null, universes: [], tasks: [], status: null, error: null, loading: false};
+const INITIAL_STATE = {customer: null, universes: [], tasks: [], status: null,
+                       error: null, loading: false};
 
 export default function(state = INITIAL_STATE, action) {
   let error;
   switch(action.type) {
     case VALIDATE_FROM_TOKEN:
-      return { ...state, customer: null, status: 'storage', error: null, loading: true};
+      return { ...state, customer: null, status: 'authenticate', error: null, loading: true};
     case VALIDATE_FROM_TOKEN_SUCCESS:
       return { ...state, customer: action.payload.name, universes: action.payload.universes, status: 'authenticated', error: null, loading: false}; //<-- authenticated
     case VALIDATE_FROM_TOKEN_FAILURE:// return error and make loading = false
-      error = action.payload.data || {message: action.payload.message};//2nd one is network or server down errors
-      return { ...state, customer: null, status: 'storage', error: error, loading: false};
+      error = "Unable to Authenticate Customer"
+      return { ...state, customer: null, status: 'authenticate_failure', error: error, loading: false};
     case REGISTER:// sign up user, set loading = true and status = register
       return { ...state, customer: null, status: 'register', error: null, loading: true};
     case REGISTER_SUCCESS://return user, status = authenticated and make loading = false
@@ -36,11 +38,15 @@ export default function(state = INITIAL_STATE, action) {
     case LOGIN_SUCCESS://return authenticated user,  make loading = false and status = authenticated
       return { ...state, customer: action.payload.data.authToken, status: 'authenticated', error:null, loading: false}; //<-- authenticated
     case LOGIN_FAILURE:// return error and make loading = false
+      if (isValidObject(action.payload.data)) {
       if (typeof action.payload.data.error === 'string'){
         error = action.payload.data.error;
       } else {
         error = action.payload.data.error.password[0];
-       }
+      }
+      } else {
+        error = "No Response From Server"
+      }
       return { ...state, customer: null, status: 'login', error: error, loading: false};
     case LOGOUT:
       return {...state, status: 'logout', error: null, loading: true};
