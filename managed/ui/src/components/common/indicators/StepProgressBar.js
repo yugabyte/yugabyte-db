a3eca8925f0cb50e43cb4c7c4915059f7423d41f
Pritam Roy <jordanblink@users.noreply.github.com>
2016-12-02 10:29:43 -0800

    Created Component For Task Progress Indicator
    Summary:
    {F1501}
    Created Task Progress Indicator in accordance with UX guidelines.
    This is a new component, hence I created a new file, although I imagine we'll get rid of ProgressWidget component and this will eventually become ProgressWidget
    
    Test Plan:
    Create a new Universe/Edit and Existing one.
    Go to Universe Details page.
    Go to Tasks tab.
    You should see the current task progress monitor like the one in screenshot.
    
    Reviewers: karthik, ram
    
    Reviewed By: ram
    
    Subscribers: eng
    
    Differential Revision: http://phabricator.dev.yugabyte.com:80/D884

---
+++ managed/ui/src/components/common/indicators/StepProgressBar.js
@@ -0,0 +1,31 @@
+// Copyright (c) YugaByte, Inc.
+
+import React, { Component } from 'react';
+
+export default class StepProgressBar extends Component {
+  render() {
+    const { details: { taskDetails } } = this.props.progressData;
+    var taskClassName = "";
+    var getTaskClass = function(type) {
+      if ( type === "Initializing" ) {
+        return "pending";
+      } else if ( type === "Success" ) {
+        return "finished";
+      } else if ( type === "Running" ) {
+        return "running";
+      }
+      return null;
+    }
+    const listLabels = taskDetails.map(function(item, idx) {
+      taskClassName = getTaskClass(item.state);
+      return (
+        <li key={idx} className={taskClassName}>{item.title}</li>
+      );
+    }, this);
+    return (
+      <ul className="progressbar">
+            {listLabels}
+      </ul>
+    )
+  }
+}
