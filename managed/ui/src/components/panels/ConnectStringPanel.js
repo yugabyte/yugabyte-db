257fe46e08c41ec2a776dd28ce85e2b28d0955ef
Pritam Roy <jordanblink@users.noreply.github.com>
2016-12-02 10:30:50 -0800

    Fixed Universe Detail Layout based on discussions on 12/01
    Summary:
    Fixed Layout based on recommendations from @karthik, @ram @sid
    {F1548}
    
    Test Plan:
    Go to Universe Detail page.
    The layout should look as in the screenshot.
    
    Reviewers: karthik, ram
    
    Reviewed By: ram
    
    Subscribers: karthik, ram, sid, eng
    
    Differential Revision: http://phabricator.dev.yugabyte.com:80/D901

--- managed/ui/src/components/panels/ConnectStringPanel.js
+++ managed/ui/src/components/panels/ConnectStringPanel.js
@@ -1,24 +1,29 @@
 // Copyright (c) YugaByte, Inc.
 
 import React, { Component } from 'react';
-
+import { FormattedDate } from 'react-intl';
 import { DescriptionList } from '../common/descriptors';
 import { ROOT_URL } from '../../config';
 import { YBPanelItem } from '.';
 
 export default class UniverseInfoPanel extends Component {
   render() {
-    const endpointUrl = ROOT_URL + "/customers/" + this.props.customerId +
-                        "/universes/" + this.props.universeId + "/masters";
+    const {universeInfo, customerId} = this.props;
+    var universeId = universeInfo.universeUUID;
+    const endpointUrl = ROOT_URL + "/customers/" + customerId +
+                        "/universes/" + universeId + "/masters";
     const connect_string = "yb_load_test_tool --load_test_master_endpoint " + endpointUrl;
     const endpoint =
       <a href={endpointUrl} target="_blank">Endpoint &nbsp;
         <i className="fa fa-external-link" aria-hidden="true"></i>
       </a>;
-
+    var formattedCreationDate = <FormattedDate value={universeInfo.creationDate}
+                                               year='numeric' month='long' day='2-digit'
+                                               hour='2-digit' minute='2-digit' second='2-digit' timeZoneName='short' />
     var connectStringPanelItems = [
-      {name: "Universe ID", data: this.props.universeId},
-      {name: "Customer ID", data: this.props.customerId},
+      {name: "Launch Time", data: formattedCreationDate},
+      {name: "Universe ID", data: universeId},
+      {name: "Customer ID", data: customerId},
       {name: "Meta Masters", data: endpoint},
       {name: "Load Test", data: connect_string, dataClass: "ybCodeSnippet well well-sm"}
     ];
