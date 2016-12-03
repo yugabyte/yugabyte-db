ea129f3430589a01d4ff77b3556a8f64d736e3dd
Bharat Baddepudi <bbaddepudi@users.noreply.github.com>
2016-12-02 16:34:12 -0800

    Bump server tarball hash
    Summary: Bump server tarball hash
    
    Test Plan:
    Create universe done.
    
    Reviewers: bogdan, ram
    
    Reviewed By: ram
    
    Subscribers: ram, eng
    
    Differential Revision: https://phabricator.dev.yugabyte.com/D908

--- managed/ui/src/config.js
+++ managed/ui/src/config.js
@@ -2,4 +2,4 @@
 
 export const ROOT_URL = location.href.indexOf('localhost') > 0 ? 'http://localhost:9000/api' : '/api';
 
-export const SOFTWARE_PACKAGE = "s3://no-such-url/yugabyte/yb-server-0.0.1-SNAPSHOT.df2ea290b55ea6284c71750c0ad7a36c5f1b2612.tar.gz";
+export const SOFTWARE_PACKAGE = "s3://no-such-url/yugabyte/yb-server-0.0.1-SNAPSHOT.aac20c4f98e7c5ee3178b8836006b71941915961.tar.gz";
