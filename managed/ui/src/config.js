de987e002d0ada5766910d6c539d6bdef26fd04e
Bharat Baddepudi <bbaddepudi@users.noreply.github.com>
2016-12-28 12:07:48 -0800

    Update server tarball.
    Summary:
    To take care of server side changes in the past 3 weeks.
    
    Test Plan:
    Ran create/expand and the data move did not get stuck.
    
    Reviewers: karthik, hector, kannan
    
    Reviewed By: kannan
    
    Subscribers: eng
    
    Differential Revision: https://phabricator.dev.yugabyte.com/D991

--- managed/ui/src/config.js
+++ managed/ui/src/config.js
@@ -2,4 +2,4 @@
 
 export const ROOT_URL = location.href.indexOf('localhost') > 0 ? 'http://localhost:9000/api' : '/api';
 
-export const SOFTWARE_PACKAGE = "s3://no-such-url/yugabyte/yb-server-0.0.1-SNAPSHOT.aac20c4f98e7c5ee3178b8836006b71941915961.tar.gz";
+export const SOFTWARE_PACKAGE = "s3://no-such-url/yugabyte/yb-server-0.0.1-SNAPSHOT.df587f5023d31c3a268e92aa7e0e132614f02265.tar.gz";
