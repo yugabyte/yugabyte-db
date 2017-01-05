a344d595f891b8b19410fbab9c17168f324ce06d
Ram Sri <ramkumarvs@users.noreply.github.com>
2017-01-05 14:50:18 -0800

    Update yb_release to match other scripts
    Summary:
    - Update yb_release params to match other scripts
    - Add destination param
    - Build yugabyte option inside of the release script
    
    Test Plan:
    ./yb_release --build release
    ./yb_release --destination /tmp
    
    Reviewers: karthik
    
    Reviewed By: karthik
    
    Subscribers: eng
    
    Differential Revision: https://phabricator.dev.yugabyte.com/D1015

--- build-support/yb_release.py
+++ build-support/yb_release.py
@@ -4,30 +4,39 @@
 import os
 import logging
 import argparse
+import shutil
+from subprocess import call
 from ybops.utils import init_env, log_message
 from ybops.release_manager import ReleaseManager
+from ybops.common.exceptions import YBOpsRuntimeError
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--publish', action="store_true")
+parser.add_argument('--build', help='Build type (debug/release)', default="debug")
+parser.add_argument('--publish', action='store_true', help='Publish release to S3.')
+parser.add_argument('--destination', help='Copy release to Destination folder.')
 args = parser.parse_args()
 
-init_env(logging.INFO)
-repository_root = os.path.abspath(os.path.dirname(os.path.dirname(__file__)))
-build_dir = os.path.join("build", "latest")
-
-release_type = 'debug'
-# We need to find out what the symlink path looks like.
-if 'release' in os.path.basename(os.path.realpath(build_dir)):
-    release_type = 'release'
-
 try:
+    init_env(logging.INFO)
+    repository_root = os.path.abspath(os.path.dirname(os.path.dirname(__file__)))
+    log_message(logging.INFO, "Building Yugabyte code: '{}' build".format(args.build))
+
+    os.chdir(repository_root)
+    call(["./yb_build.sh", args.build])
+
+    build_dir = os.path.join("build", "latest")
     release_manager = ReleaseManager({"repository": repository_root,
                                       "name": "yb-server",
-                                      "type": release_type})
+                                      "type": args.build})
     log_message(logging.INFO, "Generating release")
-    release_manager.generate_release()
+    release_file = release_manager.generate_release()
+
     if args.publish:
         log_message(logging.INFO, "Publishing release")
         release_manager.publish_release()
+    elif args.destination:
+        if not os.path.exists(args.destination):
+            raise YBOpsRuntimeError("Destination {} not a directory.".format(args.destination))
+        shutil.copy(release_file, args.destination)
 except RuntimeError as e:
     log_message(logging.ERROR, e)
