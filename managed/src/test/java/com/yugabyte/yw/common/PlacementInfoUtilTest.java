4146c9feb50af49316d18e876379947e5cef1374
Bharat Baddepudi <bbaddepudi@users.noreply.github.com>
2017-01-11 14:56:32 -0800

    ENG-752 : Support reducing the number of nodes for edit universe.
    Summary:
    This came up as a requirement from IONOS. Allow for reducing the number of tablet servers.
    
    Only tserver nodes with no masters running are chosen and decommissioned. We wait for the data to move out of these nodes by blacklisting them.
    
    Also cleaned up some variable names in Edit universe codepath to reflect deleted nodes vs blacklisted tservers.
    
    Test Plan:
    Create and then shrink universe - 2region,7nodes to 2region,5nodes. Attached app log.
    ```
    2017-01-15 23:10:33,408 [INFO] from com.yugabyte.yw.common.PlacementInfoUtil in application-akka.actor.default-dispatcher-56 - Num nodes changing from 7 to 5.
    2017-01-15 23:10:33,409 [INFO] from com.yugabyte.yw.common.PlacementInfoUtil in application-akka.actor.default-dispatcher-56 - Az Map {cf74bca9-3b72-4300-a80f-06c641d8286d=3, fc23dbb9-f759-47bd-b74f-515476b60f04=2, 12920eea-707b-4b85-91f7-75ab47d8638e=2}
    2017-01-15 23:10:33,409 [DEBUG] from com.yugabyte.yw.common.PlacementInfoUtil in application-akka.actor.default-dispatcher-56 - Removing node [name: yb-1-bbr-n7.clo
    udInfo: us-west-2c.us-west-2.aws, type: m3.medium, ip: 10.129.51.50, isMaster: false, isTserver: true].
    2017-01-15 23:10:33,409 [DEBUG] from com.yugabyte.yw.common.PlacementInfoUtil in application-akka.actor.default-dispatcher-56 - Removing node [name: yb-1-bbr-n2.clo
    udInfo: us-west-2a.us-west-2.aws, type: m3.medium, ip: 10.129.16.241, isMaster: true, isTserver: true].
    ```
    
    Also ensured expand/edit did not regress.
    
    Retested after rebase with a 3region,8node to 3region,4node shrink (with fixes for bunch of devops issues and ENG-782) . Attaching app log.
    
    ```
    2017-01-19 17:36:47,505 [INFO] from com.yugabyte.yw.common.PlacementInfoUtil in application-akka.actor.default-dispatcher-21 - Num nodes changing from 8 to 4.
    2017-01-19 17:36:47,506 [INFO] from com.yugabyte.yw.common.PlacementInfoUtil in application-akka.actor.default-dispatcher-21 - Az Map {19a38395-a280-46d0-a468-2b9016f58fb1=3, 85d6f691-3e55-4022-b9e3-49e2d379fdc3=3, 9d1046d6-b17a-4fef-87cd-0a214b1f823b=2}
    2017-01-19 17:36:47,506 [DEBUG] from com.yugabyte.yw.common.PlacementInfoUtil in application-akka.actor.default-dispatcher-21 - Removing node [name: yb-1-bbaabb-n5.cloudInfo: us-west-1b.us-west-1.aws, type: m3.medium, ip: 10.128.45.39, isMaster: false, isTserver: true, state: ToBeDecommissioned].
    2017-01-19 17:36:47,506 [DEBUG] from com.yugabyte.yw.common.PlacementInfoUtil in application-akka.actor.default-dispatcher-21 - Removing node [name: yb-1-bbaabb-n7.cloudInfo: us-west-2b.us-west-2.aws, type: m3.medium, ip: 10.129.44.22, isMaster: false, isTserver: true, state: ToBeDecommissioned].
    2017-01-19 17:36:47,506 [DEBUG] from com.yugabyte.yw.common.PlacementInfoUtil in application-akka.actor.default-dispatcher-21 - Removing node [name: yb-1-bbaabb-n6.cloudInfo: us-east-1d.us-east-1.aws, type: m3.medium, ip: 10.130.52.108, isMaster: false, isTserver: true, state: ToBeDecommissioned].
    2017-01-19 17:36:47,506 [DEBUG] from com.yugabyte.yw.common.PlacementInfoUtil in application-akka.actor.default-dispatcher-21 - Removing node [name: yb-1-bbaabb-n8.cloudInfo: us-west-1b.us-west-1.aws, type: m3.medium, ip: 10.128.39.149, isMaster: false, isTserver: true, state: ToBeDecommissioned].
    ```
    
    Reviewers: karthik
    
    Reviewed By: karthik
    
    Subscribers: jenkins-bot
    
    Differential Revision: https://phabricator.dev.yugabyte.com/D1085

--- managed/src/test/java/com/yugabyte/yw/common/PlacementInfoUtilTest.java
+++ managed/src/test/java/com/yugabyte/yw/common/PlacementInfoUtilTest.java
@@ -5,6 +5,9 @@ package com.yugabyte.yw.common;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
@@ -12,6 +15,9 @@ import org.mockito.Matchers;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.runners.MockitoJUnitRunner;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import play.libs.Json;
 import play.libs.ws.WSClient;
 import play.libs.ws.WSRequest;
@@ -20,16 +26,15 @@ import play.libs.ws.WSResponse;
 import java.util.Collection;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
 import java.util.UUID;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.CompletionStage;
 
-import org.junit.After;
-import org.junit.Before;
-
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertTrue;
 import static org.mockito.Matchers.anyString;
 import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.times;
@@ -46,6 +51,8 @@ import com.yugabyte.yw.models.Universe;
 import com.yugabyte.yw.models.helpers.NodeDetails;
 
 public class PlacementInfoUtilTest extends FakeDBApplication {
+  public static final Logger LOG = LoggerFactory.getLogger(PlacementInfoUtilTest.class);
+
   private Customer customer;
   private Universe universe;
   private final String univName = "Test Universe";
@@ -53,6 +60,8 @@ public class PlacementInfoUtilTest extends FakeDBApplication {
   private AvailabilityZone az1;
   private AvailabilityZone az2;
   private AvailabilityZone az3;
+  private AvailabilityZone az4;
+  private AvailabilityZone az5;
 
   @Before
   public void setUp() {
@@ -65,6 +74,8 @@ public class PlacementInfoUtilTest extends FakeDBApplication {
     az1 = AvailabilityZone.create(r1, "PlacementAZ 1", "az-1", "subnet-1");
     az2 = AvailabilityZone.create(r1, "PlacementAZ 2", "az-2", "subnet-2");
     az3 = AvailabilityZone.create(r2, "PlacementAZ 3", "az-3", "subnet-3");
+    az4 = AvailabilityZone.create(r2, "PlacementAZ 4", "az-4", "subnet-1");
+    az5 = AvailabilityZone.create(r2, "PlacementAZ 5", "az-5", "subnet-2");
     List<UUID> regionList = new ArrayList<UUID>();
     regionList.add(r1.uuid);
     regionList.add(r2.uuid);
@@ -73,7 +84,7 @@ public class PlacementInfoUtilTest extends FakeDBApplication {
     userIntent.universeName = univName;
     userIntent.replicationFactor = 3;
     userIntent.isMultiAZ = true;
-    userIntent.numNodes = 3;
+    userIntent.numNodes = 10;
     userIntent.provider = "aws";
     userIntent.regionList = regionList;
     Universe.saveDetails(univUuid, ApiUtils.mockUniverseUpdater(userIntent));
@@ -82,7 +93,7 @@ public class PlacementInfoUtilTest extends FakeDBApplication {
 
   @After
   public void tearDown() {
-    universe.delete(univUuid);
+    Universe.delete(univUuid);
   }
 
   private Universe.UniverseUpdater setAzUUIDs(UserIntent userIntent) {
@@ -94,12 +105,19 @@ public class PlacementInfoUtilTest extends FakeDBApplication {
         for (NodeDetails node : nodes) {
           switch (node.cloudInfo.az) {
           case "az-1":
+          case "az-4":
+          case "az-7":
+          case "az-10":
             node.azUuid = az1.uuid;
             break;
           case "az-2":
+          case "az-5":
+          case "az-8":
             node.azUuid = az2.uuid;
             break;
           case "az-3":
+          case "az-6":
+          case "az-9":
             node.azUuid = az3.uuid;
             break;
           }
@@ -111,12 +129,70 @@ public class PlacementInfoUtilTest extends FakeDBApplication {
     };
   }
 
+  private void removeNodeFromUniverse(final NodeDetails node) {
+    // Persist the desired node information into the DB.
+    Universe.UniverseUpdater updater = new Universe.UniverseUpdater() {
+      @Override
+      public void run(Universe universe) {
+        UniverseDefinitionTaskParams universeDetails = universe.getUniverseDetails();
+        LOG.debug("Removing node " + node.nodeName);
+        Set<NodeDetails> nodes = universeDetails.nodeDetailsSet;
+        NodeDetails nodeToRemove = null;
+        for (NodeDetails univNode : nodes) {
+          if (node.nodeName.equals(univNode.nodeName)) {
+            nodeToRemove = univNode;
+            break;
+          }
+        }
+        universeDetails.nodeDetailsSet.remove(nodeToRemove);
+        universeDetails.userIntent.numNodes = universeDetails.userIntent.numNodes - 1;
+        universe.setUniverseDetails(universeDetails);
+      }
+    };
+
+    Universe.saveDetails(univUuid, updater);
+  }
+
+  private int sum(List<Integer> a) {
+    int sum = 0;
+    for (Integer i : a) {
+      sum += i;
+    }
+    return sum;
+  }
+  
+  private double mean(List<Integer> a) {
+    return sum(a) / (a.size() * 1.0);
+  }
+
+  private double standardDeviation(List<Integer> a) {
+    int sum = 0;
+    double mean = mean(a);
+
+    for (Integer i : a) {
+      sum += Math.pow((i - mean), 2);
+    }
+    return Math.sqrt(sum / (a.size() - 1));
+  }
+
+  private void removeNodesAndVerify(Set<NodeDetails> nodes) {
+    // Simulate an actual shrink operation completion.
+    for (NodeDetails node : nodes) {
+      if (node.state == NodeDetails.NodeState.ToBeDecommissioned) {
+        removeNodeFromUniverse(node);
+      }
+    }
+    List<Integer> numNodes =
+      new ArrayList<Integer>(PlacementInfoUtil.getAzUuidToNumNodes(nodes).values());
+    assertTrue(standardDeviation(numNodes) == 0);
+  }
+
   @Test
   public void testExpandPlacement() {
     UniverseDefinitionTaskParams ud = universe.getUniverseDetails();
     ud.universeUUID = univUuid;
     Universe.saveDetails(univUuid, setAzUUIDs(ud.userIntent));
-    ud.userIntent.numNodes = 5;
+    ud.userIntent.numNodes = 12;
     universe = Universe.get(univUuid);
     PlacementInfoUtil.updateUniverseDefinition(ud, customer.getCustomerId());
     Set<NodeDetails> nodes = ud.nodeDetailsSet;
@@ -131,14 +207,40 @@ public class PlacementInfoUtilTest extends FakeDBApplication {
     ud.universeUUID = univUuid;
     Provider p = Provider.get("Amazon");
     Region r3 = Region.create(p, "region-3", "Region 3", "yb-image-3");
-    AvailabilityZone az4 = AvailabilityZone.create(r3, "az-4", "PlacementAZ 4", "subnet-4");
+    AvailabilityZone.create(r3, "az-4", "PlacementAZ 4", "subnet-4");
     ud.userIntent.regionList.add(r3.uuid);
 
     PlacementInfoUtil.updateUniverseDefinition(ud, customer.getCustomerId());
     Set<NodeDetails> nodes = ud.nodeDetailsSet;
     assertEquals(3, PlacementInfoUtil.getMastersToBeRemoved(nodes).size());
-    assertEquals(3, PlacementInfoUtil.getTserversToBeRemoved(nodes).size());
-    assertEquals(3, PlacementInfoUtil.getTserversToProvision(nodes).size());
+    assertEquals(10, PlacementInfoUtil.getTserversToBeRemoved(nodes).size());
+    assertEquals(10, PlacementInfoUtil.getTserversToProvision(nodes).size());
     assertEquals(3, PlacementInfoUtil.getMastersToProvision(nodes).size());
   }
+
+  @Test
+  public void testShrinkPlacement() {
+    UniverseDefinitionTaskParams ud = universe.getUniverseDetails();
+    Universe.saveDetails(univUuid, setAzUUIDs(ud.userIntent));
+    ud.universeUUID = univUuid;
+    ud.userIntent.numNodes = 8;
+    PlacementInfoUtil.updateUniverseDefinition(ud, customer.getCustomerId());
+    Set<NodeDetails> nodes = ud.nodeDetailsSet;
+    assertEquals(0, PlacementInfoUtil.getMastersToBeRemoved(nodes).size());
+    assertEquals(0, PlacementInfoUtil.getMastersToProvision(nodes).size());
+    assertEquals(2, PlacementInfoUtil.getTserversToBeRemoved(nodes).size());
+    assertEquals(0, PlacementInfoUtil.getTserversToProvision(nodes).size());
+    removeNodesAndVerify(nodes);
+
+    ud = universe.getUniverseDetails();
+    ud.userIntent.numNodes = 5;
+    PlacementInfoUtil.updateUniverseDefinition(ud, customer.getCustomerId());
+    nodes = ud.nodeDetailsSet;
+    assertEquals(0, PlacementInfoUtil.getMastersToBeRemoved(nodes).size());
+    assertEquals(0, PlacementInfoUtil.getMastersToProvision(nodes).size());
+    assertEquals(3, PlacementInfoUtil.getTserversToBeRemoved(nodes).size());
+    assertEquals(0, PlacementInfoUtil.getTserversToProvision(nodes).size());
+    removeNodesAndVerify(nodes);
+  }
 }
+
