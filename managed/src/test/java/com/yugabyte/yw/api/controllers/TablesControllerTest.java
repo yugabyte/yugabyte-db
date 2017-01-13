d77848d1e2cbfd59967c3e01e45d6eae913a2d6e
Karthik Ranganathan <rkarthik007@users.noreply.github.com>
2017-01-12 17:41:01 -0800

    ENG-760: Display table names, uuid and types in the UI
    Summary: Display table names, uuid and types in the UI
    
    Test Plan:
    Tested by running yugaware on my mac, creating various tables and making sure they show up.
    Added a unit test.
    
    Reviewers: pritam, bharat, ram
    
    Reviewed By: bharat
    
    Subscribers: eng
    
    Differential Revision: https://phabricator.dev.yugabyte.com/D1068

--- managed/src/test/java/com/yugabyte/yw/api/controllers/TablesControllerTest.java
+++ managed/src/test/java/com/yugabyte/yw/api/controllers/TablesControllerTest.java
@@ -2,26 +2,44 @@
 
 package com.yugabyte.yw.api.controllers;
 
-import com.fasterxml.jackson.databind.JsonNode;
-import com.yugabyte.yw.common.services.YBClientService;
-import com.yugabyte.yw.controllers.TablesController;
-
-import org.junit.*;
-import org.yb.client.ListTablesResponse;
-import org.yb.client.YBClient;
-import play.libs.Json;
-import play.mvc.*;
-
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static org.mockito.Matchers.any;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 import static play.mvc.Http.Status.OK;
-
-import java.lang.String;
-import java.util.Arrays;
-import java.util.List;
-import static org.mockito.Mockito.*;
 import static play.test.Helpers.contentAsString;
 
-public class TablesControllerTest {
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Set;
+import java.util.UUID;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.yb.Common.TableType;
+import org.yb.client.ListTablesResponse;
+import org.yb.client.YBClient;
+import org.yb.client.shaded.com.google.protobuf.ByteString;
+import org.yb.master.Master.ListTablesResponsePB.TableInfo;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.yugabyte.yw.common.FakeDBApplication;
+import com.yugabyte.yw.common.services.YBClientService;
+import com.yugabyte.yw.controllers.TablesController;
+import com.yugabyte.yw.models.Customer;
+import com.yugabyte.yw.models.Universe;
+
+import play.libs.Json;
+import play.mvc.Result;
+
+public class TablesControllerTest extends FakeDBApplication {
+  public static final Logger LOG = LoggerFactory.getLogger(TablesControllerTest.class);
   private YBClientService mockService;
   private TablesController tablesController;
   private YBClient mockClient;
@@ -46,6 +64,55 @@ public class TablesControllerTest {
     JsonNode json = Json.parse(contentAsString(r));
     assertEquals(OK, r.status());
     assertTrue(json.get("table_names").isArray());
+ }
+
+  @Test
+  public void testListTablesFromYbClient() throws Exception {
+    List<TableInfo> tableInfoList = new ArrayList<TableInfo>();
+    Set<String> tableNames = new HashSet<String>();
+    tableNames.add("Table1");
+    tableNames.add("Table2");
+    TableInfo ti1 = TableInfo.newBuilder()
+                             .setName("Table1")
+                             .setId(ByteString.copyFromUtf8(UUID.randomUUID().toString()))
+                             .setTableType(TableType.REDIS_TABLE_TYPE)
+                             .build();
+    TableInfo ti2 = TableInfo.newBuilder()
+                             .setName("Table2")
+                             .setId(ByteString.copyFromUtf8(UUID.randomUUID().toString()))
+                             .setTableType(TableType.YSQL_TABLE_TYPE)
+                             .build();
+    tableInfoList.add(ti1);
+    tableInfoList.add(ti2);
+    when(mockResponse.getTableInfoList()).thenReturn(tableInfoList);
+
+    Customer customer = Customer.create("Valid Customer", "abd@def.ghi", "password");
+    Universe u1 = Universe.create("Universe-1", UUID.randomUUID(), customer.getCustomerId());
+    customer.addUniverseUUID(u1.universeUUID);
+    customer.save();
+    LOG.info("Created customer " + customer.uuid + " with universe " + u1.universeUUID);
+    Result r = tablesController.universeList(customer.uuid, u1.universeUUID);
+    JsonNode json = Json.parse(contentAsString(r));
+    LOG.info("Fetched table list from universe, response: " + contentAsString(r));
+    assertEquals(OK, r.status());
+    assertTrue(json.isArray());
+    Iterator<JsonNode> it = json.elements();
+    int numTables = 0;
+    while (it.hasNext()) {
+      numTables++;
+      JsonNode table = it.next();
+      String tableName = table.get("tableName").asText();
+      String tableType = table.get("tableType").asText();
+      LOG.info("Table name: " + tableName + ", table type: " + tableType);
+      assertTrue(tableNames.contains(tableName));
+      if (tableName.equals("Table1")) {
+        assertEquals(TableType.REDIS_TABLE_TYPE.toString(), tableType);
+      } else if (tableName.equals("Table2")) {
+        assertEquals(TableType.YSQL_TABLE_TYPE.toString(), tableType);
+      }
+    }
+    LOG.info("Processed " + numTables + " tables");
+    assertEquals(numTables, tableNames.size());
  }
 
   @Test
