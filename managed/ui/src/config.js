616f2562c823dcd50dc169043371ad58deb12421
Bharat Baddepudi <bbaddepudi@users.noreply.github.com>
2017-01-13 12:42:41 -0800

    Bump hash for server tarball
    Summary: Last one got a "release" appended. Adding debug version for now...
    
    Test Plan: Create univ.
    
    Reviewers: ram
    
    Reviewed By: ram
    
    Subscribers: eng
    
    Differential Revision: https://phabricator.dev.yugabyte.com/D1073

--- managed/ui/src/config.js
+++ managed/ui/src/config.js
@@ -2,4 +2,4 @@
 
 export const ROOT_URL = location.href.indexOf('localhost') > 0 ? 'http://localhost:9000/api' : '/api';
 
-export const SOFTWARE_PACKAGE = "s3://no-such-url/yugabyte/yb-server-0.0.1-SNAPSHOT.182135266178e6767ad7b8d0617480297ca80130.tar.gz";
+export const SOFTWARE_PACKAGE = "s3://no-such-url/yugabyte/yb-server-0.0.1-SNAPSHOT.1c03117328e18604253bb4fde4e1ada9f1a8b178.tar.gz";
