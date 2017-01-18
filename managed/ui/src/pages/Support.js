fa2d59880d2c788da9f9c465d2f4d771b54f4ec4
Pritam Roy <jordanblink@users.noreply.github.com>
2017-01-17 16:24:23 -0800

    Placeholder Pages for Alerts and Help
    Summary:
    Created Placeholder Components for Alerts and Helps tab so that there is no error on clicking on the items.
    The existing AlertsList component is actually the TasksList component so renamed it to the same.
    
    Test Plan:
    Go to localhost:3000
    Click on all tabs in side menu.
    You should go to the corresponding page/component and no error on any tab click
    
    Reviewers: karthik, shawn, ram
    
    Reviewed By: ram
    
    Subscribers: eng
    
    Differential Revision: https://phabricator.dev.yugabyte.com/D1090

---
+++ managed/ui/src/pages/Support.js
@@ -0,0 +1,14 @@
+// Copyright (c) YugaByte, Inc.
+
+import React, { Component } from 'react';
+import SupportItemContainer from '../containers/support/SupportItemContainer';
+
+export default class Support extends Component {
+  render() {
+    return (
+      <div>
+        <SupportItemContainer/>
+      </div>
+    )
+  }
+}
