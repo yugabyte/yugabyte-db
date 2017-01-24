3ba079b32761de32db8ec0027b48336ce6129aa5
Bogdan Matican <bmatican@users.noreply.github.com>
2017-01-24 14:26:44 -0800

    ENG-788: Adding endpoint for preparing on-prem deployment through JSON input
    Summary:
    We have decided to prepare an endpoint to POST a JSON payload to that would setup the
    on-prem deployment from one big blob.
    
    This will have three sections:
    - identification: to setup the cloud provider itself
    - description: to setup the instance types and region/zone/node data
    - access: to setup the keys and security features
    
    Test Plan:
    Ran a curl POST request with a sample JSON blob:
    `curl -XPOST  --header "X-AUTH-TOKEN: 883cb0dd-9df6-418a-bc9f-657d4598eda0" --header "Content-Type: application/json" -d @/tmp/onprem.json localhost:9000/api/providers/onprem`
    With the file at `/tmp/onprem.json` as https://phabricator.dev.yugabyte.com/P11.
    
    Reviewers: karthik, pritam, ram
    
    Reviewed By: ram
    
    Subscribers: kannan, eng
    
    Differential Revision: https://phabricator.dev.yugabyte.com/D1144

--- managed/src/main/java/com/yugabyte/yw/controllers/CloudProviderController.java
+++ managed/src/main/java/com/yugabyte/yw/controllers/CloudProviderController.java
@@ -1,9 +1,21 @@
 // Copyright (c) Yugabyte, Inc.
 package com.yugabyte.yw.controllers;
 
-import com.fasterxml.jackson.databind.node.ObjectNode;
-import com.google.inject.Inject;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.yugabyte.yw.forms.OnPremFormData;
+
+import com.yugabyte.yw.models.AvailabilityZone;
+import com.yugabyte.yw.models.Region;
+import com.yugabyte.yw.models.InstanceType;
+import com.yugabyte.yw.models.InstanceType.InstanceTypeDetails;
 import com.yugabyte.yw.models.Provider;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+
+import com.google.inject.Inject;
 
 import play.data.Form;
 import play.data.FormFactory;
@@ -13,6 +25,8 @@ import play.mvc.Result;
 import java.util.List;
 
 public class CloudProviderController extends AuthenticatedController {
+  public static final Logger LOG = LoggerFactory.getLogger(CloudProviderController.class);
+
   @Inject
   FormFactory formFactory;
 
@@ -46,5 +60,106 @@ public class CloudProviderController extends AuthenticatedController {
       responseJson.put("error", e.getMessage());
       return internalServerError(responseJson);
     }
+  }
+
+  /**
+   * POST endpoint for setting up an on-prem deployment
+   * @return JSON response of newly created provider
+   */
+  public Result setupOnPrem() {
+    Form<OnPremFormData> formData = formFactory.form(OnPremFormData.class).bindFromRequest();
+    ObjectNode responseJson = Json.newObject();
+
+    if (formData.hasErrors()) {
+      responseJson.set("error", formData.errorsAsJson());
+      return badRequest(responseJson);
+    }
+
+    // Get provider data or create a new provider.
+    OnPremFormData.IdData identification = formData.get().identification;
+    if (identification == null) {
+      responseJson.put("error", "Field <identification> cannot be empty!");
+      return badRequest(responseJson);
+    }
+
+    Provider p = null;
+    if (identification.uuid == null) {
+      if (identification.type == null || identification.name == null) {
+        responseJson.put(
+            "error", "Field <identification> must have either <uuid> or <name> and <type>");
+        return badRequest(responseJson);
+      }
+
+      p = Provider.get(identification.name);
+      if (p == null) {
+        p = Provider.create(identification.type, identification.name);
+        LOG.info("Created provider " + p);
+      }
+    } else {
+      p = Provider.find.byId(identification.uuid);
+    }
+
+    OnPremFormData.CloudData description = formData.get().description;
+    if (description != null) {
+      if (description.instance_types != null) {
+        for (OnPremFormData.InstanceTypeData i : description.instance_types) {
+          InstanceType it = InstanceType.get(p.code, i.code);
+          if (it == null) {
+            // TODO: instance type metadata?
+            it = InstanceType.upsert(
+                p.code,
+                i.code,
+                0, // numCores
+                0.0, // memSizeGB
+                0, // volumeCount
+                0, // volumeSizeGB
+                InstanceType.VolumeType.SSD,
+                new InstanceTypeDetails()
+                );
+          }
+        }
+      }
+      if (description.regions != null) {
+        for (OnPremFormData.RegionData r : description.regions) {
+          Region region = Region.getByCode(r.code);
+          if (region == null) {
+            // TODO: region name vs code?
+            region = Region.create(
+                p,
+                r.code, // code
+                r.code, // name
+                "" // ybImage
+                );
+          }
+          if (r.zones != null) {
+            for (OnPremFormData.ZoneData z : r.zones) {
+              AvailabilityZone zone = AvailabilityZone.getByCode(z.code);
+              if (zone == null) {
+                // TODO: zone name vs node?
+                zone = AvailabilityZone.create(
+                    region,
+                    z.code, // code
+                    z.code, // name
+                    "" // subnet
+                    );
+              }
+              if (z.nodes != null) {
+                for (OnPremFormData.NodeData n : z.nodes) {
+                  // TODO: placeholder for node processing
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+
+    // TODO: placeholder
+    OnPremFormData.AccessData access = formData.get().access;
+    if (access != null) {
+      responseJson.set("access", Json.toJson(access));
+    }
+
+    return ok(responseJson);
   }
 }
