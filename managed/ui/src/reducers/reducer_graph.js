6450d9a962b95ffbf4ec817a4abd91d1641f57aa
Pritam Roy <jordanblink@users.noreply.github.com>
2017-01-27 11:23:24 -0800

    Universe List Display and Universe Read/Write Line-Map Metrics Components
    Summary:
    Created Components for Universe Read/Write Metrics and Universe List page as according to latest mockups.
    Used plotly for maps
    {F2151}
    
    Test Plan:
    Go to universes tab in dashboard.
    The resulting view should be as in screenshot.
    
    Reviewers: shawn, karthik, ram
    
    Reviewed By: ram
    
    Subscribers: eng
    
    Differential Revision: https://phabricator.dev.yugabyte.com/D1087

--- managed/ui/src/reducers/reducer_graph.js
+++ managed/ui/src/reducers/reducer_graph.js
@@ -2,11 +2,11 @@
 
 import { CHANGE_GRAPH_QUERY_PERIOD, RESET_GRAPH_QUERY_PERIOD,
 QUERY_METRICS, QUERY_METRICS_SUCCESS, QUERY_METRICS_FAILURE,
-RESET_METRICS } from '../actions/graph';
+RESET_METRICS, SET_UNIVERSE_LIST_METRICS_FAILURE, SET_UNIVERSE_LIST_METRICS_SUCCESS } from '../actions/graph';
 import { DEFAULT_GRAPH_FILTER } from '../components/metrics'
 
 const INITIAL_STATE = {graphFilter: DEFAULT_GRAPH_FILTER, metrics: {},
-  loading: false, error: null};
+  loading: false, error: null, universeMetricList: []};
 
 export default function(state = INITIAL_STATE, action) {
   switch(action.type) {
@@ -27,6 +27,10 @@ export default function(state = INITIAL_STATE, action) {
         loading: false};
     case RESET_METRICS:
       return { ...state, metrics: [], loading: false, panelType: null};
+    case SET_UNIVERSE_LIST_METRICS_SUCCESS:
+      return {...state, universeMetricList: action.payload.data};
+    case SET_UNIVERSE_LIST_METRICS_FAILURE:
+      return {...state, universeMetricList: []};
     default:
       return state;
   }
