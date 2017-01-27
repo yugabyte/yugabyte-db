95032683ca655bfc9f77f3a2ec0d02519f5e7fcb
Pritam Roy <jordanblink@users.noreply.github.com>
2017-01-27 08:38:10 -0800

    Show Correct Universe Status in Universe Detail Page
    Summary:
    The correct universe status needs to be shown in universe detail page.
    Currently it shows placeholder value(ready) for all status.
    Added checks to show correct status based in updateSucceeded and updateInProgress flags.
    
    {F2175}
    {F2176}
    
    {F2177}
    
    Test Plan: Go to universeDetail page, the correct universe status should be shown .
    
    Reviewers: karthik, ram
    
    Reviewed By: ram
    
    Subscribers: eng
    
    Differential Revision: https://phabricator.dev.yugabyte.com/D1142

--- managed/ui/src/components/universes/UniverseStatus.js
+++ managed/ui/src/components/universes/UniverseStatus.js
@@ -5,18 +5,40 @@ import './stylesheets/UniverseStatus.scss';
 
 export default class UniverseStatus extends Component {
   render() {
-    const { universe, showLabelText } = this.props;
-    var statusText = '';
-    // TODO: Add other statuses here.
-    var statusClassName = 'good';
-    if (showLabelText) {
-      // TODO: Add other statuses here.
-      statusText = 'Ready';
+    const { universe, universe: {universeDetails}, showLabelText } = this.props;
+    var updateInProgress = universeDetails.updateInProgress;
+    var updateSucceeded = universeDetails.updateSucceeded;
+    var statusIcon = <span/>;
+    var statusClassName = "";
+    var statusText = "";
+    if (updateSucceeded) {
+      statusIcon = <i className="fa fa-check-circle" />;
+      statusClassName = 'good';
+      if (showLabelText) {
+        statusText = 'Ready';
+      }
+    } else {
+      if (updateInProgress) {
+        statusIcon = <i className="fa fa-spinner fa-spin" />;
+        statusClassName = 'pending';
+        if (showLabelText) {
+          statusText = 'Pending';
+        }
+      } else {
+        statusIcon  = <i className="fa fa-times-circle" />;
+        statusClassName = 'error';
+        if (showLabelText) {
+          statusText = 'Error';
+        }
+      }
     }
+
+
 
     return (
       <div className={'universe-status ' + statusClassName}>
-        <i className="fa fa-check-circle" />
+
+        {statusIcon}
         {statusText && <span>{statusText}</span>}
       </div>
     );
