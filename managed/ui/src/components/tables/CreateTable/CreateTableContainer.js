6aad4d19b8d55db517d489068b922408c2f510ac
Pritam Roy <jordanblink@users.noreply.github.com>
2017-02-03 17:33:50 -0800

    Create Table Endpoint wiring
    Summary:
    The middleware payload as shown in sample curl request expects a tableUUID, changed that to generate a new tableUUID in middleware itself.
    
    Test Plan:
    Create Table with one row for each partitionKey, clusteringKey and otherColumn and valid entries should return success response.
    
    Reviewers: karthik, jeff, ram
    
    Reviewed By: jeff, ram
    
    Subscribers: eng
    
    Differential Revision: https://phabricator.dev.yugabyte.com/D1207

--- managed/ui/src/components/tables/CreateTable/CreateTableContainer.js
+++ managed/ui/src/components/tables/CreateTable/CreateTableContainer.js
@@ -3,16 +3,52 @@
 import { connect } from 'react-redux';
 import { CreateTable } from '../../tables';
 import { reduxForm } from 'redux-form';
-import { createUniverseTable, createUniverseTableFailure, createUniverseTableSuccess } from '../../../actions/tables'
+import { createUniverseTable, createUniverseTableFailure, createUniverseTableSuccess,
+         fetchColumnTypes, fetchColumnTypesSuccess, fetchColumnTypesFailure } from '../../../actions/tables';
 
 const mapDispatchToProps = (dispatch) => {
   return {
-    submitCreateTable: (universeUUID, values) => {
-      dispatch(createUniverseTable(universeUUID, values)).then((response) => {
+    submitCreateTable: (currentUniverse, values) => {
+      var rowCounter = 1;
+      var partitionKeyList = values.partitionKeyColumns.map(function(row, idx){
+        return {"columnOrder": rowCounter ++, "name": row.name, "type": row.selected,
+                "isPartitionKey": true, "isClusteringKey": false};
+      });
+      var clusteringColumnList = values.clusteringColumns.map(function(row, idx){
+        return {"columnOrder": rowCounter ++, "name": row.name, "type": row.selected,
+          "isPartitionKey": false, "isClusteringKey": true};
+      });
+      var otherColumnList = values.otherColumns.map(function(row, idx){
+        return {"columnOrder": rowCounter ++, "name": row.name, "type": row.selected,
+          "isPartitionKey": false, "isClusteringKey": false};
+      });
+      var tableDetails = partitionKeyList.concat(clusteringColumnList, otherColumnList);
+
+      var payload = {};
+      payload.cloud = currentUniverse.universeDetails.cloud;
+      payload.universeUUID = currentUniverse.universeUUID;
+      payload.tableName = values.tableName;
+      payload.tableType = "YSQL_TABLE_TYPE";
+      payload.tableDetails = {
+        "tableName": values.tableName,
+        "columns": tableDetails
+      };
+      var universeUUID = currentUniverse.universeUUID;
+
+      dispatch(createUniverseTable(universeUUID, payload)).then((response) => {
         if (response.payload.status !== 200) {
           dispatch(createUniverseTableFailure(response.payload));
         } else {
           dispatch(createUniverseTableSuccess(response.payload));
+        }
+      });
+    },
+    fetchTableColumnTypes: () => {
+      dispatch(fetchColumnTypes()).then((response) => {
+        if (response.payload.status !== 200) {
+          dispatch(fetchColumnTypesFailure(response.payload));
+        } else {
+          dispatch(fetchColumnTypesSuccess(response.payload));
         }
       });
     }
