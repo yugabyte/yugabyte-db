e6690ea25873d7d907058c6679aaf78f906d03c8
Pritam Roy <jordanblink@users.noreply.github.com>
2017-02-09 12:11:28 -0800

    Exposed Cassandra Metrics via metrics.yml to UI
    Summary:
    Added CQL type metrics to metrics exposed to UI
    {F2407}
    
    Test Plan:
    Go to localhost:3000
    Make sure node_prefix is set.
    You should be able to see YQL types
    
    Reviewers: karthik, ram, amitanand
    
    Reviewed By: amitanand
    
    Subscribers: eng
    
    Differential Revision: https://phabricator.dev.yugabyte.com/D1249

--- managed/ui/src/components/metrics/CustomerMetricsPanel/CustomerMetricsPanel.js
+++ managed/ui/src/components/metrics/CustomerMetricsPanel/CustomerMetricsPanel.js
@@ -12,10 +12,10 @@ export default class CustomerMetricsPanel extends Component {
     const {origin} = this.props;
     return (
       <GraphPanelHeaderContainer origin={origin}>
-
         <GraphPanelContainer type={"server"} nodePrefixes={this.props.nodePrefixes}/>
         <GraphPanelContainer type={"tserver"} nodePrefixes={this.props.nodePrefixes}/>
         <GraphPanelContainer type={"redis"} nodePrefixes={this.props.nodePrefixes}/>
+        <GraphPanelContainer type={"cql"} nodePrefixes={this.props.nodePrefixes}/>
       </GraphPanelHeaderContainer>
     )
   }
