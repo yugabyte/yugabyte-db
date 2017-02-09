7624b6db38b8b1107d18cfa34f710f502640a615
Pritam Roy <jordanblink@users.noreply.github.com>
2017-02-08 16:05:37 -0800

    ENG-901, InstanceType, Provider and Region List should be in alpahbetical order
    Summary:
    Created util function that sorts array by key.
    Sorting array before it is persisted in reducer
    
    Test Plan:
    Go to create universe form.
    The provider, region and instanceType list should be sorted in alphabetical order
    
    Reviewers: karthik, ram
    
    Reviewed By: ram
    
    Subscribers: eng
    
    Differential Revision: https://phabricator.dev.yugabyte.com/D1253

--- managed/ui/src/reducers/reducer_cloud.js
+++ managed/ui/src/reducers/reducer_cloud.js
@@ -5,6 +5,7 @@ import { GET_REGION_LIST, GET_REGION_LIST_SUCCESS, GET_REGION_LIST_FAILURE,
   GET_INSTANCE_TYPE_LIST, GET_INSTANCE_TYPE_LIST_SUCCESS, GET_INSTANCE_TYPE_LIST_FAILURE,
   RESET_PROVIDER_LIST, GET_SUPPORTED_REGION_DATA, GET_SUPPORTED_REGION_DATA_SUCCESS,
   GET_SUPPORTED_REGION_DATA_FAILURE} from '../actions/cloud';
+import _ from 'lodash';
 
 const INITIAL_STATE = {regions: [], providers: [], instanceTypes: [],
   selectedProvider: null, error: null, supportedRegionList: []};
@@ -15,21 +16,21 @@ export default function(state = INITIAL_STATE, action) {
     case GET_PROVIDER_LIST:
       return {...state, providers: [], status: 'storage', error: null, loading: true};
     case GET_PROVIDER_LIST_SUCCESS:
-      return {...state, providers: action.payload, status: 'provider_fetch_success', error: null, loading:false};
+      return {...state, providers: _.sortBy(action.payload, "name"), status: 'provider_fetch_success', error: null, loading:false};
     case GET_PROVIDER_LIST_FAILURE:
       error = action.payload.data || {message: action.payload.message};//2nd one is network or server down errors
       return { ...state, providers: null, status: 'provider_fetch_failure', error: error, loading: false};
     case GET_REGION_LIST:
       return { ...state, regions: [], status: 'storage', error: null, loading: true};
     case GET_REGION_LIST_SUCCESS:
-      return { ...state, regions: action.payload.data, status: 'region_fetch_success', error: null, loading: false}; //<-- authenticated
+      return { ...state, regions: _.sortBy(action.payload.data, "name"), status: 'region_fetch_success', error: null, loading: false}; //<-- authenticated
     case GET_REGION_LIST_FAILURE:// return error and make loading = false
       error = action.payload.data || {message: action.payload.message};//2nd one is network or server down errors
       return { ...state, regions: null, status: 'region_fetch_failure', error: error, loading: false};
     case GET_INSTANCE_TYPE_LIST:
       return {...state, instanceTypes: [], status: 'storge', error: null, loading: true};
     case GET_INSTANCE_TYPE_LIST_SUCCESS:
-      return {...state, instanceTypes: action.payload.data, status: 'region_fetch_success', error: null, loading: false};
+      return {...state, instanceTypes: _.sortBy(action.payload.data, "instanceTypeCode"), status: 'region_fetch_success', error: null, loading: false};
     case GET_INSTANCE_TYPE_LIST_FAILURE:
       error = action.payload.data || {message: action.payload.message};//2nd one is network or server down errors
       return { ...state, instanceTypes: null, status: 'instance_type_fetch_failure', error: error, loading: false};
