8a262808b22070294f9bd39b3f21d714762e40a5
Pritam Roy <jordanblink@users.noreply.github.com>
2017-02-20 17:10:43 -0800

    Bump up server package
    Summary: Changed server package to latest release version of YB.
    
    Test Plan: go to localhost:3000 CRUD operations should work fine with latest package.
    
    Reviewers: karthik, ram
    
    Reviewed By: ram
    
    Subscribers: bharat, eng
    
    Differential Revision: https://phabricator.dev.yugabyte.com/D1325

--- managed/ui/src/config.js
+++ managed/ui/src/config.js
@@ -2,4 +2,4 @@
 
 export const ROOT_URL = location.href.indexOf('localhost') > 0 ? 'http://localhost:9000/api' : '/api';
 
-export const SOFTWARE_PACKAGE = "s3://no-such-url/yugabyte/yb-server.9be0f62149ae574afa27e1d815635054c27ebf31-debug.tar.gz";
+export const SOFTWARE_PACKAGE = "s3://no-such-url/yugabyte/yb-server.e13e75679c071ca7a8fae1c61519206ee142cd5e-release.tar.gz";
