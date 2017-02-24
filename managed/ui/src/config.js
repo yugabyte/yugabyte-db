9822bac162e8307db3a2366f8c7114c61ff2f454
Ramkumar Vaidyanathan <ramkumarvs@users.noreply.github.com>
2017-02-23 20:58:40 -0800

    Docker updates for it to work in replicated
    Summary: as title
    
    Test Plan: tried to bring up docker locally.
    
    Reviewers: karthik
    
    Reviewed By: karthik
    
    Subscribers: eng
    
    Differential Revision: https://phabricator.dev.yugabyte.com/D1338

--- managed/ui/src/config.js
+++ managed/ui/src/config.js
@@ -1,5 +1,5 @@
 // Copyright (c) YugaByte, Inc.
 
-export const ROOT_URL = location.href.indexOf('localhost') > 0 ? 'http://localhost:9000/api' : '/api';
+export const ROOT_URL = process.env.NODE_ENV === 'development' ? 'http://localhost:9000/api' : '/api';
 
 export const SOFTWARE_PACKAGE = "s3://no-such-url/yugabyte/yb-server.e13e75679c071ca7a8fae1c61519206ee142cd5e-release.tar.gz";
