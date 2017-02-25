f564c7ce49038ae6462063b100239e688e4f6999
Bharat Baddepudi <bbaddepudi@users.noreply.github.com>
2017-02-24 18:05:20 -0800

    Update server package for UI use.
    Summary:
    To capture past 10days of server side changes.
    
    Test Plan:
    Ran run_itest from a yugaware-server and a success email got sent.
    
    Reviewers: karthik, ram, kannan
    
    Reviewed By: kannan
    
    Subscribers: eng
    
    Differential Revision: https://phabricator.dev.yugabyte.com/D1366

--- managed/ui/src/config.js
+++ managed/ui/src/config.js
@@ -2,4 +2,4 @@
 
 export const ROOT_URL = process.env.NODE_ENV === 'development' ? 'http://localhost:9000/api' : '/api';
 
-export const SOFTWARE_PACKAGE = "s3://no-such-url/yugabyte/yb-server.e13e75679c071ca7a8fae1c61519206ee142cd5e-release.tar.gz";
+export const SOFTWARE_PACKAGE = "s3://no-such-url/yugabyte/yb-server.6781ef3c45cde8baa30aa37d759134488b659e59-release.tar.gz";
