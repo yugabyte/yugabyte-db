9632a918d402509d55f043c8e62b7421c2942a64
Shawn Van Ittersum <svicalifornia@users.noreply.github.com>
2017-03-07 14:51:50 -0800

    Restyle Metrics
    Summary:
    - Set y-axis range manually
    - Resize metrics graphs based on container size
    - Set Metrics graph font to Helvetica Neue (and family)
    
    Test Plan: Start UI, go to Metrics page, also go to Universe detail Metrics page
    
    Reviewers: ram, pritam
    
    Reviewed By: pritam
    
    Subscribers: eng
    
    Differential Revision: https://phabricator.dev.yugabyte.com/D1414

--- managed/ui/src/components/metrics/MetricsPanel/MetricsPanel.js
+++ managed/ui/src/components/metrics/MetricsPanel/MetricsPanel.js
@@ -2,15 +2,20 @@
 
 import React, { Component, PropTypes } from 'react';
 var Plotly = require('plotly.js/lib/core');
+
 import { removeNullProperties, isValidObject, isValidArray } from '../../../utils/ObjectUtils';
 import './MetricsPanel.scss';
 
-export default class MetricsPanel extends Component {
+const WIDTH_OFFSET = 5;
+const MAX_GRAPH_WIDTH_PX = 700;
+const GRAPH_GUTTER_WIDTH_PX = 15;
 
+export default class MetricsPanel extends Component {
   static propTypes = {
     metric: PropTypes.object.isRequired,
     metricKey: PropTypes.string.isRequired
   }
+
   componentDidMount() {
     const { metricKey, metric } = this.props;
     if (isValidObject(metric)) {
@@ -18,13 +23,31 @@ export default class MetricsPanel extends Component {
       removeNullProperties(metric.layout);
 
       // TODO: send this data from backend.
+      var max = 0;
+      metric.data.forEach(function (data) {
+        if (data.y) {
+          data.y.forEach(function (y) {
+            y = parseFloat(y) * 1.25;
+            if (y > max) max = y;
+          });
+        }
+      });
+      if (max === 0) max = 1.01;
       metric.layout.autosize = false;
-      metric.layout.width = 400;
+      metric.layout.width = this.getGraphWidth(this.props.containerWidth);
       metric.layout.height = 360;
       metric.layout.showlegend = true;
-      metric.layout.yaxis = {rangemode: "nonnegative"}
-      metric.layout.legend = {xanchor:"center", yanchor:"top",
-                              y:-0.3, x:0.5, orientation: "h"}
+      metric.layout.yaxis = {range: [0, max]};
+      metric.layout.font = {
+        family: 'Helvetica Neue, Helvetica, Roboto, Arial, Droid Sans, sans-serif',
+      };
+      metric.layout.legend = {
+        orientation: "h",
+        xanchor: "center",
+        yanchor: "top",
+        x: 0.5,
+        y: -0.3,
+      };
       metric.layout.margin = {
         l: 45,
         r: 25,
@@ -52,9 +75,27 @@ export default class MetricsPanel extends Component {
     }
   }
 
+  componentWillReceiveProps(newProps) {
+    if (newProps.containerWidth !== this.props.containerWidth) {
+      Plotly.relayout(this.props.metricKey, {width: this.getGraphWidth(newProps.containerWidth)});
+    }
+  }
+
+  getGraphWidth(containerWidth) {
+    var width = containerWidth - WIDTH_OFFSET;
+    var columnCount = Math.ceil(width / MAX_GRAPH_WIDTH_PX);
+    return Math.floor(width / columnCount) - GRAPH_GUTTER_WIDTH_PX;
+  }
+
+  resizeGraph(windowSize) {
+    Plotly.relayout(this.props.metricKey, {width: this.getGraphWidth(windowSize.windowWidth)});
+  }
+
   render() {
     return (
-      <div id={this.props.metricKey} className="metrics-panel" />
+      <div id={this.props.metricKey} className="metrics-panel">
+        <div />
+      </div>
     );
   }
 }
