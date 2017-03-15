f14a92c7d8e000b4052ebd95b2a939f45d529cba
Ramkumar Vaidyanathan <ramkumarvs@users.noreply.github.com>
2017-03-15 16:32:25 -0700

    Network manager changes to take host_vpc_id
    Summary: as title
    
    Test Plan:
    sbt test
    
    ran
    ./run_itest after deleting region and availability zone
    
    Reviewers: karthik, bharat
    
    Reviewed By: bharat
    
    Subscribers: eng
    
    Differential Revision: https://phabricator.dev.yugabyte.com/D1505

--- managed/src/main/java/com/yugabyte/yw/common/NetworkManager.java
+++ managed/src/main/java/com/yugabyte/yw/common/NetworkManager.java
@@ -3,11 +3,15 @@
 package com.yugabyte.yw.common;
 
 import com.fasterxml.jackson.databind.JsonNode;
+import com.google.common.collect.ImmutableList;
+import com.google.inject.Inject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import javax.inject.Singleton;
+import java.util.ArrayList;
 import java.util.Collections;
+import java.util.List;
 import java.util.UUID;
 
 @Singleton
@@ -16,11 +20,20 @@ public class NetworkManager extends DevopsBase {
 
   private static final String YB_CLOUD_COMMAND_TYPE = "network";
 
+  @Inject
+  CloudQueryHelper cloudQueryHelper;
+
   @Override
   protected String getCommandType() { return YB_CLOUD_COMMAND_TYPE; }
 
   public JsonNode bootstrap(UUID regionUUID) {
-    return execCommand(regionUUID, "bootstrap", Collections.emptyList());
+    JsonNode hostInfo = cloudQueryHelper.currentHostInfo(regionUUID, ImmutableList.of("vpc-id"));
+    List<String> commandArgs = new ArrayList();
+    if (hostInfo.has("vpc-id")) {
+      commandArgs.add("--host_vpc_id");
+      commandArgs.add(hostInfo.get("vpc-id").asText());
+    }
+    return execCommand(regionUUID, "bootstrap", commandArgs);
   }
 
   public JsonNode query(UUID regionUUID) {
