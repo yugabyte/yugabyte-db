ff2326127e4e4cdc73f5351303fecc7ae44785e4
Bogdan Matican <bmatican@users.noreply.github.com>
2017-03-16 16:20:45 -0700

    ENG-1058: Moving away from yb-server to yugabyte package prefix
    Summary: Updated package name to start with repo name.
    
    Test Plan: Ran CreateUniverse with devops changes.
    
    Reviewers: bharat, ram
    
    Reviewed By: ram
    
    Subscribers: devops, yugaware
    
    Differential Revision: https://phabricator.dev.yugabyte.com/D1512

--- managed/ui/src/config.js
+++ managed/ui/src/config.js
@@ -2,4 +2,4 @@
 
 export const ROOT_URL = process.env.NODE_ENV === 'development' ? 'http://localhost:9000/api' : '/api';
 
-export const SOFTWARE_PACKAGE = "s3://no-such-url/yugabyte/yb-server.b712a17d78e877fb152748f803d2396d3f031463-release.tar.gz";
+export const SOFTWARE_PACKAGE = "s3://no-such-url/yugabyte/yugabyte.b712a17d78e877fb152748f803d2396d3f031463-release.tar.gz";
