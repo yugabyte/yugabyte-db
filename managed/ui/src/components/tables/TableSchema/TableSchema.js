6638372999f7ed15ae3290c49b84bc7b90ff52fe
Shawn Van Ittersum <svicalifornia@users.noreply.github.com>
2017-03-15 21:24:41 -0700

    Restyle table schema
    Summary: Also fixed Row/Col container of table details
    
    Test Plan:
    1. Start UI
    2. Click any universe with table schema
    3. lick Tables tab
    4. Click any Cassandra table name (with schema)
    5. Click Schema tab
    
    Reviewers: ram, pritam
    
    Reviewed By: pritam
    
    Subscribers: sid, yugaware, ui
    
    Differential Revision: https://phabricator.dev.yugabyte.com/D1460

--- managed/ui/src/components/tables/TableSchema/TableSchema.js
+++ managed/ui/src/components/tables/TableSchema/TableSchema.js
@@ -26,42 +26,37 @@ export default class TableSchema extends Component {
     return (
       <div>
         <Row className={"schema-definition-header"}>
-          <Col lg={3}>
-            Type
-          </Col>
+          <Col lg={3}></Col>
           <Col lg={9}>
             <Row>
-              <Col lg={2}>Column Order</Col>
               <Col lg={4}>Name</Col>
-              <Col lg={2}>Type</Col>
+              <Col lg={4}>Type</Col>
+              <Col lg={3}>Order</Col>
             </Row>
           </Col>
         </Row>
+        <hr />
         <Row>
           <Col lg={3}>
-            <DescriptionItem title="Partition Key Column">
-              <span>(In Order)</span>
-            </DescriptionItem>
+            <h5 className="no-bottom-margin">Partition Key Columns</h5>
           </Col>
           <Col lg={9}>
              <SchemaRowDefinition rows={partitionKeyRows}/>
           </Col>
         </Row>
+        <hr />
         <Row>
           <Col lg={3}>
-            <DescriptionItem title="Clustering Columns">
-              <span>(In Order)</span>
-            </DescriptionItem>
+            <h5 className="no-bottom-margin">Clustering Columns</h5>
           </Col>
           <Col lg={9}>
             <SchemaRowDefinition rows={clusteringKeyRows}/>
           </Col>
         </Row>
+        <hr />
         <Row className="other-column-container">
           <Col lg={3}>
-            <DescriptionItem title="Other Columns">
-              <span/>
-            </DescriptionItem>
+            <h5 className="no-bottom-margin">Other Columns</h5>
           </Col>
           <Col lg={9}>
              <SchemaRowDefinition rows={otherKeyRows}/>
@@ -80,9 +75,9 @@ class SchemaRowDefinition extends Component {
       rowEntries = rows.map(function(item, idx){
         return (
         <Row key={idx}>
-          <Col lg={2}>{item.columnOrder}</Col>
           <Col lg={4}>{item.name}</Col>
-          <Col lg={2}>{item.type}</Col>
+          <Col lg={4}>{item.type}</Col>
+          <Col lg={3}>{item.isClusteringKey ? 'ASC' : ''}</Col>
         </Row>
         )
       });
