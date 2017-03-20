744a9e6dd7bf612b7432d68e0f540bff909ef045
Shawn Van Ittersum <svicalifornia@users.noreply.github.com>
2017-03-20 12:55:37 -0700

    Resize metrics with react-measure
    Test Plan: Start UI, go to Metrics pages (main and universe details)
    
    Reviewers: pritam, ram
    
    Reviewed By: ram
    
    Subscribers: yugaware, ui
    
    Differential Revision: https://phabricator.dev.yugabyte.com/D1528

--- managed/ui/src/components/metrics/CustomerMetricsPanel/CustomerMetricsPanel.js
+++ managed/ui/src/components/metrics/CustomerMetricsPanel/CustomerMetricsPanel.js
@@ -1,23 +1,35 @@
 // Copyright (c) YugaByte, Inc.
 
 import React, { Component } from 'react';
+import Measure from 'react-measure';
 import { GraphPanelHeaderContainer, GraphPanelContainer } from '../../metrics'
 
 export default class CustomerMetricsPanel extends Component {
+  state = {
+    dimensions: {},
+  }
+
   componentWillMount() {
     this.props.fetchUniverseList();
+  }
+
+  onResize(dimensions) {
+    dimensions.width -= 2;
+    this.setState({dimensions});
   }
 
   render() {
     const {origin, nodePrefixes} = this.props;
     return (
-      <GraphPanelHeaderContainer origin={origin}>
-        <GraphPanelContainer type={"cql"} nodePrefixes={nodePrefixes}/>
-        <GraphPanelContainer type={"redis"} nodePrefixes={nodePrefixes}/>
-        <GraphPanelContainer type={"tserver"} nodePrefixes={nodePrefixes}/>
-        <GraphPanelContainer type={"lsmdb"} nodePrefixes={nodePrefixes}/>
-        <GraphPanelContainer type={"server"} nodePrefixes={nodePrefixes}/>
-      </GraphPanelHeaderContainer>
-    )
+      <Measure onMeasure={this.onResize.bind(this)}>
+        <GraphPanelHeaderContainer origin={origin}>
+          <GraphPanelContainer width={this.state.dimensions.width} nodePrefixes={nodePrefixes} type={"cql"} />
+          <GraphPanelContainer width={this.state.dimensions.width} nodePrefixes={nodePrefixes} type={"redis"} />
+          <GraphPanelContainer width={this.state.dimensions.width} nodePrefixes={nodePrefixes} type={"tserver"} />
+          <GraphPanelContainer width={this.state.dimensions.width} nodePrefixes={nodePrefixes} type={"lsmdb"} />
+          <GraphPanelContainer width={this.state.dimensions.width} nodePrefixes={nodePrefixes} type={"server"} />
+        </GraphPanelHeaderContainer>
+      </Measure>
+    );
   }
 }
