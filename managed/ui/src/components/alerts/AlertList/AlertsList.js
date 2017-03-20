94c7b07828f0467b4de3491a68ac2ac0f2e2caae
Shawn Van Ittersum <svicalifornia@users.noreply.github.com>
2017-03-20 12:55:37 -0700

    Add empty table to Alerts page
    Test Plan: Start UI, go to Alerts page
    
    Reviewers: ram, pritam
    
    Reviewed By: pritam
    
    Subscribers: yugaware, ui
    
    Differential Revision: https://phabricator.dev.yugabyte.com/D1530

--- managed/ui/src/components/alerts/AlertList/AlertsList.js
+++ managed/ui/src/components/alerts/AlertList/AlertsList.js
@@ -2,14 +2,28 @@
 
 import React, { Component } from 'react';
 import {Row, Col} from 'react-bootstrap';
+import { BootstrapTable, TableHeaderColumn } from 'react-bootstrap-table';
 
 export default class AlertsList extends Component {
   render() {
+    const tableBodyContainer = {marginBottom: "1%", paddingBottom: "1%"};
     return (
       <div id="page-wrapper">
         <Row className="header-row">
-          <Col lg={10}>
+          <Col lg={12}>
             <h2>Alerts</h2>
+            <BootstrapTable data={[]} bodyStyle={tableBodyContainer} pagination={true}>
+              <TableHeaderColumn dataField="id" isKey={true} hidden={true}/>
+              <TableHeaderColumn dataField="createTime" columnClassName="no-border" className="no-border" dataAlign="left">
+                Time
+              </TableHeaderColumn>
+              <TableHeaderColumn dataField="type" columnClassName="no-border name-column" className="no-border">
+                Type
+              </TableHeaderColumn>
+              <TableHeaderColumn dataField="message" columnClassName="no-border name-column" className="no-border">
+                Message
+              </TableHeaderColumn>
+            </BootstrapTable>
           </Col>
         </Row>
       </div>
