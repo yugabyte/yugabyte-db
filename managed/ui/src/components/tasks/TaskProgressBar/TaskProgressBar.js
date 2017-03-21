cd71c6a9f76b27e790d670fa1f327761fa59a1c2
Shawn Van Ittersum <svicalifornia@users.noreply.github.com>
2017-03-20 21:03:54 -0700

    Restore missing TaskProgressBar and TaskProgressBarWithDetails
    Summary: Also removed unused variables in CreateTable.js
    
    Test Plan: Start UI, verify compilation
    
    Reviewers: pritam, ram
    
    Reviewed By: ram
    
    Subscribers: yugaware, ui
    
    Differential Revision: https://phabricator.dev.yugabyte.com/D1537

---
+++ managed/ui/src/components/tasks/TaskProgressBar/TaskProgressBar.js
@@ -0,0 +1,28 @@
+// Copyright (c) YugaByte, Inc.
+
+import React, { Component, PropTypes } from 'react';
+import {  ProgressBar } from 'react-bootstrap';
+
+export default class TaskProgressBar extends Component {
+  static propTypes = {
+    progressData: PropTypes.object.isRequired
+  }
+
+  getStyleByStatus(status) {
+    if ( status === "Failure" ) {
+      return "danger"
+    } else if ( status === "Success" ) {
+      return "success"
+    } else if ( status === "Running" ) {
+      return "info"
+    }
+    return null
+  }
+
+  render() {
+    const { progressData: { status, percent } } = this.props;
+    return (
+       <ProgressBar now={percent} label={`${percent}%`} bsStyle={this.getStyleByStatus(status)} />
+    );
+  }
+}
