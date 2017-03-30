a1b9a9faade92a7f88546430bb8484180a0232ff
Bharat Baddepudi <bbaddepudi@users.noreply.github.com>
2017-03-29 22:00:41 -0700

    ENG-1169: Add wait for tservers to heartbeat to master leader during create universe.
    Summary:
    As this wait is missing, it was seen once that the redis table creation did not find enough tservers.
    
    Test Plan:
    `sbt test` locally. Ran create univ from UI using release 0.0.8
    
    ```
    2017-03-29 21:55:24.654 INFO  TaskList.java:110 [TaskPool-0] Running task list WaitForTServerHeartBeats.
    2017-03-29 21:55:24.657 INFO  WaitForTServerHeartBeats.java:60 [TaskPool-CreateUniverse(1c06ca3a-d927-49a2-8468-76930d6f9cb5)-7] Running WaitForTServerHeartBeats(1c06ca3a-d927-49a2-8468-76930d6f9cb5): hostPorts=10.129.23.134:7100,10.129.33.29:7100,10.129.56.117:7100, numTservers=3.
    2017-03-29 21:55:24.822 INFO  WaitForTServerHeartBeats.java:67 [TaskPool-CreateUniverse(1c06ca3a-d927-49a2-8468-76930d6f9cb5)-7] Leader knows 3 tservers so far.
    ```
    
    Reviewers: kannan, karthik
    
    Reviewed By: karthik
    
    Subscribers: yugaware
    
    Differential Revision: https://phabricator.dev.yugabyte.com/D1644

---
+++ managed/src/main/java/com/yugabyte/yw/commissioner/tasks/subtasks/WaitForTServerHeartBeats.java
@@ -0,0 +1,85 @@
+// Copyright (c) YugaByte, Inc.
+
+package com.yugabyte.yw.commissioner.tasks.subtasks;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.yb.client.YBClient;
+
+import com.yugabyte.yw.commissioner.AbstractTaskBase;
+import com.yugabyte.yw.common.services.YBClientService;
+import com.yugabyte.yw.forms.ITaskParams;
+import com.yugabyte.yw.forms.UniverseTaskParams;
+import com.yugabyte.yw.models.Universe;
+
+import org.yb.client.ListTabletServersResponse;
+
+import play.api.Play;
+
+// Helper class to wait for a minimum number of tservers to heartbeat to the
+// master leader. Currently the minimum is the same as the replication factor,
+// so that next set of tasks like creating a table will not lack tserver resources.
+public class WaitForTServerHeartBeats extends AbstractTaskBase {
+  public static final Logger LOG = LoggerFactory.getLogger(WaitForTServerHeartBeats.class);
+
+  // The YB client to use.
+  private YBClientService ybService = null;
+
+  // Timeout when minimum number of tservers have not heartbeatean to master leader.
+  private static final long TIMEOUT_SERVER_WAIT_MS = 60000;
+
+  // Time to wait (in millisec) during each iteration of check.
+  private static final int WAIT_EACH_ATTEMPT_MS = 250;
+
+  // Parameters for tserver heartbeat wait task.
+  public static class Params extends UniverseTaskParams { }
+
+  @Override
+  protected Params taskParams() {
+    return (Params)taskParams;
+  }
+
+  @Override
+  public void initialize(ITaskParams params) {
+    this.taskParams = params;
+    ybService = Play.current().injector().instanceOf(YBClientService.class);
+  }
+
+  @Override
+  public String getName() {
+    return super.getName() + "(" + taskParams().universeUUID + ")";
+  }
+
+  @Override
+  public void run() {
+    Universe universe = Universe.get(taskParams().universeUUID);
+    String hostPorts = universe.getMasterAddresses();
+    int numTservers = universe.getTServers().size();
+    YBClient client = ybService.getClient(hostPorts);
+
+    LOG.info("Running {}: hostPorts={}, numTservers={}.", getName(), hostPorts, numTservers);
+    int numTries = 1;
+    long start = System.currentTimeMillis();
+    do {
+      try {
+        int currentNumTservers = client.listTabletServers().getTabletServersCount();
+
+        LOG.info("{} tservers heartbeating to master leader.", currentNumTservers);
+
+        if (currentNumTservers >= numTservers) {
+          return;
+        }
+
+        LOG.info("Waiting to make sure {} tservers are heartbeating to master leader; retrying " +
+                 "after {}ms. Tried {} times.", numTservers, WAIT_EACH_ATTEMPT_MS, numTries);
+
+        Thread.sleep(WAIT_EACH_ATTEMPT_MS);
+        numTries++;
+      } catch (Exception e) {
+        LOG.warn("{}: ignoring error '{}'.", getName(), e.getMessage());
+      }
+    } while (System.currentTimeMillis() < start + TIMEOUT_SERVER_WAIT_MS);
+
+    throw new RuntimeException(getName() + " timed out.");
+  }
+}
