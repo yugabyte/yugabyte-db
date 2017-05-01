3ae14182b21b0da2b7651a21ae82893731f4a1ec
Ramkumar Vaidyanathan <ramkumarvs@users.noreply.github.com>
2017-05-01 12:28:38 -0700

    UI Updates to create docker configuration
    Summary:
    - Refactor some parts of the config page to support multiple configurations
    - Update docker tab to be able to create and delete configuration
    - Update AWS tab to make sure it can create and delete configuration
    - Add Unmount tab on exit to the tab content is removed when switching tabs.
    
    Test Plan: went to UI and validated.
    
    Reviewers: jeff, karthik, pritam
    
    Reviewed By: pritam
    
    Subscribers: jenkins-bot, yugaware
    
    Differential Revision: https://phabricator.dev.yugabyte.com/D1922

--- managed/ui/src/components/config/PublicCloud/DockerProviderConfiguration.js
+++ managed/ui/src/components/config/PublicCloud/DockerProviderConfiguration.js
@@ -1,40 +1,105 @@
 // Copyright (c) YugaByte, Inc.
 
 import React, {Component} from 'react';
-import { ListGroup, ListGroupItem, Well } from 'react-bootstrap';
+import { Row, Col, Alert } from 'react-bootstrap';
 import { YBButton } from '../../common/forms/fields';
 import {withRouter} from 'react-router';
+import  { isValidArray, isEmptyObject, isValidObject } from '../../../utils/ObjectUtils';
+import { YBConfirmModal } from '../../modals';
+import { RegionMap } from '../../maps';
+
+const PROVIDER_TYPE = "docker";
 
 class DockerProviderConfiguration extends Component {
+  constructor(props) {
+    super(props);
+    this.createProviderConfig = this.createProviderConfig.bind(this);
+    this.deleteProviderConfig = this.deleteProviderConfig.bind(this);
+  }
+
+  createProviderConfig() {
+    this.props.createProvider();
+  }
+
+  deleteProviderConfig() {
+    const { configuredProviders } = this.props;
+    let dockerProvider = configuredProviders.find((provider) => provider.code === PROVIDER_TYPE)
+    this.props.deleteProviderConfig(dockerProvider.uuid);
+  }
+
+  componentWillReceiveProps(nextProps) {
+    if ( this.props.dockerBootstrap !== nextProps.dockerBootstrap ||
+         this.props.cloudBootstrap !== nextProps.cloudBootstrap) {
+        const { dockerBootstrap, cloudBootstrap } = nextProps;
+        if ((!isEmptyObject(dockerBootstrap) && !dockerBootstrap.loading) ||
+            (!isEmptyObject(cloudBootstrap) && !cloudBootstrap.loading && cloudBootstrap.type === "cleanup")) {
+          this.props.reloadCloudMetadata();
+        }
+    }
+  }
+
   render() {
+    const { handleSubmit, submitting, dockerBootstrap: { loading, error },
+            configuredProviders, configuredRegions, universeList } = this.props;
+    let dockerProvider = configuredProviders.find((provider) => provider.code === PROVIDER_TYPE)
+    let dockerRegions = configuredRegions.filter(
+      (configuredRegion) => configuredRegion.provider.code === PROVIDER_TYPE
+    );
+
+    if (isValidObject(dockerProvider)) {
+      let universeExistsForProvider = false;
+      if (isValidArray(configuredProviders) && isValidArray(universeList)){
+        universeExistsForProvider = universeList.some(universe => universe.provider && (universe.provider.uuid === dockerProvider.uuid));
+      }
+      let deleteButtonDisabled = submitting || universeExistsForProvider;
+      let deleteButtonClassName = "btn btn-default delete-aws-btn";
+      let deleteButtonTitle = "Delete this configuration.";
+      if (deleteButtonDisabled) {
+        deleteButtonTitle = "Delete all Docker based universes before deleting the configuration.";
+      } else {
+        deleteButtonClassName += " delete-btn";
+      }
+
+      return (
+        <div className="provider-config-container">
+          <Row className="config-section-header">
+            <Col md={12}>
+              <h4>Docker Configuration</h4>
+              <span className="pull-right" title={deleteButtonTitle}>
+                <YBButton btnText="Delete Configuration" disabled={deleteButtonDisabled}
+                          btnClass={deleteButtonClassName} onClick={this.props.showDeleteProviderModal}/>
+                <YBConfirmModal name="delete-docker-provider" title={"Confirm Delete"}
+                                onConfirm={handleSubmit(this.deleteProviderConfig)}
+                                currentModal="deleteDockerProvider" visibleModal={this.props.visibleModal}
+                                hideConfirmModal={this.props.hideDeleteProviderModal}>
+                  Are you sure you want to delete this Docker configuration?
+                </YBConfirmModal>
+              </span>
+            </Col>
+          </Row>
+          <RegionMap title="All Supported Regions" regions={dockerRegions} type="Root" showLabels={true}/>
+        </div>
+      );
+    }
+
     return (
       <div className="provider-config-container">
-        <ListGroup>
-          <ListGroupItem>
-            Ensure that
-            <span className="heading-text"><a href="https://docs.docker.com/" target="_blank"> Docker Platform </a></span>
-            is running on your local machine.
-          </ListGroupItem>
-          <ListGroupItem>
-            Create a universe with three nodes, each in its own container.
-            <YBButton btnText="Create Initial Docker Universe" btnClass="btn btn-default bg-orange"/>
-          </ListGroupItem>
-          <ListGroupItem>
-            Verify universe creation by checking for the following logs:
-            <Well>
-              2016-12-09 14:27:49, 410 INFO: Running ansible command "ansible-playbook"
-              "/Users/shawn/code/devops/configure-cluster-server.yml" "vault-password-file"
-              "/Users/shawn/.yugabyte/vault_password" "-i" Lorem ipsum dolor sit amet, consectetur
-              adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-              ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-              consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-              fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui
-              officia deserunt mollit anim id est laborum Lorem ipsum dolor sit amet, consectetur
-              adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-              2016-12-09 14:28:23, 410 Universe fake-universe ready.
-            </Well>
-          </ListGroupItem>
-        </ListGroup>
+        <form name="dockerConfigForm" onSubmit={handleSubmit(this.createProviderConfig)}>
+          <Row className="config-section-header">
+            <Col lg={12}>
+              <h4>Docker Configuration</h4>
+              { error && <Alert bsStyle="danger">{error}</Alert> }
+              <div className="docker-config-form form-right-aligned-labels">
+                Setup<span className="heading-text"><a href="https://docs.docker.com/" target="_blank">Docker Platform </a></span>
+              in order to create yugabyte clusters as containers running on your host machine.
+              </div>
+            </Col>
+          </Row>
+          <div className="form-action-button-container">
+            <YBButton btnText={"Setup"} btnClass={"btn btn-default save-btn"}
+                      disabled={submitting || loading } btnType="submit"/>
+          </div>
+        </form>
       </div>
     )
   }
--- managed/ui/src/reducers/common.js
+++ managed/ui/src/reducers/common.js
@@ -1,16 +1,32 @@
 // Copyright (c) YugaByte, Inc.
+import _ from 'lodash';
+import { isDefinedNotNull } from '../utils/ObjectUtils';
 
-export function setSuccessState(currentState, object, data) {
-  currentState[object] = { data: data, loading: false, error: null };
-  return currentState;
+export function setSuccessState(state, object, data) {
+  return Object.assign({}, state, {
+    [object]: { data: data, loading: false, error: null }
+  });
 }
 
-export function setLoadingState(currentState, object, data = null) {
-  currentState[object] = { data: data, loading: true, error: null };
-  return currentState;
+export function setLoadingState(state, object, data = null) {
+  return Object.assign({}, state, {
+   [object]: { data: data, error: null, loading: true }
+  });
 }
 
-export function setFailureState(currentState, object, error, data = null) {
-  currentState[object]={ data: data, loading: false, error: error };
-  return currentState;
+export function setFailureState(state, object, error, data = null) {
+  return Object.assign({}, state, {
+    [object]: { data: data, loading: false, error: error }
+  });
+}
+
+export function setResponseState(state, object, response) {
+  const { payload: { data, status }} = response;
+  let objectState = _.omit(response, ['payload', 'type']);
+  if (status !== 200 || isDefinedNotNull(data.error)) {
+    _.merge(objectState, { data: null, error: data.error, loading: false });
+  } else {
+    _.merge(objectState, { data: data, error: null, loading: false });
+  }
+  return Object.assign({}, state, { [object]: objectState });
 }
