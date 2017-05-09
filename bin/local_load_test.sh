63bcfe94917c96527ac4191ae8dd74973529287b
Hector Cuellar <hectorgcr@users.noreply.github.com>
2017-05-08 19:08:36 -0700

    Use loopback ip aliases in bin/local_load_test.sh
    Summary: This diff completes https://phabricator.dev.yugabyte.com/D1898
    
    Test Plan: Ran the test and verified that it succeeded
    
    Reviewers: robert, bogdan, pritam.damania, bharat
    
    Reviewed By: pritam.damania, bharat
    
    Subscribers: bharat, ybase
    
    Differential Revision: https://phabricator.dev.yugabyte.com/D1992

--- bin/local_load_test.sh
+++ bin/local_load_test.sh
@@ -5,4 +5,4 @@ set -euo pipefail
 yugabyte_root=$( cd "$( dirname "$0" )"/.. && pwd )
 cd "$yugabyte_root"
 
-$yugabyte_root/bin/generic_load_test.sh localhost:7101,localhost:7102,localhost:7103 $@
+$yugabyte_root/bin/generic_load_test.sh 127.0.0.1:7100,127.0.0.2:7100,127.0.0.3:7100 $@
