546af78c055ed08ff80a53aac5edff8c83a8cf01
Pritam Damania <pritamdamania87@users.noreply.github.com>
2017-05-10 17:26:15 -0700

    [ENG-1232] Increase timeout for flaky TabletServerTest.
    Summary:
    It looks like this test was failing when the disks on jenkins slave machine was really
    slow. See ENG-1232 for more details.
    
    Test Plan: jenkins
    
    Reviewers: mikhail, hector, bharat
    
    Reviewed By: bharat
    
    Subscribers: jenkins-bot, ybase
    
    Differential Revision: https://phabricator.dev.yugabyte.com/D2002

--- src/yb/tserver/tablet_server-test-base.h
+++ src/yb/tserver/tablet_server-test-base.h
@@ -131,7 +131,8 @@ class TabletServerTestBase : public YBTest {
   CHECKED_STATUS WaitForTabletRunning(const char *tablet_id) {
     scoped_refptr<tablet::TabletPeer> tablet_peer;
     RETURN_NOT_OK(mini_server_->server()->tablet_manager()->GetTabletPeer(tablet_id, &tablet_peer));
-    return tablet_peer->WaitUntilConsensusRunning(MonoDelta::FromSeconds(10));
+    // Sometimes the disk can be really slow and hence we need a high timeout to wait for consensus.
+    return tablet_peer->WaitUntilConsensusRunning(MonoDelta::FromSeconds(60));
   }
 
   void UpdateTestRowRemote(int tid,
