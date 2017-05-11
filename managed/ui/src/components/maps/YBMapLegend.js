4207fc7af7a143f24c13977580ec427cf8eb5e3f
Shawn Van Ittersum <svicalifornia@users.noreply.github.com>
2017-05-11 10:20:27 -0700

    Clean up CSS for maps and map legends
    Test Plan: Start UI, verify appearance of maps of dashboard, universe details, AWS config
    
    Reviewers: ram, pritam
    
    Reviewed By: pritam
    
    Subscribers: yugaware, ui, jenkins-bot
    
    Differential Revision: https://phabricator.dev.yugabyte.com/D2028

--- managed/ui/src/components/maps/YBMapLegend.js
+++ managed/ui/src/components/maps/YBMapLegend.js
@@ -10,7 +10,7 @@ export default class YBMapLegend extends Component {
     var asyncRegions = [{"name": "No async replicas added."}];
     var cacheRegions = [{"name": "No caches added."}];
     return (
-      <div className="map-legend-container">
+      <div className="yb-map-legend">
         {this.props.title && <h4>{this.props.title}</h4>}
         <YBMapLegendItem regions={rootRegions} title={"Primary Data"} type="Root"/>
         <YBMapLegendItem regions={asyncRegions} title={"Async Replica"} type="Async"/>
