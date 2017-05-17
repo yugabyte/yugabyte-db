5b0b639af189710a67c672c64f435e851c96d927
Bharat Baddepudi <bbaddepudi@users.noreply.github.com>
2017-05-16 18:25:30 -0700

    Fix YugaWare build with latest YB client java jar.
    Summary:
    There were recent changes on YB repo - MiniCluster can be removed and shaded imports not needed. Added google proto to sbt.
    
    Test Plan:
    - From latest yugabyte, did 'mvn clean install -DskipTests'.
    - Used local maven resolver.
    - Made sure sbt test compile fine now and also runs all tests.
    
    TODO: Need to make itest YW to use locally built java jar.
    
    Reviewers: karthik, ram
    
    Reviewed By: ram
    
    Subscribers: jenkins-bot, yugaware
    
    Differential Revision: https://phabricator.dev.yugabyte.com/D2079

--- managed/src/main/java/com/yugabyte/yw/common/services/YBMiniClusterService.java
+++
@@ -1,9 +0,0 @@
-// Copyright (c) YugaByte, Inc.
-
-package com.yugabyte.yw.common.services;
-
-import org.yb.client.MiniYBCluster;
-
-public interface YBMiniClusterService {
-  MiniYBCluster getMiniCluster(int numMasters);
-}
