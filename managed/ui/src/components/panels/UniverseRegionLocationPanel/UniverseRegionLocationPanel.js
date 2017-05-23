33c7e752cd45f1b83528f4c266d7a3e452fa3dfa
Pritam Roy <jordanblink@users.noreply.github.com>
2017-05-23 13:43:20 -0700

    Show Region Map Legend in Home Display, Make HomePage map selectors functional
    Summary:
    The Map legend in Home Page map is broken due to recent changes.
    Fixed the home page map legend display.
    Added selector functionality to Map Legend Checkboxes
    https://drive.google.com/a/yugabyte.com/file/d/0B2MFlVNRpzl3THROVjRzUGxfU28/view?usp=sharing
    
    Test Plan:
    Go to Home Page Map display.
    Try to toggle the checkboxes on/off.
    The map legend display should update accordingly.
    
    Reviewers: jeff, ram
    
    Reviewed By: ram
    
    Subscribers: jenkins-bot, sid, shawn, yugaware
    
    Differential Revision: https://phabricator.dev.yugabyte.com/D2077

--- managed/ui/src/components/panels/UniverseRegionLocationPanel/UniverseRegionLocationPanel.js
+++ managed/ui/src/components/panels/UniverseRegionLocationPanel/UniverseRegionLocationPanel.js
@@ -3,14 +3,35 @@
 import React, { Component } from 'react';
 import { RegionMap } from '../../maps';
 import { RegionMapLegend } from '../../maps';
-import { isNonEmptyArray, isValidObject } from 'utils/ObjectUtils';
+import { isNonEmptyArray, isValidObject, isEmptyArray } from 'utils/ObjectUtils';
+import {getPromiseState} from 'utils/PromiseUtils';
 
 export default class UniverseRegionLocationPanel extends Component {
+  constructor(props) {
+    super(props);
+    this.onProviderSelect = this.onProviderSelect.bind(this);
+    this.state = {selectedProviders: []};
+  }
+
+  onProviderSelect(selectedProviders) {
+    this.setState({selectedProviders: selectedProviders.map(provider => provider.code)});
+  }
+
+  componentWillReceiveProps(nextProps) {
+    if (isNonEmptyArray(nextProps.cloud.providers.data) && (nextProps.cloud.providers !== this.props.cloud.providers
+        || isEmptyArray(this.state.selectedProviders))) {
+      this.state = {selectedProviders: nextProps.cloud.providers.data.map(provider => provider.code)};
+    }
+  }
 
   render() {
-    const { cloud, universe: {universeList} } = this.props;
+    const { cloud, universe: {universeList}, cloud: {providers}} = this.props;
+    var self = this;
+    if (getPromiseState(providers).isEmpty()) {
+      return <span/>;
+    }
 
-    var completeRegionList = cloud.supportedRegionList.data;
+    var completeRegionList = cloud.supportedRegionList.data.filter((region) => self.state.selectedProviders.includes(region.provider.code));
     var universeListByRegions = {};
 
     universeList.data.forEach(function(universeItem){
@@ -34,10 +55,14 @@ export default class UniverseRegionLocationPanel extends Component {
         }
       });
     });
+    var completeProviderList = cloud.providers.data.map( (provider) => {
+      return {code: provider.code, name: provider.name};
+    });
+
     return (
       <div>
         <RegionMap title="All Supported Regions" regions={completeRegionList} type="all"/>
-        <RegionMapLegend providers={cloud.providers}/>
+        { isNonEmptyArray(completeProviderList) && <RegionMapLegend providers={completeProviderList} onProviderSelect={this.onProviderSelect}/>}
       </div>
     )
   }
