6d1573bbd16c5f244cc3164fc0ff327a507b5418
Ram Sri <ramkumarvs@users.noreply.github.com>
2017-06-01 16:35:30 -0700

    Update the docker run file to take program name
    Summary: Make the docker run command take the program name
    
    Test Plan: docker run ybadmin/sample-apps StockTicker.exe --nodes 172.17.0.2:9042 --command create-table
    
    Reviewers: karthik
    
    Reviewed By: karthik
    
    Differential Revision: https://phabricator.dev.yugabyte.com/D2188

--- java/yb-loadtester/src/main/csharp/StockTicker/run.sh
+++ java/yb-loadtester/src/main/csharp/StockTicker/run.sh
@@ -1,4 +1,14 @@
 #!/bin/sh
-mono code/StockTicker.exe $@ &
+if [ $# -eq 0 ]
+then
+  echo "App not provided. Valid Apps:"
+  cd code
+  for app in *.exe
+  do
+    echo "$app"
+  done
+  exit
+fi
+mono code/$@ &
 PID=$!
 wait $PID
