yugabyte/yugabyte-db

commit 6dda0f716bc65e70c2ce899d20cfe0ad275b176c

ENG-1637: Fix dangling prepare-statement LRU position
Summary:
- Fix dangling prepare-statement LRU position. The dangling LRU iterator position may cause illegal memory access when attempting to delete the statement from the LRU list again.
- Also remove a redundant shared lock when returning prepare-result in Statement::Prepare().

Test Plan: Ran CassandraTimeseries workload.

Reviewers: bogdan

Reviewed By: bogdan

Subscribers: yql

Differential Revision: https://phabricator.dev.yugabyte.com/D2252

src/yb/cqlserver/cql_service.cc
@@ -133,7 +133,8 @@ shared_ptr<CQLStatement> CQLServiceImpl::AllocatePreparedStatement(
     // statement to contend on. The statement will then be prepared by one client while the rest
     // wait for the results.
     stmt = prepared_stmts_map_.emplace(
-        query_id, std::make_shared<CQLStatement>(keyspace, sql_stmt)).first->second;
+        query_id, std::make_shared<CQLStatement>(
+            keyspace, sql_stmt, prepared_stmts_list_.end())).first->second;
     InsertLruPreparedStatementUnlocked(stmt);
   } else {
     // Return existing statement if found.
@@ -167,7 +168,10 @@ void CQLServiceImpl::DeletePreparedStatement(const shared_ptr<CQLStatement>& stm
   std::lock_guard<std::mutex> guard(prepared_stmts_mutex_);
 
   prepared_stmts_map_.erase(stmt->query_id());
-  prepared_stmts_list_.erase(stmt->pos());
+  if (stmt->pos() != prepared_stmts_list_.end()) {
+    prepared_stmts_list_.erase(stmt->pos());
+    stmt->set_pos(prepared_stmts_list_.end());
+  }
 
   VLOG(1) << "DeletePreparedStatement: CQL prepared statement cache count = "
           << prepared_stmts_map_.size() << "/" << prepared_stmts_list_.size()
@@ -190,8 +194,11 @@ void CQLServiceImpl::DeleteLruPreparedStatement() {
   std::lock_guard<std::mutex> guard(prepared_stmts_mutex_);
 
   if (!prepared_stmts_list_.empty()) {
-    prepared_stmts_map_.erase(prepared_stmts_list_.back()->query_id());
-    prepared_stmts_list_.pop_back();
+    const shared_ptr<CQLStatement>& stmt = prepared_stmts_list_.back();
+    prepared_stmts_map_.erase(stmt->query_id());
+    DCHECK(std::next(stmt->pos()) == prepared_stmts_list_.end()) << "invalid end-stmt pos";
+    prepared_stmts_list_.erase(stmt->pos());
+    stmt->set_pos(prepared_stmts_list_.end());
   }
 
   VLOG(1) << "DeleteLruPreparedStatement: CQL prepared statement cache count = "

src/yb/cqlserver/cql_statement.cc
@@ -11,8 +11,9 @@ namespace yb {
 namespace cqlserver {
 
 //------------------------------------------------------------------------------------------------
-CQLStatement::CQLStatement(const string& keyspace, const string& sql_stmt)
-    : Statement(keyspace, sql_stmt) {
+CQLStatement::CQLStatement(
+    const string& keyspace, const string& sql_stmt, const CQLStatementListPos pos)
+    : Statement(keyspace, sql_stmt), pos_(pos) {
 }
 
 CQLStatement::~CQLStatement() {

src/yb/cqlserver/cql_statement.h
@@ -30,7 +30,7 @@ using CQLStatementListPos = CQLStatementList::iterator;
 // A CQL statement that is prepared and cached.
 class CQLStatement : public sql::Statement {
  public:
-  CQLStatement(const std::string& keyspace, const std::string& sql_stmt);
+  CQLStatement(const std::string& keyspace, const std::string& sql_stmt, CQLStatementListPos pos);
   ~CQLStatement();
 
   // Return the query id.

src/yb/sql/statement.cc
@@ -40,26 +40,24 @@ CHECKED_STATUS Statement::Prepare(
     }
   }
 
-  // Return prepared result if requested and the statement is a SELECT statement. Do so within a
-  // shared lock.
-  {
-    boost::shared_lock<boost::shared_mutex> l(lock_);
-    if (result != nullptr) {
-      const TreeNode *root = parse_tree_->root().get();
-      if (root->opcode() != TreeNodeOpcode::kPTListNode) {
-        return STATUS(Corruption, "Internal error: statement list expected");
-      }
-      const PTListNode *stmts = static_cast<const PTListNode*>(root);
-      if (stmts->size() != 1) {
-        return STATUS(Corruption, "Internal error: only one statement expected");
-      }
-      const TreeNode *stmt = stmts->element(0).get();
-      if (stmt->opcode() == TreeNodeOpcode::kPTSelectStmt ||
-          stmt->opcode() == TreeNodeOpcode::kPTInsertStmt ||
-          stmt->opcode() == TreeNodeOpcode::kPTUpdateStmt ||
-          stmt->opcode() == TreeNodeOpcode::kPTDeleteStmt) {
-        result->reset(new PreparedResult(static_cast<const PTDmlStmt*>(stmt)));
-      }
+  // Return prepared result if requested and the statement is a SELECT statement. Do not need a
+  // lock here because we have verified that the parse tree is either present already or we have
+  // successfully prepared the statement above. The parse tree is guaranteed read-only afterwards.
+  if (result != nullptr) {
+    const TreeNode *root = parse_tree_->root().get();
+    if (root->opcode() != TreeNodeOpcode::kPTListNode) {
+      return STATUS(Corruption, "Internal error: statement list expected");
+    }
+    const PTListNode *stmts = static_cast<const PTListNode*>(root);
+    if (stmts->size() != 1) {
+      return STATUS(Corruption, "Internal error: only one statement expected");
+    }
+    const TreeNode *stmt = stmts->element(0).get();
+    if (stmt->opcode() == TreeNodeOpcode::kPTSelectStmt ||
+        stmt->opcode() == TreeNodeOpcode::kPTInsertStmt ||
+        stmt->opcode() == TreeNodeOpcode::kPTUpdateStmt ||
+        stmt->opcode() == TreeNodeOpcode::kPTDeleteStmt) {
+      result->reset(new PreparedResult(static_cast<const PTDmlStmt*>(stmt)));
     }
   }
 
@@ -70,7 +68,8 @@ bool Statement::ExecuteAsync(
     SqlProcessor* processor, const StatementParameters& params, StatementExecutedCallback cb) {
   {
     // Ensure the statement has been prepared successfully and parse tree is present. Do so within
-    // a shared lock.
+    // a shared lock. Once verified, we do not need the lock when executing the statement below
+    // because the parse-tree is guaranteed read-only after having been prepared successfully.
     boost::shared_lock<boost::shared_mutex> l(lock_);
 
     // Return false if there is no parse tree.

commit b21c8a285fe31fedd842b228c0252dc38bc16346

Fix heap-use-after-free in CQLServiceImpl::DeletePreparedStatementUnlocked().
Summary:
This issue is introduced by D2252 that added the function CQLServiceImpl::DeletePreparedStatementUnlocked(). The heap-use-after-free occurs because the function takes a "const shared_ptr&" of the CQL statement to delete from cache hash-map and LRU list. In the case when the shared_ptr is obtained from the LRU list from CQLServiceImpl::DeleteLruPreparedStatement(), the prepared_stmts_list_.erase() in DeletePreparedStatementUnlocked() will delete the very shared_ptr it is holding, causing the next "stmt->set_pos()" to refer to the deleted shared_ptr thus the issue.

To fix this issue, the "stmt" parameter is now a "const shared_ptr" and not a reference so that we hold a separate copy of the shared_ptr instead.

Test Plan: CQL ASAN Java tests.

Reviewers: mihnea

Reviewed By: mihnea

Subscribers: yql

Differential Revision: https://phabricator.dev.yugabyte.com/D2292

src/yb/cqlserver/cql_service.cc
@@ -184,9 +184,11 @@ void CQLServiceImpl::MoveLruPreparedStatementUnlocked(const shared_ptr<CQLStatem
   prepared_stmts_list_.splice(prepared_stmts_list_.begin(), prepared_stmts_list_, stmt->pos());
 }
 
-void CQLServiceImpl::DeletePreparedStatementUnlocked(const std::shared_ptr<CQLStatement>& stmt) {
+void CQLServiceImpl::DeletePreparedStatementUnlocked(const std::shared_ptr<CQLStatement> stmt) {
   // Remove statement from cache by looking it up by query ID and only when it is same statement
-  // object.
+  // object. Note that the "stmt" parameter above is not a ref ("&") intentionally so that we have
+  // a separate copy of the shared_ptr and not the very shared_ptr in prepared_stmts_map_ or
+  // prepared_stmt_list_ we are deleting.
   const auto itr = prepared_stmts_map_.find(stmt->query_id());
   if (itr != prepared_stmts_map_.end() && itr->second == stmt) {
     prepared_stmts_map_.erase(itr);

src/yb/cqlserver/cql_service.h
@@ -93,7 +93,7 @@ class CQLServiceImpl : public CQLServerServiceIf {
 
   // Delete a prepared statement from the cache and the LRU list. "prepared_stmts_mutex_" needs to
   // be locked before this call.
-  void DeletePreparedStatementUnlocked(const std::shared_ptr<CQLStatement>& stmt);
+  void DeletePreparedStatementUnlocked(const std::shared_ptr<CQLStatement> stmt);
 
   // Delete the least recently used prepared statement from the cache to free up memory.
   void DeleteLruPreparedStatement();