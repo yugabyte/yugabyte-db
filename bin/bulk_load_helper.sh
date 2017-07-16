2b72ed7cab513d5593e629284cb00e2753edc113
Pritam Damania <pritamdamania87@users.noreply.github.com>
2017-07-15 20:05:51 -0700

    ENG-1690 Helper script for bulk load to copy SSTable files to production cluster.
    Summary:
    The script takes as input the tablet_id, tablet data directory, the replicas for the tablet
    and the appropriate ssh key. It copies over the data to the appropriate nodes and then outputs the
    replica with the corresponding directory where the data was copied. This would be used by tools to
    import and cleanup that directory.
    
    Test Plan: Jenkins: skip
    
    Reviewers: bogdan, karthik, bharat
    
    Reviewed By: bharat
    
    Subscribers: ybase
    
    Differential Revision: https://phabricator.dev.yugabyte.com/D2472

---
+++ bin/bulk_load_helper.sh
@@ -0,0 +1,123 @@
+#!/usr/bin/env bash
+
+# Copyright (c) YugaByte, Inc.
+set -euo pipefail
+
+print_help() {
+  cat <<EOT
+Usage: ${0##*/} [<options>]
+  Helper script to copy generated SSTable files to a production cluster. This script is supposed to
+  be used by yb-bulk load to copy files over to the production cluster so that the tool can then
+  import those files into the production cluster.
+Options:
+  -t, --tablet_id <tablet_id>
+    the tablet id to process.
+  -r, --replicas <replicas>
+    comma separated list of IP addresses for replicas of the given tablet_id.
+  -i, --ssh_key_file <keyfile>
+    path to the ssh key for the tservers.
+  -u, --ssh_user_name <username>
+    the username to use for ssh (default: yugabyte).
+  -p, --ssh_port <port>
+    the port to use for ssh (default: 54422).
+  -d, --tablet_data_dir <tabletdatadir>
+    directory for tablet data on local filesystem.
+  -h, --help
+    Show usage.
+EOT
+}
+
+TSERVER_CONF_FILE=/etc/yugabyte/tserver.conf
+
+tablet_id=""
+replicas=""
+ssh_key_file=""
+tablet_data_dir=""
+ssh_user_name="yugabyte"
+ssh_port=54422
+while [[ $# -gt 0 ]]; do
+  case "$1" in
+    -t|--tablet_id)
+      tablet_id=$2
+      shift
+    ;;
+    -r|--replicas)
+      replicas=$2
+      shift
+    ;;
+    -i|--ssh_key_file)
+      ssh_key_file=$2
+      shift
+    ;;
+    -d|--tablet_data_dir)
+      tablet_data_dir=$2
+      shift
+    ;;
+    -u|--ssh_user_name)
+      ssh_user_name=$2
+      shift
+    ;;
+    -p|--ssh_port)
+      ssh_port=$2
+      shift
+    ;;
+    -h|--help)
+      print_help
+      exit 0
+    ;;
+    *)
+      echo "Invalid option: $1" >&2
+      print_help
+      exit 1
+  esac
+  shift
+done
+
+if [[ -z $tablet_id || -z $replicas || -z $ssh_key_file || -z $tablet_data_dir ]]; then
+  echo "Need to specify --tablet_id, --replicas, --ssh_key_file and --tablet_data_dir" >&2
+  print_help
+  exit 1
+fi
+
+# Parse comma separated replicas into an array.
+IFS=',' read -ra REPLICAS <<< "$replicas"
+
+# Process each replica.
+ssh_cmd_prefix="ssh -o 'StrictHostKeyChecking no' -p $ssh_port -i $ssh_key_file $ssh_user_name"
+for replica in "${REPLICAS[@]}"; do
+  # Find the data dirs for the tserver
+  data_dirs=$(eval $ssh_cmd_prefix@$replica "grep fs_data_dirs $TSERVER_CONF_FILE | cut -d'=' -f2"\
+  2>/dev/null)
+  IFS=',' read -ra data_dir_arr <<< "$data_dirs"
+
+  # Now find the tablet in the data dirs.
+  remote_tablet_data_dir=""
+  for data_dir in "${data_dir_arr[@]}"; do
+    remote_tablet_data_dir=$(eval $ssh_cmd_prefix@$replica "find $data_dir/data/rocksdb/ -name \
+    $tablet_id")
+    if [[ ! -z $remote_tablet_data_dir ]]; then
+      break;
+    fi
+  done
+
+  if [[ -z $remote_tablet_data_dir ]]; then
+    echo "ERROR: Couldn't find tablet: $tablet_id on replica $replica, tried data directories:
+    ${data_dir_arr[@]}" >&2
+    exit 1
+  fi
+
+  echo "Found tablet dir $remote_tablet_data_dir on replica $replica" >&2
+  tablet_parent=$(dirname $remote_tablet_data_dir)
+  staging_dir="$tablet_parent/$tablet_id.bulk_load_staging"
+
+  # Create staging directory and copy files over to remote tserver.
+  echo "Creating staging directory $staging_dir on $replica" >&2
+  $(eval $ssh_cmd_prefix@$replica "\"mkdir -p $staging_dir\"")
+
+  echo "Copying tablet data from $tablet_data_dir to $remote_tablet_data_dir on $replica" >&2
+  scp -q -o 'StrictHostKeyChecking no' -P $ssh_port -i $ssh_key_file $tablet_data_dir/* \
+  $ssh_user_name@$replica:$staging_dir/
+
+  # Output the replica and staging directory location for other tools to process.
+  echo $replica,$staging_dir
+done
