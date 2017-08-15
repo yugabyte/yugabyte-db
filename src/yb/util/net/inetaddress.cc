7ed5081e9cedf7658136453df905d64f0d018b1e
Pritam Damania <pritamdamania87@users.noreply.github.com>
2017-08-15 11:56:38 -0700

    Fix inet test failures on GCP due to lack of ipv6 support.
    Summary:
    By default building an InetAddress object from a string would try to resolve the provided
    string as a hostname. This used getaddrinfo() underneath, which would skip the DNS lookup if it
    found a valid IP address notation. Although for IPv6, this wouldn't work if the machine wasn't
    configured to use IPv6 since getaddrinfo() couldn't reach the provided IP and didn't find a route
    for it either.
    
    The solution was to check if we have a valid IP addr string and resolve the host only if it wasn't a valid IP addr string.
    
    Test Plan: jenkins
    
    Reviewers: mikhail
    
    Reviewed By: mikhail
    
    Subscribers: ybase
    
    Differential Revision: https://phabricator.dev.yugabyte.com/D2698

--- src/yb/util/net/inetaddress.cc
+++ src/yb/util/net/inetaddress.cc
@@ -4,12 +4,17 @@
 #include <boost/asio/ip/tcp.hpp>
 #include "yb/util/net/inetaddress.h"
 
+using boost::asio::ip::address;
+using boost::asio::ip::address_v4;
+using boost::asio::ip::address_v6;
+using boost::asio::ip::tcp;
+
 namespace yb {
 
 InetAddress::InetAddress() {
 }
 
-InetAddress::InetAddress(const boost::asio::ip::address& address)
+InetAddress::InetAddress(const address& address)
     : boost_addr_(address) {
 }
 
@@ -18,13 +23,13 @@ InetAddress::InetAddress(const InetAddress& other) {
 }
 
 CHECKED_STATUS ResolveInternal(const std::string& host,
-                               boost::asio::ip::tcp::resolver::iterator* iter) {
+                               tcp::resolver::iterator* iter) {
   boost::system::error_code ec;
   boost::asio::io_service io;
-  boost::asio::ip::tcp::resolver resolver(io);
+  tcp::resolver resolver(io);
   // Port 80 is just a placeholder since boost doesn't seem to support a DNS lookup API without
   // the port.
-  boost::asio::ip::tcp::resolver::query query(host, "80");
+  tcp::resolver::query query(host, "80");
   *iter = resolver.resolve(query, ec);
   if (ec.value()) {
     return STATUS_SUBSTITUTE(InvalidArgument, "$0 is an invalid host/ip address: $1", host,
@@ -34,21 +39,34 @@ CHECKED_STATUS ResolveInternal(const std::string& host,
 }
 
 CHECKED_STATUS InetAddress::Resolve(const std::string& host, std::vector<InetAddress>* addresses) {
-  boost::asio::ip::tcp::resolver::iterator iter;
-  RETURN_NOT_OK(ResolveInternal(host, &iter));
-  boost::asio::ip::tcp::resolver::iterator end;
-  while (iter != end) {
-    addresses->push_back(InetAddress(iter->endpoint().address()));
-    iter++;
+  // Try to see if we already have an IP address.
+  boost::system::error_code ec;
+  address addr = address::from_string(host, ec);
+  if (ec.value()) {
+    // Resolve the host if we don't have a valid IP addr notation string.
+    tcp::resolver::iterator iter;
+    RETURN_NOT_OK(ResolveInternal(host, &iter));
+    tcp::resolver::iterator end;
+    while (iter != end) {
+      addresses->emplace_back(iter->endpoint().address());
+      iter++;
+    }
+  } else {
+    addresses->emplace_back(addr);
   }
   return Status::OK();
 }
 
 CHECKED_STATUS InetAddress::FromString(const std::string& strval) {
-  boost::asio::ip::tcp::resolver::iterator iter;
-  RETURN_NOT_OK(ResolveInternal(strval, &iter));
-  // Pick the first IP address in the list.
-  boost_addr_ = iter->endpoint().address();
+  // Try to see if we already have an IP address.
+  boost::system::error_code ec;
+  boost_addr_ = address::from_string(strval, ec);
+  if (ec.value()) {
+    tcp::resolver::iterator iter;
+    RETURN_NOT_OK(ResolveInternal(strval, &iter));
+    // Pick the first IP address in the list.
+    boost_addr_ = iter->endpoint().address();
+  }
   return Status::OK();
 }
 
@@ -92,16 +110,16 @@ CHECKED_STATUS InetAddress::FromSlice(const Slice& slice, size_t size_hint) {
         "size_hint: $1", slice.size(), expected_size);
   }
   if (expected_size == kV4Size) {
-    boost::asio::ip::address_v4::bytes_type v4bytes;
+    address_v4::bytes_type v4bytes;
     DCHECK_EQ(expected_size, v4bytes.size());
     memcpy(v4bytes.data(), slice.data(), v4bytes.size());
-    boost::asio::ip::address_v4 v4address(v4bytes);
+    address_v4 v4address(v4bytes);
     boost_addr_ = v4address;
   } else if (expected_size == kV6Size) {
-    boost::asio::ip::address_v6::bytes_type v6bytes;
+    address_v6::bytes_type v6bytes;
     DCHECK_EQ(expected_size, v6bytes.size());
     memcpy(v6bytes.data(), slice.data(), v6bytes.size());
-    boost::asio::ip::address_v6 v6address(v6bytes);
+    address_v6 v6address(v6bytes);
     boost_addr_ = v6address;
   } else {
     return STATUS_SUBSTITUTE(InvalidArgument, "Size of slice is invalid: $0", expected_size);
