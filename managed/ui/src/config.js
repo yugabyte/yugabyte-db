a72fbc933f7d94473a0e8d92f42e863e24942b64
Shawn Van Ittersum <svicalifornia@users.noreply.github.com>
2017-08-15 01:15:47 -0700

    Allow ROOT_URL config to be set locally via env variable
    Summary:
    - Use REACT_APP_YUGAWARE_API_URL env variable if set
    - Set env variable via command line or ui/.env file
    - Remove ui/.env from git tracking
    - Ignore ui/.env (used for local env settings)
    
    Test Plan:
    1. Write to file ui/.env:
    
        REACT_APP_YUGAWARE_API_URL=http://portal.dev.yugabyte.com/api
    
    2. Run `npm start`
    3. Verify that requests go to portal API
    
    Reviewers: pritam, jeff, ram
    
    Reviewed By: ram
    
    Subscribers: jenkins-bot, yugaware, ui
    
    Differential Revision: https://phabricator.dev.yugabyte.com/D2695

--- managed/ui/src/config.js
+++ managed/ui/src/config.js
@@ -1,4 +1,8 @@
 // Copyright (c) YugaByte, Inc.
 
-export const ROOT_URL = process.env.NODE_ENV === 'development' ? 'http://localhost:9000/api' : '/api';
-export const MAP_SERVER_URL = process.env.NODE_ENV === 'development' ? 'https://no-such-url' : '/map';
+export const ROOT_URL = process.env.REACT_APP_YUGAWARE_API_URL ||
+  (process.env.NODE_ENV === 'development' ? 'http://localhost:9000/api' : '/api');
+
+export const MAP_SERVER_URL = process.env.NODE_ENV === 'development'
+  ? 'https://no-such-url'
+  : '/map';
