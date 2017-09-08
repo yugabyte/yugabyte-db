30fa14443b27c26ad4af1fc73a3223dfd4e08360
Mikhail Bautin <mbautin@users.noreply.github.com>
2017-09-07 23:17:02 -0700

    Removing some irrelevant files
    Summary:
    Removing some irrelevant files (Kudu Python client, some build support tools we are not using,
    disclaimer, etc.)
    
    Test Plan:
    Build locally
    
    Jenkins: skip
    
    Reviewers: karthik, kannan, bharat
    
    Reviewed By: kannan, bharat
    
    Subscribers: ybase
    
    Differential Revision: https://phabricator.dev.yugabyte.com/D2875

--- build-support/get-upstream-commit.sh
+++ build-support/get-upstream-commit.sh
@@ -1,18 +1,5 @@
 #!/bin/bash
 #
-# Copyright (c) YugaByte, Inc.
-#
-# Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
-# in compliance with the License.  You may obtain a copy of the License at
-#
-# http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software distributed under the License
-# is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
-# or implied.  See the License for the specific language governing permissions and limitations
-# under the License.
-#
-#
 # Licensed to the Apache Software Foundation (ASF) under one
 # or more contributor license agreements.  See the NOTICE file
 # distributed with this work for additional information
@@ -30,13 +17,26 @@
 # specific language governing permissions and limitations
 # under the License.
 #
-# Script which tries to determine the most recent git hash in the current
-# branch which was checked in using phabricator. This commit hash is printed to
-# stdout.
+# The following only applies to changes made to this file as part of YugaByte development.
 #
-# It does so by looking for the 'Reviewed By:' tag added by phabricator. This is
-# more foolproof than trying to guess at the "origin/" branch name, since the
-# developer might be working on some local topic branch.
+# Portions Copyright (c) YugaByte, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+# in compliance with the License.  You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software distributed under the License
+# is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+# or implied.  See the License for the specific language governing permissions and limitations
+# under the License.
+#
+# Script which tries to determine the most recent git hash in the current branch which was checked
+# in using Phabricator. This commit hash is printed to stdout.
+#
+# It does so by looking for the 'Reviewed By:' tag added by phabricator. This is more foolproof than
+# trying to guess at the "origin/" branch name, since the developer might be working on some local
+# topic branch.
 set -e
 
 git log --grep='Reviewed By:' -n1 --pretty=format:%H
