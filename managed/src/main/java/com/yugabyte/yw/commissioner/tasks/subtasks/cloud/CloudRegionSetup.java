f7034fe67aa3565e0ab38b0240f5b462522cf0ba
Jeff Vales Kennedy <valesken@users.noreply.github.com>
2017-09-11 17:01:37 -0700

    ENG-2105: Make CloudBootstrap work for GCP
    Summary: CloudBootstrap, which is a way of putting the cloud configuration in the task framework as a single task, only supported AWS. This diff adds support for GCP to CloudBootstrap. Also created a cloud-agnostic AbstractInitializer as part of this in order to move cloud configuration logic closer to a cloud-agnostic approach.
    
    Test Plan:
    - sbt test
    - delete GCP from database; hit `http://localhost:9000/api/customers/<cUUID>/providers/<pUUID>/bootstrap` endpoint with payload `{"regionList": ["us-west1"]}`; verify GCP is properly bootstrapped
    
    Reviewers: bogdan, bharat
    
    Reviewed By: bharat
    
    Subscribers: jenkins-bot, ram, yugaware
    
    Differential Revision: https://phabricator.dev.yugabyte.com/D2902

--- managed/src/main/java/com/yugabyte/yw/commissioner/tasks/subtasks/cloud/CloudRegionSetup.java
+++ managed/src/main/java/com/yugabyte/yw/commissioner/tasks/subtasks/cloud/CloudRegionSetup.java
@@ -3,10 +3,13 @@
 package com.yugabyte.yw.commissioner.tasks.subtasks.cloud;
 
 import com.fasterxml.jackson.databind.JsonNode;
+import com.yugabyte.yw.commissioner.Common;
 import com.yugabyte.yw.commissioner.tasks.CloudBootstrap;
 import com.yugabyte.yw.commissioner.tasks.CloudTaskBase;
+import com.yugabyte.yw.common.CloudQueryHelper;
 import com.yugabyte.yw.common.NetworkManager;
 import com.yugabyte.yw.models.AvailabilityZone;
+import com.yugabyte.yw.models.Provider;
 import com.yugabyte.yw.models.Region;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -14,6 +17,7 @@ import play.api.Play;
 import play.libs.Json;
 
 import java.util.HashSet;
+import java.util.List;
 import java.util.Map;
 
 public class CloudRegionSetup extends CloudTaskBase {
@@ -38,21 +42,42 @@ public class CloudRegionSetup extends CloudTaskBase {
       throw new RuntimeException("Region " + regionCode + " metadata not found");
     }
     JsonNode metaData = Json.toJson(regionMetadata.get(regionCode));
-    final Region region = Region.createWithMetadata(getProvider(), regionCode, metaData);
-    NetworkManager networkManager = Play.current().injector().instanceOf(NetworkManager.class);
+    Provider provider = getProvider();
+    final Region region = Region.createWithMetadata(provider, regionCode, metaData);
 
-    JsonNode vpcInfo = networkManager.bootstrap(region.uuid,
-                                                taskParams().hostVpcId,
-                                                taskParams().destVpcId);
-    if (vpcInfo.has("error") || !vpcInfo.has(regionCode)) {
-      // If network bootstrap failed, we will delete the newly created region.
-      region.delete();
-      throw new RuntimeException("Region Bootstrap failed for: " + regionCode);
+    switch (Common.CloudType.valueOf(provider.code)) {
+      case aws:
+        NetworkManager networkManager = Play.current().injector().instanceOf(NetworkManager.class);
+        JsonNode vpcInfo = networkManager.bootstrap(region.uuid, taskParams().hostVpcId,
+            taskParams().destVpcId);
+        if (vpcInfo.has("error") || !vpcInfo.has(regionCode)) {
+          // If network bootstrap failed, we will delete the newly created region.
+          region.delete();
+          throw new RuntimeException("Region Bootstrap failed for: " + regionCode);
+        }
+        Map<String, String> zoneSubnets = Json.fromJson(vpcInfo.get(regionCode).get("zones"),
+            Map.class);
+        region.zones = new HashSet<>();
+        zoneSubnets.forEach((zone, subnet) ->
+            region.zones.add(AvailabilityZone.create(region, zone, zone, subnet)));
+        break;
+      case gcp:
+        CloudQueryHelper queryHelper = Play.current().injector().instanceOf(CloudQueryHelper.class);
+        JsonNode zoneInfo = queryHelper.getZones(region);
+        if (zoneInfo.has("error") || !zoneInfo.has(regionCode)) {
+          region.delete();
+          String errMsg = "Region Bootstrap failed. Unable to fetch zones for " + regionCode;
+          throw new RuntimeException(errMsg);
+        }
+        List<String> zones = Json.fromJson(zoneInfo.get(regionCode), List.class);
+        region.zones = new HashSet<>();
+        zones.forEach(zone -> {
+          region.zones.add(AvailabilityZone.create(region, zone, zone, "subnet-" + regionCode));
+        });
+        break;
+      default:
+        throw new RuntimeException("Cannot bootstrap region " + regionCode + " for provider " +
+            provider.code + ".");
     }
-    Map<String, String> zoneSubnets =
-        Json.fromJson(vpcInfo.get(regionCode).get("zones"), Map.class);
-    region.zones = new HashSet<>();
-    zoneSubnets.forEach((zone, subnet) ->
-        region.zones.add(AvailabilityZone.create(region, zone, zone, subnet)));
   }
 }
