907bb6b1bf81d884520aa7dc3516031b6e6fc639
Jeff Vales Kennedy <valesken@users.noreply.github.com>
2017-10-02 13:54:19 -0700

    ENG-2170: Add YBInfoTip component to UI
    Summary:
    As in the JIRA, there have been many instances where a little additional information would go a long way, but we didn't have the space for it. This diff adds a little help icon you can click that shows extra information to address that need. Used [[ http://www.colorhexa.com/cd6500 | http://www.colorhexa.com/cd6500 ]] to pick an appropriate blue "help" color. Used Popover from [[ https://react-bootstrap.github.io/components.html#popovers | react-bootstrap ]] as the base component.
    
    Notes:
    - The content can contain arbitrary html (such as links). It doesn't have to just be text.
    - Title and "position" are optional.
    -- "position" refers to what direction the popover should open in when you click the help icon.
    - Component is added as part of the YBTextInput and YBTextArea components via the YBLabel component as proof of concept, but it can be added to all sorts of other places as well.
    - Constants json file (in this case, OnPremWizardConstants.json) is where the content for these info tips should be managed. Potentially, this is also where other constants for various components should be kept.
    - The YBInfoTip will close when you click outside it, so they will never overlap.
    
    Test Plan:
    Visit page containing OnPremProviderAndAccessKey and verify the component appears and performs as expected.
    
    {F5582}
    
    Reviewers: pritam, shawn
    
    Reviewed By: shawn
    
    Subscribers: bogdan, karthik, sid, ram, jenkins-bot, ui
    
    Differential Revision: https://phabricator.dev.yugabyte.com/D3069

--- managed/ui/src/components/common/forms/fields/YBTextArea.js
+++ managed/ui/src/components/common/forms/fields/YBTextArea.js
@@ -11,7 +11,8 @@ export default class YBTextArea extends Component {
   };
   render() {
     const self = this;
-    const { input, type, className, placeHolder, onValueChanged, isReadOnly, label, meta, insetError } = this.props;
+    const { input, type, className, placeHolder, onValueChanged, isReadOnly, label, meta, insetError, infoContent,
+      infoTitle, infoPlacement } = this.props;
     function onChange(event) {
       if (isFunction(onValueChanged)) {
         onValueChanged(event.target.value);
@@ -19,7 +20,8 @@ export default class YBTextArea extends Component {
       self.props.input.onChange(event.target.value);
     }
     return (
-      <YBLabel label={label} insetError={insetError} meta={meta}>
+      <YBLabel label={label} insetError={insetError} meta={meta} infoContent={infoContent} infoTitle={infoTitle}
+               infoPlacement={infoPlacement}>
         <FormControl {...input} componentClass="textarea" placeholder={placeHolder} type={type} className={className}
                    onChange={onChange} readOnly={isReadOnly} />
       </YBLabel>
