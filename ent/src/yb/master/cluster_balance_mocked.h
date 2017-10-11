cf06f858477913427e78eed0baa469652500132c
Rahul Desirazu <rahuldesirazu@users.noreply.github.com>
2017-10-11 12:39:25 -0700

    This commit provides additional functionality that is logically related to the earlier commit https://github.com/YugaByte/yugabyte-db/commit/f316b0ac8e6a02d425299de0548d776ed509d7e4
    Original commit message:
    
    ENG-1961 Affinitized leader load balancing.
    
    Summary:
    Feature to let users specify affinitized zones/regions/clouds for leaders. During leader balancing, if an affinitized area is specified, leader load moves tries to move from a non-affinitized node to an affinitized node. If no such movement exists, normal leader balancing occurs within the affinitized area.
    
    In terms of the enterprise split, the code for the actually splitting the ClusterBalancer was relatively straightforward, as I just added two new methods for enterprise and stubbed them out in the community. I did have to move around the ClusterLoadState to its own .h file so that both community and enterprise ClusterBalancer could see it. Tests were a little more complicated, since I wanted my enterprise test class to subclass the community test class, but the test class already subclassed ClusterBalancer. Therefore, I just created a ClusterBalancerMock class for both enterprise and community with catalog manager functions stubbed out and passed them in as a ClusterBalancer reference to test constructors.
    
    Test Plan: C++ unit tests.
    
    Reviewers: robert, kannan, mikhail, bogdan, oleg
    
    Reviewed By: bogdan, oleg
    
    Subscribers: bharat, ybase
    
    Differential Revision: https://phabricator.dev.yugabyte.com/D2675

---
+++ ent/src/yb/master/cluster_balance_mocked.h
@@ -0,0 +1,78 @@
+// Copyright (c) YugaByte, Inc.
+
+#ifndef ENT_SRC_YB_MASTER_CLUSTER_BALANCE_MOCKED_H
+#define ENT_SRC_YB_MASTER_CLUSTER_BALANCE_MOCKED_H
+
+#include "yb/master/cluster_balance.h"
+#include "../../src/yb/master/cluster_balance_mocked.h"
+
+namespace yb {
+namespace master {
+namespace enterprise {
+
+class ClusterLoadBalancerMocked : public ClusterLoadBalancer {
+ public:
+  ClusterLoadBalancerMocked() : ClusterLoadBalancer(nullptr) {
+    const int kHighNumber = 100;
+    options_.kMaxConcurrentAdds = kHighNumber;
+    options_.kMaxConcurrentRemovals = kHighNumber;
+    options_.kAllowLimitStartingTablets = false;
+    options_.kAllowLimitOverReplicatedTablets = false;
+  }
+
+  // Overrides for base class functionality to bypass calling CatalogManager.
+  void GetAllLiveDescriptors(TSDescriptorVector* ts_descs) const override { *ts_descs = ts_descs_; }
+
+  void GetAllAffinitizedZones(AffinitizedZonesSet* affinitized_zones) const override {
+    *affinitized_zones = affinitized_zones_;
+  }
+
+  const TabletInfoMap& GetTabletMap() const override { return tablet_map_; }
+
+  const TableInfoMap& GetTableMap() const override { return table_map_; }
+
+  const scoped_refptr<TableInfo> GetTableInfo(const TableId& table_uuid) const override {
+    return FindPtrOrNull(table_map_, table_uuid);
+  }
+
+  const PlacementInfoPB& GetClusterPlacementInfo() const override { return cluster_placement_; }
+
+  const BlacklistPB& GetServerBlacklist() const override { return blacklist_; }
+
+  void SendReplicaChanges(scoped_refptr<TabletInfo> tablet, const string& ts_uuid,
+                          const bool is_add, const bool should_remove,
+                          const string& new_leader_uuid) override {
+    // Do nothing.
+  }
+
+  void GetPendingTasks(const TableId& table_uuid,
+                       TabletToTabletServerMap* pending_add_replica_tasks,
+                       TabletToTabletServerMap* pending_remove_replica_tasks,
+                       TabletToTabletServerMap* pending_stepdown_leader_tasks) override {
+    for (const auto& tablet_id : pending_add_replica_tasks_) {
+      (*pending_add_replica_tasks)[tablet_id] = "";
+    }
+    for (const auto& tablet_id : pending_remove_replica_tasks_) {
+      (*pending_remove_replica_tasks)[tablet_id] = "";
+    }
+    for (const auto& tablet_id : pending_stepdown_leader_tasks_) {
+      (*pending_stepdown_leader_tasks)[tablet_id] = "";
+    }
+  }
+
+  TSDescriptorVector ts_descs_;
+  AffinitizedZonesSet affinitized_zones_;
+  TabletInfoMap tablet_map_;
+  TableInfoMap table_map_;
+  PlacementInfoPB cluster_placement_;
+  BlacklistPB blacklist_;
+  vector<string> pending_add_replica_tasks_;
+  vector<string> pending_remove_replica_tasks_;
+  vector<string> pending_stepdown_leader_tasks_;
+};
+
+} // namespace enterprise
+} // namespace master
+} // namespace yb
+
+#endif // ENT_SRC_YB_MASTER_CLUSTER_BALANCE_MOCKED_H
