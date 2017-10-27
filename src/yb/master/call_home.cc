68c5ecabb72c72799d918606912bed9a0b34bcd0
Ram Sri <ramkumarvs@users.noreply.github.com>
2017-10-26 20:05:37 -0700

    Update diagnostics endpoint to be https
    Summary: Update the diagnostics endpoint to be https
    
    Test Plan: Tested the endpoint via curl
    
    Reviewers: bogdan, karthik, hector
    
    Reviewed By: hector
    
    Subscribers: ybase
    
    Differential Revision: https://phabricator.dev.yugabyte.com/D3305

--- src/yb/master/call_home.cc
+++ src/yb/master/call_home.cc
@@ -33,7 +33,7 @@ static const char* kHighLevel = "high";
 DEFINE_bool(callhome_enabled, true,
             "Enables callhome feature that sends analytics data to yugabyte");
 DEFINE_int32(callhome_interval_secs, 3600, "How often to run callhome");
-DEFINE_string(callhome_url, "http://diagnostics.platform.yugabyte.com:8080",
+DEFINE_string(callhome_url, "https://diagnostics.yugabyte.com",
               "URL of callhome server");
 DEFINE_string(callhome_collection_level, kMediumLevel, "Level of details sent by callhome");
 
