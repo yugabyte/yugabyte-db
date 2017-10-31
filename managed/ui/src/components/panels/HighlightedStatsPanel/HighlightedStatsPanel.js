37f6ea165a64a9bb47abf214964305e5d759426e
Jeff Vales Kennedy <valesken@users.noreply.github.com>
2017-10-31 11:22:03 -0700

    ENG-2324: Fix for one universe being in an error state causing summary of price to be in error state
    Summary: If one universe's price becomes "N/A" then the summary price for all universes will also become "N/A". We should instead just ignore the failed universe.
    
    Test Plan:
    - Manually set device info's fields to null for a created universe (regress the universe), go to UI, validate that the universe price shows up as N/A but the summary price does not.
    {F6029}
    - sbt test
    
    Reviewers: pritam, ram, bogdan
    
    Reviewed By: bogdan
    
    Subscribers: jenkins-bot, yugaware, ui
    
    Differential Revision: https://phabricator.dev.yugabyte.com/D3348

--- managed/ui/src/components/panels/HighlightedStatsPanel/HighlightedStatsPanel.js
+++ managed/ui/src/components/panels/HighlightedStatsPanel/HighlightedStatsPanel.js
@@ -7,6 +7,7 @@ import { DescriptionItem, YBFormattedNumber } from '../../common/descriptors';
 import { getPromiseState } from 'utils/PromiseUtils';
 import {YBLoadingIcon} from '../../common/indicators';
 import './HighlightedStatsPanel.scss';
+import {isDefinedNotNull, isNonEmptyObject} from "../../../utils/ObjectUtils";
 
 class StatsPanelComponent extends Component {
   render() {
@@ -39,8 +40,13 @@ export default class HighlightedStatsPanel extends Component {
     }
 
     universeList.data.forEach(function (universeItem) {
-      numNodes += universeItem.universeDetails.userIntent.numNodes;
-      totalCost += universeItem.pricePerHour * 24 * moment().daysInMonth();
+      if (isNonEmptyObject(universeItem.universeDetails) && isNonEmptyObject(universeItem.universeDetails.userIntent)
+          && isDefinedNotNull(universeItem.universeDetails.userIntent.numNodes)) {
+        numNodes += universeItem.universeDetails.userIntent.numNodes;
+      }
+      if (isDefinedNotNull(universeItem.pricePerHour)) {
+        totalCost += universeItem.pricePerHour * 24 * moment().daysInMonth();
+      }
     });
     const formattedCost = (
       <YBFormattedNumber value={totalCost} maximumFractionDigits={2}
