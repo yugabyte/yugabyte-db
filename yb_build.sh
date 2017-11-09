yugabyte/yugabyte-db

commit 3805307dad12b511313d4d72581c8197d5d069b2

Simplify library_packager.py and move to LD_LIBRARY_PATH-based packaging to avoid including system libraries with YB
Summary:
Move away from the layout with multiple directories with symlinks.
Do not package system libraries with the product. We only really need a couple of libraries
(libsasl2, libdb) but they are found automatically in system locations.
Start using LD_LIBRARY_PATH (as every alternative I tried seems to be inferior).

Test Plan:
Deploy to a local CentOS docker container and run a load test from there
Jenkins: compile only

Reviewers: hector, timur

Reviewed By: hector

Subscribers: ybase

Differential Revision: https://phabricator.dev.yugabyte.com/D3400

yb_build.sh
@@ -208,7 +208,7 @@ while [ $# -gt 0 ]; do
     continue
   fi
 
-  case "$1" in
+  case "${1//_/-}" in
     -h|--help)
       show_help >&2
       exit 1
@@ -371,6 +371,11 @@ while [ $# -gt 0 ]; do
     ;;
     daemons|yb-daemons)
       make_targets+=( "yb-master" "yb-tserver" )
+    ;;
+    packaged-targets)
+      for packaged_target in $( "$YB_SRC_ROOT"/build-support/list_packaged_targets.py ); do
+        make_targets+=( "$packaged_target" )
+      done
     ;;
     --skip-build|--sb)
       build_cxx=false

build-support/jenkins/build-and-test.sh
@@ -58,6 +58,11 @@
 #   YB_TRACK_REGRESSIONS
 #   Default: 0
 #     Track regressions by re-running failed tests multiple times on the previous git commit.
+#     The implementation of this feature is unfinished.
+#
+#   YB_COMPILE_ONLY
+#   Default: 0
+#     Compile the code and build a package, but don't run tests.
 #
 # Portions Copyright (c) YugaByte, Inc.
 
@@ -163,6 +168,7 @@ else
   DONT_DELETE_BUILD_ROOT=${DONT_DELETE_BUILD_ROOT:-1}
 fi
 SKIP_CPP_MAKE=${SKIP_CPP_MAKE:-0}
+YB_COMPILE_ONLY=${YB_COMPILE_ONLY:-0}
 
 CTEST_OUTPUT_PATH="$BUILD_ROOT"/ctest.log
 CTEST_FULL_OUTPUT_PATH="$BUILD_ROOT"/ctest-full.log
@@ -447,13 +453,20 @@ if [[ $YB_BUILD_JAVA == "1" ]]; then
 
   # Use a unique version to avoid a race with other concurrent jobs on jar files that we install
   # into ~/.m2/repository.
-  yb_java_project_version=yugabyte-jenkins-$( date +%Y%m%dT%H%M%S ).$RANDOM.$RANDOM.$$
+  random_id=$( date +%Y%m%dT%H%M%S )_$RANDOM$RANDOM$RANDOM
+  yb_java_project_version=yugabyte-jenkins-$random_id
+
+  yb_new_group_id=org.yb$random_id
+  find . -name "pom.xml" \
+         -exec sed -i "s#<groupId>org[.]yb</groupId>#<groupId>$yb_new_group_id</groupId>#g" {} \;
 
   (
     set -x
     mvn versions:set -DnewVersion="$yb_java_project_version"
     git add -A .
-    git commit -m "Updating version to $yb_java_project_version during testing"
+    commit_msg="Updating version to $yb_java_project_version and groupId to $yb_new_group_id "
+    commit_msg+="during testing"
+    git commit -m "$commit_msg"
   )
 
   java_build_cmd_line=( --fail-never -DbinDir="$BUILD_ROOT"/bin )
@@ -466,67 +479,75 @@ if [[ $YB_BUILD_JAVA == "1" ]]; then
   popd
 fi
 
-if spark_available; then
-  if [[ $YB_BUILD_CPP == "1" || $YB_BUILD_JAVA == "1" ]]; then
-    log "Will run tests on Spark"
-    extra_args=""
-    if [[ $YB_BUILD_JAVA == "1" ]]; then
-      extra_args+="--java"
+# -------------------------------------------------------------------------------------------------
+# Run tests, either on Spark or locally.
+# If YB_COMPILE_ONLY is set to 1, we skip running all tests (Java and C++).
+
+if [[ $YB_COMPILE_ONLY != "1" ]]; then
+  if spark_available; then
+    if [[ $YB_BUILD_CPP == "1" || $YB_BUILD_JAVA == "1" ]]; then
+      log "Will run tests on Spark"
+      extra_args=""
+      if [[ $YB_BUILD_JAVA == "1" ]]; then
+        extra_args+="--java"
+      fi
+      if [[ $YB_BUILD_CPP == "1" ]]; then
+        extra_args+=" --cpp"
+      fi
+      if ! run_tests_on_spark $extra_args; then
+        EXIT_STATUS=1
+        FAILURES+=$'Distributed tests on Spark (C++ and/or Java) failed\n'
+        log "Some tests that were run on Spark failed"
+      fi
+      unset extra_args
+    else
+      log "Neither C++ or Java tests are enabled, nothing to run on Spark."
     fi
-    if [[ $YB_BUILD_CPP == "1" ]]; then
-      extra_args+=" --cpp"
-    fi
-    if ! run_tests_on_spark $extra_args; then
-      EXIT_STATUS=1
-      FAILURES+=$'Distributed tests on Spark (C++ and/or Java) failed\n'
-      log "Some tests that were run on Spark failed"
-    fi
-    unset extra_args
   else
-    log "Neither C++ or Java tests are enabled, nothing to run on Spark."
-  fi
-else
-  # A single-node way of running tests (without Spark).
+    # A single-node way of running tests (without Spark).
 
-  if [[ $YB_BUILD_CPP == "1" ]]; then
-    log "Run C++ tests in a non-distributed way"
-    export GTEST_OUTPUT="xml:$TEST_LOG_DIR/" # Enable JUnit-compatible XML output.
+    if [[ $YB_BUILD_CPP == "1" ]]; then
+      log "Run C++ tests in a non-distributed way"
+      export GTEST_OUTPUT="xml:$TEST_LOG_DIR/" # Enable JUnit-compatible XML output.
 
-    if ! spark_available; then
-      log "Did not find Spark on the system, falling back to a ctest-based way of running tests"
-      set +e
-      time ctest -j$NUM_PARALLEL_TESTS ${EXTRA_TEST_FLAGS:-} \
-          --output-log "$CTEST_FULL_OUTPUT_PATH" \
-          --output-on-failure 2>&1 | tee "$CTEST_OUTPUT_PATH"
-      if [ $? -ne 0 ]; then
-        EXIT_STATUS=1
-        FAILURES+=$'C++ tests failed\n'
+      if ! spark_available; then
+        log "Did not find Spark on the system, falling back to a ctest-based way of running tests"
+        set +e
+        time ctest -j$NUM_PARALLEL_TESTS ${EXTRA_TEST_FLAGS:-} \
+            --output-log "$CTEST_FULL_OUTPUT_PATH" \
+            --output-on-failure 2>&1 | tee "$CTEST_OUTPUT_PATH"
+        if [ $? -ne 0 ]; then
+          EXIT_STATUS=1
+          FAILURES+=$'C++ tests failed\n'
+        fi
+        set -e
       fi
-      set -e
-    fi
-    log "Finished running C++ tests (see timing information above)"
+      log "Finished running C++ tests (see timing information above)"
 
-    if [[ $DO_COVERAGE == "1" ]]; then
-      heading "Generating coverage report..."
-      if ! $YB_SRC_ROOT/thirdparty/gcovr-3.0/scripts/gcovr -r $YB_SRC_ROOT --xml \
-          > $BUILD_ROOT/coverage.xml ; then
-        EXIT_STATUS=1
-        FAILURES+=$'Coverage report failed\n'
+      if [[ $DO_COVERAGE == "1" ]]; then
+        heading "Generating coverage report..."
+        if ! $YB_SRC_ROOT/thirdparty/gcovr-3.0/scripts/gcovr -r $YB_SRC_ROOT --xml \
+            > $BUILD_ROOT/coverage.xml ; then
+          EXIT_STATUS=1
+          FAILURES+=$'Coverage report failed\n'
+        fi
       fi
     fi
-  fi
 
-  if [[ $YB_BUILD_JAVA == "1" ]]; then
-    pushd "$YB_SRC_ROOT/java"
-    log "Running Java tests in a non-distributed way"
-    if ! time build_yb_java_code_with_retries "${java_build_cmd_line[@]}" verify 2>&1; then
-      EXIT_STATUS=1
-      FAILURES+=$'Java tests failed\n'
+    if [[ $YB_BUILD_JAVA == "1" ]]; then
+      pushd "$YB_SRC_ROOT/java"
+      log "Running Java tests in a non-distributed way"
+      if ! time build_yb_java_code_with_retries "${java_build_cmd_line[@]}" verify 2>&1; then
+        EXIT_STATUS=1
+        FAILURES+=$'Java tests failed\n'
+      fi
+      log "Finished running Java tests (see timing information above)"
+      popd
     fi
-    log "Finished running Java tests (see timing information above)"
-    popd
   fi
 fi
+
+# Finished running tests.
 
 if [[ $YB_BUILD_CPP == "1" ]] && using_linuxbrew; then
   # -----------------------------------------------------------------------------------------------
@@ -537,7 +558,7 @@ if [[ $YB_BUILD_CPP == "1" ]] && using_linuxbrew; then
   rm -rf "$packaged_dest_dir"
   log "Testing creating a distribution in '$packaged_dest_dir'"
   export PYTHONPATH=${PYTHONPATH:-}:$YB_SRC_ROOT/python
-  "$YB_SRC_ROOT/python/yb/library_packager.py" \
+  python "$YB_SRC_ROOT/python/yb/library_packager.py" \
     --build-dir "$BUILD_ROOT" \
     --dest-dir "$packaged_dest_dir"
   rm -rf "$packaged_dest_dir"

build-support/list_packaged_targets.py
@@ -1,0 +1,26 @@
+#!/usr/bin/env python
+
+# Copyright (c) YugaByte, Inc.
+
+# Lists the targets that need to be built for a YB release.
+
+import os
+import json
+import re
+
+
+YB_SRC_ROOT = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+LATEST_BINARY_RE = re.compile('^build/latest/bin/([^/]+)$')
+
+
+def main():
+    with open(os.path.join(YB_SRC_ROOT, 'yb_release_manifest.json')) as manifest_input_file:
+        release_manifest = json.loads(manifest_input_file.read())
+    for bin_path in release_manifest['bin']:
+        match = LATEST_BINARY_RE.match(bin_path)
+        if match:
+            print match.group(1)
+
+
+if __name__ == '__main__':
+    main()

build-support/post_install.sh
@@ -38,7 +38,7 @@ if [[ ! -x $ld_path ]]; then
 fi
 
 cd "$bin_dir"
-for f in *; do
+for f in ../lib/unwrapped/*; do
   if [[ -x $f && $(file -b $f) =~ $ELF_FILE_PATTERN && $f != patchelf ]]; then
     ( set -x; "$patchelf_path" --set-interpreter "$ld_path" "$f" )
   fi

build-support/yb_release.py
@@ -14,20 +14,25 @@
 
 import argparse
 import atexit
+import glob
+import imp
+import json
 import logging
 import os
 import shutil
 import sys
 import tempfile
 import yaml
-import imp
 
+from xml.dom import minidom
 from subprocess import call
 from ybops.common.exceptions import YBOpsRuntimeError
 from ybops.release_manager import ReleaseManager
 from ybops.utils import init_env, log_message, RELEASE_EDITION_ALLOWED_VALUES, \
     RELEASE_EDITION_ENTERPRISE, RELEASE_EDITION_COMMUNITY
-from yb.library_packager import LibraryPackager
+from yb.library_packager import LibraryPackager, add_common_arguments
+
+YB_SRC_ROOT = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 def main():
@@ -41,30 +46,45 @@ def main():
                         default='')
     parser.add_argument('--publish', action='store_true', help='Publish release to S3.')
     parser.add_argument('--destination', help='Copy release to Destination folder.')
-    parser.add_argument('--verbose', help='Show verbose output', action='store_true')
     parser.add_argument('--force', help='Skip prompts', action='store_true')
     parser.add_argument('--edition', help='Which edition the code is built as.',
                         default=RELEASE_EDITION_ENTERPRISE,
                         choices=RELEASE_EDITION_ALLOWED_VALUES)
+    parser.add_argument('--skip-build', help='Skip building the code', action='store_true')
+    parser.add_argument(
+            '--extracted-destination',
+            help='Instead of building a tarball, put the distribution in the given'
+                 'directory. This is equivalent to building and extracting the tarball, but '
+                 'much faster.')
+    add_common_arguments(parser)
     args = parser.parse_args()
 
     init_env(logging.DEBUG if args.verbose else logging.INFO)
-    repository_root = os.path.abspath(os.path.dirname(os.path.dirname(__file__)))
-    log_message(logging.INFO, "Building Yugabyte code: '{}' build".format(args.build_type))
+    log_message(logging.INFO, "Building YugaByte code: '{}' build".format(args.build_type))
 
     tmp_dir = tempfile.mkdtemp(suffix=os.path.basename(__file__))
     atexit.register(lambda: shutil.rmtree(tmp_dir))
+
     build_desc_path = os.path.join(tmp_dir, 'build_descriptor.yaml')
     yb_distribution_dir = os.path.join(tmp_dir, 'yb_distribution')
 
-    os.chdir(repository_root)
+    os.chdir(YB_SRC_ROOT)
+
+    # Parse Java project version out of java/pom.xml.
+    java_project_version = minidom.parse('java/pom.xml').getElementsByTagName(
+            'version')[0].firstChild.nodeValue
+    log_message(logging.INFO, "Java project version from pom.xml: {}".format(java_project_version))
+
     build_edition = "enterprise" if args.edition == RELEASE_EDITION_ENTERPRISE else "community"
     build_cmd_list = [
         "./yb_build.sh", args.build_type, "--with-assembly",
         "--write-build-descriptor", build_desc_path,
         "--edition", build_edition,
+        # This will build the exact set of targets that are needed for the release.
+        "packaged_targets",
         args.build_args
         ]
+
     build_cmd_line = " ".join(build_cmd_list).strip()
     log_message(logging.INFO, "Build command line: {}".format(build_cmd_line))
     if call(build_cmd_line, shell=True) != 0:
@@ -74,10 +94,14 @@ def main():
                 build_desc_path))
     with open(build_desc_path) as build_desc_file:
         build_desc = yaml.load(build_desc_file)
+
     log_message(logging.INFO, "Build descriptor: {}".format(build_desc))
 
+    if args.extracted_destination and args.publish:
+        raise RuntimeError('--extracted-destination and --publish are incompatible')
+
     build_dir = build_desc['build_root']
-    release_manager = ReleaseManager({"repository": repository_root,
+    release_manager = ReleaseManager({"repository": YB_SRC_ROOT,
                                       "name": "yugabyte",
                                       "type": args.build_type,
                                       "edition": args.edition,
@@ -86,24 +110,84 @@ def main():
     # This points to the release manifest within the release_manager, and we are modifying that
     # directly.
     release_manifest = release_manager.release_manifest
+    for key, value_list in release_manifest.iteritems():
+        for i in xrange(len(value_list)):
+            new_value = value_list[i].replace('${project.version}', java_project_version)
+            if new_value != value_list[i]:
+                log_message(logging.INFO,
+                            "Substituting Java project version in '{}' -> '{}'".format(
+                                value_list[i], new_value))
+                value_list[i] = new_value
+
     library_packager = LibraryPackager(
             build_dir=build_dir,
             seed_executable_patterns=release_manifest['bin'],
-            dest_dir=yb_distribution_dir)
+            dest_dir=yb_distribution_dir,
+            verbose_mode=args.verbose,
+            include_system_libs=not args.no_system_libs,
+            include_licenses=args.include_licenses)
     library_packager.package_binaries()
 
     for release_subdir in ['bin']:
         if release_subdir in release_manifest:
             del release_manifest[release_subdir]
     for root, dirs, files in os.walk(yb_distribution_dir):
-        release_manifest.setdefault(os.path.relpath(root, yb_distribution_dir), []).extend([
-                os.path.join(root, f) for f in files])
+        release_manifest.setdefault(os.path.relpath(root, yb_distribution_dir), []).extend(
+                [os.path.join(root, f) for f in files])
+
+    if args.verbose:
+        log_message(logging.INFO,
+                    "Effective release manifest:\n" +
+                    json.dumps(release_manifest, indent=2, sort_keys=True))
 
     log_message(logging.INFO, "Generating release")
 
-    # We've already updated the release manifest inside release_manager with the auto-generated
-    # set of executables and libraries to package.
-    release_file = release_manager.generate_release()
+    if args.extracted_destination:
+        extracted_dest_dir = args.extracted_destination
+        if os.path.exists(extracted_dest_dir) and os.listdir(extracted_dest_dir):
+            raise RuntimeError("Directory '{}' exists and is non-empty".format(extracted_dest_dir))
+        for dir_from_manifest in release_manifest:
+            current_dest_dir = os.path.join(extracted_dest_dir, dir_from_manifest)
+            if os.path.exists(current_dest_dir):
+                if args.verbose:
+                    logging.info("Directory '{}' already exists".format(current_dest_dir))
+            else:
+                os.makedirs(current_dest_dir)
+
+            for elem in release_manifest[dir_from_manifest]:
+                if not elem.startswith('/'):
+                    elem = os.path.join(YB_SRC_ROOT, elem)
+                files = glob.glob(elem)
+                for file_path in files:
+                    if os.path.islink(file_path):
+                        link_path = os.path.join(current_dest_dir, os.path.basename(file_path))
+                        link_target = os.readlink(file_path)
+                        if args.verbose:
+                            log_message(logging.INFO,
+                                        "Creating symlink {} -> {}".format(link_path, link_target))
+                        os.symlink(link_target, link_path)
+                    elif os.path.isdir(file_path):
+                        current_dest_dir = os.path.join(current_dest_dir,
+                                                        os.path.basename(file_path))
+                        if args.verbose:
+                            log_message(logging.INFO,
+                                        "Copying directory {} to {}".format(file_path,
+                                                                            current_dest_dir))
+                        shutil.copytree(file_path, current_dest_dir)
+                    else:
+                        current_dest_dir = os.path.join(extracted_dest_dir, dir_from_manifest)
+                        if args.verbose:
+                            log_message(
+                                    logging.INFO,
+                                    "Copying file {} to directory {}".format(file_path,
+                                                                             current_dest_dir))
+                        shutil.copy(file_path, current_dest_dir)
+        log_message(logging.INFO,
+                    "Creating an non-tar distribution at '{}'".format(extracted_dest_dir))
+    else:
+        # We've already updated the release manifest inside release_manager with the auto-generated
+        # set of executables and libraries to package.
+        release_file = release_manager.generate_release()
 
     if args.publish:
         log_message(logging.INFO, "Publishing release")

python/yb/command_util.py
@@ -33,7 +33,14 @@ ProgramResult = namedtuple('ProgramResult',
                             'program_path'])
 
 
-def run_program(args, error_ok=False):
+def trim_output(output, max_lines):
+    lines = output.split("\n")
+    if len(lines) <= max_lines:
+        return output
+    return "\n".join(lines[:max_lines] + ['({} lines skipped)'.format(len(lines) - max_lines)])
+
+
+def run_program(args, error_ok=False, max_error_lines=100, cwd=None):
     """
     Run the given program identified by its argument list, and return a ProgramResult object.
 
@@ -45,7 +52,8 @@ def run_program(args, error_ok=False):
         program_subprocess = subprocess.Popen(
             args,
             stdout=subprocess.PIPE,
-            stderr=subprocess.PIPE)
+            stderr=subprocess.PIPE,
+            cwd=cwd)
     except OSError:
         logging.error("Failed to run program {}".format(args))
         raise
@@ -55,7 +63,8 @@ def run_program(args, error_ok=False):
     if program_subprocess.returncode != 0:
         error_msg = "Non-zero exit code {} from: {}, stdout: '{}', stderr: '{}'".format(
                 program_subprocess.returncode, args,
-                program_stdout.strip(), program_stderr.strip())
+                trim_output(program_stdout.strip(), max_error_lines),
+                trim_output(program_stderr.strip(), max_error_lines))
         if not error_ok:
             raise RuntimeError(error_msg)
     return ProgramResult(cmd_line=args,
@@ -64,3 +73,18 @@ def run_program(args, error_ok=False):
                          stdout=program_stdout.strip(),
                          stderr=program_stderr.strip(),
                          error_msg=error_msg)
+
+
+def mkdir_p(d):
+    """
+    Similar to the "mkdir -p ..." shell command. Creates the given directory and all enclosing
+    directories. No-op if the directory already exists.
+    """
+    if os.path.isdir(d):
+        return
+    try:
+        os.makedirs(d)
+    except OSError, e:
+        if os.path.isdir(d):
+            return
+        raise e

src/yb/rpc/proxy.cc
@@ -82,6 +82,11 @@ Proxy::Proxy(const std::shared_ptr<Messenger>& messenger,
     LOG(WARNING) << "Proxy for " << service_name_ << ": Unable to get logged-in user name: "
                  << s.ToString() << " before connecting to remote: " << remote;
   }
+  if (real_user.empty()) {
+    static const char* kDefaultUser = "yugabyte-user";
+    VLOG(4) << "Warning: logged-in user is empty, setting to '" << kDefaultUser << "'";
+    real_user = kDefaultUser;
+  }
 
   conn_id_.set_remote(remote);
   conn_id_.mutable_user_credentials()->set_real_user(real_user);

commit 0f37409956c7dfd9fa1f069189781d6bdabb39e0

Fix --gtest_filter in yb_build.sh
Summary: Fix --gtest_filter in argument parsing in yb_build.sh broken by D3400.

Test Plan: Jenkins: skip

Reviewers: pritam.damania

Reviewed By: pritam.damania

Subscribers: ybase

Differential Revision: https://phabricator.dev.yugabyte.com/D3434

yb_build.sh
@@ -296,7 +296,7 @@ while [ $# -gt 0 ]; do
     ;;
     --skip-test-existence-check|--no-test-existence-check|--ntec) test_existence_check=false ;;
     --skip-check-test-existence|--no-check-test-existence|--ncte) test_existence_check=false ;;
-    --gtest_filter)
+    --gtest-filter)
       export YB_GTEST_FILTER=$2
       shift
     ;;