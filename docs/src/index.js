1ada11306f9bb5a801a7bb3cc87da0837e5174f0
Shawn Van Ittersum <svicalifornia@gmail.com>
2017-11-20 15:13:43 -0800

    Add accordion sidebar menu
    Test Plan: npm start
    
    Reviewers: karthik, ram, sid
    
    Reviewed By: sid
    
    Subscribers: Yugaweb
    
    Differential Revision: https://phabricator.dev.yugabyte.com/D3522

--- docs/src/index.js
+++ docs/src/index.js
@@ -1,1 +1,21 @@
-// TODO: ESNext code goes here.
+// Copyright (c) YugaByte Inc.
+
+const $ = window.jQuery;
+$(document).ready(() => {
+  $('#drawerMenu').navgoco({
+    accordion: true,
+    caretHtml: '',
+    cookie: {
+      name: 'navgoco',
+      path: '/',
+      expires: false,
+    },
+    toggleSelector: 'a.node-toggle',
+    openClass: 'open',
+    save: true,
+    slide: {
+      duration: 150,
+      easing: 'swing',
+    },
+  }).show();
+});
