9e3b492a2afb194128c23a76ce19ca9ad0737051
Shawn Van Ittersum <svicalifornia@users.noreply.github.com>
2017-12-18 15:43:43 -0500

    Styling changes
    Test Plan: Run UI
    
    Reviewers: vit.pankin, sid, ram, jeff, pritam
    
    Reviewed By: pritam
    
    Subscribers: jenkins-bot, yugaware
    
    Differential Revision: https://phabricator.dev.yugabyte.com/D3680

--- managed/ui/src/components/common/descriptors/DescriptionItem.js
+++ managed/ui/src/components/common/descriptors/DescriptionItem.js
@@ -3,7 +3,7 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
-import './stylesheets/DescriptionList.css';
+import './stylesheets/DescriptionItem.scss';
 
 export default class DescriptionItem extends Component {
   static propTypes = {
