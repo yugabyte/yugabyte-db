c06641e122b7fcb99e61c6fb53195ec5485d679b
Shawn Van Ittersum <svicalifornia@users.noreply.github.com>
2017-12-20 00:02:27 -0500

    Revise Help page content
    Test Plan: npm start; click Help
    
    Reviewers: pritam, vit.pankin, sid
    
    Reviewed By: sid
    
    Subscribers: jenkins-bot, ui, yugaware
    
    Differential Revision: https://phabricator.dev.yugabyte.com/D3719

--- managed/ui/src/components/help/HelpItem/HelpItems.js
+++ managed/ui/src/components/help/HelpItem/HelpItems.js
@@ -3,39 +3,40 @@
 import React, { Component } from 'react';
 import {Row, Col} from 'react-bootstrap';
 
+import gitterIcon from './images/gitter.svg';
 import './HelpItems.scss';
 
 export default class HelpItems extends Component {
   render() {
     return (
-      <div id="page-wrapper">
-        <Row className="header-row">
-          <Col lg={12}>
-            <h2>Help</h2>
-          </Col>
-        </Row>
-        <hr/>
+      <div id="page-wrapper" className="help-links">
+        <h2 className="content-title">Help</h2>
 
         <Row>
-          <Col lg={3}>
+          <Col lg={6}>
+            <h4><i className="fa fa-globe"></i> Resources</h4>
+          </Col>
+          <Col lg={6}>
+            <p><a href="https://docs.yugabyte.com/" target="_blank"><i className="fa fa-book"></i> Documentation</a></p>
+            <p><a href="https://github.com/yugabyte" target="_blank"><i className="fa fa-github"></i> GitHub</a></p>
+          </Col>
+        </Row><br/>
+
+        <Row>
+          <Col lg={6}>
             <h4><i className="fa fa-support"></i> Support</h4>
           </Col>
-          <Col lg={9} className="help-links">
-            <p><a href=""><i className="fa fa-slack"></i> Slack Channel</a></p>
-            <p><a href="mailto:support@yugabyte.com"><i className="fa fa-envelope-o"></i> Support Email</a></p>
+          <Col lg={6}>
+            <p>
+              <a href="https://gitter.im/YugaByte/Lobby" target="_blank">
+                <object className="logo svg-logo" data={gitterIcon} type="image/svg+xml" width="16"></object> Gitter
+              </a>
+            </p>
+            <p><a href="https://www.youtube.com/channel/UCL9BhSLRowqQ1TyBndhiCEw" target="_blank"><i className="fa fa-youtube"></i> YouTube</a></p>
+            <p><a href="mailto:support@yugabyte.com"><i className="fa fa-envelope-o"></i> Email</a></p>
           </Col>
         </Row>
-        <hr/>
 
-        <Row>
-          <Col lg={3}>
-            <h4><i className="fa fa-book"></i> Documentation</h4>
-          </Col>
-          <Col lg={9} className="help-links">
-            Coming soon&hellip;
-          </Col>
-        </Row>
-        <hr/>
       </div>
     );
   }
