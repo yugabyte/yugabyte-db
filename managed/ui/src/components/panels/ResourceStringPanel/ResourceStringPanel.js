60634823bd3843951382dcd6736e332749cf80fb
Pritam Roy <jordanblink@users.noreply.github.com>
2017-12-24 23:45:52 -0800

    ENG-2647, Provider and Region Info missing in UI
    Summary:
    Regression caused due to Async cluster userIntent change not being handled on UI.
    
    {F7860}
    
    Test Plan:
    Go to universe details page.
    You should see the Provider and Regions list in Universe Detail UI.
    
    Reviewers: ram, jeff
    
    Reviewed By: ram, jeff
    
    Subscribers: jenkins-bot, bharat, yugaware
    
    Differential Revision: https://phabricator.dev.yugabyte.com/D3736

--- managed/ui/src/components/panels/ResourceStringPanel/ResourceStringPanel.js
+++ managed/ui/src/components/panels/ResourceStringPanel/ResourceStringPanel.js
@@ -9,9 +9,9 @@ export default class ResourceStringPanel extends Component {
     const { universeInfo, universeInfo: {universeDetails: {clusters}} } = this.props;
     const primaryCluster = getPrimaryCluster(clusters);
     const userIntent = primaryCluster && primaryCluster.userIntent;
-    const regionList = universeInfo.regions && universeInfo.regions.map((region) => region.name).join(", ");
+    const regionList = primaryCluster.regions && primaryCluster.regions.map((region) => region.name).join(", ");
     const connectStringPanelItems = [
-      {name: "Provider", data: universeInfo.provider && universeInfo.provider.name},
+      {name: "Provider", data: userIntent.providerType && userIntent.providerType},
       {name: "Regions", data: regionList},
       {name: "Instance Type", data: userIntent && userIntent.instanceType},
       {name: "Replication Factor", data: userIntent.replicationFactor},
