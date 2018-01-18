eefd312966bfba29e4b7963c172b5179fbc0b605
Oleg Loginov <olegloginov@users.noreply.github.com>
2018-01-18 20:41:24 +0300

    This commit provides additional functionality that is logically related to the earlier commit https://github.com/YugaByte/yugabyte-db/commit/be456121c6fb2257066ce7dc4cf0ab6193a8080f
    Original commit message:
    
    Sending snapshot's files together with the tablet in case of remote bootstrap.
    
    Summary: Extended list of files to transfer in the remote bootstrap code.
    
    Test Plan: tests-tserver/remote_bootstrap_rocksdb_client-test
    
    Reviewers: mikhail
    
    Reviewed By: mikhail
    
    Subscribers: bogdan, bharat, eng
    
    Differential Revision: https://phabricator.dev.yugabyte.com/D3929

---
+++ ent/src/yb/tserver/remote_bootstrap_rocksdb_session-test_ent.cc
@@ -0,0 +1,71 @@
+// Copyright (c) YugaByte, Inc.
+
+#include "yb/tserver/remote_bootstrap_session-test.h"
+#include "yb/tablet/operations/snapshot_operation.h"
+
+namespace yb {
+namespace tserver {
+
+using std::string;
+
+class RemoteBootstrapRocksDBTest : public RemoteBootstrapTest {
+ public:
+  RemoteBootstrapRocksDBTest() : RemoteBootstrapTest(YQL_TABLE_TYPE) {}
+
+  void InitSession() override {
+    CreateSnapshot();
+    RemoteBootstrapTest::InitSession();
+  }
+
+  void CreateSnapshot() {
+    LOG(INFO) << "Creating Snapshot " << snapshot_id_ << " ...";
+    TabletSnapshotOpRequestPB request;
+    request.set_snapshot_id(snapshot_id_);
+    tablet::SnapshotOperationState tx_state(tablet().get(), &request);
+    ASSERT_OK(tablet()->CreateSnapshot(&tx_state));
+  }
+
+ protected:
+  const string snapshot_id_ = "0123456789ABCDEF0123456789ABCDEF";
+};
+
+TEST_F(RemoteBootstrapRocksDBTest, CheckSuperBlockHasSnapshotFields) {
+  auto superblock = session_->tablet_superblock();
+  LOG(INFO) << superblock.ShortDebugString();
+  ASSERT_TRUE(superblock.table_type() == YQL_TABLE_TYPE);
+  ASSERT_TRUE(superblock.has_rocksdb_dir());
+
+  const string& rocksdb_dir = superblock.rocksdb_dir();
+  ASSERT_TRUE(env_->FileExists(rocksdb_dir));
+
+  const string top_snapshots_dir = JoinPathSegments(rocksdb_dir,
+                                                    tablet::enterprise::kSnapshotsDirName);
+  ASSERT_TRUE(env_->FileExists(top_snapshots_dir));
+
+  const string snapshot_dir = JoinPathSegments(top_snapshots_dir, snapshot_id_);
+  ASSERT_TRUE(env_->FileExists(snapshot_dir));
+
+  vector<string> snapshot_files;
+  ASSERT_OK(env_->GetChildren(snapshot_dir, &snapshot_files));
+
+  // Ignore "." and ".." entries in snapshot_dir.
+  ASSERT_EQ(superblock.snapshot_files().size(), snapshot_files.size() - 2);
+
+  for (int i = 0; i < superblock.snapshot_files().size(); ++i) {
+    const string& snapshot_id = superblock.snapshot_files(i).snapshot_id();
+    const string& snapshot_file_name = superblock.snapshot_files(i).name();
+    const uint64_t snapshot_file_size_bytes = superblock.snapshot_files(i).size_bytes();
+
+    ASSERT_EQ(snapshot_id, snapshot_id_);
+
+    const string file_path = JoinPathSegments(snapshot_dir, snapshot_file_name);
+    ASSERT_TRUE(env_->FileExists(file_path));
+
+    uint64 file_size_bytes = 0;
+    ASSERT_OK(env_->GetFileSize(file_path, &file_size_bytes));
+    ASSERT_EQ(snapshot_file_size_bytes, file_size_bytes);
+  }
+}
+
+}  // namespace tserver
+}  // namespace yb
