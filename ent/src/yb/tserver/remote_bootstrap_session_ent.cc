79cd374b6673d07a63d7347608d64f49e3d76a29
Oleg Loginov <olegloginov@users.noreply.github.com>
2018-01-18 20:41:24 +0300

    This commit provides additional functionality that is logically related to the earlier commit https://github.com/YugaByte/yugabyte-db/commit/be456121c6fb2257066ce7dc4cf0ab6193a8080f
    Original commit message:
    
    Sending snapshot's files together with the tablet in case of remote bootstrap.
    
    Summary: Extended list of files to transfer in the remote bootstrap code.
    
    Test Plan: tests-tserver/remote_bootstrap_rocksdb_client-test
    
    Reviewers: mikhail
    
    Reviewed By: mikhail
    
    Subscribers: bogdan, bharat, eng
    
    Differential Revision: https://phabricator.dev.yugabyte.com/D3929

---
+++ ent/src/yb/tserver/remote_bootstrap_session_ent.cc
@@ -0,0 +1,79 @@
+// Copyright (c) YugaByte, Inc.
+
+#include "yb/tserver/remote_bootstrap_session.h"
+
+#include "yb/tablet/tablet.h"
+#include "yb/tablet/tablet_peer.h"
+
+namespace yb {
+namespace tserver {
+namespace enterprise {
+
+using std::string;
+using std::vector;
+
+using strings::Substitute;
+using tablet::TabletMetadata;
+using tablet::TabletPeer;
+
+Status RemoteBootstrapSession::InitSnapshotFiles() {
+  const scoped_refptr<TabletMetadata>& metadata = tablet_peer_->tablet_metadata();
+  tablet_superblock_.clear_snapshot_files();
+
+  // Add snapshot files to tablet superblock.
+  const string top_snapshots_dir = JoinPathSegments(tablet_superblock_.rocksdb_dir(),
+                                                    tablet::enterprise::kSnapshotsDirName);
+
+  vector<string> snapshots;
+  if (metadata->fs_manager()->env()->FileExists(top_snapshots_dir)) {
+    RETURN_NOT_OK_PREPEND(metadata->fs_manager()->ListDir(top_snapshots_dir, &snapshots),
+                          Substitute("Unable to list directory $0", top_snapshots_dir));
+  }
+
+  for (const string& snapshot_id : snapshots) {
+    if (snapshot_id == "." || snapshot_id == "..") {
+      continue;
+    }
+
+    const string snapshot_dir = JoinPathSegments(top_snapshots_dir, snapshot_id);
+    vector<string> files;
+    RETURN_NOT_OK_PREPEND(metadata->fs_manager()->ListDir(snapshot_dir, &files),
+                          Substitute("Unable to list directory $0", snapshot_dir));
+
+    for (const string& file : files) {
+      if (file == "." || file == "..") {
+        continue;
+      }
+
+      const string path = JoinPathSegments(snapshot_dir, file);
+      uint64_t file_size = 0;
+      RETURN_NOT_OK_PREPEND(metadata->fs_manager()->env()->GetFileSize(path, &file_size),
+                            Substitute("Unable to get file size for file $0", path));
+
+      auto snapshot_file_pb = tablet_superblock_.mutable_snapshot_files()->Add();
+      snapshot_file_pb->set_name(file);
+      snapshot_file_pb->set_snapshot_id(snapshot_id);
+      snapshot_file_pb->set_size_bytes(file_size);
+    }
+  }
+
+  return Status::OK();
+}
+
+Status RemoteBootstrapSession::GetSnapshotFilePiece(const std::string snapshot_id,
+                                                    const std::string file_name,
+                                                    uint64_t offset,
+                                                    int64_t client_maxlen,
+                                                    std::string* data,
+                                                    int64_t* log_file_size,
+                                                    RemoteBootstrapErrorPB::Code* error_code) {
+  const string snapshots_dir = JoinPathSegments(tablet_superblock_.rocksdb_dir(),
+                                                tablet::enterprise::kSnapshotsDirName);
+  return GetFilePiece(
+      JoinPathSegments(snapshots_dir, snapshot_id), file_name, offset,
+      client_maxlen, data, log_file_size, error_code);
+}
+
+} // namespace enterprise
+} // namespace tserver
+} // namespace yb
