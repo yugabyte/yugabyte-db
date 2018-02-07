d9468c18f47b08d248e75afddd2f7305e5141581
Oleg Loginov <olegloginov@users.noreply.github.com>
2018-02-07 17:00:46 +0300

    This commit provides additional functionality that is logically related to the earlier commit https://github.com/YugaByte/yugabyte-db/commit/54121c07e927cb8f0bad7169cc3fc26faba65d71
    Original commit message:
    
    Added tests for ImportSnapshotMeta.
    
    Summary: new tests
    
    Test Plan: ./latest/tests-integration-tests/snapshot-test --gtest_filter=*ImportSnapshotMeta
    
    Reviewers: pritam.damania, mikhail
    
    Reviewed By: pritam.damania
    
    Subscribers: pritam.damania, bogdan, ybase, bharat
    
    Differential Revision: https://phabricator.dev.yugabyte.com/D4073

---
+++ ent/src/yb/integration-tests/external_mini_cluster_ent.cc
@@ -0,0 +1,24 @@
+// Copyright (c) YugaByte, Inc.
+
+#include "yb/integration-tests/external_mini_cluster.h"
+
+#include "yb/master/master_backup.proxy.h"
+
+namespace yb {
+namespace enterprise {
+
+using yb::master::MasterBackupServiceProxy;
+
+std::shared_ptr<MasterBackupServiceProxy> ExternalMiniCluster::master_backup_proxy() {
+  CHECK_EQ(masters_.size(), 1);
+  return master_backup_proxy(0);
+}
+
+std::shared_ptr<MasterBackupServiceProxy> ExternalMiniCluster::master_backup_proxy(int idx) {
+  CHECK_LT(idx, masters_.size());
+  return std::make_shared<MasterBackupServiceProxy>(
+      messenger_, CHECK_NOTNULL(master(idx))->bound_rpc_addr());
+}
+
+} // namespace enterprise
+} // namespace yb
