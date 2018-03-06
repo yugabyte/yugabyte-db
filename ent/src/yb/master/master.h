c8f2016c0ccc77cb42ad709803f5d36e2881fe05
Timur Yusupov <ttyusupov@users.noreply.github.com>
2018-03-06 14:39:27 +0300

    This commit provides additional functionality that is logically related to the earlier commit https://github.com/YugaByte/yugabyte-db/commit/a44baa11c86258e2cbdb6dcd8c84d5a32fa302e3
    Original commit message:
    
    ENG-2726: Managed DNS to get the endpoints for a YB EE universe.
    
    Summary: https://yugabyte.atlassian.net/browse/ENG-2726
    
    Test Plan:
    - Configure permissions as described in ENG-2726.
    - `export YB_LOCAL_MASTER_EXTRA_PARAMS="--tservers_domain_name <DOMAIN_NAME> --tservers_route53_hosted_zone_id <HOSTED_ZONE_ID>"`
    - `local_cluster.ctl start`
    - Make sure tserver addresses are appeared in Route53.
    - Send STOP signal to one of tservers, it should disappear from Route53 once it no longer considered
      as live (one minute timeout).
    - Send CONT signal to stopped tserver, it should appear again in Route53 in 5-10 seconds.
    - Also check with `nslookup <DOMAIN_NAME>`
    
    Reviewers: mikhail, bogdan, sergei
    
    Reviewed By: sergei
    
    Subscribers: ybase, bharat
    
    Differential Revision: https://phabricator.dev.yugabyte.com/D4250

--- ent/src/yb/master/master.h
+++ ent/src/yb/master/master.h
@@ -8,12 +8,15 @@ namespace master {
 namespace enterprise {
 
 class CatalogManager;
+class TSDescriptor;
 
 } // namespace enterprise
 } // namespace master
 } // namespace yb
 
 #include "../../../../src/yb/master/master.h"
+
+#include "yb/master/dns_manager.h"
 
 namespace yb {
 namespace master {
@@ -23,12 +26,21 @@ class Master : public yb::master::Master {
   typedef yb::master::Master super;
  public:
   explicit Master(const MasterOptions& opts) : super(opts) {}
+  Master(const Master&) = delete;
+  void operator=(const Master&) = delete;
+
+  // Periodically updates tservers endpoints in DNS if list of live tservers changed.
+  void OnTSHeartbeat(
+      const std::vector<std::shared_ptr<yb::master::TSDescriptor>>& live_tservers) override;
 
  protected:
   CHECKED_STATUS RegisterServices() override;
 
  private:
-  DISALLOW_COPY_AND_ASSIGN(Master);
+  DnsManager dns_manager_;
+  // The last time when DNS manager was called to update list of tserver hosts if needed.
+  std::atomic<MonoTime> last_dns_sync_{MonoTime::Min()};
+  std::atomic<bool> dns_sync_in_progress_{false};
 };
 
 } // namespace enterprise
