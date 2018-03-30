9b7c89dfb112764bebfc4bcb238e49c219773e00
Venkatesh Radhakrishnan <rven1@users.noreply.github.com>
2018-03-29 18:15:27 -0700

    Specify python version to build script
    Summary:
    If python3 is installed on the system, the script
    build-support/gen_version_info.py fails. So specifically forcing the
    python version.
    
    Test Plan: Jenkins
    
    Reviewers: bogdan, mikhail
    
    Reviewed By: mikhail
    
    Subscribers: ybase
    
    Differential Revision: https://phabricator.dev.yugabyte.com/D4521

--- build-support/gen_version_info.py
+++ build-support/gen_version_info.py
@@ -1,5 +1,5 @@
-#!/usr/bin/env python
-#
+#!/usr/bin/env python2.7
+
 # Licensed to the Apache Software Foundation (ASF) under one
 # or more contributor license agreements.  See the NOTICE file
 # distributed with this work for additional information
