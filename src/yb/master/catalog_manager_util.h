af7eea55b75fe8b9b1b7b660262c3f904d32c641
Bharat Baddepudi <bbaddepudi@users.noreply.github.com>
2018-03-30 18:18:48 -0700

    ENG-3143: Load Balancer gets stuck at 70% during cross AZ expand.
    Summary:
    Issue is that the load balancer ensures even load across AZ's, but IsLoadBalanced was checking even load across tservers. So when a single tserver is added (via expand), the load from the other tserver in that AZ is moved to and distribution is not similar across tservers. Fix is to check load uniformity across tservers per AZ.
    
    All the java/c++ minicluster tests are creating clusters using default placement az, so issue never got detected. And itest/java test is going 3->6 so load is even among tservers as they are evenly spread across 3 AZs. Added unit test for now.
    
    Test Plan:
    Added unit tests.
    
    Jenkins
    
    Reviewers: bogdan, rahuldesirazu, pritam.damania
    
    Reviewed By: rahuldesirazu
    
    Subscribers: mikhail, kannan, ybase
    
    Differential Revision: https://phabricator.dev.yugabyte.com/D4529

---
+++ src/yb/master/catalog_manager_util.h
@@ -0,0 +1,50 @@
+// Copyright (c) YugaByte, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+// in compliance with the License.  You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software distributed under the License
+// is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+// or implied.  See the License for the specific language governing permissions and limitations
+// under the License.
+//
+
+#ifndef YB_MASTER_CATALOG_MANAGER_UTIL_H
+#define YB_MASTER_CATALOG_MANAGER_UTIL_H
+
+#include <unordered_map>
+#include <vector>
+
+#include "yb/master/ts_descriptor.h"
+#include "yb/master/ts_manager.h"
+#include "yb/util/status.h"
+
+// Utility functions that can be shared between test and code for catalog manager.
+namespace yb {
+namespace master {
+
+using ZoneToDescMap = std::unordered_map<string, TSDescriptorVector>;
+
+class CatalogManagerUtil {
+ public:
+  // For the given set of descriptors, checks if the load is considered balanced across AZs in
+  // multi AZ setup, else checks load distribution across tservers (single AZ).
+  static CHECKED_STATUS IsLoadBalanced(const master::TSDescriptorVector& ts_descs);
+
+  // For the given set of descriptors, returns the map from each placement AZ to list of tservers
+  // running in that zone.
+  static CHECKED_STATUS GetPerZoneTSDesc(const TSDescriptorVector& ts_descs,
+                                         ZoneToDescMap* zone_to_ts);
+
+ private:
+  CatalogManagerUtil();
+
+  DISALLOW_COPY_AND_ASSIGN(CatalogManagerUtil);
+};
+
+} // namespace master
+} // namespace yb
+
+#endif // YB_MASTER_CATALOG_MANAGER_UTIL_H
