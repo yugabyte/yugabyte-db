7a734bf2aa78c0921779f5b3065b323b6ecefe39
Sergei Politov <spolitov@users.noreply.github.com>
2018-04-19 09:56:16 +0300

    ENG-2869: Allow integration of distributed txns with NTP clocks such as AWS time sync service
    Summary:
    Adapt physical clock interface to provide max global now.
    I.e. maximal possible time that could be in any server in cluster.
    
    Enterprise version:
    Added NTP Clock, that is suitable for cases when we have good NTP synchronization.
    
    Test Plan: Jenkins
    
    Reviewers: bogdan, pritam.damania, mikhail
    
    Reviewed By: mikhail
    
    Subscribers: ybase
    
    Differential Revision: https://phabricator.dev.yugabyte.com/D4582
    
    Note:
    This commit provides additional functionality that is logically related to
    the earlier commit https://github.com/YugaByte/yugabyte-db/commit/7b12cce17745647e1870f7f7b44a9a71068ceecd
    and supersedes the commit https://github.com/YugaByte/yugabyte-db/commit/8e84fa56cafa3a47ffafa46f174ac22b0ea80326

--- ent/src/yb/tserver/tablet_server_ent.cc
+++ ent/src/yb/tserver/tablet_server_ent.cc
@@ -1,8 +1,11 @@
 // Copyright (c) YugaByte, Inc.
 
+#include "yb/server/hybrid_clock.h"
+
 #include "yb/tserver/tablet_server.h"
 #include "yb/tserver/backup_service.h"
 #include "yb/util/flag_tags.h"
+#include "yb/util/ntp_clock.h"
 
 DEFINE_int32(ts_backup_svc_num_threads, 4,
              "Number of RPC worker threads for the TS backup service");
@@ -19,6 +22,10 @@ namespace enterprise {
 using yb::rpc::ServiceIf;
 
 Status TabletServer::RegisterServices() {
+  server::HybridClock::RegisterProvider(NtpClock::Name(), [] {
+    return std::make_shared<NtpClock>();
+  });
+
   std::unique_ptr<ServiceIf> backup_service(
       new TabletServiceBackupImpl(tablet_manager_.get(), metric_entity()));
   RETURN_NOT_OK(RpcAndWebServerBase::RegisterService(FLAGS_ts_backup_svc_queue_length,
