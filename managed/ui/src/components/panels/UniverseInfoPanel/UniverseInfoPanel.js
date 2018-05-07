88755664a7b8230f029621f2fe49656387f7a7f2
Ramkumar Vaidyanathan <ramkumarvs@users.noreply.github.com>
2018-05-07 14:31:58 -0700

    Add YEDIS service endpoint
    Summary:
    - Rename CQL service endpoint to YCQL
    - Add YEDIS service endpoint
    
    {F9489}
    
    Test Plan:
    npm start
    go to universe tab and validated
    click on both the endpoints to make sure the data is correct
    
    Reviewers: karthik, vit.pankin, sid
    
    Reviewed By: sid
    
    Subscribers: yugaware
    
    Differential Revision: https://phabricator.dev.yugabyte.com/D4758

--- managed/ui/src/components/panels/UniverseInfoPanel/UniverseInfoPanel.js
+++ managed/ui/src/components/panels/UniverseInfoPanel/UniverseInfoPanel.js
@@ -3,7 +3,7 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { FormattedDate } from 'react-intl';
-import { ROOT_URL } from '../../../config';
+import { getUniverseEndpoint } from 'actions/common';
 import { DescriptionList, YBCopyButton } from '../../common/descriptors';
 import { FlexContainer, FlexGrow, FlexShrink } from '../../common/flexbox/YBFlexBox';
 import { getPrimaryCluster } from "../../../utils/UniverseUtils";
@@ -13,29 +13,32 @@ export default class UniverseInfoPanel extends Component {
     universeInfo: PropTypes.object.isRequired
   };
 
+  renderEndpointUrl = (endpointUrl) => {
+    return (
+      <a href={endpointUrl} target="_blank" rel="noopener noreferrer">Endpoint &nbsp;
+        <i className="fa fa-external-link" aria-hidden="true"></i>
+      </a>
+    );
+  }
+
   render() {
-    const { universeInfo, universeInfo: {universeDetails: {clusters}}, currentCustomer } = this.props;
+    const { universeInfo, universeInfo: {universeDetails: {clusters}} } = this.props;
     const primaryCluster = getPrimaryCluster(clusters);
     const userIntent = primaryCluster && primaryCluster.userIntent;
     const universeId = universeInfo.universeUUID;
-    const customerId = currentCustomer.data.uuid;
     const formattedCreationDate = (
       <FormattedDate value={universeInfo.creationDate} year='numeric' month='long' day='2-digit'
                      hour='2-digit' minute='2-digit' second='2-digit' timeZoneName='short' />
     );
     const universeIdData = <FlexContainer><FlexGrow style={{overflow: 'hidden', textOverflow: 'ellipsis'}}>{universeId}</FlexGrow><FlexShrink><YBCopyButton text={universeId}/></FlexShrink></FlexContainer>;
-    const endpointUrl = ROOT_URL + "/customers/" + customerId +
-                        "/universes/" + universeId + "/yqlservers";
-    const endpoint = (
-      <a href={endpointUrl} target="_blank" rel="noopener noreferrer">Endpoint &nbsp;
-        <i className="fa fa-external-link" aria-hidden="true"></i>
-      </a>
-    );
+    const ycqlServiceUrl = getUniverseEndpoint(universeId) + "/yqlservers";
+    const yedisServiceUrl = getUniverseEndpoint(universeId) + "/redisservers";
     const universeInfoItems = [
       {name: "Universe ID", data: universeIdData},
       {name: "Launch Time", data: formattedCreationDate},
-      {name: "CQL Service", data: endpoint},
-      {name: "YugaByte Version", data: userIntent.ybSoftwareVersion || 'n/a'},
+      {name: "YCQL Service", data: this.renderEndpointUrl(ycqlServiceUrl)},
+      {name: "YEDIS Service", data: this.renderEndpointUrl(yedisServiceUrl)},
+      {name: "YugaByte DB Version", data: userIntent.ybSoftwareVersion || 'n/a'},
     ];
 
     if (userIntent.providerType === "aws") {
