5ec17a416cbf54a912851220b6472002ae4a7d45
Vit Pankin <vitpankin@users.noreply.github.com>
2018-05-07 18:04:07 -0700

    Customer Registration Form updates
    Summary: Customer Registration Form updates
    
    Test Plan: npm start and validate
    
    Reviewers: vit.pankin
    
    Reviewed By: vit.pankin
    
    Subscribers: jenkins-bot, yugaware
    
    Differential Revision: https://phabricator.dev.yugabyte.com/D4755

--- managed/ui/src/components/common/forms/fields/index.js
+++ managed/ui/src/components/common/forms/fields/index.js
@@ -8,7 +8,7 @@ export { default as YBCheckBox } from './YBCheckBox';
 export { default as YBMultiSelectWithLabel, YBNewMultiSelect, YBMultiSelect } from './YBMultiSelect';
 export { default as YBNumericInputWithLabel, YBNumericInput, YBControlledNumericInputWithLabel, YBControlledNumericInput, YBUnControlledNumericInput } from './YBNumericInput';
 export { default as YBRadioButton } from './YBRadioButton';
-export { default as YBRadioButtonBar, YBRadioButtonBarWithLabel } from './YBRadioButtonBar';
+export { default as YBRadioButtonBar, YBRadioButtonBarDefault, YBRadioButtonBarWithLabel, YBRadioButtonBarDefaultWithLabel } from './YBRadioButtonBar';
 export { default as YBSelectWithLabel, YBUnControlledSelect, YBControlledSelect, YBControlledSelectWithLabel, YBSelect } from './YBSelect';
 export { default as YBTextInputWithLabel, YBTextInput, YBInputField, YBControlledTextInput } from './YBTextInput';
 export { default as YBTextArea} from './YBTextArea';
