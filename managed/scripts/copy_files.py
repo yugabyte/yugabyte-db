df5962279393e9d6f49b1d4288efd3881cfa80f9
Bharat Baddepudi <bbaddepudi@users.noreply.github.com>
2018-05-30 21:49:58 +0000

    ENG-3319: Perform verify cqlsh after each edit step, more debug logging.
    Summary:
    To have further help in debugging such issues down the line. Added sleep after any blacklist operations for allowing master to account for dead tservers.
    
    Still commented the cqlsh checks after restore for now. Will debug further after other priorities (for read replica).
    
    Merging this in to get debug info in along with new learnings/changes.
    
    Test Plan:
    `~/code/yugaware/run_itest --perform_edits --perform_backup --use_latest_deploy --run_universe_test aws`
    
    First run output at gcp-scheduler:/home/centos/logs/20180530_200129.log
    
    Next run 10.150.0.30:/home/centos/logs/20180530_222237.log : ran into #314
    
    New run at 10.150.0.30:/home/centos/logs/20180531_054410.log : after killing master leader, ran into #294
    
    Move-decom-to-end run at  10.150.0.30:/home/centos/logs/20180601_195817.log : ran into ENG-3404
    
    `~/code/yugaware/run_itest --perform_edits --perform_backup --use_latest_deploy`
    Final run at 10.150.0.30 :/home/centos/logs/20180606_202206.log
    
    Reviewers: bogdan, oleg
    
    Reviewed By: oleg
    
    Subscribers: jenkins-bot, ybase
    
    Differential Revision: https://phabricator.dev.yugabyte.com/D4910

--- managed/scripts/copy_files.py
+++ managed/scripts/copy_files.py
@@ -8,8 +8,9 @@ import subprocess
 
 from datetime import datetime
 
-# Given a set of servers and the private access key, this script copies all the tserver and master logs
-# to a latest timestamp based directory.
+# Given a set of servers and the private access key, this script copies all the tserver and master
+# logs to a latest timestamp based directory.
+
 
 def main():
     timestamp = datetime.now().strftime('%Y%m%d_%H%M%S')
@@ -25,18 +26,24 @@ def main():
     os.makedirs(out_dir)
 
     port = "22" if "onprem" in args.private_key else "54422"
-    scp_base = ["sudo", "/usr/bin/scp", "-oStrictHostKeyChecking=no", "-P", port, "-i", args.private_key]
+    scp_base = ["sudo", "/usr/bin/scp", "-oStrictHostKeyChecking=no", "-P", port, "-i",
+                args.private_key]
     user_id = "yugabyte@"
-    master_files=":/home/yugabyte/master/logs/yb-master.*log*"
-    tserver_files=":/home/yugabyte/tserver/logs/yb-tserver.*log*"
+    master_files = ":/home/yugabyte/master/logs/yb-master.*log*"
+    tserver_files = ":/home/yugabyte/tserver/logs/yb-tserver.*log*"
 
     for ip in args.servers.split(","):
-        scp_cmd = scp_base + [user_id + ip + master_files, out_dir]
-        subprocess.check_output(scp_cmd)
+        try:
+            scp_cmd = scp_base + [user_id + ip + master_files, out_dir]
+            subprocess.check_output(scp_cmd)
+        except Exception as e:
+            print "Ignoring error " + e.output
+            pass
         scp_cmd = scp_base + [user_id + ip + tserver_files, out_dir]
         subprocess.check_output(scp_cmd)
 
     logging.info("Files copied to : {}".format(out_dir))
+
 
 if __name__ == "__main__":
     main()
