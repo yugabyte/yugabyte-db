7ca2b370fab8f1a62e9d70d76fe91e0396f2cf92
Vit Pankin <vitpankin@users.noreply.github.com>
2018-06-09 20:54:06 +0300

    Metrics loads 2 first sections
    Summary:
    Metrics loads 2 first sections
    Small layout update - Graph Panel bottom padding
    
    Test Plan: Open metrics panel and test whether exactly needed sections are loading.
    
    Reviewers: karthik, ram
    
    Reviewed By: ram
    
    Subscribers: jenkins-bot, ram, rao
    
    Differential Revision: https://phabricator.dev.yugabyte.com/D4954

--- managed/ui/src/components/metrics/CustomerMetricsPanel/CustomerMetricsPanel.js
+++ managed/ui/src/components/metrics/CustomerMetricsPanel/CustomerMetricsPanel.js
@@ -6,9 +6,18 @@ import PropTypes from 'prop-types';
 import { PanelGroup } from 'react-bootstrap';
 
 const graphPanelTypes = {
-  "universe": ['proxies', 'server', 'cql', 'redis', 'tserver', 'lsmdb'],
-  "customer": ['proxies', 'server', 'cql', 'redis', 'tserver', 'lsmdb'],
-  "table": ['lsmdb_table', 'tserver_table']
+  "universe": {
+    data: ['proxies', 'server', 'cql', 'redis', 'tserver', 'lsmdb'],
+    isOpen: [true, true, false, false, false]
+  },
+  "customer": {
+    data: ['proxies', 'server', 'cql', 'redis', 'tserver', 'lsmdb'],
+    isOpen: [true, true, false, false, false]
+  },
+  "table": {
+    data: ['lsmdb_table', 'tserver_table'],
+    isOpen: [true, true]
+  },
 };
 
 export default class CustomerMetricsPanel extends Component {
@@ -27,8 +36,8 @@ export default class CustomerMetricsPanel extends Component {
 
   render() {
     const { origin, nodePrefixes, width, tableName } = this.props;
-    const graphPanelContainers = graphPanelTypes[origin].map(function (type, idx) {
-      return (<GraphPanelContainer key={idx} isOpen={idx?false:true} type={type} width={width}
+    const graphPanelContainers = graphPanelTypes[origin].data.map(function (type, idx) {
+      return (<GraphPanelContainer key={idx} isOpen={graphPanelTypes[origin].isOpen[idx]} type={type} width={width}
                   nodePrefixes={nodePrefixes} tableName={tableName} />);
     });
 
