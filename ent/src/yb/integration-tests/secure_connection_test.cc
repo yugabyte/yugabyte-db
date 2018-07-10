4eec37523e070c31c3b0e6c9782a24ec68eb86b5
Sergei Politov <spolitov@users.noreply.github.com>
2018-07-10 22:00:15 +0300

    This commit provides additional functionality that is logically related to the earlier commit https://github.com/YugaByte/yugabyte-db/commit/fffd1433c10df2ec15371526163384b4018fe0cc
    Original commit message:
    
    ENG-3546: CQL TLS Encryption
    
    Summary:
    Support for TLS encryption for CQL connections.
    
    Introduced flags:
    use_client_to_server_encryption - Use client to server encryption
    
    certs_for_client_dir -
        Directory that contains certificate authority, private key and certificates for
        this server that should be used for client to server communications.
        When empty, the same dir as for server to server communications is used.
    
    Test Plan: Jenkins
    
    Reviewers: mikhail, pritam.damania
    
    Reviewed By: pritam.damania
    
    Subscribers: karthik, bogdan, ybase, bharat
    
    Differential Revision: https://phabricator.dev.yugabyte.com/D5109

--- ent/src/yb/integration-tests/secure_connection_test.cc
+++ ent/src/yb/integration-tests/secure_connection_test.cc
@@ -14,11 +14,19 @@
 #include "yb/client/ql-dml-test-base.h"
 #include "yb/client/table_handle.h"
 
-#include "yb/yql/cql/ql/util/errcodes.h"
-#include "yb/yql/cql/ql/util/statement_result.h"
+#include "yb/rpc/messenger.h"
+#include "yb/rpc/secure_stream.h"
+
+#include "yb/server/secure.h"
+
 #include "yb/util/env_util.h"
 
+#include "yb/yql/cql/ql/util/errcodes.h"
+#include "yb/yql/cql/ql/util/statement_result.h"
+
+DECLARE_bool(use_client_to_server_encryption);
 DECLARE_bool(use_node_to_node_encryption);
+DECLARE_bool(allow_insecure_connections);
 DECLARE_string(certs_dir);
 
 namespace yb {
@@ -30,6 +38,8 @@ class SecureConnectionTest : public client::KeyValueTableTest {
 
   void SetUp() override {
     FLAGS_use_node_to_node_encryption = true;
+    FLAGS_use_client_to_server_encryption = true;
+    FLAGS_allow_insecure_connections = false;
     std::string test_executable_path;
     const auto sub_dir = JoinPathSegments("ent", "test_certs");
     auto root_dir = env_util::GetRootDir(sub_dir);
@@ -39,9 +49,23 @@ class SecureConnectionTest : public client::KeyValueTableTest {
 
     CreateTable(client::Transactional::kFalse);
   }
+
+  CHECKED_STATUS CreateClient() override {
+    rpc::MessengerBuilder messenger_builder("test_client");
+    secure_context_ = VERIFY_RESULT(server::SetupSecureContext(
+        "", "", server::SecureContextType::kClientToServer, &messenger_builder));
+    auto messenger = VERIFY_RESULT(messenger_builder.Build());
+    client::YBClientBuilder builder;
+    builder.use_messenger(messenger);
+    return cluster_->CreateClient(&builder, &client_);
+  }
+
+  std::unique_ptr<rpc::SecureContext> secure_context_;
 };
 
 TEST_F(SecureConnectionTest, Simple) {
+  DontVerifyClusterBeforeNextTearDown(); // Verify requires insecure connection.
+
   const int32_t kKey = 1;
   const int32_t kValue = 2;
 
