yugabyte/yugabyte-db

commit 1fd1b5a612f55a903c8f94b25bd2e9e41bc0e861

[ENG-1735] #283 Implement Auth support for Redis
Summary:
Implement support for the following commands

  - Auth <password>, and
  - Config SET REQUIERPASS <password>

The config-set-requirepass command is used to set a password for redis. The password information is stored in the master's sys catalog table as a proto. The proto supports a generic redis configuration settings, by essentially acting as a string -> string map. For now, we only use the requirepass key to map to the password,  if set.

We also support caching the password in the proxy for a configurable amount of time, so that the calls to the master are reduced.

Test Plan: Jenkins. Added unit test

Reviewers: hector, karthik, sergei, kannan, bogdan

Reviewed By: bogdan

Subscribers: oleg, mikhail, ybase, yql

Differential Revision: https://phabricator.dev.yugabyte.com/D4778

src/yb/yql/redis/redisserver/redis_commands.cc
@@ -13,6 +13,7 @@
 
 #include "yb/yql/redis/redisserver/redis_commands.h"
 
+#include <boost/algorithm/string.hpp>
 #include <boost/preprocessor/seq/for_each.hpp>
 #include <boost/preprocessor/stringize.hpp>
 
@@ -26,8 +27,10 @@
 #include "yb/rpc/messenger.h"
 #include "yb/rpc/scheduler.h"
 
+#include "yb/util/crypt.h"
 #include "yb/util/metrics.h"
 #include "yb/util/stol_utils.h"
+#include "yb/util/string_util.h"
 
 #include "yb/yql/redis/redisserver/redis_constants.h"
 #include "yb/yql/redis/redisserver/redis_encoding.h"
@@ -36,7 +39,20 @@
 using namespace std::literals;
 using yb::client::YBTableName;
 
+namespace {
+static bool ValidateRedisPasswordSeparator(const char* flagname, const string& value) {
+  if (value.size() != 1) {
+    LOG(INFO) << "Expect " << flagname << " to be 1 character long";
+    return false;
+  }
+  return true;
+}
+}
+
 DEFINE_bool(yedis_enable_flush, true, "Enables FLUSHDB and FLUSHALL commands in yedis.");
+DEFINE_bool(use_hashed_redis_password, true, "Store the hash of the redis passwords instead.");
+DEFINE_string(redis_passwords_separator, ",", "The character used to separate multiple passwords.");
+DEFINE_validator(redis_passwords_separator, &ValidateRedisPasswordSeparator);
 
 namespace yb {
 namespace redisserver {
@@ -87,7 +103,7 @@ namespace redisserver {
     ((incr, Incr, 2, WRITE)) \
     ((incrby, IncrBy, 3, WRITE)) \
     ((echo, Echo, 2, LOCAL)) \
-    ((auth, Auth, -1, LOCAL)) \
+    ((auth, Auth, 2, LOCAL)) \
     ((config, Config, -1, LOCAL)) \
     ((info, Info, -1, LOCAL)) \
     ((role, Role, 1, LOCAL)) \
@@ -307,14 +323,6 @@ void HandleEcho(LocalCommandData data) {
   data.Respond(&response);
 }
 
-void HandleAuth(LocalCommandData data) {
-  data.Respond();
-}
-
-void HandleConfig(LocalCommandData data) {
-  data.Respond();
-}
-
 void AddElements(const RefCntBuffer& buffer, RedisArrayPB* array) {
   array->add_elements(buffer.data(), buffer.size());
 }
@@ -356,6 +364,74 @@ void HandleCommand(LocalCommandData data) {
 void HandleQuit(LocalCommandData data) {
   data.call()->MarkForClose();
   data.Respond();
+}
+
+bool AcceptPassword(const vector<string>& allowed, const string& candidate) {
+  for (auto& stored_hash_or_pwd : allowed) {
+    if (FLAGS_use_hashed_redis_password
+            ? (0 == yb::util::bcrypt_checkpw(candidate.c_str(), stored_hash_or_pwd.c_str()))
+            : (stored_hash_or_pwd == candidate)) {
+      return true;
+    }
+  }
+  return false;
+}
+
+void HandleConfig(LocalCommandData data) {
+  RedisResponsePB resp;
+  if (data.arg_size() != 4 ||
+      !(boost::iequals(data.arg(1).ToBuffer(), "SET") &&
+        boost::iequals(data.arg(2).ToBuffer(), "REQUIREPASS"))) {
+    data.Respond(&resp);
+    return;
+  }
+
+  // Handle Config Set Requirepass <passwords>
+  DCHECK_EQ(FLAGS_redis_passwords_separator.size(), 1);
+  vector<string> passwords =
+      yb::StringSplit(data.arg(3).ToBuffer(), FLAGS_redis_passwords_separator[0]);
+  Status status;
+  if (passwords.size() > 2) {
+    status = STATUS(InvalidArgument, "Only maximum of 2 passwords are supported");
+  } else if (FLAGS_use_hashed_redis_password) {
+    std::vector<string> hashes;
+    for (const auto& pwd : passwords) {
+      char hash[yb::util::kBcryptHashSize];
+      if (yb::util::bcrypt_hashpw(pwd.c_str(), hash) != 0) {
+        resp.set_code(RedisResponsePB_RedisStatusCode_SERVER_ERROR);
+        resp.set_error_message("ERR: Error while hashing the password.");
+        data.Respond(&resp);
+        return;
+      }
+      hashes.emplace_back(hash, yb::util::kBcryptHashSize);
+    }
+    status = data.client()->SetRedisPasswords(hashes);
+  } else {
+    status = data.client()->SetRedisPasswords(passwords);
+  }
+
+  if (!status.ok()) {
+    resp.set_code(RedisResponsePB_RedisStatusCode_SERVER_ERROR);
+    resp.set_error_message(StrCat("ERR: ", status.ToString()));
+  } else {
+    resp.set_code(RedisResponsePB::OK);
+  }
+  data.Respond(&resp);
+}
+
+void HandleAuth(LocalCommandData data) {
+  vector<string> passwords;
+  auto status = data.client()->GetRedisPasswords(&passwords);
+  RedisResponsePB resp;
+  if (!status.ok() || !AcceptPassword(passwords, data.arg(1).ToBuffer())) {
+    resp.set_code(RedisResponsePB_RedisStatusCode_SERVER_ERROR);
+    resp.set_error_message(strings::Substitute("ERR: Bad Password. $0", status.ToString()));
+  } else {
+    RedisConnectionContext& context = data.call()->connection_context();
+    context.set_authenticated(true);
+    resp.set_code(RedisResponsePB::OK);
+  }
+  data.Respond(&resp);
 }
 
 void HandleFlushDB(LocalCommandData data) {
@@ -429,7 +505,8 @@ void HandleDebugSleep(LocalCommandData data) {
 void RespondWithFailure(
     std::shared_ptr<RedisInboundCall> call,
     size_t idx,
-    const std::string& error) {
+    const std::string& error,
+    const char* redis_code) {
   // process the request
   DVLOG(4) << " Processing request from client ";
   const auto& command = call->client_batch()[idx];
@@ -441,7 +518,7 @@ void RespondWithFailure(
   // Send the result.
   DVLOG(4) << "Responding to call " << call->ToString() << " with failure " << error;
   std::string cmd = command[0].ToBuffer();
-  call->RespondFailure(idx, STATUS_FORMAT(InvalidCommand, "ERR $0: $1", cmd, error));
+  call->RespondFailure(idx, STATUS_FORMAT(InvalidCommand, "$0 $1: $2", redis_code, cmd, error));
 }
 
 void FillRedisCommands(const scoped_refptr<MetricEntity>& metric_entity,

java/yb-jedis-tests/src/test/java/com/yugabyte/jedis/TestYBJedis.java
@@ -300,7 +300,7 @@ public class TestYBJedis extends BaseJedisTest {
     config.setMaxTotal(16);
     config.setTestOnBorrow(true);
     JedisPool pool = new JedisPool(
-        config, address.getHostName(), address.getPort(), 10000, "password");
+        config, address.getHostName(), address.getPort(), 10000);
     Jedis jedis = pool.getResource();
     assertEquals("OK", jedis.set("k1", "v1"));
     assertEquals("v1", jedis.get("k1"));

src/yb/client/client-internal.cc
@@ -256,6 +256,8 @@ YB_CLIENT_SPECIALIZE_SIMPLE(AlterRole);
 YB_CLIENT_SPECIALIZE_SIMPLE(DeleteRole);
 YB_CLIENT_SPECIALIZE_SIMPLE(GrantRole);
 YB_CLIENT_SPECIALIZE_SIMPLE(GrantPermission);
+YB_CLIENT_SPECIALIZE_SIMPLE(RedisConfigSet);
+YB_CLIENT_SPECIALIZE_SIMPLE(RedisConfigGet);
 // These are not actually exposed outside, but it's nice to auto-add using directive.
 YB_CLIENT_SPECIALIZE_SIMPLE(AlterTable);
 YB_CLIENT_SPECIALIZE_SIMPLE(ChangeMasterClusterConfig);

src/yb/client/client.cc
@@ -83,6 +83,7 @@ using yb::master::CreateTableResponsePB;
 using yb::master::DeleteTableRequestPB;
 using yb::master::DeleteTableResponsePB;
 using yb::master::GetTableSchemaRequestPB;
+using yb::master::GetTableSchemaRequestPB;
 using yb::master::GetTableSchemaResponsePB;
 using yb::master::GetTableLocationsRequestPB;
 using yb::master::GetTableLocationsResponsePB;
@@ -123,6 +124,10 @@ using yb::master::GrantPermissionRequestPB;
 using yb::master::MasterServiceProxy;
 using yb::master::ReplicationInfoPB;
 using yb::master::TabletLocationsPB;
+using yb::master::RedisConfigSetRequestPB;
+using yb::master::RedisConfigSetResponsePB;
+using yb::master::RedisConfigGetRequestPB;
+using yb::master::RedisConfigGetResponsePB;
 using yb::rpc::Messenger;
 using yb::rpc::MessengerBuilder;
 using yb::rpc::RpcController;
@@ -136,9 +141,10 @@ using google::protobuf::RepeatedPtrField;
 
 using namespace yb::size_literals;
 
+DEFINE_int32(redis_password_caching_duration_ms, 5000,
+             "The duration for which we will cache the redis passwords. 0 to disable.");
 DEFINE_test_flag(int32, yb_num_total_tablets, 0,
                  "The total number of tablets per table when a table is created.");
-
 DECLARE_int32(yb_num_shards_per_tserver);
 
 namespace yb {
@@ -612,6 +618,59 @@ CHECKED_STATUS YBClient::DeleteRole(const std::string& role_name) {
   DeleteRoleResponsePB resp;
   CALL_SYNC_LEADER_MASTER_RPC(req, resp, DeleteRole);
   return Status::OK();
+}
+
+static const string kRequirePass = "requirepass";
+CHECKED_STATUS YBClient::SetRedisPasswords(const std::vector<string>& passwords) {
+  // TODO: Store hash instead of the password?
+  return SetRedisConfig(kRequirePass, passwords);
+}
+
+CHECKED_STATUS YBClient::SetRedisConfig(const string& key, const vector<string>& values) {
+  // Setting up request.
+  RedisConfigSetRequestPB req;
+  req.set_keyword(key);
+  for (const auto& value : values) {
+    req.add_args(value);
+  }
+  RedisConfigSetResponsePB resp;
+  CALL_SYNC_LEADER_MASTER_RPC(req, resp, RedisConfigSet);
+  return Status::OK();
+}
+
+CHECKED_STATUS YBClient::GetRedisConfig(const string& key, vector<string>* values) {
+  // Setting up request.
+  RedisConfigGetRequestPB req;
+  RedisConfigGetResponsePB resp;
+  req.set_keyword(key);
+  CALL_SYNC_LEADER_MASTER_RPC(req, resp, RedisConfigGet);
+  values->clear();
+  for (const auto& arg : resp.args())
+    values->push_back(arg);
+  return Status::OK();
+}
+
+CHECKED_STATUS YBClient::GetRedisPasswords(vector<string>* passwords) {
+  MonoTime now = MonoTime::Now();
+  {
+    std::lock_guard<std::mutex> lock(redis_password_mutex_);
+    if (redis_cached_password_validity_expiry_.Initialized() &&
+        now < redis_cached_password_validity_expiry_) {
+      *passwords = redis_cached_passwords_;
+      return Status::OK();
+    }
+
+    redis_cached_password_validity_expiry_ =
+        now + MonoDelta::FromMilliseconds(FLAGS_redis_password_caching_duration_ms);
+    Status s = GetRedisConfig(kRequirePass, &redis_cached_passwords_);
+    if (s.IsNotFound()) {
+      // If the redis config has no kRequirePass key.
+      redis_cached_passwords_.clear();
+      s = Status::OK();
+    }
+    *passwords = redis_cached_passwords_;
+    return s;
+  }
 }
 
 CHECKED_STATUS YBClient::GrantRole(const std::string& granted_role_name,

src/yb/client/client.h
@@ -347,6 +347,14 @@ class YBClient : public std::enable_shared_from_this<YBClient> {
   // Delete a role.
   CHECKED_STATUS DeleteRole(const std::string& role_name);
 
+  CHECKED_STATUS SetRedisPasswords(const vector<string>& passwords);
+  // Fetches the password from the local cache, or from the master if the local cached value
+  // is too old.
+  CHECKED_STATUS GetRedisPasswords(vector<string>* passwords);
+
+  CHECKED_STATUS SetRedisConfig(const string& key, const vector<string>& values);
+  CHECKED_STATUS GetRedisConfig(const string& key, vector<string>* values);
+
   // Grants a role to another role.
   CHECKED_STATUS GrantRole(const std::string& granted_role_name,
                            const std::string& recipient_role_name);
@@ -531,6 +539,10 @@ class YBClient : public std::enable_shared_from_this<YBClient> {
   // instance and is used in cases such as the load tester, for binding reads and writes from the
   // same client to the same data.
   const std::string client_id_;
+
+  std::mutex redis_password_mutex_;
+  MonoTime redis_cached_password_validity_expiry_;
+  vector<string> redis_cached_passwords_;
 
   DISALLOW_COPY_AND_ASSIGN(YBClient);
 };

src/yb/common/entity_ids.h
@@ -41,6 +41,8 @@ using SystemTableSet = std::set<NamespaceIdTableNamePair>;
 
 using FlushRequestId = std::string;
 
+using RedisConfigKey = std::string;
+
 }  // namespace yb
 
 #endif  // YB_COMMON_ENTITY_IDS_H

src/yb/master/catalog_manager.cc
@@ -60,9 +60,9 @@
 #include <unordered_map>
 #include <vector>
 
+#include <glog/logging.h>
 #include <boost/optional.hpp>
 #include <boost/thread/shared_mutex.hpp>
-#include <glog/logging.h>
 #include "yb/common/flags.h"
 #include "yb/common/partial_row.h"
 #include "yb/common/partition.h"
@@ -488,6 +488,29 @@ class ClusterConfigLoader : public Visitor<PersistentClusterConfigInfo> {
   DISALLOW_COPY_AND_ASSIGN(ClusterConfigLoader);
 };
 
+class RedisConfigLoader : public Visitor<PersistentRedisConfigInfo> {
+ public:
+  explicit RedisConfigLoader(CatalogManager* catalog_manager)
+      : catalog_manager_(catalog_manager) {}
+
+  Status Visit(const std::string& key, const SysRedisConfigEntryPB& metadata) override {
+    CHECK(!ContainsKey(catalog_manager_->redis_config_map_, key))
+        << "Redis Config with key already exists: " << key;
+    // Prepare the config object.
+    RedisConfigInfo* config = new RedisConfigInfo(key);
+    auto l = config->LockForWrite();
+    l->mutable_data()->pb.CopyFrom(metadata);
+    catalog_manager_->redis_config_map_[key] = config;
+    l->Commit();
+    return Status::OK();
+  }
+
+ private:
+  CatalogManager* catalog_manager_;
+
+  DISALLOW_COPY_AND_ASSIGN(RedisConfigLoader);
+};
+
 ////////////////////////////////////////////////////////////
 // Role Loader
 ////////////////////////////////////////////////////////////
@@ -869,6 +892,9 @@ Status CatalogManager::RunLoaders() {
   // Clear the roles mapping.
   roles_map_.clear();
 
+  // Clear redis config mapping.
+  redis_config_map_.clear();
+
   std::vector<std::shared_ptr<TSDescriptor>> descs;
   master_->ts_manager()->GetAllDescriptors(&descs);
   for (const auto& ts_desc : descs) {
@@ -908,6 +934,12 @@ Status CatalogManager::RunLoaders() {
   RETURN_NOT_OK_PREPEND(
       sys_catalog_->Visit(role_loader.get()),
       "Failed while visiting roles in sys catalog");
+
+  LOG(INFO) << __func__ << ": Loading Redis config into memory.";
+  unique_ptr<RedisConfigLoader> redis_config_loader(new RedisConfigLoader(this));
+  RETURN_NOT_OK_PREPEND(
+      sys_catalog_->Visit(redis_config_loader.get()),
+      "Failed while visiting redis config in sys catalog");
 
   return Status::OK();
 }
@@ -3680,6 +3712,51 @@ Status CatalogManager::GrantRole(const GrantRoleRequestPB* req,
                           req->granted_role(), req->recipient_role()));
     return SetupError(resp->mutable_error(), MasterErrorPB::INVALID_REQUEST, s);
   }
+  return Status::OK();
+}
+
+Status CatalogManager::RedisConfigSet(
+    const RedisConfigSetRequestPB* req, RedisConfigSetResponsePB* resp, rpc::RpcContext* rpc) {
+  DCHECK(req->has_keyword());
+  const auto& key = req->keyword();
+  SysRedisConfigEntryPB config_entry;
+  config_entry.set_key(key);
+  *config_entry.mutable_args() = req->args();
+  bool created = false;
+
+  TRACE("Acquired catalog manager lock");
+  std::lock_guard<LockType> l_big(lock_);
+  scoped_refptr<RedisConfigInfo> cfg = FindPtrOrNull(redis_config_map_, req->keyword());
+  if (cfg == nullptr) {
+    created = true;
+    cfg = new RedisConfigInfo(key);
+    redis_config_map_[key] = cfg;
+  }
+
+  auto wl = cfg->LockForWrite();
+  wl->mutable_data()->pb = std::move(config_entry);
+  if (created) {
+    CHECK_OK(sys_catalog_->AddItem(cfg.get()));
+  } else {
+    CHECK_OK(sys_catalog_->UpdateItem(cfg.get()));
+  }
+  wl->Commit();
+  return Status::OK();
+}
+
+Status CatalogManager::RedisConfigGet(
+    const RedisConfigGetRequestPB* req, RedisConfigGetResponsePB* resp, rpc::RpcContext* rpc) {
+  DCHECK(req->has_keyword());
+  resp->set_keyword(req->keyword());
+  TRACE("Acquired catalog manager lock");
+  std::lock_guard<LockType> l_big(lock_);
+  scoped_refptr<RedisConfigInfo> cfg = FindPtrOrNull(redis_config_map_, req->keyword());
+  if (cfg == nullptr) {
+    Status s = STATUS(NotFound, Substitute("Redis config for $0 does not exists", req->keyword()));
+    return SetupError(resp->mutable_error(), MasterErrorPB::REDIS_CONFIG_NOT_FOUND, s);
+  }
+  auto rci = cfg->LockForRead();
+  resp->mutable_args()->CopyFrom(rci->data().pb.args());
   return Status::OK();
 }
 

src/yb/master/catalog_manager.h
@@ -584,8 +584,26 @@ class ClusterConfigInfo : public RefCountedThreadSafe<ClusterConfigInfo>,
   DISALLOW_COPY_AND_ASSIGN(ClusterConfigInfo);
 };
 
-struct PersistentRoleInfo : public Persistent<SysRoleEntryPB, SysRowEntry::ROLE> {
+struct PersistentRedisConfigInfo
+    : public Persistent<SysRedisConfigEntryPB, SysRowEntry::REDIS_CONFIG> {};
+
+class RedisConfigInfo : public RefCountedThreadSafe<RedisConfigInfo>,
+                        public MetadataCowWrapper<PersistentRedisConfigInfo> {
+ public:
+  explicit RedisConfigInfo(const std::string key) : config_key_(key) {}
+
+  virtual const std::string& id() const override { return config_key_; }
+
+ private:
+  friend class RefCountedThreadSafe<RedisConfigInfo>;
+  ~RedisConfigInfo() = default;
+
+  const std::string config_key_;
+
+  DISALLOW_COPY_AND_ASSIGN(RedisConfigInfo);
 };
+
+struct PersistentRoleInfo : public Persistent<SysRoleEntryPB, SysRowEntry::ROLE> {};
 
 class RoleInfo : public RefCountedThreadSafe<RoleInfo>,
                  public MetadataCowWrapper<PersistentRoleInfo> {
@@ -884,6 +902,12 @@ class CatalogManager : public tserver::TabletPeerLookupIf {
   CHECKED_STATUS GrantRole(const GrantRoleRequestPB* req,
                            GrantRoleResponsePB* resp,
                            rpc::RpcContext* rpc);
+  // Set Redis Config
+  CHECKED_STATUS RedisConfigSet(
+      const RedisConfigSetRequestPB* req, RedisConfigSetResponsePB* resp, rpc::RpcContext* rpc);
+  // Get Redis Config
+  CHECKED_STATUS RedisConfigGet(
+      const RedisConfigGetRequestPB* req, RedisConfigGetResponsePB* resp, rpc::RpcContext* rpc);
 
   // Create a new User-Defined Type with the specified attributes.
   //
@@ -1090,6 +1114,7 @@ class CatalogManager : public tserver::TabletPeerLookupIf {
   friend class UDTypeLoader;
   friend class ClusterConfigLoader;
   friend class RoleLoader;
+  friend class RedisConfigLoader;
 
   FRIEND_TEST(SysCatalogTest, TestPrepareDefaultClusterConfig);
 
@@ -1456,7 +1481,11 @@ class CatalogManager : public tserver::TabletPeerLookupIf {
   typedef std::unordered_map<RoleName, scoped_refptr<RoleInfo> > RoleInfoMap;
   RoleInfoMap roles_map_;
 
-  // TODO (Bristy) : Implement (resource) --> (role->permissions) map.
+  // TODO (Bristy) : Implement (resource) --> (role->permissions) map
+
+  // RedisConfig map: RedisConfigKey -> RedisConfigInfo
+  typedef std::unordered_map<RedisConfigKey, scoped_refptr<RedisConfigInfo>> RedisConfigInfoMap;
+  RedisConfigInfoMap redis_config_map_;
 
   // Config information.
   scoped_refptr<ClusterConfigInfo> cluster_config_ = nullptr;

commit 8a8ccf00590341e3352983d1fc6d0c5bedc396f5

Fix broken release build because of an unused variable
Summary: Fix release build broken by D4778 due to an unused variable.

Test Plan:
Jenkins: skip
Tested by doing a local release build

Reviewers: kannan, bharat, amitanand

Reviewed By: kannan, bharat, amitanand

Subscribers: ybase

Differential Revision: https://phabricator.dev.yugabyte.com/D5141

src/yb/yql/redis/redisserver/redis_commands.cc
@@ -52,6 +52,8 @@ static bool ValidateRedisPasswordSeparator(const char* flagname, const string& v
 DEFINE_bool(yedis_enable_flush, true, "Enables FLUSHDB and FLUSHALL commands in yedis.");
 DEFINE_bool(use_hashed_redis_password, true, "Store the hash of the redis passwords instead.");
 DEFINE_string(redis_passwords_separator, ",", "The character used to separate multiple passwords.");
+
+__attribute__((unused))
 DEFINE_validator(redis_passwords_separator, &ValidateRedisPasswordSeparator);
 
 namespace yb {