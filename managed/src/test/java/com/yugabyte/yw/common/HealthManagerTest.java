de29738415854e71392b72a6046ad201930d33f1
Bogdan Matican <bmatican@users.noreply.github.com>
2018-08-10 07:31:04 +0000

    ENG-3746: YW change to make health check xcloud friendly
    Summary:
    Added YW code to make health checking happen per-cluster in a universe and send a JSON
    payload to the devops script with the cluster description. This includes master/tserver nodes,
    ssh port and identity file.
    
    Test Plan: tested with live cluster and with live+RR cluster
    
    Reviewers: ram, hector, bharat
    
    Reviewed By: bharat
    
    Subscribers: jenkins-bot, yugaware
    
    Differential Revision: https://phabricator.dev.yugabyte.com/D5311

--- managed/src/test/java/com/yugabyte/yw/common/HealthManagerTest.java
+++ managed/src/test/java/com/yugabyte/yw/common/HealthManagerTest.java
@@ -12,6 +12,8 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
+import play.libs.Json;
+
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 
@@ -25,22 +27,16 @@ public class HealthManagerTest extends FakeDBApplication {
   HealthManager healthManager;
 
   private List<String> healthCheckCommand(
-      String mastersCsv, String tserversCsv, String sshPort, String universeName, String privateKey,
-      String customerTag, String destination, boolean shouldSendStatusUpdate) {
+      List<HealthManager.ClusterInfo> clusters, String universeName, String customerTag,
+      String destination, boolean shouldSendStatusUpdate) {
     List<String> expectedCommand = new ArrayList<>();
 
     expectedCommand.add(DevopsBase.PY_WRAPPER);
     expectedCommand.add(HealthManager.HEALTH_CHECK_SCRIPT);
-    expectedCommand.add("--master_nodes");
-    expectedCommand.add(mastersCsv);
-    expectedCommand.add("--tserver_nodes");
-    expectedCommand.add(tserversCsv);
-    expectedCommand.add("--ssh_port");
-    expectedCommand.add(sshPort);
+    expectedCommand.add("--cluster_payload");
+    expectedCommand.add(Json.stringify(Json.toJson(clusters)));
     expectedCommand.add("--universe_name");
     expectedCommand.add(universeName);
-    expectedCommand.add("--identity_file");
-    expectedCommand.add(privateKey);
     expectedCommand.add("--customer_tag");
     expectedCommand.add(customerTag);
     if (destination != null) {
@@ -55,11 +51,14 @@ public class HealthManagerTest extends FakeDBApplication {
 
   @Test
   public void testHealthManager() {
-    String mastersCsv = "m1,m2,m3";
-    String tserversCsv = "ts1,ts2,ts3";
-    String sshPort = "22";
+    // Setup the cluster.
+    HealthManager.ClusterInfo cluster = new HealthManager.ClusterInfo();
+    cluster.sshPort = 22;
+    cluster.identityFile = "key.pem";
+    cluster.masterNodes = ImmutableList.of("m1", "m2", "m3");
+    cluster.tserverNodes = ImmutableList.of("ts1", "ts2", "ts3");
+    // Other args
     String universeName = "universe1";
-    String privateKey = "key.pem";
     String customerTag = "customer.env";
     List<String> destinationOptions = new ArrayList<>();
     destinationOptions.add("test@example.com");
@@ -68,9 +67,9 @@ public class HealthManagerTest extends FakeDBApplication {
     for (String d : destinationOptions) {
       for (Boolean sendStatus : statusOptions) {
         List<String> expectedCommand = healthCheckCommand(
-            mastersCsv, tserversCsv, sshPort, universeName, privateKey, customerTag, d, sendStatus);
+            ImmutableList.of(cluster), universeName, customerTag, d, sendStatus);
         healthManager.runCommand(
-            mastersCsv, tserversCsv, sshPort, universeName, privateKey, customerTag, d, sendStatus);
+            ImmutableList.of(cluster), universeName, customerTag, d, sendStatus);
         verify(shellProcessHandler, times(1)).run(expectedCommand, new HashMap<>());
       }
     }
