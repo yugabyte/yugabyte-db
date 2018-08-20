7432b6adb1a1dce2f0d26bd3bb464f7422dc8293
Bogdan Matican <bmatican@users.noreply.github.com>
2018-08-20 20:48:56 +0000

    ENG-3773: Adding modes in provider configure for AWS IAM role and GCP service account vs custom credentials
    Summary:
    Added dropdown option in provider configure to choose inputting custom credentials vs using
    machine credentials, if available, like IAM role in AWS or Service Account in GCP.
    
    Test Plan:
    Tested on both AWS and GCP with old mode as well as new mode with and without credentials set or enough permissions given.
    
    Also realized AWS would otherwise continue just fine if `~/.aws/credentials` exists, unless our devops code does the correct enforcement per call, which I've ensured!
    
    Reviewers: ram, bharat, karthik, vit.pankin
    
    Reviewed By: vit.pankin
    
    Subscribers: jenkins-bot, yugaware
    
    Differential Revision: https://phabricator.dev.yugabyte.com/D5342

--- managed/ui/src/components/config/PublicCloud/views/AWSProviderInitView.js
+++ managed/ui/src/components/config/PublicCloud/views/AWSProviderInitView.js
@@ -5,7 +5,6 @@ import { Row, Col, Alert } from 'react-bootstrap';
 import { YBButton, YBTextInputWithLabel, YBSelectWithLabel, YBToggle } from '../../../common/forms/fields';
 import { isDefinedNotNull, isNonEmptyString } from 'utils/ObjectUtils';
 import { change, Field } from 'redux-form';
-import { IN_DEVELOPMENT_MODE } from '../../../../config';
 import { isValidObject, trimString } from '../../../../utils/ObjectUtils';
 import {reduxForm} from 'redux-form';
 
@@ -14,7 +13,8 @@ class AWSProviderInitView extends Component {
     super(props);
     this.state = {
       networkSetupType: "new_vpc",
-      setupHostedZone: false
+      setupHostedZone: false,
+      credentialInputType: "custom_keys"
     };
   }
 
@@ -30,15 +30,20 @@ class AWSProviderInitView extends Component {
     this.setState({networkSetupType: value});
   }
 
+  credentialInputChanged = (value) => {
+    this.setState({credentialInputType: value});
+  }
+
   updateFormField = (field, value) => {
     this.props.dispatch(change("awsProviderConfigForm", field, value));
   };
 
   createProviderConfig = formValues => {
     const {hostInfo} = this.props;
-    const awsProviderConfig = {
-      'AWS_ACCESS_KEY_ID': formValues.accessKey,
-      'AWS_SECRET_ACCESS_KEY': formValues.secretKey
+    const awsProviderConfig = {};
+    if (formValues.credential_input === "custom_keys") {
+      awsProviderConfig['AWS_ACCESS_KEY_ID'] = formValues.accessKey;
+      awsProviderConfig['AWS_SECRET_ACCESS_KEY'] = formValues.secretKey;
     };
     if (isDefinedNotNull(formValues.hostedZoneId)) {
       awsProviderConfig['AWS_HOSTED_ZONE_ID'] = formValues.hostedZoneId;
@@ -68,12 +73,27 @@ class AWSProviderInitView extends Component {
 
   isHostInAWS = () => {
     const { hostInfo } = this.props;
-    return !IN_DEVELOPMENT_MODE && isValidObject(hostInfo) && isValidObject(hostInfo["aws"]) &&
+    return isValidObject(hostInfo) && isValidObject(hostInfo["aws"]) &&
       hostInfo["aws"]["error"] === undefined;
   }
 
   hostedZoneToggled = (event) => {
     this.setState({setupHostedZone: event.target.checked});
+  }
+
+  generateRow = (label, field) => {
+    return (
+      <Row className="config-provider-row">
+        <Col lg={3}>
+          <div className="form-item-custom-label">
+            {label}
+          </div>
+        </Col>
+        <Col lg={7}>
+          {field}
+        </Col>
+      </Row>
+    );
   }
 
   render() {
@@ -90,52 +110,86 @@ class AWSProviderInitView extends Component {
 
     let customVPCFields = <span />;
     if (this.state.networkSetupType !== "new_vpc") {
+      const destVpcRegionField = (
+        <Field name="destVpcRegion" type="text" component={YBTextInputWithLabel}
+          normalize={trimString} isReadOnly={this.state.networkSetupType === "host_vpc"} />
+      );
+      const destVpcIdField = (
+        <Field name="destVpcId" type="text" component={YBTextInputWithLabel}
+          normalize={trimString} isReadOnly={this.state.networkSetupType === "host_vpc"} />
+      );
       customVPCFields = (
         <Fragment>
-          <Field name="destVpcRegion" type="text" label="Custom VPC Region"
-            component={YBTextInputWithLabel} normalize={trimString}
-            isReadOnly={this.state.networkSetupType === "host_vpc"} />
-          <Field name="destVpcId" type="text" label="Custom VPC ID"
-            component={YBTextInputWithLabel} normalize={trimString}
-            isReadOnly={this.state.networkSetupType === "host_vpc"} />
+          {this.generateRow("Custom VPC Region", destVpcRegionField)}
+          {this.generateRow("Custom VPC ID", destVpcIdField)}
         </Fragment>
       );
     }
-    let hostedZoneFields = <span />;
+    let hostedZoneField = <span />;
     if (this.state.setupHostedZone) {
-      hostedZoneFields = (
-        <Field name="hostedZoneId" type="text" label="Route53 Zone ID"
-              component={YBTextInputWithLabel} normalize={trimString} />
+      hostedZoneField = this.generateRow("Route53 Zone ID",
+        <Field name="hostedZoneId" type="text" component={YBTextInputWithLabel}
+          normalize={trimString} />
       );
     }
+    const credential_input_options = [
+      <option key={1} value={"custom_keys"}>{"Input Access and Secret keys"}</option>,
+      <option key={2} value={"local_iam_role"}>{"Use IAM Role on instance"}</option>
+    ];
+    let customKeyFields = <span />;
+    if (this.state.credentialInputType === "custom_keys") {
+      const accessKeyField = (
+        <Field name="accessKey" type="text" component={YBTextInputWithLabel}
+          normalize={trimString} />);
+      const secretKeyField = (
+        <Field name="secretKey" type="text" component={YBTextInputWithLabel}
+          normalize={trimString} />);
+      customKeyFields = (
+        <Fragment>
+          {this.generateRow("Access Key ID", accessKeyField)}
+          {this.generateRow("Secret Access Key", secretKeyField)}
+        </Fragment>
+      );
+    }
+    const nameField = this.generateRow(
+        "Name",
+        <Field name="accountName" type="text" component={YBTextInputWithLabel} />);
+    const credentialInputField = this.generateRow(
+        "Credential Type",
+        <Field name="credential_input" component={YBSelectWithLabel}
+          options={credential_input_options} onInputChanged={this.credentialInputChanged} />
+    );
+    const networkInputField = this.generateRow(
+        "VPC Setup",
+        <Field name="network_setup" component={YBSelectWithLabel} options={network_setup_options}
+          onInputChanged={this.networkSetupChanged} />);
+    const hostedZoneToggleField = this.generateRow(
+        "Enable Hosted Zone",
+        <Field name="setupHostedZone" component={YBToggle}
+          defaultChecked={this.state.setupHostedZone} onToggle={this.hostedZoneToggled} />);
     return (
-      <form name="awsProviderConfigForm" onSubmit={handleSubmit(this.createProviderConfig)}>
-        <Row className="config-section-header">
-          <Col lg={6}>
-            { error && <Alert bsStyle="danger">{error}</Alert> }
-            <div className="aws-config-form form-right-aligned-labels">
-              <Field name="accountName" type="text" label="Name"
-                     component={YBTextInputWithLabel} />
-              <Field name="accessKey" type="text" label="Access Key ID"
-                     component={YBTextInputWithLabel} normalize={trimString} />
-              <Field name="secretKey" type="text" label="Secret Access Key"
-                     component={YBTextInputWithLabel} normalize={trimString} />
-              <Field name="network_setup" component={YBSelectWithLabel}
-                options={network_setup_options} label="VPC Setup"
-                onInputChanged={this.networkSetupChanged} />
-              {customVPCFields}
-              <Field name="setupHostedZone" component={YBToggle}
-                     label="Enable Hosted Zone" defaultChecked={this.state.setupHostedZone}
-                     onToggle={this.hostedZoneToggled} />
-              {hostedZoneFields}
-            </div>
-          </Col>
-        </Row>
-        <div className="form-action-button-container">
-          <YBButton btnText={"Save"} btnClass={"btn btn-default save-btn"}
-                    disabled={submitting } btnType="submit"/>
-        </div>
-      </form>
+      <div className="provider-config-container">
+        <form name="awsProviderConfigForm" onSubmit={handleSubmit(this.createProviderConfig)}>
+          <div className="editor-container">
+            <Row className="config-section-header">
+              <Col lg={8}>
+                {error && <Alert bsStyle="danger">{error}</Alert>}
+                {nameField}
+                {credentialInputField}
+                {customKeyFields}
+                {networkInputField}
+                {customVPCFields}
+                {hostedZoneToggleField}
+                {hostedZoneField}
+              </Col>
+            </Row>
+          </div>
+          <div className="form-action-button-container">
+            <YBButton btnText={"Save"} btnClass={"btn btn-default save-btn"}
+                      disabled={submitting } btnType="submit"/>
+          </div>
+        </form>
+      </div>
     );
   }
 }
--- managed/ui/src/components/config/PublicCloud/views/GCPProviderInitView.js
+++ managed/ui/src/components/config/PublicCloud/views/GCPProviderInitView.js
@@ -19,34 +19,36 @@ class GCPProviderInitView extends Component {
       providerUUID: "",
       currentProvider: {},
       hostVpcVisible: true,
-      networkSetupType: "new_vpc"
+      networkSetupType: "new_vpc",
+      credentialInputType: "upload_service_account_json"
     };
     this.hostVpcToggled = this.hostVpcToggled.bind(this);
   }
 
   createProviderConfig = vals => {
     const self = this;
+    const gcpCreateConfig = {};
+    if (isNonEmptyString(vals.destVpcId)) {
+      gcpCreateConfig["network"] = vals.destVpcId;
+      gcpCreateConfig["use_host_vpc"] = true;
+    } else {
+      gcpCreateConfig["use_host_vpc"] = false;
+    }
+    const providerName = vals.accountName;
     const configText = vals.gcpConfig;
-    if (isNonEmptyObject(configText)) {
-      const providerName = vals.accountName;
+    if (vals.credential_input === "local_service_account") {
+      return self.props.createGCPProvider(providerName, gcpCreateConfig);
+    } else if (vals.credential_input === "upload_service_account_json" && isNonEmptyObject(configText)) {
       const reader = new FileReader();
       reader.readAsText(configText);
       // Parse the file back to JSON, since the API controller endpoint doesn't support file upload
       reader.onloadend = function () {
         try {
-          const gcpCreateConfig = {
-            "config_file_contents": JSON.parse(reader.result)
-          };
-          if (isNonEmptyString(vals.destVpcId)) {
-            gcpCreateConfig["network"] = vals.destVpcId;
-            gcpCreateConfig["use_host_vpc"] = true;
-          } else {
-            gcpCreateConfig["use_host_vpc"] = false;
-          }
-          self.props.createGCPProvider(providerName, gcpCreateConfig);
+          gcpCreateConfig["config_file_contents"] = JSON.parse(reader.result);
         } catch (e) {
           self.setState({"error": "Invalid GCP config JSON file"});
         }
+        return self.props.createGCPProvider(providerName, gcpCreateConfig);
       };
     } else {
       this.setState({"error": "GCP Config JSON is required"});
@@ -55,7 +57,6 @@ class GCPProviderInitView extends Component {
 
   isHostInGCP() {
     const { hostInfo } = this.props;
-    // Removed the !IN_DEVELOPMENT_MODE check because GCP bootstrap should not break anything.
     return isNonEmptyObject(hostInfo) && isNonEmptyObject(hostInfo["gcp"]) &&
       hostInfo["gcp"]["error"] === undefined;
   }
@@ -77,6 +78,9 @@ class GCPProviderInitView extends Component {
     }
     this.setState({networkSetupType: value});
   }
+  credentialInputChanged = (value) => {
+    this.setState({credentialInputType: value});
+  }
 
   updateFormField = (field, value) => {
     this.props.dispatch(change("gcpProviderConfigForm", field, value));
@@ -87,11 +91,6 @@ class GCPProviderInitView extends Component {
     if (getPromiseState(configuredProviders).isLoading()) {
       return <YBLoading />;
     }
-    let gcpConfigFileName = "";
-    if (isNonEmptyObject(this.state.gcpConfig)) {
-      gcpConfigFileName = this.state.gcpConfig.name;
-    }
-
     const network_setup_options = [
       <option key={1} value={"new_vpc"}>{"Create a new VPC"}</option>,
       <option key={2} value={"existing_vpc"}>{"Specify an existing VPC"}</option>
@@ -101,11 +100,35 @@ class GCPProviderInitView extends Component {
         <option key={3} value={"host_vpc"}>{"Use VPC of the Admin Console instance"}</option>
       );
     }
+    const credential_input_options = [
+      <option key={1} value={"upload_service_account_json"}>{"Upload Service Account config"}</option>,
+      <option key={2} value={"local_service_account"}>{"Use Service Accont on instance"}</option>
+    ];
+    let uploadConfigField = <span />;
+    if (this.state.credentialInputType === "upload_service_account_json") {
+      let gcpConfigFileName = "";
+      if (isNonEmptyObject(this.state.gcpConfig)) {
+        gcpConfigFileName = this.state.gcpConfig.name;
+      }
+      uploadConfigField = (
+        <Row className="config-provider-row">
+          <Col lg={3}>
+            <div className="form-item-custom-label">Provider Config</div>
+          </Col>
+          <Col lg={7}>
+            <Field name="gcpConfig" component={YBDropZone} className="upload-file-button" title={"Upload GCP Config json file"}/>
+          </Col>
+          <Col lg={4}>
+            <div className="file-label">{gcpConfigFileName}</div>
+          </Col>
+        </Row>
+      );
+    }
 
     let destVpcField = <span />;
     if (this.state.networkSetupType !== "new_vpc") {
       destVpcField = (
-        <div>
+        <Row className="config-provider-row">
           <Col lg={3}>
             <div className="form-item-custom-label">
               VPC Network Name
@@ -117,7 +140,7 @@ class GCPProviderInitView extends Component {
                 className={"gcp-provider-input-field"}
                 isReadOnly={this.state.networkSetupType === "host_vpc"}/>
           </Col>
-        </div>
+        </Row>
       );
     }
 
@@ -125,7 +148,7 @@ class GCPProviderInitView extends Component {
       <div className="provider-config-container">
         <form name="gcpProviderConfigForm" onSubmit={handleSubmit(this.createProviderConfig)}>
           <div className="editor-container">
-            <Row>
+            <Row className="config-section-header">
               <Col lg={8}>
                 <Row className="config-provider-row">
                   <Col lg={3}>
@@ -136,17 +159,17 @@ class GCPProviderInitView extends Component {
                            component={YBTextInputWithLabel} className={"gcp-provider-input-field"}/>
                   </Col>
                 </Row>
-                <Row className="config-provider-row">
+                <Row>
                   <Col lg={3}>
-                    <div className="form-item-custom-label">Provider Config</div>
+                    <div className="form-item-custom-label">Credential Type</div>
                   </Col>
                   <Col lg={7}>
-                    <Field name="gcpConfig" component={YBDropZone} className="upload-file-button" title={"Upload GCP Config json file"}/>
-                  </Col>
-                  <Col lg={4}>
-                    <div className="file-label">{gcpConfigFileName}</div>
+                    <Field name="credential_input" component={YBSelectWithLabel}
+                      options={credential_input_options}
+                      onInputChanged={this.credentialInputChanged} />
                   </Col>
                 </Row>
+                {uploadConfigField}
                 <Row>
                   <Col lg={3}>
                     <div className="form-item-custom-label">VPC Setup</div>
@@ -157,9 +180,7 @@ class GCPProviderInitView extends Component {
                       onInputChanged={this.networkSetupChanged} />
                   </Col>
                 </Row>
-                <Row className="config-provider-row">
-                  {destVpcField}
-                </Row>
+                {destVpcField}
               </Col>
             </Row>
           </div>
