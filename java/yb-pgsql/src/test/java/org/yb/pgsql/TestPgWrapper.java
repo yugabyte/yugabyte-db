1ee1c2d233a840e6f0b32b938eecfe97dee23edb
Mihnea Iancu <m-iancu@users.noreply.github.com>
2018-10-22 16:10:08 -0700

    Enable some supported YB-Postgres features in parser
    Summary:
    - Enable complex SELECTs, PREPARE, EXECUTE, and EXPLAIN statements in parser grammar.
    - Disable serializable isolation and locking clause in grammar.
    - Add corresponding tests and some misc cleanup in JDBC tests
    
    Test Plan: TestPgWrapper, TestPgSelect, TestPgPrepareExecute
    
    Reviewers: mikhail, robert, neil
    
    Reviewed By: neil
    
    Subscribers: yql
    
    Differential Revision: https://phabricator.dev.yugabyte.com/D5609

--- java/yb-pgsql/src/test/java/org/yb/pgsql/TestPgWrapper.java
+++ java/yb-pgsql/src/test/java/org/yb/pgsql/TestPgWrapper.java
@@ -13,7 +13,6 @@
 
 package org.yb.pgsql;
 
-import org.apache.commons.lang3.RandomUtils;
 import org.junit.Test;
 
 import java.sql.Connection;
@@ -44,40 +43,39 @@ public class TestPgWrapper extends BasePgSQLTest {
 
   @Test
   public void testSimpleDDL() throws Exception {
-    Statement statement = connection.createStatement();
+    try (Statement statement = connection.createStatement()) {
 
-    // ---------------------------------------------------------------------------------------------
-    // Test Database
+      // -------------------------------------------------------------------------------------------
+      // Test Database
 
-    statement.execute("CREATE DATABASE dbtest");
+      statement.execute("CREATE DATABASE dbtest");
 
-    // Database already exists.
-    runInvalidQuery(statement, "CREATE DATABASE dbtest");
+      // Database already exists.
+      runInvalidQuery(statement, "CREATE DATABASE dbtest");
 
-    statement.execute("DROP DATABASE dbtest");
+      statement.execute("DROP DATABASE dbtest");
 
-    // Database does not exist.
-    runInvalidQuery(statement, "DROP DATABASE dbtest");
+      // Database does not exist.
+      runInvalidQuery(statement, "DROP DATABASE dbtest");
 
-    // ---------------------------------------------------------------------------------------------
-    // Test Table
+      // -------------------------------------------------------------------------------------------
+      // Test Table
 
-    createSimpleTable("test", "v");
+      createSimpleTable("test", "v");
 
-    // Test table with out of order primary key columns.
-    statement.execute("CREATE TABLE test2(v text, r float, h bigint, PRIMARY KEY (h, r))");
+      // Test table with out of order primary key columns.
+      statement.execute("CREATE TABLE test2(v text, r float, h bigint, PRIMARY KEY (h, r))");
 
-    // Table already exists.
-    runInvalidQuery(statement, getSimpleTableCreationStatement("test", "v"));
+      // Table already exists.
+      runInvalidQuery(statement, getSimpleTableCreationStatement("test", "v"));
 
-    // Test drop table.
-    statement.execute("DROP TABLE test");
-    statement.execute("DROP TABLE test2");
+      // Test drop table.
+      statement.execute("DROP TABLE test");
+      statement.execute("DROP TABLE test2");
 
-    // Table does not exist.
-    runInvalidQuery(statement, "DROP TABLE test");
-
-    statement.close();
+      // Table does not exist.
+      runInvalidQuery(statement, "DROP TABLE test");
+    }
   }
 
   @Test
@@ -85,55 +83,55 @@ public class TestPgWrapper extends BasePgSQLTest {
     LOG.info("START testDatatypes");
     String[] supported_types = {"smallint", "int", "bigint", "real", "double precision", "text"};
 
-    Statement statement = connection.createStatement();
+    try (Statement statement = connection.createStatement()) {
 
-    StringBuilder sb = new StringBuilder();
-    sb.append("CREATE TABLE test(");
-    // Checking every type is allowed for a column.
-    for (int i = 0; i < supported_types.length; i++) {
-      sb.append("c").append(i).append(" ");
-      sb.append(supported_types[i]);
-      sb.append(", ");
+      StringBuilder sb = new StringBuilder();
+      sb.append("CREATE TABLE test(");
+      // Checking every type is allowed for a column.
+      for (int i = 0; i < supported_types.length; i++) {
+        sb.append("c").append(i).append(" ");
+        sb.append(supported_types[i]);
+        sb.append(", ");
+      }
+
+      sb.append("PRIMARY KEY(");
+      // Checking every type is allowed as primary key.
+      for (int i = 0; i < supported_types.length; i++) {
+        if (i > 0) sb.append(", ");
+        sb.append("c").append(i);
+      }
+      sb.append("))");
+
+      String sql = sb.toString();
+      LOG.info("Creating table. SQL statement: " + sql);
+      statement.execute(sb.toString());
+      LOG.info("END testDatatypes");
     }
-
-    sb.append("PRIMARY KEY(");
-    // Checking every type is allowed as primary key.
-    for (int i = 0; i < supported_types.length; i++) {
-      if (i > 0) sb.append(", ");
-      sb.append("c").append(i);
-    }
-    sb.append("))");
-
-    String sql = sb.toString();
-    LOG.info("Creating table. SQL statement: " + sql);
-    statement.execute(sb.toString());
-    LOG.info("END testDatatypes");
   }
 
   @Test
   public void testSimpleDML() throws Exception {
-    Statement statement = connection.createStatement();
-    statement.execute("CREATE TABLE test(h bigint, r float, v text, PRIMARY KEY (h, r))");
+    try (Statement statement = connection.createStatement()) {
+      statement.execute("CREATE TABLE test(h bigint, r float, v text, PRIMARY KEY (h, r))");
 
-    statement.execute("INSERT INTO test(h, r, v) VALUES (1, 2.5, 'abc')");
-    statement.execute("INSERT INTO test(h, r, v) VALUES (1, 3.5, 'def')");
+      statement.execute("INSERT INTO test(h, r, v) VALUES (1, 2.5, 'abc')");
+      statement.execute("INSERT INTO test(h, r, v) VALUES (1, 3.5, 'def')");
 
-    ResultSet rs = statement.executeQuery("SELECT h, r, v FROM test WHERE h = 1");
+      try (ResultSet rs = statement.executeQuery("SELECT h, r, v FROM test WHERE h = 1")) {
 
-    assertTrue(rs.next());
-    assertEquals(1, rs.getLong("h"));
-    assertEquals(2.5, rs.getDouble("r"));
-    assertEquals("abc", rs.getString("v"));
+        assertTrue(rs.next());
+        assertEquals(1, rs.getLong("h"));
+        assertEquals(2.5, rs.getDouble("r"));
+        assertEquals("abc", rs.getString("v"));
 
-    assertTrue(rs.next());
-    assertEquals(1, rs.getLong("h"));
-    assertEquals(3.5, rs.getDouble("r"));
-    assertEquals("def", rs.getString("v"));
+        assertTrue(rs.next());
+        assertEquals(1, rs.getLong("h"));
+        assertEquals(3.5, rs.getDouble("r"));
+        assertEquals("def", rs.getString("v"));
 
-    assertFalse(rs.next());
-
-    rs.close();
-    statement.close();
+        assertFalse(rs.next());
+      }
+    }
   }
 
   /**
