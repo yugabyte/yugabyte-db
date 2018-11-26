093b133d49cfdb1a1fc3bb8f8a577b2990a60394
Sergei Politov <spolitov@users.noreply.github.com>
2018-11-26 11:23:44 +0300

    #605: Change the /mem-trackers internal UI page to a tree-like view
    Summary:
    Mem trackers are organized in a tree, so it should be reflected in the /mem-trackers view.
    Currently to show mem trackers we enumerate them using BFS and show only tracker parent.
    Since we have parents with the same names in different hierarchies, it is quite hard to
    understand where this tracker actually resides.
    
    To address this issue this diff changes the way of how mem trackers are enumerated and shows them
    in a table using varying levels of padding, so it looks like an actual tree of mem trackers.
    
    Other enhancement:
    1) Removed the parent column, since it is superfluous now.
    2) Moved the limit column to the end of columns, because otherwise it is easy to confuse it for the current consumption.
    3) Added spacing to end of the page, so the footer does not hide the last line of the mem trackers table.
    
    Test Plan: Tested manually.
    
    Reviewers: karthik, bogdan, kannan, mikhail
    
    Reviewed By: mikhail
    
    Subscribers: mikhail, ybase
    
    Differential Revision: https://phabricator.dev.yugabyte.com/D5753

--- src/yb/server/default-path-handlers.cc
+++ src/yb/server/default-path-handlers.cc
@@ -89,6 +89,7 @@ using strings::Substitute;
 using namespace std::placeholders;
 
 namespace {
+
 // Html/Text formatting tags
 struct Tags {
   string pre_tag, end_pre_tag, line_break, header, end_header;
@@ -110,7 +111,6 @@ struct Tags {
     }
   }
 };
-} // anonymous namespace
 
 // Writes the last FLAGS_web_log_bytes of the INFO logfile to a webpage
 // Note to get best performance, set GLOG_logbuflevel=-1 to prevent log buffering
@@ -172,39 +172,33 @@ static void MemUsageHandler(const Webserver::WebRequest& req, std::stringstream*
 #endif
 }
 
+void ProcessMemTracker(const MemTrackerPtr& tracker, int depth, std::ostream* output) {
+  std::string limit_str =
+      tracker->limit() == -1 ? "none" : HumanReadableNumBytes::ToString(tracker->limit());
+  std::string current_consumption_str = HumanReadableNumBytes::ToString(tracker->consumption());
+  std::string peak_consumption_str = HumanReadableNumBytes::ToString(tracker->peak_consumption());
+  *output << Format("  <tr data-depth=\"$0\" class=\"level$0\">\n", depth);
+  *output << Format("    <td>$0</td><td>$1</td><td>$2</td><td>$3</td>\n",
+                    tracker->id(), current_consumption_str, peak_consumption_str, limit_str);
+  *output << "  </tr>\n";
+
+  auto children = tracker->ListChildren();
+
+  for (const auto& child : children) {
+    ProcessMemTracker(child, depth + 1, output);
+  }
+}
+
 // Registered to handle "/mem-trackers", and prints out to handle memory tracker information.
 static void MemTrackersHandler(const Webserver::WebRequest& req, std::stringstream* output) {
   *output << "<h1>Memory usage by subsystem</h1>\n";
   *output << "<table class='table table-striped'>\n";
-  *output << "  <tr><th>Id</th><th>Parent</th><th>Limit</th><th>Current Consumption</th>"
-      "<th>Peak consumption</th></tr>\n";
+  *output << "  <tr><th>Id</th><th>Current Consumption</th>"
+      "<th>Peak consumption</th><th>Limit</th></tr>\n";
 
-  vector<shared_ptr<MemTracker> > trackers = MemTracker::ListTrackers();
-  for (const shared_ptr<MemTracker>& tracker : trackers) {
-    string parent = tracker->parent() == nullptr ? "none" : tracker->parent()->id();
-    string limit_str = tracker->limit() == -1 ? "none" :
-                       HumanReadableNumBytes::ToString(tracker->limit());
-    string current_consumption_str = HumanReadableNumBytes::ToString(tracker->consumption());
-    string peak_consumption_str = HumanReadableNumBytes::ToString(tracker->peak_consumption());
-    (*output) << Substitute("  <tr><td>$0</td><td>$1</td><td>$2</td>" // id, parent, limit
-                            "<td>$3</td><td>$4</td></tr>\n", // current, peak
-                            tracker->id(), parent, limit_str, current_consumption_str,
-                            peak_consumption_str);
-  }
+  ProcessMemTracker(MemTracker::GetRootTracker(), 0, output);
   *output << "</table>\n";
 }
-
-void AddDefaultPathHandlers(Webserver* webserver) {
-  webserver->RegisterPathHandler("/logs", "Logs", LogsHandler, true, false);
-  webserver->RegisterPathHandler("/varz", "Flags", FlagsHandler, true, false);
-  webserver->RegisterPathHandler("/status", "Status", StatusHandler, false, false);
-  webserver->RegisterPathHandler("/memz", "Memory (total)", MemUsageHandler, true, false);
-  webserver->RegisterPathHandler("/mem-trackers", "Memory (detail)",
-                                 MemTrackersHandler, true, false);
-
-  AddPprofPathHandlers(webserver);
-}
-
 
 static void WriteMetricsAsJson(const MetricRegistry* const metrics,
                                const Webserver::WebRequest& req, std::stringstream* output) {
@@ -244,6 +238,19 @@ static void WriteForPrometheus(const MetricRegistry* const metrics,
                                const Webserver::WebRequest& req, std::stringstream* output) {
   PrometheusWriter writer(output);
   WARN_NOT_OK(metrics->WriteForPrometheus(&writer), "Couldn't write text metrics for Prometheus");
+}
+
+} // anonymous namespace
+
+void AddDefaultPathHandlers(Webserver* webserver) {
+  webserver->RegisterPathHandler("/logs", "Logs", LogsHandler, true, false);
+  webserver->RegisterPathHandler("/varz", "Flags", FlagsHandler, true, false);
+  webserver->RegisterPathHandler("/status", "Status", StatusHandler, false, false);
+  webserver->RegisterPathHandler("/memz", "Memory (total)", MemUsageHandler, true, false);
+  webserver->RegisterPathHandler("/mem-trackers", "Memory (detail)",
+                                 MemTrackersHandler, true, false);
+
+  AddPprofPathHandlers(webserver);
 }
 
 void RegisterMetricsJsonHandler(Webserver* webserver, const MetricRegistry* const metrics) {
--- src/yb/server/webserver.cc
+++ src/yb/server/webserver.cc
@@ -411,8 +411,8 @@ const char* const PAGE_HEADER = "<!DOCTYPE html>"
 "    <link rel='shortcut icon' href='/favicon.ico'>"
 "    <link href='/bootstrap/css/bootstrap.min.css' rel='stylesheet' media='screen' />"
 "    <link href='/bootstrap/css/bootstrap-theme.min.css' rel='stylesheet' media='screen' />"
-"    <link rel=\"stylesheet\" href=\"/font-awesome/css/font-awesome.min.css\" />"
-"    <link href='/yb.css' rel='stylesheet' />"
+"    <link href='/font-awesome/css/font-awesome.min.css' rel='stylesheet' media='screen' />"
+"    <link href='/yb.css' rel='stylesheet' media='screen' />"
 "  </head>"
 "\n"
 "<body>"
@@ -462,9 +462,9 @@ void Webserver::set_footer_html(const std::string& html) {
 
 void Webserver::BootstrapPageFooter(stringstream* output) {
   boost::shared_lock<boost::shared_mutex> l(lock_);
-  *output << "</div>\n"; // end bootstrap 'container' div
+  *output << "<div class='yb-bottom-spacer'></div></div>\n"; // end bootstrap 'container' div
   if (!footer_html_.empty()) {
-    *output << "<footer class=\"footer\"><div class=\"yb-footer container text-muted\">";
+    *output << "<footer class='footer'><div class='yb-footer container text-muted'>";
     *output << footer_html_;
     *output << "</div></footer>";
   }
