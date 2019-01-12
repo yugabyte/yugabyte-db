yugabyte/yugabyte-db

commit 23f60107cb1845f4d8232207b2820fd616b47a43

#704 ENG-4422 Return a "Snapshot too old" error on attempts to read compacted-away data, e.g. during very long-running transactions
Summary:
Prior to this revision it was possible for a long-running distributed transaction to send a read request to a tablet server with a read timestamp that is lower than the "history cutoff" timestamp of a compaction that has already happened by the time the request started executing. This would result in reading incorrect data, with some values missing. This is really only possible through the PostgreSQL API, because in CQL it is hard to create long-running distributed transactions. A test is being added that reproduces this situation: we start a transaction, read a value (which should succeed), then call pg_sleep for 30 seconds, and during that time old data is being compacted away. Finally, the transaction does another read, which without the fix in this revision shows a row with a NULL value instead of the correct value.

src/yb/docdb/consensus_frontier.cc
@@ -27,6 +27,7 @@ void ConsensusFrontier::ToPB(google::protobuf::Any* any) const {
   ConsensusFrontierPB pb;
   op_id_.ToPB(pb.mutable_op_id());
   pb.set_hybrid_time(ht_.ToUint64());
+  pb.set_history_cutoff(history_cutoff_.ToUint64());
   any->PackFrom(pb);
 }
 
@@ -35,6 +36,11 @@ void ConsensusFrontier::FromPB(const google::protobuf::Any& any) {
   any.UnpackTo(&pb);
   op_id_ = OpId::FromPB(pb.op_id());
   ht_ = HybridTime(pb.hybrid_time());
+  if (pb.has_history_cutoff()) {
+    history_cutoff_ = HybridTime(pb.history_cutoff());
+  } else {
+    history_cutoff_ = HybridTime();
+  }
 }
 
 void ConsensusFrontier::FromOpIdPBDeprecated(const OpIdPB& pb) {
@@ -52,10 +58,12 @@ void ConsensusFrontier::Update(
     case rocksdb::UpdateUserValueType::kLargest:
       op_id_.MakeAtLeast(rhs.op_id_);
       ht_.MakeAtLeast(rhs.ht_);
+      history_cutoff_.MakeAtLeast(rhs.history_cutoff_);
       return;
     case rocksdb::UpdateUserValueType::kSmallest:
       op_id_.MakeAtMost(rhs.op_id_);
       ht_.MakeAtMost(rhs.ht_);
+      history_cutoff_.MakeAtMost(rhs.history_cutoff_);
       return;
   }
   FATAL_INVALID_ENUM_VALUE(rocksdb::UpdateUserValueType, type);

src/yb/docdb/consensus_frontier.h
@@ -19,11 +19,12 @@
 namespace yb {
 namespace docdb {
 
-// DocDB implementation of RocksDB UserFrontier. Sustains op id and hybrid time.
-// The distinction from user boundary values is that here hybrid time is taken from applied
-// Raft log entery, whereas user boundary values extract hybrid time from added values.
-// This is important for transactions, because values would have commit time of transaction,
-// but "apply intent" Raft log entries will have a later hybrid time.
+// DocDB implementation of RocksDB UserFrontier. Contains an op id and a hybrid time. The difference
+// between this and user boundary values is that here hybrid time is taken from committed Raft log
+// entries, whereas user boundary values extract hybrid time from keys in a memtable. This is
+// important for transactions, because boundary values would have the commit time of a transaction,
+// but e.g. "apply intent" Raft log entries will have a later hybrid time, which would be saved
+// here.
 class ConsensusFrontier : public rocksdb::UserFrontier {
  public:
   std::unique_ptr<UserFrontier> Clone() const override {
@@ -44,25 +45,37 @@ class ConsensusFrontier : public rocksdb::UserFrontier {
   void set_op_id(const PB& pb) { op_id_ = OpId::FromPB(pb); }
 
   HybridTime hybrid_time() const { return ht_; }
-  void set_hybrid_time(HybridTime value) { ht_ = value; }
+  void set_hybrid_time(HybridTime ht) { ht_ = ht; }
+
+  HybridTime history_cutoff() const { return history_cutoff_; }
+  void set_history_cutoff(HybridTime history_cutoff) { history_cutoff_ = history_cutoff; }
 
  private:
   OpId op_id_;
   HybridTime ht_;
+
+  // We use this to keep track of the maximum history cutoff hybrid time used in any compaction, and
+  // refuse to perform reads at a hybrid time at which we don't have a valid snapshot anymore. Only
+  // the largest frontier of this parameter is being used.
+  HybridTime history_cutoff_;
 };
 
 typedef rocksdb::UserFrontiersBase<ConsensusFrontier> ConsensusFrontiers;
 
-inline void set_op_id(const OpId& value, ConsensusFrontiers* frontiers) {
-  frontiers->Smallest().set_op_id(value);
-  frontiers->Largest().set_op_id(value);
+inline void set_op_id(const OpId& op_id, ConsensusFrontiers* frontiers) {
+  frontiers->Smallest().set_op_id(op_id);
+  frontiers->Largest().set_op_id(op_id);
 }
 
-inline void set_hybrid_time(HybridTime value, ConsensusFrontiers* frontiers) {
-  frontiers->Smallest().set_hybrid_time(value);
-  frontiers->Largest().set_hybrid_time(value);
+inline void set_hybrid_time(HybridTime hybrid_time, ConsensusFrontiers* frontiers) {
+  frontiers->Smallest().set_hybrid_time(hybrid_time);
+  frontiers->Largest().set_hybrid_time(hybrid_time);
 }
 
+inline void set_history_cutoff(HybridTime history_cutoff, ConsensusFrontiers* frontiers) {
+  frontiers->Smallest().set_history_cutoff(history_cutoff);
+  frontiers->Largest().set_history_cutoff(history_cutoff);
+}
 } // namespace docdb
 } // namespace yb
 

src/yb/rocksdb/db/compaction_job.cc
@@ -652,6 +652,13 @@ void CompactionJob::ProcessKeyValueCompaction(
         sub_compact->compaction->CreateCompactionFilter();
     compaction_filter = compaction_filter_from_factory.get();
   }
+
+  if (compaction_filter) {
+    // This is used to persist the history cutoff hybrid time chosen for the DocDB compaction
+    // filter.
+    largest_user_frontier_ = compaction_filter->GetLargestUserFrontier();
+  }
+
   MergeHelper merge(
       env_, cfd->user_comparator(), cfd->ioptions()->merge_operator,
       compaction_filter, db_options_.info_log.get(),
@@ -959,12 +966,19 @@ Status CompactionJob::InstallCompactionResults(
   }
 
   // Add compaction outputs
-  compaction->AddInputDeletions(compact_->compaction->edit());
+  compaction->AddInputDeletions(compaction->edit());
 
   for (const auto& sub_compact : compact_->sub_compact_states) {
     for (const auto& out : sub_compact.outputs) {
       compaction->edit()->AddFile(compaction->output_level(), out.meta);
     }
+  }
+  if (largest_user_frontier_) {
+    UserFrontier* existing_flushed_frontier = versions_->FlushedFrontier();
+    if (existing_flushed_frontier) {
+      largest_user_frontier_->Update(*existing_flushed_frontier, UpdateUserValueType::kLargest);
+    }
+    compaction->edit()->SetFlushedFrontier(std::move(largest_user_frontier_));
   }
   return versions_->LogAndApply(compaction->column_family_data(),
                                 mutable_cf_options, compaction->edit(),

commit 6f00cf95385330f52f9373a6a05f179121da646c

#730 Fix flushed frontier going backwards during a compaction
Summary:
There was a bug (https://github.com/yugabyte/yugabyte-db/issues/730) introduced in D5912 (
https://github.com/YugaByte/yugabyte-db/commit/c1494e94bcbe2dffbe5959e3e7f0f567f6132582 )
In that revision we added logic to persist the history cutoff timestamp used in a compaction in
the "flushed frontier" metadata that is stored in the VersionSet and added there through the VersionEdit
that a compaction produces. However, the way that was done (getting the current flushed frontier
value from VersionSet, updating it with history cutoff, and setting it in VersionEdit) resulted the flushed
frontier decreasing in case another flush happened between we prepared this VersionEdit in the end of
a compaction and installed it in VersionSet::LogAndApply. This is possible because LogAndApply releases
the RocksDB mutex as follows:

```
  while (!w.done && &w != manifest_writers_.front()) {
    w.cv.Wait();
  }
```

This triggered a debug-mode sanity check in tests, but it can also result incorrect (low) values
of the flushed frontier in release mode, and cause replay of too many log records on tablet
server restart.

The fix for this bug is to allow storing a value of a flushed frontier in a VersionEdit that is
not directly copied into the VersionSet, but is applied using the "update" procedure (taking
the maximum of each field).

The sanity check is also being improved: with DFATAL, we crash in debug mode and log an error
in release mode. The flushed frontier is always being updated to the maximum its current and
updated value.

As a reminder, the flushed frontier is a tuple consisting of various values (currently hybrid time,
OpId, and history cutoff). Hybrid time and OpId are set when writing key/value batches to
a memtable, based on the committed indexes and hybrid times of Raft log records. History cutoff is
chosen during a compaction. Flushed frontier is persisted in RocksDB metadata (version edits and

src/yb/docdb/CMakeLists.txt
@@ -114,3 +114,4 @@ ADD_YB_TEST(randomized_docdb-test)
 ADD_YB_TEST(shared_lock_manager-test)
 ADD_YB_TEST(subdocument-test)
 ADD_YB_TEST(value-test)
+ADD_YB_TEST(consensus_frontier-test)

src/yb/docdb/consensus_frontier.cc
@@ -48,25 +48,65 @@ void ConsensusFrontier::FromOpIdPBDeprecated(const OpIdPB& pb) {
 }
 
 std::string ConsensusFrontier::ToString() const {
-  return yb::Format("{ op_id: $0 hybrid_time: $1 }", op_id_, ht_);
+  return yb::Format(
+      "{ op_id: $0 hybrid_time: $1 history_cutoff: $2 }",
+      op_id_, ht_, history_cutoff_);
 }
 
-void ConsensusFrontier::Update(
-    const rocksdb::UserFrontier& pre_rhs, rocksdb::UpdateUserValueType type) {
-  const ConsensusFrontier& rhs = down_cast<const ConsensusFrontier&>(pre_rhs);
-  switch (type) {
+namespace {
+
+// Check if the given updated value is a correct "update" for the given previous value in the
+// specified direction. If one of the two values is not defined according to the bool conversion,
+// then there is no error.
+template<typename T>
+bool IsUpdateValidForField(
+    const T& this_value, const T& updated_value, rocksdb::UpdateUserValueType update_type) {
+  if (!this_value || !updated_value) {
+    // If any of the two values is undefined, we don't treat this as an error.
+    return true;
+  }
+  switch (update_type) {
     case rocksdb::UpdateUserValueType::kLargest:
-      op_id_.MakeAtLeast(rhs.op_id_);
-      ht_.MakeAtLeast(rhs.ht_);
-      history_cutoff_.MakeAtLeast(rhs.history_cutoff_);
+      return updated_value >= this_value;
+    case rocksdb::UpdateUserValueType::kSmallest:
+      return updated_value <= this_value;
+  }
+  FATAL_INVALID_ENUM_VALUE(rocksdb::UpdateUserValueType, update_type);
+}
+
+template<typename T>
+void UpdateField(
+    T* this_value, const T& new_value, rocksdb::UpdateUserValueType update_type) {
+  switch (update_type) {
+    case rocksdb::UpdateUserValueType::kLargest:
+      this_value->MakeAtLeast(new_value);
       return;
     case rocksdb::UpdateUserValueType::kSmallest:
-      op_id_.MakeAtMost(rhs.op_id_);
-      ht_.MakeAtMost(rhs.ht_);
-      history_cutoff_.MakeAtMost(rhs.history_cutoff_);
+      this_value->MakeAtMost(new_value);
       return;
   }
-  FATAL_INVALID_ENUM_VALUE(rocksdb::UpdateUserValueType, type);
+  FATAL_INVALID_ENUM_VALUE(rocksdb::UpdateUserValueType, update_type);
+}
+
+} // anonymous namespace
+
+void ConsensusFrontier::Update(
+    const rocksdb::UserFrontier& pre_rhs, rocksdb::UpdateUserValueType update_type) {
+  const ConsensusFrontier& rhs = down_cast<const ConsensusFrontier&>(pre_rhs);
+  UpdateField(&op_id_, rhs.op_id_, update_type);
+  UpdateField(&ht_, rhs.ht_, update_type);
+  UpdateField(&history_cutoff_, rhs.history_cutoff_, update_type);
+}
+
+bool ConsensusFrontier::IsUpdateValid(
+    const rocksdb::UserFrontier& pre_rhs, rocksdb::UpdateUserValueType update_type) const {
+  const ConsensusFrontier& rhs = down_cast<const ConsensusFrontier&>(pre_rhs);
+
+  // We don't check history cutoff here, because it is not an error when the the history cutoff
+  // for a later compaction is lower than that for an earlier compaction. This can happen if
+  // FLAGS_timestamp_history_retention_interval_sec increases.
+  return IsUpdateValidForField(op_id_, rhs.op_id_, update_type) &&
+         IsUpdateValidForField(ht_, rhs.ht_, update_type);
 }
 
 } // namespace docdb

src/yb/docdb/consensus_frontier.h
@@ -31,10 +31,18 @@ class ConsensusFrontier : public rocksdb::UserFrontier {
     return std::make_unique<ConsensusFrontier>(*this);
   }
 
+  ConsensusFrontier() {}
+  ConsensusFrontier(const OpId& op_id, HybridTime ht, HybridTime history_cutoff)
+      : op_id_(op_id),
+        ht_(ht),
+        history_cutoff_(history_cutoff) {}
+
   bool Equals(const UserFrontier& rhs) const override;
   std::string ToString() const override;
   void ToPB(google::protobuf::Any* pb) const override;
   void Update(const rocksdb::UserFrontier& rhs, rocksdb::UpdateUserValueType type) override;
+  bool IsUpdateValid(const rocksdb::UserFrontier& rhs, rocksdb::UpdateUserValueType type) const
+      override;
   void FromPB(const google::protobuf::Any& pb) override;
   void FromOpIdPBDeprecated(const OpIdPB& pb) override;
 

src/yb/rocksdb/db/compaction_job.cc
@@ -974,11 +974,7 @@ Status CompactionJob::InstallCompactionResults(
     }
   }
   if (largest_user_frontier_) {
-    UserFrontier* existing_flushed_frontier = versions_->FlushedFrontier();
-    if (existing_flushed_frontier) {
-      largest_user_frontier_->Update(*existing_flushed_frontier, UpdateUserValueType::kLargest);
-    }
-    compaction->edit()->SetFlushedFrontier(std::move(largest_user_frontier_));
+    compaction->edit()->SetFlushedFrontierUpdate(largest_user_frontier_);
   }
   return versions_->LogAndApply(compaction->column_family_data(),
                                 mutable_cf_options, compaction->edit(),

src/yb/rocksdb/db/db_impl.cc
@@ -1145,7 +1145,7 @@ Status DBImpl::RecoverLogFiles(const std::vector<uint64_t>& log_numbers,
     edit.SetColumnFamily(cfd->GetID());
     auto frontier = versions_->FlushedFrontier();
     if (frontier) {
-      edit.SetFlushedFrontier(frontier->Clone());
+      edit.SetFlushedFrontierUpdate(frontier->Clone());
     }
     version_edits.insert({cfd->GetID(), edit});
   }
@@ -5462,7 +5462,7 @@ Status DBImpl::ApplyVersionEdit(VersionEdit* edit) {
 
 Status DBImpl::SetFlushedFrontier(UserFrontierPtr frontier) {
   VersionEdit edit;
-  edit.SetFlushedFrontier(std::move(frontier));
+  edit.SetFlushedFrontierUpdate(std::move(frontier));
   return ApplyVersionEdit(&edit);
 }
 

src/yb/rocksdb/db/memtable_list.cc
@@ -349,7 +349,7 @@ Status MemTableList::InstallMemtableFlushResults(
   }
   if (frontiers) {
     DCHECK_NE(0, mems[0]->edit_.GetNewFiles().size());
-    mems[0]->edit_.SetFlushedFrontier(frontiers->Largest().Clone());
+    mems[0]->edit_.SetFlushedFrontierUpdate(frontiers->Largest().Clone());
   }
 
   // if some other thread is already committing, then return

src/yb/rocksdb/db/version_edit.h
@@ -161,7 +161,7 @@ class VersionEdit {
   void SetLastSequence(SequenceNumber seq) {
     last_sequence_ = seq;
   }
-  void SetFlushedFrontier(UserFrontierPtr value) {
+  void SetFlushedFrontierUpdate(UserFrontierPtr value) {
     flushed_frontier_ = std::move(value);
   }
   void SetMaxColumnFamily(uint32_t max_column_family) {
@@ -268,6 +268,7 @@ class VersionEdit {
   boost::optional<uint32_t> max_column_family_;
   boost::optional<SequenceNumber> last_sequence_;
   UserFrontierPtr flushed_frontier_;
+  UserFrontierPtr flushed_frontier_update_;
 
   DeletedFileSet deleted_files_;
   std::vector<std::pair<int, FileMetaData>> new_files_;

src/yb/rocksdb/db/version_set.cc
@@ -40,6 +40,7 @@
 #include <glog/logging.h>
 
 #include "yb/gutil/casts.h"
+#include "yb/util/flags.h"
 
 #include "yb/rocksdb/db/filename.h"
 #include "yb/rocksdb/db/file_numbers.h"
@@ -2336,7 +2337,7 @@ Status VersionSet::LogAndApply(ColumnFamilyData* column_family_data,
     manifest_file_size_ = new_manifest_file_size;
     prev_log_number_ = edit->prev_log_number_.get_value_or(0);
     if (edit->flushed_frontier_) {
-      SetFlushedFrontier(edit->flushed_frontier_);
+      UpdateFlushedFrontier(edit->flushed_frontier_);
     }
   } else {
     RLOG(InfoLogLevel::ERROR_LEVEL, db_options_->info_log,
@@ -2744,7 +2745,7 @@ Status VersionSet::Recover(
     SetLastSequenceNoSanityChecking(last_sequence);
     prev_log_number_ = previous_log_number;
     if (flushed_frontier) {
-      SetFlushedFrontierNoSanityChecking(std::move(flushed_frontier));
+      UpdateFlushedFrontierNoSanityChecking(std::move(flushed_frontier));
     }
 
     RLOG(InfoLogLevel::INFO_LEVEL, db_options_->info_log,
@@ -3228,6 +3229,25 @@ Status VersionSet::DumpManifest(const Options& options, const std::string& dscna
 }
 #endif  // ROCKSDB_LITE
 
+// Set the last sequence number to s.
+void VersionSet::SetLastSequence(SequenceNumber s) {
+#ifndef NDEBUG
+  EnsureNonDecreasingLastSequence(LastSequence(), s);
+#endif
+  SetLastSequenceNoSanityChecking(s);
+}
+
+// Set last sequence number without verifying that it always keeps increasing.
+void VersionSet::SetLastSequenceNoSanityChecking(SequenceNumber s) {
+  last_sequence_.store(s, std::memory_order_release);
+}
+
+// Set the last flushed op id / hybrid time / history cutoff to the specified set of values.
+void VersionSet::UpdateFlushedFrontier(UserFrontierPtr values) {
+  EnsureNonDecreasingFlushedFrontier(FlushedFrontier(), *values);
+  UpdateFlushedFrontierNoSanityChecking(std::move(values));
+}
+
 void VersionSet::MarkFileNumberUsedDuringRecovery(uint64_t number) {
   // only called during recovery which is single threaded, so this works because
   // there can't be concurrent calls
@@ -3670,28 +3690,33 @@ uint64_t VersionSet::GetTotalSstFilesSize(Version* dummy_versions) {
   return total_files_size;
 }
 
-#ifndef NDEBUG
 void VersionSet::EnsureNonDecreasingLastSequence(
     SequenceNumber prev_last_seq,
     SequenceNumber new_last_seq) {
   if (new_last_seq < prev_last_seq) {
-    LOG(FATAL) << "New last sequence id " << new_last_seq << " is lower than "
-               << "the previous last sequence " << prev_last_seq;
+    LOG(DFATAL) << "New last sequence id " << new_last_seq << " is lower than "
+                << "the previous last sequence " << prev_last_seq;
   }
 }
 
-void VersionSet::EnsureNonDecreasingFlushedFrontier(const UserFrontier* prev_value,
-                                                  const UserFrontier& new_value) {
+void VersionSet::EnsureNonDecreasingFlushedFrontier(
+    const UserFrontier* prev_value,
+    const UserFrontier& new_value) {
   if (!prev_value) {
     return;
   }
-  auto temp = prev_value->Clone();
-  temp->Update(new_value, UpdateUserValueType::kLargest);
-  if (*temp != new_value) {
-    LOG(FATAL) << "Flushed frontier decreased from " << prev_value->ToString() << " to "
-               << new_value.ToString();
+  if (!prev_value->IsUpdateValid(new_value, UpdateUserValueType::kLargest)) {
+    LOG(DFATAL) << "Attempt to decrease flushed frontier " << prev_value->ToString() << " to "
+                << new_value.ToString();
   }
 }
-#endif
+
+void VersionSet::UpdateFlushedFrontierNoSanityChecking(UserFrontierPtr values) {
+  if (flushed_frontier_) {
+    flushed_frontier_->Update(*values, UpdateUserValueType::kLargest);
+  } else {
+    flushed_frontier_ = std::move(values);
+  }
+}
 
 }  // namespace rocksdb

src/yb/rocksdb/db/version_set.h
@@ -676,29 +676,17 @@ class VersionSet {
   }
 
   // Set the last sequence number to s.
-  void SetLastSequence(SequenceNumber s) {
-#ifndef NDEBUG
-    EnsureNonDecreasingLastSequence(LastSequence(), s);
-#endif
-    SetLastSequenceNoSanityChecking(s);
-  }
+  void SetLastSequence(SequenceNumber s);
 
   // Set last sequence number without verifying that it always keeps increasing.
-  void SetLastSequenceNoSanityChecking(SequenceNumber s) {
-    last_sequence_.store(s, std::memory_order_release);
-  }
+  void SetLastSequenceNoSanityChecking(SequenceNumber s);
 
-  // Set the last flushed op id / hybrid time / history cutoff to the specified set of values.
-  void SetFlushedFrontier(UserFrontierPtr values) {
-#ifndef NDEBUG
-    EnsureNonDecreasingFlushedFrontier(FlushedFrontier(), *values);
-#endif
-    SetFlushedFrontierNoSanityChecking(std::move(values));
-  }
-
-  void SetFlushedFrontierNoSanityChecking(UserFrontierPtr values) {
-    flushed_frontier_ = std::move(values);
-  }
+  // Attempts to set the last flushed op id / hybrid time / history cutoff to the specified tuple of
+  // values. The current flushed frontier is always updated to the maximum of its current and
+  // supplied values for each dimension. This will DFATAL in case the supplied frontier regresses
+  // relative to the current frontier in any of its dimensions which have non-default (defined)
+  // values.
+  void UpdateFlushedFrontier(UserFrontierPtr values);
 
   // Mark the specified file number as used.
   // REQUIRED: this is only called during single-threaded recovery
@@ -785,12 +773,12 @@ class VersionSet {
   ColumnFamilyData* CreateColumnFamily(const ColumnFamilyOptions& cf_options,
                                        VersionEdit* edit);
 
-#ifndef NDEBUG
   static void EnsureNonDecreasingLastSequence(
       SequenceNumber prev_last_seq, SequenceNumber new_last_seq);
   static void EnsureNonDecreasingFlushedFrontier(
       const UserFrontier* prev_value, const UserFrontier& new_value);
-#endif
+
+  void UpdateFlushedFrontierNoSanityChecking(UserFrontierPtr values);
 
   std::unique_ptr<ColumnFamilySet> column_family_set_;
 

src/yb/rocksdb/metadata.h
@@ -35,6 +35,7 @@
 
 #include "yb/util/clone_ptr.h"
 #include "yb/util/slice.h"
+#include "yb/util/enums.h"
 
 #include "yb/rocksdb/types.h"
 
@@ -101,7 +102,15 @@ class UserFrontier {
   virtual void ToPB(google::protobuf::Any* pb) const = 0;
   virtual bool Equals(const UserFrontier& rhs) const = 0;
 
+  // Updates the user frontier with the new values from rhs.
   virtual void Update(const UserFrontier& rhs, UpdateUserValueType type) = 0;
+
+  // Checks if the given update is valid, i.e. that it does not move the fields of the frontier
+  // (such as OpId / hybrid time) in the direction opposite to that indicated by
+  // UpdateUserValueType. A "largest" update should only increase fields, and a "smallest" should
+  // only decrease them. Fields that are not set in rhs are not checked.
+  virtual bool IsUpdateValid(const UserFrontier& rhs, UpdateUserValueType type) const = 0;
+
   virtual void FromOpIdPBDeprecated(const yb::OpIdPB& op_id) = 0;
   virtual void FromPB(const google::protobuf::Any& pb) = 0;
 

src/yb/rocksdb/util/testutil.h
@@ -714,6 +714,17 @@ class TestUserFrontier : public UserFrontier {
     FATAL_INVALID_ENUM_VALUE(UpdateUserValueType, type);
   }
 
+  bool IsUpdateValid(const UserFrontier& rhs, UpdateUserValueType type) const override {
+    auto rhs_value = down_cast<const TestUserFrontier&>(rhs).value_;
+    switch (type) {
+      case UpdateUserValueType::kLargest:
+        return rhs_value >= value_;
+      case UpdateUserValueType::kSmallest:
+        return rhs_value <= value_;
+    }
+    FATAL_INVALID_ENUM_VALUE(UpdateUserValueType, type);
+  }
+
   void FromOpIdPBDeprecated(const yb::OpIdPB& op_id) override {}
 
   void FromPB(const google::protobuf::Any& pb) override {

src/yb/docdb/consensus_frontier-test.cc
@@ -1,0 +1,119 @@
+// Copyright (c) YugaByte, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+// in compliance with the License.  You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software distributed under the License
+// is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+// or implied.  See the License for the specific language governing permissions and limitations
+// under the License.
+//
+
+#include "yb/docdb/consensus_frontier.h"
+
+#include "yb/util/test_util.h"
+#include "yb/rocksdb/metadata.h"
+
+#include "yb/docdb/docdb.pb.h"
+
+using rocksdb::UpdateUserValueType;
+
+namespace yb {
+namespace docdb {
+
+class ConsensusFrontierTest : public YBTest {
+};
+
+namespace {
+
+std::string PbToString(const ConsensusFrontierPB& pb) {
+  google::protobuf::Any any;
+  any.PackFrom(pb);
+  ConsensusFrontier frontier;
+  frontier.FromPB(any);
+  return frontier.ToString();
+}
+
+}  // anonymous namespace
+
+TEST_F(ConsensusFrontierTest, TestUpdates) {
+  {
+    ConsensusFrontier frontier;
+    EXPECT_TRUE(frontier.Equals(frontier));
+    EXPECT_EQ(
+        "{ op_id: { term: 0 index: 0 } hybrid_time: <invalid> history_cutoff: <invalid> }",
+        frontier.ToString());
+    EXPECT_TRUE(frontier.IsUpdateValid(frontier, UpdateUserValueType::kLargest));
+    EXPECT_TRUE(frontier.IsUpdateValid(frontier, UpdateUserValueType::kSmallest));
+
+    ConsensusFrontier opid1{{0, 1}, HybridTime::kInvalid, HybridTime::kInvalid};
+    EXPECT_TRUE(frontier.IsUpdateValid(opid1, UpdateUserValueType::kLargest));
+  }
+
+  {
+    ConsensusFrontier frontier{{1, 1}, 1000_usec_ht, 500_usec_ht};
+    EXPECT_EQ(
+        "{ op_id: { term: 1 index: 1 } hybrid_time: { physical: 1000 } "
+            "history_cutoff: { physical: 500 } }",
+        frontier.ToString());
+    ConsensusFrontier higher_idx{{1, 2}, 1000_usec_ht, 500_usec_ht};
+    ConsensusFrontier higher_ht{{1, 1}, 1001_usec_ht, 500_usec_ht};
+    ConsensusFrontier higher_cutoff{{1, 1}, 1000_usec_ht, 501_usec_ht};
+    ConsensusFrontier higher_idx_lower_ht{{1, 2}, 999_usec_ht, 500_usec_ht};
+
+    EXPECT_TRUE(frontier.IsUpdateValid(higher_idx, UpdateUserValueType::kLargest));
+    EXPECT_TRUE(frontier.IsUpdateValid(higher_ht, UpdateUserValueType::kLargest));
+    EXPECT_FALSE(higher_idx.IsUpdateValid(frontier, UpdateUserValueType::kLargest));
+    EXPECT_FALSE(higher_ht.IsUpdateValid(frontier, UpdateUserValueType::kLargest));
+    EXPECT_FALSE(frontier.IsUpdateValid(higher_idx, UpdateUserValueType::kSmallest));
+    EXPECT_FALSE(frontier.IsUpdateValid(higher_ht, UpdateUserValueType::kSmallest));
+    EXPECT_TRUE(higher_idx.IsUpdateValid(frontier, UpdateUserValueType::kSmallest));
+    EXPECT_TRUE(higher_ht.IsUpdateValid(frontier, UpdateUserValueType::kSmallest));
+
+    EXPECT_FALSE(higher_idx_lower_ht.IsUpdateValid(frontier, UpdateUserValueType::kLargest));
+    EXPECT_FALSE(frontier.IsUpdateValid(higher_idx_lower_ht, UpdateUserValueType::kSmallest));
+
+    // It is OK if a later compaction runs at a lower history_cutoff.
+    EXPECT_TRUE(frontier.IsUpdateValid(higher_cutoff, UpdateUserValueType::kLargest));
+    EXPECT_TRUE(frontier.IsUpdateValid(higher_cutoff, UpdateUserValueType::kSmallest));
+    EXPECT_TRUE(higher_cutoff.IsUpdateValid(frontier, UpdateUserValueType::kLargest));
+    EXPECT_TRUE(higher_cutoff.IsUpdateValid(frontier, UpdateUserValueType::kSmallest));
+
+    // Zero OpId should be considered as an undefined value, not causing any errors.
+    ConsensusFrontier zero_op_id{{0, 0}, HybridTime::kInvalid, HybridTime::kInvalid};
+    EXPECT_TRUE(frontier.IsUpdateValid(zero_op_id, UpdateUserValueType::kLargest));
+    EXPECT_TRUE(frontier.IsUpdateValid(zero_op_id, UpdateUserValueType::kSmallest));
+    EXPECT_TRUE(zero_op_id.IsUpdateValid(frontier, UpdateUserValueType::kLargest));
+    EXPECT_TRUE(zero_op_id.IsUpdateValid(frontier, UpdateUserValueType::kSmallest));
+  }
+
+  ConsensusFrontierPB pb;
+  pb.mutable_op_id()->set_term(0);
+  pb.mutable_op_id()->set_index(0);
+  EXPECT_EQ(
+      PbToString(pb),
+      "{ op_id: { term: 0 index: 0 } hybrid_time: <min> history_cutoff: <invalid> }");
+
+  pb.mutable_op_id()->set_term(2);
+  pb.mutable_op_id()->set_index(3);
+  EXPECT_EQ(
+      PbToString(pb),
+      "{ op_id: { term: 2 index: 3 } hybrid_time: <min> history_cutoff: <invalid> }");
+
+  pb.set_hybrid_time(100000);
+  EXPECT_EQ(
+      PbToString(pb),
+      "{ op_id: { term: 2 index: 3 } hybrid_time: { physical: 24 logical: 1696 } "
+          "history_cutoff: <invalid> }");
+
+  pb.set_history_cutoff(200000);
+  EXPECT_EQ(
+        PbToString(pb),
+        "{ op_id: { term: 2 index: 3 } hybrid_time: { physical: 24 logical: 1696 } "
+            "history_cutoff: { physical: 48 logical: 3392 } }");
+}
+
+}  // namespace docdb
+}  // namespace yb

src/yb/rocksdb/db/compaction_job_test.cc
@@ -173,7 +173,7 @@ class CompactionJobTest : public testing::Test {
   void SetLastSequence(const SequenceNumber sequence_number) {
     versions_->SetLastSequence(sequence_number + 1);
     test::TestUserFrontier frontier(sequence_number + 1);
-    versions_->SetFlushedFrontier(frontier.Clone());
+    versions_->UpdateFlushedFrontier(frontier.Clone());
   }
 
   // returns expected result after compaction

src/yb/rocksdb/db/version_edit_test.cc
@@ -58,7 +58,7 @@ void SetupVersionEdit(VersionEdit* edit) {
   edit->SetNextFile(kBig + 200);
   edit->SetLastSequence(kBig + 1000);
   test::TestUserFrontier frontier(kBig + 100);
-  edit->SetFlushedFrontier(frontier.Clone());
+  edit->SetFlushedFrontierUpdate(frontier.Clone());
 }
 
 TEST_F(VersionEditTest, EncodeDecode) {