92e779a725710743a41f3a195452a94fbd14e2bd
Kyotaro Horiguchi <horiguchi.kyotaro@lab.ntt.co.jp>
2019-01-17 12:33:00 +0900

    Change version to 1.3.3
    Version bumped to 1.3.3. SPEC file is updated.

---
+++ pg_hint_plan--1.3.2--1.3.3.sql
@@ -0,0 +1,6 @@
+/* pg_hint_plan/pg_hint_plan--1.3.2--1.3.3.sql */
+
+-- complain if script is sourced in psql, rather than via CREATE EXTENSION
+\echo Use "CREATE EXTENSION pg_hint_plan" to load this file. \quit
+
+-- nothing to do upgrading from 1.3.2.
