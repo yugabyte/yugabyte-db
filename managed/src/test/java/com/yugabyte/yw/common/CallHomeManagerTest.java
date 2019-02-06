23197f19c739107a69d31db699517c5317fd9ea4
Ramkumar Vaidyanathan <ramkumarvs@users.noreply.github.com>
2019-02-06 11:57:10 -0800

    Callhome tweaks
    Summary:
    - Updated the callhome endpoint to take a auth token
    - Added some error handling
    
    Test Plan:
    sbt run
    and validated the payload sent out.
    sbt test
    
    Reviewers: bogdan, harshith
    
    Reviewed By: harshith
    
    Subscribers: jenkins-bot, yugaware
    
    Differential Revision: https://phabricator.dev.yugabyte.com/D6116

--- managed/src/test/java/com/yugabyte/yw/common/CallHomeManagerTest.java
+++ managed/src/test/java/com/yugabyte/yw/common/CallHomeManagerTest.java
@@ -11,9 +11,11 @@ import com.yugabyte.yw.models.Provider;
 import com.yugabyte.yw.models.Region;
 import com.yugabyte.yw.models.Universe;
 
-import org.junit.Assert;
+import org.asynchttpclient.util.Base64;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.ArgumentCaptor;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 
@@ -22,14 +24,13 @@ import org.mockito.runners.MockitoJUnitRunner;
 import java.time.Clock;
 import java.time.Instant;
 import java.util.Map;
+import java.util.UUID;
 
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import play.libs.Json;
 
 import com.google.common.collect.ImmutableMap;
 
+import static org.junit.Assert.assertEquals;
 import static org.mockito.Mockito.*;
 
 @RunWith(MockitoJUnitRunner.class)
@@ -47,33 +48,43 @@ public class CallHomeManagerTest extends FakeDBApplication {
   @Mock
   Clock clock;
 
-  static final Logger LOG = LoggerFactory.getLogger(CallHomeManagerTest.class);
+  Customer defaultCustomer;
+  Provider defaultProvider;
 
-  private JsonNode callHomePayload(Customer c, Provider p, Universe u){
+  @Before
+  public void setUp() {
+    defaultCustomer = ModelFactory.testCustomer();
+    defaultProvider = ModelFactory.awsProvider(defaultCustomer);
+  }
+
+  private JsonNode callHomePayload(Universe universe){
     ObjectNode expectedPayload = Json.newObject();
-    expectedPayload.put("customer_uuid", c.uuid.toString());
-    expectedPayload.put("code", c.code);
-    expectedPayload.put("email", c.email);
-    expectedPayload.put("creation_date", c.creationDate.toString());
+    expectedPayload.put("customer_uuid", defaultCustomer.uuid.toString());
+    expectedPayload.put("code", defaultCustomer.code);
+    expectedPayload.put("email", defaultCustomer.email);
+    expectedPayload.put("creation_date", defaultCustomer.creationDate.toString());
     ArrayNode universes = Json.newArray();
-    universes.add(u.toJson());
+    if (universe != null) {
+      universes.add(universe.toJson());
+    }
     ArrayNode providers = Json.newArray();
     ObjectNode provider = Json.newObject();
-    provider.put("provider_uuid", p.uuid.toString());
-    provider.put("code", p.code);
-    provider.put("name", p.name);
+    provider.put("provider_uuid", defaultProvider.uuid.toString());
+    provider.put("code", defaultProvider.code);
+    provider.put("name", defaultProvider.name);
     ArrayNode regions = Json.newArray();
-    for (Region r : p.regions) {
+    for (Region r : defaultProvider.regions) {
       regions.add(r.details);
     }
-    provider.put("regions", regions);
+    provider.set("regions", regions);
     providers.add(provider);
-    expectedPayload.put("universes", universes);
-    expectedPayload.put("providers", providers);
+    expectedPayload.set("universes", universes);
+    expectedPayload.set("providers", providers);
     Map<String, Object> ywMetadata = configHelper.getConfig(ConfigHelper.ConfigType.YugawareMetadata);
     expectedPayload.put("yugaware_uuid", ywMetadata.get("yugaware_uuid").toString());
     expectedPayload.put("version", ywMetadata.get("version").toString());
     expectedPayload.put("timestamp", clock.instant().getEpochSecond());
+    expectedPayload.set("errors", Json.newArray());
     return expectedPayload;
   }
 
@@ -86,20 +97,64 @@ public class CallHomeManagerTest extends FakeDBApplication {
     verifyZeroInteractions(apiHelper);
   }
 
+
+  @Test
+  public void testSendDiagnostics() {
+    when(configHelper.getConfig(ConfigHelper.ConfigType.YugawareMetadata))
+        .thenReturn(ImmutableMap.of("yugaware_uuid", "0146179d-a623-4b2a-a095-bfb0062eae9f",
+            "version", "0.0.1"));
+    when(clock.instant()).thenReturn(Instant.parse("2019-01-24T18:46:07.517Z"));
+    ObjectNode responseJson = Json.newObject();
+    responseJson.put("success", true);
+    when(apiHelper.postRequest(anyString(), any(), anyMap())).thenReturn(Json.toJson(responseJson));
+    callHomeManager.sendDiagnostics(defaultCustomer);
+
+    ArgumentCaptor<String> url = ArgumentCaptor.forClass(String.class);
+    ArgumentCaptor<JsonNode> params = ArgumentCaptor.forClass(JsonNode.class);
+    ArgumentCaptor<Map> headers = ArgumentCaptor.forClass(Map.class);
+
+    verify(apiHelper).postRequest(url.capture(), params.capture(), (Map<String, String>) headers.capture());
+    ObjectNode expectedPayload = (ObjectNode) callHomePayload(null);
+    assertEquals(expectedPayload, params.getValue());
+
+    System.out.println(params.getValue());
+    String expectedToken = Base64.encode(defaultCustomer.uuid.toString().getBytes());
+    assertEquals(expectedToken, headers.getValue().get("X-AUTH-TOKEN"));
+    assertEquals("http://yw-diagnostics.yugabyte.com", url.getValue());
+  }
+
   @Test
   public void testCollectDiagnostics() {
     when(configHelper.getConfig(ConfigHelper.ConfigType.YugawareMetadata))
       .thenReturn(ImmutableMap.of("yugaware_uuid", "0146179d-a623-4b2a-a095-bfb0062eae9f",
                                   "version", "0.0.1"));
     when(clock.instant()).thenReturn(Instant.parse("2019-01-24T18:46:07.517Z"));
-    Customer c = ModelFactory.testCustomer();
-    Provider p = ModelFactory.awsProvider(c);
-    Universe u = ModelFactory.createUniverse(c.getCustomerId());
-    c.addUniverseUUID(u.universeUUID);
+    Universe u = ModelFactory.createUniverse(defaultCustomer.getCustomerId());
+    defaultCustomer.addUniverseUUID(u.universeUUID);
     // Need to save customer with the new universe or else Customer.getUniverses() won't find any.
-    c.save();
-    JsonNode expectedPayload = callHomePayload(c, p, u);
-    JsonNode actualPayload = callHomeManager.CollectDiagnostics(c);
-    Assert.assertEquals(expectedPayload, actualPayload);
+    defaultCustomer.save();
+    JsonNode expectedPayload = callHomePayload(u);
+    JsonNode actualPayload = callHomeManager.CollectDiagnostics(defaultCustomer);
+    assertEquals(expectedPayload, actualPayload);
+  }
+
+  @Test
+  public void testCollectDiagnosticsWithInvalidUniverses() {
+    when(configHelper.getConfig(ConfigHelper.ConfigType.YugawareMetadata))
+        .thenReturn(ImmutableMap.of("yugaware_uuid", "0146179d-a623-4b2a-a095-bfb0062eae9f",
+            "version", "0.0.1"));
+    when(clock.instant()).thenReturn(Instant.parse("2019-01-24T18:46:07.517Z"));
+    Universe u1 = ModelFactory.createUniverse(defaultCustomer.getCustomerId());
+    UUID unknownUniverse = UUID.randomUUID();
+    defaultCustomer.addUniverseUUID(u1.universeUUID);
+    defaultCustomer.addUniverseUUID(unknownUniverse);
+
+    // Need to save customer with the new universe or else Customer.getUniverses() won't find any.
+    defaultCustomer.save();
+    ObjectNode expectedPayload = (ObjectNode) callHomePayload(u1);
+    expectedPayload.set("errors", Json.newArray().add("Cannot find universe " + unknownUniverse));
+    JsonNode actualPayload = callHomeManager.CollectDiagnostics(defaultCustomer);
+    assertEquals(expectedPayload, actualPayload);
+
   }
 }
