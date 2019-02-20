yugabyte/yugabyte-db

commit c8b129ddd93fe228d04264a7d12f4946743191d8

ENG-4602: [YCQL] Add support for more timezone formats #806
Summary:
This new behavior (functionality) is controlled by a newly added gflag "use_icu_timezones", which defaults to true. To revert back to old behavior, set this flag to false.

Added support for all cassandra supported types including undocumented such as PST,
by moving to ICU TimeZone Library. But to support type UTC+08:00 which is supported
by ybase and not cassandra, some extra work was needed.

Subset of supported Timezone formats https://docs.oracle.com/cd/E51711_01/DR/ICU_Time_Zones.html
Full database can be found at https://www.iana.org/time-zones
We support everything that Cassandra supports, like z/Z, +/-0800, +/-08:30 GMT+/-[0]7:00, and we also support UTC+/-[0]9:30 which Cassandra does not support

Test Plan:
manually tested
Commands with output:
```
cqlsh> create keyspace test;
cqlsh> use test;
cqlsh:test> create table foo (x int, b timestamp, primary key (x));
cqlsh:test> insert into foo(x,b) values (1, '2019-01-26T00:03:16.059-0830');
cqlsh:test> insert into foo(x,b) values (2, '2019-01-26T00:03:16.059Z');
cqlsh:test> insert into foo(x,b) values (3, '2019-01-26T00:03:16.059 PST');
cqlsh:test> insert into foo(x,b) values (4, '2019-01-26T00:03:16.059 America/New_York');
cqlsh:test> insert into foo(x,b) values (5, '2019-01-26T00:03:16.059 GMT-4:00');
cqlsh:test> select * from foo;

 x | b
---+---------------------------------
 5 | 2019-01-26 04:03:16.059000+0000
 1 | 2019-01-25 15:33:16.059000+0000
 4 | 2019-01-26 05:03:16.059000+0000
 2 | 2019-01-26 00:03:16.059000+0000
 3 | 2019-01-26 08:03:16.059000+0000

(5 rows)
```

added tests for new timezones in
```
java/yb-cql/src/test/java/org/yb/cql/TestTimestampDataType.java
```

Reviewers: ravi, hector, mihnea

Reviewed By: mihnea

Subscribers: kannan, ybase

Differential Revision: https://phabricator.dev.yugabyte.com/D6112

src/yb/util/date_time.cc
@@ -20,6 +20,7 @@
 #include <regex>
 #include <ctime>
 
+#include <gflags/gflags.h>
 #include "yb/util/date_time.h"
 #include "yb/util/logging.h"
 #include "boost/date_time/gregorian/gregorian.hpp"
@@ -32,6 +33,7 @@ using std::string;
 using std::regex;
 using icu::GregorianCalendar;
 using icu::TimeZone;
+using icu::UnicodeString;
 using boost::gregorian::date;
 using boost::local_time::local_date_time;
 using boost::local_time::local_time_facet;
@@ -42,6 +44,9 @@ using boost::posix_time::ptime;
 using boost::posix_time::microseconds;
 using boost::posix_time::time_duration;
 using boost::posix_time::microsec_clock;
+using boost::posix_time::milliseconds;
+
+DEFINE_bool(use_icu_timezones, true, "Use the new ICU library for timezones instead of boost");
 
 namespace yb {
 
@@ -102,6 +107,50 @@ string GetSystemTimezone() {
   return buffer;
 }
 
+/* Subset of supported Timezone formats https://docs.oracle.com/cd/E51711_01/DR/ICU_Time_Zones.html
+ * Full database can be found at https://www.iana.org/time-zones
+ * We support everything that Cassandra supports, like z/Z, +/-0800, +/-08:30 GMT+/-[0]7:00,
+ * and we also support UTC+/-[0]9:30 which Cassandra does not support
+ */
+Result<string> GetTimezone(string timezoneID) {
+  /* Parse timezone offset from string in most formats of timezones
+   * Some formats are supported by ICU and some different ones by Boost::PosixTime
+   * To capture both, return posix supported directly, and for ICU, create ICU Timezone and then
+   * convert to a supported Posix format.
+   */
+  // [+/-]0830 is not supported by ICU TimeZone or Posixtime so need to do some extra work
+  std::smatch m;
+  std::regex rgx = regex("(?:\\+|-)(\\d{2})(\\d{2})");
+  if (timezoneID.empty()) {
+    return GetSystemTimezone();
+  } else if (timezoneID == "z" || timezoneID == "Z") {
+    timezoneID = "GMT";
+  } else if (std::regex_match(timezoneID, m , rgx)) {
+    return m.str(1) + ":" + m.str(2);
+  } else if (timezoneID.at(0) == '+' || timezoneID.at(0) == '-' ||
+             timezoneID.substr(0, 3) == "UTC") {
+    return timezoneID;
+  }
+  TimeZone *tzone = TimeZone::createTimeZone(timezoneID.c_str());
+  UnicodeString id;
+  tzone->getID(id);
+  string timezone;
+  id.toUTF8String(timezone);
+  if (*tzone == TimeZone::getUnknown()) {
+    return STATUS(InvalidArgument, "Invalid Timezone: " + timezoneID +
+        "\nUse standardized timezone such as \"America/New_York\" or offset such as UTC-07:00.");
+  }
+  time_duration td = milliseconds(tzone->getRawOffset());
+  const int hours = td.hours();
+  const int minutes = td.minutes();
+  char buffer[7]; // "+HH:MM" or "-HH:MM"
+  const int result = snprintf(buffer, sizeof(buffer), "%+2.2d:%2.2d", hours, abs(minutes));
+  if (result <= 0 || result >= sizeof(buffer)) {
+    return STATUS(Corruption, "Parsing timezone into timezone offset string failed");
+  }
+  return buffer;
+}
+
 } // namespace
 
 //------------------------------------------------------------------------------------------------
@@ -124,8 +173,14 @@ Result<Timestamp> DateTime::TimestampFromString(const string& str,
       try {
         const date d(year, month, day);
         const time_duration t(hours, minutes, seconds, frac);
-        const time_zone_ptr tz(new posix_time_zone(m.str(8).empty() ? GetSystemTimezone()
-                                                                    : m.str(8)));
+        posix_time_zone *ptz;
+        if (FLAGS_use_icu_timezones) {
+          ptz = new posix_time_zone(VERIFY_RESULT(GetTimezone(m.str(8))));
+        } else {
+          ptz = new posix_time_zone(m.str(8).empty() ? GetSystemTimezone()
+            : m.str(8));
+        }
+        const time_zone_ptr tz(ptz);
         return ToTimestamp(local_date_time(d, t, tz, local_date_time::NOT_DATE_TIME_ON_ERROR));
       } catch (std::exception& e) {
         return STATUS(InvalidArgument, "Invalid timestamp", e.what());
@@ -302,6 +357,9 @@ const DateTime::InputFormat DateTime::CqlInputFormat = []() -> InputFormat {
   string frac_fmt = "\\.(\\d{1,3})";
   // Offset, i.e. +/-xx:xx, +/-0000, timezone parser will do additional checking.
   string tzX_fmt = "((?:\\+|-)\\d{2}:?\\d{2})";
+  // Zulu Timezone e.g allows user to just add z or Z at the end with no space in front to indicate
+  // Zulu Time which is equivlent to GMT/UTC.
+  string tzY_fmt = "([zZ])";
   // Timezone name, abbreviation, or offset (preceded by space), e.g. PDT, UDT+/-xx:xx, etc..
   // At this point this allows anything that starts with a letter or '+' (after space), and leaves
   // further processing to the timezone parser.
@@ -313,54 +371,72 @@ const DateTime::InputFormat DateTime::CqlInputFormat = []() -> InputFormat {
       regex(date_fmt + " " + time_fmt_no_sec + fmt_empty + fmt_empty),
       // e.g. "1992-06-04 12:30+04:00" or "1992-6-4 12:30-04:30"
       regex(date_fmt + " " + time_fmt_no_sec + fmt_empty + tzX_fmt),
+      // e.g. "1992-06-04 12:30 UTCz" or "1992-6-4 12:30Z"
+      regex(date_fmt + " " + time_fmt_no_sec + fmt_empty + tzY_fmt),
       // e.g. "1992-06-04 12:30 UTC+04:00" or "1992-6-4 12:30 UTC-04:30"
       regex(date_fmt + " " + time_fmt_no_sec + fmt_empty + tzZ_fmt),
       // e.g. "1992-06-04 12:30.321" or "1992-6-4 12:30.12"
       regex(date_fmt + " " + time_fmt_no_sec + frac_fmt + fmt_empty),
       // e.g. "1992-06-04 12:30.321+04:00" or "1992-6-4 12:30.12-04:30"
       regex(date_fmt + " " + time_fmt_no_sec + frac_fmt + tzX_fmt),
+      // e.g. "1992-06-04 12:30.321z" or "1992-6-4 12:30.12Z"
+      regex(date_fmt + " " + time_fmt_no_sec + frac_fmt + tzY_fmt),
       // e.g. "1992-06-04 12:30.321 UTC+04:00" or "1992-6-4 12:30.12 UTC-04:30"
       regex(date_fmt + " " + time_fmt_no_sec + frac_fmt + tzZ_fmt),
       // e.g. "1992-06-04 12:30:45" or "1992-6-4 12:30:45"
       regex(date_fmt + " " + time_fmt + fmt_empty + fmt_empty),
       // e.g. "1992-06-04 12:30:45+04:00" or "1992-6-4 12:30:45-04:30"
       regex(date_fmt + " " + time_fmt + fmt_empty + tzX_fmt),
+      // e.g. "1992-06-04 12:30:45z" or "1992-6-4 12:30:45Z"
+      regex(date_fmt + " " + time_fmt + fmt_empty + tzY_fmt),
       // e.g. "1992-06-04 12:30:45 UTC+04:00" or "1992-6-4 12:30:45 UTC-04:30"
       regex(date_fmt + " " + time_fmt + fmt_empty + tzZ_fmt),
       // e.g. "1992-06-04 12:30:45.321" or "1992-6-4 12:30:45.12"
       regex(date_fmt + " " + time_fmt + frac_fmt + fmt_empty),
       // e.g. "1992-06-04 12:30:45.321+04:00" or "1992-6-4 12:30:45.12-04:30"
       regex(date_fmt + " " + time_fmt + frac_fmt + tzX_fmt),
+      // e.g. "1992-06-04 12:30:45.321z" or "1992-6-4 12:30:45.12Z"
+      regex(date_fmt + " " + time_fmt + frac_fmt + tzY_fmt),
       // e.g. "1992-06-04 12:30:45.321 UTC+04:00" or "1992-6-4 12:30:45.12 UTC-04:30"
       regex(date_fmt + " " + time_fmt + frac_fmt + tzZ_fmt),
       // e.g. "1992-06-04T12:30" or "1992-6-4T12:30"
       regex(date_fmt + "T" + time_fmt_no_sec + fmt_empty + fmt_empty),
       // e.g. "1992-06-04T12:30+04:00" or "1992-6-4T12:30-04:30"
       regex(date_fmt + "T" + time_fmt_no_sec + fmt_empty + tzX_fmt),
-      // e.g. "1992-06-04T12:30 UTC+04:00" or "1992-6-4T12:30TUTC-04:30"
+      // e.g. "1992-06-04T12:30z" or "1992-6-4T12:30TZ"
+      regex(date_fmt + "T" + time_fmt_no_sec + fmt_empty + tzY_fmt),
+      // e.g. "1992-06-04T12:30 UTC+04:00" or "1992-6-4T12:30T UTC-04:30"
       regex(date_fmt + "T" + time_fmt_no_sec + fmt_empty + tzZ_fmt),
       // e.g. "1992-06-04T12:30.321" or "1992-6-4T12:30.12"
       regex(date_fmt + "T" + time_fmt_no_sec + frac_fmt + fmt_empty),
       // e.g. "1992-06-04T12:30.321+04:00" or "1992-6-4T12:30.12-04:30"
       regex(date_fmt + "T" + time_fmt_no_sec + frac_fmt + tzX_fmt),
+      // e.g. "1992-06-04T12:30.321z" or "1992-6-4T12:30.12Z"
+      regex(date_fmt + "T" + time_fmt_no_sec + frac_fmt + tzY_fmt),
       // e.g. "1992-06-04T12:30.321 UTC+04:00" or "1992-6-4T12:30.12 UTC-04:30"
       regex(date_fmt + "T" + time_fmt_no_sec + frac_fmt + tzZ_fmt),
       // e.g. "1992-06-04T12:30:45" or "1992-6-4T12:30:45"
       regex(date_fmt + "T" + time_fmt + fmt_empty + fmt_empty),
       // e.g. "1992-06-04T12:30:45+04:00" or "1992-6-4T12:30:45-04:30"
       regex(date_fmt + "T" + time_fmt + fmt_empty + tzX_fmt),
+      // e.g. "1992-06-04T12:30:45z" or "1992-6-4T12:30:45Z"
+      regex(date_fmt + "T" + time_fmt + fmt_empty + tzY_fmt),
       // e.g. "1992-06-04T12:30:45 UTC+04:00" or "1992-6-4T12:30:45 UTC-04:30"
       regex(date_fmt + "T" + time_fmt + fmt_empty + tzZ_fmt),
       // e.g. "1992-06-04T12:30:45.321" or "1992-6-4T12:30:45.12"
       regex(date_fmt + "T" + time_fmt + frac_fmt + fmt_empty),
       // e.g. "1992-06-04T12:30:45.321+04:00" or "1992-6-4T12:30:45.12-04:30"
       regex(date_fmt + "T" + time_fmt + frac_fmt + tzX_fmt),
+      // e.g. "1992-06-04T12:30:45.321z" or "1992-6-4T12:30:45.12Z"
+      regex(date_fmt + "T" + time_fmt + frac_fmt + tzY_fmt),
       // e.g. "1992-06-04T12:30:45.321 UTC+04:00" or "1992-6-4T12:30:45.12 UTC-04:30"
       regex(date_fmt + "T" + time_fmt + frac_fmt + tzZ_fmt),
       // e.g. "1992-06-04" or "1992-6-4"
       regex(date_fmt + time_empty + fmt_empty + fmt_empty),
       // e.g. "1992-06-04+04:00" or "1992-6-4-04:30"
       regex(date_fmt + time_empty + fmt_empty + tzX_fmt),
+      // e.g. "1992-06-04z" or "1992-6-4Z"
+      regex(date_fmt + time_empty + fmt_empty + tzY_fmt),
       // e.g. "1992-06-04 UTC+04:00" or "1992-6-4 UTC-04:30"
       regex(date_fmt + time_empty + fmt_empty + tzZ_fmt)};
   int input_precision = 3; // Cassandra current default

java/yb-cql/src/test/java/org/yb/cql/TestTimestampDataType.java
@@ -44,14 +44,22 @@ public class TestTimestampDataType extends BaseCQLTest {
     ts_values.put("631238445000", cal.getTime());
 
     // Generate String inputs as combinations of valid components (date/time/frac_seconds/timezone).
-    int nr_entries = 3;
-    String[] dates = {"'1992-06-04", "'1992-6-4", "'1992-06-4"};
-    String[] times_no_sec = {"12:30", "15:30", "9:00"};
-    String[] times = {"12:30:45", "15:30:45", "9:00:45"};
-    String[] times_frac = {"12:30:45.1", "15:30:45.10", "9:00:45.100"};
+    int nr_entries = 7;
+    String[] dates = {"'1992-06-04", "'1992-6-4", "'1992-06-4", "'1992-06-4",
+                      "'1992-06-4", "'1992-06-4"};
+    String[] times_no_sec = {"12:30", "15:30", "9:00", "4:30", "12:30", "21:00", "5:30"};
+    String[] times = {"12:30:45", "15:30:45", "9:00:45", "4:30:45", "12:30:45",
+                      "21:00:45", "5:30:45"};
+    String[] times_frac = {"12:30:45.1", "15:30:45.10", "9:00:45.100", "4:30:45.100",
+                           "12:30:45.100", "21:00:45.100", "5:30:45.100"};
     // Timezones correspond one-to-one with times
     //   -- so that the UTC-normalized time is the same
-    String[] timezones = {" UTC'", "+03:00'", " UTC-03:30'"};
+    // Subset of supported TZ formats https://docs.oracle.com/cd/E51711_01/DR/ICU_Time_Zones.html
+    // Full database can be found at https://www.iana.org/time-zones
+    // We support everything that Cassandra supports, like z/Z, +/-0800, +/-08:30 GMT+/-[0]7:00,
+    // and we also support UTC+/-[0]9:30 which Cassandra does not support
+    String[] timezones = {" UTC'", "+03:00'", " UTC-03:30'",
+                          " PST'", "Z'", "+0830'", " GMT-7:00'"};
     for (String date : dates) {
       cal.setTimeZone(TimeZone.getTimeZone("GMT")); // resetting
       cal.setTimeInMillis(0); // resetting
@@ -105,6 +113,21 @@ public class TestTimestampDataType extends BaseCQLTest {
     }
   }
 
+  @Test
+  public void testInsertVariableTimezone() throws Exception {
+    String tableName = "test";
+    String newTimestamp = "\'2019-01-26T00:03:16.059 America/New_York\'";
+    session.execute(String.format("CREATE TABLE %s(x int primary key, b timestamp);", tableName));
+    String ins_stmt = String.format(
+            "INSERT INTO %s(x, b) VALUES(%d, %s);",
+            tableName, 1, newTimestamp);
+    session.execute(ins_stmt);
+    String sel_stmt = String.format("SELECT * FROM %s", tableName);
+    String gmtTime = runSelect(sel_stmt).next().getTimestamp(1).toGMTString();
+    assertTrue(gmtTime.equals("26 Jan 2019 05:03:16 GMT") ||
+               gmtTime.equals("26 Jan 2019 04:03:16 GMT"));
+  }
+
   private void runInvalidInsert(String tableName, String ts) {
     String insert_stmt = String.format(
         "INSERT INTO %s(h1, h2, r1, r2, v1, v2) VALUES(%d, %d, %d, %d, %d, '%s');",
@@ -126,6 +149,11 @@ public class TestTimestampDataType extends BaseCQLTest {
     runInvalidInsert(tableName, "2017-12-21 00:00:01.000+123:30");
     runInvalidInsert(tableName, "2017-12-21 00:00:01.0000");
     runInvalidInsert(tableName, "2017-12-21 00:00:01.000000+0000");
+    runInvalidInsert(tableName, "2017-12-21 00:00:01.000Y");
+    runInvalidInsert(tableName, "2017-12-21 00:00:01.000 IIST");
+    runInvalidInsert(tableName, "2017-12-21 00:00:01.000-700");
+    runInvalidInsert(tableName, "2017-12-21 00:00:01.000 Mars/Olympus");
+    runInvalidInsert(tableName, "2017-12-21 00:00:01.000 AMERICA/NEW_YORK");
   }
 
   @Test
@@ -154,6 +182,25 @@ public class TestTimestampDataType extends BaseCQLTest {
       assertEquals(date_value, row.getTimestamp(3));
       assertEquals(date_value, row.getTimestamp(5));
     }
+  }
+
+  @Test
+  public void testUpdateVariableTimezone() throws Exception {
+    String tableName = "test";
+    String Timestamp = "\'2019-01-26T00:03:16.059 America/New_York\'";
+    String newTimestamp = "\'2019-01-26T00:03:16.059 America/Los_Angeles\'";
+    session.execute(String.format("CREATE TABLE %s(x int primary key, b timestamp);", tableName));
+    String ins_stmt = String.format(
+            "INSERT INTO %s(x, b) VALUES(%d, %s);",
+            tableName, 1, newTimestamp);
+    session.execute(ins_stmt);
+    String upd_stmt = String.format(
+            "UPDATE %s SET b = %s WHERE x = 1;", tableName, newTimestamp);
+    session.execute(upd_stmt);
+    String sel_stmt = String.format("SELECT * FROM %s", tableName);
+    String gmtTime = runSelect(sel_stmt).next().getTimestamp(1).toGMTString();
+    assertTrue(gmtTime.equals("26 Jan 2019 08:03:16 GMT") ||
+               gmtTime.equals("26 Jan 2019 07:03:16 GMT"));
   }
 
   private void runInvalidUpdate(String tableName, String ts) {
@@ -311,6 +358,42 @@ public class TestTimestampDataType extends BaseCQLTest {
     runInvalidSelectWithTimestamp(tableName, "1992-13-12");
     runInvalidSelectWithTimestamp(tableName, "1992-12-12 14:23:30:31");
     runInvalidSelectWithTimestamp(tableName, "1992-12-12 14:23:30.12.32");
+  }
+
+  @Test
+  public void testTimestampLogicGFlag() throws Exception {
+//     Testing ICU timezones flag enabled (default).
+    String tableName = "test";
+    String newTimestamp = "\'2019-01-26T00:03:16.059 America/New_York\'";
+    session.execute(String.format("CREATE TABLE %s(x int primary key, b timestamp);", tableName));
+    String ins_stmt = String.format(
+            "INSERT INTO %s(x, b) VALUES(%d, %s);",
+            tableName, 1, newTimestamp);
+    session.execute(ins_stmt);
+    String sel_stmt = String.format("SELECT * FROM %s", tableName);
+    Iterator<Row> rows = runSelect(sel_stmt);
+    assertTrue(rows.hasNext());
+    destroyMiniCluster();
+//    Testing ICU timezones flag disabled.
+    tserverArgs.add("--use_icu_timezones=false");
+    createMiniCluster();
+    setUpCqlClient();
+    session.execute(String.format("CREATE TABLE %s(x int primary key, b timestamp);", tableName));
+    runInvalidStmt(ins_stmt);
+    String[] oldTimestamps = new String[]{"\'2019-01-26T03:33:16.059 UTC\'",
+                                          "\'2019-01-26T00:03:16.059 UTC-03:30\'",
+                                          "\'2019-01-26T00:03:16.059-03:30\'"};
+    for (int i = 1; i <= 3; i++) {
+      ins_stmt = String.format(
+              "INSERT INTO %s(x, b) VALUES(%d, %s);",
+              tableName, i, oldTimestamps[i-1]);
+      session.execute(ins_stmt);
+    }
+    for(Row row: session.execute(sel_stmt)){
+      assertEquals("26 Jan 2019 03:33:16 GMT",row.getTimestamp(1).toGMTString());
+    }
+    destroyMiniCluster();
+    tserverArgs.remove("--use_icu_timezones=false");
   }
 
 }

commit 6db60f51ba8a73948394923f658ddd12fb4f5828

BUG: fixed memleak by destroying icu Timezone
Summary: fixed memleak introduced in D6112/ea0ff79

Test Plan:
Jenkins ASAN LeakSanitizer in TestQLQuery.TestPartitionHashBcall,
TestQLQuery.TestTokenBcall

Reviewers: mikhail, mihnea, sergei

Reviewed By: sergei

Subscribers: kannan, rao, ybase

Differential Revision: https://phabricator.dev.yugabyte.com/D6196

src/yb/util/date_time.cc
@@ -131,7 +131,7 @@ Result<string> GetTimezone(string timezoneID) {
              timezoneID.substr(0, 3) == "UTC") {
     return timezoneID;
   }
-  TimeZone *tzone = TimeZone::createTimeZone(timezoneID.c_str());
+  std::unique_ptr<TimeZone> tzone(TimeZone::createTimeZone(timezoneID.c_str()));
   UnicodeString id;
   tzone->getID(id);
   string timezone;