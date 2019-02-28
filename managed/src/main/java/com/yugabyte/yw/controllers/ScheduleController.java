3038748d02e2f0c54b16659472c086dec4c59f44
Arnav Agarwal <Arnav15@users.noreply.github.com>
2019-02-28 13:39:44 +0530

    ENG-4719: Added API to manage schedules (list and delete)
    Summary:
    Supports API calls to the schedules table to list current schedules and to delete unwanted
    schedules
    
    Test Plan:
    Checked response to the calls using the API after creation of the universe and adding
    schedules to it.
    
    Reviewers: bharat, ram
    
    Reviewed By: bharat, ram
    
    Subscribers: jenkins-bot, yugaware
    
    Differential Revision: https://phabricator.dev.yugabyte.com/D6249

---
+++ managed/src/main/java/com/yugabyte/yw/controllers/ScheduleController.java
@@ -0,0 +1,53 @@
+// Copyright (c) YugaByte, Inc.
+
+package com.yugabyte.yw.controllers;
+
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import com.yugabyte.yw.common.ApiResponse;
+import com.yugabyte.yw.models.Customer;
+import com.yugabyte.yw.models.Schedule;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import play.libs.Json;
+import play.mvc.Result;
+
+import java.util.List;
+import java.util.UUID;
+
+public class ScheduleController extends AuthenticatedController {
+  public static final Logger LOG = LoggerFactory.getLogger(ScheduleController.class);
+
+  public Result list(UUID customerUUID) {
+    Customer customer = Customer.get(customerUUID);
+    if (customer == null) {
+      String errMsg = "Invalid Customer UUID: " + customerUUID;
+      return ApiResponse.error(BAD_REQUEST, errMsg);
+    }
+
+    List<Schedule> schedules = Schedule.getAllActiveByCustomerUUID(customerUUID);
+    return ApiResponse.success(schedules);
+  }
+
+  public Result delete(UUID customerUUID, UUID scheduleUUID) {
+    Customer customer = Customer.get(customerUUID);
+    if (customer == null) {
+      String errMsg = "Invalid Customer UUID: " + customerUUID;
+      return ApiResponse.error(BAD_REQUEST, errMsg);
+    }
+
+    Schedule schedule = Schedule.get(scheduleUUID);
+    if (schedule == null) {
+      return ApiResponse.error(BAD_REQUEST, "Invalid Schedule UUID: " + scheduleUUID);
+    }
+
+    try {
+      schedule.stopSchedule();
+    } catch (Exception e) {
+      return ApiResponse.error(INTERNAL_SERVER_ERROR, "Unable to delete Schedule UUID: " + scheduleUUID);
+    }
+
+    ObjectNode responseJson = Json.newObject();
+    responseJson.put("success", true);
+    return ApiResponse.success(responseJson);
+  }
+}
