0ad97a590b33b5943da3c4f043858dbbae1e9d3d
Bharat Baddepudi <bbaddepudi@users.noreply.github.com>
2019-03-22 22:14:39 -0700

    This commit provides additional functionality that is logically related to the earlier commit https://github.com/YugaByte/yugabyte-db/commit/56a6029cc614fa5e390949130d285aa1e587ea30
    Original commit message:
    
    #752: Add TLS encryption support to yb-admin.
    
    Summary:
    Added option to pass in location of the certs directory on the client that can be used to talk to a server that has TLS enabled.
    
    Added new unit test. Will enable it once ENG-4900 is resolved.
    
    Test Plan:
    Ran
    ```
    ~code/yugabyte/build/latest/bin/yb-admin --master_addresses=172.151.22.197:7100,172.151.44.90:7100,172.151.61.176:7100  -certs_dir_name ~/code/yugabyte/ent/test_certs/ list_all_masters
    ```
    against a real universe with `use_node_to_node_encryption=true` and `allow_insecure_connections=false` and ensured the master leader responds correctly.
    
    *Without* this fix the command above returns:
    ```
    F20190323 23:36:56 ../../../../../src/yb/tools/yb-admin_client.cc:156] Check failed: _s.ok() Bad status: Timed out (yb/rpc/outbound_call.cc:446): Could not locate the leader master: GetMasterRegistration RPC to 172.151.61.176:7100 timed out after 0.070s
    ```
    
    Also tested on local cluster with `yb-ctl` and ensured other `yb-admin` commands without `certs_dir_name` work fine.
    
    Reviewers: sergei
    
    Reviewed By: sergei
    
    Subscribers: ybase
    
    Differential Revision: https://phabricator.dev.yugabyte.com/D6386

--- ent/src/yb/tools/yb-admin_client.h
+++ ent/src/yb/tools/yb-admin_client.h
@@ -6,6 +6,10 @@
 #include "../../../../src/yb/tools/yb-admin_client.h"
 
 #include "yb/master/master_backup.proxy.h"
+#include "yb/rpc/secure_stream.h"
+#include "yb/server/secure.h"
+#include "yb/util/env_util.h"
+#include "yb/util/path_util.h"
 
 namespace yb {
 namespace tools {
@@ -14,7 +18,9 @@ namespace enterprise {
 class ClusterAdminClient : public yb::tools::ClusterAdminClient {
   typedef yb::tools::ClusterAdminClient super;
  public:
-  ClusterAdminClient(std::string addrs, int64_t timeout_millis) : super(addrs, timeout_millis) {}
+  ClusterAdminClient(std::string addrs, int64_t timeout_millis, std::string certs_dir)
+      : super(std::move(addrs), timeout_millis, certs_dir),
+        certs_dir_(std::move(certs_dir)) {}
 
   // Initialized the client and connects to the server service proxies.
   CHECKED_STATUS Init() override;
@@ -36,6 +42,10 @@ class ClusterAdminClient : public yb::tools::ClusterAdminClient {
 
  private:
   std::unique_ptr<master::MasterBackupServiceProxy> master_backup_proxy_;
+
+  // Secure connection info to connect with tls enabled servers.
+  std::string certs_dir_;
+  std::unique_ptr<rpc::SecureContext> secure_context_;
 
   DISALLOW_COPY_AND_ASSIGN(ClusterAdminClient);
 };
