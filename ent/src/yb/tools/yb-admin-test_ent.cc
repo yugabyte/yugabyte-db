7e4885444322d6d97eba72c8a70c2179dc262a0e
Bharat Baddepudi <bbaddepudi@users.noreply.github.com>
2019-03-22 22:14:39 -0700

    This commit provides additional functionality that is logically related to the earlier commit https://github.com/YugaByte/yugabyte-db/commit/56a6029cc614fa5e390949130d285aa1e587ea30
    Original commit message:
    
    #752: Add TLS encryption support to yb-admin.
    
    Summary:
    Added option to pass in location of the certs directory on the client that can be used to talk to a server that has TLS enabled.
    
    Added new unit test. Will enable it once ENG-4900 is resolved.
    
    Test Plan:
    Ran
    ```
    ~code/yugabyte/build/latest/bin/yb-admin --master_addresses=172.151.22.197:7100,172.151.44.90:7100,172.151.61.176:7100  -certs_dir_name ~/code/yugabyte/ent/test_certs/ list_all_masters
    ```
    against a real universe with `use_node_to_node_encryption=true` and `allow_insecure_connections=false` and ensured the master leader responds correctly.
    
    *Without* this fix the command above returns:
    ```
    F20190323 23:36:56 ../../../../../src/yb/tools/yb-admin_client.cc:156] Check failed: _s.ok() Bad status: Timed out (yb/rpc/outbound_call.cc:446): Could not locate the leader master: GetMasterRegistration RPC to 172.151.61.176:7100 timed out after 0.070s
    ```
    
    Also tested on local cluster with `yb-ctl` and ensured other `yb-admin` commands without `certs_dir_name` work fine.
    
    Reviewers: sergei
    
    Reviewed By: sergei
    
    Subscribers: ybase
    
    Differential Revision: https://phabricator.dev.yugabyte.com/D6386

--- ent/src/yb/tools/yb-admin-test_ent.cc
+++ ent/src/yb/tools/yb-admin-test_ent.cc
@@ -9,6 +9,8 @@
 #include "yb/integration-tests/ts_itest-base.h"
 #include "yb/master/master_defaults.h"
 #include "yb/master/master_backup.proxy.h"
+#include "yb/util/env_util.h"
+#include "yb/util/path_util.h"
 #include "yb/util/subprocess.h"
 
 namespace yb {
@@ -32,17 +34,55 @@ class AdminCliTest : public tserver::TabletServerIntegrationTestBase {
   string GetAdminToolPath() const {
     return GetToolPath(kAdminToolName);
   }
+
+  string StartRF1CluterAndGetMasterAddrs(bool secure_server = false) {
+    FLAGS_num_tablet_servers = secure_server ? 0 : 1;
+    FLAGS_num_replicas = 1;
+
+    vector<string> ts_flags, master_flags;
+    master_flags.push_back("--replication_factor=1");
+    if (secure_server) {
+      const auto sub_dir = JoinPathSegments("ent", "test_certs");
+      auto root_dir = env_util::GetRootDir(sub_dir) + "/../../";
+      auto certs_dir = JoinPathSegments(root_dir, sub_dir);
+
+      master_flags.push_back("--use_node_to_node_encryption=true");
+      master_flags.push_back("--certs_dir=" + certs_dir);
+      master_flags.push_back("--use_client_to_server_encryption=true");
+
+      ts_flags.push_back("--use_node_to_node_encryption=true");
+      ts_flags.push_back("--certs_dir=" + certs_dir);
+      ts_flags.push_back("--use_client_to_server_encryption=true");
+    }
+    BuildAndStart(ts_flags, master_flags);
+    return ToString(cluster_->master()->bound_rpc_addr());
+  }
 };
 
-TEST_F(AdminCliTest, TestCreateSnapshot) {
-  google::FlagSaver saver;
-  FLAGS_num_tablet_servers = 1;
-  FLAGS_num_replicas = 1;
+TEST_F(AdminCliTest, TestNonTLS) {
+  string arg_str = Substitute("$0 -master_addresses $1 list_all_masters",
+                              GetAdminToolPath(),
+                              StartRF1CluterAndGetMasterAddrs());
 
-  vector<string> ts_flags, master_flags;
-  master_flags.push_back("--replication_factor=1");
-  BuildAndStart(ts_flags, master_flags);
-  string master_address = ToString(cluster_->master()->bound_rpc_addr());
+  LOG(INFO) << "Run tool:" << arg_str;
+  ASSERT_OK(Subprocess::Call(arg_str));
+}
+
+// TODO: Enabled once ENG-4900 is resolved.
+TEST_F(AdminCliTest, DISABLED_TestTLS) {
+  const auto sub_dir = JoinPathSegments("ent", "test_certs");
+  auto root_dir = env_util::GetRootDir(sub_dir) + "/../../";
+  string arg_str = Substitute("$0 -master_addresses $1 --certs_dir_name $2 list_all_masters",
+                              GetAdminToolPath(),
+                              StartRF1CluterAndGetMasterAddrs(true /* secure_server */),
+                              JoinPathSegments(root_dir, sub_dir));
+
+  LOG(INFO) << "Run tool:" << arg_str;
+  ASSERT_OK(Subprocess::Call(arg_str));
+}
+
+TEST_F(AdminCliTest, TestCreateSnapshot) {
+  string master_address = StartRF1CluterAndGetMasterAddrs();
 
   shared_ptr<YBClient> client;
   ASSERT_OK(YBClientBuilder()
