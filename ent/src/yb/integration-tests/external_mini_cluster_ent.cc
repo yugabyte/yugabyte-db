933a76b3d1edd9dcbd261e8783224a08f06fa0ab
Sergei Politov <spolitov@users.noreply.github.com>
2019-03-30 07:01:15 +0300

    ENG-4900: Add external mini cluster test with TLS support
    Summary: Adds test that launches external mini cluster with TLS encryption.
    
    Test Plan: ybd --gtest_filter ExternalMiniClusterSecureTest.Simple
    
    Reviewers: bharat
    
    Reviewed By: bharat
    
    Subscribers: ybase
    
    Differential Revision: https://phabricator.dev.yugabyte.com/D6413
    
    Note:
    This commit provides additional functionality that is logically related to
    the earlier commit https://github.com/YugaByte/yugabyte-db/commit/4096f596b5d3428e44bbc139757865ed979cd12c
    and supersedes the commit https://github.com/YugaByte/yugabyte-db/commit/b2002c024c0adfaf33cb38e363c5e7728df44d59

--- ent/src/yb/integration-tests/external_mini_cluster_ent.cc
+++ ent/src/yb/integration-tests/external_mini_cluster_ent.cc
@@ -1,24 +1,57 @@
 // Copyright (c) YugaByte, Inc.
 
-#include "yb/integration-tests/external_mini_cluster.h"
+#include "yb/integration-tests/external_mini_cluster_ent.h"
 
 #include "yb/master/master_backup.proxy.h"
 
+#include "yb/rpc/messenger.h"
+#include "yb/rpc/secure_stream.h"
+
+#include "yb/server/secure.h"
+
+#include "yb/util/test_util.h"
+
+DECLARE_string(certs_dir);
+
 namespace yb {
-namespace enterprise {
 
 using yb::master::MasterBackupServiceProxy;
 
-std::shared_ptr<MasterBackupServiceProxy> ExternalMiniCluster::master_backup_proxy() {
-  CHECK_EQ(masters_.size(), 1);
-  return master_backup_proxy(0);
+std::shared_ptr<MasterBackupServiceProxy> master_backup_proxy(
+    ExternalMiniCluster* cluster) {
+  CHECK_EQ(cluster->num_masters(), 1);
+  return master_backup_proxy(cluster, 0);
 }
 
-std::shared_ptr<MasterBackupServiceProxy> ExternalMiniCluster::master_backup_proxy(int idx) {
-  CHECK_LT(idx, masters_.size());
+std::shared_ptr<MasterBackupServiceProxy> master_backup_proxy(
+    ExternalMiniCluster* cluster, int idx) {
+  CHECK_LT(idx, cluster->num_masters());
   return std::make_shared<MasterBackupServiceProxy>(
-      proxy_cache_.get(), CHECK_NOTNULL(master(idx))->bound_rpc_addr());
+      &cluster->proxy_cache(), CHECK_NOTNULL(cluster->master(idx))->bound_rpc_addr());
 }
 
-} // namespace enterprise
+void StartSecure(
+    std::unique_ptr<ExternalMiniCluster>* cluster,
+    std::unique_ptr<rpc::SecureContext>* secure_context,
+    rpc::MessengerPtr* messenger,
+    const std::vector<std::string>& master_flags) {
+  rpc::MessengerBuilder messenger_builder("test_client");
+  *secure_context = ASSERT_RESULT(server::SetupSecureContext(
+      "", "", server::SecureContextType::kClientToServer, &messenger_builder));
+  *messenger = ASSERT_RESULT(messenger_builder.Build());
+  (**messenger).TEST_SetOutboundIpBase(ASSERT_RESULT(HostToAddress("127.0.0.1")));
+
+  ExternalMiniClusterOptions opts;
+  opts.extra_tserver_flags = {
+      "--use_node_to_node_encryption=true", "--allow_insecure_connections=false",
+      "--certs_dir=" + FLAGS_certs_dir};
+  opts.extra_master_flags = opts.extra_tserver_flags;
+  opts.extra_master_flags.insert(
+      opts.extra_master_flags.end(), master_flags.begin(), master_flags.end());
+  opts.num_tablet_servers = 3;
+  opts.use_even_ips = true;
+  *cluster = std::make_unique<ExternalMiniCluster>(opts);
+  ASSERT_OK((**cluster).Start(*messenger));
+}
+
 } // namespace yb
