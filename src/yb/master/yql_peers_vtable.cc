4b48e25b3a1fb01b0ea0067b1d59c9e0feae8c21
Sergei Politov <spolitov@users.noreply.github.com>
2019-03-30 10:06:17 +0300

    ENG-4872: #1000: Parallel DNS resolution while filling system.peers and system.local tables
    Summary:
    While serving request to system.peers or system.local tables we are doing DNS resolution,
    to convert hostname to IP address.
    This diff adds parallel DNS resolution in this scenario.
    So when DNS resolution takes significant time, those times would not add.
    
    Test Plan: Jenkins
    
    Reviewers: mihnea, robert, mikhail
    
    Reviewed By: mikhail
    
    Subscribers: bogdan, ybase, bharat
    
    Differential Revision: https://phabricator.dev.yugabyte.com/D6409

--- src/yb/master/yql_peers_vtable.cc
+++ src/yb/master/yql_peers_vtable.cc
@@ -17,6 +17,8 @@
 
 #include "yb/master/ts_descriptor.h"
 
+#include "yb/rpc/messenger.h"
+
 namespace yb {
 namespace master {
 
@@ -25,8 +27,23 @@ using std::shared_ptr;
 using std::unique_ptr;
 using std::map;
 
+namespace {
+
+const std::string kPeer = "peer";
+const std::string kDataCenter = "data_center";
+const std::string kHostId = "host_id";
+const std::string kPreferredIp = "preferred_ip";
+const std::string kRack = "rack";
+const std::string kReleaseVersion = "release_version";
+const std::string kRPCAddress = "rpc_address";
+const std::string kSchemaVersion = "schema_version";
+const std::string kTokens = "tokens";
+
+} // namespace
+
 PeersVTable::PeersVTable(const Master* const master)
-    : YQLVirtualTable(master::kSystemPeersTableName, master, CreateSchema()) {
+    : YQLVirtualTable(master::kSystemPeersTableName, master, CreateSchema()),
+      resolver_(new Resolver(master->messenger()->io_service())) {
 }
 
 Status PeersVTable::RetrieveData(const QLReadRequestPB& request,
@@ -49,8 +66,17 @@ Status PeersVTable::RetrieveData(const QLReadRequestPB& request,
   // Populate the YQL rows.
   vtable->reset(new QLRowBlock(schema_));
 
+  struct Entry {
+    size_t index;
+    TSInformationPB ts_info;
+    util::PublicPrivateIPFutures ts_ips;
+  };
+
+  std::vector<Entry> entries;
+  entries.reserve(descs.size());
+
   size_t index = 0;
-  for (const shared_ptr<TSDescriptor>& desc : descs) {
+  for (const auto& desc : descs) {
     size_t current_index = index++;
 
     // This is thread safe since all operations are reads.
@@ -67,31 +93,45 @@ Status PeersVTable::RetrieveData(const QLReadRequestPB& request,
       }
     }
 
+    entries.push_back({current_index, std::move(ts_info)});
+    auto& entry = entries.back();
+    entry.ts_ips = util::GetPublicPrivateIPFutures(entry.ts_info, resolver_.get());
+  }
+
+  for (const auto& entry : entries) {
     // The system.peers table has one entry for each of its peers, whereas there is no entry for
     // the node that the CQL client connects to. In this case, this node is the 'remote_endpoint'
     // in QLReadRequestPB since that is address of the CQL proxy which sent this request. As a
     // result, skip 'remote_endpoint' in the results.
-    auto ips = util::GetPublicPrivateIPs(ts_info);
-    if (!ips.ok()) {
-      LOG(ERROR) << "Failed to get IPs from " << ts_info.ShortDebugString() << ": " << ips.status();
+    auto private_ip = entry.ts_ips.private_ip_future.get();
+    if (!private_ip.ok()) {
+      LOG(ERROR) << "Failed to get private ip from " << entry.ts_info.ShortDebugString()
+                 << ": " << private_ip.status();
+      continue;
+    }
+
+    auto public_ip = entry.ts_ips.public_ip_future.get();
+    if (!public_ip.ok()) {
+      LOG(ERROR) << "Failed to get public ip from " << entry.ts_info.ShortDebugString()
+                 << ": " << public_ip.status();
       continue;
     }
 
     // Need to use only 1 rpc address per node since system.peers has only 1 entry for each host,
     // so pick the first one.
     QLRow &row = (*vtable)->Extend();
-    RETURN_NOT_OK(SetColumnValue(kPeer, ips->public_ip, &row));
-    RETURN_NOT_OK(SetColumnValue(kRPCAddress, ips->public_ip, &row));
-    RETURN_NOT_OK(SetColumnValue(kPreferredIp, ips->private_ip, &row));
+    RETURN_NOT_OK(SetColumnValue(kPeer, *public_ip, &row));
+    RETURN_NOT_OK(SetColumnValue(kRPCAddress, *public_ip, &row));
+    RETURN_NOT_OK(SetColumnValue(kPreferredIp, *private_ip, &row));
 
     // Datacenter and rack.
-    CloudInfoPB cloud_info = ts_info.registration().common().cloud_info();
+    CloudInfoPB cloud_info = entry.ts_info.registration().common().cloud_info();
     RETURN_NOT_OK(SetColumnValue(kDataCenter, cloud_info.placement_region(), &row));
     RETURN_NOT_OK(SetColumnValue(kRack, cloud_info.placement_zone(), &row));
 
     // HostId.
     Uuid host_id;
-    RETURN_NOT_OK(host_id.FromHexString(ts_info.tserver_instance().permanent_uuid()));
+    RETURN_NOT_OK(host_id.FromHexString(entry.ts_info.tserver_instance().permanent_uuid()));
     RETURN_NOT_OK(SetColumnValue(kHostId, host_id, &row));
 
     // schema_version.
@@ -101,7 +141,7 @@ Status PeersVTable::RetrieveData(const QLReadRequestPB& request,
 
     // Tokens.
     RETURN_NOT_OK(SetColumnValue(
-        kTokens, util::GetTokensValue(current_index, descs.size()), &row));
+        kTokens, util::GetTokensValue(entry.index, descs.size()), &row));
   }
 
   return Status::OK();
