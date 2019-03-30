6090ff176e469fa1fbf2f2e9b908b32f78ce9dc3
Sergei Politov <spolitov@users.noreply.github.com>
2019-03-30 07:01:15 +0300

    ENG-4900: Add external mini cluster test with TLS support
    Summary: Adds test that launches external mini cluster with TLS encryption.
    
    Test Plan: ybd --gtest_filter ExternalMiniClusterSecureTest.Simple
    
    Reviewers: bharat
    
    Reviewed By: bharat
    
    Subscribers: ybase
    
    Differential Revision: https://phabricator.dev.yugabyte.com/D6413
    
    Note:
    This commit provides additional functionality that is logically related to
    the earlier commit https://github.com/YugaByte/yugabyte-db/commit/4096f596b5d3428e44bbc139757865ed979cd12c
    and supersedes the commit https://github.com/YugaByte/yugabyte-db/commit/b2002c024c0adfaf33cb38e363c5e7728df44d59

---
+++ ent/src/yb/integration-tests/external_mini_cluster_ent.h
@@ -0,0 +1,38 @@
+// Copyright (c) YugaByte, Inc.
+
+#ifndef ENT_SRC_YB_INTEGRATION_TESTS_EXTERNAL_MINI_CLUSTER_ENT_H
+#define ENT_SRC_YB_INTEGRATION_TESTS_EXTERNAL_MINI_CLUSTER_ENT_H
+
+#include "yb/integration-tests/external_mini_cluster.h"
+
+#include "yb/rpc/rpc_fwd.h"
+
+namespace yb {
+
+namespace master {
+class MasterBackupServiceProxy;
+}  // namespace master
+
+namespace rpc {
+class SecureContext;
+}
+
+// If the cluster is configured for a single non-distributed master, return a backup proxy
+// to that master. Requires that the single master is running.
+std::shared_ptr<master::MasterBackupServiceProxy> master_backup_proxy(
+    ExternalMiniCluster* cluster);
+
+// Returns an RPC backup proxy to the master at 'idx'.
+// Requires that the master at 'idx' is running.
+std::shared_ptr<master::MasterBackupServiceProxy> master_backup_proxy(
+    ExternalMiniCluster* cluster, int idx);
+
+void StartSecure(
+  std::unique_ptr<ExternalMiniCluster>* cluster,
+  std::unique_ptr<rpc::SecureContext>* secure_context,
+  rpc::MessengerPtr* messenger,
+  const std::vector<std::string>& master_flags = std::vector<std::string>());
+
+} // namespace yb
+
+#endif // ENT_SRC_YB_INTEGRATION_TESTS_EXTERNAL_MINI_CLUSTER_ENT_H
