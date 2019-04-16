01473e7a8ffd08fdf874773d9af5445fcdb57f03
rajukumaryb <raju@yugabyte.com>
2019-04-16 00:23:33 +0000

    [YCQL] Insert on update of missing json entry - #1039
    Summary:
    Considering an existing json as a tree, the update action amounts to LHS = RHS where LHS is a path and RHS is a subtree. If the LHS is a path (P1) that is one hop longer than the longest matching existing path (P0) and the leaf element is an object (not an array), treat the update as insert and graft the subtree at the leaf. If P1 is multiple hops longer than P0, continue with existing behavior and report an error.
    
    For example, consider a json column with structure "A: {B: {C: Cval}}".
    
      Before: A->B->D = Dval : FAIL
      After:  A->B->D = Dval : PASS
    
      Before: A->B->E->F = Fval : FAIL
      After:  A->B->E->F = Fval : FAIL (due to E->F being missing, instead of just E missing)
    
    Test Plan: org.yb.cql.TestJson
    
    Reviewers: bogdan, robert, mikhail, neha
    
    Reviewed By: neha
    
    Subscribers: kannan, ybase, yql
    
    Differential Revision: https://phabricator.dev.yugabyte.com/D6462

--- java/yb-cql/src/test/java/org/yb/cql/TestJson.java
+++ java/yb-cql/src/test/java/org/yb/cql/TestJson.java
@@ -339,9 +339,33 @@ public class TestJson extends BaseCQLTest {
     // Invalid updates.
     // Invalid rhs (needs to be valid json)
     runInvalidStmt("UPDATE test_json SET c2->'a'->'q'->'p' = 100 WHERE c1 = 1");
-    // non-existent key.
-    runInvalidStmt("UPDATE test_json SET c2->'a'->'q'->'xyz' = '100' WHERE c1 = 1");
+
+    // Interpret update of missing entry as insert when LHS path has only one extra hop.
+    session.execute("UPDATE test_json SET c2->'a'->'q'->'xyz' = '100' WHERE c1 = 1");
+    verifyResultSet(session.execute("SELECT * FROM test_json WHERE c2->'a'->'q'->'xyz' = " +
+          "'100'"));
+
+    // Interpret update of missing entry as insert when LHS path has only one extra hop -
+    // allow subtree to be inserted.
+    String subtree =
+      "{ " +
+        "\"def\" : " +
+          "{ " +
+            "\"ghi\" : 100," +
+            "\"jkl\" : 200" +
+          "}" +
+      "}";
+    session.execute(String.format(
+          "UPDATE test_json SET c2->'a'->'q'->'abc' = '%s' WHERE c1 = 1", subtree));
+    verifyResultSet(session.execute(
+          "SELECT * FROM test_json WHERE c2->'a'->'q'->'abc'->'def'->'ghi' = '100'"));
+    verifyResultSet(session.execute(
+          "SELECT * FROM test_json WHERE c2->'a'->'q'->'abc'->'def'->'jkl' = '200'"));
+
+    // Non-existent key - cannot interpret update of missing entry as insert when LHS path has
+    // multiple extra hops.
     runInvalidStmt("UPDATE test_json SET c2->'aa'->'q'->'p' = '100' WHERE c1 = 1");
+
     // Array out of bounds.
     runInvalidStmt("UPDATE test_json SET c2->'a1'->200->'k2'->2 = '2000' WHERE c1 = 1");
     runInvalidStmt("UPDATE test_json SET c2->'a1'->-2->'k2'->2 = '2000' WHERE c1 = 1");
