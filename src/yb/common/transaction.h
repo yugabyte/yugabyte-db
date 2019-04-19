a5cba8312a98bb16719dc5cfe68626933074b15b
Neha Deodhar <ndeodhar@users.noreply.github.com>
2019-04-19 10:43:58 -0700

    #1168: CDCService GetChanges RPC
    Summary:
    CDCService.GetChanges RPC:
    This diff implements the GetChanges API for CDC/2DC.
    For more details, refer to CDC design doc: <doc-link>.qlk338iafta4
    
    Once CDC has been setup on a table, subscribers (i.e. CDC consumers) will periodically call cdc_service.GetChanges to get the latest set of changes for a tablet. Subscriber needs to be specify cdc stream ID, subscriber ID, tablet ID and from_checkpoint in the request. `from_checkpoint` is the op_id from which the subscriber is requesting for changes.
    
    If the receiving tablet server is the leader for the tablet whose changes are requested, then cdc service will read the latest set of changes starting from `from_checkpoint`, deserialize them and send the data out as CDCRecordPB.
    If the receiving tablet is no longer the leader or tablet is not running/split into two other tablets, then we'll send a corresponding error code like NOT_LEADER, TABLET_NOT_RUNNING, NOT_FOUND. In this case, subscriber needs to contact master to find the latest leader (or new tablet IDs to follow in case of a split) and then start requesting changes from the new tablet leader.
    
    Test Plan:
    Jenkins
    ```
    ctest -R consensus_queue
    ctest -R log_cache
    ctest -R cdc_service
    ```
    Tested manually on local setup to ensure that we are able to read log records as expected.
    
    Reviewers: bogdan, mikhail, hector, sergei
    
    Reviewed By: hector, sergei
    
    Subscribers: rahuldesirazu, ybase, bharat
    
    Differential Revision: https://phabricator.dev.yugabyte.com/D6504

--- src/yb/common/transaction.h
+++ src/yb/common/transaction.h
@@ -25,6 +25,7 @@
 #include "yb/common/entity_ids.h"
 #include "yb/common/hybrid_time.h"
 
+#include "yb/util/async_util.h"
 #include "yb/util/enums.h"
 #include "yb/util/monotime.h"
 #include "yb/util/logging.h"
