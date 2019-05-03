ac25cb1f5611b78ee75e2a38cbef908c157d3e45
Mikhail Bautin <mbautin@users.noreply.github.com>
2019-05-03 10:55:33 -0700

    Break catalog manager into multiple files
    Summary: Break catalog_manager.cc into multiple files. The 7000+ line catalog_manager.cc is a combination of multiple class hierarchies, utility classes, etc. This is a step towards making it more manageable.
    
    Test Plan: Jenkins
    
    Reviewers: bogdan, hector, sergei
    
    Reviewed By: sergei
    
    Subscribers: bogdan, bharat
    
    Differential Revision: https://phabricator.dev.yugabyte.com/D6563

---
+++ src/yb/master/catalog_manager_bg_tasks.cc
@@ -0,0 +1,157 @@
+// Licensed to the Apache Software Foundation (ASF) under one
+// or more contributor license agreements.  See the NOTICE file
+// distributed with this work for additional information
+// regarding copyright ownership.  The ASF licenses this file
+// to you under the Apache License, Version 2.0 (the
+// "License"); you may not use this file except in compliance
+// with the License.  You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+//
+// The following only applies to changes made to this file as part of YugaByte development.
+//
+// Portions Copyright (c) YugaByte, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+// in compliance with the License.  You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software distributed under the License
+// is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+// or implied.  See the License for the specific language governing permissions and limitations
+// under the License.
+
+#include <memory>
+
+#include "yb/util/logging.h"
+#include "yb/util/mutex.h"
+
+#include "yb/master/catalog_manager_bg_tasks.h"
+#include "yb/master/catalog_manager.h"
+#include "yb/master/scoped_leader_shared_lock.h"
+#include "yb/master/ts_descriptor.h"
+#include "yb/master/cluster_balance.h"
+#include "yb/util/flag_tags.h"
+
+using std::shared_ptr;
+
+DEFINE_int32(catalog_manager_bg_task_wait_ms, 1000,
+             "Amount of time the catalog manager background task thread waits "
+             "between runs");
+TAG_FLAG(catalog_manager_bg_task_wait_ms, hidden);
+
+namespace yb {
+namespace master {
+
+void CatalogManagerBgTasks::Wake() {
+  MutexLock lock(lock_);
+  pending_updates_ = true;
+  cond_.Broadcast();
+}
+
+void CatalogManagerBgTasks::Wait(int msec) {
+  MutexLock lock(lock_);
+  if (closing_.load()) return;
+  if (!pending_updates_) {
+    cond_.TimedWait(MonoDelta::FromMilliseconds(msec));
+  }
+  pending_updates_ = false;
+}
+
+void CatalogManagerBgTasks::WakeIfHasPendingUpdates() {
+  MutexLock lock(lock_);
+  if (pending_updates_) {
+    cond_.Broadcast();
+  }
+}
+
+Status CatalogManagerBgTasks::Init() {
+  RETURN_NOT_OK(yb::Thread::Create("catalog manager", "bgtasks",
+      &CatalogManagerBgTasks::Run, this, &thread_));
+  return Status::OK();
+}
+
+void CatalogManagerBgTasks::Shutdown() {
+  {
+    bool closing_expected = false;
+    if (!closing_.compare_exchange_strong(closing_expected, true)) {
+      VLOG(2) << "CatalogManagerBgTasks already shut down";
+      return;
+    }
+  }
+
+  Wake();
+  if (thread_ != nullptr) {
+    CHECK_OK(ThreadJoiner(thread_.get()).Join());
+  }
+}
+
+void CatalogManagerBgTasks::Run() {
+  while (!closing_.load()) {
+    // Perform assignment processing.
+    ScopedLeaderSharedLock l(catalog_manager_);
+    if (!l.catalog_status().ok()) {
+      LOG(WARNING) << "Catalog manager background task thread going to sleep: "
+                   << l.catalog_status().ToString();
+    } else if (l.leader_status().ok()) {
+      // Clear metrics for dead tservers.
+      vector<shared_ptr<TSDescriptor>> descs;
+      const auto& ts_manager = catalog_manager_->master_->ts_manager();
+      ts_manager->GetAllDescriptors(&descs);
+      for (auto& ts_desc : descs) {
+        if (!ts_manager->IsTSLive(ts_desc)) {
+          ts_desc->ClearMetrics();
+        }
+      }
+
+      // Report metrics.
+      catalog_manager_->ReportMetrics();
+
+      TabletInfos to_delete;
+      TabletInfos to_process;
+
+      // Get list of tablets not yet running or already replaced.
+      catalog_manager_->ExtractTabletsToProcess(&to_delete, &to_process);
+
+      if (!to_process.empty()) {
+        // Transition tablet assignment state from preparing to creating, send
+        // and schedule creation / deletion RPC messages, etc.
+        Status s = catalog_manager_->ProcessPendingAssignments(to_process);
+        if (!s.ok()) {
+          // If there is an error (e.g., we are not the leader) abort this task
+          // and wait until we're woken up again.
+          //
+          // TODO Add tests for this in the revision that makes
+          // create/alter fault tolerant.
+          LOG(ERROR) << "Error processing pending assignments, aborting the current task: "
+                     << s.ToString();
+        }
+      } else {
+        catalog_manager_->load_balance_policy_->RunLoadBalancer();
+      }
+    }
+
+    // if (!to_delete.empty()) {
+    //   // TODO: Run the cleaner
+    // }
+
+    // Wait for a notification or a timeout expiration.
+    //  - CreateTable will call Wake() to notify about the tablets to add
+    //  - HandleReportedTablet/ProcessPendingAssignments will call WakeIfHasPendingUpdates()
+    //    to notify about tablets creation.
+    l.Unlock();
+    Wait(FLAGS_catalog_manager_bg_task_wait_ms);
+  }
+  VLOG(1) << "Catalog manager background task thread shutting down";
+}
+
+}  // namespace master
+}  // namespace yb
---
+++ src/yb/master/scoped_leader_shared_lock.h
@@ -0,0 +1,137 @@
+// Licensed to the Apache Software Foundation (ASF) under one
+// or more contributor license agreements.  See the NOTICE file
+// distributed with this work for additional information
+// regarding copyright ownership.  The ASF licenses this file
+// to you under the Apache License, Version 2.0 (the
+// "License"); you may not use this file except in compliance
+// with the License.  You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+//
+// The following only applies to changes made to this file as part of YugaByte development.
+//
+// Portions Copyright (c) YugaByte, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+// in compliance with the License.  You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software distributed under the License
+// is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+// or implied.  See the License for the specific language governing permissions and limitations
+// under the License.
+
+#ifndef YB_MASTER_SCOPED_LEADER_SHARED_LOCK_H
+#define YB_MASTER_SCOPED_LEADER_SHARED_LOCK_H
+
+#include <chrono>
+
+#include <glog/logging.h>
+
+#include "yb/util/status.h"
+#include "yb/rpc/rpc_context.h"
+#include "yb/util/rw_mutex.h"
+
+namespace yb {
+namespace master {
+
+class CatalogManager;
+
+// Scoped "shared lock" to serialize master leader elections.
+//
+// While in scope, blocks the catalog manager in the event that it becomes
+// the leader of its Raft configuration and needs to reload its persistent
+// metadata. Once destroyed, the catalog manager is unblocked.
+//
+// Usage:
+//
+// void MasterServiceImpl::CreateTable(const CreateTableRequestPB* req,
+//                                     CreateTableResponsePB* resp,
+//                                     rpc::RpcContext* rpc) {
+//   ScopedLeaderSharedLock l(server_->catalog_manager());
+//   if (!l.CheckIsInitializedAndIsLeaderOrRespond(resp, rpc)) {
+//     return;
+//   }
+//
+//   Status s = server_->catalog_manager()->CreateTable(req, resp, rpc);
+//   CheckRespErrorOrSetUnknown(s, resp);
+//   rpc.RespondSuccess();
+// }
+//
+class ScopedLeaderSharedLock {
+ public:
+  // Creates a new shared lock, acquiring the catalog manager's leader_lock_
+  // for reading in the process. The lock is released when this object is
+  // destroyed.
+  //
+  // 'catalog' must outlive this object.
+  explicit ScopedLeaderSharedLock(CatalogManager* catalog);
+
+  ~ScopedLeaderSharedLock() { Unlock(); }
+
+  void Unlock();
+
+  // General status of the catalog manager. If not OK (e.g. the catalog
+  // manager is still being initialized), all operations are illegal and
+  // leader_status() should not be trusted.
+  const Status& catalog_status() const { return catalog_status_; }
+
+  // Leadership status of the catalog manager. If not OK, the catalog
+  // manager is not the leader, but some operations may still be legal.
+  const Status& leader_status() const {
+    DCHECK(catalog_status_.ok());
+    return leader_status_;
+  }
+
+  // First non-OK status of the catalog manager, adhering to the checking
+  // order specified above.
+  const Status& first_failed_status() const {
+    if (!catalog_status_.ok()) {
+      return catalog_status_;
+    }
+    return leader_status_;
+  }
+
+  // Check that the catalog manager is initialized. It may or may not be the
+  // leader of its Raft configuration.
+  //
+  // If not initialized, writes the corresponding error to 'resp',
+  // responds to 'rpc', and returns false.
+  template<typename RespClass>
+  bool CheckIsInitializedOrRespond(RespClass* resp, rpc::RpcContext* rpc);
+
+  // Check that the catalog manager is initialized and that it is the leader
+  // of its Raft configuration. Initialization status takes precedence over
+  // leadership status.
+  //
+  // If not initialized or if not the leader, writes the corresponding error
+  // to 'resp', responds to 'rpc', and returns false.
+  template<typename RespClass>
+  bool CheckIsInitializedAndIsLeaderOrRespond(RespClass* resp, rpc::RpcContext* rpc);
+
+  // TServer API variant of above class to set appropriate error codes.
+  template<typename RespClass>
+  bool CheckIsInitializedAndIsLeaderOrRespondTServer(RespClass* resp, rpc::RpcContext* rpc);
+
+ private:
+  template<typename RespClass, typename ErrorClass>
+  bool CheckIsInitializedAndIsLeaderOrRespondInternal(RespClass* resp, rpc::RpcContext* rpc);
+  CatalogManager* catalog_;
+  shared_lock<RWMutex> leader_shared_lock_;
+  Status catalog_status_;
+  Status leader_status_;
+  std::chrono::steady_clock::time_point start_;
+};
+
+}  // namespace master
+}  // namespace yb
+
+#endif  // YB_MASTER_SCOPED_LEADER_SHARED_LOCK_H
