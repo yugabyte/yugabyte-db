yugabyte/yugabyte-db

commit c1b75553f144c276f28bcead6d75a27c0e8ed5f3

ENG-5111: Expose tablet-servers page as a JSON
Summary:
Expose /tserver-status as json for YW to render necessary metrics

Sample ouptut JSON, the first key is Cluster UUID, which is empty because I got this from local cluster.

```{
   "":{
      "127.0.0.1:9000":{
         "time_since_hb":"0.3s",
         "status":"ALIVE",
         "uptime_seconds":15,
         "ram_used":"18.2647 MB",
         "total_sst_file_size":"0 B",
         "uncompressed_sst_file_size":"0 B",
         "read_ops_per_sec":0.0,
         "write_ops_per_sec":0.0,
         "user_tablets_total":0,
         "user_tablets_leaders":0,
         "system_tablets_total":0,
         "system_tablets_leaders":0,
         "active_tablets":0
      }
   }
}```

Test Plan:
`yb-ctl create`
Go to `http://127.0.0.1:7000/api/tablet-servers`

Reviewers: ram, karthik, vit.pankin, bogdan

Reviewed By: bogdan

Subscribers: ybase, bharat

Differential Revision: https://phabricator.dev.yugabyte.com/D6532

src/yb/master/master-path-handlers.cc
@@ -266,35 +266,7 @@ void MasterPathHandlers::HandleTabletServers(const Webserver::WebRequest& req,
 
   // Get user and system tablet leader and follower counts for each TabletServer
   TabletCountMap tablet_map;
-
-  vector<scoped_refptr<TableInfo>> tables;
-  master_->catalog_manager()->GetAllTables(&tables, true /* include only running tables */);
-  for (const auto& table : tables) {
-    TabletInfos tablets;
-    table->GetAllTablets(&tablets);
-    bool is_user_table = master_->catalog_manager()->IsUserCreatedTable(*table);
-
-    for (const auto& tablet : tablets) {
-      TabletInfo::ReplicaMap replication_locations;
-      tablet->GetReplicaLocations(&replication_locations);
-
-      for (const auto& replica : replication_locations) {
-        if (is_user_table) {
-          if (replica.second.role == consensus::RaftPeerPB_Role_LEADER) {
-            tablet_map[replica.first].user_tablet_leaders++;
-          } else {
-            tablet_map[replica.first].user_tablet_followers++;
-          }
-        } else {
-          if (replica.second.role == consensus::RaftPeerPB_Role_LEADER) {
-            tablet_map[replica.first].system_tablet_leaders++;
-          } else {
-            tablet_map[replica.first].system_tablet_followers++;
-          }
-        }
-      }
-    }
-  }
+  CalculateTabletMap(tablet_map);
 
   unordered_set<string> read_replica_uuids;
   for (auto desc : descs) {
@@ -320,6 +292,121 @@ void MasterPathHandlers::HandleTabletServers(const Webserver::WebRequest& req,
     TServerTable(output);
     TServerDisplay(read_replica_uuid, &descs, &tablet_map, output);
   }
+}
+
+void MasterPathHandlers::HandleGetTserverStatus(const Webserver::WebRequest& req,
+                                             stringstream* output) {
+  master_->catalog_manager()->AssertLeaderLockAcquiredForReading();
+
+  JsonWriter jw(output, JsonWriter::COMPACT);
+
+  SysClusterConfigEntryPB config;
+  Status s = master_->catalog_manager()->GetClusterConfig(&config);
+  if (!s.ok()) {
+    jw.StartObject();
+    jw.String("error");
+    jw.String(s.ToString());
+    return;
+  }
+
+  vector<std::shared_ptr<TSDescriptor> > descs;
+  const auto& ts_manager = master_->ts_manager();
+  ts_manager->GetAllDescriptors(&descs);
+
+  // Get user and system tablet leader and follower counts for each TabletServer.
+  TabletCountMap tablet_map;
+  CalculateTabletMap(tablet_map);
+
+  unordered_set<string> cluster_uuids;
+  auto primary_uuid = config.replication_info().live_replicas().placement_uuid();
+  cluster_uuids.insert(primary_uuid);
+  for (auto desc : descs) {
+    cluster_uuids.insert(desc->placement_uuid());
+  }
+
+  jw.StartObject();
+  for (const auto& cur_uuid : cluster_uuids) {
+    jw.String(cur_uuid);
+    jw.StartObject();
+    for (auto desc : descs) {
+      if (desc->placement_uuid() == cur_uuid) {
+        TSRegistrationPB reg = desc->GetRegistration();
+        string host_port = Substitute("$0:$1",
+                                      reg.common().http_addresses(0).host(),
+                                      reg.common().http_addresses(0).port());
+        jw.String(host_port);
+
+        jw.StartObject();
+
+        const string time_since_hb = StringPrintf("%.1fs", desc->TimeSinceHeartbeat().ToSeconds());
+        jw.String("time_since_hb");
+        jw.String(time_since_hb);
+
+        if (master_->ts_manager()->IsTSLive(desc)) {
+          jw.String("status");
+          jw.String(kTserverAlive);
+
+          jw.String("uptime_seconds");
+          jw.Uint(desc->uptime_seconds());
+        } else {
+          jw.String("status");
+          jw.String(kTserverDead);
+
+          jw.String("uptime_seconds");
+          jw.Uint(0);
+        }
+
+        jw.String("ram_used");
+        jw.String(BytesToHumanReadable(desc->total_memory_usage()));
+
+        jw.String("total_sst_file_size");
+        jw.String(BytesToHumanReadable(desc->total_sst_file_size()));
+
+        jw.String("uncompressed_sst_file_size");
+        jw.String(BytesToHumanReadable(desc->uncompressed_sst_file_size()));
+
+        jw.String("read_ops_per_sec");
+        jw.Double(desc->read_ops_per_sec());
+
+        jw.String("write_ops_per_sec");
+        jw.Double(desc->write_ops_per_sec());
+
+        auto tserver = tablet_map.find(desc->permanent_uuid());
+        uint user_tablets_total = 0;
+        uint user_tablets_leaders = 0;
+        uint system_tablets_total = 0;
+        uint system_tablets_leaders = 0;
+        int active_tablets = 0;
+        if (!(tserver == tablet_map.end())) {
+          user_tablets_total = tserver->second.user_tablet_leaders +
+            tserver->second.user_tablet_followers;
+          user_tablets_leaders = tserver->second.user_tablet_leaders;
+          system_tablets_total = tserver->second.system_tablet_leaders +
+            tserver->second.system_tablet_followers;
+          system_tablets_leaders = tserver->second.system_tablet_leaders;
+          active_tablets = desc->num_live_replicas();
+        }
+        jw.String("user_tablets_total");
+        jw.Uint(user_tablets_total);
+
+        jw.String("user_tablets_leaders");
+        jw.Uint(user_tablets_leaders);
+
+        jw.String("system_tablets_total");
+        jw.Uint(system_tablets_total);
+
+        jw.String("system_tablets_leaders");
+        jw.Uint(system_tablets_leaders);
+
+        jw.String("active_tablets");
+        jw.Int(active_tablets);
+
+        jw.EndObject();
+      }
+    }
+    jw.EndObject();
+  }
+  jw.EndObject();
 }
 
 void MasterPathHandlers::HandleCatalogManager(const Webserver::WebRequest& req,
@@ -947,6 +1034,10 @@ Status MasterPathHandlers::Register(Webserver* server) {
       "/tablet-servers", "Tablet Servers",
       std::bind(&MasterPathHandlers::CallIfLeaderOrPrintRedirect, this, _1, _2, cb), is_styled,
       is_on_nav_bar, "fa fa-server");
+  cb = std::bind(&MasterPathHandlers::HandleGetTserverStatus, this, _1, _2);
+  server->RegisterPathHandler(
+    "/api/tablet-servers", "Tserver Statuses",
+    std::bind(&MasterPathHandlers::CallIfLeaderOrPrintRedirect, this, _1, _2, cb), false, false);
   cb = std::bind(&MasterPathHandlers::HandleCatalogManager,
                  this, _1, _2, false /* skip_system_tables */);
   server->RegisterPathHandler(
@@ -1015,6 +1106,37 @@ string MasterPathHandlers::RegistrationToHtml(
                            reg.http_addresses(0).port(), link_html);
   }
   return link_html;
+}
+
+void MasterPathHandlers::CalculateTabletMap(TabletCountMap tablet_map) {
+  vector<scoped_refptr<TableInfo>> tables;
+  master_->catalog_manager()->GetAllTables(&tables, true /* include only running tables */);
+  for (const auto& table : tables) {
+    TabletInfos tablets;
+    table->GetAllTablets(&tablets);
+    bool is_user_table = master_->catalog_manager()->IsUserCreatedTable(*table);
+
+    for (const auto& tablet : tablets) {
+      TabletInfo::ReplicaMap replication_locations;
+      tablet->GetReplicaLocations(&replication_locations);
+
+      for (const auto& replica : replication_locations) {
+        if (is_user_table) {
+          if (replica.second.role == consensus::RaftPeerPB_Role_LEADER) {
+            tablet_map[replica.first].user_tablet_leaders++;
+          } else {
+            tablet_map[replica.first].user_tablet_followers++;
+          }
+        } else {
+          if (replica.second.role == consensus::RaftPeerPB_Role_LEADER) {
+            tablet_map[replica.first].system_tablet_leaders++;
+          } else {
+            tablet_map[replica.first].system_tablet_followers++;
+          }
+        }
+      }
+    }
+  }
 }
 
 } // namespace master

src/yb/master/master-path-handlers.h
@@ -119,7 +119,12 @@ class MasterPathHandlers {
                      std::stringstream* output);
   void HandleDumpEntities(const Webserver::WebRequest& req,
                           std::stringstream* output);
+  void HandleGetTserverStatus(const Webserver::WebRequest& req,
+                          std::stringstream* output);
   void HandleGetClusterConfig(const Webserver::WebRequest& req, std::stringstream* output);
+
+  // Calcuates number of leaders/followers per table.
+  void CalculateTabletMap(TabletCountMap tablet_map);
 
   // Convert location of peers to HTML, indicating the roles
   // of each tablet server in a consensus configuration.

commit 67a226538a830ca14dddb4219cf0d134e0e61018

#1352 Fix incorrect tablet counts in YB-Master UI
Summary:
Fix incorrect tablet counts in YB-Master UI

Broken by https://github.com/YugaByte/yugabyte-db/commit/c1b75553f144c276f28bcead6d75a27c0e8ed5f3

Test Plan:
Tested manually by creating a new cluster and a single table.
Before fix, all tablet counts are 0 in the master ui. After this fix the correct number of tablets
are displayed.

Reviewers: neha, wesley

Reviewed By: wesley

Subscribers: kannan, ybase, bogdan

Differential Revision: https://phabricator.dev.yugabyte.com/D6607

src/yb/master/master-path-handlers.cc
@@ -266,7 +266,7 @@ void MasterPathHandlers::HandleTabletServers(const Webserver::WebRequest& req,
 
   // Get user and system tablet leader and follower counts for each TabletServer
   TabletCountMap tablet_map;
-  CalculateTabletMap(tablet_map);
+  CalculateTabletMap(&tablet_map);
 
   unordered_set<string> read_replica_uuids;
   for (auto desc : descs) {
@@ -315,7 +315,7 @@ void MasterPathHandlers::HandleGetTserverStatus(const Webserver::WebRequest& req
 
   // Get user and system tablet leader and follower counts for each TabletServer.
   TabletCountMap tablet_map;
-  CalculateTabletMap(tablet_map);
+  CalculateTabletMap(&tablet_map);
 
   unordered_set<string> cluster_uuids;
   auto primary_uuid = config.replication_info().live_replicas().placement_uuid();
@@ -1108,7 +1108,7 @@ string MasterPathHandlers::RegistrationToHtml(
   return link_html;
 }
 
-void MasterPathHandlers::CalculateTabletMap(TabletCountMap tablet_map) {
+void MasterPathHandlers::CalculateTabletMap(TabletCountMap* tablet_map) {
   vector<scoped_refptr<TableInfo>> tables;
   master_->catalog_manager()->GetAllTables(&tables, true /* include only running tables */);
   for (const auto& table : tables) {
@@ -1123,15 +1123,15 @@ void MasterPathHandlers::CalculateTabletMap(TabletCountMap tablet_map) {
       for (const auto& replica : replication_locations) {
         if (is_user_table) {
           if (replica.second.role == consensus::RaftPeerPB_Role_LEADER) {
-            tablet_map[replica.first].user_tablet_leaders++;
+            (*tablet_map)[replica.first].user_tablet_leaders++;
           } else {
-            tablet_map[replica.first].user_tablet_followers++;
+            (*tablet_map)[replica.first].user_tablet_followers++;
           }
         } else {
           if (replica.second.role == consensus::RaftPeerPB_Role_LEADER) {
-            tablet_map[replica.first].system_tablet_leaders++;
+            (*tablet_map)[replica.first].system_tablet_leaders++;
           } else {
-            tablet_map[replica.first].system_tablet_followers++;
+            (*tablet_map)[replica.first].system_tablet_followers++;
           }
         }
       }

src/yb/master/master-path-handlers.h
@@ -124,7 +124,7 @@ class MasterPathHandlers {
   void HandleGetClusterConfig(const Webserver::WebRequest& req, std::stringstream* output);
 
   // Calcuates number of leaders/followers per table.
-  void CalculateTabletMap(TabletCountMap tablet_map);
+  void CalculateTabletMap(TabletCountMap* tablet_map);
 
   // Convert location of peers to HTML, indicating the roles
   // of each tablet server in a consensus configuration.