d1d769356fe5df9d243ba5ceef75ee66cd882f37
Arnav Agarwal <Arnav15@users.noreply.github.com>
2019-05-20 14:44:17 -0700

    #1401: Customer certificate upload API support.
    Summary:
    1) Added an API to support upload of custom certificates. [https://github.com/YugaByte/yugabyte-db/issues/1401]
    2) Minor fix for custom storage in a single-AZ k8s deployement according to the new provider config.
    
    Test Plan:
    1) Postman call to see if the certificate upload goes as expected.
    ```
    curl -X POST \
      http://localhost:9000/api/v1/customers/f33e3c9b-75ab-4c30-80ad-cba85646ea39/certificates \
      -H 'Accept: */*' \
      -H 'Cache-Control: no-cache' \
      -H 'Connection: keep-alive' \
      -H 'Content-Type: application/json' \
      -H 'Host: localhost:9000' \
      -H 'Postman-Token: 426fcc85-f945-4cb4-88f4-6b4cd02b375b,90bb6847-8d81-493e-9ef4-f5cf4ea648ec' \
      -H 'User-Agent: PostmanRuntime/7.13.0' \
      -H 'X-AUTH-TOKEN: 4b71e0a5-9aff-452e-ba14-63ce04b11533' \
      -H 'accept-encoding: gzip, deflate' \
      -H 'cache-control: no-cache' \
      -H 'content-length: 152' \
      -d '{
    	"label": "test",
    	"certStart": 1558389125208,
    	"certExpiry": 1558399125208,
    	"certContent": "fsdjfhsgkjsjgkjdskfgjkasjg",
    	"keyContent": "ajhdfajf"
    }'
    ```
    2) Created a single-cluster and multi-cluster k8s universe and verified both of them got the correct storage class on creation.
    
    Reviewers: bogdan, andrew, ram
    
    Reviewed By: ram
    
    Subscribers: jenkins-bot, yugaware
    
    Differential Revision: https://phabricator.dev.yugabyte.com/D6647

--- managed/src/main/java/com/yugabyte/yw/controllers/CertificateController.java
+++ managed/src/main/java/com/yugabyte/yw/controllers/CertificateController.java
@@ -1,11 +1,20 @@
 package com.yugabyte.yw.controllers;
 
+import com.google.inject.Inject;
+
+import com.yugabyte.yw.common.ApiResponse;
+import com.yugabyte.yw.common.CertificateHelper;
 import com.yugabyte.yw.models.CertificateInfo;
+import com.yugabyte.yw.models.Customer;
+import com.yugabyte.yw.forms.CertificateParams;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import com.yugabyte.yw.common.ApiResponse;
 import play.mvc.Result;
+import play.data.Form;
+import play.data.FormFactory;
 
+import java.util.Date;
 import java.util.List;
 import java.util.UUID;
 import java.util.stream.Collectors;
@@ -13,13 +22,45 @@ import java.util.stream.Collectors;
 public class CertificateController extends AuthenticatedController {
   public static final Logger LOG = LoggerFactory.getLogger(CertificateController.class);
 
+  @Inject
+  play.Configuration appConfig;
+
+  @Inject
+  FormFactory formFactory;
+
+  public Result upload(UUID customerUUID) {
+    Form<CertificateParams> formData = formFactory.form(CertificateParams.class)
+                                                  .bindFromRequest();
+    if (Customer.get(customerUUID) == null) {
+      return ApiResponse.error(BAD_REQUEST, "Invalid Customer UUID: " + customerUUID);
+    }
+    if (formData.hasErrors()) {
+      return ApiResponse.error(BAD_REQUEST, formData.errorsAsJson());
+    }
+    Date certStart = new Date(formData.get().certStart);
+    Date certExpiry = new Date(formData.get().certExpiry);
+    String label = formData.get().label;
+    String certContent = formData.get().certContent;
+    String keyContent = formData.get().keyContent;
+    try {
+      UUID certUUID = CertificateHelper.uploadRootCA(label, customerUUID, appConfig.getString("yb.storage.path"),
+          certContent, keyContent, certStart, certExpiry);
+      return ApiResponse.success(certUUID);
+    } catch (Exception e) {
+      return ApiResponse.error(BAD_REQUEST, "Couldn't upload certfiles");
+    }
+
+  }
+
   public Result list(UUID customerUUID) {
     List<CertificateInfo> certs = CertificateInfo.getAll(customerUUID);
     if (certs == null) {
       return ApiResponse.error(BAD_REQUEST, "Invalid Customer UUID: " + customerUUID);
     }
     try {
-      List<String> cert_labels = CertificateInfo.getAll(customerUUID).stream().map(cert->cert.label).collect(Collectors.toList());
+      List<String> cert_labels = CertificateInfo.getAll(customerUUID)
+                                                .stream().map(cert->cert.label)
+                                                .collect(Collectors.toList());
       return ApiResponse.success(cert_labels);
     } catch (RuntimeException re) {
       return ApiResponse.error(INTERNAL_SERVER_ERROR, re.getMessage());
