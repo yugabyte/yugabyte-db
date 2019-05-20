48609322b70fe564aa6b8843d567658e2044cf4c
Vit Pankin <vitpankin@users.noreply.github.com>
2019-05-20 21:13:11 +0300

    ENG-5137 [YW] Map should be expandable to a bigger screen
    Summary:
    Add big map component
    Updated styling
    
    Test Plan: open any universe overview -> click Expand icon on the map widget -> test it out
    
    Reviewers: ram, wesley, andrew
    
    Reviewed By: andrew
    
    Subscribers: jenkins-bot, ui
    
    Differential Revision: https://phabricator.dev.yugabyte.com/D6613

--- managed/ui/src/components/universes/UniverseOverview/UniverseOverviewContainerNew.js
+++ managed/ui/src/components/universes/UniverseOverview/UniverseOverviewContainerNew.js
@@ -2,14 +2,28 @@
 
 import { UniverseOverviewNew } from '../../universes';
 import { connect } from 'react-redux';
+import { openDialog, closeDialog } from '../../../actions/modal';
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    closeModal: () => {
+      dispatch(closeDialog());
+    },
+
+    showUniverseOverviewMapModal: () => {
+      dispatch(openDialog("universeOverviewMapModal"));
+    },
+  };
+};
 
 function mapStateToProps(state) {
   return {
     currentCustomer: state.customer.currentCustomer,
     layout: state.customer.layout,
     tasks: state.tasks,
+    modal: state.modal,
     tables: state.tables
   };
 }
 
-export default connect(mapStateToProps)(UniverseOverviewNew);
+export default connect(mapStateToProps, mapDispatchToProps)(UniverseOverviewNew);
