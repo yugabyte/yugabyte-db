83d7abcce107346ff91073a84d4e37a4ddce9311
Pavel Stehule <pavel.stehule@gmail.com>
2019-05-22 19:23:23 +0200

    prepare for 3.8

--- orafce--3.8.sql
+++ orafce--3.8.sql
@@ -1,4 +1,4 @@
-/* contrib/orafce--3.4.sql */
+/* orafce--3.8.sql */
 
 -- complain if script is sourced in psql, rather than via CREATE EXTENSION
 \echo Use "CREATE EXTENSION orafce" to load this file. \quit
