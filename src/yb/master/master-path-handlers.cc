yugabyte/yugabyte-db

commit 29a4a8adef8b01afbf0ec2e77331340ea675d129

#1328 Add table of tablet-peer counts by AZ to YB-Master UI
Summary:
Added a second table to the "Tablet Servers" page on the YB-Master UI, which shows the
number of tablet peers in each availability zone. I also changed the wording in both tables from
"Tablets" to "Tablet-Peers", to better reflect what the values actually represent.

Test Plan:
Manually tested by creating a local cluster with replication factor 3 and a single table,
then added 3 new nodes (with differing cloud, region, and zone values). The YB-Master UI
observed on the tablet servers page is shown below, where the first screenshot shows the page before
any new nodes were added, and the second shows the page after all 3 additional nodes were added.

{F12188}

{F12189}

Reviewers: mihnea, mikhail, rahuldesirazu

Reviewed By: mikhail, rahuldesirazu

Subscribers: rahuldesirazu, ybase, kannan

Differential Revision: https://phabricator.dev.yugabyte.com/D6609

src/yb/master/master-path-handlers.cc
@@ -93,6 +93,25 @@ string MasterPathHandlers::BytesToHumanReadable(uint64_t bytes) {
   return op_stream.str();
 }
 
+void MasterPathHandlers::TabletCounts::operator+=(const TabletCounts& other) {
+  user_tablet_leaders += other.user_tablet_leaders;
+  user_tablet_followers += other.user_tablet_followers;
+  system_tablet_leaders += other.system_tablet_leaders;
+  system_tablet_followers += other.system_tablet_followers;
+}
+
+MasterPathHandlers::ZoneTabletCounts::ZoneTabletCounts(): tablet_counts(), node_count(1) {
+}
+
+MasterPathHandlers::ZoneTabletCounts::ZoneTabletCounts(const TabletCounts& tablet_counts)
+  : tablet_counts(tablet_counts), node_count(1) {
+}
+
+void MasterPathHandlers::ZoneTabletCounts::operator+=(const ZoneTabletCounts& other) {
+  tablet_counts += other.tablet_counts;
+  node_count += other.node_count;
+}
+
 void MasterPathHandlers::CallIfLeaderOrPrintRedirect(
     const Webserver::WebRequest& req, stringstream* output,
     const Webserver::PathHandlerCallback& callback) {
@@ -161,7 +180,7 @@ inline void MasterPathHandlers::TServerTable(std::stringstream* output) {
           << "      <th>Server</th>\n"
           << "      <th>Time since </br>heartbeat</th>\n"
           << "      <th>Status & Uptime</th>\n"
-          << "      <th>User Tablets</br>Total / Leaders</th>\n"
+          << "      <th>User Tablet-Peers / Leaders</th>\n"
           << "      <th>RAM Used</th>\n"
           << "      <th>SST Files Size</th>\n"
           << "      <th>Uncompressed SST </br>Files Size</th>\n"
@@ -170,8 +189,8 @@ inline void MasterPathHandlers::TServerTable(std::stringstream* output) {
           << "      <th>Cloud</th>\n"
           << "      <th>Region</th>\n"
           << "      <th>Zone</th>\n"
-          << "      <th>System Tablets</br>Total / Leaders</th>\n"
-          << "      <th>Total Active Tablets</th>\n"
+          << "      <th>System Tablet-Peers / Leaders</th>\n"
+          << "      <th>Total Tablet-Peers</th>\n"
           << "    </tr>\n";
 }
 
@@ -247,6 +266,123 @@ void MasterPathHandlers::TServerDisplay(const std::string& current_uuid,
   *output << "</table>\n";
 }
 
+void MasterPathHandlers::DisplayTabletZonesTable(
+  const ZoneTabletCounts::CloudTree& cloud_tree,
+  std::stringstream* output
+) {
+  *output << "<h3>Tablet-Peers by Availability Zone</h3>\n"
+          << "<table class='table table-striped'>\n"
+          << "  <tr>\n"
+          << "    <th>Cloud</th>\n"
+          << "    <th>Region</th>\n"
+          << "    <th>Zone</th>\n"
+          << "    <th>Total Nodes</th>\n"
+          << "    <th>User Tablet-Peers / Leaders</th>\n"
+          << "    <th>System Tablet-Peers / Leaders</th>\n"
+          << "    <th>Total Tablet-Peers</th>\n"
+          << "  </tr>\n";
+
+  for (const auto& cloud_iter : cloud_tree) {
+    const auto& region_tree = cloud_iter.second;
+    bool needs_new_row = false;
+
+    int total_size_rows = 0;
+    for (const auto& region_iter : region_tree) {
+      total_size_rows += region_iter.second.size();
+    }
+
+    *output << "<tr>\n"
+            << "  <td rowspan=\"" << total_size_rows <<"\">" << cloud_iter.first << "</td>\n";
+
+    for (const auto& region_iter : region_tree) {
+      const auto& zone_tree = region_iter.second;
+
+      if (needs_new_row) {
+        *output << "<tr>\n";
+        needs_new_row = false;
+      }
+
+      *output << "  <td rowspan=\"" << zone_tree.size() <<"\">" << region_iter.first
+              << "</td>\n";
+
+      for (const auto& zone_iter : zone_tree) {
+        const auto& counts = zone_iter.second;
+
+        if (needs_new_row) {
+          *output << "<tr>\n";
+        }
+
+        *output << "  <td>" << zone_iter.first << "</td>\n";
+
+        uint32_t user_leaders = counts.tablet_counts.user_tablet_leaders;
+        uint32_t user_total = user_leaders + counts.tablet_counts.user_tablet_followers;
+        uint32_t system_leaders = counts.tablet_counts.system_tablet_leaders;
+        uint32_t system_total = system_leaders + counts.tablet_counts.system_tablet_followers;
+
+        *output << "  <td>" << counts.node_count << "</td>\n"
+                << "  <td>" << user_total << " / " << user_leaders << "</td>\n"
+                << "  <td>" << system_total << " / " << system_leaders << "</td>\n"
+                << "  <td>" << user_total + system_total << "</td>\n"
+                << "</tr>\n";
+
+        needs_new_row = true;
+      }
+    }
+  }
+
+  *output << "</table>\n";
+}
+
+MasterPathHandlers::ZoneTabletCounts::CloudTree MasterPathHandlers::CalculateTabletCountsTree(
+  const std::vector<std::shared_ptr<TSDescriptor>>& descriptors,
+  const TabletCountMap& tablet_count_map
+) {
+  ZoneTabletCounts::CloudTree cloud_tree;
+
+  for (const auto& descriptor : descriptors) {
+    CloudInfoPB cloud_info = descriptor->GetRegistration().common().cloud_info();
+    std::string cloud = cloud_info.placement_cloud();
+    std::string region = cloud_info.placement_region();
+    std::string zone = cloud_info.placement_zone();
+
+    auto tablet_count_search = tablet_count_map.find(descriptor->permanent_uuid());
+    ZoneTabletCounts counts = tablet_count_search == tablet_count_map.end()
+        ? ZoneTabletCounts()
+        : ZoneTabletCounts(tablet_count_search->second);
+
+    auto cloud_iter = cloud_tree.find(cloud);
+    if (cloud_iter == cloud_tree.end()) {
+      ZoneTabletCounts::RegionTree region_tree;
+      ZoneTabletCounts::ZoneTree zone_tree;
+
+      zone_tree.emplace(zone, std::move(counts));
+      region_tree.emplace(region, std::move(zone_tree));
+      cloud_tree.emplace(cloud, std::move(region_tree));
+    } else {
+      ZoneTabletCounts::RegionTree& region_tree = cloud_iter->second;
+
+      auto region_iter = region_tree.find(region);
+      if (region_iter == region_tree.end()) {
+        ZoneTabletCounts::ZoneTree zone_tree;
+
+        zone_tree.emplace(zone, std::move(counts));
+        region_tree.emplace(region, std::move(zone_tree));
+      } else {
+        ZoneTabletCounts::ZoneTree& zone_tree = region_iter->second;
+
+        auto zone_iter = zone_tree.find(zone);
+        if (zone_iter == zone_tree.end()) {
+          zone_tree.emplace(zone, std::move(counts));
+        } else {
+          zone_iter->second += counts;
+        }
+      }
+    }
+  }
+
+  return cloud_tree;
+}
+
 void MasterPathHandlers::HandleTabletServers(const Webserver::WebRequest& req,
                                              stringstream* output) {
   master_->catalog_manager()->AssertLeaderLockAcquiredForReading();
@@ -292,6 +428,9 @@ void MasterPathHandlers::HandleTabletServers(const Webserver::WebRequest& req,
     TServerTable(output);
     TServerDisplay(read_replica_uuid, &descs, &tablet_map, output);
   }
+
+  ZoneTabletCounts::CloudTree counts_tree = CalculateTabletCountsTree(descs, tablet_map);
+  DisplayTabletZonesTable(counts_tree, output);
 }
 
 void MasterPathHandlers::HandleGetTserverStatus(const Webserver::WebRequest& req,

src/yb/master/master-path-handlers.h
@@ -87,6 +87,25 @@ class MasterPathHandlers {
     uint32_t user_tablet_followers = 0;
     uint32_t system_tablet_leaders = 0;
     uint32_t system_tablet_followers = 0;
+
+    void operator+=(const TabletCounts& other);
+  };
+
+  // Struct used to store the number of nodes and tablets in an availability zone.
+  struct ZoneTabletCounts {
+    TabletCounts tablet_counts;
+    uint32_t node_count;
+
+    ZoneTabletCounts();
+
+    // Create a ZoneTabletCounts object from the TabletCounts of a TServer (one node).
+    explicit ZoneTabletCounts(const TabletCounts& tablet_counts);
+
+    void operator+=(const ZoneTabletCounts& other);
+
+    typedef std::map<std::string, ZoneTabletCounts> ZoneTree;
+    typedef std::map<std::string, ZoneTree> RegionTree;
+    typedef std::map<std::string, RegionTree> CloudTree;
   };
 
   // Map of tserver UUID -> TabletCounts
@@ -102,6 +121,20 @@ class MasterPathHandlers {
                       std::vector<std::shared_ptr<TSDescriptor>>* descs,
                       TabletCountMap* tmap,
                       std::stringstream* output);
+
+  // Outputs a ZoneTabletCounts::CloudTree as an html table with a heading.
+  static void DisplayTabletZonesTable(
+    const ZoneTabletCounts::CloudTree& counts,
+    std::stringstream* output
+  );
+
+  // Builds a "cloud -> region -> zone" tree of tablet and node counts.
+  // Each leaf of the tree is a ZoneTabletCounts struct corresponding to the
+  // unique availability zone identified by the path from the root to the leaf.
+  ZoneTabletCounts::CloudTree CalculateTabletCountsTree(
+    const std::vector<std::shared_ptr<TSDescriptor>>& descriptors,
+    const TabletCountMap& tablet_count_map
+  );
 
   void CallIfLeaderOrPrintRedirect(const Webserver::WebRequest& req, std::stringstream* output,
                                    const Webserver::PathHandlerCallback& callback);

commit 54db060b37adb40670f0176f7fba220c98c76989

#1428 Only include active tablet-peers in tablets-by-az table
Summary:
Renamed the last columns in both tables in the master tablets ui to include the word
"Active", and fixed the last column in the "Tablet-Peers by Availability Zone" table to only include
active tablet-peers (rather than active and inactive).

Caused by https://github.com/YugaByte/yugabyte-db/commit/29a4a8adef8b01afbf0ec2e77331340ea675d129

Test Plan:
Manually tested by starting a local cluster, adding nodes and tables, then verifying that the table shows the correct values. Ran `CassandraKeyValue` workload for several minutes, then restarted a node, the following screenshot shows the tablets page a few seconds after the restart. The "Total Active Tablet-Peers" column is now the correct sum.

{F12219}

Reviewers: kannan, bogdan

Reviewed By: kannan, bogdan

Subscribers: kannan, ybase, bogdan

Differential Revision: https://phabricator.dev.yugabyte.com/D6664

src/yb/master/master-path-handlers.cc
@@ -100,16 +100,17 @@ void MasterPathHandlers::TabletCounts::operator+=(const TabletCounts& other) {
   system_tablet_followers += other.system_tablet_followers;
 }
 
-MasterPathHandlers::ZoneTabletCounts::ZoneTabletCounts(): tablet_counts(), node_count(1) {
-}
-
-MasterPathHandlers::ZoneTabletCounts::ZoneTabletCounts(const TabletCounts& tablet_counts)
-  : tablet_counts(tablet_counts), node_count(1) {
+MasterPathHandlers::ZoneTabletCounts::ZoneTabletCounts(
+  const TabletCounts& tablet_counts,
+  uint32_t active_tablets_count
+  ) : tablet_counts(tablet_counts),
+      active_tablets_count(active_tablets_count) {
 }
 
 void MasterPathHandlers::ZoneTabletCounts::operator+=(const ZoneTabletCounts& other) {
   tablet_counts += other.tablet_counts;
   node_count += other.node_count;
+  active_tablets_count += other.active_tablets_count;
 }
 
 void MasterPathHandlers::CallIfLeaderOrPrintRedirect(
@@ -190,7 +191,7 @@ inline void MasterPathHandlers::TServerTable(std::stringstream* output) {
           << "      <th>Region</th>\n"
           << "      <th>Zone</th>\n"
           << "      <th>System Tablet-Peers / Leaders</th>\n"
-          << "      <th>Total Tablet-Peers</th>\n"
+          << "      <th>Active Tablet-Peers</th>\n"
           << "    </tr>\n";
 }
 
@@ -279,7 +280,7 @@ void MasterPathHandlers::DisplayTabletZonesTable(
           << "    <th>Total Nodes</th>\n"
           << "    <th>User Tablet-Peers / Leaders</th>\n"
           << "    <th>System Tablet-Peers / Leaders</th>\n"
-          << "    <th>Total Tablet-Peers</th>\n"
+          << "    <th>Active Tablet-Peers</th>\n"
           << "  </tr>\n";
 
   for (const auto& cloud_iter : cloud_tree) {
@@ -322,7 +323,7 @@ void MasterPathHandlers::DisplayTabletZonesTable(
         *output << "  <td>" << counts.node_count << "</td>\n"
                 << "  <td>" << user_total << " / " << user_leaders << "</td>\n"
                 << "  <td>" << system_total << " / " << system_leaders << "</td>\n"
-                << "  <td>" << user_total + system_total << "</td>\n"
+                << "  <td>" << counts.active_tablets_count << "</td>\n"
                 << "</tr>\n";
 
         needs_new_row = true;
@@ -348,7 +349,7 @@ MasterPathHandlers::ZoneTabletCounts::CloudTree MasterPathHandlers::CalculateTab
     auto tablet_count_search = tablet_count_map.find(descriptor->permanent_uuid());
     ZoneTabletCounts counts = tablet_count_search == tablet_count_map.end()
         ? ZoneTabletCounts()
-        : ZoneTabletCounts(tablet_count_search->second);
+        : ZoneTabletCounts(tablet_count_search->second, descriptor->num_live_replicas());
 
     auto cloud_iter = cloud_tree.find(cloud);
     if (cloud_iter == cloud_tree.end()) {

src/yb/master/master-path-handlers.h
@@ -94,12 +94,13 @@ class MasterPathHandlers {
   // Struct used to store the number of nodes and tablets in an availability zone.
   struct ZoneTabletCounts {
     TabletCounts tablet_counts;
-    uint32_t node_count;
+    uint32_t node_count = 1;
+    uint32_t active_tablets_count;
 
-    ZoneTabletCounts();
+    ZoneTabletCounts() = default;
 
     // Create a ZoneTabletCounts object from the TabletCounts of a TServer (one node).
-    explicit ZoneTabletCounts(const TabletCounts& tablet_counts);
+    ZoneTabletCounts(const TabletCounts& tablet_counts, uint32_t active_tablets_count);
 
     void operator+=(const ZoneTabletCounts& other);
 