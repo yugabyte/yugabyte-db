dba21134902732c94281f58fe8aa31ebd6513829
Jignesh Shah <jkshah@amazon.com>
2019-06-02 20:16:08 +0000

    Fix definition of is_dummy_rel based on changes to community PostgreSQL 11.3
    This breaks compatibility with older minor versions, but don't bother
    keeping it. Binary built using newer PG minor versions works for older
    minor versions.

--- core.c
+++ core.c
@@ -35,7 +35,6 @@
  *     make_rels_by_clauseless_joins()
  *     join_is_legal()
  *     has_join_restriction()
- *     is_dummy_rel()
  *     restriction_is_constant_false()
  *
  *
@@ -940,15 +939,6 @@ has_join_restriction(PlannerInfo *root, RelOptInfo *rel)
 	return false;
 }
 
-
-/*
- * is_dummy_rel --- has relation been proven empty?
- */
-static bool
-is_dummy_rel(RelOptInfo *rel)
-{
-	return IS_DUMMY_REL(rel);
-}
 
 /*
  * Mark a relation as proven empty.
