yugabyte/yugabyte-db

commit c3a62bb1d60864254f832d489d976bdd7c422145

#1411 Speed up new YSQL cluster creation by avoiding running initdb every time
Summary:
We can run initdb once during the build, capture RocksDB of the sys catalog and tablet metadata, and use
that snapshot for creating new clusters quickly.

The initial sys catalog snapshot is stored in the `share/initial_sys_catalog_snapshot` directory, under both
`$BUILD_ROOT` and inside the release tarball. This snapshot is being used by default but can be disabled
with a flag.

Also making initdb idempotent: it will check the "initdb done" flag in the master and exit with code 0 if the flag
is already set.

Another bug fix in CQL indexes vtable: we need to ignore non-CQL tables/indexes there, otherwise the Cassandra
driver might get confused.

New master flags:
- initial_sys_catalog_snapshot_path -- the path to the initial sys catalog snapshot, or an empty string for auto-detection
- use_initial_sys_catalog_snapshot -- a flag that can be used to disable using the initial snapshot
- create_initial_sys_catalog_snapshot -- can be used to create an initial snapshot.

Adding yb_build.sh commands: initdb, reinitdb

Test Plan: Jenknis

Reviewers: mihnea, neha, neil

Reviewed By: neil

Subscribers: yql

Differential Revision: https://phabricator.dev.yugabyte.com/D6649

src/yb/master/catalog_manager.cc
@@ -116,11 +116,15 @@
 #include "yb/master/yql_size_estimates_vtable.h"
 #include "yb/master/catalog_manager_bg_tasks.h"
 #include "yb/master/catalog_loaders.h"
+#include "yb/master/initial_sys_catalog_snapshot.h"
+
 #include "yb/tserver/ts_tablet_manager.h"
 #include "yb/rpc/messenger.h"
+
 #include "yb/tablet/operations/change_metadata_operation.h"
 #include "yb/tablet/tablet.h"
 #include "yb/tablet/tablet_metadata.h"
+
 #include "yb/yql/redis/redisserver/redis_constants.h"
 #include "yb/tserver/tserver_admin.proxy.h"
 
@@ -142,7 +146,7 @@
 
 #include "yb/tserver/remote_bootstrap_client.h"
 
-using namespace std::literals; // NOLINT
+using namespace std::literals;
 
 DEFINE_int32(master_ts_rpc_timeout_ms, 30 * 1000,  // 30 sec
              "Timeout used for the Master->TS async rpc calls.");
@@ -524,6 +528,42 @@ Status CatalogManager::VisitSysCatalog(int64_t term) {
   // Prepare various default system configurations.
   RETURN_NOT_OK(PrepareDefaultSysConfig(term));
 
+  if (FLAGS_use_initial_sys_catalog_snapshot &&
+      !FLAGS_initial_sys_catalog_snapshot_path.empty() &&
+      !FLAGS_create_initial_sys_catalog_snapshot) {
+    Result<bool> dir_exists =
+        Env::Default()->DoesDirectoryExist(FLAGS_initial_sys_catalog_snapshot_path);
+    if (dir_exists.ok() && *dir_exists) {
+      bool initdb_was_already_done = false;
+      {
+        auto l = ysql_catalog_config_->LockForRead();
+        initdb_was_already_done = l->data().pb.ysql_catalog_config().initdb_done();
+      }
+      if (initdb_was_already_done) {
+        LOG(INFO) << "initdb has been run before, no need to restore sys catalog from a snapshot";
+      } else {
+        LOG(INFO) << "Restoring snapshot in sys catalog";
+        Status restore_status = RestoreInitialSysCatalogSnapshot(
+            FLAGS_initial_sys_catalog_snapshot_path,
+            sys_catalog_->tablet_peer().get(),
+            term);
+        if (!restore_status.ok()) {
+          LOG(ERROR) << "Failed restoring snapshot in sys catalog";
+          return restore_status;
+        }
+
+        LOG(INFO) << "Restoring snapshot completed, considering initdb finished";
+        RETURN_NOT_OK(InitDbFinished(Status::OK(), term));
+        RETURN_NOT_OK(RunLoaders());
+      }
+
+    } else {
+      LOG(WARNING) << "Initial sys catalog snapshot directory does not exist: "
+                   << FLAGS_initial_sys_catalog_snapshot_path
+                   << (dir_exists.ok() ? "" : ", status: " + dir_exists.status().ToString());
+    }
+  }
+
   // Create the system namespaces (created only if they don't already exist).
   RETURN_NOT_OK(PrepareDefaultNamespaces(term));
 
@@ -539,10 +579,9 @@ Status CatalogManager::VisitSysCatalog(int64_t term) {
 
   permissions_manager_->BuildRecursiveRolesUnlocked();
 
-  // No error handling here -- this does not wait for initdb to finish, that is done asynchronously.
-  StartRunningInitDbIfNeeded();
-
-  return Status::OK();
+  // The error handling here is not for the initdb result -- that handling is done asynchronously.
+  // This just starts initdb in the background and returns.
+  return StartRunningInitDbIfNeeded(term);
 }
 
 Status CatalogManager::RunLoaders() {
@@ -688,50 +727,54 @@ Status CatalogManager::PrepareDefaultSysConfig(int64_t term) {
   return Status::OK();
 }
 
-void CatalogManager::StartRunningInitDbIfNeeded() {
+Status CatalogManager::StartRunningInitDbIfNeeded(int64_t term) {
   CHECK(lock_.is_locked());
   if (!FLAGS_master_auto_run_initdb) {
-    return;
+    return Status::OK();
   }
 
   {
     auto l = ysql_catalog_config_->LockForRead();
     if (l->data().pb.ysql_catalog_config().initdb_done()) {
       LOG(INFO) << "Cluster configuration indicates that initdb has already completed";
-      return;
+      return Status::OK();
     }
   }
 
-  const bool pg_proc_exists = DoesPgProcExistUnlocked();
-
-  {
-    auto l = ysql_catalog_config_->LockForWrite();
-    auto* mutable_ysql_catalog_config = l->mutable_data()->pb.mutable_ysql_catalog_config();
-
-    // If pg_proc_exists is set, we're not actually planning to run initdb (see the logic below
-    // this locking block), but we are still setting the initdb_started flag to indicate that
-    // someone else has started running initdb previously.
-    mutable_ysql_catalog_config->set_initdb_started(true);
-    if (!pg_proc_exists) {
-      mutable_ysql_catalog_config->set_initdb_started_by_master(true);
-    }
-    l->Commit();
-  }
-
-  if (pg_proc_exists) {
+  if (pg_proc_exists_) {
     LOG(INFO) << "Table pg_proc exists, assuming initdb has already been run";
-    return;
+    return Status::OK();
   }
 
   LOG(INFO) << "initdb has never been run on this cluster, running it";
   string master_addresses_str = MasterAddressesToString(
       *master_->opts().GetMasterAddresses());
 
-  initdb_future_ = std::async(std::launch::async, [this, master_addresses_str] {
-    Status initdb_status = PgWrapper::InitDbForYSQL(master_addresses_str, "/tmp");
-    InitDbFinished(initdb_status);
-    return initdb_status;
+  initdb_future_ = std::async(std::launch::async, [this, master_addresses_str, term] {
+    if (FLAGS_create_initial_sys_catalog_snapshot) {
+      initial_snapshot_writer_.emplace();
+    }
+
+    Status status = PgWrapper::InitDbForYSQL(master_addresses_str, "/tmp");
+
+    if (FLAGS_create_initial_sys_catalog_snapshot && status.ok()) {
+      Status write_snasphot_status = initial_snapshot_writer_->WriteSnapshot(
+          sys_catalog_->tablet_peer()->tablet(),
+          FLAGS_initial_sys_catalog_snapshot_path);
+      if (!write_snasphot_status.ok()) {
+        status = write_snasphot_status;
+      }
+    }
+    Status finish_status = InitDbFinished(status, term);
+    if (!finish_status.ok()) {
+      if (status.ok()) {
+        status = finish_status;
+      }
+      LOG(WARNING) << "Failed to set initdb as finished in sys catalog: " << finish_status;
+    }
+    return status;
   });
+  return Status::OK();
 }
 
 Status CatalogManager::PrepareDefaultNamespaces(int64_t term) {
@@ -1376,6 +1419,16 @@ Status CatalogManager::CreatePgsqlSysTable(const CreateTableRequestPB* req,
                              "PostgreSQL system catalog tables are non-partitioned"));
   }
 
+  if (req->table_type() != TableType::PGSQL_TABLE_TYPE) {
+    return SetupError(resp->mutable_error(), MasterErrorPB::INVALID_SCHEMA,
+                      STATUS_FORMAT(
+                          InvalidArgument,
+                          "Expected table type to be PGSQL_TABLE_TYPE ($0), got $1 ($2)",
+                          PGSQL_TABLE_TYPE,
+                          TableType_Name(req->table_type())));
+
+  }
+
   // Create partition schema and one partition.
   PartitionSchema partition_schema;
   vector<Partition> partitions;
@@ -1437,18 +1490,13 @@ Status CatalogManager::CreatePgsqlSysTable(const CreateTableRequestPB* req,
 
   partition_schema.ToPB(add_table.mutable_partition_schema());
 
-  auto operation_state = std::make_unique<tablet::ChangeMetadataOperationState>(
-      sys_catalog_->tablet_peer_->tablet(), sys_catalog_->tablet_peer_->log(), &change_req);
+  RETURN_NOT_OK(tablet::SyncReplicateChangeMetadataOperation(
+      &change_req, sys_catalog_->tablet_peer().get(), leader_ready_term_));
 
-  Synchronizer synchronizer;
-
-  operation_state->set_completion_callback(
-      std::make_unique<tablet::SynchronizerOperationCompletionCallback>(&synchronizer));
-
-  sys_catalog_->tablet_peer_->Submit(std::make_unique<tablet::ChangeMetadataOperation>(
-      std::move(operation_state)), sys_catalog_->tablet_peer_->LeaderTerm());
-
-  return synchronizer.Wait();
+  if (initial_snapshot_writer_) {
+    initial_snapshot_writer_->AddMetadataChange(change_req);
+  }
+  return Status::OK();
 }
 
 Status CatalogManager::ReservePgsqlOids(const ReservePgsqlOidsRequestPB* req,
@@ -1933,7 +1981,8 @@ Status CatalogManager::CreateTableInMemory(const CreateTableRequestPB& req,
 
   // Add the new table in "preparing" state.
   table->reset(CreateTableInfo(req, schema, partition_schema, namespace_id, index_info));
-  table_ids_map_[(*table)->id()] = *table;
+  const TableId& table_id = (*table)->id();
+  table_ids_map_[table_id] = *table;
   // Do not add Postgres tables to the name map as the table name is not unique in a namespace.
   if (req.table_type() != PGSQL_TABLE_TYPE) {
     table_names_map_[{namespace_id, req.name()}] = *table;
@@ -1944,8 +1993,10 @@ Status CatalogManager::CreateTableInMemory(const CreateTableRequestPB& req,
   }
 
   if (resp != nullptr) {
-    resp->set_table_id((*table)->id());
+    resp->set_table_id(table_id);
   }
+
+  HandleNewTableId(table_id);
 
   return Status::OK();
 }
@@ -3065,6 +3116,7 @@ scoped_refptr<TableInfo> CatalogManager::GetTableInfo(const TableId& table_id) {
   boost::shared_lock<LockType> l(lock_);
   return FindPtrOrNull(table_ids_map_, table_id);
 }
+
 scoped_refptr<TableInfo> CatalogManager::GetTableInfoFromNamespaceNameAndTableName(
     const NamespaceName& namespace_name, const TableName& table_name) {
 
@@ -4196,7 +4248,7 @@ Result<uint64_t> CatalogManager::IncrementYsqlCatalogVersion() {
   return new_version;
 }
 
-void CatalogManager::InitDbFinished(Status initdb_status) {
+Status CatalogManager::InitDbFinished(Status initdb_status, int64_t term) {
   if (initdb_status.ok()) {
     LOG(INFO) << "initdb completed successfully";
   } else {
@@ -4212,7 +4264,10 @@ void CatalogManager::InitDbFinished(Status initdb_status) {
     mutable_ysql_catalog_config->clear_initdb_error();
   }
 
+  RETURN_NOT_OK(sys_catalog_->AddItem(ysql_catalog_config_.get(), term));
+
   l->Commit();
+  return Status::OK();
 }
 
 CHECKED_STATUS CatalogManager::IsInitDbDone(
@@ -4222,8 +4277,7 @@ CHECKED_STATUS CatalogManager::IsInitDbDone(
 
   auto l = CHECK_NOTNULL(ysql_catalog_config_.get())->LockForRead();
   const auto& ysql_catalog_config = l->data().pb.ysql_catalog_config();
-  resp->set_started(ysql_catalog_config.initdb_started());
-  resp->set_started_by_master(ysql_catalog_config.initdb_started_by_master());
+  resp->set_pg_proc_exists(pg_proc_exists_.load(std::memory_order_acquire));
   resp->set_done(ysql_catalog_config.initdb_done());
   if (ysql_catalog_config.has_initdb_error() &&
       !ysql_catalog_config.initdb_error().empty()) {
@@ -5376,7 +5430,6 @@ Status CatalogManager::GetTableLocations(const GetTableLocationsRequestPB* req,
   }
 
   resp->set_table_type(table->metadata().state().pb.table_type());
-
   return Status::OK();
 }
 
@@ -5814,15 +5867,11 @@ void CatalogManager::AbortAndWaitForAllTasks(const vector<scoped_refptr<TableInf
   VLOG(1) << "Waiting on Aborting tasks done";
 }
 
-bool CatalogManager::DoesPgProcExistUnlocked() {
-  // Checking if the pg_proc table from template1 database exists.
-  // There will be a pg_proc table in each user database (e.g. the "postgres" database for the
-  // "postgres" user) too.
-
-  const uint32_t kTemplate1Oid = 1;  // Hardcoded for template1. (in initdb.c)
-  const uint32_t kPgProcTableOid = 1255;  // Hardcoded for pg_proc. (in pg_proc.h)
-  static string kPgProcTableMap = GetPgsqlTableId(kTemplate1Oid, kPgProcTableOid);
-  return table_ids_map_.count(kPgProcTableMap);
+void CatalogManager::HandleNewTableId(const TableId& table_id) {
+  if (table_id == kPgProcTableId) {
+    // Needed to track whether initdb has started running.
+    pg_proc_exists_.store(true, std::memory_order_release);
+  }
 }
 
 }  // namespace master

src/yb/master/initial_sys_catalog_snapshot.cc
@@ -1,0 +1,201 @@
+// Copyright (c) YugaByte, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+// in compliance with the License.  You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software distributed under the License
+// is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+// or implied.  See the License for the specific language governing permissions and limitations
+// under the License.
+//
+
+#include "yb/master/initial_sys_catalog_snapshot.h"
+
+#include "yb/util/countdown_latch.h"
+#include "yb/util/pb_util.h"
+#include "yb/util/env_util.h"
+#include "yb/util/path_util.h"
+
+#include "yb/master/sys_catalog_constants.h"
+
+#include "yb/tablet/tablet.h"
+#include "yb/tablet/tablet_peer.h"
+#include "yb/tablet/operations/operation.h"
+#include "yb/tablet/operations/snapshot_operation.h"
+#include "yb/tablet/operations/change_metadata_operation.h"
+
+DEFINE_string(
+    initial_sys_catalog_snapshot_path, "",
+    "If this is specified, system catalog RocksDB is checkpointed at this location after initdb "
+    "is done.");
+
+DEFINE_bool(
+    use_initial_sys_catalog_snapshot, true,
+    "Initialize sys catalog tablet from a pre-existing snapshot instead of running initdb. "
+    "Only takes effect if --initial_sys_catalog_snapshot_path is specified or can be "
+    "auto-detected.");
+
+DEFINE_bool(
+    create_initial_sys_catalog_snapshot, false,
+    "Run initdb and create an initial sys catalog data snapshot");
+
+using yb::CountDownLatch;
+using yb::tserver::TabletSnapshotOpRequestPB;
+using yb::tserver::TabletSnapshotOpResponsePB;
+using yb::tablet::SnapshotOperationState;
+using yb::tablet::LatchOperationCompletionCallback;
+using yb::pb_util::ReadPBContainerFromPath;
+
+namespace yb {
+namespace master {
+
+namespace {
+
+const char* kDefaultInitialSysCatalogSnapshotDir = "initial_sys_catalog_snapshot";
+const char* kSysCatalogSnapshotRocksDbSubDir = "rocksdb";
+const char* kSysCatalogSnapshotTabletMetadataChangesFile =
+    "exported_tablet_metadata_changes";
+const char* kUseInitialSysCatalogSnapshotEnvVar = "YB_USE_INITIAL_SYS_CATALOG_SNAPSHOT";
+}  // anonymous namespace
+
+// ------------------------------------------------------------------------------------------------
+// InitialSysCatalogSnapshotWriter
+// ------------------------------------------------------------------------------------------------
+
+void InitialSysCatalogSnapshotWriter::AddMetadataChange(
+    tserver::ChangeMetadataRequestPB metadata_change) {
+  initdb_metadata_changes_.push_back(std::move(metadata_change));
+}
+
+Status InitialSysCatalogSnapshotWriter::WriteSnapshot(
+    tablet::Tablet* sys_catalog_tablet,
+    const std::string& dest_path) {
+  RETURN_NOT_OK(sys_catalog_tablet->Flush(yb::tablet::FlushMode::kSync));
+  RETURN_NOT_OK(Env::Default()->CreateDir(dest_path));
+  RETURN_NOT_OK(sys_catalog_tablet->CreateCheckpoint(
+      JoinPathSegments(dest_path, kSysCatalogSnapshotRocksDbSubDir)));
+
+  tserver::ExportedTabletMetadataChanges exported_tablet_metadata_changes;
+  for (int i = 0; i < initdb_metadata_changes_.size(); ++i) {
+    *exported_tablet_metadata_changes.add_metadata_changes() = std::move(
+        initdb_metadata_changes_[i]);
+  }
+
+  const string metadata_changes_file = JoinPathSegments(
+      dest_path,
+      kSysCatalogSnapshotTabletMetadataChangesFile);
+  RETURN_NOT_OK(WritePBContainerToPath(
+      Env::Default(),
+      metadata_changes_file,
+      exported_tablet_metadata_changes,
+      pb_util::CreateMode::NO_OVERWRITE,
+      pb_util::SyncMode::NO_SYNC));
+  LOG(INFO) << "Wrote " << initdb_metadata_changes_.size() << " tablet metadata changes to file "
+            << metadata_changes_file;
+
+  LOG(INFO) << "Created initial sys catalog snapshot at " << dest_path;
+  return Status::OK();
+}
+
+// ------------------------------------------------------------------------------------------------
+// End of InitialSysCatalogSnapshotWriter
+// ------------------------------------------------------------------------------------------------
+
+Status RestoreInitialSysCatalogSnapshot(
+    const std::string& initial_snapshot_path,
+    tablet::TabletPeer* sys_catalog_tablet_peer,
+    int64_t term) {
+  TabletSnapshotOpRequestPB tablet_snapshot_req;
+  tablet_snapshot_req.set_operation(yb::tserver::TabletSnapshotOpRequestPB::RESTORE);
+  tablet_snapshot_req.set_tablet_id(kSysCatalogTabletId);
+  tablet_snapshot_req.set_snapshot_dir_override(
+      JoinPathSegments(initial_snapshot_path, kSysCatalogSnapshotRocksDbSubDir));
+
+  TabletSnapshotOpResponsePB tablet_snapshot_resp;
+  auto tx_state = std::make_unique<SnapshotOperationState>(
+      sys_catalog_tablet_peer->tablet(), &tablet_snapshot_req);
+
+  CountDownLatch latch(1);
+  tx_state->set_completion_callback(
+      std::make_unique<LatchOperationCompletionCallback<TabletSnapshotOpResponsePB>>(
+          &latch, &tablet_snapshot_resp));
+
+  sys_catalog_tablet_peer->Submit(
+      std::make_unique<tablet::SnapshotOperation>(std::move(tx_state)),
+      term);
+
+  // Now restore tablet metadata.
+  tserver::ExportedTabletMetadataChanges tablet_metadata_changes;
+  RETURN_NOT_OK(ReadPBContainerFromPath(
+      Env::Default(),
+      JoinPathSegments(initial_snapshot_path, kSysCatalogSnapshotTabletMetadataChangesFile),
+      &tablet_metadata_changes));
+  for (const tserver::ChangeMetadataRequestPB& change_metadata_req :
+          tablet_metadata_changes.metadata_changes()) {
+    RETURN_NOT_OK(tablet::SyncReplicateChangeMetadataOperation(
+        &change_metadata_req,
+        sys_catalog_tablet_peer,
+        term));
+  }
+  LOG(INFO) << "Imported " << tablet_metadata_changes.metadata_changes_size()
+            << " tablet metadata changes";
+
+  latch.Wait();
+  return Status::OK();
+}
+
+void SetDefaultInitialSysCatalogSnapshotFlags() {
+  // Allowing to turn off the use of initial catalog snapshot with an env variable -- useful in
+  // tests.
+  const char* env_var_value = getenv(kUseInitialSysCatalogSnapshotEnvVar);
+  if (env_var_value && strcmp(env_var_value, "0") == 0) {
+    LOG(INFO) << "Disabling the use of initial sys catalog snapshot: env var "
+              << kUseInitialSysCatalogSnapshotEnvVar << " is set to 0";
+    FLAGS_use_initial_sys_catalog_snapshot = 0;
+  }
+
+  if (FLAGS_initial_sys_catalog_snapshot_path.empty() &&
+      !FLAGS_create_initial_sys_catalog_snapshot &&
+      FLAGS_use_initial_sys_catalog_snapshot) {
+    const char* kStaticDataParentDir = "share";
+    const std::string search_for_dir = JoinPathSegments(
+        kStaticDataParentDir, kDefaultInitialSysCatalogSnapshotDir,
+        kSysCatalogSnapshotRocksDbSubDir);
+    VLOG(1) << "Searching for directory containing subdirectory " << search_for_dir;
+    const string candidate_dir =
+        JoinPathSegments(
+            env_util::GetRootDir(search_for_dir),
+            kStaticDataParentDir,
+            kDefaultInitialSysCatalogSnapshotDir);
+    VLOG(1) << "candidate_dir=" << candidate_dir;
+
+    // The metadata changes file is written last, so its presence indicates that the snapshot
+    // was successful.
+    const string candidate_metadata_changes_path =
+        JoinPathSegments(candidate_dir, kSysCatalogSnapshotTabletMetadataChangesFile);
+    VLOG(1) << "candidate_metadata_changes_path=" << candidate_metadata_changes_path;
+
+    if (Env::Default()->FileExists(candidate_metadata_changes_path)) {
+      LOG(INFO) << "Found initial sys catalog snapshot directory: " << candidate_dir;
+      FLAGS_initial_sys_catalog_snapshot_path = candidate_dir;
+      FLAGS_use_initial_sys_catalog_snapshot = true;
+      return;
+    } else {
+      VLOG(1) << "File " << candidate_metadata_changes_path << " does not exist";
+    }
+  } else {
+    VLOG(1)
+        << "Not attempting initial sys catalog snapshot auto-detection: "
+        << "FLAGS_initial_sys_catalog_snapshot_path="
+        << FLAGS_initial_sys_catalog_snapshot_path << ", "
+        << "FLAGS_create_initial_sys_catalog_snapshot="
+        << FLAGS_create_initial_sys_catalog_snapshot << ", "
+        << "FLAGS_use_initial_sys_catalog_snapshot="
+        << FLAGS_use_initial_sys_catalog_snapshot;
+  }
+}
+
+}  // namespace master
+}  // namespace yb

src/yb/yql/pgwrapper/pg_wrapper_test_base.cc
@@ -13,6 +13,11 @@
 
 #include "yb/yql/pgwrapper/pg_wrapper_test_base.h"
 
+#include "yb/util/size_literals.h"
+#include "yb/util/format.h"
+
+using namespace yb::size_literals;
+
 DECLARE_int64(retryable_rpc_single_call_timeout_ms);
 DECLARE_int32(yb_client_admin_operation_timeout_sec);
 
@@ -49,13 +54,21 @@ void PgWrapperTestBase::SetUp() {
 
   opts.extra_master_flags.emplace_back("--hide_pg_catalog_table_creation_logs");
 
-  // Test that we can start PostgreSQL servers on non-colliding ports within each tablet server.
-  opts.num_tablet_servers = 3;
+  opts.num_masters = GetNumMasters();
+
+  opts.num_tablet_servers = GetNumTabletServers();
+
+  opts.extra_master_flags.emplace_back("--client_read_write_timeout_ms=120000");
+  opts.extra_master_flags.emplace_back(Format("--memory_limit_hard_bytes=$0", 2_GB));
+
+  UpdateMiniClusterOptions(&opts);
 
   cluster_.reset(new ExternalMiniCluster(opts));
   ASSERT_OK(cluster_->Start());
 
-  pg_ts = cluster_->tablet_server(0);
+  if (cluster_->num_tablet_servers() > 0) {
+    pg_ts = cluster_->tablet_server(0);
+  }
 
   // TODO: fix cluster verification for PostgreSQL tables.
   DontVerifyClusterBeforeNextTearDown();

commit 3ec692cc0d3ccc75124931577d06677917847a1c

#1472 Don't restore system catalog from an initial snapshot by default
Summary:
Don't restore system catalog from an initial snapshot unless both of the following are true:
- It is explicitly enabled with the use_initial_sys_catalog_snapshot flag
- It is a new cluster, as indicated by the absence of system namespaces and CQL system tablets.

Explicitly enabling initial system catalog snapshot restoration in unit tests.

This issue was introduced in https://github.com/YugaByte/yugabyte-db/commit/c3a62bb1d60864254f832d489d976bdd7c422145.

Test Plan:
Jenkins
Manual testing using yb-ctl:
- `bin/yb-ctl destroy; time bin/yb-ctl create`. This will still run initdb as `--use_initial_sys_catalog_snapshot` is now disabled by default. Result: https://gist.github.com/mbautin/e7de0175dfbc0b12daaa3611383334d4
- Create a table in cqlsh, then restart the cluster with `--use_initial_sys_catalog_snaphot=true` and verify that the CQL table is still there. Result: https://gist.github.com/mbautin/30f53e2c196897480a5ffe6ad2587040
- `bin/yb-ctl destroy; time bin/yb-ctl create --master_flags use_initial_sys_catalog_snapshot=true`. This should be much faster than the initial creation where it had to run initdb, as expected. Result: https://gist.githubusercontent.com/mbautin/c45fadcb7de5c402c2bfddbdcbde5073/raw
- Also perform similar steps to the above but with the cluster initially created, and then restarted, with the flag turned on: https://gist.githubusercontent.com/mbautin/6703e99ed7cf02f5cb40546aee4ff803/raw

Reviewers: timur, dmitry, alex, mihnea, kannan

Reviewed By: mihnea

Subscribers: kannan, mihnea, ybase, bogdan

Differential Revision: https://phabricator.dev.yugabyte.com/D6697

src/yb/master/catalog_manager.cc
@@ -531,36 +531,40 @@ Status CatalogManager::VisitSysCatalog(int64_t term) {
   if (FLAGS_use_initial_sys_catalog_snapshot &&
       !FLAGS_initial_sys_catalog_snapshot_path.empty() &&
       !FLAGS_create_initial_sys_catalog_snapshot) {
-    Result<bool> dir_exists =
-        Env::Default()->DoesDirectoryExist(FLAGS_initial_sys_catalog_snapshot_path);
-    if (dir_exists.ok() && *dir_exists) {
-      bool initdb_was_already_done = false;
-      {
-        auto l = ysql_catalog_config_->LockForRead();
-        initdb_was_already_done = l->data().pb.ysql_catalog_config().initdb_done();
-      }
-      if (initdb_was_already_done) {
-        LOG(INFO) << "initdb has been run before, no need to restore sys catalog from a snapshot";
-      } else {
-        LOG(INFO) << "Restoring snapshot in sys catalog";
-        Status restore_status = RestoreInitialSysCatalogSnapshot(
-            FLAGS_initial_sys_catalog_snapshot_path,
-            sys_catalog_->tablet_peer().get(),
-            term);
-        if (!restore_status.ok()) {
-          LOG(ERROR) << "Failed restoring snapshot in sys catalog";
-          return restore_status;
-        }
-
-        LOG(INFO) << "Restoring snapshot completed, considering initdb finished";
-        RETURN_NOT_OK(InitDbFinished(Status::OK(), term));
-        RETURN_NOT_OK(RunLoaders());
-      }
-
+    if (!namespace_ids_map_.empty() || !system_tablets_.empty()) {
+      LOG(INFO) << "This is an existing cluster, not initializing from a sys catalog snapshot.";
     } else {
-      LOG(WARNING) << "Initial sys catalog snapshot directory does not exist: "
-                   << FLAGS_initial_sys_catalog_snapshot_path
-                   << (dir_exists.ok() ? "" : ", status: " + dir_exists.status().ToString());
+      Result<bool> dir_exists =
+          Env::Default()->DoesDirectoryExist(FLAGS_initial_sys_catalog_snapshot_path);
+      if (dir_exists.ok() && *dir_exists) {
+        bool initdb_was_already_done = false;
+        {
+          auto l = ysql_catalog_config_->LockForRead();
+          initdb_was_already_done = l->data().pb.ysql_catalog_config().initdb_done();
+        }
+        if (initdb_was_already_done) {
+          LOG(INFO) << "initdb has been run before, no need to restore sys catalog from "
+                    << "the initial snapshot";
+        } else {
+          LOG(INFO) << "Restoring snapshot in sys catalog";
+          Status restore_status = RestoreInitialSysCatalogSnapshot(
+              FLAGS_initial_sys_catalog_snapshot_path,
+              sys_catalog_->tablet_peer().get(),
+              term);
+          if (!restore_status.ok()) {
+            LOG(ERROR) << "Failed restoring snapshot in sys catalog";
+            return restore_status;
+          }
+
+          LOG(INFO) << "Restoring snapshot completed, considering initdb finished";
+          RETURN_NOT_OK(InitDbFinished(Status::OK(), term));
+          RETURN_NOT_OK(RunLoaders());
+        }
+      } else {
+        LOG(WARNING) << "Initial sys catalog snapshot directory does not exist: "
+                    << FLAGS_initial_sys_catalog_snapshot_path
+                    << (dir_exists.ok() ? "" : ", status: " + dir_exists.status().ToString());
+      }
     }
   }
 

src/yb/master/initial_sys_catalog_snapshot.cc
@@ -32,7 +32,7 @@ DEFINE_string(
     "is done.");
 
 DEFINE_bool(
-    use_initial_sys_catalog_snapshot, true,
+    use_initial_sys_catalog_snapshot, false,
     "Initialize sys catalog tablet from a pre-existing snapshot instead of running initdb. "
     "Only takes effect if --initial_sys_catalog_snapshot_path is specified or can be "
     "auto-detected.");
@@ -178,9 +178,8 @@ void SetDefaultInitialSysCatalogSnapshotFlags() {
     VLOG(1) << "candidate_metadata_changes_path=" << candidate_metadata_changes_path;
 
     if (Env::Default()->FileExists(candidate_metadata_changes_path)) {
-      LOG(INFO) << "Found initial sys catalog snapshot directory: " << candidate_dir;
+      VLOG(1) << "Found initial sys catalog snapshot directory: " << candidate_dir;
       FLAGS_initial_sys_catalog_snapshot_path = candidate_dir;
-      FLAGS_use_initial_sys_catalog_snapshot = true;
       return;
     } else {
       VLOG(1) << "File " << candidate_metadata_changes_path << " does not exist";

src/yb/yql/pgwrapper/pg_wrapper_test_base.cc
@@ -61,6 +61,8 @@ void PgWrapperTestBase::SetUp() {
   opts.extra_master_flags.emplace_back("--client_read_write_timeout_ms=120000");
   opts.extra_master_flags.emplace_back(Format("--memory_limit_hard_bytes=$0", 2_GB));
 
+  opts.extra_master_flags.emplace_back("--use_initial_sys_catalog_snapshot");
+
   UpdateMiniClusterOptions(&opts);
 
   cluster_.reset(new ExternalMiniCluster(opts));

java/yb-pgsql/src/test/java/org/yb/pgsql/BasePgSQLTest.java
@@ -133,6 +133,7 @@ public class BasePgSQLTest extends BaseMiniClusterTest {
     Map<String, String> flagMap = new TreeMap<>();
     flagMap.put("client_read_write_timeout_ms", "120000");
     flagMap.put("memory_limit_hard_bytes", String.valueOf(2L * 1024 * 1024 * 1024));
+    flagMap.put("use_initial_sys_catalog_snapshot", "true");
     return flagMap;
   }
 