yugabyte/yugabyte-db

commit 0290ddc974a9f103ac4dfc30fa6d707cfb47acba

#950: ENG-4812: Implemented RPC heartbeats.
Summary:
Introducing network partitions with iptables drop input packet rules causes TCP connections which were opened and had traffic before partition start to have an additional delay in acknowledging and transmitting packets even after network partition is over. For 30 seconds partitions delay is about 23 seconds and that prevents quick recovery of normal operation.

Tuning FLAGS_rpc_default_keepalive_time_ms doesn't help and sometimes even can make things worse because Reactor::ScanIdleConnections only drops server (inbound) connections, but not client (outbound) connections. So, during partition, the server (TServer) is dropping timed out connections, but client (CQLProxy) is not. And after network partition, the client (CQLProxy) is still trying to send Read/Write RPC requests to the server (TServer), but only get an error that connection is shut down after up to 30+ seconds and during that period requests are simply being sent “nowhere” and Java client requests are timed out.

- Implemented RPC heartbeats and dropping stuck connections
- Updated TestRpc

Test Plan:
- Jenkins
- Jepsen test with network partition nemesis

Reviewers: bogdan, amitanand, mikhail, sergei

Reviewed By: sergei

Subscribers: dmitry, ybase

Differential Revision: https://phabricator.dev.yugabyte.com/D6346

src/yb/rpc/messenger.cc
@@ -354,6 +354,15 @@ bool Messenger::TEST_ShouldArtificiallyRejectOutgoingCallsTo(const IpAddress &re
   return false;
 }
 
+Status Messenger::TEST_GetReactorMetrics(size_t reactor_idx, ReactorMetrics* metrics) {
+  if (reactor_idx < 0 || reactor_idx >= reactors_.size()) {
+    return STATUS_FORMAT(
+        InvalidArgument, "Invalid reactor index $0, should be >=0 and <$1", reactor_idx,
+        reactors_.size());
+  }
+  return reactors_[reactor_idx]->GetMetrics(metrics);
+}
+
 void Messenger::ShutdownAcceptor() {
   std::unique_ptr<Acceptor> acceptor;
   {

java/yb-client/src/main/java/org/yb/client/CallResponse.java
@@ -69,14 +69,22 @@ final class CallResponse {
     this.buf = buf;
 
     this.totalResponseSize = buf.readInt();
-    YRpc.checkArrayLength(buf, this.totalResponseSize);
-    TabletClient.ensureReadable(buf, this.totalResponseSize);
+    if (this.totalResponseSize > 0) {
+      YRpc.checkArrayLength(buf, this.totalResponseSize);
+      TabletClient.ensureReadable(buf, this.totalResponseSize);
 
-    final int headerSize = Bytes.readVarInt32(buf);
-    final Slice headerSlice = nextBytes(buf, headerSize);
-    RpcHeader.ResponseHeader.Builder builder = RpcHeader.ResponseHeader.newBuilder();
-    YRpc.readProtobuf(headerSlice, builder);
-    this.header = builder.build();
+      final int headerSize = Bytes.readVarInt32(buf);
+      final Slice headerSlice = nextBytes(buf, headerSize);
+      RpcHeader.ResponseHeader.Builder builder = RpcHeader.ResponseHeader.newBuilder();
+      YRpc.readProtobuf(headerSlice, builder);
+      this.header = builder.build();
+    } else {
+      this.header = null;
+    }
+  }
+
+  public boolean isEmpty() {
+    return this.totalResponseSize == 0;
   }
 
   /**

java/yb-client/src/main/java/org/yb/client/TabletClient.java
@@ -330,6 +330,10 @@ public class TabletClient extends ReplayingDecoder<VoidEnum> {
     }
 
     CallResponse response = new CallResponse(buf);
+    if (response.isEmpty()) {
+      // Skip empty messages which we are using as heartbeats.
+      return null;
+    }
 
     RpcHeader.ResponseHeader header = response.getHeader();
     if (!header.hasCallId()) {

src/yb/rpc/binary_call_parser.cc
@@ -16,16 +16,18 @@
 
 #include "yb/gutil/endian.h"
 
+#include "yb/rpc/connection.h"
+
 namespace yb {
 namespace rpc {
 
 BinaryCallParser::BinaryCallParser(
     const MemTrackerPtr& parent_tracker,
     size_t header_size, size_t size_offset, size_t max_message_length, IncludeHeader include_header,
-    BinaryCallParserListener* listener)
+    SkipEmptyMessages skip_empty_messages, BinaryCallParserListener* listener)
     : buffer_(header_size), size_offset_(size_offset),
       max_message_length_(max_message_length), include_header_(include_header),
-      listener_(listener) {
+      skip_empty_messages_(skip_empty_messages), listener_(listener) {
   buffer_tracker_ = MemTracker::FindOrCreateTracker("Reading", parent_tracker);
 }
 
@@ -74,9 +76,14 @@ Result<ProcessDataResult> BinaryCallParser::Parse(
       break;
     }
 
-    CallData call_data(total_length - body_offset);
-    IoVecsToBuffer(data, consumed + body_offset, consumed + total_length, call_data.data());
-    RETURN_NOT_OK(listener_->HandleCall(connection, &call_data));
+    // We might need to skip empty messages (we use them as low level heartbeats for inter-YB RPC
+    // connections, don't confuse with RAFT heartbeats which are higher level non-empty messages).
+    if (!skip_empty_messages_ || data_length > 0) {
+      connection->UpdateLastActivity();
+      CallData call_data(total_length - body_offset);
+      IoVecsToBuffer(data, consumed + body_offset, consumed + total_length, call_data.data());
+      RETURN_NOT_OK(listener_->HandleCall(connection, &call_data));
+    }
 
     consumed += total_length;
   }

src/yb/rpc/binary_call_parser.h
@@ -25,6 +25,7 @@ namespace yb {
 namespace rpc {
 
 YB_STRONGLY_TYPED_BOOL(IncludeHeader);
+YB_STRONGLY_TYPED_BOOL(SkipEmptyMessages);
 
 // Listener of BinaryCallParser, invoked when call is parsed.
 class BinaryCallParserListener {
@@ -39,7 +40,8 @@ class BinaryCallParser {
  public:
   explicit BinaryCallParser(const MemTrackerPtr& parent_tracker,
                             size_t header_size, size_t size_offset, size_t max_message_length,
-                            IncludeHeader include_header, BinaryCallParserListener* listener);
+                            IncludeHeader include_header, SkipEmptyMessages skip_empty_messages,
+                            BinaryCallParserListener* listener);
 
   Result<ProcessDataResult> Parse(const rpc::ConnectionPtr& connection, const IoVecs& data,
                                   ReadBufferFull read_buffer_full);
@@ -53,6 +55,7 @@ class BinaryCallParser {
   const size_t size_offset_;
   const size_t max_message_length_;
   const IncludeHeader include_header_;
+  const SkipEmptyMessages skip_empty_messages_;
   BinaryCallParserListener* const listener_;
 };
 

src/yb/rpc/connection.cc
@@ -162,10 +162,6 @@ void Connection::OutboundQueued() {
   }
 }
 
-void StartTimer(CoarseMonoClock::Duration left, ev::timer* timer) {
-  timer->start(MonoDelta(left).ToSeconds(), 0);
-}
-
 void Connection::HandleTimeout(ev::timer& watcher, int revents) {  // NOLINT
   DCHECK(reactor_->IsCurrentThread());
 
@@ -433,6 +429,8 @@ void Connection::ProcessResponseQueue() {
 Status Connection::Start(ev::loop_ref* loop) {
   DCHECK(reactor_->IsCurrentThread());
 
+  context_->SetEventLoop(loop);
+
   RETURN_NOT_OK(stream_->Start(direction_ == Direction::CLIENT, loop, this));
 
   timer_.set(*loop);
@@ -476,6 +474,14 @@ void Connection::UpdateLastActivity() {
   last_activity_time_ = reactor_->cur_time();
 }
 
+void Connection::UpdateLastRead() {
+  context_->UpdateLastRead(shared_from_this());
+}
+
+void Connection::UpdateLastWrite() {
+  context_->UpdateLastWrite(shared_from_this());
+}
+
 void Connection::Transferred(const OutboundDataPtr& data, const Status& status) {
   data->Transferred(status, this);
 }
@@ -486,6 +492,10 @@ void Connection::Destroy(const Status& status) {
 
 std::string Connection::LogPrefix() const {
   return ToString() + ": ";
+}
+
+void StartTimer(CoarseMonoClock::Duration left, ev::timer* timer) {
+  timer->start(MonoDelta(left).ToSeconds(), 0 /* repeat */);
 }
 
 }  // namespace rpc

src/yb/rpc/connection.h
@@ -33,7 +33,6 @@
 #ifndef YB_RPC_CONNECTION_H_
 #define YB_RPC_CONNECTION_H_
 
-
 #include <atomic>
 #include <cstdint>
 #include <limits>
@@ -112,6 +111,8 @@ class Connection final : public StreamContext, public std::enable_shared_from_th
   CoarseTimePoint last_activity_time() const {
     return last_activity_time_;
   }
+
+  void UpdateLastActivity() override;
 
   // Returns true if we are not in the process of receiving or sending a
   // message, and we have no outstanding calls.
@@ -193,7 +194,10 @@ class Connection final : public StreamContext, public std::enable_shared_from_th
   void ProcessResponseQueue();
 
   // Stream context implementation
-  void UpdateLastActivity() override;
+  void UpdateLastRead() override;
+
+  void UpdateLastWrite() override;
+
   void Transferred(const OutboundDataPtr& data, const Status& status) override;
   void Destroy(const Status& status) override;
   Result<ProcessDataResult> ProcessReceived(
@@ -267,6 +271,8 @@ class Connection final : public StreamContext, public std::enable_shared_from_th
 
   std::atomic<uint64_t> responded_call_count_{0};
 };
+
+void StartTimer(CoarseMonoClock::Duration left, ev::timer* timer);
 
 }  // namespace rpc
 }  // namespace yb

src/yb/rpc/connection_context.cc
@@ -13,6 +13,8 @@
 
 #include "yb/rpc/connection_context.h"
 
+#include "yb/rpc/connection.h"
+
 #include "yb/rpc/growable_buffer.h"
 
 #include "yb/util/env.h"
@@ -27,6 +29,13 @@ DEFINE_int64(read_buffer_memory_limit, -5,
 
 namespace yb {
 namespace rpc {
+
+void ConnectionContext::UpdateLastRead(const ConnectionPtr& connection) {
+  // By default any read events on connection updates it's last activity. This could be
+  // overriden in subclasses for example in order to not treat application-level heartbeats as
+  // activity preventing connection from being GCed.
+  connection->UpdateLastActivity();
+}
 
 ConnectionContextFactory::ConnectionContextFactory(
     int64_t memory_limit, const std::string& name,

commit 24a8835d4bc549dd69eb0fba1d78592a4fe8aff8

Fix build after compile error on messenger.cc
Summary:
MacOS build seems to have an issue with < 0 check on size_t that was introduced in D6346

FAILED: src/yb/rpc/CMakeFiles/yrpc.dir/messenger.cc.o
/Users/amitanand/code/yugabyte/build-support/compiler-wrappers/c++  -Dyrpc_EXPORTS -Isrc -I../../ent/src -I../../src -isystem /usr/local/opt/openssl/include -isystem ../../thirdparty/installed/clang_uninstrumented/include -isystem ../../thirdparty/installed/common/include -isystem /usr/local/opt/icu4c/include -Werror -fno-strict-aliasing -msse4.2 -Wall -Wno-sign-compare -Wno-deprecated -Winvalid-pch -pthread -DBOOST_BIND_NO_PLACEHOLDERS -DROCKSDB_PLATFORM_POSIX -DROCKSDB_USING_THREAD_STATUS=0 -DBOOST_ERROR_CODE_HEADER_ONLY -DYB_COMPILER_TYPE=clang -DYB_COMPILER_VERSION=902.0.39.1 -DROCKSDB_LIB_IO_POSIX -DBZIP2 -DSNAPPY -DZLIB -D__STDC_FORMAT_MACROS -Wno-deprecated-declarations -DGFLAGS=gflags -Wnon-virtual-dtor -Werror=enum-compare -Werror=reorder -Werror=switch -Werror=return-type -Werror=non-virtual-dtor -Werror=string-plus-int -Werror=return-stack-address -Werror=implicit-fallthrough -D_LIBCPP_ENABLE_THREAD_SAFETY_ANNOTATIONS -Wthread-safety-analysis -DOS_MACOSX -D_GLIBCXX_USE_CXX11_ABI=0 -DYB_HEADERS_NO_STUBS=1 -ggdb -O1 -fno-omit-frame-pointer -Wno-ambiguous-member-template -Wimplicit-fallthrough -Qunused-arguments -fPIC -DYB_EDITION_NS_PREFIX=enterprise:: -DTCMALLOC_ENABLED -isysroot /Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX10.13.sdk -fPIC   -std=gnu++14 -MD -MT src/yb/rpc/CMakeFiles/yrpc.dir/messenger.cc.o -MF src/yb/rpc/CMakeFiles/yrpc.dir/messenger.cc.o.d -o src/yb/rpc/CMakeFiles/yrpc.dir/messenger.cc.o -c ../../src/yb/rpc/messenger.cc

src/yb/rpc/messenger.cc
@@ -355,7 +355,7 @@ bool Messenger::TEST_ShouldArtificiallyRejectOutgoingCallsTo(const IpAddress &re
 }
 
 Status Messenger::TEST_GetReactorMetrics(size_t reactor_idx, ReactorMetrics* metrics) {
-  if (reactor_idx < 0 || reactor_idx >= reactors_.size()) {
+  if (reactor_idx >= reactors_.size()) {
     return STATUS_FORMAT(
         InvalidArgument, "Invalid reactor index $0, should be >=0 and <$1", reactor_idx,
         reactors_.size());