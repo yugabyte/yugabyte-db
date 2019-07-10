25a6626e82c3e0a72fcad83905ab07eb7405b926
Karthik Ranganathan <rkarthik007@users.noreply.github.com>
2019-07-10 11:24:01 -0700

    Adding POLYFORM licenses to various files for the managed platform version
    Summary: This diff adds the POLYFORM licenses to files.
    
    Test Plan:
    Compiled successfully locally, this only adds comments into various files. Not explicitly tested.
    
    ```
    $ sbt compile
    ...
    [success] Total time: 43 s, completed Jul 10, 2019 11:28:21 AM
    ```
    
    Reviewers: mikhail, bogdan, sid, arnav
    
    Reviewed By: arnav
    
    Subscribers: jenkins-bot, ybase
    
    Differential Revision: https://phabricator.dev.yugabyte.com/D6876

--- managed/src/main/java/com/yugabyte/yw/commissioner/tasks/CloudCleanup.java
+++ managed/src/main/java/com/yugabyte/yw/commissioner/tasks/CloudCleanup.java
@@ -1,4 +1,13 @@
-// Copyright (c) YugaByte, Inc.
+/*
+ * Copyright 2019 YugaByte, Inc. and Contributors
+ *
+ * Licensed under the Polyform Free Trial License 1.0.0 (the "License"); you
+ * may not use this file except in compliance with the License. You
+ * may obtain a copy of the License at
+ *
+ *     https://github.com/YugaByte/yugabyte-db/blob/master/licenses/POLYFORM-FREE-TRIAL-LICENSE-1.0.0.txt
+ */
+
 package com.yugabyte.yw.commissioner.tasks;
 
 import com.yugabyte.yw.commissioner.SubTaskGroup;
--- managed/src/main/java/com/yugabyte/yw/commissioner/tasks/subtasks/cloud/CloudAccessKeyCleanup.java
+++ managed/src/main/java/com/yugabyte/yw/commissioner/tasks/subtasks/cloud/CloudAccessKeyCleanup.java
@@ -1,4 +1,12 @@
-// Copyright (c) YugaByte, Inc.
+/*
+ * Copyright 2019 YugaByte, Inc. and Contributors
+ *
+ * Licensed under the Polyform Free Trial License 1.0.0 (the "License"); you
+ * may not use this file except in compliance with the License. You
+ * may obtain a copy of the License at
+ *
+ *     https://github.com/YugaByte/yugabyte-db/blob/master/licenses/POLYFORM-FREE-TRIAL-LICENSE-1.0.0.txt
+ */
 
 package com.yugabyte.yw.commissioner.tasks.subtasks.cloud;
 
