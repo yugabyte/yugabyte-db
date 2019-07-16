yugabyte/yugabyte-db

commit 76c3f3cdbbe403b2f1183ffdd58cc46cfca7042a

ENG-2117: YW & UI portion of show current spot price market rate for given AZ
Summary:
As in the title, this is the YW & UI portion of the work to suggest a spot price to use. This implementation finds the max spot price for all AZs in a given region and returns 2 * that max spot price as the suggested spot price to use.

Related Devops work: https://phabricator.dev.yugabyte.com/D2927

Test Plan:
- sbt test
- go to CreateUniverse form and see that a spot price is suggested to the user:
{F5514}
{F5303}

Reviewers: bogdan, pritam

Reviewed By: pritam

Subscribers: ram, jenkins-bot, ui, yugaware

Differential Revision: https://phabricator.dev.yugabyte.com/D3088

managed/ui/src/components/common/forms/fields/YBTextInput.js
@@ -13,7 +13,7 @@ export class YBTextInput extends Component {
 
   render() {
     const self = this;
-    const { input, type, className, placeHolder, onValueChanged, isReadOnly, normalizeOnBlur } = this.props;
+    const { input, type, className, placeHolder, onValueChanged, isReadOnly, normalizeOnBlur, initValue } = this.props;
 
     function onChange(event) {
       if (isFunction(onValueChanged)) {
@@ -31,7 +31,7 @@ export class YBTextInput extends Component {
     }
 
     return (
-      <FormControl {...input} placeholder={placeHolder} type={type} className={className}
+      <FormControl {...input} placeholder={placeHolder} type={type} className={className} value={initValue}
         onChange={onChange} readOnly={isReadOnly} onBlur={onBlur} />
     );
   }

managed/ui/src/components/universes/UniverseForm/UniverseForm.js
@@ -34,7 +34,8 @@ const initialState = {
   accessKeyCode: 'yugabyte-default',
   maxNumNodes: -1, // Maximum Number of nodes currently in use OnPrem case
   useSpotPrice: IN_DEVELOPMENT_MODE,
-  spotPrice: '0.0'
+  spotPrice: '0.0',
+  gettingSuggestedSpotPrice: false
 };
 
 class UniverseForm extends Component {
@@ -69,6 +70,7 @@ class UniverseForm extends Component {
     this.getFormPayload = this.getFormPayload.bind(this);
     this.toggleSpotPrice = this.toggleSpotPrice.bind(this);
     this.spotPriceChanged = this.spotPriceChanged.bind(this);
+    this.getSuggestedSpotPrice = this.getSuggestedSpotPrice.bind(this);
     this.state = initialState;
   }
 
@@ -259,14 +261,29 @@ class UniverseForm extends Component {
     }
   }
 
+  getSuggestedSpotPrice(instanceType, regions) {
+    const currentProvider = this.getCurrentProvider(this.state.providerSelected);
+    const regionUUIDs = regions.map(region => region.value);
+    if (isDefinedNotNull(currentProvider) && currentProvider.code === "aws" && isNonEmptyArray(regionUUIDs)) {
+      this.props.getSuggestedSpotPrice(this.state.providerSelected, instanceType, regionUUIDs);
+      this.setState({gettingSuggestedSpotPrice: true});
+    }
+  }
+
   regionListChanged(value) {
     this.setState({nodeSetViaAZList: false, regionList: value});
+    if (this.state.useSpotPrice) {
+      this.getSuggestedSpotPrice(this.state.instanceTypeSelected, value);
+    }
   }
 
   instanceTypeChanged(event) {
     const instanceTypeValue = event.target.value;
     this.setState({instanceTypeSelected: instanceTypeValue});
     this.setDeviceInfo(instanceTypeValue, this.props.cloud.instanceTypes.data);
+    if (this.state.useSpotPrice) {
+      this.getSuggestedSpotPrice(instanceTypeValue, this.state.regionList);
+    }
   }
 
   numNodesChanged(value) {
@@ -399,12 +416,10 @@ class UniverseForm extends Component {
   }
 
   toggleSpotPrice(event) {
-    const {formValues} = this.props;
-    const nextState = {
-      useSpotPrice: event.target.checked,
-      spotPrice: event.target.checked ? formValues.spotPrice : '0.00'
-    };
-    this.setState(nextState);
+    this.setState({useSpotPrice: event.target.checked});
+    if (event.target.checked) {
+      this.getSuggestedSpotPrice(this.state.instanceTypeSelected, this.state.regionList);
+    }
   }
 
   spotPriceChanged(val) {
@@ -412,7 +427,10 @@ class UniverseForm extends Component {
   }
 
   componentWillReceiveProps(nextProps) {
-    const {universe: {showModal, visibleModal, currentUniverse}, cloud: {nodeInstanceList, instanceTypes}} = nextProps;
+    const {
+      universe: {showModal, visibleModal, currentUniverse},
+      cloud: {nodeInstanceList, instanceTypes, suggestedSpotPrice}
+    } = nextProps;
 
     if (nextProps.cloud.instanceTypes.data !== this.props.cloud.instanceTypes.data
       && isNonEmptyArray(nextProps.cloud.instanceTypes.data) && this.state.providerSelected
@@ -438,6 +456,16 @@ class UniverseForm extends Component {
       && isNonEmptyArray(nextProps.softwareVersions)
       && !isNonEmptyArray(this.props.softwareVersions)) {
       this.setState({ybSoftwareVersion: nextProps.softwareVersions[0]});
+    }
+
+    // Set spot price
+    const currentPromiseState = getPromiseState(this.props.cloud.suggestedSpotPrice);
+    const nextPromiseState = getPromiseState(suggestedSpotPrice);
+    if ((currentPromiseState.isInit() || currentPromiseState.isLoading()) && nextPromiseState.isSuccess()) {
+      this.setState({
+        spotPrice: normalizeToPositiveFloat(suggestedSpotPrice.data.toString()),
+        gettingSuggestedSpotPrice: false
+      });
     }
 
     // If dialog has been closed and opened again in-case of edit, then repopulate current config
@@ -655,7 +683,27 @@ class UniverseForm extends Component {
     let spotPriceToggle = <span />;
     let spotPriceField = <span />;
     const currentProvider = this.getCurrentProvider(this.state.providerSelected);
-    if (isDefinedNotNull(currentProvider) && currentProvider.code === "aws") {
+    if (isDefinedNotNull(currentProvider) && currentProvider.code === "aws"
+        && isDefinedNotNull(self.props.universe.currentPlacementStatus)) {
+      if (this.state.gettingSuggestedSpotPrice) {
+        spotPriceField = (
+          <div className="form-group">
+            <label className="form-item-label">Spot Price (Per Hour)</label>
+            <div className="extra-info-field text-center">Loading suggested spot price...</div>
+          </div>
+        );
+      } else if (!this.state.gettingSuggestedSpotPrice && this.state.useSpotPrice) {
+        spotPriceField = (
+          <Field name="spotPrice" type="text"
+                 component={YBTextInputWithLabel}
+                 label="Spot Price (Per Hour)"
+                 isReadOnly={isFieldReadOnly || !this.state.useSpotPrice}
+                 placeHolder="0.00"
+                 initValue={this.state.spotPrice.toString()}
+                 normalizeOnBlur={normalizeToPositiveFloat}
+                 onValueChanged={this.spotPriceChanged}/>
+        );
+      }
       spotPriceToggle = (
         <Field name="useSpotPrice"
                component={YBToggle}
@@ -663,19 +711,8 @@ class UniverseForm extends Component {
                subLabel="spot pricing is suitable for test environments only, because spot instances might go away any time"
                onToggle={this.toggleSpotPrice}
                defaultChecked={this.state.useSpotPrice}
-               isReadOnly={isFieldReadOnly} />
+               isReadOnly={isFieldReadOnly || this.state.gettingSuggestedSpotPrice}/>
       );
-      if (this.state.useSpotPrice) {
-        spotPriceField = (
-          <Field name="spotPrice" type="text"
-                 component={YBTextInputWithLabel}
-                 label="Spot Price (Per Hour)"
-                 isReadOnly={isFieldReadOnly || !this.state.useSpotPrice}
-                 placeHolder="0.00"
-                 normalizeOnBlur={normalizeToPositiveFloat}
-                 onValueChanged={this.spotPriceChanged} />
-        );
-      }
     }
 
     return (

managed/src/main/java/com/yugabyte/yw/common/CloudQueryHelper.java
@@ -3,7 +3,9 @@
 package com.yugabyte.yw.common;
 
 import com.fasterxml.jackson.databind.JsonNode;
+import com.google.common.collect.ImmutableList;
 import com.yugabyte.yw.commissioner.Common;
+import com.yugabyte.yw.models.AvailabilityZone;
 import com.yugabyte.yw.models.Region;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -66,5 +68,28 @@ public class CloudQueryHelper extends DevopsBase {
   	commandArgs.add("--regions");
   	regionList.forEach(region -> commandArgs.add(region.code));
   	return execCommand(regionList.get(0).uuid, "instance_types", commandArgs);
+  }
+
+  /**
+   * Get a suggested spot price for a given list of regions. Will find the max spot price amongst all the regions and
+   * return a suggested spot price of double the max spot price found.
+   *
+   * @param regions
+   * @param instanceType
+   * @return
+   */
+  public double getSuggestedSpotPrice(List<Region> regions, String instanceType) {
+    String command = "spot-pricing";
+    double maxPriceFound = 0.0;
+    for (Region region : regions) {
+      for (AvailabilityZone availabilityZone : AvailabilityZone.getAZsForRegion(region.uuid)) {
+        List<String> cloudArgs = ImmutableList.of("--zone", availabilityZone.code);
+        List<String> commandArgs = ImmutableList.of("--instance_type", instanceType);
+        JsonNode result = parseShellResponse(execCommand(region.uuid, command, commandArgs, cloudArgs), command);
+        double price = result.get("SpotPrice").asDouble();
+        if (price > maxPriceFound) maxPriceFound = price;
+      }
+    }
+    return 2.0 * maxPriceFound;
   }
 }

managed/src/main/java/com/yugabyte/yw/controllers/InstanceTypeController.java
@@ -2,11 +2,16 @@
 
 package com.yugabyte.yw.controllers;
 
+import java.util.HashMap;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.UUID;
 
+import com.fasterxml.jackson.databind.JsonNode;
 import com.yugabyte.yw.cloud.PublicCloudConstants;
-import com.yugabyte.yw.models.helpers.DeviceInfo;
+import com.yugabyte.yw.common.CloudQueryHelper;
+import com.yugabyte.yw.models.Customer;
+import com.yugabyte.yw.models.Region;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -21,12 +26,18 @@ import play.data.FormFactory;
 import play.libs.Json;
 import play.mvc.Result;
 
+import static com.yugabyte.yw.commissioner.Common.CloudType.aws;
 import static com.yugabyte.yw.commissioner.Common.CloudType.onprem;
 
 public class InstanceTypeController extends AuthenticatedController {
+
+  public static final Logger LOG = LoggerFactory.getLogger(InstanceTypeController.class);
+
   @Inject
   FormFactory formFactory;
-  public static final Logger LOG = LoggerFactory.getLogger(InstanceTypeController.class);
+
+  @Inject
+  CloudQueryHelper cloudQueryHelper;
 
   /**
    * GET endpoint for listing instance types
@@ -145,5 +156,47 @@ public class InstanceTypeController extends AuthenticatedController {
    */
   public Result getEBSTypes() {
     return ok(Json.toJson(PublicCloudConstants.EBSType.values()));
+  }
+
+  /**
+   * Info endpoint to get a suggested spot price for a given instanceType. Only works for AWS.
+   *
+   * @param customerUUID UUID of customer.
+   * @param providerUUID UUID of AWS provider.
+   * @param instanceTypeCode Instance type code.
+   * @return JSON response with suggested spot price (2x current max spot price).
+   */
+  public Result suggestSpotPrice(UUID customerUUID, UUID providerUUID, String instanceTypeCode) {
+    Customer customer = Customer.get(customerUUID);
+    if (customer == null) {
+      return ApiResponse.error(BAD_REQUEST, "Invalid Customer UUID: " + customerUUID);
+    }
+    Provider provider = Provider.get(customerUUID, providerUUID);
+    if (provider == null || !provider.code.equals(aws.toString())) {
+      return ApiResponse.error(BAD_REQUEST, "Invalid Provider UUID: " + providerUUID);
+    }
+    InstanceType instanceType = InstanceType.get(provider.code, instanceTypeCode);
+    if (instanceType == null) {
+      return ApiResponse.error(BAD_REQUEST, "Instance Type not found: " + instanceTypeCode);
+    }
+    try {
+      List<Region> regions = new LinkedList<>();
+      ObjectNode body = (ObjectNode)request().body().asJson();
+      LOG.info(Json.stringify(body));
+      for (JsonNode regionJson : body.get("regions")) {
+        String regionUUID = regionJson.asText();
+        Region region = Region.get(UUID.fromString(regionUUID));
+        if (region == null) {
+          return ApiResponse.error(BAD_REQUEST, "Region not found: " + regionUUID);
+        }
+        regions.add(region);
+      }
+      LOG.info(regions.toString());
+      return ApiResponse.success(cloudQueryHelper.getSuggestedSpotPrice(regions, instanceTypeCode));
+    } catch (Exception e) {
+      String errMsg = "Unable to get spot price for instance type: " + instanceTypeCode;
+      LOG.error("{}: {}", errMsg, e.getMessage());
+      return ApiResponse.error(INTERNAL_SERVER_ERROR, errMsg);
+    }
   }
 }

managed/src/main/resources/routes
@@ -34,6 +34,7 @@ DELETE  /api/customers/:cUUID/providers/:pUUID/regions/:rUUID/zones/:azUUID  com
 GET     /api/customers/:cUUID/providers/:pUUID/instance_types/:code    com.yugabyte.yw.controllers.InstanceTypeController.index(cUUID: java.util.UUID, pUUID: java.util.UUID, code: java.lang.String)
 GET     /api/customers/:cUUID/providers/:pUUID/instance_types          com.yugabyte.yw.controllers.InstanceTypeController.list(cUUID: java.util.UUID, pUUID: java.util.UUID)
 POST    /api/customers/:cUUID/providers/:pUUID/instance_types          com.yugabyte.yw.controllers.InstanceTypeController.create(cUUID: java.util.UUID, pUUID: java.util.UUID)
+POST    /api/customers/:cUUID/providers/:pUUID/instance_types/:code/spot_price      com.yugabyte.yw.controllers.InstanceTypeController.suggestSpotPrice(cUUID: java.util.UUID, pUUID: java.util.UUID, code: java.lang.String)
 DELETE  /api/customers/:cUUID/providers/:pUUID/instance_types/:code    com.yugabyte.yw.controllers.InstanceTypeController.delete(cUUID: java.util.UUID, pUUID: java.util.UUID, code: java.lang.String)
 POST    /api/customers/:cUUID/providers/setup_docker        com.yugabyte.yw.controllers.CloudProviderController.setupDocker(cUUID: java.util.UUID)
 

managed/src/test/java/com/yugabyte/yw/controllers/InstanceTypeControllerTest.java
@@ -6,7 +6,6 @@ package com.yugabyte.yw.controllers;
 import static com.yugabyte.yw.common.AssertHelper.assertErrorNodeValue;
 import static com.yugabyte.yw.common.AssertHelper.assertValue;
 import static com.yugabyte.yw.common.AssertHelper.assertValues;
-import static com.yugabyte.yw.controllers.TablesControllerTest.LOG;
 import static org.hamcrest.CoreMatchers.allOf;
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.core.IsNull.notNullValue;
@@ -15,17 +14,28 @@ import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertThat;
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.assertFalse;
+import static org.mockito.Matchers.anyList;
+import static org.mockito.Matchers.anyString;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+import static play.inject.Bindings.bind;
 import static play.mvc.Http.Status.BAD_REQUEST;
 import static play.mvc.Http.Status.OK;
 import static play.test.Helpers.contentAsString;
 
+import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 import com.google.common.collect.ImmutableList;
 import com.yugabyte.yw.commissioner.Common;
+import com.yugabyte.yw.common.CloudQueryHelper;
 import com.yugabyte.yw.common.ModelFactory;
 import com.yugabyte.yw.models.Customer;
+import com.yugabyte.yw.models.Region;
+import org.hamcrest.CoreMatchers;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -38,13 +48,27 @@ import com.yugabyte.yw.models.InstanceType.VolumeDetails;
 import com.yugabyte.yw.models.InstanceType.InstanceTypeDetails;
 import com.yugabyte.yw.models.Provider;
 
+import play.Application;
+import play.inject.guice.GuiceApplicationBuilder;
 import play.libs.Json;
 import play.mvc.Result;
+import play.test.Helpers;
 
 public class InstanceTypeControllerTest extends FakeDBApplication {
   Customer customer;
   Provider awsProvider;
   Provider onPremProvider;
+  CloudQueryHelper mockCloudQueryHelper;
+
+  @Override
+  protected Application provideApplication() {
+    mockCloudQueryHelper = mock(CloudQueryHelper.class);
+
+    return new GuiceApplicationBuilder()
+        .configure((Map) Helpers.inMemoryDatabase())
+        .overrides(bind(CloudQueryHelper.class).toInstance(mockCloudQueryHelper))
+        .build();
+  }
 
   @Before
   public void setUp() {
@@ -84,6 +108,21 @@ public class InstanceTypeControllerTest extends FakeDBApplication {
     return Json.parse(contentAsString(result));
   }
 
+  private InstanceType[] setUpValidInstanceTypes(int numInstanceTypes) {
+    InstanceType[] instanceTypes = new InstanceType[numInstanceTypes];
+    for (int i = 0; i < numInstanceTypes; ++i) {
+      InstanceType.VolumeDetails volDetails = new InstanceType.VolumeDetails();
+      volDetails.volumeSizeGB = 100;
+      volDetails.volumeType = InstanceType.VolumeType.EBS;
+      InstanceTypeDetails instanceDetails = new InstanceTypeDetails();
+      instanceDetails.volumeDetailsList.add(volDetails);
+      instanceDetails.setDefaultMountPaths();
+      String code = "test-i" + Integer.toString(i);
+      instanceTypes[i] = InstanceType.upsert(awsProvider.code, code, 2, 10.5, instanceDetails);
+    }
+    return instanceTypes;
+  }
+
   @Test
   public void testListInstanceTypeWithInvalidProviderUUID() {
     UUID randomUUID = UUID.randomUUID();
@@ -99,25 +138,7 @@ public class InstanceTypeControllerTest extends FakeDBApplication {
 
   @Test
   public void testListInstanceTypeWithValidProviderUUID() {
-    InstanceType[] instanceTypes = new InstanceType[2];
-
-    // Add first InstanceType.
-    InstanceType.VolumeDetails i1VolDetails = new InstanceType.VolumeDetails();
-    i1VolDetails.volumeSizeGB = 100;
-    i1VolDetails.volumeType = InstanceType.VolumeType.EBS;
-    InstanceTypeDetails i1InstanceDetails = new InstanceTypeDetails();
-    i1InstanceDetails.volumeDetailsList.add(i1VolDetails);
-    i1InstanceDetails.setDefaultMountPaths();
-    instanceTypes[0] = InstanceType.upsert(awsProvider.code, "test-i1", 2, 10.5, i1InstanceDetails);
-
-    // Add second InstanceType.
-    InstanceType.VolumeDetails i2VolDetails = new InstanceType.VolumeDetails();
-    i2VolDetails.volumeSizeGB = 80;
-    i2VolDetails.volumeType = InstanceType.VolumeType.EBS;
-    InstanceTypeDetails i2InstanceDetails = new InstanceTypeDetails();
-    i2InstanceDetails.volumeDetailsList.add(i2VolDetails);
-    i2InstanceDetails.setDefaultMountPaths();
-    instanceTypes[1] = InstanceType.upsert(awsProvider.code, "test-i2", 3, 9.0, i2InstanceDetails);
+    InstanceType[] instanceTypes = setUpValidInstanceTypes(2);
 
     JsonNode json = doListInstanceTypesAndVerify(awsProvider.uuid, OK);
     assertEquals(2, json.size());
@@ -132,7 +153,6 @@ public class InstanceTypeControllerTest extends FakeDBApplication {
       InstanceTypeDetails itd = it.instanceTypeDetails;
       List<VolumeDetails> detailsList = itd.volumeDetailsList;
       VolumeDetails targetDetails = detailsList.get(0);
-      LOG.error(instance.toString());
       JsonNode itdNode = instance.get("instanceTypeDetails");
       JsonNode detailsListNode = itdNode.get("volumeDetailsList");
       JsonNode jsonDetails = detailsListNode.get(0);
@@ -140,7 +160,9 @@ public class InstanceTypeControllerTest extends FakeDBApplication {
       assertValue(jsonDetails, "volumeType", targetDetails.volumeType.toString());
       assertValue(jsonDetails, "mountPath", targetDetails.mountPath);
     }
-    List<String> expectedCodes = ImmutableList.of("test-i1", "test-i2");
+    List<String> expectedCodes = Arrays.asList(instanceTypes).stream()
+        .map(instanceType -> instanceType.idKey.instanceTypeCode)
+        .collect(Collectors.toList());
     assertValues(json, "instanceTypeCode", expectedCodes);
   }
 
@@ -279,5 +301,22 @@ public class InstanceTypeControllerTest extends FakeDBApplication {
     UUID randomUUID = UUID.randomUUID();
     JsonNode json = doDeleteInstanceTypeAndVerify(randomUUID, fakeInstanceCode, BAD_REQUEST);
     assertErrorNodeValue(json, "Invalid Provider UUID: " + randomUUID);
+  }
+
+  @Test
+  public void testGetSpotPrice() {
+    String regionCode = "test-region";
+    Region region = Region.create(awsProvider, regionCode, regionCode, "0.0.19.37");
+    String instanceTypeCode = setUpValidInstanceTypes(1)[0].idKey.instanceTypeCode;
+    when(mockCloudQueryHelper.getSuggestedSpotPrice(ImmutableList.of(region), instanceTypeCode)).thenReturn(0.5);
+
+    JsonNode body = Json.newObject().set("regions", Json.newArray().add(region.uuid.toString()));
+    String method = "POST";
+    String uri =  "/api/customers/" + customer.uuid + "/providers/" + awsProvider.uuid + "/instance_types/" +
+        instanceTypeCode + "/spot_price";
+    Result result = FakeApiHelper.doRequestWithBody(method, uri, body);
+
+    assertEquals(OK, result.status());
+    assertThat(Double.parseDouble(contentAsString(result)), equalTo(0.5));
   }
 }

managed/ui/src/actions/cloud.js
@@ -80,6 +80,9 @@ export const DELETE_NODE_RESPONSE = 'DELETE_NODE_RESPONSE';
 export const BOOTSTRAP_PROVIDER = 'BOOTSTRAP_PROVIDER';
 export const BOOTSTRAP_PROVIDER_RESPONSE = 'BOOTSTRAP_PROVIDER_RESPONSE';
 
+export const GET_SUGGESTED_SPOT_PRICE = 'GET_SUGGESTED_SPOT_PRICE';
+export const GET_SUGGESTED_SPOT_PRICE_RESPONSE = 'GET_SUGGESTED_SPOT_PRICE_RESPONSE';
+
 export function getProviderList() {
   const cUUID = localStorage.getItem("customer_id");
   const request = axios.get(`${ROOT_URL}/customers/${cUUID}/providers`);
@@ -124,6 +127,23 @@ export function getInstanceTypeList(providerUUID) {
 export function getInstanceTypeListResponse(responsePayload) {
   return {
     type: GET_INSTANCE_TYPE_LIST_RESPONSE,
+    payload: responsePayload
+  };
+}
+
+export function getSuggestedSpotPrice(providerUUID, instanceType, regions) {
+  const payload = {'regions': regions};
+  const url =`${getProviderEndpoint(providerUUID)}/instance_types/${instanceType}/spot_price`;
+  const request = axios.post(url, payload);
+  return {
+    type: GET_SUGGESTED_SPOT_PRICE,
+    payload: request
+  };
+}
+
+export function getSuggestedSpotPriceResponse(responsePayload) {
+  return {
+    type: GET_SUGGESTED_SPOT_PRICE_RESPONSE,
     payload: responsePayload
   };
 }

managed/ui/src/components/universes/UniverseForm/UniverseForm.scss
@@ -120,6 +120,11 @@
   }
 }
 
+.extra-info-field {
+  display: inline-block;
+  margin: 8px;
+}
+
 .universe-form-container {
   position: relative;
   background-color: white;

commit 553f3c5220d0362028d48460d4f917b7319a3ef7

Fix bug with initValue in YBTextInput
Summary:
Bug introduced by D3088 ended up causing text inputs with a default value not being rendered in many cases. This fixes that bug which retaining the ability to show dynamically generated initial suggested value for spot price

Bug:
{F5522}

Test Plan:
Try to create a spotprice-based universe, validate all init values show up and basic spot price suggestion logic remains unaffected.
{F5523}

Reviewers: pritam

Reviewed By: pritam

Subscribers: jenkins-bot, ui

Differential Revision: https://phabricator.dev.yugabyte.com/D3188

managed/ui/src/components/common/forms/fields/YBTextInput.js
@@ -4,6 +4,7 @@ import React, { Component } from 'react';
 import { FormControl } from 'react-bootstrap';
 import { isFunction } from 'lodash';
 import { YBLabel } from 'components/common/descriptors';
+import { isDefinedNotNull } from "../../../../utils/ObjectUtils";
 
 // TODO: Make default export after checking all corresponding imports.
 export class YBTextInput extends Component {
@@ -30,9 +31,13 @@ export class YBTextInput extends Component {
       }
     }
 
+    if (isDefinedNotNull(initValue)) {
+      input["value"] = initValue;
+    }
+
     return (
-      <FormControl {...input} placeholder={placeHolder} type={type} className={className} value={initValue}
-        onChange={onChange} readOnly={isReadOnly} onBlur={onBlur} />
+      <FormControl {...input} placeholder={placeHolder} type={type} className={className} onChange={onChange}
+                   readOnly={isReadOnly} onBlur={onBlur} />
     );
   }
 }

managed/ui/src/components/universes/UniverseForm/UniverseForm.js
@@ -698,9 +698,8 @@ class UniverseForm extends Component {
                  component={YBTextInputWithLabel}
                  label="Spot Price (Per Hour)"
                  isReadOnly={isFieldReadOnly || !this.state.useSpotPrice}
-                 placeHolder="0.00"
-                 initValue={this.state.spotPrice.toString()}
                  normalizeOnBlur={normalizeToPositiveFloat}
+                 initValue={this.state.spotPrice.toString()}
                  onValueChanged={this.spotPriceChanged}/>
         );
       }