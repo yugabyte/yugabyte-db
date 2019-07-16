yugabyte/yugabyte-db

commit 74e80257e0c0a7e0521d7853b95bacce3d328555

Fix UI bug after initial login attempt
Summary: Login fails after first attempt and works on second click. This issue is fixed here.

Test Plan: Start UI, logout, login, verify login works on one submit.

Reviewers: pritam, ram

Reviewed By: ram

Subscribers: yugaware, ui

Differential Revision: https://phabricator.dev.yugabyte.com/D1456

managed/ui/src/components/universes/UniverseForm/UniverseForm.js
@@ -152,11 +152,14 @@ export default class UniverseForm extends Component {
   render() {
     var self = this;
     const { visible, onHide, handleSubmit, title, universe } = this.props;
-    var universeProviderList = this.props.cloud.providers.map(function(providerItem, idx) {
-      return <option key={providerItem.uuid} value={providerItem.uuid}>
-        {providerItem.name}
-      </option>;
-    });
+    var universeProviderList = [];
+    if (isValidArray(this.props.cloud.providers)) {
+      this.props.cloud.providers.map(function(providerItem, idx) {
+        return <option key={providerItem.uuid} value={providerItem.uuid}>
+          {providerItem.name}
+        </option>;
+      });
+    }
     universeProviderList.unshift(<option key="" value=""></option>);
 
     var universeRegionList = this.props.cloud.regions.map(function (regionItem, idx) {

commit 5ffe7ddb4670160048ce3e14bff46bfd5ce6d682

Fix bug caused by D1456
Summary: Provider list is not assigned to an array so doesn't populate the provider option list.

Test Plan:
Go to create universe dialog.
Provider list should be populated

Reviewers: ram, karthik, bogdan

Reviewed By: bogdan

Subscribers: yugaware

Differential Revision: https://phabricator.dev.yugabyte.com/D1463

managed/ui/src/components/universes/UniverseForm/UniverseForm.js
@@ -163,7 +163,7 @@ export default class UniverseForm extends Component {
     const { visible, onHide, handleSubmit, title, universe } = this.props;
     var universeProviderList = [];
     if (isValidArray(this.props.cloud.providers)) {
-      this.props.cloud.providers.map(function(providerItem, idx) {
+      universeProviderList = this.props.cloud.providers.map(function(providerItem, idx) {
         return <option key={providerItem.uuid} value={providerItem.uuid}>
           {providerItem.name}
         </option>;