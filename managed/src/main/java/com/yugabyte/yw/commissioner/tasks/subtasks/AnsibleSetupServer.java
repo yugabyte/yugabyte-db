yugabyte/yugabyte-db

commit 88f9014ad73ff986f5107b3e46fc7da1a10d551f

ENG-2186: Allow non-passwordless sudo from YW & UI
Summary:
With Pritam D's work, we should now support onprem providers without passwordless sudo access. This diff supports that.
- Added endpoint to check if provider supports passwordless sudo access
- update tests
- Add toggle to onprem provider configure for passwordless sudo access
- When adding node instances to a provider, show command they should execute
- Create `provision_instance.py` script when creating non-passwordless sudo provider
- Delete previously created `provision_instance.py` script when deleting non-passwordless sudo provider

Test Plan:
- sbt test
- try to create public provider & universe & confirm still works as normal
- try to create onprem provider with passwordless sudo access & universe & confirm still works as normal
- try to create onprem provider without passwordless sudo access & universe & confirm provision step is skipped, but succeeds if suggested command is executed
-- try to delete & confirm the `provision_instance.py` script & containing directory is deleted
{F5471}
{F5510}
{F5472}

Reviewers: bogdan, karthik, pritam, ram

Reviewed By: ram

Subscribers: mikhail, jenkins-bot, pritam.damania, ui, yugaware

Differential Revision: https://phabricator.dev.yugabyte.com/D3146

managed/src/main/java/com/yugabyte/yw/commissioner/tasks/subtasks/AnsibleSetupServer.java
@@ -4,10 +4,14 @@ package com.yugabyte.yw.commissioner.tasks.subtasks;
 
 import com.yugabyte.yw.common.NodeManager;
 import com.yugabyte.yw.common.ShellProcessHandler;
+import com.yugabyte.yw.models.AccessKey;
+import com.yugabyte.yw.models.Provider;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import com.yugabyte.yw.commissioner.tasks.params.NodeTaskParams;
+
+import java.util.List;
 
 
 public class AnsibleSetupServer extends NodeTaskBase {
@@ -29,9 +33,16 @@ public class AnsibleSetupServer extends NodeTaskBase {
 
   @Override
   public void run() {
-    // Execute the ansible command.
-    ShellProcessHandler.ShellResponse response = getNodeManager().nodeCommand(
-        NodeManager.NodeCommandType.Provision, taskParams());
-    logShellResponse(response);
+    Provider p = taskParams().getProvider();
+    List<AccessKey> accessKeys = AccessKey.getAll(p.uuid);
+    // Note: For now, we assume there will only be one AccessKey.
+    if (accessKeys.size() > 0 && accessKeys.get(0).getKeyInfo().provisionInstanceScript.length() > 0) {
+      // Execute the ansible command.
+      ShellProcessHandler.ShellResponse response = getNodeManager().nodeCommand(
+          NodeManager.NodeCommandType.Provision, taskParams());
+      logShellResponse(response);
+    } else {
+      LOG.info("Skipping ansible provision because provider " + p.code + " does not support passwordless sudo access.");
+    }
   }
 }

managed/build.sbt
@@ -17,6 +17,7 @@ libraryDependencies ++= Seq(
   "org.mindrot" % "jbcrypt" % "0.3m",
   "mysql" % "mysql-connector-java" % "5.1.27",
   "org.postgresql" % "postgresql" % "9.2-1003-jdbc4",
+  "commons-io" % "commons-io" % "2.4",
   "org.apache.httpcomponents" % "httpcore" % "4.4.5",
   "org.apache.httpcomponents" % "httpclient" % "4.5.2",
   "org.flywaydb" %% "flyway-play" % "3.0.1",

managed/src/main/java/Module.java
@@ -3,12 +3,13 @@
 import com.google.inject.AbstractModule;
 import com.yugabyte.yw.cloud.AWSInitializer;
 import com.yugabyte.yw.common.AccessManager;
+import com.yugabyte.yw.common.ConfigHelper;
 import com.yugabyte.yw.common.NetworkManager;
 import com.yugabyte.yw.common.NodeManager;
 import com.yugabyte.yw.common.ReleaseManager;
 import com.yugabyte.yw.common.ShellProcessHandler;
-import com.yugabyte.yw.common.ConfigHelper;
 import com.yugabyte.yw.common.SwamperHelper;
+import com.yugabyte.yw.common.TemplateManager;
 import com.yugabyte.yw.metrics.MetricQueryHelper;
 import com.yugabyte.yw.common.services.LocalYBClientService;
 import com.yugabyte.yw.common.services.YBClientService;
@@ -34,6 +35,7 @@ public class Module extends AbstractModule {
     bind(NetworkManager.class).asEagerSingleton();
     bind(AccessManager.class).asEagerSingleton();
     bind(ReleaseManager.class).asEagerSingleton();
+    bind(TemplateManager.class).asEagerSingleton();
     bind(AWSInitializer.class).asEagerSingleton();
   }
 }

managed/src/main/java/com/yugabyte/yw/common/AccessManager.java
@@ -6,7 +6,6 @@ import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.inject.Inject;
 import com.yugabyte.yw.models.AccessKey;
-import com.yugabyte.yw.models.Provider;
 import com.yugabyte.yw.models.Region;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -22,7 +21,6 @@ import java.nio.file.attribute.PosixFilePermissions;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
-import java.util.Map;
 import java.util.Set;
 import java.util.UUID;
 
@@ -138,7 +136,7 @@ public class AccessManager extends DevopsBase {
       commandArgs.add(accessKey.getKeyInfo().privateKey);
     }
 
-    JsonNode response = execCommand(regionUUID, "add-key", commandArgs);
+    JsonNode response = execAndParseCommand(regionUUID, "add-key", commandArgs);
     if (response.has("error")) {
       throw new RuntimeException(response.get("error").asText());
     }
@@ -166,11 +164,11 @@ public class AccessManager extends DevopsBase {
     }
     commandArgs.add("--private_key_file");
     commandArgs.add(privateKeyFile);
-    return execCommand(regionUUID, "create-vault", commandArgs);
+    return execAndParseCommand(regionUUID, "create-vault", commandArgs);
   }
 
   public JsonNode listKeys(UUID regionUUID) {
-    return execCommand(regionUUID, "list-keys", Collections.emptyList());
+    return execAndParseCommand(regionUUID, "list-keys", Collections.emptyList());
   }
 
   public JsonNode deleteKey(UUID regionUUID, String keyCode) {
@@ -185,7 +183,7 @@ public class AccessManager extends DevopsBase {
     commandArgs.add(keyCode);
     commandArgs.add("--key_file_path");
     commandArgs.add(keyFilePath);
-    JsonNode response = execCommand(regionUUID, "delete-key", commandArgs);
+    JsonNode response = execAndParseCommand(regionUUID, "delete-key", commandArgs);
     if (response.has("error")) {
       throw new RuntimeException(response.get("error").asText());
     }

managed/src/main/java/com/yugabyte/yw/common/CloudQueryHelper.java
@@ -27,14 +27,14 @@ public class CloudQueryHelper extends DevopsBase {
     List<String> commandArgs = new ArrayList<String>();
     commandArgs.add("--metadata_types");
     commandArgs.addAll(metadataTypes);
-    return execCommand(cloudType, "current-host", commandArgs);
+    return execAndParseCommand(cloudType, "current-host", commandArgs);
   }
 
   public JsonNode getZones(Region region) {
   	List<String> commandArgs = new ArrayList<String>();
   	commandArgs.add("--regions");
   	commandArgs.add(region.code);
-    return execCommand(region.uuid, "zones", commandArgs);
+    return execAndParseCommand(region.uuid, "zones", commandArgs);
   }
 
   /*
@@ -67,7 +67,7 @@ public class CloudQueryHelper extends DevopsBase {
   	List<String> commandArgs = new ArrayList<String>();
   	commandArgs.add("--regions");
   	regionList.forEach(region -> commandArgs.add(region.code));
-  	return execCommand(regionList.get(0).uuid, "instance_types", commandArgs);
+  	return execAndParseCommand(regionList.get(0).uuid, "instance_types", commandArgs);
   }
 
   /**

managed/src/main/java/com/yugabyte/yw/common/DevopsBase.java
@@ -19,6 +19,7 @@ import java.util.UUID;
 
 public abstract class DevopsBase {
   public static final String YBCLOUD_SCRIPT = "bin/ybcloud.sh";
+  public static final String PY_WRAPPER = "bin/py_wrapper";
   public static final Logger LOG = LoggerFactory.getLogger(DevopsBase.class);
 
   // Command that we would need to execute eg: instance, network, access.
@@ -31,29 +32,33 @@ public abstract class DevopsBase {
   @Inject
   ShellProcessHandler shellProcessHandler;
 
-  protected JsonNode execCommand(Common.CloudType cloudType, String command,
-                                 List<String> commandArgs) {
+  protected JsonNode parseShellResponse(ShellProcessHandler.ShellResponse response, String command) {
+    if (response.code == 0) {
+      return Json.parse(response.message);
+    } else {
+      LOG.error(response.message);
+      return ApiResponse.errorJSON("YBCloud command " + getCommandType() + " (" + command + ") failed to execute.");
+    }
+  }
+
+  protected JsonNode execAndParseCommand(Common.CloudType cloudType, String command, List<String> commandArgs) {
     ShellProcessHandler.ShellResponse response = execCommand(new UUID(0L, 0L), command,
         commandArgs, cloudType, Collections.emptyList());
     // WARNING: Does not pass environment variables from config. 
     return parseShellResponse(response, command);
   }
 
-  protected JsonNode execCommand(UUID regionUUID, String command, List<String> commandArgs) {
+  protected JsonNode execAndParseCommand(UUID regionUUID, String command, List<String> commandArgs) {
     ShellProcessHandler.ShellResponse response = execCommand(regionUUID, command, commandArgs,
         null, Collections.emptyList());
     return parseShellResponse(response, command);
   }
 
-  protected JsonNode parseShellResponse(ShellProcessHandler.ShellResponse response,
-                                        String command) {
-    if (response.code == 0) {
-      return Json.parse(response.message);
-    } else {
-      LOG.error(response.message);
-      return ApiResponse.errorJSON("YBCloud command " + getCommandType() + " (" + command +
-          ") failed to execute.");
-    }
+  protected ShellProcessHandler.ShellResponse execCommand(UUID regionUUID,
+                                                          String command,
+                                                          List<String> commandArgs,
+                                                          List<String> cloudArgs) {
+    return execCommand(regionUUID, command, commandArgs, null, cloudArgs);
   }
 
   protected ShellProcessHandler.ShellResponse execCommand(UUID regionUUID,
@@ -84,19 +89,5 @@ public abstract class DevopsBase {
 
     LOG.info("Command to run: [" + String.join(" ", commandList) + "]");
     return shellProcessHandler.run(commandList, extraVars);
-  }
-
-  protected ShellProcessHandler.ShellResponse execCommand(UUID regionUUID,
-                                                          String command,
-                                                          List<String> commandArgs,
-                                                          List<String> cloudArgs) {
-    return execCommand(regionUUID, command, commandArgs, null, cloudArgs);
-  }
-
-  protected ShellProcessHandler.ShellResponse execCommand(Common.CloudType cloudType,
-                                                          String command,
-                                                          List<String> commandArgs,
-                                                          List<String> cloudArgs) {
-    return execCommand(new UUID(0L, 0L), command, commandArgs, cloudType, cloudArgs);
   }
 }

managed/src/main/java/com/yugabyte/yw/common/NetworkManager.java
@@ -32,15 +32,15 @@ public class NetworkManager extends DevopsBase {
       commandArgs.add(destVpcId);
     }
 
-    return execCommand(regionUUID, "bootstrap", commandArgs);
+    return execAndParseCommand(regionUUID, "bootstrap", commandArgs);
   }
 
   public JsonNode query(UUID regionUUID) {
-    return execCommand(regionUUID, "query", Collections.emptyList());
+    return execAndParseCommand(regionUUID, "query", Collections.emptyList());
   }
 
   public JsonNode cleanup(UUID regionUUID) {
-    JsonNode response = execCommand(regionUUID, "cleanup", Collections.emptyList());
+    JsonNode response = execAndParseCommand(regionUUID, "cleanup", Collections.emptyList());
     if (response.has("error")) {
       throw new RuntimeException(response.get("error").asText());
     }

managed/src/main/java/com/yugabyte/yw/common/TableManager.java
@@ -5,25 +5,19 @@ package com.yugabyte.yw.common;
 import com.google.inject.Inject;
 import com.google.inject.Singleton;
 import com.yugabyte.yw.forms.BulkImportParams;
-import com.yugabyte.yw.forms.TableDefinitionTaskParams;
 import com.yugabyte.yw.forms.UniverseDefinitionTaskParams;
 import com.yugabyte.yw.models.AccessKey;
-import com.yugabyte.yw.models.Customer;
-import com.yugabyte.yw.models.Provider;
 import com.yugabyte.yw.models.Region;
 import com.yugabyte.yw.models.Universe;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.UUID;
 
 @Singleton
 public class TableManager extends DevopsBase {
   private static final int EMR_MULTIPLE = 8;
   private static final String YB_CLOUD_COMMAND_TYPE = "table";
-  static final String PY_WRAPPER = "bin/py_wrapper";
   static final String BULK_LOAD_SCRIPT = "bin/yb_bulk_load.py";
 
   @Inject

commit 0f8de1a5bace07bb83148a7b6c60c086955aca6d

Fixes for bugs introduced by D3146 and D3228
Summary:
2 bugs fixed here:
1) Check in AnsibleSetupServer for sudoless provision access was inverted. We were running provision if there WAS a provision instance script, when we should be running provision only if there WASN'T a provision instance script.
2) Need to special case the spot price check in UniverseForm on sending a universe configure request to only be done for aws.

Test Plan:
- sbt test
- Try to create an aws universe
- Try to create an onprem universe with no sudoless provision access

Reviewers: bogdan, pritam

Reviewed By: bogdan, pritam

Subscribers: jenkins-bot, ui, yugaware

Differential Revision: https://phabricator.dev.yugabyte.com/D3244

managed/src/main/java/com/yugabyte/yw/commissioner/tasks/subtasks/AnsibleSetupServer.java
@@ -36,7 +36,8 @@ public class AnsibleSetupServer extends NodeTaskBase {
     Provider p = taskParams().getProvider();
     List<AccessKey> accessKeys = AccessKey.getAll(p.uuid);
     // Note: For now, we assume there will only be one AccessKey.
-    if (accessKeys.size() > 0 && accessKeys.get(0).getKeyInfo().provisionInstanceScript.length() > 0) {
+    // Also, should only execute Provision command if there is no externally accessible provision script.
+    if (accessKeys.size() > 0 && accessKeys.get(0).getKeyInfo().provisionInstanceScript.length() == 0) {
       // Execute the ansible command.
       ShellProcessHandler.ShellResponse response = getNodeManager().nodeCommand(
           NodeManager.NodeCommandType.Provision, taskParams());

managed/ui/src/components/universes/UniverseForm/UniverseForm.js
@@ -180,9 +180,9 @@ class UniverseForm extends Component {
 
   handleUniverseConfigure(universeTaskParams) {
     const {universe: {currentUniverse}} = this.props;
+    const checkSpotPrice = universeTaskParams.userIntent.providerType === 'aws' && !this.state.gettingSuggestedSpotPrice;
     if (isDefinedNotNull(this.state.instanceTypeSelected) && isNonEmptyArray(this.state.regionList) &&
-        (_.isEqual(this.state.spotPrice, normalizeToPositiveFloat(this.state.spotPrice)) ||
-        this.state.gettingSuggestedSpotPrice)) {
+        (!checkSpotPrice || _.isEqual(this.state.spotPrice, normalizeToPositiveFloat(this.state.spotPrice)))) {
       if (isNonEmptyObject(currentUniverse.data) &&
         areIntentsEqual(currentUniverse.data.universeDetails.userIntent, universeTaskParams.userIntent)) {
         this.props.getExistingUniverseConfiguration(currentUniverse.data.universeDetails);