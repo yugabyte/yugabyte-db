yugabyte/yugabyte-db

commit 46420a03e60d4177c2359a79f8800cdae6746a99

ENG-3186 Allow Edit Provider to update host zone Id if not present
Summary:
Ad in JIRA.
Created the PUT endpoint to allow update host zone id if not present.
Created UI form/action/reducer flow and necessary validations.
{F8947}

{F8948}

Interaction Screencast here

https://drive.google.com/file/d/1B_lZblwZC6Ysxk-ohZcV4v43BvGypWQh/view?usp=sharing

As discussed, provider host zone id can be updated only if not originally present.
Otherwise the form renders a read-only view.

Test Plan:
Create AWS provider without host zone id.
Once bootstrap finishes click on Edit.
Add a new host zone id.
You should be redirected to result view and see the new host zone id displayed on screen.

Click Edit again.
Form should render in read-only mode now.

Reviewers: bogdan, karthik, vit.pankin, ram

Reviewed By: ram

Subscribers: jenkins-bot, yugaware

Differential Revision: https://phabricator.dev.yugabyte.com/D4609

managed/src/main/java/com/yugabyte/yw/controllers/CloudProviderController.java
@@ -232,7 +232,7 @@ public class CloudProviderController extends AuthenticatedController {
         // instance creation. Technically, we could make it a ybcloud parameter, but we'd still need to
         // store it somewhere and the config is the easiest place to put it. As such, since all the
         // config is loaded up as env vars anyway, might as well use in in devops like that...
-        Map<String, String> config = provider.getConfig();
+        Map<String, String> config = provider.getUnmaskedConfig();
         config.put("CUSTOM_GCE_NETWORK", formData.get().destVpcId);
         provider.setConfig(config);
         provider.save();
@@ -296,5 +296,33 @@ public class CloudProviderController extends AuthenticatedController {
     ObjectNode resultNode = Json.newObject();
     resultNode.put("taskUUID", taskUUID.toString());
     return ApiResponse.success(resultNode);
+  }
+
+  public Result edit(UUID customerUUID, UUID providerUUID) {
+    Customer customer = Customer.get(customerUUID);
+    if (customer == null) {
+      ApiResponse.error(BAD_REQUEST, "Invalid Customer Context.");
+    }
+    JsonNode formData =  request().body().asJson();
+    String hostedZoneID = formData.get("hostedZoneId").asText();
+    Provider provider = Provider.get(customerUUID, providerUUID);
+    if (provider == null) {
+      return ApiResponse.error(BAD_REQUEST, "Invalid Provider UUID: " + providerUUID);
+    }
+    if (!provider.code.equals("aws")) {
+      return ApiResponse.error(BAD_REQUEST, "Expected aws found providers with code: " + provider.code);
+    }
+    if (hostedZoneID == null || hostedZoneID.length() == 0) {
+      return ApiResponse.error(BAD_REQUEST, "Required field hosted zone id");
+    }
+    try {
+      provider.updateHostedZoneId(hostedZoneID);
+    } catch (RuntimeException e) {
+      return ApiResponse.error(INTERNAL_SERVER_ERROR, e.getMessage());
+    }
+    ObjectNode resultNode = Json.newObject();
+    resultNode.put("hostedZoneID", hostedZoneID);
+    return ApiResponse.success(resultNode);
+
   }
 }

managed/src/main/java/com/yugabyte/yw/models/Provider.java
@@ -6,7 +6,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import java.util.UUID;
-
+import java.util.Iterator;
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
@@ -18,6 +18,8 @@ import javax.persistence.UniqueConstraint;
 import com.avaje.ebean.annotation.DbJson;
 import com.fasterxml.jackson.annotation.JsonBackReference;
 import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ObjectNode;
 import com.yugabyte.yw.commissioner.Common;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -68,8 +70,25 @@ public class Provider extends Model {
 
   public void setConfig(Map<String, String> configMap) { this.config = Json.toJson(configMap); }
 
-  @JsonBackReference(value="config")
   public Map<String, String> getConfig() {
+    if (this.config == null) {
+      return new HashMap();
+    } else {
+      JsonNode maskedData = this.config.deepCopy();
+      for (Iterator<String> it = maskedData.fieldNames(); it.hasNext(); ) {
+        String key = it.next();
+        if (key.contains("KEY") || key.contains("SECRET")) {
+          ((ObjectNode) maskedData).put(key, maskedData.get(key).asText().replaceAll("(?<!^.?).(?!.?$)", "*"));
+        }
+      }
+      ObjectMapper mapper = new ObjectMapper();
+      Map<String, String> result = mapper.convertValue(maskedData, Map.class);
+      return result;
+    }
+  }
+  
+  // Method returns unmasked config values for provider
+  public Map<String, String> getUnmaskedConfig() {
     if (this.config == null) {
       return new HashMap();
     } else {
@@ -152,5 +171,17 @@ public class Provider extends Model {
 
   public static Provider get(UUID providerUuid) {
     return find.byId(providerUuid);
+  }
+
+  // Update host zone if for aws provider
+  public void updateHostedZoneId(String hostedZoneId) {
+    try {
+      Map<String, String> currentProviderConfig = getUnmaskedConfig();
+      currentProviderConfig.put("AWS_HOSTED_ZONE_ID", hostedZoneId);
+      this.setConfig(currentProviderConfig);
+      this.save();
+    } catch (RuntimeException e) {
+      throw e;
+    }
   }
 }

managed/ui/src/actions/cloud.js
@@ -84,6 +84,9 @@ export const GET_SUGGESTED_SPOT_PRICE_RESPONSE = 'GET_SUGGESTED_SPOT_PRICE_RESPO
 
 export const RESET_SUGGESTED_SPOT_PRICE = 'RESET_SUGGESTED_SPOT_PRICE';
 
+export const EDIT_PROVIDER = 'EDIT_PROVIDER';
+export const EDIT_PROVIDER_RESPONSE = 'EDIT_PROVIDER_RESPONSE';
+
 export function getProviderList() {
   const cUUID = localStorage.getItem("customer_id");
   const request = axios.get(`${ROOT_URL}/customers/${cUUID}/providers`);
@@ -497,4 +500,21 @@ export function deleteInstanceResponse(response) {
     type: DELETE_INSTANCE_RESPONSE,
     payload: response
   };
+}
+
+export function editProvider(payload) {
+  const cUUID = localStorage.getItem("customer_id");
+  const pUUID = payload.accountUUID;
+  const request = axios.put(`${ROOT_URL}/customers/${cUUID}/providers/${pUUID}/edit`, payload);
+  return {
+    type: EDIT_PROVIDER,
+    payload: request
+  }
+}
+
+export function editProviderResponse(response) {
+  return {
+    type: EDIT_PROVIDER_RESPONSE,
+    payload: response
+  }
 }

managed/ui/src/components/config/PublicCloud/ProviderConfiguration.js
@@ -118,6 +118,9 @@ class ProviderConfiguration extends Component {
         {name: "Provider UUID", data: currentProvider.uuid},
         {name: "SSH Key", data: keyPairName},
       ];
+      if (currentProvider.code === "aws" && isNonEmptyString(currentProvider.config.AWS_HOSTED_ZONE_ID)) {
+        providerInfo.push({"name": "Hosted Zone ID", "data": currentProvider.config.AWS_HOSTED_ZONE_ID})
+      }
       if (isNonEmptyObject(hostInfo)) {
         if (currentProvider.code === "aws" && isNonEmptyObject(hostInfo["aws"])) {
           const awsHostInfo = hostInfo["aws"];

managed/ui/src/components/config/PublicCloud/views/EditProvider/EditProviderForm.js
@@ -1,0 +1,61 @@
+// Copyright (c) YugaByte, Inc.
+
+import React, {Component} from 'react';
+import { Alert } from 'react-bootstrap';
+import { YBButton, YBTextInputWithLabel } from '../../../../common/forms/fields';
+import { Field } from 'redux-form';
+import { isNonEmptyObject, convertSpaceToDash, isNonEmptyString } from 'utils/ObjectUtils';
+import { getPromiseState } from 'utils/PromiseUtils';
+
+export default class EditProviderForm extends Component {
+
+  submitEditProvider = (payload) => {
+    this.props.submitEditProvider(payload);
+  }
+
+  switchToResultView = () => {
+    this.props.switchToResultView();
+  }
+
+  componentWillReceiveProps(nextProps) {
+    const {editProvider} = nextProps;
+    if (isNonEmptyObject(editProvider) && getPromiseState(editProvider).isSuccess() && getPromiseState(this.props.editProvider).isLoading()) {
+      this.props.reloadCloudMetadata();
+      this.props.switchToResultView();
+    }
+  }
+
+  render() {
+    const {error, handleSubmit, hostedZoneId} = this.props;
+    let isHostedZoneIdValid = true;
+    let verifyEditConditions = true;
+    if (!isNonEmptyString(hostedZoneId)) {
+      isHostedZoneIdValid = false;
+      verifyEditConditions = false;
+    }
+    return (
+      <div>
+        <h4>Edit Provider</h4>
+        <form name="EditProviderForm" onSubmit={handleSubmit(this.submitEditProvider)}>
+          { error && <Alert bsStyle="danger">{error}</Alert> }
+          <div className="aws-config-form form-right-aligned-labels">
+            <Field name="accountName" type="text" label="Name"
+                   component={YBTextInputWithLabel} isReadOnly={true}/>
+            <Field name="accountUUID" type="text" label="Provider UUID"
+                   component={YBTextInputWithLabel} isReadOnly={true}/>
+            <Field name="secretKey" type="text" label="SSH Key"
+                   component={YBTextInputWithLabel} isReadOnly={true}/>
+            <Field name="hostedZoneId" type="text" label="Hosted Zone ID"
+                   component={YBTextInputWithLabel} isReadOnly={isHostedZoneIdValid}/>
+            <div className="form-action-button-container">
+              <YBButton btnText={"Submit"} btnClass={"btn btn-default save-btn"}
+                   btnType="submit" disabled={verifyEditConditions}/>
+              <YBButton btnText={"Cancel"} btnClass={"btn btn-default save-btn"} onClick={this.switchToResultView}/>
+            </div>
+          </div>
+        </form>
+      </div>
+    );
+  }
+}
+

managed/src/test/java/com/yugabyte/yw/controllers/CloudProviderControllerTest.java
@@ -110,6 +110,11 @@ public class CloudProviderControllerTest extends FakeDBApplication {
         "/api/customers/" + customer.uuid + "/providers/" + providerUUID, customer.createAuthToken());
   }
 
+  private Result editProvider(JsonNode bodyJson, UUID providerUUID) {
+    return FakeApiHelper.doRequestWithAuthTokenAndBody("PUT",
+            "/api/customers/" + customer.uuid + "/providers/"+ providerUUID + "/edit", customer.createAuthToken(), bodyJson);
+  }
+
   @Test
   public void testListEmptyProviders() {
     Result result = listProviders();
@@ -287,5 +292,34 @@ public class CloudProviderControllerTest extends FakeDBApplication {
     Result result = deleteProvider(p.uuid);
     assertOk(result);
     assertFalse(new File(scriptFile).exists());
+  }
+
+  @Test
+  public void testEditProviderWithAWSProviderType() {
+    Provider p = ModelFactory.newProvider(customer, Common.CloudType.aws);
+    ObjectNode bodyJson = Json.newObject();
+    bodyJson.put("hostedZoneId", "1234");
+    Result result = editProvider(bodyJson, p.uuid);
+    assertOk(result);
+    JsonNode json = Json.parse(contentAsString(result));
+    assertEquals(json.get("hostedZoneID").asText(), "1234");
+  }
+
+  @Test
+  public void testEditProviderWithInvalidProviderType()  {
+    Provider p = ModelFactory.newProvider(customer, Common.CloudType.onprem);
+    ObjectNode bodyJson = Json.newObject();
+    bodyJson.put("hostedZoneId", "1234");
+    Result result = editProvider(bodyJson, p.uuid);
+    assertBadRequest(result, "Expected aws found providers with code: onprem");
+  }
+
+  @Test
+  public void testEditProviderWithEmptyHostedZoneId() {
+    Provider p = ModelFactory.newProvider(customer, Common.CloudType.aws);
+    ObjectNode bodyJson = Json.newObject();
+    bodyJson.put("hostedZoneId", "");
+    Result result = editProvider(bodyJson, p.uuid);
+    assertBadRequest(result, "Required field hosted zone id");
   }
 }

managed/src/test/java/com/yugabyte/yw/models/ProviderTest.java
@@ -58,6 +58,14 @@ public class ProviderTest extends FakeDBApplication {
   }
 
   @Test
+  public void testGetMaskedConfig() {
+    Provider provider = Provider.create(defaultCustomer.uuid, Common.CloudType.aws,
+            "Amazon", ImmutableMap.of("AWS_ACCESS_KEY_ID", "BarBarBarBar"));
+    assertNotNull(provider.uuid);
+    assertNotNull(provider.getConfig().toString(), allOf(notNullValue(), equalTo("{AWS_ACCESS_KEY_ID=Ba********ar}")));
+  }
+
+  @Test
   public void testCreateProviderWithSameName() {
     Provider p1 = ModelFactory.awsProvider(defaultCustomer);
     Provider p2 = Provider.create(UUID.randomUUID(), Common.CloudType.aws, "Amazon");

managed/src/main/resources/routes
@@ -26,6 +26,7 @@ POST    /api/customers/:cUUID/providers               com.yugabyte.yw.controller
 DELETE  /api/customers/:cUUID/providers/:pUUID        com.yugabyte.yw.controllers.CloudProviderController.delete(cUUID: java.util.UUID, pUUID: java.util.UUID)
 POST    /api/customers/:cUUID/providers/:pUUID/bootstrap  com.yugabyte.yw.controllers.CloudProviderController.bootstrap(cUUID: java.util.UUID, pUUID: java.util.UUID)
 POST    /api/customers/:cUUID/providers/:pUUID/cleanup    com.yugabyte.yw.controllers.CloudProviderController.cleanup(cUUID: java.util.UUID, pUUID: java.util.UUID)
+PUT     /api/customers/:cUUID/providers/:pUUID/edit          com.yugabyte.yw.controllers.CloudProviderController.edit(cUUID: java.util.UUID, pUUID: java.util.UUID)
 GET     /api/customers/:cUUID/providers/:pUUID/regions                 com.yugabyte.yw.controllers.RegionController.list(cUUID: java.util.UUID, pUUID: java.util.UUID)
 POST    /api/customers/:cUUID/providers/:pUUID/regions                 com.yugabyte.yw.controllers.RegionController.create(cUUID: java.util.UUID, pUUID: java.util.UUID)
 DELETE  /api/customers/:cUUID/providers/:pUUID/regions/:rUUID          com.yugabyte.yw.controllers.RegionController.delete(cUUID: java.util.UUID, pUUID: java.util.UUID, rUUID: java.util.UUID)

commit 545a5037ea8f2be4952a070429f33d3dfc515248

Fix provider config bug
Summary:
- Fix provider config bug introduced by D4609
- Fix and add new tests

Test Plan:
- tested by creating a universe and make sure it got successfully created
- Went to the config page and validated the data is masked
- Validated adding hosted zone id

Reviewers: pritam, bogdan

Reviewed By: bogdan

Subscribers: jenkins-bot, yugaware

Differential Revision: https://phabricator.dev.yugabyte.com/D4627

managed/src/main/java/com/yugabyte/yw/controllers/CloudProviderController.java
@@ -75,7 +75,13 @@ public class CloudProviderController extends AuthenticatedController {
    */
   public Result list(UUID customerUUID) {
     List<Provider> providerList = Provider.getAll(customerUUID);
-    return ok(Json.toJson(providerList));
+    ArrayNode providers = Json.newArray();
+    providerList.forEach((provider) -> {
+      ObjectNode providerJson = (ObjectNode) Json.toJson(provider);
+      providerJson.set("config", provider.getMaskedConfig());
+      providers.add(providerJson);
+    });
+    return ApiResponse.success(providers);
   }
 
   // This endpoint we are using only for deleting provider for integration test purpose. our
@@ -232,7 +238,7 @@ public class CloudProviderController extends AuthenticatedController {
         // instance creation. Technically, we could make it a ybcloud parameter, but we'd still need to
         // store it somewhere and the config is the easiest place to put it. As such, since all the
         // config is loaded up as env vars anyway, might as well use in in devops like that...
-        Map<String, String> config = provider.getUnmaskedConfig();
+        Map<String, String> config = provider.getConfig();
         config.put("CUSTOM_GCE_NETWORK", formData.get().destVpcId);
         provider.setConfig(config);
         provider.save();
@@ -320,9 +326,6 @@ public class CloudProviderController extends AuthenticatedController {
     } catch (RuntimeException e) {
       return ApiResponse.error(INTERNAL_SERVER_ERROR, e.getMessage());
     }
-    ObjectNode resultNode = Json.newObject();
-    resultNode.put("hostedZoneID", hostedZoneID);
-    return ApiResponse.success(resultNode);
-
+    return ApiResponse.success(provider);
   }
 }

managed/src/main/java/com/yugabyte/yw/models/Provider.java
@@ -6,7 +6,6 @@ import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import java.util.UUID;
-import java.util.Iterator;
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
@@ -17,9 +16,8 @@ import javax.persistence.UniqueConstraint;
 
 import com.avaje.ebean.annotation.DbJson;
 import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.node.ObjectNode;
 import com.yugabyte.yw.commissioner.Common;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -27,6 +25,8 @@ import org.slf4j.LoggerFactory;
 import com.avaje.ebean.Model;
 import play.data.validation.Constraints;
 import play.libs.Json;
+
+import static com.yugabyte.yw.models.helpers.CommonUtils.maskConfig;
 
 
 @Table(
@@ -70,25 +70,17 @@ public class Provider extends Model {
 
   public void setConfig(Map<String, String> configMap) { this.config = Json.toJson(configMap); }
 
-  public Map<String, String> getConfig() {
+  @JsonIgnore
+  public JsonNode getMaskedConfig() {
     if (this.config == null) {
-      return new HashMap();
+      return Json.newObject();
     } else {
-      JsonNode maskedData = this.config.deepCopy();
-      for (Iterator<String> it = maskedData.fieldNames(); it.hasNext(); ) {
-        String key = it.next();
-        if (key.contains("KEY") || key.contains("SECRET")) {
-          ((ObjectNode) maskedData).put(key, maskedData.get(key).asText().replaceAll("(?<!^.?).(?!.?$)", "*"));
-        }
-      }
-      ObjectMapper mapper = new ObjectMapper();
-      Map<String, String> result = mapper.convertValue(maskedData, Map.class);
-      return result;
+      return maskConfig(this.config);
     }
   }
-  
-  // Method returns unmasked config values for provider
-  public Map<String, String> getUnmaskedConfig() {
+
+  @JsonIgnore
+  public Map<String, String> getConfig() {
     if (this.config == null) {
       return new HashMap();
     } else {
@@ -175,13 +167,9 @@ public class Provider extends Model {
 
   // Update host zone if for aws provider
   public void updateHostedZoneId(String hostedZoneId) {
-    try {
-      Map<String, String> currentProviderConfig = getUnmaskedConfig();
-      currentProviderConfig.put("AWS_HOSTED_ZONE_ID", hostedZoneId);
-      this.setConfig(currentProviderConfig);
-      this.save();
-    } catch (RuntimeException e) {
-      throw e;
-    }
+    Map<String, String> currentProviderConfig = getConfig();
+    currentProviderConfig.put("AWS_HOSTED_ZONE_ID", hostedZoneId);
+    this.setConfig(currentProviderConfig);
+    this.save();
   }
 }

managed/src/main/java/com/yugabyte/yw/models/helpers/CommonUtils.java
@@ -1,0 +1,28 @@
+// Copyright (c) YugaByte, Inc.
+
+package com.yugabyte.yw.models.helpers;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import play.libs.Json;
+
+import java.util.Iterator;
+
+public class CommonUtils {
+  private static String maskRegex = "(?<!^.?).(?!.?$)";
+
+  public static JsonNode maskConfig(JsonNode config) {
+    if (config == null || config.size() == 0) {
+      return Json.newObject();
+    }
+    JsonNode maskedData = config.deepCopy();
+    for (Iterator<String> it = maskedData.fieldNames(); it.hasNext(); ) {
+      String key = it.next();
+      // TODO: make this a constant
+      if (key.contains("KEY") || key.contains("SECRET")) {
+        ((ObjectNode) maskedData).put(key, maskedData.get(key).asText().replaceAll(maskRegex, "*"));
+      }
+    }
+    return maskedData;
+  }
+}

managed/ui/src/actions/cloud.js
@@ -509,12 +509,12 @@ export function editProvider(payload) {
   return {
     type: EDIT_PROVIDER,
     payload: request
-  }
+  };
 }
 
 export function editProviderResponse(response) {
   return {
     type: EDIT_PROVIDER_RESPONSE,
     payload: response
-  }
+  };
 }

managed/ui/src/components/config/PublicCloud/ProviderConfiguration.js
@@ -119,7 +119,7 @@ class ProviderConfiguration extends Component {
         {name: "SSH Key", data: keyPairName},
       ];
       if (currentProvider.code === "aws" && isNonEmptyString(currentProvider.config.AWS_HOSTED_ZONE_ID)) {
-        providerInfo.push({"name": "Hosted Zone ID", "data": currentProvider.config.AWS_HOSTED_ZONE_ID})
+        providerInfo.push({"name": "Hosted Zone ID", "data": currentProvider.config.AWS_HOSTED_ZONE_ID});
       }
       if (isNonEmptyObject(hostInfo)) {
         if (currentProvider.code === "aws" && isNonEmptyObject(hostInfo["aws"])) {

managed/ui/src/components/config/PublicCloud/views/EditProvider/EditProviderForm.js
@@ -4,7 +4,7 @@ import React, {Component} from 'react';
 import { Alert } from 'react-bootstrap';
 import { YBButton, YBTextInputWithLabel } from '../../../../common/forms/fields';
 import { Field } from 'redux-form';
-import { isNonEmptyObject, convertSpaceToDash, isNonEmptyString } from 'utils/ObjectUtils';
+import { isNonEmptyObject, isNonEmptyString } from 'utils/ObjectUtils';
 import { getPromiseState } from 'utils/PromiseUtils';
 
 export default class EditProviderForm extends Component {
@@ -58,4 +58,3 @@ export default class EditProviderForm extends Component {
     );
   }
 }
-

managed/src/test/java/com/yugabyte/yw/controllers/CloudProviderControllerTest.java
@@ -21,6 +21,7 @@ import static play.inject.Bindings.bind;
 import static play.test.Helpers.contentAsString;
 
 import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableMap;
 import com.yugabyte.yw.commissioner.Common;
 import com.yugabyte.yw.common.AccessManager;
 import com.yugabyte.yw.common.ApiHelper;
@@ -128,7 +129,11 @@ public class CloudProviderControllerTest extends FakeDBApplication {
   @Test
   public void testListProviders() {
     Provider p1 = ModelFactory.awsProvider(customer);
+    p1.setConfig(ImmutableMap.of("MY_KEY_DATA", "SENSITIVE_DATA", "MY_SECRET_DATA", "SENSITIVE_DATA"));
+    p1.save();
     Provider p2 = ModelFactory.gcpProvider(customer);
+    p2.setConfig(ImmutableMap.of("FOO", "BAR"));
+    p2.save();
     Result result = listProviders();
     JsonNode json = Json.parse(contentAsString(result));
 
@@ -136,6 +141,15 @@ public class CloudProviderControllerTest extends FakeDBApplication {
     assertEquals(2, json.size());
     assertValues(json, "uuid", (List) ImmutableList.of(p1.uuid.toString(), p2.uuid.toString()));
     assertValues(json, "name", (List) ImmutableList.of(p1.name, p2.name));
+    json.forEach((providerJson) -> {
+      JsonNode config = providerJson.get("config");
+      if (UUID.fromString(providerJson.get("uuid").asText()).equals(p1.uuid)) {
+        assertValue(config, "MY_KEY_DATA", "SE**********TA");
+        assertValue(config, "MY_SECRET_DATA", "SE**********TA");
+      } else {
+        assertValue(config, "FOO", "BAR");
+      }
+    });
   }
 
   @Test
@@ -302,7 +316,9 @@ public class CloudProviderControllerTest extends FakeDBApplication {
     Result result = editProvider(bodyJson, p.uuid);
     assertOk(result);
     JsonNode json = Json.parse(contentAsString(result));
-    assertEquals(json.get("hostedZoneID").asText(), "1234");
+    assertEquals(p.uuid, UUID.fromString(json.get("uuid").asText()));
+    p.refresh();
+    assertEquals("1234", p.getConfig().get("AWS_HOSTED_ZONE_ID"));
   }
 
   @Test

managed/src/test/java/com/yugabyte/yw/models/ProviderTest.java
@@ -12,7 +12,9 @@ import com.yugabyte.yw.common.FakeDBApplication;
 
 import java.util.UUID;
 
+import static com.yugabyte.yw.common.AssertHelper.assertValue;
 import static org.hamcrest.CoreMatchers.*;
+import static org.hamcrest.core.StringContains.containsString;
 import static org.junit.Assert.*;
 
 public class ProviderTest extends FakeDBApplication {
@@ -58,11 +60,21 @@ public class ProviderTest extends FakeDBApplication {
   }
 
   @Test
-  public void testGetMaskedConfig() {
+  public void testGetMaskedConfigWithSensitiveData() {
     Provider provider = Provider.create(defaultCustomer.uuid, Common.CloudType.aws,
             "Amazon", ImmutableMap.of("AWS_ACCESS_KEY_ID", "BarBarBarBar"));
     assertNotNull(provider.uuid);
-    assertNotNull(provider.getConfig().toString(), allOf(notNullValue(), equalTo("{AWS_ACCESS_KEY_ID=Ba********ar}")));
+    assertValue(provider.getMaskedConfig(), "AWS_ACCESS_KEY_ID", "Ba********ar");
+    assertEquals("BarBarBarBar", provider.getConfig().get("AWS_ACCESS_KEY_ID"));
+  }
+
+  @Test
+  public void testGetMaskedConfigWithoutSensitiveData() {
+    Provider provider = Provider.create(defaultCustomer.uuid, Common.CloudType.aws,
+        "Amazon", ImmutableMap.of("AWS_ACCESS_ID", "BarBarBarBar"));
+    assertNotNull(provider.uuid);
+    assertValue(provider.getMaskedConfig(), "AWS_ACCESS_ID", "BarBarBarBar");
+    assertEquals("BarBarBarBar", provider.getConfig().get("AWS_ACCESS_ID"));
   }
 
   @Test

managed/src/test/java/com/yugabyte/yw/models/helpers/CommonUtilsTest.java
@@ -1,0 +1,48 @@
+// Copyright (c) YugaByte, Inc.
+
+package com.yugabyte.yw.models.helpers;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import org.junit.Test;
+import play.libs.Json;
+
+import static com.yugabyte.yw.common.AssertHelper.assertValue;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+public class CommonUtilsTest {
+
+  @Test
+  public void maskConfigWithSensitiveData() {
+    ObjectNode config = Json.newObject();
+    config.put("SOME_KEY", "SENSITIVE_DATA");
+    config.put("KEY_DATA", "SENSITIVE_DATA");
+    config.put("MY_KEY_DATA", "SENSITIVE_DATA");
+    config.put("SOME_SECRET", "SENSITIVE_DATA");
+    config.put("SECRET_DATA", "SENSITIVE_DATA");
+    config.put("MY_SECRET_DATA", "SENSITIVE_DATA");
+    JsonNode maskedData = CommonUtils.maskConfig(config);
+    assertValue(maskedData, "SOME_KEY", "SE**********TA");
+    assertValue(maskedData, "KEY_DATA", "SE**********TA");
+    assertValue(maskedData, "MY_KEY_DATA", "SE**********TA");
+    assertValue(maskedData, "SOME_SECRET", "SE**********TA");
+    assertValue(maskedData, "SECRET_DATA", "SE**********TA");
+    assertValue(maskedData, "MY_SECRET_DATA", "SE**********TA");
+  }
+
+  @Test
+  public void testMaskConfigWithoutSensitiveData() {
+    ObjectNode config = Json.newObject();
+    config.put("SOME_DATA", "VISIBLE_DATA");
+    JsonNode maskedData = CommonUtils.maskConfig(config);
+    assertValue(maskedData, "SOME_DATA", "VISIBLE_DATA");
+  }
+
+  @Test
+  public void testMaskConfigWithNullData() {
+    JsonNode maskedData = CommonUtils.maskConfig(null);
+    assertEquals(0, maskedData.size());
+    assertNotNull(maskedData);
+  }
+}