fb86846daf15877d2c343e1c39876418baa606fc
Junseok Yang <jsyang@bitnine.net>
2019-07-30 13:33:58 -0700

    Add missing header file

--- ag_catalog.h
+++ ag_catalog.h
@@ -1,6 +1,8 @@
 #ifndef AG_AG_CATALOG_H
 #define AG_AG_CATALOG_H
 
+#include "postgres.h"
+
 Oid ag_catalog_namespace_id(void);
 
 #endif
--- ag_graph.h
+++ ag_graph.h
@@ -1,6 +1,8 @@
 #ifndef AG_AG_GRAPH_H
 #define AG_AG_GRAPH_H
 
+#include "postgres.h"
+
 void insert_graph(const Name graph_name, const Oid nsp_id);
 void delete_graph(const Name graph_name);
 void update_graph_name(const Name graph_name, const Name new_name);
