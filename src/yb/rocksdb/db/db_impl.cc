4febf6ae4b471e50bf1c95f7c95f785d03ab4b18
Timur Yusupov <ttyusupov@users.noreply.github.com>
2019-08-09 19:50:39 +0300

    #2010: Added checks for frontiers to be initialized into MemTable::GetSmallestFrontierLocked
    Summary:
    More details about the issue are described in https://github.com/YugaByte/yugabyte-db/issues/2010.
    This revision adds a check for `frontiers_` into `DBImpl::GetSmallestFrontierLocked` and also warning and debug fatal logs when frontiers are not initialized for non-empty memtable inside `DBImpl::GetMutableMemTableSmallestFrontier` since this should not happen.
    
    Test Plan: Jenkins
    
    Reviewers: bogdan, sergei
    
    Reviewed By: sergei
    
    Subscribers: ybase
    
    Differential Revision: https://phabricator.dev.yugabyte.com/D7033

--- src/yb/rocksdb/db/db_impl.cc
+++ src/yb/rocksdb/db/db_impl.cc
@@ -5724,8 +5724,16 @@ UserFrontierPtr DBImpl::GetMutableMemTableSmallestFrontier() {
       const auto* mem = cfd->mem();
       if (mem) {
         if (!cfd->IsDropped() && cfd->imm()->NumNotFlushed() == 0 && !mem->IsEmpty()) {
-          UserFrontier::Update(
-              mem->GetSmallestFrontierLocked().get(), UpdateUserValueType::kSmallest, &accumulated);
+          auto smallest_frontier = mem->GetSmallestFrontierLocked();
+          if (smallest_frontier) {
+            UserFrontier::Update(
+                smallest_frontier.get(), UpdateUserValueType::kSmallest, &accumulated);
+          } else {
+            const auto error = "smallest frontier is not initialized for non-empty MemTable";
+            YB_LOG_EVERY_N_SECS(WARNING, 5) << db_options_.log_prefix << "[" << cfd->GetName()
+                                            << "] " << error;
+            LOG(DFATAL) << error;
+          }
         }
       } else {
         YB_LOG_EVERY_N_SECS(WARNING, 5) << db_options_.log_prefix
