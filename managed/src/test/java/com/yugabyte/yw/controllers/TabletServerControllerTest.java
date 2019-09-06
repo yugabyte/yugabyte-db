0d8aa1041180d9f07cc35a0c7a38ad579f15105c
Daniel Isen <daniel@yugabyte.com>
2019-09-06 15:19:50 -0700

    #2205 - Platform API wrapper for tablet-server api
    Summary: Created a route to query tablet servers
    
    Test Plan: Curl endpoint: curl http://localhost:9000/api/v1/customers/f33e3c9b-75ab-4c30-80ad-cba85646ea39/universes/ed31b30a-c25d-4163-ac17-12819dd97027/tablet-servers' -H 'Accept: application/json, text/plain, */*' -H 'Referer: http://localhost:3000/' -H 'X-AUTH-TOKEN: b52bc2cc-d7b8-47d1-bdf9-8272a1cc4c7f' -H 'Sec-Fetch-Mode: cors' -H 'User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.132 Safari/537.36' -H 'Origin: http://localhost:3000' --compressed
    
    Reviewers: ram
    
    Reviewed By: ram
    
    Subscribers: yugaware
    
    Differential Revision: https://phabricator.dev.yugabyte.com/D7173

--- managed/src/test/java/com/yugabyte/yw/controllers/TabletServerControllerTest.java
+++ managed/src/test/java/com/yugabyte/yw/controllers/TabletServerControllerTest.java
@@ -8,24 +8,39 @@ import static org.mockito.Mockito.*;
 import static play.test.Helpers.contentAsString;
 
 import com.fasterxml.jackson.databind.JsonNode;
-import com.yugabyte.yw.common.services.YBClientService;
+import com.google.common.net.HostAndPort;
 
 import java.lang.String;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.UUID;
 import org.junit.*;
 import play.libs.Json;
 import play.mvc.*;
+import play.test.WithApplication;
 
 import org.yb.client.ListTabletServersResponse;
 import org.yb.client.YBClient;
 import org.yb.util.ServerInfo;
 
-public class TabletServerControllerTest {
+import com.yugabyte.yw.common.ApiHelper;
+import com.yugabyte.yw.common.ApiResponse;
+import com.yugabyte.yw.common.ApiUtils;
+import com.yugabyte.yw.common.FakeApiHelper;
+import static com.yugabyte.yw.common.ModelFactory.createUniverse;
+import com.yugabyte.yw.common.ModelFactory;
+import com.yugabyte.yw.common.services.YBClientService;
+import com.yugabyte.yw.models.Customer;
+import com.yugabyte.yw.models.Universe;
+
+public class TabletServerControllerTest extends WithApplication {
   private YBClientService mockService;
   private TabletServerController tabletController;
   private YBClient mockClient;
   private ListTabletServersResponse mockResponse;
+  private ApiHelper mockApiHelper;
+  private HostAndPort testHostAndPort = HostAndPort.fromString("0.0.0.0").withDefaultPort(11);
+  private JsonNode apiHelperMockResult = Json.newObject();
 
 
   @Before
@@ -35,7 +50,12 @@ public class TabletServerControllerTest {
     mockResponse = mock(ListTabletServersResponse.class);
     when(mockClient.listTabletServers()).thenReturn(mockResponse);
     when(mockService.getClient(any(String.class))).thenReturn(mockClient);
+    when(mockService.getClient(any(String.class), any(String.class))).thenReturn(mockClient);
+    when(mockClient.getLeaderMasterHostAndPort()).thenReturn(testHostAndPort);
     tabletController = new TabletServerController(mockService);
+    mockApiHelper = mock(ApiHelper.class);
+    when(mockApiHelper.getRequest(any(String.class))).thenReturn(Json.newObject());
+    tabletController.apiHelper = mockApiHelper;
   }
 
   @Test
@@ -59,5 +79,29 @@ public class TabletServerControllerTest {
     Result r = tabletController.list();
     assertEquals(500, r.status());
     assertEquals("Error: Unknown Error", contentAsString(r));
+  }
+
+  @Test
+  public void testListTabletServersWrapperSuccess() {
+    Customer customer = ModelFactory.testCustomer();
+    Universe u1 = createUniverse(customer.getCustomerId());
+    u1 = Universe.saveDetails(u1.universeUUID, ApiUtils.mockUniverseUpdater());
+    customer.addUniverseUUID(u1.universeUUID);
+    customer.save();
+    Result r = tabletController.listTabletServers(customer.uuid, u1.universeUUID);
+    assertEquals(200, r.status());
+  }
+
+  @Test
+  public void testListTabletServersWrapperFailure() {
+    when(mockApiHelper.getRequest(any(String.class)))
+            .thenThrow(new RuntimeException("Unknown Error"));
+    Customer customer = ModelFactory.testCustomer();
+    Universe u1 = createUniverse(customer.getCustomerId());
+    u1 = Universe.saveDetails(u1.universeUUID, ApiUtils.mockUniverseUpdater());
+    customer.addUniverseUUID(u1.universeUUID);
+    customer.save();
+    Result r = tabletController.listTabletServers(customer.uuid, u1.universeUUID);
+    assertEquals(500, r.status());
   }
 }
