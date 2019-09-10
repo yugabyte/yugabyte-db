274fcdbcf7b47bee5a92601495e2c64e9c972d7e
Andrew Cai <andrew@yugabyte.com>
2019-09-10 13:24:44 -0700

    [#2210][YW] Add toggle for enabling SSE in the Create Backup modal
    Summary:
    Add YBToggle field inside Formik form in Create Backup modal. Have to set the field imperatively because the actual input checkbox does not accept name or id attributes.
    
    @arnav If you haven't done so already, you should have the API call for the listing of the backups to return which ones are encrypted. Then I can add an icon in the table to indicate that the backup is encrypted.
    
    Test Plan: Go to universe detail, then click on Backups tab. Then press the 'Create Backup' and enable the server-side encryption toggle. Check that the API payload contains the key "sse"
    
    Reviewers: vit.pankin, arnav, ram
    
    Reviewed By: ram
    
    Subscribers: ui
    
    Differential Revision: https://phabricator.dev.yugabyte.com/D7170

---
+++ managed/ui/src/components/common/forms/fields/YBFormToggle.js
@@ -0,0 +1,36 @@
+// Copyright (c) YugaByte, Inc.
+
+import React, { Component } from 'react';
+import Toggle from 'react-toggle';
+import { YBLabel, DescriptionItem } from 'components/common/descriptors';
+import 'react-toggle/style.css';
+import './stylesheets/YBToggle.scss';
+
+export default class YBFormToggle extends Component {
+
+  render() {
+    const {
+      label,
+      isReadOnly,
+      meta,
+      insetError,
+      subLabel,
+      infoContent,
+      infoTitle,
+      field
+    } = this.props;
+    const onChange = (event) => {
+      if (field.onChange) {
+        field.onChange(event);
+      }
+    };
+    return (
+      <YBLabel label={label} meta={meta} insetError={insetError} infoContent={infoContent} infoTitle={infoTitle}>
+        <DescriptionItem title={subLabel}>
+          <Toggle checked={field.value} name={field.name} className="yb-toggle" onChange={onChange}
+                  disabled={isReadOnly} />
+        </DescriptionItem>
+      </YBLabel>
+    );
+  }
+}
