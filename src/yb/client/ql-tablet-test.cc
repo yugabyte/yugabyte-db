yugabyte/yugabyte-db

commit 5e854e8f448f463df378da7c2080056c44532c55

ENG-5287 #2053 [YSQL] Add ysql_num_shards_per_tserver gflag
Summary:
New gflag `ysql_num_shards_per_tserver` is added.
It is used for YSQL tables instead of `yb_num_shards_per_tserver`.
Default value is 2.

Test Plan:
Manual:
- start cluster with command `yb-ctl --num_shards_per_tserver -1 start`
- create CQL table `t1` via `cqlsh`
- create SQL table `t2` via `ysqlsh`
- visit WebUI and check what `t1` has 8 shards and `t2` has 2

Reviewers: mihnea, alex, neha

Reviewed By: neha

Subscribers: bogdan

Differential Revision: https://phabricator.dev.yugabyte.com/D7069

src/yb/master/catalog_manager.cc
@@ -1742,8 +1742,8 @@ Status CatalogManager::CreateTable(const CreateTableRequestPB* orig_req,
                                    rpc::RpcContext* rpc) {
   RETURN_NOT_OK(CheckOnline());
 
-  const bool is_pg_catalog_table =
-      orig_req->table_type() == PGSQL_TABLE_TYPE && orig_req->is_pg_catalog_table();
+  const bool is_pg_table = orig_req->table_type() == PGSQL_TABLE_TYPE;
+  const bool is_pg_catalog_table = is_pg_table && orig_req->is_pg_catalog_table();
   if (!is_pg_catalog_table || !FLAGS_hide_pg_catalog_table_creation_logs) {
     LOG(INFO) << "CreateTable from " << RequestorString(rpc)
                 << ":\n" << orig_req->DebugString();
@@ -1820,12 +1820,18 @@ Status CatalogManager::CreateTable(const CreateTableRequestPB* orig_req,
   if (num_tablets <= 0) {
     // Use default as client could have gotten the value before any tserver had heartbeated
     // to (a new) master leader.
-    TSDescriptorVector ts_descs;
-    master_->ts_manager()->GetAllLiveDescriptorsInCluster(
-        &ts_descs, replication_info.live_replicas().placement_uuid(), blacklistState.tservers_);
-    num_tablets = ts_descs.size() * FLAGS_yb_num_shards_per_tserver;
-    LOG(INFO) << "Setting default tablets to " << num_tablets << " with "
-              << ts_descs.size() << " primary servers";
+    if (IsSystemNamespace(ns->name())) {
+      num_tablets = 1;
+      LOG(INFO) << "Setting default tablets to " << num_tablets << " for table in system namespace";
+    } else {
+      TSDescriptorVector ts_descs;
+      master_->ts_manager()->GetAllLiveDescriptorsInCluster(
+          &ts_descs, replication_info.live_replicas().placement_uuid(), blacklistState.tservers_);
+      num_tablets = ts_descs.size() * (is_pg_table ? FLAGS_ysql_num_shards_per_tserver
+                                                   : FLAGS_yb_num_shards_per_tserver);
+      LOG(INFO) << "Setting default tablets to " << num_tablets << " with "
+                << ts_descs.size() << " primary servers";
+    }
   }
 
   // Create partitions.
@@ -1878,7 +1884,7 @@ Status CatalogManager::CreateTable(const CreateTableRequestPB* orig_req,
     }
 
     // Assign column-ids that have just been computed and assigned to "index_info".
-    if (orig_req->table_type() != PGSQL_TABLE_TYPE) {
+    if (!is_pg_table) {
       DCHECK_EQ(index_info.columns().size(), schema.num_columns())
         << "Number of columns are not the same between index_info and index_schema";
       // int colidx = 0;
@@ -1974,8 +1980,7 @@ Status CatalogManager::CreateTable(const CreateTableRequestPB* orig_req,
   TRACE("Wrote table to system table");
 
   // For index table, insert index info in the indexed table.
-  if ((req.has_index_info() || req.has_indexed_table_id()) &&
-      orig_req->table_type() != PGSQL_TABLE_TYPE) {
+  if ((req.has_index_info() || req.has_indexed_table_id()) && !is_pg_table) {
     s = AddIndexInfoToTable(indexed_table, index_info);
     if (PREDICT_FALSE(!s.ok())) {
       return AbortTableCreation(table.get(), tablets,

src/yb/client/client.cc
@@ -1265,22 +1265,6 @@ bool YBClient::IsMultiMaster() const {
   return addrs.size() > 1;
 }
 
-Result<int> YBClient::NumTabletsForUserTable() {
-  if (FLAGS_yb_num_total_tablets > 0) {
-    VLOG(1) << "num_tablets=" << FLAGS_yb_num_total_tablets
-            << ": --yb_num_total_tablets is specified.";
-    return FLAGS_yb_num_total_tablets;
-  } else {
-    int tserver_count = 0;
-    RETURN_NOT_OK(TabletServerCount(&tserver_count, true /* primary_only */));
-    int num_tablets = tserver_count * FLAGS_yb_num_shards_per_tserver;
-    VLOG(1) << "num_tablets = " << num_tablets << ": "
-            << "calculated as tserver_count * FLAGS_yb_num_shards_per_tserver ("
-            << tserver_count << " * " << FLAGS_yb_num_shards_per_tserver << ")";
-    return num_tablets;
-  }
-}
-
 void YBClient::TEST_set_admin_operation_timeout(const MonoDelta& timeout) {
   data_->default_admin_operation_timeout_ = timeout;
 }

src/yb/client/client.h
@@ -506,10 +506,6 @@ class YBClient {
 
   bool IsMultiMaster() const;
 
-  // Get the number of tablets to be created for a new user table.
-  // This will be based on --num_shards_per_tserver and number of tservers.
-  Result<int> NumTabletsForUserTable();
-
   void TEST_set_admin_operation_timeout(const MonoDelta& timeout);
 
   const MonoDelta& default_admin_operation_timeout() const;

src/yb/client/table_creator.cc
@@ -17,13 +17,13 @@
 #include "yb/client/client-internal.h"
 
 #include "yb/common/wire_protocol.h"
+#include "yb/common/common_flags.h"
 
 #include "yb/util/flag_tags.h"
 
 #include "yb/yql/redis/redisserver/redis_constants.h"
 
 DECLARE_bool(client_suppress_created_logs);
-DECLARE_int32(yb_num_shards_per_tserver);
 
 namespace yb {
 namespace client {
@@ -225,17 +225,6 @@ Status YBTableCreator::Create() {
 
   SchemaToPB(internal::GetSchema(*schema_), req.mutable_schema());
 
-  // Setup the number splits (i.e. number of tablets).
-  if (num_tablets_ <= 0) {
-    if (table_name_.is_system()) {
-      num_tablets_ = 1;
-      VLOG(1) << "num_tablets=1: using one tablet for a system table";
-    } else {
-      num_tablets_ = VERIFY_RESULT(client_->NumTabletsForUserTable());
-    }
-  } else {
-    VLOG(1) << "num_tablets: number of tablets explicitly specified: " << num_tablets_;
-  }
   req.set_num_tablets(num_tablets_);
   req.mutable_partition_schema()->CopyFrom(partition_schema_);
 

src/yb/client/yb_table_name.cc
@@ -16,6 +16,7 @@
 #include <boost/functional/hash/hash.hpp>
 
 #include "yb/master/master_defaults.h"
+#include "yb/master/master_util.h"
 #include "yb/master/master.pb.h"
 
 namespace yb {
@@ -63,12 +64,8 @@ void YBTableName::GetFromNamespaceIdentifierPB(const master::NamespaceIdentifier
   }
 }
 
-bool YBTableName::IsSystemNamespace(const std::string& namespace_name) {
-  return (namespace_name == master::kSystemNamespaceName            ||
-          namespace_name == master::kSystemAuthNamespaceName        ||
-          namespace_name == master::kSystemDistributedNamespaceName ||
-          namespace_name == master::kSystemSchemaNamespaceName      ||
-          namespace_name == master::kSystemTracesNamespaceName);
+bool YBTableName::is_system() const {
+  return master::IsSystemNamespace(resolved_namespace_name());
 }
 
 size_t hash_value(const YBTableName& table_name) {

src/yb/client/yb_table_name.h
@@ -24,7 +24,6 @@
 
 #include "yb/common/redis_constants_common.h"
 
-
 namespace yb {
 
 namespace master {
@@ -111,9 +110,7 @@ class YBTableName {
     return table_id_; // Can be empty
   }
 
-  bool is_system() const {
-    return IsSystemNamespace(resolved_namespace_name());
-  }
+  bool is_system() const;
 
   bool is_redis_namespace() const {
     return ((has_namespace() && resolved_namespace_name() == common::kRedisKeyspaceName));
@@ -155,8 +152,6 @@ class YBTableName {
 
   void SetIntoNamespaceIdentifierPB(master::NamespaceIdentifierPB* id) const;
   void GetFromNamespaceIdentifierPB(const master::NamespaceIdentifierPB& id);
-
-  static bool IsSystemNamespace(const std::string& namespace_name);
 
  private:
   std::string namespace_id_; // Optional. Can be set when the client knows the namespace id also.

src/yb/common/common_flags.cc
@@ -25,6 +25,9 @@ DEFINE_int32(yb_num_shards_per_tserver, kAutoDetectNumShardsPerTServer,
     "The default number of shards per table per tablet server when a table is created. If the "
     "value is -1, the system automatically determines the number of tablets.");
 
+DEFINE_int32(ysql_num_shards_per_tserver, 2,
+    "The default number of shards per YSQL table per tablet server when a table is created.");
+
 namespace yb {
 
 void InitCommonFlags() {

src/yb/common/common_flags.h
@@ -19,6 +19,7 @@
 static constexpr int kAutoDetectNumShardsPerTServer = -1;
 
 DECLARE_int32(yb_num_shards_per_tserver);
+DECLARE_int32(ysql_num_shards_per_tserver);
 DECLARE_bool(enable_ysql);
 
 namespace yb {

commit cfcd04bc92d71cef8d0f7c4f48b9f2f3095c0928

#2293 [YSQ]: Fix number of tablets created for system tables
Summary:
Due to a regression in https://github.com/YugaByte/yugabyte-db/commit/5e854e8f448f463df378da7c2080056c44532c55, we are creating only 1 tablet for system tables such as transactions, cdc_state and metrics.

This diff fixes it to create default number of tablets.

Test Plan:
Jenkins
Created cluster using yb-ctl and verified that transactions table has multiple tablets.

Reviewers: dmitry, mihnea, kannan

Reviewed By: kannan

Subscribers: yql, kannan, bogdan

Differential Revision: https://phabricator.dev.yugabyte.com/D7202

src/yb/master/catalog_manager.cc
@@ -1818,18 +1818,13 @@ Status CatalogManager::CreateTable(const CreateTableRequestPB* orig_req,
   if (num_tablets <= 0) {
     // Use default as client could have gotten the value before any tserver had heartbeated
     // to (a new) master leader.
-    if (IsSystemNamespace(ns->name())) {
-      num_tablets = 1;
-      LOG(INFO) << "Setting default tablets to " << num_tablets << " for table in system namespace";
-    } else {
-      TSDescriptorVector ts_descs;
-      master_->ts_manager()->GetAllLiveDescriptorsInCluster(
-          &ts_descs, replication_info.live_replicas().placement_uuid(), blacklistState.tservers_);
-      num_tablets = ts_descs.size() * (is_pg_table ? FLAGS_ysql_num_shards_per_tserver
-                                                   : FLAGS_yb_num_shards_per_tserver);
-      LOG(INFO) << "Setting default tablets to " << num_tablets << " with "
-                << ts_descs.size() << " primary servers";
-    }
+    TSDescriptorVector ts_descs;
+    master_->ts_manager()->GetAllLiveDescriptorsInCluster(
+        &ts_descs, replication_info.live_replicas().placement_uuid(), blacklistState.tservers_);
+    num_tablets = ts_descs.size() * (is_pg_table ? FLAGS_ysql_num_shards_per_tserver
+                                                 : FLAGS_yb_num_shards_per_tserver);
+    LOG(INFO) << "Setting default tablets to " << num_tablets << " with "
+              << ts_descs.size() << " primary servers";
   }
 
   // Create partitions.

src/yb/client/ql-tablet-test.cc
@@ -63,6 +63,7 @@ DECLARE_bool(flush_rocksdb_on_shutdown);
 DECLARE_int32(memstore_size_mb);
 DECLARE_int64(global_memstore_size_mb_max);
 DECLARE_bool(TEST_allow_stop_writes);
+DECLARE_int32(yb_num_shards_per_tserver);
 
 namespace yb {
 namespace client {
@@ -337,6 +338,25 @@ class QLTabletTest : public QLDmlTestBase {
     return tablets;
   }
 
+  Status WaitForTableCreation(const YBTableName& table_name,
+                              master::IsCreateTableDoneResponsePB *resp) {
+    return LoggedWaitFor([=]() -> Result<bool> {
+      master::IsCreateTableDoneRequestPB req;
+      req.mutable_table()->set_table_name(table_name.table_name());
+      req.mutable_table()->mutable_namespace_()->set_name(table_name.namespace_name());
+      resp->Clear();
+
+      auto master_proxy = std::make_shared<master::MasterServiceProxy>(
+          &client_->proxy_cache(),
+          cluster_->leader_mini_master()->bound_rpc_addr());
+      rpc::RpcController rpc;
+      rpc.set_timeout(MonoDelta::FromSeconds(30));
+
+      Status s = master_proxy->IsCreateTableDone(req, resp, &rpc);
+      return s.ok() && !resp->has_error();
+    }, MonoDelta::FromSeconds(30), "Table Creation");
+  }
+
   void TestDeletePartialKey(int num_range_keys_in_delete);
 
   TableHandle table1_;
@@ -400,6 +420,30 @@ TEST_F(QLTabletTest, OverlappedImport) {
   FillTable(0, kTotalKeys, &table1_);
   FillTable(kTotalKeys, 2 * kTotalKeys, &table2_);
   ASSERT_NOK(Import());
+}
+
+// Test expected number of tablets for transactions table - added for #2293.
+TEST_F(QLTabletTest, TransactionsTableTablets) {
+  YBSchemaBuilder builder;
+  builder.AddColumn(kKeyColumn)->Type(INT32)->HashPrimaryKey()->NotNull();
+  builder.AddColumn(kValueColumn)->Type(INT32);
+
+  // Create transactional table.
+  TableProperties table_properties;
+  table_properties.SetTransactional(true);
+  builder.SetTableProperties(table_properties);
+
+  TableHandle table;
+  ASSERT_OK(table.Create(kTable1Name, 8, client_.get(), &builder));
+
+  // Wait for transactions table to be created.
+  YBTableName table_name(master::kSystemNamespaceName, kTransactionsTableName);
+  master::IsCreateTableDoneResponsePB resp;
+  ASSERT_OK(WaitForTableCreation(table_name, &resp));
+  ASSERT_TRUE(resp.done());
+
+  auto tablets = GetTabletInfos(table_name);
+  ASSERT_EQ(tablets.size(), cluster_->num_tablet_servers() * FLAGS_yb_num_shards_per_tserver);
 }
 
 void DoStepDowns(MiniCluster* cluster) {