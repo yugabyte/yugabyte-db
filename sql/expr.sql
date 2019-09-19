f56c626d9a2920d4f178bb298a811507dbc3a6c4
Junseok Yang <jsyang@bitnine.net>
2019-09-19 13:51:55 -0700

    Fix regression test failure
    The failure happened because the regression tests used the previous type
    name, jsonbx. Replacing jsonbx to agtype solved this issue.

--- sql/expr.sql
+++ sql/expr.sql
@@ -6,19 +6,19 @@ SET search_path TO ag_catalog;
 --
 
 -- empty map
-SELECT * FROM cypher($$RETURN {}$$) AS r(c jsonbx);
+SELECT * FROM cypher($$RETURN {}$$) AS r(c agtype);
 
 -- map of scalar values
 SELECT * FROM cypher($$
 RETURN {s: 's', i: 1, f: 1.0, b: true, z: null}
-$$) AS r(c jsonbx);
+$$) AS r(c agtype);
 
 --
 -- list literal
 --
 
 -- empty list
-SELECT * FROM cypher($$RETURN []$$) AS r(c jsonbx);
+SELECT * FROM cypher($$RETURN []$$) AS r(c agtype);
 
 -- list of scalar values
-SELECT * FROM cypher($$RETURN ['str', 1, 1.0, true, null]$$) AS r(c jsonbx);
+SELECT * FROM cypher($$RETURN ['str', 1, 1.0, true, null]$$) AS r(c agtype);
