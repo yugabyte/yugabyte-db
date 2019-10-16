986a83d56de8f5a8eefc0dc2d4ea54f3982418e8
Dmitry Uspenskiy <47734295+d-uspenskiy@users.noreply.github.com>
2019-10-16 12:59:38 +0300

    #2582 [YSQL] Avoid namespace id resolving for import_namespace yb-admin command
    Summary:
    `yb_admin` allows to set new namespace and table name while importing snapshot.
    As a result `yb_admin` should not try to find namespace id by name while processing `import_snapshot` command.
    As it is valid to specify new namespace name which is not yet existed on cluster.
    New namespace with provided name will be created in this case.
    
    Test Plan:
    New test cases were introduced
    ```
    ./yb_build.sh --cxx-test yb-admin-test_ent
    ```
    
    Reviewers: bogdan, oleg
    
    Reviewed By: oleg
    
    Subscribers: yql
    
    Differential Revision: https://phabricator.dev.yugabyte.com/D7388

--- ent/src/yb/tools/yb-admin-test_ent.cc
+++ ent/src/yb/tools/yb-admin-test_ent.cc
@@ -45,67 +45,57 @@ using master::ListSnapshotsResponsePB;
 using rpc::RpcController;
 using strings::Substitute;
 
-static const char* const kAdminToolName = "yb-admin";
-
 class AdminCliTest : public tserver::TabletServerIntegrationTestBase {
  protected:
-  // Figure out where the admin tool is.
-  string GetAdminToolPath() const {
-    return GetToolPath(kAdminToolName);
-  }
-
-  string StartRF1CluterAndGetMasterAddrs(bool secure_server = false) {
+  void StartRF1Cluter(bool secure_server = false) {
     FLAGS_num_tablet_servers = secure_server ? 0 : 1;
     FLAGS_num_replicas = 1;
 
     vector<string> ts_flags, master_flags;
-    master_flags.push_back("--replication_factor=1");
     if (secure_server) {
       const auto sub_dir = JoinPathSegments("ent", "test_certs");
       auto root_dir = env_util::GetRootDir(sub_dir) + "/../../";
       auto certs_dir = JoinPathSegments(root_dir, sub_dir);
 
       master_flags.push_back("--use_node_to_node_encryption=true");
-      master_flags.push_back("--certs_dir=" + certs_dir);
+      master_flags.push_back("--certs_dir=" + JoinPathSegments(root_dir, sub_dir));
       master_flags.push_back("--use_client_to_server_encryption=true");
-
-      ts_flags.push_back("--use_node_to_node_encryption=true");
-      ts_flags.push_back("--certs_dir=" + certs_dir);
-      ts_flags.push_back("--use_client_to_server_encryption=true");
+      ts_flags = master_flags;
     }
+    master_flags.push_back("--replication_factor=1");
     BuildAndStart(ts_flags, master_flags);
-    return ToString(cluster_->master()->bound_rpc_addr());
+  }
+
+  CHECKED_STATUS RunAdminToolCommand(const std::initializer_list<string>& args) {
+    std::stringstream command;
+    command << GetToolPath("yb-admin") << " --master_addresses " <<
+        ToString(cluster_->master()->bound_rpc_addr());
+    for (const auto& a : args) {
+      command << " " << a;
+    }
+    const auto command_str = command.str();
+    LOG(INFO) << "Run tool: " << command_str;
+    return Subprocess::Call(command_str);
   }
 };
 
 TEST_F(AdminCliTest, TestNonTLS) {
-  string arg_str = Substitute("$0 -master_addresses $1 list_all_masters",
-                              GetAdminToolPath(),
-                              StartRF1CluterAndGetMasterAddrs());
-
-  LOG(INFO) << "Run tool:" << arg_str;
-  ASSERT_OK(Subprocess::Call(arg_str));
+  StartRF1Cluter();
+  ASSERT_OK(RunAdminToolCommand({"list_all_masters"}));
 }
 
 // TODO: Enabled once ENG-4900 is resolved.
 TEST_F(AdminCliTest, DISABLED_TestTLS) {
+  StartRF1Cluter();
   const auto sub_dir = JoinPathSegments("ent", "test_certs");
   auto root_dir = env_util::GetRootDir(sub_dir) + "/../../";
-  string arg_str = Substitute("$0 -master_addresses $1 --certs_dir_name $2 list_all_masters",
-                              GetAdminToolPath(),
-                              StartRF1CluterAndGetMasterAddrs(true /* secure_server */),
-                              JoinPathSegments(root_dir, sub_dir));
-
-  LOG(INFO) << "Run tool:" << arg_str;
-  ASSERT_OK(Subprocess::Call(arg_str));
+  ASSERT_OK(RunAdminToolCommand({
+    "--certs_dir_name", JoinPathSegments(root_dir, sub_dir), "list_all_masters"}));
 }
 
 TEST_F(AdminCliTest, TestCreateSnapshot) {
-  string master_address = StartRF1CluterAndGetMasterAddrs();
-
-  auto client = ASSERT_RESULT(YBClientBuilder()
-      .add_master_server_addr(master_address)
-      .Build());
+  StartRF1Cluter();
+  auto client = ASSERT_RESULT(CreateClient());
 
   // There is custom table.
   vector<YBTableName> tables;
@@ -118,25 +108,88 @@ TEST_F(AdminCliTest, TestCreateSnapshot) {
   ASSERT_OK(master_backup_proxy(cluster_.get())->ListSnapshots(req, &resp, &rpc));
   ASSERT_EQ(resp.snapshots_size(), 0);
 
-  // Default table that gets created.
-  string table_name = kTableName.table_name();
-  string keyspace = kTableName.namespace_name();
-
-  string exe_path = GetAdminToolPath();
-  string arg_str = Substitute("$0 -master_addresses $1 create_snapshot $2 $3",
-                              exe_path,
-                              master_address,
-                              keyspace,
-                              table_name);
-
-  LOG(INFO) << "Run tool:" << arg_str;
-  ASSERT_OK(Subprocess::Call(arg_str));
+  // Create snapshot of default table that gets created.
+  ASSERT_OK(RunAdminToolCommand({
+    "create_snapshot", kTableName.namespace_name(), kTableName.table_name()}));
 
   rpc.Reset();
   ASSERT_OK(master_backup_proxy(cluster_.get())->ListSnapshots(req, &resp, &rpc));
   ASSERT_EQ(resp.snapshots_size(), 1);
 
   LOG(INFO) << "Test TestCreateSnapshot finished.";
+}
+
+Result<string> GetCompletedSnapshot(ExternalMiniCluster* cluster) {
+  ListSnapshotsRequestPB req;
+  ListSnapshotsResponsePB resp;
+  RpcController rpc;
+
+  auto timeout = MonoDelta::FromSeconds(10);
+  MonoTime start = MonoTime::Now();
+  do {
+    rpc.Reset();
+    RETURN_NOT_OK(master_backup_proxy(cluster)->ListSnapshots(req, &resp, &rpc));
+    if (resp.snapshots_size() != 1) {
+      return STATUS_FORMAT(Corruption, "Wrong snapshot count $0", resp.snapshots_size());
+    }
+    if (resp.snapshots(0).entry().state() == master::SysSnapshotEntryPB_State_COMPLETE) {
+      return resp.snapshots(0).id();
+    }
+    SleepFor(MonoDelta::FromMilliseconds(100));
+  } while (MonoTime::Now().GetDeltaSince(start).LessThan(timeout));
+  return STATUS(TimedOut, "Snapshot has not been not completed in requested time");
+}
+
+TEST_F(AdminCliTest, TestImportSnapshot) {
+  StartRF1Cluter();
+
+  auto client = ASSERT_RESULT(CreateClient());
+
+  // Create snapshot of default table that gets created.
+  ASSERT_OK(RunAdminToolCommand({
+    "create_snapshot", kTableName.namespace_name(), kTableName.table_name()}));
+
+  const auto snapshot_id = CHECK_RESULT(GetCompletedSnapshot(cluster_.get()));
+
+  std::string tmp_dir;
+  ASSERT_OK(Env::Default()->GetTestDirectory(&tmp_dir));
+  const auto snapshot_file = JoinPathSegments(tmp_dir, "exported_snapshot.dat");
+  ASSERT_OK(RunAdminToolCommand({
+    "export_snapshot", snapshot_id, snapshot_file}));
+
+  auto importer = [this, &client, &snapshot_file](const string& namespace_name,
+                                                  const string& table_name) -> Status {
+    ListSnapshotsRequestPB req;
+    ListSnapshotsResponsePB resp;
+    RpcController rpc;
+    RETURN_NOT_OK(master_backup_proxy(cluster_.get())->ListSnapshots(req, &resp, &rpc));
+    const auto expected_snapshot_count = resp.snapshots_size() + 1;
+    RETURN_NOT_OK(RunAdminToolCommand({
+        "import_snapshot", snapshot_file, namespace_name, table_name}));
+    rpc.Reset();
+    RETURN_NOT_OK(master_backup_proxy(cluster_.get())->ListSnapshots(req, &resp, &rpc));
+    if (resp.snapshots_size() != expected_snapshot_count) {
+      return STATUS_FORMAT(
+          Corruption, "Wrong snapshot count $0 expected but $1 found",
+          expected_snapshot_count, resp.snapshots_size());
+    }
+    vector<YBTableName> tables;
+    RETURN_NOT_OK(client->ListTables(&tables, /* filter */ "", /* exclude_ysql */ true));
+    for (const auto& t : tables) {
+      if (t.namespace_name() == namespace_name && t.table_name() == table_name) {
+        return Status::OK();
+      }
+    }
+    return STATUS_FORMAT(NotFound, "Expected table $0.$1 not found", namespace_name, table_name);
+  };
+
+  const string new_table_name = kTableName.table_name() + "_new";
+  // Import snapshot in non existed namespace.
+  ASSERT_OK(importer(kTableName.namespace_name() + "_new", new_table_name));
+  // Import snapshot in already existed namespace.
+  ASSERT_OK(importer(kTableName.namespace_name(), new_table_name));
+  // Import snapshot in already existed namespace and table.
+  ASSERT_OK(importer(kTableName.namespace_name(), kTableName.table_name()));
 }
 
 }  // namespace tools
