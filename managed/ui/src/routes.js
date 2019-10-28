d62abbdaef7a20fd405dadcf22f57f8e595b92c7
Andrew Cai <andrew@yugabyte.com>
2019-10-28 16:55:50 -0700

    [#2048][YW] Revert routes.js change which was causing redirect loop when a feature was disabled and user navigates to it.

--- managed/ui/src/routes.js
+++ managed/ui/src/routes.js
@@ -93,9 +93,10 @@ function validateSession(store, replacePath, callback) {
         } else if ("uuid" in response.payload.data) {
           localStorage.setItem("customerId", response.payload.data["uuid"]);
         }
-      })
-      .finally(callback);
+      });
+
   }
+  callback();
 }
 
 export default (store) => {
