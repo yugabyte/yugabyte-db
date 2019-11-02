c3a8698e6445d8a8df3faf79131186b6e7c13381
Sergei Politov <spolitov@gmail.com>
2019-11-02 18:45:04 +0300

    [#2750] Disable RocksDB stalls and stops based on number of SST files
    Summary: Write throttling is replaced with new algorithm (D7474), so stalls and stop by number of SST files should be disabled.
    
    Test Plan: Jenkins
    
    Reviewers: mikhail, timur, bogdan
    
    Reviewed By: bogdan
    
    Subscribers: ybase
    
    Differential Revision: https://phabricator.dev.yugabyte.com/D7505

--- src/yb/client/ql-tablet-test.cc
+++ src/yb/client/ql-tablet-test.cc
@@ -883,7 +883,7 @@ TEST_F(QLTabletTest, ManySstFilesBootstrap) {
   {
     google::FlagSaver flag_saver;
 
-    auto original_rocksdb_level0_stop_writes_trigger = FLAGS_rocksdb_level0_stop_writes_trigger;
+    auto original_rocksdb_level0_stop_writes_trigger = 48;
     FLAGS_sst_files_hard_limit = std::numeric_limits<uint64_t>::max();
     FLAGS_sst_files_soft_limit = FLAGS_sst_files_hard_limit;
     FLAGS_rocksdb_level0_stop_writes_trigger = 10000;
