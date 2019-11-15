84d04d7dcaccc10c7533836b8da3fc33c8765dbc
Alex Abdugafarov <frozenspider@users.noreply.github.com>
2019-11-14 18:08:16 -0800

    [#1237] [YSQL] Transparent read restarts for general queries
    Summary:
    This revision extends D7403 (
    https://github.com/yugabyte/yugabyte-db/commit/3212616e351647436f808d4963d229e7881996c8 ) with the
    ability to transparently restart (1) multi-row queries as well as (2) parameterized prepared
    statements.
    
    For (1) we utilize the fact that PostgreSQL has a fixed-size (currently 8 KB) output buffer that
    accumulates data before it gets flushed to the frontend (client). While buffer hasn't been flushed
    yet, we're free to do restarts as we please. We mark a "current" position in a buffer before
    attempting to execute a query, and if execution fails, we roll back to saved position to retry,
    discarding everything added during query execution. Note that we can't simply clear the buffer as it
    may contain result of parse/bind steps for prepared queries and there's no reason to re-run those.
    If at any point buffer was flushed (e.g. if restart read error happened after we fetched >~8 KB
    worth of data), we can't restart anymore and we still propagate an error to the client.
    
    For (2) we salvage the ParamListInfo struct from the restarting portal, but we copy it twice - from
    the old portal context to execution context and then to new portal context. Note that this double
    memory copy for bind variable might technically be big - which seems to concern PostgreSQL
    developers according to the following comment excerpt from one of a shortcuts they're using:
    
    > This is grotty but is a big win when dealing with very large parameter strings.
    
    Test Plan: ./yb_build.sh --java-test org.yb.pgsql.TestPgReadRestarts
    
    Reviewers: sergei, mihnea, dmitry, neha, neil, kannan, mikhail
    
    Reviewed By: mikhail
    
    Subscribers: yql
    
    Differential Revision: https://phabricator.dev.yugabyte.com/D7549

--- src/postgres/src/include/access/xact.h
+++ src/postgres/src/include/access/xact.h
@@ -438,7 +438,7 @@ extern void EnterParallelMode(void);
 extern void ExitParallelMode(void);
 extern bool IsInParallelMode(void);
 
-extern void YBMarkDataSendAttempted(void);
-extern bool YBIsDataSendAttempted(void);
+extern void YBMarkDataSent(void);
+extern bool YBIsDataSent(void);
 
 #endif							/* XACT_H */
