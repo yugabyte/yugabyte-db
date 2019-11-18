6ad426b642ab53457fe4ace76778be4e4d405a31
Sanketh I <sanketh@yugabyte.com>
2019-11-18 14:21:23 -0800

    [#2459] [YW] Add a prometheus metrics export endpoint
    Summary: Add an API endpoint to export prometheus metrics in the right format. Also add default jvm metrics. Convert universe health checks to a single boolean time series. This will be expanded to more fine grained time series in later diffs.
    
    Test Plan:
    Run prometheus alongside local YW with a new scrape target pointing to this YW endpoint. Verify that default JVM metrics and the univ health check metric show up.
    
    {F12965}
    
    Reviewers: arnav, wesley, ram
    
    Reviewed By: ram
    
    Subscribers: jenkins-bot, yugaware
    
    Differential Revision: https://phabricator.dev.yugabyte.com/D7604

---
+++ managed/src/main/java/com/yugabyte/yw/controllers/MetricsController.java
@@ -0,0 +1,40 @@
+// Copyright (c) YugaByte, Inc.
+
+package com.yugabyte.yw.controllers;
+
+import play.mvc.Result;
+import play.mvc.Controller;
+import play.mvc.Results;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import io.prometheus.client.exporter.common.TextFormat;
+import io.prometheus.client.CollectorRegistry;
+import java.io.ByteArrayOutputStream;
+import java.io.OutputStreamWriter;
+
+import com.yugabyte.yw.common.ApiResponse;
+
+
+public class MetricsController extends Controller {
+
+  public static final Logger LOG = LoggerFactory.getLogger(MetricsController.class);
+
+  public Result index() {
+    final ByteArrayOutputStream response = new ByteArrayOutputStream(1 << 20);
+    try {
+      final OutputStreamWriter osw = new OutputStreamWriter(response);
+      TextFormat.write004(osw, CollectorRegistry.defaultRegistry.metricFamilySamples());
+      osw.flush();
+      osw.close();
+      response.flush();
+      response.close();
+    } catch (Exception e) {
+      return ApiResponse.error(INTERNAL_SERVER_ERROR, e.getMessage());
+    }
+
+    return Results.status(OK, response.toString());
+  }
+
+}
