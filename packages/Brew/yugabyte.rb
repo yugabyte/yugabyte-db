e79c4c78c644284ac49c26cf6b7c4ec3fc80f940
vp15591 <vp15591@mail.com>
2019-11-22 16:12:41 +0530

    updated the brew formula for latest yugabyte version as well update for yugabyte config file and log directory also

--- packages/Brew/yugabyte.rb
+++ packages/Brew/yugabyte.rb
@@ -1,21 +1,42 @@
 class Yugabyte < Formula
   desc "High-performance distributed SQL database Yugabyte DB"
   homepage "https://yugabyte.com"
-  url "https://downloads.yugabyte.com/yugabyte-2.0.3.0-darwin.tar.gz"
-  sha256 "5d7d35b3bbdee7b89fe7910a363734b559cd5ce7e5899cd155024fd965fd8145"
-
+  url "https://downloads.yugabyte.com/yugabyte-2.0.5.2-darwin.tar.gz"
+  sha256 "8d9b939fe10ba4116c569e27b5b177e9249e448a369dc92b42f5f4a41fa5c8b6"
+  depends_on :java => "1.8"
   def install
     rm_f buildpath/"lib/cassandra-driver-internal-only-3.13.0.post0-743d942c.zip"
     rm_f buildpath/"lib/futures-2.1.6-py2.py3-none-any.zip"
     rm_f buildpath/"lib/six-1.7.3-py2.py3-none-any.zip"
     prefix.install Dir["*"]
-    bin.install_symlink prefix/"yugabyte-db"
-    bin.install_symlink prefix/"bin/ysqlsh"
-    bin.install_symlink prefix/"bin/ycqlsh"
+    bin.install_symlink prefix/"yugabyted"
+    bin.install_symlink "ysqlsh"
+    bin.install_symlink "cqlsh"
   end
-
-  plist_options :manual => "yugabyte-db start"
-
+  def post_install
+    (var/"yugabyte_data").mkpath
+    (var/"log/yugabyte_logs").mkpath
+    if !(File.exist?((etc/"yugabyte.conf"))) then
+      (etc/"yugabyte.conf").write yugabyte_conf
+    end
+  end
+  def yugabyte_conf; <<~EOS
+    {
+    "tserver_webserver_port": 9000, 
+    "master_rpc_port": 7100, 
+    "webserver_port": 7200, 
+    "master_webserver_port": 7000, 
+    "bind_ip": "127.0.0.1", 
+    "ycql_port": 9042, 
+    "data_dir": "#{var}/yugabyte_data", 
+    "ysql_port": 5433, 
+    "log_dir": "#{var}/log/yugabyte_logs", 
+    "polling_interval": "5", 
+    "tserver_rpc_port": 9100
+    }
+  EOS
+  end
+  plist_options :manual => "yugabyted start --config #{HOMEBREW_PREFIX}/etc/yugabyte.conf"
   def plist; <<~EOS
     <?xml version="1.0" encoding="UTF-8"?>
     <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
@@ -25,15 +46,21 @@ class Yugabyte < Formula
       <string>#{plist_name}</string>
       <key>ProgramArguments</key>
       <array>
-        <string>#{prefix}/yugabyte-db</string>
+        <string>#{prefix}/yugabyted</string>
         <string>start</string>
+        <string>--config</string>
+        <string>#{etc}/yugabyte.conf</string>
       </array>
       <key>RunAtLoad</key>
       <true/>
       <key>KeepAlive</key>
       <false/>
       <key>WorkingDirectory</key>
-      <string>#{prefix}</string>
+      <string>#{HOMEBREW_PREFIX}</string>
+      <key>StandardErrorPath</key>
+      <string>#{var}/log/yugabyte_logs/yugabyted.log</string>
+      <key>StandardOutPath</key>
+      <string>#{var}/log/yugabyte_logs/yugabyted.log</string>
       <key>HardResourceLimits</key>
       <dict>
         <key>NumberOfFiles</key>
--- packages/linuxBrew/yugabyte.rb
+++ packages/linuxBrew/yugabyte.rb
@@ -1,8 +1,8 @@
 class Yugabyte < Formula
   desc "Formula for Yugabyte DB"
   homepage "https://www.yugabyte.com/"
-  url "https://downloads.yugabyte.com/yugabyte-2.0.3.0-linux.tar.gz"
-  sha256 "aebb55c60a8a67abac0b5cd06b2cb4dcccfbafdfa878f0d7fd32ca3d7f92285b"
+  url "https://downloads.yugabyte.com/yugabyte-2.0.5.2-linux.tar.gz"
+  sha256 "2506c70b05ac742a75922fc51407732150192e82d1e55adc47df85ec75a2ed09"
 
   bottle :unneeded
   depends_on "glibc"
@@ -10,12 +10,12 @@ class Yugabyte < Formula
   depends_on "patchelf"
   def install
     libexec.install Dir["*"]
-    bin.install_symlink libexec/"yugabyte-db"
+    bin.install_symlink libexec/"yugabyted"
     bin.install_symlink libexec/"bin/ysqlsh"
-    bin.install_symlink libexec/"bin/ycqlsh"
+    bin.install_symlink libexec/"bin/cqlsh"
   end
 
   test do
-    system "#{prefix}/yugabyte-db", "help"
+    system "#{prefix}/yugabyted", "help"
   end
 end
