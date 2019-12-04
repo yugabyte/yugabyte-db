2db665656b0163ea32628ce0a75d6aef438941cb
Ram Sri <ram@yugabyte.com>
2019-12-03 17:41:08 -0800

    [3057] Add run_query endpoint at universe level
    Summary:
    - Add a endpoint at universe level to run PSQL queries
    - Added tests
    - Gate this endpoint to only work with OSS version
    
    Test Plan:
    - start yugaware
    - hit /api/v1/login
    - hit /api/v1/customers/<uuid>/security and set level to insecure (to mimic OSS)
    - import a local universe manually
    - hit /api/v1/customers/<uuid>/universe/<uuid>/run_query with different payloads
    `{
      "query": "select * from products where id in (174, 45)",
      "db_name": "yb_demo_retail"
    }
    `
    and validated.
    
    Reviewers: andrew, sanketh, wesley
    
    Reviewed By: sanketh, wesley
    
    Subscribers: jenkins-bot, yugaware
    
    Differential Revision: https://phabricator.dev.yugabyte.com/D7649

--- managed/src/main/java/Module.java
+++ managed/src/main/java/Module.java
@@ -38,6 +38,7 @@ public class Module extends AbstractModule {
     bind(ConfigHelper.class).asEagerSingleton();
     // Set LocalClientService as the implementation for YBClientService
     bind(YBClientService.class).to(LocalYBClientService.class);
+    bind(QueryExecutor.class).asEagerSingleton();
 
     // We only needed to bind below ones for Platform mode.
     if (config.getString("yb.mode", "PLATFORM").equals("PLATFORM")) {
