yugabyte/yugabyte-db

commit 1984852f8aac33bed704e6995e85f7581f0c257f

[#3362] Expose percentile metrics from histograms.
Summary:
Expose percentile metrics from histograms. (p50, p95, p99, mean & max)
Reset Percentiles information on each /metrics or /prometheus-metrics endpoint pull.

To prevent explosion of the keys, define a new type of Histogram for metrics where we want the percentile information exposed.
This is enabled for a few selected RPC metrics at the "server" level.

Test Plan:
    Test for percentiles

Reviewers: hector, kannan, bogdan

Reviewed By: bogdan

Subscribers: ybase

Differential Revision: https://phabricator.dev.yugabyte.com/D7821

src/yb/util/hdr_histogram.cc
@@ -60,6 +60,7 @@ HdrHistogram::HdrHistogram(uint64_t highest_trackable_value, int num_significant
     sub_bucket_half_count_(0),
     sub_bucket_mask_(0),
     total_count_(0),
+    total_count_in_buckets_(0),
     total_sum_(0),
     min_value_(std::numeric_limits<Atomic64>::max()),
     max_value_(0),
@@ -77,6 +78,7 @@ HdrHistogram::HdrHistogram(const HdrHistogram& other)
     sub_bucket_half_count_(0),
     sub_bucket_mask_(0),
     total_count_(0),
+    total_count_in_buckets_(0),
     total_sum_(0),
     min_value_(std::numeric_limits<Atomic64>::max()),
     max_value_(0),
@@ -98,7 +100,18 @@ HdrHistogram::HdrHistogram(const HdrHistogram& other)
   // Copy the max observed value last.
   NoBarrier_Store(&max_value_, NoBarrier_Load(&other.max_value_));
   // We must ensure the total is consistent with the copied counts.
-  NoBarrier_Store(&total_count_, total_copied_count);
+  NoBarrier_Store(&total_count_, NoBarrier_Load(&other.total_count_));
+  NoBarrier_Store(&total_count_in_buckets_, total_copied_count);
+}
+
+void HdrHistogram::ResetPercentiles() {
+  for (int i = 0; i < counts_array_length_; i++) {
+    NoBarrier_Store(&counts_[i], 0);
+  }
+  NoBarrier_Store(&total_count_in_buckets_, 0);
+
+  NoBarrier_Store(&min_value_, std::numeric_limits<Atomic64>::max());
+  NoBarrier_Store(&max_value_, 0);
 }
 
 bool HdrHistogram::IsValidHighestTrackableValue(uint64_t highest_trackable_value) {
@@ -172,6 +185,7 @@ void HdrHistogram::IncrementBy(int64_t value, int64_t count) {
   // Increment bucket, total, and sum.
   NoBarrier_AtomicIncrement(&counts_[counts_index], count);
   NoBarrier_AtomicIncrement(&total_count_, count);
+  NoBarrier_AtomicIncrement(&total_count_in_buckets_, count);
   NoBarrier_AtomicIncrement(&total_sum_, value * count);
 
   // Update min, if needed.
@@ -288,12 +302,16 @@ bool HdrHistogram::ValuesAreEquivalent(uint64_t value1, uint64_t value2) const {
 }
 
 uint64_t HdrHistogram::MinValue() const {
-  if (PREDICT_FALSE(TotalCount() == 0)) return 0;
+  if (PREDICT_FALSE(TotalCountInBuckets() == 0)) {
+    return 0;
+  }
   return NoBarrier_Load(&min_value_);
 }
 
 uint64_t HdrHistogram::MaxValue() const {
-  if (PREDICT_FALSE(TotalCount() == 0)) return 0;
+  if (PREDICT_FALSE(TotalCountInBuckets() == 0)) {
+    return 0;
+  }
   return NoBarrier_Load(&max_value_);
 }
 
@@ -304,7 +322,7 @@ double HdrHistogram::MeanValue() const {
 }
 
 uint64_t HdrHistogram::ValueAtPercentile(double percentile) const {
-  uint64_t count = TotalCount();
+  uint64_t count = TotalCountInBuckets();
   if (PREDICT_FALSE(count == 0)) return 0;
 
   double requested_percentile = std::min(percentile, 100.0); // Truncate down to 100%
@@ -330,9 +348,10 @@ uint64_t HdrHistogram::ValueAtPercentile(double percentile) const {
 }
 
 void HdrHistogram::DumpHumanReadable(std::ostream* out) const {
-  *out << "Count: " << TotalCount() << endl;
+  *out << "Total Count: " << TotalCount() << endl;
   *out << "Mean: " << MeanValue() << endl;
   *out << "Percentiles:" << endl;
+  *out << "CountInBuckets: " << TotalCountInBuckets() << endl;
   *out << "   0%  (min) = " << MinValue() << endl;
   *out << "  25%        = " << ValueAtPercentile(25) << endl;
   *out << "  50%  (med) = " << ValueAtPercentile(50) << endl;

src/yb/rpc/connection.cc
@@ -62,7 +62,7 @@ using strings::Substitute;
 DEFINE_uint64(rpc_connection_timeout_ms, yb::NonTsanVsTsan(15000, 30000),
     "Timeout for RPC connection operations");
 
-METRIC_DEFINE_histogram(
+METRIC_DEFINE_histogram_with_percentiles(
     server, handler_latency_outbound_transfer, "Time taken to transfer the response ",
     yb::MetricUnit::kMicroseconds, "Microseconds spent to queue and write the response to the wire",
     60000000LU, 2);

src/yb/rpc/protoc-gen-yrpc.cc
@@ -469,7 +469,8 @@ class CodeGenerator : public ::google::protobuf::compiler::CodeGenerator {
         const MethodDescriptor *method = service->method(method_idx);
         subs->PushMethod(method);
         Print(printer, *subs,
-          "METRIC_DEFINE_histogram(server, handler_latency_$rpc_full_name_plainchars$,\n"
+          "METRIC_DEFINE_histogram_with_percentiles(server,"
+          " handler_latency_$rpc_full_name_plainchars$,\n"
           "  \"$rpc_full_name$ RPC Time\",\n"
           "  yb::MetricUnit::kMicroseconds,\n"
           "  \"Microseconds spent handling $rpc_full_name$() RPC requests\",\n"

src/yb/util/hdr_histogram.h
@@ -116,6 +116,11 @@ class HdrHistogram {
   // Count of all events recorded.
   uint64_t TotalCount() const { return base::subtle::NoBarrier_Load(&total_count_); }
 
+  // Count of all events recorded in buckets. Resets to 0 after ResetPercentiles.
+  uint64_t TotalCountInBuckets() const {
+    return base::subtle::NoBarrier_Load(&total_count_in_buckets_);
+  }
+
   // Sum of all events recorded.
   uint64_t TotalSum() const { return base::subtle::NoBarrier_Load(&total_sum_); }
 
@@ -168,6 +173,10 @@ class HdrHistogram {
   // This is a percentile in percents, i.e. 99.99 percentile.
   uint64_t ValueAtPercentile(double percentile) const;
 
+  // Resets the counts_ array to reset the percentiles information.
+  // Preserves the values for TotalSum and TotalCount.
+  void ResetPercentiles();
+
   // Get the percentile at a given value
   // TODO: implement
   // double PercentileAtOrBelowValue(uint64_t value) const;
@@ -203,6 +212,7 @@ class HdrHistogram {
 
   // Also hot.
   base::subtle::Atomic64 total_count_;
+  base::subtle::Atomic64 total_count_in_buckets_;
   base::subtle::Atomic64 total_sum_;
   base::subtle::Atomic64 min_value_;
   base::subtle::Atomic64 max_value_;

src/yb/util/metrics-test.cc
@@ -41,6 +41,7 @@
 #include "yb/gutil/bind.h"
 #include "yb/gutil/map-util.h"
 #include "yb/util/hdr_histogram.h"
+#include "yb/util/histogram.pb.h"
 #include "yb/util/jsonreader.h"
 #include "yb/util/jsonwriter.h"
 #include "yb/util/metrics.h"
@@ -180,6 +181,46 @@ TEST_F(MetricsTest, SimpleHistogramTest) {
   ASSERT_EQ(2, hist->histogram_->TotalCount());
   ASSERT_EQ(6, hist->histogram_->TotalSum());
   // TODO: Test coverage needs to be improved a lot.
+}
+
+TEST_F(MetricsTest, ResetHistogramTest) {
+  scoped_refptr<Histogram> hist = METRIC_test_hist.Instantiate(entity_);
+  for (int i = 1; i <= 100; i++) {
+    hist->Increment(i);
+  }
+  EXPECT_EQ(5050, hist->histogram_->TotalSum());
+  EXPECT_EQ(100, hist->histogram_->TotalCount());
+  EXPECT_EQ(50.5, hist->histogram_->MeanValue());
+
+  EXPECT_EQ(1, hist->histogram_->MinValue());
+  EXPECT_EQ(100, hist->histogram_->MaxValue());
+  EXPECT_EQ(10, hist->histogram_->ValueAtPercentile(10));
+  EXPECT_EQ(25, hist->histogram_->ValueAtPercentile(25));
+  EXPECT_EQ(50, hist->histogram_->ValueAtPercentile(50));
+  EXPECT_EQ(75, hist->histogram_->ValueAtPercentile(75));
+  EXPECT_EQ(99, hist->histogram_->ValueAtPercentile(99));
+  EXPECT_EQ(100, hist->histogram_->ValueAtPercentile(99.9));
+  EXPECT_EQ(100, hist->histogram_->ValueAtPercentile(100));
+
+  hist->histogram_->DumpHumanReadable(&LOG(INFO));
+  // Test that the Histogram's percentiles are reset.
+  HistogramSnapshotPB snapshot_pb;
+  ASSERT_OK(hist->GetAndResetHistogramSnapshotPB(&snapshot_pb, MetricJsonOptions()));
+  hist->histogram_->DumpHumanReadable(&LOG(INFO));
+
+  EXPECT_EQ(5050, hist->histogram_->TotalSum());
+  EXPECT_EQ(100, hist->histogram_->TotalCount());
+  EXPECT_EQ(50.5, hist->histogram_->MeanValue());
+
+  EXPECT_EQ(0, hist->histogram_->MinValue());
+  EXPECT_EQ(0, hist->histogram_->MaxValue());
+  EXPECT_EQ(0, hist->histogram_->ValueAtPercentile(10));
+  EXPECT_EQ(0, hist->histogram_->ValueAtPercentile(25));
+  EXPECT_EQ(0, hist->histogram_->ValueAtPercentile(50));
+  EXPECT_EQ(0, hist->histogram_->ValueAtPercentile(75));
+  EXPECT_EQ(0, hist->histogram_->ValueAtPercentile(99));
+  EXPECT_EQ(0, hist->histogram_->ValueAtPercentile(99.9));
+  EXPECT_EQ(0, hist->histogram_->ValueAtPercentile(100));
 }
 
 TEST_F(MetricsTest, JsonPrintTest) {

src/yb/util/metrics.cc
@@ -62,6 +62,9 @@ TAG_FLAG(metrics_retirement_age_ms, advanced);
 DEFINE_string(metric_node_name, "DEFAULT_NODE_NAME",
               "Value to use as node name for metrics reporting");
 
+DEFINE_bool(expose_metric_histogram_percentiles, true,
+            "Should we expose the percentiles information for metrics histograms.");
+
 // Process/server-wide metrics should go into the 'server' entity.
 // More complex applications will define other entities.
 METRIC_DEFINE_entity(server);
@@ -728,10 +731,12 @@ CHECKED_STATUS Counter::WriteForPrometheus(
 /////////////////////////////////////////////////
 
 HistogramPrototype::HistogramPrototype(const MetricPrototype::CtorArgs& args,
-                                       uint64_t max_trackable_value, int num_sig_digits)
+                                       uint64_t max_trackable_value, int num_sig_digits,
+                                       ExportPercentiles export_percentiles)
   : MetricPrototype(args),
     max_trackable_value_(max_trackable_value),
-    num_sig_digits_(num_sig_digits) {
+    num_sig_digits_(num_sig_digits),
+    export_percentiles_(export_percentiles) {
   // Better to crash at definition time that at instantiation time.
   CHECK(HdrHistogram::IsValidHighestTrackableValue(max_trackable_value))
       << Substitute("Invalid max trackable value on histogram $0: $1",
@@ -752,7 +757,8 @@ scoped_refptr<Histogram> HistogramPrototype::Instantiate(
 
 Histogram::Histogram(const HistogramPrototype* proto)
   : Metric(proto),
-    histogram_(new HdrHistogram(proto->max_trackable_value(), proto->num_sig_digits())) {
+    histogram_(new HdrHistogram(proto->max_trackable_value(), proto->num_sig_digits())),
+    export_percentiles_(proto->export_percentiles()) {
 }
 
 void Histogram::Increment(int64_t value) {
@@ -767,7 +773,7 @@ Status Histogram::WriteAsJson(JsonWriter* writer,
                               const MetricJsonOptions& opts) const {
 
   HistogramSnapshotPB snapshot;
-  RETURN_NOT_OK(GetHistogramSnapshotPB(&snapshot, opts));
+  RETURN_NOT_OK(GetAndResetHistogramSnapshotPB(&snapshot, opts));
   writer->Protobuf(snapshot);
   return Status::OK();
 }
@@ -775,6 +781,11 @@ Status Histogram::WriteAsJson(JsonWriter* writer,
 CHECKED_STATUS Histogram::WriteForPrometheus(
     PrometheusWriter* writer, const MetricEntity::AttributeMap& attr) const {
   HdrHistogram snapshot(*histogram_);
+  // HdrHistogram reports percentiles based on all the data points from the
+  // begining of time. We are interested in the percentiles based on just
+  // the "newly-arrived" data. So, we will reset the histogram's percentiles
+  // between each invocation.
+  histogram_->ResetPercentiles();
 
   // Representing the sum and count require suffixed names.
   std::string hist_name = prototype_->name();
@@ -783,31 +794,37 @@ CHECKED_STATUS Histogram::WriteForPrometheus(
         copy_of_attr, hist_name + "_sum", snapshot.TotalSum()));
   RETURN_NOT_OK(writer->WriteSingleEntry(
         copy_of_attr, hist_name + "_count", snapshot.TotalCount()));
-  /*
+
   // Copy the label map to add the quatiles.
-  copy_of_attr["quantile"] = "0.75";
-  RETURN_NOT_OK(writer->WriteSingleEntry(
-        copy_of_attr, hist_name, snapshot.ValueAtPercentile(75)));
-  copy_of_attr["quantile"] = "0.95";
-  RETURN_NOT_OK(writer->WriteSingleEntry(
-        copy_of_attr, hist_name, snapshot.ValueAtPercentile(95)));
-  copy_of_attr["quantile"] = "0.99";
-  RETURN_NOT_OK(writer->WriteSingleEntry(
-        copy_of_attr, hist_name, snapshot.ValueAtPercentile(99)));
-  copy_of_attr["quantile"] = "0.999";
-  RETURN_NOT_OK(writer->WriteSingleEntry(
-        copy_of_attr, hist_name, snapshot.ValueAtPercentile(99.9)));
-  copy_of_attr["quantile"] = "0.9999";
-  RETURN_NOT_OK(writer->WriteSingleEntry(
-        copy_of_attr, hist_name, snapshot.ValueAtPercentile(99.99)));
-  // TODO: add min/max/mean?
-  */
+  if (export_percentiles_ && FLAGS_expose_metric_histogram_percentiles) {
+    copy_of_attr["quantile"] = "p50";
+    RETURN_NOT_OK(writer->WriteSingleEntry(copy_of_attr, hist_name,
+                                           snapshot.ValueAtPercentile(50)));
+    copy_of_attr["quantile"] = "p95";
+    RETURN_NOT_OK(writer->WriteSingleEntry(copy_of_attr, hist_name,
+                                           snapshot.ValueAtPercentile(95)));
+    copy_of_attr["quantile"] = "p99";
+    RETURN_NOT_OK(writer->WriteSingleEntry(copy_of_attr, hist_name,
+                                           snapshot.ValueAtPercentile(99)));
+    copy_of_attr["quantile"] = "mean";
+    RETURN_NOT_OK(writer->WriteSingleEntry(copy_of_attr, hist_name,
+                                           snapshot.MeanValue()));
+    copy_of_attr["quantile"] = "max";
+    RETURN_NOT_OK(writer->WriteSingleEntry(copy_of_attr, hist_name,
+                                           snapshot.MaxValue()));
+  }
   return Status::OK();
 }
 
-Status Histogram::GetHistogramSnapshotPB(HistogramSnapshotPB* snapshot_pb,
-                                         const MetricJsonOptions& opts) const {
+Status Histogram::GetAndResetHistogramSnapshotPB(HistogramSnapshotPB* snapshot_pb,
+                                                 const MetricJsonOptions& opts) const {
   HdrHistogram snapshot(*histogram_);
+  // HdrHistogram reports percentiles based on all the data points from the
+  // begining of time. We are interested in the percentiles based on just
+  // the "newly-arrived" data. So, we will reset the histogram's percentiles
+  // between each invocation.
+  histogram_->ResetPercentiles();
+
   snapshot_pb->set_name(prototype_->name());
   if (opts.include_schema_info) {
     snapshot_pb->set_type(MetricType::Name(prototype_->type()));

src/yb/util/metrics.h
@@ -313,15 +313,21 @@
 #define METRIC_DEFINE_gauge_double(entity, name, label, unit, desc, ...) \
     METRIC_DEFINE_gauge(double, entity, name, label, unit, desc, ## __VA_ARGS__)
 
-#define METRIC_DEFINE_histogram(entity, name, label, unit, desc, max_val, num_sig_digits) \
-  ::yb::HistogramPrototype BOOST_PP_CAT(METRIC_, name)(                                   \
-      ::yb::MetricPrototype::CtorArgs(BOOST_PP_STRINGIZE(entity), \
-                                      BOOST_PP_STRINGIZE(name), \
-                                      label, \
-                                      unit, \
-                                      desc), \
-      max_val, \
-      num_sig_digits)
+#define METRIC_DEFINE_histogram_with_percentiles(                              \
+    entity, name, label, unit, desc, max_val, num_sig_digits)                  \
+  ::yb::HistogramPrototype BOOST_PP_CAT(METRIC_, name)(                        \
+      ::yb::MetricPrototype::CtorArgs(BOOST_PP_STRINGIZE(entity),              \
+                                      BOOST_PP_STRINGIZE(name), label, unit,   \
+                                      desc),                                   \
+      max_val, num_sig_digits, yb::ExportPercentiles::kTrue)
+
+#define METRIC_DEFINE_histogram(entity, name, label, unit, desc, max_val,      \
+                                num_sig_digits)                                \
+  ::yb::HistogramPrototype BOOST_PP_CAT(METRIC_, name)(                        \
+      ::yb::MetricPrototype::CtorArgs(BOOST_PP_STRINGIZE(entity),              \
+                                      BOOST_PP_STRINGIZE(name), label, unit,   \
+                                      desc),                                   \
+      max_val, num_sig_digits, yb::ExportPercentiles::kFalse)
 
 // The following macros act as forward declarations for entity types and metric prototypes.
 #define METRIC_DECLARE_entity(name) \
@@ -1208,19 +1214,24 @@ inline void IncrementCounter(const scoped_refptr<Counter>& counter) {
   }
 }
 
+YB_STRONGLY_TYPED_BOOL(ExportPercentiles);
+
 class HistogramPrototype : public MetricPrototype {
  public:
   HistogramPrototype(const MetricPrototype::CtorArgs& args,
-                     uint64_t max_trackable_value, int num_sig_digits);
+                     uint64_t max_trackable_value, int num_sig_digits,
+                     ExportPercentiles export_percentiles = ExportPercentiles::kFalse);
   scoped_refptr<Histogram> Instantiate(const scoped_refptr<MetricEntity>& entity);
 
   uint64_t max_trackable_value() const { return max_trackable_value_; }
   int num_sig_digits() const { return num_sig_digits_; }
+  ExportPercentiles export_percentiles() const { return export_percentiles_; }
   virtual MetricType::Type type() const override { return MetricType::kHistogram; }
 
  private:
   const uint64_t max_trackable_value_;
   const int num_sig_digits_;
+  const ExportPercentiles export_percentiles_;
   DISALLOW_COPY_AND_ASSIGN(HistogramPrototype);
 };
 
@@ -1245,7 +1256,8 @@ class Histogram : public Metric {
       PrometheusWriter* writer, const MetricEntity::AttributeMap& attr) const override;
 
   // Returns a snapshot of this histogram including the bucketed values and counts.
-  CHECKED_STATUS GetHistogramSnapshotPB(HistogramSnapshotPB* snapshot,
+  // Resets the bucketed counts, but not the total count/sum.
+  CHECKED_STATUS GetAndResetHistogramSnapshotPB(HistogramSnapshotPB* snapshot,
                                 const MetricJsonOptions& opts) const;
 
 
@@ -1260,10 +1272,12 @@ class Histogram : public Metric {
 
  private:
   FRIEND_TEST(MetricsTest, SimpleHistogramTest);
+  FRIEND_TEST(MetricsTest, ResetHistogramTest);
   friend class MetricEntity;
   explicit Histogram(const HistogramPrototype* proto);
 
   const gscoped_ptr<HdrHistogram> histogram_;
+  const ExportPercentiles export_percentiles_;
   DISALLOW_COPY_AND_ASSIGN(Histogram);
 };
 

src/yb/yql/cql/cqlserver/cql_processor.cc
@@ -27,33 +27,33 @@
 
 #include "yb/yql/cql/cqlserver/cql_service.h"
 
-METRIC_DEFINE_histogram(
+METRIC_DEFINE_histogram_with_percentiles(
     server, handler_latency_yb_cqlserver_CQLServerService_GetProcessor,
     "Time spent to get a processor for processing a CQL query request.",
     yb::MetricUnit::kMicroseconds,
     "Time spent to get a processor for processing a CQL query request.", 60000000LU, 2);
-METRIC_DEFINE_histogram(
+METRIC_DEFINE_histogram_with_percentiles(
     server, handler_latency_yb_cqlserver_CQLServerService_ProcessRequest,
     "Time spent processing a CQL query request. From parsing till executing",
     yb::MetricUnit::kMicroseconds,
     "Time spent processing a CQL query request. From parsing till executing", 60000000LU, 2);
-METRIC_DEFINE_histogram(
+METRIC_DEFINE_histogram_with_percentiles(
     server, handler_latency_yb_cqlserver_CQLServerService_ParseRequest,
     "Time spent parsing CQL query request", yb::MetricUnit::kMicroseconds,
     "Time spent parsing CQL query request", 60000000LU, 2);
-METRIC_DEFINE_histogram(
+METRIC_DEFINE_histogram_with_percentiles(
     server, handler_latency_yb_cqlserver_CQLServerService_QueueResponse,
     "Time spent to queue the response for a CQL query request back on the network",
     yb::MetricUnit::kMicroseconds,
     "Time spent after computing the CQL response to queue it onto the connection.", 60000000LU, 2);
-METRIC_DEFINE_histogram(
+METRIC_DEFINE_histogram_with_percentiles(
     server, handler_latency_yb_cqlserver_CQLServerService_ExecuteRequest,
     "Time spent executing the CQL query request in the handler", yb::MetricUnit::kMicroseconds,
     "Time spent executing the CQL query request in the handler", 60000000LU, 2);
 METRIC_DEFINE_counter(
     server, yb_cqlserver_CQLServerService_ParsingErrors, "Errors encountered when parsing ",
     yb::MetricUnit::kRequests, "Errors encountered when parsing ");
-METRIC_DEFINE_histogram(
+METRIC_DEFINE_histogram_with_percentiles(
     server, handler_latency_yb_cqlserver_CQLServerService_Any,
     "yb.cqlserver.CQLServerService.AnyMethod RPC Time", yb::MetricUnit::kMicroseconds,
     "Microseconds spent handling "

commit 85005d342a24fdde7d1eab59862da6c817001403

[#3362] Fix metrics error after resetting percentiles.
Summary:
Fix metrics error after resetting percentiles.
Caused by D7821

Test Plan:
ybd --cxx-test cassandra_cpp_driver-test --gtest_filter CppCassandraDriverTest.TestTableCreateIndex
Ensure that there are no log lines of the form
`[ts-1] W0125 01:16:32.928529  7378 metrics.cc:276] Failed to write log_entry_batches_per_group as JSON: Illegal state (yb/util/hdr_histogram.cc:435): Histogram array index out of bounds while traversing
`

02:32 $ grep -c metrics.cc:276 /n/users/amitanand/code/yugabyte-db/build/debug-gcc-dynamic/yb-test-logs/tests-integration-tests__cassandra_cpp_driver-test/CppCassandraDriverTest_TestTableCreateIndex.log
0
dev-server-amitanand:...TestTableCreateIndex/2020-01-25T00_57_23

Reviewers: neha, bogdan, kannan

Reviewed By: kannan

Subscribers: ybase

Differential Revision: https://phabricator.dev.yugabyte.com/D7844

src/yb/util/hdr_histogram.cc
@@ -373,7 +373,7 @@ void HdrHistogram::DumpHumanReadable(std::ostream* out) const {
 AbstractHistogramIterator::AbstractHistogramIterator(const HdrHistogram* histogram)
   : histogram_(CHECK_NOTNULL(histogram)),
     cur_iter_val_(),
-    histogram_total_count_(histogram_->TotalCount()),
+    histogram_total_count_(histogram_->TotalCountInBuckets()),
     current_bucket_index_(0),
     current_sub_bucket_index_(0),
     current_value_at_index_(0),
@@ -393,7 +393,7 @@ bool AbstractHistogramIterator::HasNext() const {
 }
 
 Status AbstractHistogramIterator::Next(HistogramIterationValue* value) {
-  if (histogram_->TotalCount() != histogram_total_count_) {
+  if (histogram_->TotalCountInBuckets() != histogram_total_count_) {
     return STATUS(IllegalState, "Concurrently modified histogram while traversing it");
   }
 