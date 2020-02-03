yugabyte/yugabyte-db

commit 0c31601c0b9e109ab6a61d063a4a2b9dcbf1c486

[#2186] Check transaction status after load
Summary:
After a tablet server restarts, the transaction participant loads
metadata for running transactions from RocksDB.  But, usually, most of
them are already completed (aborted/committed).  So there is a good
reason to request their statuses and remove aborted transactions.

In this diff, we add a background task that checks the status of all
loaded transactions in a TransactionParticipant. This status checking
process is started after both of the following happens: (1) all
transactions are loaded, and (2) the TransactionParticipant::Start()
function is called. This background status check is effectively
"single-threaded", with one outstanding RPC to a transaction coordinator
at a time. These status check RPCs request statuses of up to
FLAGS_max_transactions_in_status_request transactions at a time, and
we're adding support for batch-querying transaction statuses in this
diff. Once all statuses are requested from one status tablet, we move on
to other status tablets until we exhaust all per-status-tablet queues.

Test Plan: ybd debug --gtest_filter PgMiniTest.BulkCopyWithRestart -n 20

Reviewers: timur, bogdan, mikhail

Reviewed By: mikhail

Subscribers: ybase

Differential Revision: https://phabricator.dev.yugabyte.com/D7638

src/yb/tablet/tablet.cc
@@ -1498,11 +1498,6 @@ HybridTime Tablet::ApplierSafeTime(HybridTime min_allowed, CoarseTimePoint deadl
   return SafeTime(RequireLease::kFalse, min_allowed, deadline);
 }
 
-HybridTime Tablet::ApplierSafeTimeForFollower() {
-  return mvcc_.SafeTimeForFollower(
-      /* min_allowed= */ HybridTime::kMin, /* deadline= */ CoarseTimePoint::min());
-}
-
 Status Tablet::CreatePreparedChangeMetadata(ChangeMetadataOperationState *operation_state,
                                             const Schema* schema) {
   if (schema) {

src/yb/tablet/tablet.h
@@ -223,8 +223,6 @@ class Tablet : public AbstractTablet, public TransactionIntentApplier {
 
   HybridTime ApplierSafeTime(HybridTime min_allowed, CoarseTimePoint deadline) override;
 
-  HybridTime ApplierSafeTimeForFollower() override;
-
   // Finish the Prepare phase of a write transaction.
   //
   // Starts an MVCC transaction and assigns a timestamp for the transaction.

src/yb/tablet/tablet_peer.cc
@@ -275,6 +275,10 @@ Status TabletPeer::InitTabletPeer(const shared_ptr<TabletClass> &tablet,
     tablet_->transaction_coordinator()->Start();
   }
 
+  if (tablet_->transaction_participant()) {
+    tablet_->transaction_participant()->Start();
+  }
+
   TRACE("TabletPeer::Init() finished");
   VLOG_WITH_PREFIX(2) << "Peer Initted";
 
@@ -586,6 +590,10 @@ void TabletPeer::SubmitUpdateTransaction(
   Submit(std::move(operation), term);
 }
 
+HybridTime TabletPeer::SafeTimeForTransactionParticipant() {
+  return tablet_->mvcc_manager()->LastReplicatedHybridTime();
+}
+
 void TabletPeer::GetLastReplicatedData(RemoveIntentsData* data) {
   consensus_->GetLastCommittedOpId().ToPB(&data->op_id);
   data->log_ht = tablet_->mvcc_manager()->LastReplicatedHybridTime();
@@ -887,7 +895,8 @@ Status TabletPeer::StartReplicaOperation(
     driver->SetPropagatedSafeTime(propagated_safe_time, tablet_->mvcc_manager());
   }
 
-  if (operation_type == OperationType::kWrite) {
+  if (operation_type == OperationType::kWrite ||
+      operation_type == OperationType::kUpdateTransaction) {
     tablet()->mvcc_manager()->AddPending(&ht);
   }
 

commit 1a1f1c23131961fccfee121f961e09db5591447d

[#3495] Call TransactionParticipant shutdown from Tablet shutdown
Summary:
TransactionParticipant is destroyed when the Tablet is destroyed.  But
we use std::shared_ptr to store the pointer to Tablet from TabletPeer.
And after TabletPeer resets its Tablet pointer, there could be other
references to the Tablet that prevent its destruction.  We could get a
situation when TransactionParticipant is still alive, but the tablet_
field in TabletPeer is nullptr.  It will result in a segmentation fault
when TabletPeer::SafeTimeForTransactionParticipant is called by
TransactionParticipant.

This bug was introduced in D7638 (
https://github.com/yugabyte/yugabyte-db/commit/0c31601c0b9e109ab6a61d063a4a2b9dcbf1c486 ).
And it is possible to set max_transactions_in_status_request flag to 0
to disable this functionality.

Added StartShutdown and CompleteShutdown to TransactionParticipant, and
used it in the Tablet shutdown procedure.  It happens before TabletPeer
resets its Tablet pointer, so we could be sure that no more requests
from TransactionParticipant will happen after the Tablet pointer reset
in TabletPeer.

Test Plan: ybd --gtest_filter SnapshotTxnTest.RemoteBootstrapOnStart

Reviewers: timur, bogdan, mikhail

Reviewed By: mikhail

Subscribers: kannan, alex, ybase

Differential Revision: https://phabricator.dev.yugabyte.com/D7874

src/yb/tablet/tablet.cc
@@ -430,7 +430,12 @@ Tablet::Tablet(
 }
 
 Tablet::~Tablet() {
-  Shutdown();
+  if (StartShutdown()) {
+    CompleteShutdown();
+  } else {
+    LOG_IF_WITH_PREFIX(DFATAL, state_ != kShutdown)
+        << "Destroying Tablet that did not complete shutdown";
+  }
   mem_tracker_->UnregisterFromParent();
 }
 
@@ -751,12 +756,21 @@ void Tablet::MarkFinishedBootstrapping() {
   state_ = kOpen;
 }
 
-void Tablet::SetShutdownRequestedFlag() {
-  shutdown_requested_.store(true, std::memory_order::memory_order_release);
+bool Tablet::StartShutdown() {
+  bool expected = false;
+  if (!shutdown_requested_.compare_exchange_strong(expected, true)) {
+    return false;
+  }
+
+  if (transaction_participant_) {
+    transaction_participant_->StartShutdown();
+  }
+
+  return true;
 }
 
-void Tablet::Shutdown(IsDropTable is_drop_table) {
-  SetShutdownRequestedFlag();
+void Tablet::CompleteShutdown(IsDropTable is_drop_table) {
+  StartShutdown();
 
   auto op_pause = PauseReadWriteOperations();
   if (!op_pause.ok()) {
@@ -768,6 +782,10 @@ void Tablet::Shutdown(IsDropTable is_drop_table) {
 
   if (transaction_coordinator_) {
     transaction_coordinator_->Shutdown();
+  }
+
+  if (transaction_participant_) {
+    transaction_participant_->CompleteShutdown();
   }
 
   std::lock_guard<rw_spinlock> lock(component_lock_);

src/yb/tablet/tablet.h
@@ -221,12 +221,13 @@ class Tablet : public AbstractTablet, public TransactionIntentApplier {
 
   // This can be called to proactively prevent new operations from being handled, even before
   // Shutdown() is called.
-  void SetShutdownRequestedFlag();
+  // Returns true if it was the first call to StartShutdown.
+  bool StartShutdown();
   bool IsShutdownRequested() const {
     return shutdown_requested_.load(std::memory_order::memory_order_acquire);
   }
 
-  void Shutdown(IsDropTable is_drop_table = IsDropTable::kFalse);
+  void CompleteShutdown(IsDropTable is_drop_table = IsDropTable::kFalse);
 
   CHECKED_STATUS ImportData(const std::string& source_dir);
 

src/yb/tablet/tablet_peer.cc
@@ -369,7 +369,7 @@ bool TabletPeer::StartShutdown() {
   {
     std::lock_guard<decltype(lock_)> lock(lock_);
     if (tablet_) {
-      tablet_->SetShutdownRequestedFlag();
+      tablet_->StartShutdown();
     }
   }
 
@@ -434,7 +434,7 @@ void TabletPeer::CompleteShutdown(IsDropTable is_drop_table) {
   VLOG_WITH_PREFIX(1) << "Shut down!";
 
   if (tablet_) {
-    tablet_->Shutdown(is_drop_table);
+    tablet_->CompleteShutdown(is_drop_table);
   }
 
   // Only mark the peer as SHUTDOWN when all other components have shut down.

src/yb/tablet/transaction_participant.cc
@@ -119,11 +119,33 @@ class TransactionParticipant::Impl : public RunningTransactionContext {
   }
 
   ~Impl() {
-    LOG_WITH_PREFIX(INFO) << "Stop";
-    closing_.store(true, std::memory_order_release);
-    transactions_.clear();
-    MinRunningNotifier min_running_notifier(nullptr /* applier */);
-    TransactionsModifiedUnlocked(&min_running_notifier);
+    if (StartShutdown()) {
+      CompleteShutdown();
+    } else {
+      LOG_IF_WITH_PREFIX(DFATAL, !shutdown_done_.load(std::memory_order_acquire))
+          << "Destroying transaction participant that did not complete shutdown";
+    }
+  }
+
+  bool StartShutdown() {
+    bool expected = false;
+    if (!closing_.compare_exchange_strong(expected, true)) {
+      return false;
+    }
+
+    LOG_WITH_PREFIX(INFO) << "Shutdown";
+    return true;
+  }
+
+  void CompleteShutdown() {
+    LOG_IF_WITH_PREFIX(DFATAL, !closing_.load()) << __func__ << " w/o StartShutdown";
+
+    {
+      MinRunningNotifier min_running_notifier(nullptr /* applier */);
+      std::lock_guard<std::mutex> lock(mutex_);
+      transactions_.clear();
+      TransactionsModifiedUnlocked(&min_running_notifier);
+    }
 
     rpcs_.Shutdown();
     if (load_thread_.joinable()) {
@@ -132,6 +154,7 @@ class TransactionParticipant::Impl : public RunningTransactionContext {
     while (checking_status_.load(std::memory_order_acquire)) {
       std::this_thread::sleep_for(10ms);
     }
+    shutdown_done_.store(true, std::memory_order_release);
   }
 
   void Start() {
@@ -1031,12 +1054,16 @@ class TransactionParticipant::Impl : public RunningTransactionContext {
         &check_status_handle_);
   }
 
-  void StatusReceived(const Status& status,
+  void StatusReceived(Status status,
                       const tserver::GetTransactionStatusResponsePB& response,
                       size_t request_size) {
     VLOG_WITH_PREFIX(2) << "Received statuses: " << status << ", " << response.ShortDebugString();
 
     rpcs_.Unregister(&check_status_handle_);
+
+    if (status.ok() && response.has_error()) {
+      status = StatusFromPB(response.error().status());
+    }
 
     if (!status.ok()) {
       LOG_WITH_PREFIX(WARNING) << "Failed to request transaction statuses: " << status;
@@ -1144,6 +1171,7 @@ class TransactionParticipant::Impl : public RunningTransactionContext {
   std::atomic<HybridTime> min_running_ht_{HybridTime::kMax};
   std::atomic<CoarseTimePoint> next_check_min_running_{CoarseTimePoint()};
   HybridTime waiting_for_min_running_ht_ = HybridTime::kMax;
+  std::atomic<bool> shutdown_done_{false};
 };
 
 TransactionParticipant::TransactionParticipant(
@@ -1256,6 +1284,14 @@ OneWayBitmap TransactionParticipant::TEST_TransactionReplicatedBatches(
 std::string TransactionParticipant::ReplicatedData::ToString() const {
   return Format("{ leader_term: $0 state: $1 op_id: $2 hybrid_time: $3 already_applied: $4 }",
                leader_term, state, op_id, hybrid_time, already_applied);
+}
+
+void TransactionParticipant::StartShutdown() {
+  impl_->StartShutdown();
+}
+
+void TransactionParticipant::CompleteShutdown() {
+  impl_->CompleteShutdown();
 }
 
 } // namespace tablet

src/yb/tablet/transaction_participant.h
@@ -202,6 +202,10 @@ class TransactionParticipant : public TransactionStatusManager {
   // method `MinRunningHybridTimeSatisfied` will be invoked.
   void WaitMinRunningHybridTime(HybridTime ht);
 
+  void StartShutdown();
+
+  void CompleteShutdown();
+
   size_t TEST_GetNumRunningTransactions() const;
 
   // Returns pair of number of intents and number of transactions.

src/yb/client/snapshot-txn-test.cc
@@ -18,6 +18,8 @@
 
 #include "yb/common/ql_value.h"
 
+#include "yb/consensus/raft_consensus.h"
+
 #include "yb/tserver/mini_tablet_server.h"
 #include "yb/tserver/tablet_server.h"
 
@@ -34,6 +36,8 @@ using namespace std::literals;
 DECLARE_bool(ycql_consistent_transactional_paging);
 DECLARE_uint64(max_clock_skew_usec);
 DECLARE_int32(inject_load_transaction_delay_ms);
+DECLARE_int32(log_min_seconds_to_retain);
+DECLARE_uint64(max_transactions_in_status_request);
 
 namespace yb {
 namespace client {
@@ -677,6 +681,71 @@ TEST_F(SnapshotTxnTest, MultiWriteWithRestart) {
   }
 
   ASSERT_GE(good_keys.load(std::memory_order_relaxed), key.load(std::memory_order_relaxed) * 0.8);
+}
+
+using RemoteBootstrapOnStartBase = TransactionCustomLogSegmentSizeTest<128, SnapshotTxnTest>;
+
+TEST_F_EX(SnapshotTxnTest, RemoteBootstrapOnStart, RemoteBootstrapOnStartBase) {
+  constexpr int kTransactionsCount = RegularBuildVsSanitizers(100, 10);
+  FLAGS_log_min_seconds_to_retain = 1;
+  DisableTransactionTimeout();
+
+  for (int iteration = 0; iteration != 4; ++iteration) {
+    DisableApplyingIntents();
+
+    TestThreadHolder thread_holder;
+
+    std::atomic<int> transactions(0);
+
+    thread_holder.AddThreadFunctor(
+        [this, &stop = thread_holder.stop_flag(), &transactions] {
+      auto session = CreateSession();
+      for (int transaction_idx = 0; !stop.load(std::memory_order_acquire); ++transaction_idx) {
+        auto txn = CreateTransaction();
+        session->SetTransaction(txn);
+        WriteRows(session, transaction_idx);
+        if (txn->CommitFuture().get().ok()) {
+          transactions.fetch_add(1);
+        }
+      }
+    });
+
+    while (transactions.load(std::memory_order_acquire) < kTransactionsCount) {
+      std::this_thread::sleep_for(100ms);
+    }
+
+    cluster_->mini_tablet_server(0)->Shutdown();
+
+    SetIgnoreApplyingProbability(0.0);
+
+    std::this_thread::sleep_for(FLAGS_log_min_seconds_to_retain * 1s);
+
+    auto start_transactions = transactions.load(std::memory_order_acquire);
+    while (transactions.load(std::memory_order_acquire) < start_transactions + kTransactionsCount) {
+      std::this_thread::sleep_for(100ms);
+    }
+
+    thread_holder.Stop();
+
+    LOG(INFO) << "Flushing";
+    ASSERT_OK(cluster_->FlushTablets());
+
+    LOG(INFO) << "Clean logs";
+    ASSERT_OK(cluster_->CleanTabletLogs());
+
+    // Shutdown to reset cached logs.
+    for (int i = 1; i != cluster_->num_tablet_servers(); ++i) {
+      cluster_->mini_tablet_server(i)->Shutdown();
+    }
+
+    // Start all servers. Cluster verifier should check that all tablets are synchronized.
+    for (int i = 0; i != cluster_->num_tablet_servers(); ++i) {
+      ASSERT_OK(cluster_->mini_tablet_server(i)->Start());
+    }
+
+    ASSERT_OK(WaitFor([this] { return CheckAllTabletsRunning(); }, 20s * kTimeMultiplier,
+                      "All tablets running"));
+  }
 }
 
 } // namespace client

src/yb/tablet/tablet-metadata-test.cc
@@ -83,7 +83,8 @@ TEST_F(TestRaftGroupMetadata, TestLoadFromSuperBlock) {
   ASSERT_OK(harness_->tablet()->Flush(tablet::FlushMode::kSync));
 
   // Shut down the tablet.
-  harness_->tablet()->Shutdown();
+  harness_->tablet()->StartShutdown();
+  harness_->tablet()->CompleteShutdown();
 
   RaftGroupMetadata* meta = harness_->tablet()->metadata();
 