c8a43ee87e3fc077b2fbcedd0e444b16d91032b5
Hector Cuellar <hectorgcr@users.noreply.github.com>
2020-02-11 16:43:03 -0800

    #3418: Persist cdc min replicated index in the superblock
    Summary: Persisting cdc_min_replicated_index to avoid losing this information when a tserver is restarted. Currently, it's possible that a tserver is restarted, the GC runs, and removes log files that are necessary for CDC because the thread that updates this information hasn't had the opportunity to update this information in the Log object.
    
    Test Plan:
    New unit tests:
    TestLogCDCMinReplicatedIndexIsDurable
    TestLogAndMetaCdcIndexAreReset
    
    Reviewers: rahuldesirazu, nicolas, neha
    
    Reviewed By: neha
    
    Subscribers: bogdan, ybase
    
    Differential Revision: https://phabricator.dev.yugabyte.com/D7873

--- src/yb/tablet/tablet_metadata.cc
+++ src/yb/tablet/tablet_metadata.cc
@@ -459,7 +459,8 @@ RaftGroupMetadata::RaftGroupMetadata(FsManager* fs_manager,
       fs_manager_(fs_manager),
       wal_dir_(wal_dir),
       tablet_data_state_(tablet_data_state),
-      colocated_(colocated) {
+      colocated_(colocated),
+      cdc_min_replicated_index_(std::numeric_limits<int64_t>::max()) {
   CHECK(schema.has_column_ids());
   CHECK_GT(schema.num_key_columns(), 0);
   kv_store_.tables.emplace(
@@ -534,6 +535,7 @@ Status RaftGroupMetadata::LoadFromSuperBlock(const RaftGroupReplicaSuperBlockPB&
     } else {
       tombstone_last_logged_opid_ = OpId();
     }
+    cdc_min_replicated_index_ = superblock.cdc_min_replicated_index();
   }
 
   return Status::OK();
@@ -616,6 +618,7 @@ void RaftGroupMetadata::ToSuperBlockUnlocked(RaftGroupReplicaSuperBlockPB* super
 
   pb.set_primary_table_id(primary_table_id_);
   pb.set_colocated(colocated_);
+  pb.set_cdc_min_replicated_index(cdc_min_replicated_index_);
 
   superblock->Swap(&pb);
 }
@@ -752,6 +755,19 @@ uint32_t RaftGroupMetadata::wal_retention_secs() const {
     return 0;
   }
   return it->second->wal_retention_secs;
+}
+
+Status RaftGroupMetadata::set_cdc_min_replicated_index(int64 cdc_min_replicated_index) {
+  {
+    std::lock_guard<MutexType> lock(data_mutex_);
+    cdc_min_replicated_index_ = cdc_min_replicated_index;
+  }
+  return Flush();
+}
+
+int64_t RaftGroupMetadata::cdc_min_replicated_index() const {
+  std::lock_guard<MutexType> lock(data_mutex_);
+  return cdc_min_replicated_index_;
 }
 
 void RaftGroupMetadata::set_tablet_data_state(TabletDataState state) {
