e3d0750a1956c4b763947941aa0eb1f9c7eb4667
Wesley Wang <2791412+WesleyW@users.noreply.github.com>
2020-03-02 13:49:57 -0800

    Include itest-only path to download packages from s3 bucket to nodes
    Summary:
    When creating universe, itest slows down a lot when downloading multiple packages from cross-cloud.
    This creates a path for itest to specify an s3 bucket path to download from.
    This is meant for (Create, Edit, Upgrade) universe on (AWS,onprem) tests.
    No validation is done here to ensure the above is correct - itest should properly handle this.
    
    Itest change in D8063
    Devops change in D8153
    
    Test Plan: create universe and pass in `itestPackagePath`
    
    Reviewers: arnav, bogdan
    
    Reviewed By: bogdan
    
    Subscribers: ram, jenkins-bot, kannan, yugaware
    
    Differential Revision: https://phabricator.dev.yugabyte.com/D8062

--- managed/src/main/java/com/yugabyte/yw/forms/UniverseDefinitionTaskParams.java
+++ managed/src/main/java/com/yugabyte/yw/forms/UniverseDefinitionTaskParams.java
@@ -88,7 +88,7 @@ public class UniverseDefinitionTaskParams extends UniverseTaskParams {
   // reset each time a new operation on the universe starts, and is set at the very end of that
   // operation.
   public boolean updateSucceeded = true;
-  
+
   // The next cluster index to be used when a new read-only cluster is added.
   public int nextClusterIndex = 1;
 
@@ -97,6 +97,9 @@ public class UniverseDefinitionTaskParams extends UniverseTaskParams {
   // but defaults to true since we want universes created through pre-TLS YW to be
   // unaffected.
   public boolean allowInsecure = true;
+
+  // Development flag to download package from s3 bucket.
+  public String itestS3PackagePath = "";
 
   /**
    * Allowed states for an imported universe.
@@ -147,7 +150,7 @@ public class UniverseDefinitionTaskParams extends UniverseTaskParams {
 
     // The placement information computed from the user intent.
     public PlacementInfo placementInfo = null;
-    
+
     // The cluster index by which node names are sorted when shown in UI.
     // This is set internally by the placement util in the server, client should not set it.
     public int index = 0;
