bab48e1cdb45dbe10e8948f8ef33b025f7ac08d5
Andrew Cai <andrew@yugabyte.com>
2020-03-09 13:09:43 -0700

    [#3563][YW] Add sorting by default to the releases list and also a search bar to allow users to quickly access the desired release by entering part of the string.
    Summary:
    Add default sorting to releases list to order list by
    <major>.<minor>.<patch>.<revision>-<tag>
    format. We sort numeric entries before letter entries. So 2.1.0.0-b7 would be sorted before
    andrews-custom-version. Also add a simple version search bar based on substrings.
    
    Test Plan:
    Open versions page and make sure that the version is sorted descending, with major
    version before minor before patch before revision before tag, which is sorted alphabetically.
    
    Reviewers: ram, bogdan, wesley
    
    Reviewed By: wesley
    
    Subscribers: rao, ui
    
    Differential Revision: https://phabricator.dev.yugabyte.com/D8097

--- managed/ui/src/components/releases/ReleaseList/ReleaseList.js
+++ managed/ui/src/components/releases/ReleaseList/ReleaseList.js
@@ -5,7 +5,7 @@ import { BootstrapTable, TableHeaderColumn } from 'react-bootstrap-table';
 import { DropdownButton } from 'react-bootstrap';
 
 import { YBPanelItem } from 'components/panels';
-import { YBButton } from 'components/common/forms/fields';
+import { YBButton, YBTextInput } from 'components/common/forms/fields';
 import { TableAction } from 'components/tables';
 import { YBLoadingCircleIcon } from 'components/common/indicators';
 import { getPromiseState } from 'utils/PromiseUtils';
@@ -14,9 +14,50 @@ import { showOrRedirect } from 'utils/LayoutUtils';
 
 import './ReleaseList.scss';
 
+const versionReg = /^(0|[1-9]\d*)\.(0|[1-9]\d*)\.(0|[1-9]\d*)\.(0|[1-9]\d*)-(\S*)$/;
+// Sort descending
+const sortVersion = (a, b) => {
+  const matchA = versionReg.exec(a);
+  const matchB = versionReg.exec(b);
+
+  // Both are proper version strings
+  if (matchA && matchB) {
+    for (let i = 1; i < matchA.length - 1; i++) {
+      const groupA = matchA[i];
+      const groupB = matchB[i];
+      if (groupA !== groupB) {
+        return parseInt(groupB, 10) - parseInt(groupA, 10);
+      }
+    }
+    const tagA = matchA[5];
+    const tagB = matchB[5];
+    if (tagA !== tagB) {
+      // Sort b{n} tags before custom build tags like mihnea, sergei, or 9b180349b
+      const matchTagA = tagA.match(/b(\d+)/);
+      const matchTagB = tagB.match(/b(\d+)/);
+      if (matchTagA && matchTagB) {
+        return parseInt(matchTagB[1], 10) - parseInt(matchTagA[1], 10);
+      } else if (matchTagA || matchTagB) {
+        return !!matchTagB - !!matchTagA; // Boolean - Boolean returns -1, 0, or 1
+      }
+      return tagA.localeCompare(tagB);
+    }
+    return 0;
+  } else if (matchA || matchB) {
+    return !!matchB - !!matchA; // Boolean - Boolean returns -1, 0, or 1
+  } else {
+    return a.localeCompare(b); // Sort alphabetically ascending
+  }
+};
+
 export default class ReleaseList extends Component {
   static defaultProps = {
     title : "Releases"
+  }
+
+  state = {
+    searchResults: null,
+    searchTerm: ''
   }
 
   componentDidMount() {
@@ -32,15 +73,34 @@ export default class ReleaseList extends Component {
     this.props.getYugaByteReleases();
   }
 
+  onSearchVersions = (term) => {
+    const { releases } = this.props;
+    if (!term) {
+      this.setState({searchResults: null, searchTerm: ''});
+    } else {
+      this.setState({
+        searchResults: Object.keys(releases.data).filter(x => x.indexOf(term) > -1),
+        searchTerm: term
+      });
+    }
+  }
+
   render() {
     const { releases, title, customer: { currentCustomer }} = this.props;
+    const { searchTerm, searchResults } = this.state;
     showOrRedirect(currentCustomer.data.features, "main.releases");
 
     if (getPromiseState(releases).isLoading() ||
         getPromiseState(releases).isInit()) {
       return <YBLoadingCircleIcon size="medium" />;
     }
-    const releaseInfos = Object.keys(releases.data).map((version) => {
+    let releaseStrList = [];
+    if (searchResults != null) {
+      releaseStrList = searchResults;
+    } else {
+      releaseStrList = Object.keys(releases.data).sort(sortVersion);
+    }
+    const releaseInfos = releaseStrList.map((version) => {
       const releaseInfo = releases.data[version];
       releaseInfo.version = version;
       return releaseInfo;
@@ -84,6 +144,9 @@ export default class ReleaseList extends Component {
       <YBPanelItem
         header={
           <div>
+            <div className='pull-left'>
+              <YBTextInput placeHolder="Search versions" value={searchTerm} onValueChanged={this.onSearchVersions} />
+            </div>
             <div className='pull-right'>
               <div className="release-list-action-btn-group">
                 <YBButton btnText={"Refresh"} btnIcon={"fa fa-refresh"}
