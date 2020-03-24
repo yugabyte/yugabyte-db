54f2a28df21f76e2732eb0911284f73b1cee811d
Arnav Agarwal <14933889+Arnav15@users.noreply.github.com>
2020-03-24 19:32:33 +0530

    [#4046] Prevent data move if there is no data to move.
    Summary:
    Currently if we try to remove a node from a 3 node, RF3 cluster, the remove hangs since the
    WaitForDataMove subtask never finishes, as there is no data to move. Therefore, in scenarios where
    the node is being removed from a zone with numNodes == RF in zone, there shouldn't be any waiting
    for the data to move.
    
    Test Plan:
    Added unit tests. Created an RF3 universe and removed a node and verified that the task
    did not hang.
    
    Reviewers: ram, bogdan
    
    Reviewed By: bogdan
    
    Subscribers: jenkins-bot, yugaware
    
    Differential Revision: https://phabricator.dev.yugabyte.com/D8179

--- managed/src/test/java/com/yugabyte/yw/commissioner/tasks/RemoveNodeFromUniverseTest.java
+++ managed/src/test/java/com/yugabyte/yw/commissioner/tasks/RemoveNodeFromUniverseTest.java
@@ -52,21 +52,45 @@ public class RemoveNodeFromUniverseTest extends CommissionerBaseTest {
   YBClient mockClient;
   ShellProcessHandler.ShellResponse dummyShellResponse;
 
-  @Before
-  public void setUp() {
+  public void setUp(boolean withMaster, int numNodes, int replicationFactor, boolean multiZone) {
     super.setUp();
-    Region region = Region.create(defaultProvider, "region-1", "Region 1", "yb-image-1");
-    AvailabilityZone.create(region, "az-1", "AZ 1", "subnet-1");
+    Region region = Region.create(defaultProvider, "test-region", "Region 1", "yb-image-1");
+    AvailabilityZone.create(region, "az-1", "az-1", "subnet-1");
+    if (multiZone) {
+      AvailabilityZone.create(region, "az-2", "az-2", "subnet-2");
+      AvailabilityZone.create(region, "az-3", "az-3", "subnet-3");
+    }
     // create default universe
     userIntent = new UniverseDefinitionTaskParams.UserIntent();
-    userIntent.numNodes = 3;
+    userIntent.numNodes = numNodes;
     userIntent.ybSoftwareVersion = "yb-version";
     userIntent.accessKeyCode = "demo-access";
-    userIntent.replicationFactor = 3;
+    userIntent.replicationFactor = replicationFactor;
     userIntent.regionList = ImmutableList.of(region.uuid);
     defaultUniverse = createUniverse(defaultCustomer.getCustomerId());
     Universe.saveDetails(defaultUniverse.universeUUID,
-        ApiUtils.mockUniverseUpdater(userIntent, false /* setMasters */));
+        ApiUtils.mockUniverseUpdater(userIntent, withMaster /* setMasters */));
+    defaultUniverse = Universe.get(defaultUniverse.universeUUID);
+
+    Universe.UniverseUpdater updater = new Universe.UniverseUpdater() {
+      public void run(Universe universe) {
+        UniverseDefinitionTaskParams universeDetails = universe.getUniverseDetails();
+        Set<NodeDetails> nodes = universeDetails.nodeDetailsSet;
+        int count = 0;
+        int numZones = 1;
+        if (multiZone) {
+          numZones = 3;
+        }
+        for (NodeDetails node : nodes) {
+          node.cloudInfo.az = "az-" + (count % numZones + 1);
+          nodes.add(node);
+        }
+        universeDetails.nodeDetailsSet = nodes;
+        universe.setUniverseDetails(universeDetails);
+      }
+    };
+    Universe.saveDetails(defaultUniverse.universeUUID, updater);
+    defaultUniverse = Universe.get(defaultUniverse.universeUUID);
 
     mockClient = mock(YBClient.class);
 
@@ -162,12 +186,17 @@ public class RemoveNodeFromUniverseTest extends CommissionerBaseTest {
   };
 
   private void assertRemoveNodeSequence(Map<Integer, List<TaskInfo>> subTasksByPosition,
-                                        RemoveType type) {
+                                        RemoveType type, boolean moveData) {
     int position = 0;
+    int taskPosition = 0;
     switch (type) {
       case WITH_MASTER:
         for (TaskType taskType: REMOVE_NODE_WITH_MASTER) {
-          List<TaskInfo> tasks = subTasksByPosition.get(position);
+          if (taskType.equals(TaskType.WaitForDataMove) && !moveData) {
+            position++;
+            continue;
+          }
+          List<TaskInfo> tasks = subTasksByPosition.get(taskPosition);
           assertEquals(1, tasks.size());
           assertEquals(taskType, tasks.get(0).getTaskType());
           JsonNode expectedResults =
@@ -177,6 +206,7 @@ public class RemoveNodeFromUniverseTest extends CommissionerBaseTest {
               .collect(Collectors.toList());
           assertJsonEqual(expectedResults, taskDetails.get(0));
           position++;
+          taskPosition++;
         }
       break;
       case ONLY_TSERVER:
@@ -191,6 +221,7 @@ public class RemoveNodeFromUniverseTest extends CommissionerBaseTest {
               .collect(Collectors.toList());
           assertJsonEqual(expectedResults, taskDetails.get(0));
           position++;
+          taskPosition++;
         }
         break;
       case NOT_EXISTS:
@@ -205,6 +236,7 @@ public class RemoveNodeFromUniverseTest extends CommissionerBaseTest {
               .collect(Collectors.toList());
           assertJsonEqual(expectedResults, taskDetails.get(0));
           position++;
+          taskPosition++;
         }
         break;
     }
@@ -212,62 +244,47 @@ public class RemoveNodeFromUniverseTest extends CommissionerBaseTest {
 
   @Test
   public void testRemoveNodeSuccess() {
+    setUp(false, 4, 3, false);
     NodeTaskParams taskParams = new NodeTaskParams();
     taskParams.universeUUID = defaultUniverse.universeUUID;
-    taskParams.expectedUniverseVersion = 2;
+    taskParams.expectedUniverseVersion = 3;
+
     TaskInfo taskInfo = submitTask(taskParams, "host-n1");
     List<TaskInfo> subTasks = taskInfo.getSubTasks();
     Map<Integer, List<TaskInfo>> subTasksByPosition =
         subTasks.stream().collect(Collectors.groupingBy(w -> w.getPosition()));
-    assertRemoveNodeSequence(subTasksByPosition, RemoveType.ONLY_TSERVER);
+    assertRemoveNodeSequence(subTasksByPosition, RemoveType.ONLY_TSERVER, true);
   }
 
   @Test
   public void testRemoveNodeWithMaster() {
-    Universe universe = createUniverse("RemNodeWithMaster");
-    universe = Universe.saveDetails(universe.universeUUID,
-            ApiUtils.mockUniverseUpdater(userIntent, true /* setMasters */));
-    // Change one of the nodes' state to removed.
-    Universe.UniverseUpdater updater = new Universe.UniverseUpdater() {
-      public void run(Universe universe) {
-        UniverseDefinitionTaskParams universeDetails = universe.getUniverseDetails();
-        Set<NodeDetails> nodes = universeDetails.nodeDetailsSet;
-        for (NodeDetails node : nodes) {
-          if (node.nodeName.equals("host-n1")) {
-            node.isMaster = true;
-            nodes.add(node);
-            break;
-          }
-        }
-        universeDetails.nodeDetailsSet = nodes;
-        universe.setUniverseDetails(universeDetails);
-      }
-    };
-    Universe.saveDetails(universe.universeUUID, updater);
+    setUp(true, 4, 3, false);
     NodeTaskParams taskParams = new NodeTaskParams();
-    taskParams.universeUUID = universe.universeUUID;
+    taskParams.universeUUID = defaultUniverse.universeUUID;
     taskParams.expectedUniverseVersion = 3;
     TaskInfo taskInfo = submitTask(taskParams, "host-n1");
     List<TaskInfo> subTasks = taskInfo.getSubTasks();
     Map<Integer, List<TaskInfo>> subTasksByPosition =
         subTasks.stream().collect(Collectors.groupingBy(w -> w.getPosition()));
-    assertRemoveNodeSequence(subTasksByPosition, RemoveType.WITH_MASTER);
+    assertRemoveNodeSequence(subTasksByPosition, RemoveType.WITH_MASTER, true);
   }
 
   @Test
   public void testRemoveUnknownNode() {
+    setUp(false, 4, 3, false);
     NodeTaskParams taskParams = new NodeTaskParams();
     taskParams.universeUUID = defaultUniverse.universeUUID;
-    taskParams.expectedUniverseVersion = 2;
+    taskParams.expectedUniverseVersion = 3;
     TaskInfo taskInfo = submitTask(taskParams, "host-n9");
     assertEquals(TaskInfo.State.Failure, taskInfo.getTaskState());
   }
 
   @Test
   public void testRemoveNonExistentNode() {
+    setUp(false, 4, 3, false);
     NodeTaskParams taskParams = new NodeTaskParams();
     taskParams.universeUUID = defaultUniverse.universeUUID;
-    taskParams.expectedUniverseVersion = 2;
+    taskParams.expectedUniverseVersion = 3;
     dummyShellResponse = new ShellProcessHandler.ShellResponse();
     dummyShellResponse.message = null;
     when(mockNodeManager.nodeCommand(any(), any())).thenReturn(dummyShellResponse);
@@ -275,6 +292,45 @@ public class RemoveNodeFromUniverseTest extends CommissionerBaseTest {
     List<TaskInfo> subTasks = taskInfo.getSubTasks();
     Map<Integer, List<TaskInfo>> subTasksByPosition =
         subTasks.stream().collect(Collectors.groupingBy(w -> w.getPosition()));
-    assertRemoveNodeSequence(subTasksByPosition, RemoveType.NOT_EXISTS);
+    assertRemoveNodeSequence(subTasksByPosition, RemoveType.NOT_EXISTS, true);
+  }
+
+  @Test
+  public void testRemoveNodeWithNoDataMove() {
+    setUp(true, 3, 3, false);
+    NodeTaskParams taskParams = new NodeTaskParams();
+    taskParams.universeUUID = defaultUniverse.universeUUID;
+    taskParams.expectedUniverseVersion = 3;
+    TaskInfo taskInfo = submitTask(taskParams, "host-n1");
+    List<TaskInfo> subTasks = taskInfo.getSubTasks();
+    Map<Integer, List<TaskInfo>> subTasksByPosition =
+        subTasks.stream().collect(Collectors.groupingBy(w -> w.getPosition()));
+    assertRemoveNodeSequence(subTasksByPosition, RemoveType.WITH_MASTER, false);
+  }
+
+  @Test
+  public void testRemoveNodeWithNoDataMoveRF5() {
+    setUp(true, 5, 5, true);
+    NodeTaskParams taskParams = new NodeTaskParams();
+    taskParams.universeUUID = defaultUniverse.universeUUID;
+    taskParams.expectedUniverseVersion = 3;
+    TaskInfo taskInfo = submitTask(taskParams, "host-n1");
+    List<TaskInfo> subTasks = taskInfo.getSubTasks();
+    Map<Integer, List<TaskInfo>> subTasksByPosition =
+        subTasks.stream().collect(Collectors.groupingBy(w -> w.getPosition()));
+    assertRemoveNodeSequence(subTasksByPosition, RemoveType.WITH_MASTER, false);
+  }
+
+  @Test
+  public void testRemoveNodeRF5() {
+    setUp(true, 6, 5, true);
+    NodeTaskParams taskParams = new NodeTaskParams();
+    taskParams.universeUUID = defaultUniverse.universeUUID;
+    taskParams.expectedUniverseVersion = 3;
+    TaskInfo taskInfo = submitTask(taskParams, "host-n1");
+    List<TaskInfo> subTasks = taskInfo.getSubTasks();
+    Map<Integer, List<TaskInfo>> subTasksByPosition =
+        subTasks.stream().collect(Collectors.groupingBy(w -> w.getPosition()));
+    assertRemoveNodeSequence(subTasksByPosition, RemoveType.WITH_MASTER, true);
   }
 }
