dcde841ec4c6755c68ccfd5e75511869461a5638
sudheer <sudheer@yugabyte.com>
2020-04-06 15:49:01 -0700

    [#3305] [YSQL] Allowing a deletion of a table when the DocDB returns a 'NotFound' error.
    Summary:
    Since our drop table operation is not atomic with respect to DocDB and
    postgres metadata, it is
    possible that DocDB doesn't have a table metadata but postgres does. In
    this case we can neither
    delete the table or create a new one with the same name.
    
    With this code we should be able to delete the table from the postgres
    metadata by ignoring the
    'NotFound' error from DocDB.
    
    Test Plan:
    Manually tested by faking an error when a deletion is called for the
    first time from the DocDB
    layer, making sure that it is deleted the second time it is called.
    
    Reviewers: mihnea, neha
    
    Reviewed By: neha
    
    Subscribers: yql
    
    Differential Revision: https://phabricator.dev.yugabyte.com/D8248

--- src/yb/yql/pggate/pg_ddl.cc
+++ src/yb/yql/pggate/pg_ddl.cc
@@ -252,6 +252,7 @@ PgDropTable::~PgDropTable() {
 
 Status PgDropTable::Exec() {
   Status s = pg_session_->DropTable(table_id_);
+  pg_session_->InvalidateTableCache(table_id_);
   if (s.ok() || (s.IsNotFound() && if_exist_)) {
     return Status::OK();
   }
