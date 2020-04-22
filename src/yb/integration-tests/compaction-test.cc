yugabyte/yugabyte-db

commit 51eaf5c7a2759e2b81f9d40e14afa92326ec4e6a

#448 Backfill Index Table
Summary:
High level design doc : https://github.com/yugabyte/yugabyte-db/blob/master/architecture/design/docdb-index-backfill.md

This diff implements the master side changes to be done for a create index. Populating the index tablet at the appropriate time is implemented in D7104.

    - Implement a 4 phase index creation at the master to safely create an index and backfill the data in an online manner. Index is created with the INDEX_PERM_DELETE_ONLY permission, then updated to INDEX_PERM_WRITE_AND_DELETE, then INDEX_PERM_BACKFILLING; and finally, after the backfill is complete, set to INDEX_PERM_READ_WRITE_AND_DELETE.
   - At each phase, the master waits for all the tablets of the indexed table to be updated before moving on to the next phase.
   - Ensure that the CQL proxy/Tablet Server respects the IndexPermission and only performs the operations that are permitted.
   - Ensure that master failover in the middle of a index backfill is handled.

A new flag `disable_index_backfill` is introduced to fence the new 4-stage index create process. This defaults to `true` right now -- until all the remaining parts needed for the backfill land.

Test Plan:
./yb_build.sh --cxx-test cassandra_cpp_driver-test --gtest_filter CppCassandraDriverTest.*TestCreateIndex*
./yb_build.sh --cxx-test master_failover-itest --gtest_filter MasterFailoverTestIndexCreation.*Index*

Reviewers: mihnea, sergei, hector, bogdan, rahuldesirazu, mikhail

Reviewed By: rahuldesirazu, mikhail

Subscribers: mikhail, ybase

Differential Revision: https://phabricator.dev.yugabyte.com/D7077

src/yb/client/client.cc
@@ -498,20 +498,27 @@ Status YBClient::IsAlterTableInProgress(const YBTableName& table_name,
   return data_->IsAlterTableInProgress(this, table_name, table_id, deadline, alter_in_progress);
 }
 
+Result<YBTableInfo> YBClient::GetYBTableInfo(const YBTableName& table_name) {
+  YBTableInfo info;
+  auto deadline = CoarseMonoClock::Now() + default_admin_operation_timeout();
+  RETURN_NOT_OK(data_->GetTableSchema(this, table_name, deadline, &info));
+  return info;
+}
+
 Status YBClient::GetTableSchema(const YBTableName& table_name,
                                 YBSchema* schema,
                                 PartitionSchema* partition_schema) {
-  auto deadline = CoarseMonoClock::Now() + default_admin_operation_timeout();
-  YBTableInfo info;
-  RETURN_NOT_OK(data_->GetTableSchema(this, table_name, deadline, &info));
-
+  Result<YBTableInfo> info = GetYBTableInfo(table_name);
+  if (!info.ok()) {
+    return info.status();
+  }
   // Verify it is not an index table.
-  if (info.index_info) {
+  if (info->index_info) {
     return STATUS(NotFound, "The table does not exist");
   }
 
-  *schema = std::move(info.schema);
-  *partition_schema = std::move(info.partition_schema);
+  *schema = std::move(info->schema);
+  *partition_schema = std::move(info->partition_schema);
   return Status::OK();
 }
 

src/yb/client/client.h
@@ -294,6 +294,7 @@ class YBClient {
   CHECKED_STATUS GetTableSchema(const YBTableName& table_name,
                                 YBSchema* schema,
                                 PartitionSchema* partition_schema);
+  Result<YBTableInfo> GetYBTableInfo(const YBTableName& table_name);
 
   CHECKED_STATUS GetTableSchemaById(const TableId& table_id, std::shared_ptr<YBTableInfo> info,
                                     StatusCallback callback);
@@ -625,6 +626,7 @@ class YBClient {
   FRIEND_TEST(ClientTest, TestScanTimeout);
   FRIEND_TEST(ClientTest, TestWriteWithDeadMaster);
   FRIEND_TEST(MasterFailoverTest, DISABLED_TestPauseAfterCreateTableIssued);
+  FRIEND_TEST(MasterFailoverTestIndexCreation, TestPauseAfterCreateIndexIssued);
 
   friend std::future<Result<internal::RemoteTabletPtr>> LookupFirstTabletFuture(
       const YBTable* table);

src/yb/client/table.cc
@@ -114,9 +114,22 @@ bool YBTable::IsIndex() const {
   return info_.index_info != boost::none;
 }
 
+bool YBTable::IsUniqueIndex() const {
+  return info_.index_info.is_initialized() && info_.index_info->is_unique();
+}
+
 const IndexInfo& YBTable::index_info() const {
-  CHECK(info_.index_info);
-  return *info_.index_info;
+  static IndexInfo kEmptyIndexInfo;
+  if (info_.index_info) {
+    return *info_.index_info;
+  }
+  return kEmptyIndexInfo;
+}
+
+std::string YBTable::ToString() const {
+  return strings::Substitute(
+      "$0 $1 IndexInfo: $2 IndexMap $3", (IsIndex() ? "Index Table" : "Normal Table"), id(),
+      yb::ToString(index_info()), yb::ToString(index_map()));
 }
 
 const PartitionSchema& YBTable::partition_schema() const {

src/yb/client/table.h
@@ -86,9 +86,12 @@ class YBTable : public std::enable_shared_from_this<YBTable> {
   // Is this an index?
   bool IsIndex() const;
 
+  bool IsUniqueIndex() const;
+
   // For index table: information about this index.
   const IndexInfo& index_info() const;
 
+  std::string ToString() const;
   //------------------------------------------------------------------------------------------------
   // CQL support
   // Create a new QL operation for this table.

src/yb/client/table_creator.cc
@@ -160,6 +160,11 @@ YBTableCreator& YBTableCreator::wait(bool wait) {
   return *this;
 }
 
+YBTableCreator& YBTableCreator::TEST_use_old_style_create_request() {
+  TEST_use_old_style_create_request_ = true;
+  return *this;
+}
+
 Status YBTableCreator::Create() {
   const char *object_type = index_info_.has_indexed_table_id() ? "index" : "table";
   if (table_name_.table_name().empty()) {
@@ -234,7 +239,9 @@ Status YBTableCreator::Create() {
 
   // Index mapping with data-table being indexed.
   if (index_info_.has_indexed_table_id()) {
-    req.mutable_index_info()->CopyFrom(index_info_);
+    if (!TEST_use_old_style_create_request_) {
+      req.mutable_index_info()->CopyFrom(index_info_);
+    }
 
     // For compatibility reasons, set the old fields just in case we have new clients talking to
     // old master server during rolling upgrade.

src/yb/client/table_creator.h
@@ -87,6 +87,9 @@ class YBTableCreator {
   // For index table: sets the indexed table id of this index.
   YBTableCreator& indexed_table_id(const std::string& id);
 
+  // For index table: uses the old style request without index_info.
+  YBTableCreator& TEST_use_old_style_create_request();
+
   // For index table: sets whether this is a local index.
   YBTableCreator& is_local_index(bool is_local_index);
 
@@ -160,6 +163,8 @@ class YBTableCreator {
   // When creating index, proxy server construct index_info_, and master server will write it to
   // the data-table being indexed.
   IndexInfoPB index_info_;
+
+  bool TEST_use_old_style_create_request_ = false;
 
   MonoDelta timeout_;
   bool wait_ = true;

src/yb/common/common.proto
@@ -147,6 +147,14 @@ enum PermissionType {
   ALL_PERMISSION = 999999999;
 }
 
+enum IndexPermissions {
+  INDEX_PERM_DELETE_ONLY = 0;
+  INDEX_PERM_WRITE_AND_DELETE = 2;
+  INDEX_PERM_DO_BACKFILL = 4;
+  INDEX_PERM_READ_WRITE_AND_DELETE = 6;
+  INDEX_PERM_BACKFILL_FAILED = 8;
+}
+
 // The type used in column schemas, which may have type parameters (i.e. for collections)
 // If types have parameters, they are stored in the params field. Otherwise params is empty.
 // e.g. (using lower case of QLTypes and upper case for DataType):
@@ -225,6 +233,7 @@ message TablePropertiesPB {
   optional bool use_mangled_column_name =  6 [ default = false ];
   optional int32 num_tablets = 7 [ default = 0 ];
   optional bool is_ysql_catalog_table = 8 [ default = false ];
+  optional bool is_backfilling = 9 [ default = false ];
 }
 
 message SchemaPB {
@@ -248,6 +257,9 @@ message IndexInfoPB {
   optional uint32 version = 2 [ default = 0]; // Index table's schema version.
   optional bool is_local = 3 [ default = false ];  // Whether the index is a local index
   optional bool is_unique = 7 [ default = false ]; // Whether the index is a unique index
+  // We should only have this in the elements of "repeated IndexInfoPB indexes" of the
+  // SysTableEntryPB of the main table.
+  optional IndexPermissions index_permissions = 12 [ default = INDEX_PERM_READ_WRITE_AND_DELETE ];
 
   // Index column mapping.
   // "colexpr" is used to compute the value of this column in an INDEX.

src/yb/common/index.cc
@@ -75,6 +75,7 @@ IndexInfo::IndexInfo(const IndexInfoPB& pb)
       range_column_count_(pb.range_column_count()),
       indexed_hash_column_ids_(ColumnIdsFromPB(pb.indexed_hash_column_ids())),
       indexed_range_column_ids_(ColumnIdsFromPB(pb.indexed_range_column_ids())),
+      index_permissions_(pb.index_permissions()),
       use_mangled_column_name_(pb.use_mangled_column_name()) {
   for (const IndexInfo::IndexColumn &index_col : columns_) {
     covered_column_ids_.insert(index_col.indexed_column_id);
@@ -99,6 +100,7 @@ void IndexInfo::ToPB(IndexInfoPB* pb) const {
     pb->add_indexed_range_column_ids(id);
   }
   pb->set_use_mangled_column_name(use_mangled_column_name_);
+  pb->set_index_permissions(index_permissions_);
 }
 
 vector<ColumnId> IndexInfo::index_key_column_ids() const {

commit 3d5c91c5fbe3d5c1b95f19bfdd0022fab5f2e312

[#4211] Unit test for: Retain delete markers on index tables only during backfill
Summary:
Unit test for

Retain delete markers on index tables only during backfill
Issue introduced by 51eaf5c7a2759e2b81f9d40e14afa92326ec4e6a

Test Plan: ensure test passes with the fix, and fails without it

Reviewers: mikhail, bogdan

Reviewed By: bogdan

Subscribers: ybase

Differential Revision: https://phabricator.dev.yugabyte.com/D8287

src/yb/integration-tests/compaction-test.cc
@@ -30,6 +30,7 @@ using namespace std::literals; // NOLINT
 
 DECLARE_int64(db_write_buffer_size);
 DECLARE_int32(rocksdb_level0_file_num_compaction_trigger);
+DECLARE_int32(timestamp_history_retention_interval_sec);
 
 namespace yb {
 
@@ -146,10 +147,16 @@ class CompactionTest : public YBTest {
     workload_->set_num_write_threads(4);
     workload_->set_num_tablets(kNumTablets);
     workload_->set_transactional(isolation_level, transaction_pool_.get());
+    workload_->set_ttl(ttl_to_use());
     workload_->Setup();
   }
 
  protected:
+
+  // -1 implies no ttl.
+  virtual int ttl_to_use() {
+    return -1;
+  }
 
   size_t BytesWritten() {
     return workload_->rows_inserted() * kPayloadBytes;
@@ -227,6 +234,71 @@ TEST_F(CompactionTest, CompactionAfterTruncate) {
 TEST_F(CompactionTest, CompactionAfterTruncateTransactional) {
   SetupWorkload(IsolationLevel::SNAPSHOT_ISOLATION);
   TestCompactionAfterTruncate();
+}
+
+class CompactionTestWithTTL : public CompactionTest {
+ protected:
+  int ttl_to_use() override {
+    return kTTLSec;
+  }
+  const int kTTLSec = 1;
+};
+
+TEST_F(CompactionTestWithTTL, CompactionAfterExpiry) {
+  FLAGS_timestamp_history_retention_interval_sec = 0;
+  FLAGS_rocksdb_level0_file_num_compaction_trigger = 10;
+  SetupWorkload(IsolationLevel::NON_TRANSACTIONAL);
+
+  rocksdb_listener_->Reset();
+  auto dbs = GetAllRocksDbs(cluster_.get(), false);
+
+  // Write enough to be short of triggering compactions.
+  WriteAtLeastFilesPerDb(0.8 * FLAGS_rocksdb_level0_file_num_compaction_trigger);
+  size_t size_before_compaction = 0;
+  for (auto* db : dbs) {
+    size_before_compaction += db->GetCurrentVersionSstFilesUncompressedSize();
+  }
+  LOG(INFO) << "size_before_compaction is " << size_before_compaction;
+
+  LOG(INFO) << "Sleeping";
+  SleepFor(MonoDelta::FromSeconds(2 * kTTLSec));
+
+  // Write enough to trigger compactions.
+  WriteAtLeastFilesPerDb(FLAGS_rocksdb_level0_file_num_compaction_trigger);
+
+  AssertLoggedWaitFor(
+      [&dbs] {
+        for (auto* db : dbs) {
+          if (db->GetLiveFilesMetaData().size() >
+              FLAGS_rocksdb_level0_file_num_compaction_trigger) {
+            return false;
+          }
+        }
+        return true;
+      },
+      60s, "Waiting until we have number of SST files not higher than threshold ...", kWaitDelay);
+
+  // Assert that the data size is smaller now.
+  size_t size_after_compaction = 0;
+  for (auto* db : dbs) {
+    size_after_compaction += db->GetCurrentVersionSstFilesUncompressedSize();
+  }
+  LOG(INFO) << "size_after_compaction is " << size_after_compaction;
+  EXPECT_LT(size_after_compaction, size_before_compaction);
+
+  SleepFor(MonoDelta::FromSeconds(2 * kTTLSec));
+
+  constexpr int kCompactionTimeoutSec = 60;
+  const auto table_info = ASSERT_RESULT(FindTable(cluster_.get(), workload_->table_name()));
+  ASSERT_OK(workload_->client().FlushTable(
+    table_info->id(), kCompactionTimeoutSec, /* compaction */ true));
+  // Assert that the data size is all wiped up now.
+  size_t size_after_manual_compaction = 0;
+  for (auto* db : dbs) {
+    size_after_manual_compaction += db->GetCurrentVersionSstFilesUncompressedSize();
+  }
+  LOG(INFO) << "size_after_manual_compaction is " << size_after_manual_compaction;
+  EXPECT_EQ(size_after_manual_compaction, 0);
 }
 
 } // namespace tserver

src/yb/integration-tests/test_workload.cc
@@ -208,6 +208,9 @@ void TestWorkload::State::WriteThread(const TestWorkloadOptions& options) {
           auto req = update->mutable_request();
           QLAddInt32HashValue(req, 0);
           table.AddInt32ColumnValue(req, table.schema().columns()[1].name(), r.Next());
+          if (options.ttl >= 0) {
+            req->set_ttl(options.ttl);
+          }
           ops.push_back(update);
           CHECK_OK(session->Apply(update));
           break;
@@ -224,6 +227,9 @@ void TestWorkload::State::WriteThread(const TestWorkloadOptions& options) {
       QLAddInt32HashValue(req, key);
       table.AddInt32ColumnValue(req, table.schema().columns()[1].name(), r.Next());
       table.AddStringColumnValue(req, table.schema().columns()[2].name(), test_payload);
+      if (options.ttl > 0) {
+        req->set_ttl(options.ttl);
+      }
       ops.push_back(insert);
       CHECK_OK(session->Apply(insert));
     }

src/yb/integration-tests/test_workload.h
@@ -46,6 +46,7 @@ struct TestWorkloadOptions {
   int payload_bytes = 11;
   int num_write_threads = 4;
   int write_batch_size = 50;
+  int ttl = -1;
   MonoDelta default_rpc_timeout = std::chrono::seconds(60);
   std::chrono::milliseconds write_timeout = std::chrono::seconds(20);
   bool timeout_allowed = false;
@@ -85,6 +86,10 @@ class TestWorkload {
 
   void set_write_batch_size(int s) {
     options_.write_batch_size = s;
+  }
+
+  void set_ttl(int ttl) {
+    options_.ttl = ttl;
   }
 
   void set_client_default_rpc_timeout_millis(int t) {