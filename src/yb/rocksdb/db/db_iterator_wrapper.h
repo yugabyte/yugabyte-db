207c56a9db042c821ab4e775e824d083aef7a70c
Mikhail Bautin <mbautin@users.noreply.github.com>
2020-05-16 10:18:40 -0700

    [#4411] Add a gflag to log all operations on RocksDB iterators
    Summary:
    Add a gflag to log all operations on RocksDB iterators. This is really
    useful for understanding the overhead of the read path in different
    scenarios. Currently there is a gflag that enables logging of all
    RocksDB operations on newly created iterators to the regular glog
    stream, and it is suitable for use in pg_mini-test. The logging wrapper
    is at RocksDB level and does no DocDB aware key/value decoding yet.
    
    Examples of logging results:
    
    - Scanning a subset of rows in a YSQL table with only one primary key
      column:
      https://gist.githubusercontent.com/mbautin/ccf9accaff52d9edf620bf1999b7bea8/raw
    - The same for a table with one primary-key and two non-primary key columns:
      https://gist.githubusercontent.com/mbautin/87d35596db6122c8cb223578cb61198a/raw
    
    With @spolitov's enhancements from https://phabricator.dev.yugabyte.com/D8386
    ("[#4277] Use SeekForward for intents"):
    
    - PK column only:
      https://gist.githubusercontent.com/mbautin/6a4973a796748e330e62a57c0f3e73af/raw
    - Two non-PK columns:
      https://gist.githubusercontent.com/mbautin/757230b4260908bd41696b4f8ffb66ac/raw
    
    Test Plan:
    Jenkins
    
    New tests (inserting a few rows and overwriting them many times, and
    then logging the scan operations). The tests are not checking what is
    being logged yet.
    - `./yb_build.sh --cxx-test pg_mini-test --gtest_filter PgMiniTest.IteratorLogPkOnly`
    - `./yb_build.sh --cxx-test pg_mini-test --gtest_filter PgMiniTest.IteratorLogTwoNonPkCols`
    
    Reviewers: bogdan, neha, sergei
    
    Reviewed By: sergei
    
    Subscribers: ybase
    
    Differential Revision: https://phabricator.dev.yugabyte.com/D8436

---
+++ src/yb/rocksdb/db/db_iterator_wrapper.h
@@ -0,0 +1,102 @@
+// Copyright (c) Yugabyte, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+// in compliance with the License.  You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software distributed under the License
+// is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+// or implied.  See the License for the specific language governing permissions and limitations
+// under the License.
+
+#ifndef YB_ROCKSDB_DB_DB_ITERATOR_WRAPPER_H
+#define YB_ROCKSDB_DB_DB_ITERATOR_WRAPPER_H
+
+#include <memory>
+
+#include "yb/util/logging.h"
+
+#include "yb/rocksdb/iterator.h"
+
+namespace rocksdb {
+
+class DBIteratorWrapper : public Iterator {
+ public:
+  explicit DBIteratorWrapper(Iterator* wrapped)
+      : wrapped_(wrapped) {}
+
+  virtual ~DBIteratorWrapper() {}
+
+  bool Valid() const override {
+    return wrapped_->Valid();
+  }
+
+  void SeekToFirst() override {
+    wrapped_->SeekToFirst();
+  }
+
+  void SeekToLast() override {
+    wrapped_->SeekToLast();
+  }
+
+  void Seek(const Slice& target) override {
+    wrapped_->Seek(target);
+  }
+
+  void Next() override {
+    wrapped_->Next();
+  }
+
+  void Prev() override {
+    wrapped_->Prev();
+  }
+
+  Slice key() const override {
+    return wrapped_->key();
+  }
+
+  Slice value() const override {
+    return wrapped_->value();
+  }
+
+  Status status() const override {
+    return wrapped_->status();
+  }
+
+  Status GetProperty(std::string prop_name, std::string* prop) override {
+    return wrapped_->GetProperty(prop_name, prop);
+  }
+
+ protected:
+  std::unique_ptr<Iterator> wrapped_;
+};
+
+// A wrapper that logs all operations on the iterator.
+class TransitionLoggingIteratorWrapper : public DBIteratorWrapper {
+ public:
+  TransitionLoggingIteratorWrapper(
+      Iterator* wrapped,
+      const std::string& rocksdb_log_prefix)
+      : DBIteratorWrapper(wrapped),
+        rocksdb_log_prefix_(rocksdb_log_prefix) {}
+
+  void SeekToFirst() override;
+  void SeekToLast() override;
+  void Seek(const Slice& target) override;
+  void Next() override;
+  void Prev() override;
+
+ private:
+  std::string LogPrefix() const;
+  std::string StateStr() const;
+
+  template<typename Functor>
+  void LogBeforeAndAfter(const std::string& action_str, const Functor& functor);
+
+  std::string rocksdb_log_prefix_;
+};
+
+}  // namespace rocksdb
+
+#endif  // YB_ROCKSDB_DB_DB_ITERATOR_WRAPPER_H
