6aeb253a236f969840261566851f3cdaff3cf770
Arnav Agarwal <14933889+Arnav15@users.noreply.github.com>
2020-05-22 06:45:05 +0530

    [#4552]: Fix Customer Update API Call.
    Summary:
    Diff D8406 broke the customer update call. This diff updates the update function to not
    return early as well as ensure proper setting to all values.
    
    Test Plan:
    Ran a bunch of API calls with varying payloads to verify that the update works as
    expected.
    
    Reviewers: andrew, sshevchenko, ram
    
    Reviewed By: ram
    
    Subscribers: jenkins-bot, yugaware
    
    Differential Revision: https://phabricator.dev.yugabyte.com/D8528

--- managed/src/main/java/com/yugabyte/yw/controllers/CustomerController.java
+++ managed/src/main/java/com/yugabyte/yw/controllers/CustomerController.java
@@ -109,13 +109,20 @@ public class CustomerController extends AuthenticatedController {
       responseJson.put("error", "Invalid Customer UUID:" + customerUUID);
       return badRequest(responseJson);
     }
+
     JsonNode request = request().body().asJson();
-    if (request.has("alertingData")) {
-      Form<AlertingFormData> formData = formFactory.form(AlertingFormData.class).bindFromRequest();
-      if (formData.hasErrors()) {
-        responseJson.set("error", formData.errorsAsJson());
-        return badRequest(responseJson);
-      }
+    Form<AlertingFormData> formData = formFactory.form(AlertingFormData.class).bindFromRequest();
+    if (formData.hasErrors()) {
+      responseJson.set("error", formData.errorsAsJson());
+      return badRequest(responseJson);
+    }
+
+    if (formData.get().name != null) {
+      customer.name = formData.get().name;
+      customer.save();
+    }
+
+    if (request.has("alertingData") || request.has("smtpData")) {
 
       CustomerConfig config = CustomerConfig.getAlertConfig(customerUUID);
       if (config == null && formData.get().alertingData != null) {
@@ -125,23 +132,20 @@ public class CustomerController extends AuthenticatedController {
         config.data = Json.toJson(formData.get().alertingData);
         config.update();
       }
-      return ok(Json.toJson(customer));
-    }
 
-    Form<CustomerRegisterFormData> formData = formFactory.form(CustomerRegisterFormData.class).bindFromRequest();
-    if (formData.hasErrors()) {
-      responseJson.set("error", formData.errorsAsJson());
-      return badRequest(responseJson);
-    }
-
-
-    CustomerConfig smtpConfig = CustomerConfig.getSmtpConfig(customerUUID);
-    if (smtpConfig == null && formData.get().smtpData != null) {
-      smtpConfig = CustomerConfig.createSmtpConfig(
-          customerUUID, Json.toJson(formData.get().smtpData));
-    } else if (smtpConfig != null && formData.get().smtpData != null) {
-      smtpConfig.data = Json.toJson(formData.get().smtpData);
-      smtpConfig.update();
+      CustomerConfig smtpConfig = CustomerConfig.getSmtpConfig(customerUUID);
+      if (smtpConfig == null && formData.get().smtpData != null) {
+        smtpConfig = CustomerConfig.createSmtpConfig(
+            customerUUID, Json.toJson(formData.get().smtpData));
+      } else if (smtpConfig != null && formData.get().smtpData != null) {
+        smtpConfig.data = Json.toJson(formData.get().smtpData);
+        smtpConfig.update();
+      } // In case we want to reset the smtpData and use the default mailing server.
+      else if (request.has("smtpData") && formData.get().smtpData == null) {
+        if (smtpConfig != null) {
+          smtpConfig.delete();
+        }
+      }
     }
 
     // Features would be a nested json, so we should fetch it differently.
@@ -151,9 +155,6 @@ public class CustomerController extends AuthenticatedController {
     }
 
     CustomerConfig.upsertCallhomeConfig(customerUUID, formData.get().callhomeLevel);
-
-    customer.name = formData.get().name;
-    customer.update();
 
     return ok(Json.toJson(customer));
   }
--- managed/src/main/java/com/yugabyte/yw/forms/AlertingFormData.java
+++ managed/src/main/java/com/yugabyte/yw/forms/AlertingFormData.java
@@ -4,7 +4,7 @@ package com.yugabyte.yw.forms;
 
 
 import play.data.validation.Constraints;
-
+import java.util.Map;
 
 /**
  * This class will be used by the API and UI Form Elements to validate constraints are met
@@ -21,6 +21,8 @@ public class AlertingFormData {
 
     public String name;
 
+    public Map features;
+
     static public class AlertingData {
         @Constraints.Email
         @Constraints.MinLength(5)
@@ -35,5 +37,26 @@ public class AlertingFormData {
         public Boolean reportOnlyErrors = false;
     }
 
+    static public class SmtpData {
+        public String smtpServer = null;
+
+        public int smtpPort = -1;
+
+        public String emailFrom = null;
+
+        public String smtpUsername = null;
+
+        public String smtpPassword = null;
+
+        public boolean useSSL = true;
+
+        public boolean useTLS = false;
+      }
+
     public AlertingData alertingData;
+    public SmtpData smtpData;
+
+    @Constraints.Pattern(message="Must be one of NONE, LOW, MEDIUM, HIGH",
+                         value="\\b(?:NONE|LOW|MEDIUM|HIGH)\\b")
+    public String callhomeLevel = "MEDIUM";
 }
