11f1b23aad66f12cebe46b7132cd05347aae0b7f
Sergey Shevchenko <sshevchenko@yugabyte.com>
2020-05-26 13:16:58 -0700

    [YW][#4443] UI for SMTP credentials
    Summary:
    Added "Custom SMTP Configuration" section to "Health & Alerting" tab at customer profile page.
    Depends on D8528
    {F13664}
    
    Test Plan:
    Go to customer profile, open "Health & Alerting" tab, enable "Custom SMTP Configuration" checkbox, enter SMTP configuration, hit "Save" button.
    Open same profile page again and:
    - make sure changes are saved
    - make sure provided credentials are used
    
    Go to customer profile, open "Health & Alerting" tab, disable "Custom SMTP Configuration" checkbox, hit "Save" button.
    Open same profile page again and:
    - make sure changes are saved
    - make sure SMTP credentials are reverted back to default presets
    
    Reviewers: ram, andrew
    
    Reviewed By: andrew
    
    Subscribers: arnav, jason, ui
    
    Differential Revision: https://phabricator.dev.yugabyte.com/D8546

--- managed/ui/src/components/profile/AlertProfileForm.js
+++ managed/ui/src/components/profile/AlertProfileForm.js
@@ -2,18 +2,50 @@
 
 import React, { Component } from 'react';
 import { Row, Col } from 'react-bootstrap';
-import { YBFormInput, YBButton, YBToggle,
-         YBControlledSelectWithLabel } from '../common/forms/fields';
+import { YBFormInput, YBButton, YBToggle, YBControlledSelectWithLabel } from '../common/forms/fields';
 import { Formik, Form, Field } from 'formik';
-import { browserHistory} from 'react-router';
+import { browserHistory } from 'react-router';
 import { isNonAvailable, showOrRedirect } from '../../utils/LayoutUtils';
 import * as Yup from 'yup';
+import _ from 'lodash';
 import { isNonEmptyObject, isNonEmptyArray } from '../../utils/ObjectUtils';
 import { getPromiseState } from '../../utils/PromiseUtils';
 
 // TODO set predefined defaults another way not to share defaults this way
 const CHECK_INTERVAL_MS = 300000;
 const STATUS_UPDATE_INTERVAL_MS = 43200000;
+const DEFAULT_SMTP_PORT = 587;
+
+const validationSchema = Yup.object().shape({
+  alertingData: Yup.object({
+    sendAlertsToYb: Yup.boolean().default(false).nullable(),
+    alertingEmail: Yup.string().nullable(), // This field can be one or more emails separated by commas
+    checkIntervalMs: Yup.number().typeError('Must specify a number'),
+    statusUpdateIntervalMs: Yup.number().typeError('Must specify a number'),
+    reportOnlyErrors: Yup.boolean().default(false).nullable()
+  }),
+  customSmtp: Yup.boolean(),
+  smtpData: Yup.object().when('customSmtp', {
+    is: true,
+    then: Yup.object({
+      smtpServer: Yup.string().required('Must specify an SMTP server address'),
+      smtpPort: Yup.number().typeError('Must specify an SMTP server port number'),
+      emailFrom: Yup.string().email('Must be an email'),
+      smtpUsername: Yup.string().required('Must specify an SMTP server credentials username'),
+      smtpPassword: Yup.string().required('Must specify an SMTP server credentials password'),
+      useSSL: Yup.boolean(),
+      useTLS: Yup.boolean()
+    })
+  }),
+  callhomeLevel: Yup.string()
+});
+
+const callhomeOptions = [
+  <option value="NONE" key={0}>None</option>,
+  <option value="LOW" key={1}>Low</option>,
+  <option value="MEDIUM" key={2}>Medium</option>,
+  <option value="HIGH" key={3}>High</option>
+];
 
 export default class AlertProfileForm extends Component {
   constructor(props) {
@@ -26,16 +58,17 @@ export default class AlertProfileForm extends Component {
   componentDidMount() {
     const { customer } = this.props;
     this.props.getCustomerUsers();
-    if (isNonAvailable(customer.features, "main.profile")) browserHistory.push('/');
+    if (isNonAvailable(customer.features, 'main.profile')) browserHistory.push('/');
   }
 
   componentDidUpdate() {
     const { customerProfile, handleProfileUpdate } = this.props;
     const { statusUpdated } = this.state;
-    if (statusUpdated && (getPromiseState(customerProfile).isSuccess() ||
-        getPromiseState(customerProfile).isError())) {
+    if (
+      statusUpdated && (getPromiseState(customerProfile).isSuccess() || getPromiseState(customerProfile).isError())
+    ) {
       handleProfileUpdate(customerProfile.data);
-      this.setState({statusUpdated: false});
+      this.setState({ statusUpdated: false });
     }
   }
 
@@ -46,29 +79,7 @@ export default class AlertProfileForm extends Component {
       updateCustomerDetails
     } = this.props;
 
-    showOrRedirect(customer.data.features, "main.profile");
-
-    const validationSchema = Yup.object().shape({
-      alertingData: Yup.object({
-        sendAlertsToYb: Yup.boolean()
-          .default(false)
-          .nullable(),
-
-        // This field can be one or more emails separated by commas
-        alertingEmail: Yup.string().nullable(),
-
-        checkIntervalMs: Yup.number()
-          .typeError('Must specify a number'),
-
-        statusUpdateIntervalMs: Yup.number()
-          .typeError('Must specify a number'),
-
-        reportOnlyErrors: Yup.boolean()
-          .default(false)
-          .nullable(),
-      }),
-      callhomeLevel: Yup.string()
-    });
+    showOrRedirect(customer.data.features, 'main.profile');
 
     // Filter users for userUUID set during login
     const loginUserId = localStorage.getItem('userId');
@@ -91,17 +102,20 @@ export default class AlertProfileForm extends Component {
           customer.data.alertingData.statusUpdateIntervalMs :
           STATUS_UPDATE_INTERVAL_MS) : '',
         sendAlertsToYb: customer.data.alertingData && customer.data.alertingData.sendAlertsToYb,
-        reportOnlyErrors: customer.data.alertingData && customer.data.alertingData.reportOnlyErrors,
+        reportOnlyErrors: customer.data.alertingData && customer.data.alertingData.reportOnlyErrors
+      },
+      customSmtp: isNonEmptyObject(_.get(customer, 'data.smtpData', {})),
+      smtpData: {
+        smtpServer: _.get(customer, 'data.smtpData.smtpServer', ''),
+        smtpPort: _.get(customer, 'data.smtpData.smtpPort', DEFAULT_SMTP_PORT),
+        emailFrom: _.get(customer, 'data.smtpData.emailFrom', ''),
+        smtpUsername: _.get(customer, 'data.smtpData.smtpUsername', ''),
+        smtpPassword: _.get(customer, 'data.smtpData.smtpPassword', ''),
+        useSSL: _.get(customer, 'data.smtpData.useSSL', false),
+        useTLS: _.get(customer, 'data.smtpData.useTLS', false)
       },
       callhomeLevel: customer.data.callhomeLevel || 'NONE'
     };
-
-    const callhomeOptions = [
-      <option value="NONE" key={0}>None</option>,
-      <option value="LOW" key={1}>Low</option>,
-      <option value="MEDIUM" key={2}>Medium</option>,
-      <option value="HIGH" key={3}>High</option>
-    ];
 
     return (
       <div className="bottom-bar-padding">
@@ -109,17 +123,20 @@ export default class AlertProfileForm extends Component {
           validationSchema={validationSchema}
           initialValues={initialValues}
           enableReinitialize
-          onSubmit={(values, { setSubmitting }) => {
-            updateCustomerDetails(values);
-            this.setState({statusUpdated: true});
+          onSubmit={(values, { setSubmitting, resetForm }) => {
+            const data = _.omit(values, 'customSmtp'); // don't submit internal helper field
+            if (!values.customSmtp) {
+              data.smtpData = null; // this will revert smtp settings to default presets
+            }
+
+            updateCustomerDetails(data);
+            this.setState({ statusUpdated: true });
             setSubmitting(false);
+
+            // default form to new values to avoid unwanted validation of smtp fields when they are hidden
+            resetForm(values);
           }}
-          render={({
-            values,
-            handleChange,
-            handleSubmit,
-            isSubmitting,
-          }) => (
+          render={({ values, handleChange, handleSubmit, isSubmitting }) => (
             <Form name="EditCustomerProfile" onSubmit={handleSubmit}>
               <Row>
                 <Col md={6} sm={12}>
@@ -132,37 +149,50 @@ export default class AlertProfileForm extends Component {
                     placeholder="Emails to forward alerts to"
                   />
                   <Field name="alertingData.sendAlertsToYb">
-                    {({field, form}) => (<YBToggle onToggle={handleChange}
+                    {({ field }) => (
+                      <YBToggle
+                        onToggle={handleChange}
                         name="alertingData.sendAlertsToYb"
                         input={{
                           value: field.value,
-                          onChange: field.onChange,
+                          onChange: field.onChange
                         }}
                         label="Send alert emails to YugaByte team"
                         subLabel="Whether or not to send alerting emails to the YugaByte team."
                       />
                     )}
                   </Field>
-                  <Field name="callhomeLevel"
+                  <Field
+                    name="callhomeLevel"
                     component={YBControlledSelectWithLabel}
                     label="Callhome Level"
-                    input={{name: 'callhomeLevel'}}
+                    input={{ name: 'callhomeLevel' }}
                     onInputChanged={handleChange}
                     selectVal={values.callhomeLevel}
                     options={callhomeOptions}
                   />
-                  <Field name="alertingData.checkIntervalMs" type="text"
-                         component={YBFormInput} label="Health check interval"
-                         placeholder="Miliseconds to check universe status"/>
-                  <Field name="alertingData.statusUpdateIntervalMs" type="text"
-                         component={YBFormInput} label="Report email interval"
-                         placeholder="Miliseconds to send a status report email"/>
+                  <Field
+                    name="alertingData.checkIntervalMs"
+                    type="text"
+                    component={YBFormInput}
+                    label="Health check interval"
+                    placeholder="Milliseconds to check universe status"
+                  />
+                  <Field
+                    name="alertingData.statusUpdateIntervalMs"
+                    type="text"
+                    component={YBFormInput}
+                    label="Report email interval"
+                    placeholder="Milliseconds to send a status report email"
+                  />
                   <Field name="alertingData.reportOnlyErrors">
-                    {({field, form}) => (<YBToggle onToggle={handleChange}
+                    {({ field }) => (
+                      <YBToggle
+                        onToggle={handleChange}
                         name="alertingData.reportOnlyErrors"
                         input={{
                           value: field.value,
-                          onChange: field.onChange,
+                          onChange: field.onChange
                         }}
                         label="Only include errors in alert emails"
                         subLabel="Whether or not to include errors in alert emails."
@@ -171,9 +201,95 @@ export default class AlertProfileForm extends Component {
                   </Field>
                 </Col>
               </Row>
+              <Row>
+                <br />
+                <Col md={6} sm={12}>
+                  <Field name="customSmtp">
+                    {({ field }) => (
+                      <YBToggle
+                        onToggle={handleChange}
+                        name="customSmtp"
+                        input={{
+                          value: field.value,
+                          onChange: field.onChange
+                        }}
+                        label={<h3>Custom SMTP Configuration</h3>}
+                        subLabel="Whether or not to use custom SMTP Configuration."
+                      />
+                    )}
+                  </Field>
+                  <div hidden={!values.customSmtp}>
+                    <Field
+                      name="smtpData.smtpServer"
+                      type="text"
+                      component={YBFormInput}
+                      label="Server"
+                      placeholder="SMTP server address"
+                    />
+                    <Field
+                      name="smtpData.smtpPort"
+                      type="text"
+                      component={YBFormInput}
+                      label="Port"
+                      placeholder="SMTP server port"
+                    />
+                    <Field
+                      name="smtpData.emailFrom"
+                      type="text"
+                      component={YBFormInput}
+                      label="Email From"
+                      placeholder="Send outgoing emails from"
+                    />
+                    <Field
+                      name="smtpData.smtpUsername"
+                      type="text"
+                      component={YBFormInput}
+                      label="Username"
+                      placeholder="SMTP server username"
+                    />
+                    <Field
+                      name="smtpData.smtpPassword"
+                      type="password"
+                      autoComplete="new-password"
+                      component={YBFormInput}
+                      label="Password"
+                      placeholder="SMTP server password"
+                    />
+                    <Field name="smtpData.useSSL">
+                      {({ field }) => (
+                        <YBToggle
+                          onToggle={handleChange}
+                          name="smtpData.useSSL"
+                          input={{
+                            value: field.value,
+                            onChange: field.onChange
+                          }}
+                          label="SSL"
+                          subLabel="Whether or not to use SSL."
+                        />
+                      )}
+                    </Field>
+                    <Field name="smtpData.useTLS">
+                      {({ field }) => (
+                        <YBToggle
+                          onToggle={handleChange}
+                          name="smtpData.useTLS"
+                          input={{
+                            value: field.value,
+                            onChange: field.onChange
+                          }}
+                          label="TLS"
+                          subLabel="Whether or not to use TLS."
+                        />
+                      )}
+                    </Field>
+                  </div>
+                </Col>
+              </Row>
               <div className="form-action-button-container">
                 <Col sm={12}>
-                  <YBButton btnText="Save"
+                  <YBButton
+                    btnText="Save"
                     btnType="submit"
                     disabled={isSubmitting}
                     btnClass="btn btn-orange pull-right"
