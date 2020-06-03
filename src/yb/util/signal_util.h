6ea5d1de2727fb9e640c972f088d5a99b91100bc
Alex Abdugafarov <frozenspider@users.noreply.github.com>
2020-06-03 18:59:18 +0500

    #4092: [YSQL] Fix concurrency issues with metrics webserver
    Summary:
    # Background
    
    We have a webserver providing metrics info that is running on the port 13000 (by default). It's implemented as a postgres `bgworker` - a separate `postgres` process named `YSQL webserver`:
    ```
    $ ps -ef | grep 'YSQL webserver'
      501 20779 20774   0 11:23PM ??         0:00.00 postgres: YSQL webserver
    ```
    This process runs an embedded `squeasel` webserver as a separate thread, which in turn can spin up to 50 worker threads. We currently have 6 endpoints bound for this webserver, some of which (like `/rpcz`) rely on postgres-style logic. As such, this process always has at least two threads running - main threads and squeasel request acceptor thread.
    Note though that postgres logic is NOT generally thread-safe as it relies heavily on global variables (`CurrentMemoryContext`, `CurrentTransaction`, ...) and all execution of such logic should be single-threaded.
    This diff addressed two problems, both of which are the result of an unexpected concurrency.
    
    # Issue 1: Multiple SIGTERMs
    
    This is the original issue described in https://github.com/yugabyte/yugabyte-db/issues/4092. When multiple `SIGTERM` signals are sent to the process in question at the same time, multiple threads might be assigned by OS to process them concurrently.  Configured handler function for `SIGTERM` is `bgworker_die`, which uses `ereport` - which messes with global memory context, which in turn sometimes leads to double-`pfree` and segfault as a result. This is hard to reproduce normally, but can be simulated by putting `sleep` before `PG_SETMASK(&BlockSig);` in `bgworker_die`.
    
    Fix for this is to mask (block) all signals for which we've defined non-trivial handlers for the main thread before spawning a webserver thread, and unmask them right after.  Webserver acceptor thread will be spawned with signals masked and will apply the same mask to its spawned worker threads.
    This will make sure that only the main thread will have those signals unmasked, and thus it will be the only one assigned to process however many of them.
    
    # Issue 2: Concurrent endpoint processing
    
    `/rpcz` endpoint logic, `pullRpczEntries`, uses `CurrentMemoryContext` to allocate a (global) `rpczEntry` struct. As such, pinging this endpoint concurrently may lead to memory corruption issue similar to the one described above. To simulate this, I used breakpoints inside `pullRpczEntries` to make execution flow work in the worst possible way.
    The easiest fix for this is to limit the number of squeasel worker threads to 1, thus making all endpoint logic sequential.
    
    Test Plan: Manual testing
    
    Reviewers: mihnea, sergei, neha, bogdan, mikhail
    
    Reviewed By: mikhail
    
    Subscribers: sudheer, dmitry, yql, bogdan
    
    Differential Revision: https://phabricator.dev.yugabyte.com/D8508

---
+++ src/yb/util/signal_util.h
@@ -0,0 +1,67 @@
+// Copyright (c) YugaByte, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+// in compliance with the License.  You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software distributed under the License
+// is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+// or implied.  See the License for the specific language governing permissions and limitations
+// under the License.
+//--------------------------------------------------------------------------------------------------
+// Utility functions and macros to manage process signals.
+
+#ifndef YB_UTIL_SIGNAL_UTIL_H
+#define YB_UTIL_SIGNAL_UTIL_H
+
+#include "yb/util/result.h"
+#include "yb/util/status.h"
+
+namespace yb {
+
+//
+// Generic functions
+//
+
+// On current thread, block the given signals and return an old signal mask.
+// Signals will be accumulated by OS and delivered when unblocked.
+// This is needed before starting the thread if we only want those signals to be handled
+// at the main thread to avoid concurrency issues.
+Result<sigset_t> ThreadSignalMaskBlock(const std::vector<int>& signals_to_block);
+
+// Restore previous signal mask on the current thread.
+// Unblocking signals lets the blocked signals be delivered if they had been raised in the meantime.
+CHECKED_STATUS ThreadSignalMaskRestore(sigset_t old_mask);
+
+//
+// Specific functions
+//
+
+extern const std::vector<int> kYsqlHandledSignals;
+
+// Calls ThreadSignalMaskBlock to block signals with handlers installed by postgres layer.
+Result<sigset_t> ThreadYsqlSignalMaskBlock() {
+  return ThreadSignalMaskBlock(kYsqlHandledSignals);
+}
+
+// Applies ThreadYsqlSignalMaskBlock, executes a given code (which should return a Status or Result)
+// and apples ThreadSignalMaskRestore, returning execution result.
+// Will attempt to revert a mask even if execution fails.
+// In case both execution and mask restoration fail, execution error status will be returned.
+template<typename Functor>
+typename std::result_of<Functor()>::type WithMaskedYsqlSignals(Functor callback) {
+  sigset_t old_mask = VERIFY_RESULT(ThreadYsqlSignalMaskBlock());
+  auto&& callback_status = callback();
+  Status restore_status = yb::ThreadSignalMaskRestore(old_mask);
+  if (!restore_status.ok() && !callback_status.ok()) {
+    LOG(WARNING) << "Failed to restore thread signal mask: " << restore_status;
+    return std::move(callback_status);
+  }
+  RETURN_NOT_OK(restore_status);
+  return std::move(callback_status);
+}
+
+} // namespace yb
+
+#endif // YB_UTIL_SIGNAL_UTIL_H
