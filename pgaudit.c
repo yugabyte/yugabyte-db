9a686ef2d4ddc47e2d0df09d3a20b22e8586cd9e
David Steele <david@pgmasters.net>
2020-06-18 08:22:05 -0400

    Update copyright end year.

--- pgaudit.c
+++ pgaudit.c
@@ -5,7 +5,7 @@
  * object level logging, and fully-qualified object names for all DML and DDL
  * statements where possible (See README.md for details).
  *
- * Copyright (c) 2014-2019, PostgreSQL Global Development Group
+ * Copyright (c) 2014-2020, PostgreSQL Global Development Group
  *------------------------------------------------------------------------------
  */
 #include "postgres.h"
