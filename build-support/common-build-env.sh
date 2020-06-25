994cc442d502a206fe091d0f11be1e2760924c6e
Steve Varnau <steve.varnau@yugabyte.com>
2020-06-24 17:46:22 -0700

    Fix for macOS environment without flock command
    Summary:
    In macOS environment, do not require flock to install thirdparty download. It is only needed in
    environment with multiple build-workers or spark-workers.
    
    Test Plan:
    yb_build.sh on macOS with flock
    yb_build.sh on macOS without flock
    
    Reviewers: mikhail
    
    Reviewed By: mikhail
    
    Subscribers: devops
    
    Differential Revision: https://phabricator.dev.yugabyte.com/D8743

--- build-support/common-build-env.sh
+++ build-support/common-build-env.sh
@@ -192,9 +192,16 @@ readonly -a MVN_OPTS_TO_DOWNLOAD_ALL_DEPS=(
 )
 
 if is_mac; then
-  readonly FLOCK="/usr/local/bin/flock"
+  if [[ -x /usr/local/bin/flock ]]; then
+    readonly FLOCK="/usr/local/bin/flock"
+    readonly FLOCK_MSG="File locked"
+  else
+    readonly FLOCK="/usr/bin/true"
+    readonly FLOCK_MSG="Skipped file lock"
+  fi
 else
   readonly FLOCK="/usr/bin/flock"
+  readonly FLOCK_MSG="File locked"
 fi
 
 readonly DELAY_ON_BUILD_WORKERS_LIST_HTTP_ERROR_SEC=0.5
@@ -1052,15 +1059,14 @@ download_and_extract_archive() {
       (
         "$FLOCK" -w "$YB_DOWNLOAD_LOCK_TIMEOUT_SEC" 200
         if [[ ! -d $dest_dir && ! -L $dest_dir ]]; then
-          log "[Host $(hostname)] Acquired lock $lock_path, proceeding with archive installation."
+          log "[Host $(hostname)] $FLOCK_MSG $lock_path, proceeding with archive installation."
           (
             set -x
             "$YB_SRC_ROOT/python/yb/download_and_extract_archive.py" \
               --url "$url" --dest-dir-parent "${dest_dir_parent}"
           )
         else
-          log "[Host $(hostname)] Acquired lock $lock_path but directory $dest_dir already" \
-              "exists. This is OK."
+          log "[Host $(hostname)] $FLOCK_MSG $lock_path but directory $dest_dir already exists."
         fi
       ) 200>"$lock_path"
     )
