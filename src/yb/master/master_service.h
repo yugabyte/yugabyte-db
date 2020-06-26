51be987f2ee41e995b4e5672e74a31c2660ca53a
Vivek Gopalan <vivek.gopalan@yale.edu>
2020-06-25 20:40:39 -0700

    [#3077] Added ability for user to have transaction table leaders not be bound to preferred zones.
    Summary:
    Added the gflag
    ```
    transaction_tables_use_preferred_zones
    ```
    so that users can
    specify whether transaction tables should respect leadership affinity after setting preferred zones.
    Default is 0. When set to 0 transaction tablet peer leaders are sprayed according to normal leader
    load balancing. When set to 1, transaction tablet peer leaders use preferred zones.
    
    Added src/yb/integration-tests/load_balancer_respect_affinity-test.cc
    Added `AreTransactionLeadersSpread` rpc in the context of this test.
    
    Test Plan:
    Testing with yb-ctl and ysqlsh.
    I.e:
    
    ```
    ./bin/yb-ctl --rf 3 create --placement_info "c1.r1.z1,c1.r1.z2,c1.r1.z3"
    ./build/latest/bin/yb-admin -master_addresses "127.0.0.1:7100,127.0.0.2:7100,127.0.0.3:7100" set_preferred_zones c1.r1.z1
    ./bin/ysqlsh
    yugabyte=# CREATE DATABASE test_preferred; \c test_preferred
    test_preferred=# CREATE TABLE t(i text PRIMARY KEY) SPLIT INTO 20 TABLETS;
    ```
    Now observe the master UI after load balancing is completed and see that there are 2 transaction status tablet leaders on each node while there are 20 user tablet leaders on node 1.
    ```
    ./build/latest/bin/yb-ts-cli  -server_address 127.0.0.1:7100 set_flag -force transaction_tables_use_preferred_zones 1
    ./build/latest/bin/yb-ts-cli  -server_address 127.0.0.2:7100 set_flag -force transaction_tables_use_preferred_zones 1
    ./build/latest/bin/yb-ts-cli  -server_address 127.0.0.3:7100 set_flag -force transaction_tables_use_preferred_zones 1
    ```
    Observe how now all transaction status tablet leaders are on node 1. To ensure this can be reverted, perform the following:
    ```
    ./build/latest/bin/yb-ts-cli  -server_address 127.0.0.1:7100 set_flag -force transaction_tables_use_preferred_zones 0
    ./build/latest/bin/yb-ts-cli  -server_address 127.0.0.2:7100 set_flag -force transaction_tables_use_preferred_zones 0
    ./build/latest/bin/yb-ts-cli  -server_address 127.0.0.3:7100 set_flag -force transaction_tables_use_preferred_zones 0
    ```
    And the transaction status tablet leaders return to an even spread.
    
    Testing with a C++ test.
    
    ```
    ./yb_build.sh --cxx_test load_balancer_respect_affinity-test -n 10
    ```
    
    Reviewers: nicolas, bogdan, hector
    
    Reviewed By: hector
    
    Subscribers: raju, kannan, mikhail, mihnea, sergei, ybase
    
    Differential Revision: https://phabricator.dev.yugabyte.com/D8591

--- src/yb/master/master_service.h
+++ src/yb/master/master_service.h
@@ -240,6 +240,10 @@ class MasterServiceImpl : public MasterServiceIf,
                                  AreLeadersOnPreferredOnlyResponsePB* resp,
                                  rpc::RpcContext rpc) override;
 
+  void AreTransactionLeadersSpread(const AreTransactionLeadersSpreadRequestPB* req,
+                                   AreTransactionLeadersSpreadResponsePB* resp,
+                                   rpc::RpcContext rpc) override;
+
   void FlushTables(const FlushTablesRequestPB* req,
                    FlushTablesResponsePB* resp,
                    rpc::RpcContext rpc) override;
