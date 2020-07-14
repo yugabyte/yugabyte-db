da398b84ad25a395af100451d88de57d7d0a4ac3
Kyotaro Horiguchi <horikyota.ntt@gmail.com>
2020-07-14 17:17:19 +0900

    Mask non-significant parts of regression output
    To stabilize test, mask more of non-significant parts of EXPLAIN output.

--- sql/maskout2.sh
+++ sql/maskout2.sh
@@ -1,3 +1,3 @@
 #! /bin/sh
 cat $1 | \
-sed 's/^-\+$/--(snip..)/;s/^\( \+Foreign File: \).*$/\1 (snip..)/'
+sed 's/^ *QUERY PLAN *$/--(snip..)/;s/^-\+$/--(snip..)/;s/^\( \+Foreign File: \).*$/\1 (snip..)/'
