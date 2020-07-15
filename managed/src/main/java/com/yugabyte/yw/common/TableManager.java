yugabyte/yugabyte-db

commit 508e3c3c553329de3555c97d9bfe0a4024cf608e

[YW][#4540] Add new form class to support multi-table backups and add new commissioner subtask to call TableManager command
Summary:
Add multi-table form class to handle list of keyspaces, tables, and table uuids. Extend
Backup model create to accept this additional form class. Add new subtask to handle multi-table
backups. Edit TableManager to add additional command arguments when backing up multiple tables.
Change payload for restore path to factor in change to form class. We will allow keyspace rename
but not table rename.

Test Plan:
Try backing up multiple tables at once and confirm if the table is properly backed up. Make some changes to the YCQL tables you've backed up previously. Now restore the tables and confirm that the changes were reverted back to the original at the time of backup.

Try again with a scheduled backup and confirm that backup runs successfully with multiple tables, and only generates a single backup object.

Reviewers: bogdan, wesley, ram, rahuldesirazu

Reviewed By: rahuldesirazu

Subscribers: rahuldesirazu, yugaware, jenkins-bot

Differential Revision: https://phabricator.dev.yugabyte.com/D8568

managed/src/main/java/com/yugabyte/yw/common/TableManager.java
@@ -13,6 +13,7 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.UUID;
 
 import com.yugabyte.yw.common.PlacementInfoUtil;
 
@@ -30,7 +31,6 @@ public class TableManager extends DevopsBase {
 
   public enum CommandSubType {
     BACKUP,
-
     BULK_IMPORT;
 
     public String getScript() {
@@ -75,14 +75,41 @@ public class TableManager extends DevopsBase {
     commandArgs.add(subType.getScript());
     commandArgs.add("--masters");
     commandArgs.add(universe.getMasterAddresses());
-    commandArgs.add("--table");
-    commandArgs.add(taskParams.tableName);
-    commandArgs.add("--keyspace");
-    commandArgs.add(taskParams.keyspace);
 
     switch (subType) {
       case BACKUP:
         BackupTableParams backupTableParams = (BackupTableParams) taskParams;
+        if (backupTableParams.actionType == BackupTableParams.ActionType.CREATE) {
+          if (backupTableParams.tableUUIDList != null && !backupTableParams.tableUUIDList.isEmpty()) {
+            for (int listIndex = 0; listIndex < backupTableParams.tableNameList.size(); listIndex++) {
+              commandArgs.add("--table");
+              commandArgs.add(backupTableParams.tableNameList.get(listIndex));
+              commandArgs.add("--keyspace");
+              commandArgs.add(backupTableParams.keyspace);
+              commandArgs.add("--table_uuid");
+              commandArgs.add(backupTableParams.tableUUIDList.get(listIndex).toString());
+            }
+          } else {
+            commandArgs.add("--table");
+            commandArgs.add(taskParams.tableName);
+            commandArgs.add("--keyspace");
+            commandArgs.add(taskParams.keyspace);
+          }
+        } else if (backupTableParams.actionType == BackupTableParams.ActionType.RESTORE) {
+          if (backupTableParams.tableUUIDList != null && !backupTableParams.tableUUIDList.isEmpty()) {
+            for (String tableName : backupTableParams.tableNameList) {
+              commandArgs.add("--table");
+              commandArgs.add(tableName);
+            }
+          } else if (backupTableParams.tableName != null) {
+            commandArgs.add("--table");
+            commandArgs.add(taskParams.tableName);
+          }
+          if (backupTableParams.keyspace != null) {
+            commandArgs.add("--keyspace");
+            commandArgs.add(backupTableParams.keyspace);
+          }
+        }
         Customer customer = Customer.find.where().idEq(universe.customerId).findUnique();
         CustomerConfig customerConfig = CustomerConfig.get(customer.uuid,
                                                            backupTableParams.storageConfigUUID);
@@ -130,6 +157,10 @@ public class TableManager extends DevopsBase {
       // TODO: Add support for TLS connections for bulk-loading.
       // Tracked by issue: https://github.com/YugaByte/yugabyte-db/issues/1864
       case BULK_IMPORT:
+        commandArgs.add("--table");
+        commandArgs.add(taskParams.tableName);
+        commandArgs.add("--keyspace");
+        commandArgs.add(taskParams.keyspace);
         BulkImportParams bulkImportParams = (BulkImportParams) taskParams;
         ReleaseManager.ReleaseMetadata metadata =
             releaseManager.getReleaseByVersion(userIntent.ybSoftwareVersion);

managed/src/main/java/com/yugabyte/yw/commissioner/tasks/MultiTableBackup.java
@@ -56,6 +56,7 @@ public class MultiTableBackup extends UniverseTaskBase {
     public String keyspace = null;
     public List<UUID> tableUUIDList = new ArrayList<UUID>();
     public boolean sse = false;
+    public boolean transactionalBackup = false;
   }
 
   public Params params() {
@@ -80,28 +81,37 @@ public class MultiTableBackup extends UniverseTaskBase {
       Universe universe = Universe.get(params().universeUUID);
       String masterAddresses = universe.getMasterAddresses(true);
       String certificate = universe.getCertificate();
+
       YBClient client = null;
-      List<BackupTableParams> tableBackupParams = new ArrayList<>();
       Set<UUID> tableSet = new HashSet<UUID>(params().tableUUIDList);
+      BackupTableParams tableBackupParams = new BackupTableParams();
+      List<BackupTableParams> backupParamsList = new ArrayList<>();
       try {
         client = ybService.getClient(masterAddresses, certificate);
         // If user specified the list of tables, only get info for those tables.
         if (tableSet.size() != 0) {
           for (UUID tableUUID : tableSet) {
             GetTableSchemaResponse tableSchema = client.getTableSchemaByUUID(
-                tableUUID.toString().replace("-", ""));
+              tableUUID.toString().replace("-", ""));
             // If table is not REDIS or YCQL, ignore.
             if (tableSchema.getTableType() == TableType.PGSQL_TABLE_TYPE ||
-                tableSchema.getTableType() == TableType.TRANSACTION_STATUS_TABLE_TYPE) {
+              tableSchema.getTableType() == TableType.TRANSACTION_STATUS_TABLE_TYPE) {
               LOG.info("Skipping backup of table with UUID: " + tableUUID);
               continue;
             }
-            BackupTableParams backupParams = populateBackupParams(tableSchema.getNamespace(),
-                                                                  tableSchema.getTableName(),
-                                                                  tableUUID);
-            tableBackupParams.add(backupParams);
-            LOG.info("Queuing backup for table {}:{}", backupParams.keyspace,
-                     backupParams.tableName);
+            if (params().transactionalBackup) {
+              populateBackupParams(tableBackupParams,
+                tableSchema.getNamespace(),
+                tableSchema.getTableName(),
+                tableUUID);
+            } else {
+              BackupTableParams backupParams = createBackupParams(tableSchema.getNamespace(),
+                tableSchema.getTableName(),
+                tableUUID);
+              backupParamsList.add(backupParams);
+            }
+            LOG.info("Queuing backup for table {}:{}", tableSchema.getNamespace(),
+              tableSchema.getTableName());
           }
         }
         // If user did not specify tables, that means we need to backup all tables.
@@ -114,20 +124,28 @@ public class MultiTableBackup extends UniverseTaskBase {
             UUID tableUUID = getUUIDRepresentation(tableUUIDString);
             // If table is not REDIS or YCQL, ignore.
             if (tableType == TableType.PGSQL_TABLE_TYPE ||
-                tableType == TableType.TRANSACTION_STATUS_TABLE_TYPE ||
-                table.getRelationType() == RelationType.INDEX_TABLE_RELATION) {
+              tableType == TableType.TRANSACTION_STATUS_TABLE_TYPE ||
+              table.getRelationType() == RelationType.INDEX_TABLE_RELATION) {
               LOG.info("Skipping backup of table with UUID: " + tableUUID);
               continue;
             }
-            String tableKeySpace = table.getNamespace().getName().toString();
-            BackupTableParams backupParams = populateBackupParams(tableKeySpace,
-                                                                  table.getName(),
-                                                                  tableUUID);
-            tableBackupParams.add(backupParams);
-            LOG.info("Queuing backup for table {}:{}", backupParams.keyspace,
-                   backupParams.tableName);
+            String tableKeySpace = table.getNamespace().getName();
+            if (params().transactionalBackup) {
+              populateBackupParams(tableBackupParams,
+                tableKeySpace,
+                table.getName(),
+                tableUUID);
+            } else {
+              BackupTableParams backupParams = createBackupParams(tableKeySpace,
+                table.getName(),
+                tableUUID);
+              backupParamsList.add(backupParams);
             }
+
+            LOG.info("Queuing backup for table {}:{}", tableKeySpace,
+              table.getName());
           }
+        }
         ybService.closeClient(client, masterAddresses);
       } catch (Exception e) {
         LOG.error("Failed to get list of tables in universe " + params().universeUUID, e);
@@ -135,15 +153,22 @@ public class MultiTableBackup extends UniverseTaskBase {
         unlockUniverseForUpdate();
         throw new RuntimeException(e);
       }
+
       updateBackupState(true);
 
       subTaskGroupQueue = new SubTaskGroupQueue(userTaskUUID);
 
       LOG.info("Successfully started scheduled backup of tables.");
-      for (BackupTableParams tableParams : tableBackupParams) {
-        Backup backup = Backup.create(params().customerUUID, tableParams);
-        createTableBackupTask(tableParams, backup).setSubTaskGroupType(
+      if (params().transactionalBackup) {
+        Backup backup = Backup.create(params().customerUUID, tableBackupParams);
+        createTableBackupTask(tableBackupParams, backup).setSubTaskGroupType(
+          UserTaskDetails.SubTaskGroupType.CreatingTableBackup);
+      } else {
+        for (BackupTableParams tableParams : backupParamsList) {
+          Backup backup = Backup.create(params().customerUUID, tableParams);
+          createTableBackupTask(tableParams, backup).setSubTaskGroupType(
             UserTaskDetails.SubTaskGroupType.CreatingTableBackup);
+        }
       }
 
       // Marks the update of this universe as a success only if all the tasks before it succeeded.
@@ -166,7 +191,28 @@ public class MultiTableBackup extends UniverseTaskBase {
     LOG.info("Finished {} task.", getName());
   }
 
-  private BackupTableParams populateBackupParams(String tableKeySpace, String tableName,
+  // Helper method to update passed in reference object
+  private void populateBackupParams(BackupTableParams backupParams,
+                                    String tableKeySpace,
+                                    String tableName,
+                                    UUID tableUUID) {
+
+    backupParams.actionType = BackupTableParams.ActionType.CREATE;
+    backupParams.storageConfigUUID = params().storageConfigUUID;
+    backupParams.universeUUID = params().universeUUID;
+    backupParams.sse = params().sse;
+    backupParams.keyspace = tableKeySpace;
+    backupParams.transactionalBackup = params().transactionalBackup;
+
+    if (backupParams.tableNameList == null) {
+      backupParams.tableNameList = new ArrayList<String>();
+      backupParams.tableUUIDList = new ArrayList<UUID>();
+    }
+    backupParams.tableNameList.add(tableName);
+    backupParams.tableUUIDList.add(tableUUID);
+  }
+
+  private BackupTableParams createBackupParams(String tableKeySpace, String tableName,
                                                  UUID tableUUID) {
     BackupTableParams backupParams = new BackupTableParams();
     backupParams.keyspace = tableKeySpace;

managed/src/main/java/com/yugabyte/yw/commissioner/tasks/UniverseTaskBase.java
@@ -939,7 +939,7 @@ public abstract class UniverseTaskBase extends AbstractTaskBase {
     subTaskGroupQueue.add(subTaskGroup);
     return subTaskGroup;
   }
-  
+
   /**
    * Creates a task to wait for leaders to be on preferred regions only.
    */
@@ -1064,11 +1064,11 @@ public abstract class UniverseTaskBase extends AbstractTaskBase {
 
   private boolean isServerAlive(NodeDetails node, ServerType server, String masterAddrs) {
     YBClientService ybService = Play.current().injector().instanceOf(YBClientService.class);
-    
+
     Universe universe = Universe.get(taskParams().universeUUID);
     String certificate = universe.getCertificate();
     YBClient client = ybService.getClient(masterAddrs, certificate);
-    
+
     HostAndPort hp = HostAndPort.fromParts(node.cloudInfo.private_ip,
         server == ServerType.MASTER ? node.masterRpcPort : node.tserverRpcPort);
     return client.waitForServer(hp, 5000);

managed/src/main/java/com/yugabyte/yw/controllers/TablesController.java
@@ -309,6 +309,10 @@ public class TablesController extends AuthenticatedController {
     }
 
     MultiTableBackup.Params taskParams = formData.get();
+    if (taskParams.storageConfigUUID == null) {
+      String errMsg = "Missing StorageConfig UUID: " + taskParams.storageConfigUUID;
+      return ApiResponse.error(BAD_REQUEST, errMsg);
+    }
     CustomerConfig storageConfig = CustomerConfig.get(customerUUID, taskParams.storageConfigUUID);
     if (storageConfig == null) {
       String errMsg = "Invalid StorageConfig UUID: " + taskParams.storageConfigUUID;

managed/src/main/java/com/yugabyte/yw/forms/BackupTableParams.java
@@ -4,6 +4,7 @@ package com.yugabyte.yw.forms;
 
 import play.data.validation.Constraints;
 
+import java.util.List;
 import java.util.UUID;
 
 public class BackupTableParams extends TableManagerParams {
@@ -22,6 +23,10 @@ public class BackupTableParams extends TableManagerParams {
   @Constraints.Required
   public ActionType actionType;
 
+  public List<String> tableNameList;
+
+  public List<UUID> tableUUIDList;
+
   // Specifies the frequency for running the backup in milliseconds.
   public long schedulingFrequency = 0L;
 
@@ -34,4 +39,7 @@ public class BackupTableParams extends TableManagerParams {
 
   // Should backup script enable verbose logging.
   public boolean enableVerboseLogs = false;
+
+  // Should the backup be transactional across tables
+  public boolean transactionalBackup = false;
 }

managed/src/main/java/com/yugabyte/yw/models/Backup.java
@@ -86,15 +86,22 @@ public class Backup extends Model {
   // univ-<univ_uuid>/backup-<timestamp>-<something_to_disambiguate_from_yugaware>/table-keyspace.table_name.table_uuid
   private void updateStorageLocation(BackupTableParams params) {
     CustomerConfig customerConfig = CustomerConfig.get(customerUUID, params.storageConfigUUID);
-    params.storageLocation = String.format("univ-%s/backup-%s-%d/table-%s.%s",
+    if (params.tableUUIDList != null) {
+      params.storageLocation = String.format("univ-%s/backup-%s-%d/multi-table-%s",
+        params.universeUUID, tsFormat.format(new Date()), abs(backupUUID.hashCode()),
+        params.keyspace);
+    } else {
+      params.storageLocation = String.format("univ-%s/backup-%s-%d/table-%s.%s",
         params.universeUUID, tsFormat.format(new Date()), abs(backupUUID.hashCode()),
         params.keyspace, params.tableName);
-    if (params.tableUUID != null) {
-      params.storageLocation = String.format("%s-%s",
+      if (params.tableUUID != null) {
+        params.storageLocation = String.format("%s-%s",
           params.storageLocation,
           params.tableUUID.toString().replace("-", "")
-      );
+        );
+      }
     }
+
     if (customerConfig != null) {
       // TODO: These values, S3 vs NFS / S3_BUCKET vs NFS_PATH come from UI right now...
       JsonNode storageNode = customerConfig.getData().get("BACKUP_LOCATION");

managed/src/test/java/com/yugabyte/yw/commissioner/tasks/MultiTableBackupTest.java
@@ -125,13 +125,14 @@ public class MultiTableBackupTest extends CommissionerBaseTest {
     when(mockSchemaResponse3.getTableType()).thenReturn(TableType.PGSQL_TABLE_TYPE);
   }
 
-  private TaskInfo submitTask(String keyspace, List<UUID> tableUUIDs) {
+  private TaskInfo submitTask(String keyspace, List<UUID> tableUUIDs, boolean transactional) {
     MultiTableBackup.Params backupTableParams = new MultiTableBackup.Params();
     backupTableParams.universeUUID = defaultUniverse.universeUUID;
     backupTableParams.customerUUID = defaultCustomer.uuid;
     backupTableParams.keyspace = keyspace;
     backupTableParams.storageConfigUUID = UUID.randomUUID();
     backupTableParams.tableUUIDList = tableUUIDs;
+    backupTableParams.transactionalBackup = transactional;
     try {
       UUID taskUUID = commissioner.submit(TaskType.MultiTableBackup, backupTableParams);
       CustomerTask.create(defaultCustomer, defaultUniverse.universeUUID, taskUUID,
@@ -142,6 +143,10 @@ public class MultiTableBackupTest extends CommissionerBaseTest {
       assertNull(e.getMessage());
     }
     return null;
+  }
+
+  private TaskInfo submitTask(String keyspace, List<UUID> tableUUIDs) {
+    return submitTask(keyspace, tableUUIDs, false);
   }
 
   @Test
@@ -189,6 +194,24 @@ public class MultiTableBackupTest extends CommissionerBaseTest {
     tableUUIDs.add(table3UUID);
     TaskInfo taskInfo = submitTask(null, tableUUIDs);
     verify(mockTableManager, times(2)).createBackup(any());
+    assertEquals(TaskInfo.State.Success, taskInfo.getTaskState());
+  }
+
+  @Test
+  public void testTransactionalMultiTableBackupList() {
+    Map<String, String> config = new HashMap<>();
+    config.put(Universe.TAKE_BACKUPS, "true");
+    defaultUniverse.setConfig(config);
+    ShellProcessHandler.ShellResponse shellResponse =  new ShellProcessHandler.ShellResponse();
+    shellResponse.message = "{\"success\": true}";
+    shellResponse.code = 0;
+    when(mockTableManager.createBackup(any())).thenReturn(shellResponse);
+    List<UUID> tableUUIDs = new ArrayList<UUID>();
+    tableUUIDs.add(table1UUID);
+    tableUUIDs.add(table2UUID);
+    tableUUIDs.add(table3UUID);
+    TaskInfo taskInfo = submitTask(null, tableUUIDs, true);
+    verify(mockTableManager, times(1)).createBackup(any());
     assertEquals(TaskInfo.State.Success, taskInfo.getTaskState());
   }
 

commit b317c2fac22bb130d504ba7c08e0e2560e8d5468

[YW] Fix to restore path due to previous multi-table backup change
Summary: Fix restore backup functionality by removing the required constraint on the form class. The issue was caused in 508e3c3c553329de3555c97d9bfe0a4024cf608e when the tableName and keyspace was removed from the restore backup flow due multi-table backups not having a single table. The API will return 400 error if the keyspace and table are missing due to it being required from play framework form constraints.

Test Plan: Backup multiple YCQL tables transactionally and then try restoring them all at once.

Reviewers: bogdan, rahuldesirazu, arnav

Reviewed By: rahuldesirazu

Subscribers: jenkins-bot, yugaware

Differential Revision: https://phabricator.dev.yugabyte.com/D8853

managed/src/main/java/com/yugabyte/yw/common/TableManager.java
@@ -90,8 +90,10 @@ public class TableManager extends DevopsBase {
               commandArgs.add(backupTableParams.tableUUIDList.get(listIndex).toString());
             }
           } else {
-            commandArgs.add("--table");
-            commandArgs.add(taskParams.tableName);
+            if (backupTableParams.tableName != null) {
+              commandArgs.add("--table");
+              commandArgs.add(taskParams.tableName);
+            }
             commandArgs.add("--keyspace");
             commandArgs.add(taskParams.keyspace);
           }

managed/src/main/java/com/yugabyte/yw/forms/BulkImportParams.java
@@ -5,6 +5,13 @@ package com.yugabyte.yw.forms;
 import play.data.validation.Constraints;
 
 public class BulkImportParams extends TableManagerParams {
+  // Repeating this field here to add constraint
+  @Constraints.Required
+  public String keyspace;
+
+  @Constraints.Required
+  public String tableName;
+
   // S3 bucket containing data to be bulk imported (should be of format s3://<path>/)
   @Constraints.Required
   public String s3Bucket;

managed/src/main/java/com/yugabyte/yw/forms/TableManagerParams.java
@@ -5,10 +5,8 @@ import play.data.validation.Constraints;
 import java.util.UUID;
 
 public class TableManagerParams extends UniverseTaskParams {
-  @Constraints.Required
   public String keyspace;
 
-  @Constraints.Required
   public String tableName;
 
   public UUID tableUUID;

managed/src/test/java/com/yugabyte/yw/controllers/BackupsControllerTest.java
@@ -114,8 +114,6 @@ public class BackupsControllerTest extends FakeDBApplication {
     assertEquals(BAD_REQUEST, result.status());
     JsonNode resultJson = Json.parse(contentAsString(result));
     assertErrorNodeValue(resultJson, "storageConfigUUID", "This field is required");
-    assertErrorNodeValue(resultJson, "keyspace", "This field is required");
-    assertErrorNodeValue(resultJson, "tableName", "This field is required");
     assertAuditEntry(0, defaultCustomer.uuid);
   }
 

managed/src/test/java/com/yugabyte/yw/controllers/TablesControllerTest.java
@@ -494,8 +494,6 @@ public class TablesControllerTest extends WithApplication {
     JsonNode resultJson = Json.parse(contentAsString(result));
     assertEquals(BAD_REQUEST, result.status());
     assertErrorNodeValue(resultJson, "storageConfigUUID", "This field is required");
-    assertErrorNodeValue(resultJson, "keyspace", "This field is required");
-    assertErrorNodeValue(resultJson, "tableName", "This field is required");
     assertErrorNodeValue(resultJson, "actionType", "This field is required");
     assertAuditEntry(0, customer.uuid);
   }