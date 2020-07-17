bd8f3fe6431913d1281c04efc7b0d352e1dcf3c9
Sergey Shevchenko <sshevchenko@yugabyte.com>
2020-07-17 15:37:39 -0700

    [YW][#4578 - part 1] New UI components and redesigned create/edit universe pages
    Summary:
    - introduce UI kit with new components for redesigned create/edit universe pages
    - setup folders/components/routes structure for new create/edit universe pages
    
    Test Plan:
    Run UI as usual
    No changes to existing functionality are expected
    New components are showcased at http://localhost:3000/new/universes/create page
    
    Reviewers: ram, andrew
    
    Reviewed By: andrew
    
    Subscribers: ui
    
    Differential Revision: https://phabricator.dev.yugabyte.com/D8893

---
+++ managed/ui/src/redesign/universes/wizard/InstanceConfig.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export const InstanceConfig = () => {
+  return (
+    <div>Instance Configuration</div>
+  );
+};
---
+++ managed/ui/src/redesign/universes/wizard/SecurityConfig.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export const SecurityConfig = () => {
+  return (
+    <div>Security Configuration</div>
+  );
+};
