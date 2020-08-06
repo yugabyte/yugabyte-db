063c843c9048adc27031396fcde0b007affc04da
Sergei Politov <spolitov@gmail.com>
2020-08-06 13:37:22 +0300

    [#5321] Skip tablets without intents during commit
    Summary:
    In some scenarios, a write request could be skipped by a tablet.
    For instance in case of an update of a non-existent row.
    In this case, transaction commit will produce "Apply of unknown transaction" warning.
    
    Tablets without intents should be ignored during commit.
    
    Test Plan: ybd --gtest_filter PgTxnTest.EmptyUpdate
    
    Reviewers: mikhail
    
    Reviewed By: mikhail
    
    Subscribers: ybase
    
    Differential Revision: https://phabricator.dev.yugabyte.com/D9106

---
+++ src/yb/yql/pgwrapper/pg_mini_test_base.h
@@ -0,0 +1,56 @@
+// Copyright (c) YugaByte, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+// in compliance with the License.  You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software distributed under the License
+// is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+// or implied.  See the License for the specific language governing permissions and limitations
+// under the License.
+//
+
+#ifndef YB_YQL_PGWRAPPER_PG_MINI_TEST_BASE_H
+#define YB_YQL_PGWRAPPER_PG_MINI_TEST_BASE_H
+
+#include "yb/integration-tests/mini_cluster.h"
+#include "yb/integration-tests/yb_mini_cluster_test_base.h"
+
+#include "yb/yql/pgwrapper/libpq_utils.h"
+#include "yb/yql/pgwrapper/pg_wrapper.h"
+
+namespace yb {
+namespace pgwrapper {
+
+class PgMiniTestBase : public YBMiniClusterTestBase<MiniCluster> {
+ protected:
+  // This allows modifying flags before we start the postgres process in SetUp.
+  virtual void BeforePgProcessStart() {
+  }
+
+  void DoTearDown() override;
+
+  void SetUp() override;
+
+  virtual int NumTabletServers() {
+    return 3;
+  }
+
+  Result<PGConn> Connect() {
+    return PGConn::Connect(pg_host_port_);
+  }
+
+  Result<PGConn> ConnectToDB(const std::string& dbname) {
+    return PGConn::Connect(pg_host_port_, dbname);
+  }
+
+ private:
+  std::unique_ptr<PgSupervisor> pg_supervisor_;
+  HostPort pg_host_port_;
+};
+
+} // namespace pgwrapper
+} // namespace yb
+
+#endif // YB_YQL_PGWRAPPER_PG_MINI_TEST_BASE_H
