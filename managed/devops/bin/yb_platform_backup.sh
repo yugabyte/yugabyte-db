7af68f4569e2c62ebfacb76e3a48a8ee85ae8700
Wesley Wang <2791412+WesleyW@users.noreply.github.com>
2020-08-24 16:26:26 -0700

    Add docker- and vm-aware options to YW backup script #5120, #5615, #5612, #5614
    Summary: YW backup script is now aware of docker/vm environment and will do prometheus snapshots.
    
    Test Plan: take snnapshot and restore on Replicated-based and VM-based
    
    Reviewers: rahuldesirazu, sanketh
    
    Reviewed By: sanketh
    
    Subscribers: jenkins-bot, yugaware
    
    Differential Revision: https://phabricator.dev.yugabyte.com/D9165

--- managed/devops/bin/yb_platform_backup.sh
+++ managed/devops/bin/yb_platform_backup.sh
@@ -12,6 +12,23 @@ set -euo pipefail
 
 YUGAWARE_DUMP_FNAME="yugaware_dump.sql"
 
+DOCKER_BASED="$(docker ps -a | grep yugaware)"
+# VM based default.
+PROMETHEUS_DATA_DIR="/var/lib/prometheus"
+# Docker (Replicated) based default.
+if [[ -z $DOCKER_BASED ]]; then
+  PROMETHEUS_DATA_DIR="/opt/yugabyte/prometheusv2"
+fi
+
+# Takes docker container and command as arguments. Executes docker cmd if docker-based or not.
+docker_aware_cmd() {
+  if [[ -z "$DOCKER_BASED" ]]; then
+    docker exec -it $0 $1
+  else
+    sh -c "$1"
+  fi
+}
+
 create_backup() {
   now=$(date +"%y-%m-%d-%H-%M")
   output_path=$1
@@ -20,12 +37,18 @@ create_backup() {
   if [[ "$3" = true ]]; then
     exclude_prometheus_flag=" --exclude prometheus* "
   fi
-  tar="${output_path}/backup_${now}.tgz"
+  tarname="${output_path}/backup_${now}.tgz"
   trap "cleanup ${data_dir}/${YUGAWARE_DUMP_FNAME}" EXIT
-  sh -c "docker exec -i postgres pg_dump -U postgres -Fc yugaware > \
+  docker_aware_cmd "postgres" "pg_dump -U postgres -Fc yugaware > \
                                  ${data_dir}/${YUGAWARE_DUMP_FNAME}"
-  tar $exclude_prometheus_flag --exclude "postgresql" -czf $tar -C $data_dir .
-  echo "Finished creating backup $tar"
+  # Backup prometheus data.
+  if [[ "$3" = false ]]; then
+    snapshot_dir=$(curl -X POST http://localhost:9090/api/v1/admin/tsdb/snapshot |
+      python -c "import sys, json; print(json.load(sys.stdin)['data']['name'])")
+    sudo cp -aR "$PROMETHEUS_DATA_DIR/snapshots/$snapshot_dir" "$data_dir/prometheus_snapshot"
+  fi
+  tar $exclude_prometheus_flag --exclude "postgresql" -czf $tarname -C $data_dir .
+  echo "Finished creating backup $tarname"
 }
 
 restore_backup() {
@@ -34,18 +57,38 @@ restore_backup() {
   yugaware_dump="${destination}/${YUGAWARE_DUMP_FNAME}"
   trap "cleanup $yugaware_dump" EXIT
   tar -xzf $input_path --directory $destination
-  sh -c "docker exec -i postgres pg_restore -U postgres -d yugaware -c < ${yugaware_dump}"
+  docker_aware_cmd "postgres" "pg_restore -U postgres -d yugaware -c < ${yugaware_dump}"
+  # Restore prometheus data.
+  if [[ -d "$destination/prometheus_snapshot" ]]; then
+    set_prometheus_data_dir
+    if [[ -z "$PROMETHEUS_DATA_DIR" ]]; then
+      echo "Failed to find prometheud data directory."
+      exit 1
+    fi
+    sudo rm -rf "${PROMETHEUS_DATA_DIR}/*"
+    sudo mv "$destination/prometheus_snapshot/*" $PROMETHEUS_DATA_DIR
+    if [[ -z "$DOCKER_BASED" ]]; then
+      sudo chown -R nobody:nogroup "$PROMETHEUS_DATA_DIR"
+    else
+      sudo chown -R prometheus:prometheus "$PROMETHEUS_DATA_DIR"
+    fi
+  fi
+  # Create following directory if it wasn't created yet so restore will succeed.
+  mkdir "$destination/release"
   echo "Finished restoring backup"
 }
 
+set_prometheus_data_dir() {
+  PROMETHEUS_DATA_DIR=$(curl http://localhost:9090/api/v1/status/flags |
+    python -c "import sys, json; print(json.load(sys.stdin)['data']['storage.tsdb.path'])")
+}
+
 print_backup_usage() {
-  echo "ERROR: Backup Usage"
   echo "$0 backup --output <output_path> [--data_dir <data_dir>] [--exclude_prometheus]"
   echo "Backup YW to a specified output location"
 }
 
 print_restore_usage() {
-  echo "ERROR: Restore usage"
   echo "$0 restore --input <input_path> [--destination <desination>]"
   echo "Restore YW from a specified input location"
 }
@@ -53,6 +96,15 @@ print_restore_usage() {
 cleanup () {
   rm -f "$1"
 }
+
+if [[ $# -eq 0 ]]; then
+  echo "ERROR: Please use one of the following:"
+  echo ""
+  print_backup_usage
+  echo ""
+  print_restore_usage
+  exit 1
+fi
 
 command=$1
 shift
@@ -75,14 +127,20 @@ case $command in
           data_dir=$2
           shift 2
           ;;
+        --verbose)
+          set -x
+          shift
+          ;;
         *)
           echo "$1"
+          echo "ERROR: Backup Usage"
           print_backup_usage
           exit 1
       esac
     done
 
     if [[ -z "$output_path" ]]; then
+      echo "ERROR: Backup Usage"
       print_backup_usage
       exit 1
     fi
@@ -101,12 +159,18 @@ case $command in
           destination=$2
           shift 2
           ;;
+        --verbose)
+          set -x
+          shift
+          ;;
         *)
+          echo "ERROR: Restore usage"
           print_restore_usage
           exit 1
       esac
     done
     if [[ -z "$input_path" ]]; then
+      echo "ERROR: Restore usage"
       print_restore_usage
       exit 1
     fi
