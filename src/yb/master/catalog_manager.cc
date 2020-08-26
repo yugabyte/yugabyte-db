ae447c5d849bda1c702f8c99eb91da87f7364373
Rahul Desirazu <rahuldesirazu@users.noreply.github.com>
2020-08-25 19:00:12 -0700

    [#5496] 2.2: Remove --tablegroup_master_only from Backport
    Test Plan: Jenkins: compile only, rebase: 2.2
    
    Subscribers: bogdan
    
    Differential Revision: https://phabricator.dev.yugabyte.com/D9252

--- src/yb/master/catalog_manager.cc
+++ src/yb/master/catalog_manager.cc
@@ -290,10 +290,6 @@ DEFINE_test_flag(bool, simulate_crash_after_table_marked_deleting, false,
 
 DECLARE_int32(yb_client_admin_operation_timeout_sec);
 
-DEFINE_test_flag(bool, tablegroup_master_only, false,
-                 "This is only for MasterTest to be able to test tablegroups without the"
-                 " transaction status table being created.");
-
 DEFINE_bool(enable_register_ts_from_raft, false, "Whether to register a tserver from the consensus "
                                                  "information of a reported tablet.");
 
