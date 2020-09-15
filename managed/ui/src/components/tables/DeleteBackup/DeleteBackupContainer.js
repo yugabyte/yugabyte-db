616b651cebec6424e5495672d0258aa687c18283
Arnav Agarwal <14933889+Arnav15@users.noreply.github.com>
2020-09-16 01:35:44 +0530

    [#4493]: Functionality to delete backups through platform.
    Summary:
    Adding an endpoint as well as schedulable task to delete old backups after a specified time
    period.
    1) Backup can be deleted through just a stand alone endpoint.
    2) For scheduled backups, the user can set an expiry time, and the scheduler will delete the backup
    after the specified amount of time.
    The backups now have an assosciated schedule (if created via a schedule) as well as an expiry. The
    backups only get deleted as long as the universe exists (this is also due to the fact that we don't
    have a way to see orphaned backups).
    
    Test Plan:
    Created a stand-alone backup and deleted it. Also created a schedule and saw that the old
    backups got deleted after the set amount of time. Also tested it out on s3, gcp and azure backups.
    UI mocks: {F14065}
    
    {F14066}
    
    {F14067}
    
    Reviewers: wesley, daniel, sanketh
    
    Reviewed By: sanketh
    
    Subscribers: andrew, zyu, jenkins-bot, yugaware
    
    Differential Revision: https://phabricator.dev.yugabyte.com/D9361

---
+++ managed/ui/src/components/tables/DeleteBackup/DeleteBackupContainer.js
@@ -0,0 +1,17 @@
+// Copyright (c) YugaByte, Inc.
+
+import { connect } from 'react-redux';
+import { DeleteBackup } from '../';
+import { deleteBackup, deleteBackupResponse } from '../../../actions/tables';
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    deleteBackup: (backupUUID) => {
+      dispatch(deleteBackup(backupUUID)).then((response) => {
+        dispatch(deleteBackupResponse(response));
+      })
+    }
+  };
+};
+
+export default connect(null, mapDispatchToProps)(DeleteBackup);
