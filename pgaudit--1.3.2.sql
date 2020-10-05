08e259ae81e337d3b2bd2ef837d216b05b3348b7
David Steele <david@pgmasters.net>
2020-10-05 15:44:09 -0400

    Stamp 1.3.2.

--- pgaudit--1.3.2.sql
+++ pgaudit--1.3.2.sql
@@ -1,4 +1,4 @@
-/* pgaudit/pgaudit--1.3.1.sql */
+/* pgaudit/pgaudit--1.3.2.sql */
 
 -- complain if script is sourced in psql, rather than via CREATE EXTENSION
 \echo Use "CREATE EXTENSION pgaudit" to load this file.\quit
