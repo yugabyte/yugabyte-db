fa1ba0af4f9654de0b34d53ac0f519c5c386bc1e
Oleg Loginov <loginov@yugabyte.com>
2020-10-11 22:34:02 +0300

    #5958:[backup][YCQL] Fixed incorrect column-ids in restored table if original table was altered.
    Summary:
    Before the fix `CatalogManager::CreateTable()` always sets column ids into 0,1,2,3..
    Due to ALTER TABLE statements columns can be added/removed. (E.g. into 0,2,3 after `ALTER TABLE t DROP c1;`)
    But on the backup-restore step RecreateTable() calls CreateTable(), which sets the column ids into '0,1,2' instead of using existing in the backup ids.
    As result the restored table schema references to wrong columns.
    
    The fix include the following code changes:
    - In `CatalogManager::RecreateTable()`: removed code for column ids clean-up.
    - In `ValidateCreateTableSchema()`:  added ability to allow incoming column ids.
    - In `CatalogManager::CreateTable()`: the table schema must use old (provided by the caller) column ids.
    
    Test Plan: ybd --java-test org.yb.cql.TestYbBackup#testAlteredYCQLTableBackup
    
    Reviewers: mihnea, mikhail, neil, bogdan
    
    Reviewed By: bogdan
    
    Subscribers: kannan, yql
    
    Differential Revision: https://phabricator.dev.yugabyte.com/D9608

--- java/yb-cql/src/test/java/org/yb/cql/TestYbBackup.java
+++ java/yb-cql/src/test/java/org/yb/cql/TestYbBackup.java
@@ -591,4 +591,34 @@ public class TestYbBackup extends BaseCQLTest {
                 "Row[1, {\"a\":{\"b\":\"b1\"},\"c\":1}]");
     assertQuery("select * from ks7.test_json_tbl where h=9999;", "");
   }
+
+  @Test
+  public void testAlteredYCQLTableBackup() throws Exception {
+    session.execute("create table test_tbl (h int primary key, a int, b float) " +
+                    "with transactions = { 'enabled' : true }");
+
+    for (int i = 1; i <= 2000; ++i) {
+      session.execute("insert into test_tbl (h, a, b) values" +
+                      " (" + String.valueOf(i) +                     // h
+                      ", " + String.valueOf(100 + i) +               // a
+                      ", " + String.valueOf(2.14 + (float)i) + ")"); // b
+    }
+
+    session.execute("alter table test_tbl drop a;");
+    runYbBackupCreate("--keyspace", DEFAULT_TEST_KEYSPACE, "--table", "test_tbl");
+    session.execute("insert into test_tbl (h, b) values (9999, 8.9)");
+
+    runYbBackupRestore("--keyspace", "ks1");
+    assertQuery("select * from " + DEFAULT_TEST_KEYSPACE + ".test_tbl where h=1", "Row[1, 3.14]");
+    assertQuery("select * from " + DEFAULT_TEST_KEYSPACE + ".test_tbl where h=2000",
+                "Row[2000, 2002.14]");
+    assertQuery("select * from " + DEFAULT_TEST_KEYSPACE + ".test_tbl where h=9999",
+                "Row[9999, 8.9]");
+
+    assertQuery("select * from ks1.test_tbl where h=1", "Row[1, 3.14]");
+    assertQuery("select b from ks1.test_tbl where h=1", "Row[3.14]");
+    assertQuery("select * from ks1.test_tbl where h=2000", "Row[2000, 2002.14]");
+    assertQuery("select h from ks1.test_tbl where h=2000", "Row[2000]");
+    assertQuery("select * from ks1.test_tbl where h=9999", "");
+  }
 }
