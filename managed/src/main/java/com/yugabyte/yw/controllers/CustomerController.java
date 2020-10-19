6db96efe0ec88134dc60c1896ed6ad745cb33c73
Sanketh I <sanketh@yugabyte.com>
2020-10-18 19:17:20 -0700

    [#5548] [YW] Fix mount point for disk usage query in onprem universes
    Summary:
    Disk usage is based on prometheus queries on node exporter
    metrics. These metrics are filtered by mountpoint /mnt by default. For
    onprem universes, where a custom storage path is specified in the
    provider, /mnt is very likely incorrect.
    
    In this diff, the mount root corresponding to the onprem storage path is
    recorded during a `ybcloud.sh instance list` in the node details inside
    the universe details json. When a metrics query is performed against an
    onprem universe, this node details entry is consulted and the mountpoint
    filter is modified to use this mount root instead of /mnt.
    
    To make the disk usage UI show up properly on an already existing onprem
    universe, follow the process below after upgrading to this change:
    1. Pick any node in the onprem universe on the Nodes page, click "Stop
    Processes"
    2. Click "Release Instance" on the same node
    3. Click "Add Node" on the same node.
    4. Go back to the overview page and observe disk usage metrics.
    
    Note that this approach has some corner cases that will not work
    precisely:
    1. Mount roots can technically be different on multiple hosts but an
    arbitrary one will be chosen for the query
    2. Disk usage queries for universes with onprem primary clusters and a
    non-onprem read replica cluster will be inaccurate.
    
    Test Plan:
    1. Create an on prem universe with storage root /storage and
    verify that disk usage now shows up correctly.
    
    2.Ran the standalone ybcloud instance list command with a comma
    separated list of two storage paths and verified that two mount roots
    are returned in the json.
    
    Reviewers: arnav, wesley
    
    Reviewed By: wesley
    
    Subscribers: jenkins-bot, yugaware
    
    Differential Revision: https://phabricator.dev.yugabyte.com/D9666

--- managed/src/main/java/com/yugabyte/yw/controllers/CustomerController.java
+++ managed/src/main/java/com/yugabyte/yw/controllers/CustomerController.java
@@ -16,6 +16,7 @@ package com.yugabyte.yw.controllers;
 
 import java.util.List;
 import java.util.Map;
+import java.util.HashMap;
 import java.util.UUID;
 import java.util.stream.Collectors;
 
@@ -26,6 +27,7 @@ import com.fasterxml.jackson.databind.node.ObjectNode;
 import com.google.common.collect.ImmutableList;
 import com.google.inject.Inject;
 import com.yugabyte.yw.commissioner.Common;
+import com.yugabyte.yw.commissioner.Common.CloudType;
 import com.yugabyte.yw.common.ApiResponse;
 import com.yugabyte.yw.common.CallHomeManager;
 import com.yugabyte.yw.common.CloudQueryHelper;
@@ -222,7 +224,7 @@ public class CustomerController extends AuthenticatedController {
       return ApiResponse.error(BAD_REQUEST, formData.errorsAsJson());
     }
     Map<String, String> params = formData.data();
-
+    HashMap<String, Map<String, String>> filterOverrides = new HashMap<>();
     // Given we have a limitation on not being able to rename the pod labels in
     // kubernetes cadvisor metrics, we try to see if the metric being queried is for
     // container or not, and use pod_name vs exported_instance accordingly.
@@ -275,10 +277,13 @@ public class CustomerController extends AuthenticatedController {
           filterJson.put(universeFilterLabel, completeNamespace);
         }
       } else {
-        filterJson.put(universeFilterLabel, params.remove("nodePrefix"));
+        final String nodePrefix = params.remove("nodePrefix");
+        filterJson.put(universeFilterLabel, nodePrefix);
         if (params.containsKey("nodeName")) {
           filterJson.put(nodeFilterLabel, params.remove("nodeName"));
         }
+
+        filterOverrides.putAll(getFilterOverrides(customer, nodePrefix, formData.get()));
       }
     }
     if (params.containsKey("tableName")) {
@@ -286,7 +291,7 @@ public class CustomerController extends AuthenticatedController {
     }
     params.put("filters", Json.stringify(filterJson));
     try {
-      JsonNode response = metricQueryHelper.query(formData.get().metrics, params);
+      JsonNode response = metricQueryHelper.query(formData.get().metrics, params, filterOverrides);
       if (response.has("error")) {
         return ApiResponse.error(BAD_REQUEST, response.get("error"));
       }
@@ -308,5 +313,42 @@ public class CustomerController extends AuthenticatedController {
         Common.CloudType.gcp, null));
 
     return ApiResponse.success(hostInfo);
+  }
+
+  private HashMap<String, HashMap<String, String>> getFilterOverrides(
+    Customer customer,
+    String nodePrefix,
+    MetricQueryParams mqParams) {
+
+    HashMap<String, HashMap<String, String>> filterOverrides = new HashMap<>();
+    // For a disk usage metric query, the mount point has to be modified to match the actual
+    // mount point for an onprem universe.
+    if (mqParams.metrics.contains("disk_usage")) {
+      List<Universe> universes =  customer.getUniverses().stream()
+        .filter(u -> u.getUniverseDetails().nodePrefix.equals(nodePrefix))
+        .collect(Collectors.toList());
+      if (universes.get(0).getUniverseDetails().getPrimaryCluster().userIntent.providerType ==
+          CloudType.onprem) {
+        final String mountRoots = universes.get(0).getNodes().stream().
+                                  filter(n -> n.cloudInfo != null &&
+                                         n.cloudInfo.mount_roots != null &&
+                                         !n.cloudInfo.mount_roots.isEmpty()).
+                                  map(n -> n.cloudInfo.mount_roots).
+                                  findFirst().
+                                  orElse("");
+        // TODO: technically, this code is based on the primary cluster being onprem
+        // and will return inaccurate results if the universe has a read replica that is
+        // not onprem.
+        if (!mountRoots.isEmpty()) {
+          HashMap<String, String> mountFilters = new HashMap<>();
+          mountFilters.put("mountpoint", mountRoots.replace(',', '|'));
+          // convert "/storage1,/bar" to the filter "/storage1|/bar"
+          filterOverrides.put("disk_usage", mountFilters);
+        } else {
+          LOG.debug("No mount points found in onprem universe {}", nodePrefix);
+        }
+      }
+    }
+    return filterOverrides;
   }
 }
