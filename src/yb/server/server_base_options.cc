08d5ccbbc9213198099b926764ed2db121fe6af4
jhe <jhe@yugabyte.com>
2020-10-21 15:48:07 +0000

    [docdb] YB looks for node.0.0.0.0.crt when TLS is enabled and rpc_bind_addresses is 0.0.0.0 (#5641)
    Summary:
    Adding new gflag `cert_node_filename`. When set, we will check for certificate and keys that
    have this flag in their names (`node.{cert_node_filename}.{key|crt}`). If not set, then
    default to `--server_broadcast_addresses` and if that is not set, fallback to `--rpc_bind_addresses` (for
    yb-master) or `--cql_proxy_bind_address` (for yb-tserver).
    
    Test Plan:
    Setup certs dir with dummy ca.crt file.
    ```
    mkdir -p /tmp/certs/yugabyte
    cd /tmp/certs/yugabyte
    openssl genrsa 2048 > ca.key
    chmod 400 ca.key
    openssl req -new -x509 -nodes -sha256 -days 365 -key ca.key -out ca.crt
    ```
    
    Now run
    ```
    rm -rf /tmp/test
    yb-master --fs_data_dirs=/tmp/test --rpc_bind_addresses=0.0.0.0:7100 \
    --master_addresses=127.0.0.1:7100 --certs_dir=/tmp/certs/yugabyte --use_node_to_node_encryption=true \
    --allow_insecure_connections=false --use_client_to_server_encryption=true \
    --certs_for_client_dir=/tmp/certs/yugabyte
    ```
    This should fail saying that `/tmp/certs/yugabyte/node.0.0.0.0.key` is not found.
    
    Run
    ```
    yb-master --fs_data_dirs=/tmp/test --rpc_bind_addresses=0.0.0.0:7100 \
    --master_addresses=127.0.0.1:7100 --certs_dir=/tmp/certs/yugabyte --use_node_to_node_encryption=true \
    --allow_insecure_connections=false --use_client_to_server_encryption=true \
    --certs_for_client_dir=/tmp/certs/yugabyte --server_broadcast_addresses=127.0.0.2:7100
    ```
    This should fail saying that `/tmp/certs/yugabyte/node.127.0.0.2.key` is not found.
    
    Finally run
    ```
    yb-master --fs_data_dirs=/tmp/test --rpc_bind_addresses=0.0.0.0:7100 \
    --master_addresses=127.0.0.1:7100 --certs_dir=/tmp/certs/yugabyte --use_node_to_node_encryption=true \
    --allow_insecure_connections=false --use_client_to_server_encryption=true \
    --certs_for_client_dir=/tmp/certs/yugabyte --server_broadcast_addresses=127.0.0.2:7100 \
    --cert_node_filename=127.0.0.3
    ```
    This should fail saying that `/tmp/certs/yugabyte/node.127.0.0.3.key` is not found.
    
    Similarly, running the above for `yb-tserver` and using `cql_proxy_bind_address` instead of
    `rpc_bind_addresses` should yield the same results.
    
    Reviewers: bogdan, rahuldesirazu, sanketh
    
    Reviewed By: sanketh
    
    Subscribers: zyu, ybase
    
    Differential Revision: https://phabricator.dev.yugabyte.com/D9603

--- src/yb/server/server_base_options.cc
+++ src/yb/server/server_base_options.cc
@@ -93,7 +93,8 @@ ServerBaseOptions::ServerBaseOptions(int default_port)
       dump_info_path(FLAGS_server_dump_info_path),
       dump_info_format(FLAGS_server_dump_info_format),
       metrics_log_interval_ms(FLAGS_metrics_log_interval_ms),
-      placement_uuid(FLAGS_placement_uuid) {
+      placement_uuid(FLAGS_placement_uuid),
+      server_broadcast_addresses(FLAGS_server_broadcast_addresses) {
   rpc_opts.default_port = default_port;
   if (!FLAGS_server_broadcast_addresses.empty()) {
     auto status = HostPort::ParseStrings(FLAGS_server_broadcast_addresses, default_port,
@@ -114,6 +115,7 @@ ServerBaseOptions::ServerBaseOptions(const ServerBaseOptions& options)
       metrics_log_interval_ms(options.metrics_log_interval_ms),
       placement_uuid(options.placement_uuid),
       master_addresses_flag(options.master_addresses_flag),
+      server_broadcast_addresses(options.server_broadcast_addresses),
       broadcast_addresses(options.broadcast_addresses),
       placement_cloud_(options.placement_cloud_),
       placement_region_(options.placement_region_),
