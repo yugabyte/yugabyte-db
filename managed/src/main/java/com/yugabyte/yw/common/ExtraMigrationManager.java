c080849fd873d19a3721787591eb54825619641e
Wesley Wang <2791412+WesleyW@users.noreply.github.com>
2020-10-27 16:55:41 -0700

    #6181: Safeguard extra migration for pre-provisioning
    Summary:
    If there are orphaned access keys without related providers, then `V52__Update_Access_Key_Create_Extra_Migration`
    can fail because of this. Add safeguard to check if provider is null beforehand.
    
    Test Plan: add oprhaned acces key with no provider exising for its provider_uuid
    
    Reviewers: sanketh, arnav, daniel
    
    Reviewed By: daniel
    
    Subscribers: jenkins-bot, yugaware
    
    Differential Revision: https://phabricator.dev.yugabyte.com/D9764

--- managed/src/main/java/com/yugabyte/yw/common/ExtraMigrationManager.java
+++ managed/src/main/java/com/yugabyte/yw/common/ExtraMigrationManager.java
@@ -11,7 +11,6 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
-import com.yugabyte.yw.common.TemplateManager;
 import com.yugabyte.yw.models.AccessKey;
 import com.yugabyte.yw.models.Provider;
 
@@ -32,7 +31,8 @@ public class ExtraMigrationManager extends DevopsBase {
 
   public void V52__Update_Access_Key_Create_Extra_Migration() {
     for (AccessKey accessKey: AccessKey.getAll()) {
-      if (Provider.get(accessKey.getProviderUUID()).code.equals(onprem.name())) {
+      Provider p = Provider.get(accessKey.getProviderUUID());
+      if (p != null && p.code.equals(onprem.name())) {
         AccessKey.KeyInfo keyInfo = accessKey.getKeyInfo();
         templateManager.createProvisionTemplate(
           accessKey, keyInfo.airGapInstall, keyInfo.passwordlessSudoAccess,
