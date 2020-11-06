831330777dae7d7f9ab28298b20aa3a56166035e
Jason Kim <git@jasonk.me>
2020-11-06 15:56:14 -0800

    [YSQL] add CREATE INDEX NONCONCURRENTLY grammar (#6237)
    Summary:
    
    Add a new grammar rule so that `CREATE INDEX NONCONCURRENTLY` manually
    skips backfill for the statement even when index backfill for YSQL is
    enabled.
    
    Close: #6237
    
    Test Plan:
    
    `./yb_build.sh --cxx-test pgwrapper_pg_libpq-test --gtest_filter
    PgLibPqTest.BackfillNonconcurrent`
    
    Reviewers: zyu, mihnea
    
    Reviewed By: mihnea
    
    Subscribers: yql
    
    Differential Revision: https://phabricator.dev.yugabyte.com/D9815

--- src/postgres/src/test/regress/sql/yb_create_index.sql
+++ src/postgres/src/test/regress/sql/yb_create_index.sql
@@ -331,3 +331,23 @@ SELECT * FROM test_index_with_oids ORDER BY v1;
 
 CREATE INDEX index_with_duplicate_table_oid ON test_index_with_oids (v1) with (table_oid = 1111111);
 set yb_enable_create_with_table_oid=0;
+
+-- Test creating index nonconcurrently (i.e. without index backfill, without
+-- online schema migration)
+CREATE TABLE test_index_nonconcurrently (i INT, t TEXT);
+INSERT INTO test_index_nonconcurrently VALUES (generate_series(1, 10), 'a');
+
+CREATE INDEX NONCONCURRENTLY ON test_index_nonconcurrently (i);
+EXPLAIN (COSTS OFF) SELECT i FROM test_index_nonconcurrently WHERE i = 1;
+SELECT * FROM test_index_nonconcurrently WHERE i = 1;
+DROP INDEX test_index_nonconcurrently_i_idx;
+
+CREATE UNIQUE INDEX NONCONCURRENTLY ON test_index_nonconcurrently (i);
+EXPLAIN (COSTS OFF) SELECT i FROM test_index_nonconcurrently WHERE i = 1;
+INSERT INTO test_index_nonconcurrently VALUES (1, 'b');
+DROP INDEX test_index_nonconcurrently_i_idx;
+
+INSERT INTO test_index_nonconcurrently VALUES (1, 'b');
+CREATE UNIQUE INDEX NONCONCURRENTLY ON test_index_nonconcurrently (i);
+
+DROP TABLE test_index_nonconcurrently;
