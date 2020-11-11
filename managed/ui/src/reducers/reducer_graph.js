4d688a5cbda320bbab4b79ea1aaaea0746b59840
Andrew Cai <andrew@yugabyte.com>
2020-11-11 11:20:03 -0800

    [YW][#6254] Add content editable div for allowing the user to enter multiple GFlags at once.
    Summary:
    Add content editable div in GFlags component to allow the user to switch to editor and add
    multiple GFlags at once. This allows for copy-pasting a large number of gflags, which has been a
    pain point for customers. The required format of user input is YAML-esque with a `<key>: <value>`
    where key must be an alphanumeric string with optional underscores. The value must not contain
    whitespace, otherwise it must be wrapped in single or double quotes.
    
    This also fixes linting warnings and a redux error I've been seeing.
    
    Test Plan:
    Go to Create Universe and notice the icon next to the Master GFlags section. Click on the
    icon and copy-paste a bunch of GFlags in this format: `<key>: <value>`
    
    Example of GFlags section in Create Universe
    {F14389}
    
    Clicking on the icon will reveal the editor where flags can be entered
    {F14390}
    
    Clicking on the list icon will convert the flags (if entered correctly) back to field inputs
    {F14391}
    
    A max-height has been set on the editor so it doesn't grow indefinitely
    {F14392}
    
    Example of allowed double quotes and single quotes
    {F14393}
    
    Reviewers: sshevchenko
    
    Reviewed By: sshevchenko
    
    Subscribers: ui
    
    Differential Revision: https://phabricator.dev.yugabyte.com/D9814

--- managed/ui/src/reducers/reducer_graph.js
+++ managed/ui/src/reducers/reducer_graph.js
@@ -27,17 +27,19 @@ export default function (state = INITIAL_STATE, action) {
       return { ...state, graphFilter: null };
     case QUERY_METRICS:
       return { ...state, loading: true };
-    case QUERY_METRICS_SUCCESS:
-      let metricData = state.metrics;
+    case QUERY_METRICS_SUCCESS: {
+      const metricData = state.metrics;
       metricData[action.panelType] = {
         ...metricData[action.panelType],
         ...action.payload.data
       };
       return { ...state, metrics: metricData, loading: false };
-    case QUERY_METRICS_FAILURE:
-      metricData = state.metrics;
+    }
+    case QUERY_METRICS_FAILURE: {
+      const metricData = state.metrics;
       metricData[action.panelType] = {};
       return { ...state, metrics: metricData, error: action.payload.response, loading: false };
+    }
     case RESET_METRICS:
       return { ...state, metrics: {}, loading: false, panelType: null };
     default:
