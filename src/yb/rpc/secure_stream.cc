44c736d1afe96b2ce6aa5f3c803f9af39b8a4d0a
Sergei Politov <spolitov@gmail.com>
2020-11-13 09:27:12 +0300

    [#6266] Replace retry counter with check that data is ready
    Summary:
    When SSL_write returns SSL_ERROR_WANT_WRITE we have retry limit controlled with ssl_write_max_retries flag.
    This diff changes the logic to have unlimited retry while SSL_write produces new data to send.
    
    Test Plan: Jenkins
    
    Reviewers: zyu, bogdan
    
    Reviewed By: bogdan
    
    Subscribers: tramer, ybase
    
    Differential Revision: https://phabricator.dev.yugabyte.com/D9890

--- src/yb/rpc/secure_stream.cc
+++ src/yb/rpc/secure_stream.cc
@@ -35,7 +35,6 @@ using namespace std::literals;
 
 DEFINE_bool(allow_insecure_connections, true, "Whether we should allow insecure connections.");
 DEFINE_bool(dump_certificate_entries, false, "Whether we should dump certificate entries.");
-DEFINE_int32(ssl_write_max_retries, 100, "Max number of retries for SSL write.");
 
 namespace yb {
 namespace rpc {
@@ -355,7 +354,7 @@ class SecureStream : public Stream, public StreamContext {
   static int VerifyCallback(int preverified, X509_STORE_CTX* store_context);
   bool Verify(bool preverified, X509_STORE_CTX* store_context);
   CHECKED_STATUS SendEncrypted(OutboundDataPtr data);
-  CHECKED_STATUS WriteEncrypted(OutboundDataPtr data);
+  Result<bool> WriteEncrypted(OutboundDataPtr data);
   CHECKED_STATUS ReadDecrypted();
   Result<size_t> SslRead(void* buf, int num);
 
@@ -406,27 +405,28 @@ Status SecureStream::SendEncrypted(OutboundDataPtr data) {
   data->Serialize(&queue);
   for (const auto& buf : queue) {
     Slice slice(buf.data(), buf.size());
-    for (int retries_left = FLAGS_ssl_write_max_retries;;) {
+    for (;;) {
       auto len = SSL_write(ssl_.get(), slice.data(), slice.size());
       if (len == slice.size()) {
         break;
       }
       auto error = len <= 0 ? SSL_get_error(ssl_.get(), len) : SSL_ERROR_NONE;
+      VLOG_WITH_PREFIX(4) << "SSL_write was not full: " << slice.size() << ", written: " << len
+                          << ", error: " << error;
       if (error != SSL_ERROR_NONE) {
-        if (error != SSL_ERROR_WANT_WRITE || --retries_left <= 0) {
+        if (error != SSL_ERROR_WANT_WRITE || !VERIFY_RESULT(WriteEncrypted(nullptr))) {
           return STATUS_FORMAT(
               NetworkError, "SSL write failed: $0 ($1)", SSLErrorMessage(error), error);
         }
+      } else {
+        RETURN_NOT_OK(WriteEncrypted(nullptr));
       }
-      VLOG_WITH_PREFIX(4) << "SSL_write was not full: " << slice.size() << ", written: " << len
-                          << ", error: " << error;
-      RETURN_NOT_OK(WriteEncrypted(nullptr));
       if (len > 0) {
         slice.remove_prefix(len);
       }
     }
   }
-  return WriteEncrypted(std::move(data));
+  return ResultToStatus(WriteEncrypted(std::move(data)));
 }
 
 Result<size_t> SecureStream::Send(OutboundDataPtr data) {
@@ -445,18 +445,18 @@ Result<size_t> SecureStream::Send(OutboundDataPtr data) {
   FATAL_INVALID_ENUM_VALUE(SecureState, state_);
 }
 
-Status SecureStream::WriteEncrypted(OutboundDataPtr data) {
+Result<bool> SecureStream::WriteEncrypted(OutboundDataPtr data) {
   auto pending = BIO_ctrl_pending(bio_.get());
   if (pending == 0) {
-    return data ? STATUS(NetworkError, "No pending data during write") : Status::OK();
+    return data ? STATUS(NetworkError, "No pending data during write") : Result<bool>(false);
   }
   RefCntBuffer buf(pending);
   auto len = BIO_read(bio_.get(), buf.data(), buf.size());
   LOG_IF_WITH_PREFIX(DFATAL, len != buf.size())
       << "BIO_read was not full: " << buf.size() << ", read: " << len;
   VLOG_WITH_PREFIX(4) << "Write encrypted: " << len << ", " << yb::ToString(data);
-  return ResultToStatus(lower_stream_->Send(
-      std::make_shared<SecureOutboundData>(buf, std::move(data))));
+  RETURN_NOT_OK(lower_stream_->Send(std::make_shared<SecureOutboundData>(buf, std::move(data))));
+  return true;
 }
 
 Status SecureStream::TryWrite() {
