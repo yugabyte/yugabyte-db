861e92df8972346ca3774662117c4c41ebe4fa89
Andrew Cai <andrew@yugabyte.com>
2020-12-14 23:00:01 -0800

    [#6458][YW] Add public DNS or IP address for live queries dashboard.
    Summary:
    Live queries dashboard contains a link on the node name that should direct the user to the
    node where the query row is coming from. We only send the private IP or DNS to the UI to be
    displayed so this change adds the public DNS (if available) or IP address as part of the query
    payload that is returned to the UI.
    
    Test Plan:
    Go to any universe with queries running and click Queries tab. Try clicking on the node
    name for any query and check whether the link is valid.
    
    Reviewers: sanketh, sshevchenko, daniel
    
    Reviewed By: sshevchenko, daniel
    
    Subscribers: cnarang, yugaware, jenkins-bot
    
    Differential Revision: https://phabricator.dev.yugabyte.com/D10064

--- managed/ui/src/utils/UniverseUtils.js
+++ managed/ui/src/utils/UniverseUtils.js
@@ -1,7 +1,7 @@
 // Copyright (c) YugaByte, Inc.
 
 import { isNonEmptyArray, isNonEmptyObject, isDefinedNotNull } from './ObjectUtils';
-import { PROVIDER_TYPES } from '../config';
+import { PROVIDER_TYPES, IN_DEVELOPMENT_MODE } from '../config';
 
 export function isNodeRemovable(nodeState) {
   return nodeState === 'To Be Added';
@@ -152,3 +152,13 @@ export const readUploadedFile = (inputFile, isRequired) => {
     }
   });
 };
+
+export const getProxyNodeAddress = (universeUUID, customer, nodeIp, nodePort) => {
+  let href = '';
+  if (IN_DEVELOPMENT_MODE || !!customer.INSECURE_apiToken) {
+    href = `http://${nodeIp}:${nodePort}`;
+  } else {
+    href = `/universes/${universeUUID}/proxy/${nodeIp}:${nodePort}/`;
+  }
+  return href;
+}
