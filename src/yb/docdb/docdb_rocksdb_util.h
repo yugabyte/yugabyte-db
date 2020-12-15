1c9165ec612aac77c63428856ca683fc1070ed1c
rsami <rsami@yugabyte.com>
2020-12-15 16:02:53 -0500

    [adhoc] Fix logical issue in ForceRocksDBCompact
    Summary:
    We had an extraneous condition in our while loop that would prevent us from returning a TimedOut status when
    a forced compaction was timed out. This diff fixes that issue.
    
    Test Plan: Let jenkins tests run
    
    Reviewers: timur, zyu
    
    Reviewed By: zyu
    
    Differential Revision: https://phabricator.dev.yugabyte.com/D10129

--- src/yb/docdb/docdb_rocksdb_util.h
+++ src/yb/docdb/docdb_rocksdb_util.h
@@ -102,16 +102,8 @@ std::unique_ptr<IntentAwareIterator> CreateIntentAwareIterator(
     std::shared_ptr<rocksdb::ReadFileFilter> file_filter = nullptr,
     const Slice* iterate_upper_bound = nullptr);
 
-
-// Return true if the DB has a currently pending compaction.
-bool HasPendingCompaction(rocksdb::DB* db);
-// Return true if the DB has a currently running compaction.
-bool HasRunningCompaction(rocksdb::DB* db);
-
 // Request RocksDB compaction and wait until it completes.
-CHECKED_STATUS ForceRocksDBCompact(
-    rocksdb::DB* db,
-    const MonoDelta timeout = MonoDelta::FromMilliseconds(50));
+CHECKED_STATUS ForceRocksDBCompact(rocksdb::DB* db);
 
 // Initialize the RocksDB 'options'.
 // The 'statistics' object provided by the caller will be used by RocksDB to maintain the stats for
