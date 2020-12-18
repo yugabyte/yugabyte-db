76799a325b12aa0aa711f5b3a2bb5f4e88e54bc3
Daniel Isen <daniel@yugabyte.com>
2020-12-18 13:08:50 -0800

    [#6574] Platform: Take platform backup from within deployment environment
    Summary:
    Depends on D10102
    
    Add ability to run yb_platform_backup.sh from within the YW container so that it can be invoked by the Java layer.
    
    More specifically this diff adds:
      # Option to pg_dump from/pg_restore to remote DB (i.e. user-inputted host/port)
      # Option to skip backing up release/releases directories (this is to reduce the size of the backup for platform HA work).
      # Detailed help messaging
      # Better integration of the verbose command line option (i.e. integrate it into pg_dump/pg_restore/tar)
      # Don't run commands prefixed with sudo if the script is run by the root user
      # Mount prometheus data dir within the YW container so it can be backed up from within the container
    
    Test Plan:
      - Run the script from within a replicated-based YW docker container (New behavior: Ensure backup containing expected data happens)
      - Run the script from outside of a replicated-based docker container (Legacy behavior: Ensure no regression in this ability)
      - Run the script from a VM-based platform install (Legacy behavior: Ensure no regression in this ability)
    
    Reviewers: sanketh, sb-yb, wesley
    
    Reviewed By: wesley
    
    Subscribers: steve.varnau, jharveysmith, yugaware, jenkins-bot
    
    Differential Revision: https://phabricator.dev.yugabyte.com/D10111

--- managed/devops/bin/yb_platform_backup.sh
+++ managed/devops/bin/yb_platform_backup.sh
@@ -1,6 +1,6 @@
-#!/bin/bash
+#!/usr/bin/env bash
 #
-# Copyright 2019 YugaByte, Inc. and Contributors
+# Copyright 2020 YugaByte, Inc. and Contributors
 #
 # Licensed under the Polyform Free Trial License 1.0.0 (the "License"); you
 # may not use this file except in compliance with the License. You
@@ -10,30 +10,131 @@
 
 set -euo pipefail
 
-YUGAWARE_DUMP_FNAME="yugaware_dump.sql"
+SCRIPT_NAME=$(basename "$0")
+USER=$(whoami)
+PLATFORM_DUMP_FNAME="platform_dump.sql"
+PLATFORM_DB_NAME="yugaware"
+PROMETHEUS_SNAPSHOT_DIR="prometheus_snapshot"
+# This is the UID for nobody user which is used by the prometheus container as the default user.
+NOBODY_UID=65534
 
 set +e
-DOCKER_CMD=$(command -v docker)
-set -e
-DOCKER_BASED=""
-if [[ ! -z "$DOCKER_CMD" ]]; then
-  DOCKER_BASED="$(docker ps -a | grep yugaware)"
+# Check whether the script is being run from a VM running replicated-based Yugabyte Platform.
+docker ps -a 2> /dev/null | grep yugabyte-yugaware > /dev/null 2>&1
+DOCKER_CHECK="$?"
+
+if [[ $DOCKER_CHECK -eq 0 ]]; then
+  DOCKER_BASED=true
+else
+  DOCKER_BASED=false
 fi
 
-# VM based default.
-PROMETHEUS_DATA_DIR="/var/lib/prometheus"
-# Docker (Replicated) based default.
-if [[ -z $DOCKER_BASED ]]; then
-  PROMETHEUS_DATA_DIR="/opt/yugabyte/prometheusv2"
+
+# Check whether the script is being run from within a Yugabyte Platform docker container.
+grep 'docker' /proc/1/cgroup > /dev/null 2>&1
+CONTAINER_CHECK="$?"
+
+if [[ $CONTAINER_CHECK -eq 0 ]] && [[ "$DOCKER_BASED" = false ]]; then
+  INSIDE_CONTAINER=true
+else
+  INSIDE_CONTAINER=false
 fi
-PROMETHEUS_SNAPSHOT_DIR="prometheus_snapshot"
+set -e
+
+# Assume the script is being run from a systemctl-based Yugabyte Platform installation otherwise.
+if [[ "$DOCKER_BASED" = false ]] && [[ "$INSIDE_CONTAINER" = false ]]; then
+  SERVICE_BASED=true
+else
+  SERVICE_BASED=false
+fi
 
 # Takes docker container and command as arguments. Executes docker cmd if docker-based or not.
 docker_aware_cmd() {
-  if [[ -z "$DOCKER_BASED" ]]; then
-    sh -c "$2"
+  if [[ "$DOCKER_BASED" = false ]]; then
+    $2
   else
-    docker exec -i $1 $2
+    docker exec -i "${1}" $2
+  fi
+}
+
+run_sudo_cmd() {
+  if [[ "${USER}" = "root" ]]; then
+    $1
+  else
+    sudo $1
+  fi
+}
+
+# Query prometheus for it's data directory and set as env var
+set_prometheus_data_dir() {
+  prometheus_host="$1"
+  PROMETHEUS_DATA_DIR=$(curl "http://${prometheus_host}:9090/api/v1/status/flags" |
+    python -c "import sys, json; print(json.load(sys.stdin)['data']['storage.tsdb.path'])")
+  if [[ -z "$PROMETHEUS_DATA_DIR" ]]; then
+    echo "Failed to find prometheus data directory"
+    exit 1
+  fi
+}
+
+# Modify service status if the script is being run against a service-based Yugabyte Platform
+modify_service() {
+  if [[ "$SERVICE_BASED" = true ]]; then
+    set +e
+    service="$1"
+    operation="$2"
+    echo "Performing operation $operation on service $service"
+    run_sudo_cmd "systemctl ${operation} ${service}"
+    set -e
+  fi
+}
+
+# Creates a Yugabyte Platform DB backup.
+create_postgres_backup() {
+  backup_path="$1"
+  db_username="$2"
+  db_host="$3"
+  db_port="$4"
+  verbose="$5"
+  if [[ "${verbose}" = true ]]; then
+    backup_cmd="pg_dump -h ${db_host} -p ${db_port} -U ${db_username} -Fc -v ${PLATFORM_DB_NAME}"
+  else
+    backup_cmd="pg_dump -h ${db_host} -p ${db_port} -U ${db_username} -Fc ${PLATFORM_DB_NAME}"
+  fi
+  # Run pg_dump.
+  echo "Creating Yugabyte Platform DB backup ${backup_path}..."
+  docker_aware_cmd "postgres" "${backup_cmd}" > "${backup_path}"
+  echo "Done"
+}
+
+# Restores a Yugabyte Platform DB backup.
+restore_postgres_backup() {
+  backup_path="$1"
+  db_username="$2"
+  db_host="$3"
+  db_port="$4"
+  verbose="$5"
+  if [[ "${verbose}" = true ]]; then
+    restore_cmd="pg_restore -h ${db_host} -p ${db_port} -U ${db_username} -c -v -d \
+    ${PLATFORM_DB_NAME}"
+  else
+    restore_cmd="pg_restore -h ${db_host} -p ${db_port} -U ${db_username} -c -d ${PLATFORM_DB_NAME}"
+  fi
+  # Run pg_restore.
+  echo "Restoring Yugabyte Platform DB backup ${backup_path}..."
+  docker_aware_cmd "postgres" "${restore_cmd}" < "${backup_path}"
+  echo "Done"
+}
+
+# Deletes a Yugabyte Platform DB backup.
+delete_postgres_backup() {
+  backup_path="$1"
+  echo "Deleting Yugabyte Platform DB backup ${backup_path}..."
+  if [[ -f "${backup_path}" ]]; then
+    cleanup "${backup_path}"
+    echo "Done"
+  else
+    echo "${backup_path} does not exist. Cannot delete"
+    exit 1
   fi
 }
 
@@ -42,108 +143,141 @@ create_backup() {
   output_path="$1"
   data_dir="$2"
   exclude_prometheus="$3"
-  exclude_prometheus_flag=" "
+  exclude_releases="$4"
+  db_username="$5"
+  db_host="$6"
+  db_port="$7"
+  verbose="$8"
+  prometheus_host="$9"
+  exclude_prometheus_flag=""
+  exclude_releases_flag=""
   if [[ "$exclude_prometheus" = true ]]; then
-    exclude_prometheus_flag=" --exclude prometheus* "
+    exclude_prometheus_flag="--exclude prometheus*"
   fi
+
+  if [[ "$exclude_releases" = true ]]; then
+    exclude_releases_flag="--exclude release*"
+  fi
+
+  exclude_dirs="--exclude postgresql --exclude devops --exclude yugaware/lib \
+  --exclude yugaware/logs"
 
   modify_service yb-platform stop
 
-  tarname="${output_path}/backup_${now}.tgz"
-  trap "cleanup ${data_dir}/${YUGAWARE_DUMP_FNAME}" EXIT
-  echo "Creating snapshot of platform data"
-  docker_aware_cmd "postgres" "pg_dump -U postgres -Fc yugaware" > \
-                              "${data_dir}/${YUGAWARE_DUMP_FNAME}"
+  tar_name="${output_path}/backup_${now}.tgz"
+  db_backup_path="${data_dir}/${PLATFORM_DUMP_FNAME}"
+  trap 'delete_postgres_backup ${db_backup_path}' RETURN
+  create_postgres_backup "${db_backup_path}" "${db_username}" "${db_host}" "${db_port}" "${verbose}"
 
   # Backup prometheus data.
   if [[ "$exclude_prometheus" = false ]]; then
-    echo "Creating prometheus snapshot"
-    set_prometheus_data_dir
-    snapshot_dir=$(curl -X POST http://localhost:9090/api/v1/admin/tsdb/snapshot |
+    echo "Creating prometheus snapshot..."
+    set_prometheus_data_dir "${prometheus_host}"
+    snapshot_dir=$(curl -X POST "http://${prometheus_host}:9090/api/v1/admin/tsdb/snapshot" |
       python -c "import sys, json; print(json.load(sys.stdin)['data']['name'])")
     mkdir -p "$data_dir/$PROMETHEUS_SNAPSHOT_DIR"
-    sudo cp -aR "$PROMETHEUS_DATA_DIR/snapshots/$snapshot_dir" "$data_dir/$PROMETHEUS_SNAPSHOT_DIR"
-    sudo rm -rf "$PROMETHEUS_DATA_DIR/snapshots/$snapshot_dir"
+    run_sudo_cmd "cp -aR ${PROMETHEUS_DATA_DIR}/snapshots/${snapshot_dir} \
+    ${data_dir}/${PROMETHEUS_SNAPSHOT_DIR}"
+    run_sudo_cmd "rm -rf ${PROMETHEUS_DATA_DIR}/snapshots/${snapshot_dir}"
   fi
-  echo "Creating platform backup package"
-  tar $exclude_prometheus_flag --exclude "postgresql" -czf $tarname -C $data_dir .
-  echo "Finished creating backup $tarname"
+  echo "Creating platform backup package..."
+  if [[ "${verbose}" = true ]]; then
+    tar ${exclude_prometheus_flag} ${exclude_releases_flag} ${exclude_dirs} -czvf "${tar_name}" \
+    -C "${data_dir}" .
+  else
+    tar ${exclude_prometheus_flag} ${exclude_releases_flag} ${exclude_dirs} -czf "${tar_name}" \
+    -C "${data_dir}" .
+  fi
 
+  echo "Finished creating backup ${tar_name}"
   modify_service yb-platform restart
-  echo "Done!"
 }
 
 restore_backup() {
   input_path="$1"
   destination="$2"
-  is_prometheus=false
-
+  db_host="$3"
+  db_port="$4"
+  db_username="$5"
+  verbose="$6"
+  prometheus_host="$7"
   prometheus_dir_regex="^${PROMETHEUS_SNAPSHOT_DIR}/$"
-  if tar -tf $input_path | grep $prometheus_dir_regex; then
-    is_prometheus=true
-    set_prometheus_data_dir
-  fi
 
   modify_service yb-platform stop
 
-  if [[ "$is_prometheus" = true ]]; then
-    modify_service prometheus stop
+  db_backup_path="${destination}/${PLATFORM_DUMP_FNAME}"
+  trap 'delete_postgres_backup ${db_backup_path}' RETURN
+  if [[ "${verbose}" = true ]]; then
+    tar -xzvf "${input_path}" --directory "${destination}"
+  else
+    tar -xzf "${input_path}" --directory "${destination}"
   fi
 
-  yugaware_dump="${destination}/${YUGAWARE_DUMP_FNAME}"
-  trap "cleanup $yugaware_dump" EXIT
-  tar -xzf $input_path --directory $destination
-  echo "Restoring platform data to database"
-  docker_aware_cmd "postgres" "pg_restore -U postgres -d yugaware -c" < "${yugaware_dump}"
+  restore_postgres_backup "${db_backup_path}" "${db_username}" "${db_host}" "${db_port}" \
+  "${verbose}"
   # Restore prometheus data.
-  if [[ "$is_prometheus" = true ]]; then
-    if [[ -z "$PROMETHEUS_DATA_DIR" ]]; then
-      echo "Failed to find prometheus data directory."
-      exit 1
-    fi
-    sudo rm -rf "${PROMETHEUS_DATA_DIR}/*"
-    sudo mv "$destination/$PROMETHEUS_SNAPSHOT_DIR/*" $PROMETHEUS_DATA_DIR
-    if [[ -z "$DOCKER_BASED" ]]; then
-      sudo chown -R nobody:nogroup "$PROMETHEUS_DATA_DIR"
+  if tar -tf "${input_path}" | grep $prometheus_dir_regex; then
+    echo "Restoring prometheus snapshot..."
+    set_prometheus_data_dir "${prometheus_host}"
+    modify_service prometheus stop
+    trap 'modify_service prometheus restart' RETURN
+    run_sudo_cmd "rm -rf ${PROMETHEUS_DATA_DIR}/*"
+    run_sudo_cmd "mv ${destination}/${PROMETHEUS_SNAPSHOT_DIR}/* ${PROMETHEUS_DATA_DIR}"
+    if [[ "$SERVICE_BASED" = true ]]; then
+      run_sudo_cmd "chown -R prometheus:prometheus ${PROMETHEUS_DATA_DIR}"
     else
-      sudo chown -R prometheus:prometheus "$PROMETHEUS_DATA_DIR"
+      run_sudo_cmd "chown -R ${NOBODY_UID}:${NOBODY_UID} ${PROMETHEUS_DATA_DIR}"
     fi
   fi
   # Create following directory if it wasn't created yet so restore will succeed.
-  mkdir -p "$destination/release"
+  mkdir -p "${destination}/release"
 
   modify_service yb-platform restart
-
-  if [[ "$is_prometheus" = true ]]; then
-    modify_service yb-platform restart
-  fi
 
   echo "Finished restoring backup"
 }
 
-set_prometheus_data_dir() {
-  PROMETHEUS_DATA_DIR=$(curl http://localhost:9090/api/v1/status/flags |
-    python -c "import sys, json; print(json.load(sys.stdin)['data']['storage.tsdb.path'])")
-}
-
-modify_service() {
-  if [[ -z "$DOCKER_BASED" ]]; then
-    set +e
-    service="$1"
-    operation="$2"
-    echo "Performing operation $operation on service $service"
-    sudo systemctl "$operation" "$service"
-  fi
-}
-
 print_backup_usage() {
-  echo "$0 backup --output <output_path> [--data_dir <data_dir>] [--exclude_prometheus]"
-  echo "Backup YW to a specified output location"
+  echo "Create: ${SCRIPT_NAME} create --output <output_path> [options]"
+  echo "<output_path> the directory that the platform backup tar.gz is written to"
+  echo "options:"
+  echo "  -m, --exclude_prometheus       exclude prometheus metric data from backup (default: false)"
+  echo "  -r, --exclude_releases         exclude Yugabyte releases from backup (default: false)"
+  echo "  -d, --data_dir=DIRECTORY       data directory (default: /opt/yugabyte)"
+  echo "  -v, --verbose                  verbose output of script (default: false)"
+  echo "  -u, --db_username=USERNAME     postgres username (default: postgres)"
+  echo "  -h, --db_host=HOST             postgres host (default: localhost)"
+  echo "  -P, --db_port=PORT             postgres port (default: 5432)"
+  echo "  -n, --prometheus_host=HOST     prometheus host (default: localhost)"
+  echo "  -?, --help                     show create help, then exit"
+  echo
 }
 
 print_restore_usage() {
-  echo "$0 restore --input <input_path> [--destination <desination>]"
-  echo "Restore YW from a specified input location"
+  echo "Restore: ${SCRIPT_NAME} restore --input <input_path> [options]"
+  echo "<input_path> the path to the platform backup tar.gz"
+  echo "options:"
+  echo "  -d, --destination=DIRECTORY    where to un-tar the backup (default: /opt/yugabyte)"
+  echo "  -v, --verbose                  verbose output of script (default: false)"
+  echo "  -u, --db_username=USERNAME     postgres username (default: postgres)"
+  echo "  -h, --db_host=HOST             postgres host (default: localhost)"
+  echo "  -P, --db_port=PORT             postgres port (default: 5432)"
+  echo "  -n, --prometheus_host=HOST     prometheus host (default: localhost)"
+  echo "  -?, --help                     show restore help, then exit"
+  echo
+}
+
+print_help() {
+  echo "Create or restore a Yugabyte Platform backup"
+  echo
+  echo "Usage: ${SCRIPT_NAME} <command>"
+  echo "command:"
+  echo "  create                         create a Yugabyte Platform backup"
+  echo "  restore                        restore a Yugabyte Platform backup"
+  echo "  -?, --help                     show this help, then exit"
+  echo
+  print_backup_usage
+  print_restore_usage
 }
 
 cleanup () {
@@ -151,86 +285,163 @@ cleanup () {
 }
 
 if [[ $# -eq 0 ]]; then
-  echo "ERROR: Please use one of the following:"
-  echo ""
-  print_backup_usage
-  echo ""
-  print_restore_usage
+  print_help
   exit 1
 fi
 
 command=$1
 shift
 
+# Default global options.
+db_username=postgres
+db_host=localhost
+db_port=5432
+prometheus_host=localhost
+verbose=false
+
 case $command in
-  backup)
+  -?|--help)
+    print_help
+    exit 0
+    ;;
+  create)
+    # Default create options.
     exclude_prometheus=false
+    exclude_releases=false
     data_dir=/opt/yugabyte
+    output_path=""
+
+    if [[ $# -eq 0 ]]; then
+      print_backup_usage
+      exit 1
+    fi
+
     while (( "$#" )); do
       case "$1" in
-        --output)
+        -o|--output)
           output_path=$2
           shift 2
           ;;
-        --exclude_prometheus)
+        -m|--exclude_prometheus)
           exclude_prometheus=true
           shift
           ;;
-        --data_dir)
+        -r|exclude_releases)
+          exclude_releases=true
+          shift
+          ;;
+        -d|--data_dir)
           data_dir=$2
           shift 2
           ;;
-        --verbose)
+        -v|--verbose)
+          verbose=true
           set -x
           shift
           ;;
+        -u|--db_username)
+          db_username=$2
+          shift 2
+          ;;
+        -h|--db_host)
+          db_host=$2
+          shift 2
+          ;;
+        -P|--db_port)
+          db_port=$2
+          shift 2
+          ;;
+        -n|--prometheus_host)
+          prometheus_host=$2
+          shift 2
+          ;;
+        -?|--help)
+          print_backup_usage
+          exit 0
+          ;;
         *)
-          echo "$1"
-          echo "ERROR: Backup Usage"
+          echo "${SCRIPT_NAME}: Unrecognized argument ${1}"
+          echo
           print_backup_usage
           exit 1
       esac
     done
 
     if [[ -z "$output_path" ]]; then
-      echo "ERROR: Backup Usage"
+      echo "${SCRIPT_NAME}: output_path is required"
+      echo
       print_backup_usage
       exit 1
     fi
-    create_backup "$output_path" "$data_dir" "$exclude_prometheus"
+    create_backup "$output_path" "$data_dir" "$exclude_prometheus" "$exclude_releases" \
+    "$db_username" "$db_host" "$db_port" "$verbose" "$prometheus_host"
     exit 0
     ;;
   restore)
+    # Default restore options.
     destination=/opt/yugabyte
+    input_path=""
+
+    if [[ $# -eq 0 ]]; then
+      print_restore_usage
+      exit 1
+    fi
+
     while (( "$#" )); do
       case "$1" in
-        --input)
+        -i|--input)
           input_path=$2
           shift 2
           ;;
-        --destination)
+        -d|--destination)
           destination=$2
           shift 2
           ;;
-        --verbose)
+        -v|--verbose)
+          verbose=true
           set -x
           shift
           ;;
+        -u|--db_username)
+          db_username=$2
+          shift 2
+          ;;
+        -h|--db_host)
+          db_host=$2
+          shift 2
+          ;;
+        -P|--db_port)
+          db_port=$2
+          shift 2
+          ;;
+        -n|--prometheus_host)
+          prometheus_host=$2
+          shift 2
+          ;;
+        -?|--help)
+          print_restore_usage
+          exit 0
+          ;;
         *)
-          echo "ERROR: Restore usage"
+          echo "${SCRIPT_NAME}: Unrecognized option ${1}"
+          echo
           print_restore_usage
           exit 1
       esac
     done
     if [[ -z "$input_path" ]]; then
-      echo "ERROR: Restore usage"
+      echo "${SCRIPT_NAME}: input_path is required"
+      echo
       print_restore_usage
       exit 1
     fi
-    restore_backup "$input_path" "$destination"
+    restore_backup "$input_path" "$destination" "$db_host" "$db_port" "$db_username" "$verbose" \
+    "$prometheus_host"
     exit 0
     ;;
   *)
-    echo "ERROR: Command must be either 'backup' or 'restore'"
+    echo "${SCRIPT_NAME}: Unrecognized command ${command}"
+    echo
+    print_help
     exit 1
 esac
