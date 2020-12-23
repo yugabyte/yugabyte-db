fee0047e55f0aa73f9805a7b4e191a75c41d1368
SB YB <sbapat@yugabyte.com>
2020-12-23 15:51:13 -0800

    [#6341] Platform: Refactor to make ConfigType enum object oriented.
    Summary: Refactor only.
    
    Test Plan: N/A
    
    Reviewers: arnav, spotachev
    
    Reviewed By: spotachev
    
    Subscribers: jenkins-bot, yugaware
    
    Differential Revision: https://phabricator.dev.yugabyte.com/D10218

--- managed/src/main/java/com/yugabyte/yw/commissioner/Common.java
+++ managed/src/main/java/com/yugabyte/yw/commissioner/Common.java
@@ -2,23 +2,37 @@
 
 package com.yugabyte.yw.commissioner;
 
+import com.yugabyte.yw.common.ConfigHelper;
+
+import java.util.Optional;
+
 public class Common {
   // The various cloud types supported.
   public enum CloudType {
     unknown("unknown"),
-    aws("aws"),
-    gcp("gcp"),
-    azu("azu"),
-    docker("docker"),
+    aws("aws", ConfigHelper.ConfigType.AWSRegionMetadata),
+    gcp("gcp", ConfigHelper.ConfigType.GCPRegionMetadata),
+    azu("azu", ConfigHelper.ConfigType.AZURegionMetadata),
+    docker("docker", ConfigHelper.ConfigType.DockerRegionMetadata),
     onprem("onprem"),
     kubernetes("kubernetes"),
     local("cloud-1"),
     other("other");
 
     private final String value;
+    private final Optional<ConfigHelper.ConfigType> regionMetadataConfigType;
 
-    private CloudType(String value) {
+    CloudType(String value, ConfigHelper.ConfigType regionMetadataConfigType) {
       this.value = value;
+      this.regionMetadataConfigType = Optional.ofNullable(regionMetadataConfigType);
+    }
+
+    CloudType(String value) {
+      this(value, null);
+    }
+
+    public Optional<ConfigHelper.ConfigType> getRegionMetadataConfigType() {
+      return this.regionMetadataConfigType;
     }
 
     public String toString() {
