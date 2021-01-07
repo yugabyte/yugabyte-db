f36b4c86c024820ae188a5ad516c7141481a396b
Sergey Shevchenko <sshevchenko@yugabyte.com>
2021-01-06 16:50:21 -0800

    [Backport 2.4][#6717, #6726] Platform: UI for rotating universe certs and rolling restart action
    Summary:
    Original diff: D10240
    Note: depends on D10276
    
    - add sliding submenu "Edit Security" to the universe actions dropdown
    - add "Initiate Rolling Restart" item to the universe actions dropdown
    - add "Edit TLS Configuration" modal
    - add "Initiate Rolling Restart" modal
    - refactor and cleanup RollingUpgradeForm component
    - extract compound components from UniverseDetail.js
    {F14739}
    {F14740}
    {F14738}
    {F14737}
    
    Test Plan:
    Run UI, open any universe, click on "Actions" button and see "Edit Security" and "Initiate Rolling Restart" menu items
    The "Encryption in-Flight TLS" submenu item should be clickable for onprem universe with a custom cert only
    
    Reviewers: andrew
    
    Reviewed By: andrew
    
    Subscribers: jenkins-bot, ui, yugaware
    
    Differential Revision: https://phabricator.dev.yugabyte.com/D10279

---
+++ managed/ui/src/components/universes/UniverseDetail/compounds/YBMenuItem.jsx
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+import { LinkContainer } from 'react-router-bootstrap';
+import { MenuItem } from 'react-bootstrap';
+import { isEnabled, isHidden } from '../../../../utils/LayoutUtils';
+
+export class YBMenuItem extends Component {
+  render() {
+    const { availability, to, id, className, onClick } = this.props;
+    if (isHidden(availability) && availability !== undefined) return null;
+    if (isEnabled(availability)) {
+      if (to) {
+        return (
+          <LinkContainer to={to} id={id}>
+            <MenuItem className={className} onClick={onClick}>
+              {this.props.children}
+            </MenuItem>
+          </LinkContainer>
+        );
+      } else {
+        return (
+          <MenuItem className={className} onClick={onClick}>
+            {this.props.children}
+          </MenuItem>
+        );
+      }
+    }
+    return (
+      <li className={availability}>
+        <div className={className}>{this.props.children}</div>
+      </li>
+    );
+  }
+}
