b4937046f30fcf7e774c7ffeb2b36a45786645be
Sergey Shevchenko <sshevchenko@yugabyte.com>
2021-01-06 16:50:21 -0800

    [#6717, #6726] Platform: UI for rotating universe certs and rolling restart action
    Summary:
    Note: depends on D9970
    
    - add sliding submenu "Edit Security" to the universe actions dropdown
    - add "Initiate Rolling Restart" item to the universe actions dropdown
    - add "Edit TLS Configuration" modal
    - add "Initiate Rolling Restart" modal
    - refactor and cleanup RollingUpgradeForm component
    - extract compound components from UniverseDetail.js
    {F14739}
    {F14740}
    {F14738}
    {F14737}
    
    Test Plan:
    Run UI, open any universe, click on "Actions" button and see "Edit Security" and "Initiate Rolling Restart" menu items
    The "Encryption in-Flight TLS" submenu item should be clickable for onprem universe with a custom cert only
    
    Reviewers: andrew
    
    Reviewed By: andrew
    
    Subscribers: yugaware, ui
    
    Differential Revision: https://phabricator.dev.yugabyte.com/D10240

--- managed/ui/src/components/common/forms/RollingUpgradeForm/RollingUpgradeFormContainer.js
+++ managed/ui/src/components/common/forms/RollingUpgradeForm/RollingUpgradeFormContainer.js
@@ -8,7 +8,6 @@ import {
   fetchCustomerTasksSuccess,
   fetchCustomerTasksFailure
 } from '../../../../actions/tasks';
-import { closeDialog } from '../../../../actions/modal';
 import {
   rollingUpgrade,
   rollingUpgradeResponse,
@@ -23,27 +22,24 @@ import {
 import { isDefinedNotNull, isNonEmptyObject } from '../../../../utils/ObjectUtils';
 import { getPrimaryCluster } from '../../../../utils/UniverseUtils';
 import { TASK_LONG_TIMEOUT } from '../../../tasks/constants';
+import { getPromiseState } from '../../../../utils/PromiseUtils';
 
 const FORM_NAME = 'RollingUpgradeForm';
 
 const mapDispatchToProps = (dispatch) => {
   return {
     /**
-     * Dispatch Rolling Upgrade/ Gflag restart to endpoint and handle response.
+     * Dispatch Rolling Upgrade/ Gflag / TLS restart to endpoint and handle response.
      * @param values form data payload
      * @param universeUUID UUID of the current Universe
-     * @param reset function that sets the value of the form to pristine state
      */
-    submitRollingUpgradeForm: (values, universeUUID, reset) => {
-      dispatch(rollingUpgrade(values, universeUUID)).then((response) => {
+    submitRollingUpgradeForm: (values, universeUUID) => {
+      dispatch(resetRollingUpgrade()); // reset previous rollingUpgrade() error state, if any
+      return dispatch(rollingUpgrade(values, universeUUID)).then((response) => {
         if (!response.error) {
-          dispatch(closeDialog());
           dispatch(closeUniverseDialog());
         }
-        dispatch(rollingUpgradeResponse(response.payload));
-        // Reset the Rolling upgrade form fields to pristine state,
-        // component may be called multiple times within the context of Universe Detail.
-        reset();
+        return dispatch(rollingUpgradeResponse(response.payload));
       });
     },
     fetchCustomerTasks: () => {
@@ -80,41 +76,58 @@ function mapStateToProps(state, ownProps) {
   const {
     universe: { currentUniverse }
   } = state;
-  const initalGFlagValues = {};
+
+  const initialValues = {};
   if (isNonEmptyObject(currentUniverse) && isNonEmptyObject(currentUniverse.data.universeDetails)) {
+    initialValues.tlsCertificate = currentUniverse.data.universeDetails.rootCA;
+
     const primaryCluster = getPrimaryCluster(currentUniverse.data.universeDetails.clusters);
     if (isDefinedNotNull(primaryCluster)) {
       const masterGFlags = primaryCluster.userIntent.masterGFlags;
       const tserverGFlags = primaryCluster.userIntent.tserverGFlags;
       if (isNonEmptyObject(masterGFlags)) {
-        initalGFlagValues.masterGFlags = Object.keys(masterGFlags).map(function (gFlagKey) {
+        initialValues.masterGFlags = Object.keys(masterGFlags).map((gFlagKey) => {
           return { name: gFlagKey, value: masterGFlags[gFlagKey] };
         });
       }
       if (isNonEmptyObject(tserverGFlags)) {
-        initalGFlagValues.tserverGFlags = Object.keys(tserverGFlags).map(function (gFlagKey) {
+        initialValues.tserverGFlags = Object.keys(tserverGFlags).map((gFlagKey) => {
           return { name: gFlagKey, value: tserverGFlags[gFlagKey] };
         });
       }
     }
   }
-  initalGFlagValues.ybSoftwareVersion = state.customer.softwareVersions[0];
-  initalGFlagValues.timeDelay = TASK_LONG_TIMEOUT / 1000;
-  initalGFlagValues.upgradeOption = 'Rolling';
-  initalGFlagValues.rollingUpgrade = true;
+  initialValues.ybSoftwareVersion = state.customer.softwareVersions[0];
+  initialValues.timeDelay = TASK_LONG_TIMEOUT / 1000;
+  initialValues.upgradeOption = 'Rolling';
+  initialValues.rollingUpgrade = true;
+
+  let certificates = [];
+  const allCertificates = state.customer.userCertificates;
+  if (getPromiseState(allCertificates).isSuccess()) {
+    const rootCert = allCertificates.data.find((item) => item.uuid === initialValues.tlsCertificate);
+    // show custom certs with same root cert only
+    certificates = allCertificates.data.filter(
+      (item) => item.certType === 'CustomCertHostPath' && item.checksum === rootCert?.checksum
+    );
+  }
+
   const selector = formValueSelector(FORM_NAME);
-  const upgradeOption = selector(state, 'upgradeOption');
+  const formValues = selector(state, 'upgradeOption', 'ybSoftwareVersion', 'tlsCertificate');
+
   return {
     modal: state.modal,
     universe: state.universe,
     softwareVersions: state.customer.softwareVersions,
-    initialValues: initalGFlagValues,
-    upgradeOption
+    initialValues,
+    certificates,
+    formValues
   };
 }
 
 const rollingUpgradeForm = reduxForm({
-  form: FORM_NAME
+  form: FORM_NAME,
+  enableReinitialize: true // to reinitialize form every time the initialValues prop changes
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(rollingUpgradeForm(RollingUpgradeForm));
