e6e1b520dbc2305deb417edff2dccadb039df818
Jason Kim <git@jasonk.me>
2021-01-06 17:15:08 -0800

    [#5324] ysql: improve tserver-postgres auth for backfill
    Summary:
    
    Index backfill will not work when the `yugabyte` user is dropped or
    authentication is needed.  This is because backfill makes a tserver to
    postgres libpq connection to run the backfill command as the `yugabyte`
    user.
    
    To solve this,
    
    1. change the user running backfill to `postgres`, which can't be
       dropped
    1. allow unix domain sockets to be used to connect to postgres
    1. configure the unix domain socket to not clash with other clusters,
       and restrict the socket's permissions to the unix user starting the
       cluster
    1. add a new authentication method "yb-tserver-key" where tserver
       generates a random uint64 key, shares it through tserver-postgres
       shared memory, and uses it as a password for this authentication
       method
    1. add an internal default YSQL HBA config line to allow `postgres`
       access over local unix-domain socket using the "yb-tserver-key"
       authentication method
    1. make backfill connect over the socket
    
    Note that the unix-domain socket is created under `/tmp`.
    
    Also,
    
    - block off these changes using `FLAGS_ysql_disable_index_backfill`
    - restrict the `BACKFILL INDEX` command so that only tserver can run it
    - restrict tserver to only be able to run the `BACKFILL INDEX` command
    - update `PgLibPqTest.BackfillAuth` to have authentication enabled on
      `postgres` and to expect backfill to succeed.
    
    Leave the following work for later:
    
    - clean up temporary socket directories when cluster is down?
    
    Close: #5324
    
    Test Plan:
    
    - manually test backfill with TLS + authentication
    - run
    
      ```
      ./yb_build.sh --cxx-test pgwrapper_pg_libpq-test --gtest_filter \
        PgLibPqTest.BackfillAuth
      ```
    
    Reviewers: dmitry, mbautin, mihnea
    
    Reviewed By: mihnea
    
    Subscribers: bogdan, yql
    
    Differential Revision: https://phabricator.dev.yugabyte.com/D9976

--- src/yb/tablet/tablet.h
+++ src/yb/tablet/tablet.h
@@ -206,7 +206,8 @@ class Tablet : public AbstractTablet, public TransactionIntentApplier {
       const CoarseTimePoint deadline,
       const HybridTime read_time,
       const HostPort& pgsql_proxy_bind_address,
-      const std::string& database_name);
+      const std::string& database_name,
+      const uint64_t postgres_auth_key);
   Result<std::string> BackfillIndexes(const std::vector<IndexInfo>& indexes,
                                       const std::string& backfill_from,
                                       const CoarseTimePoint deadline,
