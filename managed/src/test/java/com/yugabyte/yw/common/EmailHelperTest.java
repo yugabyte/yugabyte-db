07b777ec1392c5e08692a78f4d16cd5052780e1e
Sergey Potachev <spotachev@yugabyte.com>
2021-01-16 19:41:35 +0300

    [#6901]Platform: YW Fails to send alert email
    Summary:
    The problem introduced while implementing #6581.
    Actual fix is very small - a few lines in EmailHelper.java:
      lines 116-118 + 125  and in line 216
    
    Other lines - junit tests improvements + small logic corrections (according to changes in EmailHelper).
    
    Test Plan:
    Test cases from #6581 + one specific scenario:
       1. New customer
       2. SMTP is never configured (see picture from the issue).
       3. There is a universe with failures (in health checks, as example).
    Check that there are no exceptions in YW logs related to emails sending (with alerts).
    
    Reviewers: daniel, sb-yb
    
    Reviewed By: sb-yb
    
    Subscribers: jenkins-bot, yugaware
    
    Differential Revision: https://phabricator.dev.yugabyte.com/D10380

--- managed/src/test/java/com/yugabyte/yw/common/EmailHelperTest.java
+++ managed/src/test/java/com/yugabyte/yw/common/EmailHelperTest.java
@@ -3,6 +3,7 @@ package com.yugabyte.yw.common;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertTrue;
 import static org.mockito.Mockito.when;
 
@@ -10,6 +11,7 @@ import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.util.Collections;
 import java.util.List;
+import java.util.Properties;
 
 import javax.mail.MessagingException;
 import javax.mail.internet.MimeMessage;
@@ -35,6 +37,7 @@ import com.yugabyte.yw.models.CustomerConfig;
 
 import junitparams.JUnitParamsRunner;
 import junitparams.Parameters;
+import junitparams.converters.Nullable;
 import play.libs.Json;
 
 @RunWith(JUnitParamsRunner.class)
@@ -51,6 +54,12 @@ public class EmailHelperTest extends FakeDBApplication {
   private static final String EMAIL_TEST_USER_PWD = "2345-5432";
 
   private static final String YB_DEFAULT_EMAIL = "test@yugabyte.com";
+
+  private static final String EMAIL_SMTP_SERVER = "test-server";
+
+  private static final int EMAIL_SMTP_PORT = 25;
+
+  private static final int EMAIL_SMTP_PORT_SSL = 587;
 
   @Rule
   public MockitoRule rule = MockitoJUnit.rule();
@@ -77,6 +86,12 @@ public class EmailHelperTest extends FakeDBApplication {
     when(mockCustomerConfig.getString("yb.health.ses_email_password"))
         .thenReturn(EMAIL_TEST_USER_PWD);
     when(mockCustomerConfig.getBoolean("yb.health.default_ssl")).thenReturn(false);
+
+    when(mockCustomerConfig.getString("yb.health.default_smtp_server"))
+        .thenReturn(EMAIL_SMTP_SERVER);
+    when(mockCustomerConfig.getInt("yb.health.default_smtp_port")).thenReturn(EMAIL_SMTP_PORT);
+    when(mockCustomerConfig.getInt("yb.health.default_smtp_port_ssl"))
+        .thenReturn(EMAIL_SMTP_PORT_SSL);
   }
 
   @Test
@@ -92,7 +107,7 @@ public class EmailHelperTest extends FakeDBApplication {
     GreenMail mailServer = EmailFixtures.setupMailServer(serverHost, serverPort, smtpData.emailFrom,
         smtpData.smtpUsername, smtpData.smtpPassword);
     try {
-      emailHelper.sendEmail(EMAIL_SUBJECT, EMAIL_TO, smtpData,
+      emailHelper.sendEmail(defaultCustomer, EMAIL_SUBJECT, EMAIL_TO, smtpData,
           Collections.singletonMap("plain/text", EMAIL_TEXT));
 
       MimeMessage[] messages = mailServer.getReceivedMessages();
@@ -137,7 +152,7 @@ public class EmailHelperTest extends FakeDBApplication {
   }
 
   @Test
-  public void testGetSmtpData_NoConfig() {
+  public void testGetSmtpData_NoDbConfig() {
     SmtpData smtpData = emailHelper.getSmtpData(defaultCustomer.uuid);
     assertEquals(YB_DEFAULT_EMAIL, smtpData.emailFrom);
     assertEquals(EMAIL_TEST_USER, smtpData.smtpUsername);
@@ -146,7 +161,7 @@ public class EmailHelperTest extends FakeDBApplication {
   }
 
   @Test
-  public void testGetSmtpData_ConfigExistsAndEmailFromFilled() {
+  public void testGetSmtpData_DbConfigExistsAndEmailFromFilled() {
     SmtpData testSmtpData = EmailFixtures.createSmtpData();
     CustomerConfig.createSmtpConfig(defaultCustomer.uuid, Json.toJson(testSmtpData));
 
@@ -158,7 +173,7 @@ public class EmailHelperTest extends FakeDBApplication {
   }
 
   @Test
-  public void testGetSmtpData_ConfigExistsAndEmailEmpty() {
+  public void testGetSmtpData_DbConfigExistsAndEmailEmpty_AppConfigHasDefaultEmail() {
     SmtpData testSmtpData = EmailFixtures.createSmtpData();
     testSmtpData.emailFrom = "";
     CustomerConfig.createSmtpConfig(defaultCustomer.uuid, Json.toJson(testSmtpData));
@@ -168,5 +183,77 @@ public class EmailHelperTest extends FakeDBApplication {
     assertEquals(testSmtpData.smtpUsername, smtpData.smtpUsername);
     assertEquals(testSmtpData.smtpPassword, smtpData.smtpPassword);
     assertEquals(testSmtpData.useSSL, smtpData.useSSL);
+  }
+
+  @Test
+  public void testGetSmtpData_DbConfigExistsAndEmailEmpty_AppConfigHasNoDefaultEmail() {
+    when(mockCustomerConfig.getString("yb.health.default_email")).thenReturn(null);
+
+    SmtpData testSmtpData = EmailFixtures.createSmtpData();
+    testSmtpData.emailFrom = "";
+    CustomerConfig.createSmtpConfig(defaultCustomer.uuid, Json.toJson(testSmtpData));
+
+    assertNull(emailHelper.getSmtpData(defaultCustomer.uuid));
+  }
+
+  @Test
+  @Parameters({
+    // @formatter:off
+    "localhost, -1, false",
+    "localhost, -1, true",
+    "localhost, 999, false",
+    "localhost, 999, true",
+    ", -1, false",
+    ", -1, true",
+    "null, -1, false",
+    "null, -1, true",
+    // @formatter:on
+  })
+  public void testSmtpDataToProperties(@Nullable String smtpServer, int smtpPort, boolean useSSL)
+      throws MessagingException, IOException {
+    SmtpData smtpData = EmailFixtures.createSmtpData();
+    smtpData.smtpServer = smtpServer;
+    smtpData.smtpPort = smtpPort;
+    smtpData.useSSL = useSSL;
+    String expectedSmtpServer = StringUtils.isEmpty(smtpServer) ? EMAIL_SMTP_SERVER : smtpServer;
+    int expectedSmtpPort = smtpPort == -1 ? (useSSL ? EMAIL_SMTP_PORT_SSL : EMAIL_SMTP_PORT)
+        : smtpPort;
+
+    Properties props = emailHelper.smtpDataToProperties(defaultCustomer, smtpData);
+    assertNotNull(props);
+
+    assertEquals(smtpData.smtpUsername, props.get("mail.smtp.user"));
+    assertEquals("true", props.get("mail.smtp.auth"));
+    assertEquals(String.valueOf(smtpData.useTLS), props.get("mail.smtp.starttls.enable"));
+    assertEquals(expectedSmtpServer, props.get("mail.smtp.host"));
+    assertEquals(String.valueOf(expectedSmtpPort), props.get("mail.smtp.port"));
+    assertEquals(String.valueOf(useSSL), props.get("mail.smtp.ssl.enable"));
+    if (useSSL) {
+      assertEquals(expectedSmtpServer, props.get("mail.smtp.ssl.trust"));
+    }
+  }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void testSmtpDataToProperties_SmtpDataIsNull_ThrowsException() {
+    emailHelper.smtpDataToProperties(defaultCustomer, null);
+  }
+
+  @Test
+  @Parameters({
+    // @formatter:off
+    "to@to.to, false",
+    ", true",
+    "null, true"
+    // @formatter:on
+  })
+  public void testSmtpDataToProperties_EmailFrom(@Nullable String emailFrom, boolean shouldFail) {
+    SmtpData smtpData = EmailFixtures.createSmtpData();
+    smtpData.emailFrom = emailFrom;
+    try {
+      emailHelper.smtpDataToProperties(defaultCustomer, smtpData);
+      assertFalse(shouldFail);
+    } catch (IllegalArgumentException e) {
+      assertTrue(shouldFail);
+    }
   }
 }
