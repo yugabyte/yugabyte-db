4fc5acd612763c357ac0b58304a74b10a339c14e
Jaydeep Ahir <jaydeepkumar.a@hashedin.com>
2021-01-22 17:14:42 +0530

    [#5172] Health check time is always in the Pacific timezone  (#6933)
    * Fix timezone issue
    
    * Changed moment timezone to moment

--- managed/ui/src/components/universes/UniverseOverview/UniverseOverviewNew.js
+++ managed/ui/src/components/universes/UniverseOverview/UniverseOverviewNew.js
@@ -236,7 +236,7 @@ class HealthInfoPanel extends PureComponent {
     let disabledUntilStr = '';
     if (getPromiseState(healthCheck).isSuccess()) {
       const healthCheckData = JSON.parse([...healthCheck.data].reverse()[0]);
-      const lastUpdateDate = moment(healthCheckData.timestamp);
+      const lastUpdateDate = moment.utc(healthCheckData.timestamp).local();
       if (universeInfo.universeConfig && 'disableAlertsUntilSecs' in universeInfo.universeConfig) {
         const disabledUntilSecs = Number(universeInfo.universeConfig.disableAlertsUntilSecs);
         const now = Date.now() / 1000;
