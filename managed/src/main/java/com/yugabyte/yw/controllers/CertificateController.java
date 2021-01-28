59878fde71b7afb9d66c719434098c78767182a7
mahendra bhat <mahenrda.bhat@hashedin.com>
2021-01-28 19:15:47 +0530

    review comment fixes

--- managed/src/main/java/com/yugabyte/yw/controllers/CertificateController.java
+++ managed/src/main/java/com/yugabyte/yw/controllers/CertificateController.java
@@ -12,6 +12,7 @@ import com.yugabyte.yw.forms.ClientCertParams;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.node.ObjectNode;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
