2dc730938c2e1d99fdc3a58e8fbee6b4b4dae99e
Ram Srivatsa Kannan <ramsri@umich.edu>
2021-01-28 10:58:36 -0800

    [#6720] YSQL: pg_hint_plan -- controls execution plan with hinting phrases in comment of special form.
    Summary:
    PostgreSQL uses a cost-based optimizer, that estimates the costs of each possible execution plan for an SQL statement. The execution plan with the lowest cost finally is executed. The planner does its best to select the best execution plan, but it is not perfect. Additionally, the version of YB's planner is sub-optimal. For instance, the cost-based optimizer can operate on inaccurate size estimates of row counts. Row counts however play a crucial role in calculating the cost estimates. To overcome these limitations, we enable pg_hint_plan.
    
    pg_hint_plan makes it possible to tweak execution plans using so-called "hints", which are simple descriptions of an execution plan in the form of a SQL comment.
    
    Detailed documentation of pg_hint_plan in YB is presented in this [[ https://github.com/yugabyte/yugabyte-db/issues/7009 | github ticket ]].
    
    Test Plan:
    The associated test for pg_hint_plan can be executed by running the following command.
    
    ```
    ybd --java-test org.yb.pgsql.TestPgRegressExtension
    ```
    
    Reviewers: mbautin, kannan, mihnea, sudheer
    
    Reviewed By: mihnea, sudheer
    
    Subscribers: jason
    
    Differential Revision: https://phabricator.dev.yugabyte.com/D10286

--- python/yb/build_postgres.py
+++ python/yb/build_postgres.py
@@ -136,6 +136,7 @@ class PostgresBuilder(YbBuildToolBase):
         self.pg_prefix = None
         self.build_type = None
         self.postgres_src_dir = None
+        self.pg_config_root = None
         self.compiler_type = None
         self.env_vars_for_build_stamp = set()
 
@@ -198,6 +199,7 @@ class PostgresBuilder(YbBuildToolBase):
         self.build_stamp_path = os.path.join(self.pg_build_root, 'build_stamp')
         self.pg_prefix = os.path.join(self.build_root, 'postgres')
         self.postgres_src_dir = os.path.join(YB_SRC_ROOT, 'src', 'postgres')
+        self.pg_config_root = os.path.join(self.build_root, 'postgres', 'bin', 'pg_config')
         self.compiler_type = self.args.compiler_type
         self.openssl_include_dir = self.args.openssl_include_dir
         self.openssl_lib_dir = self.args.openssl_lib_dir
@@ -245,7 +247,6 @@ class PostgresBuilder(YbBuildToolBase):
         is_make_step = step == 'make'
 
         self.set_env_var('YB_PG_BUILD_STEP', step)
-        self.set_env_var('YB_BUILD_ROOT', self.build_root)
         self.set_env_var('YB_THIRDPARTY_DIR', self.thirdparty_dir)
         self.set_env_var('YB_SRC_ROOT', YB_SRC_ROOT)
 
@@ -538,7 +539,8 @@ class PostgresBuilder(YbBuildToolBase):
 
         compile_commands_files = []
 
-        work_dirs = [self.pg_build_root, os.path.join(self.pg_build_root, 'contrib')]
+        work_dirs = [self.pg_build_root, os.path.join(self.pg_build_root, 'contrib'),
+                     os.path.join(self.pg_build_root, 'third-party-extensions')]
 
         for work_dir in work_dirs:
             with WorkDirContext(work_dir):
@@ -554,13 +556,18 @@ class PostgresBuilder(YbBuildToolBase):
 
                 run_program(['chmod', 'u+x', make_script_path])
 
+                pg_config = []
+                if 'third-party-extensions' in work_dir:
+                    pg_config = ['PG_CONFIG=' + self.pg_config_root]
                 # Actually run Make.
                 if is_verbose_mode():
                     logging.info("Running make in the %s directory", work_dir)
 
                 make_result = run_program(
-                    make_cmd, stdout_stderr_prefix='make', cwd=work_dir, shell=True, error_ok=True
+                    make_cmd + pg_config, stdout_stderr_prefix='make', cwd=work_dir, shell=True,
+                    error_ok=True
                 )
+
                 if make_result.failure():
                     make_result.print_output_to_stdout()
                     raise RuntimeError("PostgreSQL compilation failed")
@@ -571,8 +578,8 @@ class PostgresBuilder(YbBuildToolBase):
                             "generating the compilation database", work_dir)
                 else:
                     run_program(
-                        'make install', stdout_stderr_prefix='make_install',
-                        cwd=work_dir, shell=True, error_ok=True
+                        'make install' + ' ' + ''.join(pg_config),
+                        stdout_stderr_prefix='make_install', cwd=work_dir, shell=True, error_ok=True
                     ).print_output_and_raise_error_if_failed()
                     logging.info("Successfully ran make in the %s directory", work_dir)
 
