yugabyte/yugabyte-db

commit 3c643b505ca83fcf05622a1b1162cae8696870aa

[Backport 2.4][#6546][Platform]: Failing preflight checks should not lock an already created universe
Summary:
If preflight checks fail while adding or editing a universe, the universe should not be changed at all and instead a warning will be shown on the universes page/dashboard. Going to the task will still show an error.

In the backend, I used `errorString` to keep track of what error the last task ran into. If it succeeds,
then it'll be replaced with null. This variable wasn't used before and was just inherited from a superclass.

When releasing an  onprem instance, the `node_exporter` service will also be stopped now.

Also removed "Retry Task" button on the UI if it the task isn't onprem "Create Universe" button, as
that's the only task that's retryable at the moment.

{F14812}

Test Plan:
Create onprem universe (RF1). Add a node with an IP that doesn't exist. Confirm on the UI that a warning
shows up, but I can still edit the page.

Reviewers: sanketh, arnav, sb-yb, daniel

Reviewed By: daniel

Subscribers: yugaware, jenkins-bot

Differential Revision: https://phabricator.dev.yugabyte.com/D10422

managed/src/main/java/com/yugabyte/yw/commissioner/tasks/AddNodeToUniverse.java
@@ -25,8 +25,8 @@ import com.yugabyte.yw.models.helpers.NodeDetails.NodeState;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
+import java.util.Collections;
 import java.util.HashMap;
-import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.UUID;
@@ -52,6 +52,8 @@ public class AddNodeToUniverse extends UniverseDefinitionTaskBase {
              taskParams().nodeName, taskParams().universeUUID);
     NodeDetails currentNode = null;
     boolean hitException = false;
+    String errorString = null;
+
     try {
       // Create the task list sequence.
       subTaskGroupQueue = new SubTaskGroupQueue(userTaskUUID);
@@ -74,19 +76,13 @@ public class AddNodeToUniverse extends UniverseDefinitionTaskBase {
         throw new RuntimeException(msg);
       }
 
-      // Update Node State to being added.
-      createSetNodeStateTask(currentNode, NodeState.Adding)
-          .setSubTaskGroupType(SubTaskGroupType.StartingNode);
-
       Cluster cluster = taskParams().getClusterByUuid(currentNode.placementUuid);
-      Collection<NodeDetails> node = new HashSet<NodeDetails>(Arrays.asList(currentNode));
+      Collection<NodeDetails> node = Collections.singletonList(currentNode);
 
-      // First spawn an instance for Decommissioned node.
       boolean wasDecommissioned = currentNode.state == NodeState.Decommissioned;
-      if (wasDecommissioned) {
-        if (cluster.userIntent.providerType.equals(CloudType.onprem)) {
-          // For onprem universes, allocate an available node
-          // from the provider's node_instance table.
+      // For onprem universes, allocate an available node
+      // from the provider's node_instance table.
+      if (wasDecommissioned && cluster.userIntent.providerType.equals(CloudType.onprem)) {
           Map<UUID, List<String>> onpremAzToNodes = new HashMap<UUID, List<String>>();
           List<String> nodeNameList = new ArrayList<>();
           nodeNameList.add(currentNode.nodeName);
@@ -95,106 +91,117 @@ public class AddNodeToUniverse extends UniverseDefinitionTaskBase {
 
           Map<String, NodeInstance> nodeMap = NodeInstance.pickNodes(onpremAzToNodes, instanceType);
           currentNode.nodeUuid = nodeMap.get(currentNode.nodeName).nodeUuid;
+      }
+
+      NodeTaskParams nodeParams = new NodeTaskParams();
+      UserIntent userIntent = taskParams().getClusterByUuid(currentNode.placementUuid).userIntent;
+      nodeParams.nodeName = currentNode.nodeName;
+      nodeParams.deviceInfo = userIntent.deviceInfo;
+      nodeParams.azUuid = currentNode.azUuid;
+      nodeParams.universeUUID = taskParams().universeUUID;
+      nodeParams.extraDependencies.installNodeExporter =
+        taskParams().extraDependencies.installNodeExporter;
+
+      String preflightStatus = performPreflightCheck(currentNode, nodeParams);
+      if (preflightStatus != null) {
+        Map<NodeInstance, String> failedNodes = new HashMap<>();
+        failedNodes.put(NodeInstance.getByName(currentNode.nodeName), preflightStatus);
+        createFailedPrecheckTask(failedNodes)
+          .setSubTaskGroupType(SubTaskGroupType.PreflightChecks);
+        errorString = "Preflight checks failed.";
+      } else {
+        // Update Node State to being added.
+        createSetNodeStateTask(currentNode, NodeState.Adding)
+            .setSubTaskGroupType(SubTaskGroupType.StartingNode);
+
+        // First spawn an instance for Decommissioned node.
+        if (wasDecommissioned) {
+            createSetupServerTasks(node)
+                .setSubTaskGroupType(SubTaskGroupType.Provisioning);
+
+            createServerInfoTasks(node)
+                .setSubTaskGroupType(SubTaskGroupType.Provisioning);
         }
 
-        createPrecheckTasks(node)
-            .setSubTaskGroupType(SubTaskGroupType.PreflightChecks);
+        // Re-install software.
+        // TODO: Remove the need for version for existing instance, NodeManger needs changes.
+        createConfigureServerTasks(node, true /* isShell */)
+            .setSubTaskGroupType(SubTaskGroupType.InstallingSoftware);
 
-        createSetupServerTasks(node)
-            .setSubTaskGroupType(SubTaskGroupType.Provisioning);
+        // Set default gflags
+        addDefaultGFlags(cluster.userIntent);
 
-        createServerInfoTasks(node)
-            .setSubTaskGroupType(SubTaskGroupType.Provisioning);
-      }
+        // Bring up any masters, as needed.
+        boolean masterAdded = false;
+        if (areMastersUnderReplicated(currentNode, universe)) {
+          LOG.info(
+            "Bringing up master for under replicated universe {} ({})",
+            universe.universeUUID, universe.name
+          );
+          // Set gflags for master.
+          createGFlagsOverrideTasks(node, ServerType.MASTER);
 
-      // Re-install software.
-      // TODO: Remove the need for version for existing instance, NodeManger needs changes.
-      createConfigureServerTasks(node, true /* isShell */)
-          .setSubTaskGroupType(SubTaskGroupType.InstallingSoftware);
-
-      // Set default gflags
-      addDefaultGFlags(cluster.userIntent);
-
-      // Bring up any masters, as needed.
-      boolean masterAdded = false;
-      if (areMastersUnderReplicated(currentNode, universe)) {
-        LOG.info(
-          "Bringing up master for under replicated universe {} ({})",
-          universe.universeUUID, universe.name
-        );
-        // Set gflags for master.
-        createGFlagsOverrideTasks(node, ServerType.MASTER);
-
-        // Start a shell master process.
-        createStartMasterTasks(node)
-            .setSubTaskGroupType(SubTaskGroupType.StartingNodeProcesses);
-
-        // Mark node as a master in YW DB.
-        // Do this last so that master addresses does not pick up current node.
-        createUpdateNodeProcessTask(taskParams().nodeName, ServerType.MASTER, true)
-            .setSubTaskGroupType(SubTaskGroupType.StartingNodeProcesses);
-
-        // Wait for master to be responsive.
-        createWaitForServersTasks(node, ServerType.MASTER)
-            .setSubTaskGroupType(SubTaskGroupType.ConfigureUniverse);
-
-        // Add it into the master quorum.
-        createChangeConfigTask(currentNode, true, SubTaskGroupType.WaitForDataMigration);
-
-        masterAdded = true;
-      }
-
-      // Set gflags for the tserver.
-      createGFlagsOverrideTasks(node, ServerType.TSERVER);
-
-      // Add the tserver process start task.
-      createTServerTaskForNode(currentNode, "start")
+          // Start a shell master process.
+          createStartMasterTasks(node)
           .setSubTaskGroupType(SubTaskGroupType.StartingNodeProcesses);
 
-      // Mark the node as tserver in the YW DB.
-      createUpdateNodeProcessTask(taskParams().nodeName, ServerType.TSERVER, true)
-          .setSubTaskGroupType(SubTaskGroupType.StartingNodeProcesses);
+          // Mark node as a master in YW DB.
+          // Do this last so that master addresses does not pick up current node.
+          createUpdateNodeProcessTask(taskParams().nodeName, ServerType.MASTER, true)
+              .setSubTaskGroupType(SubTaskGroupType.StartingNodeProcesses);
 
-      // Wait for new tablet servers to be responsive.
-      createWaitForServersTasks(node, ServerType.TSERVER)
-          .setSubTaskGroupType(SubTaskGroupType.ConfigureUniverse);
+          // Wait for master to be responsive.
+          createWaitForServersTasks(node, ServerType.MASTER)
+              .setSubTaskGroupType(SubTaskGroupType.ConfigureUniverse);
 
-      // Update the swamper target file.
-      createSwamperTargetUpdateTask(false /* removeFile */);
+          // Add it into the master quorum.
+          createChangeConfigTask(currentNode, true, SubTaskGroupType.WaitForDataMigration);
 
-      // Clear the host from master's blacklist.
-      if (currentNode.state == NodeState.Removed) {
-        createModifyBlackListTask(Arrays.asList(currentNode), false /* isAdd */)
+          masterAdded = true;
+        }
+
+        // Mark the node as tserver in the YW DB.
+        createUpdateNodeProcessTask(taskParams().nodeName, ServerType.TSERVER, true)
+            .setSubTaskGroupType(SubTaskGroupType.StartingNodeProcesses);
+
+        // Wait for new tablet servers to be responsive.
+        createWaitForServersTasks(node, ServerType.TSERVER)
             .setSubTaskGroupType(SubTaskGroupType.ConfigureUniverse);
+
+        // Update the swamper target file.
+        createSwamperTargetUpdateTask(false /* removeFile */);
+
+        // Clear the host from master's blacklist.
+        if (currentNode.state == NodeState.Removed) {
+            createModifyBlackListTask(Arrays.asList(currentNode), false /* isAdd */)
+                .setSubTaskGroupType(SubTaskGroupType.ConfigureUniverse);
+        }
+
+        // Wait for load to balance.
+        createWaitForLoadBalanceTask()
+            .setSubTaskGroupType(SubTaskGroupType.WaitForDataMigration);
+
+        // Update all tserver conf files with new master information.
+        if (masterAdded) {
+            createMasterInfoUpdateTask(universe, currentNode);
+        }
+
+        // Update node state to live.
+        createSetNodeStateTask(currentNode, NodeState.Live)
+            .setSubTaskGroupType(SubTaskGroupType.StartingNode);
+
+        if (wasDecommissioned) {
+            // Update the DNS entry for this universe.
+            createDnsManipulationTask(DnsManager.DnsCommandType.Edit, false,
+                                      userIntent.providerType, userIntent.provider,
+                                      userIntent.universeName)
+                .setSubTaskGroupType(SubTaskGroupType.ConfigureUniverse);
+        }
+
+        // Mark universe task state to success.
+        createMarkUniverseUpdateSuccessTasks()
+            .setSubTaskGroupType(SubTaskGroupType.StartingNode);
       }
-
-      // Wait for load to balance.
-      createWaitForLoadBalanceTask()
-          .setSubTaskGroupType(SubTaskGroupType.WaitForDataMigration);
-
-      // Update all tserver conf files with new master information.
-      if (masterAdded) {
-        createMasterInfoUpdateTask(universe, currentNode);
-      }
-
-      // Update node state to live.
-      createSetNodeStateTask(currentNode, NodeState.Live)
-          .setSubTaskGroupType(SubTaskGroupType.StartingNode);
-
-      if (wasDecommissioned) {
-        UserIntent userIntent = universe.getUniverseDetails()
-                                        .getClusterByUuid(currentNode.placementUuid)
-                                        .userIntent;
-
-        // Update the DNS entry for this universe.
-        createDnsManipulationTask(DnsManager.DnsCommandType.Edit, false, userIntent.providerType,
-                                  userIntent.provider, userIntent.universeName)
-            .setSubTaskGroupType(SubTaskGroupType.ConfigureUniverse);
-      }
-
-      // Mark universe task state to success.
-      createMarkUniverseUpdateSuccessTasks()
-          .setSubTaskGroupType(SubTaskGroupType.StartingNode);
 
       // Run all the tasks.
       subTaskGroupQueue.run();
@@ -209,7 +216,7 @@ public class AddNodeToUniverse extends UniverseDefinitionTaskBase {
       }
 
       // Mark the update of the universe as done. This will allow future updates to the universe.
-      unlockUniverseForUpdate();
+      unlockUniverseForUpdate(errorString);
     }
     LOG.info("Finished {} task.", getName());
   }

managed/devops/opscli/ybops/cloud/onprem/method.py
@@ -129,9 +129,10 @@ class OnPremDestroyInstancesMethod(DestroyInstancesMethod):
     def __init__(self, base_command):
         super(OnPremDestroyInstancesMethod, self).__init__(base_command)
 
-    def get_ssh_user(self):
-        # Force destroy instances to use the "yugabyte" user.
-        return "yugabyte"
+    def add_extra_args(self):
+        super(OnPremDestroyInstancesMethod, self).add_extra_args()
+        self.parser.add_argument("--install_node_exporter", action="store_true",
+                                 help='Check if node exporter should be stopped.')
 
     def callback(self, args):
         host_info = self.cloud.get_host_info(args)
@@ -139,7 +140,15 @@ class OnPremDestroyInstancesMethod(DestroyInstancesMethod):
             logging.error("Host {} does not exists.".format(args.search_pattern))
             return
 
-        # Force destroy instances to use the "yugabyte" user.
+        # Run non-db related tasks.
+        self.update_ansible_vars_with_args(args)
+        if args.install_node_exporter:
+            logging.info(("[app] Running control script stop " +
+                          "against thirdparty services at {}").format(host_info['name']))
+            self.cloud.run_control_script(
+                "thirdparty", "stop-services", args, self.extra_vars, host_info)
+
+        # Force db-related commands to use the "yugabyte" user.
         args.ssh_user = "yugabyte"
         self.update_ansible_vars_with_args(args)
         servers = ["master", "tserver"]

managed/devops/opscli/ybops/utils/__init__.py
@@ -45,9 +45,9 @@ if sys.version_info[0] == 2:
 BLOCK_SIZE = 4096
 HOME_FOLDER = os.environ["HOME"]
 YB_FOLDER_PATH = os.path.join(HOME_FOLDER, ".yugabyte")
-SSH_RETRY_LIMIT = 20
+SSH_RETRY_LIMIT = 2
 DEFAULT_SSH_PORT = 22
-SSH_TIMEOUT = 15
+SSH_TIMEOUT = 5
 
 RSA_KEY_LENGTH = 2048
 RELEASE_VERSION_FILENAME = "version.txt"

managed/devops/yb-server-ctl.yml
@@ -15,3 +15,18 @@
   tasks:
     - name: Call the ctl script with appropriate args
       shell: "{{ script_to_run }} {{ process }} {{ command }}"
+      when: process != "thirdparty"
+
+- name: Stopping thirdparty services
+  hosts: "{{ instance_search_pattern }}"
+  vars_files:
+    - "{{ vars_file | default(placeholder_vars_file) }}"
+  tasks:
+    - name: Stop node exporter service
+      service:
+        enabled: no
+        name: node_exporter
+        state: stopped
+      become: yes
+      become_method: sudo
+      when: process == "thirdparty"

managed/src/main/java/com/yugabyte/yw/commissioner/SubTaskGroup.java
@@ -170,7 +170,6 @@ public class SubTaskGroup implements Runnable {
 
   public boolean waitFor() {
     boolean hasErrored = false;
-    boolean alwaysRunAll = this.getSubTaskGroupType().getAlwaysRunAll();
     for (Future<?> future : futuresMap.keySet()) {
       // Wait for each future to finish.
       String errorString = null;
@@ -194,9 +193,6 @@ public class SubTaskGroup implements Runnable {
           details.put("errorString", errorString);
           taskInfo.setTaskDetails(details);
           taskInfo.save();
-          if (!alwaysRunAll) {
-            return false;
-          }
         }
       }
     }

managed/src/main/java/com/yugabyte/yw/commissioner/UserTaskDetails.java
@@ -19,7 +19,7 @@ public class UserTaskDetails {
     Invalid,
 
     // Perform preflight checks to determine if the node is ready to be configured or provisioned.
-    PreflightChecks(true),
+    PreflightChecks,
 
     // Deploying machines in the desired cloud, fetching information (ip address, etc) of these
     // newly deployed machines, etc.
@@ -149,20 +149,6 @@ public class UserTaskDetails {
 
     // Rotate Node Certs.
     RotatingCert;
-
-    private boolean alwaysRunAll;
-
-    SubTaskGroupType() {
-      this.alwaysRunAll = false;
-    }
-
-    SubTaskGroupType(boolean alwaysRunAll) {
-      this.alwaysRunAll = alwaysRunAll;
-    }
-
-    public boolean getAlwaysRunAll() {
-      return this.alwaysRunAll;
-    }
   }
 
   public List<SubTaskDetails> taskDetails;

managed/src/main/java/com/yugabyte/yw/commissioner/tasks/CreateUniverse.java
@@ -10,6 +10,8 @@
 
 package com.yugabyte.yw.commissioner.tasks;
 
+import java.util.Map;
+import java.util.HashMap;
 import java.util.Set;
 
 import org.slf4j.Logger;
@@ -26,6 +28,8 @@ import com.yugabyte.yw.commissioner.tasks.params.NodeTaskParams;
 import com.yugabyte.yw.common.DnsManager;
 import com.yugabyte.yw.common.PlacementInfoUtil;
 import com.yugabyte.yw.forms.UniverseDefinitionTaskParams.Cluster;
+import com.yugabyte.yw.forms.UniverseDefinitionTaskParams.UserIntent;
+import com.yugabyte.yw.models.NodeInstance;
 import com.yugabyte.yw.models.Universe;
 import com.yugabyte.yw.models.helpers.NodeDetails;
 
@@ -60,8 +64,31 @@ public class CreateUniverse extends UniverseDefinitionTaskBase {
       Cluster primaryCluster = taskParams().getPrimaryCluster();
 
       // Check if nodes are able to be provisioned/configured properly.
-      createPrecheckTasks(taskParams().nodeDetailsSet)
+      Map<NodeInstance, String> failedNodes = new HashMap<>();
+      for (NodeDetails node: taskParams().nodeDetailsSet) {
+        if (!universe.getCluster(node.placementUuid)
+                    .userIntent.providerType.equals(CloudType.onprem)) {
+          continue;
+        }
+
+        NodeTaskParams nodeParams = new NodeTaskParams();
+        UserIntent userIntent = taskParams().getClusterByUuid(node.placementUuid).userIntent;
+        nodeParams.nodeName = node.nodeName;
+        nodeParams.deviceInfo = userIntent.deviceInfo;
+        nodeParams.azUuid = node.azUuid;
+        nodeParams.universeUUID = taskParams().universeUUID;
+        nodeParams.extraDependencies.installNodeExporter =
+          taskParams().extraDependencies.installNodeExporter;
+
+        String preflightStatus = performPreflightCheck(node, nodeParams);
+        if (preflightStatus != null) {
+            failedNodes.put(NodeInstance.getByName(node.nodeName), preflightStatus);
+        }
+      }
+      if (!failedNodes.isEmpty()) {
+        createFailedPrecheckTask(failedNodes, true)
           .setSubTaskGroupType(SubTaskGroupType.PreflightChecks);
+      }
 
       // Update the universe to the latest state and
       // check if the nodes already exist in the cloud provider, if so,

managed/src/main/java/com/yugabyte/yw/commissioner/tasks/EditUniverse.java
@@ -43,6 +43,7 @@ public class EditUniverse extends UniverseDefinitionTaskBase {
   @Override
   public void run() {
     LOG.info("Started {} task for uuid={}", getName(), taskParams().universeUUID);
+    String errorString = null;
 
     try {
       // Verify the task params.
@@ -58,26 +59,31 @@ public class EditUniverse extends UniverseDefinitionTaskBase {
       // Set all the node names.
       setNodeNames(UniverseOpType.EDIT, universe);
 
-      // Select master nodes, if needed.
-      selectMasters();
+      // Run preflight checks on onprem nodes to be added.
+      if (reserveAndCheckOnpremNodesToBeAdded()) {
+        // Select master nodes, if needed.
+        selectMasters();
 
-      // Update the user intent.
-      writeUserIntentToUniverse();
+        // Update the user intent.
+        writeUserIntentToUniverse(false, false);
 
-      for (Cluster cluster : taskParams().clusters) {
-        addDefaultGFlags(cluster.userIntent);
-        editCluster(universe, cluster);
+        for (Cluster cluster : taskParams().clusters) {
+          addDefaultGFlags(cluster.userIntent);
+          editCluster(universe, cluster);
+        }
+
+        // Update the DNS entry for this universe, based in primary provider info.
+        UserIntent primaryIntent = universe.getUniverseDetails().getPrimaryCluster().userIntent;
+        createDnsManipulationTask(DnsManager.DnsCommandType.Edit, false, primaryIntent.providerType,
+                primaryIntent.provider, primaryIntent.universeName)
+                .setSubTaskGroupType(SubTaskGroupType.ConfigureUniverse);
+
+        // Marks the update of this universe as a success only if all the tasks before it succeeded.
+        createMarkUniverseUpdateSuccessTasks()
+          .setSubTaskGroupType(SubTaskGroupType.ConfigureUniverse);
+      } else {
+        errorString = "Preflight checks failed.";
       }
-
-      // Update the DNS entry for this universe, based in primary provider info.
-      UserIntent primaryIntent = universe.getUniverseDetails().getPrimaryCluster().userIntent;
-      createDnsManipulationTask(DnsManager.DnsCommandType.Edit, false, primaryIntent.providerType,
-              primaryIntent.provider, primaryIntent.universeName)
-              .setSubTaskGroupType(SubTaskGroupType.ConfigureUniverse);
-
-      // Marks the update of this universe as a success only if all the tasks before it succeeded.
-      createMarkUniverseUpdateSuccessTasks()
-        .setSubTaskGroupType(SubTaskGroupType.ConfigureUniverse);
 
       // Run all the tasks.
       subTaskGroupQueue.run();
@@ -87,7 +93,7 @@ public class EditUniverse extends UniverseDefinitionTaskBase {
     } finally {
       // Mark the update of the universe as done. This will allow future edits/updates to the
       // universe to happen.
-      unlockUniverseForUpdate();
+      unlockUniverseForUpdate(errorString);
     }
     LOG.info("Finished {} task.", getName());
   }
@@ -120,9 +126,6 @@ public class EditUniverse extends UniverseDefinitionTaskBase {
     }
 
     if (!nodesToProvision.isEmpty()) {
-      createPrecheckTasks(nodesToProvision)
-          .setSubTaskGroupType(SubTaskGroupType.PreflightChecks);
-
       // Create the required number of nodes in the appropriate locations.
       createSetupServerTasks(nodesToProvision)
           .setSubTaskGroupType(SubTaskGroupType.Provisioning);

commit 677c693cb8b46e0059b5bf0564155a6bc511f361

[Backport 2.4][#7019] Platform: AddNode operation fails with timeout
Summary:
When doing an Add Node operation, platform does not bring up the tserver, so whole operation fails
with timeout when waiting for the server to come up. This diff fixes that.

This was broken in commit 3c643b505ca83fcf05622a1b1162cae8696870aa

Test Plan: Create RF3 universe, Remove node, release node, add node.

Reviewers: sanketh, daniel, arnav

Reviewed By: arnav

Subscribers: jenkins-bot, yugaware

Differential Revision: https://phabricator.dev.yugabyte.com/D10483

managed/src/main/java/com/yugabyte/yw/commissioner/tasks/AddNodeToUniverse.java
@@ -163,6 +163,13 @@ public class AddNodeToUniverse extends UniverseDefinitionTaskBase {
           masterAdded = true;
         }
 
+        // Set gflags for the tserver.
+        createGFlagsOverrideTasks(node, ServerType.TSERVER);
+
+        // Add the tserver process start task.
+        createTServerTaskForNode(currentNode, "start")
+            .setSubTaskGroupType(SubTaskGroupType.StartingNodeProcesses);
+
         // Mark the node as tserver in the YW DB.
         createUpdateNodeProcessTask(taskParams().nodeName, ServerType.TSERVER, true)
             .setSubTaskGroupType(SubTaskGroupType.StartingNodeProcesses);