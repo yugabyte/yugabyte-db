d8fda10528ff6a085d5226779c8fdcbe5e5154eb
mahendra bhat <mahenrda.bhat@hashedin.com>
2021-02-03 17:06:14 +0530

    Removed unused imports

--- managed/src/test/java/com/yugabyte/yw/controllers/UniverseControllerTest.java
+++ managed/src/test/java/com/yugabyte/yw/controllers/UniverseControllerTest.java
@@ -21,7 +21,6 @@ import static org.hamcrest.CoreMatchers.allOf;
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.CoreMatchers.notNullValue;
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertThat;
