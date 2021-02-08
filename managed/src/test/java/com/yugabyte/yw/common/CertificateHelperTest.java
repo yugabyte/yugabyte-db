073b50ad5a34188a87782de7d263b30b6cc55d94
jitendra-12113 <jitedra.kumar@hashedin.com>
2021-02-09 02:15:28 +0530

    Heading: [5594][Platform] Handle invalid certs/keys correctly Description: We wanted to verify the cert at upload time itself. Testing: When we try to upload selfSigned cert, we were able to upload any garbage files. Now with this fix, we have handled this scenrio. User won't be able to upload garbage file. Incase of self signed cert upload workflow, We are matching the modulus of cert RSA Public key with the modulus of its private key if its matched then it means certs are valid otheriwse Invalid. For CA Cert, we are just validating the Cert format. Added the unit Tests as well.
    (cherry picked from commit 5f8d39a0366fd8a0552f8d8d3d0b119509b82549)

--- managed/src/test/java/com/yugabyte/yw/common/CertificateHelperTest.java
+++ managed/src/test/java/com/yugabyte/yw/common/CertificateHelperTest.java
@@ -150,6 +150,28 @@ public class CertificateHelperTest extends FakeDBApplication{
     clientCer.verify(cer.getPublicKey(), "BC");
   }
 
+  private String getCertContent() {
+    return "-----BEGIN CERTIFICATE-----\n" +
+      "MIIDDjCCAfagAwIBAgIGAXVXb5y/MA0GCSqGSIb3DQEBCwUAMDQxHDAaBgNVBAMM\n" +
+      "E3liLWFkbWluLXRlc3QtYXJuYXYxFDASBgNVBAoMC2V4YW1wbGUuY29tMB4XDTIw\n" +
+      "MTAyMzIxNDg1M1oXDTIxMTAyMzIxNDg1M1owNDEcMBoGA1UEAwwTeWItYWRtaW4t\n" +
+      "dGVzdC1hcm5hdjEUMBIGA1UECgwLZXhhbXBsZS5jb20wggEiMA0GCSqGSIb3DQEB\n" +
+      "AQUAA4IBDwAwggEKAoIBAQCVWSZiQhr9e+L2MkSXP38dwXwF7RlZGhrYKrL7pp6l\n" +
+      "aHkLZ0lsFgxI6h0Yyn5S+Hhi/jGWbBso6kXw7frUwVY5kX2Q6iv+E+rKqbYQgNV3\n" +
+      "0vpCtOmNNolhNN3x4SKAIXyKOB5dXMGesjvba/qD6AstKS8bvRCUZcYDPjIUQGPu\n" +
+      "cYLmywV/EdXgB+7WLhUOOY2eBRWBrnGxk60pcHJZeW44g1vas9cfiw81OWVp5/V5\n" +
+      "apA631bE0MTgg283OCyYz+CV/YtnytUTg/+PUEqzM2cWsWdvpEz7TkKYXinRdN4d\n" +
+      "SwgOQEIvb7A/GYYmVf3yk4moUxEh4NLoV9CBDljEBUjZAgMBAAGjJjAkMBIGA1Ud\n" +
+      "EwEB/wQIMAYBAf8CAQEwDgYDVR0PAQH/BAQDAgLkMA0GCSqGSIb3DQEBCwUAA4IB\n" +
+      "AQAFR0m7r1I3FyoatuLBIG+alaeGHqsgNqseAJTDGlEyajGDz4MT0c76ZIQkTSGM\n" +
+      "vsM49Ad2D04sJR44/WlI2AVijubzHBr6Sj8ZdB909nPvGtB+Z8OnvKxJ0LUKyG1K\n" +
+      "VUbcCnN3qSoVeY5PaPeFMmWF0Qv4S8lRTZqAvCnk34bckwkWoHkuuNGO49CsNb40\n" +
+      "Z2NBy9Ckp0KkfeDpGcv9lHuUrl13iakCY09irvYRbfi0lVGF3+wXZtefV8ZAxfnN\n" +
+      "Vt4faawkJ79oahlXDYs6WCKEd3zVM3mR3STnzwxvtB6WacjhqgP4ozXdt6PUbTfZ\n" +
+      "jZPSP3OuL0IXk96wFHScay8S\n" +
+      "-----END CERTIFICATE-----\n";
+  }
+
   @Test
   public void testUploadRootCA() {
     Calendar cal = Calendar.getInstance();
@@ -157,9 +179,11 @@ public class CertificateHelperTest extends FakeDBApplication{
     cal.add(Calendar.YEAR, 1);
     Date certExpiry = cal.getTime();
     UUID rootCA = null;
-    CertificateInfo.Type type = CertificateInfo.Type.SelfSigned;
+    CertificateInfo.Type type = CertificateInfo.Type.CustomCertHostPath;
+    String cert_content = getCertContent();
+
     try {
-      rootCA = CertificateHelper.uploadRootCA("test", c.uuid, "/tmp", "test_cert", "test_key",
+      rootCA = CertificateHelper.uploadRootCA("test", c.uuid, "/tmp", cert_content, null,
           certStart, certExpiry, type, null);
     } catch (Exception e) {
       fail(e.getMessage());
@@ -167,4 +191,37 @@ public class CertificateHelperTest extends FakeDBApplication{
     assertNotNull(CertificateInfo.get(rootCA));
   }
 
+  @Test
+  public void testUploadRootCAWithInvalidCertContent() {
+    Calendar cal = Calendar.getInstance();
+    Date certStart = cal.getTime();
+    cal.add(Calendar.YEAR, 1);
+    Date certExpiry = cal.getTime();
+    UUID rootCA = null;
+    CertificateInfo.Type type = CertificateInfo.Type.CustomCertHostPath;
+
+    try {
+      rootCA = CertificateHelper.uploadRootCA("test", c.uuid, "/tmp", "invalid_cert", null,
+        certStart, certExpiry, type, null);
+    } catch (Exception e) {
+      assertEquals("Unable to get cert Object", e.getMessage());
+    }
+  }
+
+  @Test
+  public void testSelfSignedCertUploadRootCA() {
+    Calendar cal = Calendar.getInstance();
+    Date certStart = cal.getTime();
+    cal.add(Calendar.YEAR, 1);
+    Date certExpiry = cal.getTime();
+    UUID rootCA = null;
+    CertificateInfo.Type type = CertificateInfo.Type.SelfSigned;
+    String cert_content = getCertContent();
+    try {
+      rootCA = CertificateHelper.uploadRootCA("test", c.uuid, "/tmp", cert_content, "test_key",
+        certStart, certExpiry, type, null);
+    } catch (Exception e) {
+      assertEquals("Invalid certificate.", e.getMessage());
+    }
+  }
 }
