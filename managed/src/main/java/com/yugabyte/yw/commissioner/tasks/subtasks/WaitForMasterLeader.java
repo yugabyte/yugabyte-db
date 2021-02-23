18d6fd100cf60a99ab149a47acc1af12394ab413
Arnav Agarwal <14933889+Arnav15@users.noreply.github.com>
2021-02-23 10:18:54 +0530

    [Backport2.6][Platform][#6716, #8084]: Add mTLS support for platform and separate internal and external TLS connectivity options.
    Summary:
    Since the DB supports mTLS, we should ensure that the platform also communicates with the
    DB using both client and server side verification. This diff adds support for providing a client
    side cert to the servers while making RPCs through the platform.
    This diff also fixes the issue of not being able to use client to node TLS if node to node TLS was turned off due to a platform limitation.
    
    Test Plan:
    Tested the following scenarios:
    1) Creating a node-to-node (no client-to-node) enabled universe with an existing self-signed cert.
    ```
    --node_to_node_encryption_use_client_certificates=true
    --use_client_to_server_encryption=false
    --use_node_to_node_encryption=true
    ```
    2) Creating a node-to-node enabled universe with a new self-signed cert.
    ```
    --node_to_node_encryption_use_client_certificates=true
    --use_client_to_server_encryption=false
    --use_node_to_node_encryption=true
    ```
    3) Created a client-to-node (no node-to-node) enabled universe with a new self-signed cert.
    ```
    --node_to_node_encryption_use_client_certificates=false
    --use_client_to_server_encryption=true
    --use_node_to_node_encryption=false
    ```
    4) Create a node-to-node and client-to-node enabled universe.
    ```
    --node_to_node_encryption_use_client_certificates=true
    --use_client_to_server_encryption=true
    --use_node_to_node_encryption=true
    ```
    All universes got created successfully.
    
    Reviewers: sanketh
    
    Reviewed By: sanketh
    
    Subscribers: yugaware, jenkins-bot, sshevchenko
    
    Differential Revision: https://phabricator.dev.yugabyte.com/D11433

--- managed/src/main/java/com/yugabyte/yw/commissioner/tasks/subtasks/WaitForMasterLeader.java
+++ managed/src/main/java/com/yugabyte/yw/commissioner/tasks/subtasks/WaitForMasterLeader.java
@@ -55,11 +55,12 @@ public class WaitForMasterLeader extends AbstractTaskBase {
   public void run() {
     Universe universe = Universe.getOrBadRequest(taskParams().universeUUID);
     String hostPorts = universe.getMasterAddresses();
-    String certificate = universe.getCertificate();
+    String certificate = universe.getCertificateNodeToNode();
+    String[] rpcClientCertFiles = universe.getFilesForMutualTLS();
     YBClient client = null;
     try {
       LOG.info("Running {}: hostPorts={}.", getName(), hostPorts);
-      client = ybService.getClient(hostPorts, certificate);
+      client = ybService.getClient(hostPorts, certificate, rpcClientCertFiles);
       client.waitForMasterLeader(config.getDuration("yb.wait_for_server_timeout").toMillis());
     } catch (Exception e) {
       LOG.error("{} hit error : {}", getName(), e.getMessage());
