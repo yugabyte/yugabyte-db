5b459514413e969126551c05aea3a2c202e1b1dc
Ibrar Ahmed <ibrar.ahmad@gmail.com>
2021-02-23 16:23:19 +0000

    PG-183. Move tests to separate directory.
    Remove unwanted sql and expected directory.

--- sql/basic.sql
+++
@@ -1,7 +0,0 @@
-CREATE EXTENSION pg_stat_monitor;
-SELECT pg_stat_monitor_reset();
-select pg_sleep(.5);
-SELECT 1;
-SELECT query FROM pg_stat_monitor ORDER BY query COLLATE "C";
-SELECT pg_stat_monitor_reset();
-DROP EXTENSION pg_stat_monitor;
