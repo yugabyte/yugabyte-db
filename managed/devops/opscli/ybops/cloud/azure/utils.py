yugabyte/yugabyte-db

commit 4b10467c6df656c42cb14a50e886149fc8c026a6

#5626: Enable Hosted Zone for Azure
Summary:
Users can enter a private DNS zone to use when they create an Azure provider.
Any instances created with that provider will create the relevent DNS records and update them as
necessary.

Test Plan:
Create provider with custom private DNS zone. Create universe. Check on Azure portal if the
Records are correct.

Reviewers: sanketh, daniel, sb-yb, arnav, spotachev

Reviewed By: spotachev

Subscribers: jenkins-bot, yugaware

Differential Revision: https://phabricator.dev.yugabyte.com/D10648

managed/devops/opscli/ybops/cloud/azure/utils.py
@@ -11,11 +11,9 @@ from azure.mgmt.network import NetworkManagementClient
 from azure.mgmt.resource import ResourceManagementClient
 from azure.mgmt.compute import ComputeManagementClient
 from azure.mgmt.compute.models import DiskCreateOption
+from azure.mgmt.privatedns import PrivateDnsManagementClient
 from msrestazure.azure_exceptions import CloudError
-
-from collections import defaultdict
-
-from ybops.utils import is_valid_ip_address, validated_key_file, format_rsa_key, wait_for_ssh
+from ybops.utils import validated_key_file, format_rsa_key, DNS_RECORD_SET_TTL
 from ybops.common.exceptions import YBOpsRuntimeError
 
 import logging
@@ -30,6 +28,7 @@ RESOURCE_GROUP = os.environ.get("AZURE_RG")
 SUBNET_ID_FORMAT_STRING = "/subscriptions/{}/resourceGroups/{}/providers/Microsoft.Network/virtualNetworks/{}/subnets/{}"
 NSG_ID_FORMAT_STRING = "/subscriptions/{}/resourceGroups/{}/providers/Microsoft.Network/networkSecurityGroups/{}"
 VNET_ID_FORMAT_STRING = "/subscriptions/{}/resourceGroups/{}/providers/Microsoft.Network/virtualNetworks/{}"
+PRIVATE_DNS_ZONE_ID_FORMAT_STRING = "/subscriptions/{}/resourceGroups/{}/providers/Microsoft.Network/privateDnsZones/{}"
 AZURE_SKU_FORMAT = {"premium_lrs": "Premium_LRS",
                     "standardssd_lrs": "StandardSSD_LRS",
                     "ultrassd_lrs": "UltraSSD_LRS"}
@@ -43,6 +42,7 @@ VM_PRICING_URL_FORMAT = "https://prices.azure.com/api/retail/prices?$filter=" \
     "serviceFamily eq 'Compute' " \
     "and serviceName eq 'Virtual Machines' and priceType eq 'Consumption' " \
     "and armRegionName eq '{}'"
+PRIVATE_DNS_ZONE_ID_REGEX = re.compile("/subscriptions/(?P<subscription_id>[^/]*)/resourceGroups/(?P<resource_group>[^/]*)/providers/Microsoft.Network/privateDnsZones/(?P<zone_name>[^/]*)")
 
 
 def get_credentials():
@@ -288,6 +288,7 @@ class AzureCloudAdmin():
         self.credentials = get_credentials()
         self.compute_client = ComputeManagementClient(self.credentials, SUBSCRIPTION_ID)
         self.network_client = NetworkManagementClient(self.credentials, SUBSCRIPTION_ID)
+        self.dns_client = PrivateDnsManagementClient(self.credentials, SUBSCRIPTION_ID)
 
     def network(self, per_region_meta={}):
         return AzureBootstrapClient(per_region_meta, self.network_client, self.metadata)
@@ -671,7 +672,48 @@ class AzureCloudAdmin():
 
         subnet = id_to_name(nic.ip_configurations[0].subnet.id)
         server_type = vm.tags.get("yb-server-type", None) if vm.tags else None
-        return {"private_ip": private_ip, "public_ip": public_ip, "region": region,
+        return {"private_ip": public_ip, "public_ip": public_ip, "region": region,
                 "zone": "{}-{}".format(region, zone), "name": vm.name, "ip_name": ip_name,
                 "instance_type": vm.hardware_profile.vm_size, "server_type": server_type,
                 "subnet": subnet, "nic": nic_name, "id": vm.name}
+
+    def list_dns_record_set(self, dns_zone_id):
+        zone_info = PRIVATE_DNS_ZONE_ID_REGEX.match(dns_zone_id)
+        if not zone_info:
+            return PRIVATE_DNS_ZONE_ID_FORMAT_STRING.format(
+                SUBSCRIPTION_ID, RESOURCE_GROUP, dns_zone_id)
+        return self.dns_client.private_zones.get(
+            zone_info.group('resource_group'), zone_info.group('zone_name'))
+
+    def create_dns_record_set(self, dns_zone_id, domain_name_prefix, ip_list):
+        parameters = self._get_dns_record_set_args(dns_zone_id, domain_name_prefix, ip_list)
+        # Setting if_none_match="*" will cause this to error if a record with the name exists.
+        return self.dns_client.record_sets.create_or_update(if_none_match="*", **parameters)
+
+    def edit_dns_record_set(self, dns_zone_id, domain_name_prefix, ip_list):
+        parameters = self._get_dns_record_set_args(dns_zone_id, domain_name_prefix, ip_list)
+        return self.dns_client.record_sets.update(**parameters)
+
+    def delete_dns_record_set(self, dns_zone_id, domain_name_prefix):
+        parameters = self._get_dns_record_set_args(dns_zone_id, domain_name_prefix)
+        return self.dns_client.record_sets.delete(**parameters)
+
+    def _get_dns_record_set_args(self, dns_zone_id, domain_name_prefix, ip_list=None):
+        zone_info = PRIVATE_DNS_ZONE_ID_REGEX.match(dns_zone_id)
+        rg = zone_info.group('resource_group')
+        zone_name = zone_info.group('zone_name')
+        args = {
+            "resource_group_name": rg,
+            "private_zone_name": zone_name,
+            "record_type": "A",
+            "relative_record_set_name": "{}.{}".format(domain_name_prefix, zone_name),
+        }
+
+        if ip_list is not None:
+            params = {
+                "ttl": DNS_RECORD_SET_TTL,
+                "arecords": [{"ipv4_address": ip} for ip in ip_list]
+            }
+            args["parameters"] = params
+
+        return args

managed/devops/opscli/ybops/cloud/aws/utils.py
@@ -15,7 +15,7 @@ import os
 import re
 
 from ipaddress import ip_network
-from ybops.utils import get_or_create, get_and_cleanup
+from ybops.utils import get_or_create, get_and_cleanup, DNS_RECORD_SET_TTL
 from ybops.common.exceptions import YBOpsRuntimeError
 from ybops.cloud.common.utils import request_retry_decorator
 
@@ -1042,7 +1042,7 @@ def _update_dns_record_set(hosted_zone_id, domain_name_prefix, ip_list, action):
             'ResourceRecordSet': {
                 'Name': "{}.{}".format(domain_name_prefix, hosted_zone_name),
                 'Type': 'A',
-                'TTL': 5,
+                'TTL': DNS_RECORD_SET_TTL,
                 'ResourceRecords': records
             }
         }]

managed/devops/opscli/ybops/cloud/azure/cloud.py
@@ -15,7 +15,7 @@ import os
 from ybops.common.exceptions import YBOpsRuntimeError
 from ybops.cloud.common.cloud import AbstractCloud
 from ybops.cloud.azure.command import AzureNetworkCommand, AzureInstanceCommand, \
-    AzureAccessCommand, AzureQueryCommand
+    AzureAccessCommand, AzureQueryCommand, AzureDnsCommand
 from ybops.cloud.azure.utils import AzureBootstrapClient, AzureCloudAdmin, create_resource_group
 
 
@@ -40,6 +40,7 @@ class AzureCloud(AbstractCloud):
         self.add_subcommand(AzureNetworkCommand())
         self.add_subcommand(AzureAccessCommand())
         self.add_subcommand(AzureQueryCommand())
+        self.add_subcommand(AzureDnsCommand())
 
     def network_bootstrap(self, args):
         # Each region code maps to dictionary containing
@@ -154,3 +155,15 @@ class AzureCloud(AbstractCloud):
 
     def update_disk(self, args):
         raise YBOpsRuntimeError("Update Disk not implemented for Azure")
+
+    def list_dns_record_set(self, dns_zone_id):
+        return self.get_admin().list_dns_record_set(dns_zone_id)
+
+    def create_dns_record_set(self, dns_zone_id, domain_name_prefix, ip_list):
+        return self.get_admin().create_dns_record_set(dns_zone_id, domain_name_prefix, ip_list)
+
+    def edit_dns_record_set(self, dns_zone_id, domain_name_prefix, ip_list):
+        return self.get_admin().edit_dns_record_set(dns_zone_id, domain_name_prefix, ip_list)
+
+    def delete_dns_record_set(self, dns_zone_id, domain_name_prefix):
+        return self.get_admin().delete_dns_record_set(dns_zone_id, domain_name_prefix)

managed/devops/opscli/ybops/cloud/azure/command.py
@@ -12,7 +12,8 @@ from ybops.cloud.azure.method import AzureNetworkBootstrapMethod, AzureProvision
     AzureCreateInstancesMethod, AzureAccessAddKeyMethod, AzureQueryVPCMethod, \
     AzureQueryRegionsMethod, AzureQueryZonesMethod, AzureDestroyInstancesMethod, \
     AzureQueryInstanceTypesMethod, AzureQueryVnetMethod, AzureAccessDeleteKeyMethod, \
-    AzureNetworkCleanupMethod, AzureQueryUltraMethod
+    AzureNetworkCleanupMethod, AzureQueryUltraMethod, AzureCreateDnsEntryMethod, \
+    AzureEditDnsEntryMethod, AzureDeleteDnsEntryMethod, AzureListDnsEntryMethod
 from ybops.cloud.common.method import AccessCreateVaultMethod, ConfigureInstancesMethod, \
     ListInstancesMethod, InitYSQLMethod, UpdateDiskMethod, CronCheckMethod
 
@@ -62,3 +63,14 @@ class AzureQueryCommand(QueryCommand):
         self.add_method(AzureQueryInstanceTypesMethod(self))
         self.add_method(AzureQueryVnetMethod(self))
         self.add_method(AzureQueryUltraMethod(self))
+
+
+class AzureDnsCommand(DnsCommand):
+    def __init__(self):
+        super(AzureDnsCommand, self).__init__()
+
+    def add_methods(self):
+        self.add_method(AzureCreateDnsEntryMethod(self))
+        self.add_method(AzureEditDnsEntryMethod(self))
+        self.add_method(AzureDeleteDnsEntryMethod(self))
+        self.add_method(AzureListDnsEntryMethod(self))

managed/devops/opscli/ybops/cloud/azure/method.py
@@ -187,3 +187,65 @@ class AzureQueryUltraMethod(AbstractMethod):
         self.parser.add_argument("--regions", nargs='+')
         self.parser.add_argument("--folder", required=True,
                                  help="Folder to write region ultra disk json.")
+
+
+class AbstractDnsMethod(AbstractMethod):
+    def __init__(self, base_command, method_name):
+        super(AbstractDnsMethod, self).__init__(base_command, method_name)
+        self.ip_list = []
+        self.naming_info_required = True
+
+    def add_extra_args(self):
+        super(AbstractDnsMethod, self).add_extra_args()
+        self.parser.add_argument("--hosted_zone_id", required=True,
+                                 help="The ID of the Azure private DNS zone.")
+        self.parser.add_argument("--domain_name_prefix", required=self.naming_info_required,
+                                 help="The prefix to create the RecordSet with, in your Zone.")
+        self.parser.add_argument("--node_ips", required=self.naming_info_required,
+                                 help="The CSV of the node IPs to associate to this DNS entry.")
+
+    def preprocess_args(self, args):
+        super(AbstractDnsMethod, self).preprocess_args(args)
+        if args.node_ips:
+            self.ip_list = args.node_ips.split(',')
+
+
+class AzureCreateDnsEntryMethod(AbstractDnsMethod):
+    def __init__(self, base_command):
+        super(AzureCreateDnsEntryMethod, self).__init__(base_command, "create")
+
+    def callback(self, args):
+        self.cloud.create_dns_record_set(
+            args.hosted_zone_id, args.domain_name_prefix, self.ip_list)
+
+
+class AzureEditDnsEntryMethod(AbstractDnsMethod):
+    def __init__(self, base_command):
+        super(AzureEditDnsEntryMethod, self).__init__(base_command, "edit")
+
+    def callback(self, args):
+        self.cloud.edit_dns_record_set(
+            args.hosted_zone_id, args.domain_name_prefix, self.ip_list)
+
+
+class AzureDeleteDnsEntryMethod(AbstractDnsMethod):
+    def __init__(self, base_command):
+        super(AzureDeleteDnsEntryMethod, self).__init__(base_command, "delete")
+
+    def callback(self, args):
+        self.cloud.delete_dns_record_set(args.hosted_zone_id, args.domain_name_prefix)
+
+
+class AzureListDnsEntryMethod(AbstractDnsMethod):
+    def __init__(self, base_command):
+        super(AzureListDnsEntryMethod, self).__init__(base_command, "list")
+        self.naming_info_required = False
+
+    def callback(self, args):
+        try:
+            result = self.cloud.list_dns_record_set(args.hosted_zone_id)
+            print(json.dumps({
+                'name': result.name
+            }))
+        except Exception as e:
+            print(json.dumps({'error': repr(e)}))

managed/devops/opscli/ybops/utils/__init__.py
@@ -49,7 +49,7 @@ YB_FOLDER_PATH = os.path.join(HOME_FOLDER, ".yugabyte")
 SSH_RETRY_LIMIT = 20
 DEFAULT_SSH_PORT = 22
 # Timeout in seconds.
-SSH_TIMEOUT = 15
+SSH_TIMEOUT = 45
 
 RSA_KEY_LENGTH = 2048
 RELEASE_VERSION_FILENAME = "version.txt"
@@ -58,6 +58,9 @@ RELEASE_REPOS = set(["devops", "yugaware", "yugabyte"])
 
 # Home directory of node instances. Try to read home dir from env, else assume it's /home/yugabyte.
 YB_HOME_DIR = os.environ.get("YB_HOME_DIR") or "/home/yugabyte"
+
+# TTL in seconds for how long DNS records will be cached.
+DNS_RECORD_SET_TTL = 5
 
 
 class ReleasePackage(object):

managed/src/main/java/com/yugabyte/yw/commissioner/tasks/UniverseTaskBase.java
@@ -221,8 +221,8 @@ public abstract class UniverseTaskBase extends AbstractTaskBase {
   public Universe lockUniverseForUpdate(int expectedUniverseVersion, boolean isResumeOrDelete) {
     UniverseUpdater updater = getLockingUniverseUpdater(
         expectedUniverseVersion,
-        true, 
-        false, 
+        true,
+        false,
         isResumeOrDelete
     );
     return lockUniverseForUpdate(expectedUniverseVersion, updater);
@@ -413,7 +413,7 @@ public abstract class UniverseTaskBase extends AbstractTaskBase {
     return subTaskGroup;
   }
 
-  
+
   /**
    * Creates a task list to pause the nodes and adds to the task queue.
    *
@@ -451,10 +451,10 @@ public abstract class UniverseTaskBase extends AbstractTaskBase {
     }
     subTaskGroupQueue.add(subTaskGroup);
     return subTaskGroup;
-  }  
+  }
 
 
-    
+
   /**
    * Creates a task list to resume nodes and adds it to the task queue.
    *
@@ -492,8 +492,8 @@ public abstract class UniverseTaskBase extends AbstractTaskBase {
     }
     subTaskGroupQueue.add(subTaskGroup);
     return subTaskGroup;
-  }  
-  
+  }
+
 
   /**
    * Create tasks to update the state of the nodes.
@@ -1036,12 +1036,12 @@ public abstract class UniverseTaskBase extends AbstractTaskBase {
       DnsManager.DnsCommandType eventType, boolean isForceDelete, CloudType providerType,
       String provider, String universeName) {
     SubTaskGroup subTaskGroup = new SubTaskGroup("UpdateDnsEntry", executor);
-    if (!providerType.equals(CloudType.aws)) {
+    if (!Provider.HostedZoneEnabledProviders.contains(providerType.toString())) {
       return subTaskGroup;
     }
     Provider p = Provider.get(UUID.fromString(provider));
     // TODO: shared constant with javascript land?
-    String hostedZoneId = p.getConfig().get("AWS_HOSTED_ZONE_ID");
+    String hostedZoneId = p.getHostedZoneId();
     if (hostedZoneId == null || hostedZoneId.isEmpty()) {
       return subTaskGroup;
     }

managed/src/main/java/com/yugabyte/yw/controllers/CloudProviderController.java
@@ -174,11 +174,11 @@ public class CloudProviderController extends AuthenticatedController {
     try {
       Provider provider = Provider.create(customerUUID, providerCode, formData.get().name, config);
       if (!config.isEmpty()) {
+        String hostedZoneId = provider.getHostedZoneId();
         switch (provider.code) {
           case "aws":
-            String hostedZoneId = provider.getAwsHostedZoneId();
             if (hostedZoneId != null) {
-              return validateAwsHostedZoneUpdate(provider, hostedZoneId);
+              return validateHostedZoneUpdate(provider, hostedZoneId);
             }
             break;
           case "gcp":
@@ -190,6 +190,11 @@ public class CloudProviderController extends AuthenticatedController {
               createKubernetesInstanceTypes(provider, customerUUID);
             } catch (javax.persistence.PersistenceException ex) {
               // TODO: make instance types more multi-tenant friendly...
+            }
+            break;
+          case "azu":
+            if (hostedZoneId != null) {
+              return validateHostedZoneUpdate(provider, hostedZoneId);
             }
             break;
         }
@@ -572,12 +577,12 @@ public class CloudProviderController extends AuthenticatedController {
       return ApiResponse.error(BAD_REQUEST, "Invalid Provider UUID: " + providerUUID);
     }
 
-    if (provider.code.equals("aws")) {
+    if (Provider.HostedZoneEnabledProviders.contains(provider.code)) {
       String hostedZoneId = formData.get("hostedZoneId").asText();
       if (hostedZoneId == null || hostedZoneId.length() == 0) {
         return ApiResponse.error(BAD_REQUEST, "Required field hosted zone id");
       }
-      return validateAwsHostedZoneUpdate(provider, hostedZoneId);
+      return validateHostedZoneUpdate(provider, hostedZoneId);
     } else if (provider.code.equals("kubernetes")) {
       Map<String, String> config = processConfig(formData, Common.CloudType.kubernetes);
       if (config != null) {
@@ -593,7 +598,7 @@ public class CloudProviderController extends AuthenticatedController {
     }
   }
 
-  private Result validateAwsHostedZoneUpdate(Provider provider, String hostedZoneId) {
+  private Result validateHostedZoneUpdate(Provider provider, String hostedZoneId) {
     // TODO: do we have a good abstraction to inspect this AND know that it's an error outside?
     ShellResponse response = dnsManager.listDnsRecord(
         provider.uuid, hostedZoneId);

commit 794ba77c3a8f1dffa35f3861cfc287abec974dbf

[Platform][#7549]: Platform uses public IP instead of private IP to connect to Azure universes
Summary:
When creating universes, the public IP is used for the private IP instead of the actual private IP.
This diff fixes that.

Caused by D10648

Test Plan: create azure universe and check private ip in db

Reviewers: arnav, daniel, sb-yb, spotachev, sanketh

Reviewed By: sanketh

Subscribers: yugaware

Differential Revision: https://phabricator.dev.yugabyte.com/D10832

managed/devops/opscli/ybops/cloud/azure/utils.py
@@ -672,7 +672,7 @@ class AzureCloudAdmin():
 
         subnet = id_to_name(nic.ip_configurations[0].subnet.id)
         server_type = vm.tags.get("yb-server-type", None) if vm.tags else None
-        return {"private_ip": public_ip, "public_ip": public_ip, "region": region,
+        return {"private_ip": private_ip, "public_ip": public_ip, "region": region,
                 "zone": "{}-{}".format(region, zone), "name": vm.name, "ip_name": ip_name,
                 "instance_type": vm.hardware_profile.vm_size, "server_type": server_type,
                 "subnet": subnet, "nic": nic_name, "id": vm.name}