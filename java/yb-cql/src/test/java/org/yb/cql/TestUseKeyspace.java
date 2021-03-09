efede2a8a3364b0b8db30513bd25c4246ee54773
Alex Abdugafarov <frozenspider@users.noreply.github.com>
2021-03-10 00:22:57 +0500

    [#5922] YCQL: Improve audit logging
    Summary:
    - Using strongly typed bools, minor improvements
    - Moving CQLMessage to yb/cql/ql/util, along with `lz4` and `snappy` dependencies.
    - Slight tweaks to `using namespace` - Improving tests
    - Rolled back changes done in #5918
    - Updated `(C)QLProcessor`'s contract to make it clear that `Rescheduler` implementation methods are exempt from non-concurrent guarantees.
      - Removed related workaround with `AuditLogger` concurrency introduced in #5918
    - Fixed Java tests reusing the same logger
    - Fixed Java driver "cluster" not being closed
    
    ---
    
    Resolves #5922
    
    Test Plan: ybd --java-test org.yb.cql.TestAudit
    
    Reviewers: mihnea, dmitry, oleg, mbautin
    
    Reviewed By: oleg, mbautin
    
    Subscribers: sergei, mbautin, jason, yql
    
    Differential Revision: https://phabricator.dev.yugabyte.com/D10501

--- java/yb-cql/src/test/java/org/yb/cql/TestUseKeyspace.java
+++ java/yb-cql/src/test/java/org/yb/cql/TestUseKeyspace.java
@@ -19,9 +19,12 @@ import static org.yb.AssertionWrappers.fail;
 import org.yb.YBTestRunner;
 
 import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @RunWith(value=YBTestRunner.class)
 public class TestUseKeyspace extends BaseCQLTest {
+  private static final Logger LOG = LoggerFactory.getLogger(TestUseKeyspace.class);
 
   private void testCreateExistingKeyspace(String keyspace) throws Exception {
     try {
