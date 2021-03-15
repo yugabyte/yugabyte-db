41a4d12bc0533c55c040cce2e398d38d22d3e122
Alex Zhao <azhao@yugabyte.com>
2021-03-15 16:31:58 -0700

    [#7369] ysql: Respect leader affinity on master sys catalog tablet
    Summary:
    Currently the master sys catalog tablet doesn't respect the cluster config's preferred
    zones. We add a background task to catalog manager that checks the current affinity specification
    and sends a StepDown request to a preferred master upon mismatch.
    
    Test Plan:
    New test suite `SysCatalogRespectAffinityTest` testing no preferred zone, leader in
    preferred zone, and leader not in preferred zone.
    
    Reviewers: nicolas, rahuldesirazu
    
    Reviewed By: rahuldesirazu
    
    Subscribers: bogdan, ybase
    
    Differential Revision: https://phabricator.dev.yugabyte.com/D10879

--- src/yb/master/catalog_manager.cc
+++ src/yb/master/catalog_manager.cc
@@ -9088,5 +9088,60 @@ void CatalogManager::RebuildYQLSystemPartitions() {
   }, wait_time);
 }
 
+Status CatalogManager::SysCatalogRespectLeaderAffinity() {
+  SysClusterConfigEntryPB config;
+  RETURN_NOT_OK(GetClusterConfig(&config));
+
+  const auto& affinitized_leaders = config.replication_info().affinitized_leaders();
+  if (affinitized_leaders.empty()) {
+    return Status::OK();
+  }
+
+  ServerRegistrationPB reg;
+  RETURN_NOT_OK(GetRegistration(&reg));
+
+  for (const CloudInfoPB& cloud_info : affinitized_leaders) {
+    // Do nothing if already in an affinitized zone.
+    if (CatalogManagerUtil::IsCloudInfoEqual(cloud_info, reg.cloud_info())) {
+      return Status::OK();
+    }
+  }
+
+  // Not in affinitized zone, try finding a master to send a step down request to.
+  std::vector<ServerEntryPB> masters;
+  RETURN_NOT_OK(master_->ListMasters(&masters));
+
+  for (const ServerEntryPB& master : masters) {
+    auto master_cloud_info = master.registration().cloud_info();
+
+    for (const CloudInfoPB& config_cloud_info : affinitized_leaders) {
+      if (CatalogManagerUtil::IsCloudInfoEqual(config_cloud_info, master_cloud_info)) {
+        LOG_WITH_PREFIX(INFO) << "Sys catalog tablet is not in an affinitized zone, "
+                              << "sending step down request to master uuid "
+                              << master.instance_id().permanent_uuid()
+                              << " in zone "
+                              << TSDescriptor::generate_placement_id(master_cloud_info);
+        std::shared_ptr<TabletPeer> tablet_peer;
+        RETURN_NOT_OK(GetTabletPeer(sys_catalog_->tablet_id(), &tablet_peer));
+
+        consensus::LeaderStepDownRequestPB req;
+        req.set_tablet_id(sys_catalog_->tablet_id());
+        req.set_dest_uuid(sys_catalog_->tablet_peer()->permanent_uuid());
+        req.set_new_leader_uuid(master.instance_id().permanent_uuid());
+
+        consensus::LeaderStepDownResponsePB resp;
+        RETURN_NOT_OK(tablet_peer->consensus()->StepDown(&req, &resp));
+        if (resp.has_error()) {
+          return StatusFromPB(resp.error().status());
+        }
+        LOG_WITH_PREFIX(INFO) << "Successfully stepped down to new master";
+        return Status::OK();
+      }
+    }
+  }
+
+  return STATUS(NotFound, "Couldn't find a master in an affinitized zone");
+}
+
 }  // namespace master
 }  // namespace yb
