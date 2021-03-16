4dc552710202aea172671ce3ac24b3ba0856dc31
gaurav061 <60691495+gaurav061@users.noreply.github.com>
2021-03-16 21:11:31 +0530

    [#6460] Platform: Patch to have a check for provides other than AWS. (#7673)
    Description: Region form value is added to pass the region details for AWS provider to validate was keys, This check is not there for any other provider, Which is causing the page crash if we try to add a provider. The extra parameter is added conditionally for the AWS provider
    
    Test scenarios: Tried adding GCP provider and observed the payload there was no region parameter in payload and there was no error.

--- managed/ui/src/actions/cloud.js
+++ managed/ui/src/actions/cloud.js
@@ -207,16 +207,18 @@ export function resetProviderList() {
   };
 }
 
-export function createProvider(type, name, config, regionFormVals) {
+export function createProvider(type, name, config, regionFormVals = null) {
   const customerUUID = localStorage.getItem('customerId');
   const provider = PROVIDER_TYPES.find((providerType) => providerType.code === type);
-  const region = Object.keys(regionFormVals.perRegionMetadata)[0] || '';
   const formValues = {
     code: provider.code,
     name: name,
     config: config,
-    region: region
   };
+  if (regionFormVals) {
+    const region = Object.keys(regionFormVals?.perRegionMetadata)[0] || '';
+    formValues['region'] = region;
+  }
   const request = axios.post(`${ROOT_URL}/customers/${customerUUID}/providers`, formValues);
   return {
     type: CREATE_PROVIDER,
