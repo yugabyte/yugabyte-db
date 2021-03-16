234b6bccf7a89d41c34a5b33b79c427bef5c2200
Jeff Harvey-Smith <jharveysmith@yugabyte.com>
2021-03-16 13:57:36 -0700

    [#161] Remove extra call to create_test_tmpdir
    Summary:
    create_test_tmpdir was getting called twice for each cxx test, once in run-test.sh and
    again in the prepare_for_running_cxx_test function in common-test-env.sh.  Since create_test_tmpdir
    unconditionally creates a new TEST_TMPDIR each time it is called, we were ending up with an extra
    empty directory under /tmp for each cxx test.
    
    Test Plan:
    Jenkins: build type: release, build platform: centos
    
    Ran ./yb_build.sh --cxx-test client_backup-txn-test --gtest_filter BackupTxnTest.Consistency
    locally while cleaning out /tmp between runs.
    
    Reviewers: devops, mbautin
    
    Reviewed By: mbautin
    
    Subscribers: mbautin, devops
    
    Differential Revision: https://phabricator.dev.yugabyte.com/D10929

--- build-support/common-test-env.sh
+++ build-support/common-test-env.sh
@@ -506,7 +506,9 @@ prepare_for_running_cxx_test() {
     test_cmd_line+=( "--gtest_filter=$test_name" )
   fi
 
-  create_test_tmpdir
+  # Ensure we have a TEST_TMPDIR defined and it exists.
+  # If it doesn't exit, we need to make a new one
+  [[ -d "${TEST_TMPDIR:-}" ]] || create_test_tmpdir
   test_log_path="$test_log_path_prefix.log"
 
   # gtest won't overwrite old junit test files, resulting in a build failure
