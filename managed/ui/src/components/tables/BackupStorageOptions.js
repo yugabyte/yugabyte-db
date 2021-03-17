fdd72244ffd342fbf4c10bb9c6aaaccaf57f10d6
nishantsharma459 <nishant.sharma@hashedin.com>
2021-03-17 16:09:41 +0530

    [#7349] Platform: Ran Prettier.

--- managed/ui/src/components/tables/BackupStorageOptions.js
+++ managed/ui/src/components/tables/BackupStorageOptions.js
@@ -4,32 +4,34 @@
 //
 // This file will return the set of storage type options.
 
-import React from "react";
-import { isNonEmptyArray, isNonEmptyObject } from "../../utils/ObjectUtils";
+import React from 'react';
+import { isNonEmptyArray, isNonEmptyObject } from '../../utils/ObjectUtils';
 
 export const BackupStorageOptions = (storageConfigs) => {
   let configTypeList = <option />;
   const optGroups =
-    storageConfigs
-    && isNonEmptyArray(storageConfigs)
-    && storageConfigs.reduce((val, indx) => {
+    storageConfigs &&
+    isNonEmptyArray(storageConfigs) &&
+    storageConfigs.reduce((val, indx) => {
       const configType = indx.name;
       val[configType]
         ? val[configType].push({
-          "configName": indx.configName,
-          "configUUID": indx.configUUID
-        })
-        : (val[configType] = [{
-          "configName": indx.configName,
-          "configUUID": indx.configUUID
-        }]);
+            configName: indx.configName,
+            configUUID: indx.configUUID
+          })
+        : (val[configType] = [
+            {
+              configName: indx.configName,
+              configUUID: indx.configUUID
+            }
+          ]);
       return val;
     }, {});
 
   if (isNonEmptyObject(optGroups)) {
     configTypeList = Object.keys(optGroups).map(function (key, idx) {
       return (
-        <optgroup label={(key + " " + "storage").toUpperCase()} key={key + idx}>
+        <optgroup label={(key + ' ' + 'storage').toUpperCase()} key={key + idx}>
           {optGroups[key]
             .sort((a, b) => /\d+(?!\.)/.exec(a) - /\d+(?!\.)/.exec(b))
             .map((item, arrIdx) => (
@@ -43,4 +45,4 @@ export const BackupStorageOptions = (storageConfigs) => {
   }
 
   return configTypeList;
-}
+};
