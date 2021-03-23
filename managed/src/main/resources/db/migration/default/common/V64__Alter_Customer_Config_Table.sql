8e2548c45869d60641b7199e96cf434a96915728
mahendra bhat <mahendra.bhat1@hashedin.com>
2021-03-23 15:36:29 +0530

    review comment fixes

--- managed/src/main/resources/db/migration/default/common/V64__Alter_Customer_Config_Table.sql
+++ managed/src/main/resources/db/migration/default/common/V64__Alter_Customer_Config_Table.sql
@@ -1,4 +1,4 @@
 -- Copyright (c) YugaByte, Inc.
 
 alter table if exists customer_config add column if not exists config_name varchar(64) null;
-update if exists customer_config set config_name = concat(name, '-Default');
+update if exists customer_config set config_name = concat(name, '-Default') where config_name is null;
