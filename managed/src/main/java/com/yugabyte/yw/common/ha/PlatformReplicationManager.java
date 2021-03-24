yugabyte/yugabyte-db

commit df1604a8b8e6bad7b50d976cb77539fbe55d4408

[#7662] Platform: Add more HA unit tests
Summary: Add some more unit tests for some HA code that was missing coverage

Test Plan: sbt test. Also ran sbt jacoco to examine the code coverage of HA related unit tests

Reviewers: sanketh

Reviewed By: sanketh

Subscribers: jenkins-bot

Differential Revision: https://phabricator.dev.yugabyte.com/D10933

managed/src/main/java/com/yugabyte/yw/common/ha/PlatformReplicationManager.java
@@ -282,13 +282,10 @@ public class PlatformReplicationManager {
         config.getLocal().ifPresent(localInstance -> {
           localInstance.updateLastBackup();
 
-          // Send the platform backup to the remote instances.
-          List<PlatformInstance> instancesToUpdate = remoteInstances.stream()
+          // Send the platform backup to remote instances that successfully received the backup.
+          remoteInstances.stream()
             .filter(this::sendBackup)
-            .collect(Collectors.toList());
-
-          // Sync cluster data to remote address that successfully received the backup.
-          instancesToUpdate.forEach(replicationHelper::syncToRemoteInstance);
+            .forEach(replicationHelper::syncToRemoteInstance);
         });
       } catch (Exception e) {
         LOG.error("Error running sync for HA config {}", config.getUUID(), e);
@@ -465,6 +462,7 @@ public class PlatformReplicationManager {
     if (response.code != 0) {
       LOG.error("Restore failed: " + response.message);
     }
+
     return response.code == 0;
   }
 }

managed/src/test/java/com/yugabyte/yw/common/FakeDBApplication.java
@@ -46,6 +46,7 @@ public class FakeDBApplication extends WithApplication {
   public YamlWrapper mockYamlWrapper = mock(YamlWrapper.class);
   public QueryAlerts mockQueryAlerts = mock(QueryAlerts.class);
   public Executors mockExecutors = mock(Executors.class);
+  public ShellProcessHandler mockShellProcessHandler = mock(ShellProcessHandler.class);
 
   @Override
   protected Application provideApplication() {
@@ -80,6 +81,7 @@ public class FakeDBApplication extends WithApplication {
       .overrides(bind(QueryAlerts.class).toInstance(mockQueryAlerts))
       .overrides(bind(CloudAPI.Factory.class).toInstance(mockCloudAPIFactory))
       .overrides(bind(Scheduler.class).toInstance(mock(Scheduler.class)))
+      .overrides(bind(ShellProcessHandler.class).toInstance(mockShellProcessHandler))
       .build();
   }
 

managed/src/test/java/com/yugabyte/yw/controllers/InternalHAControllerTest.java
@@ -397,4 +397,62 @@ public class InternalHAControllerTest extends FakeDBApplication {
       FakeApiHelper.doRequestWithHATokenAndBody("PUT", uri, clusterKey, body);
     assertBadRequest(syncResult, "Cannot import instances from stale leader");
   }
+
+  @Test
+  public void testDemoteLocalInstanceSuccess() {
+    JsonNode haConfigJson = createHAConfig();
+    HighAvailabilityConfig config = Json.fromJson(haConfigJson, HighAvailabilityConfig.class);
+    UUID configUUID = config.getUUID();
+    String clusterKey = haConfigJson.get("cluster_key").asText();
+    createPlatformInstance(configUUID, true, true);
+    haConfigJson = getHAConfig();
+    config = Json.fromJson(haConfigJson, HighAvailabilityConfig.class);
+    assertTrue(config.isLocalLeader());
+    String uri = "/api/settings/ha/internal/config/demote/" + new Date().getTime();
+    JsonNode body = Json.newObject().put("leader_address", "http://1.2.3.4");
+    Result demoteResult = FakeApiHelper.doRequestWithHATokenAndBody("PUT", uri, clusterKey, body);
+    assertOk(demoteResult);
+    haConfigJson = getHAConfig();
+    config = Json.fromJson(haConfigJson, HighAvailabilityConfig.class);
+    assertFalse(config.isLocalLeader());
+  }
+
+  @Test
+  public void testDemoteLocalInstanceStaleFailover() {
+    JsonNode haConfigJson = createHAConfig();
+    HighAvailabilityConfig config = Json.fromJson(haConfigJson, HighAvailabilityConfig.class);
+    UUID configUUID = config.getUUID();
+    String clusterKey = haConfigJson.get("cluster_key").asText();
+    Date staleFailover = new Date();
+    createPlatformInstance(configUUID, true, true);
+    haConfigJson = getHAConfig();
+    config = Json.fromJson(haConfigJson, HighAvailabilityConfig.class);
+    assertTrue(config.isLocalLeader());
+    String uri = "/api/settings/ha/internal/config/demote/" + staleFailover.getTime();
+    JsonNode body = Json.newObject().put("leader_address", "http://1.2.3.4");
+    Result demoteResult = FakeApiHelper.doRequestWithHATokenAndBody("PUT", uri, clusterKey, body);
+    assertBadRequest(demoteResult, "Rejecting demote request from stale leader");
+    haConfigJson = getHAConfig();
+    config = Json.fromJson(haConfigJson, HighAvailabilityConfig.class);
+    assertTrue(config.isLocalLeader());
+  }
+
+  @Test
+  public void testDemoteLocalInstanceSuccessAlreadyFollower() {
+    JsonNode haConfigJson = createHAConfig();
+    HighAvailabilityConfig config = Json.fromJson(haConfigJson, HighAvailabilityConfig.class);
+    UUID configUUID = config.getUUID();
+    String clusterKey = haConfigJson.get("cluster_key").asText();
+    createPlatformInstance(configUUID, true, false);
+    haConfigJson = getHAConfig();
+    config = Json.fromJson(haConfigJson, HighAvailabilityConfig.class);
+    assertFalse(config.isLocalLeader());
+    String uri = "/api/settings/ha/internal/config/demote/" + new Date().getTime();
+    JsonNode body = Json.newObject().put("leader_address", "http://1.2.3.4");
+    Result demoteResult = FakeApiHelper.doRequestWithHATokenAndBody("PUT", uri, clusterKey, body);
+    assertOk(demoteResult);
+    haConfigJson = getHAConfig();
+    config = Json.fromJson(haConfigJson, HighAvailabilityConfig.class);
+    assertFalse(config.isLocalLeader());
+  }
 }

managed/src/test/java/com/yugabyte/yw/controllers/PlatformInstanceControllerTest.java
@@ -14,7 +14,10 @@ import com.fasterxml.jackson.databind.JsonNode;
 import com.yugabyte.yw.common.FakeApiHelper;
 import com.yugabyte.yw.common.FakeDBApplication;
 import com.yugabyte.yw.common.ModelFactory;
+import com.yugabyte.yw.common.ShellResponse;
 import com.yugabyte.yw.models.Customer;
+import com.yugabyte.yw.models.HighAvailabilityConfig;
+import com.yugabyte.yw.models.PlatformInstance;
 import com.yugabyte.yw.models.Users;
 import org.junit.Before;
 import org.junit.Test;
@@ -26,6 +29,8 @@ import java.util.UUID;
 import static com.yugabyte.yw.common.AssertHelper.*;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
+import static org.mockito.ArgumentMatchers.*;
+import static org.mockito.Mockito.when;
 import static play.test.Helpers.contentAsString;
 
 public class PlatformInstanceControllerTest extends FakeDBApplication {
@@ -199,5 +204,55 @@ public class PlatformInstanceControllerTest extends FakeDBApplication {
     assertBadRequest(createResult, "");
     JsonNode node = Json.parse(contentAsString(createResult));
     assertErrorNodeValue(node, "address", "Invalid URL provided");
+  }
+
+  @Test
+  public void testPromoteInstanceBackupFileNonexistent() {
+    when(mockShellProcessHandler.run(anyList(), anyMap(), anyBoolean()))
+      .thenReturn(new ShellResponse());
+    JsonNode haConfigJson = createHAConfig();
+    HighAvailabilityConfig config = Json.fromJson(haConfigJson, HighAvailabilityConfig.class);
+    UUID configUUID = config.getUUID();
+    Result createResult = createPlatformInstance(configUUID, "http://abc.com", true, false);
+    assertOk(createResult);
+    JsonNode instanceJson = Json.parse(contentAsString(createResult));
+    PlatformInstance instance = Json.fromJson(instanceJson, PlatformInstance.class);
+    UUID instanceUUID = instance.getUUID();
+    PlatformInstance remoteLeader = PlatformInstance.create(config, "http://def.com", true, false);
+    remoteLeader.save();
+    String uri = String.format(
+      "/api/settings/ha/config/%s/instance/%s/promote",
+      configUUID.toString(),
+      instanceUUID.toString()
+    );
+    String authToken = user.createAuthToken();
+    JsonNode body = Json.newObject().put("backup_file", "/foo/bar");
+    Result promoteResult =  FakeApiHelper
+      .doRequestWithAuthTokenAndBody("POST", uri, authToken, body);
+    assertBadRequest(promoteResult, "Could not find backup file");
+  }
+
+  @Test
+  public void testPromoteInstanceNoLeader() {
+    when(mockShellProcessHandler.run(anyList(), anyMap(), anyBoolean()))
+      .thenReturn(new ShellResponse());
+    JsonNode haConfigJson = createHAConfig();
+    HighAvailabilityConfig config = Json.fromJson(haConfigJson, HighAvailabilityConfig.class);
+    UUID configUUID = config.getUUID();
+    Result createResult = createPlatformInstance(configUUID, "http://abc.com", true, false);
+    assertOk(createResult);
+    JsonNode instanceJson = Json.parse(contentAsString(createResult));
+    PlatformInstance instance = Json.fromJson(instanceJson, PlatformInstance.class);
+    UUID instanceUUID = instance.getUUID();
+    String uri = String.format(
+      "/api/settings/ha/config/%s/instance/%s/promote",
+      configUUID.toString(),
+      instanceUUID.toString()
+    );
+    String authToken = user.createAuthToken();
+    JsonNode body = Json.newObject().put("backup_file", "/foo/bar");
+    Result promoteResult =  FakeApiHelper
+      .doRequestWithAuthTokenAndBody("POST", uri, authToken, body);
+    assertBadRequest(promoteResult, "Could not find leader instance");
   }
 }

commit 957a2deaa584a4461754c4e8582e01df9271d558

[#7800] Platform: Fix regression with sync timestamps replicated to standby platforms
Summary: Small bug was re-introduced accidentally in commit: df1604a8b8e6bad7b50d976cb77539fbe55d4408

Test Plan: Create HA cluster and ensure backup times are in sync.

Reviewers: wesley

Reviewed By: wesley

Differential Revision: https://phabricator.dev.yugabyte.com/D11044

managed/src/main/java/com/yugabyte/yw/common/ha/PlatformReplicationManager.java
@@ -282,10 +282,13 @@ public class PlatformReplicationManager {
         config.getLocal().ifPresent(localInstance -> {
           localInstance.updateLastBackup();
 
-          // Send the platform backup to remote instances that successfully received the backup.
-          remoteInstances.stream()
+          // Send the platform backup to all followers.
+          Set<PlatformInstance> instancesToSync = remoteInstances.stream()
             .filter(this::sendBackup)
-            .forEach(replicationHelper::syncToRemoteInstance);
+            .collect(Collectors.toSet());
+
+          // Sync the HA cluster state to all followers that successfully received a backup.
+          instancesToSync.forEach(replicationHelper::syncToRemoteInstance);
         });
       } catch (Exception e) {
         LOG.error("Error running sync for HA config {}", config.getUUID(), e);