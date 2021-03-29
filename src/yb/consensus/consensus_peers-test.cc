a4eb8aed68450b35e3491a4d758ca84d6b762d2c
Amitanand Aiyer <amitanandaiyer@users.noreply.github.com>
2021-03-29 14:25:52 -0700

    [#7805] Share Histograms across tablets belonging to a table instead of having Histograms (in TabletMetrics and other objects) separately for each tablet.
    Summary:
    Reduce memory footprint of the per tablet histograms.
     - Define Histogram Metrics at a table level MetricEntity, instead of having one each at the Tablet level.
       We export them to prometheus-metrics as a roll up at table level, so no loss of precision here.
     - Counters/Gauges are still going to be at the tablet level.
       Counters using incr/decr could probably be moved up as well. However guages using set_value will need to be at the tablet level.
       Metrics such as `is_raft_leader` do not make sense at the table level.
    
    Test Plan:
    Jenkins
    
    spin up local yb-ctl
    compare output from 127.0.0.1:9000/prometheus-metrics with and without the changes
    
    {F15979}
    
    {F15980}
    
    Space usage in-alloc bytes
    
    Before
    
    {F15984}
    
    vs after
    
    {F15983}
    
    Reviewers: bogdan, kannan, rahuldesirazu
    
    Reviewed By: rahuldesirazu
    
    Subscribers: ybase
    
    Differential Revision: https://phabricator.dev.yugabyte.com/D11089

--- src/yb/consensus/consensus_peers-test.cc
+++ src/yb/consensus/consensus_peers-test.cc
@@ -96,7 +96,8 @@ class ConsensusPeersTest : public YBTest {
                        fs_manager_->uuid(),
                        schema_,
                        0, // schema_version
-                       NULL,
+                       nullptr, // table_metric_entity
+                       nullptr, // tablet_metric_entity
                        log_thread_pool_.get(),
                        log_thread_pool_.get(),
                        std::numeric_limits<int64_t>::max(), // cdc_min_replicated_index
