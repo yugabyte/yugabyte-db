82edd004f118be86f42f0d7b8f18262b17c30dd9
Deepthi Srinivasan <deeps1991@gmail.com>
2021-03-30 13:18:20 -0700

    [#7863][YSQL] Single row optimization for Updates is not being used for partitioned tables even when only one partition is being updated.
    Summary:
    Before this patch, we use the single row optimization for update
    operations only if we are operating on just one relation. We check this
    by executing the if-condition: root->simple_rel_array_size == 2,
    where root->simple_rel_array is created during the planning phase and contains
    the relations that need to be updated in the current UPDATE operation.
    
    While looking at the root->simple_rel_array_size is valid for non
    partitioned tables, it is not the appropriate check for partitioned
    tables. This is because at the time of initialization of the planner
    structure, setup_simple_rel_array will simply initialize
    root->simple_rel_array_size and root->simple_rel_array assuming that
    all partitions of the partitioned table will be updated. At this point
    root->simple_rel_array will contain only NULL values.
    
    Later, as the planner elucidates the possible plans to execute the
     query, it will figure out that the update is to a partitioned table. It
    will iterate through the children, and add non-nul values to root->simple_rel_array
    (see inheritance_planner(), grouping_planner()) corresponding to the
    child ONLY if the child will not be eliminated due to constraint
    exclusion or partition pruning.
    
    Thus, our check for whether single row optimization can be applied to an
    UPDATE or not should not merely check for simple_rel_array_size, rather
    it should check for the number of non null entries in simple_rel_array.
    
    Test Plan: ybd --scb --sj --java-test org.yb.pgsql.TestPgRegressPartitions
    
    Reviewers: mihnea, dmitry
    
    Reviewed By: dmitry
    
    Subscribers: dmitry, yql
    
    Differential Revision: https://phabricator.dev.yugabyte.com/D11096

--- src/postgres/src/backend/optimizer/plan/createplan.c
+++ src/postgres/src/backend/optimizer/plan/createplan.c
@@ -2518,7 +2518,7 @@ yb_single_row_update_or_delete_path(PlannerInfo *root,
 									bool *no_row_trigger,
 									List **no_update_index_list)
 {
-	RelOptInfo *relInfo;
+	RelOptInfo *relInfo = NULL;
 	Oid relid;
 	Relation relation;
 	Path *subpath;
@@ -2551,10 +2551,47 @@ yb_single_row_update_or_delete_path(PlannerInfo *root,
 
 	/*
 	 * Multi-relation implies multi-shard.
-	 * Note that simple_rel_array is one-based, so size of two implies one entry.
 	 */
-	if (list_length(path->resultRelations) != 1 || root->simple_rel_array_size != 2)
+	if (list_length(path->resultRelations) != 1)
 		return false;
+
+	/*
+	 * Check that the number of relations being updated is 1.
+	 * Note that simple_rel_array is one-based.
+	 */
+	for (int rti = 1; rti < root->simple_rel_array_size; ++rti)
+	{
+			RelOptInfo *rel = root->simple_rel_array[rti];
+			if (rel != NULL)
+			{
+					if (relInfo == NULL)
+					{
+						/* Found the first non null RelOptInfo.
+						 * Set relInfo and relid.
+						 */
+						relInfo = rel;
+						relid = root->simple_rte_array[rti]->relid;
+					}
+					else
+					{
+							/*
+							 * There are multiple entries in simple_rel_array.
+							 * This implies that multiple relations are being
+							 * affected. Single row optimization is not
+							 * applicable here.
+							 */
+							return false;
+					}
+			}
+	}
+
+	/*
+	 * One relation must be updated.
+	 */
+	if (relInfo == NULL)
+	{
+			return false;
+	}
 
 	/* ON CONFLICT clause is not supported here yet. */
 	if (path->onconflict)
@@ -2567,10 +2604,6 @@ yb_single_row_update_or_delete_path(PlannerInfo *root,
 	/* Only allow at most one returning list. */
 	if (list_length(path->returningLists) > 1)
 		return false;
-
-	/* Extract the relation. Must be first entry since we only have one relation (one-based). */
-	relInfo = root->simple_rel_array[1];
-	relid = root->simple_rte_array[1]->relid;
 
 	/* Verify we're a YB relation. */
 	if (!IsYBRelationById(relid))
