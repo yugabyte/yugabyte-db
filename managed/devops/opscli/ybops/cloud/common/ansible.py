2cf66ea38c6c36b70fa9f726e7cacbc161ae76c4
Wesley Wang <2791412+WesleyW@users.noreply.github.com>
2021-04-13 14:59:14 -0700

    [Backport 2.5.3.2][#7597] Platform: Add python modules to PYTHONPATH in non-replicated installations
    Summary:
    For non-replicated installations, `bin/install_python_requirements.sh --create_package` will
    download all the python scripts that are added to PATH (i.e. ansible) first, and then download
    the libraries needed.
    
    To work around the use of namespace packages in the libaries (like for `google-api-core`), we
    add a custom startup script called `sitecustomize.py` for python (see https://docs.python.org/3/library/site.html)
    that adds the necessary packages to the path.
    
    This squashes D10985, D11010, D11025, D11034, D11050, D11093
    
    Test Plan: Ran itest pointing to non-replicated setup.
    
    Reviewers: daniel, rtsisyk
    
    Reviewed By: rtsisyk
    
    Subscribers: yugaware, jenkins-bot
    
    Differential Revision: https://phabricator.dev.yugabyte.com/D11233

--- managed/devops/opscli/ybops/cloud/common/ansible.py
+++ managed/devops/opscli/ybops/cloud/common/ansible.py
@@ -118,12 +118,14 @@ class AnsibleProcess(object):
 
         # Setup the full list of extra-vars needed for ansible plays.
         process_args.extend(["--extra-vars", json.dumps(playbook_args)])
-        env = {'PROFILE_TASKS_TASK_OUTPUT_LIMIT': '30'}
+        env = os.environ.copy()
+        if env.get('APPLICATION_CONSOLE_LOG_LEVEL') != 'INFO':
+            env['PROFILE_TASKS_TASK_OUTPUT_LIMIT'] = '30'
         logging.info("[app] Running ansible playbook {} against target {}".format(
                         filename, inventory_target))
         logging.info("Running ansible command {}".format(json.dumps(process_args,
                                                                     separators=(' ', ' '))))
-        p = subprocess.Popen(process_args, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        p = subprocess.Popen(process_args, stdout=subprocess.PIPE, stderr=subprocess.PIPE, env=env)
         stdout, stderr = p.communicate()
         if print_output:
             print(stdout)
