f9ca9cb42a583bdf5b58a8462ee567c76fd5aed6
mahendra bhat <mahendra.bhat1@hashedin.com>
2021-04-30 00:57:55 +0530

    review comment fixes and test cases

--- managed/src/test/java/com/yugabyte/yw/controllers/CustomerTaskControllerTest.java
+++ managed/src/test/java/com/yugabyte/yw/controllers/CustomerTaskControllerTest.java
@@ -5,7 +5,6 @@ package com.yugabyte.yw.controllers;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 import com.google.common.collect.ImmutableList;
-import com.typesafe.config.Config;
 import com.yugabyte.yw.commissioner.Commissioner;
 import com.yugabyte.yw.commissioner.UserTaskDetails;
 import com.yugabyte.yw.common.FakeApiHelper;
