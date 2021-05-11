156c5490d4bb67914d01554a6f96d95df0b617b1
jitendra-12113 <57384873+jitendra-12113@users.noreply.github.com>
2021-05-11 15:58:10 +0530

    [#8333] [Platform] [API] Release controller error handling. (#8231)
    * Description:
    Use the new global exception handling in the Release controller.
    
    Test Plan:
    
    Its mostly refactor. Unit tests are updated.
    Run server and test basic operations.
    
    * Resolved merge conflicts and updated the test cases
    
    Co-authored-by: jitendra-12113 <jitedra.kumar@hashedin.com>

--- managed/src/main/java/com/yugabyte/yw/controllers/ReleaseController.java
+++ managed/src/main/java/com/yugabyte/yw/controllers/ReleaseController.java
@@ -6,6 +6,8 @@ import com.fasterxml.jackson.databind.node.ObjectNode;
 import com.google.inject.Inject;
 import com.yugabyte.yw.common.ApiResponse;
 import com.yugabyte.yw.common.ReleaseManager;
+import com.yugabyte.yw.common.YWServiceException;
+import com.yugabyte.yw.common.ValidatingFormFactory;
 import com.yugabyte.yw.forms.ReleaseFormData;
 import com.yugabyte.yw.forms.YWSuccess;
 import com.yugabyte.yw.models.Audit;
@@ -28,28 +30,17 @@ public class ReleaseController extends AuthenticatedController {
   ReleaseManager releaseManager;
 
   @Inject
-  FormFactory formFactory;
+  ValidatingFormFactory formFactory;
 
   public Result create(UUID customerUUID) {
-    Customer customer = Customer.get(customerUUID);
+    Customer customer = Customer.getOrBadRequest(customerUUID);
 
-    if (customer == null) {
-      return ApiResponse.error(BAD_REQUEST, "Invalid Customer UUID: " + customerUUID);
-    }
-
-    Form<ReleaseFormData> formData = formFactory.form(ReleaseFormData.class).bindFromRequest();
-
-    ObjectNode responseJson = Json.newObject();
-    if (formData.hasErrors()) {
-      responseJson.set("error", formData.errorsAsJson());
-      return badRequest(responseJson);
-    }
+    Form<ReleaseFormData> formData = formFactory.getFormDataOrBadRequest(ReleaseFormData.class);
     ReleaseFormData releaseFormData = formData.get();
-
     try {
       releaseManager.addRelease(releaseFormData.version);
     } catch (RuntimeException re) {
-      return ApiResponse.error(INTERNAL_SERVER_ERROR, re.getMessage());
+      throw new YWServiceException(INTERNAL_SERVER_ERROR, re.getMessage());
     }
     auditService().createAuditEntry(ctx(), request(), Json.toJson(formData.data()));
     return YWSuccess.asResult();
@@ -57,65 +48,46 @@ public class ReleaseController extends AuthenticatedController {
 
 
   public Result list(UUID customerUUID, Boolean includeMetadata) {
-    Customer customer = Customer.get(customerUUID);
-
-    if (customer == null) {
-      return ApiResponse.error(BAD_REQUEST, "Invalid Customer UUID: " + customerUUID);
-    }
-    try {
-      Map<String, Object> releases = releaseManager.getReleaseMetadata();
-      // Filter out any deleted releases
-      Map<String, Object> filtered = releases.entrySet().stream().filter(
-          f -> !Json.toJson(f.getValue()).get("state").asText().equals("DELETED")
-      ).collect(Collectors.toMap(p -> p.getKey(), p -> p.getValue()));
-      if (includeMetadata) {
-        return ApiResponse.success(filtered);
-      } else {
-        return ApiResponse.success(filtered.keySet());
-      }
-    } catch (RuntimeException re) {
-      return ApiResponse.error(INTERNAL_SERVER_ERROR, re.getMessage());
+    Customer customer = Customer.getOrBadRequest(customerUUID);
+    Map<String, Object> releases = releaseManager.getReleaseMetadata();
+    // Filter out any deleted releases
+    Map<String, Object> filtered = releases.entrySet().stream().filter(
+      f -> !Json.toJson(f.getValue()).get("state").asText().equals("DELETED")
+    ).collect(Collectors.toMap(p -> p.getKey(), p -> p.getValue()));
+    if (includeMetadata) {
+      return ApiResponse.success(filtered);
+    } else {
+      return ApiResponse.success(filtered.keySet());
     }
   }
 
   public Result update(UUID customerUUID, String version) {
-    Customer customer = Customer.get(customerUUID);
-
-    if (customer == null) {
-      return ApiResponse.error(BAD_REQUEST, "Invalid Customer UUID: " + customerUUID);
-    }
+    Customer customer = Customer.getOrBadRequest(customerUUID);
 
     ObjectNode formData;
-    try {
-      ReleaseManager.ReleaseMetadata m = releaseManager.getReleaseByVersion(version);
-      if (m == null) {
-        return ApiResponse.error(BAD_REQUEST, "Invalid Release version: " + version);
-      }
-      formData = (ObjectNode)request().body().asJson();
-      // For now we would only let the user change the state on their releases.
-      if (formData.has("state")) {
-        m.state = ReleaseManager.ReleaseState.valueOf(formData.get("state").asText());
-        releaseManager.updateReleaseMetadata(version, m);
-      } else {
-        return ApiResponse.error(BAD_REQUEST, "Missing Required param: State");
-      }
-      auditService().createAuditEntry(ctx(), request(), formData);
-      return ApiResponse.success(m);
-    } catch (RuntimeException re) {
-      return ApiResponse.error(INTERNAL_SERVER_ERROR, re.getMessage());
+    ReleaseManager.ReleaseMetadata m = releaseManager.getReleaseByVersion(version);
+    if (m == null) {
+      throw new YWServiceException(BAD_REQUEST, "Invalid Release version: " + version);
     }
+    formData = (ObjectNode)request().body().asJson();
+    // For now we would only let the user change the state on their releases.
+    if (formData.has("state")) {
+      m.state = ReleaseManager.ReleaseState.valueOf(formData.get("state").asText());
+      releaseManager.updateReleaseMetadata(version, m);
+    } else {
+      throw new YWServiceException(BAD_REQUEST, "Missing Required param: State");
+    }
+    auditService().createAuditEntry(ctx(), request(), Json.toJson(formData));
+    return ApiResponse.success(m);
   }
 
   public Result refresh(UUID customerUUID) {
-    Customer customer = Customer.get(customerUUID);
+    Customer customer = Customer.getOrBadRequest(customerUUID);
 
-    if (customer == null) {
-      return ApiResponse.error(BAD_REQUEST, "Invalid Customer UUID: " + customerUUID);
-    }
     try {
       releaseManager.importLocalReleases();
     } catch (RuntimeException re) {
-      return ApiResponse.error(INTERNAL_SERVER_ERROR, re.getMessage());
+      throw new YWServiceException(INTERNAL_SERVER_ERROR, re.getMessage());
     }
     return YWSuccess.asResult();
   }
