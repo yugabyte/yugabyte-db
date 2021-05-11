93982a1083e56d44aa935851d2fb8911eb59ff05
nishantsharma459 <nishant.sharma@hashedin.com>
2021-05-11 12:58:42 +0530

    [#8083] Platform: Removed loading condition.

--- managed/ui/src/components/universes/UniverseForm/ClusterFields.js
+++ managed/ui/src/components/universes/UniverseForm/ClusterFields.js
@@ -320,6 +320,7 @@ export default class ClusterFields extends Component {
         this.setState({
           providerType: formValues[clusterType].providerType,
           providerSelected: formValues[clusterType].provider,
+          regionList: formValues[clusterType].regionList,
           numNodes: formValues[clusterType].numNodes ? formValues[clusterType].numNodes : 3,
           replicationFactor: formValues[clusterType].replicationFactor
             ? Number(formValues[clusterType].replicationFactor)
@@ -491,10 +492,7 @@ export default class ClusterFields extends Component {
       this.props.fetchUniverseResources(nextProps.universe.universeConfigTemplate.data);
     }
     // If nodeInstanceList changes, fetch number of available nodes
-    if (
-      getPromiseState(nodeInstanceList).isSuccess() &&
-      getPromiseState(this.props.cloud.nodeInstanceList).isLoading()
-    ) {
+    if (getPromiseState(nodeInstanceList).isSuccess()) {
       let numNodesAvailable = nodeInstanceList.data.reduce((acc, val) => {
         if (!val.inUse) {
           acc++;
@@ -568,11 +566,15 @@ export default class ClusterFields extends Component {
       this.configureUniverseNodeList();
     } else if (currentProvider && currentProvider.code === 'onprem') {
       toggleDisableSubmit(false);
+      const primaryReplicaNodeLen = formValues?.primary?.regionList?.length > 0 ?
+        formValues?.primary?.numNodes : 0;
+      const asyncReplicaNodeLen = formValues?.async?.regionList?.length > 0 ?
+        formValues?.async?.numNodes : 0;
       if (
         isNonEmptyArray(this.state.regionList) &&
         currentProvider &&
         this.state.instanceTypeSelected &&
-        this.state.numNodes > this.state.maxNumNodes
+        ((primaryReplicaNodeLen + asyncReplicaNodeLen) > this.state.maxNumNodes)
       ) {
         const placementStatusObject = {
           error: {
