fda2b4a0bc4fbc66be14319a99f76fafda73699d
Mahendra Bhat <50577199+mahendranbhat@users.noreply.github.com>
2021-05-13 00:04:33 +0530

    [#7864] [Platform]Delete associated backups after deleting the universe. (#7864)
    **Description**:
    Currently, we are not deleting backups after the universe got delete. Give an option to the user whether to delete the backups or not while deleting the universe.
    
    **Test Plan**: 
    
    1. Created a universe and created some tables.
    2. Taken a couple of backups.
    3. Delete the universe by selecting delete backups in the modal. ( by default it will be turned off ) 
    4. When the destroy universe finishes, the associated backups also deleted from the respective backup config.
    5. If the option is not chosen then the backup should stay. Currently, we can not see it anywhere in the UI.
    6. Scheduled backup's delete also tested with schedule with expiry time. (NFS and AWS)
    7. Tested without deleting backup.
    8. For paused universe, delete backup option disabled.
    
    Reviewers: Jaydeep, Sergey, Sergey Shevchenko  , Shashank, Arnav, Gaurav
    Reviewed by : Sergey , Sergey Shevchenko.

--- managed/src/test/java/com/yugabyte/yw/commissioner/tasks/DestroyUniverseTest.java
+++ managed/src/test/java/com/yugabyte/yw/commissioner/tasks/DestroyUniverseTest.java
@@ -3,8 +3,11 @@
 package com.yugabyte.yw.commissioner.tasks;
 
 import static com.yugabyte.yw.common.ModelFactory.createUniverse;
+import static com.yugabyte.yw.common.ModelFactory.createBackup;
 import static org.junit.Assert.*;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 import java.util.List;
@@ -19,9 +22,12 @@ import org.mockito.junit.MockitoJUnitRunner;
 import com.google.common.collect.ImmutableList;
 import com.yugabyte.yw.commissioner.Commissioner;
 import com.yugabyte.yw.common.ApiUtils;
+import com.yugabyte.yw.common.ModelFactory;
 import com.yugabyte.yw.common.ShellResponse;
 import com.yugabyte.yw.forms.UniverseDefinitionTaskParams;
 import com.yugabyte.yw.models.Alert;
+import com.yugabyte.yw.models.Backup;
+import com.yugabyte.yw.models.CustomerConfig;
 import com.yugabyte.yw.models.AvailabilityZone;
 import com.yugabyte.yw.models.Region;
 import com.yugabyte.yw.models.TaskInfo;
@@ -32,7 +38,8 @@ import com.yugabyte.yw.models.helpers.TaskType;
 public class DestroyUniverseTest extends CommissionerBaseTest {
 
   private static final String ALERT_TEST_MESSAGE = "Test message";
-
+  private CustomerConfig s3StorageConfig;
+  
   @InjectMocks
   private Commissioner commissioner;
 
@@ -67,6 +74,7 @@ public class DestroyUniverseTest extends CommissionerBaseTest {
     taskParams.universeUUID = defaultUniverse.universeUUID;
     taskParams.customerUUID = defaultCustomer.uuid;
     taskParams.isForceDelete = Boolean.FALSE;
+    taskParams.isDeleteBackups = Boolean.FALSE;
 
     Alert.create(defaultCustomer.uuid, defaultUniverse.universeUUID, Alert.TargetType.UniverseType,
         "errorCode", "Warning", ALERT_TEST_MESSAGE);
@@ -80,6 +88,51 @@ public class DestroyUniverseTest extends CommissionerBaseTest {
     assertEquals(2, alerts.size());
     assertEquals(Alert.State.RESOLVED, alerts.get(0).state);
     assertEquals(Alert.State.RESOLVED, alerts.get(1).state);
+  }
+
+  @Test
+  public void testDestroyUniverseAndDeleteBackups() {
+    s3StorageConfig = ModelFactory.createS3StorageConfig(defaultCustomer);
+    Backup b = ModelFactory.createBackup(defaultCustomer.uuid, defaultUniverse.universeUUID,
+        s3StorageConfig.configUUID);
+    b.transitionState(Backup.BackupState.Completed);
+    ShellResponse shellResponse =  new ShellResponse();
+    shellResponse.message = "{\"success\": true}";
+    shellResponse.code = 0;
+    when(mockTableManager.deleteBackup(any())).thenReturn(shellResponse);
+    DestroyUniverse.Params taskParams = new DestroyUniverse.Params();
+    taskParams.universeUUID = defaultUniverse.universeUUID;
+    taskParams.customerUUID = defaultCustomer.uuid;
+    taskParams.isForceDelete = Boolean.FALSE;
+    taskParams.isDeleteBackups = Boolean.TRUE;
+    TaskInfo taskInfo = submitTask(taskParams, 4);
+
+    Backup backup = Backup.get(defaultCustomer.uuid, b.backupUUID);
+    verify(mockTableManager, times(1)).deleteBackup(any());
+    // Backup state should be DELETED.
+    assertEquals(Backup.BackupState.Deleted, backup.state);
+    assertFalse(Universe.checkIfUniverseExists(defaultUniverse.name));
+  }
+
+  @Test
+  public void testDestroyUniverseAndDeleteBackupsFalse() {
+    s3StorageConfig = ModelFactory.createS3StorageConfig(defaultCustomer);
+    Backup b = ModelFactory.createBackup(defaultCustomer.uuid, defaultUniverse.universeUUID,
+        s3StorageConfig.configUUID);
+    b.transitionState(Backup.BackupState.Completed);
+    DestroyUniverse.Params taskParams = new DestroyUniverse.Params();
+    taskParams.universeUUID = defaultUniverse.universeUUID;
+    taskParams.customerUUID = defaultCustomer.uuid;
+    taskParams.isForceDelete = Boolean.FALSE;
+    taskParams.isDeleteBackups = Boolean.FALSE;
+    TaskInfo taskInfo = submitTask(taskParams, 4);
+    b.setTaskUUID(taskInfo.getTaskUUID());
+
+    Backup backup = Backup.get(defaultCustomer.uuid, b.backupUUID);
+    verify(mockTableManager, times(0)).deleteBackup(any());
+    // Backup should be in COMPLETED state.
+    assertEquals(Backup.BackupState.Completed, backup.state);
+    assertFalse(Universe.checkIfUniverseExists(defaultUniverse.name));
   }
 
   private TaskInfo submitTask(DestroyUniverse.Params taskParams, int version) {
