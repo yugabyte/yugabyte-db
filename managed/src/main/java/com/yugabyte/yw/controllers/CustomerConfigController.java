1cbda7efa7711ce07c7bd6fd5639051734f11836
nishantSharma459 <75416933+nishantSharma459@users.noreply.github.com>
2021-05-12 19:09:46 +0530

    [#7706] Platform: Edit backup config credentials. (#8050)
    Description:
    Backup config security credentials can change over time. The platform should support editing the security credentials even if there are existing backups associated with a Backup Config. When security credentials expire, the Platform will fail to take new backups and also fail to delete old backups.
    
    This change should be implemented for all three storage configs:
    
    AWS: Allow editing Access Key/Secret, also toggling between IAM and AccessKey
    GCP: Allow editing GCS Credentials [Do not allow editing bucket name]
    Azure: Allow editing Azure SAS token [Do not allow editing Container URL]
    
    Test Plan:
    
    1. Hit the config button from the SideNav.
    2. Click on Backup Tab and then hit the amazon s3 tab.
    3. Create a backup config for s3.
    4. Once it got created then you'll see the Edit configuration button next to the Delete configuration button.
    5. Click on the Edit Configuration button.
    6. AWS access credentials should be editable.
    
    Do the same for the rest of the configs.
    
    Reviewers: @sshev , @jaydeepkumara, @gaurav061, @mjoshi0923, @SergeyPotachev, @Arnav15, @yb-andrew 
    
    Reviewed by: @SergeyPotachev, @sshev, @jaydeepkumara

--- managed/src/main/java/com/yugabyte/yw/controllers/CustomerConfigController.java
+++ managed/src/main/java/com/yugabyte/yw/controllers/CustomerConfigController.java
@@ -2,6 +2,7 @@
 
 package com.yugabyte.yw.controllers;
 
+import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 
 import com.google.inject.Inject;
@@ -10,14 +11,17 @@ import com.yugabyte.yw.common.ApiResponse;
 import com.yugabyte.yw.forms.YWSuccess;
 import com.yugabyte.yw.models.Audit;
 import com.yugabyte.yw.models.CustomerConfig;
+import com.yugabyte.yw.models.helpers.CommonUtils;
 import com.yugabyte.yw.models.helpers.CustomerConfigValidator;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import play.libs.Json;
 import play.mvc.Result;
 
 import java.util.UUID;
+
 
 public class CustomerConfigController extends AuthenticatedController {
   public static final Logger LOG = LoggerFactory.getLogger(CustomerConfigController.class);
@@ -61,5 +65,32 @@ public class CustomerConfigController extends AuthenticatedController {
 
   public Result list(UUID customerUUID) {
     return ApiResponse.success(CustomerConfig.getAll(customerUUID));
+  }
+
+  public Result edit(UUID customerUUID, UUID configUUID) {
+    JsonNode formData =  request().body().asJson();
+    ObjectNode errorJson = configValidator.validateFormData(formData);
+    if (errorJson.size() > 0) {
+      return ApiResponse.error(BAD_REQUEST, errorJson);
+    }
+
+    errorJson = configValidator.validateDataContent(formData);
+    if (errorJson.size() > 0) {
+      return ApiResponse.error(BAD_REQUEST, errorJson);
+    }
+    CustomerConfig customerConfig = CustomerConfig.get(customerUUID, configUUID);
+    if (customerConfig == null) {
+      return ApiResponse.error(BAD_REQUEST, "Invalid configUUID: " + configUUID);
+    }
+    CustomerConfig config = CustomerConfig.get(configUUID);
+    JsonNode data = Json.toJson(formData.get("data"));
+    if (data != null && data.get("BACKUP_LOCATION") != null) {
+      ((ObjectNode)data).put("BACKUP_LOCATION", config.data.get("BACKUP_LOCATION"));
+    }
+    JsonNode updatedData = CommonUtils.unmaskConfig(config.data, data);
+    config.data = Json.toJson(updatedData);
+    config.update();
+    Audit.createAuditEntry(ctx(), request());
+    return ApiResponse.success(config);
   }
 }
