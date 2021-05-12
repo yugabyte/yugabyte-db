8f777a538f9146daea0908defd0b379dd7744180
Deepthi Srinivasan <deeps1991@gmail.com>
2021-05-12 07:14:57 -0700

    [BACKPORT 2.6][#7530] YB-Master HTML page should show the tablespace and tablespace replication info for a table
    Summary:
    Today the YB-Master HTML page has a field showing the replication info for a table.
    Currently it only displays the correct replication info if the table's placement policy
    was either set using yb-admin or if it defaults to cluster config.
    
    This patch contains changes to display the tablespace replication info
    if the table is associated with a tablespace. It also displays the
    tablespace oid if the table is associated with a custom tablespace.
    
    Backported from: https://phabricator.dev.yugabyte.com/D11576
    
    Test Plan:
    Manual test of YB-Master UI page before and after tablespace task ran.
    Jenkins: rebase: 2.6
    
    Reviewers: rahuldesirazu
    
    Reviewed By: rahuldesirazu
    
    Subscribers: ybase, yql, bogdan
    
    Differential Revision: https://phabricator.dev.yugabyte.com/D12141

--- src/yb/master/catalog_manager.h
+++ src/yb/master/catalog_manager.h
@@ -785,6 +785,9 @@ class CatalogManager : public tserver::TabletPeerLookupIf {
       const ReplicationInfoPB& table_replication_info,
       const TablespaceId& tablespace_id);
 
+  Result<boost::optional<TablespaceId>> GetTablespaceForTable(
+      const scoped_refptr<TableInfo>& table);
+
   void ProcessTabletPathInfo(const std::string& ts_uuid, const TabletPathInfoPB& report);
 
   void CheckTableDeleted(const TableInfoPtr& table);
