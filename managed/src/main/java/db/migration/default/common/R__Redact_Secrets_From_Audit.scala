30162811f5bfe8b93f24991b62d27f0daf57b4c4
Aleksandr Malyshev <amalyshev@yugabyte.com>
2021-05-14 22:17:38 +0300

    [#8051] Additional code review comments
    Summary:
    Additional code review comments was provided during backporting diff https://phabricator.dev.yugabyte.com/D11452 to 2.6 branch here:
    https://phabricator.dev.yugabyte.com/D11509.
    Applied code review comments to master branch as well as part of this diff.
    
    Test Plan:
    Migration scripts:
    1. Before upgrade to this Platform version configure the following:
      * All provider types.
      * Replication configuration
      * All backup providers
      * Security -> Encription at Rest
      * Security -> Encription oin transit
      * Create new user
      * Custom SMTP configuration
    2. Perform Platform upgrade and start new Platform version
    3. Make sure no secrets in audit table (use 'psql -d yugaware -U postgres' on the platform machine to connect)
    
    New configuration
    1. On new version of the Platform configure the following:
      * All provider types.
      * Replication configuration
      * All backup providers
      * Security -> Encription at Rest
      * Security -> Encription oin transit
      * Create new user
      * Custom SMTP configuration
    3. Make sure no secrets in audit table (use 'psql -d yugaware -U postgres' on the platform machine to connect)
    
    Reviewers: spotachev, sb-yb
    
    Reviewed By: sb-yb
    
    Subscribers: jenkins-bot, yugaware
    
    Differential Revision: https://phabricator.dev.yugabyte.com/D11561

---
+++ managed/src/main/java/db/migration/default/common/R__Redact_Secrets_From_Audit.scala
@@ -0,0 +1,40 @@
+// Copyright (c) YugaByte, Inc.
+package db.migration.default.common
+
+import com.sangupta.murmur.Murmur3
+import com.yugabyte.yw.common.audit.AuditService
+import org.apache.commons.lang3.StringUtils
+import org.flywaydb.core.api.migration.MigrationChecksumProvider
+
+import java.sql.Connection
+import org.flywaydb.core.api.migration.jdbc.JdbcMigration
+
+import java.util.Objects
+import scala.util.hashing.MurmurHash3
+
+class R__Redact_Secrets_From_Audit extends JdbcMigration with MigrationChecksumProvider {
+
+  override def migrate(connection: Connection): Unit = {
+    val selectStmt = "SELECT id, payload FROM audit"
+    val resultSet = connection.createStatement().executeQuery(selectStmt)
+
+    while (resultSet.next()) {
+      val id = resultSet.getString("id")
+      val payloadStr = resultSet.getString("payload")
+      if (StringUtils.isNotEmpty(payloadStr)) {
+        val payload = play.libs.Json.parse(payloadStr);
+        val newPayload = AuditService.filterSecretFields(payload)
+        if (!payload.equals(newPayload)) {
+          connection.createStatement().execute(s"UPDATE audit SET payload = " +
+            s"'${play.libs.Json.stringify(newPayload)}' WHERE id = '$id'")
+        }
+      }
+    }
+  }
+
+  override def getChecksum: Integer = {
+    val codeChecksum: Int = 82918220 // Change me if you want to force migration to run
+    val secretPathsChecksum: Int = MurmurHash3.arrayHash(AuditService.SERCET_PATHS.toArray);
+    MurmurHash3.arrayHash(Array(codeChecksum, secretPathsChecksum))
+  }
+}
