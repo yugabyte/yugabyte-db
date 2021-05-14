fd166be4ff14d7f9208307d84a7b569d83333a13
Sergei Politov <spolitov@gmail.com>
2021-05-14 07:34:36 +0300

    [#7126] PITR: Handle master failover
    Summary:
    This diff adds handling of hidden tablets during master failover.
    
    We introduce a new persistent hide_state on the Table objects in the master.
    - When deleting a table covered by PITR, we leave it in RUNNING state, but change the hide_state to HIDING.
    - Once all tablets are also hidden, we transition the table's hide_state to HIDDEN
    - Once the table goes out of PITR scope, we then change it from RUNNING to DELETED
    
    This also buttons up all callsites that use GetTables, to ensure we don't display hidden tables to clients that do not care about them. This is relevant for YCQL system tables, for example. In the master UIs, we can keep displaying hidden tables as well.
    
    Test Plan: ybd --gtest_filter YbAdminSnapshotScheduleTest.UndeleteTableWithRestart
    
    Reviewers: bogdan
    
    Reviewed By: bogdan
    
    Subscribers: ybase
    
    Differential Revision: https://phabricator.dev.yugabyte.com/D11563

--- src/yb/integration-tests/external_mini_cluster.h
+++ src/yb/integration-tests/external_mini_cluster.h
@@ -362,6 +362,11 @@ class ExternalMiniCluster : public MiniClusterBase {
     return GetProxy<T>(master(i));
   }
 
+  template <class T>
+  std::shared_ptr<T> GetLeaderMasterProxy() {
+    return GetProxy<T>(GetLeaderMaster());
+  }
+
   // If the cluster is configured for a single non-distributed master, return a proxy to that
   // master. Requires that the single master is running.
   std::shared_ptr<master::MasterServiceProxy> master_proxy();
@@ -822,6 +827,8 @@ template <class T>
 std::shared_ptr<T> ExternalMiniCluster::GetProxy(ExternalDaemon* daemon) {
   return std::make_shared<T>(proxy_cache_.get(), daemon->bound_rpc_addr());
 }
+
+CHECKED_STATUS RestartAllMasters(ExternalMiniCluster* cluster);
 
 }  // namespace yb
 #endif  // YB_INTEGRATION_TESTS_EXTERNAL_MINI_CLUSTER_H_
