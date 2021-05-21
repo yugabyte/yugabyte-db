28397fcd9ac697835ab6c75dfbf540a1867acd75
Amitanand Aiyer <amitanandaiyer@users.noreply.github.com>
2021-05-21 11:57:17 -0700

    [Backport 2.4] [#8254] No leader lease needed for BackfillIndex
    Summary:
    When backfill index was designed, the master was supposed to spawn out the
    backfill tasks to the tservers. This was done by selecting the leader tablet for
    each tablet. However, it is not required that the backfill be done at the leader.
    
    Any of the followers that is past the backfilling_time can perform the backfill task;
    
    This diff allows followers (i.e. non-leaders) to also perform backfill. While the design
    is still to perform the backfill at the leader, this change is useful to ensure that backfill
    is not stuck/gets timed-out in case there is a leadership change.
    
    Original commit: https://github.com/yugabyte/yugabyte-db/commit/2f485f56bc890f5b74e575aa79f2e8888d34f070
    Original Diff Revision: https://phabricator.dev.yugabyte.com/D11688
    
    changes to external_mini_cluster.h/cc are from https://phabricator.dev.yugabyte.com/D11149, we require that for the tests here
    
    Test Plan:
    ybd --cxx-test integration-tests_cassandra_cpp_driver-test --gtest_filter CppCassandraDriverTest.TestTableBackfillWithLeaderMoves -n 10 --tp 1
    Jenkins: rebase: 2.4
    
    Reviewers: jason
    
    Reviewed By: jason
    
    Subscribers: ybase, bogdan
    
    Differential Revision: https://phabricator.dev.yugabyte.com/D12542

--- src/yb/integration-tests/external_mini_cluster.h
+++ src/yb/integration-tests/external_mini_cluster.h
@@ -240,8 +240,14 @@ class ExternalMiniCluster : public MiniClusterBase {
   // Send a ping request to the rpc port of the master. Return OK() only if it is reachable.
   CHECKED_STATUS PingMaster(ExternalMaster* master) const;
 
-  // Add a Tablet Server to the blacklist
+  // Add a Tablet Server to the blacklist.
   CHECKED_STATUS AddTServerToBlacklist(ExternalMaster* master, ExternalTabletServer* ts);
+
+  // Add a Tablet Server to the leader blacklist.
+  CHECKED_STATUS AddTServerToLeaderBlacklist(ExternalMaster* master, ExternalTabletServer* ts);
+
+  // Empty blacklist.
+  CHECKED_STATUS EmptyBlacklist(ExternalMaster* master);
 
   // Starts a new master and returns the handle of the new master object on success.  Not thread
   // safe for now. We could move this to a static function outside External Mini Cluster, but
