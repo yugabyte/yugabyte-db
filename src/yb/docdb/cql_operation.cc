717719d22bdf607e09b6b48528913393d630704f
Piyush Jain <piyushjain1996@gmail.com>
2021-06-10 18:15:59 -0700

    [#8834] Revert "[#7641] YCQL: Fix checks in index update path that determine full row removal."
    Summary: This reverts commit db98f761faf3eb97fa1e70a5c76133398fe0cca0.
    
    Test Plan: N/A
    
    Reviewers: neil, mihnea
    
    Reviewed By: mihnea
    
    Subscribers: yql
    
    Differential Revision: https://phabricator.dev.yugabyte.com/D11882

--- src/yb/docdb/cql_operation.cc
+++ src/yb/docdb/cql_operation.cc
@@ -36,7 +36,6 @@
 
 #include "yb/util/bfpg/tserver_opcodes.h"
 #include "yb/util/flag_tags.h"
-#include "yb/util/status.h"
 #include "yb/util/trace.h"
 
 #include "yb/yql/cql/ql/util/errcodes.h"
@@ -1073,15 +1072,11 @@ ValueState GetValueState(const QLTableRow& row, const ColumnId column_id) {
 
 } // namespace
 
-Result<bool> QLWriteOperation::IsRowDeleted(const QLTableRow& existing_row,
-                                            const QLTableRow& new_row) const {
+bool QLWriteOperation::IsRowDeleted(const QLTableRow& existing_row,
+                                    const QLTableRow& new_row) const {
   // Delete the whole row?
   if (request_.type() == QLWriteRequestPB::QL_STMT_DELETE && request_.column_values().empty()) {
     return true;
-  }
-
-  if (existing_row.IsEmpty()) { // If the row doesn't exist, don't check further.
-    return false;
   }
 
   // For update/delete, if there is no liveness column, the row will be deleted after the DML unless
@@ -1102,14 +1097,9 @@ Result<bool> QLWriteOperation::IsRowDeleted(const QLTableRow& existing_row,
       switch (GetValueState(existing_row, column_id)) {
         case ValueState::kNull: continue;
         case ValueState::kNotNull: return false;
-        case ValueState::kMissing:
-          // In case there exists a row with the same primary key, we definitely need to know if its
-          // columns have value NULL or not. Populate the column before executing this function.
-          RSTATUS_DCHECK(false, InternalError, "CQL proxy should mention all required columns in "
-            "QLWriteRequestPB's column_refs.");
+        case ValueState::kMissing: break;
       }
     }
-
     return true;
   }
 
@@ -1147,7 +1137,7 @@ Status QLWriteOperation::UpdateIndexes(const QLTableRow& existing_row, const QLT
     bool index_key_changed = false;
     bool index_pred_existing_row = true;
     bool index_pred_new_row = true;
-    bool is_row_deleted = VERIFY_RESULT(IsRowDeleted(existing_row, new_row));
+    bool is_row_deleted = IsRowDeleted(existing_row, new_row);
 
     if (index->where_predicate_spec()) {
       RETURN_NOT_OK(EvalCondition(
