9cd8cc298c427bda58f68fd874162722d1046061
Timur Yusupov <ttyusupov@users.noreply.github.com>
2021-06-17 23:47:47 +0300

    [#8944] docdb: Refactored usage of AssertLoggedWaitFor
    Summary:
    `AssertLoggedWaitFor` does the following:
    ```
    void AssertLoggedWaitFor(
        const std::function<Result<bool>()>& condition,
        MonoDelta timeout,
        const string& description,
        MonoDelta initial_delay,
        double delay_multiplier,
        MonoDelta max_delay) {
      LOG(INFO) << description;
      ASSERT_OK(WaitFor(condition, timeout, description, initial_delay));
      LOG(INFO) << description << " - DONE";
    }
    ```
    
    In case of failure, execution returns out of `AssertLoggedWaitFor` but continues in the call site until checking for FATALS or end of the test.
    
    This change gets rid of `AssertLoggedWaitFor` at all and uses `LoggedWaitFor` (with `ASSERT_OK` where appropriate).
    
    Test Plan: Jenkins
    
    Reviewers: mbautin
    
    Reviewed By: mbautin
    
    Subscribers: ybase, bogdan
    
    Differential Revision: https://phabricator.dev.yugabyte.com/D11969

--- src/yb/util/test_util.h
+++ src/yb/util/test_util.h
@@ -215,16 +215,6 @@ CHECKED_STATUS WaitFor(
     double delay_multiplier = test_util::kDefaultWaitDelayMultiplier,
     MonoDelta max_delay = MonoDelta::FromMilliseconds(test_util::kDefaultMaxWaitDelayMs));
 
-// DEPRECATED: use ASSERT_OK(LoggedWaitFor(...)) instead, so we don't forget to use
-// ASSERT_NO_FATALS with AssertLoggedWaitFor.
-void AssertLoggedWaitFor(
-    const std::function<Result<bool>()>& condition,
-    MonoDelta timeout,
-    const string& description,
-    MonoDelta initial_delay = MonoDelta::FromMilliseconds(test_util::kDefaultInitialWaitMs),
-    double delay_multiplier = test_util::kDefaultWaitDelayMultiplier,
-    MonoDelta max_delay = MonoDelta::FromMilliseconds(test_util::kDefaultMaxWaitDelayMs));
-
 CHECKED_STATUS LoggedWaitFor(
     const std::function<Result<bool>()>& condition,
     MonoDelta timeout,
