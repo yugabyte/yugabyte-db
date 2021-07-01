ae5955a889dfd2172cffe0f82e38471f2b48a2ec
Sanketh I <sanketh@yugabyte.com>
2021-07-01 14:01:12 -0700

    Revert "Remove Raw Json for Tables Controller"
    Summary:
    This reverts commit e5a049d8fe0e20165d1a2b711aea952783bf2b9e.
    
    Latest itest run failed with the error
    
    ```
    2021-07-01 09:34:49,283 requester.py:52 DEBUG testsimpleops-k8s-rf3 GET successful, response: [{"tableUUID":"000030ad-0000-3000-8000-000000004000","keySpace":"postgres","tableType":"PGSQL_TABLE_TYPE","tableName":"postgresqlkeyvalue","relationType":"USER_TABLE_RELATION","sizeBytes":8294220.0,"isIndexTable":false},{"tableUUID":"cad34414-dc31-4122-be18-6a244a48d982","keySpace":"ybdemo_keyspace","tableType":"YQL_TABLE_TYPE","tableName":"cassandrakeyvalue","relationType":"USER_TABLE_RELATION","sizeBytes":1.1703687E7,"isIndexTable":false}]
    2021-07-01 09:34:49,284 yw_proxy.py:516 ERROR testsimpleops-k8s-rf3 Did not find REDIS_TABLE_TYPE system_redis.redis Table
    2021-07-01 09:34:49,284 test_base.py:155 ERROR testsimpleops-k8s-rf3 ITEST FAILED testsimpleops-k8s-rf3 : RuntimeError('Table REDIS_TABLE_TYPE system_redis.redis not found.')
    ```
    
    Test Plan: Create a universe with yedis enabled, verify that the table listing returns redis table.
    
    Reviewers: wesley, daniel
    
    Reviewed By: daniel
    
    Subscribers: jenkins-bot
    
    Differential Revision: https://phabricator.dev.yugabyte.com/D12148

--- managed/src/test/java/com/yugabyte/yw/controllers/TabletServerControllerTest.java
+++ managed/src/test/java/com/yugabyte/yw/controllers/TabletServerControllerTest.java
@@ -2,6 +2,7 @@
 
 package com.yugabyte.yw.controllers;
 
+import com.fasterxml.jackson.databind.JsonNode;
 import com.google.common.net.HostAndPort;
 import com.yugabyte.yw.common.ApiUtils;
 import com.yugabyte.yw.common.FakeDBApplication;
@@ -12,23 +13,29 @@ import org.junit.Before;
 import org.junit.Test;
 import org.yb.client.ListTabletServersResponse;
 import org.yb.client.YBClient;
+import org.yb.util.ServerInfo;
 import play.libs.Json;
 import play.mvc.Result;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
 
 import static com.yugabyte.yw.common.AssertHelper.assertAuditEntry;
 import static com.yugabyte.yw.common.AssertHelper.assertYWSE;
 import static com.yugabyte.yw.common.ModelFactory.createUniverse;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 import static org.mockito.Matchers.anyString;
 import static org.mockito.Mockito.*;
+import static play.mvc.Http.Status.OK;
+import static play.test.Helpers.contentAsString;
 
 public class TabletServerControllerTest extends FakeDBApplication {
   private TabletServerController tabletController;
   private YBClient mockClient;
   private ListTabletServersResponse mockResponse;
-  private final HostAndPort testHostAndPort = HostAndPort.fromString("0.0.0.0").withDefaultPort(11);
+  private HostAndPort testHostAndPort = HostAndPort.fromString("0.0.0.0").withDefaultPort(11);
 
   @Before
   public void setUp() throws Exception {
@@ -38,8 +45,33 @@ public class TabletServerControllerTest extends FakeDBApplication {
     when(mockClient.getLeaderMasterHostAndPort()).thenReturn(testHostAndPort);
     when(mockService.getClient(any())).thenReturn(mockClient);
     when(mockService.getClient(any(), any())).thenReturn(mockClient);
-    tabletController = new TabletServerController(mockApiHelper);
+    tabletController = new TabletServerController(mockService);
     when(mockApiHelper.getRequest(anyString())).thenReturn(Json.newObject());
+    tabletController.apiHelper = mockApiHelper;
+  }
+
+  @Test
+  public void testListTabletServersSuccess() {
+    when(mockResponse.getTabletServersCount()).thenReturn(2);
+    List<ServerInfo> mockTabletSIs = new ArrayList<ServerInfo>();
+    ServerInfo si = new ServerInfo("UUID1", "abc", 1001, false, "ALIVE");
+    mockTabletSIs.add(si);
+    si = new ServerInfo("UUID2", "abc", 2001, true, "ALIVE");
+    mockTabletSIs.add(si);
+    when(mockResponse.getTabletServersList()).thenReturn(mockTabletSIs);
+    Result r = tabletController.list();
+    JsonNode json = Json.parse(contentAsString(r));
+    assertEquals(OK, r.status());
+    assertTrue(json.get("servers").isArray());
+  }
+
+  @Test
+  public void testListTabletServersFailure() {
+    when(mockResponse.getTabletServersCount()).thenThrow(new RuntimeException("Unknown Error"));
+    Result result = assertYWSE(() -> tabletController.list());
+    assertEquals(500, result.status());
+    JsonNode json = Json.parse(contentAsString(result));
+    assertEquals("Error: Unknown Error", json.get("error").asText());
   }
 
   @Test
