ab4bb31b606359d143d9922209c1ab92695e29e6
rsami <rsami@yugabyte.com>
2021-07-02 16:53:13 -0400

    [#8891] docdb - Do not remote bootstrap newly split tablets to raft peers who have accepted split op in parent tablet
    Summary:
    When a split operation is raft committed, it will be asynchronously applied at each node. Suppose some nodes A and B apply the split op before another node C. Then for each subtablet, node A and B will form raft groups which include node C. If raft messages are exchanged and the leader of a raft group realized that node C is still not aware of a new subtablet, it will initiate a remote bootstrap, not realizing that node C has not had enough time to try applying the split op. On the other hand, applying the split op locally at node C would be much more efficient, since no data needs to be streamed.
    
    To guard against remote bootstraps of newly split subtablets, we augment the remote bootstrap protocol such that StartRemoteBootstrapRequestPB includes an optional parent_tablet_id which is filled out if the tablet_id being bootstrapped is the result of a tablet split. In this case, we will reject the request at the tablet_service level if the local catalog manager is aware of the parent tablet id, since this means there's a chance for it to locally apply the raft-committed split op rather than remote bootstrapping to create the new subtablets.
    
    Test Plan: ybd --cxx-test integration-tests_tablet-split-itest --gtest_filter TabletSplitExternalMiniClusterITest.FaultedSplitNodeRejectsRemoteBootstrap
    
    Reviewers: bogdan, timur
    
    Reviewed By: timur
    
    Subscribers: ybase, bogdan
    
    Differential Revision: https://phabricator.dev.yugabyte.com/D12006

--- src/yb/integration-tests/tablet-split-itest.cc
+++ src/yb/integration-tests/tablet-split-itest.cc
@@ -27,6 +27,7 @@
 #include "yb/common/ql_value.h"
 
 #include "yb/consensus/consensus.h"
+#include "yb/consensus/consensus.pb.h"
 #include "yb/consensus/consensus_util.h"
 
 #include "yb/docdb/doc_key.h"
@@ -38,6 +39,7 @@
 #include "yb/integration-tests/redis_table_test_base.h"
 #include "yb/integration-tests/test_workload.h"
 
+#include "yb/master/catalog_manager-internal.h"
 #include "yb/master/catalog_manager.h"
 #include "yb/master/master.pb.h"
 #include "yb/master/master_error.h"
@@ -1727,15 +1729,26 @@ TEST_F(TabletSplitSingleServerITest, TabletServerSplitAlreadySplitTablet) {
 class TabletSplitExternalMiniClusterITest : public TabletSplitITestBase<ExternalMiniCluster> {
  public:
   void SetUp() override {
-    auto tserver_flags = {
-      "--cleanup_split_tablets_interval_sec=1",
-      "--tserver_heartbeat_metrics_interval_ms=100"
-    };
-    for (const auto& flag : tserver_flags) {
+    for (const auto& flag : GetTserverFlags()) {
       this->mini_cluster_opt_.extra_tserver_flags.push_back(flag);
     }
 
-    auto master_flags = {
+    for (const auto& flag : GetMasterFlags()) {
+      this->mini_cluster_opt_.extra_master_flags.push_back(flag);
+    }
+
+    TabletSplitITestBase<ExternalMiniCluster>::SetUp();
+  }
+
+  virtual std::vector<std::string> GetTserverFlags() const {
+    return {
+      "--cleanup_split_tablets_interval_sec=1",
+      "--tserver_heartbeat_metrics_interval_ms=100"
+    };
+  }
+
+  virtual std::vector<std::string> GetMasterFlags() const {
+    return {
       "--TEST_disable_split_tablet_candidate_processing=true",
       "--tablet_split_low_phase_shard_count_per_node=-1",
       "--tablet_split_high_phase_shard_count_per_node=-1",
@@ -1743,11 +1756,6 @@ class TabletSplitExternalMiniClusterITest : public TabletSplitITestBase<External
       "--tablet_split_high_phase_size_threshold_bytes=-1",
       "--tablet_force_split_threshold_bytes=-1",
     };
-    for (const auto& flag : master_flags) {
-      this->mini_cluster_opt_.extra_master_flags.push_back(flag);
-    }
-
-    TabletSplitITestBase<ExternalMiniCluster>::SetUp();
   }
 
   CHECKED_STATUS SplitTablet(const std::string& tablet_id) {
@@ -1822,6 +1830,57 @@ TEST_F(TabletSplitExternalMiniClusterITest, Simple) {
   ASSERT_OK(WaitForTablets(3));
 }
 
+TEST_F(TabletSplitExternalMiniClusterITest, FaultedSplitNodeRejectsRemoteBootstrap) {
+  constexpr int kTabletSplitInjectDelayMs = 20000 * kTimeMultiplier;
+  CreateSingleTablet();
+  ASSERT_OK(WriteRows());
+  const auto tablet_id = CHECK_RESULT(GetOnlyTabletId());
+
+  const auto leader_idx = CHECK_RESULT(cluster_->GetTabletLeaderIndex(tablet_id));
+  const auto healthy_follower_idx = (leader_idx + 1) % 3;
+  const auto faulted_follower_idx = (leader_idx + 2) % 3;
+
+  auto faulted_follower = cluster_->tablet_server(faulted_follower_idx);
+  ASSERT_OK(cluster_->SetFlag(
+      faulted_follower, "TEST_crash_before_apply_tablet_split_op", "true"));
+
+  ASSERT_OK(SplitTablet(tablet_id));
+  ASSERT_OK(cluster_->WaitForTSToCrash(faulted_follower));
+
+  ASSERT_OK(faulted_follower->Restart(ExternalMiniClusterOptions::kDefaultStartCqlProxy, {
+    std::make_pair(
+        "TEST_apply_tablet_split_inject_delay_ms", Format("$0", kTabletSplitInjectDelayMs))
+  }));
+
+  consensus::StartRemoteBootstrapRequestPB req;
+  req.set_split_parent_tablet_id(tablet_id);
+  req.set_dest_uuid(faulted_follower->uuid());
+  // We put some bogus values for these next two required fields.
+  req.set_tablet_id("::std::string &&value");
+  req.set_bootstrap_peer_uuid("abcdefg");
+  consensus::StartRemoteBootstrapResponsePB resp;
+  rpc::RpcController rpc;
+  rpc.set_timeout(kRpcTimeout);
+  auto s = cluster_->GetConsensusProxy(faulted_follower)->StartRemoteBootstrap(req, &resp, &rpc);
+  EXPECT_OK(s);
+  EXPECT_TRUE(resp.has_error());
+  EXPECT_EQ(resp.error().code(), TabletServerErrorPB::TABLET_SPLIT_PARENT_STILL_LIVE);
+
+  SleepFor(1ms * kTabletSplitInjectDelayMs);
+  EXPECT_OK(WaitForTablets(2));
+  EXPECT_OK(WaitForTablets(2, faulted_follower_idx));
+
+  // By shutting down the healthy follower and writing rows to the table, we ensure the faulted
+  // follower is eventually able to rejoin the raft group.
+  auto healthy_follower = cluster_->tablet_server(healthy_follower_idx);
+  healthy_follower->Shutdown();
+  EXPECT_OK(WaitFor([&]() -> Result<bool> {
+    return WriteRows(10).ok();
+  }, 20s * kTimeMultiplier, "Write rows after requiring faulted follower."));
+
+  ASSERT_OK(healthy_follower->Restart());
+}
+
 TEST_F(TabletSplitExternalMiniClusterITest, CrashesAfterChildLogCopy) {
   ASSERT_OK(cluster_->SetFlagOnMasters("unresponsive_ts_rpc_retry_limit", "0"));
 
@@ -1862,7 +1921,15 @@ TEST_F(TabletSplitExternalMiniClusterITest, CrashesAfterChildLogCopy) {
   CHECK_OK(non_faulted_follower->Restart());
 }
 
-TEST_F(TabletSplitExternalMiniClusterITest, TestSplitAfterFailedRemoteBootstrapCreatesDirectories) {
+class TabletSplitRemoteBootstrapEnabledTest : public TabletSplitExternalMiniClusterITest {
+  std::vector<std::string> GetTserverFlags() const override {
+    return {
+      "--TEST_disable_post_split_tablet_rbs_check=true",
+    };
+  }
+};
+
+TEST_F(TabletSplitRemoteBootstrapEnabledTest, TestSplitAfterFailedRbsCreatesDirectories) {
   const auto kApplyTabletSplitDelay = 15s * kTimeMultiplier;
 
   const auto get_tablet_meta_dirs =
