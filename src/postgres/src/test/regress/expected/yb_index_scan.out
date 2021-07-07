yugabyte/yugabyte-db

commit 8e3ba0b11c7bac28a88a8a7110caa0ac43df921f

#7047 [YSQL] Read minimal possible number of columns in case of index scan
Summary:
For handling `SELECT` queries data for target columns and for columns in `WHERE` clause is required only.

```
CREATE TABLE table (k INT PRIMARY KEY, v1 INT, v2 INT, v3 INT);
SELECT v1 FROM table WHERE v3 > 1;
```

In the following example `v1` is required as target column and `v3` is required as column in `WHERE` clause (YB has an extra step to filter fetched tuples on postgres side as not all conditions can be pushed to DocDB ) .
It is not necessary to read `k`, `v2` columns.

In case of `INDEX SCAN` for primary key same approach can be used.

```
SELECT v1 FROM table WHERE k = 1;
```
Only `v1` and `k` is required.

To perform such kind of optimization in case of index scan YB code must know set of target columns for scan and columns in `WHERE` clause. This information can be retrieved from scan plan (`Scan` structure). It is extracted from postgres node structure and provided to YB code via `yb_scan_plan` field of the `IndexScanDescData` structure.

Current change fixes fixes all the case of index scans:
- Index Only Scan
- Index Scan with secondary index
- Index Scan with table primary key

**Note:**
1. It will be good to generalize setting required set of columns in case of `INDEX SCAN` and `SEQ SCAN`.
Now for `SEQ SCAN` case set of required target is built in different place - at the `ybcGetForeignPlan` function.

2. Fetching of `ybctid` in case of `Index Scan with primary index` and fetching of `ybbasectid` in case of `Index Only Scan` potentially can be omitted. But some extra work is required. This optimization is not implemented in context of this diff.

Test Plan:
New unit test has been added

```
./yb_build.sh --java-test 'org.yb.pgsql.TestPgColumnReadEfficiency'
```

Reviewers: rskannan, mihnea, alex, tnayak

Reviewed By: mihnea, alex, tnayak

Subscribers: rsami, yql

Differential Revision: https://phabricator.dev.yugabyte.com/D10601

src/postgres/src/backend/access/ybc/ybcam.c
@@ -40,6 +40,7 @@
 #include "miscadmin.h"
 #include "nodes/relation.h"
 #include "optimizer/cost.h"
+#include "optimizer/var.h"
 #include "utils/datum.h"
 #include "utils/rel.h"
 #include "utils/lsyscache.h"
@@ -262,10 +263,7 @@ static HeapTuple ybcFetchNextHeapTuple(YbScanDesc ybScan, bool is_forward_scan)
 		{
 			tuple->t_ybctid = PointerGetDatum(syscols.ybctid);
 		}
-		if (ybScan->tableOid != InvalidOid)
-		{
-			tuple->t_tableOid = ybScan->tableOid;
-		}
+		tuple->t_tableOid = RelationGetRelid(ybScan->relation);
 	}
 	pfree(values);
 	pfree(nulls);
@@ -369,9 +367,10 @@ static IndexTuple ybcFetchNextIndexTuple(YbScanDesc ybScan, Relation index, bool
  *    - For this scan, YugaByte ALWAYS return index-tuple, which is expected by Postgres layer.
  */
 static void
-ybcSetupScanPlan(Relation relation, Relation index, bool xs_want_itup,
-				 YbScanDesc ybScan, YbScanPlan scan_plan)
+ybcSetupScanPlan(bool xs_want_itup, YbScanDesc ybScan, YbScanPlan scan_plan)
 {
+	Relation relation = ybScan->relation;
+	Relation index = ybScan->index;
 	int i;
 	memset(scan_plan, 0, sizeof(*scan_plan));
 
@@ -385,7 +384,6 @@ ybcSetupScanPlan(Relation relation, Relation index, bool xs_want_itup,
 	 * NOTE: Primary index is a special case as there isn't a primary index
 	 * table in YugaByte.
 	 */
-	ybScan->index = index;
 
 	ybScan->prepare_params.querying_colocated_table =
 		IsSystemRelation(relation);
@@ -617,11 +615,10 @@ static int int_compar_cb(const void *v1, const void *v2)
 }
 
 /* Use the scan-descriptor and scan-plan to setup scan key for filtering */
-static void	ybcSetupScanKeys(Relation relation,
-							 Relation index,
-							 YbScanDesc ybScan,
-							 YbScanPlan scan_plan)
+static void
+ybcSetupScanKeys(YbScanDesc ybScan, YbScanPlan scan_plan)
 {
+	Relation relation = ybScan->relation;
 	/*
 	 * Find the scan keys that are the primary key.
 	 */
@@ -654,10 +651,10 @@ static void	ybcSetupScanKeys(Relation relation,
 }
 
 /* Use the scan-descriptor and scan-plan to setup binds for the queryplan */
-static void ybcBindScanKeys(Relation relation,
-							Relation index,
-							YbScanDesc ybScan,
-							YbScanPlan scan_plan) {
+static void
+ybcBindScanKeys(YbScanDesc ybScan, YbScanPlan scan_plan) {
+	Relation relation = ybScan->relation;
+	Relation index = ybScan->index;
 	Oid		dboid    = YBCGetDatabaseOid(relation);
 	Oid		relid    = RelationGetRelid(relation);
 
@@ -930,24 +927,100 @@ static void ybcBindScanKeys(Relation relation,
 	}
 }
 
-/* Setup the targets */
-static void ybcSetupTargets(Relation relation,
-														Relation index,
-														YbScanDesc ybScan,
-														YbScanPlan scan_plan) {
-	if (scan_plan->target_relation->rd_rel->relhasoids)
-		ybcAddTargetColumn(ybScan, ObjectIdAttributeNumber);
+/*
+ * YbColumnFilter struct stores list of target columns required by scan plan.
+ * All other non system columns may be excluded from reading from DocDB for optimization.
+ * Required columns are:
+ * - all bound key columns
+ * - all query targets columns (from index for Index Only Scan case and from table otherwise)
+ * - all table columns required for filtering resulting rows on postgres' side
+ *
+ * Example:
+ * SELECT <target columns from index or table> FROM t
+ *     WHERE <bound key columns> + <table filtering columns>
+ */
+typedef struct YbColumnFilter {
+	YbScanDesc ybScan;
+	int min_attr;
+	Bitmapset *required_attrs;
+} YbColumnFilter;
 
+static void
+ybcInitColumnFilter(YbColumnFilter *filter, YbScanDesc ybScan, Scan *pg_scan_plan) {
+	const int min_attr = YBGetFirstLowInvalidAttributeNumber(
+		ybScan->index ? ybScan->index : ybScan->relation);
+
+	filter->required_attrs = NULL;
+	filter->ybScan = ybScan;
+	filter->min_attr = min_attr;
+	if (!pg_scan_plan)
+		return;
+
+	Bitmapset *items = NULL;
+	/* Collect bound key attributes */
+	AttrNumber *sk_attno = ybScan->target_key_attnums;
+	for (AttrNumber *sk_attno_end = sk_attno + ybScan->nkeys; sk_attno != sk_attno_end; ++sk_attno)
+		items = bms_add_member(items, *sk_attno - min_attr + 1);
+
+	ListCell *lc;
+	Index target_relid = INDEX_VAR;
+	if (!ybScan->prepare_params.index_only_scan)
+	{
+		/* Collect table filtering attributes */
+		foreach(lc, pg_scan_plan->plan.qual)
+			pull_varattnos_min_attr((Node *) lfirst(lc), pg_scan_plan->scanrelid, &items, min_attr);
+		target_relid = pg_scan_plan->scanrelid;
+	}
+
+	/* Collect target attributes */
+	foreach(lc, pg_scan_plan->plan.targetlist)
+	{
+		TargetEntry *tle = (TargetEntry *) lfirst(lc);
+		pull_varattnos_min_attr((Node *) tle->expr, target_relid, &items, min_attr);
+	}
+
+	/* In case InvalidAttrNumber is set whole row columns are required */
+	if (bms_is_member(InvalidAttrNumber - min_attr + 1, items))
+		bms_free(items);
+	else
+		filter->required_attrs = items;
+}
+
+static void
+ybcResetColumnFilter(YbColumnFilter *filter) {
+	bms_free(filter->required_attrs);
+	filter->required_attrs = NULL;
+	filter->ybScan = NULL;
+}
+
+static void
+ybcAddTargetColumnIfRequired(YbColumnFilter *filter, AttrNumber attnum) {
+	if (!filter->required_attrs ||
+	    bms_is_member(attnum - filter->min_attr + 1, filter->required_attrs))
+		ybcAddTargetColumn(filter->ybScan, attnum);
+}
+
+/* Setup the targets */
+static void
+ybcSetupTargets(YbScanDesc ybScan, YbScanPlan scan_plan, Scan *pg_scan_plan) {
+	Relation index = ybScan->index;
+
+	YbColumnFilter filter;
+	ybcInitColumnFilter(&filter, ybScan, pg_scan_plan);
 	if (ybScan->prepare_params.index_only_scan && index->rd_index->indisprimary)
 		/*
 		 * Special case: For Primary-Key-ONLY-Scan, we select ONLY the primary key from the target
 		 * table instead of the whole target table.
 		 */
 		for (int i = 0; i < index->rd_index->indnatts; i++)
-			ybcAddTargetColumn(ybScan, index->rd_index->indkey.values[i]);
+		ybcAddTargetColumnIfRequired(&filter, index->rd_index->indkey.values[i]);
 	else
 		for (AttrNumber attnum = 1; attnum <= ybScan->target_desc->natts; attnum++)
-			ybcAddTargetColumn(ybScan, attnum);
+			ybcAddTargetColumnIfRequired(&filter, attnum);
+	ybcResetColumnFilter(&filter);
+
+	if (scan_plan->target_relation->rd_rel->relhasoids)
+		ybcAddTargetColumn(ybScan, ObjectIdAttributeNumber);
 
 	if (scan_plan->target_relation->rd_index)
 		/*
@@ -992,7 +1065,9 @@ static void ybcSetupTargets(Relation relation,
  *   identify the desired row.
  */
 YbScanDesc
-ybcBeginScan(Relation relation, Relation index, bool xs_want_itup, int nkeys, ScanKey key)
+ybcBeginScan(
+	Relation relation, Relation index, bool xs_want_itup, int nkeys, ScanKey key,
+	Scan *pg_scan_plan)
 {
 	if (nkeys > YB_MAX_SCAN_KEYS)
 		ereport(ERROR,
@@ -1005,22 +1080,20 @@ ybcBeginScan(Relation relation, Relation index, bool xs_want_itup, int nkeys, Sc
 	ybScan->key   = key;
 	ybScan->nkeys = nkeys;
 	ybScan->exec_params = NULL;
-	ybScan->tableOid = RelationGetRelid(relation);
 	ybScan->quit_scan = false;
+	ybScan->relation = relation;
+	ybScan->index = index;
 
 	/* Setup the scan plan */
 	YbScanPlanData	scan_plan;
-	ybcSetupScanPlan(relation, index, xs_want_itup, ybScan, &scan_plan);
+	ybcSetupScanPlan(xs_want_itup, ybScan, &scan_plan);
 
 	/* Setup binds for the scan-key */
-	ybcSetupScanKeys(relation, index, ybScan, &scan_plan);
-	ybcBindScanKeys(relation, index, ybScan, &scan_plan);
+	ybcSetupScanKeys(ybScan, &scan_plan);
+	ybcBindScanKeys(ybScan, &scan_plan);
 
-	/*
-	 * Set up the scan targets. If the table is indexed and only the indexed columns should be
-	 * returned, fetch just those columns. Otherwise, fetch all "real" columns.
-	 */
-	ybcSetupTargets(relation, index, ybScan, &scan_plan);
+	/* Setup the scan targets with respect to postgres scan plan (i.e. set only required targets) */
+	ybcSetupTargets(ybScan, &scan_plan, pg_scan_plan);
 
 	/*
 	 * Set the current syscatalog version (will check that we are up to date).
@@ -1249,7 +1322,9 @@ SysScanDesc ybc_systable_beginscan(Relation relation,
 		}
 	}
 
-	YbScanDesc ybScan = ybcBeginScan(relation, index, false /* xs_want_itup */, nkeys, key);
+	Scan *pg_scan_plan = NULL; /* In current context scan plan is not available */
+	YbScanDesc ybScan = ybcBeginScan(
+		relation, index, false /* xs_want_itup */, nkeys, key, pg_scan_plan);
 
 	/* Set up Postgres sys table scan description */
 	SysScanDesc scan_desc = (SysScanDesc) palloc0(sizeof(SysScanDescData));
@@ -1293,8 +1368,9 @@ HeapScanDesc ybc_heap_beginscan(Relation relation,
                                 bool temp_snap)
 {
 	/* Restart should not be prevented if operation caused by system read of system table. */
-	YbScanDesc ybScan = ybcBeginScan(relation, NULL /* index */, false /* xs_want_itup */,
-	                                 nkeys, key);
+	Scan *pg_scan_plan = NULL; /* In current context scan plan is not available */
+	YbScanDesc ybScan = ybcBeginScan(
+		relation, NULL /* index */, false /* xs_want_itup */, nkeys, key, pg_scan_plan);
 
 	/* Set up Postgres sys table scan description */
 	HeapScanDesc scan_desc = (HeapScanDesc) palloc0(sizeof(HeapScanDescData));

java/yb-pgsql/src/test/java/org/yb/pgsql/TestPgColumnReadEfficiency.java
@@ -1,0 +1,129 @@
+// Copyright (c) YugaByte, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+// in compliance with the License.  You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software distributed under the License
+// is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+// or implied.  See the License for the specific language governing permissions and limitations
+// under the License.
+//
+
+package org.yb.pgsql;
+
+import static org.yb.AssertionWrappers.assertEquals;
+import static org.yb.AssertionWrappers.assertGreaterThan;
+
+import com.google.common.collect.Streams;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.yb.util.YBTestRunnerNonTsanOnly;
+
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
+@RunWith(YBTestRunnerNonTsanOnly.class)
+public class TestPgColumnReadEfficiency extends BasePgSQLTest {
+
+  @Test
+  public void testScans() throws Exception {
+    try (Statement stmt = connection.createStatement()) {
+      stmt.execute("CREATE TABLE t(" +
+        "h INT, r INT, v1 INT, v2 INT, v3 INT, t1 text, t2 text, t3 text, t4 text, t5 text, " +
+        "PRIMARY KEY(h, r ASC))");
+      stmt.execute("CREATE INDEX ON t(v1 ASC, v2, v3, t1, t2, t3, t4, t5)");
+
+      String longTextPrefix = Streams.concat(IntStream.range(0, 100).mapToObj(i -> "long"),
+                                             Stream.of("_prefix")).collect(Collectors.joining());
+      stmt.execute(String.format(
+        "INSERT INTO t SELECT 1, s, 1, s, s, '%1$s_1', '%1$s_2', '%1$s_3', '%1$s_4', '%1$s_5'" +
+          "FROM generate_series(1, 30000) AS s",
+        longTextPrefix));
+      stmt.execute("SELECT COUNT(*) FROM t WHERE h = 1");
+      // The following functions compare median execution time of 'fat' and 'thin' queries.
+      // Where 'fat' queries contain multiple heavy text columns and 'thin' contain single
+      // lightweight int column.
+      // Median execution time in milliseconds on local PC for 'fat' / 'thin' queries:
+      //                                  debug          release
+      // - checkIndexScan            565.1 / 252.9    414.5 / 201.8
+      // - checkForeignScan          532.8 / 249.5    395.8 / 187.7
+      // - checkIndexOnlyScan        928.8 / 843.8    524.1 / 468.7
+      // - checkSecondaryIndexScan  1034.6 / 879.1    731.6 / 499.4
+      checkIndexScan(stmt);
+      checkForeignScan(stmt);
+      checkIndexOnlyScan(stmt);
+      checkSecondaryIndexScan(stmt);
+    }
+  }
+
+  private double getExecutionTime(
+      Statement stmt, String query, String expectedScanType) throws SQLException {
+    String resultJson = getRowList(
+        stmt, "EXPLAIN (ANALYZE true, FORMAT json) " + query).get(0).get(0).toString();
+    JsonObject obj = new JsonParser().parse(resultJson).getAsJsonArray().get(0).getAsJsonObject();
+    assertEquals(expectedScanType,
+                 obj.getAsJsonObject("Plan").get("Node Type").getAsString());
+    return obj.get("Execution Time").getAsDouble();
+  }
+
+  /** Run 'fat' and 'thin' queries multiple times and compare median execution time. */
+  private void checkQuery(Statement stmt,
+                          String queryThinColumns,
+                          String queryFatColumns,
+                          String expectedScanType) throws SQLException {
+    List<Double> fatTime = new ArrayList<>();
+    List<Double> thinTime = new ArrayList<>();
+    int attempts = 30;
+    for (int i = 0; i < attempts; ++i) {
+      thinTime.add(getExecutionTime(stmt, queryThinColumns, expectedScanType));
+      fatTime.add(getExecutionTime(stmt, queryFatColumns, expectedScanType));
+    }
+    Comparator<Double> comparator = Comparator.naturalOrder();
+    Collections.sort(thinTime, comparator);
+    Collections.sort(fatTime, comparator);
+    int medianIndex = attempts / 2;
+    double fatTimeMedian = fatTime.get(medianIndex);
+    double thinTimeMedian = thinTime.get(medianIndex);
+    assertGreaterThan(
+        String.format(
+            "Expected median time %f of '%s' to be greater than median time %f of '%s'",
+            fatTimeMedian, queryFatColumns, thinTimeMedian, queryThinColumns),
+        fatTimeMedian, thinTimeMedian);
+  }
+
+  private void checkIndexScan(Statement stmt) throws SQLException {
+    String pattern = "SELECT %s FROM t WHERE h = 1";
+    checkQuery(
+        stmt, String.format(pattern, "v1"), String.format(pattern, "*"), "Index Scan");
+  }
+
+  private void checkForeignScan(Statement stmt) throws SQLException {
+    String pattern = "SELECT %s FROM t";
+    checkQuery(
+        stmt, String.format(pattern, "v1"), String.format(pattern, "*"), "Seq Scan");
+  }
+
+  private void checkIndexOnlyScan(Statement stmt) throws SQLException {
+    String pattern = "SELECT %s FROM t WHERE v1 = 1";
+    checkQuery(stmt,
+               String.format(pattern, "v2"),
+               String.format(pattern, "v1, v2, v3, t1, t2, t3, t4, t5"),
+               "Index Only Scan");
+  }
+
+  private void checkSecondaryIndexScan(Statement stmt) throws SQLException {
+    String pattern = "SELECT %s FROM t WHERE v1 = 1";
+    checkQuery(stmt, String.format(pattern, "h"), String.format(pattern, "*"), "Index Scan");
+  }
+}

src/postgres/src/backend/access/index/genam.c
@@ -129,6 +129,7 @@ RelationGetIndexScan(Relation indexRelation, int nkeys, int norderbys)
 	scan->xs_continue_hot = false;
 
 	scan->yb_exec_params = NULL;
+	scan->yb_scan_plan = NULL;
 	return scan;
 }
 

src/postgres/src/backend/access/ybc/ybcin.c
@@ -294,8 +294,7 @@ ybcinrescan(IndexScanDesc scan, ScanKey scankey, int nscankeys,	ScanKey orderbys
 	}
 
 	YbScanDesc ybScan = ybcBeginScan(scan->heapRelation, scan->indexRelation, scan->xs_want_itup,
-																	 nscankeys, scankey);
-	ybScan->index = scan->indexRelation;
+	                                 nscankeys, scankey, scan->yb_scan_plan);
 	scan->opaque = ybScan;
 }
 

src/postgres/src/backend/executor/nodeIndexonlyscan.c
@@ -95,7 +95,7 @@ IndexOnlyNext(IndexOnlyScanState *node)
 								   node->ioss_NumOrderByKeys);
 
 		node->ioss_ScanDesc = scandesc;
-
+		scandesc->yb_scan_plan = (Scan *)node->ss.ps.plan;
 
 		/* Set it up for index-only scan */
 		node->ioss_ScanDesc->xs_want_itup = true;

src/postgres/src/backend/executor/nodeIndexscan.c
@@ -117,6 +117,7 @@ IndexNext(IndexScanState *node)
 								   node->iss_NumOrderByKeys);
 
 		node->iss_ScanDesc = scandesc;
+		scandesc->yb_scan_plan = (Scan *)node->ss.ps.plan;
 
 		/*
 		 * If no run-time keys to calculate or they are ready, go ahead and
@@ -241,6 +242,7 @@ IndexNextWithReorder(IndexScanState *node)
 								   node->iss_NumOrderByKeys);
 
 		node->iss_ScanDesc = scandesc;
+		scandesc->yb_scan_plan = (Scan *)node->ss.ps.plan;
 
 		/*
 		 * If no run-time keys to calculate or they are ready, go ahead and

src/postgres/src/include/access/relscan.h
@@ -159,6 +159,17 @@ typedef struct IndexScanDescData
 	 *   index-scan execution in YugaByte.
 	 */
 	YBCPgExecParameters *yb_exec_params;
+
+	/*
+	 * yb_scan_plan stores postgres scan plan for current index scan.
+	 * This information is used to determine target columns that must be read from DocDB
+	 * and columns which can be omitted.
+	 * TODO: Calculate set of required YB targets on plan stage and use it here
+	 *       instead of scan plan. In addition to code speedup this approach will allow to
+	 *       remove scan plan from IndexScanDescData structure. Native postgres code doesn't
+	 *       have plan information in scan state structures.
+	 */
+	Scan *yb_scan_plan;
 }			IndexScanDescData;
 
 /* Generic structure for parallel scans */

src/postgres/src/include/access/ybcam.h
@@ -64,6 +64,7 @@ typedef struct YbScanDescData
 	YBCPgStatement handle;
 	bool is_exec_done;
 
+	Relation relation;
 	Relation index;
 
 	int nkeys;
@@ -71,9 +72,6 @@ typedef struct YbScanDescData
 
 	TupleDesc target_desc;
 	AttrNumber target_key_attnums[YB_MAX_SCAN_KEYS];
-
-	/* Oid of the table being scanned */
-	Oid tableOid;
 
 	/* Kept query-plan control to pass it to PgGate during preparation */
 	YBCPgPrepareParameters prepare_params;
@@ -87,6 +85,7 @@ typedef struct YbScanDescData
 	 *   execution in YB tablet server.
 	 */
 	YBCPgExecParameters *exec_params;
+
 	/*
 	 * Flag used for bailing out from scan early. Currently used to bail out from scans where
 	 * one of the bind conditions is a search array and is empty.
@@ -135,7 +134,8 @@ YbScanDesc ybcBeginScan(Relation relation,
                         Relation index,
                         bool xs_want_itup,
                         int nkeys,
-                        ScanKey key);
+                        ScanKey key,
+                        Scan *pg_scan_plan);
 
 HeapTuple ybc_getnext_heaptuple(YbScanDesc ybScan, bool is_forward_scan, bool *recheck);
 IndexTuple ybc_getnext_indextuple(YbScanDesc ybScan, bool is_forward_scan, bool *recheck);

commit 2a9bb43f0797f759156c55db920f0c00e84654c5

[#9176] YSQL: Fix the column filter for index only scan
Summary:
Columns from the `plan->qual` is excluded in the case of index only scan. YB doesn't read these columns from DocDB and uses NULL values instead. As a result further filtering on YB side works incorrectly.

Example: `SELECT col3 FROM table WHERE col1 = 1 AND col2 LIKE '%x%';`
does not work properly if there is index on table(col1, col2, col3);

Solution is to add columns from the `plan->qual` into set of required fields in case of index only scan.

In collaboration with Zhihong Yu @tedyu

**Note:** The issue was introduced by the https://github.com/yugabyte/yugabyte-db/commit/8e3ba0b11c7bac28a88a8a7110caa0ac43df921f commit

Test Plan:
Regression tests included
```
./yb_build.sh --java-test 'org.yb.pgsql.TestPgRegressIndex'
```

Reviewers: mihnea, jason, zyu, dmitry

Reviewed By: dmitry

Subscribers: yql

Differential Revision: https://phabricator.dev.yugabyte.com/D12191

src/postgres/src/backend/access/ybc/ybcam.c
@@ -955,7 +955,8 @@ ybcBindScanKeys(YbScanDesc ybScan, YbScanPlan scan_plan) {
  * Required columns are:
  * - all bound key columns
  * - all query targets columns (from index for Index Only Scan case and from table otherwise)
- * - all table columns required for filtering resulting rows on postgres' side
+ * - all qual columns (not bound to the scan keys), they are requirted for filtering results
+ *   on the postgres side
  *
  * Example:
  * SELECT <target columns from index or table> FROM t
@@ -985,14 +986,8 @@ ybcInitColumnFilter(YbColumnFilter *filter, YbScanDesc ybScan, Scan *pg_scan_pla
 		items = bms_add_member(items, *sk_attno - min_attr + 1);
 
 	ListCell *lc;
-	Index target_relid = INDEX_VAR;
-	if (!ybScan->prepare_params.index_only_scan)
-	{
-		/* Collect table filtering attributes */
-		foreach(lc, pg_scan_plan->plan.qual)
-			pull_varattnos_min_attr((Node *) lfirst(lc), pg_scan_plan->scanrelid, &items, min_attr);
-		target_relid = pg_scan_plan->scanrelid;
-	}
+	Index target_relid =
+		ybScan->prepare_params.index_only_scan ? INDEX_VAR : pg_scan_plan->scanrelid;
 
 	/* Collect target attributes */
 	foreach(lc, pg_scan_plan->plan.targetlist)
@@ -1000,6 +995,10 @@ ybcInitColumnFilter(YbColumnFilter *filter, YbScanDesc ybScan, Scan *pg_scan_pla
 		TargetEntry *tle = (TargetEntry *) lfirst(lc);
 		pull_varattnos_min_attr((Node *) tle->expr, target_relid, &items, min_attr);
 	}
+
+	/* Collect table filtering attributes */
+	foreach(lc, pg_scan_plan->plan.qual)
+		pull_varattnos_min_attr((Node *) lfirst(lc), target_relid, &items, min_attr);
 
 	/* In case InvalidAttrNumber is set whole row columns are required */
 	if (bms_is_member(InvalidAttrNumber - min_attr + 1, items))
@@ -1443,12 +1442,12 @@ void ybcCostEstimate(RelOptInfo *baserel, Selectivity selectivity,
 	 *     it requires extra request to the main table.
 	 */
 	double tsp_cost = 0.0;
-	bool is_valid_tsp_cost = !is_uncovered_idx_scan 
-								&& get_yb_tablespace_cost(index_tablespace_oid, 
+	bool is_valid_tsp_cost = !is_uncovered_idx_scan
+								&& get_yb_tablespace_cost(index_tablespace_oid,
 															&tsp_cost);
 	Cost yb_per_tuple_cost_factor = YB_DEFAULT_PER_TUPLE_COST;
 
-	if (is_valid_tsp_cost && yb_per_tuple_cost_factor > tsp_cost) 
+	if (is_valid_tsp_cost && yb_per_tuple_cost_factor > tsp_cost)
 	{
 		yb_per_tuple_cost_factor = tsp_cost;
 	}

src/postgres/src/test/regress/expected/yb_index_scan.out
@@ -686,6 +686,37 @@ SELECT * FROM test WHERE col6 = 5554;
   5 |    5 |    5 |    5 |    5 | 5554 | Ee   |    5 |  199
 (1 row)
 
+-- test index only scan with non-target column refs in qual (github issue #9176)
+-- baseline, col5 is in target columns
+EXPLAIN SELECT col4, col5 FROM test WHERE col4 = 232 and col5 % 3 = 0;
+                                          QUERY PLAN
+-----------------------------------------------------------------------------------------------
+ Index Only Scan using idx_col4_idx_col5_idx_col6 on test  (cost=0.00..16.25 rows=100 width=8)
+   Index Cond: (col4 = 232)
+   Filter: ((col5 % 3) = 0)
+(3 rows)
+
+SELECT col4, col5 FROM test WHERE col4 = 232 and col5 % 3 = 0;
+ col4 | col5
+------+------
+  232 |  345
+(1 row)
+
+-- same lines are expected without col5 in the target list
+EXPLAIN SELECT col4 FROM test WHERE col4 = 232 and col5 % 3 = 0;
+                                          QUERY PLAN
+-----------------------------------------------------------------------------------------------
+ Index Only Scan using idx_col4_idx_col5_idx_col6 on test  (cost=0.00..16.25 rows=100 width=4)
+   Index Cond: (col4 = 232)
+   Filter: ((col5 % 3) = 0)
+(3 rows)
+
+SELECT col4 FROM test WHERE col4 = 232 and col5 % 3 = 0;
+ col4
+------
+  232
+(1 row)
+
 -- testing update on primary key
 update test set pk=17 where pk=1;
 update test set pk=25, col4=777 where pk=2;
@@ -725,4 +756,3 @@ select * from test where pk=25;
 ----+------+------+------+------+------+------+------+------
  25 |    2 |    2 |  777 |  223 |    2 | Bb   |    2 |  199
 (1 row)
-

src/postgres/src/test/regress/sql/yb_index_scan.sql
@@ -217,6 +217,14 @@ update test set col6=5554 where pk=5;
 EXPLAIN SELECT * FROM test WHERE col6 = 5554;
 SELECT * FROM test WHERE col6 = 5554;
 
+-- test index only scan with non-target column refs in qual (github issue #9176)
+-- baseline, col5 is in target columns
+EXPLAIN SELECT col4, col5 FROM test WHERE col4 = 232 and col5 % 3 = 0;
+SELECT col4, col5 FROM test WHERE col4 = 232 and col5 % 3 = 0;
+-- same lines are expected without col5 in the target list
+EXPLAIN SELECT col4 FROM test WHERE col4 = 232 and col5 % 3 = 0;
+SELECT col4 FROM test WHERE col4 = 232 and col5 % 3 = 0;
+
 -- testing update on primary key
 update test set pk=17 where pk=1;
 update test set pk=25, col4=777 where pk=2;