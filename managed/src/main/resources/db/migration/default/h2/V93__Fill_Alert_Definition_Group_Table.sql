cde8265437f52ce6d21401ea5b422613b82fb9e4
Aleksandr Malyshev <amalyshev@yugabyte.com>
2021-07-08 11:57:44 +0300

    [#9215] Fix repeatable DDL migration
    Summary:
    Need to avoid having repeatable DDL migrations. One was submitted and breaks fresh platform install because versioned migration is dependent on it.
    Moved code from repeatable migration to versioned.
    
    Test Plan:
    Run platform on top of DB with already applied old versioned and repeatable migrations.
    Get checksum mismatch error.
    Update checksum manually by running:
    update schema_version set checksum = 1462224359 where version = '93';
    Run platform once again and make sure it runs successfully.
    
    Reviewers: amindrov, sb-yb, spotachev
    
    Reviewed By: amindrov, sb-yb, spotachev
    
    Subscribers: jenkins-bot, yugaware
    
    Differential Revision: https://phabricator.dev.yugabyte.com/D12206

--- managed/src/main/resources/db/migration/default/h2/V93__Fill_Alert_Definition_Group_Table.sql
+++ managed/src/main/resources/db/migration/default/h2/V93__Fill_Alert_Definition_Group_Table.sql
@@ -1,5 +1,7 @@
 -- Copyright (c) YugaByte, Inc.
 
+alter table runtime_config_entry alter column value set data type blob;
+
 -- No need to fill anything for h2.
 alter table alert_definition drop column if exists name;
 alter table alert_definition drop column if exists query_duration_sec;
--- managed/src/main/resources/db/migration/default/postgres/R__Extend_RuntimeConfigEntry_For_Large_Values.sql
+++ managed/src/main/resources/db/migration/default/postgres/R__Extend_RuntimeConfigEntry_For_Large_Values.sql
@@ -1,14 +1,3 @@
-create function migrate_values_to_binary() returns void as
-$$
-declare
-  value_type text;
-begin
-  select data_type into value_type from information_schema.columns where table_name= 'runtime_config_entry' and column_name = 'value';
-  if value_type = 'text' then
-    alter table runtime_config_entry alter column value type bytea using convert_to(value::text, 'utf8');
-  end if;
-end;
-$$ language plpgsql;
+-- Copyright (c) YugaByte, Inc.
 
-select migrate_values_to_binary();
-drop function migrate_values_to_binary();
+-- Moved to V93 migration
