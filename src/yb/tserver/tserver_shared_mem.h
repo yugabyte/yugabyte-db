dc1f25fc2b51370e767d1e7628a826ffa333f0b5
Mikhail Bautin <mbautin@users.noreply.github.com>
2021-07-08 09:38:43 -0700

    [#9154] [#2519] [#1973] Initial changes for supporting the aarch64 architecture
    Summary:
    Initial changes to make the aarch64 build work:
    - Intel-specific compiler flags (`-mno-avx`, `-mno-bmi`, `-mno-bmi2`, `-mno-fma`, `-mno-abm`, and `-mno-movbe`) should not be used on aarch64.
    - Set LIBUNWIND_ARCH to aarch64 to use the correct architecture-specific libunwind library.
    - Add a dependency on the boost_atomic library. The code fails to link without it on aarch64, even though we might already be using Boost atomics, perhaps in a header-only way, on x86_64.
    - Remove the -DUSE_SSE42_CRC32C=1 flag in PostgreSQL when building on aarch64.
    - Fix key_encoder.h to avoid using SSE on aarch64.
    - Add gutil/atomicops-internals-arm64.h based on https://github.com/apache/kudu/blob/425449ff2afd60b21e8c39028276b7370591f650/src/kudu/gutil/atomicops-internals-arm64.h and include it in src/yb/gutil/atomicops.h.
    - Add the ARM64 architecture to `cpu.cc`.
    - Update `src/yb/gutil/linux_syscall_support.h` based on https://github.com/apache/kudu/blob/master/src/kudu/gutil/linux_syscall_support.h ( based on commit https://github.com/apache/kudu/commit/0a46332b2129cb256cf9d55c091643d96267724f, which is based on commit fd00dbbd0c06a309c657d89e9430143b179ff6db from https://chromium.googlesource.com/linux-syscall-support ).
    
    Other changes / cleanup:
    - Annotate switch fallthrough in murmurhash.cc.
    - Remove a few `.pump.h` files that were not being used.
    - Only enabling Linuxbrew automatically on the x86_64 architecture.
    - Fixing lint in a few existing files that are being modified.
    
    Relevant changes in Kudu:
    - https://github.com/apache/kudu/commit/74459bd1b3ba311360a8b832873d9e3273dfc83a -- `KUDU-3007 (1/3): Import sse2neon.h into tree to Adapt SSE intrinsics on aarch64` (relatively less important for us).
    - https://github.com/apache/kudu/commit/425449ff2afd60b21e8c39028276b7370591f650 -- `KUDU-3007 (2/3): Add atomicops-internals-arm64.h into tree to support atomicops on aarch64`.
    - https://github.com/apache/kudu/commit/f5d2899a6483fc08a17ade8d9b26b3e6930da4e4 ( to browse the repository as of this commit: https://github.com/apache/kudu/tree/f5d2899a6483fc08a17ade8d9b26b3e6930da4e4 ) -- `KUDU-3007 (3/3): Support building and running Kudu on aarch64 platform`
    
    Relevant files in Kudu (at a specific commit):
    - https://github.com/apache/kudu/blob/f5d2899a6483fc08a17ade8d9b26b3e6930da4e4/src/kudu/util/debug-util.cc
    - https://github.com/apache/kudu/blob/f5d2899a6483fc08a17ade8d9b26b3e6930da4e4/src/kudu/gutil/spinlock_linux-inl.h
    - https://github.com/apache/kudu/blob/f5d2899a6483fc08a17ade8d9b26b3e6930da4e4/src/kudu/gutil/port.h
    - https://github.com/apache/kudu/blob/f5d2899a6483fc08a17ade8d9b26b3e6930da4e4/src/kudu/gutil/cycleclock-inl.h
    - https://github.com/apache/kudu/blob/425449ff2afd60b21e8c39028276b7370591f650/src/kudu/gutil/atomicops-internals-arm64 -- this file is really similar to https://github.com/protocolbuffers/protobuf/blob/3.5.x/src/google/protobuf/stubs/atomicops_internals_arm64_gcc.h
    
    Current limitations:
    - tcmalloc needs to be disabled for now in aarch64 builds ( https://github.com/yugabyte/yugabyte-db/issues/9206 )
    - Removed lock-free checks from a few places when building under aarch64 ( https://github.com/yugabyte/yugabyte-db/issues/9196 )
    - Not trying a Clang build for now ( https://github.com/yugabyte/yugabyte-db/issues/9207 )
    
    Thanks to @TiannV and @giminni for their efforts to identify an initial set of aarch64 porting issues and propose fixes/workarounds.
    
    Test Plan:
    Jenkins
    
    Testing on a Graviton2 EC2 instance:
    
    export YB_THIRDPARTY_DIR=$HOME/code/yugabyte-db-thirdparty
    export YB_DISABLE_LINUXBREW=1
    cd ~/code/yugabyte-db
    ./yb_build.sh --save-log release --no-tcmalloc "$@"
    
    Reviewers: bogdan, timur, sergei
    
    Reviewed By: sergei
    
    Subscribers: ybase
    
    Differential Revision: https://phabricator.dev.yugabyte.com/D12151

--- src/yb/tserver/tserver_shared_mem.h
+++ src/yb/tserver/tserver_shared_mem.h
@@ -19,6 +19,7 @@
 #include "yb/util/shared_mem.h"
 
 #include "yb/tserver/tserver_util_fwd.h"
+#include "yb/util/lockfree.h"
 
 namespace yb {
 namespace tserver {
@@ -33,7 +34,7 @@ class TServerSharedData {
     // for shared memory! Some atomics claim to be lock-free but still require
     // read-write access for a `load()`.
     // E.g. for 128 bit objects: https://stackoverflow.com/questions/49816855.
-    LOG_IF(FATAL, !catalog_version_.is_lock_free())
+    LOG_IF(FATAL, !IsAcceptableAtomicImpl(catalog_version_))
         << "Shared memory atomics must be lock-free";
     host_[0] = 0;
   }
