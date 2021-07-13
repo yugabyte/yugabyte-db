b57a3bcefd7531b236d917b5f83c1fcb04d95d91
Jitendra Kumar <jitendrkumar@deloitte.com>
2021-07-13 15:26:02 +0530

    [Platfrom] [API] Swagger annotation for Alert Model.
    Summary: Added the swagger annotation for Alert Model
    
    Test Plan: Bringup the local server and test the basic scenerio and also did sbt test.
    
    Reviewers: sb-yb
    
    Reviewed By: sb-yb
    
    Subscribers: jenkins-bot
    
    Differential Revision: https://phabricator.dev.yugabyte.com/D12228

--- managed/src/main/java/com/yugabyte/yw/models/Alert.java
+++ managed/src/main/java/com/yugabyte/yw/models/Alert.java
@@ -13,6 +13,8 @@ import com.yugabyte.yw.models.paging.PagedQuery;
 import io.ebean.ExpressionList;
 import io.ebean.Finder;
 import io.ebean.Model;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.experimental.Accessors;
@@ -25,11 +27,14 @@ import java.util.UUID;
 import java.util.stream.Collectors;
 
 import static com.yugabyte.yw.models.helpers.CommonUtils.*;
+import static io.swagger.annotations.ApiModelProperty.AccessMode.READ_ONLY;
+import static io.swagger.annotations.ApiModelProperty.AccessMode.READ_WRITE;
 
 @Entity
 @Data
 @Accessors(chain = true)
 @EqualsAndHashCode(callSuper = false)
+@ApiModel(description = "Alert information. which is used to send alert notification.")
 public class Alert extends Model implements AlertLabelsProvider {
 
   public enum State {
@@ -66,42 +71,56 @@ public class Alert extends Model implements AlertLabelsProvider {
 
   @Id
   @Column(nullable = false, unique = true)
+  @ApiModelProperty(value = "Alert uuid", accessMode = READ_ONLY)
   private UUID uuid;
 
   @Column(nullable = false)
+  @ApiModelProperty(value = "Cutomer uuid", accessMode = READ_ONLY)
   private UUID customerUUID;
 
   @Column(nullable = false)
   @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss")
+  @ApiModelProperty(value = "Create Date time info.", accessMode = READ_ONLY)
   private Date createTime = nowWithoutMillis();
 
   @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss")
+  @ApiModelProperty(value = "Acknowledge Date time info.", accessMode = READ_ONLY)
   private Date acknowledgedTime;
 
   @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss")
+  @ApiModelProperty(value = "Resolved Date time info.", accessMode = READ_ONLY)
   private Date resolvedTime;
 
   @Column(columnDefinition = "Text", nullable = false)
+  @ApiModelProperty(value = "Error Code.", accessMode = READ_ONLY)
   private String errCode;
 
   @Enumerated(EnumType.STRING)
+  @ApiModelProperty(value = "Alert definition group serverity.", accessMode = READ_ONLY)
   private AlertDefinitionGroup.Severity severity;
 
   @Column(columnDefinition = "Text", nullable = false)
+  @ApiModelProperty(value = "Alert Message.", accessMode = READ_ONLY)
   private String message;
 
   @Enumerated(EnumType.STRING)
+  @ApiModelProperty(value = "Alert State.", accessMode = READ_ONLY)
   private State state = State.CREATED;
 
   @Enumerated(EnumType.STRING)
+  @ApiModelProperty(value = "Target State.", accessMode = READ_ONLY)
   private State targetState = State.ACTIVE;
 
+  @ApiModelProperty(value = "Whether to send an Email or not.", accessMode = READ_ONLY)
   private boolean sendEmail;
 
+  @ApiModelProperty(value = "Alert Definition Uuid", accessMode = READ_ONLY)
   private UUID definitionUuid;
 
+  @ApiModelProperty(value = "Alert group Uuid", accessMode = READ_ONLY)
   private UUID groupUuid;
 
+  @ApiModelProperty(value = "Alert definition group type", accessMode = READ_ONLY)
   private AlertDefinitionGroup.TargetType groupType;
 
   @OneToMany(mappedBy = "alert", cascade = CascadeType.ALL, orphanRemoval = true)
