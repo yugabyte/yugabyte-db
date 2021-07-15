efb3b11c385d077330a7d096d041ea92a3caf771
Mahendra Bhat <50577199+mahendranbhat@users.noreply.github.com>
2021-07-15 15:26:52 +0530

    [#8296][Platform] Ability to stop backups from admin console. (#9310)
    Summary:
    When running long backups which might take minutes to hours, it would be handy to be to able to stop backup processes via admin console. yb_backup.py script has to be stopped by logging in to the platform server, if it process has to be stopped for some reason such as long-running backup, conflicts with another process, high resource consumption, etc.
    
    Test Plan:
    Created the universe as the change is to shellprocesshandler
    Created the backup.
    Stopped backup and verified.
    Created the complete backup as well using NFS.
    Verified the status of the backup.
    
    Reviewers: Sergey, Jaydeep, Jitendra, Shashank
    Reviewed by : Sergey, Sergey Shevchecko.
    
    Diff : https://phabricator.dev.yugabyte.com/D12173

--- managed/src/main/java/com/yugabyte/yw/controllers/BackupsController.java
+++ managed/src/main/java/com/yugabyte/yw/controllers/BackupsController.java
@@ -7,11 +7,13 @@ import com.fasterxml.jackson.databind.node.ObjectNode;
 import com.google.inject.Inject;
 import com.yugabyte.yw.commissioner.Commissioner;
 import com.yugabyte.yw.commissioner.tasks.subtasks.DeleteBackup;
+import com.yugabyte.yw.common.Util;
 import com.yugabyte.yw.common.YWServiceException;
 import com.yugabyte.yw.forms.BackupTableParams;
 import com.yugabyte.yw.forms.YWResults.YWError;
 import com.yugabyte.yw.forms.YWResults;
 import com.yugabyte.yw.models.*;
+import com.yugabyte.yw.models.Backup.BackupState;
 import com.yugabyte.yw.models.helpers.CommonUtils;
 import com.yugabyte.yw.models.helpers.TaskType;
 import org.slf4j.Logger;
@@ -34,6 +36,7 @@ import java.util.UUID;
 @Api(value = "Backups", authorizations = @Authorization(AbstractPlatformController.API_KEY_AUTH))
 public class BackupsController extends AuthenticatedController {
   public static final Logger LOG = LoggerFactory.getLogger(BackupsController.class);
+  private static int maxRetryCount = 5;
 
   @Inject Commissioner commissioner;
 
@@ -237,5 +240,47 @@ public class BackupsController extends AuthenticatedController {
       }
     }
     return new YWResults.YWTasks(taskUUIDList).asResult();
+  }
+
+  public Result stop(UUID customerUUID, UUID backupUUID) {
+    Customer.getOrBadRequest(customerUUID);
+    Process process = Util.getProcessOrBadRequest(backupUUID);
+    Backup backup = Backup.getOrBadRequest(customerUUID, backupUUID);
+    if (backup.state != Backup.BackupState.InProgress) {
+      LOG.info("The backup {} you are trying to stop is not in progress.", backupUUID);
+      throw new YWServiceException(
+          BAD_REQUEST, "The backup you are trying to stop is not in process.");
+    }
+    if (process == null) {
+      LOG.info("The backup {} process you want to stop doesn't exist.", backupUUID);
+      throw new YWServiceException(
+          BAD_REQUEST, "The backup process you want to stop doesn't exist.");
+    } else {
+      process.destroyForcibly();
+    }
+    Util.removeProcess(backupUUID);
+    try {
+      waitForTask(backup.taskUUID);
+    } catch (InterruptedException e) {
+      LOG.info("Error while waiting for the backup task to get finished.");
+    }
+    backup.transitionState(BackupState.Stopped);
+    return YWResults.YWSuccess.withMessage("Successfully stopped the backup process.");
+  }
+
+  private static void waitForTask(UUID taskUUID) throws InterruptedException {
+    int numRetries = 0;
+    while (numRetries < maxRetryCount) {
+      TaskInfo taskInfo = TaskInfo.get(taskUUID);
+      if (taskInfo.getTaskState() == TaskInfo.State.Success
+          || taskInfo.getTaskState() == TaskInfo.State.Failure) {
+        return;
+      }
+      Thread.sleep(1000);
+      numRetries++;
+    }
+    throw new YWServiceException(
+        BAD_REQUEST,
+        "WaitFor task exceeded maxRetries! Task state is " + TaskInfo.get(taskUUID).getTaskState());
   }
 }
