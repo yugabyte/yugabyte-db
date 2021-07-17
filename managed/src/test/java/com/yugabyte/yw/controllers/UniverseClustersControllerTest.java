013b80df1b56f60e4d5a7259fe5ac92a26b1c678
SB YB <sbapat@yugabyte.com>
2021-07-17 04:12:45 -0700

    [#8990] UniverseClustsersController unit tests
    Summary: Reuse tests for universe create for testing new api and make sure they pass with new API as well.
    
    Test Plan: N/A
    
    Reviewers: spotachev
    
    Reviewed By: spotachev
    
    Subscribers: jenkins-bot, yugaware
    
    Differential Revision: https://phabricator.dev.yugabyte.com/D12294

--- managed/src/test/java/com/yugabyte/yw/controllers/UniverseClustersControllerTest.java
+++ managed/src/test/java/com/yugabyte/yw/controllers/UniverseClustersControllerTest.java
@@ -10,60 +10,30 @@
 
 package com.yugabyte.yw.controllers;
 
-import com.fasterxml.jackson.databind.node.ArrayNode;
-import com.fasterxml.jackson.databind.node.ObjectNode;
-import com.yugabyte.yw.common.ModelFactory;
-import com.yugabyte.yw.models.AvailabilityZone;
-import com.yugabyte.yw.models.InstanceType;
-import com.yugabyte.yw.models.Provider;
-import com.yugabyte.yw.models.Region;
+import static com.yugabyte.yw.common.AssertHelper.assertOk;
+import static com.yugabyte.yw.common.FakeApiHelper.doRequestWithAuthToken;
+import static play.test.Helpers.contentAsString;
+
+import com.fasterxml.jackson.databind.JsonNode;
 import junitparams.JUnitParamsRunner;
-import org.junit.Test;
 import org.junit.runner.RunWith;
 import play.libs.Json;
 import play.mvc.Result;
 
-import static com.yugabyte.yw.common.AssertHelper.*;
-import static com.yugabyte.yw.common.FakeApiHelper.doRequestWithAuthTokenAndBody;
-
 @RunWith(JUnitParamsRunner.class)
-public class UniverseClustersControllerTest extends UniverseControllerTestBase {
-
-  @Test
-  public void testUniverseCreateWithInvalidParams() {
-    String url = "/api/customers/" + customer.uuid + "/universes/clusters";
-    Result result =
-        assertYWSE(() -> doRequestWithAuthTokenAndBody("POST", url, authToken, Json.newObject()));
-    assertBadRequest(result, "clusters: This field is required");
-    assertAuditEntry(0, customer.uuid);
+public class UniverseClustersControllerTest extends UniverseCreateControllerTestBase {
+  @Override
+  protected String universeCreateUrl() {
+    return "/api/customers/" + customer.uuid + "/universes/clusters";
   }
 
-  @Test
-  public void testUniverseCreateWithInvalidUniverseName() {
-    String url = "/api/customers/" + customer.uuid + "/universes/clusters";
-    Provider p = ModelFactory.awsProvider(customer);
-    Region r = Region.create(p, "region-1", "PlacementRegion 1", "default-image");
-    AvailabilityZone.createOrThrow(r, "az-1", "PlacementAZ 1", "subnet-1");
-    AvailabilityZone.createOrThrow(r, "az-2", "PlacementAZ 2", "subnet-2");
-    InstanceType i =
-        InstanceType.upsert(p.uuid, "c3.xlarge", 10, 5.5, new InstanceType.InstanceTypeDetails());
-
-    ObjectNode bodyJson = Json.newObject();
-    ObjectNode userIntentJson =
-        Json.newObject()
-            .put("universeName", "Foo_Bar")
-            .put("instanceType", i.getInstanceTypeCode())
-            .put("replicationFactor", 3)
-            .put("numNodes", 3)
-            .put("provider", p.uuid.toString());
-    ArrayNode regionList = Json.newArray().add(r.uuid.toString());
-    userIntentJson.set("regionList", regionList);
-    ArrayNode clustersJsonArray =
-        Json.newArray().add(Json.newObject().set("userIntent", userIntentJson));
-    bodyJson.set("clusters", clustersJsonArray);
-    Result result =
-        assertYWSE(() -> doRequestWithAuthTokenAndBody("POST", url, authToken, bodyJson));
-    assertBadRequest(result, "Invalid universe name format, valid characters [a-zA-Z0-9-].");
-    assertAuditEntry(0, customer.uuid);
+  @Override
+  protected JsonNode getUniverseJson(Result universeCreateResponse) {
+    String universeUUID =
+        Json.parse(contentAsString(universeCreateResponse)).get("resourceUUID").asText();
+    String url = "/api/customers/" + customer.uuid + "/universes/" + universeUUID;
+    Result getResponse = doRequestWithAuthToken("GET", url, authToken);
+    assertOk(getResponse);
+    return Json.parse(contentAsString(getResponse));
   }
 }
