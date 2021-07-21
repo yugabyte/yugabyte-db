ef9960cdaa9cee3cf8480ff9a12cf6b45ecb9abd
Timur Yusupov <ttyusupov@users.noreply.github.com>
2021-07-21 21:55:54 +0300

    [#9392] docdb: fixed GCC9 debug build
    Summary:
    Turning off -O1 optimizations for debug build (9d4bfc080dce31afae23d87471de9112919b5493/D10121) made GCC9 to check for signed vs unigned comparisons and treat those as errors.
    Added utility functions for safe integral comparisons from C++20 and updated unsafe comparisons to use added functions instead.
    
    Test Plan: Jenkins
    
    Reviewers: mbautin
    
    Reviewed By: mbautin
    
    Subscribers: ybase
    
    Differential Revision: https://phabricator.dev.yugabyte.com/D12316

--- src/yb/rocksdb/util/io_posix.cc
+++ src/yb/rocksdb/util/io_posix.cc
@@ -49,6 +49,7 @@
 #include "yb/util/malloc.h"
 #include "yb/util/slice.h"
 #include "yb/util/stats/iostats_context_imp.h"
+#include "yb/util/std_util.h"
 #include "yb/util/string_util.h"
 
 namespace rocksdb {
@@ -333,8 +334,8 @@ Status PosixMmapFile::InvalidateCache(size_t offset, size_t length) {
 
 #ifdef ROCKSDB_FALLOCATE_PRESENT
 Status PosixMmapFile::Allocate(uint64_t offset, uint64_t len) {
-  assert(offset <= static_cast<uint64_t>(std::numeric_limits<off_t>::max()));
-  assert(len <= static_cast<uint64_t>(std::numeric_limits<off_t>::max()));
+  assert(yb::std_util::cmp_less_equal(offset, std::numeric_limits<off_t>::max()));
+  assert(yb::std_util::cmp_less_equal(len, std::numeric_limits<off_t>::max()));
   TEST_KILL_RANDOM("PosixMmapFile::Allocate:0", rocksdb_kill_odds);
   int alloc_status = 0;
   if (allow_fallocate_) {
@@ -464,8 +465,8 @@ Status PosixWritableFile::InvalidateCache(size_t offset, size_t length) {
 
 #ifdef ROCKSDB_FALLOCATE_PRESENT
 Status PosixWritableFile::Allocate(uint64_t offset, uint64_t len) {
-  assert(offset <= static_cast<uint64_t>(std::numeric_limits<off_t>::max()));
-  assert(len <= static_cast<uint64_t>(std::numeric_limits<off_t>::max()));
+  assert(yb::std_util::cmp_less_equal(offset, std::numeric_limits<off_t>::max()));
+  assert(yb::std_util::cmp_less_equal(len, std::numeric_limits<off_t>::max()));
   TEST_KILL_RANDOM("PosixWritableFile::Allocate:0", rocksdb_kill_odds);
   IOSTATS_TIMER_GUARD(allocate_nanos);
   int alloc_status = 0;
@@ -482,8 +483,8 @@ Status PosixWritableFile::Allocate(uint64_t offset, uint64_t len) {
 }
 
 Status PosixWritableFile::RangeSync(uint64_t offset, uint64_t nbytes) {
-  assert(offset <= static_cast<uint64_t>(std::numeric_limits<off_t>::max()));
-  assert(nbytes <= static_cast<uint64_t>(std::numeric_limits<off_t>::max()));
+  assert(yb::std_util::cmp_less_equal(offset, std::numeric_limits<off_t>::max()));
+  assert(yb::std_util::cmp_less_equal(nbytes, std::numeric_limits<off_t>::max()));
   if (sync_file_range(fd_, static_cast<off_t>(offset),
       static_cast<off_t>(nbytes), SYNC_FILE_RANGE_WRITE) == 0) {
     return Status::OK();
