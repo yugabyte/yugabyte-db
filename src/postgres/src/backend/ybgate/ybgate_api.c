yugabyte/yugabyte-db

commit 638f4188da8b3f81a595eea7b7e45f2ed97104ab

[#8756] YSQL: Enable statistic collection by ANALYZE command
Summary:
This implements data sample collection from the Yugabyte table.
Postgres uses this sample to calculate and store various statistics for the optimizer to cost
query plan variants.
We use Vitter's reservoir sampling algorithm to select random rows implemented by Postgres.
Current implementation scans table partitions in random order, one by one, until required number of
rows is selected. DocDB returns ybctids of selected rows. After that selected rows are fetched,
similar to index scans. If not all partitions were scanned during sample total number of rows
extrapolated, assuming rows are evenly distributed across partitions. If all partitions are scanned
(single partition table, or small table having less rows than requested for the sample) total number
of rows is accurate (same as returned by select count(*) from table).
Analyze is still a beta feature and disabled by default, so
  --tserver_flags 'ysql_beta_features=1'
is required on order to work.

Test Plan:
For manual testing following cases have been tested:
 - empty table
 - small table (few rows)
 - large table (millions of row)
All cases tested in:
 - single node cluster, single partition tables
 - multinode cluster, multiple partition tables
In each case ANALYZE command was executed and pg_class and pg_statistic tables were examined.
Automated/unit tests to be added, suggestions?

Reviewers: mihnea, mtakahara, rsami, rskannan, neil

Reviewed By: neil

Subscribers: zyu, yql, mbautin

Differential Revision: https://phabricator.dev.yugabyte.com/D11820

src/postgres/src/backend/ybgate/ybgate_api.c
@@ -258,3 +258,24 @@ YbgStatus YbgSplitArrayDatum(uint64_t datum,
 	return PG_STATUS_OK;
 }
 
+//-----------------------------------------------------------------------------
+// Relation sampling
+//-----------------------------------------------------------------------------
+
+YbgStatus YbgSamplerRandomFract(SamplerRandomState randstate, double *value)
+{
+	PG_SETUP_ERROR_REPORTING();
+
+	*value = sampler_random_fract(randstate);
+
+	return PG_STATUS_OK;
+}
+
+YbgStatus YbgReservoirGetNextS(ReservoirState rs, double t, int n, double *s)
+{
+	PG_SETUP_ERROR_REPORTING();
+
+	*s = reservoir_get_next_S(rs, t, n);
+
+	return PG_STATUS_OK;
+}

src/postgres/src/include/ybgate/ybgate_api.h
@@ -24,6 +24,8 @@
 #include <stddef.h>
 #include <stdint.h>
 #include "yb/yql/pggate/ybc_pg_typedefs.h"
+#include "yb/gutil/integral_types.h"
+#include "utils/sampling.h"
 
 #ifdef __cplusplus
 extern "C" {
@@ -136,6 +138,21 @@ YbgStatus YbgEvalExpr(char* expr_cstring, YbgExprContext expr_ctx, uint64_t *dat
  * 'result_datum_array' will be allocated in this function itself, pre-allocation is not needed.
  */
 YbgStatus YbgSplitArrayDatum(uint64_t datum, int type, uint64_t **result_datum_array, int *nelems);
+
+//-----------------------------------------------------------------------------
+// Relation sampling
+//-----------------------------------------------------------------------------
+
+/*
+ * Select a random value R uniformly distributed in (0 - 1)
+ */
+YbgStatus YbgSamplerRandomFract(SamplerRandomState randstate, double *value);
+
+/*
+ * Calculate next number of rows to skip based on current number of scanned rows
+ * and requested sample size.
+ */
+YbgStatus YbgReservoirGetNextS(ReservoirState rs, double t, int n, double *s);
 
 #ifdef __cplusplus
 }

src/yb/docdb/pgsql_operation.cc
@@ -28,6 +28,7 @@
 #include "yb/docdb/doc_pgsql_scanspec.h"
 #include "yb/docdb/doc_rowwise_iterator.h"
 #include "yb/docdb/docdb_debug.h"
+#include "yb/docdb/docdb_pgapi.h"
 #include "yb/docdb/docdb_rocksdb_util.h"
 #include "yb/docdb/intent_aware_iterator.h"
 #include "yb/docdb/primitive_value_util.h"
@@ -662,6 +663,10 @@ Result<size_t> PgsqlReadOperation::Execute(const common::YQLStorageIf& ql_storag
     fetched_rows = VERIFY_RESULT(ExecuteBatchYbctid(
         ql_storage, deadline, read_time, schema,
         request_.unknown_ybctid_allowed(), result_buffer, restart_read_ht));
+  } else if (request_.has_sampling_state()) {
+    fetched_rows = VERIFY_RESULT(ExecuteSample(
+        ql_storage, deadline, read_time, is_explicit_request_read_time, schema,
+        result_buffer, restart_read_ht, &has_paging_state));
   } else {
     fetched_rows = VERIFY_RESULT(ExecuteScalar(
         ql_storage, deadline, read_time, is_explicit_request_read_time, schema, index_schema,
@@ -670,6 +675,121 @@ Result<size_t> PgsqlReadOperation::Execute(const common::YQLStorageIf& ql_storag
 
   VTRACE(1, "Fetched $0 rows. $1 paging state", fetched_rows, (has_paging_state ? "No" : "Has"));
   *restart_read_ht = table_iter_->RestartReadHt();
+  return fetched_rows;
+}
+
+Result<size_t> PgsqlReadOperation::ExecuteSample(const common::YQLStorageIf& ql_storage,
+                                                 CoarseTimePoint deadline,
+                                                 const ReadHybridTime& read_time,
+                                                 bool is_explicit_request_read_time,
+                                                 const Schema& schema,
+                                                 faststring *result_buffer,
+                                                 HybridTime *restart_read_ht,
+                                                 bool *has_paging_state) {
+  *has_paging_state = false;
+  size_t scanned_rows = 0;
+  PgsqlSamplingStatePB sampling_state = request_.sampling_state();
+  // Requested total number of rows to collect
+  int targrows = sampling_state.targrows();
+  // Number of rows collected so far
+  int numrows = sampling_state.numrows();
+  // Total number of rows scanned
+  double samplerows = sampling_state.samplerows();
+  // Current number of rows to skip before collecting next one for sample
+  double rowstoskip = sampling_state.rowstoskip();
+  // Variables for the random numbers generator
+  ReservoirStateData rstate;
+  rstate.W = sampling_state.rstate_w();
+  Uint64ToSamplerRandomState(rstate.randstate, sampling_state.rand_state());
+  // Buffer to hold selected row ids from the current page
+  std::unique_ptr<QLValuePB[]> reservoir = std::make_unique<QLValuePB[]>(targrows);
+  // Number of rows to scan for the current page.
+  // Too low row count limit is inefficient since we have to allocate and initialize a reservoir
+  // capable to hold potentially large (targrows) number of tuples. The row count has to be at least
+  // targrows for a chance to fill up the reservoir. Actually, the algorithm selects targrows only
+  // for very first page of the table, then it starts to skip tuples, the further it goes, the more
+  // it skips. For a large enough table it eventually starts to select less than targrows per page,
+  // regardless of the row_count_limit.
+  // Anyways, double targrows seems like reasonable minimum for the row_count_limit.
+  size_t row_count_limit = 2 * targrows;
+  if (request_.has_limit() && request_.limit() > row_count_limit) {
+    row_count_limit = request_.limit();
+  }
+  // Request is not supposed to contain any column refs, we just need the liveness column.
+  Schema projection;
+  RETURN_NOT_OK(CreateProjection(schema, request_.column_refs(), &projection));
+  // Request may carry paging state, CreateIterator takes care of positioning
+  table_iter_ = VERIFY_RESULT(CreateIterator(
+      ql_storage, request_, projection, schema, txn_op_context_,
+      deadline, read_time, is_explicit_request_read_time));
+  bool scan_time_exceeded = false;
+  while (scanned_rows++ < row_count_limit &&
+         VERIFY_RESULT(table_iter_->HasNext()) &&
+         !scan_time_exceeded) {
+    if (numrows < targrows) {
+      // Select first targrows of the table. If first partition(s) have less than that, next
+      // partition starts to continue populating it's reservoir starting from the numrows' position:
+      // the numrows, as well as other sampling state variables is returned and copied over to the
+      // next sampling request
+      Slice ybctid = VERIFY_RESULT(table_iter_->GetTupleId());
+      reservoir[numrows++].set_binary_value(ybctid.data(), ybctid.size());
+    } else {
+      // At least targrows tuples have already been collected, now algorithm skips increasing number
+      // of row before taking next one into the reservoir
+      if (rowstoskip <= 0) {
+        // Take ybctid of the current row
+        Slice ybctid = VERIFY_RESULT(table_iter_->GetTupleId());
+        // Pick random tuple in the reservoir to replace
+        double rvalue;
+        int k;
+        YbgSamplerRandomFract(rstate.randstate, &rvalue);
+        k = static_cast<int>(targrows * rvalue);
+        // Overwrite previous value with new one
+        reservoir[k].set_binary_value(ybctid.data(), ybctid.size());
+        // Choose next number of rows to skip
+        YbgReservoirGetNextS(&rstate, samplerows, targrows, &rowstoskip);
+      } else {
+        rowstoskip -= 1;
+      }
+    }
+    // Taking tuple ID does not advance the table iterator. Move it now.
+    table_iter_->SkipRow();
+    // Periodically check if we are running out of time
+    if (scanned_rows % 1024 == 0) {
+      scan_time_exceeded = CoarseMonoClock::now() >= deadline;
+    }
+  }
+  // Count live rows we have scanned TODO how to count dead rows?
+  samplerows += (scanned_rows - 1);
+  // Return collected tuples from the reservoir.
+  // Tuples are returned as (index, ybctid) pairs, where index is in [0..targrows-1] range.
+  // As mentioned above, for large tables reservoirs become increasingly sparse from page to page.
+  // So we hope to save by sending variable number of index/ybctid pairs vs exactly targrows of
+  // nullable ybctids. It also helps in case of extremely small table or partition.
+  int fetched_rows = 0;
+  for (int i = 0; i < numrows; i++) {
+    QLValuePB index;
+    if (reservoir[i].has_binary_value()) {
+      index.set_int32_value(i);
+      RETURN_NOT_OK(pggate::WriteColumn(index, result_buffer));
+      RETURN_NOT_OK(pggate::WriteColumn(reservoir[i], result_buffer));
+      fetched_rows++;
+    }
+  }
+
+  // Return sampling state to continue with next page
+  PgsqlSamplingStatePB *new_sampling_state = response_.mutable_sampling_state();
+  new_sampling_state->set_numrows(numrows);
+  new_sampling_state->set_targrows(targrows);
+  new_sampling_state->set_samplerows(samplerows);
+  new_sampling_state->set_rowstoskip(rowstoskip);
+  new_sampling_state->set_rstate_w(rstate.W);
+  new_sampling_state->set_rand_state(SamplerRandomStateToUint64(rstate.randstate));
+
+  // Return paging state if scan has not been completed
+  RETURN_NOT_OK(SetPagingStateIfNecessary(table_iter_.get(), scanned_rows, row_count_limit,
+                                          scan_time_exceeded, &schema, read_time,
+                                          has_paging_state));
   return fetched_rows;
 }
 

java/yb-pgsql/src/test/java/org/yb/pgsql/TestPgRegressAnalyze.java
@@ -1,0 +1,43 @@
+// Copyright (c) YugaByte, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+// in compliance with the License.  You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software distributed under the License
+// is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+// or implied.  See the License for the specific language governing permissions and limitations
+// under the License.
+//
+package org.yb.pgsql;
+
+import java.util.Map;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.yb.minicluster.MiniYBClusterBuilder;
+import org.yb.util.YBTestRunnerNonTsanOnly;
+
+import com.google.common.collect.ImmutableMap;
+
+/**
+ * Runs the pg_regress test suite on YB code.
+ */
+@RunWith(value=YBTestRunnerNonTsanOnly.class)
+public class TestPgRegressAnalyze extends BasePgSQLTest {
+
+  private Map<String, String> commonTserverFlags = ImmutableMap.of(
+      "ysql_beta_features", "1");
+
+  @Override
+  protected void customizeMiniClusterBuilder(MiniYBClusterBuilder builder) {
+    super.customizeMiniClusterBuilder(builder);
+    builder.addCommonTServerFlags(commonTserverFlags);
+  }
+
+  @Test
+  public void testPgRegressAnalyze() throws Exception {
+    runPgRegressTest("yb_analyze_schedule");
+  }
+}

src/postgres/src/backend/access/ybc/ybcam.c
@@ -1696,3 +1696,141 @@ HeapTuple YBCFetchTuple(Relation relation, Datum ybctid)
 	YBCPgDeleteStatement(ybc_stmt);
 	return tuple;
 }
+
+/*
+ * ANALYZE support: take random sample of a YB table data
+ */
+
+YbSample
+ybBeginSample(Relation rel, int targrows)
+{
+	ReservoirStateData rstate;
+	Oid dboid = YBCGetDatabaseOid(rel);
+	Oid relid = RelationGetRelid(rel);
+	TupleDesc tupdesc = RelationGetDescr(rel);
+	YbSample ybSample = (YbSample) palloc0(sizeof(YbSampleData));
+	ybSample->relation = rel;
+	ybSample->targrows = targrows;
+	ybSample->liverows = 0;
+	ybSample->deadrows = 0;
+
+	/*
+	 * Create new sampler command
+	 */
+	HandleYBStatus(YBCPgNewSample(dboid, relid, targrows, &ybSample->handle));
+
+	/*
+	 * Set up the scan targets. We need to return all "real" columns.
+	 */
+	if (RelationGetForm(ybSample->relation)->relhasoids)
+	{
+		YBCPgTypeAttrs type_attrs = { 0 };
+		YBCPgExpr	expr = YBCNewColumnRef(ybSample->handle,
+										   ObjectIdAttributeNumber,
+										   InvalidOid,
+										   &type_attrs);
+		HandleYBStatus(YBCPgDmlAppendTarget(ybSample->handle, expr));
+	}
+	for (AttrNumber attnum = 1; attnum <= tupdesc->natts; attnum++)
+	{
+		Form_pg_attribute att = TupleDescAttr(tupdesc, attnum - 1);
+		YBCPgTypeAttrs type_attrs = { att->atttypmod };
+		YBCPgExpr   expr = YBCNewColumnRef(ybSample->handle,
+										   attnum,
+										   att->atttypid,
+										   &type_attrs);
+		HandleYBStatus(YBCPgDmlAppendTarget(ybSample->handle, expr));
+	}
+
+	/*
+	 * Initialize sampler random state
+	 */
+	reservoir_init_selection_state(&rstate, targrows);
+	HandleYBStatus(YBCPgInitRandomState(ybSample->handle,
+										rstate.W,
+										SamplerRandomStateToUint64(rstate.randstate)));
+
+	return ybSample;
+}
+
+/*
+ * Sequentially scan next block of YB table and select rows for the sample.
+ * Block is a sequence of rows from one partition, up to specific number of
+ * rows or the end of the partition.
+ * Algorithm selects every scanned row until targrows are selected, then it
+ * select random rows, with decreasing probability, to replace one of the
+ * previously selected rows.
+ * The IDs of selected rows are stored in the internal buffer (reservoir).
+ * Scan ends and function returns false if one of two is true:
+ *  - end of the table is reached
+ *  or
+ *  - targrows are selected and end of a table partition is reached.
+ */
+bool
+ybSampleNextBlock(YbSample ybSample)
+{
+	bool has_more;
+	HandleYBStatus(YBCPgSampleNextBlock(ybSample->handle, &has_more));
+	return has_more;
+}
+
+/*
+ * Fetch the rows selected for the sample into pre-allocated buffer.
+ * Return number of rows fetched.
+ */
+int
+ybFetchSample(YbSample ybSample, HeapTuple *rows)
+{
+	Oid relid = RelationGetRelid(ybSample->relation);
+	TupleDesc	tupdesc = RelationGetDescr(ybSample->relation);
+	Datum	   *values = (Datum *) palloc0(tupdesc->natts * sizeof(Datum));
+	bool	   *nulls  = (bool *) palloc(tupdesc->natts * sizeof(bool));
+	int 		numrows;
+
+	/*
+	 * Execute equivalent of
+	 *   SELECT * FROM table WHERE ybctid IN [yctid0, ybctid1, ...];
+	 */
+	HandleYBStatus(YBCPgExecSample(ybSample->handle));
+	/*
+	 * Retrieve liverows and deadrows counters.
+	 * TODO: count deadrows
+	 */
+	HandleYBStatus(YBCPgGetEstimatedRowCount(ybSample->handle,
+											 &ybSample->liverows,
+											 &ybSample->deadrows));
+
+	for (numrows = 0; numrows < ybSample->targrows; numrows++)
+	{
+		bool has_data = false;
+		YBCPgSysColumns syscols;
+
+		/* Fetch one row. */
+		HandleYBStatus(YBCPgDmlFetch(ybSample->handle,
+									 tupdesc->natts,
+									 (uint64_t *) values,
+									 nulls,
+									 &syscols,
+									 &has_data));
+
+		if (!has_data)
+			break;
+
+		/* Make a heap tuple in current memory context */
+		rows[numrows] = heap_form_tuple(tupdesc, values, nulls);
+		if (syscols.oid != InvalidOid)
+		{
+			HeapTupleSetOid(rows[numrows], syscols.oid);
+		}
+		if (syscols.ybctid != NULL)
+		{
+			rows[numrows]->t_ybctid = PointerGetDatum(syscols.ybctid);
+		}
+		rows[numrows]->t_tableOid = relid;
+	}
+	pfree(values);
+	pfree(nulls);
+	/* Close the DocDB statement */
+	YBCPgDeleteStatement(ybSample->handle);
+	return numrows;
+}

src/postgres/src/backend/commands/analyze.c
@@ -23,6 +23,7 @@
 #include "access/tuptoaster.h"
 #include "access/visibilitymap.h"
 #include "access/xact.h"
+#include "access/ybcam.h"
 #include "catalog/catalog.h"
 #include "catalog/index.h"
 #include "catalog/indexing.h"
@@ -98,6 +99,9 @@ static int	compare_rows(const void *a, const void *b);
 static int acquire_inherited_sample_rows(Relation onerel, int elevel,
 							  HeapTuple *rows, int targrows,
 							  double *totalrows, double *totaldeadrows);
+static int yb_acquire_sample_rows(Relation onerel, int elevel,
+					   HeapTuple *rows, int targrows,
+					   double *totalrows, double *totaldeadrows);
 static void update_attstats(Oid relid, bool inh,
 				int natts, VacAttrStats **vacattrstats);
 static Datum std_fetch_func(VacAttrStatsP stats, int rownum, bool *isNull);
@@ -245,8 +249,13 @@ analyze_rel(Oid relid, RangeVar *relation, int options,
 	/*
 	 * Check that it's of an analyzable relkind, and set up appropriately.
 	 */
-	if (onerel->rd_rel->relkind == RELKIND_RELATION ||
-		onerel->rd_rel->relkind == RELKIND_MATVIEW)
+	if (IsYBRelation(onerel))
+	{
+		acquirefunc = yb_acquire_sample_rows;
+		relpages = 0;
+	}
+	else if (onerel->rd_rel->relkind == RELKIND_RELATION ||
+			 onerel->rd_rel->relkind == RELKIND_MATVIEW)
 	{
 		/* Regular table, so we'll use the regular row acquisition function */
 		acquirefunc = acquire_sample_rows;
@@ -367,16 +376,6 @@ do_analyze_rel(Relation onerel, int options, VacuumParams *params,
 	Oid			save_userid;
 	int			save_sec_context;
 	int			save_nestlevel;
-
-	/*
-	 * ANALYZE not supported for Yugabyte relations.
-	 */
-	if (IsYBRelation(onerel))
-	{
-		ereport(WARNING,
-				(errmsg("analyzing non-temporary tables will be ignored")));
-		return;
-	}
 
 	if (inh)
 		ereport(elevel,
@@ -619,7 +618,7 @@ do_analyze_rel(Relation onerel, int options, VacuumParams *params,
 
 		/*
 		 * Emit the completed stats rows into pg_statistic, replacing any
-		 * previous statistics for the target columns.  (If there are stats in
+		 * previous statistics for the target columns. (If there are stats in
 		 * pg_statistic for columns we didn't process, we leave them alone.)
 		 */
 		update_attstats(RelationGetRelid(onerel), inh,
@@ -1555,6 +1554,44 @@ acquire_inherited_sample_rows(Relation onerel, int elevel,
 	return numrows;
 }
 
+/*
+ * yb_acquire_sample_rows -- acquire a random sample of rows from the YB table
+ *
+ * This has the same API as acquire_sample_rows, and uses similar approach.
+ */
+static int
+yb_acquire_sample_rows(Relation onerel, int elevel,
+					   HeapTuple *rows, int targrows,
+					   double *totalrows, double *totaldeadrows)
+{
+	YbSample	ybSample;
+	int			numrows = 0;
+
+	Assert(targrows > 0);
+
+	/* Prepare to take sample */
+	ybSample = ybBeginSample(onerel, targrows);
+
+	/* Loop over the table blocks until sample is selected */
+	while (ybSampleNextBlock(ybSample)) {
+		vacuum_delay_point();
+	}
+
+	/* Fetch selected rows */
+	numrows = ybFetchSample(ybSample, rows);
+
+	/* Get row counters */
+	*totalrows = ybSample->liverows + ybSample->deadrows;
+	*totaldeadrows = ybSample->deadrows;
+
+	ereport(elevel,
+			(errmsg("\"%s\": scanned, "
+					"%d rows in sample, %.0f estimated total rows",
+					RelationGetRelationName(onerel),
+					numrows, *totalrows)));
+
+	return numrows;
+}
 
 /*
  *	update_attstats() -- update attribute statistics for one relation

src/postgres/src/backend/commands/vacuum.c
@@ -180,7 +180,14 @@ vacuum(int options, List *relations, VacuumParams *params,
 	{
 		ereport(WARNING,
 				(errmsg("VACUUM will be ignored")));
-		return;
+		if (options & VACOPT_ANALYZE)
+		{
+			options &= ~VACOPT_VACUUM;
+		}
+		else
+		{
+			return;
+		}
 	}
 
 	Assert(params != NULL);

src/postgres/src/backend/utils/misc/pg_yb_utils.c
@@ -1197,6 +1197,12 @@ bool IsTransactionalDdlStatement(PlannedStmt *pstmt,
 			*is_breaking_catalog_change = false;
 			return true;
 
+		case T_VacuumStmt:
+			/* Vacuum with analyze updates relation and attribute statistics */
+			*is_catalog_version_increment = false;
+			*is_breaking_catalog_change = false;
+			return castNode(VacuumStmt, parsetree)->options & VACOPT_ANALYZE;
+
 		default:
 			/* Not a DDL operation. */
 			*is_catalog_version_increment = false;

commit a8f972002b589a87eb3af06057a0fab2de70b79d

[YSQL] Fix Mac build after ANALYZE support commit
Summary:
Commit 638f4188da8b3f81a595eea7b7e45f2ed97104ab broke the Mac build by
using PG types from Yugabyte C++ codebase.
To fix, introduce corresponding interface types on ybgate_api.h to use instead.

Test Plan: Jenkins

Reviewers: neil, sergei, amartsinchyk

Reviewed By: amartsinchyk

Subscribers: eng

Differential Revision: https://phabricator.dev.yugabyte.com/D12452

src/postgres/src/backend/ybgate/ybgate_api.c
@@ -29,6 +29,7 @@
 #include "nodes/primnodes.h"
 #include "utils/memutils.h"
 #include "utils/numeric.h"
+#include "utils/sampling.h"
 
 //-----------------------------------------------------------------------------
 // Memory Context
@@ -262,20 +263,39 @@ YbgStatus YbgSplitArrayDatum(uint64_t datum,
 // Relation sampling
 //-----------------------------------------------------------------------------
 
-YbgStatus YbgSamplerRandomFract(SamplerRandomState randstate, double *value)
+struct YbgReservoirStateData {
+	ReservoirStateData rs;
+};
+
+YbgStatus YbgSamplerCreate(double rstate_w, uint64_t randstate, YbgReservoirState *yb_rs) 
 {
 	PG_SETUP_ERROR_REPORTING();
-
-	*value = sampler_random_fract(randstate);
-
+	YbgReservoirState rstate = (YbgReservoirState) palloc0(sizeof(struct YbgReservoirStateData));
+	rstate->rs.W = rstate_w;
+	Uint64ToSamplerRandomState(rstate->rs.randstate, randstate);
+	*yb_rs = rstate;
 	return PG_STATUS_OK;
 }
 
-YbgStatus YbgReservoirGetNextS(ReservoirState rs, double t, int n, double *s)
+YbgStatus YbgSamplerGetState(YbgReservoirState yb_rs, double *rstate_w, uint64_t *randstate)
 {
 	PG_SETUP_ERROR_REPORTING();
+	*rstate_w = yb_rs->rs.W;
+	*randstate = SamplerRandomStateToUint64(yb_rs->rs.randstate);
+	return PG_STATUS_OK;
+}
 
-	*s = reservoir_get_next_S(rs, t, n);
+YbgStatus YbgSamplerRandomFract(YbgReservoirState yb_rs, double *value)
+{
+	PG_SETUP_ERROR_REPORTING();
+	ReservoirState rs = &yb_rs->rs;
+	*value = sampler_random_fract(rs->randstate);
+	return PG_STATUS_OK;
+}
 
+YbgStatus YbgReservoirGetNextS(YbgReservoirState yb_rs, double t, int n, double *s)
+{
+	PG_SETUP_ERROR_REPORTING();
+	*s = reservoir_get_next_S(&yb_rs->rs, t, n);
 	return PG_STATUS_OK;
 }

src/postgres/src/include/ybgate/ybgate_api.h
@@ -24,8 +24,6 @@
 #include <stddef.h>
 #include <stdint.h>
 #include "yb/yql/pggate/ybc_pg_typedefs.h"
-#include "yb/gutil/integral_types.h"
-#include "utils/sampling.h"
 
 #ifdef __cplusplus
 extern "C" {
@@ -143,16 +141,32 @@ YbgStatus YbgSplitArrayDatum(uint64_t datum, int type, uint64_t **result_datum_a
 // Relation sampling
 //-----------------------------------------------------------------------------
 
+#ifdef __cplusplus
+typedef void* YbgReservoirState;
+#else
+typedef struct YbgReservoirStateData* YbgReservoirState;
+#endif
+
+/*
+ * Allocate and initialize a YbgReservoirState.
+ */
+YbgStatus YbgSamplerCreate(double rstate_w, uint64_t randstate, YbgReservoirState *yb_rs);
+
+/*
+ * Allocate and initialize a YbgReservoirState.
+ */
+YbgStatus YbgSamplerGetState(YbgReservoirState yb_rs, double *rstate_w, uint64_t *randstate);
+
 /*
  * Select a random value R uniformly distributed in (0 - 1)
  */
-YbgStatus YbgSamplerRandomFract(SamplerRandomState randstate, double *value);
+YbgStatus YbgSamplerRandomFract(YbgReservoirState yb_rs, double *value);
 
 /*
  * Calculate next number of rows to skip based on current number of scanned rows
  * and requested sample size.
  */
-YbgStatus YbgReservoirGetNextS(ReservoirState rs, double t, int n, double *s);
+YbgStatus YbgReservoirGetNextS(YbgReservoirState yb_rs, double t, int n, double *s);
 
 #ifdef __cplusplus
 }

src/yb/docdb/pgsql_operation.cc
@@ -698,9 +698,9 @@ Result<size_t> PgsqlReadOperation::ExecuteSample(const common::YQLStorageIf& ql_
   // Current number of rows to skip before collecting next one for sample
   double rowstoskip = sampling_state.rowstoskip();
   // Variables for the random numbers generator
-  ReservoirStateData rstate;
-  rstate.W = sampling_state.rstate_w();
-  Uint64ToSamplerRandomState(rstate.randstate, sampling_state.rand_state());
+  YbgPrepareMemoryContext();
+  YbgReservoirState rstate = NULL;
+  YbgSamplerCreate(sampling_state.rstate_w(), sampling_state.rand_state(), &rstate);
   // Buffer to hold selected row ids from the current page
   std::unique_ptr<QLValuePB[]> reservoir = std::make_unique<QLValuePB[]>(targrows);
   // Number of rows to scan for the current page.
@@ -742,12 +742,12 @@ Result<size_t> PgsqlReadOperation::ExecuteSample(const common::YQLStorageIf& ql_
         // Pick random tuple in the reservoir to replace
         double rvalue;
         int k;
-        YbgSamplerRandomFract(rstate.randstate, &rvalue);
+        YbgSamplerRandomFract(rstate, &rvalue);
         k = static_cast<int>(targrows * rvalue);
         // Overwrite previous value with new one
         reservoir[k].set_binary_value(ybctid.data(), ybctid.size());
         // Choose next number of rows to skip
-        YbgReservoirGetNextS(&rstate, samplerows, targrows, &rowstoskip);
+        YbgReservoirGetNextS(rstate, samplerows, targrows, &rowstoskip);
       } else {
         rowstoskip -= 1;
       }
@@ -783,8 +783,12 @@ Result<size_t> PgsqlReadOperation::ExecuteSample(const common::YQLStorageIf& ql_
   new_sampling_state->set_targrows(targrows);
   new_sampling_state->set_samplerows(samplerows);
   new_sampling_state->set_rowstoskip(rowstoskip);
-  new_sampling_state->set_rstate_w(rstate.W);
-  new_sampling_state->set_rand_state(SamplerRandomStateToUint64(rstate.randstate));
+  uint64_t randstate = 0;
+  double rstate_w = 0;
+  YbgSamplerGetState(rstate, &rstate_w, &randstate);
+  new_sampling_state->set_rstate_w(rstate_w);
+  new_sampling_state->set_rand_state(randstate);
+  YbgDeleteMemoryContext();
 
   // Return paging state if scan has not been completed
   RETURN_NOT_OK(SetPagingStateIfNecessary(table_iter_.get(), scanned_rows, row_count_limit,