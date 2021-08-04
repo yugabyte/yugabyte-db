yugabyte/yugabyte-db

commit 3e5edfae71079c67ab91f2cf5b5dff9007f75118

[#9550] ysql: output NOTICE when CREATE INDEX in txn block
Summary:
There exists a DEBUG1 error message that indicates that the index is not
built concurrently when it is created in a transaction block.  To
increase visibility of this message, change it to NOTICE.  Also, reword
it better.

Close: #9550

Test Plan:
Jenkins: build platform: centos, compiler: clang, build type: debug

    ./yb_build.sh --java-test TestPgRegressTable
    ./yb_build.sh --java-test TestPgRegressTypesUDT

Reviewers: mihnea

Reviewed By: mihnea

Subscribers: yql

Differential Revision: https://phabricator.dev.yugabyte.com/D12447

src/postgres/src/backend/tcop/utility.c
@@ -1373,11 +1373,14 @@ ProcessUtilitySlow(ParseState *pstate,
 							 * build.
 							 * TODO(jason): heed issue #6240.
 							 */
-							ereport(DEBUG1,
+							ereport(NOTICE,
 									(errmsg("making create index for table "
-											"\"%s\" in transaction block "
-											"nonconcurrent",
-											stmt->relation->relname)));
+											"\"%s\" nonconcurrent",
+											stmt->relation->relname),
+									 errdetail("Create index in transaction"
+											   " block cannot be concurrent."),
+									 errhint("Consider running it outside of a"
+											 " transaction block. See https://github.com/yugabyte/yugabyte-db/issues/6240.")));
 							stmt->concurrent = false;
 						}
 						else

src/postgres/src/test/regress/expected/yb_create_table.out
@@ -708,3 +708,9 @@ set yb_enable_create_with_table_oid=0;
 create table with_table_oid_variable_false (a int) with (table_oid = 55555);
 ERROR:  create table with table_oid is not allowed
 HINT:  Try enabling the session variable yb_enable_create_with_table_oid.
+RESET yb_enable_create_with_table_oid;
+-- CREATE TABLE with implicit UNIQUE INDEX shouldn't spout a notice about it
+-- being nonconcurrent.
+BEGIN;
+CREATE TABLE tab_with_unique (i int, UNIQUE (i));
+COMMIT;

src/postgres/src/test/regress/expected/yb_pg_create_index.out
@@ -27,3 +27,30 @@ CREATE INDEX iix ON ihighway USING lsm (name text_ops);
 ERROR:  relation "ihighway" does not exist
 CREATE INDEX six ON shighway USING lsm (name text_ops);
 ERROR:  relation "shighway" does not exist
+--
+-- Try some concurrent index builds
+--
+-- Unfortunately this only tests about half the code paths because there are
+-- no concurrent updates happening to the table at the same time.
+CREATE TABLE concur_heap (f1 text, f2 text);
+-- empty table
+CREATE INDEX CONCURRENTLY concur_index1 ON concur_heap(f2,f1);
+ERROR:  CREATE INDEX CONCURRENTLY not supported yet
+LINE 1: CREATE INDEX CONCURRENTLY concur_index1 ON concur_heap(f2,f1...
+                     ^
+HINT:  Please report the issue on https://github.com/YugaByte/yugabyte-db/issues
+-- You can't do a concurrent index build in a transaction
+BEGIN;
+CREATE INDEX CONCURRENTLY concur_index7 ON concur_heap(f1);
+ERROR:  CREATE INDEX CONCURRENTLY not supported yet
+LINE 1: CREATE INDEX CONCURRENTLY concur_index7 ON concur_heap(f1);
+                     ^
+HINT:  Please report the issue on https://github.com/YugaByte/yugabyte-db/issues
+COMMIT;
+-- But you can do a regular index build in a transaction
+BEGIN;
+CREATE INDEX std_index on concur_heap(f2);
+NOTICE:  making create index for table "concur_heap" nonconcurrent
+DETAIL:  Create index in transaction block cannot be concurrent.
+HINT:  Consider running it outside of a transaction block. See https://github.com/yugabyte/yugabyte-db/issues/6240.
+COMMIT;

src/postgres/src/test/regress/sql/yb_create_table.sql
@@ -541,3 +541,10 @@ select relname, oid from pg_class where relname = 'with_table_oid_2';
 -- Test with session variable off
 set yb_enable_create_with_table_oid=0;
 create table with_table_oid_variable_false (a int) with (table_oid = 55555);
+RESET yb_enable_create_with_table_oid;
+
+-- CREATE TABLE with implicit UNIQUE INDEX shouldn't spout a notice about it
+-- being nonconcurrent.
+BEGIN;
+CREATE TABLE tab_with_unique (i int, UNIQUE (i));
+COMMIT;

src/postgres/src/test/regress/sql/yb_pg_create_index.sql
@@ -37,3 +37,23 @@ CREATE INDEX rix ON road USING lsm (name text_ops);
 CREATE INDEX iix ON ihighway USING lsm (name text_ops);
 
 CREATE INDEX six ON shighway USING lsm (name text_ops);
+
+--
+-- Try some concurrent index builds
+--
+-- Unfortunately this only tests about half the code paths because there are
+-- no concurrent updates happening to the table at the same time.
+
+CREATE TABLE concur_heap (f1 text, f2 text);
+-- empty table
+CREATE INDEX CONCURRENTLY concur_index1 ON concur_heap(f2,f1);
+
+-- You can't do a concurrent index build in a transaction
+BEGIN;
+CREATE INDEX CONCURRENTLY concur_index7 ON concur_heap(f1);
+COMMIT;
+
+-- But you can do a regular index build in a transaction
+BEGIN;
+CREATE INDEX std_index on concur_heap(f2);
+COMMIT;

commit fc3b0f97ffa28278dfb30ad020e573511cf9a941

test: fix yb_large_copy_ddl, yb_pg_triggers output
Summary:
Commit 3e5edfae71079c67ab91f2cf5b5dff9007f75118 broke some tests.  Fix
`yb_large_copy_ddl` and `yb_pg_triggers` regress test output to show
notice on CREATE INDEX in txn block.  This should unbreak Java tests
`TestPgRegressLargeTable` and `TestPgRegressTrigger`, respectively.

Test Plan:
Jenkins: build platform: centos, build type: release, urgent

    ./yb_build.sh release --java-test TestPgRegressLargeTable
    ./yb_build.sh release --java-test TestPgRegressTrigger

Reviewers: mihnea

Reviewed By: mihnea

Subscribers: yql

Differential Revision: https://phabricator.dev.yugabyte.com/D12480

src/postgres/src/test/regress/expected/yb_pg_triggers.out
@@ -1770,6 +1770,9 @@ begin
   return new;
 end$$ language plpgsql;
 insert into trigger_ddl_table values (1, 42);  -- fail
+NOTICE:  making create index for table "trigger_ddl_table" nonconcurrent
+DETAIL:  Create index in transaction block cannot be concurrent.
+HINT:  Consider running it outside of a transaction block. See https://github.com/yugabyte/yugabyte-db/issues/6240.
 ERROR:  cannot CREATE INDEX "trigger_ddl_table" because it is being used by active queries in this session
 CONTEXT:  SQL statement "create index on trigger_ddl_table (col2)"
 PL/pgSQL function trigger_ddl_func() line 3 at SQL statement

src/postgres/src/test/regress/output/yb_large_copy_ddl.source
@@ -3,6 +3,12 @@
 --
 BEGIN;
 CREATE INDEX ON airports (type);
+NOTICE:  making create index for table "airports" nonconcurrent
+DETAIL:  Create index in transaction block cannot be concurrent.
+HINT:  Consider running it outside of a transaction block. See https://github.com/yugabyte/yugabyte-db/issues/6240.
 CREATE INDEX ON airports (name);
+NOTICE:  making create index for table "airports" nonconcurrent
+DETAIL:  Create index in transaction block cannot be concurrent.
+HINT:  Consider running it outside of a transaction block. See https://github.com/yugabyte/yugabyte-db/issues/6240.
 COPY airports FROM '@abs_srcdir@/data/airport-codes.csv' CSV HEADER;
 COMMIT;