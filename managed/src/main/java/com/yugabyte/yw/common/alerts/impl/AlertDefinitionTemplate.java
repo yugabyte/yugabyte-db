7bee16ac0583bdcb624166b1c07faa6c81790f58
Aleksandr Malyshev <amalyshev@yugabyte.com>
2021-08-13 17:33:38 +0300

    [#9667] Additional validations and filters for alert and alert definition group service
    Summary:
    Added an ability to filter alert definition groups by target name prefix.
    Created indexes to make alert UI and backend queries work fast with large number of alerts.
    Added target UUID and target type validation is added for alert definition group operations.
    Changed request parsing error code to 400 bad request to make sure our API is consistent to API recommendations.
    As additional capability - garbage collection for old resolved alerts (older than 120 days) is implemented.
    
    Test Plan:
    Test new alert definition group filter and validations via API.
    Create resolved alert and set resolution date as 121 days in the past. Restart platform. Old resolved alert should disappear.
    
    Reviewers: spotachev
    
    Reviewed By: spotachev
    
    Subscribers: jenkins-bot, yugaware
    
    Differential Revision: https://phabricator.dev.yugabyte.com/D12558

---
+++ managed/src/main/java/com/yugabyte/yw/common/alerts/impl/AlertDefinitionTemplate.java
@@ -0,0 +1,37 @@
+/*
+ * Copyright 2021 YugaByte, Inc. and Contributors
+ *
+ * Licensed under the Polyform Free Trial License 1.0.0 (the "License"); you
+ * may not use this file except in compliance with the License. You
+ * may obtain a copy of the License at
+ *
+ * http://github.com/YugaByte/yugabyte-db/blob/master/licenses/POLYFORM-FREE-TRIAL-LICENSE-1.0.0.txt
+ */
+package com.yugabyte.yw.common.alerts.impl;
+
+import static io.swagger.annotations.ApiModelProperty.AccessMode.READ_ONLY;
+
+import com.fasterxml.jackson.annotation.JsonUnwrapped;
+import com.yugabyte.yw.models.AlertDefinitionGroup;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+import lombok.experimental.Accessors;
+
+@Data
+@Accessors(chain = true)
+@ApiModel(description = "Alert configuration template.")
+public class AlertDefinitionTemplate {
+  @ApiModelProperty(value = "Alert definition group with template defaults", accessMode = READ_ONLY)
+  @JsonUnwrapped
+  private AlertDefinitionGroup defaultGroup;
+
+  @ApiModelProperty(value = "Alert threshold minimal value", accessMode = READ_ONLY)
+  private double thresholdMinValue;
+
+  @ApiModelProperty(value = "Alert threshold maximal value", accessMode = READ_ONLY)
+  private double thresholdMaxValue;
+
+  @ApiModelProperty(value = "Is alert threshold integer or floating point", accessMode = READ_ONLY)
+  private boolean thresholdInteger;
+}
