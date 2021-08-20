4aac9101db18bbd1c60d986e4ed46ead694feb7f
Jason Vigil <jvigil@yugabyte.com>
2021-08-20 07:27:31 -0700

    [PLAT-1522] Support downloading releases directly from GCS
    Summary:
    Similar to downloading releases directly from S3/HTTP, this commit adds
    the capability to specify a GCS release location. When configured (via
    api call to `PLATFORM_URL/api/customers/[Customer UUID]/releases`), the
    database nodes will download the release tarball directly from GCS.
    
    Test Plan:
    - Create release with GCS location / credentials
    - Create universe (via GCS release)
    - Unit tests
    
    Reviewers: spotachev, sanketh
    
    Reviewed By: sanketh
    
    Subscribers: yugaware, jenkins-bot
    
    Differential Revision: https://phabricator.dev.yugabyte.com/D12663

--- managed/src/main/java/com/yugabyte/yw/forms/ReleaseFormData.java
+++ managed/src/main/java/com/yugabyte/yw/forms/ReleaseFormData.java
@@ -2,6 +2,7 @@
 
 package com.yugabyte.yw.forms;
 
+import com.yugabyte.yw.common.ReleaseManager.ReleaseMetadata.GCSLocation;
 import com.yugabyte.yw.common.ReleaseManager.ReleaseMetadata.HttpLocation;
 import com.yugabyte.yw.common.ReleaseManager.ReleaseMetadata.S3Location;
 import io.swagger.annotations.ApiModel;
@@ -12,6 +13,8 @@ public class ReleaseFormData {
   public String version;
 
   @Valid public S3Location s3;
+
+  @Valid public GCSLocation gcs;
 
   @Valid public HttpLocation http;
 }
