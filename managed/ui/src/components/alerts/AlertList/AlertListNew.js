4cd04e9a2cbd66569d424a49de8bd31d31beaf8f
Karthik K <kkannan@yugabyte.com>
2021-08-25 10:52:57 +0530

    [9733][Platform][Alerts] - Implement alert listing
    Summary:
    This diff implements a new alert listing UI, when you can filter, sort, view details and acknowledge alerts.
    Operations like filtering, sort , etc is done at the server side.
    
    Test Plan:
    Tested on portal by toggling between different filters and sorting.
    
    image3.png{F19097}
    
    image2.png{F19098}
    
    image1.png{F19099}
    
    image4.png{F19100}
    
    Reviewers: mjoshi, sshevchenko, andrew, ssutar
    
    Reviewed By: ssutar
    
    Subscribers: jenkins-bot, ui
    
    Differential Revision: https://phabricator.dev.yugabyte.com/D12594

---
+++ managed/ui/src/components/alerts/AlertList/AlertListNew.js
@@ -0,0 +1,132 @@
+import React, { Component } from 'react';
+import { Row, Col } from 'react-bootstrap';
+import { YBCheckBox, YBSelectWithLabel } from '../../common/forms/fields';
+import AlertsTable from './AlertsTable';
+
+import './AlertListNew.scss';
+
+const FILTER_TYPES = {
+  states: {
+    label: 'Target State',
+    values: ['Active', 'Acknowledged', 'Resolved']
+  },
+
+  severities: {
+    label: 'Severity',
+    values: ['Severe', 'Warning']
+  },
+
+  groupTypes: {
+    label: 'Group Type',
+    values: ['Customer', 'Universe']
+  }
+};
+
+const getFilter = (filter_text, onClickFn) => (
+  <YBCheckBox onClick={onClickFn} label={<span className="checkbox-label">{filter_text}</span>} />
+);
+
+export class AlertListNew extends Component {
+  state = {
+    filter_groups: {},
+    universesList: []
+  };
+
+  componentDidMount() {
+    this.props.fetchUniverseList().then((data) => {
+      const universesList = [
+        <option value="" key={'unSelected'}>
+          Select an universe
+        </option>,
+        ...data.map((universe) => (
+          <option value={universe.name} key={universe.universeUUID}>
+            {universe.name}
+          </option>
+        ))
+      ];
+
+      this.setState({
+        universesList
+      });
+    });
+  }
+
+  updateTargetName = (value) => {
+    const { filter_groups } = this.state;
+    filter_groups['targetName'] = value;
+    this.setState({
+      filter_groups
+    });
+  };
+
+  updateFilters = (group, filter, isChecked) => {
+    const { filter_groups } = this.state;
+
+    if (!filter_groups[group]) {
+      filter_groups[group] = [];
+    }
+
+    if (isChecked) {
+      filter_groups[group] = [...filter_groups[group], filter.toUpperCase()];
+    } else {
+      filter_groups[group] = filter_groups[group].filter((f) => f !== filter.toUpperCase());
+    }
+
+    this.setState({
+      filter_groups
+    });
+  };
+
+  render() {
+    const { filter_groups, universesList } = this.state;
+
+    return (
+      <div>
+        <h2 className="content-title">Alerts</h2>
+        <Row className="alerts-page">
+          <Col lg={12} className="alerts-container">
+            <Row>
+              <Col className="alerts-filters" lg={2}>
+                <h3>Filters</h3>
+                {Object.keys(FILTER_TYPES).map((filter_key) => (
+                  <Row className="filter-group" key={filter_key}>
+                    <Col lg={12}>
+                      <h6 className="to-uppercase">{FILTER_TYPES[filter_key].label}</h6>
+                      {FILTER_TYPES[filter_key]['values'].map((filter) => {
+                        return (
+                          <Row key={filter}>
+                            <Col lg={12} lgOffset={1} className="noMargin">
+                              {getFilter(filter, (e) => {
+                                this.updateFilters(filter_key, filter, e.target.checked);
+                              })}
+                            </Col>
+                          </Row>
+                        );
+                      })}
+                    </Col>
+                  </Row>
+                ))}
+                <Row className="filter-group">
+                  <Col lg={12}>
+                    <h6 className="to-uppercase">Universes</h6>
+                    <Row>
+                      <Col lg={12} lgOffset={1} className="noMargin">
+                        <YBSelectWithLabel
+                          options={universesList}
+                          onInputChanged={this.updateTargetName}
+                        />
+                      </Col>
+                    </Row>
+                  </Col>
+                </Row>
+              </Col>
+              <Col lg={10}>
+                <AlertsTable filters={filter_groups} />
+              </Col>
+            </Row>
+          </Col>
+        </Row>
+      </div>
+    );
+  }
+}
