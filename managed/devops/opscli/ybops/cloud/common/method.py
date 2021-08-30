yugabyte/yugabyte-db

commit 4aac9101db18bbd1c60d986e4ed46ead694feb7f

[PLAT-1522] Support downloading releases directly from GCS
Summary:
Similar to downloading releases directly from S3/HTTP, this commit adds
the capability to specify a GCS release location. When configured (via
api call to `PLATFORM_URL/api/customers/[Customer UUID]/releases`), the
database nodes will download the release tarball directly from GCS.

Test Plan:
- Create release with GCS location / credentials
- Create universe (via GCS release)
- Unit tests

Reviewers: spotachev, sanketh

Reviewed By: sanketh

Subscribers: yugaware, jenkins-bot

Differential Revision: https://phabricator.dev.yugabyte.com/D12663

managed/devops/opscli/ybops/cloud/common/method.py
@@ -668,6 +668,8 @@ class ConfigureInstancesMethod(AbstractInstancesMethod):
         self.parser.add_argument('--s3_remote_download', action="store_true")
         self.parser.add_argument('--aws_access_key')
         self.parser.add_argument('--aws_secret_key')
+        self.parser.add_argument('--gcs_remote_download', action="store_true")
+        self.parser.add_argument('--gcs_credentials_json')
         self.parser.add_argument('--http_remote_download', action="store_true")
         self.parser.add_argument('--http_package_checksum', default='')
 
@@ -782,6 +784,26 @@ class ConfigureInstancesMethod(AbstractInstancesMethod):
                     self.extra_vars['s3_package_path'] = args.package
                     self.extra_vars['aws_access_key'] = aws_access_key
                     self.extra_vars['aws_secret_key'] = aws_secret_key
+                    logging.info(
+                        "Variables to download {} directly on the remote host added."
+                        .format(args.package))
+                if args.gcs_remote_download:
+                    gcs_credentials_json = args.gcs_credentials_json or \
+                                           os.getenv('GCS_CREDENTIALS_JSON')
+
+                    if gcs_credentials_json is None:
+                        raise YBOpsRuntimeError("GCS credentials are not specified, nor found in " +
+                                                "the environment to download YB package from {}"
+                                                .format(args.package))
+
+                    gcs_uri_pattern = r"^gs:\/\/(?:[^\/]+)\/(?:.+)$"
+                    match = re.match(gcs_uri_pattern, args.package)
+                    if not match:
+                        raise YBOpsRuntimeError("{} is not a valid gs URI. Must match {}"
+                                                .format(args.package, gcs_uri_pattern))
+
+                    self.extra_vars['gcs_package_path'] = args.package
+                    self.extra_vars['gcs_credentials_json'] = gcs_credentials_json
                     logging.info(
                         "Variables to download {} directly on the remote host added."
                         .format(args.package))

managed/devops/roles/install_package/tasks/download-package.yml
@@ -29,6 +29,37 @@
     AWS_SECRET_ACCESS_KEY: "{{ aws_secret_key | default(lookup('env', 'AWS_SECRET_ACCESS_KEY')) }}"
   when: s3_package_path is defined
 
+- name: Download package | Create credentials file and download "{{ package_name }}" from GCS
+  block:
+    - name: Download package | Create GCS credentials file
+      tempfile:
+        path: "{{ yb_home_dir }}"
+      register: gcs_credentials_json_file
+    - name: Download package | Copy GCS credentials to file
+      template:
+        src: gcs_credentials_json.j2
+        dest: "{{ gcs_credentials_json_file.path }}"
+        owner: "{{ user_name }}"
+        group: "{{ user_name }}"
+        mode: 0600
+    - name: Download package | Download "{{ package_name }} from GCS
+      command:
+        argv:
+          - "gsutil"
+          - "-o"
+          - "Credentials:gs_service_key_file={{ gcs_credentials_json_file.path }}"
+          - "cp"
+          - "{{ gcs_package_path }}"
+          - "{{ remote_tmp_package }}"
+        creates: "{{ remote_tmp_package }}"
+  always:
+    - name: Download package | Cleanup GCS credentials file
+      when: gcs_credentials_json_file is defined and gcs_credentials_json_file.path != ""
+      file:
+        path: "{{ gcs_credentials_json_file.path }}"
+        state: absent
+  when: gcs_package_path is defined
+
 - name: Download package | Download "{{ package_name }}" from a HTTP(S) link
   get_url:
     url: "{{ http_package_path }}"

managed/devops/roles/install_package/templates/gcs_credentials_json.j2
@@ -1,0 +1,1 @@
+{{ gcs_credentials_json }}

managed/src/main/java/com/yugabyte/yw/common/NodeManager.java
@@ -487,6 +487,11 @@ public class NodeManager extends DevopsBase {
           subcommand.add(releaseMetadata.s3.accessKeyId);
           subcommand.add("--aws_secret_key");
           subcommand.add(releaseMetadata.s3.secretAccessKey);
+        } else if (releaseMetadata.gcs != null) {
+          subcommand.add("--gcs_remote_download");
+          ybServerPackage = releaseMetadata.gcs.paths.x86_64;
+          subcommand.add("--gcs_credentials_json");
+          subcommand.add(releaseMetadata.gcs.credentialsJson);
         } else if (releaseMetadata.http != null) {
           subcommand.add("--http_remote_download");
           ybServerPackage = releaseMetadata.http.paths.x86_64;

managed/src/main/java/com/yugabyte/yw/common/ReleaseManager.java
@@ -72,7 +72,11 @@ public class ReleaseManager {
     @ApiModelProperty(value = "S3 link and credentials")
     public S3Location s3;
 
-    // S3 link and credentials for remote downloading of the release
+    // GCS link and credentials for remote downloading of the release
+    @ApiModelProperty(value = "GCS link and credentials")
+    public GCSLocation gcs;
+
+    // HTTP link for remote downloading of the release
     @ApiModelProperty(value = "HTTP link to the release")
     public HttpLocation http;
 
@@ -81,8 +85,8 @@ public class ReleaseManager {
     public static class PackagePaths {
       @ApiModelProperty(value = "Path to x86_64 package")
       @Constraints.Pattern(
-          message = "Must be prefixed with s3:// or http(s)://",
-          value = "\\b(?:(https|s3):\\/\\/).+\\b")
+          message = "Must be prefixed with s3://, gs://, or http(s)://",
+          value = "\\b(?:(https|s3|gs):\\/\\/).+\\b")
       public String x86_64;
 
       @ApiModelProperty(required = false, value = "Checksum for x86_64 package")
@@ -114,6 +118,20 @@ public class ReleaseManager {
       @ApiModelProperty(value = "access key secret", hidden = true)
       @Constraints.Required
       public String secretAccessKey;
+    }
+
+    @lombok.Getter
+    @lombok.Setter
+    public static class GCSLocation {
+      @ApiModelProperty(value = "package paths")
+      @Constraints.Required
+      @Valid
+      public PackagePaths paths;
+
+      // S3 credentials.
+      @ApiModelProperty(value = "gcs service key json", hidden = true)
+      @Constraints.Required
+      public String credentialsJson;
     }
 
     @lombok.Getter
@@ -231,8 +249,8 @@ public class ReleaseManager {
       }
 
       // At list one link should be available.
-      if (versionData.s3 == null && versionData.http == null) {
-        throw new RuntimeException("S3 link or HTTP link must be specified");
+      if (versionData.s3 == null && versionData.gcs == null && versionData.http == null) {
+        throw new RuntimeException("S3 link, GCS link, or HTTP link must be specified");
       }
 
       if (versionData.s3 != null) {
@@ -246,6 +264,19 @@ public class ReleaseManager {
         }
         if (!versionData.s3.paths.x86_64.startsWith("s3://")) {
           throw new RuntimeException("S3 path should be prefixed with s3://");
+        }
+      }
+
+      if (versionData.gcs != null) {
+        if (versionData.gcs.paths == null) {
+          throw new RuntimeException("No paths provided for GCS packages");
+        }
+        if (StringUtils.isBlank(versionData.gcs.paths.x86_64)
+            || StringUtils.isBlank(versionData.gcs.credentialsJson)) {
+          throw new RuntimeException("GCS needs non-empty path and credentials JSON");
+        }
+        if (!versionData.gcs.paths.x86_64.startsWith("gs://")) {
+          throw new RuntimeException("GCS path should be prefixed with gs://");
         }
       }
 
@@ -269,6 +300,11 @@ public class ReleaseManager {
       if (versionData.s3 != null) {
         metadata.s3 = versionData.s3;
         metadata.filePath = metadata.s3.paths.x86_64;
+      }
+
+      if (versionData.gcs != null) {
+        metadata.gcs = versionData.gcs;
+        metadata.filePath = metadata.gcs.paths.x86_64;
       }
 
       if (versionData.http != null) {

managed/src/main/java/com/yugabyte/yw/forms/ReleaseFormData.java
@@ -2,6 +2,7 @@
 
 package com.yugabyte.yw.forms;
 
+import com.yugabyte.yw.common.ReleaseManager.ReleaseMetadata.GCSLocation;
 import com.yugabyte.yw.common.ReleaseManager.ReleaseMetadata.HttpLocation;
 import com.yugabyte.yw.common.ReleaseManager.ReleaseMetadata.S3Location;
 import io.swagger.annotations.ApiModel;
@@ -12,6 +13,8 @@ public class ReleaseFormData {
   public String version;
 
   @Valid public S3Location s3;
+
+  @Valid public GCSLocation gcs;
 
   @Valid public HttpLocation http;
 }

managed/src/test/java/com/yugabyte/yw/controllers/ReleaseControllerTest.java
@@ -152,6 +152,27 @@ public class ReleaseControllerTest extends FakeDBApplication {
   }
 
   @Test
+  public void testCreateGCSRelease() {
+    ObjectNode pathsNode =
+        (ObjectNode)
+            Json.newObject()
+                .put(
+                    "x86_64",
+                    "gs://my-gcs-buucket/2.7.2.0-b137/"
+                        + "yugabyte-2.7.2.0-b137-centos-x86_64.tar.gz");
+    ObjectNode gcs =
+        (ObjectNode) Json.newObject().put("credentialsJson", "{}").set("paths", pathsNode);
+
+    ObjectNode body = (ObjectNode) Json.newObject().set("foo", Json.newObject().set("gcs", gcs));
+    Result result = createRelease(customer.uuid, body);
+    verify(mockReleaseManager, times(1)).addReleaseWithMetadata(anyString(), anyObject());
+    JsonNode json = Json.parse(contentAsString(result));
+    assertEquals(OK, result.status());
+    assertTrue(json.get("success").asBoolean());
+    assertAuditEntry(1, customer.uuid);
+  }
+
+  @Test
   public void testCreateReleaseWithInvalidCustomer() {
     ObjectNode body = Json.newObject();
     UUID randomUUID = UUID.randomUUID();

commit dfe5764320e2feaa8056391a1ea9fb743b77694e

[PLAT-1522] Fix s3 release breakage
Summary:
The `if` (instead of `elif`) incorrectly caused s3 releases to use the
`else` branch.

This issue was introduced in:
https://github.com/yugabyte/yugabyte-db/commit/4aac9101db18bbd1c60d986e4ed46ead694feb7f

Test Plan: Create s3 release, verify it is able to download correctly.

Reviewers: sanketh

Reviewed By: sanketh

Subscribers: jenkins-bot, yugaware

Differential Revision: https://phabricator.dev.yugabyte.com/D12796

managed/devops/opscli/ybops/cloud/common/method.py
@@ -786,7 +786,7 @@ class ConfigureInstancesMethod(AbstractInstancesMethod):
                     logging.info(
                         "Variables to download {} directly on the remote host added."
                         .format(args.package))
-                if args.gcs_remote_download:
+                elif args.gcs_remote_download:
                     gcs_credentials_json = args.gcs_credentials_json or \
                                            os.getenv('GCS_CREDENTIALS_JSON')
 