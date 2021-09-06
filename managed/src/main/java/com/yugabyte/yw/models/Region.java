yugabyte/yugabyte-db

commit 349c6922b6b0cb51755d096b3097ad34e62b1284

[docs] platform API: ApiModel edits (#9880)
Edits to API models and parameters

managed/src/main/java/com/yugabyte/yw/models/Region.java
@@ -43,12 +43,12 @@ import play.libs.Json;
 @JsonInclude(JsonInclude.Include.NON_NULL)
 @ApiModel(
     description =
-        "Region within a given provider. Typically this will map to a "
-            + "single cloud provider region")
+        "Region within a given provider. Typically, this maps to a "
+            + "single cloud provider region.")
 public class Region extends Model {
 
   @Id
-  @ApiModelProperty(value = "Region uuid", accessMode = READ_ONLY)
+  @ApiModelProperty(value = "Region UUID", accessMode = READ_ONLY)
   public UUID uuid;
 
   @Column(length = 25, nullable = false)
@@ -59,7 +59,10 @@ public class Region extends Model {
   public String code;
 
   @Column(length = 100, nullable = false)
-  @ApiModelProperty(value = "Cloud provider region name", example = "TODO", accessMode = READ_WRITE)
+  @ApiModelProperty(
+      value = "Cloud provider region name",
+      example = "US West (Oregon)",
+      accessMode = READ_WRITE)
   public String name;
 
   @ApiModelProperty(
@@ -69,13 +72,13 @@ public class Region extends Model {
   public String ybImage;
 
   @Column(columnDefinition = "float")
-  @ApiModelProperty(value = "Longitude of this region", example = "-120.01", accessMode = READ_ONLY)
+  @ApiModelProperty(value = "The region's longitude", example = "-120.01", accessMode = READ_ONLY)
   @Constraints.Min(-180)
   @Constraints.Max(180)
   public double longitude = -90;
 
   @Column(columnDefinition = "float")
-  @ApiModelProperty(value = "Latitude of this region", example = "37.22", accessMode = READ_ONLY)
+  @ApiModelProperty(value = "The region's latitude", example = "37.22", accessMode = READ_ONLY)
   @Constraints.Min(-90)
   @Constraints.Max(90)
   public double latitude = -90;
@@ -183,7 +186,7 @@ public class Region extends Model {
    * @param provider Cloud Provider
    * @param code Unique PlacementRegion Code
    * @param name User Friendly PlacementRegion Name
-   * @param ybImage The YB image id that we need to use for provisioning in this region
+   * @param ybImage The YB image ID that we need to use for provisioning in this region
    * @return instance of PlacementRegion
    */
   public static Region create(Provider provider, String code, String name, String ybImage) {
@@ -224,11 +227,11 @@ public class Region extends Model {
   }
 
   public static Region getByCode(Provider provider, String code) {
-    return find.query().where().eq("provider_uuid", provider.uuid).eq("code", code).findOne();
+    return find.query().where().eq("provider_UUID", provider.uuid).eq("code", code).findOne();
   }
 
   public static List<Region> getByProvider(UUID providerUUID) {
-    return find.query().where().eq("provider_uuid", providerUUID).findList();
+    return find.query().where().eq("provider_UUID", providerUUID).findList();
   }
 
   public static Region getOrBadRequest(UUID customerUUID, UUID providerUUID, UUID regionUUID) {
@@ -245,14 +248,14 @@ public class Region extends Model {
     String regionQuery =
         " select r.uuid, r.code, r.name"
             + "   from region r join provider p on p.uuid = r.provider_uuid "
-            + "  where r.uuid = :r_uuid and p.uuid = :p_uuid and p.customer_uuid = :c_uuid";
+            + "  where r.uuid = :r_uuid and p.uuid = :p_uuid and p.customer_uuid = :c_UUID";
 
     RawSql rawSql = RawSqlBuilder.parse(regionQuery).create();
     Query<Region> query = Ebean.find(Region.class);
     query.setRawSql(rawSql);
-    query.setParameter("r_uuid", regionUUID);
-    query.setParameter("p_uuid", providerUUID);
-    query.setParameter("c_uuid", customerUUID);
+    query.setParameter("r_UUID", regionUUID);
+    query.setParameter("p_UUID", providerUUID);
+    query.setParameter("c_UUID", customerUUID);
     return query.findOne();
   }
 
@@ -267,7 +270,7 @@ public class Region extends Model {
         " select r.uuid, r.code, r.name"
             + "   from region r join provider p on p.uuid = r.provider_uuid "
             + "   left outer join availability_zone zone on zone.region_uuid = r.uuid "
-            + "  where p.uuid = :p_uuid and p.customer_uuid = :c_uuid"
+            + "  where p.uuid = :p_uuid and p.customer_uuid = :c_UUID"
             + "  group by r.uuid "
             + " having count(zone.uuid) >= "
             + minZoneCount;
@@ -275,8 +278,8 @@ public class Region extends Model {
     RawSql rawSql = RawSqlBuilder.parse(regionQuery).create();
     Query<Region> query = Ebean.find(Region.class);
     query.setRawSql(rawSql);
-    query.setParameter("p_uuid", providerUUID);
-    query.setParameter("c_uuid", customerUUID);
+    query.setParameter("p_UUID", providerUUID);
+    query.setParameter("c_UUID", customerUUID);
     return query.findList();
   }
 
@@ -286,10 +289,10 @@ public class Region extends Model {
       setActiveFlag(false);
       update();
       String s =
-          "UPDATE availability_zone set active = :active_flag where region_uuid = :region_uuid";
+          "UPDATE availability_zone set active = :active_flag where region_uuid = :region_UUID";
       SqlUpdate updateStmt = Ebean.createSqlUpdate(s);
       updateStmt.setParameter("active_flag", false);
-      updateStmt.setParameter("region_uuid", uuid);
+      updateStmt.setParameter("region_UUID", uuid);
       Ebean.execute(updateStmt);
       commitTransaction();
     } catch (Exception e) {

managed/src/main/java/com/yugabyte/yw/commissioner/tasks/CloudBootstrap.java
@@ -37,7 +37,7 @@ public class CloudBootstrap extends CloudTaskBase {
     super(baseTaskDependencies);
   }
 
-  @ApiModel(value = "Cloud bootstrap parameters", description = "")
+  @ApiModel(value = "CloudBootstrapParams", description = "Cloud bootstrap parameters")
   public static class Params extends CloudTaskParams {
     public static Params fromProvider(Provider provider) {
       Params taskParams = new Params();

managed/src/main/java/com/yugabyte/yw/commissioner/tasks/MultiTableBackup.java
@@ -60,7 +60,7 @@ public class MultiTableBackup extends UniverseTaskBase {
     super(baseTaskDependencies);
   }
 
-  @ApiModel(value = "Multi-table backup parameters", description = "")
+  @ApiModel(value = "MultiTableBackupParams", description = "Multi-table backup parameters")
   public static class Params extends BackupTableParams {
     public UUID customerUUID;
     public List<UUID> tableUUIDList = new ArrayList<>();

managed/src/main/java/com/yugabyte/yw/common/ReleaseManager.java
@@ -47,7 +47,7 @@ public class ReleaseManager {
 
   final ConfigHelper.ConfigType CONFIG_TYPE = ConfigHelper.ConfigType.SoftwareReleases;
 
-  @ApiModel(value = "Release metadata", description = "YB release metadata")
+  @ApiModel(description = "Yugabyte release metadata")
   public static class ReleaseMetadata {
 
     @ApiModelProperty(value = "Release state", example = "ACTIVE")

managed/src/main/java/com/yugabyte/yw/common/Util.java
@@ -362,9 +362,7 @@ public class Util {
     }
   }
 
-  @ApiModel(
-      value = "Universe detail subset",
-      description = "A small subset of universe information")
+  @ApiModel(value = "UniverseDetailSubset", description = "A small subset of universe information")
   @Getter
   public static class UniverseDetailSubset {
     final UUID uuid;

managed/src/main/java/com/yugabyte/yw/common/alerts/SmtpData.java
@@ -14,7 +14,7 @@ public class SmtpData {
   @ApiModelProperty(value = "SMTP port number", example = "465")
   public int smtpPort = -1;
 
-  @ApiModelProperty(value = "SMTP email id", example = "test@example.com")
+  @ApiModelProperty(value = "SMTP email 'from' address", example = "test@example.com")
   public String emailFrom = null;
 
   @ApiModelProperty(value = "SMTP email username", example = "testsmtp")
@@ -23,10 +23,10 @@ public class SmtpData {
   @ApiModelProperty(value = "SMTP password", example = "XurenRknsc")
   public String smtpPassword = null;
 
-  @ApiModelProperty(value = "Use SMTP SSL", example = "true")
+  @ApiModelProperty(value = "Connect to SMTP server using SSL", example = "true")
   public boolean useSSL = true;
 
-  @ApiModelProperty(value = "Use SMTP TLS", example = "false")
+  @ApiModelProperty(value = "Connect to SMTP server using TLS", example = "false")
   public boolean useTLS = false;
 
   @Override

managed/src/main/java/com/yugabyte/yw/controllers/AbstractPlatformController.java
@@ -47,7 +47,9 @@ import play.mvc.Http;
     produces = {"application/json"},
     schemes = {SwaggerDefinition.Scheme.HTTP, SwaggerDefinition.Scheme.HTTPS},
     externalDocs =
-        @ExternalDocs(value = "About Yugabyte Platform", url = "https://docs.yugabyte.com"),
+        @ExternalDocs(
+            value = "About Yugabyte Platform",
+            url = "https://docs.yugabyte.com/latest/yugabyte-platform/"),
     securityDefinition =
         @SecurityDefinition(
             apiKeyAuthDefinitions = {
@@ -55,7 +57,7 @@ import play.mvc.Http;
                   key = AbstractPlatformController.API_KEY_AUTH,
                   name = API_TOKEN_HEADER,
                   in = ApiKeyAuthDefinition.ApiKeyLocation.HEADER,
-                  description = "Api key passed as header")
+                  description = "API token passed as header")
             }))
 public abstract class AbstractPlatformController extends Controller {
 

managed/src/main/java/com/yugabyte/yw/controllers/AccessKeyController.java
@@ -65,7 +65,10 @@ public class AccessKeyController extends AuthenticatedController {
     return PlatformResults.withData(accessKeys);
   }
 
-  @ApiOperation(value = "Create an access key", response = AccessKey.class)
+  @ApiOperation(
+      nickname = "create_accesskey",
+      value = "Create an access key",
+      response = AccessKey.class)
   public Result create(UUID customerUUID, UUID providerUUID) throws IOException {
     Form<AccessKeyFormData> formData = formFactory.getFormDataOrBadRequest(AccessKeyFormData.class);
 
@@ -144,7 +147,10 @@ public class AccessKeyController extends AuthenticatedController {
     return PlatformResults.withData(accessKey);
   }
 
-  @ApiOperation(value = "Delete an access key", response = YBPSuccess.class)
+  @ApiOperation(
+      nickname = "delete_accesskey",
+      value = "Delete an access key",
+      response = YBPSuccess.class)
   public Result delete(UUID customerUUID, UUID providerUUID, String keyCode) {
     Customer.getOrBadRequest(customerUUID);
     Provider.getOrBadRequest(customerUUID, providerUUID);

commit 96fec44a85ed6033394125681bc9d92bbf4524ba

[PLAT-1653] Fix region get/list.
Summary:
https://github.com/yugabyte/yugabyte-db/commit/349c6922b6b0cb51755d096b3097ad34e62b1284
introduced a bug that caused the regions list and get APIs to fail due to incorrect params being
passed.

Test Plan:
Ran unit test before the change, verified it failed. Ran the unit test after the change,
and verified that it succeeded.

Reviewers: sb-yb, sanketh, daniel, vpatibandla, hkandala

Reviewed By: hkandala

Subscribers: jenkins-bot, yugaware

Differential Revision: https://phabricator.dev.yugabyte.com/D12891

managed/src/main/java/com/yugabyte/yw/models/Region.java
@@ -248,7 +248,7 @@ public class Region extends Model {
     String regionQuery =
         " select r.uuid, r.code, r.name"
             + "   from region r join provider p on p.uuid = r.provider_uuid "
-            + "  where r.uuid = :r_uuid and p.uuid = :p_uuid and p.customer_uuid = :c_UUID";
+            + "  where r.uuid = :r_UUID and p.uuid = :p_UUID and p.customer_uuid = :c_UUID";
 
     RawSql rawSql = RawSqlBuilder.parse(regionQuery).create();
     Query<Region> query = Ebean.find(Region.class);
@@ -270,7 +270,7 @@ public class Region extends Model {
         " select r.uuid, r.code, r.name"
             + "   from region r join provider p on p.uuid = r.provider_uuid "
             + "   left outer join availability_zone zone on zone.region_uuid = r.uuid "
-            + "  where p.uuid = :p_uuid and p.customer_uuid = :c_UUID"
+            + "  where p.uuid = :p_UUID and p.customer_uuid = :c_UUID"
             + "  group by r.uuid "
             + " having count(zone.uuid) >= "
             + minZoneCount;