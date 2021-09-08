yugabyte/yugabyte-db

commit fdb729333f3b8dae734264b88cc2d7cd354a23ce

[BACKPORT-2.9.0][PLAT-1620] Added secondary subnet for allowing two network interfaces
Summary:
Some use cases might require us to listen on two interfaces on a single VM. This makes the
appropriate changes to ensure that platform can take in two subnets and create a NIC in each of the
subnet. The primary one is the one that is used by the platform for communication, while the second
one can be used by some external entity.
Traffic should be routed through the 2nd NIC , if this traffic was received on this 2nd NIC. This is
required, because the host should be able to route answer packages to 2nd NIC. There are no defaults
for routing traffic in Linux. Which is why we introduce the configure nic script.
Original diff:https://phabricator.dev.yugabyte.com/D12636
Original commit: https://github.com/yugabyte/yugabyte-db/commit/a7c266d5ab86169775f94d985597f63104c3d368

Test Plan:
1) Unit tests.
2) Test with existing flows. Verified they worked as is.
3) Test by passing in the extra subnet info
4) Ran itest to verify the current flow works without any issues.

Reviewers: sanketh, daniel

Reviewed By: daniel

Subscribers: jenkins-bot, yugaware

Differential Revision: https://phabricator.dev.yugabyte.com/D12893

managed/devops/opscli/ybops/cloud/common/method.py
@@ -100,6 +100,10 @@ class AbstractInstancesMethod(AbstractMethod):
         self.parser.add_argument("--cloud_subnet",
                                  required=False,
                                  help="The VPC subnet id into which we want to provision")
+        self.parser.add_argument("--cloud_subnet_secondary",
+                                 required=False,
+                                 help="The VPC subnet id into which we want to provision "
+                                 "the secondary network interface")
         if self.required_host:
             self.parser.add_argument("search_pattern", default=None)
         else:
@@ -391,8 +395,17 @@ class ProvisionInstancesMethod(AbstractInstancesMethod):
 
         self.update_ansible_vars_with_args(args)
 
-        self.extra_vars.update(get_ssh_host_port(host_info, args.custom_ssh_port,
-                                                 default_port=True))
+        if host_info:
+            self.extra_vars.update(get_ssh_host_port(host_info, args.custom_ssh_port,
+                                                     default_port=True))
+
+        # Check if secondary subnet is present. If so, configure it.
+        if host_info.get('secondary_subnet'):
+            self.cloud.configure_secondary_interface(
+                args, self.extra_vars, self.cloud.get_subnet_cidr(args,
+                                                                  host_info['secondary_subnet']))
+
+        self.preprovision(args)
 
         if not args.skip_preprovision:
             self.preprovision(args)
@@ -790,7 +803,7 @@ class ConfigureInstancesMethod(AbstractInstancesMethod):
                         .format(args.package))
                 if args.gcs_remote_download:
                     gcs_credentials_json = args.gcs_credentials_json or \
-                                           os.getenv('GCS_CREDENTIALS_JSON')
+                        os.getenv('GCS_CREDENTIALS_JSON')
 
                     if gcs_credentials_json is None:
                         raise YBOpsRuntimeError("GCS credentials are not specified, nor found in " +

managed/devops/opscli/ybops/cloud/aws/method.py
@@ -35,6 +35,7 @@ class AwsReplaceRootVolumeMethod(ReplaceRootVolumeMethod):
 class AwsListInstancesMethod(ListInstancesMethod):
     """Subclass for listing instances in AWS. Currently doesn't provide any extra functionality.
     """
+
     def __init__(self, base_command):
         super(AwsListInstancesMethod, self).__init__(base_command)
 
@@ -43,6 +44,7 @@ class AwsCreateInstancesMethod(CreateInstancesMethod):
     """Subclass for creating instances in AWS. This is responsible for taking in the AWS specific
     flags, such as VPCs, AMIs and more.
     """
+
     def __init__(self, base_command):
         super(AwsCreateInstancesMethod, self).__init__(base_command)
 
@@ -106,6 +108,7 @@ class AwsProvisionInstancesMethod(ProvisionInstancesMethod):
     """Subclass for provisioning instances in AWS. Setups the proper Create method to point to the
     AWS specific one.
     """
+
     def __init__(self, base_command):
         super(AwsProvisionInstancesMethod, self).__init__(base_command)
 
@@ -148,16 +151,17 @@ class AwsDestroyInstancesMethod(DestroyInstancesMethod):
     """Subclass for destroying an instance in AWS, we fetch the host info and update the extra_vars
     with necessary parameters
     """
+
     def __init__(self, base_command):
         super(AwsDestroyInstancesMethod, self).__init__(base_command)
 
     def callback(self, args):
         filters = [
-                {
-                    "Name": "instance-state-name",
-                    "Values": ["stopped", "running"]
-                }
-            ]
+            {
+                "Name": "instance-state-name",
+                "Values": ["stopped", "running"]
+            }
+        ]
         host_info = self.cloud.get_host_info_specific_args(
             args.region,
             args.search_pattern,
@@ -168,16 +172,9 @@ class AwsDestroyInstancesMethod(DestroyInstancesMethod):
         if not host_info:
             logging.error("Host {} does not exist.".format(args.search_pattern))
             return
-        if args.delete_static_public_ip:
-            self.cloud.delete_instance(args.region, host_info["id"], True)
-            return
-        self.extra_vars.update({
-            "cloud_subnet": host_info["subnet"],
-            "cloud_region": host_info["region"],
-            "private_ip": host_info['private_ip']
-        })
 
-        super(AwsDestroyInstancesMethod, self).callback(args)
+        self.cloud.delete_instance(args.region, host_info['id'],
+                                   args.delete_static_public_ip)
 
 
 class AwsPauseInstancesMethod(AbstractInstancesMethod):
@@ -185,6 +182,7 @@ class AwsPauseInstancesMethod(AbstractInstancesMethod):
     Subclass for stopping an instance in AWS, we fetch the host info
     and call the stop_instance method.
     """
+
     def __init__(self, base_command):
         super(AwsPauseInstancesMethod, self).__init__(base_command, "pause")
 
@@ -213,6 +211,7 @@ class AwsResumeInstancesMethod(AbstractInstancesMethod):
     Subclass for resuming an instance in AWS, we fetch the host info
     and call the start_instance method.
     """
+
     def __init__(self, base_command):
         super(AwsResumeInstancesMethod, self).__init__(base_command, "resume")
 
@@ -223,11 +222,11 @@ class AwsResumeInstancesMethod(AbstractInstancesMethod):
 
     def callback(self, args):
         filters = [
-                {
-                    "Name": "instance-state-name",
-                    "Values": ["stopped"]
-                }
-            ]
+            {
+                "Name": "instance-state-name",
+                "Values": ["stopped"]
+            }
+        ]
         host_info = self.cloud.get_host_info_specific_args(
             args.region,
             args.search_pattern,

managed/devops/opscli/ybops/cloud/gcp/method.py
@@ -8,14 +8,15 @@
 #
 # https://github.com/YugaByte/yugabyte-db/blob/master/licenses/POLYFORM-FREE-TRIAL-LICENSE-1.0.0.txt
 
-from ybops.cloud.common.method import CreateInstancesMethod, ProvisionInstancesMethod,\
-    AbstractMethod, DestroyInstancesMethod, AbstractAccessMethod, CreateRootVolumesMethod, \
-    ReplaceRootVolumeMethod, ChangeInstanceTypeMethod
-from ybops.common.exceptions import YBOpsRuntimeError, get_exception_message
-from ybops.utils import validated_key_file, format_rsa_key
-from ybops.cloud.gcp.utils import GCP_PERSISTENT, GCP_SCRATCH
-
 import json
+
+from ybops.cloud.common.method import (AbstractAccessMethod, AbstractMethod,
+                                       ChangeInstanceTypeMethod, CreateInstancesMethod,
+                                       CreateRootVolumesMethod, DestroyInstancesMethod,
+                                       ProvisionInstancesMethod, ReplaceRootVolumeMethod)
+from ybops.cloud.gcp.utils import GCP_PERSISTENT, GCP_SCRATCH
+from ybops.common.exceptions import YBOpsRuntimeError, get_exception_message
+from ybops.utils import format_rsa_key, validated_key_file
 
 
 class GcpReplaceRootVolumeMethod(ReplaceRootVolumeMethod):
@@ -65,18 +66,14 @@ class GcpCreateInstancesMethod(CreateInstancesMethod):
             public_key = format_rsa_key(rsa_key, public_key=True)
             ssh_keys = "{}:{} {}".format(self.SSH_USER, public_key, self.SSH_USER)
 
-        self.cloud.get_admin().create_instance(
-            args.region, args.zone, args.cloud_subnet, args.search_pattern, args.instance_type,
-            server_type, args.use_preemptible, can_ip_forward, machine_image, args.num_volumes,
-            args.volume_type, args.volume_size, args.boot_disk_size_gb, args.assign_public_ip,
-            args.assign_static_public_ip, ssh_keys, boot_script=args.boot_script,
-            auto_delete_boot_disk=args.auto_delete_boot_disk, tags=args.instance_tags)
+        self.cloud.create_instance(args, server_type, can_ip_forward, machine_image, ssh_keys)
 
 
 class GcpProvisionInstancesMethod(ProvisionInstancesMethod):
     """Subclass for provisioning instances in GCP. Sets up the proper Create method to point to the
     GCP specific one.
     """
+
     def __init__(self, base_command):
         super(GcpProvisionInstancesMethod, self).__init__(base_command)
 
@@ -107,14 +104,17 @@ class GcpCreateRootVolumesMethod(CreateRootVolumesMethod):
             "sourceImage": args.machine_image})
         return res["targetLink"]
 
-    def delete_instance(self, args, instance_id):
+    # Not invoked. Just keeping if for consistency.
+    def delete_instance(self, args):
+        name = args.search_pattern[:63] if len(args.search_pattern) > 63 else args.search_pattern
         self.cloud.get_admin().delete_instance(
-            args.region, args.zone, instance_id, has_static_ip=args.delete_static_public_ip)
+            args.region, args.zone, name, has_static_ip=args.assign_static_public_ip)
 
 
 class GcpDestroyInstancesMethod(DestroyInstancesMethod):
     """Subclass for deleting instances in GCP. Uses the API to delete instance bypassing Ansible.
     """
+
     def __init__(self, base_command):
         super(GcpDestroyInstancesMethod, self).__init__(base_command)
 

managed/devops/opscli/ybops/cloud/aws/cloud.py
@@ -8,29 +8,28 @@
 #
 # https://github.com/YugaByte/yugabyte-db/blob/master/licenses/POLYFORM-FREE-TRIAL-LICENSE-1.0.0.txt
 
-import boto3
 import json
 import logging
 import os
 import socket
+import time
 
-from botocore.utils import InstanceMetadataFetcher
+import boto3
 from botocore.exceptions import ClientError
-
-from six.moves.urllib.request import urlopen
+from botocore.utils import InstanceMetadataFetcher
 from six.moves.urllib.error import URLError
-
-from ybops.cloud.aws.command import AwsInstanceCommand, AwsNetworkCommand, \
-    AwsAccessCommand, AwsQueryCommand, AwsDnsCommand
-from ybops.cloud.aws.utils import get_vpc_for_subnet
+from six.moves.urllib.request import urlopen
+from ybops.cloud.aws.command import (AwsAccessCommand, AwsDnsCommand, AwsInstanceCommand,
+                                     AwsNetworkCommand, AwsQueryCommand)
+from ybops.cloud.aws.utils import (ROOT_VOLUME_LABEL, AwsBootstrapClient, YbVpcComponents,
+                                   change_instance_type, create_instance, delete_vpc, get_client,
+                                   get_clients, get_device_names, get_spot_pricing,
+                                   get_vpc_for_subnet, get_zones, has_ephemerals, modify_tags,
+                                   query_vpc, update_disk)
 from ybops.cloud.common.cloud import AbstractCloud
-from ybops.utils import is_valid_ip_address, validated_key_file, format_rsa_key
-
 from ybops.common.exceptions import YBOpsRuntimeError
-from ybops.cloud.aws.utils import query_vpc, get_zones, \
-    delete_vpc, get_client, get_clients, AwsBootstrapClient, \
-    get_spot_pricing, YbVpcComponents, create_instance, has_ephemerals, get_device_names, \
-    modify_tags, update_disk, ROOT_VOLUME_LABEL, change_instance_type
+from ybops.utils import (DEFAULT_SSH_PORT, DEFAULT_SSH_USER, format_rsa_key, get_datafile_path,
+                         is_valid_ip_address, remote_exec_command, scp_to_tmp, validated_key_file)
 
 
 class AwsCloud(AbstractCloud):
@@ -63,7 +62,7 @@ class AwsCloud(AbstractCloud):
         return get_vpc_for_subnet(get_client(region), subnet)
 
     def get_image(self, region=None):
-        regions = [region] if region is not None else get_regions()
+        regions = [region] if region is not None else self.get_regions()
         output = {}
         for r in regions:
             output[r] = self.metadata["regions"][r]["image"]
@@ -245,7 +244,7 @@ class AwsCloud(AbstractCloud):
         elif metadata_type in ["region", "privateIp"]:
             identity_data = urlopen(self.INSTANCE_IDENTITY_API,
                                     timeout=self.METADATA_API_TIMEOUT_SECONDS) \
-                                        .read().decode('utf-8')
+                .read().decode('utf-8')
             return json.loads(identity_data).get(metadata_type) if identity_data else None
         elif metadata_type in ["role"]:
             # Arg timeout is in MS.
@@ -332,16 +331,31 @@ class AwsCloud(AbstractCloud):
             disks = data.get("BlockDeviceMappings")
             root_vol = next(disk for disk in disks if disk.get("DeviceName") == ROOT_VOLUME_LABEL)
 
+            primary_private_ip = None
+            secondary_private_ip = None
+            primary_subnet = None
+            secondary_subnet = None
+            network_interfaces = data.get("NetworkInterfaces")
+            for interface in network_interfaces:
+                if interface.get("Attachment").get("DeviceIndex") == 0:
+                    primary_private_ip = interface.get("PrivateIpAddress")
+                    primary_subnet = interface.get("SubnetId")
+                elif interface.get("Attachment").get("DeviceIndex") == 1:
+                    secondary_private_ip = interface.get("PrivateIpAddress")
+                    secondary_subnet = interface.get("SubnetId")
+
             result = dict(
                 id=data.get("InstanceId", None),
                 name=name_tags[0] if name_tags else None,
                 public_ip=data.get("PublicIpAddress", None),
-                private_ip=data["PrivateIpAddress"],
+                private_ip=primary_private_ip,
+                secondary_private_ip=secondary_private_ip,
                 public_dns=data["PublicDnsName"],
                 private_dns=data["PrivateDnsName"],
                 launch_time=data["LaunchTime"].isoformat(),
                 zone=zone,
-                subnet=data["SubnetId"],
+                subnet=primary_subnet,
+                secondary_subnet=secondary_subnet,
                 region=region if region is not None else zone[:-1],
                 instance_type=data["InstanceType"],
                 server_type=server_tags[0] if server_tags else None,
@@ -360,8 +374,20 @@ class AwsCloud(AbstractCloud):
         else:
             return get_device_names(args.instance_type, args.num_volumes)
 
+    def get_subnet_cidr(self, args, subnet_id):
+        ec2 = boto3.resource('ec2', args.region)
+        subnet = ec2.Subnet(subnet_id)
+        return subnet.cidr_block
+
     def create_instance(self, args):
-        return create_instance(args)
+        # If we are configuring second NIC, ensure that this only happens for a
+        # centOS AMI right now.
+        if args.cloud_subnet_secondary:
+            ec2 = boto3.resource('ec2', args.region)
+            image = ec2.Image(args.machine_image)
+            if 'centos' not in image.name.lower():
+                raise YBOpsRuntimeError("Second NIC can only be configured for CentOS right now")
+        create_instance(args)
 
     def delete_instance(self, region, instance_id, has_elastic_ip=False):
         logging.info("Deleting AWS instance {} in region {}".format(instance_id, region))
@@ -369,18 +395,16 @@ class AwsCloud(AbstractCloud):
         instance = ec2.Instance(instance_id)
         if has_elastic_ip:
             client = boto3.client('ec2', region)
-            elastic_ip = client.describe_addresses(
-                Filters=[{'Name': 'public-ip', 'Values': [instance.public_ip_address]}],
-                DryRun=False
-            )["Addresses"][0]
-            client.disassociate_address(
-                AssociationId=elastic_ip["AssociationId"],
-                DryRun=False
-            )
-            client.release_address(
-                AllocationId=elastic_ip["AllocationId"],
-                DryRun=False
-            )
+            elastic_ip_list = client.describe_addresses(
+                Filters=[{'Name': 'public-ip', 'Values': [instance.public_ip_address]}]
+            )["Addresses"]
+            for elastic_ip in elastic_ip_list:
+                client.disassociate_address(
+                    AssociationId=elastic_ip["AssociationId"]
+                )
+                client.release_address(
+                    AllocationId=elastic_ip["AllocationId"]
+                )
             logging.info(
                 "Deleted elastic ip at {} from VM {}".format(elastic_ip["PublicIp"], instance_id))
         instance.terminate()
@@ -464,4 +488,3 @@ class AwsCloud(AbstractCloud):
             logging.error(e)
         finally:
             sock.close()
-

managed/devops/opscli/ybops/cloud/aws/utils.py
@@ -131,7 +131,7 @@ def add_cidr_to_rules(rules, cidr):
         "from_port": 0,
         "to_port": 65535,
         "cidr_ip": cidr
-        }
+    }
     rules.append(rule_block)
 
 
@@ -646,7 +646,7 @@ def query_vpc(region):
     raw_client = boto3.client("ec2", region_name=region)
     zones = [z["ZoneName"]
              for z in raw_client.describe_availability_zones(
-                Filters=get_filters("state", "available")).get("AvailabilityZones", [])]
+        Filters=get_filters("state", "available")).get("AvailabilityZones", [])]
     # Default to empty lists, in case some zones do not have subnets, so we can use this as a query
     # for all available AZs in this region.
     subnets_by_zone = {z: [] for z in zones}
@@ -855,17 +855,7 @@ def has_ephemerals(instance_type):
     return not is_nvme(instance_type) and not is_next_gen(instance_type)
 
 
-def create_instance(args):
-    client = get_client(args.region)
-    vars = {
-        "ImageId": args.machine_image,
-        "KeyName": args.key_pair_name,
-        "MinCount": 1,
-        "MaxCount": 1,
-        "InstanceType": args.instance_type,
-    }
-    # Network setup.
-    # Lets assume they have provided security group id comma delimited.
+def __get_security_group(client, args):
     sg_ids = args.security_group_id.split(",") if args.security_group_id else None
     if sg_ids is None:
         # Figure out which VPC this instance will be brought up in and search for the SG in there.
@@ -878,12 +868,39 @@ def create_instance(args):
         sg_name = get_yb_sg_name(args.region)
         sg = get_security_group(client, sg_name, vpc)
         sg_ids = [sg.id]
+    return sg_ids
+
+
+def create_instance(args):
+    client = get_client(args.region)
+    vars = {
+        "ImageId": args.machine_image,
+        "KeyName": args.key_pair_name,
+        "MinCount": 1,
+        "MaxCount": 1,
+        "InstanceType": args.instance_type,
+    }
+    # Network setup.
+    # Lets assume they have provided security group id comma delimited.
+    sg_ids = __get_security_group(client, args)
+
     vars["NetworkInterfaces"] = [{
         "DeviceIndex": 0,
-        "AssociatePublicIpAddress": args.assign_public_ip,
         "SubnetId": args.cloud_subnet,
-        "Groups": sg_ids
+        "Groups": sg_ids,
+        "DeleteOnTermination": True
     }]
+    if args.cloud_subnet_secondary:
+        vars["NetworkInterfaces"].append({
+            "DeviceIndex": 1,
+            "SubnetId": args.cloud_subnet_secondary,
+            "Groups": sg_ids,
+            "DeleteOnTermination": True
+        })
+    # AWS limitation that no public IP can be assigned if using two network interfaces.
+    else:
+        vars["NetworkInterfaces"][0]["AssociatePublicIpAddress"] = args.assign_public_ip
+
     # Volume setup.
     volumes = []
 
@@ -978,6 +995,9 @@ def create_instance(args):
             len(instance_ids)))
     instance = instance_ids[0]
     instance.wait_until_running()
+
+    logging.info("[app] AWS VM {} created.".format(args.search_pattern))
+
     if args.assign_static_public_ip:
         # Create elastic IP.
         eip_tags = list(user_tags)
@@ -989,22 +1009,27 @@ def create_instance(args):
         ec2_client = boto3.client("ec2", region_name=args.region)
         eip = ec2_client.allocate_address(
             Domain="vpc",
-            DryRun=False,
             TagSpecifications=[{
                 "ResourceType": "elastic-ip",
                 "Tags": eip_tags
             }]
         )
+        # Re-fetch instance to get latest state.
+        instance = client.Instance(instance.id)
+        # Get instance's primary network interface and attach IP.
+        interface_id = None
+        for i in instance.network_interfaces:
+            if i.attachment.get("DeviceIndex") == 0:
+                interface_id = i.id
         # Associate elastic IP with instance.
         ec2_client.associate_address(
             AllocationId=eip["AllocationId"],
-            InstanceId=instance.id,
-            DryRun=False
+            NetworkInterfaceId=interface_id
         )
         logging.info("[app] Created Elastic IP address at {} in region {} for AWS VM {}"
                      .format(eip["PublicIp"], args.region, args.search_pattern))
 
-    logging.info("[app] AWS VM {} created.".format(args.search_pattern))
+    return instance.id
 
 
 def modify_tags(region, instance_id, tags_to_set_str, tags_to_remove_str):
@@ -1112,8 +1137,8 @@ def _update_dns_record_set(hosted_zone_id, domain_name_prefix, ip_list, action):
     client.get_waiter('resource_record_sets_changed').wait(
         Id=result['ChangeInfo']['Id'],
         WaiterConfig={
-          'Delay': 10,
-          'MaxAttempts': 60
+            'Delay': 10,
+            'MaxAttempts': 60
         })
 
 

managed/devops/opscli/ybops/cloud/common/cloud.py
@@ -8,30 +8,21 @@
 #
 # https://github.com/YugaByte/yugabyte-db/blob/master/licenses/POLYFORM-FREE-TRIAL-LICENSE-1.0.0.txt
 
-from ybops.cloud.common.ansible import AnsibleProcess
-from ybops.cloud.common.base import AbstractCommandParser
-from ybops.utils import get_ssh_host_port, get_datafile_path, \
-    get_internal_datafile_path, YBOpsRuntimeError, YB_HOME_DIR
-
-from ybops.utils.remote_shell import RemoteShell
-
-from cryptography import x509
-from cryptography.x509.oid import NameOID
-from cryptography.hazmat.backends import default_backend
-from cryptography.hazmat.primitives import hashes
-from cryptography.hazmat.primitives.asymmetric import rsa
-from cryptography.hazmat.primitives.serialization import (load_pem_private_key, Encoding,
-                                                          PrivateFormat, NoEncryption)
-import datetime
-import six
 import logging
 import os
-import tempfile
-import yaml
-import socket
-import time
 import re
+import socket
 import ssl
+import tempfile
+import time
+
+import yaml
+from ybops.cloud.common.ansible import AnsibleProcess
+from ybops.cloud.common.base import AbstractCommandParser
+from ybops.utils import (YB_HOME_DIR, YBOpsRuntimeError, get_datafile_path,
+                         get_internal_datafile_path, get_ssh_host_port, remote_exec_command,
+                         scp_to_tmp)
+from ybops.utils.remote_shell import RemoteShell
 
 
 class AbstractCloud(AbstractCommandParser):
@@ -55,7 +46,7 @@ class AbstractCloud(AbstractCommandParser):
     CERT_LOCATION_NODE = "node"
     CERT_LOCATION_PLATFORM = "platform"
     SSH_RETRY_COUNT = 30
-    SSH_WAIT_SECONDS = 10
+    SSH_WAIT_SECONDS = 30
 
     def __init__(self, name):
         super(AbstractCloud, self).__init__(name)
@@ -215,11 +206,45 @@ class AbstractCloud(AbstractCommandParser):
             "-U postgres\"".format(master_addresses, init_db_path)
         )
 
+    def configure_secondary_interface(self, args, extra_vars, subnet_cidr):
+        logging.info("[app] Configuring second NIC")
+        self._wait_for_ssh_port(extra_vars["ssh_host"], args.search_pattern,
+                                extra_vars["ssh_port"])
+        subnet_network, subnet_netmask = subnet_cidr.split('/')
+        # Copy and run script to configure routes
+        scp_to_tmp(
+            get_datafile_path('configure_nic.sh'), extra_vars["ssh_host"],
+            extra_vars["ssh_user"], extra_vars["ssh_port"], args.private_key_file)
+        cmd = ("sudo /tmp/configure_nic.sh "
+               "--subnet_network {} --subnet_netmask {} --cloud {}").format(
+            subnet_network, subnet_netmask, self.name)
+        rc, stdout, stderr = remote_exec_command(
+            extra_vars["ssh_host"], extra_vars["ssh_port"],
+            extra_vars["ssh_user"], args.private_key_file, cmd)
+        if rc:
+            raise YBOpsRuntimeError(
+                "Could not configure second nic {} {}".format(stdout, stderr))
+        # Since this is on start, wait for ssh on default port
+        # Reboot instance
+        remote_exec_command(
+            extra_vars["ssh_host"], extra_vars["ssh_port"], extra_vars["ssh_user"],
+            args.private_key_file, 'sudo reboot')
+        self._wait_for_ssh_port(extra_vars["ssh_host"],
+                                args.search_pattern, extra_vars["ssh_port"])
+        # Verify that the command ran successfully:
+        rc, stdout, stderr = remote_exec_command(extra_vars["ssh_host"], extra_vars["ssh_port"],
+                                                 extra_vars["ssh_user"], args.private_key_file,
+                                                 'ls /tmp/dhclient-script-*')
+        if rc:
+            raise YBOpsRuntimeError(
+                "Second nic not configured at start up")
+
     # Compare certificate content and return
     # 0 -> if cert1 equals to cert2
     # 1 -> if cert1 is subset of cert2
     # 2 -> if cert2 is subset of cert1
     # 3 -> if none of the above satisfies
+
     def compare_certs(self, cert1, cert2):
         # Extract certificate values list from the string
         cert1_re = re.findall(
@@ -499,20 +524,25 @@ class AbstractCloud(AbstractCommandParser):
 
     def _wait_for_ssh_port(self, private_ip, instance_name, ssh_port):
         try:
-            sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            sock = None
             retry_count = 0
 
+            ssh_port = int(ssh_port)
+
             while retry_count < self.SSH_RETRY_COUNT:
+                logging.info("[app] Waiting for ssh: {}:{}".format(private_ip, str(ssh_port)))
                 time.sleep(self.SSH_WAIT_SECONDS)
                 retry_count = retry_count + 1
+                sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
                 result = sock.connect_ex((private_ip, ssh_port))
                 if result == 0:
                     break
             else:
-                logging.error("Start instance {} exceeded maxRetries!".format(instance_name))
+                logging.error("[app] Start instance {} exceeded maxRetries!".format(instance_name))
                 raise YBOpsRuntimeError(
                     "Cannot reach the instance {} after its start at port {}".format(
                         instance_name, ssh_port)
                 )
         finally:
-            sock.close()
+            if sock:
+                sock.close()

commit f8993fb95021b82f02646b2c4f86a69639cd8fa3

[Platform][2.9.0] Fix double call to preprovision
Summary:
Commit
https://github.com/yugabyte/yugabyte-db/commit/fdb729333f3b8dae734264b88cc2d7cd354a23ce introduced a
bug of calling preprovision twice, calling the second call to fail due to the ssh port being
switched as part of the first call.

Test Plan: Create a universe and verify it succeeds.

Reviewers: muthu, sanketh, daniel

Reviewed By: daniel

Subscribers: jenkins-bot, yugaware

Differential Revision: https://phabricator.dev.yugabyte.com/D12927

managed/devops/opscli/ybops/cloud/common/method.py
@@ -405,8 +405,6 @@ class ProvisionInstancesMethod(AbstractInstancesMethod):
                 args, self.extra_vars, self.cloud.get_subnet_cidr(args,
                                                                   host_info['secondary_subnet']))
 
-        self.preprovision(args)
-
         if not args.skip_preprovision:
             self.preprovision(args)
 