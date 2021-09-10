ebc9e69fbcc3a7e2a52299d7ba3dc796fbea392d
Andrew Cai <andrew@yugabyte.com>
2021-09-10 12:11:49 -0700

    [PLAT-1682][2.4] Fix node comparison function from accessing undefined cluster
    Summary:
    This fix addresses the immediate problem of a blank screen on the universe overview page
    but does not solve the underlying issue. Based on the sequence of tasks on the universe, it is
    likely the user was trying to add a read replica but failed to do so. While this seems relatively
    harmless, since the resources were not created, it inadvertently left metadata of the created nodes
    in the universe's json. This caused the utils function to fail since we expect a node to be
    associated with the universe's cluster.
    
    Test Plan: Go to 2.4 portal and try to access universe with failed to create read replica.
    
    Reviewers: sshevchenko
    
    Reviewed By: sshevchenko
    
    Subscribers: ui, jenkins-bot
    
    Differential Revision: https://phabricator.dev.yugabyte.com/D12964

--- managed/ui/src/utils/UniverseUtils.js
+++ managed/ui/src/utils/UniverseUtils.js
@@ -103,6 +103,10 @@ export function getProviderMetadata(provider) {
 
 export function getClusterIndex(nodeDetails, clusters) {
   const cluster = clusters.find((cluster) => cluster.uuid === nodeDetails.placementUuid);
+  if (!cluster) {
+    // Move orphaned nodes to end of list
+    return Number.MAX_SAFE_INTEGER;
+  }
   return cluster.index;
 }
 
@@ -110,9 +114,9 @@ export function nodeComparisonFunction(nodeDetailsA, nodeDetailsB, clusters) {
   const aClusterIndex = getClusterIndex(nodeDetailsA, clusters);
   const bClusterIndex = getClusterIndex(nodeDetailsB, clusters);
   if (aClusterIndex !== bClusterIndex) {
-    return aClusterIndex > bClusterIndex;
+    return aClusterIndex - bClusterIndex;
   }
-  return nodeDetailsA.nodeIdx > nodeDetailsB.nodeIdx;
+  return nodeDetailsA.nodeIdx - nodeDetailsB.nodeIdx;
 }
 
 export function hasLiveNodes(universe) {
