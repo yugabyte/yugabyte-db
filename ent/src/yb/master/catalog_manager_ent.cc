yugabyte/yugabyte-db

commit e9d782852be1f9e193e132d9e5b1e5ce622d4acb

[#9909] ysql: further fix backup restore for NULL col attr
Summary:
The nullable column attribute fix in commit
d4b6ece62ba1d0aee0b436c54b1fa3e933795a60 handled only one code path but
not another.  The two code paths are

1. namespace id and table id match (fixed here)
2. otherwise, namespace, schema, and table name match (fixed previously)

This mistake is likely due to code duplication.  Deduplicate the checks
on schema match (using the version that ignores nullable column
attribute, thereby fixing this bug), number of tablets match, etc.

Now, backup restore from an old cluster (like 2.2.0.0) should work when
namespace id and table id match.

Do other changes:

- Restructure an if else on table type so that it flows better and also
  errors on bad table types.
- Add comments that make it much easier for me to follow what's going on
  in ImportTableEntry.
- Remove dead code that creates variable `orig_table_name`.
- Remove an outdated comment due to fix in commit
  ad6a74a05cb5bc58ac811c38a1eb6a4a0afc71aa.

Close: #9909

Test Plan:
Run the repro in issue #9909 before and after this change.

To make sure that there are no regressions,

    ./yb_build.sh --cxx-test tools_yb-backup-test_ent
    ./yb_build.sh --java-test org.yb.cql.ParameterizedTestYbBackup
    ./yb_build.sh --java-test org.yb.cql.TestYbBackup
    ./yb_build.sh --java-test org.yb.pgsql.TestYbBackup

Reviewers: oleg

Reviewed By: oleg

Subscribers: yql, bogdan

Differential Revision: https://phabricator.dev.yugabyte.com/D12845

ent/src/yb/master/catalog_manager_ent.cc
@@ -1189,7 +1189,8 @@ Status CatalogManager::ImportTableEntry(const NamespaceMap& namespace_map,
   const vector<ColumnId>& column_ids = schema.column_ids();
   scoped_refptr<TableInfo> table;
 
-  // Create new table if namespace was changed.
+  // First, check if namespace id and table id match. If, in addition, other properties match, we
+  // found the destination table.
   if (new_namespace_id == table_data->old_namespace_id) {
     TRACE("Looking up table");
     {
@@ -1197,46 +1198,14 @@ Status CatalogManager::ImportTableEntry(const NamespaceMap& namespace_map,
       table = FindPtrOrNull(*table_ids_map_, table_data->old_table_id);
     }
 
-    // Check table is active, table name and table schema are equal to backed up ones.
     if (table != nullptr) {
+      // At this point, namespace id and table id match. Check other properties, like whether the
+      // table is active and whether table name matches.
       auto table_lock = table->LockForRead();
       if (table_lock->visible_to_client() && table->name() == meta.name()) {
         LOG_WITH_FUNC(INFO) << "Found existing table: '" << table->ToString() << "'";
-        // Check the found table schema.
-        Schema persisted_schema;
-        RETURN_NOT_OK(table->GetSchema(&persisted_schema));
-        // Schema::Equals() compares only column names & types.
-        // Check the column ids and number of tablets separately.
-        if (!persisted_schema.Equals(schema)) {
-          const string msg = Format(
-              "Found by id $0 $1 table '$2' in namespace id $3 has schema={$4}, expected={$5}",
-              table_data->old_table_id, TableType_Name(meta.table_type()), meta.name(),
-              new_namespace_id, persisted_schema, schema);
-          LOG_WITH_FUNC(WARNING) << msg;
-          return STATUS(InvalidArgument, msg, MasterError(MasterErrorPB::SNAPSHOT_FAILED));
-        }
-
-        if (persisted_schema.column_ids() != column_ids) {
-          const string msg = Format(
-              "Found by id $0 $1 table '$2' in namespace id $3 has column ids=$4, expected=$5",
-              table_data->old_table_id, TableType_Name(meta.table_type()), meta.name(),
-              new_namespace_id, ToString(persisted_schema.column_ids()), ToString(column_ids));
-          LOG_WITH_FUNC(WARNING) << msg;
-          return STATUS(InvalidArgument, msg, MasterError(MasterErrorPB::SNAPSHOT_FAILED));
-        }
-
-        if (schema.table_properties().num_tablets() > 0 &&
-            persisted_schema.table_properties().num_tablets() !=
-            schema.table_properties().num_tablets()) {
-          const string msg = Format(
-              "Found by id $0 $1 table '$2' in namespace id $3 has number of tablets=$4, "
-              "expected=$5", table_data->old_table_id, TableType_Name(meta.table_type()),
-              meta.name(), new_namespace_id, persisted_schema.table_properties().num_tablets(),
-              schema.table_properties().num_tablets());
-          LOG_WITH_FUNC(WARNING) << msg;
-          return STATUS(InvalidArgument, msg, MasterError(MasterErrorPB::SNAPSHOT_FAILED));
-        }
       } else {
+        // A property did not match, so this search by ids failed.
         LOG_WITH_FUNC(WARNING) << "Existing table " << table->ToString() << " not suitable: "
                                << table_lock->pb.ShortDebugString()
                                << ", name: " << table->name() << " vs " << meta.name();
@@ -1245,10 +1214,14 @@ Status CatalogManager::ImportTableEntry(const NamespaceMap& namespace_map,
     }
   }
 
+  // Second, if we still didn't find a match...
   if (table == nullptr) {
-    if (meta.table_type() == TableType::PGSQL_TABLE_TYPE) {
-      // YSQL table must be created via external call. Find it by name.
-      // Expecting the table name is unique in the YSQL database.
+    if (meta.table_type() == TableType::YQL_TABLE_TYPE) {
+      // For YCQL, simply create the missing table.
+      RETURN_NOT_OK(RecreateTable(new_namespace_id, table_map, table_data));
+    } else if (meta.table_type() == TableType::PGSQL_TABLE_TYPE) {
+      // For YSQL, the table must be created via external call. Therefore, continue the search for
+      // the table, this time checking for name matches rather than id matches.
 
       if (meta.colocated() && IsColocatedParentTableId(table_data->old_table_id)) {
         // For the parent colocated table we need to generate the new_table_id ourselves
@@ -1316,99 +1289,102 @@ Status CatalogManager::ImportTableEntry(const NamespaceMap& namespace_map,
         }
       }
     } else {
-      RETURN_NOT_OK(RecreateTable(new_namespace_id, table_map, table_data));
-    }
-
-    TRACE("Looking up new table");
-    {
-      SharedLock lock(mutex_);
-      table = FindPtrOrNull(*table_ids_map_, table_data->new_table_id);
-    }
-
-    if (table == nullptr) {
-      const string msg = Format("Created table not found: $0", table_data->new_table_id);
-      LOG_WITH_FUNC(WARNING) << msg;
-      return STATUS(InternalError, msg, MasterError(MasterErrorPB::OBJECT_NOT_FOUND));
-    }
-
-    // Don't do schema validation/column updates on the parent colocated table.
-    // However, still do the validation for regular colocated tables.
-    if (!is_parent_colocated_table) {
-      Schema persisted_schema;
-      {
-        TRACE("Locking table");
-        auto table_lock = table->LockForRead();
-        RETURN_NOT_OK(table->GetSchema(&persisted_schema));
-      }
-
-      // Ignore 'nullable' attribute - due to difference in implementation
-      // of PgCreateTable::AddColumn() and PgAlterTable::AddColumn().
-      struct CompareColumnsExceptNullable {
-        bool operator ()(const ColumnSchema& a, const ColumnSchema& b) {
-          return ColumnSchema::CompHashKey(a, b) && ColumnSchema::CompSortingType(a, b) &&
-              ColumnSchema::CompTypeInfo(a, b) && ColumnSchema::CompName(a, b);
-        }
-      } comparator;
-      // Schema::Equals() compares only column names & types. It does not compare the column ids.
-      if (!persisted_schema.Equals(schema, comparator)
-          || persisted_schema.column_ids().size() != column_ids.size()) {
-        const string msg = Format(
-            "Invalid created $0 table '$1' in namespace id $2: schema={$3}, expected={$4}",
-            TableType_Name(meta.table_type()), meta.name(), new_namespace_id,
-            persisted_schema, schema);
-        LOG_WITH_FUNC(WARNING) << msg;
-        return STATUS(InternalError, msg, MasterError(MasterErrorPB::SNAPSHOT_FAILED));
-      }
-
-      if (table_data->num_tablets > 0 &&
-          persisted_schema.table_properties().num_tablets() != table_data->num_tablets) {
-        const string msg = Format(
-            "Wrong number of tablets in created $0 table '$1' in namespace id $2: $3 (expected $4)",
-            TableType_Name(meta.table_type()), meta.name(), new_namespace_id,
-            persisted_schema.table_properties().num_tablets(), table_data->num_tablets);
-        LOG_WITH_FUNC(WARNING) << msg;
-        return STATUS(InternalError, msg, MasterError(MasterErrorPB::SNAPSHOT_FAILED));
-      }
-
-      // Update the table column ids if it's not equal to the stored ids.
-      if (persisted_schema.column_ids() != column_ids) {
-        if (meta.table_type() != TableType::PGSQL_TABLE_TYPE) {
-          LOG_WITH_FUNC(WARNING) << "Unexpected wrong column ids in "
-                                 << TableType_Name(meta.table_type()) << " table '" << meta.name()
-                                 << "' in namespace id " << new_namespace_id;
-        }
-
-        LOG_WITH_FUNC(INFO) << "Restoring column ids in " << TableType_Name(meta.table_type())
-                            << " table '" << meta.name() << "' in namespace id "
-                            << new_namespace_id;
-        auto l = table->LockForWrite();
-        size_t col_idx = 0;
-        for (auto& column : *l.mutable_data()->pb.mutable_schema()->mutable_columns()) {
-          // Expecting here correct schema (columns - order, names, types), but with only wrong
-          // column ids. Checking correct column order and column names below.
-          if (column.name() != schema.column(col_idx).name()) {
-            const string msg = Format(
-                "Unexpected column name for index=$0: name=$1, expected name=$2",
-                col_idx, schema.column(col_idx).name(), column.name());
-            LOG_WITH_FUNC(WARNING) << msg;
-            return STATUS(InternalError, msg, MasterError(MasterErrorPB::SNAPSHOT_FAILED));
-          }
-          // Copy the column id from imported (original) schema.
-          column.set_id(column_ids[col_idx++]);
-        }
-
-        l.mutable_data()->pb.set_next_column_id(schema.max_col_id() + 1);
-        l.mutable_data()->pb.set_version(l->pb.version() + 1);
-        // Update sys-catalog with the new table schema.
-        RETURN_NOT_OK(sys_catalog_->Upsert(leader_ready_term(), table));
-        l.Commit();
-        // Update the new table schema in tablets.
-        RETURN_NOT_OK(SendAlterTableRequest(table));
-      }
+      return STATUS(
+          InvalidArgument,
+          Format("Unexpected table type: $0", TableType_Name(meta.table_type())),
+          MasterError(MasterErrorPB::INVALID_TABLE_TYPE));
     }
   } else {
     table_data->new_table_id = table_data->old_table_id;
     LOG_WITH_FUNC(INFO) << "Use existing table " << table_data->new_table_id;
+  }
+
+  // The destination table should be found or created by now.
+  TRACE("Looking up new table");
+  {
+    SharedLock lock(mutex_);
+    table = FindPtrOrNull(*table_ids_map_, table_data->new_table_id);
+  }
+  if (table == nullptr) {
+    const string msg = Format("Created table not found: $0", table_data->new_table_id);
+    LOG_WITH_FUNC(WARNING) << msg;
+    return STATUS(InternalError, msg, MasterError(MasterErrorPB::OBJECT_NOT_FOUND));
+  }
+
+  // Don't do schema validation/column updates on the parent colocated table.
+  // However, still do the validation for regular colocated tables.
+  if (!is_parent_colocated_table) {
+    Schema persisted_schema;
+    {
+      TRACE("Locking table");
+      auto table_lock = table->LockForRead();
+      RETURN_NOT_OK(table->GetSchema(&persisted_schema));
+    }
+
+    // Ignore 'nullable' attribute - due to difference in implementation
+    // of PgCreateTable::AddColumn() and PgAlterTable::AddColumn().
+    struct CompareColumnsExceptNullable {
+      bool operator ()(const ColumnSchema& a, const ColumnSchema& b) {
+        return ColumnSchema::CompHashKey(a, b) && ColumnSchema::CompSortingType(a, b) &&
+            ColumnSchema::CompTypeInfo(a, b) && ColumnSchema::CompName(a, b);
+      }
+    } comparator;
+    // Schema::Equals() compares only column names & types. It does not compare the column ids.
+    if (!persisted_schema.Equals(schema, comparator)
+        || persisted_schema.column_ids().size() != column_ids.size()) {
+      const string msg = Format(
+          "Invalid created $0 table '$1' in namespace id $2: schema={$3}, expected={$4}",
+          TableType_Name(meta.table_type()), meta.name(), new_namespace_id,
+          persisted_schema, schema);
+      LOG_WITH_FUNC(WARNING) << msg;
+      return STATUS(InternalError, msg, MasterError(MasterErrorPB::SNAPSHOT_FAILED));
+    }
+
+    if (table_data->num_tablets > 0 &&
+        persisted_schema.table_properties().num_tablets() != table_data->num_tablets) {
+      const string msg = Format(
+          "Wrong number of tablets in created $0 table '$1' in namespace id $2: $3 (expected $4)",
+          TableType_Name(meta.table_type()), meta.name(), new_namespace_id,
+          persisted_schema.table_properties().num_tablets(), table_data->num_tablets);
+      LOG_WITH_FUNC(WARNING) << msg;
+      return STATUS(InternalError, msg, MasterError(MasterErrorPB::SNAPSHOT_FAILED));
+    }
+
+    // Update the table column ids if it's not equal to the stored ids.
+    if (persisted_schema.column_ids() != column_ids) {
+      if (meta.table_type() != TableType::PGSQL_TABLE_TYPE) {
+        LOG_WITH_FUNC(WARNING) << "Unexpected wrong column ids in "
+                               << TableType_Name(meta.table_type()) << " table '" << meta.name()
+                               << "' in namespace id " << new_namespace_id;
+      }
+
+      LOG_WITH_FUNC(INFO) << "Restoring column ids in " << TableType_Name(meta.table_type())
+                          << " table '" << meta.name() << "' in namespace id "
+                          << new_namespace_id;
+      auto l = table->LockForWrite();
+      size_t col_idx = 0;
+      for (auto& column : *l.mutable_data()->pb.mutable_schema()->mutable_columns()) {
+        // Expecting here correct schema (columns - order, names, types), but with only wrong
+        // column ids. Checking correct column order and column names below.
+        if (column.name() != schema.column(col_idx).name()) {
+          const string msg = Format(
+              "Unexpected column name for index=$0: name=$1, expected name=$2",
+              col_idx, schema.column(col_idx).name(), column.name());
+          LOG_WITH_FUNC(WARNING) << msg;
+          return STATUS(InternalError, msg, MasterError(MasterErrorPB::SNAPSHOT_FAILED));
+        }
+        // Copy the column id from imported (original) schema.
+        column.set_id(column_ids[col_idx++]);
+      }
+
+      l.mutable_data()->pb.set_next_column_id(schema.max_col_id() + 1);
+      l.mutable_data()->pb.set_version(l->pb.version() + 1);
+      // Update sys-catalog with the new table schema.
+      RETURN_NOT_OK(sys_catalog_->Upsert(leader_ready_term(), table));
+      l.Commit();
+      // Update the new table schema in tablets.
+      RETURN_NOT_OK(SendAlterTableRequest(table));
+    }
   }
 
   // Set the type of the table in the response pb (default is TABLE so only set if colocated).

ent/src/yb/tools/yb-admin_client_ent.cc
@@ -718,10 +718,6 @@ Status ClusterAdminClient::ImportSnapshotMetaFile(const string& file_name,
           return STATUS(IllegalState, "Could not find keyspace name from snapshot metadata");
         }
 
-        YBTableName orig_table_name;
-        orig_table_name.set_namespace_name(meta.namespace_name());
-        orig_table_name.set_table_name(meta.name());
-
         // Update the table name if needed.
         if (update_meta) {
           entry.set_data(meta.SerializeAsString());

commit f5956cfaa1ced62ce5306b1e9f2fc05ae474879c

[#9963] backup: fix to reallow YEDIS on restore
Summary:
Commit e9d782852be1f9e193e132d9e5b1e5ce622d4acb broke backup/restore for
redis tables on the import step because it disallowed table types other
than YCQL and YSQL.  Fix by allowing YEDIS, like before.

Test Plan:
Jenkins: urgent

A dedicated test will be added later; this lands first because it is an
urgent fix.

Reviewers: oleg, bogdan

Reviewed By: bogdan

Subscribers: ybase, bogdan

Differential Revision: https://phabricator.dev.yugabyte.com/D12952

ent/src/yb/master/catalog_manager_ent.cc
@@ -1277,8 +1277,9 @@ Status CatalogManager::ImportTableEntry(const NamespaceMap& namespace_map,
   // Second, if we still didn't find a match...
   if (table == nullptr) {
     VLOG_WITH_PREFIX(3) << "Begin second search";
-    if (meta.table_type() == TableType::YQL_TABLE_TYPE) {
-      // For YCQL, simply create the missing table.
+    if (meta.table_type() == TableType::YQL_TABLE_TYPE ||
+        meta.table_type() == TableType::REDIS_TABLE_TYPE) {
+      // For YCQL and YEDIS, simply create the missing table.
       RETURN_NOT_OK(RecreateTable(new_namespace_id, table_map, table_data));
     } else if (meta.table_type() == TableType::PGSQL_TABLE_TYPE) {
       // For YSQL, the table must be created via external call. Therefore, continue the search for