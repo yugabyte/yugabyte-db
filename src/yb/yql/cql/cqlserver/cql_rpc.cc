07f5797a4e184e4ae5447985ed6e8922c347337d
Amitanand Aiyer <amitanandaiyer@users.noreply.github.com>
2021-09-15 09:47:49 -0700

    [#10038] YQL: Support for displaying the bind values for a prepared statement(s).
    Summary:
    Not sure if we decided to intentionally ignore the bind values
    while displaying prepared statements in https://phabricator.dev.yugabyte.com/D3093.
    
    Having these could be useful for debugging batched/prepared statements
    
    Support for displaying the bind values for a prepared statement(s).
    
    Test Plan:
    launch a local cluster. Run CassandraBatchedKeyValue workload.
    check /rpcz for details (screenshot attached).
    
    Reviewers: mihnea, kannan, oleg, pjain
    
    Reviewed By: pjain
    
    Subscribers: yql
    
    Differential Revision: https://phabricator.dev.yugabyte.com/D12380

--- src/yb/yql/cql/cqlserver/cql_rpc.cc
+++ src/yb/yql/cql/cqlserver/cql_rpc.cc
@@ -41,6 +41,8 @@ DEFINE_int32(rpcz_max_cql_batch_dump_count, 4_KB,
              "The maximum number of CQL batch elements in the RPCZ dump.");
 DEFINE_bool(throttle_cql_calls_on_soft_memory_limit, true,
             "Whether to reject CQL calls when soft memory limit is reached.");
+DEFINE_bool(display_bind_params_in_cql_details, true,
+            "Whether to show bind params for CQL calls details in the RPCZ dump.");
 
 constexpr int kDropPolicy = 1;
 constexpr int kRejectPolicy = 0;
@@ -251,17 +253,23 @@ void CQLInboundCall::GetCallDetails(rpc::RpcCallInProgressPB *call_in_progress_p
       details_pb->set_sql_string((static_cast<const ql::PrepareRequest&>(*request)).query()
                                     .substr(0, FLAGS_rpcz_max_cql_query_dump_size));
       return;
-    case CQLMessage::Opcode::EXECUTE:
+    case CQLMessage::Opcode::EXECUTE: {
       call_in_progress->set_type("EXECUTE");
       details_pb = call_in_progress->add_call_details();
-      query_id = (static_cast<const ql::ExecuteRequest&>(*request)).query_id();
+      const auto& exec_request = static_cast<const ql::ExecuteRequest&>(*request);
+      query_id = exec_request.query_id();
       details_pb->set_sql_id(b2a_hex(query_id));
       statement_ptr = service_impl_->GetPreparedStatement(query_id);
       if (statement_ptr != nullptr) {
         details_pb->set_sql_string(statement_ptr->text()
                                        .substr(0, FLAGS_rpcz_max_cql_query_dump_size));
       }
+      if (FLAGS_display_bind_params_in_cql_details) {
+        details_pb->set_params(yb::ToString(exec_request.params().values)
+                                   .substr(0, FLAGS_rpcz_max_cql_query_dump_size));
+      }
       return;
+    }
     case CQLMessage::Opcode::QUERY:
       call_in_progress->set_type("QUERY");
       details_pb = call_in_progress->add_call_details();
@@ -277,13 +285,12 @@ void CQLInboundCall::GetCallDetails(rpc::RpcCallInProgressPB *call_in_progress_p
           details_pb->set_sql_id(b2a_hex(batchQuery.query_id));
           statement_ptr = service_impl_->GetPreparedStatement(batchQuery.query_id);
           if (statement_ptr != nullptr) {
-            if (statement_ptr->text().size() > FLAGS_rpcz_max_cql_query_dump_size) {
-              string short_text = statement_ptr->text()
-                  .substr(0, FLAGS_rpcz_max_cql_query_dump_size);
-              details_pb->set_sql_string(short_text);
-            } else {
-              details_pb->set_sql_string(statement_ptr->text());
-            }
+            details_pb->set_sql_string(
+                statement_ptr->text().substr(0, FLAGS_rpcz_max_cql_query_dump_size));
+          }
+          if (FLAGS_display_bind_params_in_cql_details) {
+            details_pb->set_params(yb::ToString(batchQuery.params.values)
+                                       .substr(0, FLAGS_rpcz_max_cql_query_dump_size));
           }
         } else {
           details_pb->set_sql_string(batchQuery.query
