52dfcce547ac56c1090389151314107b2c171eac
Mikhail Bautin <mbautin@users.noreply.github.com>
2021-09-18 14:42:52 -0700

    Improve OS detection in thirdparty_tool
    Summary:
    - Use the new sys-detection module (https://pypi.org/project/sys-detection/) to detect the OS version for the purpose of selecting the third-party archive to download.
    - Improve archive selection for RedHat compatible distributions: prefer exact match, and use an inexact match of a compatible distribution (Alpine, CentOS, etc.) if there is no exact match available.
    - Move aarch64 third-party archive metadata to a separate file that is not being updated automatically yet.
    
    Test Plan:
    build-support/thirdparty_tool --update
    
    Jenkins
    
    Reviewers: jharveysmith, pjain, steve.varnau
    
    Reviewed By: steve.varnau
    
    Subscribers: ybase
    
    Differential Revision: https://phabricator.dev.yugabyte.com/D13064

--- python/yb/thirdparty_tool.py
+++ python/yb/thirdparty_tool.py
@@ -23,40 +23,60 @@ import re
 import os
 import logging
 import argparse
+from autorepr import autorepr  # type: ignore
 
 from github import Github
 from github.GitRelease import GitRelease
 
-from typing import Dict, List, Any, Optional
+from typing import DefaultDict, Dict, List, Any, Optional, Pattern, Tuple
 from datetime import datetime
 
 from yb.common_util import (
     init_env, YB_SRC_ROOT, read_file, load_yaml_file, write_yaml_file, to_yaml_str)
-from yb.os_detection import get_short_os_name, get_arch
+from sys_detection import local_sys_conf, SHORT_OS_NAME_REGEX_STR
 
+from collections import defaultdict
 
 THIRDPARTY_ARCHIVES_REL_PATH = os.path.join('build-support', 'thirdparty_archives.yml')
+MANUAL_THIRDPARTY_ARCHIVES_REL_PATH = os.path.join(
+    'build-support', 'thirdparty_archives_manual.yml')
 
 NUM_TOP_COMMITS = 10
 
 DOWNLOAD_URL_PREFIX = 'https://github.com/yugabyte/yugabyte-db-thirdparty/releases/download/'
-TAG_RE = re.compile(
-    r'^v(?:(?P<branch_name>[0-9.]+)-)?'
-    r'(?P<timestamp>[0-9]+)-'
-    r'(?P<sha_prefix>[0-9a-f]+)'
-    r'(?:-(?:x86_64|arm64))?'
-    r'(?:-(?P<os>(?:ubuntu|centos|macos|alpine)[a-z0-9.]*))'
-    r'(?:-(?P<architecture>x86_64|aarch64))?'
-    r'(?:-(?P<is_linuxbrew>linuxbrew))?'
+
+ARCH_REGEX_STR = '|'.join(['x86_64', 'aarch64'])
+
+
+def get_arch_regex(index: int) -> str:
+    """
+    There are two places where the architecture could appear in the third-party archive release tag.
+    We make them available under "architecture1" and "architecture2" capture group names.
+    """
+    return r'(?:-(?P<architecture%d>%s))?' % (index, ARCH_REGEX_STR)
+
+
+TAG_RE = re.compile(''.join([
+    r'^v(?:(?P<branch_name>[0-9.]+)-)?',
+    r'(?P<timestamp>[0-9]+)-',
+    r'(?P<sha_prefix>[0-9a-f]+)',
+    get_arch_regex(1),
+    r'(?:-(?P<os>(?:%s)[a-z0-9.]*))' % SHORT_OS_NAME_REGEX_STR,
+    get_arch_regex(2),
+    r'(?:-(?P<is_linuxbrew>linuxbrew))?',
     # "devtoolset" really means just "gcc" here. We should replace it with "gcc" in release names.
-    r'(?:-(?P<compiler_type>(?:gcc|clang|devtoolset-?)[a-z0-9.]+))?'
-    r'$')
+    r'(?:-(?P<compiler_type>(?:gcc|clang|devtoolset-?)[a-z0-9.]+))?',
+    r'$',
+]))
 
 # We will store the SHA1 to be used for the local third-party checkout under this key.
 SHA_FOR_LOCAL_CHECKOUT_KEY = 'sha_for_local_checkout'
 
 UBUNTU_OS_TYPE_RE = re.compile(r'^(ubuntu)([0-9]{2})([0-9]{2})$')
 RHEL_FAMILY_RE = re.compile(r'^(almalinux|centos|rhel)([0-9]+)$')
+
+# Skip these problematic tags.
+BROKEN_TAGS = set(['v20210907234210-47a70bc7dc-centos7-x86_64-linuxbrew-gcc5'])
 
 
 def get_archive_name_from_tag(tag: str) -> str:
@@ -72,26 +92,32 @@ def adjust_os_type(os_type: str) -> str:
 
 
 def compatible_os(archive_os: str, target_os: str) -> bool:
-    centos_like = RHEL_FAMILY_RE.match(target_os)
-    if centos_like:
-        target_os = f'centos{centos_like.group(2)}'
-
+    rhel_like1 = RHEL_FAMILY_RE.match(archive_os)
+    rhel_like2 = RHEL_FAMILY_RE.match(target_os)
+    if rhel_like1 and rhel_like2 and rhel_like1.group(2) == rhel_like2.group(2):
+        return True
     return archive_os == target_os
 
 
 class YBDependenciesRelease:
 
-    FIELDS_TO_PERSIST = ['os_type', 'architecture', 'compiler_type', 'tag', 'sha']
+    # The list of fields without the release tag. The tag is special because it includes the
+    # timestamp, so by repeating a build on the same commit in yugabyte-db-thirdparty, we could get
+    # multiple releases that have the same OS/architecture/compiler type/SHA but different tags.
+    # Therefore we distinguish between "key with tag" and "key with no tag"
+    KEY_FIELDS_NO_TAG = ['os_type', 'architecture', 'compiler_type', 'sha']
+    KEY_FIELDS_WITH_TAG = KEY_FIELDS_NO_TAG + ['tag']
+
+    os_type: str
+    architecture: str
+    compiler_type: str
+    sha: str
+    tag: str
 
     github_release: GitRelease
-    sha: str
 
     timestamp: str
     url: str
-    compiler_type: str
-    os_type: str
-    architecture: str
-    tag: str
     branch_name: Optional[str]
 
     def __init__(self, github_release: GitRelease) -> None:
@@ -113,7 +139,12 @@ class YBDependenciesRelease:
 
         self.timestamp = group_dict['timestamp']
         self.os_type = adjust_os_type(group_dict['os'])
-        self.architecture = group_dict['architecture']
+
+        arch1 = group_dict['architecture1']
+        arch2 = group_dict['architecture2']
+        if arch1 is not None and arch2 is not None and arch1 != arch2:
+            raise ValueError("Contradicting values of arhitecture in tag '%s'" % tag)
+        self.architecture = arch1 or arch2
         self.is_linuxbrew = bool(group_dict.get('is_linuxbrew'))
 
         compiler_type = group_dict.get('compiler_type')
@@ -137,7 +168,12 @@ class YBDependenciesRelease:
     def validate_url(self) -> None:
         asset_urls = [asset.browser_download_url for asset in self.github_release.get_assets()]
 
-        assert(len(asset_urls) == 2)
+        if len(asset_urls) != 2:
+            raise ValueError(
+                "Expected to find exactly two asset URLs for a release "
+                "(one for the .tar.gz, the other for the checksum), "
+                f"but found {len(asset_urls)}: {asset_urls}")
+
         non_checksum_urls = [url for url in asset_urls if not url.endswith('.sha256')]
         assert(len(non_checksum_urls) == 1)
         self.url = non_checksum_urls[0]
@@ -158,17 +194,18 @@ class YBDependenciesRelease:
                 f"actual name: {archive_basename}, url: {self.url}")
 
     def as_dict(self) -> Dict[str, str]:
-        return {k: getattr(self, k) for k in self.FIELDS_TO_PERSIST}
+        return {k: getattr(self, k) for k in self.KEY_FIELDS_WITH_TAG}
 
-    def get_sort_key(self) -> List[str]:
-        return [getattr(self, k) for k in self.FIELDS_TO_PERSIST]
+    def get_sort_key(self, include_tag: bool = True) -> Tuple[str, ...]:
+        return tuple(
+            getattr(self, k) for k in
+            (self.KEY_FIELDS_WITH_TAG if include_tag else self.KEY_FIELDS_NO_TAG))
 
     def is_consistent_with_yb_version(self, yb_version: str) -> bool:
         return (self.branch_name is None or
                 yb_version.startswith((self.branch_name + '.', self.branch_name + '-')))
 
-    def __str__(self) -> str:
-        return str(self.as_dict())
+    __str__ = __repr__ = autorepr(KEY_FIELDS_WITH_TAG)
 
 
 class ReleaseGroup:
@@ -231,6 +268,9 @@ def parse_args() -> argparse.Namespace:
     parser.add_argument(
         '--verbose',
         help='Verbose debug information')
+    parser.add_argument(
+        '--tag-filter-regex',
+        help='Only look at tags satisfying this regular expression.')
 
     if len(sys.argv) == 1:
         parser.print_help(sys.stderr)
@@ -241,6 +281,10 @@ def parse_args() -> argparse.Namespace:
 
 def get_archive_metadata_file_path() -> str:
     return os.path.join(YB_SRC_ROOT, THIRDPARTY_ARCHIVES_REL_PATH)
+
+
+def get_manual_archive_metadata_file_path() -> str:
+    return os.path.join(YB_SRC_ROOT, MANUAL_THIRDPARTY_ARCHIVES_REL_PATH)
 
 
 def get_github_token(token_file_path: Optional[str]) -> Optional[str]:
@@ -257,85 +301,142 @@ def get_github_token(token_file_path: Optional[str]) -> Optional[str]:
     return github_token
 
 
-def update_archive_metadata_file(github_token_file: Optional[str]) -> None:
-    yb_version = read_file(os.path.join(YB_SRC_ROOT, 'version.txt')).strip()
+class MetadataUpdater:
+    github_token_file_path: str
+    tag_filter_pattern: Optional[Pattern]
 
-    archive_metadata_path = get_archive_metadata_file_path()
-    logging.info(f"Updating third-party archive metadata file in {archive_metadata_path}")
+    def __init__(
+            self,
+            github_token_file_path: str,
+            tag_filter_regex_str: Optional[str]) -> None:
+        self.github_token_file_path = github_token_file_path
+        if tag_filter_regex_str:
+            self.tag_filter_pattern = re.compile(tag_filter_regex_str)
+        else:
+            self.tag_filter_pattern = None
 
-    github_client = Github(get_github_token(github_token_file))
-    repo = github_client.get_repo('yugabyte/yugabyte-db-thirdparty')
+    def update_archive_metadata_file(self) -> None:
+        yb_version = read_file(os.path.join(YB_SRC_ROOT, 'version.txt')).strip()
 
-    releases_by_commit: Dict[str, ReleaseGroup] = {}
+        archive_metadata_path = get_archive_metadata_file_path()
+        logging.info(f"Updating third-party archive metadata file in {archive_metadata_path}")
 
-    num_skipped_old_tag_format = 0
-    num_skipped_wrong_branch = 0
-    num_releases_found = 0
-    for release in repo.get_releases():
-        sha: str = release.target_commitish
-        assert(isinstance(sha, str))
-        tag_name = release.tag_name
-        if len(tag_name.split('-')) <= 2:
-            logging.debug(f"Skipping release tag: {tag_name} (old format, too few components)")
-            num_skipped_old_tag_format += 1
-            continue
+        github_client = Github(get_github_token(self.github_token_file_path))
+        repo = github_client.get_repo('yugabyte/yugabyte-db-thirdparty')
 
-        yb_dep_release = YBDependenciesRelease(release)
-        if not yb_dep_release.is_consistent_with_yb_version(yb_version):
-            logging.debug(f"Skipping release tag: {tag_name} (does not match version {yb_version}")
-            num_skipped_wrong_branch += 1
-            continue
+        releases_by_commit: Dict[str, ReleaseGroup] = {}
+        num_skipped_old_tag_format = 0
+        num_skipped_wrong_branch = 0
+        num_releases_found = 0
 
-        if sha not in releases_by_commit:
-            releases_by_commit[sha] = ReleaseGroup(sha)
+        for release in repo.get_releases():
+            sha: str = release.target_commitish
+            assert(isinstance(sha, str))
+            tag_name = release.tag_name
+            if len(tag_name.split('-')) <= 2:
+                logging.debug(f"Skipping release tag: {tag_name} (old format, too few components)")
+                num_skipped_old_tag_format += 1
+                continue
+            if self.tag_filter_pattern and not self.tag_filter_pattern.match(tag_name):
+                logging.info(f'Skipping tag {tag_name}, does not match the filter')
+                continue
 
-        num_releases_found += 1
-        logging.info(f"Found release: {yb_dep_release}")
-        releases_by_commit[sha].add_release(yb_dep_release)
+            yb_dep_release = YBDependenciesRelease(release)
+            if not yb_dep_release.is_consistent_with_yb_version(yb_version):
+                logging.debug(
+                    f"Skipping release tag: {tag_name} (does not match version {yb_version}")
+                num_skipped_wrong_branch += 1
+                continue
 
-    if num_skipped_old_tag_format > 0:
-        logging.info(f"Skipped {num_skipped_old_tag_format} releases due to old tag format")
-    if num_skipped_wrong_branch > 0:
-        logging.info(f"Skipped {num_skipped_wrong_branch} releases due to branch mismatch")
-    logging.info(
-        f"Found {num_releases_found} releases for {len(releases_by_commit)} different commits")
-    latest_group_by_max = max(
-        releases_by_commit.values(), key=ReleaseGroup.get_max_creation_timestamp)
-    latest_group_by_min = max(
-        releases_by_commit.values(), key=ReleaseGroup.get_min_creation_timestamp)
-    if latest_group_by_max is not latest_group_by_min:
-        raise ValueError(
-            "Overlapping releases for different commits. No good way to identify latest release: "
-            f"e.g. {latest_group_by_max.sha} and {latest_group_by_min.sha}.")
+            if sha not in releases_by_commit:
+                releases_by_commit[sha] = ReleaseGroup(sha)
 
-    latest_group = latest_group_by_max
+            num_releases_found += 1
+            logging.info(f"Found release: {yb_dep_release}")
+            releases_by_commit[sha].add_release(yb_dep_release)
 
-    sha = latest_group.sha
-    logging.info(
-        f"Latest released yugabyte-db-thirdparty commit: f{sha}. "
-        f"Released at: {latest_group.get_max_creation_timestamp()}.")
+        if num_skipped_old_tag_format > 0:
+            logging.info(f"Skipped {num_skipped_old_tag_format} releases due to old tag format")
+        if num_skipped_wrong_branch > 0:
+            logging.info(f"Skipped {num_skipped_wrong_branch} releases due to branch mismatch")
+        logging.info(
+            f"Found {num_releases_found} releases for {len(releases_by_commit)} different commits")
+        latest_group_by_max = max(
+            releases_by_commit.values(), key=ReleaseGroup.get_max_creation_timestamp)
+        latest_group_by_min = max(
+            releases_by_commit.values(), key=ReleaseGroup.get_min_creation_timestamp)
+        if latest_group_by_max is not latest_group_by_min:
+            raise ValueError(
+                "Overlapping releases for different commits. No good way to identify latest "
+                "release: e.g. {latest_group_by_max.sha} and {latest_group_by_min.sha}.")
 
-    new_metadata: Dict[str, Any] = {
-        SHA_FOR_LOCAL_CHECKOUT_KEY: sha,
-        'archives': []
-    }
-    releases_for_one_commit = latest_group.releases
-    for yb_thirdparty_release in releases_for_one_commit:
-        yb_thirdparty_release.validate_url()
+        latest_group = latest_group_by_max
 
-    releases_for_one_commit.sort(key=YBDependenciesRelease.get_sort_key)
+        sha = latest_group.sha
+        logging.info(
+            f"Latest released yugabyte-db-thirdparty commit: f{sha}. "
+            f"Released at: {latest_group.get_max_creation_timestamp()}.")
 
-    for yb_thirdparty_release in releases_for_one_commit:
-        new_metadata['archives'].append(yb_thirdparty_release.as_dict())
+        new_metadata: Dict[str, Any] = {
+            SHA_FOR_LOCAL_CHECKOUT_KEY: sha,
+            'archives': []
+        }
+        releases_for_one_commit = [
+            rel for rel in latest_group.releases
+            if rel.tag not in BROKEN_TAGS
+        ]
 
-    write_yaml_file(new_metadata, archive_metadata_path)
-    logging.info(
-        f"Wrote information for {len(releases_for_one_commit)} pre-built yugabyte-db-thirdparty "
-        f"archives to {archive_metadata_path}.")
+        releases_by_key_without_tag: DefaultDict[Tuple[str, ...], List[YBDependenciesRelease]] = \
+            defaultdict(list)
+
+        for yb_thirdparty_release in releases_for_one_commit:
+            yb_thirdparty_release.validate_url()
+            releases_by_key_without_tag[
+                yb_thirdparty_release.get_sort_key(include_tag=False)
+            ].append(yb_thirdparty_release)
+
+        keys_with_duplicate_releases: List[Tuple[str, ...]] = []
+        for key_without_tag, releases_for_key in releases_by_key_without_tag.items():
+            if len(releases_for_key) > 1:
+                logging.info(
+                    "Multiple releases found for the same key (excluding the tag). "
+                    "Key: %s, releases: %s" % (
+                        key_without_tag,
+                        releases_for_key))
+                keys_with_duplicate_releases.append(key_without_tag)
+        if keys_with_duplicate_releases:
+            raise ValueError(
+                "Multiple releases found for these keys: %s" % keys_with_duplicate_releases)
+
+        releases_for_one_commit.sort(key=YBDependenciesRelease.get_sort_key)
+
+        for yb_thirdparty_release in releases_for_one_commit:
+            new_metadata['archives'].append(yb_thirdparty_release.as_dict())
+
+        write_yaml_file(new_metadata, archive_metadata_path)
+        logging.info(
+            f"Wrote information for {len(releases_for_one_commit)} pre-built "
+            f"yugabyte-db-thirdparty archives to {archive_metadata_path}.")
 
 
 def load_metadata() -> Dict[str, Any]:
     return load_yaml_file(get_archive_metadata_file_path())
+
+
+def load_manual_metadata() -> Dict[str, Any]:
+    return load_yaml_file(get_manual_archive_metadata_file_path())
+
+
+def filter_for_os(archive_candidates: List[Dict[str, str]], os_type: str) -> List[Dict[str, str]]:
+    filtered_exactly = [
+        candidate for candidate in archive_candidates if candidate['os_type'] == os_type
+    ]
+    if filtered_exactly:
+        return filtered_exactly
+    return [
+        candidate for candidate in archive_candidates
+        if compatible_os(candidate['os_type'], os_type)
+    ]
 
 
 def get_download_url(
@@ -344,17 +445,16 @@ def get_download_url(
         os_type: Optional[str],
         architecture: Optional[str]) -> str:
     if not os_type:
-        os_type = get_short_os_name()
+        os_type = local_sys_conf().short_os_name_and_version()
     if not architecture:
-        architecture = get_arch()
+        architecture = local_sys_conf().architecture
 
     candidates: List[Any] = []
     available_archives = metadata['archives']
     for archive in available_archives:
-        if compatible_os(archive['os_type'], os_type) \
-           and archive['compiler_type'] == compiler_type \
-           and archive['architecture'] == architecture:
+        if archive['compiler_type'] == compiler_type and archive['architecture'] == architecture:
             candidates.append(archive)
+    candidates = filter_for_os(candidates, os_type)
 
     if len(candidates) == 1:
         tag = candidates[0]['tag']
@@ -374,9 +474,16 @@ def get_download_url(
 
         logging.info(f"Available release archives:\n{to_yaml_str(available_archives)}")
         raise ValueError(
-            "Could not find a third-party release archive to download for OS type "
-            f"'{os_type}' and compiler type '{compiler_type}'. "
+            "Could not find a third-party release archive to download for "
+            f"OS type '{os_type}', "
+            f"compiler type '{compiler_type}', and "
+            f"architecture '{architecture}'. "
             "Please see the list of available thirdparty archives above.")
+
+    i = 1
+    for candidate in candidates:
+        logging.warning("Third-party release archive candidate #%d: %s", i, candidate)
+        i += 1
 
     raise ValueError(
         f"Found too many third-party release archives to download for OS type "
@@ -387,12 +494,19 @@ def main() -> None:
     args = parse_args()
     init_env(verbose=args.verbose)
     if args.update:
-        update_archive_metadata_file(args.github_token_file)
+        updater = MetadataUpdater(
+            github_token_file_path=args.github_token_file,
+            tag_filter_regex_str=args.tag_filter_regex)
+        updater.update_archive_metadata_file()
         return
+
     metadata = load_metadata()
+    manual_metadata = load_manual_metadata()
     if args.get_sha1:
         print(metadata[SHA_FOR_LOCAL_CHECKOUT_KEY])
         return
+
+    metadata['archives'].extend(manual_metadata['archives'])
 
     if args.save_download_url_to_file:
         if not args.compiler_type:
