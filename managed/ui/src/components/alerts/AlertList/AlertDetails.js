965657bc5f236cad90fb34b147bb7dea2f74444d
Karthik K <kkannan@yugabyte.com>
2021-09-23 14:00:29 +0530

    [PLAT-1766][Platform][Alerts] UI: Cleanup
    Summary:
    Alerts UI was revamped to address some ui issues.
    
    Changes done:
    Added sort functionality to source and name
    Added tag to the source name
    Navigation link for universe
    sorted alert history chronologically
    changed normal select to react-select to support typeahead
    Changes order of filters
    other small UI and Typo Fixes
    
    Test Plan:
    Tested Manually.
    
    {F19636}
    
    {F19637}
    
    Reviewers: cpadinjareveettil, ssutar
    
    Reviewed By: ssutar
    
    Subscribers: jenkins-bot, ui
    
    Differential Revision: https://phabricator.dev.yugabyte.com/D13148

--- managed/ui/src/components/alerts/AlertList/AlertDetails.js
+++ managed/ui/src/components/alerts/AlertList/AlertDetails.js
@@ -7,6 +7,20 @@ const findValueforlabel = (labels, labelToFind) => {
   return label ? label.value : '';
 };
 
+const getSourceName = (alertDetails) => {
+  const source_name = findValueforlabel(alertDetails.labels, 'source_name');
+  if (alertDetails.configurationType !== 'UNIVERSE') {
+    return source_name;
+  }
+  const universeUUID = findValueforlabel(alertDetails.labels, 'source_uuid');
+
+  return (
+    <a target="_blank" rel="noopener noreferrer" className="universeLink" href={`/universes/${universeUUID}`}>
+      {source_name}
+    </a>
+  );
+};
+
 function getSeverityLabel(severity) {
   let labelType = '';
   switch (severity) {
@@ -39,7 +53,6 @@ export default class AlertDetails extends Component {
 
     if (!alertDetails) return null;
 
-    const source_name = findValueforlabel(alertDetails.labels, 'source_name');
     return (
       <div id="universe-tab-panel-pane-queries" className={'alert-details'}>
         <div className={`side-panel`}>
@@ -51,11 +64,22 @@ export default class AlertDetails extends Component {
           </div>
           <div className="side-panel__content">
             <Row>
-              <Col lg={12} title={alertDetails.message} className="alert-message noLeftPadding">
-                {alertDetails.message}
+              <Col className="alert-label noLeftPadding" xs={12} md={12} lg={12}>
+                <h6 className="alert-label-header">Name</h6>
+                <div title={alertDetails.name} className="alert-label-value">
+                  {alertDetails.name}
+                </div>
               </Col>
             </Row>
             <Row>
+              <Col
+                lg={12}
+                title={alertDetails.message}
+                className="alert-label noLeftPadding noMarginBottom"
+              >
+                <h6 className="alert-label-header">DESCRIPTION</h6>
+                <div className="alert-label-value">{alertDetails.message}</div>
+              </Col>
               <Col lg={12} className="noLeftPadding">
                 {getSeverityLabel(alertDetails.severity)}
               </Col>
@@ -63,22 +87,22 @@ export default class AlertDetails extends Component {
             <Row>
               <Col lg={12} className="noLeftPadding">
                 <Row className="marginTop">
-                  <Col className="alert-label noLeftPadding" xs={6} md={6} lg={3}>
+                  <Col className="alert-label noLeftPadding" xs={12} md={12} lg={12}>
                     <h6 className="alert-label-header">Source</h6>
-                    <div title={source_name} className="alert-label-value">
-                      {source_name}
-                    </div>
+                    <div className="alert-label-value">{getSourceName(alertDetails)}</div>
                   </Col>
-                  <Col className="alert-label" xs={6} md={6} lg={3}>
+                </Row>
+                <Row>
+                  <Col className="alert-label noLeftPadding" xs={6} md={6} lg={3}>
                     <h6 className="alert-label-header">Start</h6>
                     <div label={alertDetails.createTime} className="alert-label-value">
                       {alertDetails.createTime}
                     </div>
                   </Col>
-                  <Col className="alert-label" xs={6} md={6} lg={3}>
+                  <Col className="alert-label noLeftPadding" xs={6} md={6} lg={3}>
                     <h6 className="alert-label-header">End</h6>
                     <div label={alertDetails.acknowledgedTime} className="alert-label-value">
-                      {alertDetails.acknowledgedTime}
+                      {alertDetails.resolvedTime ?? '-'}
                     </div>
                   </Col>
                 </Row>
@@ -113,13 +137,9 @@ export default class AlertDetails extends Component {
                 <h5>History</h5>
                 <div className="alert-history">
                   <ul>
-                    {alertDetails.createTime && (
+                    {alertDetails.resolvedTime && (
                       <li className="alert-history-item">
-                        <div className="content">
-                          <div className="timeline-flow">
-                            Alert created on {alertDetails.createTime}
-                          </div>
-                        </div>
+                        <div className="content">Alert resolved on {alertDetails.resolvedTime}</div>
                       </li>
                     )}
                     {alertDetails.acknowledgedTime && (
@@ -129,9 +149,13 @@ export default class AlertDetails extends Component {
                         </div>
                       </li>
                     )}
-                    {alertDetails.resolvedTime && (
+                    {alertDetails.createTime && (
                       <li className="alert-history-item">
-                        <div className="content">Alert resolved on {alertDetails.resolvedTime}</div>
+                        <div className="content">
+                          <div className="timeline-flow">
+                            Alert Triggered on {alertDetails.createTime}
+                          </div>
+                        </div>
                       </li>
                     )}
                   </ul>
