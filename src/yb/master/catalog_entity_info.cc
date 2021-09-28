yugabyte/yugabyte-db

commit cd94edc31fd750422357d8d003d4aff0727ef3a4

[#9862] PITR: Allow consecutive restore
Summary:
Currently if table was created after restoration time, it is deleted during restore.
It blocks us from restoring to some later time, where this table is present.

This diff addresses the issue by marking such tables and tablets as hidden, instead of deleting them.

Test Plan: YbAdminSnapshotScheduleTest.ConsecutiveRestore

Reviewers: bogdan, skedia

Reviewed By: skedia

Subscribers: ybase

Differential Revision: https://phabricator.dev.yugabyte.com/D12955

src/yb/master/catalog_entity_info.cc
@@ -348,53 +348,64 @@ TableType TableInfo::GetTableType() const {
   return LockForRead()->pb.table_type();
 }
 
-bool TableInfo::RemoveTablet(const string& partition_key_start) {
-  std::lock_guard<decltype(lock_)> l(lock_);
-  return EraseKeyReturnValuePtr(&tablet_map_, partition_key_start) != nullptr;
-}
-
-void TableInfo::AddTablet(TabletInfo *tablet) {
+void TableInfo::AddTablet(const TabletInfoPtr& tablet) {
   std::lock_guard<decltype(lock_)> l(lock_);
   AddTabletUnlocked(tablet);
 }
 
-void TableInfo::AddTablets(const vector<TabletInfo*>& tablets) {
+void TableInfo::AddTablets(const TabletInfos& tablets) {
   std::lock_guard<decltype(lock_)> l(lock_);
-  for (TabletInfo *tablet : tablets) {
+  for (const auto& tablet : tablets) {
     AddTabletUnlocked(tablet);
   }
 }
 
-void TableInfo::RemoveSplitTablet(const TabletId& tablet_id) {
+bool TableInfo::RemoveTablet(const TabletId& tablet_id, InactiveOnly inactive_only) {
   std::lock_guard<decltype(lock_)> l(lock_);
-  split_tablets_.erase(tablet_id);
+  auto it = tablets_.find(tablet_id);
+  if (it == tablets_.end()) {
+    return false;
+  }
+  bool result = false;
+  auto partitions_it = partitions_.find(
+      it->second->metadata().dirty().pb.partition().partition_key_start());
+  if (partitions_it != partitions_.end() && partitions_it->second == it->second) {
+    if (inactive_only) {
+      return false;
+    }
+    partitions_.erase(partitions_it);
+    result = true;
+  }
+  tablets_.erase(it);
+  return result;
 }
 
-void TableInfo::AddTabletUnlocked(TabletInfo* tablet) {
-  const auto& tablet_meta = tablet->metadata().dirty().pb;
-  const auto& partition_key_start = tablet_meta.partition().partition_key_start();
-  auto it = tablet_map_.find(partition_key_start);
-  if (it == tablet_map_.end()) {
-    tablet_map_.emplace(partition_key_start, tablet);
+void TableInfo::AddTabletUnlocked(const TabletInfoPtr& tablet) {
+  const auto& dirty = tablet->metadata().dirty();
+  const auto& tablet_meta = dirty.pb;
+  tablets_.emplace(tablet->id(), tablet.get());
+
+  if (dirty.is_hidden() || dirty.is_deleted()) {
     return;
   }
 
-  const auto old_split_depth = it->second->LockForRead()->pb.split_depth();
-  if (tablet_meta.split_depth() < old_split_depth) {
-    // This can happen during LoadSysCatalogDataTask on newly elected master leader.
-    // We need to remember not yet deleted tablet that was split in order to be able to delete it
-    // with the table in case of drop table request.
-    split_tablets_.emplace(tablet->id(), tablet);
+  const auto& partition_key_start = tablet_meta.partition().partition_key_start();
+  auto p = partitions_.emplace(partition_key_start, tablet.get());
+  if (p.second) {
     return;
   }
-  VLOG(1) << "Replacing tablet " << it->second->tablet_id()
-          << " (split_depth = " << old_split_depth << ")"
-          << " with tablet " << tablet->tablet_id()
-          << " (split_depth = " << tablet_meta.split_depth() << ")";
+
+  auto it = p.first;
+  const auto old_split_depth = it->second->LockForRead()->pb.split_depth();
   if (tablet_meta.split_depth() > old_split_depth) {
-    split_tablets_.emplace(it->second->id(), it->second);
+    VLOG(1) << "Replacing tablet " << it->second->tablet_id()
+            << " (split_depth = " << old_split_depth << ")"
+            << " with tablet " << tablet->tablet_id()
+            << " (split_depth = " << tablet_meta.split_depth() << ")";
+    it->second = tablet.get();
+    return;
   }
-  it->second = tablet;
+
   // TODO: can we assert that the replaced tablet is not in Running state?
   // May be a little tricky since we don't know whether to look at its committed or
   // uncommitted state.
@@ -410,31 +421,33 @@ void TableInfo::GetTabletsInRange(
     TabletInfos* ret, const int32_t max_returned_locations) const {
   SharedLock<decltype(lock_)> l(lock_);
 
-  TableInfo::TabletInfoMap::const_iterator it, it_end;
+  LOG(INFO) << __func__ << ": " << AsString(partitions_);
+
+  decltype(partitions_)::const_iterator it, it_end;
   if (partition_key_start.empty()) {
-    it = tablet_map_.begin();
+    it = partitions_.begin();
   } else {
-    it = tablet_map_.upper_bound(partition_key_start);
-    if (it != tablet_map_.begin()) {
+    it = partitions_.upper_bound(partition_key_start);
+    if (it != partitions_.begin()) {
       --it;
     }
   }
   if (partition_key_end.empty()) {
-    it_end = tablet_map_.end();
+    it_end = partitions_.end();
   } else {
-    it_end = tablet_map_.upper_bound(partition_key_end);
+    it_end = partitions_.upper_bound(partition_key_end);
   }
 
   int32_t count = 0;
   for (; it != it_end && count < max_returned_locations; ++it) {
-    ret->push_back(make_scoped_refptr(it->second));
+    ret->push_back(it->second);
     count++;
   }
 }
 
 bool TableInfo::IsAlterInProgress(uint32_t version) const {
   SharedLock<decltype(lock_)> l(lock_);
-  for (const TableInfo::TabletInfoMap::value_type& e : tablet_map_) {
+  for (const auto& e : partitions_) {
     if (e.second->reported_schema_version(table_id_) < version) {
       VLOG_WITH_PREFIX_AND_FUNC(3)
           << "ALTER in progress due to tablet "
@@ -446,14 +459,9 @@ bool TableInfo::IsAlterInProgress(uint32_t version) const {
   return false;
 }
 
-bool TableInfo::HasTablets() const {
-  SharedLock<decltype(lock_)> l(lock_);
-  return !tablet_map_.empty();
-}
-
 bool TableInfo::AreAllTabletsHidden() const {
   SharedLock<decltype(lock_)> l(lock_);
-  for (const auto& e : tablet_map_) {
+  for (const auto& e : tablets_) {
     if (!e.second->LockForRead()->is_hidden()) {
       VLOG_WITH_PREFIX_AND_FUNC(4) << "Not hidden tablet: " << e.second->ToString();
       return false;
@@ -464,7 +472,7 @@ bool TableInfo::AreAllTabletsHidden() const {
 
 bool TableInfo::AreAllTabletsDeleted() const {
   SharedLock<decltype(lock_)> l(lock_);
-  for (const auto& e : tablet_map_) {
+  for (const auto& e : tablets_) {
     if (!e.second->LockForRead()->is_deleted()) {
       VLOG_WITH_PREFIX_AND_FUNC(4) << "Not deleted tablet: " << e.second->ToString();
       return false;
@@ -475,7 +483,7 @@ bool TableInfo::AreAllTabletsDeleted() const {
 
 bool TableInfo::IsCreateInProgress() const {
   SharedLock<decltype(lock_)> l(lock_);
-  for (const TableInfo::TabletInfoMap::value_type& e : tablet_map_) {
+  for (const auto& e : tablets_) {
     auto tablet_info_lock = e.second->LockForRead();
     if (!tablet_info_lock->is_running() && tablet_info_lock->pb.split_depth() == 0) {
       return true;
@@ -492,7 +500,7 @@ Status TableInfo::SetIsBackfilling() {
                   MasterError(MasterErrorPB::SPLIT_OR_BACKFILL_IN_PROGRESS));
   }
 
-  for (const auto& tablet_it : tablet_map_) {
+  for (const auto& tablet_it : partitions_) {
     const auto& tablet = tablet_it.second;
     if (tablet->LockForRead()->pb.state() != SysTabletsEntryPB::RUNNING) {
       return STATUS_EC_FORMAT(IllegalState,
@@ -634,31 +642,32 @@ std::unordered_set<std::shared_ptr<MonitoredTask>> TableInfo::GetTasks() {
   return pending_tasks_;
 }
 
-std::size_t TableInfo::NumTablets() const {
+std::size_t TableInfo::NumPartitions() const {
   SharedLock<decltype(lock_)> l(lock_);
-  return tablet_map_.size();
+  return partitions_.size();
 }
 
-void TableInfo::GetAllTablets(
-    TabletInfos* ret, const IncludeSplitTablets include_split_tablets) const {
-  ret->clear();
+TabletInfos TableInfo::GetTablets(IncludeInactive include_inactive) const {
+  TabletInfos result;
   SharedLock<decltype(lock_)> l(lock_);
-  for (const TableInfo::TabletInfoMap::value_type& e : tablet_map_) {
-    ret->push_back(make_scoped_refptr(e.second));
-  }
-  if (include_split_tablets) {
-    for (const auto& e : split_tablets_) {
-      ret->push_back(make_scoped_refptr(e.second));
+  if (include_inactive) {
+    result.reserve(tablets_.size());
+    for (const auto& p : tablets_) {
+      result.push_back(p.second);
+    }
+  } else {
+    result.reserve(partitions_.size());
+    for (const auto& p : partitions_) {
+      result.push_back(p.second);
     }
   }
+  return result;
 }
 
 TabletInfoPtr TableInfo::GetColocatedTablet() const {
   SharedLock<decltype(lock_)> l(lock_);
-  if (colocated()) {
-    for (const TableInfo::TabletInfoMap::value_type& e : tablet_map_) {
-      return make_scoped_refptr(e.second);
-    }
+  if (colocated() && !partitions_.empty()) {
+    return partitions_.begin()->second;
   }
   return nullptr;
 }
@@ -713,8 +722,7 @@ const std::string& PersistentTableInfo::indexed_table_id() const {
 // ================================================================================================
 
 DeletedTableInfo::DeletedTableInfo(const TableInfo* table) : table_id_(table->id()) {
-  vector<scoped_refptr<TabletInfo>> tablets;
-  table->GetAllTablets(&tablets);
+  auto tablets = table->GetTablets();
 
   for (const scoped_refptr<TabletInfo>& tablet : tablets) {
     auto tablet_lock = tablet->LockForRead();

src/yb/master/catalog_entity_info.h
@@ -52,7 +52,8 @@
 namespace yb {
 namespace master {
 
-YB_STRONGLY_TYPED_BOOL(IncludeSplitTablets);
+YB_STRONGLY_TYPED_BOOL(IncludeInactive);
+YB_STRONGLY_TYPED_BOOL(InactiveOnly);
 
 // Drive usage information on a current replica of a tablet.
 // This allows us to look at individual resource usage per replica of a tablet.
@@ -428,17 +429,13 @@ class TableInfo : public RefCountedThreadSafe<TableInfo>,
   }
 
   // Add a tablet to this table.
-  void AddTablet(TabletInfo *tablet);
+  void AddTablet(const TabletInfoPtr& tablet);
 
   // Add multiple tablets to this table.
-  void AddTablets(const std::vector<TabletInfo*>& tablets);
+  void AddTablets(const TabletInfos& tablets);
 
-  // Remove tablet that has been split.
-  void RemoveSplitTablet(const TabletId& tablet_id);
-
-  // Return true if tablet with 'partition_key_start' has been
-  // removed from 'tablet_map_' below.
-  bool RemoveTablet(const std::string& partition_key_start);
+  // Return true if tablet has been removed from 'partitions_' below.
+  bool RemoveTablet(const TabletId& tablet_id, InactiveOnly inactive_only = InactiveOnly::kFalse);
 
   // This only returns tablets which are in RUNNING state.
   void GetTabletsInRange(const GetTableLocationsRequestPB* req, TabletInfos *ret) const;
@@ -447,16 +444,12 @@ class TableInfo : public RefCountedThreadSafe<TableInfo>,
       TabletInfos* ret,
       int32_t max_returned_locations = std::numeric_limits<int32_t>::max()) const;
 
-  std::size_t NumTablets() const;
+  std::size_t NumPartitions() const;
 
   // Get all tablets of the table.
   // If include_split_tablets is true, also returns not yet deleted split parent tablets for
   // which we've already registered child split tablets.
-  void GetAllTablets(
-      TabletInfos* ret,
-      IncludeSplitTablets include_split_tablets = IncludeSplitTablets::kFalse) const;
-
-  bool HasTablets() const;
+  TabletInfos GetTablets(IncludeInactive include_inactive = IncludeInactive::kFalse) const;
 
   // Get the tablet of the table.  The table must be colocated.
   TabletInfoPtr GetColocatedTablet() const;
@@ -528,7 +521,7 @@ class TableInfo : public RefCountedThreadSafe<TableInfo>,
   friend class RefCountedThreadSafe<TableInfo>;
   ~TableInfo();
 
-  void AddTabletUnlocked(TabletInfo* tablet) REQUIRES_SHARED(lock_);
+  void AddTabletUnlocked(const TabletInfoPtr& tablet) REQUIRES_SHARED(lock_);
   void AbortTasksAndCloseIfRequested(bool close);
 
   std::string LogPrefix() const {
@@ -541,9 +534,8 @@ class TableInfo : public RefCountedThreadSafe<TableInfo>,
 
   // Sorted index of tablet start partition-keys to TabletInfo.
   // The TabletInfo objects are owned by the CatalogManager.
-  typedef std::map<PartitionKey, TabletInfo*> TabletInfoMap;
-  TabletInfoMap tablet_map_ GUARDED_BY(lock_);
-  std::unordered_map<TabletId, TabletInfo*> split_tablets_ GUARDED_BY(lock_);
+  std::map<PartitionKey, TabletInfo*> partitions_ GUARDED_BY(lock_);
+  std::unordered_map<TabletId, TabletInfo*> tablets_ GUARDED_BY(lock_);
 
   // Protects tablet_map_ and pending_tasks_.
   mutable rw_spinlock lock_;

ent/src/yb/integration-tests/snapshot-test.cc
@@ -617,8 +617,7 @@ TEST_F(SnapshotTest, ImportSnapshotMeta) {
       scoped_refptr<TableInfo> info = cluster_->mini_master()->master()->catalog_manager()->
           GetTableInfo(table_pair.new_id());
       ASSERT_EQ(old_table_name, info->name());
-      vector<scoped_refptr<TabletInfo>> tablets;
-      info->GetAllTablets(&tablets);
+      auto tablets = info->GetTablets();
       ASSERT_EQ(old_table_num_tablets, tablets.size());
 
       const RepeatedPtrField<IdPairPB>& tablets_map = table_meta.tablets_ids();

ent/src/yb/master/catalog_manager_ent.cc
@@ -413,6 +413,7 @@ void CatalogManager::Submit(std::unique_ptr<tablet::Operation> operation, int64_
 Result<SysRowEntries> CatalogManager::CollectEntries(
     const google::protobuf::RepeatedPtrField<TableIdentifierPB>& table_identifiers,
     CollectFlags flags) {
+  RETURN_NOT_OK(CheckIsLeaderAndReady());
   SysRowEntries entries;
   std::unordered_set<NamespaceId> namespaces;
   auto tables = VERIFY_RESULT(CollectTables(table_identifiers, flags, &namespaces));
@@ -1374,7 +1375,7 @@ Status CatalogManager::ImportTableEntry(const NamespaceMap& namespace_map,
       TRACE("Locking table");
       auto table_lock = table->LockForRead();
       RETURN_NOT_OK(table->GetSchema(&persisted_schema));
-      new_num_tablets = table->NumTablets();
+      new_num_tablets = table->NumPartitions();
     }
 
     // Ignore 'nullable' attribute - due to difference in implementation
@@ -1453,11 +1454,11 @@ Status CatalogManager::ImportTableEntry(const NamespaceMap& namespace_map,
     }
   }
 
-  vector<scoped_refptr<TabletInfo>> new_tablets;
+  TabletInfos new_tablets;
   {
     TRACE("Locking table");
     auto table_lock = table->LockForRead();
-    table->GetAllTablets(&new_tablets);
+    new_tablets = table->GetTablets();
   }
 
   for (const scoped_refptr<TabletInfo>& tablet : new_tablets) {
@@ -1607,6 +1608,7 @@ Status CatalogManager::RestoreSysCatalog(
     RETURN_NOT_OK(state.ProcessPgCatalogRestores(
         pg_yb_catalog_version_schema, doc_db, tablet->doc_db(), &write_batch));
   }
+
   // Restore the other tables.
   RETURN_NOT_OK(state.PrepareWriteBatch(schema(), &write_batch));
 
@@ -1620,7 +1622,8 @@ Status CatalogManager::RestoreSysCatalog(
 }
 
 Status CatalogManager::VerifyRestoredObjects(const SnapshotScheduleRestoration& restoration) {
-  auto entries = VERIFY_RESULT(CollectEntriesForSnapshot(restoration.filter.tables().tables()));
+  auto entries = VERIFY_RESULT(CollectEntriesForSnapshot(
+      restoration.schedules[0].second.tables().tables()));
   auto objects_to_restore = restoration.non_system_objects_to_restore;
   VLOG_WITH_PREFIX(1) << "Objects to restore: " << AsString(objects_to_restore);
   for (const auto& entry : entries.entries()) {
@@ -2455,16 +2458,16 @@ Status CatalogManager::CleanUpDeletedCDCStreams(
   std::set<CDCStreamId> failed_streams;
   for (const auto& stream : streams) {
     LOG(INFO) << "Deleting rows for stream " << stream->id();
-    vector<scoped_refptr<TabletInfo>> tablets;
+    TabletInfos tablets;
     scoped_refptr<TableInfo> table;
     {
       TRACE("Acquired catalog manager lock");
       SharedLock lock(mutex_);
       table = FindPtrOrNull(*table_ids_map_, stream->table_id());
     }
-    // GetAllTablets locks lock_ in shared mode.
+    // GetTablets locks lock_ in shared mode.
     if (table) {
-      table->GetAllTablets(&tablets);
+      tablets = table->GetTablets();
     }
 
     for (const auto& tablet : tablets) {

ent/src/yb/master/restore_sys_catalog_state.cc
@@ -167,14 +167,14 @@ Status RestoreSysCatalogState::Process() {
   VLOG_WITH_FUNC(4) << "Restoring namespaces: " << AsString(restoring_objects_.namespaces);
   faststring buffer;
   RETURN_NOT_OK_PREPEND(DetermineEntries(
-      &restoring_objects_,
+      &restoring_objects_, nullptr,
       [this, &buffer](const auto& id, auto* pb) {
         return AddRestoringEntry(id, pb, &buffer);
   }), "Determine restoring entries failed");
 
   VLOG_WITH_FUNC(2) << "Check existing objects";
   RETURN_NOT_OK_PREPEND(DetermineEntries(
-      &existing_objects_,
+      &existing_objects_, &retained_existing_tables_,
       [this](const auto& id, auto* pb) {
         return CheckExistingEntry(id, *pb);
   }), "Determine obsolete entries failed");
@@ -193,13 +193,15 @@ Status RestoreSysCatalogState::Process() {
 
 template <class ProcessEntry>
 Status RestoreSysCatalogState::DetermineEntries(
-    Objects* objects, const ProcessEntry& process_entry) {
+    Objects* objects, RetainedExistingTables* retained_existing_tables,
+    const ProcessEntry& process_entry) {
   std::unordered_set<NamespaceId> namespaces;
   std::unordered_set<TableId> tables;
 
+  const auto& filter = restoration_.schedules[0].second;
+
   for (auto& id_and_metadata : objects->namespaces) {
-    if (!VERIFY_RESULT(MatchNamespace(
-            restoration_.filter, id_and_metadata.first, id_and_metadata.second))) {
+    if (!VERIFY_RESULT(MatchNamespace(filter, id_and_metadata.first, id_and_metadata.second))) {
       continue;
     }
     if (!namespaces.insert(id_and_metadata.first).second) {
@@ -212,25 +214,26 @@ Status RestoreSysCatalogState::DetermineEntries(
     VLOG_WITH_FUNC(3) << "Checking: " << id_and_metadata.first << ", "
                       << id_and_metadata.second.ShortDebugString();
 
-    bool match;
     if (TableDeleted(id_and_metadata.second)) {
       continue;
     }
-    if (id_and_metadata.second.has_index_info()) {
-      auto it = objects->tables.find(id_and_metadata.second.index_info().indexed_table_id());
-      if (it == objects->tables.end()) {
-        return STATUS_FORMAT(
-            NotFound, "Indexed table $0 not found for index $1 ($2)",
-            id_and_metadata.second.index_info().indexed_table_id(), id_and_metadata.first,
-            id_and_metadata.second.name());
-      }
-      match = VERIFY_RESULT(MatchTable(restoration_.filter, it->first, it->second));
-    } else {
-      match = VERIFY_RESULT(MatchTable(
-          restoration_.filter, id_and_metadata.first, id_and_metadata.second));
-    }
+    auto& root_table_id_and_metadata = VERIFY_RESULT(objects->FindRootTable(id_and_metadata)).get();
+    auto match = VERIFY_RESULT(MatchTable(
+        filter, root_table_id_and_metadata.first, root_table_id_and_metadata.second));
     if (!match) {
       continue;
+    }
+    if (retained_existing_tables) {
+      auto& retaining_schedules = retained_existing_tables->emplace(
+          id_and_metadata.first, std::vector<SnapshotScheduleId>()).first->second;
+      retaining_schedules.push_back(restoration_.schedules[0].first);
+      for (size_t i = 1; i != restoration_.schedules.size(); ++i) {
+        if (VERIFY_RESULT(MatchTable(
+            restoration_.schedules[i].second, root_table_id_and_metadata.first,
+            root_table_id_and_metadata.second))) {
+          retaining_schedules.push_back(restoration_.schedules[i].first);
+        }
+      }
     }
     // Process pg_catalog tables that need to be restored.
     if (namespaces.insert(id_and_metadata.second.namespace_id()).second) {
@@ -257,6 +260,34 @@ Status RestoreSysCatalogState::DetermineEntries(
             << id_and_metadata.second.ShortDebugString();
   }
   return Status::OK();
+}
+
+Result<const std::pair<const TableId, SysTablesEntryPB>&>
+    RestoreSysCatalogState::Objects::FindRootTable(
+    const std::pair<const TableId, SysTablesEntryPB>& id_and_metadata) {
+  if (!id_and_metadata.second.has_index_info()) {
+    return id_and_metadata;
+  }
+
+  auto it = tables.find(id_and_metadata.second.index_info().indexed_table_id());
+  if (it == tables.end()) {
+    return STATUS_FORMAT(
+        NotFound, "Indexed table $0 not found for index $1 ($2)",
+        id_and_metadata.second.index_info().indexed_table_id(), id_and_metadata.first,
+        id_and_metadata.second.name());
+  }
+  const auto& ref = *it;
+  return ref;
+}
+
+Result<const std::pair<const TableId, SysTablesEntryPB>&>
+    RestoreSysCatalogState::Objects::FindRootTable(
+    const TableId& table_id) {
+  auto it = tables.find(table_id);
+  if (it == tables.end()) {
+    return STATUS_FORMAT(NotFound, "Table $0 not found for index", table_id);
+  }
+  return FindRootTable(*it);
 }
 
 std::string RestoreSysCatalogState::Objects::SizesToString() const {
@@ -368,8 +399,18 @@ Status RestoreSysCatalogState::PrepareWriteBatch(
 Status RestoreSysCatalogState::PrepareTabletCleanup(
     const TabletId& id, SysTabletsEntryPB pb, const Schema& schema,
     docdb::DocWriteBatch* write_batch) {
+  VLOG_WITH_FUNC(4) << id;
+
   QLWriteRequestPB write_request;
-  pb.set_state(SysTabletsEntryPB::DELETED);
+
+  auto it = retained_existing_tables_.find(pb.table_id());
+  if (it != retained_existing_tables_.end()) {
+    pb.set_hide_hybrid_time(restoration_.write_time.ToUint64());
+    auto& out_schedules = *pb.mutable_retained_by_snapshot_schedules();
+    for (const auto& schedule_id : it->second) {
+      out_schedules.Add()->assign(schedule_id.AsSlice().cdata(), schedule_id.size());
+    }
+  }
   RETURN_NOT_OK(FillSysCatalogWriteRequest(
       SysRowEntry::TABLET, id, pb.SerializeAsString(),
       QLWriteRequestPB::QL_STMT_UPDATE, schema, &write_request));
@@ -379,8 +420,10 @@ Status RestoreSysCatalogState::PrepareTabletCleanup(
 Status RestoreSysCatalogState::PrepareTableCleanup(
     const TableId& id, SysTablesEntryPB pb, const Schema& schema,
     docdb::DocWriteBatch* write_batch) {
+  VLOG_WITH_FUNC(4) << id;
+
   QLWriteRequestPB write_request;
-  pb.set_state(SysTablesEntryPB::DELETING);
+  pb.set_hide_state(SysTablesEntryPB::HIDING);
   pb.set_version(pb.version() + 1);
   RETURN_NOT_OK(FillSysCatalogWriteRequest(
       SysRowEntry::TABLE, id, pb.SerializeAsString(),
@@ -390,7 +433,7 @@ Status RestoreSysCatalogState::PrepareTableCleanup(
 
 Result<bool> RestoreSysCatalogState::TEST_MatchTable(
     const TableId& id, const SysTablesEntryPB& table) {
-  return MatchTable(restoration_.filter, id, table);
+  return MatchTable(restoration_.schedules[0].second, id, table);
 }
 
 void RestoreSysCatalogState::WriteToRocksDB(

ent/src/yb/master/restore_sys_catalog_state.h
@@ -67,10 +67,13 @@ class RestoreSysCatalogState {
 
  private:
   struct Objects;
+  using RetainedExistingTables = std::unordered_map<TableId, std::vector<SnapshotScheduleId>>;
 
   // Determine entries that should be restored. I.e. apply filter and serialize.
   template <class ProcessEntry>
-  CHECKED_STATUS DetermineEntries(Objects* objects, const ProcessEntry& process_entry);
+  CHECKED_STATUS DetermineEntries(
+      Objects* objects, RetainedExistingTables* retained_existing_tables,
+      const ProcessEntry& process_entry);
 
   template <class PB>
   CHECKED_STATUS IterateSysCatalog(
@@ -113,6 +116,11 @@ class RestoreSysCatalogState {
     std::unordered_map<TabletId, SysTabletsEntryPB> tablets;
 
     std::string SizesToString() const;
+
+    Result<const std::pair<const TableId, SysTablesEntryPB>&> FindRootTable(
+        const TableId& table_id);
+    Result<const std::pair<const TableId, SysTablesEntryPB>&> FindRootTable(
+        const std::pair<const TableId, SysTablesEntryPB>& id_and_metadata);
   };
 
   SnapshotScheduleRestoration& restoration_;
@@ -120,6 +128,7 @@ class RestoreSysCatalogState {
 
   Objects restoring_objects_;
   Objects existing_objects_;
+  RetainedExistingTables retained_existing_tables_;
 };
 
 }  // namespace master

ent/src/yb/master/restore_sys_catalog_state_test.cc
@@ -57,7 +57,8 @@ TEST(RestoreSysCatalogStateTest, Filter) {
   table_entry.set_namespace_id(kNamespaceId);
   table_entry.set_namespace_name(kNamespaceName);
 
-  TableIdentifierPB& table_identifier = *restoration.filter.mutable_tables()->add_tables();
+  restoration.schedules.emplace_back(SnapshotScheduleId::Nil(), SnapshotScheduleFilterPB());
+  auto& table_identifier = *restoration.schedules[0].second.mutable_tables()->add_tables();
 
   for (bool use_table_id : {true, false}) {
     SCOPED_TRACE(Format("use_table_id: $0", use_table_id));

commit 15f9ebef88c944edb61860d41a7ea8a7d0442177

[#9862] Fix org.yb.client.TestTserverHealthChecks#testTserverHealthChecks
Summary:
org.yb.client.TestTserverHealthChecks#testTserverHealthChecks was broken in cd94edc31fd750422357d8d003d4aff0727ef3a4/D12955.

Fixing by correct check that table was created.

Test Plan: org.yb.client.TestTserverHealthChecks

Reviewers: bogdan, skedia

Reviewed By: skedia

Subscribers: ybase

Differential Revision: https://phabricator.dev.yugabyte.com/D13192

src/yb/master/catalog_entity_info.cc
@@ -353,6 +353,16 @@ void TableInfo::AddTablet(const TabletInfoPtr& tablet) {
   AddTabletUnlocked(tablet);
 }
 
+void TableInfo::ReplaceTablet(const TabletInfoPtr& old_tablet, const TabletInfoPtr& new_tablet) {
+  std::lock_guard<decltype(lock_)> l(lock_);
+  auto it = partitions_.find(old_tablet->metadata().dirty().pb.partition().partition_key_start());
+  if (it != partitions_.end() && it->second == old_tablet.get()) {
+    partitions_.erase(it);
+  }
+  AddTabletUnlocked(new_tablet);
+}
+
+
 void TableInfo::AddTablets(const TabletInfos& tablets) {
   std::lock_guard<decltype(lock_)> l(lock_);
   for (const auto& tablet : tablets) {
@@ -396,8 +406,9 @@ void TableInfo::AddTabletUnlocked(const TabletInfoPtr& tablet) {
   }
 
   auto it = p.first;
-  const auto old_split_depth = it->second->LockForRead()->pb.split_depth();
-  if (tablet_meta.split_depth() > old_split_depth) {
+  auto old_tablet_lock = it->second->LockForRead();
+  const auto old_split_depth = old_tablet_lock->pb.split_depth();
+  if (tablet_meta.split_depth() > old_split_depth || old_tablet_lock->is_deleted()) {
     VLOG(1) << "Replacing tablet " << it->second->tablet_id()
             << " (split_depth = " << old_split_depth << ")"
             << " with tablet " << tablet->tablet_id()
@@ -405,6 +416,10 @@ void TableInfo::AddTabletUnlocked(const TabletInfoPtr& tablet) {
     it->second = tablet.get();
     return;
   }
+
+  LOG_IF(DFATAL, tablet_meta.split_depth() == old_split_depth)
+      << "Two tablets with the same partition key start and split depth: "
+      << tablet_meta.ShortDebugString() << " and " << old_tablet_lock->pb.ShortDebugString();
 
   // TODO: can we assert that the replaced tablet is not in Running state?
   // May be a little tricky since we don't know whether to look at its committed or
@@ -480,7 +495,7 @@ bool TableInfo::AreAllTabletsDeleted() const {
 
 bool TableInfo::IsCreateInProgress() const {
   SharedLock<decltype(lock_)> l(lock_);
-  for (const auto& e : tablets_) {
+  for (const auto& e : partitions_) {
     auto tablet_info_lock = e.second->LockForRead();
     if (!tablet_info_lock->is_running() && tablet_info_lock->pb.split_depth() == 0) {
       return true;

src/yb/master/catalog_entity_info.h
@@ -431,6 +431,9 @@ class TableInfo : public RefCountedThreadSafe<TableInfo>,
   // Add a tablet to this table.
   void AddTablet(const TabletInfoPtr& tablet);
 
+  // Replace existing tablet with a new one.
+  void ReplaceTablet(const TabletInfoPtr& old_tablet, const TabletInfoPtr& new_tablet);
+
   // Add multiple tablets to this table.
   void AddTablets(const TabletInfos& tablets);
 

src/yb/master/catalog_manager.cc
@@ -8279,7 +8279,7 @@ void CatalogManager::HandleAssignCreatingTablet(TabletInfo* tablet,
                << "the allowed timeout. Replacing with a new tablet "
                << replacement->tablet_id();
 
-  tablet->table()->AddTablet(replacement);
+  tablet->table()->ReplaceTablet(tablet, replacement);
   {
     LockGuard lock(mutex_);
     auto tablet_map_checkout = tablet_map_.CheckOut();