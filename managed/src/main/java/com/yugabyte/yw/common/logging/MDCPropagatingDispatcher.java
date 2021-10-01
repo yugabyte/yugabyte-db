58754f812dbb572d2210de48562694022c13eb3d
Artem Mindrov <amindrov@yugabyte.com>
2021-10-01 12:44:26 +0000

    [Platform][CLOUDGA-2345] unit tests for the dispatcher, thread pool executor and filter
    Summary:
    unit tests for the new dispatcher, thread pool executor and http filter used for MDC
    propagation.
    
    Test Plan: sbt test
    
    Reviewers: sanketh, sb-yb
    
    Reviewed By: sb-yb
    
    Subscribers: jenkins-bot, yugaware
    
    Differential Revision: https://phabricator.dev.yugabyte.com/D13274

--- managed/src/main/java/com/yugabyte/yw/common/logging/MDCPropagatingDispatcher.java
+++ managed/src/main/java/com/yugabyte/yw/common/logging/MDCPropagatingDispatcher.java
@@ -10,7 +10,46 @@ import java.util.Map;
 import org.slf4j.MDC;
 import scala.concurrent.ExecutionContext;
 
+/**
+ * This custom dispatcher is used to capture MDC, propagate it through an {@link
+ * scala.concurrent.ExecutionContext} and restore when the executor finishes. Note that capturing
+ * the context in the dispatcher constructor and overriding {@link
+ * akka.dispatch.Dispatcher#execute(Runnable)} will result in context being set only once and
+ * subsequent runnables on the same thread will no longer be able to access it after it is cleared.
+ */
 public class MDCPropagatingDispatcher extends Dispatcher {
+
+  class MDCPropagatingExecutionContext implements ExecutionContext {
+    private final Map<String, String> context = MDC.getCopyOfContextMap();
+    private final MDCPropagatingDispatcher self = MDCPropagatingDispatcher.this;
+
+    @Override
+    public void reportFailure(Throwable cause) {
+      self.reportFailure(cause);
+    }
+
+    @Override
+    public void execute(Runnable runnable) {
+      Runnable mdcAwareRunnable = new MDCAwareRunnable(context, runnable);
+
+      if (self.batchable(runnable)) {
+        self.execute(
+            new Batchable() {
+              @Override
+              public boolean isBatchable() {
+                return true;
+              }
+
+              @Override
+              public void run() {
+                mdcAwareRunnable.run();
+              }
+            });
+      } else {
+        self.execute(mdcAwareRunnable);
+      }
+    }
+  }
 
   private final boolean cloudLoggingEnabled;
 
@@ -38,36 +77,6 @@ public class MDCPropagatingDispatcher extends Dispatcher {
       return super.prepare();
     }
 
-    return new ExecutionContext() {
-      private final Map<String, String> context = MDC.getCopyOfContextMap();
-      private final MDCPropagatingDispatcher self = MDCPropagatingDispatcher.this;
-
-      @Override
-      public void reportFailure(Throwable cause) {
-        self.reportFailure(cause);
-      }
-
-      @Override
-      public void execute(Runnable runnable) {
-        Runnable mdcAwareRunnable = new MDCAwareRunnable(context, runnable);
-
-        if (self.batchable(runnable)) {
-          self.execute(
-              new Batchable() {
-                @Override
-                public boolean isBatchable() {
-                  return true;
-                }
-
-                @Override
-                public void run() {
-                  mdcAwareRunnable.run();
-                }
-              });
-        } else {
-          self.execute(mdcAwareRunnable);
-        }
-      }
-    };
+    return new MDCPropagatingExecutionContext();
   }
 }
