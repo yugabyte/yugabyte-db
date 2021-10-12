55275feb4343bf369cff50f9b2b289fc0106d9eb
Jethro Mak <jmak@yugabyte.com>
2021-10-12 15:33:27 -0400

    [Plat-1777] Add basic filtering and sorting
    Summary:
    New universe list page is under '/universes_new'.
    New universe view:
    '/universes_new' : UniverseConsole (page) -> UniverseConsoleContainer -> UniverseConsole -> UniverseViewContainer ->UniverseView
    
    Old universe view:
     '/universes' : UniverseList (page) -> UniverseListContainer -> UniverseList -> UniverseTableContainer -> UniverseTable
    
    Enable filtering for universe list by adapting the query search bar from live queries.
    Filterable fields:
      - Universe Name
      - Provider Type
      - Provider Name
      - Cost per Month
      - Created Date
      - Status
    
    Enable sorting for universe list.
    Sortable fields:
    - Universe Name
    - Cost per Month
    - Created Date
    - Status
    
    Moved "Import Universe" and "Create Universe" buttons to a toolbar container under UniverseView.
    
    Example usage:
    {F19673}
    
    Refactoring:
    - Convert YBUniverseItem, CellLocationPanel, CellResourcePanel into functional components and place into separate files
    
    Test Plan:
    Testing Filtering:
    i) Create 3 universes with different names and same Provider (ex. AWS)
    ii) Create 1 universe with different name and different provider (ex. GCP)
    iii) Create 1 universe with bad configuration/provider leading to error state.
    iv) Pause 2 of the 3 universes in i)
    v) Filter by state (Ready, Paused, Pending). Wait for the universe in iii) to error and check error state filtering as well.
    vi) Filter by universe name, cost, date created, and provider name.
    vii) Filter using any combination of the above fields.
    viii) Expected behaviour is correct universe list is returned given any combination of filter values.
    
    Testing Sorting:
    i) Repeat steps i-iv from above.
    ii) Sort by universe name, cost per month, created date, and status
    iii) Filter on any combination of fields and test sorting again on the fields from ii)
    iv) Expected behaviour is correct universe list returned and sorted
    
    Reviewers: sshevchenko, andrew
    
    Reviewed By: andrew
    
    Subscribers: jenkins-bot
    
    Differential Revision: https://phabricator.dev.yugabyte.com/D13171

---
+++ managed/ui/src/components/universes/UniverseView/UniverseViewContainer.js
@@ -0,0 +1,44 @@
+// Copyright (c) YugaByte, Inc.
+
+import { connect } from 'react-redux';
+
+import { UniverseView } from './UniverseView';
+import { fetchUniverseMetadata, resetUniverseTasks } from '../../../actions/universe';
+import {
+  fetchCustomerTasks,
+  fetchCustomerTasksSuccess,
+  fetchCustomerTasksFailure
+} from '../../../actions/tasks';
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    fetchUniverseMetadata: () => {
+      dispatch(fetchUniverseMetadata());
+    },
+
+    fetchUniverseTasks: () => {
+      dispatch(fetchCustomerTasks()).then((response) => {
+        if (!response.error) {
+          dispatch(fetchCustomerTasksSuccess(response.payload));
+        } else {
+          dispatch(fetchCustomerTasksFailure(response.payload));
+        }
+      });
+    },
+    resetUniverseTasks: () => {
+      dispatch(resetUniverseTasks());
+    }
+  };
+};
+
+function mapStateToProps(state) {
+  return {
+    universe: state.universe,
+    customer: state.customer,
+    graph: state.graph,
+    tasks: state.tasks,
+    providers: state.cloud.providers
+  };
+}
+
+export const UniverseViewContainer = connect(mapStateToProps, mapDispatchToProps)(UniverseView);
