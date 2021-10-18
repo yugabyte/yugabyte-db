c55d757cca2dbd76514323849561b22330915824
Fizaa Luthra <fizaaluthra@Fizaas-MacBook-Pro.local>
2021-10-18 12:59:44 -0400

    [#10317] YSQL: Import `Allow users with BYPASSRLS to alter their own passwords.`
    Summary:
    Upstream commit was `d907bd0543aa63e59653d7345840bed0f8b3a83b`
    
    This commit allows users with BYPASSRLS attribute to change their own passwords.
    In addition to backporting the upstream commit, this diff adds a test for
    
    Commit message was:
    ```
    Allow users with BYPASSRLS to alter their own passwords.
    
    The intention in commit 491c029db was to require superuserness to
    change the BYPASSRLS property, but the actual effect of the coding
    in AlterRole() was to require superuserness to change anything at all
    about a BYPASSRLS role.  Other properties of a BYPASSRLS role should
    be changeable under the same rules as for a normal role, though.
    
    Fix that, and also take care of some documentation omissions related
    to BYPASSRLS and REPLICATION role properties.
    
    Tom Lane and Stephen Frost, per bug report from Wolfgang Walther.
    Back-patch to all supported branches.
    
    Discussion: https://postgr.es/m/a5548a9f-89ee-3167-129d-162b5985fcf8@technowledgy.de
    ```
    
    Test Plan:
    The following shouldn't return an error:
    
    ```
    CREATE ROLE user1 BYPASSRLS;
    ALTER ROLE user1 PASSWORD 'password';
    ```
    
    Run the `yb_roles` test using the `TestPgRegressAuthorization` test suite.
    
    Build Yugabyte DB and run test suite via Jenkins
    
    Reviewers: smishra
    
    Reviewed By: smishra
    
    Subscribers: sanketh, yql
    
    Differential Revision: https://phabricator.dev.yugabyte.com/D13449

--- src/postgres/src/test/regress/sql/yb_roles.sql
+++ src/postgres/src/test/regress/sql/yb_roles.sql
@@ -1,4 +1,4 @@
--- test YB roles
+-- test yb_extension role
 CREATE USER regress_priv_user;
 SET SESSION AUTHORIZATION regress_priv_user;
 CREATE EXTENSION pgcrypto; -- should fail
@@ -10,3 +10,8 @@ ALTER EXTENSION pgcrypto UPDATE TO '1.3';
 DROP EXTENSION pgcrypto;
 \c -
 DROP USER regress_priv_user;
+
+-- test password change with BYPASSRLS attribute
+CREATE ROLE user1 BYPASSRLS;
+ALTER ROLE user1 PASSWORD 'password';
+DROP ROLE user1;
