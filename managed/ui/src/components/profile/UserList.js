028180e9256097ca3014c87025b25bc0386f208e
Sean Kim <skim@yugabyte.com>
2021-10-26 16:22:05 -0400

    [PLAT-2081] Platform: Show Error message when trying to create existing user
    Summary:
    When creating customer users, if I put in an email that already exists in customer's users,
    the user is not getting added, but no error message pops up.
    
    Fixed by adding an error pop-up when creating a user that already exists.
    
    Test Plan:
    1. Click user icon on top-right corner
    2. Go to Profile and go to Users tab
    3. Click Add User, and try adding a user with an email that already exists
    4. An error message saying "User already exists" should pop up
    
    Reviewers: andrew, sshevchenko
    
    Reviewed By: sshevchenko
    
    Subscribers: jenkins-bot, yugaware
    
    Differential Revision: https://phabricator.dev.yugabyte.com/D13626

--- managed/ui/src/components/profile/UserList.js
+++ managed/ui/src/components/profile/UserList.js
@@ -16,10 +16,13 @@ import { isNotHidden, isDisabled } from '../../utils/LayoutUtils';
 import {
   changeUserRole,
   createUser,
-  createUserResponse,
+  createUserFailure,
+  createUserSuccess,
   deleteUser,
   deleteUserResponse
 } from '../../actions/customers';
+import {toast} from "react-toastify";
+import {createErrorMessage} from "../alerts/AlertConfiguration/AlertUtils";
 
 class UserList extends Component {
   constructor(props) {
@@ -170,7 +173,17 @@ const mapDispatchToProps = (dispatch) => {
   return {
     createUser: (payload) => {
       return dispatch(createUser(payload)).then((response) => {
-        return dispatch(createUserResponse(response.payload));
+        try {
+          if (response.payload.isAxiosError || response.payload.status !== 200) {
+            toast.error(createErrorMessage(response.payload));
+            return dispatch(createUserFailure(response.payload));
+          } else {
+            toast.success('User created successfully');
+            return dispatch(createUserSuccess(response.payload));
+          }
+        } catch (error) {
+          console.error('Error while creating customer users');
+        }
       });
     },
     changeUserRole: (userUUID, newRole) => {
