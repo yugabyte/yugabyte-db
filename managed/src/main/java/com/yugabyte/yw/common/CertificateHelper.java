bd1f4f644f2c043d3613e2f45d0561821abb0146
Harish Kandala <kandalaharish95@gmail.com>
2021-10-26 23:06:28 +0530

    [Backport 2.8][PLAT-2032] Platform: Append number to self-signed certificate labels when rotating certs
    Summary:
    Self-signed certificate labels used to remain same when rotating certs with "Create new certificate" option
    This diff fixes it by appending number to the newly created self-signed certificates
    
    Test Plan:
    Jenkins: rebase: 2.8
    
    Tested locally by rotating certs multiple times
    
    Reviewers: sanketh, arnav
    
    Reviewed By: arnav
    
    Subscribers: jenkins-bot, yugaware
    
    Differential Revision: https://phabricator.dev.yugabyte.com/D13620

--- managed/src/main/java/com/yugabyte/yw/common/CertificateHelper.java
+++ managed/src/main/java/com/yugabyte/yw/common/CertificateHelper.java
@@ -40,6 +40,7 @@ import java.util.ArrayList;
 import java.util.Base64;
 import java.util.Calendar;
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.Date;
 import java.util.List;
 import java.util.UUID;
@@ -124,23 +125,32 @@ public class CertificateHelper {
       X509Certificate x509 = converter.getCertificate(holder);
       String certPath =
           String.format(
-              CERT_PATH + "/ca.%s",
-              storagePath,
-              customerUUID.toString(),
-              rootCA_UUID.toString(),
-              ROOT_CERT);
+              CERT_PATH + "/ca.%s", storagePath, customerUUID.toString(), rootCA_UUID, ROOT_CERT);
       writeCertFileContentToCertPath(Collections.singletonList(x509), certPath);
       String keyPath =
-          String.format(
-              CERT_PATH + "/ca.key.pem",
-              storagePath,
-              customerUUID.toString(),
-              rootCA_UUID.toString());
+          String.format(CERT_PATH + "/ca.key.pem", storagePath, customerUUID, rootCA_UUID);
       writeKeyFileContentToKeyPath(keyPair.getPrivate(), keyPath);
       CertificateInfo.Type certType = CertificateInfo.Type.SelfSigned;
+
+      // Default the cert label with node prefix.
+      // If cert with the label already exists append number
+      String certLabel = nodePrefix;
+      List<CertificateInfo> certificateInfoList =
+          CertificateInfo.getWhereLabelStartsWith(nodePrefix, certType);
+      if (!certificateInfoList.isEmpty()) {
+        certificateInfoList.sort(Comparator.comparing(a -> a.label, Comparator.reverseOrder()));
+        String[] labelArray = certificateInfoList.get(0).label.split("~");
+        int lastCount = 0;
+        try {
+          lastCount = Integer.parseInt(labelArray[labelArray.length - 1]);
+        } catch (NumberFormatException ignored) {
+        }
+        certLabel = nodePrefix + "~" + (++lastCount);
+      }
+
       LOG.info(
           "Generated self signed cert label {} uuid {} of type {} for customer {} at paths {}, {}",
-          nodePrefix,
+          certLabel,
           rootCA_UUID,
           certType,
           customerUUID,
@@ -151,14 +161,14 @@ public class CertificateHelper {
           CertificateInfo.create(
               rootCA_UUID,
               customerUUID,
-              nodePrefix,
+              certLabel,
               certStart,
               certExpiry,
               keyPath,
               certPath,
               certType);
 
-      LOG.info("Created Root CA for universe {}.", nodePrefix);
+      LOG.info("Created Root CA for universe {}.", certLabel);
       return cert.uuid;
     } catch (NoSuchAlgorithmException
         | IOException
