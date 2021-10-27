e52a1eafcf24e5e2452a6b758f4d51918245b153
Mikhail Bautin <mbautin@users.noreply.github.com>
2021-10-26 21:37:22 -0700

    Enforce the use of arc wrapper
    Summary:
    We have an internal Yugabyte wrapper around Phabricator's Arcanist (arc) tool that performs additional checks against the internal CI/CD systems before allowing "arc land" to proceed.
    This revision will enforce internal usage of the arc wrapper by refusing to build the code if the wrapper is not installed.
    
    Test Plan:
    Run yb_build.sh with and without the arc wrapper in ~/tools/bin
    
    Jenkins: build platform: alma8, build type: release, compile only
    
    Reviewers: steve.varnau, bogdan, sergei
    
    Reviewed By: sergei
    
    Subscribers: devops, ybase
    
    Differential Revision: https://phabricator.dev.yugabyte.com/D13638

--- build-support/common-build-env.sh
+++ build-support/common-build-env.sh
@@ -1480,7 +1480,7 @@ decide_whether_to_use_ninja() {
       if [[ $BUILD_ROOT == *-ninja ]]; then
         export YB_USE_NINJA=1
       fi
-    elif command -v ninja || [[ -x /usr/local/bin/ninja ]]; then
+    elif command -v ninja >/dev/null || [[ -x /usr/local/bin/ninja ]]; then
       export YB_USE_NINJA=1
     elif using_linuxbrew; then
       local yb_ninja_path_candidate=$YB_LINUXBREW_DIR/bin/ninja
@@ -2433,6 +2433,35 @@ set_prebuilt_thirdparty_url() {
       log "YB_THIRDPARTY_URL is already set to '$YB_THIRDPARTY_URL', not trying to set it" \
           "automatically."
     fi
+  fi
+}
+
+check_arc_wrapper() {
+  if is_jenkins || [[ ${YB_SKIP_ARC_WRAPPER_CHECK:-0} == "1" ]]; then
+    return
+  fi
+  if [[ -f $HOME/.yb_build_env_bashrc ]]; then
+    # This is a Yugabyte workstation or dev server.
+    local arc_path
+    set +e
+    arc_path=$( which arc )
+    set -e
+    if [[ ! -f $arc_path ]]; then
+      # OK if arc is not found. Then people cannot "arc land" changes that do not pass tests.
+      return
+    fi
+    local expected_arc_path=$HOME/tools/bin/arc
+    if [[ $arc_path == "$expected_arc_path" ]]; then
+      # OK, this is where we install the arc wrapper.
+      return
+    fi
+
+    if grep -Eq "Wrapper for arcanist arc" "$arc_path"; then
+      # This seems to be a valid arc wrapper installed elsewhere.
+      return
+    fi
+
+    fatal "Not a valid arc wrapper: $arc_path (required for internal Yugabyte hosts)"
   fi
 }
 
