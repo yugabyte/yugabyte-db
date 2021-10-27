92626694ca41382fc4bb25f569273c9591a3c2ac
Mikhail Bautin <mbautin@users.noreply.github.com>
2021-10-26 20:40:09 -0700

    Revert "[#5395][docdb] Add metrics for master async tasks"
    Summary: This reverts commit 19ede1af119c1914ee093f0cb46e248f10ed9ff4 beacuse it introduces over 500 test failures in TSAN.
    
    Test Plan: Jenkins: urgent
    
    Reviewers: bogdan
    
    Reviewed By: bogdan
    
    Subscribers: ybase
    
    Differential Revision: https://phabricator.dev.yugabyte.com/D13635

--- src/yb/server/server_base.h
+++ src/yb/server/server_base.h
@@ -87,8 +87,6 @@ class RpcServerBase {
 
   MetricRegistry* metric_registry() { return metric_registry_.get(); }
 
-  std::map<std::string, scoped_refptr<Histogram>>* master_metrics() { return &master_metrics_;}
-
   // Returns this server's clock.
   Clock* clock() { return clock_.get(); }
 
@@ -152,7 +150,6 @@ class RpcServerBase {
   CountDownLatch stop_metrics_logging_latch_;
 
   std::unique_ptr<ScopedGLogMetrics> glog_metrics_;
-  std::map<string, scoped_refptr<Histogram>> master_metrics_;
 
   DISALLOW_COPY_AND_ASSIGN(RpcServerBase);
 };
