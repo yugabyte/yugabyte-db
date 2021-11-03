3370e89bb5fc4ba6ccf08054b292b615f22acf44
Jason Kim <git@jasonk.me>
2021-11-02 21:36:19 -0700

    [#9960] ysql: have ybgin borrow cost estimates from gin
    Summary:
    ybgincostestimate is hardly implemented because it sets all costs to
    zero, making it always preferred.  Improve it by simply calling
    gincostestimate, which works out of the box with a small change to
    ignore a getGinStats call.  With this, ybgin index scans using search
    mode "all" or "everything" are avoided.  This appears particularly
    beneficial for a wide variety of jsonpath scans.
    
    For some reason, enable_seqscan=off does not seem to work in forcing an
    index scan on these queries that would have resulted in search mode all
    or everything: sequential scan is chosen.  In tests, resort to using
    pg_hint_plan, which seems to work in forcing index scan.
    
    Depends on D13767
    
    Test Plan: ./yb_build.sh --java-test TestPgRegressGin
    
    Reviewers: amartsinchyk, mihnea
    
    Reviewed By: mihnea
    
    Subscribers: yql
    
    Differential Revision: https://phabricator.dev.yugabyte.com/D13768

--- src/postgres/src/test/regress/sql/yb_ybgin_operators.sql
+++ src/postgres/src/test/regress/sql/yb_ybgin_operators.sql
@@ -14,60 +14,88 @@ INSERT INTO jsonbs (j) VALUES ('{"aaa":[-1,2.5,"5"], "date":"2021-06-30"}');
 DROP INDEX jsonbs_j_idx;
 CREATE INDEX ON jsonbs USING ybgin (j);
 -- jsonpath: number + number
+/*+IndexScan(jsonbs)*/
 EXPLAIN (costs off)
 SELECT * FROM jsonbs WHERE j @? '$.aaa[*] ? (@ + 2 == 4)';
+/*+IndexScan(jsonbs)*/
 SELECT * FROM jsonbs WHERE j @? '$.aaa[*] ? (@ + 2 == 4)';
 -- jsonpath: + number
+/*+IndexScan(jsonbs)*/
 EXPLAIN (costs off)
 SELECT * FROM jsonbs WHERE j @? '$.aaa[*] ? (+@ == 5)';
+/*+IndexScan(jsonbs)*/
 SELECT * FROM jsonbs WHERE j @? '$.aaa[*] ? (+@ == 5)';
 -- jsonpath: number - number
+/*+IndexScan(jsonbs)*/
 EXPLAIN (costs off)
 SELECT * FROM jsonbs WHERE j @? '$.aaa[*] ? (@ - 2 == 3)';
+/*+IndexScan(jsonbs)*/
 SELECT * FROM jsonbs WHERE j @? '$.aaa[*] ? (@ - 2 == 3)';
 -- jsonpath: - number
+/*+IndexScan(jsonbs)*/
 EXPLAIN (costs off)
 SELECT * FROM jsonbs WHERE j @? '$.aaa.bbb[*] ? (-@ < -3)';
+/*+IndexScan(jsonbs)*/
 SELECT * FROM jsonbs WHERE j @? '$.aaa.bbb[*] ? (-@ < -3)';
 -- jsonpath: number * number
+/*+IndexScan(jsonbs)*/
 EXPLAIN (costs off)
 SELECT * FROM jsonbs WHERE j @? '$.aaa[*] ? (@ * 2 == 4)';
+/*+IndexScan(jsonbs)*/
 SELECT * FROM jsonbs WHERE j @? '$.aaa[*] ? (@ * 2 == 4)';
 -- jsonpath: number / number
+/*+IndexScan(jsonbs)*/
 EXPLAIN (costs off)
 SELECT * FROM jsonbs WHERE j @? '$.aaa[*] ? (@ / 2 == 1)';
+/*+IndexScan(jsonbs)*/
 SELECT * FROM jsonbs WHERE j @? '$.aaa[*] ? (@ / 2 == 1)';
 -- jsonpath: number % number
+/*+IndexScan(jsonbs)*/
 EXPLAIN (costs off)
 SELECT * FROM jsonbs WHERE j @? '$.aaa[*] ? (@ % 2 == 1)';
+/*+IndexScan(jsonbs)*/
 SELECT * FROM jsonbs WHERE j @? '$.aaa[*] ? (@ % 2 == 1)';
 -- jsonpath: value . type()
+/*+IndexScan(jsonbs)*/
 EXPLAIN (costs off)
 SELECT * FROM jsonbs WHERE j @? '$[*] ? (@.type() == "string")';
+/*+IndexScan(jsonbs)*/
 SELECT * FROM jsonbs WHERE j @? '$[*] ? (@.type() == "string")';
 -- jsonpath: value . size()
+/*+IndexScan(jsonbs)*/
 EXPLAIN (costs off)
 SELECT * FROM jsonbs WHERE j @@ '$.aaa.size() == 3';
+/*+IndexScan(jsonbs)*/
 SELECT * FROM jsonbs WHERE j @@ '$.aaa.size() == 3';
 -- jsonpath: value . double()
+/*+IndexScan(jsonbs)*/
 EXPLAIN (costs off)
 SELECT * FROM jsonbs WHERE j @@ '$.double() * 3 == 9';
+/*+IndexScan(jsonbs)*/
 SELECT * FROM jsonbs WHERE j @@ '$.double() * 3 == 9';
 -- jsonpath: value . ceiling()
+/*+IndexScan(jsonbs)*/
 EXPLAIN (costs off)
 SELECT * FROM jsonbs WHERE j @? '$.aaa[*] ? (@.ceiling() == 2)';
+/*+IndexScan(jsonbs)*/
 SELECT * FROM jsonbs WHERE j @? '$.aaa[*] ? (@.ceiling() == 2)';
 -- jsonpath: value . floor()
+/*+IndexScan(jsonbs)*/
 EXPLAIN (costs off)
 SELECT * FROM jsonbs WHERE j @? '$.aaa[*] ? (@.floor() == 2)';
+/*+IndexScan(jsonbs)*/
 SELECT * FROM jsonbs WHERE j @? '$.aaa[*] ? (@.floor() == 2)';
 -- jsonpath: value . abs()
+/*+IndexScan(jsonbs)*/
 EXPLAIN (costs off)
 SELECT * FROM jsonbs WHERE j @? '$.aaa[*] ? (@.abs() == 1)';
+/*+IndexScan(jsonbs)*/
 SELECT * FROM jsonbs WHERE j @? '$.aaa[*] ? (@.abs() == 1)';
 -- jsonpath: value . datetime()
+/*+IndexScan(jsonbs)*/
 EXPLAIN (costs off)
 SELECT * FROM jsonbs WHERE j @@ '$.date.datetime() < "2021-07-01".datetime()';
+/*+IndexScan(jsonbs)*/
 SELECT * FROM jsonbs WHERE j @@ '$.date.datetime() < "2021-07-01".datetime()';
 -- jsonpath: value . datetime(template)
 -- (skip this)
@@ -76,8 +104,10 @@ SELECT * FROM jsonbs WHERE j @@ '$.date.datetime() < "2021-07-01".datetime()';
 -- jsonpath: value == value
 -- (skip this)
 -- jsonpath: value != value
+/*+IndexScan(jsonbs)*/
 EXPLAIN (costs off)
 SELECT * FROM jsonbs WHERE j @? '$.aaa[*] ? (@ != 1)';
+/*+IndexScan(jsonbs)*/
 SELECT * FROM jsonbs WHERE j @? '$.aaa[*] ? (@ != 1)';
 -- jsonpath: value < value
 -- (skip this)
@@ -88,32 +118,44 @@ SELECT * FROM jsonbs WHERE j @? '$.aaa[*] ? (@ != 1)';
 -- jsonpath: value >= value
 -- (skip this)
 -- jsonpath: boolean && boolean
+/*+IndexScan(jsonbs)*/
 EXPLAIN (costs off)
 SELECT * FROM jsonbs WHERE j @? '$.aaa[*] ? (@ != -1 && @ != 2.5)';
+/*+IndexScan(jsonbs)*/
 SELECT * FROM jsonbs WHERE j @? '$.aaa[*] ? (@ != -1 && @ != 2.5)';
 -- jsonpath: boolean || boolean
 EXPLAIN (costs off)
 SELECT * FROM jsonbs WHERE j @? '$.aaa[*] ? (@ == -1 || @ == 1)';
 SELECT * FROM jsonbs WHERE j @? '$.aaa[*] ? (@ == -1 || @ == 1)';
 -- jsonpath: ! boolean
+/*+IndexScan(jsonbs)*/
 EXPLAIN (costs off)
 SELECT * FROM jsonbs WHERE j @? '$.aaa[*] ? (!(@ > 0))';
+/*+IndexScan(jsonbs)*/
 SELECT * FROM jsonbs WHERE j @? '$.aaa[*] ? (!(@ > 0))';
 -- jsonpath: boolean is unknown
+/*+IndexScan(jsonbs)*/
 EXPLAIN (costs off)
 SELECT * FROM jsonbs WHERE j @? '$.aaa[*] ? ((@ == "1") is unknown)';
+/*+IndexScan(jsonbs)*/
 SELECT * FROM jsonbs WHERE j @? '$.aaa[*] ? ((@ == "1") is unknown)';
 -- jsonpath: string like_regex string
+/*+IndexScan(jsonbs)*/
 EXPLAIN (costs off)
 SELECT * FROM jsonbs WHERE j @? '$.aaa[*] ? (@ like_regex "^[4-7]+$")';
+/*+IndexScan(jsonbs)*/
 SELECT * FROM jsonbs WHERE j @? '$.aaa[*] ? (@ like_regex "^[4-7]+$")';
 -- jsonpath: string starts with string
+/*+IndexScan(jsonbs)*/
 EXPLAIN (costs off)
 SELECT * FROM jsonbs WHERE j @? '$.aaa[*] ? (@ starts with "5")';
+/*+IndexScan(jsonbs)*/
 SELECT * FROM jsonbs WHERE j @? '$.aaa[*] ? (@ starts with "5")';
 -- jsonpath: exists ( path_expression )
+/*+IndexScan(jsonbs)*/
 EXPLAIN (costs off)
 SELECT * FROM jsonbs WHERE j @? '$.* ? (exists (@ ? (@[*] < 0 || @[*] > 5)))';
+/*+IndexScan(jsonbs)*/
 SELECT * FROM jsonbs WHERE j @? '$.* ? (exists (@ ? (@[*] < 0 || @[*] > 5)))';
 
 --
@@ -124,60 +166,88 @@ SELECT * FROM jsonbs WHERE j @? '$.* ? (exists (@ ? (@[*] < 0 || @[*] > 5)))';
 DROP INDEX jsonbs_j_idx;
 CREATE INDEX ON jsonbs USING ybgin (j jsonb_path_ops);
 -- jsonpath: number + number
+/*+IndexScan(jsonbs)*/
 EXPLAIN (costs off)
 SELECT * FROM jsonbs WHERE j @? '$.aaa[*] ? (@ + 2 == 4)';
+/*+IndexScan(jsonbs)*/
 SELECT * FROM jsonbs WHERE j @? '$.aaa[*] ? (@ + 2 == 4)';
 -- jsonpath: + number
+/*+IndexScan(jsonbs)*/
 EXPLAIN (costs off)
 SELECT * FROM jsonbs WHERE j @? '$.aaa[*] ? (+@ == 5)';
+/*+IndexScan(jsonbs)*/
 SELECT * FROM jsonbs WHERE j @? '$.aaa[*] ? (+@ == 5)';
 -- jsonpath: number - number
+/*+IndexScan(jsonbs)*/
 EXPLAIN (costs off)
 SELECT * FROM jsonbs WHERE j @? '$.aaa[*] ? (@ - 2 == 3)';
+/*+IndexScan(jsonbs)*/
 SELECT * FROM jsonbs WHERE j @? '$.aaa[*] ? (@ - 2 == 3)';
 -- jsonpath: - number
+/*+IndexScan(jsonbs)*/
 EXPLAIN (costs off)
 SELECT * FROM jsonbs WHERE j @? '$.aaa.bbb[*] ? (-@ < -3)';
+/*+IndexScan(jsonbs)*/
 SELECT * FROM jsonbs WHERE j @? '$.aaa.bbb[*] ? (-@ < -3)';
 -- jsonpath: number * number
+/*+IndexScan(jsonbs)*/
 EXPLAIN (costs off)
 SELECT * FROM jsonbs WHERE j @? '$.aaa[*] ? (@ * 2 == 4)';
+/*+IndexScan(jsonbs)*/
 SELECT * FROM jsonbs WHERE j @? '$.aaa[*] ? (@ * 2 == 4)';
 -- jsonpath: number / number
+/*+IndexScan(jsonbs)*/
 EXPLAIN (costs off)
 SELECT * FROM jsonbs WHERE j @? '$.aaa[*] ? (@ / 2 == 1)';
+/*+IndexScan(jsonbs)*/
 SELECT * FROM jsonbs WHERE j @? '$.aaa[*] ? (@ / 2 == 1)';
 -- jsonpath: number % number
+/*+IndexScan(jsonbs)*/
 EXPLAIN (costs off)
 SELECT * FROM jsonbs WHERE j @? '$.aaa[*] ? (@ % 2 == 1)';
+/*+IndexScan(jsonbs)*/
 SELECT * FROM jsonbs WHERE j @? '$.aaa[*] ? (@ % 2 == 1)';
 -- jsonpath: value . type()
+/*+IndexScan(jsonbs)*/
 EXPLAIN (costs off)
 SELECT * FROM jsonbs WHERE j @? '$[*] ? (@.type() == "string")';
+/*+IndexScan(jsonbs)*/
 SELECT * FROM jsonbs WHERE j @? '$[*] ? (@.type() == "string")';
 -- jsonpath: value . size()
+/*+IndexScan(jsonbs)*/
 EXPLAIN (costs off)
 SELECT * FROM jsonbs WHERE j @@ '$.aaa.size() == 3';
+/*+IndexScan(jsonbs)*/
 SELECT * FROM jsonbs WHERE j @@ '$.aaa.size() == 3';
 -- jsonpath: value . double()
+/*+IndexScan(jsonbs)*/
 EXPLAIN (costs off)
 SELECT * FROM jsonbs WHERE j @@ '$.double() * 3 == 9';
+/*+IndexScan(jsonbs)*/
 SELECT * FROM jsonbs WHERE j @@ '$.double() * 3 == 9';
 -- jsonpath: value . ceiling()
+/*+IndexScan(jsonbs)*/
 EXPLAIN (costs off)
 SELECT * FROM jsonbs WHERE j @? '$.aaa[*] ? (@.ceiling() == 2)';
+/*+IndexScan(jsonbs)*/
 SELECT * FROM jsonbs WHERE j @? '$.aaa[*] ? (@.ceiling() == 2)';
 -- jsonpath: value . floor()
+/*+IndexScan(jsonbs)*/
 EXPLAIN (costs off)
 SELECT * FROM jsonbs WHERE j @? '$.aaa[*] ? (@.floor() == 2)';
+/*+IndexScan(jsonbs)*/
 SELECT * FROM jsonbs WHERE j @? '$.aaa[*] ? (@.floor() == 2)';
 -- jsonpath: value . abs()
+/*+IndexScan(jsonbs)*/
 EXPLAIN (costs off)
 SELECT * FROM jsonbs WHERE j @? '$.aaa[*] ? (@.abs() == 1)';
+/*+IndexScan(jsonbs)*/
 SELECT * FROM jsonbs WHERE j @? '$.aaa[*] ? (@.abs() == 1)';
 -- jsonpath: value . datetime()
+/*+IndexScan(jsonbs)*/
 EXPLAIN (costs off)
 SELECT * FROM jsonbs WHERE j @@ '$.date.datetime() < "2021-07-01".datetime()';
+/*+IndexScan(jsonbs)*/
 SELECT * FROM jsonbs WHERE j @@ '$.date.datetime() < "2021-07-01".datetime()';
 -- jsonpath: value . datetime(template)
 -- (skip this)
@@ -186,8 +256,10 @@ SELECT * FROM jsonbs WHERE j @@ '$.date.datetime() < "2021-07-01".datetime()';
 -- jsonpath: value == value
 -- (skip this)
 -- jsonpath: value != value
+/*+IndexScan(jsonbs)*/
 EXPLAIN (costs off)
 SELECT * FROM jsonbs WHERE j @? '$.aaa[*] ? (@ != 1)';
+/*+IndexScan(jsonbs)*/
 SELECT * FROM jsonbs WHERE j @? '$.aaa[*] ? (@ != 1)';
 -- jsonpath: value < value
 -- (skip this)
@@ -198,30 +270,40 @@ SELECT * FROM jsonbs WHERE j @? '$.aaa[*] ? (@ != 1)';
 -- jsonpath: value >= value
 -- (skip this)
 -- jsonpath: boolean && boolean
+/*+IndexScan(jsonbs)*/
 EXPLAIN (costs off)
 SELECT * FROM jsonbs WHERE j @? '$.aaa[*] ? (@ != -1 && @ != 2.5)';
+/*+IndexScan(jsonbs)*/
 SELECT * FROM jsonbs WHERE j @? '$.aaa[*] ? (@ != -1 && @ != 2.5)';
 -- jsonpath: boolean || boolean
 EXPLAIN (costs off)
 SELECT * FROM jsonbs WHERE j @? '$.aaa[*] ? (@ == -1 || @ == 1)';
 SELECT * FROM jsonbs WHERE j @? '$.aaa[*] ? (@ == -1 || @ == 1)';
 -- jsonpath: ! boolean
+/*+IndexScan(jsonbs)*/
 EXPLAIN (costs off)
 SELECT * FROM jsonbs WHERE j @? '$.aaa[*] ? (!(@ > 0))';
+/*+IndexScan(jsonbs)*/
 SELECT * FROM jsonbs WHERE j @? '$.aaa[*] ? (!(@ > 0))';
 -- jsonpath: boolean is unknown
 EXPLAIN (costs off)
 SELECT * FROM jsonbs WHERE j @? '$.aaa[*] ? (@ is unknown)';
 SELECT * FROM jsonbs WHERE j @? '$.aaa[*] ? (@ is unknown)';
 -- jsonpath: string like_regex string
+/*+IndexScan(jsonbs)*/
 EXPLAIN (costs off)
 SELECT * FROM jsonbs WHERE j @? '$.aaa[*] ? (@ like_regex "^[4-7]+$")';
+/*+IndexScan(jsonbs)*/
 SELECT * FROM jsonbs WHERE j @? '$.aaa[*] ? (@ like_regex "^[4-7]+$")';
 -- jsonpath: string starts with string
+/*+IndexScan(jsonbs)*/
 EXPLAIN (costs off)
 SELECT * FROM jsonbs WHERE j @? '$.aaa[*] ? (@ starts with "5")';
+/*+IndexScan(jsonbs)*/
 SELECT * FROM jsonbs WHERE j @? '$.aaa[*] ? (@ starts with "5")';
 -- jsonpath: exists ( path_expression )
+/*+IndexScan(jsonbs)*/
 EXPLAIN (costs off)
 SELECT * FROM jsonbs WHERE j @? '$.* ? (exists (@ ? (@[*] < 0 || @[*] > 5)))';
+/*+IndexScan(jsonbs)*/
 SELECT * FROM jsonbs WHERE j @? '$.* ? (exists (@ ? (@[*] < 0 || @[*] > 5)))';
