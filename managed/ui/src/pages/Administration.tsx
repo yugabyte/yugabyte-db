yugabyte/yugabyte-db

commit 0ada7e44702bcd724d70594e75707aa9a3a374b7

[PLAT-2071] Implement read-only user functionality for Alert UIs
Summary:
Alerts functionality is not working for read-only users.
This happens because we're using POST methods for some read operations for alerts.
Also, UI code does hot handle user permissions correctly.

Test Plan:
Create, edit and delete alert configuration with admin user.
Create, edit and delete alert destination with admin user.
Create, edit and delete alert channels with admin user.
View and acknowledge alers with with admin user.
Modify health settings with admin user.

View alert configurations, destinations and channels with read only user. Make sure edit functionality is not available.
View health settings with read only user. Make sure edit functionality is not available.
View alerts with read only user. Make sure acknowledge functionality is not available.

Screenshots for readonly user:

{F20243}
{F20244}
{F20246}
{F20250}
{F20273}

{F20274}

{F20275}

{F20276}

{F20277}

{F20278}

Reviewers: spotachev, kkannan

Reviewed By: spotachev, kkannan

Subscribers: jenkins-bot, sanketh, yugaware

Differential Revision: https://phabricator.dev.yugabyte.com/D13616

managed/ui/src/pages/Administration.tsx
@@ -1,10 +1,9 @@
 import React, { FC, useEffect } from 'react';
 import { Tab } from 'react-bootstrap';
-import { browserHistory } from 'react-router';
 import { Selector, useSelector } from 'react-redux';
 import { RouteComponentProps } from 'react-router-dom';
 import { YBTabsPanel, YBTabsWithLinksPanel } from '../components/panels';
-import { showOrRedirect } from '../utils/LayoutUtils';
+import { isAvailable, showOrRedirect } from '../utils/LayoutUtils';
 import { HAInstances, HAReplication } from '../components/ha';
 import './Administration.scss';
 import { AlertConfigurationContainer } from '../components/alerts';
@@ -50,19 +49,18 @@ interface RouteParams {
   section: HighAvailabilityTabs | AlertConfigurationTabs;
 }
 
-const DEFAULT_ADMIN_PAGE = `/admin/${AdministrationTabs.HA}/${HighAvailabilityTabs.Replication}`;
 const customerSelector: Selector<Store, Customer> = (state) => state.customer.currentCustomer;
 const featureFlags: Selector<FeatureStore, FetureFlags> = (state) => state.featureFlags;
 
 export const Administration: FC<RouteComponentProps<{}, RouteParams>> = ({ params }) => {
   const currentCustomer = useSelector(customerSelector);
   const { test, released } = useSelector(featureFlags);
+  const defaultTab = isAvailable(currentCustomer.data.features, 'administration.highAvailability')
+    ? AdministrationTabs.HA
+    : AdministrationTabs.AC;
 
   useEffect(() => {
     showOrRedirect(currentCustomer.data.features, 'menu.administration');
-    if (!params.tab && !params.section) {
-      browserHistory.replace(DEFAULT_ADMIN_PAGE);
-    }
   }, [currentCustomer, params.tab, params.section]);
 
   const getAlertTab = () => {
@@ -74,55 +72,59 @@ export const Administration: FC<RouteComponentProps<{}, RouteParams>> = ({ param
           routePrefix={`/admin/${AdministrationTabs.AC}/`}
         />
       </Tab>
-    ) : (
-      <span />
-    );
+    ) : null;
   };
+
+  const getHighAvailabilityTab = () => {
+    return isAvailable(currentCustomer.data.features, 'administration.highAvailability') ? (
+      <Tab eventKey="ha" title="High Availability" key="high-availability">
+        <YBTabsPanel
+          defaultTab={HighAvailabilityTabs.Replication}
+          activeTab={params.section}
+          routePrefix={`/admin/${AdministrationTabs.HA}/`}
+          id="administration-ha-subtab"
+          className="config-tabs"
+        >
+          <Tab
+            eventKey={HighAvailabilityTabs.Replication}
+            title={
+              <span>
+                <i className="fa fa-clone tab-logo" aria-hidden="true"></i> Replication
+                Configuration
+              </span>
+            }
+            unmountOnExit
+          >
+            <HAReplication />
+          </Tab>
+          <Tab
+            eventKey={HighAvailabilityTabs.Instances}
+            title={
+              <span>
+                <i className="fa fa-codepen tab-logo" aria-hidden="true"></i> Instance
+                Configuration
+              </span>
+            }
+            unmountOnExit
+          >
+            <HAInstances />
+          </Tab>
+        </YBTabsPanel>
+      </Tab>
+    ) : null;
+  }
 
   return (
     <div>
       <h2 className="content-title">Platform Configuration</h2>
       <YBTabsWithLinksPanel
-        defaultTab={AdministrationTabs.HA}
+        defaultTab={defaultTab}
         activeTab={params.tab}
         routePrefix="/admin/"
         id="administration-main-tabs"
         className="universe-detail data-center-config-tab"
       >
-        <Tab eventKey="ha" title="High Availability" key="high-availability">
-          <YBTabsPanel
-            defaultTab={HighAvailabilityTabs.Replication}
-            activeTab={params.section}
-            routePrefix={`/admin/${AdministrationTabs.HA}/`}
-            id="administration-ha-subtab"
-            className="config-tabs"
-          >
-            <Tab
-              eventKey={HighAvailabilityTabs.Replication}
-              title={
-                <span>
-                  <i className="fa fa-clone tab-logo" aria-hidden="true"></i> Replication
-                  Configuration
-                </span>
-              }
-              unmountOnExit
-            >
-              <HAReplication />
-            </Tab>
-            <Tab
-              eventKey={HighAvailabilityTabs.Instances}
-              title={
-                <span>
-                  <i className="fa fa-codepen tab-logo" aria-hidden="true"></i> Instance
-                  Configuration
-                </span>
-              }
-              unmountOnExit
-            >
-              <HAInstances />
-            </Tab>
-          </YBTabsPanel>
-        </Tab>
+        {getHighAvailabilityTab()}
         {getAlertTab()}
       </YBTabsWithLinksPanel>
     </div>

managed/src/main/java/com/yugabyte/yw/common/alerts/AlertConfigurationService.java
@@ -22,7 +22,7 @@ import com.google.common.collect.ImmutableSet;
 import com.yugabyte.yw.common.AlertTemplate;
 import com.yugabyte.yw.common.BeanValidator;
 import com.yugabyte.yw.common.PlatformServiceException;
-import com.yugabyte.yw.common.alerts.impl.AlertConfigurationTemplate;
+import com.yugabyte.yw.models.extended.AlertConfigurationTemplate;
 import com.yugabyte.yw.common.concurrent.MultiKeyLock;
 import com.yugabyte.yw.common.config.RuntimeConfigFactory;
 import com.yugabyte.yw.common.metrics.MetricLabelsBuilder;

managed/src/main/java/com/yugabyte/yw/common/audit/AuditService.java
@@ -18,13 +18,11 @@ import com.jayway.jsonpath.JsonPath;
 import com.jayway.jsonpath.spi.json.JacksonJsonNodeJsonProvider;
 import com.jayway.jsonpath.spi.mapper.JacksonMappingProvider;
 import com.yugabyte.yw.models.Audit;
-import com.yugabyte.yw.models.Users;
+import com.yugabyte.yw.models.extended.UserWithFeatures;
 import java.util.List;
 import java.util.UUID;
 import java.util.stream.Collectors;
 import javax.inject.Singleton;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import play.mvc.Http;
 
 @Singleton
@@ -107,11 +105,11 @@ public class AuditService {
   @Deprecated
   public void createAuditEntry(
       Http.Context ctx, Http.Request request, JsonNode params, UUID taskUUID) {
-    Users user = (Users) ctx.args.get("user");
+    UserWithFeatures user = (UserWithFeatures) ctx.args.get("user");
     String method = request.method();
     String path = request.path();
     JsonNode redactedParams = filterSecretFields(params);
-    Audit.create(user.uuid, user.customerUUID, path, method, redactedParams, taskUUID);
+    Audit.create(user.getUser(), path, method, redactedParams, taskUUID);
   }
 
   public List<Audit> getAll(UUID customerUUID) {

managed/src/main/java/com/yugabyte/yw/common/user/UserService.java
@@ -1,0 +1,56 @@
+/*
+ * Copyright 2021 YugaByte, Inc. and Contributors
+ *
+ * Licensed under the Polyform Free Trial License 1.0.0 (the "License"); you
+ * may not use this file except in compliance with the License. You
+ * may obtain a copy of the License at
+ *
+ * http://github.com/YugaByte/yugabyte-db/blob/master/licenses/POLYFORM-FREE-TRIAL-LICENSE-1.0.0.txt
+ */
+package com.yugabyte.yw.common.user;
+
+import static play.mvc.Http.Status.INTERNAL_SERVER_ERROR;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.yugabyte.yw.common.PlatformServiceException;
+import com.yugabyte.yw.models.Customer;
+import com.yugabyte.yw.models.Users;
+import com.yugabyte.yw.models.extended.UserWithFeatures;
+import java.io.IOException;
+import java.io.InputStream;
+import javax.inject.Inject;
+import javax.inject.Singleton;
+import play.Environment;
+
+@Singleton
+public class UserService {
+
+  private final Environment environment;
+
+  @Inject
+  public UserService(Environment environment) {
+    this.environment = environment;
+  }
+
+  public UserWithFeatures getUserWithFeatures(Customer customer, Users user) {
+    try {
+      UserWithFeatures userWithFeatures = new UserWithFeatures().setUser(user);
+      String configFile = user.getRole().getFeaturesFile();
+      if (customer.code.equals("cloud")) {
+        configFile = "cloudFeatureConfig.json";
+      }
+      if (configFile == null) {
+        return userWithFeatures;
+      }
+      try (InputStream featureStream = environment.resourceAsStream(configFile)) {
+        ObjectMapper mapper = new ObjectMapper();
+        JsonNode features = mapper.readTree(featureStream);
+        return userWithFeatures.setFeatures(features);
+      }
+    } catch (IOException e) {
+      throw new PlatformServiceException(
+          INTERNAL_SERVER_ERROR, "Failed to parse user features config file: " + e.getMessage());
+    }
+  }
+}

managed/src/main/java/com/yugabyte/yw/controllers/AbstractPlatformController.java
@@ -16,7 +16,7 @@ import com.google.inject.Inject;
 import com.yugabyte.yw.common.PlatformServiceException;
 import com.yugabyte.yw.common.ValidatingFormFactory;
 import com.yugabyte.yw.common.audit.AuditService;
-import com.yugabyte.yw.models.Users;
+import com.yugabyte.yw.models.extended.UserWithFeatures;
 import io.swagger.annotations.ApiKeyAuthDefinition;
 import io.swagger.annotations.Contact;
 import io.swagger.annotations.ExternalDocs;
@@ -72,7 +72,7 @@ public abstract class AbstractPlatformController extends Controller {
   @Inject private AuditService auditService;
 
   protected AuditService auditService() {
-    Users user = (Users) Http.Context.current().args.get("user");
+    UserWithFeatures user = (UserWithFeatures) Http.Context.current().args.get("user");
     if (user == null) {
       throw new IllegalStateException("Shouldn't audit unauthenticated requests!");
     }

managed/src/main/java/com/yugabyte/yw/controllers/AlertController.java
@@ -21,7 +21,7 @@ import com.yugabyte.yw.common.alerts.AlertChannelService;
 import com.yugabyte.yw.common.alerts.AlertConfigurationService;
 import com.yugabyte.yw.common.alerts.AlertDestinationService;
 import com.yugabyte.yw.common.alerts.AlertService;
-import com.yugabyte.yw.common.alerts.impl.AlertConfigurationTemplate;
+import com.yugabyte.yw.models.extended.AlertConfigurationTemplate;
 import com.yugabyte.yw.common.metrics.MetricService;
 import com.yugabyte.yw.forms.AlertChannelFormData;
 import com.yugabyte.yw.forms.AlertDestinationFormData;

managed/src/main/java/com/yugabyte/yw/controllers/BackupsController.java
@@ -22,7 +22,7 @@ import com.yugabyte.yw.models.CustomerConfig;
 import com.yugabyte.yw.models.CustomerTask;
 import com.yugabyte.yw.models.TaskInfo;
 import com.yugabyte.yw.models.Universe;
-import com.yugabyte.yw.models.Users;
+import com.yugabyte.yw.models.extended.UserWithFeatures;
 import com.yugabyte.yw.models.helpers.CommonUtils;
 import com.yugabyte.yw.models.helpers.TaskType;
 import io.swagger.annotations.Api;
@@ -72,7 +72,7 @@ public class BackupsController extends AuthenticatedController {
             Customer.get(customerUUID).getFeatures(), "universes.details.backups.storageLocation");
     boolean isStorageLocMasked = custStorageLoc != null && custStorageLoc.asText().equals("hidden");
     if (!isStorageLocMasked) {
-      Users user = (Users) ctx().args.get("user");
+      UserWithFeatures user = (UserWithFeatures) ctx().args.get("user");
       JsonNode userStorageLoc =
           CommonUtils.getNodeProperty(
               user.getFeatures(), "universes.details.backups.storageLocation");

managed/src/main/java/com/yugabyte/yw/controllers/CustomerController.java
@@ -43,6 +43,7 @@ import com.yugabyte.yw.models.Provider;
 import com.yugabyte.yw.models.Region;
 import com.yugabyte.yw.models.Universe;
 import com.yugabyte.yw.models.Users;
+import com.yugabyte.yw.models.extended.UserWithFeatures;
 import com.yugabyte.yw.models.helpers.CommonUtils;
 import com.yugabyte.yw.models.helpers.NodeDetails;
 import io.swagger.annotations.Api;
@@ -130,7 +131,7 @@ public class CustomerController extends AuthenticatedController {
     responseJson.put(
         "callhomeLevel", CustomerConfig.getOrCreateCallhomeLevel(customerUUID).toString());
 
-    Users user = (Users) ctx().args.get("user");
+    UserWithFeatures user = (UserWithFeatures) ctx().args.get("user");
     if (customer.getFeatures().size() != 0 && user.getFeatures().size() != 0) {
       JsonNode featureSet = user.getFeatures();
       CommonUtils.deepMerge(featureSet, customer.getFeatures());

commit 0c6c93c6700c13eddc139437bf4952d598433fa5

[PLAT-2200] Platform UI: Fix regression with HA "standby" overlay
Summary: Fix for a regression introduced in D13616 when "standby" overlay is visible on all pages, including HA Administration.

Test Plan: Run UI on the standby instance, click the "Admin" icon from the sidebar. Expected that the overlay will go away and the HA Administration page will be visible.

Reviewers: andrew, jmak, skim

Reviewed By: skim

Subscribers: jenkins-bot, yugaware, ui

Differential Revision: https://phabricator.dev.yugabyte.com/D13810

managed/ui/src/pages/Administration.tsx
@@ -1,5 +1,6 @@
 import React, { FC, useEffect } from 'react';
 import { Tab } from 'react-bootstrap';
+import { browserHistory } from 'react-router';
 import { Selector, useSelector } from 'react-redux';
 import { RouteComponentProps } from 'react-router-dom';
 import { YBTabsPanel, YBTabsWithLinksPanel } from '../components/panels';
@@ -61,7 +62,12 @@ export const Administration: FC<RouteComponentProps<{}, RouteParams>> = ({ param
 
   useEffect(() => {
     showOrRedirect(currentCustomer.data.features, 'menu.administration');
-  }, [currentCustomer, params.tab, params.section]);
+    // redirect to a proper url on navigation from sidebar "Admin" button (the "/admin" route)
+    if (!params.tab && !params.section) {
+      const url = `/admin/${defaultTab}`;
+      browserHistory.replace(url);
+    }
+  }, [currentCustomer, defaultTab, params.tab, params.section]);
 
   const getAlertTab = () => {
     return test?.adminAlertsConfig || released?.adminAlertsConfig ? (