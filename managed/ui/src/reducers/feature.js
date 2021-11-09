cc44e9c34721c13d33b09acbe063b1a4ad610d12
kkannan <kkannan@yugabyte.com>
2021-11-09 16:28:21 +0530

    [PLAT-526][Platform][UI] UI support for xCluster replication management phase-1
    Summary:
    Implemented XCluster UI and integrated with the backend API.
    
    Features Implemented:
    1) Configure/Create XCluster Replication
    2) List Replications
    3) Pause/Enable Replication
    4) Delete Replication
    5) View overview of replications
    6) Edit Replication
    7) Add/removed tables to/from replications
    
    Figma Link: https://www.figma.com/file/QZk6Zb6wn0shsv1KDKWeFA/Platform-WIP-Review-files?node-id=2%3A21
    
    Some parts are not implemented since API support is not available
    
    Test Plan:
    Tested Manually.
    
    {F20344}
    
    {F20345}
    
    {F19965}
    
    {F19966}
    
    {F19967}
    
    Reviewers: spotachev, andrew, jvigil, ssutar, cpadinjareveettil
    
    Reviewed By: jvigil, ssutar, cpadinjareveettil
    
    Subscribers: jvigil, hsu, jenkins-bot
    
    Differential Revision: https://phabricator.dev.yugabyte.com/D12728

--- managed/ui/src/reducers/feature.js
+++ managed/ui/src/reducers/feature.js
@@ -6,6 +6,7 @@ const initialStateFeatureInTest = {
   adminAlertsConfig:true,
   enableNewEncryptionInTransitModal: true,
   addRestoreTimeStamp: false,
+  enableXCluster: false,
 };
 
 const initialStateFeatureReleased = {
@@ -14,6 +15,7 @@ const initialStateFeatureReleased = {
   adminAlertsConfig:true,
   enableNewEncryptionInTransitModal: true,
   addRestoreTimeStamp: false,
+  enableXCluster: false,
 };
 
 export const FeatureFlag = (
