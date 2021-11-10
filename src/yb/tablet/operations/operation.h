bd2626d2b50f6dc5d1c9180119a53adb8c371e6d
John Meehan <jmeehan16@users.noreply.github.com>
2021-11-09 23:50:25 -0500

    [#10504] docdb - Move value TTL metadata collection to DocWriteBatch
    Summary:
    Request TTL metadata is currently missing from tablet replicas, leading to replica SST files
    missing value TTL frontier metadata necessary for expiration. This is happening because the metadata
    was being stored in operations (which are rebuilt on replicas) rather than being stored in the doc
    batch write requests themselves.
    
    This commit moves the value TTL metadata collection into DocWriteBatch and adds a field for ttl in the
    KeyValueWriteBatchPB. It also modifies the logic on the tablet side to calculate the expiration timestamp
    based on the KeyValueWriteBatchPB metadata.
    
    Also adds the CompactionTestWithFileExpirationRF3 test suite.
    
    Test Plan:
    Jenkins: hot
    ybd --cxx-test integration-tests_compaction-test --gtest_filter CompactionTestWithFileExpiration.ReplicatedMetadataCanExpireFile
    ybd --cxx-test integration-tests_compaction-test --gtest_filter CompactionTestWithFileExpiration.ReplicatedNoMetadataUsesTableTTL
    ybd --cxx-test docdb_docdb-test --gtest_filter DocDBTests/DocDBTestWrapper.TestUpdateDocWriteBatchTTL/0
    
    Reviewers: timur, rsami, sergei
    
    Reviewed By: sergei
    
    Subscribers: ybase, anallan, bogdan
    
    Differential Revision: https://phabricator.dev.yugabyte.com/D13855

--- src/yb/tablet/operations/operation.h
+++ src/yb/tablet/operations/operation.h
@@ -181,18 +181,6 @@ class Operation {
     return op_id_;
   }
 
-  void UpdateIfMaxTtl(const MonoDelta& ttl);
-
-  const MonoDelta ttl() const {
-    std::lock_guard<simple_spinlock> l(mutex_);
-    return ttl_;
-  }
-
-  bool has_ttl() const {
-    std::lock_guard<simple_spinlock> l(mutex_);
-    return ttl_.Initialized();
-  }
-
   bool has_completion_callback() const {
     return completion_clbk_ != nullptr;
   }
@@ -246,8 +234,6 @@ class Operation {
 
   // This OpId stores the canonical "anchor" OpId for this transaction.
   OpId op_id_ GUARDED_BY(mutex_);
-
-  MonoDelta ttl_ GUARDED_BY(mutex_);
 
   scoped_refptr<consensus::ConsensusRound> consensus_round_;
 };
