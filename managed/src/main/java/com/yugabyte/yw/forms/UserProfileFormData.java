40328bcad18d474edd6cbc501c0eeec06cf32876
Sean Kim <skim@yugabyte.com>
2021-11-17 11:58:50 -0500

    [PLAT-2295] Platform: Timezone Selector on user profile (Backend) (WIP)
    Summary:
    API endpoint that allows updating user profile (password and timezone) and one that allows updating user's timezone.
    It also includes a migration file that adds a timezone column to users table.
    
    Test Plan: Test if the /update_profile endpoint updates the user's data correctly, and returns a response containing the updated user data
    
    Reviewers: amalyshev
    
    Reviewed By: amalyshev
    
    Subscribers: jenkins-bot, sanketh, yugaware
    
    Differential Revision: https://phabricator.dev.yugabyte.com/D13987

--- managed/src/main/java/com/yugabyte/yw/forms/UserProfileFormData.java
+++ managed/src/main/java/com/yugabyte/yw/forms/UserProfileFormData.java
@@ -1,23 +1,15 @@
-// Copyright (c) Yugabyte, Inc.
-
 package com.yugabyte.yw.forms;
 
-import static com.yugabyte.yw.models.Users.Role;
-
+import com.yugabyte.yw.models.Users;
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
-import java.util.Map;
 import play.data.validation.Constraints;
 
 /** This class will be used by the API and UI Form Elements to validate constraints are met */
 @ApiModel(
-    value = "UserRegistrationData",
-    description = "User registration data. The API and UI use this to validate form data.")
-public class UserRegisterFormData {
-  @Constraints.Required()
-  @ApiModelProperty(value = "Email address", example = "test@example.com", required = true)
-  @Constraints.Email
-  private String email;
+    value = "UserProfileData",
+    description = "User profile data. The API and UI use this to validate form data.")
+public class UserProfileFormData {
 
   @ApiModelProperty(value = "Password", example = "Test@1234")
   private String password;
@@ -25,23 +17,12 @@ public class UserRegisterFormData {
   @ApiModelProperty(value = "Password confirmation", example = "Test@1234")
   private String confirmPassword;
 
-  @ApiModelProperty(value = "User features")
-  private Map features;
-
   @Constraints.Required()
   @ApiModelProperty(value = "User role", example = "Admin", required = true)
-  private Role role;
+  private Users.Role role;
 
   @ApiModelProperty(value = "User timezone", example = "America/Toronto")
   private String timezone;
-
-  public String getEmail() {
-    return email;
-  }
-
-  public void setEmail(String email) {
-    this.email = email;
-  }
 
   public String getPassword() {
     return password;
@@ -59,19 +40,11 @@ public class UserRegisterFormData {
     this.confirmPassword = confirmPassword;
   }
 
-  public Map getFeatures() {
-    return features;
-  }
-
-  public void setFeatures(Map features) {
-    this.features = features;
-  }
-
-  public Role getRole() {
+  public Users.Role getRole() {
     return role;
   }
 
-  public void setRole(Role role) {
+  public void setRole(Users.Role role) {
     this.role = role;
   }
 
