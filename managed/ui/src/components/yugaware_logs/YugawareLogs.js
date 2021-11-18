bd8111c285606982aea2849d397eca1cb0468593
kkannan <kkannan@yugabyte.com>
2021-11-18 12:35:46 +0530

    [PLAT-2261][Platform] Enhancements to /logs UI endpoint
    Summary:
    This diff implements
    1) Getting search params from the url
    2) UI enhancement
    3) Disabled debounce to remove auto search
    
    Test Plan:
    Tested Manually
    
    Navigated between pages and tested whether the params in the url are getting fetched correctly.
    
    {F20565}
    
    Reviewers: ssutar, mjoshi, cpadinjareveettil
    
    Reviewed By: cpadinjareveettil
    
    Subscribers: jenkins-bot, ui
    
    Differential Revision: https://phabricator.dev.yugabyte.com/D14006

--- managed/ui/src/components/yugaware_logs/YugawareLogs.js
+++ managed/ui/src/components/yugaware_logs/YugawareLogs.js
@@ -7,8 +7,17 @@ import 'ace-builds/src-noconflict/ext-searchbox';
 import 'ace-builds/src-noconflict/mode-java';
 import 'ace-builds/src-noconflict/theme-github';
 import { Col, Row } from 'react-bootstrap';
-import { YBControlledNumericInput, YBInputField, YBSelectWithLabel } from '../common/forms/fields';
-import { useDebounce, useMount } from 'react-use';
+import {
+  YBButton,
+  YBControlledNumericInputWithLabel,
+  YBControlledSelectWithLabel,
+  YBControlledTextInput
+} from '../common/forms/fields';
+import { useMount } from 'react-use';
+
+import './YugawareLogs.scss';
+
+const DEFAULT_MAX_LINES = 100;
 
 const YugawareLogs = ({ currentCustomer, yugawareLogs, getLogs, logError, fetchUniverseList }) => {
   const editorStyle = {
@@ -16,22 +25,43 @@ const YugawareLogs = ({ currentCustomer, yugawareLogs, getLogs, logError, fetchU
     height: 'calc(100vh - 150px)'
   };
 
-  const [maxLines, setMaxLines] = useState(100);
+  const [maxLines, setMaxLines] = useState(DEFAULT_MAX_LINES);
   const [regex, setRegex] = useState(undefined);
   const [selectedUniverse, setSelectedUniverse] = useState(undefined);
   const [universeList, setUniverseList] = useState([<option key={'loading'}>Loading..</option>]);
 
-  useDebounce(
-    () => {
-      getLogs(maxLines, regex, selectedUniverse);
-    },
-    500,
-    [maxLines, regex, selectedUniverse]
-  );
+  const doSearch = () => {
+    getLogs(maxLines, regex, selectedUniverse);
+
+    var newURL = new URL(
+      window.location.protocol + '//' + window.location.host + window.location.pathname
+    );
+    if (regex) {
+      newURL.searchParams.set('queryRegex', regex);
+    }
+    if (selectedUniverse) {
+      newURL.searchParams.set('universeName', selectedUniverse);
+    }
+    if (maxLines) {
+      newURL.searchParams.set('maxLines', maxLines);
+    }
+    window.history.replaceState('', '', newURL.toString());
+  };
 
   useMount(() => {
     showOrRedirect(currentCustomer.data.features, 'main.logs');
-    getLogs(maxLines, regex, selectedUniverse);
+
+    const params = new URLSearchParams(window.location.search);
+    const regexFromParam = params.get('queryRegex') || undefined;
+    const maxLinesFromParam = params.get('maxLines') || DEFAULT_MAX_LINES;
+    const universeFromParam = params.get('universeName') || undefined;
+
+    setRegex(regexFromParam);
+    setMaxLines(maxLinesFromParam);
+    setSelectedUniverse(universeFromParam);
+
+    getLogs(maxLinesFromParam, regexFromParam, universeFromParam);
+
     fetchUniverseList().then((resp) => {
       const universesOptions = resp.map((uni) => (
         <option key={uni.universeUUID} value={uni.name}>
@@ -48,36 +78,55 @@ const YugawareLogs = ({ currentCustomer, yugawareLogs, getLogs, logError, fetchU
   });
 
   return (
-    <div>
+    <div className="yugaware-logs">
       <h2 className="content-title">
         <b>YugaWare logs</b>
       </h2>
       <Row>
-        <Col lg={8}>
-          <YBInputField
-            placeHolder="Enter your regex here"
-            onValueChanged={(val) => setRegex(val)}
-          />
-        </Col>
         <Col lg={2}>
-          <YBSelectWithLabel
+          <YBControlledSelectWithLabel
+            selectVal={selectedUniverse}
+            label="Universe to filter"
             options={universeList}
-            onInputChanged={(value) => setSelectedUniverse(value ? value : undefined)}
+            onInputChanged={(event) => {
+              setSelectedUniverse(event.target.value ? event.target.value : undefined);
+            }}
           />
         </Col>
-        <Col lg={2}>
-          <YBControlledNumericInput
-            placeHolder="max lines"
+        <Col lg={3}>
+          <YBControlledNumericInputWithLabel
+            label="Max lines to display"
             minVal={10}
             val={maxLines}
             onInputChanged={setMaxLines}
           />
         </Col>
+        <Col lg={7} className="noPadding">
+          <Row className="vertical-align">
+            <Col lg={10}>
+              <YBControlledTextInput
+                val={regex}
+                label="Regex"
+                placeHolder="Enter your regex"
+                onValueChanged={(e) => setRegex(e.target.value)}
+              />
+            </Col>
+            <Col lg={2}>
+              <YBButton
+                btnText="Search"
+                className="search-button"
+                btnClass="btn btn-orange"
+                onClick={doSearch}
+              />
+            </Col>
+          </Row>
+        </Col>
       </Row>
-
       <div>
         {logError ? (
-          <div>Something went wrong fetching logs.</div>
+          <Row>
+            <Col lg={12}>Something went wrong while fetching logs.</Col>
+          </Row>
         ) : (
           <Row>
             <Col lg={12}>
