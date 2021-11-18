9709c4a0227d81c6045af9e4774ccb3354a5bdce
Rahul Desirazu <rahuldesirazu@users.noreply.github.com>
2021-11-18 12:36:30 -0800

    docdb [#9877]: Fix xCluster Stream Deletion Race Condition
    Summary:
    Fix a race condition in xCluster stream deletion where cdc service upserts the checkpoint for cdc_state, causing insert of entries for streams entries. Fix this by making the update checkpoint operation a pure update and handling initial setup insert into the cdc_state table as part of master's CreateCDCStream RPC. Deletion is already handled in DeleteCDCStream so no changes are needed on the delete path.
    
    We still want to let BootstrapProducer modify cdc_state with a unique opid and bootstrap id, so we don't modify cdc_state in CreateCDCStream if the call comes from bootstrap.
    
    Test Plan: ybd release --cxx-test integration-tests_cdc_service-int-test --gtest_filter *TestDeleteCDCStream/1
    
    Reviewers: nicolas, jhe
    
    Reviewed By: jhe
    
    Subscribers: ybase, bogdan
    
    Differential Revision: https://phabricator.dev.yugabyte.com/D14028

--- src/yb/master/master.h
+++ src/yb/master/master.h
@@ -152,6 +152,10 @@ class Master : public tserver::DbServerBase {
     return *async_client_init_;
   }
 
+  yb::client::AsyncClientInitialiser& cdc_state_client_initializer() {
+    return *cdc_state_client_init_;
+  }
+
   enum MasterMetricType {
     TaskMetric,
     AttemptMetric,
@@ -222,6 +226,7 @@ class Master : public tserver::DbServerBase {
   std::unique_ptr<MasterTabletServer> master_tablet_server_;
 
   std::unique_ptr<yb::client::AsyncClientInitialiser> async_client_init_;
+  std::unique_ptr<yb::client::AsyncClientInitialiser> cdc_state_client_init_;
   std::mutex master_metrics_mutex_;
   std::map<std::string, scoped_refptr<Histogram>> master_metrics_ GUARDED_BY(master_metrics_mutex_);
 
