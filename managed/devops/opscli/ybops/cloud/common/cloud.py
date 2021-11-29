0366a6b5b0fc09221819a436e34d844ea65366b3
Yury Shchetinin <yshchetinin@yugabyte.com>
2021-11-29 19:52:29 +0300

    [PLAT-2374] Fix creating certificates for non-ip onprem hosts
    Summary: Do not add SAN entry in case of non-ip (same as in master)
    
    Test Plan:
    Jenkins: rebase: 2.6
    1) Add onprem instance using dns-name
    2) Create universe with TLS ON - success
    3) Create universe with TLS ON in 2.6 - failure
    
    Reviewers: sanketh, spotachev, amalyshev
    
    Reviewed By: spotachev, amalyshev
    
    Subscribers: jenkins-bot, yugaware
    
    Differential Revision: https://phabricator.dev.yugabyte.com/D14167

--- managed/devops/opscli/ybops/cloud/common/cloud.py
+++ managed/devops/opscli/ybops/cloud/common/cloud.py
@@ -467,9 +467,13 @@ class AbstractCloud(AbstractCommandParser):
         builder = builder.public_key(public_key)
         builder = builder.add_extension(x509.BasicConstraints(ca=False, path_length=None),
                                         critical=True)
-        builder = builder.add_extension(x509.SubjectAlternativeName(
-                                        [x509.IPAddress(ipaddress.ip_address(node_ip))]),
-                                        critical=False)
+        try:
+            builder = builder.add_extension(x509.SubjectAlternativeName(
+                [x509.IPAddress(ipaddress.ip_address(node_ip))]),
+                 critical=False)
+        except ValueError:
+            pass
+
         certificate = builder.sign(private_key=root_key, algorithm=hashes.SHA256(),
                                    backend=default_backend())
         # Write private key to file
