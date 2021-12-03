7c68772925fe5ae9bcd07312785ed1ce333bdb7a
Charles Wang <wang.charles234@gmail.com>
2021-12-02 19:18:26 -0500

    [BACKPORT 2.11.0][PLAT-2434]Platform Fix omitted log configurations after application start
    Summary:
    Logback is configured using Java system properties. Play has a setting, `play.logger.includeConfigProperties` which allows .conf entries to be configured to system properties.
    However, reloading logback.xml will not allow these entries from .conf to be treated as system properties. This fix will resolve this issue, by getting specific .conf entries needed from
    logback.xml and setting them at the beginning of app creation right before we reload logback.xml.
    
    These properties include `log.override.path,` `application.home,` and `yb.cloud.enabled`
    
     Original commit: [[ https://phabricator.dev.yugabyte.com/D14214 | D12070  ]] / [[ https://github.com/yugabyte/yugabyte-db/commit/a24436c2b7648cc949e4c9fe3b7888f3352274df | a24436c2b7648cc949e4c9fe3b7888f3352274df ]]
    
    Partial additions from: [[ https://phabricator.dev.yugabyte.com/D14085 | D14085 ]] / [[ https://github.com/yugabyte/yugabyte-db/commit/1cf9eb1167edde618dfa2f0c8ca53f16f45ea991#diff-4b5677de3037f77d96b2c76fe7683783683b90ac4de291ce5f22cf4082a523df | 1cf9eb1167edde618dfa2f0c8ca53f16f45ea991 ]]
    
    Test Plan:
    Series of Test cases (in every test, restart the application). The purpose of these tests is to demonstrate that after logback.xml is reloaded, the system variable values in logback.xml are maintained and not lost
    1. No changes to `reference.conf`,  application logs should write to `./logs/application.log`
    2. Add a temporary `log.override.path,` for example: `/<home>/tmp`. The application logs should write to  `/<home>/tmp/application.log` throughout and should not change location of where the file is written
    3.  Add a temporary `log.override.path,` for example: `/<home>/tmp` and temporary `application.home`, for example `/<home>/sample-home/tmp`. The application logs should write to  `/<home>/tmp/application.log` throughout and should not change location of where the file is written
    4. Only have temporary `application.home`, for example `/<home>/sample-home/tmp`. The application logs should now write to `/<home>/sample-home/tmp/logs/application.log` and should not change the location of where the file is written
    5. Only set `yb.cloud.enabled` to be `true`. Throughout the entire application start and continuation, all terminal logs should be in json format and should not revert back to  default line format (application.log lines will be normal and not json here)
    
    If the below code is not added, for tests 2-4, after logback.xml is refreshed, the logs will switch to writing to `./logs/application.log` instead of the expected location.
    Also for test 5, the json format for logs in the terminal will not be maintained after logback.xml is refreshed
    
    Reviewers: spotachev, sanketh, yshchetinin
    
    Reviewed By: spotachev, yshchetinin
    
    Subscribers: jenkins-bot, yugaware
    
    Differential Revision: https://phabricator.dev.yugabyte.com/D14226

--- managed/src/main/java/com/yugabyte/yw/common/logging/LogUtil.java
+++ managed/src/main/java/com/yugabyte/yw/common/logging/LogUtil.java
@@ -4,6 +4,7 @@ import ch.qos.logback.classic.LoggerContext;
 import ch.qos.logback.classic.util.ContextInitializer;
 import ch.qos.logback.core.joran.spi.JoranException;
 import com.google.inject.Singleton;
+import com.typesafe.config.Config;
 import com.yugabyte.yw.common.config.impl.SettableRuntimeConfigFactory;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -15,7 +16,26 @@ public class LogUtil {
   private static final String LOGGING_CONFIG_KEY = "yb.logging.config";
   private static final String APPLICATION_LOG_LEVEL_SYS_PROPERTY = "applicationLogLevel";
 
-  public static String getLoggingConfig(SettableRuntimeConfigFactory sConfigFactory) {
+  private static final String LOG_OVERRIDE_PATH_SYS_PROPERTY = "log.override.path";
+  private static final String YB_CLOUD_ENABLED_SYS_PROPERTY = "yb.cloud.enabled";
+  private static final String APPLICATION_HOME_SYS_PROPERTY = "application.home";
+  private static final String[] fixedLogbackSysProperties = {
+    LOG_OVERRIDE_PATH_SYS_PROPERTY, YB_CLOUD_ENABLED_SYS_PROPERTY, APPLICATION_HOME_SYS_PROPERTY
+  };
+
+  public static void updateLoggingFromConfig(
+      SettableRuntimeConfigFactory sConfigFactory, Config config) {
+    String logLevel = LogUtil.getLoggingLevel(sConfigFactory);
+    // need to configure this to simulate the effect of play.logger.includeConfigProperties=true
+    configureFixedLogbackSystemProperties(config);
+    try {
+      setLoggingLevel(logLevel);
+    } catch (JoranException ex) {
+      LOG.warn("Could not initialize logback");
+    }
+  }
+
+  private static String getLoggingLevel(SettableRuntimeConfigFactory sConfigFactory) {
     return sConfigFactory.globalRuntimeConf().getString(LOGGING_CONFIG_KEY);
   }
 
@@ -35,5 +55,23 @@ public class LogUtil {
     loggerContext.reset();
     ContextInitializer ci = new ContextInitializer(loggerContext);
     ci.autoConfig();
+  }
+
+  public static void configureFixedLogbackSystemProperties(Config config) {
+    for (String sysPropertyName : fixedLogbackSysProperties) {
+      boolean hasPath = config.hasPath(sysPropertyName);
+      if (hasPath) {
+        String value = config.getString(sysPropertyName);
+        setSystemProperty(sysPropertyName, value, false);
+      }
+    }
+  }
+
+  private static void setSystemProperty(String key, String value, boolean eraseIfEmpty) {
+    if (value != null) {
+      System.setProperty(key, value);
+    } else if (eraseIfEmpty) {
+      System.clearProperty(key);
+    }
   }
 }
