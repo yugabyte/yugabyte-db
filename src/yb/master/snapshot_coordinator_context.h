d3d44b0a37bfa971977b24b2a4003028cc6fa56c
Sanket Kedia <kediasanket11121993@gmail.com>
2021-12-08 09:46:41 -0800

    [#10482] DST: Rate-limit Create/Delete Snapshot requests
    Summary:
    When a Create/Delete Snapshot is executed on a large number of tables/tablets it results in
    too many RPC requests being sent to tservers that result in higher read/write latencies to the clients.
    We should have a way of rate-limiting these requests so that there is progress and at the same time
    the tservers don't get overwhelmed. Note that there is a scope to throttle other types of RPC
    requests too and a generic solution in the long term is expected but this diff fixes the short-term
    solution for at-least snapshots.
    
    The degree of concurrency is controlled by master flags max_concurrent_snapshot_rpcs and
    max_concurrent_snapshot_rpcs_per_tserver which are a measure of the maximum number of snapshot
    requests that can be concurrent at any given point in time.
    
    The following determine how to use them. If the value of max_concurrent_snapshot_rpcs is:
    ```
    > 0:  use max_concurrent_snapshot_rpcs value as #concurrent RPCs
    = 0 : INT_MAX is the #concurrent RPCs
    < 0 : #concurrent RPCs is num_servers * max_concurrent_snapshot_rpcs_per_tserver
    ```
    
    There is no effect on the client timeout. It replicates the operation at the master and responds
    with a snapshot id as it is doing today. The background thread then takes care of throttling.
    
    Test Plan:
    Simulated a mixed workload comprising of 75% reads and 25% writes and measured the latency of queries
    during backup both without the change and with the change. The Spikes in Delete, Select, and Update latency
    was much lower for the case of throttling v/s unthrottled create snapshots RPCs.
    For updates, it is 10-20 ms if throttled v/s 100-400 ms if unthrottled. For deletes, it is 10-20 ms v/s 100-200 ms.
    For selects, it is 4-5 ms v/s 30-40 ms.
    
    Reviewers: bogdan, sergei
    
    Reviewed By: sergei
    
    Subscribers: zyu, kannan, ybase
    
    Differential Revision: https://phabricator.dev.yugabyte.com/D13786

--- src/yb/master/snapshot_coordinator_context.h
+++ src/yb/master/snapshot_coordinator_context.h
@@ -77,6 +77,8 @@ class SnapshotCoordinatorContext {
 
   virtual server::Clock* Clock() = 0;
 
+  virtual size_t GetNumLiveTServersForActiveCluster() = 0;
+
   virtual ~SnapshotCoordinatorContext() = default;
 };
 
