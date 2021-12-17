df049bdcf09eb57ff4a828eee055e6df356efb69
Viktor <viktormihailovviksa@gmail.com>
2021-12-17 18:33:50 +0200

    Fixing style errors

--- ent/src/yb/tools/yb-admin_client_ent.cc
+++ ent/src/yb/tools/yb-admin_client_ent.cc
@@ -123,8 +123,9 @@ Status ClusterAdminClient::ListSnapshots(const ListSnapshotsFlags& flags) {
   rapidjson::Value json_snapshots(rapidjson::kArrayType);
   if (!json) {
     if (resp.snapshots_size()) {
-      // Using 2 tabs so that the header can be aligned to the time
-      cout << RightPadToUuidWidth("Snapshot UUID") << kColumnSep << "State" << kColumnSep << kColumnSep << "Creation Time" << endl;
+      // Using 2 tabs so that the header can be aligned to the time.
+      cout << RightPadToUuidWidth("Snapshot UUID") << kColumnSep 
+           << "State" << kColumnSep << kColumnSep << "Creation Time" << endl;
     } else {
       cout << "No snapshots" << endl;
     }
@@ -147,8 +148,9 @@ Status ClusterAdminClient::ListSnapshots(const ListSnapshotsFlags& flags) {
           HybridTimeToString(HybridTime::FromPB(entry.previous_snapshot_hybrid_time())),
           &json_snapshot, &document.GetAllocator());
     } else {
-      cout << SnapshotIdToString(snapshot.id()) << kColumnSep << snapshot.entry().state() << 
-        kColumnSep << HybridTimeToString(HybridTime::FromPB(snapshot.entry().snapshot_hybrid_time())) << endl;
+      cout << SnapshotIdToString(snapshot.id()) << kColumnSep 
+           << snapshot.entry().state() << kColumnSep 
+           << HybridTimeToString(HybridTime::FromPB(snapshot.entry().snapshot_hybrid_time())) << endl;
     }
 
     // Not implemented in json mode.
