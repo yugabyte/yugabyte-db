22f357a73d168e11c4a6e5003a130560f234cf3f
Sean Kim <skim@yugabyte.com>
2021-12-20 03:23:55 -0500

    [PLAT-2555] Platform: Node Comparison Graph Panel component
    Summary:
    This diff contains the Rechart graph components of the node comparison modal
    The parent component is MetricsComparisoGraphPanel component that contains two MetricsComparisonPanel.
    One for each node/group of nodes selected.
    
    It also includes the functionality to fetch the data needed to generate the graphs.
    Two queries are run, one for each node group
    
    {F21100}
    
    Test Plan:
    1. Check if the fetched data is correct
    2. Check that the data is being displayed correctly
    3. Test that the filters in the header work correctly with the graphs
    3.1. Refresh interval should update the data
    3.2. Time filter should update the x-axis time range of the graphs
    
    Reviewers: sshevchenko
    
    Reviewed By: sshevchenko
    
    Subscribers: jenkins-bot, yugaware
    
    Differential Revision: https://phabricator.dev.yugabyte.com/D14431

--- managed/ui/src/components/metrics/MetricsComparisonModal/MetricsComparisonContent.js
+++ managed/ui/src/components/metrics/MetricsComparisonModal/MetricsComparisonContent.js
@@ -1,5 +1,89 @@
-import React from 'react';
+import React, {useCallback, useContext, useEffect, useRef} from 'react';
+import { FilterContext } from './ComparisonFilterContextProvider';
+import { useQuery } from 'react-query';
+import { getQueryMetrics } from '../../../actions/graph';
+import { MetricsComparisonGraphPanel } from './MetricsComparisonGraphPanel';
+import moment from 'moment';
 
 export const MetricsComparisonContent = ({ universe, visible }) => {
-  return <></>;
+  const [state, dispatch] = useContext(FilterContext);
+  const { filters, nodeNameFirst, nodeNameSecond, selectedMetrics, refreshFilters } = state;
+
+  const intervalRef = useRef();
+
+  const stableDispatch = useCallback(dispatch, []);
+
+  const refreshModalGraphQuery = useCallback(() => {
+    const newFilter = {
+      startMoment: moment().subtract(filters.value, filters.type),
+      endMoment: moment()
+    };
+    stableDispatch({
+      type: 'CHANGE_GRAPH_FILTER',
+      payload: { ...newFilter }
+    });
+  }, [filters, stableDispatch]);
+
+  useEffect(() => {
+    if (visible) {
+      refreshModalGraphQuery();
+    } else {
+      dispatch({
+        type: 'RESET_REFRESH_INTERVAL'
+      });
+      clearInterval(intervalRef.current);
+    }
+  }, [visible, dispatch, refreshModalGraphQuery]);
+
+  useEffect(() => {
+    if (refreshFilters.refreshInterval !== 'off') {
+      const id = setInterval(() => {
+        refreshModalGraphQuery();
+      }, refreshFilters.refreshInterval);
+      intervalRef.current = id;
+    }
+    return () => {
+      clearInterval(intervalRef.current);
+    }
+  }, [refreshFilters, refreshModalGraphQuery]);
+
+  const queryParams = {};
+  queryParams.start = filters.startMoment.unix();
+  queryParams.end = filters.endMoment.unix();
+  queryParams.nodePrefix = universe.universeDetails.nodePrefix;
+  if (nodeNameFirst !== 'all') {
+    queryParams.nodeName = nodeNameFirst;
+  }
+  queryParams.metrics = selectedMetrics;
+  queryParams.isRecharts = true;
+
+  useQuery(['getQueryMetrics', queryParams], () => getQueryMetrics(queryParams), {
+    onSuccess: (response) => {
+      dispatch({
+        type: 'CHANGE_METRICS_DATA',
+        payload: { firstMetricsData: response }
+      });
+    }
+  });
+
+  if (nodeNameSecond !== 'all') {
+    queryParams.nodeName = nodeNameSecond;
+  } else {
+    delete queryParams.nodeName;
+  }
+
+  useQuery(['getQueryMetrics', queryParams], () => getQueryMetrics(queryParams), {
+    onSuccess: (response) => {
+      dispatch({
+        type: 'CHANGE_METRICS_DATA',
+        payload: { secondMetricsData: response }
+      });
+    }
+  });
+
+  const graphPanels = selectedMetrics.map((metricsKey) => {
+    return <MetricsComparisonGraphPanel key={metricsKey} metricsKey={metricsKey} />;
+  });
+
+  return <>{graphPanels}</>;
 };
