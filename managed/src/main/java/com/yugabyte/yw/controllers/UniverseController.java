734735aa46af2ac83f348f3424f287ad548c5514
Harish Kandala <kandalaharish95@gmail.com>
2021-12-20 18:05:14 +0530

    [PLAT-2538] Platform: Fix missing yugabytedb.crt when newly created self-signed cert is rotated
    Summary:
    Client certs (yugabytedb.crt) are missing when self-signed certs created from UI are used to rotate.
    When "Create New Certificate" option is selected they are generated but for the above case it was failing.
    Moved the client cert generation logic outside the condition, so that we make sure client certificates are present all the time.
    
    Test Plan: Tested locally by performing self-signed cert rotation
    
    Reviewers: vpatibandla, arnav
    
    Reviewed By: arnav
    
    Subscribers: jenkins-bot, yugaware
    
    Differential Revision: https://phabricator.dev.yugabyte.com/D14435

--- managed/src/main/java/com/yugabyte/yw/controllers/UniverseController.java
+++ managed/src/main/java/com/yugabyte/yw/controllers/UniverseController.java
@@ -1517,24 +1517,13 @@ public class UniverseController extends AuthenticatedController {
         case Certs:
           UserIntent userIntent = universe.getUniverseDetails().getPrimaryCluster().userIntent;
           customerTaskType = CustomerTask.TaskType.UpdateCert;
+          // Create self-signed cert if null value is provided
           if (taskParams.certUUID == null) {
             if (taskParams.createNewSelfSignedRootCA) {
               taskParams.rotateRoot = true;
               taskParams.certUUID =
                   CertificateHelper.createRootCA(
                       taskParams.nodePrefix, customerUUID, appConfig.getString("yb.storage.path"));
-              if (userIntent.enableClientToNodeEncrypt) {
-                CertificateHelper.createClientCertificate(
-                    taskParams.certUUID,
-                    String.format(
-                        CertificateHelper.CERT_PATH,
-                        appConfig.getString("yb.storage.path"),
-                        customerUUID,
-                        taskParams.certUUID.toString()),
-                    CertificateHelper.DEFAULT_CLIENT,
-                    null,
-                    null);
-              }
             } else {
               return ApiResponse.error(
                   BAD_REQUEST, "certUUID is required for taskType: " + taskParams.taskType);
@@ -1555,6 +1544,20 @@ public class UniverseController extends AuthenticatedController {
                   universe.getUniverseDetails().rootCA, taskParams.certUUID)) {
             throw new IllegalArgumentException(
                 "CA certificates cannot be different when rotateRoot set to false.");
+          }
+          // Generate client certs for the provided cert to make sure it exists
+          if (userIntent.enableClientToNodeEncrypt
+              && cert.certType == CertificateInfo.Type.SelfSigned) {
+            CertificateHelper.createClientCertificate(
+                taskParams.certUUID,
+                String.format(
+                    CertificateHelper.CERT_PATH,
+                    appConfig.getString("yb.storage.path"),
+                    customerUUID,
+                    taskParams.certUUID.toString()),
+                CertificateHelper.DEFAULT_CLIENT,
+                null,
+                null);
           }
       }
 
