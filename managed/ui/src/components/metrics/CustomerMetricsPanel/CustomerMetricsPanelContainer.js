968b5229a736acc0c30bfabaf9f535fa60c3e878
Sean Kim <skim@yugabyte.com>
2021-12-20 02:59:44 -0500

    [PLAT-2559] Platform: Node Comparison Modal - Overall Wireframe (UI)
    Summary:
    The basic wireframe component of the node comparison modal feature
    
    This includes
    - <Compare> button in GraphPanelHeader
    - Sass file with styling for the modal
    - Context Provider of the sub-components
    - Feature flag settings
    - Npm Recharts library installation
    
    {F21110}
    
    {F21111}
    
    Test Plan:
    1. Check the <Compare> button is hidden by default, and need to set feature flag in order to enable the feature
    2. The modal only contains the node selectors in this diff, but should open without issues
    
    Reviewers: sshevchenko
    
    Reviewed By: sshevchenko
    
    Subscribers: sshevchenko, jenkins-bot, yugaware
    
    Differential Revision: https://phabricator.dev.yugabyte.com/D14429

--- managed/ui/src/components/metrics/CustomerMetricsPanel/CustomerMetricsPanelContainer.js
+++ managed/ui/src/components/metrics/CustomerMetricsPanel/CustomerMetricsPanelContainer.js
@@ -6,7 +6,9 @@ import { CustomerMetricsPanel } from '../../metrics';
 
 const mapStateToProps = (state) => {
   return {
-    customer: state.customer
+    customer: state.customer,
+    universe: state.universe,
+    visibleModal: state.modal.visibleModal
   };
 };
 
