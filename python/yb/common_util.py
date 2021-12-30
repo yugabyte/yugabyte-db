6d3465d6e5c32e4940a326a6f6e7efb1e87f3dfa
Mikhail Bautin <mbautin@users.noreply.github.com>
2021-12-30 13:03:50 -0800

    [#10920] Support build with Clang 12 and Linuxbrew
    Summary:
    Support a build with Clang 12 and Linuxbrew. This is the same version of Linuxbrew that we have been using with GCC 5. The LLVM toolchain is not built for Linuxbrew but for the native OS, so we specify all libraries and include directories explicitly in order to build with Linuxbrew includes and libraries. The resulting package should have the same portability across target Linux distributions as the package we have been building with GCC 5.
    
    This will allow us to drop support for GCC 5 and use a much newer compiler for our production release package.
    
    Enhancing thirdparty_tool.py to allow using Linuxbrew-based packages built on a different OS, because they should not depend on the Linux distribution.
    
    Relevant PR in yugabyte-db-thirdparty: https://github.com/yugabyte/yugabyte-db-thirdparty/pull/95
    
    Other changes:
    - Removing automatic installation of Linuxbrew into `~/.linuxbrew-yb-build`. Our standard practice is to install it into a path like `/opt/yb-build/brew/linuxbrew-20181203T161736v9`.
    - Remove unused constant SHARED_LINUXBREW_BUILDS_DIR.
    - Do not use Ninja from the Linuxbrew directory.
    - Activate the Python virtual environment early in the build-and-test.sh script used by Jenkins.
    - Add a python_with_venv.sh script to simplify running Python commands with the build virtualenv.
    - Simplify checking for Clang and GCC compiler families in CMake scripts (IS_CLANG and IS_GCC variables).
    - Add type annotations dependency_graph.py and refactor it. Add it to codecheck.ini.
    - Add a Python function arg_str_to_bool useful for creating user-friendly boolean arguments using argparse.
    
    Test Plan:
    Jenkins
    
    Run the build in various configurations on CentOS 7, AlmaLinux 8, macOS, across x86_64 and ARM architectures.
    
    Reviewers: steve.varnau, sergei
    
    Reviewed By: sergei
    
    Subscribers: ybase
    
    Differential Revision: https://phabricator.dev.yugabyte.com/D14487

--- python/yb/common_util.py
+++ python/yb/common_util.py
@@ -24,6 +24,7 @@ import subprocess
 import shlex
 import io
 import platform
+import argparse
 
 import typing
 from typing import (
@@ -70,7 +71,7 @@ def set_thirdparty_dir(thirdparty_dir: str) -> None:
 
 
 def sorted_grouped_by(
-        arr: List[_GroupElementType],
+        arr: Iterable[_GroupElementType],
         key_fn: Callable[[_GroupElementType], _GroupKeyType]
         ) -> List[Tuple[_GroupKeyType, List[_GroupElementType]]]:
     """
@@ -82,7 +83,7 @@ def sorted_grouped_by(
 
 
 def group_by(
-        arr: List[_GroupElementType],
+        arr: Iterable[_GroupElementType],
         key_fn: Callable[[_GroupElementType], _GroupKeyType]
         ) -> Dict[_GroupKeyType, List[_GroupElementType]]:
     """
@@ -227,6 +228,13 @@ def get_yb_src_root_from_build_root(
         if must_succeed:
             raise RuntimeError(error_msg)
 
+    return yb_src_root
+
+
+def ensure_yb_src_root_from_build_root(build_dir: str, verbose: bool = False) -> str:
+    yb_src_root = get_yb_src_root_from_build_root(
+        build_dir=build_dir, verbose=verbose, must_succeed=True)
+    assert yb_src_root is not None
     return yb_src_root
 
 
@@ -379,6 +387,15 @@ def write_yaml_file(content: Any, output_file_path: str) -> None:
         yaml.dump(content, output_file)
 
 
+def write_file(content: str, output_file_path: str) -> None:
+    with open(output_file_path, 'w') as output_file:
+        output_file.write(content)
+
+
+def make_parent_dir(path: str) -> None:
+    os.makedirs(os.path.dirname(os.path.abspath(path)), exist_ok=True)
+
+
 def to_yaml_str(content: Any) -> str:
     out = io.StringIO()
     yaml = get_ruamel_yaml_instance()
@@ -403,3 +420,15 @@ def check_arch() -> None:
 
 def is_macos_arm64() -> bool:
     return is_macos() and get_target_arch() == 'arm64'
+
+
+# From https://stackoverflow.com/questions/15008758/parsing-boolean-values-with-argparse
+# To be used for boolean arguments.
+def arg_str_to_bool(v: Any) -> bool:
+    if isinstance(v, bool):
+        return v
+    if v.lower() in ('yes', 'true', 't', 'y', '1'):
+        return True
+    if v.lower() in ('no', 'false', 'f', 'n', '0'):
+        return False
+    raise argparse.ArgumentTypeError('Boolean value expected, got: %s' % v)
