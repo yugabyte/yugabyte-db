5b9e66c7d8c9130ccb9d9e1899de25f01bb1567a
AAYUSH CHAUHAN <achauhan@yugabyte.com>
2022-01-13 13:18:35 +0000

    [Backport 2.11.1] [PLAT-2749] YSQL_DUMP based backup-create and backup-restore
    Summary:
    The PG based backup mode can be enabled via:
    --pg_based_backup option (adding it as a runtime config in platform)
    --detect_enums (=True by default for now) if the ENUM type is found.
    
    Flow for backup-create:
       # Create YSQLDump for schema only.
       # Create YSQLDump_data, containing data only.
       # Upload both YSQLDump and YSQLDump_data to backup location.
    
    Flow for backup-restore:
      # Apply YSQLDump to create tables, types, etc. (supports DB renaming, sed-expr applying)
      # Apply YSQLDump_data (only for loading data into created DB, all objects have been created in previous steps.)
    
    **Note**:
      # Snapshots don't play any role in pg based backup, hence snapshot creating/exporting/importing/restoring/deleting operations are not applicable.
      # These changes doesn't affect YCQL/Redis backup.
    
    Original commit: [c6e3f9](https://github.com/yugabyte/yugabyte-db/commit/c6e3f958120e55c71b7a6a39e0ef7a181ec502fd)
    
    Original Diff: https://phabricator.dev.yugabyte.com/D14747
    
    Test Plan:
    Jenkins: rebase: 2.11.1
    Manual testing.
    
    Added C++ backup test in yb-backup-test_ent: TestYSQLBackupWithEnum, TestYSQLPgBasedBackup
    
    Generic backup tests:
    ybd --cxx-test tools_yb-backup-test_ent
    
    ybd --java-test org.yb.pgsql.TestYbBackup --tp 1
    ybd --java-test org.yb.cql.TestYbBackup --tp 1
    ybd --java-test org.yb.cql.ParameterizedTestYbBackup --tp 1
    dev-itest-pipeline run: https://jenkins.dev.yugabyte.com/job/dev-itest-pipeline/585/
    
    Reviewers: spotachev, oleg
    
    Reviewed By: spotachev, oleg
    
    Subscribers: yugaware, yql, ybase, jenkins-bot, spotachev
    
    Differential Revision: https://phabricator.dev.yugabyte.com/D14853

--- managed/src/test/java/com/yugabyte/yw/common/TableManagerTest.java
+++ managed/src/test/java/com/yugabyte/yw/common/TableManagerTest.java
@@ -11,11 +11,14 @@ import static org.junit.Assert.assertNull;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
+import static org.mockito.ArgumentMatchers.any;
 
 import com.yugabyte.yw.forms.BackupTableParams;
 import com.yugabyte.yw.forms.BulkImportParams;
 import com.yugabyte.yw.forms.UniverseDefinitionTaskParams;
 import com.yugabyte.yw.forms.UniverseDefinitionTaskParams.UserIntent;
+import com.typesafe.config.Config;
+import com.yugabyte.yw.common.config.RuntimeConfigFactory;
 import com.yugabyte.yw.models.AccessKey;
 import com.yugabyte.yw.models.AvailabilityZone;
 import com.yugabyte.yw.models.Backup;
@@ -54,6 +57,9 @@ public class TableManagerTest extends FakeDBApplication {
   @Mock ReleaseManager releaseManager;
 
   @InjectMocks TableManager tableManager;
+
+  @Mock RuntimeConfigFactory mockruntimeConfigFactory;
+  @Mock Config mockConfigUniverseScope;
 
   private Provider testProvider;
   private Customer testCustomer;
@@ -288,6 +294,8 @@ public class TableManagerTest extends FakeDBApplication {
     ReleaseManager.ReleaseMetadata metadata = new ReleaseManager.ReleaseMetadata();
     metadata.filePath = "/yb/release.tar.gz";
     when(releaseManager.getReleaseByVersion("0.0.1")).thenReturn(metadata);
+    when(mockruntimeConfigFactory.forUniverse(any())).thenReturn(mockConfigUniverseScope);
+    when(mockConfigUniverseScope.getBoolean("yb.backup.pg_based")).thenReturn(false);
   }
 
   @Test
