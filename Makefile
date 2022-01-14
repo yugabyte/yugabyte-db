8d5d2f4aa4ad8702cca0e09b6bc3c609e45ac413
Dehowe Feng <dehowefeng@gmail.com>
2022-01-13 16:25:06 -0800

    Update AGE from 0.6.0 to 0.7.0
    Updated the Makefile, the README, the default version in age.control,
    added released notes and updated the age--*.sql filename.

--- Makefile
+++ Makefile
@@ -61,7 +61,7 @@ OBJS = src/backend/age.o \
 
 EXTENSION = age
 
-DATA = age--0.6.0.sql
+DATA = age--0.7.0.sql
 
 # sorted in dependency order
 REGRESS = scan \
--- doc/conf.py
+++ doc/conf.py
@@ -43,7 +43,7 @@ author = u'Apache AGE (incubating) Team'
 # The short X.Y version
 version = u''
 # The full version, including alpha/beta/rc tags
-release = u'0.6.0'
+release = u'0.7.0'
 
 
 # -- General configuration ---------------------------------------------------
