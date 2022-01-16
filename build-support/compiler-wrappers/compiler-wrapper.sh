yugabyte/yugabyte-db

commit 662aff140c115ba3bef4e758fb6c8e574ae71de3

[#10942, #11033, #11034, #10964] Enable link-time optimization with Clang 12
Summary:
Link-time optimization ( https://llvm.org/docs/LinkTimeOptimization.html ) allows for more aggressive optimizations, including inlining, compared to the shared library based model that we currently use.

This diff enables link-time optimization for the Clang 12 Linuxbrew-based release build for the yb-tserver executable only, producing a binary that statically links all object files needed by yb-tserver, including those that are included in the yb_pgbackend library. Third-party libraries are being linked statically but they are not LTO-enabled yet.

The linking of the final LTO-enabled binary is currently being done outside of the CMake build system, using the dependency_graph.py tool that can access the dependency graph of targets and object files, and therefore has all the information needed to construct the linker command line. This also gives us more flexibility customizing the linker command line compared to attempts to do this in the CMake build system. Moving this linking step to CMake may be a future project.

Refactored the dependency_graph.py script into multiple modules: dependency_graph.py, dep_graph_common.py, source_files.py, as well as lto.py (with the new LTO logic).

Also refactored master_main.cc and tablet_server_main.cc and extracted common initialization code to tserver/server_main_util.cc. It is in the tserver directory because the master code currently uses the tserver code.

For building LTO-enabled binaries, we need to use LLVM's lld linker. It has issues with our distributed compilation framework ( https://github.com/yugabyte/yugabyte-db/issues/11034 ). Fixing this by always running LLD-enabled linking commands locally and not on a remote build worker.

Various static initialization issues were identified as fixed as part of this work. If not fixed, these would result in the yb-tserver binary crashing immediately with a core dump.

build-support/compiler-wrappers/compiler-wrapper.sh
@@ -287,6 +287,10 @@ has_yb_c_files=false
 compiler_args_no_output=()
 analyzer_checkers_specified=false
 
+linking=false
+use_lld=false
+lld_linking=false
+
 while [[ $# -gt 0 ]]; do
   is_output_arg=false
   case "$1" in
@@ -335,6 +339,9 @@ while [[ $# -gt 0 ]]; do
     -analyzer-checker=*)
       analyzer_checkers_specified=true
     ;;
+    -fuse-ld=lld)
+      use_lld=true
+    ;;
   esac
   if ! "$is_output_arg"; then
     compiler_args_no_output+=( "$1" )
@@ -342,9 +349,21 @@ while [[ $# -gt 0 ]]; do
   shift
 done
 
-if [[ $output_file != *.o && ${#library_files[@]} -gt 0 ]]; then
-  input_files+=( "${library_files[@]}" )
-  library_files=()
+if [[ $output_file == *.o ]]; then
+  # Compiling.
+  linking=false
+else
+  linking=true
+fi
+
+if [[ $linking == "true" && $use_lld == "true" ]]; then
+  lld_linking=true
+fi
+
+if [[ $YB_COMPILER_TYPE == clang* && $output_file == "jsonpath_gram.o" ]]; then
+  # To avoid this error:
+  # https://gist.github.com/mbautin/b943fb426bfead7388dde17ddb1b0fa7
+  compiler_args+=( -Wno-error=implicit-fallthrough )
 fi
 
 # -------------------------------------------------------------------------------------------------
@@ -362,9 +381,12 @@ if [[ $HOSTNAME == build-worker* ]]; then
   is_build_worker=true
 fi
 
+# If linking with LLVM's lld linker, do it locally.
+# See https://github.com/yugabyte/yugabyte-db/issues/11034 for more details.
 if [[ $local_build_only == "false" &&
       ${YB_REMOTE_COMPILATION:-} == "1" &&
-      $is_build_worker == "false" ]]; then
+      $is_build_worker == "false" &&
+      $lld_linking == "false" ]]; then
 
   trap remote_build_exit_handler EXIT
 
@@ -551,6 +573,12 @@ run_compiler_and_save_stderr() {
 
 # -------------------------------------------------------------------------------------------------
 # Local build
+
+if [[ $output_file =~ libyb_pgbackend*  ]]; then
+  # We record the linker command used for the libyb_pgbackend library so we can use it when
+  # producing the LTO build for yb-tserver.
+  echo "${compiler_args[*]}" >"link_cmd_${output_file}.txt"
+fi
 
 if [[ ${build_type:-} == "asan" &&
       $PWD == */postgres_build/src/backend/utils/adt &&

CMakeLists.txt
@@ -35,7 +35,6 @@ cmake_minimum_required(VERSION 3.7.0)
 
 project(YugabyteDB)
 
-
 if("$CACHE{YB_PCH_ON}" STREQUAL "")
   if("$ENV{YB_USE_PCH}" STREQUAL "1")
     set(YB_USE_PCH ON)
@@ -102,17 +101,31 @@ set(YB_ALL_DEPS "" CACHE INTERNAL "All dependencies" FORCE)
 # This is used to let the add_executable wrapper know if we're adding a test.
 set(YB_ADDING_TEST_EXECUTABLE "FALSE" CACHE INTERNAL "" FORCE)
 
-if ("$ENV{YB_DO_NOT_BUILD_TESTS}" STREQUAL "1")
-  message("Skipping building tests (YB_DO_NOT_BUILD_TESTS environment variable is set to 1)")
-  set(BUILD_TESTS OFF)
-else()
+if(NOT "$ENV{YB_DO_NOT_BUILD_TESTS}" STREQUAL "")
+  if("$ENV{YB_DO_NOT_BUILD_TESTS}" STREQUAL "1")
+    message("YB_DO_NOT_BUILD_TESTS is set to 1, will not build tests")
+    set(BUILD_TESTS OFF)
+  elseif("$ENV{YB_DO_NOT_BUILD_TESTS}" STREQUAL "0")
+    message("YB_DO_NOT_BUILD_TESTS is set to 0, will build tests")
+    set(BUILD_TESTS ON)
+  else()
+    message(FATAL_ERROR
+            "Invalid value of the YB_DO_NOT_BUILD_TESTS environment variable, expected 0 or 1 but "
+            "got '$ENV{YB_DO_NOT_BUILD_TESTS}'.")
+  endif()
+  set(BUILD_TESTS ${BUILD_TESTS} CACHE BOOL "Whether to build tests")
+elseif("$CACHE{BUILD_TESTS}" STREQUAL "")
   set(BUILD_TESTS ON)
+  message("Will build tests by default")
+else()
+  message("BUILD_TESTS from cache: ${BUILD_TESTS} (set YB_DO_NOT_BUILD_TESTS to 0 or 1 to change)")
 endif()
 
 parse_build_root_basename()
 if("${YB_BUILD_TYPE}" STREQUAL "")
   message(FATAL_ERROR "YB_BUILD_TYPE still not set after parse_build_root_basename")
 endif()
+
 message("YB_BUILD_TYPE: ${YB_BUILD_TYPE}")
 message("CMAKE_MAKE_PROGRAM: ${CMAKE_MAKE_PROGRAM}")
 
@@ -244,6 +257,7 @@ elseif (IS_GCC)
 else()
   message(FATAL_ERROR "Unknown compiler family: '${COMPILER_FAMILY}'.")
 endif()
+
 message("THIRDPARTY_INSTRUMENTATION_TYPE=${THIRDPARTY_INSTRUMENTATION_TYPE}")
 
 # Make sure third-party dependency is up-to-date.
@@ -301,6 +315,7 @@ endif()
 
 VALIDATE_COMPILER_TYPE()
 DETECT_BREW()
+enable_lto_if_needed()
 
 message("Using COMPILER_FAMILY=${COMPILER_FAMILY}")
 

build-support/common-build-env.sh
@@ -292,6 +292,11 @@ if [[ -n ${YB_LINUXBREW_DIR:-} ]]; then
   yb_linuxbrew_dir_origin=" (from environment)"
 fi
 
+yb_llvm_toolchain_url_origin=""
+if [[ -n ${YB_LLVM_TOOLCHAIN_URL:-} ]]; then
+  yb_llvm_toolchain_url_origin=" (from environment)"
+fi
+
 yb_llvm_toolchain_dir_origin=""
 if [[ -n ${YB_LLVM_TOOLCHAIN_DIR:-} ]]; then
   yb_llvm_toolchain_dir_origin=" (from environment)"
@@ -1146,7 +1151,9 @@ save_var_to_file_in_build_dir() {
     if [[ ! -d $BUILD_ROOT ]]; then
       mkdir -p "$BUILD_ROOT"
     fi
-    echo "$value" >"$BUILD_ROOT/$file_name"
+    if ! echo "$value" >"$BUILD_ROOT/$file_name"; then
+      fatal "Could not save value '$value' to file '$BUILD_ROOT/$file_name'"
+    fi
   fi
 }
 
@@ -1217,14 +1224,16 @@ download_thirdparty() {
 
 download_toolchain() {
   local toolchain_urls=()
-  if [[ -n ${YB_THIRDPARTY_URL:-} && ${YB_THIRDPARTY_URL##*/} == *linuxbrew* ]]; then
-    # TODO: get rid of this and always include linuxbrew_url.txt in the thirdparty archives that are
-    # built for Linuxbrew.
-    toolchain_urls+=( "https://github.com/yugabyte/brew-build/releases/download/\
-20181203T161736v9/linuxbrew-20181203T161736v9.tar.gz" )
-    if [[ -f $BUILD_ROOT/llvm_url.txt ]]; then
-      toolchain_urls+=( "$(<"$BUILD_ROOT/llvm_url.txt")" )
-    fi
+  local linuxbrew_url=""
+  if [[ -n ${YB_THIRDPARTY_DIR:-} && -f "$YB_THIRDPARTY_DIR/linuxbrew_url.txt" ]]; then
+    local linuxbrew_url_file_path="${YB_THIRDPARTY_DIR}/linuxbrew_url.txt"
+    linuxbrew_url="$(<"${linuxbrew_url_file_path}")"
+  elif [[ -n ${YB_THIRDPARTY_URL:-} && ${YB_THIRDPARTY_URL##*/} == *linuxbrew* ||
+          -n ${YB_THIRDPARTY_DIR:-} && ${YB_THIRDPARTY_DIR##*/} == *linuxbrew* ]]; then
+    # TODO: get rid of the hard-coded URL below and always include linuxbrew_url.txt in the
+    # thirdparty archives that are built for Linuxbrew.
+    local linuxbrew_url="https://github.com/yugabyte/brew-build/releases/download/"
+    linuxbrew_url+="20181203T161736v9/linuxbrew-20181203T161736v9.tar.gz"
   else
     for file_name_part in linuxbrew toolchain; do
       local url_file_path="$YB_THIRDPARTY_DIR/${file_name_part}_url.txt"
@@ -1234,6 +1243,14 @@ download_toolchain() {
       fi
     done
   fi
+
+  if [[ -n ${linuxbrew_url:-} ]]; then
+    toolchain_urls+=( "$linuxbrew_url" )
+  fi
+  if [[ -n ${YB_LLVM_TOOLCHAIN_URL:-} ]]; then
+    toolchain_urls+=( "${YB_LLVM_TOOLCHAIN_URL}" )
+  fi
+
   if [[ ${#toolchain_urls[@]} -eq 0 ]]; then
     return
   fi
@@ -1250,7 +1267,7 @@ download_toolchain() {
       is_linuxbrew=true
     else
       fatal "Unable to determine the installation parent directory for the toolchain archive" \
-            "named '$toolchain_url_basename'. Toolchain URL: '$toolchain_url'."
+            "named '$toolchain_url_basename'. Toolchain URL: '${toolchain_url}'."
     fi
 
     download_and_extract_archive "$toolchain_url" "$toolchain_dir_parent"
@@ -1890,6 +1907,9 @@ log_thirdparty_and_toolchain_details() {
     if is_linux && [[ -n ${YB_LINUXBREW_DIR:-} ]]; then
       echo "    YB_LINUXBREW_DIR: $YB_LINUXBREW_DIR$yb_linuxbrew_dir_origin"
     fi
+    if [[ -n ${YB_LLVM_TOOLCHAIN_URL:-} ]]; then
+      echo "    YB_LLVM_TOOLCHAIN_URL: $YB_LLVM_TOOLCHAIN_URL$yb_llvm_toolchain_url_origin"
+    fi
     if [[ -n ${YB_LLVM_TOOLCHAIN_DIR:-} ]]; then
       echo "    YB_LLVM_TOOLCHAIN_DIR: $YB_LLVM_TOOLCHAIN_DIR$yb_llvm_toolchain_dir_origin"
     fi
@@ -2437,8 +2457,16 @@ set_prebuilt_thirdparty_url() {
         fatal "Could not automatically determine the third-party archive URL to download."
       fi
       log "Setting third-party URL to $YB_THIRDPARTY_URL"
-
       save_var_to_file_in_build_dir "$YB_THIRDPARTY_URL" thirdparty_url.txt
+
+      if [[ -f $llvm_url_file_path ]]; then
+        YB_LLVM_TOOLCHAIN_URL=$(<"$llvm_url_file_path")
+        export YB_LLVM_TOOLCHAIN_URL
+        yb_llvm_toolchain_url_origin=" (determined automatically based on the OS and compiler type)"
+        log "Setting LLVM toolchain URL to $YB_LLVM_TOOLCHAIN_URL"
+        save_var_to_file_in_build_dir "$YB_LLVM_TOOLCHAIN_URL" llvm_url.txt
+      fi
+
     else
       log "YB_THIRDPARTY_URL is already set to '$YB_THIRDPARTY_URL', not trying to set it" \
           "automatically."
@@ -2551,6 +2579,10 @@ is_apple_silicon() {
   fi
 
   return 1
+}
+
+should_use_lto() {
+  using_linuxbrew && [[ "${YB_COMPILER_TYPE}" == "clang12" && "${build_type}" == "release" ]]
 }
 
 # -------------------------------------------------------------------------------------------------

build-support/jenkins/build-and-test.sh
@@ -725,6 +725,22 @@ export YB_GZIP_PER_TEST_METHOD_LOGS=1
 export YB_GZIP_TEST_LOGS=1
 export YB_DELETE_SUCCESSFUL_PER_TEST_METHOD_LOGS=1
 
+if should_use_lto; then
+  log "Using LTO"
+  (
+    set -x
+    "$YB_SRC_ROOT/python/yb/dependency_graph.py" \
+        --build-root "$BUILD_ROOT" \
+        --file-regex "^.*/yb-tserver$" \
+        --lto-output-suffix="" \
+        link-whole-program
+    ls -l "$BUILD_ROOT/bin/yb-tserver"
+    ldd "$BUILD_ROOT/bin/yb-tserver"
+  )
+else
+  log "Not using LTO: YB_COMPILER_TYPE=${YB_COMPILER_TYPE}, build_type=${build_type}"
+fi
+
 if [[ $YB_COMPILE_ONLY != "1" ]]; then
   if spark_available; then
     if [[ $YB_BUILD_CPP == "1" || $YB_BUILD_JAVA == "1" ]]; then
@@ -740,11 +756,14 @@ if [[ $YB_COMPILE_ONLY != "1" ]]; then
         test_conf_path="$BUILD_ROOT/test_conf.json"
         # YB_GIT_COMMIT_FOR_DETECTING_TESTS allows overriding the commit to use to detect the set
         # of tests to run. Useful when testing this script.
-        "$YB_SRC_ROOT/python/yb/dependency_graph.py" \
-            --build-root "$BUILD_ROOT" \
-            --git-commit "${YB_GIT_COMMIT_FOR_DETECTING_TESTS:-$current_git_commit}" \
-            --output-test-config "$test_conf_path" \
-            affected
+        (
+          set -x
+          "$YB_SRC_ROOT/python/yb/dependency_graph.py" \
+              --build-root "$BUILD_ROOT" \
+              --git-commit "${YB_GIT_COMMIT_FOR_DETECTING_TESTS:-$current_git_commit}" \
+              --output-test-config "$test_conf_path" \
+              affected
+        )
         run_tests_extra_args+=( "--test_conf" "$test_conf_path" )
         unset test_conf_path
       fi

build-support/tserver_lto.sh
@@ -1,0 +1,13 @@
+#!/usr/bin/env bash
+
+. "${BASH_SOURCE%/*}/common-build-env.sh"
+
+activate_virtualenv
+set_pythonpath
+
+set -x
+"$YB_SRC_ROOT/python/yb/dependency_graph.py" \
+    --build-root "$YB_SRC_ROOT/build/release-clang12-linuxbrew-dynamic-ninja" \
+    --file-regex "^.*/yb-tserver$" \
+    link-whole-program \
+    "$@"

build-support/yb_release.py
@@ -47,7 +47,7 @@ def main():
                              'false if --build_target is specified, true otherwise.')
     parser.add_argument('--destination', help='Copy release to Destination folder.')
     parser.add_argument('--force', help='Skip prompts', action='store_true')
-    parser.add_argument('--commit', help='Custom specify a git commit.')
+    parser.add_argument('--commit', help='Specifies a custom git commit to use in archive name.')
     parser.add_argument('--skip_build', help='Skip building the code', action='store_true')
     parser.add_argument('--build_target',
                         help='Target directory to put the YugaByte distribution into. This can '
@@ -221,8 +221,13 @@ def main():
     # This points to the release manifest within the release_manager, and we are modifying that
     # directly.
     release_util = ReleaseUtil(
-        YB_SRC_ROOT, build_type, build_target, args.force, args.commit, build_root,
-        args.package_name)
+        repository=YB_SRC_ROOT,
+        build_type=build_type,
+        distribution_path=build_target,
+        force=args.force,
+        commit=args.commit,
+        build_root=build_root,
+        package_name=args.package_name)
 
     system = platform.system().lower()
     library_packager_args = dict(

cmake_modules/YugabyteFunctions.cmake
@@ -729,3 +729,30 @@ function(parse_build_root_basename)
         "but CMAKE_SYSTEM_PROCESSOR is ${CMAKE_SYSTEM_PROCESSOR}")
   endif()
 endfunction()
+
+macro(enable_lto_if_needed)
+  if(NOT DEFINED COMPILER_FAMILY)
+    message(FATAL_ERROR "COMPILER_FAMILY not defined")
+  endif()
+  if(NOT DEFINED USING_LINUXBREW)
+    message(FATAL_ERROR "USING_LINUXBREW not defined")
+  endif()
+  if(NOT DEFINED YB_BUILD_TYPE)
+    message(FATAL_ERROR "YB_BUILD_TYPE not defined")
+  endif()
+
+  if("${YB_BUILD_TYPE}" STREQUAL "release" AND
+     "${COMPILER_FAMILY}" STREQUAL "clang" AND
+     "${YB_COMPILER_TYPE}" STREQUAL "clang12" AND
+     USING_LINUXBREW AND
+     NOT APPLE)
+    message("Enabling full LTO and lld linker")
+    ADD_CXX_FLAGS("-flto=full -fuse-ld=lld")
+  else()
+    message("Not enabling LTO: "
+            "YB_BUILD_TYPE=${YB_BUILD_TYPE}, "
+            "COMPILER_FAMILY=${COMPILER_FAMILY}, "
+            "USING_LINUXBREW=${USING_LINUXBREW}, "
+            "APPLE=${APPLE}")
+  endif()
+endmacro()

codecheck.ini
@@ -38,11 +38,15 @@ included_regex_list =
     ^python/yb/command_util[.]py$
     ^python/yb/common_util[.]py$
     ^python/yb/compile_commands[.]py$
+    ^python/yb/dep_graph_common[.]py$
     ^python/yb/dependency_graph[.]py$
     ^python/yb/library_packager[.]py$
     ^python/yb/linuxbrew[.]py$
+    ^python/yb/lto[.]py$
     ^python/yb/os_detection[.]py$
+    ^python/yb/release_util[.]py$
     ^python/yb/run_pvs_studio_analyzer[.]py$
+    ^python/yb/source_files[.]py$
     ^python/yb/thirdparty_tool[.]py$
     ^python/yb/tool_base[.]py$
     ^python/yb/yb_dist_tests[.]py$

commit 34e81fafa7af4fc2fbb8910f63ff875d59f5d88d

Fix shellcheck in compiler-wrapper.sh
Summary: Fix a shellcheck warning in compiler-wrapper.sh, broken by commit 662aff140c115ba3bef4e758fb6c8e574ae71de3.

Test Plan:
./yb_build.sh --shellcheck
Jenkins: skip

Reviewers: sergei

Reviewed By: sergei

Subscribers: ybase

Differential Revision: https://phabricator.dev.yugabyte.com/D14783

build-support/compiler-wrappers/compiler-wrapper.sh
@@ -276,7 +276,6 @@ fi
 
 output_file=""
 input_files=()
-library_files=()
 compiling_pch=false
 yb_pch=false
 