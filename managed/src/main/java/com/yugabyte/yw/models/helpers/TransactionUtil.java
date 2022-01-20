221349fc9bb31e012eae229bd29fb979c53f03d9
Naorem Khogendro Singh <nsingh@yugabyte.com>
2022-01-19 22:07:33 -0800

    [PLAT-2741][Platform] Multiple iTest failures due to "Error[ERROR: could not serialize access due to read/write dependencies among transactions Detail"
    Summary:
    If either transaction were running at the Repeatable Read isolation level, both would be allowed to commit; but since there is no serial order of execution consistent with the result, using Serializable transactions will allow one transaction to commit and will roll the other back with this message:
    ERROR:  could not serialize access due to read/write dependencies among transactions
    
    Test Plan: Itest passed https://jenkins.dev.yugabyte.com/view/Dev/job/dev-itest-pipeline/577/ and manual run passed.
    
    Reviewers: nbhatia, amalyshev, sb-yb
    
    Reviewed By: amalyshev, sb-yb
    
    Subscribers: jenkins-bot, yugaware
    
    Differential Revision: https://phabricator.dev.yugabyte.com/D14870

--- managed/src/main/java/com/yugabyte/yw/models/helpers/TransactionUtil.java
+++ managed/src/main/java/com/yugabyte/yw/models/helpers/TransactionUtil.java
@@ -22,14 +22,15 @@ public final class TransactionUtil {
 
   private TransactionUtil() {}
 
+  // AttemptCount overrides the time and resets the back-off time when the max elapsed time is hit.
   public static final RetryConfig DEFAULT_RETRY_CONFIG =
       RetryConfig.builder()
-          .maxAttemptCount(5)
+          .maxAttemptCount(50)
           .backOff(
               new ExponentialBackOff.Builder()
-                  .setInitialIntervalMillis(100)
-                  .setMaxElapsedTimeMillis(5000)
-                  .setMaxIntervalMillis(1000)
+                  .setInitialIntervalMillis(10)
+                  .setMaxElapsedTimeMillis(1000)
+                  .setMaxIntervalMillis(100)
                   .setMultiplier(1.2)
                   .build())
           .build();
@@ -45,7 +46,13 @@ public final class TransactionUtil {
   private static boolean isRetryable(PersistenceException e) {
     String errMsg = e.getMessage();
     if (errMsg != null) {
-      return errMsg.contains("could not serialize access due to concurrent update");
+      if (errMsg.contains("could not serialize access due to concurrent update")) {
+        return true;
+      }
+      if (errMsg.contains(
+          "could not serialize access due to read/write dependencies among transactions")) {
+        return true;
+      }
     }
     return false;
   }
