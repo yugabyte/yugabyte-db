fc932b034362de60eb5582d2b94637405c10590a
Naorem Khogendro Singh <nsingh@yugabyte.com>
2022-01-21 10:37:57 -0800

    [BACKPORT 2.11.2][PLAT-2705][Platform] Platform restart can leave some tasks in incomplete stuck state
    Summary:
    Some restarts (mostly with pods) can leave customer tasks in incomplete state and universe in updateInProgress state forever.
    
    [BACKPORT 2.11.2][PLAT-2664][Platform] {"updateInProgress" : " true"} fails to reset on task failure
    
    Unlock universe on platform restart. The following additional tasks are identified to lock the Universe. Target types like Cluster, Table return true on isUniverseTarget().
    
            CustomerTask.TargetType.Backup,
            CustomerTask.TaskType.Create,
    
            CustomerTask.TargetType.Backup,
            CustomerTask.TaskType.Delete,
    
            CustomerTask.TargetType.Backup,
            CustomerTask.TaskType.Restore,
    
            CustomerTask.TargetType.Node,
            nodeAction.getCustomerTask(),
    
            CustomerTask.TargetType.Table,
            CustomerTask.TaskType.BulkImportData,
    
            CustomerTask.TargetType.Table,
            CustomerTask.TaskType.Create,
    
            CustomerTask.TargetType.Table,
            CustomerTask.TaskType.Delete,
    
            TargetType.Universe,
            CustomerTask.TaskType.SyncXClusterConfig,
    
            CustomerTask.TargetType.Cluster,
            CustomerTask.TaskType.Delete,
    
            CustomerTask.TargetType.Cluster,
            CustomerTask.TaskType.Create
    
    Test Plan:
    Unit tested and ran manual tests by stopping platform.
    
    Unit tests + manually verified.
    
    Reviewers: spotachev, sb-yb, amalyshev
    
    Reviewed By: amalyshev
    
    Subscribers: yugaware, jenkins-bot
    
    Differential Revision: https://phabricator.dev.yugabyte.com/D14962

--- managed/src/main/java/com/yugabyte/yw/common/CustomerTaskManager.java
+++ managed/src/main/java/com/yugabyte/yw/common/CustomerTaskManager.java
@@ -14,7 +14,9 @@ import com.yugabyte.yw.models.TaskInfo;
 import com.yugabyte.yw.models.Universe;
 import io.ebean.Ebean;
 import java.util.List;
+import java.util.Objects;
 import java.util.UUID;
+import java.util.stream.Collectors;
 import javax.inject.Singleton;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -24,68 +26,84 @@ public class CustomerTaskManager {
 
   public static final Logger LOG = LoggerFactory.getLogger(CustomerTaskManager.class);
 
+  private void setTaskError(TaskInfo taskInfo) {
+    taskInfo.setTaskState(TaskInfo.State.Failure);
+    JsonNode jsonNode = taskInfo.getTaskDetails();
+    if (jsonNode instanceof ObjectNode) {
+      ObjectNode details = (ObjectNode) jsonNode;
+      JsonNode errNode = details.get("errorString");
+      if (errNode == null || errNode.isNull()) {
+        details.put("errorString", "Platform restarted.");
+      }
+    }
+  }
+
   public void failPendingTask(CustomerTask customerTask, TaskInfo taskInfo) {
     try {
-      // Mark each subtask as a failure
+      // Mark each subtask as a failure if it is not completed.
       taskInfo
           .getIncompleteSubTasks()
           .forEach(
               subtask -> {
-                subtask.setTaskState(TaskInfo.State.Failure);
+                setTaskError(subtask);
                 subtask.save();
               });
-      // Mark task as a failure
-      taskInfo.setTaskState(TaskInfo.State.Failure);
-      JsonNode jsonNode = taskInfo.getTaskDetails();
-      if (jsonNode != null && jsonNode instanceof ObjectNode) {
-        ObjectNode details = (ObjectNode) jsonNode;
-        details.put("errorString", "Platform restarted.");
+
+      // Use isUniverseTarget() instead of directly comparing with Universe type because some
+      // targets like Cluster, Node are Universe targets.
+      boolean unlockUniverse = customerTask.getTarget().isUniverseTarget();
+      if (customerTask.getTarget().equals(TargetType.Backup)) {
+        // Backup is not universe target.
+        TaskType type = customerTask.getType();
+        if (TaskType.Create.equals(type)) {
+          // Make transition state false for inProgress backups
+          UUID taskUUID = taskInfo.getTaskUUID();
+          List<Backup> backupList = Backup.fetchAllBackupsByTaskUUID(taskUUID);
+          backupList
+              .stream()
+              .filter(backup -> backup.state.equals(Backup.BackupState.InProgress))
+              .forEach(backup -> backup.transitionState(Backup.BackupState.Failed));
+          unlockUniverse = true;
+        } else if (TaskType.Delete.equals(type)) {
+          // NOOP because Delete does not lock Universe.
+        } else if (TaskType.Restore.equals(type)) {
+          // Restore only locks the Universe but does not set backupInProgress flag.
+          unlockUniverse = true;
+        }
       }
-      taskInfo.save();
-      // Mark customer task as completed
+
+      if (unlockUniverse) {
+        // Unlock the universe for future operations.
+        Universe.maybeGet(customerTask.getTargetUUID())
+            .ifPresent(
+                u -> {
+                  UniverseDefinitionTaskParams details = u.getUniverseDetails();
+                  if (details.backupInProgress || details.updateInProgress) {
+                    // Create the update lambda.
+                    Universe.UniverseUpdater updater =
+                        universe -> {
+                          UniverseDefinitionTaskParams universeDetails =
+                              universe.getUniverseDetails();
+                          universeDetails.backupInProgress = false;
+                          universeDetails.updateInProgress = false;
+                          universe.setUniverseDetails(universeDetails);
+                        };
+
+                    Universe.saveDetails(customerTask.getTargetUUID(), updater, false);
+                    LOG.debug("Unlocked universe {}.", customerTask.getTargetUUID());
+                  }
+                });
+      }
+
+      // Mark task as a failure after the universe is unlocked.
+      if (TaskInfo.INCOMPLETE_STATES.contains(taskInfo.getTaskState())) {
+        setTaskError(taskInfo);
+        taskInfo.save();
+      }
+      // Mark customer task as completed.
+      // Customer task is marked completed after the task state is updated in TaskExecutor.
+      // Moreover, this method has an internal guard to set the completion time only if it is null.
       customerTask.markAsCompleted();
-
-      if (customerTask.getTarget().equals(TargetType.Backup)
-          && customerTask.getType().equals(TaskType.Create)) {
-        // Make transition state false for inProgress backups
-        UUID taskUUID = taskInfo.getTaskUUID();
-        List<Backup> backupList = Backup.fetchAllBackupsByTaskUUID(taskUUID);
-        backupList
-            .stream()
-            .filter(backup -> backup.state.equals(Backup.BackupState.InProgress))
-            .forEach(backup -> backup.transitionState(Backup.BackupState.Failed));
-        // Create the update lambda.
-        Universe.UniverseUpdater updater =
-            universe -> {
-              UniverseDefinitionTaskParams universeDetails = universe.getUniverseDetails();
-              if (universeDetails.backupInProgress) {
-                universeDetails.backupInProgress = false;
-              }
-              if (universeDetails.updateInProgress) {
-                universeDetails.updateInProgress = false;
-              }
-              universe.setUniverseDetails(universeDetails);
-            };
-
-        Universe.saveDetails(customerTask.getTargetUUID(), updater, false);
-        LOG.debug("Unlocked universe {} for backups.", customerTask.getTargetUUID());
-      }
-
-      // Unlock the universe for future operations
-      if (customerTask.getTarget().equals(TargetType.Universe)) {
-        // Create the update lambda.
-        Universe.UniverseUpdater updater =
-            universe -> {
-              UniverseDefinitionTaskParams universeDetails = universe.getUniverseDetails();
-              if (universeDetails.updateInProgress) {
-                universeDetails.updateInProgress = false;
-                universe.setUniverseDetails(universeDetails);
-              }
-            };
-
-        Universe.saveDetails(customerTask.getTargetUUID(), updater, false);
-        LOG.debug("Unlocked universe {} for updates.", customerTask.getTargetUUID());
-      }
     } catch (Exception e) {
       LOG.error(String.format("Error encountered failing task %s", customerTask.getTaskUUID()), e);
     }
@@ -95,25 +113,20 @@ public class CustomerTaskManager {
     LOG.info("Failing incomplete tasks...");
     try {
       String incompleteStates =
-          String.join(
-              "', '",
-              new String[] {
-                TaskInfo.State.Created.name(),
-                TaskInfo.State.Initializing.name(),
-                TaskInfo.State.Running.name(),
-                TaskInfo.State.Abort.name()
-              });
-      // Retrieve all incomplete customer tasks.
-      // TODO It is possible that completion_time == NULL
-      // but task_state is completed. Retry will not appear on UI.
+          TaskInfo.INCOMPLETE_STATES
+              .stream()
+              .map(Objects::toString)
+              .collect(Collectors.joining("','"));
+      // Retrieve all incomplete customer tasks or task in incomplete state. Task state update and
+      // customer completion time update are not transactional.
       String query =
           "SELECT ti.uuid AS task_uuid, ct.id AS customer_task_id "
               + "FROM task_info ti, customer_task ct "
               + "WHERE ti.uuid = ct.task_uuid "
-              + "AND ct.completion_time IS NULL "
-              + "AND ti.task_state IN ('"
+              + "AND (ct.completion_time IS NULL "
+              + "OR ti.task_state IN ('"
               + incompleteStates
-              + "')";
+              + "'))";
       // TODO use Finder.
       Ebean.createSqlQuery(query)
           .findList()
