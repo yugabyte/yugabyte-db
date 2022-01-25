yugabyte/yugabyte-db

commit a624c808ea448fffbc6e8d68dc55dd0ea78c9ac7

ENG-2059: Implement CQL TRUNCATE statement and Redis FLUSHDB and FLUSHALL commands.
Summary:
- implements truncate table in YBClient API that sends TruncateTable RPC to master, which in turn sends Truncate RPC requests to the tablets
- in tserver, Truncate is replicated as Raft consensus round, with each replica dropping the existing rocksdb db and recreating a new one.
- when recreating a new rocksdb, the last persistent opid is initialized that of the Truncate opid

Test Plan: TestTruncate Java test and TestRedisService.TestFlushAll / TestFlushDb C++ tests.

Reviewers: neil, oleg, mikhail, sergei

Reviewed By: oleg, mikhail, sergei

Subscribers: venkatesh, ybase, bogdan, yql, bharat

Differential Revision: https://phabricator.dev.yugabyte.com/D3600

src/yb/rocksdb/db/db_impl.cc
@@ -5203,14 +5203,7 @@ Status DBImpl::Import(const std::string& source_dir) {
   if (!status.ok()) {
     return status;
   }
-  auto cfd = versions_->GetColumnFamilySet()->GetDefault();
-  InstrumentedMutexLock lock(&mutex_);
-  status = versions_->LogAndApply(cfd, *cfd->GetCurrentMutableCFOptions(), &edit, &mutex_);
-  if (!status.ok()) {
-    return status;
-  }
-  cfd->InstallSuperVersion(new SuperVersion(), &mutex_);
-  return Status::OK();
+  return ApplyVersionEdit(&edit);
 }
 
 void DBImpl::GetApproximateSizes(ColumnFamilyHandle* column_family,
@@ -5449,7 +5442,7 @@ void DBImpl::GetLiveFilesMetaData(std::vector<LiveFileMetaData>* metadata) {
 OpId DBImpl::GetFlushedOpId() {
   InstrumentedMutexLock l(&mutex_);
   auto result = versions_->FlushedOpId();
-  if (yb::IsDebug() || !result) {
+  if (!result) {
     std::vector<LiveFileMetaData> files;
     versions_->GetLiveFilesMetaData(&files);
     OpId last_op_id;
@@ -5462,6 +5455,23 @@ OpId DBImpl::GetFlushedOpId() {
     result = last_op_id;
   }
   return result;
+}
+
+Status DBImpl::ApplyVersionEdit(VersionEdit* edit) {
+  auto cfd = versions_->GetColumnFamilySet()->GetDefault();
+  InstrumentedMutexLock lock(&mutex_);
+  auto status = versions_->LogAndApply(cfd, *cfd->GetCurrentMutableCFOptions(), edit, &mutex_);
+  if (!status.ok()) {
+    return status;
+  }
+  cfd->InstallSuperVersion(new SuperVersion(), &mutex_);
+  return Status::OK();
+}
+
+Status DBImpl::SetFlushedOpId(const OpId& op_id) {
+  VersionEdit edit;
+  edit.SetFlushedOpId(op_id);
+  return ApplyVersionEdit(&edit);
 }
 
 void DBImpl::GetColumnFamilyMetaData(

java/yb-cql/src/test/java/org/yb/cql/TestTruncate.java
@@ -1,0 +1,56 @@
+// Copyright (c) YugaByte, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+// in compliance with the License.  You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software distributed under the License
+// is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+// or implied.  See the License for the specific language governing permissions and limitations
+// under the License.
+//
+package org.yb.cql;
+
+import com.datastax.driver.core.Row;
+
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
+
+public class TestTruncate extends BaseCQLTest {
+
+  @Test
+  public void testTruncate() throws Exception {
+
+    // Create test table with rows.
+    final int ROW_COUNT = 10;
+    setupTable("test_truncate", ROW_COUNT);
+
+    // Verify row count.
+    int count = 0;
+    for (Row row : session.execute("select * from test_truncate;")) {
+      count++;
+    }
+    assertEquals(ROW_COUNT, count);
+
+    // Truncate rows and verify the table is empty.
+    session.execute("truncate test_truncate;");
+    assertNull(session.execute("select * from test_truncate;").one());
+
+    // Truncate again (an NOOP). Verify there is no issue.
+    session.execute("truncate test_truncate;");
+    assertNull(session.execute("select * from test_truncate;").one());
+
+    // Truncate a newly created, empty table (an NOOP also). Verify there is no issue.
+    setupTable("test_empty_truncate", 0);
+    session.execute("truncate test_empty_truncate;");
+    assertNull(session.execute("select * from test_empty_truncate;").one());
+  }
+
+  @Test
+  public void testInvalidTruncate() throws Exception {
+    runInvalidStmt("truncate table invalid_namespace.test_truncate;");
+    runInvalidStmt("truncate table invalid_table;");
+  }
+}

src/yb/client/client-internal.cc
@@ -83,6 +83,8 @@ using master::ChangeMasterClusterConfigRequestPB;
 using master::ChangeMasterClusterConfigResponsePB;
 using master::CreateTableRequestPB;
 using master::CreateTableResponsePB;
+using master::TruncateTableRequestPB;
+using master::TruncateTableResponsePB;
 using master::DeleteTableRequestPB;
 using master::DeleteTableResponsePB;
 using master::GetMasterClusterConfigRequestPB;
@@ -94,6 +96,8 @@ using master::IsAlterTableDoneRequestPB;
 using master::IsAlterTableDoneResponsePB;
 using master::IsCreateTableDoneRequestPB;
 using master::IsCreateTableDoneResponsePB;
+using master::IsTruncateTableDoneRequestPB;
+using master::IsTruncateTableDoneResponsePB;
 using master::IsDeleteTableDoneRequestPB;
 using master::IsDeleteTableDoneResponsePB;
 using master::GetTableLocationsRequestPB;
@@ -525,15 +529,13 @@ Status YBClient::Data::IsCreateTableInProgress(YBClient* client,
   IsCreateTableDoneResponsePB resp;
   table_name.SetIntoTableIdentifierPB(req.mutable_table());
 
-  // TODO: Add client rpc timeout and use 'default_admin_operation_timeout_' as
-  // the default timeout for all admin operations.
   const Status s =
       SyncLeaderMasterRpc<IsCreateTableDoneRequestPB, IsCreateTableDoneResponsePB>(
           deadline,
           client,
           req,
           &resp,
-          nullptr,
+          nullptr /* num_attempts */,
           "IsCreateTableDone",
           &MasterServiceProxy::IsCreateTableDone);
   // RETURN_NOT_OK macro can't take templated function call as param,
@@ -607,15 +609,13 @@ Status YBClient::Data::IsDeleteTableInProgress(YBClient* client,
   IsDeleteTableDoneResponsePB resp;
   req.set_table_id(deleted_table_id);
 
-  // TODO: Add client rpc timeout and use 'default_admin_operation_timeout_' as
-  // the default timeout for all admin operations.
   const Status s =
       SyncLeaderMasterRpc<IsDeleteTableDoneRequestPB, IsDeleteTableDoneResponsePB>(
           deadline,
           client,
           req,
           &resp,
-          nullptr,
+          nullptr /* num_attempts */,
           "IsDeleteTableDone",
           &MasterServiceProxy::IsDeleteTableDone);
   // RETURN_NOT_OK macro can't take templated function call as param,
@@ -638,9 +638,62 @@ Status YBClient::Data::WaitForDeleteTableToFinish(YBClient* client,
       std::bind(&YBClient::Data::IsDeleteTableInProgress, this, client, deleted_table_id, _1, _2));
 }
 
+Status YBClient::Data::TruncateTable(YBClient* client,
+                                     const string& table_id,
+                                     const MonoTime& deadline,
+                                     bool wait) {
+  TruncateTableRequestPB req;
+  TruncateTableResponsePB resp;
+
+  req.set_table_id(table_id);
+  RETURN_NOT_OK((SyncLeaderMasterRpc<TruncateTableRequestPB, TruncateTableResponsePB>(
+      deadline, client, req, &resp, nullptr /* num_attempts */, "TruncateTable",
+      &MasterServiceProxy::TruncateTable)));
+  if (resp.has_error()) {
+    return StatusFromPB(resp.error().status());
+  }
+
+  // Spin until the table is fully truncated, if requested.
+  if (wait) {
+    RETURN_NOT_OK(WaitForTruncateTableToFinish(client, table_id, deadline));
+  }
+
+  LOG(INFO) << "Truncated table " << table_id;
+  return Status::OK();
+}
+
+Status YBClient::Data::IsTruncateTableInProgress(YBClient* client,
+                                                 const std::string& table_id,
+                                                 const MonoTime& deadline,
+                                                 bool* truncate_in_progress) {
+  DCHECK_ONLY_NOTNULL(truncate_in_progress);
+  IsTruncateTableDoneRequestPB req;
+  IsTruncateTableDoneResponsePB resp;
+
+  req.set_table_id(table_id);
+  RETURN_NOT_OK((SyncLeaderMasterRpc<IsTruncateTableDoneRequestPB, IsTruncateTableDoneResponsePB>(
+      deadline, client, req, &resp, nullptr /* num_attempts */, "IsTruncateTableDone",
+      &MasterServiceProxy::IsTruncateTableDone)));
+  if (resp.has_error()) {
+    return StatusFromPB(resp.error().status());
+  }
+
+  *truncate_in_progress = !resp.done();
+  return Status::OK();
+}
+
+Status YBClient::Data::WaitForTruncateTableToFinish(YBClient* client,
+                                                    const std::string& table_id,
+                                                    const MonoTime& deadline) {
+  return RetryFunc(
+      deadline, "Waiting on Truncate Table to be completed",
+      "Timed out waiting for Table Truncation",
+      std::bind(&YBClient::Data::IsTruncateTableInProgress, this, client, table_id, _1, _2));
+}
+
 Status YBClient::Data::AlterTable(YBClient* client,
-                                    const AlterTableRequestPB& req,
-                                    const MonoTime& deadline) {
+                                  const AlterTableRequestPB& req,
+                                  const MonoTime& deadline) {
   AlterTableResponsePB resp;
   Status s =
       SyncLeaderMasterRpc<AlterTableRequestPB, AlterTableResponsePB>(
@@ -648,7 +701,7 @@ Status YBClient::Data::AlterTable(YBClient* client,
           client,
           req,
           &resp,
-          nullptr,
+          nullptr /* num_attempts */,
           "AlterTable",
           &MasterServiceProxy::AlterTable);
   RETURN_NOT_OK(s);
@@ -677,7 +730,7 @@ Status YBClient::Data::IsAlterTableInProgress(YBClient* client,
           client,
           req,
           &resp,
-          nullptr,
+          nullptr /* num_attempts */,
           "IsAlterTableDone",
           &MasterServiceProxy::IsAlterTableDone);
   RETURN_NOT_OK(s);
@@ -1181,7 +1234,7 @@ Status YBClient::Data::SetReplicationInfo(
   GetMasterClusterConfigRequestPB get_req;
   GetMasterClusterConfigResponsePB get_resp;
   Status s = SyncLeaderMasterRpc<GetMasterClusterConfigRequestPB, GetMasterClusterConfigResponsePB>(
-      deadline, client, get_req, &get_resp, nullptr, "GetMasterClusterConfig",
+      deadline, client, get_req, &get_resp, nullptr /* num_attempts */, "GetMasterClusterConfig",
       &MasterServiceProxy::GetMasterClusterConfig);
   RETURN_NOT_OK(s);
   if (get_resp.has_error()) {
@@ -1198,8 +1251,8 @@ Status YBClient::Data::SetReplicationInfo(
 
   // Try to update it on the live cluster.
   s = SyncLeaderMasterRpc<ChangeMasterClusterConfigRequestPB, ChangeMasterClusterConfigResponsePB>(
-      deadline, client, change_req, &change_resp, nullptr, "ChangeMasterClusterConfig",
-      &MasterServiceProxy::ChangeMasterClusterConfig);
+      deadline, client, change_req, &change_resp, nullptr /* num_attempts */,
+      "ChangeMasterClusterConfig", &MasterServiceProxy::ChangeMasterClusterConfig);
   RETURN_NOT_OK(s);
   if (change_resp.has_error()) {
     // Retry on config mismatch.

src/yb/client/client-internal.h
@@ -112,6 +112,20 @@ class YBClient::Data {
                                             const std::string& deleted_table_id,
                                             const MonoTime& deadline);
 
+  CHECKED_STATUS TruncateTable(YBClient* client,
+                               const std::string& table_id,
+                               const MonoTime& deadline,
+                               bool wait = true);
+
+  CHECKED_STATUS IsTruncateTableInProgress(YBClient* client,
+                                           const std::string& table_id,
+                                           const MonoTime& deadline,
+                                           bool *truncate_in_progress);
+
+  CHECKED_STATUS WaitForTruncateTableToFinish(YBClient* client,
+                                              const std::string& table_id,
+                                              const MonoTime& deadline);
+
   CHECKED_STATUS AlterTable(YBClient* client,
                     const master::AlterTableRequestPB& req,
                     const MonoTime& deadline);

src/yb/client/client.cc
@@ -333,6 +333,11 @@ Status YBClient::IsCreateTableInProgress(const YBTableName& table_name,
   return data_->IsCreateTableInProgress(this, table_name, deadline, create_in_progress);
 }
 
+Status YBClient::TruncateTable(const string& table_id, bool wait) {
+  MonoTime deadline = MonoTime::Now() + default_admin_operation_timeout();
+  return data_->TruncateTable(this, table_id, deadline, wait);
+}
+
 Status YBClient::DeleteTable(const YBTableName& table_name, bool wait) {
   MonoTime deadline = MonoTime::Now();
   deadline.AddDelta(default_admin_operation_timeout());

src/yb/client/client.h
@@ -257,6 +257,10 @@ class YBClient : public std::enable_shared_from_this<YBClient> {
   CHECKED_STATUS IsCreateTableInProgress(const YBTableName& table_name,
                                          bool *create_in_progress);
 
+  // Truncate the specified table.
+  // Set 'wait' to true if the call must wait for the table to be fully truncated before returning.
+  CHECKED_STATUS TruncateTable(const std::string& table_id, bool wait = true);
+
   // Delete the specified table.
   // Set 'wait' to true if the call must wait for the table to be fully deleted before returning.
   CHECKED_STATUS DeleteTable(const YBTableName& table_name, bool wait = true);

src/yb/consensus/consensus.proto
@@ -110,6 +110,7 @@ enum OperationType {
   CHANGE_CONFIG_OP = 5;
   UPDATE_TRANSACTION_OP = 6;
   SNAPSHOT_OP = 7;
+  TRUNCATE_OP = 8;
 }
 
 // The transaction driver type: indicates whether a transaction is
@@ -197,6 +198,7 @@ message ReplicateMsg {
   optional tserver.AlterSchemaRequestPB alter_schema_request = 6;
   optional tserver.TransactionStatePB transaction_state = 10;
   optional tserver.TabletSnapshotOpRequestPB snapshot_request = 11;
+  optional tserver.TruncateRequestPB truncate_request = 12;
   optional ChangeConfigRecordPB change_config_record = 7;
 
   // The Raft operation ID known to the leader to be committed at the time this message was sent.

src/yb/master/async_rpc_tasks.cc
@@ -299,12 +299,15 @@ Status RetryingTSRpcTask::ResetTSProxy() {
   // TODO: if there is no replica available, should we still keep the task running?
   RETURN_NOT_OK(replica_picker_->PickReplica(&target_ts_desc_));
 
-  shared_ptr<tserver::TabletServerAdminServiceProxy> ts_proxy;
-  RETURN_NOT_OK(target_ts_desc_->GetProxy(master_->messenger(), &ts_proxy));
-  ts_proxy_.swap(ts_proxy);
-
+  shared_ptr<tserver::TabletServerServiceProxy> ts_proxy;
+  shared_ptr<tserver::TabletServerAdminServiceProxy> ts_admin_proxy;
   shared_ptr<consensus::ConsensusServiceProxy> consensus_proxy;
+  RETURN_NOT_OK(target_ts_desc_->GetProxy(master_->messenger(), &ts_proxy));
+  RETURN_NOT_OK(target_ts_desc_->GetProxy(master_->messenger(), &ts_admin_proxy));
   RETURN_NOT_OK(target_ts_desc_->GetProxy(master_->messenger(), &consensus_proxy));
+
+  ts_proxy_.swap(ts_proxy);
+  ts_admin_proxy_.swap(ts_admin_proxy);
   consensus_proxy_.swap(consensus_proxy);
 
   return Status::OK();
@@ -354,7 +357,7 @@ void AsyncCreateReplica::HandleResponse(int attempt) {
 }
 
 bool AsyncCreateReplica::SendRequest(int attempt) {
-  ts_proxy_->CreateTabletAsync(req_, &resp_, &rpc_, BindRpcCallback());
+  ts_admin_proxy_->CreateTabletAsync(req_, &resp_, &rpc_, BindRpcCallback());
   VLOG(1) << "Send create tablet request to " << permanent_uuid_ << ":\n"
           << " (attempt " << attempt << "):\n"
           << req_.DebugString();
@@ -428,7 +431,7 @@ bool AsyncDeleteReplica::SendRequest(int attempt) {
     req.set_cas_config_opid_index_less_or_equal(*cas_config_opid_index_less_or_equal_);
   }
 
-  ts_proxy_->DeleteTabletAsync(req, &resp_, &rpc_, BindRpcCallback());
+  ts_admin_proxy_->DeleteTabletAsync(req, &resp_, &rpc_, BindRpcCallback());
   VLOG(1) << "Send delete tablet request to " << permanent_uuid_
           << " (attempt " << attempt << "):\n"
           << req.DebugString();
@@ -452,11 +455,11 @@ string AsyncAlterTable::description() const {
   return tablet_->ToString() + " Alter Table RPC";
 }
 
-string AsyncAlterTable::tablet_id() const {
+TabletId AsyncAlterTable::tablet_id() const {
   return tablet_->tablet_id();
 }
 
-string AsyncAlterTable::permanent_uuid() const {
+TabletServerId AsyncAlterTable::permanent_uuid() const {
   return target_ts_desc_ != nullptr ? target_ts_desc_->permanent_uuid() : "";
 }
 
@@ -508,8 +511,59 @@ bool AsyncAlterTable::SendRequest(int attempt) {
 
   l->Unlock();
 
-  ts_proxy_->AlterSchemaAsync(req, &resp_, &rpc_, BindRpcCallback());
+  ts_admin_proxy_->AlterSchemaAsync(req, &resp_, &rpc_, BindRpcCallback());
   VLOG(1) << "Send alter table request to " << permanent_uuid()
+          << " (attempt " << attempt << "):\n"
+          << req.DebugString();
+  return true;
+}
+
+// ============================================================================
+//  Class AsyncTruncate.
+// ============================================================================
+AsyncTruncate::AsyncTruncate(Master *master,
+                             ThreadPool* callback_pool,
+                             const scoped_refptr<TabletInfo>& tablet)
+    : RetryingTSRpcTask(master,
+                        callback_pool,
+                        gscoped_ptr<TSPicker>(new PickLeaderReplica(tablet)),
+                        tablet->table().get()),
+      tablet_(tablet) {
+}
+
+string AsyncTruncate::description() const {
+  return tablet_->ToString() + " Truncate Tablet RPC";
+}
+
+TabletId AsyncTruncate::tablet_id() const {
+  return tablet_->tablet_id();
+}
+
+TabletServerId AsyncTruncate::permanent_uuid() const {
+  return target_ts_desc_ != nullptr ? target_ts_desc_->permanent_uuid() : "";
+}
+
+void AsyncTruncate::HandleResponse(int attempt) {
+  if (resp_.has_error()) {
+    const Status s = StatusFromPB(resp_.error().status());
+    const TabletServerErrorPB::Code code = resp_.error().code();
+    LOG(WARNING) << "TS " << permanent_uuid() << ": truncate failed for tablet " << tablet_id()
+                 << " with error code " << TabletServerErrorPB::Code_Name(code)
+                 << ": " << s.ToString();
+  } else {
+    VLOG(1) << "TS " << permanent_uuid() << ": truncate complete on tablet " << tablet_id();
+    PerformStateTransition(kStateRunning, kStateComplete);
+  }
+
+  server::UpdateClock(resp_, master_->clock());
+}
+
+bool AsyncTruncate::SendRequest(int attempt) {
+  tserver::TruncateRequestPB req;
+  req.set_tablet_id(tablet_id());
+  req.set_propagated_hybrid_time(master_->clock()->Now().ToUint64());
+  ts_proxy_->TruncateAsync(req, &resp_, &rpc_, BindRpcCallback());
+  VLOG(1) << "Send truncate tablet request to " << permanent_uuid()
           << " (attempt " << attempt << "):\n"
           << req.DebugString();
   return true;
@@ -530,11 +584,11 @@ CommonInfoForRaftTask::CommonInfoForRaftTask(
   deadline_ = MonoTime::Max();  // Never time out.
 }
 
-string CommonInfoForRaftTask::tablet_id() const {
+TabletId CommonInfoForRaftTask::tablet_id() const {
   return tablet_->tablet_id();
 }
 
-string CommonInfoForRaftTask::permanent_uuid() const {
+TabletServerId CommonInfoForRaftTask::permanent_uuid() const {
   return target_ts_desc_ != nullptr ? target_ts_desc_->permanent_uuid() : "";
 }
 

commit 08b49e8fdb42669cb5f6a0db03c5c75bda752635

[#11180] Fix DBImpl::ApplyVersionEdit
Summary:
Both issues were introduced by a624c808ea448fffbc6e8d68dc55dd0ea78c9ac7, but started to appear more frequently because of more intensive use of ApplyVersionEdit through ModifyFlushedFrontier.

1)The mutex is acquired and LogAndApply is invoked with reference to current superversion mutable_cf_options.
But LogAndApply could unlock and lock mutex back.
When mutex is unlocked superversion could be deleted.
Later LogAndApply will try to access provided mutable_cf_options, that is part of destroyed superversion.
It results to heap-use-after-free and process crash.

Fixed by referencing superversion for period of LogAndApply.

2) ApplyVersionEdit calls InstallSuperVersion that could return SuperVersion that should be deleted after mutex is released.
But result of InstallSuperVersion is just ignored, so SuperVersion could be destroyed while mutex is held.

Add MUST_USE_RESULT

Test Plan: ybd asan --gtest_filter YbAdminSnapshotScheduleTest.TestGCHiddenTables -n 20

Reviewers: timur

Reviewed By: timur

Subscribers: bogdan, mbautin, ybase

Differential Revision: https://phabricator.dev.yugabyte.com/D14947

src/yb/rocksdb/db/column_family.h
@@ -315,11 +315,13 @@ class ColumnFamilyData {
   // As argument takes a pointer to allocated SuperVersion to enable
   // the clients to allocate SuperVersion outside of mutex.
   // IMPORTANT: Only call this from DBImpl::InstallSuperVersion()
-  std::unique_ptr<SuperVersion> InstallSuperVersion(SuperVersion* new_superversion,
-                                                    InstrumentedMutex* db_mutex,
-                                                    const MutableCFOptions& mutable_cf_options);
-  std::unique_ptr<SuperVersion> InstallSuperVersion(SuperVersion* new_superversion,
-                                                    InstrumentedMutex* db_mutex);
+  MUST_USE_RESULT std::unique_ptr<SuperVersion> InstallSuperVersion(
+      SuperVersion* new_superversion,
+      InstrumentedMutex* db_mutex,
+      const MutableCFOptions& mutable_cf_options);
+  MUST_USE_RESULT std::unique_ptr<SuperVersion> InstallSuperVersion(
+      SuperVersion* new_superversion,
+      InstrumentedMutex* db_mutex);
 
   void ResetThreadLocalSuperVersions();
 

src/yb/rocksdb/db/compacted_db_impl.cc
@@ -109,17 +109,14 @@ std::vector<Status> CompactedDBImpl::MultiGet(const ReadOptions& options,
 }
 
 Status CompactedDBImpl::Init(const Options& options) {
-  mutex_.Lock();
-  ColumnFamilyDescriptor cf(kDefaultColumnFamilyName,
-                            ColumnFamilyOptions(options));
-  Status s = Recover({ cf }, true /* read only */, false);
-  if (s.ok()) {
+  {
+    std::unique_ptr<SuperVersion> old_superversion;
+    InstrumentedMutexLock lock(&mutex_);
+    ColumnFamilyDescriptor cf(kDefaultColumnFamilyName,
+                              ColumnFamilyOptions(options));
+    RETURN_NOT_OK(Recover({ cf }, true /* read only */, false));
     cfd_ = down_cast<ColumnFamilyHandleImpl*>(DefaultColumnFamily())->cfd();
-    cfd_->InstallSuperVersion(new SuperVersion(), &mutex_);
-  }
-  mutex_.Unlock();
-  if (!s.ok()) {
-    return s;
+    old_superversion = cfd_->InstallSuperVersion(new SuperVersion(), &mutex_);
   }
   version_ = cfd_->GetSuperVersion()->current;
   user_comparator_ = cfd_->user_comparator();

src/yb/rocksdb/db/db_impl.cc
@@ -46,7 +46,6 @@
 
 #include <boost/container/small_vector.hpp>
 
-
 #include "yb/gutil/stringprintf.h"
 #include "yb/util/string_util.h"
 #include "yb/util/scope_exit.h"
@@ -6004,12 +6003,22 @@ UserFrontierPtr DBImpl::GetMutableMemTableFrontier(UpdateUserValueType type) {
 
 Status DBImpl::ApplyVersionEdit(VersionEdit* edit) {
   auto cfd = versions_->GetColumnFamilySet()->GetDefault();
+  std::unique_ptr<SuperVersion> superversion_to_free_after_unlock_because_of_install;
+  std::unique_ptr<SuperVersion> superversion_to_free_after_unlock_because_of_unref;
   InstrumentedMutexLock lock(&mutex_);
-  auto status = versions_->LogAndApply(cfd, *cfd->GetCurrentMutableCFOptions(), edit, &mutex_);
+  auto current_sv = cfd->GetSuperVersion()->Ref();
+  auto se = yb::ScopeExit([&superversion_to_free_after_unlock_because_of_unref, current_sv]() {
+    if (current_sv->Unref()) {
+      current_sv->Cleanup();
+      superversion_to_free_after_unlock_because_of_unref.reset(current_sv);
+    }
+  });
+  auto status = versions_->LogAndApply(cfd, current_sv->mutable_cf_options, edit, &mutex_);
   if (!status.ok()) {
     return status;
   }
-  cfd->InstallSuperVersion(new SuperVersion(), &mutex_);
+  superversion_to_free_after_unlock_because_of_install = cfd->InstallSuperVersion(
+      new SuperVersion(), &mutex_);
 
   return Status::OK();
 }

src/yb/rocksdb/db/db_impl_readonly.cc
@@ -148,28 +148,32 @@ Status DB::OpenForReadOnly(
   *dbptr = nullptr;
   handles->clear();
 
+  Status s;
   DBImplReadOnly* impl = new DBImplReadOnly(db_options, dbname);
-  impl->mutex_.Lock();
-  Status s = impl->Recover(column_families, true /* read only */,
-                           error_if_log_file_exist);
-  if (s.ok()) {
-    // set column family handles
-    for (auto cf : column_families) {
-      auto cfd =
-          impl->versions_->GetColumnFamilySet()->GetColumnFamily(cf.name);
-      if (cfd == nullptr) {
-        s = STATUS(InvalidArgument, "Column family not found: ", cf.name);
-        break;
+  {
+    // Used to destroy superversions outside of lock.
+    std::vector<std::unique_ptr<SuperVersion>> old_superversions;
+
+    InstrumentedMutexLock lock(&impl->mutex_);
+    s = impl->Recover(column_families, true /* read only */, error_if_log_file_exist);
+    if (s.ok()) {
+      // set column family handles
+      for (auto cf : column_families) {
+        auto cfd =
+            impl->versions_->GetColumnFamilySet()->GetColumnFamily(cf.name);
+        if (cfd == nullptr) {
+          s = STATUS(InvalidArgument, "Column family not found: ", cf.name);
+          break;
+        }
+        handles->push_back(new ColumnFamilyHandleImpl(cfd, impl, &impl->mutex_));
       }
-      handles->push_back(new ColumnFamilyHandleImpl(cfd, impl, &impl->mutex_));
+    }
+    if (s.ok()) {
+      for (auto cfd : *impl->versions_->GetColumnFamilySet()) {
+        old_superversions.push_back(cfd->InstallSuperVersion(new SuperVersion(), &impl->mutex_));
+      }
     }
   }
-  if (s.ok()) {
-    for (auto cfd : *impl->versions_->GetColumnFamilySet()) {
-      cfd->InstallSuperVersion(new SuperVersion(), &impl->mutex_);
-    }
-  }
-  impl->mutex_.Unlock();
   if (s.ok()) {
     *dbptr = impl;
   } else {

src/yb/integration-tests/ts_itest-base.cc
@@ -383,10 +383,10 @@ void TabletServerIntegrationTestBase::TearDown() {
   client_.reset();
   if (cluster_) {
     for (const auto* daemon : cluster_->master_daemons()) {
-      EXPECT_TRUE(daemon->IsShutdown() || daemon->IsProcessAlive());
+      EXPECT_TRUE(daemon->IsShutdown() || daemon->IsProcessAlive()) << "Daemon: " << daemon->id();
     }
     for (const auto* daemon : cluster_->tserver_daemons()) {
-      EXPECT_TRUE(daemon->IsShutdown() || daemon->IsProcessAlive());
+      EXPECT_TRUE(daemon->IsShutdown() || daemon->IsProcessAlive()) << "Daemon: " << daemon->id();
     }
     cluster_->Shutdown();
   }