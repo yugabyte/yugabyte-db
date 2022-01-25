477f3eb397ffb3b4aee904a2812d286f324d2f3f
Akshat Jain <ajain@yugabyte.com>
2022-01-25 15:19:17 +0000

    Make ybPrebuiltAmi flag provider level field instead of CreateUniverse param
    Test Plan: All relevant unit tests are passing locally. Also simulated the flow for the new param via curl requests.
    
    Reviewers: hkandala, hvontela, amindrov
    
    Reviewed By: amindrov
    
    Subscribers: jenkins-bot
    
    Differential Revision: https://phabricator.dev.yugabyte.com/D15000

--- managed/src/main/java/com/yugabyte/yw/forms/UniverseDefinitionTaskParams.java
+++ managed/src/main/java/com/yugabyte/yw/forms/UniverseDefinitionTaskParams.java
@@ -404,12 +404,6 @@ public class UniverseDefinitionTaskParams extends UniverseTaskParams {
 
     @ApiModelProperty() public boolean useSystemd = false;
 
-    // When this is set to true, CreateUniverse is expected to be called with a provider which has
-    // been configured to use an AMI ID which has required package installations,
-    // Yugabyte release package, etc built in, as Ansible tasks corresponding to
-    // these would be skipped.
-    @ApiModelProperty() public boolean ybPrebuiltAmi = false;
-
     // Info of all the gflags that the user would like to save to the universe. These will be
     // used during edit universe, for example, to set the flags on new nodes to match
     // existing nodes' settings.
@@ -466,7 +460,6 @@ public class UniverseDefinitionTaskParams extends UniverseTaskParams {
       newUserIntent.numNodes = numNodes;
       newUserIntent.ybSoftwareVersion = ybSoftwareVersion;
       newUserIntent.useSystemd = useSystemd;
-      newUserIntent.ybPrebuiltAmi = ybPrebuiltAmi;
       newUserIntent.accessKeyCode = accessKeyCode;
       newUserIntent.assignPublicIP = assignPublicIP;
       newUserIntent.assignStaticPublicIP = assignStaticPublicIP;
@@ -501,8 +494,7 @@ public class UniverseDefinitionTaskParams extends UniverseTaskParams {
           && assignPublicIP == other.assignPublicIP
           && assignStaticPublicIP == other.assignStaticPublicIP
           && useTimeSync == other.useTimeSync
-          && useSystemd == other.useSystemd
-          && ybPrebuiltAmi == other.ybPrebuiltAmi) {
+          && useSystemd == other.useSystemd) {
         return true;
       }
       return false;
