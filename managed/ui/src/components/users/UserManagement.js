24729ca799d0bdec33c252d282b3d954d45e318d
Athul <athulsn2@gmail.com>
2022-01-30 16:03:45 +0530

    [PLAT-2615] UI Support for LDAP
    Summary:
    Added New "User Authentication" tab
    Added LDAP UI support
    
    Test Plan:
    Tested manually
    Tested saving LDAP config
    
    Read Only users cant access User Auth tab
    {F22191}
    
    {F22192}
    
    Confirmation modal for disabling LDAP
    {F22193}
    
    {F22194}
    
    {F22195}
    
    Reviewers: kkannan, mjoshi, cpadinjareveettil, ssutar, lsangappa
    
    Reviewed By: cpadinjareveettil, ssutar, lsangappa
    
    Subscribers: jenkins-bot, ui
    
    Differential Revision: https://phabricator.dev.yugabyte.com/D14927

--- managed/ui/src/components/users/UserManagement.js
+++ managed/ui/src/components/users/UserManagement.js
@@ -1,11 +1,41 @@
 import React from 'react';
-import { Tab } from 'react-bootstrap';
+import { Tab, OverlayTrigger, Tooltip } from 'react-bootstrap';
 import { YBTabsPanel } from '../panels';
 import UsersListContainer from './Users/UsersListContainer';
+import UserAuthContainer from './UserAuth/UserAuthContainer';
+import KeyIcon from './icons/key_icon';
 import './styles.scss';
+import { YBLoading } from '../common/indicators';
 
 export const UserManagement = (props) => {
-  const { activeTab, defaultTab, routePrefix } = props;
+  const { activeTab, defaultTab, routePrefix, currentUserInfo } = props;
+  const role = currentUserInfo?.role;
+  const isAdmin = ['Admin', 'SuperAdmin'].includes(role);
+  const isLoading = !role;
+
+  const AuthTab = () => {
+    return (
+      <span>
+        <KeyIcon />
+        User Authentication
+      </span>
+    );
+  };
+
+  const AuthTabWithOverlay = () => {
+    return (
+      <OverlayTrigger
+        placement="right"
+        overlay={
+          <Tooltip className="high-index" id="user-auth-tooltip">
+            You don't have enough permission
+          </Tooltip>
+        }
+      >
+        {AuthTab()}
+      </OverlayTrigger>
+    );
+  };
 
   return (
     <div>
@@ -26,6 +56,14 @@ export const UserManagement = (props) => {
           unmountOnExit
         >
           <UsersListContainer />
+        </Tab>
+        <Tab
+          disabled={!isAdmin || isLoading}
+          eventKey="user-auth"
+          title={isLoading || isAdmin ? <AuthTab /> : <AuthTabWithOverlay />}
+          unmountOnExit
+        >
+          {isLoading ? <YBLoading /> : <UserAuthContainer isAdmin={isAdmin} />}
         </Tab>
       </YBTabsPanel>
     </div>
