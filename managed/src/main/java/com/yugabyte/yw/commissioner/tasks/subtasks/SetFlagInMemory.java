8268694f9caee3b93e78135f48a324ff5a276760
Arnav Agarwal <14933889+Arnav15@users.noreply.github.com>
2022-02-02 11:59:15 -0800

    [CLOUDGA-2545] Update broadcast/bind addresses for dual NIC clusters
    Summary:
    For supporting smart clients, multi-region and x-cluster deployments, we need to use the
    correct routes for communication between VMs, as well as appropriately set the RPC bind addresses to
    listen to communication
    
    Test Plan: Create a dual NIC universe and verify
    
    Reviewers: sanketh, hkandala, dskorobogaty, pkrishnaiah, jvigil, daniel
    
    Reviewed By: dskorobogaty, pkrishnaiah, jvigil, daniel
    
    Subscribers: jenkins-bot, yugaware
    
    Differential Revision: https://phabricator.dev.yugabyte.com/D14826

--- managed/src/main/java/com/yugabyte/yw/commissioner/tasks/subtasks/SetFlagInMemory.java
+++ managed/src/main/java/com/yugabyte/yw/commissioner/tasks/subtasks/SetFlagInMemory.java
@@ -55,7 +55,8 @@ public class SetFlagInMemory extends ServerSubTaskBase {
   @Override
   public void run() {
     checkParams();
-    String masterAddresses = getMasterAddresses();
+    // Check if secondary IP is present, and if so, use that for the gflag value.
+    String masterAddresses = getMasterAddresses(true);
     boolean isTserverTask = taskParams().serverType == ServerType.TSERVER;
     HostAndPort hp = getHostPort();
 
