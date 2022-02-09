00e0f2a615b1a5e78598c5fe8a4c3155e5ac43b8
Athul <athulsn2@gmail.com>
2022-02-09 18:31:50 +0530

    [PLAT-3021][Backport 2.12] Pre Requisite for LDAP UI & Migrate User Listing from Profile Page to User Management Tab
    Summary:
    Pre requisites needed for backporting LDAP changes ( for easy code review )
    
    Added styles needed for LDAP
    Added icons needed for LDAP
    Added new YBBanner component
    Added action and methods for fetching runtime config
    
    Test Plan:
    Tested manually
    
    Non breaking change
    
    Reviewers: cpadinjareveettil, ssutar, kkannan, lsangappa
    
    Reviewed By: kkannan, lsangappa
    
    Subscribers: jenkins-bot, ui
    
    Differential Revision: https://phabricator.dev.yugabyte.com/D15346

--- managed/ui/src/reducers/reducer_customer.js
+++ managed/ui/src/reducers/reducer_customer.js
@@ -44,6 +44,10 @@ import {
   FETCH_CUSTOMER_CONFIGS_RESPONSE,
   DELETE_CUSTOMER_CONFIG,
   DELETE_CUSTOMER_CONFIG_RESPONSE,
+  FETCH_RUNTIME_CONFIGS,
+  FETCH_RUNTIME_CONFIGS_RESPONSE,
+  SET_RUNTIME_CONFIG,
+  SET_RUNTIME_CONFIG_RESPONSE,
   GET_LOGS,
   GET_LOGS_SUCCESS,
   GET_LOGS_FAILURE,
@@ -378,6 +382,14 @@ export default function (state = INITIAL_STATE, action) {
       return setLoadingState(state, 'configs', []);
     case FETCH_CUSTOMER_CONFIGS_RESPONSE:
       return setPromiseResponse(state, 'configs', action);
+    case FETCH_RUNTIME_CONFIGS:
+      return setLoadingState(state, 'runtimeConfigs', []);
+    case FETCH_RUNTIME_CONFIGS_RESPONSE:
+      return setPromiseResponse(state, 'runtimeConfigs', action);
+    case SET_RUNTIME_CONFIG:
+      return setLoadingState(state, 'updateRuntimeConfig', {});
+    case SET_RUNTIME_CONFIG_RESPONSE:
+      return setPromiseResponse(state, 'updateRuntimeConfig', action);
     case DELETE_CUSTOMER_CONFIG:
       return setLoadingState(state, 'deleteConfig', {});
     case DELETE_CUSTOMER_CONFIG_RESPONSE:
