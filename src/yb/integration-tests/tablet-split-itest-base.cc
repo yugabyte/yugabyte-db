1ee4f1dfb4ac79982abf6d50f0e7e282102246f5
Anton Rybochkin <79331145+arybochkin@users.noreply.github.com>
2022-02-11 13:30:23 +0300

    [#10729] docdb: TabletSplitITest.TestInitiatesCompactionAfterSplit flaky in master
    Summary:
    Test may fail as it does not take into account a leader's re-election case during running of `GetTabletsBytesRead/GetInactiveTabletsBytesWritten` methods. For example, `GetTabletsBytesRead` gets only one peer from `ListTableActiveTabletLeadersPeers` if re-election is happening or finished recently.
    To fix the issue, new method `WaitForTableActiveTabletLeadersPeers` was implemented, and is called before `GetTabletsBytesRead/GetInactiveTabletsBytesWritten` to make sure leader's re-election is completed and expected number of tablets leaders is available.
    
    Test Plan:
    ybd --cxx-test integration-tests_tablet-split-itest --gtest_filter TabletSplitITest.TestInitiatesCompactionAfterSplit -n 1000
    
    To check code replaces with `WaitForTableActiveTabletLeadersPeers`:
    ybd --cxx-test integration-tests_tablet-split-itest --gtest_filter TabletSplitITest.SplitTabletDuringReadWriteLoad
    ybd --cxx-test integration-tests_tablet-split-itest --gtest_filter *TabletSplitITestWithIsolationLevel.SplitSingleTablet*
    ybd --cxx-test integration-tests_tablet-split-itest --gtest_filter TabletSplitITest.TestBackfillDuringSplit
    ybd --cxx-test integration-tests_tablet-split-itest --gtest_filter TabletSplitITest.TestSplitDuringBackfill
    ybd --cxx-test integration-tests_tablet-split-itest --gtest_filter TabletSplitITest.SplitDuringReplicaOffline
    
    Reviewers: rsami, timur
    
    Reviewed By: timur
    
    Subscribers: ybase
    
    Differential Revision: https://phabricator.dev.yugabyte.com/D15412

--- src/yb/integration-tests/tablet-split-itest-base.cc
+++ src/yb/integration-tests/tablet-split-itest-base.cc
@@ -618,50 +618,6 @@ Result<int> TabletSplitITest::NumPostSplitTabletPeersFullyCompacted() {
   return count;
 }
 
-Result<uint64_t> TabletSplitITest::GetActiveTabletsBytesRead() {
-  uint64_t read_bytes_1 = 0, read_bytes_2 = 0;
-  auto peers = ListTableActiveTabletLeadersPeers(
-      this->cluster_.get(), VERIFY_RESULT(GetTestTableId()));
-  for (auto peer : peers) {
-    auto this_peer_read_bytes = peer->tablet()->regulardb_statistics()->getTickerCount(
-        rocksdb::Tickers::COMPACT_READ_BYTES);
-    if (read_bytes_1 == 0) {
-      read_bytes_1 = this_peer_read_bytes;
-    } else if (read_bytes_2 == 0) {
-      read_bytes_2 = this_peer_read_bytes;
-    } else {
-      if (this_peer_read_bytes != read_bytes_1 && this_peer_read_bytes != read_bytes_2) {
-        return STATUS_FORMAT(IllegalState,
-            "Expected this peer's read bytes ($0) to equal one of the existing peer's read bytes "
-            "($1 or $2)",
-            this_peer_read_bytes, read_bytes_1, read_bytes_2);
-      }
-    }
-  }
-  if (read_bytes_1 <= 0 || read_bytes_2 <= 0) {
-    return STATUS_FORMAT(IllegalState,
-        "Peer's read bytes should be greater than zero. Found $0 and $1",
-        read_bytes_1, read_bytes_2);
-  }
-  return read_bytes_1 + read_bytes_2;
-}
-
-Result<uint64_t> TabletSplitITest::GetInactiveTabletsBytesWritten() {
-  uint64_t write_bytes = 0;
-  for (auto peer : VERIFY_RESULT(ListSplitCompleteTabletPeers())) {
-    auto this_peer_written_bytes = peer->tablet()->regulardb_statistics()->getTickerCount(
-        rocksdb::Tickers::COMPACT_WRITE_BYTES);
-    if (write_bytes == 0) write_bytes = this_peer_written_bytes;
-    if (write_bytes != this_peer_written_bytes) {
-      return STATUS_FORMAT(IllegalState,
-          "Expected the number of written bytes at each peer to be the same. Found one with $0 and "
-          "another with $1",
-          write_bytes, this_peer_written_bytes);
-    }
-  }
-  return write_bytes;
-}
-
 Result<uint64_t> TabletSplitITest::GetMinSstFileSizeAmongAllReplicas(const std::string& tablet_id) {
   const auto test_table_id = VERIFY_RESULT(GetTestTableId());
   auto peers = ListTabletPeers(this->cluster_.get(), [&tablet_id](auto peer) {
@@ -688,12 +644,8 @@ Status TabletSplitITest::CheckPostSplitTabletReplicasData(
   }
 
   const auto test_table_id = VERIFY_RESULT(GetTestTableId());
-  std::vector<tablet::TabletPeerPtr> active_leader_peers;
-  RETURN_NOT_OK(LoggedWaitFor([&] {
-    active_leader_peers = ListTableActiveTabletLeadersPeers(this->cluster_.get(), test_table_id);
-    LOG(INFO) << "active_leader_peers.size(): " << active_leader_peers.size();
-    return active_leader_peers.size() == num_active_tablets;
-  }, 30s * kTimeMultiplier, "Waiting for leaders ..."));
+  auto active_leader_peers = VERIFY_RESULT(WaitForTableActiveTabletLeadersPeers(
+      this->cluster_.get(), test_table_id, num_active_tablets));
 
   std::unordered_map<TabletId, OpId> last_on_leader;
   for (auto peer : active_leader_peers) {
