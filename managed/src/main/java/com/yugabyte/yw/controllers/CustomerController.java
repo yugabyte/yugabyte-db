f9dc3e0524ffceedcfd5e1d0774e22751be3a463
Hamidreza Zare <hzare@yugabyte.com>
2022-02-18 14:41:33 -0500

    [PLAT-3148] Add backend support for PLAT-3113
    Summary:
    Currently the metrics shown for replication lags is the maximum lags among all replication
    configs for every table. This diff adds backend support to select the metrics for one specific
    replication config.
    To use the added feature, the POST request to /customers/:cUUID/metrics should use the following body:
    
    ```
    {
        "start": [start time],
        "end": [end time],
        "nodePrefix": [Source Universe nodePrefix],
        "metrics": ["tserver_async_replication_lag_micros"],
        "xClusterConfigUuid": [replication UUID]
    }
    ```
    
    Test Plan:
    I took the following steps to test the code:
    * Created two xCluster config between two universes, one with two tables, and one with one table.
    * Ran the sample app on one table and two other custom workloads on the other two tables.
    * Updated the front-end code to use the added code and made sure that the correct graph is shown for each xCluster config.
    
    Reviewers: amalyshev, spotachev, muthu, nbhatia, nsingh
    
    Reviewed By: nsingh
    
    Subscribers: yugaware
    
    Differential Revision: https://phabricator.dev.yugabyte.com/D15568

--- managed/src/main/java/com/yugabyte/yw/controllers/CustomerController.java
+++ managed/src/main/java/com/yugabyte/yw/controllers/CustomerController.java
@@ -49,6 +49,7 @@ import com.yugabyte.yw.models.Provider;
 import com.yugabyte.yw.models.Region;
 import com.yugabyte.yw.models.Universe;
 import com.yugabyte.yw.models.Users;
+import com.yugabyte.yw.models.XClusterConfig;
 import com.yugabyte.yw.models.extended.UserWithFeatures;
 import com.yugabyte.yw.models.filters.AlertFilter;
 import com.yugabyte.yw.models.helpers.CommonUtils;
@@ -396,6 +397,12 @@ public class CustomerController extends AuthenticatedController {
     }
     if (params.containsKey("tableName")) {
       filterJson.put("table_name", params.remove("tableName"));
+    }
+    if (params.containsKey("xClusterConfigUuid")) {
+      XClusterConfig xClusterConfig =
+          XClusterConfig.getOrBadRequest(UUID.fromString(params.remove("xClusterConfigUuid")));
+      String tableIdRegex = String.join("|", xClusterConfig.getTables());
+      filterJson.put("table_id", tableIdRegex);
     }
     params.put("filters", Json.stringify(filterJson));
     JsonNode response;
