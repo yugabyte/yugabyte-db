4fa7894610ab1a85aee86b194bb6b13fbbe387ac
Kumar Vivek <kv83821@gmail.com>
2022-02-22 04:34:18 +0000

    [PLAT-3170] Filter backups on actionType field in listing
    Summary: - Added actionType filter in backups list paginated query
    
    Test Plan:
    - Verified only CREATE type actionType returned for old Backup APIs
    - Verified "null" type actionType returned for new Backup APIs
    
    Reviewers: vpatibandla
    
    Reviewed By: vpatibandla
    
    Subscribers: jenkins-bot, yugaware
    
    Differential Revision: https://phabricator.dev.yugabyte.com/D15588

--- managed/src/main/java/com/yugabyte/yw/common/BackupUtil.java
+++ managed/src/main/java/com/yugabyte/yw/common/BackupUtil.java
@@ -6,9 +6,12 @@ import com.cronutils.model.Cron;
 import com.cronutils.model.definition.CronDefinitionBuilder;
 import com.cronutils.model.time.ExecutionTime;
 import com.cronutils.parser.CronParser;
+import com.google.common.collect.Sets;
+import com.yugabyte.yw.forms.BackupTableParams.ActionType;
 import java.time.Duration;
 import java.time.Instant;
 import java.time.ZoneId;
+import java.util.Set;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -21,6 +24,8 @@ public class BackupUtil {
 
   public static final long MIN_SCHEDULE_DURATION_IN_SECS = 3600L;
   public static final long MIN_SCHEDULE_DURATION_IN_MILLIS = MIN_SCHEDULE_DURATION_IN_SECS * 1000L;
+  public static final Set<ActionType> OMIT_ACTION_TYPES =
+      Sets.immutableEnumSet(ActionType.DELETE, ActionType.RESTORE, ActionType.RESTORE_KEYS);
 
   public static void validateBackupCronExpression(String cronExpression)
       throws PlatformServiceException {
