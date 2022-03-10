d6daddfbf4c6f31c8cad029626dae586725ba886
Arnav Agarwal <14933889+Arnav15@users.noreply.github.com>
2022-03-09 16:16:38 -0800

    [PLAT-3328] Fix change master config for Dual Nic clusters
    Summary:
    During master config changes, the platform was setting the address to the primary one,
    which is used for platform communication, but not for DB node communications.
    
    Test Plan:
    1) Perform a full move
    2) Perform a node resize
    Verify the address changes are propagated correctly.
    Did this for both regular and dual NIC clusters.
    
    Reviewers: daniel, bogdan
    
    Reviewed By: bogdan
    
    Subscribers: jenkins-bot, yugaware
    
    Differential Revision: https://phabricator.dev.yugabyte.com/D15896

--- managed/src/main/java/com/yugabyte/yw/commissioner/tasks/subtasks/ChangeMasterConfig.java
+++ managed/src/main/java/com/yugabyte/yw/commissioner/tasks/subtasks/ChangeMasterConfig.java
@@ -102,7 +102,20 @@ public class ChangeMasterConfig extends AbstractTaskBase {
     YBClientService.Config config = new YBClientService.Config(masterAddresses, certificate);
     config.setAdminOperationTimeout(YBCLIENT_ADMIN_OPERATION_TIMEOUT);
     YBClient client = ybService.getClientWithConfig(config);
-    String ipToUse = node.cloudInfo.private_ip;
+
+    // If the cluster has a secondary IP, we want to ensure that we use the correct addresses.
+    // The ipToUse is the address that we need to add to the config.
+    // The ipForPlatform is the address that the platform uses to connect to the host.
+    boolean hasSecondaryIp =
+        node.cloudInfo.secondary_private_ip != null
+            && !node.cloudInfo.secondary_private_ip.equals("null");
+    boolean shouldUseSecondary =
+        universe.getConfig().getOrDefault(Universe.DUAL_NET_LEGACY, "true").equals("false");
+    String ipToUse =
+        hasSecondaryIp && shouldUseSecondary
+            ? node.cloudInfo.secondary_private_ip
+            : node.cloudInfo.private_ip;
+    String ipForPlatform = node.cloudInfo.private_ip;
     try {
       // The call changeMasterConfig is not idempotent. The client library internally keeps retrying
       // for a long time until it gives up if the node is already added or removed.
@@ -121,7 +134,7 @@ public class ChangeMasterConfig extends AbstractTaskBase {
       }
       response =
           client.changeMasterConfig(
-              ipToUse, node.masterRpcPort, isAddMasterOp, taskParams().useHostPort);
+              ipForPlatform, node.masterRpcPort, isAddMasterOp, taskParams().useHostPort, ipToUse);
     } catch (Exception e) {
       String msg =
           "Error "
