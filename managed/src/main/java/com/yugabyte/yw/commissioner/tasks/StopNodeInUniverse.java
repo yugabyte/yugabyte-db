62e99ec9b5184d735e1f9f6462017b5483262dce
Naorem Khogendro Singh <nsingh@yugabyte.com>
2022-03-15 21:59:28 -0700

    [PLAT-3378][Platform]Update TaskExecutor() for better usability in junit tests and [PLAT-3160][Platform]Task Improvement - Remove the deprecated SubTaskGroup and SubTaskGroupQueue to use directly use the task executor constructs.
    Summary:
    [PLAT-3378][Platform]Update TaskExecutor() for better usability in junit tests and [PLAT-3160][Platform]Task Improvement - Remove the deprecated SubTaskGroup and SubTaskGroupQueue to use directly use the task executor constructs.
    
    Both are related to clean up the old deprecated classes. reset() method in TaskExecutor is changed to not reset the subTaskPosition. So, any subtasks in finally block are appended to the existing set of tasks in the DB.
    
    Test Plan:
    The previous classes were just bridging between the new TaskExecutor and the old usage. So, existing unit tests + itest should work.
    
    I will run itest for sanity.
    
    Reviewers: amalyshev, spotachev
    
    Reviewed By: spotachev
    
    Subscribers: jenkins-bot, yugaware
    
    Differential Revision: https://phabricator.dev.yugabyte.com/D16018

--- managed/src/main/java/com/yugabyte/yw/commissioner/tasks/StopNodeInUniverse.java
+++ managed/src/main/java/com/yugabyte/yw/commissioner/tasks/StopNodeInUniverse.java
@@ -11,7 +11,6 @@
 package com.yugabyte.yw.commissioner.tasks;
 
 import com.yugabyte.yw.commissioner.BaseTaskDependencies;
-import com.yugabyte.yw.commissioner.SubTaskGroupQueue;
 import com.yugabyte.yw.commissioner.UserTaskDetails.SubTaskGroupType;
 import com.yugabyte.yw.commissioner.tasks.UniverseDefinitionTaskBase.ServerType;
 import com.yugabyte.yw.commissioner.tasks.params.NodeTaskParams;
@@ -51,8 +50,6 @@ public class StopNodeInUniverse extends UniverseTaskBase {
 
     try {
       checkUniverseVersion();
-      // Create the task list sequence.
-      subTaskGroupQueue = new SubTaskGroupQueue(userTaskUUID);
 
       // Set the 'updateInProgress' flag to prevent other updates from happening.
       Universe universe = lockUniverseForUpdate(taskParams().expectedUniverseVersion);
@@ -145,7 +142,7 @@ public class StopNodeInUniverse extends UniverseTaskBase {
       // Mark universe task state to success
       createMarkUniverseUpdateSuccessTasks().setSubTaskGroupType(SubTaskGroupType.StoppingNode);
 
-      subTaskGroupQueue.run();
+      getRunnableTask().runSubTasks();
     } catch (Throwable t) {
       log.error("Error executing task {}, error='{}'", getName(), t.getMessage(), t);
       hitException = true;
@@ -160,11 +157,12 @@ public class StopNodeInUniverse extends UniverseTaskBase {
         // remove leader blacklist for current node if task failed and leader blacklist is not
         // removed
         if (isBlacklistLeaders) {
-          subTaskGroupQueue = new SubTaskGroupQueue(userTaskUUID);
+          // Clear previous subtasks if any.
+          getRunnableTask().reset();
           createModifyBlackListTask(
                   Arrays.asList(currentNode), false /* isAdd */, true /* isLeaderBlacklist */)
               .setSubTaskGroupType(SubTaskGroupType.StoppingNodeProcesses);
-          subTaskGroupQueue.run();
+          getRunnableTask().runSubTasks();
         }
       } finally {
         unlockUniverseForUpdate();
