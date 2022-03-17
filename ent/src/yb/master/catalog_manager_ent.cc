387d4c96f494e843e4c26a9592786dd339296f9a
jhe <jhe@yugabyte.com>
2022-03-17 16:16:28 -0700

    [#11767] Backups: Fix restoring colocated db to db with the same id
    Summary: In the case that we restore a colocated db back into a db with the same id, then we go through a different code path in `CatalogManager::ImportTableEntry` that does not flip the `is_parent_colocated_table` flag. This is an issue later since the parent colocated table does not have any partition info related to it, so we detect partition mismatch and attempt to repartition the table when we shouldn't.
    
    Test Plan:
    Manual test:
    ```
    bin/yb-ctl wipe_restart
    ---
    create database coloc colocate = true;
    \c coloc
    create table test(i int primary key):
    insert into test values (1):
    ---
    take backup of ysql.coloc
    bin/yb-ctl wipe_restart
    restore backup and ensure that it passes
    ```
    
    ```
    ybd  --java-test "org.yb.pgsql.TestYbBackup#testColocatedDatabaseRestoreToOriginalDB"
    ```
    
    Reviewers: oleg, jason
    
    Reviewed By: jason
    
    Subscribers: yql, bogdan
    
    Differential Revision: https://phabricator.dev.yugabyte.com/D16024

--- ent/src/yb/master/catalog_manager_ent.cc
+++ ent/src/yb/master/catalog_manager_ent.cc
@@ -1492,6 +1492,10 @@ Status CatalogManager::ImportTableEntry(const NamespaceMap& namespace_map,
       SharedLock lock(mutex_);
       if (VERIFY_RESULT(CheckTableForImport(table, table_data))) {
         LOG_WITH_FUNC(INFO) << "Found existing table: '" << table->ToString() << "'";
+        if (meta.colocated() && IsColocatedParentTableId(table_data->old_table_id)) {
+          // Parent colocated tables don't have partition info, so make sure to mark them.
+          is_parent_colocated_table = true;
+        }
       } else {
         // A property did not match, so this search by ids failed.
         auto table_lock = table->LockForRead();
@@ -1751,7 +1755,11 @@ Status CatalogManager::PreprocessTabletEntry(const SysRowEntry& entry,
   SysTabletsEntryPB meta = VERIFY_RESULT(ParseFromSlice<SysTabletsEntryPB>(entry.data()));
 
   ExternalTableSnapshotData& table_data = (*table_map)[meta.table_id()];
-  ++table_data.num_tablets;
+  if (meta.colocated()) {
+    table_data.num_tablets = 1;
+  } else {
+    ++table_data.num_tablets;
+  }
   if (meta.has_partition()) {
     table_data.partitions.push_back(meta.partition());
   }
