7333ee718f7a45c5a29834e30c9b811687abccb9
Dmitry Uspenskiy <47734295+d-uspenskiy@users.noreply.github.com>
2022-04-02 21:27:15 +0300

    [#11165] YSQL: Detect conflict in case of INSERT from 2 SERIALIZABLE txns
    Summary:
    The D11239 / 83a150b08015a9eb98a11795bea45a1350019d29 introduces the optimization for UPDATE statement based on relaxing lock intent strength. After the change UPDATE of particular row takes `weak read` intent instead of `strong read` intent. This is achieved by creating `strong read` intent on row's `kLivenessColumn` column instead of row key itself.
    But this change introduces the bug that INSERT/UPSERT tries to create `strong read` intent on `kLivenessColumn` as well.
    Solution is to limit intent relaxing optimization for UPDATE operation only.
    
    Test Plan:
    Jenkins
    
    New unit tests were introduced
    
    ```
    ./yb_build.sh --gtest_filter PgMiniTest.DuplicateInsert*
    ./yb_build.sh --gtest_filter PgMiniTest.DuplicateUniqueIndexInsert*
    ./yb_build.sh --gtest_filter PgMiniTest.DuplicateNonUniqueIndexInsert*
    ```
    
    Reviewers: mbautin, sergei, pjain, mihnea
    
    Reviewed By: sergei, mihnea
    
    Subscribers: yql
    
    Differential Revision: https://phabricator.dev.yugabyte.com/D16278

--- src/yb/yql/pgwrapper/pg_mini-test.cc
+++ src/yb/yql/pgwrapper/pg_mini-test.cc
@@ -1171,8 +1171,7 @@ class PgMiniTestTxnHelper : public PgMiniTestNoTxnRetry {
   }
 
   void TestInOperatorLock() {
-    auto conn = ASSERT_RESULT(Connect());
-    ASSERT_OK(conn.Execute("SET yb_transaction_priority_lower_bound = 0.9"));
+    auto conn = ASSERT_RESULT(SetHighPriTxn(Connect()));
     ASSERT_OK(conn.Execute(
         "CREATE TABLE t (h INT, r1 INT, r2 INT, PRIMARY KEY(h, r1 ASC, r2 ASC))"));
     ASSERT_OK(conn.Execute(
@@ -1181,12 +1180,11 @@ class PgMiniTestTxnHelper : public PgMiniTestNoTxnRetry {
     auto res = ASSERT_RESULT(conn.Fetch(
         "SELECT * FROM t WHERE h = 1 AND r1 IN (11, 12) AND r2 = 1 FOR KEY SHARE"));
 
-    auto extra_conn = ASSERT_RESULT(Connect());
-    ASSERT_OK(extra_conn.Execute("SET yb_transaction_priority_upper_bound = 0.1"));
-    ASSERT_OK(extra_conn.Execute("BEGIN"));
+    auto extra_conn = ASSERT_RESULT(SetLowPriTxn(Connect()));
+    ASSERT_OK(StartTxn(&extra_conn));
     ASSERT_NOK(extra_conn.Execute("DELETE FROM t WHERE h = 1 AND r1 = 11 AND r2 = 1"));
     ASSERT_OK(extra_conn.Execute("ROLLBACK"));
-    ASSERT_OK(extra_conn.Execute("BEGIN"));
+    ASSERT_OK(StartTxn(&extra_conn));
     ASSERT_OK(extra_conn.Execute("DELETE FROM t WHERE h = 1 AND r1 = 13 AND r2 = 1"));
     ASSERT_OK(extra_conn.Execute("COMMIT"));
 
@@ -1196,19 +1194,36 @@ class PgMiniTestTxnHelper : public PgMiniTestNoTxnRetry {
     res = ASSERT_RESULT(conn.Fetch(
         "SELECT * FROM t WHERE h IN (1, 2) AND r1 = 11 FOR KEY SHARE"));
 
-    ASSERT_OK(extra_conn.Execute("BEGIN"));
+    ASSERT_OK(StartTxn(&extra_conn));
     ASSERT_NOK(extra_conn.Execute("DELETE FROM t WHERE h = 1 AND r1 = 11 AND r2 = 1"));
     ASSERT_OK(extra_conn.Execute("ROLLBACK"));
-    ASSERT_OK(extra_conn.Execute("BEGIN"));
+    ASSERT_OK(StartTxn(&extra_conn));
     ASSERT_NOK(extra_conn.Execute("DELETE FROM t WHERE h = 2 AND r1 = 11 AND r2 = 2"));
     ASSERT_OK(extra_conn.Execute("ROLLBACK"));
-    ASSERT_OK(extra_conn.Execute("BEGIN"));
+    ASSERT_OK(StartTxn(&extra_conn));
     ASSERT_OK(extra_conn.Execute("DELETE FROM t WHERE h = 2 AND r1 = 12 AND r2 = 2"));
     ASSERT_OK(extra_conn.Execute("COMMIT"));
 
     ASSERT_OK(conn.Execute("COMMIT;"));
     const auto count = ASSERT_RESULT(conn.template FetchValue<int64_t>("SELECT COUNT(*) FROM t"));
     ASSERT_EQ(4, count);
+  }
+
+  // Check that 2 rows with same PK can't be inserted from different txns.
+  void TestDuplicateInsert() {
+    DuplicateInsertImpl(IndexRequirement::NO, true /* low_pri_txn_insert_same_key */);
+  }
+
+  // Check that 2 rows with identical unique index can't be inserted from different txns.
+  void TestDuplicateUniqueIndexInsert() {
+    DuplicateInsertImpl(IndexRequirement::UNIQUE, false /* low_pri_txn_insert_same_key */);
+  }
+
+  // Check that 2 rows with identical non-unique index can be inserted from different txns.
+  void TestDuplicateNonUniqueIndexInsert() {
+    DuplicateInsertImpl(IndexRequirement::NON_UNIQUE,
+    false /* low_pri_txn_insert_same_key */,
+    true /* low_pri_txn_succeed */);
   }
 
   static Result<PGConn> SetHighPriTxn(Result<PGConn> connection) {
@@ -1236,6 +1251,39 @@ class PgMiniTestTxnHelper : public PgMiniTestNoTxnRetry {
 
   static Result<PGResultPtr> FetchInTxn(PGConn* connection, const std::string& query) {
     return TxnHelper<level>::FetchInTxn(connection, query);
+  }
+
+ private:
+  enum class IndexRequirement {
+    NO,
+    UNIQUE,
+    NON_UNIQUE
+  };
+
+  void DuplicateInsertImpl(
+    IndexRequirement index, bool low_pri_txn_insert_same_key, bool low_pri_txn_succeed = false) {
+    auto conn = ASSERT_RESULT(SetHighPriTxn(Connect()));
+    ASSERT_OK(conn.Execute("CREATE TABLE t (k INT PRIMARY KEY, v INT)"));
+    if (index != IndexRequirement::NO) {
+      ASSERT_OK(conn.Execute(Format("CREATE $0 INDEX ON t(v)",
+                                    index == IndexRequirement::UNIQUE ? "UNIQUE" : "")));
+    }
+    ASSERT_OK(StartTxn(&conn));
+    auto extra_conn = ASSERT_RESULT(SetLowPriTxn(Connect()));
+    ASSERT_OK(StartTxn(&extra_conn));
+    ASSERT_OK(extra_conn.Execute(Format("INSERT INTO t VALUES($0, 10)",
+                                        low_pri_txn_insert_same_key ? "1" : "2")));
+    ASSERT_OK(conn.Execute("INSERT INTO t VALUES(1, 10)"));
+    ASSERT_OK(conn.Execute("COMMIT"));
+    const auto low_pri_txn_commit_status = extra_conn.Execute("COMMIT");
+    if (low_pri_txn_succeed) {
+      ASSERT_OK(low_pri_txn_commit_status);
+    } else {
+      ASSERT_NOK(low_pri_txn_commit_status);
+    }
+    const auto count = ASSERT_RESULT(
+      extra_conn.template FetchValue<int64_t>("SELECT COUNT(*) FROM t WHERE v = 10"));
+    ASSERT_EQ(low_pri_txn_succeed ? 2 : 1, count);
   }
 };
 
@@ -1367,6 +1415,42 @@ TEST_F_EX(PgMiniTest,
           YB_DISABLE_TEST_IN_TSAN(SameColumnUpdateSnapshot),
           PgMiniTestTxnHelperSnapshot) {
   TestSameColumnUpdate();
+}
+
+TEST_F_EX(PgMiniTest,
+          YB_DISABLE_TEST_IN_TSAN(DuplicateInsertSerializable),
+          PgMiniTestTxnHelperSerializable) {
+  TestDuplicateInsert();
+}
+
+TEST_F_EX(PgMiniTest,
+          YB_DISABLE_TEST_IN_TSAN(DuplicateInsertSnapshot),
+          PgMiniTestTxnHelperSnapshot) {
+  TestDuplicateInsert();
+}
+
+TEST_F_EX(PgMiniTest,
+          YB_DISABLE_TEST_IN_TSAN(DuplicateUniqueIndexInsertSerializable),
+          PgMiniTestTxnHelperSerializable) {
+  TestDuplicateUniqueIndexInsert();
+}
+
+TEST_F_EX(PgMiniTest,
+          YB_DISABLE_TEST_IN_TSAN(DuplicateUniqueIndexInsertSnapshot),
+          PgMiniTestTxnHelperSnapshot) {
+  TestDuplicateUniqueIndexInsert();
+}
+
+TEST_F_EX(PgMiniTest,
+          YB_DISABLE_TEST_IN_TSAN(DuplicateNonUniqueIndexInsertSerializable),
+          PgMiniTestTxnHelperSerializable) {
+  TestDuplicateNonUniqueIndexInsert();
+}
+
+TEST_F_EX(PgMiniTest,
+          YB_DISABLE_TEST_IN_TSAN(DuplicateNonUniqueIndexInsertSnapshot),
+          PgMiniTestTxnHelperSnapshot) {
+  TestDuplicateNonUniqueIndexInsert();
 }
 
 // ------------------------------------------------------------------------------------------------
