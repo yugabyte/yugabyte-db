9f6ce87f3c847843657dc31e8f644e15b9cc7104
Ted Yu <yuzhihong@gmail.com>
2022-04-04 13:00:37 -0700

    [#11887] YSQL: Include the index table Id when ybctid DocKey isn't found in main table
    Summary:
    Commit 7793ec81151716d4834ed5d235c64aea9859cd4a / D16204 introduced an if statement checking whether table Id is present for the log statement.
    
    As Dmitry pointed out, the table Id is present for the index request.
    
    This revision removes the if statement - the table Id is directly added to the log statement.
    
    Test Plan:
    New test case is not included in the revision because there is no unit test precedent on data corruption.
    
    Build yugabyte db and run test suite via Jenkins
    
    Reviewers: mihnea, amartsinchyk
    
    Reviewed By: amartsinchyk
    
    Subscribers: yql
    
    Differential Revision: https://phabricator.dev.yugabyte.com/D16355

--- src/yb/docdb/pgsql_operation.cc
+++ src/yb/docdb/pgsql_operation.cc
@@ -1013,15 +1013,11 @@ Result<size_t> PgsqlReadOperation::ExecuteScalar(const YQLStorageIf& ql_storage,
       if (!VERIFY_RESULT(table_iter_->SeekTuple(tuple_id->binary_value()))) {
         DocKey doc_key;
         RETURN_NOT_OK(doc_key.DecodeFrom(tuple_id->binary_value()));
-        const PgsqlReadRequestPB& index_request = request_.index_request();
-        if (index_request.has_table_id()) {
-          return STATUS_FORMAT(
+        return STATUS_FORMAT(
             Corruption,
             "ybctid $0 not found in indexed table. index table id is $1",
             doc_key,
-            index_request.table_id());
-        }
-        return STATUS_FORMAT(Corruption, "ybctid $0 not found in indexed table", doc_key);
+            request_.index_request().table_id());
       }
       row.Clear();
       RETURN_NOT_OK(table_iter_->NextRow(projection, &row));
