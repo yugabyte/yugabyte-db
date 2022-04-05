3c97e8f75c76c35a2c0a4626ac485f7dca2ead84
SB YB <sbapat@yugabyte.com>
2022-04-04 21:04:17 -0700

    [BACKPORT 2.12][PLAT-4422] Ability to add c6 instance types
    Summary:
    [PLAT-2720] Improved logging and renames
    minor refactor
    
    Test Plan:
    unit test
    
    # Tip: In Git and Mercurial, use a branch like "T123" to automatically associate
    # changes with the corresponding task.
    
    Reviewers: nbhatia, muthu
    
    Reviewed By: muthu
    
    Subscribers: jenkins-bot, yugaware
    
    Differential Revision: https://phabricator.dev.yugabyte.com/D18186

--- managed/src/main/java/com/yugabyte/yw/controllers/InstanceTypeController.java
+++ managed/src/main/java/com/yugabyte/yw/controllers/InstanceTypeController.java
@@ -91,7 +91,7 @@ public class InstanceTypeController extends AuthenticatedController {
                 configHelper,
                 runtimeConfigFactory
                     .forProvider(provider)
-                    .getBoolean("yb.internal.allow_instances"))
+                    .getBoolean("yb.internal.allow_unsupported_instances"))
             .stream()
             .collect(toMap(InstanceType::getInstanceTypeCode, identity()));
 
