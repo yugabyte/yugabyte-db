1c6778f29f111a5ba8ac9039c07325464f9ed041
Dennis Felsing <dennis@felsin9.de>
2022-04-05 17:33:25 +0200

    [#11982] Prevent scope misuage in some more cases
    Test Plan: Build
    
    Reviewers: sergei, asrivastava
    
    Reviewed By: asrivastava
    
    Subscribers: ybase, bogdan
    
    Differential Revision: https://phabricator.dev.yugabyte.com/D16366

--- src/yb/tserver/tablet_service.cc
+++ src/yb/tserver/tablet_service.cc
@@ -104,6 +104,7 @@
 #include "yb/util/status.h"
 #include "yb/util/status_callback.h"
 #include "yb/util/status_format.h"
+#include "yb/util/status_fwd.h"
 #include "yb/util/status_log.h"
 #include "yb/util/string_util.h"
 #include "yb/util/trace.h"
@@ -1866,7 +1867,7 @@ void ConsensusServiceImpl::GetNodeInstance(const GetNodeInstanceRequestPB* req,
 
 namespace {
 
-class RpcScope {
+class NODISCARD_CLASS RpcScope {
  public:
   template<class Req, class Resp>
   RpcScope(TabletPeerLookupIf* tablet_manager,
