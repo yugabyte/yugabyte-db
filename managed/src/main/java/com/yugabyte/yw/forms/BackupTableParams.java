1639a8c41658403bab249d08c6eee352746468aa
Kumar Vivek <kv83821@gmail.com>
2022-04-07 07:37:53 +0000

    [PLAT-3599] Add ability to distinguish between full universe backup and specific backups
    Summary: - Added a boolean denoting whether the backup was full-table type/partial
    
    Test Plan: - Verified the boolean was returned correctly in the List response
    
    Reviewers: vpatibandla, vbansal
    
    Reviewed By: vbansal
    
    Subscribers: jenkins-bot, yugaware
    
    Differential Revision: https://phabricator.dev.yugabyte.com/D16422

--- managed/src/main/java/com/yugabyte/yw/forms/BackupTableParams.java
+++ managed/src/main/java/com/yugabyte/yw/forms/BackupTableParams.java
@@ -39,6 +39,9 @@ public class BackupTableParams extends TableManagerParams {
   @ApiModelProperty(value = "Action type")
   public ActionType actionType;
 
+  @ApiModelProperty(value = "Full Table type backup")
+  public boolean isFullBackup = false;
+
   @ApiModelProperty(value = "Backup type")
   public TableType backupType;
 
