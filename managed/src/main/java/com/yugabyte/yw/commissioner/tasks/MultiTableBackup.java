e9f11f7b4d7e428724d8f60faad27eed9191a061
Naorem Khogendro Singh <nsingh@yugabyte.com>
2022-04-11 15:45:02 -0700

    [BACKPORT 2.13.1][PLAT-3646][Platform] Backup failure doesn't update task and backup state correctly
    Summary:
    Original diff - https://phabricator.dev.yugabyte.com/D16477
    
    The real reason for failure is due to version mismatch.
    
    But, whatever be the reason, backup task should just fail and release the universe for other tasks.
    
    This is similar to this old change https://phabricator.dev.yugabyte.com/D13830 and https://phabricator.dev.yugabyte.com/D14311
    
    After the backup completes, unlocking does not try to increment the version in both platform and DB.
    
    Test Plan:
    As there is no itest, I ran a few backup tests manually by temporary changing the below code to fake HA setup to increment version.
    
     protected boolean shouldIncrementVersion() {
    
        if (!HighAvailabilityConfig.get().isPresent()) {
          return false;
        }
    ....
    
    to
    
     protected boolean shouldIncrementVersion() {
    
        if (!HighAvailabilityConfig.get().isPresent()) {
          return true;
        }
     ...
    
    Reviewers: vpatibandla, hkandala, sanketh
    
    Reviewed By: sanketh
    
    Subscribers: yugaware, jenkins-bot
    
    Differential Revision: https://phabricator.dev.yugabyte.com/D16591

--- managed/src/main/java/com/yugabyte/yw/commissioner/tasks/MultiTableBackup.java
+++ managed/src/main/java/com/yugabyte/yw/commissioner/tasks/MultiTableBackup.java
@@ -18,7 +18,6 @@ import static com.yugabyte.yw.commissioner.tasks.BackupUniverse.SCHEDULED_BACKUP
 import static com.yugabyte.yw.commissioner.tasks.BackupUniverse.SCHEDULED_BACKUP_SUCCESS_COUNTER;
 import static com.yugabyte.yw.common.Util.SYSTEM_PLATFORM_DB;
 import static com.yugabyte.yw.common.Util.getUUIDRepresentation;
-import static com.yugabyte.yw.common.Util.lockedUpdateBackupState;
 import static com.yugabyte.yw.common.metrics.MetricService.buildMetricTemplate;
 
 import com.fasterxml.jackson.databind.JsonNode;
@@ -96,7 +95,7 @@ public class MultiTableBackup extends UniverseTaskBase {
 
       // Update universe 'backupInProgress' flag to true or throw an exception if universe is
       // already having a backup in progress.
-      lockedUpdateBackupState(params().universeUUID, this, true);
+      lockedUpdateBackupState(true);
       try {
         String masterAddresses = universe.getMasterAddresses(true);
         String certificate = universe.getCertificateNodetoNode();
@@ -330,7 +329,7 @@ public class MultiTableBackup extends UniverseTaskBase {
         }
         throw t;
       } finally {
-        lockedUpdateBackupState(params().universeUUID, this, false);
+        lockedUpdateBackupState(false);
       }
     } catch (Throwable t) {
       log.error("Error executing task {} with error='{}'.", getName(), t.getMessage(), t);
