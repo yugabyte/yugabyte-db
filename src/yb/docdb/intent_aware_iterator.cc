861a290bc8a2cbbfddb347f06458bc4cefbb983b
Amitanand Aiyer <amitanandaiyer@users.noreply.github.com>
2022-04-14 10:14:48 -0700

    [#12161] DocDb: Jepsen fix. Revert D13877 completely.
    Summary:
    It is reported that Jepsen failures are happening since https://phabricator.dev.yugabyte.com/D13877 / https://github.com/yugabyte/yugabyte-db/commit/f5733cfd236b846c1d8095a568bae96f768c13f0 has landed.
    
    Some of the changes were reverted in https://phabricator.dev.yugabyte.com/D16341
    https://github.com/yugabyte/yugabyte-db/commit/93dc191376bc54f234812c0d976514895907a952 which fixes #11516.
    
     However on further testing we see issue #12161
    
    This diff Reverts all changes to src/yb/docdb/intent_aware_iterator.cc
    made in D13877.
    
    Test Plan:
    Run jepsen with the diff.
    
    So far (30 runs) jepsen has been passing while testing with
    `TEST_inject_sleep_before_applying_intents_ms=50` for #12161
    
    Reviewers: sergei, bogdan, rthallam
    
    Reviewed By: rthallam
    
    Subscribers: rsami, pjain, rthallam, ybase
    
    Differential Revision: https://phabricator.dev.yugabyte.com/D16550

--- src/yb/docdb/intent_aware_iterator.cc
+++ src/yb/docdb/intent_aware_iterator.cc
@@ -147,7 +147,6 @@ std::ostream& operator<<(std::ostream& out, const DecodeStrongWriteIntentResult&
 // For current transaction returns intent record hybrid time as value_time.
 // Consumes intent from value_slice leaving only value itself.
 Result<DecodeStrongWriteIntentResult> DecodeStrongWriteIntent(
-    HybridTime global_limit,
     const TransactionOperationContext& txn_op_context,
     rocksdb::Iterator* intent_iter,
     TransactionStatusCache* transaction_status_cache) {
@@ -180,9 +179,6 @@ Result<DecodeStrongWriteIntentResult> DecodeStrongWriteIntent(
       } else {
         result.value_time = decoded_intent_key.doc_ht;
       }
-    } else if (result.intent_time.hybrid_time() > global_limit) {
-      VTRACE(1, "Ignoring intent from a different txn written after read.global_limit");
-      result.value_time = DocHybridTime::kMin;
     } else {
       auto commit_data = VERIFY_RESULT(transaction_status_cache->GetCommitData(decoded_txn_id));
       auto commit_ht = commit_data.commit_ht;
@@ -650,7 +646,7 @@ bool IntentAwareIterator::SatisfyBounds(const Slice& slice) {
 
 void IntentAwareIterator::ProcessIntent() {
   auto decode_result = DecodeStrongWriteIntent(
-      read_time_.global_limit, txn_op_context_, &intent_iter_, &transaction_status_cache_);
+      txn_op_context_, &intent_iter_, &transaction_status_cache_);
   if (!decode_result.ok()) {
     status_ = decode_result.status();
     return;
