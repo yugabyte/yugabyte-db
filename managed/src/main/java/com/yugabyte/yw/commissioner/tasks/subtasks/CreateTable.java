d4293ae552570a67e630ac354f87c27b6e116c03
Jagdish Shinde <jshinde@yugabyte.com>
2022-04-14 06:26:09 +0000

    [PLAT-3579] Platform side API for fetching tablespaces and their placements
    Summary:
    For Geo Partitioning functionality in YB cloud, tablespaces and their placements are
    required in UI. This PR exposes an API for fetching this information.
    
    Merge branch 'PLAT-3579' into PLAT-3579_1
    
    Test Plan:
    Testing via Platform UI and UT
    
    Testing via Platform UI(Manual) and UT done.
    
    Reviewers: arnav, sb-yb, spotachev
    
    Reviewed By: spotachev
    
    Subscribers: jenkins-bot, yugaware
    
    Differential Revision: https://phabricator.dev.yugabyte.com/D16544

--- managed/src/main/java/com/yugabyte/yw/commissioner/tasks/subtasks/CreateTable.java
+++ managed/src/main/java/com/yugabyte/yw/commissioner/tasks/subtasks/CreateTable.java
@@ -21,6 +21,7 @@ import com.yugabyte.yw.common.Util;
 import com.yugabyte.yw.forms.UniverseDefinitionTaskParams;
 import com.yugabyte.yw.forms.UniverseTaskParams;
 import com.yugabyte.yw.models.Universe;
+import com.yugabyte.yw.models.helpers.CommonUtils;
 import com.yugabyte.yw.models.helpers.NodeDetails;
 import com.yugabyte.yw.models.helpers.NodeDetails.NodeState;
 import com.yugabyte.yw.models.helpers.TableDetails;
@@ -34,6 +35,7 @@ import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 import javax.inject.Inject;
 import lombok.extern.slf4j.Slf4j;
+
 import org.apache.commons.lang3.StringUtils;
 import org.yb.CommonTypes.TableType;
 import org.yb.client.YBClient;
@@ -86,28 +88,14 @@ public class CreateTable extends AbstractTaskBase {
     }
     TableDetails tableDetails = taskParams().tableDetails;
     Universe universe = Universe.getOrBadRequest(taskParams().universeUUID);
-    UniverseDefinitionTaskParams.Cluster primaryCluster =
-        universe.getUniverseDetails().getPrimaryCluster();
 
     String createTableStatement = tableDetails.getPgSqlCreateTableString(taskParams().ifNotExist);
-    List<NodeDetails> tserverLiveNodes =
-        universe
-            .getUniverseDetails()
-            .getNodesInCluster(primaryCluster.uuid)
-            .stream()
-            .filter(nodeDetails -> nodeDetails.isTserver)
-            .filter(nodeDetails -> nodeDetails.state == NodeState.Live)
-            .collect(Collectors.toList());
-    if (tserverLiveNodes.isEmpty()) {
-      throw new IllegalStateException(
-          "No live TServers for a table creation op in " + taskParams().universeUUID);
-    }
+
     boolean tableCreated = false;
-    Random random = new Random();
     int attempt = 0;
     Instant timeout = Instant.now().plusSeconds(MAX_TIMEOUT_SEC);
     while (Instant.now().isBefore(timeout) || attempt < 2) {
-      NodeDetails randomTServer = tserverLiveNodes.get(random.nextInt(tserverLiveNodes.size()));
+      NodeDetails randomTServer = CommonUtils.getARandomLiveTServer(universe);
       ShellResponse response =
           nodeUniverseManager.runYsqlCommand(
               randomTServer, universe, tableDetails.keyspace, createTableStatement);
