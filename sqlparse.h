3c6390a01325cb7edddb75de5b42219c01f14edc
Noriyoshi Shinoda <noriyoshi.shinoda@hpe.com>
2022-05-02 15:42:32 +0900

    Restore sqlparse.c and sqlparse.h

--- sqlparse.h
+++ sqlparse.h
@@ -1,8 +1,9 @@
-/* A Bison parser, made by GNU Bison 3.0.4.  */
+/* A Bison parser, made by GNU Bison 3.7.4.  */
 
 /* Bison interface for Yacc-like parsers in C
 
-   Copyright (C) 1984, 1989-1990, 2000-2015 Free Software Foundation, Inc.
+   Copyright (C) 1984, 1989-1990, 2000-2015, 2018-2020 Free Software Foundation,
+   Inc.
 
    This program is free software: you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
@@ -30,6 +31,10 @@
    This special exception was added by the Free Software Foundation in
    version 2.2 of Bison.  */
 
+/* DO NOT RELY ON FEATURES THAT ARE NOT DOCUMENTED in the manual,
+   especially those whose name start with YY_ or yy_.  They are
+   private implementation details that can be changed or removed.  */
+
 #ifndef YY_ORAFCE_SQL_YY_SQLPARSE_H_INCLUDED
 # define YY_ORAFCE_SQL_YY_SQLPARSE_H_INCLUDED
 /* Debug traces.  */
@@ -40,30 +45,34 @@
 extern int orafce_sql_yydebug;
 #endif
 
-/* Token type.  */
+/* Token kinds.  */
 #ifndef YYTOKENTYPE
 # define YYTOKENTYPE
   enum yytokentype
   {
-    X_IDENT = 258,
-    X_NCONST = 259,
-    X_SCONST = 260,
-    X_OP = 261,
-    X_PARAM = 262,
-    X_COMMENT = 263,
-    X_WHITESPACE = 264,
-    X_KEYWORD = 265,
-    X_OTHERS = 266,
-    X_TYPECAST = 267
+    YYEMPTY = -2,
+    YYEOF = 0,                     /* "end of file"  */
+    YYerror = 256,                 /* error  */
+    YYUNDEF = 257,                 /* "invalid token"  */
+    X_IDENT = 258,                 /* X_IDENT  */
+    X_NCONST = 259,                /* X_NCONST  */
+    X_SCONST = 260,                /* X_SCONST  */
+    X_OP = 261,                    /* X_OP  */
+    X_PARAM = 262,                 /* X_PARAM  */
+    X_COMMENT = 263,               /* X_COMMENT  */
+    X_WHITESPACE = 264,            /* X_WHITESPACE  */
+    X_KEYWORD = 265,               /* X_KEYWORD  */
+    X_OTHERS = 266,                /* X_OTHERS  */
+    X_TYPECAST = 267               /* X_TYPECAST  */
   };
+  typedef enum yytokentype yytoken_kind_t;
 #endif
 
 /* Value type.  */
 #if ! defined YYSTYPE && ! defined YYSTYPE_IS_DECLARED
-
 union YYSTYPE
 {
-#line 63 "sqlparse.y" /* yacc.c:1909  */
+#line 63 "sqlparse.y"
 
 	int 	ival;
 	orafce_lexnode	*node;
@@ -77,9 +86,9 @@ union YYSTYPE
 		char *modificator;
 	}				val;
 
-#line 81 "sqlparse.h" /* yacc.c:1909  */
-};
+#line 90 "sqlparse.h"
 
+};
 typedef union YYSTYPE YYSTYPE;
 # define YYSTYPE_IS_TRIVIAL 1
 # define YYSTYPE_IS_DECLARED 1
