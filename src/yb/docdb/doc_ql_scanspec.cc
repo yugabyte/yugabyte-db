99aa9a8e9e263a7e2b1e0621fafe8c0a221407aa
Tanuj Nayak <tanujnay112@live.com>
2022-05-12 02:23:32 -0700

    [#12481] DocDB: Fix null scan bound conversion in scanspecs
    Summary:
    Before this change, the scanspecs were indiscriminately using `FromQLValuePB` to convert from a `QLValuePB` to a `KeyEntryValue`/`PrimitiveValue`. This method converts nulls that were represented by a `QLValuePB` whose type is `VALUE_NOT_SET`. `FromQLValuePB` converts these objects into a `KeyEntryValue`/`PrimitiveValue` with type `kTombstone` which cannot be used in DocKeys. This later results in problems down the stack such as a crash in DocRowwiseIterator when it improperly tries to use the tombstone as a scan bound.
    
    The proper thing to do when producing values for keys is to check whether or not the `QLValuePB` represents a null value and transform to `kNullLow`/`kNullHigh` accordingly. This change introduces a new method called `FromQLValuePBForKey` to encapsulate this logic.
    
    There are numerous places in the code that either follow the pattern or should be following the pattern of checking for a `QLValuePB` being null before invoking `FromQLValuePB` on it. These should technically be using the new `FromQLValuePBForKey` method but this will be done in a later change.
    
    Test Plan:
    ./yb_build.sh release --java-test org.yb.cql.TestIndexSelection
    ./yb_build.sh release --java-test org.yb.pgsql.TestPgRegressIndex
    
    Reviewers: rsami, mihnea
    
    Reviewed By: mihnea
    
    Subscribers: bogdan, yql
    
    Differential Revision: https://phabricator.dev.yugabyte.com/D16927

--- src/yb/docdb/doc_ql_scanspec.cc
+++ src/yb/docdb/doc_ql_scanspec.cc
@@ -132,7 +132,7 @@ void DocQLScanSpec::InitRangeOptions(const QLConditionPB& condition) {
       range_options_indexes_.emplace_back(condition.operands(0).column_id());
 
       if (condition.op() == QL_OP_EQUAL) {
-        auto pv = KeyEntryValue::FromQLValuePB(condition.operands(1).value(), sortingType);
+        auto pv = KeyEntryValue::FromQLValuePBForKey(condition.operands(1).value(), sortingType);
         (*range_options_)[col_idx - num_hash_cols].push_back(std::move(pv));
       } else { // QL_OP_IN
         DCHECK_EQ(condition.op(), QL_OP_IN);
@@ -146,7 +146,7 @@ void DocQLScanSpec::InitRangeOptions(const QLConditionPB& condition) {
         for (int i = 0; i < opt_size; i++) {
           int elem_idx = is_reverse_order ? opt_size - i - 1 : i;
           const auto &elem = options.elems(elem_idx);
-          auto pv = KeyEntryValue::FromQLValuePB(elem, sortingType);
+          auto pv = KeyEntryValue::FromQLValuePBForKey(elem, sortingType);
           (*range_options_)[col_idx - num_hash_cols].push_back(std::move(pv));
         }
       }
