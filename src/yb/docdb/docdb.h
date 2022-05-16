eb6a860202c2d79994d7ac90e8f40ff1702150d1
Rahul Desirazu <rahuldesirazu@users.noreply.github.com>
2022-05-16 17:29:33 -0400

    [#12554] [xCluster] Fix Replication for For Transactions Spanning Multiple Write Batches
    Summary:
    When transactions are processed across multiple write batches/GetChanges calls, we see a data mismatch after transaction commit. This happens as a result of a bug on the target docdb side: For each row written to intentsdb, the format is `<txn_id, write_id> => key,value`, where write_id is an increasing counter to differentiate different rows of the same transaction. While write_id increases over a single write batch, it resets to 0 on the next write batch. Because we are writing to the same write_id across batches, we end up overwriting past rows written to intentsdb.
    
    Fix this by keeping a map of `txn_id -> write_id` at the target tablet layer and using that to increase write_id across write batches to be shared by all writes coming into the given tablet.
    
    Test Plan:
    ybd release --cxx-test twodc_ysql-test --gtest_filter *GenerateSeriesMultipleTransactions*
    ybd release --cxx-test twodc_ysql-test --gtest_filter *GenerateSeries*
    
    Reviewers: nicolas, jhe, sergei
    
    Reviewed By: sergei
    
    Subscribers: lnguyen, slingam, ybase
    
    Differential Revision: https://phabricator.dev.yugabyte.com/D17055

--- src/yb/docdb/docdb.h
+++ src/yb/docdb/docdb.h
@@ -147,15 +147,23 @@ struct ExternalTxnApplyStateData {
 
 using ExternalTxnApplyState = std::map<TransactionId, ExternalTxnApplyStateData>;
 
+class ExternalTxnIntentsState {
+ public:
+  IntraTxnWriteId GetWriteIdAndIncrement(const TransactionId& txn_id);
+  void EraseEntry(const TransactionId& txn_id);
+ private:
+  std::mutex mutex_;
+  std::unordered_map<TransactionId, IntraTxnWriteId, TransactionIdHash> map_;
+};
 // Adds external pair to write batch.
 // Returns true if add was skipped because pair is a regular (non external) record.
 bool AddExternalPairToWriteBatch(
     const KeyValuePairPB& kv_pair,
     HybridTime hybrid_time,
-    int write_id,
     ExternalTxnApplyState* apply_external_transactions,
     rocksdb::WriteBatch* regular_write_batch,
-    rocksdb::WriteBatch* intents_write_batch);
+    rocksdb::WriteBatch* intents_write_batch,
+    ExternalTxnIntentsState* external_txn_intents_state);
 
 // Prepares external part of non transaction write batch.
 // Batch could contain intents for external transactions, in this case those intents
@@ -167,7 +175,8 @@ bool PrepareExternalWriteBatch(
     HybridTime hybrid_time,
     rocksdb::DB* intents_db,
     rocksdb::WriteBatch* regular_write_batch,
-    rocksdb::WriteBatch* intents_write_batch);
+    rocksdb::WriteBatch* intents_write_batch,
+    ExternalTxnIntentsState* external_txn_intents_state);
 
 YB_STRONGLY_TYPED_BOOL(LastKey);
 
