1020230fd6e54c383d1621c0d97d8624c1b36e9d
Dmitry Uspenskiy <47734295+d-uspenskiy@users.noreply.github.com>
2022-05-25 08:44:20 +0300

    [#12327] YSQL: Workaround for FK constraint violation in case of dynamic table split
    Summary:
    In context of the change D16751 / 08591e17d50e14a73e9e3c7c545f32a302c21050 redundant sorting of ybctids (before calling of the `PgDocOp::PopulateDmlByYbctidOps` method) was removed. But this change highlighted the problem that in case ybctids is not sorted read result might be incomplete in case of dynamic table split. The issue https://github.com/yugabyte/yugabyte-db/issues/12648 is created to solve the problem and create unit tests.
    Current change restores code with ybctids sorting as a workaround to unblock TPC-C benchmark.
    
    Test Plan:
    Jenkins
    
    Unit test will be added withing the context of fix for https://github.com/yugabyte/yugabyte-db/issues/12648
    
    Reviewers: lnguyen
    
    Reviewed By: lnguyen
    
    Subscribers: smishra, yql
    
    Differential Revision: https://phabricator.dev.yugabyte.com/D17142

--- src/yb/yql/pggate/pggate.cc
+++ src/yb/yql/pggate/pggate.cc
@@ -251,7 +251,13 @@ Status FetchExistingYbctids(PgSession::ScopedRefPtr session,
                             uint64_t read_time) {
   // Group the items by the table ID.
   std::sort(ybctids->begin(), ybctids->end(), [](const auto& a, const auto& b) {
+    // TODO(dmitry): By design it is only necessary to group ybctids by table, sorting of ybctids
+    // itself is not required. But due to problem described in #12648 unsorted ybctids may produce
+    // incomplete result. Remove ybctid comparision once #12648 is fixed.
+    if (a.table_id != b.table_id) {
       return a.table_id < b.table_id;
+    }
+    return a.ybctid < b.ybctid;
   });
 
   auto arena = std::make_shared<Arena>();
