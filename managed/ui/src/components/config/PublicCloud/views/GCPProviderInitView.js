c1e89f0d0b916209f65613a7e3e55d17f37241aa
kkannan <kkannan@yugabyte.com>
2022-05-30 11:49:25 +0530

    [PLAT-4140][Platform][2.14]Remove YB beta for NTP
    Summary: Removed YBBeta tag from ntp servers
    
    Test Plan: Tested Manually
    
    Reviewers: lsangappa, ssutar, asathyan, cpadinjareveettil, mjoshi
    
    Reviewed By: asathyan, cpadinjareveettil, mjoshi
    
    Subscribers: jenkins-bot, ui
    
    Differential Revision: https://phabricator.dev.yugabyte.com/D17241

--- managed/ui/src/components/config/PublicCloud/views/GCPProviderInitView.js
+++ managed/ui/src/components/config/PublicCloud/views/GCPProviderInitView.js
@@ -17,7 +17,6 @@ import { isNonEmptyObject, isNonEmptyString, trimString } from '../../../../util
 import { reduxForm, FieldArray } from 'redux-form';
 import { FlexContainer, FlexGrow, FlexShrink } from '../../../common/flexbox/YBFlexBox';
 import { NTPConfig, NTP_TYPES } from './NTPConfig';
-import { YBTag } from '../../../common/YBTag';
 
 const validationIsRequired = (value) => (value && value.trim() !== '' ? undefined : 'Required');
 
@@ -359,7 +358,7 @@ class GCPProviderInitView extends Component {
                 {regionInput}
               <Row>
                 <Col lg={3}>
-                  <div className="form-item-custom-label">NTP Setup<YBTag>Beta</YBTag></div>
+                  <div className="form-item-custom-label">NTP Setup</div>
                 </Col>
                 <Col lg={7}>
                   <NTPConfig onChange={this.updateFormField} hideHelp={true}/>
