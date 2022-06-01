4f7a6873907bf2a6da5070dde57d670b4616b90b
paullee-yb <plee@yugabyte.com>
2022-06-01 19:23:23 +0000

    [BACKPORT 2.12][#12548] YSQL: Make permission error strings more informative
    Summary:
    Previously, any permission errors related to `yb_fdw` and `yb_extension` only mentions how the user needs to be a superuser. This diff updates all related error strings to be more descriptive by specifically mentioning that the user can also be a member of these roles. This will be helpful for users in YB Managed since these 2 roles are used to perform operations that are only allowed by superusers (which aren't allowed in YB Managed due to security issues).
    
    This diff also updated `yb_db_admin` error messages to be more descriptive and more clearly specify how a user needs to be a member of the role, not the role itself.
    
    Original commit: D17051 / 51fed86d5754c5fbcd2161455dd999233a38967c
    
    Test Plan:
    ybd --java-test 'org.yb.pgsql.TestPgRegressAuthorization'
    ybd --java-test 'org.yb.pgsql.TestPgRegressForeignData'
    ybd --java-test 'org.yb.pgsql.TestPgRegressTrigger'
    ybd --java-test 'org.yb.pgsql.TestPgRegressTablespaces#testPgRegressTablespaces'
    Jenkins run
    
    Reviewers: jason, fizaa
    
    Reviewed By: fizaa
    
    Subscribers: yql, smishra
    
    Differential Revision: https://phabricator.dev.yugabyte.com/D17178

--- src/postgres/src/backend/commands/user.c
+++ src/postgres/src/backend/commands/user.c
@@ -307,7 +307,8 @@ CreateRole(ParseState *pstate, CreateRoleStmt *stmt)
 		if (!superuser() && !IsYbDbAdminUser(GetUserId()))
 			ereport(ERROR,
 					(errcode(ERRCODE_INSUFFICIENT_PRIVILEGE),
-					 errmsg("must be superuser or yb_db_admin role to change bypassrls attribute")));
+					 errmsg("must be superuser or yb_db_admin role member to "
+					 		"change bypassrls attribute")));
 	}
 	else
 	{
@@ -707,7 +708,8 @@ AlterRole(AlterRoleStmt *stmt)
 		if (!superuser() && !IsYbDbAdminUser(GetUserId()))
 			ereport(ERROR,
 					(errcode(ERRCODE_INSUFFICIENT_PRIVILEGE),
-					 errmsg("must be superuser or yb_db_admin role to change bypassrls attribute")));
+					 errmsg("must be superuser or yb_db_admin role member "
+					 		"to change bypassrls attribute")));
 	}
 	else if (!have_createrole_privilege())
 	{
