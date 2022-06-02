59ca184181bed031d81190843c074f903519bb91
Jethro Mak <88681329+Jethro-M@users.noreply.github.com>
2022-06-02 17:42:00 -0400

    [PLAT-3623] Make node actions display dependent on current universe status
    Summary:
    **Context**
    Node actions dropdown is not shown after resume universe operation fails.
    The reason is because we were showing node actions only when the universe is
    not `isReadOnlyUniverse` and not `universePaused`.
    JIRA: https://yugabyte.atlassian.net/browse/PLAT-3623
    
    **Change**
    This diff shows the node action dropdown when the universe is not `isReadOnlyUniverse` and
    when the universe status is not `universeState.PAUSED`.
    Here we are using the same universe status logic used in the universe view page.
    
    Test Plan:
    - Unpause a paused universe
    {F25894}
    - Stop a node of the universe on AWS console while it is starting
    {F25895}
    - Universe should be in error state now
    - Verify that the node actions dropdown is still shown
    {F25898}{F25899}{F25900}
    
    Reviewers: andrew, cpadinjareveettil, lsangappa, kkannan
    
    Reviewed By: kkannan
    
    Differential Revision: https://phabricator.dev.yugabyte.com/D17005

--- managed/ui/src/components/universes/NodeDetails/NodeDetailsTable.js
+++ managed/ui/src/components/universes/NodeDetails/NodeDetailsTable.js
@@ -16,6 +16,7 @@ import { isNotHidden, isDisabled, isHidden } from '../../../utils/LayoutUtils';
 import { YBPanelItem } from '../../panels';
 import { NodeAction } from '../../universes';
 import { setCookiesFromLocalStorage } from '../../../routes';
+import { getUniverseStatus, universeState } from '../helpers/universeHelpers';
 
 import 'react-bootstrap-table/css/react-bootstrap-table.css';
 
@@ -34,7 +35,6 @@ export default class NodeDetailsTable extends Component {
     const warningIcon = <i className="fa fa-warning yb-fail-color" />;
     const sortedNodeDetails = nodeDetails.sort((a, b) => a.nodeIdx - b.nodeIdx);
     const universeUUID = currentUniverse.data.universeUUID;
-    const universePaused = currentUniverse?.data?.universeDetails?.universePaused;
     const providerConfig = providers.data.find((provider) => provider.uuid === providerUUID)
       ?.config;
 
@@ -253,9 +253,11 @@ export default class NodeDetailsTable extends Component {
     };
 
     const panelTitle = clusterType === 'primary' ? 'Primary Cluster' : 'Read Replicas';
+
+    const universeStatus = getUniverseStatus(currentUniverse.data);
     const displayNodeActions =
       !this.props.isReadOnlyUniverse &&
-      !universePaused &&
+      universeStatus.state !== universeState.PAUSED &&
       isNotHidden(customer.currentCustomer.data.features, 'universes.tableActions');
 
     return (
