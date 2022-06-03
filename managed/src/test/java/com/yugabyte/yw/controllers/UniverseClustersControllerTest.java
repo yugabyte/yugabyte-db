99df46c380d34b1b5cd2fc476f8173ace538bec2
Yury Shchetinin <yshchetinin@yugabyte.com>
2022-06-03 19:52:46 +0300

    [PLAT-3971] Validation for inconsistent settings between main cluster and read replica
    Summary: Validate consistency between readonly and primary clusters before save
    
    Test Plan:
    sbt test
    Add read replica through UI - success
    
    Reviewers: amalyshev, hzare
    
    Reviewed By: hzare
    
    Subscribers: jenkins-bot, yugaware
    
    Differential Revision: https://phabricator.dev.yugabyte.com/D17293

--- managed/src/test/java/com/yugabyte/yw/controllers/UniverseClustersControllerTest.java
+++ managed/src/test/java/com/yugabyte/yw/controllers/UniverseClustersControllerTest.java
@@ -11,13 +11,26 @@
 package com.yugabyte.yw.controllers;
 
 import static com.yugabyte.yw.common.AssertHelper.assertOk;
+import static com.yugabyte.yw.common.AssertHelper.assertPlatformException;
 import static com.yugabyte.yw.common.FakeApiHelper.doRequestWithAuthToken;
 import static com.yugabyte.yw.common.FakeApiHelper.doRequestWithAuthTokenAndBody;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
 import static play.test.Helpers.contentAsString;
 
 import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.node.ArrayNode;
 import com.fasterxml.jackson.databind.node.ObjectNode;
+import com.yugabyte.yw.cloud.PublicCloudConstants;
+import com.yugabyte.yw.common.ModelFactory;
+import com.yugabyte.yw.forms.UniverseDefinitionTaskParams;
+import com.yugabyte.yw.models.Universe;
+import com.yugabyte.yw.models.helpers.DeviceInfo;
+import com.yugabyte.yw.models.helpers.TaskType;
+import java.util.UUID;
+import java.util.function.Consumer;
 import junitparams.JUnitParamsRunner;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import play.libs.Json;
 import play.mvc.Result;
@@ -78,8 +91,69 @@ public class UniverseClustersControllerTest extends UniverseCreateControllerTest
     body.remove("currentClusterType");
     return doRequestWithAuthTokenAndBody(
         "POST",
-        "/api/customers/" + customer.uuid + "/universes/clusters/read_only",
+        "/api/customers/"
+            + customer.uuid
+            + "/universes/"
+            + body.get("universeUUID").asText()
+            + "/clusters/read_only",
         authToken,
         body);
+  }
+
+  @Test
+  public void createReadonlyClusterSuccessTest() {
+    testCreateReadonlyCluster(null, null, true);
+  }
+
+  @Test
+  public void createReadonlyClusterFailTest() {
+    testCreateReadonlyCluster(null, intent -> intent.assignPublicIP = false, false);
+  }
+
+  private void testCreateReadonlyCluster(
+      Consumer<UniverseDefinitionTaskParams.UserIntent> primaryMutator,
+      Consumer<UniverseDefinitionTaskParams.UserIntent> readonlyMutator,
+      boolean success) {
+    UUID fakeTaskUUID = UUID.randomUUID();
+    when(mockCommissioner.submit(any(TaskType.class), any(UniverseDefinitionTaskParams.class)))
+        .thenReturn(fakeTaskUUID);
+
+    Universe universe = ModelFactory.createUniverse(customer.getCustomerId());
+    UniverseDefinitionTaskParams.Cluster primaryCluster =
+        universe.getUniverseDetails().getPrimaryCluster();
+    UniverseDefinitionTaskParams taskParams = new UniverseDefinitionTaskParams();
+    taskParams.universeUUID = universe.universeUUID;
+    UniverseDefinitionTaskParams.Cluster newCluster =
+        new UniverseDefinitionTaskParams.Cluster(
+            UniverseDefinitionTaskParams.ClusterType.ASYNC, primaryCluster.userIntent);
+    newCluster.uuid = UUID.randomUUID();
+    DeviceInfo deviceInfo = new DeviceInfo();
+    deviceInfo.volumeSize = 10;
+    deviceInfo.numVolumes = 2;
+    deviceInfo.storageType = PublicCloudConstants.StorageType.GP2;
+    newCluster.userIntent.deviceInfo = deviceInfo;
+    newCluster.userIntent.instanceType = "c3.xlarge";
+
+    if (primaryMutator != null) {
+      Universe.saveDetails(
+          universe.universeUUID,
+          univ -> {
+            primaryMutator.accept(univ.getUniverseDetails().getPrimaryCluster().userIntent);
+          });
+    }
+    if (readonlyMutator != null) {
+      readonlyMutator.accept(newCluster.userIntent);
+    }
+    ObjectNode bodyJson = Json.newObject();
+    ArrayNode clustersJsonArray = Json.newArray().add(Json.toJson(newCluster));
+    bodyJson.set("clusters", clustersJsonArray);
+    bodyJson.set("deviceInfo", Json.toJson(deviceInfo));
+    bodyJson.put("universeUUID", universe.universeUUID.toString());
+    if (success) {
+      Result result = sendAsyncCreateConfigureRequest(bodyJson);
+      assertOk(result);
+    } else {
+      assertPlatformException(() -> sendAsyncCreateConfigureRequest(bodyJson));
+    }
   }
 }
