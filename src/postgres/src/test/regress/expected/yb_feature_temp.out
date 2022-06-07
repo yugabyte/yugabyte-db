yugabyte/yugabyte-db

commit 074e22bea2071688feba863378623942e0cff447

[#12234] YSQL: Add per-tuple memory context usage to scan nodes
Summary:
Before this change, we were allocating all index scan tuples in the query memory context. This causes a huge buildup in memory usage if we are scanning large indexes. However, foreign scans don't run into this issue as they allocate everything to do with a received DocDB tuple in a per-tuple memory context that is reset after each tuple is scanned.

This diff brings the same per-tuple memory context usage pattern to index scans and index only scans to alleviate the aforementioned memory buildup issues.

On a 1GB table with 1 million rows a full index scan used to locally take 17.5k ms on the first run and then be around 13.2k ms on subsequent runs in the same session. After this change, the same query runs in around 13.1k ms even from the first run. Peak memory usage of this query used to be 1648MB before this change and it is down to 265MB with this change.

These timings were taken from the EXPLAIN ANALYZE output of these queries.

Test Plan: Jenkins

Reviewers: dmitry

Reviewed By: dmitry

Subscribers: yql

Differential Revision: https://phabricator.dev.yugabyte.com/D16786

src/postgres/src/backend/executor/nodeIndexonlyscan.c
@@ -126,6 +126,8 @@ IndexOnlyNext(IndexOnlyScanState *node)
 	/*
 	 * OK, now that we have what we need, fetch the next tuple.
 	 */
+	MemoryContext oldcontext;
+	oldcontext = MemoryContextSwitchTo(node->ss.ps.ps_ExprContext->ecxt_per_tuple_memory);
 	while ((tid = index_getnext_tid(scandesc, direction)) != NULL)
 	{
 		HeapTuple	tuple = NULL;
@@ -228,9 +230,10 @@ IndexOnlyNext(IndexOnlyScanState *node)
 		if (scandesc->xs_recheck)
 		{
 			econtext->ecxt_scantuple = slot;
-			if (!ExecQualAndReset(node->indexqual, econtext))
+			if (!ExecQual(node->indexqual, econtext))
 			{
 				/* Fails recheck, so drop it and loop back for another */
+				ResetExprContext(econtext);
 				InstrCountFiltered2(node, 1);
 				continue;
 			}
@@ -262,9 +265,10 @@ IndexOnlyNext(IndexOnlyScanState *node)
 			PredicateLockPage(scandesc->heapRelation,
 							  ItemPointerGetBlockNumber(tid),
 							  estate->es_snapshot);
-
+		MemoryContextSwitchTo(oldcontext);
 		return slot;
 	}
+	MemoryContextSwitchTo(oldcontext);
 
 	/*
 	 * if we get here it means the index scan failed so we are at the end of

src/postgres/src/backend/executor/nodeIndexscan.c
@@ -168,6 +168,9 @@ IndexNext(IndexScanState *node)
 	/*
 	 * ok, now that we have what we need, fetch the next tuple.
 	 */
+	MemoryContext oldcontext;
+	oldcontext = MemoryContextSwitchTo(
+		node->ss.ps.ps_ExprContext->ecxt_per_tuple_memory);
 	while ((tuple = index_getnext(scandesc, direction)) != NULL)
 	{
 		CHECK_FOR_INTERRUPTS();
@@ -189,14 +192,15 @@ IndexNext(IndexScanState *node)
 		if (scandesc->xs_recheck)
 		{
 			econtext->ecxt_scantuple = slot;
-			if (!ExecQualAndReset(node->indexqualorig, econtext))
+			if (!ExecQual(node->indexqualorig, econtext))
 			{
+				ResetExprContext(econtext);
 				/* Fails recheck, so drop it and loop back for another */
 				InstrCountFiltered2(node, 1);
 				continue;
 			}
 		}
-
+		MemoryContextSwitchTo(oldcontext);
 		return slot;
 	}
 
@@ -205,6 +209,7 @@ IndexNext(IndexScanState *node)
 	 * the scan..
 	 */
 	node->iss_ReachedEnd = true;
+	MemoryContextSwitchTo(oldcontext);
 	return ExecClearTuple(slot);
 }
 

commit 10bd5e3d6b039126879d701397785f5eabfc1859

[#12740] ysql: Don't store index using per-tuple memory context for temp table
Summary:
As what was pointed out in GH 12740, we shouldn't use per-tuple memory context to store index for temp table.
Otherwise we would encounter segmentation fault.

The regression was caused by D16786 / 074e22bea2071688feba863378623942e0cff447

This revision adds a check to ensure that the per-tuple memory context is used only for non-temp tables.

Test Plan: ybd --java-test 'org.yb.pgsql.TestPgRegressFeature'

Reviewers: amartsinchyk, tnayak

Reviewed By: tnayak

Subscribers: yql

Differential Revision: https://phabricator.dev.yugabyte.com/D17312

src/postgres/src/backend/executor/nodeIndexonlyscan.c
@@ -127,7 +127,12 @@ IndexOnlyNext(IndexOnlyScanState *node)
 	 * OK, now that we have what we need, fetch the next tuple.
 	 */
 	MemoryContext oldcontext;
-	oldcontext = MemoryContextSwitchTo(node->ss.ps.ps_ExprContext->ecxt_per_tuple_memory);
+	/*
+	 * To handle dead tuple for temp table, we shouldn't store its index
+	 * in per-tuple memory context.
+	 */
+	if (IsYBRelation(node->ss.ss_currentRelation))
+		oldcontext = MemoryContextSwitchTo(node->ss.ps.ps_ExprContext->ecxt_per_tuple_memory);
 	while ((tid = index_getnext_tid(scandesc, direction)) != NULL)
 	{
 		HeapTuple	tuple = NULL;
@@ -265,10 +270,12 @@ IndexOnlyNext(IndexOnlyScanState *node)
 			PredicateLockPage(scandesc->heapRelation,
 							  ItemPointerGetBlockNumber(tid),
 							  estate->es_snapshot);
-		MemoryContextSwitchTo(oldcontext);
+		if (IsYBRelation(node->ss.ss_currentRelation))
+			MemoryContextSwitchTo(oldcontext);
 		return slot;
 	}
-	MemoryContextSwitchTo(oldcontext);
+	if (IsYBRelation(node->ss.ss_currentRelation))
+		MemoryContextSwitchTo(oldcontext);
 
 	/*
 	 * if we get here it means the index scan failed so we are at the end of

src/postgres/src/backend/executor/nodeIndexscan.c
@@ -169,8 +169,13 @@ IndexNext(IndexScanState *node)
 	 * ok, now that we have what we need, fetch the next tuple.
 	 */
 	MemoryContext oldcontext;
-	oldcontext = MemoryContextSwitchTo(
-		node->ss.ps.ps_ExprContext->ecxt_per_tuple_memory);
+	/*
+	 * To handle dead tuple for temp table, we shouldn't store its index
+	 * in per-tuple memory context.
+	 */
+	if (IsYBRelation(node->ss.ss_currentRelation))
+		oldcontext = MemoryContextSwitchTo(
+			node->ss.ps.ps_ExprContext->ecxt_per_tuple_memory);
 	while ((tuple = index_getnext(scandesc, direction)) != NULL)
 	{
 		CHECK_FOR_INTERRUPTS();
@@ -200,7 +205,8 @@ IndexNext(IndexScanState *node)
 				continue;
 			}
 		}
-		MemoryContextSwitchTo(oldcontext);
+		if (IsYBRelation(node->ss.ss_currentRelation))
+			MemoryContextSwitchTo(oldcontext);
 		return slot;
 	}
 
@@ -209,7 +215,8 @@ IndexNext(IndexScanState *node)
 	 * the scan..
 	 */
 	node->iss_ReachedEnd = true;
-	MemoryContextSwitchTo(oldcontext);
+	if (IsYBRelation(node->ss.ss_currentRelation))
+		MemoryContextSwitchTo(oldcontext);
 	return ExecClearTuple(slot);
 }
 

src/postgres/src/test/regress/expected/yb_feature_temp.out
@@ -413,3 +413,16 @@ SELECT ALL t4.c0 FROM t4 ORDER BY t4.c0 ASC;
 ------
  0.05
 (1 row)
+
+CREATE TEMP TABLE tempt (k int PRIMARY KEY, v1 int, v2 int);
+CREATE UNIQUE INDEX ON tempt (v1);
+INSERT INTO tempt VALUES (1, 2, 3), (4, 5, 6);
+INSERT INTO tempt VALUES (2, 2, 3);
+ERROR:  duplicate key value violates unique constraint "tempt_v1_idx"
+DETAIL:  Key (v1)=(2) already exists.
+SELECT * FROM tempt ORDER BY k;
+ k | v1 | v2
+---+----+----
+ 1 |  2 |  3
+ 4 |  5 |  6
+(2 rows)

src/postgres/src/test/regress/sql/yb_feature_temp.sql
@@ -339,3 +339,9 @@ CREATE temp TABLE t4(c0 DECIMAL NULL, UNIQUE(c0));
 INSERT INTO t4(c0) VALUES(0.03);
 UPDATE t4 SET c0 = (0.05) WHERE t4.c0 = 0.03;
 SELECT ALL t4.c0 FROM t4 ORDER BY t4.c0 ASC;
+
+CREATE TEMP TABLE tempt (k int PRIMARY KEY, v1 int, v2 int);
+CREATE UNIQUE INDEX ON tempt (v1);
+INSERT INTO tempt VALUES (1, 2, 3), (4, 5, 6);
+INSERT INTO tempt VALUES (2, 2, 3);
+SELECT * FROM tempt ORDER BY k;