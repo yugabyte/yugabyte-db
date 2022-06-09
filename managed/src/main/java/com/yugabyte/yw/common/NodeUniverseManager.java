0cf4fda8a33032a7701cd32d0c8c2fd3767040af
Aleksandr Malyshev <amalyshev@yugabyte.com>
2022-06-09 22:24:29 +0300

    [PLAT-4182] Fix health checks on Ubuntu
    Summary:
    Health check script fails on Ubuntu.
    One reason is that Ubuntu node only has python 3, but no 'default' python.
    Another reason is in incorrect command wrapping in health check script - which seem to work on python 2 but not python 3.
    Also changed logging level for health check commands to trace
    
    Test Plan:
    Create centos universe. Make sure health checks are successful.
    Create ubuntu universe. Make sure health chekcs are successful.
    
    Reviewers: muthu
    
    Reviewed By: muthu
    
    Subscribers: jenkins-bot, yugaware
    
    Differential Revision: https://phabricator.dev.yugabyte.com/D17560

--- managed/src/main/java/com/yugabyte/yw/common/NodeUniverseManager.java
+++ managed/src/main/java/com/yugabyte/yw/common/NodeUniverseManager.java
@@ -15,8 +15,10 @@ import java.util.Collections;
 import java.util.List;
 import java.util.UUID;
 import java.util.concurrent.TimeUnit;
+import lombok.extern.slf4j.Slf4j;
 
 @Singleton
+@Slf4j
 public class NodeUniverseManager extends DevopsBase {
   private static final ShellProcessContext DEFAULT_CONTEXT =
       ShellProcessContext.builder().logCmdOutput(true).build();
@@ -240,7 +242,12 @@ public class NodeUniverseManager extends DevopsBase {
     }
     commandArgs.add(nodeAction.name().toLowerCase());
     commandArgs.addAll(actionArgs);
-    LOG.debug("Executing command: " + commandArgs);
+    String logMsg = "Executing command: " + commandArgs;
+    if (context.isTraceLogging()) {
+      log.trace(logMsg);
+    } else {
+      log.debug(logMsg);
+    }
     return shellProcessHandler.run(commandArgs, context);
   }
 
