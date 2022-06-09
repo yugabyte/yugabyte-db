ec7e2f635b588699c6ef944b5315e24029d1c04c
Mehul Joshi <mjoshi@yugabyte.com>
2022-06-09 23:09:19 +0530

    [PLAT-4289] Preselected dates errors in bundle creatation because nothing gets set in payload without custom selection
    Summary: Preselected dates errors in bundle creatation because nothing gets set in payload without custom selection
    
    Test Plan: manual
    
    Reviewers: cpadinjareveettil, asathyan, ssutar, kkannan
    
    Reviewed By: ssutar, kkannan
    
    Subscribers: jenkins-bot, ui
    
    Differential Revision: https://phabricator.dev.yugabyte.com/D17566

--- managed/ui/src/components/universes/UniverseSupportBundle/DateRangePicker/DateRangePicker.js
+++ managed/ui/src/components/universes/UniverseSupportBundle/DateRangePicker/DateRangePicker.js
@@ -1,25 +1,35 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect, useCallback} from 'react';
 import { DatePicker } from 'react-widgets';
 
 import './DateRangePicker.scss';
 
 
 export const CustomDateRangePicker = ({ onRangeChange }) => {
-  const [localStartDate, setLocalStartDate] = useState(new Date());
+  const yesterday = new Date();
+  yesterday.setDate(new Date().getDate() - 1);
+  const [localStartDate, setLocalStartDate] = useState(yesterday);
+  const [localEndDate, setLocalEndDate] = useState(new Date());
 
   const handleStartDateTimeChange = (timestamp) => {
     setLocalStartDate(timestamp);
+    onRangeChange({ start:localEndDate, end: timestamp});
   };
 
-  const handleEndDateTimeChange = (timestamp) => {
+  const handleEndDateTimeChange = useCallback((timestamp) => {
+    setLocalEndDate(timestamp)
     onRangeChange({ start:localStartDate, end: timestamp});
-  };
+  }, [setLocalEndDate, onRangeChange, localStartDate]);
+
+  useEffect(() => {
+    handleEndDateTimeChange(new Date());
+  }, [handleEndDateTimeChange]);
+
 
   return (
     <span className="support-bundle-custom-date-range">
       <DatePicker
         placeholder="yyyy-MM-DD"
-        defaultValue={new Date()}
+        defaultValue={yesterday}
         onChange={handleStartDateTimeChange}
         max={new Date()}
       />
