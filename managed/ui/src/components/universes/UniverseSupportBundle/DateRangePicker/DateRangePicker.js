8df676def77bc4d59427b8ff9e27a53bf6611772
Mehul Joshi <mjoshi@yugabyte.com>
2022-06-24 11:53:23 +0530

    [PLAT-4289] Custom date range setting issue
    Summary: Becase setting custom date range was returning undefined on defaults, causing create bundle fail
    
    Test Plan: manual
    
    Reviewers: cpadinjareveettil, kkannan, asathyan, ssutar
    
    Reviewed By: ssutar
    
    Subscribers: jenkins-bot, ui
    
    Differential Revision: https://phabricator.dev.yugabyte.com/D17868

--- managed/ui/src/components/universes/UniverseSupportBundle/DateRangePicker/DateRangePicker.js
+++ managed/ui/src/components/universes/UniverseSupportBundle/DateRangePicker/DateRangePicker.js
@@ -1,4 +1,4 @@
-import React, {useState, useEffect, useCallback} from 'react';
+import React, {useState} from 'react';
 import { DatePicker } from 'react-widgets';
 
 import './DateRangePicker.scss';
