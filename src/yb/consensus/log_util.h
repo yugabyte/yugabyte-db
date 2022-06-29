d658df219dc7b6df7fcbb277c1c3abd70512281a
Timur Yusupov <ttyusupov@users.noreply.github.com>
2022-06-29 14:01:04 +0300

    [#10960] docdb: Implemented Raft log index durable persistence and lazy loading
    Summary:
    Raft log index allows to find the WAL segment file and offset based on a Raft operation index. A Raft log index is managed by the `LogIndex` class, where the two most used API functions are `AddEntry` (used during Raft log write to add an entry offset and segment number to the log index) and `GetEntry` (used to get the offset and segment number for the specific Raft log operation by operation index).
    `LogIndex` class is implemented in such a way that on `AddEntry` call it updates log index in mmaped file (actually it uses multiple files - chunks, multiple entries per chunk file). And `LogIndex` maintains a map of mmaped files in memory (`LogIndex::open_chunks_`).
    `LogIndex::GetEntry` looks for a log index chunk inside `LogIndex::open_chunks_` and returns a "not found" error if the chunk is not opened even if chunk is present on disk.
    `LogIndex` class was not designed to be durable and survive restarts and is intended to be rebuilt on restarts. This is corroborated by the following existing comments:
    ```
    // An on-disk structure which indexes from OpId index to the specific position in the WAL
    // which contains the latest ReplicateMsg for that index.
    //
    // This structure is on-disk but *not durable*. We use mmap()ed IO to write it out, and
    // never sync it to disk. Its only purpose is to allow random-reading earlier entries from
    // the log to serve to Raft followers.
    ```
    
    Before we implemented the local bootstrap optimizer ( D6938 / 41ef3f75e3c68686595c7613f53b649823b84fed ), this approach was working fine, because the whole Raft log was being replayed when opening the tablet, and we were rebuilding `LogIndex` from scratch as part of that. But now, the local bootstrap optimizer can skip replaying some of the initial segments of the Raft log.
    
    Another potential issue is that since `LogIndex` is not durable, its latest state might be not synced/flushed to the disk and at tserver restart, we will read garbage from the part of log index chunk/file that was not rebuilt during tablet bootstrap. And then this garbage will be used as an offset to where to read Raft log entry from.
    
    To address the issue, this revision implements persistence of Raft log index within Raft log segment file and lazily loads it inside `LogIndex` instance that is used as a log index cache (the way it was originally designed to be used).
    
    Other related changes includes:
    - Added `LogIndexBlockHeaderPB` and `LogSegmentFooterPB.index_start_offset`
    - Added `LogIndex::GetIndexBlock`, `WritableLogSegment::WriteIndex`, `WritableLogSegment::WriteIndexBlock` to support saving of Raft log index inside log segment file.
    - Removed `LogIndex::Flush` and dependent functions.
    - Removed `LogIndex::CopyTo`. Instead, log index is copied as a part of WAL segment file
    - Added `LogIndex::Load*` and `LogReader::GetIndexEntry` to support loading Raft log index from log segment files.
    - Added new test `RaftConsensusITest.CatchupAfterLeaderRestarted`
    - Fixed `LogReader::LookupHeader`
    
    Backward compatibility note: `save_index_into_wal_segments` should be switched to `true` only after all nodes in cluster have been upgraded to a version containing this diff.
    
    Test Plan: log-test, log_index-test, RaftConsensusITest.CatchupAfterLeaderRestarted
    
    Reviewers: mbautin
    
    Reviewed By: mbautin
    
    Subscribers: bogdan
    
    Differential Revision: https://phabricator.dev.yugabyte.com/D16954

--- src/yb/consensus/log_util.h
+++ src/yb/consensus/log_util.h
@@ -49,7 +49,6 @@
 #include "yb/gutil/macros.h"
 #include "yb/gutil/ref_counted.h"
 
-#include "yb/util/atomic.h"
 #include "yb/util/compare_util.h"
 #include "yb/util/env.h"
 #include "yb/util/monotime.h"
@@ -109,7 +108,7 @@ struct LogOptions {
 
   std::string peer_uuid;
 
-  uint64_t initial_active_segment_sequence_number = 0;
+  int64_t initial_active_segment_sequence_number = 0;
 
   LogOptions();
 };
@@ -156,6 +155,8 @@ struct FirstEntryMetadata {
   }
 };
 
+YB_DEFINE_ENUM(EntriesToRead, (kAll)(kReplicate));
+
 // A segment of the log can either be a ReadableLogSegment (for replay and
 // consensus catch-up) or a WritableLogSegment (where the Log actually stores
 // state). LogSegments have a maximum size defined in LogOptions (set from the
@@ -201,10 +202,17 @@ class ReadableLogSegment : public RefCountedThreadSafe<ReadableLogSegment> {
   // In case of failure status field of result is not ok.
   //
   // Will stop after reading max_entries_to_read entries.
-  ReadEntriesResult ReadEntries(int64_t max_entries_to_read = std::numeric_limits<int64_t>::max());
+  ReadEntriesResult ReadEntries(
+      int64_t max_entries_to_read = std::numeric_limits<int64_t>::max(),
+      EntriesToRead entries_to_read = EntriesToRead::kAll);
 
   // Reads the metadata of the first entry in the segment
   Result<FirstEntryMetadata> ReadFirstEntryMetadata();
+
+  // Returns op id of the first replicate entry.
+  // - NotFound error if no replicate entries found.
+  // - Other errors if failed to read.
+  Result<OpId> ReadFirstReplicateEntryOpId();
 
   // Rebuilds this segment's footer by scanning its entries.
   // This is an expensive operation as it reads and parses the whole segment
@@ -212,10 +220,13 @@ class ReadableLogSegment : public RefCountedThreadSafe<ReadableLogSegment> {
   // missing because we didn't have the time to write it out.
   Status RebuildFooterByScanning();
 
-  // Copies log segment up to up_to_op_id into dest_file at dest_path and updates
-  // log_index_to_rebuild.
-  Status CopyTo(const OpId& up_to_op_id, const std::string& dest_path,
-      const std::shared_ptr<WritableFile>& dest_file, LogIndex* log_index_to_rebuild);
+  // Copies log segment up to up_to_op_id into new segment at dest_path.
+  Status CopyTo(
+      Env* env, const WritableFileOptions& writable_file_options, const std::string& dest_path,
+      const OpId& up_to_op_id);
+
+  // Reads index block from file at specified offset into tmp_buf.
+  Result<LogIndexBlock> ReadIndexBlock(uint64_t* offset, faststring* tmp_buf);
 
   bool IsInitialized() const {
     return is_initialized_;
@@ -256,21 +267,20 @@ class ReadableLogSegment : public RefCountedThreadSafe<ReadableLogSegment> {
   }
 
   int64_t file_size() const {
-    return file_size_.Load();
+    return file_size_.load(std::memory_order_acquire);
   }
 
   int64_t first_entry_offset() const {
     return first_entry_offset_;
   }
 
-  int64_t get_header_size() const {
+  int64_t get_encryption_header_size() const {
     return readable_file_->GetEncryptionHeaderSize();
   }
 
-  // Returns the full size of the file, if the segment is closed and has
-  // a footer, or the offset where the last written, non corrupt entry
-  // ends.
-  int64_t readable_up_to() const;
+  int64_t readable_to_offset() const {
+    return readable_to_offset_.load(std::memory_order_acquire);
+  }
 
  private:
   friend class RefCountedThreadSafe<ReadableLogSegment>;
@@ -342,23 +352,21 @@ class ReadableLogSegment : public RefCountedThreadSafe<ReadableLogSegment> {
 
   void UpdateReadableToOffset(int64_t readable_to_offset);
 
-  int64_t GetOffsetReadUpTo();
+  int64_t ReadEntriesUpTo();
 
   const std::string path_;
 
   // The size of the readable file.
-  // This is set by Init(). In the case of a log being written to,
-  // this may be increased by UpdateReadableToOffset()
-  AtomicInt<int64_t> file_size_;
+  // This is set by Init(). In the case of a log being written to, this may be increased by
+  // UpdateReadableToOffset().
+  std::atomic<int64_t> file_size_;
 
   // The offset up to which we can read the file.
-  // For already written segments this is fixed and equal to the file size
-  // but for the segments currently written to this is the offset up to which
-  // we can read without the fear of reading garbage/zeros.
+  // Contains full size of the file, if the segment is closed and has a footer, or the offset where
+  // the last written, non corrupt entry ends.
   // This is atomic because the Log thread might be updating the segment's readable
   // offset while an async reader is reading the segment's entries.
-  // is reading it.
-  AtomicInt<int64_t> readable_to_offset_;
+  std::atomic<int64_t> readable_to_offset_;
 
   // a readable file for a log segment (used on replay)
   const std::shared_ptr<RandomAccessFile> readable_file_;
@@ -389,9 +397,9 @@ class WritableLogSegment {
   // Opens the segment by writing the header.
   Status WriteHeaderAndOpen(const LogSegmentHeaderPB& new_header);
 
-  // Closes the segment by writing the footer and then actually closing the
+  // Closes the segment by writing the index, the footer, and then actually closing the
   // underlying WritableFile.
-  Status WriteFooterAndClose(const LogSegmentFooterPB& footer);
+  Status WriteIndexWithFooterAndClose(LogIndex* log_index, LogSegmentFooterPB* footer);
 
   bool IsClosed() {
     return IsHeaderWritten() && IsFooterWritten();
@@ -447,6 +455,11 @@ class WritableLogSegment {
     return writable_file_;
   }
 
+  // Writes index stored inside log_index within specified range into the WAL segment file.
+  Status WriteIndex(LogIndex* log_index, int64_t start_index, int64_t end_index_inclusive);
+
+  Status WriteIndexBlock(const LogIndexBlock& index_block);
+
   // The path to the log file.
   const std::string path_;
 
@@ -466,6 +479,8 @@ class WritableLogSegment {
 
   // The offset where the last written entry ends.
   int64_t written_offset_;
+
+  faststring index_block_header_buffer_;
 
   DISALLOW_COPY_AND_ASSIGN(WritableLogSegment);
 };
