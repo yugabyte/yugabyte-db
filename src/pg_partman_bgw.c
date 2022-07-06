1a6c67bf279257ee175bcaaee16c3048e657d8ee
Keith Fiske <keith@keithf4.com>
2022-07-06 15:23:21 -0400

    4.7.0 PG15 Support. Add new run_analyze procedure. See CHANGELOG for more details.

--- src/pg_partman_bgw.c
+++ src/pg_partman_bgw.c
@@ -520,7 +520,9 @@ void pg_partman_bgw_run_maint(Datum arg) {
     SPI_finish();
     PopActiveSnapshot();
     CommitTransactionCommand();
+    #if (PG_VERSION_NUM < 150000)
     ProcessCompletedNotifies();
+    #endif
     pgstat_report_activity(STATE_IDLE, NULL);
     elog(DEBUG1, "pg_partman dynamic BGW shutting down gracefully for database %s.", dbname);
 
