87851e48e254be85adfab0c7d6c550a70489d7f6
vipul <vipul.bansal.yb@gmail.com>
2022-07-08 10:24:33 +0000

    [PLAT-3505]: Add Support for backup abortion created using YB-Controller.
    Summary:
    Added the changes to abort backup task created using YB-Controller.
    Also, added the changes to delete tasks on YB-Controller on either completion or abortion of backup task.
    
    Test Plan: Tested Manually that task is successfully aborted and the task gets deleted from YB-controller.
    
    Reviewers: vpatibandla, spotachev, vkumar
    
    Reviewed By: vkumar
    
    Subscribers: jenkins-bot, yugaware
    
    Differential Revision: https://phabricator.dev.yugabyte.com/D18091

--- managed/src/main/java/com/yugabyte/yw/common/YbcManager.java
+++ managed/src/main/java/com/yugabyte/yw/common/YbcManager.java
@@ -13,6 +13,13 @@ import org.slf4j.LoggerFactory;
 import org.yb.client.YbcClient;
 import org.yb.ybc.BackupServiceNfsDirDeleteRequest;
 import org.yb.ybc.BackupServiceNfsDirDeleteResponse;
+import java.util.UUID;
+import org.yb.ybc.BackupServiceTaskAbortRequest;
+import org.yb.ybc.BackupServiceTaskAbortResponse;
+import org.yb.ybc.BackupServiceTaskDeleteRequest;
+import org.yb.ybc.BackupServiceTaskDeleteResponse;
+import org.yb.ybc.BackupServiceTaskResultRequest;
+import org.yb.ybc.BackupServiceTaskResultResponse;
 import org.yb.ybc.ControllerStatus;
 
 @Singleton
@@ -21,12 +28,12 @@ public class YbcManager {
   private static final Logger LOG = LoggerFactory.getLogger(YbcManager.class);
 
   private final YbcClientService ybcClientService;
-
   private final CustomerConfigService customerConfigService;
-
   private final YbcBackupUtil ybcBackupUtil;
-
   private final BackupUtil backupUtil;
+
+  private static final int WAIT_EACH_ATTEMPT_MS = 5000;
+  private static final int MAX_RETRIES = 10;
 
   @Inject
   public YbcManager(
@@ -72,11 +79,84 @@ public class YbcManager {
       LOG.error("Backup {} deletion failed with error: {}", backup.backupUUID, e.getMessage());
       return false;
     } finally {
-      if (ybcClient != null) {
-        ybcClientService.closeClient(ybcClient);
-      }
+      ybcClientService.closeClient(ybcClient);
     }
     LOG.debug("Nfs dir for backup {} is successfully deleted.", backup.backupUUID);
     return true;
+  }
+
+  public void abortBackupTask(UUID customerUUID, UUID backupUUID, String taskID) {
+    Backup backup = Backup.getOrBadRequest(customerUUID, backupUUID);
+    YbcClient ybcClient = null;
+    try {
+      ybcClient = ybcBackupUtil.getYbcClient(backup.universeUUID);
+      BackupServiceTaskAbortRequest abortTaskRequest =
+          BackupServiceTaskAbortRequest.newBuilder().setTaskId(taskID).build();
+      BackupServiceTaskAbortResponse abortTaskResponse =
+          ybcClient.backupServiceTaskAbort(abortTaskRequest);
+      if (!abortTaskResponse.getStatus().getCode().equals(ControllerStatus.OK)) {
+        LOG.error(
+            "Aborting backup {} task errored out with {}.",
+            backup.backupUUID,
+            abortTaskResponse.getStatus().getErrorMessage());
+        return;
+      }
+      BackupServiceTaskResultRequest taskResultRequest =
+          BackupServiceTaskResultRequest.newBuilder().setTaskId(taskID).build();
+      BackupServiceTaskResultResponse taskResultResponse =
+          ybcClient.backupServiceTaskResult(taskResultRequest);
+      if (!taskResultResponse.getTaskStatus().equals(ControllerStatus.ABORT)) {
+        LOG.error(
+            "Aborting backup {} task errored out and is in {} state.",
+            backup.backupUUID,
+            taskResultResponse.getTaskStatus());
+        return;
+      } else {
+        LOG.info("Backup {} task is successfully aborted on Yb-controller.", backup.backupUUID);
+        deleteYbcBackupTask(backup.universeUUID, taskID);
+      }
+    } catch (Exception e) {
+      LOG.error("Backup {} task abort failed with error: {}.", backup.backupUUID, e.getMessage());
+    } finally {
+      ybcClientService.closeClient(ybcClient);
+    }
+  }
+
+  public void deleteYbcBackupTask(UUID universeUUID, String taskID) {
+    YbcClient ybcClient = null;
+    try {
+      ybcClient = ybcBackupUtil.getYbcClient(universeUUID);
+      BackupServiceTaskResultRequest taskResultRequest =
+          BackupServiceTaskResultRequest.newBuilder().setTaskId(taskID).build();
+      BackupServiceTaskResultResponse taskResultResponse =
+          ybcClient.backupServiceTaskResult(taskResultRequest);
+      if (taskResultResponse.getTaskStatus().equals(ControllerStatus.NOT_FOUND)) {
+        return;
+      }
+      BackupServiceTaskDeleteRequest taskDeleteRequest =
+          BackupServiceTaskDeleteRequest.newBuilder().setTaskId(taskID).build();
+      BackupServiceTaskDeleteResponse taskDeleteResponse = null;
+      int numRetries = 0;
+      while (numRetries < MAX_RETRIES) {
+        taskDeleteResponse = ybcClient.backupServiceTaskDelete(taskDeleteRequest);
+        if (!taskDeleteResponse.getStatus().getCode().equals(ControllerStatus.IN_PROGRESS)) {
+          break;
+        }
+        Thread.sleep(WAIT_EACH_ATTEMPT_MS);
+        numRetries++;
+      }
+      if (!taskDeleteResponse.getStatus().getCode().equals(ControllerStatus.OK)) {
+        LOG.error(
+            "Deleting task {} errored out and is in {} state.",
+            taskID,
+            taskDeleteResponse.getStatus());
+        return;
+      }
+      LOG.info("Task {} is successfully deleted on Yb-controller.", taskID);
+    } catch (Exception e) {
+      LOG.error("Task {} deletion failed with error: {}", taskID, e.getMessage());
+    } finally {
+      ybcClientService.closeClient(ybcClient);
+    }
   }
 }
