b9588694c102e983e5776399159f5b61301d6b5f
Sanketh I <sanketh@yugabyte.com>
2022-07-12 23:05:49 -0700

    [PLAT-4658] [PLAT-4650] Rotating CA through UI results in loss of gflags and sets unexpected flags
    Summary:
    Rotating CA through UI sends a partial request params like
    
    ```
    {
        "enableNodeToNodeEncrypt": true,
        "enableClientToNodeEncrypt": true,
        "rootCA": "183f0066-7a04-4c07-aae4-24eb2eca5619",
        "createNewRootCA": false,
        "clientRootCA": "183f0066-7a04-4c07-aae4-24eb2eca5619",
        "createNewClientRootCA": false,
        "rootAndClientRootCASame": true,
        "upgradeOption": "Rolling",
        "sleepAfterMasterRestartMillis": 2000,
        "sleepAfterTServerRestartMillis": 2000
    }
    ```
    
    without the full universe details. This results in a taskParams that has the default values of UniverseDefinitionTaskParams and certain gflags generated from these have unexpected values.
    
    Secondly, the resulting invocation of NodeManager for root and client certificate rotation fails to append to existing gflags. Instead it builds cert-related gflags on an empty list of gflags, thereby causing a loss of user set gflags.
    
    Test Plan:
    1. Rotate root CA, verify user set gflags are preserved, allow_insecure_connections stays at false.
    2. Rotate client root CA, verify `--allow_insecure_connections` stays at false.
    3. Unit test to verify merge logic
    4. Toggle TLS on/off, verify gflags and allow_insecure.
    5. Verify (1), (2) on systemd enabled universe, verify tasks complete successfully.
    6. dev itest pipeline run for tls test cases.
    
    Reviewers: arnav, yshchetinin
    
    Reviewed By: arnav, yshchetinin
    
    Subscribers: yshchetinin, jenkins-bot, yugaware
    
    Differential Revision: https://phabricator.dev.yugabyte.com/D18300

--- managed/src/main/java/com/yugabyte/yw/controllers/handlers/UniverseCRUDHandler.java
+++ managed/src/main/java/com/yugabyte/yw/controllers/handlers/UniverseCRUDHandler.java
@@ -52,6 +52,7 @@ import com.yugabyte.yw.models.Provider;
 import com.yugabyte.yw.models.Region;
 import com.yugabyte.yw.models.TaskInfo;
 import com.yugabyte.yw.models.Universe;
+import com.yugabyte.yw.models.helpers.CommonUtils;
 import com.yugabyte.yw.models.helpers.NodeDetails;
 import com.yugabyte.yw.models.helpers.NodeDetails.NodeState;
 import com.yugabyte.yw.models.helpers.TaskType;
@@ -71,6 +72,7 @@ import java.util.stream.Collectors;
 import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import play.libs.Json;
 import play.mvc.Http;
 import play.mvc.Http.Status;
 
@@ -1237,6 +1239,9 @@ public class UniverseCRUDHandler {
 
   public UUID tlsConfigUpdate(
       Customer customer, Universe universe, TlsConfigUpdateParams taskParams) {
+
+    LOG.info("tlsConfigUpdate: {}", Json.toJson(CommonUtils.maskObject(taskParams)));
+
     UniverseDefinitionTaskParams universeDetails = universe.getUniverseDetails();
     UserIntent userIntent = universeDetails.getPrimaryCluster().userIntent;
 
@@ -1360,15 +1365,11 @@ public class UniverseCRUDHandler {
                 runtimeConfigFactory.staticApplicationConf().getString("yb.storage.path"));
       }
 
-      CertsRotateParams certsRotateParams = new CertsRotateParams();
-      certsRotateParams.rootCA = isRootCA ? taskParams.rootCA : null;
-      certsRotateParams.clientRootCA = isClientRootCA ? taskParams.clientRootCA : null;
-      certsRotateParams.selfSignedServerCertRotate = taskParams.selfSignedServerCertRotate;
-      certsRotateParams.selfSignedClientCertRotate = taskParams.selfSignedClientCertRotate;
-      certsRotateParams.rootAndClientRootCASame = taskParams.rootAndClientRootCASame;
-      certsRotateParams.upgradeOption = taskParams.upgradeOption;
-      certsRotateParams.sleepAfterMasterRestartMillis = taskParams.sleepAfterMasterRestartMillis;
-      certsRotateParams.sleepAfterTServerRestartMillis = taskParams.sleepAfterTServerRestartMillis;
+      CertsRotateParams certsRotateParams =
+          CertsRotateParams.mergeUniverseDetails(taskParams, universe.getUniverseDetails());
+
+      LOG.info("CertsRotateParams : {}", Json.toJson(CommonUtils.maskObject(certsRotateParams)));
+
       return upgradeUniverseHandler.rotateCerts(certsRotateParams, customer, universe);
     }
 
