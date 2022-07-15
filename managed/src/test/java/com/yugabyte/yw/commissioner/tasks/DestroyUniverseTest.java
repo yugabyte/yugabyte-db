cd03306fb4a1a550546ece6d7995b712636ebf04
Aleksandr Malyshev <amalyshev@yugabyte.com>
2022-07-15 13:58:59 +0300

    [Backport 2.15.0][PLAT-4586] Correctly clean up yb_univ_health_status metric on universe pause/removal/config changes
    Summary:
    Legacy yb_univ_health_status is collected on each health check for each node and represents check statuses for particular node.
    The issue with this metric is that it's never cleaned up correctly - in case of universe pause/removal/or configuration change it will remain for all the nodes until YBA is restarted.
    This is causing issues with Cloud alerts - as they're alerting for health check failures using this metric.
    We're modifying metric collection to use YBA MetricStorage - which handles universe removalse/pasuses and the rest of configuration changes correctly.
    
    Another change here is that we're adding node metrics for few healthc checks, which were missing it - Clock skew, core files, YCQL/Redis connectivity checks.
    
    Original diff: https://phabricator.dev.yugabyte.com/D18279
    
    Test Plan:
    Create universe with YSQL, YCQL and Redis enabled.
    Wait for health check for this universe.
    Make sure yb_node_core_files_check, yb_node_clock_skew_check, yb_node_version_check, yb_node_ycql_connect and yb_node_redis_connect metrics are present for each node.
    Make sure yb_univ_health_status metric is present for all the checks, including metrics-only check.
    Make sure health check result looks as before.
    
    Stop processes on the node.
    Wait for health check.
    Make sure health check result has all nodes expect the one, where we stopped processes.
    Make sure yb_node_core_files_check, yb_node_clock_skew_check, yb_node_version_check, yb_node_ycql_connect and yb_node_redis_connect metrics are present for each node expect the one, where we stopped processes.
    Make sure yb_univ_health_status metric is present for all the checks and nodes expect the one, where we stopped processes.
    Make sure yb_univ_health_status metric is present only for some metric-only checks for the node, where we stopped processes.
    
    Pause universe.
    Make sure health check is not running for this universe.
    Make sure yb_node_core_files_check, yb_node_clock_skew_check, yb_node_version_check, yb_node_ycql_connect and yb_node_redis_connect are cleaned.
    Make sure yb_univ_health_status metric is cleaned for this universe.
    
    Resume universe.
    Wait for health check for this universe.
    Make sure yb_node_core_files_check, yb_node_clock_skew_check, yb_node_version_check, yb_node_ycql_connect and yb_node_redis_connect metrics are present for each node.
    Make sure yb_univ_health_status metric is present for all the checks, including metrics-only check.
    Make sure health check result looks as before.
    
    Remove universe.
    Make sure yb_node_core_files_check, yb_node_clock_skew_check, yb_node_version_check, yb_node_ycql_connect and yb_node_redis_connect are cleaned.
    Make sure yb_univ_health_status metric is cleaned for this universe.
    
    Reviewers: hzare
    
    Reviewed By: hzare
    
    Subscribers: yugaware, jenkins-bot
    
    Differential Revision: https://phabricator.dev.yugabyte.com/D18374

--- managed/src/test/java/com/yugabyte/yw/commissioner/tasks/DestroyUniverseTest.java
+++ managed/src/test/java/com/yugabyte/yw/commissioner/tasks/DestroyUniverseTest.java
@@ -2,7 +2,6 @@
 
 package com.yugabyte.yw.commissioner.tasks;
 
-import static com.yugabyte.yw.common.AssertHelper.assertPlatformException;
 import static com.yugabyte.yw.common.ModelFactory.createUniverse;
 import static com.yugabyte.yw.common.TestHelper.createTempFile;
 import static com.yugabyte.yw.common.metrics.MetricService.buildMetricTemplate;
@@ -18,7 +17,6 @@ import static org.mockito.Mockito.when;
 import com.yugabyte.yw.common.certmgmt.CertConfigType;
 import com.google.common.collect.ImmutableList;
 import com.yugabyte.yw.common.ApiUtils;
-import com.yugabyte.yw.common.AssertHelper;
 import com.yugabyte.yw.common.ModelFactory;
 import com.yugabyte.yw.common.ShellResponse;
 import com.yugabyte.yw.common.metrics.MetricService;
@@ -39,7 +37,6 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.junit.MockitoJUnitRunner;
-import play.mvc.Result;
 
 @RunWith(MockitoJUnitRunner.class)
 public class DestroyUniverseTest extends CommissionerBaseTest {
@@ -112,7 +109,7 @@ public class DestroyUniverseTest extends CommissionerBaseTest {
         MetricKey.builder()
             .customerUuid(defaultCustomer.getUuid())
             .name(PlatformMetrics.HEALTH_CHECK_STATUS.getMetricName())
-            .targetUuid(defaultUniverse.getUniverseUUID())
+            .sourceUuid(defaultUniverse.getUniverseUUID())
             .build();
     assertThat(metricService.get(metricKey), nullValue());
   }
