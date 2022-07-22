aa2060a1cd284473d1eeacfab7246e506a7fca71
Timur Yusupov <ttyusupov@users.noreply.github.com>
2022-07-22 15:58:38 +0300

    [#1661] docdb: Extracted Env::IOPriority from rocksdb::Env into separate enum inside yb/util
    Summary: Extracted Env::IOPriority from rocksdb::Env into separate enum inside yb/util as a part of effort to separate filesystem-related logic from Env and unify rocksdb::Env and yb::Env.
    
    Test Plan: Jenkins
    
    Reviewers: mbautin
    
    Reviewed By: mbautin
    
    Subscribers: ybase
    
    Differential Revision: https://phabricator.dev.yugabyte.com/D18459

--- src/yb/rocksdb/db/builder.cc
+++ src/yb/rocksdb/db/builder.cc
@@ -90,7 +90,7 @@ std::unique_ptr<TableBuilder> NewTableBuilder(
 
 namespace {
   Status CreateWritableFileWriter(const std::string& filename, const EnvOptions& env_options,
-      const Env::IOPriority io_priority, Env* env,
+      const yb::IOPriority io_priority, Env* env,
       std::shared_ptr<WritableFileWriter>* file_writer) {
     unique_ptr<WritableFile> file;
     Status s = NewWritableFile(env, filename, &file, env_options);
@@ -120,7 +120,7 @@ Status BuildTable(const std::string& dbname,
                   bool paranoid_file_checks,
                   InternalStats* internal_stats,
                   BoundaryValuesExtractor* boundary_values_extractor,
-                  const Env::IOPriority io_priority,
+                  const yb::IOPriority io_priority,
                   TableProperties* table_properties) {
   // Reports the IOStats for flush for every following bytes.
   const size_t kReportFlushIOStatsEvery = 1048576;
