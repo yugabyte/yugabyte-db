eaaa196873a93324c979537ebecaa64442d6f565
Mikhail Bautin <mbautin@users.noreply.github.com>
2022-07-25 10:32:24 -0700

    [BACKPORT 2.14] [#12738] Add static libraries based on shared library dependencies during LTO
    Summary:
    The libgssapi_krb5 shared library depends on other shared libraries built as part of krb5, but similar dependencies are not encoded in the corresponding libgssapi_krb5 static library, because static libraries do not work that way. The build system is supposed to add of the corresponding static libraries instead. As a general approach to fix this issue, in our LTO build scripts, we need to look at shared library dependencies of each shared library we encounter as we compose the linker command line (we can find those using ldd) and add the corresponding static library for each of those dependencies as well. In this diff we only do that for libraries that are part of the third-party package. We don't need multi-level traversal here because ldd already computes the transitive closure of all needed shared libraries.
    
    Other changes include:
    - Adding `-lresolv` because there are undefined symbols in Ubuntu 20.04 aarch64 LTO without it.
    - Preventing static linking with system libraries. Previously, librt.a might have been statically linked. It is part of glibc and we should not statically link with any glibc libraries.
    
    Test Plan:
    Jenkins
    
    Running ldd on the LTO-enabled yb-tserver executable on Ubuntu 20.04 aarch64 now yields this:
    
    ```
    linux-vdso.so.1 (0x0000fffff7ffc000)
    librt.so.1 => /lib/aarch64-linux-gnu/librt.so.1 (0x0000fffff7fb4000)
    libresolv.so.2 => /lib/aarch64-linux-gnu/libresolv.so.2 (0x0000fffff7f8e000)
    libm.so.6 => /lib/aarch64-linux-gnu/libm.so.6 (0x0000fffff7ee3000)
    libc.so.6 => /lib/aarch64-linux-gnu/libc.so.6 (0x0000fffff7d70000)
    libdl.so.2 => /lib/aarch64-linux-gnu/libdl.so.2 (0x0000fffff7d5a000)
    libpthread.so.0 => /lib/aarch64-linux-gnu/libpthread.so.0 (0x0000fffff7d29000)
    /lib/ld-linux-aarch64.so.1 (0x0000fffff7fcc000)
    ```
    
    Also checking the x86_64 case (AlmaLinux 8, Linuxbrew enabled, third-party package `yugabyte-db-thirdparty-v20220531054925-62ed7bc00f-almalinux8-x86_64-clang12-linuxbrew-full-lto`):
    ```
    > ldd build/release-clang12-linuxbrew-full-lto-ninja/bin/yb-tserver-lto
    linux-vdso.so.1 (0x00007ffea0952000)
    librt.so.1 => /opt/yb-build/brew/linuxbrew-20181203T161736v9/lib/librt.so.1 (0x00007fa01a069000)
    libresolv.so.2 => /opt/yb-build/brew/linuxbrew-20181203T161736v9/lib/libresolv.so.2 (0x00007fa019e52000)
    libm.so.6 => /opt/yb-build/brew/linuxbrew-20181203T161736v9/lib/libm.so.6 (0x00007fa019b4f000)
    libgcc_s.so.1 => /opt/yb-build/brew/linuxbrew-20181203T161736v9/lib/libgcc_s.so.1 (0x00007fa019938000)
    libc.so.6 => /opt/yb-build/brew/linuxbrew-20181203T161736v9/lib/libc.so.6 (0x00007fa01959c000)
    libdl.so.2 => /opt/yb-build/brew/linuxbrew-20181203T161736v9/lib/libdl.so.2 (0x00007fa019398000)
    libpthread.so.0 => /opt/yb-build/brew/linuxbrew-20181203T161736v9/lib/libpthread.so.0 (0x00007fa01917b000)
    /opt/yb-build/brew/linuxbrew-20181203T161736v9/lib/ld.so => /lib64/ld-linux-x86-64.so.2 (0x00007fa01a271000)
    ```
    
    Reviewers: rthallam, esheng, steve.varnau
    
    Reviewed By: esheng
    
    Subscribers: ybase
    
    Differential Revision: https://phabricator.dev.yugabyte.com/D18155

--- python/yb/lto.py
+++ python/yb/lto.py
@@ -14,8 +14,9 @@ import os
 import time
 import subprocess
 import logging
+import re
 
-from typing import Iterable, Set, List, Optional, Tuple
+from typing import Iterable, Set, List, Optional, Tuple, Dict
 from yb.dep_graph_common import (
     DependencyGraph,
     Node,
@@ -38,7 +39,6 @@ from pathlib import Path
 SKIPPED_ARGS: Set[str] = {
     '-fPIC',
     '-lm',
-    '-lrt',
     '-ldl',
     '-latomic',
     '-lcrypt',
@@ -49,6 +49,42 @@ SKIPPED_ARGS: Set[str] = {
     '-Wall',
 }
 
+LDD_OUTPUT_LINE_RE = re.compile(r'^(\S+) => (\S+) .*$')
+
+SHARED_LIB_SUFFIX_RE = re.compile(r'^(.*)([.]so(?:[.]\d+)*)$')
+
+# This is necessary to remove the -2.4 suffix to transform "liblber-2.4.so.2" to "liblber.a".
+DASH_VERSION_SUFFIX_RE = re.compile(r'^(.*)-\d+(?:[.]\d+)*$')
+
+LIBCXX_STATIC_LIB_NAMES = ('libc++.a', 'libc++abi.a')
+
+SYSTEM_LIBRARY_PREFIXES = ('/lib/', '/lib64/', '/usr/')
+
+STATIC_LIBRARY_SUFFIXES = ('.a', '-s.a')
+
+
+def is_cmd_line_option(arg: str) -> bool:
+    return arg.startswith('-')
+
+
+def is_system_lib(lib_path: str) -> bool:
+    return not is_cmd_line_option(lib_path) and lib_path.startswith(SYSTEM_LIBRARY_PREFIXES)
+
+
+def is_static_lib(lib_path: str) -> bool:
+    return not is_cmd_line_option(lib_path) and lib_path.endswith('.a')
+
+
+def is_shared_lib(lib_path: str) -> bool:
+    if is_cmd_line_option(lib_path):
+        return False
+    name = os.path.basename(lib_path)
+    return '.so.' in name or name.endswith('.so')
+
+
+def should_skip_argument(arg: str) -> bool:
+    return arg in SKIPPED_ARGS or arg.startswith('--gcc-toolchain')
+
 
 def get_static_lib_paths(thirdparty_path: str) -> List[str]:
     static_lib_paths = []
@@ -56,7 +92,7 @@ def get_static_lib_paths(thirdparty_path: str) -> List[str]:
         for root, dirs, files in os.walk(
                 os.path.join(thirdparty_path, 'installed', thirdparty_subdir)):
             for file_name in files:
-                if file_name.endswith('.a'):
+                if is_static_lib(file_name):
                     static_lib_path = os.path.join(root, file_name)
                     static_lib_paths.append(static_lib_path)
     if not static_lib_paths:
@@ -90,22 +126,25 @@ class LinkCommand:
     """
     A wrapper around an array to manipulate a linker command line.
     """
-    args: List[str]
+    _args: List[str]
+    _arg_set: Set[str]
 
     def __init__(self, initial_args: List[str] = []) -> None:
-        self.args = list(initial_args)
+        self._args = list(initial_args)
+        self._arg_set = set(initial_args)
 
     def append(self, arg: str) -> None:
-        self.args.append(arg)
+        self._args.append(arg)
+        self._arg_set.add(arg)
 
     def extend(self, more_args: List[str]) -> None:
-        self.args.extend(more_args)
+        for arg in more_args:
+            self.append(arg)
 
     def add_new_arg(self, new_arg: str) -> None:
-        should_skip = (
-            new_arg in SKIPPED_ARGS or
-            new_arg.startswith('--gcc-toolchain')
-        )
+        if self.contains(new_arg):
+            return
+        should_skip = should_skip_argument(new_arg)
 
         if should_skip:
             logging.info("Skipping %s", new_arg)
@@ -114,7 +153,10 @@ class LinkCommand:
         self.append(new_arg)
 
     def contains(self, arg: str) -> bool:
-        return arg in self.args
+        return arg in self._arg_set
+
+    def as_list(self) -> List[str]:
+        return list(self._args)
 
 
 def process_original_link_cmd(original_link_cmd: List[str]) -> List[str]:
@@ -135,6 +177,37 @@ def process_original_link_cmd(original_link_cmd: List[str]) -> List[str]:
 
 def is_yb_library(rel_path: str) -> bool:
     return rel_path.startswith(('lib/', 'postgres/lib/')) and rel_path.endswith(DYLIB_SUFFIX)
+
+
+def split_shared_lib_ext(shared_lib_path: str) -> Tuple[Optional[str], Optional[str]]:
+    """
+
+    >>> split_shared_lib_ext('libkrb5.so.3')
+    ('libkrb5', '.so.3')
+    """
+    m = SHARED_LIB_SUFFIX_RE.match(shared_lib_path)
+    if m:
+        return m.group(1), m.group(2)
+    return None, None
+
+
+def find_shared_lib_from_static(static_lib_path: str) -> str:
+    assert is_static_lib(static_lib_path)
+    shared_lib_path = static_lib_path[:-2] + '.so'
+    if not os.path.exists(shared_lib_path):
+        logging.warn("Shared library path does not exist: %s", shared_lib_path)
+    return shared_lib_path
+
+
+def remove_dash_numeric_suffix(lib_prefix: str) -> Optional[str]:
+    """
+    >>> remove_dash_numeric_suffix('liblber-2.4')
+    'liblber'
+    """
+    m = DASH_VERSION_SUFFIX_RE.match(lib_prefix)
+    if m:
+        return m.group(1)
+    return None
 
 
 class LinkHelper:
@@ -172,6 +245,18 @@ class LinkHelper:
     # Populated by consume_original_link_cmd.
     final_output_name: str
 
+    # We look at shared library dependencies (detected using ldd) of the libraries we add, and for
+    # those dependencies that fall within the third-party directory, we determine the corresponding
+    # static libraries and add them to the list below so we can link with them. This is necessary
+    # because in some cases, e.g. for libgssapi_krb5, the static libraries we need to add cannot be
+    # determined in any other way. The dictionary below maps the static library to the list of
+    # shared libraries that necessitated its addition.
+    static_libs_from_ldd: Dict[str, Set[str]]
+
+    # Set of shared library file paths for which we have already examined dependencies using ldd
+    # as described above.
+    processed_shared_lib_deps_for: Set[str]
+
     def __init__(
             self,
             dep_graph: DependencyGraph,
@@ -197,23 +282,37 @@ class LinkHelper:
 
         self.lto_output_suffix = lto_output_suffix
 
+        self.static_libs_from_ldd = {}
+        self.processed_shared_lib_deps_for = set()
+
     def convert_to_static_lib(self, arg: str) -> Optional[str]:
         """
         Given an argument to the original linker command, try to interpret it as a library, either
         specified as a shared library path, or using -l... syntax, and return the corresponding
         static library path if available.
         """
-        if arg.startswith('/') and arg.endswith('.so'):
-            arg_static_prefix = arg[:-3]
+        assert not is_system_lib(arg)
 
-            static_found = False
-            for suffix in ['.a', '-s.a']:
-                arg_static = arg_static_prefix + suffix
-                if os.path.exists(arg_static):
-                    logging.info("Using static library %s instead of shared library %s",
-                                 arg_static, arg)
-                    return arg_static
-            logging.info("Did not find static library corresponding to %s", arg)
+        if os.path.isabs(arg):
+            lib_path_prefix, shared_lib_suffix = split_shared_lib_ext(arg)
+            if lib_path_prefix is not None:
+                static_found = False
+                lib_path_prefixes: List[str] = [
+                    item for item in [lib_path_prefix, remove_dash_numeric_suffix(lib_path_prefix)]
+                    if item is not None
+                ]
+                static_lib_candidates = [
+                    lib_path_prefix + suffix
+                    for lib_path_prefix in lib_path_prefixes
+                    for suffix in STATIC_LIBRARY_SUFFIXES
+                ]
+
+                for static_lib_path in static_lib_candidates:
+                    if os.path.exists(static_lib_path):
+                        logging.info("Using static library %s instead of shared library %s",
+                                     static_lib_path, arg)
+                        return static_lib_path
+                raise ValueError("Did not find static library corresponding to %s" % arg)
 
         if arg.startswith('-l'):
             static_found = False
@@ -221,28 +320,70 @@ class LinkHelper:
             lib_name = arg[2:]
             for static_lib_path in self.static_lib_paths:
                 static_lib_basename = os.path.basename(static_lib_path)
-                if (static_lib_basename == 'lib' + lib_name + '.a' or
-                        static_lib_basename == 'lib' + lib_name + '-s.a'):
+                if any(static_lib_basename == 'lib' + lib_name + suffix
+                       for suffix in STATIC_LIBRARY_SUFFIXES):
                     logging.info("Found static lib for %s: %s", lib_name, static_lib_path)
+                    self.add_shared_library_dependencies(
+                        find_shared_lib_from_static(static_lib_path))
                     return static_lib_path
             logging.info("Did not find a static lib for %s", lib_name)
 
-        if arg.endswith('.so') or '.so.' in arg:
-            logging.info("Still using a shared library: %s", arg)
+        if is_shared_lib(arg):
+            raise ValueError("Still using a shared library: %s" % arg)
 
         return None
 
+    def add_shared_library_dependencies(self, shared_library_path: str) -> None:
+        if shared_library_path in self.processed_shared_lib_deps_for:
+            return
+
+        self.processed_shared_lib_deps_for.add(shared_library_path)
+        if not os.path.exists(shared_library_path):
+            logging.info("File does ont exist, not running ldd: %s", shared_library_path)
+            return
+
+        ldd_output = subprocess.check_output(['ldd', shared_library_path]).decode('utf-8')
+        for line in ldd_output.split('\n'):
+            line = line.strip()
+            ldd_output_line_match = LDD_OUTPUT_LINE_RE.match(line)
+            if ldd_output_line_match:
+                so_name = ldd_output_line_match.group(1)
+                so_path = ldd_output_line_match.group(2)
+                if so_path.startswith(self.thirdparty_path + '/'):
+                    static_lib_path = self.convert_to_static_lib(so_path)
+                    if static_lib_path:
+                        if os.path.basename(static_lib_path) in LIBCXX_STATIC_LIB_NAMES:
+                            # Skip libc++ and libc++abi, we will add them explicitly later.
+                            # All third-party libraries written in C++ will depend on these and it
+                            # is not very useful to include that in the output.
+                            continue
+                        if static_lib_path not in self.static_libs_from_ldd:
+                            self.static_libs_from_ldd[static_lib_path] = set()
+                        self.static_libs_from_ldd[static_lib_path].add(os.path.realpath(
+                            shared_library_path))
+
     def process_arg(self, arg: str) -> None:
+        assert arg is not None
         if arg in SKIPPED_ARGS:
             logging.info("Skipping argument: %s", arg)
             return
 
-        new_arg = self.convert_to_static_lib(arg)
-        if new_arg:
-            if not self.new_args.contains(new_arg):
-                self.new_args.add_new_arg(new_arg)
+        if is_system_lib(arg):
+            if is_static_lib(arg):
+                raise ValueError("Linking with a system static library is not allowed: %s" % arg)
+            if is_shared_lib(arg):
+                name = os.path.basename(arg)
+                if name == 'librt.so':
+                    arg = '-lrt'
+                else:
+                    raise ValueError("System shared library: %s" % arg)
         else:
-            self.new_args.add_new_arg(arg)
+            if is_shared_lib(arg):
+                self.add_shared_library_dependencies(arg)
+
+            arg = self.convert_to_static_lib(arg) or arg
+
+        self.new_args.add_new_arg(arg)
 
     def consume_original_link_cmd(self) -> None:
         """
@@ -316,6 +457,18 @@ class LinkHelper:
 
     def add_final_args(self, lto_type: str) -> None:
         assert lto_type in ['full', 'thin']
+        for static_lib_path in sorted(self.static_libs_from_ldd):
+            if not self.new_args.contains(static_lib_path):
+                logging.info(
+                    "Adding a static library determined using shared library dependencies: %s "
+                    "(needed by: %s)",
+                    static_lib_path,
+                    # The static_libs_from_ldd dictionary stores the set of shared library paths
+                    # that caused us to add a particular static library dependency as the value
+                    # corresponding to that static library's path in the key.
+                    ', '.join(sorted(self.static_libs_from_ldd[static_lib_path])))
+                self.new_args.append(static_lib_path)
+
         self.new_args.extend([
             '-L%s' % os.path.join(self.build_root, 'postgres', 'lib'),
             '-l:libpgcommon.a',
@@ -324,10 +477,13 @@ class LinkHelper:
             '-fwhole-program',
             '-Wl,-v',
             '-nostdlib++',
+            # For __res_nsearch, ns_initparse, ns_parserr, ns_name_uncompress.
+            # See https://github.com/yugabyte/yugabyte-db/issues/12738 for details.
+            '-lresolv',
             '-flto=' + lto_type,
         ])
 
-        for lib_name in ['libc++.a', 'libc++abi.a']:
+        for lib_name in LIBCXX_STATIC_LIB_NAMES:
             self.new_args.append(os.path.join(
                 self.thirdparty_path, 'installed', 'uninstrumented', 'libcxx', 'lib', lib_name))
 
@@ -339,7 +495,7 @@ class LinkHelper:
             start_time_sec = time.time()
             logging.info("Running linker")
             try:
-                subprocess.check_call(self.new_args.args)
+                subprocess.check_call(self.new_args.as_list())
             except subprocess.CalledProcessError as ex:
                 # Avoid printing the extremely long command line.
                 logging.error("Linker returned exit code %d", ex.returncode)
@@ -349,7 +505,7 @@ class LinkHelper:
     def write_link_cmd_file(self, out_path: str) -> None:
         logging.info("Writing the linker command line (one argument per line) to %s",
                      os.path.abspath(out_path))
-        write_file('\n'.join(self.new_args.args), out_path)
+        write_file('\n'.join(self.new_args.as_list()), out_path)
 
 
 def link_whole_program(
