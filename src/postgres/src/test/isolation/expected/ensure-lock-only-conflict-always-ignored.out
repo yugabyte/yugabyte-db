yugabyte/yugabyte-db

commit c3931e803a19e9573160a462451c1c96e2f1717d

[#12767] YSQL: Send list of aborted sub txns to the status tablet during a savepoint rollback + 2 bug fixes
Summary:
As part of this change, YSQL will complete a savepoint rollback only after it
sends the list of aborted sub txns to the transaction status tablet using the
UpdateTransactionRequestPB heartbeat.

This is to ensure that after rolling back some part of transaction T1, any
statement in another transaction T2 should be able to see that the rolled back
provisional writes are invalid/ aborted.

Other bug fixes -

(1) a minor bug in the 036e704 -- the list of aborted sub txns, which is sent
periodically by YSQL, isn't always updated in the status tablet. The aborted
list was accepted by the transaction coordinator only if
`data.state.aborted().set_size() > aborted_.set_size()`, but the set_size() here
isn't the same as the number of aborted sub txns (see AbortedSubTransactionSet).

(2) Intents written for explicit row locks didn't persist sub txn id with them. This is
because for read queries with row level locks, the `write_batch` didn't have
sub txn id set. This is now done in `ReadQuery::DoPerform()`.

Test Plan:
Jenkins: urgent

./yb_build.sh --java-test org.yb.pgsql.TestPgConflictWithAbortedSubTxns#ensureNoConflictsWithAbortedSubTxns
./yb_build.sh --java-test org.yb.pgsql.TestPgIsolationRegress#isolationRegress
    - Enabled aborted-keyrevoke and delete-abort-savept-2 in the isolation test schedule

Reviewers: esheng, rsami

Reviewed By: esheng, rsami

Subscribers: zyu, bogdan, yql

Differential Revision: https://phabricator.dev.yugabyte.com/D17358

src/yb/tablet/transaction_coordinator.cc
@@ -781,10 +781,10 @@ class TransactionState {
     }
     last_touch_ = data.hybrid_time;
     first_entry_raft_index_ = data.op_id.index;
-    // TODO -- consider swapping instead of copying here.
-    if (data.state.aborted().set_size() > aborted_.set_size()) {
-      aborted_ = data.state.aborted();
-    }
+
+    // TODO(savepoints) -- consider swapping instead of copying here.
+    aborted_ = data.state.aborted();
+
     return Status::OK();
   }
 

src/postgres/src/test/isolation/yb_pg_isolation_schedule
@@ -29,3 +29,5 @@ test: multixact-no-deadlock
 test: read-only-anomaly
 test: nowait-2
 test: nowait-3
+test: aborted-keyrevoke
+test: delete-abort-savept-2

java/yb-pgsql/src/test/java/org/yb/pgsql/TestPgConflictWithAbortedSubTxns.java
@@ -1,0 +1,154 @@
+// Copyright (c) YugaByte, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+// in compliance with the License.  You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software distributed under the License
+// is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+// or implied.  See the License for the specific language governing permissions and limitations
+// under the License.
+
+package org.yb.pgsql;
+
+import static org.yb.AssertionWrappers.*;
+
+import java.sql.Connection;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import org.yb.util.ThreadUtil;
+import org.yb.util.YBTestRunnerNonTsanOnly;
+
+/**
+ * This test mimics the scenario in #12767 -
+ *
+ * Before this commit's fix, the following scenario leads to a situation where 2 single statement
+ * read committed transactions block each other -
+ *
+ *    CREATE TABLE test (id int primary key, v timestamp with time zone NOT NULL);
+ *    CREATE INDEX idx ON test USING btree (v);
+ *
+ * 1. 2 read committed isolation transactions are started in separate sessions with begin;
+ * 2. Both sessions get the request from client to execute a INSERT ON CONFLICT DO UPDATE query on a
+ *    row in the main table.
+ *
+ *    INSERT INTO test AS x (id, v) VALUES (1, statement_timestamp())
+ *    ON CONFLICT ON CONSTRAINT test_pkey
+ *    DO UPDATE SET v = statement_timestamp() WHERE x.id = excluded.id
+ *
+ * 3. Backends of both sessions first read the main table to check if the key already exists. Since
+ *    it does, both backends then try to perform the ON CONFLICT DO UPDATE by issuing three rpcs
+ *    simultaneously to tserver processes -
+ *
+ *      i) a PGSQL_UPDATE to the main table to update the v time
+ *      ii) a PGSQL_DELETE to the secondary index to remove the entry with existing v value
+ *      iii) a PGSQL_UPSERT to the secondary index to insert a new index entry with new v value
+ *
+ * 4. Rpc [a] of session 1 reaches the main table first and performs the write.
+ *    Rpc [b] (and/or [c]) of session 2 reaches the index table first and performs the write there.
+ *
+ * 5. So, session 1 has successfully written a provisional entry to the main table and session 2 has
+ *    successfully written provisional entries to the index table.
+ *
+ * 6. Now, the other rpcs in both session will fail due to a conflict i.e., rpc [b] and [c] of
+ *    session 1 and rpc [a] of session 2 will fail.
+ *
+ * 7. Both sessions, after facing the conflict, will retry the statement by rolling back to the
+ *    internal savepoint registered before the statement's execution (as is done by our read
+ *    committed implementation). Rolling back will effectively mark the provisional data written by
+ *    earlier rpcs in the statement as invalid so that the backends can retry their statement and no
+ *    other transactions should be able to see those invalid provisional entries.
+ *
+ * 8. However, even after rolling back savepoints, other transactions consider the rolled back
+ *    provisional entries for conflict resolution and hence still face conflicts. This is because
+ *    the list of aborted sub txns isn't available to the txn participants during conflict
+ *    detection.
+ *
+ * 9. This leads to both sessions retrying and facing false conflicts till statement timeout is hit.
+ */
+@RunWith(value = YBTestRunnerNonTsanOnly.class)
+public class TestPgConflictWithAbortedSubTxns extends BasePgSQLTest {
+  private static final Logger LOG =
+    LoggerFactory.getLogger(TestPgConflictWithAbortedSubTxns.class);
+
+  private static final int NUM_THREADS = 9;
+  private static final int TEST_DURATION_SECS = 120;
+  private static final int STATEMENT_TIMEOUT_MS = 10 * 1000;
+
+  @Override
+  protected Map<String, String> getTServerFlags() {
+    Map<String, String> flags = super.getTServerFlags();
+    flags.put("yb_enable_read_committed_isolation", "true");
+    flags.put("ysql_pg_conf_csv", "statement_timeout=" + STATEMENT_TIMEOUT_MS);
+    return flags;
+  }
+
+  @Test
+  public void ensureNoConflictsWithAbortedSubTxns() throws Exception {
+    try (Statement statement = connection.createStatement()) {
+      statement.execute(
+        "CREATE TABLE test (id int primary key, v timestamp with time zone NOT NULL)");
+      statement.execute("CREATE INDEX idx ON test USING btree (v)");
+    }
+
+    ExecutorService es = Executors.newFixedThreadPool(NUM_THREADS);
+    List<Future<?>> futures = new ArrayList<>();
+    List<Runnable> runnables = new ArrayList<>();
+
+    for (int i = 0; i < NUM_THREADS; i++) {
+      runnables.add(() -> {
+        try (Connection conn =
+               getConnectionBuilder().withIsolationLevel(IsolationLevel.READ_COMMITTED)
+               .withAutoCommit(AutoCommit.ENABLED).connect();
+             Statement stmt = conn.createStatement();) {
+
+          long end_time = System.currentTimeMillis() + TEST_DURATION_SECS*1000;
+
+          while (System.currentTimeMillis() < end_time) {
+            stmt.execute("BEGIN");
+            stmt.execute(
+              "INSERT INTO test AS x (id, v) VALUES (1, statement_timestamp()) " +
+              "ON CONFLICT ON CONSTRAINT test_pkey " +
+              "DO UPDATE SET v = statement_timestamp() WHERE x.id = excluded.id"
+            );
+            stmt.execute("COMMIT");
+          }
+        } catch (Exception ex) {
+          fail("Failed due to exception: " + ex.getMessage());
+        }
+      });
+    }
+
+    for (Runnable r : runnables) {
+      futures.add(es.submit(r));
+    }
+
+    try {
+      LOG.info("Waiting for all threads");
+      for (Future<?> future : futures) {
+        future.get(TEST_DURATION_SECS + 10, TimeUnit.SECONDS);
+      }
+    } catch (TimeoutException ex) {
+      LOG.warn("Threads info:\n\n" + ThreadUtil.getAllThreadsInfo());
+      fail("Waiting for threads timed out, this is unexpected!");
+    }
+
+    try (Statement statement = connection.createStatement()) {
+      statement.execute("DROP TABLE test");
+    }
+  }
+}

src/postgres/src/backend/access/transam/xact.c
@@ -4156,6 +4156,8 @@ DefineSavepoint(const char *name)
 			/* Normal subtransaction start */
 			PushTransaction();
 			s = CurrentTransactionState;	/* changed by push */
+			elog(DEBUG2, "new sub txn created by savepoint, subtxn_id: %d",
+					 s->subTransactionId);
 
 			/*
 			 * Savepoint names, like the TransactionState block itself, live
@@ -4442,7 +4444,7 @@ RollbackToSavepoint(const char *name)
 		elog(FATAL, "RollbackToSavepoint: unexpected state %s",
 			 BlockStateAsString(xact->blockState));
 
-	YBCRollbackSubTransaction(target->subTransactionId);
+	YBCRollbackToSubTransaction(target->subTransactionId);
 }
 
 /*
@@ -4493,6 +4495,8 @@ BeginInternalSubTransaction(const char *name)
 			/* Normal subtransaction start */
 			PushTransaction();
 			s = CurrentTransactionState;	/* changed by push */
+			elog(DEBUG2, "new sub txn created internally, subtxn_id: %d",
+					 s->subTransactionId);
 
 			/*
 			 * Savepoint names, like the TransactionState block itself, live
@@ -4539,7 +4543,6 @@ BeginInternalSubTransaction(const char *name)
  */
 void
 BeginInternalSubTransactionForReadCommittedStatement() {
-	elog(DEBUG2, "Begin internal sub txn for statement in READ COMMITTED isolation");
 
 	YBFlushBufferedOperations();
 	TransactionState s = CurrentTransactionState;
@@ -4556,6 +4559,7 @@ BeginInternalSubTransactionForReadCommittedStatement() {
 	/* Normal subtransaction start */
 	PushTransaction();
 	s = CurrentTransactionState;	/* changed by push */
+	elog(DEBUG2, "new internal sub txn in READ COMMITTED subtxn_id: %d", s->subTransactionId);
 
 	s->name = MemoryContextStrdup(TopTransactionContext, YB_READ_COMMITTED_INTERNAL_SUB_TXN_NAME);
 
@@ -5158,7 +5162,7 @@ AbortSubTransaction(void)
 		AtEOSubXact_ApplyLauncher(false, s->nestingLevel);
 	}
 
-	YBCRollbackSubTransaction(s->subTransactionId);
+	YBCRollbackToSubTransaction(s->subTransactionId);
 
 	/*
 	 * Restore the upper transaction's read-only state, too.  This should be

src/postgres/src/backend/utils/misc/pg_yb_utils.c
@@ -578,10 +578,10 @@ YBCSetActiveSubTransaction(SubTransactionId id)
 }
 
 void
-YBCRollbackSubTransaction(SubTransactionId id)
+YBCRollbackToSubTransaction(SubTransactionId id)
 {
 	if (YBSavepointsEnabled())
-		HandleYBStatus(YBCPgRollbackSubTransaction(id));
+		HandleYBStatus(YBCPgRollbackToSubTransaction(id));
 }
 
 bool

src/postgres/src/include/pg_yb_utils.h
@@ -278,7 +278,7 @@ extern void YBCAbortTransaction();
 
 extern void YBCSetActiveSubTransaction(SubTransactionId id);
 
-extern void YBCRollbackSubTransaction(SubTransactionId id);
+extern void YBCRollbackToSubTransaction(SubTransactionId id);
 
 /*
  * Return true if we want to allow PostgreSQL's own locking. This is needed

commit d8c3f84d81a5cd22ffd5d490f8e6b7e46ca19026

[#13222] YSQL: Ensure that the list of aborted sub-txns is not spuriously removed by asynchronous heartbeats
Summary:
036e70461baf69fc11d6b0d76588afcfcc0afd00 added logic that enabled transactions to
ignore conflicting intents of aborted sub-txns. This was done by asynchronously
sending the aborted sub-txn list to the transactions coordinator using the
existing heartbeats. The aborted sub-txns list is then used by transaction
participants during conflicting resolution.

c3931e803a19e9573160a462451c1c96e2f1717d, which came later, ensured that YSQL
synchronously updates the list of aborted sub-txns on the transaction status
record of a status tablet when some sub-txn is rolled back. This was to
eliminate any window where a transaction might face a conflict error when another
transaction's aborted intents.

But the second commit introduces the following buggy line in
PendingReplicationFinished() that causes the aborted sub-txns list to disappear
in the next asynchronous heartbeat from YSQL to the status tablet:
`aborted_ = data.state.aborted();`.
The second commit removed the aborted sub-txns list from the asynchronous
heartbeats. And due to the above buggy line, the empty list replaces the
aborted sub-txn list on the status record in the next heartbeat.

The implication of this is: other transactions are not able to ignore conflicting
intents that are part of aborted sub-txns.

The fix is to update `aborted_` only when `data.state.aborted()` is not empty.
This will ensure that asynchronous heartbeats don't update the `aborted_`
state variable.

The two tests introduced in 036e70461baf69fc11d6b0d76588afcfcc0afd00
are moved to the isolation_regress test suite:

(1) testIgnoresIntentOfRolledBackSavepointCrossTxn
(2) testIgnoresLockOnlyConflictOfCommittedTxn

The bug introduced by c3931e803a19e9573160a462451c1c96e2f1717d is tested now
by an extra case in testIgnoresIntentOfRolledBackSavepointCrossTxn: a sleep

src/yb/tablet/transaction_coordinator.cc
@@ -791,7 +791,10 @@ class TransactionState {
     first_entry_raft_index_ = data.op_id.index;
 
     // TODO(savepoints) -- consider swapping instead of copying here.
-    aborted_ = data.state.aborted();
+    // Asynchronous heartbeats don't include aborted sub-txn set (and hence the set is empty), so
+    // avoid updating in those cases.
+    if (!data.state.aborted().set().empty())
+      aborted_ = data.state.aborted();
 
     return Status::OK();
   }

java/yb-pgsql/src/test/java/org/yb/pgsql/TestPgSavepoints.java
@@ -61,7 +61,6 @@ public class TestPgSavepoints extends BasePgSQLTest {
   protected Map<String, String> getTServerFlags() {
     Map<String, String> flags = super.getTServerFlags();
     flags.put("txn_max_apply_batch_records", String.format("%d", LARGE_BATCH_ROW_THRESHOLD));
-    flags.put("TEST_inject_sleep_before_applying_intents_ms", "10000");
     return flags;
   }
 
@@ -153,37 +152,6 @@ public class TestPgSavepoints extends BasePgSQLTest {
         conn.rollback();
       }
     }
-  }
-
-  @Test
-  public void testIgnoresIntentOfRolledBackSavepointCrossTxn() throws Exception {
-    createTable();
-
-    Connection conn1 = getConnectionBuilder()
-                                  .withIsolationLevel(IsolationLevel.READ_COMMITTED)
-                                  .withAutoCommit(AutoCommit.DISABLED)
-                                  .connect();
-    Connection conn2 = getConnectionBuilder()
-                                  .withIsolationLevel(IsolationLevel.READ_COMMITTED)
-                                  .withAutoCommit(AutoCommit.DISABLED)
-                                  .connect();
-
-    Statement s1 = conn1.createStatement();
-    Statement s2 = conn2.createStatement();
-
-    s1.execute("INSERT INTO t VALUES (1, 1)");
-    s1.execute("SAVEPOINT a");
-    s1.execute("INSERT INTO t VALUES (2, 1)");
-    s1.execute("ROLLBACK TO a");
-
-    s2.execute("INSERT INTO t VALUES (2, 2)");
-
-    conn1.commit();
-    conn2.commit();
-
-
-    assertEquals(OptionalInt.of(1), getSingleValue(conn1, 1));
-    assertEquals(OptionalInt.of(2), getSingleValue(conn1, 2));
   }
 
   @Test

src/postgres/src/test/isolation/expected/ensure-lock-only-conflict-always-ignored.out
@@ -1,0 +1,53 @@
+Parsed test spec with 2 sessions
+
+starting permutation: s1_select s2_select s1_savepoint_a s1_update_k_1 s1_rollback_to_a s1_lock_k_1 s1_update_k_3 s1_commit s2_update_k_1 s2_commit s1_select
+step s1_select: SELECT * FROM t;
+k              v              
+
+5              0              
+1              0              
+6              0              
+7              0              
+9              0              
+10             0              
+4              0              
+2              0              
+8              0              
+3              0              
+step s2_select: SELECT * FROM t;
+k              v              
+
+5              0              
+1              0              
+6              0              
+7              0              
+9              0              
+10             0              
+4              0              
+2              0              
+8              0              
+3              0              
+step s1_savepoint_a: SAVEPOINT a;
+step s1_update_k_1: UPDATE t SET v=1 WHERE k=1;
+step s1_rollback_to_a: ROLLBACK TO a;
+step s1_lock_k_1: SELECT * FROM t WHERE k=1 FOR UPDATE;
+k              v              
+
+1              0              
+step s1_update_k_3: UPDATE t SET v=1 WHERE k=3;
+step s1_commit: COMMIT;
+step s2_update_k_1: UPDATE t SET v=2 WHERE k=1;
+step s2_commit: COMMIT;
+step s1_select: SELECT * FROM t;
+k              v              
+
+5              0              
+1              2              
+6              0              
+7              0              
+9              0              
+10             0              
+4              0              
+2              0              
+8              0              
+3              1              

src/postgres/src/test/isolation/expected/ignore-intent-of-rolled-back-savepoint-cross-txn.out
@@ -1,0 +1,33 @@
+Parsed test spec with 2 sessions
+
+starting permutation: s1_insert_1_1 s1_savepoint_a s1_insert_2_1 s1_rollback_to_a s2_insert_2_2 s1_commit s2_commit s1_select
+step s1_insert_1_1: INSERT INTO t VALUES (1, 1);
+step s1_savepoint_a: SAVEPOINT a;
+step s1_insert_2_1: INSERT INTO t VALUES (2, 1);
+step s1_rollback_to_a: ROLLBACK TO a;
+step s2_insert_2_2: INSERT INTO t VALUES (2, 2);
+step s1_commit: COMMIT;
+step s2_commit: COMMIT;
+step s1_select: SELECT * FROM t;
+key            v              
+
+1              1              
+2              2              
+
+starting permutation: s1_insert_1_1 s1_savepoint_a s1_insert_2_1 s1_rollback_to_a s1_sleep s2_insert_2_2 s1_commit s2_commit s1_select
+step s1_insert_1_1: INSERT INTO t VALUES (1, 1);
+step s1_savepoint_a: SAVEPOINT a;
+step s1_insert_2_1: INSERT INTO t VALUES (2, 1);
+step s1_rollback_to_a: ROLLBACK TO a;
+step s1_sleep: SELECT pg_sleep(2);
+pg_sleep       
+
+               
+step s2_insert_2_2: INSERT INTO t VALUES (2, 2);
+step s1_commit: COMMIT;
+step s2_commit: COMMIT;
+step s1_select: SELECT * FROM t;
+key            v              
+
+1              1              
+2              2              

src/postgres/src/test/isolation/specs/ensure-lock-only-conflict-always-ignored.spec
@@ -1,0 +1,51 @@
+# This tests ensures the following guarantee:
+#
+#  1. Txn 1 registers a savepoint and modifies a row
+#  2. Txn 1 rolls back to the savepoint, undoing the modification
+#  3. Txn 1 then locks the same row
+#  4. Txn 1 commits but is yet to apply the intents on the txn participant.
+#  5. Another transaction tries to modify the same row. This new transaction sees 2 conflicts - a
+#     conflict with the modification from Txn 1 and a conflict with the lock-only intent from Txn 1.
+#     The conflict with the modification should be ignored since it is part of an aborted sub-txn.
+#     The conflict with the lock-only intent should also be ignored since the Txn 1 has committed
+#     and no longer holds the lock.
+#
+# This scenario is only tested with "org.yb.pgsql.TestPgIsolationRegress#withDelayedTxnApply" which
+# uses TEST_inject_sleep_before_applying_intents_ms to induce a delay before applying intents. This
+# is needed for step (4) above. Without this, the lock-only intent would be removed and the second
+# transaction wouldn't see it (and we want to test the fact that the second transaction should
+# ignore it after seeing it in intents db because it is a lock-only intent).
+#
+# This test was introduced due to a review comment in 4fb1676a785319fa35e2d75241a870e3115f1637. The
+# comment was due to the following issue in the work in progress code: a modification intent that
+# was part of an aborted sub-txn was causing a non-aborted lock-only intent of an aborted txn to
+# conflict with other transactions.
+
+setup
+{
+  CREATE TABLE t (k INT PRIMARY KEY, v INT);
+  INSERT INTO t SELECT generate_series(1, 10), 0;
+}
+
+teardown
+{
+  DROP TABLE t;
+}
+
+session "s1"
+setup			{ BEGIN TRANSACTION ISOLATION LEVEL REPEATABLE READ; }
+step "s1_select"		{ SELECT * FROM t; }
+step "s1_savepoint_a"	{ SAVEPOINT a; }
+step "s1_update_k_1" { UPDATE t SET v=1 WHERE k=1; }
+step "s1_rollback_to_a" { ROLLBACK TO a; }
+step "s1_lock_k_1" { SELECT * FROM t WHERE k=1 FOR UPDATE; }
+step "s1_update_k_3" { UPDATE t SET v=1 WHERE k=3; }
+step "s1_commit" { COMMIT; }
+
+session "s2"
+setup			{ BEGIN TRANSACTION ISOLATION LEVEL REPEATABLE READ; }
+step "s2_select"		{ SELECT * FROM t; }
+step "s2_update_k_1"		{ UPDATE t SET v=2 WHERE k=1; }
+step "s2_commit"		{ COMMIT; }
+
+permutation "s1_select" "s2_select" "s1_savepoint_a" "s1_update_k_1" "s1_rollback_to_a" "s1_lock_k_1" "s1_update_k_3" "s1_commit" "s2_update_k_1" "s2_commit" "s1_select"

src/postgres/src/test/isolation/specs/ignore-intent-of-rolled-back-savepoint-cross-txn.spec
@@ -1,0 +1,27 @@
+setup
+{
+  CREATE TABLE t (key INT PRIMARY KEY, v INT);
+}
+
+teardown
+{
+  DROP TABLE t;
+}
+
+session "s1"
+setup			{ BEGIN; }
+step "s1_insert_1_1"		{ INSERT INTO t VALUES (1, 1); }
+step "s1_savepoint_a"	{ SAVEPOINT a; }
+step "s1_insert_2_1"		{ INSERT INTO t VALUES (2, 1); }
+step "s1_rollback_to_a"		{ ROLLBACK TO a; }
+step "s1_commit"		{ COMMIT; }
+step "s1_select"		{ SELECT * FROM t; }
+step "s1_sleep" { SELECT pg_sleep(2); }
+
+session "s2"
+setup			{ BEGIN; }
+step "s2_insert_2_2"		{ INSERT INTO t VALUES (2, 2); }
+step "s2_commit"		{ COMMIT; }
+
+permutation "s1_insert_1_1" "s1_savepoint_a" "s1_insert_2_1" "s1_rollback_to_a" "s2_insert_2_2" "s1_commit" "s2_commit" "s1_select"
+permutation "s1_insert_1_1" "s1_savepoint_a" "s1_insert_2_1" "s1_rollback_to_a" "s1_sleep" "s2_insert_2_2" "s1_commit" "s2_commit" "s1_select"

src/postgres/src/test/isolation/yb_pg_isolation_schedule
@@ -32,3 +32,5 @@ test: nowait-2
 test: nowait-3
 test: aborted-keyrevoke
 test: delete-abort-savept-2
+test: ignore-intent-of-rolled-back-savepoint-cross-txn
+test: ensure-lock-only-conflict-always-ignored