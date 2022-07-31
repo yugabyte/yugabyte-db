1a91d194b0d96afd50ee5355beb73db2d83f8f3b
Mikhail Bautin <mbautin@users.noreply.github.com>
2022-07-30 21:32:55 -0700

    Revert "[#8398][YSQL] Enable concurrent transactions on remaining ALTER TABLE DDL Statements"
    Summary: This reverts commit cb407a68afe758da4da9112fd81b413c6e714147 ( https://phabricator.dev.yugabyte.com/D12505 ), which introduced a lot of broken PgAlterTableChangePrimaryKey tests.
    
    Test Plan: Jenkins
    
    Reviewers: ena, myang, aagrawal
    
    Reviewed By: aagrawal
    
    Subscribers: yql, bogdan
    
    Differential Revision: https://phabricator.dev.yugabyte.com/D18649

--- src/yb/yql/pggate/pggate.h
+++ src/yb/yql/pggate/pggate.h
@@ -290,8 +290,6 @@ class PgApiImpl {
   Status AlterTableRenameTable(PgStatement *handle, const char *db_name,
                                        const char *newname);
 
-  Status AlterTableIncrementSchemaVersion(PgStatement *handle);
-
   Status ExecAlterTable(PgStatement *handle);
 
   Status NewDropTable(const PgObjectId& table_id,
