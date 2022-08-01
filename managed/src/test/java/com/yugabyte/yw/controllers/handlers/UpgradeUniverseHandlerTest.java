bae2df11ede39c761e2e7cffc29e8a6e1b8d463d
Charles Wang <wang.charles234@gmail.com>
2022-08-01 14:32:02 -0700

    [PLAT-4815] Record old and new gflag values in audit payload for GFlag Upgrade tasks
    Summary:
    When running upgradeGFlags task, we are now able to store old, new, and default values for gflags that are going to be changed in the upgrade in the `additional_details` column of the `audit` table.
    
    We are able to handle adding, removing, and modifying gflags for a universe
    
    The payload is structured as follows:
    ```
    {"gflags":
      {"master":
        [{"name":"gFlagName1",
           "old":"oldGFlagValue1",
           "new":"newGFlagValue1",
           "default":"defaultGFlagValue1"}],
      "tserver":
        [{"name":"gFlagName2",
           "old":"oldGFlagValue2",
           "new":"newGFlagValue2",
           "default":"defaultGFlagValue1"}]
      }
    }```
    
    If certain values do not exist, they will be set as `null`
    
    Also, added the request json blob the payload column in the audit table for upgrades, as it was removed during upgrade refactor code
    
    Test Plan:
    Create a 3 node universe (AWS or GCP)
    
    a) Upgrade GFlags from a clean new universe
    1. Go to actions in the universe page and click`Edit Flags`
    2. Click Add Flags -> Add to T-Server -> `find max_log_size`, change that to a different value other than 1800 (ex: 100) -> click Add Flag
    3. Modify Master Value for `max_log_size` to 200
    4. Set to Rolling upgrade with delay between servers to `0`
    5. Click apply changes
    6. Go to local postgres (most likely docker), and `exec` into it. For docker: `docker exec -it postgres psql -d postgres -U postgres`
    7. Connect to yugaware database, `\c yugaware`
    8.  Perform this query `select * from audit where action = 'Upgrade GFlags' and additional_details != ''  ORDER BY  id desc;` We should see an audit row with the new payload added. Something similar to:
    ```{"gflags": {"master":[{"name":"max_log_size","old":null,"new":"200","default":"1800"}],"tserver":[{"name":"max_log_size","old":null,"new":"100","default":"1800"}]}}```
    
    b) Upgrade GFlags again with same gflag but different values. This time, we should see the correct old values, from the previous gflags upgrade and the new values. Say we changes the new gflag values for both tserver and master to 500, we should have something similar to:
    ```{"gflags": {"master":[{"name":"max_log_size","old":"200","new":"500","default":"1800"}],"tserver":[{"name":"max_log_size","old":"100","new":"500","default":"1800"}]}}```
    
    c) Upgrade GFlags another time but remove all these gFlags. We should see the new values for the gflag to be set as `null`
    ```{"gflags": {"master":[{"name":"max_log_size","old":"500","new":null,"default":"1800"}],"tserver":[{"name":"max_log_size","old":"500","new":null,"default":"1800"}]}}```
    
    d) Perform, a rolling upgrade or any other upgrade task. Make sure that the upgrade and audit record works as previously intended
    
    Reviewers: sanketh, yshchetinin
    
    Reviewed By: yshchetinin
    
    Subscribers: yshchetinin, yugaware
    
    Differential Revision: https://phabricator.dev.yugabyte.com/D18670

--- managed/src/test/java/com/yugabyte/yw/controllers/handlers/UpgradeUniverseHandlerTest.java
+++ managed/src/test/java/com/yugabyte/yw/controllers/handlers/UpgradeUniverseHandlerTest.java
@@ -8,8 +8,10 @@ import static org.mockito.Mockito.when;
 
 import com.yugabyte.yw.commissioner.Commissioner;
 import com.yugabyte.yw.commissioner.Common;
+import com.yugabyte.yw.commissioner.tasks.UniverseDefinitionTaskBase.ServerType;
 import com.yugabyte.yw.common.KubernetesManagerFactory;
 import com.yugabyte.yw.common.config.RuntimeConfigFactory;
+import com.yugabyte.yw.common.gflags.GFlagDiffEntry;
 import com.yugabyte.yw.forms.ITaskParams;
 import com.yugabyte.yw.forms.ResizeNodeParams;
 import com.yugabyte.yw.forms.TlsToggleParams;
@@ -19,6 +21,9 @@ import com.yugabyte.yw.models.Universe;
 import com.yugabyte.yw.models.helpers.DeviceInfo;
 import com.yugabyte.yw.models.helpers.TaskType;
 import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.UUID;
 import junitparams.JUnitParamsRunner;
 import junitparams.Parameters;
@@ -42,7 +47,8 @@ public class UpgradeUniverseHandlerTest {
         new UpgradeUniverseHandler(
             mockCommissioner,
             mock(KubernetesManagerFactory.class),
-            mock(RuntimeConfigFactory.class));
+            mock(RuntimeConfigFactory.class),
+            mock(GFlagsValidationHandler.class));
   }
 
   private static Object[] tlsToggleCustomTypeNameParams() {
@@ -129,5 +135,52 @@ public class UpgradeUniverseHandlerTest {
     Assert.assertEquals(NEW_INSTANCE_TYPE, submitIntent.instanceType);
     Assert.assertEquals(150, submitIntent.deviceInfo.volumeSize.intValue());
     Assert.assertEquals(2, submitIntent.deviceInfo.numVolumes.intValue());
+  }
+
+  @Test
+  public void testGenerateGFlagEntries() {
+    Map<String, String> oldGFlags = new HashMap<>();
+    Map<String, String> newGFlags = new HashMap<>();
+    String serverType = ServerType.TSERVER.toString();
+    String softwareVersion = "2.6";
+
+    // removed gflag
+    oldGFlags.put("stderrthreshold", "1");
+    List<GFlagDiffEntry> gFlagDiffEntries =
+        handler.generateGFlagEntries(oldGFlags, newGFlags, serverType, softwareVersion);
+    Assert.assertEquals(1, gFlagDiffEntries.size());
+    Assert.assertEquals("stderrthreshold", gFlagDiffEntries.get(0).name);
+    Assert.assertEquals("1", gFlagDiffEntries.get(0).oldValue);
+    Assert.assertEquals(null, gFlagDiffEntries.get(0).newValue);
+
+    // added gflag
+    oldGFlags.clear();
+    newGFlags.clear();
+    newGFlags.put("minloglevel", "2");
+    gFlagDiffEntries =
+        handler.generateGFlagEntries(oldGFlags, newGFlags, serverType, softwareVersion);
+    Assert.assertEquals("minloglevel", gFlagDiffEntries.get(0).name);
+    Assert.assertEquals(null, gFlagDiffEntries.get(0).oldValue);
+    Assert.assertEquals("2", gFlagDiffEntries.get(0).newValue);
+
+    // updated gflag
+    oldGFlags.clear();
+    newGFlags.clear();
+    oldGFlags.put("max_log_size", "0");
+    newGFlags.put("max_log_size", "1000");
+    gFlagDiffEntries =
+        handler.generateGFlagEntries(oldGFlags, newGFlags, serverType, softwareVersion);
+    Assert.assertEquals("max_log_size", gFlagDiffEntries.get(0).name);
+    Assert.assertEquals("0", gFlagDiffEntries.get(0).oldValue);
+    Assert.assertEquals("1000", gFlagDiffEntries.get(0).newValue);
+
+    // unchanged gflag
+    oldGFlags.clear();
+    newGFlags.clear();
+    oldGFlags.put("max_log_size", "2000");
+    newGFlags.put("max_log_size", "2000");
+    gFlagDiffEntries =
+        handler.generateGFlagEntries(oldGFlags, newGFlags, serverType, softwareVersion);
+    Assert.assertEquals(0, gFlagDiffEntries.size());
   }
 }
