yugabyte/yugabyte-db

commit c37ec4ae0cfce04426759bc6f3a048d00862d4e8

[#9944] DocDB: Prevent duplicate schedules in the same keyspace in yb-admin
Summary:
The `yb-admin create_snapshot_schedule` command will now fail to create a snapshot schedule if one
already exists in the same keyspace.

Test Plan: ctest -R yb-admin-snapshot-schedule-test

Reviewers: skedia

Reviewed By: skedia

Subscribers: bogdan, zdrudi, ybase

Differential Revision: https://phabricator.dev.yugabyte.com/D18324

src/yb/master/master_snapshot_coordinator.cc
@@ -19,6 +19,7 @@
 #include <boost/multi_index/mem_fun.hpp>
 #include <boost/asio/io_context.hpp>
 
+#include "yb/common/common_types_util.h"
 #include "yb/common/snapshot.h"
 
 #include "yb/docdb/consensus_frontier.h"
@@ -530,6 +531,22 @@ class MasterSnapshotCoordinator::Impl {
 
   Result<SnapshotScheduleId> CreateSchedule(
       const CreateSnapshotScheduleRequestPB& req, int64_t leader_term, CoarseTimePoint deadline) {
+    // Get the validated table from the request.
+    const TableIdentifierPB& table = VERIFY_RESULT(ParseCreateSnapshotScheduleRequest(req));
+
+    // Fail the request if at least one keyspace in the requested snapshot schedules already exists.
+    const std::string namespace_name = table.namespace_().name();
+    const YQLDatabase namespace_type = table.namespace_().database_type();
+    {
+      std::lock_guard<std::mutex> lock(mutex_);
+      const auto& existing_schedule = FindSnapshotSchedule(namespace_name, namespace_type);
+      if (existing_schedule.ok()) {
+        return STATUS(AlreadyPresent, "Snapshot schedule already exists for the given keyspace",
+                      Format("$0.$1", DatabaseTypeName(namespace_type), namespace_name),
+                      MasterError(MasterErrorPB::OBJECT_ALREADY_PRESENT));
+      }
+    }
+
     SnapshotScheduleState schedule(&context_, req);
 
     docdb::KeyValueWriteBatchPB write_batch;
@@ -538,6 +555,39 @@ class MasterSnapshotCoordinator::Impl {
     RETURN_NOT_OK(SynchronizedWrite(std::move(write_batch), leader_term, deadline, &context_));
 
     return schedule.id();
+  }
+
+  Result<const TableIdentifierPB&> ParseCreateSnapshotScheduleRequest(
+      const CreateSnapshotScheduleRequestPB& req) {
+    if (req.options().filter().tables().tables_size() == 0) {
+      return STATUS(InvalidArgument, "Request must contain a keyspace",
+                    MasterError(MasterErrorPB::INVALID_REQUEST));
+    }
+
+    if (req.options().filter().tables().tables_size() > 1) {
+      return STATUS(InvalidArgument, "Request cannot contain more than one keyspace",
+                    MasterError(MasterErrorPB::INVALID_REQUEST));
+    }
+
+    const auto& table = req.options().filter().tables().tables()[0];
+
+    if (!table.has_namespace_()) {
+      return STATUS(InvalidArgument, "Request does not contain a keyspace. Snapshot schedules must "
+                    "be created at the db level", MasterError(MasterErrorPB::INVALID_REQUEST));
+    }
+
+    if (table.namespace_().database_type() != YQLDatabase::YQL_DATABASE_CQL &&
+        table.namespace_().database_type() != YQLDatabase::YQL_DATABASE_PGSQL) {
+      return STATUS(InvalidArgument, "The keyspace must be of type YSQL or YCQL",
+                    MasterError(MasterErrorPB::INVALID_REQUEST));
+    }
+
+    if (table.namespace_().name().empty()) {
+      return STATUS(InvalidArgument, "The keyspace name must be non-empty",
+                    MasterError(MasterErrorPB::INVALID_REQUEST));
+    }
+
+    return table;
   }
 
   Status ListSnapshotSchedules(
@@ -905,6 +955,33 @@ class MasterSnapshotCoordinator::Impl {
                     MasterError(MasterErrorPB::SNAPSHOT_NOT_FOUND));
     }
     return **it;
+  }
+
+  Result<SnapshotScheduleState&> FindSnapshotSchedule(
+      const std::string& namespace_name,
+      const YQLDatabase namespace_type,
+      const bool ignore_deleted = true) REQUIRES(mutex_) {
+    for (const auto& schedule : schedules_) {
+      if (ignore_deleted && schedule->deleted()) {
+        continue;
+      }
+
+      DCHECK_EQ(schedule->options().filter().tables().tables_size(), 1);
+      if (schedule->options().filter().tables().tables_size() != 1) {
+        LOG(WARNING) << Format("Detected a schedule with an unexpected number of identifiers: $1",
+                               schedule->options().filter().tables().tables_size());
+      }
+
+      for (const auto& table : schedule->options().filter().tables().tables()) {
+        if (table.namespace_().name() == namespace_name &&
+            table.namespace_().database_type() == namespace_type) {
+          return *schedule;
+        }
+      }
+    }
+
+    return STATUS(NotFound, "Could not find snapshot schedule", namespace_name,
+                    MasterError(MasterErrorPB::SNAPSHOT_NOT_FOUND));
   }
 
   void ExecuteOperations(const TabletSnapshotOperations& operations, int64_t leader_term) {

src/yb/tools/yb-admin-snapshot-schedule-test.cc
@@ -3604,5 +3604,23 @@ TEST_F_EX(
   ASSERT_EQ(res, "after");
 }
 
+TEST_F_EX(YbAdminSnapshotScheduleTest, YB_DISABLE_TEST_IN_TSAN(CreateDuplicateSchedules),
+          YbAdminSnapshotScheduleTestWithYsql) {
+  ASSERT_OK(PrepareCommon());
+
+  auto conn = ASSERT_RESULT(PgConnect());
+  ASSERT_OK(conn.ExecuteFormat("CREATE DATABASE $0", client::kTableName.namespace_name()));
+
+  ASSERT_OK(CreateSnapshotScheduleAndWaitSnapshot(
+      "ysql." + client::kTableName.namespace_name(), kInterval, kRetention));
+
+  // Try and fail to create a snapshot in the same keyspace.
+  auto res = CreateSnapshotSchedule(kInterval, kRetention, "ysql." + kTableName.namespace_name());
+  ASSERT_FALSE(res.ok());
+  ASSERT_STR_CONTAINS(res.ToString(),
+    "Snapshot schedule already exists for the given keyspace: "
+    "ysql." + kTableName.namespace_name());
+}
+
 }  // namespace tools
 }  // namespace yb

src/yb/common/CMakeLists.txt
@@ -68,6 +68,7 @@ ADD_YB_LIBRARY(yb_common_base
 set(COMMON_SRCS
   clock.cc
   common_flags.cc
+  common_types_util.cc
   column_id.cc
   consistent_read_point.cc
   entity_ids.cc

src/yb/common/common_types_util.cc
@@ -1,0 +1,52 @@
+// Copyright (c) YugaByte, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+// in compliance with the License.  You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software distributed under the License
+// is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+// or implied.  See the License for the specific language governing permissions and limitations
+// under the License.
+//
+
+#include "yb/common/common_types_util.h"
+
+#include <array>
+#include <utility>
+
+#include <glog/logging.h>
+
+using std::array;
+using std::make_pair;
+using std::pair;
+using std::string;
+
+namespace yb {
+
+const char* DatabaseTypeName(YQLDatabase db) {
+  switch (db) {
+    case YQL_DATABASE_UNKNOWN: break;
+    case YQL_DATABASE_CQL: return kDBTypeNameCql;
+    case YQL_DATABASE_PGSQL: return kDBTypeNamePgsql;
+    case YQL_DATABASE_REDIS: return kDBTypeNameRedis;
+  }
+  CHECK(false) << "Unexpected db type " << db;
+  return kDBTypeNameUnknown;
+}
+
+YQLDatabase DatabaseTypeByName(const string& db_type_name) {
+  static const array<pair<const char*, YQLDatabase>, 3> db_types{
+    make_pair(kDBTypeNameCql, YQLDatabase::YQL_DATABASE_CQL),
+    make_pair(kDBTypeNamePgsql, YQLDatabase::YQL_DATABASE_PGSQL),
+    make_pair(kDBTypeNameRedis, YQLDatabase::YQL_DATABASE_REDIS)};
+  for (const auto& db : db_types) {
+    if (db_type_name == db.first) {
+      return db.second;
+    }
+  }
+  return YQLDatabase::YQL_DATABASE_UNKNOWN;
+}
+
+} // namespace yb

src/yb/common/common_types_util.h
@@ -1,0 +1,34 @@
+// Copyright (c) YugaByte, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+// in compliance with the License.  You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software distributed under the License
+// is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+// or implied.  See the License for the specific language governing permissions and limitations
+// under the License.
+//
+
+#ifndef YB_COMMON_COMMON_TYPES_UTIL_H
+#define YB_COMMON_COMMON_TYPES_UTIL_H
+
+#include "yb/common/common_types.pb.h"
+
+namespace yb {
+
+static constexpr const char* kDBTypeNameUnknown = "unknown";
+static constexpr const char* kDBTypeNameCql = "ycql";
+static constexpr const char* kDBTypeNamePgsql = "ysql";
+static constexpr const char* kDBTypeNameRedis = "yedis";
+
+// Returns the string name of a db type.
+const char* DatabaseTypeName(YQLDatabase db);
+
+// Returns the db type from its string name.
+YQLDatabase DatabaseTypeByName(const std::string& db_type_name);
+
+} // namespace yb
+
+#endif  // YB_COMMON_COMMON_TYPES_UTIL_H

src/yb/master/master-path-handlers.cc
@@ -39,6 +39,7 @@
 #include <sstream>
 #include <unordered_set>
 
+#include "yb/common/common_types_util.h"
 #include "yb/common/hybrid_time.h"
 #include "yb/common/partition.h"
 #include "yb/common/schema.h"
@@ -88,36 +89,8 @@ DECLARE_int32(ysql_tablespace_info_refresh_secs);
 namespace yb {
 
 namespace {
-static constexpr const char* kDBTypeNameUnknown = "unknown";
-static constexpr const char* kDBTypeNameCql = "ycql";
-static constexpr const char* kDBTypeNamePgsql = "ysql";
-static constexpr const char* kDBTypeNameRedis = "yedis";
 
 const int64_t kCurlTimeoutSec = 180;
-
-const char* DatabaseTypeName(YQLDatabase db) {
-  switch (db) {
-    case YQL_DATABASE_UNKNOWN: break;
-    case YQL_DATABASE_CQL: return kDBTypeNameCql;
-    case YQL_DATABASE_PGSQL: return kDBTypeNamePgsql;
-    case YQL_DATABASE_REDIS: return kDBTypeNameRedis;
-  }
-  CHECK(false) << "Unexpected db type " << db;
-  return kDBTypeNameUnknown;
-}
-
-YQLDatabase DatabaseTypeByName(const string& db_type_name) {
-  static const std::array<pair<const char*, YQLDatabase>, 3> db_types{
-      make_pair(kDBTypeNameCql, YQLDatabase::YQL_DATABASE_CQL),
-      make_pair(kDBTypeNamePgsql, YQLDatabase::YQL_DATABASE_PGSQL),
-      make_pair(kDBTypeNameRedis, YQLDatabase::YQL_DATABASE_REDIS)};
-  for (const auto& db : db_types) {
-    if (db_type_name == db.first) {
-      return db.second;
-    }
-  }
-  return YQLDatabase::YQL_DATABASE_UNKNOWN;
-}
 
 std::optional<HostPortPB> GetPublicHttpHostPort(const ServerRegistrationPB& registration) {
   if (registration.http_addresses().empty()) {

commit 7d86211b8faae08ff21b8ff07e1ed6c1c2b5a34b

[#9944] DocDB: Misc Cleanup
Summary:
Commit c37ec4ae0cfce04426759bc6f3a048d00862d4e8 caused some SnapshotScheduleTest cases to fail.

I accidentally ran `arc land` instead of `arc patch` on the fix for #9944. This patch is a follow-up
that fixes a few style issues and a test-only failure.

Test Plan: ./yb_build.sh --cxx_test snapshot-schedule-test

Reviewers: skedia

Reviewed By: skedia

Subscribers: mbautin, ybase, bogdan

Differential Revision: https://phabricator.dev.yugabyte.com/D18692

src/yb/client/snapshot_test_util.cc
@@ -258,13 +258,14 @@ Result<ImportedSnapshotData> SnapshotTestUtil::StartImportSnapshot(
 }
 
 Result<SnapshotScheduleId> SnapshotTestUtil::CreateSchedule(
-    const TableHandle& table, MonoDelta interval, MonoDelta retention) {
-  return CreateSchedule(table, WaitSnapshot::kFalse, interval, retention);
+    const TableHandle& table, const YQLDatabase db_type, const std::string& db_name,
+    const MonoDelta interval, const MonoDelta retention) {
+  return CreateSchedule(table, db_type, db_name, WaitSnapshot::kFalse, interval, retention);
 }
 
 Result<SnapshotScheduleId> SnapshotTestUtil::CreateSchedule(
-    const TableHandle& table, WaitSnapshot wait_snapshot,
-    MonoDelta interval, MonoDelta retention) {
+    const TableHandle& table, const YQLDatabase db_type, const std::string& db_name,
+    const WaitSnapshot wait_snapshot, const MonoDelta interval, const MonoDelta retention) {
   rpc::RpcController controller;
   controller.set_timeout(60s);
   master::CreateSnapshotScheduleRequestPB req;
@@ -272,7 +273,11 @@ Result<SnapshotScheduleId> SnapshotTestUtil::CreateSchedule(
   options.set_interval_sec(interval.ToSeconds());
   options.set_retention_duration_sec(retention.ToSeconds());
   auto& tables = *options.mutable_filter()->mutable_tables()->mutable_tables();
-  tables.Add()->set_table_id(table.table()->id());
+  master::TableIdentifierPB* table_identifier = tables.Add();
+  table_identifier->set_table_id(table.table()->id());
+  master::NamespaceIdentifierPB* namespace_identifier = table_identifier->mutable_namespace_();
+  namespace_identifier->set_database_type(db_type);
+  namespace_identifier->set_name(db_name);
   master::CreateSnapshotScheduleResponsePB resp;
   RETURN_NOT_OK(
       VERIFY_RESULT(MakeBackupServiceProxy()).CreateSnapshotSchedule(req, &resp, &controller));

src/yb/client/snapshot_test_util.h
@@ -89,11 +89,12 @@ class SnapshotTestUtil {
   Status WaitAllSnapshotsCleaned();
 
   Result<SnapshotScheduleId> CreateSchedule(
-      const TableHandle& table,
+      const TableHandle& table, YQLDatabase db_type, const std::string& db_name,
       MonoDelta interval = kSnapshotInterval, MonoDelta retention = kSnapshotRetention);
   Result<SnapshotScheduleId> CreateSchedule(
-      const TableHandle& table, WaitSnapshot wait_snapshot,
-      MonoDelta interval = kSnapshotInterval, MonoDelta retention = kSnapshotRetention);
+      const TableHandle& table, YQLDatabase db_type, const std::string& db_name,
+      WaitSnapshot wait_snapshot, MonoDelta interval = kSnapshotInterval,
+      MonoDelta retention = kSnapshotRetention);
 
   Result<Schedules> ListSchedules(const SnapshotScheduleId& id = SnapshotScheduleId::Nil());
 

src/yb/master/master_snapshot_coordinator.cc
@@ -532,17 +532,19 @@ class MasterSnapshotCoordinator::Impl {
   Result<SnapshotScheduleId> CreateSchedule(
       const CreateSnapshotScheduleRequestPB& req, int64_t leader_term, CoarseTimePoint deadline) {
     // Get the validated table from the request.
-    const TableIdentifierPB& table = VERIFY_RESULT(ParseCreateSnapshotScheduleRequest(req));
+    const auto& table = VERIFY_RESULT(ParseCreateSnapshotScheduleRequest(req));
 
     // Fail the request if at least one keyspace in the requested snapshot schedules already exists.
-    const std::string namespace_name = table.namespace_().name();
-    const YQLDatabase namespace_type = table.namespace_().database_type();
+    const std::string& namespace_name = table.get().namespace_().name();
+    const YQLDatabase namespace_type = table.get().namespace_().database_type();
     {
       std::lock_guard<std::mutex> lock(mutex_);
       const auto& existing_schedule = FindSnapshotSchedule(namespace_name, namespace_type);
       if (existing_schedule.ok()) {
-        return STATUS(AlreadyPresent, "Snapshot schedule already exists for the given keyspace",
-                      Format("$0.$1", DatabaseTypeName(namespace_type), namespace_name),
+        return STATUS(AlreadyPresent,
+                      Format("Snapshot schedule $0 already exists for the given keyspace $1.$2",
+                             existing_schedule->id(), DatabaseTypeName(namespace_type),
+                             namespace_name),
                       MasterError(MasterErrorPB::OBJECT_ALREADY_PRESENT));
       }
     }

src/yb/client/snapshot-schedule-test.cc
@@ -57,7 +57,8 @@ class SnapshotScheduleTest : public TransactionTestBase<MiniCluster> {
 TEST_F(SnapshotScheduleTest, Create) {
   std::vector<SnapshotScheduleId> ids;
   for (int i = 0; i != 3; ++i) {
-    auto id = ASSERT_RESULT(snapshot_util_->CreateSchedule(table_));
+    auto id = ASSERT_RESULT(snapshot_util_->CreateSchedule(
+      table_, YQLDatabase::YQL_DATABASE_PGSQL, Format("yugabyte.$0", i)));
     LOG(INFO) << "Schedule " << i << " id: " << id;
     ids.push_back(id);
 
@@ -85,7 +86,8 @@ TEST_F(SnapshotScheduleTest, Snapshot) {
   FLAGS_timestamp_history_retention_interval_sec = kTimeMultiplier;
 
   ASSERT_NO_FATALS(WriteData());
-  auto schedule_id = ASSERT_RESULT(snapshot_util_->CreateSchedule(table_));
+  auto schedule_id = ASSERT_RESULT(
+    snapshot_util_->CreateSchedule(table_, YQLDatabase::YQL_DATABASE_PGSQL, "yugabyte"));
   ASSERT_OK(snapshot_util_->WaitScheduleSnapshot(schedule_id));
 
   // Write data to update history retention.
@@ -134,7 +136,8 @@ TEST_F(SnapshotScheduleTest, Snapshot) {
 TEST_F(SnapshotScheduleTest, GC) {
   FLAGS_snapshot_coordinator_cleanup_delay_ms = 100;
   // When retention matches snapshot interval we expect at most 2 snapshots for schedule.
-  ASSERT_RESULT(snapshot_util_->CreateSchedule(table_, kSnapshotInterval, kSnapshotInterval));
+  ASSERT_RESULT(snapshot_util_->CreateSchedule(
+    table_, YQLDatabase::YQL_DATABASE_PGSQL, "yugabyte", kSnapshotInterval, kSnapshotInterval));
 
   std::unordered_set<SnapshotScheduleId, SnapshotScheduleIdHash> all_snapshot_ids;
   while (all_snapshot_ids.size() < 4) {
@@ -168,7 +171,8 @@ TEST_F(SnapshotScheduleTest, GC) {
 TEST_F(SnapshotScheduleTest, Index) {
   FLAGS_timestamp_history_retention_interval_sec = kTimeMultiplier;
 
-  auto schedule_id = ASSERT_RESULT(snapshot_util_->CreateSchedule(table_));
+  auto schedule_id = ASSERT_RESULT(snapshot_util_->CreateSchedule(
+    table_, YQLDatabase::YQL_DATABASE_PGSQL, "yugabyte"));
   ASSERT_OK(snapshot_util_->WaitScheduleSnapshot(schedule_id));
 
   CreateIndex(Transactional::kTrue, 1, false);
@@ -216,7 +220,8 @@ TEST_F(SnapshotScheduleTest, Index) {
 
 TEST_F(SnapshotScheduleTest, Restart) {
   ASSERT_NO_FATALS(WriteData());
-  auto schedule_id = ASSERT_RESULT(snapshot_util_->CreateSchedule(table_));
+  auto schedule_id = ASSERT_RESULT(
+    snapshot_util_->CreateSchedule(table_, YQLDatabase::YQL_DATABASE_PGSQL, "yugabyte"));
   ASSERT_OK(snapshot_util_->WaitScheduleSnapshot(schedule_id));
   ASSERT_OK(cluster_->RestartSync());
 
@@ -228,7 +233,8 @@ TEST_F(SnapshotScheduleTest, Restart) {
 
 TEST_F(SnapshotScheduleTest, RestoreSchema) {
   ASSERT_NO_FATALS(WriteData());
-  auto schedule_id = ASSERT_RESULT(snapshot_util_->CreateSchedule(table_));
+  auto schedule_id = ASSERT_RESULT(
+    snapshot_util_->CreateSchedule(table_, YQLDatabase::YQL_DATABASE_PGSQL, "yugabyte"));
   auto hybrid_time = cluster_->mini_master(0)->master()->clock()->Now();
   auto old_schema = table_.schema();
   auto alterer = client_->NewTableAlterer(table_.name());
@@ -262,7 +268,8 @@ TEST_F(SnapshotScheduleTest, RemoveNewTablets) {
   const auto kInterval = 5s * kTimeMultiplier;
   const auto kRetention = kInterval * 2;
   auto schedule_id = ASSERT_RESULT(snapshot_util_->CreateSchedule(
-      table_, WaitSnapshot::kTrue, kInterval, kRetention));
+      table_, YQLDatabase::YQL_DATABASE_PGSQL, "yugabyte", WaitSnapshot::kTrue, kInterval,
+      kRetention));
   auto before_index_ht = cluster_->mini_master(0)->master()->clock()->Now();
   CreateIndex(Transactional::kTrue, 1, false);
   auto after_time_ht = cluster_->mini_master(0)->master()->clock()->Now();

src/yb/integration-tests/tablet-split-itest.cc
@@ -2161,7 +2161,8 @@ Status TabletSplitSingleServerITest::TestSplitBeforeParentDeletion(bool hide_onl
     auto snapshot_util = std::make_unique<client::SnapshotTestUtil>();
     snapshot_util->SetProxy(&client_->proxy_cache());
     snapshot_util->SetCluster(cluster_.get());
-    VERIFY_RESULT(snapshot_util->CreateSchedule(table_));
+    VERIFY_RESULT(
+      snapshot_util->CreateSchedule(table_, YQLDatabase::YQL_DATABASE_PGSQL, "yugabyte"));
   }
 
   const auto split_hash_code = VERIFY_RESULT(WriteRowsAndGetMiddleHashCode(kNumRows));

src/yb/tools/yb-admin-snapshot-schedule-test.cc
@@ -3618,8 +3618,7 @@ TEST_F_EX(YbAdminSnapshotScheduleTest, YB_DISABLE_TEST_IN_TSAN(CreateDuplicateSc
   auto res = CreateSnapshotSchedule(kInterval, kRetention, "ysql." + kTableName.namespace_name());
   ASSERT_FALSE(res.ok());
   ASSERT_STR_CONTAINS(res.ToString(),
-    "Snapshot schedule already exists for the given keyspace: "
-    "ysql." + kTableName.namespace_name());
+    "already exists for the given keyspace ysql." + kTableName.namespace_name());
 }
 
 }  // namespace tools