d586f50acc53e5a4e9452d882c9100789fe72aff
Hari Krishna Sunder <hari90@users.noreply.github.com>
2022-08-10 10:29:27 -0700

    [#13559] DocDB: Add file name and line number to StackTrace in Debug mode
    Summary:
    StackTraceLineFormat::DEFAULT is currently set to SHORT.
    Use CLION_CLICKABLE in Debug mode so that we print filename and line number.
    
    New trace :
    
    ```
    long_operation_tracker.cc:117	TSHeartbeat running for 1.000s in thread 255540:
         @ 0x7f87033fcb1f
         @ 0x7f8703781837
    src/yb/util/condition_variable.cc:136: @ 0x7f8705553761 yb::ConditionVariable::TimedWait(yb::MonoDelta const&) const
    src/yb/util/condition_variable.cc:88: @ 0x7f870555387f yb::ConditionVariable::WaitUntil(yb::MonoTime const&) const
    src/yb/util/countdown_latch.cc:69: @ 0x7f8705553bbd yb::CountDownLatch::WaitUntil(yb::MonoTime) const
    src/yb/util/countdown_latch.cc:59: @ 0x7f8705553c1a yb::CountDownLatch::WaitUntil(std::chrono::time_point<yb::CoarseMonoClock, std::chrono::duration<long, std::ratio<1l, 1000000000l> > >) const
    src/yb/master/sys_catalog.cc:703: @ 0x7f87092cb3ef yb::master::SysCatalogTable::SyncWrite(yb::master::SysCatalogWriter*)
    src/yb/master/sys_catalog-internal.h:84: @ 0x7f870911fe0d yb::Status yb::master::SysCatalogTable::Mutate<std::vector<yb::master::TabletInfo*, std::allocator<yb::master::TabletInfo*> >&>(yb::QLWriteRequestPB_QLStmtType, long, std::vector<yb::master::TabletInfo*, std::allocator<yb::master::TabletInfo*> >&)
    src/yb/master/sys_catalog-internal.h:71: @ 0x7f87091b07c9 yb::Status yb::master::SysCatalogTable::Upsert<std::vector<yb::master::TabletInfo*, std::allocator<yb::master::TabletInfo*> >&>(long, std::vector<yb::master::TabletInfo*, std::allocator<yb::master::TabletInfo*> >&)
    ```
    
    Old trace:
    
    ```
    long_operation_tracker.cc:113] Read running for 3.485s in thread 139:
        @     0x7f1610a7711f  (unknown)
        @     0x7f161eb0a860  yb::docdb::SubDocument::MoveFrom()
        @     0x7f161eb1708c  yb::docdb::(anonymous namespace)::ProcessSubDocument()
        @     0x7f161eb18d8b  yb::docdb::SubDocumentReader::Get()
        @     0x7f161ea93fb3  yb::docdb::DocDBTableReader::Get()
        @     0x7f161eabe14d  yb::docdb::DocRowwiseIterator::HasNext()
    ```
    
    Test Plan: Jenkins tests
    
    Reviewers: mbautin
    
    Reviewed By: mbautin
    
    Subscribers: ybase
    
    Differential Revision: https://phabricator.dev.yugabyte.com/D18845

--- src/yb/util/debug-util-test.cc
+++ src/yb/util/debug-util-test.cc
@@ -84,17 +84,23 @@ TEST_F(DebugUtilTest, TestStackTrace) {
 TEST_F(DebugUtilTest, TestGetStackTrace) {
   string stack_trace = GetStackTrace();
 
-  const std::string kExpectedLineFormatNoFileLineReStr = R"#(^\s*@\s+0x[0-9a-f]+\s+.*)#";
+  const std::string kExpectedLineFormatNoFileLineReStr = R"#(\s*@\s+0x[0-9a-f]+\s+.*)#";
   SCOPED_TRACE(Format("Regex with no file/line: $0", kExpectedLineFormatNoFileLineReStr));
-  const std::string kFileLineReStr = R"#( \(\S+:\d+\))#";
+  const std::string kFileLineReStr = R"#(\S+:\d+:)#";
+  const std::string kFileLineInBracesReStr = R"#( \(\S+:\d+\))#";
 
-  const std::regex kExpectedLineFormatNoFileLineRe(kExpectedLineFormatNoFileLineReStr + "$");
+  const std::regex kExpectedLineFormatNoFileLineRe("^" + kExpectedLineFormatNoFileLineReStr + "$");
   const std::regex kExpectedLineFormatWithFileLineRe(
-      kExpectedLineFormatNoFileLineReStr + kFileLineReStr + "$");
+      "^" + kFileLineReStr + kExpectedLineFormatNoFileLineReStr + "$");
+  const std::regex kExpectedLineFormatWithFileLineInBracesRe(
+      "^" + kExpectedLineFormatNoFileLineReStr + kFileLineInBracesReStr + "$");
   const std::regex kNilUnknownRe(R"#(^\s*@\s+\(nil\)\s+\(unknown\)$)#");
 
   // Expected line format:
-  // @ 0x41255d yb::DebugUtilTest_TestGetStackTrace_Test::TestBody() (yb/util/debug-util-test.cc:73)
+  // @ 0x41255d yb::DebugUtilTest_TestGetStackTrace_Test::TestBody() (yb/util/debug-util-test.cc:85)
+  // or
+  // ../../src/yb/util/debug-util-test.cc:85: @ 0x270715
+  // yb::DebugUtilTest_TestGetStackTrace_Test::TestBody()
   SCOPED_TRACE(Format("Stack trace to be checked:\n:$0", stack_trace));
   std::stringstream ss(stack_trace);
   std::smatch match;
@@ -109,7 +115,8 @@ TEST_F(DebugUtilTest, TestGetStackTrace) {
   while (ss) {
     const auto line = next_line;
     std::getline(ss, next_line);
-    if (std::regex_match(line, match, kExpectedLineFormatWithFileLineRe)) {
+    if (std::regex_match(line, match, kExpectedLineFormatWithFileLineRe) ||
+        std::regex_match(line, match, kExpectedLineFormatWithFileLineInBracesRe)) {
       ++with_file_line;
       debug_info << "Line matched regex with file/line number: " << line << std::endl;
     } else if (std::regex_match(line, match, kExpectedLineFormatNoFileLineRe)) {
