5ea8a81c260df8ec10e24b11a8398f2b827737b5
govardhanjalla <gova.reddy9@gmail.com>
2022-08-16 14:13:36 -0700

    [PLAT-4654][Platform] Edit primary cluster of kubernetes can delete the primary cluster.
    Summary: Added check to see if pods are already updated. If userIntentChanged and az is found in new AZ list, we don't have to delete pods explicitly as it is taken care of in previous steps while updating existing tservers.
    
    Test Plan:
    Manually tested following scenario.
    Create universe with 2 AZs each with 4 pods with rf 3.
    Edit universe by changing instance type and removing one AZ completely without modifying number of pods in other AZ. (Other az continues to have 4 pods).
    Observed one AZ pods being terminated and all pods in other AZ are in running state.
    
    Reviewers: sanketh, bgandhi, nsingh
    
    Reviewed By: nsingh
    
    Subscribers: jenkins-bot, yugaware
    
    Differential Revision: https://phabricator.dev.yugabyte.com/D18729

--- managed/src/main/java/com/yugabyte/yw/commissioner/tasks/KubernetesTaskBase.java
+++ managed/src/main/java/com/yugabyte/yw/commissioner/tasks/KubernetesTaskBase.java
@@ -430,7 +430,7 @@ public abstract class KubernetesTaskBase extends UniverseDefinitionTaskBase {
       KubernetesPlacement currPlacement,
       String masterAddresses,
       KubernetesPlacement newPlacement,
-      boolean userIntentChange,
+      boolean instanceTypeChanged,
       boolean isMultiAz,
       boolean isReadOnlyCluster) {
     Cluster primaryCluster = taskParams().getPrimaryCluster();
@@ -479,10 +479,12 @@ public abstract class KubernetesTaskBase extends UniverseDefinitionTaskBase {
       // If the new placement also has the AZ, we need to scale down. But if there
       // was a change in the instance type, the updateRemainingPod itself would have taken care
       // of the deployments' scale down.
-
       boolean keepDeployment = false;
       if (edit) {
-        keepDeployment = newPlacement.configs.containsKey(azUUID) && !userIntentChange;
+        keepDeployment = newPlacement.configs.containsKey(azUUID);
+        if (keepDeployment && instanceTypeChanged) {
+          continue;
+        }
       }
       if (keepDeployment) {
         PlacementInfo tempPI = new PlacementInfo();
