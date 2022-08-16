c83af425ba2e363b7f2335469e99e68268734036
Eric Sheng <esheng@yugabyte.com>
2022-08-15 20:29:08 -0700

    [#7683, #13349, #13064] docdb: Fix webserver crash when enabling SSL
    Summary:
    This diff fixes a crash when enable SSL for the webserver, due to an old version
    of Squeasel:
    
    1. searching for and loading OpenSSL from the system rather than using the one bundled with
       thirdparty.
    
    2. depending on the dynamic library exposing symbols that have been removed with OpenSSL 1.1.0
       (SSLv23_... methods, which have been replaced with a macro).
    
    This is fixed by rebasing to a newer upstream commit, which does not require those symbols, and
    which further presents the ssl_global_init flag to stop Squeasel from loading OpenSSL on its own.
    
    This diff also adds the `webserver_private_key_file` and `webserver_private_key_password` gflags,
    corresponding to the `ssl_private_key` and `ssl_private_key_password` options present in newer
    versions of Squeasel.
    
    This diff also fixes a crash when pthread_setspecific fails in libcds, by catching the exception
    and forcing the worker thread to exit instead of letting it bubble up and kill the whole process.
    
    Also pulling https://github.com/yugabyte/yugabyte-db-thirdparty/commit/fc8dae1fb4c56ca4f237b72d75ce9e710943d135 to fix C++ exception handling in Clang-based LTO builds (https://github.com/yugabyte/yugabyte-db/issues/13064).
    
    Test Plan: `ybd --cxx_test server_webserver-test --gtest_filter WebserverSecureTest.TestIndexPage`
    
    Reviewers: mbautin
    
    Reviewed By: mbautin
    
    Subscribers: cwang, sanketh, ybase, bogdan
    
    Differential Revision: https://phabricator.dev.yugabyte.com/D18419

--- src/yb/server/webserver.h
+++ src/yb/server/webserver.h
@@ -43,8 +43,7 @@
 #ifndef YB_SERVER_WEBSERVER_H
 #define YB_SERVER_WEBSERVER_H
 
-#include <map>
-#include <shared_mutex>
+#include <memory>
 #include <string>
 #include <vector>
 
@@ -54,10 +53,6 @@
 #include "yb/util/net/net_fwd.h"
 #include "yb/util/web_callback_registry.h"
 
-struct sq_connection;
-struct sq_request_info;
-struct sq_context;
-
 namespace yb {
 
 // Wrapper class for the Squeasel web server library. Clients may register callback
@@ -66,7 +61,7 @@ class Webserver : public WebCallbackRegistry {
  public:
   // Using this constructor, the webserver will bind to all available
   // interfaces. The server_name is used for display purposes.
-  explicit Webserver(const WebserverOptions& opts, const std::string& server_name);
+  Webserver(const WebserverOptions& opts, const std::string& server_name);
 
   ~Webserver();
 
@@ -84,11 +79,12 @@ class Webserver : public WebCallbackRegistry {
   // Return the single HostPort that this server was asked to bind on
   Status GetInputHostPort(HostPort* hp) const;
 
-  virtual void RegisterPathHandler(const std::string& path, const std::string& alias,
-                                   const PathHandlerCallback& callback,
-                                   bool is_styled = true,
-                                   bool is_on_nav_bar = true,
-                                   const std::string icon = "") override;
+  void RegisterPathHandler(const std::string& path,
+                           const std::string& alias,
+                           const PathHandlerCallback& callback,
+                           bool is_styled = true,
+                           bool is_on_nav_bar = true,
+                           const std::string icon = "") override;
 
   // Change the footer HTML to be displayed at the bottom of all styled web pages.
   void set_footer_html(const std::string& html);
@@ -97,105 +93,8 @@ class Webserver : public WebCallbackRegistry {
   bool IsSecure() const;
 
  private:
-  // Container class for a list of path handler callbacks for a single URL.
-  class PathHandler {
-   public:
-    PathHandler(bool is_styled, bool is_on_nav_bar, std::string alias, const std::string icon)
-        : is_styled_(is_styled),
-          is_on_nav_bar_(is_on_nav_bar),
-          alias_(std::move(alias)),
-          icon_(icon) {}
-
-    void AddCallback(const PathHandlerCallback& callback) {
-      callbacks_.push_back(callback);
-    }
-
-    bool is_styled() const { return is_styled_; }
-    bool is_on_nav_bar() const { return is_on_nav_bar_; }
-    const std::string& alias() const { return alias_; }
-    const std::string& icon() const { return icon_; }
-    const std::vector<PathHandlerCallback>& callbacks() const { return callbacks_; }
-
-   private:
-    // If true, the page appears is rendered styled.
-    bool is_styled_;
-
-    // If true, the page appears in the navigation bar.
-    bool is_on_nav_bar_;
-
-    // Alias used when displaying this link on the nav bar.
-    std::string alias_;
-
-    // Icon used when displaying this link on the nav bar.
-    std::string icon_;
-
-    // List of callbacks to render output for this page, called in order.
-    std::vector<PathHandlerCallback> callbacks_;
-  };
-
-  bool static_pages_available() const;
-
-  // Build the string to pass to mongoose specifying where to bind.
-  Status BuildListenSpec(std::string* spec) const;
-
-  // Renders a common Bootstrap-styled header
-  void BootstrapPageHeader(std::stringstream* output);
-
-  // Renders a common Bootstrap-styled footer. Must be used in conjunction with
-  // BootstrapPageHeader.
-  void BootstrapPageFooter(std::stringstream* output);
-
-  static void EnterWorkerThreadCallbackStatic();
-  static void LeaveWorkerThreadCallbackStatic();
-
-  // Dispatch point for all incoming requests.
-  // Static so that it can act as a function pointer, and then call the next method
-  static int BeginRequestCallbackStatic(struct sq_connection* connection);
-  int BeginRequestCallback(struct sq_connection* connection,
-                           struct sq_request_info* request_info);
-
-  int RunPathHandler(const PathHandler& handler,
-                     struct sq_connection* connection,
-                     struct sq_request_info* request_info);
-
-  // Callback to funnel mongoose logs through glog.
-  static int LogMessageCallbackStatic(const struct sq_connection* connection,
-                                      const char* message);
-
-  // Registered to handle "/", and prints a list of available URIs
-  void RootHandler(const WebRequest& args, Webserver::WebResponse* resp);
-
-  // Builds a map of argument name to argument value from a typical URL argument
-  // string (that is, "key1=value1&key2=value2.."). If no value is given for a
-  // key, it is entered into the map as (key, "").
-  void BuildArgumentMap(const std::string& args, ArgumentMap* output);
-
-  const WebserverOptions opts_;
-
-  // Lock guarding the path_handlers_ map and footer_html.
-  std::shared_timed_mutex lock_;
-
-  // Map of path to a PathHandler containing a list of handlers for that
-  // path. More than one handler may register itself with a path so that many
-  // components may contribute to a single page.
-  typedef std::map<std::string, PathHandler*> PathHandlerMap;
-  PathHandlerMap path_handlers_;
-
-  // Snippet of HTML which will be displayed in the footer of all pages
-  // rendered by this server. Protected by 'lock_'.
-  std::string footer_html_;
-
-  // The address of the interface on which to run this webserver.
-  std::string http_address_;
-
-  // Handle to Mongoose context; owned and freed by Mongoose internally
-  struct sq_context* context_;
-
-  // Server name for display purposes
-  std::string server_name_;
-
-  // Mutex guarding against concurrenct calls to Stop().
-  std::mutex stop_mutex_;
+  class Impl;
+  std::unique_ptr<Impl> impl_;
 };
 
 } // namespace yb
