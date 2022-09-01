87871dd2ef9d48a6ff86c8eee63ab47c8670d979
Yusong Yan <Yusong.Yan@yugabyte.com>
2022-09-01 18:20:45 -0400

    [#13611] DocDB: Disable enable_lease_revocation by default
    Summary: Disabled enable_lease_revocation by default in order to temporary work around the issue in #12758
    
    Test Plan: manual test
    
    Reviewers: rthallam, mbautin
    
    Reviewed By: mbautin
    
    Subscribers: bogdan, ybase
    
    Differential Revision: https://phabricator.dev.yugabyte.com/D19002

--- src/yb/client/snapshot-txn-test.cc
+++ src/yb/client/snapshot-txn-test.cc
@@ -61,6 +61,7 @@
 
 using namespace std::literals;
 
+DECLARE_bool(enable_lease_revocation);
 DECLARE_bool(TEST_disallow_lmp_failures);
 DECLARE_bool(enable_multi_raft_heartbeat_batcher);
 DECLARE_bool(fail_on_out_of_range_clock_skew);
@@ -379,6 +380,7 @@ TEST_F(SnapshotTxnTest, BankAccountsWithTimeStrobe) {
 }
 
 TEST_F(SnapshotTxnTest, BankAccountsWithTimeJump) {
+  SetAtomicFlag(true, &FLAGS_enable_lease_revocation);
   FLAGS_fail_on_out_of_range_clock_skew = false;
 
   TestBankAccounts(
