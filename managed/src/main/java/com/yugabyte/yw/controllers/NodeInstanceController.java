b869c3281457618a0730e744da72e94ebdac9655
Yury Shchetinin <yshchetinin@yugabyte.com>
2022-09-08 18:47:17 +0300

    [BACKPORT 2.12][PLAT-5463][PLAT-5301][Platform][Onprem] Onprem Node Pre Check API returns None value
    Summary:
    Return task UUID in response for preflight check API
    Original diff: https://phabricator.dev.yugabyte.com/D19411
    
    Test Plan: run preflight check in browser and check response
    
    Reviewers: skhilar, muthu
    
    Reviewed By: muthu
    
    Subscribers: yugaware, jenkins-bot
    
    Differential Revision: https://phabricator.dev.yugabyte.com/D19526

--- managed/src/main/java/com/yugabyte/yw/controllers/NodeInstanceController.java
+++ managed/src/main/java/com/yugabyte/yw/controllers/NodeInstanceController.java
@@ -177,7 +177,7 @@ public class NodeInstanceController extends AuthenticatedController {
         node.getNodeName());
 
     auditService().createAuditEntry(ctx(), request());
-    return Results.status(OK);
+    return new YBPTask(taskUUID).asResult();
   }
 
   @ApiOperation(value = "Delete a node instance", response = YBPSuccess.class)
