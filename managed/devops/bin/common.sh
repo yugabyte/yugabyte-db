0462b4f27f798cabca1c104bbd7c7f2df15bbbc9
Jayanth Gunda <jgunda@yugabyte.com>
2022-09-09 15:27:15 -0700

    [PLAT-4724] Implement distro-independent ybcloud.py packaging in Jenkins release workflow
    Summary:
    As part of the distribution independent Python packaging for Ybansytaller, we want to automate the generation of the final repaired PEX file at release time
    so that the repaired PEX (testDockerFinalExtracted) does not have to be checked into the codebase. This diff modifies yb_release in the managed/devops folder
    so that the PEX generation/repair process is executed prior to performing the regular steps in yb_release to generate the release tarball. We execute docker
    in the script directly (which should be already installed on the Jenkins build images) to build and repair the PEX, place it in the automated_pex_generation
    folder, and then add the entire folder (including the bash scripts, wheel directory, and test Python scripts) to the devops release manifest. This will allow
    the PEX to be generated at release time and used by customers installing Yugabundle.
    
    Test Plan:
    Ran ./ybrelease --destination ~/code/devopsRelease to verify that the release tarball for the devops package is created properly, and contains the
    automated_pex_generation folder (and specifically the dynamically generated testDockerFinalExtracted PEX file within in).
    
    Created a Jenkins dev-build job and dev-itest-pipeline build job to verify that the release tarball is being generated correctly, and that there are no
    regressions with using the PEX.
    
    dev-build job link: https://jenkins.dev.yugabyte.com/job/dev-build-job/1066/
    dev-itest-pipeline build job link: https://jenkins.dev.yugabyte.com/job/dev-itest-pipeline/1011/
    
    Reviewers: sanketh, nbhatia, muthu
    
    Reviewed By: muthu
    
    Subscribers: jenkins-bot, yugaware
    
    Differential Revision: https://phabricator.dev.yugabyte.com/D18672

--- managed/devops/bin/common.sh
+++ managed/devops/bin/common.sh
@@ -57,6 +57,7 @@ set_python_executable() {
   for py_executable in "${executables[@]}"; do
     if which "$py_executable" > /dev/null 2>&1; then
       PYTHON_EXECUTABLE="$py_executable"
+      export PYTHON_EXECUTABLE
       return
     fi
   done
@@ -65,10 +66,12 @@ set_python_executable() {
     if python -c 'import sys; sys.exit(1) if sys.version_info[0] != 2 else sys.exit(0)';  then
       if [[ "$YB_MANAGED_DEVOPS_USE_PYTHON3" == "0" ]]; then
         PYTHON_EXECUTABLE="python"
+        export PYTHON_EXECUTABLE
         return
       fi
     elif [[ "$YB_MANAGED_DEVOPS_USE_PYTHON3" == "1" ]]; then
       PYTHON_EXECUTABLE="python"
+      export PYTHON_EXECUTABLE
       return
     fi
   fi
@@ -81,7 +84,13 @@ set_python_executable() {
 # Constants
 # -------------------------------------------------------------------------------------------------
 readonly PYTHON2_EXECUTABLES=('python2' 'python2.7')
-readonly PYTHON3_EXECUTABLES=('python3.6' 'python3' 'python3.7' 'python3.8')
+readonly PYTHON3_EXECUTABLES=('python3.6' 'python3' 'python3.7' 'python3.8', 'python3.9')
+readonly PYTHON3_VERSIONS=('python3.6' 'python3.7' 'python3.8' 'python3.9')
+readonly LINUX_PLATFORMS=('manylinux2014_x86_64-cp-36-cp36m' 'manylinux2014_x86_64-cp-37-cp37m' \
+                         'manylinux2014_x86_64-cp-38-cp38' 'manylinux2014_x86_64-cp-39-cp39')
+readonly MACOS_PLATFORMS=('macosx-10.10-x86_64-cp-36-cp36m' 'macosx-10.10-x86_64-cp-37-cp37m', \
+                         'macosx-10.10-x86_64-cp-38-cp38' 'macosx-10.10-x86_64-cp-39-cp39')
+DOCKER_IMAGE_NAME="quay.io/pypa/manylinux2014_x86_64"
 PYTHON_EXECUTABLE=""
 
 readonly YB_MANAGED_DEVOPS_USE_PYTHON3=${YB_MANAGED_DEVOPS_USE_PYTHON3:-1}
@@ -576,6 +585,27 @@ detect_os() {
   fi
 }
 
+# Function that is needed to activate the PEX environment. Note that using the PEX requires that
+# the PEX exists at the provided PEX_PATH location (which should happen automatically as part of
+# the Devops release generation process). Exports PEX_EXTRA_SYS_PATH when the PEX is used during the
+# execution of ybcloud.sh or pywrapper (needed to import node_client_utils.py when running
+# run_node_action.py). Also export PEX_PATH, SITE_PACKAGES, and SCRIPT_PATH, so that they
+# can be picked up by ybcloud.sh and py_wrapper to run Python scripts with the PEX env.
+activate_pex() {
+  PEX_EXTRA_SYS_PATH="$yb_devops_home/bin"
+  export PEX_EXTRA_SYS_PATH
+  ans_whl="ansible-2.9.19-py3-none-any.whl"
+  PEX_ANSIBLE_PLAYBOOK_PATH="$yb_devops_home/pex/pexEnv/.deps/$ans_whl/.prefix/bin/"
+  export PATH="$PATH:$PEX_ANSIBLE_PLAYBOOK_PATH"
+  PEX_PATH="$yb_devops_home/pex/pexEnv"
+  export PEX_PATH
+  SCRIPT_PATH="$yb_devops_home/opscli/ybops/scripts/ybcloud.py"
+  export SCRIPT_PATH
+  mitogen_wheel_path="pexEnv/.deps/mitogen-0.2.9-py3-none-any.whl"
+  SITE_PACKAGES="$yb_devops_home/pex/$mitogen_wheel_path"
+  export SITE_PACKAGES
+}
+
 # -------------------------------------------------------------------------------------------------
 # Initialization
 # -------------------------------------------------------------------------------------------------
@@ -594,3 +624,12 @@ export ANSIBLE_HOST_KEY_CHECKING=False
 log_dir=$HOME/logs
 
 readonly virtualenv_dir=$yb_devops_home/$YB_VIRTUALENV_BASENAME
+
+# Use the PEX environment when we are not executing on macOS, and if the environment variable
+# YB_USE_VIRTUAL_ENV is unset. Also, the pexEnv folder should exist before activating the
+# PEX virtual environment.
+if [ "$is_mac" == false ] && [[ -z ${YB_USE_VIRTUAL_ENV:-} ]] && [ -d "$yb_devops_home/pex/pexEnv" ]; then
+
+  activate_pex
+
+fi
--- managed/devops/bin/ybcloud.sh
+++ managed/devops/bin/ybcloud.sh
@@ -9,9 +9,18 @@
 # https://github.com/YugaByte/yugabyte-db/blob/master/licenses/POLYFORM-FREE-TRIAL-LICENSE-1.0.0.txt
 
 set -e
+
 . "${BASH_SOURCE%/*}"/common.sh
 
-activate_virtualenv
-cd "$yb_devops_home"
+detect_os
 
-"$PYTHON_EXECUTABLE" "$(which ybcloud.py)" "$@"
+# The virtual environment is used if the operating system is macOS, or if the
+# environment variable YB_USE_VIRTUAL_ENV is set (default unset).
+if [ "$is_mac" == true ] || [[ -n ${YB_USE_VIRTUAL_ENV:-} ]]; then
+    activate_virtualenv
+    cd "$yb_devops_home"
+    "$PYTHON_EXECUTABLE" "$(which ybcloud.py)" "$@"
+# The PEX environment is used in all other cases.
+else
+    $PYTHON_EXECUTABLE $PEX_PATH $SCRIPT_PATH "$@"
+fi
