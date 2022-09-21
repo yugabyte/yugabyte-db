4cc450c5d64c71bd2ca3589ee6d1d9762075544d
Artem Mindrov <amindrov@yugabyte.com>
2022-09-21 19:49:33 +0000

    [Platform][CLOUDGA-8654][PLAT-1866] Optimize GET /instance_types
    Summary:
    This resolves the two most obvious issues with the current implementation:
    1. N+1 queries.
    2. Every entry in the response payload has a provider association with all the region info. Cloud platforms in GCP have up to 160+ instance types with 113 regions in a provider, which results in a huge response payload.
    
    (1) is caused by
     - additional explicit Instance.get for GCP providers
     - text column definition for instanceTypeDetails makes ebean fetch the entity again:
    ```
        at com.yugabyte.yw.models.InstanceType._ebean_get_instanceTypeDetails(InstanceType.java:6)
        at com.yugabyte.yw.models.InstanceType._ebean_set_instanceTypeDetails(InstanceType.java:1)
        at com.yugabyte.yw.models.InstanceType.setInstanceTypeDetails(InstanceType.java)
        at com.yugabyte.yw.models.InstanceType.populateDetails(InstanceType.java:168)
        at com.yugabyte.yw.models.InstanceType.get(InstanceType.java:155)
        at com.yugabyte.yw.models.InstanceType.lambda$findByProvider$2(InstanceType.java:346)
    ```
    This doesn't happen if the property is annotated with DbJson and the underlying column is migrated to json type.
    
    Provider association was not removed completely since this breaks the upsert logic (ebean requires the association to be bi-directional to be able to save InstanceTypes, otherwise updates should be done through the Provider model; fetching all the provider's instance types is not desirable).
    
    Test Plan:
    Example response payload after the change (redundant idKey was removed):
    ```
    [
       {
          "active":true,
          "numCores":2.0,
          "memSizeGB":7.5,
          "instanceTypeDetails":{
             "volumeDetailsList":[
                {
                   "volumeSizeGB":40,
                   "mountPath":"/storage"
                }
             ]
          },
          "instanceTypeCode":"n1-standard-2",
          "providerCode":"onprem",
          "providerUuid":"840cdd27-5d98-46f8-9672-2cbcc9ee29c0"
       }
    ]
    ```
    
    1. Create a GCP/AWS universe, check the selected instance type matches the actual VM.
    2. Edit the universe, change its instance type, verify the change is reflected.
    3. Create an on-prem provider, provision a universe using it.
    
    Reviewers: sanketh, sb-yb, nsingh, amalyshev
    
    Reviewed By: amalyshev
    
    Subscribers: jenkins-bot, yugaware
    
    Differential Revision: https://phabricator.dev.yugabyte.com/D19533

---
+++ managed/src/main/java/com/yugabyte/yw/forms/InstanceTypeResp.java
@@ -0,0 +1,29 @@
+package com.yugabyte.yw.forms;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonUnwrapped;
+import com.yugabyte.yw.models.InstanceType;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+import lombok.experimental.Accessors;
+
+import java.util.UUID;
+
+import static io.swagger.annotations.ApiModelProperty.AccessMode.READ_ONLY;
+
+@Data
+@Accessors(chain = true)
+@ApiModel(description = "Details of a cloud instance type")
+public class InstanceTypeResp {
+  @JsonUnwrapped
+  @JsonIgnoreProperties({"idKey", "provider"})
+  @ApiModelProperty(value = "Instance type", accessMode = READ_ONLY)
+  private InstanceType instanceType;
+
+  @ApiModelProperty(value = "Cloud provider code", accessMode = READ_ONLY)
+  private String providerCode;
+
+  @ApiModelProperty(value = "Provider UUID", accessMode = READ_ONLY)
+  private UUID providerUuid;
+}
