0b06a814bd0117c8bcd3d6c8d022d0c8c23ddd1e
Sami Ahmed Siddiqui <sami.siddiqui@yasglobal.com>
2022-09-23 02:34:58 +0500

    [Docs] Fix Deeplinking issue (#14107)

--- docs/src/index.js
+++ docs/src/index.js
@@ -243,10 +243,13 @@ $(document).ready(() => {
   });
 
   // Right sidebar click move content to inpage link.
-  $(document).on('click', '.td-toc #TableOfContents a,.td-content h2 a,.td-content h3 a,.td-content h4 a', (event) => {
+  $(document).on('click', '.td-toc #TableOfContents a,.td-content a', (event) => {
     const linkHref = $(event.currentTarget).attr('href');
-    window.location.hash = linkHref;
+    if (!linkHref.startsWith('#')) {
+      return;
+    }
 
+    window.location.hash = linkHref;
     if ($(window).width() > 767) {
       $('html, body').scrollTop(($(linkHref).offset().top) - 70);
     } else {
@@ -257,8 +260,8 @@ $(document).ready(() => {
   });
 
   // Hash link move page to his inpage link.
-  if (window.location.hash) {
-    $(`.td-toc #TableOfContents a[href="${window.location.hash}"]`).click();
+  if (window.location.hash && $(`.td-content ${window.location.hash} a`)) {
+    $(`.td-content ${window.location.hash} a`).click();
   }
 
   // Expand / collapse left navigation on click.
