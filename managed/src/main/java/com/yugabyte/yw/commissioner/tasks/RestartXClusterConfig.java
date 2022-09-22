b9af31da7432de76ac5c015e70cbc695e3afdda8
Hamidreza Zare <hzare@yugabyte.com>
2022-09-22 11:51:31 -0400

    [PLAT-3895][xCluster] Add bootstrap for adding a table using AlterUniverseReplication
    Summary:
    Currently, bootstrapping through YBA can only be done during xCluster config creation. This diff adds the ability to do bootstrapping while adding tables by modifying an existing xCluster config.
    
    To trigger adding/removing tables from an xCluster config, the user can make the following request (Please make sure to fill the variables with the correct values):
    ```
    curl --location --request PUT '<Platform-address>/api/v1/customers/<customer-uuid>/xcluster_configs/<xCluster-config-uuid>' \
    --header 'Content-Type: application/json' \
    --header 'X-AUTH-YW-API-TOKEN: <auth-token>' \
    --data-raw '{
       "tables":[<table-id1>, <table-id2>],
       "bootstrapParams": {
          "tables":[<table-id1>],
          "backupRequestParams": {
            "storageConfigUUID": <storage-config-uuid>
          }
        }
    }'
    ```
    `tables` in the main body is the final table list that the user intends the xCluster config to have. `tables` in `bootstrapParams` object is the tables that user intends to do bootstarpping for if required while adding them (the user can skip bootstrapping by not specifying any table. This is for compatibility reasons with the current UI).
    
    E.g.:
    ```
    curl --location --request PUT 'http://192.168.56.102:9000/api/v1/customers/f33e3c9b-75ab-4c30-80ad-cba85646ea39/xcluster_configs/e55b1faa-77f1-428a-bbe4-d82837429417' \
    --header 'Content-Type: application/json' \
    --header 'X-AUTH-YW-API-TOKEN: 2894987a-4fdf-47cf-95a2-be3f2039cf79' \
    --data-raw '{
       "tables":["ffbb0d2fdc6048f7bc3d5933a096decc", "fedca4472e8b4a1cbe086606d6d32f11"],
       "bootstrapParams": {
          "tables":["ffbb0d2fdc6048f7bc3d5933a096decc", "fedca4472e8b4a1cbe086606d6d32f11"],
          "backupRequestParams": {
            "storageConfigUUID": "cb5e0444-3f36-486d-aa07-20e10c37b382"
          }
        }
    }'
    ```
    
    Test Plan:
      - Created xCluster configs with/without bootstrapping for both YSQL and YCQL tables.
      - Added a subset of tables to an xCluster config for  YCQL tables and observed it skips bootstrapping for tables that do not need it and set up replication right away for them. Then, it does bootstrapping for other tables and set up replication for them DB by DB.
      - Added all the tables in one YSQL DB to an xCluster config for YSQL tables, and observed the right behavior. When at least one table requires bootstrapping, it will be done for all the tables in that DB, otherwise, it skips to set up replication for them right away.
    
    Reviewers: sanketh, jmak, nbhatia, sb-yb, amalyshev
    
    Reviewed By: amalyshev
    
    Subscribers: jenkins-bot, sanketh, yugaware
    
    Differential Revision: https://phabricator.dev.yugabyte.com/D19617

--- managed/src/main/java/com/yugabyte/yw/commissioner/tasks/RestartXClusterConfig.java
+++ managed/src/main/java/com/yugabyte/yw/commissioner/tasks/RestartXClusterConfig.java
@@ -7,6 +7,7 @@ import com.yugabyte.yw.models.Universe;
 import com.yugabyte.yw.models.XClusterConfig;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 import javax.inject.Inject;
 import lombok.extern.slf4j.Slf4j;
 import org.yb.master.MasterDdlOuterClass;
@@ -33,26 +34,38 @@ public class RestartXClusterConfig extends CreateXClusterConfig {
         // Lock the target universe.
         lockUniverseForUpdate(targetUniverse.universeUUID, targetUniverse.version);
 
+        createXClusterConfigSetStatusTask(XClusterConfig.XClusterConfigStatusType.Updating)
+            .setSubTaskGroupType(UserTaskDetails.SubTaskGroupType.DeleteXClusterReplication);
+
         // Set needBootstrap to true for all tables. It will check if it is required.
         xClusterConfig.setNeedBootstrapForTables(
             xClusterConfig.getTables(), true /* needBootstrap */);
 
+        Set<String> tableIds = xClusterConfig.getTables();
         Map<String, List<String>> mainTableIndexTablesMap =
-            getMainTableIndexTablesMap(sourceUniverse, xClusterConfig.getTables());
-        Map<String, List<MasterDdlOuterClass.ListTablesResponsePB.TableInfo>>
-            requestedNamespaceTablesInfoMap = checkTables(mainTableIndexTablesMap);
+            getMainTableIndexTablesMap(sourceUniverse, tableIds);
+        addIndexTables(tableIds, mainTableIndexTablesMap);
 
-        createXClusterConfigSetStatusTask(XClusterConfig.XClusterConfigStatusType.Updating)
-            .setSubTaskGroupType(UserTaskDetails.SubTaskGroupType.DeleteXClusterReplication);
+        List<MasterDdlOuterClass.ListTablesResponsePB.TableInfo> requestedTableInfoList =
+            getRequestedTableInfoList(tableIds, sourceUniverse, targetUniverse);
+        xClusterConfig.setTableType(requestedTableInfoList);
 
         // Delete the xCluster config.
         createDeleteXClusterConfigSubtasks(xClusterConfig, true /* keepEntry */);
 
+        createXClusterConfigSetStatusTask(XClusterConfig.XClusterConfigStatusType.Updating);
+
         addSubtasksToCreateXClusterConfig(
-            sourceUniverse,
-            targetUniverse,
-            requestedNamespaceTablesInfoMap,
-            mainTableIndexTablesMap);
+            sourceUniverse, targetUniverse, requestedTableInfoList, mainTableIndexTablesMap);
+
+        createXClusterConfigSetStatusTask(XClusterConfig.XClusterConfigStatusType.Running)
+            .setSubTaskGroupType(UserTaskDetails.SubTaskGroupType.ConfigureUniverse);
+
+        createMarkUniverseUpdateSuccessTasks(targetUniverse.universeUUID)
+            .setSubTaskGroupType(UserTaskDetails.SubTaskGroupType.ConfigureUniverse);
+
+        createMarkUniverseUpdateSuccessTasks(sourceUniverse.universeUUID)
+            .setSubTaskGroupType(UserTaskDetails.SubTaskGroupType.ConfigureUniverse);
 
         getRunnableTask().runSubTasks();
       } finally {
