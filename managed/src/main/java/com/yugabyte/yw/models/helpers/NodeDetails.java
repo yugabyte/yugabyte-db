f060e755b818f0cdaa4da01ba4e24e3b5df654a1
Bhavin Gandhi <bhavin7392@gmail.com>
2022-09-27 16:56:11 +0530

    [PLAT-5050][PLAT-5051] Support custom pod addresses in Kubernetes
    Summary:
    This introduces new provider configuration key
    KUBE_POD_ADDRESS_TEMPLATE. This will enable us to support
    multi-cluster uses cases like Istio, GKE MCS, etc. Where the
    address/DNS to reach individual pod differs based on the
    implementation.
    
    - Modifies the run_node_action.py, yb_backup.py etc to use a pod
      address to cofiguration map object.
    - getDomainPerAZ now returns the cluster domain without svc in
      it. This makes cluster_domain consistent with what users
      provide. i.e. only the last part like cluster.local.
    - The namespace, and pod name is now stored in CloudSpecificInfo
      object, and is reused at other places.
    
    Test Plan:
    Universe with normal single Kubernetes cluster provider:
    - Node porxy, logs, healthchecks work as expected.
    - ysqlsh, and ycqlsh work.
    - Edit universe: add node, remove node works.
    - Gflags update works as expected.
    
    Universe with Istio enabled Kubernetes multi-cluster provider:
    - Above mentioned things work except:
    - TLS node to node, client to node.
    - CQL interface.
    - Backups and restore.
    These are tracked separately and will be fixed in upcoming changes.
    
    Reviewers: gjalla, sneelakantan, sanketh
    
    Reviewed By: sanketh
    
    Subscribers: jenkins-bot, yugaware
    
    Differential Revision: https://phabricator.dev.yugabyte.com/D19530

--- managed/src/main/java/com/yugabyte/yw/models/helpers/NodeDetails.java
+++ managed/src/main/java/com/yugabyte/yw/models/helpers/NodeDetails.java
@@ -23,6 +23,8 @@ import java.util.Set;
 import java.util.UUID;
 import java.util.stream.Collectors;
 import org.apache.commons.lang3.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /** Represents all the details of a cloud node that are of interest. */
 @JsonIgnoreProperties(
@@ -31,6 +33,8 @@ import org.apache.commons.lang3.StringUtils;
     ignoreUnknown = true)
 @ApiModel(description = "Details of a cloud node")
 public class NodeDetails {
+  public static final Logger LOG = LoggerFactory.getLogger(NodeDetails.class);
+
   // The id of the node. This is usually present in the node name.
   @ApiModelProperty(value = "Node ID")
   public int nodeIdx = -1;
@@ -383,5 +387,40 @@ public class NodeDetails {
 
   public UUID getNodeUuid() {
     return nodeUuid;
+  }
+
+  // The variables KubernetesPodName and KubernetesNamespace will
+  // eventually be set to correct values, till then we depend on the
+  // private_ip, which is the pod FQDN in single Kubernetes cluster
+  // setups. For multi Kubernetes cluster environments these values
+  // will always be set.
+  @JsonIgnore
+  public String getK8sPodName() {
+    String pod = this.cloudInfo.kubernetesPodName;
+    if (StringUtils.isBlank(pod)) {
+      LOG.warn(
+          "The pod name is blank for {}, inferring it from the first part of node private_ip",
+          this.nodeName);
+      if (StringUtils.isBlank(this.cloudInfo.private_ip)) {
+        throw new RuntimeException(this.nodeName + " has a blank private_ip (FQDN)");
+      }
+      pod = this.cloudInfo.private_ip.split("\\.")[0];
+    }
+    return pod;
+  }
+
+  @JsonIgnore
+  public String getK8sNamespace() {
+    String namespace = this.cloudInfo.kubernetesNamespace;
+    if (StringUtils.isBlank(namespace)) {
+      LOG.warn(
+          "The namesapce is blank for {}, inferring it from the third part of the node private_ip",
+          this.nodeName);
+      if (StringUtils.isBlank(this.cloudInfo.private_ip)) {
+        throw new RuntimeException(this.nodeName + " has a blank private_ip (FQDN)");
+      }
+      namespace = this.cloudInfo.private_ip.split("\\.")[2];
+    }
+    return namespace;
   }
 }
