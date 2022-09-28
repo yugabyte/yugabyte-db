344f55be73d032ea714fe2815f47d1c7ecce73a5
Hamidreza Zare <hzare@yugabyte.com>
2022-09-28 18:00:02 -0400

    [PLAT-5619][xCluster] Do not use sudo user for transferring source root cert
    Summary:
    Currently, the `Transfer_XCluster_Certs` node command uses the sudo user to copy the source universe root certificate to the target universe nodes. However, the certificate must be created under the yugabyte user home directory. This diff fixes this issue.
    
    Test Plan: Created a provider with a specified `ssh_user` and verified the problem exists and it is resolved with this diff.
    
    Reviewers: sanketh, amalyshev, muthu
    
    Reviewed By: muthu
    
    Subscribers: yugaware
    
    Differential Revision: https://phabricator.dev.yugabyte.com/D19870

--- managed/src/main/java/com/yugabyte/yw/common/NodeManager.java
+++ managed/src/main/java/com/yugabyte/yw/common/NodeManager.java
@@ -272,8 +272,7 @@ public class NodeManager extends DevopsBase {
             || type == NodeCommandType.Destroy
             || type == NodeCommandType.Create
             || type == NodeCommandType.Disk_Update
-            || type == NodeCommandType.Update_Mounted_Disks
-            || type == NodeCommandType.Transfer_XCluster_Certs)
+            || type == NodeCommandType.Update_Mounted_Disks)
         && keyInfo.sshUser != null) {
       subCommand.add("--ssh_user");
       subCommand.add(keyInfo.sshUser);
