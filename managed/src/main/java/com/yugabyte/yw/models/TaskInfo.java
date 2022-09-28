713be0007c31c7eb1de9d05182b1b2bd8edb95a2
Naorem Khogendro Singh <nsingh@yugabyte.com>
2022-09-27 18:23:38 -0700

    [PLAT-5603] Add a way to cache task runtime data while the task is running
    Summary: We are going make changes to report transient, runtime progress data of task like data move % completion, number of tablets moved etc. This enables caching the data to the task while it is running.
    
    Test Plan: Unit test + trivial change
    
    Reviewers: amalyshev, shagarwal
    
    Reviewed By: shagarwal
    
    Subscribers: jenkins-bot, yugaware
    
    Differential Revision: https://phabricator.dev.yugabyte.com/D19867

--- managed/src/main/java/com/yugabyte/yw/models/TaskInfo.java
+++ managed/src/main/java/com/yugabyte/yw/models/TaskInfo.java
@@ -35,7 +35,6 @@ import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import java.util.UUID;
-import java.util.stream.Collectors;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.EnumType;
