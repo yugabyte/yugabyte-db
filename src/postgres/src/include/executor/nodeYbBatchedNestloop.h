5cfbb2c77cd55e346b1a9d59e3ba2895aff39d1c
Tanuj Nayak <tanujnay112@live.com>
2022-09-30 13:53:02 -0700

    [#14278] YSQL: Make sure copy function for YbBatchedNestLoop does a deep copy for its fields
    Summary:
    `_copyYbBatchedNestLoop` in `copyfuncs.c` was doing a shallow copy of its `nl` field. This results in the field having invalid fields when a copy of a `YbBatchedNestLoop` plan is reused over multiple connections. This revision fixes that issue by adding the appropriate copy logic in `_copyYbBatchedNestLoop`.
    
    This revision also fixes a couple glaring cosmetic issues in code relevant to batched nested loop joins.
    
    Test Plan:
    ./yb_build.sh release --java-test 'org.yb.pgsql.TestPgMisc'
    
    Reviewers: rskannan, amartsinchyk
    
    Reviewed By: amartsinchyk
    
    Differential Revision: https://phabricator.dev.yugabyte.com/D19919

--- src/postgres/src/include/executor/nodeYbBatchedNestloop.h
+++ src/postgres/src/include/executor/nodeYbBatchedNestloop.h
@@ -15,7 +15,7 @@
  * or implied.  See the License for the specific language governing permissions and limitations
  * under the License.
  *
- * src/postgres/src/include/executor/nodeYbBatchedNestLoop.h
+ * src/postgres/src/include/executor/nodeYbBatchedNestloop.h
  *
  *-------------------------------------------------------------------------
  */
