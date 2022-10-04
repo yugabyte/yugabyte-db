95db522c54716558243647da48b2f3ad3f7698ba
Ted Yu <yuzhihong@gmail.com>
2022-10-04 11:10:29 -0700

    [#14204] ycql: Create "null" and empty Jsonb object only once
    Summary:
    Currently in `Executor::ColumnArgsToPB`, we create "null" Jsonb object upon every invocation of the method.
    This is unnecessary.
    
    This revision creates "null" Jsonb object only once and references the static string in `Executor::ColumnArgsToPB`.
    
    Same technique is applied to the empty jsonb object used in `QLWriteOperation::ApplyForJsonOperators`
    
    Test Plan: Existing test suite
    
    Reviewers: neil, pjain, oleg
    
    Reviewed By: oleg
    
    Subscribers: yql
    
    Differential Revision: http://phabricator.dev.yugabyte.com/D19809

--- src/yb/common/jsonb.cc
+++ src/yb/common/jsonb.cc
@@ -28,6 +28,16 @@
 namespace yb {
 namespace common {
 
+string prepareSerializedJsonb(const string& body) {
+     common::Jsonb jsonb;
+     auto s = jsonb.FromString(body);
+     LOG_IF(DFATAL, !s.ok()) << "Unable to parse " << body;
+     return s.ok() ? jsonb.SerializedJsonb() : "";
+}
+
+string Jsonb::kSerializedJsonbNull = prepareSerializedJsonb("null");
+string Jsonb::kSerializedJsonbEmpty = prepareSerializedJsonb("{}");
+
 bool Jsonb::IsScalar(const JEntry& jentry) {
   uint32_t jentry_type = GetJEType(jentry);
   return ((jentry_type) != kJEIsArray && (jentry_type) != kJEIsObject);
