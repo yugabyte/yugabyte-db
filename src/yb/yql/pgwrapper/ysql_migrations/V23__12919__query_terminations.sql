945cfd206877d86e5987a7c20628469a806ea0ae
Jason Kim <git@jasonk.me>
2022-10-03 17:10:27 -0700

    YSQL: fix V23 migration oid
    Summary:
    There are two issues with D18220:
    
    - It skips oid 8041, using 8042 (probably misinterpreted
      "YB_MIN_UNUSED_OID" as max used oid).
    - It doesn't update YB_MIN_UNUSED_OID.
    
    Fix both issues.  In the future, a test should ensure these kinds of
    things, if such a test is easily possible.
    
    Note this requires reinitdb if this migration was already applied or
    initdb was done over this faulty 8042 oid.  The hope is that this change
    lands before any releases are built off this faulty catalog change.
    
    Test Plan: ./yb_build.sh $buildargs --java-test TestYsqlUpgrade
    
    Reviewers: rskannan, telgersma
    
    Reviewed By: telgersma
    
    Subscribers: yql
    
    Differential Revision: https://phabricator.dev.yugabyte.com/D19951

--- src/yb/yql/pgwrapper/ysql_migrations/V23__12919__query_terminations.sql
+++ src/yb/yql/pgwrapper/ysql_migrations/V23__12919__query_terminations.sql
@@ -6,7 +6,7 @@ INSERT INTO pg_catalog.pg_proc (
   pronargdefaults, prorettype, proargtypes, proallargtypes, proargmodes, proargnames,
   proargdefaults, protrftypes, prosrc, probin, proconfig, proacl
 ) VALUES
-  (8042, 'yb_pg_stat_get_queries', 11, 10, 12, 1, 100, 0, '-', 'f', false, false, false, true,
+  (8041, 'yb_pg_stat_get_queries', 11, 10, 12, 1, 100, 0, '-', 'f', false, false, false, true,
    's', 'r', 1, 0, 2249, '26', '{26,26,23,25,25,1184,1184}', '{i,o,o,o,o,o,o}',
    '{db_oid,db_oid,backend_pid,query_text,termination_reason,query_start,query_end}',
    NULL, NULL, 'yb_pg_stat_get_queries', NULL, NULL, NULL)
@@ -18,11 +18,11 @@ DO $$
 BEGIN
   IF NOT EXISTS (
     SELECT FROM pg_catalog.pg_depend
-      WHERE refclassid = 1255 AND refobjid = 8042
+      WHERE refclassid = 1255 AND refobjid = 8041
   ) THEN
     INSERT INTO pg_catalog.pg_depend (
       classid, objid, objsubid, refclassid, refobjid, refobjsubid, deptype
     ) VALUES
-      (0, 0, 0, 1255, 8042, 0, 'p');
+      (0, 0, 0, 1255, 8041, 0, 'p');
   END IF;
 END $$;
