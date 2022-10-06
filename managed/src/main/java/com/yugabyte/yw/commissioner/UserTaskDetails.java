c462dddc5d81e619d1e64fb1a45bb2bd5ee38ee9
Shristy Agarwal <shagarwal@yugabyte.com>
2022-10-06 05:57:00 -0700

    [PLAT-4006] Get progress in terms of tablet numbers during data migration.
    Summary:
    During, cluster edit when tablet movements take long time Cloud wants us
    to give granularity in terms of the number of tablets moved/remaining and
    not just a task percent.
    
    This change keeps this data in the Task Cache and does not store the
    data movement in the DB. We do not need this data once the task has
    finished.
    This change needs a previous diff which actually introduced this concept
    of TaskCache https://phabricator.dev.yugabyte.com/D19867
    
    Also, remove a condition check because we never add 'error' field in
    task progress, a comment in Customer Task Controller.
    
    Test Plan:
    Manual- Edit a cluster to have some data movement and see the task response getting updated.
    Sample API Call
        http://localhost:9000/api/v1/customers/<c_uuid>/tasks/<t_uuid>
    
    Sample Response
    ```
        {
            ...
            "details": {
                "taskDetails": [
                    {
                    },
                    {
                        "title": "Waiting for data migration",
                        "description": "Waiting for the data to get copied into the new set of machines to achieve the desired configuration.",
                        "state": "Unknown",
                        "extraDetails": [
                            {
                                "tabletMoveElapsedMilliSecs": 142,
                                "numTotalTablets": 1,
                                "numRemainingTablets": 0,
                                "numMovedTablets": 1,
                                "tabletMovePercentCompleted": 100
                            }
                        ]
                    }, ...
                ]
            },
            ...
        }
    ```
    
    Reviewers: nsingh
    
    Reviewed By: nsingh
    
    Subscribers: jenkins-bot, yugaware
    
    Differential Revision: https://phabricator.dev.yugabyte.com/D20032

--- managed/src/main/java/com/yugabyte/yw/commissioner/UserTaskDetails.java
+++ managed/src/main/java/com/yugabyte/yw/commissioner/UserTaskDetails.java
@@ -1,5 +1,6 @@
 package com.yugabyte.yw.commissioner;
 
+import com.fasterxml.jackson.databind.JsonNode;
 import com.yugabyte.yw.models.TaskInfo;
 import java.util.ArrayList;
 import java.util.List;
@@ -500,10 +501,14 @@ public class UserTaskDetails {
     // The state of the task.
     private TaskInfo.State state;
 
+    // Extra task details about a subtask like progress in tablet movement.
+    public List<JsonNode> extraDetails;
+
     private SubTaskDetails(String title, String description) {
       this.title = title;
       this.description = description;
       this.state = TaskInfo.State.Unknown;
+      this.extraDetails = new ArrayList<>();
     }
 
     public void setState(TaskInfo.State state) {
@@ -520,6 +525,12 @@ public class UserTaskDetails {
 
     public TaskInfo.State getState() {
       return state;
+    }
+
+    public void populateDetails(JsonNode data) {
+      if (data != null) {
+        this.extraDetails.add(data);
+      }
     }
   }
 }
