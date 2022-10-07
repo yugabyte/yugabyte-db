yugabyte/yugabyte-db

commit 1d9809a7462da861e27348d8d62736784484a413

[PLAT-4246] Create gRPC server on node agent to allow addition of future endpoints
Summary:
This has a number of changes:
1. Add gRPC service and make the java client available to platform via lib folder (known to sbt).
2. IP and port validation of node-agent. (TODO for platform changes)
3. Code clean-up including duplicate removal using go reflect.
4. Added a parent pom file to which modules can be added for future modules like node-agent.

Test Plan:
Unit tests + manual test on a universe node.
[yugabyte@yb-admin-nsingh-test-universe1-n1 ~]$ ./installer.sh -t install -u http://100.98.0.42:9000 -at eea5697a-73b5-473b-8cab-27657780ce23
* Starting YB Node Agent install
* Creating Node Agent Directory
* Changing directory to node agent
* Creating Sub Directories
* Downloading YB Node Agent build package
* Getting Linux/amd64 package
* Downloaded Version - 2.15.3.0
* Extracting the build package
* The current value of Node IP is not set; Enter new value or enter to skip: 10.25.1.2
* The current value of Node Name is not set; Enter new value or enter to skip: Test
Key: Content-Type, Value: application/json
Key: X-AUTH-YW-API-TOKEN, Value: eea5697a-73b5-473b-8cab-27657780ce23
* Select your Customer
1. Customer ID: f33e3c9b-75ab-4c30-80ad-cba85646ea39, Customer Name: admin
	 Enter the option number: 1
Key: Content-Type, Value: application/json
Key: X-AUTH-YW-API-TOKEN, Value: eea5697a-73b5-473b-8cab-27657780ce23
* Select your User
1. User ID: a9190fe6-1067-409f-95cf-dbd96893c9c9, Role: SuperAdmin
	 Enter the option number: 1
Key: Content-Type, Value: application/json
Key: X-AUTH-YW-API-TOKEN, Value: eea5697a-73b5-473b-8cab-27657780ce23
* Select your Onprem Provider
1. Provder ID: 7db39e6e-0525-4a42-ad58-dcdc3fc3a652, Provider Name: onprem
2. Provder ID: dfa8480d-d1ea-4835-9bc7-48cd1f51f412, Provider Name: onprem-test-1
	 Enter the option number: 1
Key: Content-Type, Value: application/json
Key: X-AUTH-YW-API-TOKEN, Value: eea5697a-73b5-473b-8cab-27657780ce23

managed/build.sbt
@@ -90,11 +90,11 @@ lazy val versionGenerate = taskKey[Int]("Add version_metadata.json file")
 
 lazy val buildVenv = taskKey[Int]("Build venv")
 lazy val buildUI = taskKey[Int]("Build UI")
-lazy val buildNodeAgent = taskKey[Int]("Build Node Agent")
+lazy val buildModules = taskKey[Int]("Build modules")
 
 lazy val cleanUI = taskKey[Int]("Clean UI")
 lazy val cleanVenv = taskKey[Int]("Clean venv")
-lazy val cleanNodeAgent = taskKey[Int]("Clean Node Agent")
+lazy val cleanModules = taskKey[Int]("Clean modules")
 
 
 lazy val compileJavaGenClient = taskKey[Int]("Compile generated Java code")
@@ -284,10 +284,9 @@ externalResolvers := {
 }
 
 (Compile / compilePlatform) := {
-  (Compile / compile).value
+  ((Compile / compile) dependsOn buildModules).value
   buildVenv.value
   buildUI.value
-  //buildNodeAgent.value
   versionGenerate.value
 }
 
@@ -295,7 +294,7 @@ cleanPlatform := {
   clean.value
   cleanVenv.value
   cleanUI.value
-  cleanNodeAgent.value
+  cleanModules.value
 }
 
 versionGenerate := {
@@ -321,9 +320,9 @@ buildUI := {
   status
 }
 
-buildNodeAgent := {
-  ybLog("Building node agent...")
-  val status = Process("./build.sh clean build package " + version.value, baseDirectory.value / "node-agent").!
+buildModules := {
+  ybLog("Building modules...")
+  val status = Process("mvn install -f parent.xml").!
   status
 }
 
@@ -339,9 +338,9 @@ cleanUI := {
   status
 }
 
-cleanNodeAgent := {
+cleanModules := {
   ybLog("Cleaning Node Agent...")
-  val status = Process("./build.sh clean", baseDirectory.value / "node-agent").!
+  val status = Process("mvn clean -f parent.xml").!
   status
 }
 

managed/.gitignore
@@ -52,3 +52,4 @@ ui/junit.xml
 src/public/*
 
 client/*/generated
+lib

managed/node-agent/adapters/cli/node/configure.go
@@ -1,256 +1,0 @@
-// Copyright (c) YugaByte, Inc.
-
-package node
-
-import (
-	"context"
-	"errors"
-	"fmt"
-	"net/url"
-	"node-agent/app/task"
-	"node-agent/model"
-	"node-agent/util"
-	"strconv"
-	"strings"
-
-	"github.com/spf13/cobra"
-)
-
-var (
-	configureCmd = &cobra.Command{
-		Use:   "configure",
-		Short: "Configures a node",
-		RunE:  configureNodeHandler,
-	}
-)
-
-func SetupConfigureCommand(parentCmd *cobra.Command) {
-	configureCmd.PersistentFlags().
-		StringP("setup", "s", "interactive", "Setup style for the configuring the node agent.")
-	configureCmd.PersistentFlags().StringP("url", "u", "", "Platform URL")
-	configureCmd.PersistentFlags().String("api_token", "", "API Token for fetching config info.")
-	configureCmd.PersistentFlags().String("version", "", "Node Agent Version")
-	configureCmd.MarkPersistentFlagRequired("api_token")
-	configureCmd.MarkPersistentFlagRequired("url")
-	configureCmd.MarkPersistentFlagRequired("version")
-	parentCmd.AddCommand(configureCmd)
-}
-
-func configureNodeHandler(cmd *cobra.Command, args []string) error {
-	configureType, err := cmd.Flags().GetString("setup")
-	if err != nil {
-		util.CliLogger.Errorf("Error while reading flags - %s", err.Error())
-		return err
-	}
-	if configureType == "interactive" {
-		if interactiveConfigHandler(cmd) != nil {
-			return err
-		}
-	}
-	return nil
-}
-
-//Provides a fully interactive configuration setup for the user to configure
-//the node agent. It uses api token to fetch customers and finds
-//subsequent properties using the customerID.
-func interactiveConfigHandler(cmd *cobra.Command) error {
-	apiToken, err := cmd.Flags().GetString("api_token")
-	if err != nil {
-		return errors.New("Need API Token during interactive config setup")
-	}
-	platformUrl, err := cmd.Flags().GetString("url")
-	if err != nil {
-		return errors.New("Need Platform URL during interactive config setup")
-	}
-	ctx := context.Background()
-	parsedUrl, err := url.Parse(platformUrl)
-	if err != nil {
-		return errors.New("Malformed Platform URL passed.")
-	}
-	version, err := cmd.Flags().GetString("version")
-	if err != nil {
-		return errors.New("Need Node Agent version for registration.")
-	}
-	appConfig.Update(util.PlatformVersion, version)
-	appConfig.Update(
-		util.PlatformHost,
-		fmt.Sprintf("%s://%s", parsedUrl.Scheme, parsedUrl.Hostname()),
-	)
-	appConfig.Update(util.PlatformPort, parsedUrl.Port())
-	task.InitHttpClient(appConfig)
-	//Get Node Agent name and Node Agent IP
-	checkConfigAndUpdate(util.NodeIP, "Node IP")
-	checkConfigAndUpdate(util.NodeName, "Node Name")
-
-	//Get Customers from the platform
-	response, err := taskExecutor.ExecuteTask(ctx, task.HandleGetCustomers(apiToken))
-	if err != nil {
-		util.CliLogger.Errorf("Error fetching the customers - %s", err)
-		return err
-	}
-	customersData, ok := response.(*[]model.Customer)
-	if !ok {
-		panic("Incorrect inference type passed")
-	}
-	ops := make([]model.DisplayInterface, len(*customersData))
-	for i, v := range *customersData {
-		ops[i] = v
-	}
-	customerNum, err := displayOptionsAndGetSelected(ops, "Customer")
-	if err != nil {
-		util.CliLogger.Errorf("Error while displaying customers: %s", err.Error())
-		return err
-	}
-	appConfig.Update(util.CustomerId, (*customersData)[customerNum].CustomerId)
-
-	// Get Users from the platform.
-	response, err = taskExecutor.ExecuteTask(ctx, task.HandleGetUsers(apiToken))
-	if err != nil {
-		util.CliLogger.Errorf("Error fetching the users - %s", err)
-		return err
-	}
-	usersData, ok := response.(*[]model.User)
-	if !ok {
-		panic("Incorrect inference type passed")
-	}
-	//Do not display Read Only User.
-	i := 0
-	for _, data := range *usersData {
-		if !strings.EqualFold(data.Role, "ReadOnly") {
-			(*usersData)[i] = data
-			i++
-		}
-	}
-
-	validUsers := (*usersData)[:i]
-	ops = make([]model.DisplayInterface, len(validUsers))
-	for i, v := range validUsers {
-		ops[i] = v
-	}
-	userNum, err := displayOptionsAndGetSelected(ops, "User")
-	if err != nil {
-		util.CliLogger.Errorf("Error while displaying customers: %s", err.Error())
-		return err
-	}
-	appConfig.Update(util.UserId, (validUsers)[userNum].UserId)
-
-	// Get Providers from the platform (only on-prem providers displayed)
-	response, err = taskExecutor.ExecuteTask(ctx, task.HandleGetProviders(apiToken))
-	if err != nil {
-		util.CliLogger.Errorf("Error fetching the providers - %s", err)
-		return err
-	}
-
-	providersData, ok := response.(*[]model.Provider)
-	if !ok {
-		panic("Incorrect inference type passed")
-	}
-	i = 0
-	for _, data := range *providersData {
-		if data.Code == "onprem" {
-			(*providersData)[i] = data
-			i++
-		}
-	}
-
-	onpremProviders := (*providersData)[:i]
-
-	ops = make([]model.DisplayInterface, len(onpremProviders))
-	for i, v := range onpremProviders {
-		ops[i] = v
-	}
-	providerNum, err := displayOptionsAndGetSelected(ops, "Onprem Provider")
-	if err != nil {
-		util.CliLogger.Errorf("Error while displaying customers: %s", err.Error())
-		return err
-	}
-	appConfig.Update(util.ProviderId, (onpremProviders)[providerNum].Uuid)
-
-	// Get Instance Types for the provider from the platform.
-	response, err = taskExecutor.ExecuteTask(ctx, task.HandleGetInstanceTypes(apiToken))
-	if err != nil {
-		util.CliLogger.Errorf("Error fetching the instance types - %s", err)
-		return err
-	}
-	instancesData, ok := response.(*[]model.NodeInstanceType)
-	if !ok {
-		panic("Incorrect inference type passed")
-	}
-	ops = make([]model.DisplayInterface, len(*instancesData))
-	for i, v := range *instancesData {
-		ops[i] = v
-	}
-	instanceNum, err := displayOptionsAndGetSelected(ops, "Instance Type")
-	if err != nil {
-		util.CliLogger.Errorf("Error while displaying instance Types: %s", err.Error())
-		return err
-	}
-	selectedInstanceType := (*instancesData)[instanceNum]
-	appConfig.Update(util.NodeInstanceType, selectedInstanceType.InstanceTypeCode)
-
-	regions := selectedInstanceType.Provider.Regions
-
-	// Select Region.
-	ops = make([]model.DisplayInterface, len(regions))
-	for i, v := range regions {
-		ops[i] = v
-	}
-	regionNum, err := displayOptionsAndGetSelected(ops, "Region")
-	if err != nil {
-		util.CliLogger.Errorf("Error while displaying regions: %s", err.Error())
-		return err
-	}
-	appConfig.Update(util.NodeRegion, regions[regionNum].Code)
-
-	// Update availability Zone.
-	zones := regions[regionNum].Zones
-	ops = make([]model.DisplayInterface, len(zones))
-	for i, v := range zones {
-		ops[i] = v
-	}
-	zoneNum, err := displayOptionsAndGetSelected(ops, "Zone")
-	if err != nil {
-		util.CliLogger.Errorf("Error while displaying zones: %s", err.Error())
-		return err
-	}
-	appConfig.Update(util.NodeAzId, zones[zoneNum].Uuid)
-	appConfig.Update(util.NodeZone, zones[zoneNum].Code)
-	fmt.Println("* Completed Node Agent Configuration")
-
-	// Update with default values.
-	appConfig.Update(util.RequestTimeout, "20")
-	appConfig.Update(util.NodePingInterval, "20")
-
-	return registerCmdHandler(cmd, []string{})
-}
-
-//Displays the options and prompts the user to select an option followed by validating the option.
-func displayOptionsAndGetSelected(
-	options []model.DisplayInterface,
-	displayHead string,
-) (int, error) {
-	if len(options) == 0 {
-		return -1, fmt.Errorf("No record found for %s", displayHead)
-	}
-	fmt.Printf("* Select your %s\n", displayHead)
-	for i, option := range options {
-		fmt.Printf("%d. %s\n", i+1, option.ToString())
-	}
-	for {
-		fmt.Printf("\t Enter the option number: ")
-		var newVal string
-		fmt.Scanln(&newVal)
-		optionNum, err := strconv.Atoi(newVal)
-		if err != nil {
-			util.CliLogger.Errorf("Expected a number")
-			fmt.Println()
-			continue
-		}
-		if optionNum < 1 || optionNum > len(options) {
-			util.CliLogger.Errorf("Expected an option within the range")
-			fmt.Println()
-			continue
-		}
-		return optionNum - 1, nil
-	}
-}

managed/node-agent/adapters/cli/node/node.go
@@ -1,53 +1,0 @@
-// Copyright (c) YugaByte, Inc.
-
-package node
-
-import (
-	"fmt"
-	"node-agent/app/executor"
-	"node-agent/app/service"
-	"node-agent/util"
-
-	"github.com/spf13/cobra"
-)
-
-var (
-	nodeCmd = &cobra.Command{
-		Use:   "node ...",
-		Short: "Command for node agent",
-	}
-	appConfig    *util.Config
-	taskExecutor *executor.TaskExecutor
-)
-
-func SetupNodeCommand(parentCmd *cobra.Command) {
-	appConfig = util.GetConfig()
-	taskExecutor = executor.GetInstance(service.Context())
-	SetupConfigureCommand(nodeCmd)
-	SetupPreflightCheckCommand(nodeCmd)
-	SetupRegisterCommand(nodeCmd)
-	parentCmd.AddCommand(nodeCmd)
-}
-
-// Accepts a key and desciption and updates the config with user input.
-func checkConfigAndUpdate(key string, desc string) {
-	val := appConfig.GetString(key)
-
-	//Display the current set config
-	if val != "" {
-		fmt.Printf(
-			"* The current value of %s is set to %s; Enter new value or enter to skip: ",
-			desc,
-			val,
-		)
-	} else {
-		fmt.Printf("* The current value of %s is not set; Enter new value or enter to skip: ", desc)
-	}
-
-	var newVal string
-	fmt.Scanln(&newVal)
-
-	if newVal != "" {
-		appConfig.Update(key, newVal)
-	}
-}

managed/node-agent/adapters/cli/node/preflight_check.go
@@ -1,75 +1,0 @@
-// Copyright (c) YugaByte, Inc.
-
-package node
-
-import (
-	"context"
-	"errors"
-	"node-agent/app/task"
-	"node-agent/model"
-	"node-agent/util"
-
-	"github.com/spf13/cobra"
-)
-
-var (
-	preflightCheck = &cobra.Command{
-		Use:   "preflight-check",
-		Short: "Check Preflight steps in the node",
-		RunE:  preFlightCheckHandler,
-	}
-)
-
-func SetupPreflightCheckCommand(parentCmd *cobra.Command) {
-	parentCmd.AddCommand(preflightCheck)
-}
-
-func preFlightCheckHandler(cmd *cobra.Command, args []string) error {
-	util.CliLogger.Debug("Starting Pre Flight Checks")
-	util.CliLogger.Debug("Fetching Config from the Platform")
-	ctx := context.Background()
-	//Get Preflight checks config from platform.
-	response, err := taskExecutor.ExecuteTask(ctx, task.HandleGetPlatformConfig())
-	if err != nil {
-		util.CliLogger.Errorf("Failed fetching config from the platform - %s", err)
-		return err
-	}
-	util.CliLogger.Info("Fetched Config from the Platform")
-	data, ok := response.(*model.NodeInstanceType)
-	if !ok {
-		panic("NodeInstanceType type inference error")
-	}
-
-	util.CliLogger.Info("Running Pre-flight checks")
-	response, err = taskExecutor.ExecuteTask(ctx, task.HandlePreflightCheck(*data))
-	if err != nil {
-		util.CliLogger.Errorf("Task Execution Failed - %s", err.Error())
-		return err
-	}
-	preflightChecksData, ok := response.(map[string]model.PreflightCheckVal)
-	if !ok {
-		panic("Type Inference failed for preflight checks")
-	}
-	task.OutputPreflightCheck(preflightChecksData)
-
-	util.CliLogger.Info("Evaluating the Preflight Checks")
-	response, err = taskExecutor.ExecuteTask(
-		ctx,
-		task.HandleSendNodeCapability(preflightChecksData),
-	)
-	if err != nil {
-		util.CliLogger.Errorf("Preflight Checks Failed - %s", err)
-		return err
-	}
-	nodeCapData, ok := response.(*map[string]model.NodeCapabilityResponse)
-	if !ok {
-		errStr := "Preflight Checks success response inference error"
-		util.CliLogger.Errorf(errStr)
-		return errors.New(errStr)
-	}
-	nodeUuid := (*nodeCapData)[appConfig.GetString(util.NodeIP)].NodeUuid
-	//Update the config with NodeUUID
-	appConfig.Update(util.NodeId, nodeUuid)
-	util.CliLogger.Infof("Node Instance created with Node UUID - %s", nodeUuid)
-	return nil
-}

managed/node-agent/adapters/cli/node/register.go
@@ -1,123 +1,0 @@
-// Copyright (c) YugaByte, Inc.
-
-package node
-
-import (
-	"context"
-	"errors"
-	"fmt"
-	"node-agent/app/task"
-	"node-agent/model"
-	"node-agent/util"
-
-	"github.com/spf13/cobra"
-)
-
-var (
-	registerCmd = &cobra.Command{
-		Use:   "register",
-		Short: "Registers a node",
-		Long:  "Registers a node with the Platform by making a call to the platform.",
-		RunE:  registerCmdHandler,
-	}
-
-	unregisterCmd = &cobra.Command{
-		Use:   "unregister",
-		Short: "Unregisters a node",
-		RunE:  unregisterCmdHandler,
-	}
-)
-
-func SetupRegisterCommand(parentCmd *cobra.Command) {
-	registerCmd.PersistentFlags().String("api_token", "", "API Token for registering the node.")
-	registerCmd.MarkPersistentFlagRequired("api_token")
-	parentCmd.AddCommand(registerCmd)
-	parentCmd.AddCommand(unregisterCmd)
-}
-
-func unregisterCmdHandler(cmd *cobra.Command, args []string) error {
-	//Run the unregister flow using JWT.
-	return unregisterHandler(true, "")
-}
-
-func unregisterHandler(useJWT bool, apiToken string) error {
-
-	ctx := context.Background()
-	nodeAgentId := appConfig.GetString(util.NodeAgentId)
-
-	//Return error if there is no node agent id present in the config.
-	if nodeAgentId == "" {
-		err := errors.New(
-			"Node Agent Unregistration Failed - Node Agent ID not found in the config",
-		)
-		util.CliLogger.Errorf(err.Error())
-		return err
-	}
-	util.CliLogger.Infof("Unregistering Node Agent - %s", nodeAgentId)
-
-	_, err := taskExecutor.ExecuteTask(ctx, task.HandleAgentUnregister(useJWT, apiToken))
-	if err != nil {
-		util.CliLogger.Errorf("Node Agent Unregistration Failed - %s", err)
-		return err
-	}
-	appConfig.Update(util.NodeAgentId, "")
-	util.CliLogger.Infof("Node Agent Unregistration Successful")
-	return nil
-}
-
-func registerCmdHandler(cmd *cobra.Command, args []string) error {
-	apiToken, err := cmd.Flags().GetString("api_token")
-	if err != nil {
-		util.CliLogger.Errorf("Unable to get API token - %s", err.Error())
-	}
-	ctx := context.Background()
-
-	util.CliLogger.Infof(
-		"Starting Node Agent registration (Version: %s)",
-		appConfig.GetString(util.PlatformVersion),
-	)
-	util.CliLogger.Info("Submiting Registration task to the executor.")
-	//Call the Platform to register the node agent.
-	response, err := taskExecutor.ExecuteTask(ctx, task.HandleAgentRegistration(apiToken))
-	if err != nil {
-		util.CliLogger.Errorf("Node Agent Registration Failed - %s", err)
-		return err
-	}
-	data, ok := response.(*model.RegisterResponseSuccess)
-	if !ok {
-		err = fmt.Errorf("Node Agent Registering Failed - Type Inference Error")
-		util.CliLogger.Error(err.Error())
-		return err
-	}
-
-	nuuid := data.Uuid
-	//Update the config with the node uuid.
-	appConfig.Update(util.NodeAgentId, nuuid)
-	//Save the certs in the node.
-	util.CliLogger.Info("Saving the node agent certs.")
-
-	certsUUID := util.NewUUID().String()
-	appConfig.Update(util.PlatformCerts, certsUUID)
-	err = util.SaveCerts(appConfig, data.Config.ServerCert, data.Config.ServerKey, certsUUID)
-	if err != nil {
-		util.CliLogger.Info(
-			"Error while saving certs, unregistering the node-agent in the platform.",
-		)
-		//Use API token to run the unregistration to avoid using corrupt JWT.
-		unregisterHandler(false, apiToken)
-		return err
-	}
-	util.CliLogger.Info("Setting node agent state to LIVE.")
-	// TODO add generic retry for HTTP calls.
-	response, err = taskExecutor.ExecuteTask(
-		ctx,
-		task.HandlePutAgentState(model.Live, appConfig.GetString(util.PlatformVersion)),
-	)
-	if err != nil {
-		util.CliLogger.Errorf("Node Agent Registration Failed - %s", err)
-		return err
-	}
-
-	util.CliLogger.Infof("Node Agent Registration Successful with Node ID - %s.", nuuid)
-	return nil
-}

managed/node-agent/app/executor/result.go
@@ -1,37 +1,0 @@
-// Copyright (c) YugaByte, Inc.
-
-package executor
-
-//Struct that stores the result of a task executor handler.
-type Result struct {
-	err    error
-	status string
-	data   any
-}
-
-func NewEmptyResult() *Result {
-	return &Result{}
-}
-func NewResult(err error, status string, data any) *Result {
-	return &Result{err: err, status: status, data: data}
-}
-
-func (r Result) Err() error {
-	return r.err
-}
-
-func (r Result) Data() any {
-	return r.data
-}
-
-func (r *Result) SetErr(err error) {
-	r.err = err
-}
-
-func (r *Result) SetStatus(status string) {
-	r.status = status
-}
-
-func (r *Result) SetData(dto any) {
-	r.data = dto
-}

managed/node-agent/app/executor/task_executor.go
@@ -4,6 +4,7 @@ package executor
 
 import (
 	"context"
+	"errors"
 	"fmt"
 	"node-agent/util"
 	"sync"
@@ -11,26 +12,18 @@ import (
 
 // Future is a struct for facilitating async tasks.
 type Future struct {
-	done       bool
-	responseWg *sync.WaitGroup
-	data       *Result
+	ch   chan struct{}
+	data any
+	err  error
 }
 
-func (f *Future) Get() *Result {
-	f.responseWg.Wait()
-	f.done = true
-	return f.data
+func (f *Future) Done() <-chan struct{} {
+	return f.ch
 }
 
-func (f *Future) IsDone() bool {
-	return f.done
-}
-
-// This is a wrapper for handler that allows any handler to
-// run asynchronously.
-type asyncTask struct {
-	taskHandle   util.Handler
-	futureHandle *Future
+func (f *Future) Get() (any, error) {
+	<-f.ch
+	return f.data, f.err
 }
 
 var (
@@ -72,55 +65,40 @@ func (te *TaskExecutor) WaitOnShutdown() {
 	te.wg.Wait()
 }
 
-// Runs the task handler and puts the result in Future.data.
-// Listens for the ctx.cancel signal to cancel the task at
-// task executor level as well as the task level.
-func (te *TaskExecutor) runTask(ctx context.Context, asyncTask asyncTask) {
-	defer te.wg.Done()
-	defer asyncTask.futureHandle.responseWg.Done()
-	var result *Result
-	select {
-	//TaskExecutor level context.
-	case <-te.ctx.Done():
-		result = NewResult(fmt.Errorf("TaskExecutor is closed"), "canceled", nil)
-	//Task level context
-	case <-ctx.Done():
-		result = NewResult(fmt.Errorf("Task cancelled"), "canceled", nil)
-	default:
-		response, err := asyncTask.taskHandle(ctx)
-		if err != nil {
-			result = NewResult(err, "error", response)
-		} else {
-			result = NewResult(nil, "success", response)
-		}
-	}
-	asyncTask.futureHandle.data = result
-}
-
 // SubmitTask wraps a task in asyncTask and assigns the
 // async task to a goroutine. It returns a Future.
 func (te *TaskExecutor) SubmitTask(ctx context.Context, handler util.Handler) (*Future, error) {
 	if te.isShutdown() {
-		return nil, fmt.Errorf("TaskExecutor is closed")
+		return nil, fmt.Errorf("TaskExecutor is shutdown")
 	}
 	te.wg.Add(1)
-	futureHandler := &Future{responseWg: &sync.WaitGroup{}}
-	futureHandler.responseWg.Add(1)
-	futureTask := asyncTask{taskHandle: handler, futureHandle: futureHandler}
-	go te.runTask(ctx, futureTask)
-	return futureTask.futureHandle, nil
+	future := &Future{ch: make(chan struct{})}
+	go func() {
+		defer close(future.ch)
+		defer te.wg.Done()
+		select {
+		// TaskExecutor level context.
+		case <-te.ctx.Done():
+			future.err = errors.New("TaskExecutor is shutdown")
+		// Task level context.
+		case <-ctx.Done():
+			future.err = errors.New("Task is cancelled")
+		default:
+			future.data, future.err = handler(ctx)
+		}
+	}()
+	return future, nil
 }
 
 // Submits a task and waits for completion.
-func (te *TaskExecutor) ExecuteTask(ctx context.Context, handler util.Handler) (any, error) {
+func (te *TaskExecutor) ExecuteTask(ctx context.Context, handler util.Handler) error {
 	future, err := te.SubmitTask(ctx, handler)
 	if err != nil {
-		return nil, fmt.Errorf("Failed to submit the task. Error: %s", err.Error())
+		return fmt.Errorf("Failed to submit the task. Error: %s", err.Error())
 	}
-	result := future.Get()
-	if result.Err() != nil {
-		//result.data might return nil. The caller needs to nil check.
-		return result.data, fmt.Errorf("Error in executing the task. Error: %s", result.Err())
+	_, err = future.Get()
+	if err != nil {
+		return fmt.Errorf("Error in executing the task. Error: %s", err.Error())
 	}
-	return result.data, nil
+	return nil
 }

commit 10178870b3c01b01b7a213b6e6b29aae74e6bc19

[PLAT-4931][PLAT-5650] Remove dependency overrides for jackson libraries
Summary:
Step 3 of moving swaggerGen out of prod dependencies.
Here we are getting rid of bunch of workarounds in prod depes that we had to do only to make swagger play happy.
Not that swagger play has moved to its own project, we do not need to carry this burdon.

This may unblock few other features that needed specific jackson-databind dependencies

Also fix sbt shell history broken by D19442
Now the history resides under `~/.sbt/yugaware-history` thus will be shared for yugaware projects opened across source trees.

Test Plan: Make sure that unitests and itests pass.

Reviewers: nsingh, amalyshev

Reviewed By: amalyshev

Subscribers: jenkins-bot, yugaware

Differential Revision: http://phabricator.dev.yugabyte.com/D20025

managed/build.sbt
@@ -5,6 +5,8 @@ import sbt.Tests._
 
 import scala.sys.process.Process
 
+historyPath := Some(file(System.getenv("HOME") + "/.sbt/.yugaware-history"))
+
 useCoursier := false
 
 // ------------------------------------------------------------------------------------------------
@@ -423,18 +425,10 @@ libraryDependencies ++= Seq(
   "org.slf4j" % "slf4j-ext" % "1.7.26",
   "net.minidev" % "json-smart" % "2.4.8",
   "com.nimbusds" % "nimbus-jose-jwt" % "7.9",
-  // TODO(Shashank): Remove this in Step 3:
-  // Overrides to address vulnerability in swagger-play2
-  "com.typesafe.akka" %% "akka-actor" % "2.5.16"
 )
 
 dependencyOverrides += "com.google.protobuf" % "protobuf-java" % "3.19.4"
 dependencyOverrides += "com.google.guava" % "guava" % "23.0"
-// TODO(Shashank): Remove these in Step 3:
-dependencyOverrides += "com.fasterxml.jackson.module" %% "jackson-module-scala" % "2.9.10"
-dependencyOverrides += "com.fasterxml.jackson.dataformat" % "jackson-dataformat-cbor" % "2.9.10"
-dependencyOverrides += "com.fasterxml.jackson.datatype" % "jackson-datatype-jsr310" % "2.9.10"
-dependencyOverrides += "com.fasterxml.jackson.core" % "jackson-databind" % "2.9.10.8"
 
 concurrentRestrictions in Global := Seq(Tags.limitAll(16))
 

managed/src/test/java/com/yugabyte/yw/controllers/CustomerConfigControllerTest.java
@@ -106,7 +106,7 @@ public class CustomerConfigControllerTest extends FakeDBApplication {
         result,
         "Cannot deserialize value of type "
             + "`com.yugabyte.yw.models.configs.CustomerConfig$ConfigType` from String \\\"foo\\\": "
-            + "value not one of declared Enum instance names: "
+            + "not one of the values accepted for Enum class: "
             + "[STORAGE, CALLHOME, PASSWORD_POLICY, ALERTS]");
     assertAuditEntry(0, defaultCustomer.uuid);
   }
@@ -127,8 +127,8 @@ public class CustomerConfigControllerTest extends FakeDBApplication {
 
     assertBadRequest(
         result,
-        "Cannot deserialize instance of `com.fasterxml.jackson.databind.node.ObjectNode` "
-            + "out of VALUE_STRING token");
+        "Cannot deserialize value of type `com.fasterxml.jackson.databind.node.ObjectNode` "
+            + "from String value");
     assertAuditEntry(0, defaultCustomer.uuid);
   }
 