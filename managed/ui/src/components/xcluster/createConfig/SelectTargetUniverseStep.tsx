bc6f1979d3d9b67473b956d48ae9be7a020a82c1
Jethro Mak <88681329+Jethro-M@users.noreply.github.com>
2022-10-07 21:02:15 -0400

    [BACKPORT 2.17.0][PLAT-5646][PLAT-5640][PLAT-5670] Improve xCluster table selection
    Summary:
    **Context**
    During xCluster config creation, we should disable the checkbox for YSQL namespace entries which
    contain at least one ineligible table.
    This is because creating xCluster configurations for a subset of the tables in a YSQL keyspace is currently not supported.
    In addition, if a YSQL keyspace contains ineligible tables, then the whole keyspace will not be selectable for replication.
    If needed, one may still use yb-admin to create xCluster configurations for a subset of the tables in a YSQL keyspace.
    
    Please be aware that we currently do not support backup/restore at table-level granularity for YSQL.
    The bootstrapping step involves a backup/restore of the source universe data, and initiating a restart replication task
    from the UI will involve bootstrapping. For a smooth experience managing the xCluster configuration from the UI,
    we do not recommend creating xCluster configurations for a subset of the tables in a YSQL keyspace.
    
    We have 2 criteria for eligible tables:
    - Table not already in use
    
      The table is not involved in another xCluster configuration between the same two universes in the same direction.
    
    - Matching table exists on target universe
    
      A table with the same name in the same keyspace and schema exists on the target universe.
    
    If a table fails to meet any of the above criteria, then it is considered an ineligible table for xCluster purposes.
    
    Since we have the concept of `ineligible` tables, we should expose this on the UI for a better user experience.
    
    **Changes**
    - Disable row selection for YSQL keyspaces that contains at least one ineligible table.
    - Disable row selection for YCQL keyspaces which contain only ineligible tables.
    - Don't hide `ineligible` tables for YCQL anyone.
    - This change also enables row expansion for YSQL keyspaces.
    
    Addressing the need to expose the concept of `ineligible` tables:
    Attach a label on each ineligible table which describes exactly which condition failed.
    If the table is used in another xCluster config, we show the name of the xCluster config as well.
    
    To make all this clear to the user, we add an expandable note which explains the concept of `eligible/ineligible table` and
    why certain keyspaces are not selectable.
    
    JIRA: https://yugabyte.atlassian.net/browse/PLAT-5646
    JIRA: https://yugabyte.atlassian.net/browse/PLAT-5640
    JIRA: https://yugabyte.atlassian.net/browse/PLAT-5670
    
    Original commit: 4094e4ad6b17d42fa57bbe326d239ceed086180b / D19946
    
    Test Plan:
    - Create two universes.
    - Create several YSQL keyspaces in both universes.
      Make sure to have a few keyspaces that are shared by both universes.
    - In the shared keyspaces, create the same tables on both universes.
    - In a subset of the shared keyspaces, create additional 'unique' tables
      which exist in only one of the two universe.
    - Try to create an xCluster config from either of the universes.
    - Verify that the YSQL keyspaces which contain unique tables are unselectable at the
      select keyspace step.
      Note: Remember that we are replicating the source universe keyspace. If the unique table
      exists in the target universe keyspace (of the same name), then the keyspace will still be
      selectable since all the tables selected in the source universe exist in the target universe.
    
    {F30417}{F30416}{F30420}
    
    Reviewers: rmadhavan, kkannan, asathyan, lsangappa, hzare
    
    Reviewed By: hzare
    
    Subscribers: yugaware, jenkins-bot
    
    Differential Revision: https://phabricator.dev.yugabyte.com/D20775

--- managed/ui/src/components/xcluster/createConfig/SelectTargetUniverseStep.tsx
+++ managed/ui/src/components/xcluster/createConfig/SelectTargetUniverseStep.tsx
@@ -1,6 +1,5 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { useQuery } from 'react-query';
-import { Collapse } from 'react-bootstrap';
 
 import { YBFormInput, YBFormSelect } from '../../common/forms/fields';
 
@@ -10,11 +9,41 @@ import { Field, FormikProps } from 'formik';
 import { getUniverseStatus, universeState } from '../../universes/helpers/universeHelpers';
 import { fetchUniversesList } from '../../../actions/xClusterReplication';
 import { YBErrorIndicator, YBLoading } from '../../common/indicators';
+import { CollapsibleNote } from '../common/CollapsibleNote';
 
 import styles from './SelectTargetUniverseStep.module.scss';
 
 const YB_ADMIN_XCLUSTER_DOCUMENTATION_URL =
   'https://docs.yugabyte.com/preview/admin/yb-admin/#xcluster-replication-commands';
+
+const FORM_INSTRUCTION = 'Select the target universe';
+
+const NOTE_CONTENT = (
+  <p>
+    <b>Note!</b> If the tables to replicate are not empty on this source universe, ensure that the
+    <b> backup storage config can be accessed from both source and target universe </b>
+    for bootstrapping. Particularly, <b>Network File System (NFS)</b> based storage configs may not
+    be accessible from universes in different regions. In that case, refer to the based storage
+    configs may not be accessible from universes in different regions. In that case, refer to the{' '}
+    <a href={YB_ADMIN_XCLUSTER_DOCUMENTATION_URL}>
+      {'documentation for creating replication using yb-admin.'}
+    </a>
+  </p>
+);
+
+const NOTE_EXPAND_CONTENT = (
+  <div>
+    <b>What is bootstrapping?</b>
+    <p>
+      Bootstrapping brings your target universe to the same checkpoint as your source universe
+      before turning on async replication. First, YBAnywhere creates a backup of the source
+      universe. Then it restores the backup to the target universe. Hence the need to ensure the
+      backup storage config (which you will select on step 3) is accessible from both source &amp;
+      target universe. If the tables selected for xcluster replication are empty on the source
+      universe, no bootstrapping is needed.
+    </p>
+  </div>
+);
 
 interface SelectTargetUniverseStepProps {
   formik: React.MutableRefObject<FormikProps<CreateXClusterConfigFormValues>>;
@@ -25,8 +54,6 @@ export const SelectTargetUniverseStep = ({
   formik,
   currentUniverseUUID
 }: SelectTargetUniverseStepProps) => {
-  const [isNoteDetailsExpanded, setIsNoteDetailsExpanded] = useState(false);
-
   const universeListQuery = useQuery<Universe[]>(['universeList'], () =>
     fetchUniversesList().then((res) => res.data)
   );
@@ -42,7 +69,7 @@ export const SelectTargetUniverseStep = ({
   const { values } = formik.current;
   return (
     <>
-      <div className={styles.formInstruction}>1. Select the target universe</div>
+      <div className={styles.formInstruction}>1. {FORM_INSTRUCTION}</div>
       <div className={styles.formFieldContainer}>
         <Field
           name="configName"
@@ -73,53 +100,7 @@ export const SelectTargetUniverseStep = ({
           label="Target Universe"
         />
       </div>
-      <div className={styles.note}>
-        <i className="fa fa-exclamation-circle" aria-hidden="true" />
-        <div className={styles.noteText}>
-          <p>
-            <b>Note!</b> If the tables to replicate are not empty on this source universe, ensure
-            that the
-            <b> backup storage config can be accessed from both source and target universe </b>
-            for bootstrapping. Particularly, <b>Network File System (NFS)</b> based storage configs
-            may not be accessible from universes in different regions. In that case, refer to the
-            based storage configs may not be accessible from universes in different regions. In that
-            case, refer to the{' '}
-            <a href={YB_ADMIN_XCLUSTER_DOCUMENTATION_URL}>
-              {'documentation for creating replication using yb-admin.'}
-            </a>
-          </p>
-          <button
-            className={styles.toggleNoteDetailsBtn}
-            onClick={(e) => {
-              setIsNoteDetailsExpanded(!isNoteDetailsExpanded);
-            }}
-            type="button"
-          >
-            {isNoteDetailsExpanded ? (
-              <span>
-                Less details <i className="fa fa-caret-up" aria-hidden="true" />
-              </span>
-            ) : (
-              <span>
-                More details <i className="fa fa-caret-down" aria-hidden="true" />
-              </span>
-            )}
-          </button>
-          <Collapse in={isNoteDetailsExpanded}>
-            <div>
-              <h6>What is bootstrapping?</h6>
-              <p>
-                Bootstrapping brings your target universe to the same checkpoint as your source
-                universe before turning on async replication. First, YBAnywhere creates a backup of
-                the source universe. Then it restores the backup to the target universe. Hence the
-                need to ensure the backup storage config (which you will select on step 3) is
-                accessible from both source &amp; target universe. If the tables selected for
-                xcluster replication are empty on the source universe, no bootstrapping is needed.
-              </p>
-            </div>
-          </Collapse>
-        </div>
-      </div>
+      <CollapsibleNote noteContent={NOTE_CONTENT} expandContent={NOTE_EXPAND_CONTENT} />
     </>
   );
 };
