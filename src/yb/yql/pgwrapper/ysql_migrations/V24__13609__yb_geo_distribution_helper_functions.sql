1b9f4463f472ed875149514e76811670f097b02a
Jason Kim <git@jasonk.me>
2022-10-13 17:03:06 -0700

    YSQL: fix V24 migration oids
    Summary:
    D19826 (commit e83aaee12fd0af074f417e83d039c0fcc4f65fd1) adds migration
    V24 but allocates oids skipping 8042.  Shift oids down to allocate 8042.
    See also related commit 945cfd206877d86e5987a7c20628469a806ea0ae that
    fixes V23 oid.
    
    Test Plan:
        ./yb_build.sh fastdebug --gcc11 \
          --java-test 'TestYsqlUpgrade#migratingIsEquivalentToReinitdb'
    
    Reviewers: rachelphan, fizaa, plee
    
    Reviewed By: plee
    
    Subscribers: yql
    
    Differential Revision: https://phabricator.dev.yugabyte.com/D20183

--- src/yb/yql/pgwrapper/ysql_migrations/V24__13609__yb_geo_distribution_helper_functions.sql
+++ src/yb/yql/pgwrapper/ysql_migrations/V24__13609__yb_geo_distribution_helper_functions.sql
@@ -7,15 +7,15 @@ INSERT INTO pg_catalog.pg_proc (
   proargdefaults, protrftypes, prosrc, probin, proconfig, proacl
 ) VALUES
   -- implementation of yb_server_region
-  (8043, 'yb_server_region', 11, 10, 12, 1, 0, 0, '-', 'f', false, false, true, false,
+  (8042, 'yb_server_region', 11, 10, 12, 1, 0, 0, '-', 'f', false, false, true, false,
    's', 's', 0, 0, 1043, '', NULL, NULL, NULL,
    NULL, NULL, 'yb_server_region', NULL, NULL, NULL),
   -- implementation of yb_server_cloud
-  (8044, 'yb_server_cloud', 11, 10, 12, 1, 0, 0, '-', 'f', false, false, true, false,
+  (8043, 'yb_server_cloud', 11, 10, 12, 1, 0, 0, '-', 'f', false, false, true, false,
    's', 's', 0, 0, 1043, '', NULL, NULL, NULL,
    NULL, NULL, 'yb_server_cloud', NULL, NULL, NULL),
   -- implementation of yb_server_zone
-  (8045, 'yb_server_zone', 11, 10, 12, 1, 0, 0, '-', 'f', false, false, true, false,
+  (8044, 'yb_server_zone', 11, 10, 12, 1, 0, 0, '-', 'f', false, false, true, false,
    's', 's', 0, 0, 1043, '', NULL, NULL, NULL,
    NULL, NULL, 'yb_server_zone', NULL, NULL, NULL)
 ON CONFLICT DO NOTHING;
@@ -26,13 +26,13 @@ DO $$
 BEGIN
   IF NOT EXISTS (
     SELECT FROM pg_catalog.pg_depend
-      WHERE refclassid = 1255 AND refobjid = 8043
+      WHERE refclassid = 1255 AND refobjid = 8042
   ) THEN
     INSERT INTO pg_catalog.pg_depend (
       classid, objid, objsubid, refclassid, refobjid, refobjsubid, deptype
     ) VALUES
+      (0, 0, 0, 1255, 8042, 0, 'p'),
       (0, 0, 0, 1255, 8043, 0, 'p'),
-      (0, 0, 0, 1255, 8044, 0, 'p'),
-      (0, 0, 0, 1255, 8045, 0, 'p');
+      (0, 0, 0, 1255, 8044, 0, 'p');
   END IF;
 END $$;
