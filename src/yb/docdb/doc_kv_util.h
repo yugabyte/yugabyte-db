7eb865180299dadb948b0ae9de7b4e655ce4b5b7
Sergei Politov <spolitov@gmail.com>
2022-10-15 08:40:27 +0300

    [#14280] DocDB: Avoid decoding doc hybrid time during compaction when possible
    Summary:
    We could avoid decoding doc hybrid time during compaction in most cases.
    Usually we could just compare encoded doc hybrid times.
    The exception is the following cases, when we need hybrid time, that could be extracted only from a decoded doc hybrid time:
    1) Keep write time for YCQL columns while updating packed row.
    2) Handle TTL.
    
    Added new test to measure compaction time.
    Executed the following for master and this diff:
    ```ybd release --gtest_filter QLTabletTest.ShortPKCompactionTime -n 20 --clang13 -- -p 1 -k```
    
    Average compaction time for master: 226410us
    Average compaction time for this diff: 213359us
    I.e. 6% improvement.
    
    Test Plan: Jenkins
    
    Reviewers: mbautin
    
    Reviewed By: mbautin
    
    Subscribers: ybase
    
    Differential Revision: https://phabricator.dev.yugabyte.com/D19626

--- src/yb/docdb/doc_kv_util.h
+++ src/yb/docdb/doc_kv_util.h
@@ -37,14 +37,12 @@ constexpr int kEncodedKeyStrTerminatorSize = 2;
 // document key (a key that has already had zero characters escaped). This is done simply by
 // checking that the key starts with the encoded document key followed by two zero characters.
 // This is only used in unit tests as of 08/02/2016.
-bool KeyBelongsToDocKeyInTest(const rocksdb::Slice &key, const std::string &encoded_doc_key);
+bool KeyBelongsToDocKeyInTest(const Slice &key, const std::string &encoded_doc_key);
 
 // Given a DocDB key stored in RocksDB, validate the DocHybridTime size stored as the
 // last few bits of the final byte of the key, and ensure that the ValueType byte preceding that
 // encoded DocHybridTime is ValueType::kHybridTime.
-Status CheckHybridTimeSizeAndValueType(
-    const rocksdb::Slice& key,
-    size_t* ht_byte_size_dest);
+Result<size_t> CheckHybridTimeSizeAndValueType(const Slice& key);
 
 template <class Buffer>
 void AppendUInt16ToKey(uint16_t val, Buffer* dest) {
