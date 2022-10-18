8dca3c6ce9ce9a1e61eafbf4f1b1b7d41919d510
Naorem Khogendro Singh <nsingh@yugabyte.com>
2022-10-18 14:24:35 -0700

    [PLAT-5667] Add gRPC interceptor to authenticate requests to node-agent from platform
    Summary:
    The changes:
    1. Enables Java client to send request with JWT to a node-agent server which authenticates the token.
    2. Add code to enable adding of more tests for gRPC server in golang side and client requests tests on Java side.
    3. Added NodeAgentClient.java to have all the methods needed to talk to a node-agent. Future methods will be added here.
    
    Test Plan:
    Unit tests ran for ping.
    
    2022/10/18 16:36:37 Listening to server address 127.0.0.1:49590
    === RUN   TestPing
    --- PASS: TestPing (0.00s)
    
    For the cert interceptor, I tested it manually and saw the log lines.
    
    ```
    2022-10-19T21:32:28.398Z  [debug] 9b028cb2-6161-45c2-b55d-b6bd3b74afa7 NodeAgentClient.java:81 [application-akka.actor.default-dispatcher-271] com.yugabyte.yw.common.NodeAgentClient$GrpcClientRequestInterceptor Setting authorizaton token in header
    2022-10-19T21:32:28.613Z  [warn] 9b028cb2-6161-45c2-b55d-b6bd3b74afa7 NodeAgentHandler.java:177 [application-akka.actor.default-dispatcher-271] com.yugabyte.yw.controllers.handlers.NodeAgentHandler Node agent 63444543-cd02-47c5-97c2-d7de320921c8 is still reachable but not heartbeating. Skipping clean-up
    ```
    
    Reviewers: cwang, nbhatia, amalyshev
    
    Reviewed By: amalyshev
    
    Subscribers: jenkins-bot, yugaware
    
    Differential Revision: https://phabricator.dev.yugabyte.com/D20259

---
+++ managed/src/test/java/com/yugabyte/yw/common/NodeAgentClientTest.java
@@ -0,0 +1,84 @@
+// Copyright (c) Yugabyte, Inc.
+
+package com.yugabyte.yw.common;
+
+import static org.mockito.AdditionalAnswers.delegatesTo;
+import static org.mockito.Mockito.mock;
+
+import com.typesafe.config.Config;
+import com.yugabyte.yw.controllers.handlers.NodeAgentHandler;
+import com.yugabyte.yw.forms.NodeAgentForm;
+import com.yugabyte.yw.models.Customer;
+import com.yugabyte.yw.models.NodeAgent;
+import com.yugabyte.yw.nodeagent.NodeAgentGrpc;
+import com.yugabyte.yw.nodeagent.Server.PingRequest;
+import com.yugabyte.yw.nodeagent.Server.PingResponse;
+import io.grpc.ManagedChannel;
+import io.grpc.inprocess.InProcessChannelBuilder;
+import io.grpc.inprocess.InProcessServerBuilder;
+import io.grpc.testing.GrpcCleanupRule;
+import java.io.IOException;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.junit.MockitoJUnitRunner;
+
+@RunWith(MockitoJUnitRunner.class)
+public class NodeAgentClientTest extends FakeDBApplication {
+  private NodeAgentClient nodeAgentClient;
+  private NodeAgentHandler nodeAgentHandler;
+  private Customer customer;
+  private NodeAgent nodeAgent;
+
+  // Graceful shutdown of the registered servers and their channels after the tests.
+  @Rule public final GrpcCleanupRule grpcCleanup = new GrpcCleanupRule();
+
+  private NodeAgentGrpc.NodeAgentImplBase serviceImpl;
+
+  @Before
+  public void setup() throws IOException {
+    customer = ModelFactory.testCustomer();
+    nodeAgentHandler = app.injector().instanceOf(NodeAgentHandler.class);
+    NodeAgentForm payload = new NodeAgentForm();
+    payload.version = "2.12.0";
+    payload.name = "node";
+    payload.ip = "10.20.30.40";
+    nodeAgentHandler.enableConnectionValidation(false);
+    nodeAgent = nodeAgentHandler.register(customer.uuid, payload);
+    serviceImpl =
+        mock(
+            NodeAgentGrpc.NodeAgentImplBase.class,
+            delegatesTo(
+                new NodeAgentGrpc.NodeAgentImplBase() {
+                  @Override
+                  public void ping(
+                      PingRequest request,
+                      io.grpc.stub.StreamObserver<PingResponse> responseObserver) {
+                    responseObserver.onNext(
+                        PingResponse.newBuilder().setData(request.getData()).build());
+                  }
+                }));
+    // Generate a unique in-process server name.
+    String serverName = InProcessServerBuilder.generateName();
+
+    // Create a server, add service, start, and register for automatic graceful shutdown.
+    grpcCleanup.register(
+        InProcessServerBuilder.forName(serverName)
+            .directExecutor()
+            .addService(serviceImpl)
+            .build()
+            .start());
+
+    // Create a client channel and register for automatic graceful shutdown.
+    ManagedChannel channel =
+        grpcCleanup.register(InProcessChannelBuilder.forName(serverName).directExecutor().build());
+
+    nodeAgentClient = new NodeAgentClient(mock(Config.class), config -> channel);
+  }
+
+  @Test
+  public void testPingSuccess() {
+    nodeAgentClient.validateConnection(nodeAgent, true);
+  }
+}
