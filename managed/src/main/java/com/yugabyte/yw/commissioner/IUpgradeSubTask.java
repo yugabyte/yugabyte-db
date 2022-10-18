73e3d6267ec0fb308fb761d0db6c6f58b456393e
mohansoundar-yb <mohansoundar-yb@outlook.in>
2022-10-18 16:03:25 +0000

    Reverting changes brought in via diff D19334 to unblock master builds
    Summary: D19334 brought in the 'hot reload of certs'. However the developers complain of master build failing. Reverting it and will try to narrow down to the buggy file
    
    Test Plan: Just make sure the build goes through
    
    Reviewers: hzare, vpatibandla
    
    Reviewed By: vpatibandla
    
    Subscribers: jenkins-bot, yugaware
    
    Differential Revision: https://phabricator.dev.yugabyte.com/D20243

--- managed/src/main/java/com/yugabyte/yw/commissioner/IUpgradeSubTask.java
+++ managed/src/main/java/com/yugabyte/yw/commissioner/IUpgradeSubTask.java
@@ -7,10 +7,6 @@ import com.yugabyte.yw.models.helpers.NodeDetails;
 import java.util.List;
 import java.util.Set;
 
-/**
- * This is a sub task creator interface. This will be called at appropriate moment, when the tasks
- * are to be added
- */
 @FunctionalInterface
 public interface IUpgradeSubTask {
   void run(List<NodeDetails> nodes, Set<ServerType> processTypes);
