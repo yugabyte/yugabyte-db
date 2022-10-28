ca33d43d0062990bd7650eada5ea2c1727f9af00
lingeshwar <lingesh100@gmail.com>
2022-10-28 11:41:18 +0530

    [BACKPORT 2.17.0][PLAT-6009] - fix : All metrics are in loading state in UI
    Summary: Error handled when we get bad response
    
    Test Plan:
    Tested Manually
    
    {F32236}
    
    Reviewers: kkannan, jmak, asathyan
    
    Reviewed By: asathyan
    
    Subscribers: jenkins-bot, ui, yugaware
    
    Differential Revision: https://phabricator.dev.yugabyte.com/D20559

--- managed/ui/src/reducers/reducer_graph.js
+++ managed/ui/src/reducers/reducer_graph.js
@@ -17,7 +17,7 @@ const INITIAL_STATE = {
   loading: false,
   error: null,
   universeMetricList: [],
-  prometheusQueryEnabled: true,
+  prometheusQueryEnabled: true
 };
 
 export default function (state = INITIAL_STATE, action) {
@@ -39,12 +39,18 @@ export default function (state = INITIAL_STATE, action) {
     }
     case QUERY_METRICS_FAILURE: {
       const metricData = state.metrics;
-      metricData[action.panelType] = {};
+      metricData[action.panelType] = { error: true };
       return { ...state, metrics: metricData, error: action.payload.response, loading: false };
     }
     case RESET_METRICS:
       // Graph Filter needs to be reset when user jumps to metrics view in different places
-      return { ...state, metrics: {}, loading: false, panelType: null, graphFilter: DEFAULT_GRAPH_FILTER };
+      return {
+        ...state,
+        metrics: {},
+        loading: false,
+        panelType: null,
+        graphFilter: DEFAULT_GRAPH_FILTER
+      };
     case TOGGLE_PROMETHEUS_QUERY:
       return { ...state, prometheusQueryEnabled: !state.prometheusQueryEnabled };
     default:
