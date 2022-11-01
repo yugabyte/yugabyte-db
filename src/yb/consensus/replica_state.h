a4d02ef6c65885e227eb1c61a4d8c81cbcf39d3c
Jonathan Hu <qhu@yugabyte.com>
2022-11-01 00:10:32 +0000

    [#14225] docdb: disallow advance max_safe_time_returned_with_lease_ before NoOp of current term is replicated
    Summary:
    The issue [[ https://github.com/yugabyte/yugabyte-db/issues/14225 | GH-14225 ]] is caused by the following scenario:
    ```
    1. old leader ts-1 has operations not majority replicated and crashed.
    ts-1 has 1.1(NoOp) 1.2 1.3 1.4 last committed 1.3 and last received 1.4
    ts-2 has 1.1(NoOp) 1.2 1.3 last committed 1.3 and last received 1.3
    ts-3 has 1.1(NoOp) last committed 1.1 and last received 1.1
    
    2. ts-2 becomes the new leader and replicate 1.2 1.3 to ts-3, but before NO_OP replicated on ts-3, it serves a read,
    and advanced the safe time lower bound.
    
    ts-1 crashed
    ts-2 has 1.1(NoOp) 1.2 1.3 2.4(NoOp) last committed 1.3 and last received 2.4
    ts-3 has 1.1(NoOp) 1.2 1.3 last committed 1.3 and last received 1.3
    
    3. ts-1 becomes the leader again and replicated its pending operation (1.4) with lower hybrid time to ts-2, ts-2 raise a fatal error
    
    New operation's hybrid time too low: { physical: 1666367884528238 }, op id: 1.4
    ```
    
    We should avoid advancing `max_safe_time_returned_with_lease_` before NoOp of current term is majority replicated.
    
    Without the fix, test RaftConsensusITest.GetSafeTimeBeforeNoOpReplicated cannot be able to agree at index 5 in the end because of the fatal error and fail.
    
    Test Plan: RaftConsensusITest.GetSafeTimeBeforeNoOpReplicated
    
    Reviewers: timur, rthallam, mbautin, amitanand, sergei
    
    Reviewed By: sergei
    
    Subscribers: bogdan, ybase
    
    Differential Revision: https://phabricator.dev.yugabyte.com/D20221

--- src/yb/consensus/replica_state.h
+++ src/yb/consensus/replica_state.h
@@ -371,7 +371,7 @@ class ReplicaState {
   void UpdateOldLeaderLeaseExpirationOnNonLeaderUnlocked(
       const CoarseTimeLease& lease, const PhysicalComponentLease& ht_lease);
 
-  void SetMajorityReplicatedLeaseExpirationUnlocked(
+  Status SetMajorityReplicatedLeaseExpirationUnlocked(
       const MajorityReplicatedData& majority_replicated_data,
       EnumBitSet<SetMajorityReplicatedLeaseExpirationFlag> flags);
 
