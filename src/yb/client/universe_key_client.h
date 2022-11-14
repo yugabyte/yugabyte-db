b4c4f84fcdc8f1a658f0c31d330fbb47e689f78d
Siddharth Shah <sshah@yugabyte.com>
2022-11-14 17:30:33 +0530

    [BACKPORT 2.14] Revert "[14585] DocDB: Backoff on Retry for Fetching Latest Universe Key on Master Init"
    Summary:
    Original Commit: b959cc9adfabca177d79a124ab959b0d99d32bb7 / D21082
    
    Reverted because of a data race condition mentioned in #14976
    
    Test Plan: Checking if the TSAN test failure disappear.
    
    Reviewers: dfelsing
    
    Reviewed By: dfelsing
    
    Differential Revision: https://phabricator.dev.yugabyte.com/D21084

--- src/yb/client/universe_key_client.h
+++ src/yb/client/universe_key_client.h
@@ -25,7 +25,6 @@
 
 #include "yb/rpc/rpc_fwd.h"
 
-#include "yb/util/backoff_waiter.h"
 #include "yb/util/status_fwd.h"
 #include "yb/util/net/net_util.h"
 
@@ -47,12 +46,11 @@ class UniverseKeyClient {
  private:
 
   void ProcessGetUniverseKeyRegistryResponse(
-      std::shared_ptr<master::GetUniverseKeyRegistryResponsePB> resp,
-      std::shared_ptr<rpc::RpcController> rpc,
-      HostPort hp,
-      CoarseBackoffWaiter backoff_waiter);
+    std::shared_ptr<master::GetUniverseKeyRegistryResponsePB> resp,
+    std::shared_ptr<rpc::RpcController> rpc,
+    HostPort hp);
 
-  void SendAsyncRequest(HostPort host_port, CoarseBackoffWaiter backoff_waiter);
+  void SendAsyncRequest(HostPort host_port);
 
   mutable std::mutex mutex_;
   mutable std::condition_variable cond_;
