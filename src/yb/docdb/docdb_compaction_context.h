09ddeae926fabbf5750073a0c2faaecdf9595d10
Jonathan Hu <qhu@yugabyte.com>
2022-11-17 16:10:34 +0000

    [#14632] docdb: disallow packed row when compacting co-located tables, add unit test cases
    Summary:
    The previous diff D20429 introduced flag ysql_enable_packed_row_for_colocated_table.
    But it doesn't protect compaction from packing the rows.
    This diff adds the support.
    
    Test Plan:
    PgPackedRowTest.ColocatedCompactionPackRowDisabled
    PgPackedRowTest.ColocatedPackRowDisabled
    
    Reviewers: sergei
    
    Reviewed By: sergei
    
    Subscribers: ybase, bogdan
    
    Differential Revision: https://phabricator.dev.yugabyte.com/D21164

--- src/yb/docdb/docdb_compaction_context.h
+++ src/yb/docdb/docdb_compaction_context.h
@@ -46,6 +46,8 @@ YB_STRONGLY_TYPED_BOOL(ShouldRetainDeleteMarkersInMajorCompaction);
 struct Expiration;
 using ColumnIds = std::unordered_set<ColumnId, boost::hash<ColumnId>>;
 
+bool PackedRowEnabled(TableType table_type, bool is_colocated);
+
 // A "directive" of how a particular compaction should retain old (overwritten or deleted) values.
 struct HistoryRetentionDirective {
   // We will not keep history below this hybrid_time. The view of the database at this hybrid_time
@@ -66,8 +68,8 @@ struct CompactionSchemaInfo {
   std::shared_ptr<const SchemaPacking> schema_packing;
   Uuid cotable_id;
   ColumnIds deleted_cols;
+  bool enabled;
 
-  bool enabled() const;
   size_t pack_limit() const; // As usual, when not specified size is in bytes.
 
   // Whether we should keep original write time when combining columns updates into packed row.
