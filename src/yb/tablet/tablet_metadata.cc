e1958e9d0eb14c0a851f99ad14aab90cbb2c42b0
Jonathan Hu <qhu@yugabyte.com>
2022-11-18 15:22:30 +0000

    [BACKPORT 2.16][#14632] docdb: disallow packed row when compacting co-located tables, add unit test cases
    Summary:
    Original commit: 3b0e2e7b012613d39dc9881e47828599d9d38a5c / D21164
    The previous diff D20429 introduced flag ysql_enable_packed_row_for_colocated_table.
    But it doesn't protect compaction from packing the rows.
    This diff adds the support.
    
    Test Plan:
    PgPackedRowTest.ColocatedCompactionPackRowDisabled
    PgPackedRowTest.ColocatedPackRowDisabled
    
    Reviewers: sergei, slingam
    
    Reviewed By: slingam
    
    Subscribers: bogdan, ybase
    
    Differential Revision: https://phabricator.dev.yugabyte.com/D21216

--- src/yb/tablet/tablet_metadata.cc
+++ src/yb/tablet/tablet_metadata.cc
@@ -51,6 +51,7 @@
 
 #include "yb/docdb/doc_read_context.h"
 #include "yb/docdb/docdb_rocksdb_util.h"
+#include "yb/docdb/pgsql_operation.h"
 
 #include "yb/gutil/atomicops.h"
 #include "yb/gutil/dynamic_annotations.h"
@@ -280,6 +281,8 @@ Result<docdb::CompactionSchemaInfo> TableInfo::Packing(
     .schema_packing = rpc::SharedField(self, packing.get_ptr()),
     .cotable_id = self->cotable_id,
     .deleted_cols = std::move(deleted_before_history_cutoff),
+    .enabled =
+        docdb::PackedRowEnabled(self->table_type, self->doc_read_context->schema.is_colocated())
   };
 }
 
