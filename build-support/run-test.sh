2eaaaccf0cd76c031170313f393545ec42b7df0c
Mikhail Bautin <mbautin@users.noreply.github.com>
2022-11-29 22:46:05 -0800

    [#15176] Fix build with custom Clang on Apple Silicon macOS and other build improvements
    Summary:
    Fix the build on macOS with Apple Silicon using our custom-build Clang 15 at http://github.com/yugabyte/build-clang. This is necessary for indexing the code using `clangd-indexer`. Relevant changes:
    - Use the automatically set CMAKE_OSX_SYSROOT variable to locate macOS SDK instead of trying to locate it using custom CMake logic.
    - For the actual indexing, use the proper compilation database file, currently at `${BUILD_ROOT}/compile_commands/combined_postprocessed/compile_commands.json` (not `${BUILD_ROOT}/compile_commands.json`, which is the raw compilation database file generated by CMake).
    - When no thirdparty archive is found for the specific Clang version, using the one built with Apple Clang, as it will work just fine for local development.
    
    Improvements to the Python code of the build system:
    - Adding py.typed file to the python/yb module directory to indicate it has type information.
    - Consolidating the init_logging from yb.common_util across various files that need to initialize logging.
    
    Other improvements to the build system:
    - Simplifying the handling of building tests vs. not building them. This is now controlled by the YB_BUILD_TESTS CMake variable, which is stored in CMake cache, and is set on --skip-tests/--no-tests vs. --with-tests flags of yb_build.sh.
    - Removing some compiler version checks for compilers we no longer support.
    - When CMake fails, output the contents of various CMake output files that could provide insight about what went wrong.
    - Support bigger numbers as the argument of the -j option to yb_build.sh (parallelism for Make or Ninja).
    - When composing the compiler flags for Postgres build, do not specify the same include directories multiple times. This removes a lot of clutter in Postgres command lines.
    - Removing an unnecessary check for build root in run-test.sh, which was causing local run_test_on_spark.py invocations to fail. This is useful when testing run_test_on_spark.py on a single-node Spark cluster, e.g. on a development machine.
    
    Other improvements:
    - Getting rid of the ROCKSDB_LIB_IO_POSIX macro.
    
    Test Plan: Jenkins
    
    Reviewers: jharveysmith, hsunder, steve.varnau
    
    Reviewed By: steve.varnau
    
    Subscribers: ybase
    
    Differential Revision: https://phabricator.dev.yugabyte.com/D21412

--- build-support/run-test.sh
+++ build-support/run-test.sh
@@ -131,17 +131,8 @@ fi
 yb_ninja_executable_not_needed=true
 if [[ -z ${BUILD_ROOT:-} ]]; then
   set_build_root
-else
-  preset_build_root=$BUILD_ROOT
-  set_build_root --no-readonly
-  if [[ $preset_build_root != "$BUILD_ROOT" ]] &&
-     ! "$YB_BUILD_SUPPORT_DIR/is_same_path.py" "$preset_build_root" "$BUILD_ROOT"; then
-    fatal "Build root was already set to $preset_build_root, but we determined it must be set" \
-          "to $BUILD_ROOT, and these two paths do not point to the same location."
-  fi
-  readonly BUILD_ROOT
-  unset preset_build_root
 fi
+readonly BUILD_ROOT
 
 find_or_download_ysql_snapshots
 find_or_download_thirdparty
