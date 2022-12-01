f602efe87bba97e4f53b016bc1d144bbf89cef2b
sureshdash2022-yb <sdash@yugabyte.com>
2022-12-01 10:49:39 +0530

    [BACKPORT 2.16][#15154] CDCSDK: Before image failure with packed row
    Summary:
    "Original commit:524fd98111f68596700ee87264576733409e07e4/D21434"
    With packed row enabled, if the current schema version is different than before the image schema version, DocDB will fail to decode the packed row.
    
    For example:-
    1. Enable packed row.
    2. Create a table with 2 columns. So the schema version is V1.
    3. No insert one record into the table.
    4. Do ALTER of the table by adding a new column, so the total columns in the table are 3. Here the schema version is V2.
    5. Now update the record that is inserted in step 2.
    
    Now call GetChanges with the before image enabled. Today the scenario is failing because the before image is in V1 version and the schema version CDC provides to read the before image is V2.
    
    During analysis, we found that CDC uses// docdb::DocRowwiseIterator //with the corresponding schema to read rows from DocDB.  Method //DocRowwiseIterator::HasNext// is used to get the next row from DocDB, which internally calls the Prepare() method of doc_reader to check the row type it reads from DocDB. If the type of row is ValueEntryType::kPackedRow it checks the packed row’s schema version vs the input schema version, if there is a mismatch it throws an error.
    
    To handle this we will use the current running schema version to fetch the before image row from the DocDb, if it fails to get the row, CDC will ignore adding the before image record for the corresponding change records.
    
    Test Plan: Running all the before image unit testcases with packed row enabled
    
    Reviewers: skumar, srangavajjula, abharadwaj
    
    Reviewed By: abharadwaj
    
    Subscribers: ycdcxcluster
    
    Differential Revision: https://phabricator.dev.yugabyte.com/D21451

--- ent/src/yb/cdc/cdcsdk_producer.cc
+++ ent/src/yb/cdc/cdcsdk_producer.cc
@@ -204,7 +204,7 @@ Status PopulateBeforeImage(
     const std::shared_ptr<tablet::TabletPeer>& tablet_peer, const ReadHybridTime& read_time,
     RowMessage* row_message, const EnumOidLabelMap& enum_oid_label_map,
     const CompositeAttsMap& composite_atts_map, const docdb::SubDocKey& decoded_primary_key,
-    const Schema& schema, const SchemaVersion schema_version) {
+     const Schema& schema, const SchemaVersion schema_version) {
   auto tablet = tablet_peer->shared_tablet();
   auto docdb = tablet->doc_db();
 
@@ -219,7 +219,10 @@ Status PopulateBeforeImage(
 
   QLTableRow row;
   QLValue ql_value;
-  if (VERIFY_RESULT(iter.HasNext())) RETURN_NOT_OK(iter.NextRow(&row));
+  // If CDC is failed to get the before image row, skip adding before image columns.
+  if (iter.HasNext().ok()) {
+    RETURN_NOT_OK(iter.NextRow(&row));
+  }
 
   std::vector<ColumnSchema> columns(schema.columns());
 
@@ -390,7 +393,7 @@ Status PopulateCDCSDKIntentRecord(
               RETURN_NOT_OK(PopulateBeforeImage(
                   tablet_peer, ReadHybridTime::FromUint64(hybrid_time), row_message,
                   enum_oid_label_map, composite_atts_map, prev_decoded_key, schema,
-                  tablet_peer->tablet()->metadata()->schema_version()));
+                  schema_version));
             } else {
               for (size_t index = 0; index < schema.num_columns(); ++index) {
                 row_message->add_old_tuple();
@@ -453,8 +456,7 @@ Status PopulateCDCSDKIntentRecord(
           auto hybrid_time = commit_time - 1;
           RETURN_NOT_OK(PopulateBeforeImage(
               tablet_peer, ReadHybridTime::FromUint64(hybrid_time), row_message, enum_oid_label_map,
-              composite_atts_map, decoded_key, schema,
-              tablet_peer->tablet()->metadata()->schema_version()));
+              composite_atts_map, decoded_key, schema, schema_version));
         }
 
         if (row_message->old_tuple_size() == 0) {
@@ -532,8 +534,7 @@ Status PopulateCDCSDKIntentRecord(
             auto hybrid_time = commit_time - 1;
             RETURN_NOT_OK(PopulateBeforeImage(
                 tablet_peer, ReadHybridTime::FromUint64(hybrid_time), row_message,
-                enum_oid_label_map, composite_atts_map, decoded_key, schema,
-                tablet_peer->tablet()->metadata()->schema_version()));
+                enum_oid_label_map, composite_atts_map, decoded_key, schema, schema_version));
           } else {
             for (size_t index = 0; index < schema.num_columns(); ++index) {
               row_message->add_old_tuple();
@@ -557,8 +558,7 @@ Status PopulateCDCSDKIntentRecord(
         auto hybrid_time = commit_time - 1;
         RETURN_NOT_OK(PopulateBeforeImage(
             tablet_peer, ReadHybridTime::FromUint64(hybrid_time), row_message, enum_oid_label_map,
-            composite_atts_map, prev_decoded_key, schema,
-            tablet_peer->tablet()->metadata()->schema_version()));
+            composite_atts_map, prev_decoded_key, schema, schema_version));
       } else {
         for (size_t index = 0; index < schema.num_columns(); ++index) {
           row_message->add_old_tuple();
@@ -639,8 +639,7 @@ Status PopulateCDCSDKWriteRecord(
         if (metadata.record_type == cdc::CDCRecordType::ALL) {
           RETURN_NOT_OK(PopulateBeforeImage(
               tablet_peer, ReadHybridTime::FromUint64(msg->hybrid_time() - 1), row_message,
-              enum_oid_label_map, composite_atts_map, prev_decoded_key, schema,
-              tablet_peer->tablet()->metadata()->schema_version()));
+              enum_oid_label_map, composite_atts_map, prev_decoded_key, schema, schema_version));
         } else {
           for (int new_tuple_index = 0; new_tuple_index < row_message->new_tuple_size();
                ++new_tuple_index) {
@@ -679,8 +678,7 @@ Status PopulateCDCSDKWriteRecord(
           (row_message->op() == RowMessage_Op_DELETE)) {
         RETURN_NOT_OK(PopulateBeforeImage(
             tablet_peer, ReadHybridTime::FromUint64(msg->hybrid_time() - 1), row_message,
-            enum_oid_label_map, composite_atts_map, decoded_key, schema,
-            tablet_peer->tablet()->metadata()->schema_version()));
+            enum_oid_label_map, composite_atts_map, decoded_key, schema, schema_version));
 
         if (row_message->old_tuple_size() == 0) {
           RETURN_NOT_OK(AddPrimaryKey(
@@ -734,8 +732,7 @@ Status PopulateCDCSDKWriteRecord(
     if (metadata.record_type == cdc::CDCRecordType::ALL) {
       RETURN_NOT_OK(PopulateBeforeImage(
           tablet_peer, ReadHybridTime::FromUint64(msg->hybrid_time() - 1), row_message,
-          enum_oid_label_map, composite_atts_map, prev_decoded_key, schema,
-          tablet_peer->tablet()->metadata()->schema_version()));
+          enum_oid_label_map, composite_atts_map, prev_decoded_key, schema, schema_version));
     } else {
       for (int index = 0; index < row_message->new_tuple_size(); ++index) {
         row_message->add_old_tuple();
