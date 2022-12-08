e2e049b22829e07a3a78e8cb5536986d0e78176e
Hamidreza Zare <hzare@yugabyte.com>
2022-12-08 12:32:34 -0500

    [PLAT-5875][xCluster] Transfer the xCluster certs for newly added nodes
    Summary:
    If node-to-node encryption in enabled on the source universe,
    we have to transfer the source root certificate to the target universe
    for the xCluster replication to work. Previously, we did not transfer
    it when new nodes were added to a universe (through edit universe,
    add node, etc. operations) for existing xCluster configs.
    
    This diff transfer the source root certificate to the target universe's
    newly added nodes/pods.
    
    Test Plan:
    Created K8s and non-K8s universes with node-to-node encryption
    enabled/disabled and did the following operations:
    
      # Remove node, Release node, Add node
      # Remove node, Add node
      # Edit universe with and without changing master nodes
    
    Reviewers: sanketh, yshchetinin, amalyshev
    
    Reviewed By: amalyshev
    
    Subscribers: jenkins-bot, yugaware
    
    Differential Revision: https://phabricator.dev.yugabyte.com/D21423

--- managed/src/main/java/com/yugabyte/yw/commissioner/tasks/KubernetesTaskBase.java
+++ managed/src/main/java/com/yugabyte/yw/commissioner/tasks/KubernetesTaskBase.java
@@ -20,6 +20,7 @@ import com.yugabyte.yw.models.helpers.NodeDetails;
 import com.yugabyte.yw.models.helpers.PlacementInfo;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.Comparator;
 import java.util.HashMap;
 import java.util.HashSet;
@@ -464,6 +465,10 @@ public abstract class KubernetesTaskBase extends UniverseDefinitionTaskBase {
             azCode,
             config,
             isReadOnlyCluster);
+
+        // Copy the source root certificate to the pods.
+        createTransferXClusterCertsCopyTasks(
+            Collections.singleton(node), getUniverse(), SubTaskGroupType.ConfigureUniverse);
 
         createWaitForServersTasks(nodeList, serverType)
             .setSubTaskGroupType(SubTaskGroupType.ConfigureUniverse);
