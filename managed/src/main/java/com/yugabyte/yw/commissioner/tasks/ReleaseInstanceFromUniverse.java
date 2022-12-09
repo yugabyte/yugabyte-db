yugabyte/yugabyte-db

commit 7fd0462a18596e2696f0fdd25df3f6e0a37c7cc1

[PLAT-4772][Platform] Allow stop + remove to wait for data to move off tserver
Summary:
Given a universe where data can be moved off a node, we now wait for tablets to move off of the tserver that was first stopped on platform and then removed . Previously, if a node is stopped on Platform, and then the node is removed, we do not wait for the tablets to move off and just continue for the removal of the node from the universe.

We try to move tablets off of a node when possible

We remove the `isTServer` condition in `UpdatePlacementInfo.java` when blacklisting nodes because if a node is stopped, it's `isTServer` value is set as `false` but we would still like to blacklist this node

Test Plan:
Some things to understand beforehand:
1. When a node’s tserver/master is not running, isTserver/isMaster is false
2. If a node is stopped, the node is still alive, just that the tserver/master process is not running, thus `isTserverAliveOnNode` will be false on a stopped node

Create a GCP universe with 6 nodes and rf3, with AZs comprising of us-west-1, us-west-2, and us-east1. In the below tests, we should have a clean universe and all the nodes should be live.

Perform the following tests:

a) Happy path, stopping and then immediately removing a node from the universe
  1. Stop a node in us-west-2
  2. Immediately after the node is stopped, remove the same node from the universe
  3. Go to master UI at <master-ip>:7000/tablet-servers, on a node that is currently not being removed
  4. Keep refreshing the page, we should see the values for the node's `User Tablet-Peers / Leaders` slowly decrease until it hits 0 / 0
  5. The node should successfully be removed

b) Edge Case #1, Only removing a node from the universe
  1. Remove a node in us-west-2 from the universe
  2. Go to master UI at <master-ip>:7000/tablet-servers, on a node that is currently not being removed

managed/src/main/java/com/yugabyte/yw/commissioner/tasks/RemoveNodeFromUniverse.java
@@ -24,6 +24,7 @@ import com.yugabyte.yw.models.helpers.NodeDetails;
 import com.yugabyte.yw.models.helpers.NodeDetails.NodeState;
 import com.yugabyte.yw.models.helpers.PlacementInfo;
 import java.util.Arrays;
+import java.util.Collection;
 import java.util.HashSet;
 import javax.inject.Inject;
 import lombok.extern.slf4j.Slf4j;
@@ -121,44 +122,43 @@ public class RemoveNodeFromUniverse extends UniverseTaskBase {
       // if node is not reachable so as to avoid cases like 1 node in an 3 node cluster is being
       // removed and we know LoadBalancer will not be able to handle that.
       if (instanceAlive) {
-
+        Collection<NodeDetails> nodesExcludingCurrentNode = new HashSet<>(universe.getNodes());
+        nodesExcludingCurrentNode.remove(currentNode);
         int rfInZone =
             PlacementInfoUtil.getZoneRF(
                 pi,
                 currentNode.cloudInfo.cloud,
                 currentNode.cloudInfo.region,
                 currentNode.cloudInfo.az);
-        long nodesInZone =
+        long nodesActiveInAZExcludingCurrentNode =
             PlacementInfoUtil.getNumActiveTserversInZone(
-                universe.getNodes(),
+                nodesExcludingCurrentNode,
                 currentNode.cloudInfo.cloud,
                 currentNode.cloudInfo.region,
                 currentNode.cloudInfo.az);
 
         if (rfInZone == -1) {
           log.error(
-              "Unexpected placement info in univ {} {} {}", universe.name, rfInZone, nodesInZone);
+              "Unexpected placement info in univ {} {} {}",
+              universe.name,
+              rfInZone,
+              nodesActiveInAZExcludingCurrentNode);
           throw new RuntimeException(
               "Error getting placement info for cluster with node: " + currentNode.nodeName);
         }
 
-        // Perform a data migration and stop the tserver process only if it is reachable.
-        boolean tserverReachable = isTserverAliveOnNode(currentNode, masterAddrs);
-        log.info("Tserver {}, reachable = {}.", currentNode.cloudInfo.private_ip, tserverReachable);
-        if (tserverReachable) {
-          // Since numNodes can never be less, that will mean there is a potential node to move
-          // data to.
-          if (userIntent.numNodes > userIntent.replicationFactor) {
-            // We only want to move data if the number of nodes in the zone are more than the RF
-            // of the zone.
-            if (nodesInZone > rfInZone) {
-              createWaitForDataMoveTask()
-                  .setSubTaskGroupType(SubTaskGroupType.WaitForDataMigration);
-            }
+        // Since numNodes can never be less, that will mean there is a potential node to move
+        // data to.
+        if (userIntent.numNodes > userIntent.replicationFactor) {
+          // We only want to move data if the number of nodes in the zone are more than or equal
+          //  the RF of the zone.
+          // We would like to remove currentNode whether it is in live/stopped state
+          if (nodesActiveInAZExcludingCurrentNode >= rfInZone) {
+            createWaitForDataMoveTask().setSubTaskGroupType(SubTaskGroupType.WaitForDataMigration);
           }
-          createTServerTaskForNode(currentNode, "stop")
-              .setSubTaskGroupType(SubTaskGroupType.StoppingNodeProcesses);
         }
+        createTServerTaskForNode(currentNode, "stop")
+            .setSubTaskGroupType(SubTaskGroupType.StoppingNodeProcesses);
       }
 
       // Remove master status (even when it does not exists or is not reachable).

managed/src/main/java/com/yugabyte/yw/commissioner/tasks/subtasks/UpdatePlacementInfo.java
@@ -184,7 +184,7 @@ public class UpdatePlacementInfo extends UniverseTaskBase {
             configBuilder.getServerBlacklistBuilder();
         for (String nodeName : blacklistNodes) {
           NodeDetails node = universe.getNode(nodeName);
-          if (node.isTserver && node.cloudInfo.private_ip != null) {
+          if (node.cloudInfo.private_ip != null) {
             blacklistBuilder.addHosts(
                 ProtobufHelper.hostAndPortToPB(
                     HostAndPort.fromParts(node.cloudInfo.private_ip, node.tserverRpcPort)));

commit ba758393c58df3b9c1e000a26a82564c8d2a3ca7

[BACKPORT 2.16] [PLAT-6630] [PLAT-6656] Fix failure to remove a node if the underlying VM has failed
Summary:
1. Remove a dead node fails because we were expecting the "stop tserver" and "stop ybc" action to succeed - this was partly a regression from
https://github.com/yugabyte/yugabyte-db/commit/7fd0462a18596e2696f0fdd25df3f6e0a37c7cc1 while trying
to fix [PLAT-4772] and partly new ybc code.
2. Releasing a dead node fails on an onprem universe because we were expecting the "stop ybc" action to succeed - this was new ybc related code.

Original commit: https://phabricator.dev.yugabyte.com/D21643

Test Plan:
1. Create a 6 node univ, stop a VM from cloud provider, then remove node from Platform UI
2. Test regular Remove Node with a healthy VM, verify this workflow hasn't changed.
3. Test Stop -> Remove workflow on a healthy VM, verify this workflow hasn't changed
4. Create an onprem univ, remove node -> release node, verify it works.

Reviewers: anijhawan, nsingh, cwang

Reviewed By: cwang

Subscribers: yugaware, anijhawan, jenkins-bot

Differential Revision: https://phabricator.dev.yugabyte.com/D21650

managed/src/main/java/com/yugabyte/yw/commissioner/tasks/ReleaseInstanceFromUniverse.java
@@ -100,7 +100,7 @@ public class ReleaseInstanceFromUniverse extends UniverseTaskBase {
           createStopServerTasks(currentNodeDetails, "tserver", true /* isForceDelete */)
               .setSubTaskGroupType(SubTaskGroupType.StoppingNodeProcesses);
           if (universe.isYbcEnabled()) {
-            createStopYbControllerTasks(new HashSet<>(currentNodeDetails))
+            createStopYbControllerTasks(new HashSet<>(currentNodeDetails), true /*isIgnoreError*/)
                 .setSubTaskGroupType(SubTaskGroupType.StoppingNodeProcesses);
           }
         }

managed/src/main/java/com/yugabyte/yw/commissioner/tasks/RemoveNodeFromUniverse.java
@@ -159,11 +159,13 @@ public class RemoveNodeFromUniverse extends UniverseTaskBase {
             createWaitForDataMoveTask().setSubTaskGroupType(SubTaskGroupType.WaitForDataMigration);
           }
         }
-        createTServerTaskForNode(currentNode, "stop")
+
+        createTServerTaskForNode(currentNode, "stop", true /*isIgnoreErrors*/)
             .setSubTaskGroupType(SubTaskGroupType.StoppingNodeProcesses);
 
         if (universe.isYbcEnabled()) {
-          createStopYbControllerTasks(new HashSet<>(Arrays.asList(currentNode)))
+          createStopYbControllerTasks(
+                  new HashSet<>(Arrays.asList(currentNode)), true /*isIgnoreErrors*/)
               .setSubTaskGroupType(SubTaskGroupType.StoppingNodeProcesses);
         }
       }

managed/src/main/java/com/yugabyte/yw/commissioner/tasks/UniverseTaskBase.java
@@ -1622,6 +1622,9 @@ public abstract class UniverseTaskBase extends AbstractTaskBase {
     subTaskGroup.setSubTaskGroupType(subTask);
   }
 
+  public SubTaskGroup createTServerTaskForNode(NodeDetails currentNode, String taskType) {
+    return createTServerTaskForNode(currentNode, taskType, false /*isIgnoreError*/);
+  }
   /**
    * Start T-Server process on the given node
    *
@@ -1629,7 +1632,8 @@ public abstract class UniverseTaskBase extends AbstractTaskBase {
    * @param taskType Command start/stop
    * @return Subtask group
    */
-  public SubTaskGroup createTServerTaskForNode(NodeDetails currentNode, String taskType) {
+  public SubTaskGroup createTServerTaskForNode(
+      NodeDetails currentNode, String taskType, boolean isIgnoreError) {
     SubTaskGroup subTaskGroup =
         getTaskExecutor().createSubTaskGroup("AnsibleClusterServerCtl", executor);
     AnsibleClusterServerCtl.Params params = new AnsibleClusterServerCtl.Params();
@@ -1643,6 +1647,7 @@ public abstract class UniverseTaskBase extends AbstractTaskBase {
     // The service and the command we want to run.
     params.process = "tserver";
     params.command = taskType;
+    params.isIgnoreError = isIgnoreError;
     // Set the InstanceType
     params.instanceType = currentNode.cloudInfo.instance_type;
     params.useSystemd = userIntent.useSystemd;
@@ -1776,16 +1781,22 @@ public abstract class UniverseTaskBase extends AbstractTaskBase {
    *
    * @param nodes set of nodes on which yb-controller has to be stopped
    */
-  public SubTaskGroup createStopYbControllerTasks(Collection<NodeDetails> nodes) {
-    return createStopServerTasks(nodes, "controller", false);
+  public SubTaskGroup createStopYbControllerTasks(
+      Collection<NodeDetails> nodes, boolean isIgnoreError) {
+    return createStopServerTasks(nodes, "controller", isIgnoreError);
   }
+
+  public SubTaskGroup createStopYbControllerTasks(Collection<NodeDetails> nodes) {
+    return createStopYbControllerTasks(nodes, false /*isIgnoreError*/);
+  }
+
   /**
    * Creates a task list to stop the tservers of the cluster and adds it to the task queue.
    *
    * @param nodes set of nodes to be stopped as master
    */
   public SubTaskGroup createStopServerTasks(
-      Collection<NodeDetails> nodes, String serverType, boolean isForceDelete) {
+      Collection<NodeDetails> nodes, String serverType, boolean isIgnoreError) {
     SubTaskGroup subTaskGroup =
         getTaskExecutor().createSubTaskGroup("AnsibleClusterServerCtl", executor);
     for (NodeDetails node : nodes) {
@@ -1802,7 +1813,7 @@ public abstract class UniverseTaskBase extends AbstractTaskBase {
       params.command = "stop";
       // Set the InstanceType
       params.instanceType = node.cloudInfo.instance_type;
-      params.isForceDelete = isForceDelete;
+      params.isIgnoreError = isIgnoreError;
       // Set the systemd parameter.
       params.useSystemd = userIntent.useSystemd;
       // Create the Ansible task to get the server info.

managed/src/main/java/com/yugabyte/yw/commissioner/tasks/subtasks/AnsibleClusterServerCtl.java
@@ -31,7 +31,7 @@ public class AnsibleClusterServerCtl extends NodeTaskBase {
     public String process;
     public String command;
     public int sleepAfterCmdMills = 0;
-    public boolean isForceDelete = false;
+    public boolean isIgnoreError = false;
 
     public boolean checkVolumesAttached = false;
   }
@@ -62,7 +62,7 @@ public class AnsibleClusterServerCtl extends NodeTaskBase {
           .nodeCommand(NodeManager.NodeCommandType.Control, taskParams())
           .processErrors();
     } catch (Exception e) {
-      if (!taskParams().isForceDelete) {
+      if (!taskParams().isIgnoreError) {
         throw e;
       } else {
         log.debug("Ignoring error: {}", e.getMessage());