yugabyte/yugabyte-db

commit b45efd3ca087bf4c9bf34c70ce1b0e52f27ccbca

[#14910] docdb: use multi index container for table map
Summary:
Replaces the std::map used for the main id->table map in the CatalogManager with a `boost::multi_index_container`. The `boost::multi_index_container` is indexed by id and `!IsColocatedUserTable`. This change greatly reduces the amount of time the tablet split manager and load balancer hold the `CatalogManager.mutex_` lock for clusters with vastly more colocated tables than regular tables.

Some possible additional indices we could add:

  # is_deleted to speed up `AreTablesDeleting`
  # namespace_id to speed up `CollectTables`

Timing information:
I ran an experiment on my dev-server (alma8, 14GB of RAM, 4 cores). I created 60k tables in a single tablegroup with this diff (that took 382.5 minutes). I built the yb binaries using:
```
ybd release
```

Highlights:

  - With this diff, the `PrimaryTables` index had 688 tables and the entire data structure had 60688.
  - The load balancer is over an order of magnitude faster. The max time decreased by two orders of magnitude. It is iterating through 2 orders of magnitude fewer tables so this is believable. CopyForTableSplitting is also over an order of magnitude faster.
  - Iterating through the entire data structure is comparable to master. The copy loop is slightly faster, `CatalogManager::ListTables` appears to be slightly slower.

Then I ran yugabyted with:
```
yugabyted start --master_flags "log_min_seconds_to_retain=120,catalog_manager_bg_task_wait_ms=25,load_balancer_initial_delay_secs=5" --tserver_flags "log_min_seconds_to_retain=120,ysql_beta_feature_tablegroup"
```
for 2 minutes, both with this diff and against master. I timed 3 places where we loop through tables: the load balancer, the copy done for tablet splitting, and the `AreTablesDeleting` check. All invoked through the background task.

Times with this diff:
```
RunLoadBalancerWithOptions
avg: < 1ms
max: 1ms

src/yb/master/catalog_loaders.cc
@@ -72,11 +72,11 @@ Status TableLoader::Visit(const TableId& table_id, const SysTablesEntryPB& metad
     return Status::OK();
   }
 
-  CHECK(!ContainsKey(*catalog_manager_->table_ids_map_, table_id))
-        << "Table already exists: " << table_id;
+  CHECK(catalog_manager_->tables_->FindTableOrNull(table_id) == nullptr)
+      << "Table already exists: " << table_id;
 
   // Setup the table info.
-  scoped_refptr<TableInfo> table = catalog_manager_->NewTableInfo(table_id);
+  scoped_refptr<TableInfo> table = catalog_manager_->NewTableInfo(table_id, metadata.colocated());
   auto l = table->LockForWrite();
   auto& pb = l.mutable_data()->pb;
   pb.CopyFrom(metadata);
@@ -98,8 +98,8 @@ Status TableLoader::Visit(const TableId& table_id, const SysTablesEntryPB& metad
 
   // Add the table to the IDs map and to the name map (if the table is not deleted). Do not
   // add Postgres tables to the name map as the table name is not unique in a namespace.
-  auto table_ids_map_checkout = catalog_manager_->table_ids_map_.CheckOut();
-  (*table_ids_map_checkout)[table->id()] = table;
+  auto table_map_checkout = catalog_manager_->tables_.CheckOut();
+  table_map_checkout->AddTable(table);
   if (!l->started_deleting() && !l->started_hiding()) {
     if (l->table_type() != PGSQL_TABLE_TYPE) {
       catalog_manager_->table_names_map_[{l->namespace_id(), l->name()}] = table;
@@ -156,7 +156,7 @@ Status TabletLoader::Visit(const TabletId& tablet_id, const SysTabletsEntryPB& m
   }
 
   // Lookup the table.
-  TableInfoPtr first_table = FindPtrOrNull(*catalog_manager_->table_ids_map_, metadata.table_id());
+  TableInfoPtr first_table = catalog_manager_->tables_->FindTableOrNull(metadata.table_id());
 
   // TODO: We need to properly remove deleted tablets.  This can happen async of master loading.
   if (!first_table) {
@@ -212,7 +212,7 @@ Status TabletLoader::Visit(const TabletId& tablet_id, const SysTabletsEntryPB& m
     bool should_delete_tablet = !tablet_deleted;
 
     for (const auto& table_id : table_ids) {
-      TableInfoPtr table = FindPtrOrNull(*catalog_manager_->table_ids_map_, table_id);
+      TableInfoPtr table = catalog_manager_->tables_->FindTableOrNull(table_id);
 
       if (table == nullptr) {
         // If the table is missing and the tablet is in "preparing" state
@@ -226,9 +226,8 @@ Status TabletLoader::Visit(const TabletId& tablet_id, const SysTabletsEntryPB& m
         }
 
         // Otherwise, something is wrong...
-        LOG(WARNING) << "Missing table " << table_id << " required by tablet " << tablet_id
-                     << ", metadata: " << metadata.DebugString()
-                     << ", tables: " << yb::ToString(*catalog_manager_->table_ids_map_);
+        LOG(WARNING) << Format("Missing table $0 required by tablet $1, metadata: $2",
+                               table_id, tablet_id, metadata.DebugString());
         // If we ignore deleted tables, then a missing table can be expected and we continue.
         if (PREDICT_TRUE(FLAGS_master_ignore_deleted_on_load)) {
           continue;

src/yb/master/catalog_manager-test_base.h
@@ -58,8 +58,9 @@ inline scoped_refptr<TabletInfo> CreateTablet(
   return tablet;
 }
 
-void CreateTable(const std::vector<std::string> split_keys, const int num_replicas, bool setup_placement,
-                 TableInfo* table, std::vector<scoped_refptr<TabletInfo>>* tablets) {
+void CreateTable(
+    const std::vector<std::string> split_keys, const int num_replicas, bool setup_placement,
+    TableInfo* table, std::vector<scoped_refptr<TabletInfo>>* tablets) {
   const size_t kNumSplits = split_keys.size();
   for (size_t i = 0; i <= kNumSplits; i++) {
     const std::string& start_key = (i == 0) ? "" : split_keys[i - 1];
@@ -160,12 +161,12 @@ class TestLoadBalancerBase {
         ts_descs_(cb->ts_descs_),
         affinitized_zones_(cb->affinitized_zones_),
         tablet_map_(cb->tablet_map_),
-        table_map_(cb->table_map_),
+        tables_(cb->tables_),
         replication_info_(cb->replication_info_),
         pending_add_replica_tasks_(cb->pending_add_replica_tasks_),
         pending_remove_replica_tasks_(cb->pending_remove_replica_tasks_),
         pending_stepdown_leader_tasks_(cb->pending_stepdown_leader_tasks_) {
-    scoped_refptr<TableInfo> table(new TableInfo(table_id));
+    scoped_refptr<TableInfo> table(new TableInfo(table_id, /* colocated */ false));
     std::vector<scoped_refptr<TabletInfo>> tablets;
 
     // Generate 12 tablets total: 4 splits and 3 replicas each.
@@ -176,9 +177,8 @@ class TestLoadBalancerBase {
     CreateTable(splits, num_replicas, false, table.get(), &tablets);
 
     tablets_ = std::move(tablets);
-
-    table_map_[table_id] = table;
     cur_table_uuid_ = table_id;
+    tables_.AddTable(std::move(table));
   }
 
   void TestAlgorithm() {
@@ -247,9 +247,10 @@ class TestLoadBalancerBase {
     cb_->GetAllReportedDescriptors(&cb_->global_state_->ts_descs_);
     cb_->SetBlacklistAndPendingDeleteTS();
 
+    const auto& table = tables_.FindTableOrNull(cur_table_uuid_);
     const auto& replication_info =
-        VERIFY_RESULT(cb_->GetTableReplicationInfo(table_map_[cur_table_uuid_]));
-    RETURN_NOT_OK(cb_->PopulateReplicationInfo(table_map_[cur_table_uuid_], replication_info));
+        VERIFY_RESULT(cb_->GetTableReplicationInfo(table));
+    RETURN_NOT_OK(cb_->PopulateReplicationInfo(table, replication_info));
 
     cb_->InitializeTSDescriptors();
     return cb_->AnalyzeTabletsUnlocked(cur_table_uuid_);
@@ -1120,7 +1121,7 @@ class TestLoadBalancerBase {
   TSDescriptorVector& ts_descs_;
   std::vector<AffinitizedZonesSet>& affinitized_zones_;
   TabletInfoMap& tablet_map_;
-  TableInfoMap& table_map_;
+  TableIndex& tables_;
   ReplicationInfoPB& replication_info_;
   std::vector<TabletId>& pending_add_replica_tasks_;
   std::vector<TabletId>& pending_remove_replica_tasks_;

src/yb/master/table_index.cc
@@ -1,0 +1,57 @@
+// Copyright (c) YugaByte, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+// in compliance with the License.  You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software distributed under the License
+// is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+// or implied.  See the License for the specific language governing permissions and limitations
+// under the License.
+//
+
+#include "yb/master/table_index.h"
+
+#include "yb/gutil/map-util.h"
+#include "yb/master/catalog_entity_info.h"
+
+namespace yb {
+namespace master {
+
+scoped_refptr<TableInfo> TableIndex::FindTableOrNull(const TableId& id) const {
+  auto result = tables_.find(id);
+  if (result == tables_.end()) {
+    return nullptr;
+  }
+  return *result;
+}
+
+TableIndex::TablesRange TableIndex::GetAllTables() const {
+  return boost::make_iterator_range(
+      tables_.get<PrimaryTableTag>().begin(), tables_.get<PrimaryTableTag>().end());
+}
+
+TableIndex::TablesRange TableIndex::GetPrimaryTables() const {
+  auto range = tables_.get<PrimaryTableTag>().equal_range(false);
+  return boost::make_iterator_range(range.first, range.second);
+}
+
+void TableIndex::Clear() {
+  tables_.clear();
+}
+
+void TableIndex::AddTable(scoped_refptr<TableInfo> table) {
+  tables_.emplace(std::move(table));
+}
+
+size_t TableIndex::Erase(const TableId& id) {
+  return tables_.erase(id);
+}
+
+size_t TableIndex::Size() const {
+  return tables_.size();
+}
+
+}  // namespace master
+}  // namespace yb

src/yb/master/table_index.h
@@ -1,0 +1,73 @@
+// Copyright (c) YugaByte, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+// in compliance with the License.  You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software distributed under the License
+// is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+// or implied.  See the License for the specific language governing permissions and limitations
+// under the License.
+//
+
+#pragma once
+
+#include <boost/multi_index/global_fun.hpp>
+#include <boost/multi_index/hashed_index.hpp>
+#include <boost/multi_index/mem_fun.hpp>
+#include <boost/multi_index/member.hpp>
+#include <boost/multi_index_container.hpp>
+
+#include "yb/master/catalog_entity_info.h"
+#include "yb/master/master_fwd.h"
+#include "yb/util/version_tracker.h"
+
+namespace yb {
+namespace master {
+
+class TableIndex {
+ public:
+  // TODO(zdrudi): add additional indices.
+  // the catalog manager has a pattern of looping through:
+  //     * transaction tables (done through an additional set functioning as an index)
+  //     * non system tables
+  class PrimaryTableTag;
+  // Use a boost::multi_index_container to speed up common operations on tables by adding additional
+  // indices. Indices:
+  //     Primary index on TableId for point lookups.
+  //     Secondary index on IsColocatedUserTable to speed up the tablet split manager and load
+  //     balancer for clusters with many colocated tables.
+  using Tables = boost::multi_index_container<
+    TableInfoPtr,
+    boost::multi_index::indexed_by<
+      boost::multi_index::hashed_unique<
+        boost::multi_index::const_mem_fun<TableInfo, const TableId&, &TableInfo::id>>,
+      boost::multi_index::hashed_non_unique<
+        boost::multi_index::tag<PrimaryTableTag>,
+        boost::multi_index::const_mem_fun<TableInfo, bool, &TableInfo::IsColocatedUserTable>>>>;
+
+  using TablesRange = boost::iterator_range<Tables::nth_index<1>::type::iterator>;
+
+  TableIndex() = default;
+  ~TableIndex() = default;
+  scoped_refptr<TableInfo> FindTableOrNull(const TableId& id) const;
+
+  TablesRange GetAllTables() const;
+  TablesRange GetPrimaryTables() const;
+
+  void AddTable(scoped_refptr<TableInfo> table);
+
+  void Clear();
+
+  size_t Erase(const TableId& id);
+
+  size_t Size() const;
+
+ private:
+  class Impl;
+  Tables tables_;
+};
+
+}  // namespace master
+}  // namespace yb

src/yb/master/table_index-test.cc
@@ -1,0 +1,79 @@
+// Copyright (c) YugaByte, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+// in compliance with the License.  You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software distributed under the License
+// is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+// or implied.  See the License for the specific language governing permissions and limitations
+// under the License.
+//
+
+#include "yb/master/table_index.h"
+
+#include <format>
+
+#include "gmock/gmock.h"
+#include "yb/master/master_util.h"
+#include <gtest/gtest.h>
+
+namespace tt = testing;
+
+namespace yb {
+namespace master {
+
+TableInfoPtr CreateTable(const TableId& table_id, bool colocated = false) {
+  auto table = scoped_refptr<TableInfo>(new TableInfo(table_id, colocated));
+  if (colocated) {
+    auto mut = table->mutable_metadata();
+    mut->StartMutation();
+    mut->mutable_dirty()->pb.set_colocated(true);
+    mut->CommitMutation();
+  }
+  return table;
+}
+
+TableInfoPtr CreateParentTablegroupTable() {
+  const TableId id = std::string(std::string(31, '0') + '1' + kColocatedDbParentTableIdSuffix);
+  return CreateTable(id, true);
+}
+
+TEST(TableIndexTest, PointLookup) {
+  const auto id = "t1";
+  const auto missing_id = "t2";
+  auto table = CreateTable(id);
+  auto table_index = TableIndex();
+  table_index.AddTable(table);
+  auto result = table_index.FindTableOrNull(id);
+  EXPECT_THAT(table_index.FindTableOrNull(id), tt::Eq(table));
+  EXPECT_THAT(table_index.FindTableOrNull(missing_id), tt::IsNull());
+}
+
+TEST(TableIndexTest, GetAllTables) {
+  std::vector<TableInfoPtr> tables;
+  std::vector<TableId> ids = {"t1", "t2", "t3"};
+  auto table_index = TableIndex();
+  for (const auto& id : ids) {
+    auto table = CreateTable(id);
+    tables.push_back(table);
+    table_index.AddTable(std::move(table));
+  }
+  EXPECT_THAT(table_index.GetAllTables(), tt::UnorderedElementsAreArray(tables));
+}
+
+TEST(TableIndexTest, GetPrimaryTables) {
+  auto colocated_table = CreateTable("colocated", true);
+  auto parent_table = CreateParentTablegroupTable();
+  ASSERT_TRUE(parent_table->IsColocationParentTable());
+  auto regular_table = CreateTable("regular");
+  TableIndex tables;
+  tables.AddTable(colocated_table);
+  tables.AddTable(parent_table);
+  tables.AddTable(regular_table);
+  EXPECT_THAT(tables.GetPrimaryTables(), tt::UnorderedElementsAre(parent_table, regular_table));
+}
+
+}  // namespace master
+}  // namespace yb

ent/src/yb/integration-tests/cdcsdk_ysql-test.cc
@@ -13,7 +13,9 @@
 #include <algorithm>
 #include <chrono>
 #include <utility>
+
 #include <boost/assign.hpp>
+#include <gmock/gmock.h>
 #include <gtest/gtest.h>
 
 #include "yb/cdc/cdc_service.h"
@@ -8373,16 +8375,20 @@ TEST_F(CDCSDKYsqlTest, YB_DISABLE_TEST_IN_TSAN(TestAddColocatedTableToNamespaceW
   CheckTabletsInCDCStateTable(expected_tablet_ids, test_client());
 
   // Wait for a background task cycle to complete.
+  std::sort(expected_table_ids.begin(), expected_table_ids.end());
   auto result = WaitFor(
       [&]() -> Result<bool> {
-        return VERIFY_RESULT(GetCDCStreamTableIds(stream_id)) == expected_table_ids;
+        auto actual_table_ids = VERIFY_RESULT(GetCDCStreamTableIds(stream_id));
+        std::sort(actual_table_ids.begin(), actual_table_ids.end());
+        return actual_table_ids == expected_table_ids;
       },
       MonoDelta::FromSeconds(10) * kTimeMultiplier,
       "Waiting for background task to update cdc streams.");
   EXPECT_OK(result);
   // Extra ASSERT here to get nicely formatted debug information in case of failure.
   if (!result.ok()) {
-    ASSERT_EQ(ASSERT_RESULT(GetCDCStreamTableIds(stream_id)), expected_table_ids);
+    EXPECT_THAT(ASSERT_RESULT(GetCDCStreamTableIds(stream_id)),
+                testing::UnorderedElementsAreArray(expected_table_ids));
   }
 }
 
@@ -8438,8 +8444,12 @@ TEST_F(CDCSDKYsqlTest, YB_DISABLE_TEST_IN_TSAN(TestAddTableToNamespaceWithMultip
   CheckTabletsInCDCStateTable(expected_tablet_ids, test_client(), stream_id_1);
 
   // Check that both the streams metadata has all the 3 table ids.
-  ASSERT_EQ(ASSERT_RESULT(GetCDCStreamTableIds(stream_id)), expected_table_ids);
-  ASSERT_EQ(ASSERT_RESULT(GetCDCStreamTableIds(stream_id_1)), expected_table_ids);
+  ASSERT_THAT(
+      ASSERT_RESULT(GetCDCStreamTableIds(stream_id)),
+      testing::UnorderedElementsAreArray(expected_table_ids));
+  ASSERT_THAT(
+      ASSERT_RESULT(GetCDCStreamTableIds(stream_id_1)),
+      testing::UnorderedElementsAreArray(expected_table_ids));
 }
 
 TEST_F(
@@ -8504,9 +8514,15 @@ TEST_F(
   CheckTabletsInCDCStateTable(expected_tablet_ids, test_client(), stream_id_2);
 
   // Check that both the streams metadata has all the 3 table ids.
-  ASSERT_EQ(ASSERT_RESULT(GetCDCStreamTableIds(stream_id)), expected_table_ids);
-  ASSERT_EQ(ASSERT_RESULT(GetCDCStreamTableIds(stream_id_1)), expected_table_ids);
-  ASSERT_EQ(ASSERT_RESULT(GetCDCStreamTableIds(stream_id_2)), expected_table_ids);
+  ASSERT_THAT(
+      ASSERT_RESULT(GetCDCStreamTableIds(stream_id)),
+      testing::UnorderedElementsAreArray(expected_table_ids));
+  ASSERT_THAT(
+      ASSERT_RESULT(GetCDCStreamTableIds(stream_id_1)),
+      testing::UnorderedElementsAreArray(expected_table_ids));
+  ASSERT_THAT(
+      ASSERT_RESULT(GetCDCStreamTableIds(stream_id_2)),
+      testing::UnorderedElementsAreArray(expected_table_ids));
 }
 
 TEST_F(CDCSDKYsqlTest, YB_DISABLE_TEST_IN_TSAN(TestAddMultipleTableToNamespaceWithActiveStream)) {

commit c613c46b52234d6287b7d0ca47db490b2615e9cc

[#15216] docdb: Overwrite existing mappings when adding new tables to TableIndex.
Summary: b45efd3ca087bf4c9bf34c70ce1b0e52f27ccbca broke a unit test creates a system table after failure. The behaviour before b45efd3ca087bf4c9bf34c70ce1b0e52f27ccbca would replace any existing mapping in the table_ids_map with a new entry by using the subscripting syntax with c++ std::map. b45efd3ca087bf4c9bf34c70ce1b0e52f27ccbca instead refused to modify existing mappings when given a new table with the same id. This diff restores the behaviour to how it was before.

Test Plan:
```
yb_build.sh --java-test 'org.yb.pgsql.TestYsqlUpgrade#creatingSystemRelsAfterFailure'
ybd --cxx-test  table_index-test
```

Reviewers: bogdan, skedia, msun, asrivastava

Reviewed By: asrivastava

Subscribers: myang, slingam, ybase, bogdan

Differential Revision: https://phabricator.dev.yugabyte.com/D21544

src/yb/master/catalog_loaders.cc
@@ -99,7 +99,7 @@ Status TableLoader::Visit(const TableId& table_id, const SysTablesEntryPB& metad
   // Add the table to the IDs map and to the name map (if the table is not deleted). Do not
   // add Postgres tables to the name map as the table name is not unique in a namespace.
   auto table_map_checkout = catalog_manager_->tables_.CheckOut();
-  table_map_checkout->AddTable(table);
+  table_map_checkout->AddOrReplace(table);
   if (!l->started_deleting() && !l->started_hiding()) {
     if (l->table_type() != PGSQL_TABLE_TYPE) {
       catalog_manager_->table_names_map_[{l->namespace_id(), l->name()}] = table;

src/yb/master/catalog_manager-test_base.h
@@ -178,7 +178,7 @@ class TestLoadBalancerBase {
 
     tablets_ = std::move(tablets);
     cur_table_uuid_ = table_id;
-    tables_.AddTable(std::move(table));
+    tables_.AddOrReplace(table);
   }
 
   void TestAlgorithm() {

src/yb/master/catalog_manager.cc
@@ -1617,7 +1617,7 @@ Status CatalogManager::PrepareSysCatalogTable(int64_t term) {
     metadata.set_version(0);
 
     auto table_map_checkout = tables_.CheckOut();
-    table_map_checkout->AddTable(table);
+    table_map_checkout->AddOrReplace(table);
     sys_catalog_table = table;
     table_names_map_[{kSystemSchemaNamespaceId, kSysCatalogTableName}] = table;
     table->set_is_system();
@@ -4360,7 +4360,7 @@ Status CatalogManager::CreateTableInMemory(const CreateTableRequestPB& req,
       << "Table: " << (**table).ToString() << ", create_tablets: " << (tablets ? "YES" : "NO");
 
   auto table_map_checkout = tables_.CheckOut();
-  table_map_checkout->AddTable(*table);
+  table_map_checkout->AddOrReplace(*table);
   // Do not add Postgres tables to the name map as the table name is not unique in a namespace.
   if (req.table_type() != PGSQL_TABLE_TYPE) {
     table_names_map_[{namespace_id, req.name()}] = *table;

src/yb/master/table_index.cc
@@ -41,8 +41,17 @@ void TableIndex::Clear() {
   tables_.clear();
 }
 
-void TableIndex::AddTable(scoped_refptr<TableInfo> table) {
-  tables_.emplace(std::move(table));
+void TableIndex::AddOrReplace(const scoped_refptr<TableInfo>& table) {
+  auto [pos, insertion_successful] = tables_.insert(table);
+  if (!insertion_successful) {
+    std::string first_id = (*pos)->id();
+    auto replace_successful = tables_.replace(pos, table);
+    if (!replace_successful) {
+      LOG(ERROR) << Format(
+          "Multiple tables prevented inserting a new table with id $0. First id was $1",
+          table->id(), first_id);
+    }
+  }
 }
 
 size_t TableIndex::Erase(const TableId& id) {

src/yb/master/table_index.h
@@ -56,7 +56,7 @@ class TableIndex {
   TablesRange GetAllTables() const;
   TablesRange GetPrimaryTables() const;
 
-  void AddTable(scoped_refptr<TableInfo> table);
+  void AddOrReplace(const scoped_refptr<TableInfo>& table);
 
   void Clear();
 

src/yb/master/table_index-test.cc
@@ -43,7 +43,7 @@ TEST(TableIndexTest, PointLookup) {
   const auto missing_id = "t2";
   auto table = CreateTable(id);
   auto table_index = TableIndex();
-  table_index.AddTable(table);
+  table_index.AddOrReplace(table);
   auto result = table_index.FindTableOrNull(id);
   EXPECT_THAT(table_index.FindTableOrNull(id), tt::Eq(table));
   EXPECT_THAT(table_index.FindTableOrNull(missing_id), tt::IsNull());
@@ -56,7 +56,7 @@ TEST(TableIndexTest, GetAllTables) {
   for (const auto& id : ids) {
     auto table = CreateTable(id);
     tables.push_back(table);
-    table_index.AddTable(std::move(table));
+    table_index.AddOrReplace(table);
   }
   EXPECT_THAT(table_index.GetAllTables(), tt::UnorderedElementsAreArray(tables));
 }
@@ -67,10 +67,24 @@ TEST(TableIndexTest, GetPrimaryTables) {
   ASSERT_TRUE(parent_table->IsColocationParentTable());
   auto regular_table = CreateTable("regular");
   TableIndex tables;
-  tables.AddTable(colocated_table);
-  tables.AddTable(parent_table);
-  tables.AddTable(regular_table);
+  tables.AddOrReplace(colocated_table);
+  tables.AddOrReplace(parent_table);
+  tables.AddOrReplace(regular_table);
   EXPECT_THAT(tables.GetPrimaryTables(), tt::UnorderedElementsAre(parent_table, regular_table));
+}
+
+// Existing system behaviour relies on the table data structure in the catalog manager doing
+// silent replacements of existing entries, at least for system tables. See GH issue
+// https://github.com/yugabyte/yugabyte-db/issues/15216
+TEST(TableIndexTest, ReplaceExistingEntry) {
+  auto id = "t";
+  auto regular_table = CreateTable(id, /* colocated */ false);
+  auto colocated_table = CreateTable(id, /* colocated */ true);
+  TableIndex tables;
+  tables.AddOrReplace(regular_table);
+  EXPECT_THAT(tables.FindTableOrNull(id), tt::Eq(regular_table));
+  tables.AddOrReplace(colocated_table);
+  EXPECT_THAT(tables.FindTableOrNull(id), tt::Eq(colocated_table));
 }
 
 }  // namespace master