c6fcf118c49e8eb0264ddec3972cd15729b4ec12
kkannan <kkannan@yugabyte.com>
2022-12-15 14:49:37 +0530

    [PLAT-6460][Platform]Change the UI check during restore of YCQl tables backup to only check for non existence of the specific tables listed instead of forcing users to always restore to a non existent namespace.
    Summary:
    there was a request from the customer that they want to restore a ycql backup to db with same keyspace name. They already have the table deleted in the db. so,there won't be any table name conflict.
    
    Since, we do keyspace name check, we won't allow restoring backup with keyspace name that already exists in the db.
    
    These checks are valid only for single YCQL keyspace restore, not for "Restore Entire Backup" and not for 'YSQL'.
    
    For, restore entire backup , we follow the same procedure as before.
    
    For more info: see this [[ https://yugabyte.atlassian.net/browse/PLAT-6460 | ticket ]]
    
    Test Plan: Tested by restoring ycql backup, ysql backup etc. tested all the flows
    
    Reviewers: asathyan, lsangappa, jmak, rmadhavan
    
    Reviewed By: rmadhavan
    
    Subscribers: jenkins-bot, ui, yugaware
    
    Differential Revision: https://phabricator.dev.yugabyte.com/D21751

--- managed/ui/src/components/backupv2/components/BackupAdvancedRestore.tsx
+++ managed/ui/src/components/backupv2/components/BackupAdvancedRestore.tsx
@@ -388,10 +388,10 @@ function RestoreForm({
                   values['api_type'].value === BACKUP_API_TYPES.YSQL ? 'Database' : 'Keyspace'
                 } name`}
                 validate={(name: string) => {
-                  // Restoring with duplicate keyspace name is supported in redis
+                  // Restoring with duplicate keyspace name is supported in redis and YCQL
                   if (
                     Array.isArray(tablesInUniverse) &&
-                    values['api_type'].value !== BACKUP_API_TYPES.YEDIS &&
+                    values['api_type'].value === BACKUP_API_TYPES.YSQL &&
                     find(tablesInUniverse, { tableType: values['api_type'].value, keySpace: name })
                   ) {
                     setFieldValue('should_rename_keyspace', true, false);
