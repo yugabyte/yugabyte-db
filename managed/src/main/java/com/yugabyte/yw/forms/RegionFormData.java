31e3f8f3b1b83b52b7b9e8c69e99ac709b89fb1c
Artem Mindrov <amindrov@yugabyte.com>
2022-12-26 09:13:13 +0000

    [Platform][CLOUDGA-9644] edit region/AZ endpoints + hard delete region/AZ
    Summary:
    We don't have a way to modify SG/subnet data for existing regions and AZs, and there is no option to recreate them since the delete endpoint only marks them inactive, while the edit provider endpoint skips modifying already existing regions.
    
    This diff adds the ability to modify some of the region/AZ attributes, as well as delete them if they don't host any universe nodes.
    
    Test Plan:
    1. Create a universe, try to delete any AZ/region hosting its nodes. Expected: 403.
    2. Try to delete a zone/region that doesn't have any nodes.
    3. Modify both SG and image of a region, expected response: changed region.
    4. Modify only the SG, expected: ybImage doesn't change or reset.
    5. Explicitly set the SG to null in the request body, verify that the SG gets reset to null.
    6. Repeat 5,6 for ybImage.
    7. Modify an AZ's subnets, expected response: changed AZ.
    8. Modify only the primary subnet, expected: secondary subnet neither changed nor reset.
    9. Set subnet to null, verify that the subnet is reset.
    10. Repeat 8,9 for the secondary subnet.
    
    Reviewers: arnav, sanketh, sb-yb
    
    Reviewed By: sb-yb
    
    Subscribers: jmak, jenkins-bot, yugaware
    
    Differential Revision: https://phabricator.dev.yugabyte.com/D20331

--- managed/src/main/java/com/yugabyte/yw/forms/RegionFormData.java
+++ managed/src/main/java/com/yugabyte/yw/forms/RegionFormData.java
@@ -7,16 +7,13 @@ import play.data.validation.Constraints;
 /**
  * This class will be used by the API and UI Form Elements to validate constraints for Region Data
  */
-public class RegionFormData {
+public class RegionFormData extends RegionEditFormData {
   @Constraints.Required()
   @Constraints.MaxLength(25)
   public String code;
 
   @Constraints.MaxLength(100)
   public String name;
-
-  @Constraints.MaxLength(255)
-  public String ybImage;
 
   public String hostVpcRegion;
 
