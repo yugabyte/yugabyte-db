yugabyte/yugabyte-db

commit a245c1191e7fa7a097c4fa2144ce090eb6d179fb

[#15051] DocDB: Retain schema packing for non flushed entries
Summary:
After compaction we perform schema packing GC, to remove outdated schema packings.
All SST files are iterated and minimal used schema packing is taken.
Then all schema packing before this one are removed.
But this logic ignores non flushed entries, so could remove schema packing that is actually in use.

Fixed by taking such entries into account.

Also fix DocReadContext log prefix when TableInfo is loaded from protobuf.

Test Plan: CqlPackedRowTest.RetainPacking

Reviewers: rthallam, bogdan

Reviewed By: bogdan

Subscribers: ybase

Differential Revision: https://phabricator.dev.yugabyte.com/D21726

src/yb/tablet/tablet_metadata.cc
@@ -111,8 +111,9 @@ const std::string kSnapshotsDirSuffix = ".snapshots";
 //  Raft group metadata
 // ============================================================================
 
-TableInfo::TableInfo()
-    : doc_read_context(new docdb::DocReadContext(log_prefix)),
+TableInfo::TableInfo(const std::string& log_prefix_, PrivateTag)
+    : log_prefix(log_prefix_),
+      doc_read_context(new docdb::DocReadContext(log_prefix)),
       index_map(std::make_unique<IndexMap>()) {
 }
 
@@ -181,14 +182,21 @@ TableInfo::TableInfo(const TableInfo& other, SchemaVersion min_schema_version)
 
 TableInfo::~TableInfo() = default;
 
-Status TableInfo::LoadFromPB(
+Result<TableInfoPtr> TableInfo::LoadFromPB(
     const std::string& tablet_log_prefix, const TableId& primary_table_id, const TableInfoPB& pb) {
+  Primary primary(primary_table_id == pb.table_id());
+  auto log_prefix = MakeTableInfoLogPrefix(tablet_log_prefix, primary, pb.table_id());
+  auto result = std::make_shared<TableInfo>(log_prefix, PrivateTag());
+  RETURN_NOT_OK(result->DoLoadFromPB(primary, pb));
+  return result;
+}
+
+Status TableInfo::DoLoadFromPB(Primary primary, const TableInfoPB& pb) {
   table_id = pb.table_id();
   namespace_name = pb.namespace_name();
   namespace_id = pb.namespace_id();
   table_name = pb.table_name();
   table_type = pb.table_type();
-  Primary primary(primary_table_id == table_id);
   cotable_id = VERIFY_RESULT(ParseCotableId(primary, table_id));
 
   RETURN_NOT_OK(doc_read_context->LoadFromPB(pb));
@@ -209,8 +217,6 @@ Status TableInfo::LoadFromPB(
     RETURN_NOT_OK(DeletedColumn::FromPB(deleted_col, &col));
     deleted_cols.push_back(col);
   }
-
-  log_prefix = MakeTableInfoLogPrefix(tablet_log_prefix, primary, table_id);
 
   return Status::OK();
 }
@@ -319,14 +325,12 @@ Status KvStoreInfo::LoadTablesFromPB(
     const google::protobuf::RepeatedPtrField<TableInfoPB>& pbs, const TableId& primary_table_id) {
   tables.clear();
   for (const auto& table_pb : pbs) {
-    const TableId table_id = table_pb.table_id();
-    TableInfoPtr& table_info =
-        tables.emplace(table_id, std::make_shared<TableInfo>()).first->second;
-
-    RETURN_NOT_OK(table_info->LoadFromPB(tablet_log_prefix, primary_table_id, table_pb));
+    TableInfoPtr table_info = VERIFY_RESULT(TableInfo::LoadFromPB(
+        tablet_log_prefix, primary_table_id, table_pb));
+    tables.emplace(table_info->table_id, table_info);
 
     const Schema& schema = table_info->schema();
-    if (table_id != primary_table_id) {
+    if (table_info->primary()) {
       if (schema.table_properties().is_ysql_catalog_table()) {
         // TODO(#79): when adding for multiple KV-stores per Raft group support - check if we need
         // to set cotable ID.

src/yb/integration-tests/cql-packed-row-test.cc
@@ -16,6 +16,8 @@
 
 using namespace std::literals;
 
+DECLARE_int32(timestamp_history_retention_interval_sec);
+
 namespace yb {
 
 class CqlPackedRowTest : public PackedRowTestBase<CqlTestBase<MiniCluster>> {
@@ -166,6 +168,32 @@ TEST_F(CqlPackedRowTest, WriteTime) {
       "SELECT writetime(v1), writetime(v2) FROM t", processor));
   ASSERT_EQ(old_v1time, v1time);
   ASSERT_EQ(old_v2time, v2time);
+}
+
+TEST_F(CqlPackedRowTest, RetainPacking) {
+  // Set retention interval to 0, to repack all recently flushed entries.
+  FLAGS_timestamp_history_retention_interval_sec = 0;
+
+  auto session = ASSERT_RESULT(EstablishSession(driver_.get()));
+
+  ASSERT_OK(session.ExecuteQuery(
+      "CREATE TABLE t (key INT PRIMARY KEY, v1 INT) WITH tablets = 1"));
+
+  ASSERT_OK(session.ExecuteQuery("INSERT INTO t (key, v1) VALUES (1, 1)"));
+  ASSERT_OK(cluster_->FlushTablets());
+
+  ASSERT_OK(session.ExecuteQuery("INSERT INTO t (key, v1) VALUES (2, 2)"));
+  ASSERT_OK(cluster_->FlushTablets());
+
+  ASSERT_OK(session.ExecuteQuery("ALTER TABLE t ADD v2 INT"));
+  ASSERT_OK(session.ExecuteQuery("INSERT INTO t (key, v1, v2) VALUES (3, 3, 3)"));
+
+  // The first compaction to compact all flushed entries with an old schema, so all disk entries
+  // have only the most recent schema version.
+  ASSERT_OK(cluster_->CompactTablets(docdb::SkipFlush::kTrue));
+
+  // Compaction with flush to check that we did not lost used schema versions.
+  ASSERT_OK(cluster_->CompactTablets());
 }
 
 } // namespace yb

src/yb/tablet/tablet.cc
@@ -375,7 +375,16 @@ class Tablet::RegularRocksDbListener : public rocksdb::EventListener {
       }
       ERROR_NOT_OK(metadata.Flush(), log_prefix_);
     }
+
+    // Collect min schema version from all DB entries. I.e. stored in memory and flushed to disk.
     std::unordered_map<Uuid, SchemaVersion, UuidHash> table_id_to_min_schema_version;
+    {
+      auto smallest = db->CalcMemTableFrontier(rocksdb::UpdateUserValueType::kSmallest);
+      if (smallest) {
+        down_cast<docdb::ConsensusFrontier&>(*smallest).MakeExternalSchemaVersionsAtMost(
+            &table_id_to_min_schema_version);
+      }
+    }
     for (const auto& file : db->GetLiveFilesMetaData()) {
       if (!file.smallest.user_frontier) {
         continue;

src/yb/tablet/tablet_metadata.h
@@ -78,6 +78,9 @@ const uint64_t kNoLastFullCompactionTime = HybridTime::kMin.ToUint64();
 YB_STRONGLY_TYPED_BOOL(Primary);
 
 struct TableInfo {
+ private:
+  class PrivateTag {};
+ public:
   // Table id, name and type.
   std::string table_id;
   std::string namespace_name;
@@ -107,7 +110,8 @@ struct TableInfo {
   // We use the retention time from the primary table.
   uint32_t wal_retention_secs = 0;
 
-  TableInfo();
+  // Public ctor with private argument to allow std::make_shared, but prevent public usage.
+  TableInfo(const std::string& log_prefix, PrivateTag);
   TableInfo(const std::string& tablet_log_prefix,
             Primary primary,
             std::string table_id,
@@ -127,7 +131,7 @@ struct TableInfo {
   TableInfo(const TableInfo& other, SchemaVersion min_schema_version);
   ~TableInfo();
 
-  Status LoadFromPB(
+  static Result<TableInfoPtr> LoadFromPB(
       const std::string& tablet_log_prefix, const TableId& primary_table_id, const TableInfoPB& pb);
   void ToPB(TableInfoPB* pb) const;
 
@@ -149,8 +153,14 @@ struct TableInfo {
     return log_prefix;
   }
 
+  bool primary() const {
+    return cotable_id.IsNil();
+  }
+
   // Should account for every field in TableInfo.
   static bool TEST_Equals(const TableInfo& lhs, const TableInfo& rhs);
+ private:
+  Status DoLoadFromPB(Primary primary, const TableInfoPB& pb);
 };
 
 // Describes KV-store. Single KV-store is backed by one or two RocksDB instances, depending on

commit 76106cda00283b037e0093ce67dd731c46dfc062

[#15443] DocDB: Fix setting cotable id for non primary ysql catalog tables
Summary:
There is a regression in a245c1191e7fa7a097c4fa2144ce090eb6d179fb/D21726.
That breaks initializing cotable id for table info in ysql catalog tables.

Fixed by adjusting check.

Test Plan: Jenkins

Reviewers: bogdan, rthallam

Reviewed By: rthallam

Subscribers: ybase

Differential Revision: https://phabricator.dev.yugabyte.com/D22220

src/yb/tablet/tablet_metadata.cc
@@ -330,12 +330,10 @@ Status KvStoreInfo::LoadTablesFromPB(
     tables.emplace(table_info->table_id, table_info);
 
     const Schema& schema = table_info->schema();
-    if (table_info->primary()) {
-      if (schema.table_properties().is_ysql_catalog_table()) {
-        // TODO(#79): when adding for multiple KV-stores per Raft group support - check if we need
-        // to set cotable ID.
-        table_info->doc_read_context->schema.set_cotable_id(table_info->cotable_id);
-      }
+    if (!table_info->primary() && schema.table_properties().is_ysql_catalog_table()) {
+      // TODO(#79): when adding for multiple KV-stores per Raft group support - check if we need
+      // to set cotable ID.
+      table_info->doc_read_context->schema.set_cotable_id(table_info->cotable_id);
     }
     if (schema.has_colocation_id()) {
       colocation_to_table.emplace(schema.colocation_id(), table_info);