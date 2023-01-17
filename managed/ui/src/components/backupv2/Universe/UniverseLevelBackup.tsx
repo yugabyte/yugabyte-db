498a7386b9096d965369e110a4edf5c16815d700
Athul Sathyan <44346129+athulsn@users.noreply.github.com>
2023-01-17 10:56:53 +0530

    [Backport 2.16][PLAT-6992][PITR][UI]Show PITR tab from software version 2.14 onwards
    Summary: Show PITR tab if software version from 2.14 onwards
    
    Test Plan: Tested manually
    
    Reviewers: kkannan, vpatibandla, lsangappa
    
    Reviewed By: lsangappa
    
    Subscribers: jenkins-bot, ui, yugaware
    
    Differential Revision: https://phabricator.dev.yugabyte.com/D22281

--- managed/ui/src/components/backupv2/Universe/UniverseLevelBackup.tsx
+++ managed/ui/src/components/backupv2/Universe/UniverseLevelBackup.tsx
@@ -15,7 +15,7 @@ import { withRouter } from 'react-router';
 import { ScheduledBackup } from '../scheduled/ScheduledBackup';
 import { useSelector } from 'react-redux';
 import { PointInTimeRecovery } from '../pitr/PointInTimeRecovery';
-import { isYbcInstalledInUniverse } from '../../../utils/UniverseUtils';
+import { isYbcInstalledInUniverse, getPrimaryCluster } from '../../../utils/UniverseUtils';
 import { BackupThrottleParameters } from '../components/BackupThrottleParameters';
 import { BackupAdvancedRestore } from '../components/BackupAdvancedRestore';
 import './UniverseLevelBackup.scss';
@@ -27,9 +27,23 @@ interface UniverseBackupProps {
   };
 }
 
+const isPITRSupported = (version: string): boolean => {
+  //PITR is supported from 2.14
+  const [major, minor] = version.split('.');
+  return parseInt(major, 10) > 2 || (parseInt(major, 10) === 2 && parseInt(minor, 10) >= 14);
+};
+
 const UniverseBackup: FC<UniverseBackupProps> = ({ params: { uuid } }) => {
   const featureFlags = useSelector((state: any) => state.featureFlags);
   const currentUniverse = useSelector((reduxState: any) => reduxState.universe.currentUniverse);
+  const primaryCluster = getPrimaryCluster(currentUniverse.data.universeDetails.clusters);
+  const currentSoftwareVersion = primaryCluster.userIntent.ybSoftwareVersion.split('-')[0];
+
+  //PITR
+  const enablePITR =
+    isPITRSupported(currentSoftwareVersion) &&
+    (featureFlags.test.enablePITR || featureFlags.released.enablePITR);
+
   const YBCInstalled =
     (featureFlags.test.enableYbc || featureFlags.released.enableYbc) &&
     isYbcInstalledInUniverse(currentUniverse.data.universeDetails);
@@ -59,7 +73,7 @@ const UniverseBackup: FC<UniverseBackupProps> = ({ params: { uuid } }) => {
       <Tab eventKey="backupSchedule" title="Scheduled Backup Policies" unmountOnExit>
         <ScheduledBackup universeUUID={uuid} />
       </Tab>
-      {(featureFlags.test.enablePITR || featureFlags.released.enablePITR) && (
+      {enablePITR && (
         <Tab eventKey="point-in-time-recovery" title="Point-in-time Recovery" unmountOnExit>
           <PointInTimeRecovery universeUUID={uuid} />
         </Tab>
