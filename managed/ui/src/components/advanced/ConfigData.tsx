f8c74cf83e2f7ea194a039bce99a9a08e8b588ed
Rajagopalan Madhavan <rmadhavan@yugabyte.com>
2023-01-17 22:57:53 -0500

    [PLAT-6882]: Add runtime config metadata to UI
    Summary:
    1. Add runtime config metadata to UI like help text and display name
    2. For editing configuration, especially for boolean type keys, display a dropdown to select to true or false
    3. Add "mutable_keys_info" as a redux action
    4. Ensure to move all strings to internationalization file
    5. Handle scenarios for cases when there are no Universe or providers or universes
    
    Test Plan:
    Attaching some screenshots
    {F34030}
    
    {F34031}
    
    {F34032}
    
    {F34033}
    
    {F34034}
    
    Reviewers: lsangappa, asathyan, kkannan, jmak
    
    Reviewed By: jmak
    
    Subscribers: jenkins-bot, yugaware
    
    Differential Revision: https://phabricator.dev.yugabyte.com/D22227

--- managed/ui/src/components/advanced/ConfigData.tsx
+++ managed/ui/src/components/advanced/ConfigData.tsx
@@ -1,17 +1,24 @@
-import React, { FC, useState } from 'react';
-import { DropdownButton, MenuItem } from 'react-bootstrap';
+import React, { FC, useEffect, useState } from 'react';
+import { DropdownButton, OverlayTrigger, MenuItem, Tooltip } from 'react-bootstrap';
 import { BootstrapTable, TableHeaderColumn } from 'react-bootstrap-table';
 import { useTranslation } from 'react-i18next';
 import { useSelector } from 'react-redux';
-
 import { YBCheckBox } from '../common/forms/fields';
 import { YBErrorIndicator, YBLoading } from '../common/indicators';
 import { EditConfig } from './EditConfig';
 import { ResetConfig } from './ResetConfig';
+import { RunTimeConfigData } from '../../redesign/helpers/dtos';
 import { getPromiseState } from '../../utils/PromiseUtils';
 import { isNonEmptyArray } from '../../utils/ObjectUtils';
 
 import './AdvancedConfig.scss';
+
+const ConfigScopePriority = {
+  GLOBAL: 1,
+  CUSTOMER: 2,
+  UNIVERSE: 3,
+  PROVIDER: 4
+};
 
 interface GlobalConfigProps {
   setRuntimeConfig: (key: string, value: string, scope?: string) => void;
@@ -32,37 +39,70 @@ export const ConfigData: FC<GlobalConfigProps> = ({
 }) => {
   const { t } = useTranslation();
   const runtimeConfigs = useSelector((state: any) => state.customer.runtimeConfigs);
+  const runtimeConfigsKeyInfo = useSelector((state: any) => state.customer.runtimeConfigsKeyInfo);
   // Helps in deciding if the logged in user can mutate the config values
   const isScopeMutable = runtimeConfigs?.data?.mutableScope;
-  const [editConfig, setEditConfig] = useState(false);
-  const [resetConfig, setResetConfig] = useState(false);
-  const [showOverridenValues, setShowOverridenValues] = useState(false);
-  const [configData, setConfigData] = useState({
+  const [editConfig, setEditConfig] = useState<boolean>(false);
+  const [resetConfig, setResetConfig] = useState<boolean>(false);
+  const [showOverridenValues, setShowOverridenValues] = useState<boolean>(false);
+  const [listItems, setListItems] = useState([{ isConfigInherited: false }]);
+  const [configData, setConfigData] = useState<RunTimeConfigData>({
     configID: 0,
     configKey: '',
     configValue: '',
-    isConfigInherited: true
+    isConfigInherited: true,
+    displayName: '',
+    helpTxt: '',
+    type: '',
+    scope: ''
   });
 
-  if (runtimeConfigs?.data && getPromiseState(runtimeConfigs).isLoading()) {
-    return <YBLoading />;
-  } else if (runtimeConfigs?.error) {
-    return (<YBErrorIndicator
-      customErrorMessage={t('admin.advanced.globalConfig.GlobalConfigReqFailed')} />);
-  }
+  const runtimeConfigEntries = runtimeConfigs?.data?.configEntries;
 
-  const globalConfigEntries = runtimeConfigs?.data?.configEntries;
-  let listItems: Array<any> = [];
-  if (isNonEmptyArray(globalConfigEntries)) {
-    listItems = globalConfigEntries.map(function (entry: any, idx: number) {
-      return {
-        configID: idx + 1,
-        configKey: entry.key,
-        configValue: entry.value,
-        isConfigInherited: entry.inherited
-      };
-    });
-  }
+  useEffect(() => {
+    if (isNonEmptyArray(runtimeConfigEntries)) {
+      const runtimeConfigItems = runtimeConfigEntries
+        .map((entry: any, idx: number) => {
+          return {
+            configID: idx + 1,
+            configKey: entry.key,
+            configValue: entry.value,
+            isConfigInherited: entry.inherited
+          };
+        })
+        ?.filter((item: any) => {
+          return runtimeConfigsKeyInfo?.data?.find((keyInfo: any) => {
+            let isScopeValid = false;
+            if (ConfigScopePriority[scope] === 1) {
+              isScopeValid = true;
+            } else if (
+              ConfigScopePriority[scope] === 2 &&
+              ConfigScopePriority[keyInfo.scope] >= 2
+            ) {
+              isScopeValid = true;
+            } else if (
+              ConfigScopePriority[scope] === 3 &&
+              ConfigScopePriority[keyInfo.scope] === 3
+            ) {
+              isScopeValid = true;
+            } else if (
+              ConfigScopePriority[scope] === 4 &&
+              ConfigScopePriority[keyInfo.scope] === 4
+            ) {
+              isScopeValid = true;
+            }
+            if (keyInfo.key === item.configKey && isScopeValid) {
+              item.displayName = keyInfo.displayName;
+              item.helpTxt = keyInfo.helpTxt;
+              item.type = keyInfo.dataType?.name;
+              item.scope = keyInfo.scope;
+            }
+            return keyInfo.key === item.configKey && isScopeValid;
+          });
+        });
+      setListItems(runtimeConfigItems);
+    }
+  }, [scope, universeUUID, providerUUID, customerUUID, runtimeConfigEntries]); // eslint-disable-line react-hooks/exhaustive-deps
 
   const openEditConfig = (row: any) => {
     setEditConfig(true);
@@ -74,12 +114,33 @@ export const ConfigData: FC<GlobalConfigProps> = ({
     setConfigData(row);
   };
 
+  const formatDisplayName = (cell: any, row: any) => {
+    return (
+      <>
+        {row.displayName}
+        <OverlayTrigger
+          placement="right"
+          overlay={
+            <Tooltip className="high-index" id="runtime-config-tooltip">
+              {row.helpTxt}
+            </Tooltip>
+          }
+        >
+          <span>
+            &nbsp;&nbsp;
+            <i className="fa fa-question-circle yb-help-color yb-info-tip yb-table-cell-align" />
+          </span>
+        </OverlayTrigger>
+      </>
+    );
+  };
+
   const formatActionButtons = (cell: any, row: any) => {
     return (
       <DropdownButton
         className="btn btn-default"
         title="Actions"
-        id="runtime-config-nested-dropdown"
+        id="runtime-config-nested-dropdown middle-aligned-table"
         pullRight
       >
         <MenuItem
@@ -90,19 +151,31 @@ export const ConfigData: FC<GlobalConfigProps> = ({
           {t('admin.advanced.globalConfig.ModelEditConfigTitle')}
         </MenuItem>
 
-        {(!row.isConfigInherited) && <MenuItem
-          onClick={() => {
-            openResetConfig(row);
-          }}
-        >
-          {t('admin.advanced.globalConfig.ModelResetConfigTitle')}
-        </MenuItem>}
+        {!row.isConfigInherited && (
+          <MenuItem
+            onClick={() => {
+              openResetConfig(row);
+            }}
+          >
+            {t('admin.advanced.globalConfig.ModelResetConfigTitle')}
+          </MenuItem>
+        )}
       </DropdownButton>
     );
   };
 
+  if (runtimeConfigs?.data && getPromiseState(runtimeConfigs).isLoading()) {
+    return <YBLoading />;
+  } else if (runtimeConfigs?.error) {
+    return (
+      <YBErrorIndicator
+        customErrorMessage={t('admin.advanced.globalConfig.GlobalConfigReqFailed')}
+      />
+    );
+  }
+
   const rowClassNameFormat = (row: any) => {
-    return row.isConfigInherited ? "config-inherited-row" : "config-non-inherited-row";
+    return row.isConfigInherited ? 'config-inherited-row' : 'config-non-inherited-row';
   };
 
   return (
@@ -112,11 +185,13 @@ export const ConfigData: FC<GlobalConfigProps> = ({
           label={
             <span className="checkbox-label">
               {t('admin.advanced.globalConfig.ShowOverridenConfigs')}
-            </span>}
+            </span>
+          }
           input={{
             onChange: (e: any) => {
               setShowOverridenValues(e.target.checked);
-            }
+            },
+            checked: showOverridenValues
           }}
         />
       </span>
@@ -129,9 +204,18 @@ export const ConfigData: FC<GlobalConfigProps> = ({
       >
         <TableHeaderColumn dataField="configID" isKey={true} hidden={true} />
         <TableHeaderColumn
+          width="15%"
+          className={'middle-aligned-table'}
+          columnClassName={'yb-table-cell yb-table-cell-align'}
+          dataFormat={formatDisplayName}
+          dataSort
+        >
+          Display Name
+        </TableHeaderColumn>
+        <TableHeaderColumn
           dataField={'configKey'}
           width="15%"
-          columnClassName={'table-name-label yb-table-cell'}
+          columnClassName={'yb-table-cell yb-table-cell-align'}
           dataSort
         >
           Config Key
@@ -139,6 +223,8 @@ export const ConfigData: FC<GlobalConfigProps> = ({
         <TableHeaderColumn
           dataField={'configValue'}
           width="15%"
+          columnClassName={'yb-table-cell yb-table-cell-align'}
+          dataSort
         >
           Config Value
         </TableHeaderColumn>
@@ -153,7 +239,7 @@ export const ConfigData: FC<GlobalConfigProps> = ({
           </TableHeaderColumn>
         )}
       </BootstrapTable>
-      {editConfig &&
+      {editConfig && (
         <EditConfig
           configData={configData}
           onHide={() => setEditConfig(false)}
@@ -162,8 +248,9 @@ export const ConfigData: FC<GlobalConfigProps> = ({
           universeUUID={universeUUID}
           providerUUID={providerUUID}
           customerUUID={customerUUID}
-        />}
-      {resetConfig &&
+        />
+      )}
+      {resetConfig && (
         <ResetConfig
           configData={configData}
           onHide={() => setResetConfig(false)}
@@ -172,7 +259,8 @@ export const ConfigData: FC<GlobalConfigProps> = ({
           universeUUID={universeUUID}
           providerUUID={providerUUID}
           customerUUID={customerUUID}
-        />}
+        />
+      )}
     </div>
   );
 };
