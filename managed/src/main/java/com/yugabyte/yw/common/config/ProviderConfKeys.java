df74c031bded0b595bc43f6998ddb39d5e2ae73a
asharma-yb <asharma@yugabyte.com>
2023-01-18 10:03:26 +0000

    [PLAT-6948][PLAT-6516] Add tags to Key metadata
    Summary:
    This diff adds List<tags> to the key metadata.
    
    Also move a couple of keys to customer scope.
    
    Test Plan: manually checked API response of key info api.
    
    Reviewers: sb-yb
    
    Reviewed By: sb-yb
    
    Subscribers: tsinghania, rmadhavan, jenkins-bot, yugaware
    
    Differential Revision: https://phabricator.dev.yugabyte.com/D22336

--- managed/src/main/java/com/yugabyte/yw/common/config/ProviderConfKeys.java
+++ managed/src/main/java/com/yugabyte/yw/common/config/ProviderConfKeys.java
@@ -10,6 +10,7 @@
 
 package com.yugabyte.yw.common.config;
 
+import com.google.common.collect.ImmutableList;
 import com.yugabyte.yw.forms.RuntimeConfigFormData.ScopedConfig.ScopeType;
 
 public class ProviderConfKeys extends RuntimeConfigKeysModule {
@@ -20,20 +21,31 @@ public class ProviderConfKeys extends RuntimeConfigKeysModule {
           ScopeType.PROVIDER,
           "Allow Unsupported Instances",
           "Enabling removes supported instance type filtering on AWS providers.",
-          ConfDataType.BooleanType);
+          ConfDataType.BooleanType,
+          ImmutableList.of(ConfKeyTags.PUBLIC));
   public static final ConfKeyInfo<String> defaultAwsInstanceType =
       new ConfKeyInfo<>(
           "yb.internal.default_aws_instance_type",
           ScopeType.PROVIDER,
           "Default AWS Instance Type",
           "Default AWS Instance Type",
-          ConfDataType.StringType);
-  // TODO(artem): Add correct metadata
+          ConfDataType.StringType,
+          ImmutableList.of(ConfKeyTags.PUBLIC));
   public static final ConfKeyInfo<String> universeBootScript =
       new ConfKeyInfo<>(
           "yb.universe_boot_script",
           ScopeType.PROVIDER,
           "Universe Boot Script",
           "Custom script to run on VM boot during universe provisioning",
-          ConfDataType.StringType);
+          ConfDataType.StringType,
+          ImmutableList.of(ConfKeyTags.PUBLIC));
+  // TODO Yury
+  public static final ConfKeyInfo<Boolean> skipKeyPairValidation =
+      new ConfKeyInfo<>(
+          "yb.provider.skip_keypair_validation",
+          ScopeType.PROVIDER,
+          "Skip Key Pair Validation",
+          "TODO",
+          ConfDataType.BooleanType,
+          ImmutableList.of(ConfKeyTags.BETA));
 }
