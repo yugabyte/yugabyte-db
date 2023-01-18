dbf6ac1f0121c4228852a9d3c5901a52a805780b
asharma-yb <asharma@yugabyte.com>
2023-01-18 10:03:26 +0000

    [PLAT-6948][PLAT-6516] Add tags to Key metadata
    Summary:
    This diff adds List<tags> to the key metadata.
    
    Also move a couple of keys to customer scope.
    
    Test Plan: manually checked API response of key info api.
    
    Reviewers: sb-yb
    
    Reviewed By: sb-yb
    
    Subscribers: tsinghania, rmadhavan, jenkins-bot, yugaware
    
    Differential Revision: https://phabricator.dev.yugabyte.com/D22336

--- managed/src/main/java/com/yugabyte/yw/common/config/CustomerConfKeys.java
+++ managed/src/main/java/com/yugabyte/yw/common/config/CustomerConfKeys.java
@@ -10,17 +10,35 @@
 
 package com.yugabyte.yw.common.config;
 
+import com.google.common.collect.ImmutableList;
 import com.yugabyte.yw.forms.RuntimeConfigFormData.ScopedConfig.ScopeType;
 import java.time.Duration;
 
 public class CustomerConfKeys extends RuntimeConfigKeysModule {
 
-  // TODO(Shashank): Add correct metadata
   public static final ConfKeyInfo<Duration> taskGcRetentionDuration =
       new ConfKeyInfo<>(
           "yb.taskGC.task_retention_duration",
           ScopeType.CUSTOMER,
           "Task Garbage Collection Retention Duration",
           "We garbage collect stale tasks after this duration",
-          ConfDataType.DurationType);
+          ConfDataType.DurationType,
+          ImmutableList.of(ConfKeyTags.PUBLIC));
+  public static final ConfKeyInfo<Boolean> isAuthEnforced =
+      new ConfKeyInfo<>(
+          "yb.universe.auth.is_enforced",
+          ScopeType.CUSTOMER,
+          "Enforce Auth",
+          "Enforces users to enter password for YSQL/YCQL during Universe creation",
+          ConfDataType.BooleanType,
+          ImmutableList.of(ConfKeyTags.PUBLIC));
+  public static final ConfKeyInfo<Integer> taskDbQueryLimit =
+      new ConfKeyInfo<>(
+          "yb.customer_task_db_query_limit",
+          ScopeType.CUSTOMER,
+          "Max Number of Customer Tasks to fetch",
+          "Knob that can be used when there are too many customer tasks"
+              + " overwhelming the server",
+          ConfDataType.IntegerType,
+          ImmutableList.of(ConfKeyTags.PUBLIC));
 }
