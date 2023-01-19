b53f1e5c85198c1eed1bcc940617b1812be31f8b
Sahith Kurapati <skurapati@yugabyte.com>
2023-01-19 18:47:57 +0000

    [PLAT-5884] [HCV] Remove the dependency of hard coded universe_key for KMS
    Summary:
    This PR adds the following key/value pair to the authConfig object in the Hashicorp vault `kms_config` table:
    {"HC_VAULT_KEY_NAME" : "key_yugabyte"}
    
    We add a migration to change this in the DB, as well as a failover mechanism in the code to retrieve the previously hardcoded key name.
    
    Going forward we want to give the users the option to create a key on their vault with their own custom name (similar to AWS/GCP/AZU KMS). If no key name is given in the payload (from the UI/API), then we default to using the original hardcoded name. If a key with the same name already exists, we will use it as it is.
    
    Test Plan:
    Ran UTs. Ran itests.
    
    Ran the following manual tests:
    * Create KMS config without "HC_VAULT_KEY_NAME" in the payload via API. Ensured the field "HC_VAULT_KEY_NAME" exists when listing KMS configs.
    * Create KMS config with "HC_VAULT_KEY_NAME" = "hcv_test_key". Ensured the key is created and the field "HC_VAULT_KEY_NAME" exists when listing KMS configs.
    * Create KMS config with "HC_VAULT_KEY_NAME" = "hcv_test_key". Ensured the same key is used instead of creating another one or throwing error and the field "HC_VAULT_KEY_NAME" exists when listing KMS configs.
    
    Reviewers: svarshney
    
    Reviewed By: svarshney
    
    Subscribers: jenkins-bot, yugaware
    
    Differential Revision: https://phabricator.dev.yugabyte.com/D22376

--- managed/src/main/java/com/yugabyte/yw/common/kms/services/HashicorpEARService.java
+++ managed/src/main/java/com/yugabyte/yw/common/kms/services/HashicorpEARService.java
@@ -20,10 +20,8 @@ import com.yugabyte.yw.common.kms.util.KeyProvider;
 import com.yugabyte.yw.common.kms.util.hashicorpvault.VaultSecretEngineBase;
 import com.yugabyte.yw.forms.EncryptionAtRestConfig;
 import com.yugabyte.yw.models.KmsConfig;
-
 import java.util.UUID;
 import java.util.List;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -49,15 +47,14 @@ public class HashicorpEARService extends EncryptionAtRestService<HashicorpVaultA
   }
 
   @Override
-  protected ObjectNode createAuthConfigWithService(UUID configUUID, ObjectNode config) {
+  protected ObjectNode createAuthConfigWithService(UUID configUUID, ObjectNode authConfig) {
     ObjectNode result = null;
 
     try {
-
       // creates vault accessor object and validates the token
-      VaultSecretEngineBase engine = HashicorpEARServiceUtil.getVaultSecretEngine(config);
+      VaultSecretEngineBase engine = HashicorpEARServiceUtil.getVaultSecretEngine(authConfig);
       List<Object> ttlInfo = engine.getTTL();
-      result = config;
+      result = authConfig;
 
       LOG.debug(
           "Updating HC_VAULT_TTL_EXPIRY for createAuthConfigWithService with {} and {}",
@@ -72,6 +69,16 @@ public class HashicorpEARService extends EncryptionAtRestService<HashicorpVaultA
       LOG.error(errMsg, e);
     }
 
+    try {
+      // Check if key with given name in the authConfig exists, else create a new one.
+      HashicorpEARServiceUtil.createVaultKEK(configUUID, authConfig);
+    } catch (Exception e) {
+      LOG.error(
+          "Error while trying to check/create a key in the vault for config UUID '{}'.",
+          configUUID,
+          e);
+      return null;
+    }
     LOG.info("Returing from createAuthConfigWithService");
     return result;
   }
@@ -116,7 +123,7 @@ public class HashicorpEARService extends EncryptionAtRestService<HashicorpVaultA
     try {
       final ObjectNode authConfig = getAuthConfig(configUUID);
       // currently we use only KEK property of vault transit engine (created only if required)
-      HashicorpEARServiceUtil.createVaultKEK(universeUUID, configUUID, authConfig);
+      HashicorpEARServiceUtil.createVaultKEK(configUUID, authConfig);
     } catch (Exception e) {
       final String errMsg = "Error occurred creating encryption key";
       LOG.error(errMsg, e);
