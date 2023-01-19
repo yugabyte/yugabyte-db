271a98bf9c5f8351c6786045456cb78f6806d239
jhe <jhe@yugabyte.com>
2023-01-19 12:18:47 -0800

    [BACKPORT 2.16][#15698] docdb: Handle null placement info due to faulty restores
    Summary:
    Original commit: 3367835ae765912b0882711f56b7a9a54b5228cc / D22335
    During `RecreateTable` as part of restores, we used to blindly copy the `replication_info` from the snapshot - even if it was empty. This would result in a null `replication_info`, but that would still pass the `.has_replication_info()` checks used by the LB.
    
    The LB would then choose this empty placement info over any other placement infos, and as a result, would place tablets anywhere - which could result in multiple tablets being placed in the same region. This would also result in tablet leaders not being placed in preferred zones, as when choosing possible leaders we do an intersection of a tablet's replication info with the preferred zones list (in this case we would do the intersection of an empty set with a valid set = empty set, resulting in preferred zones being ignored).
    
    Fixing by not checking `.has_replication_info()` but instead running `IsReplicationInfoSet` to ensure that this value is valid.
    
    Test Plan:
    ```
    ybd --cxx-test load_balancer-test --gtest_filter LoadBalancerTest.TableWithNullPartitionInfo
    ```
    
    Reviewers: skedia, slingam, asrivastava
    
    Reviewed By: asrivastava
    
    Subscribers: bogdan, slingam, ybase
    
    Differential Revision: https://phabricator.dev.yugabyte.com/D22384

--- src/yb/master/catalog_manager.cc
+++ src/yb/master/catalog_manager.cc
@@ -2080,7 +2080,9 @@ Result<ReplicationInfoPB> CatalogManager::GetTableReplicationInfo(
     const scoped_refptr<const TableInfo>& table) const {
   {
     auto table_lock = table->LockForRead();
-    if (table_lock->pb.has_replication_info()) {
+    // Check that the replication info is present and is valid (could be set to invalid null value
+    // due to restore issue, see #15698).
+    if (IsReplicationInfoSet(table_lock->pb.replication_info())) {
       return table_lock->pb.replication_info();
     }
   }
@@ -2131,7 +2133,7 @@ Result<boost::optional<ReplicationInfoPB>> CatalogManager::GetTablespaceReplicat
   return tablespace_manager->GetTablespaceReplicationInfo(tablespace_id);
 }
 
-bool CatalogManager::IsReplicationInfoSet(const ReplicationInfoPB& replication_info) {
+bool CatalogManager::IsReplicationInfoSet(const ReplicationInfoPB& replication_info) const {
   const auto& live_placement_info = replication_info.live_replicas();
   if (!(live_placement_info.placement_blocks().empty() && live_placement_info.num_replicas() <= 0 &&
         live_placement_info.placement_uuid().empty()) ||
@@ -2143,7 +2145,8 @@ bool CatalogManager::IsReplicationInfoSet(const ReplicationInfoPB& replication_i
   return false;
 }
 
-Status CatalogManager::ValidateTableReplicationInfo(const ReplicationInfoPB& replication_info) {
+Status CatalogManager::ValidateTableReplicationInfo(
+    const ReplicationInfoPB& replication_info) const {
   if (!IsReplicationInfoSet(replication_info)) {
     return STATUS(InvalidArgument, "No replication info set.");
   }
