1b6ddd62933e0fa189db0185cfc8f49182983cd8
Muthu Chidambaram <mchidambaram@yugabyte.com>
2023-01-20 19:24:48 +0000

    [PLAT-6423]: Fix YBA vulnerability
    Summary:
    As part of this changes updated following libs to make YBA more secure.
    
     - jackson-databind - 2.13.4.2,
     - ansible - 2.9.27
     - node_exporter 1.5.0
     - woodstox-core - 6.4.0
     - google-oauth-client - 1.34.1
    
    Removed Following libs
     -  "org.apache.velocity" % "velocity" % "1.7" as didn't find any use of it.
     -  "jetty-io" and "jetty-server" as they are dependencies and their exclusion does not raise any issue.
    
    Test Plan:
    Tested Manually by creating universes and perform day-1 and day-2 operations.
    
    Dependency changes:
    
    ```
     6c6
    < [info] loading settings for project root from ui-build.sbt,build.sbt ...
    ---
    > [info] loading settings for project root from build.sbt,ui-build.sbt ...
    41,43c41,43
    < [info] com.fasterxml.jackson.core:jackson-annotations:2.14.0-rc3
    < [info] com.fasterxml.jackson.core:jackson-core:2.14.0-rc3
    < [info] com.fasterxml.jackson.core:jackson-databind:2.14.0-rc3
    ---
    > [info] com.fasterxml.jackson.core:jackson-annotations:2.13.3
    > [info] com.fasterxml.jackson.core:jackson-core:2.13.3
    > [info] com.fasterxml.jackson.core:jackson-databind:2.13.4.2
    49c49
    < [info] com.fasterxml.woodstox:woodstox-core:6.2.7
    ---
    > [info] com.fasterxml.woodstox:woodstox-core:6.4.0
    91c91
    < [info] com.google.http-client:google-http-client:1.41.8
    ---
    > [info] com.google.http-client:google-http-client:1.42.0
    94c94
    < [info] com.google.http-client:google-http-client-gson:1.41.8
    ---
    > [info] com.google.http-client:google-http-client-gson:1.42.0
    100c100
    < [info] com.google.oauth-client:google-oauth-client:1.33.2
    ---
    > [info] com.google.oauth-client:google-oauth-client:1.34.1
    172d171
    < [info] commons-lang:commons-lang:2.4
    312d310
    < [info] org.apache.velocity:velocity:1.7
    
    ```
    
    Reviewers: sanketh, ddinh, amalyshev, sb-yb, muthu, sneelakantan
    
    Reviewed By: amalyshev, sb-yb, sneelakantan
    
    Subscribers: sneelakantan, jenkins-bot, yugaware
    
    Differential Revision: https://phabricator.dev.yugabyte.com/D22018

--- managed/src/main/java/com/yugabyte/yw/controllers/handlers/CloudProviderHandler.java
+++ managed/src/main/java/com/yugabyte/yw/controllers/handlers/CloudProviderHandler.java
@@ -82,7 +82,7 @@ import java.util.UUID;
 import java.util.stream.Collectors;
 import javax.persistence.PersistenceException;
 import org.apache.commons.collections.MapUtils;
-import org.apache.commons.lang.StringUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import play.Configuration;
