1408ecc38cd38bb22fbf13da18e3153a5b5d2f98
asharma-yb <asharma@yugabyte.com>
2023-01-24 18:15:54 +0000

    [PLAT-7132] Add runtime key for UI tag filter.
    Test Plan: Added UT.
    
    Reviewers: rmadhavan, sb-yb
    
    Reviewed By: rmadhavan, sb-yb
    
    Subscribers: jenkins-bot, yugaware
    
    Differential Revision: https://phabricator.dev.yugabyte.com/D22483

--- managed/src/test/java/com/yugabyte/yw/common/config/ConfDataTypeTest.java
+++ managed/src/test/java/com/yugabyte/yw/common/config/ConfDataTypeTest.java
@@ -3,9 +3,11 @@ package com.yugabyte.yw.common.config;
 import static com.yugabyte.yw.common.AssertHelper.assertPlatformException;
 import static com.yugabyte.yw.common.config.ConfDataType.parseBoolean;
 import static com.yugabyte.yw.common.config.ConfDataType.parseStrList;
+import static com.yugabyte.yw.common.config.ConfDataType.parseTagsList;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
+import com.yugabyte.yw.common.config.ConfKeyInfo.ConfKeyTags;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -29,5 +31,15 @@ public class ConfDataTypeTest {
     // Strings should be enclosed within double quotes
     assertPlatformException(() -> parseStrList("[Three,Sample,String]"));
     assertPlatformException(() -> parseStrList("Random string"));
+  }
+
+  @Test
+  public void tagListParse() {
+    List<ConfKeyTags> list =
+        new ArrayList<>(Arrays.asList(ConfKeyTags.PUBLIC, ConfKeyTags.BETA, ConfKeyTags.UIDriven));
+    assertTrue(list.equals(parseTagsList("[\"PUBLIC\",\"BETA\",\"UIDriven\"]")));
+    // Strings should be enclosed within double quotes
+    assertPlatformException(() -> parseTagsList("[Three,Sample,String]"));
+    assertPlatformException(() -> parseTagsList("[\"Invalid\",\"tags\"]"));
   }
 }
