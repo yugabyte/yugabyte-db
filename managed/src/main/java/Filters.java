bf4e65a885376b56c4333b012eb6f80fb547d851
SB YB <sbapat@yugabyte.com>
2023-01-26 10:34:07 -0800

    [PLAT-7144][PLAT-4924] Remove RedirectHttpSFilter and GZipFilter
    Summary:
    These were unrelated changes that I had shoved in and testing was going to happen later on live system.
    These cause itest failure as https redirect is happening by default.
    Backing out this part for now.
    
    Test Plan: Make sure that unit test and itest should pass after this
    
    Reviewers: hzare
    
    Reviewed By: hzare
    
    Subscribers: jenkins-bot, yugaware
    
    Differential Revision: https://phabricator.dev.yugabyte.com/D22542

--- managed/src/main/java/Filters.java
+++ managed/src/main/java/Filters.java
@@ -3,11 +3,9 @@
 import com.google.inject.Inject;
 import play.filters.cors.CORSFilter;
 import play.filters.csrf.CSRFFilter;
-import play.filters.gzip.GzipFilter;
-import play.filters.https.RedirectHttpsFilter;
 import play.http.DefaultHttpFilters;
 
-// TODO(sbapat): Get rid of this class an switch to config mechanism to enable filters
+// TODO(sbapat): Get rid of this class and switch to config mechanism to enable filters
 public class Filters extends DefaultHttpFilters {
 
   @Inject
@@ -15,15 +13,7 @@ public class Filters extends DefaultHttpFilters {
       CSRFFilter csrfFilter,
       CORSFilter corsFilter,
       RequestLoggingFilter requestLoggingFilter,
-      RequestHeaderFilter requestHeaderFilter,
-      GzipFilter gzipFilter,
-      RedirectHttpsFilter redirectHttpsFilter) {
-    super(
-        corsFilter,
-        csrfFilter,
-        requestLoggingFilter,
-        requestHeaderFilter,
-        gzipFilter,
-        redirectHttpsFilter);
+      RequestHeaderFilter requestHeaderFilter) {
+    super(corsFilter, csrfFilter, requestLoggingFilter, requestHeaderFilter);
   }
 }
