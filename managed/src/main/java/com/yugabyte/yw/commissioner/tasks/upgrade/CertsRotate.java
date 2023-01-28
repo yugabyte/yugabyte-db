d34b1c81dd0283c63dea10b69e4a39dffd5a7269
vpatibandla-yb <vpatibandla@yugabyte.com>
2023-01-28 14:07:27 +0000

    [PLAT-7189] Edit Universe fails when failed nodes are replaced with new nodes as WaitForYbcServer fails
    Summary: Perform WaitForYbc only on newly added universes, not on all the nodes that we are currently doing.
    
    Test Plan: https://jenkins.dev.yugabyte.com/view/Dev/job/dev-itest-pipeline/1315/
    
    Reviewers: vbansal, vkumar, nbhatia
    
    Reviewed By: vkumar, nbhatia
    
    Subscribers: nbhatia, jenkins-bot, yugaware
    
    Differential Revision: https://phabricator.dev.yugabyte.com/D22584

--- managed/src/main/java/com/yugabyte/yw/commissioner/tasks/upgrade/CertsRotate.java
+++ managed/src/main/java/com/yugabyte/yw/commissioner/tasks/upgrade/CertsRotate.java
@@ -280,7 +280,8 @@ public class CertsRotate extends UpgradeTaskBase {
           .setSubTaskGroupType(SubTaskGroupType.StartingNodeProcesses);
 
       // Wait for yb-controller to be responsive on each node.
-      createWaitForYbcServerTask(null).setSubTaskGroupType(SubTaskGroupType.ConfigureUniverse);
+      createWaitForYbcServerTask(nodesPair.getRight())
+          .setSubTaskGroupType(SubTaskGroupType.ConfigureUniverse);
     }
   }
 
