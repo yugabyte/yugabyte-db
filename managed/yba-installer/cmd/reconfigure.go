36745f772395ad6210f56595d3416cd577179d75
Daniel Shubin <dshubin@yugabyte.com>
2023-01-28 08:46:39 -0800

    [PLAT-7211] YBA-installer preflight check improvement
    Summary:
    running preflight checks standalone prints a more clear message
    on 100% pass
    
    standalone preflight checks can also run the 'upgrade' checks
    
    upgrade command can now skip preflight checks
    
    Test Plan: ran multiple versions of preflight checks to validate results
    
    Reviewers: muthu, sanketh
    
    Reviewed By: sanketh
    
    Subscribers: jenkins-bot, sanketh, muthu, yugaware
    
    Differential Revision: https://phabricator.dev.yugabyte.com/D22620

--- managed/yba-installer/cmd/reconfigure.go
+++ managed/yba-installer/cmd/reconfigure.go
@@ -12,11 +12,11 @@ import (
 var reconfigureCmd = &cobra.Command{
 	Use: "reconfigure",
 	Short: "The reconfigure command is used to apply changes made to yba-ctl.yml to running " +
-		"Yugabyte Anywhere services.",
+		"YugabyteDB Anywhere services.",
 	Args: cobra.NoArgs,
 	Long: `
     The reconfigure command is used to apply changes made to yba-ctl.yml to running 
-	Yugabyte Anywhere services. The process involves stopping all associated services
+	YugabyteDB Anywhere services. The process involves stopping all associated services
 	and restarting them.`,
 	Run: func(cmd *cobra.Command, args []string) {
 		for _, name := range serviceOrder {
--- managed/yba-installer/cmd/root.go
+++ managed/yba-installer/cmd/root.go
@@ -22,9 +22,9 @@ var logLevel string
 
 var rootCmd = &cobra.Command{
 	Use:   "yba-ctl",
-	Short: "YBA Installer is used to install Yugabyte Anywhere in an automated manner.",
+	Short: "YBA Installer is used to install YugabyteDB Anywhere in an automated manner.",
 	Long: `
-    YBA Installer is your one stop shop for deploying Yugabyte Anywhere! Through
+    YBA Installer is your one stop shop for deploying YugabyteDB Anywhere! Through
     YBA Installer, you can perform numerous actions related to your Yugabyte
     Anywhere instance through our command line CLI, such as clean, createBackup,
     restoreBackup, install, and upgrade! View the CLI menu to learn more!`,
