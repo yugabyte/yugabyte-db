3b6d44a9753d0fbd731db121d0cc6b197866f570
rsami <rsami@yugabyte.com>
2023-01-31 23:48:21 -0500

    [BACKPORT 2.16][#15807] DocDB: Ensure IntraTxnWriteId is properly decoded even in weak lock intents
    Summary:
    Original commit: 70919660e24edac317158140e3c291a2b699b3cd / D22608
    During conflict resolution, we decode an intent value to determine which txn and subtxn a
    conflicting intent originates from, and to determine if it is a lock-only conflict. We depend on the
    method DecodeIntentValue in intent.h to return a DecodedIntentValue object which captures the
    txn_id, subtxn_id, optionally the write_id, and any value remaining after all three of these.
    However, we only decode/consume the bytes of the write_id if the argument has_strong_intent is true.
    
    It seems that in the case of a `select .... for key share`, we persist a kWeakRead, but also a
    write_id appears in the intent value. The conflict resolution lock depends on finding a kRowLock as
    the first byte in the remaining undecoded intent value body, and in the case of this kWeakRead
    intent it was instead finding a kWriteId, causing this intent to be treated as a modification rather
    than a lock only.
    
    This revision modifies the behavior of DecodeIntentValue to always consume and decode the
    IntraTxnWriteId from the intent value.
    
    Test Plan: ybd --java-test org.yb.pgsql.TestPgWaitQueuesRegress
    
    Reviewers: pjain, sergei
    
    Reviewed By: sergei
    
    Subscribers: ybase, tvesely, qhu, bkolagani, esheng, bogdan
    
    Differential Revision: https://phabricator.dev.yugabyte.com/D22664

--- src/yb/docdb/conflict_resolution.cc
+++ src/yb/docdb/conflict_resolution.cc
@@ -248,15 +248,20 @@ class ConflictResolver : public std::enable_shared_from_this<ConflictResolver> {
       auto existing_intent = VERIFY_RESULT(
           docdb::ParseIntentKey(intent_iter_.key(), existing_value));
 
-      VLOG_WITH_PREFIX_AND_FUNC(4) << "Found: " << existing_value.ToDebugString()
+      VLOG_WITH_PREFIX_AND_FUNC(4) << "Found: " << SubDocKey::DebugSliceToString(existing_key)
+                                   << ", with value: " << existing_value.ToDebugString()
                                    << " has intent types " << ToString(existing_intent.types);
-      auto decoded_value = VERIFY_RESULT(DecodeIntentValue(
-          existing_value, nullptr /* verify_transaction_id_slice */,
-          HasStrong(existing_intent.types)));
       const auto intent_mask = kIntentTypeSetMask[existing_intent.types.ToUIntPtr()];
       if ((conflicting_intent_types & intent_mask) != 0) {
+        auto decoded_value = VERIFY_RESULT(DecodeIntentValue(
+            existing_value, nullptr /* verify_transaction_id_slice */,
+            HasStrong(existing_intent.types)));
         auto transaction_id = decoded_value.transaction_id;
         bool lock_only = decoded_value.body.starts_with(ValueEntryTypeAsChar::kRowLock);
+        VLOG_WITH_PREFIX_AND_FUNC(4)
+            << "Found conflict with exiting transaction: " << transaction_id
+            << ", lock_only: " << lock_only
+            << ", body: " << decoded_value.body.ToDebugHexString();
 
         if (!context_->IgnoreConflictsWith(transaction_id)) {
           auto p = conflicts_.emplace(transaction_id, std::make_shared<SubtxnHasNonLockConflict>());
