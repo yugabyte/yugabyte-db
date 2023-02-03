33fbaf99075a3f24cc34059db8548662c2ddf71c
Aleksandr Malyshev <amalyshev@yugabyte.com>
2023-02-03 15:08:21 +0300

    [PLAT-7260] Allow configuring alert notification templates per channel type
    Summary:
    According to last UI mocks for custom alert notification templates - we're configuring notification templates per channel type.
    Need to introduce such a functionality, including API to configure/list/get templates.
    
    Test Plan:
    Configure custom alert notification template for Email channels via API.
    Raise alert. Make sure alert email title and body are generated using configured template.
    Unit tests.
    
    Reviewers: vbansal
    
    Reviewed By: vbansal
    
    Subscribers: jenkins-bot, sanketh, yugaware
    
    Differential Revision: https://phabricator.dev.yugabyte.com/D22738

--- managed/src/test/java/com/yugabyte/yw/controllers/AlertControllerTest.java
+++ managed/src/test/java/com/yugabyte/yw/controllers/AlertControllerTest.java
@@ -70,6 +70,8 @@ import com.yugabyte.yw.common.alerts.AlertChannelEmailParams;
 import com.yugabyte.yw.common.alerts.AlertChannelParams;
 import com.yugabyte.yw.common.alerts.AlertChannelService;
 import com.yugabyte.yw.common.alerts.AlertChannelSlackParams;
+import com.yugabyte.yw.common.alerts.AlertChannelTemplateService;
+import com.yugabyte.yw.common.alerts.AlertChannelTemplateServiceTest;
 import com.yugabyte.yw.common.alerts.AlertConfigurationService;
 import com.yugabyte.yw.common.alerts.AlertDestinationService;
 import com.yugabyte.yw.common.alerts.AlertUtils;
@@ -85,6 +87,7 @@ import com.yugabyte.yw.forms.paging.AlertPagedApiQuery;
 import com.yugabyte.yw.models.Alert;
 import com.yugabyte.yw.models.AlertChannel;
 import com.yugabyte.yw.models.AlertChannel.ChannelType;
+import com.yugabyte.yw.models.AlertChannelTemplates;
 import com.yugabyte.yw.models.AlertConfiguration;
 import com.yugabyte.yw.models.AlertConfiguration.SortBy;
 import com.yugabyte.yw.models.AlertConfiguration.TargetType;
@@ -307,6 +310,7 @@ public class AlertControllerTest extends FakeDBApplication {
   private int alertDestinationIndex;
 
   private AlertChannelService alertChannelService;
+  private AlertChannelTemplateService alertChannelTemplateService;
   private AlertDestinationService alertDestinationService;
   private AlertConfigurationService alertConfigurationService;
   private AlertController alertController;
@@ -323,6 +327,7 @@ public class AlertControllerTest extends FakeDBApplication {
     universe = ModelFactory.createUniverse();
 
     alertChannelService = app.injector().instanceOf(AlertChannelService.class);
+    alertChannelTemplateService = app.injector().instanceOf(AlertChannelTemplateService.class);
     alertDestinationService = app.injector().instanceOf(AlertDestinationService.class);
     alertConfigurationService = app.injector().instanceOf(AlertConfigurationService.class);
     alertController = app.injector().instanceOf(AlertController.class);
@@ -1476,6 +1481,80 @@ public class AlertControllerTest extends FakeDBApplication {
                 + customer.getUuid()
                 + "/alert_template_settings/"
                 + settings.getUuid(),
+            authToken);
+    assertThat(result.status(), equalTo(OK));
+  }
+
+  @Test
+  public void testGetChannelTemplates() {
+    AlertChannelTemplates templates =
+        AlertChannelTemplateServiceTest.createTemplates(customer.getUuid(), ChannelType.Email);
+    alertChannelTemplateService.save(templates);
+
+    Result result =
+        doRequestWithAuthToken(
+            "GET",
+            "/api/customers/" + customer.getUuid() + "/alert_channel_templates/Email",
+            authToken);
+    assertThat(result.status(), equalTo(OK));
+    JsonNode templateJson = Json.parse(contentAsString(result));
+    AlertChannelTemplates resultTemplates =
+        Json.fromJson(templateJson, AlertChannelTemplates.class);
+    assertThat(resultTemplates, equalTo(templates));
+  }
+
+  @Test
+  public void testListChannelTemplates() {
+    AlertChannelTemplates templates =
+        AlertChannelTemplateServiceTest.createTemplates(customer.getUuid(), ChannelType.Email);
+    alertChannelTemplateService.save(templates);
+    AlertChannelTemplates templates2 =
+        AlertChannelTemplateServiceTest.createTemplates(customer.getUuid(), ChannelType.Slack);
+    alertChannelTemplateService.save(templates2);
+
+    Result result =
+        doRequestWithAuthToken(
+            "GET", "/api/customers/" + customer.getUuid() + "/alert_channel_templates", authToken);
+    assertThat(result.status(), equalTo(OK));
+    JsonNode templatesJson = Json.parse(contentAsString(result));
+    List<AlertChannelTemplates> listedTemplates =
+        Arrays.asList(Json.fromJson(templatesJson, AlertChannelTemplates[].class));
+    assertThat(listedTemplates, containsInAnyOrder(templates, templates2));
+  }
+
+  @Test
+  public void testSetChannelTemplates() {
+    AlertChannelTemplates templates =
+        AlertChannelTemplateServiceTest.createTemplates(customer.getUuid(), ChannelType.Email);
+    // Should be able to set without these fields
+    templates.setType(null);
+    templates.setCustomerUUID(null);
+
+    Result result =
+        doRequestWithAuthTokenAndBody(
+            "POST",
+            "/api/customers/" + customer.getUuid() + "/alert_channel_templates/Email",
+            authToken,
+            Json.toJson(templates));
+    assertThat(result.status(), equalTo(OK));
+    JsonNode templateJson = Json.parse(contentAsString(result));
+    AlertChannelTemplates resultTemplates =
+        Json.fromJson(templateJson, AlertChannelTemplates.class);
+
+    AlertChannelTemplates expected =
+        AlertChannelTemplateServiceTest.createTemplates(customer.getUuid(), ChannelType.Email);
+    assertThat(resultTemplates, equalTo(expected));
+  }
+
+  @Test
+  public void testDeleteChannelTemplates() {
+    AlertChannelTemplates templates =
+        AlertChannelTemplateServiceTest.createTemplates(customer.getUuid(), ChannelType.Email);
+    alertChannelTemplateService.save(templates);
+    Result result =
+        doRequestWithAuthToken(
+            "DELETE",
+            "/api/customers/" + customer.getUuid() + "/alert_channel_templates/Email",
             authToken);
     assertThat(result.status(), equalTo(OK));
   }
