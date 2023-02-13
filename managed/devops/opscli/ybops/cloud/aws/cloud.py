yugabyte/yugabyte-db

commit cb3002c0d8a21121b573abbf6f8ee96c6c942710

[PLAT-7078] Fail vmImageUpgrade if replace root volume with new disk fails
Summary:
We start the instance with the current root volume even if mounting the
newly created fails. As we do not fail in this scenario, it is a bug
that we could proceed to the next node and complete the upgrade task
while not actually upgrading any VM images.

While mounting the original current volume there was another exception
as the URL was malformed, fixed that by prefixing the full url.
Also, this change adds more logging and some comments for debugging.

Test Plan:
1. VM image upgrade in a GCP universe.
2. Introduce failure just before mounting new disk and ensure upgrade task fails
   as expected. And, the node is started with the original mount.

Reviewers: sanketh, nbhatia, amindrov, muthu, hzare

Reviewed By: hzare

Subscribers: jenkins-bot, yugaware

Differential Revision: https://phabricator.dev.yugabyte.com/D22809

managed/devops/opscli/ybops/cloud/aws/cloud.py
@@ -485,6 +485,8 @@ class AwsCloud(AbstractCloud):
         self.wait_for_server_ports(host_info["private_ip"], host_info["name"], server_ports)
 
     def mount_disk(self, host_info, vol_id, label):
+        logging.info("Mounting volume {} on host {} in zone {}; label {}".format(
+                     vol_id, host_info['id'], label))
         ec2 = boto3.client('ec2', region_name=host_info['region'])
         ec2.attach_volume(
             Device=label,
@@ -495,6 +497,7 @@ class AwsCloud(AbstractCloud):
         waiter.wait(VolumeIds=[vol_id])
 
     def unmount_disk(self, host_info, vol_id):
+        logging.info("Unmounting volume {} from host {}".format(vol_id, host_info['id']))
         ec2 = boto3.client('ec2', region_name=host_info['region'])
         ec2.detach_volume(VolumeId=vol_id, InstanceId=host_info['id'])
         waiter = ec2.get_waiter('volume_available')

managed/devops/opscli/ybops/cloud/gcp/cloud.py
@@ -110,9 +110,13 @@ class GcpCloud(AbstractCloud):
         return output
 
     def mount_disk(self, args, body):
+        logging.info("Mounting disk on host {} in zone {}; volume info is {}".format(
+                     args['search_pattern'], args['zone'], body))
         self.get_admin().mount_disk(args['zone'], args['search_pattern'], body)
 
     def unmount_disk(self, args, name):
+        logging.info("Unmounting disk {} from host {} in zone {}".format(
+                     name, args['search_pattern'], args['zone']))
         self.get_admin().unmount_disk(args['zone'], args['search_pattern'], name)
 
     def stop_instance(self, args):

managed/devops/opscli/ybops/cloud/common/method.py
@@ -554,7 +554,15 @@ class ReplaceRootVolumeMethod(AbstractInstancesMethod):
         except Exception as e:
             logging.exception(e)
             if unmounted:
-                self._mount_root_volume(host_info, current_root_volume)
+                # Note: This doesn't work if VM image upgrade has already been done successfully
+                # at least once before. The boot disk name will no longer correlate directly with
+                # the instance name(id).
+                old_disk_url = os.path.join(
+                    args.replacement_disk[:args.replacement_disk.rfind('/')], id)
+                self._mount_root_volume(host_info, old_disk_url)
+                logging.warning("Mounted the original volume {} on {} before failing".format(
+                                old_disk_url, id))
+            raise e
         finally:
             server_ports = self.get_server_ports_to_check(args)
             self.cloud.start_instance(host_info, server_ports)
@@ -846,12 +854,14 @@ class CreateRootVolumesMethod(AbstractInstancesMethod):
     def callback(self, args):
         unique_string = ''.join(random.choice(string.ascii_lowercase) for i in range(6))
         args.search_pattern = "{}-".format(unique_string) + args.search_pattern
-        vid = self.create_master_volume(args)
-        output = [vid]
+        volume_id = self.create_master_volume(args)
+        output = [volume_id]
         num_disks = int(args.num_disks) - 1
 
+        # Now clone and create the remaining disks if any as the machine image is the same
         if num_disks > 0:
-            output.extend(self.cloud.clone_disk(args, vid, num_disks))
+            logging.info("Cloning {} other disks using volume_id {}".format(num_disks, volume_id))
+            output.extend(self.cloud.clone_disk(args, volume_id, num_disks))
 
         logging.info("==> Created volumes {}".format(output))
         print(json.dumps(output))

managed/devops/opscli/ybops/cloud/gcp/method.py
@@ -108,7 +108,7 @@ class GcpCreateRootVolumesMethod(CreateRootVolumesMethod):
             "sourceImage": args.machine_image})
         return res["targetLink"]
 
-    # Not invoked. Just keeping if for consistency.
+    # Not invoked. Just keeping it for consistency.
     def delete_instance(self, args):
         name = args.search_pattern[:63] if len(args.search_pattern) > 63 else args.search_pattern
         self.cloud.get_admin().delete_instance(

managed/devops/opscli/ybops/cloud/gcp/utils.py
@@ -171,6 +171,7 @@ class Waiter():
     def wait(self, operation, region=None, zone=None):
         # This allows easier chaining of waits on functions that are NOOPs if items already exist.
         if operation is None:
+            logging.warning("Returning waiting for a None Operation")
             return
         retry_count = 0
         name = operation["name"]
@@ -582,6 +583,7 @@ class GoogleCloudAdmin():
     def create_disk(self, zone, instance_tags, body):
         if instance_tags is not None:
             body.update({"labels": json.loads(instance_tags)})
+        # Create a persistent disk with wait
         operation = self.compute.disks().insert(project=self.project,
                                                 zone=zone,
                                                 body=body).execute()
@@ -642,6 +644,7 @@ class GoogleCloudAdmin():
             self.compute.disks().delete(project=self.project, zone=zone, disk=disk_name).execute()
 
     def mount_disk(self, zone, instance, body):
+        logging.info("Attaching disk on instance {} in zone {}".format(instance, zone))
         operation = self.compute.instances().attachDisk(project=self.project,
                                                         zone=zone,
                                                         instance=instance,
@@ -649,6 +652,7 @@ class GoogleCloudAdmin():
         return self.waiter.wait(operation, zone=zone)
 
     def unmount_disk(self, zone, instance, name):
+        logging.info("Detaching disk {} from instance {}".format(name, instance))
         operation = self.compute.instances().detachDisk(project=self.project,
                                                         zone=zone,
                                                         instance=instance,

commit 68b2bb1f58c37299eaee8c17a08d67353847d452

[PLAT-7379] Remove zone from log message in AWS mount method
Summary:
We do not have zone in this scope.
Fixing this formatting regression with commit cb3002c0d8a21121b573abbf6f8ee96c6c942710

Test Plan: Manual

Reviewers: anijhawan

Reviewed By: anijhawan

Subscribers: yugaware

Differential Revision: https://phabricator.dev.yugabyte.com/D22911

managed/devops/opscli/ybops/cloud/aws/cloud.py
@@ -485,7 +485,7 @@ class AwsCloud(AbstractCloud):
         self.wait_for_server_ports(host_info["private_ip"], host_info["name"], server_ports)
 
     def mount_disk(self, host_info, vol_id, label):
-        logging.info("Mounting volume {} on host {} in zone {}; label {}".format(
+        logging.info("Mounting volume {} on host {}; label {}".format(
                      vol_id, host_info['id'], label))
         ec2 = boto3.client('ec2', region_name=host_info['region'])
         ec2.attach_volume(