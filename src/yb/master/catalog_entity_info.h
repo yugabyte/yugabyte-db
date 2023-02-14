yugabyte/yugabyte-db

commit 6abc1af5af26b3236c751c9eed153cfd9c0d5de8

[#14043] DST: PITR - Handle txn ddl gc in conjunction with PITR
Summary:
If we trigger a PITR restore after a table was created but before the ddl gc on the master removes
the txn metadata then post restore it will enqueue txn verification for the table again. If table
has been hidden as part of restore (i.e. restore time is a time before the table was created) then
txn verification will fail which will trigger a delete of a hidden table. This leads to the table
getting hard deleted now whereas actually this delete should be idempotent i.e. it should continue
to keep the table hidden.

Fixed by ignoring delete table request for a hidden table.

Test Plan: ybd --cxx_test yb-admin-snapshot-schedule-test --gtest-filter YbAdminSnapshotScheduleTest.BeforeCreateFinishes

Reviewers: sergei, msun, dsrinivasan, zdrudi

Reviewed By: zdrudi

Subscribers: slingam, ybase, bogdan

Differential Revision: https://phabricator.dev.yugabyte.com/D21099

src/yb/master/catalog_entity_info.h
@@ -360,6 +360,10 @@ struct PersistentTableInfo : public Persistent<SysTablesEntryPB, SysRowEntryType
     return is_hiding() || is_hidden();
   }
 
+  bool started_hiding_or_deleting() const {
+    return started_hiding() || started_deleting();
+  }
+
   // Return the table's name.
   const TableName& name() const {
     return pb.name();
@@ -454,6 +458,10 @@ class TableInfo : public RefCountedThreadSafe<TableInfo>,
 
   bool is_running() const;
   bool is_deleted() const;
+  bool IsOperationalForClient() const {
+    auto l = LockForRead();
+    return !l->started_hiding_or_deleting();
+  }
 
   std::string ToString() const override;
   std::string ToStringWithState() const;

src/yb/master/catalog_manager.cc
@@ -5727,6 +5727,10 @@ Status CatalogManager::DeleteTable(
     return Status::OK();
   }
 
+  if (CatalogManagerUtil::IsDuplicateDeleteTableRequest(table)) {
+    return Status::OK();
+  }
+
   // For now, only disable dropping YCQL tables under xCluster replication.
   bool result = table->GetTableType() == YQL_TABLE_TYPE && IsCdcEnabled(*table);
   if (!FLAGS_enable_delete_truncate_xcluster_replicated_table && result) {
@@ -5833,6 +5837,13 @@ Status CatalogManager::DeleteTable(
 // IMPORTANT: If modifying, consider updating DeleteYsqlDBTables(), the bulk deletion API.
 Status CatalogManager::DeleteTableInternal(
     const DeleteTableRequestPB* req, DeleteTableResponsePB* resp, rpc::RpcContext* rpc) {
+
+  scoped_refptr<TableInfo> table = VERIFY_RESULT(FindTable(req->table()));
+
+  if (CatalogManagerUtil::IsDuplicateDeleteTableRequest(table)) {
+    return Status::OK();
+  }
+
   auto schedules_to_tables_map = VERIFY_RESULT(
       MakeSnapshotSchedulesToObjectIdsMap(SysRowEntryType::TABLE));
 

src/yb/master/catalog_manager_util.h
@@ -109,7 +109,8 @@ class CatalogManagerUtil {
       const SetPreferredZonesRequestPB* req, ReplicationInfoPB* replication_info);
 
   static void GetAllAffinitizedZones(
-      const ReplicationInfoPB& replication_info, std::vector<AffinitizedZonesSet>* affinitized_zones);
+      const ReplicationInfoPB& replication_info,
+      std::vector<AffinitizedZonesSet>* affinitized_zones);
 
   static Status CheckValidLeaderAffinity(const ReplicationInfoPB& replication_info);
 
@@ -174,6 +175,16 @@ class CatalogManagerUtil {
       const TableId& table_id, const std::string& table_name, const TableType& table_type,
       const Schema& schema, uint32_t schema_version, const PartitionSchema& partition_schema,
       tablet::TableInfoPB* pb);
+
+  // If the table is already hidden or deleted then treats it as a duplicate delete request.
+  // Don't invoke this function if attempting to hard delete an already hidden table.
+  static bool IsDuplicateDeleteTableRequest(const scoped_refptr<TableInfo>& table) {
+    if (!table->IsOperationalForClient()) {
+      LOG(INFO) << "Table " << table->id() << " is already deleted. Duplicate request.";
+      return true;
+    }
+    return false;
+  }
 
  private:
   CatalogManagerUtil();

src/yb/master/catalog_loaders.cc
@@ -306,8 +306,7 @@ Status TabletLoader::Visit(const TabletId& tablet_id, const SysTabletsEntryPB& m
 
   // Add the tablet to tablegroup_manager_ if the tablet is for a tablegroup.
   if (first_table->IsTablegroupParentTable()) {
-    auto lock = first_table->LockForRead();
-    if (!lock->started_hiding() && !lock->started_deleting()) {
+    if (first_table->IsOperationalForClient()) {
       const auto tablegroup_id = GetTablegroupIdFromParentTableId(first_table->id());
 
       auto* tablegroup =
@@ -319,8 +318,7 @@ Status TabletLoader::Visit(const TabletId& tablet_id, const SysTabletsEntryPB& m
       // Loop through tablet_colocation_map to add child tables to our tablegroup info.
       for (const auto& colocation_info : tablet_colocation_map) {
         if (!IsTablegroupParentTableId(colocation_info.second->id())) {
-          auto child_table_lock = colocation_info.second->LockForRead();
-          if (!child_table_lock->started_hiding() && !child_table_lock->started_deleting()) {
+          if (colocation_info.second->IsOperationalForClient()) {
             RETURN_NOT_OK(tablegroup->AddChildTable(colocation_info.second->id(),
                 colocation_info.first));
           }

src/yb/master/master_snapshot_coordinator.cc
@@ -1242,6 +1242,11 @@ class MasterSnapshotCoordinator::Impl {
             LOG(WARNING) << "Table " << table_id << " does not exist";
             continue;
           }
+          // Ignore if deleted or hidden.
+          if (!(*table_info_result)->IsOperationalForClient()) {
+            LOG(WARNING) << "Table " << table_id << " has been deleted";
+            continue;
+          }
           task->SetColocatedTableMetadata(table_id, (*table_info_result)->LockForRead()->pb);
         }
       }

src/yb/master/ysql_transaction_ddl.cc
@@ -28,15 +28,21 @@
 
 #include "yb/tserver/tserver_service.pb.h"
 
+#include "yb/util/flags.h"
 #include "yb/util/logging.h"
 #include "yb/util/monotime.h"
 #include "yb/util/net/net_fwd.h"
 #include "yb/util/status_log.h"
-#include "yb/util/flags.h"
+
+#include "yb/util/flags/flag_tags.h"
 
 DEFINE_UNKNOWN_int32(ysql_transaction_bg_task_wait_ms, 200,
   "Amount of time the catalog manager background task thread waits "
   "between runs");
+
+DEFINE_test_flag(bool, skip_transaction_verification, false,
+    "Test only flag to keep the txn metadata in SysTablesEntryPB and skip"
+    " transaction verification on the master");
 
 using std::string;
 using std::vector;
@@ -142,6 +148,9 @@ void YsqlTransactionDdl::VerifyTransaction(
     scoped_refptr<TableInfo> table,
     bool has_ysql_ddl_txn_state,
     std::function<Status(bool)> complete_callback) {
+  if (FLAGS_TEST_skip_transaction_verification) {
+    return;
+  }
 
   SleepFor(MonoDelta::FromMilliseconds(FLAGS_ysql_transaction_bg_task_wait_ms));
 

src/yb/tools/yb-admin-snapshot-schedule-test.cc
@@ -4626,5 +4626,35 @@ TEST_F_EX(YbAdminSnapshotScheduleTest, YB_DISABLE_TEST_IN_TSAN(CreateDuplicateSc
     "already exists for the given keyspace ysql." + kTableName.namespace_name());
 }
 
+class YbAdminSnapshotScheduleTestWithYsqlTransactionalDDL
+    : public YbAdminSnapshotScheduleTestWithYsql {
+ public:
+  std::vector<std::string> ExtraMasterFlags() override {
+    // To speed up tests.
+    return { "--snapshot_coordinator_cleanup_delay_ms=1000",
+             "--snapshot_coordinator_poll_interval_ms=500",
+             "--enable_transactional_ddl_gc=true" };
+  }
+};
+
+TEST_F_EX(YbAdminSnapshotScheduleTest, YB_DISABLE_TEST_IN_TSAN(BeforeCreateFinishes),
+          YbAdminSnapshotScheduleTestWithYsqlTransactionalDDL) {
+  auto schedule_id = ASSERT_RESULT(PreparePg(YsqlColocationConfig::kDBColocated));
+
+  auto conn = ASSERT_RESULT(PgConnect(kTableName.namespace_name()));
+  ASSERT_OK(conn.ExecuteFormat("CREATE TABLE demo_new (id INT, name TEXT)"));
+
+  // We want to restore to time before table was created.
+  auto time = ASSERT_RESULT(GetCurrentTime());
+  // Skip txn verification at the master so that txn metadata remains.
+  ASSERT_OK(cluster_->SetFlagOnMasters("TEST_skip_transaction_verification", "true"));
+  ASSERT_OK(conn.ExecuteFormat("CREATE TABLE demo (id INT, name TEXT)"));
+
+  // Reset the flag.
+  ASSERT_OK(cluster_->SetFlagOnMasters("TEST_skip_transaction_verification", "false"));
+  // Restore to this time. There should be no fatals.
+  ASSERT_OK(RestoreSnapshotSchedule(schedule_id, time));
+}
+
 }  // namespace tools
 }  // namespace yb

commit eca5c13e26a30735741df1f8fe97d821f72f9307

[16033] DST: Fix *PgRegress* tests post commit 6abc1af5af26b3236c751c9eed153cfd9c0d5de8
Summary:
Commit 6abc1af5af26b3236c751c9eed153cfd9c0d5de8 caused several *PgRegress* tests like
testPgRegressExtension and testPgRegressTable to fail. These tests were issuing
multiple delete index requests including duplicate delete index requests even after deleting an index.
Before commit 6abc1af5af26b3236c751c9eed153cfd9c0d5de8, delete table was idempotent i.e. trying to
delete an already deleted table would cause no change in the cluster and would populate the
`DeleteTableResponsePB` properly that was used by pg clients to invalidate cache, etc. However,
hiding an already hidden table was not idempotent which is what the diff was trying to fix.

In the commit, I had added a top level check to ignore delete table request if a table was already
hidden OR deleted. However, I was not populating the `DeleteTableResponsePB` properly causing these
tests to fail. This diff fixes it by keeping the top level check and returning early only if table
is already hidden. If it is already deleted then we now take the same route as before commit
6abc1af5af26b3236c751c9eed153cfd9c0d5de8.

Test Plan: Jenkins

Reviewers: zdrudi

Reviewed By: zdrudi

Subscribers: bogdan, slingam, ybase

Differential Revision: https://phabricator.dev.yugabyte.com/D22875

src/yb/master/catalog_entity_info.h
@@ -463,6 +463,16 @@ class TableInfo : public RefCountedThreadSafe<TableInfo>,
     return !l->started_hiding_or_deleting();
   }
 
+  // If the table is already hidden then treat it as a duplicate hide request.
+  bool IgnoreHideRequest() {
+    auto l = LockForRead();
+    if (l->started_hiding()) {
+      LOG(INFO) << "Table " << id() << " is already hidden. Duplicate request.";
+      return true;
+    }
+    return false;
+  }
+
   std::string ToString() const override;
   std::string ToStringWithState() const;
 

src/yb/master/catalog_manager.cc
@@ -5787,7 +5787,7 @@ Status CatalogManager::DeleteTable(
     return Status::OK();
   }
 
-  if (CatalogManagerUtil::IsDuplicateDeleteTableRequest(table)) {
+  if (table->IgnoreHideRequest()) {
     return Status::OK();
   }
 
@@ -5900,7 +5900,7 @@ Status CatalogManager::DeleteTableInternal(
 
   scoped_refptr<TableInfo> table = VERIFY_RESULT(FindTable(req->table()));
 
-  if (CatalogManagerUtil::IsDuplicateDeleteTableRequest(table)) {
+  if (table->IgnoreHideRequest()) {
     return Status::OK();
   }
 

src/yb/master/catalog_manager_util.h
@@ -176,16 +176,6 @@ class CatalogManagerUtil {
       const Schema& schema, uint32_t schema_version, const PartitionSchema& partition_schema,
       tablet::TableInfoPB* pb);
 
-  // If the table is already hidden or deleted then treats it as a duplicate delete request.
-  // Don't invoke this function if attempting to hard delete an already hidden table.
-  static bool IsDuplicateDeleteTableRequest(const scoped_refptr<TableInfo>& table) {
-    if (!table->IsOperationalForClient()) {
-      LOG(INFO) << "Table " << table->id() << " is already deleted. Duplicate request.";
-      return true;
-    }
-    return false;
-  }
-
  private:
   CatalogManagerUtil();
 