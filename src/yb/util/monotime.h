90a3030bf7435c6cbbedcb911ac6c451e7477469
Anton Rybochkin <79331145+arybochkin@users.noreply.github.com>
2023-02-17 12:04:39 +0100

    [BACKPORT 2.14][#16140] docdb: Bump RBS session default timeout
    Summary:
    Original commit: b1a605ecbb651fd4c6e12a1f99e57a03b912a650 / D23002
    
    Current default timeout for RBS session is 180 seconds. This timeout is applied for local bootstrap
    part of RBSing. The longest local bootstrap post-RBS has been seen is about 1 hour. Thus, as a
    short term solution it is required to bump default timeout for local bootstrap post-RBS to prevent
    RBS session removing while the local bootstrap is still on the fly. Hence, changing the value of
    `remote_bootstrap_idle_timeout_ms` from 180000 milliseconds to 2 hours.
    Additional side effect is that the timeout for downloading of any file is also increased to 2 hours,
    as the gflags also controls this timeout. In any case WALs are kept anchored even in case of any
    issues at downloading part, so there's no risk of GCing.
    Longer term, we could evaluate options in #15877, such as keeping the session alive, as long as
    the destination is actively making progress on bootstrapping.
    
    Test Plan: Jenkins
    
    Reviewers: sergei, bogdan
    
    Reviewed By: bogdan
    
    Subscribers: ybase, timur, amitanand, bkolagani
    
    Differential Revision: https://phabricator.dev.yugabyte.com/D23352

--- src/yb/util/monotime.h
+++ src/yb/util/monotime.h
@@ -148,12 +148,19 @@ class MonoTime {
   static constexpr int64_t kMicrosecondsPerMillisecond = 1000L;
   static constexpr int64_t kMillisecondsPerSecond = 1000L;
   static constexpr int64_t kSecondsPerMinute = 60L;
+  static constexpr int64_t kMinutesPerHour = 60L;
 
-  static constexpr int64_t kNanosecondsPerMillisecond =
-      kNanosecondsPerMicrosecond * kMicrosecondsPerMillisecond;
+  static constexpr int64_t kMillisecondsPerMinute =
+      kMillisecondsPerSecond * kSecondsPerMinute;
+
+  static constexpr int64_t kMillisecondsPerHour =
+      kMillisecondsPerMinute * kMinutesPerHour;
 
   static constexpr int64_t kMicrosecondsPerSecond =
       kMillisecondsPerSecond * kMicrosecondsPerMillisecond;
+
+  static constexpr int64_t kNanosecondsPerMillisecond =
+      kNanosecondsPerMicrosecond * kMicrosecondsPerMillisecond;
 
   static constexpr int64_t kNanosecondsPerSecond =
       kNanosecondsPerMillisecond * kMillisecondsPerSecond;
