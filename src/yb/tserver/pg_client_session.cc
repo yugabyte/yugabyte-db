27c222c33943201d4c72ad746a09186c551b6ea0
Piyush Jain <piyushjain1996@gmail.com>
2023-02-21 14:03:46 -0800

    [#16034, #15933] YSQL: Avoid picking hybrid timestamps in YSQL backends
    Summary:
    As part of commit c5f51258, a major refactor was done in 2.14 to move the
    YSQL-to-tserver clients from YSQL backend processes into the same node's
    tserver process. This was done because before the commit, each YSQL backend
    would have a connection to each tserver (for issuing reads/ writes) resulting in
    a lot of connections in the cluster. After the commit, all YSQL backends would
    only talk to a proxy running on the local node's tserver process which would
    forward the request to the required tserver. This eliminated the need for a
    connection from each Postgres backend to each tserver on the cluster. A new
    PgClientService was introduced on the tserver process to serve as the proxy.
    
    This refactor resulted in a subtle regression where in the hybrid time wasn't
    propagated from the transaction participants (i.e., tservers where reads/ writes
    were performed) to the YSQL backends. The hybrid time was propagated from these
    tservers to the PgClientService proxies, but not to the YSQL backends.
    
    This results in certain scenarios where clock skew can cause data visibility
    issues. One such visibility bug occurs as follows:
    
    (1) Assume a main table `tbl` (k primary key, v int) and a secondary
    index `idx` on v.
    (2) If an update was performed to a main table `tbl` such that
    it sets v to the same value, it would still update the index by performing a
    DELETE on the index key and then re-INSERTing the index key.
    (3) Assume the update to the main table is being handled by query layer on
    node N1.
    (4) Assume the physical clock on N1 is lagging behind the physical clock on
    the tserver where the index DELETE is performed (say N2).
    (5) Since the `propagated_hybrid_time` is no longer sent from N2 to the YSQL
    query layer on N1, the insert rpc picks an `in_txn_limit` using the local
    clock which gives a value lower than the DELETE rpc's write hybrid time.
    
    See ReadHybridTimePB and src/yb/yql/pggate/README.txt for information on
    in_txn_limit.
    
    Not propagating the hybrid time from tservers serving reads/ writes to the YSQL
    query layer might result in other unknown issues. Propagating hybrid times
    helps ensure that we maintain causality information i.e., if
    event A on some node (occurring at hybrid time ht_a) leads to event B on
    another node, event B's code path, for any purpose, will always pick a
    hybrid time > ht_a. In the above example, event A is the write and event B
    is the INSERT code path on the query layer which picks a hybrid time for
    `in_txn_limit` which is < ht_a.
    
    This issue can be solved in 2 ways -
    (1) Propagate hybrid times between YSQL and the local node's tserver proxy.
    (2) Move all logic that picks hybrid times to the local node's tserver proxy.
    This ensures that any hybrid time picked for fields like in_txn_limit have the
    causal knowledge of past events backed in.
    
    We are moving forward with approach (2).
    
    The diff removes usage of HybridClock in YSQL for picking `in_txn_limit`. There
    are two other instances in YSQL where HybridClock is used, but they will be
    removed in future since they are known to be safe.
    
    Test Plan:
    Jenkins: urgent
    ./yb_build.sh --cxx-test pgwrapper_pg_clock_skew-test --gtest_filter PgClockSkewTest.InTxnLimitBug
    
    Reviewers: dmitry, sergei
    
    Reviewed By: dmitry, sergei
    
    Subscribers: mbautin, dnolan, yql, bogdan
    
    Differential Revision: https://phabricator.dev.yugabyte.com/D23075

--- src/yb/tserver/pg_client_session.cc
+++ src/yb/tserver/pg_client_session.cc
@@ -325,6 +325,7 @@ struct PerformData {
   PgTableCache* table_cache;
   PgClientSession::UsedReadTimePtr used_read_time;
   PgResponseCache::Setter cache_setter;
+  HybridTime used_in_txn_limit;
 
   void FlushDone(client::FlushStatus* flush_status) {
     auto status = CombineErrorsToStatus(flush_status->errors, flush_status->status);
@@ -376,6 +377,9 @@ struct PerformData {
       }
       op_resp.set_partition_list_version(op->table()->GetPartitionListVersion());
     }
+    if (used_in_txn_limit) {
+      resp->set_used_in_txn_limit_ht(used_in_txn_limit.ToUint64());
+    }
 
     return Status::OK();
   }
@@ -387,6 +391,14 @@ client::YBSessionPtr CreateSession(
   result->SetForceConsistentRead(client::ForceConsistentRead::kTrue);
   result->set_allow_local_calls_in_curr_thread(false);
   return result;
+}
+
+HybridTime GetInTxnLimit(const PgPerformOptionsPB& options, ClockBase* clock) {
+  if (!options.has_in_txn_limit_ht()) {
+    return HybridTime();
+  }
+  auto in_txn_limit = HybridTime::FromPB(options.in_txn_limit_ht().value());
+  return in_txn_limit ? in_txn_limit : clock->Now();
 }
 
 } // namespace
@@ -731,6 +743,7 @@ Status PgClientSession::FinishTransaction(
 
 Status PgClientSession::Perform(
     PgPerformRequestPB* req, PgPerformResponsePB* resp, rpc::RpcContext* context) {
+  VLOG_WITH_PREFIX(5) << "Perform req=" << req->ShortDebugString();
   PgResponseCache::Setter setter;
   auto& options = *req->mutable_options();
   if (options.has_caching_info()) {
@@ -741,7 +754,9 @@ Status PgClientSession::Perform(
     }
   }
 
-  auto session_info = VERIFY_RESULT(SetupSession(*req, context->GetClientDeadline()));
+  const auto in_txn_limit = GetInTxnLimit(options, clock_.get());
+  VLOG_WITH_PREFIX(5) << "using in_txn_limit_ht: " << in_txn_limit;
+  auto session_info = VERIFY_RESULT(SetupSession(*req, context->GetClientDeadline(), in_txn_limit));
   auto* session = session_info.first.session.get();
   auto ops = VERIFY_RESULT(PrepareOperations(req, session, &context->sidecars(), &table_cache_));
   auto ops_count = ops.size();
@@ -752,7 +767,8 @@ Status PgClientSession::Perform(
     .ops = std::move(ops),
     .table_cache = &table_cache_,
     .used_read_time = session_info.second,
-    .cache_setter = std::move(setter)
+    .cache_setter = std::move(setter),
+    .used_in_txn_limit = in_txn_limit
   });
 
   auto transaction = session_info.first.transaction;
@@ -830,7 +846,8 @@ Status PgClientSession::UpdateReadPointForXClusterConsistentReads(
 }
 
 Result<std::pair<PgClientSession::SessionData, PgClientSession::UsedReadTimePtr>>
-PgClientSession::SetupSession(const PgPerformRequestPB& req, CoarseTimePoint deadline) {
+PgClientSession::SetupSession(
+    const PgPerformRequestPB& req, CoarseTimePoint deadline, HybridTime in_txn_limit) {
   const auto& options = req.options();
   PgClientSessionKind kind;
   if (options.use_catalog_session()) {
@@ -901,12 +918,12 @@ PgClientSession::SetupSession(const PgPerformRequestPB& req, CoarseTimePoint dea
     session->DeferReadPoint();
   }
 
+  // TODO: Reset in_txn_limit which might be on session from past Perform? Not resetting will not
+  // cause any issue, but should we reset for safety?
   if (!options.ddl_mode() && !options.use_catalog_session()) {
     txn_serial_no_ = options.txn_serial_no();
-
-    const auto in_txn_limit = HybridTime::FromPB(options.in_txn_limit_ht());
     if (in_txn_limit) {
-      VLOG_WITH_PREFIX(3) << "In txn limit: " << in_txn_limit;
+      // TODO: Shouldn't the below logic for DDL transactions as well?
       session->SetInTxnLimit(in_txn_limit);
     }
   }
