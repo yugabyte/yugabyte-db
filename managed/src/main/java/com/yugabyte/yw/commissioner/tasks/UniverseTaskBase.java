8ce6b0b31578f14204974672b44030eb83caf2c5
chandrakant vankayalapati <cvankayalpati@yugabyte.com>
2023-02-22 14:19:01 -0700

    [PLAT-7455] Add runtime config at the provider level for enabling node-agent
    Summary: This diff includes changes to allow users to enable node-agent at provider level using runtime config.
    
    Test Plan:
    1. Created universe succesfully using existing ssh based ansible flow with flags disabled.
    2. Node agent was installed on the DB node when node-agent flags were enabled. Ansible
    started using node_agent_rpc connection to run playbooks on the DB node. However, universe creation
    requires further testing.
    
    Reviewers: nbhatia, nsingh
    
    Reviewed By: nsingh
    
    Subscribers: yugaware
    
    Differential Revision: https://phabricator.dev.yugabyte.com/D23106

--- managed/src/main/java/com/yugabyte/yw/commissioner/tasks/UniverseTaskBase.java
+++ managed/src/main/java/com/yugabyte/yw/commissioner/tasks/UniverseTaskBase.java
@@ -1036,14 +1036,16 @@ public abstract class UniverseTaskBase extends AbstractTaskBase {
     SubTaskGroup subTaskGroup =
         getTaskExecutor().createSubTaskGroup(InstallNodeAgent.class.getSimpleName(), executor);
     NodeAgentManager nodeAgentManager = application.injector().instanceOf(NodeAgentManager.class);
-    if (nodeAgentManager.isServerToBeInstalled()) {
-      Universe universe = getUniverse();
-      for (NodeDetails node : nodes) {
-        if (node.cloudInfo == null) {
-          continue;
-        }
-        Cluster cluster = getUniverse().getCluster(node.placementUuid);
-        Provider provider = Provider.getOrBadRequest(UUID.fromString(cluster.userIntent.provider));
+    boolean createSubTaskGroup = false;
+    Universe universe = getUniverse();
+    for (NodeDetails node : nodes) {
+      if (node.cloudInfo == null) {
+        continue;
+      }
+      Cluster cluster = getUniverse().getCluster(node.placementUuid);
+      Provider provider = Provider.getOrBadRequest(UUID.fromString(cluster.userIntent.provider));
+      if (nodeAgentManager.isServerToBeInstalled(provider)) {
+        createSubTaskGroup = true;
         if (provider.getCloudCode() == CloudType.onprem) {
           AccessKey accessKey =
               AccessKey.getOrBadRequest(provider.uuid, cluster.userIntent.accessKeyCode);
@@ -1065,6 +1067,8 @@ public abstract class UniverseTaskBase extends AbstractTaskBase {
         task.initialize(params);
         subTaskGroup.addSubTask(task);
       }
+    }
+    if (createSubTaskGroup) {
       getRunnableTask().addSubTaskGroup(subTaskGroup);
     }
     return subTaskGroup;
@@ -1074,11 +1078,15 @@ public abstract class UniverseTaskBase extends AbstractTaskBase {
     SubTaskGroup subTaskGroup =
         getTaskExecutor().createSubTaskGroup(WaitForNodeAgent.class.getSimpleName(), executor);
     NodeAgentClient nodeAgentClient = application.injector().instanceOf(NodeAgentClient.class);
-    if (nodeAgentClient.isClientEnabled()) {
-      for (NodeDetails node : nodes) {
-        if (node.cloudInfo == null) {
-          continue;
-        }
+    boolean createSubTaskGroup = false;
+    for (NodeDetails node : nodes) {
+      if (node.cloudInfo == null) {
+        continue;
+      }
+      Cluster cluster = getUniverse().getCluster(node.placementUuid);
+      Provider provider = Provider.getOrBadRequest(UUID.fromString(cluster.userIntent.provider));
+      if (nodeAgentClient.isClientEnabled(provider)) {
+        createSubTaskGroup = true;
         WaitForNodeAgent.Params params = new WaitForNodeAgent.Params();
         params.nodeName = node.nodeName;
         params.azUuid = node.azUuid;
@@ -1088,6 +1096,8 @@ public abstract class UniverseTaskBase extends AbstractTaskBase {
         task.initialize(params);
         subTaskGroup.addSubTask(task);
       }
+    }
+    if (createSubTaskGroup) {
       getRunnableTask().addSubTaskGroup(subTaskGroup);
     }
     return subTaskGroup;
