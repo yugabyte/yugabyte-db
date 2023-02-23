a4df471ce3dbb342adcda3d276d77a0f048f0a4c
Arunabh Das <pannasg@hotmail.com>
2023-02-23 17:54:29 +0800

    UPDATED Python Driver ANTLR 4.9.3 -> 4.11.1 (#706)

--- drivers/python/setup.py
+++ drivers/python/setup.py
@@ -29,7 +29,7 @@ setup(
     author_email     = 'rhizome.ai@gmail.com',
     url              = 'https://github.com/apache/age',
     license          = 'Apache2.0',
-    install_requires = [ 'psycopg2', 'antlr4-python3-runtime==4.9.3'],
+    install_requires = [ 'psycopg2', 'antlr4-python3-runtime==4.11.1'],
     packages         = ['age', 'age.gen'],
     keywords         = ['Graph Database', 'Apache AGE', 'PostgreSQL'],
     python_requires  = '>=3.9',
