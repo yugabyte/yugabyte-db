17ca0f7bc48a24cbb71ebafd44857db65c09d41f
saratkumar-yb <skarri@yugabyte.com>
2023-02-28 23:10:55 +0530

    Break release build_test scripts to two different scripts
    Summary: Break release build_test scripts to two different scripts (build & testing)
    
    Test Plan: Created a new release alma8 test job and phab db build test job on both old branches and master
    
    Reviewers: jharveysmith, steve.varnau
    
    Reviewed By: steve.varnau
    
    Subscribers: jenkins-bot, yugaware
    
    Differential Revision: https://phabricator.dev.yugabyte.com/D22071

--- build-support/jenkins/yb-jenkins-test.sh
+++ build-support/jenkins/yb-jenkins-test.sh
@@ -34,6 +34,8 @@ Environment variables:
   BUILD_TYPE
     Passed directly to build-and-test.sh. The default value is determined based on the job name
     if this environment variable is not specified or if the value is "auto".
+  YB_NUM_TESTS_TO_RUN
+    Maximum number of tests ctest should run before exiting. Used for testing Jenkins scripts.
 EOT
 }
 
@@ -62,18 +64,14 @@ echo "Build type: ${BUILD_TYPE}";
 
 set_compiler_type_based_on_jenkins_job_name
 
-echo "Deleting branches starting with 'arcpatch-D'"
-current_branch=$( git rev-parse --abbrev-ref HEAD )
-for branch_name in $( git for-each-ref --format="%(refname)" refs/heads/ ); do
-  branch_name=${branch_name#refs/heads/}
-  if [[ "$branch_name" =~ ^arcpatch-D ]]; then
-    if [ "$branch_name" == "$current_branch" ]; then
-      echo "'$branch_name' is the current branch, not deleting."
-    else
-      ( set -x; git branch -D "$branch_name" )
-    fi
+if [ -n "${YB_NUM_TESTS_TO_RUN:-}" ]; then
+
+  if [[ ! "$YB_NUM_TESTS_TO_RUN" =~ ^[0-9]+$ ]]; then
+    echo "Invalid number of tests to run: $YB_NUM_TESTS_TO_RUN" >&2
+    exit 1
   fi
-done
+  export EXTRA_TEST_FLAGS="-I1,$YB_NUM_TESTS_TO_RUN"
+fi
 
 export YB_MINIMIZE_VERSION_DEFINES_CHANGES=1
 export YB_MINIMIZE_RECOMPILATION=1
@@ -82,40 +80,10 @@ export YB_BUILD_JAVA=${YB_BUILD_JAVA:-1}
 export YB_BUILD_PYTHON=${YB_BUILD_PYTHON:-0}
 export YB_BUILD_CPP=${YB_BUILD_CPP:-1}
 
-echo
-echo ----------------------------------------------------------------------------------------------
-echo "ifconfig (without the 127.0.x.x IPs)"
-echo ----------------------------------------------------------------------------------------------
-echo
-
 set +e
-ifconfig | grep -vE "inet 127[.]0[.]"
-set -e
-
-echo
-echo ----------------------------------------------------------------------------------------------
-echo
-
-echo "Max number of open files:"
-ulimit -n
-echo
-
-show_disk_usage
-
-if is_mac; then
-  "$YB_BUILD_SUPPORT_DIR"/kill_long_running_minicluster_daemons.py
-fi
-
-set +e
-"$YB_BUILD_SUPPORT_DIR"/jenkins/build.sh
+"$YB_BUILD_SUPPORT_DIR"/jenkins/test.sh
 exit_code=$?
-set -e
 
-# Un-gzip build log files for easy viewing in the Jenkins UI.
-for f in build/debug/test-logs/*.txt.gz; do
-  if [ -f "$f" ]; then
-    gzip -d "$f"
-  fi
-done
+set -e
 
 exit $exit_code
