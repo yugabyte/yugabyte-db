5166e6c9fd9868158c3f9174a8e8a77ac20dff24
kkannan <kkannan@yugabyte.com>
2023-03-06 11:56:42 +0530

    [PLAT-5370][UI] Add Custom Variable Support for Email Notification Template
    Summary:
    Added:
    Custom variable creation modal.
    
    All features are behind feature flag
    
    [[ https://www.figma.com/file/Jahyk9bTcrjTF9kUDhMqgf/Custom-Notification-Template?node-id=382%3A32101&t=HgVWhKzUGqOnC2iR-0 | Figma ]]
    
    [[ https://docs.google.com/spreadsheets/d/1pRnOEqqQrY-KejW8q48B4WlTEm-gzF1C684w5XkyjqQ/edit?usp=sharing | Code Review Checklist ]]
    
    Test Plan:
    tested manually
    
    {F34930}
    {F34931}
    {F34932}
    {F34933}
    {F34934}
    {F34935}
    {F34936}
    
    Reviewers: jmak, rmadhavan, lsangappa, asathyan
    
    Reviewed By: lsangappa, asathyan
    
    Subscribers: jenkins-bot, yugaware
    
    Differential Revision: https://phabricator.dev.yugabyte.com/D23120

---
+++ managed/ui/src/redesign/features/alerts/TemplateComposer/CommonStyles.ts
@@ -0,0 +1,47 @@
+/*
+ * Created on Thu Feb 16 2023
+ *
+ * Copyright 2021 YugaByte, Inc. and Contributors
+ * Licensed under the Polyform Free Trial License 1.0.0 (the "License")
+ * You may not use this file except in compliance with the License. You may obtain a copy of the License at
+ * http://github.com/YugaByte/yugabyte-db/blob/master/licenses/POLYFORM-FREE-TRIAL-LICENSE-1.0.0.txt
+ */
+
+import { makeStyles } from '@material-ui/core';
+
+/**
+ * default styles for Composer components
+ */
+export const useCommonStyles = makeStyles((theme) => ({
+  defaultBorder: {
+    border: `1px solid ${theme.palette.ybacolors.ybGrayHover}`
+  },
+  helpText: {
+    fontFamily: 'Inter',
+    fontWeight: 400,
+    lineHeight: `${theme.spacing(2)}px`,
+    color: '#67666C'
+  },
+  formErrText: {
+    color: theme.palette.error[500],
+    margin: 0,
+    fontSize: '11.5px',
+    fontWeight: 400,
+    lineHeight: `${theme.spacing(2)}px`
+  },
+  menuStyles: {
+    '& .MuiMenuItem-root': {
+      height: '50px',
+      minWidth: '190px'
+    },
+    '& .MuiMenuItem-root svg': {
+      marginRight: theme.spacing(1.4),
+      color: theme.palette.orange[500],
+      height: '20px',
+      width: '20px'
+    }
+  },
+  clickable: {
+    cursor: 'pointer'
+  }
+}));
