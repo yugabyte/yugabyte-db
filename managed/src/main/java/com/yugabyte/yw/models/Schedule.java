8727a84dcf6441d2131d8111cc29b5d7b9b4e6c7
Charles Wang <wang.charles234@gmail.com>
2023-03-06 12:07:37 -0800

    [PLAT-7365] Add scheduled backups, backups, storage configs to attach/detach
    Summary:
    Include scheduled backups + relevant storage configs in attach/detach tar gz
    - Allow for one stop detach -> attach from source to target platform
    
    Examples:
    Sample detach:
            python3 ./yb_attach_detach.py -u edf981dc-a11a-46d1-9518-c022b1f0bc80 detach
            -cs f33e3c9b-75ab-4c30-80ad-cba85646ea39  -ts ad09f10f-c377-4cdf-985c-898d13eae783
            -ps http://167.123.191.88:9000   -f /tmp/universe-export-spec.tar.gz -s
    
     Sample attach:
            python3 ./yb_attach_detach.py -u edf981dc-a11a-46d1-9518-c022b1f0bc80 attach
            -cd c920db92-063c-483e-839f-e8264dbe5a0d  -td 4abb151c-3020-43fb-bd04-f6a63934e4e5
            -pd http://10.150.7.155:9000   -f /tmp/universe-export-spec.tar.gz
    
    Sample one stop detach/attach:
            python3 ./yb_attach_detach.py -u edf981dc-a11a-46d1-9518-c022b1f0bc80 run
            -cs f33e3c9b-75ab-4c30-80ad-cba85646ea39  -ts ad09f10f-c377-4cdf-985c-898d13eae783
            -ps http://167.123.191.88:9000   -f /tmp/universe-export-spec.tar.gz -s
            -cd c920db92-063c-483e-839f-e8264dbe5a0d  -td 4abb151c-3020-43fb-bd04-f6a63934e4e5
            -pd http://10.150.7.155:9000
    
    Test Plan:
    Create GCP and AWS universes and for each universe on source platform:
    
    1. Add a backup config (GCS or s3) on source platform.
    2. Create a backup using backup config on source platform.
    3. Add a scheduled backup using the config on source platform.
    4. Run detach universe (see example in summary) using `yb_attach_detach.py` script
    5. Run attach universe (see example in summary) using `yb_attach_detach.py` script
    6. Perform a rolling restart on destination platform and make sure that universe works as expected.
    
    Instead of running steps 4-5, can instead use the `run` command in the `yb_attach_detach.py` script which will complete steps 4 and 5 in one go
    
    Reviewers: nsingh, sanketh
    
    Reviewed By: nsingh, sanketh
    
    Subscribers: jenkins-bot, yugaware
    
    Differential Revision: https://phabricator.dev.yugabyte.com/D23362

--- managed/src/main/java/com/yugabyte/yw/models/Schedule.java
+++ managed/src/main/java/com/yugabyte/yw/models/Schedule.java
@@ -15,6 +15,7 @@ import com.cronutils.parser.CronParser;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ObjectNode;
 import com.yugabyte.yw.commissioner.tasks.MultiTableBackup;
 import com.yugabyte.yw.common.PlatformServiceException;
 import com.yugabyte.yw.common.ScheduleUtil;
@@ -131,6 +132,12 @@ public class Schedule extends Model {
 
   public UUID getCustomerUUID() {
     return customerUUID;
+  }
+
+  public void setCustomerUUID(UUID customerUUID) {
+    this.customerUUID = customerUUID;
+    ObjectNode scheduleTaskParams = (ObjectNode) getTaskParams();
+    scheduleTaskParams.set("customerUUID", Json.toJson(customerUUID));
   }
 
   @ApiModelProperty(value = "Number of failed backup attempts", accessMode = READ_ONLY)
@@ -263,7 +270,6 @@ public class Schedule extends Model {
   public void setCronExpressionAndTaskParams(String cronExpression, ITaskParams params) {
     this.cronExpression = cronExpression;
     this.taskParams = Json.toJson(params);
-    save();
   }
 
   public void setFailureCount(int count) {
@@ -271,7 +277,6 @@ public class Schedule extends Model {
     if (count >= MAX_FAIL_COUNT) {
       this.status = State.Paused;
     }
-    save();
   }
 
   public void resetSchedule() {
@@ -315,7 +320,6 @@ public class Schedule extends Model {
 
   public void setRunningState(boolean state) {
     this.runningState = state;
-    save();
   }
 
   public void updateIncrementalBackupFrequencyAndTimeUnit(
@@ -448,6 +452,11 @@ public class Schedule extends Model {
         .eq("status", "Active")
         .eq("task_type", taskType)
         .findList();
+  }
+
+  public static List<Schedule> getAllSchedulesByOwnerUUIDAndType(
+      UUID ownerUUID, TaskType taskType) {
+    return find.query().where().eq("owner_uuid", ownerUUID).in("task_type", taskType).findList();
   }
 
   public static List<Schedule> getAllByCustomerUUIDAndType(UUID customerUUID, TaskType taskType) {
