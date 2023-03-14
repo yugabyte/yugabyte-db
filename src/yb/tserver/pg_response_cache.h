defa6c49d5880636e1f37355a8b4c9b4eab21d0d
Dmitry Uspenskiy <47734295+d-uspenskiy@users.noreply.github.com>
2023-03-14 13:09:51 +0300

    [#15761] YSQL: Renew request cache entry in case of `Snapshot too old` error
    Summary:
    The `Snapshot too old` error may happen in the following situation:
    - several days ago at time T1 first connection was established to DB
    - multiple (due to paging) responses for YSQL sys catalog cache were cached on a local tserver
       by the PgResponseCache. These responses has catalog_read_time equal to T1
    - later some (but not all) of these responses were discarded from the LRU cache
         in the PgResponseCache
    - new connection is establishing to same DB
    - PgResponseCache provides cached response for initial request with read time T1
    - PgResponseCache doesn't have cached responses for further requests with read time T1 and
         send read request to a Master
    - Master responds with 'Snapshot too old' error on attempt to read at really old read time T1
    
    The problem is solved by retrying cache build process with using different settings for reading from response cache.
    There are 4 strategy:
    - `TRUST_CACHE` (any entry in response cache is treated as valid)
    - `RENEW_CACHE_SOFT` (entries created less than 3mins (configurable by `pg_cache_response_renew_soft_lifetime_limit_ms` GFLAG) are treated as valid, other entries will be renewed)
    - `RENEW_CACHE_HARD` (no entries are treated as valid and will be renewed)
    - `NO_CACHE` (response cache will not be used at all)
    
    But default YSQL tries to build cache by using the `TRUST_CACHE` strategy. In case of getting `Snapshot too old` error YSQL will make another attempt with next strategy.
    
    It is expected that the `RENEW_CACHE_SOFT` strategy will solve `Snapshot too old`.
    `RENEW_CACHE_HARD` and `NO_CACHE` are used as a fallback options in `release` build just not to loose the ability to connect to DB in case of some unexpected state of the response cache.
    
    Test Plan:
    New unit test was introduced
    
    ```
    ./yb_build.sh --gtest_filter PgCatalogPerfTest.ResponseCacheWithTooOldSnapshot
    ```
    
    Reviewers: mihnea, sergei, tnayak, myang
    
    Reviewed By: myang
    
    Subscribers: ssong, bogdan
    
    Differential Revision: https://phabricator.dev.yugabyte.com/D22407

--- src/yb/tserver/pg_response_cache.h
+++ src/yb/tserver/pg_response_cache.h
@@ -36,26 +36,30 @@ class MetricEntity;
 
 namespace tserver {
 
-YB_STRONGLY_TYPED_BOOL(IsFailure);
-
 class PgResponseCache {
  public:
   explicit PgResponseCache(MetricEntity* metric_entity);
   ~PgResponseCache();
 
   struct Response {
-    Response(PgPerformResponsePB&& response_, std::vector<RefCntSlice>&& rows_data_)
-        : response(std::move(response_)), rows_data(std::move(rows_data_)) {
+    Response(
+        bool is_ok_status_, PgPerformResponsePB response_, std::vector<RefCntSlice> rows_data_)
+        : is_ok_status(is_ok_status_),
+          response(std::move(response_)),
+          rows_data(std::move(rows_data_)) {
       DCHECK_EQ(response.responses_size(), rows_data.size());
     }
 
+    bool is_ok_status;
     PgPerformResponsePB response;
     std::vector<RefCntSlice> rows_data;
   };
 
-  using Setter = std::function<void(Response&&, IsFailure)>;
+  using Setter = std::function<void(Response&&)>;
 
-  Setter Get(std::string&& cache_key, PgPerformResponsePB* response, rpc::RpcContext* context);
+  Result<Setter> Get(
+      PgPerformOptionsPB::CachingInfoPB* cache_info,
+      PgPerformResponsePB* response, rpc::RpcContext* context);
 
  private:
   class Impl;
