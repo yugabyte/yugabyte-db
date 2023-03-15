8a954766fb7af234ca6fa65094cca496fe4f65da
Emily Na <ena@yugabyte.com>
2023-03-15 15:33:34 -0400

    [BACKPORT 2.16][#16009] YSQL: Fix the system catalog versioning behaviour on CREATE TABLE with FK reference command
    Summary:
    Today CREATE TABLE with Foreign key (ie. the reference table) does not increment the system catalog version. The catalog version must be incremented so that the active Postgres connections refresh the Postgres cache and become  aware of the new foreign key relationship. This will allow incoming DML operations like insert or delete to adhere to the new constraint.
    
    The reason why catalog version is not incremented on this command is because when the statement runs, internally we have a nested subcommand (in this case AlterTable) which gets invoked to set the foreign key constraints.  Although the AlterTable subcommand determines the catalog version must be incremented, the result does not get persisted up to the top-level CreateStmt DDL. When we reach the top-level CreateStmt DDL, the catalog version is set to stay the same thus the version does not get incremented as it's supposed to.
    
    With this change, we persist the two flags `is_catalog_version_increment` and `is_breaking_catalog_change` inside the `DdlTransactionState` such that when the process utility is a top-level command or is atomic SPI command we increment the version and/or set it as breaking change. Only specific subcommand will trigger the flags to update such as with CREATE TABLE with FOREIGN KEY reference command. For example, we use the new field in the `DdlTransactionState` called `original_node_tag` to record the initial command as `CreateStmt`. Later when we're inside the subcommand `AlterStmt` and foreign key constraint gets calculated, we ensure to increment the catalog version.
    
    Original commit: `9ce3fa3052d5349c3ec65917b5d64db091297391` / `D23619`
    
    Test Plan:
    Updated the test file which tracks system catalog version on DDLs.
    
    Verified the behaviour on YSQL shell:
    
    // connection 1:
    create table p1 (col int unique);
    insert into p1 values(1);
    insert into p1 values(2);
    
    // connection 2:
    create table p2 (col int references p1(col));
    insert into p2 values(2);
    
    // connection 1:
    delete from p1 where col = 2; -- prior to this change the delete went through
    ERROR:  update or delete on table "p1" violates foreign key constraint "p2_col_fkey" on table "p2"
    DETAIL:  Key (col)=(2) is still referenced from table "p2".
    
    Reviewers: mihnea, myang, tverona
    
    Reviewed By: myang
    
    Subscribers: yql, tverona
    
    Differential Revision: https://phorge.dev.yugabyte.com/D26875

--- src/postgres/src/backend/commands/indexcmds.c
+++ src/postgres/src/backend/commands/indexcmds.c
@@ -1487,8 +1487,7 @@ DefineIndex(Oid relationId,
 	 * change.
 	 * TODO(jason): handle nested CREATE INDEX (this assumes we're at nest
 	 */
-	YBDecrementDdlNestingLevel(true /* is_catalog_version_increment */,
-	                           false /* is_breaking_catalog_change */);
+	YBDecrementDdlNestingLevel();
 	CommitTransactionCommand();
 
 	/* Delay after committing pg_index update. */
@@ -1506,7 +1505,8 @@ DefineIndex(Oid relationId,
 	pgstat_progress_update_param(PROGRESS_CREATEIDX_INDEX_OID,
 								 indexRelationId);
 
-	YBIncrementDdlNestingLevel();
+	YBIncrementDdlNestingLevel(true /* is_catalog_version_increment */,
+							   false /* is_breaking_catalog_change */);
 
 	/*
 	 * Update the pg_index row to mark the index as ready for inserts.
@@ -1520,8 +1520,7 @@ DefineIndex(Oid relationId,
 	 * TODO(jason): handle nested CREATE INDEX (this assumes we're at nest
 	 * level 1).
 	 */
-	YBDecrementDdlNestingLevel(true /* is_catalog_version_increment */,
-	                           false /* is_breaking_catalog_change */);
+	YBDecrementDdlNestingLevel();
 	CommitTransactionCommand();
 
 	/* Delay after committing pg_index update. */
@@ -1532,7 +1531,8 @@ DefineIndex(Oid relationId,
 									"concurrent index backfill");
 
 	StartTransactionCommand();
-	YBIncrementDdlNestingLevel();
+	YBIncrementDdlNestingLevel(true /* is_catalog_version_increment */,
+							   false /* is_breaking_catalog_change */);
 
 	if (IsYugaByteEnabled())
 		pgstat_progress_update_param(PROGRESS_CREATEIDX_PHASE,
--- src/postgres/src/test/regress/sql/yb_catalog_version.sql
+++ src/postgres/src/test/regress/sql/yb_catalog_version.sql
@@ -40,8 +40,10 @@ SELECT * FROM pg_yb_catalog_version;
 REVOKE CONNECT ON DATABASE cv_test_database from cv_test_role;
 SELECT * FROM pg_yb_catalog_version;
 
--- The next CREATE TABLE will increment current_version.
+-- The next CREATE TABLE should not cause any catalog version change.
 CREATE TABLE cv_test_table(id int);
+SELECT * FROM pg_yb_catalog_version;
+
 -- The next GRANT SELECT will increment current_version.
 GRANT SELECT ON cv_test_table TO cv_test_role;
 SELECT * FROM pg_yb_catalog_version;
@@ -231,4 +233,84 @@ SELECT * FROM pg_yb_catalog_version;
 
 -- The next REVOKE USAGE should not cause any catalog version change.
 REVOKE USAGE ON TYPE cv_test_type from cv_test_role;
+SELECT * FROM pg_yb_catalog_version;
+
+-- Tables with various constraint types should not increment catalog version.
+CREATE TABLE t_check (col INT CHECK (col > 0));
+SELECT * FROM pg_yb_catalog_version;
+CREATE TABLE t_not_null (col INT NOT NULL);
+SELECT * FROM pg_yb_catalog_version;
+CREATE TABLE t_primary_key (col INT PRIMARY KEY);
+SELECT * FROM pg_yb_catalog_version;
+CREATE TABLE t_sequence (col SERIAL, value TEXT);
+SELECT * FROM pg_yb_catalog_version;
+CREATE TABLE t_unique (col INT UNIQUE);
+SELECT * FROM pg_yb_catalog_version;
+CREATE TABLE t_identity (col INT GENERATED ALWAYS AS IDENTITY);
+SELECT * FROM pg_yb_catalog_version;
+CREATE TABLE t_primary_key_sequence_identity (c1 INT PRIMARY KEY, c2 SERIAL, c3 INT GENERATED ALWAYS AS IDENTITY);
+SELECT * FROM pg_yb_catalog_version;
+
+-- The CREATE TABLE with FOREIGN KEY will increment current_version.
+CREATE TABLE t1 (col INT UNIQUE);
+CREATE TABLE t2 (col INT REFERENCES t1(col));
+SELECT * FROM pg_yb_catalog_version;
+
+-- The ALTER TABLE will increment current_version.
+ALTER TABLE t1 ADD COLUMN val INT;
+SELECT * FROM pg_yb_catalog_version;
+
+-- The CREATE PROCEDURE will not increment current_version.
+CREATE PROCEDURE test() AS $$ BEGIN INSERT INTO t1 VALUES(1); END; $$ LANGUAGE 'plpgsql';
+SELECT * FROM pg_yb_catalog_version;
+-- The CALL to PROCEDURE will not increment current_version.
+CALL test();
+SELECT * FROM pg_yb_catalog_version;
+
+-- The CREATE FUNCTION will increment current_version.
+CREATE TABLE evt_trig_table (id serial PRIMARY KEY, evt_trig_name text);
+CREATE OR REPLACE FUNCTION evt_trig_fn() RETURNS event_trigger AS $$ BEGIN INSERT INTO evt_trig_table (evt_trig_name) VALUES (TG_EVENT); END; $$ LANGUAGE plpgsql;
+SELECT * FROM pg_yb_catalog_version;
+-- The CREATE EVENT TRIGGER will increment current_version.
+CREATE EVENT TRIGGER evt_ddl_start ON ddl_command_start EXECUTE PROCEDURE evt_trig_fn();
+SELECT * FROM pg_yb_catalog_version;
+-- The DDLs proceeding the trigger will increment current_version based on the command's individual behaviour.
+-- The ALTER TABLE will increment current_version.
+ALTER TABLE evt_trig_table ADD COLUMN val INT;
+SELECT * FROM pg_yb_catalog_version;
+-- The CREATE TABLE will not increment current_version.
+CREATE TABLE post_trigger_table (id INT);
+SELECT * FROM pg_yb_catalog_version;
+
+-- The execution on atomic SPI context function will increment current_version.
+CREATE FUNCTION atomic_spi(INT) RETURNS INT AS $$
+DECLARE TOTAL INT;
+BEGIN
+  CREATE TEMP TABLE temp_table(a INT);
+  INSERT INTO temp_table VALUES($1);
+  INSERT INTO temp_table VALUES(1);
+  SELECT SUM(a) INTO TOTAL FROM temp_table;
+  DROP TABLE temp_table;
+  RETURN TOTAL;
+END
+$$ LANGUAGE PLPGSQL;
+
+SELECT atomic_spi(1);
+SELECT * FROM pg_yb_catalog_version;
+SELECT atomic_spi(2);
+SELECT * FROM pg_yb_catalog_version;
+
+-- The execution on non-atomic SPI context will not increment current_version.
+CREATE TABLE non_atomic_spi(a INT, b INT);
+CREATE PROCEDURE proc(x INT, y INT) LANGUAGE PLPGSQL AS $$
+BEGIN FOR i IN 0..x LOOP INSERT INTO non_atomic_spi(a, b) VALUES (i, y);
+IF i % 2 = 0 THEN COMMIT;
+ELSE ROLLBACK;
+END IF;
+END LOOP;
+END $$;
+CREATE PROCEDURE p1() LANGUAGE PLPGSQL AS $$ BEGIN CALL p2(); END $$;
+CREATE PROCEDURE p2() LANGUAGE PLPGSQL AS $$ BEGIN CALL proc(2, 2); END $$;
+
+CALL p1();
 SELECT * FROM pg_yb_catalog_version;
