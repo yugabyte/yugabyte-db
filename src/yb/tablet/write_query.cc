5345a8fa205a7fd10b3282e49f04f928354c8fc6
rsami <rsami@yugabyte.com>
2023-03-15 17:38:05 -0400

    [#13578] DocDB: Push commit/abort signals from transaction participant to wait queue
    Summary:
    In case a transaction is committed, the transaction coordinator will send an UpdateTransaction request to each participating tablet. When the transaction participant processes this RPC, we can signal to the wait queue that such transaction was committed in case the wait queue is managing any waiting transactions blocked on this one. This should be more performant than relying on periodic call of WaitQueue::Poll to detect that a blocker is committed and unblock its waiters.
    
    In case a transaction is aborted, the query layer client will send an UpdateTransaction request with status IMMEDIATE_CLEANUP to every involved transaction participant. In such a case we can similarly signal to the wait queue that this transaction was aborted.
    
    In order to ensure a re-run of conflict resolution sees the latest signaled changes, we also modify the contract between conflict resolution and wait queue code to allow the wait queue to advance the resolution_ht used by conflict resolution beyond the ht of the commit/abort which triggered the waiter to be re-run.
    
    Given these changes, we can achieve high fairness in most normal workloads. For sufficiently contentious workloads, we need to set `wait_queue_poll_interval_ms` to a fairly small setting to maintain fairness. Immediate follow-up work will reduce this dependency on `wait_queue_poll_interval_ms`: see https://github.com/yugabyte/yugabyte-db/issues/16440
    
    This commit includes another change to refactor the wait queue to be owned by the transaction participant to resolve some lifetime issues with this signaling approach.
    
    Test Plan: Jenkins: hot
    
    Reviewers: bkolagani, sergei
    
    Reviewed By: sergei
    
    Subscribers: pjain, bogdan
    
    Differential Revision: https://phabricator.dev.yugabyte.com/D23614

--- src/yb/tablet/write_query.cc
+++ src/yb/tablet/write_query.cc
@@ -507,6 +507,11 @@ Status WriteQuery::DoExecute() {
   TEST_SYNC_POINT("WriteQuery::DoExecute::PreparedDocWriteOps");
 
   auto* transaction_participant = tablet->transaction_participant();
+  docdb::WaitQueue* wait_queue = nullptr;
+
+  if (transaction_participant) {
+    wait_queue = transaction_participant->wait_queue();
+  }
 
   if (!tablet->txns_enabled() || !transactional_table) {
     CompleteExecute();
@@ -515,13 +520,12 @@ Status WriteQuery::DoExecute() {
 
   if (isolation_level_ == IsolationLevel::NON_TRANSACTIONAL) {
     auto now = tablet->clock()->Now();
-    auto conflict_management_policy = GetConflictManagementPolicy(
-        tablet->wait_queue(), write_batch);
+    auto conflict_management_policy = GetConflictManagementPolicy(wait_queue, write_batch);
     return docdb::ResolveOperationConflicts(
         doc_ops_, conflict_management_policy, now, tablet->doc_db(),
         partial_range_key_intents, transaction_participant,
         tablet->metrics()->transaction_conflicts.get(), &prepare_result_.lock_batch,
-        tablet->wait_queue(),
+        wait_queue,
         [this, now](const Result<HybridTime>& result) {
           if (!result.ok()) {
             ExecuteDone(result.status());
@@ -552,8 +556,7 @@ Status WriteQuery::DoExecute() {
     }
   }
 
-  auto conflict_management_policy = GetConflictManagementPolicy(
-      tablet->wait_queue(), write_batch);
+  auto conflict_management_policy = GetConflictManagementPolicy(wait_queue, write_batch);
 
   // TODO(wait-queues): Ensure that wait_queue respects deadline() during conflict resolution.
   return docdb::ResolveTransactionConflicts(
@@ -561,7 +564,7 @@ Status WriteQuery::DoExecute() {
       read_time_ ? read_time_.read : HybridTime::kMax,
       tablet->doc_db(), partial_range_key_intents,
       transaction_participant, tablet->metrics()->transaction_conflicts.get(),
-      &prepare_result_.lock_batch, tablet->wait_queue(),
+      &prepare_result_.lock_batch, wait_queue,
       [this](const Result<HybridTime>& result) {
         if (!result.ok()) {
           ExecuteDone(result.status());
