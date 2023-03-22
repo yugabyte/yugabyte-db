bb45f78f0a54258ce103e70f835a8b24b9cd6bb2
Hamidreza Zare <hzare@yugabyte.com>
2023-03-22 14:48:54 -0400

    [PLAT-7952] Fix showing null value for chart path if it does not exist
    Summary:
    Changes in 6d332e5396b50604bee3a55794dbd8bea2132936 / D22087 set helmChart
    to string "null" if it is not specified during release creation. It should set it to null value so
    the helmChart field will be removed during serialization.
    
    Test Plan:
    1.Created releases without specifying helmChart and saw that it is set to "null" string.
    After restarting YBA, those fields were removed.
    2.Verified new release without specifying helmChart will not set helmChart to "null".
    
    Reviewers: rmadhavan, jmak, sanketh, nsingh, sb-yb
    
    Reviewed By: nsingh, sb-yb
    
    Subscribers: sb-yb, yugaware
    
    Differential Revision: https://phabricator.dev.yugabyte.com/D23803

---
+++ managed/src/main/java/db/migration/default_/common/V246__Remove_Null_Str_From_ChartPath.java
@@ -0,0 +1,62 @@
+// Copyright (c) YugaByte, Inc.
+
+package db.migration.default_.common;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.Iterator;
+import java.util.Map.Entry;
+import java.util.Objects;
+import java.util.Optional;
+import org.flywaydb.core.api.migration.jdbc.BaseJdbcMigration;
+import play.libs.Json;
+
+public class V246__Remove_Null_Str_From_ChartPath extends BaseJdbcMigration {
+
+  @Override
+  public void migrate(Connection connection) throws Exception {
+    ResultSet softwareReleasesResult =
+        connection
+            .createStatement()
+            .executeQuery("SELECT value FROM yugaware_property WHERE name = 'SoftwareReleases'");
+    while (softwareReleasesResult.next()) {
+      String softwareReleasesString = softwareReleasesResult.getString("value");
+      if (softwareReleasesString != null) {
+        JsonNode softwareReleasesJsonNode = Json.parse(softwareReleasesString);
+        if (softwareReleasesJsonNode.isObject()) {
+          ObjectNode updatedSoftwareReleases = Json.newObject();
+          ObjectNode softwareReleases = (ObjectNode) softwareReleasesJsonNode;
+          Iterator<Entry<String, JsonNode>> releaseIterator = softwareReleases.fields();
+          while (releaseIterator.hasNext()) {
+            Entry<String, JsonNode> release = releaseIterator.next();
+            String version = release.getKey();
+            if (release.getValue().isObject()) {
+              ObjectNode releaseObject = (ObjectNode) release.getValue();
+              Optional.ofNullable(releaseObject.get("chartPath"))
+                  .ifPresent(
+                      chartPath -> {
+                        if (Objects.equals(chartPath.asText(), "null")) {
+                          releaseObject.remove("chartPath");
+                        }
+                      });
+              updatedSoftwareReleases.set(version, releaseObject);
+            } else {
+              updatedSoftwareReleases.set(version, release.getValue());
+            }
+          }
+
+          PreparedStatement updateSoftwareReleasesStmt =
+              connection.prepareStatement("UPDATE yugaware_property SET value = ? WHERE name = ?");
+          updateSoftwareReleasesStmt.setString(1, Json.stringify(updatedSoftwareReleases));
+          updateSoftwareReleasesStmt.setString(2, "SoftwareReleases");
+          updateSoftwareReleasesStmt.executeUpdate();
+          updateSoftwareReleasesStmt.close();
+        }
+      }
+    }
+    softwareReleasesResult.close();
+  }
+}
