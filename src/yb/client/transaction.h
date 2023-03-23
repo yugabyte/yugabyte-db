14f74ec0261a8bd0fb1aeb3c9dc886a81fef9c5f
Piyush Jain <piyushjain1996@gmail.com>
2023-03-23 10:21:47 -0700

    [#15014, #14333] YSQL: Refactor some code to honour coding best pratices
    Summary: Fix code style errors in commit 28415cb8.
    
    Test Plan: Jenkins
    
    Reviewers: hsunder, dmitry
    
    Reviewed By: hsunder, dmitry
    
    Subscribers: yql, bogdan
    
    Differential Revision: https://phabricator.dev.yugabyte.com/D23866

--- src/yb/client/transaction.h
+++ src/yb/client/transaction.h
@@ -179,11 +179,11 @@ class YBTransaction : public std::enable_shared_from_this<YBTransaction> {
   void SetLogPrefixTag(const LogPrefixName& name, uint64_t value);
 
   void IncreaseMutationCounts(
-      SubTransactionId subtxn_id, const TableId& table_id, uint64 mutation_count);
+      SubTransactionId subtxn_id, const TableId& table_id, uint64_t mutation_count);
 
   // Get aggregated mutations for each table across the whole transaction (exclude aborted
   // sub-transactions).
-  const std::map<TableId, uint64> GetTableMutationCounts() const;
+  std::unordered_map<TableId, uint64_t> GetTableMutationCounts() const;
 
  private:
   class Impl;
@@ -202,7 +202,7 @@ class YBSubTransaction {
 
   bool HasSubTransaction(SubTransactionId id) const;
 
-  const SubTransactionMetadata& get();
+  const SubTransactionMetadata& get() const;
 
   std::string ToString() const;
 
