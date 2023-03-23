1128bad219040a65812365c078187527b33b2cf4
Sanket Kedia <kediasanket11121993@gmail.com>
2023-03-23 11:00:07 -0700

    [#16507] DST: PITR + Packed - Fix sys catalog restoration logic
    Summary:
    With packed columns enabled, we could have the following scenario:
    For illustration, consider a ysql system table table t with three columns. This is hypothetical but serves to illustrate the point that any ysql system table is prone to this:
    
    c1 -> INT PARTITION KEY
    c2 -> INT NOT PARTITION KEY
    c3 -> INT NOT PARTITION KEY
    
    1. Say I insert (c1 = 1, c2 = 2, c3 = 3) into t, note down time
    2. Now the kv for this row is: 1 => [2, 3]
    3. Now I update c2 and c3 for the same row (i.e. c1 = 1) to 4 and 5
    4. We'll have two more entries now: [1, column_id(c2)] => 4 and [1, column_id(c3)] => 5
    5. Now, perform PITR to time noted in (1) - we'll end up deleting [1, column_id(c2)] => DEL and [1, column_id(c3)] => DEL since they were not present in the restoring state but present in the
    existing state
    6. Because they were written at a more recent time if we do a select * for c1 = 1, we will get c2 and c3 as DEL
    
    Because this logic is applicable to all the ysql system tables, they are susceptible to the above scenario. Also, the sequences_data table also re-uses the same merge technique so it is also
    prone.
    
    This diff fixes this by storing the last packed row of the restoring state and inserting that kv instead of a DEL when we encounter a kv present in the existing state but absent in the
    restoring state.
    
    Test Plan: ybd --cxx_test yb-admin-snapshot-schedule-test --gtest-filter YbAdminSnapshotScheduleTest.PgsqlSingleColumnUpdate
    
    Reviewers: zdrudi, sergei
    
    Reviewed By: sergei
    
    Subscribers: bogdan, mhaddad, slingam, ybase
    
    Differential Revision: https://phabricator.dev.yugabyte.com/D23807

--- src/yb/tablet/restore_util.cc
+++ src/yb/tablet/restore_util.cc
@@ -13,9 +13,13 @@
 #include "yb/tablet/restore_util.h"
 
 #include "yb/docdb/docdb.messages.h"
+#include "yb/docdb/doc_read_context.h"
 
 #include "yb/rpc/lightweight_message.h"
 
+#include "yb/tablet/tablet_metadata.h"
+
+#include "yb/util/logging.h"
 namespace yb {
 
 Status FetchState::SetPrefix(const Slice& prefix) {
@@ -103,22 +107,63 @@ Status FetchState::Next(MoveForward move_forward) {
 
 Status RestorePatch::ProcessCommonEntry(
     const Slice& key, const Slice& existing_value, const Slice& restoring_value) {
+  VLOG_WITH_FUNC(3) << "Key: " << key.ToDebugHexString() << ", existing value: "
+                    << existing_value.ToDebugHexString() << ", restoring value: "
+                    << restoring_value.ToDebugHexString();
   if (restoring_value.compare(existing_value)) {
     IncrementTicker(RestoreTicker::kUpdates);
     AddKeyValue(key, restoring_value, doc_batch_);
   }
-  return Status::OK();
+  // If this is a packed row, update the state.
+  return TryUpdateLastPackedRow(key, restoring_value);
 }
 
 Status RestorePatch::ProcessRestoringOnlyEntry(
     const Slice& restoring_key, const Slice& restoring_value) {
+  VLOG_WITH_FUNC(3) << "Restoring key: " << restoring_key.ToDebugHexString() << ", "
+                    << "restoring value: " << restoring_value.ToDebugHexString();
   IncrementTicker(RestoreTicker::kInserts);
   AddKeyValue(restoring_key, restoring_value, doc_batch_);
-  return Status::OK();
+  // If this is a packed row, update the state.
+  return TryUpdateLastPackedRow(restoring_key, restoring_value);
 }
 
 Status RestorePatch::ProcessExistingOnlyEntry(
     const Slice& existing_key, const Slice& existing_value) {
+  VLOG_WITH_FUNC(3) << "Existing key: " << existing_key.ToDebugHexString() << ", "
+                    << "existing value: " << existing_value.ToDebugHexString() << ", "
+                    << "last packed row key: "
+                    << last_packed_row_restoring_state_.key.AsSlice().ToDebugHexString()
+                    << ", last packed row value: "
+                    << last_packed_row_restoring_state_.value.AsSlice().ToDebugHexString();
+  if (!last_packed_row_restoring_state_.key.empty() &&
+      existing_key.starts_with(last_packed_row_restoring_state_.key.AsSlice())) {
+    // Find out this column's value from the packed row.
+    Slice subkey = existing_key.WithoutPrefix(last_packed_row_restoring_state_.key.size());
+    if (!subkey.empty()) {
+      char type = subkey.consume_byte();
+      if (docdb::IsColumnId(static_cast<docdb::KeyEntryType>(type))) {
+        Slice packed_value = last_packed_row_restoring_state_.value.AsSlice();
+        const docdb::SchemaPacking& packing = VERIFY_RESULT(
+            table_info_->doc_read_context->schema_packing_storage.GetPacking(&packed_value));
+        int64_t column_id_as_int64 = VERIFY_RESULT(util::FastDecodeSignedVarIntUnsafe(&subkey));
+        // Expect only one subkey.
+        SCHECK_EQ(subkey.empty(), true, Corruption, "Only one subkey expected");
+        ColumnId column_id;
+        RETURN_NOT_OK(ColumnId::FromInt64(column_id_as_int64, &column_id));
+        auto value = packing.GetValue(column_id, packed_value);
+        // Insert this column's packed row value.
+        if (value) {
+          VLOG_WITH_FUNC(1) << "Inserting key: " << existing_key.ToDebugHexString()
+                            << ", value: " << (*value).ToDebugHexString();
+          AddKeyValue(existing_key, *value, doc_batch_);
+          IncrementTicker(RestoreTicker::kInserts);
+          return Status::OK();
+        }
+      }
+    }
+  }
+  // Otherwise delete this kv.
   char tombstone_char = docdb::ValueEntryTypeAsChar::kTombstone;
   Slice tombstone(&tombstone_char, 1);
   IncrementTicker(RestoreTicker::kDeletes);
@@ -174,6 +219,20 @@ Status RestorePatch::PatchCurrentStateFromRestoringState() {
     RETURN_NOT_OK(existing_state_->Next());
   }
 
+  return Status::OK();
+}
+
+Status RestorePatch::TryUpdateLastPackedRow(const Slice& key, const Slice& value) {
+  VLOG_WITH_FUNC(3) << "Key: " << key.ToDebugHexString()
+                    << ", value: " << value.ToDebugHexString();
+  auto value_slice = value;
+  RETURN_NOT_OK(docdb::ValueControlFields::Decode(&value_slice));
+  if (value_slice.TryConsumeByte(docdb::ValueEntryTypeAsChar::kPackedRow)) {
+    VLOG_WITH_FUNC(2) << "Packed row encountered in the restoring state. Key: "
+                      << key.ToDebugHexString() << ", value: " << value.ToDebugHexString();
+    last_packed_row_restoring_state_.key = key;
+    last_packed_row_restoring_state_.value = value_slice;
+  }
   return Status::OK();
 }
 
