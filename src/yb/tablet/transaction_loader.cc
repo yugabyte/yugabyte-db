1f450b1595dae0ecd50ca906c3e7fae1848e29e2
Hari Krishna Sunder <hari90@users.noreply.github.com>
2023-03-24 15:35:14 -0700

    [#16472] DocDB: Replace std::thread with yb::Thread
    Summary:
    Most of our code base uses yb::Thread. It has the advantage of tracking the thread in the threadz endpoint.
    Replace all uses of std::thread from the production code to yb::Thread.
    
    Removed CDSAttacher from these functions.
    
    Test Plan: all tests
    
    Reviewers: asrivastava, dmitry
    
    Reviewed By: asrivastava
    
    Subscribers: dmitry, bogdan, slingam
    
    Differential Revision: https://phabricator.dev.yugabyte.com/D23796

--- src/yb/tablet/transaction_loader.cc
+++ src/yb/tablet/transaction_loader.cc
@@ -71,15 +71,13 @@ class TransactionLoader::Executor {
     }
     regular_iterator_ = CreateFullScanIterator(db.regular);
     intents_iterator_ = CreateFullScanIterator(db.intents);
-    auto& load_thread = loader_.load_thread_;
-    load_thread = std::thread(&Executor::Execute, this);
+    CHECK_OK(yb::Thread::Create(
+        "transaction_loader", "loader", &Executor::Execute, this, &loader_.load_thread_))
     return true;
   }
 
  private:
   void Execute() {
-    CDSAttacher attacher;
-
     SetThreadName("TransactionLoader");
 
     auto se = ScopeExit([this] {
@@ -390,8 +388,8 @@ void TransactionLoader::WaitAllLoaded() NO_THREAD_SAFETY_ANALYSIS {
 }
 
 void TransactionLoader::Shutdown() {
-  if (load_thread_.joinable()) {
-    load_thread_.join();
+  if (load_thread_) {
+    load_thread_->Join();
   }
 }
 
