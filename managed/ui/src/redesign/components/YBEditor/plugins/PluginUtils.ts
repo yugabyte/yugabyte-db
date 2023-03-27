b4dc323e563536c4aca11019ad470f59d11374fa
kkannan <kkannan@yugabyte.com>
2023-03-27 15:06:17 +0530

    [PLAT-5368][UI] Implement Custom Notification Email Template
    Summary:
    this diff implements slatejs editor with plugins.
    The following plugins are implemented
    
      # Basic Plugin - supports bold,italics,underline etc
      # SingleLine Plugin - restricts the editor to single line
      # Alert variable plugin - allows to add alert variable to the editor, the user can type '{{' to launch the popover, which has the variable list.
    
    This diff also implements a support for serializing and de-serializing slate js contents to html and vice versa
    
    Note: The design is not implemented as per figma, only the functionalities are implemented. The design will be pushed in the following diff.
    
    Created the separate ticket to track this issue. [[ https://yugabyte.atlassian.net/browse/PLAT-7958 | PLAT-7958 ]]
    
    Test Plan:
    Tested manually by creating and editing template using the editor.
    
    {F36041}
    {F36042}
    {F36043}
    
    Reviewers: lsangappa, jmak, rmadhavan, asathyan
    
    Reviewed By: asathyan
    
    Subscribers: ui, yugaware
    
    Differential Revision: https://phabricator.dev.yugabyte.com/D23723

--- managed/ui/src/redesign/components/YBEditor/plugins/PluginUtils.ts
+++ managed/ui/src/redesign/components/YBEditor/plugins/PluginUtils.ts
@@ -7,26 +7,37 @@
  * http://github.com/YugaByte/yugabyte-db/blob/master/licenses/POLYFORM-FREE-TRIAL-LICENSE-1.0.0.txt
  */
 
-import { Editor, Transforms, Element as SlateElement } from 'slate';
-import { CustomElement, IYBEditor, TextDecorators } from './custom-types';
+import ReactDOM from 'react-dom';
+import { Editor, Transforms, Element as SlateElement, Range } from 'slate';
+import { ReactEditor } from 'slate-react';
+import { CustomElement, IYBEditor, Paragraph, TextDecorators } from './custom-types';
 import {
   IYBSlatePluginReturnProps,
   SlateRenderElementProps,
   SlateRenderLeafProps
 } from './IPlugin';
 
+export const DefaultElement: Paragraph = {
+  type: 'paragraph',
+  align: 'left',
+  children: [{ text: '' }]
+};
+
 /**
  * common function which can be used to return for non enabled plugins.
  */
 export const nonActivePluginReturnType: Omit<IYBSlatePluginReturnProps, 'name'> = {
-  renderElement: (props: SlateRenderElementProps) => undefined,
+  isEnabled: () => false,
+
+  renderElement: (_p: SlateRenderElementProps) => undefined,
 
   onKeyDown: (_e: React.KeyboardEvent<HTMLDivElement>) => false,
 
-  renderLeaf: (_props: SlateRenderLeafProps) => undefined
+  renderLeaf: (_props: SlateRenderLeafProps) => undefined,
+
+  defaultComponents: []
 };
 
-const LIST_TYPES = ['numbered-list', 'bulleted-list'];
 const TEXT_ALIGN_TYPES = ['left', 'center', 'right', 'justify'];
 
 /**
@@ -46,7 +57,7 @@ export const isBlockActive = (editor: IYBEditor, format: string, blockType = 'ty
   return !!match;
 };
 
-export const isMarkActive = (editor: IYBEditor, mark: CustomElement['type']) => {
+export const isMarkActive = (editor: IYBEditor, mark: TextDecorators) => {
   const marks = Editor.marks(editor);
   return marks ? marks[mark] === true : false;
 };
@@ -58,14 +69,9 @@ export const toggleBlock = (editor: IYBEditor, block: string) => {
     TEXT_ALIGN_TYPES.includes(block) ? 'align' : 'type'
   );
 
-  const isList = LIST_TYPES.includes(block);
-
   Transforms.unwrapNodes(editor, {
     match: (n) =>
-      !Editor.isEditor(n) &&
-      SlateElement.isElement(n) &&
-      LIST_TYPES.includes(n.type) &&
-      !TEXT_ALIGN_TYPES.includes(block),
+      !Editor.isEditor(n) && SlateElement.isElement(n) && !TEXT_ALIGN_TYPES.includes(block),
     split: true
   });
   let newProperties: Partial<SlateElement>;
@@ -75,14 +81,14 @@ export const toggleBlock = (editor: IYBEditor, block: string) => {
     };
   } else {
     newProperties = {
-      type: isActive ? 'paragraph' : isList ? 'list-item' : block
+      type: (isActive ? 'paragraph' : block) as any
     };
   }
   Transforms.setNodes<SlateElement>(editor, newProperties);
 
-  if (!isActive && isList) {
+  if (!isActive) {
     const b = { type: block, children: [] };
-    Transforms.wrapNodes(editor, b);
+    Transforms.wrapNodes(editor, b as any);
   }
 };
 
@@ -94,4 +100,47 @@ export const toggleMark = (editor: IYBEditor, mark: TextDecorators) => {
   } else {
     Editor.addMark(editor, mark, true);
   }
+};
+
+export const Portal = ({ children }: { children: React.ReactChild | null }) => {
+  return typeof document === 'object' ? ReactDOM.createPortal(children, document.body) : null;
+};
+
+export const getBeforeNChars = (editor: IYBEditor, numOfChars: number) => {
+  const { selection } = editor;
+  if (selection && Range.isCollapsed(selection)) {
+    const { anchor } = selection;
+    const block = Editor.above(editor, {
+      match: (n) => SlateElement.isElement(n) && Editor.isBlock(editor, n)
+    });
+
+    const path = block ? block[1] : [];
+    const start = Editor.start(editor, path);
+    const range = { anchor, focus: start };
+    const beforeText = Editor.string(editor, range);
+
+    return beforeText.slice(-1 * numOfChars);
+  }
+  return undefined;
+};
+
+export const deleteNChars = (editor: IYBEditor, numOfChars: number, reverse = false) => {
+  Transforms.delete(editor, { reverse, distance: numOfChars, unit: 'character' });
+};
+
+export const deleteElement = (editor: IYBEditor, element: CustomElement) => {
+  const path = ReactEditor.findPath(editor, element);
+  Transforms.removeNodes(editor, { at: path });
+};
+
+export const clearEditor = (editor: IYBEditor) => {
+  Transforms.delete(editor, {
+    at: {
+      anchor: Editor.start(editor, []),
+      focus: Editor.end(editor, [])
+    }
+  });
+  Transforms.removeNodes(editor, {
+    at: [0]
+  });
 };
