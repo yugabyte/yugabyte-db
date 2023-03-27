dca57884d454d5133e8f78dd1723ed3de064ef9a
SB YB <sbapat@yugabyte.com>
2023-03-27 11:05:36 -0700

    Add gauge for version mismatch events
    Summary:
    Adding a gauge so that we can start tracking version mismatch between primary and standby YBA install.
    In subsequent diff we will add an alert based on this new metric.
    
    Test Plan: unit test
    
    Reviewers: amalyshev
    
    Reviewed By: amalyshev
    
    Subscribers: jenkins-bot, yugaware
    
    Differential Revision: https://phabricator.dev.yugabyte.com/D23905

--- managed/src/main/java/com/yugabyte/yw/controllers/InternalHAController.java
+++ managed/src/main/java/com/yugabyte/yw/controllers/InternalHAController.java
@@ -12,6 +12,8 @@ package com.yugabyte.yw.controllers;
 
 import com.google.inject.Inject;
 import com.yugabyte.yw.common.ApiResponse;
+import com.yugabyte.yw.common.ConfigHelper;
+import com.yugabyte.yw.common.ConfigHelper.ConfigType;
 import com.yugabyte.yw.common.Util;
 import com.yugabyte.yw.common.ValidatingFormFactory;
 import com.yugabyte.yw.common.ha.PlatformReplicationManager;
@@ -43,12 +45,16 @@ public class InternalHAController extends Controller {
 
   private final PlatformReplicationManager replicationManager;
   private final ValidatingFormFactory formFactory;
+  private final ConfigHelper configHelper;
 
   @Inject
   InternalHAController(
-      PlatformReplicationManager replicationManager, ValidatingFormFactory formFactory) {
+      PlatformReplicationManager replicationManager,
+      ValidatingFormFactory formFactory,
+      ConfigHelper configHelper) {
     this.replicationManager = replicationManager;
     this.formFactory = formFactory;
+    this.configHelper = configHelper;
   }
 
   private String getClusterKey() {
@@ -202,6 +208,14 @@ public class InternalHAController extends Controller {
 
       // Demote the local instance.
       replicationManager.demoteLocalInstance(localInstance.get(), formData.leader_address);
+
+      String version =
+          configHelper
+              .getConfig(ConfigType.YugawareMetadata)
+              .getOrDefault("version", "UNKNOWN")
+              .toString();
+
+      localInstance.get().updateYbaVersionForLocalInstance(version);
 
       return PlatformResults.withData(localInstance);
     } catch (Exception e) {
