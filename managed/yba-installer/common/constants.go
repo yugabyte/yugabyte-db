cd3ce187c913ebe48d968c30d90d49500d24ad5a
Shristy Agarwal <shagarwal@yugabyte.com>
2023-03-29 18:08:36 -0700

    [PLAT-6894] Remove dependency to convert PEM to JKS in YBA installer
    Summary:
    We've stopped bundling nginx in YBA installer for quite sometime now.
    With this removal we needed to terminate HTTPs at PLAY framework in
    place of the reverse-proxy nginx. This was initially done by converting
    PEM format certificates to JavaKeyStore(JKS) using the external library
    pemtokeystore. Back then, this was needed as Play did not understand how
    to use PEM files.
    
    However, with commit
    https://github.com/yugabyte/yugabyte-db/commit/60f037cd13be1f26fe54fd4c085277f400e1fd62
    we do not need this conversion anymore as our Java layer understands PEM
    certificates now. Removing the conversion of PEM format certificates to
    JavaKeyStore and creating a single server.pem for TLS. Made PLAY read this PEM file for TLS.
    
    Test Plan:
    1. Using PEM format certificates works for TLS in k8s and replicated install.
       Ensure PLAY framework in YBA-I can read this file for HTTPs runs.
    2. Test YBA installer with HTTP and HTTPs
    3. Tested root and non-root user scenario
    
    Reviewers: muthu, nbhatia, sb-yb, dshubin, sanketh
    
    Reviewed By: dshubin
    
    Subscribers: jenkins-bot, yugaware
    
    Differential Revision: https://phabricator.dev.yugabyte.com/D24001

--- managed/yba-installer/common/constants.go
+++ managed/yba-installer/common/constants.go
@@ -2,8 +2,8 @@ package common
 
 const DefaultServiceUser string = "yugabyte"
 
-const ServerKeyStorePath string = "server.ks"
-
 const ServerCertPath string = "server-cert.pem"
 
 const ServerKeyPath string = "server-key.pem"
+
+const ServerPemPath string = "server.pem"
