727b73dbcb6b12793a162d8729b084f8f6661cf6
vkumar <vkumar@yugabyte.com>
2023-04-06 15:14:58 +0000

    [BACKPORT 2.17.3][PLAT-8246][PLAT-8243] Improve Incremental backup resource consumption. Enable verbose logs by default on YBC.
    Summary:
    Backport 1: [PLAT-8246]
    Original PR: https://phabricator.dev.yugabyte.com/D24222
    Original commit: da54e17655083a7955e65df9301eb2743e4cf3f7
    - Attempt to reduce number of postgres connections made when doing backups
    
    Backport 2: [PLAT-8243]
    Original PR: https://phabricator.dev.yugabyte.com/D24217
    Original commit: 6d97e62bdf863afe03e7ed8c0898a6c0aaf16457
    - Set "v=1" in YBC flags
    
    Test Plan: Dev itests
    
    Reviewers: vpatibandla
    
    Reviewed By: vpatibandla
    
    Subscribers: jenkins-bot, yugaware
    
    Differential Revision: https://phabricator.dev.yugabyte.com/D24261

--- managed/src/main/java/com/yugabyte/yw/commissioner/tasks/UniverseTaskBase.java
+++ managed/src/main/java/com/yugabyte/yw/commissioner/tasks/UniverseTaskBase.java
@@ -2414,6 +2414,11 @@ public abstract class UniverseTaskBase extends AbstractTaskBase {
     YbcBackupNodeRetriever nodeRetriever =
         new YbcBackupNodeRetriever(backupParams.universeUUID, parallelDBBackups);
     nodeRetriever.initializeNodePoolForBackups(backupStates);
+    Backup previousBackup =
+        (!backupParams.baseBackupUUID.equals(backupParams.backupUuid))
+            ? Backup.getLastSuccessfulBackupInChain(
+                backupParams.customerUuid, backupParams.baseBackupUUID)
+            : null;
     backupParams
         .backupList
         .stream()
@@ -2422,6 +2427,7 @@ public abstract class UniverseTaskBase extends AbstractTaskBase {
             bTP -> {
               BackupTableYbc task = createTask(BackupTableYbc.class);
               BackupTableYbc.Params backupYbcParams = new BackupTableYbc.Params(bTP, nodeRetriever);
+              backupYbcParams.previousBackup = previousBackup;
               backupYbcParams.nodeIp = backupStates.get(bTP.getKeyspace()).nodeIp;
               backupYbcParams.taskID = backupStates.get(bTP.getKeyspace()).currentYbcTaskId;
               task.initialize(backupYbcParams);
