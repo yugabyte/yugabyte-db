cb20e6fdbc1fa463f71a854b2c849ccf8d115eee
Subramanian Neelakantan <sneelakantan@yugabyte.com>
2023-04-07 16:35:58 +0000

    [BACKPORT 2.17.3][PLAT-8237] Enabling YBC on read replica of k8s
    Summary:
    Looks like adding a RR cluster to an existing k8s universe is handled separately. It is not part of EditKubernetesUniverse. So this change adds the YBC installation to this workflow as well in ReadOnlyKubernetesClusterCreate workflow.
    Original commit: 14243aebe0ba9409396d67ced469f6e810ba8b01 / D24218
    
    Test Plan:
    Manually verify that a tserver in RR cluster comes up with ybc installed and running in it. Also the health of the universe is green.
    Verify for both single AZ and multi AZ universes.
    
    Reviewers: vpatibandla
    
    Reviewed By: vpatibandla
    
    Subscribers: yugaware, jenkins-bot
    
    Differential Revision: https://phabricator.dev.yugabyte.com/D24234

--- managed/src/main/java/com/yugabyte/yw/commissioner/tasks/ReadOnlyKubernetesClusterCreate.java
+++ managed/src/main/java/com/yugabyte/yw/commissioner/tasks/ReadOnlyKubernetesClusterCreate.java
@@ -89,7 +89,7 @@ public class ReadOnlyKubernetesClusterCreate extends KubernetesTaskBase {
               taskParams().useNewHelmNamingStyle);
 
       boolean isMultiAz = PlacementInfoUtil.isMultiAZ(provider);
-      createPodsTask(universe.name, placement, masterAddresses, true);
+      createPodsTask(universe.name, placement, masterAddresses, true, universe.isYbcEnabled());
 
       // Following method assumes primary cluster.
       createSingleKubernetesExecutorTask(
@@ -101,6 +101,13 @@ public class ReadOnlyKubernetesClusterCreate extends KubernetesTaskBase {
       // Wait for new tablet servers to be responsive.
       createWaitForServersTasks(tserversAdded, ServerType.TSERVER)
           .setSubTaskGroupType(SubTaskGroupType.ConfigureUniverse);
+
+      // Install YBC on the RR tservers and wait for its completion
+      if (universe.isYbcEnabled()) {
+        installYbcOnThePods(
+            universe.name, tserversAdded, true, universe.getUniverseDetails().ybcSoftwareVersion);
+        createWaitForYbcServerTask(tserversAdded);
+      }
 
       // Persist the placement info into the YB master leader.
       createPlacementInfoTask(null /* blacklistNodes */)
