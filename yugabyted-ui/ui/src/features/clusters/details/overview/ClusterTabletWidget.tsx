3163493800486e35f352ed5bf294deae0a9ec809
Jaseel <12792882+Spikatrix@users.noreply.github.com>
2023-04-07 05:51:23 +0530

    [#16165][#16358][yugabyted][UI] New design implementation for Overview and Database pages (#16167)
    * Rearrange left navbar
    
    * Cluster page tabs
    
    * Update header design
    
    * Update overview static text layout
    
    * Added 'Nodes' and 'Tablets' sections
    
    * Added unavailable tablet field and status icons
    
    * Activity and Alert section UI
    
    * Disk usage UI, fix cluster info layout
    
    * vCPU sankey chart implementation
    
    * Replace nbsp with dx spacing
    
    * Linked sections to their corresponding pages
    
    * Activity tab UI
    
    * Settings tab UI
    
    * Activity drawer UI
    
    * Link sections, minor misc updates
    
    * Databases page UI
    
    * vCPU sankey chart in the performance page
    
    * Show an alert icon in the header when nodes are down
    
    * Fix header badge condition
    
    * Region API integration
    
    * Rename total memory to used memory
    
    * Add node selector to GFlags
    
    * vCPU sankey chart API integration
    
    * Gray out activities and alerts for now
    
    * Data table fixes
    
    * Sankey chart refactor and show available cores
    
    * Integrated G-Flags, total memory and replication factor
    
    * Fix unused variables error, Disable GFlags drift for now
    
    * Fixes
    
    * Sankey node filter, country flag bug fix
    
    * Sankey data bug fix, misc fixes

--- yugabyted-ui/ui/src/features/clusters/details/overview/ClusterTabletWidget.tsx
+++ yugabyted-ui/ui/src/features/clusters/details/overview/ClusterTabletWidget.tsx
@@ -1,31 +1,46 @@
 import React, { FC } from 'react';
 import { useTranslation } from 'react-i18next';
-import { Grid, makeStyles, Paper, Typography } from '@material-ui/core';
+import { Box, Grid, makeStyles, Typography } from '@material-ui/core';
 import { useGetClusterTabletsQuery } from '@app/api/src';
 
 // Local imports
 import type { HealthCheckInfo } from '@app/api/src';
+import clsx from 'clsx';
+import { STATUS_TYPES, YBStatus } from '@app/components';
 
 const useStyles = makeStyles((theme) => ({
-  clusterInfo: {
-    padding: theme.spacing(2),
-    flexGrow: 1,
-    flexBasis: 0,
-    border: `1px solid ${theme.palette.grey[200]}`
-  },
   container: {
-    justifyContent: 'space-between'
+    flexWrap: "nowrap",
+    marginTop: theme.spacing(1.5),
+    justifyContent: 'space-between',
+  },
+  section: {
+    marginRight: theme.spacing(2),
+  },
+  sectionInvisible: {
+    marginRight: theme.spacing(4),
+  },
+  sectionBorder: {
+    paddingLeft: theme.spacing(2),
+    borderLeft: `1px solid ${theme.palette.grey[300]}`
+  },
+  title: {
+    color: theme.palette.grey[900],
+    fontWeight: theme.typography.fontWeightRegular as number,
+    flexGrow: 1,
   },
   label: {
-    color: theme.palette.grey[600],
+    color: theme.palette.grey[500],
     fontWeight: theme.typography.fontWeightMedium as number,
+    marginTop: theme.spacing(1.25),
     marginBottom: theme.spacing(0.75),
     textTransform: 'uppercase',
-    textAlign: 'center'
+    textAlign: 'left'
   },
   value: {
-    paddingTop: theme.spacing(0.57),
-    textAlign: 'center'
+    color: theme.palette.grey[900],
+    textAlign: 'left',
+    paddingTop: theme.spacing(0.25)
   },
 }));
 
@@ -47,37 +62,43 @@ export const ClusterTabletWidget: FC<ClusterTabletWidgetProps> = ({ health }) =>
 //   const unavailableTablets = tabletIds.filter((key) => {
 //     !tabletsList[key].has_leader
 //   });
-  const numUnavailableTablets = unavailableTablets.length
-
 
   return (
-    <Paper className={classes.clusterInfo}>
+    <Box>
+      <Typography variant="body2" className={classes.title}>{t('clusterDetail.overview.tablets')}</Typography>
       <Grid container className={classes.container}>
-        <div>
-          <Typography variant="subtitle2" className={classes.label}>
-            {t('clusterDetail.overview.totalTablets')}
-          </Typography>
-          <Typography variant="body2" className={classes.value}>
+        <div className={classes.section}>
+          <Typography variant="h4" className={classes.value}>
             {totalTablets}
           </Typography>
-        </div>
-        <div>
-          <Typography variant="subtitle2" className={classes.label}>
-            {t('clusterDetail.overview.underReplicatedTablets')}
-          </Typography>
-          <Typography variant="body2" className={classes.value}>
-            {underReplicatedTablets.length}
+          <Typography variant="body2" className={classes.label}>
+            {t('clusterDetail.overview.total')}
           </Typography>
         </div>
-        <div>
-          <Typography variant="subtitle2" className={classes.label}>
-            {t('clusterDetail.overview.unavailableTablets')}
-          </Typography>
-          <Typography variant="body2" className={classes.value}>
-            {numUnavailableTablets}
+        <div className={clsx(classes.section, classes.sectionBorder)}>
+          <Box display="flex" gridGap={7}>
+            <Typography variant="h4" className={classes.value}>
+              {underReplicatedTablets.length}
+            </Typography>
+            <YBStatus type={STATUS_TYPES.WARNING}/>
+          </Box>
+          <Typography variant="body2" className={classes.label}>
+            {t('clusterDetail.overview.underReplicated')}
           </Typography>
         </div>
+        <div className={classes.section}>
+          <Box display="flex" gridGap={7}>
+            <Typography variant="h4" className={classes.value}>
+              {unavailableTablets.length}
+            </Typography>
+            <YBStatus type={STATUS_TYPES.FAILED}/>
+          </Box>
+          <Typography variant="body2" className={classes.label}>
+            {t('clusterDetail.overview.unavailable')}
+          </Typography>
+        </div>
+        <div className={classes.sectionInvisible}></div>
       </Grid>
-    </Paper>
+    </Box>
   );
 };
