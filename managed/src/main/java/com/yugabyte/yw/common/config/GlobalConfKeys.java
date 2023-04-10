7836e3c3336c5dd32a05fe506d00316fd7876ee6
Muthu Chidambaram <mchidambaram@yugabyte.com>
2023-04-10 17:09:42 -0500

    [BACKPORT 2.18][PLAT-7121] HA implementation for YBA-installer
    Summary:
    Adds support to Java, backup script, and yba-installer YBA config for HA support of YBA-installer
    installations. Java code mimics a lot of the logic in backup.go to pass the right arguments to the
    backup script. Some of the information necessary is pulled from the YBA config that is now updated
    with flags to indicate installation type and pg_dump/restore paths for custom installations. Also
    fixes some issues with backup script that interfere with normal backups.
    
    Need to add ca_cert.pem of standby instance to the active instance peer certificates.
    
    Original diff: D24211 / 13acb83559635783579c96f2fb0d118cdc335b95
    
    Test Plan:
    Setup HA with two YBA-installer platforms.
    Same with custom postgres.
    
    Reviewers: sanketh, sb-yb, dshubin
    
    Reviewed By: dshubin
    
    Subscribers: yugaware, jenkins-bot
    
    Differential Revision: https://phabricator.dev.yugabyte.com/D24295

--- managed/src/main/java/com/yugabyte/yw/common/config/GlobalConfKeys.java
+++ managed/src/main/java/com/yugabyte/yw/common/config/GlobalConfKeys.java
@@ -568,4 +568,22 @@ public class GlobalConfKeys extends RuntimeConfigKeysModule {
           "It indicates whether YBA should support transactional xCluster configs",
           ConfDataType.BooleanType,
           ImmutableList.of(ConfKeyTags.PUBLIC));
+  public static final ConfKeyInfo<String> pgDumpPath =
+      new ConfKeyInfo<>(
+          "db.default.pg_dump_path",
+          ScopeType.GLOBAL,
+          "Path to pg_dump on the YBA node",
+          "Set during yba-installer for both custom postgres and version specific postgres "
+              + "installation",
+          ConfDataType.StringType,
+          ImmutableList.of(ConfKeyTags.PUBLIC));
+  public static final ConfKeyInfo<String> pgRestorePath =
+      new ConfKeyInfo<>(
+          "db.default.pg_restore_path",
+          ScopeType.GLOBAL,
+          "Path to pg_restore on the YBA node",
+          "Set during yba-installer for both custom postgres and version specific postgres "
+              + "installation",
+          ConfDataType.StringType,
+          ImmutableList.of(ConfKeyTags.PUBLIC));
 }
