36f439b8adc79ff298e556733e22c6477cd4cf8e
Jason Kim <git@jasonk.me>
2023-04-10 11:13:42 -0700

    [#16816] test: port onek2, tenk2 regress test queries
    Summary:
    Port postgres regress tests related to onek2, tenk2.  The test that
    populates the tables is create_misc.  Port only those statements to a
    new yb_pg_create_misc, and add that test as a dependency to any of the
    following affected tests.
    
    - Port onek2 queries to yb_pg_create_index.
    - Port onek2 queries to yb_pg_select.
    - Port tenk2 queries to yb_pg_join.
    - Properly use dependency yb_pg_create_misc for onek2 and tenk2 in
      yb_pg_portals and yb_pg_portals_p2.
    
    Omit porting tests whose file has not yet been ported:
    
    - misc
    - sanity_check
    - select_into
    - select_parallel
    - stats
    
    Also fix up surrounding things:
    
    - In yb_pg_create_index test, re-port btree CREATE INDEX statements to
      match upstream better.
    - Move YB queries from yb_pg_select to yb_select.
    - Move YB queries from yb_pg_select_distinct to yb_select.
      - Also get rid of inconsistent EXPLAIN ANALYZE and add todo #16633.
    - Make yb_obj_properties_functions more resilient to OID changes.
    
    Close: #16816
    
    Test Plan: Jenkins: test regex: .*(TestLintPgRegressSchedules|TestPgRegress(Cursor|Join|Misc|PgMisc|Table|TypesUDT)#testPgRegress).*
    
    Reviewers: tnayak
    
    Reviewed By: tnayak
    
    Subscribers: smishra, neil, myang, yql
    
    Differential Revision: https://phabricator.dev.yugabyte.com/D24043

---
+++ src/postgres/src/test/regress/sql/yb_pg_create_misc.sql
@@ -0,0 +1,11 @@
+--
+-- CREATE_MISC
+--
+
+-- CLASS POPULATION
+--	(any resemblance to real life is purely coincidental)
+--
+
+INSERT INTO tenk2 SELECT * FROM tenk1;
+
+SELECT * INTO TABLE onek2 FROM onek;
