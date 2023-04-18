278e2b3b610ea095462cb7516f314924f238d0af
Aleksandr Malyshev <amalyshev@yugabyte.com>
2023-04-18 17:11:25 +0300

    [Backport 2.18][PLAT-8439] Fix test alert generation in case no universes defined
    Summary:
    Currently, when we generate test alert (used fo both "Send Test Alert" and "Notification Preview" functionality) - we're creating alert manually.
    In case universe exists - we're using it for alert generation. Otherwise - we need to mock universe object to generate correct alert.
    
    Original diff: https://phabricator.dev.yugabyte.com/D24572
    
    Test Plan:
    Delete all universes.
    Make sure Send Test Alert functionality works for all alert configs.
    MAke sure alert notification preview functionality works for all alert configs.
    
    Reviewers: vbansal
    
    Reviewed By: vbansal
    
    Subscribers: yugaware, jenkins-bot
    
    Differential Revision: https://phabricator.dev.yugabyte.com/D24633

--- managed/src/main/java/com/yugabyte/yw/controllers/CustomerTaskController.java
+++ managed/src/main/java/com/yugabyte/yw/controllers/CustomerTaskController.java
@@ -41,6 +41,7 @@ import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
 import java.util.Optional;
 import java.util.Set;
 import java.util.UUID;
