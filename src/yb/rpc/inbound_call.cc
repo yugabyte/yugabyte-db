yugabyte/yugabyte-db

commit 127abcf2d39a65e8b5fc53ee819f49d7111c31ef

[#16571] Docdb: Protect InboundCall::trace_ with the mutex
Summary:
 EnsureTraceCreated is used to create InboundCall::trace_ if the caller
 requests that a trace be collected.

 Since this may happen after the InboundCall has been constructed, we need
 to ensure that trace_ is being updated in a thread-safe manner.

 The fix ensures that trace_ is GUAREDED_BY(mutex_) and updates various
 references to the same with a call to trace() instead.

Test Plan: ybd asan --cxx-test tools_yb-admin-test --gtest_filter AdminCliTest.TestCreateTransactionStatusTablesWithPlacements -n 200 --tp 1

Reviewers: rsami, rthallam

Reviewed By: rthallam

Subscribers: ybase

Differential Revision: https://phabricator.dev.yugabyte.com/D23917

src/yb/rpc/inbound_call.cc
@@ -76,23 +76,24 @@ InboundCall::InboundCall(ConnectionPtr conn, RpcMetrics* rpc_metrics,
       conn_(std::move(conn)),
       rpc_metrics_(rpc_metrics ? rpc_metrics : &conn_->rpc_metrics()),
       call_processed_listener_(call_processed_listener) {
-  TRACE_TO(trace_, "Created InboundCall");
+  TRACE_TO(trace(), "Created InboundCall");
   IncrementCounter(rpc_metrics_->inbound_calls_created);
   IncrementGauge(rpc_metrics_->inbound_calls_alive);
 }
 
 InboundCall::~InboundCall() {
-  TRACE_TO(trace_, "Destroying InboundCall");
-  if (trace_) {
+  Trace *my_trace = trace();
+  TRACE_TO(my_trace, "Destroying InboundCall");
+  if (my_trace) {
     YB_LOG_IF_EVERY_N(INFO, FLAGS_print_trace_every > 0, FLAGS_print_trace_every)
-        << "Tracing op: \n " << trace_->DumpToString(true);
+        << "Tracing op: \n " << my_trace->DumpToString(true);
   }
   DecrementGauge(rpc_metrics_->inbound_calls_alive);
 }
 
 void InboundCall::NotifyTransferred(const Status& status, Connection* conn) {
   if (status.ok()) {
-    TRACE_TO(trace_, "Transfer finished");
+    TRACE_TO(trace(), "Transfer finished");
   } else {
     YB_LOG_EVERY_N_SECS(WARNING, 10) << LogPrefix() << "Connection torn down before " << ToString()
                                      << " could send its response: " << status.ToString();
@@ -103,17 +104,24 @@ void InboundCall::NotifyTransferred(const Status& status, Connection* conn) {
 }
 
 void InboundCall::EnsureTraceCreated() {
-  if (!trace_) {
+  scoped_refptr<Trace> trace = nullptr;
+  {
+    std::lock_guard<simple_spinlock> lock(mutex_);
+    if (trace_) {
+      return;
+    }
     trace_ = new Trace;
-    if (timing_.time_received.Initialized()) {
-      TRACE_TO_WITH_TIME(trace_, ToCoarse(timing_.time_received), "Created InboundCall");
-    }
-    if (timing_.time_handled.Initialized()) {
-      TRACE_TO_WITH_TIME(trace_, ToCoarse(timing_.time_handled), "Handling the call");
-    }
-    DCHECK(!timing_.time_completed.Initialized());
-    TRACE_TO(trace_, "Trace Created");
+    trace = trace_;
   }
+
+  if (timing_.time_received.Initialized()) {
+    TRACE_TO_WITH_TIME(trace, ToCoarse(timing_.time_received), "Created InboundCall");
+  }
+  if (timing_.time_handled.Initialized()) {
+    TRACE_TO_WITH_TIME(trace, ToCoarse(timing_.time_handled), "Handling the call");
+  }
+  DCHECK(!timing_.time_completed.Initialized());
+  TRACE_TO(trace, "Trace Created");
 }
 
 const Endpoint& InboundCall::remote_address() const {
@@ -134,7 +142,8 @@ ConnectionContext& InboundCall::connection_context() const {
   return conn_->context();
 }
 
-Trace* InboundCall::trace() {
+Trace* InboundCall::trace() const {
+  std::lock_guard<simple_spinlock> lock(mutex_);
   return trace_.get();
 }
 
@@ -193,7 +202,7 @@ bool InboundCall::ClientTimedOut() const {
 }
 
 void InboundCall::QueueResponse(bool is_success) {
-  TRACE_TO(trace_, is_success ? "Queueing success response" : "Queueing failure response");
+  TRACE_TO(trace(), is_success ? "Queueing success response" : "Queueing failure response");
   LogTrace();
   bool expected = false;
   if (responded_.compare_exchange_strong(expected, true, std::memory_order_acq_rel)) {
@@ -229,7 +238,7 @@ void InboundCall::Clear() {
 }
 
 size_t InboundCall::DynamicMemoryUsage() const {
-  return request_data_memory_usage_.load(std::memory_order_acquire) + DynamicMemoryUsageOf(trace_);
+  return request_data_memory_usage_.load(std::memory_order_acquire) + DynamicMemoryUsageOf(trace());
 }
 
 void InboundCall::InboundCallTask::Run() {

src/yb/rpc/inbound_call.h
@@ -128,7 +128,7 @@ class InboundCall : public RpcCall, public MPSCQueueEntry<InboundCall> {
   ConnectionPtr connection() const;
   ConnectionContext& connection_context() const;
 
-  Trace* trace();
+  Trace* trace() const EXCLUDES(mutex_);
 
   // When this InboundCall was received (instantiated).
   // Should only be called once on a given instance.
@@ -214,7 +214,7 @@ class InboundCall : public RpcCall, public MPSCQueueEntry<InboundCall> {
 
   // For requests that have requested traces to be collected, we will ensure
   // that trace_ is not null and can be used for collecting the requested data.
-  void EnsureTraceCreated();
+  void EnsureTraceCreated() EXCLUDES(mutex_);
 
  protected:
   ThreadPoolTask* BindTask(InboundCallHandler* handler, int64_t rpc_queue_limit);
@@ -239,9 +239,6 @@ class InboundCall : public RpcCall, public MPSCQueueEntry<InboundCall> {
   CallData request_data_;
   std::atomic<size_t> request_data_memory_usage_{0};
 
-  // The trace buffer.
-  scoped_refptr<Trace> trace_;
-
   // Timing information related to this RPC call.
   InboundCallTiming timing_;
 
@@ -252,10 +249,13 @@ class InboundCall : public RpcCall, public MPSCQueueEntry<InboundCall> {
   scoped_refptr<Counter> rpc_method_response_bytes_;
   scoped_refptr<Histogram> rpc_method_handler_latency_;
 
-  bool cleared_ = false;
   mutable simple_spinlock mutex_;
+  bool cleared_ GUARDED_BY(mutex_) = false;
 
  private:
+  // The trace buffer.
+  scoped_refptr<Trace> trace_ GUARDED_BY(mutex_);
+
   // The connection on which this inbound call arrived. Can be null for LocalYBInboundCall.
   ConnectionPtr conn_ = nullptr;
   RpcMetrics* rpc_metrics_;

src/yb/rpc/yb_rpc.cc
@@ -316,8 +316,9 @@ string YBInboundCall::ToString() const {
 bool YBInboundCall::DumpPB(const DumpRunningRpcsRequestPB& req,
                            RpcCallInProgressPB* resp) {
   header_.ToPB(resp->mutable_header());
-  if (req.include_traces() && trace_) {
-    resp->set_trace_buffer(trace_->DumpToString(true));
+  auto my_trace = trace();
+  if (req.include_traces() && my_trace) {
+    resp->set_trace_buffer(my_trace->DumpToString(true));
   }
   resp->set_elapsed_millis(MonoTime::Now().GetDeltaSince(timing_.time_received)
       .ToMilliseconds());
@@ -335,20 +336,22 @@ void YBInboundCall::LogTrace() const {
       // The traces may also be too large to fit in a log message.
       LOG(WARNING) << ToString() << " took " << total_time << "ms (client timeout "
                    << header_.timeout_ms << "ms).";
-      if (trace_) {
-        LOG(WARNING) << "Trace:\n" << trace_->DumpToString(1, true);
+      auto my_trace = trace();
+      if (my_trace) {
+        LOG(WARNING) << "Trace:\n" << my_trace->DumpToString(1, true);
       }
       return;
     }
   }
 
+  auto my_trace = trace();
   if (PREDICT_FALSE(
-          (trace_ && trace_->must_print()) ||
+          (my_trace && my_trace->must_print()) ||
           FLAGS_rpc_dump_all_traces ||
           total_time > FLAGS_rpc_slow_query_threshold_ms)) {
     LOG(INFO) << ToString() << " took " << total_time << "ms. Trace:";
-    if (trace_) {
-      trace_->Dump(&LOG(INFO), true);
+    if (my_trace) {
+      my_trace->Dump(&LOG(INFO), true);
     }
   }
 }

src/yb/yql/cql/cqlserver/cql_rpc.cc
@@ -331,6 +331,7 @@ void CQLInboundCall::GetCallDetails(rpc::RpcCallInProgressPB *call_in_progress_p
 void CQLInboundCall::LogTrace() const {
   MonoTime now = MonoTime::Now();
   auto total_time = now.GetDeltaSince(timing_.time_received).ToMilliseconds();
+  auto trace_ = trace();
   if (PREDICT_FALSE(FLAGS_rpc_dump_all_traces
           // rpcs with an invalid request may have a null request_
           || (trace_ && request_ && request_->trace_requested())
@@ -353,6 +354,7 @@ std::string CQLInboundCall::ToString() const {
 bool CQLInboundCall::DumpPB(const rpc::DumpRunningRpcsRequestPB& req,
                             rpc::RpcCallInProgressPB* resp) {
 
+  auto trace_ = trace();
   if (req.include_traces() && trace_) {
     resp->set_trace_buffer(trace_->DumpToString(true));
   }

src/yb/yql/redis/redisserver/redis_rpc.cc
@@ -288,6 +288,7 @@ void RedisInboundCall::LogTrace() const {
   MonoTime now = MonoTime::Now();
   auto total_time = now.GetDeltaSince(timing_.time_received).ToMilliseconds();
 
+  auto trace_ = trace();
   if (PREDICT_FALSE(FLAGS_rpc_dump_all_traces
           || (trace_ && trace_->must_print())
           || total_time > FLAGS_rpc_slow_query_threshold_ms)) {
@@ -307,6 +308,7 @@ string RedisInboundCall::ToString() const {
 
 bool RedisInboundCall::DumpPB(const rpc::DumpRunningRpcsRequestPB& req,
                               rpc::RpcCallInProgressPB* resp) {
+  auto trace_ = trace();
   if (req.include_traces() && trace_) {
     resp->set_trace_buffer(trace_->DumpToString(true));
   }

commit fd268ae46a512871db151b7ef20738198e6b8019

[BACKPORT 2.16][#16571] Docdb: Protect InboundCall::trace_ with the mutex/atomic_ptr
Summary:
 Original commit: 127abcf2d39a65e8b5fc53ee819f49d7111c31ef / D23917
 Also includes:
 Original commit: fda06b89ee0dddb4e8b90697408a5e834d07edc9 / D24005

1) EnsureTraceCreated is used to create InboundCall::trace_ if the caller
 requests that a trace be collected.

 Since this may happen after the InboundCall has been constructed, we need
 to ensure that trace_ is being updated in a thread-safe manner.

 The fix ensures that trace_ is GUAREDED_BY(mutex_) and updates various
 references to the same with a call to trace() instead.

2) Also included, the regression was recently introduced by 127abcf2d39a65e8b5fc53ee819f49d7111c31ef/D23917.
Average scan time in PgMiniTest.Scan was increased from 1.75s to 1.84s.

InboundCall::trace() method is invoked quite frequently, but mutex was added to it.

Fixed by replacing mutex with atomic.

Test Plan: ybd asan --cxx-test tools_yb-admin-test --gtest_filter AdminCliTest.TestCreateTransactionStatusTablesWithPlacements -n 200 --tp 1

Reviewers: rsami, sergei, rthallam

Reviewed By: rthallam

Subscribers: bogdan, sergei, ybase

Differential Revision: https://phabricator.dev.yugabyte.com/D23965

src/yb/rpc/inbound_call.cc
@@ -77,27 +77,29 @@ namespace rpc {
 
 InboundCall::InboundCall(ConnectionPtr conn, RpcMetrics* rpc_metrics,
                          CallProcessedListener* call_processed_listener)
-    : trace_(Trace::NewTrace()),
+    : trace_holder_(Trace::NewTrace()),
+      trace_(trace_holder_.get()),
       conn_(std::move(conn)),
       rpc_metrics_(rpc_metrics ? rpc_metrics : &conn_->rpc_metrics()),
       call_processed_listener_(call_processed_listener) {
-  TRACE_TO(trace_, "Created InboundCall");
+  TRACE_TO(trace(), "Created InboundCall");
   IncrementCounter(rpc_metrics_->inbound_calls_created);
   IncrementGauge(rpc_metrics_->inbound_calls_alive);
 }
 
 InboundCall::~InboundCall() {
-  TRACE_TO(trace_, "Destroying InboundCall");
-  if (trace_) {
+  Trace *my_trace = trace();
+  TRACE_TO(my_trace, "Destroying InboundCall");
+  if (my_trace) {
     YB_LOG_IF_EVERY_N(INFO, FLAGS_print_trace_every > 0, FLAGS_print_trace_every)
-        << "Tracing op: \n " << trace_->DumpToString(true);
+        << "Tracing op: \n " << my_trace->DumpToString(true);
   }
   DecrementGauge(rpc_metrics_->inbound_calls_alive);
 }
 
 void InboundCall::NotifyTransferred(const Status& status, Connection* conn) {
   if (status.ok()) {
-    TRACE_TO(trace_, "Transfer finished");
+    TRACE_TO(trace(), "Transfer finished");
   } else {
     YB_LOG_EVERY_N_SECS(WARNING, 10) << LogPrefix() << "Connection torn down before " << ToString()
                                      << " could send its response: " << status.ToString();
@@ -108,17 +110,25 @@ void InboundCall::NotifyTransferred(const Status& status, Connection* conn) {
 }
 
 void InboundCall::EnsureTraceCreated() {
-  if (!trace_) {
-    trace_ = new Trace;
-    if (timing_.time_received.Initialized()) {
-      TRACE_TO_WITH_TIME(trace_, ToCoarse(timing_.time_received), "Created InboundCall");
+  scoped_refptr<Trace> trace = nullptr;
+  {
+    std::lock_guard<simple_spinlock> lock(mutex_);
+    if (trace_holder_) {
+      return;
     }
-    if (timing_.time_handled.Initialized()) {
-      TRACE_TO_WITH_TIME(trace_, ToCoarse(timing_.time_handled), "Handling the call");
-    }
-    DCHECK(!timing_.time_completed.Initialized());
-    TRACE_TO(trace_, "Trace Created");
+    trace = new Trace;
+    trace_holder_ = trace;
+    trace_.store(trace.get(), std::memory_order_relaxed);
   }
+
+  if (timing_.time_received.Initialized()) {
+    TRACE_TO_WITH_TIME(trace, ToCoarse(timing_.time_received), "Created InboundCall");
+  }
+  if (timing_.time_handled.Initialized()) {
+    TRACE_TO_WITH_TIME(trace, ToCoarse(timing_.time_handled), "Handling the call");
+  }
+  DCHECK(!timing_.time_completed.Initialized());
+  TRACE_TO(trace, "Trace Created");
 }
 
 const Endpoint& InboundCall::remote_address() const {
@@ -137,10 +147,6 @@ ConnectionPtr InboundCall::connection() const {
 
 ConnectionContext& InboundCall::connection_context() const {
   return conn_->context();
-}
-
-Trace* InboundCall::trace() {
-  return trace_.get();
 }
 
 void InboundCall::RecordCallReceived() {
@@ -198,7 +204,7 @@ bool InboundCall::ClientTimedOut() const {
 }
 
 void InboundCall::QueueResponse(bool is_success) {
-  TRACE_TO(trace_, is_success ? "Queueing success response" : "Queueing failure response");
+  TRACE_TO(trace(), is_success ? "Queueing success response" : "Queueing failure response");
   LogTrace();
   bool expected = false;
   if (responded_.compare_exchange_strong(expected, true, std::memory_order_acq_rel)) {
@@ -234,7 +240,7 @@ void InboundCall::Clear() {
 }
 
 size_t InboundCall::DynamicMemoryUsage() const {
-  return request_data_memory_usage_.load(std::memory_order_acquire) + DynamicMemoryUsageOf(trace_);
+  return request_data_memory_usage_.load(std::memory_order_acquire) + DynamicMemoryUsageOf(trace());
 }
 
 void InboundCall::InboundCallTask::Run() {

src/yb/rpc/inbound_call.h
@@ -129,7 +129,9 @@ class InboundCall : public RpcCall, public MPSCQueueEntry<InboundCall> {
   ConnectionPtr connection() const;
   ConnectionContext& connection_context() const;
 
-  Trace* trace();
+  inline Trace* trace() const EXCLUDES(mutex_) {
+    return trace_.load(std::memory_order_relaxed);
+  }
 
   // When this InboundCall was received (instantiated).
   // Should only be called once on a given instance.
@@ -215,7 +217,7 @@ class InboundCall : public RpcCall, public MPSCQueueEntry<InboundCall> {
 
   // For requests that have requested traces to be collected, we will ensure
   // that trace_ is not null and can be used for collecting the requested data.
-  void EnsureTraceCreated();
+  void EnsureTraceCreated() EXCLUDES(mutex_);
 
  protected:
   ThreadPoolTask* BindTask(InboundCallHandler* handler, int64_t rpc_queue_limit);
@@ -240,9 +242,6 @@ class InboundCall : public RpcCall, public MPSCQueueEntry<InboundCall> {
   CallData request_data_;
   std::atomic<size_t> request_data_memory_usage_{0};
 
-  // The trace buffer.
-  scoped_refptr<Trace> trace_;
-
   // Timing information related to this RPC call.
   InboundCallTiming timing_;
 
@@ -253,10 +252,14 @@ class InboundCall : public RpcCall, public MPSCQueueEntry<InboundCall> {
   scoped_refptr<Counter> rpc_method_response_bytes_;
   scoped_refptr<Histogram> rpc_method_handler_latency_;
 
-  bool cleared_ = false;
   mutable simple_spinlock mutex_;
+  bool cleared_ GUARDED_BY(mutex_) = false;
 
  private:
+  // The trace buffer.
+  scoped_refptr<Trace> trace_holder_ GUARDED_BY(mutex_);
+  std::atomic<Trace*> trace_ = nullptr;
+
   // The connection on which this inbound call arrived. Can be null for LocalYBInboundCall.
   ConnectionPtr conn_ = nullptr;
   RpcMetrics* rpc_metrics_;

src/yb/rpc/yb_rpc.cc
@@ -380,8 +380,9 @@ string YBInboundCall::ToString() const {
 bool YBInboundCall::DumpPB(const DumpRunningRpcsRequestPB& req,
                            RpcCallInProgressPB* resp) {
   header_.ToPB(resp->mutable_header());
-  if (req.include_traces() && trace_) {
-    resp->set_trace_buffer(trace_->DumpToString(true));
+  auto my_trace = trace();
+  if (req.include_traces() && my_trace) {
+    resp->set_trace_buffer(my_trace->DumpToString(true));
   }
   resp->set_elapsed_millis(MonoTime::Now().GetDeltaSince(timing_.time_received)
       .ToMilliseconds());
@@ -399,20 +400,22 @@ void YBInboundCall::LogTrace() const {
       // The traces may also be too large to fit in a log message.
       LOG(WARNING) << ToString() << " took " << total_time << "ms (client timeout "
                    << header_.timeout_ms << "ms).";
-      if (trace_) {
-        LOG(WARNING) << "Trace:\n" << trace_->DumpToString(1, true);
+      auto my_trace = trace();
+      if (my_trace) {
+        LOG(WARNING) << "Trace:\n" << my_trace->DumpToString(1, true);
       }
       return;
     }
   }
 
+  auto my_trace = trace();
   if (PREDICT_FALSE(
-          (trace_ && trace_->must_print()) ||
+          (my_trace && my_trace->must_print()) ||
           FLAGS_rpc_dump_all_traces ||
           total_time > FLAGS_rpc_slow_query_threshold_ms)) {
     LOG(INFO) << ToString() << " took " << total_time << "ms. Trace:";
-    if (trace_) {
-      trace_->Dump(&LOG(INFO), true);
+    if (my_trace) {
+      my_trace->Dump(&LOG(INFO), true);
     }
   }
 }

src/yb/yql/cql/cqlserver/cql_rpc.cc
@@ -330,6 +330,7 @@ void CQLInboundCall::GetCallDetails(rpc::RpcCallInProgressPB *call_in_progress_p
 void CQLInboundCall::LogTrace() const {
   MonoTime now = MonoTime::Now();
   auto total_time = now.GetDeltaSince(timing_.time_received).ToMilliseconds();
+  auto trace_ = trace();
   if (PREDICT_FALSE(FLAGS_rpc_dump_all_traces
           // rpcs with an invalid request may have a null request_
           || (trace_ && request_ && request_->trace_requested())
@@ -352,6 +353,7 @@ std::string CQLInboundCall::ToString() const {
 bool CQLInboundCall::DumpPB(const rpc::DumpRunningRpcsRequestPB& req,
                             rpc::RpcCallInProgressPB* resp) {
 
+  auto trace_ = trace();
   if (req.include_traces() && trace_) {
     resp->set_trace_buffer(trace_->DumpToString(true));
   }

src/yb/yql/redis/redisserver/redis_rpc.cc
@@ -287,6 +287,7 @@ void RedisInboundCall::LogTrace() const {
   MonoTime now = MonoTime::Now();
   auto total_time = now.GetDeltaSince(timing_.time_received).ToMilliseconds();
 
+  auto trace_ = trace();
   if (PREDICT_FALSE(FLAGS_rpc_dump_all_traces
           || (trace_ && trace_->must_print())
           || total_time > FLAGS_rpc_slow_query_threshold_ms)) {
@@ -306,6 +307,7 @@ string RedisInboundCall::ToString() const {
 
 bool RedisInboundCall::DumpPB(const rpc::DumpRunningRpcsRequestPB& req,
                               rpc::RpcCallInProgressPB* resp) {
+  auto trace_ = trace();
   if (req.include_traces() && trace_) {
     resp->set_trace_buffer(trace_->DumpToString(true));
   }