40be019026b471dd89ed6a071f0c8609be9d00f6
Shubham <svarshney@yugabyte.com>
2023-04-18 09:59:37 +0000

    [BACKPORT 2.17.3][PLAT-8417] Do not return true from updateKubeConfig method when trying to set pull secret for k8s provider
    Summary:
    Recently with https://phabricator.dev.yugabyte.com/D24331 we started returning `true` when we were setting `pullSecretContent` for k8s provider.
    This introduced the issue for k8s deployments using service accounts as we have a check in place https://github.com/yugabyte/yugabyte-db/blob/master/managed/src/main/java/com/yugabyte/yw/controllers/handlers/CloudProviderHandler.java#L274 where we set kubeConfig as "" for service accounts.
    Now since above as returning true, we were skipping this step here & hence the deployment were failing. Fixed this.
    Depends on D24540
    
    Test Plan:
    UTs
    iTest pipeline
    testcustomnamespace-k8s-rf3-and-rf3
    
    Reviewers: anijhawan, anabaria
    
    Reviewed By: anabaria
    
    Subscribers: jenkins-bot, yugaware
    
    Differential Revision: https://phabricator.dev.yugabyte.com/D24563

--- managed/src/main/java/com/yugabyte/yw/controllers/handlers/CloudProviderHandler.java
+++ managed/src/main/java/com/yugabyte/yw/controllers/handlers/CloudProviderHandler.java
@@ -465,9 +465,6 @@ public class CloudProviderHandler {
         k8sMetadata.setKubernetesPullSecret(pullSecretFile);
         k8sMetadata.setKubernetesPullSecretName(null);
         k8sMetadata.setKubernetesPullSecretContent(null);
-
-        // In case the pull secret is specified.
-        return true;
       }
     }
     return hasKubeConfig;
@@ -1023,6 +1020,9 @@ public class CloudProviderHandler {
 
     Map<String, String> providerConfig = CloudInfoInterface.fetchEnvVars(reqProvider);
     boolean isConfigInProvider = updateKubeConfig(provider, providerConfig, edit);
+    // We will update the pull secret related infotmation for the provider.
+    Map<String, String> updatedProviderConfig = CloudInfoInterface.fetchEnvVars(provider);
+
     Map<String, String> regionConfig = CloudInfoInterface.fetchEnvVars(rd);
     Region region = Region.getByCode(provider, rd.code);
     if (region == null) {
@@ -1063,7 +1063,7 @@ public class CloudProviderHandler {
     if (regionUpdateNeeded || isConfigInRegion) {
       region.save();
     }
-    if (isConfigInProvider) {
+    if (isConfigInProvider || !providerConfig.equals(updatedProviderConfig)) {
       // Top level provider properties are handled in `updateProviderData` with other provider
       // types.
       provider.save();
