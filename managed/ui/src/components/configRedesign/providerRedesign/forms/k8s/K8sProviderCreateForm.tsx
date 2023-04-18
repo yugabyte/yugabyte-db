d754fa203fb2534baad0ab3a59d631f5f384c0f0
Jethro Mak <88681329+Jethro-M@users.noreply.github.com>
2023-04-18 13:56:57 -0400

    [BACKPORT 2.18][PLAT-8453] Add loading spinners for onprem and k8s forms
    Summary:
    This diff adds a loading spinner when the create/edit onprem/k8s provider forms are validating or
    submitting.
    
    Original commit: 5dca862887550b905468cf276631b4aeb6943ff8 / D24578
    
    Test Plan:
    Verify that the spinner shows up when the form is validating or submitting.
    The spinner should be right aligned above the submit button.
    
    Reviewers: rmadhavan
    
    Reviewed By: rmadhavan
    
    Subscribers: yugaware, jenkins-bot
    
    Differential Revision: https://phabricator.dev.yugabyte.com/D24669

--- managed/ui/src/components/configRedesign/providerRedesign/forms/k8s/K8sProviderCreateForm.tsx
+++ managed/ui/src/components/configRedesign/providerRedesign/forms/k8s/K8sProviderCreateForm.tsx
@@ -6,7 +6,7 @@
  */
 import React, { useState } from 'react';
 import JsYaml from 'js-yaml';
-import { Box, FormHelperText, Typography } from '@material-ui/core';
+import { Box, CircularProgress, FormHelperText, Typography } from '@material-ui/core';
 import { FormProvider, SubmitHandler, useForm } from 'react-hook-form';
 import { array, mixed, object, string } from 'yup';
 import { toast } from 'react-toastify';
@@ -370,6 +370,11 @@ export const K8sProviderCreateForm = ({
                 </FormHelperText>
               )}
             </FieldGroup>
+            {(formMethods.formState.isValidating || formMethods.formState.isSubmitting) && (
+              <Box display="flex" gridGap="5px" marginLeft="auto">
+                <CircularProgress size={16} color="primary" thickness={5} />
+              </Box>
+            )}
           </Box>
           <Box marginTop="16px">
             <YBButton
--- managed/ui/src/components/configRedesign/providerRedesign/forms/k8s/K8sProviderEditForm.tsx
+++ managed/ui/src/components/configRedesign/providerRedesign/forms/k8s/K8sProviderEditForm.tsx
@@ -6,7 +6,7 @@
  */
 import React, { useState } from 'react';
 import JsYaml from 'js-yaml';
-import { Box, FormHelperText, Typography } from '@material-ui/core';
+import { Box, CircularProgress, FormHelperText, Typography } from '@material-ui/core';
 import { FormProvider, SubmitHandler, useForm } from 'react-hook-form';
 import { array, mixed, object, string } from 'yup';
 import { toast } from 'react-toastify';
@@ -385,6 +385,11 @@ export const K8sProviderEditForm = ({
                 </FormHelperText>
               )}
             </FieldGroup>
+            {(formMethods.formState.isValidating || formMethods.formState.isSubmitting) && (
+              <Box display="flex" gridGap="5px" marginLeft="auto">
+                <CircularProgress size={16} color="primary" thickness={5} />
+              </Box>
+            )}
           </Box>
           <Box marginTop="16px">
             <YBButton
