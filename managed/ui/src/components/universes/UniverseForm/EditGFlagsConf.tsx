70e27c894739e463ccff35d082a62143b7ecd297
Rajagopalan Madhavan <rmadhavan@yugabyte.com>
2023-04-25 10:03:26 -0400

    Improve handling for LDAP strings in ysql_hba_conf_csv
    Summary:
    Configuring LDAP authentication for YSQL is extremely challenging for customers. There are a lot of obscure quoting and formatting rules that need to be followed.
    
    I have yet to see a customer get this right the first time which generates a lot of ticket volume.
    
    It would be helpful to have a GUI configuration (with a test button!) that can apply the requested LDAP configuration directly to the nodes. In the short term, perhaps we could provide a string builder UI that
    will accept all of the required and optional parameters, then generate a pre-escaped string that can be pasted directly into the GFlag configuration.
    
    UX requirements
    1. Separate each record field into a separate input (row) so users don’t have to concatenate the strings which involves complicated syntax with commas, 2x double quotation marks, etc.
    2. Validate each input row for syntax (static validation).
    3. Wrap text if input exceeds a row.
    4. Allow drag and drop for re-ordering of the rows/record fields.
    5. Low priority - User can preview the concatenated value.
    
    See interactive prototype & Figma handoff file.
    
    Test Plan:
    Please refer to the video
    {F36382}
    
    Reviewers: jmak
    
    Reviewed By: jmak
    
    Subscribers: jenkins-bot, yugaware
    
    Differential Revision: https://phabricator.dev.yugabyte.com/D24254

---
+++ managed/ui/src/components/universes/UniverseForm/EditGFlagsConf.tsx
@@ -0,0 +1,294 @@
+import React, { FC, useState } from 'react';
+import { FieldArray } from 'formik';
+import clsx from 'clsx';
+import { useTranslation } from 'react-i18next';
+import { Box, makeStyles, InputAdornment, IconButton } from '@material-ui/core';
+import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd';
+import { YBButton, YBInput } from '../../../redesign/components';
+import { isNonEmptyArray, isNonEmptyString } from '../../../utils/ObjectUtils';
+import {
+  CONST_VALUES,
+  GFLAG_EDIT,
+  formatLDAPConf,
+  verifyLDAPAttributes,
+  unformatLDAPConf
+} from '../../../utils/UniverseUtils';
+import { ReactComponent as DraggableIcon } from '../../../redesign/assets/draggable.svg';
+import { ReactComponent as CloseIcon } from '../../../redesign/assets/close.svg';
+
+const useStyles = makeStyles((theme) => ({
+  numNodesInputField: {
+    overflow: 'auto',
+    maxHeight: '206px'
+  },
+  gflagBox: {
+    marginBottom: theme.spacing(2)
+  },
+  draggableIcon: {
+    marginLeft: theme.spacing(1),
+    cursor: 'pointer'
+  },
+  overrideMuiInput: {
+    '& .MuiInput-root.MuiInput-multiline': {
+      padding: 4
+    }
+  },
+  overrideMuiHelperText: {
+    '& .MuiFormHelperText-root': {
+      color: 'orange'
+    }
+  }
+}));
+
+interface EditGFlagConfProps {
+  formProps: any;
+  mode: string;
+}
+
+interface GFlagRowProps {
+  id: string;
+  index: number;
+  content: string;
+  error: boolean;
+}
+
+const getGFlagRows = (rowCount: number) =>
+  Array.from({ length: rowCount }, (v, k) => k).map((k) => ({
+    id: `item-${k}`,
+    index: k,
+    content: CONST_VALUES.EMPTY_STRING,
+    error: false,
+    errorMessage: '',
+    isWarning: false
+  }));
+
+const reorderGFlagRows = (GFlagRows: any, startIndex: number, endIndex: number) => {
+  let result = Array.from(GFlagRows);
+  const [removed] = result.splice(startIndex, 1);
+  result.splice(endIndex, 0, removed);
+  result = result.filter((data) => data !== undefined);
+  return result;
+};
+
+export const EditGFlagsConf: FC<EditGFlagConfProps> = ({ formProps, mode }) => {
+  let unformattedLDAPConf: string | null = null;
+  const GFlagConfData = CONST_VALUES.EMPTY_STRING;
+  const { t } = useTranslation();
+  const classes = useStyles();
+
+  // Define state variables
+  const [rowCount, setRowCount] = useState<number>(2);
+  const GFlagValueConfObject = formProps?.values?.flagvalueobject;
+  if (mode === GFLAG_EDIT && !GFlagValueConfObject) {
+    unformattedLDAPConf = unformatLDAPConf(formProps?.values?.flagvalue);
+  }
+
+  const [errorMessage, setErrorMessage] = useState<string>(CONST_VALUES.EMPTY_STRING);
+  const [GFlagRows, setGFlagConfRows] = useState<any>(
+    GFlagValueConfObject?.length > 0
+      ? GFlagValueConfObject
+      : mode === GFLAG_EDIT
+      ? unformattedLDAPConf
+      : getGFlagRows(rowCount)
+  );
+
+  const getPlaceholder = (index: number) => {
+    let message = CONST_VALUES.EMPTY_STRING;
+    switch (index) {
+      case 0:
+        message = 'universeForm.gFlags.hbaLDAPConflocal';
+        break;
+      case 1:
+        message = 'universeForm.gFlags.hbaLDAPConfHost';
+        break;
+      case 2:
+        message = 'universeForm.gFlags.hbaLDAPConfHostSSL';
+        break;
+    }
+    return message;
+  };
+
+  const addItem = (rowCount: number) => {
+    GFlagRows.push({
+      id: `item-${rowCount - 1}`,
+      index: rowCount - 1,
+      content: CONST_VALUES.EMPTY_STRING,
+      error: false,
+      errorMessage: '',
+      isWarning: false
+    });
+    return GFlagRows;
+  };
+
+  const removeItem = (index: number) => {
+    GFlagRows.splice(index, 1);
+    setGFlagConfRows(GFlagRows);
+    setRowCount(rowCount - 1);
+    buildGFlagConf(GFlagRows);
+  };
+
+  const handleRowClick = () => {
+    const addedRow = addItem(rowCount + 1);
+    setGFlagConfRows(addedRow);
+    setRowCount(rowCount + 1);
+  };
+
+  const buildGFlagConf = (arrangedRows?: any) => {
+    const rows = arrangedRows ?? GFlagRows;
+    let isGFlagInvalid = false;
+    const filteredRows = rows?.filter((GFlagRow: GFlagRowProps) => {
+      isGFlagInvalid = isGFlagInvalid || GFlagRow.error;
+      return isNonEmptyString(GFlagRow.content);
+    });
+
+    const confData = filteredRows?.reduce((accumulator: string, GFlagRow: any, index: number) => {
+      let appendChar = CONST_VALUES.COMMA_SEPARATOR;
+      const endChar = CONST_VALUES.EMPTY_STRING;
+      let GFlagRowContent = GFlagRow.content;
+
+      if (index === 0) {
+        appendChar = CONST_VALUES.EMPTY_STRING;
+      }
+      if (GFlagRowContent.includes(CONST_VALUES.LDAP)) {
+        GFlagRowContent = `"${formatLDAPConf(GFlagRowContent)}"`;
+      }
+
+      return accumulator + appendChar + GFlagRowContent + endChar;
+    }, GFlagConfData);
+
+    isGFlagInvalid
+      ? formProps.setFieldValue('flagvalue', CONST_VALUES.EMPTY_STRING)
+      : formProps.setFieldValue('flagvalue', confData);
+    formProps.setFieldValue('previewFlagValue', confData);
+    formProps.setFieldValue('flagvalueobject', GFlagRows);
+    formProps.setFieldValue('previewFlagError', errorMessage);
+  };
+
+  // Triggered when users enters any value in the text box
+  const handleChange = (GFlagInput: string, index: number) => {
+    const { isErrorInValidation, errorMessage, isWarning } = validateInput(GFlagInput);
+
+    // Set row in error state if isErrorInValidation is true
+    isErrorInValidation ? (GFlagRows[index].error = true) : (GFlagRows[index].error = false);
+    // Set row in warning state if isWarning is true
+    GFlagRows[index].isWarning = isWarning;
+    GFlagRows[index].errorMessage = errorMessage;
+    isWarning || isNonEmptyString(errorMessage)
+      ? setErrorMessage(errorMessage)
+      : setErrorMessage(CONST_VALUES.EMPTY_STRING);
+
+    GFlagRows[index].content = GFlagInput;
+  };
+
+  const validateInput = (GFlagInput: string) => {
+    const { isAttributeInvalid, errorMessage, isWarning } = verifyLDAPAttributes(GFlagInput);
+
+    return {
+      isErrorInValidation: isAttributeInvalid,
+      errorMessage,
+      isWarning
+    };
+  };
+
+  // The onDragEnd event occurs when a user has finished dragging a selection.
+  const onDragEnd = (result: any) => {
+    if (!result.destination) {
+      return;
+    }
+
+    const reorderedItems = reorderGFlagRows(
+      GFlagRows,
+      result.source.index,
+      result.destination.index
+    );
+
+    setGFlagConfRows(reorderedItems);
+    buildGFlagConf(reorderedItems);
+  };
+
+  return (
+    <Box className={classes.numNodesInputField}>
+      <DragDropContext onDragEnd={onDragEnd}>
+        <Droppable droppableId="droppable">
+          {(provided, snapshot) => (
+            <div {...provided.droppableProps} ref={provided.innerRef}>
+              <FieldArray
+                name="flagvalue"
+                render={() => (
+                  <>
+                    {/* eslint-disable-next-line react/display-name */}
+                    {GFlagRows?.map((item: any, index: number) => (
+                      <Draggable key={item.id} draggableId={item.id} index={index}>
+                        {(provided, snapshot) => (
+                          <div
+                            className={classes.gflagBox}
+                            ref={provided.innerRef}
+                            {...provided.draggableProps}
+                            {...provided.dragHandleProps}
+                          >
+                            <Box display="flex" flexDirection="row">
+                              <YBInput
+                                name={`flagvalue.${index}`}
+                                id={`${index}`}
+                                fullWidth
+                                multiline={true}
+                                minRows={1}
+                                maxRows={8}
+                                placeholder={t(getPlaceholder(item.index))}
+                                defaultValue={
+                                  isNonEmptyArray(GFlagValueConfObject) ||
+                                  isNonEmptyArray(unformattedLDAPConf)
+                                    ? GFlagRows[index].content
+                                    : CONST_VALUES.EMPTY_STRING
+                                }
+                                onChange={(e: any) => handleChange(e.target.value, index)}
+                                onBlur={() => buildGFlagConf()}
+                                error={GFlagRows[index]?.error}
+                                helperText={t(GFlagRows[index]?.errorMessage)}
+                                inputProps={{
+                                  'data-testid': `UniverseGFlagsConfField-item${index}`
+                                }}
+                                className={clsx(classes.overrideMuiInput, {
+                                  [classes.overrideMuiHelperText]: GFlagRows[index]?.isWarning
+                                })}
+                                InputProps={{
+                                  startAdornment: (
+                                    <Box className={classes.draggableIcon}>
+                                      <InputAdornment position="start">
+                                        <DraggableIcon />
+                                      </InputAdornment>
+                                    </Box>
+                                  )
+                                }}
+                              />
+                              <IconButton
+                                onClick={() => {
+                                  removeItem(index);
+                                }}
+                              >
+                                <CloseIcon />
+                              </IconButton>
+                            </Box>
+                          </div>
+                        )}
+                      </Draggable>
+                    ))}
+                  </>
+                )}
+              ></FieldArray>
+            </div>
+          )}
+        </Droppable>
+      </DragDropContext>
+
+      <YBButton
+        variant="primary"
+        onClick={handleRowClick}
+        size="medium"
+        data-testid={`UniverseGFlagsConfField-AddRowButton`}
+      >
+        {t('universeForm.gFlags.addRow')}
+      </YBButton>
+    </Box>
+  );
+};
---
+++ managed/ui/src/components/universes/UniverseForm/PreviewGFlagsConf.tsx
@@ -0,0 +1,34 @@
+import React, { FC } from 'react';
+import { Box } from '@material-ui/core';
+import { useTranslation } from 'react-i18next';
+import { YBTextarea } from '../../../redesign/components';
+import { CONST_VALUES, verifyLDAPAttributes } from '../../../utils/UniverseUtils';
+
+const CONF_PREFIX = '--ysql_hba_conf_csv=';
+interface PreviewConfFormProps {
+  formProps?: any;
+}
+
+export const PreviewGFlagsConf: FC<PreviewConfFormProps> = ({ formProps }) => {
+  const { t } = useTranslation();
+  const previewConfValue =
+    CONF_PREFIX +
+    CONST_VALUES.SINGLE_QUOTES_SEPARATOR +
+    formProps?.values?.previewFlagValue +
+    CONST_VALUES.SINGLE_QUOTES_SEPARATOR;
+  const { isAttributeInvalid, errorMessage, isWarning } = verifyLDAPAttributes(previewConfValue);
+
+  return (
+    <Box>
+      <YBTextarea
+        minRows={9}
+        maxRows={15}
+        readOnly={true}
+        value={previewConfValue}
+        error={isAttributeInvalid}
+        isWarning={isWarning}
+        message={t(errorMessage)}
+      />
+    </Box>
+  );
+};
