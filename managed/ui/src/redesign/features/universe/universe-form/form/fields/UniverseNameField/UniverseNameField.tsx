b0857181b812d94f48b01ba927280723626d703f
Lingeshwar S <lingesh100@gmail.com>
2023-04-25 11:26:26 +0530

    [PLAT-8532] - fix : Universe name validation missing for GCP and kubernetes universe
    Summary: Added missing validation for universe name field
    
    Test Plan:
    Tested Manually
    
    {F36809}
    
    Reviewers: asathyan, kkannan
    
    Reviewed By: asathyan, kkannan
    
    Subscribers: jenkins-bot, yugaware, ui
    
    Differential Revision: https://phabricator.dev.yugabyte.com/D24776

--- managed/ui/src/redesign/features/universe/universe-form/form/fields/UniverseNameField/UniverseNameField.tsx
+++ managed/ui/src/redesign/features/universe/universe-form/form/fields/UniverseNameField/UniverseNameField.tsx
@@ -1,11 +1,12 @@
 import React, { ReactElement } from 'react';
+import _ from 'lodash';
 import { useTranslation } from 'react-i18next';
-import { useFormContext, ValidateResult, Controller } from 'react-hook-form';
+import { useFormContext, ValidateResult, Controller, useWatch } from 'react-hook-form';
 import { Box } from '@material-ui/core';
 import { YBLabel, YBInput } from '../../../../../../components';
 import { api } from '../../../../../../helpers/api';
-import { UniverseFormData } from '../../../utils/dto';
-import { UNIVERSE_NAME_FIELD } from '../../../utils/constants';
+import { UniverseFormData, CloudType } from '../../../utils/dto';
+import { UNIVERSE_NAME_FIELD, PROVIDER_FIELD } from '../../../utils/constants';
 import { useFormFieldStyles } from '../../../universeMainStyle';
 
 interface UniverseNameFieldProps {
@@ -17,6 +18,8 @@ export const UniverseNameField = ({ disabled }: UniverseNameFieldProps): ReactEl
   const classes = useFormFieldStyles();
   const { t } = useTranslation();
 
+  const provider = useWatch({ name: PROVIDER_FIELD });
+
   // TODO : Move this to global helper file
   const trimSpecialChars = (string: String) => {
     return string?.replace(/[^a-zA-Z0-9/-]+/g, '');
@@ -26,6 +29,17 @@ export const UniverseNameField = ({ disabled }: UniverseNameFieldProps): ReactEl
   const validateUniverseName = async (value_: unknown): Promise<ValidateResult> => {
     const value = value_ as string;
     if (disabled || !value_) return true; // don't validate disabled field or empty
+    if ([CloudType.gcp, CloudType.kubernetes].includes(provider?.code)) {
+      const specialCharsRegex = /^[a-z0-9-]*$/;
+      const displayCode =
+        provider?.code === CloudType.gcp
+          ? CloudType.gcp.toUpperCase()
+          : _.upperFirst(CloudType.kubernetes);
+      if (!specialCharsRegex.test(value))
+        return t('universeForm.validation.universeNameValidation', {
+          cloudType: displayCode
+        }) as string;
+    }
     try {
       const universeList = await api.findUniverseByName(value);
       return universeList?.length
