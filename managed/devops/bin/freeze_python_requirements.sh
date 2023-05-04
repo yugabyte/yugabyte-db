be32e84ef5acf679216664664f15e25c7975869e
Muthu Chidambaram <mchidambaram@yugabyte.com>
2023-05-03 23:34:15 -0500

    [PLAT-8262] Adding azure host caching for data disks
    Summary:
    Default setting for disk caching is None for data disks. Using ReadOnly for data disks can increase
    performance for universes. This diff defaults all data disks on newly created universes to ReadOnly
    caching.
    
    Also upgraded Azure APIs because caching option was not present in older API.
    
    Test Plan: Create azure universe and ensure data disk caching is set properly. Verify delete also works.
    
    Reviewers: nsingh, yshchetinin, amalyshev
    
    Reviewed By: amalyshev
    
    Subscribers: jenkins-bot, yugaware
    
    Differential Revision: https://phabricator.dev.yugabyte.com/D24962

--- managed/devops/bin/freeze_python_requirements.sh
+++ managed/devops/bin/freeze_python_requirements.sh
@@ -57,13 +57,13 @@ log "Generating $FROZEN_REQUIREMENTS_FILE"
 
 # Patch grpcio and protobuf versions
 # The current version is only for python <= 3.9.*
-sed -ie 's/\(grpcio.*==.*\)/\1; python_version < 3.10/' $FROZEN_REQUIREMENTS_FILE
-sed -ie 's/\(protobuf.*==.*\)/\1; python_version < 3.10/' $FROZEN_REQUIREMENTS_FILE
+sed -ie "s/\(grpcio.*==.*\)/\1; python_version < '3.10'/" $FROZEN_REQUIREMENTS_FILE
+sed -ie "s/\(protobuf.*==.*\)/\1; python_version < '3.10'/" $FROZEN_REQUIREMENTS_FILE
 
 # Now, add our 3.10+ version of grpcio (and grpcio-tools)
-echo "grpcio==${GRPCIO_PY310_VERSION};python_version >= 3.10" >> $FROZEN_REQUIREMENTS_FILE
-echo "grpcio-tools==${GRPCIO_PY310_VERSION};python_version >= 3.10" >> $FROZEN_REQUIREMENTS_FILE
-echo "protobuf==${PROTOBUF_PY310_VERSION};python_version >= 3.10" >> $FROZEN_REQUIREMENTS_FILE
+echo "grpcio==${GRPCIO_PY310_VERSION};python_version >= '3.10'" >> $FROZEN_REQUIREMENTS_FILE
+echo "grpcio-tools==${GRPCIO_PY310_VERSION};python_version >= '3.10'" >> $FROZEN_REQUIREMENTS_FILE
+echo "protobuf==${PROTOBUF_PY310_VERSION};python_version >= '3.10'" >> $FROZEN_REQUIREMENTS_FILE
 
 
 log_empty_line
