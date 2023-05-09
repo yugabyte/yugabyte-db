yugabyte/yugabyte-db

commit 3b3bbe490aad25a58dab31dfc94badeebfd8d4aa

[PLAT-8238] Return default value for runtime keys
Summary:
To get value for runtime keys we query the DB, but values aren't written to the DB unless manually set via API/UI.
This results in 404 error when default value should be returned.

To fix this, we now fetch the default value from static config if DB doesn't have value for a key.

Test Plan: Tested manually at platform startup and after resetting key via UI.

Reviewers: sb-yb

Reviewed By: sb-yb

Subscribers: jenkins-bot, yugaware

Differential Revision: https://phabricator.dev.yugabyte.com/D24394

managed/src/main/java/com/yugabyte/yw/common/config/RuntimeConfService.java
@@ -10,6 +10,8 @@
 
 package com.yugabyte.yw.common.config;
 
+import static com.yugabyte.yw.models.ScopedRuntimeConfig.GLOBAL_SCOPE_UUID;
+
 import com.google.common.collect.ImmutableSet;
 import com.google.common.collect.Sets;
 import com.google.common.collect.Streams;
@@ -59,11 +61,14 @@ public class RuntimeConfService extends AuthenticatedController {
 
   private final Map<String, ConfKeyInfo<?>> keyMetaData;
 
+  private final Config staticConfig;
+
   @Inject
   public RuntimeConfService(
       SettableRuntimeConfigFactory settableRuntimeConfigFactory,
       RuntimeConfigPreChangeNotifier preChangeNotifier,
       RuntimeConfigChangeNotifier changeNotifier,
+      Config config,
       Map<String, ConfKeyInfo<?>> keyMetaData) {
     this.settableRuntimeConfigFactory = settableRuntimeConfigFactory;
     this.mutableObjects =
@@ -74,6 +79,7 @@ public class RuntimeConfService extends AuthenticatedController {
     this.preChangeNotifier = preChangeNotifier;
     this.changeNotifier = changeNotifier;
     this.keyMetaData = keyMetaData;
+    this.staticConfig = config;
     this.mutableKeys = buildMutableKeysSet();
   }
 
@@ -142,12 +148,21 @@ public class RuntimeConfService extends AuthenticatedController {
 
   public String getKeyOrBadRequest(
       UUID customerUUID, UUID scopeUUID, String path, boolean isSuperAdmin) {
-    return maybeGetKey(customerUUID, scopeUUID, path, isSuperAdmin)
-        .orElseThrow(
-            () ->
-                new PlatformServiceException(
-                    NOT_FOUND,
-                    String.format("Key %s is not defined in scope %s", path, scopeUUID)));
+    Optional<String> value = maybeGetKey(customerUUID, scopeUUID, path, isSuperAdmin);
+    if (value.isPresent()) return value.get();
+    else {
+      // return default value if DB doesn't have value defined
+      if (isValidScope(path, scopeUUID)) return staticConfig.getString(path);
+      else {
+        throw new PlatformServiceException(
+            NOT_FOUND, String.format("Key %s is not defined in scope %s", path, scopeUUID));
+      }
+    }
+  }
+
+  private boolean isValidScope(String path, UUID scopeUUID) {
+    ScopeType scope = keyMetaData.get(path).getScope();
+    return scope.isValid(scopeUUID);
   }
 
   public String getKeyIfPresent(

managed/src/test/java/com/yugabyte/yw/controllers/RuntimeConfControllerTest.java
@@ -17,6 +17,7 @@ import static com.yugabyte.yw.models.helpers.ExternalScriptHelper.EXT_SCRIPT_CON
 import static com.yugabyte.yw.models.helpers.ExternalScriptHelper.EXT_SCRIPT_PARAMS_CONF_PATH;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertThrows;
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
 import static play.mvc.Http.Status.INTERNAL_SERVER_ERROR;
@@ -176,9 +177,11 @@ public class RuntimeConfControllerTest extends FakeDBApplication {
 
   @Test
   public void key() {
-    assertEquals(
-        NOT_FOUND,
-        assertPlatformException(() -> getKey(GLOBAL_SCOPE_UUID, GC_CHECK_INTERVAL_KEY)).status());
+    String defaultInterval = "1 hour";
+    Result res = getKey(GLOBAL_SCOPE_UUID, GC_CHECK_INTERVAL_KEY);
+    assertEquals(OK, res.status());
+    assertEquals(defaultInterval, contentAsString(res));
+
     String newInterval = "2 days";
     assertEquals(OK, setGCInterval(newInterval, GLOBAL_SCOPE_UUID).status());
     RuntimeConfigFactory runtimeConfigFactory =
@@ -188,8 +191,7 @@ public class RuntimeConfControllerTest extends FakeDBApplication {
     assertEquals(newInterval, contentAsString(getKey(GLOBAL_SCOPE_UUID, GC_CHECK_INTERVAL_KEY)));
     assertEquals(OK, deleteKey(GLOBAL_SCOPE_UUID, GC_CHECK_INTERVAL_KEY).status());
     assertEquals(
-        NOT_FOUND,
-        assertPlatformException(() -> getKey(GLOBAL_SCOPE_UUID, GC_CHECK_INTERVAL_KEY)).status());
+        defaultInterval, contentAsString(getKey(GLOBAL_SCOPE_UUID, GC_CHECK_INTERVAL_KEY)));
   }
 
   private Result setGCInterval(String interval, UUID scopeUUID) {
@@ -198,11 +200,11 @@ public class RuntimeConfControllerTest extends FakeDBApplication {
 
   @Test
   public void keyObj() {
-    assertEquals(
-        NOT_FOUND,
-        assertPlatformException(
-                () -> getKey(defaultUniverse.getUniverseUUID(), GC_CHECK_INTERVAL_KEY))
-            .status());
+    String defaultInterval = "1 hour";
+    Result res = getKey(GLOBAL_SCOPE_UUID, GC_CHECK_INTERVAL_KEY);
+    assertEquals(OK, res.status());
+    assertEquals(defaultInterval, contentAsString(res));
+
     String newInterval = "2 days";
     String newRetention = "32 days";
     assertEquals(
@@ -248,11 +250,10 @@ public class RuntimeConfControllerTest extends FakeDBApplication {
         OK,
         deleteKey(defaultUniverse.getUniverseUUID(), EXT_SCRIPT_KEY).status());
 
-    assertEquals(
-        "The object was deleted. So expecting NOT_FOUND status",
-        NOT_FOUND,
-        assertPlatformException(() -> getKey(defaultUniverse.getUniverseUUID(), EXT_SCRIPT_KEY))
-            .status());
+    // Expect NullPointerException since default value in reference.conf is null
+    assertThrows(
+        NullPointerException.class,
+        () -> getKey(defaultUniverse.getUniverseUUID(), EXT_SCRIPT_KEY));
   }
 
   private Result setExtScriptObject(String schedule, String content, UUID scopeUUID) {

commit 39f022e70a4ba9332e8a7832ac608adad14cb64f

[PLAT-7425][SSO]Unit test for new runtime config for OIDC
Summary:
Unit test for new config value
Also fix NPE bug introduced by D24394

Test Plan: unit test

Reviewers: asharma

Reviewed By: asharma

Subscribers: yugaware

Differential Revision: https://phabricator.dev.yugabyte.com/D25166

managed/src/main/java/com/yugabyte/yw/common/config/GlobalConfKeys.java
@@ -91,6 +91,14 @@ public class GlobalConfKeys extends RuntimeConfigKeysModule {
           "Hidden because this key has dedicated UI",
           ConfDataType.StringType,
           ImmutableList.of(ConfKeyTags.UIDriven));
+  public static final ConfKeyInfo<String> oidcProviderMetadata =
+      new ConfKeyInfo<>(
+          "yb.security.oidcProviderMetadata",
+          ScopeType.GLOBAL,
+          "Provider Metadata from discoveryURI",
+          "Hidden because this key has dedicated UI",
+          ConfDataType.StringType,
+          ImmutableList.of(ConfKeyTags.UIDriven));
   public static final ConfKeyInfo<String> oidcScope =
       new ConfKeyInfo<>(
           "yb.security.oidcScope",

managed/src/main/java/com/yugabyte/yw/common/config/RuntimeConfService.java
@@ -157,8 +157,12 @@ public class RuntimeConfService extends AuthenticatedController {
   }
 
   private boolean isValidScope(String path, UUID scopeUUID) {
-    ScopeType scope = keyMetaData.get(path).getScope();
-    return scope.isValid(scopeUUID);
+    if (keyMetaData.containsKey(path)) {
+      ScopeType scope = keyMetaData.get(path).getScope();
+      return scope.isValid(scopeUUID);
+    } else {
+      return true;
+    }
   }
 
   public String getKeyIfPresent(

managed/src/main/resources/reference.conf
@@ -951,6 +951,7 @@ runtime_config {
     "yb.security.clientID",
     "yb.security.secret",
     "yb.security.discoveryURI",
+    "yb.security.oidcProviderMetadata",
     "yb.security.oidcScope",
     "yb.security.oidcEmailAttribute",
     "yb.security.ssh2_enabled"

managed/src/test/java/com/yugabyte/yw/controllers/RuntimeConfControllerTest.java
@@ -32,11 +32,15 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.common.collect.ImmutableSet;
+import com.nimbusds.oauth2.sdk.ParseException;
+import com.nimbusds.openid.connect.sdk.op.OIDCProviderMetadata;
 import com.typesafe.config.Config;
+import com.typesafe.config.ConfigException;
 import com.typesafe.config.ConfigFactory;
 import com.yugabyte.yw.common.FakeDBApplication;
 import com.yugabyte.yw.common.ModelFactory;
 import com.yugabyte.yw.common.PlatformServiceException;
+import com.yugabyte.yw.common.TestUtils;
 import com.yugabyte.yw.common.config.ConfKeyInfo.ConfKeyTags;
 import com.yugabyte.yw.common.config.CustomerConfKeys;
 import com.yugabyte.yw.common.config.GlobalConfKeys;
@@ -193,6 +197,33 @@ public class RuntimeConfControllerTest extends FakeDBApplication {
         defaultInterval, contentAsString(getKey(GLOBAL_SCOPE_UUID, GC_CHECK_INTERVAL_KEY)));
   }
 
+  // Multiline json should be posted as triple quoted string
+  // UI should wrap before request such multiline content in
+  // triple quotes (and similarly unwrap the response)
+  @Test
+  public void multilineJsonContent() throws ParseException {
+    String metadataKey = "yb.security.oidcProviderMetadata";
+    String defaultValue = "";
+    Result res = getKey(GLOBAL_SCOPE_UUID, metadataKey);
+    assertEquals(OK, res.status());
+    assertEquals(defaultValue, contentAsString(res));
+    String multilineJsonAsString =
+        TestUtils.readResource("com/yugabyte/yw/controllers/test_openid_config.txt");
+    String tripleQuoted = "\"\"\"" + multilineJsonAsString + "\"\"\"";
+    assertEquals(OK, setKey(metadataKey, tripleQuoted, GLOBAL_SCOPE_UUID).status());
+    RuntimeConfigFactory runtimeConfigFactory =
+        app.injector().instanceOf(RuntimeConfigFactory.class);
+    String m = runtimeConfigFactory.globalRuntimeConf().getString(metadataKey);
+    OIDCProviderMetadata metadata = OIDCProviderMetadata.parse(m);
+    // internally we get it without quotes
+    assertEquals(multilineJsonAsString, m);
+    res = getKey(GLOBAL_SCOPE_UUID, metadataKey);
+    assertEquals(OK, res.status());
+
+    // triple quotes are preserved on HTTP GET response
+    assertEquals(tripleQuoted, contentAsString(res));
+  }
+
   private Result setGCInterval(String interval, UUID scopeUUID) {
     return setKey(GC_CHECK_INTERVAL_KEY, interval, scopeUUID);
   }
@@ -249,9 +280,9 @@ public class RuntimeConfControllerTest extends FakeDBApplication {
         OK,
         deleteKey(defaultUniverse.getUniverseUUID(), EXT_SCRIPT_KEY).status());
 
-    // Expect NullPointerException since default value in reference.conf is null
+    // Expect ConfigException since default value in reference.conf is null
     assertThrows(
-        NullPointerException.class,
+        ConfigException.WrongType.class,
         () -> getKey(defaultUniverse.getUniverseUUID(), EXT_SCRIPT_KEY));
   }
 