yugabyte/yugabyte-db

commit 8efef848c5a7298a322fe3e2766811d78aa49520

[PLAT-4700][PLAT-4989] Fix java S3 client for AWS IAM based access
Summary:
- Added support for IAM backups on YB-Controller
- Reverted temporary fix for IAM: [[ https://phabricator.dev.yugabyte.com/D18372 | D18372 ]]
- Added fix for [PLAT-4989]: In the case of YEDIS backup, use yb_backup instead of yb-controller

Test Plan:
- Manually verified S3 config creation
- Manually verified S3 backups using IAM in YB-Controller
- Manually verified S3 backups deletion

Reviewers: vpatibandla, spotachev

Reviewed By: spotachev

Subscribers: jenkins-bot, yugaware

Differential Revision: https://phabricator.dev.yugabyte.com/D18877

managed/src/main/java/com/yugabyte/yw/common/AWSUtil.java
@@ -2,54 +2,57 @@
 
 package com.yugabyte.yw.common;
 
-import static play.mvc.Http.Status.INTERNAL_SERVER_ERROR;
-
 import com.amazonaws.SdkClientException;
-import static play.mvc.Http.Status.BAD_REQUEST;
 
-import com.amazonaws.AmazonClientException;
 import com.amazonaws.auth.AWSCredentials;
 import com.amazonaws.auth.AWSCredentialsProvider;
 import com.amazonaws.auth.AWSStaticCredentialsProvider;
 import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.auth.BasicSessionCredentials;
 import com.amazonaws.auth.InstanceProfileCredentialsProvider;
 import com.amazonaws.client.builder.AwsClientBuilder.EndpointConfiguration;
+import com.amazonaws.services.ec2.model.AmazonEC2Exception;
+import com.amazonaws.services.identitymanagement.AmazonIdentityManagement;
+import com.amazonaws.services.identitymanagement.AmazonIdentityManagementClient;
+import com.amazonaws.services.identitymanagement.model.AmazonIdentityManagementException;
+import com.amazonaws.services.identitymanagement.model.GetRoleRequest;
+import com.amazonaws.services.identitymanagement.model.Role;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3Client;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
-import com.amazonaws.services.s3.internal.AmazonS3ExceptionBuilder;
 import com.amazonaws.services.s3.model.AmazonS3Exception;
 import com.amazonaws.services.s3.model.Bucket;
 import com.amazonaws.services.s3.model.DeleteObjectsRequest;
 import com.amazonaws.services.s3.model.GetBucketLocationRequest;
 import com.amazonaws.services.s3.model.DeleteObjectsRequest.KeyVersion;
-import com.amazonaws.services.s3.model.GetObjectRequest;
+import com.amazonaws.services.securitytoken.AWSSecurityTokenService;
+import com.amazonaws.services.securitytoken.AWSSecurityTokenServiceClient;
+import com.amazonaws.services.securitytoken.AWSSecurityTokenServiceClientBuilder;
+import com.amazonaws.services.securitytoken.model.AssumeRoleRequest;
+import com.amazonaws.services.securitytoken.model.AssumeRoleResult;
+import com.amazonaws.services.securitytoken.model.Credentials;
+import com.amazonaws.util.EC2MetadataUtils;
+import com.amazonaws.util.EC2MetadataUtils.IAMSecurityCredential;
 import com.amazonaws.services.s3.model.ListObjectsV2Result;
-import com.amazonaws.services.s3.model.S3Object;
 import com.amazonaws.services.s3.model.S3ObjectSummary;
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.google.inject.Inject;
 import com.google.inject.Singleton;
 import com.yugabyte.yw.models.configs.data.CustomerConfigData;
 import com.yugabyte.yw.models.configs.data.CustomerConfigStorageS3Data;
-import java.io.InputStream;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Collection;
+import java.sql.Timestamp;
+import java.text.SimpleDateFormat;
+import java.time.ZoneId;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.StringJoiner;
+import java.util.TimeZone;
 import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.collections.CollectionUtils;
+import org.apache.commons.collections.MapUtils;
 import org.apache.commons.lang3.StringUtils;
-import org.yb.ybc.CloudStoreConfig;
 import org.yb.ybc.CloudStoreSpec;
-import org.yb.ybc.CloudType;
-import org.apache.http.HttpStatus;
 
 @Singleton
 @Slf4j
@@ -61,20 +64,22 @@ public class AWSUtil implements CloudUtil {
   private static final String AWS_STANDARD_HOST_BASE_PATTERN = "s3([.](.+)|)[.]amazonaws[.]com";
   public static final String AWS_DEFAULT_REGION = "us-east-1";
   public static final String AWS_DEFAULT_ENDPOINT = "s3.amazonaws.com";
+  public static final String AWS_DEFAULT_REGIONAL_STS_ENDPOINT = "sts.us-east-1.amazonaws.com";
 
   public static final String YBC_AWS_ACCESS_KEY_ID_FIELDNAME = "AWS_ACCESS_KEY_ID";
   public static final String YBC_AWS_SECRET_ACCESS_KEY_FIELDNAME = "AWS_SECRET_ACCESS_KEY";
   public static final String YBC_AWS_PATH_STYLE_ACCESS = "PATH_STYLE_ACCESS";
   public static final String YBC_AWS_ENDPOINT_FIELDNAME = "AWS_ENDPOINT";
   public static final String YBC_AWS_DEFAULT_REGION_FIELDNAME = "AWS_DEFAULT_REGION";
+  public static final String YBC_AWS_ACCESS_TOKEN_FIELDNAME = "AWS_ACCESS_TOKEN";
 
+  private static final String ZULU_TIME_FORMAT = "yyyy-MM-dd'T'HH:mm:ss'Z'";
   private static final Pattern standardHostBaseCompiled =
       Pattern.compile(AWS_STANDARD_HOST_BASE_PATTERN);
 
   // This method is a way to check if given S3 config can extract objects.
   public boolean canCredentialListObjects(CustomerConfigData configData, List<String> locations) {
-    if (CollectionUtils.isEmpty(locations)
-        || ((CustomerConfigStorageS3Data) configData).isIAMInstanceProfile) {
+    if (CollectionUtils.isEmpty(locations)) {
       return true;
     }
     for (String location : locations) {
@@ -135,34 +140,119 @@ public class AWSUtil implements CloudUtil {
     return split;
   }
 
+  // Fetch temporary credentials from EC2 metadata.
+  private Credentials getTemporaryCredentialsInstanceProfile() throws Exception {
+    Map<String, IAMSecurityCredential> instanceProfileCredentials =
+        EC2MetadataUtils.getIAMSecurityCredentials();
+    Credentials credentials = null;
+    if (MapUtils.isNotEmpty(instanceProfileCredentials)
+        && instanceProfileCredentials.values().iterator().hasNext()) {
+      IAMSecurityCredential credential = instanceProfileCredentials.values().iterator().next();
+      SimpleDateFormat formatter = new SimpleDateFormat(ZULU_TIME_FORMAT);
+      formatter.setTimeZone(TimeZone.getTimeZone("UTC"));
+      Date date = formatter.parse(credential.expiration);
+      credentials =
+          new Credentials(
+              credential.accessKeyId, credential.secretAccessKey, credential.token, date);
+    }
+    return credentials;
+  }
+
+  // Fetch temporary credentials using Assume role via STS client.
+  private synchronized Credentials getTemporaryCredentialsAssumeRole() throws Exception {
+    // Create STS client to make subsequent calls.
+    EndpointConfiguration ec =
+        new EndpointConfiguration(AWS_DEFAULT_REGIONAL_STS_ENDPOINT, AWS_DEFAULT_REGION);
+    AWSSecurityTokenServiceClientBuilder stsClientBuilder = AWSSecurityTokenServiceClient.builder();
+    stsClientBuilder.withEndpointConfiguration(ec);
+    AWSCredentialsProvider creds = new InstanceProfileCredentialsProvider(false);
+    AWSSecurityTokenService stsService = stsClientBuilder.withCredentials(creds).build();
+
+    // Fetch role name for the IAM instance, should be same as instance-profile name.
+    EC2MetadataUtils.IAMInfo iamInfo = EC2MetadataUtils.getIAMInstanceProfileInfo();
+    String instanceProfileArn = iamInfo.instanceProfileArn;
+    String[] arnSplit = instanceProfileArn.split("/", 0);
+    String role = arnSplit[arnSplit.length - 1];
+
+    // Fetch max session limit for the role.
+    AmazonIdentityManagement iamClient =
+        AmazonIdentityManagementClient.builder().withCredentials(creds).build();
+    Role iamRole = iamClient.getRole(new GetRoleRequest().withRoleName(role)).getRole();
+    int maxDuration = iamRole.getMaxSessionDuration();
+
+    // Generate temporary credentials valid until the max session duration
+    // required because we are sending creds to nodes, no mechanism to fetch creds via instance
+    // there.
+    Timestamp timestamp = new Timestamp(System.currentTimeMillis());
+    AssumeRoleRequest roleRequest =
+        new AssumeRoleRequest()
+            .withDurationSeconds(maxDuration)
+            .withRoleArn(iamRole.getArn())
+            .withRoleSessionName(Long.toString(timestamp.toInstant().toEpochMilli()));
+    AssumeRoleResult roleResult = stsService.assumeRole(roleRequest);
+    Credentials temporaryCredentials = roleResult.getCredentials();
+    return temporaryCredentials;
+  }
+
+  private Credentials getTemporaryCredentials() {
+    Credentials instanceCredentials = null;
+    Credentials assumeRoleCredentials = null;
+    try {
+      assumeRoleCredentials = getTemporaryCredentialsAssumeRole();
+    } catch (Exception e) {
+      log.error("Fetching temporary credentials from STS client failed: {}", e.getMessage());
+    }
+    try {
+      instanceCredentials = getTemporaryCredentialsInstanceProfile();
+    } catch (Exception e) {
+      log.error("Fetching instance credentials failed: {}", e.getMessage());
+    }
+    if (assumeRoleCredentials != null) {
+      if (assumeRoleCredentials.getExpiration().compareTo(instanceCredentials.getExpiration())
+          >= 0) {
+        log.info(
+            "Using assume role credentials with expiry: {}",
+            assumeRoleCredentials.getExpiration().toString());
+        return assumeRoleCredentials;
+      }
+      log.info(
+          "Assume role expiry: {} is less than instance profile credentials expiry: {},"
+              + "using instance profile credentials",
+          assumeRoleCredentials.getExpiration().toString(),
+          instanceCredentials.getExpiration().toString());
+      return instanceCredentials;
+    }
+    log.info(
+        "Unable to assume Role, defaulting to intance profile credentials with expiry: {}",
+        instanceCredentials.getExpiration().toString());
+    return instanceCredentials;
+  }
+
   public static AmazonS3 createS3Client(CustomerConfigStorageS3Data s3Data)
       throws AmazonS3Exception {
     AmazonS3ClientBuilder s3ClientBuilder = AmazonS3Client.builder();
+    AWSCredentialsProvider creds = null;
     if (s3Data.isIAMInstanceProfile) {
-      s3ClientBuilder.withCredentials(new InstanceProfileCredentialsProvider(false));
+      // Using instance creds from ec2.services.com here
+      // since the client is used on Platform itself unlike backups.
+      creds = new InstanceProfileCredentialsProvider(false);
     } else {
       String key = s3Data.awsAccessKeyId;
       String secret = s3Data.awsSecretAccessKey;
-      boolean isPathStyleAccess = s3Data.isPathStyleAccess;
-      String endpoint = s3Data.awsHostBase;
       AWSCredentials credentials = new BasicAWSCredentials(key, secret);
-      AWSCredentialsProvider creds = new AWSStaticCredentialsProvider(credentials);
-
-      s3ClientBuilder.withCredentials(creds).withForceGlobalBucketAccessEnabled(true);
-
-      if (isPathStyleAccess) {
-        s3ClientBuilder.withPathStyleAccessEnabled(true);
-      }
-      EndpointConfiguration endpointConfiguration = null;
-      if (StringUtils.isNotBlank(endpoint)) {
-        // Need to set default region because region-chaining may
-        // fail if correct environment variables not found.
-        endpointConfiguration = new EndpointConfiguration(endpoint, AWS_DEFAULT_REGION);
-      } else {
-        endpointConfiguration = new EndpointConfiguration(AWS_DEFAULT_ENDPOINT, AWS_DEFAULT_REGION);
-      }
-      s3ClientBuilder.withEndpointConfiguration(endpointConfiguration);
+      creds = new AWSStaticCredentialsProvider(credentials);
     }
+    s3ClientBuilder.withCredentials(creds).withForceGlobalBucketAccessEnabled(true);
+    EndpointConfiguration endpointConfiguration = null;
+    String endpoint = s3Data.awsHostBase;
+    if (StringUtils.isNotBlank(endpoint)) {
+      // Need to set default region because region-chaining may
+      // fail if correct environment variables not found.
+      endpointConfiguration = new EndpointConfiguration(endpoint, AWS_DEFAULT_REGION);
+    } else {
+      endpointConfiguration = new EndpointConfiguration(AWS_DEFAULT_ENDPOINT, AWS_DEFAULT_REGION);
+    }
+    s3ClientBuilder.withEndpointConfiguration(endpointConfiguration);
     return s3ClientBuilder.build();
   }
 
@@ -274,8 +364,16 @@ public class AWSUtil implements CloudUtil {
   private Map<String, String> createCredsMapYbc(CustomerConfigData configData, String bucket) {
     CustomerConfigStorageS3Data s3Data = (CustomerConfigStorageS3Data) configData;
     Map<String, String> s3CredsMap = new HashMap<>();
-    s3CredsMap.put(YBC_AWS_ACCESS_KEY_ID_FIELDNAME, s3Data.awsAccessKeyId);
-    s3CredsMap.put(YBC_AWS_SECRET_ACCESS_KEY_FIELDNAME, s3Data.awsSecretAccessKey);
+    if (s3Data.isIAMInstanceProfile) {
+      Credentials temporaryCredentials = getTemporaryCredentials();
+      s3CredsMap.put(YBC_AWS_ACCESS_TOKEN_FIELDNAME, temporaryCredentials.getSessionToken());
+      s3CredsMap.put(YBC_AWS_ACCESS_KEY_ID_FIELDNAME, temporaryCredentials.getAccessKeyId());
+      s3CredsMap.put(
+          YBC_AWS_SECRET_ACCESS_KEY_FIELDNAME, temporaryCredentials.getSecretAccessKey());
+    } else {
+      s3CredsMap.put(YBC_AWS_ACCESS_KEY_ID_FIELDNAME, s3Data.awsAccessKeyId);
+      s3CredsMap.put(YBC_AWS_SECRET_ACCESS_KEY_FIELDNAME, s3Data.awsSecretAccessKey);
+    }
     String bucketRegion = getBucketRegion(bucket, s3Data);
     String hostBase = getOrCreateHostBase(s3Data, bucket, bucketRegion);
     s3CredsMap.put(YBC_AWS_ENDPOINT_FIELDNAME, hostBase);

managed/src/main/java/com/yugabyte/yw/commissioner/BackupGarbageCollector.java
@@ -21,7 +21,6 @@ import com.yugabyte.yw.models.Backup;
 import com.yugabyte.yw.models.Backup.BackupCategory;
 import com.yugabyte.yw.models.Backup.BackupState;
 import com.yugabyte.yw.models.configs.CustomerConfig;
-import com.yugabyte.yw.models.configs.data.CustomerConfigStorageS3Data;
 import com.yugabyte.yw.models.Customer;
 import com.yugabyte.yw.models.Universe;
 import java.time.Duration;
@@ -149,32 +148,8 @@ public class BackupGarbageCollector {
         backup.transitionState(BackupState.DeleteInProgress);
         try {
           switch (customerConfig.name) {
-              // for cases S3, NFS, GCS, we get Util from CloudUtil class
+              // for cases S3, AZ, GCS, we get Util from CloudUtil class
             case S3:
-              CustomerConfigStorageS3Data s3Data =
-                  (CustomerConfigStorageS3Data) customerConfig.getDataObject();
-              if (s3Data.isIAMInstanceProfile) {
-                if (isUniversePresent(backup)) {
-                  BackupTableParams backupParams = backup.getBackupInfo();
-                  List<BackupTableParams> backupList =
-                      backupParams.backupList == null
-                          ? ImmutableList.of(backupParams)
-                          : backupParams.backupList;
-                  boolean success = deleteScriptBackup(backupList);
-                  if (success) {
-                    backup.delete();
-                    log.info("Backup {} is successfully deleted", backupUUID);
-                  } else {
-                    backup.transitionState(BackupState.FailedToDelete);
-                  }
-                } else {
-                  backup.transitionState(BackupState.FailedToDelete);
-                  log.info(
-                      "Cannot delete S3 IAM Backup {} as universe is not present",
-                      backup.backupUUID);
-                }
-                break;
-              }
             case GCS:
             case AZ:
               CloudUtil cloudUtil = CloudUtil.getCloudUtil(customerConfig.name);

managed/src/main/java/com/yugabyte/yw/commissioner/tasks/CreateBackup.java
@@ -85,7 +85,8 @@ public class CreateBackup extends UniverseTaskBase {
     MetricLabelsBuilder metricLabelsBuilder = MetricLabelsBuilder.create().appendSource(universe);
     BACKUP_ATTEMPT_COUNTER.labels(metricLabelsBuilder.getPrometheusValues()).inc();
     boolean isUniverseLocked = false;
-    boolean ybcBackup = universe.isYbcEnabled();
+    boolean ybcBackup =
+        universe.isYbcEnabled() && !params().backupType.equals(TableType.REDIS_TABLE_TYPE);
     try {
       checkUniverseVersion();
 

managed/src/main/java/com/yugabyte/yw/models/configs/validators/CustomerConfigStorageS3Validator.java
@@ -49,44 +49,42 @@ public class CustomerConfigStorageS3Validator extends CustomerConfigStorageValid
             "Aws credentials are null and IAM profile is not used.");
       }
     }
-    if (!StringUtils.isEmpty(s3data.awsAccessKeyId)) {
+    try {
+      // Disable cert checking while connecting with s3
+      // Enabling it can potentially fail when s3 compatible storages like
+      // Dell ECS are provided and custom certs are needed to connect
+      // Reference: https://yugabyte.atlassian.net/browse/PLAT-2497
+      System.setProperty(SDKGlobalConfiguration.DISABLE_CERT_CHECKING_SYSTEM_PROPERTY, "true");
+
+      AmazonS3 s3Client = null;
+      String exceptionMsg = null;
       try {
-        // Disable cert checking while connecting with s3
-        // Enabling it can potentially fail when s3 compatible storages like
-        // Dell ECS are provided and custom certs are needed to connect
-        // Reference: https://yugabyte.atlassian.net/browse/PLAT-2497
-        System.setProperty(SDKGlobalConfiguration.DISABLE_CERT_CHECKING_SYSTEM_PROPERTY, "true");
-
-        AmazonS3 s3Client = null;
-        String exceptionMsg = null;
-        try {
-          s3Client = factory.createS3Client(s3data);
-        } catch (AmazonS3Exception s3Exception) {
-          exceptionMsg = s3Exception.getErrorMessage();
-          throwBeanValidatorError(CustomerConfigConsts.BACKUP_LOCATION_FIELDNAME, exceptionMsg);
-        }
-
-        validateBucket(
-            s3Client, CustomerConfigConsts.BACKUP_LOCATION_FIELDNAME, s3data.backupLocation);
-        if (s3data.regionLocations != null) {
-          for (RegionLocations location : s3data.regionLocations) {
-            if (StringUtils.isEmpty(location.region)) {
-              throwBeanValidatorError(
-                  CustomerConfigConsts.REGION_FIELDNAME, "This field cannot be empty.");
-            }
-            validateUrl(
-                CustomerConfigConsts.AWS_HOST_BASE_FIELDNAME, location.awsHostBase, true, true);
-            validateUrl(
-                CustomerConfigConsts.REGION_LOCATION_FIELDNAME, location.location, true, false);
-            validateBucket(
-                s3Client, CustomerConfigConsts.REGION_LOCATION_FIELDNAME, location.location);
-          }
-        }
-
-      } finally {
-        // Re-enable cert checking as it applies globally
-        System.setProperty(SDKGlobalConfiguration.DISABLE_CERT_CHECKING_SYSTEM_PROPERTY, "false");
+        s3Client = factory.createS3Client(s3data);
+      } catch (AmazonS3Exception s3Exception) {
+        exceptionMsg = s3Exception.getErrorMessage();
+        throwBeanValidatorError(CustomerConfigConsts.BACKUP_LOCATION_FIELDNAME, exceptionMsg);
       }
+
+      validateBucket(
+          s3Client, CustomerConfigConsts.BACKUP_LOCATION_FIELDNAME, s3data.backupLocation);
+      if (s3data.regionLocations != null) {
+        for (RegionLocations location : s3data.regionLocations) {
+          if (StringUtils.isEmpty(location.region)) {
+            throwBeanValidatorError(
+                CustomerConfigConsts.REGION_FIELDNAME, "This field cannot be empty.");
+          }
+          validateUrl(
+              CustomerConfigConsts.AWS_HOST_BASE_FIELDNAME, location.awsHostBase, true, true);
+          validateUrl(
+              CustomerConfigConsts.REGION_LOCATION_FIELDNAME, location.location, true, false);
+          validateBucket(
+              s3Client, CustomerConfigConsts.REGION_LOCATION_FIELDNAME, location.location);
+        }
+      }
+
+    } finally {
+      // Re-enable cert checking as it applies globally
+      System.setProperty(SDKGlobalConfiguration.DISABLE_CERT_CHECKING_SYSTEM_PROPERTY, "false");
     }
   }
 

managed/src/test/java/com/yugabyte/yw/commissioner/BackupGarbageCollectorTest.java
@@ -272,41 +272,4 @@ public class BackupGarbageCollectorTest extends FakeDBApplication {
     backup.refresh();
     assertEquals(BackupState.FailedToDelete, backup.state);
   }
-
-  @Test
-  public void testDeleteIAMS3BackupSuccess() {
-    CustomerConfig customerConfig =
-        ModelFactory.createS3StorageConfigWithIAM(defaultCustomer, "TEST12");
-    BackupTableParams bp = new BackupTableParams();
-    bp.storageConfigUUID = customerConfig.configUUID;
-    bp.universeUUID = defaultUniverse.universeUUID;
-    Backup backup = Backup.create(defaultCustomer.uuid, bp);
-    backup.transitionState(BackupState.QueuedForDeletion);
-    ShellResponse shellResponse = new ShellResponse();
-    shellResponse.message = "{\"success\": true}";
-    shellResponse.code = 0;
-    when(mockTableManagerYb.deleteBackup(any())).thenReturn(shellResponse);
-    backupGC.scheduleRunner();
-    assertThrows(
-        PlatformServiceException.class,
-        () -> Backup.getOrBadRequest(defaultCustomer.uuid, backup.backupUUID));
-  }
-
-  @Test
-  public void testDeleteIAMS3BackupFailure() {
-    CustomerConfig customerConfig =
-        ModelFactory.createS3StorageConfigWithIAM(defaultCustomer, "TEST13");
-    BackupTableParams bp = new BackupTableParams();
-    bp.storageConfigUUID = customerConfig.configUUID;
-    bp.universeUUID = defaultUniverse.universeUUID;
-    Backup backup = Backup.create(defaultCustomer.uuid, bp);
-    backup.transitionState(BackupState.QueuedForDeletion);
-    ShellResponse shellResponse = new ShellResponse();
-    shellResponse.message = "{\"error\": true}";
-    shellResponse.code = 2;
-    when(mockTableManagerYb.deleteBackup(any())).thenReturn(shellResponse);
-    backupGC.scheduleRunner();
-    backup = Backup.getOrBadRequest(defaultCustomer.uuid, backup.backupUUID);
-    assertEquals(BackupState.FailedToDelete, backup.state);
-  }
 }

commit 16fd9748076a8dd0a639e65dc6422c3495e94c27

[PLAT-8773]Fix regression where pathStyleAccess is not respected by backend
Summary: Regression introduced in: https://github.com/yugabyte/yugabyte-db/commit/8efef848c5a7298a322fe3e2766811d78aa49520

Test Plan: No tests

Reviewers: kkg

Reviewed By: kkg

Subscribers: yugaware

Differential Revision: https://phabricator.dev.yugabyte.com/D25156

managed/src/main/java/com/yugabyte/yw/common/AWSUtil.java
@@ -190,6 +190,9 @@ public class AWSUtil implements CloudUtil {
       AWSCredentialsProvider creds = new AWSStaticCredentialsProvider(credentials);
       s3ClientBuilder.withCredentials(creds);
     }
+    if (s3Data.isPathStyleAccess) {
+      s3ClientBuilder.withPathStyleAccessEnabled(true);
+    }
     s3ClientBuilder.withForceGlobalBucketAccessEnabled(true);
     String endpoint = s3Data.awsHostBase;
     String region = getClientRegion(s3Data.fallbackRegion);