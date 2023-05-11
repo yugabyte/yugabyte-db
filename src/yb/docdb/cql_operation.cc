81080a5e0eae7438b8cbded7702ea71037dcf0ce
Saurav Tiwary <21277837+dr0pdb@users.noreply.github.com>
2023-05-11 19:40:52 +0530

    [BACKPORT 2.18][#17041] YCQL: Add cache in FindMemberForIndex for jsonb
    Summary:
    Original commit: eb5a9a43f5af1f3120292a02a6bbc2d2cb636e1d / D24999
    The `FindMemberForIndex` function is used to find the member in the jsonb document at a particular index/name.
    
    Before this diff, the `FindMember` function from rapidjson was being used to find a member. This function has `O(num_members)` time complexity.
    When `FindMemberForIndex` is executed in a loop inside `QLWriteOperation::ApplyForJsonOperators`, it leads to quadratic complexity.
    
    This diff adds a small cache for speedup of member lookups inside `FindMemberForIndex`.
    When asked to find a particular member, we now loop through all the members of the json node & store the member index in the cache.
    This can sometimes lead to unnecessary work if there are no future member lookups needed on the json node but since this is all in-memory &
    only done once per node, the cost shouldn't be high.
    
    The cache logic is protected by a tablet server GFlag `ycql_jsonb_use_member_cache` and set to `true` by default.
    
    Identification of a node is done via a `uintptr_t` which is casted from `rapidjson::Value*`. As a result, after any reallocation, the pointer might not be valid
    and can lead to cache miss and repopulation of all the members. The cast to `uintptr_t` helps avoid accidental dereferencing of the pointers.
    
    Performance improvements on a single query updating 10k attributes on my Mac M1:
    ```
    Before: 1790 ms
    After: 110 ms
    ```
    Python script for query: https://gist.github.com/dr0pdb/f7e6f476300a209a1ed023b952422ed7
    See `upsert_individual_prepared` query. Set `NUM_JSON_ATTRIBUTES = 10k` for reproducing if needed.
    Jira Issue(s): DB-6352
    
    Test Plan:
    New test case
    `./yb_build.sh --java-test 'org.yb.cql.TestJson#testUpsertWithNestedJsonWithMemberCache'`
    `./yb_build.sh --java-test 'org.yb.cql.TestJson#testUpsertWithNestedJsonWithoutMemberCache'`
    
    Existing test cases
    `./yb_build.sh --java-test 'org.yb.cql.TestJson'`
    `./yb_build.sh --java-test 'org.yb.cql.TestJsonIndex'`
    `./yb_build.sh --java-test 'org.yb.cql.TestUpdate'`
    
    Reviewers: amitanand, skumar, aagrawal, oleg
    
    Reviewed By: oleg
    
    Subscribers: skumar
    
    Differential Revision: https://phabricator.dev.yugabyte.com/D25250

--- src/yb/docdb/cql_operation.cc
+++ src/yb/docdb/cql_operation.cc
@@ -77,6 +77,9 @@ constexpr bool kYcqlPackedRowEnabled = true;
 DEFINE_RUNTIME_bool(ycql_enable_packed_row, kYcqlPackedRowEnabled,
                     "Whether packed row is enabled for YCQL.");
 
+DEFINE_RUNTIME_bool(ycql_jsonb_use_member_cache, true,
+                    "Whether we use member cache during jsonb processing in YCQL.");
+
 DEFINE_UNKNOWN_uint64(
     ycql_packed_row_size_limit, 0,
     "Packed row size limit for YCQL in bytes. 0 to make this equal to SSTable block size.");
@@ -211,13 +214,20 @@ bool JoinNonStaticRow(
   return join_successful;
 }
 
+typedef rapidjson::SizeType RapidJsonMemberIndex;
+typedef std::unordered_map<uintptr_t, std::unordered_map<std::string, RapidJsonMemberIndex>>
+    RapidJsonMemberCache;
+
+uintptr_t MemberCacheValueHash(rapidjson::Value* p) { return reinterpret_cast<uintptr_t>(p); }
+
 Status FindMemberForIndex(const QLColumnValuePB& column_value,
                           int index,
                           rapidjson::Value* document,
                           rapidjson::Value::MemberIterator* memberit,
                           rapidjson::Value::ValueIterator* valueit,
                           bool* last_elem_object,
-                          IsInsert is_insert) {
+                          IsInsert is_insert,
+                          RapidJsonMemberCache* member_cache) {
   *last_elem_object = false;
 
   int64_t array_index;
@@ -246,8 +256,38 @@ Status FindMemberForIndex(const QLColumnValuePB& column_value,
 
     *last_elem_object = true;
 
-    const auto& member = column_value.json_args(index).operand().value().string_value().c_str();
-    *memberit = document->FindMember(member);
+    const auto& member = column_value.json_args(index).operand().value().string_value();
+    *memberit = document->MemberEnd();
+
+    if (FLAGS_ycql_jsonb_use_member_cache) {
+      auto node_itr = member_cache->find(MemberCacheValueHash(document));
+      if (node_itr != member_cache->end()) {
+        // Find the member from the cache.
+        auto member_itr = node_itr->second.find(member);
+        if (member_itr != node_itr->second.end()) {
+          LOG_IF(DFATAL, member_itr->second >= document->MemberCount())
+              << "Invalid index in the member cache";
+          *memberit = document->MemberBegin() + member_itr->second;
+        }
+      } else {
+        // Fetch and cache all the members of the document.
+        std::unordered_map<std::string, RapidJsonMemberIndex> members;
+        RapidJsonMemberIndex member_idx = 0;
+        for (auto m = document->MemberBegin(); m != document->MemberEnd(); m++, member_idx++) {
+          auto curr_member_name = m->name.GetString();
+          if (curr_member_name == member) {
+            LOG_IF(DFATAL, !(*memberit == document->MemberEnd()))
+                << "Duplicate member found in the json object";
+            *memberit = m;
+          }
+          members[curr_member_name] = member_idx;
+        }
+        (*member_cache)[MemberCacheValueHash(document)] = std::move(members);
+      }
+    } else {
+      *memberit = document->FindMember(member.c_str());
+    }
+
     if (memberit->operator==(document->MemberEnd())) {
       return STATUS_SUBSTITUTE(QLError, "Could not find member: ", member);
     }
@@ -678,6 +718,7 @@ Status QLWriteOperation::ApplyForJsonOperators(
   using common::Jsonb;
   rapidjson::Document document;
   QLValue qlv;
+  RapidJsonMemberCache member_cache;
   bool read_needed = true;
   for (const auto& column_value_ptr : col_map.find(col_id)->second) {
     const auto& column_value = *column_value_ptr;
@@ -717,11 +758,11 @@ Status QLWriteOperation::ApplyForJsonOperators(
 
     int i = 0;
     auto status = FindMemberForIndex(column_value, i, node, &memberit, &valueit,
-        &last_elem_object, is_insert);
+        &last_elem_object, is_insert, &member_cache);
     for (i = 1; i < column_value.json_args_size() && status.ok(); i++) {
       node = (last_elem_object) ? &(memberit->value) : &(*valueit);
       status = FindMemberForIndex(column_value, i, node, &memberit, &valueit,
-          &last_elem_object, is_insert);
+          &last_elem_object, is_insert, &member_cache);
     }
 
     bool update_missing = false;
@@ -737,7 +778,11 @@ Status QLWriteOperation::ApplyForJsonOperators(
         auto val = column_value.json_args(i - 1).operand().value().string_value();
         rapidjson::Value v(
             val.c_str(), narrow_cast<rapidjson::SizeType>(val.size()), document.GetAllocator());
+        // Add the member and update the cache if enabled.
         node->AddMember(v, rhs_doc, document.GetAllocator());
+        if (FLAGS_ycql_jsonb_use_member_cache) {
+          member_cache[MemberCacheValueHash(node)][val] = node->MemberCount() - 1;
+        }
       } else {
         RETURN_NOT_OK(status);
       }
