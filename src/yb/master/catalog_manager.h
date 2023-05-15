0b7eeb1ce33b7ef8f075ceef1449eb4ec6dbd94f
Hari Krishna Sunder <hari90@users.noreply.github.com>
2023-05-15 15:46:40 -0700

    [#16469] xCluster: Create AddTableToXClusterTask to handle async operations
    Summary:
    Currently the logic to add a table\index to transactional xCluster is kicked off by the Client driven IsCreateTablDone RPC call. This is error prone, and the task has several infinite waits, which causes it to block master shutdown.
    With #16397 we now have a way to create AddTableToXClusterTask, a RunnableMonitored task that can handle all operations with async callbacks and dynamic retries based off of the reactor pool.
    
    A YB Table starts out in PREPARING state and transitions to RUNNING only after all its tablets have transitioned to RUNNING state. With this change, we now intercept this transition, and schedule  AddTableToXClusterTask. The task will Bootstrap and add the table to xCluster in a transactionally consistent manner described in #13488. The difference now is that we use Async RPC calls, and use delayed reactor thread scheduling instead of sleeping and blocking the thread.
    If the master fails over in the middle of the task we will now Abort the task and get rescheduled on the new leader.
    If the table is dropped while we are in the middle of the task we will get Aborted, and cleaned up before the table drop proceeds.
    
    As part of this change common code from derived the classes of server::MonitoredTask have been moved into the base itself. This includes members start_timestamp_, completion_timestamp_ and state_ and their associated functions.
    
    Test Plan: XClusterYsqlIndexTest
    
    Reviewers: slingam, jhe, rahuldesirazu, mlillibridge
    
    Reviewed By: rahuldesirazu
    
    Subscribers: bogdan, ybase, xCluster
    
    Differential Revision: https://phorge.dev.yugabyte.com/D25048

--- src/yb/master/catalog_manager.h
+++ src/yb/master/catalog_manager.h
@@ -673,11 +673,11 @@ class CatalogManager : public tserver::TabletPeerLookupIf,
   bool IsSystemTable(const TableInfo& table) const override;
 
   // Is the table a user created table?
-  bool IsUserTable(const TableInfo& table) const override;
+  bool IsUserTable(const TableInfo& table) const override EXCLUDES(mutex_);
   bool IsUserTableUnlocked(const TableInfo& table) const REQUIRES_SHARED(mutex_);
 
   // Is the table a user created index?
-  bool IsUserIndex(const TableInfo& table) const override;
+  bool IsUserIndex(const TableInfo& table) const override EXCLUDES(mutex_);
   bool IsUserIndexUnlocked(const TableInfo& table) const REQUIRES_SHARED(mutex_);
 
   // Is the table a special sequences system table?
@@ -688,7 +688,7 @@ class CatalogManager : public tserver::TabletPeerLookupIf,
 
   // Is the table created by user?
   // Note that table can be regular table or index in this case.
-  bool IsUserCreatedTable(const TableInfo& table) const override;
+  bool IsUserCreatedTable(const TableInfo& table) const override EXCLUDES(mutex_);
   bool IsUserCreatedTableUnlocked(const TableInfo& table) const REQUIRES_SHARED(mutex_);
 
   // Let the catalog manager know that we have received a response for a prepare delete
@@ -979,7 +979,7 @@ class CatalogManager : public tserver::TabletPeerLookupIf,
       const PeerId& peer_id, int32_t num_retries, StdStatusCallback callback);
 
   // Schedule a task to run on the async task thread pool.
-  Status ScheduleTask(std::shared_ptr<RetryingTSRpcTask> task) override;
+  Status ScheduleTask(std::shared_ptr<server::RunnableMonitoredTask> task) override;
 
   // Time since this peer became master leader. Caller should verify that it is leader before.
   MonoDelta TimeSinceElectedLeader();
@@ -1360,6 +1360,9 @@ class CatalogManager : public tserver::TabletPeerLookupIf,
 
   HybridTime AllowedHistoryCutoffProvider(tablet::RaftGroupMetadata* metadata);
 
+  // Promote the table from a PREPARING state to a RUNNING state, and persist in sys_catalog.
+  Status PromoteTableToRunningState(TableInfoPtr table_info) override;
+
  protected:
   // TODO Get rid of these friend classes and introduce formal interface.
   friend class TableLoader;
@@ -1379,6 +1382,7 @@ class CatalogManager : public tserver::TabletPeerLookupIf,
   friend class XClusterConfigLoader;
   friend class YsqlBackendsManager;
   friend class BackendsCatalogVersionJob;
+  friend class AddTableToXClusterTask;
 
   FRIEND_TEST(yb::MasterPartitionedTest, VerifyOldLeaderStepsDown);
 
@@ -2622,7 +2626,7 @@ class CatalogManager : public tserver::TabletPeerLookupIf,
 
   // Gets the set of CDC stream info for an xCluster consumer table.
   XClusterConsumerTableStreamInfoMap GetXClusterStreamInfoForConsumerTable(
-      const TableId& table_id) const;
+      const TableId& table_id) const EXCLUDES(mutex_);
 
   XClusterConsumerTableStreamInfoMap GetXClusterStreamInfoForConsumerTableUnlocked(
       const TableId& table_id) const REQUIRES_SHARED(mutex_);
@@ -2742,17 +2746,20 @@ class CatalogManager : public tserver::TabletPeerLookupIf,
 
   Status DeleteCDCStreamsForTables(const std::vector<TableId>& table_ids) EXCLUDES(mutex_);
 
-  // Returns true if xCluster table bootstrap async task is running. Schedules the async task if
-  // needed.
-  Result<bool> ScheduleBootstrapForXclusterIfNeeded(
-      const TableInfoPtr& table, const SysTablesEntryPB& pb);
+  bool ShouldAddTableToXClusterReplication(const TableInfo& index_info, const SysTablesEntryPB& pb)
+      EXCLUDES(mutex_);
 
-  Result<bool> ShouldAddTableToXClusterReplication(
-      const TableInfo& index_info, const SysTablesEntryPB& pb);
+  // Schedule AddTableToXClusterTask if needed. Returns true if task is needed and false otherwise.
+  bool ScheduleAddTableToXClusterTaskIfNeeded(TableInfoPtr table_info, const SysTablesEntryPB& pb)
+      EXCLUDES(mutex_);
 
-  Status AddYsqlIndexToXClusterReplication(const TableInfo& index_info);
+  void ScheduleAddTableToXClusterTaskForAllTables() EXCLUDES(mutex_);
 
-  Result<HybridTime> BootstrapAndAddIndexToXClusterReplication(const TableInfo& index_info);
+  Result<std::string> GetIndexesTableReplicationGroup(const TableInfo& index_info);
+
+  Status BootstrapTable(
+      const std::string& replication_group, const TableInfo& index_info,
+      client::BootstrapProducerCallback callback);
 
   // Wait for all tables under xCluster replication to catch up to the current xCluster safe time.
   // When new tables\indexes are added to an existing replication group they will start at a
@@ -2768,6 +2775,24 @@ class CatalogManager : public tserver::TabletPeerLookupIf,
       ClusterConfigInfo* cluster_config, ClusterConfigInfo::WriteLock* l);
 
   Status RemoveTableFromXcluster(const std::vector<TabletId>& table_ids);
+
+  // Wait for all tables under xCluster replication to catch up to the current xCluster safe time.
+  // When new tables\indexes are added to an existing replication group they will start at a
+  // replication time less than the last computed xCluster safe time. Since the safe time cannot
+  // move backwards it wait until the new tables\indexes have moved past this time.
+  Status WaitForAllXClusterConsumerTablesToCatchUpToSafeTime(
+      const NamespaceId& namespace_id, const HybridTime& min_safe_time, MonoDelta timeout);
+
+  // For a table that is currently in PREPARING state, if all its tablets have transitioned to
+  // RUNNING state, either advance the table from PREPARING to RUNNING state, or start any async
+  // tasks if needed (ex: AddTableToXClusterTask).
+  // new_running_tablets is the new set of tablets that are being transitioned to RUNNING state
+  // (dirty copy is modified) and yet to be persisted. Returns true if the table state has changed.
+  // Note:
+  //    WriteLock on the table is required.
+  //    Caller is responsible for persisting the table to sys_catalog if its state has changed.
+  Result<bool> HandleNewRunningTabletsForTable(
+      TableInfoPtr table_info, const std::set<TabletId>& new_running_tablets);
 
   // Should be bumped up when tablet locations are changed.
   std::atomic<uintptr_t> tablet_locations_version_{0};
