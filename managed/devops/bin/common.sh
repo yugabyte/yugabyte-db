98e525fa7bb9c7ac1e1500bce716d49f15cc225e
Muthu Chidambaram <mchidambaram@yugabyte.com>
2023-05-17 03:36:50 -0500

    [BACKPORT 2.17.4.1035][PLAT-8262] Adding azure host caching for data disks
    Summary:
    Default setting for disk caching is None for data disks. Using ReadOnly for data disks can increase
    performance for universes. This diff defaults all data disks on newly created universes to ReadOnly
    caching.
    
    Also upgraded Azure APIs because caching option was not present in older API.
    
    Original diff: 20ef32f18114186ee14788df28720a010524aecc / D25215
    
    Test Plan: Create azure universe and ensure data disk caching is set properly. Verify delete also works.
    
    Reviewers: yshchetinin, jharveysmith, nsingh, amalyshev
    
    Reviewed By: amalyshev
    
    Subscribers: jenkins-bot, yugaware
    
    Differential Revision: https://phorge.dev.yugabyte.com/D25633

--- managed/devops/bin/common.sh
+++ managed/devops/bin/common.sh
@@ -84,7 +84,7 @@ set_python_executable() {
 # Constants
 # -------------------------------------------------------------------------------------------------
 readonly PYTHON2_EXECUTABLES=('python2' 'python2.7')
-readonly PYTHON3_EXECUTABLES=('python3.6' 'python3' 'python3.7' 'python3.8')
+readonly PYTHON3_EXECUTABLES=('python3.8' 'python3' 'python3.7' 'python3.6')
 readonly PYTHON2_VERSIONS=('python2.7')
 readonly PYTHON3_VERSIONS=('python3.6' 'python3.7' 'python3.8' 'python3.9' 'python3.10' \
                            'python3.11')
@@ -346,12 +346,15 @@ activate_virtualenv() {
 }
 
 create_pymodules_package() {
+  activate_virtualenv
   rm -rf "$YB_PYTHON_MODULES_DIR"
   mkdir -p "$YB_PYTHON_MODULES_DIR"
+  chmod 777 "$YB_PYTHON_MODULES_DIR"
   extra_install_flags=""
   if [[ $YB_MANAGED_DEVOPS_USE_PYTHON3 == "0" ]]; then
     extra_install_flags="setuptools<45"
   fi
+  run_pip install --upgrade pip > /dev/null
   # Download the scripts necessary (i.e. ansible). Remove the modules afterwards to avoid
   # system-specific libraries.
   log "Downloading package scripts"
