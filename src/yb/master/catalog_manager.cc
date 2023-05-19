8e28b8bf793b7126f77efa5a3cdae76f0c664460
Oleg Loginov <loginov@yugabyte.com>
2023-05-19 14:58:51 +0000

    [BACKPORT 2.18][#17107] YCQL: Prevent DROP TYPE for the used YCQL type
    Summary:
    Original commit: aca0f6d1b30976f4b9015e46fdeee76cff602bd7 / D25229
    GH link: https://github.com/yugabyte/yugabyte-db/commit/aca0f6d1b30976f4b9015e46fdeee76cff602bd7
    
    The current functionality allows dropping a User-Defined Type (UDT) even if it is still being referenced.
    Only if the type is used directly as a column type - it cannot be deleted.
    A reference from a collection or frozen is ignored now.
    This can lead to unexpected behavior and errors in applications that rely on the UDT.
    
    `CatalogManager::DeleteUDType` is updated to prevent dropping used UDT which is still being used.
    
    Test Plan: ybd --java-test org.yb.cql.TestUserDefinedTypes#testNestedUDTsWithCollections
    
    Reviewers: mihnea, stiwary, yguan
    
    Reviewed By: stiwary
    
    Subscribers: bogdan, yql
    
    Differential Revision: https://phorge.dev.yugabyte.com/D25300

--- src/yb/master/catalog_manager.cc
+++ src/yb/master/catalog_manager.cc
@@ -84,6 +84,7 @@
 #include "yb/common/partial_row.h"
 #include "yb/common/partition.h"
 #include "yb/common/ql_type.h"
+#include "yb/common/ql_type_util.h"
 #include "yb/common/ql_wire_protocol.h"
 #include "yb/common/roles_permissions.h"
 #include "yb/common/schema.h"
@@ -9638,11 +9639,19 @@ Status CatalogManager::DeleteUDType(const DeleteUDTypeRequestPB* req,
       auto ltm = table->LockForRead();
       if (!ltm->started_deleting()) {
         for (const auto &col : ltm->schema().columns()) {
-          if (col.type().main() == DataType::USER_DEFINED_TYPE &&
-              col.type().udtype_info().id() == tp->id()) {
+          const Status tp_is_not_used = IterateAndDoForUDT(
+              col.type(),
+              [&tp](const QLTypePB::UDTypeInfo& udtype_info) -> Status {
+                return udtype_info.id() == tp->id()
+                    ? STATUS(QLError, Substitute("Used type $0 id $1", tp->name(), tp->id()))
+                    : Status::OK();
+              });
+
+          if (!tp_is_not_used.ok()) {
             Status s = STATUS(QLError,
                 Substitute("Cannot delete type '$0.$1'. It is used in column $2 of table $3",
                     ns->name(), tp->name(), col.name(), ltm->name()));
+            LOG_WITH_FUNC(WARNING) << s << ": " << tp_is_not_used;
             return SetupError(resp->mutable_error(), MasterErrorPB::INVALID_REQUEST, s);
           }
         }
