927c449f2e7b79d55a28362c9d4c00127ac4f74a
Rahul Desirazu <rahuldesirazu@users.noreply.github.com>
2023-05-23 19:00:06 -0700

    [#17463] XCluster: Producer and consumer side network throttling
    Summary:
    After a network partition heal, it is possible that the xcluster path full blast fetches and applies all unreplicated changes. As a result, disk on the consumer cannot keep up with the write throughput. To keep the rate of xcluster write traffic throttled, this diff has changes to add per-server throttling to the producer GetChanges path and consumer ApplyChanges path. The two flags are `get_changes_max_send_rate_mbps` and `apply_changes_max_send_rate_mbps`, both initially set to 100 mpbs. This is to reflect typical disk throughput of 125 mbps * 80% for xcluster traffic.
    Jira: DB-6628
    
    Test Plan:
    Jenkins
    Manual testing of amex workload with a 7 hour pause + resume.
    
    Reviewers: xCluster, jhe, mlillibridge
    
    Reviewed By: mlillibridge
    
    Subscribers: bogdan
    
    Differential Revision: https://phorge.dev.yugabyte.com/D25653

--- src/yb/tserver/xcluster_poller.h
+++ src/yb/tserver/xcluster_poller.h
@@ -58,7 +58,8 @@ class XClusterPoller : public std::enable_shared_from_this<XClusterPoller> {
       bool use_local_tserver,
       const std::vector<TabletId>& global_transaction_status_tablets,
       bool enable_replicate_transaction_status_table,
-      SchemaVersion last_compatible_consumer_schema_version);
+      SchemaVersion last_compatible_consumer_schema_version,
+      rocksdb::RateLimiter* rate_limiter);
   ~XClusterPoller();
 
   void Shutdown();
