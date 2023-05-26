b64cc0682bce7ed62206cb78c9e5edbe0d448aa1
Jeff Harvey-Smith <jharveysmith@yugabyte.com>
2023-05-26 15:43:04 -0700

    [BACKPORT 2.19.0][#17502] Fix the shared library interpreter used for yugabyted-ui
    Summary:
    Original commit: 75a83fe75b5905f1e89ccd96311ef63a102e453e / D25752
    Recently, around the time we upgraded Go on our build hosts from 1.18 to 1.20, the yugabyted-ui executable stopped working in x86_64 release builds. The yugabyted-ui executable ended up having the wrong dynamic linker (ld.so from Linuxbrew). This is most likely due to changes in the mechanism of linker selection by the Go compiler driver. https://tip.golang.org/doc/go1.20 says "On Linux, the linker now selects the dynamic interpreter for glibc or musl at link time." Apparently, before the change Go would use the system linker (/bin/ld or /usr/bin/ld) and now it uses the ld binary available on the PATH, which happens to be the Linuxbrew version of ld in the Linuxbrew build. The Linuxbrew ld binary makes its output executables use the Linuxbrew dynamic linker.
    
    To counteract this, we are removing Linuxbrew bin directory from PATH when building yugabyted-ui. Also adding a check in yugabyted-ui/build.sh script that a valid system-wide dynamic linker is used, and if that is not the case for whatever reason, attempting to fix it using the patchelf tool.
    
    Test Plan: Jenkins: compile only
    
    Reviewers: mbautin
    
    Reviewed By: mbautin
    
    Subscribers: ybase
    
    Differential Revision: https://phorge.dev.yugabyte.com/D25769

--- build-support/common-build-env.sh
+++ build-support/common-build-env.sh
@@ -1564,6 +1564,13 @@ add_brew_bin_to_path() {
   fi
 }
 
+remove_linuxbrew_bin_from_path() {
+  if using_linuxbrew; then
+    ensure_linuxbrew_dir_is_set
+    remove_path_entry "$YB_LINUXBREW_DIR/bin"
+  fi
+}
+
 detect_num_cpus() {
   if [[ ! ${YB_NUM_CPUS:-} =~ ^[0-9]+$ ]]; then
     if is_linux; then
@@ -2235,6 +2242,7 @@ run_shellcheck() {
     build-support/jenkins/yb-jenkins-build.sh
     build-support/jenkins/yb-jenkins-test.sh
     build-support/run-test.sh
+    yugabyted-ui/build.sh
     yb_build.sh
   )
   pushd "$YB_SRC_ROOT"
