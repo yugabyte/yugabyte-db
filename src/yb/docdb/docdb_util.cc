130aaaff74d0a07894e7fec6b49f1234cddd6974
Karan <kpopali@yugabyte.com>
2023-05-30 17:45:18 -0700

    [#16826] DocDB: xCluster: Use RocksDB DirectWriter for applying external intents
    Summary:
    This change uses RocksDB DirectWriter for applying the external batch, which helps avoid creating additional copy of data being copied from IntentsDB to RegularDB. It will help reduce the latency of apply operation as well as the transient memory usage for xcluster operations.
    Jira: DB-6176
    
    Test Plan: Jenkins
    
    Reviewers: xCluster, slingam, sergei, mlillibridge
    
    Reviewed By: slingam
    
    Subscribers: mlillibridge, rthallam, ybase
    
    Differential Revision: https://phorge.dev.yugabyte.com/D25843

--- src/yb/docdb/docdb_util.cc
+++ src/yb/docdb/docdb_util.cc
@@ -14,12 +14,14 @@
 #include "yb/docdb/docdb_util.h"
 
 #include "yb/docdb/consensus_frontier.h"
-#include "yb/dockv/doc_key.h"
 #include "yb/docdb/docdb.h"
 #include "yb/docdb/docdb.messages.h"
 #include "yb/docdb/docdb_debug.h"
 #include "yb/docdb/docdb_rocksdb_util.h"
 #include "yb/docdb/rocksdb_writer.h"
+
+#include "yb/dockv/doc_key.h"
+#include "yb/dockv/doc_kv_util.h"
 
 #include "yb/rocksutil/write_batch_formatter.h"
 #include "yb/rocksutil/yb_rocksdb.h"
@@ -349,14 +351,17 @@ Status DocDBRocksDBUtil::AddExternalIntents(
     }
 
     void Apply(rocksdb::WriteBatch* batch) {
-      ThreadSafeArena arena;
-      LWKeyValuePairPB kv_pair(&arena);
-      kv_pair.dup_key(key_.AsSlice());
-      kv_pair.dup_value(value_.AsSlice());
-      ExternalTxnApplyState external_txn_apply_state;
-      AddExternalPairToWriteBatch(
-          kv_pair, hybrid_time_, &external_txn_apply_state,
-          /* regular_write_batch= */ nullptr, batch, nullptr);
+      DocHybridTimeBuffer doc_ht_buffer;
+      dockv::DocHybridTimeWordBuffer inverted_doc_ht_buffer;
+      auto key_value = value_.AsSlice();
+
+      std::array<Slice, 2> key_parts = {{
+          key_.AsSlice(),
+          doc_ht_buffer.EncodeWithValueType(hybrid_time_, /*write_id=*/0),
+      }};
+      key_parts[1] = dockv::InvertEncodedDocHT(key_parts[1], &inverted_doc_ht_buffer);
+      constexpr size_t kNumValueParts = 1;
+      batch->Put(key_parts, {&key_value, kNumValueParts});
     }
 
     boost::optional<std::pair<Slice, Slice>> Next() override {
