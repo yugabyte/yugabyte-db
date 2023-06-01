9ce7e5e7314eba9477b5e79fc7b38c4483b5af34
Naorem Khogendro Singh <nsingh@yugabyte.com>
2023-06-01 09:58:57 -0700

    [BACKPORT 2.18][PLAT-9140] Support more filters for UI and include universe and provider names with UUIDs in the node agent page API
    Summary:
    Original diff - https://phorge.dev.yugabyte.com/D25896 (b97af35c7640b8cab114eb55660971651b6aa16b)
    
    This adds provider and universe details to the list and get responses. As node-agent is not tied to a universe or provider and node-agent can move to other provider, it is done so.
    
    Test Plan:
    Manually tested.
    
    ```
    {
            "uuid": "9fa8a516-f20e-4af4-befe-35ed172e0d67",
            "name": "yb-admin-nsingh-test-universe1-n1",
            "ip": "10.9.134.173",
            "port": 10070,
            "customerUuid": "f33e3c9b-75ab-4c30-80ad-cba85646ea39",
            "version": "2.19.1.0-PRE_RELEASE",
            "state": "READY",
            "updatedAt": "2023-06-01T16:52:46Z",
            "config": {
                "certPath": "/opt/yugaware/node-agent/certs/f33e3c9b-75ab-4c30-80ad-cba85646ea39/9fa8a516-f20e-4af4-befe-35ed172e0d67/0",
                "offloadable": false
            },
            "osType": "LINUX",
            "archType": "AMD64",
            "home": "/root/node-agent",
            "versionMatched": true,
            "reachable": true,
            "providerName": "aws2",
            "universeName": "nsingh-test-universe1",
            "providerUuid": "78d7a767-f218-4131-be8f-5c93dafc6b82",
            "universeUuid": "d8e52342-6e2d-414b-826a-5441308d7438"
        },
        {
            "uuid": "59be1a38-bd49-4bca-a719-69a61e6577c7",
            "name": "onprem-test-1",
            "ip": "10.9.95.52",
            "port": 10070,
            "customerUuid": "f33e3c9b-75ab-4c30-80ad-cba85646ea39",
            "version": "2.19.1.0-PRE_RELEASE",
            "state": "READY",
            "updatedAt": "2023-06-01T16:52:46Z",
            "config": {
                "certPath": "/opt/yugaware/node-agent/certs/f33e3c9b-75ab-4c30-80ad-cba85646ea39/59be1a38-bd49-4bca-a719-69a61e6577c7/0",
                "offloadable": true
            },
            "osType": "LINUX",
            "archType": "AMD64",
            "home": "/home/yugabyte/node-agent",
            "versionMatched": true,
            "reachable": true,
            "providerName": "onprem-provision-test1",
            "providerUuid": "b9137cc1-153a-42b8-9a31-8ca9148241e6"
        }
    ```
    
    Reviewers: amalyshev, rmadhavan
    
    Reviewed By: amalyshev
    
    Subscribers: yugaware
    
    Differential Revision: https://phorge.dev.yugabyte.com/D26297

--- managed/src/main/java/com/yugabyte/yw/forms/filters/NodeAgentApiFilter.java
+++ managed/src/main/java/com/yugabyte/yw/forms/filters/NodeAgentApiFilter.java
@@ -2,20 +2,41 @@
 
 package com.yugabyte.yw.forms.filters;
 
+import com.yugabyte.yw.commissioner.Common.CloudType;
 import com.yugabyte.yw.models.filters.NodeAgentFilter;
-import java.util.List;
+import java.util.Set;
+import java.util.UUID;
 import lombok.Data;
 import org.apache.commons.collections.CollectionUtils;
 
 @Data
 public class NodeAgentApiFilter {
-  private List<String> nodeIps;
+  private Set<String> nodeIps;
+  private CloudType cloudType;
+  private UUID providerUuid;
+  private UUID universeUuid;
+  private UUID regionUuid;
+  private UUID zoneUuid;
 
-  // Convert to the filter for model.
   public NodeAgentFilter toFilter() {
     NodeAgentFilter.NodeAgentFilterBuilder builder = NodeAgentFilter.builder();
-    if (!CollectionUtils.isEmpty(nodeIps)) {
+    if (CollectionUtils.isNotEmpty(nodeIps)) {
       builder.nodeIps(nodeIps);
+    }
+    if (cloudType != null) {
+      builder.cloudType(cloudType);
+    }
+    if (providerUuid != null) {
+      builder.providerUuid(providerUuid);
+    }
+    if (universeUuid != null) {
+      builder.universeUuid(universeUuid);
+    }
+    if (regionUuid != null) {
+      builder.regionUuid(regionUuid);
+    }
+    if (zoneUuid != null) {
+      builder.zoneUuid(zoneUuid);
     }
     return builder.build();
   }
