602f4e67729c590f7795d66dad236bf1526a8241
Sahith Kurapati <skurapati@yugabyte.com>
2023-06-13 06:46:26 +0000

    [PLAT-9057] RBAC Roles Datamodel
    Summary: RBAC Roles Datamodel. Added a Role class and DB table with all the utility helper functions and unit tests.
    
    Test Plan:
    Added UTs.
    Run itests.
    
    Reviewers: vpatibandla, sneelakantan
    
    Reviewed By: vpatibandla
    
    Subscribers: vpatibandla, sanketh, yugaware
    
    Differential Revision: https://phorge.dev.yugabyte.com/D26140

--- managed/src/main/java/Module.java
+++ managed/src/main/java/Module.java
@@ -64,6 +64,7 @@ import com.yugabyte.yw.common.kms.util.GcpEARServiceUtil;
 import com.yugabyte.yw.common.metrics.PlatformMetricsProcessor;
 import com.yugabyte.yw.common.metrics.SwamperTargetsFileUpdater;
 import com.yugabyte.yw.common.rbac.PermissionUtil;
+import com.yugabyte.yw.common.rbac.RoleUtil;
 import com.yugabyte.yw.common.services.LocalYBClientService;
 import com.yugabyte.yw.common.services.YBClientService;
 import com.yugabyte.yw.common.services.config.YbClientConfigFactory;
@@ -196,6 +197,7 @@ public class Module extends AbstractModule {
       bind(YbcUpgrade.class).asEagerSingleton();
       bind(PerfAdvisorScheduler.class).asEagerSingleton();
       bind(PermissionUtil.class).asEagerSingleton();
+      bind(RoleUtil.class).asEagerSingleton();
       requestStaticInjection(CertificateInfo.class);
       requestStaticInjection(HealthCheck.class);
       requestStaticInjection(AppConfigHelper.class);
