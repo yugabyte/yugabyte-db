9eb207bed3a6d13bc54cc5387dcf4a97ee1cd19f
Aleksandr Malyshev <amalyshev@yugabyte.com>
2023-06-15 12:39:49 +0300

    [PLAT-9288] Fix alert channel API docs
    Summary:
    Currently we don't have explicit channelType field in alert channel params, which make API description incorrect and also make it hard to get type having
    AlertChannel object.
    MOdified it to have explicit field. DB already contains this field so no upgrade scripts are needed.
    
    Test Plan:
    Unit tested.
    Open and modify existing alert channels.
    
    Reviewers: vbansal
    
    Reviewed By: vbansal
    
    Subscribers: yugaware
    
    Differential Revision: https://phorge.dev.yugabyte.com/D26217

--- managed/src/main/java/com/yugabyte/yw/models/AlertChannel.java
+++ managed/src/main/java/com/yugabyte/yw/models/AlertChannel.java
@@ -7,14 +7,7 @@ import static io.swagger.annotations.ApiModelProperty.AccessMode.READ_WRITE;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
-import com.fasterxml.jackson.annotation.JsonSubTypes;
-import com.fasterxml.jackson.annotation.JsonTypeInfo;
-import com.fasterxml.jackson.annotation.JsonTypeInfo.As;
-import com.yugabyte.yw.common.alerts.AlertChannelEmailParams;
-import com.yugabyte.yw.common.alerts.AlertChannelPagerDutyParams;
 import com.yugabyte.yw.common.alerts.AlertChannelParams;
-import com.yugabyte.yw.common.alerts.AlertChannelSlackParams;
-import com.yugabyte.yw.common.alerts.AlertChannelWebHookParams;
 import io.ebean.ExpressionList;
 import io.ebean.Finder;
 import io.ebean.Model;
@@ -27,7 +20,6 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
 import java.util.UUID;
-import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.Id;
@@ -68,33 +60,22 @@ public class AlertChannel extends Model {
   }
 
   @Id
-  @Column(nullable = false, unique = true)
   @ApiModelProperty(value = "Channel UUID", accessMode = READ_ONLY)
   private UUID uuid;
 
   @NotNull
   @Size(min = 1, max = 63)
-  @Column(columnDefinition = "Text", nullable = false)
   @ApiModelProperty(value = "Name", accessMode = READ_WRITE)
   private String name;
 
   @NotNull
-  @Column(nullable = false)
   @JsonProperty("customer_uuid")
   @ApiModelProperty(value = "Customer UUID", accessMode = READ_ONLY)
   private UUID customerUUID;
 
   @NotNull
   @Valid
-  @Column(columnDefinition = "TEXT", nullable = false)
   @DbJson
-  @JsonTypeInfo(use = JsonTypeInfo.Id.NAME, include = As.PROPERTY, property = "channelType")
-  @JsonSubTypes({
-    @JsonSubTypes.Type(value = AlertChannelEmailParams.class, name = "Email"),
-    @JsonSubTypes.Type(value = AlertChannelSlackParams.class, name = "Slack"),
-    @JsonSubTypes.Type(value = AlertChannelPagerDutyParams.class, name = "PagerDuty"),
-    @JsonSubTypes.Type(value = AlertChannelWebHookParams.class, name = "WebHook")
-  })
   @ApiModelProperty(value = "Channel params", accessMode = READ_WRITE)
   @Encrypted
   private AlertChannelParams params;
