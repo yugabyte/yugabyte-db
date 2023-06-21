ba04adf9e1043df663c5c1a883d60acfb0a454a0
yusong-yan <yusongyan3@gmail.com>
2023-06-21 19:48:35 +0000

    [#17868] docdb : Export histogram quantile p50/95/99 as gauge to Prometheus #TYPE
    Summary:
    Histogram quantile p50/95/99 should be exported as gauge, because as we reset the histogram, it gets computed based on ad-hoc latency values, so it can go up/down over time.
    Jira: DB-6952
    
    Test Plan: Check Prometheus endpoint
    
    Reviewers: rthallam, jhe, amitanand
    
    Reviewed By: rthallam, amitanand
    
    Subscribers: bogdan, ybase
    
    Differential Revision: https://phorge.dev.yugabyte.com/D26343

--- src/yb/util/metrics-test.cc
+++ src/yb/util/metrics-test.cc
@@ -620,9 +620,7 @@ TEST_F(MetricsTest, VerifyHelpAndTypeTags) {
       "# HELP t_hist_sum Test histogram description\n# TYPE t_hist_sum counter"));
   EXPECT_EQ(1, StringOccurence(output_str,
       "# HELP t_hist_count Test histogram description\n# TYPE t_hist_count counter"));
-  EXPECT_EQ(3, StringOccurence(output_str,
-      "# HELP t_hist Test histogram description\n# TYPE t_hist counter"));
-  EXPECT_EQ(2, StringOccurence(output_str,
+  EXPECT_EQ(5, StringOccurence(output_str,
       "# HELP t_hist Test histogram description\n# TYPE t_hist gauge"));
   // Check gauge output.
   EXPECT_EQ(1, StringOccurence(output_str,
