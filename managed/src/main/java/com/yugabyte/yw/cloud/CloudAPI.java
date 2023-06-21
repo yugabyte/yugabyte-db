d717dba5b383efdbc2a68628d27bbbc595fa730a
Prarabdh Garg <pgarg@yugabyte.com>
2023-06-21 11:56:23 -0700

    [PLAT-5938] Add impl for Azu load balancer
    Summary: Add AZU specific implementation to manage netowrk load balancers. The API updates configurations of existing load balancers based on the universe configurations. However, it currently does not create new load balancers.
    
    Test Plan:
    Manual testing by creating universes on AZU and testing the following scenarios:
    
     - LB correctly configured. API should make no changes
     - LB missing some instance groups. API should add the necessary ones, without affecting the already existing ones
     - Instances in instance group incorrectly configured. API should make the necessary updates to the instance group
     - No instance groups present in LB at all. API should correctly create new instance groups, and add instances to them
     - Incorrect health check configured on LB. API should update the health check appropriately
    Also added UTs
    
    Reviewers: sb-yb
    
    Reviewed By: sb-yb
    
    Subscribers: yugaware
    
    Differential Revision: https://phorge.dev.yugabyte.com/D26551

--- managed/src/main/java/com/yugabyte/yw/cloud/CloudAPI.java
+++ managed/src/main/java/com/yugabyte/yw/cloud/CloudAPI.java
@@ -72,4 +72,14 @@ public interface CloudAPI {
       List<Integer> ports);
 
   void validateInstanceTemplate(Provider provider, String instanceTemplate);
+
+  // Helper function to extract Resource name from resource URL
+  // It only works for URls that end with the resource Name.
+  static String getResourceNameFromResourceUrl(String resourceUrl) {
+    if (resourceUrl != null && !resourceUrl.isEmpty()) {
+      String[] urlParts = resourceUrl.split("/", 0);
+      return urlParts[urlParts.length - 1];
+    }
+    return null;
+  }
 }
