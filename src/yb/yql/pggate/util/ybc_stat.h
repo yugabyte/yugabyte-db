30556a1ae03f0ec7acf0dab00f5eb243ffe1f743
Rajat Venkatesh <1638298+vrajat@users.noreply.github.com>
2023-06-26 10:52:20 +0530

    Add new file ybc_stat.h

---
+++ src/yb/yql/pggate/util/ybc_stat.h
@@ -0,0 +1,45 @@
+// Copyright (c) YugaByte, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+// in compliance with the License.  You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software distributed under the License
+// is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+// or implied.  See the License for the specific language governing permissions and limitations
+// under the License.
+
+// C wrappers around some YB utilities. Suitable for inclusion into C codebases such as our modified
+// version of PostgreSQL.
+
+#pragma once
+
+#include <stdarg.h>
+#include <stddef.h>
+#include <stdint.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* ----------
+ * YB AUH Wait Classes
+ * ----------
+ */
+#define YB_WAIT_PERFORM					0xFE000000U
+
+/* ----------
+ * Wait Events - PERFORM
+ *
+ * Use this category when a process is waiting for a response from Perform RPC calls.
+ * ----------
+ */
+typedef enum {
+    YB_WAIT_EVENT_DML_READ = YB_WAIT_PERFORM,
+    YB_WAIT_EVENT_DML_WRITE
+} YBWaitEventPerform;
+
+#ifdef __cplusplus
+} // extern "C"
+#endif
