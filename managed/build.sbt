55edc1663afbef5ff7ff65da6c7479a390ab2432
vkumar <vkumar@yugabyte.com>
2023-07-04 11:35:23 +0000

    [PLAT-7956] Added default true runtime flag for YBC to "listen on all interfaces" i.e. bind to 0.0.0.0
    Summary:
    - For K8s, added a runtime flag at provider level "listen_on_all_interfaces_k8s".
    - By default this flag is true, i.e. YBC binds to "0.0.0.0" or "[::]" depending on whether IPv6 is being used.
    - If turned off, YBC binds on node's private_ip address.
    
    Test Plan:
    - Validated on Istio cluster
    - Validated YBC binds on [::] on Rakuten's environment and took backups
    - Normal itest run captures general K8s clusters
    
    Reviewers: sneelakantan
    
    Reviewed By: sneelakantan
    
    Subscribers: yugaware
    
    Differential Revision: https://phorge.dev.yugabyte.com/D26645

--- managed/build.sbt
+++ managed/build.sbt
@@ -467,7 +467,7 @@ runPlatform := {
   Project.extract(newState).runTask(runPlatformTask, newState)
 }
 
-libraryDependencies += "org.yb" % "ybc-client" % "2.0.0.0-b5"
+libraryDependencies += "org.yb" % "ybc-client" % "2.0.0.0-b6"
 libraryDependencies += "org.yb" % "yb-client" % "0.8.59-SNAPSHOT"
 libraryDependencies += "org.yb" % "yb-perf-advisor" % "1.0.0-b30"
 
--- managed/src/main/java/com/yugabyte/yw/common/config/ProviderConfKeys.java
+++ managed/src/main/java/com/yugabyte/yw/common/config/ProviderConfKeys.java
@@ -346,4 +346,12 @@ public class ProviderConfKeys extends RuntimeConfigKeysModule {
           "Timeout interval to wait for resource creation operations to complete sucessfully.",
           ConfDataType.LongType,
           ImmutableList.of(ConfKeyTags.PUBLIC));
+  public static final ConfKeyInfo<Boolean> ybcListenOnAllInterfacesK8s =
+      new ConfKeyInfo<>(
+          "yb.ybc_flags.listen_on_all_interfaces_k8s",
+          ScopeType.PROVIDER,
+          "Make YBC listen on 0.0.0.0",
+          "Makes YBC bind on all network interfaces",
+          ConfDataType.BooleanType,
+          ImmutableList.of(ConfKeyTags.PUBLIC));
 }
