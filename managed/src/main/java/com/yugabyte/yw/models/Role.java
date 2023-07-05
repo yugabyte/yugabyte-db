b35c55a1c2b71283d94fc1d24a5bbf402d49dfc8
Sahith Kurapati <skurapati@yugabyte.com>
2023-07-05 11:53:08 +0000

    [PLAT-9059] RBAC Roles APIs
    Summary:
    This diff covers all of the following RBAC role lifecycle APIs:
    1. Create role
    2. Edit role
    3. Get role info
    4. List all roles info
    5. Delete role
    
    **API Usage:**
    
    1. Create role
    Example API request:
    ```
    curl --location --request POST 'http://localhost:9000/api/v1/customers/<customerUUID>/rbac/role' \
    --data '{
        "name": "customUniverseRole2",
        "permission_list": [
            {
                "resource_type": "UNIVERSE",
                "permission": "READ"
            },
            {
                "resource_type": "UNIVERSE",
                "permission": "CREATE"
            }
        ]
    }'
    ```
    Example API response:
    ```
    {
        "roleUUID": "a535b712-bc13-455c-8e51-e8f2300ef4bd",
        "customerUUID": "<customerUUID>",
        "name": "customUniverseRole2",
        "createdOn": "2023-07-05T10:40:30Z",
        "updatedOn": "2023-07-05T10:40:30Z",
        "roleType": "Custom",
        "permissionDetails": {
            "permissionList": [
                {
                    "resource_type": "UNIVERSE",
                    "permission": "CREATE"
                },
                {
                    "resource_type": "UNIVERSE",
                    "permission": "READ"
                }
            ]
        }
    }
    ```
    2. Edit role
    Example API request:
    ```
    curl --location --request PUT 'http://localhost:9000/api/v1/customers/<customerUUID>/rbac/role/a535b712-bc13-455c-8e51-e8f2300ef4bd' \
    --data '{
        "permission_list": [
            {
                "resource_type": "UNIVERSE",
                "permission": "READ"
            }
        ]
    }'
    ```
    Example API response:
    ```
    {
        "roleUUID": "a535b712-bc13-455c-8e51-e8f2300ef4bd",
        "customerUUID": "<customerUUID>",
        "name": "customUniverseRole2",
        "createdOn": "2023-07-05T09:46:51Z",
        "updatedOn": "2023-07-05T10:35:43Z",
        "roleType": "Custom",
        "permissionDetails": {
            "permissionList": [
                {
                    "resource_type": "UNIVERSE",
                    "permission": "READ"
                }
            ]
        }
    }
    ```
    3. Get role info
    Example API request:
    ```
    curl --location --request GET 'http://localhost:9000/api/v1/customers/<customerUUID>/rbac/role/a535b712-bc13-455c-8e51-e8f2300ef4bd'
    ```
    Example API response:
    ```
    {
        "roleUUID": "a535b712-bc13-455c-8e51-e8f2300ef4bd",
        "customerUUID": "<customerUUID>",
        "name": "customUniverseRole2",
        "createdOn": "2023-07-05T09:46:51Z",
        "updatedOn": "2023-07-05T10:35:43Z",
        "roleType": "Custom",
        "permissionDetails": {
            "permissionList": [
                {
                    "resource_type": "UNIVERSE",
                    "permission": "READ"
                }
            ]
        }
    }
    ```
    4. List all roles info
    Example API request:
    ```
    curl --location --request GET 'http://localhost:9000/api/v1/customers/<customerUUID>/rbac/role'
    ```
    Example API response:
    ```
    [
        {
            "roleUUID": "a535b712-bc13-455c-8e51-e8f2300ef4bd",
            "customerUUID": "<customerUUID>",
            "name": "customUniverseRole2",
            "createdOn": "2023-07-05T09:46:51Z",
            "updatedOn": "2023-07-05T10:35:43Z",
            "roleType": "Custom",
            "permissionDetails": {
                "permissionList": [
                    {
                        "resource_type": "UNIVERSE",
                        "permission": "READ"
                    }
                ]
            }
        },
        {
            ...
        }
    ]
    ```
    5. Delete role
    Example API request:
    ```
    curl --location --request DELETE 'http://localhost:9000/api/v1/customers/<customerUUID>/rbac/role/a535b712-bc13-455c-8e51-e8f2300ef4bd'
    ```
    Example API response:
    ```
    {
        "success": true,
        "message": "Successfully deleted role with UUID: a535b712-bc13-455c-8e51-e8f2300ef4bd"
    }
    ```
    
    Test Plan:
    Manually tested the APIs.
    Added 10 UTs covering all edge cases.
    Run UTs.
    Run itests.
    
    Reviewers: sneelakantan, vpatibandla, #yba-api-review
    
    Reviewed By: sneelakantan, #yba-api-review
    
    Subscribers: yugaware
    
    Differential Revision: https://phorge.dev.yugabyte.com/D26673

--- managed/src/main/java/com/yugabyte/yw/models/Role.java
+++ managed/src/main/java/com/yugabyte/yw/models/Role.java
@@ -10,7 +10,11 @@ import io.ebean.Finder;
 import io.ebean.Model;
 import io.ebean.annotation.DbJson;
 import io.ebean.annotation.EnumValue;
+import io.ebean.annotation.WhenCreated;
+import io.ebean.annotation.WhenModified;
+import io.swagger.annotations.ApiModelProperty;
 import java.util.Date;
+import java.util.List;
 import java.util.Set;
 import java.util.UUID;
 import javax.persistence.Column;
@@ -20,32 +24,43 @@ import javax.persistence.Enumerated;
 import javax.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
+import lombok.extern.slf4j.Slf4j;
 
 @Entity
 @Getter
 @AllArgsConstructor
+@NoArgsConstructor
 @ToString
+@Slf4j
 public class Role extends Model {
 
   @Id
   @Column(name = "role_uuid", nullable = false)
+  @ApiModelProperty(value = "Role UUID")
   private UUID roleUUID;
 
   @Column(name = "customer_uuid", nullable = false)
+  @ApiModelProperty(value = "Customer UUID")
   private UUID customerUUID;
 
-  @Column(name = "name", nullable = false)
+  @Column(name = "name", nullable = false, unique = true)
+  @ApiModelProperty(value = "Role name")
   private String name;
 
   @Column(name = "created_on", nullable = false)
   @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'")
+  @WhenCreated
+  @ApiModelProperty(value = "Role create time", example = "2022-12-12T13:07:18Z")
   private Date createdOn;
 
   @Setter
   @Column(name = "updated_on", nullable = false)
   @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'")
+  @ApiModelProperty(value = "Role last updated time", example = "2022-12-12T13:07:18Z")
+  @WhenModified
   private Date updatedOn;
 
   public enum RoleType {
@@ -58,11 +73,13 @@ public class Role extends Model {
 
   @Enumerated(EnumType.STRING)
   @Column(name = "role_type", nullable = false)
+  @ApiModelProperty(value = "Type of the role")
   private RoleType roleType;
 
   @Setter
   @DbJson
   @Column(name = "permission_details")
+  @ApiModelProperty(value = "Set of permissions given to the role")
   private PermissionDetails permissionDetails;
 
   public static final Finder<UUID, Role> find = new Finder<UUID, Role>(Role.class) {};
@@ -98,8 +115,20 @@ public class Role extends Model {
   public static Role getOrBadRequest(UUID roleUUID) throws PlatformServiceException {
     Role role = get(roleUUID);
     if (role == null) {
-      throw new PlatformServiceException(BAD_REQUEST, "Invalid role UUID:" + roleUUID);
+      throw new PlatformServiceException(BAD_REQUEST, "Invalid role UUID: " + roleUUID);
     }
     return role;
+  }
+
+  public static Role get(String name) {
+    return find.query().where().eq("name", name).findOne();
+  }
+
+  public static List<Role> getAll() {
+    return find.query().findList();
+  }
+
+  public static List<Role> getAll(RoleType roleType) {
+    return find.query().where().eq("role_type", roleType.toString()).findList();
   }
 }
