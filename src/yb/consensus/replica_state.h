424374ef0f343eb09a87dd586d6ded7dc2b7e708
Jonathan Hu <qhu@yugabyte.com>
2023-07-05 18:46:43 +0000

    [#13171] docdb: reject expired retryable requests
    Summary:
    If a really old retryable request comes in after the request id has been GCed, the current mechanism cannot detect it.
    To harden the duplicate detection, in this diff, introduced a `start_time` to the write rpc and the tserver can tell if the start_time is too old by checking if it exceeds `retryable_request_timeout_secs`.
    
    To be conservative, do the comparison with `retryable_request_timeout_secs - max_clock_skew_usec` in case the clock of the server hosting the client is slower than the Tserver's clock.
    
    Note that this check is only done at leader side, the follower should respect the decision of the leader to avoid the situation that leader accepts the write but the followers reject it.
    Jira: DB-2856
    
    Test Plan:
    RetryableRequestTest.TestRetryableRequestTooOld
    RetryableRequestTest.TestRejectOldOriginalRequest
    QLStressTest.RetryWrites
    QLStressTest.RetryWritesWithRestarts
    
    Reviewers: sergei, timur, rthallam, bogdan
    
    Reviewed By: timur
    
    Subscribers: ybase, qhu, bogdan
    
    Differential Revision: https://phorge.dev.yugabyte.com/D24974

--- src/yb/consensus/replica_state.h
+++ src/yb/consensus/replica_state.h
@@ -418,7 +418,8 @@ class ReplicaState {
 
   OpId MinRetryableRequestOpId();
 
-  Result<bool> RegisterRetryableRequest(const ConsensusRoundPtr& round);
+  Result<bool> RegisterRetryableRequest(
+    const ConsensusRoundPtr& round, tablet::IsLeaderSide is_leader_side);
 
   RestartSafeCoarseMonoClock& Clock();
 
