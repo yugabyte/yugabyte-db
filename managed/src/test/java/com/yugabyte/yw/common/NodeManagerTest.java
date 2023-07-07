588bceb1cdfe92104444e87f581ff7c89f4ce9aa
Charles Wang <wang.charles234@gmail.com>
2023-07-07 13:42:39 -0700

    [BACKPORT 2.18][PLAT-9385] Propagate --cluster_uuid gflag to all masters/tservers from YBA during master/tserver startup
    Summary:
    Original Commit: 05f97ef1ce2d / D26413
    
    Allow the ability to populate the `--cluster_uuid` gflag on both tserver and master processes. Before, YBA only set this GFLAG on the master nodes. We add this change so future YBDB  versions can use this gflag to check inter-node RPCs (For this issue: https://github.com/yugabyte/yugabyte-db/issues/17904).
    
    Adding these changes will allow the `--cluster_uuid` gflag to be populated once we perform any action that configures gflags or installs software. So upon software upgrade, all nodes containing at least one of tserver/master processes will have this gflag set in its conf file (with exclusion of read-replicas, where the master/conf/server.conf will not be set).
    
    Example of tserver's, `server.conf` file (located under `~/tserver/conf/server.conf` in the node)
    ```
    --allow_insecure_connections=false
    --callhome_collection_level=medium
    --cert_node_filename=10.150.2.146
    --certs_dir=/home/yugabyte/yugabyte-tls-config
    --certs_for_cdc_dir=/home/yugabyte/yugabyte-tls-producer
    --certs_for_client_dir=/home/yugabyte/yugabyte-client-tls-config
    --cluster_uuid=4ea76522-acfe-42ef-8832-a46fb7929768
    --cql_proxy_bind_address=10.150.2.146:9042
    --cql_proxy_webserver_port=12000
    --enable_ysql=true
    --fs_data_dirs=/mnt/d0
    --max_log_size=256
    --metric_node_name=yb-admin-cwang-4-node-universe-n2
    --pgsql_proxy_bind_address=10.150.2.146:5433
    --pgsql_proxy_webserver_port=13000
    --placement_cloud=gcp
    --placement_region=us-west1
    --placement_uuid=4e3a2744-618b-46ec-8aff-0e4b7170062b
    --placement_zone=us-west1-c
    --redis_proxy_bind_address=10.150.2.146:6379
    --replication_factor=3
    --rpc_bind_addresses=10.150.2.146:9100
    --server_broadcast_addresses=
    --start_cql_proxy=true
    --start_redis_proxy=false
    --tserver_master_addrs=10.150.2.144:7100,10.150.2.146:7100,10.150.2.155:7100
    --txn_table_wait_min_ts_count=4
    --undefok=enable_ysql
    --use_cassandra_authentication=true
    --use_client_to_server_encryption=true
    --use_node_to_node_encryption=true
    --webserver_ca_certificate_file=/home/yugabyte/yugabyte-tls-config/ca.crt
    --webserver_certificate_file=/home/yugabyte/yugabyte-tls-config/node.10.150.2.146.crt
    --webserver_interface=10.150.2.146
    --webserver_port=9000
    --webserver_private_key_file=/home/yugabyte/yugabyte-tls-config/node.10.150.2.146.key
    --webserver_redirect_http_to_https=true
    --ysql_enable_auth=true
    --ysql_hba_conf_csv=local all yugabyte trust
    ```
    
    Note:
    - `cluster_uuid` gflag is equivalent to `universe_uuid` from YBA standpoint.
    
    Test Plan:
    Tested the following scenarios:
    
    1. Create a new universe with read replicas. A universe with 6 nodes in the primary cluster with replication factor of 3. Also have a read-replica cluster with 3 nodes. Make sure that upon successful creation, all nodes in the primary cluster have both their master and tserver gflags conf file containing the `cluster_uuid` gflag set (see summary for example). For the read-replica cluster, check that the tserver's conf file contains the `cluster_uuid` gflag. The master conf file will not contain the gflag (RR will never have a master process).
    
    2. Have an existing universe created before this change was added with the same configurations as (1). Then only master nodes will have the gflag set for the master/tserver conf files. Perform a software upgrade. After the software upgrade, we should see that all nodes (considering the RR edge case) should have both their master and tserver gflags conf file populated with the `cluster_uuid` gflag.
    
    Reviewers: sanketh, hzare, yshchetinin, nbhatia
    
    Reviewed By: sanketh
    
    Subscribers: yugaware
    
    Differential Revision: https://phorge.dev.yugabyte.com/D27304

--- managed/src/test/java/com/yugabyte/yw/common/NodeManagerTest.java
+++ managed/src/test/java/com/yugabyte/yw/common/NodeManagerTest.java
@@ -675,8 +675,8 @@ public class NodeManagerTest extends FakeDBApplication {
       gflags.put("start_cql_proxy", "false");
     }
 
+    gflags.put("cluster_uuid", String.valueOf(configureParams.getUniverseUUID()));
     if (configureParams.isMaster) {
-      gflags.put("cluster_uuid", String.valueOf(configureParams.getUniverseUUID()));
       gflags.put("replication_factor", String.valueOf(userIntent.replicationFactor));
     }
 
@@ -858,6 +858,7 @@ public class NodeManagerTest extends FakeDBApplication {
         break;
       case Configure:
         AnsibleConfigureServers.Params configureParams = (AnsibleConfigureServers.Params) params;
+        Map<String, String> gflags = new TreeMap<>(configureParams.gflags);
 
         expectedCommand.add("--master_addresses_for_tserver");
         expectedCommand.add(MASTER_ADDRESSES);
@@ -917,6 +918,10 @@ public class NodeManagerTest extends FakeDBApplication {
             case Install:
               expectedCommand.add("--tags");
               expectedCommand.add("install-software");
+              expectedCommand.add("--tags");
+              expectedCommand.add("override_gflags");
+              expectedCommand.add("--gflags");
+              expectedCommand.add(Json.stringify(Json.toJson(gflags)));
               break;
             case None:
               break;
@@ -927,8 +932,6 @@ public class NodeManagerTest extends FakeDBApplication {
           expectedCommand.add("--num_releases_to_keep");
           expectedCommand.add("3");
         }
-
-        Map<String, String> gflags = new TreeMap<>(configureParams.gflags);
 
         if (configureParams.type == Everything) {
           if ((configureParams.enableNodeToNodeEncrypt
