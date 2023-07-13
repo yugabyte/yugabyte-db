yugabyte/yugabyte-db

commit f31d42eb39bc593a9d88930b9daf6fb7f8b39783

[#18041] xCluster: Make CDCStreamId strongly typed uuid and rename to xrepl::StreamId
Summary:
Make CDCStreamId strongly typed uuid and rename it to xrepl::StreamId. This will help us to avoid passing the wrong arguments to functions and force the use of CDCStreamId everywhere instead of a mix of std::string and xrepl::StreamId, making the code more readable.
UUID uses only 16 bytes instead of the 32 + (12 to 32) bytes used by strings.

Moving StreamId into the xrepl namespace since this is shared between both cdc and xcluster.
Fixed some headers that incorrectly referred to stream_id as namespace_id or table_id.

Fixes #18041
Jira: DB-7093

Test Plan: All tests

Reviewers: jhe, xCluster, #xcluster-project

Reviewed By: jhe

Subscribers: mlillibridge, bogdan, ybase

Differential Revision: https://phorge.dev.yugabyte.com/D26598

src/yb/cdc/cdcsdk_producer.cc
@@ -1972,7 +1972,7 @@ Status HandleGetChangesForSnapshotRequest(
 // to read intents from WAL.
 
 Status GetChangesForCDCSDK(
-    const CDCStreamId& stream_id,
+    const xrepl::StreamId& stream_id,
     const TabletId& tablet_id,
     const CDCSDKCheckpointPB& from_op_id,
     const StreamMetadata& stream_metadata,

src/yb/master/master_xrepl-test.cc
@@ -43,9 +43,9 @@ static const Schema kTableSchema({
 
 class MasterTestXRepl  : public MasterTestBase {
  protected:
-  Status CreateCDCStream(const TableId& table_id, CDCStreamId* stream_id);
-  Result<GetCDCStreamResponsePB> GetCDCStream(const CDCStreamId& stream_id);
-  Status DeleteCDCStream(const CDCStreamId& stream_id);
+  Result<xrepl::StreamId> CreateCDCStream(const TableId& table_id);
+  Result<GetCDCStreamResponsePB> GetCDCStream(const xrepl::StreamId& stream_id);
+  Status DeleteCDCStream(const xrepl::StreamId& stream_id);
   Result<ListCDCStreamsResponsePB> ListCDCStreams();
   Result<bool> IsObjectPartOfXRepl(const TableId& table_id);
 
@@ -57,7 +57,7 @@ class MasterTestXRepl  : public MasterTestBase {
 
 };
 
-Status MasterTestXRepl::CreateCDCStream(const TableId& table_id, CDCStreamId* stream_id) {
+Result<xrepl::StreamId> MasterTestXRepl::CreateCDCStream(const TableId& table_id) {
   CreateCDCStreamRequestPB req;
   CreateCDCStreamResponsePB resp;
 
@@ -81,23 +81,23 @@ Status MasterTestXRepl::CreateCDCStream(const TableId& table_id, CDCStreamId* st
     return true;
   }, MonoDelta::FromSeconds(30), "Wait for cdc_state table creation to finish"));
 
-  *stream_id = resp.stream_id();
-  return Status::OK();
+  return xrepl::StreamId::FromString(resp.stream_id());
 }
 
-Result<GetCDCStreamResponsePB> MasterTestXRepl::GetCDCStream(const CDCStreamId& stream_id) {
+Result<GetCDCStreamResponsePB> MasterTestXRepl::GetCDCStream(
+    const xrepl::StreamId& stream_id) {
   GetCDCStreamRequestPB req;
   GetCDCStreamResponsePB resp;
-  req.set_stream_id(stream_id);
+  req.set_stream_id(stream_id.ToString());
 
   RETURN_NOT_OK(proxy_replication_->GetCDCStream(req, &resp, ResetAndGetController()));
   return resp;
 }
 
-Status MasterTestXRepl::DeleteCDCStream(const CDCStreamId& stream_id) {
+Status MasterTestXRepl::DeleteCDCStream(const xrepl::StreamId& stream_id) {
   DeleteCDCStreamRequestPB req;
   DeleteCDCStreamResponsePB resp;
-  req.add_stream_id(stream_id);
+  req.add_stream_id(stream_id.ToString());
 
   RETURN_NOT_OK(proxy_replication_->DeleteCDCStream(req, &resp, ResetAndGetController()));
   if (resp.has_error()) {
@@ -197,9 +197,8 @@ TEST_F(MasterTestXRepl, TestCreateCDCStream) {
   TableId table_id;
   ASSERT_OK(CreateTable(kTableName, kTableSchema, &table_id));
 
-  CDCStreamId stream_id;
   ANNOTATE_UNPROTECTED_WRITE(FLAGS_cdc_state_table_num_tablets) = 1;
-  ASSERT_OK(CreateCDCStream(table_id, &stream_id));
+  auto stream_id = ASSERT_RESULT(CreateCDCStream(table_id));
 
   auto resp = ASSERT_RESULT(GetCDCStream(stream_id));
   ASSERT_EQ(resp.stream().table_id().Get(0), table_id);
@@ -209,9 +208,8 @@ TEST_F(MasterTestXRepl, TestDeleteCDCStream) {
   TableId table_id;
   ASSERT_OK(CreateTable(kTableName, kTableSchema, &table_id));
 
-  CDCStreamId stream_id;
   ANNOTATE_UNPROTECTED_WRITE(FLAGS_cdc_state_table_num_tablets) = 1;
-  ASSERT_OK(CreateCDCStream(table_id, &stream_id));
+  auto stream_id = ASSERT_RESULT(CreateCDCStream(table_id));
 
   auto resp = ASSERT_RESULT(GetCDCStream(stream_id));
   ASSERT_EQ(resp.stream().table_id().Get(0), table_id);
@@ -228,9 +226,8 @@ TEST_F(MasterTestXRepl, TestDeleteTableWithCDCStream) {
   TableId table_id;
   ASSERT_OK(CreateTable(kTableName, kTableSchema, &table_id));
 
-  CDCStreamId stream_id;
   ANNOTATE_UNPROTECTED_WRITE(FLAGS_cdc_state_table_num_tablets) = 1;
-  ASSERT_OK(CreateCDCStream(table_id, &stream_id));
+  auto stream_id = ASSERT_RESULT(CreateCDCStream(table_id));
 
   auto resp = ASSERT_RESULT(GetCDCStream(stream_id));
   ASSERT_EQ(resp.stream().table_id().Get(0), table_id);
@@ -248,7 +245,7 @@ TEST_F(MasterTestXRepl, YB_DISABLE_TEST_IN_SANITIZERS(TestDeleteCDCStreamNoForce
   TableId table_id;
   ASSERT_OK(CreateTable(kTableName, kTableSchema, &table_id));
 
-  CDCStreamId stream_id;
+  auto stream_id = xrepl::StreamId::Nil();
   ANNOTATE_UNPROTECTED_WRITE(FLAGS_cdc_state_table_num_tablets) = 1;
   // CreateCDCStream, simulating a fully-created XCluster configuration.
   {
@@ -264,8 +261,7 @@ TEST_F(MasterTestXRepl, YB_DISABLE_TEST_IN_SANITIZERS(TestDeleteCDCStreamNoForce
     if (resp.has_error()) {
       ASSERT_OK(StatusFromPB(resp.error().status()));
     }
-
-    stream_id = resp.stream_id();
+    stream_id = ASSERT_RESULT(CreateCDCStream(table_id));
   }
 
   auto resp = ASSERT_RESULT(GetCDCStream(stream_id));
@@ -284,13 +280,21 @@ TEST_F(MasterTestXRepl, TestListCDCStreams) {
   TableId table_id;
   ASSERT_OK(CreateTable(kTableName, kTableSchema, &table_id));
 
-  CDCStreamId stream_id;
   ANNOTATE_UNPROTECTED_WRITE(FLAGS_cdc_state_table_num_tablets) = 1;
-  ASSERT_OK(CreateCDCStream(table_id, &stream_id));
+  auto stream_id = ASSERT_RESULT(CreateCDCStream(table_id));
 
   auto resp = ASSERT_RESULT(ListCDCStreams());
   ASSERT_EQ(1, resp.streams_size());
-  ASSERT_EQ(stream_id, resp.streams(0).stream_id());
+  ASSERT_EQ(stream_id.ToString(), resp.streams(0).stream_id());
+}
+
+TEST_F(MasterTestXRepl, TestIsObjectPartOfXRepl) {
+  TableId table_id;
+  ASSERT_OK(CreateTable(kTableName, kTableSchema, &table_id));
+
+  FLAGS_cdc_state_table_num_tablets = 1;
+  ASSERT_RESULT(CreateCDCStream(table_id));
+  ASSERT_TRUE(ASSERT_RESULT(IsObjectPartOfXRepl(table_id)));
 }
 
 TEST_F(MasterTestXRepl, TestIsObjectPartOfXRepl) {

commit 847a1dc65c8cba202cb9c86e96accd48680ca4f3

[#18041] xCluster: Fix build break
Summary:
Fix build break introduced in f31d42eb39bc593a9d88930b9daf6fb7f8b39783.

 * CDCStreamId renamed.
 * Duplicate test name.
Jira: DB-7093

Test Plan: jenkins: compile only, urgent

Reviewers: hsunder, pjain, xCluster

Reviewed By: hsunder

Subscribers: jason, ybase, yql, bogdan

Differential Revision: https://phorge.dev.yugabyte.com/D26953

src/yb/cdc/cdcsdk_producer.cc
@@ -1860,8 +1860,8 @@ void AcknowledgeStreamedMultiShardTxn(
 }
 
 Status HandleGetChangesForSnapshotRequest(
-    const CDCStreamId& stream_id, const TabletId& tablet_id, const CDCSDKCheckpointPB& from_op_id,
-    const std::shared_ptr<tablet::TabletPeer>& tablet_peer,
+    const xrepl::StreamId& stream_id, const TabletId& tablet_id,
+    const CDCSDKCheckpointPB& from_op_id, const std::shared_ptr<tablet::TabletPeer>& tablet_peer,
     const EnumOidLabelMap& enum_oid_label_map, const CompositeAttsMap& composite_atts_map,
     client::YBClient* client, GetChangesResponsePB* resp, SchemaDetailsMap* cached_schema_details,
     const TableId& colocated_table_id, const tablet::TabletPtr& tablet_ptr, string* table_name,

src/yb/master/master_xrepl-test.cc
@@ -297,16 +297,6 @@ TEST_F(MasterTestXRepl, TestIsObjectPartOfXRepl) {
   ASSERT_TRUE(ASSERT_RESULT(IsObjectPartOfXRepl(table_id)));
 }
 
-TEST_F(MasterTestXRepl, TestIsObjectPartOfXRepl) {
-  TableId table_id;
-  ASSERT_OK(CreateTable(kTableName, kTableSchema, &table_id));
-
-  CDCStreamId stream_id;
-  FLAGS_cdc_state_table_num_tablets = 1;
-  ASSERT_OK(CreateCDCStream(table_id, &stream_id));
-  ASSERT_TRUE(ASSERT_RESULT(IsObjectPartOfXRepl(table_id)));
-}
-
 TEST_F(MasterTestXRepl, TestSetupUniverseReplication) {
   std::string producer_id = "producer_universe";
   std::vector<std::string> producer_masters {"127.0.0.1:7100"};