5afd313360f9b0c433e28fee7d4b0d385ef9d69d
Hemant Bhanawat <hbhanawat@yugabyte.com>
2023-07-13 14:03:19 +0530

    Removed pgsentinel and pg_wait_sampling. Not needed any more

--- src/postgres/contrib/pgsentinel/sql/pgsentinel-test.sql
+++
@@ -1,6 +0,0 @@
-CREATE EXTENSION pg_stat_statements;
-CREATE EXTENSION pgsentinel;
-select pg_sleep(3);
-select count(*) > 0 AS has_data from pg_active_session_history where queryid in (select queryid from pg_stat_statements);
-DROP EXTENSION pgsentinel;
-DROP EXTENSION pg_stat_statements;
