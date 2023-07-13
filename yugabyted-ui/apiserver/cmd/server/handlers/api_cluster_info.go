d61a9e36b7fbce32877b4ef549b786cd9ffe5702
Daniel Jiang <djiang@dj-mbp-4k4l1.tail5955.ts.net>
2023-07-13 17:22:26 -0400

    [#17959][yugabyted] Use new /api/v1/tablets tserver api in go apiserver
    Summary:
    Use the new /api/v1/tablets tserver json endpoint instead
    of parsing html to get list of tablets
    
    Test Plan: no test plan
    
    Reviewers: nikhil
    
    Reviewed By: nikhil
    
    Subscribers: djiang
    
    Differential Revision: https://phorge.dev.yugabyte.com/D26981

--- yugabyted-ui/apiserver/cmd/server/handlers/api_cluster_info.go
+++ yugabyted-ui/apiserver/cmd/server/handlers/api_cluster_info.go
@@ -1186,12 +1186,19 @@ func (c *Container) GetClusterTablets(ctx echo.Context) error {
         return ctx.String(http.StatusInternalServerError, tabletsList.Error.Error())
     }
     for tabletId, tabletInfo := range tabletsList.Tablets {
+        hasLeader := false
+        for _, obj := range tabletInfo.RaftConfig {
+            if _, ok := obj["LEADER"]; ok {
+                hasLeader = true
+                break
+            }
+        }
         tabletListResponse.Data[tabletId] = models.ClusterTablet{
             Namespace: tabletInfo.Namespace,
             TableName: tabletInfo.TableName,
-            TableUuid: tabletInfo.TableUuid,
+            TableUuid: tabletInfo.TableId,
             TabletId:  tabletId,
-            HasLeader: tabletInfo.HasLeader,
+            HasLeader: hasLeader,
         }
     }
     return ctx.JSON(http.StatusOK, tabletListResponse)
