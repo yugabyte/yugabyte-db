yugabyte/yugabyte-db

commit 9b8a6cd15ced797a26cc6067d786126ebc9d4aed

[#15857] YSQL: support IndexOnlyScan aggregate pushdown
Summary:
Before this change, aggregate pushdown (not to be confused with
expression pushdown and distinct pushdown) is supported by only
ForeignScan.  The implementation is hacky in that the child node hijacks
the result scan slot into its own format.  Also, the logic would ideally
start from the planner, but it is instead all in the executor.  For the
most part, follow the existing logic when supporting IndexOnlyScan.

1. @Agg node: determine if pushdown can be done using the plan tree.
1. @Agg node: store aggrefs to scan state.
1. @IndexOnlyScan node: use aggrefs to overwrite the result scan slot to
   the aggregate pushdown format.
1. @IndexOnlyScan node: copy scan state aggrefs to scan desc.
1. @amrescan: setup aggregate targets using scan desc aggrefs.
1. @IndexOnlyScan node: copy slot reference to scan desc.
1. @amgettuple: use ybFetchNext to populate the slot.
1. @Agg node: read the scan slot in the pushdown format.

Changes involved:

- Add IndexOnlyScan.yb_ioss_aggrefs, and support IndexOnlyScan in
  YbPlanStateGetAggrefs helper.
- Move most existing aggregate logic to a new function
  YbDmlAppendTargetsAggregate.
- Modify parts of nodeIndexonlyscan.c and YB access method functions for
  aggregate pushdown.
- Expand existing tests for index only scan coverage.

Some other minor items:

- Remove irrelevant varno check when checking outer plan.
- Add helper function YbGetIndexAttnum.
- Replace "ybc_state" with "handle" in ybc_fdw.
Jira: DB-5261

Test Plan:
    ./script.sh fastdebug --gcc11

where

    #!/usr/bin/env zsh
    # script.sh
    set -eux
    ./yb_build.sh $@
    for javatest (
      'TestPgPushdown#aggregates*'
      'TestPgRegressMisc#testPgRegressMiscSerial'
      TestPgRegressAggregates
      TestPgRegressGin
      TestPgRegressHashIndex
      TestPgRegressIndex
      TestPgRegressPgMiscIndependent
    ) ./yb_build.sh $@ --sj --java-test "$javatest"

Reviewers: amartsinchyk

src/postgres/src/backend/executor/nodeIndexonlyscan.c
@@ -83,6 +83,23 @@ IndexOnlyNext(IndexOnlyScanState *node)
 
 	if (scandesc == NULL)
 	{
+		if (IsYugaByteEnabled() && node->yb_ioss_aggrefs)
+		{
+			/*
+			 * For aggregate pushdown, we read just the aggregates from DocDB
+			 * and pass that up to the aggregate node (agg pushdown wouldn't be
+			 * enabled if we needed to read more than that).  Set up a dummy
+			 * scan slot to hold that as many attributes as there are pushed
+			 * aggregates.
+			 */
+			TupleDesc tupdesc =
+				CreateTemplateTupleDesc(list_length(node->yb_ioss_aggrefs),
+										false /* hasoid */);
+			ExecInitScanTupleSlot(estate, &node->ss, tupdesc);
+			/* Refresh the local pointer. */
+			slot = node->ss.ss_ScanTupleSlot;
+		}
+
 		IndexOnlyScan *plan = castNode(IndexOnlyScan, node->ss.ps.plan);
 
 		/*
@@ -97,13 +114,19 @@ IndexOnlyNext(IndexOnlyScanState *node)
 								   node->ioss_NumOrderByKeys);
 
 		node->ioss_ScanDesc = scandesc;
-		scandesc->yb_scan_plan = (Scan *) plan;
-		scandesc->yb_rel_pushdown =
-			YbInstantiatePushdownParams(&plan->yb_pushdown, estate);
+
 
 		/* Set it up for index-only scan */
 		node->ioss_ScanDesc->xs_want_itup = true;
 		node->ioss_VMBuffer = InvalidBuffer;
+
+		if (IsYugaByteEnabled())
+		{
+			scandesc->yb_scan_plan = (Scan *) plan;
+			scandesc->yb_rel_pushdown =
+				YbInstantiatePushdownParams(&plan->yb_pushdown, estate);
+			scandesc->yb_aggrefs = node->yb_ioss_aggrefs;
+		}
 
 		/*
 		 * If no run-time keys to calculate or they are ready, go ahead and
@@ -117,14 +140,21 @@ IndexOnlyNext(IndexOnlyScanState *node)
 						 node->ioss_NumOrderByKeys);
 	}
 
-	/*
-	 * Setup LIMIT and future execution parameter before calling YugaByte scanning rountines.
-	 */
-	if (IsYugaByteEnabled()) {
+	if (IsYugaByteEnabled())
+	{
+		/*
+		 * Set up LIMIT and future execution parameter before calling Yugabyte
+		 * scanning rountines.
+		 */
 		scandesc->yb_exec_params = &estate->yb_exec_params;
-
-		// TODO(hector) Add row marks for INDEX_ONLY_SCAN
+		/* TODO(hector) Add row marks for INDEX_ONLY_SCAN. */
 		scandesc->yb_exec_params->rowmark = -1;
+
+		/*
+		 * Set reference to slot in scan desc so that YB amgettuple can use it
+		 * during aggregate pushdown.
+		 */
+		scandesc->yb_agg_slot = slot;
 	}
 
 	/*
@@ -228,6 +258,10 @@ IndexOnlyNext(IndexOnlyScanState *node)
 		}
 		else if (scandesc->xs_itup)
 			StoreIndexTuple(slot, scandesc->xs_itup, scandesc->xs_itupdesc);
+		else if (IsYugaByteEnabled() && scandesc->yb_agg_slot)
+		{
+			/* Slot should have already been updated by YB amgettuple. */
+		}
 		else
 			elog(ERROR, "no data returned for index-only scan");
 
@@ -390,11 +424,16 @@ ExecReScanIndexOnlyScan(IndexOnlyScanState *node)
 	/* reset index scan */
 	if (node->ioss_ScanDesc)
 	{
-		IndexScanDesc scandesc = node->ioss_ScanDesc;
-		IndexOnlyScan *plan = (IndexOnlyScan *) scandesc->yb_scan_plan;
-		EState *estate = node->ss.ps.state;
-		scandesc->yb_rel_pushdown =
-			YbInstantiatePushdownParams(&plan->yb_pushdown, estate);
+		if (IsYugaByteEnabled())
+		{
+			IndexScanDesc scandesc = node->ioss_ScanDesc;
+			IndexOnlyScan *plan = (IndexOnlyScan *) scandesc->yb_scan_plan;
+			EState *estate = node->ss.ps.state;
+			scandesc->yb_rel_pushdown =
+				YbInstantiatePushdownParams(&plan->yb_pushdown, estate);
+			scandesc->yb_aggrefs = node->yb_ioss_aggrefs;
+		}
+
 		index_rescan(node->ioss_ScanDesc,
 					 node->ioss_ScanKeys, node->ioss_NumScanKeys,
 					 node->ioss_OrderByKeys, node->ioss_NumOrderByKeys);

java/yb-pgsql/src/test/java/org/yb/pgsql/TestPgPushdown.java
@@ -1352,18 +1352,29 @@ public class TestPgPushdown extends BasePgSQLTest {
 
   /** Ensure pushing down aggregate functions with constant argument. */
   @Test
-  public void aggregates_const() throws Exception {
-    String tableName = "aggregate";
-    int numRows = 5000;
+  public void aggregatesConst() throws Exception {
+    new AggregatePushdownTester("COUNT(*)").test();
+    new AggregatePushdownTester("COUNT(0)").test();
+    new AggregatePushdownTester("COUNT(NULL)").test();
 
-    new AggregatePushdownTester(tableName, numRows, "COUNT(*)").test();
-    new AggregatePushdownTester(tableName, numRows, "COUNT(0)").test();
-    new AggregatePushdownTester(tableName, numRows, "COUNT(NULL)").test();
-
-    new AggregatePushdownTester(tableName, numRows, "SUM(2)").test();
-    new AggregatePushdownTester(tableName, numRows, "SUM(NULL::int)").test();
+    new AggregatePushdownTester("SUM(2)").test();
+    new AggregatePushdownTester("SUM(NULL::int)").test();
 
     // Postgres optimizes MAX(<const>) or MIN(<const>) so it isn't a real pushdown.
+
+    new AggregatePushdownTester("AVG(1)").test();
+    // TODO(#18002): uncomment the following when avg(null) is pushed down.
+    /*new AggregatePushdownTester("AVG(NULL::int)").test();*/
+  }
+
+  /** Ensure pushing down aggregate functions with variables (columns). */
+  @Test
+  public void aggregatesVar() throws Exception {
+    for (String agg : Arrays.asList("COUNT", "SUM", "MAX", "MIN", "AVG")) {
+      for (String column : Arrays.asList("id", "v")) {
+        new AggregatePushdownTester(String.format("%s(%s)", agg, column)).test();
+      }
+    }
   }
 
   //
@@ -1697,13 +1708,12 @@ public class TestPgPushdown extends BasePgSQLTest {
    * Uses a {@code (id int PRIMARY KEY, v int)} table
    */
   private class AggregatePushdownTester {
-    private final String tableName;
-    private final int numRowsToInsert;
+    private final String tableName = "aggregate";
+    private final String indexName = "aggregate_index";
+    private final int numRowsToInsert = 5000;
     private final String optimizedExpr;
 
-    public AggregatePushdownTester(String tableName, int numRowsToInsert, String optimizedExpr) {
-      this.tableName = tableName;
-      this.numRowsToInsert = numRowsToInsert;
+    public AggregatePushdownTester(String optimizedExpr) {
       this.optimizedExpr = optimizedExpr;
     }
 
@@ -1713,17 +1723,21 @@ public class TestPgPushdown extends BasePgSQLTest {
             "CREATE TABLE %s (id int PRIMARY KEY, v int)",
             tableName));
         stmt.executeUpdate(String.format(
+            "CREATE INDEX %s ON %s (v, id)",
+            indexName, tableName));
+        stmt.executeUpdate(String.format(
             "INSERT INTO %s ("
                 + "SELECT generate_series, generate_series + 1 FROM generate_series(1, %s)"
                 + ");",
             tableName, numRowsToInsert));
-        verifyPushdown(stmt);
+        verifyPushdown(stmt, "" /* hint */);
+        verifyPushdown(stmt, String.format("/*+IndexOnlyScan(%s %s)*/", tableName, indexName));
         stmt.executeUpdate(String.format("DROP TABLE %s", tableName));
       }
     }
 
-    private void verifyPushdown(Statement stmt) throws Exception {
-      String query = String.format("SELECT %s FROM %s", optimizedExpr, tableName);
+    private void verifyPushdown(Statement stmt, String hint) throws Exception {
+      String query = String.format("%s SELECT %s FROM %s", hint, optimizedExpr, tableName);
       verifyStatementMetric(
           stmt,
           query,

src/postgres/src/backend/access/index/genam.c
@@ -132,6 +132,8 @@ RelationGetIndexScan(Relation indexRelation, int nkeys, int norderbys)
 	scan->yb_scan_plan = NULL;
 	scan->yb_rel_pushdown = NULL;
 	scan->yb_idx_pushdown = NULL;
+	scan->yb_aggrefs = NIL;
+	scan->yb_agg_slot = NULL;
 	return scan;
 }
 

src/postgres/src/backend/access/yb_access/yb_lsm.c
@@ -426,7 +426,7 @@ ybcinrescan(IndexScanDesc scan, ScanKey scankey, int nscankeys,	ScanKey orderbys
 	YbScanDesc ybScan = ybcBeginScan(scan->heapRelation, scan->indexRelation,
 									 scan->xs_want_itup, nscankeys, scankey,
 									 scan->yb_scan_plan, scan->yb_rel_pushdown,
-									 scan->yb_idx_pushdown);
+									 scan->yb_idx_pushdown, scan->yb_aggrefs);
 	scan->opaque = ybScan;
 }
 
@@ -454,6 +454,53 @@ ybcingettuple(IndexScanDesc scan, ScanDirection dir)
 		ereport(DEBUG1, (errmsg("null exec_params")));
 	}
 	Assert(PointerIsValid(ybscan));
+
+	/* Special case: aggregate pushdown. */
+	if (scan->yb_aggrefs)
+	{
+		/*
+		 * TODO(#18018): deduplicate with ybc_getnext_heaptuple,
+		 * ybc_getnext_indextuple.
+		 */
+		if (ybscan->quit_scan)
+			return NULL;
+
+		/*
+		 * As of 2023-06-28, aggregate pushdown is only implemented for
+		 * IndexOnlyScan, not IndexScan.
+		 */
+		Assert(ybscan->prepare_params.index_only_scan);
+
+		/*
+		 * TODO(#18018): deduplicate with ybc_getnext_heaptuple,
+		 * ybc_getnext_indextuple.
+		 */
+		scan->xs_recheck = (ybscan->nhash_keys > 0 ||
+							!ybscan->is_full_cond_bound);
+		if (!ybscan->is_exec_done)
+		{
+			HandleYBStatus(YBCPgSetForwardScan(ybscan->handle,
+											   is_forward_scan));
+			HandleYBStatus(YBCPgExecSelect(ybscan->handle,
+										   ybscan->exec_params));
+			ybscan->is_exec_done = true;
+		}
+
+		/*
+		 * Aggregate pushdown directly modifies the scan slot rather than
+		 * passing it through xs_hitup or xs_itup.
+		 *
+		 * The index id passed into ybFetchNext is likely not going to be used
+		 * as it is only used for system table scans, which have oid, and there
+		 * shouldn't exist any system table secondary indexes that index the
+		 * oid column.
+		 * TODO(jason): deduplicate with ybcingettuple.
+		 */
+		scan->yb_agg_slot =
+			ybFetchNext(ybscan->handle, scan->yb_agg_slot,
+						RelationGetRelid(scan->indexRelation));
+		return !scan->yb_agg_slot->tts_isempty;
+	}
 
 	/*
 	 * IndexScan(SysTable, Index) --> HeapTuple.

src/postgres/src/backend/access/yb_access/yb_scan.c
@@ -57,6 +57,7 @@
 #include "yb/yql/pggate/ybc_pggate.h"
 #include "pg_yb_utils.h"
 #include "access/nbtree.h"
+#include "catalog/yb_type.h"
 
 typedef struct YbScanPlanData
 {
@@ -754,6 +755,21 @@ YbGetLengthOfKey(ScanKey *key_ptr)
 	/* We also want to include the last element. */
 	length_of_key++;
 	return length_of_key;
+}
+
+/*
+ * Given a table attribute number, get a corresponding index attribute number.
+ * Throw an error if it is not found.
+ */
+static AttrNumber
+YbGetIndexAttnum(AttrNumber table_attno, Relation index)
+{
+	for (int i = 0; i < IndexRelationGetNumberOfAttributes(index); ++i)
+	{
+		if (table_attno == index->rd_index->indkey.values[i])
+			return i + 1;
+	}
+	elog(ERROR, "column is not in index");
 }
 
 /*
@@ -1930,6 +1946,106 @@ ybcSetupTargets(YbScanDesc ybScan, YbScanPlan scan_plan, Scan *pg_scan_plan)
 }
 
 /*
+ * Set aggregate targets into handle.  If index is not null, convert column
+ * attribute numbers from table-based numbers to index-based ones.
+ */
+void
+YbDmlAppendTargetsAggregate(List *aggrefs, TupleDesc tupdesc,
+							Relation index, YBCPgStatement handle)
+{
+	ListCell   *lc;
+
+	/* Set aggregate scan targets. */
+	foreach(lc, aggrefs)
+	{
+		Aggref *aggref = lfirst_node(Aggref, lc);
+		char *func_name = get_func_name(aggref->aggfnoid);
+		ListCell *lc_arg;
+		YBCPgExpr op_handle;
+		const YBCPgTypeEntity *type_entity;
+
+		/* Get type entity for the operator from the aggref. */
+		type_entity = YbDataTypeFromOidMod(InvalidAttrNumber, aggref->aggtranstype);
+
+		/* Create operator. */
+		HandleYBStatus(YBCPgNewOperator(handle, func_name, type_entity, aggref->aggcollid, &op_handle));
+
+		/* Handle arguments. */
+		if (aggref->aggstar) {
+			/*
+			 * Add dummy argument for COUNT(*) case, turning it into COUNT(0).
+			 * We don't use a column reference as we want to count rows
+			 * even if all column values are NULL.
+			 */
+			YBCPgExpr const_handle;
+			HandleYBStatus(YBCPgNewConstant(handle,
+							 type_entity,
+							 false /* collate_is_valid_non_c */,
+							 NULL /* collation_sortkey */,
+							 0 /* datum */,
+							 false /* is_null */,
+							 &const_handle));
+			HandleYBStatus(YBCPgOperatorAppendArg(op_handle, const_handle));
+		} else {
+			/* Add aggregate arguments to operator. */
+			foreach(lc_arg, aggref->args)
+			{
+				TargetEntry *tle = lfirst_node(TargetEntry, lc_arg);
+				if (IsA(tle->expr, Const))
+				{
+					Const* const_node = castNode(Const, tle->expr);
+					/* Already checked by yb_agg_pushdown_supported */
+					Assert(const_node->constisnull || const_node->constbyval);
+
+					YBCPgExpr const_handle;
+					HandleYBStatus(YBCPgNewConstant(handle,
+									 type_entity,
+									 false /* collate_is_valid_non_c */,
+									 NULL /* collation_sortkey */,
+									 const_node->constvalue,
+									 const_node->constisnull,
+									 &const_handle));
+					HandleYBStatus(YBCPgOperatorAppendArg(op_handle, const_handle));
+				}
+				else if (IsA(tle->expr, Var))
+				{
+					/*
+					 * Use original attribute number (varoattno) instead of projected one (varattno)
+					 * as projection is disabled for tuples produced by pushed down operators.
+					 */
+					int attno = castNode(Var, tle->expr)->varoattno;
+					/*
+					 * For index (only) scans, translate the table-based
+					 * attribute number to an index-based one.
+					 */
+					if (index)
+						attno = YbGetIndexAttnum(attno, index);
+					Form_pg_attribute attr = TupleDescAttr(tupdesc, attno - 1);
+					YBCPgTypeAttrs type_attrs = {attr->atttypmod};
+
+					YBCPgExpr arg = YBCNewColumnRef(handle,
+													attno,
+													attr->atttypid,
+													attr->attcollation,
+													&type_attrs);
+					HandleYBStatus(YBCPgOperatorAppendArg(op_handle, arg));
+				}
+				else
+				{
+					/* Should never happen. */
+					ereport(ERROR,
+							(errcode(ERRCODE_INTERNAL_ERROR),
+							 errmsg("unsupported aggregate function argument type")));
+				}
+			}
+		}
+
+		/* Add aggregate operator as scan target. */
+		HandleYBStatus(YBCPgDmlAppendTarget(handle, op_handle));
+	}
+}
+
+/*
  * YbDmlAppendTargets
  *
  * Add targets to the statement.  The colref list is expected to be made up of
@@ -2044,7 +2160,8 @@ ybcBeginScan(Relation relation,
 			 int nkeys, ScanKey keys,
 			 Scan *pg_scan_plan,
 			 PushdownExprs *rel_pushdown,
-			 PushdownExprs *idx_pushdown)
+			 PushdownExprs *idx_pushdown,
+			 List *aggrefs)
 {
 	if (nkeys > YB_MAX_SCAN_KEYS)
 		ereport(ERROR,
@@ -2101,49 +2218,59 @@ ybcBeginScan(Relation relation,
 
 	/* Set up binds */
 	if (!YbBindScanKeys(ybScan, &scan_plan) || !YbBindHashKeys(ybScan))
-		ybScan->quit_scan = true;
-	else
 	{
-		/*
-		 * Set up the scan targets with respect to postgres scan plan
-		 * (i.e. set only required targets)
-		 */
+		ybScan->quit_scan = true;
+		bms_free(scan_plan.hash_key);
+		bms_free(scan_plan.primary_key);
+		bms_free(scan_plan.sk_cols);
+		return ybScan;
+	}
+
+	/*
+	 * Set up targets.  There are two separate cases:
+	 * - aggregate pushdown
+	 * - not aggregate pushdown
+	 * This ought to be reworked once aggregate pushdown supports a mix of
+	 * non-aggregate and aggregate targets.
+	 */
+	if (aggrefs != NIL)
+		YbDmlAppendTargetsAggregate(aggrefs, ybScan->target_desc, index,
+									ybScan->handle);
+	else
 		ybcSetupTargets(ybScan, &scan_plan, pg_scan_plan);
 
-		/*
-		* Set up pushdown expressions.
-		*/
-		if (rel_pushdown != NULL)
-		{
-			YbDmlAppendQuals(rel_pushdown->quals, true /* is_primary */,
-							 ybScan->handle);
-			YbDmlAppendColumnRefs(rel_pushdown->colrefs, true /* is_primary */,
-								  ybScan->handle);
-		}
-		if (idx_pushdown != NULL)
-		{
-			YbDmlAppendQuals(idx_pushdown->quals, false /* is_primary */,
-							 ybScan->handle);
-			YbDmlAppendColumnRefs(idx_pushdown->colrefs, false /* is_primary */,
-								  ybScan->handle);
-		}
-
-		/*
-		* Set the current syscatalog version (will check that we are up to
-		* date). Avoid it for syscatalog tables so that we can still use this
-		* for refreshing the caches when we are behind.
-		* Note: This works because we do not allow modifying schemas
-		* (alter/drop) for system catalog tables.
-		*/
-		if (!IsSystemRelation(relation))
-			YbSetCatalogCacheVersion(ybScan->handle,
-									 YbGetCatalogCacheVersion());
+	/*
+	 * Set up pushdown expressions.
+	 */
+	if (rel_pushdown != NULL)
+	{
+		YbDmlAppendQuals(rel_pushdown->quals, true /* is_primary */,
+						 ybScan->handle);
+		YbDmlAppendColumnRefs(rel_pushdown->colrefs, true /* is_primary */,
+							  ybScan->handle);
 	}
+	if (idx_pushdown != NULL)
+	{
+		YbDmlAppendQuals(idx_pushdown->quals, false /* is_primary */,
+						 ybScan->handle);
+		YbDmlAppendColumnRefs(idx_pushdown->colrefs, false /* is_primary */,
+							  ybScan->handle);
+	}
+
+	/*
+	 * Set the current syscatalog version (will check that we are up to
+	 * date). Avoid it for syscatalog tables so that we can still use this
+	 * for refreshing the caches when we are behind.
+	 * Note: This works because we do not allow modifying schemas
+	 * (alter/drop) for system catalog tables.
+	 */
+	if (!IsSystemRelation(relation))
+		YbSetCatalogCacheVersion(ybScan->handle,
+								 YbGetCatalogCacheVersion());
 
 	bms_free(scan_plan.hash_key);
 	bms_free(scan_plan.primary_key);
 	bms_free(scan_plan.sk_cols);
-
 	return ybScan;
 }
 
@@ -2318,20 +2445,8 @@ SysScanDesc ybc_systable_beginscan(Relation relation,
 			 * - When selecting using INDEX, the key values are bound to the IndexTable, so index attnum
 			 *   must be used for bindings.
 			 */
-			int i, j;
-			for (i = 0; i < nkeys; i++)
-			{
-				for (j = 0; j < IndexRelationGetNumberOfAttributes(index); j++)
-				{
-					if (key[i].sk_attno == index->rd_index->indkey.values[j])
-					{
-						key[i].sk_attno = j + 1;
-						break;
-					}
-				}
-				if (j == IndexRelationGetNumberOfAttributes(index))
-					elog(ERROR, "column is not in index");
-			}
+			for (int i = 0; i < nkeys; ++i)
+				key[i].sk_attno = YbGetIndexAttnum(key[i].sk_attno, index);
 		}
 	}
 
@@ -2343,7 +2458,8 @@ SysScanDesc ybc_systable_beginscan(Relation relation,
 									 key,
 									 pg_scan_plan,
 									 NULL /* rel_pushdown */,
-									 NULL /* idx_pushdown */);
+									 NULL /* idx_pushdown */,
+									 NULL /* aggrefs */);
 
 	/* Set up Postgres sys table scan description */
 	SysScanDesc scan_desc = (SysScanDesc) palloc0(sizeof(SysScanDescData));
@@ -2394,7 +2510,8 @@ HeapScanDesc ybc_heap_beginscan(Relation relation,
 									 key,
 									 pg_scan_plan,
 									 NULL /* rel_pushdown */,
-									 NULL /* idx_pushdown */);
+									 NULL /* idx_pushdown */,
+									 NULL /* aggrefs */);
 
 	/* Set up Postgres sys table scan description */
 	HeapScanDesc scan_desc = (HeapScanDesc) palloc0(sizeof(HeapScanDescData));
@@ -2453,7 +2570,8 @@ ybc_remote_beginscan(Relation relation,
 									 NULL /* key */,
 									 pg_scan_plan,
 									 pushdown /* rel_pushdown */,
-									 NULL /* idx_pushdown */);
+									 NULL /* idx_pushdown */,
+									 NULL /* aggrefs */);
 
 	/* Set up Postgres sys table scan description */
 	HeapScanDesc scan_desc = (HeapScanDesc) palloc0(sizeof(HeapScanDescData));

src/postgres/src/backend/access/ybgin/ybginget.c
@@ -516,7 +516,17 @@ ybginDoFirstExec(IndexScanDesc scan, ScanDirection dir)
 	ybginSetupBinds(scan);
 
 	/* targets */
-	ybginSetupTargets(scan);
+	if (scan->yb_aggrefs != NIL)
+		/*
+		 * As of 2023-06-28, aggregate pushdown is only implemented for
+		 * IndexOnlyScan, not IndexScan.
+		 */
+		YbDmlAppendTargetsAggregate(scan->yb_aggrefs,
+									RelationGetDescr(scan->indexRelation),
+									scan->indexRelation,
+									ybso->handle);
+	else
+		ybginSetupTargets(scan);
 
 	YbSetCatalogCacheVersion(ybso->handle, YbGetCatalogCacheVersion());
 
@@ -588,6 +598,36 @@ ybgingettuple(IndexScanDesc scan, ScanDirection dir)
 
 	/* fetch */
 	scan->xs_ctup.t_ybctid = 0;
+	if (scan->yb_aggrefs)
+	{
+		/*
+		 * As of 2023-06-28, aggregate pushdown is only implemented for
+		 * IndexOnlyScan, not IndexScan.  Also, this codepath is not exercised
+		 * because such queries hit error "non-default search mode" when
+		 * setting up binds.
+		 */
+		Assert(scan->xs_want_itup);
+
+		/*
+		 * TODO(jason): don't assume that recheck is needed.
+		 */
+		scan->xs_recheck = true;
+
+		/*
+		 * Aggregate pushdown directly modifies the scan slot rather than
+		 * passing it through xs_hitup or xs_itup.
+		 *
+		 * The index id passed into ybFetchNext is likely not going to be used
+		 * as it is only used for system table scans, which have oid, and there
+		 * shouldn't exist any system table secondary indexes that index the
+		 * oid column.
+		 * TODO(jason): deduplicate with ybcingettuple.
+		 */
+		scan->yb_agg_slot =
+			ybFetchNext(ybso->handle, scan->yb_agg_slot,
+						RelationGetRelid(scan->indexRelation));
+		return !scan->yb_agg_slot->tts_isempty;
+	}
 	while (HeapTupleIsValid(tup = ybginFetchNextHeapTuple(scan)))
 	{
 		if (true)				/* TODO(jason): don't assume a match. */

src/postgres/src/backend/executor/nodeAgg.c
@@ -1554,7 +1554,8 @@ yb_agg_pushdown_supported(AggState *aggstate)
 		return;
 
 	/* Supported outer plan. */
-	if (!IsA(outerPlanState(aggstate), ForeignScanState))
+	if (!(IsA(outerPlanState(aggstate), ForeignScanState) ||
+		  IsA(outerPlanState(aggstate), IndexOnlyScanState)))
 		return;
 	ss = (ScanState *) outerPlanState(aggstate);
 
@@ -1688,16 +1689,17 @@ yb_agg_pushdown_supported(AggState *aggstate)
 		 *   select sum(1) from (select random() as r from foo) as res;
 		 *   select sum(1) from (select (null=random())::int as r from foo) as res;
 		 * and pushdown will still be supported.
-		 * For simplicity, we do not try to match Var between aggref->args and outplan
-		 * targetlist and simply reject once we see any item that is not a simple column
-		 * reference.
+		 * TODO(#18122): For simplicity, we do not try to match Var between
+		 * aggref->args and outplan targetlist and simply reject once we see
+		 * any item that is not a simple column reference.  This should be
+		 * improved.
 		 */
 		ListCell   *t;
 		foreach(t, outerPlanState(aggstate)->plan->targetlist)
 		{
 			TargetEntry *tle = lfirst_node(TargetEntry, t);
 
-			if (!IsA(tle->expr, Var) || IS_SPECIAL_VARNO(castNode(Var, tle->expr)->varno))
+			if (!IsA(tle->expr, Var))
 				return;
 		}
 	}

commit a55a475f7d4d3c5ddc1304e20e354edfb674c6bd

[#15857] refactor: fix logic in an error codepath
Summary:
In an error/unexpected codepath, the error reporting is avoided by a bad
condition introduced by commit 9b8a6cd15ced797a26cc6067d786126ebc9d4aed:

    [#15857] YSQL: support IndexOnlyScan aggregate pushdown

The condition intends to check whether aggregate pushdown is being used,
but it uses scandesc->yb_agg_slot, which is always non-null for YB.
Switch to using the proper scandesc->yb_aggrefs.  Tests are unaffected
since they don't exercise this codepath.

Also update an aggregate pushdown code comment to be clearer.
Jira: DB-5261

Test Plan: jenkins

Reviewers: amartsinchyk

Reviewed By: amartsinchyk

Subscribers: yql

Differential Revision: https://phorge.dev.yugabyte.com/D26959

src/postgres/src/backend/executor/nodeIndexonlyscan.c
@@ -86,11 +86,11 @@ IndexOnlyNext(IndexOnlyScanState *node)
 		if (IsYugaByteEnabled() && node->yb_ioss_aggrefs)
 		{
 			/*
-			 * For aggregate pushdown, we read just the aggregates from DocDB
-			 * and pass that up to the aggregate node (agg pushdown wouldn't be
-			 * enabled if we needed to read more than that).  Set up a dummy
-			 * scan slot to hold that as many attributes as there are pushed
-			 * aggregates.
+			 * For aggregate pushdown, we only read aggregate results from
+			 * DocDB and pass that up to the aggregate node (agg pushdown
+			 * wouldn't be enabled if we needed to read other expressions). Set
+			 * up a dummy scan slot to hold as many attributes as there are
+			 * pushed aggregates.
 			 */
 			TupleDesc tupdesc =
 				CreateTemplateTupleDesc(list_length(node->yb_ioss_aggrefs),
@@ -258,7 +258,7 @@ IndexOnlyNext(IndexOnlyScanState *node)
 		}
 		else if (scandesc->xs_itup)
 			StoreIndexTuple(slot, scandesc->xs_itup, scandesc->xs_itupdesc);
-		else if (IsYugaByteEnabled() && scandesc->yb_agg_slot)
+		else if (IsYugaByteEnabled() && scandesc->yb_aggrefs)
 		{
 			/* Slot should have already been updated by YB amgettuple. */
 		}