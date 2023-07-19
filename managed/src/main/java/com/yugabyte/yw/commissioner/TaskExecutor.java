6a4a7f13993edc6362054cd4299f6dc536d04263
Aleksandr Malyshev <amalyshev@yugabyte.com>
2023-07-19 12:49:59 +0300

    [Backport 2.19.1.3322][PLAT-9707] Properly redact API responses VS logs
    Summary:
    Initially we redacted API responses and audit logs via different code paths.
    Once these 2 was merged - we started to redact both with the same code and we redacted the same list of paths.
    But the point is that we don't want to redatc all these paths in API responses - we typically just mask these and redact only a few specific fields.
    This change brings the old logic back, while still shares logic between the 2.
    
    Original diff: https://phorge.dev.yugabyte.com/D27098
    
    Test Plan:
    Create universe.
    Edit a few settings which we mask, but should not redact (smtp password, etc.)
    Apply custom hook with * characters against the universe via API.
    Make sure task params are redacted correctly.
    
    Reviewers: #yba-api-review!, svarshney, nsingh!
    
    Reviewed By: svarshney
    
    Subscribers: yugaware
    
    Differential Revision: https://phorge.dev.yugabyte.com/D27210

--- managed/src/main/java/com/yugabyte/yw/commissioner/TaskExecutor.java
+++ managed/src/main/java/com/yugabyte/yw/commissioner/TaskExecutor.java
@@ -17,10 +17,11 @@ import com.yugabyte.yw.commissioner.ITask.Abortable;
 import com.yugabyte.yw.commissioner.ITask.Retryable;
 import com.yugabyte.yw.commissioner.UserTaskDetails.SubTaskGroupType;
 import com.yugabyte.yw.common.DrainableMap;
+import com.yugabyte.yw.common.RedactingService;
+import com.yugabyte.yw.common.RedactingService.RedactionTarget;
 import com.yugabyte.yw.common.ShutdownHookHandler;
 import com.yugabyte.yw.common.Util;
 import com.yugabyte.yw.common.ha.PlatformReplicationManager;
-import com.yugabyte.yw.common.password.RedactingService;
 import com.yugabyte.yw.forms.ITaskParams;
 import com.yugabyte.yw.models.CustomerTask;
 import com.yugabyte.yw.models.ScheduleTask;
@@ -463,7 +464,8 @@ public class TaskExecutor {
     // Create a new task info object.
     TaskInfo taskInfo = new TaskInfo(taskType);
     // Set the task details.
-    taskInfo.setDetails(RedactingService.filterSecretFields(task.getTaskDetails()));
+    taskInfo.setDetails(
+        RedactingService.filterSecretFields(task.getTaskDetails(), RedactionTarget.APIS));
     // Set the owner info.
     taskInfo.setOwner(taskOwner);
     return taskInfo;
@@ -536,7 +538,8 @@ public class TaskExecutor {
       int subTaskCount = getSubTaskCount();
       log.info("Adding task #{}: {}", subTaskCount, subTask.getName());
       if (log.isDebugEnabled()) {
-        JsonNode redactedTask = RedactingService.filterSecretFields(subTask.getTaskDetails());
+        JsonNode redactedTask =
+            RedactingService.filterSecretFields(subTask.getTaskDetails(), RedactionTarget.LOGS);
         log.debug(
             "Details for task #{}: {} details= {}", subTaskCount, subTask.getName(), redactedTask);
       }
