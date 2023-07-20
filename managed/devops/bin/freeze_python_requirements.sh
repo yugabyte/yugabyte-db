a59d09cb4d288f476bd9eff996a77bae75f568ce
Muthu Chidambaram <mchidambaram@yugabyte.com>
2023-07-20 13:59:06 -0500

    [BACKPORT 2.16][PLAT-9640] Updating cryptography package
    Summary:
    Upgrading cryptography by removing it from direct dependencies as we don't import it anymore.
    
    Original diff: bfe63f48d93116dce26d85bc653d54508dca8aca / D26995
    
    Test Plan: itest
    
    Reviewers: anijhawan, svarshney
    
    Reviewed By: anijhawan
    
    Subscribers: yugaware
    
    Differential Revision: https://phorge.dev.yugabyte.com/D27062

--- managed/devops/bin/freeze_python_requirements.sh
+++ managed/devops/bin/freeze_python_requirements.sh
@@ -43,6 +43,9 @@ cd "$yb_devops_home"
 log "There should be no pre-installed Python modules in the virtualenv"
 ( set -x; run_pip list )
 
+log "Upgrading pip to latest version"
+(set -x; run_pip install --upgrade pip)
+
 log "Installing Python modules according to the ${REQUIREMENTS_FILE_NAME} file"
 ( set -x; run_pip install -r "${REQUIREMENTS_FILE_NAME}" )
 
