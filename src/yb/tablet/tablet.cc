yugabyte/yugabyte-db

commit c965b08a84527b09591e86b03daa5ebd1a6e40af

[#16196] docdb : Add per-tablet's metrics memory usage to MemTracker
Summary:
Created a new metric MemTracker under per-tablet's root Memtracker. This new one tracks tablet's counter, gauge, lag, and histogram. And it will be removed when tablet's metric entity's
Jira: DB-5627

Test Plan: Checked the memory breakdown page on local dev server

Reviewers: rthallam, esheng

Reviewed By: esheng

Subscribers: yql, bogdan, ybase

Differential Revision: https://phorge.dev.yugabyte.com/D26876

src/yb/tablet/tablet.cc
@@ -489,10 +489,13 @@ Tablet::Tablet(const TabletInitData& data)
     attrs["table_name"] = metadata_->table_name();
     attrs["table_type"] = TableType_Name(metadata_->table_type());
     attrs["namespace_name"] = metadata_->namespace_name();
+    auto metric_mem_tracker = MemTracker::CreateTracker("Metrics", mem_tracker_,
+        AddToParent::kTrue, CreateMetrics::kFalse);
     table_metrics_entity_ =
         METRIC_ENTITY_table.Instantiate(data.metric_registry, metadata_->table_id(), attrs);
     tablet_metrics_entity_ =
-        METRIC_ENTITY_tablet.Instantiate(data.metric_registry, tablet_id(), attrs);
+        METRIC_ENTITY_tablet.Instantiate(
+            data.metric_registry, tablet_id(), attrs, std::move(metric_mem_tracker));
     // If we are creating a KV table create the metrics callback.
     regulardb_statistics_ =
         rocksdb::CreateDBStatistics(table_metrics_entity_, tablet_metrics_entity_);

src/yb/util/mem_tracker.cc
@@ -201,7 +201,8 @@ class MemTracker::TrackerMetrics {
           metric_entity_->prototype().name(), std::move(name),
           CreateMetricLabel(mem_tracker), MetricUnit::kBytes,
           CreateMetricDescription(mem_tracker), yb::MetricLevel::kInfo)),
-        mem_tracker.consumption());
+        static_cast<int64_t>(0));
+    metric_->set_value(mem_tracker.consumption());
   }
 
   TrackerMetrics(TrackerMetrics&) = delete;
@@ -813,8 +814,9 @@ void MemTracker::SetMetricEntity(
         << metrics_->metric_entity_->id();
     return;
   }
-  metrics_ = std::make_unique<TrackerMetrics>(metric_entity);
-  metrics_->Init(*this, name_suffix);
+  auto metrics = std::make_unique<TrackerMetrics>(metric_entity);
+  metrics->Init(*this, name_suffix);
+  metrics_ = std::move(metrics);
 }
 
 void MemTracker::TEST_SetReleasedMemorySinceGC(int64_t value) {

src/yb/util/metric_entity.cc
@@ -142,8 +142,9 @@ MetricEntityPrototype::~MetricEntityPrototype() {
 scoped_refptr<MetricEntity> MetricEntityPrototype::Instantiate(
     MetricRegistry* registry,
     const std::string& id,
-    const MetricEntity::AttributeMap& initial_attrs) const {
-  return registry->FindOrCreateEntity(this, id, initial_attrs);
+    const MetricEntity::AttributeMap& initial_attrs,
+    std::shared_ptr<MemTracker> mem_tracker) const {
+  return registry->FindOrCreateEntity(this, id, initial_attrs, std::move(mem_tracker));
 }
 
 scoped_refptr<MetricEntity> MetricEntityPrototype::Instantiate(
@@ -156,13 +157,18 @@ scoped_refptr<MetricEntity> MetricEntityPrototype::Instantiate(
 //
 
 MetricEntity::MetricEntity(const MetricEntityPrototype* prototype,
-                           std::string id, AttributeMap attributes)
+                           std::string id, AttributeMap attributes,
+                           std::shared_ptr<MemTracker> mem_tracker)
     : prototype_(prototype),
       id_(std::move(id)),
-      attributes_(std::move(attributes)) {
+      attributes_(std::move(attributes)),
+      mem_tracker_(std::move(mem_tracker)) {
 }
 
 MetricEntity::~MetricEntity() {
+  if (mem_tracker_) {
+    mem_tracker_->UnregisterFromParent();
+  }
 }
 
 const std::regex& PrometheusNameRegex() {
@@ -431,6 +437,9 @@ scoped_refptr<Counter> MetricEntity::FindOrCreateCounter(
   if (!m) {
     m = new Counter(proto);
     InsertOrDie(&metric_map_, proto, m);
+    if (mem_tracker_) {
+      mem_tracker_->Consume(sizeof(Counter));
+    }
   }
   return m;
 }
@@ -443,6 +452,9 @@ scoped_refptr<Counter> MetricEntity::FindOrCreateCounter(
   if (!m) {
     m = new Counter(std::move(proto));
     InsertOrDie(&metric_map_, m->prototype(), m);
+    if (mem_tracker_) {
+      mem_tracker_->Consume(sizeof(Counter));
+    }
   }
   return m;
 }
@@ -455,6 +467,9 @@ scoped_refptr<MillisLag> MetricEntity::FindOrCreateMillisLag(
   if (!m) {
     m = new MillisLag(proto);
     InsertOrDie(&metric_map_, proto, m);
+    if (mem_tracker_) {
+      mem_tracker_->Consume(sizeof(MillisLag));
+    }
   }
   return m;
 }
@@ -468,6 +483,9 @@ scoped_refptr<AtomicMillisLag> MetricEntity::FindOrCreateAtomicMillisLag(
   if (!m) {
     m = new AtomicMillisLag(proto);
     InsertOrDie(&metric_map_, proto, m);
+    if (mem_tracker_) {
+      mem_tracker_->Consume(sizeof(AtomicMillisLag));
+    }
   }
   return m;
 }
@@ -480,6 +498,9 @@ scoped_refptr<Histogram> MetricEntity::FindOrCreateHistogram(
   if (!m) {
     m = new Histogram(proto);
     InsertOrDie(&metric_map_, proto, m);
+    if (mem_tracker_) {
+      mem_tracker_->Consume(m->DynamicMemoryUsage());
+    }
   }
   return m;
 }
@@ -496,6 +517,9 @@ scoped_refptr<Histogram> MetricEntity::FindOrCreateHistogram(
     m = new Histogram(std::move(proto), highest_trackable_value, num_significant_digits,
                       export_percentile);
     InsertOrDie(&metric_map_, m->prototype(), m);
+    if (mem_tracker_) {
+      mem_tracker_->Consume(m->DynamicMemoryUsage());
+    }
   }
   return m;
 }

src/yb/util/metric_entity.h
@@ -21,6 +21,7 @@
 
 #include "yb/gutil/callback_forward.h"
 #include "yb/util/locks.h"
+#include "yb/util/mem_tracker.h"
 #include "yb/util/metrics_fwd.h"
 #include "yb/util/status_fwd.h"
 
@@ -104,7 +105,8 @@ class MetricEntityPrototype {
   scoped_refptr<MetricEntity> Instantiate(
       MetricRegistry* registry,
       const std::string& id,
-      const std::unordered_map<std::string, std::string>& initial_attrs) const;
+      const std::unordered_map<std::string, std::string>& initial_attrs,
+      std::shared_ptr<MemTracker> mem_tracker = nullptr) const;
 
  private:
   const char* const name_;
@@ -204,7 +206,8 @@ class MetricEntity : public RefCountedThreadSafe<MetricEntity> {
   friend class MetricRegistry;
   friend class RefCountedThreadSafe<MetricEntity>;
 
-  MetricEntity(const MetricEntityPrototype* prototype, std::string id, AttributeMap attributes);
+  MetricEntity(const MetricEntityPrototype* prototype, std::string id, AttributeMap attributes,
+               std::shared_ptr<MemTracker> mem_tracker = nullptr);
   ~MetricEntity();
 
   // Ensure that the given metric prototype is allowed to be instantiated
@@ -222,6 +225,8 @@ class MetricEntity : public RefCountedThreadSafe<MetricEntity> {
 
   // The key/value attributes. Protected by lock_
   AttributeMap attributes_;
+
+  std::shared_ptr<MemTracker> mem_tracker_ GUARDED_BY(lock_);
 
   // The set of metrics which should never be retired. Protected by lock_.
   std::vector<scoped_refptr<Metric> > never_retire_metrics_;

src/yb/util/metrics.h
@@ -256,6 +256,7 @@
 #include "yb/util/metrics_fwd.h"
 #include "yb/util/status_fwd.h"
 #include "yb/util/atomic.h"
+#include "yb/util/hdr_histogram.h"
 #include "yb/util/jsonwriter.h"
 #include "yb/util/metrics_writer.h"
 #include "yb/util/monotime.h"
@@ -488,7 +489,8 @@ class MetricRegistry {
 
   scoped_refptr<MetricEntity> FindOrCreateEntity(const MetricEntityPrototype* prototype,
                                                  const std::string& id,
-                                                 const MetricEntity::AttributeMap& initial_attrs);
+                                                 const MetricEntity::AttributeMap& initial_attrs,
+                                                 std::shared_ptr<MemTracker> mem_tracker = nullptr);
 
   // Writes metrics in this registry to 'writer'.
   //
@@ -1064,6 +1066,8 @@ class Histogram : public Metric {
   //   // is thread safe.
   const HdrHistogram* histogram() const { return histogram_.get(); }
 
+  size_t DynamicMemoryUsage() const { return histogram_->DynamicMemoryUsage() + sizeof(*this); }
+
   uint64_t CountInBucketForValueForTests(uint64_t value) const;
   uint64_t MinValueForTests() const;
   uint64_t MaxValueForTests() const;
@@ -1131,6 +1135,9 @@ inline scoped_refptr<AtomicGauge<T>> MetricEntity::FindOrCreateGauge(
   if (it == metric_map_.end()) {
     auto result = new AtomicGauge<T>(proto, initial_value);
     metric_map_.emplace(proto, result);
+    if (mem_tracker_) {
+      mem_tracker_->Consume(sizeof(AtomicGauge<T>));
+    }
     return result;
   }
   return down_cast<AtomicGauge<T>*>(it->second.get());
@@ -1148,6 +1155,9 @@ inline scoped_refptr<AtomicGauge<T> > MetricEntity::FindOrCreateGauge(
   }
   auto result = new AtomicGauge<T>(std::move(proto), initial_value);
   metric_map_.emplace(result->prototype(), result);
+  if (mem_tracker_) {
+    mem_tracker_->Consume(sizeof(AtomicGauge<T>));
+  }
   return result;
 }
 
@@ -1163,6 +1173,9 @@ inline scoped_refptr<FunctionGauge<T> > MetricEntity::FindOrCreateFunctionGauge(
   }
   auto result = new FunctionGauge<T>(proto, function);
   metric_map_.emplace(proto, result);
+  if (mem_tracker_) {
+     mem_tracker_->Consume(sizeof(FunctionGauge<T>));
+  }
   return result;
 }
 

src/yb/util/hdr_histogram.cc
@@ -372,6 +372,10 @@ void HdrHistogram::DumpHumanReadable(std::ostream* out) const {
   }
 }
 
+size_t HdrHistogram::DynamicMemoryUsage() const {
+  return sizeof(*this) + sizeof(Atomic64) * counts_array_length_;
+}
+
 ///////////////////////////////////////////////////////////////////////
 // AbstractHistogramIterator
 ///////////////////////////////////////////////////////////////////////

src/yb/util/hdr_histogram.h
@@ -196,6 +196,8 @@ class HdrHistogram {
   // Dump a formatted, multiline string describing this histogram to 'out'.
   void DumpHumanReadable(std::ostream* out) const;
 
+  size_t DynamicMemoryUsage() const;
+
  private:
   friend class AbstractHistogramIterator;
 

src/yb/util/metrics.cc
@@ -327,11 +327,12 @@ void MetricPrototype::WriteFields(JsonWriter* writer,
 scoped_refptr<MetricEntity> MetricRegistry::FindOrCreateEntity(
     const MetricEntityPrototype* prototype,
     const std::string& id,
-    const MetricEntity::AttributeMap& initial_attributes) {
+    const MetricEntity::AttributeMap& initial_attributes,
+    std::shared_ptr<MemTracker> mem_tracker) {
   std::lock_guard l(lock_);
   scoped_refptr<MetricEntity> e = FindPtrOrNull(entities_, id);
   if (!e) {
-    e = new MetricEntity(prototype, id, initial_attributes);
+    e = new MetricEntity(prototype, id, initial_attributes, std::move(mem_tracker));
     InsertOrDie(&entities_, id, e);
   } else {
     e->SetAttributes(initial_attributes);

commit 2e30a8ec941ea597a747034237b3310a2b6f1b9d

[#16196] DocDB: Fix ASAN build
Summary:
Fix regression introduced in D26876/c965b08a84527b09591e86b03daa5ebd1a6e40af.

In this diff we got the following circular dependency:
tablet mem tracker retains tablet metric entity
tablet metric entity retains metric mem tracker
metric mem tracker retains tablet mem tracker

Fixed by moving metric mem tracker ownership to the tablets and storing mem tracker in tablet metric entity as weak pointer.
Jira: DB-5627

Test Plan: Jenkins

Reviewers: yyan, xCluster

Reviewed By: yyan

Subscribers: yql, ybase

Differential Revision: https://phorge.dev.yugabyte.com/D27335

src/yb/tablet/tablet.cc
@@ -489,13 +489,12 @@ Tablet::Tablet(const TabletInitData& data)
     attrs["table_name"] = metadata_->table_name();
     attrs["table_type"] = TableType_Name(metadata_->table_type());
     attrs["namespace_name"] = metadata_->namespace_name();
-    auto metric_mem_tracker = MemTracker::CreateTracker("Metrics", mem_tracker_,
-        AddToParent::kTrue, CreateMetrics::kFalse);
+    metric_mem_tracker_ = MemTracker::CreateTracker(
+        "Metrics", mem_tracker_, AddToParent::kTrue, CreateMetrics::kFalse);
     table_metrics_entity_ =
         METRIC_ENTITY_table.Instantiate(data.metric_registry, metadata_->table_id(), attrs);
-    tablet_metrics_entity_ =
-        METRIC_ENTITY_tablet.Instantiate(
-            data.metric_registry, tablet_id(), attrs, std::move(metric_mem_tracker));
+    tablet_metrics_entity_ = METRIC_ENTITY_tablet.Instantiate(
+            data.metric_registry, tablet_id(), attrs, metric_mem_tracker_);
     // If we are creating a KV table create the metrics callback.
     regulardb_statistics_ =
         rocksdb::CreateDBStatistics(table_metrics_entity_, tablet_metrics_entity_);
@@ -577,6 +576,9 @@ Tablet::~Tablet() {
   }
   if (intentdb_mem_tracker_) {
     intentdb_mem_tracker_->UnregisterFromParent();
+  }
+  if (metric_mem_tracker_) {
+    metric_mem_tracker_->UnregisterFromParent();
   }
   mem_tracker_->UnregisterFromParent();
 }

src/yb/tablet/tablet.h
@@ -992,6 +992,7 @@ class Tablet : public AbstractTablet,
 
   MetricEntityPtr tablet_metrics_entity_;
   MetricEntityPtr table_metrics_entity_;
+  MemTrackerPtr metric_mem_tracker_;
   std::unique_ptr<TabletMetrics> metrics_;
   std::shared_ptr<void> metric_detacher_;
 

src/yb/util/mem_tracker.cc
@@ -198,10 +198,11 @@ class MemTracker::TrackerMetrics {
     }
     metric_ = metric_entity_->FindOrCreateGauge(
         std::unique_ptr<GaugePrototype<int64_t>>(new OwningGaugePrototype<int64_t>(
-          metric_entity_->prototype().name(), std::move(name),
-          CreateMetricLabel(mem_tracker), MetricUnit::kBytes,
-          CreateMetricDescription(mem_tracker), yb::MetricLevel::kInfo)),
+            metric_entity_->prototype().name(), std::move(name),
+            CreateMetricLabel(mem_tracker), MetricUnit::kBytes,
+            CreateMetricDescription(mem_tracker), yb::MetricLevel::kInfo)),
         static_cast<int64_t>(0));
+    // Consumption could be changed when gauge is created, so set it separately.
     metric_->set_value(mem_tracker.consumption());
   }
 

src/yb/util/metric_entity.cc
@@ -165,11 +165,7 @@ MetricEntity::MetricEntity(const MetricEntityPrototype* prototype,
       mem_tracker_(std::move(mem_tracker)) {
 }
 
-MetricEntity::~MetricEntity() {
-  if (mem_tracker_) {
-    mem_tracker_->UnregisterFromParent();
-  }
-}
+MetricEntity::~MetricEntity() = default;
 
 const std::regex& PrometheusNameRegex() {
   return prometheus_name_regex;
@@ -429,6 +425,12 @@ void MetricEntity::SetAttribute(const string& key, const string& val) {
   attributes_[key] = val;
 }
 
+void MetricEntity::AddConsumption(int64_t consumption) {
+  if (auto mem_tracker = mem_tracker_.lock()) {
+    mem_tracker->Consume(consumption);
+  }
+}
+
 scoped_refptr<Counter> MetricEntity::FindOrCreateCounter(
     const CounterPrototype* proto) {
   CheckInstantiation(proto);
@@ -437,9 +439,7 @@ scoped_refptr<Counter> MetricEntity::FindOrCreateCounter(
   if (!m) {
     m = new Counter(proto);
     InsertOrDie(&metric_map_, proto, m);
-    if (mem_tracker_) {
-      mem_tracker_->Consume(sizeof(Counter));
-    }
+    AddConsumption(sizeof(Counter));
   }
   return m;
 }
@@ -452,9 +452,7 @@ scoped_refptr<Counter> MetricEntity::FindOrCreateCounter(
   if (!m) {
     m = new Counter(std::move(proto));
     InsertOrDie(&metric_map_, m->prototype(), m);
-    if (mem_tracker_) {
-      mem_tracker_->Consume(sizeof(Counter));
-    }
+    AddConsumption(sizeof(Counter));
   }
   return m;
 }
@@ -467,9 +465,7 @@ scoped_refptr<MillisLag> MetricEntity::FindOrCreateMillisLag(
   if (!m) {
     m = new MillisLag(proto);
     InsertOrDie(&metric_map_, proto, m);
-    if (mem_tracker_) {
-      mem_tracker_->Consume(sizeof(MillisLag));
-    }
+    AddConsumption(sizeof(MillisLag));
   }
   return m;
 }
@@ -483,9 +479,7 @@ scoped_refptr<AtomicMillisLag> MetricEntity::FindOrCreateAtomicMillisLag(
   if (!m) {
     m = new AtomicMillisLag(proto);
     InsertOrDie(&metric_map_, proto, m);
-    if (mem_tracker_) {
-      mem_tracker_->Consume(sizeof(AtomicMillisLag));
-    }
+    AddConsumption(sizeof(AtomicMillisLag));
   }
   return m;
 }
@@ -498,8 +492,8 @@ scoped_refptr<Histogram> MetricEntity::FindOrCreateHistogram(
   if (!m) {
     m = new Histogram(proto);
     InsertOrDie(&metric_map_, proto, m);
-    if (mem_tracker_) {
-      mem_tracker_->Consume(m->DynamicMemoryUsage());
+    if (auto mem_tracker = mem_tracker_.lock()) {
+      mem_tracker->Consume(m->DynamicMemoryUsage());
     }
   }
   return m;
@@ -517,8 +511,8 @@ scoped_refptr<Histogram> MetricEntity::FindOrCreateHistogram(
     m = new Histogram(std::move(proto), highest_trackable_value, num_significant_digits,
                       export_percentile);
     InsertOrDie(&metric_map_, m->prototype(), m);
-    if (mem_tracker_) {
-      mem_tracker_->Consume(m->DynamicMemoryUsage());
+    if (auto mem_tracker = mem_tracker_.lock()) {
+      mem_tracker->Consume(m->DynamicMemoryUsage());
     }
   }
   return m;

src/yb/util/metric_entity.h
@@ -215,6 +215,8 @@ class MetricEntity : public RefCountedThreadSafe<MetricEntity> {
   // type defined within the metric prototype.
   void CheckInstantiation(const MetricPrototype* proto) const;
 
+  void AddConsumption(int64_t consumption) REQUIRES(lock_);
+
   const MetricEntityPrototype* const prototype_;
   const std::string id_;
 
@@ -226,7 +228,7 @@ class MetricEntity : public RefCountedThreadSafe<MetricEntity> {
   // The key/value attributes. Protected by lock_
   AttributeMap attributes_;
 
-  std::shared_ptr<MemTracker> mem_tracker_ GUARDED_BY(lock_);
+  std::weak_ptr<MemTracker> mem_tracker_ GUARDED_BY(lock_);
 
   // The set of metrics which should never be retired. Protected by lock_.
   std::vector<scoped_refptr<Metric> > never_retire_metrics_;

src/yb/util/metrics.h
@@ -1135,9 +1135,7 @@ inline scoped_refptr<AtomicGauge<T>> MetricEntity::FindOrCreateGauge(
   if (it == metric_map_.end()) {
     auto result = new AtomicGauge<T>(proto, initial_value);
     metric_map_.emplace(proto, result);
-    if (mem_tracker_) {
-      mem_tracker_->Consume(sizeof(AtomicGauge<T>));
-    }
+    AddConsumption(sizeof(AtomicGauge<T>));
     return result;
   }
   return down_cast<AtomicGauge<T>*>(it->second.get());
@@ -1155,9 +1153,7 @@ inline scoped_refptr<AtomicGauge<T> > MetricEntity::FindOrCreateGauge(
   }
   auto result = new AtomicGauge<T>(std::move(proto), initial_value);
   metric_map_.emplace(result->prototype(), result);
-  if (mem_tracker_) {
-    mem_tracker_->Consume(sizeof(AtomicGauge<T>));
-  }
+  AddConsumption(sizeof(AtomicGauge<T>));
   return result;
 }
 
@@ -1173,9 +1169,7 @@ inline scoped_refptr<FunctionGauge<T> > MetricEntity::FindOrCreateFunctionGauge(
   }
   auto result = new FunctionGauge<T>(proto, function);
   metric_map_.emplace(proto, result);
-  if (mem_tracker_) {
-     mem_tracker_->Consume(sizeof(FunctionGauge<T>));
-  }
+  AddConsumption(sizeof(FunctionGauge<T>));
   return result;
 }
 