f2f6a1ecbb845539fa934ab20282103b8fe39040
Steve Song <5407809+SteveXiSong@users.noreply.github.com>
2023-07-28 19:02:59 +0000

    [#18321] YSQL: Enable preloading additional tables by specifying catalog list in flags
    Summary:
    For the additional catalog tables to preload, different customers may have different schemas and the need to preload varies case by case. We had to make a customized build to adjust which catalogs to preload, which took repetitive work. This approach is also not forward-compatible.
    
    Instead, unlike controlling preloading the core tables by `ysql_enable_read_request_caching`, we should make the flag take a list of catalog tables as input and introduce a new string gflag `ysql_catalog_preload_additional_table_list`. This allows us and our customers to choose what to preload without extra customized builds.
    
    Caveat:
    Though this change doesn't implement all PG catalog name mappings, it is extensible as needed and it won't cause conflicts for different preload needs, in contrast to using the old boolean flag `ysql_catalog_preload_additional_tables`, which is dependent on hardcoded catalogs.
    Jira: DB-7310
    
    Test Plan:
    **Manual test
    **Restart cluster with
    ```
    ./bin/yb-ctl restart --data_dir ~/yugabyte-data-debug --tserver_flags='ysql_enable_read_request_caching=true,"ysql_catalog_preload_additional_table_list=pg_range,pg_statistic,pg_sth,pg_sth2"'
    ```
    Checked logs (removed before sending for review) that additional catalogs are registered.
    
    Got the following in the logs
    ```
    W0725 07:01:44.150847 1699151 relcache.c:2743] Found unrecognized catalog "pg_sth" in the flag. Ignored.
    W0725 07:01:44.150902 1699151 relcache.c:2743] Found unrecognized catalog "pg_sth2" in the flag. Ignored.
    I0725 07:01:44.150913 1699151 relcache.c:2763] YSQL is prefetching additional catalogs.
    ```
    
    Validated invalid chars in this flag and it failed the validation:
    ```
    E0728 17:53:07.141602 937851 ybc_pggate.cc:113] Found invalid character * in flag ysql_catalog_preload_additional_table_list
    ERROR: failed validation of new value 'pg_range,pg_statistic,,pg_invalid,*,@' for flag 'ysql_catalog_preload_additional_table_list'
    ```
    
    Also, validate it in the `debug` build.
    
    **Automatic test**
    ```
    ./yb_build.sh debug --ccmds --clang16 --sjb --cxx-test pg_catalog_perf-test --gtest_filter PgCatalogPerfTest.RPCCountOnStartupAdditionalPreload --no-remote
    ```
    
    Reviewers: dmitry, myang
    
    Reviewed By: myang
    
    Subscribers: jason, yql
    
    Differential Revision: https://phorge.dev.yugabyte.com/D27186

--- src/postgres/src/backend/utils/misc/pg_yb_utils.c
+++ src/postgres/src/backend/utils/misc/pg_yb_utils.c
@@ -60,7 +60,9 @@
 #include "catalog/pg_partitioned_table.h"
 #include "catalog/pg_policy.h"
 #include "catalog/pg_proc.h"
+#include "catalog/pg_range_d.h"
 #include "catalog/pg_rewrite.h"
+#include "catalog/pg_statistic_d.h"
 #include "catalog/pg_tablespace.h"
 #include "catalog/pg_trigger.h"
 #include "catalog/pg_type.h"
@@ -3249,8 +3251,14 @@ void YbRegisterSysTableForPrefetching(int sys_table_id) {
 		case RelationRelationId:                          // pg_class
 			sys_table_index_id = ClassNameNspIndexId;
 			break;
+		case RangeRelationId:                             // pg_range
+			sys_only_filter_attr = Anum_pg_range_rngtypid;
+			break;
 		case RewriteRelationId:                           // pg_rewrite
 			sys_table_index_id = RewriteRelRulenameIndexId;
+			break;
+		case StatisticRelationId:                         // pg_statistic
+			sys_only_filter_attr = Anum_pg_statistic_starelid;
 			break;
 		case TriggerRelationId:                           // pg_trigger
 			sys_table_index_id = TriggerRelidNameIndexId;
