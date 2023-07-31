yugabyte/yugabyte-db

commit d6d0cf6ea4553efc3334f8f550aae7164740692e

[#14114] DocDB: Throw detailed error message when a deadlock is detected
Summary:
In the exisiting implementation, we return a generic error message when transactions are aborted due to a deadlock.

This diff addresses the issue by returning a detailed error info when transactions are aborted due to a deadlock. Introduced a new flag `clear_deadlocked_txns_info_older_than_seconds`. The deadlock detector now stores transactions aborted due to a deadlock, and we prune the list every `transaction_deadlock_detection_interval_usec`. The info gets removed if it is older than `clear_deadlocked_txns_info_older_than_seconds`.

The transaction coordinator now checks with the deadlock detector before reporting `ABORTED` for a txn that it is unable to find. If the txn is found at the deadlock detector, `GetTransactionStatusResponsePB` resp is populated with an newly added field `AppStatusPB reason`, which contains detailed info on the deadlock error.

Deadlocked transactions waiting in the wait-queue are aborted in 2 ways:
1. When the txn participant isn't able to find the txn, we directly poll the status tablet for the waiter's status.
2. The txn participant responds with the status it is last aware of (in this case, `ABORTED`).

Since both these codepaths use `GetTransactionStatusResponsePB`, we explicitly check if `reason` is populated and return back the detailed error, if any.

Note: In some cases, it is still possible that we return a generic error. When a waiter txn is notified that all of its blockers have been aborted, it re-runs conflict resolution. When `PrepareMetadata` is executed, the participant returns a generic error message if the txn is not found.
This can happen as the waiter txn too could been aborted as part of the deadlock, but that signal got delayed. If we deterministically abort just one txn when we find a deadlock (https://github.com/yugabyte/yugabyte-db/issues/14165), it should probably solve this issue.
Jira: DB-3597

Test Plan:

src/yb/docdb/deadlock_detector.cc
@@ -19,6 +19,7 @@
 
 #include "yb/client/transaction_rpc.h"
 
+#include "yb/common/pgsql_error.h"
 #include "yb/common/transaction.h"
 #include "yb/common/wire_protocol.h"
 
@@ -41,6 +42,7 @@
 #include "yb/util/strongly_typed_uuid.h"
 #include "yb/util/tsan_util.h"
 #include "yb/util/unique_lock.h"
+#include "yb/util/yb_pg_errcodes.h"
 
 using namespace std::placeholders;
 using namespace std::literals;
@@ -52,6 +54,12 @@ DEFINE_UNKNOWN_int32(
     "is too low, we may remove entries too aggressively and end up failing to report deadlocks.");
 TAG_FLAG(clear_active_probes_older_than_seconds, hidden);
 TAG_FLAG(clear_active_probes_older_than_seconds, advanced);
+
+DEFINE_RUNTIME_int32(
+    clear_deadlocked_txns_info_older_than_heartbeats, 10,
+    "Minimum number of transaction heartbeat periods for which a deadlocked transaction's info is "
+    "retained, after it has been reported to be aborted. This ensures the memory used to track "
+    "info of deadlocked transactions does not grow unbounded.");
 
 METRIC_DEFINE_coarse_histogram(
     tablet, deadlock_size, "Deadlock size", yb::MetricUnit::kTransactions,
@@ -66,6 +74,8 @@ METRIC_DEFINE_gauge_uint64(
 DEFINE_test_flag(int32, sleep_amidst_iterating_blockers_ms, 0,
     "Time for which the thread sleeps in each iteration while looping over the computed wait-for "
     "probes and sending information to the waiters.");
+
+DECLARE_uint64(transaction_heartbeat_usec);
 
 namespace yb {
 namespace tablet {
@@ -358,6 +368,22 @@ using LocalProbeProcessorPtr = std::shared_ptr<LocalProbeProcessor>;
 
 } // namespace
 
+std::string ConstructDeadlockedMessage(const TransactionId& waiter,
+                                 const tserver::ProbeTransactionDeadlockResponsePB& resp) {
+  std::stringstream ss;
+  ss << Format("Transaction $0 aborted due to a deadlock.\n$0", waiter.ToString());
+  for (auto i = 1 ; i < resp.deadlocked_txn_ids_size() ; i++) {
+    auto id_or_status = FullyDecodeTransactionId(resp.deadlocked_txn_ids(i));
+    if (!id_or_status.ok()) {
+      ss << Format(" -> [Error decoding txn id: $0]", id_or_status.status());
+    } else {
+      ss << Format(" -> $0", *id_or_status);
+    }
+  }
+  ss << Format(" -> $0\n", waiter.ToString());
+  return ss.str();
+}
+
 class DeadlockDetector::Impl : public std::enable_shared_from_this<DeadlockDetector::Impl> {
  public:
   explicit Impl(
@@ -562,6 +588,20 @@ class DeadlockDetector::Impl : public std::enable_shared_from_this<DeadlockDetec
       // TODO(wait-queues): Trigger probes only for waiters which which have
       // wait_start_time > Now() - N seconds
       probes_to_send = GetProbesToSend(waiters_);
+
+      // Clear the info of old deadlocked transactions.
+      auto interval = FLAGS_clear_deadlocked_txns_info_older_than_heartbeats *
+          FLAGS_transaction_heartbeat_usec * 1us;
+      auto expired_cutoff_time = CoarseMonoClock::Now() - interval;
+      for (auto it = recently_deadlocked_txns_info_.begin();
+           it != recently_deadlocked_txns_info_.end();) {
+        const auto& deadlock_time = it->second.second;
+        if (deadlock_time < expired_cutoff_time) {
+          it = recently_deadlocked_txns_info_.erase(it);
+        } else {
+          it++;
+        }
+      }
     }
 
     for (auto& processor : probes_to_send) {
@@ -579,6 +619,16 @@ class DeadlockDetector::Impl : public std::enable_shared_from_this<DeadlockDetec
                             << " old probes from tracking for remote detector " << detector_id;
       }
     }
+  }
+
+  Status GetTransactionDeadlockStatus(const TransactionId& txn_id) {
+    SharedLock<decltype(mutex_)> l(mutex_);
+    auto it = recently_deadlocked_txns_info_.find(txn_id);
+    if (it == recently_deadlocked_txns_info_.end()) {
+      return Status::OK();
+    }
+    return STATUS_EC_FORMAT(
+        Expired, PgsqlError(YBPgErrorCode::YB_PG_T_R_DEADLOCK_DETECTED), it->second.first);
   }
 
  private:
@@ -630,9 +680,14 @@ class DeadlockDetector::Impl : public std::enable_shared_from_this<DeadlockDetec
             LOG(ERROR) << "Failed to decode transaction id in detected deadlock!";
           } else {
             const auto& waiter = *waiter_or_status;
+            {
+              UniqueLock<decltype(detector->mutex_)> l(detector->mutex_);
+              auto deadlock_info = std::make_pair(ConstructDeadlockedMessage(waiter, resp),
+                                                  CoarseMonoClock::Now());
+              detector->recently_deadlocked_txns_info_.emplace(waiter, deadlock_info);
+            }
             detector->controller_->Abort(
-                waiter,
-                std::bind(&DeadlockDetector::Impl::TxnAbortCallback, detector, _1, waiter));
+                waiter, std::bind(&DeadlockDetector::Impl::TxnAbortCallback, detector, _1, waiter));
           }
         }
       });
@@ -770,7 +825,7 @@ class DeadlockDetector::Impl : public std::enable_shared_from_this<DeadlockDetec
 
   void TxnAbortCallback(Result<TransactionStatusResult> res, const TransactionId txn_id) {
     if (res.ok()) {
-      if (res->status == TransactionStatus::ABORTED && res->status_time.is_valid()) {
+      if (res->status == TransactionStatus::ABORTED) {
         LOG_WITH_FUNC(INFO) << "Aborting deadlocked transaction " << txn_id << " succeeded.";
         return;
       }
@@ -812,6 +867,9 @@ class DeadlockDetector::Impl : public std::enable_shared_from_this<DeadlockDetec
   Waiters waiters_ GUARDED_BY(mutex_);
 
   std::atomic<uint32_t> seq_no_ = 0;
+
+  std::unordered_map<TransactionId, std::pair<std::string, CoarseTimePoint>, TransactionIdHash>
+      recently_deadlocked_txns_info_ GUARDED_BY(mutex_);
 };
 
 DeadlockDetector::DeadlockDetector(
@@ -839,6 +897,10 @@ void DeadlockDetector::ProcessWaitFor(
 
 void DeadlockDetector::TriggerProbes() {
   return impl_->TriggerProbes();
+}
+
+Status DeadlockDetector::GetTransactionDeadlockStatus(const TransactionId& txn_id) {
+  return impl_->GetTransactionDeadlockStatus(txn_id);
 }
 
 void DeadlockDetector::Shutdown() {

src/yb/tablet/running_transaction.cc
@@ -17,6 +17,7 @@
 
 #include "yb/common/hybrid_time.h"
 #include "yb/common/pgsql_error.h"
+#include "yb/common/wire_protocol.h"
 
 #include "yb/tablet/transaction_participant_context.h"
 
@@ -123,9 +124,11 @@ void RunningTransaction::RequestStatusAt(const StatusRequest& request,
           transaction_status == TransactionStatus::PENDING) {
         local_commit_aborted_subtxn_set = last_known_aborted_subtxn_set_;
       }
+      auto last_known_deadlock_status = last_known_deadlock_status_;
       lock->unlock();
       request.callback(TransactionStatusResult{
-          *transaction_status, last_known_status_hybrid_time, local_commit_aborted_subtxn_set});
+          *transaction_status, last_known_status_hybrid_time, local_commit_aborted_subtxn_set,
+          last_known_deadlock_status});
       return;
     }
   }
@@ -152,7 +155,7 @@ bool RunningTransaction::WasAborted() const {
 
 Status RunningTransaction::CheckAborted() const {
   if (WasAborted()) {
-    return MakeAbortedStatus(id());
+    return last_known_deadlock_status_.ok() ? MakeAbortedStatus(id()) : last_known_deadlock_status_;
   }
   return Status::OK();
 }
@@ -278,7 +281,8 @@ void RunningTransaction::StatusReceived(
 
 bool RunningTransaction::UpdateStatus(
     TransactionStatus transaction_status, HybridTime time_of_status,
-    HybridTime coordinator_safe_time, SubtxnSet aborted_subtxn_set) {
+    HybridTime coordinator_safe_time, SubtxnSet aborted_subtxn_set,
+    const Status& expected_deadlock_status) {
   if (!local_commit_time_ && transaction_status != TransactionStatus::ABORTED) {
     // If we've already committed locally, then last_known_aborted_subtxn_set_ is already set
     // properly. Otherwise, we should update it here.
@@ -303,6 +307,8 @@ bool RunningTransaction::UpdateStatus(
     context_.NotifyAbortedTransactionDecrement(id());
   }
   last_known_status_ = transaction_status;
+  DCHECK(expected_deadlock_status.ok() || transaction_status == TransactionStatus::ABORTED);
+  last_known_deadlock_status_ = expected_deadlock_status;
 
   return transaction_status == TransactionStatus::ABORTED;
 }
@@ -323,6 +329,7 @@ void RunningTransaction::DoStatusReceived(const Status& status,
   decltype(status_waiters_) status_waiters;
   HybridTime time_of_status = HybridTime::kMin;
   TransactionStatus transaction_status = TransactionStatus::PENDING;
+  Status expected_deadlock_status = Status::OK();
   SubtxnSet aborted_subtxn_set;
   const bool ok = status.ok();
   int64_t new_request_id = -1;
@@ -339,11 +346,11 @@ void RunningTransaction::DoStatusReceived(const Status& status,
     }
 
     if (response.status_hybrid_time().size() != 1 ||
-        response.status().size() != 1 ||
+        response.status().size() != 1 || response.deadlock_reason().size() != 1 ||
         (response.aborted_subtxn_set().size() != 0 && response.aborted_subtxn_set().size() != 1)) {
       LOG_WITH_PREFIX(DFATAL)
-          << "Wrong number of status, status hybrid time, or aborted subtxn set entries, "
-          << "exactly one entry expected: "
+          << "Wrong number of status, status hybrid time, deadlock_reason, or aborted subtxn "
+          << "set entries, exactly one entry expected: "
           << response.ShortDebugString();
     } else if (PREDICT_FALSE(response.aborted_subtxn_set().empty())) {
       YB_LOG_EVERY_N(WARNING, 1)
@@ -356,6 +363,10 @@ void RunningTransaction::DoStatusReceived(const Status& status,
         time_of_status = HybridTime(response.status_hybrid_time()[0]);
         transaction_status = response.status(0);
         aborted_subtxn_set = aborted_subtxn_set_or_status.get();
+        if (response.deadlock_reason(0).code() != AppStatusPB::OK) {
+          // response contains a deadlock specific error.
+          expected_deadlock_status = StatusFromPB(response.deadlock_reason(0));
+        }
       } else {
         LOG_WITH_PREFIX(DFATAL)
             << "Could not deserialize SubtxnSet: "
@@ -370,7 +381,8 @@ void RunningTransaction::DoStatusReceived(const Status& status,
     auto coordinator_safe_time = response.coordinator_safe_time().size() == 1
         ? HybridTime::FromPB(response.coordinator_safe_time(0)) : HybridTime();
     auto did_abort_txn = UpdateStatus(
-        transaction_status, time_of_status, coordinator_safe_time, aborted_subtxn_set);
+        transaction_status, time_of_status, coordinator_safe_time, aborted_subtxn_set,
+        expected_deadlock_status);
     if (did_abort_txn) {
       context_.NotifyAbortedTransactionIncrement(id());
       context_.EnqueueRemoveUnlocked(id(), RemoveReason::kStatusReceived, &min_running_notifier);
@@ -410,7 +422,9 @@ void RunningTransaction::DoStatusReceived(const Status& status,
   if (new_request_id >= 0) {
     SendStatusRequest(new_request_id, shared_self);
   }
-  NotifyWaiters(serial_no, time_of_status, transaction_status, aborted_subtxn_set, status_waiters);
+  NotifyWaiters(
+      serial_no, time_of_status, transaction_status, aborted_subtxn_set, status_waiters,
+      expected_deadlock_status);
 }
 
 std::vector<StatusRequest> RunningTransaction::ExtractFinishedStatusWaitersUnlocked(
@@ -441,7 +455,8 @@ std::vector<StatusRequest> RunningTransaction::ExtractFinishedStatusWaitersUnloc
 void RunningTransaction::NotifyWaiters(int64_t serial_no, HybridTime time_of_status,
                                        TransactionStatus transaction_status,
                                        const SubtxnSet& aborted_subtxn_set,
-                                       const std::vector<StatusRequest>& status_waiters) {
+                                       const std::vector<StatusRequest>& status_waiters,
+                                       const Status& expected_deadlock_status) {
   for (const auto& waiter : status_waiters) {
     auto status_for_waiter = GetStatusAt(
         waiter.global_limit_ht, time_of_status, transaction_status, external_transaction());
@@ -452,6 +467,7 @@ void RunningTransaction::NotifyWaiters(int64_t serial_no, HybridTime time_of_sta
           result.status == TransactionStatus::PENDING) {
         result.aborted_subtxn_set = aborted_subtxn_set;
       }
+      result.expected_deadlock_status = expected_deadlock_status;
       waiter.callback(std::move(result));
     } else if (time_of_status >= waiter.read_ht) {
       // It means that between read_ht and global_limit_ht transaction was pending.
@@ -461,7 +477,8 @@ void RunningTransaction::NotifyWaiters(int64_t serial_no, HybridTime time_of_sta
           << "Notify waiter with request id greater than id of status request: "
           << waiter.serial_no << " vs " << serial_no;
       waiter.callback(TransactionStatusResult{
-          TransactionStatus::PENDING, time_of_status, aborted_subtxn_set});
+          TransactionStatus::PENDING, time_of_status, aborted_subtxn_set,
+          expected_deadlock_status});
     } else {
       waiter.callback(STATUS(TryAgain,
           Format("Cannot determine transaction status with read_ht $0, and global_limit_ht $1, "
@@ -507,7 +524,8 @@ void RunningTransaction::AbortReceived(const Status& status,
     if (result.ok() && result->status_time != HybridTime::kMax) {
       auto coordinator_safe_time = HybridTime::FromPB(response.coordinator_safe_time());
       if (UpdateStatus(
-          result->status, result->status_time, coordinator_safe_time, result->aborted_subtxn_set)) {
+          result->status, result->status_time, coordinator_safe_time, result->aborted_subtxn_set,
+          result->expected_deadlock_status)) {
         context_.NotifyAbortedTransactionIncrement(id());
         context_.EnqueueRemoveUnlocked(id(), RemoveReason::kAbortReceived, &min_running_notifier);
       }

commit d38077987f1ca0276bed19a3566fa95d50b092e1

[#18286][#14114][#18435] YSQL: Fix regression caused by error code set in reported deadlocks
Summary:
Commit d6d0cf6ea4553efc3334f8f550aae7164740692e caused a regression as we were setting `YB_PG_T_R_DEADLOCK_DETECTED` error code for deadlocks.

For READ COMMITTED isolation, Pg retries the whole statement with a new read point when it faces a kConflict or kReadRestart error. In case of SERIALIZABLE and REPEATABLE READ isolation levels, Pg retries the whole transaction with a new read point on kConflict or kReadRestart errors, only when  no data has been sent yet to the external client as part of the transaction (i.e., no prior statement has been executed and ysql_output_buffer_size hasn't yet been used by the first statement).

The above commit changed the error code, which led to increased failures in a conflicting workload as transactions weren't being retired. This diff addresses the issue by modifying the retry logic to also check for `kDeadlock` and act accordingly.

- For READ COMMITTED, we have the ability to retry a statement in case of `kConflict` or `kReadRestart`, and we do so indefinitely to honor read committed semantics unless a filled buffer of response data has already been sent to the client for the statement (see `YBIsDataSentForCurrQuery()`). But for deadlock errors, there is no point in retrying the statement because the deadlock cycle might involve granted locks from previous statement(s). But we can still retry if it is the first statement in the transaction and no response data has been sent back to the client (retrying in this case will result in releasing the granted locks as well and hence allows progress). We check for this condition using `YBIsDataSent()`.
- For other isolation levels (SERIALIZABLE & REPEATABLE READ), we retry the transaction for at most `ysql_max_write_restart_attempts` only when no data has been sent so far (i.e., `YBIsDataSent()` is false).

Here's the log when a transaction encounters a deadlock (with the changes)

src/postgres/src/backend/tcop/postgres.c
@@ -3837,14 +3837,14 @@ static void YBPrepareCacheRefreshIfNeeded(ErrorData *edata,
 	*/
 	bool is_read_restart_error = YBCIsRestartReadError(edata->yb_txn_errcode);
 	bool is_conflict_error     = YBCIsTxnConflictError(edata->yb_txn_errcode);
-
+	bool is_deadlock_error	   = YBCIsTxnDeadlockError(edata->yb_txn_errcode);
 	/*
 	 * Note that 'is_dml' could be set for a Select operation on a pg_catalog
 	 * table. Even if it fails due to conflict, a retry is expected to succeed
 	 * without refreshing the cache (as the schema of a PG catalog table cannot
 	 * change).
 	 */
-	if (is_dml && (is_read_restart_error || is_conflict_error))
+	if (is_dml && (is_read_restart_error || is_conflict_error || is_deadlock_error))
 	{
 		return;
 	}
@@ -4163,10 +4163,11 @@ yb_is_restart_possible(const ErrorData* edata,
 			 edata->message, edata->filename, edata->lineno);
 	bool is_read_restart_error = YBCIsRestartReadError(edata->yb_txn_errcode);
 	bool is_conflict_error     = YBCIsTxnConflictError(edata->yb_txn_errcode);
-	if (!is_read_restart_error && !is_conflict_error)
+	bool is_deadlock_error	   = YBCIsTxnDeadlockError(edata->yb_txn_errcode);
+	if (!is_read_restart_error && !is_conflict_error && !is_deadlock_error)
 	{
 		if (yb_debug_log_internal_restarts)
-			elog(LOG, "Restart isn't possible, code %d isn't a read restart/conflict error",
+			elog(LOG, "Restart isn't possible, code %d isn't a read restart/conflict/deadlock error",
 			          edata->yb_txn_errcode);
 		return false;
 	}
@@ -4195,6 +4196,41 @@ yb_is_restart_possible(const ErrorData* edata,
 		if (yb_debug_log_internal_restarts)
 			elog(LOG, "Restart isn't possible, we're out of read restart attempts (%d)",
 			          attempt);
+		*retries_exhausted = true;
+		return false;
+	}
+
+	/*
+	 * For isolation levels other than READ COMMITTED, retries on deadlock are capped at
+	 * ysql_max_write_restart_attempts, given that no data has been sent as part of the transaction.
+	 */
+	if (!IsYBReadCommitted() && is_deadlock_error &&
+		attempt >= *YBCGetGFlags()->ysql_max_write_restart_attempts)
+	{
+		if (yb_debug_log_internal_restarts)
+			elog(LOG, "Restart isn't possible, we're out of read/write restart attempts (%d) on deadlock",
+			          attempt);
+		*retries_exhausted = true;
+		return false;
+	}
+	
+	/*
+	 * In case of READ COMMITTED, retries involve restarting the current statement and not the whole
+	 * transaction.
+	 *
+	 * We can't differentiate if locks acquired by this transaction that are part of the deadlock
+	 * cycle were acquired in a previous statement or the current statement. If acquired in a previous
+	 * statement, restarting the current statement is of no use. If acquired in the current statement,
+	 * a restart could help in resolving the deadlock since the acquired locks would be released in an
+	 * attempt to reacquire.
+	 *
+	 * Hence retries on deadlock in READ COMMITTED isolation are performed indefinitely until statement
+	 * timeout only when no data has been sent as part of the transaction (which also subsumes ensuring
+	 * that this was the first statement in the transaction).
+	 */
+	if (IsYBReadCommitted() && is_deadlock_error && YBIsDataSent()) {
+		if (yb_debug_log_internal_restarts)
+			elog(LOG, "Restart isn't possible, encountered deadlock in READ COMMITTED isolation");
 		*retries_exhausted = true;
 		return false;
 	}
@@ -4586,7 +4622,8 @@ yb_attempt_to_restart_on_error(int attempt,
 			{
 				HandleYBStatus(YBCPgRestartReadPoint());
 			}
-			else if (YBCIsTxnConflictError(edata->yb_txn_errcode))
+			else if (YBCIsTxnConflictError(edata->yb_txn_errcode) ||
+					 YBCIsTxnDeadlockError(edata->yb_txn_errcode))
 			{
 				HandleYBStatus(YBCPgResetTransactionReadPoint());
 				yb_maybe_sleep_on_txn_conflict(attempt);
@@ -4620,7 +4657,8 @@ yb_attempt_to_restart_on_error(int attempt,
 			{
 				YBCRestartTransaction();
 			}
-			else if (YBCIsTxnConflictError(edata->yb_txn_errcode))
+			else if (YBCIsTxnConflictError(edata->yb_txn_errcode) ||
+					 YBCIsTxnDeadlockError(edata->yb_txn_errcode))
 			{
 				/*
 				 * Recreate the YB state for the transaction. This call preserves the

src/postgres/src/backend/utils/misc/pg_yb_utils.c
@@ -558,6 +558,17 @@ GetStatusMsgAndArgumentsByCode(const uint32_t pg_err_code,
 			*msg_args = (const char **) palloc(sizeof(const char *));
 			(*msg_args)[0] = FetchUniqueConstraintName(YBCStatusRelationOid(s));
 			break;
+		case ERRCODE_T_R_DEADLOCK_DETECTED:
+			if (YBCIsTxnDeadlockError(txn_err_code)) {
+				*msg_buf = "deadlock detected";
+				*msg_nargs = 0;
+				*msg_args = NULL;
+
+				*detail_buf = status_msg;
+				*detail_nargs = status_nargs;
+				*detail_args = status_args;
+			}
+			break;
 		default:
 			break;
 	}

src/yb/common/transaction_error.h
@@ -26,7 +26,8 @@ YB_DEFINE_ENUM(TransactionErrorCode,
     (kReadRestartRequired)
     (kConflict)
     (kSnapshotTooOld)
-    (kSkipLocking));
+    (kSkipLocking)
+    (kDeadlock));
 
 struct TransactionErrorTag : IntegralErrorTag<TransactionErrorCode> {
   // It is part of the wire protocol and should not be changed once released.

src/yb/docdb/deadlock_detector.cc
@@ -21,6 +21,7 @@
 
 #include "yb/common/pgsql_error.h"
 #include "yb/common/transaction.h"
+#include "yb/common/transaction_error.h"
 #include "yb/common/wire_protocol.h"
 
 #include "yb/gutil/stl_util.h"
@@ -380,7 +381,7 @@ std::string ConstructDeadlockedMessage(const TransactionId& waiter,
       ss << Format(" -> $0", *id_or_status);
     }
   }
-  ss << Format(" -> $0\n", waiter.ToString());
+  ss << Format(" -> $0 ", waiter.ToString());
   return ss.str();
 }
 
@@ -627,8 +628,10 @@ class DeadlockDetector::Impl : public std::enable_shared_from_this<DeadlockDetec
     if (it == recently_deadlocked_txns_info_.end()) {
       return Status::OK();
     }
+    // Deadlock status is currently used in wait-queues alone. It is required that we set status
+    // type as InternalError for pg to decode the error and throw ERRCODE_T_R_DEADLOCK_DETECTED.
     return STATUS_EC_FORMAT(
-        Expired, PgsqlError(YBPgErrorCode::YB_PG_T_R_DEADLOCK_DETECTED), it->second.first);
+        InternalError, TransactionError(TransactionErrorCode::kDeadlock), it->second.first);
   }
 
  private:

src/yb/tablet/running_transaction.cc
@@ -289,6 +289,11 @@ bool RunningTransaction::UpdateStatus(
   }
   last_known_status_hybrid_time_ = time_of_status;
 
+  // Reset last_known_deadlock_status_ with the latest status. deadlock status is currently
+  // used in wait-queues alone and shouldn't cause any correctness issues.
+  DCHECK(expected_deadlock_status.ok() || transaction_status == TransactionStatus::ABORTED);
+  last_known_deadlock_status_ = expected_deadlock_status;
+
   if (transaction_status == last_known_status_) {
     return false;
   }
@@ -296,8 +301,6 @@ bool RunningTransaction::UpdateStatus(
     context_.NotifyAbortedTransactionDecrement(id());
   }
   last_known_status_ = transaction_status;
-  DCHECK(expected_deadlock_status.ok() || transaction_status == TransactionStatus::ABORTED);
-  last_known_deadlock_status_ = expected_deadlock_status;
 
   return transaction_status == TransactionStatus::ABORTED;
 }

src/yb/yql/pggate/util/ybc_util.cc
@@ -172,6 +172,9 @@ YBPgErrorCode FetchErrorCode(YBCStatus s) {
         case TransactionErrorCode::kSnapshotTooOld:
           result = YBPgErrorCode::YB_PG_SNAPSHOT_TOO_OLD;
           break;
+        case TransactionErrorCode::kDeadlock:
+          result = YBPgErrorCode::YB_PG_T_R_DEADLOCK_DETECTED;
+          break;
         case TransactionErrorCode::kNone: FALLTHROUGH_INTENDED;
         default:
           break;
@@ -274,6 +277,10 @@ bool YBCIsTxnConflictError(uint16_t txn_errcode) {
 
 bool YBCIsTxnSkipLockingError(uint16_t txn_errcode) {
   return txn_errcode == to_underlying(TransactionErrorCode::kSkipLocking);
+}
+
+bool YBCIsTxnDeadlockError(uint16_t txn_errcode) {
+  return txn_errcode == to_underlying(TransactionErrorCode::kDeadlock);
 }
 
 uint16_t YBCGetTxnConflictErrorCode() {

src/yb/yql/pggate/util/ybc_util.h
@@ -148,6 +148,7 @@ bool YBCIsRestartReadError(uint16_t txn_errcode);
 
 bool YBCIsTxnConflictError(uint16_t txn_errcode);
 bool YBCIsTxnSkipLockingError(uint16_t txn_errcode);
+bool YBCIsTxnDeadlockError(uint16_t txn_errcode);
 uint16_t YBCGetTxnConflictErrorCode();
 
 void YBCResolveHostname();

src/yb/yql/pgwrapper/libpq_utils.cc
@@ -680,13 +680,16 @@ std::string PqEscapeIdentifier(const std::string& input) {
 }
 
 bool HasTransactionError(const Status& status) {
+  // TODO: Refactor the function to check for specific error codes instead of checking multiple
+  // errors as few tests that shouldn't encounter a 40P01 would also get through on usage of a
+  // generic check. Refer https://github.com/yugabyte/yugabyte-db/issues/18478 for details.
   static const auto kExpectedErrors = {
-      "could not serialize access due to concurrent update",
-      "Transaction aborted:",
-      "expired or aborted by a conflict:",
-      "Unknown transaction, could be recently aborted:"
+      // ERRCODE_T_R_SERIALIZATION_FAILURE
+      "pgsql error 40001",
+      // ERRCODE_T_R_DEADLOCK_DETECTED
+      "pgsql error 40P01"
   };
-  return HasSubstring(status.message(), kExpectedErrors);
+  return HasSubstring(status.ToString(), kExpectedErrors);
 }
 
 bool IsRetryable(const Status& status) {

java/yb-pgsql/src/test/java/org/yb/pgsql/TestPgTransactions.java
@@ -53,7 +53,12 @@ public class TestPgTransactions extends BasePgSQLTest {
   private static final Logger LOG = LoggerFactory.getLogger(TestPgTransactions.class);
 
   private static boolean isYBTransactionError(PSQLException ex) {
-    return ex.getSQLState().equals("40001");
+    // TODO: Refactor the function to check for specific error codes instead of checking multiple
+    // errors as few tests that shouldn't encounter a 40P01 would also get through on usage of a
+    // generic check. Refer https://github.com/yugabyte/yugabyte-db/issues/18477 for details.
+    //
+    // Return true on exceptions of kind SERIALIZATION_FAILURE or DEADLOCK_DETECTED.
+    return ex.getSQLState().equals("40001") || ex.getSQLState().equals("40P01");
   }
 
   private static boolean isTransactionAbortedError(PSQLException ex) {

src/yb/yql/pgwrapper/pg_libpq-test.cc
@@ -940,6 +940,10 @@ TEST_F(PgLibPqTest, TestConcurrentCounterRepeatableRead) {
   TestConcurrentCounter(IsolationLevel::SNAPSHOT_ISOLATION);
 }
 
+TEST_F(PgLibPqTest, TestConcurrentCounterReadCommitted) {
+  TestConcurrentCounter(IsolationLevel::READ_COMMITTED);
+}
+
 TEST_F(PgLibPqTest, SecondaryIndexInsertSelect) {
   constexpr int kThreads = 4;
 

src/yb/yql/pgwrapper/pg_wait_on_conflict-test.cc
@@ -94,6 +94,10 @@ class PgWaitQueuesTest : public PgMiniTestBase {
   }
 
   void TestDeadlockWithWrites() const;
+
+  virtual IsolationLevel GetIsolationLevel() const {
+    return SNAPSHOT_ISOLATION;
+  }
 };
 
 auto GetBlockerIdx(auto idx, auto cycle_length) {
@@ -188,7 +192,7 @@ void PgWaitQueuesTest::TestDeadlockWithWrites() const {
     thread_holder.AddThreadFunctor(
         [this, i, &first_update, &done, &succeeded_second_update, &succeeded_commit] {
       auto conn = ASSERT_RESULT(Connect());
-      ASSERT_OK(conn.StartTransaction(IsolationLevel::SNAPSHOT_ISOLATION));
+      ASSERT_OK(conn.StartTransaction(GetIsolationLevel()));
 
       ASSERT_OK(conn.ExecuteFormat("UPDATE foo SET v=$0 WHERE k=$0", i));
       first_update.CountDown();
@@ -1027,6 +1031,17 @@ TEST_F(PgWaitQueuesTest, YB_DISABLE_TEST_IN_TSAN(ParallelUpdatesDetectDeadlock))
       did_deadlock.CountDown();
     }
   }
+}
+
+class PgWaitQueuesReadCommittedTest : public PgWaitQueuesTest {
+ protected:
+  IsolationLevel GetIsolationLevel() const override {
+    return IsolationLevel::READ_COMMITTED;
+  }
+};
+
+TEST_F(PgWaitQueuesReadCommittedTest, TestDeadlockSimple) {
+  TestDeadlockWithWrites();
 }
 
 class PgWaitQueuePackedRowTest : public PgWaitQueuesTest {