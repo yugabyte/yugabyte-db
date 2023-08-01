9840b59420304818cb8fc1ce0f1d5f68a422812f
Anubhav Srivastava <asrivastava@yugabyte.com>
2023-08-01 14:54:58 -0400

    [BACKPORT 2.16][#18164] docdb: Ignore deleted tablets in leaderless endpoint
    Summary:
    Original commit: f6e7c62bedeb85eff48ee92f4a308d8dc1df6c91 / D26896
    The leaderless tablet endpoint currently uses IncludeInactive::kTrue when getting the tablets for a table, but this includes REPLACED and DELETED, when it should not.
    Jira: DB-7199
    
    Test Plan: `ybd --cxx-test integration-tests_master_path_handlers-itest --gtest_filter MasterPathHandlersItest.TestLeaderlessDeletedTablet -n 100`
    
    Reviewers: qhu
    
    Reviewed By: qhu
    
    Subscribers: ybase, bogdan
    
    Differential Revision: https://phorge.dev.yugabyte.com/D27457

--- src/yb/master/master-path-handlers.cc
+++ src/yb/master/master-path-handlers.cc
@@ -1418,6 +1418,9 @@ std::vector<TabletInfoPtr> MasterPathHandlers::GetLeaderlessTablets() {
   auto nonsystem_tablets = GetNonSystemTablets();
 
   for (TabletInfoPtr t : nonsystem_tablets) {
+    if (t.get()->LockForRead()->is_deleted()) {
+      continue;
+    }
     auto rm = t.get()->GetReplicaLocations();
 
     auto has_leader = std::any_of(
