fab4c85c8c1114bcfd068bf1cee6fb282aa8f4fa
kkannan <kkannan@yugabyte.com>
2023-08-01 10:32:44 +0530

    [PLAT-9058]Create Role UI
    Summary:
    This diff implements following tickets
    
    [[ https://yugabyte.atlassian.net/browse/PLAT-9058 | PLAT-9058 ]] - Create Role UI
    [[ https://yugabyte.atlassian.net/browse/PLAT-9060 | PLAT-9060 ]] - Edit Role UI
    [[ https://yugabyte.atlassian.net/browse/PLAT-9063 | PLAT-9063 ]] - View Role UI
    [[ https://yugabyte.atlassian.net/browse/PLAT-9064 | PLAT-9064 ]] - Delete Role UI
    
    [[ https://www.figma.com/file/mYvL4D0dYovNUgAjioup5z/YBA-RBAC?node-id=1455%3A77497&mode=dev | Figma ]]
    
    Test Plan:
    Tested manually
    
    {F93098}
    {F93099}
    {F93102}
    {F93103}
    {F93106}
    
    Reviewers: asathyan, lsangappa, jmak, rmadhavan
    
    Reviewed By: jmak
    
    Subscribers: ui, yugaware
    
    Differential Revision: https://phorge.dev.yugabyte.com/D27232

---
+++ managed/ui/src/redesign/features/rbac/permission/IPermission.ts
@@ -0,0 +1,30 @@
+/*
+ * Created on Wed Jul 12 2023
+ *
+ * Copyright 2021 YugaByte, Inc. and Contributors
+ * Licensed under the Polyform Free Trial License 1.0.0 (the "License")
+ * You may not use this file except in compliance with the License. You may obtain a copy of the License at
+ * http://github.com/YugaByte/yugabyte-db/blob/master/licenses/POLYFORM-FREE-TRIAL-LICENSE-1.0.0.txt
+ */
+
+export const Action = {
+  CREATE: 'CREATE',
+  READ: 'READ',
+  UPDATE: 'UPDATE',
+  DELETE: 'DELETE'
+} as const;
+
+export const Resource = {
+  UNIVERSE: 'UNIVERSE',
+  DEFAULT: 'DEFAULT'
+} as const;
+
+export type ResourceType = typeof Resource[keyof typeof Resource];
+export type ActionType = typeof Action[keyof typeof Action];
+
+export interface Permission {
+  description: string;
+  permission: ActionType;
+  resource_type: ResourceType;
+  prerequisite_permissions: Pick<Permission, 'permission' | 'resource_type'>[];
+}
