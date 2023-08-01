498c22dc35d12b550cea7283be6867ee0e3957e6
Chinmay Vidyadhar Chiplunkar <chinmayvc@gmail.com>
2023-08-01 22:57:27 +0000

    [#18672] DocDB: Support for table level locks in DocDB
    Summary:
    This fix introduces support for Postgres table level locks in docdb.
    
    Three gflags(tagged as hidden and experimental and OFF by default) can be used to turn ON the acquisition of table locks:
    ysql_enable_table_level_locks_for_dml_reads - When ON, table locks are acquired for serializable reads and row mark reads.
    ysql_enable_table_level_locks_for_dml_writes - When ON, table locks are acquired for all DML writes.
    ysql_use_ddl_table_lock_on_alter_table - When ON, table locks are acquired for Alter Table DDLs
    
    The above three gflags should be combined into a single autoflag in the future.
    
    In order to support pg-like table level locks in YSQL, two new KeyEntryType values are introduced:
    KeyEntryType::kWeakTableLock
    KeyEntryType::kStrongTableLock
    
    DocDB writes table locks to intents db in the following format:
    |kTableId <table_id> (if colocated)|, kWeakTableLock, <IntentTypeSet>, <HT> -> <txn_id>
    |kTableId <table_id> (if colocated)|, kStrongTableLock, <IntentTypeSet>, <HT> -> <txn_id>
    
    GetTableLockIntents in docdb.cc captures the mapping between pg table locks to DocDB intents.
    
    Functional spec link - https://docs.google.com/document/d/1UCO86Fd3rl5lXZALZ5HZt7hiz0vzexlGKyyQ5kMVJXs/edit?usp=sharing
    
    WIP design doc link - https://docs.google.com/document/d/1tumDacBiDCSrP6RZTrcZ6xlmWGINwe0bZfucYB_PoTo/edit
    
    A new unit test TabletPeerTest.TestTableLevelLocks is added to validate the lock conflict matrix.
    
    The behavior of table locks was manually verified end-to-end and the steps for reproducing the same can be found in the design doc.
    Jira: DB-7591
    
    Test Plan:
    TabletPeerTest.TestTableLevelLocks
    
    Jenkins: urgent
    
    Reviewers: rsami, rthallam, pjain
    
    Reviewed By: rsami, rthallam
    
    Subscribers: myang, bogdan, yql, qhu, ybase
    
    Differential Revision: https://phorge.dev.yugabyte.com/D26560

--- src/yb/dockv/primitive_value.cc
+++ src/yb/dockv/primitive_value.cc
@@ -351,6 +351,8 @@ void KeyEntryValue::AppendToKey(KeyBytes* key_bytes) const {
     CASE_EMPTY_KEY_ENTRY_TYPES
       return;
 
+    case KeyEntryType::kWeakTableLock: FALLTHROUGH_INTENDED;
+    case KeyEntryType::kStrongTableLock: FALLTHROUGH_INTENDED;
     case KeyEntryType::kCollString: FALLTHROUGH_INTENDED;
     case KeyEntryType::kString:
       key_bytes->AppendString(str_val_);
@@ -819,6 +821,8 @@ Status KeyEntryValue::DecodeKey(Slice* slice, KeyEntryValue* out) {
     }
 
     case KeyEntryType::kCollString: FALLTHROUGH_INTENDED;
+    case KeyEntryType::kWeakTableLock: FALLTHROUGH_INTENDED;
+    case KeyEntryType::kStrongTableLock: FALLTHROUGH_INTENDED;
     case KeyEntryType::kString: {
       if (out) {
         string result;
@@ -2623,6 +2627,10 @@ KeyEntryValue KeyEntryValue::FromQLVirtualValue(QLVirtualValuePB value) {
 
 std::string KeyEntryValue::ToString(AutoDecodeKeys auto_decode_keys) const {
   switch (type_) {
+    case KeyEntryType::kWeakTableLock:
+      return "WeakTableLock";
+    case KeyEntryType::kStrongTableLock:
+      return "StrongTableLock";
     case KeyEntryType::kNullHigh: FALLTHROUGH_INTENDED;
     case KeyEntryType::kNullLow:
       return "null";
@@ -2762,6 +2770,8 @@ int KeyEntryValue::CompareTo(const KeyEntryValue& other) const {
     case KeyEntryType::kDecimalDescending: FALLTHROUGH_INTENDED;
     case KeyEntryType::kVarIntDescending:
       return other.str_val_.compare(str_val_);
+    case KeyEntryType::kWeakTableLock: FALLTHROUGH_INTENDED;
+    case KeyEntryType::kStrongTableLock: FALLTHROUGH_INTENDED;
     case KeyEntryType::kCollString: FALLTHROUGH_INTENDED;
     case KeyEntryType::kString: FALLTHROUGH_INTENDED;
     case KeyEntryType::kDecimal: FALLTHROUGH_INTENDED;
@@ -2976,6 +2986,8 @@ bool operator==(const KeyEntryValue& lhs, const KeyEntryValue& rhs) {
     CASE_EMPTY_KEY_ENTRY_TYPES
         return true;
 
+    case KeyEntryType::kWeakTableLock: FALLTHROUGH_INTENDED;
+    case KeyEntryType::kStrongTableLock: FALLTHROUGH_INTENDED;
     case KeyEntryType::kCollStringDescending: FALLTHROUGH_INTENDED;
     case KeyEntryType::kCollString: FALLTHROUGH_INTENDED;
     case KeyEntryType::kStringDescending: FALLTHROUGH_INTENDED;
