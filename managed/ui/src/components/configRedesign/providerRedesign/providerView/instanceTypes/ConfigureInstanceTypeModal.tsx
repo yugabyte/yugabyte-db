9bfc01749d6d8d5a7103ff3babc5ffb738e2a89d
Jethro Mak <88681329+Jethro-M@users.noreply.github.com>
2023-08-01 19:43:37 -0400

    [PLAT-9903] Update on-prem instance management text for node agent
    Summary:
    **Context**
    The help text and labels on the on-prem instance management page need to be updated for consistency and
    node agent support.
    
    **Change**
    This diff adds node agent specific flags to th on-prem node pre-provision script.
    This diff also adds a copy button to the pre-prevision script.
    This diff also updates some of our old labels which aren't consistent.
    
    Test Plan:
    Verify text change is as expected with relevant stakeholders.
    Label Updates
    {F95851}
    {F95852}
    {F95805}
    
    Provider with node agent enabled
    {F95848}
    
    Provider without node agent enabled
    {F95831}
    
    Reviewers: rmadhavan, nsingh, ghodge, yng
    
    Reviewed By: rmadhavan, ghodge
    
    Subscribers: yugaware
    
    Differential Revision: https://phorge.dev.yugabyte.com/D27464

--- managed/ui/src/components/configRedesign/providerRedesign/providerView/instanceTypes/ConfigureInstanceTypeModal.tsx
+++ managed/ui/src/components/configRedesign/providerRedesign/providerView/instanceTypes/ConfigureInstanceTypeModal.tsx
@@ -63,11 +63,11 @@ export const ConfigureInstanceTypeModal = ({
         {...modalProps}
       >
         <div className={classes.formField}>
-          <Typography variant="body2">Machine Type</Typography>
+          <Typography variant="body2">Name</Typography>
           <YBInputField
             control={formMethods.control}
             name="instanceTypeCode"
-            rules={{ required: 'Machine type is required.' }}
+            rules={{ required: 'Instance type name is required.' }}
             fullWidth
           />
         </div>
