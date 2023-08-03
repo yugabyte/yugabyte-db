280ea93e5629c8c86f142544c5c2fa96a4d8fa26
Abhinab Saha <asaha@yugabyte.com>
2023-08-03 19:45:35 +0530

    fix client node ip

--- src/yb/yql/cql/cqlserver/cql_service.cc
+++ src/yb/yql/cql/cqlserver/cql_service.cc
@@ -211,7 +211,7 @@ void CQLServiceImpl::Handle(yb::rpc::InboundCallPtr inbound_call) {
     return;
   }
   (**processor).auh_metadata().top_level_request_id = {util::AUHRandom::GenerateRandom64(), util::AUHRandom::GenerateRandom64()};
-  (**processor).auh_metadata().top_level_node_id = yb::ToString(inbound_call->remote_address());
+  (**processor).auh_metadata().client_node_ip = yb::ToString(inbound_call->remote_address());
   MonoTime got_processor = MonoTime::Now();
   cql_metrics_->time_to_get_cql_processor_->Increment(
       got_processor.GetDeltaSince(start).ToMicroseconds());
