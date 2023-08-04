1557f455b9994e99b58c0a1dc2a81b870ae360dd
Gaurav Kukreja <gaurav@gauravk.in>
2023-08-04 18:51:21 +0200

    [#18513] YSQL: Fix estimated seeks and nexts in base table for Index Scan
    Summary:
    The base table lookup in index scan in a colocated database is not batched. In
    other words, for each key we first lookup the secondary index and then find the
    matching base table row, before looking up the next key in the index. This means
    that each lookup in the base table results in a seek operation in DocDB.
    
    In the previous logic, we assumed that lookups in the base table were batched
    and performed in the order in which the keys were found in the secondary index.
    We assumed then that if there is high correlation in the ordering of the index
    and the base table, then some of the base table accesses would be performed
    using cheaper nexts. This was a misunderstanding however, especially in the case
    of colocated database. This may be optimized in future at which point, the logic
    that was removed in this commit may become relevant again.
    
    The base table access in the case of distributed tables is actually batched and
    this logic may be relevant for that scenario but we are currently focussing on
    the colocated database scenario.
    Jira: DB-7471
    
    Test Plan: ybd --java-test 'org.yb.pgsql.TestPgRegressPlanner'
    
    Reviewers: tverona, mihnea, mtakahara, kpopali, yguan
    
    Reviewed By: yguan
    
    Subscribers: yql
    
    Differential Revision: https://phorge.dev.yugabyte.com/D27564

--- src/postgres/src/backend/optimizer/plan/createplan.c
+++ src/postgres/src/backend/optimizer/plan/createplan.c
@@ -207,7 +207,8 @@ static IndexOnlyScan *make_indexonlyscan(List *qptlist, List *qpqual,
 				   Index scanrelid, Oid indexid,
 				   List *indexqual, List *indexorderby,
 				   List *indextlist,
-				   ScanDirection indexscandir);
+				   ScanDirection indexscandir, double estimated_num_nexts,
+				   double estimated_num_seeks);
 static BitmapIndexScan *make_bitmap_indexscan(Index scanrelid, Oid indexid,
 					  List *indexqual,
 					  List *indexqualorig);
@@ -3816,7 +3817,9 @@ create_indexscan_plan(PlannerInfo *root,
 												fixed_indexquals,
 												fixed_indexorderbys,
 												best_path->indexinfo->indextlist,
-												best_path->indexscandir);
+												best_path->indexscandir,
+												best_path->estimated_num_nexts,
+												best_path->estimated_num_seeks);
 		index_only_scan_plan->yb_indexqual_for_recheck =
 			YbBuildIndexqualForRecheck(fixed_indexquals, best_path->indexinfo);
 		index_only_scan_plan->yb_distinct_prefixlen =
@@ -6461,7 +6464,9 @@ make_indexonlyscan(List *qptlist,
 				   List *indexqual,
 				   List *indexorderby,
 				   List *indextlist,
-				   ScanDirection indexscandir)
+				   ScanDirection indexscandir,
+				   double estimated_num_nexts,
+				   double estimated_num_seeks)
 {
 	IndexOnlyScan *node = makeNode(IndexOnlyScan);
 	Plan	   *plan = &node->scan.plan;
@@ -6478,6 +6483,8 @@ make_indexonlyscan(List *qptlist,
 	node->indexorderdir = indexscandir;
 	node->yb_pushdown.colrefs = yb_pushdown_colrefs;
 	node->yb_pushdown.quals = yb_pushdown_quals;
+	node->estimated_num_nexts = estimated_num_nexts;
+	node->estimated_num_seeks = estimated_num_seeks;
 
 	return node;
 }
