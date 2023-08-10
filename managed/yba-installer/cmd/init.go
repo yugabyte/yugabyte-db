cb231c19158e532a62b4fadceb2745d4cdbb3f50
Daniel Shubin <dshubin@yugabyte.com>
2023-08-10 20:23:23 +0000

    [PLAT-9129][PLAT-8173] Update PG version and java JRE
    Summary:
    Updating to a more recent postgres version 14.8
    yba-installer now uses a JRE instead of the JDK
    
    Test Plan:
    1. builds succeed (new pg version + jre)
    2. Install succeeds
    3. PG upgrade succeeds
    4. java switches from jdk to jre on upgrade
    
    Reviewers: muthu, sanketh
    
    Reviewed By: muthu, sanketh
    
    Subscribers: sanketh, muthu, yugaware
    
    Differential Revision: https://phorge.dev.yugabyte.com/D27760

--- managed/yba-installer/cmd/init.go
+++ managed/yba-installer/cmd/init.go
@@ -89,7 +89,7 @@ func initServices() {
 	services = make(map[string]common.Component)
 	installPostgres := viper.GetBool("postgres.install.enabled")
 	installYbdb := viper.GetBool("ybdb.install.enabled")
-	services[PostgresServiceName] = NewPostgres("10.23")
+	services[PostgresServiceName] = NewPostgres("14.8")
 	// services[YbdbServiceName] = NewYbdb("2.17.2.0")
 	services[PrometheusServiceName] = NewPrometheus("2.44.0")
 	services[YbPlatformServiceName] = NewPlatform(common.GetVersion())
