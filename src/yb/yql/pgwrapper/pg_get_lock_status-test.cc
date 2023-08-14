08f1f1a1ee5b566658e09afe43eb9d35d7f3c7b9
Basava <basava726.k@gmail.com>
2023-08-14 17:25:55 +0000

    [#18429] DocDB: Annotate expected race within pg_locks_test_base library
    Summary:
    Annotate `ExpectBlockedAsync` function to ignore racey reads. This is a helper func to execute blocking statements in an async manner and return only after we ensure that the connection is busy. Ignoring the race doesn't affect correctness.
    
    The long term fix for this issue will be tracked as part of  https://github.com/yugabyte/yugabyte-db/issues/17295
    Jira: DB-7416
    
    Test Plan:
    ./yb_build.sh tsan  --cxx-test pgwrapper_pg_get_lock_status-test --gtest_filter PgGetLockStatusTest.TestWaiterLockContainingColumnId
    ./yb_build.sh tsan --cxx-test pgwrapper_pg_get_lock_status-test --gtest_filter PgGetLockStatusTest.TestLocksFromWaitQueue
    
    Reviewers: rsami
    
    Reviewed By: rsami
    
    Subscribers: yql, ybase
    
    Differential Revision: https://phorge.dev.yugabyte.com/D27595

--- src/yb/yql/pgwrapper/pg_get_lock_status-test.cc
+++ src/yb/yql/pgwrapper/pg_get_lock_status-test.cc
@@ -169,7 +169,7 @@ TEST_F(PgGetLockStatusTest, TestGetLockStatusWithCustomTransactionsList) {
   ASSERT_EQ(num_txns, 1);
 }
 
-TEST_F(PgGetLockStatusTest, YB_DISABLE_TEST_IN_TSAN(TestLocksFromWaitQueue)) {
+TEST_F(PgGetLockStatusTest, TestLocksFromWaitQueue) {
   const auto table = "foo";
   const auto key = "1";
   auto session = ASSERT_RESULT(Init(table, key));
@@ -181,15 +181,12 @@ TEST_F(PgGetLockStatusTest, YB_DISABLE_TEST_IN_TSAN(TestLocksFromWaitQueue)) {
   auto status_future = ASSERT_RESULT(
       ExpectBlockedAsync(&conn, Format("UPDATE $0 SET v=v+10 WHERE k=$1", table, key)));
 
-  // Assert that locks corresponding to the waiter txn as well are returned in
-  // GetLockStatusResponsePB.
+  // Assert that locks corresponding to the waiter txn as well are returned in pg_locks;
   SleepFor(MonoDelta::FromSeconds(2 * kTimeMultiplier));
-  const auto& tablet_id = session.first_involved_tablet;
-  auto resp = ASSERT_RESULT(GetLockStatus(tablet_id));
-  auto num_txns = ASSERT_RESULT(GetNumTxnsInLockStatusResponse(resp));
+  auto num_txns = ASSERT_RESULT(session.conn->FetchValue<int64>(
+      "SELECT COUNT(DISTINCT(ybdetails->>'transactionid')) FROM pg_locks"));
   ASSERT_EQ(num_txns, 2);
 
-  ASSERT_TRUE(conn.IsBusy());
   ASSERT_OK(session.conn->Execute("COMMIT"));
   ASSERT_OK(status_future.get());
 }
@@ -341,23 +338,13 @@ TEST_F(PgGetLockStatusTest, TestWaiterLockContainingColumnId) {
       ExpectBlockedAsync(&conn, Format("UPDATE $0 SET v=1 WHERE k=$1", table, key)));
 
   SleepFor(2s * kTimeMultiplier);
-  // Workaround to get the other transaction id, currently can't get it through a pg command.
-  auto tserver_lock_status_resp = ASSERT_RESULT(GetLockStatus(session.first_involved_tablet));
-  auto txns_set = ASSERT_RESULT(GetTxnsInLockStatusResponse(tserver_lock_status_resp));
-  txns_set.erase(session.txn_id);
-  ASSERT_EQ(txns_set.size(), 1);
-  auto other_txn = *txns_set.begin();
-  ASSERT_NE(other_txn, session.txn_id);
-
   auto res = ASSERT_RESULT(session.conn->FetchValue<int64_t>(
-    Format("SELECT COUNT(*) FROM pg_locks WHERE ybdetails->>'transactionid' = '$0'",
-    other_txn.ToString())));
+      "SELECT COUNT(*) FROM pg_locks WHERE NOT granted"));
   // The waiter acquires 3 locks in total,
   // 1 {STRONG_READ,STRONG_WRITE} on the column
   // 1 {WEAK_READ,WEAK_WRITE} on the row
   // 1 {WEAK_READ,WEAK_WRITE} on the table
   ASSERT_EQ(res, 3);
-  ASSERT_TRUE(conn.IsBusy());
   ASSERT_OK(session.conn->Execute("COMMIT"));
 }
 
