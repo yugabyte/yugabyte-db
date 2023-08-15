961cbfd80b498ff05b875d604c3090cc7f9c2b8e
Piyush Jain <piyushjain1996@gmail.com>
2023-08-15 18:31:35 +0000

    [#18281] YSQL: Fix query layer's read restart retries in Read Committed isolation
    Summary:
    Currently in read committed isolation, when the query layer faces a kReadRestart
    error, it is supposed to retry the statement with the "restart read" timestamp
    provided by the tserver. Once restarted with the "restart read" timestamp, the
    chances of facing another kReadRestart reduce drastically. Also, the chances keep
    reducing further with more retries using newer "restart read" timestamps until it
    reaches 0 (it would surely be 0 after max_clock_skew time has elapsed i.e., when all
    nodes will have a hybrid time >= global_limit of the read point). In practice, it would
    be very hard to see >= 2 retries with a kReadRestart error.
    
    However, the retry logic for kReadRestart is broken: the retry ends up resetting
    the read time to the current hybrid time instead of using the "restart read"
    timestamp. Resetting the read time is as good as executing a fresh statement
    which has the same chances of facing a kReadRestart error, and thus this cycle
    of kReadRestart error and incorrect retries can go on indefiniteiy.
    
    The cause of the broken retry is: as part of a retry the query layer sets the
    ReadTimeManipulation to RESTART for the next set of Perform rpcs (see
    YBCPgRestartReadPoint() in yb_attempt_to_restart_on_error()). However, it is
    overwritten to RESET via YBCPgResetTransactionReadPoint() called in
    GetTransactionSnapshot(), which happens for each new statement execution in a
    read committed isolation transaction.
    Jira: DB-7277
    
    Test Plan:
    Jenkins: urgent
    
    Tried adding a reliable unit test that fails without this fix, but couldn't construct one after spending ~2 days.
    
    Also, itest covers it, so this diff doesn't add a unit test. Created a gh issue to add a unit test later (#18824).
    
    To reproduce manually, run the below insert and update workloads simultaneously using ysql_bench:
    
    create table test (k serial primary key, v int);
    
    ```
    > cat insert.sql
    insert into test (v) values (1);
    ```
    
    ```
    > cat update.sql
    begin;
    update test set v=2 where k>1;
    commit;
    ```
    
    ./build/latest/postgres/bin/ysql_bench --jobs=2 --client=2 --file=insert.sql --progress=10 --time=60
    ./build/latest/postgres/bin/ysql_bench --jobs=1 --client=1 --file=update.sql --progress=10 --time=60
    
    Reviewers: tvesely, bkolagani, tfoucher, rsami
    
    Reviewed By: tfoucher, rsami
    
    Subscribers: rsami, yql
    
    Differential Revision: https://phorge.dev.yugabyte.com/D27817

--- src/yb/yql/pggate/ybc_pggate.h
+++ src/yb/yql/pggate/ybc_pggate.h
@@ -557,6 +557,7 @@ YBCStatus YBCPgRecreateTransaction();
 YBCStatus YBCPgRestartTransaction();
 YBCStatus YBCPgResetTransactionReadPoint();
 YBCStatus YBCPgRestartReadPoint();
+bool YBCIsRestartReadPointRequested();
 YBCStatus YBCPgCommitTransaction();
 YBCStatus YBCPgAbortTransaction();
 YBCStatus YBCPgSetTransactionIsolationLevel(int isolation);
