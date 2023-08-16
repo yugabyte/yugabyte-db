a471ddb9fe4b6fd59ac999f61600e624385ca9b0
kkannan <kkannan@yugabyte.com>
2023-08-16 10:26:29 +0530

    [PLAT-10036][Master]Enable enableNewRestoreModal feature flag by default for selective restore
    Summary: Enabled new restore modal by default
    
    Test Plan: tested manually
    
    Reviewers: asathyan, lsangappa, jmak, rmadhavan
    
    Reviewed By: lsangappa
    
    Subscribers: ui, yugaware
    
    Differential Revision: https://phorge.dev.yugabyte.com/D27828

--- managed/ui/src/reducers/feature.js
+++ managed/ui/src/reducers/feature.js
@@ -33,7 +33,7 @@ const initialStateFeatureInTest = {
   enableS3BackupProxy: false,
   enableRRGflags: true,
   enableLDAPRoleMapping: true,
-  enableNewRestoreModal: false,
+  enableNewRestoreModal: true,
   enableConfigureDBApi: false,
   enableRBAC: false
 };
