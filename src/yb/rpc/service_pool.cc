352efdeb67192b02d9c81dd0aff8f307ba7dcab4
Abhinab Saha <asaha@yugabyte.com>
2023-08-23 09:39:18 +0530

    pull changes

--- src/yb/rpc/service_pool.cc
+++ src/yb/rpc/service_pool.cc
@@ -263,9 +263,6 @@ class ServicePoolImpl final : public InboundCallHandler {
         TRACE_TO(incoming->trace(), "Handling call $0", AsString(incoming->method_name()));
         service_->Handle(std::move(incoming));
       }
-      // if (service_->service_name() != "yb.cqlserver.CQLServerService") {
-      //   SET_WAIT_STATUS(util::WaitStateCode::HandlingDone);
-      // }
       SET_WAIT_STATUS_IF_AT(util::WaitStateCode::Handling, util::WaitStateCode::HandlingDone);
       return;
     }
