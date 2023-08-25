92f972f96c207e99cf6ad500cb8fa208a8b84c00
Rafsun Masud <rafsun82@gmail.com>
2023-08-25 12:46:09 -0700

    Removed unnecessary assignment (#1190)
    Original work was done by Panagiotis Foliadis in PR #1035.
    
    Co-authored-by: Panagiotis Foliadis <pfoliadis@hotmail.com>

--- src/backend/parser/cypher_clause.c
+++ src/backend/parser/cypher_clause.c
@@ -4389,10 +4389,6 @@ static Node *make_qual(cypher_parsestate *cpstate,
         char *entity_name;
         ColumnRef *cr = makeNode(ColumnRef);
 
-        // cast graphid to agtype
-        qualified_name = list_make2(makeString("ag_catalog"),
-                                    makeString("graphid_to_agtype"));
-
         if (entity->type == ENT_EDGE)
         {
             entity_name = entity->entity.node->name;
