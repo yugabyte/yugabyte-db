8716b758ab773ba177814a3175a5c223c935c373
Shubham <svarshney@yugabyte.com>
2023-08-27 13:36:44 +0000

    [PLAT-10079] Bump up postgres version
    Summary: Bumps up postgres version from 14.8 -> 14.9
    
    Test Plan:
    Created a replicated deployment with postgres 14.8
    Deployed the upgraded YBA with postgres 14.9
    Ensured that the pg 14.9 is up with data intact
    
    Tested the above flow for YBA installer as well.
    
    Reviewers: anijhawan, sneelakantan, amalyshev, sanketh
    
    Reviewed By: amalyshev
    
    Subscribers: yugaware
    
    Differential Revision: https://phorge.dev.yugabyte.com/D27952

--- managed/yba-installer/cmd/init.go
+++ managed/yba-installer/cmd/init.go
@@ -89,7 +89,7 @@ func initServices() {
 	services = make(map[string]common.Component)
 	installPostgres := viper.GetBool("postgres.install.enabled")
 	installYbdb := viper.GetBool("ybdb.install.enabled")
-	services[PostgresServiceName] = NewPostgres("14.8")
+	services[PostgresServiceName] = NewPostgres("14.9")
 	// services[YbdbServiceName] = NewYbdb("2.17.2.0")
 	services[PrometheusServiceName] = NewPrometheus("2.46.0")
 	services[YbPlatformServiceName] = NewPlatform(common.GetVersion())
