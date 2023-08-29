yugabyte/yugabyte-db

commit 98586ef82bb2530345cd2711e6fd24b32dca9270

[#18744] DocDB: Add ability to recover from follower lag caused by stuck OutboundCall
Summary:
In production, we saw a few cases where follower lag was continuously increasing for few tablets. Also we noticed that the replica which was lagging was not removed from quorum by ConsensusQueue either. After capturing a dump of a node where leader was hosted, we were able to figure out that the performing_update_mutex was held for a long time for the affected Peer.

Peer acquires the performing_update_mutex when it is building a request to send to peer and keeps the mutex locked until it receives a response. In the captured dump, we noticed that the OutboundCall was in SENT state, but we were not able to confirm if the connection (on which it was sent) was active or not -- we did an analysis of OutboundCall references and we believe that the connection had been shut down, but we were not sure.

This change tries to detect the stuck OutboundCall in Peer:
* Whenever we try to send more data or heartbeat to the peer (in `SignalRequest`), we check if we can acquire the `performing_update_mutex`. If the mutex is already held, then we try to see if it has been more than the time specified by `FLAGS_stuck_peer_call_threshold_ms` + request timeout since the call start time.
* If the lock is held for more than timeout + `FLAGS_stuck_peer_call_threshold_ms` time duration, then we log additional details which can help identify the root cause of the issue (see below for examples).
* And when `FLAGS_force_recover_from_stuck_peer_call` is set to true, we try to mark the stuck call as failed. If the call object is not present, then we won't be able to recover from this situation.

src/yb/consensus/consensus_peers.cc
@@ -97,6 +97,15 @@ DEFINE_test_flag(int32, delay_removing_peer_with_failed_tablet_secs, 0,
                  "indicating that a tablet is in the FAILED state, and before marking this peer "
                  "as failed.");
 
+DEFINE_RUNTIME_int32(consensus_stuck_peer_call_threshold_ms, 10000,
+    "Time to wait after timeout before considering a RPC call as stuck.");
+TAG_FLAG(consensus_stuck_peer_call_threshold_ms, advanced);
+
+DEFINE_RUNTIME_bool(consensus_force_recover_from_stuck_peer_call, false,
+    "Set this flag to true in addition to consensus_stuck_peer_call_threshold_ms to automatically "
+    "recover from stuck RPC call.");
+TAG_FLAG(consensus_force_recover_from_stuck_peer_call, advanced);
+
 // Allow for disabling remote bootstrap in unit tests where we want to test
 // certain scenarios without triggering bootstrap of a remote peer.
 DEFINE_test_flag(bool, enable_remote_bootstrap, true,
@@ -158,6 +167,30 @@ Status Peer::SignalRequest(RequestTriggerMode trigger_mode) {
   // If there are new requests in the queue we'll get them on ProcessResponse().
   auto performing_update_lock = LockPerformingUpdate(std::try_to_lock);
   if (!performing_update_lock.owns_lock()) {
+    // In production, we have seen some cases where OutboundCall is stuck in SENT state and there is
+    // no callback invoked for the call. When FLAGS_consensus_stuck_peer_call_threshold_ms config is
+    // set, we try to detect this situation. And if
+    // FLAGS_consensus_force_recover_from_stuck_peer_call config is set to true, then we will mark
+    // the outstanding call as failed. Note: if there is no outstanding call in controller or
+    // timeout is not initialized, then we won't be able to recover it (we didn't see this situation
+    // in production).
+    auto timeout = controller_.timeout();
+    if (FLAGS_consensus_stuck_peer_call_threshold_ms > 0 && timeout.Initialized()) {
+      const MonoDelta stuck_threshold = FLAGS_consensus_stuck_peer_call_threshold_ms * 1ms;
+      auto now = CoarseMonoClock::Now();
+      auto last_rpc_start_time = last_rpc_start_time_.load(std::memory_order_acquire);
+      if (last_rpc_start_time != CoarseTimePoint::min() &&
+          now > last_rpc_start_time + stuck_threshold + timeout && !controller_.finished()) {
+        LOG_WITH_PREFIX(INFO) << Format(
+            "Found a RPC call in stuck state - timeout: $0, last_rpc_start_time: $1, "
+            "stuck threshold: $2, force recover: $3, call state: $4",
+            timeout, last_rpc_start_time, stuck_threshold,
+            FLAGS_consensus_force_recover_from_stuck_peer_call, controller_.CallStateDebugString());
+        if (FLAGS_consensus_force_recover_from_stuck_peer_call) {
+          controller_.MarkCallAsFailed();
+        }
+      }
+    }
     return Status::OK();
   }
 
@@ -203,10 +236,14 @@ void Peer::DumpToHtml(std::ostream& out) const {
   out << "Peer:" << std::endl;
   std::lock_guard lock(peer_lock_);
   out << Format(
-             "<ul><li>$0</li><li>$1</li><li>$2</li><li>$3</li></ul>",
+             "<ul><li>$0</li><li>$1</li><li>$2</li><li>$3</li><li>$4</li><li>$5</li></ul>",
              EscapeForHtmlToString(Format("State: $0", state_)),
              EscapeForHtmlToString(Format("Current Heartbeat Id: $0", cur_heartbeat_id_)),
              EscapeForHtmlToString(Format("Failed Attempts: $0", failed_attempts_)),
+             EscapeForHtmlToString(Format(
+                 "Last RPC start time: $0", last_rpc_start_time_.load(std::memory_order_acquire))),
+             EscapeForHtmlToString(
+                 Format("WaitingForRPCResponse: $0", performing_update_mutex_.is_locked())),
              peer_pb_str)
       << std::endl;
 }
@@ -366,6 +403,7 @@ void Peer::SendNextRequest(RequestTriggerMode trigger_mode) {
   processing_lock.unlock();
   performing_update_lock.release();
   controller_.set_invoke_callback_mode(rpc::InvokeCallbackMode::kThreadPoolHigh);
+  last_rpc_start_time_.store(CoarseMonoClock::now(), std::memory_order_release);
   proxy_->UpdateAsync(update_request_, trigger_mode, update_response_, &controller_,
                       std::bind(&Peer::ProcessResponse, retain_self));
 }
@@ -454,6 +492,7 @@ void Peer::ProcessResponse() {
   if (status.ok()) {
     status = controller_.thread_pool_failure();
   }
+  last_rpc_start_time_.store(CoarseTimePoint::min(), std::memory_order_release);
   controller_.Reset();
 
   auto performing_update_lock = LockPerformingUpdate(std::adopt_lock);
@@ -473,6 +512,7 @@ void Peer::ProcessResponse() {
 void Peer::ProcessHeartbeatResponse(const Status& status) {
   DCHECK(performing_heartbeat_mutex_.is_locked()) << "Got a heartbeat when nothing was pending.";
   DCHECK(heartbeat_request_.ops().empty()) << "Got a heartbeat with a non-zero number of ops.";
+  last_rpc_start_time_.store(CoarseMonoClock::now(), std::memory_order_release);
 
   auto performing_heartbeat_lock = LockPerformingHeartbeat(std::adopt_lock);
   auto processing_lock = StartProcessingUnlocked();
@@ -508,6 +548,7 @@ Status Peer::SendRemoteBootstrapRequest() {
   YB_LOG_WITH_PREFIX_EVERY_N_SECS(INFO, 30) << "Sending request to remotely bootstrap";
   controller_.set_invoke_callback_mode(rpc::InvokeCallbackMode::kThreadPoolNormal);
   return raft_pool_token_->SubmitFunc([retain_self = shared_from_this()]() {
+    retain_self->last_rpc_start_time_.store(CoarseMonoClock::now(), std::memory_order_release);
     retain_self->proxy_->StartRemoteBootstrap(
       &retain_self->rb_request_, &retain_self->rb_response_, &retain_self->controller_,
       std::bind(&Peer::ProcessRemoteBootstrapResponse, retain_self));
@@ -516,6 +557,7 @@ Status Peer::SendRemoteBootstrapRequest() {
 
 void Peer::ProcessRemoteBootstrapResponse() {
   Status status = controller_.status();
+  last_rpc_start_time_.store(CoarseTimePoint::min(), std::memory_order_release);
   controller_.Reset();
 
   auto performing_update_lock = LockPerformingUpdate(std::adopt_lock);
@@ -621,10 +663,9 @@ void RpcPeerProxy::UpdateAsync(const LWConsensusRequestPB* request,
   consensus_proxy_->UpdateConsensusAsync(*request, response, controller, callback);
 }
 
-void RpcPeerProxy::RequestConsensusVoteAsync(const VoteRequestPB* request,
-                                             VoteResponsePB* response,
-                                             rpc::RpcController* controller,
-                                             const rpc::ResponseCallback& callback) {
+void RpcPeerProxy::RequestConsensusVoteAsync(
+    const VoteRequestPB* request, VoteResponsePB* response, rpc::RpcController* controller,
+    const rpc::ResponseCallback& callback) {
   consensus_proxy_->RequestConsensusVoteAsync(*request, response, controller, callback);
 }
 
@@ -636,17 +677,15 @@ void RpcPeerProxy::RunLeaderElectionAsync(const RunLeaderElectionRequestPB* requ
   consensus_proxy_->RunLeaderElectionAsync(*request, response, controller, callback);
 }
 
-void RpcPeerProxy::LeaderElectionLostAsync(const LeaderElectionLostRequestPB* request,
-                                           LeaderElectionLostResponsePB* response,
-                                           rpc::RpcController* controller,
-                                           const rpc::ResponseCallback& callback) {
+void RpcPeerProxy::LeaderElectionLostAsync(
+    const LeaderElectionLostRequestPB* request, LeaderElectionLostResponsePB* response,
+    rpc::RpcController* controller, const rpc::ResponseCallback& callback) {
   consensus_proxy_->LeaderElectionLostAsync(*request, response, controller, callback);
 }
 
-void RpcPeerProxy::StartRemoteBootstrap(const StartRemoteBootstrapRequestPB* request,
-                                        StartRemoteBootstrapResponsePB* response,
-                                        rpc::RpcController* controller,
-                                        const rpc::ResponseCallback& callback) {
+void RpcPeerProxy::StartRemoteBootstrap(
+    const StartRemoteBootstrapRequestPB* request, StartRemoteBootstrapResponsePB* response,
+    rpc::RpcController* controller, const rpc::ResponseCallback& callback) {
   consensus_proxy_->StartRemoteBootstrapAsync(*request, response, controller, callback);
 }
 

src/yb/consensus/consensus_peers-test.cc
@@ -50,6 +50,7 @@
 #include "yb/util/metrics.h"
 #include "yb/util/opid.h"
 #include "yb/util/scope_exit.h"
+#include "yb/util/source_location.h"
 #include "yb/util/status_log.h"
 #include "yb/util/test_macros.h"
 #include "yb/util/test_util.h"
@@ -58,6 +59,8 @@
 using namespace std::chrono_literals;
 
 METRIC_DECLARE_entity(tablet);
+DECLARE_int32(stuck_peer_call_threshold_ms);
+DECLARE_bool(force_recover_from_stuck_peer_call);
 
 namespace yb {
 namespace consensus {

src/yb/consensus/consensus_peers.h
@@ -258,6 +258,7 @@ class Peer : public std::enable_shared_from_this<Peer> {
   StartRemoteBootstrapResponsePB rb_response_;
 
   rpc::RpcController controller_;
+  std::atomic<CoarseTimePoint> last_rpc_start_time_{CoarseTimePoint::min()};
 
   // Held if there is an outstanding request.  This is used in order to ensure that we only have a
   // single request outstanding at a time, and to wait for the outstanding requests at Close().

src/yb/rpc/connection.cc
@@ -32,6 +32,8 @@
 
 #include "yb/rpc/connection.h"
 
+#include <atomic>
+#include <sstream>
 #include <thread>
 #include <utility>
 
@@ -47,6 +49,7 @@
 #include "yb/rpc/rpc_introspection.pb.h"
 #include "yb/rpc/rpc_metrics.h"
 
+#include "yb/util/debug-util.h"
 #include "yb/util/enums.h"
 #include "yb/util/format.h"
 #include "yb/util/logging.h"
@@ -121,7 +124,16 @@ std::string Connection::ReasonNotIdle() const {
   return reason;
 }
 
-void Connection::Shutdown(const Status& status) {
+void Connection::Shutdown(const Status& provided_status) {
+  if (provided_status.ok()) {
+    LOG_WITH_PREFIX(DFATAL)
+        << "Connection shutdown called with an OK status, replacing with an error:\n"
+        << GetStackTrace();
+  }
+  const Status status =
+      provided_status.ok()
+          ? STATUS_FORMAT(RuntimeError, "Connection shutdown called with OK status")
+          : provided_status;
   {
     std::vector<OutboundDataPtr> outbound_data_being_processed;
     {
@@ -131,8 +143,11 @@ void Connection::Shutdown(const Status& status) {
       shutdown_status_ = status;
     }
 
+    shutdown_time_.store(reactor_->cur_time(), std::memory_order_release);
+
+    auto self = shared_from_this();
     for (auto& call : outbound_data_being_processed) {
-      call->Transferred(status, this);
+      call->Transferred(status, self);
     }
   }
 
@@ -140,6 +155,7 @@ void Connection::Shutdown(const Status& status) {
   stream_->Shutdown(status);
 
   // Clear any calls which have been sent and were awaiting a response.
+  active_calls_during_shutdown_.store(active_calls_.size(), std::memory_order_release);
   for (auto& v : active_calls_) {
     if (v.call && !v.call->IsFinished()) {
       v.call->SetFailed(status);
@@ -157,16 +173,19 @@ void Connection::OutboundQueued() {
   auto status = stream_->TryWrite();
   if (!status.ok()) {
     VLOG_WITH_PREFIX(1) << "Write failed: " << status;
-    // Even though we are already on the reactor thread, try to schedule a task so that it would run
-    // later than all other already scheduled tasks, to preserve historical behavior.
-    auto scheduling_status = reactor_->ScheduleReactorTask(
-        MakeFunctorReactorTask(
-            std::bind(&Reactor::DestroyConnection, reactor_, this, status),
-            shared_from_this(), SOURCE_LOCATION()));
-    if (!scheduling_status.ok()) {
-      LOG(WARNING) << "Failed to schedule DestroyConnection: " << scheduling_status
-                   << "on reactor, destroying connection immediately";
-      reactor_->DestroyConnection(this, status);
+
+    if (!queued_destroy_connection_.exchange(true, std::memory_order_acq_rel)) {
+      // Even though we are already on the reactor thread, try to schedule a task so that it
+      // would run later than all other already scheduled tasks, to preserve historical
+      // behavior.
+      auto scheduling_status = reactor_->ScheduleReactorTask(MakeFunctorReactorTask(
+          std::bind(&Reactor::DestroyConnection, reactor_, this, status), shared_from_this(),
+          SOURCE_LOCATION()));
+      if (!scheduling_status.ok()) {
+        LOG(WARNING) << "Failed to schedule DestroyConnection: " << scheduling_status
+                     << "on reactor, destroying connection immediately";
+        reactor_->DestroyConnection(this, status);
+      }
     }
   }
 }
@@ -280,7 +299,8 @@ size_t Connection::DoQueueOutboundData(OutboundDataPtr outbound_data, bool batch
     YB_LOG_EVERY_N_SECS(INFO, 5) << "Connection::DoQueueOutboundData data: "
                                  << AsString(outbound_data) << " shutdown_status_: "
                                  << shutdown_status;
-    outbound_data->Transferred(shutdown_status, this);
+    outbound_data->Transferred(shutdown_status, shared_from_this());
+    calls_queued_after_shutdown_.fetch_add(1, std::memory_order_acq_rel);
     return std::numeric_limits<size_t>::max();
   }
 
@@ -372,6 +392,51 @@ std::string Connection::ToString() const {
   FATAL_INVALID_ENUM_VALUE(Direction, direction_);
 }
 
+void Connection::QueueDumpConnectionState(int32_t call_id, const void* call_ptr) const {
+  auto task = MakeFunctorReactorTask(
+      std::bind(&Connection::DumpConnectionState, this, call_id, call_ptr), shared_from_this(),
+      SOURCE_LOCATION());
+  auto scheduling_status = reactor_->ScheduleReactorTask(task);
+  LOG_IF_WITH_PREFIX(DFATAL, !scheduling_status.ok())
+      << "Failed to schedule call to dump connection state: " << scheduling_status;
+}
+
+void Connection::DumpConnectionState(int32_t call_id, const void* call_ptr) const {
+  auto earliest_expiry = active_calls_.empty()
+                             ? CoarseTimePoint()
+                             : active_calls_.get<ExpirationTag>().begin()->expires_at;
+  auto found_call_id = active_calls_.find(call_id) != active_calls_.end();
+  LOG_WITH_PREFIX(INFO) << Format(
+      "LastActivityTime: $0, "
+      "ActiveCalls stats: { "
+      "during shutdown: $1, "
+      "current size: $2, "
+      "earliest expiry: $3, "
+      "}, "
+      "OutboundCall: { "
+      "ptr: $4, "
+      "call id: $5, "
+      "is active: $6 "
+      "}, "
+      "Shutdown status: $7, "
+      "Shutdown time: $8, "
+      "Queue attempts after shutdown: { "
+      "calls: $9, "
+      "responses: $10 "
+      "}",
+      /* $0 */ last_activity_time(),
+      /* $1 */ active_calls_during_shutdown_.load(std::memory_order_acquire),
+      /* $2 */ active_calls_.size(),
+      /* $3 */ earliest_expiry,
+      /* $4 */ call_ptr,
+      /* $5 */ call_id,
+      /* $6 */ found_call_id,
+      /* $7 */ shutdown_status_,
+      /* $8 */ shutdown_time_,
+      /* $9 */ calls_queued_after_shutdown_.load(std::memory_order_acquire),
+      /* $10 */ responses_queued_after_shutdown_.load(std::memory_order_acquire));
+}
+
 Status Connection::DumpPB(const DumpRunningRpcsRequestPB& req,
                           RpcConnectionPB* resp) {
   resp->set_remote_ip(yb::ToString(remote()));
@@ -429,6 +494,7 @@ Status Connection::QueueOutboundData(OutboundDataPtr outbound_data) {
           std::bind(&OutboundData::Transferred, outbound_data, _2, /* conn */ nullptr),
           SOURCE_LOCATION());
       outbound_data_queue_lock.unlock();
+      responses_queued_after_shutdown_.fetch_add(1, std::memory_order_acq_rel);
       auto scheduling_status = reactor_->ScheduleReactorTask(task, true /* even_if_not_running */);
       LOG_IF_WITH_PREFIX(DFATAL, !scheduling_status.ok())
           << "Failed to schedule OutboundData::Transferred: " << scheduling_status;
@@ -520,11 +586,13 @@ void Connection::UpdateLastWrite() {
 }
 
 void Connection::Transferred(const OutboundDataPtr& data, const Status& status) {
-  data->Transferred(status, this);
+  data->Transferred(status, shared_from_this());
 }
 
 void Connection::Destroy(const Status& status) {
-  reactor_->DestroyConnection(this, status);
+  if (!queued_destroy_connection_.exchange(true, std::memory_order_acq_rel)) {
+    reactor_->DestroyConnection(this, status);
+  }
 }
 
 std::string Connection::LogPrefix() const {

src/yb/rpc/connection.h
@@ -155,6 +155,11 @@ class Connection final : public StreamContext, public std::enable_shared_from_th
   // Safe to be called from other threads.
   std::string ToString() const;
 
+  // Dump the connection state for provided call id. This includes the information realted to
+  // connection shutdown time, and whether the call id is present in the active_calls_ or not.
+  // call_ptr is used only for logging to correlate with OutboundCall trace.
+  void QueueDumpConnectionState(int32_t call_id, const void* call_ptr) const;
+
   Direction direction() const { return direction_; }
 
   // Queue a call response back to the client on the server side.
@@ -225,6 +230,8 @@ class Connection final : public StreamContext, public std::enable_shared_from_th
   StreamReadBuffer& ReadBuffer() override;
 
   void CleanupExpirationQueue(CoarseTimePoint now) ON_REACTOR_THREAD;
+  // call_ptr is used only for logging to correlate with OutboundCall trace.
+  void DumpConnectionState(int32_t call_id, const void* call_ptr) const ON_REACTOR_THREAD;
 
   std::string LogPrefix() const;
 
@@ -299,6 +306,7 @@ class Connection final : public StreamContext, public std::enable_shared_from_th
 
   // Starts as Status::OK, gets set to a shutdown status upon Shutdown().
   Status shutdown_status_ GUARDED_BY(outbound_data_queue_mtx_);
+  std::atomic<CoarseTimePoint> shutdown_time_{CoarseTimePoint::min()};
 
   std::shared_ptr<ReactorTask> process_response_queue_task_ GUARDED_BY(outbound_data_queue_mtx_);
 
@@ -307,9 +315,14 @@ class Connection final : public StreamContext, public std::enable_shared_from_th
   // ----------------------------------------------------------------------------------------------
 
   std::atomic<uint64_t> responded_call_count_{0};
+  std::atomic<size_t> active_calls_during_shutdown_{0};
+  std::atomic<size_t> calls_queued_after_shutdown_{0};
+  std::atomic<size_t> responses_queued_after_shutdown_{0};
 
   // The last time we read or wrote from the socket.
-  std::atomic<CoarseTimePoint> last_activity_time_;
+  std::atomic<CoarseTimePoint> last_activity_time_{CoarseTimePoint::min()};
+
+  std::atomic<bool> queued_destroy_connection_{false};
 };
 
 }  // namespace rpc

src/yb/rpc/inbound_call.cc
@@ -88,7 +88,7 @@ InboundCall::~InboundCall() {
   DecrementGauge(rpc_metrics_->inbound_calls_alive);
 }
 
-void InboundCall::NotifyTransferred(const Status& status, Connection* conn) {
+void InboundCall::NotifyTransferred(const Status& status, const ConnectionPtr& conn) {
   if (status.ok()) {
     TRACE_TO(trace(), "Transfer finished");
   } else {

src/yb/rpc/inbound_call.h
@@ -226,7 +226,7 @@ class InboundCall : public RpcCall, public MPSCQueueEntry<InboundCall> {
  protected:
   ThreadPoolTask* BindTask(InboundCallHandler* handler, int64_t rpc_queue_limit);
 
-  void NotifyTransferred(const Status& status, Connection* conn) override;
+  void NotifyTransferred(const Status& status, const ConnectionPtr& conn) override;
 
   virtual void Clear();
 

src/yb/rpc/outbound_call.cc
@@ -191,7 +191,8 @@ OutboundCall::~OutboundCall() {
   DecrementGauge(rpc_metrics_->outbound_calls_alive);
 }
 
-void OutboundCall::NotifyTransferred(const Status& status, Connection* conn) {
+void OutboundCall::NotifyTransferred(const Status& status, const ConnectionPtr& conn) {
+  sent_time_ = CoarseMonoClock::Now();
   if (IsFinished()) {
     auto current_state = state();
     LOG_IF_WITH_PREFIX(DFATAL, !IsTimedOut())
@@ -200,6 +201,11 @@ void OutboundCall::NotifyTransferred(const Status& status, Connection* conn) {
         << "(" << StateName(current_state) << ")"
         << ", status: " << this->status();
   } else if (status.ok()) {
+    if (!conn) {
+      LOG_WITH_PREFIX_AND_FUNC(WARNING)
+          << this << " - Unexpected - Connection is null with ok status";
+    }
+    connection_ = conn;
     SetSent();
   } else {
     VLOG_WITH_PREFIX(1) << "Connection torn down: " << status;
@@ -360,6 +366,10 @@ bool OutboundCall::SetState(State new_state) {
 }
 
 void OutboundCall::InvokeCallback() {
+  invoke_callback_time_ = CoarseMonoClock::Now();
+  // Release the connection reference once the callback is invoked. This helps prevent circular
+  // dependencies for cleanup between OutboundCall and Connection.
+  connection_.reset();
   if (callback_thread_pool_) {
     callback_task_.SetOutboundCall(shared_from(this));
     callback_thread_pool_->Enqueue(&callback_task_);
@@ -399,6 +409,12 @@ void OutboundCall::InvokeCallbackSync() {
 
 void OutboundCall::SetResponse(CallResponse&& resp) {
   DCHECK(!IsFinished());
+
+  if (test_ignore_response) {
+    LOG_WITH_PREFIX(WARNING) << "Skipping OutboundCall response processing: " << this << " - "
+                             << ToString();
+    return;
+  }
 
   auto now = CoarseMonoClock::Now();
   TRACE_TO_WITH_TIME(trace_, now, "Response received.");
@@ -591,6 +607,18 @@ Status OutboundCall::InitHeader(RequestHeader* header) {
     header->set_timeout_millis(VERIFY_RESULT(TimeoutMs()));
   }
   return Status::OK();
+}
+
+std::string OutboundCall::DebugString() const {
+  if (connection_) {
+    connection_->QueueDumpConnectionState(call_id_, this);
+  }
+
+  return Format(
+      "OutboundCall($0): $1, start_time: $2, sent_time: $3, callback_time: $4, now: $5, "
+      "connection: $6",
+      this, ToString(), start_, sent_time_, invoke_callback_time_, CoarseMonoClock::Now(),
+      connection_);
 }
 
 ///

commit 0dc13e60f2f320ac17ec507221f8f70b264db311

[#18744] DocDB: Fixed issue with last_rpc_start_time_ reset which resulted in tsan race in connection
Summary:
This change fixes TSAN race issues caused by change 98586ef82bb2530345cd2711e6fd24b32dca9270. In initial change, we were incorrectly setting the RPC start time to now when processing response, because of which detection logic was not working correctly, hence resulting in tsan race issues.

tsan race details -
- Connection getting reset when we are trying to get DebugString. This is solved by avoiding the call to DebugString() until we figure out that call is actually stuck.
```
[ts-2]   Read of size 8 at 0x7b54000c0198 by thread T21 (mutexes: write M0):
[ts-2]     #0 std::shared_ptr<yb::rpc::Connection>::get[abi:v160006]() const /opt/yb-build/thirdparty/yugabyte-db-thirdparty-v20230803170255-057e0a1188-centos7-x86_64-clang16/installed/tsan/libcxx/include/c++/v1/__memory/shared_ptr.h:843:16 (libyrpc.so+0x11383f)
[ts-2]     #1 std::shared_ptr<yb::rpc::Connection>::operator bool[abi:v160006]() const /opt/yb-build/thirdparty/yugabyte-db-thirdparty-v20230803170255-057e0a1188-centos7-x86_64-clang16/installed/tsan/libcxx/include/c++/v1/__memory/shared_ptr.h:875:16 (libyrpc.so+0x11383f)
[ts-2]     #2 yb::rpc::OutboundCall::DebugString() const ${BUILD_ROOT}/../../src/yb/rpc/outbound_call.cc:613:7 (libyrpc.so+0x11383f)
[ts-2]     #3 yb::rpc::RpcController::CallStateDebugString() const ${BUILD_ROOT}/../../src/yb/rpc/rpc_controller.cc:163:19 (libyrpc.so+0x156976)
[ts-2]     #4 yb::consensus::Peer::SignalRequest(yb::consensus::RequestTriggerMode) ${BUILD_ROOT}/../../src/yb/consensus/consensus_peers.cc:188:77 (libconsensus.so+0x94c2e)

[ts-2]   Previous write of size 8 at 0x7b54000c0198 by thread T25:

src/yb/consensus/consensus_peers.cc
@@ -512,7 +512,7 @@ void Peer::ProcessResponse() {
 void Peer::ProcessHeartbeatResponse(const Status& status) {
   DCHECK(performing_heartbeat_mutex_.is_locked()) << "Got a heartbeat when nothing was pending.";
   DCHECK(heartbeat_request_.ops().empty()) << "Got a heartbeat with a non-zero number of ops.";
-  last_rpc_start_time_.store(CoarseMonoClock::now(), std::memory_order_release);
+  last_rpc_start_time_.store(CoarseTimePoint::min(), std::memory_order_release);
 
   auto performing_heartbeat_lock = LockPerformingHeartbeat(std::adopt_lock);
   auto processing_lock = StartProcessingUnlocked();