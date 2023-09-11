639df0a1f0e2d8db5f96d508162098f55c73ba63
Naorem Khogendro Singh <nsingh@yugabyte.com>
2023-09-11 16:59:33 -0700

    [BACKPORT 2.18][PLAT-9814] Do not garbage collect the customer task which is still being referred in the universe task or the last task for a provider
    Summary:
    Original diff - https://phorge.dev.yugabyte.com/D28457 (0bd85937dbfe15bf6b9f3d288d0fe506c93dd193)
    
    This change makes sure that the task garbage collector does not delete the lastest task for provider or if the task UUID is being referenced in a universe.
    
    No change in API.
    
    Test Plan: Unit test
    
    Reviewers: amalyshev, cwang, sanketh
    
    Reviewed By: cwang
    
    Subscribers: yugaware
    
    Differential Revision: https://phorge.dev.yugabyte.com/D28584

--- managed/src/main/java/com/yugabyte/yw/commissioner/TaskExecutor.java
+++ managed/src/main/java/com/yugabyte/yw/commissioner/TaskExecutor.java
@@ -385,7 +385,7 @@ public class TaskExecutor {
       return Optional.empty();
     }
     RunnableTask runnableTask = optional.get();
-    ITask task = runnableTask.task;
+    ITask task = runnableTask.getTask();
     if (!isTaskAbortable(task.getClass())) {
       throw new RuntimeException("Task " + task.getName() + " is not abortable");
     }
@@ -398,7 +398,7 @@ public class TaskExecutor {
     // Update the task state in the memory and DB.
     runnableTask.compareAndSetTaskState(
         Sets.immutableEnumSet(State.Initializing, State.Created, State.Running), State.Abort);
-    return Optional.of(runnableTask.taskInfo);
+    return Optional.of(runnableTask.getTaskInfo());
   }
 
   /**
@@ -578,7 +578,7 @@ public class TaskExecutor {
         RunnableSubTask runnableSubTask,
         Throwable throwable) {
       if (throwable != null) {
-        log.error("Error occurred in subtask " + runnableSubTask.taskInfo, throwable);
+        log.error("Error occurred in subtask " + runnableSubTask.getTaskInfo(), throwable);
       }
       taskIterator.remove();
       numTasksCompleted.incrementAndGet();
@@ -635,7 +635,7 @@ public class TaskExecutor {
             } else if (abortTime != null
                 && Duration.between(abortTime, Instant.now()).compareTo(defaultAbortTaskTimeout) > 0
                 && (skipSubTaskAbortableCheck
-                    || isTaskAbortable(runnableSubTask.task.getClass()))) {
+                    || isTaskAbortable(runnableSubTask.getTask().getClass()))) {
               future.cancel(true);
               // Report aborted to the parent task.
               // Update the subtask state to aborted if the execution timed out.
@@ -738,15 +738,15 @@ public class TaskExecutor {
    * started running. Synchronization is on the this object for taskInfo.
    */
   public abstract class AbstractRunnableTask implements Runnable {
-    final ITask task;
-    final TaskInfo taskInfo;
+    private final ITask task;
+    private final TaskInfo taskInfo;
     // Timeout limit for this task.
-    final Duration timeLimit;
-    final String[] creatorCallstack;
+    private final Duration timeLimit;
+    private final String[] creatorCallstack;
 
-    Instant taskScheduledTime;
-    Instant taskStartTime;
-    Instant taskCompletionTime;
+    private Instant taskScheduledTime;
+    private Instant taskStartTime;
+    private Instant taskCompletionTime;
 
     // Future of the task that is set after it is submitted to the ExecutorService.
     Future<?> future = null;
@@ -778,6 +778,14 @@ public class TaskExecutor {
       } else {
         creatorCallstack = new String[0];
       }
+    }
+
+    public ITask getTask() {
+      return task;
+    }
+
+    public TaskInfo getTaskInfo() {
+      return taskInfo;
     }
 
     @VisibleForTesting
@@ -1015,9 +1023,9 @@ public class TaskExecutor {
     /** Invoked by the ExecutorService. Do not invoke this directly. */
     @Override
     public void run() {
-      UUID taskUUID = taskInfo.getTaskUUID();
+      UUID taskUUID = getTaskInfo().getTaskUUID();
       try {
-        task.setUserTaskUUID(taskUUID);
+        getTask().setUserTaskUUID(taskUUID);
         super.run();
       } catch (Exception e) {
         Throwables.propagate(e);
@@ -1202,9 +1210,9 @@ public class TaskExecutor {
     @Override
     public void run() {
       // Sets the top-level user task UUID.
-      task.setUserTaskUUID(getUserTaskUUID());
+      getTask().setUserTaskUUID(getUserTaskUUID());
       int currentAttempt = 0;
-      int retryLimit = task.getRetryLimit();
+      int retryLimit = getTask().getRetryLimit();
 
       while (currentAttempt < retryLimit) {
         try {
@@ -1216,10 +1224,9 @@ public class TaskExecutor {
             throw e;
           }
 
-          log.warn("Task {} attempt {} has failed", task, currentAttempt);
-          task.onFailure(taskInfo, e);
+          log.warn("Task {} attempt {} has failed", getTask(), currentAttempt);
+          getTask().onFailure(getTaskInfo(), e);
         }
-
         currentAttempt++;
       }
     }
@@ -1240,18 +1247,18 @@ public class TaskExecutor {
     }
 
     public synchronized void setSubTaskGroupType(SubTaskGroupType subTaskGroupType) {
-      if (taskInfo.getSubTaskGroupType() != subTaskGroupType) {
-        taskInfo.setSubTaskGroupType(subTaskGroupType);
-        taskInfo.save();
+      if (getTaskInfo().getSubTaskGroupType() != subTaskGroupType) {
+        getTaskInfo().setSubTaskGroupType(subTaskGroupType);
+        getTaskInfo().save();
       }
     }
 
     private synchronized void setRunnableTaskContext(
         RunnableTask parentRunnableTask, int position) {
       this.parentRunnableTask = parentRunnableTask;
-      taskInfo.setParentUuid(parentRunnableTask.getTaskUUID());
-      taskInfo.setPosition(position);
-      taskInfo.save();
+      getTaskInfo().setParentUuid(parentRunnableTask.getTaskUUID());
+      getTaskInfo().setPosition(position);
+      getTaskInfo().save();
     }
   }
 }
--- managed/src/main/java/com/yugabyte/yw/common/CustomerTaskManager.java
+++ managed/src/main/java/com/yugabyte/yw/common/CustomerTaskManager.java
@@ -24,7 +24,7 @@ import com.yugabyte.yw.models.ScheduleTask;
 import com.yugabyte.yw.models.TaskInfo;
 import com.yugabyte.yw.models.Universe;
 import com.yugabyte.yw.models.helpers.TaskType;
-import io.ebean.Ebean;
+import io.ebean.DB;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
@@ -86,7 +86,10 @@ public class CustomerTaskManager {
                 subtask.save();
               });
 
-      Optional<Universe> optUniv = Universe.maybeGet(customerTask.getTargetUUID());
+      Optional<Universe> optUniv =
+          customerTask.getTargetType().isUniverseTarget()
+              ? Universe.maybeGet(customerTask.getTargetUUID())
+              : Optional.empty();
       if (LOAD_BALANCER_TASK_TYPES.contains(taskInfo.getTaskType())) {
         Boolean isLoadBalanceAltered = false;
         JsonNode node = taskInfo.getDetails();
@@ -175,24 +178,22 @@ public class CustomerTaskManager {
 
       if (unlockUniverse) {
         // Unlock the universe for future operations.
-        Universe.maybeGet(customerTask.getTargetUUID())
-            .ifPresent(
-                u -> {
-                  UniverseDefinitionTaskParams details = u.getUniverseDetails();
-                  if (details.updateInProgress) {
-                    // Create the update lambda.
-                    Universe.UniverseUpdater updater =
-                        universe -> {
-                          UniverseDefinitionTaskParams universeDetails =
-                              universe.getUniverseDetails();
-                          universeDetails.updateInProgress = false;
-                          universe.setUniverseDetails(universeDetails);
-                        };
+        optUniv.ifPresent(
+            u -> {
+              UniverseDefinitionTaskParams details = u.getUniverseDetails();
+              if (details.updateInProgress) {
+                // Create the update lambda.
+                Universe.UniverseUpdater updater =
+                    universe -> {
+                      UniverseDefinitionTaskParams universeDetails = universe.getUniverseDetails();
+                      universeDetails.updateInProgress = false;
+                      universe.setUniverseDetails(universeDetails);
+                    };
 
-                    Universe.saveDetails(customerTask.getTargetUUID(), updater, false);
-                    LOG.debug("Unlocked universe {}.", customerTask.getTargetUUID());
-                  }
-                });
+                Universe.saveDetails(customerTask.getTargetUUID(), updater, false);
+                LOG.debug("Unlocked universe {}.", customerTask.getTargetUUID());
+              }
+            });
       }
 
       // Mark task as a failure after the universe is unlocked.
@@ -204,15 +205,13 @@ public class CustomerTaskManager {
       // Resume tasks if any
       TaskType taskType = taskInfo.getTaskType();
       UniverseTaskParams taskParams = null;
-      LOG.info("Resume Task: " + String.valueOf(resumeTask));
+      LOG.info("Resume Task: {}", resumeTask);
 
       try {
         if (resumeTask && optUniv.isPresent()) {
           Universe universe = optUniv.get();
           if (!taskUUID.equals(universe.getUniverseDetails().updatingTaskUUID)) {
-            String errMsg =
-                String.format("Invalid task state: Task %s cannot be resumed", taskUUID);
-            LOG.debug(errMsg);
+            LOG.debug("Invalid task state: Task {} cannot be resumed", taskUUID);
             customerTask.markAsCompleted();
             return;
           }
@@ -229,7 +228,7 @@ public class CustomerTaskManager {
               taskParams = restoreParams;
               break;
             default:
-              LOG.error(String.format("Invalid task type: %s during platform restart", taskType));
+              LOG.error("Invalid task type: {} during platform restart", taskType);
               return;
           }
           taskParams.setPreviousTaskUUID(taskUUID);
@@ -238,15 +237,14 @@ public class CustomerTaskManager {
           try {
             customerTask.updateTaskUUID(newTaskUUID);
             customerTask.resetCompletionTime();
-            TaskInfo task = TaskInfo.get(taskUUID);
-            if (task != null) {
-              task.getSubTasks().forEach(st -> st.delete());
-              task.delete();
+            Optional<TaskInfo> optional = TaskInfo.maybeGet(taskUUID);
+            if (optional.isPresent()) {
+              optional.get().getSubTasks().forEach(st -> st.delete());
+              optional.get().delete();
             }
             commitTransaction();
           } catch (Exception e) {
-            throw new RuntimeException(
-                "Unable to delete the previous task info: " + taskUUID.toString());
+            throw new RuntimeException("Unable to delete the previous task info: " + taskUUID);
           } finally {
             endTransaction();
           }
@@ -274,9 +272,18 @@ public class CustomerTaskManager {
           TaskInfo.INCOMPLETE_STATES.stream()
               .map(Objects::toString)
               .collect(Collectors.joining("','"));
-      // Retrieve all incomplete customer tasks or task in incomplete state. Task state update and
-      // customer completion time update are not transactional.
+
+      // Delete orphaned parent tasks which do not have any associated customer task.
+      // It is rare but can happen as a customer task and task info are not saved in transaction.
+      // TODO It can be handled better with transaction but involves bigger change.
       String query =
+          "DELETE FROM task_info WHERE parent_uuid IS NULL AND uuid NOT IN "
+              + "(SELECT task_uuid FROM customer_task)";
+      DB.sqlUpdate(query).execute();
+
+      // Retrieve all incomplete customer tasks or task in incomplete state. Task state update
+      // and customer completion time update are not transactional.
+      query =
           "SELECT ti.uuid AS task_uuid, ct.id AS customer_task_id "
               + "FROM task_info ti, customer_task ct "
               + "WHERE ti.uuid = ct.task_uuid "
@@ -287,11 +294,11 @@ public class CustomerTaskManager {
               + "(ti.task_state='Aborted' AND ti.details->>'errorString' = 'Platform shutdown'"
               + " AND ct.completion_time IS NULL))";
       // TODO use Finder.
-      Ebean.createSqlQuery(query)
+      DB.sqlQuery(query)
           .findList()
           .forEach(
               row -> {
-                TaskInfo taskInfo = TaskInfo.get(row.getUUID("task_uuid"));
+                TaskInfo taskInfo = TaskInfo.getOrBadRequest(row.getUUID("task_uuid"));
                 CustomerTask customerTask = CustomerTask.get(row.getLong("customer_task_id"));
                 handlePendingTask(customerTask, taskInfo);
               });
