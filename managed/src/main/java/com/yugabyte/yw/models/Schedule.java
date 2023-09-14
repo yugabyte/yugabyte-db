94d679b384de3f8682c992bfa4b7c44d2db6f5f7
Vipul Bansal <vipul.bansal.yb@gmail.com>
2023-09-14 04:18:51 +0000

    [BACKPORT 2.16][PLAT-10419]: Pass schedule backup parallelism in GET schedule API response
    Summary:
    Pass schedule backup parallelism in GET schedule API response
    Original Diff/commit: D28523/1b4d8b9
    
    Test Plan: Tested manually by creating a schedule with custom parallelism and verified new value is visible in UI.
    
    Reviewers: #yba-api-review, vkumar, vpatibandla
    
    Reviewed By: #yba-api-review, vkumar
    
    Subscribers: yugaware, dkumar
    
    Differential Revision: https://phorge.dev.yugabyte.com/D28536

--- managed/src/main/java/com/yugabyte/yw/models/Schedule.java
+++ managed/src/main/java/com/yugabyte/yw/models/Schedule.java
@@ -695,6 +695,7 @@ public class Schedule extends Model {
             .fullBackup(CollectionUtils.isEmpty(params.keyspaceTableList))
             .useTablespaces(params.useTablespaces)
             .expiryTimeUnit(params.expiryTimeUnit)
+            .parallelism(params.parallelism)
             .build();
     return backupInfo;
   }
@@ -720,6 +721,7 @@ public class Schedule extends Model {
             .expiryTimeUnit(params.expiryTimeUnit)
             .fullBackup(StringUtils.isEmpty(params.getKeyspace()))
             .useTablespaces(params.useTablespaces)
+            .parallelism(params.parallelism)
             .build();
     return backupInfo;
   }
