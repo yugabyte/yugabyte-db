7b29e8b4da4215098dc0d42e119166b8e26de8af
Sahith Kurapati <skurapati@yugabyte.com>
2023-09-15 06:57:24 +0000

    [PLAT-10437] Add unique constraint on role name in the DB
    Summary:
    Add unique constraint on (role name, customer UUID) in the Role table.
    We don't want duplicate role names for any customer.
    This implicitly adds that we cannot create a customer role with a built-in role as they will already be present in the DB.
    
    Test Plan:
    Manually tested.
    Added a UT.
    Run UTs.
    Run itests.
    
    Reviewers: sneelakantan, vpatibandla
    
    Reviewed By: sneelakantan
    
    Subscribers: sanketh, yugaware
    
    Differential Revision: https://phorge.dev.yugabyte.com/D28564

--- managed/src/test/java/com/yugabyte/yw/models/RoleTest.java
+++ managed/src/test/java/com/yugabyte/yw/models/RoleTest.java
@@ -4,6 +4,7 @@ package com.yugabyte.yw.models;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertThrows;
 import static org.junit.Assert.assertTrue;
 
 import com.yugabyte.yw.common.FakeDBApplication;
@@ -13,6 +14,7 @@ import com.yugabyte.yw.common.rbac.PermissionInfo.Action;
 import com.yugabyte.yw.common.rbac.PermissionInfo.ResourceType;
 import com.yugabyte.yw.models.rbac.Role;
 import com.yugabyte.yw.models.rbac.Role.RoleType;
+import io.ebean.DuplicateKeyException;
 import java.util.Arrays;
 import java.util.HashSet;
 import org.junit.Before;
@@ -24,8 +26,8 @@ import org.mockito.junit.MockitoJUnitRunner;
 public class RoleTest extends FakeDBApplication {
 
   private Customer customer;
-  public Permission permission1 = new Permission(ResourceType.UNIVERSE, Action.CREATE);
-  public Permission permission2 = new Permission(ResourceType.OTHER, Action.CREATE);
+  public Permission permission1 = new Permission(ResourceType.UNIVERSE, Action.READ);
+  public Permission permission2 = new Permission(ResourceType.OTHER, Action.READ);
 
   @Before
   public void setUp() {
@@ -46,8 +48,38 @@ public class RoleTest extends FakeDBApplication {
     assertNotNull(role.getCreatedOn());
     assertEquals(1, role.getPermissionDetails().getPermissionList().size());
     for (Permission info : role.getPermissionDetails().getPermissionList()) {
-      assertTrue(info.getAction().equals(Action.CREATE));
+      assertTrue(info.getAction().equals(Action.READ));
       assertTrue(info.getResourceType().equals(ResourceType.UNIVERSE));
     }
+  }
+
+  @Test
+  public void testRoleNameUnique() {
+    // Create role with name FakeRole1 in customer.
+    Role role1 =
+        Role.create(
+            customer.getUuid(),
+            "FakeRole1",
+            "testDescription",
+            RoleType.Custom,
+            new HashSet<>(Arrays.asList(permission1)));
+    assertNotNull(role1.getRoleUUID());
+    assertEquals("FakeRole1", role1.getName());
+    assertNotNull(role1.getCreatedOn());
+    assertEquals(1, role1.getPermissionDetails().getPermissionList().size());
+
+    // Try to create another role with same name "FakeRole1" in the same customer.
+    // Should fail due to unique constraint.
+    assertThrows(
+        DuplicateKeyException.class,
+        () -> {
+          Role role2 =
+              Role.create(
+                  customer.getUuid(),
+                  "FakeRole1",
+                  "testDescription2",
+                  RoleType.Custom,
+                  new HashSet<>(Arrays.asList(permission2)));
+        });
   }
 }
