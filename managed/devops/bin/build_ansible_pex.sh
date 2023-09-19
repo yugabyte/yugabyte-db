b9c2d3b1ce3b3a7928164f9c54f2acf5a4dd8e43
Chandrakant Vankayalapati <cvankayalapati@yugabyte.com>
2023-09-18 22:55:56 -0700

    [PLAT-10465] Drop support for python 2 from the node agent pex
    Summary: This diff drops python 2 support from the node agent pex env, which eliminates a lot of security vulnerabities.
    
    Test Plan: Confirmed that the node agent pex env no longer includes Python 2.7 deps after the update.
    
    Reviewers: nsingh, muthu, nbhatia
    
    Reviewed By: nsingh, muthu
    
    Subscribers: yugaware
    
    Differential Revision: https://phorge.dev.yugabyte.com/D28624

--- managed/devops/bin/build_ansible_pex.sh
+++ managed/devops/bin/build_ansible_pex.sh
@@ -35,4 +35,4 @@ docker build -t "$DOCKER_PEX_IMAGE_NAME" .
 # Execute the build_pex.sh script inside the built docker image
 # to generate the repaired PEX.
 docker run --rm -v "$yb_devops_home:/code" -u "$UID:$(id -g $UID)" \
-"$DOCKER_PEX_IMAGE_NAME" -c "-r" "/code/ansible_python_requirements.txt" "--include_python2"
+"$DOCKER_PEX_IMAGE_NAME" -c "-r" "/code/ansible_python_requirements.txt"
