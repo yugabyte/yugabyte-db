0f09d1561defac6d7fdd10a27cf6f360a398212e
Timur Yusupov <ttyusupov@users.noreply.github.com>
2023-09-26 21:05:28 +0300

    [#19221] docdb: Improved timeout handling for YCQL index scan
    Summary:
    Scan by indexed column in YCQL can take longer than client timeout and cause a log spew in the following scenario:
    
    1. `select * from ks.test_table where category=0` is executed, it is important that there is an index on `category` column, so index scan is performed.
    2. YCQL sets the deadline to `now + client_read_write_timeout_ms` (4 seconds in the example above).
    3. Index tablet lookup is requested from MetaCache and first GetTableLocations RPC is sent to master and response is returned quickly.
    4. YCQL sends Read RPC to the index tablet leader with the `timeout` calculated based on deadline determined in step 2.
    5. Index tablet leader starts scan, but there are 1M records with category = 0 and index tablet scan is completed around or after the `timeout`, but before tserver timesout the request (there is a some granularity in detecting deadline).
    6. CQL receives answer from index tablet leader after deadline, but before `Connection::CleanupExpirationQueue` is executed, so index tablet scan is not timed out at YCQLProxy/YBClient side.
    `Connection::CleanupExpirationQueue` is executed by timer at `FLAGS_rpc_connection_timeout_ms` (60 seconds by default) granularity and also when we send outgoing call on the same connection.
    7. CQL is preparing Read operations from the main table based on keys received and adding these operations to `YBSession` which adds them to `Batcher`.  `Batcher` is doing loop over these 1M operations and asks `MetaCache` to determine tablet for each one.
    9. Since we’ve already hit deadline at this step, `MetaCache` gets error `Timed out after deadline expired` even before sending `GetTableLocations` RPC to the master.
    10. Tablet lookup for each of 1M+ operations is failing with the same `Timed out after deadline expired` and every error is logged by `client_master_rpc.cc`.
    11. Due to log spew from step 10 we have a longer CQL call execution time.
    
    Important factor here is that paging is off or page size is big enough, so index table page scan takes around or a little more than YCQL request timeout.
    
    Implemented the following changes to address that:
    - Enhanced VLOGs
    - ClientMasterRpcBase
      - Fixed log spew when having a lot of errors
      - Avoid re-requesting master leader in case RPC was timed out because it was simply attempted to send after deadline
    - MetaCache::LookupTabletByKey - fail earlier if we've hit the deadline and avoid attempts to create and send GetTableLocations RPC in this case
    - Enhanced VLOGs
    - Added CqlIndexTest.SlowIndexResponse
    Jira: DB-8015
    
    Test Plan: CqlIndexTest.SlowIndexResponse
    
    Reviewers: arybochkin, amitanand
    
    Reviewed By: amitanand
    
    Subscribers: yql, bogdan, ybase
    
    Differential Revision: https://phorge.dev.yugabyte.com/D28728

--- src/yb/docdb/deadline_info.cc
+++ src/yb/docdb/deadline_info.cc
@@ -28,14 +28,16 @@ namespace docdb {
 
 DeadlineInfo::DeadlineInfo(CoarseTimePoint deadline) : deadline_(deadline) {}
 
-// Every 1024 iterations, check whether the deadline passed and returning failure if it was
-// already timed out.
+// Every kDeadlineCheckGranularity iterations, check whether the deadline passed and returning
+// failure if it was already timed out.
 Status DeadlineInfo::CheckDeadlinePassed() {
   if (PREDICT_FALSE(FLAGS_TEST_tserver_timeout)) {
     return STATUS(Expired, "TEST: Deadline for query passed");
   }
 
-  if (PREDICT_FALSE((++counter_ & 1023) == 0 && CoarseMonoClock::now() > deadline_)) {
+  if (PREDICT_FALSE(
+          (++counter_ & (kDeadlineCheckGranularity - 1)) == 0 &&
+          CoarseMonoClock::now() > deadline_)) {
     return STATUS_FORMAT(
         Expired, "Deadline for query passed $0 ago", CoarseMonoClock::now() - deadline_);
   }
