6959e9999d1cee2c534889f6052ec00c23bf1f1b
vkumar <vkumar@yugabyte.com>
2023-09-26 08:53:14 +0000

    [PLAT-10552]Add useTablespaces boolean in list backup response
    Summary: Add useTablespaces boolean in list backups response
    
    Test Plan: Manually tested with `useTablespaces` response in backup list response.
    
    Reviewers: skurapati, #yba-api-review!
    
    Reviewed By: skurapati
    
    Subscribers: sneelakantan, yugaware
    
    Differential Revision: https://phorge.dev.yugabyte.com/D28762

--- managed/src/main/java/com/yugabyte/yw/common/backuprestore/BackupUtil.java
+++ managed/src/main/java/com/yugabyte/yw/common/backuprestore/BackupUtil.java
@@ -349,7 +349,8 @@ public class BackupUtil {
             .hasIncrementalBackups(hasIncrements)
             .lastIncrementalBackupTime(lastIncrementDate)
             .lastBackupState(lastBackupState)
-            .scheduleName(backup.getScheduleName());
+            .scheduleName(backup.getScheduleName())
+            .useTablespaces(backup.getBackupInfo().useTablespaces);
     return builder.build();
   }
 
