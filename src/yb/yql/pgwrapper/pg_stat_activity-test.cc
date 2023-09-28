14dfdcbb30489e2ee0fa60c8a30dd3b64b612699
Karthik Ramanathan <kramanathan@yugabyte.com>
2023-09-28 11:55:04 -0700

    [BACKPORT 2.20][#18711] YSQL: Fix pg_stat_activity taking exclusive lock on t-server session
    Summary:
    Original commit: f38b75f11acec5079e942657f83068e3bccffc25 / D28666
    **Background**
    pg_stat_activity issues an RPC (GetActiveTransactionList) to the local t-server to fetch a mapping between currently active sessions and the transactions that these sessions are executing.
    At the local t-server, this RPC results in the execution of the following pseudo-code:
    ```
    acquire exclusive pg_client_service_mutex
    make a shallow copy of active_sessions
    release exclusive pg_client_service_mutex
    
    for session_x in copied sessions:
        acquire exclusive per_session_lock for session_x
        read transaction info from session_x
        release exclusive per_session_lock for session_x
    ```
    
    In a nutshell, the RPC seeks to acquire an exclusive lock on each of the sessions active at the t-server.
    This causes contention on a given session's lock whenever the session is performing active work concurrent to a a pg_stat_activity request.
    This manifests itself particularly during:
      # Long running DML operations (such as CreateTable/Index)
      # The WaitForBackendsCatalogVersion flow, where an exclusive lock is acquired on each of the active sessions at the t-server.
    
    In theory, any operation which acquires a per-session lock at the t-server for a non-trivial duration will experience contention from a concurrently running pg_stat_activity request.
    
    **Fix**
    This revision introduces a transaction cache at the t-server, which stores the mapping between active sessions and their transactions.
    The cache is written to, at the beginning and end of each transaction by the session initiating the transaction under an exclusive lock for the cache.
    The cache is read from, by the session servicing the pg_stat_activity request, under a shared lock for the cache. This alleviates the need for pg_stat_activity to take an exclusive lock on the session.
    Jira: DB-7616
    
    Test Plan:
    ```
    ./yb_build.sh --cxx-test pgwrapper_pg_stat_activity-test --gtest_filter PgStatActivityDelayTest.SlowDDLOperation
    ```
    
    Reviewers: jason, myang, pjain, dmitry
    
    Reviewed By: myang
    
    Subscribers: bogdan, yql, ybase, smishra
    
    Differential Revision: https://phorge.dev.yugabyte.com/D28922

--- src/yb/yql/pgwrapper/pg_stat_activity-test.cc
+++ src/yb/yql/pgwrapper/pg_stat_activity-test.cc
@@ -22,8 +22,8 @@
 #include "yb/util/test_thread_holder.h"
 #include "yb/util/uuid.h"
 
+#include "yb/yql/pgwrapper/libpq_test_base.h"
 #include "yb/yql/pgwrapper/libpq_utils.h"
-#include "yb/yql/pgwrapper/pg_mini_test_base.h"
 
 using namespace std::literals;
 
@@ -31,6 +31,7 @@ namespace yb::pgwrapper {
 namespace {
 
 constexpr auto* kTableName = "t";
+constexpr int kSimulateSlowDDLSecs = 3;
 
 struct TxnInfo {
   TxnInfo(int32_t pid_, Uuid txn_id_) : pid(pid_), txn_id(txn_id_) {}
@@ -39,15 +40,20 @@ struct TxnInfo {
   Uuid txn_id;
 };
 
-class PgStatActivityTest : public PgMiniTestBase {
+class PgStatActivityTest : public LibPqTestBase {
  protected:
   void SetUp() override {
-    PgMiniTestBase::SetUp();
+    LibPqTestBase::SetUp();
     auto conn = ASSERT_RESULT(Connect());
     ASSERT_OK(conn.ExecuteFormat("CREATE TABLE $0 (k INT PRIMARY KEY, v INT)", kTableName));
   }
 
-  size_t NumTabletServers() override { return 1; }
+  int GetNumTabletServers() const override { return 1; }
+
+  void UpdateMiniClusterOptions(ExternalMiniClusterOptions* options) override {
+    options->extra_master_flags.push_back("--replication_factor=1");
+    LibPqTestBase::UpdateMiniClusterOptions(options);
+  }
 
   static Result<TxnInfo> GetTransactionInfo(PGConn* conn) {
     auto opt_txn_id =
@@ -79,6 +85,15 @@ class PgStatActivityTest : public PgMiniTestBase {
   }
 };
 
+class PgStatActivityDelayTest : public PgStatActivityTest {
+ public:
+  void UpdateMiniClusterOptions(ExternalMiniClusterOptions* options) override {
+    options->extra_master_flags.push_back(
+        Format("--TEST_simulate_slow_table_create_secs=$0", kSimulateSlowDDLSecs));
+    PgStatActivityTest::UpdateMiniClusterOptions(options);
+  }
+};
+
 }  // namespace
 
 // The test checks that yb_get_current_transaction returns non-null transaction id in case
@@ -103,6 +118,16 @@ TEST_F(PgStatActivityTest, CurrentTransaction) {
   ASSERT_OK(conn.StartTransaction(IsolationLevel::SERIALIZABLE_ISOLATION));
   ASSERT_OK(conn.FetchFormat("SELECT * FROM $0", kTableName));
   ASSERT_TRUE(ASSERT_RESULT(HasTransactionId(&conn)));
+  ASSERT_OK(conn.RollbackTransaction());
+
+  // Canceled transaction via yb_cancel_transaction()
+  ASSERT_OK(conn.StartTransaction(IsolationLevel::SNAPSHOT_ISOLATION));
+  ASSERT_OK(conn.FetchFormat("SELECT * FROM $0 FOR KEY SHARE", kTableName));
+  auto txn_info = ASSERT_RESULT(GetTransactionInfo(&conn));
+  auto txn_id = ASSERT_RESULT(FullyDecodeTransactionId(txn_info.txn_id.AsSlice()));
+  ASSERT_TRUE(ASSERT_RESULT(
+      conn.FetchValue<bool>(Format("SELECT yb_cancel_transaction(uuid('$0'))", txn_id))));
+  ASSERT_FALSE(ASSERT_RESULT(HasTransactionId(&conn)));
   ASSERT_OK(conn.RollbackTransaction());
 }
 
@@ -169,6 +194,31 @@ TEST_F(PgStatActivityTest, DDLInsideDMLTransaction) {
   ASSERT_EQ(dml_txn_id, ASSERT_RESULT(GetTransactionId(&conn)));
   ASSERT_OK(conn.RollbackTransaction());
   ASSERT_TRUE(ASSERT_RESULT(GetTransactionId(&conn)).IsNil());
+}
+
+// The test checks that the pg_stat_activity function does not get blocked
+// behind a long running DDL operation.
+TEST_F(PgStatActivityDelayTest, SlowDDLOperation) {
+  constexpr auto* kDdlTableName = "kDdl";
+  auto conn = ASSERT_RESULT(Connect());
+  auto aux_conn = ASSERT_RESULT(Connect());
+  CountDownLatch latch(1);
+  TestThreadHolder threads;
+  threads.AddThreadFunctor([&aux_conn, &latch, kDdlTableName] {
+    ASSERT_OK(aux_conn.ExecuteFormat("CREATE TABLE $0 (k INT PRIMARY KEY, v INT)", kDdlTableName));
+    latch.CountDown();
+  });
+
+  // Sleep to ensure that table creation thread has acquired session lock
+  const auto kSleepDuration = MonoDelta::FromMilliseconds(1000);
+  SleepFor(kSleepDuration);
+
+  const auto start = MonoTime::Now();
+  ASSERT_RESULT(conn.Fetch("SELECT pid, yb_backend_xid FROM pg_stat_activity"));
+  const auto end = MonoTime::Now();
+
+  ASSERT_LT((end - start).ToSeconds(), 1);
+  latch.Wait();
 }
 
 }  // namespace yb::pgwrapper
