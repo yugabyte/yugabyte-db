388f2a6d734d4ff4a9e07dfdb31b3698ca61382f
tnayak <tanujnay112@live.com>
2023-10-02 16:42:00 -0700

    [#19384] YSQL: Fix handling of case where a given RowCompareExpression cannot be bound
    Summary:
    Before this change, we treated RowCompareExpression bindings like normal expressions that are to be bound to the PgGate request. RowCompareExpression bindings don't necessarily interfere with other normal expression bindings to the same column. RowCompareExpression bindings on primary range key tuples only set the upper/lower DocKey bounds and don't add anything to the condition or range_partition_values fields of the request proto. YbBindScanKeys takes that into account in this change.
    
    Before this change YbBindScanKeys was mistakenly recording unpushdownable RowCompareExpression's as bound to a particular column (the first column in the row expression) due to a bug in the codepath. `ybScan->all_ordinary_keys_bound` should have also been set to false in such cases to signal the need for a recheck. Because of this, wrong results would be output and in some cases the process would crash in `YbBindScanKeys`.
    Jira: DB-8180
    
    Test Plan:
    ./yb_build.sh --java-test 'org.yb.pgsql.TestPgRegressIndex'
    
    yb_index_scan should pass within TestPgRegressIndex
    
    Reviewers: amartsinchyk, jason
    
    Reviewed By: jason
    
    Subscribers: jason, yql
    
    Differential Revision: https://phorge.dev.yugabyte.com/D29001

--- src/postgres/src/backend/access/yb_access/yb_scan.c
+++ src/postgres/src/backend/access/yb_access/yb_scan.c
@@ -1112,9 +1112,9 @@ YbCheckScanTypes(YbScanDesc ybScan, YbScanPlan scan_plan, int i)
 	       IsPolymorphicType(valtypid);
 }
 
-static void
+static bool
 YbBindRowComparisonKeys(YbScanDesc ybScan, YbScanPlan scan_plan,
-						int skey_index)
+								int skey_index, bool is_for_precheck)
 {
 	int last_att_no = YBFirstLowInvalidAttributeNumber;
 	Relation index = ybScan->index;
@@ -1170,16 +1170,15 @@ YbBindRowComparisonKeys(YbScanDesc ybScan, YbScanPlan scan_plan,
 	 * to push down.
 	 */
 
-	for (int i = 0; i < index->rd_index->indnkeyatts; i++)
+	for (int i = 0; (i < index->rd_index->indnkeyatts) && can_pushdown; i++)
 	{
 		if (index->rd_indoption[i] & INDOPTION_HASH)
 		{
 			can_pushdown = false;
-			break;
 		}
 	}
 
-	if (can_pushdown)
+	if (can_pushdown && !is_for_precheck)
 	{
 
 		YBCPgExpr *col_values =
@@ -1289,6 +1288,8 @@ YbBindRowComparisonKeys(YbScanDesc ybScan, YbScanPlan scan_plan,
 										 is_inclusive));
 		}
 	}
+
+	return can_pushdown;
 }
 
 /*
@@ -1538,14 +1539,21 @@ YbBindScanKeys(YbScanDesc ybScan, YbScanPlan scan_plan, bool is_for_precheck)
 	{
 		length_of_key = YbGetLengthOfKey(&ybScan->keys[i]);
 		ScanKey key = ybScan->keys[i];
-		if (!is_for_precheck)
+		/* Check if this is full key row comparison expression */
+		if (YbIsRowHeader(key) &&
+			!YbIsSearchArray(key))
 		{
-			/* Check if this is full key row comparison expression */
-			if (YbIsRowHeader(key) &&
-				!YbIsSearchArray(key))
-			{
-				YbBindRowComparisonKeys(ybScan, scan_plan, i);
-			}
+			bool can_pushdown =
+				YbBindRowComparisonKeys(ybScan, scan_plan, i, is_for_precheck);
+			ybScan->all_ordinary_keys_bound &= can_pushdown;
+			/*
+			 * Full primary-key RowComparison bindings don't interact
+			 * or interfere with other bindings to the same columns. They
+			 * just set the upper/lower bounds of the requested scan. We
+			 * can just continue looking at the next keys without recording this
+			 * key in the offsets array below.
+			 */
+			continue;
 		}
 
 		/* Check if this is primary columns */
