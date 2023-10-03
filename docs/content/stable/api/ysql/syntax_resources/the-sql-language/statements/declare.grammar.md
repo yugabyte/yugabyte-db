9f16ee99059733b6b059528cf6ec8d9a2ac03030
Rajagopalan Madhavan <rmadhavan@yugabyte.com>
2023-10-03 12:39:27 -0400

    Revert "[BACKPORT 2.18][PLAT-9588]: User cannot reset timezone to default"
    Summary: This reverts commit b1e1c0a0b5f6ed693918517fa88e1beeece5b052.
    
    Test Plan: Please refer to the diff content
    
    Reviewers: #yba-api-review, anijhawan
    
    Reviewed By: #yba-api-review, anijhawan
    
    Subscribers: yugaware
    
    Differential Revision: https://phorge.dev.yugabyte.com/D29034

--- docs/content/stable/api/ysql/syntax_resources/the-sql-language/statements/declare.grammar.md
+++
@@ -1,4 +0,0 @@
-```output.ebnf
-declare ::= DECLARE name [ BINARY ] [ INSENSITIVE ] [ [ NO ] SCROLL ]  
-            CURSOR [ { WITH | WITHOUT } HOLD ] FOR subquery
-```
--- managed/src/main/java/com/yugabyte/yw/controllers/UsersController.java
+++ managed/src/main/java/com/yugabyte/yw/controllers/UsersController.java
@@ -355,7 +355,8 @@ public class UsersController extends AuthenticatedController {
           BAD_REQUEST, "ConnectOnly/ReadOnly/BackupAdmin users can't change their timezone");
     }
 
-    if (!formData.getTimezone().equals(user.getTimezone())) {
+    if (StringUtils.isNotEmpty(formData.getTimezone())
+        && !formData.getTimezone().equals(user.getTimezone())) {
       user.setTimezone(formData.getTimezone());
     }
     if (formData.getRole() != user.getRole()) {
