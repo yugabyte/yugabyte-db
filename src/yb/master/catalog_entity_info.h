yugabyte/yugabyte-db

commit 7176ec67936f0d79636fe1e1f0ead62f312d8890

[#14910] docdb: use multi index container for table map
Summary:
Replaces the std::map used for the main id->table map in the CatalogManager with a `boost::multi_index_container`. The `boost::multi_index_container` is indexed by id and `!IsColocatedUserTable`. This change greatly reduces the amount of time the tablet split manager and load balancer hold the `CatalogManager.mutex_` lock for clusters with vastly more colocated tables than regular tables.

Some possible additional indices we could add:

  # is_deleted to speed up `AreTablesDeleting`
  # namespace_id to speed up `CollectTables`

Timing information:
I ran an experiment on my dev-server (alma8, 14GB of RAM, 4 cores). I created 60k tables in a single tablegroup with this diff (that took 382.5 minutes). I built the yb binaries using:
```
ybd release
```

Highlights:

  - With this diff, the `PrimaryTables` index had 688 tables and the entire data structure had 60688.
  - The load balancer is over an order of magnitude faster. The max time decreased by two orders of magnitude. It is iterating through 2 orders of magnitude fewer tables so this is believable. CopyForTableSplitting is also over an order of magnitude faster.
  - Iterating through the entire data structure is comparable to master. The copy loop is slightly faster, `CatalogManager::ListTables` appears to be slightly slower.

Then I ran yugabyted with:
```
yugabyted start --master_flags "log_min_seconds_to_retain=120,catalog_manager_bg_task_wait_ms=25,load_balancer_initial_delay_secs=5" --tserver_flags "log_min_seconds_to_retain=120,ysql_beta_feature_tablegroup"
```
for 2 minutes, both with this diff and against master. I timed 3 places where we loop through tables: the load balancer, the copy done for tablet splitting, and the `AreTablesDeleting` check. All invoked through the background task.

Times with this diff:
```
RunLoadBalancerWithOptions
avg: < 1ms
max: 1ms

src/yb/master/catalog_entity_info.h
@@ -431,10 +431,19 @@ struct TabletWithSplitPartitions {
 //
 // The non-persistent information about the table is protected by an internal
 // spin-lock.
+//
+// N.B. The catalog manager stores this object in a TableIndex data structure with multiple indices.
+// Any change to the value of the fields indexed need to be registered with the TableIndex or the
+// indices will break. The proper value for the indexed fields needs to be set before the TableInfo
+// is added to the TableIndex.
+//
+// Currently indexed values:
+//     colocated
 class TableInfo : public RefCountedThreadSafe<TableInfo>,
                   public MetadataCowWrapper<PersistentTableInfo> {
  public:
-  explicit TableInfo(TableId table_id, scoped_refptr<TasksTracker> tasks_tracker = nullptr);
+  explicit TableInfo(
+      TableId table_id, bool colocated, scoped_refptr<TasksTracker> tasks_tracker = nullptr);
 
   const TableName name() const;
 
@@ -458,9 +467,6 @@ class TableInfo : public RefCountedThreadSafe<TableInfo>,
   // True if all the column schemas have pg_type_oid set.
   bool has_pg_type_oid() const;
 
-  // True if the table is colocated (including tablegroups, excluding YSQL system tables).
-  bool colocated() const;
-
   std::string matview_pg_table_id() const;
   // True if the table is a materialized view.
   bool is_matview() const;
@@ -481,6 +487,11 @@ class TableInfo : public RefCountedThreadSafe<TableInfo>,
 
   void set_is_system() { is_system_ = true; }
   bool is_system() const { return is_system_; }
+
+  // True if the table is colocated (including tablegroups, excluding YSQL system tables). This is
+  // cached in memory separately from the underlying proto with the expectation it will never
+  // change.
+  bool colocated() const { return colocated_; }
 
   // Return the table type of the table.
   TableType GetTableType() const;
@@ -673,6 +684,8 @@ class TableInfo : public RefCountedThreadSafe<TableInfo>,
   bool is_backfilling_ = false;
 
   std::atomic<bool> is_system_{false};
+
+  const bool colocated_;
 
   // List of pending tasks (e.g. create/alter tablet requests).
   std::unordered_set<std::shared_ptr<server::MonitoredTask>> pending_tasks_ GUARDED_BY(lock_);

src/yb/master/catalog_manager.h
@@ -60,18 +60,19 @@
 #include "yb/master/catalog_manager_util.h"
 #include "yb/master/cdc_split_driver.h"
 #include "yb/master/master_dcl.fwd.h"
-#include "yb/master/master_encryption.fwd.h"
 #include "yb/master/master_defaults.h"
-#include "yb/master/sys_catalog_initialization.h"
+#include "yb/master/master_encryption.fwd.h"
 #include "yb/master/scoped_leader_shared_lock.h"
+#include "yb/master/sys_catalog.h"
+#include "yb/master/sys_catalog_initialization.h"
 #include "yb/master/system_tablet.h"
+#include "yb/master/table_index.h"
 #include "yb/master/tablet_split_candidate_filter.h"
 #include "yb/master/tablet_split_driver.h"
 #include "yb/master/tablet_split_manager.h"
 #include "yb/master/ts_descriptor.h"
 #include "yb/master/ts_manager.h"
 #include "yb/master/ysql_tablespace_manager.h"
-#include "yb/master/sys_catalog.h"
 
 #include "yb/rpc/rpc.h"
 #include "yb/rpc/scheduler.h"
@@ -898,7 +899,7 @@ class CatalogManager : public tserver::TabletPeerLookupIf,
 
   intptr_t tablets_version() const override NO_THREAD_SAFETY_ANALYSIS {
     // This method should not hold the lock, because Version method is thread safe.
-    return tablet_map_.Version() + table_ids_map_.Version();
+    return tablet_map_.Version() + tables_.Version();
   }
 
   intptr_t tablet_locations_version() const override {
@@ -1138,6 +1139,7 @@ class CatalogManager : public tserver::TabletPeerLookupIf,
                              const NamespaceId& namespace_id,
                              const NamespaceName& namespace_name,
                              const std::vector<Partition>& partitions,
+                             bool colocated,
                              IndexInfoPB* index_info,
                              TabletInfos* tablets,
                              CreateTableResponsePB* resp,
@@ -1175,6 +1177,7 @@ class CatalogManager : public tserver::TabletPeerLookupIf,
                                            const PartitionSchema& partition_schema,
                                            const NamespaceId& namespace_id,
                                            const NamespaceName& namespace_name,
+                                           bool colocated,
                                            IndexInfoPB* index_info) REQUIRES(mutex_);
 
   // Helper for creating the initial TabletInfo state.
@@ -1466,6 +1469,7 @@ class CatalogManager : public tserver::TabletPeerLookupIf,
   void ResetTasksTrackers();
   // Aborts all tasks belonging to 'tables' and waits for them to finish.
   void AbortAndWaitForAllTasks(const std::vector<scoped_refptr<TableInfo>>& tables);
+  void AbortAndWaitForAllTasksUnlocked() REQUIRES_SHARED(mutex_);
 
   // Can be used to create background_tasks_ field for this master.
   // Used on normal master startup or when master comes out of the shell mode.
@@ -1515,7 +1519,7 @@ class CatalogManager : public tserver::TabletPeerLookupIf,
   void HandleNewTableId(const TableId& id);
 
   // Creates a new TableInfo object.
-  scoped_refptr<TableInfo> NewTableInfo(TableId id) override;
+  scoped_refptr<TableInfo> NewTableInfo(TableId id, bool colocated) override;
 
   // Register the tablet server with the ts manager using the Raft config. This is called for
   // servers that are part of the Raft config but haven't registered as yet.
@@ -1632,8 +1636,8 @@ class CatalogManager : public tserver::TabletPeerLookupIf,
   // Note: Namespaces and tables for YSQL databases are identified by their ids only and therefore
   // are not saved in the name maps below.
 
-  // Table map: table-id -> TableInfo
-  VersionTracker<TableInfoMap> table_ids_map_ GUARDED_BY(mutex_);
+  // Data structure containing all tables.
+  VersionTracker<TableIndex> tables_ GUARDED_BY(mutex_);
 
   // Table map: [namespace-id, table-name] -> TableInfo
   // Don't have to use VersionTracker for it, since table_ids_map_ already updated at the same time.

ent/src/yb/integration-tests/cdcsdk_ysql-test.cc
@@ -13,7 +13,9 @@
 #include <algorithm>
 #include <chrono>
 #include <utility>
+
 #include <boost/assign.hpp>
+#include <gmock/gmock.h>
 #include <gtest/gtest.h>
 
 #include "yb/cdc/cdc_service.h"
@@ -8373,16 +8375,20 @@ TEST_F(CDCSDKYsqlTest, YB_DISABLE_TEST_IN_TSAN(TestAddColocatedTableToNamespaceW
   CheckTabletsInCDCStateTable(expected_tablet_ids, test_client());
 
   // Wait for a background task cycle to complete.
+  std::sort(expected_table_ids.begin(), expected_table_ids.end());
   auto result = WaitFor(
       [&]() -> Result<bool> {
-        return VERIFY_RESULT(GetCDCStreamTableIds(stream_id)) == expected_table_ids;
+        auto actual_table_ids = VERIFY_RESULT(GetCDCStreamTableIds(stream_id));
+        std::sort(actual_table_ids.begin(), actual_table_ids.end());
+        return actual_table_ids == expected_table_ids;
       },
       MonoDelta::FromSeconds(10) * kTimeMultiplier,
       "Waiting for background task to update cdc streams.");
   EXPECT_OK(result);
   // Extra ASSERT here to get nicely formatted debug information in case of failure.
   if (!result.ok()) {
-    ASSERT_EQ(ASSERT_RESULT(GetCDCStreamTableIds(stream_id)), expected_table_ids);
+    EXPECT_THAT(ASSERT_RESULT(GetCDCStreamTableIds(stream_id)),
+                testing::UnorderedElementsAreArray(expected_table_ids));
   }
 }
 
@@ -8438,8 +8444,12 @@ TEST_F(CDCSDKYsqlTest, YB_DISABLE_TEST_IN_TSAN(TestAddTableToNamespaceWithMultip
   CheckTabletsInCDCStateTable(expected_tablet_ids, test_client(), stream_id_1);
 
   // Check that both the streams metadata has all the 3 table ids.
-  ASSERT_EQ(ASSERT_RESULT(GetCDCStreamTableIds(stream_id)), expected_table_ids);
-  ASSERT_EQ(ASSERT_RESULT(GetCDCStreamTableIds(stream_id_1)), expected_table_ids);
+  ASSERT_THAT(
+      ASSERT_RESULT(GetCDCStreamTableIds(stream_id)),
+      testing::UnorderedElementsAreArray(expected_table_ids));
+  ASSERT_THAT(
+      ASSERT_RESULT(GetCDCStreamTableIds(stream_id_1)),
+      testing::UnorderedElementsAreArray(expected_table_ids));
 }
 
 TEST_F(
@@ -8504,9 +8514,15 @@ TEST_F(
   CheckTabletsInCDCStateTable(expected_tablet_ids, test_client(), stream_id_2);
 
   // Check that both the streams metadata has all the 3 table ids.
-  ASSERT_EQ(ASSERT_RESULT(GetCDCStreamTableIds(stream_id)), expected_table_ids);
-  ASSERT_EQ(ASSERT_RESULT(GetCDCStreamTableIds(stream_id_1)), expected_table_ids);
-  ASSERT_EQ(ASSERT_RESULT(GetCDCStreamTableIds(stream_id_2)), expected_table_ids);
+  ASSERT_THAT(
+      ASSERT_RESULT(GetCDCStreamTableIds(stream_id)),
+      testing::UnorderedElementsAreArray(expected_table_ids));
+  ASSERT_THAT(
+      ASSERT_RESULT(GetCDCStreamTableIds(stream_id_1)),
+      testing::UnorderedElementsAreArray(expected_table_ids));
+  ASSERT_THAT(
+      ASSERT_RESULT(GetCDCStreamTableIds(stream_id_2)),
+      testing::UnorderedElementsAreArray(expected_table_ids));
 }
 
 TEST_F(CDCSDKYsqlTest, YB_DISABLE_TEST_IN_TSAN(TestAddMultipleTableToNamespaceWithActiveStream)) {

ent/src/yb/master/catalog_manager_ent.cc
@@ -341,7 +341,7 @@ class CDCStreamLoader : public Visitor<PersistentCDCStreamInfo> {
         return Status::OK();
       }
     } else {
-      table = FindPtrOrNull(*catalog_manager_->table_ids_map_, metadata.table_id(0));
+      table = catalog_manager_->tables_->FindTableOrNull(metadata.table_id(0));
       if (!table) {
         LOG(ERROR) << "Invalid table ID " << metadata.table_id(0) << " for stream " << stream_id;
         // TODO (#2059): Potentially signals a race condition that table got deleted while stream
@@ -914,7 +914,7 @@ Status CatalogManager::RepackSnapshotsForBackup(ListSnapshotsResponsePB* resp) {
       // in different schema have same name.
       if (entry.type() == SysRowEntryType::TABLE) {
         TRACE("Looking up table");
-        scoped_refptr<TableInfo> table_info = FindPtrOrNull(*table_ids_map_, entry.id());
+        scoped_refptr<TableInfo> table_info = tables_->FindTableOrNull(entry.id());
         if (table_info == nullptr) {
           return STATUS(
               InvalidArgument, "Table not found by ID", entry.id(),
@@ -1085,7 +1085,7 @@ Status CatalogManager::RestoreEntry(const SysRowEntry& entry, const SnapshotId& 
     }
     case SysRowEntryType::TABLE: { // Restore TABLES.
       TRACE("Looking up table");
-      scoped_refptr<TableInfo> table = FindPtrOrNull(*table_ids_map_, entry.id());
+      scoped_refptr<TableInfo> table = tables_->FindTableOrNull(entry.id());
       if (table == nullptr) {
         // Restore Table.
         // TODO: implement
@@ -1850,7 +1850,7 @@ Status CatalogManager::RecreateTable(const NamespaceId& new_namespace_id,
     {
       SharedLock lock(mutex_);
       // Try to find the specified indexed table by id.
-      indexed_table = FindPtrOrNull(*table_ids_map_, req.indexed_table_id());
+      indexed_table = tables_->FindTableOrNull(req.indexed_table_id());
     }
 
     if (indexed_table == nullptr) {
@@ -2129,7 +2129,7 @@ Status CatalogManager::ImportTableEntry(const NamespaceMap& namespace_map,
     TRACE("Looking up table");
     {
       SharedLock lock(mutex_);
-      table = FindPtrOrNull(*table_ids_map_, table_data->old_table_id);
+      table = tables_->FindTableOrNull(table_data->old_table_id);
     }
 
     if (table != nullptr) {
@@ -2196,8 +2196,7 @@ Status CatalogManager::ImportTableEntry(const NamespaceMap& namespace_map,
           }
           SharedLock lock(mutex_);
 
-          for (const auto& entry : *table_ids_map_) {
-            table = entry.second;
+          for (const auto& table : tables_->GetAllTables()) {
 
             if (new_namespace_id != table->namespace_id()) {
               VLOG_WITH_FUNC(3) << "Namespace ids do not match: "
@@ -2217,15 +2216,15 @@ Status CatalogManager::ImportTableEntry(const NamespaceMap& namespace_map,
 
             // Found the new YSQL table by name.
             if (table_data->new_table_id.empty()) {
-              LOG_WITH_FUNC(INFO) << "Found existing table " << entry.first << " for "
+              LOG_WITH_FUNC(INFO) << "Found existing table " << table->id() << " for "
                                   << new_namespace_id << "/" << meta.name() << " (old table "
                                   << table_data->old_table_id << ") with schema "
                                   << table_data->pg_schema_name;
-              table_data->new_table_id = entry.first;
-            } else if (table_data->new_table_id != entry.first) {
+              table_data->new_table_id = table->id();
+            } else if (table_data->new_table_id != table->id()) {
               const string msg = Format(
                   "Found 2 YSQL tables with the same name: $0 - $1, $2",
-                  meta.name(), table_data->new_table_id, entry.first);
+                  meta.name(), table_data->new_table_id, table->id());
               LOG_WITH_FUNC(WARNING) << msg;
               return STATUS(InvalidArgument, msg, MasterError(MasterErrorPB::SNAPSHOT_FAILED));
             }
@@ -2255,7 +2254,7 @@ Status CatalogManager::ImportTableEntry(const NamespaceMap& namespace_map,
   TRACE("Looking up new table");
   {
     SharedLock lock(mutex_);
-    table = FindPtrOrNull(*table_ids_map_, table_data->new_table_id);
+    table = tables_->FindTableOrNull(table_data->new_table_id);
   }
   if (table == nullptr) {
     const string msg = Format("Created table not found: $0", table_data->new_table_id);
@@ -2730,10 +2729,10 @@ void CatalogManager::CleanupHiddenObjects(const ScheduleMinRestoreTime& schedule
   {
     SharedLock lock(mutex_);
     hidden_tablets = hidden_tablets_;
-    tables.reserve(table_ids_map_->size());
-    for (const auto& p : *table_ids_map_) {
-      if (!p.second->is_system()) {
-        tables.push_back(p.second);
+    tables.reserve(tables_->Size());
+    for (const auto& p : tables_->GetAllTables()) {
+      if (!p->is_system()) {
+        tables.push_back(p);
       }
     }
   }
@@ -3851,7 +3850,7 @@ Status CatalogManager::FindCDCSDKStreamsForAddedTables(
     if (ltm->pb.state() == SysCDCStreamEntryPB::ACTIVE) {
       const auto cdcsdk_unprocessed_tables = stream_info->cdcsdk_unprocessed_tables;
       for (const auto& table_id : cdcsdk_unprocessed_tables) {
-        auto table = FindPtrOrNull(*table_ids_map_, table_id);
+        auto table = tables_->FindTableOrNull(table_id);
         Schema schema;
         auto status = table->GetSchema(&schema);
         if (!status.ok()) {
@@ -3899,10 +3898,9 @@ void CatalogManager::FindAllTablesMissingInCDCSDKStream(
   // Get all the tables associated with the namespace.
   // If we find any table present only in the namespace, but not in the namespace, we add the table
   // id to 'cdcsdk_unprocessed_tables'.
-  for (const auto& entry : *table_ids_map_) {
-    auto& table_info = *entry.second;
+  for (const auto& table_info : tables_->GetAllTables()) {
     {
-      auto ltm = table_info.LockForRead();
+      auto ltm = table_info->LockForRead();
       if (!ltm->visible_to_client()) {
         continue;
       }
@@ -3914,7 +3912,7 @@ void CatalogManager::FindAllTablesMissingInCDCSDKStream(
       for (const auto& col : ltm->schema().columns()) {
         if (col.order() == static_cast<int32_t>(PgSystemAttrNum::kYBRowId)) {
           // ybrowid column is added for tables that don't have user-specified primary key.
-          VLOG(1) << "Table: " << table_info.id()
+          VLOG(1) << "Table: " << table_info->id()
                   << ", will not be added to CDCSDK stream, since it does not have a primary key";
           has_pk = false;
           break;
@@ -3925,17 +3923,17 @@ void CatalogManager::FindAllTablesMissingInCDCSDKStream(
       }
     }
 
-    if (IsMatviewTable(table_info)) {
+    if (IsMatviewTable(*table_info)) {
       continue;
     }
-    if (!IsUserTableUnlocked(table_info)) {
+    if (!IsUserTableUnlocked(*table_info)) {
       continue;
     }
 
-    if (!stream_table_ids.contains(table_info.id())) {
-      LOG(INFO) << "Found unprocessed table: " << table_info.id()
+    if (!stream_table_ids.contains(table_info->id())) {
+      LOG(INFO) << "Found unprocessed table: " << table_info->id()
                 << ", for stream: " << stream_info;
-      stream_info->cdcsdk_unprocessed_tables.insert(table_info.id());
+      stream_info->cdcsdk_unprocessed_tables.insert(table_info->id());
     }
   }
 }
@@ -4090,7 +4088,7 @@ void CatalogManager::GetValidTabletsAndDroppedTablesForStream(
     {
       TRACE("Acquired catalog manager lock");
       SharedLock lock(mutex_);
-      table = FindPtrOrNull(*table_ids_map_, table_id);
+      table = tables_->FindTableOrNull(table_id);
     }
     // GetTablets locks lock_ in shared mode.
     if (table) {
@@ -6934,7 +6932,7 @@ Status CatalogManager::UpdateConsumerOnProducerMetadata(
     consumer_table_id = iter->first;
 
     // The destination table should be found or created by now.
-    table = FindPtrOrNull(*table_ids_map_, consumer_table_id);
+    table = tables_->FindTableOrNull(consumer_table_id);
   }
   SCHECK(table, NotFound, Substitute("Missing table id $0", consumer_table_id));
 

commit 259d95522063e2d31450d7aa6c8e061f0f696b15

docdb: Refactor method that cleans up hidden tables.
Summary:
Every time this method is changed in a diff I'm reviewing I struggle to understand the flow. This diff clearly separates the extra work required for colocated tables from the normal flow and does away with the complex lambda passed to `EraseIf`.

This also fixes a deadlock potentially introduced by D21198. `CleanupHiddenTables` does not acquire the write locks in id order, which is the required lock acquisition order when acquiring the write locks of multiple tables.

Test Plan: existing tests

Reviewers: asrivastava, mhaddad

Reviewed By: asrivastava

Subscribers: bogdan, ybase, slingam

Differential Revision: https://phorge.dev.yugabyte.com/D28962

src/yb/master/catalog_entity_info.h
@@ -461,6 +461,10 @@ struct PersistentTableInfo : public Persistent<SysTablesEntryPB, SysRowEntryType
     return started_hiding() || started_deleting();
   }
 
+  bool is_hidden_but_not_deleting() const {
+    return is_hidden() && !started_deleting();
+  }
+
   // Return the table's name.
   const TableName& name() const {
     return pb.name();
@@ -569,6 +573,11 @@ class TableInfo : public RefCountedThreadSafe<TableInfo>,
   bool IsOperationalForClient() const {
     auto l = LockForRead();
     return !l->started_hiding_or_deleting();
+  }
+
+  bool IsHiddenButNotDeleting() const {
+    auto l = LockForRead();
+    return l->is_hidden_but_not_deleting();
   }
 
   // If the table is already hidden then treat it as a duplicate hide request.

src/yb/master/catalog_manager.h
@@ -2120,7 +2120,7 @@ class CatalogManager : public tserver::TabletPeerLookupIf,
   // Tablet maps: tablet-id -> TabletInfo
   VersionTracker<TabletInfoMap> tablet_map_ GUARDED_BY(mutex_);
 
-  // Tablets that was hidden instead of deleting, used to cleanup such tablets when time comes.
+  // Tablets that were hidden instead of deleted. Used to clean up such tablets when they expire.
   std::vector<TabletInfoPtr> hidden_tablets_ GUARDED_BY(mutex_);
 
   // Split parent tablets that are now hidden and still being replicated by some CDC stream. Keep
@@ -2637,6 +2637,13 @@ class CatalogManager : public tserver::TabletPeerLookupIf,
   // Will filter tables content, so pass it by value here.
   void CleanupHiddenTables(
       std::vector<TableInfoPtr> tables, const ScheduleMinRestoreTime& schedule_min_restore_time,
+      const LeaderEpoch& epoch);
+
+  // Checks the colocated table is hidden and is not within the retention of any snapshot schedule
+  // or covered by any live snapshot. If the checks pass sends a request to the relevant tserver
+  // to remove this table from its metadata for the parent tablet.
+  void RemoveHiddenColocatedTableFromTablet(
+      const TableInfoPtr& table, const ScheduleMinRestoreTime& schedule_min_restore_time,
       const LeaderEpoch& epoch);
 
   rpc::Scheduler& Scheduler() override;

src/yb/master/catalog_manager_ext.cc
@@ -2703,55 +2703,74 @@ void CatalogManager::CleanupHiddenTablets(
   }
 }
 
-void CatalogManager::CleanupHiddenTables(
-    std::vector<TableInfoPtr> tables,
-    const ScheduleMinRestoreTime& schedule_min_restore_time,
+void CatalogManager::RemoveHiddenColocatedTableFromTablet(
+    const TableInfoPtr& table, const ScheduleMinRestoreTime& schedule_min_restore_time,
     const LeaderEpoch& epoch) {
-  std::vector<TableInfo::WriteLock> locks;
-  EraseIf([this, &locks, &schedule_min_restore_time, &epoch](const TableInfoPtr& table) {
-    {
-      auto lock = table->LockForRead();
-      // If the table is colocated and hidden then remove it from its colocated tablet if
-      // it has expired.
-      if (lock->is_hidden() && !lock->started_deleting()) {
-        auto tablet_info = table->GetColocatedUserTablet();
-        if (tablet_info) {
-          auto tablet_lock = tablet_info->LockForRead();
-          auto hide_hybrid_time = HybridTime::FromPB(lock->pb.hide_hybrid_time());
-          bool cleanup = !CatalogManagerUtil::RetainTablet(
-              tablet_lock->pb.retained_by_snapshot_schedules(), schedule_min_restore_time,
-              hide_hybrid_time, tablet_info->tablet_id()) &&
-              !snapshot_coordinator_.IsTabletCoveredBySnapshot(tablet_info->tablet_id());
-          if (!cleanup) {
-            return true;
-          }
-          LOG(INFO) << "Cleaning up HIDDEN colocated table " << table->name();
-          auto call = std::make_shared<AsyncRemoveTableFromTablet>(
-              master_, AsyncTaskPool(), tablet_info, table, epoch);
-          table->AddTask(call);
-          WARN_NOT_OK(ScheduleTask(call), "Failed to send RemoveTableFromTablet request");
-          table->ClearTabletMaps();
-        }
-      }
-      if (!lock->is_hidden() || lock->started_deleting() || !table->AreAllTabletsDeleted()) {
-        return true;
-      }
-    }
-    auto lock = table->LockForWrite();
-    if (lock->started_deleting()) {
-      return true;
-    }
-    LOG_WITH_PREFIX(INFO) << "Should delete table: " << AsString(table);
-    lock.mutable_data()->set_state(
-        SysTablesEntryPB::DELETED, Format("Cleanup hidden table at $0", LocalTimeAsString()));
-    locks.push_back(std::move(lock));
-    return false;
-  }, &tables);
-  if (tables.empty()) {
+  auto lock = table->LockForRead();
+  if (!lock->is_hidden_but_not_deleting()) {
     return;
   }
+  auto tablet_info = table->GetColocatedUserTablet();
+  if (!tablet_info) {
+    return;
+  }
+  auto tablet_lock = tablet_info->LockForRead();
+  auto hide_hybrid_time = HybridTime::FromPB(lock->pb.hide_hybrid_time());
+  if (CatalogManagerUtil::RetainTablet(
+          tablet_lock->pb.retained_by_snapshot_schedules(), schedule_min_restore_time,
+          hide_hybrid_time, tablet_info->tablet_id()) ||
+      snapshot_coordinator_.IsTabletCoveredBySnapshot(tablet_info->tablet_id())) {
+    return;
+  }
+  LOG(INFO) << "Removing hidden colocated table " << table->name() << " from its parent tablet";
+  auto call = std::make_shared<AsyncRemoveTableFromTablet>(
+      master_, AsyncTaskPool(), tablet_info, table, epoch);
+  table->AddTask(call);
+  WARN_NOT_OK(ScheduleTask(call), "Failed to send RemoveTableFromTablet request");
+  table->ClearTabletMaps();
+}
 
-  Status s = sys_catalog_->Upsert(epoch, tables);
+void CatalogManager::CleanupHiddenTables(
+    std::vector<TableInfoPtr> tables, const ScheduleMinRestoreTime& schedule_min_restore_time,
+    const LeaderEpoch& epoch) {
+  std::vector<TableInfoPtr> expired_tables;
+  for (auto& table : tables) {
+    if (table->GetColocatedUserTablet() != nullptr) {
+      // Table is colocated and still registered with its parent tablet. Remove it from its parent
+      // tablet's metadata first.
+      RemoveHiddenColocatedTableFromTablet(table, schedule_min_restore_time, epoch);
+    }
+    if (!table->IsHiddenButNotDeleting() || !table->AreAllTabletsDeleted()) {
+      continue;
+    }
+    expired_tables.push_back(std::move(table));
+  }
+  // Sort the expired tables so we acquire write locks in id order. This is the required lock
+  // acquisition order for tables.
+  std::sort(
+      expired_tables.begin(), expired_tables.end(),
+      [](const TableInfoPtr& lhs, const TableInfoPtr& rhs) { return lhs->id() < rhs->id(); });
+  std::vector<TableInfo::WriteLock> locks;
+  for (const auto& table : expired_tables) {
+    auto write_lock = table->LockForWrite();
+    if (write_lock->started_deleting()) {
+      continue;
+    }
+    // Because tablets for hidden tables are deleted first, there is nothing left to delete besides
+    // the table metadata itself now. So we skip the DELETING state and transition directly to
+    // DELETED.
+    write_lock.mutable_data()->set_state(
+        SysTablesEntryPB::DELETED, Format("Cleanup hidden table at $0", LocalTimeAsString()));
+    LOG_WITH_PREFIX(INFO) << Format(
+        "Cleaning up hidden table $0: $1", table->name(), AsString(table));
+    locks.push_back(std::move(write_lock));
+  }
+  if (locks.empty()) {
+    return;
+  }
+  // We skip writes for unmodified sys catalog entries so don't worry about expired tables we
+  // skipped.
+  Status s = sys_catalog_->Upsert(epoch, expired_tables);
   if (!s.ok()) {
     LOG_WITH_PREFIX(WARNING) << "Failed to mark tables as deleted: " << s;
     return;

src/yb/master/catalog_manager_util.cc
@@ -531,10 +531,10 @@ void CatalogManagerUtil::FillTableInfoPB(
 }
 
 bool CatalogManagerUtil::RetainTablet(
-    const google::protobuf::RepeatedPtrField<std::string>& retaininng_snapshot_schedules,
+    const google::protobuf::RepeatedPtrField<std::string>& retaining_snapshot_schedules,
     const ScheduleMinRestoreTime& schedule_to_min_restore_time,
     HybridTime hide_hybrid_time, const TabletId& tablet_id) {
-  for (const auto& schedule_id_str : retaininng_snapshot_schedules) {
+  for (const auto& schedule_id_str : retaining_snapshot_schedules) {
     auto schedule_id = TryFullyDecodeSnapshotScheduleId(schedule_id_str);
     auto it = schedule_to_min_restore_time.find(schedule_id);
     // If schedule is not present in schedule_min_restore_time then it means that schedule

src/yb/master/catalog_manager_util.h
@@ -179,7 +179,7 @@ class CatalogManagerUtil {
       tablet::TableInfoPB* pb);
 
   static bool RetainTablet(
-      const google::protobuf::RepeatedPtrField<std::string>& retaininng_snapshot_schedules,
+      const google::protobuf::RepeatedPtrField<std::string>& retaining_snapshot_schedules,
       const ScheduleMinRestoreTime& schedule_to_min_restore_time,
       HybridTime hide_hybrid_time, const TabletId& tablet_id);
 