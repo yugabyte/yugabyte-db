0f0522eb0e4605d9241939af9f17de8f5f8aa59e
Naorem Khogendro Singh <nsingh@yugabyte.com>
2023-10-04 13:00:18 -0700

    [PLAT-10628] Fix for Missing Node UUID causes certain tasks to fail in new YBA releases.
    Summary: This diff uses raw JSON instead of depending on the model classes as suggested by Alex.
    
    Test Plan:
    Manually created a universe, removed the UUID for some nodes, removed the migration. Restarted platform with the migration to see the UUIDs populated.
    
    Verified manually for on-prem universes.
    
    Reviewers: amalyshev
    
    Reviewed By: amalyshev
    
    Subscribers: yugaware
    
    Differential Revision: https://phorge.dev.yugabyte.com/D29076

--- managed/src/main/java/db/migration/default_/common/V299__Set_Universe_Node_UUID.java
+++ managed/src/main/java/db/migration/default_/common/V299__Set_Universe_Node_UUID.java
@@ -2,18 +2,16 @@
 
 package db.migration.default_.common;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonNode;
-import com.yugabyte.yw.commissioner.Common.CloudType;
+import com.fasterxml.jackson.databind.node.ObjectNode;
 import com.yugabyte.yw.common.Util;
-import com.yugabyte.yw.forms.UniverseDefinitionTaskParams;
-import com.yugabyte.yw.forms.UniverseDefinitionTaskParams.Cluster;
-import com.yugabyte.yw.models.NodeInstance;
-import com.yugabyte.yw.models.helpers.NodeDetails;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
 import java.util.UUID;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.StringUtils;
@@ -24,9 +22,52 @@ import play.libs.Json;
 @Slf4j
 public class V299__Set_Universe_Node_UUID extends BaseJavaMigration {
 
+  private Map<String, UUID> getNodeInstanceNameToUuidMap(Connection connection)
+      throws SQLException {
+    Map<String, UUID> nodeInstances = new HashMap<>();
+    ResultSet nodeInstanceRs =
+        connection
+            .createStatement()
+            .executeQuery("SELECT node_uuid, node_name from node_instance where in_use = true");
+    while (nodeInstanceRs.next()) {
+      UUID nodeUuid = UUID.fromString(nodeInstanceRs.getString("node_uuid"));
+      String nodeName = nodeInstanceRs.getString("node_name");
+      if (StringUtils.isBlank(nodeName)) {
+        log.warn("Node {} is being used but has no name", nodeUuid);
+        continue;
+      }
+      nodeInstances.put(nodeName, nodeUuid);
+    }
+    return nodeInstances;
+  }
+
+  private Map<String, String> getClusterUuidToProviderTypeMap(JsonNode universeDetailsJson) {
+    Map<String, String> clusterUuidToProviderTypeMap = new HashMap<>();
+    JsonNode clusters = universeDetailsJson.get("clusters");
+    Iterator<JsonNode> iter = clusters.iterator();
+    while (iter.hasNext()) {
+      JsonNode clusterNode = iter.next();
+      String clusterUuidStr = clusterNode.get("uuid").asText();
+      JsonNode userIntentNode = clusterNode.get("userIntent");
+      if (userIntentNode == null || userIntentNode.isNull()) {
+        log.warn("User intent is not found for cluster {}", clusterUuidStr);
+        continue;
+      }
+      JsonNode providerTypeNode = userIntentNode.get("providerType");
+      if (providerTypeNode == null || providerTypeNode.isNull()) {
+        log.warn("Provider type is not set in the user intent for cluster {}", clusterUuidStr);
+        continue;
+      }
+
+      clusterUuidToProviderTypeMap.put(clusterUuidStr, providerTypeNode.asText());
+    }
+    return clusterUuidToProviderTypeMap;
+  }
+
   @Override
   public void migrate(Context context) throws SQLException {
     Connection connection = context.getConnection();
+    Map<String, UUID> nodeInstanceNameToUuidMap = getNodeInstanceNameToUuidMap(connection);
     PreparedStatement updateUniverseDetailsStmt =
         connection.prepareStatement(
             "UPDATE universe SET universe_details_json = ? WHERE universe_uuid = ?");
@@ -38,48 +79,53 @@ public class V299__Set_Universe_Node_UUID extends BaseJavaMigration {
       boolean fixNeeded = false;
       UUID universeUuid = UUID.fromString(universeRs.getString("universe_uuid"));
       String universeDetailsJson = universeRs.getString("universe_details_json");
-      UniverseDefinitionTaskParams universeDetails;
-      try {
-        universeDetails =
-            Json.mapper().readValue(universeDetailsJson, UniverseDefinitionTaskParams.class);
-      } catch (JsonProcessingException e) {
-        // Extra caution against invalid JSON. It must not happen.
-        log.warn("Invalid universe details found for universe UUID " + universeUuid, e);
+      JsonNode node = Json.parse(universeDetailsJson);
+      if (node == null || node.isNull()) {
+        log.warn("Invalid universe details found for universe UUID {}", universeUuid);
         continue;
       }
-      for (Cluster cluster : universeDetails.clusters) {
-        if (cluster.userIntent == null) {
+      Map<String, String> clusterUuidToProviderTypeMap = getClusterUuidToProviderTypeMap(node);
+      ObjectNode objectNode = node.deepCopy();
+      JsonNode nodeDetailsSetNode = objectNode.get("nodeDetailsSet");
+      if (nodeDetailsSetNode == null || nodeDetailsSetNode.isNull()) {
+        log.warn("Invalid node details set for universe {}", universeUuid);
+        continue;
+      }
+      Iterator<JsonNode> iter = nodeDetailsSetNode.elements();
+      while (iter.hasNext()) {
+        ObjectNode nodeDetails = (ObjectNode) iter.next();
+        if (nodeDetails.has("nodeUuid") || !nodeDetails.has("nodeName")) {
+          // Node which has not been added does not have a name. As node UUID and name are set in
+          // transaction, both are either set or unset together.
           continue;
         }
-        for (NodeDetails nodeDetails : universeDetails.getNodesInCluster(cluster.uuid)) {
-          if (nodeDetails.nodeUuid != null || StringUtils.isBlank(nodeDetails.nodeName)) {
-            // Node name can be null if the node is in ToBeAdded state. As node name and UUID are
-            // set in transaction, both must be set together.
-            continue;
-          }
-          if (cluster.userIntent.providerType == CloudType.onprem) {
-            NodeInstance instance = NodeInstance.getByName(nodeDetails.nodeName);
-            if (!instance.isInUse()) {
-              log.warn(
-                  "Onprem node {} is referenced in universe {}, but is not marked in-use",
-                  nodeDetails.nodeName,
-                  universeUuid);
-              // This must not happen if the name is set.
-              continue;
-            }
-            nodeDetails.nodeUuid = instance.getNodeUuid();
-          } else {
-            nodeDetails.nodeUuid = Util.generateNodeUUID(universeUuid, nodeDetails.nodeName);
-          }
-          fixNeeded = true;
+        String nodeName = nodeDetails.get("nodeName").asText();
+        JsonNode placementUuidNode = nodeDetails.get("placementUuid");
+        if (placementUuidNode == null || placementUuidNode.isNull()) {
+          log.warn("Placement UUID is not found for node {}", nodeName);
+          continue;
         }
+        String providerType = clusterUuidToProviderTypeMap.get(placementUuidNode.asText());
+        if (StringUtils.isBlank(providerType)) {
+          log.warn("Provider type is not found for node {}", nodeName);
+          continue;
+        }
+        UUID nodeUuid =
+            "onprem".equals(providerType)
+                ? nodeInstanceNameToUuidMap.get(nodeName)
+                : Util.generateNodeUUID(universeUuid, nodeName);
+        if (nodeUuid == null) {
+          log.warn("Node UUID is not found for node {}", nodeName);
+          continue;
+        }
+        nodeDetails.set("nodeUuid", Json.toJson(nodeUuid));
+        fixNeeded = true;
       }
       if (!fixNeeded) {
         continue;
       }
       log.info("Fixing node UUID for universe {}", universeUuid);
-      JsonNode jsonNode = Json.toJson(universeDetails);
-      updateUniverseDetailsStmt.setString(1, Json.stringify(jsonNode));
+      updateUniverseDetailsStmt.setString(1, Json.stringify(objectNode));
       updateUniverseDetailsStmt.setObject(2, universeUuid);
       updateUniverseDetailsStmt.executeUpdate();
     }
