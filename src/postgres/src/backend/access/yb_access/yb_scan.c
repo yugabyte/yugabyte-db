yugabyte/yugabyte-db

commit 6d19303d4c67e8521f2535ab75184c70da5a5089

[MERGE PG15] Access fixes
Summary:
Summary of the changes:

  # PG15 commutes `RestrictInfo.clause` if the variable is on the right (for instance, in query `select * from emp where 1 = id;`). For implementation, search `commrinfo = commute_restrictinfo(rinfo, comm_op);` in `indxpath.c`. As a consequence function `deconstruct_indexquals` and struct `IndexQualInfo` have been removed. This diff handles this change for YB.

  # Instead of maintaining `is_hashed` state (`IndexQualInfo` is removed), it is converted into a helper function. See `is_hashed` in `yb_scan.c`. This is open for review.

  # Handles type change of `ReservoirStateData.randstate` from array to a struct of two `uint64`s.

Test Plan: Jenkins: rebase: no

Reviewers: smishra, mihnea, neil, tnayak

Reviewed By: tnayak

Subscribers: yql, ybase

Differential Revision: https://phorge.dev.yugabyte.com/D27737

src/postgres/src/backend/access/yb_access/yb_scan.c
@@ -42,8 +42,10 @@
 #include "catalog/pg_type.h"
 #include "miscadmin.h"
 #include "access/relation.h"
+#include "nodes/nodeFuncs.h"
 #include "optimizer/cost.h"
 #include "optimizer/optimizer.h"
+#include "optimizer/paths.h"
 #include "utils/datum.h"
 #include "utils/rel.h"
 #include "utils/lsyscache.h"
@@ -138,14 +140,6 @@ static void ybcLoadTableInfo(Relation relation, YbScanPlan scan_plan)
 	{
 		ybcCheckPrimaryKeyAttribute(scan_plan, ybc_table_desc, attnum);
 	}
-
-#ifdef NEIL_OID
-	/* OID is now a regular column */
-	if (relation->rd_rel->relhasoids)
-	{
-		ybcCheckPrimaryKeyAttribute(scan_plan, ybc_table_desc, ObjectIdAttributeNumber);
-	}
-#endif
 }
 
 static Oid ybc_get_atttypid(TupleDesc bind_desc, AttrNumber attnum)
@@ -443,14 +437,6 @@ static HeapTuple ybcFetchNextHeapTuple(YbScanDesc ybScan, bool is_forward_scan)
 	if (has_data)
 	{
 		tuple = heap_form_tuple(tupdesc, values, nulls);
-
-#ifdef NEIL_OID
-		/* OID is now a regular column */
-		if (syscols.oid != InvalidOid)
-		{
-			HeapTupleSetOid(tuple, syscols.oid);
-		}
-#endif
 		if (syscols.ybctid != NULL)
 		{
 			HEAPTUPLE_YBCTID(tuple) = PointerGetDatum(syscols.ybctid);
@@ -1946,15 +1932,6 @@ ybcSetupTargets(YbScanDesc ybScan, YbScanPlan scan_plan, Scan *pg_scan_plan)
 		}
 	}
 
-#ifdef NEIL_OID
-	/* OID is now a regular column */
-	if (scan_plan->target_relation->rd_rel->relhasoids)
-	{
-		YbDmlAppendTargetSystem(ObjectIdAttributeNumber, ybScan->handle);
-		target_added = true;
-	}
-#endif
-
 	if (is_index_only_scan)
 	{
 		/*
@@ -2687,18 +2664,12 @@ void ybcCostEstimate(RelOptInfo *baserel, Selectivity selectivity,
 	*total_cost   = *startup_cost + cost_per_tuple * baserel->tuples * selectivity;
 }
 
-#ifdef YB_TODO
-/* YB_TODO(ram kannan@yugabyte) Reimplement cost functions.
- * - PG13 removed IndexQualInfo.
- * - PG13 introduced IndexClause.
- * - Yugabyte needs to reimplement the cost calculation using IndexClause.
- */
 /*
  * Evaluate the selectivity for yb_hash_code qualifiers.
  * Returns 1.0 if there are no yb_hash_code comparison expressions for this
  * index.
  */
-static double ybcEvalHashSelectivity(List *hashed_qinfos)
+static double ybcEvalHashSelectivity(List *hashed_rinfos)
 {
 	bool greatest_set = false;
 	int greatest = 0;
@@ -2708,13 +2679,16 @@ static double ybcEvalHashSelectivity(List *hashed_qinfos)
 	double selectivity;
 	ListCell * lc;
 
-	foreach(lc, hashed_qinfos)
+	foreach(lc, hashed_rinfos)
 	{
-		IndexQualInfo *qinfo = (IndexQualInfo *) lfirst(lc);
-		RestrictInfo *rinfo = qinfo->rinfo;
+		RestrictInfo *rinfo = (RestrictInfo *) lfirst(lc);
 		Expr	   *clause = rinfo->clause;
 
-		if (!IsA(qinfo->other_operand, Const))
+		Assert(IsA(clause, OpExpr));
+		OpExpr	   *op = (OpExpr *) clause;
+		Node *other_operand = (Node *) lsecond(op->args);
+
+		if (!IsA(other_operand, Const))
 		{
 			continue;
 		}
@@ -2729,7 +2703,7 @@ static double ybcEvalHashSelectivity(List *hashed_qinfos)
 								   &lefttype,
 								   &righttype);
 
-		int signed_val = ((Const*) qinfo->other_operand)->constvalue;
+		int signed_val = ((Const*) other_operand)->constvalue;
 		signed_val = signed_val < 0 ? 0 : signed_val;
 		uint32_t val = signed_val > USHRT_MAX ? USHRT_MAX : signed_val;
 
@@ -2783,7 +2757,6 @@ static double ybcEvalHashSelectivity(List *hashed_qinfos)
 #endif
 	return selectivity;
 }
-#endif
 
 /*
  * Evaluate the selectivity for some qualified cols given the hash and primary key cols.
@@ -2826,16 +2799,48 @@ Oid ybc_get_attcollation(TupleDesc desc, AttrNumber attnum)
 	return attnum > 0 ? TupleDescAttr(desc, attnum - 1)->attcollation : InvalidOid;
 }
 
+bool
+yb_is_hashed(Expr *clause, IndexOptInfo *index)
+{
+	bool is_hashed = false;
+	Node	   *leftop;
+
+	if (IsA(clause, OpExpr))
+	{
+		leftop = get_leftop(clause);
+		if (IsA(leftop, FuncExpr))
+		{
+			is_hashed =
+				(((FuncExpr *) leftop)->funcid == YB_HASH_CODE_OID);
+			ListCell *ls;
+			if (is_hashed)
+			{
+				/*
+				 * YB: We aren't going to push down a yb_hash_code call
+				 * if we matched the call against an expression
+				 */
+				foreach (ls, index->indexprs)
+				{
+					Node *indexpr = (Node *) lfirst(ls);
+					if (indexpr && IsA(indexpr, RelabelType))
+						indexpr = (Node *) ((RelabelType *) indexpr)->arg;
+					if (equal(indexpr, leftop))
+					{
+						is_hashed = false;
+						break;
+					}
+				}
+			}
+		}
+	}
+	return is_hashed;
+}
+
+
 void ybcIndexCostEstimate(struct PlannerInfo *root, IndexPath *path,
 						  Selectivity *selectivity, Cost *startup_cost,
 						  Cost *total_cost)
 {
-	/* YB_TODO(ram kannan@yugabyte) Reimplement cost functions.
-	 * - PG13 removed IndexQualInfo.
-	 * - PG13 introduced IndexClause.
-	 * - Yugabyte needs to reimplement the cost calculation using IndexClause.
-	 */
-
 	Relation	index = RelationIdGetRelation(path->indexinfo->indexoid);
 	bool		isprimary = index->rd_index->indisprimary;
 	Relation	relation = isprimary ? RelationIdGetRelation(index->rd_index->indrelid) : NULL;
@@ -2845,9 +2850,7 @@ void ybcIndexCostEstimate(struct PlannerInfo *root, IndexPath *path,
 	bool        is_unique = index->rd_index->indisunique;
 	bool        is_partial_idx = path->indexinfo->indpred != NIL && path->indexinfo->predOK;
 	Bitmapset  *const_quals = NULL;
-	#ifdef YB_TODO
-	List	   *hashed_qinfos = NIL;
-	#endif
+	List	   *hashed_rinfos = NIL;
 	List	   *clauses = NIL;
 	double 		baserel_rows_estimate;
 
@@ -2865,11 +2868,14 @@ void ybcIndexCostEstimate(struct PlannerInfo *root, IndexPath *path,
 	{
 		IndexClause *iclause = lfirst_node(IndexClause, lc);
 		int			indexcol = iclause->indexcol;
-		ListCell   *lc2;
+		ListCell *lc2, *lci;
+		List* indexcols = iclause->indexcols == NIL ? list_make1_int(indexcol) : iclause->indexcols;
 
-		foreach(lc2, iclause->indexquals)
+		Assert(iclause->indexquals->length == indexcols->length);
+		forboth(lc2, iclause->indexquals, lci, indexcols)
 		{
 			RestrictInfo *rinfo = lfirst_node(RestrictInfo, lc2);
+			int			  indexcol = lfirst_int(lci);
 			AttrNumber	 attnum = isprimary ? index->rd_index->indkey.values[indexcol]
 											: (indexcol + 1);
 			Expr	   *clause = rinfo->clause;
@@ -2894,28 +2900,23 @@ void ybcIndexCostEstimate(struct PlannerInfo *root, IndexPath *path,
 				}
 			}
 
-			#ifdef YB_TODO
-			if (qinfo->is_hashed)
+			if (yb_is_hashed(clause, path->indexinfo))
 			{
-				hashed_qinfos = lappend(hashed_qinfos, qinfo);
+				hashed_rinfos = lappend(hashed_rinfos, rinfo);
 			}
 			else
 			{
 				clauses = lappend(clauses, rinfo);
 			}
-			#endif
-			clauses = lappend(clauses, rinfo);
 		}
 	}
-	#ifdef YB_TODO
-	if (hashed_qinfos != NIL)
+	if (hashed_rinfos != NIL)
 	{
-		*selectivity = ybcEvalHashSelectivity(hashed_qinfos);
+		*selectivity = ybcEvalHashSelectivity(hashed_rinfos);
 		baserel_rows_estimate = baserel->tuples * (*selectivity);
 	}
 	else
 	{
-	#endif
 		if (yb_enable_optimizer_statistics)
 		{
 			*selectivity = clauselist_selectivity(root /* PlannerInfo */,
@@ -2937,9 +2938,7 @@ void ybcIndexCostEstimate(struct PlannerInfo *root, IndexPath *path,
 														scan_plan.primary_key);
 			baserel_rows_estimate = baserel->tuples * (*selectivity);
 		}
-	#ifdef YB_TODO
 	}
-	#endif
 
 	path->path.rows = baserel_rows_estimate;
 
@@ -3003,14 +3002,6 @@ YbFetchRowData(YBCPgStatement ybc_stmt, Relation relation, ItemPointer tid,
 	/*
 	 * Set up the scan targets. For index-based scan we need to return all "real" columns.
 	 */
-#ifdef YB_TODO
-	/* YB_TODO(neil@yugabyte)
-	 * - OID is now a regular column.
-	 * - Yugabyte code might need modification when selecting column 'oid'
-	 */
-	if (RelationGetForm(relation)->relhasoids)
-		YbDmlAppendTargetSystem(ObjectIdAttributeNumber, ybc_stmt);
-#endif
 	for (AttrNumber attnum = 1; attnum <= tupdesc->natts; attnum++)
 	{
 		if (!TupleDescAttr(tupdesc, attnum - 1)->attisdropped)
@@ -3041,9 +3032,11 @@ YbFetchTableSlot(Relation relation, ItemPointer tid,  TupleTableSlot *slot)
 	bool has_data = false;
 	YBCPgStatement ybc_stmt;
 
+	Assert(slot != NULL);
+	Assert(slot->tts_values != NULL);
+	Assert(slot->tts_isnull != NULL);
+
 	TupleDesc tupdesc = RelationGetDescr(relation);
-	slot->tts_values = (Datum *) palloc0(tupdesc->natts * sizeof(Datum));
-	slot->tts_isnull  = (bool *) palloc(tupdesc->natts * sizeof(bool));
 	YBCPgSysColumns syscols;
 
 	/* Read data */
@@ -3066,11 +3059,6 @@ YbFetchTableSlot(Relation relation, ItemPointer tid,  TupleTableSlot *slot)
 		}
 	}
 
-	#ifdef YB_TODO
-	/* YB_TODO(neil): pfree values and nulls at appropriate place. */
-	pfree(values);
-	pfree(nulls);
-	#endif
 	YBCPgDeleteStatement(ybc_stmt);
 	return has_data;
 }
@@ -3098,16 +3086,6 @@ YbFetchHeapTuple(Relation relation, ItemPointer tid, HeapTuple* tuple)
 	if (has_data)
 	{
 		*tuple = heap_form_tuple(tupdesc, values, nulls);
-#ifdef YB_TODO
-		/* YB_TODO(neil@yugabyte)
-		 * - OID is now a regular column.
-		 * - Yugabyte code might need modification when selecting column 'oid'
-		 */
-		if (syscols.oid != InvalidOid)
-		{
-			HeapTupleSetOid(tuple, syscols.oid);
-		}
-#endif
 		(*tuple)->t_tableOid = RelationGetRelid(relation);
 		if (syscols.ybctid != NULL)
 		{
@@ -3232,15 +3210,6 @@ ybBeginSample(Relation rel, int targrows)
 								  targrows,
 								  YBCIsRegionLocal(rel),
 								  &ybSample->handle));
-
-#ifdef YB_TODO
-	/* YB_TODO(neil) OID is now a regular column */
-	/*
-	 * Set up the scan targets. We need to return all "real" columns.
-	 */
-	if (RelationGetForm(ybSample->relation)->relhasoids)
-		YbDmlAppendTargetSystem(ObjectIdAttributeNumber, ybSample->handle);
-#endif
 	for (AttrNumber attnum = 1; attnum <= tupdesc->natts; attnum++)
 	{
 		if (!TupleDescAttr(tupdesc, attnum - 1)->attisdropped)
@@ -3251,12 +3220,10 @@ ybBeginSample(Relation rel, int targrows)
 	 * Initialize sampler random state
 	 */
 	reservoir_init_selection_state(&rstate, targrows);
-#ifdef YB_TODO
-	/* YB_TODO(neil) Random state is no longer an array */
 	HandleYBStatus(YBCPgInitRandomState(ybSample->handle,
 										rstate.W,
-										SamplerRandomStateToUint64(rstate.randstate)));
-#endif
+										rstate.randstate.s0,
+										rstate.randstate.s1));
 
 	return ybSample;
 }
@@ -3326,13 +3293,6 @@ ybFetchSample(YbSample ybSample, HeapTuple *rows)
 
 		/* Make a heap tuple in current memory context */
 		rows[numrows] = heap_form_tuple(tupdesc, values, nulls);
-#ifdef NEIL_OID
-		/* OID is now a regular column */
-		if (syscols.oid != InvalidOid)
-		{
-			HeapTupleSetOid(rows[numrows], syscols.oid);
-		}
-#endif
 		if (syscols.ybctid != NULL)
 		{
 			HEAPTUPLE_YBCTID(rows[numrows]) = PointerGetDatum(syscols.ybctid);
@@ -3399,7 +3359,7 @@ ybFetchNext(YBCPgStatement handle,
 			MemoryContextSwitchTo(oldcontext);
 		}
 #endif
+		slot->tts_tableOid = relid;
 	}
-
 	return slot;
 }

src/postgres/src/include/access/yb_scan.h
@@ -140,7 +140,7 @@ typedef struct YbScanDescData
 	bool quit_scan;
 } YbScanDescData;
 
-/* TODO(neil) Reorganize code so that description can be placed at appropriate places.
+/* YB_TODO(neil) Reorganize code so that description can be placed at appropriate places.
 typedef struct YbScanDescData *YbScanDesc;
  */
 

src/postgres/src/backend/access/common/reloptions.c
@@ -1011,28 +1011,6 @@ add_int_reloption(bits32 kinds, const char *name, const char *desc, int default_
 	add_reloption((relopt_gen *) newoption);
 }
 
-#ifdef YB_TODO
-/* YB_TODO(alex@yugabyte) This function is no longer needed? */
-/*
- * add_oid_reloption
- *		Add a new OID reloption
- */
-void
-add_oid_reloption(bits32 kinds, const char *name, const char *desc, Oid default_val,
-				  Oid min_val, Oid max_val)
-{
-	relopt_oid *newoption;
-
-	newoption = (relopt_oid *) allocate_reloption(kinds, RELOPT_TYPE_OID,
-												  name, desc);
-	newoption->default_val = default_val;
-	newoption->min = min_val;
-	newoption->max = max_val;
-
-	add_reloption((relopt_gen *) newoption);
-}
-#endif
-
 /*
  * add_local_int_reloption
  *		Add a new local integer reloption

src/postgres/src/backend/access/common/tupconvert.c
@@ -134,142 +134,6 @@ convert_tuples_by_name(TupleDesc indesc,
 	return map;
 }
 
-#ifdef YB_TODO
-/* YB_TODO(neil) This function is no longer in Pg15 */
-/*
- * Return a palloc'd bare attribute map for tuple conversion, matching input
- * and output columns by name.  (Dropped columns are ignored in both input and
- * output.)  This is normally a subroutine for convert_tuples_by_name, but can
- * be used standalone.
- */
-AttrNumber *
-convert_tuples_by_name_map(TupleDesc indesc, TupleDesc outdesc, const char *msg,
-						   bool yb_ignore_type_mismatch)
-{
-	AttrNumber *attrMap;
-	int			n;
-	int			i;
-
-	n = outdesc->natts;
-	attrMap = (AttrNumber *) palloc0(n * sizeof(AttrNumber));
-	for (i = 0; i < n; i++)
-	{
-		Form_pg_attribute outatt = TupleDescAttr(outdesc, i);
-		char	   *attname;
-		Oid			atttypid;
-		int32		atttypmod;
-		int			j;
-
-		if (outatt->attisdropped)
-			continue;			/* attrMap[i] is already 0 */
-		attname = NameStr(outatt->attname);
-		atttypid = outatt->atttypid;
-		atttypmod = outatt->atttypmod;
-		for (j = 0; j < indesc->natts; j++)
-		{
-			Form_pg_attribute inatt = TupleDescAttr(indesc, j);
-
-			if (inatt->attisdropped)
-				continue;
-			if (strcmp(attname, NameStr(inatt->attname)) == 0)
-			{
-				/* Found it, check type */
-				if (!(IsYugaByteEnabled() && yb_ignore_type_mismatch) &&
-					(atttypid != inatt->atttypid ||
-					 atttypmod != inatt->atttypmod))
-					ereport(ERROR,
-							(errcode(ERRCODE_DATATYPE_MISMATCH),
-							 errmsg_internal("%s", _(msg)),
-							 errdetail("Attribute \"%s\" of type %s does not match corresponding attribute of type %s.",
-									   attname,
-									   format_type_be(outdesc->tdtypeid),
-									   format_type_be(indesc->tdtypeid))));
-				attrMap[i] = (AttrNumber) (j + 1);
-				break;
-			}
-		}
-		if (attrMap[i] == 0)
-			ereport(ERROR,
-					(errcode(ERRCODE_DATATYPE_MISMATCH),
-					 errmsg_internal("%s", _(msg)),
-					 errdetail("Attribute \"%s\" of type %s does not exist in type %s.",
-							   attname,
-							   format_type_be(outdesc->tdtypeid),
-							   format_type_be(indesc->tdtypeid))));
-	}
-
-	return attrMap;
-}
-
-/*
- * Returns mapping created by convert_tuples_by_name_map, or NULL if no
- * conversion not required. This is a convenience routine for
- * convert_tuples_by_name() and other functions.
- */
-AttrNumber *
-convert_tuples_by_name_map_if_req(TupleDesc indesc,
-								  TupleDesc outdesc,
-								  const char *msg)
-{
-	AttrNumber *attrMap;
-	int			n = outdesc->natts;
-	int			i;
-	bool		same;
-
-	/* Verify compatibility and prepare attribute-number map */
-	attrMap = convert_tuples_by_name_map(indesc, outdesc, msg,
-										 false /* yb_ignore_type_mismatch */);
-
-	/*
-	 * Check to see if the map is one-to-one, in which case we need not do a
-	 * tuple conversion.  We must also insist that both tupdescs either
-	 * specify or don't specify an OID column, else we need a conversion to
-	 * add/remove space for that.  (For some callers, presence or absence of
-	 * an OID column perhaps would not really matter, but let's be safe.)
-	 */
-	if (indesc->natts == outdesc->natts &&
-		indesc->tdhasoid == outdesc->tdhasoid)
-	{
-		same = true;
-		for (i = 0; i < n; i++)
-		{
-			Form_pg_attribute inatt;
-			Form_pg_attribute outatt;
-
-			if (attrMap[i] == (i + 1))
-				continue;
-
-			/*
-			 * If it's a dropped column and the corresponding input column is
-			 * also dropped, we needn't convert.  However, attlen and attalign
-			 * must agree.
-			 */
-			inatt = TupleDescAttr(indesc, i);
-			outatt = TupleDescAttr(outdesc, i);
-			if (attrMap[i] == 0 &&
-				inatt->attisdropped &&
-				inatt->attlen == outatt->attlen &&
-				inatt->attalign == outatt->attalign)
-				continue;
-
-			same = false;
-			break;
-		}
-	}
-	else
-		same = false;
-
-	if (same)
-	{
-		/* Runtime conversion is not needed */
-		pfree(attrMap);
-		return NULL;
-	}
-	else
-		return attrMap;
-}
-#endif
-
 /*
  * Perform conversion of a tuple according to the map.
  */
@@ -373,7 +237,6 @@ execute_attr_map_slot(AttrMap *attrMap,
  *
  * Note: Postgres uses FirstLowInvalidHeapAttributeNumber.
  */
-/* YB_TODO(neil) Need to choose between attrMap and map when fixing compiling errors */
 Bitmapset *
 execute_attr_map_cols(AttrMap *attrMap, Bitmapset *in_cols, Relation rel)
 {

src/postgres/src/backend/access/heap/heapam_handler.c
@@ -1290,7 +1290,8 @@ heapam_index_build_range_scan(Relation heapRelation,
 		snapshot = scan->rs_snapshot;
 	}
 
-	/* YB_TODO(arpan): scan can be instance of YBScanDesc as well. */
+	/* YB_TODO(arpan): scan can be instance of YBScanDesc as well. hscan is only
+	 * used in non YB code path except for one place. See hscan usages. */
 	hscan = (HeapScanDesc) scan;
 
 	/*

src/postgres/src/backend/access/yb_access/yb_lsm.c
@@ -527,10 +527,6 @@ ybcingettuple(IndexScanDesc scan, ScanDirection dir)
 		}
 	}
 
-#ifdef YB_TODO
-	/* YB_TODO(neil) Recheck this change */
-	return scan->xs_heaptid.yb_item.ybctid != 0;
-#endif
 	return has_tuple;
 }
 

src/postgres/src/backend/access/ybgin/ybginget.c
@@ -573,11 +573,6 @@ ybginFetchNextHeapTuple(IndexScanDesc scan)
 		tuple->t_tableOid = RelationGetRelid(scan->heapRelation);
 		if (syscols.ybctid != NULL)
 			HEAPTUPLE_YBCTID(tuple) = PointerGetDatum(syscols.ybctid);
-#ifdef YB_TODO
-		/* YB_TODO(jasonk@yugabyte) Set & get OID is no longer valid */
-		if (syscols.oid != InvalidOid)
-			HeapTupleSetOid(tuple, syscols.oid);
-#endif
 	}
 	pfree(values);
 	pfree(nulls);

src/postgres/src/backend/ybgate/ybgate_api.c
@@ -765,38 +765,31 @@ struct YbgReservoirStateData {
 	ReservoirStateData rs;
 };
 
-YbgStatus YbgSamplerCreate(double rstate_w, uint64_t randstate, YbgReservoirState *yb_rs)
+YbgStatus YbgSamplerCreate(double rstate_w, uint64_t randstate_s0, uint64_t randstate_s1, YbgReservoirState *yb_rs)
 {
 	PG_SETUP_ERROR_REPORTING();
 	YbgReservoirState rstate = (YbgReservoirState) palloc0(sizeof(struct YbgReservoirStateData));
 	rstate->rs.W = rstate_w;
-#ifdef YB_TODO
-	/* YB_TODO(neil) Random state is no longer an array */
-	Uint64ToSamplerRandomState(rstate->rs.randstate, randstate);
-#endif
+	rstate->rs.randstate.s0 = randstate_s0;
+	rstate->rs.randstate.s1 = randstate_s1;
 	*yb_rs = rstate;
 	PG_STATUS_OK();
 }
 
-YbgStatus YbgSamplerGetState(YbgReservoirState yb_rs, double *rstate_w, uint64_t *randstate)
+YbgStatus YbgSamplerGetState(YbgReservoirState yb_rs, double *rstate_w, uint64_t *randstate_s0, uint64_t *randstate_s1)
 {
 	PG_SETUP_ERROR_REPORTING();
 	*rstate_w = yb_rs->rs.W;
-#ifdef YB_TODO
-	/* YB_TODO(neil) Random state is no longer an array */
-	*randstate = SamplerRandomStateToUint64(yb_rs->rs.randstate);
-#endif
+	*randstate_s0 = yb_rs->rs.randstate.s0;
+	*randstate_s1 = yb_rs->rs.randstate.s1;
 	PG_STATUS_OK();
 }
 
 YbgStatus YbgSamplerRandomFract(YbgReservoirState yb_rs, double *value)
 {
 	PG_SETUP_ERROR_REPORTING();
-#ifdef YB_TODO
-	/* YB_TODO(neil) This function needs reimplementation */
 	ReservoirState rs = &yb_rs->rs;
-	*value = sampler_random_fract(rs->randstate);
-#endif
+	*value = sampler_random_fract(&rs->randstate);
 	PG_STATUS_OK();
 }
 

commit 1ba179370bc29d19d3fd98e146a8b186fa8a4aed

[MERGE PG15] Fixes index scan with row comparision
Summary:
Fixes the following issues:

  # Master rebase commit: 06d92004c8b4f033e842e99014cd14502f5f7cda (Rebase with yb::master up to commit 61302b0) erroneously replaced `ScanKey keys[YB_MAX_SCAN_KEYS];` with `ScanKey *keys;`. This was causing memory corruption due to buffer overflow.

  # Commit 6d19303d4c67e8521f2535ab75184c70da5a5089 (Access fixes) introduced an erroneous assertion `Assert(iclause->indexquals->length == indexcols->length);`.

Test Plan: Jenkins: rebase: pg15, test regex: org.yb.pgsql.TestPgRegress.*

Reviewers: jason, tnayak

Reviewed By: jason

Subscribers: tnayak, yql

Tags: #jenkins-ready

Differential Revision: https://phorge.dev.yugabyte.com/D29099

src/postgres/src/backend/access/yb_access/yb_scan.c
@@ -2202,10 +2202,6 @@ ybcBeginScan(Relation relation,
 	tsdesc->rs_key = keys;
 	tsdesc->rs_nkeys = nkeys;
 
-	/* Reorder the keys to regular key first then yb_hash_code. */
-	ybScan->keys = (ScanKey*) palloc(sizeof(ScanKey) * nkeys);
-	ybScan->nkeys = 0;
-	ybScan->nhash_keys = 0;
 	for (int i = 0; i < nkeys; ++i)
 	{
 		ScanKey key = &keys[i];
@@ -2867,15 +2863,11 @@ void ybcIndexCostEstimate(struct PlannerInfo *root, IndexPath *path,
 	foreach(lc, path->indexclauses)
 	{
 		IndexClause *iclause = lfirst_node(IndexClause, lc);
-		int			indexcol = iclause->indexcol;
-		ListCell *lc2, *lci;
-		List* indexcols = iclause->indexcols == NIL ? list_make1_int(indexcol) : iclause->indexcols;
-
-		Assert(iclause->indexquals->length == indexcols->length);
-		forboth(lc2, iclause->indexquals, lci, indexcols)
+		int      indexcol = iclause->indexcol;
+		ListCell   *lc2;
+		foreach (lc2, iclause->indexquals)
 		{
 			RestrictInfo *rinfo = lfirst_node(RestrictInfo, lc2);
-			int			  indexcol = lfirst_int(lci);
 			AttrNumber	 attnum = isprimary ? index->rd_index->indkey.values[indexcol]
 											: (indexcol + 1);
 			Expr	   *clause = rinfo->clause;

src/postgres/src/include/access/yb_scan.h
@@ -98,7 +98,7 @@ typedef struct YbScanDescData
 	 * Array of keys that are reordered to regular keys first then yb_hash_code().
 	 * Size and contents are the same as rs_keys in different order.
 	 */
-	ScanKey *keys;
+	ScanKey keys[YB_MAX_SCAN_KEYS];
 	/* number of regular keys */
 	int nkeys;
 	/* number of keys which represents the yb_hash_code function */

src/postgres/src/test/regress/expected/yb_pg15.out
@@ -377,3 +377,84 @@ select * from address2 order by id;
 CREATE DATABASE mytest;
 -- drop database
 DROP DATABASE mytest;
+-- Index scan test row comparison expressions
+CREATE TABLE pk_range_int_asc (r1 INT, r2 INT, r3 INT, v INT, PRIMARY KEY(r1 asc, r2 asc, r3 asc));
+INSERT INTO pk_range_int_asc SELECT i/25, (i/5) % 5, i % 5, i FROM generate_series(1, 125) AS i;
+EXPLAIN (COSTS OFF, TIMING OFF, SUMMARY OFF, ANALYZE) SELECT * FROM pk_range_int_asc WHERE (r1, r2, r3) <= (2,3,2);
+                                     QUERY PLAN
+-------------------------------------------------------------------------------------
+ Index Scan using pk_range_int_asc_pkey on pk_range_int_asc (actual rows=67 loops=1)
+   Index Cond: (ROW(r1, r2, r3) <= ROW(2, 3, 2))
+(2 rows)
+
+SELECT * FROM pk_range_int_asc WHERE (r1, r2, r3) <= (2,3,2);
+ r1 | r2 | r3 | v
+----+----+----+----
+  0 |  0 |  1 |  1
+  0 |  0 |  2 |  2
+  0 |  0 |  3 |  3
+  0 |  0 |  4 |  4
+  0 |  1 |  0 |  5
+  0 |  1 |  1 |  6
+  0 |  1 |  2 |  7
+  0 |  1 |  3 |  8
+  0 |  1 |  4 |  9
+  0 |  2 |  0 | 10
+  0 |  2 |  1 | 11
+  0 |  2 |  2 | 12
+  0 |  2 |  3 | 13
+  0 |  2 |  4 | 14
+  0 |  3 |  0 | 15
+  0 |  3 |  1 | 16
+  0 |  3 |  2 | 17
+  0 |  3 |  3 | 18
+  0 |  3 |  4 | 19
+  0 |  4 |  0 | 20
+  0 |  4 |  1 | 21
+  0 |  4 |  2 | 22
+  0 |  4 |  3 | 23
+  0 |  4 |  4 | 24
+  1 |  0 |  0 | 25
+  1 |  0 |  1 | 26
+  1 |  0 |  2 | 27
+  1 |  0 |  3 | 28
+  1 |  0 |  4 | 29
+  1 |  1 |  0 | 30
+  1 |  1 |  1 | 31
+  1 |  1 |  2 | 32
+  1 |  1 |  3 | 33
+  1 |  1 |  4 | 34
+  1 |  2 |  0 | 35
+  1 |  2 |  1 | 36
+  1 |  2 |  2 | 37
+  1 |  2 |  3 | 38
+  1 |  2 |  4 | 39
+  1 |  3 |  0 | 40
+  1 |  3 |  1 | 41
+  1 |  3 |  2 | 42
+  1 |  3 |  3 | 43
+  1 |  3 |  4 | 44
+  1 |  4 |  0 | 45
+  1 |  4 |  1 | 46
+  1 |  4 |  2 | 47
+  1 |  4 |  3 | 48
+  1 |  4 |  4 | 49
+  2 |  0 |  0 | 50
+  2 |  0 |  1 | 51
+  2 |  0 |  2 | 52
+  2 |  0 |  3 | 53
+  2 |  0 |  4 | 54
+  2 |  1 |  0 | 55
+  2 |  1 |  1 | 56
+  2 |  1 |  2 | 57
+  2 |  1 |  3 | 58
+  2 |  1 |  4 | 59
+  2 |  2 |  0 | 60
+  2 |  2 |  1 | 61
+  2 |  2 |  2 | 62
+  2 |  2 |  3 | 63
+  2 |  2 |  4 | 64
+  2 |  3 |  0 | 65
+  2 |  3 |  1 | 66
+  2 |  3 |  2 | 67
+(67 rows)

src/postgres/src/test/regress/sql/yb_pg15.sql
@@ -176,3 +176,9 @@ CREATE DATABASE mytest;
 
 -- drop database
 DROP DATABASE mytest;
+
+-- Index scan test row comparison expressions
+CREATE TABLE pk_range_int_asc (r1 INT, r2 INT, r3 INT, v INT, PRIMARY KEY(r1 asc, r2 asc, r3 asc));
+INSERT INTO pk_range_int_asc SELECT i/25, (i/5) % 5, i % 5, i FROM generate_series(1, 125) AS i;
+EXPLAIN (COSTS OFF, TIMING OFF, SUMMARY OFF, ANALYZE) SELECT * FROM pk_range_int_asc WHERE (r1, r2, r3) <= (2,3,2);
+SELECT * FROM pk_range_int_asc WHERE (r1, r2, r3) <= (2,3,2);