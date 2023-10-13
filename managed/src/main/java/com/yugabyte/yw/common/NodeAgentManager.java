5a6b83455838ad6c7a03b3586950e216a9882d61
Naorem Khogendro Singh <nsingh@yugabyte.com>
2023-10-13 10:58:57 -0700

    [PLAT-10809] Use serializable transaction with retry for node-agent cert update as it is very critical
    Summary: Node agent records are updated in transaction. One potential issue was in updating UPGRADED and cert path which were not transactional.
    
    Test Plan:
    1. Created a universe with node agent.
    2. Force upgrade by changing the version.
    
    Reviewers: amalyshev, cwang
    
    Reviewed By: cwang
    
    Subscribers: yugaware
    
    Differential Revision: https://phorge.dev.yugabyte.com/D29333

--- managed/src/main/java/com/yugabyte/yw/common/NodeAgentManager.java
+++ managed/src/main/java/com/yugabyte/yw/common/NodeAgentManager.java
@@ -14,6 +14,7 @@ import com.yugabyte.yw.models.NodeAgent;
 import com.yugabyte.yw.models.NodeAgent.ArchType;
 import com.yugabyte.yw.models.NodeAgent.OSType;
 import com.yugabyte.yw.models.NodeAgent.State;
+import io.ebean.annotation.Transactional;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import java.io.BufferedInputStream;
@@ -421,13 +422,15 @@ public class NodeAgentManager {
    * @param includeCertContents if it is true, server cert and key contents are included.
    * @return the updated node agent record along with cert and key in the config.
    */
+  @Transactional
   public NodeAgent create(NodeAgent nodeAgent, boolean includeCertContents) {
     nodeAgent.setConfig(new NodeAgent.Config());
     nodeAgent.setState(State.REGISTERING);
     nodeAgent.insert();
     Path certDirPath = getOrCreateNextCertDirectory(nodeAgent);
     Pair<X509Certificate, KeyPair> serverPair = generateNodeAgentCerts(nodeAgent, certDirPath);
-    nodeAgent.updateCertDirPath(certDirPath);
+    nodeAgent.getConfig().setCertPath(certDirPath.toString());
+    nodeAgent.save();
     if (includeCertContents) {
       X509Certificate serverCert = serverPair.getLeft();
       KeyPair serverKeyPair = serverPair.getRight();
@@ -513,7 +516,7 @@ public class NodeAgentManager {
     }
     // Point to the new directory and persist in the DB before deleting.
     log.info("Updating the cert dir to {} for node agent {}", newCertDirPath, nodeAgent.getUuid());
-    nodeAgent.updateCertDirPath(newCertDirPath);
+    nodeAgent.updateCertDirPath(newCertDirPath, State.UPGRADED);
     try {
       // Delete the old cert directory.
       log.info(
