yugabyte/yugabyte-db

commit 8db7dd7509de3973b0369c52f657c5ae2839b7e0

wip
Test Plan: wip

Subscribers: yql

Differential Revision: https://phorge.dev.yugabyte.com/D28071

src/postgres/src/backend/utils/cache/relcache.c
@@ -2847,19 +2847,6 @@ YbPreloadRelCacheImpl(YbRunWithPrefetcherContext *ctx)
 		YbRegisterTables(prefetcher, tables, lengthof(tables));
 	}
 
-	if (ctx->is_using_response_cache)
-	{
-		static const YbPFetchTable tables[] = {
-			YB_PFETCH_TABLE_PG_CAST,
-			YB_PFETCH_TABLE_PG_INHERITS,
-			YB_PFETCH_TABLE_PG_POLICY,
-			YB_PFETCH_TABLE_PG_PROC,
-			YB_PFETCH_TABLE_PG_TABLESPACE,
-			YB_PFETCH_TABLE_PG_TRIGGER
-		};
-		YbRegisterTables(prefetcher, tables, lengthof(tables));
-	}
-
 	YBCStatus status = YbPrefetch(prefetcher);
 	if (status)
 		return status;

commit 93ae4b00b55c18a8ebd56623aa406852c74ad4d0

[#19363] YSQL: Add MyDatabaseId to T-server cache key
Summary:
Adds MyDatabaseId to the T-server cache key to address an issue caused by different databases sharing T-server cache entries.

When a Postgres backend starts, it prefetches these 3 shared tables:
```
        YbRegisterTable(prefetcher, YB_PFETCH_TABLE_PG_AUTH_MEMBERS);
	YbRegisterTable(prefetcher, YB_PFETCH_TABLE_PG_DATABASE);
	YbRegisterTable(prefetcher, YB_PFETCH_TABLE_PG_DB_ROLE_SETTINGS);
```
These tables are then cached in the T-server cache, which is keyed by the database OID and the OIDs of these tables. Because these are shared tables, the OID of the template1 database is used. As a result, when another backend process starts up, it will issue the same prefetch request, which will result in a hit in the T-server cache (assuming the catalog version has not changed).

Here is how the issue manifests in detail by @tverona, (requires D28071, ysql_enable_read_request_caching=true):

### 1. Start with just `yugabytedb`. Connect to `yugabytedb` for the first time.
   * **a.** Relcacheinit file is built. So we preload a bunch of tables from master.
   * **b.** Create tserver cache entry for those tables (which includes `pg_database`). Key contains `yugabytedb` oid (since that’s part of the request).
   * **c.** Create `db1`.

### 2. Connect to `db1` for the first time.
   * **a.** Same flow as #1 above - we create a new relcacheinit file for `db1`.
   * **b.** We create another tserver cache entry (might be more than one, but just simplifying) with key containing `db1` oid.

### 3. Connect to `db1` for the 2nd time.
   * **a. With D28071:**
     - **i.** Relcache file is not built, since cache is not invalidated.
     - **ii.** We fetch the 3 tables (including `pg_database`) from master and create a new tserver cache entry, with key include `template0` dbid(?). Values include `db1`.
   * **b. Without D28071:**
     - **i.** We preload a bunch of tables for `db1`. We match on tserver cache entry from 2.b. We do not hit master.
   * **c.** Create `db2`.

### 4. Connect to `db2` for the first time.
   * **a.** Same flow as #1 above - we create a new relcacheinit file for `db2`.
   * **b.** We create another tserver cache entry (might be more than one, but just simplifying) with key containing `db2` oid.

### 5. Connect to `db2` for the 2nd time.
   * **a. With D28071:**
     - **i.** Relcache file is not built, since cache is not invalidated.
     - **ii.** We fetch the 3 tables (including `pg_database`) from master and match on the key in 3.a.ii. We do not hit master. We get back `pg_database` containing entries for `db1` but not `db2`.
     - **iii.** We fail later in `CheckMyDatabase`.
   * **b. Without the diff:**
     - **i.** We preload a bunch of tables for `db2`. We match on tserver cache entry from 4.b. We do not hit master.

By always including MyDatabaseId in the cache key, we avoid serving stale versions of shared relations to different databases.

**Upgrade/Rollback safety:**
Only PG to T-Server RPCs are changed.
Jira: DB-8163

Test Plan:
  # Connect to yugabyte
  # Connect to yugabyte
    # Create db1
  # Connect to db1
  # Connect to db1 <-- fails before this change with D28071, ysql_enable_read_request_caching=true

src/postgres/src/backend/utils/cache/relcache.c
@@ -2563,7 +2563,7 @@ static bool
 YbPrefetcherStarterWithCacheCall(YbPrefetcherStarterFunctor *functor) {
 	const YbPrefetcherStarterWithCache *this =
 		(const YbPrefetcherStarterWithCache *)functor;
-	YBCStartSysTablePrefetching(*this->version, this->mode);
+	YBCStartSysTablePrefetching(MyDatabaseId, *this->version, this->mode);
 	return true;
 }
 

src/yb/tserver/pg_client.proto
@@ -428,8 +428,9 @@ message OptionalUint64PB { uint64 value = 1; }
 
 message PgPerformOptionsPB {
   message CachingInfoPB {
-    bytes key = 1;
-    OptionalUint32PB lifetime_threshold_ms = 2;
+    uint64 key_group = 1;
+    bytes key_value = 2;
+    OptionalUint32PB lifetime_threshold_ms = 3;
   }
 
   // Cannot use IsolationLevel enum, since we cannot use proto2 enum in proto3 messages.

src/yb/tserver/pg_response_cache.cc
@@ -18,6 +18,7 @@
 #include <mutex>
 #include <utility>
 
+#include <boost/functional/hash.hpp>
 #include <boost/multi_index/member.hpp>
 
 #include "yb/client/yb_op.h"
@@ -243,11 +244,27 @@ class Value {
   std::shared_ptr<Data> data_;
 };
 
-struct Entry {
-  explicit Entry(std::string&& key_)
-      : key(std::move(key_)) {}
+struct Key {
+  uint64_t group;
+  std::string value;
+  explicit Key (uint64_t key_group, std::string&& key_value)
+      : group(key_group), value(std::move(key_value)) {}
 
-  std::string key;
+  friend bool operator==(const Key&, const Key&) = default;
+};
+
+inline size_t hash_value(const Key& key) {
+  size_t value = 0;
+  boost::hash_combine(value, key.group);
+  boost::hash_range(value, key.value.begin(), key.value.end());
+  return value;
+}
+
+struct Entry {
+  explicit Entry(uint64_t key_group, std::string&& key_value)
+      : key(key_group, std::move(key_value)) {}
+
+  Key key;
   Value value;
 };
 
@@ -313,7 +330,8 @@ class PgResponseCache::Impl : private GarbageCollector {
       }
     });
     std::lock_guard lock(mutex_);
-    const auto& value = entries_.emplace(std::move(*cache_info->mutable_key()))->value;
+    const auto& value = entries_.emplace(
+        cache_info->key_group(), std::move(*cache_info->mutable_key_value()))->value;
     const auto& data = value.data();
     bool loading_required = false;
     if (!data ||
@@ -420,7 +438,7 @@ class PgResponseCache::Impl : private GarbageCollector {
   std::mutex mutex_;
   LRUCache<
       Entry,
-      boost::multi_index::member<Entry, std::string, &Entry::key>
+      boost::multi_index::member<Entry, Key, &Entry::key>
   > entries_ GUARDED_BY(mutex_);
 
   DISALLOW_COPY_AND_ASSIGN(Impl);

src/yb/yql/pggate/pg_session.cc
@@ -727,7 +727,8 @@ Result<PerformFuture> PgSession::Perform(BufferableOperations&& ops, PerformOpti
   if (ops_options.cache_options) {
     auto& cache_options = *ops_options.cache_options;
     auto& caching_info = *options.mutable_caching_info();
-    caching_info.set_key(std::move(cache_options.key));
+    caching_info.set_key_group(cache_options.key_group);
+    caching_info.set_key_value(std::move(cache_options.key_value));
     if (cache_options.lifetime_threshold_ms) {
       caching_info.mutable_lifetime_threshold_ms()->set_value(*cache_options.lifetime_threshold_ms);
     }
@@ -955,7 +956,7 @@ Result<PerformFuture> PgSession::RunAsync(const ReadOperationGenerator& generato
 
 Result<PerformFuture> PgSession::RunAsync(
     const ReadOperationGenerator& generator, CacheOptions&& cache_options) {
-  SCHECK(!cache_options.key.empty(), InvalidArgument, "Cache key can't be empty");
+  RSTATUS_DCHECK(!cache_options.key_value.empty(), InvalidArgument, "Cache key can't be empty");
   // Ensure no buffered requests will be added to cached request.
   RETURN_NOT_OK(buffer_.Flush());
   return DoRunAsync(generator, HybridTime(), ForceNonBufferable::kFalse, std::move(cache_options));

src/yb/yql/pggate/pg_session.h
@@ -253,7 +253,8 @@ class PgSession : public RefCountedThreadSafe<PgSession> {
       ForceNonBufferable force_non_bufferable = ForceNonBufferable::kFalse);
 
   struct CacheOptions {
-    std::string key;
+    uint64_t key_group;
+    std::string key_value;
     std::optional<uint32_t> lifetime_threshold_ms;
   };
 

src/yb/yql/pggate/pg_sys_table_prefetcher.cc
@@ -311,7 +311,8 @@ class VersionInfoWriter {
     yb::ThreadSafeArena* arena, const ReadHybridTime& catalog_read_time,
     const std::vector<OperationInfo>& ops, const PrefetcherOptions::CachingInfo& caching_info) {
   return {
-      .key = BuildCacheKey(arena, catalog_read_time, ops, caching_info.version_info),
+      .key_group = caching_info.db_oid,
+      .key_value = BuildCacheKey(arena, catalog_read_time, ops, caching_info.version_info),
       .lifetime_threshold_ms = GetCacheLifetimeThreshold(caching_info.mode)
   };
 }

src/yb/yql/pggate/pg_sys_table_prefetcher.h
@@ -19,6 +19,7 @@
 
 #include <boost/container/small_vector.hpp>
 
+#include "yb/common/pg_types.h"
 #include "yb/common/pgsql_protocol.fwd.h"
 
 #include "yb/rpc/rpc_fwd.h"
@@ -27,10 +28,8 @@
 #include "yb/util/result.h"
 #include "yb/util/status.h"
 
-namespace yb {
-struct PgObjectId;
+namespace yb::pggate {
 
-namespace pggate {
 class PgSession;
 
 YB_DEFINE_ENUM(PrefetchingCacheMode, (TRUST_CACHE)(RENEW_CACHE_SOFT)(RENEW_CACHE_HARD));
@@ -48,6 +47,7 @@ struct PrefetcherOptions {
 
   struct CachingInfo {
     VersionInfo version_info;
+    PgOid db_oid;
     PrefetchingCacheMode mode;
 
     std::string ToString() const;
@@ -82,5 +82,4 @@ class PgSysTablePrefetcher {
   std::unique_ptr<Impl> impl_;
 };
 
-} // namespace pggate
-} // namespace yb
+} // namespace yb::pggate

src/yb/yql/pggate/ybc_pggate.cc
@@ -1683,10 +1683,12 @@ void YBCStartSysTablePrefetchingNoCache() {
 }
 
 void YBCStartSysTablePrefetching(
+    YBCPgOid database_oid,
     YBCPgLastKnownCatalogVersionInfo version_info,
     YBCPgSysTablePrefetcherCacheMode cache_mode) {
   YBCStartSysTablePrefetchingImpl(PrefetcherOptions::CachingInfo{
       {version_info.version, version_info.is_db_catalog_version_mode},
+      database_oid,
       YBCMapPrefetcherCacheMode(cache_mode)});
 }
 

src/yb/yql/pggate/ybc_pggate.h
@@ -695,6 +695,7 @@ YBCStatus YBCGetIndexBackfillProgress(YBCPgOid* index_oids, YBCPgOid* database_o
 void YBCStartSysTablePrefetchingNoCache();
 
 void YBCStartSysTablePrefetching(
+    YBCPgOid database_oid,
     YBCPgLastKnownCatalogVersionInfo catalog_version,
     YBCPgSysTablePrefetcherCacheMode cache_mode);
 

src/yb/yql/pgwrapper/pg_catalog_perf-test.cc
@@ -200,9 +200,9 @@ class PgCatalogPerfTestBase : public PgMiniTestBase {
     return res;
   }
 
-  Result<uint64_t> RPCCountOnStartUp() {
-    auto res = VERIFY_RESULT(metrics_->Delta([this] {
-      VERIFY_RESULT(Connect());
+  Result<uint64_t> RPCCountOnStartUp(const std::string& db_name = {}) {
+    auto res = VERIFY_RESULT(metrics_->Delta([this, &db_name] {
+      VERIFY_RESULT(ConnectToDB(db_name));
       return static_cast<Status>(Status::OK());
     })).master_read_rpc;
     return res;
@@ -302,6 +302,7 @@ class PgCatalogWithStaleResponseCacheTest : public PgCatalogWithUnlimitedCachePe
 
 constexpr uint64_t kFirstConnectionRPCCountDefault = 5;
 constexpr uint64_t kFirstConnectionRPCCountWithAdditionalTables = 6;
+constexpr uint64_t kSubsequentConnectionRPCCount = 2;
 static_assert(kFirstConnectionRPCCountDefault <= kFirstConnectionRPCCountWithAdditionalTables);
 
 } // namespace
@@ -315,7 +316,7 @@ TEST_F(PgCatalogPerfTest, StartupRPCCount) {
   const auto first_connect_rpc_count = ASSERT_RESULT(RPCCountOnStartUp());
   ASSERT_EQ(first_connect_rpc_count, kFirstConnectionRPCCountDefault);
   const auto subsequent_connect_rpc_count = ASSERT_RESULT(RPCCountOnStartUp());
-  ASSERT_EQ(subsequent_connect_rpc_count, 2);
+  ASSERT_EQ(subsequent_connect_rpc_count, kSubsequentConnectionRPCCount);
 }
 
 // Test checks number of RPC in case of cache refresh without partitioned tables.
@@ -554,6 +555,30 @@ TEST_F_EX(PgCatalogPerfTest,
           PgPreloadAdditionalCatBothTest) {
   const auto rpc_count = ASSERT_RESULT(RPCCountOnStartUp());
   ASSERT_EQ(rpc_count, kFirstConnectionRPCCountWithAdditionalTables);
+}
+
+// Test checks that response cache is DB specific.
+// First, start up an initial connection and measure the startup RPC count (a).
+// Then, create a new database. Create a new connection to this database and
+// measure the startup RPC count (b). Both (a) and (b) should be equal to the
+// initial startup RPC count, because the second connection can't use the
+// cached responses from the first connection. We also check that upon creating
+// a subsequent connection to the new database, the startup RPC count is what we
+// expect for a second connection.
+TEST_F_EX(PgCatalogPerfTest, ResponseCacheIsDBSpecific, PgCatalogWithUnlimitedCachePerfTest) {
+  constexpr auto* kDBName = "db1";
+  auto rpc_count_checker = [this](const std::string& db_name = {}) -> Status {
+    for (auto expected_rpc_count : {kFirstConnectionRPCCountWithAdditionalTables,
+                                    kSubsequentConnectionRPCCount}) {
+      const auto rpc_count = VERIFY_RESULT(RPCCountOnStartUp(db_name));
+      SCHECK_EQ(rpc_count, expected_rpc_count, IllegalState, "Unexpected rpc count");
+    }
+    return Status::OK();
+  };
+  ASSERT_OK(rpc_count_checker());
+  auto conn = ASSERT_RESULT(Connect());
+  ASSERT_OK(conn.ExecuteFormat("CREATE DATABASE $0", kDBName));
+  ASSERT_OK(rpc_count_checker(kDBName));
 }
 
 } // namespace yb::pgwrapper