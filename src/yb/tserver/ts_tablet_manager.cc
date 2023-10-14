37bd59c72ab05b00708922484e037e2e845b7efe
Amitanand Aiyer <amitanandaiyer@users.noreply.github.com>
2023-10-14 14:36:31 +0530

    [#19532] Docdb: Allow Traces to get past Glog's 30k limit
    Summary:
    Currently we use the pattern of
    
    ```
    LOG(INFO) << .. trace->DumpToString()
    ```
    while this works for small traces, for long traces that are longer than
    GLog's LogMessage::kMaxLogMessageLen = 30000, this can result in truncated trace logs.
    
    With end-to-end traces, and newly introduced traces for UpdateConsensus or tracing for transactions, we may go over this limit easily.
    
    We could support printing these traces by splitting the output to limit each line to be less than 30k,  instead of printing the whole trace as one line.
    
    Note: As this change causes a trace to be printed over multiple lines -- it is possible that the output from different threads may get multiplexed to the log. However, since GLOG prints the thread-id for each line -- we can filter it out to isolate the trace that we are interested in.
    
    Main changes here include:
     - Implement DumpToLogINFO(..) to print the trace to LOG(INFO)
     - Implement a hack to make the printing work for LOG_EVERY/EVERY_N macros
     - Introduce a Gflag `trace_max_dump_size` (defaults to 25000) to limit the size of traces printed. This can be increased to something larger to print bigger traces.
    Jira: DB-8323
    
    Test Plan:
    ```
    export YB_EXTRA_TSERVER_FLAGS="--collect_end_to_end_traces=true --print_trace_every=1 --enable_tracing=false --sampled_trace_1_in_n=1000"
    ybd --cxx-test pg_index_backfill-test --gtest_filter PgIndexBackfillTest.Large
    
    ybd --cxx-test trace-test --gtest_filter TraceTest.TestDumpLargeTrace
    ```
    
    Reviewers: arybochkin, rthallam, esheng
    
    Reviewed By: arybochkin
    
    Subscribers: yql, jason, ybase, bogdan
    
    Differential Revision: https://phorge.dev.yugabyte.com/D29347

--- src/yb/tserver/ts_tablet_manager.cc
+++ src/yb/tserver/ts_tablet_manager.cc
@@ -105,7 +105,6 @@
 #include "yb/util/fault_injection.h"
 #include "yb/util/flags.h"
 #include "yb/util/format.h"
-#include "yb/util/logging.h"
 #include "yb/util/mem_tracker.h"
 #include "yb/util/metrics.h"
 #include "yb/util/monotime.h"
@@ -1741,8 +1740,8 @@ void TSTabletManager::OpenTablet(const RaftGroupMetadataPtr& meta,
   if (elapsed_ms > FLAGS_tablet_start_warn_threshold_ms) {
     LOG(WARNING) << kLogPrefix << "Tablet startup took " << elapsed_ms << "ms";
     if (Trace::CurrentTrace()) {
-      LOG(WARNING) << kLogPrefix << "Trace:" << std::endl
-                   << Trace::CurrentTrace()->DumpToString(true);
+      LOG(INFO) << kLogPrefix << "Trace:";
+      Trace::CurrentTrace()->DumpToLogInfo(true);
     }
   }
 
