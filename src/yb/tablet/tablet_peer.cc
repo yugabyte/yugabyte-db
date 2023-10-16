yugabyte/yugabyte-db

commit eed270f2e6d727c2909383175c75fe08ce4abe7e

[#17830] DocDB: Ensure tablet consensus is valid before being used
Summary:
The consensus_ member of TabletPeer is protected by lock_. There were a lot of places in the code that were not getting the lock. And the consensus_ member can be nullptr during initialization or shutdown so this should be checked as well.
TabletPeer exposed raw pointer to consensus_ member via `consensus` and `raft_consensus` functions which are unsafe so these have been removed.
`shared_consensus` and `shared_raft_consensus` functions have been renamed to `GetConsensus` and `GetRaftConsensus`. These will return a non null shared pointer to the consensus_ member if it is valid. Otherwise they will return a bad Status.

Fixes #17830

Jira: DB-6918

Test Plan: All Jenkins test

Reviewers: sergei, rsami, bogdan, xCluster

Reviewed By: sergei

Subscribers: ybase

Differential Revision: https://phorge.dev.yugabyte.com/D26335

src/yb/tablet/tablet_peer.cc
@@ -323,7 +323,8 @@ Status TabletPeer::InitTabletPeer(
   }
   // End of lock scope for lock_.
 
-  ChangeConfigReplicated(RaftConfig()); // Set initial flag value.
+  auto raft_config = VERIFY_RESULT(GetRaftConsensus())->CommittedConfig();
+  ChangeConfigReplicated(raft_config);  // Set initial flag value.
 
   RETURN_NOT_OK(prepare_thread_->Start());
 
@@ -355,8 +356,9 @@ Result<FixedHybridTimeLease> TabletPeer::HybridTimeLease(
   // min_allowed could contain non zero logical part, so we add one microsecond to be sure that
   // the resulting ht_lease is at least min_allowed.
   auto min_allowed_micros = min_allowed.CeilPhysicalValueMicros();
-  MicrosTime lease_micros = VERIFY_RESULT(consensus_->MajorityReplicatedHtLeaseExpiration(
-      min_allowed_micros, deadline));
+  MicrosTime lease_micros =
+      VERIFY_RESULT(VERIFY_RESULT(GetConsensus())
+                        ->MajorityReplicatedHtLeaseExpiration(min_allowed_micros, deadline));
   if (lease_micros >= kMaxHybridTimePhysicalMicros) {
     // This could happen when leader leases are disabled.
     return FixedHybridTimeLease();
@@ -368,7 +370,8 @@ Result<FixedHybridTimeLease> TabletPeer::HybridTimeLease(
 }
 
 Result<HybridTime> TabletPeer::PreparePeerRequest() {
-  auto leader_term = consensus_->GetLeaderState(/* allow_stale= */ true).term;
+  auto leader_term =
+      VERIFY_RESULT(GetRaftConsensus())->GetLeaderState(/* allow_stale= */ true).term;
   if (leader_term >= 0) {
     auto last_write_ht = tablet_->mvcc_manager()->LastReplicatedHybridTime();
     auto propagated_history_cutoff =
@@ -395,15 +398,12 @@ Result<HybridTime> TabletPeer::PreparePeerRequest() {
   return tablet_->mvcc_manager()->SafeTime(ht_lease);
 }
 
-void TabletPeer::MajorityReplicated() {
-  auto ht_lease = HybridTimeLease(
-      /* min_allowed= */ HybridTime::kMin, /* deadline */ CoarseTimePoint::max());
-  if (!ht_lease.ok()) {
-    LOG_WITH_PREFIX(DFATAL) << "Failed to get current lease: " << ht_lease.status();
-    return;
-  }
+Status TabletPeer::MajorityReplicated() {
+  auto ht_lease = VERIFY_RESULT(HybridTimeLease(
+      /* min_allowed= */ HybridTime::kMin, /* deadline */ CoarseTimePoint::max()));
 
-  tablet_->mvcc_manager()->UpdatePropagatedSafeTimeOnLeader(*ht_lease);
+  tablet_->mvcc_manager()->UpdatePropagatedSafeTimeOnLeader(ht_lease);
+  return Status::OK();
 }
 
 void TabletPeer::ChangeConfigReplicated(const RaftConfigPB& config) {
@@ -429,7 +429,8 @@ Status TabletPeer::Start(const ConsensusBootstrapInfo& bootstrap_info) {
 
     VLOG_WITH_PREFIX(2) << "Peer starting";
 
-    VLOG(2) << "RaftConfig before starting: " << consensus_->CommittedConfig().DebugString();
+    auto consensus = GetRaftConsensusUnsafe();
+    VLOG(2) << "RaftConfig before starting: " << consensus->CommittedConfig().DebugString();
 
     // If tablet was previously considered shutdown w.r.t. metrics,
     // fix that for a tablet now being reinstated.
@@ -437,7 +438,7 @@ Status TabletPeer::Start(const ConsensusBootstrapInfo& bootstrap_info) {
       << "Remove from set of tablets that have been shutdown so as to allow reporting metrics";
     metric_registry_->tablets_shutdown_erase(tablet_id());
 
-    RETURN_NOT_OK(consensus_->Start(bootstrap_info));
+    RETURN_NOT_OK(consensus->Start(bootstrap_info));
     RETURN_NOT_OK(UpdateState(RaftGroupStatePB::BOOTSTRAPPING, RaftGroupStatePB::RUNNING,
                               "Incorrect state to start TabletPeer, "));
   }
@@ -450,11 +451,6 @@ Status TabletPeer::Start(const ConsensusBootstrapInfo& bootstrap_info) {
   mark_dirty_clbk_.Run(context);
 
   return tablet_->EnableCompactions(/* blocking_rocksdb_shutdown_start_ops_pause */ nullptr);
-}
-
-consensus::RaftConfigPB TabletPeer::RaftConfig() const {
-  CHECK(consensus_) << "consensus is null";
-  return consensus_->CommittedConfig();
 }
 
 bool TabletPeer::StartShutdown() {
@@ -488,11 +484,7 @@ bool TabletPeer::StartShutdown() {
   // indirectly end up calling into the log, which we are about to shut down.
   UnregisterMaintenanceOps();
 
-  std::shared_ptr<consensus::RaftConsensus> consensus;
-  {
-    std::lock_guard lock(lock_);
-    consensus = consensus_;
-  }
+  auto consensus = GetRaftConsensusUnsafe();
   if (consensus) {
     consensus->Shutdown();
   }
@@ -532,13 +524,15 @@ void TabletPeer::CompleteShutdown(
   tablet_obj_state_.store(TabletObjectState::kDestroyed, std::memory_order_release);
 
   // Only mark the peer as SHUTDOWN when all other components have shut down.
+  std::shared_ptr<consensus::RaftConsensus> consensus;
   {
     std::lock_guard lock(lock_);
     strand_.reset();
     retryable_requests_flusher_.reset();
     // Release mem tracker resources.
     has_consensus_.store(false, std::memory_order_release);
-    consensus_.reset();
+    // Clear the consensus and destroy it outside the lock.
+    consensus_.swap(consensus);
     prepare_thread_.reset();
     tablet_.reset();
     auto state = state_.load(std::memory_order_acquire);
@@ -658,7 +652,7 @@ Status TabletPeer::WaitUntilConsensusRunning(const MonoDelta& timeout) {
                      RaftGroupStatePB_Name(cached_state)));
     }
     if (cached_state == RUNNING && has_consensus_.load(std::memory_order_acquire) &&
-        consensus_->IsRunning()) {
+        VERIFY_RESULT(GetRaftConsensus())->IsRunning()) {
       break;
     }
     MonoTime now(MonoTime::Now());
@@ -969,7 +963,7 @@ Result<int64_t> TabletPeer::GetEarliestNeededLogIndex(std::string* details) cons
     *details += Format("Min pending op id index: $0\n", min_pending_op_index);
   }
 
-  auto min_retryable_request_op_id = consensus_->MinRetryableRequestOpId();
+  auto min_retryable_request_op_id = VERIFY_RESULT(GetRaftConsensus())->MinRetryableRequestOpId();
   min_index = std::min(min_index, min_retryable_request_op_id.index);
   if (details) {
     *details += Format("Min retryable request op id: $0\n", min_retryable_request_op_id);
@@ -1001,7 +995,7 @@ Result<int64_t> TabletPeer::GetEarliestNeededLogIndex(std::string* details) cons
   // - New data gets written and Raft-committed, but not yet flushed to an SSTable.
   // - We still don't garbage-collect the logs containing the committed but unflushed data,
   //   because the earlier value of the last committed op id that we read prevents us from doing so.
-  auto last_committed_op_id = consensus()->GetLastCommittedOpId();
+  auto last_committed_op_id = VERIFY_RESULT(GetConsensus())->GetLastCommittedOpId();
   min_index = std::min(min_index, last_committed_op_id.index);
   if (details) {
     *details += Format("Last committed op id: $0\n", last_committed_op_id);
@@ -1364,21 +1358,19 @@ bool TabletPeer::ShouldApplyWrite() {
   return tablet_->ShouldApplyWrite();
 }
 
-consensus::Consensus* TabletPeer::consensus() const {
-  return raft_consensus();
+Result<std::shared_ptr<consensus::Consensus>> TabletPeer::GetConsensus() const {
+  return GetRaftConsensus();
 }
 
-consensus::RaftConsensus* TabletPeer::raft_consensus() const {
+Result<shared_ptr<consensus::RaftConsensus>> TabletPeer::GetRaftConsensus() const {
   std::lock_guard lock(lock_);
-  return consensus_.get();
-}
+  SCHECK(!IsShutdownStarted(), NotFound, "Tablet peer $0 is shutting down", LogPrefix());
+  SCHECK(consensus_, IllegalState, "Consensus not yet initialized for tablet peer $0", LogPrefix());
 
-shared_ptr<consensus::Consensus> TabletPeer::shared_consensus() const {
-  std::lock_guard lock(lock_);
   return consensus_;
 }
 
-shared_ptr<consensus::RaftConsensus> TabletPeer::shared_raft_consensus() const {
+shared_ptr<consensus::RaftConsensus> TabletPeer::GetRaftConsensusUnsafe() const {
   std::lock_guard lock(lock_);
   return consensus_;
 }
@@ -1478,8 +1470,8 @@ std::string TabletPeer::LogPrefix() const {
 scoped_refptr<OperationDriver> TabletPeer::CreateOperationDriver() {
   return scoped_refptr<OperationDriver>(new OperationDriver(
       &operation_tracker_,
-      consensus_.get(),
-      prepare_thread_.get(),
+      GetRaftConsensusUnsafe().get(),
+      prepare_thread_.get(),  // May be nullptr
       tablet_->table_type()));
 }
 
@@ -1499,11 +1491,7 @@ Result<client::YBClient*> TabletPeer::client() const {
 }
 
 int64_t TabletPeer::LeaderTerm() const {
-  shared_ptr<consensus::Consensus> consensus;
-  {
-    std::lock_guard lock(lock_);
-    consensus = consensus_;
-  }
+  auto consensus = GetRaftConsensusUnsafe();
   return consensus ? consensus->LeaderTerm() : yb::OpId::kUnknownTerm;
 }
 
@@ -1512,17 +1500,14 @@ Result<HybridTime> TabletPeer::LeaderSafeTime() const {
 }
 
 consensus::LeaderStatus TabletPeer::LeaderStatus(bool allow_stale) const {
-  shared_ptr<consensus::Consensus> consensus;
-  {
-    std::lock_guard lock(lock_);
-    consensus = consensus_;
-  }
+  auto consensus = GetRaftConsensusUnsafe();
   return consensus ? consensus->GetLeaderStatus(allow_stale) : consensus::LeaderStatus::NOT_LEADER;
 }
 
-HybridTime TabletPeer::HtLeaseExpiration() const {
+Result<HybridTime> TabletPeer::HtLeaseExpiration() const {
+  auto consensus = VERIFY_RESULT(GetRaftConsensus());
   HybridTime result(
-      CHECK_RESULT(consensus_->MajorityReplicatedHtLeaseExpiration(0, CoarseTimePoint::max())), 0);
+      CHECK_RESULT(consensus->MajorityReplicatedHtLeaseExpiration(0, CoarseTimePoint::max())), 0);
   return std::max(result, tablet_->mvcc_manager()->LastReplicatedHybridTime());
 }
 
@@ -1584,7 +1569,7 @@ void TabletPeer::StrandEnqueue(rpc::StrandTask* task) {
 }
 
 bool TabletPeer::CanBeDeleted() {
-  const auto consensus = shared_raft_consensus();
+  const auto consensus = GetRaftConsensusUnsafe();
   if (!consensus || consensus->LeaderTerm() == OpId::kUnknownTerm) {
     return false;
   }
@@ -1599,7 +1584,7 @@ bool TabletPeer::CanBeDeleted() {
     return false;
   }
 
-  const auto all_applied_op_id = consensus->GetAllAppliedOpId();
+  const auto all_applied_op_id = consensus.get()->GetAllAppliedOpId();
   if (all_applied_op_id < op_id) {
     return false;
   }
@@ -1619,26 +1604,12 @@ rpc::Scheduler& TabletPeer::scheduler() const {
 // Called from within RemoteBootstrapSession and RemoteBootstrapServiceImpl.
 Status TabletPeer::ChangeRole(const std::string& requestor_uuid) {
   MAYBE_FAULT(FLAGS_TEST_fault_crash_leader_before_changing_role);
-  shared_ptr<consensus::Consensus> consensus = shared_consensus();
-
-  // This check fixes an issue with test TestDeleteTabletDuringRemoteBootstrap in which a tablet is
-  // tombstoned while the bootstrap is happening. This causes the peer's consensus object to be
-  // null.
-  if (!consensus) {
-    RaftGroupStatePB tablet_state = state();
-    return STATUS(
-        IllegalState,
-        Substitute(
-            "Unable to change role for server $0 in config for tablet $1. Consensus is not "
-            "available. "
-            "Tablet state: $2 ($3)",
-            requestor_uuid, tablet_id(), RaftGroupStatePB_Name(tablet_state), tablet_state));
-  }
+  auto consensus = VERIFY_RESULT_PREPEND(GetConsensus(), "Unable to change role for tablet peer");
 
   // If peer being bootstrapped is already a VOTER, don't send the ChangeConfig request. This could
   // happen when a tserver that is already a VOTER in the configuration tombstones its tablet, and
   // the leader starts bootstrapping it.
-  const consensus::RaftConfigPB config = RaftConfig();
+  const auto config = consensus->CommittedConfig();
   for (const RaftPeerPB& peer_pb : config.peers()) {
     if (peer_pb.permanent_uuid() != requestor_uuid) {
       continue;
@@ -1695,13 +1666,7 @@ bool TabletPeer::FlushRetryableRequestsEnabled() const {
 }
 
 Result<consensus::RetryableRequests> TabletPeer::GetRetryableRequests() {
-  auto raft_consensus = shared_raft_consensus();
-  // raft_consensus is nullptr during bootstrap.
-  SCHECK_FORMAT(raft_consensus,
-                IllegalState,
-                "Tablet $0 raft_consensus not initialized",
-                tablet_id_);
-  return raft_consensus->GetRetryableRequests();
+  return VERIFY_RESULT(GetRaftConsensus())->GetRetryableRequests();
 }
 
 Status TabletPeer::FlushRetryableRequests() {

src/yb/tablet/tablet_peer.h
@@ -239,11 +239,10 @@ class TabletPeer : public std::enable_shared_from_this<TabletPeer>,
   // Returns false if it is preferable to don't apply write operation.
   bool ShouldApplyWrite() override;
 
-  consensus::Consensus* consensus() const;
-  consensus::RaftConsensus* raft_consensus() const;
-
-  std::shared_ptr<consensus::Consensus> shared_consensus() const;
-  std::shared_ptr<consensus::RaftConsensus> shared_raft_consensus() const;
+  // Returns valid shared pointer to the consensus. Returns a not OK status if the consensus is not
+  // in a valid state.
+  Result<std::shared_ptr<consensus::Consensus>> GetConsensus() const EXCLUDES(lock_);
+  Result<std::shared_ptr<consensus::RaftConsensus>> GetRaftConsensus() const EXCLUDES(lock_);
 
   std::shared_ptr<RetryableRequestsFlusher> shared_retryable_requests_flusher() const;
 
@@ -274,9 +273,6 @@ class TabletPeer : public std::enable_shared_from_this<TabletPeer>,
   }
 
   TabletDataState data_state() const;
-
-  // Returns the current Raft configuration.
-  consensus::RaftConfigPB RaftConfig() const;
 
   TabletStatusListener* status_listener() const {
     return status_listener_.get();
@@ -364,7 +360,7 @@ class TabletPeer : public std::enable_shared_from_this<TabletPeer>,
   consensus::LeaderStatus LeaderStatus(bool allow_stale = false) const;
   Result<HybridTime> LeaderSafeTime() const override;
 
-  HybridTime HtLeaseExpiration() const override;
+  Result<HybridTime> HtLeaseExpiration() const override;
 
   const scoped_refptr<log::LogAnchorRegistry>& log_anchor_registry() const {
     return log_anchor_registry_;
@@ -499,7 +495,7 @@ class TabletPeer : public std::enable_shared_from_this<TabletPeer>,
   TabletWeakPtr tablet_weak_;
   std::atomic<TabletObjectState> tablet_obj_state_{TabletObjectState::kUninitialized};
 
-  std::shared_ptr<consensus::RaftConsensus> consensus_;
+  std::shared_ptr<consensus::RaftConsensus> consensus_ GUARDED_BY(lock_);
   std::unique_ptr<TabletStatusListener> status_listener_;
   simple_spinlock prepare_replicate_lock_;
 
@@ -553,7 +549,7 @@ class TabletPeer : public std::enable_shared_from_this<TabletPeer>,
 
   Result<FixedHybridTimeLease> HybridTimeLease(HybridTime min_allowed, CoarseTimePoint deadline);
   Result<HybridTime> PreparePeerRequest() override;
-  void MajorityReplicated() override;
+  Status MajorityReplicated() override;
   void ChangeConfigReplicated(const consensus::RaftConfigPB& config) override;
   uint64_t NumSSTFiles() override;
   void ListenNumSSTFilesChanged(std::function<void()> listener) override;
@@ -570,6 +566,9 @@ class TabletPeer : public std::enable_shared_from_this<TabletPeer>,
   bool IsLeader() override {
     return LeaderTerm() != OpId::kUnknownTerm;
   }
+
+  // Returns the consensus. Can be nullptr.
+  std::shared_ptr<consensus::RaftConsensus> GetRaftConsensusUnsafe() const EXCLUDES(lock_);
 
   TabletSplitter* tablet_splitter_;
 

src/yb/integration-tests/mini_cluster.cc
@@ -841,7 +841,7 @@ void StepDownAllTablets(MiniCluster* cluster) {
       consensus::LeaderStepDownRequestPB req;
       req.set_tablet_id(peer->tablet_id());
       consensus::LeaderStepDownResponsePB resp;
-      ASSERT_OK(peer->consensus()->StepDown(&req, &resp));
+      ASSERT_OK(ASSERT_RESULT(peer->GetConsensus())->StepDown(&req, &resp));
     }
   }
 }
@@ -854,7 +854,7 @@ void StepDownRandomTablet(MiniCluster* cluster) {
     consensus::LeaderStepDownRequestPB req;
     req.set_tablet_id(peer->tablet_id());
     consensus::LeaderStepDownResponsePB resp;
-    ASSERT_OK(peer->consensus()->StepDown(&req, &resp));
+    ASSERT_OK(ASSERT_RESULT(peer->GetConsensus())->StepDown(&req, &resp));
   }
 }
 
@@ -898,16 +898,18 @@ std::vector<tablet::TabletPeerPtr> ListTabletPeers(
         if (!filter_transaction_status_tablets(peer)) {
           return false;
         }
-        auto consensus = peer->shared_consensus();
-        return consensus && consensus->GetLeaderStatus() != consensus::LeaderStatus::NOT_LEADER;
+        auto consensus_result = peer->GetConsensus();
+        return consensus_result &&
+               consensus_result.get()->GetLeaderStatus() != consensus::LeaderStatus::NOT_LEADER;
       });
     case ListPeersFilter::kNonLeaders:
       return ListTabletPeers(cluster, [filter_transaction_status_tablets](const auto& peer) {
         if (!filter_transaction_status_tablets(peer)) {
           return false;
         }
-        auto consensus = peer->shared_consensus();
-        return consensus && consensus->GetLeaderStatus() == consensus::LeaderStatus::NOT_LEADER;
+        auto consensus_result = peer->GetConsensus();
+        return consensus_result &&
+               consensus_result.get()->GetLeaderStatus() == consensus::LeaderStatus::NOT_LEADER;
       });
   }
 
@@ -927,10 +929,11 @@ std::vector<tablet::TabletPeerPtr> ListTabletPeers(
     for (const auto& peer : peers) {
       WARN_NOT_OK(
           WaitFor(
-              [peer] { return peer->consensus() != nullptr || peer->IsShutdownStarted(); }, 5s,
+              [peer] { return peer->GetConsensus() || peer->IsShutdownStarted(); },
+              5s,
               Format("Waiting peer T $0 P $1 ready", peer->tablet_id(), peer->permanent_uuid())),
           "List tablet peers failure");
-      if (peer->consensus() != nullptr && filter(peer)) {
+      if (peer->GetConsensus() && filter(peer)) {
         result.push_back(peer);
       }
     }
@@ -979,7 +982,8 @@ std::vector<tablet::TabletPeerPtr> ListTableActiveTabletLeadersPeers(
     return peer->tablet_metadata() && IsForTable(*peer, table_id) &&
            peer->tablet_metadata()->tablet_data_state() !=
                tablet::TabletDataState::TABLET_DATA_SPLIT_COMPLETED &&
-           peer->consensus()->GetLeaderStatus() != consensus::LeaderStatus::NOT_LEADER;
+           CHECK_RESULT(peer->GetConsensus())->GetLeaderStatus() !=
+               consensus::LeaderStatus::NOT_LEADER;
   });
 }
 
@@ -1000,7 +1004,7 @@ std::vector<tablet::TabletPeerPtr> ListTableActiveTabletPeers(
 std::vector<tablet::TabletPeerPtr> ListActiveTabletLeadersPeers(MiniCluster* cluster) {
   return ListTabletPeers(cluster, [](const auto& peer) {
     const auto tablet_meta = peer->tablet_metadata();
-    const auto consensus = peer->shared_consensus();
+    const auto consensus = CHECK_RESULT(peer->GetConsensus());
     return tablet_meta && tablet_meta->table_type() != TableType::TRANSACTION_STATUS_TABLE_TYPE &&
            IsActive(tablet_meta->tablet_data_state()) &&
            consensus->GetLeaderStatus() != consensus::LeaderStatus::NOT_LEADER;
@@ -1041,9 +1045,9 @@ tserver::MiniTabletServer* GetLeaderForTablet(
 Result<tablet::TabletPeerPtr> GetLeaderPeerForTablet(
     MiniCluster* cluster, const std::string& tablet_id) {
   auto leaders = ListTabletPeers(cluster, [&tablet_id](const auto& peer) {
-    auto consensus = peer->shared_consensus();
-    return tablet_id == peer->tablet_id() && consensus &&
-           consensus->GetLeaderStatus() == consensus::LeaderStatus::LEADER_AND_READY;
+    auto consensus_result = peer->GetConsensus();
+    return tablet_id == peer->tablet_id() && consensus_result &&
+           consensus_result.get()->GetLeaderStatus() == consensus::LeaderStatus::LEADER_AND_READY;
   });
 
   SCHECK_EQ(
@@ -1069,13 +1073,16 @@ Result<std::vector<tablet::TabletPeerPtr>> WaitForTableActiveTabletLeadersPeers(
 
 Status WaitUntilTabletHasLeader(
     MiniCluster* cluster, const string& tablet_id, MonoTime deadline) {
-  return Wait([cluster, &tablet_id] {
-    auto tablet_peers = ListTabletPeers(cluster, [&tablet_id](auto peer) {
-      return peer->tablet_id() == tablet_id
-          && peer->consensus()->GetLeaderStatus() != consensus::LeaderStatus::NOT_LEADER;
-    });
-    return tablet_peers.size() == 1;
-  }, deadline, "Waiting for election in tablet " + tablet_id);
+  return Wait(
+      [cluster, &tablet_id] {
+        auto tablet_peers = ListTabletPeers(cluster, [&tablet_id](auto peer) {
+          auto consensus_result = peer->GetConsensus();
+          return peer->tablet_id() == tablet_id && consensus_result &&
+                 consensus_result.get()->GetLeaderStatus() != consensus::LeaderStatus::NOT_LEADER;
+        });
+        return tablet_peers.size() == 1;
+      },
+      deadline, "Waiting for election in tablet " + tablet_id);
 }
 
 Status WaitUntilMasterHasLeader(MiniCluster* cluster, MonoDelta timeout) {
@@ -1109,7 +1116,7 @@ Status StepDown(
     req.set_force_step_down(true);
   }
   consensus::LeaderStepDownResponsePB resp;
-  RETURN_NOT_OK(leader->consensus()->StepDown(&req, &resp));
+  RETURN_NOT_OK(VERIFY_RESULT(leader->GetConsensus())->StepDown(&req, &resp));
   if (resp.has_error()) {
     return STATUS_FORMAT(RuntimeError, "Step down failed: $0", resp);
   }
@@ -1375,7 +1382,7 @@ Status WaitAllReplicasSynchronizedWithLeader(
   auto leaders = ListTabletPeers(cluster, ListPeersFilter::kLeaders);
   std::unordered_map<TabletId, int64_t> last_committed_idx;
   for (const auto& peer : leaders) {
-    auto idx = peer->consensus()->GetLastCommittedOpId().index;
+    auto idx = VERIFY_RESULT(peer->GetConsensus())->GetLastCommittedOpId().index;
     last_committed_idx.emplace(peer->tablet_id(), idx);
     LOG(INFO) << "Committed op id for " << peer->tablet_id() << ": " << idx;
   }
@@ -1385,11 +1392,12 @@ Status WaitAllReplicasSynchronizedWithLeader(
     if (it == last_committed_idx.end()) {
       return STATUS_FORMAT(IllegalState, "Unknown committed op id for $0", peer->tablet_id());
     }
-    RETURN_NOT_OK(Wait([idx = it->second, peer]() {
-      return peer->consensus()->GetLastCommittedOpId().index >= idx;
-    },
-    deadline, Format("Wait T $0 P $1 commit $2",
-                     peer->tablet_id(), peer->permanent_uuid(), it->second)));
+    RETURN_NOT_OK(Wait(
+        [idx = it->second, peer]() -> Result<bool> {
+          return VERIFY_RESULT(peer->GetConsensus())->GetLastCommittedOpId().index >= idx;
+        },
+        deadline,
+        Format("Wait T $0 P $1 commit $2", peer->tablet_id(), peer->permanent_uuid(), it->second)));
   }
   return Status::OK();
 }

src/yb/cdc/cdc_service.cc
@@ -743,13 +743,7 @@ Status DoUpdateCDCConsumerOpId(
     const std::shared_ptr<tablet::TabletPeer>& tablet_peer,
     const OpId& checkpoint,
     const TabletId& tablet_id) {
-  std::shared_ptr<consensus::Consensus> shared_consensus = tablet_peer->shared_consensus();
-
-  if (shared_consensus == nullptr) {
-    return STATUS_FORMAT(InternalError, "Failed to get tablet $0 peer consensus", tablet_id);
-  }
-
-  shared_consensus->UpdateCDCConsumerOpId(checkpoint);
+  VERIFY_RESULT(tablet_peer->GetConsensus())->UpdateCDCConsumerOpId(checkpoint);
   return Status::OK();
 }
 

commit 8e4dd6530930b621f7fbbaf7bbd282a344fd4841

[#19033] docdb: PGSQL operation triggered during parent tablet shutting down may be not retried
Summary:
A Pgsql operation, triggered during parent tablet shutting down may be not retried and return
an error back to PG layer in some rare cases. The issue was several times during local testing
on a dev server and in jenkins runs and was related to select statement:
```
[ts-1] W0904 21:41:31.858565 16536 tablet_rpc.cc:464] Not found (yb/tablet/tablet_peer.cc:1366): Failed Read(tablet: 0ed5e8c690a7451bb42b3c71c8c7aa55, num_ops: 1, num_attempts: 1, txn: 00000000-0000-0000-0000-000000000000, subtxn: [none]) to tablet 0ed5e8c690a7451bb42b3c71c8c7aa55 on tablet server { uuid: e35f1a6355214712953a70c3616f32a7 private: [host: "127.0.0.2" port: 23080] public: [host: "127.0.0.3.ip.yugabyte" port: 23080] cloud_info: placement_cloud: "cloud1" placement_region: "rack1" placement_zone: "zone" after 1 attempt(s): Tablet peer T 0ed5e8c690a7451bb42b3c71c8c7aa55 P e35f1a6355214712953a70c3616f32a7 [state=QUIESCING]:  is shutting down (tablet server error 12)
...
[ts-1] I0904 21:41:31.861160 16401 raft_consensus.cc:2765] T 0ed5e8c690a7451bb42b3c71c8c7aa55 P e35f1a6355214712953a70c3616f32a7 [term 1 LEADER]: Shutdown.
../../src/yb/yql/pgwrapper/pg_tablet_split-test.cc:535: Failure
Failed
Bad status: Network error (yb/yql/pgwrapper/libpq_utils.cc:355): Fetch 'SELECT * FROM t ORDER BY k ASC' failed: 7, message: ERROR:  Tablet peer T 0ed5e8c690a7451bb42b3c71c8c7aa55 P e35f1a6355214712953a70c3616f32a7 [state=QUIESCING]:  is shutting down (pgsql error XX000) (aux msg ERROR:  Tablet peer T 0ed5e8c690a7451bb42b3c71c8c7aa55 P e35f1a6355214712953a70c3616f32a7 [state=QUIESCING]:  is shutting down)
```

It is expected that the error should be handled correctly on tserver's yb-client side and there's
a sufficient handling of ShutdownInProgress status which is expected to be returned by a tablet
in QUIESCING state (IllegalState is also a valid status for QUIESCING state).

However, it turns out that consensus logic returns `NotFound` status for the shutting down case,
and later the status is extended with `TabletServerErrorPB::TABLET_NOT_RUNNING` error code. Such
combination is not expected and is not considered as retryable operation by yb-client.

The issue is a regression after eed270f2e6d727c2909383175c75fe08ce4abe7e / D26335, which refactors

src/yb/tablet/tablet_peer.cc
@@ -225,6 +225,8 @@ Status TabletPeer::InitTabletPeer(
     std::this_thread::sleep_for(FLAGS_TEST_delay_init_tablet_peer_ms * 1ms);
   }
 
+  // Additional `consensus` variable is required for the out-of-lock-block usage in this method.
+  shared_ptr<consensus::RaftConsensus> consensus;
   {
     std::lock_guard lock(lock_);
     auto state = state_.load(std::memory_order_acquire);
@@ -306,6 +308,7 @@ Status TabletPeer::InitTabletPeer(
         retryable_requests_manager,
         multi_raft_manager);
     has_consensus_.store(true, std::memory_order_release);
+    consensus = consensus_;
 
     auto flush_retryable_requests_pool_token = flush_retryable_requests_pool
         ? flush_retryable_requests_pool->NewToken(ThreadPool::ExecutionMode::SERIAL) : nullptr;
@@ -323,7 +326,7 @@ Status TabletPeer::InitTabletPeer(
   }
   // End of lock scope for lock_.
 
-  auto raft_config = VERIFY_RESULT(GetRaftConsensus())->CommittedConfig();
+  auto raft_config = consensus->CommittedConfig();
   ChangeConfigReplicated(raft_config);  // Set initial flag value.
 
   RETURN_NOT_OK(prepare_thread_->Start());
@@ -429,6 +432,7 @@ Status TabletPeer::Start(const ConsensusBootstrapInfo& bootstrap_info) {
     VLOG_WITH_PREFIX(2) << "Peer starting";
 
     auto consensus = GetRaftConsensusUnsafe();
+    CHECK_NOTNULL(consensus.get()); // Sanity check, consensus must be alive at this point.
     VLOG(2) << "RaftConfig before starting: " << consensus->CommittedConfig().DebugString();
 
     // If tablet was previously considered shutdown w.r.t. metrics,
@@ -1366,9 +1370,12 @@ Result<std::shared_ptr<consensus::Consensus>> TabletPeer::GetConsensus() const {
 
 Result<shared_ptr<consensus::RaftConsensus>> TabletPeer::GetRaftConsensus() const {
   std::lock_guard lock(lock_);
-  SCHECK(!IsShutdownStarted(), NotFound, "Tablet peer $0 is shutting down", LogPrefix());
-  SCHECK(consensus_, IllegalState, "Consensus not yet initialized for tablet peer $0", LogPrefix());
-
+  // Cannot use NotFound status for the shutting down case as later the status may be extended with
+  // TabletServerErrorPB::TABLET_NOT_RUNNING error code, and this combination of the status and
+  // the code is not expected and is not considered as a retryable operation at least by yb-client.
+  // Refer to https://github.com/yugabyte/yugabyte-db/issues/19033 for the details.
+  SCHECK(!IsShutdownStarted(), IllegalState, "Tablet peer $0 is shutting down", LogPrefix());
+  SCHECK(consensus_, IllegalState, "Tablet peer $0 is not started yet", LogPrefix());
   return consensus_;
 }
 

src/yb/tablet/tablet_peer.h
@@ -240,7 +240,7 @@ class TabletPeer : public std::enable_shared_from_this<TabletPeer>,
   bool ShouldApplyWrite() override;
 
   // Returns valid shared pointer to the consensus. Returns a not OK status if the consensus is not
-  // in a valid state.
+  // in a valid state or a peer is not running (shutting down or shut down).
   Result<std::shared_ptr<consensus::Consensus>> GetConsensus() const EXCLUDES(lock_);
   Result<std::shared_ptr<consensus::RaftConsensus>> GetRaftConsensus() const EXCLUDES(lock_);
 

src/yb/integration-tests/mini_cluster.cc
@@ -928,6 +928,8 @@ std::vector<tablet::TabletPeerPtr> ListTabletPeers(
     auto peers = server->tablet_manager()->GetTabletPeers();
     for (const auto& peer : peers) {
       WARN_NOT_OK(
+          // Checking for consensus is not enough here, we also need to not wait for peers
+          // which are being shut down -- these peers are being filtered out by next statement.
           WaitFor(
               [peer] { return peer->GetConsensus() || peer->IsShutdownStarted(); },
               5s,