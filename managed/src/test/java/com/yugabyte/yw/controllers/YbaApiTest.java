2033a3c4f57fd9fd520c81ca3ee33f358644cab2
Aleksandr Malyshev <amalyshev@yugabyte.com>
2023-10-18 17:33:52 +0300

    [PLAT-10937] Alert, PA, Maintenance and few other APIs scrubbing
    Summary:
    Marked APIs according to the doc.
    Also modified api test to output all errors at once - as it's easier to fix them all at the same time that way.
    
    Test Plan: sbt swaggerGen
    
    Reviewers: sneelakantan, #yba-api-review
    
    Reviewed By: sneelakantan, #yba-api-review
    
    Subscribers: yugaware
    
    Differential Revision: https://phorge.dev.yugabyte.com/D29462

--- managed/src/test/java/com/yugabyte/yw/controllers/YbaApiTest.java
+++ managed/src/test/java/com/yugabyte/yw/controllers/YbaApiTest.java
@@ -10,6 +10,8 @@
 
 package com.yugabyte.yw.controllers;
 
+import static org.junit.Assert.fail;
+
 import com.google.common.base.Strings;
 import com.yugabyte.yw.common.FakeDBApplication;
 import com.yugabyte.yw.models.common.YbaApi;
@@ -22,13 +24,7 @@ import java.io.InputStreamReader;
 import java.lang.annotation.Annotation;
 import java.lang.reflect.Field;
 import java.lang.reflect.Method;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.PriorityQueue;
-import java.util.Queue;
-import java.util.Set;
+import java.util.*;
 import java.util.stream.Collectors;
 import org.junit.Assert;
 import org.junit.BeforeClass;
@@ -152,7 +148,7 @@ public class YbaApiTest extends FakeDBApplication {
       default:
         // Cannot come here, since setting the visibility is mandatory in a YbaApi
         // annotation
-        Assert.fail("Setting the visibility is mandatory in a YbaApi annotation");
+        fail("Setting the visibility is mandatory in a YbaApi annotation");
         return null;
     }
   }
@@ -166,8 +162,13 @@ public class YbaApiTest extends FakeDBApplication {
 
   @Test
   public void verifyYbaApiAnnotatedMethods() {
-    for (String className : ybaApiAnnotatedMethods.keySet()) {
-      Set<Method> ybaApiMethods = ybaApiAnnotatedMethods.get(className);
+    List<String> errorMessages = new ArrayList<>();
+    List<String> sortedClasses = ybaApiAnnotatedMethods.keySet().stream().sorted().toList();
+    for (String className : sortedClasses) {
+      List<Method> ybaApiMethods =
+          ybaApiAnnotatedMethods.get(className).stream()
+              .sorted(Comparator.comparing(Method::getName))
+              .toList();
       for (Method m : ybaApiMethods) {
         String errMsg = "For " + className + "." + m.getName() + "(): ";
         // A method having @YbaApi should also have @ApiOperation
@@ -180,20 +181,27 @@ public class YbaApiTest extends FakeDBApplication {
           annDescription = apiOpAnn.value();
         } else if (isGetterSetter) {
           ApiModelProperty apiPropAnn = m.getAnnotation(ApiModelProperty.class);
-          Assert.assertNotNull(
-              errMsg + "Add corresponding @ApiModelProperty annotation", apiPropAnn);
+          if (apiPropAnn == null) {
+            errorMessages.add(errMsg + "Add corresponding @ApiModelProperty annotation");
+            continue;
+          }
           annDescription = apiPropAnn.value();
         } else {
-          Assert.fail(errMsg + "Add corresponding @ApiOperation annotation");
+          errorMessages.add(errMsg + "Add corresponding @ApiOperation annotation");
+          continue;
         }
         // @Deprecated should exist only for visibility=DEPRECATED
         if (!isGetterSetter && ybaApiAnn.visibility().equals(YbaApiVisibility.DEPRECATED)) {
-          Assert.assertNotNull(
-              errMsg + "Should have @Deprecated annotation", m.getAnnotation(Deprecated.class));
+          if (m.getAnnotation(Deprecated.class) == null) {
+            errorMessages.add(errMsg + "Should have @Deprecated annotation");
+            continue;
+          }
         } else {
-          Assert.assertFalse(
-              "@Deprecated can be added only along with @YbaApi(visbility=DEPRECATED)",
-              m.isAnnotationPresent(Deprecated.class));
+          if (m.isAnnotationPresent(Deprecated.class)) {
+            errorMessages.add(
+                errMsg + "@Deprecated can be added only along with @YbaApi(visbility=DEPRECATED)");
+            continue;
+          }
         }
         // @ApiOperation should have a message corresponding to the visibility of @YbaApi
         String expectedVisibilityMsg = expectedVisibilityMessage(ybaApiAnn);
@@ -202,38 +210,58 @@ public class YbaApiTest extends FakeDBApplication {
                 + (isGetterSetter ? "@ApiModelProperty(value = \"" : "@ApiOperation(value = \"")
                 + expectedVisibilityMsg
                 + ". <...anything else here...>\")";
-        Assert.assertNotNull(errMsg, annDescription);
-        Assert.assertTrue(errMsg, annDescription.contains(expectedVisibilityMsg));
+        if (annDescription == null
+            || (expectedVisibilityMsg != null && !annDescription.contains(expectedVisibilityMsg))) {
+          errorMessages.add(errMsg);
+        }
         // validate the runtimeConfig flag of @YbaApi if present
         verifyRuntimeConfig(ybaApiAnn);
 
         // TODO: validate the params
       }
     }
+    if (!errorMessages.isEmpty()) {
+      fail("\n" + String.join("\n", errorMessages));
+    }
   }
 
   @Test
   public void verifyYbaApiAnnotatedFields() {
-    for (String className : ybaApiAnnotatedFields.keySet()) {
-      Set<Field> ybaApiFields = ybaApiAnnotatedFields.get(className);
+    List<String> errorMessages = new ArrayList<>();
+    List<String> sortedClasses = ybaApiAnnotatedFields.keySet().stream().sorted().toList();
+    for (String className : sortedClasses) {
+      List<Field> ybaApiFields =
+          ybaApiAnnotatedFields.get(className).stream()
+              .sorted(Comparator.comparing(Field::getName))
+              .toList();
       for (Field f : ybaApiFields) {
         String errMsg = "For " + className + "." + f.getName() + ": ";
         // A field having @YbaApi should also have @ApiModelProperty
         YbaApi ybaApiAnn = f.getAnnotation(YbaApi.class);
         Assert.assertNotNull(errMsg, ybaApiAnn);
         ApiModelProperty apiPropAnn = f.getAnnotation(ApiModelProperty.class);
-        Assert.assertNotNull(errMsg + "Add corresponding @ApiModelProperty annotation", apiPropAnn);
+        if (apiPropAnn == null) {
+          errorMessages.add(errMsg + "Add corresponding @ApiModelProperty annotation");
+          continue;
+        }
         // @ApiModelProperty should have a message corresponding to the visibility of @YbaApi
         String expectedVisibilityMsg = expectedVisibilityMessage(ybaApiAnn);
         errMsg +=
             "Please update corresponding @ApiModelProperty(value = \""
                 + expectedVisibilityMsg
                 + ". <...anything else here...>\")";
-        Assert.assertNotNull(errMsg, apiPropAnn.value());
-        Assert.assertTrue(errMsg, apiPropAnn.value().contains(expectedVisibilityMsg));
+        if (apiPropAnn.value() == null
+            || (expectedVisibilityMsg != null
+                && !apiPropAnn.value().contains(expectedVisibilityMsg))) {
+          errorMessages.add(errMsg);
+          continue;
+        }
         // validate the runtimeConfig flag of @YbaApi if present
         verifyRuntimeConfig(ybaApiAnn);
       }
+    }
+    if (!errorMessages.isEmpty()) {
+      fail("\n" + String.join("\n", errorMessages));
     }
   }
 
@@ -261,8 +289,13 @@ public class YbaApiTest extends FakeDBApplication {
 
   @Test
   public void verifyApiOperationMethods() {
-    for (String className : allApiMethods.keySet()) {
-      Set<Method> apiMethods = allApiMethods.get(className);
+    List<String> errorMessages = new ArrayList<>();
+    List<String> sortedClasses = allApiMethods.keySet().stream().sorted().toList();
+    for (String className : sortedClasses) {
+      List<Method> apiMethods =
+          allApiMethods.get(className).stream()
+              .sorted(Comparator.comparing(Method::getName))
+              .toList();
       for (Method m : apiMethods) {
         String errMsg = "For " + className + "." + m.getName() + "(): ";
         ApiOperation apiOpAnn = m.getAnnotation(ApiOperation.class);
@@ -270,29 +303,45 @@ public class YbaApiTest extends FakeDBApplication {
         if (expectedVisibility != null) {
           errMsg += "Expected @YbaApi(visibility=" + expectedVisibility.name() + ")";
           YbaApi ybaApi = m.getAnnotation(YbaApi.class);
-          Assert.assertNotNull(errMsg, ybaApi);
-          Assert.assertTrue(errMsg, ybaApi.visibility().equals(expectedVisibility));
+          if (ybaApi == null || !ybaApi.visibility().equals(expectedVisibility)) {
+            errorMessages.add(errMsg);
+          }
         }
       }
+    }
+    if (!errorMessages.isEmpty()) {
+      fail("\n" + String.join("\n", errorMessages));
     }
   }
 
   @Test
   public void verifyApiModelPropertyFields() {
-    for (String className : allApiFields.keySet()) {
-      Set<Field> apiFields = allApiFields.get(className);
+    List<String> errorMessages = new ArrayList<>();
+    List<String> sortedClasses = allApiFields.keySet().stream().sorted().toList();
+    for (String className : sortedClasses) {
+      List<Field> apiFields =
+          allApiFields.get(className).stream()
+              .sorted(Comparator.comparing(Field::getName))
+              .toList();
       for (Field f : apiFields) {
         String errMsg = "For " + className + "." + f.getName() + ": ";
         ApiModelProperty apiPropAnn = f.getAnnotation(ApiModelProperty.class);
-        Assert.assertNotNull(errMsg + "Add corresponding @ApiModelProperty annotation", apiPropAnn);
+        if (apiPropAnn == null) {
+          errorMessages.add(errMsg + "Add corresponding @ApiModelProperty annotation");
+          continue;
+        }
         YbaApiVisibility expectedVisibility = expectedVisibility(apiPropAnn.value());
         if (expectedVisibility != null) {
           errMsg += "Expected @YbaApi(visibility=" + expectedVisibility.name() + ")";
           YbaApi ybaApiAnn = f.getAnnotation(YbaApi.class);
-          Assert.assertNotNull(errMsg, ybaApiAnn);
-          Assert.assertTrue(errMsg, ybaApiAnn.visibility().equals(expectedVisibility));
+          if (ybaApiAnn == null || !ybaApiAnn.visibility().equals(expectedVisibility)) {
+            errorMessages.add(errMsg);
+          }
         }
       }
+    }
+    if (!errorMessages.isEmpty()) {
+      fail("\n" + String.join("\n", errorMessages));
     }
   }
 }
