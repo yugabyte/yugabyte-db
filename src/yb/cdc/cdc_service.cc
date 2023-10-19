68ddefa74c4d42b1494735481d0ea8324e126aec
Vaibhav Kushwaha <vkushwaha@yugabyte.com>
2023-10-19 05:57:29 +0000

    [2.18][#19428] Return error to client if scan of cdc_state fails
    Summary:
    Without this diff, if there is a failure while reading the `cdc_state` then we log the error but we do not return the error to client. So in this case, if the read fails and we have only read half the `cdc_state` table, we will end up returning an incorrect list of tablets.
    
    This diff modifies the behaviour and adds code to return the error to the client so that client can take action accordingly. Ideally, we want this error to be retried by the client.
    
    **Reason for not landing to master/2.20:**
    The code changes this diff is introducing are already present in master because of the refactors which happened there so the bug fix is not needed there.
    
    Test Plan:
    Jenkins: skip
    
    Run existing tests to ensure no regression.
    
    Reviewers: skumar, stiwary, hsunder
    
    Reviewed By: skumar, hsunder
    
    Subscribers: ycdcxcluster
    
    Tags: #jenkins-ready
    
    Differential Revision: https://phorge.dev.yugabyte.com/D29457

--- src/yb/cdc/cdc_service.cc
+++ src/yb/cdc/cdc_service.cc
@@ -1328,8 +1328,12 @@ void CDCServiceImpl::GetTabletListToPollForCDC(
 
     resp->mutable_tablet_checkpoint_pairs()->Reserve(
         static_cast<int>(tablet_checkpoint_pairs.size()));
+    LOG(INFO) << "Processing tablets for table " << table_id
+              << " for GetTabletListToPollForCDCResponse";
     for (auto tablet_checkpoint_pair : tablet_checkpoint_pairs) {
       auto tablet_checkpoint_pair_pb = resp->add_tablet_checkpoint_pairs();
+
+      LOG(INFO) << "Adding tablet " << tablet_checkpoint_pair.first;
 
       tablet_checkpoint_pair_pb->mutable_tablet_locations()->CopyFrom(
           tablet_id_to_tablet_locations_map[tablet_checkpoint_pair.first]);
@@ -2880,6 +2884,15 @@ Status CDCServiceImpl::GetTabletIdsToPoll(
     if (add_to_result) {
       result->push_back(std::make_pair(tablet_id, checkpoint_pb));
     }
+  }
+
+  // If scanning cdc_state table fails, return the error to the client so that it can be retried
+  // accordingly.
+  if (!failer_status.ok()) {
+    RefreshCdcStateTable();
+    return STATUS_FORMAT(
+        IllegalState, "Failed to scan table $0: $1", kCdcStateTableName.table_name(),
+        failer_status);
   }
 
   return Status::OK();
