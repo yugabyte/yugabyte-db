352f6764978670ff2635d188e2dbbea12b5c13a6
Jason Kim <git@jasonk.me>
2023-10-19 17:51:33 -0700

    [PG15] Port regress for 02ddd499322a
    Summary:
    Port regress test changes as in upstream PG commit
    02ddd499322ab6f2f0d58692955dc9633c2150fc, titled
    
        Change floating-point output format for improved performance.
    
    - contrib/btree_gist: skip (tests never ported nor supported)
    - contrib/cube
      - cube: skip one pair of extra_float_digits GUC set/unset because the
        relevant query is not ported.
      - cube_sci: skip (test was never ported)
    - contrib/pg_trgm
    - contrib/seg: skip (test was never ported)
    - regress
      - aggregates
      - circle
      - float4-misrounded-input: skip (test was never ported)
      - float4: skip newly added queries
      - float8-small-is-zero: skip (test was never ported)
      - float8: skip newly added queries except for the changes related to
        manipulating the GUC extra_float_digits.
      - int8
      - jsonb
      - line: also port similarly to an additional query that should have
        been removed by upstream PG commit
        a3d2844852dc664718320b15cbc6d6bfa264e66e (that commit is not ported
        yet).
      - point
      - rules
      - tsearch: skip (relevant query was never ported)
      - tstypes
      - updatable_views: skip (test was never ported)
      - window
    
    Test Plan:
    Manually check yb_pg_int8 (part of TestPgRegressTypesNumeric) only fails
    on differences related to the lines starting with
    
        ERROR:  invalid input syntax for type
    
    Jenkins: rebase: pg15, test regex: .*TestPgRegress.*, build type: release
    
    Reviewers: aagrawal, tfoucher
    
    Reviewed By: tfoucher
    
    Subscribers: yql
    
    Differential Revision: https://phorge.dev.yugabyte.com/D29549

--- src/postgres/src/test/regress/sql/yb_pg_circle.sql
+++ src/postgres/src/test/regress/sql/yb_pg_circle.sql
@@ -2,6 +2,9 @@
 -- CIRCLE
 --
 
+-- avoid bit-exact output here because operations may not be bit-exact.
+SET extra_float_digits = 0;
+
 CREATE TABLE CIRCLE_TBL (id int primary key, f1 circle);
 
 INSERT INTO CIRCLE_TBL VALUES (1, '<(5,1),3>');
