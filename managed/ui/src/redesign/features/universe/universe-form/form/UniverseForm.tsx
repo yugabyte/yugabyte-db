cd70bf933647100ecd24bf2b1ad75ed27f19a6b0
Rajagopalan Madhavan <rmadhavan@yugabyte.com>
2023-10-25 13:40:41 -0700

    [PLAT-11083]: Make multiline GFlag option runtime configurable
    Summary: Make multiline GFlag option runtime configurable
    
    Test Plan:
    Please refer to the video
    {F123149}
    
    Reviewers: jmak
    
    Reviewed By: jmak
    
    Subscribers: yugaware
    
    Differential Revision: https://phorge.dev.yugabyte.com/D29661

--- managed/ui/src/redesign/features/universe/universe-form/form/UniverseForm.tsx
+++ managed/ui/src/redesign/features/universe/universe-form/form/UniverseForm.tsx
@@ -268,7 +268,7 @@ export const UniverseForm: FC<UniverseFormProps> = ({
             <AdvancedConfiguration />
           </>
         )}
-        <GFlags />
+        <GFlags runtimeConfigs={runtimeConfigs} />
         {isPrimary && <HelmOverrides />}
         <UserTags />
       </>
