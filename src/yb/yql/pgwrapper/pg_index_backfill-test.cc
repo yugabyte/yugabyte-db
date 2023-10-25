yugabyte/yugabyte-db

commit 660e13af4a32cc3b1022109bb1db08cee3db5b3c

[#3520] DocDB: Use packed for for insert and check it during read
Summary:
This diff adds PackedRow usage during YSQL insert.
Also PackedRow is handled during read.

Introduced flag max_packed_row_columns to specify max number of columns in packed row.
By default its value is -1, that disables packed rows. It is required because of backward compatibility.

Only value columns are counted. If table has only key columns, then number of value columns will be 0.
So it we set this value to 0, then packed rows will be active for such tables.
Because of that we use -1 to disable this feature.

Updated tablet metadata to store schema packing for old schema versions.

Test Plan: PgMiniTest.PackedRow

Reviewers: timur, bogdan

Reviewed By: timur, bogdan

Subscribers: rthallam, ybase

Differential Revision: https://phabricator.dev.yugabyte.com/D16344

src/yb/docdb/doc_read_context.h
@@ -1,0 +1,58 @@
+// Copyright (c) YugaByte, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+// in compliance with the License.  You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software distributed under the License
+// is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+// or implied.  See the License for the specific language governing permissions and limitations
+// under the License.
+//
+
+#ifndef YB_DOCDB_DOC_READ_CONTEXT_H
+#define YB_DOCDB_DOC_READ_CONTEXT_H
+
+#include "yb/common/schema.h"
+
+#include "yb/docdb/packed_row.h"
+
+namespace yb {
+namespace docdb {
+
+struct DocReadContext {
+  DocReadContext() = default;
+
+  DocReadContext(const Schema& schema_, uint32_t schema_version) : schema(schema_) {
+    schema_packing_storage.AddSchema(schema_version, schema_);
+  }
+
+  DocReadContext(const DocReadContext& rhs, const Schema& schema_, uint32_t schema_version)
+      : schema(schema_), schema_packing_storage(rhs.schema_packing_storage) {
+    schema_packing_storage.AddSchema(schema_version, schema_);
+  }
+
+  template <class PB>
+  CHECKED_STATUS LoadFromPB(const PB& pb) {
+    RETURN_NOT_OK(SchemaFromPB(pb.schema(), &schema));
+    RETURN_NOT_OK(schema_packing_storage.LoadFromPB(pb.old_schema_packings()));
+    schema_packing_storage.AddSchema(pb.schema_version(), schema);
+    return Status::OK();
+  }
+
+  template <class PB>
+  void ToPB(uint32_t schema_version, PB* out) {
+    DCHECK(schema.has_column_ids());
+    SchemaToPB(schema, out->mutable_schema());
+    schema_packing_storage.ToPB(schema_version, out->mutable_old_schema_packings());
+  }
+
+  Schema schema;
+  SchemaPackingStorage schema_packing_storage;
+};
+
+} // namespace docdb
+} // namespace yb
+
+#endif // YB_DOCDB_DOC_READ_CONTEXT_H

src/yb/tablet/tablet.cc
@@ -445,7 +445,7 @@ Tablet::Tablet(const TabletInitData& data)
   if (table_info->index_info && table_info->index_info->is_unique()) {
     unique_index_key_schema_ = std::make_unique<Schema>();
     const auto ids = table_info->index_info->index_key_column_ids();
-    CHECK_OK(table_info->schema->CreateProjectionByIdsIgnoreMissing(
+    CHECK_OK(table_info->schema().CreateProjectionByIdsIgnoreMissing(
         ids, unique_index_key_schema_.get()));
   }
 
@@ -1093,7 +1093,7 @@ Result<std::unique_ptr<docdb::YQLRowwiseIteratorIf>> Tablet::NewRowIterator(
 
   const std::shared_ptr<tablet::TableInfo> table_info =
       VERIFY_RESULT(metadata_->GetTableInfo(table_id));
-  const Schema& schema = *table_info->schema;
+  const Schema& schema = table_info->schema();
   auto mapped_projection = std::make_unique<Schema>();
   RETURN_NOT_OK(schema.GetMappedReadProjection(projection, mapped_projection.get()));
 
@@ -1104,8 +1104,8 @@ Result<std::unique_ptr<docdb::YQLRowwiseIteratorIf>> Tablet::NewRowIterator(
       ? read_hybrid_time
       : ReadHybridTime::SingleTime(VERIFY_RESULT(SafeTime(RequireLease::kFalse)));
   auto result = std::make_unique<DocRowwiseIterator>(
-      std::move(mapped_projection), schema, txn_op_ctx, doc_db(),
-      deadline, read_time, &pending_non_abortable_op_counter_);
+      std::move(mapped_projection), *table_info->doc_read_context, txn_op_ctx,
+      doc_db(), deadline, read_time, &pending_non_abortable_op_counter_);
   RETURN_NOT_OK(result->Init(table_type_, sub_doc_key));
   return std::move(result);
 }
@@ -1114,7 +1114,7 @@ Result<std::unique_ptr<docdb::YQLRowwiseIteratorIf>> Tablet::NewRowIterator(
     const TableId& table_id) const {
   const std::shared_ptr<tablet::TableInfo> table_info =
       VERIFY_RESULT(metadata_->GetTableInfo(table_id));
-  return NewRowIterator(*table_info->schema, {}, table_id);
+  return NewRowIterator(table_info->schema(), {}, table_id);
 }
 
 Status Tablet::ApplyRowOperations(
@@ -1446,12 +1446,12 @@ Status Tablet::HandlePgsqlReadRequest(
   Result<TransactionOperationContext> txn_op_ctx =
       CreateTransactionOperationContext(
           transaction_metadata,
-          table_info->schema->table_properties().is_ysql_catalog_table(),
+          table_info->schema().table_properties().is_ysql_catalog_table(),
           &subtransaction_metadata);
   RETURN_NOT_OK(txn_op_ctx);
-  return AbstractTablet::HandlePgsqlReadRequest(
+  return ProcessPgsqlReadRequest(
       deadline, read_time, is_explicit_request_read_time,
-      pgsql_read_request, *txn_op_ctx, result, num_rows_read);
+      pgsql_read_request, table_info, *txn_op_ctx, result, num_rows_read);
 }
 
 // Returns true if the query can be satisfied by rows present in current tablet.
@@ -1842,9 +1842,9 @@ Status Tablet::MarkBackfillDone(const TableId& table_id) {
   auto table_info = table_id.empty() ?
     metadata_->primary_table_info() : VERIFY_RESULT(metadata_->GetTableInfo(table_id));
   LOG_WITH_PREFIX(INFO) << "Setting backfill as done. Current schema  "
-                        << table_info->schema->ToString();
+                        << table_info->schema().ToString();
   const vector<DeletedColumn> empty_deleted_cols;
-  Schema new_schema = *table_info->schema;
+  Schema new_schema = table_info->schema();
   new_schema.SetRetainDeleteMarkers(false);
   metadata_->SetSchema(
       new_schema, *table_info->index_map, empty_deleted_cols, table_info->schema_version, table_id);
@@ -1855,7 +1855,7 @@ Status Tablet::AlterSchema(ChangeMetadataOperation *operation) {
   auto current_table_info = VERIFY_RESULT(metadata_->GetTableInfo(
         operation->request()->has_alter_table_id() ?
         operation->request()->alter_table_id() : ""));
-  auto key_schema = current_table_info->schema->CreateKeyProjection();
+  auto key_schema = current_table_info->schema().CreateKeyProjection();
 
   RSTATUS_DCHECK_NE(operation->schema(), static_cast<void*>(nullptr), InvalidArgument,
                     "Schema could not be null");
@@ -1875,14 +1875,14 @@ Status Tablet::AlterSchema(ChangeMetadataOperation *operation) {
     return Status::OK();
   }
 
-  LOG_WITH_PREFIX(INFO) << "Alter schema from " << current_table_info->schema->ToString()
+  LOG_WITH_PREFIX(INFO) << "Alter schema from " << current_table_info->schema().ToString()
                         << " version " << current_table_info->schema_version
                         << " to " << operation->schema()->ToString()
                         << " version " << operation->schema_version();
 
   // Find out which columns have been deleted in this schema change, and add them to metadata.
   vector<DeletedColumn> deleted_cols;
-  for (const auto& col : current_table_info->schema->column_ids()) {
+  for (const auto& col : current_table_info->schema().column_ids()) {
     if (operation->schema()->find_column_by_id(col) == Schema::kColumnNotFound) {
       deleted_cols.emplace_back(col, clock_->Now());
       LOG_WITH_PREFIX(INFO) << "Column " << col << " recorded as deleted.";
@@ -1908,7 +1908,7 @@ Status Tablet::AlterSchema(ChangeMetadataOperation *operation) {
 
   // Create transaction manager and index table metadata cache for secondary index update.
   if (!operation->index_map().empty()) {
-    if (current_table_info->schema->table_properties().is_transactional() &&
+    if (current_table_info->schema().table_properties().is_transactional() &&
         !transaction_manager_) {
       transaction_manager_ = std::make_unique<client::TransactionManager>(
           client_future_.get(), scoped_refptr<server::Clock>(clock_), local_tablet_filter_);
@@ -3349,14 +3349,18 @@ Status Tablet::CreateReadIntents(
       /* is_ysql_catalog_table */ pgsql_batch.size() > 0 && is_sys_catalog_,
       &subtransaction_metadata));
 
+  auto table_info = metadata_->primary_table_info();
   for (const auto& ql_read : ql_batch) {
     docdb::QLReadOperation doc_op(ql_read, txn_op_ctx);
-    RETURN_NOT_OK(doc_op.GetIntents(*GetSchema(), write_batch));
+    RETURN_NOT_OK(doc_op.GetIntents(table_info->schema(), write_batch));
   }
 
   for (const auto& pgsql_read : pgsql_batch) {
+    if (table_info == nullptr || table_info->table_id != pgsql_read.table_id()) {
+      table_info = VERIFY_RESULT(metadata_->GetTableInfo(pgsql_read.table_id()));
+    }
     docdb::PgsqlReadOperation doc_op(pgsql_read, txn_op_ctx);
-    RETURN_NOT_OK(doc_op.GetIntents(*GetSchema(pgsql_read.table_id()), write_batch));
+    RETURN_NOT_OK(doc_op.GetIntents(table_info->schema(), write_batch));
   }
 
   return Status::OK();
@@ -3726,12 +3730,10 @@ void Tablet::UnregisterOperationFilterUnlocked(OperationFilter* filter) {
   operation_filters_.erase(operation_filters_.iterator_to(*filter));
 }
 
-SchemaPtr Tablet::GetSchema(const std::string& table_id) const {
-  if (table_id.empty()) {
-    return metadata_->schema();
-  }
-  auto table_info = CHECK_RESULT(metadata_->GetTableInfo(table_id));
-  return SchemaPtr(table_info, table_info->schema.get());
+docdb::DocReadContextPtr Tablet::GetDocReadContext(const std::string& table_id) const {
+  auto table_info = table_id.empty()
+      ? metadata_->primary_table_info() : CHECK_RESULT(metadata_->GetTableInfo(table_id));
+  return docdb::DocReadContextPtr(table_info, table_info->doc_read_context.get());
 }
 
 Schema Tablet::GetKeySchema(const std::string& table_id) const {
@@ -3739,7 +3741,7 @@ Schema Tablet::GetKeySchema(const std::string& table_id) const {
     return *key_schema_;
   }
   auto table_info = CHECK_RESULT(metadata_->GetTableInfo(table_id));
-  return table_info->schema->CreateKeyProjection();
+  return table_info->schema().CreateKeyProjection();
 }
 
 // ------------------------------------------------------------------------------------------------

commit 9d1e331180da7027f7c9f592ec70b7fa889762bb

[#19544] docdb: Hash and Range Indexes grow in size even after rows are deleted, leading to slower queries
Summary:
Schema table properties' field `retain_delete_markers` is not updated properly on index backfilling
is done on tablet server side. This leads to using of HT<min> as a min time to check if tombstones
should be removed from SST file. And as this is the min possible time, they are always kept in SST
files during compaction and it's even not possible to remove them with manual compactions and
with a help of tserver restart as the parameter `retain_delete_markers` is persisted as `true` and
no other backfill done events can happen on an index table created and marked as backfilled.

On getting a backfill done event we were copying the current schema and were adding exactly one
change: `new_schema.SetRetainDeleteMarkers(false)` and then `metadata->SetSchema(new_schema)`.
But the new schema was not applied as a subsequent call to `TableInfo` constructor with new_schema
as an argument was ignoring a new schema as it has a requirement to have a differnt schema_version
to make new schema be applied.

The issue is a regression from https://github.com/yugabyte/yugabyte-db/commit/660e13af4a32cc3b1022109bb1db08cee3db5b3c#diff-76041cb130748abb6ea5213095534a6fceea69c597d19a72c9ca55c078e98524L134

The change introduces a new `TableInfo` constructor that accepts new schema without checking a
schema version and correpondingly reworks a way how backfilling done event is handled on tserver
side. It's not possible to bump a schema version on tserver side and master will have an old version
which will lead to different errors from PG layer.

**The change will work only for newly created indexes and does not have a code to auto-recover from
the buggy state. **
Jira: DB-8331

Test Plan:
ybd --cxx-test pgwrapper_pg_index_backfill-test --gtest_filter PgIndexBackfillTest.RetainDeleteMarkers

src/yb/docdb/doc_read_context.cc
@@ -50,6 +50,13 @@ DocReadContext::DocReadContext(
       << ", added: " << schema_version;
 }
 
+DocReadContext::DocReadContext(const DocReadContext& rhs, const Schema& schema)
+    : schema_packing_storage(rhs.schema_packing_storage), is_index_(rhs.is_index_), schema_(schema),
+      log_prefix_(rhs.log_prefix_) {
+  UpdateKeyPrefix();
+  LOG_WITH_PREFIX(INFO) << "DocReadContext, copy and replace schema";
+}
+
 DocReadContext::DocReadContext(const DocReadContext& rhs, SchemaVersion min_schema_version)
     : schema_packing_storage(rhs.schema_packing_storage, min_schema_version),
       is_index_(rhs.is_index_), schema_(rhs.schema_), log_prefix_(rhs.log_prefix_) {

src/yb/docdb/doc_read_context.h
@@ -33,6 +33,8 @@ struct DocReadContext {
 
   DocReadContext(const DocReadContext& rhs, const Schema& schema, SchemaVersion schema_version);
 
+  DocReadContext(const DocReadContext& rhs, const Schema& schema);
+
   DocReadContext(const DocReadContext& rhs, SchemaVersion min_schema_version);
 
   template <class PB>

src/yb/tablet/tablet.cc
@@ -2173,16 +2173,8 @@ Status Tablet::RemoveTable(const std::string& table_id, const OpId& op_id) {
 }
 
 Status Tablet::MarkBackfillDone(const OpId& op_id, const TableId& table_id) {
-  auto table_info = table_id.empty() ?
-    metadata_->primary_table_info() : VERIFY_RESULT(metadata_->GetTableInfo(table_id));
-  LOG_WITH_PREFIX(INFO) << "Setting backfill as done. Current schema  "
-                        << table_info->schema().ToString();
-  const vector<DeletedColumn> empty_deleted_cols;
-  Schema new_schema = table_info->schema();
-  new_schema.SetRetainDeleteMarkers(false);
-  metadata_->SetSchema(
-      new_schema, *table_info->index_map, empty_deleted_cols, table_info->schema_version,
-      op_id, table_id);
+  LOG_WITH_PREFIX(INFO) << "Setting backfill as done.";
+  metadata_->OnBackfillDone(op_id, table_id);
   return metadata_->Flush();
 }
 

src/yb/tablet/tablet_metadata.cc
@@ -189,6 +189,25 @@ TableInfo::TableInfo(const TableInfo& other,
   CompleteInit();
 }
 
+// Specific case, use for schema update when schema version change does not required.
+// Example: MarkBackfillDone, refer to https://github.com/yugabyte/yugabyte-db/issues/19544.
+TableInfo::TableInfo(const TableInfo& other,
+                     const Schema& schema)
+    : table_id(other.table_id),
+      namespace_name(other.namespace_name),
+      table_name(other.table_name),
+      table_type(other.table_type),
+      cotable_id(other.cotable_id),
+      log_prefix(other.log_prefix),
+      doc_read_context(std::make_shared<docdb::DocReadContext>(*other.doc_read_context, schema)),
+      index_map(std::make_shared<IndexMap>(*other.index_map)),
+      index_info(other.index_info ? new IndexInfo(*other.index_info) : nullptr),
+      schema_version(other.schema_version),
+      partition_schema(other.partition_schema),
+      deleted_cols(other.deleted_cols) {
+  CompleteInit();
+}
+
 TableInfo::TableInfo(const TableInfo& other, SchemaVersion min_schema_version)
     : table_id(other.table_id),
       namespace_name(other.namespace_name),
@@ -1975,6 +1994,26 @@ OpId RaftGroupMetadata::MinUnflushedChangeMetadataOpId() const {
   MutexLock l_flush(flush_lock_);
   std::lock_guard lock(data_mutex_);
   return min_unflushed_change_metadata_op_id_;
+}
+
+void RaftGroupMetadata::OnBackfillDone(const OpId& op_id, const TableId& table_id) {
+  std::lock_guard lock(data_mutex_);
+
+  TableId target_table_id = table_id.empty() ? primary_table_id_ : table_id;
+  auto it = kv_store_.tables.find(target_table_id);
+  CHECK(it != kv_store_.tables.end());
+
+  Schema new_schema = it->second->schema();
+  new_schema.SetRetainDeleteMarkers(false);
+
+  TableInfoPtr new_table_info = std::make_shared<TableInfo>(*it->second, new_schema);
+  VLOG_WITH_PREFIX(1) << raft_group_id_ << " Updating table " << target_table_id
+                      << " to Schema version " << new_table_info->schema_version
+                      << " from \n" << AsString(it->second)
+                      << " to \n" << AsString(new_table_info);
+  it->second.swap(new_table_info);
+
+  OnChangeMetadataOperationAppliedUnlocked(op_id);
 }
 
 bool RaftGroupMetadata::OnPostSplitCompactionDone() {

src/yb/tablet/tablet_metadata.h
@@ -131,6 +131,8 @@ struct TableInfo {
             const qlexpr::IndexMap& index_map,
             const std::vector<DeletedColumn>& deleted_cols,
             SchemaVersion schema_version);
+  TableInfo(const TableInfo& other,
+            const Schema& schema);
   TableInfo(const TableInfo& other, SchemaVersion min_schema_version);
   ~TableInfo();
 
@@ -648,8 +650,11 @@ class RaftGroupMetadata : public RefCountedThreadSafe<RaftGroupMetadata>,
 
   OpId MinUnflushedChangeMetadataOpId() const;
 
-  // Updates related meta data when post split compction as a reaction for post split compaction
-  // completed. Returns true if any field has been updated and a flush may be required.
+  // Updates related meta data as a reaction to index table backfilling is done.
+  void OnBackfillDone(const OpId& op_id, const TableId& table_id);
+
+  // Updates related meta data as a reaction for post split compaction completed. Returns true
+  // if any field has been updated and a flush may be required.
   bool OnPostSplitCompactionDone();
 
  private:

src/yb/yql/pgwrapper/pg_index_backfill-test.cc
@@ -21,6 +21,7 @@
 #include "yb/common/schema.h"
 
 #include "yb/integration-tests/backfill-test-util.h"
+#include "yb/integration-tests/external_mini_cluster_fs_inspector.h"
 
 #include "yb/master/master_admin.proxy.h"
 #include "yb/master/master_admin.pb.h"
@@ -33,6 +34,7 @@
 #include "yb/util/countdown_latch.h"
 #include "yb/util/format.h"
 #include "yb/util/monotime.h"
+#include "yb/util/pb_util.h"
 #include "yb/util/status_format.h"
 #include "yb/util/string_util.h"
 #include "yb/util/test_thread_holder.h"
@@ -266,6 +268,35 @@ void PgIndexBackfillTest::TestRetainDeleteMarkers(const std::string& db_name) {
 
   ASSERT_EQ(table_info->schema.version(), 0);
   ASSERT_FALSE(table_info->schema.table_properties().retain_delete_markers());
+
+  // Validate the value if retain_delete_markers is persisted correctly in a tablet meta-data:
+  // let's get all tablets for an index table and validate it's superblock on a disk.
+  google::protobuf::RepeatedPtrField<master::TabletLocationsPB> tablets;
+  ASSERT_OK(client->GetTabletsFromTableId(table_id, 0, &tablets));
+  ASSERT_GT(tablets.size(), 0);
+
+  itest::ExternalMiniClusterFsInspector inspector { cluster_.get() };
+  for (const auto& tablet : tablets) {
+    for (size_t n = 0; n < cluster_->num_tablet_servers(); ++n) {
+      tablet::RaftGroupReplicaSuperBlockPB superblock;
+      ASSERT_OK(inspector.ReadTabletSuperBlockOnTS(n, tablet.tablet_id(), &superblock));
+      ASSERT_TRUE(superblock.has_kv_store());
+      ASSERT_GT(superblock.kv_store().tables_size(), 0);
+      for (const auto& table_pb : superblock.kv_store().tables()) {
+        // Take into accound only index table (required in case of colocation).
+        if (table_pb.has_table_name() && table_pb.table_name() != index_name) {
+          continue;
+        }
+
+        ASSERT_TRUE(table_pb.has_schema());
+        ASSERT_TRUE(table_pb.schema().has_table_properties());
+        LOG(INFO) << "P " << cluster_->tablet_server(n)->id() << " T " << tablet.tablet_id()
+                  << (table_pb.has_table_name() ? " Table " + table_pb.table_name() : "")
+                  << " properties: " << table_pb.schema().table_properties().ShortDebugString();
+        ASSERT_FALSE(table_pb.schema().table_properties().retain_delete_markers());
+      }
+    }
+  }
 }
 
 void PgIndexBackfillTest::TestLargeBackfill(const int num_rows) {