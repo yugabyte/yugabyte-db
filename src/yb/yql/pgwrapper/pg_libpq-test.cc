yugabyte/yugabyte-db

commit 0391e31ac455a4eb0db22e9193bcc4a27992f92a

[#14066] YSQL: Add funcs to show the current memory usage of PostgreSQL backends
Summary:
Added functions:
- `yb_pg_stat_get_backend_allocated_mem_bytes(beid)`: returns the heap memory usage of the process (PostgreSQL memory usage + pggate memory usage + cached memory - memory that was freed but not recycled)
- `yb_pg_stat_get_backend_rss_mem_bytes(beid)`: returns the rss memory usage of the process

Ideally, we want to add two columns `yb_allocated_mem_bytes` and `yb_rss_mem_bytes` to a new `yb_pg_stat_activity` view. However, YB pinned object cache is not updated immediately after inserts to `pg_depend` during `YsqlUpgrade` yet ([[https://github.com/yugabyte/yugabyte-db/issues/12307 | #12307 ]]). Therefore, we are only exposing the two functions `yb_pg_stat_get_backend_allocated_mem_bytes(beid)` and `yb_pg_stat_get_backend_rss_mem_bytes(beid)`.

Specs doc: https://docs.google.com/document/d/1683ugwqPbVvjQFafk8EicXEYc7NLxvULxher-kKQTw0/edit#

Test Plan:
Manual test for retrieving RSS memory usage for both Linux and MacOS:
- Get RSS value from the OS using `ps -p <pid> -o rss` (KiB).
- Get RSS value from `ysqlsh` using `select yb_pg_stat_get_backend_rss_mem_bytes(beid);`. The return value is in bytes. Convert it to KiB and compare with the value we get from `ps`.
Example:
pid: 82673 <=> beid: 1
From `ysqlsh`:
```
yugabyte=# select yb_pg_stat_get_backend_rss_mem_bytes(1);
 yb_pg_stat_get_backend_rss_mem_bytes
--------------------------------------
                             42627072
(1 row)
```
From terminal:

```
ps -p 82673 -o rss
RSS
41628
```

Test for migration:
`./yb_build.sh --java-test TestYsqlUpgrade#migratingIsEquivalentToReinitdb`

Unit Test for added functions:
`./yb_build.sh --java-test TestYbPgStatActivity`

TPCC benchmark test result: http://perf.dev.yugabyte.com/dashboard/output/589102

Reviewers: plee, ssong, fizaa, alex

Reviewed By: ssong, fizaa, alex

Subscribers: neil, alex, jason, tverona, smishra, yql

src/postgres/src/backend/postmaster/pgstat.c
@@ -2994,6 +2994,9 @@ pgstat_bestart(void)
 		dbForm = (Form_pg_database) GETSTRUCT(tuple);
 		strcpy(lbeentry.st_databasename, dbForm->datname.data);
 		ReleaseSysCache(tuple);
+
+		/* Initialization of allocated memory measurement value */
+		lbeentry.yb_st_allocated_mem_bytes = PgMemTracker.backend_cur_allocated_mem_bytes;
 	}
 
 	/* We have userid for client-backends, wal-sender and bgworker processes */
@@ -3504,6 +3507,11 @@ pgstat_read_current_status(void)
 									   &localentry->backend_xid,
 									   &localentry->backend_xmin);
 
+			if (YBIsEnabledInPostgresEnvVar())
+			{
+				localentry->yb_backend_rss_mem_bytes =
+					YbPgGetCurRSSMemUsage(localentry->backendStatus.st_procpid);
+			}
 			localentry++;
 			localappname += NAMEDATALEN;
 			localclienthostname += NAMEDATALEN;
@@ -6904,4 +6912,26 @@ PgBackendStatus **
 getBackendStatusArrayPointer(void)
 {
 	return &BackendStatusArray;
+}
+
+/* ----------
+ * yb_pgstat_report_allocated_mem_bytes() -
+ *
+ *	Called from utils/mmgr/mcxt.c to update our allocated memory measurement
+ *	value
+ * ----------
+ */
+void
+yb_pgstat_report_allocated_mem_bytes(void)
+{
+	volatile PgBackendStatus *beentry = MyBEEntry;
+
+	if (!beentry)
+		return;
+
+	PGSTAT_BEGIN_WRITE_ACTIVITY(beentry);
+
+	beentry->yb_st_allocated_mem_bytes = PgMemTracker.backend_cur_allocated_mem_bytes;
+
+	PGSTAT_END_WRITE_ACTIVITY(beentry);
 }

java/yb-pgsql/src/test/java/org/yb/pgsql/TestYbPgStatActivity.java
@@ -1,0 +1,138 @@
+// Copyright (c) YugaByte, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+// in compliance with the License.  You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software distributed under the License
+// is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+// or implied.  See the License for the specific language governing permissions and limitations
+// under the License.
+//
+
+package org.yb.pgsql;
+
+import static org.yb.AssertionWrappers.assertEquals;
+import static org.yb.AssertionWrappers.assertNotEquals;
+import static org.yb.AssertionWrappers.assertTrue;
+
+import java.sql.Connection;
+import java.sql.Statement;
+import java.sql.ResultSet;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import org.yb.util.BuildTypeUtil;
+import org.yb.util.YBTestRunnerNonTsanOnly;
+
+@RunWith(value = YBTestRunnerNonTsanOnly.class)
+
+public class TestYbPgStatActivity extends BasePgSQLTest {
+
+  private void consumeMem(Statement stmt) throws Exception {
+    stmt.execute("SELECT * FROM pg_proc;");
+    stmt.execute("SELECT * FROM pg_stat_activity;");
+    stmt.execute("SELECT * FROM pg_attribute;");
+    stmt.execute("CREATE TABLE test (id int);");
+    stmt.execute("DROP TABLE test;");
+  }
+
+  private long getAllocatedMem(Statement stmt, int beid) throws Exception {
+    return getSingleRow(stmt, String.format("SELECT " +
+        "yb_pg_stat_get_backend_allocated_mem_bytes(%d);", beid)).getLong(0);
+  }
+
+  private long getRssMem(Statement stmt, int beid) throws Exception {
+    return getSingleRow(stmt, String.format("SELECT " +
+        "yb_pg_stat_get_backend_rss_mem_bytes(%d);", beid)).getLong(0);
+  }
+
+  private int getPgBackendBeid(Statement stmt) throws Exception {
+    return getSingleRow(stmt, "SELECT s.backendid FROM " +
+        "(SELECT pg_stat_get_backend_idset() AS backendid) AS s " +
+        "WHERE pg_backend_pid() = pg_stat_get_backend_pid(s.backendid);").getInt(0);
+  }
+
+  @Test
+  public void testMemUsageFuncsReturnValues() throws Exception{
+    try (Connection connection = getConnectionBuilder().withTServer(0).connect();
+         Statement stmt = connection.createStatement()) {
+      int beid = getPgBackendBeid(stmt);
+
+      // Verify that the allocated_mem_bytes and rss_mem_bytes are successfully retrieved
+      long allocatedMem = getAllocatedMem(stmt, beid);
+      long rssMem = getRssMem(stmt, beid);
+
+      // TcMalloc is not enabled in macOS and ASAN build, so don't look for it.
+      if (!BuildTypeUtil.isASAN())
+        assertNotEquals("yb_pg_stat_get_backend_allocated_mem_bytes wasn't retrieved.",
+            allocatedMem, 0);
+      assertNotEquals("yb_pg_stat_get_backend_rss_mem_bytes wasn't retrieved or returned " +
+          "incorrect value.", rssMem, -1);
+    }
+  }
+
+  @Test
+  public void testMemUsageFuncsWithMultipleBackends() throws Exception {
+    try (Connection connection1 = getConnectionBuilder().withTServer(0).connect();
+         Connection connection2 = getConnectionBuilder().withTServer(0).connect();
+         Connection connection3 = getConnectionBuilder().withTServer(0).connect();
+         Statement stmt1 = connection1.createStatement();
+         Statement stmt2 = connection2.createStatement();
+         Statement stmt3 = connection3.createStatement();) {
+      int beid1 = getPgBackendBeid(stmt1);
+      int beid2 = getPgBackendBeid(stmt2);
+      int beid3 = getPgBackendBeid(stmt3);
+
+      // Use connection3 to query the memory usage of c1 and c2
+      long c1AllocatedMem1 = getAllocatedMem(stmt3, beid1);
+      long c2AllocatedMem1 = getAllocatedMem(stmt3, beid2);
+      long c1RssMem1 = getRssMem(stmt3, beid1);
+
+      // Verify that after memory consumption in c1, the functions' return values changed.
+      consumeMem(stmt1);
+      long c1AllocatedMem2 = getAllocatedMem(stmt3, beid1);
+      long c1RssMem2 = getRssMem(stmt3, beid1);
+
+      // TcMalloc is not enabled in ASAN, so don't look for it.
+      if (!BuildTypeUtil.isASAN())
+        assertNotEquals("Allocated mem usage wasn't reflected. ",
+            c1AllocatedMem2, c1AllocatedMem1);
+      assertNotEquals("Rss mem usage wasn't correctly reflected. ", c1RssMem2, c1RssMem1);
+
+      // Verify that memory consumption in connection1 does not affect connection2's allocated
+      // memory. Since there could be other memory allocated & reported to TcMalloc in connection2
+      // that's not related to connection1, we expect the returned value within ~10% of change.
+      long c2AllocatedMem2 = getAllocatedMem(stmt3, beid2);
+      if (!BuildTypeUtil.isASAN())
+        assertTrue("Allocating memomry in connection1 changed allocated memory in connection2.",
+          c2AllocatedMem1 * 0.9 <= c2AllocatedMem2 && c2AllocatedMem2 <= c2AllocatedMem1 * 1.1);
+    }
+  }
+
+  @Test
+  public void testMemUsageFuncsWithExplicitTransactions() throws Exception {
+    try (Connection connection = getConnectionBuilder().withTServer(0).connect();
+         Statement stmt = connection.createStatement()) {
+      int beid = getPgBackendBeid(stmt);
+
+      // Verify that the mem usage values don't change during an explicit transaction.
+      connection.setAutoCommit(false);
+      long allocatedMem1 = getAllocatedMem(stmt, beid);
+      long rssMem1 = getRssMem(stmt, beid);
+      consumeMem(stmt);
+      long allocatedMem2 = getAllocatedMem(stmt, beid);
+      long rssMem2 = getRssMem(stmt, beid);
+      connection.commit();
+
+      // TcMalloc is not enabled in ASAN, so don't look for it.
+      if (!BuildTypeUtil.isASAN())
+        assertEquals("Allocated mem usage changed during explicit transactions.",
+          allocatedMem1, allocatedMem2);
+      assertEquals("Rss mem usage changed during explicit transactions.",
+          rssMem1, rssMem2);
+    }
+  }
+}

src/postgres/src/backend/utils/adt/pgstatfuncs.c
@@ -1957,3 +1957,35 @@ pg_stat_get_archiver(PG_FUNCTION_ARGS)
 	PG_RETURN_DATUM(HeapTupleGetDatum(
 									  heap_form_tuple(tupdesc, values, nulls)));
 }
+
+/* Returns backend_allocated_mem_bytes from the process's beid */
+Datum
+yb_pg_stat_get_backend_allocated_mem_bytes(PG_FUNCTION_ARGS)
+{
+	int32		beid = PG_GETARG_INT32(0);
+	int64		result;
+	PgBackendStatus *beentry;
+
+	if ((beentry = pgstat_fetch_stat_beentry(beid)) == NULL)
+		PG_RETURN_NULL();
+
+	result = beentry->yb_st_allocated_mem_bytes;
+
+	PG_RETURN_INT64(result);
+}
+
+/* Returns rss_mem_bytes from the process's beid */
+Datum
+yb_pg_stat_get_backend_rss_mem_bytes(PG_FUNCTION_ARGS)
+{
+	int32		beid = PG_GETARG_INT32(0);
+	int64		result;
+	LocalPgBackendStatus *local_beentry;
+
+	if ((local_beentry = pgstat_fetch_stat_local_beentry(beid)) == NULL)
+		PG_RETURN_NULL();
+
+	result = local_beentry->yb_backend_rss_mem_bytes;
+
+	PG_RETURN_INT64(result);
+}

src/postgres/src/backend/utils/mmgr/mcxt.c
@@ -25,8 +25,18 @@
 #include "miscadmin.h"
 #include "utils/memdebug.h"
 #include "utils/memutils.h"
-#include "yb/yql/pggate/ybc_pggate.h"
+
+/* YB includes */
+#include "pgstat.h"
 #include "pg_yb_utils.h"
+#include "yb/yql/pggate/ybc_pggate.h"
+
+#ifdef __linux__
+#include <stdio.h>
+#include <unistd.h>
+#else
+#include <libproc.h>
+#endif
 
 YbPgMemTracker PgMemTracker = {0};
 
@@ -65,6 +75,50 @@ YbPgMemUpdateMax()
 			snapshot_mem - PgMemTracker.stmt_max_mem_base_bytes);
 }
 
+/*
+ * Update the current actual heap memory usage in MemTracker by getting
+ * the value from TCMalloc.
+ */
+static void
+YbPgMemUpdateCur()
+{
+#ifdef TCMALLOC_ENABLED
+	YbGetActualHeapSizeBytes(&PgMemTracker.backend_cur_allocated_mem_bytes);
+	yb_pgstat_report_allocated_mem_bytes();
+#endif
+}
+
+int64_t
+YbPgGetCurRSSMemUsage(int pid)
+{
+#ifdef __linux__
+	uint64 resident = 0;
+	char path[20];
+	snprintf(path, 20, "/proc/%d/statm", pid);
+	FILE* fp = fopen(path, "r");
+
+	if (fp == NULL)
+		return -1; /* Can't open */
+
+	if (fscanf(fp, "%*s%lu", &resident) != 1)
+	{
+		fclose(fp);
+		return -1; /* Can't read */
+	}
+	fclose(fp);
+	return resident * sysconf(_SC_PAGESIZE);
+#else
+	struct proc_taskallinfo info;
+	int result = proc_pidinfo(pid, PROC_PIDTASKALLINFO, 0, &info,
+		sizeof(struct proc_taskallinfo));
+
+	if (result == 0 || result < sizeof(info))
+		return -1; /* Can't be determined or wrong value */
+
+	return info.ptinfo.pti_resident_size;
+#endif
+}
+
 void
 YbPgMemAddConsumption(Size sz)
 {
@@ -83,6 +137,9 @@ YbPgMemAddConsumption(Size sz)
 
 	/* Only update max memory when memory is increasing */
 	YbPgMemUpdateMax();
+
+	/* Update current heap memory usage */
+	YbPgMemUpdateCur();
 }
 
 void
@@ -97,6 +154,9 @@ YbPgMemSubConsumption(Size sz)
 	// return value.
 	if (PgMemTracker.pggate_alive)
 		PgMemTracker.pggate_alive = YBCTryMemRelease(sz);
+
+	/* Update current heap memory usage */
+	YbPgMemUpdateCur();
 }
 
 void

src/postgres/src/include/catalog/catalog.h
@@ -29,7 +29,7 @@
  * If you increment it, make sure you didn't forget to add a new SQL migration
  * (see pg_yb_migration.dat and src/yb/yql/pgwrapper/ysql_migrations/README.md)
  */
-#define YB_LAST_USED_OID 8045
+#define YB_LAST_USED_OID 8047
 
 extern bool IsSystemRelation(Relation relation);
 extern bool IsToastRelation(Relation relation);

src/postgres/src/include/catalog/pg_proc.dat
@@ -10383,4 +10383,15 @@
   proname => 'binary_upgrade_set_next_tablegroup_oid', provolatile => 'v',
   proparallel => 'r', prorettype => 'void', proargtypes => 'oid',
   prosrc => 'binary_upgrade_set_next_tablegroup_oid' },
+
+{ oid => '8046', descr => 'statistics: memory usage of backend from TCMalloc, including PostgreSQL
+  memory usage + pggate memory usage + cached memory - memory that was freed but not recycled',
+  proname => 'yb_pg_stat_get_backend_allocated_mem_bytes', provolatile => 's',
+  proparallel => 'r', prorettype => 'int8', proargtypes => 'int4',
+  prosrc => 'yb_pg_stat_get_backend_allocated_mem_bytes' },
+
+{ oid => '8047', descr => 'statistics: RSS memory usage of backend retrieved from OS',
+  proname => 'yb_pg_stat_get_backend_rss_mem_bytes', provolatile => 's',
+  proparallel => 'r', prorettype => 'int8', proargtypes => 'int4',
+  prosrc => 'yb_pg_stat_get_backend_rss_mem_bytes' },
 ]

src/postgres/src/include/catalog/pg_yb_migration.dat
@@ -12,7 +12,7 @@
 [
 
 # For better version control conflict detection, list latest migration filename
-# here: V25__11632__binary_upgrade_set_next_tablegroup_oid.sql
-{ major => '25', minor => '0', name => '<baseline>', time_applied => '_null_' }
+# here: V26__14066__yb_pg_backend_memory_functions.sql
+{ major => '26', minor => '0', name => '<baseline>', time_applied => '_null_' }
 
 ]

src/postgres/src/include/pgstat.h
@@ -1103,6 +1103,12 @@ typedef struct PgBackendStatus
 	ProgressCommandType st_progress_command;
 	Oid			st_progress_command_target;
 	int64		st_progress_param[PGSTAT_NUM_PROGRESS_PARAM];
+
+	/*
+	 * Memory usage of backend from TCMalloc, including PostgreSQL memory usage
+	 * + pggate memory usage + cached memory - memory that was freed but not recycled
+	 */
+	int64_t yb_st_allocated_mem_bytes;
 } PgBackendStatus;
 
 /*
@@ -1202,6 +1208,9 @@ typedef struct LocalPgBackendStatus
 	 * not.
 	 */
 	TransactionId backend_xmin;
+
+	/* Backend's RSS memory usage */
+	int64_t yb_backend_rss_mem_bytes;
 } LocalPgBackendStatus;
 
 /*
@@ -1456,4 +1465,9 @@ extern PgStat_ArchiverStats *pgstat_fetch_stat_archiver(void);
 extern PgStat_GlobalStats *pgstat_fetch_global(void);
 extern PgBackendStatus **getBackendStatusArrayPointer(void);
 
+/* ----------
+ * YB functions called from backends
+ * ----------
+ */
+extern void yb_pgstat_report_allocated_mem_bytes(void);
 #endif							/* PGSTAT_H */

commit 466d5108b43c030d4e2b7096500153fcdaa238c8

[#19672] YSQL: Avoid segmentation fault in stats collector after Postmaster reset
Summary:
=== Background ===

D14099 (5738e0fc73143989ec17fbe6095ba46a38a1d649) avoided restarting all Postgres backends when one crashed, but did not
fully clean up the crashed backend.

D20454 (70fbb382e1a913c651a6dbc85a75bcf2e25d32a8) introduced new logic to clean up after terminated backends. It also
changed an `ereport(LOG,...` to an `ereport(ERROR, ...` Which unknowningly changed some behaviour.
* In Postmaster, ERROR is treated as FATAL - so the Postmaster was killed and restarted. The typical Postmaster restart
  logic isn't a true restart, but actually a reset. Shared memory and structures are reset, but the same process and PID
  are used.
* This full restart lead to `PgLibPqTest.TestLWPgBackendKillAfterLWLockAcquire` hanging on Linux builds, motivating the
  next diff.

D21884 (6f01e99c7ffc5d5f5f8e3e3ad43fb595fadd4a9e) changed the ERROR back to INFO. This fixed the timeout issue mentioned
above, but revealed a new issue (the one identified in this ticket).

D18220 (bb650487e5260159692a545713c2547f270fc2de) added the function `yb_pg_stat_get_queries` (now a view called
`yb_terminated_queries`) to identify queries terminated because they exceeded the temp_file_limit. The stats collector
gathers information about the query and sends it to a pg_stats file.

D19706 (d570ef8c940494c004d5288587bab3b22095fe5d) updated `yb_pg_stat_get_queries` to include queries terminated by SIGSEGV
and SIGKILL. In doing so, the stats collector's `MyBEEntry` is overwritten with the terminated queries beentry.

=== The Issue ===

When a query is terminated by SIGSEGV or SIGKILL, the stats collector's MyBEEntry is overwritten. The stats collector can still
function as normal with this new beentry (although it is technically wrong). (D19706).

When a query is terminated while holding a LWLock, the postmaster is reset - including SharedMemory. The stats collector holds an
invalid reference to shared memory, and so it crashes with a segmentation fault. This was hidden until D21884, because until then,
the postmaster was fully restarted by the `ereport(FATAL` - so everything was restarted.

Note: While the incorrect behaviour is present with the combination of D14099 and D19076, we might expect that the issue is reproducible after D19076 and before D20454. However, the stats collector's segmentation faults are on a line introduced by
D20797 (643a622a1c82afbf9c121ce286c250bcd89690da).

src/postgres/src/backend/postmaster/pgstat.c
@@ -2868,20 +2868,33 @@ void
 pgstat_report_query_termination(const char *termination_reason, int32 backend_pid)
 {
 	PgStat_MsgQueryTermination msg;
+	int			i;
+	volatile PgBackendStatus *beentry = BackendStatusArray;
 
-	if (pgStatSock == PGINVALID_SOCKET || !MyBEEntry)
+	if (beentry == NULL || pgStatSock == PGINVALID_SOCKET)
 		return;
 
-	pgstat_setheader(&msg.m_hdr, PGSTAT_MTYPE_QUERYTERMINATION);
-	msg.m_databaseoid = MyBEEntry->st_databaseid;
-	msg.backend_pid = backend_pid;
+	for (i = 0; i < MaxBackends; i++)
+	{
+		if (beentry->st_procpid == backend_pid)
+		{
+			pgstat_setheader(&msg.m_hdr, PGSTAT_MTYPE_QUERYTERMINATION);
+			msg.m_databaseoid = beentry->st_databaseid;
+			msg.backend_pid = backend_pid;
 
-	msg.activity_start_timestamp = MyBEEntry->st_activity_start_timestamp;
-	msg.m_st_userid = MyBEEntry->st_userid;
-	msg.activity_end_timestamp = GetCurrentTimestamp();
-	StrNCpy(msg.query_string, MyBEEntry->st_activity_raw, sizeof(msg.query_string));
-	StrNCpy(msg.termination_reason, termination_reason, sizeof(msg.termination_reason));
-	pgstat_send(&msg, sizeof(msg));
+			msg.activity_start_timestamp = beentry->st_activity_start_timestamp;
+			msg.m_st_userid = beentry->st_userid;
+			msg.activity_end_timestamp = GetCurrentTimestamp();
+			StrNCpy(msg.query_string, beentry->st_activity_raw, sizeof(msg.query_string));
+			StrNCpy(msg.termination_reason, termination_reason, sizeof(msg.termination_reason));
+			pgstat_send(&msg, sizeof(msg));
+
+			return;
+		}
+
+		beentry++;
+	}
+	elog(WARNING, "could not find BEEntry for pid %d to add to yb_terminated_queries", backend_pid);
 }
 
 /* ----------
@@ -4331,7 +4344,6 @@ pgstat_get_crashed_backend_activity(int pid, char *buffer, int buflen)
 			 */
 			ascii_safe_strlcpy(buffer, activity,
 							   Min(buflen, pgstat_track_activity_query_size));
-			MyBEEntry = (PgBackendStatus *) beentry;
 
 			return buffer;
 		}

src/postgres/src/backend/postmaster/postmaster.c
@@ -3444,7 +3444,14 @@ CleanupBackend(int pid,
 	dlist_mutable_iter iter;
 
 	if (YBIsEnabledInPostgresEnvVar())
+	{
 		LogChildExit(EXIT_STATUS_0(exitstatus) ? DEBUG2 : WARNING, _("server process"), pid, exitstatus);
+
+		if (WTERMSIG(exitstatus) == SIGKILL)
+			pgstat_report_query_termination("Terminated by SIGKILL", pid);
+		else if (WTERMSIG(exitstatus) == SIGSEGV)
+			pgstat_report_query_termination("Terminated by SIGSEGV", pid);
+	}
 	else
 		LogChildExit(DEBUG2, _("server process"), pid, exitstatus);
 
@@ -3819,11 +3826,6 @@ LogChildExit(int lev, const char *procname, int pid, int exitstatus)
 				 activity ? errdetail("Failed process was running: %s", activity) : 0));
 	else if (WIFSIGNALED(exitstatus))
 	{
-		if (WTERMSIG(exitstatus) == SIGKILL)
-			pgstat_report_query_termination("Terminated by SIGKILL", pid);
-		else if (WTERMSIG(exitstatus) == SIGSEGV)
-			pgstat_report_query_termination("Terminated by SIGSEGV", pid);
-
 #if defined(WIN32)
 		ereport(lev,
 

java/yb-pgsql/src/test/java/org/yb/pgsql/TestPgYbStat.java
@@ -43,7 +43,7 @@ public class TestPgYbStat extends BasePgSQLTest {
   private void executeQueryAndExpectNoResults(final String query,
     final Connection inputConnection) throws Exception {
     try (Statement statement = inputConnection.createStatement()) {
-      statement.executeQuery(String.format(query));
+      statement.executeQuery(query);
     } catch (PSQLException exception) {
       assertEquals("No results were returned by the query.", exception.getMessage());
     }
@@ -52,7 +52,7 @@ public class TestPgYbStat extends BasePgSQLTest {
   private void executeQueryAndExpectTempFileLimitExceeded(final String query,
     final Connection inputConnection) throws Exception {
     try (Statement statement = inputConnection.createStatement()) {
-      statement.executeQuery(String.format(query));
+      statement.executeQuery(query);
     } catch (PSQLException exception) {
       assertEquals("ERROR: temporary file size exceeds temp_file_limit (0kB)",
                    exception.getMessage());
@@ -132,7 +132,7 @@ public class TestPgYbStat extends BasePgSQLTest {
     int retries = 0;
     while (retries++ < MAX_PG_STAT_RETRIES) {
       try (Statement statement = inputConnection.createStatement()) {
-        ResultSet resultSet = statement.executeQuery(String.format(query));
+        ResultSet resultSet = statement.executeQuery(query);
         if (checkResultSetCallback.checkResultSet(resultSet))
           return true;
       }
@@ -188,7 +188,6 @@ public class TestPgYbStat extends BasePgSQLTest {
                         .equals("Terminated by SIGSEGV"))
               return false;
           }
-
 
           if (!resultSet.next()
            || !oversized_query.equals(resultSet.getString("query_text"))

src/yb/yql/pgwrapper/pg_libpq-test.cc
@@ -3302,6 +3302,14 @@ TEST_F_EX(PgLibPqTest,
   auto conn2 = ASSERT_RESULT(Connect());
   ASSERT_NOK(conn1.FetchFormat("SELECT pg_stat_statements_reset()"));
   ASSERT_NOK(conn2.FetchFormat("SELECT 1"));
+
+  // validate that this query is added to yb_terminated_queries
+  auto conn3 = ASSERT_RESULT(Connect());
+  const string get_yb_terminated_queries =
+    "SELECT query_text, termination_reason FROM yb_terminated_queries";
+  auto row_str = ASSERT_RESULT(conn3.FetchAllAsString(get_yb_terminated_queries));
+  LOG(INFO) << "Result string: " << row_str;
+  ASSERT_EQ(row_str, "SELECT pg_stat_statements_reset(), Terminated by SIGKILL");
 }
 
 #ifdef __linux__