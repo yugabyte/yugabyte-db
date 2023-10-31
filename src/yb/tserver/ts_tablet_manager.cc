2ae6a74eebe28ab9e933d4febcbec509696cd321
yusong-yan <yusongyan3@gmail.com>
2023-10-31 18:52:02 +0000

    [BACKPORT 2.18][#19840] docdb: Delete tablet's superblock if delete type is TABLET_DATA_DELETED
    Summary:
    Original commit: 99b0b0e0be7179b241b15edc8a0badda78f5ab40 / D29817
    **Issue:**
    Currently, we only delete the tablet's superblock during startup. And it caused server Init to take long time to finish when there are many superblocks on disk. For example, we notice the tserver takes about 9 min on 10100 tablets' opening metadata + tablet deletion.
    
    **Why can't we just delete the superblock in DeleteTablet to avoid the slow init problem?**
    In the past, KUDU moved superblock deletion from DeleteTablet to server startup. Then, the fork of KUDU that Yugabyte got has this bad code.(It turns out that KUDU later also encountered problems with slow server initialization because many deleted tablet superblocks on startup.)
    The reason KUDU had to move superblock deletion to server start-up is because they noticed the `DeleteTable` operation caused unnecessary RBS. Here are the 3 events they observed:
    1. Master serviced the `DeleteTable` request, and sent to a particular tablet to
       a quorum of 2 peers.
    2. Follower received and processed the DeleteTablet
       before the leader.
    3. leader reacts to the missing replica by asking the follower to RBS itself.
    
    However, we are not clear on what happens next. To understand the real impact, we set up a unit test that simulates follower tablet receiving DeleteTablet request 3 seconds before the leader receives it. Here is what will happen next:
    4. While the follower processes the RBS, it reports itself to the Master.
    5. Master has marked this tablet as DELETED, so it sends a DeleteTablet request again to the follower.
    6. Follower RBS succeeds, then processes Master's DeleteTablet request again.
    Step3 to step6 may happen many times until the leader receives DeleteTablet.
    7. Leader receives and processe`DeleteTablet`. And this point, if the follower is deleted, or in the process of being deleted. Then, no further RBS will happen, and `DeleteTable` operation quickly succeeds after both leader and follower are deleted. If the follower is in the process of RBS :
     * If the follower is in the copy data state, the follower(RBS destination) is likely to fail the RBS with `TOMBSTONED_NOT_OK`, as it realizes the leader(RBS source) has been removed. Then follower processes Master's DeleteTablet request, which changes its state from TOMBSTONED to DELETED state. (There is a very small chance that follower RBS fails before reporting itself to the Master, which can leave the follower with a TOMBSTONED state.) In the end, `DeleteTable` operation succeeds after both leader and follower are deleted.
     * If the follower completed the copy data, and is in local recovery state. `DeleteTable` operation has to wait for this follower to finish the current RBS and another round of DeleteTablet. (Note that if the tablet is large, `DeleteTable` can take a little longer to complete because of the long-running local recovery)
    
    Since no other errors were found, we believe it is safe to delete the superblock in DeleteTablet.
    
    **Fix:**
    To address the slow init problem, make `DeleteTablet()` to delete the tablet superblock if the `delete_type` is TABLET_DATA_DELETED. So, there won't be many DELETED tablet superblocks on the next server startup.
    Jira: DB-8777
    
    Test Plan:
    ./yb_build.sh --cxx-test integration-tests_delete_table-test --gtest_filter DeleteTableTest.TestDeleteTabletFollowerFirst
    Plus, modified many existing DeleteTableTest from asserting SUPERBLOCK_EXPECTED to SUPERBLOCK_NOT_EXPECTED.
    
    Reviewers: timur, rthallam, arybochkin
    
    Reviewed By: rthallam
    
    Subscribers: bogdan, esheng, ybase
    
    Tags: #jenkins-ready
    
    Differential Revision: https://phorge.dev.yugabyte.com/D31228

--- src/yb/tserver/ts_tablet_manager.cc
+++ src/yb/tserver/ts_tablet_manager.cc
@@ -259,6 +259,9 @@ DEFINE_RUNTIME_int32(bg_superblock_flush_interval_secs, 60,
     "thread. Applicable only when lazily_flush_superblock is enabled. 0 indicates that the "
     "background task is fully disabled.");
 
+DEFINE_test_flag(bool, pause_delete_tablet, false,
+                 "Make DeletTablet stuck.");
+
 DECLARE_bool(enable_wait_queues);
 DECLARE_bool(lazily_flush_superblock);
 
@@ -1356,6 +1359,7 @@ Status TSTabletManager::DeleteTablet(
     bool hide_only,
     bool keep_data,
     boost::optional<TabletServerErrorPB::Code>* error_code) {
+  TEST_PAUSE_IF_FLAG(TEST_pause_delete_tablet);
 
   if (delete_type != TABLET_DATA_DELETED && delete_type != TABLET_DATA_TOMBSTONED) {
     return STATUS(InvalidArgument, "DeleteTablet() requires an argument that is one of "
@@ -2368,14 +2372,6 @@ Status TSTabletManager::HandleNonReadyTabletOnStartup(
     RETURN_NOT_OK(DeleteTabletData(meta, data_state, fs_manager_->uuid(), yb::OpId()));
   }
 
-  // We only delete the actual superblock of a TABLET_DATA_DELETED tablet on startup.
-  // TODO: Consider doing this after a fixed delay, instead of waiting for a restart.
-  // See KUDU-941.
-  if (data_state == TABLET_DATA_DELETED) {
-    LOG(INFO) << kLogPrefix << "Deleting tablet superblock";
-    return meta->DeleteSuperBlock();
-  }
-
   // Register TOMBSTONED tablets so that they get reported to the Master, which
   // allows us to permanently delete replica tombstones when a table gets deleted.
   if (data_state == TABLET_DATA_TOMBSTONED) {
@@ -2840,8 +2836,7 @@ Status DeleteTabletData(const RaftGroupMetadataPtr& meta,
   // Only TABLET_DATA_DELETED tablets get this far.
   RETURN_NOT_OK(ConsensusMetadata::DeleteOnDiskData(meta->fs_manager(), meta->raft_group_id()));
   MAYBE_FAULT(FLAGS_TEST_fault_crash_after_cmeta_deleted);
-
-  return Status::OK();
+  return meta->DeleteSuperBlock();
 }
 
 void LogAndTombstone(const RaftGroupMetadataPtr& meta,
