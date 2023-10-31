9f8705ee17a84ff25c1381c3db0d71c71fc6a74d
Minghui Yang <myang@yugabyte.com>
2023-10-31 00:12:30 +0000

    [#12417] YSQL: Change per-database catalog version mode test gflag to preview gflag
    Summary:
    This diff changes the test gflag --FLAGS_TEST_enable_db_catalog_version_mode to
    a tech preview gflag --FLAGS_ysql_enable_db_catalog_version_mode. Besides the gflag
    name change by replacing `TEST` with `ysql`, a few unit tests are updated to have `allowed_preview_flags_csv=ysql_enable_db_catalog_version_mode` so that the new
    tech preview gflag can be turned on.
    Jira: DB-517
    
    Test Plan:
    (1) Run these tests that involve per-database catalog version mode gflag.
    ./yb_build.sh --cxx-test pg_catalog_version-test
    ./yb_build.sh --cxx-test yb-admin-snapshot-schedule-test
    ./yb_build.sh --cxx-test yb-backup-test
    ./yb_build.sh --cxx-test pgwrapper_pg_mini-test
    
    (2) With --ysql_enable_db_catalog_version_mode=true, do reinitdb then run the same tests
    YB_EXTRA_DAEMON_FLAGS="--ysql_enable_db_catalog_version_mode=true --allowed_preview_flags_csv=ysql_enable_db_catalog_version_mode" ./yb_build.sh reinitdb
    YB_EXTRA_DAEMON_FLAGS="--ysql_enable_db_catalog_version_mode=true --allowed_preview_flags_csv=ysql_enable_db_catalog_version_mode" ./yb_build.sh --cxx-test pg_catalog_version-test
    YB_EXTRA_DAEMON_FLAGS="--ysql_enable_db_catalog_version_mode=true --allowed_preview_flags_csv=ysql_enable_db_catalog_version_mode" ./yb_build.sh --cxx-test yb-admin-snapshot-schedule-test
    YB_EXTRA_DAEMON_FLAGS="--ysql_enable_db_catalog_version_mode=true --allowed_preview_flags_csv=ysql_enable_db_catalog_version_mode" ./yb_build.sh --cxx-test yb-backup-test
    ./yb_build.sh --cxx-test pgwrapper_pg_mini-test --test_args --ysql_enable_db_catalog_version_mode=true --allowed_preview_flags_csv=ysql_enable_db_catalog_version_mode
    
    Reviewers: jason
    
    Reviewed By: jason
    
    Subscribers: yql, ybase, bogdan
    
    Differential Revision: https://phorge.dev.yugabyte.com/D29824

--- src/yb/common/common_flags.h
+++ src/yb/common/common_flags.h
@@ -22,7 +22,7 @@ DECLARE_int32(ysql_num_shards_per_tserver);
 DECLARE_bool(enable_ysql);
 DECLARE_bool(ysql_disable_index_backfill);
 DECLARE_bool(log_ysql_catalog_versions);
-DECLARE_bool(TEST_enable_db_catalog_version_mode);
+DECLARE_bool(ysql_enable_db_catalog_version_mode);
 DECLARE_bool(ysql_enable_pg_per_database_oid_allocator);
 DECLARE_uint32(wait_for_ysql_backends_catalog_version_client_master_rpc_margin_ms);
 
