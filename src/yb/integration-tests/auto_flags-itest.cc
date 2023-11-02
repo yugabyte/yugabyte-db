7c9798fa546311ed8910928ed3c2f0fb6a0341f0
Hari Krishna Sunder <hari90@users.noreply.github.com>
2023-11-02 09:53:21 -0700

    [BACKPORT 2.18][#16181] DocDB: Add Version info to AutoFlag not found error
    Summary:
    Original commit: ab20af218a9961f2db5baf1c8852d64d9d5c0d2e / D29888
    When the yb process is incorrectly started on an older version after AutoFlags have been enabled we crash the process with an error about the missing flag.
    This change includes the version info to the error message so that it is easier to identify the problem.
    
    New error message:
    
    ```
    W1101 22:06:33.327406 3651326720 auto_flags-itest.cc:585] Not found (yb/client/auto_flags_manager.cc:280): AutoFlag 'missing_flag' is not found. Upgrade the process to a version that contains this AutoFlag. Current version: version 2.21.0.0 build PRE_RELEASE revision a18721cf7b4c42b890dedc911a42eb263686e5ef build_type DEBUG built at 01 Nov 2023 22:06:08 PST
    ```
    
    Fixes #16181
    Jira: DB-5614
    
    Test Plan: AutoFlagsMiniClusterTest.CheckMissingFlag
    
    Reviewers: rahuldesirazu, slingam
    
    Reviewed By: slingam
    
    Subscribers: ybase
    
    Tags: #jenkins-ready
    
    Differential Revision: https://phorge.dev.yugabyte.com/D29903

--- src/yb/integration-tests/auto_flags-itest.cc
+++ src/yb/integration-tests/auto_flags-itest.cc
@@ -32,8 +32,11 @@
 #include "yb/tserver/mini_tablet_server.h"
 #include "yb/tserver/tablet_server.h"
 
+#include "yb/client/auto_flags_manager.h"
+
 #include "yb/util/flags.h"
 #include "yb/util/backoff_waiter.h"
+#include "yb/util/version_info.h"
 
 DECLARE_bool(TEST_auto_flags_initialized);
 DECLARE_bool(TEST_auto_flags_new_install);
@@ -291,6 +294,25 @@ TEST_F(AutoFlagsMiniClusterTest, Promote) {
         RETURN_NOT_OK(ValidateConfigOnMasters(config_version));
         return ValidateConfigOnTservers(config_version);
       }));
+}
+
+TEST_F(AutoFlagsMiniClusterTest, CheckMissingFlag) {
+  ASSERT_OK(RunSetUp());
+  ASSERT_OK(ValidateConfig());
+
+  auto leader_master = ASSERT_RESULT(cluster_->GetLeaderMiniMaster());
+  auto auto_flags_manager = leader_master->master()->auto_flags_manager();
+
+  auto config = auto_flags_manager->GetConfig();
+  for (auto& promoted_flags : *config.mutable_promoted_flags()) {
+    promoted_flags.add_flags("missing_flag");
+  }
+  config.set_config_version(config.config_version() + 1);
+
+  auto s = auto_flags_manager->LoadFromConfig(config, ApplyNonRuntimeAutoFlags::kFalse);
+  ASSERT_NOK(s);
+  ASSERT_TRUE(s.ToString().find("missing_flag") != std::string::npos) << s;
+  ASSERT_TRUE(s.ToString().find(VersionInfo::GetShortVersionString()) != std::string::npos) << s;
 }
 
 class AutoFlagsExternalMiniClusterTest : public ExternalMiniClusterITestBase {
