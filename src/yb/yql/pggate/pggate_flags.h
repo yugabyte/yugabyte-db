4b6756307f15db6be4e2b6d4935db03485274c93
Saurav Tiwary <21277837+dr0pdb@users.noreply.github.com>
2023-11-06 12:59:00 +0530

    [BACKPORT 2.14][#19509] YSQL: Fix YB does not release LWLocks on AbortTransaction
    Summary:
    Original commit: 7e22b285bc0cb9b3a2a01706649541f5de6ddf8d / D29194
    
    This revision backports the fix for releasing LWLocks on AbortTransaction. It is imported from the replication slot support revision: https://phorge.dev.yugabyte.com/D29194.
    
    The issue was introduced in 3f536a8aa19c828a9f8622e76283332ccebfe6ee / D5518
    Jira: DB-8305
    
    Test Plan:
    ```
    ./yb_build.sh --cxx-test pgwrapper_pg_libpq-test --gtest_filter PgLibPqTest.TestLWPgBackendErrorAfterLWLockAcquire
    ```
    
    Reviewers: telgersma
    
    Reviewed By: telgersma
    
    Subscribers: yql
    
    Tags: #jenkins-ready
    
    Differential Revision: https://phorge.dev.yugabyte.com/D29995

--- src/yb/yql/pggate/pggate_flags.h
+++ src/yb/yql/pggate/pggate_flags.h
@@ -46,5 +46,6 @@ DECLARE_int32(ysql_max_write_restart_attempts);
 DECLARE_bool(ysql_sleep_before_retry_on_txn_conflict);
 DECLARE_bool(ysql_disable_portal_run_context);
 DECLARE_bool(TEST_yb_lwlock_crash_after_acquire_pg_stat_statements_reset);
+DECLARE_bool(TEST_yb_lwlock_error_after_acquire_pg_stat_statements_reset);
 DECLARE_bool(TEST_yb_test_fail_matview_refresh_after_creation);
 #endif  // YB_YQL_PGGATE_PGGATE_FLAGS_H
