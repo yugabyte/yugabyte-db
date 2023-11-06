bd447313586d7fcc94f08cd72abc79281fefb57a
Fizaa Luthra <fizaaluthra@gmail.com>
2023-11-06 13:07:48 -0500

    [#19355] YSQL: Skip ALTER TABLE ADD COLUMN DEFAULT NOT NULL table scan
    Summary:
    Adding a new column with a NOT NULL constraint typically requires a table scan,
    because if the table is not empty the operation may lead to a constraint violation.
    However, when adding a column with a `NOT NUL` constraint AND a
    non-volatile `DEFAULT` value, we do NOT need to scan the table. This is because,
    all the existing rows will use the non-volatile `DEFAULT` value in their new column.
    So we effectively only need to check if the default value itself is nullable.
    
    PG already has this optimization; however, it was blocked off in YB code because YB
    doesn't set/use the `pg_attribute.hasmissing` and `pg_attribute.attmissingval`
    fields.
    
    This diff reverts the code to use these PG fields. These PG fields are also useful for
    when we add support for DDL xCluster replication (as we would have to include the
    computed default value in the replicated command).
    Jira: DB-8157
    
    Test Plan:
    1) Manually tested:
    
    Create table, insert data:
    
    ```
    yugabyte=# CREATE TABLE test_table (t int) SPLIT INTO 1 TABLETS;
    CREATE TABLE
    yugabyte=# INSERT INTO test_table VALUES (generate_series(1,1000));
    INSERT 0 1000
    ```
    
    Check tablet metrics:
    ```
    rocksdb_number_db_next{... table_name="test_table" ...} 0 1698164550469
    
    ```
    
    Perform basic `SELECT` (scan the table):
    ```
    yugabyte=# SELECT * FROM test_table;
    ...
    ```
    
    Check tablet metrics:
    
    ```
    rocksdb_number_db_next{... table_name="test_table" ...} 2000 1698164679284
    ```
    
    Add a column without any constraints (does not scan the table):
    
    ```
    yugabyte=# ALTER TABLE test_table ADD COLUMN col1 int;
    ```
    
    Check tablet metrics (unchanged):
    ```
    rocksdb_number_db_next{... table_name="test_table" ...} 2000 1698164770173
    ```
    
    Add a column with a NOT NULL constraint (scans the table):
    ```
    yugabyte=# ALTER TABLE test_table ADD COLUMN col2 int NOT NULL;
    ```
    
    Check tablet metrics (changed):
    ```
    rocksdb_number_db_next{... table_name="test_table" ...} 4000 1698164853033
    ```
    
    Add a column with a NOT NULL constraint with a DEFAULT value (does **NOT** scan the table):
    ```
    yugabyte=# ALTER TABLE test_table ADD COLUMN col3 int NOT NULL DEFAULT 5;
    ```
    
    Check tablet metrics (unchanged)
    ```
    rocksdb_number_db_next{... table_name="test_table" ...} 4000 1698164895548
    ```
    
    2) `yb_alter_table` in `TestPgRegressTable` to verify that adding a non-nullable column with a default value fails when the default value itself is nullable.
    
    Reviewers: dsrinivasan
    
    Reviewed By: dsrinivasan
    
    Subscribers: yql
    
    Differential Revision: https://phorge.dev.yugabyte.com/D29591

--- src/postgres/src/backend/catalog/heap.c
+++ src/postgres/src/backend/catalog/heap.c
@@ -1831,9 +1831,8 @@ RemoveAttributeById(Oid relid, AttrNumber attnum)
 
 			CatalogTupleUpdate(attr_rel, &tuple->t_self, tuple);
 		}
-		/* YB note: attmissingval is unused in YB relations. */
 		/* clear the missing value if any */
-		if (!IsYBRelationById(relid) && attStruct->atthasmissing)
+		if (attStruct->atthasmissing)
 		{
 			Datum		valuesAtt[Natts_pg_attribute];
 			bool		nullsAtt[Natts_pg_attribute];
@@ -2189,9 +2188,6 @@ heap_drop_with_catalog(Oid relid)
 void
 RelationClearMissing(Relation rel)
 {
-	/* YB note: attmissingval is unused in YB relations. */
-	if (IsYBRelation(rel))
-		return;
 	Relation	attr_rel;
 	Oid			relid = RelationGetRelid(rel);
 	int			natts = RelationGetNumberOfAttributes(rel);
@@ -2260,9 +2256,6 @@ RelationClearMissing(Relation rel)
 void
 SetAttrMissing(Oid relid, char *attname, char *value)
 {
-	/* YB note: attmissingval is unused in YB relations. */
-	if (IsYBRelationById(relid))
-		return;
 	Datum		valuesAtt[Natts_pg_attribute];
 	bool		nullsAtt[Natts_pg_attribute];
 	bool		replacesAtt[Natts_pg_attribute];
@@ -2409,11 +2402,7 @@ StoreAttrDefault(Relation rel, AttrNumber attnum,
 		valuesAtt[Anum_pg_attribute_atthasdef - 1] = true;
 		replacesAtt[Anum_pg_attribute_atthasdef - 1] = true;
 
-		/*
-		 * YB note: attmissingval is unused in YB relations - the missing value
-		 * is stored in the DocDB schema instead.
-		 */
-		if (!IsYBRelation(rel) && add_column_mode)
+		if (add_column_mode)
 		{
 			expr2 = expression_planner(expr2);
 			estate = CreateExecutorState();
