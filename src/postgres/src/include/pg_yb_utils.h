40b5eea13b5a1382b30e0a1604ef9cdbf0bbab6b
Mikhail Bautin <552936+mbautin@users.noreply.github.com>
2023-11-07 13:24:02 -0800

    [#19201] Properly manage palloc-d filename and funcname in ErrorData
    Summary: Copy file name and function name fields of ErrorData when necessary to avoid ASAN issues. In contrast with Postgres, in YugabyteDB, values of these fields might originate in Status objects and might not be static strings. We set the new yb_owns_file_and_func field of ErrorData in HandleYBStatusAtErrorLevel, and appropriately pstrdup/pfree the filename/fileline fields when copying and destroying the ErrorData.
    
    Test Plan:
    Jenkins: build type: asan
    
    ./yb_build.sh asan --java-test org.yb.pgsql.TestIndexBackfill#insertsWhileCreatingIndex
    ./yb_build.sh asan --java-test org.yb.pgsql.TestPgDdlConcurrency#testModifiedTableWrite
    
    Reviewers: amartsinchyk
    
    Reviewed By: amartsinchyk
    
    Subscribers: yql
    
    Differential Revision: https://phorge.dev.yugabyte.com/D30007

--- src/postgres/src/include/pg_yb_utils.h
+++ src/postgres/src/include/pg_yb_utils.h
@@ -890,8 +890,7 @@ OptSplit *YbGetSplitOptions(Relation rel);
  * here. Also we don't need ereport's flexibility, as we support transfer of
  * limited subset of Postgres error fields.
  *
- * Similar to ereport, we have a version for compilers without support for
- * __builtin_constant_p, though we may drop it eventually.
+ * We require the compiler to support __builtin_constant_p.
  */
 #ifdef HAVE__BUILTIN_CONSTANT_P
 #define HandleYBStatusAtErrorLevel(status, elevel) \
@@ -917,12 +916,14 @@ OptSplit *YbGetSplitOptions(Relation rel);
 										   &detail_buf, &detail_nargs, \
 										   &detail_args); \
 			YBCFreeStatus(_status); \
-			if (errstart(elevel, filename ? filename : __FILE__, \
+			if (errstart(elevel, __FILE__, \
 						 lineno > 0 ? lineno : __LINE__, \
-						 funcname ? funcname : PG_FUNCNAME_MACRO, TEXTDOMAIN)) \
+						 PG_FUNCNAME_MACRO, \
+						 TEXTDOMAIN)) \
 			{ \
 				yb_errmsg_from_status_data(msg_buf, msg_nargs, msg_args); \
 				yb_detail_from_status_data(detail_buf, detail_nargs, detail_args); \
+				yb_set_pallocd_error_file_and_func(filename, funcname); \
 				errcode(pg_err_code); \
 				yb_txn_errcode(txn_err_code); \
 				errhidecontext(true); \
@@ -930,44 +931,12 @@ OptSplit *YbGetSplitOptions(Relation rel);
 				if (__builtin_constant_p(elevel) && (elevel) >= ERROR) \
 					pg_unreachable(); \
 			} \
-		} \
-	} while (0)
-#else							/* !HAVE__BUILTIN_CONSTANT_P */
-#define HandleYBStatusAtErrorLevel(status, elevel) \
-	do \
-	{ \
-		AssertMacro(!IsMultiThreadedMode()); \
-		YBCStatus _status = (status); \
-		if (_status) \
-		{ \
-			const int		elevel_ = (elevel); \
-			const uint32_t	pg_err_code = YBCStatusPgsqlError(_status); \
-			const uint16_t	txn_err_code = YBCStatusTransactionError(_status); \
-			const char	   *filename = YBCStatusFilename(_status); \
-			int				lineno = YBCStatusLineNumber(_status); \
-			const char	   *funcname = YBCStatusFuncname(_status); \
-			const char	   *msg_buf = NULL; \
-			const char	   *detail_buf = NULL; \
-			size_t		    msg_nargs = 0; \
-			size_t		    detail_nargs = 0; \
-			const char	  **msg_args = NULL; \
-			const char	  **detail_args = NULL; \
-			GetStatusMsgAndArgumentsByCode(pg_err_code, _status, &msg_buf, \
-										   &msg_nargs, &msg_args, &detail_buf, \
-										   &detail_nargs, &detail_args); \
-			YBCFreeStatus(_status); \
-			if (errstart(elevel_, filename ? filename : __FILE__, \
-						 lineno > 0 ? lineno : __LINE__, \
-						 funcname ? funcname : PG_FUNCNAME_MACRO, TEXTDOMAIN)) \
+			else \
 			{ \
-				yb_errmsg_from_status_data(msg_buf, msg_nargs, msg_args); \
-				yb_detail_from_status_data(detail_buf, detail_nargs, detail_args); \
-				errcode(pg_err_code); \
-				yb_txn_errcode(txn_err_code); \
-				errhidecontext(true); \
-				errfinish(0); \
-				if (elevel_ >= ERROR) \
-					pg_unreachable(); \
+				if (filename) \
+					pfree((void*) filename); \
+				if (funcname) \
+					pfree((void*) funcname); \
 			} \
 		} \
 	} while (0)
