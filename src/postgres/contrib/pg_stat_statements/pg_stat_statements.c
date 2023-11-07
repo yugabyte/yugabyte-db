yugabyte/yugabyte-db

commit 3f536a8aa19c828a9f8622e76283332ccebfe6ee

Integrating PostgreSQL with YugaByte distributed transactions; adding PostgreSQL to yb-ctl and the release package
Summary:
Integrating PostgreSQL with YugaByte distributed transactions.
- PgTxnManager manages the TransactionManager, YBSession, and Transaction instances. There is at most one active transaction at a time per PostgreSQL backend process (i.e. per conneciton).
- PgTxnManager's member functions BeginTransaction() / CommitTransaction() / AbortTransaction() are invoked from PostgreSQL's BEGIN / COMMIT / ROLLBACK.

Adding PostgreSQL to yb-ctl and the release package.
- yb-ctl is our local cluster management script. Now, running `bin/yb-ctl create --enable_postgres` will start the PostgreSQL server in addition to yb-master and yb-tserver processes.
- Modifying yb_release.py and related Python libraries so that PostgreSQL is included as a top-level directory inside the YugaByte release package. On Linux the `bin/post_install.sh` script inside the release package will now also fix RPATHs and the interpreter path in PostgreSQL binaries.
- yb-ctl works with PostgreSQL from both the local directory and from the release package.

Development efficiency improvements:
- Adding a script called `dev_postgres.sh` to manage the local PostgreSQL server running outside of the YB cluster during development.
- An experimental mode of running our PostgreSQL tests against a PostgreSQL server running in a non-YugaByte mode, which should be identical in behavior to regular PostgreSQL. This can be enabled by setting the YB_USE_VANILLA_POSTGRES_IN_TEST environment variable to 1. This is useful to validate our tests.
- In case a PostgreSQL test gets stuck, we automatically cause core dumps of its backend processes and dump stack traces.

Bug fixes:
- Adding a missing thread pool shutdown in `TransactionManager` destructor.
- Fixing a bug in generating a combined `compile_commands.json` file in `build_postgres.py`.

src/postgres/src/backend/access/transam/xact.c
@@ -66,6 +66,7 @@
 #include "utils/timestamp.h"
 #include "pg_trace.h"
 
+#include "pg_yb_utils.h"
 
 /*
  *	User-tweakable parameters
@@ -1927,6 +1928,10 @@ StartTransaction(void)
 	 */
 	s->state = TRANS_INPROGRESS;
 
+	if (YBTransactionsEnabled()) {
+		YBCPgTxnManager_BeginTransaction(YBCGetPgTxnManager());
+	}
+
 	ShowTransactionState("StartTransaction");
 }
 
@@ -2324,8 +2329,10 @@ PrepareTransaction(void)
 	StartPrepare(gxact);
 
 	AtPrepare_Notify();
-	AtPrepare_Locks();
-	AtPrepare_PredicateLocks();
+	if (YBIsPgLockingEnabled()) {
+		AtPrepare_Locks();
+		AtPrepare_PredicateLocks();
+	}
 	AtPrepare_PgStat();
 	AtPrepare_MultiXact();
 	AtPrepare_RelationMap();
@@ -2385,8 +2392,10 @@ PrepareTransaction(void)
 
 	PostPrepare_MultiXact(xid);
 
-	PostPrepare_Locks(xid);
-	PostPrepare_PredicateLocks(xid);
+	if (YBIsPgLockingEnabled()) {
+		PostPrepare_Locks(xid);
+		PostPrepare_PredicateLocks(xid);
+	}
 
 	ResourceOwnerRelease(TopTransactionResourceOwner,
 						 RESOURCE_RELEASE_LOCKS,
@@ -2461,13 +2470,15 @@ AbortTransaction(void)
 	AtAbort_Memory();
 	AtAbort_ResourceOwner();
 
-	/*
-	 * Release any LW locks we might be holding as quickly as possible.
-	 * (Regular locks, however, must be held till we finish aborting.)
-	 * Releasing LW locks is critical since we might try to grab them again
-	 * while cleaning up!
-	 */
-	LWLockReleaseAll();
+	if (YBIsPgLockingEnabled()) {
+		/*
+		* Release any LW locks we might be holding as quickly as possible.
+		* (Regular locks, however, must be held till we finish aborting.)
+		* Releasing LW locks is critical since we might try to grab them again
+		* while cleaning up!
+		*/
+		LWLockReleaseAll();
+	}
 
 	/* Clear wait information and command progress indicator */
 	pgstat_report_wait_end();
@@ -2483,11 +2494,13 @@ AbortTransaction(void)
 	/* Cancel condition variable sleep */
 	ConditionVariableCancelSleep();
 
-	/*
-	 * Also clean up any open wait for lock, since the lock manager will choke
-	 * if we try to wait for another lock before doing this.
-	 */
-	LockErrorCleanup();
+	if (YBIsPgLockingEnabled()) {
+		/*
+		* Also clean up any open wait for lock, since the lock manager will choke
+		* if we try to wait for another lock before doing this.
+		*/
+		LockErrorCleanup();
+	}
 
 	/*
 	 * If any timeout events are still active, make sure the timeout interrupt
@@ -2618,6 +2631,10 @@ AbortTransaction(void)
 		pgstat_report_xact_timestamp(0);
 	}
 
+	if (YBTransactionsEnabled()) {
+		YBCPgTxnManager_AbortTransaction(YBCGetPgTxnManager());
+	}
+
 	/*
 	 * State remains TRANS_ABORT until CleanupTransaction().
 	 */
@@ -2743,6 +2760,22 @@ StartTransactionCommand(void)
 	MemoryContextSwitchTo(CurTransactionContext);
 }
 
+void
+YBCCommitTransactionAndUpdateBlockState() {
+	TransactionState s = CurrentTransactionState;
+	if (YBCCommitTransaction()) {
+		/*
+		 * This is still needed in the YugaByte case because we need to manage the
+		 * PostgreSQL transaction state correctly.
+		 */
+		CommitTransaction();
+		s->blockState = TBLOCK_DEFAULT;
+	} else {
+		s->blockState = TBLOCK_ABORT;
+		YBCHandleCommitError();
+	}
+}
+
 /*
  *	CommitTransactionCommand
  */
@@ -2770,6 +2803,11 @@ CommitTransactionCommand(void)
 			 * transaction commit, and return to the idle state.
 			 */
 		case TBLOCK_STARTED:
+			if (YBTransactionsEnabled())
+			{
+				YBCCommitTransactionAndUpdateBlockState();
+				break;
+			}
 			CommitTransaction();
 			s->blockState = TBLOCK_DEFAULT;
 			break;
@@ -2799,6 +2837,11 @@ CommitTransactionCommand(void)
 			 * idle state.
 			 */
 		case TBLOCK_END:
+			if (YBTransactionsEnabled())
+			{
+				YBCCommitTransactionAndUpdateBlockState();
+				break;
+			}
 			CommitTransaction();
 			s->blockState = TBLOCK_DEFAULT;
 			break;
@@ -3538,6 +3581,14 @@ EndTransactionBlock(void)
 			 */
 		case TBLOCK_INPROGRESS:
 			s->blockState = TBLOCK_END;
+			if (YBTransactionsEnabled()) {
+				/*
+				 * YugaByte transaction commit happens here, but could also happen in
+				 * CommitTransactionCommand if this function is not called first.
+				 */
+				result = YBCCommitTransaction();
+				break;
+			}
 			result = true;
 			break;
 

commit 6fb7091d7e2bbb61c0b7d3eb5596dbf4f90311a5

[BACKPORT 2.20][#19509] YSQL: Fix YB does not release LWLocks on AbortTransaction
Summary:
Original commit: 7e22b285bc0cb9b3a2a01706649541f5de6ddf8d / D29194

This revision backports the fix for releasing LWLocks on AbortTransaction. It is imported from the replication slot support revision:
https://phorge.dev.yugabyte.com/D29194.

The issue was introduced in 3f536a8aa19c828a9f8622e76283332ccebfe6ee / D5518
Jira: DB-8305

Test Plan: ```./yb_build.sh --cxx-test pgwrapper_pg_libpq-test --gtest_filter PgLibPqTest.TestLWPgBackendErrorAfterLWLockAcquire```

Reviewers: telgersma

Reviewed By: telgersma

Subscribers: yql

Tags: #jenkins-ready

Differential Revision: https://phorge.dev.yugabyte.com/D30022

src/postgres/contrib/pg_stat_statements/pg_stat_statements.c
@@ -2695,6 +2695,25 @@ yb_lwlock_crash_after_acquire_pg_stat_statements_reset()
 }
 
 /*
+ * Function that caches environmental variable
+ * FLAGS_TEST_yb_lwlock_error_after_acquire_pg_stat_statements_reset.
+ *
+ * This avoids the process of checking the value of the environmental variable
+ * time and again.
+ */
+bool
+yb_lwlock_error_after_acquire_pg_stat_statements_reset()
+{
+	static int cached_value = -1;
+	if (cached_value == -1)
+	{
+		cached_value = YBCIsEnvVarTrue(
+			"FLAGS_TEST_yb_lwlock_error_after_acquire_pg_stat_statements_reset");
+	}
+	return cached_value;
+}
+
+/*
  * Release all entries.
  */
 static void
@@ -2714,6 +2733,14 @@ entry_reset(void)
 
 	if (yb_lwlock_crash_after_acquire_pg_stat_statements_reset())
 	  kill(getpid(), 9);
+
+	if (yb_lwlock_error_after_acquire_pg_stat_statements_reset())
+		ereport(ERROR,
+				(errcode(ERRCODE_INTERNAL_ERROR),
+				 errmsg("Forced error since "
+				 		"yb_lwlock_error_after_acquire_pg_stat_statements_reset"
+						" is set")));
+
 	/*
 	 * Write new empty query file, perhaps even creating a new one to recover
 	 * if the file was missing.

src/postgres/src/backend/access/transam/xact.c
@@ -2691,15 +2691,13 @@ AbortTransaction(void)
 	AtAbort_Memory();
 	AtAbort_ResourceOwner();
 
-	if (YBIsPgLockingEnabled()) {
-		/*
-		* Release any LW locks we might be holding as quickly as possible.
-		* (Regular locks, however, must be held till we finish aborting.)
-		* Releasing LW locks is critical since we might try to grab them again
-		* while cleaning up!
-		*/
-		LWLockReleaseAll();
-	}
+	/*
+	 * Release any LW locks we might be holding as quickly as possible.
+	 * (Regular locks, however, must be held till we finish aborting.)
+	 * Releasing LW locks is critical since we might try to grab them again
+	 * while cleaning up!
+	 */
+	LWLockReleaseAll();
 
 	/* Clear wait information and command progress indicator */
 	pgstat_report_wait_end();

src/yb/yql/pggate/pggate_flags.cc
@@ -137,6 +137,10 @@ DEFINE_UNKNOWN_bool(yb_enable_read_committed_isolation, false,
 DEFINE_test_flag(bool, yb_lwlock_crash_after_acquire_pg_stat_statements_reset, false,
              "Issue sigkill for crash test after acquiring a LWLock in pg_stat_statements reset.");
 
+DEFINE_test_flag(bool, yb_lwlock_error_after_acquire_pg_stat_statements_reset, false,
+                 "Issue ereport(ERROR) for error test after acquiring a LWLock"
+                 " in pg_stat_statements reset.");
+
 DEFINE_test_flag(bool, yb_test_fail_matview_refresh_after_creation, false,
                  "Fail a refresh on a matview after the creation of a new relation.");
 

src/yb/yql/pggate/pggate_flags.h
@@ -45,6 +45,7 @@ DECLARE_int32(ysql_max_write_restart_attempts);
 DECLARE_bool(ysql_sleep_before_retry_on_txn_conflict);
 DECLARE_bool(ysql_disable_portal_run_context);
 DECLARE_bool(TEST_yb_lwlock_crash_after_acquire_pg_stat_statements_reset);
+DECLARE_bool(TEST_yb_lwlock_error_after_acquire_pg_stat_statements_reset);
 DECLARE_bool(TEST_yb_test_fail_matview_refresh_after_creation);
 DECLARE_bool(ysql_enable_read_request_caching);
 DECLARE_bool(ysql_enable_create_database_oid_collision_retry);

src/yb/yql/pgwrapper/pg_libpq-test.cc
@@ -3327,6 +3327,32 @@ TEST_F_EX(PgLibPqTest,
 }
 #endif
 
+// The motive of this test is to prove that when a postgres backend errors out
+// while possessing an LWLock, the lock is released.
+// TEST_yb_lwlock_error_after_acquire_pg_stat_statements_reset when set true
+// will error out a postgres backend after acquiring a LWLock. Specifically in
+// this example, when pg_stat_statements_reset() function is called when this
+// flag is set, it errors out after acquiring a lock on pgss->lock.
+// We verify that future commands on the same connection do not deadlock as the
+// lock should have been released after error.
+class PgLibPqYSQLBackendError: public PgLibPqTest {
+ public:
+  void UpdateMiniClusterOptions(ExternalMiniClusterOptions* options) override {
+    options->extra_tserver_flags.push_back(
+        Format("--TEST_yb_lwlock_error_after_acquire_pg_stat_statements_reset=true"));
+  }
+};
+
+TEST_F_EX(PgLibPqTest,
+          YB_DISABLE_TEST_IN_TSAN(TestLWPgBackendErrorAfterLWLockAcquire),
+          PgLibPqYSQLBackendError) {
+  auto conn = ASSERT_RESULT(Connect());
+  ASSERT_NOK(conn.FetchFormat("SELECT pg_stat_statements_reset()"));
+
+  // Verify that future commands on the same connection works.
+  EXPECT_OK(conn.FetchFormat("SELECT 1"));
+}
+
 TEST_F_EX(PgLibPqTest, YbTableProperties, PgLibPqTestRF1) {
   const string kDatabaseName = "yugabyte";
   const string kTableName ="test";