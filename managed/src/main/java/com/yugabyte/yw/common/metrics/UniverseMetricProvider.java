ff7d952de2b1fa350bf8b2a6f61cbf8ae257edee
vmallepalli <vmallepalli@yugabyte.com>
2023-11-08 14:00:24 -0600

    Title: [PLAT-11289] Provisioned iops/throughput metrics consider per AZ overrides
    Summary: [PLAT-11289] Provisioned iops/throughput metrics consider per AZ overrides
    
    Test Plan:
    Create universe with per AZ overrides for iops/throughput. Ensure each node's provisioned iops/throughput
    metrics are exported accurately.
    
    Manual testing (will be done post-merge in yba devportal for this specific diff.):
    - Create cluster with per AZ overrides that specify different iops/throughput
    - Verify provisioned iops/throughput metrics output az specific values
    
    Reviewers: yshchetinin, daniel, cwang
    
    Reviewed By: yshchetinin
    
    Subscribers: yugaware
    
    Differential Revision: https://phorge.dev.yugabyte.com/D30069

--- managed/src/main/java/com/yugabyte/yw/common/metrics/UniverseMetricProvider.java
+++ managed/src/main/java/com/yugabyte/yw/common/metrics/UniverseMetricProvider.java
@@ -30,6 +30,7 @@ import com.yugabyte.yw.models.*;
 import com.yugabyte.yw.models.ImageBundle.ImageBundleType;
 import com.yugabyte.yw.models.KmsHistoryId.TargetType;
 import com.yugabyte.yw.models.filters.MetricFilter;
+import com.yugabyte.yw.models.helpers.DeviceInfo;
 import com.yugabyte.yw.models.helpers.KnownAlertLabels;
 import com.yugabyte.yw.models.helpers.NodeDetails;
 import com.yugabyte.yw.models.helpers.PlatformMetrics;
@@ -306,9 +307,10 @@ public class UniverseMetricProvider implements MetricsProvider {
               if (nodeDetails.placementUuid != null) {
                 UniverseDefinitionTaskParams.Cluster cluster =
                     universe.getCluster(nodeDetails.placementUuid);
-                if (cluster != null && cluster.userIntent.deviceInfo != null) {
-                  Integer iops = cluster.userIntent.deviceInfo.diskIops;
-                  Integer throughput = cluster.userIntent.deviceInfo.throughput;
+                if (cluster != null && cluster.userIntent != null) {
+                  DeviceInfo deviceInfo = cluster.userIntent.getDeviceInfoForNode(nodeDetails);
+                  Integer iops = deviceInfo.diskIops;
+                  Integer throughput = deviceInfo.throughput;
                   if (iops != null) {
                     universeGroup.metric(
                         createNodeMetric(
