95a94785e585ef2a1f0c281cc3abddb717ca8a18
yusong-yan <yusongyan3@gmail.com>
2023-11-09 15:57:05 +0000

    [#19904] docdb : Fix Master's tablet_overhead mem_tracker zero consumption issue
    Summary:
    Issue:
    In Master's memory breakdown UI page, we observed tablet_overhead tracker always shows zero memory consumption even if the per tablet trackers have memory usage. This is due to Master using server mem_tracker per-tablet's parent_mem_tracker in `SysCatalogTable::OpenTablet()`
    
    Another side effect of this is, TServer and Master have different MemTracker metric name, for example:
    TServer: `mem_tracker_server_Tablets_overhead_PerTablet_RegularDB_MemTable`
    Master: `mem_tracker_server_PerTablet_RegularDB_MemTable`
    
    Fix:
    Pass tablets_overhed_mem_trackers to each tablet, rather than Master's server mem_tracker
    Jira: DB-8850
    
    Test Plan:
    ./yb_build.sh --cxx-test master_sys_catalog-test --gtest_filter SysCatalogTest.TestTabletMemTracker --clang17
    
    Verified by checking local:7000/mem-trackers UI page
    
    Reviewers: rthallam, zdrudi
    
    Reviewed By: rthallam, zdrudi
    
    Subscribers: ybase, bogdan
    
    Differential Revision: https://phorge.dev.yugabyte.com/D30097

--- src/yb/master/sys_catalog.cc
+++ src/yb/master/sys_catalog.cc
@@ -598,7 +598,7 @@ Status SysCatalogTable::OpenTablet(const scoped_refptr<tablet::RaftGroupMetadata
       .metadata = metadata,
       .client_future = master_->async_client_initializer().get_client_future(),
       .clock = scoped_refptr<server::Clock>(master_->clock()),
-      .parent_mem_tracker = master_->mem_tracker(),
+      .parent_mem_tracker = mem_manager_->tablets_overhead_mem_tracker(),
       .block_based_table_mem_tracker = mem_manager_->block_based_table_mem_tracker(),
       .metric_registry = metric_registry_,
       .log_anchor_registry = tablet_peer()->log_anchor_registry(),
