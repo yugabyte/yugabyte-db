yugabyte/yugabyte-db

commit 1524735bd0028cf3878fb2d861b94c48dedef4d3

[#14559][YSQL][YCQL] Implement select distinct pushdown to DocDB
Summary:
We implement pushdown for SELECT DISTINCT query.

We extend the Hybrid Scan work to support scanning tuple prefixes. Specifically, with a given prefix length, Hybrid Scan will advance to the next prefix that is different from the previous one.

We need to determine the prefix length to be used when scanning in DocDB. This should equal the index of the last column to be requested in the scan.

Test Plan:
unit test added.

A example to demonstrate pushdown working as intended:

Populate data

```
yugabyte=# create table t(h int, c int);
CREATE TABLE
create index idx on t(h ASC, c ASC);

yugabyte=# insert into t (select 1, i from generate_series(1, 1000000) as i);
INSERT 0 1000000
yugabyte=# insert into t (select 2, i from generate_series(1, 1000000) as i);
INSERT 0 1000000
```

Before

```
yugabyte=# explain analyze select distinct h from t where h <= 2;
                                                          QUERY PLAN
-------------------------------------------------------------------------------------------------------------------------------
 Unique  (cost=0.00..15.50 rows=82 width=4) (actual time=7.259..12649.788 rows=2 loops=1)
   ->  Index Only Scan using idx on t  (cost=0.00..15.25 rows=100 width=4) (actual time=7.255..12415.214 rows=2000000 loops=1)
         Index Cond: (h <= 2)
         Heap Fetches: 0
 Planning Time: 0.094 ms
 Execution Time: 12649.868 ms
 Peak Memory Usage: 8 kB
(7 rows)
```

After

```
yugabyte=# explain analyze select distinct h from t where h <= 2;
                                                     QUERY PLAN
---------------------------------------------------------------------------------------------------------------------
 Unique  (cost=0.00..15.50 rows=82 width=4) (actual time=2.182..2.191 rows=2 loops=1)
   ->  Index Only Scan using idx on t  (cost=0.00..15.25 rows=100 width=4) (actual time=2.178..2.183 rows=2 loops=1)
         Index Cond: (h <= 2)

src/yb/docdb/doc_rowwise_iterator.cc
@@ -122,8 +122,8 @@ Status DocRowwiseIterator::Init(TableType table_type, const Slice& sub_doc_key) 
 
 Result<bool> DocRowwiseIterator::InitScanChoices(
     const DocQLScanSpec& doc_spec, const KeyBytes& lower_doc_key, const KeyBytes& upper_doc_key) {
-  scan_choices_ = ScanChoices::Create(
-      doc_read_context_.schema, doc_spec, lower_doc_key, upper_doc_key);
+  scan_choices_ = ScanChoices::Create(doc_read_context_.schema, doc_spec, lower_doc_key,
+    upper_doc_key, doc_spec.prefix_length());
 
   if (scan_choices_ && scan_choices_->IsInitialPositionKnown()) {
     // Let's not seek to the lower doc key or upper doc key. We know exactly what we want.
@@ -137,8 +137,8 @@ Result<bool> DocRowwiseIterator::InitScanChoices(
 Result<bool> DocRowwiseIterator::InitScanChoices(
     const DocPgsqlScanSpec& doc_spec, const KeyBytes& lower_doc_key,
     const KeyBytes& upper_doc_key) {
-  scan_choices_ = ScanChoices::Create(
-      doc_read_context_.schema, doc_spec, lower_doc_key, upper_doc_key);
+  scan_choices_ = ScanChoices::Create(doc_read_context_.schema, doc_spec, lower_doc_key,
+    upper_doc_key, doc_spec.prefix_length());
 
   if (scan_choices_ && scan_choices_->IsInitialPositionKnown()) {
     // Let's not seek to the lower doc key or upper doc key. We know exactly what we want.

src/yb/docdb/scan_choices.cc
@@ -48,9 +48,11 @@ HybridScanChoices::HybridScanChoices(
   const std::shared_ptr<std::vector<OptionList>>& range_options,
   const std::vector<ColumnId>& range_bounds_col_ids,
   const QLScanRange* range_bounds,
-  const ColGroupHolder& col_groups)
+  const ColGroupHolder& col_groups,
+  const size_t prefix_length)
     : ScanChoices(is_forward_scan), lower_doc_key_(lower_doc_key),
-      upper_doc_key_(upper_doc_key), col_groups_(col_groups) {
+      upper_doc_key_(upper_doc_key), col_groups_(col_groups),
+      prefix_length_(prefix_length) {
   size_t num_hash_cols = schema.num_hash_key_columns();
 
   for (size_t idx = num_hash_cols; idx < schema.num_key_columns(); idx++) {
@@ -166,23 +168,25 @@ HybridScanChoices::HybridScanChoices(
 const Schema& schema,
 const DocPgsqlScanSpec& doc_spec,
 const KeyBytes& lower_doc_key,
-const KeyBytes& upper_doc_key)
+const KeyBytes& upper_doc_key,
+const size_t prefix_length)
 : HybridScanChoices(
   schema, lower_doc_key, upper_doc_key, doc_spec.is_forward_scan(),
   doc_spec.range_options_indexes(), doc_spec.range_options(),
   doc_spec.range_bounds_indexes(), doc_spec.range_bounds(),
-  doc_spec.range_options_groups()) {}
+  doc_spec.range_options_groups(), prefix_length) {}
 
 HybridScanChoices::HybridScanChoices(
   const Schema& schema,
   const DocQLScanSpec& doc_spec,
   const KeyBytes& lower_doc_key,
-  const KeyBytes& upper_doc_key)
+  const KeyBytes& upper_doc_key,
+  const size_t prefix_length)
   : HybridScanChoices(
     schema, lower_doc_key, upper_doc_key, doc_spec.is_forward_scan(),
     doc_spec.range_options_indexes(), doc_spec.range_options(),
     doc_spec.range_bounds_indexes(), doc_spec.range_bounds(),
-    doc_spec.range_options_groups()) {}
+    doc_spec.range_options_groups(), prefix_length) {}
 
 std::vector<OptionRange>::const_iterator
 HybridScanChoices::GetOptAtIndex(size_t opt_list_idx, size_t opt_index) {
@@ -669,8 +673,9 @@ std::vector<OptionRange> HybridScanChoices::TEST_GetCurrentOptions() {
 Status HybridScanChoices::DoneWithCurrentTarget() {
   // prev_scan_target_ is necessary for backwards scans
   prev_scan_target_ = current_scan_target_;
-  RETURN_NOT_OK(
-      IncrementScanTargetAtOptionList(static_cast<int>(current_scan_target_ranges_.size()) - 1));
+  int incr_idx =
+      static_cast<int>(prefix_length_ ? prefix_length_ : current_scan_target_ranges_.size()) - 1;
+  RETURN_NOT_OK(IncrementScanTargetAtOptionList(incr_idx));
   current_scan_target_.AppendKeyEntryType(KeyEntryType::kGroupEnd);
 
   // if we we incremented the last index then
@@ -756,9 +761,11 @@ Status HybridScanChoices::SeekToCurrentTarget(IntentAwareIteratorIf* db_iter) {
 
 ScanChoicesPtr ScanChoices::Create(
     const Schema& schema, const DocQLScanSpec& doc_spec,
-    const KeyBytes& lower_doc_key, const KeyBytes& upper_doc_key) {
+    const KeyBytes& lower_doc_key, const KeyBytes& upper_doc_key,
+    const size_t prefix_length) {
   if (doc_spec.range_options() || doc_spec.range_bounds()) {
-    return std::make_unique<HybridScanChoices>(schema, doc_spec, lower_doc_key, upper_doc_key);
+    return std::make_unique<HybridScanChoices>(
+        schema, doc_spec, lower_doc_key, upper_doc_key, prefix_length);
   }
 
   return nullptr;
@@ -766,9 +773,11 @@ ScanChoicesPtr ScanChoices::Create(
 
 ScanChoicesPtr ScanChoices::Create(
     const Schema& schema, const DocPgsqlScanSpec& doc_spec,
-    const KeyBytes& lower_doc_key, const KeyBytes& upper_doc_key) {
+    const KeyBytes& lower_doc_key, const KeyBytes& upper_doc_key,
+    const size_t prefix_length) {
   if (doc_spec.range_options() || doc_spec.range_bounds()) {
-    return std::make_unique<HybridScanChoices>(schema, doc_spec, lower_doc_key, upper_doc_key);
+    return std::make_unique<HybridScanChoices>(
+        schema, doc_spec, lower_doc_key, upper_doc_key, prefix_length);
   }
 
   return nullptr;

src/yb/docdb/scan_choices.h
@@ -60,11 +60,11 @@ class ScanChoices {
 
   static ScanChoicesPtr Create(
       const Schema& schema, const DocQLScanSpec& doc_spec, const KeyBytes& lower_doc_key,
-      const KeyBytes& upper_doc_key);
+      const KeyBytes& upper_doc_key, const size_t prefix_length);
 
   static ScanChoicesPtr Create(
       const Schema& schema, const DocPgsqlScanSpec& doc_spec, const KeyBytes& lower_doc_key,
-      const KeyBytes& upper_doc_key);
+      const KeyBytes& upper_doc_key, const size_t prefix_length);
 
  protected:
   const bool is_forward_scan_;
@@ -253,19 +253,22 @@ class HybridScanChoices : public ScanChoices {
       const std::shared_ptr<std::vector<OptionList>>& range_options,
       const std::vector<ColumnId>& range_bounds_col_ids,
       const QLScanRange* range_bounds,
-      const ColGroupHolder& col_groups);
+      const ColGroupHolder& col_groups,
+      const size_t prefix_length);
 
   HybridScanChoices(
       const Schema& schema,
       const DocPgsqlScanSpec& doc_spec,
       const KeyBytes& lower_doc_key,
-      const KeyBytes& upper_doc_key);
+      const KeyBytes& upper_doc_key,
+      const size_t prefix_length);
 
   HybridScanChoices(
       const Schema& schema,
       const DocQLScanSpec& doc_spec,
       const KeyBytes& lower_doc_key,
-      const KeyBytes& upper_doc_key);
+      const KeyBytes& upper_doc_key,
+      const size_t prefix_length);
 
   Result<bool> SkipTargetsUpTo(const Slice& new_target) override;
   Status DoneWithCurrentTarget() override;
@@ -337,6 +340,8 @@ class HybridScanChoices : public ScanChoices {
   // The scan keys we would iterate over would be
   // (1,4,3), (1,6,3), (2,4,3), (2,6,3)
   ColGroupHolder col_groups_;
+
+  size_t prefix_length_ = 0;
 };
 
 }  // namespace docdb

java/yb-cql/src/test/java/org/yb/cql/TestSelect.java
@@ -2253,6 +2253,64 @@ public class TestSelect extends BaseCQLTest {
   }
 
   @Test
+  public void testDistinctPushdown() throws Exception {
+    session.execute("create table t(h int, c int, primary key(h, c))");
+    session.execute("insert into t(h, c) values (0, 0)");
+    session.execute("insert into t(h, c) values (0, 1)");
+    session.execute("insert into t(h, c) values (0, 2)");
+    session.execute("insert into t(h, c) values (1, 0)");
+    session.execute("insert into t(h, c) values (1, 1)");
+    session.execute("insert into t(h, c) values (1, 2)");
+
+    // For both queries, the scan should jump directly to the relevant primary key,
+    // so the number of seeks is equal to the items to be retrived.
+    {
+      String query = "select distinct h from t where h = 0";
+      String[] rows = {"Row[0]"};
+
+      RocksDBMetrics metrics = assertPartialRangeSpec("t", query, rows);
+      assertEquals(1, metrics.seekCount);
+    }
+
+    {
+      String query = "select distinct h from t where h in (0, 1)";
+      String[] rows = {"Row[0]", "Row[1]"};
+
+      RocksDBMetrics metrics = assertPartialRangeSpec("t", query, rows);
+      assertEquals(2, metrics.seekCount);
+    }
+  }
+
+  @Test
+  public void testDistinctPushdownSecondColumn() throws Exception {
+    session.execute("create table t(r1 int, r2 int, r3 int, primary key(r2, r3))");
+    session.execute("insert into t(r1, r2, r3) values (0, 0, 0)");
+    session.execute("insert into t(r1, r2, r3) values (0, 0, 1)");
+    session.execute("insert into t(r1, r2, r3) values (0, 0, 2)");
+    session.execute("insert into t(r1, r2, r3) values (1, 1, 0)");
+    session.execute("insert into t(r1, r2, r3) values (1, 1, 1)");
+    session.execute("insert into t(r1, r2, r3) values (1, 1, 2)");
+
+    // For both queries, the scan should jump directly to the relevant primary key,
+    // so the number of seeks is equal to the items to be retrived.
+    {
+      String query = "select distinct r2 from t where r2 = 0";
+      String[] rows = {"Row[0]"};
+
+      RocksDBMetrics metrics = assertPartialRangeSpec("t", query, rows);
+      assertEquals(1, metrics.seekCount);
+    }
+
+    {
+      String query = "select distinct r2 from t where r2 in (0, 1)";
+      String[] rows = {"Row[0]", "Row[1]"};
+
+      RocksDBMetrics metrics = assertPartialRangeSpec("t", query, rows);
+      assertEquals(2, metrics.seekCount);
+    }
+  }
+
+  @Test
   public void testToJson() throws Exception {
     // Create test table.
     session.execute("CREATE TABLE test_tojson (c1 int PRIMARY KEY, c2 float, c3 double, c4 " +

java/yb-pgsql/src/test/java/org/yb/pgsql/TestPgSelect.java
@@ -953,6 +953,204 @@ public class TestPgSelect extends BasePgSQLTest {
   }
 
   @Test
+  public void testIndexDistinctRangeScan() throws Exception {
+    String query = "CREATE TABLE t(r1 INT, r2 INT, PRIMARY KEY(r1 ASC, r2 ASC))";
+    try (Statement statement = connection.createStatement()) {
+      statement.execute(query);
+
+      query = "INSERT INTO t (SELECT 1, i FROM GENERATE_SERIES(1, 10) AS i)";
+      statement.execute(query);
+
+      query = "INSERT INTO t (SELECT 2, i FROM GENERATE_SERIES(1, 10) AS i)";
+      statement.execute(query);
+
+      query = "INSERT INTO t (SELECT 3, i FROM GENERATE_SERIES(1, 10) AS i)";
+      statement.execute(query);
+
+      Set<Row> expectedRows = new HashSet<>();
+      expectedRows.add(new Row(1));
+      expectedRows.add(new Row(2));
+      expectedRows.add(new Row(3));
+      query = "SELECT DISTINCT r1 FROM t WHERE r1 <= 3";
+      assertRowSet(statement, query, expectedRows);
+
+      // With DISTINCT pushed down to DocDB, we only to scan three keys:
+      // 1. From kLowest, seek to 1.
+      // 2. From 1, seek to 2.
+      // 3. From 2, seek to 3.
+      // The constraint r1 <= 3 implies we are done after the third seek.
+      RocksDBMetrics metrics = assertFullDocDBFilter(statement, query, "t");
+      assertEquals(3, metrics.seekCount);
+    }
+  }
+
+  @Test
+  public void testIndexDistinctMulticolumnsScan() throws Exception {
+    String query = "CREATE TABLE t(r1 INT, r2 INT, r3 INT, PRIMARY KEY(r1 ASC, r2 ASC, r3 ASC))";
+    try (Statement statement = connection.createStatement()) {
+      statement.execute(query);
+
+      query = "INSERT INTO t (SELECT 1, 1, i FROM GENERATE_SERIES(1, 100) AS i)";
+      statement.execute(query);
+
+      query = "INSERT INTO t (SELECT 2, 2, i FROM GENERATE_SERIES(1, 100) AS i)";
+      statement.execute(query);
+
+      query = "INSERT INTO t (SELECT 3, 1, i FROM GENERATE_SERIES(1, 100) AS i)";
+      statement.execute(query);
+
+      query = "INSERT INTO t (SELECT 3, 2, i FROM GENERATE_SERIES(1, 100) AS i)";
+      statement.execute(query);
+
+      query = "INSERT INTO t (SELECT 3, 3, i FROM GENERATE_SERIES(1, 100) AS i)";
+      statement.execute(query);
+
+      Set<Row> expectedRows = new HashSet<>();
+      expectedRows.add(new Row(1, 1));
+      expectedRows.add(new Row(2, 2));
+      expectedRows.add(new Row(3, 1));
+      expectedRows.add(new Row(3, 2));
+      expectedRows.add(new Row(3, 3));
+      query = "SELECT DISTINCT r1, r2 FROM t WHERE r1 <= 3";
+      assertRowSet(statement, query, expectedRows);
+
+      // We need to do 6 seeks here:
+      // 1. Seek from (kLowest, kLowest), found (1, 1).
+      // 2. Seek from (1, 1), found (2, 2).
+      // 3. Seek from (2, 2), found (3, 1).
+      // 4. Seek from (3, 1), found (3, 2).
+      // 5. Seek from (3, 2), found (3, 3).
+      // 6. Seek from (3, 3), found no more key.
+      // Note that we need the last seek, since under the condition r1 <= 3, we don't know whether
+      // there are more items to be scanned.
+      RocksDBMetrics metrics = assertFullDocDBFilter(statement, query, "t");
+      assertEquals(6, metrics.seekCount);
+    }
+  }
+
+  @Test
+  public void testIndexDistinctSkipColumnScan() throws Exception {
+    String query = "CREATE TABLE t(r1 INT, r2 INT, r3 INT, r4 INT, " +
+                    " PRIMARY KEY(r1 ASC, r2 ASC, r3 ASC, r4 ASC))";
+
+    try (Statement statement = connection.createStatement()) {
+      statement.execute(query);
+
+      query = "INSERT INTO t (SELECT 1, 1, 1, i FROM GENERATE_SERIES(1, 100) AS i)";
+      statement.execute(query);
+
+      query = "INSERT INTO t (SELECT 2, 2, 2, i FROM GENERATE_SERIES(1, 100) AS i)";
+      statement.execute(query);
+
+      query = "INSERT INTO t (SELECT 3, 3, 3, i FROM GENERATE_SERIES(1, 100) AS i)";
+      statement.execute(query);
+
+      Set<Row> expectedRows = new HashSet<>();
+      expectedRows.add(new Row(1, 1));
+      expectedRows.add(new Row(2, 2));
+      expectedRows.add(new Row(3, 3));
+      query = "SELECT DISTINCT r1, r3 FROM t WHERE r3 <= 3";
+      assertRowSet(statement, query, expectedRows);
+
+      RocksDBMetrics metrics = assertFullDocDBFilter(statement, query, "t");
+      assertEquals(1, metrics.seekCount);
+    }
+  }
+
+  @Test
+  public void testDistinctScanHashColumn() throws Exception {
+    String query = "CREATE TABLE t(r1 INT, r2 INT, r3 INT, PRIMARY KEY(r1 HASH, r2 ASC, r3 ASC))";
+    try (Statement statement = connection.createStatement()) {
+      statement.execute(query);
+
+      query = "INSERT INTO t (SELECT i, i, i FROM GENERATE_SERIES(1, 100) AS i)";
+      statement.execute(query);
+
+      {
+        Set<Row> expectedRows = new HashSet<>();
+        for (int i = 1; i <= 100; i++) {
+          expectedRows.add(new Row(i));
+        }
+
+        query = "SELECT DISTINCT r1 FROM t WHERE r1 <= 100";
+        assertRowSet(statement, query, expectedRows);
+
+        // Here we do a sequential scan.
+        RocksDBMetrics metrics = assertFullDocDBFilter(statement, query, "t");
+        assertEquals(3, metrics.seekCount);
+      }
+
+      {
+        Set<Row> expectedRows = new HashSet<>();
+        expectedRows.add(new Row(100));
+
+        query = "SELECT DISTINCT r1 FROM t WHERE r1 = 100";
+        assertRowSet(statement, query, expectedRows);
+
+        // Here we do an index scan.
+        RocksDBMetrics metrics = assertFullDocDBFilter(statement, query, "t");
+        assertEquals(1, metrics.seekCount);
+      }
+    }
+  }
+
+  @Test
+  public void testDistinctMultiHashColumns() throws Exception {
+    String query = "CREATE TABLE t(h1 INT, h2 INT, r INT, PRIMARY KEY((h1, h2) HASH, r ASC))";
+    try (Statement statement = connection.createStatement()) {
+      statement.execute(query);
+
+      query = "INSERT INTO t (SELECT i, i, i FROM GENERATE_SERIES(1, 100) AS i)";
+      statement.execute(query);
+
+      {
+        Set<Row> expectedRows = new HashSet<>();
+        expectedRows.add(new Row(1, 1));
+
+        query = "SELECT DISTINCT h1, h2 FROM t WHERE h1 = 1 AND h2 = 1";
+        assertRowSet(statement, query, expectedRows);
+
+        RocksDBMetrics metrics = assertFullDocDBFilter(statement, query, "t");
+        assertEquals(1, metrics.seekCount);
+      }
+    }
+  }
+
+  @Test
+  public void testDistinctOnNonPrefixScan() throws Exception {
+    String query = "CREATE TABLE t(r1 INT, r2 INT, r3 INT)";
+    try (Statement statement = connection.createStatement()) {
+      statement.execute(query);
+
+      query = "CREATE INDEX idx on t(r3 ASC)";
+      statement.execute(query);
+
+      query = "INSERT INTO t (SELECT i, 1, 1 FROM GENERATE_SERIES(1, 100) AS i)";
+      statement.execute(query);
+
+      {
+        Set<Row> expectedRows = new HashSet<>();
+        expectedRows.add(new Row(1));
+
+        query = "SELECT DISTINCT r3 FROM t WHERE r3 <= 10";
+        assertRowSet(statement, query, expectedRows);
+
+        // Here we perform the seek on the index table with two seeks:
+        // 1. From kLowest, we seek to 1.
+        // 2. From 1, we seek to the next key, which is not found.
+        // Note that we need to seek on the index table. The main table will result
+        // in zero seeks.
+
+        RocksDBMetrics metrics = assertFullDocDBFilter(statement, query, "t");
+        assertEquals(0, metrics.seekCount);
+
+        metrics = assertFullDocDBFilter(statement, query, "idx");
+        assertEquals(2, metrics.seekCount);
+      }
+    }
+  }
+
+  @Test
   public void testStrictInequalities() throws Exception {
     String query = "CREATE TABLE sample_table(h INT, r1 INT, r2 INT, r3 INT, " +
                    "v INT, PRIMARY KEY(h HASH, r1 ASC, r2 ASC, r3 DESC))";

src/postgres/src/backend/executor/nodeUnique.c
@@ -53,6 +53,13 @@ ExecUnique(PlanState *pstate)
 	PlanState  *outerPlan;
 
 	CHECK_FOR_INTERRUPTS();
+	
+	/*
+	 * SELECT DISTINCT is only enabled for an index scan. Specifically, for a scan on hash columns,
+	 * the index scan will not be used.
+	 */
+	if (IsYugaByteEnabled())
+		pstate->state->yb_exec_params.is_select_distinct = true;
 
 	/*
 	 * get information from the node

src/yb/common/pgsql_protocol.proto
@@ -407,6 +407,10 @@ message PgsqlReadRequestPB {
   // Reading distinct columns?
   optional bool distinct = 11 [default = false];
 
+  // Current only used on SELECT DISTINCT scan. If the value is greater than 0, use the specified
+  // prefix length to scan the table.
+  optional uint64 prefix_length = 39 [default = 0];
+
   // Flag for reading aggregate values.
   optional bool is_aggregate = 12 [default = false];
 

src/yb/common/ql_protocol.proto
@@ -252,6 +252,10 @@ message QLReadRequestPB {
   // Reading distinct columns?
   optional bool distinct = 12 [default = false];
 
+  // Current only used on SELECT DISTINCT scan. If the value is greater than 0, use the specified
+  // prefix length to scan the table.
+  optional uint64 prefix_length = 23 [default = 0];
+
   // Limit number of rows to return. For QL SELECT, this limit is the smaller of the page size (max
   // (max number of rows to return per fetch) & the LIMIT clause if present in the SELECT statement.
   optional uint64 limit = 8;

commit 2c203f78b471f44797aaf70110b169da0121d0be

[#19911] DocDB: Avoid AdvanceToNextRow seeking past live rows upon detecting a deleted row during distinct iteration
Summary:
Distinct iteration works by generating distinct targets to seek to before seeking to them through ScanChoices. To ScanChoices, there is no difference between a deleted key and a live key. So if there is a set of keys of the form:

a b
1,1 -> dead tuple
1,2 -> live tuple

Where a and b are both ascending columns

`SELECT DISTINCT a FROM sample_table WHERE a > 0;`  will go to the first value of (a,b), see that (1,1) is the value and then attempt to seek to the next distinct value of a by seeking to (1,Inf), missing (1,2). Normally, if (1,1) was alive such a seek would correctly carry out the DISTINCT a operation by skipping over (1,2). After this change, we make ScanChoices::AdvanceToNextRow aware of whether the currently fetched row is a deleted one or not.

In HybridScanChoices, if the column restrictions don't span across all the columns, and we encounter a dead tuple, we allow `AdvanceToNextRow` to return false without seeking anywhere, signaling to callers to iterate to the next row.

This logic is similar to the scenario where a table, `sample`, with columns (a asc, b asc, c asc) encountered a query `SELECT * FROM sample WHERE a IN (1,2,4) AND b >= 9;`. In this case, the given column predicates do not span the entire keyspace and `AdvanceToNextRow` always returns false without seeking underlying iterators anywhere, signaling callers to move their iterator to the next row.

Before this change, distinct queries to tables with dead tuples could be missing rows.
This was due to a bug introduced in D20742/e3af9205a50655bd257c662cd463b45939054176 in release 2.17.1
Backports required to 2.17, 2.18, 2.19, 2.20
Jira: DB-8855

Test Plan: ./yb_build.sh --java-test 'org.yb.pgsql.TestPgRegressDistinctPushdown'

Reviewers: sergei

Reviewed By: sergei

Subscribers: smishra, ybase, yql

Differential Revision: https://phorge.dev.yugabyte.com/D30092

src/yb/docdb/doc_rowwise_iterator.cc
@@ -187,9 +187,11 @@ inline void DocRowwiseIterator::PrevDocKey(Slice key) {
   }
 }
 
-Status DocRowwiseIterator::AdvanceIteratorToNextDesiredRow(bool row_finished) {
+Status DocRowwiseIterator::AdvanceIteratorToNextDesiredRow(bool row_finished,
+                                                           bool current_fetched_row_skipped) {
   if (!IsFetchedRowStatic() &&
-      VERIFY_RESULT(scan_choices_->AdvanceToNextRow(&row_key_, db_iter_.get()))) {
+      VERIFY_RESULT(scan_choices_->AdvanceToNextRow(&row_key_, db_iter_.get(),
+                                                    current_fetched_row_skipped))) {
     return Status::OK();
   }
   if (!is_forward_scan_) {
@@ -229,7 +231,7 @@ Result<bool> DocRowwiseIterator::FetchNextImpl(TableRow table_row) {
 
   if (prev_doc_found_ != DocReaderResult::kNotFound) {
     RETURN_NOT_OK(AdvanceIteratorToNextDesiredRow(
-        prev_doc_found_ == DocReaderResult::kFoundAndFinished));
+        prev_doc_found_ == DocReaderResult::kFoundAndFinished, false));
     prev_doc_found_ = DocReaderResult::kNotFound;
   }
 
@@ -328,7 +330,8 @@ Result<bool> DocRowwiseIterator::FetchNextImpl(TableRow table_row) {
       break;
     }
 
-    RETURN_NOT_OK(AdvanceIteratorToNextDesiredRow(/* row_finished= */ false));
+    RETURN_NOT_OK(AdvanceIteratorToNextDesiredRow(/* row_finished= */ false,
+                                                  /* current_fetched_row_skipped */ true));
   }
   return true;
 }

src/yb/docdb/doc_rowwise_iterator.h
@@ -122,7 +122,9 @@ class DocRowwiseIterator final : public DocRowwiseIteratorBase {
   // ensures that the iterator will be positioned on the first kv-pair of the next row.
   // row_finished - true when current row was fully iterated. So we would not have to perform
   // extra Seek in case of full scan.
-  Status AdvanceIteratorToNextDesiredRow(bool row_finished);
+  // current_fetched_row_skipped is true if we skipped processing the most recently fetched row.
+  Status AdvanceIteratorToNextDesiredRow(bool row_finished,
+                                         bool current_fetched_row_skipped);
 
   // Read next row into a value map using the specified projection.
   Status FillRow(qlexpr::QLTableRow* table_row, const dockv::ReaderProjection* projection);

src/yb/docdb/scan_choices.cc
@@ -726,10 +726,10 @@ std::vector<OptionRange> HybridScanChoices::TEST_GetCurrentOptions() {
 }
 
 // Method called when the scan target is done being used
-Result<bool> HybridScanChoices::DoneWithCurrentTarget() {
+Result<bool> HybridScanChoices::DoneWithCurrentTarget(bool current_row_skipped) {
   bool result = false;
   if (schema_num_keys_ == scan_options_.size() ||
-      prefix_length_ > 0) {
+      (prefix_length_ > 0 && !current_row_skipped)) {
 
     ssize_t incr_idx =
         (prefix_length_ ? prefix_length_ : current_scan_target_ranges_.size()) - 1;
@@ -832,8 +832,10 @@ Result<bool> HybridScanChoices::InterestedInRow(
 }
 
 Result<bool> HybridScanChoices::AdvanceToNextRow(
-    dockv::KeyBytes* row_key, IntentAwareIteratorIf* iter) {
-  if (!VERIFY_RESULT(DoneWithCurrentTarget()) || CurrentTargetMatchesKey(row_key->AsSlice())) {
+    dockv::KeyBytes* row_key, IntentAwareIteratorIf* iter, bool current_fetched_row_skipped) {
+
+  if (!VERIFY_RESULT(DoneWithCurrentTarget(current_fetched_row_skipped)) ||
+      CurrentTargetMatchesKey(row_key->AsSlice())) {
     return false;
   }
   SeekToCurrentTarget(iter);
@@ -866,7 +868,9 @@ class EmptyScanChoices : public ScanChoices {
     return true;
   }
 
-  Result<bool> AdvanceToNextRow(dockv::KeyBytes* row_key, IntentAwareIteratorIf* iter) override {
+  Result<bool> AdvanceToNextRow(dockv::KeyBytes* row_key,
+                                IntentAwareIteratorIf* iter,
+                                bool current_fetched_row_live) override {
     return false;
   }
 };

src/yb/docdb/scan_choices.h
@@ -44,7 +44,9 @@ class ScanChoices {
   // Check whether scan choices is interested in specified row.
   // Seek on specified iterator to the next row of interest.
   virtual Result<bool> InterestedInRow(dockv::KeyBytes* row_key, IntentAwareIteratorIf* iter) = 0;
-  virtual Result<bool> AdvanceToNextRow(dockv::KeyBytes* row_key, IntentAwareIteratorIf* iter) = 0;
+  virtual Result<bool> AdvanceToNextRow(dockv::KeyBytes* row_key,
+                                        IntentAwareIteratorIf* iter,
+                                        bool current_fetched_row_skipped) = 0;
 
   static ScanChoicesPtr Create(
       const Schema& schema, const qlexpr::YQLScanSpec& doc_spec,
@@ -236,14 +238,16 @@ class HybridScanChoices : public ScanChoices {
   }
 
   Result<bool> InterestedInRow(dockv::KeyBytes* row_key, IntentAwareIteratorIf* iter) override;
-  Result<bool> AdvanceToNextRow(dockv::KeyBytes* row_key, IntentAwareIteratorIf* iter) override;
+  Result<bool> AdvanceToNextRow(dockv::KeyBytes* row_key,
+                                IntentAwareIteratorIf* iter,
+                                bool current_fetched_row_skipped) override;
 
  private:
   friend class ScanChoicesTest;
 
   // Sets current_scan_target_ to the first tuple in the filter space that is >= new_target.
   Result<bool> SkipTargetsUpTo(Slice new_target);
-  Result<bool> DoneWithCurrentTarget();
+  Result<bool> DoneWithCurrentTarget(bool current_row_skipped = false);
   void SeekToCurrentTarget(IntentAwareIteratorIf* db_iter);
 
   bool CurrentTargetMatchesKey(Slice curr);

src/postgres/src/test/regress/expected/yb_distinct_pushdown_pred.out
@@ -874,3 +874,22 @@ SELECT DISTINCT r1 FROM t t1 JOIN t t2 USING (r1) WHERE t2.r1 = 1 AND t2.r2 = 1 
 (1 row)
 
 DROP TABLE t;
+CREATE TABLE sample(a int, b int, primary key(a asc, b asc));
+INSERT INTO sample VALUES (1,1), (1,2);
+DELETE FROM sample where b = 1;
+EXPLAIN (ANALYZE, COSTS OFF, TIMING OFF, SUMMARY OFF) SELECT DISTINCT a FROM sample WHERE a > 0;
+                                  QUERY PLAN
+-------------------------------------------------------------------------------
+ Unique (actual rows=1 loops=1)
+   ->  Distinct Index Scan using sample_pkey on sample (actual rows=1 loops=1)
+         Index Cond: (a > 0)
+         Distinct Prefix: 1
+(4 rows)
+
+SELECT DISTINCT a FROM sample WHERE a > 0;
+ a
+---
+ 1
+(1 row)
+
+DROP TABLE sample;

src/postgres/src/test/regress/sql/yb_distinct_pushdown_pred.sql
@@ -298,3 +298,12 @@ SELECT DISTINCT r1 FROM t t1 JOIN t t2 USING (r1, r2);
 SELECT DISTINCT r1 FROM t t1 JOIN t t2 USING (r1) WHERE t2.r1 = 1 AND t2.r2 = 1 AND t2.r3 = 1;
 
 DROP TABLE t;
+
+CREATE TABLE sample(a int, b int, primary key(a asc, b asc));
+INSERT INTO sample VALUES (1,1), (1,2);
+DELETE FROM sample where b = 1;
+
+EXPLAIN (ANALYZE, COSTS OFF, TIMING OFF, SUMMARY OFF) SELECT DISTINCT a FROM sample WHERE a > 0;
+SELECT DISTINCT a FROM sample WHERE a > 0;
+
+DROP TABLE sample;