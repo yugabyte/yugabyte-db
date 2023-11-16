ae7861ec8e8c5ccd25e001c70af534811cfc10f1
Daniel Jiang <djiang@yugabyte.com>
2023-11-16 19:27:05 +0000

    [#19972] yugabyted: Alerts page API call for other nodes should go through the yugabyted API server
    Summary:
    ### Issue
    
    Yugabyted UI is not able to display the `Alert` messages from other yugabyted nodes.
    
    ### Analysis
    
    Yugabyted UI fetches the `Alert` messages from different yugabyted nodes by directly invoking the `node2:15433/alerts` API of other nodes.
    
    This causes the API calls to fail when the internal ip-address of the other yugabyted nodes are not rechargable from where the yugabyted UI is running. Generally, yugabyted UI is invoked from the web browser to personal laptop or VM outside of the same VPC network.
    
    ### Code changes
    
    For the`/alerts` endpoint in the API server,  we need to add `node_address` parameter to fetch the alerts of the other nodes.
    
    ```
    node1:15433/alerts?node_address=<node2-address>
    ```
    Jira: DB-8947
    
    Test Plan: no test plan
    
    Reviewers: nikhil
    
    Reviewed By: nikhil
    
    Subscribers: yugabyted-dev, djiang
    
    Differential Revision: https://phorge.dev.yugabyte.com/D30278

--- yugabyted-ui/ui/src/features/clusters/details/settings/GFlagsOverview.tsx
+++ yugabyted-ui/ui/src/features/clusters/details/settings/GFlagsOverview.tsx
@@ -2,7 +2,7 @@ import React, { FC, useMemo, useState } from "react";
 import { useTranslation } from "react-i18next";
 import { Box, LinearProgress, makeStyles, MenuItem, Paper, Typography } from "@material-ui/core";
 import { YBSelect, YBTable } from "@app/components";
-import { useGetClusterNodesQuery, useGetGflagsQuery } from "@app/api/src";
+import { useGetClusterNodesQuery, useGetGflagsQuery, useGetNodeAddressQuery } from "@app/api/src";
 
 const useStyles = makeStyles((theme) => ({
   paperContainer: {
@@ -49,14 +49,14 @@ export const GFlagsOverview: FC<GFlagsOverviewProps> = (/* { showDrift, toggleDr
   const { data: nodesResponse, isFetching: isFetchingNodes } = useGetClusterNodesQuery();
   const nodesNamesList =
     nodesResponse?.data.map((node) => ({ label: node.name, value: node.host })) ?? [];
+  const { data: nodeAddress } = useGetNodeAddressQuery();
 
-  const [currentNode, setCurrentNode] = useState<string | undefined>("");
+  const [currentNode, setCurrentNode] = useState<string | undefined>(nodeAddress ?? "");
   React.useEffect(() => {
-    if (!currentNode && nodesResponse) {
-      setCurrentNode(nodesNamesList[0].value);
+    if (currentNode === "" || !nodesNamesList.find((node) => node.value === currentNode)) {
+      setCurrentNode(nodeAddress ?? "");
     }
-  }, [nodesResponse]);
-
+  }, [nodesNamesList, nodeAddress])
   const { data, isFetching: isFetchingGFlags } = useGetGflagsQuery<UpstreamGflagResponseType>(
     { node_address: currentNode || "" },
     { query: { enabled: !!currentNode } }
