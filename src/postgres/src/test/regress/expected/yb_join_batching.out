yugabyte/yugabyte-db

commit 12e23adb6ac517cde3bacfcbe64a8292470701cf

[#14068] YSQL: Allow Batched Nested Loop joins for conjunction clauses
Summary:
This change allows for batching on nested loop join clauses that are conjunctions. A join clause of the form
`inner_var1 = f1(outer_vars) AND inner_var2 = f2(outer_vars)` is converted into `ROW(inner_var1, inner_var2) IN (ROW(f1(outer_vars_$1),  f2(outer_vars_$1)), ROW(f1(outer_vars_$2),  f2(outer_vars_$2)), ROW(f1(outer_vars_$3), f2(outer_vars_$3))...)` when batching. Before this change DocDB and PgGate could not handle such IN conditions on RowExpressions. This change adds those necessary changes to to PgGate and the DocDB iterator as well.

Summary of changes:
- indxpath.c has been edited to allow batching conjunction clauses as above.
- A mechanism to take a bunch of individually batched clauses (inner_var1 = BatchedExpr(f1(outer_vars)), inner_var2 = BatchedExpr(f2(outer_vars))) and "zip them up" to (ROW(inner_var1, inner_var2) = BatchedExpr(ROW(f1(outer_vars), f2(outer_vars))) has been added to restrictinfo.c.
- nodeindexscan.c has had its ScanKey building mechanism augmented to allow for ScalarArrayOperations on RowExpressions.
- yb_scan.c has been edited in the way it processes RowExpressions. Previously, all the ScanKeys of a `Scan` object was isomorphically translated into the ScanKey array of the `YbScanDesc` object in `ybcBeginScan`. This was the case for RowExpression ScanKeys as well. Their subsidiary ScanKeys in the sk_argument array were not flattened into the ScanKey array of the `YbScanDesc`. This would cause a lot of unnecessary casing in order to process Row keys. This has been alleviated. This file also binds ScalaryArrayOperations on Row expressions via the `ybcBindTupleExprCondIn` method which is the Row counterpart for `ybcBindColumnCondIn`.
- Row expressions are bound as tuples in Pggate. Changes have been made throughout pggate to account for this notion.

src/yb/yql/pggate/pg_dml_read.cc
@@ -189,13 +189,31 @@ Status PgDmlRead::ProcessEmptyPrimaryBinds() {
   bool miss_partition_columns = false;
   bool has_partition_columns = false;
 
+  // Collecting column indexes that are involved in a tuple
+  std::vector<size_t> tuple_col_ids;
+
   for (size_t index = 0; index != bind_->num_hash_key_columns(); ++index) {
     auto expr = bind_.ColumnForIndex(index).bind_pb();
+    auto colid = bind_.ColumnForIndex(index).id();
     // For IN clause expr->has_condition() returns 'true'.
-    if (!expr || (!expr->has_condition() && (expr_binds_.find(expr) == expr_binds_.end()))) {
+    if (!expr || (!expr->has_condition() &&
+                  (expr_binds_.find(expr) == expr_binds_.end()) &&
+                  (std::find(tuple_col_ids.begin(), tuple_col_ids.end(), colid) ==
+                   tuple_col_ids.end()))) {
       miss_partition_columns = true;
+      continue;
     } else {
       has_partition_columns = true;
+    }
+
+    if (expr && expr->has_condition()) {
+      const auto& lhs = *expr->condition().operands().begin();
+      if (lhs.has_tuple()) {
+        const auto& tuple = lhs.tuple();
+        for (const auto& elem : tuple.elems()) {
+          tuple_col_ids.push_back(elem.column_id());
+        }
+      }
     }
   }
 
@@ -417,45 +435,66 @@ Status PgDmlRead::BindColumnCondBetween(int attr_num, PgExpr *attr_value,
   return Status::OK();
 }
 
-Status PgDmlRead::BindColumnCondIn(int attr_num, int n_attr_values, PgExpr **attr_values) {
+Status PgDmlRead::BindColumnCondIn(PgExpr *lhs, int n_attr_values, PgExpr **attr_values) {
   if (secondary_index_query_) {
     // Bind by secondary key.
-    return secondary_index_query_->BindColumnCondIn(attr_num, n_attr_values, attr_values);
+    return secondary_index_query_->BindColumnCondIn(lhs, n_attr_values, attr_values);
   }
 
-  SCHECK(attr_num != static_cast<int>(PgSystemAttrNum::kYBTupleId),
-         InvalidArgument,
-         "Operator IN cannot be applied to ROWID");
-
-  // Find column.
-  PgColumn& col = VERIFY_RESULT(bind_.ColumnForAttr(attr_num));
+  auto cols = VERIFY_RESULT(lhs->GetColumns(&bind_));
+  for (PgColumn &col : cols) {
+    SCHECK(col.attr_num() != static_cast<int>(PgSystemAttrNum::kYBTupleId),
+           InvalidArgument,
+           "Operator IN cannot be applied to ROWID");
+  }
 
   // Check datatype.
   // TODO(neil) Current code combine TEXT and BINARY datatypes into ONE representation.  Once that
   // is fixed, we can remove the special if() check for BINARY type.
-  if (col.internal_type() != InternalType::kBinaryValue) {
-    for (int i = 0; i < n_attr_values; i++) {
-      if (attr_values[i]) {
-        SCHECK_EQ(col.internal_type(), attr_values[i]->internal_type(), Corruption,
-            "Attribute value type does not match column type");
+  for (int i = 0; i < n_attr_values; i++) {
+    if (attr_values[i]) {
+      auto vals = attr_values[i]->Unpack();
+      auto curr_val_it = vals.begin();
+      for (const PgColumn &curr_col : cols) {
+        const PgExpr &curr_val = *curr_val_it;
+        auto col_type = curr_val.internal_type();
+        if (curr_col.internal_type() == InternalType::kBinaryValue)
+            continue;
+        SCHECK_EQ(curr_col.internal_type(), col_type, Corruption,
+          "Attribute value type does not match column type");
+        curr_val_it++;
       }
     }
   }
 
-  if (col.is_primary()) {
+  for (const PgColumn &curr_col : cols) {
+    // Check primary column bindings
+    if (curr_col.is_primary() && curr_col.bind_pb() != nullptr) {
+      if (expr_binds_.find(curr_col.bind_pb()) != expr_binds_.end()) {
+        LOG(WARNING) << strings::Substitute("Column $0 is already bound to another value.",
+                                            curr_col.attr_num());
+      }
+    }
+  }
+
+  // Find column.
+  // Note that in the case that we are dealing with a tuple IN,
+  // we only bind this condition to the first column in the IN. The nature of that
+  // column (hash or range) will decide how this tuple IN condition will be processed.
+  PgColumn& col = cols.front();
+  bool col_is_primary = col.is_primary();
+
+  if (col_is_primary) {
     // Alloc the protobuf.
     auto *bind_pb = col.bind_pb();
     if (bind_pb == nullptr) {
       bind_pb = AllocColumnBindPB(&col);
-    } else {
-      if (expr_binds_.find(bind_pb) != expr_binds_.end()) {
-        LOG(WARNING) << strings::Substitute("Column $0 is already bound to another value.",
-                                            attr_num);
-      }
     }
 
     bind_pb->mutable_condition()->set_op(QL_OP_IN);
-    bind_pb->mutable_condition()->add_operands()->set_column_id(col.id());
+    auto lhs_bind = bind_pb->mutable_condition()->add_operands();
+
+    RETURN_NOT_OK(lhs->PrepareForRead(this, lhs_bind));
 
     // There's no "list of expressions" field so we simulate it with an artificial nested OR
     // with repeated operands, one per bind expression.
@@ -712,7 +751,7 @@ Status PgDmlRead::MoveBoundKeyInOperator(PgColumn* col, const LWPgsqlConditionPB
   condition_expr_pb->mutable_condition()->set_op(QL_OP_IN);
 
   auto op1_pb = condition_expr_pb->mutable_condition()->add_operands();
-  op1_pb->set_column_id(col->id());
+  *op1_pb = *in_operator.operands().begin();
 
   auto op2_pb = condition_expr_pb->mutable_condition()->add_operands();
   auto it = in_operator.operands().begin();

src/postgres/src/test/regress/expected/yb_join_batching.out
@@ -11,6 +11,11 @@ ANALYZE p3;
 CREATE TABLE p4 (a int, b int, c varchar, primary key(a,b));
 INSERT INTO p4 SELECT i, i % 25, to_char(i, 'FM0000') FROM generate_series(0, 599) i WHERE i % 7 = 0;
 ANALYZE p4;
+CREATE TABLE p5 (a int, b int, c varchar, primary key(a asc,b asc));
+INSERT INTO p5 SELECT i / 10, i % 10, to_char(i, 'FM0000') FROM generate_series(0, 599) i;
+CREATE INDEX p5_hash ON p5((a,b) hash);
+CREATE INDEX p5_hash_asc ON p5(a hash, b asc);
+ANALYZE p5;
 -- We're testing nested loop join batching in this file
 SET enable_hashjoin = off;
 SET enable_mergejoin = off;
@@ -118,25 +123,188 @@ SELECT * FROM p1 t1 JOIN p2 t2 ON t1.a - 1 = t2.a + 1 WHERE t1.a <= 100 AND t2.a
  26 |  1 | 0026 | 24 | 24 | 0024
 (17 rows)
 
--- Disabling batching for compound clauses
+-- Batching on compound clauses
 /*+ Leading((p2 p1)) */ EXPLAIN (COSTS OFF) SELECT * FROM p1 JOIN p2 ON p1.a = p2.b AND p2.a = p1.b;
-                   QUERY PLAN
--------------------------------------------------
- Nested Loop
+                                        QUERY PLAN                                        
+------------------------------------------------------------------------------------------
+ YB Batched Nested Loop Join
+   Join Filter: ((p1.a = p2.b) AND (p1.b = p2.a))
    ->  Seq Scan on p2
    ->  Index Scan using p1_pkey on p1
-         Index Cond: ((a = p2.b) AND (b = p2.a))
-(4 rows)
+         Index Cond: (ROW(a, b) = ANY (ARRAY[ROW(p2.b, p2.a), ROW($1, $4), ROW($2, $5)]))
+(5 rows)
+
+/*+ Leading((p2 p1)) */ SELECT * FROM p1 JOIN p2 ON p1.a = p2.b AND p2.a = p1.b;
+ a  | b  |  c   | a  | b  |  c   
+----+----+------+----+----+------
+ 12 | 12 | 0012 | 12 | 12 | 0012
+  6 |  6 | 0006 |  6 |  6 | 0006
+ 18 | 18 | 0018 | 18 | 18 | 0018
+  0 |  0 | 0000 |  0 |  0 | 0000
+ 24 | 24 | 0024 | 24 | 24 | 0024
+(5 rows)
+
+explain (costs off) select * from p1 left join p5 on p1.a - 1 = p5.a and p1.b - 1 = p5.b where p1.a <= 30;
+                                                          QUERY PLAN                                                          
+------------------------------------------------------------------------------------------------------------------------------
+ YB Batched Nested Loop Left Join
+   Join Filter: (((p1.a - 1) = p5.a) AND ((p1.b - 1) = p5.b))
+   ->  Seq Scan on p1
+         Remote Filter: (a <= 30)
+   ->  Index Scan using p5_pkey on p5
+         Index Cond: (ROW(a, b) = ANY (ARRAY[ROW((p1.a - 1), (p1.b - 1)), ROW(($1 - 1), ($4 - 1)), ROW(($2 - 1), ($5 - 1))]))
+(6 rows)
+
+select * from p1 left join p5 on p1.a - 1 = p5.a and p1.b - 1 = p5.b where p1.a <= 30;
+ a  | b  |  c   | a  | b |  c   
+----+----+------+----+---+------
+  6 |  6 | 0006 |  5 | 5 | 0055
+ 12 | 12 | 0012 |    |   | 
+ 16 | 16 | 0016 |    |   | 
+  4 |  4 | 0004 |  3 | 3 | 0033
+ 10 | 10 | 0010 |  9 | 9 | 0099
+ 18 | 18 | 0018 |    |   | 
+  2 |  2 | 0002 |  1 | 1 | 0011
+ 30 |  5 | 0030 | 29 | 4 | 0294
+  0 |  0 | 0000 |    |   | 
+  8 |  8 | 0008 |  7 | 7 | 0077
+ 14 | 14 | 0014 |    |   | 
+ 22 | 22 | 0022 |    |   | 
+ 26 |  1 | 0026 | 25 | 0 | 0250
+ 28 |  3 | 0028 | 27 | 2 | 0272
+ 24 | 24 | 0024 |    |   | 
+ 20 | 20 | 0020 |    |   | 
+(16 rows)
+
+/*+IndexScan(p5 p5_hash)*/explain (costs off) select * from p1 left join p5 on p1.a - 1 = p5.a and p1.b - 1 = p5.b where p1.a <= 30;
+                                                          QUERY PLAN                                                          
+------------------------------------------------------------------------------------------------------------------------------
+ YB Batched Nested Loop Left Join
+   Join Filter: (((p1.a - 1) = p5.a) AND ((p1.b - 1) = p5.b))
+   ->  Seq Scan on p1
+         Remote Filter: (a <= 30)
+   ->  Index Scan using p5_hash on p5
+         Index Cond: (ROW(a, b) = ANY (ARRAY[ROW((p1.a - 1), (p1.b - 1)), ROW(($1 - 1), ($4 - 1)), ROW(($2 - 1), ($5 - 1))]))
+(6 rows)
+
+/*+IndexScan(p5 p5_hash)*/ select * from p1 left join p5 on p1.a - 1 = p5.a and p1.b - 1 = p5.b where p1.a <= 30;
+ a  | b  |  c   | a  | b |  c   
+----+----+------+----+---+------
+  6 |  6 | 0006 |  5 | 5 | 0055
+ 12 | 12 | 0012 |    |   | 
+ 16 | 16 | 0016 |    |   | 
+  4 |  4 | 0004 |  3 | 3 | 0033
+ 10 | 10 | 0010 |  9 | 9 | 0099
+ 18 | 18 | 0018 |    |   | 
+  2 |  2 | 0002 |  1 | 1 | 0011
+ 30 |  5 | 0030 | 29 | 4 | 0294
+  0 |  0 | 0000 |    |   | 
+  8 |  8 | 0008 |  7 | 7 | 0077
+ 14 | 14 | 0014 |    |   | 
+ 22 | 22 | 0022 |    |   | 
+ 26 |  1 | 0026 | 25 | 0 | 0250
+ 28 |  3 | 0028 | 27 | 2 | 0272
+ 24 | 24 | 0024 |    |   | 
+ 20 | 20 | 0020 |    |   | 
+(16 rows)
+
+/*+IndexScan(p5 p5_hash_asc)*/explain (costs off) select * from p1 left join p5 on p1.a - 1 = p5.a and p1.b - 1 = p5.b where p1.a <= 30;
+                                                          QUERY PLAN                                                          
+------------------------------------------------------------------------------------------------------------------------------
+ YB Batched Nested Loop Left Join
+   Join Filter: (((p1.a - 1) = p5.a) AND ((p1.b - 1) = p5.b))
+   ->  Seq Scan on p1
+         Remote Filter: (a <= 30)
+   ->  Index Scan using p5_hash_asc on p5
+         Index Cond: (ROW(a, b) = ANY (ARRAY[ROW((p1.a - 1), (p1.b - 1)), ROW(($1 - 1), ($4 - 1)), ROW(($2 - 1), ($5 - 1))]))
+(6 rows)
+
+/*+IndexScan(p5 p5_hash_asc)*/ select * from p1 left join p5 on p1.a - 1 = p5.a and p1.b - 1 = p5.b where p1.a <= 30;
+ a  | b  |  c   | a  | b |  c   
+----+----+------+----+---+------
+  6 |  6 | 0006 |  5 | 5 | 0055
+ 12 | 12 | 0012 |    |   | 
+ 16 | 16 | 0016 |    |   | 
+  4 |  4 | 0004 |  3 | 3 | 0033
+ 10 | 10 | 0010 |  9 | 9 | 0099
+ 18 | 18 | 0018 |    |   | 
+  2 |  2 | 0002 |  1 | 1 | 0011
+ 30 |  5 | 0030 | 29 | 4 | 0294
+  0 |  0 | 0000 |    |   | 
+  8 |  8 | 0008 |  7 | 7 | 0077
+ 14 | 14 | 0014 |    |   | 
+ 22 | 22 | 0022 |    |   | 
+ 26 |  1 | 0026 | 25 | 0 | 0250
+ 28 |  3 | 0028 | 27 | 2 | 0272
+ 24 | 24 | 0024 |    |   | 
+ 20 | 20 | 0020 |    |   | 
+(16 rows)
 
 /*+ Leading((p2 p1)) IndexScan(p1 p1_b_idx) */ EXPLAIN (COSTS OFF) SELECT * FROM p1 JOIN p2 ON p1.a = p2.b AND p2.a = p1.b;
-                   QUERY PLAN
--------------------------------------------------
- Nested Loop
+                                        QUERY PLAN                                        
+------------------------------------------------------------------------------------------
+ YB Batched Nested Loop Join
+   Join Filter: ((p1.a = p2.b) AND (p1.b = p2.a))
    ->  Index Scan using p1_b_idx on p1
    ->  Index Scan using p2_pkey on p2
-         Index Cond: ((a = p1.b) AND (b = p1.a))
-(4 rows)
+         Index Cond: (ROW(a, b) = ANY (ARRAY[ROW(p1.b, p1.a), ROW($1, $4), ROW($2, $5)]))
+(5 rows)
 
+CREATE TABLE t10 (r1 int, r2 int, r3 int, r4 int);
+INSERT INTO t10
+  SELECT DISTINCT
+    i1, i2+5, i3, i4
+  FROM generate_series(1, 5) i1,
+       generate_series(1, 5) i2,
+       generate_series(1, 5) i3,
+       generate_series(1, 10) i4;
+CREATE index i_t ON t10 (r1 ASC, r2 ASC, r3 ASC, r4 ASC);
+CREATE TABLE t11 (c1 int, c3 int, x int);
+INSERT INTO t11 VALUES (1,2,0), (1,3,0), (5,2,0), (5,3,0), (5,4,0);
+CREATE TABLE t12 (c4 int, c2 int, y int);
+INSERT INTO t12 VALUES (3,7,0),(6,9,0),(9,7,0),(4,9,0);
+EXPLAIN (COSTS OFF) /*+ Leading((t12 (t11 t10))) Set(enable_seqscan true) */ SELECT t10.* FROM t12, t11, t10 WHERE x = y AND c1 = r1 AND c2 = r2 AND c3 = r3 AND c4 = r4 order by c1, c2, c3, c4;
+                                                                    QUERY PLAN                                                                    
+--------------------------------------------------------------------------------------------------------------------------------------------------
+ Sort
+   Sort Key: t10.r1, t10.r2, t10.r3, t10.r4
+   ->  Nested Loop
+         ->  Seq Scan on t12
+         ->  YB Batched Nested Loop Join
+               Join Filter: ((t12.y = t11.x) AND (t11.c1 = t10.r1) AND (t11.c3 = t10.r3))
+               ->  Seq Scan on t11
+               ->  Index Only Scan using i_t on t10
+                     Index Cond: ((ROW(r1, r3) = ANY (ARRAY[ROW(t11.c1, t11.c3), ROW($1, $4), ROW($2, $5)])) AND (r2 = t12.c2) AND (r4 = t12.c4))
+(9 rows)
+
+/*+ Leading((t12 (t11 t10))) Set(enable_seqscan true) */ SELECT t10.* FROM t12, t11, t10 WHERE x = y AND c1 = r1 AND c2 = r2 AND c3 = r3 AND c4 = r4 order by c1, c2, c3, c4;
+ r1 | r2 | r3 | r4 
+----+----+----+----
+  1 |  7 |  2 |  3
+  1 |  7 |  2 |  9
+  1 |  7 |  3 |  3
+  1 |  7 |  3 |  9
+  1 |  9 |  2 |  4
+  1 |  9 |  2 |  6
+  1 |  9 |  3 |  4
+  1 |  9 |  3 |  6
+  5 |  7 |  2 |  3
+  5 |  7 |  2 |  9
+  5 |  7 |  3 |  3
+  5 |  7 |  3 |  9
+  5 |  7 |  4 |  3
+  5 |  7 |  4 |  9
+  5 |  9 |  2 |  4
+  5 |  9 |  2 |  6
+  5 |  9 |  3 |  4
+  5 |  9 |  3 |  6
+  5 |  9 |  4 |  4
+  5 |  9 |  4 |  6
+(20 rows)
+
+DROP TABLE t10;
+DROP TABLE t11;
+DROP TABLE t12;
 EXPLAIN (COSTS OFF) SELECT * FROM p3 t3 LEFT OUTER JOIN (SELECT t1.a as a FROM p1 t1 JOIN p2 t2 ON t1.a = t2.b WHERE t1.a <= 100 AND t2.a <= 100) s ON t3.a = s.a WHERE t3.a <= 30;
                         QUERY PLAN
 -----------------------------------------------------------
@@ -168,7 +336,7 @@ SELECT * FROM p3 t3 LEFT OUTER JOIN (SELECT t1.a as a FROM p1 t1 JOIN p2 t2 ON t
 (8 rows)
 
 EXPLAIN (COSTS OFF) SELECT * FROM p3 t3 RIGHT OUTER JOIN (SELECT t1.a as a FROM p1 t1 JOIN p2 t2 ON t1.a = t2.b WHERE t1.b <= 10 AND t2.b <= 15) s ON t3.a = s.a;
-                                QUERY PLAN
+                                QUERY PLAN                                 
 ---------------------------------------------------------------------------
  YB Batched Nested Loop Left Join
    Join Filter: (t3.a = t1.a)
@@ -177,7 +345,7 @@ EXPLAIN (COSTS OFF) SELECT * FROM p3 t3 RIGHT OUTER JOIN (SELECT t1.a as a FROM 
          ->  Index Scan using p2_pkey on p2 t2
                Index Cond: (b <= 15)
          ->  Index Scan using p1_pkey on p1 t1
-               Index Cond: ((a = ANY (ARRAY[t2.b, $1, $2])) AND (b <= 10))
+               Index Cond: ((b <= 10) AND (a = ANY (ARRAY[t2.b, $1, $2])))
    ->  Index Scan using p3_pkey on p3 t3
          Index Cond: (a = ANY (ARRAY[t1.a, $4, $5]))
 (10 rows)
@@ -429,6 +597,7 @@ DROP TABLE p1;
 DROP TABLE p2;
 DROP TABLE p3;
 DROP TABLE p4;
+DROP TABLE p5;
 CREATE TABLE s1(r1 int, r2 int, r3 int);
 CREATE TABLE s2(r1 int, r2 int, r3 int);
 CREATE TABLE s3(r1 int, r2 int);
@@ -500,9 +669,33 @@ on s1.a = s2.a left outer join s3 on s2.a = s3.a where s1.a > 20;
 drop table s1;
 drop table s2;
 drop table s3;
+SET yb_bnl_batch_size = 3;
+-- Testing column groups in HybridScanChoices
+create table test2 (a int, pp int, b int, pp2 int, c int, primary key(a asc, pp asc, b asc, pp2 asc, c asc));
+insert into test2 values (1,0, 2,0,1), (2,0, 3,0,3), (2,0,3,0,5);
+create table test1 (a int, pp int, b int, pp2 int, c int, primary key(a asc, pp asc, b asc, pp2 asc, c asc));
+insert into test1 values (1,0,2,0,1), (1,0,2,0,2), (2,0,3,0,3), (2,0,4,0,4), (2,0,4,0,5), (2,0,4,0,6);
+explain (costs off) select * from test1 p1 join test2 p2 on p1.a = p2.a AND p1.b = p2.b AND p1.c = p2.c;
+                                                QUERY PLAN                                                 
+-----------------------------------------------------------------------------------------------------------
+ YB Batched Nested Loop Join
+   Join Filter: ((p1.a = p2.a) AND (p1.b = p2.b) AND (p1.c = p2.c))
+   ->  Index Scan using test1_pkey on test1 p1
+   ->  Index Scan using test2_pkey on test2 p2
+         Index Cond: (ROW(a, b, c) = ANY (ARRAY[ROW(p1.a, p1.b, p1.c), ROW($1, $4, $7), ROW($2, $5, $8)]))
+(5 rows)
+
+select * from test1 p1 join test2 p2 on p1.a = p2.a AND p1.b = p2.b AND p1.c = p2.c;
+ a | pp | b | pp2 | c | a | pp | b | pp2 | c 
+---+----+---+-----+---+---+----+---+-----+---
+ 1 |  0 | 2 |   0 | 1 | 1 |  0 | 2 |   0 | 1
+ 2 |  0 | 3 |   0 | 3 | 2 |  0 | 3 |   0 | 3
+(2 rows)
+
+drop table test1;
+drop table test2;
 -- Test on unhashable join operations. These should use the tuplestore
 -- strategy.
-SET yb_bnl_batch_size = 3;
 CREATE TABLE m1 (a money, primary key(a asc));
 INSERT INTO m1 SELECT i*2 FROM generate_series(1, 2000) i;
 CREATE TABLE m2 (a money, primary key(a asc));

src/postgres/src/test/regress/sql/yb_join_batching.sql
@@ -15,6 +15,12 @@ CREATE TABLE p4 (a int, b int, c varchar, primary key(a,b));
 INSERT INTO p4 SELECT i, i % 25, to_char(i, 'FM0000') FROM generate_series(0, 599) i WHERE i % 7 = 0;
 ANALYZE p4;
 
+CREATE TABLE p5 (a int, b int, c varchar, primary key(a asc,b asc));
+INSERT INTO p5 SELECT i / 10, i % 10, to_char(i, 'FM0000') FROM generate_series(0, 599) i;
+CREATE INDEX p5_hash ON p5((a,b) hash);
+CREATE INDEX p5_hash_asc ON p5(a hash, b asc);
+ANALYZE p5;
+
 -- We're testing nested loop join batching in this file
 SET enable_hashjoin = off;
 SET enable_mergejoin = off;
@@ -32,10 +38,46 @@ SELECT * FROM p1 t1 JOIN p2 t2 ON t1.a = t2.a + 1 WHERE t1.a <= 100 AND t2.a <= 
 EXPLAIN (COSTS OFF) SELECT * FROM p1 t1 JOIN p2 t2 ON t1.a - 1 = t2.a + 1 WHERE t1.a <= 100 AND t2.a <= 100;
 SELECT * FROM p1 t1 JOIN p2 t2 ON t1.a - 1 = t2.a + 1 WHERE t1.a <= 100 AND t2.a <= 100;
 
--- Disabling batching for compound clauses
+-- Batching on compound clauses
 /*+ Leading((p2 p1)) */ EXPLAIN (COSTS OFF) SELECT * FROM p1 JOIN p2 ON p1.a = p2.b AND p2.a = p1.b;
+/*+ Leading((p2 p1)) */ SELECT * FROM p1 JOIN p2 ON p1.a = p2.b AND p2.a = p1.b;
+
+explain (costs off) select * from p1 left join p5 on p1.a - 1 = p5.a and p1.b - 1 = p5.b where p1.a <= 30;
+select * from p1 left join p5 on p1.a - 1 = p5.a and p1.b - 1 = p5.b where p1.a <= 30;
+
+/*+IndexScan(p5 p5_hash)*/explain (costs off) select * from p1 left join p5 on p1.a - 1 = p5.a and p1.b - 1 = p5.b where p1.a <= 30;
+/*+IndexScan(p5 p5_hash)*/ select * from p1 left join p5 on p1.a - 1 = p5.a and p1.b - 1 = p5.b where p1.a <= 30;
+
+/*+IndexScan(p5 p5_hash_asc)*/explain (costs off) select * from p1 left join p5 on p1.a - 1 = p5.a and p1.b - 1 = p5.b where p1.a <= 30;
+/*+IndexScan(p5 p5_hash_asc)*/ select * from p1 left join p5 on p1.a - 1 = p5.a and p1.b - 1 = p5.b where p1.a <= 30;
 
 /*+ Leading((p2 p1)) IndexScan(p1 p1_b_idx) */ EXPLAIN (COSTS OFF) SELECT * FROM p1 JOIN p2 ON p1.a = p2.b AND p2.a = p1.b;
+
+CREATE TABLE t10 (r1 int, r2 int, r3 int, r4 int);
+
+INSERT INTO t10
+  SELECT DISTINCT
+    i1, i2+5, i3, i4
+  FROM generate_series(1, 5) i1,
+       generate_series(1, 5) i2,
+       generate_series(1, 5) i3,
+       generate_series(1, 10) i4;
+
+CREATE index i_t ON t10 (r1 ASC, r2 ASC, r3 ASC, r4 ASC);
+
+CREATE TABLE t11 (c1 int, c3 int, x int);
+INSERT INTO t11 VALUES (1,2,0), (1,3,0), (5,2,0), (5,3,0), (5,4,0);
+
+CREATE TABLE t12 (c4 int, c2 int, y int);
+INSERT INTO t12 VALUES (3,7,0),(6,9,0),(9,7,0),(4,9,0);
+
+EXPLAIN (COSTS OFF) /*+ Leading((t12 (t11 t10))) Set(enable_seqscan true) */ SELECT t10.* FROM t12, t11, t10 WHERE x = y AND c1 = r1 AND c2 = r2 AND c3 = r3 AND c4 = r4 order by c1, c2, c3, c4;
+
+/*+ Leading((t12 (t11 t10))) Set(enable_seqscan true) */ SELECT t10.* FROM t12, t11, t10 WHERE x = y AND c1 = r1 AND c2 = r2 AND c3 = r3 AND c4 = r4 order by c1, c2, c3, c4;
+
+DROP TABLE t10;
+DROP TABLE t11;
+DROP TABLE t12;
 
 EXPLAIN (COSTS OFF) SELECT * FROM p3 t3 LEFT OUTER JOIN (SELECT t1.a as a FROM p1 t1 JOIN p2 t2 ON t1.a = t2.b WHERE t1.a <= 100 AND t2.a <= 100) s ON t3.a = s.a WHERE t3.a <= 30;
 SELECT * FROM p3 t3 LEFT OUTER JOIN (SELECT t1.a as a FROM p1 t1 JOIN p2 t2 ON t1.a = t2.b WHERE t1.a <= 100 AND t2.a <= 100) s ON t3.a = s.a WHERE t3.a <= 30;
@@ -78,6 +120,7 @@ DROP TABLE p1;
 DROP TABLE p2;
 DROP TABLE p3;
 DROP TABLE p4;
+DROP TABLE p5;
 
 CREATE TABLE s1(r1 int, r2 int, r3 int);
 CREATE TABLE s2(r1 int, r2 int, r3 int);
@@ -112,9 +155,20 @@ drop table s1;
 drop table s2;
 drop table s3;
 
+SET yb_bnl_batch_size = 3;
+
+-- Testing column groups in HybridScanChoices
+create table test2 (a int, pp int, b int, pp2 int, c int, primary key(a asc, pp asc, b asc, pp2 asc, c asc));
+insert into test2 values (1,0, 2,0,1), (2,0, 3,0,3), (2,0,3,0,5);
+create table test1 (a int, pp int, b int, pp2 int, c int, primary key(a asc, pp asc, b asc, pp2 asc, c asc));
+insert into test1 values (1,0,2,0,1), (1,0,2,0,2), (2,0,3,0,3), (2,0,4,0,4), (2,0,4,0,5), (2,0,4,0,6);
+explain (costs off) select * from test1 p1 join test2 p2 on p1.a = p2.a AND p1.b = p2.b AND p1.c = p2.c;
+select * from test1 p1 join test2 p2 on p1.a = p2.a AND p1.b = p2.b AND p1.c = p2.c;
+drop table test1;
+drop table test2;
+
 -- Test on unhashable join operations. These should use the tuplestore
 -- strategy.
-SET yb_bnl_batch_size = 3;
 CREATE TABLE m1 (a money, primary key(a asc));
 INSERT INTO m1 SELECT i*2 FROM generate_series(1, 2000) i;
 

commit 89b8c53d7bc739056ee0d5ce8ce6839d85a46d50

[#19753] YSQL: Fix tuple IN condition type-checking in the presence of binary columns
Summary:
This change fixes a simple bug that shows up when type-checking bound tuple IN conditions that involve binary columns such as UUID.

This bug was introduced in D20059/eb0951a6580d807ad3dc9063198e0a30c21de748 appearing in releases 2.17.1+

Backport needed to 2.17, 2.18, 2.19, 2.20
Jira: DB-8599

Test Plan: ./yb_build.sh --java-test 'org.yb.pgsql.TestPgRegressJoin'

Reviewers: mtakahara, amartsinchyk

Reviewed By: mtakahara, amartsinchyk

Subscribers: smishra, yql

Tags: #jenkins-ready

Differential Revision: https://phorge.dev.yugabyte.com/D30263

src/yb/yql/pggate/pg_dml_read.cc
@@ -500,13 +500,13 @@ Status PgDmlRead::BindColumnCondIn(PgExpr *lhs, int n_attr_values, PgExpr **attr
       auto vals = attr_values[i]->Unpack();
       auto curr_val_it = vals.begin();
       for (const PgColumn &curr_col : cols) {
-        const PgExpr &curr_val = *curr_val_it;
+        const PgExpr &curr_val = *curr_val_it++;
+
         auto col_type = curr_val.internal_type();
         if (curr_col.internal_type() == InternalType::kBinaryValue)
             continue;
         SCHECK_EQ(curr_col.internal_type(), col_type, Corruption,
           "Attribute value type does not match column type");
-        curr_val_it++;
       }
     }
   }

src/postgres/src/test/regress/expected/yb_join_batching.out
@@ -368,6 +368,41 @@ drop table d1;
 drop table d2;
 drop table d3;
 drop table d4;
+create table test (
+    id   uuid NOT NULL,
+    num  int4 NOT NULL,
+    PRIMARY KEY ((id)HASH, num DESC)
+);
+insert into test(id, num) VALUES
+('774cee8f-f0e9-4c46-8f3d-3b5e7db8b839'::uuid, 1);
+/*+ Set(yb_bnl_batch_size 3) */ explain (costs off)
+select * from test t1
+join test t2 on (t1.id = t2.id and t1.num = t2.num)
+where t1.id in (
+	'774cee8f-f0e9-4c46-8f3d-3b5e7db8b839'::uuid,
+	'884cee8f-f0e9-4c46-8f3d-3b5e7db8b847'::uuid);
+                                                       QUERY PLAN
+------------------------------------------------------------------------------------------------------------------------
+ YB Batched Nested Loop Join
+   Join Filter: ((t1.id = t2.id) AND (t1.num = t2.num))
+   ->  Index Scan using test_pkey on test t1
+         Index Cond: (id = ANY ('{774cee8f-f0e9-4c46-8f3d-3b5e7db8b839,884cee8f-f0e9-4c46-8f3d-3b5e7db8b847}'::uuid[]))
+   ->  Index Scan using test_pkey on test t2
+         Index Cond: (ROW(id, num) = ANY (ARRAY[ROW(t1.id, t1.num), ROW($1, $4), ROW($2, $5)]))
+(6 rows)
+
+/*+ Set(yb_bnl_batch_size 3) */
+select * from test t1
+join test t2 on (t1.id = t2.id and t1.num = t2.num)
+where t1.id in (
+	'774cee8f-f0e9-4c46-8f3d-3b5e7db8b839'::uuid,
+	'884cee8f-f0e9-4c46-8f3d-3b5e7db8b847'::uuid);
+                  id                  | num |                  id                  | num
+--------------------------------------+-----+--------------------------------------+-----
+ 774cee8f-f0e9-4c46-8f3d-3b5e7db8b839 |   1 | 774cee8f-f0e9-4c46-8f3d-3b5e7db8b839 |   1
+(1 row)
+
+drop table test;
 EXPLAIN (COSTS OFF) SELECT * FROM p3 t3 LEFT OUTER JOIN (SELECT t1.a as a FROM p1 t1 JOIN p2 t2 ON t1.a = t2.b WHERE t1.a <= 100 AND t2.a <= 100) s ON t3.a = s.a WHERE t3.a <= 30;
                         QUERY PLAN
 -----------------------------------------------------------

src/postgres/src/test/regress/sql/yb_join_batching.sql
@@ -100,6 +100,30 @@ drop table d2;
 drop table d3;
 drop table d4;
 
+create table test (
+    id   uuid NOT NULL,
+    num  int4 NOT NULL,
+    PRIMARY KEY ((id)HASH, num DESC)
+);
+insert into test(id, num) VALUES
+('774cee8f-f0e9-4c46-8f3d-3b5e7db8b839'::uuid, 1);
+
+/*+ Set(yb_bnl_batch_size 3) */ explain (costs off)
+select * from test t1
+join test t2 on (t1.id = t2.id and t1.num = t2.num)
+where t1.id in (
+	'774cee8f-f0e9-4c46-8f3d-3b5e7db8b839'::uuid,
+	'884cee8f-f0e9-4c46-8f3d-3b5e7db8b847'::uuid);
+
+/*+ Set(yb_bnl_batch_size 3) */
+select * from test t1
+join test t2 on (t1.id = t2.id and t1.num = t2.num)
+where t1.id in (
+	'774cee8f-f0e9-4c46-8f3d-3b5e7db8b839'::uuid,
+	'884cee8f-f0e9-4c46-8f3d-3b5e7db8b847'::uuid);
+
+drop table test;
+
 EXPLAIN (COSTS OFF) SELECT * FROM p3 t3 LEFT OUTER JOIN (SELECT t1.a as a FROM p1 t1 JOIN p2 t2 ON t1.a = t2.b WHERE t1.a <= 100 AND t2.a <= 100) s ON t3.a = s.a WHERE t3.a <= 30;
 SELECT * FROM p3 t3 LEFT OUTER JOIN (SELECT t1.a as a FROM p1 t1 JOIN p2 t2 ON t1.a = t2.b WHERE t1.a <= 100 AND t2.a <= 100) s ON t3.a = s.a WHERE t3.a <= 30;
 