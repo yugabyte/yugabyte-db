5fccd696ad3799eac69fd22b8b3f292480934cfc
kkannan <kkannan@yugabyte.com>
2023-11-21 15:03:16 +0530

    [PLAT-11508][RBAC|UI] Disable LDAP button not disabled for read only user - [PLAT-11510][RBAC|UI] Edit Role is allowed for LDAP user - [PLAT-11511][RBAC|UI] Users tab should have column name as Username/Email
    Summary: Fixed rbac ui bugs
    
    Test Plan: Tested manually
    
    Reviewers: asathyan, lsangappa
    
    Reviewed By: lsangappa
    
    Subscribers: yugaware
    
    Differential Revision: https://phorge.dev.yugabyte.com/D30393

--- managed/ui/src/redesign/features/rbac/users/interface/Users.ts
+++ managed/ui/src/redesign/features/rbac/users/interface/Users.ts
@@ -10,12 +10,18 @@
 import { UniverseResource } from '../../policy/IPolicy';
 import { Role } from '../../roles';
 
+export enum UserTypes {
+  LOCAL = 'local',
+  LDAP = 'ldap'
+};
+
 export interface RbacUser {
   uuid: string;
   email: string;
   password?: string;
   confirmPassword?: string;
   creationDate?: string;
+  userType?: UserTypes;
 }
 
 export interface RbacUserWithResources extends Partial<RbacUser> {
