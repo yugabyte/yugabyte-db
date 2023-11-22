417e2dee0c36700365d7f9a226fbe9b41c56ca82
Minghui Yang <myang@yugabyte.com>
2023-11-22 00:49:26 +0000

    [#19975] YSQL: Fix assertion failure in per-database catalog version
    Summary:
    To reproduce the bug:
    
    1. Create a cluster and convert it into per-database catalog version mode
    ```
    yugabyte=# select * from pg_yb_catalog_version;
     db_oid | current_version | last_breaking_version
    --------+-----------------+-----------------------
          1 |               1 |                     1
      13242 |               1 |                     1
      13243 |               1 |                     1
      13245 |               1 |                     1
      13246 |               1 |                     1
    (5 rows)
    ```
    The above query output indicates that the cluster is running in per-database
    catalog version mode.
    
    2. Run the following DDL global-impact DDL statement
    ```
    yugabyte=# ALTER ROLE yugabyte SUPERUSER;
    server closed the connection unexpectedly
            This probably means the server terminated abnormally
            before or while processing the request.
    
    ```
    
    In the PG log:
    
    ```
    F1122 01:04:40.396021 15991 pg_txn_manager.cc:346] Check failed: !IsDdlMode() READ COMMITTED semantics don't apply to DDL transactions
    ```
    
    This happens when we call `yb_increment_all_db_catalog_versions` to increment
    catalog versions for all databases. This is done inside function
    `YBDecrementDdlNestingLevel` which first does
    ```
        --ddl_transaction_state.nesting_level;
    ```
    and then if `ddl_transaction_state.nesting_level == 0` it calls
    `yb_increment_all_db_catalog_versions` if the current DDL statement has global
    impact. In `GetTransactionSnapshot` we have
    ```
           if (IsYBReadCommitted() && YBGetDdlNestingLevel() == 0)
    ```
    The function `yb_increment_all_db_catalog_versions` is still called as part of
    the DDL statement, but `ddl_transaction_state.nesting_level` is already 0 so
    `YBGetDdlNestingLevel()` returns 0. As a result the above `if` condition is true
    and the DDL statement is treated as a non-DDL statement, eventually leading to
    the assertion failure.
    
    The DDL mode is already transferred to pggate when we call
    `YBCPgEnterSeparateDdlTxnMode`. So I added `YBCPgIsDdlMode()` and uses it to
    replace `YBGetDdlNestingLevel() == 0`.
    
    I reverted some work arounds in `pg_catalog_version-test.cc` which used snapshot
    isolation because otherwise those unit tests would fail under read committed
    isolation.
    
    Test Plan: ./yb_build.sh debug --cxx-test pg_catalog_version-test
    
    Reviewers: tvesely, pjain
    
    Reviewed By: pjain
    
    Subscribers: yql
    
    Differential Revision: https://phorge.dev.yugabyte.com/D30417

--- src/yb/yql/pgwrapper/pg_catalog_version-test.cc
+++ src/yb/yql/pgwrapper/pg_catalog_version-test.cc
@@ -299,10 +299,7 @@ class PgCatalogVersionTest : public LibPqTestBase {
     constexpr auto* kTestGroup = "test_group";
     constexpr auto* kTestTablespace = "test_tsp";
     // Test setup.
-    // TODO (#19975): Enable read committed isolation
-    auto conn_yugabyte = ASSERT_RESULT(
-        SetDefaultTransactionIsolation(ConnectToDB(kYugabyteDatabase),
-        IsolationLevel::SNAPSHOT_ISOLATION));
+    auto conn_yugabyte = ASSERT_RESULT(ConnectToDB(kYugabyteDatabase));
     ASSERT_OK(PrepareDBCatalogVersion(&conn_yugabyte));
     if (disable_global_ddl) {
       RestartClusterWithDBCatalogVersionMode(
@@ -312,10 +309,7 @@ class PgCatalogVersionTest : public LibPqTestBase {
     }
     LOG(INFO) << "Connects to database " << kYugabyteDatabase << " on node at index 0.";
     pg_ts = cluster_->tablet_server(0);
-    // TODO (#19975): Enable read committed isolation
-    conn_yugabyte = ASSERT_RESULT(EnableCacheEventLog(
-        SetDefaultTransactionIsolation(ConnectToDB(kYugabyteDatabase),
-        IsolationLevel::SNAPSHOT_ISOLATION)));
+    conn_yugabyte = ASSERT_RESULT(EnableCacheEventLog(ConnectToDB(kYugabyteDatabase)));
     LOG(INFO) << "Create a new database";
     ASSERT_OK(conn_yugabyte.ExecuteFormat("CREATE DATABASE $0", kTestDatabase));
     LOG(INFO) << "Create two new test users";
@@ -403,9 +397,7 @@ class PgCatalogVersionTest : public LibPqTestBase {
 
     LOG(INFO) << "Connects to database template1 on node at index 0.";
     pg_ts = cluster_->tablet_server(0);
-    // TODO (#19975): Enable read committed isolation
-    auto conn_template1 = ASSERT_RESULT(SetDefaultTransactionIsolation(
-        ConnectToDB("template1"), IsolationLevel::SNAPSHOT_ISOLATION));
+    auto conn_template1 = ASSERT_RESULT(ConnectToDB("template1"));
 
     // The following ALTER is a global DDL that writes to shared relations
     // pg_authid and pg_auth_members so it should cause catalog cache refresh
@@ -706,9 +698,7 @@ TEST_F(PgCatalogVersionTest, IncrementAllDBCatalogVersions) {
 // with pg_database according to 'per_database_mode' argument.
 TEST_F(PgCatalogVersionTest, FixCatalogVersionTable) {
   RestartClusterWithDBCatalogVersionMode();
-  // TODO (#19975): Enable read committed isolation
-  auto conn_template1 = ASSERT_RESULT(
-      SetDefaultTransactionIsolation(ConnectToDB("template1"), IsolationLevel::SNAPSHOT_ISOLATION));
+  auto conn_template1 = ASSERT_RESULT(ConnectToDB("template1"));
   // Prepare the table pg_yb_catalog_version for per-db catalog version mode.
   ASSERT_OK(PrepareDBCatalogVersion(&conn_template1, true /* per_database_mode */));
   // Verify pg_database and pg_yb_catalog_version are in sync.
@@ -739,9 +729,7 @@ TEST_F(PgCatalogVersionTest, FixCatalogVersionTable) {
   WaitForCatalogVersionToPropagate();
 
   // Connect to database "yugabyte".
-  // TODO (#19975): Enable read committed isolation
-  auto conn_yugabyte = ASSERT_RESULT(
-      SetDefaultTransactionIsolation(ConnectToDB("yugabyte"), IsolationLevel::SNAPSHOT_ISOLATION));
+  auto conn_yugabyte = ASSERT_RESULT(ConnectToDB("yugabyte"));
   // Prepare the table pg_yb_catalog_version for global catalog version mode.
   // Note that this is not a supported scenario where the table pg_yb_catalog_version
   // shrinks while the gflag --ysql_enable_db_catalog_version_mode is still on.
@@ -790,16 +778,12 @@ TEST_F(PgCatalogVersionTest, FixCatalogVersionTable) {
   // true, the fact that the table pg_yb_catalog_version has only one row prevents
   // a new connection to enter per-database catalog version mode. Verify that we
   // can make a new connection to database "yugabyte".
-  // TODO (#19975): Enable read committed isolation
-  ASSERT_RESULT(
-      SetDefaultTransactionIsolation(ConnectToDB("yugabyte"), IsolationLevel::SNAPSHOT_ISOLATION));
+  ASSERT_RESULT(ConnectToDB("yugabyte"));
 
   // We can also make a new connection to database "template1" but the fact that
   // now it is the only database that has a row in pg_yb_catalog_version table is
   // not relevant.
-  // TODO (#19975): Enable read committed isolation
-  conn_template1 = ASSERT_RESULT(
-      SetDefaultTransactionIsolation(ConnectToDB("template1"), IsolationLevel::SNAPSHOT_ISOLATION));
+  conn_template1 = ASSERT_RESULT(ConnectToDB("template1"));
 
   // Sync up pg_yb_catalog_version with pg_database.
   ASSERT_OK(PrepareDBCatalogVersion(&conn_template1, true /* per_database_mode */));
@@ -807,11 +791,8 @@ TEST_F(PgCatalogVersionTest, FixCatalogVersionTable) {
   ASSERT_TRUE(ASSERT_RESULT(
       VerifyCatalogVersionTableDbOids(&conn_template1, false /* single_row */)));
   // Verify that we can connect to "yugabyte" and "template1".
-  // TODO (#19975): Enable read committed isolation
-  ASSERT_RESULT(
-      SetDefaultTransactionIsolation(ConnectToDB("yugabyte"), IsolationLevel::SNAPSHOT_ISOLATION));
-  ASSERT_RESULT(
-      SetDefaultTransactionIsolation(ConnectToDB("template1"), IsolationLevel::SNAPSHOT_ISOLATION));
+  ASSERT_RESULT(ConnectToDB("yugabyte"));
+  ASSERT_RESULT(ConnectToDB("template1"));
 }
 
 // This test exercises the wrap around logic in tserver shared memory free
