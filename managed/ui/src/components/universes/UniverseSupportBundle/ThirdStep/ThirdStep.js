c0cf1d384836cebfce02e6deef6334b1e745f046
Rajagopalan Madhavan <rmadhavan@yugabyte.com>
2023-11-27 16:37:29 -0500

    [BACKPORT 2.18][PLAT-11604]: When using custom date picker in create support bundle, the start and end dates get switched.
    Summary:
    When using custom date picker in create support bundle, the start and end dates get switched.
    
    Original Commit: D30507/4bb33b7
    
    Test Plan:
    Please refer to screenshot before fix
    {F130450}
    
    Please refer to screenshot after fix
    {F130449}
    
    Reviewers: jmak
    
    Reviewed By: jmak
    
    Subscribers: yugaware
    
    Tags: #jenkins-ready
    
    Differential Revision: https://phorge.dev.yugabyte.com/D30551

--- managed/ui/src/components/universes/UniverseSupportBundle/ThirdStep/ThirdStep.js
+++ managed/ui/src/components/universes/UniverseSupportBundle/ThirdStep/ThirdStep.js
@@ -1,25 +1,46 @@
-import React, {useEffect, useState} from "react";
-import clsx from "clsx";
-import {withRouter} from "react-router";
-import {BootstrapTable, TableHeaderColumn} from "react-bootstrap-table";
-import {DropdownButton, MenuItem} from "react-bootstrap";
+import React, { useEffect, useState } from 'react';
+import clsx from 'clsx';
+import { withRouter } from 'react-router';
+import { BootstrapTable, TableHeaderColumn } from 'react-bootstrap-table';
+import { DropdownButton, MenuItem } from 'react-bootstrap';
 
-import {YBMenuItem} from "../../UniverseDetail/compounds/YBMenuItem";
-import {YBButton, YBModal} from "../../../common/forms/fields";
+import { YBMenuItem } from '../../UniverseDetail/compounds/YBMenuItem';
+import { YBButton, YBModal } from '../../../common/forms/fields';
 
-import { ybFormatDate, YBTimeFormats } from "../../../../redesign/helpers/DateUtils";
-import {formatBytes} from "../../../xcluster/ReplicationUtils";
+import { ybFormatDate, YBTimeFormats } from '../../../../redesign/helpers/DateUtils';
+import { formatBytes } from '../../../xcluster/ReplicationUtils';
 
-import "./ThirdStep.scss";
-
+import './ThirdStep.scss';
 
 const statusElementsIcons = {
-  'Success': <span className="status success">Ready <i className="fa fa-check"/></span>,
-  'Failed': <span className="status failed">Creation Failed <i className="fa fa-spinner fa-spin"/></span>,
-  'Running': <span className="status creating">Creating <i className="fa fa-spinner fa-spin"/></span>
+  Success: (
+    <span className="status success">
+      Ready <i className="fa fa-check" />
+    </span>
+  ),
+  Failed: (
+    <span className="status failed">
+      Creation Failed <i className="fa fa-exclamation-circle" />
+    </span>
+  ),
+  Running: (
+    <span className="status creating">
+      Creating <i className="fa fa-spinner fa-spin" />
+    </span>
+  )
 };
 
-const getActions = (uuid, row, handleViewLogs, handleDeleteBundle, isConfirmDeleteOpen, setIsConfirmDeleteOpen, handleDownloadBundle, creatingBundle, setDeleteBundleObj) => {
+const getActions = (
+  uuid,
+  row,
+  handleViewLogs,
+  handleDeleteBundle,
+  isConfirmDeleteOpen,
+  setIsConfirmDeleteOpen,
+  handleDownloadBundle,
+  creatingBundle,
+  setDeleteBundleObj
+) => {
   const isReady = row.status === 'Success';
   return (
     <>
@@ -29,7 +50,9 @@ const getActions = (uuid, row, handleViewLogs, handleDeleteBundle, isConfirmDele
         noCaret
         drop={'start'}
         title={
-          <span className="dropdown-text"><i className="fa fa-ellipsis-h"/></span>
+          <span className="dropdown-text">
+            <i className="fa fa-ellipsis-h" />
+          </span>
         }
         pullRight
         className="support-action-dropdown"
@@ -41,7 +64,7 @@ const getActions = (uuid, row, handleViewLogs, handleDeleteBundle, isConfirmDele
               handleDownloadBundle(row.bundleUUID);
             }}
           >
-            <i className="fa fa-download"/> Download
+            <i className="fa fa-download" /> Download
           </MenuItem>
         )}
         {!isReady && (
@@ -51,7 +74,7 @@ const getActions = (uuid, row, handleViewLogs, handleDeleteBundle, isConfirmDele
               handleViewLogs('/logs');
             }}
           >
-            <i className="fa fa-file"/> View logs
+            <i className="fa fa-file" /> View logs
           </MenuItem>
         )}
         <YBMenuItem
@@ -62,14 +85,14 @@ const getActions = (uuid, row, handleViewLogs, handleDeleteBundle, isConfirmDele
             setDeleteBundleObj(row);
           }}
         >
-          <i className="fa fa-trash"/> Delete
+          <i className="fa fa-trash" /> Delete
         </YBMenuItem>
       </DropdownButton>
     </>
   );
 };
 
-const ConfirmDeleteModal = ({createdOn, closeModal, confirmDelete}) => {
+const ConfirmDeleteModal = ({ createdOn, closeModal, confirmDelete }) => {
   return (
     <YBModal
       visible
@@ -83,125 +106,141 @@ const ConfirmDeleteModal = ({createdOn, closeModal, confirmDelete}) => {
       onFormSubmit={confirmDelete}
       className="support-bundle-confirm-delete"
     >
-      You are about to delete the support bundle that was created on <span className="created-on-date">{createdOn}</span>. This can not be undone
+      You are about to delete the support bundle that was created on{' '}
+      <span className="created-on-date">{createdOn}</span>. This can not be undone
     </YBModal>
   );
 };
 
-export const ThirdStep = withRouter(({
-  onCreateSupportBundle,
-  handleDeleteBundle,
-  handleDownloadBundle,
-  supportBundles,
-  router
-}) => {
+export const ThirdStep = withRouter(
+  ({ onCreateSupportBundle, handleDeleteBundle, handleDownloadBundle, supportBundles, router }) => {
+    const [creatingBundle, setCreatingBundle] = useState(
+      supportBundles &&
+        Array.isArray(supportBundles) &&
+        supportBundles.find((supportBundle) => supportBundle.status === 'Running') !== undefined
+    );
+    const [isConfirmDeleteOpen, setIsConfirmDeleteOpen] = useState(false);
+    const [deleteBundleObj, setDeleteBundleObj] = useState({});
 
-  const [creatingBundle, setCreatingBundle] = useState(supportBundles && Array.isArray(supportBundles) && supportBundles.find((supportBundle) => supportBundle.status === 'Running') !== undefined);
-  const [isConfirmDeleteOpen, setIsConfirmDeleteOpen] = useState(false);
-  const [deleteBundleObj, setDeleteBundleObj] = useState({});
+    useEffect(() => {
+      if (
+        supportBundles &&
+        Array.isArray(supportBundles) &&
+        supportBundles.find((supportBundle) => supportBundle.status === 'Running') !== undefined
+      ) {
+        setCreatingBundle(true);
+      } else {
+        setCreatingBundle(false);
+      }
+    }, [supportBundles, setCreatingBundle]);
 
-  useEffect(() => {
-    if(supportBundles && Array.isArray(supportBundles) && supportBundles.find((supportBundle) => supportBundle.status === 'Running') !== undefined) {
-      setCreatingBundle(true);
-    } else {
-      setCreatingBundle(false);
-    }
-  }, [supportBundles, setCreatingBundle]);
-
-  return (
-    <>
-      {isConfirmDeleteOpen && (
-        <ConfirmDeleteModal
-          closeModal={() => {
-            setIsConfirmDeleteOpen(false);
-          }}
-          createdOn={ybFormatDate(deleteBundleObj.creationDate)}
-          confirmDelete={() => {
-            handleDeleteBundle(deleteBundleObj.bundleUUID);
-            setDeleteBundleObj({});
-            setIsConfirmDeleteOpen(false);
-          }}
-        />
-      )}
-      <div className="universe-support-bundle-step-three">
-        {
-          creatingBundle && (
+    return (
+      <>
+        {isConfirmDeleteOpen && (
+          <ConfirmDeleteModal
+            closeModal={() => {
+              setIsConfirmDeleteOpen(false);
+            }}
+            createdOn={ybFormatDate(deleteBundleObj.creationDate)}
+            confirmDelete={() => {
+              handleDeleteBundle(deleteBundleObj.bundleUUID);
+              setDeleteBundleObj({});
+              setIsConfirmDeleteOpen(false);
+            }}
+          />
+        )}
+        <div className="universe-support-bundle-step-three">
+          {creatingBundle && (
             <div className="creating-bundle">
               <span>
-                <i className="fa icon fa-spinner fa-spin"/>
-              Creating bundle. Depending on the size of the bundle this may take a few minutes.
+                <i className="fa icon fa-spinner fa-spin" />
+                Creating bundle. Depending on the size of the bundle this may take a few minutes.
               </span>
-              <i onClick={() => setCreatingBundle(false)} className="fa fa-close"/>
+              <i onClick={() => setCreatingBundle(false)} className="fa fa-close" />
             </div>
-          )
-        }
-
-        <div className="create-bundle">
-          <YBButton
-            variant="outline-dark"
-            onClick={onCreateSupportBundle}
-            btnText={(
-              <>
-                <i className="fa fa-plus create-bundle-icon" aria-hidden="true"/>
-              Create Support Bundle
-              </>
-            )}
-          />
-        </div>
-        <div className={clsx('selection-area', {'create-bundle-close': !creatingBundle})}>
-          {supportBundles && Array.isArray(supportBundles) && (
-            <BootstrapTable data={supportBundles}>
-              <TableHeaderColumn
-                dataField="creationDate"
-                dataFormat={(creationDate) => ybFormatDate(creationDate, YBTimeFormats.YB_DATE_ONLY_TIMESTAMP)}
-                isKey={true}
-                className={'node-name-field'}
-                columnClassName={'node-name-field'}
-              >
-              Date Created
-              </TableHeaderColumn>
-              <TableHeaderColumn
-                dataField="expirationDate"
-                dataFormat={(expirationDate) => ybFormatDate(expirationDate, YBTimeFormats.YB_DATE_ONLY_TIMESTAMP)}
-                className={'yb-node-status-cell'}
-                columnClassName={'yb-node-status-cell'}
-              >
-              Expiration Date
-              </TableHeaderColumn>
-              <TableHeaderColumn
-                dataField="status"
-                dataFormat={(status) => {
-                  return statusElementsIcons[status];
-                }}
-              >
-              Status
-              </TableHeaderColumn>
-
-              <TableHeaderColumn
-                dataField="sizeInBytes"
-                dataFormat={(sizeInBytes) => {
-                  if(sizeInBytes === 0) {
-                    return "-";
-                  }
-
-                  return formatBytes(sizeInBytes);
-                }}
-                className={'node-name-field'}
-                columnClassName={'node-name-field'}
-              >
-              Size
-              </TableHeaderColumn>
-
-              <TableHeaderColumn
-                dataField="bundleUUID"
-                dataFormat={(bundleUUID, row) => {
-                  return getActions(bundleUUID, row, router.push, handleDeleteBundle, isConfirmDeleteOpen, setIsConfirmDeleteOpen, handleDownloadBundle, row.status === 'Running', setDeleteBundleObj);
-                }}
-              />
-            </BootstrapTable>
           )}
+
+          <div className="create-bundle">
+            <YBButton
+              variant="outline-dark"
+              onClick={onCreateSupportBundle}
+              btnText={
+                <>
+                  <i className="fa fa-plus create-bundle-icon" aria-hidden="true" />
+                  Create Support Bundle
+                </>
+              }
+            />
+          </div>
+          <div className={clsx('selection-area', { 'create-bundle-close': !creatingBundle })}>
+            {supportBundles && Array.isArray(supportBundles) && (
+              <BootstrapTable data={supportBundles}>
+                <TableHeaderColumn
+                  dataField="creationDate"
+                  dataFormat={(creationDate) =>
+                    ybFormatDate(creationDate, YBTimeFormats.YB_DATE_ONLY_TIMESTAMP)
+                  }
+                  isKey={true}
+                  className={'node-name-field'}
+                  columnClassName={'node-name-field'}
+                >
+                  Date Created
+                </TableHeaderColumn>
+                <TableHeaderColumn
+                  dataField="expirationDate"
+                  dataFormat={(expirationDate) =>
+                    ybFormatDate(expirationDate, YBTimeFormats.YB_DATE_ONLY_TIMESTAMP)
+                  }
+                  className={'yb-node-status-cell'}
+                  columnClassName={'yb-node-status-cell'}
+                >
+                  Expiration Date
+                </TableHeaderColumn>
+                <TableHeaderColumn
+                  dataField="status"
+                  dataFormat={(status) => {
+                    return statusElementsIcons[status];
+                  }}
+                >
+                  Status
+                </TableHeaderColumn>
+
+                <TableHeaderColumn
+                  dataField="sizeInBytes"
+                  dataFormat={(sizeInBytes) => {
+                    if (sizeInBytes === 0) {
+                      return '-';
+                    }
+
+                    return formatBytes(sizeInBytes);
+                  }}
+                  className={'node-name-field'}
+                  columnClassName={'node-name-field'}
+                >
+                  Size
+                </TableHeaderColumn>
+
+                <TableHeaderColumn
+                  dataField="bundleUUID"
+                  dataFormat={(bundleUUID, row) => {
+                    return getActions(
+                      bundleUUID,
+                      row,
+                      router.push,
+                      handleDeleteBundle,
+                      isConfirmDeleteOpen,
+                      setIsConfirmDeleteOpen,
+                      handleDownloadBundle,
+                      row.status === 'Running',
+                      setDeleteBundleObj
+                    );
+                  }}
+                />
+              </BootstrapTable>
+            )}
+          </div>
         </div>
-      </div>
-    </>
-  );
-});
+      </>
+    );
+  }
+);
