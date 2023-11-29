ba046dc241888272d5fb41e267f259e57d0f5419
cdavid <catalindavid2003@gmail.com>
2023-11-29 14:00:02 -0800

    [PLAT-11621] Set up the initial backend for the Troubleshooting Service
    Summary: Create the folder for the Troubleshooting Service
    
    Test Plan: n/a
    
    Reviewers: amalyshev, sanketh, rmadhavan, nikhil
    
    Reviewed By: amalyshev, rmadhavan
    
    Differential Revision: https://phorge.dev.yugabyte.com/D30587

---
+++ troubleshoot/backend/src/main/java/com/yugabyte/troubleshoot/ts/HelloController.java
@@ -0,0 +1,13 @@
+package com.yugabyte.troubleshoot.ts;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+  @GetMapping("/")
+  public String index() {
+    return "Greetings from Spring Boot!";
+  }
+}
