346cd3426ab4226a3809e37ca34955d094129a2d
cdavid <catalindavid2003@gmail.com>
2023-11-29 14:00:02 -0800

    [PLAT-11621] Set up the initial backend for the Troubleshooting Service
    Summary: Create the folder for the Troubleshooting Service
    
    Test Plan: n/a
    
    Reviewers: amalyshev, sanketh, rmadhavan, nikhil
    
    Reviewed By: amalyshev, rmadhavan
    
    Differential Revision: https://phorge.dev.yugabyte.com/D30587

---
+++ troubleshoot/backend/src/test/java/com/yugabyte/troubleshoot/ts/TsApplicationTests.java
@@ -0,0 +1,11 @@
+package com.yugabyte.troubleshoot.ts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TsApplicationTests {
+
+  @Test
+  void contextLoads() {}
+}
