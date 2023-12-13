250c16cba28d7d9bd10434897087b90d52fdb1ca
Jason Kim <git@jasonk.me>
2023-12-13 15:58:43 -0800

    [pg15] test: port regress for 2da896182ce1
    Summary:
    Port regress test changes as in upstream PG commit
    2da896182ce11240774af6c4d769777f90a09536 titled
    
        Rename create_function_N test scripts for clarity.
    
    Adjust YB schedules and pg15_tests accordingly.
    
    Test Plan:
    On Almalinux 8:
    
        #!/usr/bin/env bash
        ./yb_build.sh fastdebug --gcc11
        pg15_tests/test_D29226.sh fastdebug --gcc11 --sj --scb --sp
        pg15_tests/test_D29490.sh fastdebug --gcc11 --sj --scb --sp
    
    Reviewers: aagrawal, tfoucher
    
    Reviewed By: tfoucher
    
    Subscribers: yql
    
    Differential Revision: https://phorge.dev.yugabyte.com/D31049

--- pg15_tests/test_D29226.sh
+++ pg15_tests/test_D29226.sh
@@ -6,12 +6,10 @@ pushd src/postgres/src/test/regress
 diff <(for f in yb_*_schedule; do ./yb_lint_regress_schedule.sh "$f" || echo "$f"; done) - <<EOT
 test: yb_pg_rolenames
 yb_pg_auth_serial_schedule
-test: yb_pg_create_function_3
 test: yb_pg_identity
 yb_pg_misc_independent_serial_schedule
 test: yb_pg_create_type
 test: yb_pg_create_table
-test: yb_pg_create_function_0
 yb_pg_misc_serial_schedule
 EOT
 popd
