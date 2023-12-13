32705bbadb6f1a1898b7cf60f069a525e48e5e58
Arpan Agrawal <arpan3495@gmail.com>
2023-12-13 12:35:42 +0530

    [pg15] test: Fix test_D29662.sh
    Summary: Fix test_D29662.sh
    
    Test Plan: Jenkins: skip
    
    Reviewers: jason, tfoucher
    
    Differential Revision: https://phorge.dev.yugabyte.com/D31000

--- pg15_tests/test_D29662.sh
+++ pg15_tests/test_D29662.sh
@@ -1,10 +1,9 @@
 #!/usr/bin/env bash
 source "${BASH_SOURCE[0]%/*}"/common.sh
 
-# With D30923, pg_select should pass.
 java_test 'TestPgRegressMisc#testPgRegressMiscSerial' false
 grep_in_java_test \
-  'failed tests: [yb_create_language, yb_depend, yb_guc, yb_query_consistent_snapshot]' \
+  'failed tests: [yb_create_language, yb_depend, yb_guc, yb_query_consistent_snapshot, yb_select]' \
   'TestPgRegressMisc#testPgRegressMiscSerial'
 # yb_get_range_split_clause should pass.
 java_test 'TestPgRegressMisc#testPgRegressMiscSerial2'
