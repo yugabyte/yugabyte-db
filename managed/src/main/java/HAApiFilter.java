2c1d47f824fb29472d79a41fcd432dcd7945e932
Muthu Chidambaram <mchidambaram@yugabyte.com>
2023-12-13 18:14:22 +0000

    [BACKPORT 2.20][PLAT-11617][PLAT-11256] Disabling API access for HA follower
    Summary:
    We need to disable access to the API on HA followers to prevent accidental universe creation or modification to resources for a YBA that is a follower. All updates should only take place on the leader and then propagated to followers via backups. This diff blocks any non GET requests, except for a few special APIs that are still necessary for follower functionality.
    Original commit: facd8b53873237d424c9b17da5d650ff0402dc80 / D30758
    
    Test Plan: Run YBA in both leader/standby mode and ensure different levels of API access.
    
    Reviewers: svarshney, anijhawan, sneelakantan, sanketh
    
    Reviewed By: svarshney
    
    Subscribers: yugaware
    
    Tags: #jenkins-ready
    
    Differential Revision: https://phorge.dev.yugabyte.com/D31002

---
+++ managed/src/main/java/HAApiFilter.java
@@ -0,0 +1,56 @@
+import static play.mvc.Http.Status.INTERNAL_SERVER_ERROR;
+
+import akka.stream.Materializer;
+import com.google.inject.Inject;
+import com.google.inject.Singleton;
+import com.yugabyte.yw.common.PlatformServiceException;
+import com.yugabyte.yw.controllers.TokenAuthenticator;
+import com.yugabyte.yw.models.HighAvailabilityConfig;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.CompletionStage;
+import java.util.function.Function;
+import java.util.regex.Pattern;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import play.mvc.Filter;
+import play.mvc.Http;
+import play.mvc.Result;
+import play.mvc.Results;
+
+@Singleton
+public class HAApiFilter extends Filter {
+
+  public static final Logger LOG = LoggerFactory.getLogger(HAApiFilter.class);
+  private final String HA_ENDPOINT_REGEX = "/api/.*(/?)settings/ha/.*";
+  private final Pattern HA_ENDPOINT_PATTERN = Pattern.compile(HA_ENDPOINT_REGEX);
+
+  @Inject
+  public HAApiFilter(Materializer mat) {
+    super(mat);
+  }
+
+  @Override
+  public CompletionStage<Result> apply(
+      Function<Http.RequestHeader, CompletionStage<Result>> next, Http.RequestHeader rh) {
+    try {
+      if (HighAvailabilityConfig.isFollower()) {
+        // Only allow read access for HA follower
+        if (!rh.method().equals("GET")) {
+          // Also allow any HA, login, or read only specific APIs to succeed
+          if (!HA_ENDPOINT_PATTERN.matcher(rh.path()).matches()
+              && !rh.path().startsWith("/api/v1/login")
+              && TokenAuthenticator.READ_POST_ENDPOINTS.stream()
+                  .noneMatch(path -> rh.path().endsWith(path))) {
+            Result result = Results.status(503, "API not available for follower YBA");
+            return CompletableFuture.completedFuture(result);
+          }
+        }
+      }
+      return next.apply(rh);
+    } catch (Exception e) {
+      LOG.error("Error retrieving HA config", e);
+
+      throw new PlatformServiceException(INTERNAL_SERVER_ERROR, "Error retrieving HA config");
+    }
+  }
+}
