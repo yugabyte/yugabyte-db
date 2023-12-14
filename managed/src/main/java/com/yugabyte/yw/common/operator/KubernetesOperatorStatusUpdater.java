48adb9dc06d98949e2f17cfa7636b99da4719db4
Kumar Vivek <kv83821@gmail.com>
2023-12-14 06:51:19 +0000

    [PLAT-10495] Add additional columns in ybuniverse get resource K8s operator
    Summary:
    - Added additional columns: Universe state, DB version for `ybuniverse` resource when printing `kubectl get` response
    
    ```
    vk-mbp-kmd6m:~ kv83821$ kubectl get ybuniverse -n yb-vkumar
    NAME           STATE      SOFTWARE VERSION
    yb-op-univ-3   Creating   2.21.0.0-b99924221
    
    vk-mbp-kmd6m:charts kv83821$ kubectl get ybuniverse -n yb-vkumar
    NAME           STATE   SOFTWARE VERSION
    yb-op-univ-3   Ready   2.21.0.0-b99924221
    
    vk-mbp-kmd6m:~ kv83821$ kubectl get ybuniverse -n yb-vkumar
    NAME           STATE   SOFTWARE VERSION
    yb-op-univ-3   Error   2.21.0.0-b99924221
    ```
    - The universe state is almost equivalent to what we have in the YBA UI: [Creating, Editing, Error, Ready, Deleting, Paused]
    - Fixed minor bug where universe `onUpdate` was not checking null for `deleteTimestamp`.
    
    Test Plan: Manually tested fields getting populated
    
    Reviewers: anijhawan, dshubin
    
    Reviewed By: anijhawan, dshubin
    
    Subscribers: yugaware
    
    Differential Revision: https://phorge.dev.yugabyte.com/D31062

--- managed/src/main/java/com/yugabyte/yw/common/operator/KubernetesOperatorStatusUpdater.java
+++ managed/src/main/java/com/yugabyte/yw/common/operator/KubernetesOperatorStatusUpdater.java
@@ -77,13 +77,17 @@ public class KubernetesOperatorStatusUpdater implements OperatorStatusUpdater {
    */
   @Override
   public void createYBUniverseEventStatus(
-      Universe universe, KubernetesResourceDetails universeName, String taskName, UUID taskUUID) {
+      Universe universe,
+      KubernetesResourceDetails universeName,
+      String taskName,
+      UUID taskUUID,
+      UniverseState universeState) {
     if (universe.getUniverseDetails().isKubernetesOperatorControlled) {
       try {
         String eventStr =
             String.format(
                 "Starting task %s (%s) on universe %s", taskName, taskUUID, universe.getName());
-        this.updateUniverseStatus(universe, universeName, eventStr);
+        this.updateUniverseStatus(universe, universeName, eventStr, universeState);
       } catch (Exception e) {
         log.warn("Error in creating Kubernetes Operator Universe status", e);
       }
@@ -182,6 +186,7 @@ public class KubernetesOperatorStatusUpdater implements OperatorStatusUpdater {
       KubernetesResourceDetails universeName,
       String taskName,
       UUID taskUUID,
+      UniverseState state,
       Throwable t) {
     if (universe.getUniverseDetails().isKubernetesOperatorControlled) {
       try {
@@ -191,7 +196,7 @@ public class KubernetesOperatorStatusUpdater implements OperatorStatusUpdater {
         String statusStr =
             String.format(
                 "Task %s (%s) on universe %s %s", taskName, taskUUID, universe.getName(), status);
-        updateUniverseStatus(universe, universeName, statusStr);
+        updateUniverseStatus(universe, universeName, statusStr, state);
       } catch (Exception e) {
         log.warn("Error in creating Kubernetes Operator Universe status", e);
       }
@@ -199,7 +204,7 @@ public class KubernetesOperatorStatusUpdater implements OperatorStatusUpdater {
   }
 
   private void updateUniverseStatus(
-      Universe u, KubernetesResourceDetails universeName, String status) {
+      Universe u, KubernetesResourceDetails universeName, String status, UniverseState state) {
     try (final KubernetesClient kubernetesClient =
         new KubernetesClientBuilder().withConfig(k8sClientConfig).build()) {
       YBUniverse ybUniverse = getYBUniverse(kubernetesClient, universeName);
@@ -213,6 +218,8 @@ public class KubernetesOperatorStatusUpdater implements OperatorStatusUpdater {
       YBUniverseStatus ybUniverseStatus = new YBUniverseStatus();
       ybUniverseStatus.setCqlEndpoints(cqlEndpoints);
       ybUniverseStatus.setSqlEndpoints(sqlEndpoints);
+      ybUniverseStatus.setUniverseStatus(status);
+      ybUniverseStatus.setUniverseState(state.getUniverseStateString());
       log.info("Universe status is: {}", status);
       ybUniverse.setStatus(ybUniverseStatus);
       kubernetesClient
