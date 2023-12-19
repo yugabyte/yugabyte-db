yugabyte/yugabyte-db

commit d2a700a3a35edc3a0e74a5ee72d36e9cac7319c1

[#17087, #18054] YSQL: Exposes YB Transaction IDs in Postgres.
Summary:
This change exposes the transaction IDs present in the tserver via two endpoints:

  - yb_get_current_transaction() - a PG function that displays information about the transaction that the invoking backend is currently executing.
  - pg_stat_activity - Adds a new yb-specific column to the view, to display information about active transactions across all backends/connections running in the node.

In particular, the latter is achieved through the addition of a new `session_id` field to the PG shared memory context.
The session ID is mapped to a PgBackend and remains constant throughout the life of the backend. This enables such use cases in the future, where a where a PG backend queries the tserver for information about a specific backend.
In particular, mapping between PG Backends and their corresponding transactions is achieved through a transitive mapping between PgBackend --> pg_client session --> transaction.
This change introduces a new RPC `GetActiveTransactionsList` for this purpose.

To protect against scenarios where tserver upgrades may lag behind PG upgrades, the use of these RPCs is protected by the `yb_enable_pg_locks` auto flag.

Sample output of pg_stat_activity:
Scenario: Two active backends, one of which is running a transaction.

```
yugabyte=# SELECT * FROM pg_stat_activity;
datid | datname  |  pid  | usesysid | usename  | application_name | client_addr | client_hostname | client_port |         backend_start         |          xact_start           |          query_start          |         state_change          | wait_event_type |    wait_event    |        state        | backend_xid | backend_xmin |              query              |  backend_type  | catalog_version | allocated_mem_bytes | rss_mem_bytes |            yb_backend_xid

src/postgres/src/backend/utils/adt/pgstatfuncs.c
@@ -32,7 +32,10 @@
 
 /* YB includes */
 #include "commands/progress.h"
+#include "inttypes.h"
 #include "pg_yb_utils.h"
+#include "utils/uuid.h"
+#include "yb/yql/pggate/ybc_pg_typedefs.h"
 
 #define UINT32_ACCESS_ONCE(var)		 ((uint32)(*((volatile uint32 *)&(var))))
 
@@ -633,15 +636,19 @@ pg_stat_get_progress_info(PG_FUNCTION_ARGS)
 Datum
 pg_stat_get_activity(PG_FUNCTION_ARGS)
 {
-#define PG_STAT_GET_ACTIVITY_COLS	24
+#define PG_STAT_GET_ACTIVITY_COLS 24
+/* YB specific fields in pg_stat_activity */
+#define YB_PG_STAT_GET_ACTIVITY_COLS 1
+#define YB_BACKEND_XID_COL			 24
 	int			num_backends = pgstat_fetch_stat_numbackends();
 	int			curr_backend;
-	int			pid = PG_ARGISNULL(0) ? -1 : PG_GETARG_INT32(0);
+	int pid = PG_ARGISNULL(0) ? -1 : PG_GETARG_INT32(0);
 	ReturnSetInfo *rsinfo = (ReturnSetInfo *) fcinfo->resultinfo;
 	TupleDesc	tupdesc;
 	Tuplestorestate *tupstore;
 	MemoryContext per_query_ctx;
 	MemoryContext oldcontext;
+	TimestampTz txn_rpc_timestamp;
 
 	/* check to see if caller supports us returning a tuplestore */
 	if (rsinfo == NULL || !IsA(rsinfo, ReturnSetInfo))
@@ -668,12 +675,32 @@ pg_stat_get_activity(PG_FUNCTION_ARGS)
 
 	MemoryContextSwitchTo(oldcontext);
 
+	YBCPgSessionTxnInfo *txn_infos = NULL;
+	if (YBIsEnabledInPostgresEnvVar() && yb_enable_pg_locks)
+	{
+		txn_infos = (YBCPgSessionTxnInfo *) palloc0(
+			sizeof(YBCPgSessionTxnInfo) * num_backends);
+		/* 1-based index */
+		for (curr_backend = 1; curr_backend <= num_backends; ++curr_backend)
+		{
+			const PgBackendStatus *beentry =
+				pgstat_fetch_stat_beentry(curr_backend);
+			if (!beentry)
+				break;
+			txn_infos[curr_backend - 1].session_id = beentry->yb_session_id;
+		}
+		txn_rpc_timestamp = GetCurrentTimestamp();
+		HandleYBStatus(YBCPgActiveTransactions(txn_infos, num_backends));
+	}
+
 	/* 1-based index */
 	for (curr_backend = 1; curr_backend <= num_backends; curr_backend++)
 	{
 		/* for each row */
-		Datum		values[PG_STAT_GET_ACTIVITY_COLS];
-		bool		nulls[PG_STAT_GET_ACTIVITY_COLS];
+		Datum 		values[PG_STAT_GET_ACTIVITY_COLS +
+			YB_PG_STAT_GET_ACTIVITY_COLS];
+		bool 		nulls[PG_STAT_GET_ACTIVITY_COLS +
+			YB_PG_STAT_GET_ACTIVITY_COLS];
 		LocalPgBackendStatus *local_beentry;
 		PgBackendStatus *beentry;
 		PGPROC	   *proc;
@@ -945,6 +972,26 @@ pg_stat_get_activity(PG_FUNCTION_ARGS)
 			nulls[13] = true;
 			nulls[14] = true;
 			nulls[17] = true;
+		}
+
+		nulls[YB_BACKEND_XID_COL] = true;
+		/* The activity_start_timestamp is updated at the start of every
+		 * query. When the query start timestamp is later (greater) than the
+		 * timestamp of the RPC above, this indicates that the data in the
+		 * RPC is out-of-date. In such cases, we skip printing the
+		 * transaction ID.  */
+
+		if (yb_enable_pg_locks && beentry->yb_session_id &&
+			beentry->st_activity_start_timestamp <= txn_rpc_timestamp)
+		{
+			Assert(txn_infos);
+			const YBCPgSessionTxnInfo *info = txn_infos + curr_backend - 1;
+
+			if (info->is_not_null)
+			{
+				values[YB_BACKEND_XID_COL] = UUIDPGetDatum(&info->txn_id);
+				nulls[YB_BACKEND_XID_COL] = false;
+			}
 		}
 
 		tuplestore_putvalues(tupstore, tupdesc, values, nulls);

src/postgres/src/backend/catalog/yb_system_views.sql
@@ -751,12 +751,13 @@ CREATE VIEW pg_stat_activity AS
             S.wait_event,
             S.state,
             S.backend_xid,
-            s.backend_xmin,
+            S.backend_xmin,
             S.query,
             S.backend_type,
             yb_pg_stat_get_backend_catalog_version(B.beid) AS catalog_version,
             yb_pg_stat_get_backend_allocated_mem_bytes(B.beid) AS allocated_mem_bytes,
-            yb_pg_stat_get_backend_rss_mem_bytes(B.beid) AS rss_mem_bytes
+            yb_pg_stat_get_backend_rss_mem_bytes(B.beid) AS rss_mem_bytes,
+            S.yb_backend_xid
     FROM pg_stat_get_activity(NULL) AS S
         LEFT JOIN pg_database AS D ON (S.datid = D.oid)
         LEFT JOIN pg_authid AS U ON (S.usesysid = U.oid)

src/postgres/src/backend/postmaster/pgstat.c
@@ -3162,6 +3162,7 @@ yb_pgstat_clear_entry_pid(int pid)
 		{
 			PGSTAT_BEGIN_WRITE_ACTIVITY(beentry);
 			beentry->st_procpid = 0;	/* mark invalid */
+			beentry->yb_session_id = 0;
 			PGSTAT_END_WRITE_ACTIVITY(beentry);
 			return;
 		}
@@ -3200,6 +3201,7 @@ pgstat_beshutdown_hook(int code, Datum arg)
 	PGSTAT_BEGIN_WRITE_ACTIVITY(beentry);
 
 	beentry->st_procpid = 0;	/* mark invalid */
+	beentry->yb_session_id = 0;
 
 	PGSTAT_END_WRITE_ACTIVITY(beentry);
 }
@@ -7018,6 +7020,30 @@ yb_pgstat_set_has_catalog_version(bool has_version)
 	PGSTAT_BEGIN_WRITE_ACTIVITY(vbeentry);
 
 	vbeentry->yb_st_catalog_version.has_version = has_version;
+
+	PGSTAT_END_WRITE_ACTIVITY(vbeentry);
+}
+
+void
+yb_pgstat_add_session_info(uint64_t session_id)
+{
+	volatile PgBackendStatus *vbeentry = NULL;
+
+	/* This code could be invoked either in a regular backend or in an
+	 * auxiliary process. In case of the latter, skip initializing shared
+	 * memory context. See note in pgstat_initalize() */
+	if (MyBEEntry == NULL)
+	{
+		/* Must be an auxiliary process */
+		Assert(MyAuxProcType != NotAnAuxProcess);
+		return;
+	}
+
+	vbeentry = MyBEEntry;
+
+	PGSTAT_BEGIN_WRITE_ACTIVITY(vbeentry);
+
+	vbeentry->yb_session_id = session_id;
 
 	PGSTAT_END_WRITE_ACTIVITY(vbeentry);
 }

src/postgres/src/backend/utils/misc/pg_yb_utils.c
@@ -671,6 +671,14 @@ YBInitPostgresBackend(
 		HandleYBStatus(YBCPgInitSession(db_name ? db_name : user_name,
 										&yb_session_stats.current_state));
 		YBCSetTimeout(StatementTimeout, NULL);
+
+		/*
+		 * Upon completion of the first heartbeat to the local tserver, retrieve
+		 * and store the session ID in shared memory, so that entities
+		 * associated with a session ID (like txn IDs) can be transitively
+		 * mapped to PG backends.
+		 */
+		yb_pgstat_add_session_info(YBCPgGetSessionID());
 	}
 }
 
@@ -2678,6 +2686,30 @@ Datum
 yb_get_effective_transaction_isolation_level(PG_FUNCTION_ARGS)
 {
 	PG_RETURN_CSTRING(yb_fetch_effective_transaction_isolation_level());
+}
+
+Datum
+yb_get_current_transaction(PG_FUNCTION_ARGS)
+{
+	pg_uuid_t *txn_id = NULL;
+	bool is_null = false;
+
+	if (!yb_enable_pg_locks)
+	{
+		ereport(ERROR, (errcode(ERRCODE_INVALID_PARAMETER_VALUE),
+						errmsg("get_current_transaction is unavailable"),
+						errdetail("yb_enable_pg_locks is false or a system "
+								  "upgrade is in progress")));
+	}
+
+	txn_id = (pg_uuid_t *) palloc(sizeof(pg_uuid_t));
+	HandleYBStatus(
+		YBCPgGetSelfActiveTransaction((YBCPgUuid *) txn_id, &is_null));
+
+	if (is_null)
+		PG_RETURN_NULL();
+
+	PG_RETURN_UUID_P(txn_id);
 }
 
 Datum

src/postgres/src/include/catalog/catalog.h
@@ -29,7 +29,7 @@
  * If you increment it, make sure you didn't forget to add a new SQL migration
  * (see pg_yb_migration.dat and src/yb/yql/pgwrapper/ysql_migrations/README.md)
  */
-#define YB_LAST_USED_OID 8063
+#define YB_LAST_USED_OID 8064
 
 extern bool IsSystemRelation(Relation relation);
 extern bool IsToastRelation(Relation relation);

src/postgres/src/include/catalog/pg_proc.dat
@@ -5178,9 +5178,9 @@
   proname => 'pg_stat_get_activity', prorows => '100', proisstrict => 'f',
   proretset => 't', provolatile => 's', proparallel => 'r',
   prorettype => 'record', proargtypes => 'int4',
-  proallargtypes => '{int4,oid,int4,oid,text,text,text,text,text,timestamptz,timestamptz,timestamptz,timestamptz,inet,text,int4,xid,xid,text,bool,text,text,int4,bool,text}',
-  proargmodes => '{i,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o}',
-  proargnames => '{pid,datid,pid,usesysid,application_name,state,query,wait_event_type,wait_event,xact_start,query_start,backend_start,state_change,client_addr,client_hostname,client_port,backend_xid,backend_xmin,backend_type,ssl,sslversion,sslcipher,sslbits,sslcompression,sslclientdn}',
+  proallargtypes => '{int4,oid,int4,oid,text,text,text,text,text,timestamptz,timestamptz,timestamptz,timestamptz,inet,text,int4,xid,xid,text,bool,text,text,int4,bool,text,uuid}',
+  proargmodes => '{i,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o}',
+  proargnames => '{pid,datid,pid,usesysid,application_name,state,query,wait_event_type,wait_event,xact_start,query_start,backend_start,state_change,client_addr,client_hostname,client_port,backend_xid,backend_xmin,backend_type,ssl,sslversion,sslcipher,sslbits,sslcompression,sslclientdn,yb_backend_xid}',
   prosrc => 'pg_stat_get_activity' },
 { oid => '8041',
   descr => 'statistics: terminated queries for the given database',
@@ -10464,5 +10464,11 @@
   proname => 'yb_cancel_transaction', provolatile => 'v', prorettype => 'bool',
   proargtypes => 'uuid', proargnames => '{txid}',
   prosrc => 'yb_cancel_transaction' },
+
+{ oid => '8064',
+  descr => 'Get the UUID of the current active distributed transaction. If no distributed transaction is active, return NULL',
+  proname => 'yb_get_current_transaction', provolatile => 'v',
+  prorettype => 'uuid', proargtypes => '',
+  prosrc => 'yb_get_current_transaction' },
 
 ]

src/postgres/src/include/catalog/pg_yb_migration.dat
@@ -12,7 +12,7 @@
 [
 
 # For better version control conflict detection, list latest migration filename
-# here: V42__16918__pg_locks_view.sql
-{ major => '42', minor => '0', name => '<baseline>', time_applied => '_null_' }
+# here: V43__17087__yb_backend_xid_in_pg_stat_activity.sql
+{ major => '43', minor => '0', name => '<baseline>', time_applied => '_null_' }
 
 ]

src/postgres/src/include/pg_yb_utils.h
@@ -794,7 +794,7 @@ uint32_t YbGetNumberOfDatabases();
  */
 void YBSetRowLockPolicy(int *docdb_wait_policy, LockWaitPolicy pg_wait_policy);
 
-const char* yb_fetch_current_transaction_priority(void);
+const char *yb_fetch_current_transaction_priority(void);
 
 void GetStatusMsgAndArgumentsByCode(
 	const uint32_t pg_err_code, uint16_t txn_err_code, YBCStatus s,

commit bd98c94d2621056cc4a284ab8e219c6429a8d612

[#20281] YSQL: Fix formatting in pg_stat_get_activity
Summary:
This revision fixes errors in formatting introduced by commit d2a700a3a35edc3a0e74a5ee72d36e9cac7319c1.
Jira: DB-9241

Test Plan:
Run the following test:
```
./yb_build.sh --cxx-test pgwrapper_pg_stat_activity-test --gtest_filter PgStatActivityTest.AllBackendsTransaction
```

Reviewers: jason

Reviewed By: jason

Subscribers: yql

Tags: #jenkins-ready

Differential Revision: https://phorge.dev.yugabyte.com/D30967

src/postgres/src/backend/utils/adt/pgstatfuncs.c
@@ -636,19 +636,19 @@ pg_stat_get_progress_info(PG_FUNCTION_ARGS)
 Datum
 pg_stat_get_activity(PG_FUNCTION_ARGS)
 {
-#define PG_STAT_GET_ACTIVITY_COLS 24
+#define PG_STAT_GET_ACTIVITY_COLS	24
 /* YB specific fields in pg_stat_activity */
 #define YB_PG_STAT_GET_ACTIVITY_COLS 1
-#define YB_BACKEND_XID_COL			 24
+#define YB_BACKEND_XID_COL			PG_STAT_GET_ACTIVITY_COLS
 	int			num_backends = pgstat_fetch_stat_numbackends();
 	int			curr_backend;
-	int pid = PG_ARGISNULL(0) ? -1 : PG_GETARG_INT32(0);
+	int			pid = PG_ARGISNULL(0) ? -1 : PG_GETARG_INT32(0);
 	ReturnSetInfo *rsinfo = (ReturnSetInfo *) fcinfo->resultinfo;
 	TupleDesc	tupdesc;
 	Tuplestorestate *tupstore;
 	MemoryContext per_query_ctx;
 	MemoryContext oldcontext;
-	TimestampTz txn_rpc_timestamp;
+	TimestampTz yb_txn_rpc_timestamp;
 
 	/* check to see if caller supports us returning a tuplestore */
 	if (rsinfo == NULL || !IsA(rsinfo, ReturnSetInfo))
@@ -689,7 +689,7 @@ pg_stat_get_activity(PG_FUNCTION_ARGS)
 				break;
 			txn_infos[curr_backend - 1].session_id = beentry->yb_session_id;
 		}
-		txn_rpc_timestamp = GetCurrentTimestamp();
+		yb_txn_rpc_timestamp = GetCurrentTimestamp();
 		HandleYBStatus(YBCPgActiveTransactions(txn_infos, num_backends));
 	}
 
@@ -697,10 +697,10 @@ pg_stat_get_activity(PG_FUNCTION_ARGS)
 	for (curr_backend = 1; curr_backend <= num_backends; curr_backend++)
 	{
 		/* for each row */
-		Datum 		values[PG_STAT_GET_ACTIVITY_COLS +
-			YB_PG_STAT_GET_ACTIVITY_COLS];
-		bool 		nulls[PG_STAT_GET_ACTIVITY_COLS +
-			YB_PG_STAT_GET_ACTIVITY_COLS];
+		Datum		values[PG_STAT_GET_ACTIVITY_COLS +
+						   YB_PG_STAT_GET_ACTIVITY_COLS];
+		bool		nulls[PG_STAT_GET_ACTIVITY_COLS +
+						  YB_PG_STAT_GET_ACTIVITY_COLS];
 		LocalPgBackendStatus *local_beentry;
 		PgBackendStatus *beentry;
 		PGPROC	   *proc;
@@ -982,7 +982,7 @@ pg_stat_get_activity(PG_FUNCTION_ARGS)
 		 * transaction ID.  */
 
 		if (yb_enable_pg_locks && beentry->yb_session_id &&
-			beentry->st_activity_start_timestamp <= txn_rpc_timestamp)
+			beentry->st_activity_start_timestamp <= yb_txn_rpc_timestamp)
 		{
 			Assert(txn_infos);
 			const YBCPgSessionTxnInfo *info = txn_infos + curr_backend - 1;