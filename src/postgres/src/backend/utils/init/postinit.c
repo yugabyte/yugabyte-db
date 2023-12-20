yugabyte/yugabyte-db

commit de8c183828258b99c8b4601f20bf889043b9d3b0

[#19135] YSQL, ASH: Generate ASH Metadata, store it in PGPROC and pack it with Perform RPCs
Summary:
This diff adds Active Session History (ASH) metadata to the outgoing `Perform` RPCs from `pggate` to the local tserver so that the wait states in the tserver layer have enough context. The following fields make up the ASH metadata -

- `root_request_id` (16 bytes): A unique id corresponding to a YSQL query.
- `yql_endpoint_tserver_uuid` (16 bytes): UUID of the node where the YSQL query originated in bytes.
- `rpc_request_id` (8 bytes): A single YSQL query can generate multiple RPCs. Each RPC can be differentiated with this rpc_request_id. This is not globally unique. This is generated for incoming requests. For PG, it's the same as the last 8 bytes of `root_request_id`.
- `query_id` (8 bytes): Query id as seen on pg_stat_statements to identify identical normalized queries. There might be many queries with different root_request_id but with the same query_id.
- `client_addr` (16 bytes): IP address of the client which sent the query to PG.
- `client_port` (2 bytes): Port of the client which sent the query to PG.
- `addr_family` (1 byte): Stores the address family of the socket.

`client_addr` and `client_port` is null if `addr_family` is not `AF_INET` or `AF_INET6`.

The ASH metadata is stored in `PGPROC` because the ASH collector should be able to read this data. Another reason is that `wait_event_info` is also stored inside `PGPROC` which will be used by ASH to determine the wait event.

For utility statements, query id is always set to zero and needs special handling, this will be addressed in a future diff. The ASH metadata instrumentation is going to be polished in future diffs.

This diff also updates the `yb_enable_ash` GFlag from a preview flag to a test flag.

Additionally, there are alternative methods distinct from the `yb_enable_ash` GFlag for enabling or disabling ASH, which is not intended. More details on this issue (#20180).

src/postgres/src/backend/utils/init/postinit.c
@@ -78,12 +78,15 @@
 #include "utils/timeout.h"
 #include "utils/tqual.h"
 
+#include <arpa/inet.h>
 #include "pg_yb_utils.h"
 #include "catalog/pg_yb_catalog_version.h"
 #include "catalog/pg_yb_profile.h"
 #include "catalog/pg_yb_role_profile.h"
 #include "catalog/pg_yb_tablegroup.h"
 #include "catalog/yb_catalog_version.h"
+#include "common/ip.h"
+#include "utils/builtins.h"
 #include "utils/yb_inheritscache.h"
 
 static HeapTuple GetDatabaseTuple(const char *dbname);
@@ -98,6 +101,8 @@ static void IdleInTransactionSessionTimeoutHandler(void);
 static bool ThereIsAtLeastOneRole(void);
 static void process_startup_options(Port *port, bool am_superuser);
 static void process_settings(Oid databaseid, Oid roleid);
+
+static void YbSetAshClientAddrAndPort();
 
 /*** InitPostgres support ***/
 
@@ -693,6 +698,13 @@ InitPostgresImpl(const char *in_dbname, Oid dboid, const char *username,
 		YBInitPostgresBackend("postgres", "", username, session_id);
 	else
 		YBInitPostgresBackend("postgres", in_dbname, username, session_id);
+
+	/*
+	 * Set client_addr and client_host which will remain constant
+	 * throughout the session.
+	 */
+	if (IsYugaByteEnabled() && YBEnableAsh())
+		YbSetAshClientAddrAndPort();
 
 	if (IsYugaByteEnabled() && !bootstrap)
 	{
@@ -1370,4 +1382,68 @@ ThereIsAtLeastOneRole(void)
 	heap_close(pg_authid_rel, AccessShareLock);
 
 	return result;
+}
+
+/*
+ * Sets the client address and port for ASH metadata.
+ * If the address family is not AF_INET or AF_INET6, then the PGPPROC ASH metadata
+ * fields for client address and port don't mean anything. Otherwise, if
+ * pg_getnameinfo_all returns non-zero value, a warning is printed with the error
+ * code and ASH keeps working without client address and port for the current PG
+ * backend.
+ */
+static void
+YbSetAshClientAddrAndPort()
+{
+	Assert(MyProcPort != NULL);
+
+	LWLockAcquire(&MyProc->yb_ash_metadata_lock, LW_EXCLUSIVE);
+
+	/* Set the address family and null the client_addr and client_port */
+	MyProc->yb_ash_metadata.addr_family = MyProcPort->raddr.addr.ss_family;
+	MemSet(MyProc->yb_ash_metadata.client_addr, 0, 16);
+	MyProc->yb_ash_metadata.client_port = 0;
+
+	switch (MyProcPort->raddr.addr.ss_family)
+	{
+		case AF_INET:
+#ifdef HAVE_IPV6
+		case AF_INET6:
+#endif
+			break;
+		default:
+			LWLockRelease(&MyProc->yb_ash_metadata_lock);
+			return;
+	}
+
+	char		remote_host[NI_MAXHOST];
+	int			ret;
+
+	ret = pg_getnameinfo_all(&MyProcPort->raddr.addr, MyProcPort->raddr.salen,
+							 remote_host, sizeof(remote_host),
+							 NULL, 0,
+							 NI_NUMERICHOST | NI_NUMERICSERV);
+
+	if (ret != 0)
+	{
+		ereport(WARNING,
+				(errmsg("pg_getnameinfo_all while setting ash metadata failed"),
+				 errdetail("%s\naddress family: %u",
+						   gai_strerror(ret),
+						   MyProcPort->raddr.addr.ss_family)));
+
+		LWLockRelease(&MyProc->yb_ash_metadata_lock);
+		return;
+	}
+
+	clean_ipv6_addr(MyProcPort->raddr.addr.ss_family, remote_host);
+
+	/* Setting ip address */
+	inet_pton(MyProcPort->raddr.addr.ss_family, remote_host,
+			  MyProc->yb_ash_metadata.client_addr);
+
+	/* Setting port */
+	MyProc->yb_ash_metadata.client_port = atoi(MyProcPort->remote_port);
+
+	LWLockRelease(&MyProc->yb_ash_metadata_lock);
 }

src/postgres/contrib/yb_ash/Makefile
@@ -6,6 +6,9 @@ OBJS = yb_ash.o $(WIN32RES)
 EXTENSION = yb_ash
 DATA =
 PGFILEDESC = "yb_ash - Yugabyte Active Session History"
+SHLIB_LINK += -L$(YB_BUILD_ROOT)/lib -lyb_pggate_util
+
+LDFLAGS_SL += $(filter -lm, $(LIBS))
 
 REGRESS_OPTS = --temp-config $(top_srcdir)/contrib/yb_ash/yb_ash.conf
 REGRESS = yb_ash

src/postgres/contrib/yb_ash/yb_ash.c
@@ -17,18 +17,20 @@
 
 #include "postgres.h"
 
+#include "executor/executor.h"
+#include "miscadmin.h"
+#include "parser/analyze.h"
+#include "pgstat.h"
 #include "postmaster/bgworker.h"
-
 #include "storage/ipc.h"
 #include "storage/latch.h"
 #include "storage/lwlock.h"
 #include "storage/shmem.h"
-
+#include "tcop/utility.h"
 #include "utils/guc.h"
 #include "utils/timestamp.h"
 
-#include "miscadmin.h"
-#include "pgstat.h"
+#include "pg_yb_utils.h"
 
 PG_MODULE_MAGIC;
 PG_FUNCTION_INFO_V1(yb_active_session_history);
@@ -40,6 +42,10 @@ static int ash_sample_size;
 
 /* Saved hook values in case of unload */
 static shmem_startup_hook_type prev_shmem_startup_hook = NULL;
+static post_parse_analyze_hook_type prev_post_parse_analyze_hook = NULL;
+static ExecutorStart_hook_type prev_ExecutorStart = NULL;
+static ExecutorEnd_hook_type prev_ExecutorEnd = NULL;
+static ProcessUtility_hook_type prev_ProcessUtility = NULL;
 
 typedef struct YbAshEntry
 {
@@ -65,8 +71,17 @@ void _PG_fini(void);
 static int yb_ash_cb_max_entries(void);
 static Size yb_ash_memsize(void);
 static void yb_ash_startup(void);
+static void yb_set_ash_metadata(uint64_t query_id);
+static void yb_unset_ash_metadata();
 
 static void yb_ash_startup_hook(void);
+static void yb_ash_post_parse_analyze(ParseState *pstate, Query *query);
+static void yb_ash_ExecutorStart(QueryDesc *queryDesc, int eflags);
+static void yb_ash_ExecutorEnd(QueryDesc *queryDesc);
+static void yb_ash_ProcessUtility(PlannedStmt *pstmt, const char *queryString,
+								  ProcessUtilityContext context, ParamListInfo params,
+								  QueryEnvironment *queryEnv, DestReceiver *dest,
+								  char *completionTag);
 
 static volatile sig_atomic_t got_sigterm = false;
 static volatile sig_atomic_t got_sighup = false;
@@ -141,6 +156,18 @@ _PG_init(void)
 	 */
 	prev_shmem_startup_hook = shmem_startup_hook;
 	shmem_startup_hook = yb_ash_startup_hook;
+
+	prev_post_parse_analyze_hook = post_parse_analyze_hook;
+	post_parse_analyze_hook = yb_ash_post_parse_analyze;
+
+	prev_ExecutorStart = ExecutorStart_hook;
+	ExecutorStart_hook = yb_ash_ExecutorStart;
+
+	prev_ExecutorEnd = ExecutorEnd_hook;
+	ExecutorEnd_hook = yb_ash_ExecutorEnd;
+
+	prev_ProcessUtility = ProcessUtility_hook;
+	ProcessUtility_hook = yb_ash_ProcessUtility;
 }
 
 /*
@@ -151,6 +178,10 @@ _PG_fini(void)
 {
 	/* Uninstall hooks. */
 	shmem_startup_hook = prev_shmem_startup_hook;
+	post_parse_analyze_hook = prev_post_parse_analyze_hook;
+	ExecutorStart_hook = prev_ExecutorStart;
+	ExecutorEnd_hook = prev_ExecutorEnd;
+	ProcessUtility_hook = prev_ProcessUtility;
 }
 
 static void
@@ -194,6 +225,97 @@ yb_ash_startup(void)
 		yb_ash->index = 0;
 		yb_ash->max_entries = yb_ash_cb_max_entries();
 	}
+}
+
+static void
+yb_ash_post_parse_analyze(ParseState *pstate, Query *query)
+{
+	if (prev_post_parse_analyze_hook)
+		prev_post_parse_analyze_hook(pstate, query);
+
+	/* query_id will be set to zero if pg_stat_statements is disabled. */
+	yb_set_ash_metadata(query->queryId);
+}
+
+static void
+yb_ash_ExecutorStart(QueryDesc *queryDesc, int eflags)
+{
+	/*
+	 * In case of prepared statements, the 'Parse' phase might be skipped.
+	 * We set the ASH metadata here if it's not been set yet.
+	 * Note that query_id may be set to zero for utility stmts, but this
+	 * function will not be executed in that case.
+	 */
+	if (MyProc->yb_ash_metadata.query_id == 0)
+		yb_set_ash_metadata(queryDesc->plannedstmt->queryId);
+
+	if (prev_ExecutorStart)
+		prev_ExecutorStart(queryDesc, eflags);
+	else
+		standard_ExecutorStart(queryDesc, eflags);
+}
+
+static void
+yb_ash_ExecutorEnd(QueryDesc *queryDesc)
+{
+	if (prev_ExecutorEnd)
+		prev_ExecutorEnd(queryDesc);
+	else
+		standard_ExecutorEnd(queryDesc);
+
+	/*
+	 * Unset ASH metadata. Utility statements do not go through this
+	 * code path.
+	 */
+	yb_unset_ash_metadata();
+}
+
+static void
+yb_ash_ProcessUtility(PlannedStmt *pstmt, const char *queryString,
+					  ProcessUtilityContext context, ParamListInfo params,
+					  QueryEnvironment *queryEnv, DestReceiver *dest,
+					  char *completionTag)
+{
+	if (prev_ProcessUtility)
+		prev_ProcessUtility(pstmt, queryString,
+								 context, params, queryEnv,
+								 dest, completionTag);
+	else
+		standard_ProcessUtility(pstmt, queryString,
+								context, params, queryEnv,
+								dest, completionTag);
+
+	/*
+	 * Unset ASH metadata in case of utility statements. This function
+	 * might recurse, and we only want to unset in the last step.
+	 */
+	if (YBGetDdlNestingLevel() == 0)
+		yb_unset_ash_metadata();
+}
+
+static void
+yb_set_ash_metadata(uint64_t query_id)
+{
+	LWLockAcquire(&MyProc->yb_ash_metadata_lock, LW_EXCLUSIVE);
+
+	MyProc->yb_ash_metadata.query_id = query_id;
+	YBCGenerateAshRootRequestId(MyProc->yb_ash_metadata.root_request_id);
+	MyProc->yb_ash_metadata.is_set = true;
+
+	LWLockRelease(&MyProc->yb_ash_metadata_lock);
+}
+
+static void
+yb_unset_ash_metadata()
+{
+	LWLockAcquire(&MyProc->yb_ash_metadata_lock, LW_EXCLUSIVE);
+
+	MyProc->yb_ash_metadata.query_id = 0;
+	MemSet(MyProc->yb_ash_metadata.root_request_id, 0,
+		   sizeof(MyProc->yb_ash_metadata.root_request_id));
+	MyProc->yb_ash_metadata.is_set = false;
+
+	LWLockRelease(&MyProc->yb_ash_metadata_lock);
 }
 
 static void

src/postgres/src/backend/storage/ipc/procarray.c
@@ -269,6 +269,7 @@ CreateSharedProcArray(void)
 
 	/* Register and initialize fields of ProcLWLockTranche */
 	LWLockRegisterTranche(LWTRANCHE_PROC, "proc");
+	LWLockRegisterTranche(LWTRANCHE_YB_ASH_METADATA, "yb_ash_metadata");
 }
 
 /*

src/postgres/src/backend/storage/lmgr/proc.c
@@ -236,6 +236,7 @@ InitProcGlobal(void)
 			procs[i].sem = PGSemaphoreCreate();
 			InitSharedLatch(&(procs[i].procLatch));
 			LWLockInitialize(&(procs[i].backendLock), LWTRANCHE_PROC);
+			LWLockInitialize(&(procs[i].yb_ash_metadata_lock), LWTRANCHE_YB_ASH_METADATA);
 		}
 		procs[i].pgprocno = i;
 
@@ -448,6 +449,14 @@ InitProcess(void)
 
 	MyProc->ybLWLockAcquired = false;
 	MyProc->ybSpinLocksAcquired = 0;
+
+	MemSet(MyProc->yb_ash_metadata.root_request_id, 0,
+		   sizeof(MyProc->yb_ash_metadata.root_request_id));
+	MyProc->yb_ash_metadata.query_id = 0;
+	MemSet(MyProc->yb_ash_metadata.client_addr, 0,
+		   sizeof(MyProc->yb_ash_metadata.client_addr));
+	MyProc->yb_ash_metadata.client_port = 0;
+	MyProc->yb_ash_metadata.addr_family = AF_UNSPEC;
 
 	/*
 	 * Acquire ownership of the PGPROC's latch, so that we can use WaitLatch

src/postgres/src/backend/utils/misc/pg_yb_utils.c
@@ -820,7 +820,7 @@ YBInitPostgresBackend(
 		callbacks.UnixEpochToPostgresEpoch = &YbUnixEpochToPostgresEpoch;
 		callbacks.ConstructArrayDatum = &YbConstructArrayDatum;
 		callbacks.CheckUserMap = &check_usermap;
-		YBCInitPgGate(type_table, count, callbacks, session_id);
+		YBCInitPgGate(type_table, count, callbacks, session_id, &MyProc->yb_ash_metadata);
 		YBCInstallTxnDdlHook();
 
 		/*

src/postgres/src/common/pg_yb_common.c
@@ -242,3 +242,13 @@ YBColocateDatabaseByDefault()
 	}
 	return cached_value;
 }
+
+bool
+YBEnableAsh()
+{
+	static int cached_value = -1;
+	if (cached_value == -1)
+		cached_value = YBCIsEnvVarTrue("FLAGS_TEST_yb_enable_ash");
+
+	return cached_value;
+}

src/postgres/src/include/common/pg_yb_common.h
@@ -155,4 +155,9 @@ extern const bool kTestOnlyUseOSDefaultCollation;
  */
 extern bool YBColocateDatabaseByDefault();
 
+/**
+ * Returns true if Active Session History should be enabled.
+ */
+extern bool YBEnableAsh();
+
 #endif /* PG_YB_COMMON_H */

commit 48f7e3c734b54ed3b5163bc4796e523838e574a9

[#20338] YSQL, ASH: Fix an assertion when ASH is enabled
Summary:
Background workers may not have MyProcPort set. This results in an assertion
failure in ASH code if a postgres backend was initialized by the background
worker and causes the background worker to crash. The assertion failure is only
hit when ASH is enabled. This assertion was incorrectly introduced by commit
de8c183828258b99c8b4601f20bf889043b9d3b0

This diff checks and doesn't set client address in ASH metadata if MyProcPort is null,
i.e. when a background worker creates a postgres backend.
Jira: DB-9324

Test Plan: Jenkins: compile only

Reviewers: jason

Reviewed By: jason

Subscribers: amitanand, hbhanawat, yql

Differential Revision: https://phorge.dev.yugabyte.com/D31159

src/postgres/src/backend/utils/init/postinit.c
@@ -1391,11 +1391,19 @@ ThereIsAtLeastOneRole(void)
  * pg_getnameinfo_all returns non-zero value, a warning is printed with the error
  * code and ASH keeps working without client address and port for the current PG
  * backend.
+ *
+ * ASH samples only normal backends and this excludes background workers.
+ * So it's fine in that case to not set the client address.
  */
 static void
 YbSetAshClientAddrAndPort()
 {
-	Assert(MyProcPort != NULL);
+	/* Background workers which creates a postgres backend may have null MyProcPort. */
+	if (MyProcPort == NULL)
+	{
+		Assert(MyProc->isBackgroundWorker == true);
+		return;
+	}
 
 	LWLockAcquire(&MyProc->yb_ash_metadata_lock, LW_EXCLUSIVE);
 