715dc7b09ebfc8179f283386b73552c710730622
asrinivasanyb <asrinivasan@yugabyte.com>
2023-12-21 13:13:29 +0530

    [#20367] CDCSDK: Make CDC Consistent Snapshot PREVIEW feature, USE_SNAPSHOT as default consistent snapshot option
    Summary:
    This is a part of the CDC Consistent Snapshot feature (GH #18508).
    
    This diff does the following -
    1) Consistent Snapshot feature is made a preview feature. The gflag, yb_enable_cdc_consistent_snapshot_streams,
    is now made a RUNTIME_PREVIEW flag
    
    2) The default consistent snapshot option while creating a stream is made as USE_SNAPSHOT
    
    The following will create a consistent snapshot stream with USE_SNAPSHOT as the consistent snapshot option -
    yb-admin --master_addresses ${IP}:7100 create_change_data_stream ysql.yugabyte
    
    UPGRADE/ROLLBACK SAFETY:
    Upgrade safe
    Rollback safe
    
    This feature is currently disabled. Changes are guarded by the PREVIEW flag
    yb_enable_cdc_consistent_snapshot_streams which is set to false by default.
    Jira: DB-9359
    
    Test Plan:
    All tests
    Manual Testing
    
    Reviewers: stiwary, skumar, xCluster, hsunder
    
    Reviewed By: stiwary
    
    Subscribers: ybase, ycdcxcluster, bogdan
    
    Differential Revision: https://phorge.dev.yugabyte.com/D31283

--- src/yb/tools/yb-admin_client.h
+++ src/yb/tools/yb-admin_client.h
@@ -391,7 +391,7 @@ class ClusterAdminClient {
   Status CreateCDCSDKDBStream(
       const TypedNamespaceName& ns, const std::string& CheckPointType,
       const cdc::CDCRecordType RecordType,
-      const std::optional<std::string>& ConsistentSnapshotOption);
+      const std::string& ConsistentSnapshotOption);
 
   Status DeleteCDCStream(const std::string& stream_id, bool force_delete = false);
 
