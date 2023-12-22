da04309d60f0367cc796b37c64d21ef1cb3c2781
Minghui Yang <myang@yugabyte.com>
2023-12-22 02:32:30 +0000

    [#20303] YSQL: Fix restart read error in cross DB concurrent DDLs
    Summary:
    While performing Cross-DB Concurrent DDLs with per-database catalog version turned on,
    some DDLs failed with the below error,
    
    ```
    ERROR: Restart read required at: { read: { physical: 1702489614501399 } local_limit: { physical: 1702489614501399 } global_limit: <min> in_txn_limit: <max> serial_no: 0 }.
    ```
    
    Some DDLs were
    
    ```
    CREATE INDEX ON tb_0(k);
    ALTER TABLE tb_0 ADD newColumn3 TEXT DEFAULT 'Lorem Ipsum is simply dummy text of the printing and
    industry'
    CREATE VIEW view1_tb_0 AS SELECT k from tb_0
    CREATE TABLE tempTable2 AS SELECT * FROM tb_0
    ```
    
    After debugging, I found that in all the failed DDLs, they are the first DDL
    executed after a connection is setup. With a custom build that added extra logs,
    I found that the DDL statement is reading the table pg_shdepend which is a
    shared relation. Many DDLs do update pg_shdepend table to record the the
    dependency relationships between database objects and shared objects, such as
    roles. The reading is done as a full table scan. Because there are many
    concurrent DDLs (e.g., 10) executing at the same time, the table pg_shdepend is
    constantly changing so doing a full table scan of pg_shdepend is likely to hit
    `Restart read required` error.
    
    Further debugging found the scan of pg_shdepend is done after the DDL statement
    has already started, that's why it must use a fixed read time (selected by the
    local tserver) to read a consistent snapshot. This prevented automatic read
    restart at the server side (in this case the server is yb-master). The scan of
    pg_shdepend is triggered by the function `YbInitPinnedCacheIfNeeded`. I made a
    change to call `YbInitPinnedCacheIfNeeded` before we start the DDL transaction,
    in this way the scan does not use a fixed read time and therefore can be
    automatically retried at the server side. This can prevent the "Restart read
    required" error at the client side. If the automatic retry at the server side is
    exhausted, we will see a different error according to the following code:
    
    ```
        if (CoarseMonoClock::now() > context_.GetClientDeadline()) {
          TRACE("Read timed out");
          return STATUS(TimedOut, "Read timed out");
        }
    ```
    
    I also found a improper usage of yb_non_ddl_txn_for_sys_tables_allowed with
    a DDL statement (CREATE OR REPLACE VIEW) that can cause wrong session
    type to be used and resulted in SQL error when running YSQL upgrade test
    in per-database catalog version mode with this fix. This GUC should only be
    used with non-DDL statement. I removed the improper use of it.
    
    Test Plan:
    (1) Deploy a package with this fix and run the SqlCrossDBLoadWithDDL test and
    confirm that the "Restart read error" disappeared.
    
    (2)
    ./yb_build.sh --cxx-test pg_catalog_version-test
    
    Reviewers: jason, tverona
    
    Reviewed By: jason
    
    Subscribers: yql
    
    Differential Revision: https://phorge.dev.yugabyte.com/D31315

--- src/postgres/src/backend/utils/misc/pg_yb_utils.c
+++ src/postgres/src/backend/utils/misc/pg_yb_utils.c
@@ -2045,7 +2045,28 @@ YBTxnDdlProcessUtility(
 	PG_TRY();
 	{
 		if (is_ddl)
+		{
+			if (YBIsDBCatalogVersionMode())
+				/*
+				 * In order to support concurrent non-global-impact DDLs
+				 * across different databases, call YbInitPinnedCacheIfNeeded
+				 * now which triggers a scan of pg_shdepend and pg_depend.
+				 * This ensure that the scan is done without using a read time
+				 * of the DDL transaction so that yb-master can retry read
+				 * restarts automatically. Otherwise, a read restart error is
+				 * returned to the PG backend the DDL statement will fail
+				 * because DDLs cannot be restarted.
+				 *
+				 * YB NOTE: this implies a performance hit for DDL statements
+				 * that do not need to call YbInitPinnedCacheIfNeeded.
+				 *
+				 * TODO(myang): we can optimize to only read pg_shdepend here
+				 * to reduce its performance penalty.
+				 */
+				YbInitPinnedCacheIfNeeded();
+
 			YBIncrementDdlNestingLevel(ddl_mode.value);
+		}
 
 		if (prev_ProcessUtility)
 			prev_ProcessUtility(pstmt, queryString,
