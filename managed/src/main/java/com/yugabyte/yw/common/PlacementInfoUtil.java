6f5159754d7a3dc9dcf952002f6f4f0fe6188961
Rajagopalan Madhavan <rmadhavan@yugabyte.com>
2023-12-26 16:26:26 -0500

    [PLAT-11921]: Pass all the nodes in the node details set when editing RR cluster
    Summary:
    So I found the exact issue, here are the steps:
    You first create a universe with PRIMAY CLUSTER (1 node for example) and ASYNC CLUSTER (2 node for example with num read replicas as 2), lets say name
    of ASYNC nodes are RR-1, RR-2
    Now when you EDIT RR, increase node count of RR by 3, now in the universe_configure call, you will see RR nodes, RR-1, RR-2, RR-3 (ToBeAddedState)
    Now in after you increase node count in above step, now INCREASE NUM READ REPLICAS from 2 to 3, this is where the issue happens.
    
    In Step 1: RR-1 is in us-west-1a and us-west-1b
    In Step 2: RR-1 is in  us-west-1a , RR-2 is in us-west-1b , RR-3 is in us-west-1a
    In step 3: RR-1 is in  us-west-1a , RR-2 is in us-west-1b , RR-3 is in us-west-1c
    
    Now the delta between step 2 and step 3 is that initially we had 2 nodes in us-west-2a but after step 3, now there is 1 node in us-west-1a and 1 node
    in us-west-1c
    The issue happens exactly in this line where it removes 1 RR node in us-west-1a after step 3
    
    Test Plan:
    Tried
    1. FULL MOVE
    2. EDIT RR
    3. EDIT PRIMARY
    4. EXPAND NODES IN RR
    5. SHRINK NODES IN RR
    
    **[DO NOT MERGE]**
    
    Reviewers: yshchetinin
    
    Reviewed By: yshchetinin
    
    Subscribers: yugaware
    
    Differential Revision: https://phorge.dev.yugabyte.com/D31123

--- managed/src/main/java/com/yugabyte/yw/common/PlacementInfoUtil.java
+++ managed/src/main/java/com/yugabyte/yw/common/PlacementInfoUtil.java
@@ -1260,6 +1260,7 @@ public class PlacementInfoUtil {
       NodeDetails currentNode = nodeIter.next();
       if (currentNode.isInPlacement(clusterUUID)
           && currentNode.azUuid.equals(targetAZUuid)
+          && currentNode.state == NodeState.ToBeAdded
           && !currentNode.isMaster) {
         nodeIter.remove();
 
