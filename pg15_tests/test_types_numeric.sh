acc620ffec01d51e524a6c05b7d1d19730c4562a
Jason Kim <git@jasonk.me>
2023-12-27 14:46:35 -0800

    [pg15] test: fix yb_pg_int8, yb_pg_float4
    Summary:
    Fix tests failing in TestPgRegressTypesNumeric, then move it to
    pg15_tests/passing_java.tsv.
    
    - yb_pg_int8: change expected output as in upstream PG
      3522d0eaba5a976f09a48810dd25dff6ab3565df.
    - yb_pg_float4: change expected output as in upstream PG
      f397e08599a3c3c08b3af3b318c531db5882f57d.  Don't port the new queries.
    
    Test Plan:
    On Almalinux 8:
    
        #!/usr/bin/env bash
        set -e
        ./yb_build.sh fastdebug --gcc11
        ./yb_build.sh fastdebug --gcc11 --java-test TestPgRegressTypesNumeric --sj
        pg15_tests/test_lint.sh fastdebug --gcc11
    
    Jenkins: skip
    
    Reviewers: aagrawal, tfoucher
    
    Reviewed By: tfoucher
    
    Subscribers: yql
    
    Differential Revision: https://phorge.dev.yugabyte.com/D31377

--- pg15_tests/test_types_numeric.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-source "${BASH_SOURCE[0]%/*}"/common.sh
-
-failing_java_test TestPgRegressTypesNumeric
-grep_in_java_test \
-  "failed tests: [yb_pg_float4, yb_pg_int8]" \
-  TestPgRegressTypesNumeric
