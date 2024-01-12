ab8883c336ff0e36dd0f1a90d0250cf41571c69c
Jason Kim <git@jasonk.me>
2024-01-11 17:23:07 -0800

    [pg15] test: fixup mistake
    Summary:
    Previous commit 4aea54e8a151540eb3ff67ac0e4a45ab88b21679 missed a step
    to move the nulls test to passing.  So it results in failure on
    pg15_tests/test_alter_table_change_primary_key_null.sh.  Must not have taken a
    close look at the test plan's exit status either.  Fix it now.
    
    Test Plan: Jenkins: rebase: pg15
    
    Reviewers: tfoucher
    
    Reviewed By: tfoucher
    
    Differential Revision: https://phorge.dev.yugabyte.com/D31660

--- pg15_tests/test_alter_table_change_primary_key_null.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-source "${BASH_SOURCE[0]%/*}"/common.sh
-
-failing_java_test 'TestPgAlterTableChangePrimaryKey#nulls'
-grep_in_java_test \
-  "java.lang.AssertionError: Unexpected Error Message. Got: 'ERROR: Missing/null value for primary key column', Expected to contain one of the error messages: 'column \"id\" of relation \"nopk\" contains null values'." \
-  'TestPgAlterTableChangePrimaryKey#nulls'
