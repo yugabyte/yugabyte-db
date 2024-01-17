6644ec73e3c81a802c5beff0f6486546ebcd7d36
Aleksandr Malyshev <amalyshev@yugabyte.com>
2024-01-17 14:57:06 +0300

    [PLAT-12260] Modify pg stat statements storage to store latencies instead of raw data + mode query details to separate table.
    Summary:
    Initially we stored just raw pg_stat_statements data in TS database.
    Later we decided to store latencies and use these for anomaly detection.
    Made changes to achieve that.
    Also query details were moved to a separate table - to make stats table lighter.
    
    Test Plan: unit tested
    
    Reviewers: cdavid
    
    Reviewed By: cdavid
    
    Subscribers: yugaware
    
    Differential Revision: https://phorge.dev.yugabyte.com/D31749

--- troubleshoot/backend/src/main/resources/db/migration/V3__Adding_pg_stat_statements_table.sql
+++ troubleshoot/backend/src/main/resources/db/migration/V3__Adding_pg_stat_statements_table.sql
@@ -3,20 +3,15 @@ CREATE
         pg_stat_statements(
             scheduled_timestamp timestamptz NOT NULL,
             actual_timestamp timestamptz NOT NULL,
-            customer_id uuid NOT NULL,
             universe_id uuid NOT NULL,
-            cluster_id uuid NOT NULL,
             node_name varchar(1000) NOT NULL,
-            cloud varchar(100) NOT NULL,
-            region varchar(100) NOT NULL,
-            az varchar(100) NOT NULL,
 
-            db_id varchar(1000) NOT NULL,
-            db_name varchar(4000) NOT NULL,
             query_id bigint NOT NULL,
-            query text NOT NULL,
             calls bigint NOT NULL,
-            total_time double precision NOT NULL,
             rows bigint NOT NULL,
-            yb_latency_histogram jsonb
+            avg_latency double precision NOT NULL,
+            mean_latency double precision,
+            p90_latency double precision,
+            p99_latency double precision,
+            max_latency double precision
         );
