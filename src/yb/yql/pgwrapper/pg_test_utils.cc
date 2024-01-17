yugabyte/yugabyte-db

commit 32e1d30782cb6901df46908ae723b467cc89afcc

[#20580] YSQL: Fix tsan warning in the MetricWatcher helper class used in tests
Summary:
the `MetricWatcher` helped class (which is used in tests) uses the `UnsafeMetricsMapForTests` method of the `MetricEntity` class to get access to a map which is not guarded by the lock. As a result tests might fail with tsan warning.

Solution is to use the `MetricEntity` class itself instead of accessing internal map.

Jira: DB-9584

Test Plan:
Jenkins

Run existing test multiple times

```
./yb_build.sh tsan --gtest_filter PgCatalogPerfTest.ResponseCacheWithTooOldSnapshot -n 20
```

Reviewers: myang, pjain

Reviewed By: myang

Subscribers: yql

Tags: #jenkins-ready

Differential Revision: https://phorge.dev.yugabyte.com/D31669

src/yb/yql/pgwrapper/pg_test_utils.cc
@@ -26,10 +26,11 @@ namespace yb::pgwrapper {
 namespace {
 
 Result<size_t> GetMetric(const MetricWatcherDescriptor& desc) {
-  auto item = desc.map.find(&desc.proto);
-  RSTATUS_DCHECK(item != desc.map.end(), IllegalState, "Metric not found");
-  const auto& metric = *item->second;
-  switch(metric.prototype()->type()) {
+  auto metric_ptr = desc.entity.FindOrNull(desc.proto);
+  RSTATUS_DCHECK(metric_ptr, IllegalState, "Metric not found");
+  const auto& metric = *metric_ptr;
+  const auto& type = metric.prototype()->type();
+  switch(type) {
     case MetricType::kHistogram: return down_cast<const Histogram&>(metric).TotalCount();
     case MetricType::kEventStats:
         return down_cast<const EventStats&>(metric).TotalCount();
@@ -38,8 +39,7 @@ Result<size_t> GetMetric(const MetricWatcherDescriptor& desc) {
     case MetricType::kGauge: break;
     case MetricType::kLag: break;
   }
-  RSTATUS_DCHECK(
-      false, IllegalState, Format("Unsupported metric type $0", metric.prototype()->type()));
+  RSTATUS_DCHECK(false, IllegalState, Format("Unsupported metric type $0", type));
   return 0;
 }
 
@@ -56,11 +56,6 @@ Status UpdateDelta(
     d->delta_receiver = VERIFY_RESULT(GetMetric(*d)) - d->delta_receiver;
   }
   return Status::OK();
-}
-
-const MetricEntity::MetricMap& GetMetricMap(
-    std::reference_wrapper<const server::RpcServerBase> server) {
-  return server.get().metric_entity()->UnsafeMetricsMapForTests();
 }
 
 bool HasTransactionError(const Status& status) {
@@ -100,5 +95,16 @@ std::string_view SerializeAccessErrorMessageSubstring() {
 std::string MaxQueryLayerRetriesConf(uint16_t max_retries) {
   return Format("yb_max_query_layer_retries=$0", max_retries);
 }
+
+SingleMetricDescriber::SingleMetricDescriber(
+    std::reference_wrapper<const MetricEntity> entity,
+    std::reference_wrapper<const MetricPrototype> proto)
+    : descriptors{Descriptor{&delta, entity, proto}} {}
+
+
+SingleMetricDescriber::SingleMetricDescriber(
+    std::reference_wrapper<const server::RpcServerBase> server,
+    std::reference_wrapper<const MetricPrototype> proto)
+    : SingleMetricDescriber(*server.get().metric_entity(), proto) {}
 
 } // namespace yb::pgwrapper

src/yb/yql/pgwrapper/pg_catalog_perf-test.cc
@@ -90,9 +90,9 @@ struct MetricCounters {
 };
 
 struct MetricCountersDescriber : public MetricWatcherDeltaDescriberTraits<MetricCounters, 8> {
-  explicit MetricCountersDescriber(
-      std::reference_wrapper<const MetricEntity::MetricMap> master_metric,
-      std::reference_wrapper<const MetricEntity::MetricMap> tserver_metric)
+  MetricCountersDescriber(
+      std::reference_wrapper<const MetricEntity> master_metric,
+      std::reference_wrapper<const MetricEntity> tserver_metric)
       : descriptors{
           Descriptor{
               &delta.master_read_rpc, master_metric,
@@ -143,8 +143,8 @@ class PgCatalogPerfTestBase : public PgMiniTestBase {
     }
     ANNOTATE_UNPROTECTED_WRITE(FLAGS_ysql_use_relcache_file) = config.use_relcache_file;
     PgMiniTestBase::SetUp();
-    metrics_.emplace(GetMetricMap(*cluster_->mini_master()->master()),
-                     GetMetricMap(*cluster_->mini_tablet_server(0)->server()));
+    metrics_.emplace(*cluster_->mini_master()->master()->metric_entity(),
+                     *cluster_->mini_tablet_server(0)->server()->metric_entity());
   }
 
   size_t NumTabletServers() override {

src/yb/yql/pgwrapper/pg_fkey-test.cc
@@ -53,14 +53,14 @@ struct RpcCountMetric {
 };
 
 struct RpcCountMetricDescriber : public MetricWatcherDeltaDescriberTraits<RpcCountMetric, 3> {
-  explicit RpcCountMetricDescriber(std::reference_wrapper<const MetricEntity::MetricMap> map)
+  explicit RpcCountMetricDescriber(std::reference_wrapper<const MetricEntity> entity)
       : descriptors{
           Descriptor{
-              &delta.read, map, METRIC_handler_latency_yb_tserver_TabletServerService_Read},
+              &delta.read, entity, METRIC_handler_latency_yb_tserver_TabletServerService_Read},
           Descriptor{
-              &delta.write, map, METRIC_handler_latency_yb_tserver_TabletServerService_Write},
+              &delta.write, entity, METRIC_handler_latency_yb_tserver_TabletServerService_Write},
           Descriptor{
-              &delta.perform, map, METRIC_handler_latency_yb_tserver_PgClientService_Perform}}
+              &delta.perform, entity, METRIC_handler_latency_yb_tserver_PgClientService_Perform}}
   {}
 
   DeltaType delta;
@@ -73,7 +73,7 @@ class PgFKeyTest : public PgMiniTestBase {
     FLAGS_enable_automatic_tablet_splitting = false;
     ANNOTATE_UNPROTECTED_WRITE(FLAGS_ysql_pg_conf_csv) = MaxQueryLayerRetriesConf(0);
     PgMiniTestBase::SetUp();
-    rpc_count_.emplace(GetMetricMap(*cluster_->mini_tablet_server(0)->server()));
+    rpc_count_.emplace(*cluster_->mini_tablet_server(0)->server()->metric_entity());
   }
 
   size_t NumTabletServers() override {

src/yb/yql/pgwrapper/pg_test_utils.h
@@ -37,12 +37,12 @@ namespace pgwrapper {
 struct MetricWatcherDescriptor {
   MetricWatcherDescriptor(
       size_t* delta_receiver_,
-      std::reference_wrapper<const MetricEntity::MetricMap> map_,
+      std::reference_wrapper<const MetricEntity> entity_,
       std::reference_wrapper<const MetricPrototype> proto_)
-      : delta_receiver(*delta_receiver_), map(map_), proto(proto_) {}
+      : delta_receiver(*delta_receiver_), entity(entity_), proto(proto_) {}
 
   size_t& delta_receiver;
-  const MetricEntity::MetricMap& map;
+  const MetricEntity& entity;
   const MetricPrototype& proto;
 
  private:
@@ -53,9 +53,6 @@ using MetricWatcherFunctor = std::function<Status()>;
 
 Status UpdateDelta(
   const MetricWatcherDescriptor* descrs, size_t count, const MetricWatcherFunctor& func);
-
-const MetricEntity::MetricMap& GetMetricMap(
-    std::reference_wrapper<const server::RpcServerBase> server);
 
 template<class Describer>
 class MetricWatcherBase {
@@ -91,14 +88,12 @@ struct MetricWatcherDeltaDescriberTraits {
 };
 
 struct SingleMetricDescriber : public MetricWatcherDeltaDescriberTraits<size_t, 1> {
-  SingleMetricDescriber(std::reference_wrapper<const MetricEntity::MetricMap> map,
-                        std::reference_wrapper<const MetricPrototype> metric)
-      : descriptors{Descriptor{&delta, map, metric}} {}
+  SingleMetricDescriber(std::reference_wrapper<const MetricEntity> entity,
+                        std::reference_wrapper<const MetricPrototype> proto);
 
 
   SingleMetricDescriber(std::reference_wrapper<const server::RpcServerBase> server,
-                        std::reference_wrapper<const MetricPrototype> metric)
-      : SingleMetricDescriber(GetMetricMap(server), metric) {}
+                        std::reference_wrapper<const MetricPrototype> proto);
 
   DeltaType delta;
   Descriptors descriptors;

commit 325463392e21f28043955e073d5be3f3f6caceb7

build: Fix compile issue with FindOrNull metrics
Summary: D30650 changed MetricEntity's FindOrNull to be templated on the metric type to match FindOrCreateMetric. This diff fixes a compilation issue introduced by an interaction with D31669.

Test Plan: jenkins:compile only, urgent

Reviewers: steve.varnau

Reviewed By: steve.varnau

Subscribers: yql

Tags: #jenkins-ready

Differential Revision: https://phorge.dev.yugabyte.com/D31760

src/yb/yql/pgwrapper/pg_test_utils.cc
@@ -26,7 +26,7 @@ namespace yb::pgwrapper {
 namespace {
 
 Result<size_t> GetMetric(const MetricWatcherDescriptor& desc) {
-  auto metric_ptr = desc.entity.FindOrNull(desc.proto);
+  auto metric_ptr = desc.entity.FindOrNull<Metric>(desc.proto);
   RSTATUS_DCHECK(metric_ptr, IllegalState, "Metric not found");
   const auto& metric = *metric_ptr;
   const auto& type = metric.prototype()->type();