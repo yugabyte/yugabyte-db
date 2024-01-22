yugabyte/yugabyte-db

commit 3af5fea4204a9776a421670fd60eb4472c40169a

[#19589] YSQL: Add batch-sorting to BNL execution
Summary:
This diff mainly introduces sorting capabilities to BNL nodes. A BNL can match the sorting properties of other join types by sorting each individual batch of joined on the outer-side pathkeys. This was in order to get rid of the biggest remaining regression between BNL and NL where an ORDER BY LIMIT query would require a BNL to materialize all its tuples while an NL join wouldn't. This diff also introduces an optimization to decrease the size of the first requested batch of a BNL to the limit that's pushed down through the yb_exec_params to get closer to NL performance in the presence of small LIMIT clauses. The GUC flag `yb_bnl_optimize_first_batch` has been added to control this behavior.
Jira: DB-8368

Test Plan: ./yb_build.sh --java-test 'org.yb.pgsql.TestPgRegressJoin'

Reviewers: mtakahara

Reviewed By: mtakahara

Subscribers: yql

Differential Revision: https://phorge.dev.yugabyte.com/D30441

src/postgres/src/backend/executor/nodeYbBatchedNestloop.c
@@ -31,20 +31,23 @@
 
 #include "postgres.h"
 
+#include <math.h>
+
 #include "executor/execdebug.h"
 #include "executor/executor.h"
 #include "executor/nodeYbBatchedNestloop.h"
 #include "nodes/relation.h"
 #include "miscadmin.h"
 #include "utils/memutils.h"
+#include "utils/tuplesort.h"
 
 
 bool yb_bnl_enable_hashing = true;
 
 /* Methods to help keep track of outer tuple batches */
 bool CreateBatch(YbBatchedNestLoopState *bnlstate, ExprContext *econtext);
-int GetBatchSize(YbBatchedNestLoop *plan);
-bool IsBatched(YbBatchedNestLoop *plan);
+int GetMaxBatchSize(YbBatchedNestLoop *plan);
+int GetCurrentBatchSize(YbBatchedNestLoopState *bnlstate);
 
 /* Local join methods that use the tuplestore batching strategy */
 bool FlushTupleTS(YbBatchedNestLoopState *bnlstate, ExprContext *econtext);
@@ -67,7 +70,11 @@ void AddTupleToOuterBatchHash(YbBatchedNestLoopState *bnlstate,
 							  TupleTableSlot *slot);
 void FreeBatchHash(YbBatchedNestLoopState *bnlstate);
 void EndHash(YbBatchedNestLoopState *bnlstate);
+static TupleTableSlot *ProcessSorting(YbBatchedNestLoopState *bnlstate);
+static void EndSorting(YbBatchedNestLoopState *bnlstate);
 
+static bool
+PutSorting(YbBatchedNestLoopState *bnlstate, TupleTableSlot *slot);
 /* Wrappers for invoking local join methods with the correct strategy */
 #define REGISTER_LOCAL_JOIN_FN(fn, strategy) bnlstate->fn##Impl = &fn##strategy
 #define LOCAL_JOIN_FN(fn, node, ...) (*node->fn##Impl)(node, ## __VA_ARGS__)
@@ -86,7 +93,7 @@ void EndHash(YbBatchedNestLoopState *bnlstate);
  *		overall workflow are outlined as follows:
  *
  *		- BNL_INIT:	   The current tuple batch is invalid and we must create a
- *				  	   fresh batch and then transition to BNL_NEWINNER.
+ *						fresh batch and then transition to BNL_NEWINNER.
  *
  *		- BNL_NEWINNER: We need a new inner tuple. This can occur as a result of
  *						the outer tuple batch being freshly populated or the
@@ -104,7 +111,7 @@ void EndHash(YbBatchedNestLoopState *bnlstate);
  *
  *		- BNL_FLUSHING: The current tuple batch is valid and we have run out of
  *						matching inner tuples. If this is an outer/anti join, we
- *						are iterating over tuples that have not been matched 
+ *						are iterating over tuples that have not been matched
  *						with any inner tuple. Once we run out of unmatched outer
  *						tuples here, we invalidate the current batch by
  *						transitioning to BNL_INIT.
@@ -140,12 +147,29 @@ ExecYbBatchedNestLoop(PlanState *pstate)
 	 * storage allocated in the previous tuple cycle.
 	 */
 	ResetExprContext(econtext);
-
 	/*
 	 * Ok, everything is setup for the join so now loop until we return a
 	 * qualifying join tuple.
 	 */
 	elog(DEBUG2, "entering main loop");
+
+	bnlstate->batch_size = GetMaxBatchSize(batchnl);
+
+	if (!pstate->state->yb_exec_params.limit_use_default)
+	{
+		uint32_t limit = pstate->state->yb_exec_params.limit_count;
+		limit = ceil(limit * batchnl->first_batch_factor);
+		if (limit > 0 && limit < GetMaxBatchSize(batchnl))
+		{
+			if (!bnlstate->is_first_batch_done)
+				bnlstate->batch_size = limit;
+
+			limit = GetCurrentBatchSize(bnlstate);
+			pstate->state->yb_exec_params.limit_count = limit;
+		}
+	}
+
+	Assert(bnlstate->batch_size > 0);
 
 	for (;;)
 	{
@@ -157,15 +181,22 @@ ExecYbBatchedNestLoop(PlanState *pstate)
 		switch (bnlstate->bnl_currentstatus)
 		{
 			case BNL_INIT:
+				if (bnlstate->bnl_needs_sorting)
+				{
+					TupleTableSlot *result = ProcessSorting(bnlstate);
+					if (result)
+						return result;
+				}
+
 				if (CreateBatch(bnlstate, econtext))
 				{
 					/* Transition */
+					bnlstate->is_first_batch_done = true;
+					bnlstate->batch_size = GetMaxBatchSize(batchnl);
 					bnlstate->bnl_currentstatus = BNL_NEWINNER;
 				}
 				else
-				{
 					return NULL;
-				}
 
 				/*
 				 * now rescan the inner plan
@@ -204,10 +235,9 @@ ExecYbBatchedNestLoop(PlanState *pstate)
 					continue;
 				}
 				break;
-
 			case BNL_FLUSHING:
-				if (batchnl->nl.join.jointype == JOIN_INNER
-					|| batchnl->nl.join.jointype == JOIN_SEMI)
+				if (batchnl->nl.join.jointype == JOIN_INNER ||
+					batchnl->nl.join.jointype == JOIN_SEMI)
 				{
 					/* This state is not applicable here: transition */
 					bnlstate->bnl_currentstatus = BNL_INIT;
@@ -225,7 +255,7 @@ ExecYbBatchedNestLoop(PlanState *pstate)
 			default:
 				Assert(false);
 		}
-		
+
 		Assert(!TupIsNull(econtext->ecxt_outertuple));
 
 		innerTupleSlot = econtext->ecxt_innertuple;
@@ -254,8 +284,11 @@ ExecYbBatchedNestLoop(PlanState *pstate)
 				 * ExecProject().
 				 */
 				elog(DEBUG2, "qualification succeeded, projecting tuple");
+				TupleTableSlot *slot = ExecProject(bnlstate->js.ps.ps_ProjInfo);
+				if (PutSorting(bnlstate, slot))
+					continue;
 
-				return ExecProject(bnlstate->js.ps.ps_ProjInfo);
+				return slot;
 			}
 			else
 				InstrCountFiltered2(bnlstate, 1);
@@ -284,7 +317,7 @@ ExecYbBatchedNestLoop(PlanState *pstate)
 			if (bnlstate->js.jointype == JOIN_ANTI)
 			{
 				/*
-				 * This outer tuple has been matched so never think about 
+				 * This outer tuple has been matched so never think about
 				 * this outer tuple again.
 				 */
 				continue;		/* return to top of loop */
@@ -298,7 +331,11 @@ ExecYbBatchedNestLoop(PlanState *pstate)
 				 */
 				elog(DEBUG2, "qualification succeeded, projecting tuple");
 
-				return ExecProject(bnlstate->js.ps.ps_ProjInfo);
+				TupleTableSlot *slot = ExecProject(bnlstate->js.ps.ps_ProjInfo);
+				if (PutSorting(bnlstate, slot))
+					continue;
+
+				return slot;
 			}
 			else
 				InstrCountFiltered2(bnlstate, 1);
@@ -315,6 +352,87 @@ ExecYbBatchedNestLoop(PlanState *pstate)
 	}
 }
 
+static void
+EndSorting(YbBatchedNestLoopState *bnlstate)
+{
+	if (!bnlstate->bnl_needs_sorting)
+		return;
+
+	Tuplesortstate *sorter = bnlstate->bnl_tuple_sort;
+	bnlstate->bnl_is_sorted = false;
+	if (sorter)
+	{
+		tuplesort_end(sorter);
+		bnlstate->bnl_tuple_sort = NULL;
+	}
+}
+
+static bool
+PutSorting(YbBatchedNestLoopState *bnlstate, TupleTableSlot *slot)
+{
+	if (bnlstate->bnl_tuple_sort)
+	{
+		tuplesort_puttupleslot(bnlstate->bnl_tuple_sort, (slot));
+		return true;
+	}
+	return false;
+}
+
+/*
+ * If we are running a sorted BNL, this method encapsulates sorting-related
+ * logic. If the sorter has tuples left to be returned, each invocation of this
+ * method flushes a tuple by returning it in sorted order. Else, it frees any
+ * previously-held sorting resources and starts anew.
+ */
+static TupleTableSlot *
+ProcessSorting(YbBatchedNestLoopState *bnlstate)
+{
+	TupleTableSlot *result_slot = bnlstate->js.ps.ps_ResultTupleSlot;
+	Tuplesortstate *sorter = bnlstate->bnl_tuple_sort;
+	if (!bnlstate->bnl_needs_sorting)
+		return NULL;
+
+	YbBatchedNestLoop *batchnl_plan =
+		(YbBatchedNestLoop *) bnlstate->js.ps.plan;
+	if (sorter)
+	{
+		if (!bnlstate->bnl_is_sorted)
+			tuplesort_performsort(sorter);
+
+		bnlstate->bnl_is_sorted = true;
+
+		/* Flush sorter. */
+		bool result = tuplesort_gettupleslot(
+			sorter, true,  false, result_slot, NULL);
+		Assert(!result || !result_slot->tts_isempty);
+		if (result)
+			return result_slot;
+
+		/* Done flushing sorter, make new tuples now. */
+		EndSorting(bnlstate);
+	}
+
+	if (!bnlstate->bnl_tuple_sort)
+	{
+		TupleDesc result_desc =
+			result_slot->tts_tupleDescriptor;
+		sorter = tuplesort_begin_heap(result_desc,
+									  batchnl_plan->numSortCols,
+									  batchnl_plan->sortColIdx,
+									  batchnl_plan->sortOperators,
+									  batchnl_plan->collations,
+									  batchnl_plan->nullsFirst,
+									  work_mem,
+									  NULL,
+									  false);
+
+		if (bnlstate->bound)
+			tuplesort_set_bound(sorter, bnlstate->bound);
+
+		bnlstate->bnl_tuple_sort = sorter;
+	}
+	return NULL;
+}
 
 /*
  * Whether or not we are using the hash batching strategy. We go with
@@ -342,7 +460,7 @@ InitHash(YbBatchedNestLoopState *bnlstate)
 
 	int num_hashClauseInfos = plan->num_hashClauseInfos;
 	Oid *eqops = palloc(num_hashClauseInfos * (sizeof(Oid)));
-	
+
 	bnlstate->numLookupAttrs = num_hashClauseInfos;
 	bnlstate->innerAttrs =
 		palloc(num_hashClauseInfos * sizeof(AttrNumber));
@@ -383,7 +501,7 @@ InitHash(YbBatchedNestLoopState *bnlstate)
 		YbBuildTupleHashTableExt(&bnlstate->js.ps, outer_tdesc,
 								 num_hashClauseInfos, keyexprs, tab_eq_fn,
 								 eqFuncOids, bnlstate->hashFunctions,
-								 GetBatchSize(plan), 0,
+								 GetMaxBatchSize(plan), 0,
 								 econtext->ecxt_per_query_memory, tablecxt,
 								 econtext->ecxt_per_tuple_memory, econtext,
 								 false);
@@ -402,7 +520,7 @@ FlushTupleHash(YbBatchedNestLoopState *bnlstate, ExprContext *econtext)
 		bnlstate->hashiterinit = true;
 		bnlstate->current_hash_entry = NULL;
 	}
-	
+
 	/* Find the current/next bucket that we'll be using */
 	TupleHashEntry entry = bnlstate->current_hash_entry;
 	if (entry == NULL)
@@ -478,12 +596,12 @@ GetNewOuterTupleHash(YbBatchedNestLoopState *bnlstate, ExprContext *econtext)
 		bnlstate->current_ht_tuple = btinfo;
 
 		Assert(data != NULL);
-		return true;	
+		return true;
 	}
 
-	/* 
+	/*
 	 * There are no more matches for the current inner tuple so reset
-	 * this bucket's state and return false. 
+	 * this bucket's state and return false.
 	 */
 	binfo->current = list_head(binfo->tuples);
 	return false;
@@ -605,7 +723,7 @@ FlushTupleTS(YbBatchedNestLoopState *bnlstate, ExprContext *econtext)
 	Assert(bnlstate->bnl_tupleStoreState != NULL);
 	while (bnlstate->bnl_batchTupNo < tuplestore_tuple_count(bnlstate->bnl_tupleStoreState))
 	{
-		ListCell *lc = list_nth_cell(bnlstate->bnl_batchMatchedInfo, 
+		ListCell *lc = list_nth_cell(bnlstate->bnl_batchMatchedInfo,
 								 	 bnlstate->bnl_batchTupNo);
 		if (lfirst_int(lc) == 0)
 		{
@@ -623,7 +741,7 @@ RegisterOuterMatchTS(YbBatchedNestLoopState *bnlstate, ExprContext *econtext)
 {
 	Assert(bnlstate->bnl_tupleStoreState != NULL);
 	(void) econtext;
-	ListCell *lc = list_nth_cell(bnlstate->bnl_batchMatchedInfo, 
+	ListCell *lc = list_nth_cell(bnlstate->bnl_batchMatchedInfo,
 								 bnlstate->bnl_batchTupNo - 1);
 	lc->data.int_value = 1;
 	return;
@@ -649,7 +767,7 @@ GetNewOuterTupleTS(YbBatchedNestLoopState *bnlstate, ExprContext *econtext)
 		 */
 		if (bnlstate->js.single_match)
 		{
-			ListCell *lc = list_nth_cell(bnlstate->bnl_batchMatchedInfo, 
+			ListCell *lc = list_nth_cell(bnlstate->bnl_batchMatchedInfo,
 								 		 current_tup_no);
 			if (lfirst_int(lc) > 0)
 			{
@@ -704,18 +822,22 @@ CreateBatch(YbBatchedNestLoopState *bnlstate, ExprContext *econtext)
 	PlanState  *innerPlan = innerPlanState(bnlstate);
 	LOCAL_JOIN_FN(FreeBatch, bnlstate);
 
-	for (int batchno = 0; batchno < GetBatchSize(batchnl); batchno++)
+	bool have_outer_tuple = false;
+
+	for (int batchno = 0; batchno < GetMaxBatchSize(batchnl); batchno++)
 	{
 		elog(DEBUG2, "getting new outer tuple");
-		if (!bnlstate->bnl_outerdone)
+		if (batchno < GetCurrentBatchSize(bnlstate) &&
+			!bnlstate->bnl_outerdone)
 		{
 			outerTupleSlot = ExecProcNode(outerPlan);
 			/*
-			 * We want to wrap up our current batch if the outerPlan has just been
-			 * exhausted but don't want future invocations of CreateBatch to attempt
-			 * ExecProcNode on outerPlan.
+			 * We want to wrap up our current batch if the outerPlan has
+			 * just been exhausted but don't want future invocations of
+			 * CreateBatch to attempt ExecProcNode on outerPlan.
 			 */
-			bnlstate->bnl_outerdone = TupIsNull(outerTupleSlot);
+			have_outer_tuple = !TupIsNull(outerTupleSlot);
+			bnlstate->bnl_outerdone = !have_outer_tuple;
 		}
 
 		/*
@@ -731,9 +853,20 @@ CreateBatch(YbBatchedNestLoopState *bnlstate, ExprContext *econtext)
 		}
 		else
 		{
-			elog(DEBUG2, "saving new outer tuple information");
-			econtext->ecxt_outertuple = outerTupleSlot;
-			LOCAL_JOIN_FN(AddTupleToOuterBatch, bnlstate, outerTupleSlot);
+			/*
+			 * It is possible for the outer side to not be done but we don't
+			 * have an outer tuple because our current batch size is smaller
+			 * than the table size. The first batch in a join with a pushed down
+			 * LIMIT could have a batch size smaller than the max batch size,
+			 * causing both bnlstate->bnl_outerdone and have_outer_tuple to be
+			 * false once that first batch is done.
+			 */
+			if (have_outer_tuple)
+			{
+				elog(DEBUG2, "saving new outer tuple information");
+				econtext->ecxt_outertuple = outerTupleSlot;
+				LOCAL_JOIN_FN(AddTupleToOuterBatch, bnlstate, outerTupleSlot);
+			}
 		}
 
 		/*
@@ -752,7 +885,7 @@ CreateBatch(YbBatchedNestLoopState *bnlstate, ExprContext *econtext)
 			Assert(IsA(nlp->paramval, Var));
 			Assert(nlp->paramval->varno == OUTER_VAR);
 			Assert(nlp->paramval->varattno > 0);
-			if (!bnlstate->bnl_outerdone)
+			if (have_outer_tuple)
 			{
 				prm->value = slot_getattr(outerTupleSlot,
 										  nlp->paramval->varattno,
@@ -773,24 +906,20 @@ CreateBatch(YbBatchedNestLoopState *bnlstate, ExprContext *econtext)
 	return true;
 }
 
-
 int
-GetBatchSize(YbBatchedNestLoop *plan)
+GetCurrentBatchSize(YbBatchedNestLoopState *bnlstate)
 {
-	if (plan->nl.nestParams == NULL)
-	{
-		return 1;
-	}
-
-	NestLoopParam *nlp =
-		(NestLoopParam *) linitial(plan->nl.nestParams);
-	return nlp->yb_batch_size;
+	return bnlstate->batch_size;
 }
 
-bool
-IsBatched(YbBatchedNestLoop *plan)
+int
+GetMaxBatchSize(YbBatchedNestLoop *plan)
 {
-	return GetBatchSize(plan) > 1;
+	Assert(list_length(plan->nl.nestParams) > 0);
+	NestLoopParam *nlp =
+		(NestLoopParam *) linitial(plan->nl.nestParams);
+	Assert(nlp->yb_batch_size > 1);
+	return nlp->yb_batch_size;
 }
 
 
@@ -817,8 +946,6 @@ ExecInitYbBatchedNestLoop(YbBatchedNestLoop *plan, EState *estate, int eflags)
 	bnlstate->js.ps.state = estate;
 	bnlstate->js.ps.ExecProcNode = ExecYbBatchedNestLoop;
 	bnlstate->bnl_tupleStoreState = NULL;
-
-	Assert(IsBatched(plan));
 
 	/*
 	 * Miscellaneous initialization
@@ -864,6 +991,8 @@ ExecInitYbBatchedNestLoop(YbBatchedNestLoop *plan, EState *estate, int eflags)
 	bnlstate->js.single_match = (plan->nl.join.inner_unique ||
 								plan->nl.join.jointype == JOIN_SEMI);
 
+	bnlstate->is_first_batch_done = false;
+
 	/* set up null tuples for outer joins, if needed */
 	switch (plan->nl.join.jointype)
 	{
@@ -893,7 +1022,12 @@ ExecInitYbBatchedNestLoop(YbBatchedNestLoop *plan, EState *estate, int eflags)
 	bnlstate->bnl_batchMatchedInfo = NIL;
 	bnlstate->bnl_batchTupNo = 0;
 	bnlstate->bnl_outerdone = false;
-	
+
+	/* Initialize sorting structures. */
+	bnlstate->bnl_needs_sorting = plan->numSortCols > 0;
+	bnlstate->bnl_is_sorted = false;
+	bnlstate->bnl_tuple_sort = NULL;
+
 	if (UseHash(plan, bnlstate))
 	{
 		InitHash(bnlstate);
@@ -934,6 +1068,7 @@ ExecEndYbBatchedNestLoop(YbBatchedNestLoopState *bnlstate)
 	NL1_printf("ExecEndYbBatchedNestLoop: %s\n",
 			   "ending node processing");
 
+	EndSorting(bnlstate);
 	LOCAL_JOIN_FN(End, bnlstate);
 
 	/*
@@ -971,7 +1106,10 @@ ExecReScanYbBatchedNestLoop(YbBatchedNestLoopState *bnlstate)
 	 */
 	if (outerPlan->chgParam == NULL)
 		ExecReScan(outerPlan);
-	
+
+	bnlstate->is_first_batch_done = false;
+	EndSorting(bnlstate);
+
 	LOCAL_JOIN_FN(FreeBatch, bnlstate);
 	bnlstate->bnl_outerdone = false;
 	bnlstate->bnl_currentstatus = BNL_INIT;

src/postgres/src/test/regress/expected/yb_join_batching.out
@@ -1101,17 +1101,16 @@ analyze q2;
 SET yb_bnl_batch_size = 3;
 -- Make sure a sort node is inserted above a batched NL join when appropriate
 explain (costs off) select q1.c1 from q1 join q2 on q1.c2 = q2.c2 order by q1.c1 limit 10;
-                            QUERY PLAN
--------------------------------------------------------------------
+                         QUERY PLAN
+-------------------------------------------------------------
  Limit
-   ->  Sort
-         Sort Key: q1.c1
-         ->  YB Batched Nested Loop Join
-               Join Filter: (q1.c2 = q2.c2)
-               ->  Index Scan using q1_pkey on q1
-               ->  Index Scan using q2_pkey on q2
-                     Index Cond: (c2 = ANY (ARRAY[q1.c2, $1, $2]))
-(8 rows)
+   ->  YB Batched Nested Loop Join
+         Join Filter: (q1.c2 = q2.c2)
+         Sort Keys: q1.c1
+         ->  Index Scan using q1_pkey on q1
+         ->  Index Scan using q2_pkey on q2
+               Index Cond: (c2 = ANY (ARRAY[q1.c2, $1, $2]))
+(7 rows)
 
 select q1.c1 from q1 join q2 on q1.c2 = q2.c2 order by q1.c1 limit 10;
  c1
@@ -1128,6 +1127,148 @@ select q1.c1 from q1 join q2 on q1.c2 = q2.c2 order by q1.c1 limit 10;
   4
 (10 rows)
 
+explain (costs off) select q1.c1 from q1 join q2 on q1.c2 = q2.c2 order by q1.c1 DESC limit 10;
+                         QUERY PLAN
+-------------------------------------------------------------
+ Limit
+   ->  YB Batched Nested Loop Join
+         Join Filter: (q1.c2 = q2.c2)
+         Sort Keys: q1.c1 DESC
+         ->  Index Scan Backward using q1_pkey on q1
+         ->  Index Scan using q2_pkey on q2
+               Index Cond: (c2 = ANY (ARRAY[q1.c2, $1, $2]))
+(7 rows)
+
+select q1.c1 from q1 join q2 on q1.c2 = q2.c2 order by q1.c1 DESC limit 10;
+ c1
+-----
+ 199
+ 199
+ 198
+ 198
+ 197
+ 197
+ 196
+ 196
+ 195
+ 195
+(10 rows)
+
+CREATE TABLE q1nulls (a int, b int);
+CREATE INDEX ON q1nulls (a ASC NULLS FIRST, b DESC NULLS LAST);
+INSERT INTO q1nulls SELECT i/10, i % 10 from generate_series(1, 100) i;
+INSERT INTO q1nulls VALUES (null, 9), (null, 8), (null, 8);
+EXPLAIN (COSTS OFF) SELECT q1nulls.a, q1nulls.b FROM q1nulls, q2 WHERE q1nulls.b = q2.c2 ORDER BY q1nulls.a ASC LIMIT 10;
+                              QUERY PLAN
+-----------------------------------------------------------------------
+ Limit
+   ->  Sort
+         Sort Key: q1nulls.a
+         ->  YB Batched Nested Loop Join
+               Join Filter: (q1nulls.b = q2.c2)
+               ->  Index Only Scan using q1nulls_a_b_idx on q1nulls
+               ->  Index Scan using q2_pkey on q2
+                     Index Cond: (c2 = ANY (ARRAY[q1nulls.b, $1, $2]))
+(8 rows)
+
+SELECT q1nulls.a, q1nulls.b FROM q1nulls, q2 WHERE q1nulls.b = q2.c2 ORDER BY q1nulls.a ASC LIMIT 10;
+ a | b
+---+---
+ 0 | 5
+ 0 | 6
+ 0 | 8
+ 0 | 8
+ 0 | 5
+ 0 | 7
+ 0 | 7
+ 0 | 9
+ 0 | 9
+ 0 | 6
+(10 rows)
+
+EXPLAIN (COSTS OFF) SELECT q1nulls.a, q1nulls.b FROM q1nulls, q2 WHERE q1nulls.b = q2.c2 ORDER BY q1nulls.a ASC NULLS FIRST LIMIT 10;
+                           QUERY PLAN
+-----------------------------------------------------------------
+ Limit
+   ->  YB Batched Nested Loop Join
+         Join Filter: (q1nulls.b = q2.c2)
+         Sort Keys: q1nulls.a NULLS FIRST
+         ->  Index Only Scan using q1nulls_a_b_idx on q1nulls
+         ->  Index Scan using q2_pkey on q2
+               Index Cond: (c2 = ANY (ARRAY[q1nulls.b, $1, $2]))
+(7 rows)
+
+SELECT q1nulls.a, q1nulls.b FROM q1nulls, q2 WHERE q1nulls.b = q2.c2 ORDER BY q1nulls.a ASC NULLS FIRST LIMIT 10;
+ a | b
+---+---
+   | 9
+   | 9
+   | 8
+   | 8
+   | 8
+   | 8
+ 0 | 7
+ 0 | 7
+ 0 | 9
+ 0 | 9
+(10 rows)
+
+EXPLAIN (COSTS OFF) SELECT q1nulls.a, q1nulls.b FROM q1nulls, q2 WHERE q1nulls.b = q2.c2 ORDER BY q1nulls.a ASC NULLS FIRST, q1nulls.b DESC LIMIT 10;
+                              QUERY PLAN
+-----------------------------------------------------------------------
+ Limit
+   ->  Sort
+         Sort Key: q1nulls.a NULLS FIRST, q1nulls.b DESC
+         ->  YB Batched Nested Loop Join
+               Join Filter: (q1nulls.b = q2.c2)
+               ->  Index Only Scan using q1nulls_a_b_idx on q1nulls
+               ->  Index Scan using q2_pkey on q2
+                     Index Cond: (c2 = ANY (ARRAY[q1nulls.b, $1, $2]))
+(8 rows)
+
+SELECT q1nulls.a, q1nulls.b FROM q1nulls, q2 WHERE q1nulls.b = q2.c2 ORDER BY q1nulls.a ASC NULLS FIRST, q1nulls.b DESC LIMIT 10;
+ a | b
+---+---
+   | 9
+   | 9
+   | 8
+   | 8
+   | 8
+   | 8
+ 0 | 9
+ 0 | 9
+ 0 | 8
+ 0 | 8
+(10 rows)
+
+EXPLAIN (COSTS OFF) SELECT q1nulls.a, q1nulls.b FROM q1nulls, q2 WHERE q1nulls.b = q2.c2 ORDER BY q1nulls.a ASC NULLS FIRST, q1nulls.b DESC NULLS LAST LIMIT 10;
+                             QUERY PLAN
+---------------------------------------------------------------------
+ Limit
+   ->  YB Batched Nested Loop Join
+         Join Filter: (q1nulls.b = q2.c2)
+         Sort Keys: q1nulls.a NULLS FIRST, q1nulls.b DESC NULLS LAST
+         ->  Index Only Scan using q1nulls_a_b_idx on q1nulls
+         ->  Index Scan using q2_pkey on q2
+               Index Cond: (c2 = ANY (ARRAY[q1nulls.b, $1, $2]))
+(7 rows)
+
+SELECT q1nulls.a, q1nulls.b FROM q1nulls, q2 WHERE q1nulls.b = q2.c2 ORDER BY q1nulls.a ASC NULLS FIRST, q1nulls.b DESC NULLS LAST LIMIT 10;
+ a | b
+---+---
+   | 9
+   | 9
+   | 8
+   | 8
+   | 8
+   | 8
+ 0 | 9
+ 0 | 9
+ 0 | 8
+ 0 | 8
+(10 rows)
+
+DROP TABLE q1nulls;
 create table q3(a int, b int, c name, primary key(a,b));
 create index q3_range on q3(a asc);
 /*+Set(enable_hashjoin off) Set(enable_mergejoin off) Set(yb_bnl_batch_size 3) Set(enable_seqscan off) Set(enable_material off)*/ explain (costs off) select * from q1 p1 left join (SELECT p2.c1 as a1, p3.a as a2 from q2 p2 join q3 p3 on true) j1 on j1.a1 = p1.c1;

src/postgres/src/test/regress/sql/yb_join_batching.sql
@@ -313,6 +313,27 @@ SET yb_bnl_batch_size = 3;
 explain (costs off) select q1.c1 from q1 join q2 on q1.c2 = q2.c2 order by q1.c1 limit 10;
 select q1.c1 from q1 join q2 on q1.c2 = q2.c2 order by q1.c1 limit 10;
 
+explain (costs off) select q1.c1 from q1 join q2 on q1.c2 = q2.c2 order by q1.c1 DESC limit 10;
+select q1.c1 from q1 join q2 on q1.c2 = q2.c2 order by q1.c1 DESC limit 10;
+
+CREATE TABLE q1nulls (a int, b int);
+CREATE INDEX ON q1nulls (a ASC NULLS FIRST, b DESC NULLS LAST);
+INSERT INTO q1nulls SELECT i/10, i % 10 from generate_series(1, 100) i;
+INSERT INTO q1nulls VALUES (null, 9), (null, 8), (null, 8);
+EXPLAIN (COSTS OFF) SELECT q1nulls.a, q1nulls.b FROM q1nulls, q2 WHERE q1nulls.b = q2.c2 ORDER BY q1nulls.a ASC LIMIT 10;
+SELECT q1nulls.a, q1nulls.b FROM q1nulls, q2 WHERE q1nulls.b = q2.c2 ORDER BY q1nulls.a ASC LIMIT 10;
+
+EXPLAIN (COSTS OFF) SELECT q1nulls.a, q1nulls.b FROM q1nulls, q2 WHERE q1nulls.b = q2.c2 ORDER BY q1nulls.a ASC NULLS FIRST LIMIT 10;
+SELECT q1nulls.a, q1nulls.b FROM q1nulls, q2 WHERE q1nulls.b = q2.c2 ORDER BY q1nulls.a ASC NULLS FIRST LIMIT 10;
+
+EXPLAIN (COSTS OFF) SELECT q1nulls.a, q1nulls.b FROM q1nulls, q2 WHERE q1nulls.b = q2.c2 ORDER BY q1nulls.a ASC NULLS FIRST, q1nulls.b DESC LIMIT 10;
+SELECT q1nulls.a, q1nulls.b FROM q1nulls, q2 WHERE q1nulls.b = q2.c2 ORDER BY q1nulls.a ASC NULLS FIRST, q1nulls.b DESC LIMIT 10;
+
+EXPLAIN (COSTS OFF) SELECT q1nulls.a, q1nulls.b FROM q1nulls, q2 WHERE q1nulls.b = q2.c2 ORDER BY q1nulls.a ASC NULLS FIRST, q1nulls.b DESC NULLS LAST LIMIT 10;
+SELECT q1nulls.a, q1nulls.b FROM q1nulls, q2 WHERE q1nulls.b = q2.c2 ORDER BY q1nulls.a ASC NULLS FIRST, q1nulls.b DESC NULLS LAST LIMIT 10;
+
+DROP TABLE q1nulls;
+
 create table q3(a int, b int, c name, primary key(a,b));
 create index q3_range on q3(a asc);
 

java/yb-pgsql/src/test/java/org/yb/pgsql/TestPgPrefetchControl.java
@@ -162,7 +162,7 @@ public class TestPgPrefetchControl extends BasePgSQLTest {
         "SELECT t1.a, t2.b FROM %s AS t1 JOIN %s AS t2 ON t1.a = t2.a LIMIT %d",
         tableName1, tableName2, limitCount);
     int innerTableRequests = 1;
-    int outerTableRequests = 4;
+    int outerTableRequests = 1;
 
     try (Statement statement = connection.createStatement()) {
       statement.execute(String.format(createStatement, tableName1));

commit 1709e5010a83f2b5f570eade50ea0bb603133980

[#20707] YSQL: Fix BNL first batch optimization logic for when the given limit is exactly the outer table's size
Summary:
While build the first outer batch, we don't reset `have_outer_tuple` to false every time before we try to get another tuple from the outer side. This tricks the batch-loading code into behaving incorrectly when the provisional first batch size is exactly the size of the outer table. When this happens, the code does not get a chance to receive a NULL tuple from the outer side to signify the outer side's end and does not get a chance to set `have_outer_tuple` to false.
To remedy this, we set `have_outer_tuple` to false before attempting to pull a tuple from the outer side and commented more of the surrounding batch-loading logic.

This handles a regression introduced by D30441/3af5fea4204a9776a421670fd60eb4472c40169a. This needs backports to 2.20 and 2.18.
Jira: DB-9711

Test Plan: ./yb_build.sh --java-test 'org.yb.pgsqlTestPgRegressJoin'

Reviewers: mtakahara

Reviewed By: mtakahara

Subscribers: yql

Differential Revision: https://phorge.dev.yugabyte.com/D31826

src/postgres/src/backend/executor/nodeYbBatchedNestloop.c
@@ -825,10 +825,9 @@ CreateBatch(YbBatchedNestLoopState *bnlstate, ExprContext *econtext)
 	PlanState  *innerPlan = innerPlanState(bnlstate);
 	LOCAL_JOIN_FN(FreeBatch, bnlstate);
 
-	bool have_outer_tuple = false;
-
 	for (int batchno = 0; batchno < GetMaxBatchSize(batchnl); batchno++)
 	{
+		bool have_outer_tuple = false;
 		elog(DEBUG2, "getting new outer tuple");
 		if (batchno < GetCurrentBatchSize(bnlstate) &&
 			!bnlstate->bnl_outerdone)
@@ -857,12 +856,12 @@ CreateBatch(YbBatchedNestLoopState *bnlstate, ExprContext *econtext)
 		else
 		{
 			/*
-			 * It is possible for the outer side to not be done but we don't
-			 * have an outer tuple because our current batch size is smaller
-			 * than the table size. The first batch in a join with a pushed down
-			 * LIMIT could have a batch size smaller than the max batch size,
-			 * causing both bnlstate->bnl_outerdone and have_outer_tuple to be
-			 * false once that first batch is done.
+			 * have_outer_tuple can be false even if bnlstate->bnl_outerdone
+			 * is false if the pushed down LIMIT leads our first batch to be
+			 * smaller than the outer table size. While we decrease the size
+			 * of our first batch, we can't decrease the size of the inner
+			 * index scan's corresponding IN list and must fill in the remaining
+			 * values with NULLs.
 			 */
 			if (have_outer_tuple)
 			{

src/postgres/src/test/regress/expected/yb_join_batching.out
@@ -1387,6 +1387,42 @@ explain (costs off) SELECT * FROM q1, q2 where pg_backend_pid() >= 0 and q1.c1 =
 DROP TABLE q1;
 DROP TABLE q2;
 DROP TABLE q3;
+create table ss1(a int, primary key(a asc));
+insert into ss1 select generate_series(1,5);
+create table ss2(a int, b int, primary key(a asc, b asc));
+insert into ss2 select i, i from generate_series(1,5) i;
+insert into ss2 select i, i+1 from generate_series(1,5) i;
+analyze ss1;
+analyze ss2;
+/*+Set(enable_seqscan off) Set(yb_bnl_batch_size 1024) Leading((ss1 ss2))*/ explain (costs off) select * from ss1, ss2 where ss1.a = ss2.a order by ss1.a limit 10;
+                               QUERY PLAN
+------------------------------------------------------------------------
+ Limit
+   ->  YB Batched Nested Loop Join
+         Join Filter: (ss1.a = ss2.a)
+         Sort Keys: ss1.a
+         ->  Index Scan using ss1_pkey on ss1
+         ->  Index Scan using ss2_pkey on ss2
+               Index Cond: (a = ANY (ARRAY[ss1.a, $1, $2, ..., $1023]))
+(7 rows)
+
+/*+Set(enable_seqscan off) Set(yb_bnl_batch_size 1024) Leading((ss1 ss2))*/ select * from ss1, ss2 where ss1.a = ss2.a order by ss1.a limit 10;
+ a | a | b
+---+---+---
+ 1 | 1 | 1
+ 1 | 1 | 2
+ 2 | 2 | 2
+ 2 | 2 | 3
+ 3 | 3 | 3
+ 3 | 3 | 4
+ 4 | 4 | 4
+ 4 | 4 | 5
+ 5 | 5 | 5
+ 5 | 5 | 6
+(10 rows)
+
+drop table ss1;
+drop table ss2;
 create table q1(a int, b int);
 create table q2(a int, b int);
 create index on q2(b, a);

src/postgres/src/test/regress/sql/yb_join_batching.sql
@@ -365,6 +365,19 @@ DROP TABLE q1;
 DROP TABLE q2;
 DROP TABLE q3;
 
+create table ss1(a int, primary key(a asc));
+insert into ss1 select generate_series(1,5);
+create table ss2(a int, b int, primary key(a asc, b asc));
+insert into ss2 select i, i from generate_series(1,5) i;
+insert into ss2 select i, i+1 from generate_series(1,5) i;
+analyze ss1;
+analyze ss2;
+/*+Set(enable_seqscan off) Set(yb_bnl_batch_size 1024) Leading((ss1 ss2))*/ explain (costs off) select * from ss1, ss2 where ss1.a = ss2.a order by ss1.a limit 10;
+/*+Set(enable_seqscan off) Set(yb_bnl_batch_size 1024) Leading((ss1 ss2))*/ select * from ss1, ss2 where ss1.a = ss2.a order by ss1.a limit 10;
+
+drop table ss1;
+drop table ss2;
+
 create table q1(a int, b int);
 create table q2(a int, b int);
 create index on q2(b, a);