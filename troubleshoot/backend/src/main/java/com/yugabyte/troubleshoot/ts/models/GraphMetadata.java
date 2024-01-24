62aaebf6e12b74d889a16884d483c8177c6f85f4
Aleksandr Malyshev <amalyshev@yugabyte.com>
2024-01-24 19:47:17 +0300

    [PLAT-12261] Troubleshooting controller with mocked data1
    Summary: Inital version of mocked TS anomaly + graphs controller.
    
    Test Plan: Tested manually.
    
    Reviewers: cdavid, rmadhavan
    
    Reviewed By: rmadhavan
    
    Subscribers: yugaware
    
    Differential Revision: https://phorge.dev.yugabyte.com/D31885

---
+++ troubleshoot/backend/src/main/java/com/yugabyte/troubleshoot/ts/models/GraphMetadata.java
@@ -0,0 +1,19 @@
+package com.yugabyte.troubleshoot.ts.models;
+
+import java.util.List;
+import java.util.Map;
+import lombok.Data;
+
+@Data
+public class GraphMetadata {
+  private String name;
+  private Double threshold;
+  private Map<GraphFilter, List<String>> filters;
+
+  public enum GraphFilter {
+    queryId,
+    dbId,
+    universeUuid,
+    instanceName
+  }
+}
