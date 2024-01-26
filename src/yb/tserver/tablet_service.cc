yugabyte/yugabyte-db

commit 7f52d0190487ae127c3d95a3d34c0a99b82f1b02

[BACKPORT 2.20][#18195] DocDB: Include fast path txns as part of pg_locks
Summary:
Original commit: ec3bce1b9cb1c1ca4d190675a00bb6f83557105e / D30625
For pg_locks, we query all status tablets for old transactions and their involved tablets, and then query the involved tablets for info of granted/awaiting locks corresponding to these transactions. Since Single Shard transactions (fast-path transactions) aren't tracked by the coordinator, they were being skipped as part of pg_locks.

We now enhance pg_locks by including the results from single shard waiters as well. Single Shard waiters are registered with the local waiting transaction registry at the corresponding tserver. When broadcasting requests to all status tablets for old transactions, we also broadcast requests to all tservers for old single shard waiters. We then filter out the oldest `yb_locks_max_transactions` transactions and send lock status requests to all the involved tablets. If the oldest transactions contain single shard waiter(s), we set `max_single_shard_waiter_start_time_us` for the `GetLockStatusRequestPB` to the latest start time across all the oldest transactions.

At the involved tablets, we scan through the single shard waiters and return all single shard waiters with start time <= `max_single_shard_waiter_start_time_us`. It could happen that a single shard waiter gets restarted in the meanwhile, thus changing the wait start time, and leading to lesser transactions being returned as part of `pg_locks` than `yb_locks_max_transactions`, despite presence of active single shard waiters. This would be addressed as part of https://github.com/yugabyte/yugabyte-db/issues/20120.

Note that since a single shard transaction doesn't write to intentsdb, we would never observe granted locks for single shard waiters in pg_locks.
Jira: DB-7217

**Upgrade/Rollback safety:**

src/yb/tserver/tablet_service.cc
@@ -1229,6 +1229,20 @@ void TabletServiceImpl::GetOldTransactions(const GetOldTransactionsRequestPB* re
   });
 }
 
+void TabletServiceImpl::GetOldSingleShardWaiters(const GetOldSingleShardWaitersRequestPB* req,
+                                                 GetOldSingleShardWaitersResponsePB* resp,
+                                                rpc::RpcContext context) {
+  TRACE("GetOldSingleShardWaiters");
+
+  auto s =
+      server_->tablet_manager()->waiting_txn_registry()->GetOldSingleShardWaiters(*req, resp);
+  if (s.ok()) {
+    context.RespondSuccess();
+  } else {
+    SetupErrorAndRespond(resp->mutable_error(), s, &context);
+  }
+}
+
 void TabletServiceImpl::GetTransactionStatusAtParticipant(
     const GetTransactionStatusAtParticipantRequestPB* req,
     GetTransactionStatusAtParticipantResponsePB* resp,
@@ -2873,10 +2887,12 @@ void TabletServiceImpl::GetLockStatus(const GetLockStatusRequestPB* req,
           }
           transactions.emplace(std::make_pair(*id_or_status, *aborted_subtxns_or_status));
         }
-        s = tablet_peer->shared_tablet()->GetLockStatus(transactions, tablet_lock_info);
+        s = tablet_peer->shared_tablet()->GetLockStatus(
+            transactions, tablet_lock_info, req->max_single_shard_waiter_start_time_us());
       } else {
         DCHECK(!limit_resp_to_txns.empty());
-        s = tablet_peer->shared_tablet()->GetLockStatus(limit_resp_to_txns, tablet_lock_info);
+        s = tablet_peer->shared_tablet()->GetLockStatus(
+            limit_resp_to_txns, tablet_lock_info, req->max_single_shard_waiter_start_time_us());
       }
       if (!s.ok()) {
         resp->Clear();

src/yb/docdb/local_waiting_txn_registry.cc
@@ -66,6 +66,12 @@ struct WaitingTransactionData {
   rpc::Rpcs::Handle rpc_handle;
 };
 
+// Container for metadata corresponding to a single shard waiting transaction that is being tracked.
+struct WaitingSingleShardTxnData {
+  const TabletId tablet_id = "";
+  uint64_t wait_start_us;
+};
+
 // Container for holding StatusTabletData and it's corresponding ThreadPoolToken. StatusTabletData
 // instance is automatically destroyed when there are no active waiter transactions waiting on it
 // and all shared_ptr references from oustanding RPCs have been released. The corresponding
@@ -246,8 +252,18 @@ class LocalWaitingTxnRegistry::Impl {
       return registry_->RegisterWaitingFor(waiting, std::move(blockers), status_tablet, this);
     }
 
-    void SetData(std::shared_ptr<WaitingTransactionData>&& blocked_data) {
+    Status RegisterSingleShardWaiter(
+        const TabletId& tablet_id, uint64_t wait_start_us) override {
+      return registry_->RegisterSingleShardWaiter(
+          tablet_id, wait_start_us, this);
+    }
+
+    void SetWaitingTxnData(std::shared_ptr<WaitingTransactionData>&& blocked_data) {
       blocked_data_ = std::move(blocked_data);
+    }
+
+    void SetWaitingSingleShardTxnData(std::shared_ptr<WaitingSingleShardTxnData>&& txn_data) {
+      txn_data_ = std::move(txn_data);
     }
 
     int64 GetDataUseCount() const override {
@@ -257,6 +273,7 @@ class LocalWaitingTxnRegistry::Impl {
    private:
     LocalWaitingTxnRegistry::Impl* const registry_;
     std::shared_ptr<WaitingTransactionData> blocked_data_ = nullptr;
+    std::shared_ptr<WaitingSingleShardTxnData> txn_data_ = nullptr;
   };
 
   std::unique_ptr<WaitingTransactionDataWrapper> Create() {
@@ -287,6 +304,38 @@ class LocalWaitingTxnRegistry::Impl {
         it = status_tablets_.erase(it);
       }
     }
+  }
+
+  Status GetOldSingleShardWaiters(
+      const tserver::GetOldSingleShardWaitersRequestPB& req,
+      tserver::GetOldSingleShardWaitersResponsePB* resp) EXCLUDES(mutex_) {
+    UniqueLock<decltype(mutex_)> l(mutex_);
+    if (shutting_down_) {
+      return STATUS(ShutdownInProgress, "Local Waiting Transaction Registry is shutting down.");
+    }
+    auto min_age = req.min_txn_age_ms() * 1ms;
+    auto now = clock_->Now();
+    for (auto it = single_shard_waiters_txn_data_.begin();
+         it != single_shard_waiters_txn_data_.end();) {
+      if (static_cast<uint32_t>(resp->txn_size()) >= req.max_num_txns()) {
+        break;
+      }
+      auto waiting_txn_data = it->lock();
+      if (!waiting_txn_data) {
+        it = single_shard_waiters_txn_data_.erase(it);
+        continue;
+      }
+      auto age = (now.GetPhysicalValueMicros() - waiting_txn_data->wait_start_us) * 1us;
+      if (age <= min_age) {
+        break;
+      }
+      // TODO(#20116): Populate host node uuid for single shard waiters.
+      auto* resp_txn = resp->add_txn();
+      resp_txn->set_start_time(waiting_txn_data->wait_start_us);
+      resp_txn->set_tablet(waiting_txn_data->tablet_id);
+      it++;
+    }
+    return Status::OK();
   }
 
   void StartShutdown() {
@@ -374,7 +423,27 @@ class LocalWaitingTxnRegistry::Impl {
     // See: https://github.com/yugabyte/yugabyte-db/issues/13576
     RETURN_NOT_OK(shared_tablet_data->SendPartialUpdateAsync(blocked_data, clock_->Now()));
 
-    wrapper->SetData(std::move(blocked_data));
+    wrapper->SetWaitingTxnData(std::move(blocked_data));
+    return Status::OK();
+  }
+
+  Status RegisterSingleShardWaiter(
+      const TabletId& tablet_id, uint64_t wait_start_us,
+      WaitingTransactionDataWrapper* wrapper) EXCLUDES(mutex_) {
+    UniqueLock<decltype(mutex_)> l(mutex_);
+    if (shutting_down_) {
+      return STATUS(ShutdownInProgress, "Local Waiting Transaction Registry is shutting down.");
+    }
+
+    auto txn_data = std::make_shared<WaitingSingleShardTxnData>(WaitingSingleShardTxnData {
+      .tablet_id = tablet_id,
+      .wait_start_us = wait_start_us,
+    });
+    single_shard_waiters_txn_data_.insert(txn_data);
+    // Move 'txn_data' to the wrapper so that it gets destroyed as soon as the there is no strong
+    // reference to the single shard waiter. That way, we'll be able to track all active single
+    // shard waiters at the local waiting transaction registry.
+    wrapper->SetWaitingSingleShardTxnData(std::move(txn_data));
     return Status::OK();
   }
 
@@ -395,6 +464,22 @@ class LocalWaitingTxnRegistry::Impl {
   const std::string tserver_uuid_;
 
   ThreadPool* thread_pool_;
+  struct WaitingSingleShardTxnDataComparator {
+    bool operator()(
+        const std::weak_ptr<WaitingSingleShardTxnData>& lhs,
+        const std::weak_ptr<WaitingSingleShardTxnData>& rhs) const {
+      auto shared_lhs = lhs.lock();
+      auto shared_rhs = rhs.lock();
+      if (shared_lhs && shared_rhs) {
+        if (shared_lhs->wait_start_us != shared_rhs->wait_start_us) {
+          return shared_lhs->wait_start_us < shared_rhs->wait_start_us;
+        }
+      }
+      return &lhs < &rhs;
+    }
+  };
+  std::set<std::weak_ptr<WaitingSingleShardTxnData>, WaitingSingleShardTxnDataComparator>
+      single_shard_waiters_txn_data_ GUARDED_BY(mutex_);
 };
 
 LocalWaitingTxnRegistry::LocalWaitingTxnRegistry(
@@ -410,6 +495,12 @@ std::unique_ptr<ScopedWaitingTxnRegistration> LocalWaitingTxnRegistry::Create() 
 
 void LocalWaitingTxnRegistry::SendWaitForGraph() {
   impl_->SendWaitForGraph();
+}
+
+Status LocalWaitingTxnRegistry::GetOldSingleShardWaiters(
+    const tserver::GetOldSingleShardWaitersRequestPB& req,
+    tserver::GetOldSingleShardWaitersResponsePB* resp) {
+  return impl_->GetOldSingleShardWaiters(req, resp);
 }
 
 void LocalWaitingTxnRegistry::StartShutdown() {

src/yb/docdb/local_waiting_txn_registry.h
@@ -16,6 +16,7 @@
 #include "yb/client/client.h"
 #include "yb/docdb/wait_queue.h"
 #include "yb/server/server_fwd.h"
+#include "yb/tserver/tserver_service.fwd.h"
 
 namespace yb {
 namespace docdb {
@@ -37,13 +38,15 @@ class LocalWaitingTxnRegistry : public WaitingTxnRegistry {
   // registered wait-for relationship.
   std::unique_ptr<docdb::ScopedWaitingTxnRegistration> Create() override;
 
-  Status RegisterWaitingFor(
-      const TransactionId& waiting, std::shared_ptr<ConflictDataManager> blockers,
-      const TabletId& status_tablet_id, docdb::ScopedWaitingTxnRegistration* wrapper);
-
   // Triggers a report of all wait-for relationships tracked by this instance to each waiting
   // transaction's coordinator.
   void SendWaitForGraph();
+
+  // Populates old single shard waiting transactions and their metadata (involved tablet,
+  // start time) based on the arguments in the request. Used by pg_client_service to determine
+  // which transactions to display in pg_locks/yb_lock_status.
+  Status GetOldSingleShardWaiters(const tserver::GetOldSingleShardWaitersRequestPB& req,
+                                  tserver::GetOldSingleShardWaitersResponsePB* resp);
 
   void StartShutdown();
 

src/yb/docdb/wait_queue.cc
@@ -1016,12 +1016,18 @@ class WaitQueue::Impl {
     // TODO(wait-queues): similar to pg, we can wait 1s or so before beginning deadlock detection.
     // See https://github.com/yugabyte/yugabyte-db/issues/13576
     auto scoped_reporter = waiting_txn_registry_->Create();
-    if (!waiter_txn_id.IsNil()) {
+    if (waiter_txn_id.IsNil()) {
       // If waiter_txn_id is Nil, then we're processing a single-shard transaction. We do not have
       // to report single shard transactions to transaction coordinators because they can't
       // possibly be involved in a deadlock. This is true because no transactions can wait on
       // single shard transactions, so they only have out edges in the wait-for graph and cannot
       // be a part of a cycle.
+      //
+      // We still register the single shard waiters with the local waiting transaction registry
+      // for the purpose of observability in pg_locks.
+      RETURN_NOT_OK(scoped_reporter->RegisterSingleShardWaiter(
+          txn_status_manager_->tablet_id(), request_start_us));
+    } else {
       DCHECK(!status_tablet_id.empty());
       // TODO(wait-queues): Instead of moving blockers to local_waiting_txn_registry, we could store
       // the blockers in the waiter_data record itself. That way, we could avoid re-running conflict
@@ -1352,6 +1358,7 @@ class WaitQueue::Impl {
   }
 
   Status GetLockStatus(const std::map<TransactionId, SubtxnSet>& transactions,
+                       uint64_t max_single_shard_waiter_start_time_us,
                        const TableInfoProvider& table_info_provider,
                        TransactionLockInfoManager* lock_info_manager) const {
     std::vector<WaiterLockStatusInfo> waiter_lock_entries;
@@ -1359,9 +1366,9 @@ class WaitQueue::Impl {
       SharedLock l(mutex_);
       // If the wait-queue is being shutdown, waiter_status_ would  be empty. No need to
       // explicitly check 'shutting_down_' and return.
-
-      if (transactions.empty()) {
-        // When transactions is empty, return awaiting locks info of all waiters.
+      if (transactions.empty() && max_single_shard_waiter_start_time_us == 0) {
+        // When transactions is empty and the max start time of single shard waiters isn't set,
+        // return awaiting locks info of all waiters.
         for (const auto& [_, waiter_data] : waiter_status_) {
           waiter_lock_entries.push_back(waiter_data->GetWaiterLockStatusInfo());
         }
@@ -1373,6 +1380,11 @@ class WaitQueue::Impl {
           const auto& it = waiter_status_.find(txn_id);
           if (it != waiter_status_.end()) {
             waiter_lock_entries.push_back(it->second->GetWaiterLockStatusInfo());
+          }
+        }
+        for (const auto& waiter_data : single_shard_waiters_) {
+          if (waiter_data->request_start_us <= max_single_shard_waiter_start_time_us) {
+            waiter_lock_entries.push_back(waiter_data->GetWaiterLockStatusInfo());
           }
         }
       }
@@ -1414,8 +1426,8 @@ class WaitQueue::Impl {
           .types = lock_batch_entry.intent_types,
           .doc_ht = Slice(),
         };
-        // TODO(pglocks): Populate 'subtransaction_id' & 'is_explicit' info of waiter txn(s) in
-        // the LockInfoPB response. Currently we don't track either for waiter txn(s).
+        // TODO(pglocks): Populate 'is_explicit' info of waiter txn(s) in the LockInfoPB response.
+        // Currently we don't track it for waiter txn(s).
         auto* lock = waiter_info->add_locks();
         RETURN_NOT_OK(docdb::PopulateLockInfoFromParsedIntent(
             parsed_intent, DecodedIntentValue{}, table_info_provider, lock,
@@ -1746,9 +1758,11 @@ void WaitQueue::SignalPromoted(const TransactionId& id, TransactionStatusResult&
 }
 
 Status WaitQueue::GetLockStatus(const std::map<TransactionId, SubtxnSet>& transactions,
+                                uint64_t max_single_shard_waiter_start_time_us,
                                 const TableInfoProvider& table_info_provider,
                                 TransactionLockInfoManager* lock_info_manager) const {
-  return impl_->GetLockStatus(transactions, table_info_provider, lock_info_manager);
+  return impl_->GetLockStatus(
+      transactions, max_single_shard_waiter_start_time_us, table_info_provider, lock_info_manager);
 }
 
 }  // namespace docdb

src/yb/docdb/wait_queue.h
@@ -36,6 +36,9 @@ class ScopedWaitingTxnRegistration {
     const TransactionId& waiting,
     std::shared_ptr<ConflictDataManager> blockers,
     const TabletId& status_tablet) = 0;
+  virtual Status RegisterSingleShardWaiter(
+      const TabletId& tablet_id,
+      uint64_t wait_start_us) = 0;
   virtual int64 GetDataUseCount() const = 0;
   virtual ~ScopedWaitingTxnRegistration() = default;
 };
@@ -122,6 +125,7 @@ class WaitQueue {
   // from this wait queue. If transactions is not empty, restrict returned information to locks
   // which are requested by the given set of transactions.
   Status GetLockStatus(const std::map<TransactionId, SubtxnSet>& transactions,
+                       uint64_t max_single_shard_waiter_start_time_us,
                        const TableInfoProvider& table_info_provider,
                        TransactionLockInfoManager* lock_info_manager) const;
 

src/yb/tablet/tablet.cc
@@ -4452,7 +4452,8 @@ Status PopulateLockInfoFromIntent(
 }
 
 Status Tablet::GetLockStatus(const std::map<TransactionId, SubtxnSet>& transactions,
-                             TabletLockInfoPB* tablet_lock_info) const {
+                             TabletLockInfoPB* tablet_lock_info,
+                             uint64_t max_single_shard_waiter_start_time_us) const {
   if (metadata_->table_type() != PGSQL_TABLE_TYPE) {
     return STATUS_FORMAT(
         InvalidArgument, "Cannot get lock status for non YSQL table $0", metadata_->table_id());
@@ -4553,7 +4554,8 @@ Status Tablet::GetLockStatus(const std::map<TransactionId, SubtxnSet>& transacti
 
   const auto& wait_queue = transaction_participant()->wait_queue();
   if (wait_queue) {
-    RETURN_NOT_OK(wait_queue->GetLockStatus(transactions, *this, &lock_info_manager));
+    RETURN_NOT_OK(wait_queue->GetLockStatus(
+        transactions, max_single_shard_waiter_start_time_us, *this, &lock_info_manager));
   }
 
   return Status::OK();

src/yb/tablet/tablet.h
@@ -887,7 +887,8 @@ class Tablet : public AbstractTablet,
   // present in the associated aborted subtxns.
   Status GetLockStatus(
       const std::map<TransactionId, SubtxnSet>& transactions,
-      TabletLockInfoPB* tablet_lock_info) const;
+      TabletLockInfoPB* tablet_lock_info,
+      uint64_t max_single_shard_waiter_start_time_us) const;
 
   // The returned SchemaPackingProvider lives only as long as this.
   docdb::SchemaPackingProvider& GetSchemaPackingProvider();

commit d21b72123aafa31c6dfcbe9a59f49c77996b604b

[BACKPORT 2.20][#20772] DocDB: Address pg_locks segv issues in release builds
Summary:
Commit https://github.com/yugabyte/yugabyte-db/commit/7f52d0190487ae127c3d95a3d34c0a99b82f1b02 caused segv issues for 2.20 release builds alone due to the following code snippet.
```
auto s =
      server_->tablet_manager()->waiting_txn_registry()->GetOldSingleShardWaiters(*req, resp);
```

Addressing the above issue by explicitly checking values of `FLAGS_enable_wait_queues` and `FLAGS_disable_deadlock_detection` flags.
Jira: DB-9769

Test Plan:
./yb_build.sh release --cxx-test pgwrapper_pg_tablet_split-test --gtest_filter PgLocksTabletSplitTest.TestPgLocksSplitAfterFetchingParentLocation
./yb_build.sh release --cxx-test pgwrapper_pg_tablet_split-test --gtest_filter PgLocksTabletSplitTest.TestPgLocks
./yb_build.sh release --java-test 'org.yb.pgsql.TestPgRegressProc#testPgRegressProc'

Reviewers: rsami, rthallam, tvesely

Reviewed By: rsami

Subscribers: bogdan

Tags: #jenkins-ready

Differential Revision: https://phorge.dev.yugabyte.com/D31942

src/yb/tserver/tablet_service.cc
@@ -1253,8 +1253,14 @@ void TabletServiceImpl::GetOldSingleShardWaiters(const GetOldSingleShardWaitersR
                                                 rpc::RpcContext context) {
   TRACE("GetOldSingleShardWaiters");
 
-  auto s =
-      server_->tablet_manager()->waiting_txn_registry()->GetOldSingleShardWaiters(*req, resp);
+  auto* waiting_txn_registry = server_->tablet_manager()->waiting_txn_registry();
+  if (!waiting_txn_registry) {
+    LOG(INFO) << "Skipping query for old single shard waiters as the server's "
+              << "waiting_txn_registry doesn't exist.";
+    context.RespondSuccess();
+    return;
+  }
+  auto s = waiting_txn_registry->GetOldSingleShardWaiters(*req, resp);
   if (s.ok()) {
     context.RespondSuccess();
   } else {