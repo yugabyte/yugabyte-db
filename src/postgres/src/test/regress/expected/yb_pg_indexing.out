yugabyte/yugabyte-db

commit 3bc10b296108ff0c768774ddac040317e170ba58

[BACKPORT 2.20][#19360] YSQL: Issue a notice for unsafe ALTER TABLE operations
Summary:
Table rewrite operations (ALTER primary key, ALTER TYPE) currently do not acquire a table lock
and are therefore unsafe (concurrent writes may be dropped). This diff adds a `NOTICE` to
explicitly indicate the same when executing these commands.'

The `NOTICE` can be suppressed by setting the `ysql_suppress_unsafe_alter_notice` tserver gflag to true.
Jira: DB-8160

Original commit: 0baeed21d9a6531070f570d47b7dbc2dcbc9a839 / D29430

Test Plan: Jenkins: java only

Reviewers: jason

Reviewed By: jason

Tags: #jenkins-ready

Differential Revision: https://phorge.dev.yugabyte.com/D31134

src/postgres/src/backend/commands/tablecmds.c
@@ -17397,6 +17397,17 @@ YbATCloneTableAndGetMappings(CreateStmt *create_stmt, const Relation old_rel,
 							 AttrNumber **new2old_attmap,
 							 bool ignore_type_mismatch)
 {
+
+	if (!YBSuppressUnsafeAlterNotice())
+		ereport(NOTICE,
+				(errmsg("table rewrite may lead to inconsistencies"),
+				 errdetail("Concurrent DMLs may not be reflected in the new"
+						   " table."),
+				 errhint("See https://github.com/yugabyte/yugabyte-db/issues/"
+						 "19860. Set 'ysql_suppress_unsafe_alter_notice'"
+						 " yb-tserver gflag to true to suppress this"
+						 " notice.")));
+
 	/* clang-format on */
 	ObjectAddress address =
 		DefineRelation(create_stmt, RELKIND_RELATION, old_rel->rd_rel->relowner,

src/postgres/src/common/pg_yb_common.c
@@ -126,6 +126,17 @@ YBUnsupportedFeatureSignalLevel()
 }
 
 bool
+YBSuppressUnsafeAlterNotice()
+{
+	static int cached_value = -1;
+	if (cached_value == -1) {
+		cached_value = YBCIsEnvVarTrue(
+			"FLAGS_ysql_suppress_unsafe_alter_notice");
+	}
+	return cached_value;
+}
+
+bool
 YBIsNonTxnCopyEnabled()
 {
 	static int cached_value = -1;

src/postgres/src/include/common/pg_yb_common.h
@@ -79,6 +79,11 @@ extern bool YBIsUsingYBParser();
 extern int YBUnsupportedFeatureSignalLevel();
 
 /**
+ * Returns whether unsafe ALTER notice should be suppressed.
+ */
+extern bool YBSuppressUnsafeAlterNotice();
+
+/**
  * Returns whether non-transactional COPY gflag is enabled.
  */
 extern bool YBIsNonTxnCopyEnabled();

src/postgres/src/test/regress/expected/yb_alter_table.out
@@ -309,6 +309,9 @@ SELECT * FROM dummy;
 
 -- Verify that missing values work after table rewrite.
 ALTER TABLE foo ADD PRIMARY KEY (a);
+NOTICE:  table rewrite may lead to inconsistencies
+DETAIL:  Concurrent DMLs may not be reflected in the new table.
+HINT:  See https://github.com/yugabyte/yugabyte-db/issues/19860. Set 'ysql_suppress_unsafe_alter_notice' yb-tserver gflag to true to suppress this notice.
 SELECT * FROM foo ORDER BY a;
  a | b |        c        |            d             | e  
 ---+---+-----------------+--------------------------+----

src/postgres/src/test/regress/expected/yb_collate_icu_utf8.out
@@ -1224,6 +1224,7 @@ $$;
 create event trigger event_rewrite on table_rewrite
   execute procedure trig_rewrite();
 ALTER TABLE tab1 ALTER COLUMN id SET DATA TYPE varchar(5) collate "en-US-x-icu"; -- rewrite
+NOTICE:  table rewrite may lead to inconsistencies
 NOTICE:  rewriting table
 \d tab1
                     Table "collate_tests.tab1"

src/postgres/src/test/regress/expected/yb_feature_alter_table.out
@@ -275,24 +275,36 @@ drop table atacc1;
 -- test dropping primary key constraints
 CREATE TABLE with_simple_pk_i (i int PRIMARY KEY);
 ALTER TABLE with_simple_pk_i DROP CONSTRAINT with_simple_pk_i_pkey;
+NOTICE:  table rewrite may lead to inconsistencies
+DETAIL:  Concurrent DMLs may not be reflected in the new table.
+HINT:  See https://github.com/yugabyte/yugabyte-db/issues/19860. Set 'ysql_suppress_unsafe_alter_notice' yb-tserver gflag to true to suppress this notice.
 INSERT INTO with_simple_pk_i VALUES (1);
 INSERT INTO with_simple_pk_i VALUES (1);
 DROP TABLE with_simple_pk_i;
 --
 CREATE TABLE with_simple_pk_ij (i int, j int, PRIMARY KEY(i, j));
 ALTER TABLE with_simple_pk_ij DROP CONSTRAINT with_simple_pk_ij_pkey;
+NOTICE:  table rewrite may lead to inconsistencies
+DETAIL:  Concurrent DMLs may not be reflected in the new table.
+HINT:  See https://github.com/yugabyte/yugabyte-db/issues/19860. Set 'ysql_suppress_unsafe_alter_notice' yb-tserver gflag to true to suppress this notice.
 INSERT INTO with_simple_pk_ij VALUES (1, 1);
 INSERT INTO with_simple_pk_ij VALUES (1, 1);
 DROP TABLE with_simple_pk_ij;
 --
 CREATE TABLE with_named_pk_i (i int CONSTRAINT named_pk_i PRIMARY KEY);
 ALTER TABLE with_named_pk_i DROP CONSTRAINT named_pk_i;
+NOTICE:  table rewrite may lead to inconsistencies
+DETAIL:  Concurrent DMLs may not be reflected in the new table.
+HINT:  See https://github.com/yugabyte/yugabyte-db/issues/19860. Set 'ysql_suppress_unsafe_alter_notice' yb-tserver gflag to true to suppress this notice.
 INSERT INTO with_named_pk_i VALUES (1);
 INSERT INTO with_named_pk_i VALUES (1);
 DROP TABLE with_named_pk_i;
 --
 CREATE TABLE with_named_pk_ij (i int, j int, CONSTRAINT named_pk_ij PRIMARY KEY (i HASH, j ASC));
 ALTER TABLE with_named_pk_ij DROP CONSTRAINT named_pk_ij;
+NOTICE:  table rewrite may lead to inconsistencies
+DETAIL:  Concurrent DMLs may not be reflected in the new table.
+HINT:  See https://github.com/yugabyte/yugabyte-db/issues/19860. Set 'ysql_suppress_unsafe_alter_notice' yb-tserver gflag to true to suppress this notice.
 INSERT INTO with_named_pk_ij VALUES (1, 1);
 INSERT INTO with_named_pk_ij VALUES (1, 1);
 DROP TABLE with_named_pk_ij;
@@ -732,18 +744,42 @@ select * from test_alter_column_type;
 alter table test_alter_column_type alter column a type varchar(1);
 alter table test_alter_column_type alter column a type varchar(5);
 alter table test_alter_column_type alter column a type varchar(1);
+NOTICE:  table rewrite may lead to inconsistencies
+DETAIL:  Concurrent DMLs may not be reflected in the new table.
+HINT:  See https://github.com/yugabyte/yugabyte-db/issues/19860. Set 'ysql_suppress_unsafe_alter_notice' yb-tserver gflag to true to suppress this notice.
 alter table test_alter_column_type alter column a type char(10);
+NOTICE:  table rewrite may lead to inconsistencies
+DETAIL:  Concurrent DMLs may not be reflected in the new table.
+HINT:  See https://github.com/yugabyte/yugabyte-db/issues/19860. Set 'ysql_suppress_unsafe_alter_notice' yb-tserver gflag to true to suppress this notice.
 alter table test_alter_column_type alter column b type name;
 alter table test_alter_column_type alter column b type varchar(100);
+NOTICE:  table rewrite may lead to inconsistencies
+DETAIL:  Concurrent DMLs may not be reflected in the new table.
+HINT:  See https://github.com/yugabyte/yugabyte-db/issues/19860. Set 'ysql_suppress_unsafe_alter_notice' yb-tserver gflag to true to suppress this notice.
 alter table test_alter_column_type alter column c type text;
 alter table test_alter_column_type alter column c type varchar(100);
+NOTICE:  table rewrite may lead to inconsistencies
+DETAIL:  Concurrent DMLs may not be reflected in the new table.
+HINT:  See https://github.com/yugabyte/yugabyte-db/issues/19860. Set 'ysql_suppress_unsafe_alter_notice' yb-tserver gflag to true to suppress this notice.
 alter table test_alter_column_type alter column d type char(1);
 alter table test_alter_column_type alter column d type varchar(100);
+NOTICE:  table rewrite may lead to inconsistencies
+DETAIL:  Concurrent DMLs may not be reflected in the new table.
+HINT:  See https://github.com/yugabyte/yugabyte-db/issues/19860. Set 'ysql_suppress_unsafe_alter_notice' yb-tserver gflag to true to suppress this notice.
 alter table test_alter_column_type alter column d type char(100);
+NOTICE:  table rewrite may lead to inconsistencies
+DETAIL:  Concurrent DMLs may not be reflected in the new table.
+HINT:  See https://github.com/yugabyte/yugabyte-db/issues/19860. Set 'ysql_suppress_unsafe_alter_notice' yb-tserver gflag to true to suppress this notice.
 alter table test_alter_column_type alter column e type varbit(1);
 alter table test_alter_column_type alter column e type varbit(5);
 alter table test_alter_column_type alter column e type varbit(1);
+NOTICE:  table rewrite may lead to inconsistencies
+DETAIL:  Concurrent DMLs may not be reflected in the new table.
+HINT:  See https://github.com/yugabyte/yugabyte-db/issues/19860. Set 'ysql_suppress_unsafe_alter_notice' yb-tserver gflag to true to suppress this notice.
 alter table test_alter_column_type alter column f type varchar(100);
+NOTICE:  table rewrite may lead to inconsistencies
+DETAIL:  Concurrent DMLs may not be reflected in the new table.
+HINT:  See https://github.com/yugabyte/yugabyte-db/issues/19860. Set 'ysql_suppress_unsafe_alter_notice' yb-tserver gflag to true to suppress this notice.
 alter table test_alter_column_type alter column f type varbit(100); --fails
 ERROR:  column "f" cannot be cast automatically to type bit varying
 HINT:  You might need to specify "USING f::bit varying(100)".
@@ -752,9 +788,15 @@ ERROR:  column "f" cannot be cast automatically to type integer
 HINT:  You might need to specify "USING f::integer".
 alter table test_alter_column_type alter column g type varchar;
 alter table test_alter_column_type alter column g type varchar(5);
+NOTICE:  table rewrite may lead to inconsistencies
+DETAIL:  Concurrent DMLs may not be reflected in the new table.
+HINT:  See https://github.com/yugabyte/yugabyte-db/issues/19860. Set 'ysql_suppress_unsafe_alter_notice' yb-tserver gflag to true to suppress this notice.
 alter table test_alter_column_type alter column g type text;
 alter table test_alter_column_type alter column h type varbit;
 alter table test_alter_column_type alter column h type varbit(5);
+NOTICE:  table rewrite may lead to inconsistencies
+DETAIL:  Concurrent DMLs may not be reflected in the new table.
+HINT:  See https://github.com/yugabyte/yugabyte-db/issues/19860. Set 'ysql_suppress_unsafe_alter_notice' yb-tserver gflag to true to suppress this notice.
 insert into test_alter_column_type values ('abcde', '-', '-', '-', B'10101', 0, '-', B'0'); --fails
 ERROR:  bit string too long for type bit varying(1)
 insert into test_alter_column_type values ('abcde', '-', '-', '-', B'1', 0, '-', B'0');
@@ -779,6 +821,9 @@ select * from test_alter_column_type order by a;
  h      | bit varying(5)         |           |          |
 
 alter table test_alter_column_type alter column a type varchar;
+NOTICE:  table rewrite may lead to inconsistencies
+DETAIL:  Concurrent DMLs may not be reflected in the new table.
+HINT:  See https://github.com/yugabyte/yugabyte-db/issues/19860. Set 'ysql_suppress_unsafe_alter_notice' yb-tserver gflag to true to suppress this notice.
 alter table test_alter_column_type alter column e type varbit;
 \d test_alter_column_type
               Table "public.test_alter_column_type"

src/postgres/src/test/regress/expected/yb_feature_colocation.out
@@ -557,6 +557,9 @@ Colocation: true
 
 INSERT INTO tbl_no_pk (k, v) VALUES (1, 1), (2, 2), (3, 3);
 ALTER TABLE tbl_no_pk ADD PRIMARY KEY (k ASC);
+NOTICE:  table rewrite may lead to inconsistencies
+DETAIL:  Concurrent DMLs may not be reflected in the new table.
+HINT:  See https://github.com/yugabyte/yugabyte-db/issues/19860. Set 'ysql_suppress_unsafe_alter_notice' yb-tserver gflag to true to suppress this notice.
 \d tbl_no_pk
              Table "public.tbl_no_pk"
  Column |  Type   | Collation | Nullable | Default
@@ -577,6 +580,9 @@ SELECT * FROM tbl_no_pk ORDER BY k;
 
 -- Alter colocated table "DROP PRIMARY KEY"
 ALTER TABLE tbl_no_pk DROP CONSTRAINT tbl_no_pk_pkey;
+NOTICE:  table rewrite may lead to inconsistencies
+DETAIL:  Concurrent DMLs may not be reflected in the new table.
+HINT:  See https://github.com/yugabyte/yugabyte-db/issues/19860. Set 'ysql_suppress_unsafe_alter_notice' yb-tserver gflag to true to suppress this notice.
 \d tbl_no_pk
              Table "public.tbl_no_pk"
  Column |  Type   | Collation | Nullable | Default

src/postgres/src/test/regress/expected/yb_pg_alter_table.out
@@ -614,6 +614,9 @@ CREATE TABLE fail_part (
 ALTER TABLE list_parted ATTACH PARTITION fail_part FOR VALUES IN (1);
 ERROR:  child table "fail_part" has different type for column "b"
 ALTER TABLE fail_part ALTER b TYPE char (2) COLLATE "POSIX";
+NOTICE:  table rewrite may lead to inconsistencies
+DETAIL:  Concurrent DMLs may not be reflected in the new table.
+HINT:  See https://github.com/yugabyte/yugabyte-db/issues/19860. Set 'ysql_suppress_unsafe_alter_notice' yb-tserver gflag to true to suppress this notice.
 -- Uncomment when ALTER TABLE COLLATE is supported (#1013)
 -- ALTER TABLE list_parted ATTACH PARTITION fail_part FOR VALUES IN (1);
 DROP TABLE fail_part;

commit 68cae37a02edcf564e0c66f18838e90d20546d27

[2.20][#20806] YSQL: Fix TestPgRegressIndex failures
Summary:
Fix TestPgRegressIndex failures introduced by commit 3bc10b296108ff0c768774ddac040317e170ba58
Jira: DB-9800

Test Plan: Jenkins: rebase: 2.20, test regex: .*TestPgRegressIndex.*

Reviewers: jason

Reviewed By: jason

Subscribers: jason, yql

Differential Revision: https://phorge.dev.yugabyte.com/D31986

src/postgres/src/test/regress/expected/yb_pg_indexing.out
@@ -1003,6 +1003,9 @@ drop table idxpart;
 CREATE TABLE idxpart (c1 INT PRIMARY KEY, c2 INT, c3 VARCHAR(10)) PARTITION BY RANGE(c1);
 CREATE TABLE idxpart1 (LIKE idxpart);
 ALTER TABLE idxpart1 ADD PRIMARY KEY (c1, c2);
+NOTICE:  table rewrite may lead to inconsistencies
+DETAIL:  Concurrent DMLs may not be reflected in the new table.
+HINT:  See https://github.com/yugabyte/yugabyte-db/issues/19860. Set 'ysql_suppress_unsafe_alter_notice' yb-tserver gflag to true to suppress this notice.
 ALTER TABLE idxpart ATTACH PARTITION idxpart1 FOR VALUES FROM (100) TO (200);
 ERROR:  multiple primary keys for table "idxpart1" are not allowed
 DROP TABLE idxpart, idxpart1;
@@ -1019,6 +1022,9 @@ drop table idxpart, idxpart1, idxpart11;
 create table idxpart (a int) partition by range (a);
 create table idxpart0 (like idxpart);
 alter table idxpart0 add primary key (a);
+NOTICE:  table rewrite may lead to inconsistencies
+DETAIL:  Concurrent DMLs may not be reflected in the new table.
+HINT:  See https://github.com/yugabyte/yugabyte-db/issues/19860. Set 'ysql_suppress_unsafe_alter_notice' yb-tserver gflag to true to suppress this notice.
 -- Uncomment when changing primary key of a partitioned table is implemented (#14196)
 /*
 alter table idxpart attach partition idxpart0 for values from (0) to (1000);
@@ -1390,6 +1396,9 @@ alter table parted_pk_detach_test1 drop constraint parted_pk_detach_test1_pkey;	
 ERROR:  cannot drop inherited constraint "parted_pk_detach_test1_pkey" of relation "parted_pk_detach_test1"
 alter table parted_pk_detach_test detach partition parted_pk_detach_test1;
 alter table parted_pk_detach_test1 drop constraint parted_pk_detach_test1_pkey;
+NOTICE:  table rewrite may lead to inconsistencies
+DETAIL:  Concurrent DMLs may not be reflected in the new table.
+HINT:  See https://github.com/yugabyte/yugabyte-db/issues/19860. Set 'ysql_suppress_unsafe_alter_notice' yb-tserver gflag to true to suppress this notice.
 drop table parted_pk_detach_test, parted_pk_detach_test1;
 create table parted_uniq_detach_test (a int unique) partition by list (a);
 create table parted_uniq_detach_test1 partition of parted_uniq_detach_test for values in (1);