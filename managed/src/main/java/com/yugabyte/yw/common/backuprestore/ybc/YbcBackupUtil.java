40692215027ba04d75a8f40c58279f8a32e5addb
vkumar <vkumar@yugabyte.com>
2024-01-30 15:45:01 +0000

    Revert "[PLAT-12460]Fix Table by table backup unintended behavior"
    Summary: This reverts commit af65833d3539750c23729798cf5f2457c4439089.
    
    Test Plan: No tests
    
    Reviewers: vpatibandla, skurapati
    
    Reviewed By: skurapati
    
    Subscribers: yugaware
    
    Differential Revision: https://phorge.dev.yugabyte.com/D32060

--- managed/src/main/java/com/yugabyte/yw/common/backuprestore/ybc/YbcBackupUtil.java
+++ managed/src/main/java/com/yugabyte/yw/common/backuprestore/ybc/YbcBackupUtil.java
@@ -505,7 +505,7 @@ public class YbcBackupUtil {
     BackupServiceTaskCreateRequest.Builder backupServiceTaskCreateRequestBuilder =
         backupServiceTaskCreateBuilder(taskID, namespaceType, extendedArgs);
     backupServiceTaskCreateRequestBuilder.setCsConfig(cloudStoreConfig);
-    if ((!backupTableParams.allTables || backupTableParams.tableByTableBackup)
+    if (!backupTableParams.allTables
         && CollectionUtils.isNotEmpty(backupTableParams.tableNameList)) {
       backupServiceTaskCreateRequestBuilder.setTbs(getTableBackupSpec(backupTableParams));
     } else {
