082f8a8e456f083745f8c7a952f1b5de992f11c1
Yury Shchetinin <yshchetinin@yugabyte.com>
2024-02-08 19:41:37 +0300

    [PLAT-10858] During upgrades sort the target nodes in the order of zones/read-replicas
    Summary:
    Added different sort order for tservers in rolling upgrades:
    1) smaller zones first
    2) taking all the nodes from each zone before proceeding to next zone
    
    Test Plan: sbt test
    
    Reviewers: cwang, nsingh
    
    Reviewed By: cwang, nsingh
    
    Subscribers: yugaware
    
    Differential Revision: https://phorge.dev.yugabyte.com/D32319

--- managed/src/test/java/com/yugabyte/yw/commissioner/tasks/upgrade/RebootUniverseTest.java
+++ managed/src/test/java/com/yugabyte/yw/commissioner/tasks/upgrade/RebootUniverseTest.java
@@ -10,17 +10,29 @@ import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 
 import com.google.common.collect.ImmutableList;
+import com.yugabyte.yw.common.ApiUtils;
 import com.yugabyte.yw.common.TestUtils;
+import com.yugabyte.yw.forms.UniverseDefinitionTaskParams;
 import com.yugabyte.yw.forms.UpgradeTaskParams;
 import com.yugabyte.yw.forms.UpgradeTaskParams.UpgradeOption;
+import com.yugabyte.yw.models.AvailabilityZone;
 import com.yugabyte.yw.models.CustomerTask;
 import com.yugabyte.yw.models.RuntimeConfigEntry;
 import com.yugabyte.yw.models.TaskInfo;
+import com.yugabyte.yw.models.Universe;
+import com.yugabyte.yw.models.helpers.NodeDetails;
 import com.yugabyte.yw.models.helpers.TaskType;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 import java.util.UUID;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.function.BiConsumer;
 import java.util.stream.Collectors;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -137,5 +149,59 @@ public class RebootUniverseTest extends UpgradeTaskTest {
         TaskType.RebootUniverse,
         taskParams,
         false);
+  }
+
+  @Test
+  public void testRollingRebootNodeOrder() {
+    Universe.saveDetails(
+        defaultUniverse.getUniverseUUID(),
+        u -> {
+          UniverseDefinitionTaskParams details = u.getUniverseDetails();
+          Set<NodeDetails> detailsSet = new HashSet<>();
+          AtomicInteger idx = new AtomicInteger();
+          BiConsumer<AvailabilityZone, Boolean> nodeCreator =
+              (az, isMaster) -> {
+                NodeDetails node = ApiUtils.getDummyNodeDetails(idx.incrementAndGet());
+                node.isMaster = isMaster;
+                node.azUuid = az.getUuid();
+                node.placementUuid = details.getPrimaryCluster().uuid;
+                detailsSet.add(node);
+              };
+
+          nodeCreator.accept(az1, true); // 1
+          nodeCreator.accept(az2, true); // 2
+          nodeCreator.accept(az3, true); // 3
+          nodeCreator.accept(az3, false); // 4
+          nodeCreator.accept(az3, false); // 5
+          nodeCreator.accept(az2, false); // 6
+          nodeCreator.accept(az1, false); // 7
+
+          details.nodeDetailsSet = detailsSet;
+          u.setUniverseDetails(details);
+        },
+        false);
+    UpgradeTaskParams taskParams = new UpgradeTaskParams();
+    taskParams.upgradeOption = UpgradeOption.ROLLING_UPGRADE;
+    TaskInfo taskInfo = submitTask(taskParams);
+    List<TaskInfo> subTasks = taskInfo.getSubTasks();
+
+    List<String> expected =
+        Arrays.asList(
+            // masters first
+            "host-n1",
+            "host-n3",
+            "host-n2", // leader
+            "host-n6",
+            "host-n7",
+            "host-n4",
+            "host-n5");
+
+    List<String> nodeNames = new ArrayList<>();
+    for (TaskInfo subTask : subTasks) {
+      if (subTask.getTaskType() == TaskType.RebootServer) {
+        nodeNames.add(subTask.getDetails().get("nodeName").textValue());
+      }
+    }
+    Assert.assertEquals(expected, nodeNames);
   }
 }
