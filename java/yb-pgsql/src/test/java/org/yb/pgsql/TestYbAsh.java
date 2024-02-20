yugabyte/yugabyte-db

commit e7e0824b63d0d62cd582a53aade88c6b43fb336d

[#19136] YSQL, ASH: Set query id in ASH metadata for utility statements
Summary:
`pg_stat_statements` generates a query id for utility statements using the
redacted query string, but it doesn't set this query id in the internal
data structures. Although, it still fills up the hash table with this query id, so when
we query `pg_stat_statements`, we can see that the query id is non-zero
for utility statements as well.

Previously, ASH used whatever query id was set by `pg_stat_statements`
for its metadata. But it doesn't work in case of utility statements as it is
always zero.

This diff recalculates the query id if it is zero in the same way that
`pg_stat_statements` does, and sets it in the ASH metadata.
Jira: DB-7934

Test Plan: ./yb_build.sh --java-test TestYbAsh#testUtilityStatementsQueryId

Reviewers: jason

Reviewed By: jason

Subscribers: hbhanawat, yql, amitanand

Differential Revision: https://phorge.dev.yugabyte.com/D31784

src/postgres/src/backend/utils/misc/yb_ash.c
@@ -26,6 +26,7 @@
 
 #include "yb_ash.h"
 
+#include "access/hash.h"
 #include "executor/executor.h"
 #include "funcapi.h"
 #include "miscadmin.h"
@@ -74,8 +75,9 @@ typedef struct YbAsh
 static YbAsh *yb_ash = NULL;
 
 static int yb_ash_cb_max_entries(void);
-static void yb_set_ash_metadata(uint64_t query_id);
+static void yb_set_ash_metadata(uint64 query_id);
 static void yb_unset_ash_metadata();
+static uint64 yb_ash_utility_query_id(const char *str, int len);
 static void YbAshAcquireBufferLock(bool exclusive);
 static void YbAshReleaseBufferLock();
 
@@ -191,8 +193,20 @@ yb_ash_post_parse_analyze(ParseState *pstate, Query *query)
 	if (prev_post_parse_analyze_hook)
 		prev_post_parse_analyze_hook(pstate, query);
 
-	/* query_id will be set to zero if pg_stat_statements is disabled. */
-	yb_set_ash_metadata(query->queryId);
+	/*
+	 * query->queryId will be zero when it's a utility statement, in that case
+	 * pg_stat_statements calculates a query id based on the redacted query
+	 * string and stores that on it's hash table, but it doesn't set it in
+	 * query->queryId. We need to store the same query id in ASH metadata,
+	 * so that we can join the ASH view with pg_stat_statements.
+	 * yb_ash_utility_query_id calculates the query id in the same way that
+	 * pg_stat_statements does. query_id can also be zero when pg_stat_statements
+	 * is disabled, then this field won't be useful for ASH users at all.
+	 */
+	uint64 query_id = query->queryId != 0
+					  ? query->queryId
+					  : yb_ash_utility_query_id(pstate->p_sourcetext, query->stmt_len);
+	yb_set_ash_metadata(query_id);
 }
 
 static void
@@ -203,7 +217,13 @@ yb_ash_ExecutorStart(QueryDesc *queryDesc, int eflags)
 	 * We set the ASH metadata here if it's not been set yet.
 	 */
 	if (MyProc->yb_is_ash_metadata_set == false)
-		yb_set_ash_metadata(queryDesc->plannedstmt->queryId);
+	{
+		uint64 query_id = queryDesc->plannedstmt->queryId != 0
+						  ? queryDesc->plannedstmt->queryId
+						  : yb_ash_utility_query_id(queryDesc->sourceText,
+					   								queryDesc->plannedstmt->stmt_len);
+		yb_set_ash_metadata(query_id);
+	}
 
 	if (prev_ExecutorStart)
 		prev_ExecutorStart(queryDesc, eflags);
@@ -250,7 +270,7 @@ yb_ash_ProcessUtility(PlannedStmt *pstmt, const char *queryString,
 }
 
 static void
-yb_set_ash_metadata(uint64_t query_id)
+yb_set_ash_metadata(uint64 query_id)
 {
 	LWLockAcquire(&MyProc->yb_ash_metadata_lock, LW_EXCLUSIVE);
 
@@ -273,6 +293,21 @@ yb_unset_ash_metadata()
 	MyProc->yb_is_ash_metadata_set = false;
 
 	LWLockRelease(&MyProc->yb_ash_metadata_lock);
+}
+
+/*
+ * Calculate the query id for utility statements like pg_stat_statements.
+ */
+static uint64
+yb_ash_utility_query_id(const char *str, int len)
+{
+	const char *redacted_query;
+	int			redacted_query_len;
+
+	Assert(str != NULL);
+	YbGetRedactedQueryString(str, len, &redacted_query, &redacted_query_len);
+	return DatumGetUInt64(hash_any_extended((const unsigned char *) redacted_query,
+											redacted_query_len, 0));
 }
 
 static void

java/yb-pgsql/src/test/java/org/yb/pgsql/TestYbAsh.java
@@ -56,6 +56,8 @@ public class TestYbAsh extends BasePgSQLTest {
    */
   @Test
   public void testAshViewWithoutEnablingAsh() throws Exception {
+    // We need to restart the cluster because ASH may already have been enabled
+    restartCluster();
     try (Statement statement = connection.createStatement()) {
       runInvalidQuery(statement, "SELECT * FROM " + ASH_VIEW,
           "TEST_yb_enable_ash gflag must be enabled");
@@ -128,6 +130,28 @@ public class TestYbAsh extends BasePgSQLTest {
       int res = getSingleRow(statement, "SELECT COUNT(*) FROM " + ASH_VIEW +
           " WHERE wait_event_component='TServer'").getLong(0).intValue();
       assertGreaterThan(res, 0);
+    }
+  }
+
+  /**
+   * Query id of utility statements shouldn't be zero
+   */
+  @Test
+  public void testUtilityStatementsQueryId() throws Exception {
+    setAshConfigAndRestartCluster(100, ASH_SAMPLE_SIZE);
+    try (Statement statement = connection.createStatement()) {
+      statement.execute("CREATE TABLE test_table1(k INT)");
+      statement.execute("ALTER TABLE test_table1 ADD value TEXT");
+      statement.execute("ALTER TABLE test_table1 RENAME TO test_table2");
+      for (int i = 0; i < 10; ++i) {
+        statement.execute(String.format("INSERT INTO test_table2 VALUES(%d, 'v-%d')", i, i));
+      }
+      statement.execute("TRUNCATE TABLE test_table2");
+      statement.execute("DROP TABLE test_table2");
+      // TODO: remove wait_event_component='Postgres' once ASH metadata is propagated to
+      // TServer
+      assertOneRow(statement, "SELECT COUNT(*) FROM " + ASH_VIEW + " WHERE query_id = 0 " +
+          "AND wait_event_component='Postgres'", 0);
     }
   }
 }

src/postgres/contrib/pg_stat_statements/pg_stat_statements.c
@@ -1566,9 +1566,7 @@ pgss_store(const char *query, uint64 queryId,
 		query_len--;
 
 	/* Use the redacted query for checking purposes. */
-	redacted_query = pnstrdup(query, query_len);
-	redacted_query = RedactPasswordIfExists(redacted_query);
-	redacted_query_len = strlen(redacted_query);
+	YbGetRedactedQueryString(query, query_len, &redacted_query, &redacted_query_len);
 
 	/*
 	 * For utility statements, we just hash the query string to get an ID.

src/postgres/src/backend/utils/misc/pg_yb_utils.c
@@ -4391,3 +4391,12 @@ YbSortOrdering(SortByDir ordering, bool is_colocated, bool is_tablegroup,
 
 	return ordering;
 }
+
+void
+YbGetRedactedQueryString(const char* query, int query_len,
+						 const char** redacted_query, int* redacted_query_len)
+{
+	*redacted_query = pnstrdup(query, query_len);
+	*redacted_query = RedactPasswordIfExists(*redacted_query);
+	*redacted_query_len = strlen(*redacted_query);
+}

src/postgres/src/include/pg_yb_utils.h
@@ -1076,4 +1076,7 @@ extern void YbIndexSetNewRelfileNode(Relation indexRel, Oid relfileNodeId,
  */
 extern SortByDir YbSortOrdering(SortByDir ordering, bool is_colocated, bool is_tablegroup, bool is_first_key);
 
+extern void YbGetRedactedQueryString(const char* query, int query_len,
+									 const char** redacted_query, int* redacted_query_len);
+
 #endif /* PG_YB_UTILS_H */

commit 1caa3dad53765551642ebd5543001e9fa5e77aa6

[#21030] YSQL, ASH: Fix parse error while executing extension sql scripts
Summary:
D31784 / e7e0824b63d0d62cd582a53aade88c6b43fb336d introduced
a bug where the statements inside extension scripts had incorrect offset and
incorrect length which led to syntax errors due to incorrect parsing when ASH
was enabled.

This diff fixes the offset of the query just like pg_stat_statements.
Jira: DB-10001

Test Plan: ./yb_build.sh --java-test TestYbAsh#testNestedQueriesWithAsh

Reviewers: jason

Reviewed By: jason

Subscribers: hbhanawat, yql, amitanand

Differential Revision: https://phorge.dev.yugabyte.com/D32349

src/postgres/src/backend/utils/misc/yb_ash.c
@@ -31,6 +31,7 @@
 #include "funcapi.h"
 #include "miscadmin.h"
 #include "parser/analyze.h"
+#include "parser/scansup.h"
 #include "pgstat.h"
 #include "postmaster/bgworker.h"
 #include "storage/ipc.h"
@@ -77,7 +78,8 @@ static YbAsh *yb_ash = NULL;
 static int yb_ash_cb_max_entries(void);
 static void yb_set_ash_metadata(uint64 query_id);
 static void yb_unset_ash_metadata();
-static uint64 yb_ash_utility_query_id(const char *str, int len);
+static uint64 yb_ash_utility_query_id(const char *query, int query_len,
+									  int query_location);
 static void YbAshAcquireBufferLock(bool exclusive);
 static void YbAshReleaseBufferLock();
 
@@ -205,7 +207,8 @@ yb_ash_post_parse_analyze(ParseState *pstate, Query *query)
 	 */
 	uint64 query_id = query->queryId != 0
 					  ? query->queryId
-					  : yb_ash_utility_query_id(pstate->p_sourcetext, query->stmt_len);
+					  : yb_ash_utility_query_id(pstate->p_sourcetext, query->stmt_len,
+					  							query->stmt_location);
 	yb_set_ash_metadata(query_id);
 }
 
@@ -218,10 +221,12 @@ yb_ash_ExecutorStart(QueryDesc *queryDesc, int eflags)
 	 */
 	if (MyProc->yb_is_ash_metadata_set == false)
 	{
+		/* Query id can be zero here only if pg_stat_statements is disabled */
 		uint64 query_id = queryDesc->plannedstmt->queryId != 0
 						  ? queryDesc->plannedstmt->queryId
 						  : yb_ash_utility_query_id(queryDesc->sourceText,
-					   								queryDesc->plannedstmt->stmt_len);
+					   								queryDesc->plannedstmt->stmt_len,
+													queryDesc->plannedstmt->stmt_location);
 		yb_set_ash_metadata(query_id);
 	}
 
@@ -296,16 +301,46 @@ yb_unset_ash_metadata()
 }
 
 /*
- * Calculate the query id for utility statements like pg_stat_statements.
+ * Calculate the query id for utility statements. This takes parts of pgss_store
+ * from pg_stat_statements.
  */
 static uint64
-yb_ash_utility_query_id(const char *str, int len)
+yb_ash_utility_query_id(const char *query, int query_len, int query_location)
 {
 	const char *redacted_query;
 	int			redacted_query_len;
 
-	Assert(str != NULL);
-	YbGetRedactedQueryString(str, len, &redacted_query, &redacted_query_len);
+	Assert(query != NULL);
+
+	if (query_location >= 0)
+	{
+		Assert(query_location <= strlen(query));
+		query += query_location;
+		/* Length of 0 (or -1) means "rest of string" */
+		if (query_len <= 0)
+			query_len = strlen(query);
+		else
+			Assert(query_len <= strlen(query));
+	}
+	else
+	{
+		/* If query location is unknown, distrust query_len as well */
+		query_location = 0;
+		query_len = strlen(query);
+	}
+
+	/*
+	 * Discard leading and trailing whitespace, too.  Use scanner_isspace()
+	 * not libc's isspace(), because we want to match the lexer's behavior.
+	 */
+	while (query_len > 0 && scanner_isspace(query[0]))
+		query++, query_location++, query_len--;
+	while (query_len > 0 && scanner_isspace(query[query_len - 1]))
+		query_len--;
+
+	/* Use the redacted query for checking purposes. */
+	YbGetRedactedQueryString(query, query_len, &redacted_query, &redacted_query_len);
+
 	return DatumGetUInt64(hash_any_extended((const unsigned char *) redacted_query,
 											redacted_query_len, 0));
 }

java/yb-pgsql/src/test/java/org/yb/pgsql/TestYbAsh.java
@@ -154,4 +154,42 @@ public class TestYbAsh extends BasePgSQLTest {
           "AND wait_event_component='Postgres'", 0);
     }
   }
+
+  /**
+   * Sanity check that nested queries work with ASH enabled
+   */
+  @Test
+  public void testNestedQueriesWithAsh() throws Exception {
+    setAshConfigAndRestartCluster(ASH_SAMPLING_INTERVAL, ASH_SAMPLE_SIZE);
+    try (Statement statement = connection.createStatement()) {
+      String tableName = "test_table";
+
+      // Queries inside extension scripts
+      statement.execute("DROP EXTENSION IF EXISTS pg_stat_statements");
+      statement.execute("CREATE EXTENSION pg_stat_statements");
+
+      // Queries inside functions
+      statement.execute("CREATE TABLE " + tableName + "(k INT, v TEXT)");
+      statement.execute("CREATE FUNCTION insert_into_table(k INT, v TEXT) " +
+          "RETURNS void AS $$ INSERT INTO " + tableName + " VALUES($1, $2) $$ " +
+          "LANGUAGE SQL");
+
+      for (int i = 0; i < 10; ++i) {
+        statement.execute(String.format("SELECT insert_into_table(%d, 'v-%d')", i, i));
+      }
+
+      // Queries inside triggers
+      statement.execute("TRUNCATE " + tableName);
+      statement.execute("CREATE FUNCTION trigger_fn() " +
+          "RETURNS TRIGGER AS $$ BEGIN UPDATE test_table SET v = '1' " +
+          "WHERE k = 1; RETURN NEW; END; $$ LANGUAGE plpgsql");
+      statement.execute("CREATE TRIGGER trig AFTER INSERT ON test_table " +
+          "FOR EACH STATEMENT EXECUTE PROCEDURE trigger_fn()");
+
+      for (int i = 0; i < 10; ++i) {
+        statement.execute(String.format("INSERT INTO %s VALUES(%d, 'v-%d')",
+            tableName, i, i));
+      }
+    }
+  }
 }