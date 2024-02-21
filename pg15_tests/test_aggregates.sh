9581fe53929d679782f6102ae48eaecec8c9b11c
Jason Kim <git@jasonk.me>
2024-02-20 16:31:20 -0800

    [pg15] test: fix regress test yb_pg_aggregates
    Summary:
    Fix output differences in yb_pg_aggregates to get it to pass.
    
    - Change some error messages: upstream PG
      83884682f4df96184549b91869a1cf79dafb4f94
    - Change some output ordering: upstream PG
      d97b714a219959a50f9e7b37ded674f5132f93f3
    
    Update pg15_tests/test_aggregates.sh to no longer expect
    yb_pg_aggregates to fail.  yb_aggregates still fails, mainly due to lack
    of pg_hint_plan support.
    
    Test Plan:
    On Almalinux 8:
    
        #!/usr/bin/env bash
        ./yb_build.sh fastdebug --gcc11
        pg15_tests/test_aggregates.sh fastdebug --gcc11
    
    Depends on D32548
    Jenkins: skip
    
    Reviewers: aagrawal, tfoucher, tvesely
    
    Reviewed By: tfoucher
    
    Subscribers: yql
    
    Differential Revision: https://phorge.dev.yugabyte.com/D32549

--- pg15_tests/test_aggregates.sh
+++ pg15_tests/test_aggregates.sh
@@ -3,5 +3,5 @@ source "${BASH_SOURCE[0]%/*}"/common.sh
 
 failing_java_test TestPgRegressAggregates
 grep_in_java_test \
-  "failed tests: [yb_aggregates, yb_pg_aggregates]" \
+  "failed tests: [yb_aggregates]" \
   TestPgRegressAggregates
