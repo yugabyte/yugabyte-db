97f6ed8e4c658df95cc05284de5af179512805e3
Daniel Shubin <dshubin@yugabyte.com>
2024-02-23 00:56:27 +0000

    [PLAT-12321] Feature flag for release decoupling
    Summary:
    Added a new feature flag to allow enabling the release decoupling
    work. Also wrote a wrapper around ReleaseMetadata and Release to allow
    ReleaseManager and its clients to be able to easily work with
    both implementations.
    
    Test Plan:
    with the feature flag disabled:
    1. create universe (vm and k8s)
    2. upgrade univers (vm and k8s)
    3. add new releases
    4. delete releases
    
    no testing with flag enabled, it is not yet supported
    
    Reviewers: muthu, anijhawan
    
    Reviewed By: muthu
    
    Subscribers: yugaware
    
    Differential Revision: https://phorge.dev.yugabyte.com/D32653

--- managed/src/main/java/com/yugabyte/yw/common/NodeManager.java
+++ managed/src/main/java/com/yugabyte/yw/common/NodeManager.java
@@ -838,26 +838,23 @@ public class NodeManager extends DevopsBase {
     String ybServerPackage = null, ybcPackage = null, ybcDir = null;
     Map<String, String> ybcFlags = new HashMap<>();
     if (taskParam.ybSoftwareVersion != null) {
-      ReleaseManager.ReleaseMetadata releaseMetadata =
-          releaseManager.getReleaseByVersion(taskParam.ybSoftwareVersion);
-      if (releaseMetadata != null) {
+      ReleaseContainer release = releaseManager.getReleaseByVersion(taskParam.ybSoftwareVersion);
+      if (release != null) {
         if (arch != null) {
-          ybServerPackage = releaseMetadata.getFilePath(arch);
+          ybServerPackage = release.getFilePath(arch);
         } else {
           // Fallback to region in case arch is not present
-          ybServerPackage = releaseMetadata.getFilePath(taskParam.getRegion());
+          ybServerPackage = release.getFilePath(taskParam.getRegion());
         }
-        if (releaseMetadata.s3 != null && releaseMetadata.s3.paths.x86_64.equals(ybServerPackage)) {
+        if (release.isS3Download(ybServerPackage)) {
           subcommand.add("--s3_remote_download");
-        } else if (releaseMetadata.gcs != null
-            && releaseMetadata.gcs.paths.x86_64.equals(ybServerPackage)) {
+        } else if (release.isGcsDownload(ybcPackage)) {
           subcommand.add("--gcs_remote_download");
-        } else if (releaseMetadata.http != null
-            && releaseMetadata.http.paths.x86_64.equals(ybServerPackage)) {
+        } else if (release.isHttpDownload(ybServerPackage)) {
           subcommand.add("--http_remote_download");
-          if (StringUtils.isNotBlank(releaseMetadata.http.paths.x86_64_checksum)) {
+          if (StringUtils.isNotBlank(release.getHttpChecksum())) {
             subcommand.add("--http_package_checksum");
-            subcommand.add(releaseMetadata.http.paths.x86_64_checksum.toLowerCase());
+            subcommand.add(release.getHttpChecksum());
           }
         }
       }
@@ -2042,7 +2039,7 @@ public class NodeManager extends DevopsBase {
           if (nodeTaskParam.deviceInfo != null) {
             commandArgs.addAll(getDeviceArgs(nodeTaskParam));
           }
-          sensitiveData.putAll(getReleaseSensitiveData(taskParam));
+          sensitiveData.putAll(getReleaseSensitiveData(taskParam, arch));
           String localPackagePath = getThirdpartyPackagePath();
           if (localPackagePath != null) {
             commandArgs.add("--local_package_path");
@@ -2634,17 +2631,18 @@ public class NodeManager extends DevopsBase {
     }
   }
 
-  private Map<String, String> getReleaseSensitiveData(AnsibleConfigureServers.Params taskParam) {
+  private Map<String, String> getReleaseSensitiveData(
+      AnsibleConfigureServers.Params taskParam, Architecture arch) {
     Map<String, String> data = new HashMap<>();
     if (taskParam.ybSoftwareVersion != null) {
-      ReleaseManager.ReleaseMetadata releaseMetadata =
+      ReleaseContainer releaseContainer =
           releaseManager.getReleaseByVersion(taskParam.ybSoftwareVersion);
-      if (releaseMetadata != null) {
-        if (releaseMetadata.s3 != null) {
-          data.put("--aws_access_key", releaseMetadata.s3.accessKeyId);
-          data.put("--aws_secret_key", releaseMetadata.s3.secretAccessKey);
-        } else if (releaseMetadata.gcs != null) {
-          data.put("--gcs_credentials_json", releaseMetadata.gcs.credentialsJson);
+      if (releaseContainer != null) {
+        if (releaseContainer.isAws(arch)) {
+          data.put("--aws_access_key", releaseContainer.getAwsAccessKey(arch));
+          data.put("--aws_secret_key", releaseContainer.getAwsSecretKey(arch));
+        } else if (releaseContainer.isGcs(arch)) {
+          data.put("--gcs_credentials_json", releaseContainer.getGcsCredentials(arch));
         }
       }
     }
@@ -2707,13 +2705,12 @@ public class NodeManager extends DevopsBase {
 
   public String getYbServerPackageName(String ybSoftwareVersion, Region region, Architecture arch) {
     String ybServerPackage = null;
-    ReleaseManager.ReleaseMetadata releaseMetadata =
-        releaseManager.getReleaseByVersion(ybSoftwareVersion);
-    if (releaseMetadata != null) {
+    ReleaseContainer release = releaseManager.getReleaseByVersion(ybSoftwareVersion);
+    if (release != null) {
       if (arch != null) {
-        ybServerPackage = releaseMetadata.getFilePath(arch);
+        ybServerPackage = release.getFilePath(arch);
       } else {
-        ybServerPackage = releaseMetadata.getFilePath(region);
+        ybServerPackage = release.getFilePath(region);
       }
     }
     return ybServerPackage;
