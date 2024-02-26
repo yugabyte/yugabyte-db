3e13e2b8bf138506eb3d68244db65b70e2161a4d
Muthu Chidambaram <mchidambaram@yugabyte.com>
2024-02-26 16:30:23 -0600

    [PLAT-12855] Updating Almalinux 8 images
    Summary:
    Issue with GPG key on older Almalinux 8 default images we had. This diff updates the generate script
    to be a little easier to use (and gets rid of Azure ARM support) and reruns it with 8.9 as the
    desired version.
    
    Test Plan:
    create universe on aws/gcp/azure, ensure create universe succeeds and new packages
    (glibc-langpack-en) can be downloaded on the OS.
    
    Reviewers: svarshney, prarabdh.garg
    
    Reviewed By: prarabdh.garg
    
    Subscribers: nbhatia, yugaware
    
    Differential Revision: https://phorge.dev.yugabyte.com/D32652

--- managed/devops/opscli/ybops/scripts/generateMetadataFiles.py
+++ managed/devops/opscli/ybops/scripts/generateMetadataFiles.py
@@ -13,9 +13,9 @@ YB_LISCENCE = '''
 
 '''
 
-AWS_OS_VERSION = "8.8"
-GCP_OS_VERSION = "8.7"
-AZU_OS_VERSION = "8.5"
+AWS_OS_VERSION = "8.9"
+GCP_OS_VERSION = "8"
+AZU_OS_VERSION = "8-gen2"
 
 AWS_METADATA_FILENAME = "aws-metadata.yml"
 GCP_METADATA_FILENAME = "gcp-metadata.yml"
@@ -40,21 +40,21 @@ AWS_X86_CMD = 'aws ec2 describe-images \
 GCP_X86_CMD = f'gcloud compute images \
                 --project almalinux-cloud list \
                     --filter="architecture="X86_64" AND \
-                              family="almalinux-{GCP_OS_VERSION.split(".")[0]}"" \
+                              family="almalinux-{GCP_OS_VERSION}"" \
                     --format=json'
 
 AZU_X86_CMD = f'az vm image list \
                 --architecture x64 \
                 --offer almalinux \
                 --publisher almalinux \
-                --sku {AZU_OS_VERSION.replace(".", "_")} \
+                --sku {AZU_OS_VERSION} \
                 --all'
 
 AZU_ARM_CMD = f'az vm image list \
                 --architecture Arm64 \
                 --offer almalinux \
                 --publisher almalinux \
-                --sku {AZU_OS_VERSION.replace(".", "_")} \
+                --sku {AZU_OS_VERSION} \
                 --all'
 
 
@@ -79,7 +79,7 @@ def get_azu_image_for_cmd(cmd):
             print('Auth Error')
             return None
         result_json = json.loads(result)
-        filtered_result = [x for x in result_json if x['sku'] == AZU_OS_VERSION.replace(".", "_")]
+        filtered_result = [x for x in result_json if x['sku'] == AZU_OS_VERSION]
         filtered_result.sort(key=lambda image: image['version'])
         latest_image = filtered_result[-1]
         return latest_image['urn']
@@ -155,12 +155,12 @@ def update_azure_metadata_file(path):
         x86_image = get_azu_image_for_cmd(AZU_X86_CMD)
         if x86_image is None:
             print('Failed to fetch latest x86 image')
-        arm_image = get_azu_image_for_cmd(AZU_ARM_CMD)
-        if arm_image is None:
-            print('Failed to get latest arm image')
+        # arm_image = get_azu_image_for_cmd(AZU_ARM_CMD)
+        # if arm_image is None:
+        #     print('Failed to get latest arm image')
         for region in config['regions']:
-            if arm_image is not None:
-                config['regions'][region]['arm_image'] = arm_image
+            # if arm_image is not None:
+            #     config['regions'][region]['arm_image'] = arm_image
             if x86_image is not None:
                 config['regions'][region]['image'] = x86_image
         print('Azure Metadata file updated successfully')
