58cf3140d008eb12665c009b2cdab38eb43dbf45
Mark Lillibridge <mlillibridge@yugabyte.com>
2024-02-27 11:34:07 -0800

    [#20667] DocDB: Make tablet guardrail mechanism wait for TServer registrations
    Summary:
    When master leadership changes, initially the master has no information about any TServers.  That information comes in over time as TServers heartbeat for the first time to the new master.
    
    However, the tablet guardrail mechanism as it currently stands assumes it knows all the live TServers's information at all times.  This means if they CREATE TABLE request comes in shortly after he master leadership has changes then we might be missing information about some of the TServers and thus conclude that we can support fewer tablets than we actually can and incorrectly reject the create request.
    
    This diff removes these false-positives (incorrectly rejecting) at the cost of introducing false negatives (failing to reject when should) by simply not checking for too many tablets for the first 120 seconds (controlled by a gflag) after master leadership changes.
    
    I made a fairly generic flag:
    ```
    ~/code/yugabyte-db/src/yb/master/catalog_manager.cc:638:
    DEFINE_RUNTIME_uint32(wait_for_registration_after_leadership_change_secs,
        yb::master::kDelayAfterFailoverSecs,
        "Amount of time to wait between becoming master leader and relying on all live TServers having "
        "registered.");
    TAG_FLAG(wait_for_registration_after_leadership_change_secs, advanced);
    ```
    rather than tying it explicitly to the tablet guardrail mechanism because I thought we might want to use the same wait for future features.
    
    I did not use the pre-existing load balancer delay:
    ```
    ~/code/yugabyte-db/src/yb/master/catalog_manager_bg_tasks.cc:62:
    DEFINE_RUNTIME_int32(load_balancer_initial_delay_secs, yb::master::kDelayAfterFailoverSecs,
                 "Amount of time to wait between becoming master leader and enabling the load "
                 "balancer.");
    ```
    because I thought it might need to be longer than just the registration wait.
    
    Please advise if this makes sense or if I should rename the flag.
    Jira: DB-9663
    
    Test Plan:
    ```
    ybd --cxx-test tablet_limits_test --test-args ''  |& tee /tmp/generic.mdl.log
    ybd --cxx-test tablet_limits_integration_test --test-args ''  |& tee /tmp/generic.mdl.log
    ```
    If I do not set the reduce the wait flag, then some of the integration tests do not pass.  I did not create an extra test just to verify that going forward.
    
    Reviewers: hsunder
    
    Reviewed By: hsunder
    
    Subscribers: ybase, bogdan
    
    Differential Revision: https://phorge.dev.yugabyte.com/D32683

--- src/yb/master/catalog_manager.cc
+++ src/yb/master/catalog_manager.cc
@@ -641,6 +641,12 @@ DEFINE_RUNTIME_uint32(maximum_tablet_leader_lease_expired_secs, 2 * 60,
 
 DEFINE_test_flag(bool, disable_set_catalog_version_table_in_perdb_mode, false,
                  "Whether to disable setting the catalog version table in perdb mode.");
+DEFINE_RUNTIME_uint32(initial_tserver_registration_duration_secs,
+    yb::master::kDelayAfterFailoverSecs,
+    "Amount of time to wait between becoming master leader and relying on all live TServers having "
+    "registered.");
+TAG_FLAG(initial_tserver_registration_duration_secs, advanced);
+
 namespace yb {
 namespace master {
 
@@ -3910,7 +3916,11 @@ Status CatalogManager::CreateTable(const CreateTableRequestPB* orig_req,
 
   int num_tablets = VERIFY_RESULT(CalculateNumTabletsForTableCreation(req, schema, placement_info));
   Status s = CanAddPartitionsToTable(num_tablets, placement_info);
-  if (s.ok()) {
+  // Don't check for tablet limits if we potentially don't have information from all the live
+  // TServers.  To make sure we have the needed information, don't check for
+  // FLAGS_initial_tserver_registration_duration_secs after a master leadership change.
+  if (s.ok() && (TimeSinceElectedLeader() >=
+                 MonoDelta::FromSeconds(FLAGS_initial_tserver_registration_duration_secs))) {
     s = CanCreateTabletReplicas(num_tablets, replication_info, GetAllLiveNotBlacklistedTServers());
   }
   if (!s.ok()) {
@@ -3920,7 +3930,6 @@ Status CatalogManager::CreateTable(const CreateTableRequestPB* orig_req,
   }
   const auto [partition_schema, partitions] =
       VERIFY_RESULT(CreatePartitions(schema, num_tablets, colocated, &req, resp));
-
 
   if (!FLAGS_TEST_skip_placement_validation_createtable_api) {
     ValidateReplicationInfoRequestPB validate_req;
