d4018f7a4656f3202844601766f60a524284d0a0
Vinod Sridharan <visridha@microsoft.com>
2024-02-27 00:01:36 +0000

    Merged PR 1283033: [PG16] Part2: Enable backend tests for PG16
    ### Does this PR have any customer impact?
    No
    ### Type (Feature, Refactoring, Bugfix, DevOps, Testing, Perf, etc)
    Feature
    
    ### Does it involve schema level changes? (Table, Column, Index, UDF, etc level changes)
    No
    ### Release Note (Refer [Template](../docs/release_notes/pgmongo_template.md))
    
    ### Description
    Changes:
    - Unify rum_tuplesort_get_estimated_itemcount between PG16/PG15
    - Remove set local force_parallel_mode TO ON; - This flag is removed in PG16
    - Remove legacy query generation for lookup tests
    - Citus 12.1 made the default behavior not to push down filters - set htat GUC to true for the extension

---
+++ pg_helio_api/include/utils/version_utils_private.h
@@ -0,0 +1,37 @@
+/*-------------------------------------------------------------------------
+ * Copyright (c) Microsoft Corporation.  All rights reserved.
+ *
+ * include/utils/version_utils.h
+ *
+ * Utilities that Provide extension functions to handle version upgrade
+ * scenarios.
+ *
+ *-------------------------------------------------------------------------
+ */
+#include <postgres.h>
+
+#ifndef VERSION_UTILS_PRIVATE_H
+#define VERSION_UTILS_PRIVATE_H
+
+/*
+ * The current Extensions' version.
+ * Note Extension versions are of the form
+ * <Major>.<Minor>-<Patch>
+ */
+typedef struct ExtensionVersion
+{
+	/* The major version of the extension */
+	int Major;
+
+	/* The minor version of the extension */
+	int Minor;
+
+	/* The patch version of the extension */
+	int Patch;
+} ExtensionVersion;
+
+extern ExtensionVersion CurrentVersion;
+
+void RefreshCurrentVersion(void);
+
+#endif
---
+++ pg_helio_api/src/utils/helio_api_version_utils.c
@@ -0,0 +1,120 @@
+/*-------------------------------------------------------------------------
+ * Copyright (c) Microsoft Corporation.  All rights reserved.
+ *
+ * src/helio_api_version_utils.c
+ *
+ * Utilities that Provide extension functions to handle version upgrade
+ * scenarios for the current extension.
+ *
+ *-------------------------------------------------------------------------
+ */
+
+#include "utils/version_utils.h"
+#include "utils/version_utils_private.h"
+#include "utils/query_utils.h"
+#include "utils/guc_utils.h"
+#include <utils/builtins.h>
+
+/*
+ * Global value tracking the Current Version deployed across
+ * the cluster.
+ */
+ExtensionVersion CurrentVersion = { 0 };
+
+extern char *ApiExtensionName;
+
+char *VersionRefreshQuery =
+	"SELECT regexp_split_to_array(extversion, '[-\\.]')::int4[] FROM pg_extension WHERE extname = 'pg_helio_api'";
+
+/*
+ * Returns true if the cluster version is >= given major.minor.patch version
+ */
+bool
+IsClusterVersionAtleastThis(int major, int minor, int patch)
+{
+	RefreshCurrentVersion();
+
+	if (CurrentVersion.Major < major)
+	{
+		return false;
+	}
+	else if (CurrentVersion.Minor < minor)
+	{
+		return false;
+	}
+	else if (CurrentVersion.Major != major || CurrentVersion.Minor != minor)
+	{
+		/* if CurrentVersion.Major or CurrentVersion.Minor are greater than the expected version */
+		/* parts we are on a later version, no need to compare the patch. */
+		return true;
+	}
+
+	/* Major and Minor are the expected ones, we should compare the patch version. */
+	return CurrentVersion.Patch >= patch;
+}
+
+
+/*
+ * Hook to invalidate the version cache.
+ */
+void
+InvalidateVersionCache()
+{
+	CurrentVersion = (ExtensionVersion) {
+		0
+	};
+}
+
+
+void
+RefreshCurrentVersion(void)
+{
+	if (CurrentVersion.Major != 0)
+	{
+		return;
+	}
+
+	/*
+	 * Temporarily disable unimportant logs related to version lookup
+	 * so that regression test outputs don't become flaky (e.g.: due to commands
+	 * being executed by Citus locally).
+	 */
+	int savedGUCLevel = NewGUCNestLevel();
+	SetGUCLocally("client_min_messages", "WARNING");
+	bool readOnly = true;
+	bool isNull = false;
+
+	char *versionString = ExtensionExecuteQueryOnLocalhostViaLibPQ(VersionRefreshQuery);
+
+	if (strcmp(versionString, "") == 0)
+	{
+		RollbackGUCChange(savedGUCLevel);
+		return;
+	}
+
+	int nargs = 1;
+	Oid argTypes[1] = { TEXTOID };
+	Datum argValues[1] = { CStringGetTextDatum(versionString) };
+	char *argNulls = NULL;
+
+	/* LibPQ returns an array as a string in the form of {Major,Minor,Hotfix}, so instead
+	 * of parsing the string directly, let's use postgres to cast it to an array as a datum
+	 * and just use the deconstructed array to get the values out of it. */
+	Datum versionDatum = ExtensionExecuteQueryWithArgsViaSPI("SELECT $1::int4[]",
+															 nargs, argTypes, argValues,
+															 argNulls, readOnly,
+															 SPI_OK_SELECT, &isNull);
+
+	ArrayType *arrayValue = DatumGetArrayTypeP(versionDatum);
+	RollbackGUCChange(savedGUCLevel);
+
+	Datum *elements = NULL;
+	int numElements = 0;
+	deconstruct_array(arrayValue, INT4OID, sizeof(int), true, TYPALIGN_INT,
+					  &elements, NULL, &numElements);
+
+	Assert(numElements == 3);
+	CurrentVersion.Major = DatumGetInt32(elements[0]);
+	CurrentVersion.Minor = DatumGetInt32(elements[1]);
+	CurrentVersion.Patch = DatumGetInt32(elements[2]);
+}
