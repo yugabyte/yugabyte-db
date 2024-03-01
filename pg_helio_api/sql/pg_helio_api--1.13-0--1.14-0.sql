b635c244d815f300f267d7da0031b1c75c28bacd
Lucas Borges Fernandes <lucasbo@microsoft.com>
2024-03-01 23:17:47 +0000

    Merged PR 1249041: [OSS] Adding C impl for rename_collection UDF

--- pg_helio_api/sql/pg_helio_api--1.13-0--1.14-0.sql
+++ pg_helio_api/sql/pg_helio_api--1.13-0--1.14-0.sql
@@ -1,5 +1,6 @@
 SET search_path TO helio_api;
 #include "udfs/query/bson_query_match--1.14-0.sql"
+#include "udfs/schema_mgmt/rename_collection--1.14-0.sql"
 #include "schema/background_index_queue--1.14-0.sql"
 
 RESET search_path;
