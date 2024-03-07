b3526521583eb77f3e0959dad965e1fd96884810
Rajagopalan Madhavan <rmadhavan@yugabyte.com>
2024-03-07 15:05:00 -0500

    [PLAT-13036]: Clicking on actionas dropdown once should open dropdown options and not side panel
    Summary:
    Initially when I worked on the feature, I assumed side panel will open on double click
    Hence changed that to single click action
    
    Also the action dropdown need to stop propogating the actions to its parent onClick function to ensure side panel does not open
    
    Test Plan:
    Please refer to the video below
    {F157484}
    
    Reviewers: jmak, lsangappa, kkannan
    
    Reviewed By: jmak
    
    Subscribers: yugaware
    
    Differential Revision: https://phorge.dev.yugabyte.com/D32962

--- managed/ui/src/redesign/features/releases/components/ReleaseList.tsx
+++ managed/ui/src/redesign/features/releases/components/ReleaseList.tsx
@@ -345,6 +345,7 @@ export const NewReleaseList = () => {
             value={value}
             onClick={(e: any) => {
               onActionClick(value, row);
+              e.stopPropagation();
             }}
           >
             {value}
@@ -364,6 +365,7 @@ export const NewReleaseList = () => {
           value={action}
           onClick={(e: any) => {
             onActionClick(action, row);
+            e.stopPropagation();
           }}
         >
           {action}
@@ -389,6 +391,7 @@ export const NewReleaseList = () => {
           value={value}
           onClick={(e: any) => {
             onActionClick(value, row);
+            e.stopPropagation();
           }}
         >
           {value}
@@ -401,7 +404,12 @@ export const NewReleaseList = () => {
 
   const formatActionButtons = (cell: any, row: any) => {
     return (
-      <DropdownButton title="Actions" id="release-list-actions" pullRight={false}>
+      <DropdownButton
+        title="Actions"
+        id="release-list-actions"
+        pullRight={false}
+        onClick={(e) => e.stopPropagation()}
+      >
         {getMenuItemsActions(row)}
       </DropdownButton>
     );
@@ -451,7 +459,7 @@ export const NewReleaseList = () => {
     setOpenSidePanel(false);
   };
 
-  const onRowDoubleClick = (row: any, event: any) => {
+  const onRowClick = (row: any, event: any) => {
     setSelectedReleaseDetails(row);
     setOpenSidePanel(true);
   };
@@ -597,7 +605,7 @@ export const NewReleaseList = () => {
               data={filteredReleaseList}
               pagination={true}
               options={{
-                onRowDoubleClick: onRowDoubleClick
+                onRowClick: onRowClick
               }}
             >
               <TableHeaderColumn dataField={'release_uuid'} isKey={true} hidden={true} />
