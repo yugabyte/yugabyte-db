dc2a39971a88b1ba3bc0096b9be5a846890cacc6
Naorem Khogendro Singh <nsingh@yugabyte.com>
2024-03-07 13:48:36 -0800

    [PLAT-12715] Set master addresses more accurately during a full move
    Summary:
    The changes are:
    1. Get the remote master addresses from the DB in the beginning when the task starts running.
    2. Save the master nodes in the execution context of this task.
    3. On master move, update(add/remove) the set of masters. Each update creating a copy to give the snapshot at the time.
    4. As the IP can get populated late (e.g by AnsibleUpdateInfo), the supplier evaluates the addresses late only when the subtask is run.
    
    Test Plan:
    Tested by running full move, abort and retry.
    
    Saw the move of masters followed by every update with the current actual masters. Also checked the conf files.
    
    ```
    com.yugabyte.yw.common.ShellProcessHandler Starting proc (full cmd) - 'bin/ybcloud.sh' 'aws' '--region' 'us-west-2' '--zone' 'us-west-2b' 'instance' 'configure' '--master_addresses_for_tserver' '10.9.141.194:7100,10.9.107.115:7100,10.9.198.194:7100'
    ```
    Also, before move master, tservers have the current actual masters only.
    
    ```
    
    [yugabyte@ip-10-9-105-94 ~]$ cat tserver/conf/server.conf
    --allow_insecure_connections=false
    --callhome_collection_level=medium
    --cert_node_filename=10.9.105.94
    --certs_dir=/home/yugabyte/yugabyte-tls-config
    --certs_for_cdc_dir=/home/yugabyte/yugabyte-tls-producer
    --certs_for_client_dir=/home/yugabyte/yugabyte-client-tls-config
    --cluster_uuid=befb1811-943f-4f4f-a702-30be9ab12b55
    --cql_proxy_bind_address=10.9.105.94:9042
    --cql_proxy_webserver_port=12000
    --enable_ysql=true
    --fs_data_dirs=/mnt/d0
    --load_balancer_initial_delay_secs=480
    --max_log_size=256
    --metric_node_name=yb-admin-nsingh-test-universe1-n10
    --pgsql_proxy_bind_address=10.9.105.94:5433
    --pgsql_proxy_webserver_port=13000
    --placement_cloud=aws
    --placement_region=us-west-2
    --placement_uuid=8786a69b-3318-4ab1-931a-decf713ee15b
    --placement_zone=us-west-2a
    --redis_proxy_bind_address=10.9.105.94:6379
    --replication_factor=3
    --rpc_bind_addresses=10.9.105.94:9100
    --server_broadcast_addresses=
    --start_cql_proxy=true
    --start_redis_proxy=false
    --tserver_master_addrs=10.9.83.44:7100,10.9.134.75:7100,10.9.193.230:7100
    
    ```
    
    Reviewers: cwang, sanketh, yshchetinin, amalyshev
    
    Reviewed By: amalyshev
    
    Subscribers: yugaware
    
    Differential Revision: https://phorge.dev.yugabyte.com/D32967

--- managed/src/main/java/com/yugabyte/yw/commissioner/tasks/UniverseTaskBase.java
+++ managed/src/main/java/com/yugabyte/yw/commissioner/tasks/UniverseTaskBase.java
@@ -23,6 +23,7 @@ import com.yugabyte.yw.commissioner.Common.CloudType;
 import com.yugabyte.yw.commissioner.TaskExecutor.SubTaskGroup;
 import com.yugabyte.yw.commissioner.UserTaskDetails;
 import com.yugabyte.yw.commissioner.UserTaskDetails.SubTaskGroupType;
+import com.yugabyte.yw.commissioner.tasks.UniverseDefinitionTaskBase.PortType;
 import com.yugabyte.yw.commissioner.tasks.params.NodeTaskParams;
 import com.yugabyte.yw.commissioner.tasks.params.ServerSubTaskParams;
 import com.yugabyte.yw.commissioner.tasks.subtasks.AnsibleClusterServerCtl;
@@ -224,9 +225,11 @@ import java.util.UUID;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicReference;
+import java.util.function.BiConsumer;
 import java.util.function.Consumer;
 import java.util.function.Function;
 import java.util.function.Predicate;
+import java.util.function.Supplier;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 import javax.annotation.Nullable;
@@ -389,14 +392,17 @@ public abstract class UniverseTaskBase extends AbstractTaskBase {
   private final AtomicReference<ExecutionContext> executionContext = new AtomicReference<>();
 
   public class ExecutionContext {
+    private final UUID universeUuid;
     private final boolean blacklistLeaders;
     private final int leaderBacklistWaitTimeMs;
     private final boolean followerLagCheckEnabled;
     private boolean loadBalancerOff = false;
     private final Set<UUID> lockedUniversesUuid = ConcurrentHashMap.newKeySet();
+    private final AtomicReference<Set<NodeDetails>> masterNodes = new AtomicReference<>();
 
     ExecutionContext() {
-      Universe universe = Universe.getOrBadRequest(taskParams().getUniverseUUID());
+      this.universeUuid = taskParams().getUniverseUUID();
+      Universe universe = Universe.getOrBadRequest(this.universeUuid);
       blacklistLeaders =
           confGetter.getConfForScope(universe, UniverseConfKeys.ybUpgradeBlacklistLeaders);
 
@@ -429,6 +435,49 @@ public abstract class UniverseTaskBase extends AbstractTaskBase {
 
     public void unlockUniverse(UUID universeUUID) {
       lockedUniversesUuid.remove(universeUUID);
+    }
+
+    public void setMasterNodes(Set<NodeDetails> nodes) {
+      masterNodes.set(ImmutableSet.copyOf(Objects.requireNonNull(nodes)));
+    }
+
+    // A supplier is evaluated late when the subtask is run. Initially, when a subtask is created
+    // the node may not have IP.
+    public Supplier<String> getMasterAddrsSupplier() {
+      // Take the current set of the masters when this is invoked.
+      final Set<NodeDetails> nodes = masterNodes.get();
+      if (CollectionUtils.isEmpty(nodes)) {
+        return null;
+      }
+      return () -> {
+        // Refresh the nodes from the DB to get IPs.
+        Universe universe = Universe.getOrBadRequest(universeUuid);
+        return universe.getHostPortsString(
+            universe.getNodes().stream().filter(n -> nodes.contains(n)).collect(Collectors.toSet()),
+            ServerType.MASTER,
+            PortType.RPC);
+      };
+    }
+
+    public void removeMasterNode(NodeDetails node) {
+      masterNodes.getAndUpdate(
+          v -> {
+            if (v != null) {
+              Set<NodeDetails> nodes = new HashSet<>(v);
+              nodes.remove(node);
+              return Collections.unmodifiableSet(nodes);
+            }
+            return null;
+          });
+    }
+
+    public void addMasterNode(NodeDetails node) {
+      masterNodes.getAndUpdate(
+          v -> {
+            Set<NodeDetails> nodes = v == null ? new HashSet<>() : new HashSet<>(v);
+            nodes.add(node);
+            return Collections.unmodifiableSet(nodes);
+          });
     }
   }
 
@@ -2137,6 +2186,42 @@ public abstract class UniverseTaskBase extends AbstractTaskBase {
   }
 
   /**
+   * Gets the current masters reported by the DB for the given universe.
+   *
+   * @param universe the given universe.
+   * @return Set of master nodes.
+   */
+  protected Set<NodeDetails> getRemoteMasterNodes(Universe universe) {
+    String masterAddresses = universe.getMasterAddresses();
+    try (YBClient client =
+        ybService.getClient(masterAddresses, universe.getCertificateNodetoNode())) {
+      return client.listMasters().getMasters().stream()
+          .map(
+              serverInfo -> {
+                // Port in ServerInfo is set to 0.
+                NodeDetails node = universe.getNodeByPrivateIP(serverInfo.getHost());
+                if (node == null || !node.isMaster) {
+                  String errMsg =
+                      String.format(
+                          "Master %s on DB is not in YBA masters %s",
+                          serverInfo.getHost(), masterAddresses);
+                  log.error(errMsg);
+                  throw new IllegalStateException(errMsg);
+                }
+                return node;
+              })
+          .collect(Collectors.toSet());
+    } catch (Exception e) {
+      String msg =
+          String.format(
+              "Error while getting masters from DB. Current YBA masters %s - %s",
+              masterAddresses, e.getMessage());
+      log.error(msg, e);
+      throw new RuntimeException(msg);
+    }
+  }
+
+  /**
    * Create tasks to execute Cluster CTL command against specific process in parallel
    *
    * @param nodes set of nodes to issue control command in parallel.
@@ -2561,9 +2646,9 @@ public abstract class UniverseTaskBase extends AbstractTaskBase {
   }
 
   /**
-   * Add or Remove Master process on the node
+   * Add or Remove Master process on the node.
    *
-   * @param node the node to add/remove master process on
+   * @param node the node to add/remove master process on.
    * @param isAdd whether Master is being added or removed.
    * @param subTask subtask type
    */
@@ -2577,7 +2662,7 @@ public abstract class UniverseTaskBase extends AbstractTaskBase {
     }
   }
 
-  public void createChangeConfigTask(
+  private void createChangeConfigTask(
       NodeDetails node, boolean isAdd, UserTaskDetails.SubTaskGroupType subTask) {
     // Create a new task list for the change config so that it happens one by one.
     String subtaskGroupName =
@@ -4133,25 +4218,21 @@ public abstract class UniverseTaskBase extends AbstractTaskBase {
 
   public SubTaskGroup createUpdateMasterAddrsInMemoryTasks(
       Collection<NodeDetails> nodes, ServerType serverType) {
-    return createSetFlagInMemoryTasks(nodes, serverType, true, (node) -> null, true);
+    return createSetFlagInMemoryTasks(
+        nodes,
+        serverType,
+        (node, params) -> {
+          params.force = true;
+          params.updateMasterAddrs = true;
+          params.masterAddrsOverride = getOrCreateExecutionContext().getMasterAddrsSupplier();
+        });
   }
 
   // Subtask to update gflags in memory.
   public SubTaskGroup createSetFlagInMemoryTasks(
       Collection<NodeDetails> nodes,
       ServerType serverType,
-      boolean force,
-      Map<String, String> gflags) {
-    return createSetFlagInMemoryTasks(nodes, serverType, force, (n) -> gflags, false);
-  }
-
-  // Subtask to update gflags in memory.
-  public SubTaskGroup createSetFlagInMemoryTasks(
-      Collection<NodeDetails> nodes,
-      ServerType serverType,
-      boolean force,
-      Function<NodeDetails, Map<String, String>> gflagsGetter,
-      boolean updateMasterAddrs) {
+      BiConsumer<NodeDetails, SetFlagInMemory.Params> paramCustomizer) {
     SubTaskGroup subTaskGroup = createSubTaskGroup("InMemoryGFlagUpdate");
     for (NodeDetails node : nodes) {
       // Create the task params.
@@ -4162,13 +4243,7 @@ public abstract class UniverseTaskBase extends AbstractTaskBase {
       params.setUniverseUUID(taskParams().getUniverseUUID());
       // The server type for the flag.
       params.serverType = serverType;
-      // If the flags need to be force updated.
-      params.force = force;
-      // The flags to update.
-      params.gflags = gflagsGetter.apply(node);
-      // If only master addresses need to be updated.
-      params.updateMasterAddrs = updateMasterAddrs;
-
+      paramCustomizer.accept(node, params);
       // Create the task.
       SetFlagInMemory setFlag = createTask(SetFlagInMemory.class);
       setFlag.initialize(params);
