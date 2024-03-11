6b6478cefbc18b45353945e928196a393ad329ab
Taylor Vesely <tvesely@yugabyte.com>
2024-03-11 16:42:09 +0000

    [#21043] YSQL: Improve error messages for aborted transactions
    Summary:
    There are several code paths that can result in an aborted transaction
    being reported to Postgres, resulting in inconsistent error message
    generation. This commit provides more consistent error messages
    in line with with how we treat error messages from serialization and
    deadlock errors.
    Jira: DB-10012
    
    Test Plan: ./yb_build.sh --java-test 'org.yb.pgsql.TestPgWithoutWaitQueuesIsolationRegress'
    
    Reviewers: bkolagani, tvesely
    
    Reviewed By: bkolagani
    
    Subscribers: smishra, yql
    
    Differential Revision: https://phorge.dev.yugabyte.com/D32661

--- src/yb/yql/pggate/util/ybc_util.h
+++ src/yb/yql/pggate/util/ybc_util.h
@@ -189,6 +189,7 @@ bool YBCIsRestartReadError(uint16_t txn_errcode);
 bool YBCIsTxnConflictError(uint16_t txn_errcode);
 bool YBCIsTxnSkipLockingError(uint16_t txn_errcode);
 bool YBCIsTxnDeadlockError(uint16_t txn_errcode);
+bool YBCIsTxnAbortedError(uint16_t txn_errcode);
 uint16_t YBCGetTxnConflictErrorCode();
 
 void YBCResolveHostname();
