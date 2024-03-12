4548b2ac13d7b29827ae629b175e13c949070795
Jason Kim <git@jasonk.me>
2024-03-12 11:34:38 -0700

    [#9960] YSQL: avoid some unsupported GIN index scans
    Summary:
    Tweak the ybgin cost estimation so that some queries that do not
    currently support ybgin index scan are not taken.  Instead, sequential
    scan may be taken, for example.
    
    One case that showed up recently is the planner choosing to use a
    partial ybgin index with the "everything" search mode only because it
    matched the partial index's condition.  That was very likely not the
    intended use of the index, so it is a good example of a case that would
    benefit from this change.  The workaround before this change is to try
    using pg_hint_plan or the GUC toggles to disable this index scan.
    
    The logic currently detects and adds a huge cost to the following cases:
    - search mode: ybgin only supports the "default" search mode
    - multiple scan keys: ybgin only supports 1 scan key
    It fails to detect the following cases:
    - multiple required scan entries: ybgin only supports 1 required scan
      entry (and any number of additional scan entries)
    - null entries: ybgin only supports normal scan entries
    These cases are hard to detect in the cost estimation part of the code
    because the information is not exposed (by default).  Rather than spend
    time on plumbing the information into cost estimation, it is probably
    more worth putting that time into actually fixing/supporting those types
    of scan pattern.  So leave them as todo.
    
    The huge cost is calculated as yb_test_ybgin_disable_cost_factor *
    disable_cost:
    
    - yb_test_ybgin_disable_cost_factor: new GUC defaulting to 2.0
    - disable_cost: hard-coded constant 1.0e10
    
    This is so that unsupported index scan costs higher than a sequential
    scan even when enable_seqscan is off.  Make many tests use
    yb_test_ybgin_disable_cost_factor = 0.5 so that index scan is chosen
    when enable_seqscan = off.
    
    Add a new regress test yb_ybgin_cost_estimate.  It is sensitive to
    placement in the schedule because it relies on the jsonbs's index using
    the default jsonb_ops opclass, not jsonb_path_ops.  Perhaps in the
    future, the dependencies among the gin tests can be disentangled.
    
    Also make a minor update to regress test yb_ybgin_search_modes to
    comment that a query covers GIN_SEARCH_MODE_EVERYTHING.
    Jira: DB-1747
    
    Test Plan:
        ./yb_build.sh fastdebug --gcc11 --java-test TestPgRegressGin
        ./yb_build.sh fastdebug --gcc11 --java-test TestPgRegressContribPgTrgm
        ./yb_build.sh fastdebug --gcc11 --java-test TestPgRegressJson
        ./yb_build.sh fastdebug --gcc11 --java-test TestPgRegressTypesString
    
    Reviewers: mihnea, gkukreja
    
    Reviewed By: gkukreja
    
    Subscribers: gkukreja, yql
    
    Differential Revision: https://phorge.dev.yugabyte.com/D32036

--- src/postgres/src/backend/utils/adt/selfuncs.c
+++ src/postgres/src/backend/utils/adt/selfuncs.c
@@ -7608,6 +7608,20 @@ gincost_pattern(IndexOptInfo *index, int indexcol,
 		return false;
 	}
 
+	if (IsYugaByteEnabled() && index->relam == YBGIN_AM_OID &&
+		searchMode != GIN_SEARCH_MODE_DEFAULT)
+	{
+		/*
+		 * TODO(#7850): for ybgin, non-default search mode queries aren't
+		 * supported yet.
+		 *
+		 * Piggyback on haveFullScan, which the caller translates to
+		 * disable_cost.
+		 */
+		counts->haveFullScan = true;
+		return true;
+	}
+
 	for (i = 0; i < nentries; i++)
 	{
 		/*
@@ -7847,6 +7861,18 @@ gincostestimate(PlannerInfo *root, IndexPath *path, double loop_count,
 	/* Do preliminary analysis of indexquals */
 	qinfos = deconstruct_indexquals(path);
 
+	if (IsYugaByteEnabled() && index->relam == YBGIN_AM_OID &&
+		list_length(qinfos) > 1)
+	{
+		/*
+		 * TODO(#7850): for ybgin, queries using multiple scan keys aren't
+		 * supported yet.
+		 */
+		*indexStartupCost = *indexTotalCost =
+			yb_test_ybgin_disable_cost_factor * disable_cost;
+		return;
+	}
+
 	/*
 	 * Obtain statistical information from the meta page, if possible.  Else
 	 * set ginStats to zeroes, and we'll cope below.
@@ -8010,6 +8036,16 @@ gincostestimate(PlannerInfo *root, IndexPath *path, double loop_count,
 
 	if (counts.haveFullScan || indexQuals == NIL)
 	{
+		if (IsYugaByteEnabled() && index->relam == YBGIN_AM_OID)
+		{
+			/*
+			 * TODO(#7850): for ybgin, full scan is not supported.
+			 */
+			*indexStartupCost = *indexTotalCost =
+				yb_test_ybgin_disable_cost_factor * disable_cost;
+			return;
+		}
+
 		/*
 		 * Full index scan will be required.  We treat this as if every key in
 		 * the index had been listed in the query; is that reasonable?
