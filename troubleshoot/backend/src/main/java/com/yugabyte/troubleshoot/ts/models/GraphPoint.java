7e4195f5d4d6efdca14b375d78ed66ac89560620
Aleksandr Malyshev <amalyshev@yugabyte.com>
2024-03-13 21:52:32 +0300

    [PLAT-12758]  NaN -> 0 for the graph API
    Summary: Replaced all NaNs in graph API response to 0 similar to YBA + properly format values to remove trailing 0s
    
    Test Plan: Unit tested
    
    Reviewers: rmadhavan
    
    Reviewed By: rmadhavan
    
    Subscribers: yugaware
    
    Differential Revision: https://phorge.dev.yugabyte.com/D33121

--- troubleshoot/backend/src/main/java/com/yugabyte/troubleshoot/ts/models/GraphPoint.java
+++ troubleshoot/backend/src/main/java/com/yugabyte/troubleshoot/ts/models/GraphPoint.java
@@ -8,4 +8,16 @@ import lombok.experimental.Accessors;
 public class GraphPoint {
   private Long x;
   private Double y;
+
+  public GraphPoint setY(Double y) {
+    // For now we replace NaN with 0, similar to YBA.
+    // As effectively we have NaN for latencies in case no responses were made.
+    // It makes sense to show 0 as latency in this case.
+    if (y != null && y.isNaN()) {
+      this.y = 0D;
+    } else {
+      this.y = y;
+    }
+    return this;
+  }
 }
