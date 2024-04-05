yugabyte/yugabyte-db

commit e3e166d10abe05036609bbf2b902a338c44d0168

[#21544] xCluster: Support colocated DBs in DB Scoped replication
Summary:
xCluster setup requires at least one table to be present in the database.
For colocated databases we need to have the colocated table group, and at least one colocated table. This is because we map the DocDB table Ids of the tablegroups, between the two universes using the tables in the groups.
This change adds checks to make sure these conditions are satisfied.

- Moved `IsSequencesSystemTable` from `CatalogManager` to `TableInfo`
- Created a common `IsTableEligibleForXClusterReplication` util function that can be used for both source and target side checks.

Fixes #21544
Jira: DB-10429

Test Plan:
XClusterDBScopedTest.TestCreateWithCheckpoint
XClusterDBScopedTest.ColocatedDB

Reviewers: jhe, slingam, xCluster

Reviewed By: jhe

Subscribers: yql, xCluster, ybase, bogdan

Differential Revision: https://phorge.dev.yugabyte.com/D33688

src/yb/client/xcluster_client.cc
@@ -83,6 +83,10 @@ Result<IsOperationDoneResult> XClusterClient::IsCreateXClusterReplicationDone(
     return StatusFromPB(resp.error().status());
   }
 
+  if (!resp.done()) {
+    return IsOperationDoneResult::NotDone();
+  }
+
   if (resp.has_replication_error()) {
     return IsOperationDoneResult::Done(StatusFromPB(resp.replication_error()));
   }
@@ -247,12 +251,16 @@ Result<IsOperationDoneResult> XClusterRemoteClient::IsSetupUniverseReplicationDo
     return StatusFromPB(resp.error().status());
   }
 
+  if (!resp.done()) {
+    return IsOperationDoneResult::NotDone();
+  }
+
   if (resp.has_replication_error()) {
     // IsSetupUniverseReplicationDoneRequestPB will contain an OK status on success.
     return IsOperationDoneResult::Done(StatusFromPB(resp.replication_error()));
   }
 
-  return resp.done() ? IsOperationDoneResult::Done() : IsOperationDoneResult::NotDone();
+  return IsOperationDoneResult::Done();
 }
 
 Status XClusterRemoteClient::GetXClusterTableCheckpointInfos(

src/yb/integration-tests/xcluster/xcluster_db_scoped-test.cc
@@ -37,11 +37,31 @@ class XClusterDBScopedTest : public XClusterYsqlTestBase {
 };
 
 TEST_F(XClusterDBScopedTest, TestCreateWithCheckpoint) {
-  ASSERT_OK(SetUpClusters());
+  SetupParams param;
+  param.num_producer_tablets = {};
+  param.num_consumer_tablets = {};
+  ASSERT_OK(SetUpClusters(param));
+
+  ASSERT_NOK_STR_CONTAINS(
+      CheckpointReplicationGroup(),
+      "Database should have at least one table in order to be part of xCluster replication");
+
+  auto producer_table_name = ASSERT_RESULT(CreateYsqlTable(
+      /*idx=*/0, /*num_tablets=*/3, &producer_cluster_));
+  ASSERT_OK(producer_client()->OpenTable(producer_table_name, &producer_table_));
 
   ASSERT_OK(CheckpointReplicationGroup());
 
+  ASSERT_OK(InsertRowsInProducer(0, 50));
+
   ASSERT_NOK(CreateReplicationFromCheckpoint("bad-master-addr"));
+
+  ASSERT_NOK_STR_CONTAINS(
+      CreateReplicationFromCheckpoint(), "Could not find matching table for yugabyte.test_table_0");
+
+  auto consumer_table_name = ASSERT_RESULT(CreateYsqlTable(
+      /*idx=*/0, /*num_tablets=*/3, &consumer_cluster_));
+  ASSERT_OK(producer_client()->OpenTable(consumer_table_name, &consumer_table_));
 
   ASSERT_OK(CreateReplicationFromCheckpoint());
 
@@ -52,7 +72,7 @@ TEST_F(XClusterDBScopedTest, TestCreateWithCheckpoint) {
   ASSERT_EQ(resp.entry().tables_size(), 1);
   ASSERT_EQ(resp.entry().tables(0), producer_table_->id());
 
-  ASSERT_OK(InsertRowsInProducer(0, 50));
+  ASSERT_OK(InsertRowsInProducer(50, 100));
 
   ASSERT_OK(VerifyWrittenRecords());
 }
@@ -63,10 +83,10 @@ TEST_F(XClusterDBScopedTest, CreateTable) {
   ASSERT_OK(CreateReplicationFromCheckpoint());
 
   // Creating a new table on target first should fail.
-  auto status = CreateYsqlTable(
-      /*idx=*/1, /*num_tablets=*/3, &consumer_cluster_);
-  ASSERT_NOK(status);
-  ASSERT_STR_CONTAINS(status.ToString(), "Table public.test_table_1 not found");
+  ASSERT_NOK_STR_CONTAINS(
+      CreateYsqlTable(
+          /*idx=*/1, /*num_tablets=*/3, &consumer_cluster_),
+      "Table public.test_table_1 not found");
 
   auto new_producer_table_name = ASSERT_RESULT(CreateYsqlTable(
       /*idx=*/1, /*num_tablets=*/3, &producer_cluster_));
@@ -123,6 +143,103 @@ TEST_F(XClusterDBScopedTest, DropTableOnProducerThenConsumer) {
   auto result = promise.get_future().get();
   ASSERT_NOK(result) << result->DebugString();
   ASSERT_STR_CONTAINS(result.status().ToString(), "test_table_0 not found in namespace");
+}
+
+TEST_F(XClusterDBScopedTest, ColocatedDB) {
+  namespace_name = "colocated_db";
+  SetupParams param;
+  param.is_colocated = true;
+
+  // Create clusters with colocated database, and 1 non-colocated table.
+  ASSERT_OK(SetUpClusters(param));
+
+  ASSERT_NOK_STR_CONTAINS(
+      CheckpointReplicationGroup(),
+      "Colocated database should have at least one colocated table in order to be part of "
+      "xCluster replication");
+
+  auto producer_colocated_table_name = ASSERT_RESULT(CreateYsqlTable(
+      /*idx=*/1, /*num_tablets=*/3, &producer_cluster_,
+      /*tablegroup_name=*/boost::none, /*colocated=*/true));
+  std::shared_ptr<client::YBTable> producer_colocated_table;
+  ASSERT_OK(producer_client()->OpenTable(producer_colocated_table_name, &producer_colocated_table));
+
+  ASSERT_OK(CheckpointReplicationGroup());
+
+  ASSERT_OK(InsertRowsInProducer(0, 10));
+  ASSERT_OK(InsertRowsInProducer(0, 50, producer_colocated_table));
+
+  ASSERT_NOK_STR_CONTAINS(
+      CreateReplicationFromCheckpoint(),
+      "Could not find matching table for colocated_db.test_table_1");
+
+  auto consumer_colocated_table_name = ASSERT_RESULT(CreateYsqlTable(
+      /*idx=*/1, /*num_tablets=*/3, &consumer_cluster_,
+      /*tablegroup_name=*/boost::none, /*colocated=*/true));
+  std::shared_ptr<client::YBTable> consumer_colocated_table;
+  ASSERT_OK(consumer_client()->OpenTable(consumer_colocated_table_name, &consumer_colocated_table));
+
+  ASSERT_OK(CreateReplicationFromCheckpoint());
+
+  ASSERT_OK(VerifyWrittenRecords());
+  ASSERT_OK(VerifyWrittenRecords(producer_colocated_table_name, consumer_colocated_table_name));
+  ASSERT_OK(VerifyWrittenRecords(producer_colocated_table, consumer_colocated_table));
+
+  // Make sure we only colocated parent table and one non-colocated table
+  master::GetUniverseReplicationResponsePB resp;
+  ASSERT_OK(VerifyUniverseReplication(&resp));
+  ASSERT_EQ(resp.entry().tables_size(), 2);
+
+  auto producer_table2_name = ASSERT_RESULT(CreateYsqlTable(
+      /*idx=*/2, /*num_tablets=*/3, &producer_cluster_));
+  std::shared_ptr<client::YBTable> producer_table2;
+  ASSERT_OK(producer_client()->OpenTable(producer_table2_name, &producer_table2));
+
+  ASSERT_OK(InsertRowsInProducer(0, 50, producer_table2));
+
+  auto consumer_table2_name = ASSERT_RESULT(CreateYsqlTable(
+      /*idx=*/2, /*num_tablets=*/3, &consumer_cluster_));
+  std::shared_ptr<client::YBTable> consumer_table2;
+  ASSERT_OK(producer_client()->OpenTable(consumer_table2_name, &consumer_table2));
+
+  ASSERT_OK(VerifyWrittenRecords(producer_table2, consumer_table2));
+
+  auto producer_colocated_table2_name = ASSERT_RESULT(CreateYsqlTable(
+      /*idx=*/3, /*num_tablets=*/3, &producer_cluster_,
+      /*tablegroup_name=*/boost::none, /*colocated=*/true));
+  std::shared_ptr<client::YBTable> producer_colocated_table2;
+  ASSERT_OK(
+      producer_client()->OpenTable(producer_colocated_table2_name, &producer_colocated_table2));
+  ASSERT_OK(InsertRowsInProducer(0, 50, producer_colocated_table2));
+
+  auto consumer_colocated_table2_name = ASSERT_RESULT(CreateYsqlTable(
+      /*idx=*/3, /*num_tablets=*/3, &consumer_cluster_,
+      /*tablegroup_name=*/boost::none, /*colocated=*/true));
+  std::shared_ptr<client::YBTable> consumer_colocated_table2;
+  ASSERT_OK(
+      consumer_client()->OpenTable(consumer_colocated_table2_name, &consumer_colocated_table2));
+  ASSERT_OK(VerifyWrittenRecords(producer_colocated_table2, consumer_colocated_table2));
+
+  ASSERT_OK(DropYsqlTable(producer_cluster_, *producer_colocated_table));
+  ASSERT_OK(DropYsqlTable(consumer_cluster_, *consumer_colocated_table));
+
+  ASSERT_OK(VerifyUniverseReplication(&resp));
+  ASSERT_EQ(resp.entry().tables_size(), 3);
+
+  // Insert some rows to the initial table.
+  ASSERT_OK(InsertRowsInProducer(10, 20, producer_table_));
+  ASSERT_OK(InsertRowsInProducer(50, 100, producer_table2));
+  ASSERT_OK(VerifyWrittenRecords());
+
+  // Make sure the other table remains unchanged.
+  ASSERT_OK(VerifyWrittenRecords(producer_table2, consumer_table2));
+  ASSERT_OK(VerifyWrittenRecords(producer_colocated_table2, consumer_colocated_table2));
+
+  ASSERT_OK(DropYsqlTable(producer_cluster_, *producer_table2));
+  ASSERT_OK(DropYsqlTable(consumer_cluster_, *consumer_table2));
+
+  ASSERT_OK(VerifyUniverseReplication(&resp));
+  ASSERT_EQ(resp.entry().tables_size(), 2);
 }
 
 }  // namespace yb

src/yb/integration-tests/xcluster/xcluster_test_base.cc
@@ -219,7 +219,7 @@ Status XClusterTestBase::CreateDatabase(
     Cluster* cluster, const std::string& namespace_name, bool colocated) {
   auto conn = VERIFY_RESULT(cluster->Connect());
   return conn.ExecuteFormat(
-      "CREATE DATABASE $0$1", namespace_name, colocated ? " colocated = true" : "");
+      "CREATE DATABASE $0$1", namespace_name, colocated ? " COLOCATION = true" : "");
 }
 
 Status XClusterTestBase::DropDatabase(Cluster& cluster, const std::string& namespace_name) {

src/yb/integration-tests/xcluster/xcluster_ysql_test_base.cc
@@ -286,6 +286,7 @@ Result<YBTableName> XClusterYsqlTestBase::CreateYsqlTable(
       }
     }
   }
+  LOG_WITH_FUNC(INFO) << "Executing: " << query;
   RETURN_NOT_OK(conn.Execute(query));
 
   // Only check the schema name if it is set AND we created the table with a valid pgschema_name.

src/yb/master/CMakeLists.txt
@@ -136,6 +136,7 @@ set(MASTER_SRCS
   util/yql_vtable_helpers.cc
   xcluster/add_table_to_xcluster_source_task.cc
   xcluster/add_table_to_xcluster_target_task.cc
+  xcluster/master_xcluster_util.cc
   xcluster/xcluster_catalog_entity.cc
   xcluster/xcluster_config.cc
   xcluster/xcluster_consumer_metrics.cc

src/yb/master/catalog_entity_info.cc
@@ -960,6 +960,30 @@ bool TableInfo::IsColocatedUserTable() const {
   return colocated() && !IsColocationParentTable();
 }
 
+bool TableInfo::IsSequencesSystemTable() const {
+  if (GetTableType() == PGSQL_TABLE_TYPE && !IsColocationParentTable()) {
+    // This case commonly occurs during unit testing. Avoid unnecessary assert within Get().
+    if (!IsPgsqlId(namespace_id()) || !IsPgsqlId(id())) {
+      LOG(WARNING) << "Not PGSQL IDs " << namespace_id() << ", " << id();
+      return false;
+    }
+    Result<uint32_t> database_oid = GetPgsqlDatabaseOid(namespace_id());
+    if (!database_oid.ok()) {
+      LOG(WARNING) << "Invalid Namespace ID " << namespace_id();
+      return false;
+    }
+    Result<uint32_t> table_oid = GetPgsqlTableOid(id());
+    if (!table_oid.ok()) {
+      LOG(WARNING) << "Invalid Table ID " << id();
+      return false;
+    }
+    if (*database_oid == kPgSequencesDataDatabaseOid && *table_oid == kPgSequencesDataTableOid) {
+      return true;
+    }
+  }
+  return false;
+}
+
 TablespaceId TableInfo::TablespaceIdForTableCreation() const {
   SharedLock<decltype(lock_)> l(lock_);
   return tablespace_id_for_table_creation_;
@@ -1291,6 +1315,11 @@ void UniverseReplicationInfo::SetSetupUniverseReplicationErrorStatus(const Statu
 Status UniverseReplicationInfo::GetSetupUniverseReplicationErrorStatus() const {
   SharedLock<decltype(lock_)> l(lock_);
   return setup_universe_replication_error_;
+}
+
+bool UniverseReplicationInfo::IsDbScoped() const {
+  auto l = LockForRead();
+  return l->pb.has_db_scoped_info() && l->pb.db_scoped_info().namespace_infos_size() > 0;
 }
 
 // ================================================================================================

src/yb/master/catalog_entity_info.h
@@ -725,6 +725,7 @@ class TableInfo : public RefCountedThreadSafe<TableInfo>,
   bool IsColocatedDbParentTable() const;
   bool IsTablegroupParentTable() const;
   bool IsColocatedUserTable() const;
+  bool IsSequencesSystemTable() const;
 
   // Provides the ID of the tablespace that will be used to determine
   // where the tablets for this table should be placed when the table
@@ -1235,6 +1236,8 @@ class UniverseReplicationInfo : public UniverseReplicationInfoBase,
 
   // Get the Status of the last error from the current SetupUniverseReplication.
   Status GetSetupUniverseReplicationErrorStatus() const;
+
+  bool IsDbScoped() const;
 
  private:
   friend class RefCountedThreadSafe<UniverseReplicationInfo>;

src/yb/master/catalog_manager.cc
@@ -7455,10 +7455,8 @@ Status CatalogManager::GetTableSchemaInternal(const GetTableSchemaRequestPB* req
 
   // Due to pgschema_name being added after 2.13, older YSQL tables may not have this field.
   // So backfill pgschema_name for older YSQL tables. Skip for some special cases.
-  if (l->table_type() == TableType::PGSQL_TABLE_TYPE &&
-      resp->schema().pgschema_name().empty() &&
-      !table->is_system() &&
-      !IsSequencesSystemTable(*table) &&
+  if (l->table_type() == TableType::PGSQL_TABLE_TYPE && resp->schema().pgschema_name().empty() &&
+      !table->is_system() && !table->IsSequencesSystemTable() &&
       !table->IsColocationParentTable()) {
     SharedLock lock(mutex_);
     TRACE("Acquired catalog manager lock for schema name lookup");
@@ -7934,7 +7932,7 @@ bool CatalogManager::IsUserCreatedTable(const TableInfo& table) const {
 
 bool CatalogManager::IsUserCreatedTableUnlocked(const TableInfo& table) const {
   if (table.GetTableType() == PGSQL_TABLE_TYPE || table.GetTableType() == YQL_TABLE_TYPE) {
-    if (!IsSystemTable(table) && !IsSequencesSystemTable(table) &&
+    if (!IsSystemTable(table) && !table.IsSequencesSystemTable() &&
         GetNamespaceNameUnlocked(table.namespace_id()) != kSystemNamespaceName &&
         !table.IsColocationParentTable()) {
       return true;
@@ -7959,30 +7957,6 @@ bool CatalogManager::IsUserIndex(const TableInfo& table) const {
 
 bool CatalogManager::IsUserIndexUnlocked(const TableInfo& table) const {
   return IsUserCreatedTableUnlocked(table) && !table.indexed_table_id().empty();
-}
-
-bool CatalogManager::IsSequencesSystemTable(const TableInfo& table) const {
-  if (table.GetTableType() == PGSQL_TABLE_TYPE && !table.IsColocationParentTable()) {
-    // This case commonly occurs during unit testing. Avoid unnecessary assert within Get().
-    if (!IsPgsqlId(table.namespace_id()) || !IsPgsqlId(table.id())) {
-      LOG(WARNING) << "Not PGSQL IDs " << table.namespace_id() << ", " << table.id();
-      return false;
-    }
-    Result<uint32_t> database_oid = GetPgsqlDatabaseOid(table.namespace_id());
-    if (!database_oid.ok()) {
-      LOG(WARNING) << "Invalid Namespace ID " << table.namespace_id();
-      return false;
-    }
-    Result<uint32_t> table_oid = GetPgsqlTableOid(table.id());
-    if (!table_oid.ok()) {
-      LOG(WARNING) << "Invalid Table ID " << table.id();
-      return false;
-    }
-    if (*database_oid == kPgSequencesDataDatabaseOid && *table_oid == kPgSequencesDataTableOid) {
-      return true;
-    }
-  }
-  return false;
 }
 
 bool CatalogManager::IsMatviewTable(const TableInfo& table) const {

commit a3e9bd10f80f57c234d6c623dd82af4dd695508b

[#21827] DocDB: Fix ClientTest.TestCreateTableWithRangePartition
Summary:
`ClientTest.TestCreateTableWithRangePartition` was broken by e3e166d10abe05036609bbf2b902a338c44d0168/D33688
The test has a hard coded namespace Id that is not a valid PG namespace Id. This diff fixes the test by creating namespace and letting the system pick the proper namespace Id.

Fixes #21827
Jira: DB-10728

Test Plan: ClientTest.TestCreateTableWithRangePartition

Reviewers: tfoucher, jason

Reviewed By: tfoucher

Subscribers: ybase

Differential Revision: https://phorge.dev.yugabyte.com/D33856

src/yb/client/client-test.cc
@@ -170,7 +170,6 @@ constexpr int32_t kNoBound = kint32max;
 constexpr int kNumTablets = 2;
 
 const std::string kKeyspaceName = "my_keyspace";
-const std::string kPgsqlKeyspaceID = "1234567890abcdef1234567890abcdef";
 const std::string kPgsqlKeyspaceName = "psql" + kKeyspaceName;
 const std::string kPgsqlSchemaName = "my_schema";
 
@@ -489,6 +488,13 @@ class ClientTest: public YBMiniClusterTestBase<MiniCluster> {
     DEAD_TSERVER
   };
   void DoTestWriteWithDeadServer(WhichServerToKill which);
+
+  Result<NamespaceId> GetPGNamespaceId() {
+    master::GetNamespaceInfoResponsePB namespace_info;
+    RETURN_NOT_OK(client_->GetNamespaceInfo(
+        "" /* namespace_id */, kPgsqlKeyspaceName, YQL_DATABASE_PGSQL, &namespace_info));
+    return namespace_info.namespace_().id();
+  }
 
   YBSchema schema_;
 
@@ -2410,9 +2416,6 @@ TEST_F(ClientTest, TestCreateTableWithRangePartition) {
   const std::string kPgsqlTableId = "pgsqlrangepartitionedtableid";
   const size_t kColIdx = 1;
   const int64_t kKeyValue = 48238;
-  auto pgsql_table_name = YBTableName(
-      YQL_DATABASE_PGSQL, kPgsqlKeyspaceID, kPgsqlKeyspaceName, kPgsqlTableName);
-
   auto yql_table_name = YBTableName(YQL_DATABASE_CQL, kKeyspaceName, "yqlrangepartitionedtable");
 
   YBSchemaBuilder schema_builder;
@@ -2422,10 +2425,13 @@ TEST_F(ClientTest, TestCreateTableWithRangePartition) {
   // in GetTableSchema (part of OpenTable).
   schema_builder.SetSchemaName(kPgsqlSchemaName);
   YBSchema schema;
-  EXPECT_OK(client_->CreateNamespaceIfNotExists(kPgsqlKeyspaceName,
-                                                YQLDatabase::YQL_DATABASE_PGSQL,
-                                                "" /* creator_role_name */,
-                                                kPgsqlKeyspaceID));
+  EXPECT_OK(
+      client_->CreateNamespaceIfNotExists(kPgsqlKeyspaceName, YQLDatabase::YQL_DATABASE_PGSQL));
+  auto namespace_id = ASSERT_RESULT(GetPGNamespaceId());
+
+  auto pgsql_table_name =
+      YBTableName(YQL_DATABASE_PGSQL, namespace_id, kPgsqlKeyspaceName, kPgsqlTableName);
+
   // Create a PGSQL table using range partition.
   EXPECT_OK(schema_builder.Build(&schema));
   Status s = table_creator->table_name(pgsql_table_name)
@@ -2646,14 +2652,10 @@ TEST_F(ClientTest, GetNamespaceInfo) {
   GetNamespaceInfoResponsePB resp;
 
   // Setup.
-  ASSERT_OK(client_->CreateNamespace(kPgsqlKeyspaceName,
-                                     YQLDatabase::YQL_DATABASE_PGSQL,
-                                     "" /* creator_role_name */,
-                                     kPgsqlKeyspaceID,
-                                     "" /* source_namespace_id */,
-                                     boost::none /* next_pg_oid */,
-                                     nullptr /* txn */,
-                                     true /* colocated */));
+  ASSERT_OK(client_->CreateNamespace(
+      kPgsqlKeyspaceName, YQLDatabase::YQL_DATABASE_PGSQL, "" /* creator_role_name */,
+      "" /* namespace_id */, "" /* source_namespace_id */, boost::none /* next_pg_oid */,
+      nullptr /* txn */, true /* colocated */));
 
   // CQL non-colocated.
   ASSERT_OK(client_->GetNamespaceInfo(
@@ -2664,8 +2666,15 @@ TEST_F(ClientTest, GetNamespaceInfo) {
 
   // SQL colocated.
   ASSERT_OK(client_->GetNamespaceInfo(
-        kPgsqlKeyspaceID, "" /* namespace_name */, YQL_DATABASE_PGSQL, &resp));
-  ASSERT_EQ(resp.namespace_().id(), kPgsqlKeyspaceID);
+      "" /* namespace_id */, kPgsqlKeyspaceName, YQL_DATABASE_PGSQL, &resp));
+  ASSERT_EQ(resp.namespace_().name(), kPgsqlKeyspaceName);
+  ASSERT_EQ(resp.namespace_().database_type(), YQL_DATABASE_PGSQL);
+  ASSERT_TRUE(resp.colocated());
+  auto namespace_id = resp.namespace_().id();
+
+  ASSERT_OK(
+      client_->GetNamespaceInfo(namespace_id, "" /* namespace_name */, YQL_DATABASE_PGSQL, &resp));
+  ASSERT_EQ(resp.namespace_().id(), namespace_id);
   ASSERT_EQ(resp.namespace_().name(), kPgsqlKeyspaceName);
   ASSERT_EQ(resp.namespace_().database_type(), YQL_DATABASE_PGSQL);
   ASSERT_TRUE(resp.colocated());
@@ -2865,17 +2874,18 @@ TEST_F(ClientTest, LegacyColocatedDBColocatedTablesLookupTablet) {
   const auto kTabletLookupTimeout = 10s;
   const auto kNumTables = 10;
 
-  YBTableName common_table_name(
-      YQLDatabase::YQL_DATABASE_PGSQL, kPgsqlKeyspaceID, kPgsqlKeyspaceName, "table_name");
   ASSERT_OK(client_->CreateNamespace(
-      common_table_name.namespace_name(),
-      common_table_name.namespace_type(),
-      /* creator_role_name =*/ "",
-      common_table_name.namespace_id(),
-      /* source_namespace_id =*/ "",
-      /* next_pg_oid =*/ boost::none,
-      /* txn =*/ nullptr,
-      /* colocated =*/ true));
+      kPgsqlKeyspaceName, YQLDatabase::YQL_DATABASE_PGSQL,
+      /* creator_role_name =*/"",
+      /* namespace_id =*/"",
+      /* source_namespace_id =*/"",
+      /* next_pg_oid =*/boost::none,
+      /* txn =*/nullptr,
+      /* colocated =*/true));
+
+  auto namespace_id = ASSERT_RESULT(GetPGNamespaceId());
+  YBTableName common_table_name(
+      YQLDatabase::YQL_DATABASE_PGSQL, namespace_id, kPgsqlKeyspaceName, "table_name");
 
   YBSchemaBuilder schema_builder;
   schema_builder.AddColumn("key")->PrimaryKey()->Type(DataType::INT64);