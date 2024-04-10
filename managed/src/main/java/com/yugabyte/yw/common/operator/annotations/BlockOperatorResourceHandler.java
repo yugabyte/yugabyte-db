ad0dcbdeba897e004e33599c3f37e2a114990937
Daniel Shubin <dshubin@yugabyte.com>
2024-04-10 23:10:01 +0000

    [PLAT-13479][PLAT-11422] Kubernetes operator improvements
    Summary:
    The support bundle CRD now allows leaving out "components" fields, which will default
    to collecting all components.
    
    the auto-created provider should now also appear under "managed kubernetes services" tab
    
    Test Plan: tested support bundle create
    
    Reviewers: anijhawan
    
    Reviewed By: anijhawan
    
    Subscribers: yugaware
    
    Differential Revision: https://phorge.dev.yugabyte.com/D34005

--- managed/src/main/java/com/yugabyte/yw/common/operator/annotations/BlockOperatorResourceHandler.java
+++ managed/src/main/java/com/yugabyte/yw/common/operator/annotations/BlockOperatorResourceHandler.java
@@ -7,6 +7,7 @@ import com.google.re2j.Matcher;
 import com.google.re2j.Pattern;
 import com.yugabyte.yw.common.config.GlobalConfKeys;
 import com.yugabyte.yw.common.config.RuntimeConfigCache;
+import com.yugabyte.yw.models.Provider;
 import com.yugabyte.yw.models.Universe;
 import java.util.UUID;
 import java.util.concurrent.CompletableFuture;
@@ -39,32 +40,51 @@ public class BlockOperatorResourceHandler extends Action<BlockOperatorResource> 
 
     switch (configuration.resource()) {
       case UNIVERSE:
-        {
-          UUID universeUUID = null;
-          Pattern uniRegex = Pattern.compile(String.format(".*/universes/%s/?.*", UUID_PATTERN));
-          Matcher uniMatcher = uniRegex.matcher(request.path());
-          if (uniMatcher.find()) {
-            universeUUID = UUID.fromString(uniMatcher.group(1));
-          }
-          if (universeUUID == null) {
-            log.debug("No universe UUID found, assuming resource is not owned by the operator");
-            return delegate.call(request);
-          }
-
-          Universe universe = Universe.getOrBadRequest(universeUUID);
-          if (universe.getUniverseDetails().isKubernetesOperatorControlled) {
-            log.warn(
-                "blocking api call %s, universe is owned by kubernetes operator", request.path());
-            return CompletableFuture.completedFuture(
-                Results.forbidden("resource is controlled by kubernetes operator"));
-          }
-        }
-      default:
-        {
-          log.warn("unknown resource %s, skipping operator owned check", configuration.resource());
+        Pattern uniRegex = Pattern.compile(String.format(".*/universes/%s/?.*", UUID_PATTERN));
+        UUID universeUUID = getUUIDFromPath(uniRegex, request.path());
+        if (universeUUID == null) {
+          log.debug("No universe UUID found, assuming resource is not owned by the operator");
           return delegate.call(request);
         }
+
+        Universe universe = Universe.getOrBadRequest(universeUUID);
+        if (universe.getUniverseDetails().isKubernetesOperatorControlled) {
+          log.warn(
+              "blocking api call %s, universe is owned by kubernetes operator", request.path());
+          return CompletableFuture.completedFuture(
+              Results.forbidden("resource is controlled by kubernetes operator"));
+        }
+        return delegate.call(request);
+      case PROVIDER:
+        Pattern pattern =
+            Pattern.compile(String.format(".*/universe/.*/providers/%s/?.*", UUID_PATTERN));
+        UUID providerUUID = getUUIDFromPath(pattern, request.path());
+        if (providerUUID == null) {
+          log.debug("No provider UUID found, assuming resource is not owned by the operator");
+          return delegate.call(request);
+        }
+        Provider provider = Provider.getOrBadRequest(providerUUID);
+        if (provider.getDetails().getCloudInfo() != null
+            && provider.getDetails().getCloudInfo().kubernetes != null
+            && provider.getDetails().getCloudInfo().kubernetes.isKubernetesOperatorControlled) {
+          log.warn(
+              "blocking api call %s, provider is owned by kubernetes operator", request.path());
+          return CompletableFuture.completedFuture(
+              Results.forbidden("resource is controlled by kubernetes operator"));
+        }
+        return delegate.call(request);
+      default:
+        log.warn("unknown resource %s, skipping operator owned check", configuration.resource());
+        return delegate.call(request);
     }
+  }
+
+  private UUID getUUIDFromPath(Pattern pattern, String path) {
+    Matcher matcher = pattern.matcher(path);
+    if (matcher.find()) {
+      return UUID.fromString(matcher.group(1));
+    }
+    return null;
   }
 
   private Boolean enabled() {
