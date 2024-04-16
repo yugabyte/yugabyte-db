9510a10022bf103f9720f20528335e7bb51ab0fc
Euler Taveira <euler@eulerto.com>
2024-04-16 13:24:33 -0300

    Add version
    Packages might rely on the version from source code instead of
    extracting the version from the tarball file name.
    
    If the WAL2JSON_VERSION contains "dev" suffix it means it is not a
    released version. WAL2JSON_VERSION_NUM changes only for release. After a
    release, the next commit will add the "dev" suffix that will be removed
    only for the next release.

--- wal2json.c
+++ wal2json.c
@@ -34,6 +34,9 @@
 #include "utils/rel.h"
 #include "utils/syscache.h"
 
+#define WAL2JSON_VERSION				"2.5dev"
+#define WAL2JSON_VERSION_NUM			205
+
 #define	WAL2JSON_FORMAT_VERSION			2
 #define	WAL2JSON_FORMAT_MIN_VERSION		1
 
