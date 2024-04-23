42121fefb41c7673f3bbaf25c1e8d3287f5358df
Yury Shchetinin <yshchetinin@yugabyte.com>
2024-04-23 14:50:21 +0300

    [BACKPORT-2024.1][PLAT-13657] Set minimal required version for ListLiveTabletServers
    Summary:
    Original diff: 692b0fefcd2436107b88cc8fca8ff22997a54157/D34420
    
    ListLiveTabletServers API that was used for precheck functionality is not implemented for
    db versions less than 2.8. We should skip checks for versions earlier than that.
    
    Test Plan:
    create a universe with 2.6 version
    upgrade to 2.8 -> success
    
    Reviewers: cwang
    
    Reviewed By: cwang
    
    Subscribers: yugaware
    
    Tags: #jenkins-ready
    
    Differential Revision: https://phorge.dev.yugabyte.com/D34521

--- managed/src/main/java/com/yugabyte/yw/commissioner/tasks/subtasks/CheckClusterConsistency.java
+++ managed/src/main/java/com/yugabyte/yw/commissioner/tasks/subtasks/CheckClusterConsistency.java
@@ -14,6 +14,7 @@ import com.yugabyte.yw.common.config.UniverseConfKeys;
 import com.yugabyte.yw.common.gflags.GFlagsUtil;
 import com.yugabyte.yw.models.Customer;
 import com.yugabyte.yw.models.Universe;
+import com.yugabyte.yw.models.helpers.CommonUtils;
 import com.yugabyte.yw.models.helpers.NodeDetails;
 import java.util.ArrayList;
 import java.util.HashSet;
@@ -62,6 +63,16 @@ public class CheckClusterConsistency extends ServerSubTaskBase {
     boolean cloudEnabled =
         confGetter.getConfForScope(
             Customer.get(universe.getCustomerId()), CustomerConfKeys.cloudEnabled);
+    if (cloudEnabled) {
+      log.debug("Skipping check for ybm");
+      return;
+    }
+    String softwareVersion =
+        universe.getUniverseDetails().getPrimaryCluster().userIntent.ybSoftwareVersion;
+    if (CommonUtils.isReleaseBefore(CommonUtils.MIN_LIVE_TABLET_SERVERS_RELEASE, softwareVersion)) {
+      log.debug("ListLiveTabletServers is not supported for {} version", softwareVersion);
+      return;
+    }
     try (YBClient ybClient = ybService.getClient(masterAddresses, certificate)) {
       errors = doCheckServers(ybClient, universe, cloudEnabled);
     } catch (Exception e) {
