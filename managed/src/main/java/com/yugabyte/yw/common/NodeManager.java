f768f19cdebc117370e688ccc8ffb423226a3068
Shubham Varshney <svarshney@yugabyte.com>
2024-04-24 10:01:18 +0530

    [PLAT-13632] Use the sshUser configured in the provider in case machineImage is used
    Summary:
    In YBM, we still use the centos7 AMIs that is passed as `machineImage` param during
    universe create.
    imageBundle will have the `ec2-user` configured as the sshUser given the fact that we do not
    specify the AMI during
    provider creation & ends up creating YBA Managed Bundles.
    
    This diff falls back to sshUser configured in the provider in case `machineImage` is passed in
    the universe create params.
    
    Test Plan: Manually verified
    
    Reviewers: vbansal
    
    Reviewed By: vbansal
    
    Subscribers: yugaware
    
    Differential Revision: https://phorge.dev.yugabyte.com/D34448

--- managed/src/main/java/com/yugabyte/yw/common/NodeManager.java
+++ managed/src/main/java/com/yugabyte/yw/common/NodeManager.java
@@ -1947,6 +1947,11 @@ public class NodeManager extends DevopsBase {
               // Backward compatiblity.
               imageBundleDefaultImage = taskParam.getRegion().getYbImage();
             }
+            if (StringUtils.isNotBlank(taskParam.getMachineImage())) {
+              // YBM use case - in case machineImage is used for deploying the universe we should
+              // fallback to sshUser configured in the provider.
+              taskParam.sshUserOverride = provider.getDetails().getSshUser();
+            }
             String ybImage =
                 Optional.ofNullable(taskParam.getMachineImage()).orElse(imageBundleDefaultImage);
             if (ybImage != null && !ybImage.isEmpty()) {
