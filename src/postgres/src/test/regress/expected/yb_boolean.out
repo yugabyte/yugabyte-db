yugabyte/yugabyte-db

commit 81b8523b0fc4694084504d02ec0a3f5702636977

[#18407] DocDB: Build decoder for key columns
Summary:
The key columns could be queried by the user.
In this case, when suitable row is found, we decode key columns from key to PgTableRow.
But this decoding contains a lot of actions that could be done once per read RPC.

This diff splits decoding into 2 parts - build decoder and apply decoder.
So we could build decoder only once during read, then just apply this decoder to all rows.

Perf comparison using PgSingleTServerTest.ScanComplexPK:
master (a8800ed9641e4380674086a1ece5c3c5fa295823): 0.71s (full read), 0.49s (tserver time)
this diff: 0.62s (full read), 0.40s (tserver time)
Jira: DB-7398

Test Plan: PgSingleTServerTest.ScanComplexPK

Reviewers: mbautin, arybochkin, kpopali

Reviewed By: kpopali

Subscribers: bogdan, yql, ybase

Differential Revision: https://phorge.dev.yugabyte.com/D27184

src/yb/dockv/pg_key_decoder.cc
@@ -1,0 +1,390 @@
+// Copyright (c) YugabyteDB, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+// in compliance with the License.  You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software distributed under the License
+// is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+// or implied.  See the License for the specific language governing permissions and limitations
+// under the License.
+//
+
+#include "yb/dockv/pg_key_decoder.h"
+
+#include "yb/common/ql_type.h"
+#include "yb/common/schema.h"
+
+#include "yb/dockv/doc_kv_util.h"
+#include "yb/dockv/pg_row.h"
+#include "yb/dockv/reader_projection.h"
+
+#include "yb/util/algorithm_util.h"
+#include "yb/util/decimal.h"
+#include "yb/util/logging.h"
+
+namespace yb::dockv {
+
+namespace {
+
+template <SortOrder kSortOrder, bool kAppendZero>
+class StringDecoder {
+ public:
+  Result<const char*> Decode(
+      const char* input, const char* end, PgTableRow* row, size_t index) const {
+    return row->DecodeComparableString(index, input, end, kAppendZero, kSortOrder);
+  }
+
+  Result<const char*> Skip(const char* input, const char* end) const {
+    return kSortOrder == SortOrder::kAscending
+        ? SkipZeroEncodedStr(input, end)
+        : SkipComplementZeroEncodedStr(input, end);
+  }
+};
+
+template <SortOrder kSortOrder>
+class DecimalDecoder {
+ public:
+  static constexpr size_t kMinSize = 0;
+
+  Result<const char*> Decode(
+      const char* input, const char* end, PgTableRow* row, size_t index) const {
+    util::Decimal decimal;
+    size_t num_decoded_bytes = 0;
+    RETURN_NOT_OK(decimal.DecodeFromComparable(Slice(input, end), &num_decoded_bytes));
+
+    if (kSortOrder != SortOrder::kAscending) {
+      decimal.Negate();
+    }
+
+    row->SetBinary(index, decimal.EncodeToComparable(), true);
+    return input + num_decoded_bytes;
+  }
+
+  Result<const char*> Skip(const char* input, const char* end) const {
+    util::Decimal decimal;
+    size_t num_decoded_bytes = 0;
+    RETURN_NOT_OK(decimal.DecodeFromComparable(Slice(input, end), &num_decoded_bytes));
+    return input + num_decoded_bytes;
+  }
+};
+
+template <SortOrder kSortOrder, class Value>
+auto ApplySortOrder(Value value) {
+  return kSortOrder == SortOrder::kAscending ? value : ~value;
+}
+
+template <SortOrder kSortOrder>
+auto DecodePrimitive(const char* input, int32_t*) {
+  return bit_cast<uint32_t>(ApplySortOrder<kSortOrder>(util::DecodeInt32FromKey(input)));
+}
+
+template <SortOrder kSortOrder>
+int64_t DecodePrimitive(const char* input, int64_t*) {
+  return ApplySortOrder<kSortOrder>(util::DecodeInt64FromKey(input));
+}
+
+template <SortOrder kSortOrder>
+uint32_t DecodePrimitive(const char* input, uint32_t*) {
+  return ApplySortOrder<kSortOrder>(BigEndian::Load32(input));
+}
+
+template <SortOrder kSortOrder>
+uint64_t DecodePrimitive(const char* input, uint64_t*) {
+  return ApplySortOrder<kSortOrder>(BigEndian::Load64(input));
+}
+
+template <SortOrder kSortOrder>
+auto DecodePrimitive(const char* input, float*) {
+  return bit_cast<uint32_t>(util::DecodeFloatFromKey(input, kSortOrder != SortOrder::kAscending));
+}
+
+template <SortOrder kSortOrder>
+auto DecodePrimitive(const char* input, double*) {
+  return bit_cast<uint64_t>(util::DecodeDoubleFromKey(input, kSortOrder != SortOrder::kAscending));
+}
+
+template <class Value, SortOrder kSortOrder>
+class PrimitiveDecoder {
+ public:
+  static constexpr size_t kMinSize = sizeof(Value);
+
+  const char* Decode(const char* input, const char* end, PgTableRow* row, size_t index) const {
+    row->SetDatum(index, DecodePrimitive<kSortOrder>(input, static_cast<Value*>(nullptr)));
+    return input + sizeof(Value);
+  }
+
+  const char* Skip(const char* input, const char* end) const {
+    return input + sizeof(Value);
+  }
+};
+
+template <bool kMatchedId, bool kLastColumn>
+Status CallNextDecoder(
+    const char* input, const char* end, PgTableRow* row, size_t index, void*const* chain) {
+  if (kLastColumn) {
+    return Status::OK();
+  }
+  if (kMatchedId) {
+    ++index;
+  }
+  auto next_decoder = reinterpret_cast<PgKeyColumnDecoder>(*chain);
+  return next_decoder(input, end, row, index, ++chain);
+}
+
+template <bool kMatchedId, bool kLastColumn, KeyEntryType kEntryType>
+Status HandleDifferentEntryType(
+    const char* input, const char* end, PgTableRow* row, size_t index, void*const* chain,
+    KeyEntryType entry_type) {
+  if (entry_type == KeyEntryType::kNullLow || entry_type == KeyEntryType::kNullHigh) {
+    if (kMatchedId) {
+      row->SetNull(index);
+    }
+    return CallNextDecoder<kMatchedId, kLastColumn>(input, end, row, index, chain);
+  }
+  return STATUS_FORMAT(
+      Corruption, "Wrong key entry type $0 expected but $1 found", kEntryType, entry_type);
+}
+
+template <bool kConsumeGroupEnd, bool kMatchedId, bool kLastColumn, SortOrder kSortOrder,
+          KeyEntryType kEntryType, class Decoder>
+Status DecodeColumn(
+    const char* input, const char* end, PgTableRow* row, size_t index, void*const* chain) {
+  constexpr auto kPrefixSize = (kConsumeGroupEnd ? 1 : 0) + 1;
+  auto required_size = kPrefixSize + Decoder::kMinSize;
+  if (PREDICT_FALSE(input + required_size > end)) {
+    return STATUS_FORMAT(
+        Corruption, "Not enough bytes to decode $0: $1", kEntryType, end - input - kPrefixSize);
+  }
+  if (kConsumeGroupEnd) {
+    SCHECK_EQ(*input++, dockv::KeyEntryTypeAsChar::kGroupEnd,
+              Corruption, "Missing group end");
+  }
+  auto entry_type = static_cast<KeyEntryType>(*input++);
+  if (PREDICT_FALSE(entry_type != kEntryType)) {
+    return HandleDifferentEntryType<kMatchedId, kLastColumn, kEntryType>(
+        input, end, row, index, chain, entry_type);
+  }
+  Decoder decoder;
+  if (kMatchedId) {
+    input = OPTIONAL_VERIFY_RESULT(decoder.Decode(input, end, row, index));
+  } else {
+    input = OPTIONAL_VERIFY_RESULT(decoder.Skip(input, end));
+  }
+  return CallNextDecoder<kMatchedId, kLastColumn>(input, end, row, index, chain);
+}
+
+template <bool kConsumeGroupEnd, bool kMatchedId, bool kLastColumn, SortOrder kSortOrder>
+Status DecodeBoolColumn(
+    const char* input, const char* end, PgTableRow* row, size_t index, void*const* chain) {
+  if (kConsumeGroupEnd) {
+    SCHECK_EQ(*input++, dockv::KeyEntryTypeAsChar::kGroupEnd,
+              Corruption, "Missing group end");
+  }
+  auto entry_type = static_cast<KeyEntryType>(*input++);
+  constexpr auto kTrue = kSortOrder == SortOrder::kAscending
+      ? KeyEntryType::kTrue : KeyEntryType::kTrueDescending;
+  constexpr auto kFalse = kSortOrder == SortOrder::kAscending
+      ? KeyEntryType::kFalse : KeyEntryType::kFalseDescending;
+  if (entry_type == kTrue) {
+    if (kMatchedId) {
+      row->SetDatum(index, 1);
+    }
+  } else if (entry_type == kFalse) {
+    if (kMatchedId) {
+      row->SetDatum(index, 0);
+    }
+  } else if (entry_type == KeyEntryType::kNullLow) {
+    if (kMatchedId) {
+      row->SetNull(index);
+    }
+  } else {
+    return STATUS_FORMAT(Corruption, "Wrong key entry type $0 for bool column", entry_type);
+  }
+  return CallNextDecoder<kMatchedId, kLastColumn>(input, end, row, index, chain);
+}
+
+template <bool kConsumeGroupEnd, bool kMatchedId, bool kLastColumn, SortOrder kSortOrder,
+          bool kAppendZero>
+Status DecodeStringColumn(
+    const char* input, const char* end, PgTableRow* row, size_t index, void*const* chain) {
+  if (kConsumeGroupEnd) {
+    SCHECK_EQ(*input++, dockv::KeyEntryTypeAsChar::kGroupEnd,
+              Corruption, "Missing group end");
+  }
+  auto entry_type = static_cast<KeyEntryType>(*input++);
+  constexpr auto kRegularString = kSortOrder == SortOrder::kAscending
+      ? KeyEntryType::kString : KeyEntryType::kStringDescending;
+  constexpr auto kCollString = !kAppendZero
+      ? kRegularString
+      : kSortOrder == SortOrder::kAscending
+          ? KeyEntryType::kCollString : KeyEntryType::kCollStringDescending;
+  if (PREDICT_FALSE(entry_type != kRegularString && entry_type != kCollString)) {
+    return HandleDifferentEntryType<kMatchedId, kLastColumn, kRegularString>(
+        input, end, row, index, chain, entry_type);
+  }
+  StringDecoder<kSortOrder, kAppendZero> decoder;
+  if (kMatchedId) {
+    input = OPTIONAL_VERIFY_RESULT(decoder.Decode(input, end, row, index));
+  } else {
+    input = OPTIONAL_VERIFY_RESULT(decoder.Skip(input, end));
+  }
+  return CallNextDecoder<kMatchedId, kLastColumn>(input, end, row, index, chain);
+}
+
+template <bool kConsumeGroupEnd, bool kMatchedId, bool kLastColumn, SortOrder kSortOrder>
+struct DecodeColumnFactory {
+  template <KeyEntryType kAscEntryType, KeyEntryType kDescEntryType, class Decoder>
+  static PgKeyColumnDecoder Apply() {
+    return DecodeColumn<
+        kConsumeGroupEnd, kMatchedId, kLastColumn, kSortOrder,
+        kSortOrder == SortOrder::kAscending ? kAscEntryType : kDescEntryType, Decoder>;
+  }
+
+  static PgKeyColumnDecoder ApplyBoolColumn() {
+    return DecodeBoolColumn<kConsumeGroupEnd, kMatchedId, kLastColumn, kSortOrder>;
+  }
+
+  template <bool kAppendZero>
+  static PgKeyColumnDecoder ApplyStringColumn() {
+    return DecodeStringColumn<kConsumeGroupEnd, kMatchedId, kLastColumn, kSortOrder, kAppendZero>;
+  }
+};
+
+template <bool kConsumeGroupEnd, bool kMatchedId, bool kLastColumn, SortOrder kSortOrder>
+PgKeyColumnDecoder GetDecoder5(const ColumnSchema& column) {
+  using Factory = DecodeColumnFactory<kConsumeGroupEnd, kMatchedId, kLastColumn, kSortOrder>;
+  auto data_type = column.type()->main();
+  switch (data_type) {
+    case DataType::BINARY:
+      return Factory::template ApplyStringColumn<false>();
+    case DataType::STRING:
+      return Factory::template ApplyStringColumn<true>();
+    case DataType::DECIMAL:
+      return Factory::template Apply<
+          KeyEntryType::kDecimal, KeyEntryType::kDecimalDescending,
+          DecimalDecoder<kSortOrder>>();
+    case DataType::INT8: [[fallthrough]];
+    case DataType::INT16: [[fallthrough]];
+    case DataType::INT32:
+      return Factory::template Apply<
+          KeyEntryType::kInt32, KeyEntryType::kInt32Descending,
+          PrimitiveDecoder<int32_t, kSortOrder>>();
+    case DataType::INT64:
+      return Factory::template Apply<
+          KeyEntryType::kInt64, KeyEntryType::kInt64Descending,
+          PrimitiveDecoder<int64_t, kSortOrder>>();
+    case DataType::UINT32:
+      return Factory::template Apply<
+          KeyEntryType::kUInt32, KeyEntryType::kUInt32Descending,
+          PrimitiveDecoder<uint32_t, kSortOrder>>();
+    case DataType::UINT64:
+      return Factory::template Apply<
+          KeyEntryType::kUInt64, KeyEntryType::kUInt64Descending,
+          PrimitiveDecoder<uint64_t, kSortOrder>>();
+    case DataType::BOOL:
+      return Factory::ApplyBoolColumn();
+    case DataType::FLOAT:
+      return Factory::template Apply<
+          KeyEntryType::kFloat, KeyEntryType::kFloatDescending,
+          PrimitiveDecoder<float, kSortOrder>>();
+    case DataType::DOUBLE:
+      return Factory::template Apply<
+          KeyEntryType::kDouble, KeyEntryType::kDoubleDescending,
+          PrimitiveDecoder<double, kSortOrder>>();
+    default:
+      break;
+  }
+
+  LOG(FATAL) << "Data type not supported: " << data_type;
+  return nullptr;
+}
+
+template <bool kConsumeGroupEnd, bool kMatchedId, bool kLastColumn>
+PgKeyColumnDecoder GetDecoder4(const ColumnSchema& column) {
+  auto sorting_type = column.sorting_type();
+  if (sorting_type == SortingType::kDescending ||
+      sorting_type == SortingType::kDescendingNullsLast) {
+    return GetDecoder5<kConsumeGroupEnd, kMatchedId, kLastColumn, SortOrder::kDescending>(column);
+  } else {
+    return GetDecoder5<kConsumeGroupEnd, kMatchedId, kLastColumn, SortOrder::kAscending>(column);
+  }
+}
+
+template <bool kConsumeGroupEnd, bool kMatchedId>
+PgKeyColumnDecoder GetDecoder3(const ColumnSchema& column, bool last_column) {
+  if (last_column) {
+    return GetDecoder4<kConsumeGroupEnd, kMatchedId, true>(column);
+  } else {
+    return GetDecoder4<kConsumeGroupEnd, kMatchedId, false>(column);
+  }
+}
+
+template <bool kConsumeGroupEnd>
+PgKeyColumnDecoder GetDecoder2(const ColumnSchema& column, bool matched_id, bool last_column) {
+  if (matched_id) {
+    return GetDecoder3<kConsumeGroupEnd, true>(column, last_column);
+  } else {
+    return GetDecoder3<kConsumeGroupEnd, false>(column, last_column);
+  }
+}
+
+PgKeyColumnDecoder GetDecoder(
+    const ColumnSchema& column, bool consume_group_end, bool matched_id, bool last_column) {
+  if (consume_group_end) {
+    return GetDecoder2<true>(column, matched_id, last_column);
+  } else {
+    return GetDecoder2<false>(column, matched_id, last_column);
+  }
+}
+
+Status NopDecoder(const char*, const char*, PgTableRow*, size_t, void*const*) {
+  return Status::OK();
+}
+
+Status UpdateSlice(
+  const char* input, const char* end, PgTableRow* row, size_t index, void*const* chain) {
+  Slice* slice = static_cast<Slice*>(*chain);
+  *slice = Slice(input, end);
+  return Status::OK();
+}
+
+} // namespace
+
+PgKeyDecoder::PgKeyDecoder(const Schema& schema, const ReaderProjection& projection) {
+  if (projection.num_key_columns == 0) {
+    decoders_.push_back(NopDecoder);
+    return;
+  }
+  size_t out_idx = 0;
+  decoders_.reserve(schema.num_key_columns());
+  for (size_t k = 0;; ++k) {
+    auto matched_id = schema.column_id(k) == projection.columns[out_idx].id;
+    auto last_column = matched_id && ++out_idx >= projection.num_key_columns;
+    decoders_.push_back(GetDecoder(
+        schema.columns()[k], k && k == schema.num_hash_key_columns(), matched_id, last_column));
+    if (last_column) {
+      break;
+    }
+  }
+}
+
+PgKeyDecoder::~PgKeyDecoder() = default;
+
+Status PgKeyDecoder::Decode(Slice key, PgTableRow* out) const {
+  auto* decoders = decoders_.data();
+  return (*decoders)(key.cdata(), key.cend(), out, 0, reinterpret_cast<void*const*>(decoders + 1));
+}
+
+Status PgKeyDecoder::DecodeEntry(
+    Slice* key, const ColumnSchema& column, PgTableRow* out, size_t index) {
+  auto decoder = GetDecoder(column, false, true, false);
+  void* chain[] = {
+    reinterpret_cast<void*>(UpdateSlice),
+    key,
+  };
+  return decoder(key->cdata(), key->cend(), out, index, chain);
+}
+
+}  // namespace yb::dockv

src/yb/docdb/doc_rowwise_iterator_base.cc
@@ -68,11 +68,11 @@ Status StoreValue(
   return Status::OK();
 }
 
-Status StoreValue(
+/*Status StoreValue(
     const Schema& schema, const dockv::ProjectedColumn& column, size_t col_idx,
     dockv::DocKeyDecoder* decoder, dockv::PgTableRow* row) {
   return row->DecodeKey(col_idx, decoder->mutable_input());
-}
+}*/
 
 } // namespace
 
@@ -379,7 +379,32 @@ Status DocRowwiseIteratorBase::CopyKeyColumnsToRow(
 
 Status DocRowwiseIteratorBase::CopyKeyColumnsToRow(
     const dockv::ReaderProjection& projection, dockv::PgTableRow* row) {
-  return DoCopyKeyColumnsToRow(projection, row);
+//  RETURN_NOT_OK(DoCopyKeyColumnsToRow(projection, row));
+  if (!row) {
+    return Status::OK();
+  }
+  if (!pg_key_decoder_) {
+    pg_key_decoder_.emplace(doc_read_context_.schema(), projection);
+  }
+  return pg_key_decoder_->Decode(
+      row_key_.AsSlice().WithoutPrefix(doc_read_context_.key_prefix_encoded_len()), row);
+/*
+  dockv::PgTableRow temp_row(row->projection());
+  CHECK_OK(doc_read_context_.pg_row_key_decoder().Decode(
+      row_key_.AsSlice().WithoutPrefix(doc_read_context_.key_prefix_encoded_len()),
+      &temp_row));
+  for (size_t i = 0; i != row->projection().num_key_columns; ++i) {
+    auto o1 = row->GetValueByIndex(i);
+    auto o2 = temp_row.GetValueByIndex(i);
+    if (!o1 || !o2) {
+      CHECK(!o1 && !o2) << "index: " << i;
+      continue;
+    }
+    auto v1 = o1->ToQLValuePB(row->projection().columns[i].data_type);
+    auto v2 = o1->ToQLValuePB(row->projection().columns[i].data_type);
+    CHECK_EQ(v1.ShortDebugString(), v2.ShortDebugString()) << "index: " << i;
+  }
+  return Status::OK();*/
 }
 
 template <class Row>

src/yb/docdb/doc_rowwise_iterator_base.h
@@ -28,6 +28,7 @@
 #include "yb/docdb/read_operation_data.h"
 
 #include "yb/dockv/dockv_fwd.h"
+#include "yb/dockv/pg_key_decoder.h"
 #include "yb/dockv/reader_projection.h"
 
 #include "yb/util/status_fwd.h"
@@ -179,6 +180,7 @@ class DocRowwiseIteratorBase : public YQLRowwiseIteratorIf {
   dockv::KeyBytes row_key_;
 
   const dockv::ReaderProjection& projection_;
+  std::optional<dockv::PgKeyDecoder> pg_key_decoder_;
 
   // Key for seeking a YSQL tuple. Used only when the table has a cotable id.
   boost::optional<dockv::KeyBytes> tuple_key_;

src/yb/docdb/docdb_rocksdb_util.cc
@@ -613,6 +613,7 @@ void InitRocksDBOptions(
   } else {
     options->write_buffer_size = FLAGS_memstore_size_mb * 1_MB;
   }
+  LOG(INFO) << log_prefix << "Write buffer size: " << options->write_buffer_size;
   options->env = tablet_options.rocksdb_env;
   options->checkpoint_env = rocksdb::Env::Default();
   options->priority_thread_pool_for_compactions_and_flushes = GetGlobalPriorityThreadPool();

src/yb/dockv/CMakeLists.txt
@@ -33,6 +33,7 @@ set(DOCKV_SRCS
     expiration.cc
     intent.cc
     key_bytes.cc
+    pg_key_decoder.cc
     pg_row.cc
     packed_row.cc
     partial_row.cc

src/yb/dockv/doc_kv_util.cc
@@ -78,6 +78,16 @@ void Xor(Ch* begin, Ch* end) {
   }
 }
 
+template<char kChar, class Out>
+void Xor(Out* out, size_t old_size) {
+  Xor<kChar>(out->data() + old_size, out->data() + out->size());
+}
+
+template<char kChar>
+void Xor(ValueBuffer* out, size_t old_size) {
+  Xor<kChar>(out->mutable_data() + old_size, out->mutable_data() + out->size());
+}
+
 template <bool desc>
 void AppendEncodedStrToKey(const Slice& s, KeyBuffer *dest) {
   const auto* p = s.cdata();
@@ -105,12 +115,10 @@ inline void TerminateEncodedKeyStr(KeyBuffer *dest) {
 }
 
 template<char kEndOfString, class Out>
-Status DecodeEncodedStr(Slice* slice, Out* result) {
+Result<const char*> DecodeEncodedStr(const char* p, const char* end, Out* result) {
   static_assert(kEndOfString == '\0' || kEndOfString == '\xff',
                 "Invalid kEndOfString character. Only '\0' and '\xff' accepted");
   constexpr char kEndOfStringEscape = kEndOfString ^ 1;
-  const char* p = slice->cdata();
-  const char* end = p + slice->size();
   auto old_size = result->size();
 
   // Loop invariant: remaining bytes to be processed are [p, end)
@@ -118,7 +126,7 @@ Status DecodeEncodedStr(Slice* slice, Out* result) {
     auto stop = Find<kEndOfString>(p, end);
     if (PREDICT_FALSE(stop >= end - 1)) {
       if (stop == end) {
-        if (slice->empty()) {
+        if (p == end) {
           return STATUS(Corruption, "Encoded string is empty");
         }
         return STATUS(
@@ -146,8 +154,15 @@ Status DecodeEncodedStr(Slice* slice, Out* result) {
     result->append(p, stop + 1);
     p = stop + 2;
   } while (p != end);
-  slice->remove_prefix(p - slice->cdata());
-  Xor<kEndOfString>(result->data() + old_size, result->data() + result->size());
+  Xor<kEndOfString>(result, old_size);
+  return p;
+}
+
+template<char kEndOfString, class Out>
+Status DecodeEncodedStr(Slice* slice, Out* result) {
+  const auto* begin = VERIFY_RESULT(DecodeEncodedStr<kEndOfString>(
+     slice->cdata(), slice->cend(), result));
+  *slice = Slice(begin, slice->cend());
   return Status::OK();
 }
 
@@ -217,6 +232,25 @@ Status DecodeZeroEncodedStr(Slice* slice, std::string* result) {
     return DecodeEncodedStr<'\0'>(slice, &dev_null);
   }
   return DecodeEncodedStr<'\0'>(slice, result);
+}
+
+Result<const char*> DecodeZeroEncodedStr(const char* begin, const char* end, ValueBuffer* out) {
+  return DecodeEncodedStr<'\0'>(begin, end, out);
+}
+
+Result<const char*> SkipZeroEncodedStr(const char* begin, const char* end) {
+  DevNull dev_null;
+  return DecodeEncodedStr<'\0'>(begin, end, &dev_null);
+}
+
+Result<const char*> DecodeComplementZeroEncodedStr(
+    const char* begin, const char* end, ValueBuffer* out) {
+  return DecodeEncodedStr<'\xff'>(begin, end, out);
+}
+
+Result<const char*> SkipComplementZeroEncodedStr(const char* begin, const char* end) {
+  DevNull dev_null;
+  return DecodeEncodedStr<'\xff'>(begin, end, &dev_null);
 }
 
 Result<std::string> DecodeZeroEncodedStr(const Slice& encoded_str) {

src/yb/dockv/doc_kv_util.h
@@ -110,6 +110,8 @@ inline std::string ComplementZeroEncodeStr(const Slice& s) {
 //   result - the resulting decoded string
 // Returns OK status if and only if string is properly encoded.
 Status DecodeZeroEncodedStr(Slice* slice, std::string* result);
+Result<const char*> DecodeZeroEncodedStr(const char* begin, const char* end, ValueBuffer* out);
+Result<const char*> SkipZeroEncodedStr(const char* begin, const char* end);
 
 // A version of the above function that ensures the encoding is correct and all characters are
 // consumed.
@@ -124,6 +126,9 @@ Result<std::string> DecodeZeroEncodedStr(const Slice& encoded_str);
 //   result - the resulting decoded string
 // Returns OK status if and only if string is properly encoded.
 Status DecodeComplementZeroEncodedStr(Slice* slice, std::string* result);
+Result<const char*> DecodeComplementZeroEncodedStr(
+    const char* begin, const char* end, ValueBuffer* out);
+Result<const char*> SkipComplementZeroEncodedStr(const char* begin, const char* end);
 
 Result<std::string> DecodeComplementZeroEncodedStr(const Slice& encoded_str);
 

src/yb/dockv/dockv_fwd.h
@@ -30,6 +30,7 @@ class KeyBytes;
 class KeyEntryValue;
 class Partition;
 class PartitionSchema;
+class PgKeyDecoder;
 class PgTableRow;
 class PgValue;
 class PrimitiveValue;

commit ae79e6626f704c3cc2ce09a00589c0fa045a8040

[#22121] YSQL: fix error on bool NULLS LAST index
Summary:
Commit 81b8523b0fc4694084504d02ec0a3f5702636977 introduced
DecodeBoolColumn which covers true, false, `kNullLow`, but not
`kNullHigh`, throwing an error in that case:

    ERROR:  Wrong key entry type kNullHigh for bool column

In other words, if a secondary index (since PK does not support null)
has a bool column sorted NULLS LAST, any SELECT trying to decode null
values from that column from that index will hit the above error.  (Note
that ASC is NULLS LAST by default.)  Handle that case.

YCQL is not affected since it does not use this decoding code.
Jira: DB-11047

Test Plan:
On Almalinux 8:

    ./yb_build.sh fastdebug --gcc11 \
      --java-test 'TestPgRegressMisc#testPgRegressMiscIndependent'

Backport-through: 2.20
Close: #22121

Reviewers: sergei

Reviewed By: sergei

Subscribers: yql, ybase

Differential Revision: https://phorge.dev.yugabyte.com/D34430

src/yb/dockv/pg_key_decoder.cc
@@ -234,7 +234,7 @@ UnsafeStatus DecodeBoolColumn(
     if (kMatchedId) {
       row->SetDatum(index, 0);
     }
-  } else if (entry_type == KeyEntryType::kNullLow) {
+  } else if (entry_type == KeyEntryType::kNullLow || entry_type == KeyEntryType::kNullHigh) {
     if (kMatchedId) {
       row->SetNull(index);
     }

src/postgres/src/test/regress/expected/yb_boolean.out
@@ -1,0 +1,39 @@
+CREATE TABLE bools (b bool);
+INSERT INTO bools VALUES (null);
+CREATE INDEX NONCONCURRENTLY asc_nulls_last ON bools (b ASC NULLS LAST); -- = (b ASC)
+CREATE INDEX NONCONCURRENTLY asc_nulls_first ON bools (b ASC NULLS FIRST);
+CREATE INDEX NONCONCURRENTLY desc_nulls_last ON bools (b DESC NULLS LAST);
+CREATE INDEX NONCONCURRENTLY desc_nulls_first ON bools (b DESC NULLS FIRST); -- = (b DESC)
+/*+IndexOnlyScan(bools asc_nulls_last)*/ SELECT * FROM bools;
+ b
+---
+
+(1 row)
+
+/*+IndexOnlyScan(bools asc_nulls_first)*/ SELECT * FROM bools;
+ b
+---
+
+(1 row)
+
+/*+IndexOnlyScan(bools desc_nulls_last)*/ SELECT * FROM bools;
+ b
+---
+
+(1 row)
+
+/*+IndexOnlyScan(bools desc_nulls_first)*/ SELECT * FROM bools;
+ b
+---
+
+(1 row)
+
+/*+IndexOnlyScan(bools asc_nulls_last)*/ SELECT FROM bools;
+--
+(1 row)
+
+/*+IndexOnlyScan(bools asc_nulls_last)*/ SELECT count(*) FROM bools;
+ count
+-------
+     1
+(1 row)

src/postgres/src/test/regress/sql/yb_boolean.sql
@@ -1,0 +1,13 @@
+CREATE TABLE bools (b bool);
+INSERT INTO bools VALUES (null);
+CREATE INDEX NONCONCURRENTLY asc_nulls_last ON bools (b ASC NULLS LAST); -- = (b ASC)
+CREATE INDEX NONCONCURRENTLY asc_nulls_first ON bools (b ASC NULLS FIRST);
+CREATE INDEX NONCONCURRENTLY desc_nulls_last ON bools (b DESC NULLS LAST);
+CREATE INDEX NONCONCURRENTLY desc_nulls_first ON bools (b DESC NULLS FIRST); -- = (b DESC)
+/*+IndexOnlyScan(bools asc_nulls_last)*/ SELECT * FROM bools;
+/*+IndexOnlyScan(bools asc_nulls_first)*/ SELECT * FROM bools;
+/*+IndexOnlyScan(bools desc_nulls_last)*/ SELECT * FROM bools;
+/*+IndexOnlyScan(bools desc_nulls_first)*/ SELECT * FROM bools;
+
+/*+IndexOnlyScan(bools asc_nulls_last)*/ SELECT FROM bools;
+/*+IndexOnlyScan(bools asc_nulls_last)*/ SELECT count(*) FROM bools;

src/postgres/src/test/regress/yb_misc_independent_schedule
@@ -6,6 +6,7 @@
 ####################################################################################################
 
 test: yb_catalog_version
+test: yb_boolean
 test: yb_disabled
 test: yb_errors
 test: yb_truncate