a56f0debc044c7a192dd84b1d05c072ba37de63a
Spikatrix <12792882+Spikatrix@users.noreply.github.com>
2024-05-03 06:45:26 +0000

    Add assessment refactoring suggestion/error table UI

--- yugabyted-ui/ui/src/features/clusters/details/migration/steps/assessment/AssessmentRefactoring.tsx
+++ yugabyted-ui/ui/src/features/clusters/details/migration/steps/assessment/AssessmentRefactoring.tsx
@@ -1,7 +1,16 @@
-import React, { FC } from "react";
-import { Box, Divider, MenuItem, Paper, Typography, makeStyles } from "@material-ui/core";
+import React, { FC, Fragment, useMemo } from "react";
+import {
+  Box,
+  Divider,
+  MenuItem,
+  Paper,
+  TableCell,
+  TableRow,
+  Typography,
+  makeStyles,
+} from "@material-ui/core";
 import { useTranslation } from "react-i18next";
-import { YBSelect } from "@app/components";
+import { YBCodeBlock, YBInput, YBSelect, YBTable, YBToggle } from "@app/components";
 import {
   Bar,
   BarChart,
@@ -14,7 +23,9 @@ import {
   XAxis,
   YAxis,
 } from "recharts";
+import SearchIcon from "@app/assets/search.svg";
 import type { NameType, ValueType } from "recharts/types/component/DefaultTooltipContent";
+import clsx from "clsx";
 
 const useStyles = makeStyles((theme) => ({
   heading: {
@@ -23,13 +34,12 @@ const useStyles = makeStyles((theme) => ({
   label: {
     color: theme.palette.grey[500],
     fontWeight: theme.typography.fontWeightMedium as number,
-    marginBottom: theme.spacing(0.75),
+    marginBottom: theme.spacing(0.25),
     textTransform: "uppercase",
     textAlign: "left",
   },
-  selectBox: {
-    marginTop: theme.spacing(-0.5),
-    minWidth: "220px",
+  fullWidth: {
+    width: "100%",
   },
   divider: {
     margin: theme.spacing(1, 0, 1, 0),
@@ -40,6 +50,31 @@ const useStyles = makeStyles((theme) => ({
     borderRadius: theme.shape.borderRadius,
     boxShadow: theme.shadows[2],
   },
+  tableRow: {
+    cursor: "pointer",
+    "&:hover": {
+      backgroundColor: theme.palette.background.default,
+    },
+  },
+  tableCell: {
+    padding: theme.spacing(1, 2),
+    maxWidth: 120,
+    wordBreak: "break-word",
+  },
+  rowTableCell: {
+    paddingTop: "10px",
+    borderBottom: "unset",
+  },
+  actionsCell: {
+    width: theme.spacing(8),
+  },
+  queryTableCell: {
+    padding: theme.spacing(1),
+  },
+  queryCodeBlock: {
+    lineHeight: 1.5,
+    padding: theme.spacing(1),
+  },
 }));
 
 interface MigrationAssessmentRefactoringProps {
@@ -49,16 +84,130 @@ interface MigrationAssessmentRefactoringProps {
     automaticDDLImport: number;
     manualRefactoring: number;
   }>;
+  suggestionsErrors: ReadonlyArray<{
+    objectType: string;
+    filePath: string;
+    reason: string;
+    sql: string;
+    issue: string;
+  }>;
 }
+
+type SugErrAckArray = Array<
+  MigrationAssessmentRefactoringProps["suggestionsErrors"][number] & { ack: boolean }
+>;
+
+const getRowCellComponent = (
+  displayedRows: SugErrAckArray,
+  classes: ReturnType<typeof useStyles>
+) => {
+  const rowCellComponent = (data: any, dataIndex: number) => {
+    return (
+      <Fragment key={`row-fragment-${data}`}>
+        <TableRow className={classes.tableRow}>
+          {data.map((val: any, index: number) => (
+            <TableCell
+              key={`row-${dataIndex}-body-cell-${index}`}
+              className={clsx(classes.tableCell, classes.rowTableCell)}
+            >
+              {typeof val === "function" ? val(dataIndex) : val}
+            </TableCell>
+          ))}
+        </TableRow>
+        {!displayedRows[dataIndex].ack && (
+          <TableRow>
+            <TableCell colSpan={7} className={classes.queryTableCell}>
+              <YBCodeBlock
+                text={
+                  <>
+                    {displayedRows[dataIndex].sql}
+                    <Divider className={classes.divider} />
+                    <Typography variant="body2">{displayedRows[dataIndex].reason}</Typography>
+                    <Divider className={classes.divider} />
+                    <Typography variant="body2">{displayedRows[dataIndex].issue}</Typography>
+                  </>
+                }
+                preClassName={classes.queryCodeBlock}
+              />
+            </TableCell>
+          </TableRow>
+        )}
+      </Fragment>
+    );
+  };
+  return rowCellComponent;
+};
 
 export const MigrationAssessmentRefactoring: FC<MigrationAssessmentRefactoringProps> = ({
   schemaList,
   sqlObjects,
+  suggestionsErrors,
 }) => {
   const classes = useStyles();
   const { t } = useTranslation();
 
-  const [selectedNode, setSelectedNode] = React.useState<string>("");
+  const [selectedSchema, setSelectedSchema] = React.useState<string>("");
+  const [search, setSearch] = React.useState<string>("");
+  const [selectedObjectType, setSelectedObjectType] = React.useState<string>("");
+  const [selectedAck, setSelectedAck] = React.useState<string>("");
+
+  const [sugErrData, setSugErrData] = React.useState<SugErrAckArray>(
+    suggestionsErrors.map((sugErr) => ({ ...sugErr, ack: false }))
+  );
+
+  const filteredSugErr = useMemo(() => {
+    const searchQuery = search.toLowerCase();
+    return sugErrData.filter(
+      (sugErr) =>
+        (selectedSchema ? sugErr.objectType === selectedSchema : true) &&
+        (selectedObjectType ? sugErr.objectType === selectedObjectType : true) &&
+        (selectedAck ? sugErr.ack === (selectedAck === "Acknowledged") : true) &&
+        (search
+          ? sugErr.filePath.toLowerCase().includes(searchQuery) ||
+            sugErr.reason.toLowerCase().includes(searchQuery)
+          : true)
+    );
+  }, [search, selectedSchema, selectedObjectType, selectedAck, sugErrData]);
+
+  const suggestionErrorColumns = [
+    {
+      name: "objectType",
+      label: t("clusterDetail.voyager.planAndAssess.refactoring.objectType"),
+      options: {
+        setCellHeaderProps: () => ({ style: { padding: "8px 16px" } }),
+      },
+    },
+    {
+      name: "filePath",
+      label: t("clusterDetail.voyager.planAndAssess.refactoring.fileDirectory"),
+      options: {
+        setCellHeaderProps: () => ({ style: { padding: "8px 16px" } }),
+      },
+    },
+    {
+      name: "ack",
+      label: t("clusterDetail.voyager.planAndAssess.refactoring.acknowledge"),
+      options: {
+        sort: false,
+        customBodyRenderLite: (dataIndex: number) => (
+          <YBToggle
+            checked={filteredSugErr[dataIndex].ack}
+            label={t("clusterDetail.voyager.planAndAssess.refactoring.acknowledge")}
+            onChange={(e) => {
+              setSugErrData((prev) =>
+                prev.map((sugErr, index) => ({
+                  ...sugErr,
+                  ack: index === dataIndex ? e.target.checked : sugErr.ack,
+                }))
+              );
+            }}
+          />
+        ),
+        setCellHeaderProps: () => ({ style: { padding: "8px 16px" } }),
+        setCellProps: () => ({ style: { padding: "8px 16px" } }),
+      },
+    },
+  ];
 
   return (
     <Paper>
@@ -78,9 +227,9 @@ export const MigrationAssessmentRefactoring: FC<MigrationAssessmentRefactoringPr
             {t("clusterDetail.voyager.planAndAssess.refactoring.schema")}
           </Typography>
           <YBSelect
-            className={classes.selectBox}
-            value={selectedNode}
-            onChange={(e) => setSelectedNode(e.target.value)}
+            style={{ minWidth: "250px" }}
+            value={selectedSchema}
+            onChange={(e) => setSelectedSchema(e.target.value)}
           >
             <MenuItem value="">All</MenuItem>
             <Divider className={classes.divider} />
@@ -178,6 +327,72 @@ export const MigrationAssessmentRefactoring: FC<MigrationAssessmentRefactoringPr
               />
             </BarChart>
           </ResponsiveContainer>
+        </Box>
+
+        <Divider orientation="horizontal" variant="middle" className={classes.divider} />
+
+        <Box display="flex" alignItems="center" gridGap={10} my={2}>
+          <Box flex={3}>
+            <Typography variant="body1" className={classes.label}>
+              {t("clusterDetail.voyager.planAndAssess.refactoring.search")}
+            </Typography>
+            <YBInput
+              className={classes.fullWidth}
+              placeholder={t("clusterDetail.voyager.planAndAssess.refactoring.searchPlaceholder")}
+              InputProps={{
+                startAdornment: <SearchIcon />,
+              }}
+              onChange={(ev) => setSearch(ev.target.value)}
+              value={search}
+            />
+          </Box>
+          <Box flex={1}>
+            <Typography variant="body1" className={classes.label}>
+              {t("clusterDetail.voyager.planAndAssess.refactoring.objectType")}
+            </Typography>
+            <YBSelect
+              className={classes.fullWidth}
+              value={selectedObjectType}
+              onChange={(e) => setSelectedObjectType(e.target.value)}
+            >
+              <MenuItem value="">All</MenuItem>
+              <Divider className={classes.divider} />
+              {sqlObjects.map((object) => {
+                return (
+                  <MenuItem key={object.objectType} value={object.objectType}>
+                    {object.objectType}
+                  </MenuItem>
+                );
+              })}
+            </YBSelect>
+          </Box>
+          <Box flex={1}>
+            <Typography variant="body1" className={classes.label}>
+              {t("clusterDetail.voyager.planAndAssess.refactoring.acknowledged")}
+            </Typography>
+            <YBSelect
+              className={classes.fullWidth}
+              value={selectedAck}
+              onChange={(e) => setSelectedAck(e.target.value)}
+            >
+              <MenuItem value="">All</MenuItem>
+              <Divider className={classes.divider} />
+              <MenuItem value="Acknowledged">Acknowledged</MenuItem>
+              <MenuItem value="Not acknowledged">Not acknowledged</MenuItem>
+            </YBSelect>
+          </Box>
+        </Box>
+
+        <Box>
+          <YBTable
+            data={filteredSugErr}
+            columns={suggestionErrorColumns}
+            options={{
+              customRowRender: getRowCellComponent(filteredSugErr, classes),
+              pagination: true,
+            }}
+            withBorder={false}
+          />
         </Box>
       </Box>
     </Paper>
