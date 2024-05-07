bd0536e74817b34b76871865fa807f73ae92e70f
tnayak <tanujnay112@live.com>
2024-05-06 19:27:34 -0700

    [#21954] YSQL: Prevent BNL in cases with unbound batchable index filters on bound columns
    Summary:
    In `yb_get_batched_index_paths` it was wrongly assumed that any batchable clause with its LHS belonging to an indexed column in the restriction list must be bound to the index as an index condition. However, it can be the case that such batchable clauses might have the LHS as `indexed_col::bpchar` where `indexed_col` is an indexed text column. In such cases, the LHS doesn't exactly match an index column and, leaving the whole clause to be treated as a qpqual/index filter.
    
    Because of this assumption, `yb_get_batched_index_paths` misses checking if such unbound batchable clauses are present. There is already a check to prevent batching in the presence of a qual whose LHS column is not one that's bound to any batched clause (i.e: not present in `batched_inner_attnos`). This diff moves logic here to also prevent batching in the other case where such a qual might have an LHS column that's already bound to a batched clause.
    
    One can technically also achieve the desired effect by maintaining a list of bound index conditions and checking amongst the quals to see which one is bound but this would be a bit more computationally intensive.
    
    Needs backports to: 2024.1, 2.20, 2.18
    Jira: DB-10870
    
    Test Plan: ./yb_build.sh --java-test 'org.yb.pgsql.TestPgRegressJoin'
    
    Reviewers: mtakahara
    
    Reviewed By: mtakahara
    
    Subscribers: yql
    
    Differential Revision: https://phorge.dev.yugabyte.com/D34809

--- src/postgres/src/test/regress/sql/yb_join_batching.sql
+++ src/postgres/src/test/regress/sql/yb_join_batching.sql
@@ -645,6 +645,12 @@ DROP TABLE x2;
 DROP TABLE x3;
 
 SET yb_bnl_batch_size = 3;
+create table ss1(a varchar(1024), b char(10));
+create table ss2(a varchar(1024));
+create index on ss2 (a asc);
+explain (costs off) select * from ss1 t1, ss2 t2 where t1.a = t2.a and t1.b = t2.a;
+drop table ss1;
+drop table ss2;
 
 --
 --
