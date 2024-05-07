yugabyte/yugabyte-db

commit 55aac07e3d4de9d5e76b16209f784f0f8f67fe41

[#18507] YSQL: Fix bug: yb_catalog_version() does not exist
Summary:
A customer reported that backup failed and the reason is that the function
`yb_catalog_version()` does not exist. This function is introduced in the
migration script `V1__3979__pg_yb_catalog_version.sql`.

After debugging I found there is a code bug in the YSQL upgrade code.
Specifically:

```
Result<int> GetMajorVersionFromSystemCatalogState(PGConn* pgconn) {
  int major_version = 0;

  // Helper macro removing boilerplate.
    if (VERIFY_RESULT(oneliner_with_result)) { \
      ++major_version; \
    } else { \
      return major_version; \
    }

  // V1: #3979 introducing pg_yb_catalog_version table.
  INCREMENT_VERSION_OR_RETURN_IT(SystemTableHasRows(pgconn, "pg_yb_catalog_version"))
```
This function tries to determine which version we should start from. For
example, it checks the existence of a non-empty table `pg_yb_catalog_version` to
decide whether we need to skip V1 migration script or not. The V1 migration
script has two blocks:

* The first block introduces the table `pg_catalog.pg_yb_catalog_version`
* The second block introduces the function `yb_catalog_version()`

In a 2.4.x or 2.6.x cluster, `pg_catalog.pg_yb_catalog_version` already exists
but `yb_catalog_version()` does not. As a result, the above code will skip V1
migration script and that's why function `yb_catalog_version` isn't introduced.

I have verified that any older release < 2.4 (e.g., 2.2.7.0), or any newer
release > 2.6 (e.g., 2.8.0.0) do not have this bug. In an older release < 2.4,
`pg_catalog.pg_yb_catalog_version` does not exist so the above code
will not skip V1 migration script. In a newer release > 2.6, `yb_catalog_version()`
also exists so it is correct to skip V1 migration script.

The customer cluster was manually fixed by reapplying V1 migration script. I
made a fix such that after the normal migration has completed, check whether

src/yb/yql/pgwrapper/ysql_upgrade.cc
@@ -421,12 +421,29 @@ Status YsqlUpgradeHelper::Upgrade() {
     }
   }
 
+  // Fix for https://github.com/yugabyte/yugabyte-db/issues/18507:
+  // This bug only shows up when upgrading from 2.4.x or 2.6.x, in these
+  // two releases the table pg_yb_catalog_version exists but the function
+  // yb_catalog_version does not exist. However we have skipped V1 because
+  // SystemTableHasRows(pgconn, "pg_yb_catalog_version") returns true.
+  for (auto& entry : databases) {
+    auto conn = VERIFY_RESULT(entry->GetConnection());
+    if (!VERIFY_RESULT(FunctionExists(conn.get(), "yb_catalog_version"))) {
+      LOG(WARNING) << "Function yb_catalog_version is missing in " << entry->database_name_;
+      // Run V1 migration script to introduce function "yb_catalog_version".
+      const Version version = {0, 0};
+      RETURN_NOT_OK(MigrateOnce(entry.get(), &version));
+    } else {
+      LOG(INFO) << "Found function yb_catalog_version in " << entry->database_name_;
+    }
+  }
+
   return Status::OK();
 }
 
-Status YsqlUpgradeHelper::MigrateOnce(DatabaseEntry* db_entry) {
+Status YsqlUpgradeHelper::MigrateOnce(DatabaseEntry* db_entry, const Version* historical_version) {
   const auto& db_name = db_entry->database_name_;
-  const auto& version = db_entry->version_;
+  const auto& version = historical_version ? *historical_version : db_entry->version_;
 
   auto pgconn = VERIFY_RESULT(db_entry->GetConnection());
 
@@ -476,6 +493,11 @@ Status YsqlUpgradeHelper::MigrateOnce(DatabaseEntry* db_entry) {
   if (!catalog_version_migration_applied_) {
     SleepFor(MonoDelta::FromMilliseconds(2 * heartbeat_interval_ms_));
     catalog_version_migration_applied_ = true;
+  }
+
+  if (historical_version) {
+    LOG(INFO) << db_name << ": migration successfully applied without version bump";
+    return Status::OK();
   }
 
   RETURN_NOT_OK_PREPEND(

src/yb/yql/pgwrapper/ysql_upgrade.h
@@ -49,7 +49,8 @@ class YsqlUpgradeHelper {
   Result<std::unique_ptr<DatabaseEntry>> MakeDatabaseEntry(std::string database_name);
 
   // Migrate a given database to the next version, updating it in the given database entry.
-  Status MigrateOnce(DatabaseEntry* db_entry);
+  // If historical_version isn't nullptr, use it to override db_entry->version_.
+  Status MigrateOnce(DatabaseEntry* db_entry, const Version* historical_version = nullptr);
 
   const HostPort ysql_proxy_addr_;
 

commit 7a78311f78673c6a93efeceff2aa30c5f66d804b

[#22283] YSQL: Fix ysql upgrade exception in single connection mode
Summary:
To reproduce the bug, follow these steps

1. Download old release package for yugabyte-2.6.9.0
2. ./bin/yb-ctl create --rf 1 --master_flags initial_sys_catalog_snapshot_path=$HOME/tmp/yugabyte-2.6.9.0/share/initial_sys_catalog_snapshot
3. ./build/latest/bin/yb-admin --timeout_ms=720000 upgrade_ysql use_single_connection

Step 3 fails with an error like:
```
Error running upgrade_ysql: Internal error (yb/yql/pgwrapper/ysql_upgrade.cc:259): Unable to upgrade YSQL cluster: New connection is requested before the old one is released!

```

In YSQL upgrade single connection mode, we assert that there cannot be an
existing connection when trying to make a new connection to a given database.

A recent commit 55aac07e3d4de9d5e76b16209f784f0f8f67fe41 introduced a bug that
violated this assertion. The relevant code:

```
450   // Fix for https://github.com/yugabyte/yugabyte-db/issues/18507:
451   // This bug only shows up when upgrading from 2.4.x or 2.6.x, in these
452   // two releases the table pg_yb_catalog_version exists but the function
453   // yb_catalog_version does not exist. However we have skipped V1 because
454   // SystemTableHasRows(pgconn, "pg_yb_catalog_version") returns true.
455   for (auto& entry : databases) {
456     auto conn = VERIFY_RESULT(entry->GetConnection());
457     if (!VERIFY_RESULT(FunctionExists(conn.get(), "yb_catalog_version"))) {
458       LOG(WARNING) << "Function yb_catalog_version is missing in " << entry->database_name_;
459       // Run V1 migration script to introduce function "yb_catalog_version".
460       const Version version = {0, 0};
461       RETURN_NOT_OK(MigrateOnce(entry.get(), &version));
462     } else {
463       LOG(INFO) << "Found function yb_catalog_version in " << entry->database    _name_;
464     }
465   }

```

In particular, at line 456 we already get a connection, before `conn` goes out of

src/yb/yql/pgwrapper/ysql_upgrade.cc
@@ -384,6 +384,11 @@ YsqlUpgradeHelper::MakeDatabaseEntry(std::string database_name) {
   return entry;
 }
 
+Result<bool> YsqlUpgradeHelper::HasYbCatalogVersion(DatabaseEntry* db_entry) {
+  auto conn = VERIFY_RESULT(db_entry->GetConnection());
+  return FunctionExists(conn.get(), "yb_catalog_version");
+}
+
 Status YsqlUpgradeHelper::Upgrade() {
   RETURN_NOT_OK(AnalyzeMigrationFiles());
   LOG(INFO) << "Latest version defined in migrations is " << latest_version_;
@@ -453,8 +458,7 @@ Status YsqlUpgradeHelper::Upgrade() {
   // yb_catalog_version does not exist. However we have skipped V1 because
   // SystemTableHasRows(pgconn, "pg_yb_catalog_version") returns true.
   for (auto& entry : databases) {
-    auto conn = VERIFY_RESULT(entry->GetConnection());
-    if (!VERIFY_RESULT(FunctionExists(conn.get(), "yb_catalog_version"))) {
+    if (!VERIFY_RESULT(HasYbCatalogVersion(entry.get()))) {
       LOG(WARNING) << "Function yb_catalog_version is missing in " << entry->database_name_;
       // Run V1 migration script to introduce function "yb_catalog_version".
       const Version version = {0, 0};

src/yb/yql/pgwrapper/ysql_upgrade.h
@@ -52,6 +52,9 @@ class YsqlUpgradeHelper {
   // If historical_version isn't nullptr, use it to override db_entry->version_.
   Status MigrateOnce(DatabaseEntry* db_entry, const Version* historical_version = nullptr);
 
+  // Check whether function yb_catalog_version exists in a database.
+  Result<bool> HasYbCatalogVersion(DatabaseEntry* db_entry);
+
   const HostPort ysql_proxy_addr_;
 
   const uint64_t ysql_auth_key_;