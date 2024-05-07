yugabyte/yugabyte-db

commit 9bb04fd5c25b5894c14dd1418ddf8292f16d2fd4

[#22258] docdb: Move tcmalloc profiling code to util directory
Summary:
The code for tcmalloc profiling is currently in src/yb/server/pprof-path-handlers-util.h / .cc. It should be in the utils folder instead.
Jira: DB-11176

Test Plan: Built with `--use_gperftools_tcmalloc` and `--use_google_tcmalloc` and ran `tcmalloc_profile-test.cc` tests.

Reviewers: kfranz

Reviewed By: kfranz

Subscribers: esheng, ybase

Differential Revision: https://phorge.dev.yugabyte.com/D34747

src/yb/util/tcmalloc_profile.cc
@@ -10,34 +10,29 @@
 // or implied.  See the License for the specific language governing permissions and limitations
 // under the License.
 
-#include "yb/server/pprof-path-handlers_util.h"
-
-#include <cstdint>
-#include <iomanip>
-#include <string>
-#include <utility>
-#include <unordered_map>
+#include "yb/util/tcmalloc_profile.h"
 
 #if YB_ABSL_ENABLED
 #include "absl/debugging/symbolize.h"
 #endif
 
-#include "yb/util/logging.h"
-
-#include "yb/gutil/strings/numbers.h"
-#include "yb/util/format.h"
+#include "yb/gutil/casts.h"
+#include "yb/util/flags/flag_tags.h"
 #include "yb/util/monotime.h"
 #include "yb/util/symbolize.h"
-#include "yb/util/tcmalloc_util.h"
-#include "yb/util/url-coding.h"
 
-using std::vector;
+DEFINE_RUNTIME_int32(dump_heap_snapshot_min_interval_sec, 600,
+    "The minimum time to wait between dumping heap snapshots. A value of <= 0 means the logging is "
+    "disabled.");
+DEFINE_RUNTIME_int32(dump_heap_snapshot_max_call_stacks, 10,
+    "The maximum number of call stacks to log from TCMalloc's heap snapshot when TryDumpSnapshot "
+    " is called.");
 
 namespace yb {
 
 #if YB_TCMALLOC_ENABLED
 namespace {
-void SortSamplesByOrder(vector<Sample>* samples, SampleOrder order) {
+void SortSamplesByOrder(std::vector<Sample>* samples, SampleOrder order) {
   std::sort(
       samples->begin(),
       samples->end(),
@@ -67,6 +62,38 @@ bool Symbolize(void *pc, char *out, int out_size) {
 }  // namespace
 #endif
 
+Result<std::vector<Sample>> GetAggregateAndSortHeapSnapshot(
+    SampleOrder order, HeapSnapshotType snapshot_type, SampleFilter filter) {
+#if YB_GPERFTOOLS_TCMALLOC
+  switch (order) {
+    case SampleOrder::kSampledBytes:
+    case SampleOrder::kSampledCount:
+      break;
+    case SampleOrder::kEstimatedBytes:
+      return STATUS(
+          NotSupported, Format("Invalid sample order $0 used with gperftools tcmalloc", order));
+  }
+  switch (snapshot_type) {
+    case HeapSnapshotType::kPeakHeap:
+      return STATUS(NotSupported, "peak_heap is not supported with gperftools tcmalloc");
+    case HeapSnapshotType::kCurrentHeap:
+      break;
+  }
+  switch (filter) {
+    case SampleFilter::kGrowthOnly:
+      return STATUS(NotSupported, "growth-only snapshot is not supported with gperftools tcmalloc");
+    case SampleFilter::kAllSamples:
+      break;
+  }
+  return GetAggregateAndSortHeapSnapshotGperftools(order);
+#elif YB_GOOGLE_TCMALLOC
+  auto current_profile = GetHeapSnapshot(snapshot_type);
+  return AggregateAndSortProfile(current_profile, filter, order);
+#else
+  return STATUS(NotSupported, "Heap snapshot is only available if tcmalloc is enabled.");
+#endif
+}
+
 #if YB_GOOGLE_TCMALLOC
 Result<tcmalloc::Profile> GetHeapProfile(int seconds, int64_t sample_freq_bytes) {
   static std::atomic<bool> heap_profile_running{false};
@@ -89,15 +116,19 @@ Result<tcmalloc::Profile> GetHeapProfile(int seconds, int64_t sample_freq_bytes)
 }
 
 tcmalloc::Profile GetHeapSnapshot(HeapSnapshotType snapshot_type) {
-  if (snapshot_type == HeapSnapshotType::kPeakHeap) {
-    return tcmalloc::MallocExtension::SnapshotCurrent(tcmalloc::ProfileType::kPeakHeap);
-  } else {
-    return tcmalloc::MallocExtension::SnapshotCurrent(tcmalloc::ProfileType::kHeap);
+  switch (snapshot_type) {
+    case HeapSnapshotType::kPeakHeap:
+      return tcmalloc::MallocExtension::SnapshotCurrent(tcmalloc::ProfileType::kPeakHeap);
+    case HeapSnapshotType::kCurrentHeap:
+      return tcmalloc::MallocExtension::SnapshotCurrent(tcmalloc::ProfileType::kHeap);
+    default:
+      LOG_WITH_FUNC(DFATAL) << "Invalid snapshot type " << snapshot_type;
+      return tcmalloc::Profile();
   }
 }
 
-vector<Sample> AggregateAndSortProfile(
-    const tcmalloc::Profile& profile, bool only_growth, SampleOrder order) {
+std::vector<Sample> AggregateAndSortProfile(
+    const tcmalloc::Profile& profile, SampleFilter filter, SampleOrder order) {
   LOG(INFO) << "Analyzing TCMalloc sampling profile";
   int failed_symbolizations = 0;
   std::unordered_map<std::string, SampleInfo> samples_map;
@@ -113,7 +144,7 @@ vector<Sample> AggregateAndSortProfile(
     // If we only want growth, exclude samples for which we saw a deallocation event.
     // "Censored" means we observed an allocation but not a deallocation. (Deallocation-only events
     // are not reported).
-    if (only_growth && !sample.is_censored) {
+    if (filter == SampleFilter::kGrowthOnly && !sample.is_censored) {
       return;
     }
 
@@ -179,12 +210,9 @@ void* GetSampleProgramCounter(void** entry, uintptr_t i) {
 }
 } // namespace
 
-std::vector<Sample> GetAggregateAndSortHeapSnapshot(SampleOrder order) {
-  if (order != SampleOrder::kSampledBytes && order != SampleOrder::kSampledCount) {
-    LOG(WARNING) << Format("Invalid sample order $0 used with gperftools tcmalloc", order);
-    return {};
-  }
-
+// Do not call this directly, instead use GetAggregateAndSortHeapSnapshot.
+// Assumes that the supplied sample order is valid for gperftools tcmalloc.
+std::vector<Sample> GetAggregateAndSortHeapSnapshotGperftools(SampleOrder order) {
   int sample_period;
   void** samples = MallocExtension::instance()->ReadStackTraces(&sample_period);
 
@@ -231,76 +259,5 @@ std::vector<Sample> GetAggregateAndSortHeapSnapshot(SampleOrder order) {
 }
 
 #endif // YB_GPERFTOOLS_TCMALLOC
-
-namespace {
-  std::string FormatNumericTableRow(const std::string& value) {
-    return Format("<td align=\"right\">$0</td>", value);
-  }
-
-  std::string FormatNumericTableRow(int64_t value) {
-    return FormatNumericTableRow(SimpleItoaWithCommas(value));
-  }
-
-  std::string FormatNumericTableRow(std::optional<int64_t> value) {
-    if (value) {
-      return FormatNumericTableRow(*value);
-    }
-    return FormatNumericTableRow("N/A");
-  }
-} // namespace
-
-void GenerateTable(std::stringstream* output, const std::vector<Sample>& samples,
-    const std::string& title, size_t max_call_stacks, SampleOrder order) {
-  // Generate the output table.
-  (*output) << std::fixed;
-  (*output) << std::setprecision(2);
-  (*output) << Format("<b>Top $0 call stacks for:</b> $1<br>\n", max_call_stacks, title);
-  if (samples.size() > max_call_stacks) {
-    (*output) << Format("$0 call stacks omitted<br>\n", samples.size() - max_call_stacks);
-  }
-  (*output) << Format("<b>Ordering call stacks by:</b> $0<br>\n", order);
-  (*output) << Format(
-      "<b>Current sampling frequency:</b> $0 bytes (on average)<br>\n",
-      GetTCMallocSamplingFrequency());
-  (*output) << Format("Values shown below are for allocations still in use "
-      "(i.e., objects that have been deallocated are not included)<br>\n");
-  (*output) << "<p>\n";
-  (*output) << "<table style=\"border-collapse: collapse\" border=1>\n";
-  (*output) << "<style>td, th { padding: 5px; }</style>";
-  (*output) << "<tr>\n";
-  (*output) << "<th>Estimated Bytes</th>\n";
-  (*output) << "<th>Estimated Count</th>\n";
-  (*output) << "<th>Avg Bytes Per Allocation</th>\n";
-  (*output) << "<th>Sampled Bytes</th>\n";
-  (*output) << "<th>Sampled Count</th>\n";
-  (*output) << "<th>Call Stack</th>\n";
-  (*output) << "</tr>\n";
-
-  for (size_t i = 0; i < std::min(max_call_stacks, samples.size()); ++i) {
-    const auto& entry = samples.at(i);
-    (*output) << "<tr>";
-
-    (*output) << FormatNumericTableRow(entry.second.estimated_bytes);
-
-    (*output) << FormatNumericTableRow(entry.second.estimated_count);
-
-    std::optional<int64_t> avg_bytes;
-    if (entry.second.sampled_count > 0) {
-      avg_bytes = std::round(
-          static_cast<double>(entry.second.sampled_allocated_bytes) / entry.second.sampled_count);
-    }
-    (*output) << FormatNumericTableRow(avg_bytes);
-
-    (*output) << FormatNumericTableRow(entry.second.sampled_allocated_bytes);
-
-    (*output) << FormatNumericTableRow(entry.second.sampled_count);
-
-    // Call stack.
-    (*output) << Format("<td><pre>$0</pre></td>", EscapeForHtmlToString(entry.first));
-
-    (*output) << "</tr>";
-  }
-  (*output) << "</table>";
-}
 
 } // namespace yb

src/yb/common/CMakeLists.txt
@@ -118,7 +118,6 @@ set(COMMON_LIBS
 
 if (NOT APPLE)
   list(APPEND COMMON_LIBS libuuid)
-  list(APPEND COMMON_LIBS absl)
 endif()
 
 ADD_YB_LIBRARY(yb_common

src/yb/server/CMakeLists.txt
@@ -89,7 +89,6 @@ set(SERVER_PROCESS_SRCS
   generic_service.cc
   glog_metrics.cc
   monitored_task.cc
-  pprof-path-handlers_util.cc
   pprof-path-handlers.cc
   rpc_server.cc
   rpcz-path-handler.cc
@@ -120,16 +119,12 @@ if("${YB_TCMALLOC_ENABLED}" STREQUAL "1")
   target_link_libraries(server_process profiler)
 endif()
 
-# This module depends on tcmalloc and profiler directly, so need to make
-# sure that they get linked in the right order.
-
 #########################################
 # server_process tests
 #########################################
 
 set(YB_TEST_LINK_LIBS server_process ${YB_MIN_TEST_LIBS})
 ADD_YB_TEST(webserver-test)
-ADD_YB_TEST(pprof-path-handler_util-test)
 
 #########################################
 # auto_flags_manager_common

src/yb/server/pprof-path-handlers.cc
@@ -50,6 +50,7 @@
 #endif  // YB_GPERFTOOLS_TCMALLOC
 
 #include <fstream>
+#include <iomanip>
 #include <string>
 #include <vector>
 
@@ -62,7 +63,6 @@
 #include "yb/gutil/strings/substitute.h"
 #include "yb/gutil/sysinfo.h"
 
-#include "yb/server/pprof-path-handlers_util.h"
 #include "yb/server/webserver.h"
 
 #include "yb/util/callsite_profiling.h"
@@ -73,7 +73,8 @@
 #include "yb/util/status_log.h"
 #include "yb/util/status.h"
 #include "yb/util/symbolize.h"
-#include "yb/util/tcmalloc_impl_util.h"
+#include "yb/util/tcmalloc_profile.h"
+#include "yb/util/tcmalloc_util.h"
 #include "yb/util/url-coding.h"
 
 #include "yb/common/path-handler-util.h"
@@ -114,6 +115,77 @@ SampleOrder ParseSampleOrder(const Webserver::ArgumentMap& parsed_args) {
   return SampleOrder::kSampledCount;
 }
 
+namespace {
+std::string FormatNumericTableRow(const std::string& value) {
+  return Format("<td align=\"right\">$0</td>", value);
+}
+
+std::string FormatNumericTableRow(int64_t value) {
+  return FormatNumericTableRow(SimpleItoaWithCommas(value));
+}
+
+std::string FormatNumericTableRow(std::optional<int64_t> value) {
+  if (value) {
+    return FormatNumericTableRow(*value);
+  }
+  return FormatNumericTableRow("N/A");
+}
+
+void GenerateTable(std::stringstream* output, const std::vector<Sample>& samples,
+    const std::string& title, size_t max_call_stacks, SampleOrder order) {
+  // Generate the output table.
+  (*output) << std::fixed;
+  (*output) << std::setprecision(2);
+  (*output) << Format("<b>Top $0 call stacks for:</b> $1<br>\n", max_call_stacks, title);
+  if (samples.size() > max_call_stacks) {
+    (*output) << Format("$0 call stacks omitted<br>\n", samples.size() - max_call_stacks);
+  }
+  (*output) << Format("<b>Ordering call stacks by:</b> $0<br>\n", order);
+  (*output) << Format(
+      "<b>Current sampling frequency:</b> $0 bytes (on average)<br>\n",
+      GetTCMallocSamplingFrequency());
+  (*output) << Format("Values shown below are for allocations still in use "
+      "(i.e., objects that have been deallocated are not included)<br>\n");
+  (*output) << "<p>\n";
+  (*output) << "<table style=\"border-collapse: collapse\" border=1>\n";
+  (*output) << "<style>td, th { padding: 5px; }</style>";
+  (*output) << "<tr>\n";
+  (*output) << "<th>Estimated Bytes</th>\n";
+  (*output) << "<th>Estimated Count</th>\n";
+  (*output) << "<th>Avg Bytes Per Allocation</th>\n";
+  (*output) << "<th>Sampled Bytes</th>\n";
+  (*output) << "<th>Sampled Count</th>\n";
+  (*output) << "<th>Call Stack</th>\n";
+  (*output) << "</tr>\n";
+
+  for (size_t i = 0; i < std::min(max_call_stacks, samples.size()); ++i) {
+    const auto& entry = samples.at(i);
+    (*output) << "<tr>";
+
+    (*output) << FormatNumericTableRow(entry.second.estimated_bytes);
+
+    (*output) << FormatNumericTableRow(entry.second.estimated_count);
+
+    std::optional<int64_t> avg_bytes;
+    if (entry.second.sampled_count > 0) {
+      avg_bytes = std::round(
+          static_cast<double>(entry.second.sampled_allocated_bytes) / entry.second.sampled_count);
+    }
+    (*output) << FormatNumericTableRow(avg_bytes);
+
+    (*output) << FormatNumericTableRow(entry.second.sampled_allocated_bytes);
+
+    (*output) << FormatNumericTableRow(entry.second.sampled_count);
+
+    // Call stack.
+    (*output) << Format("<td><pre>$0</pre></td>", EscapeForHtmlToString(entry.first));
+
+    (*output) << "</tr>";
+  }
+  (*output) << "</table>";
+}
+} // namespace
+
 // pprof asks for the url /pprof/heap to get heap information. This should be implemented
 // by calling HeapProfileStart(filename), continue to do work, and then, some number of
 // seconds later, call GetHeapProfile() followed by HeapProfilerStop().
@@ -131,6 +203,7 @@ static void PprofHeapHandler(const Webserver::WebRequest& req,
   // Whether to only report allocations that do not have a corresponding deallocation.
   bool only_growth = ParseLeadingBoolValue(
       FindWithDefault(req.parsed_args, "only_growth", ""), false);
+  SampleFilter filter = only_growth ? SampleFilter::kGrowthOnly : SampleFilter::kAllSamples;
 
   SampleOrder order = ParseSampleOrder(req.parsed_args);
 
@@ -149,7 +222,7 @@ static void PprofHeapHandler(const Webserver::WebRequest& req,
     (*output) << profile.status().message();
     return;
   }
-  auto samples = AggregateAndSortProfile(*profile, only_growth, order);
+  auto samples = AggregateAndSortProfile(*profile, filter, order);
   GenerateTable(output, samples, title, 1000 /* max_call_stacks */, order);
 #endif  // YB_GOOGLE_TCMALLOC
 
@@ -177,36 +250,21 @@ static void PprofHeapHandler(const Webserver::WebRequest& req,
 #endif  // YB_TCMALLOC_ENABLED
 }
 
-void PprofHeapSnapshotHandler(const Webserver::WebRequest& req,
-                              Webserver::WebResponse* resp) {
-  std::stringstream *output = &resp->output;
-#if !YB_TCMALLOC_ENABLED
-  (*output) << "Heap snapshot is only available if tcmalloc is enabled.";
-  return;
-#else
-
+void PprofHeapSnapshotHandler(const Webserver::WebRequest& req, Webserver::WebResponse* resp) {
+  std::stringstream* output = &resp->output;
   bool peak_heap = ParseLeadingBoolValue(FindWithDefault(req.parsed_args, "peak_heap", ""), false);
   SampleOrder order = ParseSampleOrder(req.parsed_args);
 
-  string title = peak_heap ? "Peak heap snapshot" : "Current heap snapshot";
-#ifdef YB_GOOGLE_TCMALLOC
-  auto profile = GetHeapSnapshot(
-      peak_heap ? HeapSnapshotType::kPeakHeap : HeapSnapshotType::kCurrentHeap);
-  vector<Sample> samples = AggregateAndSortProfile(profile, false /* only_growth */, order);
-#elif YB_GPERFTOOLS_TCMALLOC
-  if (peak_heap) {
-    (*output) << "peak_heap is not supported with gperftools tcmalloc";
+  Result<vector<Sample>> sample_result = GetAggregateAndSortHeapSnapshot(
+      order, peak_heap ? HeapSnapshotType::kPeakHeap : HeapSnapshotType::kCurrentHeap);
+  if (!sample_result.ok()) {
+    (*output) << sample_result.status().message();
     return;
   }
-  if (order != SampleOrder::kSampledBytes && order != SampleOrder::kSampledCount) {
-    (*output) << Format("Order \"$0\" is not supported with gperftools tcmalloc", order);
-    return;
-  }
-  vector<Sample> samples = GetAggregateAndSortHeapSnapshot(order);
-#endif // YB_GPERFTOOLS_TCMALLOC
+  vector<Sample> samples = sample_result.get();
 
+  string title = peak_heap ? "Peak heap snapshot" : "Current heap snapshot";
   GenerateTable(output, samples, title, 1000 /* max_call_stacks */, order);
-#endif // YB_TCMALLOC_ENABLED
 }
 
 // pprof asks for the url /pprof/profile?seconds=XX to get cpu-profiling information.

src/yb/util/CMakeLists.txt
@@ -232,6 +232,7 @@ set(UTIL_SRCS
   striped64.cc
   subprocess.cc
   sync_point.cc
+  tcmalloc_profile.cc
   tcmalloc_trace.cc
   tcmalloc_util.cc
   test_graph.cc
@@ -282,6 +283,7 @@ if(NOT APPLE)
   set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -DROCKSDB_FALLOCATE_PRESENT")
   set(UTIL_LIBS
     ${UTIL_LIBS}
+    absl
     rt
     libbacktrace)
 endif()
@@ -328,7 +330,6 @@ endif()
 set(YB_TEST_LINK_LIBS yb_test_util gutil gmock gtest
     ${YB_MIN_TEST_LIBS})
 ADD_YB_TEST(atomic-test)
-ADD_YB_TEST(flags/auto_flags-test)
 ADD_YB_TEST(background_task-test)
 ADD_YB_TEST(backoff_waiter-test)
 ADD_YB_TEST(bit-util-test)
@@ -336,14 +337,19 @@ ADD_YB_TEST(bitmap-test)
 ADD_YB_TEST(blocking_queue-test)
 ADD_YB_TEST(bloom_filter-test)
 ADD_YB_TEST(byte_buffer-test)
+ADD_YB_TEST(bytes_formatter-test)
 ADD_YB_TEST(callback_bind-test)
 ADD_YB_TEST(countdown_latch-test)
 ADD_YB_TEST(crc-test RUN_SERIAL true) # has a benchmark
 ADD_YB_TEST(crypt-test)
+ADD_YB_TEST(date_time-test)
 ADD_YB_TEST(debug-util-test)
+ADD_YB_TEST(decimal-test)
 ADD_YB_TEST(env-test LABELS no_tsan)
 ADD_YB_TEST(errno-test)
 ADD_YB_TEST(failure_detector-test)
+ADD_YB_TEST(fast_varint-test)
+ADD_YB_TEST(flags/auto_flags-test)
 ADD_YB_TEST(flags/flag_tags-test)
 ADD_YB_TEST(flags/flags-test)
 ADD_YB_TEST(flags/flags_callback-test)
@@ -370,11 +376,13 @@ ADD_YB_TEST(mt-hdr_histogram-test RUN_SERIAL true)
 ADD_YB_TEST(mt-metrics-test RUN_SERIAL true)
 ADD_YB_TEST(mt-threadlocal-test RUN_SERIAL true)
 ADD_YB_TEST(net/dns_resolver-test)
+ADD_YB_TEST(net/inetaddress-test)
 ADD_YB_TEST(net/net_util-test)
 ADD_YB_TEST(net/rate_limiter-test)
 ADD_YB_TEST(numbered_deque-test)
 ADD_YB_TEST(object_pool-test)
 ADD_YB_TEST(once-test)
+ADD_YB_TEST(operation_counter-test)
 ADD_YB_TEST(os-util-test)
 ADD_YB_TEST(path_util-test)
 ADD_YB_TEST(priority_queue-test)
@@ -395,6 +403,8 @@ if (NOT "${YB_BUILD_TYPE}" STREQUAL "asan")
   # builds). This test involves some integer overflows.
   ADD_YB_TEST(safe_math-test)
 endif()
+ADD_YB_TEST(scope_exit-test)
+ADD_YB_TEST(shared_mem-test)
 ADD_YB_TEST(slice-test)
 ADD_YB_TEST(spinlock_profiling-test)
 ADD_YB_TEST(split-test)
@@ -402,29 +412,22 @@ ADD_YB_TEST(stats/iostats_context_test)
 ADD_YB_TEST(status-test)
 ADD_YB_TEST(stol_utils-test)
 ADD_YB_TEST(string_case-test)
+ADD_YB_TEST(string_trim-test)
 ADD_YB_TEST(striped64-test)
 ADD_YB_TEST(strongly_typed_uuid-test)
 ADD_YB_TEST(subprocess-test)
 ADD_YB_TEST(sync_point-test)
 ADD_YB_TEST(taskstream-test)
+ADD_YB_TEST(tcmalloc_profile-test)
 ADD_YB_TEST(thread-test)
 ADD_YB_TEST(threadpool-test)
 ADD_YB_TEST(tostring-test)
 ADD_YB_TEST(trace-test)
 ADD_YB_TEST(url-coding-test)
 ADD_YB_TEST(user-test)
-ADD_YB_TEST(bytes_formatter-test)
-ADD_YB_TEST(string_trim-test)
-ADD_YB_TEST(varint-test)
-ADD_YB_TEST(decimal-test)
-ADD_YB_TEST(date_time-test)
-ADD_YB_TEST(net/inetaddress-test)
 ADD_YB_TEST(uuid-test)
-ADD_YB_TEST(fast_varint-test)
-ADD_YB_TEST(shared_mem-test)
-ADD_YB_TEST(operation_counter-test)
+ADD_YB_TEST(varint-test)
 ADD_YB_TEST(write_buffer-test)
-ADD_YB_TEST(scope_exit-test)
 
 #######################################
 # jsonwriter_test_proto

src/yb/util/tcmalloc_profile-test.cc
@@ -10,7 +10,7 @@
 // or implied.  See the License for the specific language governing permissions and limitations
 // under the License.
 
-#include "yb/server/pprof-path-handler_util-test.h"
+#include "yb/util/tcmalloc_profile-test.h"
 
 #include <chrono>
 #include <memory>
@@ -21,8 +21,6 @@
 #include <gtest/gtest.h>
 
 #include "yb/gutil/dynamic_annotations.h"
-
-#include "yb/server/pprof-path-handlers_util.h"
 
 #include "yb/util/flags.h"
 #include "yb/util/logging.h"
@@ -99,23 +97,12 @@ vector<Sample> SamplingProfilerTest::GetTestAllocs(const vector<Sample>& samples
   return test_samples;
 }
 
-Result<vector<Sample>> GetSamplesFromHeapSnapshot(HeapSnapshotType snapshot_type) {
-#if YB_GPERFTOOLS_TCMALLOC
-  SCHECK_EQ(snapshot_type, HeapSnapshotType::kCurrentHeap, InvalidArgument,
-      "snapshot_type must be kCurrentHeap for gperftools tcmalloc");
-  return GetAggregateAndSortHeapSnapshot(SampleOrder::kSampledCount);
-#else
-  auto current_profile = GetHeapSnapshot(snapshot_type);
-  return AggregateAndSortProfile(
-      current_profile, false /* only_growth */, SampleOrder::kSampledCount);
-#endif
-}
-
 TEST_F(SamplingProfilerTest, DisableSampling) {
   SetProfileSamplingRate(0);
   auto v = TestAllocArrayOfSize(10_KB);
 
-  auto samples = ASSERT_RESULT(GetSamplesFromHeapSnapshot(HeapSnapshotType::kCurrentHeap));
+  auto samples = ASSERT_RESULT(
+      GetAggregateAndSortHeapSnapshot(SampleOrder::kSampledCount, HeapSnapshotType::kCurrentHeap));
   ASSERT_EQ(GetTestAllocs(samples).size(), 0);
 }
 
@@ -128,11 +115,11 @@ TEST_F(SamplingProfilerTest, HeapSnapshot) {
     // Make a large allocation. We expect to find it in the heap snapshot.
     std::unique_ptr<char[]> big_alloc = TestAllocArrayOfSize(kAllocSize);
 
-    auto samples = GetAggregateAndSortHeapSnapshot(SampleOrder::kSampledCount);
+    auto samples = ASSERT_RESULT(GetAggregateAndSortHeapSnapshot(SampleOrder::kSampledCount));
     ASSERT_EQ(GetTestAllocs(samples).size(), 1);
   }
   // After the deallocation, the stack should no longer be found in the heap snapshot.
-  auto samples = GetAggregateAndSortHeapSnapshot(SampleOrder::kSampledCount);
+  auto samples = ASSERT_RESULT(GetAggregateAndSortHeapSnapshot(SampleOrder::kSampledCount));
   ASSERT_EQ(GetTestAllocs(samples).size(), 0);
 }
 #endif
@@ -149,18 +136,22 @@ TEST_F(SamplingProfilerTest, HeapSnapshot) {
     // Make a large allocation. We expect to find it in the current and peak heap snapshots.
     std::unique_ptr<char[]> big_alloc = TestAllocArrayOfSize(kAllocSize);
 
-    auto samples = ASSERT_RESULT(GetSamplesFromHeapSnapshot(HeapSnapshotType::kCurrentHeap));
+    auto samples = ASSERT_RESULT(GetAggregateAndSortHeapSnapshot(
+        SampleOrder::kSampledCount, HeapSnapshotType::kCurrentHeap));
     ASSERT_EQ(GetTestAllocs(samples).size(), 1);
 
-    samples = ASSERT_RESULT(GetSamplesFromHeapSnapshot(HeapSnapshotType::kPeakHeap));
+    samples = ASSERT_RESULT(
+        GetAggregateAndSortHeapSnapshot(SampleOrder::kSampledCount, HeapSnapshotType::kPeakHeap));
     ASSERT_EQ(GetTestAllocs(samples).size(), 1);
   }
   // After the deallocation, the stack should no longer be found in the current heap snapshot,
   // but should be in the peak heap snapshot.
-  auto samples = ASSERT_RESULT(GetSamplesFromHeapSnapshot(HeapSnapshotType::kCurrentHeap));
+  auto samples = ASSERT_RESULT(
+      GetAggregateAndSortHeapSnapshot(SampleOrder::kSampledCount, HeapSnapshotType::kCurrentHeap));
   ASSERT_EQ(GetTestAllocs(samples).size(), 0);
 
-  samples = ASSERT_RESULT(GetSamplesFromHeapSnapshot(HeapSnapshotType::kPeakHeap));
+  samples = ASSERT_RESULT(
+      GetAggregateAndSortHeapSnapshot(SampleOrder::kSampledCount, HeapSnapshotType::kPeakHeap));
   ASSERT_EQ(GetTestAllocs(samples).size(), 1);
 }
 
@@ -191,12 +182,12 @@ TEST_F(SamplingProfilerTest, HeapProfile) {
 
   // The stack for the allocations is the same so they are aggregated into one.
   auto samples = AggregateAndSortProfile(
-      profile, false /* only_growth */, SampleOrder::kSampledBytes);
+      profile, SampleFilter::kAllSamples, SampleOrder::kSampledBytes);
   ASSERT_EQ(GetTestAllocs(samples).size(), 1);
   ASSERT_EQ(samples[0].second.sampled_allocated_bytes, kAllocSizeAllocated + kAllocSizeDeallocated);
 
   // We only expect to find the non-deallocated allocation here.
-  samples = AggregateAndSortProfile(profile, true /* only_growth */, SampleOrder::kSampledBytes);
+  samples = AggregateAndSortProfile(profile, SampleFilter::kGrowthOnly, SampleOrder::kSampledBytes);
   ASSERT_EQ(GetTestAllocs(samples).size(), 1);
   ASSERT_EQ(samples[0].second.sampled_allocated_bytes, kAllocSizeAllocated);
 }
@@ -240,7 +231,8 @@ TEST_F(SamplingProfilerTest, EstimatedBytesAndCount) {
     v.push_back(TestAllocArrayOfSize(kAllocSize));
   }
 
-  auto samples = ASSERT_RESULT(GetSamplesFromHeapSnapshot(HeapSnapshotType::kCurrentHeap));
+  auto samples = ASSERT_RESULT(
+      GetAggregateAndSortHeapSnapshot(SampleOrder::kSampledCount, HeapSnapshotType::kCurrentHeap));
 
   // Allocations should get aggregated into one sample.
   ASSERT_EQ(GetTestAllocs(samples).size(), 1);

src/yb/util/tcmalloc_profile-test.h
@@ -17,15 +17,15 @@
 #include <thread>
 
 #include <gflags/gflags_declare.h>
-#include "yb/util/logging.h"
 #include <gtest/gtest.h>
 
 #include "yb/gutil/dynamic_annotations.h"
-#include "yb/server/pprof-path-handlers_util.h"
 
 #include "yb/util/flags.h"
+#include "yb/util/logging.h"
 #include "yb/util/monotime.h"
 #include "yb/util/size_literals.h"
+#include "yb/util/tcmalloc_profile.h"
 #include "yb/util/test_util.h"
 
 namespace yb {

src/yb/util/tcmalloc_profile.h
@@ -12,20 +12,15 @@
 
 #pragma once
 
-#if YB_GOOGLE_TCMALLOC
-#include <tcmalloc/malloc_extension.h>
-#elif YB_GPERFTOOLS_TCMALLOC
-#include <gperftools/malloc_extension.h>
-#endif
-
 #include <cstdint>
+#include <optional>
 #include <string>
-#include <utility>
 
 #include "yb/util/enums.h"
 #include "yb/util/format.h"
 #include "yb/util/logging.h"
 #include "yb/util/monotime.h"
+#include "yb/util/tcmalloc_impl_util.h"
 
 namespace yb {
 
@@ -49,6 +44,12 @@ typedef std::pair<SampleStack, SampleInfo> Sample;
 
 YB_DEFINE_ENUM(SampleOrder, (kSampledCount)(kSampledBytes)(kEstimatedBytes));
 YB_DEFINE_ENUM(HeapSnapshotType, (kCurrentHeap)(kPeakHeap));
+YB_DEFINE_ENUM(SampleFilter, (kAllSamples)(kGrowthOnly));
+
+Result<std::vector<Sample>> GetAggregateAndSortHeapSnapshot(
+    SampleOrder order = SampleOrder::kSampledCount,
+    HeapSnapshotType snapshot_type = HeapSnapshotType::kCurrentHeap,
+    SampleFilter filter = SampleFilter::kAllSamples);
 
 #if YB_GOOGLE_TCMALLOC
 
@@ -58,17 +59,14 @@ Result<tcmalloc::Profile> GetHeapProfile(int seconds, int64_t sample_freq_bytes)
 tcmalloc::Profile GetHeapSnapshot(HeapSnapshotType snapshot_type);
 
 std::vector<Sample> AggregateAndSortProfile(
-    const tcmalloc::Profile& profile, bool only_growth, SampleOrder order);
+    const tcmalloc::Profile& profile, SampleFilter filter, SampleOrder order);
 
 #endif // YB_GOOGLE_TCMALLOC
 
 #if YB_GPERFTOOLS_TCMALLOC
 
-std::vector<Sample> GetAggregateAndSortHeapSnapshot(SampleOrder order);
+std::vector<Sample> GetAggregateAndSortHeapSnapshotGperftools(SampleOrder order);
 
 #endif // YB_GPERFTOOLS_TCMALLOC
-
-void GenerateTable(std::stringstream* output, const std::vector<Sample>& samples,
-    const std::string& title, size_t max_call_stacks, SampleOrder order);
 
 } // namespace yb

commit 7998ef6687d0dd3315540bebfdc21f86e66080c4

docdb: Add STL headers to fix failing mac build
Summary: This fixes the build broken by D34747. The landed diff was not the most recent diff.

Test Plan: jenkins: skip (already ran jenkins on the latest version of the previous diff)

Reviewers: steve.varnau

Reviewed By: steve.varnau

Subscribers: ybase

Differential Revision: https://phorge.dev.yugabyte.com/D34839

src/yb/util/tcmalloc_profile.cc
@@ -12,6 +12,12 @@
 
 #include "yb/util/tcmalloc_profile.h"
 
+#include <algorithm>
+#include <sstream>
+#include <unordered_map>
+#include <utility>
+#include <vector>
+
 #if YB_ABSL_ENABLED
 #include "absl/debugging/symbolize.h"
 #endif