6d134a1c6a9126702083d014dcc982dfc46b58f1
Shubham Varshney <svarshney@yugabyte.com>
2024-05-07 16:54:15 +0530

    [PLAT-13808] Pass machineImage for disk update operation from the java layer
    Summary:
    Passes the machineImage if present in the nodeDetails as the args for disk update operation.
    The order is -
    1. use the machineImage present in the nodeDetails.
    2. Fallback to the AMI present in the bundle.
    3. Use the AMI id present in the host.
    
    Test Plan:
    Created a universe using machineImage on the fly.
    Deleted the AMI from the console.
    Performed disk update - verified that it fails.
    
    Performed VM Image Upgrade.
    Peformed disk update - Verified that it passes.
    
    Reviewers: muthu, daniel, yshchetinin
    
    Reviewed By: daniel
    
    Subscribers: yugaware
    
    Differential Revision: https://phorge.dev.yugabyte.com/D34811

--- managed/src/main/java/com/yugabyte/yw/common/NodeManager.java
+++ managed/src/main/java/com/yugabyte/yw/common/NodeManager.java
@@ -2345,7 +2345,10 @@ public class NodeManager extends DevopsBase {
             throw new RuntimeException("NodeTaskParams is not InstanceActions.Params");
           }
           InstanceActions.Params taskParam = (InstanceActions.Params) nodeTaskParam;
-          if (toOverwriteNodeProperties != null) {
+          if (StringUtils.isNotEmpty(taskParam.machineImage)) {
+            commandArgs.add("--machine_image");
+            commandArgs.add(taskParam.machineImage);
+          } else if (toOverwriteNodeProperties != null) {
             String ybImage = toOverwriteNodeProperties.getMachineImage();
             if (StringUtils.isNotBlank(ybImage)) {
               commandArgs.add("--machine_image");
@@ -2368,7 +2371,10 @@ public class NodeManager extends DevopsBase {
             throw new RuntimeException("NodeTaskParams is not UpdateMountedDisksTask.Params");
           }
           UpdateMountedDisks.Params taskParam = (UpdateMountedDisks.Params) nodeTaskParam;
-          if (toOverwriteNodeProperties != null) {
+          if (StringUtils.isNotEmpty(taskParam.machineImage)) {
+            commandArgs.add("--machine_image");
+            commandArgs.add(taskParam.machineImage);
+          } else if (toOverwriteNodeProperties != null) {
             String ybImage = toOverwriteNodeProperties.getMachineImage();
             if (StringUtils.isNotBlank(ybImage)) {
               commandArgs.add("--machine_image");
