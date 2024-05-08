yugabyte/yugabyte-db

commit 6ca42f33fef6b0ebefb6b2f3a2a8499466cb2b96

[#15196] YSQL: Take row-level (instead of column-level) locks for UPDATE operations
Summary:
In YugabyteDB, unlike PostgreSQL, concurrent UPDATEs to different
columns of the same row are permitted in REPEATABLE READ/READ COMMITTED
isolation levels, meaning they don't conflict.  This behavior leads to
several correctness issues in YSQL, which are not present in PostgreSQL,
as demonstrated in the following three scenarios. All the issues
described below stem from what could be termed a 'write-skew anomaly
within a row', e.g, txn T1 and T2 concurrently read col1 and col2
(respectively) of a row, and then based on some computation of the read
value, update col2 and col1, respectively.  This breaks some intuitive
invariants that users might expect.

In YSQL, concurrent transactions updating different columns of the same
row don't conflict because we implicitly write only the (kStrongWrite,
kStrongRead) intent modes on the individual columns. Owing to our
algorithm, which writes the kWeak versions of these intent modes on all
prefixes, we take the kWeak versions on the row level too. But these are
not sufficient to cause a conflict.

**Scenario 1**

CHECK constraints on more than one column can be violated if the columns are
updated concurrently in different transactions.

```
CREATE TABLE products (
 product_no integer,
 name text,
 price numeric,
 CHECK (price > 0),
 discounted_price numeric,
 CHECK (discounted_price > 0),
 CHECK (price > discounted_price)
);

INSERT INTO products values (1, 'oats', 10, 1);

--txn1
BEGIN;
UPDATE products SET price = 3 WHERE product_no = 1;

--txn2
BEGIN;
UPDATE products SET discounted_price = 4 WHERE product_no = 1;

--txn1
COMMIT;
--txn2
COMMIT;
```

```
yugabyte=# SELECT * FROM products;
 product_no | name | price | discounted_price
------------+------+-------+------------------
          1 | oats |     3 |                4
(1 row)
```

**Scenario 2**

User maintained "check" constraint via a function can be violated

```

src/yb/docdb/pgsql_operation.cc
@@ -118,6 +118,12 @@ DEFINE_RUNTIME_bool(ysql_enable_pack_full_row_update, false,
 DEFINE_RUNTIME_PREVIEW_bool(ysql_use_packed_row_v2, false,
                             "Whether to use packed row V2 when row packing is enabled.");
 
+DEFINE_NON_RUNTIME_bool(
+    ysql_skip_row_lock_for_update, false,
+    "By default DocDB operations for YSQL take row-level locks. If set to true, DocDB will instead "
+    "take finer column-level locks instead of locking the whole row. This may cause issues with "
+    "data integrity for operations with implicit dependencies between columns.");
+
 namespace yb::docdb {
 
 using dockv::DocKey;
@@ -703,6 +709,26 @@ class ExpressionHelper {
   std::vector<QLExprResult> results_;
   size_t next_result_idx_ = 0;
 };
+
+[[nodiscard]] inline bool NewValuesHaveExpression(const PgsqlWriteRequestPB& request) {
+  // In case of UPDATE row need to be protected from removing. Weak intent is enough for
+  // this purpose. To achieve this the path for row's SystemColumnIds::kLivenessColumn column
+  // is returned. The caller code will create strong intent for returned path
+  // (row's column doc key) and weak intents for all its prefixes (including row's doc key).
+  // Note: UPDATE operation may have expressions instead of exact value.
+  // These expressions may read column value.
+  // Potentially expression for updating column v1 may read value of column v2.
+  //
+  // UPDATE t SET v = v + 10 WHERE k = 1
+  // UPDATE t SET v1 = v2 + 10 WHERE k = 1
+  //
+  // Strong intent for the whole row is required in this case as it may be too expensive to
+  // determine what exact columns are read by the expression.
+
+  return std::any_of(
+      request.column_new_values().begin(), request.column_new_values().end(),
+      [](const auto& cv) { return !cv.expr().has_value(); });
+}
 
 void WriteNumRows(size_t result_rows, const WriteBufferPos& pos, WriteBuffer* buffer) {
   char encoded_rows[sizeof(uint64_t)];
@@ -1477,40 +1503,19 @@ Status PgsqlWriteOperation::GetDocPaths(GetDocPathsMode mode,
   *level = RequireReadSnapshot() ? IsolationLevel::SNAPSHOT_ISOLATION
                                  : IsolationLevel::SERIALIZABLE_ISOLATION;
 
+  const auto is_update = request_.stmt_type() == PgsqlWriteRequestPB::PGSQL_UPDATE;
   switch (mode) {
     case GetDocPathsMode::kLock: {
-      if (request_.stmt_type() == PgsqlWriteRequestPB::PGSQL_UPDATE) {
-        // In case of UPDATE row need to be protected from removing. Weak intent is enough for
-        // this purpose. To achieve this the path for row's SystemColumnIds::kLivenessColumn column
-        // is returned. The caller code will create strong intent for returned path
-        // (row's column doc key) and weak intents for all its prefixes (including row's doc key).
-        // Note: UPDATE operation may have expressions instead of exact value.
-        // These expressions may read column value.
-        // Potentially expression for updating column v1 may read value of column v2.
-        //
-        // UPDATE t SET v = v + 10 WHERE k = 1
-        // UPDATE t SET v1 = v2 + 10 WHERE k = 1
-        //
-        // Strong intent for the whole row is required in this case as it may be too expensive to
-        // determine what exact columns are read by the expression.
-
-        bool has_expression = false;
-        for (const auto& column_value : request_.column_new_values()) {
-          if (!column_value.expr().has_value()) {
-            has_expression = true;
-            break;
-          }
-        }
-        if (!has_expression) {
-          DocKeyColumnPathBuilderHolder holder(encoded_doc_key_.as_slice());
-          paths->emplace_back(holder.builder().Build(dockv::SystemColumnIds::kLivenessColumn));
-          return Status::OK();
-        }
+      if (PREDICT_FALSE(FLAGS_ysql_skip_row_lock_for_update) && is_update &&
+          !NewValuesHaveExpression(request_)) {
+        DocKeyColumnPathBuilderHolder holder(encoded_doc_key_.as_slice());
+        paths->emplace_back(holder.builder().Build(dockv::SystemColumnIds::kLivenessColumn));
+        return Status::OK();
       }
       break;
     }
     case GetDocPathsMode::kIntents: {
-      if (request_.stmt_type() != PgsqlWriteRequestPB::PGSQL_UPDATE) {
+      if (!is_update) {
         break;
       }
       const auto& column_values = request_.column_new_values();
@@ -1525,6 +1530,12 @@ Status PgsqlWriteOperation::GetDocPaths(GetDocPathsMode mode,
         paths->emplace_back(builder.Build(column_value.column_id()));
       }
       return Status::OK();
+    }
+    case GetDocPathsMode::kStrongReadIntents: {
+      if (!is_update || PREDICT_FALSE(FLAGS_ysql_skip_row_lock_for_update)) {
+        return Status::OK();
+      }
+      break;
     }
   }
   // Add row's doc key. Caller code will create strong intent for the whole row in this case.

java/yb-pgsql/src/test/java/org/yb/pgsql/TestPgWaitQueuesRegress.java
@@ -28,6 +28,11 @@ public class TestPgWaitQueuesRegress extends BasePgSQLTest {
     Map<String, String> flagMap = super.getTServerFlags();
     flagMap.put("enable_wait_queues", "true");
     flagMap.put("yb_enable_read_committed_isolation", "true");
+    /*
+     * Setting yb_max_query_layer_retries allows to reliably test wait queue semantics in
+     * isolation by avoiding query layer retries of serialization errors.
+     */
+    flagMap.put("ysql_pg_conf_csv", maxQueryLayerRetriesConf(0));
     return flagMap;
   }
 

java/yb-pgsql/src/test/java/org/yb/pgsql/TestPgWithoutWaitQueuesIsolationRegress.java
@@ -32,11 +32,11 @@ public class TestPgWithoutWaitQueuesIsolationRegress extends BasePgSQLTest {
 
   @Override
   public int getTestMethodTimeoutSec() {
-    return 180;
+    return 800;
   }
 
   @Test
-  public void runPgTransparentRestartsRegressTest() throws Exception {
+  public void runPgWithoutWaitQueuesRegressTest() throws Exception {
     runPgRegressTest(
       PgRegressBuilder.PG_ISOLATION_REGRESS_DIR, "yb_without_wait_queues_schedule",
       0 /* maxRuntimeMillis */, PgRegressBuilder.PG_ISOLATION_REGRESS_EXECUTABLE);

commit 7798e715cf71e22f3d9b6b89b6a05391441392c8

[#22057] docdb: converted ysql_skip_row_lock_for_update to auto-flag
Summary:
Combination of two commits: https://github.com/yugabyte/yugabyte-db/commit/250adda584 + https://github.com/yugabyte/yugabyte-db/commit/6ca42f33 introduced a compatibility issue between pre-250adda584 and post-6ca42f33 tserver versions if they are running at the same time during upgrade.

With the post-6ca42f33 logic `WriteQuery::DoCompleteExecute` when isolation level is not `SERIALIZABLE_ISOLATION` it can add WRITE_OP that contains both `write_pairs` and `read_pairs` to the Raft log. Corresponding `read_pair` will have key set to encoded row key and value set to `KeyEntryTypeAsChar::kNullLow`.

This WRITE_OP is then processed by `TransactionalWriter::Apply` and corresponding intents are generated and written to the intents DB.

In post-6ca42f33 version `TransactionalWriter::Apply` processes such `read_pair` and as a result generates intent `<row> -> kNullLow` of type `[kStrongRead]`.
But in pre-250adda584 version `TransactionalWriter::Apply` processes such `read_pair` in a different way and generates intent `<row> -> kNullLow` of type `[kStrongRead, kStrongWrite]`.

Then `ApplyIntentsContext::Entry` processes intents and in pre-250adda584 version due to presence of `kStrongWrite` type this intent gets written into regular DB and results in the following record:
`<row> -> kNullLow`, which is incorrect regular DB record. Effect of handing this record by DocDB is different depending on WHERE filter and presence of aggregation, it may result in either affected rows be not visible by the user statement or visible but with non-PK columns set to null.

Given that `TransactionalWriter::Apply` only writes `read_pairs` for non-SERIALIZABLE_ISOLATION when new logic is enabled by `ysql_skip_row_lock_for_update`, the solution is to convert it to an auto-flag, so new logic is only enabled after all nodes are on the new version.

src/yb/docdb/pgsql_operation.cc
@@ -118,8 +118,7 @@ DEFINE_RUNTIME_bool(ysql_enable_pack_full_row_update, false,
 DEFINE_RUNTIME_PREVIEW_bool(ysql_use_packed_row_v2, false,
                             "Whether to use packed row V2 when row packing is enabled.");
 
-DEFINE_NON_RUNTIME_bool(
-    ysql_skip_row_lock_for_update, false,
+DEFINE_RUNTIME_AUTO_bool(ysql_skip_row_lock_for_update, kExternal, true, false,
     "By default DocDB operations for YSQL take row-level locks. If set to true, DocDB will instead "
     "take finer column-level locks instead of locking the whole row. This may cause issues with "
     "data integrity for operations with implicit dependencies between columns.");
@@ -801,6 +800,15 @@ class PgsqlWriteOperation::RowPackContext {
 };
 
 //--------------------------------------------------------------------------------------------------
+
+PgsqlWriteOperation::PgsqlWriteOperation(
+    std::reference_wrapper<const PgsqlWriteRequestPB> request, DocReadContextPtr doc_read_context,
+    const TransactionOperationContext& txn_op_context, rpc::Sidecars* sidecars)
+    : DocOperationBase(request),
+      doc_read_context_(std::move(doc_read_context)),
+      txn_op_context_(txn_op_context),
+      sidecars_(sidecars),
+      ysql_skip_row_lock_for_update_(FLAGS_ysql_skip_row_lock_for_update) {}
 
 Status PgsqlWriteOperation::Init(PgsqlResponsePB* response) {
   // Initialize operation inputs.
@@ -1546,7 +1554,7 @@ Status PgsqlWriteOperation::GetDocPaths(GetDocPathsMode mode,
   const auto is_update = request_.stmt_type() == PgsqlWriteRequestPB::PGSQL_UPDATE;
   switch (mode) {
     case GetDocPathsMode::kLock: {
-      if (PREDICT_FALSE(FLAGS_ysql_skip_row_lock_for_update) && is_update &&
+      if (PREDICT_FALSE(ysql_skip_row_lock_for_update_) && is_update &&
           !NewValuesHaveExpression(request_)) {
         DocKeyColumnPathBuilderHolder holder(encoded_doc_key_.as_slice());
         paths->emplace_back(holder.builder().Build(dockv::SystemColumnIds::kLivenessColumn));
@@ -1572,7 +1580,7 @@ Status PgsqlWriteOperation::GetDocPaths(GetDocPathsMode mode,
       return Status::OK();
     }
     case GetDocPathsMode::kStrongReadIntents: {
-      if (!is_update || PREDICT_FALSE(FLAGS_ysql_skip_row_lock_for_update)) {
+      if (!is_update || PREDICT_FALSE(ysql_skip_row_lock_for_update_)) {
         return Status::OK();
       }
       break;

src/yb/docdb/pgsql_operation.h
@@ -47,12 +47,7 @@ class PgsqlWriteOperation :
   PgsqlWriteOperation(std::reference_wrapper<const PgsqlWriteRequestPB> request,
                       DocReadContextPtr doc_read_context,
                       const TransactionOperationContext& txn_op_context,
-                      rpc::Sidecars* sidecars)
-      : DocOperationBase(request),
-        doc_read_context_(std::move(doc_read_context)),
-        txn_op_context_(txn_op_context),
-        sidecars_(sidecars) {
-  }
+                      rpc::Sidecars* sidecars);
 
   // Initialize PgsqlWriteOperation. Content of request will be swapped out by the constructor.
   Status Init(PgsqlResponsePB* response);
@@ -145,6 +140,7 @@ class PgsqlWriteOperation :
   int64_t result_rows_ = 0;
   WriteBufferPos row_num_pos_;
   WriteBuffer* write_buffer_ = nullptr;
+  const bool ysql_skip_row_lock_for_update_;
 };
 
 class PgsqlReadOperation : public DocExprExecutor {