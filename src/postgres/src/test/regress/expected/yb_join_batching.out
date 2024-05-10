yugabyte/yugabyte-db

commit 59191f103a0bfd8c4d13856d7ba94b0de3b80c81

[#21366] YSQL: Allow BNL's with non-pushdownable unbatchable join clauses
Summary:
Before this change, `yb_get_batched_index_paths` did not allow BNL's on an outer table and an inner table if the inner table had an "unbatchable" join restriction. Such unbatchable clauses are typically non-simple clauses that can't accept batches of inputs such as `outer_table.a <= inner_table.a`. These clauses are only problematic if they are pushed down to the inner side of a BNL join where the BNL might push batched of the outer side tuples to the inner side.

In some cases such as in outer joins, these clauses may not be pushed down to the inner side and have no reason to be problematic. This change allows BNL's in such cases.
Jira: DB-10264

Test Plan: ./yb_build.sh --java-test 'org.yb.pgsql.TestPgRegressJoin'

Reviewers: mtakahara

Reviewed By: mtakahara

Subscribers: yql

Differential Revision: https://phorge.dev.yugabyte.com/D32986

src/postgres/src/backend/optimizer/path/indxpath.c
@@ -669,6 +669,8 @@ yb_get_batched_index_paths(PlannerInfo *root, RelOptInfo *rel,
 	batchedrelids = bms_difference(batchedrelids, unbatchablerelids);
 
 	/* See if we have any unbatchable filters. */
+	Relids batched_and_inner_relids =
+		bms_union(batchedrelids, index->rel->relids);
 	List *pclauses = NIL;
 	if (!bms_is_empty(batchedrelids)) {
 		pclauses = generate_join_implied_equalities(
@@ -676,11 +678,15 @@ yb_get_batched_index_paths(PlannerInfo *root, RelOptInfo *rel,
 			bms_union(batchedrelids, index->rel->relids),
 			batchedrelids,
 			rel);
-		pclauses = list_concat(pclauses, rel->joininfo);
-	}
 
-	Relids batched_and_inner_relids =
-		bms_union(batchedrelids, index->rel->relids);
+		foreach(lc, rel->joininfo)
+		{
+			RestrictInfo *rinfo = (RestrictInfo *) lfirst(lc);
+			if (join_clause_is_movable_into(rinfo, rel->relids,
+											batched_and_inner_relids))
+				pclauses = lappend(pclauses, rinfo);
+		}
+	}
 
 	foreach(lc, pclauses)
 	{

src/postgres/src/test/regress/expected/yb_join_batching.out
@@ -1068,6 +1068,96 @@ insert into out values (1, NULL), (1, 'abcd');
 
 drop table intable;
 drop table out;
+create table outtable(a int);
+insert into outtable values (1), (2), (3);
+create table intable(a int primary key);
+insert into intable values (2), (3), (4), (6);
+explain (costs off) select * FROM outtable t1 left outer join intable t2 on t1.a = t2.a where t1.a = 1 or t2.a = 2 order by t1.a;
+                        QUERY PLAN
+-----------------------------------------------------------
+ Sort
+   Sort Key: t1.a
+   ->  YB Batched Nested Loop Left Join
+         Join Filter: (t1.a = t2.a)
+         Filter: ((t1.a = 1) OR (t2.a = 2))
+         ->  Seq Scan on outtable t1
+         ->  Index Scan using intable_pkey on intable t2
+               Index Cond: (a = ANY (ARRAY[t1.a, $1, $2]))
+(8 rows)
+
+select * FROM outtable t1 left outer join intable t2 on t1.a = t2.a where t1.a = 1 or t2.a = 2 order by t1.a;
+ a | a
+---+---
+ 1 |
+ 2 | 2
+(2 rows)
+
+explain (costs off) select * FROM outtable t1 left outer join intable t2 on t1.a = t2.a where t1.a >= t2.a or t1.a = 1 order by t1.a;
+                        QUERY PLAN
+-----------------------------------------------------------
+ Sort
+   Sort Key: t1.a
+   ->  YB Batched Nested Loop Left Join
+         Join Filter: (t1.a = t2.a)
+         Filter: ((t1.a >= t2.a) OR (t1.a = 1))
+         ->  Seq Scan on outtable t1
+         ->  Index Scan using intable_pkey on intable t2
+               Index Cond: (a = ANY (ARRAY[t1.a, $1, $2]))
+(8 rows)
+
+select * FROM outtable t1 left outer join intable t2 on t1.a = t2.a where t1.a >= t2.a or t1.a = 1 order by t1.a;
+ a | a
+---+---
+ 1 |
+ 2 | 2
+ 3 | 3
+(3 rows)
+
+explain (costs off) select * FROM outtable t1 left outer join intable t2 on t1.a = t2.a where (t1.a + 1 > t2.a or t1.a = 1) order by t1.a;
+                        QUERY PLAN
+-----------------------------------------------------------
+ Sort
+   Sort Key: t1.a
+   ->  YB Batched Nested Loop Left Join
+         Join Filter: (t1.a = t2.a)
+         Filter: (((t1.a + 1) > t2.a) OR (t1.a = 1))
+         ->  Seq Scan on outtable t1
+         ->  Index Scan using intable_pkey on intable t2
+               Index Cond: (a = ANY (ARRAY[t1.a, $1, $2]))
+(8 rows)
+
+select * FROM outtable t1 left outer join intable t2 on t1.a = t2.a where (t1.a + 1 > t2.a or t1.a = 1) order by t1.a;
+ a | a
+---+---
+ 1 |
+ 2 | 2
+ 3 | 3
+(3 rows)
+
+-- The problematic join filter is pushdownable to the nullable side.
+-- BNL not allowed here.
+explain (costs off) select * FROM outtable t1 left outer join intable t2 on t1.a = t2.a and (t1.a + 1 > t2.a or t1.a = 1) order by t1.a;
+                       QUERY PLAN
+---------------------------------------------------------
+ Sort
+   Sort Key: t1.a
+   ->  Nested Loop Left Join
+         ->  Seq Scan on outtable t1
+         ->  Index Scan using intable_pkey on intable t2
+               Index Cond: (t1.a = a)
+               Filter: (((t1.a + 1) > a) OR (t1.a = 1))
+(7 rows)
+
+select * FROM outtable t1 left outer join intable t2 on t1.a = t2.a where (t1.a + 1 > t2.a or t1.a = 1) order by t1.a;
+ a | a
+---+---
+ 1 |
+ 2 | 2
+ 3 | 3
+(3 rows)
+
+drop table outtable;
+drop table intable;
 CREATE TABLE q1(a int);
 CREATE TABLE q2(a int);
 CREATE TABLE q3(a int primary key);

src/postgres/src/test/regress/sql/yb_join_batching.sql
@@ -276,6 +276,33 @@ insert into out values (1, NULL), (1, 'abcd');
 drop table intable;
 drop table out;
 
+create table outtable(a int);
+insert into outtable values (1), (2), (3);
+
+create table intable(a int primary key);
+insert into intable values (2), (3), (4), (6);
+
+explain (costs off) select * FROM outtable t1 left outer join intable t2 on t1.a = t2.a where t1.a = 1 or t2.a = 2 order by t1.a;
+
+select * FROM outtable t1 left outer join intable t2 on t1.a = t2.a where t1.a = 1 or t2.a = 2 order by t1.a;
+
+explain (costs off) select * FROM outtable t1 left outer join intable t2 on t1.a = t2.a where t1.a >= t2.a or t1.a = 1 order by t1.a;
+
+select * FROM outtable t1 left outer join intable t2 on t1.a = t2.a where t1.a >= t2.a or t1.a = 1 order by t1.a;
+
+explain (costs off) select * FROM outtable t1 left outer join intable t2 on t1.a = t2.a where (t1.a + 1 > t2.a or t1.a = 1) order by t1.a;
+
+select * FROM outtable t1 left outer join intable t2 on t1.a = t2.a where (t1.a + 1 > t2.a or t1.a = 1) order by t1.a;
+
+-- The problematic join filter is pushdownable to the nullable side.
+-- BNL not allowed here.
+explain (costs off) select * FROM outtable t1 left outer join intable t2 on t1.a = t2.a and (t1.a + 1 > t2.a or t1.a = 1) order by t1.a;
+
+select * FROM outtable t1 left outer join intable t2 on t1.a = t2.a where (t1.a + 1 > t2.a or t1.a = 1) order by t1.a;
+
+drop table outtable;
+drop table intable;
+
 CREATE TABLE q1(a int);
 CREATE TABLE q2(a int);
 CREATE TABLE q3(a int primary key);

commit 5ba86f0d41341b68adc6111cf726dfd513c12cb8

[#21878] YSQL: Fix illegal BNL check in the presence of join clauses requiring non-batched outerrels
Summary:
The affected lines of this change go through the join clauses to see if they would be pushed down to a batched index scan and conflict with any batched outerrels of this scan. Before this change, the logic that determined whether or not a join clause would be pushed down to it was insufficient. This change fixes that gap.

Fixes bug introduced by: 59191f103a0bfd8c4d13856d7ba94b0de3b80c81//D32986
Needs backports on 2024.1, 2.20, 2.18
Jira: DB-10781

Test Plan: ./yb_build.sh --java-test 'org.yb.pgsql.TestPgRegressJoin'

Reviewers: mtakahara

Reviewed By: mtakahara

Subscribers: yql

Differential Revision: https://phorge.dev.yugabyte.com/D34888

src/postgres/src/backend/optimizer/path/indxpath.c
@@ -645,6 +645,8 @@ yb_get_batched_index_paths(PlannerInfo *root, RelOptInfo *rel,
 
 	bool batched_paths_added = false;
 
+	Relids total_relids = NULL;
+
 	for (size_t i = 0; i < INDEX_MAX_KEYS && clauses->nonempty; i++)
 	{
 		List *colclauses = clauses->indexclauses[i];
@@ -689,16 +691,18 @@ yb_get_batched_index_paths(PlannerInfo *root, RelOptInfo *rel,
 				unbatchablerelids = bms_union(unbatchablerelids,
 											  rinfo->clause_relids);
 			}
+
+			total_relids = bms_union(total_relids, outer_relids);
 		}
 	}
+
+	total_relids = bms_union(total_relids, inner_relids);
 
 	batchedrelids = bms_del_member(batchedrelids,
 								   index->rel->relid);
 	batchedrelids = bms_difference(batchedrelids, unbatchablerelids);
 
 	/* See if we have any unbatchable filters. */
-	Relids batched_and_inner_relids =
-		bms_union(batchedrelids, index->rel->relids);
 	List *pclauses = NIL;
 	if (!bms_is_empty(batchedrelids)) {
 		pclauses = generate_join_implied_equalities(
@@ -707,11 +711,16 @@ yb_get_batched_index_paths(PlannerInfo *root, RelOptInfo *rel,
 			batchedrelids,
 			rel);
 
+		/*
+		 * Anything in joininfo that can be pushed down to this scan
+		 * will end up as a qpqual. Add these to the list of clauses
+		 * to check for batchability.
+		 */
 		foreach(lc, rel->joininfo)
 		{
 			RestrictInfo *rinfo = (RestrictInfo *) lfirst(lc);
 			if (join_clause_is_movable_into(rinfo, rel->relids,
-											batched_and_inner_relids))
+											total_relids))
 				pclauses = lappend(pclauses, rinfo);
 		}
 	}
@@ -774,7 +783,7 @@ yb_get_batched_index_paths(PlannerInfo *root, RelOptInfo *rel,
 
 	bms_free(batched_inner_attnos);
 	bms_free(pclause_batched_inner_attnos);
-	bms_free(batched_and_inner_relids);
+	bms_free(total_relids);
 
 	unbatchablerelids = bms_del_member(unbatchablerelids,
 									   index->rel->relid);

src/postgres/src/test/regress/expected/yb_join_batching.out
@@ -2233,6 +2233,39 @@ explain (costs off) select * from ss1 t1, ss2 t2 where t1.a = t2.a and t1.b = t2
 
 drop table ss1;
 drop table ss2;
+CREATE TABLE ss1(a int);
+CREATE TABLE ss2(a int);
+CREATE TABLE ss3(a int, b int, c int, primary key(a asc, b asc));
+/*+Set(enable_hashjoin OFF) Set(enable_mergejoin OFF) Set(enable_material OFF) Leading((ss2 (ss1 ss3)))
+*/EXPLAIN (COSTS OFF) SELECT *
+FROM ss1, ss2, ss3
+WHERE ss1.a = ss3.a and ss3.c <= (SELECT a FROM ss3 where a < ss2.a + ss1.a limit 1) and ss3.b <= ss2.a;
+                            QUERY PLAN
+------------------------------------------------------------------
+ Nested Loop
+   ->  Seq Scan on ss2
+   ->  Nested Loop
+         ->  Seq Scan on ss1
+         ->  Index Scan using ss3_pkey on ss3
+               Index Cond: ((a = ss1.a) AND (b <= ss2.a))
+               Filter: (c <= (SubPlan 1))
+               SubPlan 1
+                 ->  Limit
+                       ->  Index Scan using ss3_pkey on ss3 ss3_1
+                             Index Cond: (a < (ss2.a + ss1.a))
+(11 rows)
+
+/*+Set(enable_hashjoin OFF) Set(enable_mergejoin OFF) Set(enable_material OFF) Leading(((ss1 ss2) ss3))
+*/SELECT *
+FROM ss1, ss2, ss3
+WHERE ss1.a = ss3.a and ss3.c <= (SELECT a FROM ss3 where a < ss2.a + ss1.a limit 1) and ss3.b <= ss2.a;
+ a | a | a | b | c
+---+---+---+---+---
+(0 rows)
+
+DROP TABLE ss1;
+DROP TABLE ss2;
+DROP TABLE ss3;
 --
 --
 -- Inner joins (equi-joins)

src/postgres/src/test/regress/sql/yb_join_batching.sql
@@ -652,6 +652,24 @@ explain (costs off) select * from ss1 t1, ss2 t2 where t1.a = t2.a and t1.b = t2
 drop table ss1;
 drop table ss2;
 
+CREATE TABLE ss1(a int);
+CREATE TABLE ss2(a int);
+CREATE TABLE ss3(a int, b int, c int, primary key(a asc, b asc));
+
+/*+Set(enable_hashjoin OFF) Set(enable_mergejoin OFF) Set(enable_material OFF) Leading((ss2 (ss1 ss3)))
+*/EXPLAIN (COSTS OFF) SELECT *
+FROM ss1, ss2, ss3
+WHERE ss1.a = ss3.a and ss3.c <= (SELECT a FROM ss3 where a < ss2.a + ss1.a limit 1) and ss3.b <= ss2.a;
+
+/*+Set(enable_hashjoin OFF) Set(enable_mergejoin OFF) Set(enable_material OFF) Leading(((ss1 ss2) ss3))
+*/SELECT *
+FROM ss1, ss2, ss3
+WHERE ss1.a = ss3.a and ss3.c <= (SELECT a FROM ss3 where a < ss2.a + ss1.a limit 1) and ss3.b <= ss2.a;
+
+DROP TABLE ss1;
+DROP TABLE ss2;
+DROP TABLE ss3;
+
 --
 --
 -- Inner joins (equi-joins)