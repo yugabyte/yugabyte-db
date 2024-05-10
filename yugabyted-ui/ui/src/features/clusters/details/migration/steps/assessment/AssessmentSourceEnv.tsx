2587a3cb91822e18e473cf0bc2005334891c5535
Spikatrix <12792882+Spikatrix@users.noreply.github.com>
2024-05-10 05:14:49 +0000

    Rearrange source env grid items according to the new UX

--- yugabyted-ui/ui/src/features/clusters/details/migration/steps/assessment/AssessmentSourceEnv.tsx
+++ yugabyted-ui/ui/src/features/clusters/details/migration/steps/assessment/AssessmentSourceEnv.tsx
@@ -28,6 +28,10 @@ const useStyles = makeStyles((theme) => ({
   pointer: {
     cursor: "pointer",
   },
+  dividerVertical: {
+    marginLeft: theme.spacing(2.5),
+    marginRight: theme.spacing(2.5),
+  },
 }));
 
 interface MigrationSourceEnvProps {
@@ -60,104 +64,112 @@ export const MigrationSourceEnv: FC<MigrationSourceEnvProps> = ({
   return (
     <Paper>
       <Box px={2} py={3}>
-        <Box
-          display="flex"
-          justifyContent="space-between"
-          alignItems="center"
-          className={classes.heading}
-        >
-          <Box display="flex" alignItems="center" gridGap={theme.spacing(0.6)}>
-            <Typography variant="h4">
-              {t("clusterDetail.voyager.planAndAssess.sourceEnv.heading")}
-            </Typography>
-            <Box>
-              <YBTooltip title={t("clusterDetail.voyager.planAndAssess.sourceEnv.tooltip")} />
+        <Box display="flex">
+          <Box flex={1}>
+            <Box display="flex" alignItems="center" gridGap={theme.spacing(0.6)} mb={3}>
+              <Typography variant="h4">
+                {t("clusterDetail.voyager.planAndAssess.sourceEnv.heading")}
+              </Typography>
+              <Box>
+                <YBTooltip title={t("clusterDetail.voyager.planAndAssess.sourceEnv.tooltip")} />
+              </Box>
             </Box>
+
+            <Grid container spacing={4}>
+              <Grid item xs={6}>
+                <Typography variant="subtitle2" className={classes.label}>
+                  {t("clusterDetail.voyager.planAndAssess.sourceEnv.totalVcpu")}
+                </Typography>
+                <Typography variant="body2" className={classes.value}>
+                  {vcpu}
+                </Typography>
+              </Grid>
+              <Grid item xs={6}>
+                <Typography variant="subtitle2" className={classes.label}>
+                  {t("clusterDetail.voyager.planAndAssess.sourceEnv.totalMemory")}
+                </Typography>
+                <Typography variant="body2" className={classes.value}>
+                  {memory}
+                </Typography>
+              </Grid>
+              <Grid item xs={6}>
+                <Typography variant="subtitle2" className={classes.label}>
+                  {t("clusterDetail.voyager.planAndAssess.sourceEnv.totalDisk")}
+                </Typography>
+                <Typography variant="body2" className={classes.value}>
+                  {disk}
+                </Typography>
+              </Grid>
+              <Grid item xs={6}>
+                <Typography variant="subtitle2" className={classes.label}>
+                  {t("clusterDetail.voyager.planAndAssess.sourceEnv.noOfConns")}
+                </Typography>
+                <Typography variant="body2" className={classes.value}>
+                  {connectionCount}
+                </Typography>
+              </Grid>
+            </Grid>
           </Box>
 
-          <YBButton
-            variant="ghost"
-            startIcon={<CaretRightIcon />}
-            onClick={() => setShowSourceObjects(true)}
-          >
-            {t("clusterDetail.voyager.planAndAssess.sourceEnv.viewDetails")}
-          </YBButton>
-        </Box>
+          <Divider orientation="vertical" className={classes.dividerVertical} flexItem />
 
-        <Grid container spacing={4}>
-          <Grid item xs={3}>
-            <Typography variant="subtitle2" className={classes.label}>
-              {t("clusterDetail.voyager.planAndAssess.sourceEnv.totalVcpu")}
-            </Typography>
-            <Typography variant="body2" className={classes.value}>
-              {vcpu}
-            </Typography>
-          </Grid>
-          <Grid item xs={3}>
-            <Typography variant="subtitle2" className={classes.label}>
-              {t("clusterDetail.voyager.planAndAssess.sourceEnv.totalMemory")}
-            </Typography>
-            <Typography variant="body2" className={classes.value}>
-              {memory}
-            </Typography>
-          </Grid>
-          <Grid item xs={3}>
-            <Typography variant="subtitle2" className={classes.label}>
-              {t("clusterDetail.voyager.planAndAssess.sourceEnv.totalDisk")}
-            </Typography>
-            <Typography variant="body2" className={classes.value}>
-              {disk}
-            </Typography>
-          </Grid>
-          <Grid item xs={3}>
-            <Typography variant="subtitle2" className={classes.label}>
-              {t("clusterDetail.voyager.planAndAssess.sourceEnv.noOfConns")}
-            </Typography>
-            <Typography variant="body2" className={classes.value}>
-              {connectionCount}
-            </Typography>
-          </Grid>
-        </Grid>
-        <Divider orientation="horizontal" className={classes.dividerHorizontal} />
-        <Box mb={2}>
-          <Typography variant="body2">
-            {t("clusterDetail.voyager.planAndAssess.sourceEnv.sourceDB")}
-          </Typography>
+          <Box flex={1}>
+            <Box
+              display="flex"
+              alignItems="center"
+              justifyContent="space-between"
+              gridGap={theme.spacing(0.6)}
+              mb={3}
+            >
+              <Typography variant="h4">
+                {t("clusterDetail.voyager.planAndAssess.sourceEnv.sourceDB")}
+              </Typography>
+              <YBButton
+                variant="ghost"
+                startIcon={<CaretRightIcon />}
+                onClick={() => setShowSourceObjects(true)}
+              >
+                {t("clusterDetail.voyager.planAndAssess.sourceEnv.viewDetails")}
+              </YBButton>
+            </Box>
+
+            <Grid container spacing={4}>
+              <Grid item xs={6}>
+                <Typography variant="subtitle2" className={classes.label}>
+                  {t("clusterDetail.voyager.planAndAssess.sourceEnv.tableSize")}
+                </Typography>
+                <Typography variant="body2" className={classes.value}>
+                  {tableSize}
+                </Typography>
+              </Grid>
+              <Grid item xs={6}>
+                <Typography variant="subtitle2" className={classes.label}>
+                  {t("clusterDetail.voyager.planAndAssess.sourceEnv.rowCount")}
+                </Typography>
+                <Typography variant="body2" className={classes.value}>
+                  {rowCount}
+                </Typography>
+              </Grid>
+
+              <Grid item xs={6}>
+                <Typography variant="subtitle2" className={classes.label}>
+                  {t("clusterDetail.voyager.planAndAssess.sourceEnv.totalSize")}
+                </Typography>
+                <Typography variant="body2" className={classes.value}>
+                  {totalSize}
+                </Typography>
+              </Grid>
+              <Grid item xs={6}>
+                <Typography variant="subtitle2" className={classes.label}>
+                  {t("clusterDetail.voyager.planAndAssess.sourceEnv.indexSize")}
+                </Typography>
+                <Typography variant="body2" className={classes.value}>
+                  {indexSize}
+                </Typography>
+              </Grid>
+            </Grid>
+          </Box>
         </Box>
-        <Grid container spacing={4}>
-          <Grid item xs={3}>
-            <Typography variant="subtitle2" className={classes.label}>
-              {t("clusterDetail.voyager.planAndAssess.sourceEnv.tableSize")}
-            </Typography>
-            <Typography variant="body2" className={classes.value}>
-              {tableSize}
-            </Typography>
-          </Grid>
-          <Grid item xs={3}>
-            <Typography variant="subtitle2" className={classes.label}>
-              {t("clusterDetail.voyager.planAndAssess.sourceEnv.indexSize")}
-            </Typography>
-            <Typography variant="body2" className={classes.value}>
-              {indexSize}
-            </Typography>
-          </Grid>
-          <Grid item xs={3}>
-            <Typography variant="subtitle2" className={classes.label}>
-              {t("clusterDetail.voyager.planAndAssess.sourceEnv.totalSize")}
-            </Typography>
-            <Typography variant="body2" className={classes.value}>
-              {totalSize}
-            </Typography>
-          </Grid>
-          <Grid item xs={3}>
-            <Typography variant="subtitle2" className={classes.label}>
-              {t("clusterDetail.voyager.planAndAssess.sourceEnv.rowCount")}
-            </Typography>
-            <Typography variant="body2" className={classes.value}>
-              {rowCount}
-            </Typography>
-          </Grid>
-        </Grid>
       </Box>
 
       <MigrationSourceObjects
