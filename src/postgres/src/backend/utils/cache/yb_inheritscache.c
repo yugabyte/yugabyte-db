df1f849284718b3679331bbe4c4e577cef4b03cb
Minghui Yang <myang@yugabyte.com>
2024-05-13 22:00:33 +0000

    [#22262] YSQL: Fix more memory leaks in catalog cache refresh
    Summary:
    This diff fixes the remaining memory leaks in `CacheMemoryContext` that were
    identified to allow a stable cache memory after each catalog cache refresh.
    Two more identified memory leaks:
    
    * `YbCompleteAttrProcessingImpl`: `relation->rd_att->constr` needs to be freed.
    * `YbPreloadPgInheritsCache`: tighter scope of `MemoryContextSwitchTo(CacheMemoryContext)`
    
    For the first leak, we do not cleanup all the relations from rel cache on a
    catalog cache refresh. Some are considered `YbIsNonAlterableRelation` and will
    remain. But the overall processing flow of `YbCompleteAttrProcessingImpl`
    processes them anyway and will set their `relation->rd_att->constr` to the newly
    allocated `constr`.
    
    For the second leak in `YbPreloadPgInheritsCache`, PG code already appropriately
    allocates/copies into hcxt (CacheMemoryContext in this case) whenever required.
    If we still allocate any temporary memory into `CacheMemoryContext`, then they
    are not freed after being copied into `CacheMemoryContext`. Related code are:
    
    ```
    static HTAB *YbPgInheritsCache;
    ```
    and
    
    ```
    void
    YbInitPgInheritsCache()
    {
        Assert(YbPgInheritsCache == NULL);
        HASHCTL ctl = {0};
        ctl.keysize = sizeof(Oid);
        ctl.entrysize = sizeof(YbPgInheritsCacheEntryData);
        ctl.hcxt = CacheMemoryContext;
    ```
    
    Note that `ctl.hcxt = CacheMemoryContext` ensures the copying into
    `CacheMemoryContext`. By moving `MemoryContextSwitchTo(CacheMemoryContext)`
    to the minimum scope where it is truly required to ensure the right memory context,
    those temporary memory will be allocated from transaction memory that will be
    auto-freed after the transaction (created just for doing cache refresh) is over.
    
    With these two leaks fixed, I am able to add a new unit test that checks that
    the cache memory size stay the same after cache refresh.
    Jira: DB-11181
    
    Test Plan: ./yb_build.sh release --cxx-test pg_libpq-test --gtest_filter PgLibPqTest.CatalogCacheMemoryLeak -n 20
    
    Reviewers: fizaa, kfranz
    
    Reviewed By: fizaa
    
    Subscribers: yql
    
    Differential Revision: https://phorge.dev.yugabyte.com/D35017

--- src/postgres/src/backend/utils/cache/yb_inheritscache.c
+++ src/postgres/src/backend/utils/cache/yb_inheritscache.c
@@ -226,7 +226,6 @@ void
 YbPreloadPgInheritsCache()
 {
 	Assert(YbPgInheritsCache);
-	MemoryContext oldcxt = MemoryContextSwitchTo(CacheMemoryContext);
 	Relation relation = heap_open(InheritsRelationId, AccessShareLock);
 	HeapTuple	inheritsTuple;
 
@@ -253,13 +252,13 @@ YbPreloadPgInheritsCache()
 			entry->refcount = 1;
 			entry->parentOid = parentOid;
 		}
-
+		MemoryContext oldcxt = MemoryContextSwitchTo(CacheMemoryContext);
 		HeapTuple copy_inheritsTuple = heap_copytuple(inheritsTuple);
 		entry->childTuples = lappend(entry->childTuples, copy_inheritsTuple);
+		MemoryContextSwitchTo(oldcxt);
 	}
 	systable_endscan(scan);
 	heap_close(relation, AccessShareLock);
-	MemoryContextSwitchTo(oldcxt);
 }
 
 YbPgInheritsCacheEntry
