yugabyte/yugabyte-db

commit 483a74c3bc319fb28bf55a83de165d4aacdfb44b

[#21985] ASH: Track Log appender and background sync thread's wait states separately.
Summary:
Currently we have one wait state for the log for each tablet. Both the appender and bkground_sync_thread seem to update this using the ScopedWaitStatus mechanism.
Thus, if both the functions run concurrently, it is possible that the state is left in a non-Idle state when we are done with
both the functions. To fix this we track the state of the appender and background sync thread separately.

We introduce a bool field to denote whether or not the wait state should be allowed to be updated concurrently or not.
Wait states will be explicitly marked to expect concurrency when it spawns some work to be kicked off in a different/background thread in an async manner.
ScopedWaitStatus is updated to cause a DFATAL if we notice a concurrent update, when the wait-state has not been explicitly marked.
Jira: DB-10904

Test Plan: Jenkins

Reviewers: asaha, arybochkin

Reviewed By: arybochkin

Subscribers: yql, ybase

Differential Revision: https://phorge.dev.yugabyte.com/D34143

src/yb/consensus/log.cc
@@ -416,10 +416,6 @@ class Log::Appender {
     return task_stream_->ToString();
   }
 
-  const yb::ash::WaitStateInfoPtr& wait_state() const {
-    return wait_state_;
-  }
-
  private:
   // Process the given log entry batch or does a sync if a null is passed.
   void ProcessBatch(LogEntryBatch* entry_batch);
@@ -471,6 +467,7 @@ Status Log::Appender::Init() {
 // fsync will eventually be called. [if there is a background thread executing DoSync in parallel,
 // it might OR might not flush the new dirty data in the current iteration due to race condition]
 void Log::Appender::ProcessBatch(LogEntryBatch* entry_batch) {
+  ADOPT_WAIT_STATE(wait_state_);
   // A callback function to TaskStream is expected to process the accumulated batch of entries.
   if (entry_batch == nullptr) {
     // Here, we do sync and call callbacks.
@@ -664,10 +661,21 @@ Log::Log(
       log_prefix_(consensus::MakeTabletLogPrefix(tablet_id_, peer_uuid_)),
       create_new_segment_at_start_(create_new_segment),
       new_segment_allocation_callback_(callback),
-      pre_log_rollover_callback_(pre_log_rollover_callback) {
+      pre_log_rollover_callback_(pre_log_rollover_callback),
+      background_synchronizer_wait_state_(
+          ash::WaitStateInfo::CreateIfAshIsEnabled<ash::WaitStateInfo>()) {
   set_wal_retention_secs(options_.retention_secs);
   if (table_metric_entity_ && tablet_metric_entity_) {
     metrics_.reset(new LogMetrics(table_metric_entity_, tablet_metric_entity_));
+  }
+  if (background_synchronizer_wait_state_) {
+    background_synchronizer_wait_state_->set_root_request_id(yb::Uuid::Generate());
+    background_synchronizer_wait_state_->set_query_id(
+        yb::to_underlying(yb::ash::FixedQueryId::kQueryIdForLogBackgroundSync));
+    background_synchronizer_wait_state_->UpdateAuxInfo(
+        {.tablet_id = tablet_id_, .method = "RaftWAL"});
+    SET_WAIT_STATUS_TO(background_synchronizer_wait_state_, Idle);
+    yb::ash::RaftLogAppenderWaitStatesTracker().Track(background_synchronizer_wait_state_);
   }
 }
 
@@ -886,7 +894,6 @@ Status Log::AsyncAppendReplicates(const ReplicateMsgs& msgs, const yb::OpId& com
 }
 
 Status Log::DoAppend(LogEntryBatch* entry_batch, SkipWalWrite skip_wal_write) {
-  ADOPT_WAIT_STATE(appender_->wait_state());
   SCOPED_WAIT_STATUS(WAL_Append);
   if (!skip_wal_write) {
     RETURN_NOT_OK(entry_batch->Serialize());
@@ -1161,7 +1168,6 @@ Status Log::EnsureSegmentInitializedUnlocked() {
 // might not be necessary. We only call ::DoSync directly before we call ::CloseCurrentSegment
 Status Log::DoSync() {
   // Acquire the lock over active_segment_ to prevent segment rollover in the interim.
-  ADOPT_WAIT_STATE(appender_->wait_state());
   SCOPED_WAIT_STATUS(WAL_Sync);
   std::lock_guard lock(active_segment_mutex_);
   if (active_segment_->IsClosed()) {
@@ -1190,6 +1196,7 @@ Status Log::DoSync() {
 // have to use active_segment_sequence_number_ instead of fsync_task_in_queue_, in ::Sync(), to
 // determine if there is a pending fsync task corresponding to the current active segment.
 void Log::DoSyncAndResetTaskInQueue() {
+  ADOPT_WAIT_STATE(background_synchronizer_wait_state_);
   auto status = DoSync();
   if (!status.ok()) {
     // ensure that fsync gets called on the subsequent call to Log::Sync() function
@@ -1649,6 +1656,9 @@ Status Log::Close() {
     default:
       return STATUS(IllegalState, Substitute("Bad state for Close() $0", log_state_));
   }
+  if (background_synchronizer_wait_state_) {
+    yb::ash::RaftLogAppenderWaitStatesTracker().Untrack(background_synchronizer_wait_state_);
+  }
 }
 
 size_t Log::num_segments() const {
@@ -1929,7 +1939,6 @@ Status Log::SwitchToAllocatedSegment() {
   // Calling Sync() here is important because it ensures the file has a complete WAL header
   // on disk before renaming the file.
   {
-    ADOPT_WAIT_STATE(appender_->wait_state());
     SCOPED_WAIT_STATUS(WAL_Sync);
     RETURN_NOT_OK(new_segment->Sync());
   }

src/yb/ash/wait_state.cc
@@ -64,9 +64,9 @@ DEFINE_test_flag(uint32, yb_ash_wait_code_to_sleep_at, 0,
 DEPRECATE_FLAG(bool, TEST_export_ash_uuids_as_hex_strings, "04_2024");
 DEFINE_test_flag(bool, ash_debug_aux, false, "Set ASH aux_info to the first 16 characters"
     " of the method tserver is running");
-DEFINE_test_flag(bool, ash_fetch_wait_states_for_raft_log, false, "Should ASH fetch "
+DEFINE_test_flag(bool, ash_fetch_wait_states_for_raft_log, true, "Should ASH fetch "
       "background task states, such as raft log sync/append.");
-DEFINE_test_flag(bool, ash_fetch_wait_states_for_rocksdb_flush_and_compaction, false,
+DEFINE_test_flag(bool, ash_fetch_wait_states_for_rocksdb_flush_and_compaction, true,
       "Should ASH fetch background task states, such as rocksdb flush and compaction.");
 
 namespace yb::ash {
@@ -78,12 +78,12 @@ thread_local WaitStateInfoPtr threadlocal_wait_state_;
 std::atomic_bool TEST_entered_wait_state_code_for_sleep{false};
 
 void MaybeSleepForTests(WaitStateInfo* state, WaitStateCode c) {
-  if (GetAtomicFlag(&FLAGS_TEST_yb_ash_wait_code_to_sleep_at) != to_underlying(c)) {
+  if (FLAGS_TEST_yb_ash_wait_code_to_sleep_at != to_underlying(c)) {
     return;
   }
 
   TEST_entered_wait_state_code_for_sleep.store(true, std::memory_order_release);
-  auto sleep_time_ms = GetAtomicFlag(&FLAGS_TEST_yb_ash_sleep_at_wait_state_ms);
+  auto sleep_time_ms = FLAGS_TEST_yb_ash_sleep_at_wait_state_ms;
   if (sleep_time_ms <= 0) {
     return;
   }
@@ -112,7 +112,7 @@ void WaitStateInfo::TEST_SleepForTests(uint32_t sleep_time_ms) {
     return;
   }
   SleepFor(MonoDelta::FromMilliseconds(decayed_sleep_time_ms));
-  if (GetAtomicFlag(&FLAGS_TEST_trace_ash_wait_code_updates)) {
+  if (FLAGS_TEST_trace_ash_wait_code_updates) {
     VTrace(0, yb::Format("Slept for $0 ms.", decayed_sleep_time_ms));
   }
   VLOG_IF(1, TEST_num_sleeps_ == 0) << "Sleeping at " << yb::GetStackTrace();
@@ -152,13 +152,16 @@ void AshAuxInfo::UpdateFrom(const AshAuxInfo &other) {
 WaitStateInfo::WaitStateInfo()
     : metadata_(AshMetadata{}) {}
 
-void WaitStateInfo::set_code(WaitStateCode c) {
-  if (GetAtomicFlag(&FLAGS_TEST_trace_ash_wait_code_updates)) {
-    VTrace(1, __func__);
-    VTrace(0, yb::Format("$0", ash::ToString(c)));
+void WaitStateInfo::set_code(WaitStateCode code, const char* location) {
+  if (FLAGS_TEST_trace_ash_wait_code_updates) {
+    if (FLAGS_tracing_level >= 1) {
+      VTrace(1, yb::Format("$0 at $1", ash::ToString(code), location));
+    } else {
+      VTrace(0, yb::Format("$0", ash::ToString(code)));
+    }
   }
-  code_ = c;
-  MaybeSleepForTests(this, c);
+  code_ = code;
+  MaybeSleepForTests(this, code);
 }
 
 WaitStateCode WaitStateInfo::code() const {
@@ -240,6 +243,23 @@ const WaitStateInfoPtr& WaitStateInfo::CurrentWaitState() {
   return threadlocal_wait_state_;
 }
 
+void WaitStateInfo::EnableConcurrentUpdates() {
+  concurrent_updates_allowed_ = true;
+  if (FLAGS_TEST_trace_ash_wait_code_updates) {
+    VTrace(0, yb::Format("Enabling concurrent updates"));
+  }
+}
+
+bool WaitStateInfo::IsConcurrentUpdatesEnabled() {
+  return concurrent_updates_allowed_;
+}
+
+void EnableConcurrentUpdates(const WaitStateInfoPtr& ptr) {
+  if (ptr) {
+    ptr->EnableConcurrentUpdates();
+  }
+}
+
 //
 // ScopedAdoptWaitState
 //
@@ -255,19 +275,25 @@ ScopedAdoptWaitState::~ScopedAdoptWaitState() {
 //
 // ScopedWaitStatus
 //
-ScopedWaitStatus::ScopedWaitStatus(WaitStateCode code)
+ScopedWaitStatus::ScopedWaitStatus(WaitStateCode code, const char* location)
     : code_(code),
+      location_(location),
       prev_code_(
           WaitStateInfo::CurrentWaitState()
               ? WaitStateInfo::CurrentWaitState()->mutable_code().exchange(code_)
               : code_) {
-  if (const auto& wait_state = WaitStateInfo::CurrentWaitState()) {
-    if (GetAtomicFlag(&FLAGS_TEST_trace_ash_wait_code_updates)) {
-      wait_state->VTrace(1, __func__);
+  const auto& wait_state = WaitStateInfo::CurrentWaitState();
+  if (!wait_state) {
+    return;
+  }
+  if (FLAGS_TEST_trace_ash_wait_code_updates) {
+    if (FLAGS_tracing_level >= 1) {
+      wait_state->VTrace(1, yb::Format("$0 at $1", ash::ToString(code), location));
+    } else {
       wait_state->VTrace(0, yb::Format("$0", ash::ToString(code)));
     }
   }
-  MaybeSleepForTests(WaitStateInfo::CurrentWaitState().get(), code);
+  MaybeSleepForTests(wait_state.get(), code);
 }
 
 ScopedWaitStatus::~ScopedWaitStatus() {
@@ -278,14 +304,27 @@ ScopedWaitStatus::~ScopedWaitStatus() {
 
   auto expected = code_;
   if (!wait_state->mutable_code().compare_exchange_strong(expected, prev_code_)) {
-    VLOG_WITH_FUNC(3) << "not reverting to prev_code_: " << prev_code_ << " since "
-                      << " current_code: " << expected << " is not " << code_;
+    if (FLAGS_TEST_trace_ash_wait_code_updates) {
+      wait_state->VTrace(
+          0,
+          yb::Format("Failed to revert to $0 at $1. We set it to $2 but it is now $3",
+              ash::ToString(prev_code_), location_, ash::ToString(code_), ash::ToString(expected)));
+    }
+    LOG_IF(DFATAL, !wait_state->IsConcurrentUpdatesEnabled())
+        << " In " << location_ << " wait-state " << wait_state.get() << " was updated to " << code_
+        << " from " << prev_code_ << " but it is currently " << expected
+        << ". Not expecting concurrent updates."
+        << "\nTrace so far:" << wait_state->DumpTraceToString();
     return;
   }
 
-  if (GetAtomicFlag(&FLAGS_TEST_trace_ash_wait_code_updates)) {
-    wait_state->VTrace(1, __func__);
-    wait_state->VTrace(0, yb::Format("$0", ash::ToString(prev_code_)));
+  if (FLAGS_TEST_trace_ash_wait_code_updates) {
+    if (FLAGS_tracing_level >= 1) {
+      wait_state->VTrace(
+          1, yb::Format("Reverted to $0 at $1", ash::ToString(prev_code_), location_));
+    } else {
+      wait_state->VTrace(0, yb::Format("Reverted to $0", ash::ToString(prev_code_)));
+    }
   }
   MaybeSleepForTests(wait_state.get(), prev_code_);
 }
@@ -373,6 +412,7 @@ WaitStateType GetWaitStateType(WaitStateCode code) {
     case WaitStateCode::kRocksDB_ReadBlockFromFile:
     case WaitStateCode::kRocksDB_OpenFile:
     case WaitStateCode::kRocksDB_WriteToFile:
+    case WaitStateCode::kRocksDB_CloseFile:
       return WaitStateType::kDiskIO;
 
     case WaitStateCode::kRocksDB_Flush:
@@ -381,9 +421,6 @@ WaitStateType GetWaitStateType(WaitStateCode code) {
 
     case WaitStateCode::kRocksDB_PriorityThreadPoolTaskPaused:
       return WaitStateType::kWaitOnCondition;
-
-    case WaitStateCode::kRocksDB_CloseFile:
-      return WaitStateType::kDiskIO;
 
     case WaitStateCode::kRocksDB_RateLimiter:
     case WaitStateCode::kRocksDB_WaitForSubcompaction:

src/yb/ash/wait_state.h
@@ -32,8 +32,10 @@
 
 DECLARE_bool(ysql_yb_enable_ash);
 
+#define SET_WAIT_STATUS_TO_CODE(ptr, code) \
+  if ((ptr)) (ptr)->set_code(code, __PRETTY_FUNCTION__)
 #define SET_WAIT_STATUS_TO(ptr, code) \
-  if ((ptr)) (ptr)->set_code(BOOST_PP_CAT(yb::ash::WaitStateCode::k, code))
+  SET_WAIT_STATUS_TO_CODE(ptr, BOOST_PP_CAT(yb::ash::WaitStateCode::k, code))
 #define SET_WAIT_STATUS(code) \
   SET_WAIT_STATUS_TO(yb::ash::WaitStateInfo::CurrentWaitState(), code)
 
@@ -41,7 +43,14 @@ DECLARE_bool(ysql_yb_enable_ash);
   yb::ash::ScopedAdoptWaitState _scoped_state { (ptr) }
 
 #define SCOPED_WAIT_STATUS(code) \
-  yb::ash::ScopedWaitStatus _scoped_status(BOOST_PP_CAT(yb::ash::WaitStateCode::k, code))
+  yb::ash::ScopedWaitStatus _scoped_status( \
+      BOOST_PP_CAT(yb::ash::WaitStateCode::k, code), __PRETTY_FUNCTION__)
+
+#define ASH_ENABLE_CONCURRENT_UPDATES_FOR(ptr) \
+  yb::ash::EnableConcurrentUpdates(ptr)
+#define ASH_ENABLE_CONCURRENT_UPDATES() \
+  ASH_ENABLE_CONCURRENT_UPDATES_FOR(yb::ash::WaitStateInfo::CurrentWaitState())
+
 
 namespace yb {
 class Trace;
@@ -188,6 +197,7 @@ YB_DEFINE_TYPED_ENUM(FixedQueryId, uint8_t,
   ((kQueryIdForCompaction, 3))
   ((kQueryIdForRaftUpdateConsensus, 4))
   ((kQueryIdForCatalogRequests, 5))
+  ((kQueryIdForLogBackgroundSync, 6))
 );
 
 YB_DEFINE_TYPED_ENUM(WaitStateType, uint8_t,
@@ -332,7 +342,7 @@ class WaitStateInfo {
   WaitStateInfo();
   virtual ~WaitStateInfo() = default;
 
-  void set_code(WaitStateCode c);
+  void set_code(WaitStateCode c, const char* location);
   WaitStateCode code() const;
   std::atomic<WaitStateCode>& mutable_code();
 
@@ -394,6 +404,13 @@ class WaitStateInfo {
     VTraceTo(nullptr, level, data);
   }
 
+  virtual std::string DumpTraceToString() {
+    return "n/a";
+  }
+
+  void EnableConcurrentUpdates();
+  bool IsConcurrentUpdatesEnabled();
+
  protected:
   void VTraceTo(Trace* trace, int level, GStringPiece data);
 
@@ -404,8 +421,11 @@ class WaitStateInfo {
   AshMetadata metadata_ GUARDED_BY(mutex_);
   AshAuxInfo aux_info_ GUARDED_BY(mutex_);
 
+  std::atomic_bool concurrent_updates_allowed_{false};
   std::atomic<uint8_t> TEST_num_sleeps_{0};
 };
+
+void EnableConcurrentUpdates(const WaitStateInfoPtr& ptr);
 
 // A helper to adopt a WaitState and revert to the previous WaitState based on RAII.
 // This should only be used on the stack (and thus created and destroyed
@@ -436,11 +456,13 @@ class ScopedAdoptWaitState {
 // be reverted back to the previous state.
 class ScopedWaitStatus {
  public:
-  explicit ScopedWaitStatus(WaitStateCode code);
+  ScopedWaitStatus(WaitStateCode code, const char* location);
   ~ScopedWaitStatus();
 
  private:
   const WaitStateCode code_;
+  // The location where the scoped wait state is created. Used for printing useful debug messages.
+  const char* location_;
   const WaitStateCode prev_code_;
 
   DISALLOW_COPY_AND_ASSIGN(ScopedWaitStatus);

src/yb/client/batcher.cc
@@ -261,6 +261,7 @@ void Batcher::FlushAsync(
     }
   }
 
+  ASH_ENABLE_CONCURRENT_UPDATES();
   SET_WAIT_STATUS(YBClient_WaitingOnDocDB);
   for (auto& op : ops_queue_) {
     VLOG_WITH_PREFIX(4) << "Looking up tablet for " << op.ToString()

src/yb/consensus/log.h
@@ -44,7 +44,8 @@
 #include <vector>
 
 #include <boost/atomic.hpp>
-#include "yb/util/logging.h"
+
+#include "yb/ash/wait_state_fwd.h"
 
 #include "yb/common/common_fwd.h"
 #include "yb/common/opid.h"
@@ -60,6 +61,7 @@
 
 #include "yb/util/status_fwd.h"
 #include "yb/util/locks.h"
+#include "yb/util/logging.h"
 #include "yb/util/monotime.h"
 #include "yb/util/mutex.h"
 #include "yb/util/promise.h"
@@ -663,6 +665,8 @@ class Log : public RefCountedThreadSafe<Log> {
   NewSegmentAllocationCallback new_segment_allocation_callback_;
 
   PreLogRolloverCallback pre_log_rollover_callback_;
+
+  const yb::ash::WaitStateInfoPtr background_synchronizer_wait_state_;
 
   DISALLOW_COPY_AND_ASSIGN(Log);
 };

src/yb/docdb/conflict_resolution.cc
@@ -329,7 +329,7 @@ class ConflictResolver : public std::enable_shared_from_this<ConflictResolver> {
   void InvokeCallback(const Result<HybridTime>& result) {
     // ConflictResolution_ResolveConficts lasts until InvokeCallback.
     ADOPT_WAIT_STATE(wait_state_);
-    SET_WAIT_STATUS(OnCpu_Passive);
+    SET_WAIT_STATUS(OnCpu_Active);
     YB_TRANSACTION_DUMP(
         Conflicts, context_->transaction_id(),
         result.ok() ? *result : HybridTime::kInvalid,
@@ -473,6 +473,7 @@ class ConflictResolver : public std::enable_shared_from_this<ConflictResolver> {
   }
 
   void FetchTransactionStatuses() {
+    ASH_ENABLE_CONCURRENT_UPDATES();
     static const std::string kRequestReason = "conflict resolution"s;
     auto self = shared_from_this();
     pending_requests_.store(conflict_data_->NumActiveTransactions());

src/yb/docdb/wait_queue.cc
@@ -319,9 +319,11 @@ struct WaiterData : public std::enable_shared_from_this<WaiterData> {
       Status waiter_status, HybridTime resume_ht = HybridTime::kInvalid,
       CoarseTimePoint locking_deadline = GetWaitForRelockUnblockedKeysDeadline()) EXCLUDES(mutex_) {
     ADOPT_WAIT_STATE(wait_state);
+    SET_WAIT_STATUS(OnCpu_Active);
+    // ASH: This may later be set to ResolveConficts for another thread to pick up
+    // working on the wait-state.
+    ASH_ENABLE_CONCURRENT_UPDATES_FOR(wait_state);
     TRACE_FUNC();
-    SET_WAIT_STATUS(OnCpu_Passive);
-    SCOPED_WAIT_STATUS(OnCpu_Active);
     auto& status = waiter_status;
     std::optional<UnlockedBatch> unlocked_copy = AtomicConsumeUnlockedBatch();
     if (!unlocked_copy) {
@@ -1307,6 +1309,7 @@ class WaitQueue::Impl {
     // TODO(wait-queues): similar to pg, we can wait 1s or so before beginning deadlock detection.
     // See https://github.com/yugabyte/yugabyte-db/issues/13576
     TRACE_FUNC();
+    ASH_ENABLE_CONCURRENT_UPDATES();
     SET_WAIT_STATUS(ConflictResolution_WaitOnConflictingTxns);
     auto scoped_reporter = waiting_txn_registry_->Create();
     if (waiter_txn_id.IsNil()) {

src/yb/rocksdb/db/builder.cc
@@ -104,7 +104,7 @@ namespace {
       const yb::IOPriority io_priority, Env* env,
       std::shared_ptr<WritableFileWriter>* file_writer) {
     std::unique_ptr<WritableFile> file;
-    SET_WAIT_STATUS(RocksDB_OpenFile);
+    SCOPED_WAIT_STATUS(RocksDB_OpenFile);
     Status s = NewWritableFile(env, filename, &file, env_options);
     if (!s.ok()) {
       return s;
@@ -183,7 +183,7 @@ Status BuildTable(const std::string& dbname,
           meta->UpdateKey(c_iter.key(), UpdateBoundariesType::kSmallest);
         }
 
-        SET_WAIT_STATUS(RocksDB_WriteToFile);
+        SCOPED_WAIT_STATUS(RocksDB_WriteToFile);
         boost::container::small_vector<UserBoundaryValueRef, 0x10> user_values;
         for (; c_iter.Valid(); c_iter.Next()) {
           const Slice& key = c_iter.key();
@@ -234,7 +234,6 @@ Status BuildTable(const std::string& dbname,
         // Status holds for the duration of file close, and also for
         // when we are doing the post-close checks. We can refine and
         // further split the wait-state in future, if necessary.
-        SET_WAIT_STATUS(RocksDB_CloseFile);
         if (s.ok() && !empty && is_split_sst) {
           s = data_file_writer->Close();
         }

commit ce16d9aae92ef9d0b644b9ab9b6b37673f770c30

[#22501] ASH: Fix ci build after #21985
Summary:
483a74c broke one of the CI builds.

Broken build: GCC 12, debug AlmaLinux 9
https://github.com/yugabyte/yugabyte-db/actions/runs/9185098435/job/25258478418
Jira: DB-11430, DB-10904

Test Plan: Jenkins

Reviewers: asrivastava, arybochkin

Reviewed By: asrivastava

Subscribers: ybase

Differential Revision: https://phorge.dev.yugabyte.com/D35278

src/yb/consensus/log.cc
@@ -1641,6 +1641,9 @@ Status Log::Close() {
       RETURN_NOT_OK(CloseCurrentSegment());
       RETURN_NOT_OK(ReplaceSegmentInReaderUnlocked());
       log_state_ = kLogClosed;
+      if (background_synchronizer_wait_state_) {
+        yb::ash::RaftLogAppenderWaitStatesTracker().Untrack(background_synchronizer_wait_state_);
+      }
       VLOG_WITH_PREFIX(1) << "Log closed";
 
       // Release FDs held by these objects.
@@ -1655,9 +1658,6 @@ Status Log::Close() {
 
     default:
       return STATUS(IllegalState, Substitute("Bad state for Close() $0", log_state_));
-  }
-  if (background_synchronizer_wait_state_) {
-    yb::ash::RaftLogAppenderWaitStatesTracker().Untrack(background_synchronizer_wait_state_);
   }
 }
 