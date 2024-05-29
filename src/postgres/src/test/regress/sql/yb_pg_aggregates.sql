b5f03e670d381c1595f033770ec716a82be41719
Hal Takahara <8877300+mtakahar@users.noreply.github.com>
2024-05-29 15:33:20 -0400

    [BACKPORT 2.18][#21012] YSQL: Import 'Account for optimized MinMax aggregates during SS_finalize_plan.'
    Summary:
    Original commit: a2780da7b464efd5056452a6e4ec3f625e0cee4f / D35183
    Import upstream postgres REL_12_STABLE commit 686c995fc2223426706ee3f2472e220e7aac4041 that fixes a wrong result issue.
    
    **Changes from upstream commit**
     - Imported regress test validating fix from `aggregate.*` to `yb_pg_aggregate.*`
    
    **Upstream commit message**
    ```
        Account for optimized MinMax aggregates during SS_finalize_plan.
    
        We are capable of optimizing MIN() and MAX() aggregates on indexed
        columns into subqueries that exploit the index, rather than the normal
        thing of scanning the whole table.  When we do this, we replace the
        Aggref node(s) with Params referencing subquery outputs.  Such Params
        really ought to be included in the per-plan-node extParam/allParam
        sets computed by SS_finalize_plan.  However, we've never done so
        up to now because of an ancient implementation choice to perform
        that substitution during set_plan_references, which runs after
        SS_finalize_plan, so that SS_finalize_plan never sees these Params.
    
        The cleanest fix would be to perform a separate tree walk to do
        these substitutions before SS_finalize_plan runs.  That seems
        unattractive, first because a whole-tree mutation pass is expensive,
        and second because we lack infrastructure for visiting expression
        subtrees in a Plan tree, so that we'd need a new function knowing
        as much as SS_finalize_plan knows about that.  I also considered
        swapping the order of SS_finalize_plan and set_plan_references,
        but that fell foul of various assumptions that seem tricky to fix.
        So the approach adopted here is to teach SS_finalize_plan itself
        to check for such Aggrefs.  I refactored things a bit in setrefs.c
        to avoid having three copies of the code that does that.
    
        Back-patch of v17 commits d0d44049d and 779ac2c74.  When d0d44049d
        went in, there was no evidence that it was fixing a reachable bug,
        so I refrained from back-patching.  Now we have such evidence.
    
        Per bug #18465 from Hal Takahara.  Back-patch to all supported
        branches.
    
        Discussion: https://postgr.es/m/18465-2fae927718976b22@postgresql.org
        Discussion: https://postgr.es/m/2391880.1689025003@sss.pgh.pa.us
    ```
    Jira: DB-9986
    
    Test Plan: ./yb_build.sh --java-test 'org.yb.pgsql.TestPgRegressAggregates'
    
    Reviewers: tnayak, jason
    
    Reviewed By: jason
    
    Subscribers: yql
    
    Tags: #jenkins-ready
    
    Differential Revision: https://phorge.dev.yugabyte.com/D35402

--- src/postgres/src/test/regress/sql/yb_pg_aggregates.sql
+++ src/postgres/src/test/regress/sql/yb_pg_aggregates.sql
@@ -343,6 +343,22 @@ select distinct min(f1), max(f1) from minmaxtest;
 
 drop table minmaxtest cascade;
 
+-- DISTINCT can also trigger wrong answers with hash aggregation (bug #18465)
+begin;
+set local enable_sort = off;
+-- YB note: t1 access path differs from upstream because we favor index scan
+-- when the table is unanalyzed or CBO is not enabled.
+-- YB note: add consistent ordering that matches upstream:
+-- 1. mainly order by absolute value of f1.
+-- 2. order positives before negatives by adding an offset of -1/+1 using the
+--    sign function.
+explain (costs off)
+  select f1, (select distinct min(t1.f1) from int4_tbl t1 where t1.f1 = t0.f1)
+  from int4_tbl t0 order by abs(f1) - sign(f1);
+select f1, (select distinct min(t1.f1) from int4_tbl t1 where t1.f1 = t0.f1)
+from int4_tbl t0 order by abs(f1) - sign(f1);
+rollback;
+
 -- check for correct detection of nested-aggregate errors
 select max(min(unique1)) from tenk1;
 select (select max(min(unique1)) from int8_tbl) from tenk1;
