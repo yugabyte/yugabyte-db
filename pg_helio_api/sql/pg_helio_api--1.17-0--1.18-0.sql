c44eb4ae23fe99933d1b9a28a1ca9c1e67e92775
Deepak Pemmaraju <deepakpe@microsoft.com>
2024-06-04 19:13:56 +0000

    Merged PR 1359979: Adding a read only role in the backend
    ### Does this PR have any customer impact?
    By default, no. Not unless they choose to use the read only role.
    
    ### Type (Feature, Refactoring, Bugfix, DevOps, Testing, Perf, etc)
    Feature
    
    ### Does it involve schema level changes? (Table, Column, Index, UDF, etc level changes)
    Yes
    
    ### Are you introducing any new config? If yes, do you have tests with and without them being set?
    No
    
    ### Release Note (Refer [Template](../docs/release_notes/pgmongo_template.md))
    New Feature
    
    ### Description
    Adding a readonly role in the backend. The goal is to enable native readonly users in Mongo VCore
    
    Related work items: #3141137, #3158558

--- pg_helio_api/sql/pg_helio_api--1.17-0--1.18-0.sql
+++ pg_helio_api/sql/pg_helio_api--1.17-0--1.18-0.sql
@@ -6,4 +6,5 @@ SET search_path TO helio_api;
 #include "udfs/aggregation/bson_geonear_functions--1.18-0.sql"
 #include "operators/bson_geospatial_operators--1.18-0.sql"
 #include "operators/bson_gist_geospatial_op_classes_members--1.18-0.sql"
+#include "rbac/extension_readonly_setup--1.17-1.sql"
 RESET search_path;
---
+++ pg_helio_api/sql/rbac/extension_readonly_setup--1.17-1.sql
@@ -0,0 +1,23 @@
+DO
+$do$
+BEGIN
+	/*
+	 * The role is a system-wide object which is not dropped if the extension
+	 * is dropped. Therefore, if helioapi is repeatedly created and dropped,
+	 * a regular CREATE ROLE would fail since __API_ADMIN_ROLE__ still exists.
+	 * We therefore only create the role if it does not exist.
+	 */
+	IF NOT EXISTS (SELECT FROM pg_catalog.pg_roles WHERE rolname = 'helio_readonly_role') THEN
+        CREATE ROLE helio_readonly_role;
+    END IF;
+END
+$do$;
+
+GRANT USAGE ON SCHEMA __API_CATALOG_SCHEMA__ TO helio_readonly_role;
+GRANT USAGE ON SCHEMA __API_SCHEMA_INTERNAL__ TO helio_readonly_role;
+GRANT USAGE ON SCHEMA __API_SCHEMA__ TO helio_readonly_role;
+GRANT USAGE ON SCHEMA __API_DATA_SCHEMA__ TO helio_readonly_role;
+ 
+ALTER DEFAULT PRIVILEGES IN SCHEMA __API_DATA_SCHEMA__ GRANT SELECT ON TABLES TO helio_readonly_role;
+GRANT SELECT ON TABLE __API_CATALOG_SCHEMA__.collections TO helio_readonly_role;
+GRANT SELECT ON TABLE __API_CATALOG_SCHEMA__.collection_indexes TO helio_readonly_role;
