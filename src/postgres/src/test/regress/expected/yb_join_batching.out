yugabyte/yugabyte-db

commit 4f6da5b2d03adf231603d9700d7b8e9ecbac89e3

[#20715] YSQL: Allow BNL on joins over different integer types
Summary:
When we join across different types, say an `int2` and an `int4`, we only allow join-orderings that result in the bigger type being on the inner-side. This restricts the allowed join-orderings. This is to prevent lossily down-casting outer rows before they are sent to the inner table. We currently have code in `yb_scan.c` to deal with situations where we are sending an IN list of bigger types by manually checking to see if they are in bounds of the indexed column type. In this change, we leverage said code to safely allow BNL's on cross-type joins.

Changes done
-- `YbBindSearchArray` in `yb_scan.c` enhanced to check IN array elements to see if they are within the bounds of the indexed column's type. If the types of the RHS and LHS match then there is no need to check this for more than one IN element. (TODO: Skip this check if RHS type size is also less than LHS type size)

-- The hash table used in the BNL execution node has been augmented to use different hash functions for the BNL's outer and inner tuples to support differing types.
Jira: DB-9717

Test Plan: ./yb_build.sh --java-test 'org.yb.pgsql.TestPgRegressJoin'

Reviewers: mtakahara

Reviewed By: mtakahara

Subscribers: yql

Differential Revision: https://phorge.dev.yugabyte.com/D31834

src/postgres/src/backend/access/yb_access/yb_scan.c
@@ -1197,6 +1197,104 @@ static bool YbIsScanCompatible(Oid column_typid,
 	}
 }
 
+static bool YbShouldRecheckEquality(Oid column_typid, Oid value_typid)
+{
+	if (column_typid == value_typid)
+		return false;
+
+	bool is_compatible_int_type = false;
+	switch (column_typid)
+	{
+		case INT8OID:
+			switch_fallthrough();
+		case INT4OID:
+			is_compatible_int_type |= (value_typid == INT4OID);
+			switch_fallthrough();
+		case INT2OID:
+			is_compatible_int_type |= (value_typid == INT2OID);
+			break;
+		default:
+			Assert(!is_compatible_int_type);
+			break;
+	}
+	return !is_compatible_int_type;
+}
+
+static bool YbNeedTupleRangeCheck(Datum value, TupleDesc bind_desc,
+								  int key_length, ScanKey keys[])
+{
+	/* Move past header key. */
+	++keys;
+	--key_length;
+
+	Oid tupType =
+		HeapTupleHeaderGetTypeId(DatumGetHeapTupleHeader(value));
+	Oid tupTypmod =
+		HeapTupleHeaderGetTypMod(DatumGetHeapTupleHeader(value));
+	TupleDesc val_tupdesc = lookup_rowtype_tupdesc(tupType, tupTypmod);
+	bool need_check = false;
+
+	for (int i = 0; i < key_length; i++)
+	{
+		Oid val_type = ybc_get_atttypid(val_tupdesc, i + 1);
+		Oid column_type = ybc_get_atttypid(bind_desc, keys[i]->sk_attno);
+		if (YbShouldRecheckEquality(column_type, val_type))
+		{
+			need_check = true;
+			break;
+		}
+	}
+	ReleaseTupleDesc(val_tupdesc);
+	return need_check;
+}
+
+static bool YbIsTupleInRange(Datum value, TupleDesc bind_desc,
+							 int key_length, ScanKey keys[])
+{
+	/* Move past header key. */
+	++keys;
+	--key_length;
+
+	Oid tupType =
+		HeapTupleHeaderGetTypeId(DatumGetHeapTupleHeader(value));
+	Oid tupTypmod =
+		HeapTupleHeaderGetTypMod(DatumGetHeapTupleHeader(value));
+	TupleDesc val_tupdesc = lookup_rowtype_tupdesc(tupType, tupTypmod);
+
+	Datum datum_values[key_length];
+	bool datum_nulls[key_length];
+
+	HeapTupleData tuple;
+	ItemPointerSetInvalid(&(tuple.t_self));
+	tuple.t_tableOid = InvalidOid;
+	tuple.t_len = HeapTupleHeaderGetDatumLength(value);
+	tuple.t_data = DatumGetHeapTupleHeader(value);
+	heap_deform_tuple(&tuple, val_tupdesc,
+					  datum_values, datum_nulls);
+	(void) datum_nulls;
+	bool is_in_range = true;
+
+	for (int i = 0; i < key_length; i++) {
+		Datum val = datum_values[i];
+		Oid val_type = ybc_get_atttypid(val_tupdesc, i + 1);
+		Oid column_type = ybc_get_atttypid(bind_desc, keys[i]->sk_attno);
+
+		if (!YbShouldRecheckEquality(column_type, val_type))
+			continue;
+
+		if ((column_type == INT2OID || column_type == INT4OID) &&
+			!YbIsIntegerInRange(val, val_type,
+								column_type == INT2OID ? SHRT_MIN : INT_MIN,
+								column_type == INT2OID ? SHRT_MAX : INT_MAX))
+		{
+			is_in_range = false;
+			break;
+		}
+	}
+	ReleaseTupleDesc(val_tupdesc);
+	return is_in_range;
+}
+
 /*
  * We require compatible column type and value type to avoid misinterpreting the value Datum
  * using a column type that can cause wrong scan results. Returns true if the column type
@@ -1233,7 +1331,7 @@ YbCheckScanTypes(YbScanDesc ybScan, YbScanPlan scan_plan, int i)
 
 static bool
 YbBindRowComparisonKeys(YbScanDesc ybScan, YbScanPlan scan_plan,
-								int skey_index, bool is_for_precheck)
+						int skey_index, bool is_for_precheck)
 {
 	int last_att_no = YBFirstLowInvalidAttributeNumber;
 	Relation index = ybScan->index;
@@ -1503,6 +1601,10 @@ YbBindSearchArray(YbScanDesc ybScan, YbScanPlan scan_plan,
 								   scan_plan->bind_key_attnums[i]);
 
 	num_valid = 0;
+
+	bool should_check_row_range = is_row && num_elems > 0 &&
+		YbNeedTupleRangeCheck(elem_values[0], scan_plan->bind_desc,
+							  length_of_key, &ybScan->keys[i]);
 	for (j = 0; j < num_elems; j++)
 	{
 		if (elem_nulls[j])
@@ -1525,11 +1627,21 @@ YbBindSearchArray(YbScanDesc ybScan, YbScanPlan scan_plan,
 		 * This needs to be rechecked when row IN filters can
 		 * arise through other means.
 		 */
-		if ((!is_row && !elem_nulls[j])
-			|| (is_row &&
-				!HeapTupleHeaderHasNulls(
-					DatumGetHeapTupleHeader(elem_values[j]))))
-			elem_values[num_valid++] = elem_values[j];
+		if (is_row)
+		{
+			if (HeapTupleHeaderHasNulls(
+					DatumGetHeapTupleHeader(elem_values[j])))
+				continue;
+
+			if (should_check_row_range &&
+				!YbIsTupleInRange(elem_values[j],
+			 					  scan_plan->bind_desc,
+			 					  length_of_key,
+								  &ybScan->keys[i]))
+				continue;
+		}
+
+		elem_values[num_valid++] = elem_values[j];
 	}
 
 	pfree(elem_nulls);

src/postgres/src/test/regress/expected/yb_join_batching.out
@@ -800,15 +800,14 @@ SELECT * FROM p1 t1 WHERE EXISTS (SELECT 1 FROM p2 t2 WHERE t1.a = t2.b) AND t1.
 CREATE TABLE int2type (a int2, PRIMARY KEY(a ASC));
 INSERT INTO int2type VALUES (1), (4), (555), (-33), (6923);
 -- testing batching on join conditions across different types (int2, int4)
--- We shouldn't cause any casting on inner expressions.
 /*+Leading((i2 p))*/ EXPLAIN (COSTS OFF) SELECT * FROM int2type i2 JOIN p1 p ON i2.a = p.a;
-                                      QUERY PLAN
---------------------------------------------------------------------------------------
+                     QUERY PLAN
+-----------------------------------------------------
  YB Batched Nested Loop Join
    Join Filter: (i2.a = p.a)
    ->  Index Scan using int2type_pkey on int2type i2
    ->  Index Scan using p1_pkey on p1 p
-         Index Cond: (a = ANY (ARRAY[(i2.a)::integer, ($1)::integer, ($2)::integer]))
+         Index Cond: (a = ANY (ARRAY[i2.a, $1, $2]))
 (5 rows)
 
 /*+Leading((i2 p))*/ SELECT * FROM int2type i2 JOIN p1 p ON i2.a = p.a;
@@ -817,16 +816,15 @@ INSERT INTO int2type VALUES (1), (4), (555), (-33), (6923);
  4 | 4 | 4 | 0004
 (1 row)
 
--- This shouldn't be batched as the LHS of the batched IN expression would
--- have a cast.
 /*+Leading((p i2))*/ EXPLAIN (COSTS OFF) SELECT * FROM int2type i2 JOIN p1 p ON i2.a = p.a;
                      QUERY PLAN
 -----------------------------------------------------
- Nested Loop
+ YB Batched Nested Loop Join
+   Join Filter: (i2.a = p.a)
    ->  Seq Scan on p1 p
    ->  Index Scan using int2type_pkey on int2type i2
-         Index Cond: (a = p.a)
-(4 rows)
+         Index Cond: (a = ANY (ARRAY[p.a, $1, $2]))
+(5 rows)
 
 /*+Leading((p i2))*/ SELECT * FROM int2type i2 JOIN p1 p ON i2.a = p.a;
  a | a | b |  c
@@ -835,6 +833,170 @@ INSERT INTO int2type VALUES (1), (4), (555), (-33), (6923);
 (1 row)
 
 DROP TABLE int2type;
+create table floattable1(a float8, b float4, primary key(a asc, b asc));
+create table floattable2(a float4, b float8, primary key(a asc, b asc));
+insert into floattable2 values (1.12312, 29401.212931231);
+insert into floattable2 values (1.12312, 2941.212931231);
+insert into floattable1 select * from floattable2;
+explain (costs off) /*+Leading((f2 f1)) YbBatchedNL(f1 f2)*/ select * from floattable1 f1, floattable2 f2 where f1.a = f2.a;
+                                                   QUERY PLAN
+-----------------------------------------------------------------------------------------------------------------
+ YB Batched Nested Loop Join
+   Join Filter: (f1.a = f2.a)
+   ->  Index Scan using floattable2_pkey on floattable2 f2
+   ->  Index Scan using floattable1_pkey on floattable1 f1
+         Index Cond: (a = ANY (ARRAY[(f2.a)::double precision, ($1)::double precision, ($2)::double precision]))
+(5 rows)
+
+/*+Leading((f2 f1)) YbBatchedNL(f1 f2)*/ select * from floattable1 f1, floattable2 f2 where f1.a = f2.a;
+        a         |    b    |    a    |        b
+------------------+---------+---------+-----------------
+ 1.12311995029449 | 2941.21 | 1.12312 |  2941.212931231
+ 1.12311995029449 | 2941.21 | 1.12312 | 29401.212931231
+ 1.12311995029449 | 29401.2 | 1.12312 |  2941.212931231
+ 1.12311995029449 | 29401.2 | 1.12312 | 29401.212931231
+(4 rows)
+
+-- We don't support float4 IN <float8 values> yet
+explain (costs off) /*+Leading((f1 f2)) YbBatchedNL(f1 f2)*/ select * from floattable1 f1, floattable2 f2 where f1.a = f2.a;
+                        QUERY PLAN
+-----------------------------------------------------------
+ Nested Loop
+   ->  Index Scan using floattable1_pkey on floattable1 f1
+   ->  Index Scan using floattable2_pkey on floattable2 f2
+         Index Cond: (a = f1.a)
+(4 rows)
+
+/*+Leading((f1 f2)) YbBatchedNL(f1 f2)*/ select * from floattable1 f1, floattable2 f2 where f1.a = f2.a;
+        a         |    b    |    a    |        b
+------------------+---------+---------+-----------------
+ 1.12311995029449 | 2941.21 | 1.12312 |  2941.212931231
+ 1.12311995029449 | 2941.21 | 1.12312 | 29401.212931231
+ 1.12311995029449 | 29401.2 | 1.12312 |  2941.212931231
+ 1.12311995029449 | 29401.2 | 1.12312 | 29401.212931231
+(4 rows)
+
+drop table floattable1;
+drop table floattable2;
+create table ss1(a bigint, b int, primary key(a asc, b asc));
+create table ss2(a int, b bigint, primary key(a asc, b asc));
+-- 0x8000000000000001 should not join with 1 and 0x8000000000000002 should not join with 2
+insert into ss1 values (1,2), (3,4), (x'8000000000000001'::bigint, 1);
+insert into ss2 values (1,2), (3,4), (1,1), (1, x'8000000000000002'::bigint);
+explain (costs off) /*+Leading((ss1 ss2))*/ select * from ss1, ss2 where ss1.a = ss2.a and ss1.b = ss2.b;
+                                         QUERY PLAN
+--------------------------------------------------------------------------------------------
+ YB Batched Nested Loop Join
+   Join Filter: ((ss1.a = ss2.a) AND (ss1.b = ss2.b))
+   ->  Index Scan using ss1_pkey on ss1
+   ->  Index Scan using ss2_pkey on ss2
+         Index Cond: (ROW(a, b) = ANY (ARRAY[ROW(ss1.a, ss1.b), ROW($1, $4), ROW($2, $5)]))
+(5 rows)
+
+/*+Leading((ss1 ss2))*/ select * from ss1, ss2 where ss1.a = ss2.a and ss1.b = ss2.b;
+ a | b | a | b
+---+---+---+---
+ 1 | 2 | 1 | 2
+ 3 | 4 | 3 | 4
+(2 rows)
+
+explain (costs off) /*+Leading((ss2 ss1))*/ select * from ss1, ss2 where ss1.a = ss2.a and ss1.b = ss2.b;
+                                         QUERY PLAN
+--------------------------------------------------------------------------------------------
+ YB Batched Nested Loop Join
+   Join Filter: ((ss1.a = ss2.a) AND (ss1.b = ss2.b))
+   ->  Index Scan using ss2_pkey on ss2
+   ->  Index Scan using ss1_pkey on ss1
+         Index Cond: (ROW(a, b) = ANY (ARRAY[ROW(ss2.a, ss2.b), ROW($1, $4), ROW($2, $5)]))
+(5 rows)
+
+/*+Leading((ss2 ss1))*/ select * from ss1, ss2 where ss1.a = ss2.a and ss1.b = ss2.b;
+ a | b | a | b
+---+---+---+---
+ 1 | 2 | 1 | 2
+ 3 | 4 | 3 | 4
+(2 rows)
+
+drop table ss1;
+drop table ss2;
+create table ss1(a int, b float8, primary key(a asc, b asc));
+create table ss2(a bigint, b float4, primary key(a asc, b asc));
+insert into ss1 values (1, 0.0), (1, 0.5), (3, 0.625), (2, 0.625 + POW(2, -25)), (2, 0.625);
+insert into ss2 values (1, 0.0), (1, 0.5), (x'8000000000000001'::bigint, 0.5), (2, 0.625);
+explain (costs off) /*+YbBatchedNL(ss1 ss2) Leading((ss2 ss1))*/ select * from ss1, ss2 where ss1.a = ss2.a and ss1.b = ss2.b;
+                                                                       QUERY PLAN
+--------------------------------------------------------------------------------------------------------------------------------------------------------
+ YB Batched Nested Loop Join
+   Join Filter: ((ss1.a = ss2.a) AND (ss1.b = ss2.b))
+   ->  Index Scan using ss2_pkey on ss2
+   ->  Index Scan using ss1_pkey on ss1
+         Index Cond: (ROW(a, b) = ANY (ARRAY[ROW(ss2.a, (ss2.b)::double precision), ROW($1, ($4)::double precision), ROW($2, ($5)::double precision)]))
+(5 rows)
+
+/*+YbBatchedNL(ss1 ss2) Leading((ss2 ss1))*/ select * from ss1, ss2 where ss1.a = ss2.a and ss1.b = ss2.b;
+ a |   b   | a |   b
+---+-------+---+-------
+ 1 |     0 | 1 |     0
+ 1 |   0.5 | 1 |   0.5
+ 2 | 0.625 | 2 | 0.625
+(3 rows)
+
+explain (costs off) /*+YbBatchedNL(ss1 ss2) Leading((ss1 ss2))*/ select * from ss1, ss2 where ss1.a = ss2.a and ss1.b = ss2.b;
+                    QUERY PLAN
+---------------------------------------------------
+ Nested Loop
+   ->  Index Scan using ss1_pkey on ss1
+   ->  Index Scan using ss2_pkey on ss2
+         Index Cond: ((a = ss1.a) AND (b = ss1.b))
+(4 rows)
+
+/*+YbBatchedNL(ss1 ss2) Leading((ss1 ss2))*/ select * from ss1, ss2 where ss1.a = ss2.a and ss1.b = ss2.b;
+ a |   b   | a |   b
+---+-------+---+-------
+ 1 |     0 | 1 |     0
+ 1 |   0.5 | 1 |   0.5
+ 2 | 0.625 | 2 | 0.625
+(3 rows)
+
+drop table ss1;
+drop table ss2;
+create table ss1(a timestamp primary key);
+create table ss2(a date primary key);
+insert into ss1 values (to_timestamp('27 Nov 2100 08:14:30', 'DD MON YYYY HH:MI:SS')), (to_timestamp('27 Nov 2100', 'DD MON YYYY'));
+insert into ss2 values (to_date('27 Nov 2100 08:14:30', 'DD MON YYYY')), (to_date('27 Feb 2010 08:14:30', 'DD MON YYYY'));
+explain (costs off) /*+YbBatchedNL(ss1 ss2) Leading((ss2 ss1))*/ select * from ss1, ss2 where ss1.a = ss2.a;
+                                                                    QUERY PLAN
+---------------------------------------------------------------------------------------------------------------------------------------------------
+ YB Batched Nested Loop Join
+   Join Filter: (ss1.a = ss2.a)
+   ->  Seq Scan on ss2
+   ->  Index Scan using ss1_pkey on ss1
+         Index Cond: (a = ANY (ARRAY[(ss2.a)::timestamp without time zone, ($1)::timestamp without time zone, ($2)::timestamp without time zone]))
+(5 rows)
+
+/*+YbBatchedNL(ss1 ss2) Leading((ss2 ss1))*/ select * from ss1, ss2 where ss1.a = ss2.a;
+            a             |     a
+--------------------------+------------
+ Sat Nov 27 00:00:00 2100 | 11-27-2100
+(1 row)
+
+explain (costs off) /*+YbBatchedNL(ss1 ss2) Leading((ss1 ss2))*/ select * from ss1, ss2 where ss1.a = ss2.a;
+               QUERY PLAN
+----------------------------------------
+ Nested Loop
+   ->  Seq Scan on ss1
+   ->  Index Scan using ss2_pkey on ss2
+         Index Cond: (a = ss1.a)
+(4 rows)
+
+/*+YbBatchedNL(ss1 ss2) Leading((ss1 ss2))*/ select * from ss1, ss2 where ss1.a = ss2.a;
+            a             |     a
+--------------------------+------------
+ Sat Nov 27 00:00:00 2100 | 11-27-2100
+(1 row)
+
+drop table ss1;
+drop table ss2;
 CREATE TABLE q1(a int);
 CREATE TABLE q2(a int);
 CREATE TABLE q3(a int primary key);

src/postgres/src/test/regress/sql/yb_join_batching.sql
@@ -196,14 +196,60 @@ CREATE TABLE int2type (a int2, PRIMARY KEY(a ASC));
 INSERT INTO int2type VALUES (1), (4), (555), (-33), (6923);
 
 -- testing batching on join conditions across different types (int2, int4)
--- We shouldn't cause any casting on inner expressions.
 /*+Leading((i2 p))*/ EXPLAIN (COSTS OFF) SELECT * FROM int2type i2 JOIN p1 p ON i2.a = p.a;
 /*+Leading((i2 p))*/ SELECT * FROM int2type i2 JOIN p1 p ON i2.a = p.a;
--- This shouldn't be batched as the LHS of the batched IN expression would
--- have a cast.
 /*+Leading((p i2))*/ EXPLAIN (COSTS OFF) SELECT * FROM int2type i2 JOIN p1 p ON i2.a = p.a;
 /*+Leading((p i2))*/ SELECT * FROM int2type i2 JOIN p1 p ON i2.a = p.a;
 DROP TABLE int2type;
+
+create table floattable1(a float8, b float4, primary key(a asc, b asc));
+create table floattable2(a float4, b float8, primary key(a asc, b asc));
+insert into floattable2 values (1.12312, 29401.212931231);
+insert into floattable2 values (1.12312, 2941.212931231);
+insert into floattable1 select * from floattable2;
+explain (costs off) /*+Leading((f2 f1)) YbBatchedNL(f1 f2)*/ select * from floattable1 f1, floattable2 f2 where f1.a = f2.a;
+/*+Leading((f2 f1)) YbBatchedNL(f1 f2)*/ select * from floattable1 f1, floattable2 f2 where f1.a = f2.a;
+
+-- We don't support float4 IN <float8 values> yet
+explain (costs off) /*+Leading((f1 f2)) YbBatchedNL(f1 f2)*/ select * from floattable1 f1, floattable2 f2 where f1.a = f2.a;
+/*+Leading((f1 f2)) YbBatchedNL(f1 f2)*/ select * from floattable1 f1, floattable2 f2 where f1.a = f2.a;
+
+drop table floattable1;
+drop table floattable2;
+
+create table ss1(a bigint, b int, primary key(a asc, b asc));
+create table ss2(a int, b bigint, primary key(a asc, b asc));
+-- 0x8000000000000001 should not join with 1 and 0x8000000000000002 should not join with 2
+insert into ss1 values (1,2), (3,4), (x'8000000000000001'::bigint, 1);
+insert into ss2 values (1,2), (3,4), (1,1), (1, x'8000000000000002'::bigint);
+explain (costs off) /*+Leading((ss1 ss2))*/ select * from ss1, ss2 where ss1.a = ss2.a and ss1.b = ss2.b;
+/*+Leading((ss1 ss2))*/ select * from ss1, ss2 where ss1.a = ss2.a and ss1.b = ss2.b;
+explain (costs off) /*+Leading((ss2 ss1))*/ select * from ss1, ss2 where ss1.a = ss2.a and ss1.b = ss2.b;
+/*+Leading((ss2 ss1))*/ select * from ss1, ss2 where ss1.a = ss2.a and ss1.b = ss2.b;
+drop table ss1;
+drop table ss2;
+
+create table ss1(a int, b float8, primary key(a asc, b asc));
+create table ss2(a bigint, b float4, primary key(a asc, b asc));
+insert into ss1 values (1, 0.0), (1, 0.5), (3, 0.625), (2, 0.625 + POW(2, -25)), (2, 0.625);
+insert into ss2 values (1, 0.0), (1, 0.5), (x'8000000000000001'::bigint, 0.5), (2, 0.625);
+explain (costs off) /*+YbBatchedNL(ss1 ss2) Leading((ss2 ss1))*/ select * from ss1, ss2 where ss1.a = ss2.a and ss1.b = ss2.b;
+/*+YbBatchedNL(ss1 ss2) Leading((ss2 ss1))*/ select * from ss1, ss2 where ss1.a = ss2.a and ss1.b = ss2.b;
+explain (costs off) /*+YbBatchedNL(ss1 ss2) Leading((ss1 ss2))*/ select * from ss1, ss2 where ss1.a = ss2.a and ss1.b = ss2.b;
+/*+YbBatchedNL(ss1 ss2) Leading((ss1 ss2))*/ select * from ss1, ss2 where ss1.a = ss2.a and ss1.b = ss2.b;
+drop table ss1;
+drop table ss2;
+
+create table ss1(a timestamp primary key);
+create table ss2(a date primary key);
+insert into ss1 values (to_timestamp('27 Nov 2100 08:14:30', 'DD MON YYYY HH:MI:SS')), (to_timestamp('27 Nov 2100', 'DD MON YYYY'));
+insert into ss2 values (to_date('27 Nov 2100 08:14:30', 'DD MON YYYY')), (to_date('27 Feb 2010 08:14:30', 'DD MON YYYY'));
+explain (costs off) /*+YbBatchedNL(ss1 ss2) Leading((ss2 ss1))*/ select * from ss1, ss2 where ss1.a = ss2.a;
+/*+YbBatchedNL(ss1 ss2) Leading((ss2 ss1))*/ select * from ss1, ss2 where ss1.a = ss2.a;
+explain (costs off) /*+YbBatchedNL(ss1 ss2) Leading((ss1 ss2))*/ select * from ss1, ss2 where ss1.a = ss2.a;
+/*+YbBatchedNL(ss1 ss2) Leading((ss1 ss2))*/ select * from ss1, ss2 where ss1.a = ss2.a;
+drop table ss1;
+drop table ss2;
 
 CREATE TABLE q1(a int);
 CREATE TABLE q2(a int);

src/postgres/src/backend/executor/execGrouping.c
@@ -97,12 +97,15 @@ void
 execTuplesHashPrepare(int numCols,
 					  Oid *eqOperators,
 					  Oid **eqFuncOids,
-					  FmgrInfo **hashFunctions)
+					  FmgrInfo **leftHashFunctions,
+					  FmgrInfo **rightHashFunctions)
 {
 	int			i;
 
 	*eqFuncOids = (Oid *) palloc(numCols * sizeof(Oid));
-	*hashFunctions = (FmgrInfo *) palloc(numCols * sizeof(FmgrInfo));
+	*leftHashFunctions = (FmgrInfo *) palloc(numCols * sizeof(FmgrInfo));
+	if (rightHashFunctions)
+		*rightHashFunctions = (FmgrInfo *) palloc(numCols * sizeof(FmgrInfo));
 
 	for (i = 0; i < numCols; i++)
 	{
@@ -117,9 +120,13 @@ execTuplesHashPrepare(int numCols,
 			elog(ERROR, "could not find hash function for hash operator %u",
 				 eq_opr);
 		/* We're not supporting cross-type cases here */
-		Assert(left_hash_function == right_hash_function);
+		/* YB: We do support cross-type cases if rightHashFunctions is valid. */
+		Assert(rightHashFunctions != NULL ||
+			   left_hash_function == right_hash_function);
 		(*eqFuncOids)[i] = eq_function;
-		fmgr_info(right_hash_function, &(*hashFunctions)[i]);
+		if (rightHashFunctions != NULL)
+			fmgr_info(right_hash_function, &(*rightHashFunctions)[i]);
+		fmgr_info(left_hash_function, &(*leftHashFunctions)[i]);
 	}
 }
 
@@ -135,8 +142,16 @@ ybPrepareOuterExprsEqualFn(List *outer_exprs, Oid *eqOps, PlanState *parent)
 		Expr *lhs = yb_copy_replace_varnos(rhs, OUTER_VAR, INNER_VAR);
 
 		int collid = exprCollation((Node *) rhs);
+		Oid eqop;
+		/*
+		 * We can't directly use eqOps[i] as that might be a cross-type
+		 * comparison between the outer and inner sides. We attempt to get
+		 * a hashable op from the same opfamily that equates the type of lhs
+		 * with itself.
+		 */
+		(void) get_compatible_hash_operators(eqOps[i], NULL, &eqop);
 		Expr *qual =
-			make_opclause(eqOps[i], BOOLOID, false, lhs, rhs,
+			make_opclause(eqop, BOOLOID, false, lhs, rhs,
 						  collid, collid);
 		set_opfuncid((OpExpr *) qual);
 		quals = lappend(quals, qual);

src/postgres/src/backend/executor/nodeAgg.c
@@ -1414,7 +1414,8 @@ find_hash_columns(AggState *aggstate)
 		execTuplesHashPrepare(perhash->numCols,
 							  perhash->aggnode->grpOperators,
 							  &perhash->eqfuncoids,
-							  &perhash->hashfunctions);
+							  &perhash->hashfunctions,
+							  NULL);
 		perhash->hashslot =
 			ExecAllocTableSlot(&estate->es_tupleTable, hashDesc);
 

src/postgres/src/backend/executor/nodeIndexscan.c
@@ -1897,13 +1897,13 @@ ExecIndexBuildScanKeys(PlanState *planstate, Relation index,
 				 * initialize the scan key's fields appropriately
 				 */
 				ScanKeyEntryInitialize(this_key,
-									   					 flags,
-									   					 varattno,  /* attribute number to scan */
-									   					 op_strategy, /* op's strategy */
-									   					 op_righttype,	/* strategy subtype */
-									   					 saop->inputcollid,	/* collation */
-									   					 opfuncid,	/* reg proc to use */
-									   					 scanvalue);	/* constant */
+									   flags,
+									   varattno,  /* attribute number to scan */
+									   op_strategy, /* op's strategy */
+									   op_righttype,	/* strategy subtype */
+									   saop->inputcollid,	/* collation */
+									   opfuncid,	/* reg proc to use */
+									   scanvalue);	/* constant */
 				n_sub_key++;
 			}
 

src/postgres/src/backend/executor/nodeRecursiveunion.c
@@ -254,7 +254,8 @@ ExecInitRecursiveUnion(RecursiveUnion *node, EState *estate, int eflags)
 		execTuplesHashPrepare(node->numCols,
 							  node->dupOperators,
 							  &rustate->eqfuncoids,
-							  &rustate->hashfunctions);
+							  &rustate->hashfunctions,
+							  NULL);
 		build_hash_table(rustate);
 	}
 

src/postgres/src/backend/executor/nodeSetOp.c
@@ -545,7 +545,8 @@ ExecInitSetOp(SetOp *node, EState *estate, int eflags)
 		execTuplesHashPrepare(node->numCols,
 							  node->dupOperators,
 							  &setopstate->eqfuncoids,
-							  &setopstate->hashfunctions);
+							  &setopstate->hashfunctions,
+							  NULL);
 	else
 		setopstate->eqfunction =
 			execTuplesMatchPrepare(outerDesc,

commit 71f5dcc2db244efc638633df15b8ab16d4c8f45b

[#22800] YSQL: Fix logic to access bound key types during tuple IN condition recheck
Summary:
In `YbIsTupleInRange` from `yb_scan.c`, the code accesses the scan plan's bind descriptor incorrectly. The bind descriptor accessible here describes the binds of the original relation during a primary index scan. The columns of the main relation might not be ordered the same way as in the primary index. For that reason, callers of `ybc_get_atttypid` in this function usually index into a `bind_desc` variable with an index derived from `scan_plan->bind_key_attnums`. The changed lines incorrectly did not do that before this change.

Fixes a bug introduced by: D31834/98d69ec81738982de456ca3e0539919f512fc388
Needs backports to: 2024.1

Fixes #22800
Jira: DB-11701

Test Plan: ./yb_build.sh --java-test 'org.java.pgsql.TestPgRegressJoin'

Reviewers: amartsinchyk, mtakahara

Reviewed By: mtakahara

Subscribers: yql

Differential Revision: https://phorge.dev.yugabyte.com/D35766

src/postgres/src/backend/access/yb_access/yb_scan.c
@@ -1343,10 +1343,12 @@ static bool YbNeedTupleRangeCheck(Datum value, TupleDesc bind_desc,
 }
 
 static bool YbIsTupleInRange(Datum value, TupleDesc bind_desc,
-							 int key_length, ScanKey keys[])
+							 int key_length, ScanKey keys[],
+							 AttrNumber bind_key_attnums[])
 {
 	/* Move past header key. */
 	++keys;
+	++bind_key_attnums;
 	--key_length;
 
 	Oid tupType =
@@ -1371,7 +1373,7 @@ static bool YbIsTupleInRange(Datum value, TupleDesc bind_desc,
 	for (int i = 0; i < key_length; i++) {
 		Datum val = datum_values[i];
 		Oid val_type = ybc_get_atttypid(val_tupdesc, i + 1);
-		Oid column_type = ybc_get_atttypid(bind_desc, keys[i]->sk_attno);
+		Oid column_type = ybc_get_atttypid(bind_desc, bind_key_attnums[i]);
 
 		if (!YbShouldRecheckEquality(column_type, val_type))
 			continue;
@@ -1737,7 +1739,8 @@ YbBindSearchArray(YbScanDesc ybScan, YbScanPlan scan_plan,
 				!YbIsTupleInRange(elem_values[j],
 			 					  scan_plan->bind_desc,
 			 					  length_of_key,
-								  &ybScan->keys[i]))
+								  &ybScan->keys[i],
+								  &scan_plan->bind_key_attnums[i]))
 				continue;
 		}
 

src/postgres/src/test/regress/expected/yb_join_batching.out
@@ -1829,6 +1829,28 @@ a = int4table.a;
 
 drop table oidtable;
 drop table int4table;
+CREATE TABLE p_inner(a int, b text, PRIMARY KEY(b asc, a asc));
+CREATE TABLE p_outer(a bigint, b text);
+INSERT INTO p_outer VALUES (0, '1');
+INSERT INTO p_inner VALUES (0, '1');
+EXPLAIN (COSTS OFF) SELECT * FROM p_outer, p_inner where p_outer.a = p_inner.a AND p_outer.b = p_inner.b;
+                                             QUERY PLAN
+----------------------------------------------------------------------------------------------------
+ YB Batched Nested Loop Join
+   Join Filter: ((p_outer.a = p_inner.a) AND (p_outer.b = p_inner.b))
+   ->  Seq Scan on p_outer
+   ->  Index Scan using p_inner_pkey on p_inner
+         Index Cond: (ROW(b, a) = ANY (ARRAY[ROW(p_outer.b, p_outer.a), ROW($1, $4), ROW($2, $5)]))
+(5 rows)
+
+SELECT * FROM p_outer, p_inner where p_outer.a = p_inner.a AND p_outer.b = p_inner.b;
+ a | b | a | b
+---+---+---+---
+ 0 | 1 | 0 | 1
+(1 row)
+
+DROP TABLE p_inner;
+DROP TABLE p_outer;
 create table ss1(a int);
 create table ss2(a int);
 create table ss3(a int, b int, primary key(a asc, b asc));

src/postgres/src/test/regress/sql/yb_join_batching.sql
@@ -515,6 +515,15 @@ a = int4table.a;
 drop table oidtable;
 drop table int4table;
 
+CREATE TABLE p_inner(a int, b text, PRIMARY KEY(b asc, a asc));
+CREATE TABLE p_outer(a bigint, b text);
+INSERT INTO p_outer VALUES (0, '1');
+INSERT INTO p_inner VALUES (0, '1');
+EXPLAIN (COSTS OFF) SELECT * FROM p_outer, p_inner where p_outer.a = p_inner.a AND p_outer.b = p_inner.b;
+SELECT * FROM p_outer, p_inner where p_outer.a = p_inner.a AND p_outer.b = p_inner.b;
+DROP TABLE p_inner;
+DROP TABLE p_outer;
+
 create table ss1(a int);
 create table ss2(a int);
 create table ss3(a int, b int, primary key(a asc, b asc));