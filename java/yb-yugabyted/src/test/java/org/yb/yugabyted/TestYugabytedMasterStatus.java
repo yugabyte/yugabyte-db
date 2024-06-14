acf11d84104078c15e23800b6a4514e69853e84b
Nikhil Chandrappa <nikhil@yugabyte.com>
2024-06-14 16:09:21 +0000

    [#18233] Initial commit for yugabyted unit test framework.
    Summary: Initial commit for yugabyted unit test framework. Adding support for MiniYugabytedCluster.
    
    Test Plan: ./yb_build.sh --java-test 'org.yb.yugabyted.TestYugabytedMasterStatus#testSingleLeader'
    
    Reviewers: sgarg-yb, shikhar.sahay
    
    Reviewed By: sgarg-yb, shikhar.sahay
    
    Subscribers: yugabyted-dev
    
    Differential Revision: https://phorge.dev.yugabyte.com/D35863

---
+++ java/yb-yugabyted/src/test/java/org/yb/yugabyted/TestYugabytedMasterStatus.java
@@ -0,0 +1,91 @@
+package org.yb.yugabyted;
+
+import static org.yb.AssertionWrappers.assertEquals;
+import static org.yb.AssertionWrappers.assertNotEquals;
+
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Map;
+import java.util.Scanner;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.yb.YBTestRunner;
+import org.yb.client.TestMasterStatus;
+import org.yb.minicluster.MiniYBDaemon;
+
+import com.google.common.net.HostAndPort;
+
+@RunWith(value = YBTestRunner.class)
+public class TestYugabytedMasterStatus extends BaseYbdClientTest {
+
+    private static final Logger LOG = LoggerFactory.getLogger(TestYugabytedMasterStatus.class);
+
+    private String isMasterLeader(String host, int port) throws Exception {
+        try {
+            // call the <web>/api/v1/is-leader JSON endpoint
+            String raft_role = "LEADER";
+            String urlstr = String.format("http://%s:%d/api/v1/is-leader",
+                    host, port);
+
+            URL url = new URL(urlstr);
+            HttpURLConnection huc = (HttpURLConnection) url.openConnection();
+            huc.setRequestMethod("HEAD");
+            huc.connect();
+            int statusCode = huc.getResponseCode();
+
+            if (statusCode != 200) {
+                raft_role = "FOLLOWER";
+            }
+
+            LOG.info("Master RAFT Role: " + host + ":" + Integer.toString(port) +
+                    " = " + raft_role);
+            return raft_role;
+        } catch (MalformedURLException e) {
+            throw new InternalError(e.getMessage());
+        }
+    }
+
+    private String allMasterStatuses(String host, int port) throws Exception {
+        try {
+            // call the <web>/api/v1/masters JSON endpoint
+            String urlstr = String.format("http://%s:%d/api/v1/masters",
+                    host, port);
+
+            URL url = new URL(urlstr);
+            Scanner scanner = new Scanner(url.openConnection().getInputStream());
+            scanner.useDelimiter("\\A");
+            String output = scanner.hasNext() ? scanner.next() : "";
+            scanner.close();
+
+            LOG.info("Master Statuses: \n" + host + ":" + Integer.toString(port) +
+                    " = " + output);
+            return output;
+        } catch (MalformedURLException e) {
+            throw new InternalError(e.getMessage());
+        }
+    }
+
+    @Test(timeout = 60000)
+    public void testSingleLeader() throws Exception {
+        Map<HostAndPort, MiniYBDaemon> masters = miniYugabytedCluster.getYugabytedNodes();
+        HostAndPort leaderMasterHostAndPort = syncClient.getLeaderMasterHostAndPort();
+
+        LOG.info("Leader host and port: " + leaderMasterHostAndPort.toString());
+        for (HostAndPort host_and_port : masters.keySet()) {
+            String RAFT_role = isMasterLeader(host_and_port.getHost(),
+                    masters.get(host_and_port).getWebPort());
+            if (RAFT_role == "LEADER") {
+                assertEquals(host_and_port.toString(), leaderMasterHostAndPort.toString());
+            }
+
+            if (RAFT_role == "FOLLOWER") {
+                assertNotEquals(host_and_port.toString(), leaderMasterHostAndPort.toString());
+            }
+        }
+    }
+
+}
