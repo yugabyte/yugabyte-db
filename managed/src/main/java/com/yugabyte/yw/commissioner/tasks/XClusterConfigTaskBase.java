69bd6dab80c292f60b867a3803254d21cdab9bb3
Hamidreza Zare <hzare@yugabyte.com>
2024-06-17 16:02:29 -0400

    [PLAT-13756][xCluster] Add explanation to the need_boostrap response
    Summary:
    This diff adds explanation to the need_bootstrap API and takes into account that bootstrapping for tables in bidirectional replication replication cannot be bootstrapped even if they have existing data.
    
    To get the explanation you need to pass the query parameter `includeDetails=true` which is false by default and it has the old behavior.
    
    It also sets the tableType for the xCluster config object if it is UNKOWN.
    
    API call example:
    
    ```
    curl --location 'http://192.168.56.102:9000/api/v1/customers/f33e3c9b-75ab-4c30-80ad-cba85646ea39/universes/b959e45c-7655-4455-8144-9b20dd469269/need_bootstrap?includeDetails=true' \
    --header 'Content-Type: application/json' \
    --header 'X-AUTH-YW-API-TOKEN: b2ff10d0-63da-48ee-8008-f36326d5d17e' \
    --data '{
       "tables":["00004000000030008000000000004014", "0000400000003000800000000000400f", "0000400000003000800000000000400a", "00004000000030008000000000004005", "00004000000030008000000000004000"],
       "targetUniverseUUID": "c7d7aa58-45cf-4139-9f05-b170fa24c23a"
    }'
    ```
    Response:
    
    ```
    {
        "00004000000030008000000000004005": {
            "description": "The table has data and the existing data must be replicated through bootstrapping. However, bootstrapping will be skipped because the table or a sibling YSQL table is being replicated in the reverse direction. The data between the two universes might be inconsistent.",
            "reasons": [
                "TABLE_HAS_DATA",
                "BIDIRECTIONAL_REPLICATION"
            ],
            "bootstrapRequired": false
        },
        "0000400000003000800000000000400f": {
            "description": "Bootstrapping will be skipped because the table or a sibling YSQL table is being replicated in the reverse direction.",
            "reasons": [
                "BIDIRECTIONAL_REPLICATION"
            ],
            "bootstrapRequired": false
        },
        "00004000000030008000000000004014": {
            "description": "Bootstrapping will be skipped because the table or a sibling YSQL table is being replicated in the reverse direction.",
            "reasons": [
                "BIDIRECTIONAL_REPLICATION"
            ],
            "bootstrapRequired": false
        },
        "0000400000003000800000000000400a": {
            "description": "Bootstrapping will be skipped because the table or a sibling YSQL table is being replicated in the reverse direction.",
            "reasons": [
                "BIDIRECTIONAL_REPLICATION"
            ],
            "bootstrapRequired": false
        },
        "00004000000030008000000000004000": {
            "description": "Bootstrapping will be skipped because the table or a sibling YSQL table is being replicated in the reverse direction.",
            "reasons": [
                "BIDIRECTIONAL_REPLICATION"
            ],
            "bootstrapRequired": false
        }
    }
    ```
    
    Test Plan: Tested the API for all the combinations of the reasons and made sure the proper description is returned.
    
    Reviewers: #yba-api-review!, vbansal, cwang, jmak, sanketh
    
    Reviewed By: cwang, jmak
    
    Subscribers: yugaware
    
    Differential Revision: https://phorge.dev.yugabyte.com/D35882

--- managed/src/main/java/com/yugabyte/yw/commissioner/tasks/XClusterConfigTaskBase.java
+++ managed/src/main/java/com/yugabyte/yw/commissioner/tasks/XClusterConfigTaskBase.java
@@ -1319,6 +1319,22 @@ public abstract class XClusterConfigTaskBase extends UniverseDefinitionTaskBase 
   }
 
   public static Map<String, List<String>> getMainTableIndexTablesMap(
+      Collection<MasterDdlOuterClass.ListTablesResponsePB.TableInfo> tableInfoList) {
+    Map<String, List<String>> mainTableIndexTablesMap = new HashMap<>();
+    tableInfoList.forEach(
+        tableInfo -> {
+          if (TableInfoUtil.isIndexTable(tableInfo)) {
+            String mainTableId = tableInfo.getIndexedTableId().replace("-", "");
+            mainTableIndexTablesMap
+                .computeIfAbsent(mainTableId, k -> new ArrayList<>())
+                .add(getTableId(tableInfo));
+          }
+        });
+    log.debug("mainTableIndexTablesMap is {}", mainTableIndexTablesMap);
+    return mainTableIndexTablesMap;
+  }
+
+  public static Map<String, List<String>> getMainTableIndexTablesMap(
       YBClientService ybService, Universe universe, Set<String> mainTableUuidList) {
     Map<String, GetTableSchemaResponse> tableSchemaMap =
         getTableSchemas(ybService, universe, mainTableUuidList);
@@ -1511,6 +1527,13 @@ public abstract class XClusterConfigTaskBase extends UniverseDefinitionTaskBase 
           }
         });
     return namespaces;
+  }
+
+  public static Map<String, MasterDdlOuterClass.ListTablesResponsePB.TableInfo>
+      getTableIdToTableInfoMap(
+          Collection<MasterDdlOuterClass.ListTablesResponsePB.TableInfo> tablesInfoList) {
+    return tablesInfoList.stream()
+        .collect(Collectors.toMap(XClusterConfigTaskBase::getTableId, Function.identity()));
   }
 
   public static Map<String, List<MasterDdlOuterClass.ListTablesResponsePB.TableInfo>>
