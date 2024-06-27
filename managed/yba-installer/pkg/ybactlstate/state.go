e5399d967ec7fe28a99e5cad21e4ee86b0ddc5fe
Daniel Shubin <dshubin@yugabyte.com>
2024-06-27 00:30:08 +0000

    [PLAT-14511] Improved yba installer migrations to allow some versions to not exist
    Summary:
    This is very useful when backporting some migrations but not others. It allows
    yba installer to apply schemas even when a version is "missing", but allow a
    future upgrade to apply that missing schema
    
    Test Plan:
    several unit tests to validate functionality
    tested an install and an upgrade to validate the migrations
    work as expected
    
    Reviewers: muthu, sanketh
    
    Reviewed By: muthu
    
    Subscribers: yugaware
    
    Differential Revision: https://phorge.dev.yugabyte.com/D36217

--- managed/yba-installer/pkg/ybactlstate/state.go
+++ managed/yba-installer/pkg/ybactlstate/state.go
@@ -63,6 +63,7 @@ func New() *State {
 		_internalFields: internalFields{
 			ChangeID:      0,
 			SchemaVersion: getSchemaVersion(),
+			RunSchemas:    allSchemaSlice(),
 		},
 	}
 }
@@ -72,8 +73,9 @@ type YbdbState struct {
 }
 
 type internalFields struct {
-	ChangeID      int `json:"change_id"`
-	SchemaVersion int `json:"schema"`
+	ChangeID      int   `json:"change_id"`
+	SchemaVersion int   `json:"schema"` // Deprecated
+	RunSchemas    []int `json:"run_schemas"`
 }
 
 // TransitionStatus will move the state from CurrentStatus to next, after first Validating the
@@ -91,4 +93,14 @@ func (s *State) TransitionStatus(next status) error {
 			next.String(), err)
 	}
 	return nil
+}
+
+func allSchemaSlice() []int {
+	ret := make([]int, getSchemaVersion())
+	start := 1
+	for i := range getSchemaVersion() {
+		ret[i] = start
+		start++
+	}
+	return ret
 }
