28739e54d63993680803a11ae36bc6fa82a9e3f8
Daniel Shubin <dshubin@yugabyte.com>
2024-06-28 18:40:07 +0000

    [BACKPORT 2.20][PLAT-14511] Improved yba installer migrations to allow some versions to not exist
    Summary:
    Original commit: e5399d967ec7fe28a99e5cad21e4ee86b0ddc5fe / D36217
    This is very useful when backporting some migrations but not others. It allows
    yba installer to apply schemas even when a version is "missing", but allow a
    future upgrade to apply that missing schema
    
    Test Plan:
    several unit tests to validate functionality
    tested an install and an upgrade to validate the migrations
    work as expected
    
    Reviewers: muthu, sanketh
    
    Reviewed By: muthu
    
    Subscribers: yugaware
    
    Tags: #jenkins-ready
    
    Differential Revision: https://phorge.dev.yugabyte.com/D36251

--- managed/yba-installer/pkg/ybactlstate/migration.go
+++ managed/yba-installer/pkg/ybactlstate/migration.go
@@ -3,6 +3,7 @@ package ybactlstate
 import (
 	"bytes"
 	"fmt"
+	"slices"
 	"strconv"
 
 	"github.com/spf13/viper"
@@ -19,17 +20,48 @@ const ymlTypeFixMV = 4
 const promOomConfgMV = 5
 const promTLSCipherSuites = 6
 
+// Please do not use this in ybactlstate package, only use getSchemaVersion()
+var schemaVersionCache = -1
+
 func handleMigration(state *State) error {
-	for state._internalFields.SchemaVersion < schemaVersion {
-		nextSchema := state._internalFields.SchemaVersion + 1
+	if err := updateSchemaTracking(state); err != nil {
+		return err
+	}
+	nextSchema := 0
+	endSchema := getSchemaVersion()
+	for nextSchema < endSchema {
+		nextSchema++
+		if slices.Contains(state._internalFields.RunSchemas, nextSchema) {
+			continue
+		}
 		migrate := getMigrationHandler(nextSchema)
+		if migrate == nil {
+			log.Debug("skipping migration " + strconv.Itoa(nextSchema) + " as it is not defined")
+			continue
+		}
 		if err := migrate(state); err != nil {
 			return err
 		}
-		state._internalFields.SchemaVersion = nextSchema
+		state._internalFields.RunSchemas = append(state._internalFields.RunSchemas, nextSchema)
 	}
 	// StoreState in order to persist migration SchemaVersion
 	return StoreState(state)
+}
+
+// Update how we track the schema version. Previously, we tracked the max version with the schema
+// Version variable. Now, we want to explicitly track only the executed schemas. We will assume if
+// this transfer needs to be run, all migrations below the tracked schema version have been run, and
+// mark them as run.
+func updateSchemaTracking(state *State) error {
+	if state._internalFields.RunSchemas != nil {
+		return nil
+	}
+	// allSchemaSlice returns the list for the current max schema, but we need specifically those
+	// that have already been run - tracked previously by the state SchemaVersion
+	state._internalFields.RunSchemas = allSchemaSlice()[:state._internalFields.SchemaVersion]
+	log.DebugLF(fmt.Sprintf("updating schema tracking with run schemas of %v",
+		state._internalFields.RunSchemas))
+	return nil
 }
 
 type migrator func(state *State) error
@@ -72,8 +104,8 @@ func migratePostgresUser(state *State) error {
 		viper.ReadConfig(bytes.NewBufferString(config.ReferenceYbaCtlConfig))
 		if err := common.SetYamlValue(common.InputFile(), "postgres.install.username",
 			viper.GetString("postgres.install.username")); err != nil {
-				return fmt.Errorf("Error migrating postgres user: %s", err.Error())
-			}
+			return fmt.Errorf("Error migrating postgres user: %s", err.Error())
+		}
 	}
 	common.InitViper()
 	return nil
@@ -81,27 +113,27 @@ func migratePostgresUser(state *State) error {
 
 func migrateYmlTypes(state *State) error {
 	log.Info("Entering migrateYmlTypes")
-	typeMap := map[string]string {
-		"platform.port": 													"int",
-		"platform.hsts_enabled": 									"bool",
-		"platform.useOauth": 											"bool",
-		"platform.restartSeconds": 								"int",
-		"platform.proxy.enable": 									"bool",
-		"platform.proxy.java_http_proxy_port": 		"int",
-		"platform.proxy.java_https_proxy_port": 	"int",
-		"postgres.install.enabled": 							"bool",
-		"postgres.install.port": 									"int",
-		"postgres.install.ldap_enabled": 					"bool",
-		"postgres.install.ldap_port": 						"int",
-		"postgres.install.secure_ldap": 					"bool",
-		"postgres.useExisting.enabled": 					"bool",
-		"postgres.useExisting.port": 							"int",
-		"prometheus.port": 												"int",
-		"prometheus.restartSeconds": 							"int",
-		"prometheus.maxConcurrency": 							"int",
-		"prometheus.maxSamples": 									"int",
-		"prometheus.enableHttps": 								"bool",
-		"prometheus.enableAuth": 									"bool",
+	typeMap := map[string]string{
+		"platform.port":                        "int",
+		"platform.hsts_enabled":                "bool",
+		"platform.useOauth":                    "bool",
+		"platform.restartSeconds":              "int",
+		"platform.proxy.enable":                "bool",
+		"platform.proxy.java_http_proxy_port":  "int",
+		"platform.proxy.java_https_proxy_port": "int",
+		"postgres.install.enabled":             "bool",
+		"postgres.install.port":                "int",
+		"postgres.install.ldap_enabled":        "bool",
+		"postgres.install.ldap_port":           "int",
+		"postgres.install.secure_ldap":         "bool",
+		"postgres.useExisting.enabled":         "bool",
+		"postgres.useExisting.port":            "int",
+		"prometheus.port":                      "int",
+		"prometheus.restartSeconds":            "int",
+		"prometheus.maxConcurrency":            "int",
+		"prometheus.maxSamples":                "int",
+		"prometheus.enableHttps":               "bool",
+		"prometheus.enableAuth":                "bool",
 	}
 
 	for key, typeStr := range typeMap {
@@ -128,7 +160,7 @@ func migrateYmlTypes(state *State) error {
 				log.Warn(fmt.Sprintf("Could not convert %s: %s to bool.", key, value))
 				return err
 			}
-			err  = common.SetYamlValue(common.InputFile(), key, b)
+			err = common.SetYamlValue(common.InputFile(), key, b)
 			if err != nil {
 				log.Warn("Error setting yaml value " + key + value)
 				return err
@@ -172,17 +204,28 @@ func migratePrometheusTLSCipherSuites(state *State) error {
 // TODO: Also need to remember to update schemaVersion when adding migration! (automate testing?)
 var migrations map[int]migrator = map[int]migrator{
 	defaultMigratorValue: defaultMigrate,
-	promConfigMV: migratePrometheus,
-	postgresUserMV: migratePostgresUser,
-	ymlTypeFixMV: migrateYmlTypes,
-	promOomConfgMV: migratePrometheusOOMConfig,
-	promTLSCipherSuites: migratePrometheusTLSCipherSuites,
+	promConfigMV:         migratePrometheus,
+	postgresUserMV:       migratePostgresUser,
+	ymlTypeFixMV:         migrateYmlTypes,
+	promOomConfgMV:       migratePrometheusOOMConfig,
+	promTLSCipherSuites:  migratePrometheusTLSCipherSuites,
 }
 
 func getMigrationHandler(toSchema int) migrator {
 	m, ok := migrations[toSchema]
 	if !ok {
-		m = migrations[defaultMigratorValue]
+		return nil
 	}
 	return m
+}
+
+func getSchemaVersion() int {
+	if schemaVersionCache == -1 {
+		for k := range migrations {
+			if k > schemaVersionCache {
+				schemaVersionCache = k
+			}
+		}
+	}
+	return schemaVersionCache
 }
