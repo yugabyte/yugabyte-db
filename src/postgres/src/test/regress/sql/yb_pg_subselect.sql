e6c6bcdc5ae217be5139e1682f130cf2493dffff
Karthik Ramanathan <kramanathan@yugabyte.com>
2024-07-12 09:16:28 -0700

    [BACKPORT pg15-cherrypicks][#22967] YSQL: Import 'Repair more failures with SubPlans in multi-row VALUES lists.'
    Summary:
    Original commit: 471c7c4 / D36186
    Import upstream postgres commit d8e877b869cb5dc33a8d96218115fc12e66b73d4 from REL_11_STABLE.
    (postgres master commit: 41c6f9db25b5e3a8bb8afbb7d6715cff541fd41e)
    This is needed to fix a crash in releasing memory associated with TupleTableSlots that are used in SubPlans in the context of a Values Scan.
    
    **Conflict Resolution for PG15 cherrypick**
    As this commit is old, it is already incorporated in PG 15.
     - src/test/regress/yb_pg_dml_serial_schedule:
        - <location-NA>: The yb specific tests have moved from `yb_pg_dml_serial_schedule` to `yb_dml_serial_schedule`, so the new `yb_dml` test was also added to the latter.
    
    **Changes from upstream commit**
    - Imported regress test validating fix from `subselect.*` to `yb_pg_subselect.*`. Query ID was explicitly hidden in `yb_pg_subselect.*`.
    - Add a DML regress test covering bug (#22967) to new file `yb_dml.*`
    
    **Upstream commit message**
    ```
    Commit 9b63c13f0 turns out to have been fundamentally misguided:
    the parent node's subPlan list is by no means the only way in which
    a child SubPlan node can be hooked into the outer execution state.
    As shown in bug #16213 from Matt Jibson, we can also get short-lived
    tuple table slots added to the outer es_tupleTable list.  At this point
    I have little faith that there aren't other possible connections as
    well; the long time it took to notice this problem shows that this
    isn't a heavily-exercised situation.
    
    Therefore, revert that fix, returning to the coding that passed a
    NULL parent plan pointer down to the transiently-built subexpressions.
    That gives us a pretty good guarantee that they won't hook into the
    outer executor state in any way.  But then we need some other solution
    to make SubPlans work.  Adopt the solution speculated about in the
    previous commit's log message: do expression initialization at plan
    startup for just those VALUES rows containing SubPlans, abandoning the
    goal of reclaiming memory intra-query for those rows.  In practice it
    seems unlikely that queries containing a vast number of VALUES rows
    would be using SubPlans in them, so this should not give up much.
    
    (BTW, this test case also refutes my claim in connection with the prior
    commit that the issue only arises with use of LATERAL.  That was just
    wrong: some variants of SubLink always produce SubPlans.)
    
    As with previous patch, back-patch to all supported branches.
    
    Discussion: https://postgr.es/m/16213-871ac3bc208ecf23@postgresql.org
    ```
    Jira: DB-11885
    
    Test Plan:
    ```
    ./yb_build.sh --java-test 'org.yb.pgsql.TestPgRegressPgSelect'
    ./yb_build.sh --java-test 'org.yb.pgsql.TestPgRegressDml'
    ```
    
    Reviewers: jason, tfoucher
    
    Reviewed By: jason
    
    Subscribers: yql, smishra
    
    Tags: #jenkins-ready
    
    Differential Revision: https://phorge.dev.yugabyte.com/D36398

---
+++ src/postgres/src/test/regress/sql/yb_pg_subselect.sql
@@ -0,0 +1,19 @@
+--
+-- SUBSELECT
+--
+
+SET compute_query_id to regress; -- YB: hide query id for EXPLAIN VERBOSE
+
+-- another variant of that (bug #16213)
+explain (verbose, costs off)
+select * from
+(values
+  (3 not in (select * from (values (1), (2)) ss1)),
+  (false)
+) ss;
+
+select * from
+(values
+  (3 not in (select * from (values (1), (2)) ss1)),
+  (false)
+) ss;
