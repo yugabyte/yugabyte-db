0c8e37895c77fec590eaae8d7202890d1ace2138
Nikhil Chandrappa <nikhil@yugabyte.com>
2024-07-16 00:20:26 +0000

    [#23216]yugabyted: Adding required models for source db details and target recommendation details sub pages.
    Summary:
    Adding required openapi models for source db details and target recommendation details sub pages of Assessment page.
    Jira: DB-12159
    
    Test Plan: Manual Tests
    
    Reviewers: djiang
    
    Reviewed By: djiang
    
    Subscribers: yugabyted-dev
    
    Differential Revision: https://phorge.dev.yugabyte.com/D36597

--- yugabyted-ui/apiserver/cmd/server/models/model_recommended_refactoring_graph.go
+++ yugabyted-ui/apiserver/cmd/server/models/model_recommended_refactoring_graph.go
@@ -3,13 +3,5 @@ package models
 // RecommendedRefactoringGraph - Refectoring recommendations for migrating SQL objects
 type RecommendedRefactoringGraph struct {
 
-    SqlType RefactoringCount `json:"sql_type"`
-
-    Table RefactoringCount `json:"table"`
-
-    View RefactoringCount `json:"view"`
-
-    Function RefactoringCount `json:"function"`
-
-    Triggers RefactoringCount `json:"triggers"`
+    RefactorDetails []RefactoringCount `json:"refactor_details"`
 }
