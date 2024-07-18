85cb214706a42701991cb187650c7be5855b78f2
Spikatrix <12792882+Spikatrix@users.noreply.github.com>
2024-07-18 06:31:39 +0000

    Assesment Recommendation Details

--- yugabyted-ui/ui/src/features/clusters/details/migration/steps/assessment/AssessmentRecommendationSidePanel.tsx
+++ yugabyted-ui/ui/src/features/clusters/details/migration/steps/assessment/AssessmentRecommendationSidePanel.tsx
@@ -1,9 +1,23 @@
 import React, { FC, useMemo } from "react";
-import { Box, Divider, Grid, MenuItem, Paper, Typography, makeStyles } from "@material-ui/core";
+import {
+  Box,
+  Divider,
+  Grid,
+  LinearProgress,
+  MenuItem,
+  Paper,
+  Typography,
+  makeStyles,
+} from "@material-ui/core";
 import { useTranslation } from "react-i18next";
 import { YBInput, YBModal, YBSelect, YBTable } from "@app/components";
 import SearchIcon from "@app/assets/search.svg";
 import type { Migration } from "../../MigrationOverview";
+import {
+  AssessmentTargetRecommendationObject,
+  useGetAssessmentTargetRecommendationInfoQuery,
+} from "@app/api/src";
+import { convertBytesToGB } from "@app/helpers";
 
 const useStyles = makeStyles((theme) => ({
   heading: {
@@ -50,56 +64,42 @@ interface MigrationRecommendationSidePanel {
 export const MigrationRecommendationSidePanel: FC<MigrationRecommendationSidePanel> = ({
   open,
   onClose,
-  /* migration, */
+  migration,
 }) => {
   const classes = useStyles();
   const { t } = useTranslation();
 
+  const { data: targetRecommendationAPI, isFetching: isFetchingtargetRecommendationData } =
+    useGetAssessmentTargetRecommendationInfoQuery({
+      uuid: migration.migration_uuid || "migration_uuid_not_found",
+    });
+  const targetRecommendationData = targetRecommendationAPI as
+    | AssessmentTargetRecommendationObject
+    | undefined;
+
   const recommendationData = useMemo(
-    () => [
-      {
-        tableName: "table-01",
-        diskSize: "16.2 GB",
-        schemaRecommendation: "Colocated",
-      },
-      {
-        tableName: "table-02",
-        diskSize: "12.2 GB",
-        schemaRecommendation: "Sharded",
-      },
-      {
-        tableName: "table-03",
-        diskSize: "5.6 GB",
-        schemaRecommendation: "Colocated",
-      },
-      {
-        tableName: "table-04",
-        diskSize: "3.1 GB",
-        schemaRecommendation: "Sharded",
-      },
-    ],
-    []
+    () =>
+      targetRecommendationData?.recommendation_details?.map((obj) => ({
+        tableName: obj.table_name || "",
+        diskSize: obj.disk_size || 0,
+        schemaRecommendation: obj.schema_recommendation || "",
+      })) ?? [],
+    [targetRecommendationData]
   );
 
   const recommendationObjects = useMemo(
     () => ({
       colocated: {
-        totalCount: recommendationData.filter((obj) => obj.schemaRecommendation === "Colocated")
-          .length,
-        totalSize:
-          recommendationData
-            .filter((obj) => obj.schemaRecommendation === "Colocated")
-            .reduce((acc, obj) => acc + parseFloat(obj.diskSize), 0)
-            .toFixed(2) + " GB",
+        totalCount: targetRecommendationData?.num_of_colocated_tables || 0,
+        totalSize: `${convertBytesToGB(
+          targetRecommendationData?.total_size_colocated_tables || 0
+        )} GB`,
       },
       sharded: {
-        totalCount: recommendationData.filter((obj) => obj.schemaRecommendation === "Sharded")
-          .length,
-        totalSize:
-          recommendationData
-            .filter((obj) => obj.schemaRecommendation === "Sharded")
-            .reduce((acc, obj) => acc + parseFloat(obj.diskSize), 0)
-            .toFixed(2) + " GB",
+        totalCount: targetRecommendationData?.num_of_sharded_table,
+        totalSize: `${convertBytesToGB(
+          targetRecommendationData?.total_size_sharded_tables || 0
+        )} GB`,
       },
     }),
     [recommendationData]
@@ -107,7 +107,9 @@ export const MigrationRecommendationSidePanel: FC<MigrationRecommendationSidePan
 
   const types = useMemo(() => {
     const typeSet = new Set<string>();
-    recommendationData.forEach((obj) => typeSet.add(obj.schemaRecommendation));
+    recommendationData.forEach((obj) =>
+      obj.schemaRecommendation ? typeSet.add(obj.schemaRecommendation) : null
+    );
     return Array.from(typeSet);
   }, [recommendationData]);
 
@@ -138,6 +140,7 @@ export const MigrationRecommendationSidePanel: FC<MigrationRecommendationSidePan
       options: {
         setCellHeaderProps: () => ({ style: { padding: "8px 16px" } }),
         setCellProps: () => ({ style: { padding: "8px 16px" } }),
+        customBodyRender: (size: number) => `${convertBytesToGB(size)} GB`,
       },
     },
     {
@@ -160,108 +163,124 @@ export const MigrationRecommendationSidePanel: FC<MigrationRecommendationSidePan
       cancelLabel={t("common.close")}
       isSidePanel
     >
-      <Box my={2}>
-        <Paper>
-          <Box p={2} className={classes.grayBg} display="flex" gridGap={20}>
-            <Grid container spacing={2}>
-              <Grid item xs={12}>
-                <Typography variant="h5">
-                  {t("clusterDetail.voyager.planAndAssess.recommendation.schema.colocatedTables")}
-                </Typography>
-              </Grid>
-              <Grid item xs={6}>
-                <Typography variant="subtitle2" className={classes.label}>
-                  {t("clusterDetail.voyager.planAndAssess.recommendation.schema.noOfTables")}
-                </Typography>
-                <Typography variant="body2" className={classes.value}>
-                  {recommendationObjects.colocated.totalCount}
-                </Typography>
-              </Grid>
-              <Grid item xs={6}>
-                <Typography variant="subtitle2" className={classes.label}>
-                  {t("clusterDetail.voyager.planAndAssess.recommendation.schema.totalSize")}
-                </Typography>
-                <Typography variant="body2" className={classes.value}>
-                  {recommendationObjects.colocated.totalSize}
-                </Typography>
-              </Grid>
-            </Grid>
-            <Divider orientation="vertical" flexItem />
-            <Grid container spacing={2}>
-              <Grid item xs={12}>
-                <Typography variant="h5">
-                  {t("clusterDetail.voyager.planAndAssess.recommendation.schema.shardedTables")}
-                </Typography>
-              </Grid>
-              <Grid item xs={6}>
-                <Typography variant="subtitle2" className={classes.label}>
-                  {t("clusterDetail.voyager.planAndAssess.recommendation.schema.noOfTables")}
-                </Typography>
-                <Typography variant="body2" className={classes.value}>
-                  {recommendationObjects.sharded.totalCount}
-                </Typography>
-              </Grid>
-              <Grid item xs={6}>
-                <Typography variant="subtitle2" className={classes.label}>
-                  {t("clusterDetail.voyager.planAndAssess.recommendation.schema.totalSize")}
-                </Typography>
-                <Typography variant="body2" className={classes.value}>
-                  {recommendationObjects.sharded.totalSize}
-                </Typography>
-              </Grid>
-            </Grid>
+      {isFetchingtargetRecommendationData && (
+        <Box my={4}>
+          <Box textAlign="center" mt={2.5}>
+            <LinearProgress />
           </Box>
-        </Paper>
-      </Box>
-
-      <Box display="flex" alignItems="center" gridGap={10} my={2}>
-        <Box flex={1}>
-          <Typography variant="body1" className={classes.label}>
-            {t("clusterDetail.voyager.planAndAssess.recommendation.schema.schemaRecommendation")}
-          </Typography>
-          <YBSelect
-            className={classes.fullWidth}
-            value={typeFilter}
-            onChange={(e) => setTypeFilter(e.target.value)}
-          >
-            <MenuItem value="">All</MenuItem>
-            <Divider className={classes.divider} />
-            {types.map((type) => {
-              return (
-                <MenuItem key={type} value={type}>
-                  {type}
-                </MenuItem>
-              );
-            })}
-          </YBSelect>
         </Box>
-        <Box flex={2}>
-          <Typography variant="body1" className={classes.label}>
-            {t("clusterDetail.voyager.planAndAssess.recommendation.schema.search")}
-          </Typography>
-          <YBInput
-            className={classes.fullWidth}
-            placeholder={t(
-              "clusterDetail.voyager.planAndAssess.recommendation.schema.searchPlaceholder"
-            )}
-            InputProps={{
-              startAdornment: <SearchIcon />,
-            }}
-            onChange={(ev) => setSearch(ev.target.value)}
-            value={search}
-          />
-        </Box>
-      </Box>
+      )}
 
-      <Box>
-        <YBTable
-          data={filteredSourceObjects}
-          columns={sourceObjectsColumns}
-          options={{
-            pagination: true,
-          }}
-        />
-      </Box>
+      {!isFetchingtargetRecommendationData && (
+        <>
+          <Box my={2}>
+            <Paper>
+              <Box p={2} className={classes.grayBg} display="flex" gridGap={20}>
+                <Grid container spacing={2}>
+                  <Grid item xs={12}>
+                    <Typography variant="h5">
+                      {t(
+                        "clusterDetail.voyager.planAndAssess.recommendation.schema.colocatedTables"
+                      )}
+                    </Typography>
+                  </Grid>
+                  <Grid item xs={6}>
+                    <Typography variant="subtitle2" className={classes.label}>
+                      {t("clusterDetail.voyager.planAndAssess.recommendation.schema.noOfTables")}
+                    </Typography>
+                    <Typography variant="body2" className={classes.value}>
+                      {recommendationObjects.colocated.totalCount}
+                    </Typography>
+                  </Grid>
+                  <Grid item xs={6}>
+                    <Typography variant="subtitle2" className={classes.label}>
+                      {t("clusterDetail.voyager.planAndAssess.recommendation.schema.totalSize")}
+                    </Typography>
+                    <Typography variant="body2" className={classes.value}>
+                      {recommendationObjects.colocated.totalSize}
+                    </Typography>
+                  </Grid>
+                </Grid>
+                <Divider orientation="vertical" flexItem />
+                <Grid container spacing={2}>
+                  <Grid item xs={12}>
+                    <Typography variant="h5">
+                      {t("clusterDetail.voyager.planAndAssess.recommendation.schema.shardedTables")}
+                    </Typography>
+                  </Grid>
+                  <Grid item xs={6}>
+                    <Typography variant="subtitle2" className={classes.label}>
+                      {t("clusterDetail.voyager.planAndAssess.recommendation.schema.noOfTables")}
+                    </Typography>
+                    <Typography variant="body2" className={classes.value}>
+                      {recommendationObjects.sharded.totalCount}
+                    </Typography>
+                  </Grid>
+                  <Grid item xs={6}>
+                    <Typography variant="subtitle2" className={classes.label}>
+                      {t("clusterDetail.voyager.planAndAssess.recommendation.schema.totalSize")}
+                    </Typography>
+                    <Typography variant="body2" className={classes.value}>
+                      {recommendationObjects.sharded.totalSize}
+                    </Typography>
+                  </Grid>
+                </Grid>
+              </Box>
+            </Paper>
+          </Box>
+
+          <Box display="flex" alignItems="center" gridGap={10} my={2}>
+            <Box flex={1}>
+              <Typography variant="body1" className={classes.label}>
+                {t(
+                  "clusterDetail.voyager.planAndAssess.recommendation.schema.schemaRecommendation"
+                )}
+              </Typography>
+              <YBSelect
+                className={classes.fullWidth}
+                value={typeFilter}
+                onChange={(e) => setTypeFilter(e.target.value)}
+              >
+                <MenuItem value="">All</MenuItem>
+                <Divider className={classes.divider} />
+                {types.map((type) => {
+                  return (
+                    <MenuItem key={type} value={type}>
+                      {type}
+                    </MenuItem>
+                  );
+                })}
+              </YBSelect>
+            </Box>
+            <Box flex={2}>
+              <Typography variant="body1" className={classes.label}>
+                {t("clusterDetail.voyager.planAndAssess.recommendation.schema.search")}
+              </Typography>
+              <YBInput
+                className={classes.fullWidth}
+                placeholder={t(
+                  "clusterDetail.voyager.planAndAssess.recommendation.schema.searchPlaceholder"
+                )}
+                InputProps={{
+                  startAdornment: <SearchIcon />,
+                }}
+                onChange={(ev) => setSearch(ev.target.value)}
+                value={search}
+              />
+            </Box>
+          </Box>
+
+          <Box>
+            <YBTable
+              data={filteredSourceObjects}
+              columns={sourceObjectsColumns}
+              options={{
+                pagination: true,
+              }}
+            />
+          </Box>
+        </>
+      )}
     </YBModal>
   );
 };
