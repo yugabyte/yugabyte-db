4b2f29ecd69e4c864c59fe34c11cf03c4f6e555e
Jason Kim <git@jasonk.me>
2024-07-19 16:04:56 -0700

    [pg15] test: fix issues with pg15_tests
    Summary:
    - Remove pg15_tests/test_D31365.sh which should have been removed as
      part of 400152a1c9beab84a4728bb48e1a17ea7b93616a.
    - Remove TestPgRegressPgTransactions from passing_test.tsv since it is
      flaky since YB master 60ac2325a90d4a5ce7f964da435accc2f8d4825c on the
      query failing due to issue #22949.  So far, I have seen three more
      variants of output:
    
      -
    
          INSERT INTO koju VALUES (1); -- YB: TODO(#22949): fix output
          \set VERBOSITY default \\ -- YB
          INSERT INTO koju VALUES (1);
          ERROR:  Transaction metadata missing: ********-****-4***-****-************, looks like it was just aborted
          ROLLBACK;
    
      -
    
          INSERT INTO koju VALUES (1); -- YB: TODO(#22949): fix output
          \set VERBOSITY default \\ -- YB
          INSERT INTO koju VALUES (1);
          ERROR:  current transaction is expired or aborted (query layer retry isn't possible, READ COMMITTED transaction was aborted  and some data was already sent to the user)
          DETAIL:  Heartbeat: Transaction ********-****-4***-****-************ expired or aborted by a conflict: 40001: . Errors from tablet servers: [Operation expired (yb/tablet/transaction_coordinator.cc:1760): Heartbeat: Transaction ********-****-4***-****-************ expired or aborted by a conflict: 40001 (pgsql error 40001) (transaction error 1)]
          ROLLBACK;
    
      -
          INSERT INTO koju VALUES (1); -- YB: TODO(#22949): fix output
          \set VERBOSITY default \\ -- YB
          INSERT INTO koju VALUES (1);
          ERROR:  duplicate key value violates unique constraint "koju_a_key"
          ROLLBACK;
    
    Test Plan: Jenkins: skip
    
    Reviewers: tfoucher
    
    Reviewed By: tfoucher
    
    Subscribers: yql
    
    Differential Revision: https://phorge.dev.yugabyte.com/D36724

--- pg15_tests/test_D31365.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-source "${BASH_SOURCE[0]%/*}"/common.sh
-
-failing_java_test TestPgRegressMisc#testPgRegressMiscSerial4
-grep_in_java_test \
-  "failed tests: [yb_explicit_row_lock_batching, yb_explicit_row_lock_planning]" \
-  TestPgRegressMisc#testPgRegressMiscSerial4
