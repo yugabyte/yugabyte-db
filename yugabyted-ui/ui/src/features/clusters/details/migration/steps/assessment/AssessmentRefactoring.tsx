45dee021656172f7be68087ce6dc79f55d6e8838
Spikatrix <12792882+Spikatrix@users.noreply.github.com>
2024-08-01 05:57:48 +0000

    Hide refactoring section elements if they don't have data

--- yugabyted-ui/ui/src/features/clusters/details/migration/steps/assessment/AssessmentRefactoring.tsx
+++ yugabyted-ui/ui/src/features/clusters/details/migration/steps/assessment/AssessmentRefactoring.tsx
@@ -83,6 +83,15 @@ export const MigrationAssessmentRefactoring: FC<MigrationAssessmentRefactoringPr
   const barSize = 22;
   const graphHeight = graphData.length * 60 + barCategoryGap + barSize;
 
+  if (
+    !graphData.length &&
+    !unsupportedDataTypes?.length &&
+    !unsupportedFeatures?.length &&
+    !unsupportedFunctions?.length
+  ) {
+    return null;
+  }
+
   return (
     <Paper>
       <Box px={2} py={3}>
@@ -97,120 +106,140 @@ export const MigrationAssessmentRefactoring: FC<MigrationAssessmentRefactoringPr
           </Typography>
         </Box>
 
-        <Box my={4}>
-          <ResponsiveContainer width="100%" height={graphHeight}>
-            <BarChart
-              data={graphData}
-              layout="vertical"
-              margin={{
-                right: 30,
-                left: 50,
-              }}
-              barCategoryGap={barCategoryGap}
-              barSize={barSize}
-            >
-              <CartesianGrid horizontal={false} strokeDasharray="3 3" />
-              <XAxis type="number" />
-              <YAxis type="category" dataKey="objectType" textAnchor="start" dx={-90} />
-              <Tooltip content={<CustomTooltip />} />
-              <Bar
-                dataKey="automaticDDLImport"
-                fill="#2FB3FF"
-                stackId="stack"
-                isAnimationActive={false}
-              >
-                <LabelList
-                  dataKey="automaticDDLImport"
-                  position="insideRight"
-                  style={{ fill: "black" }}
-                  {...{
-                    formatter: (value: number) => value || null,
-                  }}
-                />
-              </Bar>
-              <Bar
-                dataKey="manualRefactoring"
-                fill="#FFA400"
-                stackId="stack"
-                isAnimationActive={false}
-              >
-                <LabelList
-                  dataKey="manualRefactoring"
-                  position="insideRight"
-                  style={{ fill: "black" }}
-                  {...{
-                    formatter: (value: number) => value || null,
-                  }}
-                />
-              </Bar>
-              <Legend
-                align="left"
-                content={({ payload }) => {
-                  if (!payload) {
-                    return null;
-                  }
-
-                  const formatter = (value: string) =>
-                    value
-                      .split(/(?=[A-Z][a-z])|(?<=[a-z])(?=[A-Z])/)
-                      .map((part) => part.charAt(0).toUpperCase() + part.slice(1))
-                      .join(" ");
-
-                  return (
-                    <ul
-                      style={{
-                        listStyleType: "none",
-                        display: "flex",
-                        gap: "20px",
-                        paddingLeft: "70px",
-                      }}
-                    >
-                      {payload.map((entry) => (
-                        <li
-                          key={entry.value}
-                          style={{ display: "flex", alignItems: "center", gap: "10px" }}
-                        >
-                          <div
-                            style={{
-                              height: "16px",
-                              width: "16px",
-                              borderRadius: "2px",
-                              backgroundColor: entry.color,
-                            }}
-                          />
-                          <div style={{ color: "#4E5F6D" }}>{formatter(entry.value)}</div>
-                        </li>
-                      ))}
-                    </ul>
-                  );
+        {graphData.length ? (
+          <Box my={4}>
+            <ResponsiveContainer width="100%" height={graphHeight}>
+              <BarChart
+                data={graphData}
+                layout="vertical"
+                margin={{
+                  right: 30,
+                  left: 50,
                 }}
-              />
-            </BarChart>
-          </ResponsiveContainer>
-        </Box>
+                barCategoryGap={barCategoryGap}
+                barSize={barSize}
+              >
+                <CartesianGrid horizontal={false} strokeDasharray="3 3" />
+                <XAxis type="number" />
+                <YAxis type="category" dataKey="objectType" textAnchor="start" dx={-90} tickLine={false} />
+                <Tooltip content={<CustomTooltip />} />
+                <Bar
+                  dataKey="automaticDDLImport"
+                  fill="#2FB3FF"
+                  stackId="stack"
+                  isAnimationActive={false}
+                >
+                  <LabelList
+                    dataKey="automaticDDLImport"
+                    position="insideRight"
+                    style={{ fill: "black" }}
+                    {...{
+                      formatter: (value: number) => value || null,
+                    }}
+                  />
+                </Bar>
+                <Bar
+                  dataKey="manualRefactoring"
+                  fill="#FFA400"
+                  stackId="stack"
+                  isAnimationActive={false}
+                >
+                  <LabelList
+                    dataKey="manualRefactoring"
+                    position="insideRight"
+                    style={{ fill: "black" }}
+                    {...{
+                      formatter: (value: number) => value || null,
+                    }}
+                  />
+                </Bar>
+                <Legend
+                  align="left"
+                  content={({ payload }) => {
+                    if (!payload) {
+                      return null;
+                    }
 
-        <Divider />
+                    const formatter = (value: string) =>
+                      value
+                        .split(/(?=[A-Z][a-z])|(?<=[a-z])(?=[A-Z])/)
+                        .map((part) => part.charAt(0).toUpperCase() + part.slice(1))
+                        .join(" ");
 
-        <Box my={3}>
-          <Typography variant="h5">
-            {t("clusterDetail.voyager.planAndAssess.refactoring.conversionIssues")}
-          </Typography>
-        </Box>
+                    return (
+                      <ul
+                        style={{
+                          listStyleType: "none",
+                          display: "flex",
+                          gap: "20px",
+                          paddingLeft: "70px",
+                        }}
+                      >
+                        {payload.map((entry) => (
+                          <li
+                            key={entry.value}
+                            style={{ display: "flex", alignItems: "center", gap: "10px" }}
+                          >
+                            <div
+                              style={{
+                                height: "16px",
+                                width: "16px",
+                                borderRadius: "2px",
+                                backgroundColor: entry.color,
+                              }}
+                            />
+                            <div style={{ color: "#4E5F6D" }}>{formatter(entry.value)}</div>
+                          </li>
+                        ))}
+                      </ul>
+                    );
+                  }}
+                />
+              </BarChart>
+            </ResponsiveContainer>
+          </Box>
+        ) : null}
 
-        <Box display="flex" flexDirection="column" gridGap={20}>
-          <MigrationAssessmentRefactoringTable
-            data={unsupportedDataTypes}
-            tableHeader={t("clusterDetail.voyager.planAndAssess.refactoring.unsupportedDataType")}
-          />
-          <MigrationAssessmentRefactoringTable
-            data={unsupportedFeatures}
-            tableHeader={t("clusterDetail.voyager.planAndAssess.refactoring.unsupportedFeature")}
-          />
-          <MigrationAssessmentRefactoringTable
-            data={unsupportedFunctions}
-            tableHeader={t("clusterDetail.voyager.planAndAssess.refactoring.unsupportedFunction")}
-          />
-        </Box>
+        {unsupportedDataTypes?.length ||
+        unsupportedFeatures?.length ||
+        unsupportedFunctions?.length ? (
+          <>
+            <Divider />
+
+            <Box my={3}>
+              <Typography variant="h5">
+                {t("clusterDetail.voyager.planAndAssess.refactoring.conversionIssues")}
+              </Typography>
+            </Box>
+
+            <Box display="flex" flexDirection="column" gridGap={20}>
+              {unsupportedDataTypes?.length && (
+                <MigrationAssessmentRefactoringTable
+                  data={unsupportedDataTypes}
+                  tableHeader={t(
+                    "clusterDetail.voyager.planAndAssess.refactoring.unsupportedDataType"
+                  )}
+                />
+              )}
+              {unsupportedDataTypes?.length && (
+                <MigrationAssessmentRefactoringTable
+                  data={unsupportedFeatures}
+                  tableHeader={t(
+                    "clusterDetail.voyager.planAndAssess.refactoring.unsupportedFeature"
+                  )}
+                />
+              )}
+              {unsupportedDataTypes?.length && (
+                <MigrationAssessmentRefactoringTable
+                  data={unsupportedFunctions}
+                  tableHeader={t(
+                    "clusterDetail.voyager.planAndAssess.refactoring.unsupportedFunction"
+                  )}
+                />
+              )}
+            </Box>
+          </>
+        ) : null}
       </Box>
     </Paper>
   );
