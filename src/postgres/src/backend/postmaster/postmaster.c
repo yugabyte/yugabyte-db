yugabyte/yugabyte-db

commit 632949c9556df19e10bac683869b119a87c978fb

[pg15] fix: postmaster YB_TODOs and tests
Summary:
Changes:
* `pgstat_report_wait_end` was previously required to cleanup the shared memory `PGPROC*` struct, because that struct can be reused by another backend. In PG15, the field was changed from a member of the shared struct to a static variable `my_wait_event_info`. It is not necessary to clean up a static variable. Removed `pgstat_report_wait_end` since they are no longer used by YB. Addresses a YB_TODO item.
* Removed the YB code to set the postmaster death signal in`BackendStartup`. PG15 added a new function that is invoked slightly later that sets the death signal code to either SIGINFO or SIGPWR, so that its exit handlers can get a chance to run. In YB, we don't have state to worry about in the backends, so we can just kill them. Modify `PostmasterDeathSignalInit` to use SIGKILL for YB backends. This fixes the `SignalBackendOnPostmasterDeath` test.
* Rewrote `CleanupProcSignalState` to use `CleanupProcSignalStateInternal`. This addresses a YB_TODO
* Found a bug in CV cleanup (#23274) that affects master as well. Filed an issue for it and replaced the YB_TODO with the issue number.
* Commented out the `yb_terminated_queries` portion of the `TestLWPgBackendKillAfterLWLockAcquire` test with a new `YB_TODO`. That will be addressed by enabling `yb_terminated_queries` for PG15.

Test Plan:
```
./yb_build.sh --cxx-test pgwrapper_pg_libpq-test --gtest_filter PgPostmasterExitTest.SignalBackendOnPostmasterDeath -n 20
./yb_build.sh --cxx-test pgwrapper_pg_libpq-test --gtest_filter PgPostmasterExitTest.SignalIdleBackendOnPostmasterDeath -n 20
./yb_build.sh --cxx-test pgwrapper_pg_libpq-test --gtest_filter PgLibPqTest.TestLWPgBackendKillAfterLWLockAcquire -n 20
```

```
for _ in {1..50}; do grep -E 'TestLWPgBackendKill|PgPostmasterExitTest' pg15_tests/passing_tests.tsv; done | pg15_tests/run_tests.sh
```

Reviewers: jason, aagrawal, kramanathan

Reviewed By: kramanathan

Subscribers: yql

src/postgres/src/backend/postmaster/postmaster.c
@@ -78,10 +78,6 @@
 #include <limits.h>
 #include "yb_query_diagnostics.h"
 
-#ifdef HAVE_SYS_PRCTL_H
-#include <sys/prctl.h>
-#endif
-
 #ifdef HAVE_SYS_SELECT_H
 #include <sys/select.h>
 #endif
@@ -1072,9 +1068,9 @@ PostmasterMain(int argc, char *argv[])
 	if (!YBIsEnabledInPostgresEnvVar())
 		ApplyLauncherRegister();
 
-	/* Register the query diagnostics background worker */	
+	/* Register the query diagnostics background worker */
 	if (YBIsEnabledInPostgresEnvVar() && YBIsQueryDiagnosticsEnabled())
-		YbQueryDiagnosticsBgWorkerRegister(); 
+		YbQueryDiagnosticsBgWorkerRegister();
 
 	/* Register ASH collector */
 	if (YBIsEnabledInPostgresEnvVar() && yb_ash_enable_infra)
@@ -3642,10 +3638,10 @@ CleanupKilledProcess(PGPROC *proc)
 	{
 		/* These come from ShutdownAuxiliaryProcess */
 		ConditionVariableCancelSleepForProc(proc);
-#ifdef YB_TODO
-		/* YB_TODO Rewrite this funtion first, and then reactivate this call */
-		pgstat_report_wait_end_for_proc(proc);
-#endif
+		/*
+		 * We don't need to call pgstat_report_wait_end because it refers only
+		 * to a static variable, not a member of the PGPROC struct.
+		 */
 	}
 	else
 	{
@@ -4565,16 +4561,6 @@ BackendStartup(Port *port)
 	pid = fork_process();
 	if (pid == 0)				/* child */
 	{
-#ifdef HAVE_SYS_PRCTL_H
-		/*
-		 * In YB, all backends are stateless and upon PG master termination, all
-		 * backend processes should also terminate regardless what state they are
-		 * in. No clean-up procedure is needed in the backends.
-		 */
-		if (YBIsEnabledInPostgresEnvVar())
-			prctl(PR_SET_PDEATHSIG, SIGKILL);
-#endif
-
 		free(bn);
 
 		/* Detangle from postmaster */
@@ -4856,7 +4842,7 @@ BackendInitialize(Port *port)
 	if (YBIsEnabledInPostgresEnvVar() && am_walsender)
 	{
 		char		remote_ps_data[NI_MAXHOST];
-	
+
 		if (remote_port[0] == '\0')
 			snprintf(remote_ps_data, sizeof(remote_ps_data), "%s", remote_host);
 		else

src/postgres/src/backend/storage/ipc/procsignal.c
@@ -105,11 +105,6 @@ static ProcSignalSlot *MyProcSignalSlot = NULL;
 static bool CheckProcSignal(ProcSignalReason reason);
 static void ResetProcSignalBarrierBits(uint32 flags);
 
-#ifdef YB_TODO
-/* Yb makes this public */
-static void CleanupProcSignalState(int status, Datum arg);
-#endif
-
 /*
  * ProcSignalShmemSize
  *		Compute space needed for ProcSignal's shared memory
@@ -209,11 +204,11 @@ ProcSignalInit(int pss_idx)
 	on_shmem_exit(CleanupProcSignalState, Int32GetDatum(pss_idx));
 }
 
-/* CleanupProcSignalState
- * 		Remove current process from ProcSignalSlots
+/* CleanupProcSignalStateInternal
+ * 		Remove the given process from ProcSignalSlots
  */
 static void
-CleanupProcSignalStateInternal(PGPROC *proc, int procSignalSlotIndex, volatile ProcSignalSlot *slot)
+CleanupProcSignalStateInternal(PGPROC *proc, int pss_idx, ProcSignalSlot *slot)
 {
 	/* sanity check */
 	if (slot->pss_pid != proc->pid)
@@ -223,9 +218,16 @@ CleanupProcSignalStateInternal(PGPROC *proc, int procSignalSlotIndex, volatile P
 		 * infinite loop trying to exit
 		 */
 		elog(LOG, "process %d releasing ProcSignal slot %d, but it contains %d",
-			 proc->pid, procSignalSlotIndex, (int) slot->pss_pid);
+			 proc->pid, pss_idx, (int) slot->pss_pid);
 		return;					/* XXX better to zero the slot anyway? */
 	}
+
+	/*
+	 * Make this slot look like it's absorbed all possible barriers, so that
+	 * no barrier waits block on it.
+	 */
+	pg_atomic_write_u64(&slot->pss_barrierGeneration, PG_UINT64_MAX);
+	ConditionVariableBroadcast(&slot->pss_barrierCV);
 
 	slot->pss_pid = 0;
 }
@@ -252,33 +254,9 @@ CleanupProcSignalState(int status, Datum arg)
 	 */
 	MyProcSignalSlot = NULL;
 
-	/* sanity check */
-	if (slot->pss_pid != MyProcPid)
-	{
-		/*
-		 * don't ERROR here. We're exiting anyway, and don't want to get into
-		 * infinite loop trying to exit
-		 */
-		elog(LOG, "process %d releasing ProcSignal slot %d, but it contains %d",
-			 MyProcPid, pss_idx, (int) slot->pss_pid);
-		return;					/* XXX better to zero the slot anyway? */
-	}
-
-	/*
-	 * Make this slot look like it's absorbed all possible barriers, so that
-	 * no barrier waits block on it.
-	 */
-	pg_atomic_write_u64(&slot->pss_barrierGeneration, PG_UINT64_MAX);
-	ConditionVariableBroadcast(&slot->pss_barrierCV);
-
-	slot->pss_pid = 0;
+	CleanupProcSignalStateInternal(MyProc, pss_idx, slot);
 }
 
-
-/* YB_TODO(neil)
- * - Fixed CleanupProcSignalStateForProc().
- * - Delete CleanupProcSignalStateInternal(). It's no longer correct.
- */
 /*
  * CleanupProcSignalStateForProc
  *		Remove the given process from ProcSignalSlots
@@ -289,14 +267,12 @@ CleanupProcSignalState(int status, Datum arg)
 void
 CleanupProcSignalStateForProc(PGPROC *proc)
 {
-#ifdef YB_TODO
 	int			pss_idx = proc->backendId;
-	volatile ProcSignalSlot *slot;
+	ProcSignalSlot *slot;
 
-	slot = &ProcSignalSlots[pss_idx - 1];
+	slot = &ProcSignal->psh_slot[pss_idx - 1];
 
-	CleanupProcSignalStateInternal(proc, proc->backendId, slot);
-#endif
+	CleanupProcSignalStateInternal(proc, pss_idx, slot);
 }
 
 /*

src/postgres/src/backend/storage/lmgr/condition_variable.c
@@ -258,9 +258,8 @@ ConditionVariableCancelSleep(void)
  *
  * Do nothing if nothing is pending; this allows this function to be called
  * during transaction abort to clean up any unfinished CV sleep.
- */
-/* YB_TODO(neil) Need to rewrite function ConditionVariableCancelSleepForProc) to call
- * ConditionVariableCancelSleep()
+ *
+ * TODO(#23274): Rewrite / delete ConditionVariableCancelSleepForProc
  */
 void
 ConditionVariableCancelSleepForProc(volatile PGPROC *proc)

src/postgres/src/backend/storage/ipc/pmsignal.c
@@ -437,7 +437,17 @@ void
 PostmasterDeathSignalInit(void)
 {
 #ifdef USE_POSTMASTER_DEATH_SIGNAL
-	int			signum = POSTMASTER_DEATH_SIGNAL;
+	int			signum;
+
+	/*
+	 * In YB, all backends are stateless and upon PG master termination, all
+	 * backend processes should also terminate regardless what state they are
+	 * in. No clean-up procedure is needed in the backends.
+	 */
+	if (YBIsEnabledInPostgresEnvVar())
+		signum = SIGKILL;
+	else
+		signum = POSTMASTER_DEATH_SIGNAL;
 
 	/* Register our signal handler. */
 	pqsignal(signum, postmaster_death_handler);

src/postgres/src/include/pgstat.h
@@ -737,47 +737,6 @@ extern void pgstat_end_function_usage(PgStat_FunctionCallUsage *fcu,
 extern PgStat_StatFuncEntry *pgstat_fetch_stat_funcentry(Oid funcid);
 extern PgStat_BackendFunctionEntry *find_funcstat_entry(Oid func_id);
 
-#ifdef YB_TODO
-/* These functions are no longer used in Pg15 */
-/* ----------
- * pgstat_report_wait_end_for_proc(PGPROC *proc) -
- *
- *	Called to report end of a wait for a specific process.
- *
- * NB: this *must* be able to survive being called before MyProc has been
- * initialized.
- * ----------
- */
-static inline void
-pgstat_report_wait_end_for_proc(volatile PGPROC *proc)
-{
-	if (!pgstat_track_activities || !proc)
-		return;
-
-	/*
-	 * Since this is a four-byte field which is always read and written as
-	 * four-bytes, updates are atomic.
-	 */
-	proc->wait_event_info = 0;
-}
-
-
-/* ----------
- * pgstat_report_wait_end() -
- *
- *	Called to report end of a wait.
- *
- * NB: this *must* be able to survive being called before MyProc has been
- * initialized.
- * ----------
- */
-static inline void
-pgstat_report_wait_end(void)
-{
-	return pgstat_report_wait_end_for_proc(MyProc);
-}
-#endif
-
 /*
  * Functions in pgstat_relation.c
  */

src/yb/yql/pgwrapper/pg_libpq-test.cc
@@ -3220,12 +3220,14 @@ TEST_F_EX(PgLibPqTest,
   ASSERT_NOK(conn1.Fetch("SELECT pg_stat_statements_reset()"));
   ASSERT_NOK(conn2.Fetch("SELECT 1"));
 
+#ifdef YB_TODO // yb_terminated_queries is not yet supported in PG15
   // validate that this query is added to yb_terminated_queries
   auto conn3 = ASSERT_RESULT(Connect());
   const string get_yb_terminated_queries =
     "SELECT query_text, termination_reason FROM yb_terminated_queries";
   auto row = ASSERT_RESULT((conn3.FetchRow<std::string, std::string>(get_yb_terminated_queries)));
   ASSERT_EQ(row, (decltype(row){"SELECT pg_stat_statements_reset()", "Terminated by SIGKILL"}));
+#endif
 }
 
 TEST_F_EX(PgLibPqTest,

commit 61730dec6c64f6b2c9f91f31c67559d7bed9acf9

[pg15] fix: avoid using incorrect MyProc in CVBroadcast cleanup function
Summary:
D36847 / 632949c9556df19e10bac683869b119a87c978fb broke the TestPgConnection tests.

`CleanupProcSignalStateInternal` is supposed to be able to be called from a process for itself, or from the postmaster for a recently killed process. However, it called the function `ConditionVariableBroadcast`, which relies on the static variable MyProc.

The fix is to explicitly pass in the PGPROC that we want to use for the `ConditionVariableBroadcast` function, by making `ConditionVariableBroadcast` a wrapper for a new function `YbConditionVariableBroadcastForProc`  - mostly the same except for a few changes to make it work for the given process.

Test Plan:
```
./yb_build.sh --java-test 'org.yb.pgsql.TestPgConnection'
```

Reviewers: aagrawal, kramanathan

Reviewed By: aagrawal

Subscribers: yql

Differential Revision: https://phorge.dev.yugabyte.com/D36977

src/postgres/src/backend/postmaster/postmaster.c
@@ -3637,7 +3637,7 @@ CleanupKilledProcess(PGPROC *proc)
 	if (proc->backendId == InvalidBackendId)
 	{
 		/* These come from ShutdownAuxiliaryProcess */
-		ConditionVariableCancelSleepForProc(proc);
+		YbConditionVariableCancelSleepForProc(proc);
 		/*
 		 * We don't need to call pgstat_report_wait_end because it refers only
 		 * to a static variable, not a member of the PGPROC struct.
@@ -3658,7 +3658,7 @@ CleanupKilledProcess(PGPROC *proc)
 	/* From ProcKill */
 	ReplicationSlotCleanupForProc(proc);
 	SyncRepCleanupAtProcExit(proc);
-	ConditionVariableCancelSleepForProc(proc);
+	YbConditionVariableCancelSleepForProc(proc);
 
 	if (proc->lockGroupLeader != NULL)
 	{

src/postgres/src/backend/storage/ipc/procsignal.c
@@ -227,7 +227,7 @@ CleanupProcSignalStateInternal(PGPROC *proc, int pss_idx, ProcSignalSlot *slot)
 	 * no barrier waits block on it.
 	 */
 	pg_atomic_write_u64(&slot->pss_barrierGeneration, PG_UINT64_MAX);
-	ConditionVariableBroadcast(&slot->pss_barrierCV);
+	YbConditionVariableBroadcastForProc(&slot->pss_barrierCV, proc);
 
 	slot->pss_pid = 0;
 }

src/postgres/src/backend/storage/lmgr/condition_variable.c
@@ -259,10 +259,10 @@ ConditionVariableCancelSleep(void)
  * Do nothing if nothing is pending; this allows this function to be called
  * during transaction abort to clean up any unfinished CV sleep.
  *
- * TODO(#23274): Rewrite / delete ConditionVariableCancelSleepForProc
+ * TODO(#23274): Rewrite / delete YbConditionVariableCancelSleepForProc
  */
 void
-ConditionVariableCancelSleepForProc(volatile PGPROC *proc)
+YbConditionVariableCancelSleepForProc(volatile PGPROC *proc)
 {
 	ConditionVariable *cv = cv_sleep_target;
 
@@ -300,6 +300,12 @@ ConditionVariableSignal(ConditionVariable *cv)
 		SetLatch(&proc->procLatch);
 }
 
+void
+ConditionVariableBroadcast(ConditionVariable *cv)
+{
+	return YbConditionVariableBroadcastForProc(cv, MyProc);
+}
+
 /*
  * Wake up all processes sleeping on the given CV.
  *
@@ -308,9 +314,10 @@ ConditionVariableSignal(ConditionVariable *cv)
  * will typically not get awakened.
  */
 void
-ConditionVariableBroadcast(ConditionVariable *cv)
+YbConditionVariableBroadcastForProc(ConditionVariable *cv,
+									volatile PGPROC *given_proc)
 {
-	int			pgprocno = MyProc->pgprocno;
+	int			pgprocno = given_proc->pgprocno;
 	PGPROC	   *proc = NULL;
 	bool		have_sentinel = false;
 
@@ -336,7 +343,7 @@ ConditionVariableBroadcast(ConditionVariable *cv)
 	 * care of re-establishing the lost state.
 	 */
 	if (cv_sleep_target != NULL)
-		ConditionVariableCancelSleep();
+		YbConditionVariableCancelSleepForProc(given_proc);
 
 	/*
 	 * Inspect the state of the queue.  If it's empty, we have nothing to do.
@@ -383,7 +390,7 @@ ConditionVariableBroadcast(ConditionVariable *cv)
 		have_sentinel = proclist_contains(&cv->wakeup, pgprocno, cvWaitLink);
 		SpinLockRelease(&cv->mutex);
 
-		if (proc != NULL && proc != MyProc)
+		if (proc != NULL && proc != given_proc)
 			SetLatch(&proc->procLatch);
 	}
 }

src/postgres/src/include/storage/condition_variable.h
@@ -59,8 +59,6 @@ extern bool ConditionVariableTimedSleep(ConditionVariable *cv, long timeout,
 										uint32 wait_event_info);
 extern void ConditionVariableCancelSleep(void);
 
-extern void ConditionVariableCancelSleepForProc(volatile PGPROC *proc);
-
 /*
  * Optionally, ConditionVariablePrepareToSleep can be called before entering
  * the test-and-sleep loop described above.  Doing so is more efficient if
@@ -72,5 +70,15 @@ extern void ConditionVariablePrepareToSleep(ConditionVariable *cv);
 /* Wake up a single waiter (via signal) or all waiters (via broadcast). */
 extern void ConditionVariableSignal(ConditionVariable *cv);
 extern void ConditionVariableBroadcast(ConditionVariable *cv);
+
+/*
+ * In YB, the Postmaster cleans up on behalf of abruptly terminated
+ * backends. In these cases, the CV functions cannot just use the `MyProc`
+ * variable, because that refers to the postmaster's PGPROC instead of the
+ * backend's PGPROC.
+ */
+extern void YbConditionVariableCancelSleepForProc(volatile PGPROC *proc);
+extern void YbConditionVariableBroadcastForProc(ConditionVariable *cv,
+												volatile PGPROC *proc);
 
 #endif							/* CONDITION_VARIABLE_H */