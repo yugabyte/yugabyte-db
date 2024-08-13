70d67ac81b33249381f564e2c5d0f26c51f56fd9
jhe <jhe@yugabyte.com>
2024-08-13 01:02:53 -0700

    [BACKPORT pg15-cherrypicks][#19192] XClusterDDLRepl: Support Drop Relations
    Summary:
    ```
    Modifications:
    - src/postgres/yb-extensions/yb_xcluster_ddl_replication/source_ddl_end_handler.c
      - AreCommandTagsEqual:
        - function is removed
      - ProcessSourceEventTriggerDDLCommands:
        - Replaced AreCommandTagsEqual calls with GetCommandTagEnum == CMDTAG_* enum comparisons
      - ProcessSourceEventTriggerDDLCommands:
        - replaced code to get Oid with SPI_GetOid
        - also changed OBJID_COLUMN_ID to DDL_END_OBJID_COLUMN_ID
      - ProcessSourceEventTriggerDDLCommands:
        - replaced code to get Text with SPI_GetText
        - also changed COMMAND_TAG_COLUMN_ID to DDL_END_COMMAND_TAG_COLUMN_ID
    ```
    
    Original commit: 924dada7bb / D34898
    Adding in initial support for drop table/index + some related objects.
    
    Since dropped objects are not captured by pg_event_trigger_ddl_commands and only captured by
    pg_event_trigger_dropped_objects, I am adding in a new event trigger on sql_drop to capture and
    check dropped objects. For the time being this is just doing simple checks (checking for temporary
    objects) as the trigger happens after the delete has occured, so it is difficult to do lookups. If
    necessary in the future, we could expand pg_event_trigger_dropped_objects to have additional data,
    but that is not needed at the moment.
    
    As part of this, I am pulling out should_replicate_ddl to a global variable that is per DDL. So now,
    if a DDL has any object that is dropped/created/modified and we need to replicate it, then we will
    replicate the entire command.
    Adding in a new event trigger on ddl_command_start to reset this variable and also moving prechecks
    here (eg multicommand check).
    
    Note: For the time being until ghi #22320, we will block drops of any relations in a colocated
    database. We could do as I mentioned above and add is_colocated to pg_event_trigger_dropped_objects,
    but that would be just be removed in a soon to come diff, so opting for a simpler temp solution.
    Jira: DB-7981
    
    Test Plan:
    ```
    ybd --java-test "org.yb.pgsql.TestPgRegressYbExtensionsYbXclusterDdlReplication"
    
    ybd --cxx-test xcluster_ddl_replication_pgregress-test --gtest_filter "XClusterPgRegressDDLReplicationTest.PgRegressCreateDropTable"
    ybd --cxx-test xcluster_ddl_replication_pgregress-test --gtest_filter "XClusterPgRegressDDLReplicationTest.PgRegressCreateDropTable2"
    ybd --cxx-test xcluster_ddl_replication_pgregress-test --gtest_filter "XClusterPgRegressDDLReplicationTest.PgRegressCreateTableUnsupported"
    ybd --cxx-test xcluster_ddl_replication_pgregress-test --gtest_filter "XClusterPgRegressDDLReplicationTest.PgRegressCreateDropPartitionedTable"
    ybd --cxx-test xcluster_ddl_replication_pgregress-test --gtest_filter "XClusterPgRegressDDLReplicationTest.PgRegressCreateDropTablePartitions"
    ybd --cxx-test xcluster_ddl_replication_pgregress-test --gtest_filter "XClusterPgRegressDDLReplicationTest.PgRegressCreateDropTablePartitions2"
    ```
    
    Reviewers: jason, tfoucher, xCluster, hsunder
    
    Reviewed By: jason
    
    Subscribers: yql, ybase, ycdcxcluster
    
    Tags: #jenkins-ready
    
    Differential Revision: https://phorge.dev.yugabyte.com/D37245

--- src/postgres/yb-extensions/yb_xcluster_ddl_replication/sql/create_drop_index.sql
+++ src/postgres/yb-extensions/yb_xcluster_ddl_replication/sql/create_drop_index.sql
@@ -9,6 +9,9 @@ SET yb_xcluster_ddl_replication.replication_role = SOURCE;
 CREATE TEMP TABLE temp_foo(i int PRIMARY KEY, a int);
 CREATE INDEX foo_idx_temp on temp_foo(a);
 
+DROP INDEX foo_idx_temp;
+DROP TABLE temp_foo;
+
 SELECT yb_data FROM yb_xcluster_ddl_replication.ddl_queue ORDER BY start_time;
 SET yb_xcluster_ddl_replication.replication_role = BIDIRECTIONAL;
 
@@ -27,6 +30,17 @@ CREATE ROLE new_role SUPERUSER;
 SET ROLE new_role;
 CREATE INDEX foo_idx_include ON foo(lower(b)) INCLUDE (a) SPLIT INTO 2 TABLETS;
 SET ROLE NONE;
+
+SELECT yb_data FROM yb_xcluster_ddl_replication.ddl_queue ORDER BY start_time;
+SELECT * FROM yb_xcluster_ddl_replication.replicated_ddls ORDER BY start_time;
+
+-- Now drop these indexes.
+-- Drop two indexes by themselves.
+DROP INDEX foo_idx_unique;
+DROP INDEX foo_idx_filtered;
+
+-- Drop base table and cascade deletion of other indexes.
+DROP TABLE foo;
 
 SELECT yb_data FROM yb_xcluster_ddl_replication.ddl_queue ORDER BY start_time;
 SELECT * FROM yb_xcluster_ddl_replication.replicated_ddls ORDER BY start_time;
--- src/postgres/yb-extensions/yb_xcluster_ddl_replication/sql/create_drop_table.sql
+++ src/postgres/yb-extensions/yb_xcluster_ddl_replication/sql/create_drop_table.sql
@@ -3,6 +3,7 @@ CALL TEST_reset();
 -- Verify that temporary objects are not captured.
 SET yb_xcluster_ddl_replication.replication_role = SOURCE;
 CREATE TEMP TABLE temp_foo(i int PRIMARY KEY);
+DROP TABLE temp_foo;
 
 SELECT yb_data FROM yb_xcluster_ddl_replication.ddl_queue ORDER BY start_time;
 SET yb_xcluster_ddl_replication.replication_role = BIDIRECTIONAL;
@@ -23,3 +24,27 @@ CREATE TABLE unique_foo(i int PRIMARY KEY, u text UNIQUE);
 
 SELECT yb_data FROM yb_xcluster_ddl_replication.ddl_queue ORDER BY start_time;
 SELECT * FROM yb_xcluster_ddl_replication.replicated_ddls ORDER BY start_time;
+
+-- Now test dropping these tables.
+DROP TABLE foo;
+
+-- Check with manual replication flags enabled, ddl string is captured with flag.
+SET yb_xcluster_ddl_replication.enable_manual_ddl_replication = 1;
+DROP TABLE manual_foo;
+SET yb_xcluster_ddl_replication.enable_manual_ddl_replication = 0;
+
+DROP TABLE extra_foo;
+DROP TABLE unique_foo;
+
+SELECT yb_data FROM yb_xcluster_ddl_replication.ddl_queue ORDER BY start_time;
+SELECT * FROM yb_xcluster_ddl_replication.replicated_ddls ORDER BY start_time;
+
+-- Test mix of temp and regular tables.
+SET yb_xcluster_ddl_replication.replication_role = SOURCE;
+CREATE TEMP TABLE temp_foo(i int PRIMARY KEY);
+SET yb_xcluster_ddl_replication.replication_role = BIDIRECTIONAL;
+CREATE TABLE foo(i int PRIMARY KEY);
+DROP TABLE temp_foo, foo; -- should fail
+DROP TABLE foo, temp_foo; -- should fail
+DROP TABLE temp_foo;
+DROP TABLE foo;
