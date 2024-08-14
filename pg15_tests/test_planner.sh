bbd1a46c45984a4cd26f291919903e2e4d550ba0
Jason Kim <git@jasonk.me>
2024-08-13 17:01:23 -0700

    [pg15] test: remove outdated, failing shell tests
    Summary:
    Remove some pg15_tests shell tests that were tracking tests that are now
    fully passing:
    
    - TestPgRegressMisc#testPgRegressMiscSerial: fixed by
      cf835dfe2f62aeb03da41a7288b9bfa8a5e6e0dc.
    - TestPgRegressParallel: fixed by
      9645d0c1c3b0da0defe2e7ce4f02d7abd819bfa1.
    - TestPgRegressPlanner: fixed by
      b263910ba90dd273d10a8ecffa90b8affcdea529 (unexpectedly).
    
    Test Plan:
    On Almalinux 8:
    
        #!/usr/bin/env bash
        set -eu
        ./yb_build.sh fastdebug --gcc11
        pg15_tests/get_shell_test_specs.sh | pg15_tests/run_tests.sh
    
    Jenkins: skip
    
    Reviewers: aagrawal
    
    Reviewed By: aagrawal
    
    Subscribers: yql
    
    Differential Revision: https://phorge.dev.yugabyte.com/D37286

--- pg15_tests/test_planner.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-source "${BASH_SOURCE[0]%/*}"/common.sh
-
-failing_java_test TestPgRegressPlanner
-grep_in_java_test \
-  "failed tests: [yb_planner_size_estimates]" \
-  TestPgRegressPlanner
