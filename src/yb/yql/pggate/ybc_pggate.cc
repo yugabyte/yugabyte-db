45e40a4d2a168ca31426cf5f209a91cbbabbd2b3
timothy-e <timothy.elgersma@gmail.com>
2024-08-15 10:33:03 -0400

    [BACKPORT 2.20][#22989] YSQL: Fix regular expression pushdown
    Summary:
    Original commit: d0500a02c9117ae1fdb13b994bf4c7dc776491e1 / D36095
    Regular expressions are compiled first and then executed. To avoid recompiling a regular expression each time, Postgres has implemented a cache (size 32) that stores the compiled regular expressions. This is implemented as a static array, and works well for PG because PG is single-threaded.
    
    When expression pushdown is enabled in Yugabyte, Postgres expression evaluation is carried out on DocDB side. The same code is executed in a multi-threaded environment, so multiple threads are accessing the same objects. This can result in correctness issues (because the compiled regex is not what was expected) or tserver crashes (because the cache is corrupted).
    
    Using a thread local cache solves the problem. Each compiled regex is malloc'd, so to handle its lifetime, a new object is created in C++ code that invokes the cleanup for the regular expressions in the cache.
    Jira: DB-11908
    
    Test Plan:
    ```
    ./yb_build.sh --cxx-test pgwrapper_pg_mini-test --gtest_filter PgMiniTest.RegexPushdown
    ```
    
    fails on master, passes with this change.
    
    Reviewers: amartsinchyk, dmitry
    
    Reviewed By: dmitry
    
    Subscribers: dmitry, smishra, yql
    
    Tags: #jenkins-ready
    
    Differential Revision: https://phorge.dev.yugabyte.com/D37304

--- src/yb/yql/pggate/ybc_pggate.cc
+++ src/yb/yql/pggate/ybc_pggate.cc
@@ -1650,6 +1650,15 @@ void YBCPgSetThreadLocalStrTokPtr(char *new_pg_strtok_ptr) {
   PgSetThreadLocalStrTokPtr(new_pg_strtok_ptr);
 }
 
+YBCPgThreadLocalRegexpCache* YBCPgGetThreadLocalRegexpCache() {
+  return PgGetThreadLocalRegexpCache();
+}
+
+YBCPgThreadLocalRegexpCache* YBCPgInitThreadLocalRegexpCache(
+    size_t buffer_size, YBCPgThreadLocalRegexpCacheCleanup cleanup) {
+  return PgInitThreadLocalRegexpCache(buffer_size, cleanup);
+}
+
 void* YBCPgSetThreadLocalJumpBuffer(void* new_buffer) {
   return PgSetThreadLocalJumpBuffer(new_buffer);
 }
