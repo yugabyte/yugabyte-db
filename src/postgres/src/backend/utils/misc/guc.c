91a265aa93c3a1ca9c5be4b74c7909b8cb5fb603
Bvsk Patnaik <bvsaikiran.patnaik@gmail.com>
2024-08-22 20:54:39 -0700

    [pg15] fix: re-enable commented out GUC yb_plpgsql_disable_prefetch_in_for_query
    Summary:
    Fix TestPgReadCommittedFuncs test. This is done by changing QUERY_TUNING to QUERY_TUNING_METHOD.
    
    Test Plan:
    Jenkins: rebase: pg15
    
    ./yb_build.sh --java-test TestPgReadCommittedFuncs
    
    Reviewers: pjain, jason, aagrawal, smishra
    
    Reviewed By: jason
    
    Subscribers: yql
    
    Differential Revision: https://phorge.dev.yugabyte.com/D37496

--- src/postgres/src/backend/utils/misc/guc.c
+++ src/postgres/src/backend/utils/misc/guc.c
@@ -2753,13 +2753,8 @@ static struct config_bool ConfigureNamesBool[] =
 		NULL, NULL, NULL
 	},
 
-#ifdef YB_TODO
-	/* deepthi@yugabyte
-	 * - QUERY_TUNING is no longer defined in Postgres source code.
-	 * - Please make appropriate modification.
-	 */
 	{
-		{"yb_plpgsql_disable_prefetch_in_for_query", PGC_USERSET, QUERY_TUNING,
+		{"yb_plpgsql_disable_prefetch_in_for_query", PGC_USERSET, QUERY_TUNING_METHOD,
 			gettext_noop("Disable prefetching in a PLPGSQL FOR loop over a query."),
 			NULL
 		},
@@ -2767,7 +2762,6 @@ static struct config_bool ConfigureNamesBool[] =
 		false,
 		NULL, NULL, NULL
 	},
-#endif
 
 	{
 		{"yb_enable_sequence_pushdown", PGC_USERSET, QUERY_TUNING_METHOD,
