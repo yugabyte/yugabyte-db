60c5e44cbee7f81d337c24cf96a007dcfec61169
Jethro Mak <88681329+Jethro-M@users.noreply.github.com>
2024-08-26 18:30:29 -0400

    [PLAT-15061] Update integration page naming
    Summary:
    This is a simple diff that updates the provider configurations page title to `Integrations` to be in line
    with the new tab page.
    
    The diff also adds the i18n for the string on this page.
    
    Test Plan:
    Verify the YBA integrations page loads fine and all texts appear as expected.
    {F278040}
    
    Reviewers: rmadhavan, agarg
    
    Reviewed By: rmadhavan
    
    Subscribers: yugaware
    
    Differential Revision: https://phorge.dev.yugabyte.com/D37515

--- managed/ui/src/components/configRedesign/DataCenterConfigRedesign.tsx
+++ managed/ui/src/components/configRedesign/DataCenterConfigRedesign.tsx
@@ -7,6 +7,8 @@
 import { Tab } from 'react-bootstrap';
 import { useSelector } from 'react-redux';
 import { useQuery, useQueries } from 'react-query';
+import i18next from 'i18next';
+import { useTranslation } from 'react-i18next';
 
 import SecurityConfiguration from '../config/Security/SecurityConfiguration';
 import awsLogo from '../config/ConfigProvider/images/aws.svg';
@@ -55,6 +57,7 @@ export const DataCenterConfigRedesign = ({
   const { currentCustomer } = useSelector((state: any) => state.customer);
   const featureFlags = useSelector((state: any) => state.featureFlags);
   showOrRedirect(currentCustomer.data.features, 'menu.config');
+  const { t } = useTranslation('translation', { keyPrefix: 'integrations' });
 
   // Start fetching the region metadata to seed the cache once the user navigates to the providers tab.
   // Although this data isn't required on first load, the intention is to start fetching in the background to avoid
@@ -90,7 +93,7 @@ export const DataCenterConfigRedesign = ({
     params.tab !== undefined &&
     !Object.values(ConfigTabKey).includes(params.tab as ConfigTabKey)
   ) {
-    return <YBErrorIndicator customErrorMessage="404 Page Not Found." />;
+    return <YBErrorIndicator customErrorMessage={t('error.pageNotFound')} />;
   }
 
   const defaultTab = isAvailable(currentCustomer.data.features, 'config.infra')
@@ -100,7 +103,7 @@ export const DataCenterConfigRedesign = ({
   const activeSection = params.section ?? 's3';
   return (
     <div>
-      <h2 className="content-title">Provider Configurations</h2>
+      <h2 className="content-title">{t('title')}</h2>
       <RbacValidator accessRequiredOn={ApiPermissionMap.GET_PROVIDERS}>
         <YBTabsWithLinksPanel
           defaultTab={defaultTab}
@@ -110,7 +113,11 @@ export const DataCenterConfigRedesign = ({
           className="universe-detail data-center-config-tab"
         >
           {isAvailable(currentCustomer.data.features, 'config.infra') && (
-            <Tab eventKey={ConfigTabKey.INFRA} title="Infrastructure" key="infra-config">
+            <Tab
+              eventKey={ConfigTabKey.INFRA}
+              title={t('tab.infrastructure.tabLabel')}
+              key="infra-config"
+            >
               <YBTabsPanel
                 defaultTab={ProviderCode.AWS}
                 activeTab={params.section}
@@ -215,7 +222,7 @@ export const DataCenterConfigRedesign = ({
             </Tab>
           )}
           {isAvailable(currentCustomer.data.features, 'config.backup') && (
-            <Tab eventKey="backup" title="Backup" key="storage-config">
+            <Tab eventKey="backup" title={t('tab.backup.tabLabel')} key="storage-config">
               <StorageConfigurationContainer
                 activeTab={activeSection}
                 routePrefix={CONFIG_ROUTE_PREFIX}
@@ -224,13 +231,17 @@ export const DataCenterConfigRedesign = ({
           )}
 
           {isExportLogEnabled && (
-            <Tab eventKey={ConfigTabKey.LOG} title="Log" key="log">
+            <Tab eventKey={ConfigTabKey.LOG} title={t('tab.log.tabLabel')} key="log">
               <ExportLog />
             </Tab>
           )}
 
           {isAvailable(currentCustomer.data.features, 'config.security') && (
-            <Tab eventKey={ConfigTabKey.SECURITY} title="Security" key="security-config">
+            <Tab
+              eventKey={ConfigTabKey.SECURITY}
+              title={t('tab.security.tabLabel')}
+              key="security-config"
+            >
               <SecurityConfiguration activeTab={params.section} />
             </Tab>
           )}
@@ -238,7 +249,7 @@ export const DataCenterConfigRedesign = ({
             featureFlags.released['enableMultiRegionConfig']) && (
             <Tab
               eventKey={ConfigTabKey.BACKUP_NEW}
-              title="New Backup Config"
+              title={t('tab.backupNew.tabLabel')}
               key="new-backup-config"
             >
               <NewStorageConfiguration activeTab={params.section} />
@@ -247,7 +258,7 @@ export const DataCenterConfigRedesign = ({
           {isTroubleshootingEnabled && (
             <Tab
               eventKey={ConfigTabKey.TROUBLESHOOT}
-              title="Troubleshoot"
+              title={t('tab.troubleshoot.tabLabel')}
               key="troubleshoot-config"
             >
               <TroubleshootingDetails activeTab={params.section} />
@@ -260,40 +271,41 @@ export const DataCenterConfigRedesign = ({
 };
 
 const getTabTitle = (providerCode: ProviderCode | KubernetesProviderType) => {
+  const I18N_KEY_PREFIX = 'integrations.tab.infrastructure.tab';
   switch (providerCode) {
     case ProviderCode.AWS:
       return (
         <div className="title">
           <img src={awsLogo} alt="AWS" className="aws-logo" />
-          <span>Amazon Web Services</span>
+          <span>{i18next.t(`${I18N_KEY_PREFIX}.aws`)}</span>
         </div>
       );
     case ProviderCode.GCP:
       return (
         <div className="title">
           <img src={gcpLogo} alt="GCP" className="gcp-logo" />
-          <span>Google Cloud Platform</span>
+          <span>{i18next.t(`${I18N_KEY_PREFIX}.gcp`)}</span>
         </div>
       );
     case ProviderCode.AZU:
       return (
         <div className="title">
           <img src={azureLogo} alt="Azure" className="azure-logo" />
-          <span>Microsoft Azure</span>
+          <span>{i18next.t(`${I18N_KEY_PREFIX}.azu`)}</span>
         </div>
       );
     case KubernetesProviderType.TANZU:
       return (
         <div className="title">
           <img src={tanzuLogo} alt="VMware Tanzu" />
-          <span>VMware Tanzu</span>
+          <span>{i18next.t(`${I18N_KEY_PREFIX}.vmwareTanzu`)}</span>
         </div>
       );
     case KubernetesProviderType.OPEN_SHIFT:
       return (
         <div className="title">
           <img src={openshiftLogo} alt="Red Hat OpenShift" />
-          <span>Red Hat OpenShift</span>
+          <span>{i18next.t(`${I18N_KEY_PREFIX}.redHatOpenShift`)}</span>
         </div>
       );
     case KubernetesProviderType.MANAGED_SERVICE:
@@ -301,14 +313,14 @@ const getTabTitle = (providerCode: ProviderCode | KubernetesProviderType) => {
       return (
         <div className="title">
           <img src={k8sLogo} alt="Managed Kubernetes" />
-          <span>Managed Kubernetes Service</span>
+          <span>{i18next.t(`${I18N_KEY_PREFIX}.managedK8s`)}</span>
         </div>
       );
     case ProviderCode.ON_PREM:
       return (
         <div className="title">
           <i className="fa fa-server tab-logo" />
-          <span>On-Premises Datacenters</span>
+          <span>{i18next.t(`${I18N_KEY_PREFIX}.onPrem`)}</span>
         </div>
       );
     case ProviderCode.CLOUD:
