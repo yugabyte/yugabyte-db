04482b23b342fd77e9b352065ddcdd76a8fd5a6b
Deepti-yb <dkumar@yugabyte.com>
2024-09-16 13:45:17 +0000

    [BACKPORT 2024.1][PLAT-15320][YBA CLI]K8s Universes throw `interface {} is string, not []float64` error
    Summary:
    After the introduction of template files for configuration, the float slices conversion started throwing an error. This is because viper does not contain a function to read float slices, and we ha to rely on viper.Get() to fetch the values. Despite the return type being interface, the actual type of the Get() return is string, which is what was causing the current issue. Introduced a function that would explicitly convert the string read by `viper.Get()` to a float slice in this diff.
    
    Original commit: 55b4187d6069e8bac08bb9e761b724ed1e867184 / D38065
    
    Test Plan: Tested out k8s universe creation using YBA CLI
    
    Reviewers: skurapati
    
    Reviewed By: skurapati
    
    Subscribers: yugaware
    
    Differential Revision: https://phorge.dev.yugabyte.com/D38076

--- managed/yba-cli/cmd/util/util.go
+++ managed/yba-cli/cmd/util/util.go
@@ -133,6 +133,24 @@ func CreateSingletonList(in interface{}) []interface{} {
 	return []interface{}{in}
 }
 
+// GetFloat64SliceFromString returns a slice of float64 from a string
+func GetFloat64SliceFromString(in string) ([]float64, error) {
+	if in == "" {
+		return nil, nil
+	}
+	in = strings.Trim(in, "[ ]")
+	s := strings.Split(in, ",")
+	var out []float64
+	for _, v := range s {
+		f, err := strconv.ParseFloat(v, 64)
+		if err != nil {
+			return nil, err
+		}
+		out = append(out, f)
+	}
+	return out, nil
+}
+
 // YbaStructuredError is a structure mimicking YBPError, with error being an interface{}
 // to accomodate errors thrown as YBPStructuredError
 type YbaStructuredError struct {
