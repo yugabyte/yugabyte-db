5c623b9d8339028724fae92986162f24ae2cb126
timothy-e <timothy.elgersma@gmail.com>
2024-09-19 09:47:10 -0400

    [pg15] test: Fix yb_bitmap_scan_joins regress test
    Summary:
    Upstream PG's 3d351d916b20534f973eda760cde17d96545d4c4 set the default reltuple count to -1. If reltuples is -1, we don't know how many tuples there are. If reltuples >= 0, we assume that is a reasonable estimate. `pg_yb_tablegroup` has its reltuples set to 0 by default, because that's how many rows are in the table.
    
    If the planner expects that a scan will read 100% of the tuples, it does not allow a bitmap scan. This case was hit in this test. reltuples = 0 was clamped to reltuples = 1. The planner expected 1 row returned, which was the entire table, so bitmap scans were disallowed.
    
    Creating two tablegroups and analyzing the table (updating the reltuples count in pg_class) allows the planner to use a bitmap scan again.
    
    Test Plan:
    Jenkins: test regex: .*testPgRegressYbBitmapScans.*
    
    ```
    pg15_tests/test_yb_bitmap_scans.sh
    ./yb_build.sh  --java-test 'org.yb.pgsql.TestPgRegressYbBitmapScans#testPgRegressYbBitmapScans'
    ```
    
    Now has only `yb_bitmap_scans_system` failing.
    
    ```
    [ERROR] Failures:
    [ERROR]   TestPgRegressYbBitmapScans.testPgRegressYbBitmapScans:31->BasePgRegressTest.runPgRegressTest:64->BasePgRegressTest.runPgRegressTest:58->BasePgRegressTest.runPgRegressTest:48 pg_regress exited with error code: 1, failed tests: [yb_bitmap_scans_system]
    [INFO]
    [ERROR] Tests run: 1, Failures: 1, Errors: 0, Skipped: 0
    ```
    
    Reviewers: jason, amartsinchyk
    
    Reviewed By: jason
    
    Subscribers: yql
    
    Differential Revision: https://phorge.dev.yugabyte.com/D38137

--- pg15_tests/test_yb_bitmap_scans.sh
+++ pg15_tests/test_yb_bitmap_scans.sh
@@ -3,26 +3,130 @@ source "${BASH_SOURCE[0]%/*}"/common.sh
 
 failing_java_test TestPgRegressYbBitmapScans
 grep_in_java_test \
-  "failed tests: [yb_bitmap_scans_joins, yb_bitmap_scans_system]" \
+  "failed tests: [yb_bitmap_scans_system]" \
   TestPgRegressYbBitmapScans
-diff <(diff build/latest/postgres_build/src/test/regress/{expected,results}/yb_bitmap_scans_joins.out | tail -n +2) - <<EOT
-<                                      QUERY PLAN
-< -------------------------------------------------------------------------------------
-<  Nested Loop Left Join (actual rows=1 loops=1)
-<    ->  Function Scan on yb_table_properties p (actual rows=1 loops=1)
-<    ->  YB Bitmap Table Scan on pg_yb_tablegroup gr (actual rows=0 loops=1)
-<          ->  Bitmap Index Scan on pg_yb_tablegroup_oid_index (actual rows=0 loops=1)
-<                Index Cond: (oid = p.tablegroup_oid)
-< (5 rows)
+diff <(diff build/latest/postgres_build/src/test/regress/{expected,results}/yb_bitmap_scans_system.out | tail -n +2) - <<EOT
+<                                              QUERY PLAN
+< ----------------------------------------------------------------------------------------------------
+<  Sort (actual rows=11 loops=1)
 ---
->                                         QUERY PLAN
-> -------------------------------------------------------------------------------------------
->  Merge Left Join (actual rows=1 loops=1)
->    Merge Cond: (p.tablegroup_oid = gr.oid)
->    ->  Sort (actual rows=1 loops=1)
->          Sort Key: p.tablegroup_oid
->          Sort Method: quicksort
->          ->  Function Scan on yb_table_properties p (actual rows=1 loops=1)
->    ->  Index Scan using pg_yb_tablegroup_oid_index on pg_yb_tablegroup gr (never executed)
-> (7 rows)
+>                                                               QUERY PLAN
+> --------------------------------------------------------------------------------------------------------------------------------------
+>  Sort (actual rows=15 loops=1)
+14c14
+<    ->  YB Bitmap Table Scan on pg_authid (actual rows=11 loops=1)
+---
+>    ->  YB Bitmap Table Scan on pg_authid (actual rows=15 loops=1)
+16,18c16,19
+<          ->  BitmapOr (actual rows=11 loops=1)
+<                ->  Bitmap Index Scan on pg_authid_rolname_index (actual rows=8 loops=1)
+<                      Index Cond: ((rolname >= 'pg'::name) AND (rolname < 'ph'::name))
+---
+>          Recheck Cond: (((rolname >= 'pg'::text) AND (rolname < 'ph'::text)) OR ((rolname >= 'yb'::text) AND (rolname < 'yc'::text)))
+>          ->  BitmapOr (actual rows=15 loops=1)
+>                ->  Bitmap Index Scan on pg_authid_rolname_index (actual rows=15 loops=1)
+>                      Index Cond: ((rolname >= 'pg'::text) AND (rolname < 'ph'::text))
+20,21c21,22
+<                ->  Bitmap Index Scan on pg_authid_rolname_index (actual rows=3 loops=1)
+<                      Index Cond: ((rolname >= 'yb'::name) AND (rolname < 'yc'::name))
+---
+>                ->  Bitmap Index Scan on pg_authid_rolname_index (actual rows=15 loops=1)
+>                      Index Cond: ((rolname >= 'yb'::text) AND (rolname < 'yc'::text))
+23c24
+< (12 rows)
+---
+> (13 rows)
+27,40c28,45
+<           rolname          | rolsuper | rolinherit | rolcreaterole | rolcreatedb | rolcanlogin | rolreplication | rolbypassrls | rolconnlimit | rolpassword | rolvaliduntil
+< ---------------------------+----------+------------+---------------+-------------+-------------+----------------+--------------+--------------+-------------+---------------
+<  pg_execute_server_program | f        | t          | f             | f           | f           | f              | f            |           -1 |             |
+<  pg_monitor                | f        | t          | f             | f           | f           | f              | f            |           -1 |             |
+<  pg_read_all_settings      | f        | t          | f             | f           | f           | f              | f            |           -1 |             |
+<  pg_read_all_stats         | f        | t          | f             | f           | f           | f              | f            |           -1 |             |
+<  pg_read_server_files      | f        | t          | f             | f           | f           | f              | f            |           -1 |             |
+<  pg_signal_backend         | f        | t          | f             | f           | f           | f              | f            |           -1 |             |
+<  pg_stat_scan_tables       | f        | t          | f             | f           | f           | f              | f            |           -1 |             |
+<  pg_write_server_files     | f        | t          | f             | f           | f           | f              | f            |           -1 |             |
+<  yb_db_admin               | f        | f          | f             | f           | f           | f              | f            |           -1 |             |
+<  yb_extension              | f        | t          | f             | f           | f           | f              | f            |           -1 |             |
+<  yb_fdw                    | f        | t          | f             | f           | f           | f              | f            |           -1 |             |
+< (11 rows)
+---
+>  oid  |          rolname          | rolsuper | rolinherit | rolcreaterole | rolcreatedb | rolcanlogin | rolreplication | rolbypassrls | rolconnlimit | rolpassword | rolvaliduntil
+> ------+---------------------------+----------+------------+---------------+-------------+-------------+----------------+--------------+--------------+-------------+---------------
+>  4544 | pg_checkpoint             | f        | t          | f             | f           | f           | f              | f            |           -1 |             |
+>  6171 | pg_database_owner         | f        | t          | f             | f           | f           | f              | f            |           -1 |             |
+>  4571 | pg_execute_server_program | f        | t          | f             | f           | f           | f              | f            |           -1 |             |
+>  3373 | pg_monitor                | f        | t          | f             | f           | f           | f              | f            |           -1 |             |
+>  6181 | pg_read_all_data          | f        | t          | f             | f           | f           | f              | f            |           -1 |             |
+>  3374 | pg_read_all_settings      | f        | t          | f             | f           | f           | f              | f            |           -1 |             |
+>  3375 | pg_read_all_stats         | f        | t          | f             | f           | f           | f              | f            |           -1 |             |
+>  4569 | pg_read_server_files      | f        | t          | f             | f           | f           | f              | f            |           -1 |             |
+>  4200 | pg_signal_backend         | f        | t          | f             | f           | f           | f              | f            |           -1 |             |
+>  3377 | pg_stat_scan_tables       | f        | t          | f             | f           | f           | f              | f            |           -1 |             |
+>  6182 | pg_write_all_data         | f        | t          | f             | f           | f           | f              | f            |           -1 |             |
+>  4570 | pg_write_server_files     | f        | t          | f             | f           | f           | f              | f            |           -1 |             |
+>  8039 | yb_db_admin               | f        | f          | f             | f           | f           | f              | f            |           -1 |             |
+>  8030 | yb_extension              | f        | t          | f             | f           | f           | f              | f            |           -1 |             |
+>  8032 | yb_fdw                    | f        | t          | f             | f           | f           | f              | f            |           -1 |             |
+> (15 rows)
+44,45c49,50
+<                                                               QUERY PLAN
+< --------------------------------------------------------------------------------------------------------------------------------------
+---
+>                                                                                QUERY PLAN
+> ------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+50c55
+<      ->  YB Bitmap Table Scan on pg_authid (actual rows=12 loops=1)
+---
+>      ->  YB Bitmap Table Scan on pg_authid (actual rows=16 loops=1)
+52c57,58
+<            ->  BitmapOr (actual rows=12 loops=1)
+---
+>            Recheck Cond: ((rolname = 'postgres'::name) OR ((rolname >= 'pg'::text) AND (rolname < 'ph'::text)) OR ((rolname >= 'yb'::text) AND (rolname < 'yc'::text)))
+>            ->  BitmapOr (actual rows=16 loops=1)
+55,61c61,67
+<                  ->  Bitmap Index Scan on pg_authid_rolname_index (actual rows=8 loops=1)
+<                        Index Cond: ((rolname >= 'pg'::name) AND (rolname < 'ph'::name))
+<                        Storage Index Filter: ((rolname = 'postgres'::name) OR (rolname ~~ 'pg_%'::text) OR (rolname ~~ 'yb_%'::text))
+<                  ->  Bitmap Index Scan on pg_authid_rolname_index (actual rows=3 loops=1)
+<                        Index Cond: ((rolname >= 'yb'::name) AND (rolname < 'yc'::name))
+<                        Storage Index Filter: ((rolname = 'postgres'::name) OR (rolname ~~ 'pg_%'::text) OR (rolname ~~ 'yb_%'::text))
+< (15 rows)
+---
+>                  ->  Bitmap Index Scan on pg_authid_rolname_index (actual rows=16 loops=1)
+>                        Index Cond: ((rolname >= 'pg'::text) AND (rolname < 'ph'::text))
+>                        Storage Index Filter: ((rolsuper = 'postgres'::name) OR (rolsuper ~~ 'pg_%'::text) OR (rolsuper ~~ 'yb_%'::text))
+>                  ->  Bitmap Index Scan on pg_authid_rolname_index (actual rows=16 loops=1)
+>                        Index Cond: ((rolname >= 'yb'::text) AND (rolname < 'yc'::text))
+>                        Storage Index Filter: ((rolsuper = 'postgres'::name) OR (rolsuper ~~ 'pg_%'::text) OR (rolsuper ~~ 'yb_%'::text))
+> (16 rows)
+72,73c78,79
+<                                                  QUERY PLAN
+< ------------------------------------------------------------------------------------------------------------
+---
+>                                                                                QUERY PLAN
+> ------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+78c84,86
+<      ->  YB Bitmap Table Scan on pg_authid (actual rows=12 loops=1)
+---
+>      ->  YB Bitmap Table Scan on pg_authid (actual rows=16 loops=1)
+>            Recheck Cond: ((rolname = 'postgres'::name) OR ((rolname >= 'pg'::text) AND (rolname < 'ph'::text)) OR ((rolname >= 'yb'::text) AND (rolname < 'yc'::text)))
+>            Rows Removed by Index Recheck: 2
+80c88
+<            ->  BitmapOr (actual rows=12 loops=1)
+---
+>            ->  BitmapOr (actual rows=18 loops=1)
+83,87c91,95
+<                  ->  Bitmap Index Scan on pg_authid_rolname_index (actual rows=8 loops=1)
+<                        Index Cond: ((rolname >= 'pg'::name) AND (rolname < 'ph'::name))
+<                  ->  Bitmap Index Scan on pg_authid_rolname_index (actual rows=3 loops=1)
+<                        Index Cond: ((rolname >= 'yb'::name) AND (rolname < 'yc'::name))
+< (13 rows)
+---
+>                  ->  Bitmap Index Scan on pg_authid_rolname_index (actual rows=18 loops=1)
+>                        Index Cond: ((rolname >= 'pg'::text) AND (rolname < 'ph'::text))
+>                  ->  Bitmap Index Scan on pg_authid_rolname_index (actual rows=18 loops=1)
+>                        Index Cond: ((rolname >= 'yb'::text) AND (rolname < 'yc'::text))
+> (15 rows)
 EOT
