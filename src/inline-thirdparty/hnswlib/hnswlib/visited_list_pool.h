9a819f76b9b7c992256de40438bab235e6d2b817
Mikhail Bautin <552936+mbautin@users.noreply.github.com>
2024-09-21 01:09:10 -0700

    Automatic commit by thirdparty_tool: update hnswlib to commit 2142dc6f4dd08e64ab727a7bbd93be7f732e80b0.
    Used commit of the hnswlib repository: https://github.com/nmslib/hnswlib/commits/2142dc6f4dd08e64ab727a7bbd93be7f732e80b0

--- src/inline-thirdparty/hnswlib/hnswlib/visited_list_pool.h
+++ src/inline-thirdparty/hnswlib/hnswlib/visited_list_pool.h
@@ -76,3 +76,9 @@ class VisitedListPool {
     }
 };
 }  // namespace hnswlib
+
+// This file is part of the hnswlib inline third-party dependency of YugabyteDB.
+// Git repo: https://github.com/nmslib/hnswlib
+// Git commit: 2142dc6f4dd08e64ab727a7bbd93be7f732e80b0
+//
+// See also src/inline-thirdparty/README.md.
