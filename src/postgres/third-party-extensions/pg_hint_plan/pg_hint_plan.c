yugabyte/yugabyte-db

commit 5523770fcd6e4a1a45b7714203992a17efcde983

[#23547] YSQL: fix pg_hint_plan crash with pg_hint_plan.enable_hint_table enabled
Summary:
When hint_table is enabled, pg_hint_plan looks up the hint_table using the the current query string as the key, which is done in the extension callbacks: `post_parse_analyze_hook` and `planner_hook`.

The query string is available in the `ParseState` when it is in `post_parse_analyze_hook` as it is passed down as an argument.

On the other hand, `planner_hook` in **pg11** does not have access to the current query string, and `get_current_hint_string` was assuming that it's processing the top-level query.  However, that is not always the case, and it would crash while normalizing the query if the source location of some `Const` nodes in the query tree is beyond the end of the query string.

The solution is not to use the top-level query string if `ParseState` is unavailable and `stmt_location` and `stmt_len` in the Query node indicate that it may not be the top-level query but a subquery fragment or a query in a stored procedure.

**pg15 note: the change to `pg_hint_plan.c` is not applicable to the pg15 variant because the planner_hook receives the correct query string along with the parse tree.  No `get_current_hint_string` function there.**
Jira: DB-12465

Test Plan:
  ./yb_build.sh --java-test 'org.yb.pgsql.TestPgRegressThirdPartyExtensionsPgHintPlan'

Jenkins: test regex: .*TestPgRegressThirdPartyExtensionsPgHintPlan

Reviewers: tnayak

Reviewed By: tnayak

Subscribers: smishra, fizaa, yql

Differential Revision: https://phorge.dev.yugabyte.com/D38018

src/postgres/third-party-extensions/pg_hint_plan/pg_hint_plan.c
@@ -1931,6 +1931,12 @@ get_query_string(ParseState *pstate, Query *query, Query **jumblequery)
 	else if (!jumblequery && pstate && pstate->p_sourcetext != p &&
 			 strcmp(pstate->p_sourcetext, p) != 0)
 		p = NULL;
+	/*
+	 * YB note: don't assume it is the top-level query when pstate is NULL and
+	 * the query tree does not have the source location.
+	 */
+	else if (!pstate && query->stmt_location <= 0 && query->stmt_len <= 0)
+		p = NULL;
 
 	return p;
 }

src/postgres/third-party-extensions/pg_hint_plan/expected/yb_hint_table.out
@@ -1,0 +1,10 @@
+CREATE EXTENSION pg_hint_plan;
+SET pg_hint_plan.enable_hint_table TO on;
+-- #23547 Ensure query against a view containing stored function doesn't crash
+SELECT numeric_precision FROM information_schema.columns
+    WHERE table_schema = 'pg_catalog' AND table_name = 'pg_class'
+          AND column_name = 'relname';
+ numeric_precision
+-------------------
+
+(1 row)

src/postgres/third-party-extensions/pg_hint_plan/sql/yb_hint_table.sql
@@ -1,0 +1,7 @@
+CREATE EXTENSION pg_hint_plan;
+SET pg_hint_plan.enable_hint_table TO on;
+
+-- #23547 Ensure query against a view containing stored function doesn't crash
+SELECT numeric_precision FROM information_schema.columns
+    WHERE table_schema = 'pg_catalog' AND table_name = 'pg_class'
+          AND column_name = 'relname';

src/postgres/third-party-extensions/pg_hint_plan/yb_schedule
@@ -2,3 +2,4 @@
 
 test: yb_pg_init
 test: yb_pg_pg_hint_plan
+test: yb_hint_table

commit ed153dc18c256eae4e483b6dc78c439b43a5302e

[#24055] YSQL: fix pg_hint_plan regression with executing prepared statement
Summary:
Fix a regression introduced in 5523770fcd6e4a1a45b7714203992a17efcde983 where the hints were missed out when the query was recompiled without going through semantic analysis again.

Move the query string - query tree consistency check to the hint_table specific code.  In the other case, the hints are directly taken from the query string without using the location info in the query tree hence the mismatch is harmless.

**pg15: there's nothing to merge/port to the pg15 branch.**
Jira: DB-12947

Test Plan:
  ./yb_build.sh --java-test 'org.yb.pgsql.TestPgMisc#testPgHintPlanExtendedQuery'
  ./yb_build.sh --java-test 'org.yb.pgsql.TestYbBackup#testPgHintPlan'
  ./yb_build.sh --java-test 'org.yb.pgsql.TestPgRegressThirdPartyExtensionsPgHintPlan'

Jenkins: test regex: .*PgHintPlan.*

Reviewers: tnayak

Reviewed By: tnayak

Subscribers: smishra, yql

Differential Revision: https://phorge.dev.yugabyte.com/D38265

src/postgres/third-party-extensions/pg_hint_plan/pg_hint_plan.c
@@ -1931,12 +1931,6 @@ get_query_string(ParseState *pstate, Query *query, Query **jumblequery)
 	else if (!jumblequery && pstate && pstate->p_sourcetext != p &&
 			 strcmp(pstate->p_sourcetext, p) != 0)
 		p = NULL;
-	/*
-	 * YB note: don't assume it is the top-level query when pstate is NULL and
-	 * the query tree does not have the source location.
-	 */
-	else if (!pstate && query->stmt_location <= 0 && query->stmt_len <= 0)
-		p = NULL;
 
 	return p;
 }
@@ -2900,6 +2894,14 @@ get_current_hint_string(ParseState *pstate, Query *query)
 		char		   *normalized_query = NULL;
 
 		query_str = get_query_string(pstate, query, &jumblequery);
+
+		/*
+		 * YB note: don't assume it is the top-level query when pstate is NULL
+		 * and the query tree does not have the source location.
+		 */
+		if (!pstate && query->stmt_location <= 0 && query->stmt_len <= 0 &&
+			query_str == debug_query_string)
+			query_str = NULL;
 
 		/* If this query is not for hint, just return */
 		if (!query_str)