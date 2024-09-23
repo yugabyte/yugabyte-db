yugabyte/yugabyte-db

commit a2aa0d3140ec04ea777877de0df2e3ba978913a6

[#11463] YSQL: Pushdown RowComparisonExpression filters
Summary:
Queries with a RowComparionExpression filter such as

SELECT * FROM ttt WHERE (r1,r2,r3) <= (7,0,32);

Where (r1,r2,r2) make up the entire primary key of ttt and comprise purely of range keys currently do not push down the filter. If the LHS of the filter here corresponds to the makeup and format of the DocKey for the index/table being accessed we can push the bounds on the LHS as partition bounds.

This diff aims to do so by identifying such row comparison expression filters and parsing them to create overall upper and lower DocKey scan bounds for a query. These DocKey bounds are loaded into the upper_bound and lower_bound fields in the pgsql_protocol RPC protobuf.

On the DocDB iteration side, this diff adds support for using given upper bounds in table iteration.

Right now, this diff does not support row comparison pushdown for tables that have hash keys.

Test Plan: ./yb_build.sh release --java-test 'org.yb.pgsql.TestPgRegressIndex'

Reviewers: mihnea, amartsinchyk

Reviewed By: amartsinchyk

Subscribers: zyu, smishra, yql

Differential Revision: https://phabricator.dev.yugabyte.com/D15429

src/postgres/src/backend/access/yb_access/yb_scan.c
@@ -917,6 +917,164 @@ ybcBindScanKeys(YbScanDesc ybScan, YbScanPlan scan_plan) {
 		/* Check if this is primary columns */
 		int bind_key_attnum = scan_plan->bind_key_attnums[i];
 		int idx = YBAttnumToBmsIndex(relation, bind_key_attnum);
+		/* Check if this is full key row comparison expression */
+		if (ybScan->key[i].sk_flags & SK_ROW_HEADER)
+		{
+			int j = 0;
+			ScanKey subkeys = (ScanKey) 
+					DatumGetPointer(ybScan->key[i].sk_argument);
+			int last_att_no = YBFirstLowInvalidAttributeNumber;
+
+			/* 
+				* We can only push down right now if the full primary key
+				* is specified in the correct order and the primary key 
+				* has no hashed columns. We also need to ensure that 
+				* the same comparison operation is done to all subkeys.
+				*/
+			bool can_pushdown = true;
+
+			int strategy = subkeys[0].sk_strategy;
+			int count = 0;
+			do {
+				ScanKey current = &subkeys[j];
+				/* Make sure that the specified keys are in the right order. */
+				if (!(current->sk_attno > last_att_no))
+				{
+					can_pushdown = false;
+					break;
+				}
+
+				/*
+					* Make sure that the same comparator is applied to
+					* all subkeys.
+					*/
+				if (strategy != current->sk_strategy)
+				{
+					can_pushdown = false;
+					break;
+				}
+				last_att_no = current->sk_attno;
+
+				/* Make sure that there are no hash key columns. */
+				if (index->rd_indoption[current->sk_attno - 1] 
+					& INDOPTION_HASH) {
+					can_pushdown = false;
+					break;
+				}
+				count++;
+			}
+			while((subkeys[j++].sk_flags & SK_ROW_END) == 0);
+			
+			/*
+				* Make sure that the full primary key is specified in order
+				* to push down.
+				*/
+
+			if (can_pushdown)
+			{
+
+				YBCPgExpr *col_values = palloc(sizeof(YBCPgExpr)
+											* index->rd_index->indnkeyatts);
+				/*
+					* Prepare upper/lower bound tuples determined from this
+					* clause for bind. Care must be taken in the case 
+					* that primary key columns in the index are ordered
+					* differently from each other. For example, consider
+					* if the underlying index has primary key
+					* (r1 ASC, r2 DESC, r3 ASC) and we are dealing with
+					* a clause like (r1, r2, r3) <= (40, 35, 12).
+					* We cannot simply bind (40, 35, 12) as an upper bound
+					* as that will miss tuples such as (40, 32, 0).
+					* Instead we must push down (40, Inf, 12) in this case 
+					* for correctness. (Note that +Inf in this context
+					* is higher in STORAGE order than all other values not
+					* necessarily logical order, similar to the role of
+					* docdb::ValueType::kHighest.
+					*/
+
+				/*
+					* Is the first column in ascending order in the index?
+					* This is important because whether or not the RHS of a
+					* (row key) >= (row key values) expression is
+					* considered an upper bound is dependent on the answer
+					* to this question. The RHS of such an expression will
+					* be the scan upper bound if the first column is in
+					* descending order and lower if else. Similar logic
+					* applies to the RHS of (row key) <= (row key values)
+					* expressions.
+					*/
+				bool is_direction_asc = 
+									(index->rd_indoption[
+										subkeys[0].sk_attno - 1] 
+										& INDOPTION_DESC) == 0;
+				bool gt = strategy == BTGreaterEqualStrategyNumber
+								|| strategy == BTGreaterStrategyNumber;
+				bool is_inclusive = strategy != BTGreaterStrategyNumber
+										&& strategy != BTLessStrategyNumber;
+				
+				bool is_point_scan = (count == index->rd_index->indnatts)
+										&& (strategy == BTEqualStrategyNumber);
+
+				/* Whether or not the RHS values make up a DocDB upper bound */
+				bool is_upper_bound = gt ^ is_direction_asc;
+				size_t subkey_index = 0;
+
+				for (j = 0; j < index->rd_index->indnkeyatts; j++)
+				{
+					bool is_column_specified = subkey_index < count
+											&& (subkeys[subkey_index]
+												.sk_attno - 1) == j;
+					/*
+						* Is the current column stored in ascending order in the
+						* underlying index?
+						*/
+					bool asc = (index->rd_indoption[j] & INDOPTION_DESC) == 0;
+
+					/*
+						* If this column has different directionality than the
+						* first column then we have to adjust the bounds on this
+						* column.
+						*/
+					if(!is_column_specified
+						|| (asc != is_direction_asc &&
+							!is_point_scan))
+					{
+						col_values[j] = NULL;
+					}
+					else
+					{
+						ScanKey current = &subkeys[subkey_index];
+						col_values[j] = YBCNewConstant(ybScan->handle,
+											ybc_get_atttypid
+												(scan_plan->bind_desc,
+											current->sk_attno),
+											current->sk_collation,
+											current->sk_argument,
+											false);
+					}
+
+					if (is_column_specified)
+					{
+						subkey_index++;
+					}
+				}
+
+				if (is_upper_bound || strategy == BTEqualStrategyNumber)
+				{
+					HandleYBStatus(YBCPgDmlAddRowUpperBound(ybScan->handle,
+												index->rd_index->indnkeyatts,
+												col_values, is_inclusive));
+				}
+
+				if (!is_upper_bound || strategy == BTEqualStrategyNumber)
+				{
+					HandleYBStatus(YBCPgDmlAddRowLowerBound(ybScan->handle,
+												index->rd_index->indnkeyatts,
+												col_values, is_inclusive));
+				}
+			}
+			continue;
+		}
 		if (!IsHashCodeSearch(ybScan->key[i].sk_flags)
 			&& !bms_is_member(idx, scan_plan->sk_cols))
 			continue;

src/postgres/src/test/regress/sql/yb_index_scan.sql
@@ -302,3 +302,129 @@ SELECT * FROM pk_int WHERE c0 <= ALL(ARRAY[3, 4]);
 SELECT * FROM pk_int WHERE c0 = ALL(ARRAY[3, 4]);
 SELECT * FROM pk_int WHERE c0 >= ALL(ARRAY[3, 4]);
 SELECT * FROM pk_int WHERE c0 > ALL(ARRAY[3, 4]);
+
+-- test row comparison expressions
+CREATE TABLE pk_range_int_asc (r1 INT, r2 INT, r3 INT, v INT, PRIMARY KEY(r1 asc, r2 asc, r3 asc));
+INSERT INTO pk_range_int_asc SELECT i/25, (i/5) % 5, i % 5, i FROM generate_series(1, 125) AS i;
+EXPLAIN (COSTS OFF, TIMING OFF, SUMMARY OFF, ANALYZE) SELECT * FROM pk_range_int_asc WHERE (r1, r2, r3) <= (2,3,2);
+SELECT * FROM pk_range_int_asc WHERE (r1, r2, r3) <= (2,3,2);
+EXPLAIN (COSTS OFF, TIMING OFF, SUMMARY OFF, ANALYZE) SELECT * FROM pk_range_int_asc WHERE (r1, r2, v, r3) <= (2,3,60,1);
+SELECT * FROM pk_range_int_asc WHERE (r1, r2, v, r3) <= (2,3,60,1);
+EXPLAIN (COSTS OFF, TIMING OFF, SUMMARY OFF, ANALYZE) SELECT * FROM pk_range_int_asc WHERE (r1, r2, r3) < (2,3,2);
+SELECT * FROM pk_range_int_asc WHERE (r1, r2, r3) < (2,3,2);
+EXPLAIN (COSTS OFF, TIMING OFF, SUMMARY OFF, ANALYZE) SELECT * FROM pk_range_int_asc WHERE (r1, r2, r3) <= (3,3,2);
+SELECT * FROM pk_range_int_asc WHERE (r1, r2, r3) <= (3,3,2);
+EXPLAIN (COSTS OFF, TIMING OFF, SUMMARY OFF, ANALYZE) SELECT * FROM pk_range_int_asc WHERE (r1, r2, r3) >= (3,3,2);
+SELECT * FROM pk_range_int_asc WHERE (r1, r2, r3) >= (3,3,2);
+EXPLAIN (COSTS OFF, TIMING OFF, SUMMARY OFF, ANALYZE) SELECT * FROM pk_range_int_asc WHERE (r1, r2, r3) > (3,3,2);
+SELECT * FROM pk_range_int_asc WHERE (r1, r2, r3) > (3,3,2);
+EXPLAIN (COSTS OFF, TIMING OFF, SUMMARY OFF, ANALYZE) SELECT * FROM pk_range_int_asc WHERE (r1, r2, r3) >= (1,4,5) AND (r1, r2, r3) <= (2,4,5);
+SELECT * FROM pk_range_int_asc WHERE (r1, r2, r3) >= (1,4,5) AND (r1, r2, r3) <= (2,4,5);
+EXPLAIN (COSTS OFF, TIMING OFF, SUMMARY OFF, ANALYZE) SELECT * FROM pk_range_int_asc WHERE (r1, r2, r3) >= (1,2,3) AND (r1, r2, r3) <= (1,3,2) AND r3 IN (3,2,6);
+SELECT * FROM pk_range_int_asc WHERE (r1, r2, r3) >= (1,2,3) AND (r1, r2, r3) <= (1,3,2) AND r3 IN (3,2,6);
+EXPLAIN (COSTS OFF, TIMING OFF, SUMMARY OFF, ANALYZE) SELECT * FROM pk_range_int_asc WHERE (r1, r2, r3) >= (1,1,5) AND (r1, r2, r3) <= (1,4,5) ORDER BY r1 DESC, r2 DESC, r3 DESC;
+SELECT * FROM pk_range_int_asc WHERE (r1, r2, r3) >= (1,1,5) AND (r1, r2, r3) <= (1,4,5) ORDER BY r1 DESC, r2 DESC, r3 DESC;
+EXPLAIN (COSTS OFF, TIMING OFF, SUMMARY OFF, ANALYZE) SELECT * FROM pk_range_int_asc WHERE (r1, r2, r3) = (1,6,5);
+SELECT * FROM pk_range_int_asc WHERE (r1, r2, r3) = (1,6,5);
+EXPLAIN (COSTS OFF, TIMING OFF, SUMMARY OFF, ANALYZE) SELECT * FROM pk_range_int_asc WHERE (r1, r2, r3) <= (1,6,5) AND (r1,r2,r3) < (1,6,5);
+SELECT * FROM pk_range_int_asc WHERE (r1, r2, r3) <= (1,6,5) AND (r1,r2,r3) < (1,6,5);
+EXPLAIN (COSTS OFF, TIMING OFF, SUMMARY OFF, ANALYZE) SELECT * FROM pk_range_int_asc WHERE (r1, r2, r3) <= (1,1,5) AND (r1,r2,r3) < (1,2,4) AND (r1,r2,r3) > (1,2,3) AND (r1,r2,r3) < (1,2,4) AND (r1,r2,r3) >= (1,2,3);
+SELECT * FROM pk_range_int_asc WHERE (r1, r2, r3) <= (1,1,5) AND (r1,r2,r3) < (1,2,4) AND (r1,r2,r3) > (1,2,3) AND (r1,r2,r3) < (1,2,4) AND (r1,r2,r3) >= (1,2,3);
+EXPLAIN (COSTS OFF, TIMING OFF, SUMMARY OFF, ANALYZE) SELECT * FROM pk_range_int_asc WHERE (r1, r3) <= (1,3) AND (r1,r2) < (1,3) AND (r1,r2) >= (1,2);
+SELECT * FROM pk_range_int_asc WHERE (r1, r3) <= (1,3) AND (r1,r2) < (1,3) AND (r1,r2) >= (1,2);
+EXPLAIN (COSTS OFF, TIMING OFF, SUMMARY OFF, ANALYZE) SELECT * FROM pk_range_int_asc WHERE (r1, r3) <= (1,3) AND (r1,r2) < (1,3) AND (r1,r2) >= (1,2) AND (r1,r2,r3) = (1,2,3);
+SELECT * FROM pk_range_int_asc WHERE (r1, r3) <= (1,3) AND (r1,r2) < (1,3) AND (r1,r2) >= (1,2) AND (r1,r2,r3) = (1,2,3);
+DROP TABLE pk_range_int_asc;
+
+-- test row comparison expressions where we have differing column orderings
+CREATE TABLE pk_range_asc_desc_asc (r1 BIGINT, r2 INT, r3 INT, v INT, PRIMARY KEY(r1 asc, r2 desc, r3 asc));
+INSERT INTO pk_range_asc_desc_asc SELECT i/25, (i/5) % 5, i % 5, i FROM generate_series(1, 125) AS i;
+EXPLAIN (COSTS OFF, TIMING OFF, SUMMARY OFF, ANALYZE) SELECT * FROM pk_range_asc_desc_asc WHERE (r1, r2, r3) <= (2,3,2);
+SELECT * FROM pk_range_asc_desc_asc WHERE (r1, r2, r3) <= (2,3,2);
+EXPLAIN (COSTS OFF, TIMING OFF, SUMMARY OFF, ANALYZE) SELECT * FROM pk_range_asc_desc_asc WHERE (r1, r2, v, r3) <= (2,3,60,1);
+SELECT * FROM pk_range_asc_desc_asc WHERE (r1, r2, v, r3) <= (2,3,60,1);
+EXPLAIN (COSTS OFF, TIMING OFF, SUMMARY OFF, ANALYZE) SELECT * FROM pk_range_asc_desc_asc WHERE (r1, r2, r3) >= (1,7,2);
+SELECT * FROM pk_range_asc_desc_asc WHERE (r1, r2, r3) >= (1,7,2);
+EXPLAIN (COSTS OFF, TIMING OFF, SUMMARY OFF, ANALYZE) SELECT * FROM pk_range_asc_desc_asc WHERE (r1, r2, r3) > (8,7,3);
+SELECT * FROM pk_range_asc_desc_asc WHERE (r1, r2, r3) > (8,7,3);
+EXPLAIN (COSTS OFF, TIMING OFF, SUMMARY OFF, ANALYZE) SELECT * FROM pk_range_asc_desc_asc WHERE (r1, r2, r3) >= (1,7,2) AND (r1, r2, r3) <= (3,2,1);
+SELECT * FROM pk_range_asc_desc_asc WHERE (r1, r2, r3) >= (1,7,2) AND (r1, r2, r3) <= (3,2,1);
+EXPLAIN (COSTS OFF, TIMING OFF, SUMMARY OFF, ANALYZE) SELECT * FROM pk_range_asc_desc_asc WHERE (r1, r2, r3) >= (1,7,2) AND (r1, r2, r3) <= (3,2,1) ORDER BY r1 DESC, r2 ASC, r3 DESC;
+SELECT * FROM pk_range_asc_desc_asc WHERE (r1, r2, r3) >= (1,7,2) AND (r1, r2, r3) <= (3,2,1) ORDER BY r1 DESC, r2 ASC, r3 DESC;
+EXPLAIN (COSTS OFF, TIMING OFF, SUMMARY OFF, ANALYZE) SELECT * FROM pk_range_asc_desc_asc WHERE (r1, r3) <= (1,3) AND (r1,r2) < (1,3) AND (r1,r2) >= (1,2);
+SELECT * FROM pk_range_asc_desc_asc WHERE (r1, r3) <= (1,3) AND (r1,r2) < (1,3) AND (r1,r2) >= (1,2);
+EXPLAIN (COSTS OFF, TIMING OFF, SUMMARY OFF, ANALYZE) SELECT * FROM pk_range_asc_desc_asc WHERE (r1, r3) <= (1,3) AND (r1,r2) < (1,3) AND (r1,r2) >= (1,2) AND (r1,r2,r3) = (1,2,3);
+SELECT * FROM pk_range_asc_desc_asc WHERE (r1, r3) <= (1,3) AND (r1,r2) < (1,3) AND (r1,r2) >= (1,2) AND (r1,r2,r3) = (1,2,3);
+DROP TABLE pk_range_asc_desc_asc;
+
+CREATE TABLE pk_range_desc_asc_desc (r1 BIGINT, r2 INT, r3 INT, v INT, PRIMARY KEY(r1 desc, r2 asc, r3 desc));
+INSERT INTO pk_range_desc_asc_desc SELECT i/25, (i/5) % 5, i % 5, i FROM generate_series(1, 125) AS i;
+EXPLAIN (COSTS OFF, TIMING OFF, SUMMARY OFF, ANALYZE) SELECT * FROM pk_range_desc_asc_desc WHERE (r1, r2, r3) <= (2,3,2);
+SELECT * FROM pk_range_desc_asc_desc WHERE (r1, r2, r3) <= (2,3,2);
+EXPLAIN (COSTS OFF, TIMING OFF, SUMMARY OFF, ANALYZE) SELECT * FROM pk_range_desc_asc_desc WHERE (r1, r2, v, r3) <= (2,3,60,1);
+SELECT * FROM pk_range_desc_asc_desc WHERE (r1, r2, v, r3) <= (2,3,60,1);
+EXPLAIN (COSTS OFF, TIMING OFF, SUMMARY OFF, ANALYZE) SELECT * FROM pk_range_desc_asc_desc WHERE (r1, r2, r3) >= (1,7,2);
+SELECT * FROM pk_range_desc_asc_desc WHERE (r1, r2, r3) >= (1,7,2);
+EXPLAIN (COSTS OFF, TIMING OFF, SUMMARY OFF, ANALYZE) SELECT * FROM pk_range_desc_asc_desc WHERE (r1, r2, r3) >= (1,7,2) AND (r1, r2, r3) <= (3,2,1);
+SELECT * FROM pk_range_desc_asc_desc WHERE (r1, r2, r3) >= (1,7,2) AND (r1, r2, r3) <= (3,2,1);
+EXPLAIN (COSTS OFF, TIMING OFF, SUMMARY OFF, ANALYZE) SELECT * FROM pk_range_desc_asc_desc WHERE (r1, r2, r3) >= (1,7,2) AND (r1, r2, r3) <= (3,2,1) ORDER BY r1 DESC, r2 ASC, r3 DESC;
+SELECT * FROM pk_range_desc_asc_desc WHERE (r1, r2, r3) >= (1,7,2) AND (r1, r2, r3) <= (3,2,1) ORDER BY r1 DESC, r2 ASC, r3 DESC;
+EXPLAIN (COSTS OFF, TIMING OFF, SUMMARY OFF, ANALYZE) SELECT * FROM pk_range_desc_asc_desc WHERE (r1, r3) <= (1,3) AND (r1,r2) < (1,3) AND (r1,r2) >= (1,2);
+SELECT * FROM pk_range_desc_asc_desc WHERE (r1, r3) <= (1,3) AND (r1,r2) < (1,3) AND (r1,r2) >= (1,2);
+EXPLAIN (COSTS OFF, TIMING OFF, SUMMARY OFF, ANALYZE) SELECT * FROM pk_range_desc_asc_desc WHERE (r1, r3) <= (1,3) AND (r1,r2) < (1,3) AND (r1,r2) >= (1,2) AND (r1,r2,r3) = (1,2,3);
+SELECT * FROM pk_range_desc_asc_desc WHERE (r1, r3) <= (1,3) AND (r1,r2) < (1,3) AND (r1,r2) >= (1,2) AND (r1,r2,r3) = (1,2,3);
+DROP TABLE pk_range_desc_asc_desc;
+
+CREATE TABLE pk_range_int_desc (r1 INT, r2 INT, r3 INT, v INT, PRIMARY KEY(r1 desc, r2 desc, r3 desc));
+INSERT INTO pk_range_int_desc SELECT i/25, (i/5) % 5, i % 5, i FROM generate_series(1, 125) AS i;
+EXPLAIN (COSTS OFF, TIMING OFF, SUMMARY OFF, ANALYZE) SELECT * FROM pk_range_int_desc WHERE (r1, r2, r3) <= (2,3,2);
+SELECT * FROM pk_range_int_desc WHERE (r1, r2, r3) <= (2,3,2);
+EXPLAIN (COSTS OFF, TIMING OFF, SUMMARY OFF, ANALYZE) SELECT * FROM pk_range_int_desc WHERE (r1, r2, v, r3) <= (2,3,60,1);
+SELECT * FROM pk_range_int_desc WHERE (r1, r2, v, r3) <= (2,3,60,1);
+EXPLAIN (COSTS OFF, TIMING OFF, SUMMARY OFF, ANALYZE) SELECT * FROM pk_range_int_desc WHERE (r1, r2, r3) < (2,3,2);
+SELECT * FROM pk_range_int_desc WHERE (r1, r2, r3) < (2,3,2);
+EXPLAIN (COSTS OFF, TIMING OFF, SUMMARY OFF, ANALYZE) SELECT * FROM pk_range_int_desc WHERE (r1, r2, r3) <= (3,3,2);
+SELECT * FROM pk_range_int_desc WHERE (r1, r2, r3) <= (3,3,2);
+EXPLAIN (COSTS OFF, TIMING OFF, SUMMARY OFF, ANALYZE) SELECT * FROM pk_range_int_desc WHERE (r1, r2, r3) >= (3,3,2);
+SELECT * FROM pk_range_int_desc WHERE (r1, r2, r3) >= (3,3,2);
+EXPLAIN (COSTS OFF, TIMING OFF, SUMMARY OFF, ANALYZE) SELECT * FROM pk_range_int_desc WHERE (r1, r2, r3) > (3,3,2);
+SELECT * FROM pk_range_int_desc WHERE (r1, r2, r3) > (3,3,2);
+EXPLAIN (COSTS OFF, TIMING OFF, SUMMARY OFF, ANALYZE) SELECT * FROM pk_range_int_desc WHERE (r1, r2, r3) >= (1,4,5) AND (r1, r2, r3) <= (2,4,5);
+SELECT * FROM pk_range_int_desc WHERE (r1, r2, r3) >= (1,4,5) AND (r1, r2, r3) <= (2,4,5);
+EXPLAIN (COSTS OFF, TIMING OFF, SUMMARY OFF, ANALYZE) SELECT * FROM pk_range_int_desc WHERE (r1, r2, r3) >= (1,2,3) AND (r1, r2, r3) <= (1,3,2) AND r3 IN (3,2,6);
+SELECT * FROM pk_range_int_desc WHERE (r1, r2, r3) >= (1,2,3) AND (r1, r2, r3) <= (1,3,2) AND r3 IN (3,2,6);
+EXPLAIN (COSTS OFF, TIMING OFF, SUMMARY OFF, ANALYZE) SELECT * FROM pk_range_int_desc WHERE (r1, r2, r3) >= (1,4,5) AND (r1, r2, r3) <= (1,6,5) ORDER BY r1 ASC, r2 ASC, r3 ASC;
+SELECT * FROM pk_range_int_desc WHERE (r1, r2, r3) >= (1,4,5) AND (r1, r2, r3) <= (1,6,5) ORDER BY r1 ASC, r2 ASC, r3 ASC;
+EXPLAIN (COSTS OFF, TIMING OFF, SUMMARY OFF, ANALYZE) SELECT * FROM pk_range_int_desc WHERE (r1, r2, r3) = (1,6,5);
+SELECT * FROM pk_range_int_desc WHERE (r1, r2, r3) = (1,6,5);
+EXPLAIN (COSTS OFF, TIMING OFF, SUMMARY OFF, ANALYZE) SELECT * FROM pk_range_int_desc WHERE (r1, r2, r3) <= (1,6,5) AND (r1,r2,r3) < (1,6,5);
+SELECT * FROM pk_range_int_desc WHERE (r1, r2, r3) <= (1,6,5) AND (r1,r2,r3) < (1,6,5);
+EXPLAIN (COSTS OFF, TIMING OFF, SUMMARY OFF, ANALYZE) SELECT * FROM pk_range_int_desc WHERE (r1, r2, r3) <= (1,1,5) AND (r1,r2,r3) < (1,2,4) AND (r1,r2,r3) > (1,2,3) AND (r1,r2,r3) < (1,2,4) AND (r1,r2,r3) >= (1,2,3);
+SELECT * FROM pk_range_int_desc WHERE (r1, r2, r3) <= (1,1,5) AND (r1,r2,r3) < (1,2,4) AND (r1,r2,r3) > (1,2,3) AND (r1,r2,r3) < (1,2,4) AND (r1,r2,r3) >= (1,2,3);
+EXPLAIN (COSTS OFF, TIMING OFF, SUMMARY OFF, ANALYZE) SELECT * FROM pk_range_int_desc WHERE (r1, r3) <= (1,3) AND (r1,r2) < (1,3) AND (r1,r2) >= (1,2);
+SELECT * FROM pk_range_int_desc WHERE (r1, r3) <= (1,3) AND (r1,r2) < (1,3) AND (r1,r2) >= (1,2);
+EXPLAIN (COSTS OFF, TIMING OFF, SUMMARY OFF, ANALYZE) SELECT * FROM pk_range_int_desc WHERE (r1, r3) <= (1,3) AND (r1,r2) < (1,3) AND (r1,r2) >= (1,2) AND (r1,r2,r3) = (1,2,3);
+SELECT * FROM pk_range_int_desc WHERE (r1, r3) <= (1,3) AND (r1,r2) < (1,3) AND (r1,r2) >= (1,2) AND (r1,r2,r3) = (1,2,3);
+DROP TABLE pk_range_int_desc;
+
+CREATE TABLE pk_range_int_text (r1 INT, r2 TEXT, r3 BIGINT, v INT, PRIMARY KEY(r1 asc, r2 asc, r3 asc));
+INSERT INTO pk_range_int_text SELECT i/25, concat('abc', ((i/5) % 5)::TEXT), i % 5, i FROM generate_series(1, 125) AS i;
+EXPLAIN (COSTS OFF, TIMING OFF, SUMMARY OFF, ANALYZE) SELECT * FROM pk_range_int_text WHERE (r1, r2, r3) <= (2,'ab2'::text,2);
+SELECT * FROM pk_range_int_text WHERE (r1, r2, r3) <= (2,'ab2'::text,2);
+EXPLAIN (COSTS OFF, TIMING OFF, SUMMARY OFF, ANALYZE) SELECT * FROM pk_range_int_text WHERE (r1, r2, v, r3) <= (2,'abc3'::text,60,1);
+SELECT * FROM pk_range_int_text WHERE (r1, r2, v, r3) <= (2,'abc3'::text,60,1);
+EXPLAIN (COSTS OFF, TIMING OFF, SUMMARY OFF, ANALYZE) SELECT * FROM pk_range_int_text WHERE (r1, r2, r3) < (2,'abc3'::text,2);
+SELECT * FROM pk_range_int_text WHERE (r1, r2, r3) < (2,'abc3'::text,2);
+EXPLAIN (COSTS OFF, TIMING OFF, SUMMARY OFF, ANALYZE) SELECT * FROM pk_range_int_text WHERE (r1, r2, r3) <= (3,'abb3'::text,2);
+SELECT * FROM pk_range_int_text WHERE (r1, r2, r3) <= (3,'abb3'::text,2);
+EXPLAIN (COSTS OFF, TIMING OFF, SUMMARY OFF, ANALYZE) SELECT * FROM pk_range_int_text WHERE (r1, r2, r3) >= (3,'abc3'::text,2);
+SELECT * FROM pk_range_int_text WHERE (r1, r2, r3) >= (3,'abc3'::text,2);
+EXPLAIN (COSTS OFF, TIMING OFF, SUMMARY OFF, ANALYZE) SELECT * FROM pk_range_int_text WHERE (r1, r2, r3) > (3,'abc3'::text,2);
+SELECT * FROM pk_range_int_text WHERE (r1, r2, r3) > (3,'abc3'::text,2);
+EXPLAIN (COSTS OFF, TIMING OFF, SUMMARY OFF, ANALYZE) SELECT * FROM pk_range_int_text WHERE (r1, r2, r3) >= (1,'abc4'::text,5) AND (r1, r2, r3) <= (2,'abc4'::text,5);
+SELECT * FROM pk_range_int_text WHERE (r1, r2, r3) >= (1,'abc4'::text,5) AND (r1, r2, r3) <= (2,'abc4'::text,5);
+EXPLAIN (COSTS OFF, TIMING OFF, SUMMARY OFF, ANALYZE) SELECT * FROM pk_range_int_text WHERE (r1, r2, r3) >= (1,'abc2'::text,3) AND (r1, r2, r3) <= (1,'abc3'::text,2) AND r3 IN (3,2,6);
+SELECT * FROM pk_range_int_text WHERE (r1, r2, r3) >= (1,'abc2'::text,3) AND (r1, r2, r3) <= (1,'abc3'::text,2) AND r3 IN (3,2,6);
+EXPLAIN (COSTS OFF, TIMING OFF, SUMMARY OFF, ANALYZE) SELECT * FROM pk_range_int_text WHERE (r1, r2, r3) >= (1,'ab'::text,5) AND (r1, r2, r3) <= (1,'abcd'::text,5) ORDER BY r1 ASC, r2 ASC, r3 ASC;
+SELECT * FROM pk_range_int_text WHERE (r1, r2, r3) >= (1,'ab'::text,5) AND (r1, r2, r3) <= (1,'abcd'::text,5) ORDER BY r1 ASC, r2 ASC, r3 ASC;
+DROP TABLE pk_range_int_text;

src/yb/client/yb_op.cc
@@ -263,7 +263,7 @@ Result<std::string> GetRangePartitionKey(
   RSTATUS_DCHECK(!schema.num_hash_key_columns(), IllegalState,
       "Cannot get range partition key for hash partitioned table");
 
-  auto range_components = VERIFY_RESULT(client::GetRangeComponents(schema, range_cols));
+  auto range_components = VERIFY_RESULT(client::GetRangeComponents(schema, range_cols, true));
   return docdb::DocKey(std::move(range_components)).Encode().ToStringBuffer();
 }
 
@@ -1003,7 +1003,9 @@ CHECKED_STATUS InitPartitionKey(
 }
 
 Result<std::vector<docdb::PrimitiveValue>> GetRangeComponents(
-    const Schema& schema, const google::protobuf::RepeatedPtrField<PgsqlExpressionPB>& range_cols) {
+    const Schema& schema,
+    const google::protobuf::RepeatedPtrField<PgsqlExpressionPB>& range_cols,
+    bool lower_bound) {
   int i = 0;
   auto num_range_key_columns = narrow_cast<int>(schema.num_range_key_columns());
   std::vector<docdb::PrimitiveValue> result;
@@ -1014,7 +1016,11 @@ Result<std::vector<docdb::PrimitiveValue>> GetRangeComponents(
 
     const ColumnSchema& column_schema = VERIFY_RESULT(schema.column_by_id(col_id));
     if (i >= range_cols.size() || range_cols[i].value().value_case() == QLValuePB::VALUE_NOT_SET) {
-      result.emplace_back(docdb::ValueType::kLowest);
+      if (lower_bound) {
+        result.emplace_back(docdb::ValueType::kLowest);
+      } else {
+        result.emplace_back(docdb::ValueType::kHighest);
+      }
     } else {
       result.push_back(docdb::PrimitiveValue::FromQLValuePB(
           range_cols[i].value(), column_schema.sorting_type()));
@@ -1022,6 +1028,10 @@ Result<std::vector<docdb::PrimitiveValue>> GetRangeComponents(
 
     if (++i == num_range_key_columns) {
       break;
+    }
+
+    if (!lower_bound) {
+      result.emplace_back(docdb::ValueType::kHighest);
     }
   }
   return result;
@@ -1045,9 +1055,8 @@ Status GetRangePartitionBounds(const Schema& schema,
     *upper_bound = docdb::GetRangeKeyScanSpec(
         schema, &prefixed_range_components, &scan_range, false /* upper_bound */);
   } else if (!range_cols.empty()) {
-    *lower_bound = VERIFY_RESULT(GetRangeComponents(schema, range_cols));
-    *upper_bound = *lower_bound;
-    upper_bound->emplace_back(docdb::ValueType::kHighest);
+    *lower_bound = VERIFY_RESULT(GetRangeComponents(schema, range_cols, true));
+    *upper_bound = VERIFY_RESULT(GetRangeComponents(schema, range_cols, false));
   }
   return Status::OK();
 }

src/yb/client/yb_op.h
@@ -595,7 +595,8 @@ CHECKED_STATUS InitPartitionKey(
     const Schema& schema, const PartitionSchema& partition_schema, PgsqlWriteRequestPB* request);
 
 Result<std::vector<docdb::PrimitiveValue>> GetRangeComponents(
-    const Schema& schema, const google::protobuf::RepeatedPtrField<PgsqlExpressionPB>& range_cols);
+    const Schema& schema, const google::protobuf::RepeatedPtrField<PgsqlExpressionPB>& range_cols,
+    bool lower_bound);
 
 CHECKED_STATUS GetRangePartitionBounds(
     const Schema& schema,

src/yb/common/partition.cc
@@ -465,7 +465,7 @@ string PartitionSchema::EncodeMultiColumnHashValue(uint16_t hash_value) {
 }
 
 uint16_t PartitionSchema::DecodeMultiColumnHashValue(const string& partition_key) {
-  DCHECK_EQ(partition_key.size(), kPartitionKeySize);
+  DCHECK_GE(partition_key.size(), kPartitionKeySize);
   const uint8_t *bytes = reinterpret_cast<const uint8_t *>(partition_key.data());
   return (bytes[0] << 8) | bytes[1];
 }

src/yb/docdb/doc_pgsql_scanspec.cc
@@ -147,7 +147,9 @@ DocPgsqlScanSpec::DocPgsqlScanSpec(
     const boost::optional<int32_t> max_hash_code,
     const PgsqlExpressionPB *where_expr,
     const DocKey& start_doc_key,
-    bool is_forward_scan)
+    bool is_forward_scan,
+    const DocKey& lower_doc_key,
+    const DocKey& upper_doc_key)
     : PgsqlScanSpec(where_expr),
       range_bounds_(condition ? new QLScanRange(schema, *condition) : nullptr),
       schema_(schema),
@@ -157,9 +159,20 @@ DocPgsqlScanSpec::DocPgsqlScanSpec(
       hash_code_(hash_code),
       max_hash_code_(max_hash_code),
       start_doc_key_(start_doc_key.empty() ? KeyBytes() : start_doc_key.Encode()),
-      lower_doc_key_(bound_key(schema, true)),
-      upper_doc_key_(bound_key(schema, false)),
+      lower_doc_key_(lower_doc_key.Encode()),
+      upper_doc_key_(upper_doc_key.Encode()),
       is_forward_scan_(is_forward_scan) {
+
+  auto lower_bound_key = bound_key(schema, true);
+  lower_doc_key_ = lower_bound_key > lower_doc_key_
+                    || lower_doc_key.empty()
+                    ? lower_bound_key : lower_doc_key_;
+
+  auto upper_bound_key = bound_key(schema, false);
+  upper_doc_key_ = upper_bound_key < upper_doc_key_
+                    || upper_doc_key.empty()
+                    ? upper_bound_key : upper_doc_key_;
+
   if (where_expr_) {
     // Should never get here until WHERE clause is supported.
     LOG(FATAL) << "DEVELOPERS: Add support for condition (where clause)";

src/yb/docdb/doc_pgsql_scanspec.h
@@ -54,7 +54,9 @@ class DocPgsqlScanSpec : public PgsqlScanSpec {
                    boost::optional<int32_t> max_hash_code,
                    const PgsqlExpressionPB *where_expr,
                    const DocKey& start_doc_key = DefaultStartDocKey(),
-                   bool is_forward_scan = true);
+                   bool is_forward_scan = true,
+                   const DocKey& lower_doc_key = DefaultStartDocKey(),
+                   const DocKey& upper_doc_key = DefaultStartDocKey());
 
   //------------------------------------------------------------------------------------------------
   // Access funtions.

commit afc424dfb2c0b7ea70b616a8bd80140535983915

[#23824] YSQL: Fix crash for when a RowComparisonExpression is used on a reordered primary key index
Summary:
This change fixes a crash in the titled scenario. This happens due to a bug when binding the arguments to a row comparison expression. While binding an element of the RCE argument, the referenced code finds the typeid of the argument. It does that using the base table relation's tuple descriptor but using the primary key index's attribute numbers. This change fixes that discrepancy.

Bug introduced by D15429/f66a8c0a155a46929612d0e79a1707e524ee766b
Backports required: 2.20, 2024.1
Jira: DB-12724

Test Plan: ./yb_build.sh --java-test 'org.yb.pgsql.TestPgRegressIndex'

Reviewers: amartsinchyk

Reviewed By: amartsinchyk

Subscribers: yql

Differential Revision: https://phorge.dev.yugabyte.com/D38292

src/postgres/src/backend/access/yb_access/yb_scan.c
@@ -1586,7 +1586,7 @@ YbBindRowComparisonKeys(YbScanDesc ybScan, YbScanPlan scan_plan,
 					YBCNewConstant(
 						ybScan->handle,
 						ybc_get_atttypid(scan_plan->bind_desc,
-										 current->sk_attno),
+										 scan_plan->bind_key_attnums[current->sk_attno]),
 						current->sk_collation,
 						current->sk_argument,
 						false);

src/postgres/src/test/regress/expected/yb_index_scan.out
@@ -4123,3 +4123,20 @@ select v2 from t1 where k1 = 1 and k2 > 0 and v1 IN (1, 2);
 (0 rows)
 
 drop table t1;
+create table t1(a uuid, b timestamptz, PRIMARY KEY(b asc, a asc));
+insert into t1 values ('00000000-0000-0000-0000-000000000000', '2021-01-01 00:00:00');
+insert into t1 values ('00000000-0000-0000-0000-000000000001', '2021-01-01 00:00:00');
+explain (costs off) select * from t1 where (b, a) >= ('2021-01-01 00:00:00', '00000000-0000-0000-0000-000000000001');
+                                                                QUERY PLAN
+------------------------------------------------------------------------------------------------------------------------------------------
+ Index Scan using t1_pkey on t1
+   Index Cond: (ROW(b, a) >= ROW('Fri Jan 01 00:00:00 2021 PST'::timestamp with time zone, '00000000-0000-0000-0000-000000000001'::uuid))
+(2 rows)
+
+select * from t1 where (b, a) >= ('2021-01-01 00:00:00', '00000000-0000-0000-0000-000000000001');
+                  a                   |              b
+--------------------------------------+------------------------------
+ 00000000-0000-0000-0000-000000000001 | Fri Jan 01 00:00:00 2021 PST
+(1 row)
+
+drop table t1;

src/postgres/src/test/regress/sql/yb_index_scan.sql
@@ -556,3 +556,9 @@ explain (costs off) select v2 from t1 where k1 = 1 and k2 > 0 and v1 IN (1, 2);
 select v2 from t1 where k1 = 1 and k2 > 0 and v1 IN (1, 2);
 drop table t1;
 
+create table t1(a uuid, b timestamptz, PRIMARY KEY(b asc, a asc));
+insert into t1 values ('00000000-0000-0000-0000-000000000000', '2021-01-01 00:00:00');
+insert into t1 values ('00000000-0000-0000-0000-000000000001', '2021-01-01 00:00:00');
+explain (costs off) select * from t1 where (b, a) >= ('2021-01-01 00:00:00', '00000000-0000-0000-0000-000000000001');
+select * from t1 where (b, a) >= ('2021-01-01 00:00:00', '00000000-0000-0000-0000-000000000001');
+drop table t1;