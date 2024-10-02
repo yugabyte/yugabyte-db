yugabyte/yugabyte-db

commit 6e700174dc087f51d14349cf33e343085858d9ef

[PLAT-15556] Pause universe ephemeral storage check should only apply to AWS universes
Summary:
The fix for https://yugabyte.atlassian.net/browse/PLAT-9803 identified AWS ephemeral
instances as anything with a letter 'd' in them. This check is being used to decide if pause
universe needs to be shown, so this diff fixes that check to apply to AWS univs only.

Test Plan: Create an azure universe with Standard Dsv3, verify universe is pausable.

Reviewers: jmak

Reviewed By: jmak

Subscribers: yugaware

Differential Revision: https://phorge.dev.yugabyte.com/D38596

managed/ui/src/utils/UniverseUtils.js
@@ -10,6 +10,7 @@ import {
 import { PROVIDER_TYPES, BASE_URL } from '../config';
 import { NodeState } from '../redesign/helpers/dtos';
 
+
 export const MULTILINE_GFLAGS_ARRAY = ['ysql_hba_conf_csv', 'ysql_ident_conf_csv'];
 
 const LDAP_KEYS = [
@@ -265,9 +266,27 @@ export const isOnpremUniverse = (universe) => {
   return isUniverseType(universe, 'onprem');
 };
 
+const INSTANCE_WITH_EPHEMERAL_STORAGE_ONLY = ['g5','g6','g6e',
+  'gr6','i3','i3en','i4g','i4i','im4gn',
+  'is4gen','p5','p5e','trn1','trn1n','x1','x1e'];
+
+export const isEphemeralAwsStorageInstance = (instanceType) => {
+  return INSTANCE_WITH_EPHEMERAL_STORAGE_ONLY.includes(instanceType?.split?.('.')[0]) ||
+    instanceType?.split?.('.')[0].includes('d');
+};
+
 export const isPausableUniverse = (universe) => {
+
+
+
+  if (isUniverseType(universe, 'aws')) {
+    return universe.nodeDetailsSet.find(
+      n => n !== null &&
+      isEphemeralAwsStorageInstance(n.cloudInfo?.instanceType)) !== undefined;
+  }
+
+
   return (
-    isUniverseType(universe, 'aws') ||
     isUniverseType(universe, 'gcp') ||
     isUniverseType(universe, 'azu')
   );
@@ -350,7 +369,7 @@ export const unformatConf = (GFlagInput) => {
 };
 
 /**
-  * Format Configuration string based on rules here: 
+  * Format Configuration string based on rules here:
   * https://docs.yugabyte.com/preview/reference/configuration/yb-tserver/#ysql-hba-conf-csv
   *
   * @param GFlagInput Input entered in the text field
@@ -450,7 +469,7 @@ export const verifyAttributes = (GFlagInput, searchTerm, JWKSKeyset, isOIDCSuppo
     const isJWTUrlExist = attributes?.some((input) => input.includes(CONST_VALUES.JWT_JWKS_URL));
     const isJWKSKesysetEmpty = isEmptyString(JWKSKeyset) || !JWKSKeyset;
 
-    /* 
+    /*
       Raise error when there is jwt keyword but is no JWT_JWKS_URL attribute present and Keyset is empty
     */
     if (searchTerm === CONST_VALUES.JWT && !isJWTUrlExist && isJWKSKesysetEmpty) {

managed/ui/src/components/universes/UniverseDetail/UniverseDetail.js
@@ -784,11 +784,6 @@ class UniverseDetail extends Component {
       }
     } = currentUniverse;
 
-    const isEphemeralAwsStorage =
-      nodeDetailsSet.find?.((node) => {
-        return isEphemeralAwsStorageInstance(node.cloudInfo?.instance_type);
-      }) !== undefined;
-
     /**
      * Handle the backup state toggle.
      * i.e open the confirmation model if backup is to be disabled.
@@ -1434,7 +1429,6 @@ class UniverseDetail extends Component {
                   */}
 
                   {isPausableUniverse(currentUniverse?.data) &&
-                    !isEphemeralAwsStorage &&
                     (featureFlags.test['pausedUniverse'] ||
                       featureFlags.released['pausedUniverse']) && (
                       <RbacValidator

managed/ui/src/components/universes/UniverseView/UniverseView.js
@@ -279,10 +279,6 @@ export const UniverseView = (props) => {
   };
 
   const formatUniverseActions = (_, row) => {
-    const isEphemeralAwsStorage =
-      row.universeDetails.nodeDetailsSet.find?.((node) => {
-        return isEphemeralAwsStorageInstance(node.cloudInfo?.instance_type);
-      }) !== undefined;
     const universePaused = row.universeDetails.universePaused;
     return (
       <Dropdown id="table-actions-dropdown" pullRight>
@@ -291,7 +287,6 @@ export const UniverseView = (props) => {
         </Dropdown.Toggle>
         <Dropdown.Menu>
           {isPausableUniverse(row) &&
-            !isEphemeralAwsStorage &&
             (featureFlags.test['pausedUniverse'] || featureFlags.released['pausedUniverse']) && (
               <RbacValidator
                 isControl

commit 03218900634aba94652df2c5600da50fc8047dd6

[PLAT-15556] Handle invalid nodeDetailSet in isPausableUniv
Summary: This fixes a regression from 6e700174dc087f51d14349cf33e343085858d9ef

Test Plan: dev itest

Reviewers: jmak

Reviewed By: jmak

Subscribers: nsingh, yugaware

Differential Revision: https://phorge.dev.yugabyte.com/D38649

managed/ui/src/utils/UniverseUtils.js
@@ -280,9 +280,9 @@ export const isPausableUniverse = (universe) => {
 
 
   if (isUniverseType(universe, 'aws')) {
-    return universe.nodeDetailsSet.find(
+    return universe.universeDetails?.nodeDetailsSet?.find(
       n => n !== null &&
-      isEphemeralAwsStorageInstance(n.cloudInfo?.instanceType)) !== undefined;
+      isEphemeralAwsStorageInstance(n.cloudInfo?.instanceType)) !== undefined ?? true;
   }
 
 