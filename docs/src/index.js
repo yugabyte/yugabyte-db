34aede5b8e1b3153626b0327f199d737f94cfdbc
Sami Ahmed Siddiqui <sami.siddiqui@yasglobal.com>
2024-10-04 17:30:21 +0500

    Fix JS issue and HTML id for multiple tabs

--- docs/src/index.js
+++ docs/src/index.js
@@ -376,7 +376,11 @@ $(document).ready(() => {
             tabId = `${found.groups.name}-tab`;
           }
 
-          $(`.td-content .nav-tabs-yb .nav-link.${tabId}`).trigger('click');
+          $('.td-content .nav-tabs-yb .nav-link').removeClass('active');
+          $(`.td-content .nav-tabs-yb .nav-link.${tabId}`).addClass('active');
+
+          $('.td-content .nav-tabs-yb').next(`.tab-content .tab-pane[aria-labelledby="${tabId}"]`).removeClass('active show');
+          $('.td-content .nav-tabs-yb').next(`.tab-content .tab-pane[aria-labelledby="${tabId}"]`).addClass('active show');
         }
       }
     });
