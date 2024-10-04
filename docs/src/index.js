db3e12935fe878e90f31ddedea62ac21e502a2d8
Sami Ahmed Siddiqui <sami.siddiqui@yasglobal.com>
2024-10-04 17:46:57 +0500

    Fix JS element

--- docs/src/index.js
+++ docs/src/index.js
@@ -379,8 +379,8 @@ $(document).ready(() => {
           $('.td-content .nav-tabs-yb .nav-link').removeClass('active');
           $(`.td-content .nav-tabs-yb .nav-link.${tabId}`).addClass('active');
 
-          $('.td-content .nav-tabs-yb').next(`.tab-content .tab-pane[aria-labelledby="${tabId}"]`).removeClass('active show');
-          $('.td-content .nav-tabs-yb').next(`.tab-content .tab-pane[aria-labelledby="${tabId}"]`).addClass('active show');
+          $('.td-content .nav-tabs-yb').next('.tab-content').find(`.tab-pane[aria-labelledby="${tabId}"]`).removeClass('active show');
+          $('.td-content .nav-tabs-yb').next('.tab-content').find(`.tab-pane[aria-labelledby="${tabId}"]`).addClass('active show');
         }
       }
     });
