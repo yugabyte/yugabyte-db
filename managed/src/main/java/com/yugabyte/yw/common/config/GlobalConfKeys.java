36f781352c61ca89dd1e4b70196725be1f539970
Aleksandr Malyshev <amalyshev@yugabyte.com>
2024-10-04 15:59:57 +0300

    [PLAT-15474] Make federation scrape interval configurable

--- managed/src/main/java/com/yugabyte/yw/common/config/GlobalConfKeys.java
+++ managed/src/main/java/com/yugabyte/yw/common/config/GlobalConfKeys.java
@@ -1492,4 +1492,13 @@ public class GlobalConfKeys extends RuntimeConfigKeysModule {
               + " retained in the storage bucket",
           ConfDataType.IntegerType,
           ImmutableList.of(ConfKeyTags.PUBLIC));
+  public static final ConfKeyInfo<String> metricScrapeIntervalStandby =
+      new ConfKeyInfo<>(
+          "yb.metrics.scrape_interval_standby",
+          ScopeType.GLOBAL,
+          "Standby Prometheus scrape interval",
+          "Need to increase it in case federation metrics request takes more time "
+              + " than main Prometheus scrape period to complete",
+          ConfDataType.StringType,
+          ImmutableList.of(ConfKeyTags.PUBLIC));
 }
