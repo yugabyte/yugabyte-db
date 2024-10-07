3709556c79650a095e09c03aba59ddf7dc5d5682
vkumar <vkumar@yugabyte.com>
2024-10-07 04:53:33 +0000

    [BACKPORT 2024.2][PLAT-12232] Dont print exception stack trace for non-operator controlled universes
    Summary:
    Original commit: 546ef6701ec0a7d0e3b0d9d361bbea2841b13bf1 / D38540
    We dont want to print entire status update exception stack traces for non-operator controlled universes.
    Fixed that in restores.
    
    Test Plan: No tests
    
    Reviewers: anijhawan
    
    Reviewed By: anijhawan
    
    Subscribers: yugaware
    
    Differential Revision: https://phorge.dev.yugabyte.com/D38734

--- managed/src/main/java/com/yugabyte/yw/commissioner/tasks/RestoreBackup.java
+++ managed/src/main/java/com/yugabyte/yw/commissioner/tasks/RestoreBackup.java
@@ -97,19 +97,20 @@ public class RestoreBackup extends UniverseTaskBase {
           restore.update(getTaskUUID(), Restore.State.Aborted);
           RestoreKeyspace.update(restore, TaskInfo.State.Aborted);
         }
-        kubernetesStatus.updateRestoreJobStatus("Aborted Restore task", getUserTaskUUID());
+        kubernetesStatus.updateRestoreJobStatus(
+            "Aborted Restore task", getUserTaskUUID(), universe);
         throw ce;
       }
     } catch (Throwable t) {
       log.error("Error executing task {} with error='{}'.", getName(), t.getMessage(), t);
       handleFailedBackupAndRestore(
           null, Arrays.asList(restore), isAbort, taskParams().alterLoadBalancer);
-      kubernetesStatus.updateRestoreJobStatus("Failed Restore task", getUserTaskUUID());
+      kubernetesStatus.updateRestoreJobStatus("Failed Restore task", getUserTaskUUID(), universe);
       throw t;
     } finally {
       unlockUniverseForUpdate();
     }
-    kubernetesStatus.updateRestoreJobStatus("Finished Restore", getUserTaskUUID());
+    kubernetesStatus.updateRestoreJobStatus("Finished Restore", getUserTaskUUID(), universe);
     log.info("Finished {} task.", getName());
   }
 }
