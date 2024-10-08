3192aa964f021c287cd6563392c34df74d781f78
Sumukh-Phalgaonkar <sumukhphalgaonkar@gmail.com>
2024-10-08 13:11:39 +0530

    [#24307] CDCSDK: Add slot name attribute to CDC metrics
    Summary:
    Currently the CDC metrics have tags for stream id and namespace name associated with them. This diff adds a tag for the slot name attribute in the CDC metrics.
    Jira: DB-13196
    
    Test Plan:
    Added unit test:
     - ./yb_build.sh --cxx-test integration-tests_cdcsdk_ysql-test --gtest_filter CDCSDKYsqlTest.TestSlotNameInCDCMetricsAttributes
    
    Tested the following out manually with a yugabyted setup:
      - Metrics associated with replication slot consumption have slot name attribute published along with stream id and namespace name.
      - Metrics associated with gRPC model of consumption do not have a slot name attribute.
    
    Reviewers: skumar, xCluster, hsunder
    
    Reviewed By: skumar
    
    Subscribers: ybase, ycdcxcluster
    
    Tags: #jenkins-ready
    
    Differential Revision: https://phorge.dev.yugabyte.com/D38742

--- src/yb/integration-tests/cdcsdk_ysql-test.cc
+++ src/yb/integration-tests/cdcsdk_ysql-test.cc
@@ -10744,5 +10744,54 @@ TEST_F(CDCSDKYsqlTest, TestCleanupOfEligibleAndNonEligibleTables) {
   LOG(INFO) << "Stream, after master restart, only contains the table_1.";
 }
 
+TEST_F(CDCSDKYsqlTest, TestSlotNameInCDCMetricsAttributes) {
+  ANNOTATE_UNPROTECTED_WRITE(FLAGS_update_metrics_interval_ms) = 0;
+  ANNOTATE_UNPROTECTED_WRITE(FLAGS_update_min_cdc_indices_interval_secs) = 0;
+  ANNOTATE_UNPROTECTED_WRITE(FLAGS_yb_enable_cdc_consistent_snapshot_streams) = true;
+
+  ASSERT_OK(SetUpWithParams(1, 1));
+  const auto& tserver = test_cluster()->mini_tablet_server(0)->server();
+  auto cdc_service = CDCService(tserver);
+
+  std::string kNamespaceName_2 = "test_namespace_for_old_model";
+  ASSERT_OK(CreateDatabase(&test_cluster_, kNamespaceName_2));
+
+  auto table_1 = ASSERT_RESULT(CreateTable(&test_cluster_, kNamespaceName, "test1"));
+  auto table_2 = ASSERT_RESULT(CreateTable(&test_cluster_, kNamespaceName_2, "test2"));
+
+  google::protobuf::RepeatedPtrField<master::TabletLocationsPB> tablets_1;
+  ASSERT_OK(test_client()->GetTablets(table_1, 0, &tablets_1, nullptr));
+  ASSERT_EQ(tablets_1.size(), 1);
+  google::protobuf::RepeatedPtrField<master::TabletLocationsPB> tablets_2;
+  ASSERT_OK(test_client()->GetTablets(table_2, 0, &tablets_2, nullptr));
+  ASSERT_EQ(tablets_2.size(), 1);
+
+  std::string slot_name = "test_slot";
+  auto stream_id_with_slot = ASSERT_RESULT(CreateConsistentSnapshotStreamWithReplicationSlot(
+      slot_name, CDCSDKSnapshotOption::USE_SNAPSHOT, false /*verify_snapshot_name*/,
+      kNamespaceName));
+
+  auto stream_id_without_slot = ASSERT_RESULT(CreateConsistentSnapshotStream(
+      CDCSDKSnapshotOption::USE_SNAPSHOT, CDCCheckpointType::EXPLICIT, CDCRecordType::CHANGE,
+      kNamespaceName_2));
+
+  vector<std::shared_ptr<xrepl::CDCSDKTabletMetrics>> metrics(2);
+  metrics[0] = ASSERT_RESULT(GetCDCSDKTabletMetrics(
+      *cdc_service, tablets_1[0].tablet_id(), stream_id_with_slot,
+      CreateMetricsEntityIfNotFound::kFalse));
+
+  metrics[1] = ASSERT_RESULT(GetCDCSDKTabletMetrics(
+      *cdc_service, tablets_2[0].tablet_id(), stream_id_without_slot,
+      CreateMetricsEntityIfNotFound::kFalse));
+
+  // Stream created with replication slot will have slot_name attribute in its metrics.
+  auto slot_name_attribute = ASSERT_RESULT(metrics[0]->TEST_GetAttribute("slot_name"));
+  ASSERT_EQ(slot_name_attribute, slot_name);
+
+  // Old model stream will not contain slot_name attribute in its metrics.
+  auto result = metrics[1]->TEST_GetAttribute("slot_name");
+  ASSERT_STR_CONTAINS(result.ToString(), "not found in attributes_ map");
+}
+
 }  // namespace cdc
 }  // namespace yb
