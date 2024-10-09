2d77f2313506bce0d6028608bfc38c60295f81b6
Muthu Chidambaram <mchidambaram@yugabyte.com>
2024-10-09 03:29:11 -0500

    [BACKPORT 2.20][PLAT-11305] Adding prometheus log file
    Summary:
    Original commit: ac5ac5bdf553f162a953e6c153e29d7bd8ef359f / D38644
    Original commit: 8920a4d7ec47142dd1b71b5779b462c4e395c6c8 / D38761
    We were not passing the prometheus log file anywhere to actually write the logs but were showing it in the status output which was kind of confusing. This diff adds the StandardOutput and StandardError redirection fields directly to the service file.
    
    NOTE: This only works with systemd >= 236 which should cover most modern linux distros. If the version is < 240 we will use file mode which will replace the file on each restart of the service, but at least it's logged to a file in this case.
    
    Test Plan: install yba and make sure prometheus logs present
    
    Reviewers: dshubin, sanketh
    
    Reviewed By: dshubin
    
    Subscribers: steve.varnau, svc_phabricator, yugaware
    
    Differential Revision: https://phorge.dev.yugabyte.com/D38855

--- managed/yba-installer/pkg/systemd/service_management.go
+++ managed/yba-installer/pkg/systemd/service_management.go
@@ -2,6 +2,7 @@ package systemd
 
 import (
 	"fmt"
+	"strconv"
 	"strings"
 
 	"github.com/yugabyte/yugabyte-db/managed/yba-installer/pkg/common/shell"
@@ -37,6 +38,34 @@ func DaemonReload() error {
 		return fmt.Errorf("systemctl daemon-reload failed: %w", out.Error)
 	}
 	return nil
+}
+
+// Version returns the systemd version on the system
+func Version() (int, error) {
+		// Run the 'systemctl --version' command
+		output := shell.Run("systemctl", "--version")
+		if !output.SucceededOrLog() {
+			return 0, fmt.Errorf("unable to run systemctl --version")
+		}
+
+		// Get the first line of the output, which contains the version number
+		lines := strings.Split(output.StdoutString(), "\n")
+		if len(lines) == 0 {
+			return 0, fmt.Errorf("unable to parse systemd version")
+		}
+
+		// Extract the version number from the first line
+		fields := strings.Fields(lines[0])
+		if len(fields) < 2 {
+			return 0, fmt.Errorf("unexpected output format")
+		}
+
+		// Convert the version string to an integer
+		version, err := strconv.Atoi(fields[1])
+		if err != nil {
+			return 0, fmt.Errorf("unable to parse version number: %v", err)
+		}
+		return version, nil
 }
 
 // runSysctlCmd is a helper for running some basic systemctl commands
