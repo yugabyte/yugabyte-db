CrunchyData/pg_parquet

Writes Arrow arrays to Parquet file

src/arrow_parquet.rs
@@ -1,3 +1,6 @@
 pub(crate) mod arrow_utils;
+pub(crate) mod codec;
+pub(crate) mod parquet_writer;
 pub(crate) mod pg_to_arrow;
 pub(crate) mod schema_visitor;
+pub(crate) mod uri_utils;

yugabyte/yugabyte-db

src/arrow_parquet.rs
@@ -1,3 +1,6 @@
 pub(crate) mod arrow_utils;
+pub(crate) mod compression;
+pub(crate) mod parquet_writer;
 pub(crate) mod pg_to_arrow;
 pub(crate) mod schema_parser;
+pub(crate) mod uri_utils;