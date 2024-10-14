1dcd5d358f563dd70eeca460fd8a2c315e32e7b0
Charles Wang <wang.charles234@gmail.com>
2024-10-13 17:09:37 -0700

    [BACKPORT 2024.2][PLAT-15623] Fix switchover local provider test
    Summary:
    Original commit:
    f6d4dd59fd28034e3b4f7869931c5840a749a065 / D38719
    
    Fix issue for `testDbScopedSwitchover` where `uq_xcluster_pitr_pitr_uuid` unique constraint is not being able to be dropped for h2 database. Changed files to remove addition of `xcluster_pitr`  constraint for h2 specifically.
    
    Test Plan: Make sure platform is able to run correctly (no checksum issues) + local provider tests pass.
    
    Reviewers: hzare, jmak, sanketh, vbansal
    
    Reviewed By: vbansal
    
    Subscribers: yugaware
    
    Differential Revision: https://phorge.dev.yugabyte.com/D38945

--- managed/src/test/java/com/yugabyte/yw/commissioner/tasks/local/DRDbScopedLocalTest.java
+++ managed/src/test/java/com/yugabyte/yw/commissioner/tasks/local/DRDbScopedLocalTest.java
@@ -607,7 +607,7 @@ public class DRDbScopedLocalTest extends DRLocalTestBase {
     deleteDrConfig(drConfigUUID, newSourceUniverse, newTargetUniverse);
   }
 
-  //  @Test
+  @Test
   public void testDbScopedSwitchover() throws InterruptedException {
     CreateDRMetadata createData = defaultDbDRCreate();
     UUID drConfigUUID = createData.drConfigUUID;
