2920e968f23a0f9f1b19ec77184901a8cd804885
Daniel Shubin <dshubin@yugabyte.com>
2024-10-16 16:16:48 +0000

    [BACKPORT 2024.1][PLAT-15565] Always grab the updated postgres version during build.
    Summary:
    Original commit: 24b5c4a91544d2afdf632033448f7160126721dc / D38616
    We have seen instances where the pg version was updated in init.go, but not
    in the makefile. Improved the workflow to ensure a
    pg version upgrade always grabs the correct postgres
    
    Test Plan: build to get the latest postgres
    
    Reviewers: sanketh, muthu, svarshney
    
    Reviewed By: svarshney
    
    Subscribers: yugaware
    
    Differential Revision: https://phorge.dev.yugabyte.com/D39110

--- managed/yba-installer/cmd/init.go
+++ managed/yba-installer/cmd/init.go
@@ -15,6 +15,11 @@ import (
 	log "github.com/yugabyte/yugabyte-db/managed/yba-installer/pkg/logging"
 )
 
+var (
+	PostgresVersion   string = ""
+	PrometheusVersion string = ""
+)
+
 // Get the commands that will require yba-ctl.yml to be setup before getting run
 // function because go doesn't support const <slice>
 func cmdsRequireConfigInit() []string {
@@ -87,9 +92,9 @@ func initServices() {
 	services = make(map[string]common.Component)
 	installPostgres := viper.GetBool("postgres.install.enabled")
 	installYbdb := viper.GetBool("ybdb.install.enabled")
-	services[PostgresServiceName] = NewPostgres("14.12")
+	services[PostgresServiceName] = NewPostgres(PostgresVersion)
 	// services[YbdbServiceName] = NewYbdb("2.17.2.0")
-	services[PrometheusServiceName] = NewPrometheus("2.53.1")
+	services[PrometheusServiceName] = NewPrometheus(PrometheusVersion)
 	services[YbPlatformServiceName] = NewPlatform(ybactl.Version)
 	// serviceOrder = make([]string, len(services))
 	if installPostgres {
@@ -123,5 +128,14 @@ func handleRootCheck(cmdName string) {
 				os.Exit(1)
 			}
 		}
+	}
+}
+
+func init() {
+	if PostgresVersion == "" {
+		panic("PostgresVersion not set during build")
+	}
+	if PrometheusVersion == "" {
+		panic("PrometheusVersion not set during build")
 	}
 }
