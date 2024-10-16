yugabyte/yugabyte-db

commit b7bc45e82f9cd16e63048fc2aa6a83a42f0a1405

Revert "[PLAT-13800] skip setting imageBundle reference for YBM based clusters using machineImage"
Summary: This reverts commit c698a3100420717d896d7d21d4ee151c47464493.

Test Plan: na

Reviewers: #yba-api-review!, daniel

Reviewed By: daniel

Subscribers: yugaware

Differential Revision: https://phorge.dev.yugabyte.com/D37763

managed/src/main/java/com/yugabyte/yw/common/NodeManager.java
@@ -228,11 +228,8 @@ public class NodeManager extends DevopsBase {
       return false;
     }
     UUID imageBundleUUID = Util.retreiveImageBundleUUID(arch, userIntent, provider);
-    if (imageBundleUUID != null) {
-      ImageBundle imageBundle = ImageBundle.get(imageBundleUUID);
-      return imageBundle.getDetails().useIMDSv2;
-    }
-    return false;
+    ImageBundle imageBundle = ImageBundle.get(imageBundleUUID);
+    return imageBundle.getDetails().useIMDSv2;
   }
 
   private UserIntent getUserIntentFromParams(Universe universe, NodeTaskParams nodeTaskParam) {
@@ -1803,10 +1800,8 @@ public class NodeManager extends DevopsBase {
     List<String> commandArgs = new ArrayList<>();
     UserIntent userIntent = getUserIntentFromParams(nodeTaskParam);
     ImageBundle.NodeProperties toOverwriteNodeProperties = null;
-    Config config = this.runtimeConfigFactory.forProvider(provider);
     UUID imageBundleUUID =
-        Util.retreiveImageBundleUUID(
-            arch, userIntent, nodeTaskParam.getProvider(), config.getBoolean("yb.cloud.enabled"));
+        Util.retreiveImageBundleUUID(arch, userIntent, nodeTaskParam.getProvider());
     if (imageBundleUUID != null) {
       Region region = nodeTaskParam.getRegion();
       toOverwriteNodeProperties =
@@ -1870,6 +1865,7 @@ public class NodeManager extends DevopsBase {
           if (!(nodeTaskParam instanceof AnsibleCreateServer.Params)) {
             throw new RuntimeException("NodeTaskParams is not AnsibleCreateServer.Params");
           }
+          Config config = this.runtimeConfigFactory.forProvider(provider);
           AnsibleCreateServer.Params taskParam = (AnsibleCreateServer.Params) nodeTaskParam;
           Common.CloudType cloudType = userIntent.providerType;
           if (!cloudType.equals(Common.CloudType.onprem)) {
@@ -1973,7 +1969,11 @@ public class NodeManager extends DevopsBase {
               // Backward compatiblity.
               imageBundleDefaultImage = taskParam.getRegion().getYbImage();
             }
-
+            if (StringUtils.isNotBlank(taskParam.getMachineImage())) {
+              // YBM use case - in case machineImage is used for deploying the universe we should
+              // fallback to sshUser configured in the provider.
+              taskParam.sshUserOverride = provider.getDetails().getSshUser();
+            }
             String ybImage =
                 Optional.ofNullable(taskParam.getMachineImage()).orElse(imageBundleDefaultImage);
             if (ybImage != null && !ybImage.isEmpty()) {
@@ -2045,6 +2045,11 @@ public class NodeManager extends DevopsBase {
             imageBundleDefaultImage = toOverwriteNodeProperties.getMachineImage();
           } else {
             imageBundleDefaultImage = taskParam.getRegion().getYbImage();
+          }
+          if (StringUtils.isNotBlank(taskParam.machineImage)) {
+            // YBM use case - in case machineImage is used for deploying the universe we should
+            // fallback to sshUser configured in the provider.
+            taskParam.sshUserOverride = provider.getDetails().getSshUser();
           }
           String ybImage =
               Optional.ofNullable(taskParam.machineImage).orElse(imageBundleDefaultImage);

managed/src/main/java/com/yugabyte/yw/commissioner/tasks/UniverseTaskBase.java
@@ -1991,10 +1991,7 @@ public abstract class UniverseTaskBase extends AbstractTaskBase {
                   universe.getUniverseDetails().getClusterByUuid(n.placementUuid);
               UUID imageBundleUUID =
                   Util.retreiveImageBundleUUID(
-                      universe.getUniverseDetails().arch,
-                      cluster.userIntent,
-                      provider,
-                      confGetter.getStaticConf().getBoolean("yb.cloud.enabled"));
+                      universe.getUniverseDetails().arch, cluster.userIntent, provider);
               if (imageBundleUUID != null) {
                 ImageBundle.NodeProperties toOverwriteNodeProperties =
                     imageBundleUtil.getNodePropertiesOrFail(
@@ -2002,10 +1999,6 @@ public abstract class UniverseTaskBase extends AbstractTaskBase {
                         n.cloudInfo.region,
                         cluster.userIntent.providerType.toString());
                 params.sshUser = toOverwriteNodeProperties.getSshUser();
-              } else {
-                // ImageBundleUUID will be null for the case when YBM specifies machineImage
-                // on fly during universe creation.
-                params.sshUser = providerDetails.getSshUser();
               }
 
               params.airgap = provider.getAirGapInstall();

managed/src/main/java/com/yugabyte/yw/commissioner/tasks/subtasks/UpdateClusterUserIntent.java
@@ -8,7 +8,6 @@ import com.yugabyte.yw.models.Universe.UniverseUpdater;
 import java.util.UUID;
 import javax.inject.Inject;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.commons.lang3.StringUtils;
 
 @Slf4j
 public class UpdateClusterUserIntent extends UniverseTaskBase {
@@ -57,19 +56,6 @@ public class UpdateClusterUserIntent extends UniverseTaskBase {
                       // Update the imageBundle reference for the cluster in which node
                       // is provisioned.
                       cluster.userIntent.imageBundleUUID = taskParams().imageBundleUUID;
-                      universeDetails.nodeDetailsSet.stream()
-                          .forEach(
-                              nodeDetail -> {
-                                // YBM use case where the cluster would have been deployed using the
-                                // machineImage
-                                // but patched using the imageBundle. It will be good if clear the
-                                // machineImage from
-                                // nodeDetails.
-                                if (nodeDetail.placementUuid.equals(cluster.uuid)
-                                    && StringUtils.isNotEmpty(nodeDetail.machineImage)) {
-                                  nodeDetail.machineImage = null;
-                                }
-                              });
                     }
                   });
               universe.setUniverseDetails(universeDetails);

managed/src/main/java/com/yugabyte/yw/common/NodeUniverseManager.java
@@ -577,10 +577,7 @@ public class NodeUniverseManager extends DevopsBase {
         String sshPort = provider.getDetails().sshPort.toString();
         UUID imageBundleUUID =
             Util.retreiveImageBundleUUID(
-                universe.getUniverseDetails().arch,
-                cluster.userIntent,
-                provider,
-                confGetter.getStaticConf().getBoolean("yb.cloud.enabled"));
+                universe.getUniverseDetails().arch, cluster.userIntent, provider);
         if (imageBundleUUID != null) {
           ImageBundle.NodeProperties toOverwriteNodeProperties =
               imageBundleUtil.getNodePropertiesOrFail(

managed/src/main/java/com/yugabyte/yw/common/Util.java
@@ -1232,17 +1232,10 @@ public class Util {
 
   public static UUID retreiveImageBundleUUID(
       Architecture arch, UserIntent userIntent, Provider provider) {
-    return retreiveImageBundleUUID(arch, userIntent, provider, false);
-  }
-
-  public static UUID retreiveImageBundleUUID(
-      Architecture arch, UserIntent userIntent, Provider provider, boolean cloudEnabled) {
     UUID imageBundleUUID = null;
     if (userIntent.imageBundleUUID != null) {
       imageBundleUUID = userIntent.imageBundleUUID;
-    } else if (provider.getUuid() != null && !cloudEnabled) {
-      // Don't use defaultProvider bundle for YBM, as they will
-      // specify machineImage for provisioning the node.
+    } else if (provider.getUuid() != null) {
       List<ImageBundle> bundles = ImageBundle.getDefaultForProvider(provider.getUuid());
       if (bundles.size() > 0) {
         ImageBundle bundle = ImageBundleUtil.getDefaultBundleForUniverse(arch, bundles);

managed/src/main/java/com/yugabyte/yw/controllers/handlers/UniverseCRUDHandler.java
@@ -602,9 +602,7 @@ public class UniverseCRUDHandler {
       }
       validateRegionsAndZones(provider, c);
       // Configure the defaultimageBundle in case not specified.
-      if (c.userIntent.imageBundleUUID == null
-          && provider.getCloudCode().imageBundleSupported()
-          && !cloudEnabled) {
+      if (c.userIntent.imageBundleUUID == null && provider.getCloudCode().imageBundleSupported()) {
         if (provider.getImageBundles().size() > 0) {
           List<ImageBundle> bundles = ImageBundle.getDefaultForProvider(provider.getUuid());
           if (bundles.size() > 0) {

commit 509ddb33f592671b806e39accfe176f7d8571ad3

[PLAT-15632] Remove sshUserOverride during provision stage
Summary:
We recently reverted a change that was specific to YBM - https://github.com/yugabyte/yugabyte-db/commit/b7bc45e82f9cd16e63048fc2aa6a83a42f0a1405 (which is no longer needed), which caused this code to unreverted that was no longer needed.
Removing the same now.

Test Plan: Manually verified OS patching from alma -> ubuntu

Reviewers: nsingh

Reviewed By: nsingh

Subscribers: yugaware

Differential Revision: https://phorge.dev.yugabyte.com/D38802

managed/src/main/java/com/yugabyte/yw/common/NodeManager.java
@@ -1960,11 +1960,6 @@ public class NodeManager extends DevopsBase {
               // Backward compatiblity.
               imageBundleDefaultImage = taskParam.getRegion().getYbImage();
             }
-            if (StringUtils.isNotBlank(taskParam.getMachineImage())) {
-              // YBM use case - in case machineImage is used for deploying the universe we should
-              // fallback to sshUser configured in the provider.
-              taskParam.sshUserOverride = provider.getDetails().getSshUser();
-            }
             String ybImage =
                 Optional.ofNullable(taskParam.getMachineImage()).orElse(imageBundleDefaultImage);
             if (ybImage != null && !ybImage.isEmpty()) {
@@ -2036,11 +2031,6 @@ public class NodeManager extends DevopsBase {
             imageBundleDefaultImage = toOverwriteNodeProperties.getMachineImage();
           } else {
             imageBundleDefaultImage = taskParam.getRegion().getYbImage();
-          }
-          if (StringUtils.isNotBlank(taskParam.machineImage)) {
-            // YBM use case - in case machineImage is used for deploying the universe we should
-            // fallback to sshUser configured in the provider.
-            taskParam.sshUserOverride = provider.getDetails().getSshUser();
           }
           String ybImage =
               Optional.ofNullable(taskParam.machineImage).orElse(imageBundleDefaultImage);