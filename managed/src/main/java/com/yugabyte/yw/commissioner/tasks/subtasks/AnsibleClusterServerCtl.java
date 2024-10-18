f7159380d7c4248be6c4530c04a49abd75745d34
Naorem Khogendro Singh <nsingh@yugabyte.com>
2024-10-18 10:02:45 -0700

    [PLAT-15772] Non placement modification tasks simply performing start/stop of master leaves master state behind
    Summary: Do not set master state if it was not set before. It may simply be restart of the process.
    
    Test Plan:
    Tested the scenario below
    1. Create a cron based universe.
    2. Upgrade systemd.
    3. Remove a master node to start master on another.
    
    {F301742} {F301753}
    
    Reviewers: cwang, sanketh, yshchetinin
    
    Reviewed By: cwang
    
    Subscribers: yugaware
    
    Differential Revision: https://phorge.dev.yugabyte.com/D39203

--- managed/src/main/java/com/yugabyte/yw/commissioner/tasks/subtasks/AnsibleClusterServerCtl.java
+++ managed/src/main/java/com/yugabyte/yw/commissioner/tasks/subtasks/AnsibleClusterServerCtl.java
@@ -60,13 +60,6 @@ public class AnsibleClusterServerCtl extends NodeTaskBase {
   @Override
   public void run() {
     try {
-      if (ServerType.MASTER.name().equalsIgnoreCase(taskParams().process)
-          && "start".equalsIgnoreCase(taskParams().command)) {
-        // Master is fully configured and ready to start.
-        // TODO This is not the right place but this comes after AnsibleConfigureServer
-        // which does too many things.
-        setNodeStatus(NodeStatus.builder().masterState(MasterState.Configured).build());
-      }
       NodeDetails nodeDetails = null;
       Optional<Universe> universeOpt = Universe.maybeGet(taskParams().getUniverseUUID());
       if (!universeOpt.isPresent()
@@ -76,6 +69,15 @@ public class AnsibleClusterServerCtl extends NodeTaskBase {
             taskParams().nodeName,
             taskParams().command);
         return;
+      }
+      if (ServerType.MASTER.name().equalsIgnoreCase(taskParams().process)
+          && "start".equalsIgnoreCase(taskParams().command)
+          && nodeDetails.masterState != null) {
+        // Master is fully configured and ready to start. Set this only if masterState was
+        // previously set. Some tasks may just start/stop master without changing master state.
+        // TODO This is not the right place but this comes after AnsibleConfigureServer
+        // which does too many things.
+        setNodeStatus(NodeStatus.builder().masterState(MasterState.Configured).build());
       }
       if (nodeDetails.isSoftwareDeleted()) {
         // This is to ensure that the command is not issued after the software is uninstalled.
