cb993e8d9439d413d0d373ae0515db8a5a474147
Kumar Vivek <kv83821@gmail.com>
2024-10-18 05:58:06 +0000

    [BACKPORT 2024.2][PLAT-15748] Support IAM based configs in K8s operator
    Summary:
    Original commit: aebc9d48f9da323edcca002d101a457fb24a582f / D39137
    Added option to use IAM based storage config in K8s operator. Users can configure IAM for
    S3/GCS by using `USE_IAM` param in the storageconfig crd.
    
    Test Plan: Tested manually by adding storageconfig with IAM, verified it was using IAM correctly.
    
    Reviewers: #yba-api-review, anijhawan
    
    Reviewed By: #yba-api-review, anijhawan
    
    Subscribers: yugaware
    
    Differential Revision: https://phorge.dev.yugabyte.com/D39182

--- managed/src/main/java/com/yugabyte/yw/models/helpers/CustomerConfigConsts.java
+++ managed/src/main/java/com/yugabyte/yw/models/helpers/CustomerConfigConsts.java
@@ -17,6 +17,8 @@ public class CustomerConfigConsts {
 
   public static final String USE_GCP_IAM_FIELDNAME = "USE_GCP_IAM";
 
+  public static final String USE_S3_IAM_FIELDNAME = "IAM_INSTANCE_PROFILE";
+
   public static final String NAME_S3 = "S3";
 
   public static final String NAME_GCS = "GCS";
