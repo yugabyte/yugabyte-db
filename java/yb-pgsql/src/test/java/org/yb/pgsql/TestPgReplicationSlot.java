yugabyte/yugabyte-db

commit 5e86515651d36667cd65212549f723133004685b

[#23768] YSQL: Fix table rewrite DDL before slot creation
Summary:
Previously, when a table rewrite DDL operation (such as `ALTER COLUMN TYPE`, `ADD/DROP PRIMARY KEY` or `TRUNCATE TABLE`) was performed before creating a replication slot, the changes from that table were not streamed during logical replication.

The issue arose from the inconsistent handling of DocDB's UUID for a table's ID and PG’s `table_oid`. Normally, the table’s UUID in DocDB matches the `table_oid` in PG. However, in the case of table rewrites, while PG’s `table_oid` remains the same, the DocDB UUID changes. After such rewrites, the `relfilenode` in PG becomes the one-to-one mapping to the new UUID in DocDB (commit: bb77fb2052ad259457b01c586dc96075ad88b958).

For clarity, the following terminology will be used throughout this summary:
 - `table_id`: Refers to the UUID of the underlying DocDB table.
 - `pg_table_id`: Refers to the original UUID of the DocDB table before the table rewrite.
 - `table_oid`: Represents PG’s table OID.
 - `relfilenode`: Corresponds to the relfilenode of the PG table, which maps to DocDB’s UUID.

It’s important to note that without any table rewrites, `table_oid` is equal to `relfilenode`. However, with table rewrites, `relfilenode` reflects the new underlying table, while `table_oid` remains unchanged.

The following key changes were made to address the issue in this revision:
 1. In `YBCInitVirtualWalForCDC`, `table_id` is now computed using `relfilenode` instead of `table_oid` to ensure the correct `table_id` is passed to CDC Service.

 2. The `GetConsistentChanges` response now treats the `table_id` as `relfilenode` and accurately retrieves `table_oid` to be used within PG. Previously, `table_id` was directly treated as `table_oid`, causing inconsistencies.

java/yb-pgsql/src/test/java/org/yb/pgsql/TestPgReplicationSlot.java
@@ -2901,4 +2901,168 @@ public class TestPgReplicationSlot extends BasePgSQLTest {
       assertEquals("logical", row.getString(0));
     }
   }
+
+  @Test
+  public void testWithAlterType() throws Exception {
+    try (Statement stmt = connection.createStatement()) {
+      stmt.execute("DROP TABLE IF EXISTS test");
+      stmt.execute("CREATE TABLE test (a int primary key, b int)");
+      stmt.execute("ALTER TABLE test ALTER COLUMN b TYPE text");
+      stmt.execute("CREATE PUBLICATION pub FOR ALL TABLES");
+    }
+
+    String slotName = "test_with_alter_type";
+    Connection conn = getConnectionBuilder().withTServer(0).replicationConnect();
+    PGReplicationConnection replConnection = conn.unwrap(PGConnection.class).getReplicationAPI();
+
+    createSlot(replConnection, slotName, YB_OUTPUT_PLUGIN_NAME);
+    try (Statement stmt = connection.createStatement()) {
+      stmt.execute("BEGIN");
+      stmt.execute("INSERT INTO test VALUES(1, 'abcd')");
+      stmt.execute("INSERT INTO test VALUES(2, 'defg')");
+      stmt.execute("COMMIT");
+    }
+
+    PGReplicationStream stream = replConnection.replicationStream()
+                                     .logical()
+                                     .withSlotName(slotName)
+                                     .withStartPosition(LogSequenceNumber.valueOf(0L))
+                                     .withSlotOption("proto_version", 1)
+                                     .withSlotOption("publication_names", "pub")
+                                     .start();
+
+    List<PgOutputMessage> result = new ArrayList<PgOutputMessage>();
+
+    result.addAll(receiveMessage(stream, 5));
+
+    List<PgOutputMessage> expectedResult = new ArrayList<PgOutputMessage>() {
+      {
+        add(PgOutputBeginMessage.CreateForComparison(LogSequenceNumber.valueOf("0/5"), 2));
+        add(PgOutputRelationMessage.CreateForComparison("public", "test", 'c',
+            Arrays.asList(PgOutputRelationMessageColumn.CreateForComparison("a", 23),
+                PgOutputRelationMessageColumn.CreateForComparison("b", 25))));
+        add(PgOutputInsertMessage.CreateForComparison(new PgOutputMessageTuple((short) 2,
+            Arrays.asList(new PgOutputMessageTupleColumnValue("1"),
+                new PgOutputMessageTupleColumnValue("abcd")))));
+        add(PgOutputInsertMessage.CreateForComparison(new PgOutputMessageTuple((short) 2,
+            Arrays.asList(new PgOutputMessageTupleColumnValue("2"),
+                new PgOutputMessageTupleColumnValue("defg")))));
+        add(PgOutputCommitMessage.CreateForComparison(
+            LogSequenceNumber.valueOf("0/5"), LogSequenceNumber.valueOf("0/6")));
+      }
+    };
+    assertEquals(expectedResult, result);
+
+    stream.close();
+  }
+  @Test
+  public void testWithDropAddPk() throws Exception {
+    try (Statement stmt = connection.createStatement()) {
+      stmt.execute("DROP TABLE IF EXISTS test");
+      stmt.execute("CREATE TABLE test (a int primary key, b int)");
+      stmt.execute("ALTER TABLE test DROP CONSTRAINT test_pkey");
+      stmt.execute("ALTER TABLE test ADD CONSTRAINT test_b_pkey PRIMARY KEY (b)");
+      stmt.execute("CREATE PUBLICATION pub FOR ALL TABLES");
+    }
+
+    String slotName = "test_with_add_drop_pk";
+    Connection conn = getConnectionBuilder().withTServer(0).replicationConnect();
+    PGReplicationConnection replConnection = conn.unwrap(PGConnection.class).getReplicationAPI();
+
+    createSlot(replConnection, slotName, YB_OUTPUT_PLUGIN_NAME);
+    try (Statement stmt = connection.createStatement()) {
+      stmt.execute("BEGIN");
+      stmt.execute("INSERT INTO test VALUES(1, 3)");
+      stmt.execute("INSERT INTO test VALUES(1, 4)");
+      stmt.execute("COMMIT");
+    }
+
+    PGReplicationStream stream = replConnection.replicationStream()
+                                     .logical()
+                                     .withSlotName(slotName)
+                                     .withStartPosition(LogSequenceNumber.valueOf(0L))
+                                     .withSlotOption("proto_version", 1)
+                                     .withSlotOption("publication_names", "pub")
+                                     .start();
+
+    List<PgOutputMessage> result = new ArrayList<PgOutputMessage>();
+
+    result.addAll(receiveMessage(stream, 5));
+
+    List<PgOutputMessage> expectedResult = new ArrayList<PgOutputMessage>() {
+      {
+        add(PgOutputBeginMessage.CreateForComparison(LogSequenceNumber.valueOf("0/5"), 2));
+        add(PgOutputRelationMessage.CreateForComparison("public", "test", 'c',
+            Arrays.asList(PgOutputRelationMessageColumn.CreateForComparison("a", 23),
+                PgOutputRelationMessageColumn.CreateForComparison("b", 23))));
+        add(PgOutputInsertMessage.CreateForComparison(new PgOutputMessageTuple((short) 2,
+            Arrays.asList(new PgOutputMessageTupleColumnValue("1"),
+                new PgOutputMessageTupleColumnValue("3")))));
+        add(PgOutputInsertMessage.CreateForComparison(new PgOutputMessageTuple((short) 2,
+            Arrays.asList(new PgOutputMessageTupleColumnValue("1"),
+                new PgOutputMessageTupleColumnValue("4")))));
+        add(PgOutputCommitMessage.CreateForComparison(
+            LogSequenceNumber.valueOf("0/5"), LogSequenceNumber.valueOf("0/6")));
+      }
+    };
+    assertEquals(expectedResult, result);
+
+    stream.close();
+  }
+
+  @Test
+  public void testWithTruncate() throws Exception {
+    try (Statement stmt = connection.createStatement()) {
+      stmt.execute("DROP TABLE IF EXISTS test");
+      stmt.execute("CREATE TABLE test (a int primary key, b int)");
+      stmt.execute("INSERT INTO test VALUES(101, 103)");
+      stmt.execute("INSERT INTO test VALUES(102, 104)");
+      stmt.execute("TRUNCATE TABLE test");
+      stmt.execute("CREATE PUBLICATION pub FOR ALL TABLES");
+    }
+
+    String slotName = "test_with_truncate";
+    Connection conn = getConnectionBuilder().withTServer(0).replicationConnect();
+    PGReplicationConnection replConnection = conn.unwrap(PGConnection.class).getReplicationAPI();
+
+    createSlot(replConnection, slotName, YB_OUTPUT_PLUGIN_NAME);
+    try (Statement stmt = connection.createStatement()) {
+      stmt.execute("BEGIN");
+      stmt.execute("INSERT INTO test VALUES(1, 3)");
+      stmt.execute("INSERT INTO test VALUES(2, 4)");
+      stmt.execute("COMMIT");
+    }
+
+    PGReplicationStream stream = replConnection.replicationStream()
+                                     .logical()
+                                     .withSlotName(slotName)
+                                     .withStartPosition(LogSequenceNumber.valueOf(0L))
+                                     .withSlotOption("proto_version", 1)
+                                     .withSlotOption("publication_names", "pub")
+                                     .start();
+
+    List<PgOutputMessage> result = new ArrayList<PgOutputMessage>();
+
+    result.addAll(receiveMessage(stream, 5));
+
+    List<PgOutputMessage> expectedResult = new ArrayList<PgOutputMessage>() {
+      {
+        add(PgOutputBeginMessage.CreateForComparison(LogSequenceNumber.valueOf("0/5"), 2));
+        add(PgOutputRelationMessage.CreateForComparison("public", "test", 'c',
+            Arrays.asList(PgOutputRelationMessageColumn.CreateForComparison("a", 23),
+                PgOutputRelationMessageColumn.CreateForComparison("b", 23))));
+        add(PgOutputInsertMessage.CreateForComparison(new PgOutputMessageTuple((short) 2,
+            Arrays.asList(new PgOutputMessageTupleColumnValue("1"),
+                new PgOutputMessageTupleColumnValue("3")))));
+        add(PgOutputInsertMessage.CreateForComparison(new PgOutputMessageTuple((short) 2,
+            Arrays.asList(new PgOutputMessageTupleColumnValue("2"),
+                new PgOutputMessageTupleColumnValue("4")))));
+        add(PgOutputCommitMessage.CreateForComparison(
+            LogSequenceNumber.valueOf("0/5"), LogSequenceNumber.valueOf("0/6")));
+      }
+    };
+    assertEquals(expectedResult, result);
+
+    stream.close();
+  }
 }

src/postgres/src/backend/commands/ybccmds.c
@@ -2049,14 +2049,32 @@ YBCDropReplicationSlot(const char *slot_name)
 		YBCPgExecDropReplicationSlot(handle), error_message);
 }
 
+/*
+ * Returns an of relfilenodes corresponding to input table_oids array.
+ */
+static Oid *
+YBCGetRelfileNodes(Oid *table_oids, size_t num_relations)
+{
+	Oid			*relfilenodes;
+
+	relfilenodes = palloc(sizeof(Oid) * num_relations);
+	for (size_t table_idx = 0; table_idx < num_relations; table_idx++)
+		relfilenodes[table_idx] = YbGetRelfileNodeIdFromRelId(table_oids[table_idx]);
+
+	return relfilenodes;
+}
+
 void
 YBCInitVirtualWalForCDC(const char *stream_id, Oid *relations,
 						size_t numrelations)
 {
 	Assert(MyDatabaseId);
 
+	Oid *relfilenodes;
+	relfilenodes = YBCGetRelfileNodes(relations, numrelations);
+
 	HandleYBStatus(YBCPgInitVirtualWalForCDC(stream_id, MyDatabaseId, relations,
-											 numrelations));
+											 relfilenodes, numrelations));
 }
 
 void
@@ -2065,7 +2083,11 @@ YBCUpdatePublicationTableList(const char *stream_id, Oid *relations,
 {
 	Assert(MyDatabaseId);
 
-	HandleYBStatus(YBCPgUpdatePublicationTableList(stream_id, MyDatabaseId, relations,
+	Oid *relfilenodes;
+	relfilenodes = YBCGetRelfileNodes(relations, numrelations);
+
+	HandleYBStatus(YBCPgUpdatePublicationTableList(stream_id, MyDatabaseId,
+												   relations, relfilenodes,
 												   numrelations));
 }
 

src/yb/client/client.cc
@@ -795,6 +795,13 @@ Result<YBTableInfo> YBClient::GetYBTableInfo(const YBTableName& table_name) {
   return info;
 }
 
+Result<YBTableInfo> YBClient::GetYBTableInfoById(const TableId& table_id) {
+  YBTableInfo info;
+  auto deadline = CoarseMonoClock::Now() + default_admin_operation_timeout();
+  RETURN_NOT_OK(data_->GetTableSchema(this, table_id, deadline, &info));
+  return info;
+}
+
 Status YBClient::GetTableSchema(const YBTableName& table_name,
                                 YBSchema* schema,
                                 dockv::PartitionSchema* partition_schema) {
@@ -1621,7 +1628,12 @@ Result<CDCSDKStreamInfo> YBClient::GetCDCStream(
   if (replica_identities) {
     replica_identities->reserve(resp.stream().replica_identity_map_size());
     for (const auto& entry : resp.stream().replica_identity_map()) {
-      replica_identities->emplace(VERIFY_RESULT(GetPgsqlTableOid(entry.first)), entry.second);
+      auto table_info = VERIFY_RESULT(GetYBTableInfoById(entry.first));
+
+      const auto pg_table_id = table_info.pg_table_id;
+      auto table_oid = VERIFY_RESULT(
+          (!pg_table_id.empty()) ? GetPgsqlTableOid(pg_table_id) : GetPgsqlTableOid(entry.first));
+      replica_identities->emplace(table_oid, entry.second);
     }
   }
 

src/yb/client/client.h
@@ -402,6 +402,7 @@ class YBClient {
   Status GetYBTableInfo(const YBTableName& table_name, std::shared_ptr<YBTableInfo> info,
                         StatusCallback callback);
   Result<YBTableInfo> GetYBTableInfo(const YBTableName& table_name);
+  Result<YBTableInfo> GetYBTableInfoById(const TableId& table_id);
 
   Status GetTableSchemaById(const TableId& table_id, std::shared_ptr<YBTableInfo> info,
                             StatusCallback callback);

src/yb/yql/pggate/ybc_pggate.cc
@@ -2361,12 +2361,13 @@ void YBCStoreTServerAshSamples(
 }
 
 YBCStatus YBCPgInitVirtualWalForCDC(
-    const char *stream_id, const YBCPgOid database_oid, YBCPgOid *relations, size_t num_relations) {
+    const char *stream_id, const YBCPgOid database_oid, YBCPgOid *relations, YBCPgOid *relfilenodes,
+    size_t num_relations) {
   std::vector<PgObjectId> tables;
   tables.reserve(num_relations);
 
   for (size_t i = 0; i < num_relations; i++) {
-    PgObjectId table_id(database_oid, relations[i]);
+    PgObjectId table_id(database_oid, relfilenodes[i]);
     tables.push_back(std::move(table_id));
   }
 
@@ -2379,12 +2380,13 @@ YBCStatus YBCPgInitVirtualWalForCDC(
 }
 
 YBCStatus YBCPgUpdatePublicationTableList(
-    const char* stream_id, const YBCPgOid database_oid, YBCPgOid* relations, size_t num_relations) {
+    const char* stream_id, const YBCPgOid database_oid, YBCPgOid* relations, YBCPgOid* relfilenodes,
+    size_t num_relations) {
   std::vector<PgObjectId> tables;
   tables.reserve(num_relations);
 
   for (size_t i = 0; i < num_relations; i++) {
-    PgObjectId table_id(database_oid, relations[i]);
+    PgObjectId table_id(database_oid, relfilenodes[i]);
     tables.push_back(std::move(table_id));
   }
 
@@ -2487,9 +2489,16 @@ YBCStatus YBCPgGetCDCConsistentChanges(
       old_tuple_idx++;
     }
 
-    const auto table_oid = row_message_pb.has_table_id()
-                               ? PgObjectId(row_message_pb.table_id()).object_oid
-                               : kPgInvalidOid;
+    auto table_oid = kPgInvalidOid;
+    if (row_message_pb.has_table_id()) {
+      const PgObjectId table_id = PgObjectId(row_message_pb.table_id());
+      YBCPgTableDesc tableDesc = NULL;
+      Status s = pgapi->GetTableDesc(table_id, &tableDesc);
+      if (!s.ok()) {
+        return ToYBCStatus(s);
+      }
+      table_oid = tableDesc->pg_table_id();
+    }
 
     auto col_count = narrow_cast<int>(col_name_idx_map.size());
     YBCPgDatumMessage *cols = nullptr;

src/yb/yql/pggate/ybc_pggate.h
@@ -878,10 +878,12 @@ YBCStatus YBCPgNewDropReplicationSlot(const char *slot_name,
 YBCStatus YBCPgExecDropReplicationSlot(YBCPgStatement handle);
 
 YBCStatus YBCPgInitVirtualWalForCDC(
-    const char *stream_id, const YBCPgOid database_oid, YBCPgOid *relations, size_t num_relations);
+    const char *stream_id, const YBCPgOid database_oid, YBCPgOid *relations, YBCPgOid *relfilenodes,
+    size_t num_relations);
 
 YBCStatus YBCPgUpdatePublicationTableList(
-    const char *stream_id, const YBCPgOid database_oid, YBCPgOid *relations, size_t num_relations);
+    const char *stream_id, const YBCPgOid database_oid, YBCPgOid *relations, YBCPgOid *relfilenodes,
+    size_t num_relations);
 
 YBCStatus YBCPgDestroyVirtualWalForCDC();
 

commit 84317b7563ad85c48bf5169d63c1486e54e6dd88

[#24308] CDC: Remove dropped tables from replica identity map
Summary:
In the present implementation, the replica identity map is populated at the time of stream creation. When dynamic tables are added to the stream, their entries are added to the replica identity map. However, we never remove entries from replica identity map.

This started causing problems after D37721, where in GetCDCStream, instead of converting docdb table id to pg table id using a conversion logic, a call to master was introduced. Now, if the replica identity map contained an entry corresponding to a dropped table, this call to master would fail with `NotFound` status. This cascades into failure to start replication with `slot does not exist` error.

In order to fix this issue, this diff adds mechanism to remove the entries from replica identity map for a dropped table. The removal will be done by the master background thread, same thread which removes dropped table_ids from stream metadata. The caveat here is that, an entry for a dropped table may remain in the replica identity map for some duration since the removal is done asynchronously. However, the entry will be removed eventually and the connector will succeed in starting replication upon retry.
Jira: DB-13197

Test Plan: ./yb_build.sh --java-test org.yb.pgsql.TestPgReplicationSlot#testWithDropTable

Reviewers: skumar, siddharth.shah, xCluster, hsunder

Reviewed By: skumar

Subscribers: ybase, ycdcxcluster

Tags: #jenkins-ready

Differential Revision: https://phorge.dev.yugabyte.com/D39193

src/yb/master/xrepl_catalog_manager.cc
@@ -2601,6 +2601,7 @@ Status CatalogManager::CleanupCDCSDKDroppedTablesFromStreamInfo(
         if (table_id_iter != ltm->table_id().end()) {
           need_to_update_stream = true;
           ltm.mutable_data()->pb.mutable_table_id()->erase(table_id_iter);
+          ltm.mutable_data()->pb.mutable_replica_identity_map()->erase(table_id);
         }
 
         if (ltm->pb.unqualified_table_id_size() > 0) {
@@ -2609,6 +2610,7 @@ Status CatalogManager::CleanupCDCSDKDroppedTablesFromStreamInfo(
           if (unqualified_table_id_iter != ltm->unqualified_table_id().end()) {
             need_to_update_stream = true;
             ltm.mutable_data()->pb.mutable_unqualified_table_id()->erase(unqualified_table_id_iter);
+            ltm.mutable_data()->pb.mutable_replica_identity_map()->erase(table_id);
           }
         }
       }
@@ -2841,15 +2843,17 @@ Status CatalogManager::GetCDCStream(
       stream_id = VERIFY_RESULT(xrepl::StreamId::FromString(req->stream_id()));
     } else {
       auto replication_slot_name = ReplicationSlotName(req->cdcsdk_ysql_replication_slot_name());
-        if (!cdcsdk_replication_slots_to_stream_map_.contains(replication_slot_name)) {
-          return STATUS(
+      if (!cdcsdk_replication_slots_to_stream_map_.contains(replication_slot_name)) {
+        LOG_WITH_FUNC(WARNING) << "Did not find replication_slot_name: " << replication_slot_name
+                     << " in cdcsdk_replication_slots_to_stream_map_.";
+        return STATUS(
               NotFound, "Could not find CDC stream", req->ShortDebugString(),
-              MasterError(MasterErrorPB::OBJECT_NOT_FOUND));
-        }
-        stream_id = cdcsdk_replication_slots_to_stream_map_.at(replication_slot_name);
+            MasterError(MasterErrorPB::OBJECT_NOT_FOUND));
+      }
+      stream_id = cdcsdk_replication_slots_to_stream_map_.at(replication_slot_name);
     }
 
-    stream = FindPtrOrNull(cdc_stream_map_, stream_id);
+      stream = FindPtrOrNull(cdc_stream_map_, stream_id);
   }
 
   if (stream == nullptr || stream->LockForRead()->is_deleting()) {

java/yb-pgsql/src/test/java/org/yb/pgsql/TestPgReplicationSlot.java
@@ -3062,4 +3062,58 @@ public class TestPgReplicationSlot extends BasePgSQLTest {
 
     stream.close();
   }
+
+  // Added with the fix for #24308.
+  @Test
+  public void testWithDropTable() throws Exception {
+    try (Statement stmt = connection.createStatement()) {
+      stmt.execute("DROP TABLE IF EXISTS test_1");
+      stmt.execute("DROP TABLE IF EXISTS test_2");
+      stmt.execute("CREATE TABLE test_1 (a int primary key, b int)");
+      stmt.execute("CREATE TABLE test_2 (a int primary key, b int)");
+      stmt.execute("CREATE PUBLICATION pub FOR TABLE test_1");
+    }
+
+    String slotName = "test_with_drop_table";
+    Connection conn = getConnectionBuilder().withTServer(0).replicationConnect();
+    PGReplicationConnection replConnection = conn.unwrap(PGConnection.class).getReplicationAPI();
+
+    createSlot(replConnection, slotName, YB_OUTPUT_PLUGIN_NAME);
+    try (Statement stmt = connection.createStatement()) {
+      stmt.execute("DROP TABLE test_2");
+      stmt.execute("INSERT INTO test_1 VALUES(1, 1)");
+    }
+
+    // Sleep for some time so that master background thread removes dropped table from the
+    // replica identity map.
+    Thread.sleep(3000 * kMultiplier);
+    PGReplicationStream stream = replConnection.replicationStream()
+      .logical()
+      .withSlotName(slotName)
+      .withStartPosition(LogSequenceNumber.valueOf(0L))
+      .withSlotOption("proto_version", 1)
+      .withSlotOption("publication_names", "pub")
+      .start();
+
+    List<PgOutputMessage> result = new ArrayList<PgOutputMessage>();
+
+    result.addAll(receiveMessage(stream, 4));
+
+    List<PgOutputMessage> expectedResult = new ArrayList<PgOutputMessage>() {
+      {
+        add(PgOutputBeginMessage.CreateForComparison(LogSequenceNumber.valueOf("0/4"), 2));
+        add(PgOutputRelationMessage.CreateForComparison("public", "test_1", 'c',
+          Arrays.asList(PgOutputRelationMessageColumn.CreateForComparison("a", 23),
+            PgOutputRelationMessageColumn.CreateForComparison("b", 23))));
+        add(PgOutputInsertMessage.CreateForComparison(new PgOutputMessageTuple((short) 2,
+          Arrays.asList(new PgOutputMessageTupleColumnValue("1"),
+            new PgOutputMessageTupleColumnValue("1")))));
+        add(PgOutputCommitMessage.CreateForComparison(
+          LogSequenceNumber.valueOf("0/4"), LogSequenceNumber.valueOf("0/5")));
+      }
+    };
+    assertEquals(expectedResult, result);
+
+    stream.close();
+  }
 }