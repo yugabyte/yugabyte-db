0326136fe3778c301b2f42083a7db3353dc10200
Gaurav Kukreja <gaurav@gauravk.in>
2024-10-25 13:26:27 +0200

    [BACKPORT 2024.2][#24496] YSQL: Assume index contains 1000 rows if statistics not available
    Summary:
    Original commit: fe4811942b5340a87fda0d85028508708b2f49ef / D39297
    If a table has not been ANALYZED, we do not know the number of rows it contains.
    In this case, the `pg_class.reltuples` contains the value `0` in the YB versions
    older than 2024.2 which were based on PG11. In the newer versions based on PG15,
    `pg_class.relutples` contains the value `-1` if relutples is not known.
    
    In YB, if reltuples of a table is not known, we assume the table has 1000 rows.
    This helps the query planner make reasonable plan choices. In the case of index
    however, this logic was missed. This can lead to incorrect index cost estimation
    if the new cost model is used without running `ANALYZE` on the tables. The
    old code model is not affected, because it does not utilize the number of
    tuples in the index in estimating the cost.
    
    This change fixes this issue, and likewise assumes that the index contains
    1000 rows, if reltuples is not known. Tests are added which use the new
    cost model without running ANALYZE on all or some of the tables used in the
    query.
    Jira: DB-13413
    
    Test Plan: tbd
    
    Reviewers: telgersma
    
    Reviewed By: telgersma
    
    Subscribers: yql
    
    Differential Revision: https://phorge.dev.yugabyte.com/D39458

--- src/postgres/src/backend/optimizer/util/plancat.c
+++ src/postgres/src/backend/optimizer/util/plancat.c
@@ -23,6 +23,7 @@
 #include "access/nbtree.h"
 #include "access/sysattr.h"
 #include "access/transam.h"
+#include "access/yb_scan.h"
 #include "access/xlog.h"
 #include "catalog/catalog.h"
 #include "catalog/dependency.h"
@@ -962,145 +963,154 @@ estimate_rel_size(Relation rel, int32 *attr_widths,
 	 */
 	if (IsYBRelation(rel))
 	{
+		if (rel->rd_rel->reltuples < 0)
+		{
+			*tuples = YBC_DEFAULT_NUM_ROWS;
+		}
+		else
+		{
+			*tuples = rel->rd_rel->reltuples;
+		}
+
 		*pages = rel->rd_rel->relpages;
-		*tuples = rel->rd_rel->reltuples;
 		*allvisfrac = 0;
-		return;
 	}
-
-	switch (rel->rd_rel->relkind)
+	else
 	{
-		case RELKIND_RELATION:
-		case RELKIND_INDEX:
-		case RELKIND_MATVIEW:
-		case RELKIND_TOASTVALUE:
-			/* it has storage, ok to call the smgr */
-			curpages = RelationGetNumberOfBlocks(rel);
+		switch (rel->rd_rel->relkind)
+		{
+			case RELKIND_RELATION:
+			case RELKIND_INDEX:
+			case RELKIND_MATVIEW:
+			case RELKIND_TOASTVALUE:
+				/* it has storage, ok to call the smgr */
+				curpages = RelationGetNumberOfBlocks(rel);
 
-			/*
-			 * HACK: if the relation has never yet been vacuumed, use a
-			 * minimum size estimate of 10 pages.  The idea here is to avoid
-			 * assuming a newly-created table is really small, even if it
-			 * currently is, because that may not be true once some data gets
-			 * loaded into it.  Once a vacuum or analyze cycle has been done
-			 * on it, it's more reasonable to believe the size is somewhat
-			 * stable.
-			 *
-			 * (Note that this is only an issue if the plan gets cached and
-			 * used again after the table has been filled.  What we're trying
-			 * to avoid is using a nestloop-type plan on a table that has
-			 * grown substantially since the plan was made.  Normally,
-			 * autovacuum/autoanalyze will occur once enough inserts have
-			 * happened and cause cached-plan invalidation; but that doesn't
-			 * happen instantaneously, and it won't happen at all for cases
-			 * such as temporary tables.)
-			 *
-			 * We approximate "never vacuumed" by "has relpages = 0", which
-			 * means this will also fire on genuinely empty relations.  Not
-			 * great, but fortunately that's a seldom-seen case in the real
-			 * world, and it shouldn't degrade the quality of the plan too
-			 * much anyway to err in this direction.
-			 *
-			 * There are two exceptions wherein we don't apply this heuristic.
-			 * One is if the table has inheritance children.  Totally empty
-			 * parent tables are quite common, so we should be willing to
-			 * believe that they are empty.  Also, we don't apply the 10-page
-			 * minimum to indexes.
-			 */
-			if (curpages < 10 &&
-				rel->rd_rel->relpages == 0 &&
-				!rel->rd_rel->relhassubclass &&
-				rel->rd_rel->relkind != RELKIND_INDEX)
-				curpages = 10;
+				/*
+				* HACK: if the relation has never yet been vacuumed, use a
+				* minimum size estimate of 10 pages.  The idea here is to avoid
+				* assuming a newly-created table is really small, even if it
+				* currently is, because that may not be true once some data gets
+				* loaded into it.  Once a vacuum or analyze cycle has been done
+				* on it, it's more reasonable to believe the size is somewhat
+				* stable.
+				*
+				* (Note that this is only an issue if the plan gets cached and
+				* used again after the table has been filled.  What we're trying
+				* to avoid is using a nestloop-type plan on a table that has
+				* grown substantially since the plan was made.  Normally,
+				* autovacuum/autoanalyze will occur once enough inserts have
+				* happened and cause cached-plan invalidation; but that doesn't
+				* happen instantaneously, and it won't happen at all for cases
+				* such as temporary tables.)
+				*
+				* We approximate "never vacuumed" by "has relpages = 0", which
+				* means this will also fire on genuinely empty relations.  Not
+				* great, but fortunately that's a seldom-seen case in the real
+				* world, and it shouldn't degrade the quality of the plan too
+				* much anyway to err in this direction.
+				*
+				* There are two exceptions wherein we don't apply this heuristic.
+				* One is if the table has inheritance children.  Totally empty
+				* parent tables are quite common, so we should be willing to
+				* believe that they are empty.  Also, we don't apply the 10-page
+				* minimum to indexes.
+				*/
+				if (curpages < 10 &&
+					rel->rd_rel->relpages == 0 &&
+					!rel->rd_rel->relhassubclass &&
+					rel->rd_rel->relkind != RELKIND_INDEX)
+					curpages = 10;
 
-			/* report estimated # pages */
-			*pages = curpages;
-			/* quick exit if rel is clearly empty */
-			if (curpages == 0)
-			{
+				/* report estimated # pages */
+				*pages = curpages;
+				/* quick exit if rel is clearly empty */
+				if (curpages == 0)
+				{
+					*tuples = 0;
+					*allvisfrac = 0;
+					break;
+				}
+				/* coerce values in pg_class to more desirable types */
+				relpages = (BlockNumber) rel->rd_rel->relpages;
+				reltuples = (double) rel->rd_rel->reltuples;
+				relallvisible = (BlockNumber) rel->rd_rel->relallvisible;
+
+				/*
+				* If it's an index, discount the metapage while estimating the
+				* number of tuples.  This is a kluge because it assumes more than
+				* it ought to about index structure.  Currently it's OK for
+				* btree, hash, and GIN indexes but suspect for GiST indexes.
+				*/
+				if (rel->rd_rel->relkind == RELKIND_INDEX &&
+					relpages > 0)
+				{
+					curpages--;
+					relpages--;
+				}
+
+				/* estimate number of tuples from previous tuple density */
+				if (relpages > 0)
+					density = reltuples / (double) relpages;
+				else
+				{
+					/*
+					* When we have no data because the relation was truncated,
+					* estimate tuple width from attribute datatypes.  We assume
+					* here that the pages are completely full, which is OK for
+					* tables (since they've presumably not been VACUUMed yet) but
+					* is probably an overestimate for indexes.  Fortunately
+					* get_relation_info() can clamp the overestimate to the
+					* parent table's size.
+					*
+					* Note: this code intentionally disregards alignment
+					* considerations, because (a) that would be gilding the lily
+					* considering how crude the estimate is, and (b) it creates
+					* platform dependencies in the default plans which are kind
+					* of a headache for regression testing.
+					*/
+					int32		tuple_width;
+
+					tuple_width = get_rel_data_width(rel, attr_widths);
+					tuple_width += MAXALIGN(SizeofHeapTupleHeader);
+					tuple_width += sizeof(ItemIdData);
+					/* note: integer division is intentional here */
+					density = (BLCKSZ - SizeOfPageHeaderData) / tuple_width;
+				}
+				*tuples = rint(density * (double) curpages);
+
+				/*
+				* We use relallvisible as-is, rather than scaling it up like we
+				* do for the pages and tuples counts, on the theory that any
+				* pages added since the last VACUUM are most likely not marked
+				* all-visible.  But costsize.c wants it converted to a fraction.
+				*/
+				if (relallvisible == 0 || curpages <= 0)
+					*allvisfrac = 0;
+				else if ((double) relallvisible >= curpages)
+					*allvisfrac = 1;
+				else
+					*allvisfrac = (double) relallvisible / curpages;
+				break;
+			case RELKIND_SEQUENCE:
+				/* Sequences always have a known size */
+				*pages = 1;
+				*tuples = 1;
+				*allvisfrac = 0;
+				break;
+			case RELKIND_FOREIGN_TABLE:
+				/* Just use whatever's in pg_class */
+				*pages = rel->rd_rel->relpages;
+				*tuples = rel->rd_rel->reltuples;
+				*allvisfrac = 0;
+				break;
+			default:
+				/* else it has no disk storage; probably shouldn't get here? */
+				*pages = 0;
 				*tuples = 0;
 				*allvisfrac = 0;
 				break;
-			}
-			/* coerce values in pg_class to more desirable types */
-			relpages = (BlockNumber) rel->rd_rel->relpages;
-			reltuples = (double) rel->rd_rel->reltuples;
-			relallvisible = (BlockNumber) rel->rd_rel->relallvisible;
-
-			/*
-			 * If it's an index, discount the metapage while estimating the
-			 * number of tuples.  This is a kluge because it assumes more than
-			 * it ought to about index structure.  Currently it's OK for
-			 * btree, hash, and GIN indexes but suspect for GiST indexes.
-			 */
-			if (rel->rd_rel->relkind == RELKIND_INDEX &&
-				relpages > 0)
-			{
-				curpages--;
-				relpages--;
-			}
-
-			/* estimate number of tuples from previous tuple density */
-			if (relpages > 0)
-				density = reltuples / (double) relpages;
-			else
-			{
-				/*
-				 * When we have no data because the relation was truncated,
-				 * estimate tuple width from attribute datatypes.  We assume
-				 * here that the pages are completely full, which is OK for
-				 * tables (since they've presumably not been VACUUMed yet) but
-				 * is probably an overestimate for indexes.  Fortunately
-				 * get_relation_info() can clamp the overestimate to the
-				 * parent table's size.
-				 *
-				 * Note: this code intentionally disregards alignment
-				 * considerations, because (a) that would be gilding the lily
-				 * considering how crude the estimate is, and (b) it creates
-				 * platform dependencies in the default plans which are kind
-				 * of a headache for regression testing.
-				 */
-				int32		tuple_width;
-
-				tuple_width = get_rel_data_width(rel, attr_widths);
-				tuple_width += MAXALIGN(SizeofHeapTupleHeader);
-				tuple_width += sizeof(ItemIdData);
-				/* note: integer division is intentional here */
-				density = (BLCKSZ - SizeOfPageHeaderData) / tuple_width;
-			}
-			*tuples = rint(density * (double) curpages);
-
-			/*
-			 * We use relallvisible as-is, rather than scaling it up like we
-			 * do for the pages and tuples counts, on the theory that any
-			 * pages added since the last VACUUM are most likely not marked
-			 * all-visible.  But costsize.c wants it converted to a fraction.
-			 */
-			if (relallvisible == 0 || curpages <= 0)
-				*allvisfrac = 0;
-			else if ((double) relallvisible >= curpages)
-				*allvisfrac = 1;
-			else
-				*allvisfrac = (double) relallvisible / curpages;
-			break;
-		case RELKIND_SEQUENCE:
-			/* Sequences always have a known size */
-			*pages = 1;
-			*tuples = 1;
-			*allvisfrac = 0;
-			break;
-		case RELKIND_FOREIGN_TABLE:
-			/* Just use whatever's in pg_class */
-			*pages = rel->rd_rel->relpages;
-			*tuples = rel->rd_rel->reltuples;
-			*allvisfrac = 0;
-			break;
-		default:
-			/* else it has no disk storage; probably shouldn't get here? */
-			*pages = 0;
-			*tuples = 0;
-			*allvisfrac = 0;
-			break;
+		}
 	}
 }
 
