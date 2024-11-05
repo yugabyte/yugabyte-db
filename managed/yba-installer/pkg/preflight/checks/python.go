9e734a60895ab26ed947369e20991f51c0f0fd30
Muthu Chidambaram <mchidambaram@yugabyte.com>
2024-11-05 20:00:37 +0000

    [PLAT-15313] Add yba-ctl preflight check for OpenSSL version
    Summary: On certain systems like Amazon Linux 2 (ami-06e85d4c3149db26a, us-west-2) we have python 3.11 but the openSSL version is still 1.0.2, so the python ssl module does not work. We need a preflight check to ensure OpenSSL 1.1.1+ on python versions 3.10+.
    
    Test Plan:
    install YBA with yba-ctl, ensure preflight check fails on ami-06e85d4c3149db26a
    upgrade openssl version, then ensure install succeeds
    
    Reviewers: dshubin, sanketh
    
    Reviewed By: dshubin
    
    Subscribers: yugaware
    
    Differential Revision: https://phorge.dev.yugabyte.com/D39707

--- managed/yba-installer/pkg/preflight/checks/python.go
+++ managed/yba-installer/pkg/preflight/checks/python.go
@@ -7,6 +7,7 @@ package checks
 import (
 	"fmt"
 	"regexp"
+	"strconv"
 	"strings"
 
 	"github.com/yugabyte/yugabyte-db/managed/yba-installer/pkg/common/shell"
@@ -16,11 +17,10 @@ import (
 // Python checks to ensure the correct version of python exists
 var Python = &pythonCheck{"python", false}
 
-var pythonBinaryNames = []string{
-	"python", "python3", "python3.8", "python3.9", "python3.10", "python3.11",
-}
+var pythonBinaryNames = []string{"python3.8", "python3.11", "python3.10", "python3.9", "python3"}
 
-var pythonVersionRegex = regexp.MustCompile(`Python 3.8|Python 3.9|Python 3.10|Python 3.11`)
+var pythonVersionRegex = regexp.MustCompile(`Python (\d+)\.(\d+)`)
+var openSSLRegex = regexp.MustCompile(`OpenSSL (\d+)\.(\d+)\.(\d+)`)
 
 type pythonCheck struct {
 	name        string
@@ -51,11 +51,41 @@ func (p pythonCheck) Execute() Result {
 			log.Debug("python binary " + binary + " failed, trying next")
 			continue
 		}
-		outputTrimmed := strings.TrimSuffix(out.StdoutString(), "\n")
-		if pythonVersionRegex.MatchString(outputTrimmed) {
-			log.Info("System meets Python installation requirements with version " + outputTrimmed)
-			return res
+		outputTrimmed := strings.TrimSpace(out.StdoutString())
+		match := pythonVersionRegex.FindStringSubmatch(outputTrimmed)
+		if len(match) < 3 {
+			continue
 		}
+		majorVersion, _ := strconv.Atoi(match[1])
+		minorVersion, _ := strconv.Atoi(match[2])
+		if majorVersion == 3 && minorVersion >= 8 && minorVersion <= 11 {
+			log.Info("System meets Python installation requirements with version " + outputTrimmed)
+			if minorVersion >= 10 {
+				// Need to check openssl version 1.1.1+ on Python 3.10+ because of https://docs.python.org/3/library/ssl.html#module-ssl
+				op := shell.Run("openssl", "version")
+				if !out.Succeeded() {
+					log.Debug("need openssl installed on system")
+					res.Error = fmt.Errorf("System does not have OpenSSL installed. Please install OpenSSL 1.1.1+.")
+					res.Status = StatusCritical
+					return res
+				}
+				opTrim := strings.TrimSpace(op.StdoutString())
+				sslMatch := openSSLRegex.FindStringSubmatch(opTrim)
+				if len(sslMatch) < 4 {
+					log.Warn("could not validate openssl version, skipping check")
+				}
+				major, _ := strconv.Atoi(sslMatch[1])
+				minor, _ := strconv.Atoi(sslMatch[2])
+				patch, _ := strconv.Atoi(sslMatch[3])
+				if (major < 1) || (major == 1 && minor < 1) || (major == 1 && minor == 1 && patch < 1) {
+					log.Debug("system does not meet openssl requirements: " + sslMatch[0])
+					res.Error = fmt.Errorf("System has OpenSSL version %s, needs at least 1.1.1", sslMatch[0])
+					res.Status = StatusCritical
+					return res
+				}
+			}
+		}
+		return res
 	}
 
 	res.Error = fmt.Errorf("System does not meet Python requirements. Please install any " +
