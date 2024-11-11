c6ae0de71e10ba620cde2f59daab7e32f144288b
Subramanian Neelakantan <sneelakantan@yugabyte.com>
2024-11-11 12:48:01 +0000

    [PLAT-16067] Adding SetUniverseKeys task to k8s non-restart gflags upgrade
    Summary: As noticed by SoftBank, a non-restart GFlags upgrade on a k8s EAR enabled universe always fails with a timeout waiting for tserver to come up. This workflow deletes all the pods - both tserver and master pods - and then immediately waits for tserver to come up. The tserver first needs the master to come up, and also have the encryption key set in its memory. So sequencing the tasks correctly for the k8s workflow. Verified that the VM workflow is doing the right thing here already.
    
    Test Plan: Manually verified that non-restart GFlags upgrade works correctly on a EAR enabled universe.
    
    Reviewers: vkumar, skurapati, anijhawan
    
    Reviewed By: anijhawan
    
    Subscribers: yugaware
    
    Differential Revision: https://phorge.dev.yugabyte.com/D39879

--- managed/src/main/java/com/yugabyte/yw/commissioner/tasks/KubernetesTaskBase.java
+++ managed/src/main/java/com/yugabyte/yw/commissioner/tasks/KubernetesTaskBase.java
@@ -763,9 +763,12 @@ public abstract class KubernetesTaskBase extends UniverseDefinitionTaskBase {
         getUniverse(),
         SubTaskGroupType.ConfigureUniverse);
     if (serverType.equals(ServerType.EITHER)) {
-      createWaitForServersTasks(tserverNodes, ServerType.TSERVER)
-          .setSubTaskGroupType(SubTaskGroupType.ConfigureUniverse);
       createWaitForServersTasks(masterNodes, ServerType.MASTER)
+          .setSubTaskGroupType(SubTaskGroupType.ConfigureUniverse);
+      SubTaskGroup setEncKeys =
+          createSetActiveUniverseKeysTask().setSubTaskGroupType(SubTaskGroupType.ConfigureUniverse);
+      getRunnableTask().addSubTaskGroup(setEncKeys);
+      createWaitForServersTasks(tserverNodes, ServerType.TSERVER)
           .setSubTaskGroupType(SubTaskGroupType.ConfigureUniverse);
     } else if (serverType.equals(ServerType.TSERVER)) {
       createWaitForServersTasks(tserverNodes, ServerType.TSERVER)
@@ -773,6 +776,9 @@ public abstract class KubernetesTaskBase extends UniverseDefinitionTaskBase {
     } else if (serverType.equals(ServerType.MASTER)) {
       createWaitForServersTasks(masterNodes, ServerType.MASTER)
           .setSubTaskGroupType(SubTaskGroupType.ConfigureUniverse);
+      SubTaskGroup setEncKeys =
+          createSetActiveUniverseKeysTask().setSubTaskGroupType(SubTaskGroupType.ConfigureUniverse);
+      getRunnableTask().addSubTaskGroup(setEncKeys);
     }
   }
 
