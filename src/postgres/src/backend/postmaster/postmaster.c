3122eead6e9c9902a64b734191b69a8ad0c23e00
Manav Kumar <mkumar@yugabyte.com>
2024-11-22 12:14:31 +0000

    [#25004] YSQL: Fix the backend type of the physical connection in ysql conn mgr
    Summary:
    Post pg15 merge into the master, the type/name of backends created for physical connections in transaction pool are coming to be `client backend` instead of `yb-conn-mgr worker connection`. This diff fixes this issue.
    
    **Fix**
    Based on value of guc variable `yb_is_client_ysqlconnmgr` the type of backend set to  `yb-conn-mgr worker connection` or `client backend`. While parsing the startup packet of the transactional backend process the guc variables coming in the startup packet are not yet processed (they are processed in later stage of initialization of the backend). Therefore in this diff during parsing of the startup packet itself `yb_is_client_ysqlconnmgr` guc variable is been set, based on it's correct value the type of backend is assigned.
    Jira: DB-14148
    
    Test Plan:
    Jenkins: enable connection manager, all tests
    
    Ensure below connection manager test is running successfully on master branch
    ```/yb_build.sh --java-test org.yb.ysqlconnmgr.TestMisc#testBackendType```
    
    Reviewers: skumar, stiwary, rbarigidad, vpatibandla, devansh.saxena
    
    Reviewed By: stiwary
    
    Subscribers: yql
    
    Differential Revision: https://phorge.dev.yugabyte.com/D40139

--- src/postgres/src/backend/postmaster/postmaster.c
+++ src/postgres/src/backend/postmaster/postmaster.c
@@ -2389,7 +2389,6 @@ retry1:
 							   "if the connection is made over unix domain "
 							   "socket")));
 
-				yb_is_client_ysqlconnmgr = yb_is_auth_backend;
 			}
 			else if (YBIsEnabledInPostgresEnvVar()
 					 && strcmp(nameptr, "yb_auth_remote_host") == 0)
@@ -2408,6 +2407,24 @@ retry1:
 
 				yb_logical_conn_type = *pstrdup(valptr);
 				yb_logical_conn_type_provided = true;
+			}
+			else if (YBIsEnabledInPostgresEnvVar()
+					 && strcmp(nameptr, "yb_is_client_ysqlconnmgr") == 0)
+			{
+				if (!parse_bool(valptr, &yb_is_client_ysqlconnmgr))
+					ereport(FATAL,
+							(errcode(ERRCODE_INVALID_PARAMETER_VALUE),
+							 errmsg("invalid value for parameter \"%s\": \"%s\"",
+									"yb_is_client_ysqlconnmgr",
+									valptr),
+							 errhint("Valid values are: \"false\", 0, \"true\", 1.")));
+
+				/* Client needs to be connected on the unix domain socket */
+				if (port->raddr.addr.ss_family != AF_UNIX)
+					ereport(FATAL,
+							(errcode(ERRCODE_PROTOCOL_VIOLATION),
+							 errmsg("ysql connection manager makes all connections "
+							   "over unix domain socket to postgres")));
 			}
 			else if (strncmp(nameptr, "_pq_.", 5) == 0)
 			{
@@ -2553,7 +2570,7 @@ retry1:
 
 	if (am_walsender)
 		MyBackendType = B_WAL_SENDER;
-	else if (YbIsClientYsqlConnMgr())
+	else if (YBIsEnabledInPostgresEnvVar() && yb_is_client_ysqlconnmgr)
 		MyBackendType = YB_YSQL_CONN_MGR;
 	else
 		MyBackendType = B_BACKEND;
@@ -5707,7 +5724,7 @@ sigusr1_handler(SIGNAL_ARGS)
 	}
 
 	if (CheckPostmasterSignal(PMSIGNAL_START_WALRECEIVER) &&
-	    !YBIsEnabledInPostgresEnvVar())
+		!YBIsEnabledInPostgresEnvVar())
 	{
 		/* Startup Process wants us to start the walreceiver process. */
 		/* Start immediately if possible, else remember request for later. */
