cde07deff67d3ea2a2807a8fa307fe05a1a82c60
Sumukh-Phalgaonkar <sumukhphalgaonkar@gmail.com>
2024-11-26 21:20:42 +0530

    [BACKPORT 2024.2][#24975] CDC: Dont consider expired and not of interest entries for metrics calculation
    Summary:
    #####Backport Description
    No merge conflicts were encountered.
    
    #####Original Description
    In the existing implementation for metrics, we determine that an entry is not being polled based on the absence of last replication time in state table entry. However with the introduction of consistent snapshot streams this mechanism is no longer valid, since this field is populated for every state table entry at the time of stream creation.
    
    This diff fixes this issue by explicitly verifying if a particular state table entry has expired or become not of interest. If such an entry is found then, we don't consider it for metric calculation. We will explicitly clear the values of metric entity associated with such an entry.
    
    However a thing to note here is, in the scenario where some of the tables are not being polled, we will still consider the tablets of these tables for metrics calculation until the time this table becomes not of interest.
    Jira: DB-14114
    
    Original commit: dc701f6128624c23ab05b6fb99d501c25e54651b / D40274
    
    Test Plan:
    ./yb_build.sh --cxx-test integration-tests_cdcsdk_ysql-test --gtest_filter CDCSDKYsqlTest.TestLagMetricWithNotOfInterestTableAndCSStream
    ./yb_build.sh --cxx-test integration-tests_cdcsdk_ysql-test --gtest_filter CDCSDKYsqlTest.TestLagMetricWithExpiredTableAndCSStream
    
    Reviewers: skumar, siddharth.shah, vkushwaha
    
    Reviewed By: siddharth.shah
    
    Subscribers: ycdcxcluster, vkushwaha
    
    Tags: #jenkins-ready
    
    Differential Revision: https://phorge.dev.yugabyte.com/D40285

--- src/yb/cdc/cdc_service.cc
+++ src/yb/cdc/cdc_service.cc
@@ -2119,6 +2119,7 @@ void CDCServiceImpl::ProcessMetricsForEmptyChildrenTablets(
 void CDCServiceImpl::UpdateMetrics() {
   auto tablet_checkpoints = impl_->TabletCheckpointsCopy();
   TabletInfoToLastReplicationTimeMap cdc_state_tablets_to_last_replication_time;
+  std::unordered_set<TabletStreamInfo, TabletStreamInfo::Hash> expired_entries;
   EmptyChildrenTabletMap empty_children_tablets;
 
   Status iteration_status;
@@ -2166,6 +2167,11 @@ void CDCServiceImpl::UpdateMetrics() {
     bool is_leader = (tablet_peer->LeaderStatus() == consensus::LeaderStatus::LEADER_AND_READY);
 
     if (record.GetSourceType() == CDCSDK) {
+      if (entry.active_time.has_value() &&
+          CheckTabletExpiredOrNotOfInterest(tablet_info, *entry.active_time)) {
+        expired_entries.insert(tablet_info);
+        continue;
+      }
       auto tablet_metric_result = GetCDCSDKTabletMetrics(
           *tablet_peer.get(), tablet_info.stream_id, CreateMetricsEntityIfNotFound::kTrue,
           record.GetReplicationSlotName());
@@ -2279,7 +2285,8 @@ void CDCServiceImpl::UpdateMetrics() {
   // longer replicating.
   for (const auto& checkpoint : tablet_checkpoints) {
     const TabletStreamInfo& tablet_info = checkpoint.producer_tablet_info;
-    if (!cdc_state_tablets_to_last_replication_time.contains(tablet_info)) {
+    if (!cdc_state_tablets_to_last_replication_time.contains(tablet_info) ||
+        expired_entries.contains(tablet_info)) {
       // We're no longer replicating this tablet, so set lag to 0.
       auto tablet_peer = context_->LookupTablet(checkpoint.tablet_id());
       if (!tablet_peer) {
@@ -4034,6 +4041,25 @@ Status CDCServiceImpl::CheckTabletNotOfInterest(
       InternalError,
       "Stream ID $0 unpolled for too long for Tablet ID $1", producer_tablet.stream_id,
       producer_tablet.tablet_id);
+}
+
+bool CDCServiceImpl::CheckTabletExpiredOrNotOfInterest(
+    const TabletStreamInfo& producer_tablet, int64_t last_active_time_passed) {
+  auto s = CheckStreamActive(producer_tablet, last_active_time_passed);
+  if (!s.ok()) {
+    VLOG(3) << "Tablet: " << producer_tablet.tablet_id
+            << " expired for stream: " << producer_tablet.stream_id;
+    return true;
+  }
+
+  s = CheckTabletNotOfInterest(producer_tablet, last_active_time_passed);
+  if (!s.ok()) {
+    VLOG(3) << "Tablet: " << producer_tablet.tablet_id
+            << " is not of interest for stream: " << producer_tablet.stream_id;
+    return true;
+  }
+
+  return false;
 }
 
 Result<int64_t> CDCServiceImpl::GetLastActiveTime(
