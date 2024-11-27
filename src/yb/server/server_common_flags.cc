44049baad809017302060d5f843338a8f5b4099b
Utkarsh Munjal <utkarsh.munjal@yugabyte.com>
2024-11-27 18:43:59 +0530

    [#25030] YCQL: Replace autoflag ycql_suppress_group_by_error with a regular gFlag
    Summary:
    In commit 80def06f8c19ad7cbc52f41b4be48d158157a418, an autoflag `ycql_suppress_group_by_error` was introduced to suppress the error raised when using the `GROUP BY` clause.
    
     1. This should not have been an autoflag because we want users to decide this behavior.
      - A new regular gFlag, `ycql_ignore_group_by_error`, has been introduced for future use since autoflags cannot be demoted or removed.
      - When set to true(default), no error is raised, and the `GROUP BY` clause is ignored. When set to false, the error is raised as usual.
     2. The autoflag definition was previously in pt_select.cc. Its definition has now been moved to server_common_flags.cc because, due to differences in the linking scheme for various build types, this flag was not available in master flags for certain build types.
      - The flag does not determine/change any behaviour.
    Jira: DB-14168
    
    Test Plan:
    ./yb_build.sh --java-test org.yb.cql.TestCreateTable#testCreateTableWithColumnNamedGroup
    ./yb_build.sh --java-test org.yb.cql.TestSelect#testGroupBy
    
    Reviewers: stiwary, hsunder, skumar
    
    Reviewed By: stiwary, hsunder
    
    Subscribers: yql, ybase
    
    Differential Revision: https://phorge.dev.yugabyte.com/D40327

--- src/yb/server/server_common_flags.cc
+++ src/yb/server/server_common_flags.cc
@@ -31,3 +31,10 @@ DEFINE_NON_RUNTIME_bool(enable_pg_cron, false,
 DEFINE_RUNTIME_AUTO_PG_FLAG(
     bool, yb_allow_replication_slot_lsn_types, kLocalPersisted, false, true,
     "Enable LSN types to be specified while creating replication slots.");
+
+// This autoflag was introduced in commit 80def06f8c19ad7cbc52f41b4be48d158157a418, but it had some
+// flaws and the feature required a regular gFlag. As of 27.11.2024 there does not exist an infra to
+// remove or demote an autoflag, therefore this flag is going to be a dummy flag. A new gFlag
+// ycql_ignore_group_by_error in introduced for the same functionality.
+DEFINE_RUNTIME_AUTO_bool(ycql_suppress_group_by_error, kLocalVolatile, true, false,
+    "This flag is deprecated, please use ycql_ignore_group_by_error");
