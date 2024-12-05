yugabyte/yugabyte-db

commit 0ff19c039f54da6824fed9b5e9e5e11b71f6811c

[#17798] Messenger should not queue new tasks when it is closing
Summary:
Sometimes tests might fail due to scheduled tasks in Messenger after the messenger starts closing. We should reject attempts to queue such tasks.
E.g. in XClusterTestParams/XClusterTest.ApplyOperationsRandomFailures/0 we've observed
```
[P-ts-1] W0615 16:26:05.618098 23288 meta_cache.cc:853] 0x0000614001150c58 -> LookupByIdRpc(tablet: {test_table_0_tablet_id1}, num_attempts: 3): Aborted (yb/rpc/reactor.cc:899): CDCConsumerRemote_R010: : Cannot schedule a reactor task. Current state: kClosed, even_if_not_running: 0
[P-ts-1] F0615 16:26:05.618834 22302 messenger.cc:226] Scheduled tasks is not empty after messenger shutdown: [{25, 0x0000611000d7a2d8 -> { id: 25 source_location: ../../src/yb/rpc/rpc.cc:168 }}]
```

Also, in Connection::OutboundQueued, there was a possibility of DFATAL if scheduling a DestroyConnection call as a reactor task fails. Previously, this could result in test failures such as e.g. in BackupTxnTest.CleanupAfterRestart (log: https://gist.githubusercontent.com/mbautin/df9602b8e1fc6208ce12cbe3d0da085e/raw). However, Connection::OutboundQueued must already run in the reactor thread, and we can just call DestroyConnection directly in the same thread if we fail to schedule it as a task. It still makes sense to try to schedule it as a deferred reactor task first so that it would run after all other tasks in the queue, to preserve existing behavior.

Other changes in this revision:
- Rename percpu_rwlock to PerCpuRwMutex and add a dedicated PerCpuRwSharedLock class, compatible with thread safety analysis annotations, to be used with PerCpuRwMutex instead of shared_lock of the particular CPU's lock.
- Add some thread safety analysis annotations to Messenger.
- Replace std::shared_lock with our existing SharedLock wrapper in most places where the API permits.
- Remove unnecessary template arguments to std::lock_guard.

Test Plan: Jenkins

Reviewers: sergei, xCluster

src/yb/master/master_rpc.cc
@@ -183,7 +183,7 @@ void GetLeaderMasterRpc::SendRpc() {
   std::vector<rpc::Rpcs::Handle> handles;
   handles.reserve(size);
   {
-    std::lock_guard<simple_spinlock> l(lock_);
+    std::lock_guard l(lock_);
     pending_responses_ = size;
   }
 
@@ -229,7 +229,7 @@ void GetLeaderMasterRpc::Finished(const Status& status) {
   VLOG(4) << "Completed GetLeaderMasterRpc, calling callback with status "
           << status.ToString();
   {
-    std::lock_guard<simple_spinlock> l(lock_);
+    std::lock_guard l(lock_);
     // 'completed_' prevents 'user_cb_' from being invoked twice.
     if (completed_) {
       return;
@@ -255,7 +255,7 @@ void GetLeaderMasterRpc::GetMasterRegistrationRpcCbForNode(
   // pick the one with the highest term/index as the leader.
   Status new_status = status;
   {
-    std::lock_guard<simple_spinlock> lock(lock_);
+    std::lock_guard lock(lock_);
     --pending_responses_;
     if (completed_) {
       // If 'user_cb_' has been invoked (see Finished above), we can

src/yb/rpc/messenger.cc
@@ -79,7 +79,6 @@ using namespace std::literals;
 using namespace std::placeholders;
 using namespace yb::size_literals;
 
-using std::shared_lock;
 using std::shared_ptr;
 using std::string;
 
@@ -195,7 +194,7 @@ void Messenger::Shutdown() {
   std::vector<Reactor*> reactors;
   std::unique_ptr<Acceptor> acceptor;
   {
-    std::lock_guard<percpu_rwlock> guard(lock_);
+    std::lock_guard guard(lock_);
 
     acceptor.swap(acceptor_);
 
@@ -222,7 +221,7 @@ void Messenger::Shutdown() {
   io_thread_pool_.Join();
 
   {
-    std::lock_guard<std::mutex> guard(mutex_scheduled_tasks_);
+    std::lock_guard guard(mutex_scheduled_tasks_);
     LOG_IF(DFATAL, !scheduled_tasks_.empty())
         << "Scheduled tasks is not empty after messenger shutdown: "
         << yb::ToString(scheduled_tasks_);
@@ -238,7 +237,7 @@ Status Messenger::ListenAddress(
     Endpoint* bound_endpoint) {
   Acceptor* acceptor;
   {
-    std::lock_guard<percpu_rwlock> guard(lock_);
+    std::lock_guard guard(lock_);
     if (!acceptor_) {
       acceptor_.reset(new Acceptor(
           metric_entity_, std::bind(&Messenger::RegisterInboundSocket, this, factory, _1, _2)));
@@ -259,7 +258,7 @@ Status Messenger::StartAcceptor() {
     p.second->FillEndpoints(&rpc_endpoints_);
   }
 
-  std::lock_guard<percpu_rwlock> guard(lock_);
+  std::lock_guard guard(lock_);
   if (acceptor_) {
     return acceptor_->Start();
   }
@@ -283,7 +282,7 @@ void Messenger::BreakConnectivity(const IpAddress& address, bool incoming, bool 
 
   boost::optional<CountDownLatch> latch;
   {
-    std::lock_guard<percpu_rwlock> guard(lock_);
+    std::lock_guard guard(lock_);
     if (broken_connectivity_from_.empty() || broken_connectivity_to_.empty()) {
       has_broken_connectivity_.store(true, std::memory_order_release);
     }
@@ -339,7 +338,7 @@ void Messenger::RestoreConnectivity(const IpAddress& address, bool incoming, boo
   LOG(INFO) << "TEST: Restore " << (incoming ? "incoming" : "") << "/"
             << (outgoing ? "outgoing" : "") << " connectivity with: " << address;
 
-  std::lock_guard<percpu_rwlock> guard(lock_);
+  std::lock_guard guard(lock_);
   if (incoming) {
     broken_connectivity_from_.erase(address);
   }
@@ -353,7 +352,7 @@ void Messenger::RestoreConnectivity(const IpAddress& address, bool incoming, boo
 
 bool Messenger::TEST_ShouldArtificiallyRejectIncomingCallsFrom(const IpAddress &remote) {
   if (has_broken_connectivity_.load(std::memory_order_acquire)) {
-    shared_lock<rw_spinlock> guard(lock_.get_lock());
+    PerCpuRwSharedLock guard(lock_);
     return broken_connectivity_from_.count(remote) != 0;
   }
   return false;
@@ -361,7 +360,7 @@ bool Messenger::TEST_ShouldArtificiallyRejectIncomingCallsFrom(const IpAddress &
 
 bool Messenger::TEST_ShouldArtificiallyRejectOutgoingCallsTo(const IpAddress &remote) {
   if (has_broken_connectivity_.load(std::memory_order_acquire)) {
-    shared_lock<rw_spinlock> guard(lock_.get_lock());
+    PerCpuRwSharedLock guard(lock_);
     return broken_connectivity_to_.count(remote) != 0;
   }
   return false;
@@ -379,7 +378,7 @@ Status Messenger::TEST_GetReactorMetrics(size_t reactor_idx, ReactorMetrics* met
 void Messenger::ShutdownAcceptor() {
   std::unique_ptr<Acceptor> acceptor;
   {
-    std::lock_guard<percpu_rwlock> guard(lock_);
+    std::lock_guard guard(lock_);
     acceptor.swap(acceptor_);
   }
   if (acceptor) {
@@ -396,7 +395,7 @@ rpc::ThreadPool& Messenger::ThreadPool(ServicePriority priority) {
       if (high_priority_thread_pool) {
         return *high_priority_thread_pool;
       }
-      std::lock_guard<std::mutex> lock(mutex_high_priority_thread_pool_);
+      std::lock_guard lock(mutex_high_priority_thread_pool_);
       high_priority_thread_pool = high_priority_thread_pool_.get();
       if (high_priority_thread_pool) {
         return *high_priority_thread_pool;
@@ -580,7 +579,7 @@ Messenger::Messenger(const MessengerBuilder &bld)
 }
 
 Messenger::~Messenger() {
-  std::lock_guard<percpu_rwlock> guard(lock_);
+  std::lock_guard guard(lock_);
   // This logging and the corresponding logging in the constructor is here to track down the
   // occasional CHECK(closing_) failure below in some tests (ENG-2838).
   VLOG(1) << "Messenger destructor for " << this << " called at:\n" << GetStackTrace();
@@ -591,7 +590,6 @@ Messenger::~Messenger() {
   }
 #endif
   CHECK(closing_) << "Should have already shut down";
-  reactors_.clear();
 }
 
 size_t Messenger::max_concurrent_requests() const {
@@ -623,7 +621,7 @@ Status Messenger::Init(const MessengerBuilder &bld) {
 
 Status Messenger::DumpRunningRpcs(const DumpRunningRpcsRequestPB& req,
                                   DumpRunningRpcsResponsePB* resp) {
-  shared_lock<rw_spinlock> guard(lock_.get_lock());
+  PerCpuRwSharedLock guard(lock_);
   for (const auto& reactor : reactors_) {
     RETURN_NOT_OK(reactor->DumpRunningRpcs(req, resp));
   }
@@ -632,7 +630,7 @@ Status Messenger::DumpRunningRpcs(const DumpRunningRpcsRequestPB& req,
 
 Status Messenger::QueueEventOnAllReactors(
     ServerEventListPtr server_event, const SourceLocation& source_location) {
-  shared_lock<rw_spinlock> guard(lock_.get_lock());
+  PerCpuRwSharedLock guard(lock_);
   Status overall_status;
   for (const auto& reactor : reactors_) {
     auto queuing_status = reactor->QueueEventOnAllConnections(server_event, source_location);
@@ -651,7 +649,7 @@ Status Messenger::QueueEventOnAllReactors(
 Status Messenger::QueueEventOnFilteredConnections(
     ServerEventListPtr server_event, const SourceLocation& source_location,
     ConnectionFilter connection_filter) {
-  shared_lock<rw_spinlock> guard(lock_.get_lock());
+  PerCpuRwSharedLock guard(lock_);
   Status overall_status;
   for (const auto& reactor : reactors_) {
     auto queuing_status =
@@ -670,7 +668,7 @@ Status Messenger::QueueEventOnFilteredConnections(
 
 void Messenger::RemoveScheduledTask(ScheduledTaskId id) {
   CHECK_GT(id, 0);
-  std::lock_guard<std::mutex> guard(mutex_scheduled_tasks_);
+  std::lock_guard guard(mutex_scheduled_tasks_);
   scheduled_tasks_.erase(id);
 }
 
@@ -680,7 +678,7 @@ void Messenger::AbortOnReactor(ScheduledTaskId task_id) {
 
   std::shared_ptr<DelayedTask> task;
   {
-    std::lock_guard<std::mutex> guard(mutex_scheduled_tasks_);
+    std::lock_guard guard(mutex_scheduled_tasks_);
     auto iter = scheduled_tasks_.find(task_id);
     if (iter != scheduled_tasks_.end()) {
       task = iter->second;
@@ -694,6 +692,9 @@ void Messenger::AbortOnReactor(ScheduledTaskId task_id) {
 
 Result<ScheduledTaskId> Messenger::ScheduleOnReactor(
     StatusFunctor func, MonoDelta when, const SourceLocation& source_location) {
+  if (closing_.load(std::memory_order_acquire)) {
+    return STATUS(Aborted, "Cannot schedule task, messenger is closing");
+  }
   DCHECK(!reactors_.empty());
 
   // If we're already running on a reactor thread, reuse it.
@@ -713,7 +714,7 @@ Result<ScheduledTaskId> Messenger::ScheduleOnReactor(
   auto task = std::make_shared<DelayedTask>(
       std::move(func), when, task_id, source_location, this);
   {
-    std::lock_guard<std::mutex> guard(mutex_scheduled_tasks_);
+    std::lock_guard guard(mutex_scheduled_tasks_);
     scheduled_tasks_.emplace(task_id, task);
   }
 
@@ -724,7 +725,7 @@ Result<ScheduledTaskId> Messenger::ScheduleOnReactor(
   }
 
   {
-    std::lock_guard<std::mutex> guard(mutex_scheduled_tasks_);
+    std::lock_guard guard(mutex_scheduled_tasks_);
     scheduled_tasks_.erase(task_id);
   }
 

src/yb/rpc/messenger.h
@@ -332,8 +332,7 @@ class Messenger : public ProxyContext {
 
   const Protocol* const listen_protocol_;
 
-  // Protects closing_, acceptor_pools_.
-  mutable percpu_rwlock lock_;
+  mutable PerCpuRwMutex lock_;
 
   std::atomic_bool closing_ = false;
 
@@ -349,7 +348,7 @@ class Messenger : public ProxyContext {
   const scoped_refptr<Histogram> outgoing_queue_time_;
 
   // Acceptor which is listening on behalf of this messenger.
-  std::unique_ptr<Acceptor> acceptor_;
+  std::unique_ptr<Acceptor> acceptor_ GUARDED_BY(lock_);
   IpAddress outbound_address_v4_;
   IpAddress outbound_address_v6_;
 
@@ -359,14 +358,15 @@ class Messenger : public ProxyContext {
 
   std::mutex mutex_scheduled_tasks_;
 
-  std::unordered_map<ScheduledTaskId, std::shared_ptr<DelayedTask>> scheduled_tasks_;
+  std::unordered_map<ScheduledTaskId, std::shared_ptr<DelayedTask>> scheduled_tasks_
+      GUARDED_BY(mutex_scheduled_tasks_);
 
   // Flag that we have at least on address with artificially broken connectivity.
   std::atomic<bool> has_broken_connectivity_ = {false};
 
   // Set of addresses with artificially broken connectivity.
-  std::unordered_set<IpAddress, IpAddressHash> broken_connectivity_from_;
-  std::unordered_set<IpAddress, IpAddressHash> broken_connectivity_to_;
+  std::unordered_set<IpAddress, IpAddressHash> broken_connectivity_from_ GUARDED_BY(lock_);
+  std::unordered_set<IpAddress, IpAddressHash> broken_connectivity_to_ GUARDED_BY(lock_);
 
   IoThreadPool io_thread_pool_;
   Scheduler scheduler_;

src/yb/rpc/rpc.cc
@@ -303,7 +303,7 @@ Rpcs::Rpcs(std::mutex* mutex) {
 CoarseTimePoint Rpcs::DoRequestAbortAll(RequestShutdown shutdown) {
   std::vector<Calls::value_type> calls;
   {
-    std::lock_guard<std::mutex> lock(*mutex_);
+    std::lock_guard lock(*mutex_);
     if (!shutdown_) {
       shutdown_ = shutdown;
       calls.reserve(calls_.size());
@@ -344,7 +344,7 @@ void Rpcs::Register(RpcCommandPtr call, Handle* handle) {
 }
 
 Rpcs::Handle Rpcs::Register(RpcCommandPtr call) {
-  std::lock_guard<std::mutex> lock(*mutex_);
+  std::lock_guard lock(*mutex_);
   if (shutdown_) {
     call->Abort();
     return InvalidHandle();
@@ -378,7 +378,7 @@ RpcCommandPtr Rpcs::Unregister(Handle* handle) {
   }
   auto result = **handle;
   {
-    std::lock_guard<std::mutex> lock(*mutex_);
+    std::lock_guard lock(*mutex_);
     calls_.erase(*handle);
     cond_.notify_one();
   }
@@ -387,7 +387,7 @@ RpcCommandPtr Rpcs::Unregister(Handle* handle) {
 }
 
 Rpcs::Handle Rpcs::Prepare() {
-  std::lock_guard<std::mutex> lock(*mutex_);
+  std::lock_guard lock(*mutex_);
   if (shutdown_) {
     return InvalidHandle();
   }

src/yb/util/locks.cc
@@ -36,7 +36,7 @@
 
 namespace yb {
 
-size_t percpu_rwlock::memory_footprint_excluding_this() const {
+size_t PerCpuRwMutex::memory_footprint_excluding_this() const {
   // Because locks_ is a dynamic array of non-trivially-destructable types,
   // the returned pointer from new[] isn't guaranteed to point at the start of
   // a memory block, rendering it useless for malloc_usable_size().
@@ -50,7 +50,7 @@ size_t percpu_rwlock::memory_footprint_excluding_this() const {
   return n_cpus_ * sizeof(padded_lock);
 }
 
-size_t percpu_rwlock::memory_footprint_including_this() const {
+size_t PerCpuRwMutex::memory_footprint_including_this() const {
   return malloc_usable_size(this) + memory_footprint_excluding_this();
 }
 

src/yb/util/locks.h
@@ -146,34 +146,34 @@ class CAPABILITY("mutex") rw_spinlock  {
   rw_semaphore sem_;
 };
 
-// A reader-writer lock implementation which is biased for use cases where
+// A reader-writer mutex implementation which is biased for use cases where
 // the write lock is taken infrequently, but the read lock is used often.
 //
 // Internally, this creates N underlying mutexes, one per CPU. When a thread
-// wants to lock in read (shared) mode, it locks only its own CPU's mutex. When it
-// wants to lock in write (exclusive) mode, it locks all CPU's mutexes.
+// wants to lock in read (shared) mode, it locks only its own CPU's mutex for read. When it
+// wants to lock in write (exclusive) mode, it locks all CPU's mutexes execlusively.
 //
 // This means that in the read-mostly case, different readers will not cause any
 // cacheline contention.
 //
 // Usage:
-//   percpu_rwlock mylock;
+//   PerCpuRwMutex mylock;
 //
 //   // Lock shared:
 //   {
-//     SharedLock<rw_spinlock> lock(mylock.get_lock());
+//     PerCpuRwSharedLock lock(mylock);
 //     ...
 //   }
 //
 //   // Lock exclusive:
 //
 //   {
-//     boost::lock_guard<percpu_rwlock> lock(mylock);
+//     boost::lock_guard<PerCpuRwMutex> lock(mylock);
 //     ...
 //   }
-class percpu_rwlock {
+class CAPABILITY("mutex") PerCpuRwMutex {
  public:
-  percpu_rwlock() {
+  PerCpuRwMutex() {
     errno = 0;
     n_cpus_ = base::MaxCPUIndex() + 1;
     CHECK_EQ(errno, 0) << ErrnoToString(errno);
@@ -181,25 +181,11 @@ class percpu_rwlock {
     locks_ = new padded_lock[n_cpus_];
   }
 
-  ~percpu_rwlock() {
+  ~PerCpuRwMutex() {
     delete [] locks_;
   }
 
-  rw_spinlock &get_lock() {
-#if defined(__APPLE__)
-    // OSX doesn't have a way to get the CPU, so we'll pick a random one.
-    int cpu = reinterpret_cast<uintptr_t>(this) % n_cpus_;
-#else
-    int cpu = sched_getcpu();
-    if (cpu < 0) {
-      LOG(FATAL) << ErrnoToString(errno);
-    }
-    CHECK_LT(cpu, n_cpus_);
-#endif  // defined(__APPLE__)
-    return locks_[cpu].lock;
-  }
-
-  bool try_lock() {
+  bool try_lock() TRY_ACQUIRE(true) {
     for (int i = 0; i < n_cpus_; i++) {
       if (!locks_[i].lock.try_lock()) {
         while (i--) {
@@ -220,13 +206,13 @@ class percpu_rwlock {
     return false;
   }
 
-  void lock() {
+  void lock() ACQUIRE() {
     for (int i = 0; i < n_cpus_; i++) {
       locks_[i].lock.lock();
     }
   }
 
-  void unlock() {
+  void unlock() RELEASE() {
     for (int i = 0; i < n_cpus_; i++) {
       locks_[i].lock.unlock();
     }
@@ -241,6 +227,22 @@ class percpu_rwlock {
   size_t memory_footprint_including_this() const;
 
  private:
+  friend class PerCpuRwSharedLock;
+
+  rw_spinlock &get_lock() {
+#if defined(__APPLE__)
+    // OSX doesn't have a way to get the CPU, so we'll pick a random one.
+    int cpu = reinterpret_cast<uintptr_t>(this) % n_cpus_;
+#else
+    int cpu = sched_getcpu();
+    if (cpu < 0) {
+      LOG(FATAL) << ErrnoToString(errno);
+    }
+    CHECK_LT(cpu, n_cpus_);
+#endif  // defined(__APPLE__)
+    return locks_[cpu].lock;
+  }
+
   struct padded_lock {
     rw_spinlock lock;
     static constexpr size_t kPaddingSize = CACHELINE_SIZE - (sizeof(rw_spinlock) % CACHELINE_SIZE);
@@ -251,11 +253,28 @@ class percpu_rwlock {
   padded_lock *locks_;
 };
 
+// A scoped lock for PerCpuRwMutex. Works by choosing the current CPU and locking the lock for that
+// CPU. The lock is released when the guard goes out of scope.
+class SCOPED_CAPABILITY PerCpuRwSharedLock {
+ public:
+  explicit PerCpuRwSharedLock(PerCpuRwMutex& per_cpu_rwlock)  // NOLINT
+      ACQUIRE_SHARED(per_cpu_rwlock) {
+    current_cpu_lock_ = &per_cpu_rwlock.get_lock();
+    current_cpu_lock_->lock();
+  }
+
+  ~PerCpuRwSharedLock() RELEASE() {
+    current_cpu_lock_->unlock();
+  }
+ private:
+  rw_spinlock* current_cpu_lock_;
+};
+
 template <class Container>
 auto ToVector(const Container& container, std::mutex* mutex) {
   std::vector<typename Container::value_type> result;
   {
-    std::lock_guard<std::mutex> lock(*mutex);
+    std::lock_guard lock(*mutex);
     result.reserve(container.size());
     result.assign(container.begin(), container.end());
   }

commit c0cedf0f849bdd1cd7d0073bf937c9f7477ecb11

[#25135] DocDB: Fix deadlock in Messenger test mode
Summary:
When BreakConnectivity functionality is used in Messenger, it acquires lock_ while queueing outbound call.
But queue outbound call could be invoked from reactor thread when retriable rpc is used (via RpcRetries => DelayedTask).

Messenger also acquire lock when executing DumpRunningRpcs, and keep this lock until dump is done.
But dump query reactors one by one and is blocked until all reactors are responded.

Since reactor thread could be waiting on the lock_ it results in deadlock.

It is actual only for tests, since BreakConnectivity should be used.

Fixed by introducing separate mutex for BreakConnectivity functionality.

Also fixed PerCpuRwSharedLock to use `lock_shared` instead of `lock`, that was broken by D26242.

Also added login to ProcessSupervisor to kill supervised process after 10 seconds timeout in Stop.
Jira: DB-14284

Test Plan: ./yb_build.sh fastdebug --gcc11 --gtest_filter PgNamespaceTest.CreateNamespaceFromTemplateLeaderFailover -n 4000 -- -p 20

Reviewers: hsunder

Reviewed By: hsunder

Subscribers: yql, ybase

Tags: #jenkins-ready

Differential Revision: https://phorge.dev.yugabyte.com/D40377

src/yb/master/master_rpc.cc
@@ -68,7 +68,7 @@ namespace {
 ////////////////////////////////////////////////////////////
 
 // An RPC for getting a Master server's registration.
-class GetMasterRegistrationRpc: public rpc::Rpc {
+class GetMasterRegistrationRpc : public rpc::Rpc {
  public:
 
   // Create a wrapper object for a retriable GetMasterRegistration RPC

src/yb/rpc/messenger.cc
@@ -281,40 +281,40 @@ void Messenger::BreakConnectivity(const IpAddress& address, bool incoming, bool 
   LOG(INFO) << "TEST: Break " << (incoming ? "incoming" : "") << "/" << (outgoing ? "outgoing" : "")
             << " connectivity with: " << address;
 
-  boost::optional<CountDownLatch> latch;
+  bool inserted_from = false;
+  bool inserted_to = false;
+  std::optional<CountDownLatch> latch;
   {
-    std::lock_guard guard(lock_);
+    std::lock_guard guard(broken_connectivity_lock_);
     if (broken_connectivity_from_.empty() || broken_connectivity_to_.empty()) {
       has_broken_connectivity_.store(true, std::memory_order_release);
     }
-    bool inserted_from = false;
     if (incoming) {
       inserted_from = broken_connectivity_from_.insert(address).second;
     }
-    bool inserted_to = false;
     if (outgoing) {
       inserted_to = broken_connectivity_to_.insert(address).second;
     }
-    if (inserted_from || inserted_to) {
-      latch.emplace(reactors_.size());
-      for (const auto& reactor : reactors_) {
-        auto scheduling_status = reactor->ScheduleReactorTask(MakeFunctorReactorTask(
-            [&latch, address, incoming, outgoing](Reactor* reactor) {
-              ReactorThreadRoleGuard guard;
-              if (incoming) {
-                reactor->DropIncomingWithRemoteAddress(address);
-              }
-              if (outgoing) {
-                reactor->DropOutgoingWithRemoteAddress(address);
-              }
-              latch->CountDown();
-            },
-            SOURCE_LOCATION()));
-        if (!scheduling_status.ok()) {
-          LOG(DFATAL) << "Failed to schedule drop connection with: "
-                      << address.to_string() << ": " << scheduling_status;
-          latch->CountDown();
-        }
+  }
+  if (inserted_from || inserted_to) {
+    latch.emplace(reactors_.size());
+    for (const auto& reactor : reactors_) {
+      auto scheduling_status = reactor->ScheduleReactorTask(MakeFunctorReactorTask(
+          [&latch, address, incoming, outgoing](Reactor* reactor) {
+            ReactorThreadRoleGuard guard;
+            if (incoming) {
+              reactor->DropIncomingWithRemoteAddress(address);
+            }
+            if (outgoing) {
+              reactor->DropOutgoingWithRemoteAddress(address);
+            }
+            latch->CountDown();
+          },
+          SOURCE_LOCATION()));
+      if (!scheduling_status.ok()) {
+        LOG(DFATAL) << "Failed to schedule drop connection with: "
+                    << address.to_string() << ": " << scheduling_status;
+        latch->CountDown();
       }
     }
   }
@@ -339,7 +339,7 @@ void Messenger::RestoreConnectivity(const IpAddress& address, bool incoming, boo
   LOG(INFO) << "TEST: Restore " << (incoming ? "incoming" : "") << "/"
             << (outgoing ? "outgoing" : "") << " connectivity with: " << address;
 
-  std::lock_guard guard(lock_);
+  std::lock_guard guard(broken_connectivity_lock_);
   if (incoming) {
     broken_connectivity_from_.erase(address);
   }
@@ -353,7 +353,7 @@ void Messenger::RestoreConnectivity(const IpAddress& address, bool incoming, boo
 
 bool Messenger::TEST_ShouldArtificiallyRejectIncomingCallsFrom(const IpAddress &remote) {
   if (has_broken_connectivity_.load(std::memory_order_acquire)) {
-    PerCpuRwSharedLock guard(lock_);
+    PerCpuRwSharedLock guard(broken_connectivity_lock_);
     return broken_connectivity_from_.count(remote) != 0;
   }
   return false;
@@ -361,7 +361,7 @@ bool Messenger::TEST_ShouldArtificiallyRejectIncomingCallsFrom(const IpAddress &
 
 bool Messenger::TEST_ShouldArtificiallyRejectOutgoingCallsTo(const IpAddress &remote) {
   if (has_broken_connectivity_.load(std::memory_order_acquire)) {
-    PerCpuRwSharedLock guard(lock_);
+    PerCpuRwSharedLock guard(broken_connectivity_lock_);
     return broken_connectivity_to_.count(remote) != 0;
   }
   return false;

src/yb/rpc/messenger.h
@@ -365,10 +365,13 @@ class Messenger : public ProxyContext {
 
   // Flag that we have at least on address with artificially broken connectivity.
   std::atomic<bool> has_broken_connectivity_ = {false};
+  mutable PerCpuRwMutex broken_connectivity_lock_;
 
   // Set of addresses with artificially broken connectivity.
-  std::unordered_set<IpAddress, IpAddressHash> broken_connectivity_from_ GUARDED_BY(lock_);
-  std::unordered_set<IpAddress, IpAddressHash> broken_connectivity_to_ GUARDED_BY(lock_);
+  std::unordered_set<IpAddress, IpAddressHash> broken_connectivity_from_
+      GUARDED_BY(broken_connectivity_lock_);
+  std::unordered_set<IpAddress, IpAddressHash> broken_connectivity_to_
+      GUARDED_BY(broken_connectivity_lock_);
 
   IoThreadPool io_thread_pool_;
   Scheduler scheduler_;

src/yb/rpc/rpc.cc
@@ -229,8 +229,8 @@ void RpcRetrier::DoRetry(RpcCommand* rpc, const Status& status) {
     VTRACE_TO(1, rpc->trace(), "Sending Rpc");
     rpc->SendRpc();
   } else {
-    // Service unavailable here means that we failed to to schedule delayed task, i.e. reactor
-    // is shutted down.
+    // Service unavailable here means that we failed to schedule delayed task, i.e. reactor
+    // is shut down.
     if (new_status.IsServiceUnavailable()) {
       new_status = STATUS_FORMAT(Aborted, "Aborted because of $0", new_status);
     }
@@ -309,8 +309,9 @@ CoarseTimePoint Rpcs::DoRequestAbortAll(RequestShutdown shutdown) {
     std::lock_guard lock(*mutex_);
     if (!shutdown_) {
       shutdown_ = shutdown;
-      calls.reserve(calls_.size());
       calls.assign(calls_.begin(), calls_.end());
+    } else if (shutdown) {
+      CHECK(calls_.empty());
     }
   }
   auto deadline = CoarseMonoClock::now() + FLAGS_rpcs_shutdown_timeout_ms * 1ms;

src/yb/util/locks.cc
@@ -47,7 +47,7 @@ size_t PerCpuRwMutex::memory_footprint_excluding_this() const {
   //
   // See https://code.google.com/p/address-sanitizer/issues/detail?id=395 for
   // more details.
-  return n_cpus_ * sizeof(padded_lock);
+  return n_cpus_ * sizeof(PaddedLock);
 }
 
 size_t PerCpuRwMutex::memory_footprint_including_this() const {

src/yb/util/locks.h
@@ -178,7 +178,7 @@ class CAPABILITY("mutex") PerCpuRwMutex {
     n_cpus_ = base::MaxCPUIndex() + 1;
     CHECK_EQ(errno, 0) << ErrnoToString(errno);
     CHECK_GT(n_cpus_, 0);
-    locks_ = new padded_lock[n_cpus_];
+    locks_ = new PaddedLock[n_cpus_];
   }
 
   ~PerCpuRwMutex() {
@@ -186,7 +186,7 @@ class CAPABILITY("mutex") PerCpuRwMutex {
   }
 
   bool try_lock() TRY_ACQUIRE(true) {
-    for (int i = 0; i < n_cpus_; i++) {
+    for (size_t i = 0; i < n_cpus_; i++) {
       if (!locks_[i].lock.try_lock()) {
         while (i--) {
           locks_[i].lock.unlock();
@@ -200,20 +200,20 @@ class CAPABILITY("mutex") PerCpuRwMutex {
   // Return true if this lock is held on any CPU.
   // See simple_spinlock::is_locked() for details about where this is useful.
   bool is_locked() const {
-    for (int i = 0; i < n_cpus_; i++) {
+    for (size_t i = 0; i < n_cpus_; i++) {
       if (locks_[i].lock.is_locked()) return true;
     }
     return false;
   }
 
   void lock() ACQUIRE() {
-    for (int i = 0; i < n_cpus_; i++) {
+    for (size_t i = 0; i < n_cpus_; i++) {
       locks_[i].lock.lock();
     }
   }
 
   void unlock() RELEASE() {
-    for (int i = 0; i < n_cpus_; i++) {
+    for (size_t i = 0; i < n_cpus_; i++) {
       locks_[i].lock.unlock();
     }
   }
@@ -229,28 +229,24 @@ class CAPABILITY("mutex") PerCpuRwMutex {
  private:
   friend class PerCpuRwSharedLock;
 
-  rw_spinlock &get_lock() {
-#if defined(__APPLE__)
-    // OSX doesn't have a way to get the CPU, so we'll pick a random one.
-    int cpu = reinterpret_cast<uintptr_t>(this) % n_cpus_;
-#else
-    int cpu = sched_getcpu();
-    if (cpu < 0) {
-      LOG(FATAL) << ErrnoToString(errno);
-    }
-    CHECK_LT(cpu, n_cpus_);
-#endif  // defined(__APPLE__)
-    return locks_[cpu].lock;
-  }
-
-  struct padded_lock {
+  struct alignas(CACHELINE_SIZE) PaddedLock {
     rw_spinlock lock;
-    static constexpr size_t kPaddingSize = CACHELINE_SIZE - (sizeof(rw_spinlock) % CACHELINE_SIZE);
-    char padding[kPaddingSize];
   };
 
-  int n_cpus_;
-  padded_lock *locks_;
+  PaddedLock& get_lock() {
+#if defined(__APPLE__)
+    // OSX doesn't have a way to get the CPU, so we'll pick a random one.
+    auto cpu = reinterpret_cast<uintptr_t>(this) % n_cpus_;
+#else
+    auto cpu = sched_getcpu();
+    LOG_IF(FATAL, cpu < 0) << ErrnoToString(errno);
+    CHECK_LT(cpu, n_cpus_);
+#endif  // defined(__APPLE__)
+    return locks_[cpu];
+  }
+
+  size_t n_cpus_;
+  PaddedLock* locks_;
 };
 
 // A scoped lock for PerCpuRwMutex. Works by choosing the current CPU and locking the lock for that
@@ -258,16 +254,17 @@ class CAPABILITY("mutex") PerCpuRwMutex {
 class SCOPED_CAPABILITY PerCpuRwSharedLock {
  public:
   explicit PerCpuRwSharedLock(PerCpuRwMutex& per_cpu_rwlock)  // NOLINT
-      ACQUIRE_SHARED(per_cpu_rwlock) {
-    current_cpu_lock_ = &per_cpu_rwlock.get_lock();
-    current_cpu_lock_->lock();
+      ACQUIRE_SHARED(per_cpu_rwlock)
+      : current_cpu_lock_(&per_cpu_rwlock.get_lock()) {
+    current_cpu_lock_->lock.lock_shared();
   }
 
   ~PerCpuRwSharedLock() RELEASE() {
-    current_cpu_lock_->unlock();
+    current_cpu_lock_->lock.unlock_shared();
   }
+
  private:
-  rw_spinlock* current_cpu_lock_;
+  PerCpuRwMutex::PaddedLock* current_cpu_lock_;
 };
 
 template <class Container>