6db564771b2f4a162a03951ca55b00c793ed7193
Siddharth Shah <siddharth.shah@yugabyte.com>
2024-12-09 12:17:34 +0530

    [#25148] CDC: Disable tablet splitting for logical replication
    Summary:
    We are introducing a new flag `enable_tablet_split_of_replication_slot_streamed_tables` to enable/disable automatic tablet splitting for tables that are part of a replication slot's stream metadata. The default value is set to false in order to disable splitting by default.
    
    Note: This flag is only applicable for CDC streams with replication slots. For unit tests, we have set the flag to true to allow automatic tablet splitting.
    Jira: DB-14308
    
    Test Plan:
    ./yb_build.sh --cxx-test integration-tests_cdcsdk_tablet_split-test --gtest_filter CDCSDKTabletSplitTest.TestTabletSplitDisabledForTablesWithReplicationSlot
    Manually tested the following scenario where user can have both type of streams on separate databases:
    1. Create two databases - DB1 & DB2.
    2. Create table t1 in both DB1 & DB2.
    3. Create a regular CDC stream without replication on DB1. Create a replication slot on DB2.
    4. Manually split tablet of table t1 present in DB1. Confirmed its allowed to split.
    5. Manually split tablet of table t1 present in DB2. Confirmed we receive an error stating splitting is not allowed with replication slot.
    
    Reviewers: xCluster, hsunder, skumar, sumukh.phalgaonkar
    
    Reviewed By: skumar
    
    Subscribers: ybase
    
    Tags: #jenkins-ready
    
    Differential Revision: https://phorge.dev.yugabyte.com/D40473

--- src/yb/master/catalog_manager.h
+++ src/yb/master/catalog_manager.h
@@ -2162,7 +2162,8 @@ class CatalogManager : public CatalogManagerIf, public SnapshotCoordinatorContex
   // Is this table part of xCluster or CDCSDK?
   bool IsTablePartOfXRepl(const TableId& table_id) const REQUIRES_SHARED(mutex_);
 
-  bool IsTablePartOfCDCSDK(const TableId& table_id) const REQUIRES_SHARED(mutex_);
+  bool IsTablePartOfCDCSDK(const TableId& table_id, bool require_replication_slot = false) const
+      REQUIRES_SHARED(mutex_);
 
   bool IsPitrActive();
 
