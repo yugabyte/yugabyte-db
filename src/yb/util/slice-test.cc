fd8bb1be62e21e91a3fbcffbb87b1f1c6c0dd5df
Timur Yusupov <ttyusupov@users.noreply.github.com>
2024-12-17 18:10:50 +0300

    [BACKPORT 2024.2][#25319] docdb: Fixed Slice::Less
    Summary:
    `Slice::Less` is inconsistent with `Slice::compare` which is reproduced by the following test:
    ```
        str1 = "ABC";
        str2 = "";
        const auto s1 = Slice(str1);
        const auto s2 = Slice(str2);
        ASSERT_EQ(s1.Less(s2), s1.compare(s2) < 0);
    ```
    
    Fixed `Slice::Less` and added SliceTest.OrderConsistency random test.
    
    Original commit: c53c5af77b195d62c7a3485e3fb1b1a0c04bb289 / D40717
    
    Jira: DB-14523
    
    Test Plan: ybd --gtest_filter SliceTest.OrderConsistency -n 50 -- -p 1 for release/debug/asan/tsan builds
    
    Reviewers: arybochkin, sergei
    
    Reviewed By: arybochkin
    
    Subscribers: sergei, ybase
    
    Tags: #jenkins-ready
    
    Differential Revision: https://phorge.dev.yugabyte.com/D40806

--- src/yb/util/slice-test.cc
+++ src/yb/util/slice-test.cc
@@ -37,7 +37,9 @@
 
 #include "yb/util/random_util.h"
 #include "yb/util/slice.h"
+#include "yb/util/test_util.h"
 #include "yb/util/tostring.h"
+#include "yb/util/tsan_util.h"
 
 using std::string;
 
@@ -90,6 +92,7 @@ size_t made_checks = 0;
 
 template <class... Args>
 void CheckLess(bool expected, const Slice& lhs, Args&&... rhs) {
+  static_assert(sizeof...(Args) > 0, "Args must not be empty");
   ASSERT_EQ(expected, lhs.Less(std::forward<Args>(rhs)...))
        << lhs.ToBuffer() << " vs " << AsString(std::tuple<Args...>(std::forward<Args>(rhs)...));
   ++made_checks;
@@ -97,6 +100,7 @@ void CheckLess(bool expected, const Slice& lhs, Args&&... rhs) {
 
 template <size_t prefix>
 struct TestLessHelper<prefix, prefix, /* prefix_eq_len */ true> {
+  // Requires: lhs = rhs
   template <class... Args>
   static void Apply(const Slice& lhs, Args&&... rhs) {
     CheckLess(false, lhs, std::forward<Args>(rhs)...);
@@ -129,9 +133,10 @@ struct TestLessIteration<prefix, len, new_prefix, /* new_prefix_le_len= */ true>
 
 template <size_t prefix, size_t len>
 struct TestLessHelper<prefix, len, /* prefix_eq_len= */ false> {
+  // Requires: len = lhs.size(), lhs.starts_with(rhs), prefix = rhs.size(), prefix < len
   template <class... Args>
   static void Apply(const Slice& lhs, Args&&... rhs) {
-    CheckLess(true, lhs, std::forward<Args>(rhs)...);
+    CheckLess(false, lhs, std::forward<Args>(rhs)...);
 
     TestLessIteration<prefix, len, prefix + 1, (prefix < len)>::Apply(
         lhs, std::forward<Args>(rhs)...);
@@ -139,6 +144,7 @@ struct TestLessHelper<prefix, len, /* prefix_eq_len= */ false> {
 };
 
 template <size_t prefix, size_t len, class... Args>
+// Requires: len = lhs.size(), lhs.starts_with(rhs), prefix = rhs.size()
 void TestLess(const Slice& lhs, Args&&... rhs) {
   char kMinChar = '\x00';
   char kMaxChar = '\xff';
@@ -163,6 +169,59 @@ TEST(SliceTest, Less) {
   // And compare with just kMinChar and kMaxChar. So we should get:
   // (2 ^ kLen - 1) * 3 + 2 comparisons, simplified it to (2^kLen) * 3 - 1
   ASSERT_EQ(made_checks, (1ULL << kLen) * 3 - 1);
+}
+
+template <class... Args>
+void TestLessComponents(
+    const bool expected, const size_t component_size, const Slice& lhs, const Slice& rhs0,
+    Args&&... rhs) {
+  CheckLess(expected, lhs, rhs0, std::forward<Args>(rhs)...);
+  NO_PENDING_FATALS();
+  if (RandomActWithProbability(0.9)) {
+    CheckLess(expected, lhs, rhs0, Slice(""), std::forward<Args>(rhs)...);
+    NO_PENDING_FATALS();
+  }
+  if (rhs0.size() > component_size) {
+    CheckLess(
+        expected, lhs, rhs0.Prefix(rhs0.size() - component_size), rhs0.Suffix(component_size),
+        std::forward<Args>(rhs)...);
+  }
+}
+
+void TestOrder(const std::string& str1, const std::string& str2, const size_t num_components) {
+  const auto s1 = Slice(str1);
+  const auto s2 = Slice(str2);
+  auto component_size = std::max<size_t>(s2.size() / num_components, 1);
+  TestLessComponents(s1.compare(s2) < 0, component_size, s1, s2);
+  component_size = std::max<size_t>(s1.size() / num_components, 1);
+  TestLessComponents(s2.compare(s1) < 0, component_size, s2, s1);
+}
+
+// Test that Slice::Less order is consistent with Slice::compare.
+TEST(SliceTest, OrderConsistency) {
+  constexpr auto kNumIters = ReleaseVsDebugVsAsanVsTsan(100000, 100000, 50000, 50000);
+  constexpr auto kMaxLength = 256;
+  constexpr auto kMaxComponents = 10;
+
+  TestOrder("ABC", "", /* num_components = */ kMaxComponents);
+  TestOrder("ABCD", "ABC", /* num_components = */ kMaxComponents);
+  TestOrder("ABC", "ABC", /* num_components = */ kMaxComponents);
+
+  std::string str1;
+  std::string str2;
+
+  for (int iter = 0; iter < kNumIters; ++iter) {
+    const auto len1 = RandomUniformInt<size_t>(0, kMaxLength);
+    const auto len2 = RandomUniformInt<size_t>(0, kMaxLength);
+    str1 = RandomHumanReadableString(len1);
+    str2 = RandomHumanReadableString(len2);
+
+    const auto num_components = 1 + kMaxComponents * iter / kNumIters;
+
+    TestOrder(str1, str2, num_components);
+    NO_PENDING_FATALS();
+  }
+  LOG(INFO) << "made_checks: " << made_checks;
 }
 
 } // namespace yb
