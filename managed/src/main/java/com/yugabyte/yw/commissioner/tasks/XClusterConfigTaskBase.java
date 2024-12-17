09bfa5036c9c118f5d2f49ef224b60d7fda8584d
Artem <artem.mindrov@gmail.com>
2024-12-17 17:29:30 -0500

    [BACKPORT 2.25.0.1143][PLAT-16355] make gflags mutable in SetFlagInMemory params
    Summary:
    if a universe has modified CSV flags, SetFlagInMemory subtask of PauseXClusterUniverses fails trying to update an immutable singletonMap with the merged CSV.
    
    Original commit: 3dab4910260787e4a0f61a39dec456569b5dde94 / D40758
    
    Test Plan: create two universes, set up DR, modify CSV flags on source, pause/resume.
    
    Reviewers: cwang
    
    Reviewed By: cwang
    
    Subscribers: yugaware
    
    Differential Revision: https://phorge.dev.yugabyte.com/D40780

--- managed/src/main/java/com/yugabyte/yw/commissioner/tasks/XClusterConfigTaskBase.java
+++ managed/src/main/java/com/yugabyte/yw/commissioner/tasks/XClusterConfigTaskBase.java
@@ -2990,10 +2990,11 @@ public abstract class XClusterConfigTaskBase extends UniverseDefinitionTaskBase 
         ServerType.TSERVER,
         (node, params) -> {
           params.force = true;
+          params.gflags = new HashMap<>();
+
           if (action == XClusterUniverseAction.PAUSE) {
-            params.gflags =
-                Collections.singletonMap(
-                    GFlagsUtil.LOG_MIN_SECONDS_TO_RETAIN, Integer.toString(Integer.MAX_VALUE));
+            params.gflags.put(
+                GFlagsUtil.LOG_MIN_SECONDS_TO_RETAIN, Integer.toString(Integer.MAX_VALUE));
           } else if (action == XClusterUniverseAction.RESUME) {
             Map<String, String> oldGFlags =
                 GFlagsUtil.getGFlagsForNode(
@@ -3001,12 +3002,9 @@ public abstract class XClusterConfigTaskBase extends UniverseDefinitionTaskBase 
                     ServerType.TSERVER,
                     universe.getUniverseDetails().getPrimaryCluster(),
                     universe.getUniverseDetails().clusters);
-            params.gflags =
-                Collections.singletonMap(
-                    GFlagsUtil.LOG_MIN_SECONDS_TO_RETAIN,
-                    oldGFlags.containsKey(GFlagsUtil.LOG_MIN_SECONDS_TO_RETAIN)
-                        ? oldGFlags.get(GFlagsUtil.LOG_MIN_SECONDS_TO_RETAIN)
-                        : "900");
+            params.gflags.put(
+                GFlagsUtil.LOG_MIN_SECONDS_TO_RETAIN,
+                oldGFlags.getOrDefault(GFlagsUtil.LOG_MIN_SECONDS_TO_RETAIN, "900"));
           }
         });
 
