3b11ae5888d0c7a72ab425c3878e14662be39ef4
Hari Krishna Sunder <hari90@users.noreply.github.com>
2025-01-03 18:55:37 -0800

    [BACKPORT 2.25.0][#25480] xCluster: Keep running xcluster task even if leader temporarily loses the lease
    Summary:
    Original commit: 91a5d5144fe82875480104d9d9c35c13e8f8e7b8 / D40988
    On an xCluster target universe the yb-master leader computes the xCluster safe time every second. This task stops if the leadership is lost, and restarts again when a new leader is elected and sys catalog is reloaded. In some rare cases the master can lose the raft lease, but still retain its leadership. In this case we should keep running the xCluster task until we are certain that the leadership is lost.
    
    `SCOPED_LEADER_SHARED_LOCK` checks if we are the leader and lease is valid. This change uses `CatalogManager::CheckIsLeaderAndReady` to check if the leadership is still valid.
    
    Removing unnecessary `leader_term_` member.
    Jira: DB-14730
    
    Test Plan:
    Jenkins
    
    ./yb_build.sh asan --cxx-test integration-tests_xcluster_ysql-test --gtest_filter XClusterYSqlTestConsistentTransactionsTest.AddServerIntraTransaction -n 40  --tp 1
    
    Reviewers: sergei, jhe, slingam, xCluster
    
    Reviewed By: jhe
    
    Subscribers: ybase
    
    Differential Revision: https://phorge.dev.yugabyte.com/D41012

--- src/yb/master/xcluster/xcluster_safe_time_service-test.cc
+++ src/yb/master/xcluster/xcluster_safe_time_service-test.cc
@@ -309,11 +309,6 @@ TEST_F(XClusterSafeTimeServiceTest, ComputeSafeTimeWithFilters) {
   ASSERT_EQ(safe_time_service.safe_time_map_[db2], ht_invalid);
   ASSERT_EQ(safe_time_service.entries_to_delete_.size(), 0);
 
-  ASSERT_NOK(safe_time_service.GetXClusterSafeTimeForNamespace(
-      dummy_leader_term + 1, db1, XClusterSafeTimeFilter::NONE));
-  ASSERT_NOK(safe_time_service.GetXClusterSafeTimeForNamespace(
-      dummy_leader_term - 1, db1, XClusterSafeTimeFilter::NONE));
-
   auto db1_none = ASSERT_RESULT(GetXClusterSafeTimeWithNoFilter(safe_time_service, db1));
   auto db1_ddlqueue = ASSERT_RESULT(GetXClusterSafeTimeFilterOutDdlQueue(safe_time_service, db1));
   auto db2_none = ASSERT_RESULT(GetXClusterSafeTimeWithNoFilter(safe_time_service, db2));
