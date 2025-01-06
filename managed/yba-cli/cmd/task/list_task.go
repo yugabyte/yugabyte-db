1523566ddf9d31268623aff4a3ea97e4d2a5e307
Deepti-yb <dkumar@yugabyte.com>
2025-01-06 19:31:38 +0000

    [PLAT-16341][YBA CLI]Add support for getting universe tasks only
    Summary: Add a flag to accept universe name in the `yba task list` command to filter out tasks pertaining to  single universe
    
    Test Plan: `yba task list --universe-name dkumar-cli`
    
    Reviewers: skurapati
    
    Reviewed By: skurapati
    
    Subscribers: yugaware
    
    Differential Revision: https://phorge.dev.yugabyte.com/D41040

--- managed/yba-cli/cmd/task/list_task.go
+++ managed/yba-cli/cmd/task/list_task.go
@@ -27,7 +27,38 @@ var listTaskCmd = &cobra.Command{
 	Run: func(cmd *cobra.Command, args []string) {
 		authAPI := ybaAuthClient.NewAuthAPIClientAndCustomer()
 
-		r, response, err := authAPI.TasksList().Execute()
+		universeName, err := cmd.Flags().GetString("universe-name")
+		if err != nil {
+			logrus.Fatalf(formatter.Colorize(err.Error()+"\n", formatter.RedColor))
+		}
+		universeUUID := ""
+		isUniverseUUIDPresent := false
+
+		if len(strings.TrimSpace(universeName)) != 0 {
+			r, response, err := authAPI.ListUniverses().Name(universeName).Execute()
+			if err != nil {
+				errMessage := util.ErrorFromHTTPResponse(response, err, "Task", "List - List Universes")
+				logrus.Fatalf(formatter.Colorize(errMessage.Error()+"\n", formatter.RedColor))
+			}
+			if len(r) < 1 {
+				if util.IsOutputType(formatter.TableFormatKey) {
+					logrus.Info("No universes with name "+universeName+" found\n")
+				} else {
+					logrus.Info("[]\n")
+				}
+				return
+			}
+			universeUUID = r[0].GetUniverseUUID()
+		}
+
+		listTasksRequest := authAPI.TasksList()
+
+		if len(strings.TrimSpace(universeUUID)) != 0 {
+			isUniverseUUIDPresent = true
+			listTasksRequest = listTasksRequest.UUUID(universeUUID)
+		}
+
+		r, response, err := listTasksRequest.Execute()
 		if err != nil {
 			errMessage := util.ErrorFromHTTPResponse(response, err, "Task", "List")
 			logrus.Fatalf(formatter.Colorize(errMessage.Error()+"\n", formatter.RedColor))
@@ -56,7 +87,11 @@ var listTaskCmd = &cobra.Command{
 		}
 		if len(r) < 1 {
 			if util.IsOutputType(formatter.TableFormatKey) {
-				logrus.Info("No tasks found\n")
+				if isUniverseUUIDPresent {
+					logrus.Info("No tasks found in universe " + universeName + "\n")
+				} else {
+					logrus.Info("No tasks found\n")
+				}
 			} else {
 				logrus.Info("[]\n")
 			}
@@ -71,5 +106,7 @@ func init() {
 	listTaskCmd.Flags().SortFlags = false
 
 	listTaskCmd.Flags().StringP("uuid", "u", "", "[Optional] UUID of the task.")
+	listTaskCmd.Flags().String("universe-name", "",
+		"[Optional] The name of the universe whose tasks are to be listed.")
 
 }
