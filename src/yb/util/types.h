02ca632cb8aa07eafd8b9453f590fcbcaccf350a
Eric Sheng <esheng@yugabyte.com>
2025-01-08 02:15:22 -0800

    [#25534] docdb: Add robust mutex
    Summary:
    This diff adds `RobustMutex`, which is a mutex that is robust to process crash even when held.
    
    This is implemented with `PTHREAD_MUTEX_ROBUST`. When the owner of the mutex crashes,
    the next mutex acquire will succeed and be notified that the previous owner had crashed. We
    use this to trigger a cleanup callback which will perform any necessary work needed to bring the
    state guarded by the mutex back to a consistent state.
    
    `PTHREAD_MUTEX_ROBUST` is not implemented on OS X (which follows an older version of UNIX
    that does not have it). Crashes while holding the mutex should ideally never happen, and OS X is
    not used for production deployment, so we just use a normal mutex in shared memory for OS X.
    
    This is needed for future changes to move the table locks lock manager into shared memory. For more
    context: D40272.
    Jira: DB-14789
    
    Test Plan: Added tests: `./yb_build.sh --cxx-test 'util_robust_mutex-test'`
    
    Reviewers: sergei, bkolagani, amitanand
    
    Reviewed By: sergei
    
    Subscribers: svc_phabricator, ybase, rthallam, zdrudi, amitanand
    
    Differential Revision: https://phorge.dev.yugabyte.com/D41082

---
+++ src/yb/util/types.h
@@ -0,0 +1,35 @@
+// Copyright (c) YugaByte, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+// in compliance with the License.  You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software distributed under the License
+// is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+// or implied.  See the License for the specific language governing permissions and limitations
+// under the License.
+//
+
+#pragma once
+
+#include <cstddef>
+
+#include "yb/util/cast.h"
+
+namespace yb {
+
+template<typename Container>
+Container* MemberPtrToContainer(void* member_ptr, size_t member_offset) {
+  return pointer_cast<Container*>(pointer_cast<std::byte*>(member_ptr) - member_offset);
+}
+
+template<typename Container>
+const Container* MemberPtrToContainer(const void* member_ptr, size_t member_offset) {
+  return pointer_cast<const Container*>(pointer_cast<const std::byte*>(member_ptr) - member_offset);
+}
+
+#define MEMBER_PTR_TO_CONTAINER(cls, member_name, member_ptr) \
+    ::yb::MemberPtrToContainer<cls>(member_ptr, offsetof(cls, member_name))
+
+} // namespace yb
