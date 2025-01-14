yugabyte/yugabyte-db

commit ca6dfe42d3b7dd2032fed3b1598060e493032edc

[#25501] YSQL: Do not run check_new_cluster when the new cluster is not reachable
Summary:
`check_new_cluster` connects to the new cluster to grab information about the databases and user tables present on the new cluster. The C++ tests did not run a preflight check, so they missed this case. Adding the preflight checks reproduced the same issue that the shell test ran into.

When `pg_upgrade --check` is run before any other upgrade step, all we have is a PG11 cluster. So its not possible to connect to the new cluster to run checks on it, but pg_upgrade expects to be able to do so.

However, when pg_upgrade is run during the upgrade, the new cluster is reachable. So rather than disabling the check entirely, the check is disabled only if the new cluster is not reachable.
Jira: DB-14760

Test Plan:
Jenkins
```
pg15_tests/test_upgrade.sh
./yb_build.sh release --cxx-test integration-tests_pg15_upgrade-test --gtest_filter Pg15UpgradeTest.SimpleTableUpgrade
```

Reviewers: hsunder, fizaa

Reviewed By: hsunder

Subscribers: yql

Differential Revision: https://phorge.dev.yugabyte.com/D41042

src/postgres/src/bin/pg_upgrade/check.c
@@ -198,9 +198,7 @@ check_new_cluster(void)
 	check_new_cluster_is_empty();
 	check_databases_are_compatible();
 
-	if (!is_yugabyte_enabled())
-		/* YB: would fail with ERROR:  LOAD not supported yet. */
-		check_loadable_libraries();
+	check_loadable_libraries();
 
 	switch (user_opts.transfer_mode)
 	{

src/yb/integration-tests/upgrade-tests/pg15_upgrade_test_base.h
@@ -34,6 +34,9 @@ class Pg15UpgradeTestBase : public UpgradeTestBase {
   static constexpr size_t kMixedModeTserverPg11 = 1;
   static constexpr std::optional<size_t> kAnyTserver = std::nullopt;
 
+  // Run pg_upgrade --check
+  Status ValidateUpgradeCompatibility();
+
   // Restarts all masters in the current version, runs ysql major version upgrade, and restarts
   // tserver kMixedModeTserverPg15 in the current version. Other tservers are kept in the pg11
   // version.

pg15_tests/common_upgrade.sh
@@ -8,20 +8,17 @@ pghost3=127.0.0.$((ip_start + 2))
 # TEST_always_return_consensus_info_for_succeeded_rpc=false is needed to upgrade a release build to
 # debug.
 common_pg15_flags="TEST_always_return_consensus_info_for_succeeded_rpc=false"
-# Set ysql_hba_conf to allow local socket connections. This is needed for older builds that do not
-# have D39564.
-ysql_hba_conf_flag='"ysql_hba_conf=""local all yugabyte trust,host all all all trust"""'
 # yb_enable_expression_pushdown=false is needed because the expression pushdown rewriter is not yet
 # implemented.
-common_tserver_flags=$ysql_hba_conf_flag',"ysql_pg_conf_csv=yb_enable_expression_pushdown=false"'
+common_tserver_flags='"ysql_pg_conf_csv=yb_enable_expression_pushdown=false"'
 
 # Downloads, runs, and pushds the directory for pg11.
 # Sets $pg11path to the pg11 directory.
 run_and_pushd_pg11() {
   prefix="/tmp"
-  ybversion_pg11="2024.2.0.0"
-  ybbuild="b58"
-  ybhash="d78199a0d53b4ab2bd86e6805a46a38fad783ba2"
+  ybversion_pg11="2024.2.1.0"
+  ybbuild="b116"
+  ybhash="ef5232e8f428fba9d52c6cc2002d46ffd79ab999"
   if [[ $OSTYPE = linux* ]]; then
     arch="release-clang17-centos-x86_64"
     tarbin="tar"
@@ -52,7 +49,7 @@ run_and_pushd_pg11() {
 upgrade_masters() {
   for i in {1..3}; do
     yb_ctl restart_node $i --master \
-      --master_flags="$ysql_hba_conf_flag,master_join_existing_universe=true,$common_pg15_flags"
+      --master_flags="master_join_existing_universe=true,$common_pg15_flags"
   done
 }
 
@@ -81,6 +78,20 @@ restart_node_2_in_pg15() {
 run_preflight_checks() {
   build/latest/postgres/bin/pg_upgrade --old-datadir "$data_dir/node-1/disk-1/pg_data" \
     --old-host "$PGHOST" --old-port 5433 --username "yugabyte" --check
+}
+
+finalize_ysql_major_version_catalog_upgrade() {
+  echo finalize_ysql_major_version_catalog_upgrade starting at $(date +"%r")
+  build/latest/bin/yb-admin --init_master_addrs=127.0.0.200:7100 --timeout_ms=300000 \
+    finalize_ysql_major_version_catalog_upgrade
+  echo finalize_ysql_major_version_catalog_upgrade finished at $(date +"%r")
+}
+
+finalize_upgrade() {
+  finalize_ysql_major_version_catalog_upgrade
+
+  build/latest/bin/yb-admin --init_master_addrs=127.0.0.200:7100 --timeout_ms=300000 \
+    promote_auto_flags
 }
 
 verify_simple_table_mixed_cluster() {

pg15_tests/test_upgrade.sh
@@ -80,8 +80,11 @@ INSERT 0 1
 
 EOT
 
+# Finalize the upgrade to re-enable pushdown and DDLs.
+finalize_upgrade
 # Upgrade is complete. After the restart, demonstrate that DDLs work.
 yb_ctl restart
+
 diff <(ysqlsh <<EOT | sed 's/ *$//'
 SHOW server_version_num;
 SELECT * FROM t ORDER BY h,r;

src/postgres/src/bin/pg_upgrade/pg_upgrade.c
@@ -159,7 +159,11 @@ main(int argc, char **argv)
 	if (!is_yugabyte_enabled())
 		start_postmaster(&new_cluster, true);
 
-	check_new_cluster();
+	/*
+	 * YB: If this is run as a preflight check, the new cluster is not yet started.
+	 */
+	if (!is_yugabyte_enabled() || !user_opts.check)
+		check_new_cluster();
 
 	report_clusters_compatible();
 

src/yb/integration-tests/upgrade-tests/pg15_upgrade_test_base.cc
@@ -41,7 +41,27 @@ void Pg15UpgradeTestBase::SetUp() {
       "RESET yb_non_ddl_txn_for_sys_tables_allowed"}));
 }
 
+Status Pg15UpgradeTestBase::ValidateUpgradeCompatibility() {
+  const auto tserver = cluster_->tablet_server(0);
+  const auto data_path = JoinPathSegments(tserver->GetDataDirs().front(), "../../pg_data");
+
+  const std::vector<std::string> args = {
+    GetPgToolPath("pg_upgrade"),
+    "--old-datadir", data_path,
+    "--old-host", tserver->bind_host(),
+    "--old-port", AsString(tserver->pgsql_rpc_port()),
+    "--username", "yugabyte",
+    "--check"
+  };
+
+  LOG(INFO) << "Running " << AsString(args);
+
+  return Subprocess::Call(args);
+}
+
 Status Pg15UpgradeTestBase::UpgradeClusterToMixedMode() {
+  RETURN_NOT_OK(ValidateUpgradeCompatibility());
+
   LOG(INFO) << "Upgrading cluster to mixed mode";
 
   static const MonoDelta no_delay_between_nodes = 0s;

commit 6218febcd13bf2a820cc0f9f48f32d59d753f0f0

[#25620] YSQL: Fix broken PG15 Online Upgrade tests
Summary:
D41042 / ca6dfe42d3b7dd2032fed3b1598060e493032edc broke some PG15 Online Upgrade tests.

There are three distinct issues:
1. `check_loadable_libraries` was behind a `IsYugabyteEnabled()` in `check_new_cluster()`, but an earlier draft of D41042 removed the call to `check_new_cluster()` and cleaned up `check_new_cluster()` to leave it identical to vanilla PG. However, later versions of D41042 reintroduced the call to `check_new_cluster()` again, so `check_loadable_libraries()` was called.
2. `DatabaseWithDisallowedConnections` failed because it was asserting the stdout of `pg_upgrade`, which is no longer returned.
3. `UpgradeAuthEnabledUniverse` failed because of the precheck introduced by D41042. The way the precheck is called in the tests is not auth-aware. To re-enable this test sooner, create GHI #25621 to track testing the precheck for auth enabled universes. For now, just skip the precheck for auth-enabled universes.
Jira: DB-14871

Test Plan:
```
./yb_build.sh release --cxx-test integration-tests_pg15_upgrade-test --gtest_filter Pg15UpgradeTest.Template1
./yb_build.sh release --cxx-test integration-tests_pg15_upgrade-test --gtest_filter Pg15UpgradeTestWithAuth.UpgradeAuthEnabledUniverse
./yb_build.sh release --cxx-test integration-tests_pg15_upgrade-test --gtest_filter Pg15UpgradeTest.DatabaseWithDisallowedConnections
```

Reviewers: hsunder

Reviewed By: hsunder

Subscribers: yql

Differential Revision: https://phorge.dev.yugabyte.com/D41222

src/postgres/src/bin/pg_upgrade/check.c
@@ -206,7 +206,9 @@ check_new_cluster(void)
 	check_new_cluster_is_empty();
 	check_databases_are_compatible();
 
-	check_loadable_libraries();
+	if (!is_yugabyte_enabled())
+		/* YB: would fail with ERROR:  LOAD not supported yet. */
+		check_loadable_libraries();
 
 	switch (user_opts.transfer_mode)
 	{

src/yb/integration-tests/upgrade-tests/pg15_upgrade-test.cc
@@ -534,7 +534,8 @@ TEST_F(Pg15UpgradeTest, DatabaseWithDisallowedConnections) {
   }
 
   // Should fail because we don't support upgrading databases that disallow connections.
-  ASSERT_NOK_STR_CONTAINS(UpgradeClusterToMixedMode(), "Failed to run pg_upgrade");
+  ASSERT_NOK_STR_CONTAINS(UpgradeClusterToMixedMode(),
+                          "pg_upgrade' terminated with non-zero exit status");
 }
 
 TEST_F(Pg15UpgradeTest, Template1) {
@@ -1026,6 +1027,11 @@ class Pg15UpgradeTestWithAuth : public Pg15UpgradeTest {
   void SetUpOptions(ExternalMiniClusterOptions& opts) override {
     opts.enable_ysql_auth = true;
     Pg15UpgradeTest::SetUpOptions(opts);
+  }
+
+  Status ValidateUpgradeCompatibility() override {
+    setenv("PGPASSWORD", "yugabyte", /*overwrite=*/true);
+    return Pg15UpgradeTest:: ValidateUpgradeCompatibility();
   }
 };
 

src/yb/integration-tests/upgrade-tests/pg15_upgrade_test_base.h
@@ -35,7 +35,7 @@ class Pg15UpgradeTestBase : public UpgradeTestBase {
   static constexpr std::optional<size_t> kAnyTserver = std::nullopt;
 
   // Run pg_upgrade --check
-  Status ValidateUpgradeCompatibility();
+  virtual Status ValidateUpgradeCompatibility();
 
   // Restarts all masters in the current version, runs ysql major version upgrade, and restarts
   // tserver kMixedModeTserverPg15 in the current version. Other tservers are kept in the pg11