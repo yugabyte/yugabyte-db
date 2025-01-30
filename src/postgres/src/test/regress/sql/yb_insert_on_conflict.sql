16f2042269ff5f725ebf8b5a8a236d504f6eac41
Jason Kim <git@jasonk.me>
2025-01-30 10:07:02 -0800

    [#24649] YSQL: enable INSERT ON CONFLICT batching for FK
    Summary:
    Previously, the presence of after row insert triggers prevented INSERT
    ON CONFLICT read batching.  Loosen this restriction to allow for after
    row insert triggers as long as they are for foreign key constraint
    checking.  No further code changes should be needed because the current
    design is sufficient.  Foreign key constraints are checked at the end of
    the _statement_ or _transaction_, making them easy to reason with when
    it comes to batching.
    Jira: DB-13713
    
    Test Plan:
    On Almalinux 8:
    
        #!/usr/bin/env bash
        set -euo pipefail
        ./yb_build.sh fastdebug --gcc11
        find java/yb-pgsql/src/test/java/org/yb/pgsql -name 'TestPgRegressInsertOnConflict*' \
        | grep -oE 'TestPgRegress\w+' \
        | while read -r testname; do
          ./yb_build.sh fastdebug --gcc11 --java-test "$testname" --sj
        done
    
    Close: #24649
    
    Reviewers: kramanathan, #db-approvers
    
    Reviewed By: kramanathan
    
    Subscribers: svc_phabricator, yql
    
    Differential Revision: https://phorge.dev.yugabyte.com/D41108

--- src/postgres/src/test/regress/sql/yb_insert_on_conflict.sql
+++ src/postgres/src/test/regress/sql/yb_insert_on_conflict.sql
@@ -508,15 +508,71 @@ TABLE triggers_fired;
 TABLE trigger_test;
 
 --- Foreign key
-CREATE TABLE parent_table (n numeric, t text, b bool, PRIMARY KEY (t, n));
-CREATE TABLE child_table (b bool PRIMARY KEY, n numeric, t text, FOREIGN KEY (t, n) REFERENCES parent_table);
+CREATE TABLE parent_table (n numeric, t text, b bool, PRIMARY KEY (t, n ASC));
+CREATE TABLE child_table (k numeric, n numeric, t text, CONSTRAINT fk FOREIGN KEY (t, n) REFERENCES parent_table, PRIMARY KEY (k ASC));
 INSERT INTO parent_table VALUES (1, '1', true), (2, '2', true);
-INSERT INTO child_table VALUES (false, 1, '1') ON CONFLICT DO NOTHING;
-INSERT INTO child_table VALUES (false, 1, '1') ON CONFLICT (b) DO UPDATE SET b = true;
+INSERT INTO child_table VALUES (0, 1, '1') ON CONFLICT DO NOTHING;
+INSERT INTO child_table VALUES (0, 1, '1') ON CONFLICT (k) DO UPDATE SET k = 1;
 TABLE child_table;
-INSERT INTO child_table VALUES (false, 2, '1') ON CONFLICT (b) DO UPDATE SET b = true;
-INSERT INTO child_table VALUES (true, 2, '1') ON CONFLICT (b) DO UPDATE SET t = '2';
+INSERT INTO child_table VALUES (0, 2, '1') ON CONFLICT (k) DO UPDATE SET k = 1;
+INSERT INTO child_table VALUES (1, 2, '1') ON CONFLICT (k) DO UPDATE SET t = '2';
 TABLE child_table;
+INSERT INTO parent_table VALUES (2, '2', false), (1, '1', false) ON CONFLICT (t, n) DO UPDATE SET t = EXCLUDED.t || EXCLUDED.t;
+INSERT INTO parent_table VALUES (2, '2', false) ON CONFLICT (t, n) DO UPDATE SET t = EXCLUDED.t || EXCLUDED.t;
+SELECT * FROM parent_table ORDER BY t, n;
+TRUNCATE parent_table CASCADE;
+INSERT INTO parent_table (n, t) SELECT g, '' FROM generate_series(0, 5) g;
+INSERT INTO child_table (n, t, k) SELECT g, '', g FROM generate_series(0, 4) g;
+-- NO ACTION
+ALTER TABLE child_table DROP CONSTRAINT fk;
+ALTER TABLE child_table ADD CONSTRAINT fk FOREIGN KEY (t, n) REFERENCES parent_table (t, n) ON DELETE NO ACTION ON UPDATE NO ACTION;
+BEGIN;
+INSERT INTO parent_table (n, t) SELECT g, '' FROM generate_series(0, 5) g
+    ON CONFLICT (n, t) DO UPDATE SET n = EXCLUDED.n - 1;
+TABLE parent_table;
+TABLE child_table;
+ABORT;
+-- RESTRICT
+ALTER TABLE child_table DROP CONSTRAINT fk;
+ALTER TABLE child_table ADD CONSTRAINT fk FOREIGN KEY (t, n) REFERENCES parent_table (t, n) ON DELETE RESTRICT ON UPDATE RESTRICT;
+BEGIN;
+INSERT INTO parent_table (n, t) SELECT g, '' FROM generate_series(0, 5) g
+    ON CONFLICT (n, t) DO UPDATE SET n = EXCLUDED.n - 1;
+TABLE parent_table;
+TABLE child_table;
+ABORT;
+-- CASCADE + self-referential NO ACTION
+ALTER TABLE child_table DROP CONSTRAINT fk;
+ALTER TABLE child_table ADD CONSTRAINT fk FOREIGN KEY (t, n) REFERENCES parent_table (t, n) ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE child_table ADD CONSTRAINT u UNIQUE (n);
+ALTER TABLE child_table ADD CONSTRAINT self_fk FOREIGN KEY (k) REFERENCES child_table (n) ON DELETE NO ACTION ON UPDATE NO ACTION;
+BEGIN;
+INSERT INTO parent_table (n, t) SELECT g, '' FROM generate_series(0, 5) g
+    ON CONFLICT (n, t) DO UPDATE SET n = EXCLUDED.n - 1;
+TABLE parent_table;
+TABLE child_table;
+ABORT;
+-- CASCADE + self-referential CASCADE
+ALTER TABLE child_table DROP CONSTRAINT self_fk;
+ALTER TABLE child_table ADD CONSTRAINT self_fk FOREIGN KEY (k) REFERENCES child_table (n) ON DELETE CASCADE ON UPDATE CASCADE;
+BEGIN;
+INSERT INTO parent_table (n, t) SELECT g, '' FROM generate_series(0, 5) g
+    ON CONFLICT (n, t) DO UPDATE SET n = EXCLUDED.n - 1;
+TABLE parent_table;
+TABLE child_table;
+ABORT;
+-- CASCADE + no-update/no-delete self-referential
+ALTER TABLE child_table DROP CONSTRAINT self_fk;
+ALTER TABLE child_table DROP CONSTRAINT u;
+ALTER TABLE child_table ADD CONSTRAINT self_fk FOREIGN KEY (n) REFERENCES child_table (k) ON DELETE CASCADE ON UPDATE CASCADE;
+BEGIN;
+INSERT INTO parent_table (n, t) VALUES (-1, 'other');
+INSERT INTO child_table (n, t, k) VALUES (-1, 'other', -1);
+INSERT INTO parent_table (n, t) SELECT g, '' FROM generate_series(0, 5) g
+    ON CONFLICT (n, t) DO UPDATE SET n = EXCLUDED.n - 1;
+TABLE parent_table;
+TABLE child_table;
+ABORT;
 
 --- GH-25070
 CREATE TABLE main (a INT, b TEXT, PRIMARY KEY (a, b));
