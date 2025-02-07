94d1e7f2cce18528233401c19be04b73b7ec0b4b
Siddharth Shah <siddharth.shah@yugabyte.com>
2025-02-07 18:33:30 +0530

    [BACKPORT 2024.2][#25896] CDC: Limit VWAL instance per tserver
    Summary:
    **Backport description:**
    Minor merge conflicts in cdc_service.cc due to some flags not backported from master.
    
    **Original description:**
    Original commit: dbfc090c2706e14fa897c33436a5899179aa6232 / D41694
    This diff introduces a new flag `cdc_max_virtual_wal_per_tserver` to limit the number of VirtualWAL instances per tserver. When a new client tries to initialise a VirtualWAL, it will error out if we already have max VWAL instances on the tserver. This will help increase the system stability in the worst case scenario.
    Jira: DB-15204
    
    Test Plan: Jenkins: urgent
    
    Reviewers: sumukh.phalgaonkar, skumar
    
    Reviewed By: sumukh.phalgaonkar
    
    Subscribers: ycdcxcluster, ybase, sumukh.phalgaonkar
    
    Tags: #jenkins-ready
    
    Differential Revision: https://phorge.dev.yugabyte.com/D41714

--- src/yb/cdc/cdc_service.cc
+++ src/yb/cdc/cdc_service.cc
@@ -203,6 +203,9 @@ DEFINE_RUNTIME_AUTO_bool(
     "update these entries in cdc_state table and also move the corresponding table's entry to "
     "unqualified tables list in stream metadata.");
 
+DEFINE_RUNTIME_uint32(cdc_max_virtual_wal_per_tserver, 5,
+                      "Maximum VirtualWAL instances that can be present on a tserver at any time.");
+
 DECLARE_int32(log_min_seconds_to_retain);
 
 static bool ValidateMaxRefreshInterval(const char* flag_name, uint32 value) {
@@ -4918,6 +4921,16 @@ void CDCServiceImpl::InitVirtualWALForCDC(
             AlreadyPresent, "Virtual WAL instance already exists for the session_id: $0",
             session_id),
         resp->mutable_error(), CDCErrorPB::INVALID_REQUEST, context);
+
+    RPC_CHECK_AND_RETURN_ERROR(
+        session_virtual_wal_.size() < FLAGS_cdc_max_virtual_wal_per_tserver,
+        STATUS_FORMAT(
+            InternalError,
+            "Failed to create VirtualWAL for stream: $0 & session_id: $1 as max capacity reached "
+            "for VirtualWAL "
+            "on tserver",
+            stream_id, session_id),
+        resp->mutable_error(), CDCErrorPB::INTERNAL_ERROR, context);
 
     virtual_wal = std::make_shared<CDCSDKVirtualWAL>(
         this, stream_id, session_id, lsn_type);
