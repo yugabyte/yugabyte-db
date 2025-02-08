yugabyte/yugabyte-db

commit a120c2588cab65ebe497e0b8c64bd826b54a99e1

[#24648] YSQL: batch INSERT ON CONFLICT RETURNING
Summary:
Commit 59b3636f505a7dc75ae91ab76348b20a89528e90 introduces read batching
for INSERT ON CONFLICT.  Commit 3f2ac8c27617a8775320f4edb0c829f059bd1eff
refactors the map part of the design.  This commit refactors the
ExecInsert part of the design and introduces RETURNING support.

- Decoupling of YB's read batching from PG's foreign data wrapper
  batching: before this change, the INSERT ON CONFLICT read batching
  flow sticks closely to the INSERT FDW batching.

  - Slots were added to per-relinfo buffers, and flushing happened
    whenever any of these buffers hit the batch size.  This caused some
    inconsistency in the order rows were inserted, and depending on the
    batch size, it could give different results.  Change these buffers
    to be per-plan so that an INSERT that gets routed to multiple
    partitioned tables still maintains insertion order.  One unobvious
    behavior change is that, now, in case of a mixed stream of input
    values, the write RPCs will increase as each batch touches a lot
    more tables whereas previously, each batch touched one table.
    Furthermore, since only one buffer exists per plan now rather than
    one buffer per partitioned table, the memory usage of storing these
    buffers reduces by that much.
  - The flushing also used to happen after we exceeded the batch size,
    not beforehand.  For example, if the batch size is 3, flushing
    happened when trying to insert the fourth value.  This causes some
    headache when trying to support RETURNING as that fourth value is
    already consumed from input while we are trying to flush the
    previous first to third values and trying to return those values.
    Restructure the flow to remove this odd behavior: now, the flush
    would happen after batching the third value, before reading the
    fourth value.  This involves a major code refactor, splitting

src/postgres/src/test/regress/expected/yb_insert_on_conflict.out
@@ -555,6 +555,7 @@ SELECT * FROM pp ORDER BY i;
 (40 rows)
 
 ABORT;
+BEGIN;
 INSERT INTO pp (j) SELECT g * 7 % 40 FROM generate_series(1, 40) g ON CONFLICT (j) DO UPDATE SET i = EXCLUDED.i + 100;
 SELECT * FROM pp ORDER BY i % 100;
   i  | j
@@ -601,6 +602,7 @@ SELECT * FROM pp ORDER BY i % 100;
  199 | 33
 (40 rows)
 
+ABORT;
 --- Partitioned table with TEXT partition key
 CREATE TABLE staff (id SERIAL, name TEXT, department TEXT, PRIMARY KEY (name HASH, department ASC)) PARTITION BY LIST (department);
 CREATE TABLE staff_sales PARTITION OF staff FOR VALUES IN ('Sales');
@@ -780,6 +782,53 @@ RESET yb_enable_inplace_index_update;
 CREATE TABLE index_update (a int PRIMARY KEY, b int);
 INSERT INTO index_update VALUES (1, 2);
 INSERT INTO index_update VALUES (1, 3) ON CONFLICT (a) DO UPDATE SET b = EXCLUDED.b;
+--- Before row triggers
+CREATE OR REPLACE FUNCTION loggingfunc() RETURNS trigger AS $$
+    DECLARE
+        count int;
+    BEGIN
+        SELECT count(*) INTO count FROM pp;
+        RAISE NOTICE '% % % % i=% count=%', TG_NAME, TG_TABLE_NAME, TG_WHEN, TG_OP, new.i, count;
+    RETURN NEW;
+    END;
+$$ LANGUAGE plpgsql;
+-- Trigger on parent table should disable batching for child tables.
+CREATE TRIGGER loggingtrig BEFORE INSERT ON pp FOR EACH ROW EXECUTE PROCEDURE loggingfunc();
+BEGIN;
+INSERT INTO pp (j) SELECT g * 19 % 40 FROM generate_series(1, 5) g ON CONFLICT DO NOTHING;
+NOTICE:  loggingtrig pp2 BEFORE INSERT i=101 count=20
+NOTICE:  loggingtrig pp49 BEFORE INSERT i=102 count=21
+NOTICE:  loggingtrig pp2 BEFORE INSERT i=103 count=21
+NOTICE:  loggingtrig pp49 BEFORE INSERT i=104 count=21
+NOTICE:  loggingtrig pp2 BEFORE INSERT i=105 count=22
+SELECT * FROM pp ORDER BY i;
+  i  | j
+-----+----
+   1 | 17
+   2 | 34
+   3 | 11
+   4 | 28
+   5 |  5
+   6 | 22
+   7 | 39
+   8 | 16
+   9 | 33
+  10 | 10
+  11 | 27
+  12 |  4
+  13 | 21
+  14 | 38
+  15 | 15
+  16 | 32
+  17 |  9
+  18 | 26
+  19 |  3
+  20 | 20
+ 101 | 19
+ 104 | 36
+(22 rows)
+
+ABORT;
 --- After row triggers
 CREATE TABLE trigger_test (i int2, PRIMARY KEY (i ASC));
 -- This test is derived from TestPgUpdatePrimaryKey.java.
@@ -950,6 +999,31 @@ TABLE with_a;
 ABORT;
 BEGIN;
 WITH w(i) AS (
+    INSERT INTO with_a VALUES (11) RETURNING i
+) INSERT INTO with_a VALUES (generate_series(10, 15)) ON CONFLICT (i) DO UPDATE SET i = EXCLUDED.i + (SELECT i FROM w);
+TABLE with_a;
+ i
+----
+ 22
+ 21
+ 15
+ 14
+ 13
+ 12
+  9
+  8
+  7
+  6
+  5
+  4
+  3
+  2
+  1
+(15 rows)
+
+ABORT;
+BEGIN;
+WITH w(i) AS (
     DELETE FROM with_a WHERE i = 10 RETURNING i
 ) INSERT INTO with_a VALUES (generate_series(9, 15)) ON CONFLICT (i) DO UPDATE SET i = EXCLUDED.i + (SELECT i FROM w);
 TABLE with_a;
@@ -971,5 +1045,66 @@ TABLE with_a;
   2
   1
 (15 rows)
+
+ABORT;
+BEGIN;
+WITH w(i) AS (
+    INSERT INTO with_a VALUES (generate_series(6, 11)) ON CONFLICT (i) DO UPDATE SET i = EXCLUDED.i + 100 RETURNING i
+) INSERT INTO with_a SELECT CASE
+    WHEN u < 12 THEN u
+    WHEN u < 14 THEN -(u + (SELECT max(i) FROM w))
+    ELSE u
+    END FROM generate_series(9, 15) u ON CONFLICT (i) DO UPDATE SET i = EXCLUDED.i + 10;
+TABLE with_a;
+  i
+------
+  111
+  108
+  107
+  106
+   20
+   19
+   15
+   14
+   10
+    9
+    5
+    4
+    3
+    2
+    1
+ -123
+ -124
+(17 rows)
+
+ABORT;
+BEGIN;
+WITH w(i) AS (
+    INSERT INTO with_a VALUES (generate_series(6, 10)) ON CONFLICT (i) DO UPDATE SET i = EXCLUDED.i + 100 RETURNING i
+) INSERT INTO with_a SELECT CASE
+    WHEN u < 11 THEN u
+    WHEN u < 13 THEN -(u + (SELECT max(i) FROM w))
+    ELSE u
+    END FROM generate_series(10, 15) u ON CONFLICT (i) DO UPDATE SET i = EXCLUDED.i + 10;
+TABLE with_a;
+  i
+------
+  109
+  108
+  107
+  106
+   20
+   15
+   14
+   13
+   10
+    5
+    4
+    3
+    2
+    1
+ -120
+ -121
+(16 rows)
 
 ABORT;

src/postgres/src/test/regress/expected/yb_insert_on_conflict_1.out
@@ -551,6 +551,7 @@ SELECT * FROM pp ORDER BY i;
 (40 rows)
 
 ABORT;
+BEGIN;
 INSERT INTO pp (j) SELECT g * 7 % 40 FROM generate_series(1, 40) g ON CONFLICT (j) DO UPDATE SET i = EXCLUDED.i + 100;
 SELECT * FROM pp ORDER BY i % 100;
   i  | j
@@ -597,6 +598,7 @@ SELECT * FROM pp ORDER BY i % 100;
  199 | 33
 (40 rows)
 
+ABORT;
 --- Partitioned table with TEXT partition key
 CREATE TABLE staff (id SERIAL, name TEXT, department TEXT, PRIMARY KEY (name HASH, department ASC)) PARTITION BY LIST (department);
 CREATE TABLE staff_sales PARTITION OF staff FOR VALUES IN ('Sales');
@@ -792,6 +794,53 @@ RESET yb_enable_inplace_index_update;
 CREATE TABLE index_update (a int PRIMARY KEY, b int);
 INSERT INTO index_update VALUES (1, 2);
 INSERT INTO index_update VALUES (1, 3) ON CONFLICT (a) DO UPDATE SET b = EXCLUDED.b;
+--- Before row triggers
+CREATE OR REPLACE FUNCTION loggingfunc() RETURNS trigger AS $$
+    DECLARE
+        count int;
+    BEGIN
+        SELECT count(*) INTO count FROM pp;
+        RAISE NOTICE '% % % % i=% count=%', TG_NAME, TG_TABLE_NAME, TG_WHEN, TG_OP, new.i, count;
+    RETURN NEW;
+    END;
+$$ LANGUAGE plpgsql;
+-- Trigger on parent table should disable batching for child tables.
+CREATE TRIGGER loggingtrig BEFORE INSERT ON pp FOR EACH ROW EXECUTE PROCEDURE loggingfunc();
+BEGIN;
+INSERT INTO pp (j) SELECT g * 19 % 40 FROM generate_series(1, 5) g ON CONFLICT DO NOTHING;
+NOTICE:  loggingtrig pp2 BEFORE INSERT i=101 count=20
+NOTICE:  loggingtrig pp49 BEFORE INSERT i=102 count=21
+NOTICE:  loggingtrig pp2 BEFORE INSERT i=103 count=21
+NOTICE:  loggingtrig pp49 BEFORE INSERT i=104 count=21
+NOTICE:  loggingtrig pp2 BEFORE INSERT i=105 count=22
+SELECT * FROM pp ORDER BY i;
+  i  | j
+-----+----
+   1 | 17
+   2 | 34
+   3 | 11
+   4 | 28
+   5 |  5
+   6 | 22
+   7 | 39
+   8 | 16
+   9 | 33
+  10 | 10
+  11 | 27
+  12 |  4
+  13 | 21
+  14 | 38
+  15 | 15
+  16 | 32
+  17 |  9
+  18 | 26
+  19 |  3
+  20 | 20
+ 101 | 19
+ 104 | 36
+(22 rows)
+
+ABORT;
 --- After row triggers
 CREATE TABLE trigger_test (i int2, PRIMARY KEY (i ASC));
 -- This test is derived from TestPgUpdatePrimaryKey.java.
@@ -927,6 +976,15 @@ BEGIN;
 WITH w(i) AS (
     INSERT INTO with_a VALUES (generate_series(6, 11)) ON CONFLICT (i) DO NOTHING RETURNING i
 ) INSERT INTO with_a VALUES (generate_series(10, 15)) ON CONFLICT (i) DO UPDATE SET i = EXCLUDED.i + (SELECT i FROM w);
+ERROR:  ON CONFLICT DO UPDATE command cannot affect row a second time
+HINT:  Ensure that no rows proposed for insertion within the same command have duplicate constrained values.
+TABLE with_a;
+ERROR:  current transaction is aborted, commands ignored until end of transaction block
+ABORT;
+BEGIN;
+WITH w(i) AS (
+    INSERT INTO with_a VALUES (11) RETURNING i
+) INSERT INTO with_a VALUES (generate_series(10, 15)) ON CONFLICT (i) DO UPDATE SET i = EXCLUDED.i + (SELECT i FROM w);
 ERROR:  duplicate key value violates unique constraint "with_a_pkey"
 TABLE with_a;
 ERROR:  current transaction is aborted, commands ignored until end of transaction block
@@ -954,5 +1012,48 @@ TABLE with_a;
   2
   1
 (15 rows)
+
+ABORT;
+BEGIN;
+WITH w(i) AS (
+    INSERT INTO with_a VALUES (generate_series(6, 11)) ON CONFLICT (i) DO UPDATE SET i = EXCLUDED.i + 100 RETURNING i
+) INSERT INTO with_a SELECT CASE
+    WHEN u < 12 THEN u
+    WHEN u < 14 THEN -(u + (SELECT max(i) FROM w))
+    ELSE u
+    END FROM generate_series(9, 15) u ON CONFLICT (i) DO UPDATE SET i = EXCLUDED.i + 10;
+ERROR:  ON CONFLICT DO UPDATE command cannot affect row a second time
+HINT:  Ensure that no rows proposed for insertion within the same command have duplicate constrained values.
+TABLE with_a;
+ERROR:  current transaction is aborted, commands ignored until end of transaction block
+ABORT;
+BEGIN;
+WITH w(i) AS (
+    INSERT INTO with_a VALUES (generate_series(6, 10)) ON CONFLICT (i) DO UPDATE SET i = EXCLUDED.i + 100 RETURNING i
+) INSERT INTO with_a SELECT CASE
+    WHEN u < 11 THEN u
+    WHEN u < 13 THEN -(u + (SELECT max(i) FROM w))
+    ELSE u
+    END FROM generate_series(10, 15) u ON CONFLICT (i) DO UPDATE SET i = EXCLUDED.i + 10;
+TABLE with_a;
+  i
+------
+  110
+  109
+  108
+  107
+  106
+   15
+   14
+   13
+   10
+    5
+    4
+    3
+    2
+    1
+ -121
+ -122
+(16 rows)
 
 ABORT;

src/postgres/src/test/regress/sql/yb_insert_on_conflict.sql
@@ -218,8 +218,10 @@ BEGIN;
 INSERT INTO pp (j) SELECT g * 7 % 40 FROM generate_series(1, 40) g ON CONFLICT DO NOTHING;
 SELECT * FROM pp ORDER BY i;
 ABORT;
+BEGIN;
 INSERT INTO pp (j) SELECT g * 7 % 40 FROM generate_series(1, 40) g ON CONFLICT (j) DO UPDATE SET i = EXCLUDED.i + 100;
 SELECT * FROM pp ORDER BY i % 100;
+ABORT;
 
 --- Partitioned table with TEXT partition key
 CREATE TABLE staff (id SERIAL, name TEXT, department TEXT, PRIMARY KEY (name HASH, department ASC)) PARTITION BY LIST (department);
@@ -361,6 +363,23 @@ CREATE TABLE index_update (a int PRIMARY KEY, b int);
 INSERT INTO index_update VALUES (1, 2);
 INSERT INTO index_update VALUES (1, 3) ON CONFLICT (a) DO UPDATE SET b = EXCLUDED.b;
 
+--- Before row triggers
+CREATE OR REPLACE FUNCTION loggingfunc() RETURNS trigger AS $$
+    DECLARE
+        count int;
+    BEGIN
+        SELECT count(*) INTO count FROM pp;
+        RAISE NOTICE '% % % % i=% count=%', TG_NAME, TG_TABLE_NAME, TG_WHEN, TG_OP, new.i, count;
+    RETURN NEW;
+    END;
+$$ LANGUAGE plpgsql;
+-- Trigger on parent table should disable batching for child tables.
+CREATE TRIGGER loggingtrig BEFORE INSERT ON pp FOR EACH ROW EXECUTE PROCEDURE loggingfunc();
+BEGIN;
+INSERT INTO pp (j) SELECT g * 19 % 40 FROM generate_series(1, 5) g ON CONFLICT DO NOTHING;
+SELECT * FROM pp ORDER BY i;
+ABORT;
+
 --- After row triggers
 CREATE TABLE trigger_test (i int2, PRIMARY KEY (i ASC));
 -- This test is derived from TestPgUpdatePrimaryKey.java.
@@ -419,7 +438,33 @@ TABLE with_a;
 ABORT;
 BEGIN;
 WITH w(i) AS (
+    INSERT INTO with_a VALUES (11) RETURNING i
+) INSERT INTO with_a VALUES (generate_series(10, 15)) ON CONFLICT (i) DO UPDATE SET i = EXCLUDED.i + (SELECT i FROM w);
+TABLE with_a;
+ABORT;
+BEGIN;
+WITH w(i) AS (
     DELETE FROM with_a WHERE i = 10 RETURNING i
 ) INSERT INTO with_a VALUES (generate_series(9, 15)) ON CONFLICT (i) DO UPDATE SET i = EXCLUDED.i + (SELECT i FROM w);
+TABLE with_a;
+ABORT;
+BEGIN;
+WITH w(i) AS (
+    INSERT INTO with_a VALUES (generate_series(6, 11)) ON CONFLICT (i) DO UPDATE SET i = EXCLUDED.i + 100 RETURNING i
+) INSERT INTO with_a SELECT CASE
+    WHEN u < 12 THEN u
+    WHEN u < 14 THEN -(u + (SELECT max(i) FROM w))
+    ELSE u
+    END FROM generate_series(9, 15) u ON CONFLICT (i) DO UPDATE SET i = EXCLUDED.i + 10;
+TABLE with_a;
+ABORT;
+BEGIN;
+WITH w(i) AS (
+    INSERT INTO with_a VALUES (generate_series(6, 10)) ON CONFLICT (i) DO UPDATE SET i = EXCLUDED.i + 100 RETURNING i
+) INSERT INTO with_a SELECT CASE
+    WHEN u < 11 THEN u
+    WHEN u < 13 THEN -(u + (SELECT max(i) FROM w))
+    ELSE u
+    END FROM generate_series(10, 15) u ON CONFLICT (i) DO UPDATE SET i = EXCLUDED.i + 10;
 TABLE with_a;
 ABORT;

java/yb-pgsql/src/test/java/org/yb/pgsql/TestPgRegressInsertOnConflict.java
@@ -23,7 +23,7 @@ public class TestPgRegressInsertOnConflict extends BasePgRegressTest {
 
   @Override
   public int getTestMethodTimeoutSec() {
-    return 300;
+    return 500;
   }
 
   @Test

src/postgres/src/backend/executor/execIndexing.c
@@ -151,6 +151,7 @@ static bool index_expression_changed_walker(Node *node,
 
 static void yb_batch_fetch_conflicting_rows(int idx,
 											ResultRelInfo *resultRelInfo,
+											YbInsertOnConflictBatchState *yb_ioc_state,
 											EState *estate);
 
 /* ----------------------------------------------------------------
@@ -1097,46 +1098,30 @@ ExecCheckIndexConstraints(ResultRelInfo *resultRelInfo, TupleTableSlot *slot,
 
 		checkedIndex = true;
 
-		if (YbIsInsertOnConflictReadBatchingEnabled(resultRelInfo))
+		/* Check for partial index */
+		if (indexInfo->ii_Predicate != NIL)
 		{
-			/*
-			 * YB: for batch insert, the constraint checking actually happens
-			 * later.
-			 */
-			satisfiesConstraint = true;
-
-			Assert(resultRelInfo->ri_RelationDesc == heapRelation);
-			Assert(resultRelInfo->ri_IndexRelationDescs[i] == indexRelation);
-			Assert(resultRelInfo->ri_IndexRelationInfo[i] == indexInfo);
-			yb_batch_fetch_conflicting_rows(i, resultRelInfo, estate);
+			if (!YbIsPartialIndexPredicateSatisfied(indexInfo, estate))
+				continue;
 		}
-		else
-		{
-			/* Check for partial index */
-			if (indexInfo->ii_Predicate != NIL)
-			{
-				if (!YbIsPartialIndexPredicateSatisfied(indexInfo, estate))
-					continue;
-			}
 
-			/*
-			 * FormIndexDatum fills in its values and isnull parameters with the
-			 * appropriate values for the column(s) of the index.
-			 */
-			FormIndexDatum(indexInfo,
-						   slot,
-						   estate,
-						   values,
-						   isnull);
+		/*
+			* FormIndexDatum fills in its values and isnull parameters with the
+			* appropriate values for the column(s) of the index.
+			*/
+		FormIndexDatum(indexInfo,
+						slot,
+						estate,
+						values,
+						isnull);
 
-			satisfiesConstraint =
-				check_exclusion_or_unique_constraint(heapRelation, indexRelation,
-													 indexInfo, &invalidItemPtr,
-													 values, isnull, estate, false,
-													 CEOUC_WAIT, true,
-													 conflictTid,
-													 ybConflictSlot);
-		}
+		satisfiesConstraint =
+			check_exclusion_or_unique_constraint(heapRelation, indexRelation,
+													indexInfo, &invalidItemPtr,
+													values, isnull, estate, false,
+													CEOUC_WAIT, true,
+													conflictTid,
+													ybConflictSlot);
 		if (!satisfiesConstraint)
 			return false;
 	}
@@ -1632,14 +1617,46 @@ index_expression_changed_walker(Node *node, Bitmapset *allUpdatedCols)
  */
 void
 YbBatchFetchConflictingRows(ResultRelInfo *resultRelInfo,
+							YbInsertOnConflictBatchState *yb_ioc_state,
 							EState *estate,
 							List *arbiterIndexes)
 {
-	/* YB: use ExecCheckIndexConstraints to avoid duplicating code. */
-	ItemPointerData unusedConflictTid;
-	(void) ExecCheckIndexConstraints(resultRelInfo, NULL /* slot */,
-									 estate, &unusedConflictTid,
-									 arbiterIndexes, NULL /* ybConflictSlot */);
+	int			i;
+	int			numIndices;
+	RelationPtr relationDescs;
+	Relation	heapRelation;
+	IndexInfo **indexInfoArray;
+
+	/*
+	 * Get information from the result relation info structure.
+	 */
+	numIndices = resultRelInfo->ri_NumIndices;
+	relationDescs = resultRelInfo->ri_IndexRelationDescs;
+	indexInfoArray = resultRelInfo->ri_IndexRelationInfo;
+	heapRelation = resultRelInfo->ri_RelationDesc;
+
+	for (i = 0; i < numIndices; i++)
+	{
+		Relation	indexRelation = relationDescs[i];
+		IndexInfo  *indexInfo;
+
+		if (indexRelation == NULL)
+			continue;
+
+		indexInfo = indexInfoArray[i];
+
+		if (!YbShouldCheckUniqueOrExclusionIndex(indexInfo, indexRelation,
+												 heapRelation, arbiterIndexes))
+			continue;
+
+		Assert(indexInfo->ii_ReadyForInserts ==
+			   indexRelation->rd_index->indisready);
+
+		Assert(resultRelInfo->ri_RelationDesc == heapRelation);
+		Assert(resultRelInfo->ri_IndexRelationDescs[i] == indexRelation);
+		Assert(resultRelInfo->ri_IndexRelationInfo[i] == indexInfo);
+		yb_batch_fetch_conflicting_rows(i, resultRelInfo, yb_ioc_state, estate);
+	}
 }
 
 /*
@@ -1652,13 +1669,12 @@ YbBatchFetchConflictingRows(ResultRelInfo *resultRelInfo,
  */
 static void
 yb_batch_fetch_conflicting_rows(int idx, ResultRelInfo *resultRelInfo,
+								YbInsertOnConflictBatchState *yb_ioc_state,
 								EState *estate)
 {
 	Relation	heap = resultRelInfo->ri_RelationDesc;
 	Relation	index = resultRelInfo->ri_IndexRelationDescs[idx];
 	IndexInfo  *indexInfo = resultRelInfo->ri_IndexRelationInfo[idx];
-	int			num_slots = resultRelInfo->ri_NumSlots;
-	TupleTableSlot **slots = resultRelInfo->ri_Slots;
 	Oid		   *constr_procs;
 	uint16	   *constr_strats;
 	Oid		   *index_collations = index->rd_indcollation;
@@ -1669,6 +1685,8 @@ yb_batch_fetch_conflicting_rows(int idx, ResultRelInfo *resultRelInfo,
 	ExprContext *econtext;
 	TupleTableSlot *existing_slot;
 	TupleTableSlot *save_scantuple;
+	TupleTableSlot **slots = yb_ioc_state->slots;
+	int			num_slots = yb_ioc_state->num_slots;
 
 	if (indexInfo->ii_ExclusionOps)
 	{
@@ -1719,6 +1737,9 @@ yb_batch_fetch_conflicting_rows(int idx, ResultRelInfo *resultRelInfo,
 		 * to this slot.
 		 */
 		TupleTableSlot *slot = slots[i];
+		if (slot->tts_tableOid != resultRelInfo->ri_RelationDesc->rd_id)
+			continue;
+
 		econtext->ecxt_scantuple = slot;
 
 		/* Check for partial index */
@@ -1914,7 +1935,7 @@ yb_batch_fetch_conflicting_rows(int idx, ResultRelInfo *resultRelInfo,
 		YBCPgInsertOnConflictKeyInfo info = {existing_slot};
 		YBCPgYBTupleIdDescriptor *descr =
 			YBCBuildNonNullUniqueIndexYBTupleId(index, existing_values);
-		HandleYBStatus(YBCPgAddInsertOnConflictKey(descr, &info));
+		HandleYBStatus(YBCPgAddInsertOnConflictKey(descr, yb_ioc_state, &info));
 		MemoryContextSwitchTo(oldcontext);
 
 		existing_slot = table_slot_create(heap, NULL);

src/postgres/src/backend/executor/ybcModifyTable.c
@@ -556,7 +556,6 @@ YbIsInsertOnConflictReadBatchingEnabled(ResultRelInfo *resultRelInfo)
 	return (IsYBRelation(resultRelInfo->ri_RelationDesc) &&
 			!IsCatalogRelation(resultRelInfo->ri_RelationDesc) &&
 			resultRelInfo->ri_BatchSize > 1 &&
-			!resultRelInfo->ri_projectReturning &&
 			!(resultRelInfo->ri_TrigDesc &&
 			  (resultRelInfo->ri_TrigDesc->trig_delete_after_row ||
 			   resultRelInfo->ri_TrigDesc->trig_delete_before_row ||

src/postgres/src/include/executor/executor.h
@@ -701,6 +701,7 @@ extern ResultRelInfo *ExecLookupResultRelByOid(ModifyTableState *node,
 											   bool update_cache);
 
 extern void YbBatchFetchConflictingRows(ResultRelInfo *resultRelInfo,
+										YbInsertOnConflictBatchState *yb_ioc_state,
 										EState *estate,
 										List *arbiterIndexes);
 extern bool YbShouldCheckUniqueOrExclusionIndex(IndexInfo *indexInfo,

commit 08b7a2d058cd99785d55b3605d701506c7804826

[#25836] YSQL: fix INSERT ON CONFLICT RETURNING garbage
Summary:
For INSERT ON CONFLICT with RETURNING clause and read batching enabled
(yb_insert_on_conflict_read_batch_size > 0), memory used by the
returning tuple table slot may get freed prematurely, causing the
returned data to be garbage.  This was only tested when returning text
type, but likely any type that internally involves pointers is affected.

This issue was introduced by commit
a120c2588cab65ebe497e0b8c64bd826b54a99e1 which copies the tuple table
slot creation code from FDW batching code but does so erroneously.  The
way FDW does it is by creating up to batch size slots, then for future
batches, reusing those slots, and finally dropping those slots at
ExecEndModifyTable.  The way that commit did it is drop slots as they
are flushed, then create new slots for the next batch.  The problem is
that these slots are dropped before returning the returning slot, and
the returning slot may point to data that is the linked to the dropped
slot.  Secondly, doing it this way increases the amount of tuple
descriptors that are copied since both FDW batching and YB read batching
copy the tuple descriptor whenever making these tuple table slots for
reasons described in code comments.  Postgres does not free these tuple
descriptors, instead relying on the memory context getting dropped (see
upstream postgres commit 06e10abc0bb4297a0754313b4f158bdd5622ca24).

There are two options to fix this:

- Follow what FDW batching does.  This could work by using the existing
  ri_Slots, ri_PlanSlots, ri_NumSlotsInitialized plus a global counter
  for tracking when we reach the batch size.  A benefit is that the
  inefficient for loop in YbFlushSlotsFromBatch can be replaced by a
  saner loop for each (partition) relation, and this also avoids
  dropping/creating slots repeatedly.
- Continue with the current status quo, and drop slots at the end of
  each batch.

Go with the second option for now.

Jira: DB-15132

Test Plan:

src/postgres/src/backend/executor/nodeModifyTable.c
@@ -5390,12 +5390,14 @@ YbFreeInsertOnConflictBatchState(YbInsertOnConflictBatchState *state)
 	list_free(state->pending_relids);
 	for (int i = 0; i < state->num_slots; ++i)
 	{
-		if (state->slots[i])
-		{
-			ExecDropSingleTupleTableSlot(state->slots[i]);
-			Assert(state->planSlots[i]);
-			ExecDropSingleTupleTableSlot(state->planSlots[i]);
-		}
+		/*
+		 * These slots use copied tuple descriptors, so the tuple descriptors
+		 * won't be freed by ExecDropSingleTupleTableSlot; however, there's no
+		 * need to free them explicitly since they'll be freed with the
+		 * per-query memory context.
+		 */
+		ExecDropSingleTupleTableSlot(state->slots[i]);
+		ExecDropSingleTupleTableSlot(state->planSlots[i]);
 	}
 	pfree(state->slots);
 	pfree(state->planSlots);
@@ -5544,11 +5546,9 @@ YbFlushSlotsFromBatch(ModifyTableContext *context,
 			slot = state->slots[state->flush_idx];
 			/*
 			 * Skip in case this table is a different partition from the one we
-			 * are focusing on.  That includes cases where the slot is null
-			 * because that table was previously flushed out.
+			 * are focusing on.
 			 */
-			if (!slot ||
-				slot->tts_tableOid != resultRelInfo->ri_RelationDesc->rd_id)
+			if (slot->tts_tableOid != resultRelInfo->ri_RelationDesc->rd_id)
 			{
 				state->flush_idx++;
 				continue;
@@ -5582,11 +5582,16 @@ YbFlushSlotsFromBatch(ModifyTableContext *context,
 			/* Restore scantuple */
 			econtext->ecxt_scantuple = save_scantuple;
 
-			ExecDropSingleTupleTableSlot(slot);
-			ExecDropSingleTupleTableSlot(planSlot);
-
-			state->slots[state->flush_idx] = NULL;
-			state->planSlots[state->flush_idx] = NULL;
+			/*
+			 * Hack to mark the slots of this flush_idx as processed.  We
+			 * cannot set it to NULL because then we lose track of it and
+			 * cannot ExecDropSingleTupleTableSlot; we cannot
+			 * ExecDropSingleTupleTableSlot right now because
+			 * - this slot is shared for future batches
+			 * - dropping it now might free data that is shared with returnSlot
+			 */
+			state->slots[state->flush_idx]->tts_tableOid = InvalidOid;
+			state->planSlots[state->flush_idx]->tts_tableOid = InvalidOid;
 
 			state->flush_idx++;
 
@@ -5600,6 +5605,20 @@ YbFlushSlotsFromBatch(ModifyTableContext *context,
 	}
 
 	YbDropInsertOnConflictReadSlots(state);
+	for (int i = 0; i < state->num_slots; ++i)
+	{
+		/*
+		 * Since these slots use copied tuple descriptors, they need to be
+		 * freed separately from dropping the slots.
+		 */
+		FreeTupleDesc(state->slots[i]->tts_tupleDescriptor);
+		FreeTupleDesc(state->planSlots[i]->tts_tupleDescriptor);
+		state->slots[i]->tts_tupleDescriptor = NULL;
+		state->planSlots[i]->tts_tupleDescriptor = NULL;
+
+		ExecDropSingleTupleTableSlot(state->slots[i]);
+		ExecDropSingleTupleTableSlot(state->planSlots[i]);
+	}
 	state->num_slots = 0;
 	Assert(!state->flush_idx);
 	Assert(state->pending_relids == NIL);

src/postgres/src/test/regress/expected/yb_insert_on_conflict.out
@@ -1351,3 +1351,38 @@ TABLE multidx;
 ---+---
  1 | 2
 (1 row)
+
+--- GH-25836
+CREATE TABLE it (i int2 PRIMARY KEY, t text);
+WITH w AS (
+    INSERT INTO it SELECT g, repeat(g::text, g % 1000) FROM generate_series(1, 5000) g
+        ON CONFLICT (i) DO UPDATE SET i = -EXCLUDED.i RETURNING t
+) SELECT * FROM w LIMIT 5;
+   t
+-------
+ 1
+ 22
+ 333
+ 4444
+ 55555
+(5 rows)
+
+WITH w AS (
+    INSERT INTO it SELECT g, repeat(g::text, g % 1000) FROM generate_series(1001, 4000) g
+        ON CONFLICT (i) DO UPDATE SET i = -EXCLUDED.i RETURNING t
+) SELECT * FROM w LIMIT 5;
+          t
+----------------------
+ 1001
+ 10021002
+ 100310031003
+ 1004100410041004
+ 10051005100510051005
+(5 rows)
+
+SELECT count(*), sign(i) FROM it GROUP BY (sign(i));
+ count | sign
+-------+------
+  2000 |    1
+  3000 |   -1
+(2 rows)

src/postgres/src/test/regress/expected/yb_insert_on_conflict_1.out
@@ -1369,3 +1369,38 @@ TABLE multidx;
 ---+---
  1 | 2
 (1 row)
+
+--- GH-25836
+CREATE TABLE it (i int2 PRIMARY KEY, t text);
+WITH w AS (
+    INSERT INTO it SELECT g, repeat(g::text, g % 1000) FROM generate_series(1, 5000) g
+        ON CONFLICT (i) DO UPDATE SET i = -EXCLUDED.i RETURNING t
+) SELECT * FROM w LIMIT 5;
+   t
+-------
+ 1
+ 22
+ 333
+ 4444
+ 55555
+(5 rows)
+
+WITH w AS (
+    INSERT INTO it SELECT g, repeat(g::text, g % 1000) FROM generate_series(1001, 4000) g
+        ON CONFLICT (i) DO UPDATE SET i = -EXCLUDED.i RETURNING t
+) SELECT * FROM w LIMIT 5;
+          t
+----------------------
+ 1001
+ 10021002
+ 100310031003
+ 1004100410041004
+ 10051005100510051005
+(5 rows)
+
+SELECT count(*), sign(i) FROM it GROUP BY (sign(i));
+ count | sign
+-------+------
+  2000 |    1
+  3000 |   -1
+(2 rows)

src/postgres/src/test/regress/sql/yb_insert_on_conflict.sql
@@ -596,3 +596,15 @@ CREATE UNIQUE INDEX ON multidx (b);
 INSERT INTO multidx VALUES (1, 2);
 INSERT INTO multidx VALUES (1, 2) ON CONFLICT DO NOTHING;
 TABLE multidx;
+
+--- GH-25836
+CREATE TABLE it (i int2 PRIMARY KEY, t text);
+WITH w AS (
+    INSERT INTO it SELECT g, repeat(g::text, g % 1000) FROM generate_series(1, 5000) g
+        ON CONFLICT (i) DO UPDATE SET i = -EXCLUDED.i RETURNING t
+) SELECT * FROM w LIMIT 5;
+WITH w AS (
+    INSERT INTO it SELECT g, repeat(g::text, g % 1000) FROM generate_series(1001, 4000) g
+        ON CONFLICT (i) DO UPDATE SET i = -EXCLUDED.i RETURNING t
+) SELECT * FROM w LIMIT 5;
+SELECT count(*), sign(i) FROM it GROUP BY (sign(i));