3ce96489727a6f4ccd60423ec8c60008e187bd50
Minghui Yang <myang@yugabyte.com>
2025-02-12 05:40:51 +0000

    [BACKPORT 2024.2][#25983] YSQL: Fix YBBackupTest.TestYSQLRestoreBackupToGlobalCatalogVersionMode failure
    Summary:
    The test started to fail consistently in fastdebug build since
    a0596a86f7f2cd1db9e035e428d927bfb7224372 with an assertion failure:
    
    ```
                    if (stmt->objectType == OBJECT_DATABASE)
                        Assert(ddl_transaction_state.is_global_ddl ||
                               !YBCPgHasWriteOperationsInDdlTxnMode());
    ```
    
    In global catalog version mode, is_global_ddl does not apply so it is not set.
    Therefore the assertion failure.
    
    Fixed by adding one more condition to the assertion: `|| !YBIsDBCatalogVersionMode`.
    Jira: DB-15316
    
    Original commit: 631147e15cc687f08621bf38295555811a9a93fc / D41861
    
    Test Plan: ./yb_build.sh fastdebug --cxx-test tools_yb-backup-test --gtest_filter YBBackupTest.TestYSQLRestoreBackupToGlobalCatalogVersionMode --gcc11
    
    Reviewers: kfranz, sanketh, mihnea
    
    Reviewed By: kfranz
    
    Subscribers: yql
    
    Differential Revision: https://phorge.dev.yugabyte.com/D41877

--- src/postgres/src/backend/utils/misc/pg_yb_utils.c
+++ src/postgres/src/backend/utils/misc/pg_yb_utils.c
@@ -2433,8 +2433,13 @@ CheckAlterDatabaseDdl(PlannedStmt *pstmt)
 		{
 			const RenameStmt *const stmt = castNode(RenameStmt, parsetree);
 			if (stmt->renameType == OBJECT_DATABASE)
-				/* ALTER DATABASE RENAME needs to have global impact. */
-				Assert(ddl_transaction_state.is_global_ddl);
+				/*
+				 * ALTER DATABASE RENAME needs to have global impact. In global
+				 * catalog version mode is_global_ddl does not apply so it is
+				 * not turned on.
+				 */
+				Assert(ddl_transaction_state.is_global_ddl ||
+					   !YBIsDBCatalogVersionMode());
 			break;
 		}
 		case T_AlterOwnerStmt:
@@ -2445,11 +2450,13 @@ CheckAlterDatabaseDdl(PlannedStmt *pstmt)
 			 * ALTER DATABASE OWNER needs to have global impact, however we
 			 * may have a no-op ALTER DATABASE OWNER when the new owner is the
 			 * same as the old owner and there is no write made to pg_database
-			 * to turn on is_global_ddl is not set.
+			 * to turn on is_global_ddl. Also in global catalog version mode
+			 * is_global_ddl does not apply so it is not turned on either.
 			 */
 			if (stmt->objectType == OBJECT_DATABASE)
 				Assert(ddl_transaction_state.is_global_ddl ||
-					   !YBCPgHasWriteOperationsInDdlTxnMode());
+					   !YBCPgHasWriteOperationsInDdlTxnMode() ||
+					   !YBIsDBCatalogVersionMode());
 			break;
 		}
 		default:
