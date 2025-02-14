634db3921b6fdf2117bca1a3ee47ab1a1403f9fc
Jason Kim <git@jasonk.me>
2025-02-13 17:12:34 -0800

    [#26055] YSQL: improve linter for regress schedules
    Summary:
    Followup on commit 5bf7cf19b8b0af82eed2da07af050971b43997ac to further
    improve the linter on regress test schedules:
    
    - Validate the test ordering of isolation regress schedules.
    - Sometimes, tests in schedules directly reference the original postgres
      test.  Before, only ported tests are part of the test ordering lint
      check.  Now, both ported and original tests are part of the ordering
      check.
    
    Fix violations of the lint rules:
    
    - Add yb_pg_ prefix to insert-conflict-do-nothing-2.  It has some
      YB-added tests appended to the end, but the majority of the test is
      ported.  Possibly should split the YB-part out, but leave that work to
      a future commit.
    - Add yb- prefix to various other tests.  Of particular note, renaming
      "ignore-intent-of-rolled-back-savepoint-cross-txn" to
      "yb-ignore-intent-of-rolled-back-savepoint-cross-txn" causes NOTICEs
      to be printed in the expected output matching the following code:
    
          errmsg("identifier \"%s\" will be truncated to \"%.*s\"",
    
      Avoid that by manually shortening the name to
      "yb-ignore-rolled-back-savepoint-intent".
    - Fix order of tests in isolation schedules.
    
    Jira: DB-15382
    
    Test Plan:
    On Almalinux 8:
    
        #!/usr/bin/env bash
        set -e
        shopt -s globstar
        arc lint src/postgres/**/yb_*schedule
        ./yb_build.sh fastdebug --gcc11 daemons initdb
        ./yb_build.sh fastdebug --gcc11 --java-test 'TestPgRegressIsolation#testPgRegress' --sj
        ./yb_build.sh fastdebug --gcc11 --java-test 'TestPgRegressIsolation#testPgRegressWithDelayedTxnApply' --sj
        ./yb_build.sh fastdebug --gcc11 --java-test TestPgRegressIsolationWithoutWaitQueues --sj
        ./yb_build.sh fastdebug --gcc11 --java-test TestPgRegressWaitQueues --sj
    
    Close: #26055
    Jenkins: test regex: TestPgRegress
    
    Reviewers: pjain, myang
    
    Reviewed By: myang
    
    Subscribers: myang, yql
    
    Differential Revision: https://phorge.dev.yugabyte.com/D41914

--- src/lint/regress_schedule.sh
+++ src/lint/regress_schedule.sh
@@ -19,7 +19,8 @@ set -u
 
 . "${BASH_SOURCE%/*}/util.sh"
 
-# Check schedule style: tests should match pattern "test: <test_name>".
+# Check schedule style: tests should match pattern "test: <test_name>".  That
+# is, no inline comments and no parallel tests.
 lines=$(diff \
           <(grep '^test: ' "$1") \
           <(grep -E '^test: [._[:alnum:]-]+$' "$1") \
@@ -37,26 +38,39 @@ if [ -n "$lines" ]; then
 fi
 
 # Check schedule test ordering:
-# For ported tests (those beginning with "yb_pg_"), they should be ordered the
-# same way as in parallel_schedule.  For now, also enforce ordering to be the
-# same as tests in the same group, even if it is not strictly required.  Ignore
-# some tests:
+# For ported tests (those beginning with "yb_pg_") and original tests (those
+# beginning without "yb_"), they should be ordered the same way as in the
+# upstream schedule.  For now, YB does not allow parallel tests, so when the
+# upstream schedule has a parallel group, then YB schedules should flatten that
+# to multiple lines.  For now, enforce ordering to be the same as the order the
+# tests are listed left-to-right in the same parallel group, even if that is
+# not strictly required.  Ignore some tests:
 # - yb_pg_numeric_big: this is in GNUmakefile instead of parallel_schedule
 # - yb_pg_stat: this is a YB test, not ported: prefix "yb_" + name "pg_stat"
 # - yb_pg_stat_backend: this is a YB test, not ported
 if [[ "$1" == src/postgres/src/test/regress/* ]]; then
+  upstream_schedule="src/postgres/src/test/regress/parallel_schedule"
+elif [[ "$1" == src/postgres/src/test/isolation/* ]]; then
+  upstream_schedule="src/postgres/src/test/isolation/isolation_schedule"
+else
+  upstream_schedule=""
+fi
+if [ -n "$upstream_schedule" ]; then
   lines=$(diff \
-            <(grep '^test: yb_pg_' "$1" | sed 's/test: yb_pg_//') \
-            <(grep '^test: ' "${1%/*}/parallel_schedule" \
+            <(perl -ne 'print if '\
+'/^test: (yb_pg_(?!(numeric_big|stat|stat_backend)$)|(?!yb[_-]))/' "$1" \
+                | sed -e 's/test: yb_pg_//' -e 's/test: //') \
+            <(grep '^test: ' "$upstream_schedule" \
                 | sed 's/test: //' | tr ' ' '\n') \
             | grep '^<' \
-            | sed 's/< /test: yb_pg_/' \
-            | grep -Ev '^test: yb_pg_(numeric_big$|stat$|stat_backend$)')
+            | sed 's/< /test: \(yb_pg_\)?/')
   if [ -n "$lines" ]; then
+    set -e
     while read -r line; do
-      grep -nF "$line" "$1" \
-        | sed 's/^/error:test_ordering:'\
-'Should follow parallel_schedule test order:/'
+      grep -nE "$line" "$1" \
+        | sed 's,^,error:test_ordering:'\
+"Should follow $upstream_schedule test order:,"
     done <<<"$lines"
+    set +e
   fi
 fi
