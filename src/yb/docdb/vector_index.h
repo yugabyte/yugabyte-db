f70fbaf1e549f37d5e484d12e512573baadc3c09
Sergei Politov <spolitov@gmail.com>
2025-02-18 16:16:17 +0300

    [#26046] DocDB: Vector index backfill consistency
    Summary:
    In order to provide vector index backfill consistency we should use proper read time while reading indexed table during backfill.
    It is possible to use add table operation hybrid time for this purpose, so all rows written/committed before this time will be added to index during backfill. While all rows written after this time, will be inserted to the index while intents being applied.
    Jira: DB-15374
    
    Test Plan: PgIndexBackfillTest.VectorIndex
    
    Reviewers: arybochkin
    
    Reviewed By: arybochkin
    
    Subscribers: jason, smishra, slingam, mihnea, ybase, yql
    
    Tags: #jenkins-ready
    
    Differential Revision: https://phorge.dev.yugabyte.com/D41707

--- src/yb/docdb/vector_index.h
+++ src/yb/docdb/vector_index.h
@@ -53,6 +53,7 @@ class VectorIndex {
   virtual Slice indexed_table_key_prefix() const = 0;
   virtual ColumnId column_id() const = 0;
   virtual const std::string& path() const = 0;
+  virtual HybridTime hybrid_time() const = 0;
 
   virtual Status Insert(
       const VectorIndexInsertEntries& entries, const rocksdb::UserFrontiers* frontiers) = 0;
@@ -77,6 +78,7 @@ Result<VectorIndexPtr> CreateVectorIndex(
     const std::string& data_root_dir,
     rpc::ThreadPool& thread_pool,
     Slice indexed_table_key_prefix,
+    HybridTime hybrid_time,
     const qlexpr::IndexInfo& index_info,
     const DocDB& doc_db);
 
