150c0ca8aac1fbfc9188efda2f9412eb547729e3
Jethro Mak <88681329+Jethro-M@users.noreply.github.com>
2025-02-21 16:29:12 -0500

    [PLAT-16348] Add proper error handling for retry/rollback action on universe and task details page
    Summary:
    The retry task button and rollback task button doesn't have proper error handling implemented.
    If the promise is rejected, then it results in an uncaught rejected promise.
    
    This diff removes the redux logic and simplifies the the retry/rollback calls to just axios api calls. There is no extra logic happening that would require us to use redux here.
    
    Test Plan:
    Verify that we're handling the reject promises correctly:
    - Simulate rejected promise in the api call and verify the YBA UI displays the error message as a toast.
    
    Verify that the retry and rollback apis are being called correctly:
    - Observe successful retry and rollback calls.
    
    Verify that we're able to handle displaying error toasts by getting into a state where the request would fail. (End to end test)
    - Start a switchover task for an xCluster DR config.
    - Abort the task right after the xCluster config is deleted.
    - Observe that we handle the error returned by displaying an error toast.
    {F333088}
    
    Reviewers: hzare, rmadhavan
    
    Reviewed By: rmadhavan
    
    Subscribers: yugaware
    
    Differential Revision: https://phorge.dev.yugabyte.com/D41851

--- managed/ui/src/components/universes/UniverseStatus/UniverseStatusContainer.js
+++ managed/ui/src/components/universes/UniverseStatus/UniverseStatusContainer.js
@@ -4,28 +4,12 @@ import { connect } from 'react-redux';
 
 import UniverseStatus from './UniverseStatus';
 import { fetchUniverseInfo, fetchUniverseInfoResponse } from '../../../actions/universe';
-import {
-  retryTask,
-  retryTaskResponse,
-  rollbackTask,
-  rollbackTaskResponse
-} from '../../../actions/tasks';
 
 const mapDispatchToProps = (dispatch) => {
   return {
     fetchCurrentUniverse: (universeUUID) => {
       dispatch(fetchUniverseInfo(universeUUID)).then((response) => {
         dispatch(fetchUniverseInfoResponse(response.payload));
-      });
-    },
-    retryCurrentTask: (taskUUID) => {
-      return dispatch(retryTask(taskUUID)).then((response) => {
-        return dispatch(retryTaskResponse(response.payload));
-      });
-    },
-    rollbackCurrentTask: (taskUUID) => {
-      return dispatch(rollbackTask(taskUUID)).then((response) => {
-        return dispatch(rollbackTaskResponse(response.payload));
       });
     }
   };
