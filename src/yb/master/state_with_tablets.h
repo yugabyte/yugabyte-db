e4a3a87af82ae75ba220fcb605de4dab25ab8534
Anubhav Srivastava <asrivastava@yugabyte.com>
2025-02-24 11:45:50 -0800

    [BACKPORT 2.25.1][#25628] docdb: Retry delete operations for expired snapshots
    Summary:
    Original commit: 08d48f77e8358a9ab7462c35e6482d35adf24552 / D41984
    Currently, if the DeleteSnapshot tasks time out for an expired snapshot, we will never retry the operations because TryStartDelete fails if the snapshot is already DELETING. This causes the snapshot to be stuck in the DELETING and expired state. The cluster logs an INFO log (Snapshot <id> has expired) on every poll interval (every 5s). This diff makes it so we don't call TryStartDelete on DELETING snapshots, and thus do PrepareOperations instead (which retriggers the delete snapshot tasks on tablets, accounting for whether a task is already running).
    Jira: DB-14878
    
    Test Plan: `./yb_build.sh --cxx-test snapshot-test --gtest_filter SnapshotTest.RescheduleOperationsForExpiredSnapshot`
    
    Reviewers: zdrudi
    
    Reviewed By: zdrudi
    
    Subscribers: ybase
    
    Differential Revision: https://phorge.dev.yugabyte.com/D42088

--- src/yb/master/state_with_tablets.h
+++ src/yb/master/state_with_tablets.h
@@ -62,6 +62,8 @@ class StateWithTablets {
   bool AllTabletsDone() const;
   bool PassedSinceCompletion(const MonoDelta& duration) const;
   std::vector<TabletId> TabletIdsInState(SysSnapshotEntryPB::State state);
+
+  // Returns true if the tablet is in a terminal state now.
   bool Done(const TabletId& tablet_id, int64_t serial, Status status);
   bool AllInState(SysSnapshotEntryPB::State state) const;
   bool HasInState(SysSnapshotEntryPB::State state);
