3e32b08b0eed540851b3030923c8334c2da4728b
Eric Sheng <esheng@yugabyte.com>
2025-02-27 07:00:40 -0800

    [#26261] docdb: Add annotations for shared memory
    Summary:
    When modifying data structures in shared memory from PG, it is necessary to avoid compiler
    reordering around stores, since PG may crash at any point, and compiler reordering may
    complicate crash recovery or make it impossible.
    
    Additionally, since many constructs like condition variables are not available, PG may sometimes
    only be allowed to do a subset of operations. Issues around this are generally difficult to check
    from normal testing since they only become problematic when postgres crashes at very specific points of code.
    
    This diff adds some annotations to avoid compiler reording around stores to shared memory and to
    make clear (and enforce) what fields code from PG is and is not allowed to read or modify.
    
    - `DCHECK_PARENT_PROCESS()` will add a debug-only check that ensures the code is not being called
      from PG.
    
    The following annotate fields and enforce debug-only checks on reads and writes from PG:
    - `ChildProcessForbidden<T>` marks a field as being inaccessible (both read and write) from PG. This
      is meant for cases such as pointers to private TServer memory.
    - `ChildProcessReadOnly<T>` marks a field as being readonly from PG.
    - `ChildProcessReadWrite<T>` marks a field as being read/write from PG.
    
    The following annotate individual stores/loads and are meant for use in code called from PG:
    - `SHARED_MEMORY_STORE(dest, value)` sets `dest` to `value`, where `dest` is one of the
      `ChildProcess...<T>` annotated fields above. This macro prevents any compiler reordering around
      the store.
    - `SHARED_MEMORY_LOAD(value)` does nothing special, since we don't need special handling for loads,
      but is provided for symmetry, and to hide the use of `operator*()` and cast to const required for annotated fields.
    Jira: DB-15605
    
    Test Plan: Added test: `./yb_build.sh --cxx-test util_annotations-test`
    
    Reviewers: sergei
    
    Reviewed By: sergei
    
    Subscribers: ybase
    
    Differential Revision: https://phorge.dev.yugabyte.com/D42223

---
+++ src/yb/util/shmem/annotations.h
@@ -0,0 +1,174 @@
+// Copyright (c) YugabyteDB, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+// in compliance with the License.  You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software distributed under the License
+// is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+// or implied.  See the License for the specific language governing permissions and limitations
+// under the License.
+//
+
+#pragma once
+
+#include <atomic>
+#include <type_traits>
+#include <utility>
+
+#include "yb/util/debug.h"
+#include "yb/util/logging.h"
+
+namespace yb {
+
+bool IsChildProcess();
+
+void MarkChildProcess();
+
+#define DCHECK_PARENT_PROCESS() DCHECK(!IsChildProcess()) << "Access from child process not allowed"
+
+template<typename T, bool AllowChildRead>
+class SharedMemoryAnnotatedObject {
+ public:
+  SharedMemoryAnnotatedObject() = default;
+
+  // Not explicit in order to allow various constructs that work with unannotated fields, like
+  // int_field_ = std::exchange(other.int_field_, 0).
+  template<typename... Args>
+  SharedMemoryAnnotatedObject(Args... args) // NOLINT(runtime/explicit)
+      : object_(std::forward<Args>(args)...) {}
+
+  inline T* operator->() ATTRIBUTE_ALWAYS_INLINE { return &operator*(); }
+  inline const T* operator->() const ATTRIBUTE_ALWAYS_INLINE { return &operator*(); }
+
+  inline T& operator*() ATTRIBUTE_ALWAYS_INLINE {
+    DCHECK_PARENT_PROCESS();
+    return object_;
+  }
+
+  inline const T& operator*() const ATTRIBUTE_ALWAYS_INLINE {
+    if constexpr (kIsDebug && !AllowChildRead) {
+      DCHECK_PARENT_PROCESS();
+    }
+    return object_;
+  }
+
+  inline operator T() const ATTRIBUTE_ALWAYS_INLINE {
+    return operator*();
+  }
+
+  inline void SharedMemoryStore(const T& value) ATTRIBUTE_ALWAYS_INLINE {
+    // This is only used on parent process, so no special handling needed. This is provided
+    // to provide a uniform interface with ChildProcessRW.
+    DCHECK_PARENT_PROCESS();
+    object_ = value;
+  }
+
+ private:
+  T object_;
+};
+
+template<typename T, bool Read>
+std::ostream& operator<<(std::ostream& os, const SharedMemoryAnnotatedObject<T, Read>& obj) {
+  return os << *obj;
+}
+
+template<typename T>
+using ChildProcessForbidden =
+    SharedMemoryAnnotatedObject<T, false /* AllowChildRead */>;
+
+template<typename T>
+using ChildProcessRO =
+    SharedMemoryAnnotatedObject<T, true /* AllowChildRead */>;
+
+inline ATTRIBUTE_ALWAYS_INLINE void CompilerBarrier() {
+  std::atomic_signal_fence(std::memory_order_acq_rel);
+}
+
+// This is explicitly not for concurrent use. std::atomic is only used to avoid torn writes when
+// a child process crashes.
+template<typename T>
+class ChildProcessRW {
+ public:
+  ChildProcessRW() = default;
+
+  ChildProcessRW(const ChildProcessRW& other) : object_(*other) {}
+
+  // Not explicit in order to allow various constructs that work with unannotated fields, like
+  // int_field_ = std::exchange(other.int_field_, 0).
+  template<typename... Args>
+  ChildProcessRW(Args... args) // NOLINT(runtime/explicit)
+      : object_(std::forward<Args>(args)...) {}
+
+  inline ChildProcessRW& operator=(const ChildProcessRW& other) ATTRIBUTE_ALWAYS_INLINE {
+    SharedMemoryStore(*other);
+    return *this;
+  }
+
+  inline T operator->() const ATTRIBUTE_ALWAYS_INLINE { return &operator*(); }
+
+  inline T operator*() const ATTRIBUTE_ALWAYS_INLINE {
+    return object_.load(std::memory_order_relaxed);
+  }
+
+  inline operator T() const ATTRIBUTE_ALWAYS_INLINE {
+    return operator*();
+  }
+
+  inline void SharedMemoryStore(T value) ATTRIBUTE_ALWAYS_INLINE {
+    // This is for nonconcurrent use, where we normally do not care about reordering. But we do not
+    // want writes split over multiple instructions (what if we crash halfway) or compiler
+    // reordering (makes reasoning about post-crash state difficult). CPU reordering is perfectly
+    // fine since this is nonconcurrent, so we use barrier + volatile to enforce ordering instead
+    // of seq_cst atomics.
+    CompilerBarrier();
+    static_cast<volatile std::atomic<T>*>(&object_)->store(value, std::memory_order_relaxed);
+    CompilerBarrier();
+  }
+
+ private:
+  static_assert(std::atomic<T>::is_always_lock_free);
+  std::atomic<T> object_;
+};
+
+template<typename T>
+std::ostream& operator<<(std::ostream& os, const ChildProcessRW<T>& obj) {
+  return os << *obj;
+}
+
+// Stores to shared memory from code callable by PG processes should use these helpers. All stores
+// to shared memory should be either annotated with this, or be loaded in a function with
+// DCHECK_PARENT_PROCESS().
+//
+// These helpers add compiler barriers to avoid compiler reordering, which is dangerous if the
+// process may crash at any point, e.g. for a vector's emplace_back:
+//      std::allocator_traits<Allocator>::construct(
+//          allocator_, data_ + num_elements_, std::forward<Args>(args)...);
+//      ++num_elements_;
+//
+// may be reordered to:
+//      size_t num_elments = num_elements_++;
+//      std::allocator_traits<Allocator>::construct(
+//          allocator_, data_ + num_elements, std::forward<Args>(args)...);
+//
+// and crash at:
+//      size_t num_elments = num_elements_++;
+//      -- CRASH HERE --
+//
+// resulting in a new element that was never constructed.
+//
+// This should instead be written as:
+//      std::allocator_traits<Allocator>::construct(
+//          allocator_, data_ + num_elements_, std::forward<Args>(args)...);
+//      SHARED_MEMORY_STORE(num_elements_, num_elements_ + 1);
+//
+// to ensure that the store to num_elements_ is not reordered.
+#define SHARED_MEMORY_STORE(destination, value) (destination).SharedMemoryStore(value)
+
+// Loads don't require any special handling, but this macro is provided to hide
+// the use of operator*() everywhere for the ChildProcess*<> wrappers, and to force use of
+// const overloads (since non-const access of ChildProcess*<> wrappers is treated as a write).
+#define SHARED_MEMORY_LOAD(value) (*std::as_const(value))
+
+} // namespace yb
