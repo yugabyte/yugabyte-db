yugabyte/yugabyte-db

commit 83d3cca918ed16b87d825f7b660f22e3ae389293

[#3109] YSQL: Introduce MVP support for running DDLs within regular txn block
Summary:
Before this revision, DDLs run within their own autonomous transaction and autocommit at the end of the statement.

This revision introduces support to run DDL statements within the regular transaction block i.e. without creating autonomous transactions. This support builds upon the
existing rollback support added as part of the DDL atomicity project. Earlier, the autonomous transactions were started upon receiving a DDL statement and committed right after the execution. With the unification, there won't be any separate transaction for DDLs. Instead, they will execute as part of the regular transaction (aka plain) which are created lazily upon the first write.

A notable exception to this support is the `CREATE INDEX` command which is by default executed as an online schema migration i.e. `CREATE INDEX CONCURRENTLY`. This
statement still creates its own autonomous transaction because online schema migration divides the DDL into several steps which have to be committed separately and the changes made by a single statement should be visible to all other backends.

Left for follow up revisions:
1. Handling savepoints in transactions containing DDLs
2. Table rewrites such as REINDEX, TRUNCATE are supported but need to be verified via additional unit tests in a follow-up. Note that `REINDEX CONCURRENTLY` isn't supported in YSQL currently. So only the non-concurrent version of `REINDEX` is being considered here.
3. More tests - DDLs from functions & procedures. DDL vs DDL/DML concurrency etc.
4. Promotion of transaction to global if a DDL is encountered. Need to validate that the auto promotion of transaction from local to global works with DDLs and if it doesn't, then will add code to do that. Left for follow up.

**Upgrade/Downgrade safety:**
The changes are protected via test flag `TEST_ysql_yb_ddl_transaction_block_enabled`.
Jira: DB-1839

src/yb/yql/pggate/pggate.cc
@@ -813,7 +813,8 @@ Status PgApiImpl::NewCreateDatabase(
   return AddToCurrentPgMemctx(
       std::make_unique<PgCreateDatabase>(
           pg_session_, database_name, database_oid, source_database_oid, next_oid, yb_clone_info,
-          colocated, pg_txn_manager_->IsDdlMode()),
+          colocated, pg_txn_manager_->IsDdlMode(),
+          pg_txn_manager_->IsDdlModeWithRegularTransactionBlock()),
       handle);
 }
 
@@ -889,7 +890,8 @@ Status PgApiImpl::NewCreateTablegroup(
          "Tablegroup is being created outside of DDL mode");
   return AddToCurrentPgMemctx(
       std::make_unique<PgCreateTablegroup>(
-          pg_session_, database_name, database_oid, tablegroup_oid, tablespace_oid),
+          pg_session_, database_name, database_oid, tablegroup_oid, tablespace_oid,
+          pg_txn_manager_->IsDdlModeWithRegularTransactionBlock()),
       handle);
 }
 
@@ -903,9 +905,11 @@ Status PgApiImpl::NewDropTablegroup(
          IllegalState,
          "Tablegroup is being dropped outside of DDL mode");
   return AddToCurrentPgMemctx(
-      std::make_unique<PgDropTablegroup>(pg_session_, database_oid, tablegroup_oid), handle);
+      std::make_unique<PgDropTablegroup>(
+          pg_session_, database_oid, tablegroup_oid,
+          pg_txn_manager_->IsDdlModeWithRegularTransactionBlock()),
+      handle);
 }
-
 
 Status PgApiImpl::ExecDropTablegroup(PgStatement* handle) {
   return ExecDdlWithSyscatalogChanges<PgDropTablegroup>(handle, *pg_session_);
@@ -936,7 +940,8 @@ Status PgApiImpl::NewCreateTable(const char* database_name,
           pg_session_, database_name, schema_name, table_name, table_id, is_shared_table,
           is_sys_catalog_table, if_not_exist, ybrowid_mode, is_colocated_via_database,
           tablegroup_oid, colocation_id, tablespace_oid, is_matview, pg_table_oid,
-          old_relfilenode_oid, is_truncate, pg_txn_manager_->IsDdlMode()),
+          old_relfilenode_oid, is_truncate, pg_txn_manager_->IsDdlMode(),
+          pg_txn_manager_->IsDdlModeWithRegularTransactionBlock()),
       handle);
 }
 
@@ -963,7 +968,10 @@ Status PgApiImpl::ExecCreateTable(PgStatement* handle) {
 
 Status PgApiImpl::NewAlterTable(const PgObjectId& table_id, PgStatement** handle) {
   return AddToCurrentPgMemctx(
-      std::make_unique<PgAlterTable>(pg_session_, table_id, pg_txn_manager_->IsDdlMode()), handle);
+      std::make_unique<PgAlterTable>(
+          pg_session_, table_id, pg_txn_manager_->IsDdlMode(),
+          pg_txn_manager_->IsDdlModeWithRegularTransactionBlock()),
+      handle);
 }
 
 Status PgApiImpl::AlterTableAddColumn(
@@ -1132,7 +1140,8 @@ Status PgApiImpl::NewCreateIndex(const char* database_name,
           pg_session_, database_name, schema_name, index_name, index_id, is_shared_index,
           is_sys_catalog_index, if_not_exist, PG_YBROWID_MODE_NONE, is_colocated_via_database,
           tablegroup_oid, colocation_id, tablespace_oid, false /* is_matview */, pg_table_id,
-          old_relfilenode_id, false /* is_truncate */, pg_txn_manager_->IsDdlMode(), base_table_id,
+          old_relfilenode_id, false /* is_truncate */, pg_txn_manager_->IsDdlMode(),
+          pg_txn_manager_->IsDdlModeWithRegularTransactionBlock(), base_table_id,
           is_unique_index, skip_index_backfill),
       handle);
 }
@@ -1868,6 +1877,13 @@ Status PgApiImpl::CommitPlainTransaction() {
   return pg_txn_manager_->CommitPlainTransaction();
 }
 
+Status PgApiImpl::CommitPlainTransactionContainingDDL(
+    PgOid ddl_db_oid, bool ddl_is_silent_modification) {
+  RETURN_NOT_OK(CommitTransactionScopedSessionState());
+  return pg_txn_manager_->CommitPlainTransactionContainingDDL(
+      ddl_db_oid, ddl_is_silent_modification);
+}
+
 Status PgApiImpl::AbortPlainTransaction() {
   ClearSessionState();
   return pg_txn_manager_->AbortPlainTransaction();
@@ -1899,6 +1915,11 @@ Status PgApiImpl::SetInTxnBlock(bool in_txn_blk) {
 
 Status PgApiImpl::SetReadOnlyStmt(bool read_only_stmt) {
   return pg_txn_manager_->SetReadOnlyStmt(read_only_stmt);
+}
+
+Status PgApiImpl::SetDdlStateInPlainTransaction() {
+  pg_session_->ResetHasWriteOperationsInDdlMode();
+  return pg_txn_manager_->SetDdlStateInPlainTransaction();
 }
 
 Status PgApiImpl::EnterSeparateDdlTxnMode() {

java/yb-pgsql/src/test/java/org/yb/pgsql/TestDdlTransactionBlocks.java
@@ -1,0 +1,46 @@
+// Copyright (c) YugaByte, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+// in compliance with the License.  You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software distributed under the License
+// is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+// or implied.  See the License for the specific language governing permissions and limitations
+// under the License.
+//
+package org.yb.pgsql;
+
+import org.yb.minicluster.MiniYBClusterBuilder;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.yb.util.YBTestRunnerNonTsanOnly;
+
+/**
+ * Runs the pg_regress test suite for DDL transaction blocks support.
+ */
+@RunWith(value = YBTestRunnerNonTsanOnly.class)
+public class TestDdlTransactionBlocks extends BasePgRegressTest {
+  @Override
+  public int getTestMethodTimeoutSec() {
+    return getPerfMaxRuntime(500, 1000, 1200, 1200, 1200);
+  }
+
+  @Override
+  protected void customizeMiniClusterBuilder(MiniYBClusterBuilder builder) {
+    super.customizeMiniClusterBuilder(builder);
+    builder.enablePgTransactions(true);
+    builder.addMasterFlag("vmodule", "pgsql_operation=5");
+    builder.addCommonTServerFlag(
+          "vmodule", "pg_client_session=5,pg_txn_manager=5,pg_client_service=5");
+    builder.addCommonTServerFlag("ysql_log_statement", "all");
+    builder.addCommonTServerFlag("TEST_ysql_yb_ddl_transaction_block_enabled", "true");
+  }
+
+  @Test
+  public void yb_ddl_txn_block_tests() throws Exception {
+    runPgRegressTest("yb_ddl_txn_block_schedule");
+  }
+}

src/postgres/src/backend/commands/indexcmds.c
@@ -2153,7 +2153,10 @@ DefineIndex(Oid relationId,
 
 		StartTransactionCommand();
 
-		YBIncrementDdlNestingLevel(YB_DDL_MODE_VERSION_INCREMENT);
+		YbDdlMode ddl_mode = (*YBCGetGFlags()->TEST_ysql_yb_ddl_transaction_block_enabled) ?
+							  YB_DDL_MODE_ONLINE_SCHEMA_CHANGE_VERSION_INCREMENT :
+							  YB_DDL_MODE_VERSION_INCREMENT;
+		YBIncrementDdlNestingLevel(ddl_mode);
 
 		/* Wait for all backends to have up-to-date version. */
 		YbWaitForBackendsCatalogVersion();
@@ -2183,7 +2186,7 @@ DefineIndex(Oid relationId,
 										"concurrent index backfill");
 
 		StartTransactionCommand();
-		YBIncrementDdlNestingLevel(YB_DDL_MODE_VERSION_INCREMENT);
+		YBIncrementDdlNestingLevel(ddl_mode);
 
 		/* Wait for all backends to have up-to-date version. */
 		YbWaitForBackendsCatalogVersion();

src/postgres/src/backend/utils/cache/inval.c
@@ -313,7 +313,8 @@ AddInvalidationMessage(InvalidationMsgsGroup *group, int subgroup,
 	Assert(YBGetDdlOriginalNodeTag() == T_YbBackfillIndexStmt ||
 		   YBGetDdlOriginalNodeTag() == T_Invalid ||
 		   yb_non_ddl_txn_for_sys_tables_allowed ||
-		   YBGetDdlNestingLevel() > 0);
+		   YBGetDdlNestingLevel() > 0 ||
+		   YBGetDdlUseRegularTransactionBlock());
 	if (IsYugaByteEnabled())
 		Assert(msg->id != SHAREDINVALSMGR_ID &&
 			   msg->id != SHAREDINVALRELMAP_ID);

src/postgres/src/include/pg_yb_utils.h
@@ -798,6 +798,7 @@ bool		YBIsInitDbAlreadyDone();
 
 extern int YBGetDdlNestingLevel();
 extern NodeTag YBGetDdlOriginalNodeTag();
+extern bool YBGetDdlUseRegularTransactionBlock();
 extern void YbSetIsGlobalDDL();
 extern void YbIncrementPgTxnsCommitted();
 
@@ -805,7 +806,8 @@ typedef enum YbSysCatalogModificationAspect
 {
 	YB_SYS_CAT_MOD_ASPECT_ALTERING_EXISTING_DATA = 1,
 	YB_SYS_CAT_MOD_ASPECT_VERSION_INCREMENT = 2,
-	YB_SYS_CAT_MOD_ASPECT_BREAKING_CHANGE = 4
+	YB_SYS_CAT_MOD_ASPECT_BREAKING_CHANGE = 4,
+	YB_SYS_CAT_MOD_ASPECT_ONLINE_SCHEMA_CHANGE = 8,
 } YbSysCatalogModificationAspect;
 
 typedef enum YbDdlMode
@@ -820,10 +822,17 @@ typedef enum YbDdlMode
 	YB_DDL_MODE_BREAKING_CHANGE = (YB_SYS_CAT_MOD_ASPECT_ALTERING_EXISTING_DATA |
 								   YB_SYS_CAT_MOD_ASPECT_VERSION_INCREMENT |
 								   YB_SYS_CAT_MOD_ASPECT_BREAKING_CHANGE),
+
+	YB_DDL_MODE_ONLINE_SCHEMA_CHANGE_VERSION_INCREMENT =
+		(YB_SYS_CAT_MOD_ASPECT_ALTERING_EXISTING_DATA |
+		 YB_SYS_CAT_MOD_ASPECT_VERSION_INCREMENT |
+		 YB_SYS_CAT_MOD_ASPECT_ONLINE_SCHEMA_CHANGE),
 } YbDdlMode;
 
 void		YBIncrementDdlNestingLevel(YbDdlMode mode);
 void		YBDecrementDdlNestingLevel();
+
+extern void YBCommitTransactionContainingDDL();
 
 typedef struct YbDdlModeOptional
 {

src/postgres/src/test/regress/expected/yb.orig.ddl_txn_block.out
@@ -1,0 +1,150 @@
+CREATE TABLE test1 (id int PRIMARY KEY);
+-- Test rollback of DDL+DML transaction block.
+BEGIN ISOLATION LEVEL REPEATABLE READ;
+CREATE TABLE test2 (id int);
+INSERT INTO test1 VALUES (2);
+ROLLBACK;
+-- No rows in test1.
+SELECT * FROM test1;
+ id 
+----
+(0 rows)
+
+-- test2 does not exist.
+SELECT * FROM test2;
+ERROR:  relation "test2" does not exist
+LINE 1: SELECT * FROM test2;
+                      ^
+-- Test commit of DDL+DML transaction block.
+BEGIN ISOLATION LEVEL REPEATABLE READ;
+CREATE TABLE test2 (id int);
+INSERT INTO test1 VALUES (2);
+COMMIT;
+SELECT * FROM test1;
+ id 
+----
+  2
+(1 row)
+
+SELECT * FROM test2;
+ id 
+----
+(0 rows)
+
+-- Test rollback of a block with multiple DDLs
+BEGIN ISOLATION LEVEL REPEATABLE READ;
+CREATE TABLE test3 (id int);
+CREATE TABLE test4 (id int);
+ALTER TABLE test1 ADD COLUMN value text;
+INSERT INTO test1 VALUES (3, 'text');
+ROLLBACK;
+SELECT * FROM test3;
+ERROR:  relation "test3" does not exist
+LINE 1: SELECT * FROM test3;
+                      ^
+SELECT * FROM test4;
+ERROR:  relation "test4" does not exist
+LINE 1: SELECT * FROM test4;
+                      ^
+-- TODO(#23825): The following select will retry after encountering a version
+-- mismatch error, and it will result in double output for now. This is a known
+-- issue.
+SELECT * FROM test1;
+ id 
+----
+(0 rows)
+
+ id 
+----
+  2
+(1 row)
+
+-- Test commit of a block with multiple DDLs
+BEGIN ISOLATION LEVEL REPEATABLE READ;
+CREATE TABLE test3 (id int);
+CREATE TABLE test4 (id int);
+ALTER TABLE test1 ADD COLUMN value text;
+INSERT INTO test1 VALUES (3, 'text');
+COMMIT;
+SELECT * FROM test3;
+ id 
+----
+(0 rows)
+
+SELECT * FROM test4;
+ id 
+----
+(0 rows)
+
+SELECT * FROM test1;
+ id | value 
+----+-------
+  2 | 
+  3 | text
+(2 rows)
+
+-- Same test as above but the first statement is a DML
+BEGIN ISOLATION LEVEL REPEATABLE READ;
+INSERT INTO test1 VALUES (5, 'text');
+CREATE TABLE test5 (id int);
+CREATE TABLE test6 (id int);
+ALTER TABLE test1 ADD COLUMN value1 text;
+INSERT INTO test1 VALUES (4, 'text', 'text2');
+ROLLBACK;
+SELECT * FROM test5;
+ERROR:  relation "test5" does not exist
+LINE 1: SELECT * FROM test5;
+                      ^
+SELECT * FROM test6;
+ERROR:  relation "test6" does not exist
+LINE 1: SELECT * FROM test6;
+                      ^
+-- TODO(#23825): The following select will retry after encountering a version
+-- mismatch error, and it will result in double output for now. This is a known
+-- issue.
+SELECT * FROM test1;
+ id | value 
+----+-------
+(0 rows)
+
+ id | value 
+----+-------
+  2 | 
+  3 | text
+(2 rows)
+
+BEGIN ISOLATION LEVEL REPEATABLE READ;
+INSERT INTO test1 VALUES (5, 'text');
+CREATE TABLE test5 (id int);
+CREATE TABLE test6 (id int);
+ALTER TABLE test1 ADD COLUMN value1 text;
+INSERT INTO test1 VALUES (4, 'text', 'text2');
+COMMIT;
+SELECT * FROM test5;
+ id 
+----
+(0 rows)
+
+SELECT * FROM test6;
+ id 
+----
+(0 rows)
+
+SELECT * FROM test1;
+ id | value | value1 
+----+-------+--------
+  5 | text  | 
+  4 | text  | text2
+  2 |       | 
+  3 | text  | 
+(4 rows)
+
+CREATE INDEX ON test1(value);
+SELECT value FROM test1 WHERE value='text';
+ value 
+-------
+ text
+ text
+ text
+(3 rows)
+

src/postgres/src/test/regress/sql/yb.orig.ddl_txn_block.sql
@@ -1,0 +1,79 @@
+CREATE TABLE test1 (id int PRIMARY KEY);
+
+-- Test rollback of DDL+DML transaction block.
+BEGIN ISOLATION LEVEL REPEATABLE READ;
+CREATE TABLE test2 (id int);
+INSERT INTO test1 VALUES (2);
+ROLLBACK;
+
+-- No rows in test1.
+SELECT * FROM test1;
+-- test2 does not exist.
+SELECT * FROM test2;
+
+-- Test commit of DDL+DML transaction block.
+BEGIN ISOLATION LEVEL REPEATABLE READ;
+CREATE TABLE test2 (id int);
+INSERT INTO test1 VALUES (2);
+COMMIT;
+
+SELECT * FROM test1;
+SELECT * FROM test2;
+
+-- Test rollback of a block with multiple DDLs
+BEGIN ISOLATION LEVEL REPEATABLE READ;
+CREATE TABLE test3 (id int);
+CREATE TABLE test4 (id int);
+ALTER TABLE test1 ADD COLUMN value text;
+INSERT INTO test1 VALUES (3, 'text');
+ROLLBACK;
+
+SELECT * FROM test3;
+SELECT * FROM test4;
+-- TODO(#23825): The following select will retry after encountering a version
+-- mismatch error, and it will result in double output for now. This is a known
+-- issue.
+SELECT * FROM test1;
+
+-- Test commit of a block with multiple DDLs
+BEGIN ISOLATION LEVEL REPEATABLE READ;
+CREATE TABLE test3 (id int);
+CREATE TABLE test4 (id int);
+ALTER TABLE test1 ADD COLUMN value text;
+INSERT INTO test1 VALUES (3, 'text');
+COMMIT;
+
+SELECT * FROM test3;
+SELECT * FROM test4;
+SELECT * FROM test1;
+
+-- Same test as above but the first statement is a DML
+BEGIN ISOLATION LEVEL REPEATABLE READ;
+INSERT INTO test1 VALUES (5, 'text');
+CREATE TABLE test5 (id int);
+CREATE TABLE test6 (id int);
+ALTER TABLE test1 ADD COLUMN value1 text;
+INSERT INTO test1 VALUES (4, 'text', 'text2');
+ROLLBACK;
+
+SELECT * FROM test5;
+SELECT * FROM test6;
+-- TODO(#23825): The following select will retry after encountering a version
+-- mismatch error, and it will result in double output for now. This is a known
+-- issue.
+SELECT * FROM test1;
+
+BEGIN ISOLATION LEVEL REPEATABLE READ;
+INSERT INTO test1 VALUES (5, 'text');
+CREATE TABLE test5 (id int);
+CREATE TABLE test6 (id int);
+ALTER TABLE test1 ADD COLUMN value1 text;
+INSERT INTO test1 VALUES (4, 'text', 'text2');
+COMMIT;
+
+SELECT * FROM test5;
+SELECT * FROM test6;
+SELECT * FROM test1;
+
+CREATE INDEX ON test1(value);
+SELECT value FROM test1 WHERE value='text';

commit dc47e34266f927d2b7dad5904b3c301768a2df8b

[#3190] YSQL: Fix build failure caused by D41998
Summary:
Fix the build failure caused by D41998.

https://phorge.dev.yugabyte.com/D40891 removed the function `CommitTransactionScopedSessionState` that I was using and was landed after I triggered Jenkins, but 1 hour before my revision was landed. So I guess it was missed due to that.

Please ignore the code duplication between CommitPlainTransaction and CommitPlainTransactionContainingDDL functions. I have the revision D42485 ready to
refactor that logic. This is a quick fix just for the build.

Test Plan: Jenkins:urgent,compile only

Reviewers: myang, aagrawal, skumar, kramanathan

Reviewed By: aagrawal, kramanathan

Subscribers: kramanathan, yql

Differential Revision: https://phorge.dev.yugabyte.com/D42489

src/yb/yql/pggate/pggate.cc
@@ -1879,7 +1879,14 @@ Status PgApiImpl::CommitPlainTransaction() {
 
 Status PgApiImpl::CommitPlainTransactionContainingDDL(
     PgOid ddl_db_oid, bool ddl_is_silent_modification) {
-  RETURN_NOT_OK(CommitTransactionScopedSessionState());
+  RSTATUS_DCHECK(
+      explicit_row_lock_buffer_.IsEmpty(),
+      IllegalState, "Expected row lock buffer to be empty");
+  RSTATUS_DCHECK(
+      pg_session_->IsInsertOnConflictBufferEmpty(),
+      IllegalState, "Expected INSERT ... ON CONFLICT buffer to be empty");
+  fk_reference_cache_.Clear();
+  RETURN_NOT_OK(pg_session_->FlushBufferedOperations());
   return pg_txn_manager_->CommitPlainTransactionContainingDDL(
       ddl_db_oid, ddl_is_silent_modification);
 }