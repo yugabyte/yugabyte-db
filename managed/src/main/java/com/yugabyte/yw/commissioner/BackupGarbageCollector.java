7ae4c354b8aea6974a3767dcb549c973a864be25
Vipul Bansal <vipul.bansal.yb@gmail.com>
2025-04-04 11:39:22 +0000

    [PLAT-17251]: Fix Backup deletion metrics labels
    Summary: Fixes backup deletion metrics labels in YBA
    
    Test Plan:
    Tested manually that all 5 key/value pairs are correct locally.
    ybp_delete_backup_failure{customer_name="admin", customer_uuid="f33e3c9b-75ab-4c30-80ad-cba85646ea39", instance="host.docker.internal:9000", job="platform", source_name="admin", source_type="customer", source_uuid="f33e3c9b-75ab-4c30-80ad-cba85646ea39"}
    
    Reviewers: amalyshev
    
    Reviewed By: amalyshev
    
    Subscribers: yugaware
    
    Differential Revision: https://phorge.dev.yugabyte.com/D43027

--- managed/src/main/java/com/yugabyte/yw/commissioner/BackupGarbageCollector.java
+++ managed/src/main/java/com/yugabyte/yw/commissioner/BackupGarbageCollector.java
@@ -223,7 +223,7 @@ public class BackupGarbageCollector {
           }
         }
         MetricLabelsBuilder metricLabelsBuilder =
-            MetricLabelsBuilder.create().appendCustomer(customer).appendSource(customer);
+            MetricLabelsBuilder.create().appendSource(customer);
         DELETE_BACKUP_FAILURE
             .labels(metricLabelsBuilder.getPrometheusValues())
             .set(failedToDeleteBackupCount);
