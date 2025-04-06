475261164aebf3102a0ac424eb51f36f2f4dacb1
Bvsk Patnaik <bvsaikiran.patnaik@gmail.com>
2025-04-06 23:09:04 +0000

    [BACKPORT 2024.2][#26454] YSQL: Do not early abort when in subtxn
    Summary:
    Original commit: f6e8d17e005edce48477ca05b232201f04eaa32c / D42641
    ### Issue
    
    DocDB early aborts many txns for performance reasons. Early aborts release locks quicker.
    
    However, lets take a look at the early abort logic closely
    
    ```
    bool CanAbortTransaction(...) {
      const TransactionError txn_err(status);
      if (txn_err.value() == TransactionErrorCode::kSkipLocking) {
        return false;
      }
      if (txn_metadata.isolation == IsolationLevel::READ_COMMITTED &&
          subtransaction_pb && subtransaction_pb->subtransaction_id() > kMinSubTransactionId) {
        return txn_err.value() != TransactionErrorCode::kReadRestartRequired &&
               txn_err.value() != TransactionErrorCode::kConflict;
      }
      return true;
    }
    ```
    
    The txn is aborted in repeatable read even when the user is within a subtxn. Only the subtxn must be aborted, not the top level txn.
    
    ### Example
    
    Setup
    
    ```
    CREATE TABLE tbl(k INT PRIMARY KEY, v INT);
    INSERT INTO tbl SELECT i, i FROM generate_series(1, 10) i;
    ```
    
    Simulate conflict within a subtxn with two session s1, s2.
    
    ```
    s1# BEGIN TRANSACTION ISOLATION LEVEL REPEATABLE READ;
    s1# SELECT * FROM tbl;
    s2# UPDATE tbl SET v = 2 WHERE k = 1;
    s1# SAVEPOINT pt;
    s1# UPDATE tbl SET v = 3 WHERE k = 1;
    s1# ROLLBACK TO pt;
    s1# UPDATE tbl SET v = 3 WHERE k = 2;
    s1# COMMIT;
    ```
    
    txn in s1 must commit because the subtxn was rolled back.
    
    ### Fix
    
    Do not check for READ COMMIITTED isolation level. Additionally, we also do not check for error code.
    
    ### Other Impact
    
    Another semantic change that the change brings in - say the docdb op errored due to a transport layer issue (like timeout, network error etc) and was inside a subtxn. earlier we used to fail the transaction, but now there is an option to rollback to savepoint and move forward despite such errors. but say the docdb op failed due to the participant not recognizing the txn anymore (probably the txn was aborted at the status tablet due to missed heartbeats or fail-on-conflict behavior etc), this change now doesn't abort the txn and set the error (like it did before), but the heartbeat codepath eventually would, and immaterial of rollback to savepoint, the subsequent statements would fail and force the user to rollback.
    
    ### Difference with Pg - deadlocks
    
    pg can move forward with deadlocked transactions involving savepoints, but we cannot since we abort the whole transaction at the status tablet instead of just the last subtxn that introduced the deadlock. We do not release locks taken by the sutxn unlike pg anyway.
    Jira: DB-15816
    
    Test Plan:
    Jenkins
    
    Added the above example as a test in yb.orig.only-abort-sub-txn
    
    ```
    ./yb_build.sh --java-test TestPgRegressIsolation#testPgRegress
    ```
    
    Reviewers: bkolagani, pjain
    
    Reviewed By: bkolagani
    
    Subscribers: yql
    
    Differential Revision: https://phorge.dev.yugabyte.com/D43059

--- src/yb/client/transaction.cc
+++ src/yb/client/transaction.cc
@@ -176,25 +176,12 @@ bool CanAbortTransaction(const Status& status,
                          const boost::optional<SubTransactionMetadataPB>& subtransaction_pb) {
   // We don't abort the transaction in the following scenarios:
   // 1. When we face a kSkipLocking error, so as to make further progress.
-  // 2. When running a transaction with READ COMMITTED isolation where the current subtransaction
-  //    id is not at the default value, and we face a kConflict/kReadRestart error. This is because
-  //    YB PG backend retries kConflict and kReadRestart errors for READ COMMITTED isolation by
-  //    restarting statements instead of the whole transaction if the statment is in a transactional
-  //    block. Else it restarts the whole transaction, in which case we can abort the current one.
-  //
-  //    See 'IsInTransactionBlock' function in src/postgres/src/backend/tcop/postgres.c for details.
+  // 2. When we are inside a sub transaction, so as to only abort the subtxn, not the entire txn.
   const TransactionError txn_err(status);
   if (txn_err.value() == TransactionErrorCode::kSkipLocking) {
     return false;
   }
-  if (txn_metadata.isolation == IsolationLevel::READ_COMMITTED &&
-      subtransaction_pb && subtransaction_pb->subtransaction_id() > kMinSubTransactionId) {
-    return txn_err.value() != TransactionErrorCode::kReadRestartRequired &&
-           txn_err.value() != TransactionErrorCode::kConflict;
-  }
-  // For other situations, we can safely abort the transaction. Even if the error is retriable, it
-  // will be retried by starting a new transaction (done by the query layer).
-  return true;
+  return !subtransaction_pb || subtransaction_pb->subtransaction_id() == kMinSubTransactionId;
 }
 
 YB_DEFINE_ENUM(MetadataState, (kMissing)(kMaybePresent)(kPresent));
