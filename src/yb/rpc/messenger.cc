yugabyte/yugabyte-db

commit 30fce3425c2d4f80847df30314fd0f29db6c956f

[#9936] ybase: Don't parse RequestHeader protobuf
Summary:
Each RPC call contains RequestHeader protobuf, that is parsed into appropriate class.
This protobuf contains 2 strings in remote_method field, where first string (service name) usually quite long,
so memory is allocated for this string.

This diff introduces simplified class for RequestHeader protobuf, so only call_id and timeout_ms are being parsed.
While only bounds are determined for remote_method and slice is used to point to it.

Test Plan: Jenkins

Reviewers: dmitry

Reviewed By: dmitry

Subscribers: ybase, bogdan

Differential Revision: https://phabricator.dev.yugabyte.com/D13470

src/yb/rpc/messenger.cc
@@ -247,6 +247,10 @@ Status Messenger::ListenAddress(
 }
 
 Status Messenger::StartAcceptor() {
+  for (const auto& p : rpc_services_) {
+    p.second->FillEndpoints(&rpc_endpoints_);
+  }
+
   std::lock_guard<percpu_rwlock> guard(lock_);
   if (acceptor_) {
     return acceptor_->Start();
@@ -397,16 +401,13 @@ rpc::ThreadPool& Messenger::ThreadPool(ServicePriority priority) {
 }
 
 // Register a new RpcService to handle inbound requests.
-Status Messenger::RegisterService(const string& service_name,
-                                  const scoped_refptr<RpcService>& service) {
+Status Messenger::RegisterService(
+    const std::string& service_name, const scoped_refptr<RpcService>& service) {
   DCHECK(service);
-  std::lock_guard<percpu_rwlock> guard(lock_);
-  if (InsertIfNotPresent(&rpc_services_, service_name, service)) {
-    UpdateServicesCache(&guard);
-    return Status::OK();
-  } else {
-    return STATUS_SUBSTITUTE(AlreadyPresent, "Service $0 is already present", service_name);
+  if (!rpc_services_.emplace(service_name, service).second) {
+    return STATUS_FORMAT(IllegalState, "Duplicate service: $0", service_name);
   }
+  return Status::OK();
 }
 
 void Messenger::ShutdownThreadPools() {
@@ -418,40 +419,18 @@ void Messenger::ShutdownThreadPools() {
 }
 
 void Messenger::UnregisterAllServices() {
-  decltype(rpc_services_) rpc_services_copy; // Drain rpc services here,
-                                             // to avoid deleting them in locked state.
-  {
-    std::lock_guard<percpu_rwlock> guard(lock_);
-    rpc_services_.swap(rpc_services_copy);
-    UpdateServicesCache(&guard);
-  }
+  CHECK_OK(rpc_services_counter_.DisableAndWaitForOps(CoarseTimePoint::max(), Stop::kTrue));
+  rpc_services_counter_.UnlockExclusiveOpMutex();
 
-  for (const auto& p : rpc_services_copy) {
+  for (const auto& p : rpc_services_) {
     p.second->StartShutdown();
   }
-  for (const auto& p : rpc_services_copy) {
+  for (const auto& p : rpc_services_) {
     p.second->CompleteShutdown();
   }
-  rpc_services_copy.clear();
-}
 
-// Unregister an RpcService.
-Status Messenger::UnregisterService(const string& service_name) {
-  scoped_refptr<RpcService> service;
-  {
-    std::lock_guard<percpu_rwlock> guard(lock_);
-    auto it = rpc_services_.find(service_name);
-    if (it == rpc_services_.end()) {
-      return STATUS(ServiceUnavailable, Substitute("service $0 not registered on $1",
-                   service_name, name_));
-    }
-    service = it->second;
-    rpc_services_.erase(it);
-    UpdateServicesCache(&guard);
-  }
-  service->StartShutdown();
-  service->CompleteShutdown();
-  return Status::OK();
+  rpc_services_.clear();
+  rpc_endpoints_.clear();
 }
 
 class NotifyDisconnectedReactorTask : public ReactorTask {
@@ -489,35 +468,51 @@ void Messenger::QueueOutboundCall(OutboundCallPtr call) {
   reactor->QueueOutboundCall(std::move(call));
 }
 
-void Messenger::QueueInboundCall(InboundCallPtr call) {
-  auto service = rpc_service(call->service_name());
-  if (PREDICT_FALSE(!service)) {
-    Status s = STATUS_FORMAT(ServiceUnavailable,
-                             "Service $0 not registered on $1",
-                             call->service_name(),
-                             name_);
+void Messenger::Handle(InboundCallPtr call, Queue queue) {
+  ScopedRWOperation op(&rpc_services_counter_, CoarseTimePoint::min());
+  if (!op.ok()) {
+    call->RespondFailure(ErrorStatusPB::FATAL_SERVER_SHUTTING_DOWN, MoveStatus(op));
+    return;
+  }
+  auto it = rpc_endpoints_.find(call->serialized_remote_method());
+  if (it == rpc_endpoints_.end()) {
+    auto remote_method = serialization::ParseRemoteMethod(
+        call->serialized_remote_method());
+    Status s;
+    ErrorStatusPB::RpcErrorCodePB error_code = ErrorStatusPB::ERROR_NO_SUCH_SERVICE;
+    if (remote_method.ok()) {
+      if (rpc_services_.count(remote_method->service.ToBuffer())) {
+        error_code = ErrorStatusPB::ERROR_NO_SUCH_METHOD;
+        s = STATUS_FORMAT(
+            InvalidArgument, "Call on service $0 received from $1 with an invalid method name: $2",
+            remote_method->service.ToBuffer(),
+            call->remote_address(),
+            remote_method->method.ToBuffer());
+      } else {
+        s = STATUS_FORMAT(
+            ServiceUnavailable, "Service $0 not registered on $1",
+            remote_method->service.ToBuffer(), name_);
+      }
+    } else {
+      s = remote_method.status();
+    }
     LOG(WARNING) << s;
-    call->RespondFailure(ErrorStatusPB::ERROR_NO_SUCH_SERVICE, s);
+    call->RespondFailure(error_code, s);
     return;
   }
 
   // The RpcService will respond to the client on success or failure.
-  service->QueueInboundCall(std::move(call));
+  call->set_method_index(it->second.second);
+  it->second.first->QueueInboundCall(std::move(call));
 }
 
-void Messenger::Handle(InboundCallPtr call) {
-  auto service = rpc_service(call->service_name());
-  if (PREDICT_FALSE(!service)) {
-    Status s = STATUS_FORMAT(ServiceUnavailable,
-                             "Service $0 not registered on $1",
-                             call->service_name(),
-                             name_);
-    LOG(WARNING) << s;
-    call->RespondFailure(ErrorStatusPB::ERROR_NO_SUCH_SERVICE, s);
-    return;
+RpcServicePtr Messenger::TEST_rpc_service(const std::string& service_name) const {
+  ScopedRWOperation op(&rpc_services_counter_, CoarseTimePoint::min());
+  if (!op.ok()) {
+    return nullptr;
   }
-
-  service->Handle(std::move(call));
+  auto it = rpc_services_.find(service_name);
+  return it != rpc_services_.end() ? it->second : nullptr;
 }
 
 const std::shared_ptr<MemTracker>& Messenger::parent_mem_tracker() {
@@ -554,6 +549,7 @@ Messenger::Messenger(const MessengerBuilder &bld)
       connection_context_factory_(bld.connection_context_factory_),
       stream_factories_(bld.stream_factories_),
       listen_protocol_(bld.listen_protocol_),
+      rpc_services_counter_(name_ + " endpoints"),
       metric_entity_(bld.metric_entity_),
       io_thread_pool_(name_, FLAGS_io_thread_pool_size),
       scheduler_(&io_thread_pool_.io_service()),
@@ -690,22 +686,6 @@ ScheduledTaskId Messenger::ScheduleOnReactor(
   }
 
   return kInvalidTaskId;
-}
-
-void Messenger::UpdateServicesCache(std::lock_guard<percpu_rwlock>* guard) {
-  DCHECK_ONLY_NOTNULL(guard);
-
-  rpc_services_cache_.Set(rpc_services_);
-}
-
-scoped_refptr<RpcService> Messenger::rpc_service(const string& service_name) const {
-  auto cache = rpc_services_cache_.get();
-  auto it = cache->find(service_name);
-  if (it != cache->end()) {
-    // Since our cache is a cache of whole rpc_services_ map, we could check only it.
-    return it->second;
-  }
-  return scoped_refptr<RpcService>();
 }
 
 } // namespace rpc

src/yb/rpc/rpc_service.h
@@ -41,6 +41,8 @@ class RpcService : public RefCountedThreadSafe<RpcService> {
  public:
   virtual ~RpcService() {}
 
+  virtual void FillEndpoints(RpcEndpointMap* map) = 0;
+
   // Enqueue a call for processing.
   // On failure, the RpcService::QueueInboundCall() implementation is
   // responsible for responding to the client with a failure message.

src/yb/rpc/service_pool.cc
@@ -212,11 +212,11 @@ class ServicePoolImpl final : public InboundCallHandler {
 
   void Overflow(const InboundCallPtr& call, const char* type, size_t limit) {
     const auto err_msg =
-        Substitute("$0 request on $1 from $2 dropped due to backpressure. "
+        Format("$0 request on $1 from $2 dropped due to backpressure. "
                    "The $3 queue is full, it has $4 items.",
-            call->method_name(),
+            call->method_name().ToBuffer(),
             service_->service_name(),
-            yb::ToString(call->remote_address()),
+            call->remote_address(),
             type,
             limit);
     YB_LOG_EVERY_N_SECS(WARNING, 3) << LogPrefix() << err_msg;
@@ -242,6 +242,10 @@ class ServicePoolImpl final : public InboundCallHandler {
         << call->remote_address() << " dropped because of: " << status.ToString();
     const auto response_status = STATUS(ServiceUnavailable, "Service is shutting down");
     call->RespondFailure(ErrorStatusPB::FATAL_SERVER_SHUTTING_DOWN, response_status);
+  }
+
+  void FillEndpoints(const RpcServicePtr& service, RpcEndpointMap* map) {
+    service_->FillEndpoints(service, map);
   }
 
   void Handle(InboundCallPtr incoming) override {
@@ -477,6 +481,10 @@ void ServicePool::QueueInboundCall(InboundCallPtr call) {
 
 void ServicePool::Handle(InboundCallPtr call) {
   impl_->Handle(std::move(call));
+}
+
+void ServicePool::FillEndpoints(RpcEndpointMap* map) {
+  impl_->FillEndpoints(RpcServicePtr(this), map);
 }
 
 const Counter* ServicePool::RpcsTimedOutInQueueMetricForTests() const {

src/yb/rpc/service_pool.h
@@ -71,6 +71,7 @@ class ServicePool : public RpcService {
   void StartShutdown() override;
   void CompleteShutdown() override;
 
+  void FillEndpoints(RpcEndpointMap* map) override;
   void QueueInboundCall(InboundCallPtr call) override;
   void Handle(InboundCallPtr call) override;
   const Counter* RpcsTimedOutInQueueMetricForTests() const;

ent/src/yb/integration-tests/cdc_service-int-test.cc
@@ -88,6 +88,11 @@ const client::YBTableName kTableName(YQL_DATABASE_CQL, kCDCTestKeyspace, kCDCTes
 const client::YBTableName kCdcStateTableName(
     YQL_DATABASE_CQL, master::kSystemNamespaceName, master::kCdcStateTableName);
 
+CDCServiceImpl* CDCService(tserver::TabletServer* tserver) {
+  return down_cast<CDCServiceImpl*>(
+      tserver->rpc_server()->TEST_service_pool("yb.cdc.CDCService")->TEST_get_service().get());
+}
+
 class CDCServiceTest : public YBMiniClusterTestBase<MiniCluster>,
                        public testing::WithParamInterface<bool> {
  protected:
@@ -553,8 +558,7 @@ TEST_P(CDCServiceTest, TestMetricsOnDeletedReplication) {
     ASSERT_FALSE(write_resp.has_error());
   }
 
-  auto cdc_service = dynamic_cast<CDCServiceImpl*>(
-      tserver->rpc_server()->service_pool("yb.cdc.CDCService")->TEST_get_service().get());
+  auto cdc_service = CDCService(tserver);
   // Assert that leader lag > 0.
   ASSERT_OK(WaitFor([&]() -> Result<bool> {
     auto metrics = cdc_service->GetCDCTabletMetrics({"" /* UUID */, stream_id, tablet_id});
@@ -630,8 +634,7 @@ TEST_P(CDCServiceTest, TestGetChanges) {
     }
 
     // Verify the CDC Service-level metrics match what we just did.
-    auto cdc_service = dynamic_cast<CDCServiceImpl*>(
-        tserver->rpc_server()->service_pool("yb.cdc.CDCService")->TEST_get_service().get());
+    auto cdc_service = CDCService(tserver);
     auto metrics = cdc_service->GetCDCTabletMetrics({"" /* UUID */, stream_id, tablet_id});
     ASSERT_EQ(metrics->last_read_opid_index->value(), metrics->last_readable_opid_index->value());
     ASSERT_EQ(metrics->last_read_opid_index->value(), change_resp.records_size() + 1 /* checkpt */);
@@ -839,8 +842,7 @@ TEST_P(CDCServiceTestMultipleServersOneTablet, TestMetricsAfterServerFailure) {
   auto leader_proxy = std::make_unique<CDCServiceProxy>(
       &client_->proxy_cache(),
       HostPort::FromBoundEndpoint(leader_mini_tserver->bound_rpc_addr()));
-  auto cdc_service = dynamic_cast<CDCServiceImpl*>(
-    leader_tserver->rpc_server()->service_pool("yb.cdc.CDCService")->TEST_get_service().get());
+  auto cdc_service = CDCService(leader_tserver);
   cdc_service->UpdateLagMetrics();
   auto metrics = cdc_service->GetCDCTabletMetrics({"" /* UUID */, stream_id, tablet_id});
   auto timestamp_after_write = GetCurrentTimeMicros();
@@ -891,10 +893,8 @@ TEST_P(CDCServiceTestMultipleServersOneTablet, TestUpdateLagMetrics) {
   // Use proxy for to most accurately simulate normal requests.
   const auto& proxy = leader_tserver->proxy();
 
-  auto cdc_service = dynamic_cast<CDCServiceImpl*>(
-      leader_tserver->rpc_server()->service_pool("yb.cdc.CDCService")->TEST_get_service().get());
-  auto cdc_service_follower = dynamic_cast<CDCServiceImpl*>(
-      follower_tserver->rpc_server()->service_pool("yb.cdc.CDCService")->TEST_get_service().get());
+  auto cdc_service = CDCService(leader_tserver);
+  auto cdc_service_follower = CDCService(follower_tserver);
 
   // At the start of time, assert both leader and follower at 0 lag.
   ASSERT_OK(WaitFor([&]() -> Result<bool> {
@@ -1153,8 +1153,7 @@ TEST_P(CDCServiceTestMultipleServers, TestGetChangesProxyRouting) {
 
   // Verify the CDC metrics match what we just did.
   const auto& tserver = cluster_->mini_tablet_server(0)->server();
-  auto cdc_service = dynamic_cast<CDCServiceImpl*>(
-      tserver->rpc_server()->service_pool("yb.cdc.CDCService")->TEST_get_service().get());
+  auto cdc_service = CDCService(tserver);
   auto server_metrics = cdc_service->GetCDCServerMetrics();
   ASSERT_EQ(server_metrics->cdc_rpc_proxy_count->value(), remote_tablets.size());
 }

ent/src/yb/integration-tests/cdc_service-txn-test.cc
@@ -330,7 +330,7 @@ TEST_P(CDCServiceTxnTestEnableReplicateIntents, MetricsTest) {
   ASSERT_OK(WaitFor([&]() -> Result<bool> {
     const auto& tserver = cluster_->mini_tablet_server(0)->server();
     auto cdc_service = dynamic_cast<CDCServiceImpl*>(
-        tserver->rpc_server()->service_pool("yb.cdc.CDCService")->TEST_get_service().get());
+        tserver->rpc_server()->TEST_service_pool("yb.cdc.CDCService")->TEST_get_service().get());
     auto metrics = cdc_service->GetCDCTabletMetrics({"" /* UUID */, stream_id, tablet_id});
     auto lag = metrics->async_replication_sent_lag_micros->value();
     YB_LOG_EVERY_N_SECS(INFO, 1) << "Sent lag: " << lag << "us";

ent/src/yb/integration-tests/twodc-test.cc
@@ -816,7 +816,7 @@ TEST_P(TwoDCTest, PollAndObserveIdleDampening) {
 
   // Find the CDCTabletMetric associated with the above pair.
   auto cdc_service = dynamic_cast<cdc::CDCServiceImpl*>(
-    cdc_ts->rpc_server()->service_pool("yb.cdc.CDCService")->TEST_get_service().get());
+    cdc_ts->rpc_server()->TEST_service_pool("yb.cdc.CDCService")->TEST_get_service().get());
   std::shared_ptr<cdc::CDCTabletMetrics> metrics =
       cdc_service->GetCDCTabletMetrics({"", stream_id, tablet_id});
 

src/yb/client/client-test.cc
@@ -234,7 +234,7 @@ class ClientTest: public YBMiniClusterTestBase<MiniCluster> {
       MiniTabletServer* server = cluster_->mini_tablet_server(i);
       if (server->is_started()) {
         ASSERT_EQ(0, server->server()->rpc_server()->
-            service_pool("yb.tserver.TabletServerService")->
+            TEST_service_pool("yb.tserver.TabletServerService")->
             RpcsQueueOverflowMetric()->value());
       }
     }

commit a83072b26fb878f6a27a7c7e8e3b067531c0d368

[#26697] DocDB: Fix direct handling for local call
Summary:
**Problem Summary**

When an RPC call is made, two scenarios can occur:
  - **Remote Call** – The target is a different process (handled via network).
  - **Local Call** – The target is the same process (handled internally).

For **local calls**, the intended behavior is:
  - Normally, the call should execute in a **dedicated service thread pool** (for isolation and fairness).
  - **Optimization:** If the call is **already running in the correct thread pool**, it should execute **directly in the current thread** (avoiding unnecessary queuing).

**Issues fixed by the diff**
  - The logic for determining whether a call can be handled in the current thread is adjusted to use ThreadPool::OwnsThisThread.
  - Restored routing functionality that was broken by commit 30fce3425c2d4f80847df30314fd0f29db6c956f/D13470. So calls could be executed in the same thread when possible.
Jira: DB-16073

Test Plan: Jenkins

Reviewers: dmitry

Reviewed By: dmitry

Subscribers: ybase

Tags: #jenkins-ready

Differential Revision: https://phorge.dev.yugabyte.com/D43020

src/yb/rpc/messenger.cc
@@ -532,7 +532,7 @@ void Messenger::Handle(InboundCallPtr call, Queue queue) {
 
   // The RpcService will respond to the client on success or failure.
   call->set_method_index(it->second.second);
-  it->second.first->QueueInboundCall(std::move(call));
+  it->second.first->Process(std::move(call), queue);
 }
 
 RpcServicePtr Messenger::TEST_rpc_service(const std::string& service_name) const {

src/yb/rpc/rpc_service.h
@@ -42,13 +42,12 @@ class RpcService : public RefCountedThreadSafe<RpcService> {
 
   virtual void FillEndpoints(RpcEndpointMap* map) = 0;
 
-  // Enqueue a call for processing.
-  // On failure, the RpcService::QueueInboundCall() implementation is
-  // responsible for responding to the client with a failure message.
-  virtual void QueueInboundCall(InboundCallPtr call) = 0;
-
-  // Handle a call directly.
-  virtual void Handle(InboundCallPtr call) = 0;
+  // Process a call.
+  // When queue is true, or it is impossible to handle call directly it will be enqueued.
+  // Otherwise handle call directly.
+  // On failure, the method implementation is responsible for responding to the client with a
+  // failure message.
+  virtual void Process(InboundCallPtr call, Queue queue) = 0;
 
   // Initiate RPC service shutdown.
   // Two phase shutdown is required to prevent shutdown deadlock of 2 dependent resources.

src/yb/rpc/service_pool.cc
@@ -179,7 +179,12 @@ class ServicePoolImpl final : public InboundCallHandler {
     }
   }
 
-  void Enqueue(const InboundCallPtr& call) {
+  void Process(InboundCallPtr call, Queue queue) {
+    if (!queue && thread_pool_.OwnsThisThread()) {
+      Handle(std::move(call));
+      return;
+    }
+
     TRACE_TO(call->trace(), "Inserting onto call queue");
     SET_WAIT_STATUS_TO(call->wait_state(), OnCpu_Passive);
 
@@ -191,7 +196,7 @@ class ServicePoolImpl final : public InboundCallHandler {
 
     auto call_deadline = call->GetClientDeadline();
     if (call_deadline != CoarseTimePoint::max()) {
-      pre_check_timeout_queue_.push(call);
+      pre_check_timeout_queue_.push(std::move(call));
       ScheduleCheckTimeout(call_deadline);
     }
 
@@ -481,12 +486,8 @@ void ServicePool::CompleteShutdown() {
   impl_->CompleteShutdown();
 }
 
-void ServicePool::QueueInboundCall(InboundCallPtr call) {
-  impl_->Enqueue(std::move(call));
-}
-
-void ServicePool::Handle(InboundCallPtr call) {
-  impl_->Handle(std::move(call));
+void ServicePool::Process(InboundCallPtr call, Queue queue) {
+  impl_->Process(std::move(call), queue);
 }
 
 void ServicePool::FillEndpoints(RpcEndpointMap* map) {

src/yb/rpc/service_pool.h
@@ -76,8 +76,7 @@ class ServicePool : public RpcService {
   void CompleteShutdown() override;
 
   void FillEndpoints(RpcEndpointMap* map) override;
-  void QueueInboundCall(InboundCallPtr call) override;
-  void Handle(InboundCallPtr call) override;
+  void Process(InboundCallPtr call, Queue queue) override;
   const Counter* RpcsTimedOutInQueueMetricForTests() const;
   const Counter* RpcsQueueOverflowMetric() const;
   std::string service_name() const;