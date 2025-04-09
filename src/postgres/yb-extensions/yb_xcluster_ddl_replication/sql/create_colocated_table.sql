0a852ee8cb4c45d23d54f716dd226cbc0ecf6494
jhe <jhe@yugabyte.com>
2025-04-09 13:31:05 -0700

    [#26762] xClusterDDLRepl: Remove bidirectional role in extension regress tests
    Summary:
    Now that we always write to replicated_ddls on the source as well, we can remove the complex
    bidirectional logic in the test and just use source.
    
    Also adding in an extra validation function to ensure that the replicated_ddls and ddl_queue table
    end up with the exact same primary keys (necessary for proper switchovers).
    Jira: DB-16145
    
    Test Plan:
    ```
    ybd --java-test “org.yb.pgsql.TestPgRegressYbExtensionsYbXclusterDdlReplication”
    ```
    
    Reviewers: mlillibridge, xCluster
    
    Reviewed By: mlillibridge
    
    Subscribers: ybase, yql
    
    Differential Revision: https://phorge.dev.yugabyte.com/D43149

--- src/postgres/yb-extensions/yb_xcluster_ddl_replication/sql/create_colocated_table.sql
+++ src/postgres/yb-extensions/yb_xcluster_ddl_replication/sql/create_colocated_table.sql
@@ -1,12 +1,10 @@
 \c colocation_test
 CALL TEST_reset();
 
--- Verify that temporary objects are allowed.
-SET yb_xcluster_ddl_replication.replication_role = SOURCE;
+-- Verify that temporary objects are allowed but not captured.
 CREATE TEMP TABLE temp_foo(i int PRIMARY KEY);
 
 -- Verify that colocated tables are allowed.
-SET yb_xcluster_ddl_replication.replication_role = BIDIRECTIONAL;
 CREATE TABLE coloc_foo(i int PRIMARY KEY);
 
 SELECT yb_data FROM yb_xcluster_ddl_replication.ddl_queue ORDER BY ddl_end_time;
@@ -15,4 +13,6 @@ SELECT yb_data FROM yb_xcluster_ddl_replication.ddl_queue ORDER BY ddl_end_time;
 CREATE TABLE non_coloc_foo(i int PRIMARY KEY) WITH (COLOCATION = false);
 
 SELECT yb_data FROM yb_xcluster_ddl_replication.ddl_queue ORDER BY ddl_end_time;
-SELECT * FROM yb_xcluster_ddl_replication.replicated_ddls ORDER BY ddl_end_time;
+SELECT yb_data FROM yb_xcluster_ddl_replication.replicated_ddls ORDER BY ddl_end_time;
+
+select * from TEST_verify_replicated_ddls();
