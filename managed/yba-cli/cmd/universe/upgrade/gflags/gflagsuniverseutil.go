ba6ff7207ccd14447305a3af6a7547cfd2e72092
Deepti-yb <dkumar@yugabyte.com>
2025-04-10 05:47:19 +0000

    [PLAT-16188]validate_gflags through CLI
    Summary:
    Add `validate_gflags` API in the set command for gflags upgrade.
    Introduce `dry-run` to have only validations for gflags
    Reuse `--skip-validations` flag in case the user wants to bypass the validate API call and proceed with the upgrade
    
    Test Plan:
    ```
     yba universe upgrade gflags set -n dkumar-cli --specific-gflags-file-path gflags.json
    ? Are you sure you want to upgrade Gflags universe: dkumar-cli Yes
    Successfully validated universe dkumar-cli (23a04417-b702-4e90-8c17-578c8d485973) gflags
    Error validating gflags:
    GFlag test01 does not exist
    GFlag test03 does not exist
    GFlag test02 does not exist
    Please review them and try again. If you still wish to proceed, you can bypass validations by adding the --skip-validations flag.
    
    $ yba universe upgrade gflags set -n dkumar-cli --specific-gflags-file-path gflags.json --skip-validations
    ? Are you sure you want to upgrade Gflags universe: dkumar-cli Yes
    Upgrading universe dkumar-cli (23a04417-b702-4e90-8c17-578c8d485973) gflags
    Waiting for universe dkumar-cli (23a04417-b702-4e90-8c17-578c8d485973) to be upgraded
    ```
    
    ```
    yba universe upgrade gflags set -n dkumar-cli --specific-gflags-file-path gflags.json --dry-run
    Successfully validated universe dkumar-cli (078b26c3-b01c-4000-9776-a8679d0ac8fa) gflags
    Dry run for universe dkumar-cli (078b26c3-b01c-4000-9776-a8679d0ac8fa) gflags is complete
    ```
    
    Reviewers: sneelakantan
    
    Reviewed By: sneelakantan
    
    Differential Revision: https://phorge.dev.yugabyte.com/D43160

---
+++ managed/yba-cli/cmd/universe/upgrade/gflags/gflagsuniverseutil.go
@@ -0,0 +1,112 @@
+/*
+ * Copyright (c) YugaByte, Inc.
+ */
+
+package gflags
+
+import (
+	"fmt"
+	"slices"
+
+	"github.com/sirupsen/logrus"
+	ybaclient "github.com/yugabyte/platform-go-client"
+	"github.com/yugabyte/yugabyte-db/managed/yba-cli/cmd/util"
+	"github.com/yugabyte/yugabyte-db/managed/yba-cli/internal/client"
+	"github.com/yugabyte/yugabyte-db/managed/yba-cli/internal/formatter"
+)
+
+func validateGFlags(
+	authAPI *client.AuthAPIClient,
+	universeName string,
+	universeUUID string,
+	version string,
+	cliSpecificGFlags []util.SpecificGFlagsCLIOutput) []string {
+	validateGFlags := make([]ybaclient.GFlagsValidationRequest, 0)
+
+	perProcessFlagValuesList := make([]util.PerProcessFlags, 0)
+	for _, gflagStructure := range cliSpecificGFlags {
+		gflags := gflagStructure.SpecificGFlags
+		perProcessFlagValuesList = append(perProcessFlagValuesList, *gflags.PerProcessFlags)
+		for _, v := range *gflags.PerAZ {
+			perProcessFlagValuesList = append(perProcessFlagValuesList, v)
+		}
+	}
+
+	for _, perProcessFlagValues := range perProcessFlagValuesList {
+
+		masterValues := perProcessFlagValues[util.MasterServerType]
+		tserverValues := perProcessFlagValues[util.TserverServerType]
+
+		visted := make([]string, 0)
+
+		for k, v := range masterValues {
+			validate := ybaclient.GFlagsValidationRequest{}
+			validate.SetName(k)
+			validate.SetMASTER(v)
+			if tserverValue, ok := tserverValues[k]; ok {
+				validate.SetTSERVER(tserverValue)
+			} else {
+				validate.SetTSERVER("")
+			}
+			visted = append(visted, k)
+			validateGFlags = append(validateGFlags, validate)
+		}
+		for k, v := range tserverValues {
+			if !slices.Contains(visted, k) {
+				validate := ybaclient.GFlagsValidationRequest{}
+				validate.SetName(k)
+				validate.SetMASTER("")
+				validate.SetTSERVER(v)
+				visted = append(visted, k)
+				validateGFlags = append(validateGFlags, validate)
+			}
+		}
+	}
+
+	if len(validateGFlags) == 0 {
+		logrus.Fatalf(
+			formatter.Colorize(
+				"No gflags found to validate. "+
+					"Provide specific-gflags or specific-gflags-file-path\n",
+				formatter.RedColor))
+	}
+
+	req := ybaclient.GFlagsValidationFormData{
+		Gflags: &validateGFlags,
+	}
+
+	rValidate, response, err := authAPI.ValidateGFlags(version).
+		GflagValidationFormData(req).Execute()
+	if err != nil {
+		errMessage := util.ErrorFromHTTPResponse(response, err, "Universe", "Validate GFlags")
+		logrus.Fatalf(formatter.Colorize(errMessage.Error()+"\n", formatter.RedColor))
+	}
+
+	logrus.Info(
+		fmt.Sprintf("Successfully validated universe %s (%s) gflags\n",
+			formatter.Colorize(universeName, formatter.GreenColor),
+			universeUUID,
+		))
+
+	errors := make([]string, 0)
+
+	for _, gflag := range rValidate {
+		name := gflag.GetName()
+		master := gflag.GetMASTER()
+		tserver := gflag.GetTSERVER()
+		if len(master.GetError()) != 0 {
+			err := fmt.Sprintf("GFlag %s master value: %s", name, master.GetError())
+			errors = append(errors, err)
+		}
+		if len(tserver.GetError()) != 0 {
+			err := fmt.Sprintf("GFlag %s tserver value: %s", name, tserver.GetError())
+			errors = append(errors, err)
+		}
+
+		if !master.GetExist() || !tserver.GetExist() {
+			err := fmt.Sprintf("GFlag %s does not exist", name)
+			errors = append(errors, err)
+		}
+	}
+	return errors
+}
