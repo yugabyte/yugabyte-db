e6594b0940ee1db275c6451b2f4f6b68b3cb1a85
Sahith Kurapati <sahith02@gmail.com>
2025-04-14 15:58:53 +0000

    [PLAT-17263] Restore task should check for key name in Ciphertrust KMS config
    Summary: Restore task currently passes with any Ciphertrust KMS config. This should not happen. We should only decrypt the universe key with the correct key ID in the auth config. This check is now added.
    
    Test Plan:
    Manually tested.
    Run UTs.
    Run itests.
    
    Reviewers: vkumar
    
    Reviewed By: vkumar
    
    Differential Revision: https://phorge.dev.yugabyte.com/D43220

--- managed/src/main/java/com/yugabyte/yw/common/kms/util/CiphertrustEARServiceUtil.java
+++ managed/src/main/java/com/yugabyte/yw/common/kms/util/CiphertrustEARServiceUtil.java
@@ -186,6 +186,32 @@ public class CiphertrustEARServiceUtil {
     return encryptResponse;
   }
 
+  public String getKeyId(ObjectNode authConfig) {
+    CiphertrustManagerClient ciphertrustManagerClient = getCiphertrustManagerClient(authConfig);
+    Map<String, Object> keyDetails = ciphertrustManagerClient.getKeyDetails();
+    if (keyDetails == null || keyDetails.isEmpty()) {
+      // Key does not exist
+      log.error("Key does not exist on CipherTrust manager.");
+      return null;
+    }
+    if (keyDetails.containsKey("id")) {
+      String keyId = keyDetails.get("id").toString();
+      if (keyId == null || keyId.isEmpty()) {
+        log.error("Key ID is empty.");
+        return null;
+      } else {
+        log.info(
+            "Key name is: '{}', key ID is '{}'.",
+            authConfig.path(CipherTrustKmsAuthConfigField.KEY_NAME.fieldName).asText(),
+            keyId);
+        return keyId;
+      }
+    } else {
+      log.error("Key ID not found in key details.");
+      return null;
+    }
+  }
+
   public byte[] decryptKeyWithEncryptionContext(
       ObjectNode authConfig, ObjectNode encryptionContext) {
     Map<String, Object> encryptedKeyMaterial =
@@ -195,6 +221,27 @@ public class CiphertrustEARServiceUtil {
 
   public byte[] decryptKey(ObjectNode authConfig, Map<String, Object> encryptedKeyMaterial) {
     CiphertrustManagerClient ciphertrustManagerClient = getCiphertrustManagerClient(authConfig);
+    // Check if the key name and ID correspond to the same key.
+    String keyId = getKeyId(authConfig);
+    if (keyId == null || keyId.isEmpty()) {
+      log.error("Key ID is empty in Ciphertrust KMS.");
+      return null;
+    }
+    String keyName = authConfig.path(CipherTrustKmsAuthConfigField.KEY_NAME.fieldName).asText();
+    if (keyName == null || keyName.isEmpty()) {
+      log.error("Key name is empty in Ciphertrust KMS.");
+      return null;
+    }
+    if (encryptedKeyMaterial.containsKey("id")
+        && !keyId.equals(encryptedKeyMaterial.get("id").toString())) {
+      String errMsg =
+          String.format(
+              "Key ID '%s' from the encryption context does not match the key name '%s' in the"
+                  + " CIPHERTRUST KMS auth config.",
+              keyId, keyName);
+      log.error(errMsg);
+      return null;
+    }
     // Recheck the conversion from bytes -> string once again.
     String decryptResponse = ciphertrustManagerClient.decryptText(encryptedKeyMaterial);
     if (decryptResponse == null || decryptResponse.isEmpty()) {
