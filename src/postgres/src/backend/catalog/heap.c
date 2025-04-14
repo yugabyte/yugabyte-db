948608832155b3c8660508a9cc71ea4dfcb4a7fb
Arpan Agrawal <arpan3495@gmail.com>
2025-04-14 14:03:47 +0530

    [#26819] YSQL: Add index detail on encountering missing rows in yb_index_check()
    Summary:
    Just like other index consistency issues, emit the index name for missing index row case, too. 10de037ad68e8ca068d648de9ad41ac1727d9e29/D41376 missed this.
    
    In the passing, also add a missing code comment.
    Jira: DB-16209
    
    Test Plan:
    Jenkins: test regex: TestPgRegressYbIndexCheck*
    
    ./yb_build.sh --java-test 'org.yb.pgsql.TestPgRegressYbIndexCheck'
    
    Reviewers: stiwary
    
    Reviewed By: stiwary
    
    Subscribers: jason, yql
    
    Differential Revision: https://phorge.dev.yugabyte.com/D43207

--- src/postgres/src/backend/catalog/heap.c
+++ src/postgres/src/backend/catalog/heap.c
@@ -278,9 +278,12 @@ static const FormData_pg_attribute yb_a2 = {
 	.attislocal = true,
 };
 
-
 static const FormData_pg_attribute *YbSysAtt[] = {&yb_a1, &yb_a2};
 
+/*
+ * SystemAttributeDefinition() equivalent for attnum <=
+ * YBFirstLowInvalidAttributeNumber.
+ */
 const FormData_pg_attribute *
 YbSystemAttributeDefinition(AttrNumber attno)
 {
