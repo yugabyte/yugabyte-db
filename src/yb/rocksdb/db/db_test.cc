yugabyte/yugabyte-db

commit ca835058005f671388895cbebc303b7bbb042921

[#22571] DocDB: Use total number of bytes being flushed to understand whether writes should be stalled
Summary:
Currently we block write to RocksDB when there are 2 memtables are flushing.
It works well when RocksDB flushes are determined by the size of memtable.
But there are a lot of scenarios when we initiate RocksDB flush via some external events.
Such as snapshot, intents SST file deletion, schema change and so on.
So it is pretty common case when we could have 2 flushing memtables.

This diff introduces new behaviour to determine whether we should block writes.
The total number of bytes being flushed is used to determine whether writes should be blocked.

Also fixed bug that could appear when there are more than 2 flushing mem tables.
Suppose we have 3 memtables to flush.
Because of filters we could flush 2nd memtable, skipping 1st and 3rd.
Then 1st and 3rd memtables are flushed.
So we will get 2 sst files w/o continuous seqno range.
Fixed by stopping picking memtables when filter was not passed.
Jira: DB-11489

Test Plan: Jenkins

Reviewers: rthallam, arybochkin

Reviewed By: rthallam, arybochkin

Subscribers: yql, yugaware, ybase

Tags: #jenkins-ready

Differential Revision: https://phorge.dev.yugabyte.com/D35204

src/yb/rocksdb/db/column_family.cc
@@ -559,115 +559,123 @@ int GetL0ThresholdSpeedupCompaction(int level0_file_num_compaction_trigger,
 void ColumnFamilyData::RecalculateWriteStallConditions(
       const MutableCFOptions& mutable_cf_options) {
   Version* current_version = current();
-  if (current_version != nullptr) {
-    auto* vstorage = current_version ->storage_info();
-    auto write_controller = column_family_set_->write_controller_;
-    uint64_t compaction_needed_bytes =
-        vstorage->estimated_compaction_needed_bytes();
-
-    if (imm()->NumNotFlushed() >= mutable_cf_options.max_write_buffer_number) {
-      write_controller_token_ = write_controller->GetStopToken();
-      internal_stats_->AddCFStats(InternalStats::MEMTABLE_COMPACTION, 1);
-      RLOG(InfoLogLevel::WARN_LEVEL, ioptions_.info_log,
-          "[%s] Stopping writes because we have %d immutable memtables "
-          "(waiting for flush), max_write_buffer_number is set to %d",
-          name_.c_str(), imm()->NumNotFlushed(),
-          mutable_cf_options.max_write_buffer_number);
-    } else if (vstorage->l0_delay_trigger_count() >=
-               mutable_cf_options.level0_stop_writes_trigger) {
-      write_controller_token_ = write_controller->GetStopToken();
-      internal_stats_->AddCFStats(InternalStats::LEVEL0_NUM_FILES_TOTAL, 1);
-      if (compaction_picker_->IsLevel0CompactionInProgress()) {
-        internal_stats_->AddCFStats(
-            InternalStats::LEVEL0_NUM_FILES_WITH_COMPACTION, 1);
-      }
-      RLOG(InfoLogLevel::WARN_LEVEL, ioptions_.info_log,
-          "[%s] Stopping writes because we have %d level-0 files",
-          name_.c_str(), vstorage->l0_delay_trigger_count());
-    } else if (mutable_cf_options.hard_pending_compaction_bytes_limit > 0 &&
-               compaction_needed_bytes >=
-                   mutable_cf_options.hard_pending_compaction_bytes_limit) {
-      write_controller_token_ = write_controller->GetStopToken();
-      internal_stats_->AddCFStats(
-          InternalStats::HARD_PENDING_COMPACTION_BYTES_LIMIT, 1);
-      RLOG(InfoLogLevel::WARN_LEVEL, ioptions_.info_log,
-          "[%s] Stopping writes because of estimated pending compaction "
-          "bytes %" PRIu64,
-          name_.c_str(), compaction_needed_bytes);
-    } else if (mutable_cf_options.max_write_buffer_number > 3 &&
-               imm()->NumNotFlushed() >=
-                   mutable_cf_options.max_write_buffer_number - 1) {
-      write_controller_token_ =
-          SetupDelay(ioptions_.delayed_write_rate, write_controller,
-                     compaction_needed_bytes, prev_compaction_needed_bytes_,
-                     mutable_cf_options.disable_auto_compactions);
-      internal_stats_->AddCFStats(InternalStats::MEMTABLE_SLOWDOWN, 1);
-      RLOG(InfoLogLevel::WARN_LEVEL, ioptions_.info_log,
-          "[%s] Stalling writes because we have %d immutable memtables "
-          "(waiting for flush), max_write_buffer_number is set to %d "
-          "rate %" PRIu64,
-          name_.c_str(), imm()->NumNotFlushed(),
-          mutable_cf_options.max_write_buffer_number,
-          write_controller->delayed_write_rate());
-    } else if (mutable_cf_options.level0_slowdown_writes_trigger >= 0 &&
-               vstorage->l0_delay_trigger_count() >=
-                   mutable_cf_options.level0_slowdown_writes_trigger) {
-      write_controller_token_ =
-          SetupDelay(ioptions_.delayed_write_rate, write_controller,
-                     compaction_needed_bytes, prev_compaction_needed_bytes_,
-                     mutable_cf_options.disable_auto_compactions);
-      internal_stats_->AddCFStats(InternalStats::LEVEL0_SLOWDOWN_TOTAL, 1);
-      if (compaction_picker_->IsLevel0CompactionInProgress()) {
-        internal_stats_->AddCFStats(
-            InternalStats::LEVEL0_SLOWDOWN_WITH_COMPACTION, 1);
-      }
-      RLOG(InfoLogLevel::WARN_LEVEL, ioptions_.info_log,
-          "[%s] Stalling writes because we have %d level-0 files "
-          "rate %" PRIu64,
-          name_.c_str(), vstorage->l0_delay_trigger_count(),
-          write_controller->delayed_write_rate());
-    } else if (mutable_cf_options.soft_pending_compaction_bytes_limit > 0 &&
-               vstorage->estimated_compaction_needed_bytes() >=
-                   mutable_cf_options.soft_pending_compaction_bytes_limit) {
-      write_controller_token_ =
-          SetupDelay(ioptions_.delayed_write_rate, write_controller,
-                     compaction_needed_bytes, prev_compaction_needed_bytes_,
-                     mutable_cf_options.disable_auto_compactions);
-      internal_stats_->AddCFStats(
-          InternalStats::SOFT_PENDING_COMPACTION_BYTES_LIMIT, 1);
-      RLOG(InfoLogLevel::WARN_LEVEL, ioptions_.info_log,
-          "[%s] Stalling writes because of estimated pending compaction "
-          "bytes %" PRIu64 " rate %" PRIu64,
-          name_.c_str(), vstorage->estimated_compaction_needed_bytes(),
-          write_controller->delayed_write_rate());
-    } else if (vstorage->l0_delay_trigger_count() >=
-               GetL0ThresholdSpeedupCompaction(
-                   mutable_cf_options.level0_file_num_compaction_trigger,
-                   mutable_cf_options.level0_slowdown_writes_trigger)) {
-      write_controller_token_ = write_controller->GetCompactionPressureToken();
-      RLOG(InfoLogLevel::WARN_LEVEL, ioptions_.info_log,
-          "[%s] Increasing compaction threads because we have %d level-0 "
-          "files ",
-          name_.c_str(), vstorage->l0_delay_trigger_count());
-    } else if (vstorage->estimated_compaction_needed_bytes() >=
-               mutable_cf_options.soft_pending_compaction_bytes_limit / 4) {
-      // Increase compaction threads if bytes needed for compaction exceeds
-      // 1/4 of threshold for slowing down.
-      // If soft pending compaction byte limit is not set, always speed up
-      // compaction.
-      write_controller_token_ = write_controller->GetCompactionPressureToken();
-      if (mutable_cf_options.soft_pending_compaction_bytes_limit > 0) {
-        RLOG(InfoLogLevel::WARN_LEVEL, ioptions_.info_log,
-            "[%s] Increasing compaction threads because of estimated pending "
-            "compaction "
-            "bytes %" PRIu64,
-            name_.c_str(), vstorage->estimated_compaction_needed_bytes());
-      }
-    } else {
-      write_controller_token_.reset();
-    }
-    prev_compaction_needed_bytes_ = compaction_needed_bytes;
+  if (current_version == nullptr) {
+    return;
   }
+
+  auto* vstorage = current_version ->storage_info();
+  auto write_controller = column_family_set_->write_controller_;
+  uint64_t compaction_needed_bytes =
+      vstorage->estimated_compaction_needed_bytes();
+
+  if (imm()->TotalDataSize() >= mutable_cf_options.max_flushing_bytes) {
+    write_controller_token_ = write_controller->GetStopToken();
+    RLOG(InfoLogLevel::WARN_LEVEL, ioptions_.info_log,
+        "[%s] Stopping writes because we have %" PRIu64 " bytes to flush, while % " PRIu64
+        "is allowed",
+        name_.c_str(), imm()->TotalDataSize(), mutable_cf_options.max_flushing_bytes);
+  } else if (imm()->NumNotFlushed() >= mutable_cf_options.max_write_buffer_number) {
+    write_controller_token_ = write_controller->GetStopToken();
+    internal_stats_->AddCFStats(InternalStats::MEMTABLE_COMPACTION, 1);
+    RLOG(InfoLogLevel::WARN_LEVEL, ioptions_.info_log,
+        "[%s] Stopping writes because we have %d immutable memtables "
+        "(waiting for flush), max_write_buffer_number is set to %d",
+        name_.c_str(), imm()->NumNotFlushed(),
+        mutable_cf_options.max_write_buffer_number);
+  } else if (vstorage->l0_delay_trigger_count() >=
+             mutable_cf_options.level0_stop_writes_trigger) {
+    write_controller_token_ = write_controller->GetStopToken();
+    internal_stats_->AddCFStats(InternalStats::LEVEL0_NUM_FILES_TOTAL, 1);
+    if (compaction_picker_->IsLevel0CompactionInProgress()) {
+      internal_stats_->AddCFStats(
+          InternalStats::LEVEL0_NUM_FILES_WITH_COMPACTION, 1);
+    }
+    RLOG(InfoLogLevel::WARN_LEVEL, ioptions_.info_log,
+        "[%s] Stopping writes because we have %d level-0 files",
+        name_.c_str(), vstorage->l0_delay_trigger_count());
+  } else if (mutable_cf_options.hard_pending_compaction_bytes_limit > 0 &&
+             compaction_needed_bytes >=
+                 mutable_cf_options.hard_pending_compaction_bytes_limit) {
+    write_controller_token_ = write_controller->GetStopToken();
+    internal_stats_->AddCFStats(
+        InternalStats::HARD_PENDING_COMPACTION_BYTES_LIMIT, 1);
+    RLOG(InfoLogLevel::WARN_LEVEL, ioptions_.info_log,
+        "[%s] Stopping writes because of estimated pending compaction "
+        "bytes %" PRIu64,
+        name_.c_str(), compaction_needed_bytes);
+  } else if (mutable_cf_options.max_write_buffer_number > 3 &&
+             imm()->NumNotFlushed() >=
+                 mutable_cf_options.max_write_buffer_number - 1) {
+    write_controller_token_ =
+        SetupDelay(ioptions_.delayed_write_rate, write_controller,
+                   compaction_needed_bytes, prev_compaction_needed_bytes_,
+                   mutable_cf_options.disable_auto_compactions);
+    internal_stats_->AddCFStats(InternalStats::MEMTABLE_SLOWDOWN, 1);
+    RLOG(InfoLogLevel::WARN_LEVEL, ioptions_.info_log,
+        "[%s] Stalling writes because we have %d immutable memtables "
+        "(waiting for flush), max_write_buffer_number is set to %d "
+        "rate %" PRIu64,
+        name_.c_str(), imm()->NumNotFlushed(),
+        mutable_cf_options.max_write_buffer_number,
+        write_controller->delayed_write_rate());
+  } else if (mutable_cf_options.level0_slowdown_writes_trigger >= 0 &&
+             vstorage->l0_delay_trigger_count() >=
+                 mutable_cf_options.level0_slowdown_writes_trigger) {
+    write_controller_token_ =
+        SetupDelay(ioptions_.delayed_write_rate, write_controller,
+                   compaction_needed_bytes, prev_compaction_needed_bytes_,
+                   mutable_cf_options.disable_auto_compactions);
+    internal_stats_->AddCFStats(InternalStats::LEVEL0_SLOWDOWN_TOTAL, 1);
+    if (compaction_picker_->IsLevel0CompactionInProgress()) {
+      internal_stats_->AddCFStats(
+          InternalStats::LEVEL0_SLOWDOWN_WITH_COMPACTION, 1);
+    }
+    RLOG(InfoLogLevel::WARN_LEVEL, ioptions_.info_log,
+        "[%s] Stalling writes because we have %d level-0 files "
+        "rate %" PRIu64,
+        name_.c_str(), vstorage->l0_delay_trigger_count(),
+        write_controller->delayed_write_rate());
+  } else if (mutable_cf_options.soft_pending_compaction_bytes_limit > 0 &&
+             vstorage->estimated_compaction_needed_bytes() >=
+                 mutable_cf_options.soft_pending_compaction_bytes_limit) {
+    write_controller_token_ =
+        SetupDelay(ioptions_.delayed_write_rate, write_controller,
+                   compaction_needed_bytes, prev_compaction_needed_bytes_,
+                   mutable_cf_options.disable_auto_compactions);
+    internal_stats_->AddCFStats(
+        InternalStats::SOFT_PENDING_COMPACTION_BYTES_LIMIT, 1);
+    RLOG(InfoLogLevel::WARN_LEVEL, ioptions_.info_log,
+        "[%s] Stalling writes because of estimated pending compaction "
+        "bytes %" PRIu64 " rate %" PRIu64,
+        name_.c_str(), vstorage->estimated_compaction_needed_bytes(),
+        write_controller->delayed_write_rate());
+  } else if (vstorage->l0_delay_trigger_count() >=
+             GetL0ThresholdSpeedupCompaction(
+                 mutable_cf_options.level0_file_num_compaction_trigger,
+                 mutable_cf_options.level0_slowdown_writes_trigger)) {
+    write_controller_token_ = write_controller->GetCompactionPressureToken();
+    RLOG(InfoLogLevel::WARN_LEVEL, ioptions_.info_log,
+        "[%s] Increasing compaction threads because we have %d level-0 "
+        "files ",
+        name_.c_str(), vstorage->l0_delay_trigger_count());
+  } else if (vstorage->estimated_compaction_needed_bytes() >=
+             mutable_cf_options.soft_pending_compaction_bytes_limit / 4) {
+    // Increase compaction threads if bytes needed for compaction exceeds
+    // 1/4 of threshold for slowing down.
+    // If soft pending compaction byte limit is not set, always speed up
+    // compaction.
+    write_controller_token_ = write_controller->GetCompactionPressureToken();
+    if (mutable_cf_options.soft_pending_compaction_bytes_limit > 0) {
+      RLOG(InfoLogLevel::WARN_LEVEL, ioptions_.info_log,
+          "[%s] Increasing compaction threads because of estimated pending "
+          "compaction "
+          "bytes %" PRIu64,
+          name_.c_str(), vstorage->estimated_compaction_needed_bytes());
+    }
+  } else {
+    write_controller_token_.reset();
+  }
+  prev_compaction_needed_bytes_ = compaction_needed_bytes;
 }
 
 const EnvOptions* ColumnFamilyData::soptions() const {

src/yb/client/ql-stress-test.cc
@@ -84,6 +84,7 @@ DECLARE_int32(retryable_request_range_time_limit_secs);
 DECLARE_int32(rocksdb_level0_file_num_compaction_trigger);
 DECLARE_int32(rocksdb_level0_slowdown_writes_trigger);
 DECLARE_int32(rocksdb_max_background_compactions);
+DECLARE_int32(rocksdb_max_write_buffer_number);
 DECLARE_int32(rocksdb_universal_compaction_min_merge_width);
 DECLARE_int32(rocksdb_universal_compaction_size_ratio);
 DECLARE_int64(db_write_buffer_size);
@@ -1029,6 +1030,7 @@ class QLStressDynamicCompactionPriorityTest : public QLStressTest {
     ANNOTATE_UNPROTECTED_WRITE(FLAGS_enable_ondisk_compression) = false;
     ANNOTATE_UNPROTECTED_WRITE(FLAGS_rocksdb_max_background_compactions) = 1;
     ANNOTATE_UNPROTECTED_WRITE(FLAGS_rocksdb_compact_flush_rate_limit_bytes_per_sec) = 160_KB;
+    ANNOTATE_UNPROTECTED_WRITE(FLAGS_rocksdb_max_write_buffer_number) = 2;
     QLStressTest::SetUp();
   }
 

src/yb/docdb/consensus_frontier-test.cc
@@ -46,11 +46,10 @@ TEST_F(ConsensusFrontierTest, TestUpdates) {
     ConsensusFrontier frontier;
     EXPECT_TRUE(frontier.Equals(frontier));
     EXPECT_EQ(
-        "Global filter: <invalid>, Cotables filter: []"
         "{ op_id: 0.0 hybrid_time: <invalid> "
         "history_cutoff: { cotables cutoff: <invalid>, primary cutoff: <invalid> } "
-        "max_value_level_ttl_expiration_time: <invalid> "
-        "primary_schema_version: <nullopt> cotable_schema_versions: [] }",
+        "max_value_level_ttl_expiration_time: <invalid> primary_schema_version: <nullopt> "
+        "cotable_schema_versions: [] global_filter: <invalid> cotables_filter: [] }",
         frontier.ToString());
     EXPECT_TRUE(frontier.IsUpdateValid(frontier, UpdateUserValueType::kLargest));
     EXPECT_TRUE(frontier.IsUpdateValid(frontier, UpdateUserValueType::kSmallest));
@@ -62,12 +61,10 @@ TEST_F(ConsensusFrontierTest, TestUpdates) {
   {
     ConsensusFrontier frontier{{1, 1}, 1000_usec_ht, { 500_usec_ht, 500_usec_ht }};
     EXPECT_EQ(
-        "Global filter: <invalid>, Cotables filter: []"
         "{ op_id: 1.1 hybrid_time: { physical: 1000 } "
-        "history_cutoff: { cotables cutoff: { physical: 500 }, "
-        "primary cutoff: { physical: 500 } } "
-        "max_value_level_ttl_expiration_time: <invalid> "
-        "primary_schema_version: <nullopt> cotable_schema_versions: [] }",
+        "history_cutoff: { cotables cutoff: { physical: 500 }, primary cutoff: { physical: 500 } } "
+        "max_value_level_ttl_expiration_time: <invalid> primary_schema_version: <nullopt> "
+        "cotable_schema_versions: [] global_filter: <invalid> cotables_filter: [] }",
         frontier.ToString());
     ConsensusFrontier higher_idx{{1, 2}, 1000_usec_ht, { 500_usec_ht, 500_usec_ht }};
     ConsensusFrontier higher_ht{{1, 1}, 1001_usec_ht, { 500_usec_ht, 500_usec_ht }};
@@ -124,53 +121,45 @@ TEST_F(ConsensusFrontierTest, TestUpdates) {
   pb.mutable_op_id()->set_index(0);
   EXPECT_EQ(
       PbToString(pb),
-      "Global filter: <invalid>, Cotables filter: []"
       "{ op_id: 0.0 hybrid_time: <min> "
-      "history_cutoff: { cotables cutoff: <invalid>, "
-      "primary cutoff: <invalid> } "
-      "max_value_level_ttl_expiration_time: <invalid> "
-      "primary_schema_version: <nullopt> cotable_schema_versions: [] }");
+      "history_cutoff: { cotables cutoff: <invalid>, primary cutoff: <invalid> } "
+      "max_value_level_ttl_expiration_time: <invalid> primary_schema_version: <nullopt> "
+      "cotable_schema_versions: [] global_filter: <invalid> cotables_filter: [] }");
 
   pb.mutable_op_id()->set_term(2);
   pb.mutable_op_id()->set_index(3);
   EXPECT_EQ(
       PbToString(pb),
-      "Global filter: <invalid>, Cotables filter: []"
       "{ op_id: 2.3 hybrid_time: <min> "
-      "history_cutoff: { cotables cutoff: <invalid>, "
-      "primary cutoff: <invalid> } "
-      "max_value_level_ttl_expiration_time: <invalid> "
-      "primary_schema_version: <nullopt> cotable_schema_versions: [] }");
+      "history_cutoff: { cotables cutoff: <invalid>, primary cutoff: <invalid> } "
+      "max_value_level_ttl_expiration_time: <invalid> primary_schema_version: <nullopt> "
+      "cotable_schema_versions: [] global_filter: <invalid> cotables_filter: [] }");
 
   pb.set_hybrid_time(100000);
   EXPECT_EQ(
       PbToString(pb),
-      "Global filter: <invalid>, Cotables filter: []"
       "{ op_id: 2.3 hybrid_time: { physical: 24 logical: 1696 } "
-      "history_cutoff: { cotables cutoff: <invalid>, "
-      "primary cutoff: <invalid> } "
-      "max_value_level_ttl_expiration_time: <invalid> "
-      "primary_schema_version: <nullopt> cotable_schema_versions: [] }");
+      "history_cutoff: { cotables cutoff: <invalid>, primary cutoff: <invalid> } "
+      "max_value_level_ttl_expiration_time: <invalid> primary_schema_version: <nullopt> "
+      "cotable_schema_versions: [] global_filter: <invalid> cotables_filter: [] }");
 
   pb.set_primary_cutoff_ht(200000);
   EXPECT_EQ(
       PbToString(pb),
-      "Global filter: <invalid>, Cotables filter: []"
       "{ op_id: 2.3 hybrid_time: { physical: 24 logical: 1696 } "
       "history_cutoff: { cotables cutoff: <invalid>, "
       "primary cutoff: { physical: 48 logical: 3392 } } "
-      "max_value_level_ttl_expiration_time: <invalid> "
-      "primary_schema_version: <nullopt> cotable_schema_versions: [] }");
+      "max_value_level_ttl_expiration_time: <invalid> primary_schema_version: <nullopt> "
+      "cotable_schema_versions: [] global_filter: <invalid> cotables_filter: [] }");
 
   pb.set_cotables_cutoff_ht(200000);
   EXPECT_EQ(
       PbToString(pb),
-      "Global filter: <invalid>, Cotables filter: []"
       "{ op_id: 2.3 hybrid_time: { physical: 24 logical: 1696 } "
       "history_cutoff: { cotables cutoff: { physical: 48 logical: 3392 }, "
       "primary cutoff: { physical: 48 logical: 3392 } } "
-      "max_value_level_ttl_expiration_time: <invalid> "
-      "primary_schema_version: <nullopt> cotable_schema_versions: [] }");
+      "max_value_level_ttl_expiration_time: <invalid> primary_schema_version: <nullopt> "
+      "cotable_schema_versions: [] global_filter: <invalid> cotables_filter: [] }");
 }
 
 TEST_F(ConsensusFrontierTest, TestUpdateExpirationTime) {

src/yb/docdb/consensus_frontier.cc
@@ -157,17 +157,23 @@ void ConsensusFrontier::FromOpIdPBDeprecated(const OpIdPB& pb) {
   op_id_ = OpId::FromPB(pb);
 }
 
-std::string ConsensusFrontier::ToString() const {
-  auto global_ht = GlobalFilter();
-  std::string filter = Format("Global filter: $0, ", global_ht);
-  filter += "Cotables filter: [";
-  IterateCotablesFilter(hybrid_time_filter_.AsSlice(), [&filter](uint32_t db_oid, uint64_t ht) {
-    filter += Format("$0:$1, ", db_oid, HybridTime(ht));
+std::string RenderCotablesFilter(Slice input) {
+  std::string result = "[";
+  IterateCotablesFilter(input, [&result](uint32_t db_oid, uint64_t ht) {
+    if (result.size() > 1) {
+      result += ", ";
+    }
+    result += Format("$0:$1", db_oid, HybridTime(ht));
   });
-  filter += "]";
-  return filter + YB_CLASS_TO_STRING(
+  result += "]";
+  return result;
+}
+
+std::string ConsensusFrontier::ToString() const {
+  return YB_CLASS_TO_STRING(
       op_id, hybrid_time, history_cutoff, max_value_level_ttl_expiration_time,
-      primary_schema_version, cotable_schema_versions);
+      primary_schema_version, cotable_schema_versions, (global_filter, AsString(GlobalFilter())),
+      (cotables_filter, RenderCotablesFilter(hybrid_time_filter_.AsSlice())));
 }
 
 void ConsensusFrontier::SetCoTablesFilter(

src/yb/docdb/docdb_rocksdb_util.cc
@@ -107,7 +107,10 @@ DEFINE_UNKNOWN_uint64(rocksdb_compaction_size_threshold_bytes, 2ULL * 1024 * 102
              "Threshold beyond which compaction is considered large.");
 DEFINE_UNKNOWN_uint64(rocksdb_max_file_size_for_compaction, 0,
              "Maximal allowed file size to participate in RocksDB compaction. 0 - unlimited.");
-DEFINE_UNKNOWN_int32(rocksdb_max_write_buffer_number, 2,
+
+// Use big enough default value for rocksdb_max_write_buffer_number, so behavior defined by
+// db_max_flushing_bytes will be actual default.
+DEFINE_NON_RUNTIME_int32(rocksdb_max_write_buffer_number, 100500,
              "Maximum number of write buffers that are built up in memory.");
 DECLARE_int64(db_block_size_bytes);
 
@@ -125,6 +128,11 @@ DEFINE_UNKNOWN_int64(db_write_buffer_size, -1,
 
 DEFINE_UNKNOWN_int32(memstore_size_mb, 128,
              "Max size (in mb) of the memstore, before needing to flush.");
+
+// Use a value slightly less than 2 default mem store sizes.
+DEFINE_NON_RUNTIME_uint64(db_max_flushing_bytes, 250_MB,
+    "The limit for the number of bytes in immutable mem tables. "
+    "After reaching this limit new writes are blocked. 0 - unlimited.");
 
 DEFINE_UNKNOWN_bool(use_docdb_aware_bloom_filter, true,
             "Whether to use the DocDbAwareFilterPolicy for both bloom storage and seeks.");
@@ -727,6 +735,9 @@ void InitRocksDBOptions(
   }
 
   options->max_write_buffer_number = FLAGS_rocksdb_max_write_buffer_number;
+  if (FLAGS_db_max_flushing_bytes != 0) {
+    options->max_flushing_bytes = FLAGS_db_max_flushing_bytes;
+  }
 
   options->memtable_factory = std::make_shared<rocksdb::SkipListFactory>(
       0 /* lookahead */, rocksdb::ConcurrentWrites::kFalse);

commit e648dd2ab4dba48924413d63e79ec54ba0811ad3

[#26916] docdb: Fixed db_max_flushing_bytes flag handling
Summary:
Under intensive write workload, flushes to SST file could be slower and we can accumulate multiple immutable memtables to be flushed into single SST file. Flag `db_max_flushing_bytes` (250MB by default) is expected to control how much memory could be used by immutable memtables per RocksDB instance before we block incoming writes. But due to bug this flag wasn't respected and under intensive write workload, immutable memtables could use much more memory than `db_max_flushing_bytes` per RocksDB instance (up to 3-4GBs per tablet during internal testing).

Note that this bug doesn't affect auto-flush logic controlled by `global_memstore_size_percentage`, `global_memstore_size_mb_max`, `memstore_size_mb`, `db_write_buffer_size`, it only impacted limit on memory occupied by immutable memtables when flushes are slow.

Bug was introduced with the feature in commit ca835058005f671388895cbebc303b7bbb042921.

This change fixes the bug and adds DBTest.MaxFlushingBytes unit-test that covers that logic.
Jira: DB-16347

Test Plan: DBTest.MaxFlushingBytes

Reviewers: arybochkin, sergei

Reviewed By: arybochkin

Subscribers: rthallam, ybase

Tags: #jenkins-ready

Differential Revision: https://phorge.dev.yugabyte.com/D43435

src/yb/rocksdb/db/column_family.cc
@@ -605,7 +605,7 @@ void ColumnFamilyData::RecalculateWriteStallConditions(
     write_controller_token_ = write_controller->GetStopToken();
     RLOG(InfoLogLevel::WARN_LEVEL, ioptions_.info_log,
         "[%s] Stopping writes because we have %" PRIu64 " bytes to flush, while % " PRIu64
-        "is allowed",
+        " is allowed",
         name_.c_str(), imm()->TotalDataSize(), mutable_cf_options.max_flushing_bytes);
   } else if (imm()->NumNotFlushed() >= mutable_cf_options.max_write_buffer_number) {
     write_controller_token_ = write_controller->GetStopToken();

src/yb/rocksdb/util/options.cc
@@ -159,6 +159,7 @@ ColumnFamilyOptions::ColumnFamilyOptions(const Options& options)
       compaction_filter_factory(options.compaction_filter_factory),
       write_buffer_size(options.write_buffer_size),
       max_write_buffer_number(options.max_write_buffer_number),
+      max_flushing_bytes(options.max_flushing_bytes),
       min_write_buffer_number_to_merge(
           options.min_write_buffer_number_to_merge),
       max_write_buffer_number_to_maintain(

src/yb/rocksdb/util/testutil.h
@@ -364,6 +364,10 @@ class SleepingBackgroundTask {
         done_with_sleep_(false),
         sleeping_(false) {}
 
+  ~SleepingBackgroundTask() {
+    WakeUp();
+  }
+
   bool IsSleeping() {
     MutexLock l(&mutex_);
     return sleeping_;

src/yb/rocksdb/db/db_test.cc
@@ -7167,6 +7167,85 @@ TEST_F(DBTest, LastWriteBufferDelay) {
   sleeping_task.WaitUntilDone();
 }
 
+TEST_F(DBTest, MaxFlushingBytes) {
+  constexpr auto kSingleMemtableLimit = 100_KB;
+  constexpr auto kNumEntriesPerMemtableLimit = 100;
+  constexpr auto kMaxFlushingMemTableLimits = 10;
+
+  Options options;
+  options.env = env_;
+  options = CurrentOptions(options);
+  options.db_write_buffer_size = kSingleMemtableLimit;
+  options.max_flushing_bytes =
+      kSingleMemtableLimit * kMaxFlushingMemTableLimits - kSingleMemtableLimit / 2;
+  options.max_write_buffer_number = 9999;
+  options.arena_block_size = 4 * 1024;
+  options.level0_slowdown_writes_trigger = 999999;
+  options.level0_stop_writes_trigger = 999999;
+  options.disable_auto_compactions = true;
+  options.info_log_level = InfoLogLevel::INFO_LEVEL;
+  options.info_log = std::make_shared<yb::YBRocksDBLogger>(options.log_prefix);
+
+  for (bool with_cf : {false, true}) {
+    for (int iter = 0; iter < 2; ++iter) {
+      // Block flushes.
+      test::SleepingBackgroundTask sleeping_task;
+      env_->SetBackgroundThreads(1, Env::Priority::HIGH);
+      env_->Schedule(
+          &test::SleepingBackgroundTask::DoSleepTask, &sleeping_task, Env::Priority::HIGH);
+      sleeping_task.WaitUntilSleeping();
+
+      if (iter == 0) {
+        Destroy(options);
+      }
+      if (with_cf) {
+        ReopenWithColumnFamilies({"default"}, options);
+      } else {
+        Reopen(options);
+      }
+
+      LOG(INFO) << "with_cf: " << with_cf << " iter: " << iter;
+
+      const auto num_sst_files_before = db_->GetLiveFilesMetaData().size();
+
+      std::atomic<int> callback_count(0);
+      auto& sync_point = *yb::SyncPoint::GetInstance();
+      sync_point.SetCallBack("DBImpl::DelayWrite:Wait", [&](void* arg) {
+        callback_count.fetch_add(1);
+        sleeping_task.WakeUp();
+      });
+      sync_point.EnableProcessing();
+
+      const auto value_size = static_cast<int>(kSingleMemtableLimit / kNumEntriesPerMemtableLimit);
+
+      Random rnd(301);
+      int key_idx = 0;
+      for (int i = 0; i < kNumEntriesPerMemtableLimit * (kMaxFlushingMemTableLimits - 1);
+           ++i, ++key_idx) {
+        ASSERT_OK(Put(Key(key_idx), RandomString(&rnd, value_size)));
+      }
+      auto& write_controller = dbfull()->TEST_write_controler();
+      ASSERT_FALSE(write_controller.NeedsDelay());
+      ASSERT_FALSE(write_controller.IsStopped());
+      ASSERT_EQ(0, callback_count.load());
+
+      // This should trigger stopping writes.
+      for (int i = 0; i < kNumEntriesPerMemtableLimit; ++i, ++key_idx) {
+        ASSERT_OK(Put(Key(key_idx), RandomString(&rnd, value_size)));
+      }
+      ASSERT_GE(callback_count.load(), 1);
+
+      sync_point.DisableProcessing();
+      sleeping_task.WaitUntilDone();
+
+      auto sst_files_meta = db_->GetLiveFilesMetaData();
+      ASSERT_EQ(sst_files_meta.size(), num_sst_files_before + 1);
+      ASSERT_LE(
+          sst_files_meta.back().total_size, kSingleMemtableLimit * kMaxFlushingMemTableLimits);
+    }
+  }
+}
+
 TEST_F(DBTest, FailWhenCompressionNotSupportedTest) {
   CompressionType compressions[] = {kZlibCompression, kBZip2Compression,
                                     kLZ4Compression,  kLZ4HCCompression};