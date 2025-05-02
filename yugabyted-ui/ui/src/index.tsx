689f62901be597c908e67fcec7a0d1ed4cbb828b
Krishna Tripathi <krishna.tripathi@yugabyte.com>
2025-05-02 13:47:31 +0000

    [BACKPORT 2025.1][DB-16187][DB-16186][DB-15992][DB-16307]yugabyted: Addition of notes, minimum Versions Fixed in and changing of source of data for assessment ui page.
    Summary:
    - Addition of notes to assessment UI page.
    - Addition of Minimum Fixed In Versions to analyze Schema page
    - Previously, the assessment data was fetched directly the metadata payload from database (Using invalid and total count field), but now the assessment metrics will be calculated from the assessment issues present.
    - Removal of the using of all deprecated fields from voyager payload from yugabyted's apiserver.
    Jira: DB-16187, DB-16186, DB-15992, DB-16307
    
    Original commit: 282b777aa7f003b2a0ec2c19ba7ad5dd973f831a / D43237
    
    Test Plan: manual testing
    
    Reviewers: djiang, nikhil
    
    Reviewed By: djiang
    
    Subscribers: yugabyted-dev, krishna.tripathi
    
    Differential Revision: https://phorge.dev.yugabyte.com/D43558

--- yugabyted-ui/ui/src/index.tsx
+++ yugabyted-ui/ui/src/index.tsx
@@ -4,7 +4,7 @@ import { App } from '@app/App';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
-// Snowpack's Hot Module Replacement, https://snowpack.dev/concepts/hot-module-replacement
+//  Snowpack's Hot Module Replacement, https://snowpack.dev/concepts/hot-module-replacement
 if (import.meta.hot) {
   import.meta.hot.accept();
 }
