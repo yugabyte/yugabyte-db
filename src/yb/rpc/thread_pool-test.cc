cce01d6e53550403a9ddb9b5f0ade2b0fbbaa861
Sergei Politov <spolitov@gmail.com>
2025-05-05 22:44:09 +0300

    [#27032] DocDB: Avoid holding lock while starting new thread in thread pool
    Summary:
    RPC Thread Pool currently holds the lock while starting a new worker.
    However, we could release the lock right after adding the worker to the list and start the thread afterward.
    
    Checking with newly added test in release mode on n2-standard-64.
    Average execution time on master: 8.5ms
    This diff: 3ms
    Jira: DB-16506
    
    Test Plan: ThreadPoolTest.MassEnqueue
    
    Reviewers: hsunder
    
    Reviewed By: hsunder
    
    Subscribers: ybase
    
    Tags: #jenkins-ready
    
    Differential Revision: https://phorge.dev.yugabyte.com/D43665

--- src/yb/rpc/thread_pool-test.cc
+++ src/yb/rpc/thread_pool-test.cc
@@ -296,6 +296,54 @@ TEST_F(ThreadPoolTest, TestOwns) {
   ASSERT_TRUE(pool.Owns(task.thread()));
 }
 
+TEST_F(ThreadPoolTest, MassEnqueue) {
+  constexpr size_t kTotalThreads = 64;
+  TestThreadHolder threads;
+  CountDownLatch wait_threads_latch(kTotalThreads);
+  CountDownLatch enqueue_tasks_latch(1);
+  CountDownLatch task_run_latch{kTotalThreads};
+  ThreadPool pool(ThreadPoolOptions {
+    .name = "test",
+    .max_workers = kTotalThreads,
+  });
+
+  class TestTask : public ThreadPoolTask {
+   public:
+    explicit TestTask(CountDownLatch& latch) : latch_(latch) {
+    }
+
+    void Run() override {
+      latch_.CountDown();
+    }
+
+    void Done(const Status& status) override {
+    }
+
+    void Wait() {
+      latch_.Wait();
+    }
+   private:
+    CountDownLatch& latch_;
+  };
+
+  for (size_t i = 0; i != kTotalThreads; ++i) {
+    threads.AddThreadFunctor([&wait_threads_latch, &enqueue_tasks_latch, &pool, &task_run_latch] {
+      TestTask task(task_run_latch);
+      wait_threads_latch.CountDown();
+      enqueue_tasks_latch.Wait();
+      pool.Enqueue(&task);
+      task_run_latch.Wait();
+    });
+  }
+
+  wait_threads_latch.Wait();
+  auto start = MonoTime::Now();;
+  enqueue_tasks_latch.CountDown();
+  task_run_latch.Wait();
+  auto stop = MonoTime::Now();
+  LOG(INFO) << "Passed: " << (stop - start).ToPrettyString();
+}
+
 namespace strand {
 
 constexpr size_t kPoolMaxTasks = 100;
