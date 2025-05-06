705c8afad12bdf4794b2247cfff1e6274fd47e85
Sami Ahmed Siddiqui <sami.siddiqui@yasglobal.com>
2025-05-06 18:06:27 +0500

    Copy to clipboard link on clicking heading icon

--- docs/src/index.js
+++ docs/src/index.js
@@ -106,7 +106,7 @@ function yugabyteScrollLeftNav(activeLink) {
  * based on the container width.
  */
 function yugabytePageFinderWidth() {
-  yugabytePageFinderList.forEach(({width, parent}) => {
+  yugabytePageFinderList.forEach(({ width, parent }) => {
     if (parent) {
       const innerContainer = document.querySelector('.content-area');
       if (width > innerContainer.offsetWidth) {
@@ -323,6 +323,29 @@ $(document).ready(() => {
         $('.side-nav-collapse-toggle-2').click();
       }
     });
+  })();
+
+  /**
+   * Copy heading link into clipboard.
+   */
+  (() => {
+    const headingLinks = document.querySelectorAll('.td-heading-self-link');
+    if (headingLinks) {
+      headingLinks.forEach((link) => {
+        link.addEventListener('click', (event) => {
+          const url = window.location.origin + window.location.pathname + event.target.getAttribute('href');
+          if (navigator.clipboard) {
+            navigator.clipboard.writeText(url).then(() => {
+              link.classList.add('copied');
+
+              setTimeout(() => {
+                link.classList.remove('copied');
+              }, 1500);
+            });
+          }
+        })
+      });
+    }
   })();
 
   /**
