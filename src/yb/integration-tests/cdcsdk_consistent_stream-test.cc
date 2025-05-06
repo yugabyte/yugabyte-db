c6669b1ecddfb52ade07c025e121cb302c7965cf
Sumukh-Phalgaonkar <sumukhphalgaonkar@gmail.com>
2025-05-06 14:00:14 +0530

    [#27070] CDC: Fix flaky test CDCSDKConsistentStreamTest.TestGetChangesDuringLoadTxn
    Summary:
    This revision fixes the flakiness of the test
    `CDCSDKConsistentStreamTest.TestGetChangesDuringLoadTxn`. The flakiness was due to last GetChanges
    being called before the running transaction was getting committed.
    Jira: DB-16558
    
    Test Plan: Jenkins: test regex: .*TestGetChangesDuringLoadTxn.*
    
    Reviewers: skumar, vkushwaha
    
    Reviewed By: skumar
    
    Subscribers: ycdcxcluster
    
    Tags: #jenkins-ready
    
    Differential Revision: https://phorge.dev.yugabyte.com/D43745

--- src/yb/integration-tests/cdcsdk_consistent_stream-test.cc
+++ src/yb/integration-tests/cdcsdk_consistent_stream-test.cc
@@ -1232,6 +1232,9 @@ TEST_F(CDCSDKConsistentStreamTest, TestGetChangesDuringLoadTxn) {
   ANNOTATE_UNPROTECTED_WRITE(FLAGS_yb_enable_cdc_consistent_snapshot_streams) = true;
 
   auto tablet = ASSERT_RESULT(SetUpWithOneTablet(1, 1, false));
+  auto tablet_peer =
+      ASSERT_RESULT(GetLeaderPeerForTablet(test_cluster(), tablet.begin()->tablet_id()));
+
   auto conn = ASSERT_RESULT(test_cluster_.ConnectToDB(kNamespaceName));
 
   auto stream_id = ASSERT_RESULT(CreateConsistentSnapshotStream());
@@ -1259,6 +1262,13 @@ TEST_F(CDCSDKConsistentStreamTest, TestGetChangesDuringLoadTxn) {
   ASSERT_EQ(get_change_resp.cdc_sdk_proto_records_size(), 5);
 
   ASSERT_OK(conn.Execute("COMMIT"));
+
+  ASSERT_OK(WaitFor(
+      [&]() -> Result<bool> {
+        auto tablet_safe = VERIFY_RESULT(tablet_peer->shared_tablet_safe());
+        return tablet_safe->GetMinStartHTRunningTxnsForCDCProducer() == HybridTime::kMax;
+      },
+      MonoDelta::FromSeconds(60), "Timed out waiting for the commit of running txn."));
 
   // Now that there are no running transactions we can ship all the records from all the
   // transactions.
