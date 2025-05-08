yugabyte/yugabyte-db

commit 50fb5ef10af000a41bb4e1c4d32ae6730c04374c

[#23785] YSQL: Incrementally refresh PG backend catcaches (part 2)
Summary:
This diff starts to use the two new function added in part 1:
* yb_increment_db_catalog_version_with_inval_messages
* yb_increment_all_db_catalog_versions_with_inval_messages

A new gflag `--TEST_yb_enable_invalidation_messages` is introduced to enable the
new code paths that perform incremental catalog cache invalidation. When a DDL
statement is about to increment the catalog version, it also calls
`YbGetSubGroupInvalMessages` which is a adapted from the PG function
`xactGetCommittedInvalidationMessages` to return the array of invalidation
messages that are about to be sent out if the DDL transaction is committed.
Note that we do this right before the DDL transaction is about to commit so the
array of invalidation messages are no different from those that would get
actually sent out if the DDL transaction does commit successfully.

The array of invalidation messages are simply sent as binary byte array and is
inserted into `pg_yb_invalidation_messages` table. A new gflag
`--TEST_yb_invalidation_message_expiration_secs` is used to control the expiration
time of the array of messages. A next invocation of one of these two new
functions is going to delete those old messages that are older than
`--TEST_yb_invalidation_message_expiration_secs`.

A third gflag `--TEST_yb_max_num_invalidation_messages` (default value is 4096)
is used to turn off incremental catalog cache invalidation if the DDL statement
generates too many invalidation messages. We do not want to insert unbounded
number of invalidation messages into `pg_yb_invalidation_messages` as the entire
table would be sent as part of tserver/master heartbeat responses that cost
network bandwidth and also may subject to certain RPC limits.

The three new gflags are added as test gflags, the plan is to change them to
normal gflags once the new feature stabilizes.

To avoid updating too many unit test logs, I added

src/postgres/src/backend/optimizer/plan/createplan.c
@@ -53,6 +53,7 @@
 
 /* YB includes */
 #include "access/yb_scan.h"
+#include "catalog/pg_yb_catalog_version.h"
 #include "optimizer/ybplan.h"
 #include "pg_yb_utils.h"
 
@@ -3529,7 +3530,8 @@ yb_single_row_update_or_delete_path(PlannerInfo *root,
 			 * if there are triggers. There is no easy way to tell what columns
 			 * are affected by a trigger, so we should update all indexes.
 			 */
-			if (TupleDescAttr(tupDesc, resno - 1)->attnotnull ||
+			if ((TupleDescAttr(tupDesc, resno - 1)->attnotnull &&
+				 relation->rd_id != YBCatalogVersionRelationId) ||
 				YBIsCollationValidNonC(ybc_get_attcollation(tupDesc, resno)) ||
 				!YbCanPushdownExpr(tle->expr, &colrefs))
 			{

src/yb/yql/pgwrapper/pg_catalog_version-test.cc
@@ -104,6 +104,20 @@ class PgCatalogVersionTest : public LibPqTestBase {
     RestartClusterSetDBCatalogVersionMode(true, extra_tserver_flags);
   }
 
+  void RestartClusterWithInvalMessageEnabled() {
+    LOG(INFO) << "Restart the cluster with --TEST_yb_enable_invalidation_messages=true";
+    cluster_->Shutdown();
+    for (size_t i = 0; i != cluster_->num_masters(); ++i) {
+      cluster_->master(i)->mutable_flags()->push_back(
+          "--TEST_yb_enable_invalidation_messages=true");
+    }
+    for (size_t i = 0; i != cluster_->num_tablet_servers(); ++i) {
+      cluster_->tablet_server(i)->mutable_flags()->push_back(
+          "--TEST_yb_enable_invalidation_messages=true");
+    }
+    ASSERT_OK(cluster_->Restart());
+  }
+
   // Return a MasterCatalogVersionMap by making a query of the pg_yb_catalog_version table.
   static Result<MasterCatalogVersionMap> GetMasterCatalogVersionMap(PGConn* conn) {
     const auto rows = VERIFY_RESULT((
@@ -1913,6 +1927,91 @@ TEST_F(PgCatalogVersionTest, CreateOrReplaceView) {
 
   result = ASSERT_RESULT(conn2.FetchAllAsString(query));
   ASSERT_EQ(result, expected_result2);
+}
+
+// This test does sanity check that invalidation messages are portable
+// across nodes and they are stable.
+TEST_F(PgCatalogVersionTest, InvalMessageSanityTest) {
+  RestartClusterWithInvalMessageEnabled();
+  auto ts_index = RandomUniformInt(0UL, cluster_->num_tablet_servers() - 1);
+  pg_ts = cluster_->tablet_server(ts_index);
+  LOG(INFO) << "ts_index: " << ts_index;
+  auto conn = ASSERT_RESULT(Connect());
+  ASSERT_OK(conn.Execute("SET yb_test_inval_message_portability = true"));
+  auto choice = RandomUniformInt(0, 1);
+  LOG(INFO) << "choice: " << choice;
+  // We connect to a randomly selected node, and create two types of tables.
+  if (choice) {
+    ASSERT_OK(conn.Execute("CREATE TABLE foo(a INT, b INT)"));
+  } else {
+    ASSERT_OK(conn.Execute("CREATE TABLE foo(id TEXT)"));
+  }
+  ASSERT_OK(conn.Execute("DROP TABLE foo"));
+  auto query = "SELECT current_version, encode(messages, 'hex') "
+               "FROM pg_yb_invalidation_messages"s;
+  auto expected_result0 =
+      "2, 5000000000000000cb34000040c1eb0a00000000000000004f00000000000000cb340"
+      "0005ac4b85300000000000000005000000000000000cb34000047a2537b0000000000000"
+      "0004f00000000000000cb34000021e2d2ca00000000000000000700000000000000cb340"
+      "000849f9c1300000000000000000600000000000000cb34000002517d240000000000000"
+      "0000700000000000000cb340000d519492c00000000000000000600000000000000cb340"
+      "000532bd64f00000000000000000700000000000000cb3400000ce84cf30000000000000"
+      "0000600000000000000cb340000f1f7a7e800000000000000000700000000000000cb340"
+      "000ecbba96500000000000000000600000000000000cb34000084a01e300000000000000"
+      "0000700000000000000cb3400003f53cbc600000000000000000600000000000000cb340"
+      "0001310debc00000000000000000700000000000000cb340000c76e67a20000000000000"
+      "0000600000000000000cb340000f3cf9e8c00000000000000000700000000000000cb340"
+      "00017e0201d00000000000000000600000000000000cb3400004ba32d1e0000000000000"
+      "0003700000000000000cb3400004657085300000000000000003600000000000000cb340"
+      "00021e2d2ca0000000000000000fb00000000000000cb340000300a00000000000000000"
+      "000fb00000000000000cb340000300a00000000000000000000fe00000000000000cb340"
+      "000004000000000000000000000fb00000000000000cb340000300a00000000000000000"
+      "000";
+  auto expected_result1 =
+      "2, 5000000000000000cb34000040c1eb0a00000000000000004f00000000000000cb340"
+      "0005ac4b85300000000000000005000000000000000cb34000047a2537b0000000000000"
+      "0004f00000000000000cb34000021e2d2ca00000000000000000700000000000000cb340"
+      "000849f9c1300000000000000000600000000000000cb34000002517d240000000000000"
+      "0000700000000000000cb340000d519492c00000000000000000600000000000000cb340"
+      "000532bd64f00000000000000000700000000000000cb3400000ce84cf30000000000000"
+      "0000600000000000000cb340000f1f7a7e800000000000000000700000000000000cb340"
+      "000ecbba96500000000000000000600000000000000cb34000084a01e300000000000000"
+      "0000700000000000000cb3400003f53cbc600000000000000000600000000000000cb340"
+      "0001310debc00000000000000000700000000000000cb340000c76e67a20000000000000"
+      "0000600000000000000cb340000f3cf9e8c00000000000000000700000000000000cb340"
+      "00017e0201d00000000000000000600000000000000cb34000006e678400000000000000"
+      "0000700000000000000cb3400007651cba700000000000000000600000000000000cb340"
+      "000bdf7d7b600000000000000003700000000000000cb340000465708530000000000000"
+      "0003600000000000000cb34000021e2d2ca0000000000000000fb00000000000000cb340"
+      "000300a00000000000000000000fb00000000000000cb340000300a00000000000000000"
+      "000fe00000000000000cb340000004000000000000000000000fb00000000000000cb340"
+      "000300a00000000000000000000";
+  auto result = ASSERT_RESULT(conn.FetchAllAsString(query));
+  if (choice) {
+    ASSERT_EQ(result, expected_result1);
+  } else {
+    ASSERT_EQ(result, expected_result0);
+  }
+}
+
+TEST_F(PgCatalogVersionTest, InvalMessageMultiDDLTest) {
+  RestartClusterWithInvalMessageEnabled();
+  auto conn = ASSERT_RESULT(Connect());
+  ASSERT_OK(conn.Execute("SET log_min_messages = DEBUG1"));
+  ASSERT_OK(conn.Execute("CREATE TABLE foo(id INT)"));
+  auto query = "BEGIN; "s;
+  for (int i = 1; i <= 5; i++) {
+    query += Format("ALTER TABLE foo ADD COLUMN id$0 INT; ", i);
+  }
+  query += "END";
+  LOG(INFO) << "multi-ddl query: " << query;
+  ASSERT_OK(conn.Execute(query));
+  auto yugabyte_db_oid = ASSERT_RESULT(GetDatabaseOid(&conn, kYugabyteDatabase));
+  auto result = ASSERT_RESULT(conn.FetchAllAsString(
+      Format("SELECT current_version, length(messages) FROM pg_yb_invalidation_messages "
+             "WHERE db_oid = $0", yugabyte_db_oid)));
+  LOG(INFO) << "result: " << result;
+  ASSERT_EQ(result, "2, 120; 3, 144; 4, 144; 5, 144; 6, 144");
 }
 
 } // namespace pgwrapper

java/yb-pgsql/src/test/java/org/yb/pgsql/TestPgFollowerReads.java
@@ -433,6 +433,16 @@ public class TestPgFollowerReads extends BasePgSQLTest {
   // The test checks that follower reads are not used if sys catalog reads are to be performed.
   @Test
   public void testPgSysCatalogNoFollowerReads() throws Exception {
+    // Make a new connection. The current connection was used to execute some test setup
+    // code including a DDL statement: GRANT ALL ON SCHEMA public TO public
+    // When we use invalidation messages we use a SQL function to increment the catalog
+    // version and insert the messages into the pg_yb_invalidation_messages table.
+    // Calling this SQL function caused more master catalog table reads which resulted
+    // in a larger catalog cache in the current session. In particular, it already included
+    // those catalog cache entries that would have triggered master RPCs when executing the
+    // normal query of the test. As a result, no more master RPCs were triggered. Using
+    // a new connection avoids this problem.
+    connection = getConnectionBuilder().connect();
     try (Statement stmt = connection.createStatement()) {
       stmt.execute("SET yb_follower_read_staleness_ms = " + (2 * kMaxClockSkewMs + 1));
       stmt.execute("SET yb_read_from_followers = true");

java/yb-pgsql/src/test/java/org/yb/pgsql/TestYsqlUpgrade.java
@@ -194,6 +194,7 @@ public class TestYsqlUpgrade extends BasePgSQLTest {
   private static final String CATALOG_VERSION_TABLE        = "pg_yb_catalog_version";
   private static final String MIGRATIONS_TABLE             = "pg_yb_migration";
   private static final String LOGICAL_CLIENT_VERSION_TABLE = "pg_yb_logical_client_version";
+  private static final String INVALIDATION_MESSAGES_TABLE  = "pg_yb_invalidation_messages";
 
   /** Guaranteed to be greated than any real OID, needed for sorted entities to appear at the end */
   private static final long PLACEHOLDER_OID = 1234567890L;
@@ -1249,6 +1250,13 @@ public class TestYsqlUpgrade extends BasePgSQLTest {
       postSnapshot.catalog.remove(MIGRATIONS_TABLE);
       postSnapshot.catalog.remove(CATALOG_VERSION_TABLE);
 
+      // Some migration script contains a DDL such as CREATE OR REPLACE VIEW, which is
+      // not skipped when the migration script is run again (In contrast, CREATE TABLE
+      // IF NOT EXISTS will skip re-executing CREATE TABLE when run again). Re-executing
+      // the DDL will leave different rows in pg_yb_invalidation_messages.
+      preSnapshot.catalog.remove(INVALIDATION_MESSAGES_TABLE);
+      postSnapshot.catalog.remove(INVALIDATION_MESSAGES_TABLE);
+
       assertSysCatalogSnapshotsEquals(preSnapshot, postSnapshot);
     }
   }
@@ -1948,8 +1956,15 @@ public class TestYsqlUpgrade extends BasePgSQLTest {
 
       List<Row> reinitdbRows = simplifiedFreshSnapshot.catalog.get(tableName);
 
-      assertCollectionSizes("Table '" + tableName + "' has different size after migration!",
-          reinitdbRows, migratedRows);
+      // The table pg_yb_invalidation_messages isn't empty in reinitdbRows because of DDLs
+      // executed in test setup code. Also it may not be empty after running migration scripts
+      // that contain DDLs if they are executed after the table pg_yb_invalidation_messages is
+      // created, so we are not expected to see pg_yb_invalidation_messages has same size or
+      // contents between reinitdbRows and migratedRows.
+      if (!tableName.equals(INVALIDATION_MESSAGES_TABLE)) {
+        assertCollectionSizes("Table '" + tableName + "' has different size after migration!",
+            reinitdbRows, migratedRows);
+      }
 
       for (int i = 0; i < migratedRows.size(); ++i) {
         Row reinitdbRow = reinitdbRows.get(i);

src/postgres/contrib/pg_stat_statements/pg_stat_statements.c
@@ -1746,6 +1746,9 @@ pgss_store(const char *query, uint64 queryId,
 	if (!pgss || !pgss_hash)
 		return;
 
+	if (yb_is_calling_internal_function_for_ddl)
+		return;
+
 	/*
 	 * Nothing to do if compute_query_id isn't enabled and no other module
 	 * computed a query identifier.

src/postgres/src/backend/access/transam/xact.c
@@ -2336,6 +2336,8 @@ YBCRestartWriteTransaction()
 static void
 CommitTransaction(void)
 {
+	if (IsYugaByteEnabled())
+		YbIncrementPgTxnsCommitted();
 	TransactionState s = CurrentTransactionState;
 	TransactionId latestXid;
 	bool		is_parallel_worker;

src/postgres/src/backend/catalog/yb_catalog/yb_catalog_version.c
@@ -29,6 +29,7 @@
 #include "executor/ybExpr.h"
 #include "executor/ybModifyTable.h"
 #include "nodes/makefuncs.h"
+#include "optimizer/cost.h"
 #include "utils/catcache.h"
 #include "utils/fmgroids.h"
 #include "utils/lsyscache.h"
@@ -85,6 +86,26 @@ YbGetMasterCatalogVersion()
 
 /* Modify Catalog Version */
 
+static Datum
+GetInvalidationMessages(const SharedInvalidationMessage *invalMessages, int nmsgs,
+						bool *is_null)
+{
+	if (!invalMessages)
+	{
+		Assert(nmsgs >= 0);
+		*is_null = true;
+		return (Datum) 0;
+	}
+
+	size_t str_len = sizeof(SharedInvalidationMessage) * nmsgs;
+	bytea *bstr = palloc(VARHDRSZ + str_len);
+
+	memcpy(VARDATA(bstr), invalMessages, str_len);
+	SET_VARSIZE(bstr, VARHDRSZ + str_len);
+	*is_null = false;
+	return PointerGetDatum(bstr);
+}
+
 static void
 YbCallSQLIncrementCatalogVersions(Oid functionId, bool is_breaking_change,
 								  const char *command_tag)
@@ -128,9 +149,11 @@ YbCallSQLIncrementCatalogVersions(Oid functionId, bool is_breaking_change,
 
 	PG_TRY();
 	{
+		yb_is_calling_internal_function_for_ddl = true;
 		FunctionCallInvoke(fcinfo);
 		/* Restore old values. */
 		yb_non_ddl_txn_for_sys_tables_allowed = saved;
+		yb_is_calling_internal_function_for_ddl = false;
 		SetUserIdAndSecContext(save_userid, save_sec_context);
 		if (!snapshot_set)
 			PopActiveSnapshot();
@@ -139,6 +162,7 @@ YbCallSQLIncrementCatalogVersions(Oid functionId, bool is_breaking_change,
 	{
 		/* Restore old values. */
 		yb_non_ddl_txn_for_sys_tables_allowed = saved;
+		yb_is_calling_internal_function_for_ddl = false;
 		SetUserIdAndSecContext(save_userid, save_sec_context);
 		if (!snapshot_set)
 			PopActiveSnapshot();
@@ -147,11 +171,158 @@ YbCallSQLIncrementCatalogVersions(Oid functionId, bool is_breaking_change,
 	PG_END_TRY();
 }
 
-Oid
-YbGetSQLIncrementCatalogVersionsFunctionOid()
+static void
+MaybeLogNewSQLIncrementCatalogVersion(bool success,
+									  Oid db_oid,
+									  bool is_breaking_change,
+									  bool is_global_ddl,
+									  const char *command_tag,
+									  uint64_t new_version)
 {
-	List	   *names = list_make2(makeString("pg_catalog"),
-								   makeString("yb_increment_all_db_catalog_versions"));
+	if (!(*YBCGetGFlags()->TEST_ysql_hide_catalog_version_increment_log))
+	{
+		bool log_ysql_catalog_versions =
+			*YBCGetGFlags()->log_ysql_catalog_versions;
+		char tmpbuf[60] = " failed";
+		if (success)
+			snprintf(tmpbuf, sizeof(tmpbuf),
+					 ", new version for database %u is %" PRIu64,
+					 db_oid, new_version);
+		char *action = is_global_ddl ? "all master db catalog versions"
+									 : "master db catalog version";
+		ereport(LOG,
+				(errmsg("%s: incrementing %s "
+						"(%sbreaking) with inval messages%s",
+						__func__, action, is_breaking_change ? "" : "non", tmpbuf),
+				errdetail("Local version: %" PRIu64 ", node tag: %s.",
+						  YbGetCatalogCacheVersion(), command_tag ? command_tag : "n/a"),
+				errhidestmt(!log_ysql_catalog_versions),
+				errhidecontext(!log_ysql_catalog_versions)));
+	}
+}
+
+static uint64_t
+YbCallNewSQLIncrementCatalogVersionHelper(Oid functionId,
+										  Oid db_oid,
+										  bool is_breaking_change,
+										  const char *command_tag,
+										  Datum messages,
+										  bool is_null,
+										  int expiration_secs,
+										  bool is_global_ddl)
+{
+	FmgrInfo    flinfo;
+	LOCAL_FCINFO(fcinfo, 4);
+	fmgr_info(functionId, &flinfo);
+	InitFunctionCallInfoData(*fcinfo, &flinfo, 4, InvalidOid, NULL, NULL);
+	fcinfo->args[0].value = ObjectIdGetDatum(db_oid);
+	fcinfo->args[0].isnull = false;
+	fcinfo->args[1].value = BoolGetDatum(is_breaking_change);
+	fcinfo->args[1].isnull = false;
+	fcinfo->args[2].value = messages;
+	fcinfo->args[2].isnull = is_null;
+	fcinfo->args[3].value = expiration_secs;
+	fcinfo->args[3].isnull = false;
+
+	/* Save old values and set new values to enable the call. */
+	bool saved = yb_non_ddl_txn_for_sys_tables_allowed;
+	yb_non_ddl_txn_for_sys_tables_allowed = true;
+	bool saved_enable_seqscan = enable_seqscan;
+	/*
+	 * Avoid sequential scan for non-global-impact DDL, otherwise we can get
+	 * conflicts between concurrent cross-database DDLs.
+	 */
+	if (!is_global_ddl)
+		enable_seqscan = false;
+	Oid save_userid;
+	int save_sec_context;
+	GetUserIdAndSecContext(&save_userid, &save_sec_context);
+	SetUserIdAndSecContext(BOOTSTRAP_SUPERUSERID,
+						   SECURITY_RESTRICTED_OPERATION);
+	/* Calling a user defined function requires a snapshot. */
+	bool snapshot_set = ActiveSnapshotSet();
+	if (!snapshot_set)
+		PushActiveSnapshot(GetTransactionSnapshot());
+	volatile uint64_t new_version;
+	PG_TRY();
+	{
+		yb_is_calling_internal_function_for_ddl = true;
+		Datum retval = FunctionCallInvoke(fcinfo);
+
+		/* Restore old values. */
+		yb_non_ddl_txn_for_sys_tables_allowed = saved;
+		yb_is_calling_internal_function_for_ddl = false;
+		enable_seqscan = saved_enable_seqscan;
+		SetUserIdAndSecContext(save_userid, save_sec_context);
+		if (!snapshot_set)
+			PopActiveSnapshot();
+		new_version = DatumGetUInt64(retval);
+		MaybeLogNewSQLIncrementCatalogVersion(true /* success */ ,
+											  db_oid,
+											  is_breaking_change,
+											  is_global_ddl,
+											  command_tag,
+											  new_version);
+	}
+	PG_CATCH();
+	{
+		/* Restore old values. */
+		yb_non_ddl_txn_for_sys_tables_allowed = saved;
+		yb_is_calling_internal_function_for_ddl = false;
+		enable_seqscan = saved_enable_seqscan;
+		SetUserIdAndSecContext(save_userid, save_sec_context);
+		if (!snapshot_set)
+			PopActiveSnapshot();
+		MaybeLogNewSQLIncrementCatalogVersion(false /* success */ ,
+											  db_oid,
+											  is_breaking_change,
+											  is_global_ddl,
+											  command_tag,
+											  0 /* new_version */ );
+		PG_RE_THROW();
+	}
+	PG_END_TRY();
+	return new_version;
+}
+
+static uint64_t
+YbCallNewSQLIncrementCatalogVersion(Oid functionId, Oid db_oid, bool is_breaking_change,
+									const char *command_tag,
+									Datum messages, bool is_null, int expiration_secs)
+{
+	return YbCallNewSQLIncrementCatalogVersionHelper(functionId,
+													 db_oid,
+													 is_breaking_change,
+													 command_tag,
+													 messages,
+													 is_null,
+													 expiration_secs,
+													 false /* is_global_ddl */ );
+}
+
+static uint64_t
+YbCallNewSQLIncrementAllCatalogVersions(Oid functionId,
+										Oid db_oid,
+										bool is_breaking_change,
+										const char *command_tag,
+										Datum messages,
+										bool is_null,
+										int expiration_secs)
+{
+	return YbCallNewSQLIncrementCatalogVersionHelper(functionId,
+													 db_oid,
+													 is_breaking_change,
+													 command_tag,
+													 messages,
+													 is_null,
+													 expiration_secs,
+													 true /* is_global_ddl */ );
+}
+
+static Oid
+YbGetSQLIncrementCatalogVersionFunctionOidHelper(char *fname)
+{
+	List *names = list_make2(makeString("pg_catalog"), makeString(fname));
 	FuncCandidateList clist = FuncnameGetCandidates(names,
 													-1 /* nargs */ ,
 													NIL /* argnames */ ,
@@ -159,7 +330,6 @@ YbGetSQLIncrementCatalogVersionsFunctionOid()
 													false /* expand_defaults */ ,
 													false /* include_out_arguments */ ,
 													false /* missing_ok */ );
-
 	/* We expect exactly one candidate. */
 	if (clist && clist->next == NULL)
 		return clist->oid;
@@ -168,13 +338,81 @@ YbGetSQLIncrementCatalogVersionsFunctionOid()
 	return InvalidOid;
 }
 
+Oid
+YbGetSQLIncrementCatalogVersionsFunctionOid()
+{
+	return YbGetSQLIncrementCatalogVersionFunctionOidHelper("yb_increment_all_db_catalog_versions");
+}
+
+static Oid
+YbGetNewIncrementCatalogVersionFunctionOid()
+{
+	return YbGetSQLIncrementCatalogVersionFunctionOidHelper("yb_increment_db_catalog_version_with_inval_messages");
+}
+
+static Oid
+YbGetNewIncrementAllCatalogVersionsFunctionOid()
+{
+	return YbGetSQLIncrementCatalogVersionFunctionOidHelper("yb_increment_all_db_catalog_versions_with_inval_messages");
+}
+
 static void
 YbIncrementMasterDBCatalogVersionTableEntryImpl(Oid db_oid,
 												bool is_breaking_change,
 												bool is_global_ddl,
-												const char *command_tag)
+												const char *command_tag,
+												const SharedInvalidationMessage *invalMessages,
+												int nmsgs)
 {
 	Assert(YbGetCatalogVersionType() == CATALOG_VERSION_CATALOG_TABLE);
+
+	if (*YBCGetGFlags()->TEST_yb_enable_invalidation_messages &&
+		YBIsDBCatalogVersionMode())
+	{
+		Oid func_oid = is_global_ddl ? YbGetNewIncrementAllCatalogVersionsFunctionOid()
+									 : YbGetNewIncrementCatalogVersionFunctionOid();
+		if (OidIsValid(func_oid) && YbInvalidationMessagesTableExists())
+		{
+			YbResetNewCatalogVersion();
+			bool is_null = false;
+			Datum messages = GetInvalidationMessages(invalMessages, nmsgs, &is_null);
+			int expiration_secs =
+				*YBCGetGFlags()->TEST_yb_invalidation_message_expiration_secs;
+			if (is_global_ddl)
+			{
+				/*
+				 * Call yb_increment_all_db_catalog_versions_with_inval_messages(
+				 *     is_breaking_change, messages, expiration_secs).
+				 * Pass MyDatabaseId to get the new version of MyDatabaseId.
+				 */
+				uint64_t new_version =
+					YbCallNewSQLIncrementAllCatalogVersions(func_oid, MyDatabaseId,
+															is_breaking_change,
+															command_tag, messages,
+															is_null, expiration_secs);
+				YbSetNewCatalogVersion(new_version);
+				return;
+			}
+
+			/*
+			 * Call yb_increment_db_catalog_version_with_inval_messages(
+			 *     db_oid, is_breaking_change, is_global_ddl, messages).
+			 */
+			uint64_t new_version =
+				YbCallNewSQLIncrementCatalogVersion(func_oid, db_oid,
+													is_breaking_change,
+													command_tag, messages,
+													is_null, expiration_secs);
+			/*
+			 * The new version of database_oid is only meaningful when
+			 * db_oid == MyDatabaseId.
+			 */
+			if (db_oid == MyDatabaseId)
+				YbSetNewCatalogVersion(new_version);
+
+			return;
+		}
+	}
 
 	if (is_global_ddl)
 	{
@@ -337,7 +575,9 @@ YbIncrementMasterDBCatalogVersionTableEntryImpl(Oid db_oid,
 bool
 YbIncrementMasterCatalogVersionTableEntry(bool is_breaking_change,
 										  bool is_global_ddl,
-										  const char *command_tag)
+										  const char *command_tag,
+										  const SharedInvalidationMessage *invalMessages,
+										  int nmsgs)
 {
 	if (YbGetCatalogVersionType() != CATALOG_VERSION_CATALOG_TABLE)
 		return false;
@@ -349,7 +589,9 @@ YbIncrementMasterCatalogVersionTableEntry(bool is_breaking_change,
 	YbIncrementMasterDBCatalogVersionTableEntryImpl(database_oid,
 													is_breaking_change,
 													is_global_ddl,
-													command_tag);
+													command_tag,
+													invalMessages,
+													nmsgs);
 	if (yb_test_fail_next_inc_catalog_version)
 	{
 		yb_test_fail_next_inc_catalog_version = false;

src/postgres/src/backend/storage/ipc/sinvaladt.c
@@ -790,6 +790,7 @@ SICleanupQueue(bool callerHasWriteLock, int minFree)
 
 				if (msg->yb_header.sender_pid == procPid)
 				{
+					elog(LOG, "resetState of %d", procPid);
 					stateP->resetState = true;
 					break;
 				}

commit e395d74b04fee8f0b93c51ec16ac0c4e49934484

[#27103] YSQL: Fix bug that inserted null into pg_yb_catalog_version
Summary:
The bug repro:
```
CREATE OR REPLACE FUNCTION foo(amount INT) RETURNS VOID AS
$$
  UPDATE pg_yb_catalog_version SET current_version = current_version + amount WHERE db_oid = 1;
$$ LANGUAGE SQL;
CREATE FUNCTION
SET enable_seqscan = off;
SET
SET yb_non_ddl_txn_for_sys_tables_allowed=1;
SET
SELECT foo(null);
 foo
-----

(1 row)

SELECT * FROM pg_yb_catalog_version WHERE db_oid = 1;
 db_oid | current_version | last_breaking_version
--------+-----------------+-----------------------
      1 |                 |                     1
(1 row)
```

Note that we were able to insert a NULL value into the `current_version` column
which has a not null constraint:

```
yugabyte=# \d pg_yb_catalog_version;
            Table "pg_catalog.pg_yb_catalog_version"
        Column         |  Type  | Collation | Nullable | Default
-----------------------+--------+-----------+----------+---------
 db_oid                | oid    |           | not null |
 current_version       | bigint |           | not null |
 last_breaking_version | bigint |           | not null |
Indexes:
    "pg_yb_catalog_version_db_oid_index" PRIMARY KEY, lsm (db_oid ASC), tablespace "pg_global"
Tablespace: "pg_global"

```

Commit 50fb5ef10af000a41bb4e1c4d32ae6730c04374c introduced this regression. In
order to support cross-database concurrent DDLs, we need to ensure pushdown
happens in the function `yb_increment_db_catalog_version_with_inval_messages`,
which has a update statement:

```
        (UPDATE pg_yb_catalog_version
        SET current_version = current_version + 1,
            last_breaking_version = CASE WHEN is_breaking_change THEN
                                         current_version + 1
                                    ELSE last_breaking_version END
        WHERE pg_yb_catalog_version.db_oid = yb_increment_db_catalog_version_with_inval_messages.db_oid
```

Without expression pushdown, PG would read all the rows of

src/postgres/src/backend/optimizer/plan/createplan.c
@@ -3550,7 +3550,8 @@ yb_single_row_update_or_delete_path(PlannerInfo *root,
 			 * are affected by a trigger, so we should update all indexes.
 			 */
 			if ((TupleDescAttr(tupDesc, resno - 1)->attnotnull &&
-				 relation->rd_id != YBCatalogVersionRelationId) ||
+				 (relation->rd_id != YBCatalogVersionRelationId ||
+				  !yb_is_calling_internal_sql_for_ddl)) ||
 				YBIsCollationValidNonC(ybc_get_attcollation(tupDesc, resno)) ||
 				!YbCanPushdownExpr(tle->expr, &colrefs, relid))
 			{

src/yb/yql/pgwrapper/pg_catalog_version-test.cc
@@ -2597,5 +2597,34 @@ EXECUTE PROCEDURE log_ddl();
   ASSERT_EQ(v, 3);
 }
 
+// We have made a special case to allow expression pushdown for table pg_yb_catalog_version
+// in order to ensure continued support of cross-database concurrent DDLs. Without expression
+// pushdown PG would read all the rows of pg_yb_catalog_version in order to check for not null
+// constraint on column current_version and column last_breaking_version. Reading all the rows
+// defeats concurrent cross-database DDLs which would otherwise operate on different rows without
+// conflicts. This test verifies our pushdown special case does not incorrectly allow a null
+// value gets inserted into the table pg_yb_catalog_version.
+TEST_F(PgCatalogVersionTest, NotNullConstraint) {
+  const string query =
+        R"#(
+CREATE OR REPLACE FUNCTION foo(amount INT) RETURNS VOID AS
+$$
+  UPDATE pg_yb_catalog_version SET current_version = current_version + amount WHERE db_oid = 1;
+$$ LANGUAGE SQL;
+SET enable_seqscan = off;
+SET yb_non_ddl_txn_for_sys_tables_allowed=1;
+        )#";
+  auto conn = ASSERT_RESULT(ConnectToDB(kYugabyteDatabase));
+  ASSERT_OK(conn.Execute(query));
+  auto status = conn.Execute("SELECT foo(null)");
+  ASSERT_TRUE(status.IsNetworkError()) << status;
+  ASSERT_STR_CONTAINS(status.ToString(), "null value in column \"current_version\" of relation "
+                                         "\"pg_yb_catalog_version\" violates not-null constraint");
+  auto expected = "1, 1, 1"s;
+  auto result = ASSERT_RESULT(conn.FetchAllAsString(
+      "SELECT * FROM pg_yb_catalog_version WHERE db_oid = 1"));
+  ASSERT_EQ(expected, result);
+}
+
 } // namespace pgwrapper
 } // namespace yb