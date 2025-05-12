d7d65f22b7bfe4586ae060d56012d785829fe8b3
Huapeng Yuan <huapeng.yuan@yugabyte.com>
2025-05-12 22:48:46 +0000

    [BACKPORT 2025.1][#23696] docdb: Set propagated safe time at follower after replicating all operations
    Summary:
    At follower,  the UpdateConsensus  operation can set the propagated safe time even if it's aborted.
    After the operation is aborted, We did a follower read or something else to get SafeTimeForFollower .
    The leader tries to replicate the same operation to follower, and this op had a lower hybrid time and hit
    the fatal "New operation's hybrid time too low":
    ```
    27. AddFollowerPending { ht: { physical: 1724771934654340 } op_id: 12.195059 }
    28. SetPropagatedSafeTimeOnFollower { ht: { physical: 1724771985795214 } }
    29. Aborted { ht: { physical: 1724771934654340 } op_id: 12.195059 }
    30. LastReplicatedHybridTime { last_replicated: { physical: 1724771933765517 } }
    31. SafeTimeForFollower { min_allowed: <min> deadline: -inf safe_time_with_source: { safe_time: { physical: 1724771985795214 } source: kPropagated } }
    32. LastReplicatedHybridTime { last_replicated: { physical: 1724771933765517 } }
    New operation's hybrid time too low: { physical: 1724771934654340 }, op id: 12.195059
      ...
      max_safe_time_returned_for_follower_={ safe_time: { physical: 1724771985795214 } source: kPropagated }
      !!! ht <= max_safe_time_returned_for_follower_.safe_time=1
      static_cast<int64_t>(ht.ToUint64() - max_safe_time_returned_for_follower_.safe_time.ToUint64())=-209473019904
      ht.PhysicalDiff(max_safe_time_returned_for_follower_.safe_time)=-51140874
    ```
    
    Besides the FATAL, a small window exists during which we may read inconsistent data from the follower. To avoid the
    consistency issue, we need to ensure: once the propagated safe time is set, the entry should not be popped from
    the mvcc queue due to failure.
    
    To fix this, we should propagated safe time only after a successful AddPendingOperation.
    Jira: DB-12605
    
    Original commit: 5dbb355c910bec63e612bf63a6ad172073065ca4 / D38336
    
    Test Plan:
    Add a new unit test:
    ybd release --cxx-test raft_consensus-itest --gtest_filter RaftConsensusITest.UpdateConsensusFollowerFailedAfterStartReplicaOperation
    
    Reviewers: sergei, rthallam
    
    Reviewed By: rthallam
    
    Subscribers: ybase, rthallam, svc_phabricator, huapeng.yuan
    
    Differential Revision: https://phorge.dev.yugabyte.com/D44759

--- src/yb/tablet/tablet_peer.cc
+++ src/yb/tablet/tablet_peer.cc
@@ -1493,6 +1493,10 @@ void TabletPeer::SetPropagatedSafeTime(HybridTime ht) {
   (**driver).ExecuteAsync();
 }
 
+void TabletPeer::SetMvccPropagatedSafeTime(HybridTime ht) {
+  tablet_->mvcc_manager()->SetPropagatedSafeTimeOnFollower(ht);
+}
+
 bool TabletPeer::ShouldApplyWrite() {
   return tablet_->ShouldApplyWrite();
 }
