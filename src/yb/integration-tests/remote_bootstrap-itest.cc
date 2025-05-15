4ad9a635d56a834192096a8bf4a6e4ccb946d774
yusong-yan <yusongyan3@gmail.com>
2025-05-15 13:23:20 +0000

    [BACKPORT 2025.1][#27204] docdb: Fix RemoteBootstrapITest.TestRBSWithLazySuperblockFlush
    Summary:
    Original commit: 983c4a5cdfed7320bc58eae307a5b1d1a1da742e / D44007
    Issue:
    After enabling the advisory lock feature, a new advisory lock table is created, which adds tablets on the tservers. By default, the number of advisory lock tablets is 1. which can lead to uneven tablet distribution across tservers. In this test scenario, the imbalance triggered additional tablet moves, endup causing the wait logic to exceed the expected timeout:
    ```
    Bad status: Timed out (yb/util/backoff_waiter.cc:78): Operation 'Waiting for log record 'Pausing due to flag TEST_pause_rbs_before_download_wal' on [ts-3]...' didn't complete within 10148ms
    ```
    Fix:
    Set the flag `--TEST_system_table_num_tablets=3` to ensure system tables (including the advisory lock table) are evenly distributed across all three tservers, preventing unnecessary tablet move and avoiding timeout failures.
    Jira: DB-16694
    
    Test Plan: ./yb	--cxx-test integration-tests_remote_bootstrap-itest --gtest_filter RemoteBootstrapITest.TestRBSWithLazySuperblockFlush
    
    Reviewers: aagrawal, rthallam, #db-approvers
    
    Reviewed By: rthallam, #db-approvers
    
    Subscribers: ybase, rthallam
    
    Differential Revision: https://phorge.dev.yugabyte.com/D44041

--- src/yb/integration-tests/remote_bootstrap-itest.cc
+++ src/yb/integration-tests/remote_bootstrap-itest.cc
@@ -2011,6 +2011,9 @@ RemoteBootstrapITest::FindTablet(
 }
 
 TEST_F(RemoteBootstrapITest, TestRBSWithLazySuperblockFlush) {
+  vector<string> master_flags;
+  master_flags.push_back("--TEST_system_table_num_tablets=3");
+
   vector<string> ts_flags;
   // Enable lazy superblock flush.
   ts_flags.push_back("--lazily_flush_superblock=true");
@@ -2033,7 +2036,7 @@ TEST_F(RemoteBootstrapITest, TestRBSWithLazySuperblockFlush) {
   ts_flags.push_back("--TEST_skip_force_superblock_flush=true");
 
   ASSERT_NO_FATALS(StartCluster(
-      ts_flags, /* master_flags = */ {}, /* num_tablet_servers = */ 3, /* enable_ysql = */ true));
+      ts_flags, master_flags, /* num_tablet_servers = */ 3, /* enable_ysql = */ true));
   RBSWithLazySuperblockFlush(/* num_tables */ 20);
 }
 
