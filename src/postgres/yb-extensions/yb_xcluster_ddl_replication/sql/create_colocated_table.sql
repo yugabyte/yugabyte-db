593a88fc55afbf51ec978ccdad828d34a3040a64
jhe <jhe@yugabyte.com>
2025-05-28 11:56:33 -0700

    [BACKPORT 2025.1][#27337] xClusterDDLRepl: Filter out oids in TestPgRegressYbExtensionsYbXclusterDdlReplication
    Summary:
    Adding a function in setup that will filter out relfile_oid and colocation_id from regress test
    output. This avoids the need to constantly have to update all these output files when we make a
    small change to the extension, and should also help to avoid backport annoyances.
    Jira: DB-16846
    
    Original commit: 233ff79ecef0598879f32a576ddf1d1f2d09beed / D44216
    
    Test Plan:
    ```
    ybd --java-test "org.yb.pgsql.TestPgRegressYbExtensionsYbXclusterDdlReplication"
    ```
    
    Reviewers: mlillibridge, xCluster, #db-approvers
    
    Reviewed By: mlillibridge, #db-approvers
    
    Subscribers: slingam, svc_phabricator, ybase, yql
    
    Differential Revision: https://phorge.dev.yugabyte.com/D44344

--- src/postgres/yb-extensions/yb_xcluster_ddl_replication/sql/create_colocated_table.sql
+++ src/postgres/yb-extensions/yb_xcluster_ddl_replication/sql/create_colocated_table.sql
@@ -8,12 +8,12 @@ CREATE TEMP TABLE temp_foo(i int PRIMARY KEY);
 -- Verify that colocated tables are allowed.
 CREATE TABLE coloc_foo(i int PRIMARY KEY);
 
-SELECT yb_data FROM yb_xcluster_ddl_replication.ddl_queue ORDER BY ddl_end_time;
+SELECT yb_data FROM TEST_filtered_ddl_queue() ORDER BY ddl_end_time;
 
 -- Verify that non-colocated table is captured.
 CREATE TABLE non_coloc_foo(i int PRIMARY KEY) WITH (COLOCATION = false);
 
-SELECT yb_data FROM yb_xcluster_ddl_replication.ddl_queue ORDER BY ddl_end_time;
+SELECT yb_data FROM TEST_filtered_ddl_queue() ORDER BY ddl_end_time;
 SELECT yb_data FROM yb_xcluster_ddl_replication.replicated_ddls ORDER BY ddl_end_time;
 
 select * from TEST_verify_replicated_ddls();
