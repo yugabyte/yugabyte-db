e5722eac2e061161dce39a18ffeee96f95bb9559
Daniel Shubin <dshubin@yugabyte.com>
2025-05-30 20:28:58 +0000

    [PLAT-17725] precheck for GLIBC version on install and upgrade
    Summary:
    since 2024.2 we have started building postgres to use
    GLIBC 2.25 and up. Older systems, such as RHEL7, do not
    support this, and we should block installs and upgrades
    
    Test Plan:
    ut
    tested install on valid system
    tested install and upgrade on rhel 7
    
    Reviewers: muthu
    
    Reviewed By: muthu
    
    Differential Revision: https://phorge.dev.yugabyte.com/D44413

---
+++ managed/yba-installer/pkg/preflight/checks/glibc.go
@@ -0,0 +1,99 @@
+package checks
+
+import (
+	"fmt"
+	"strconv"
+	"strings"
+
+	"github.com/yugabyte/yugabyte-db/managed/yba-installer/pkg/common/shell"
+	"github.com/yugabyte/yugabyte-db/managed/yba-installer/pkg/logging"
+)
+
+const minSupportedGlibcVersion = "2.25"
+
+var Glibc = &glibcCheck{
+	name:        "glibc",
+	skipAllowed: false,
+}
+
+type glibcCheck struct {
+	name        string
+	skipAllowed bool
+}
+
+func (g glibcCheck) Name() string {
+	return g.name
+}
+func (g glibcCheck) SkipAllowed() bool {
+	return g.skipAllowed
+}
+func (g glibcCheck) Execute() Result {
+	res := Result{
+		Check:  g.name,
+		Status: StatusPassed,
+	}
+	// Check glibc version
+	version, err := getGlibcVersion()
+	if err != nil {
+		res.Status = StatusCritical
+		res.Error = err
+		return res
+	}
+	if !isGlibcVersionSupported(version) {
+		res.Status = StatusCritical
+		res.Error = fmt.Errorf("glibc version %s is not supported. Required %s", version, minSupportedGlibcVersion)
+		return res
+	}
+
+	return res
+}
+
+func isGlibcVersionSupported(version string) bool {
+	aParts := strings.Split(version, ".")                  // Actual version
+	eParts := strings.Split(minSupportedGlibcVersion, ".") // Expected version
+	for i := 0; i < len(eParts); i++ {
+		if i >= len(aParts) {
+			// If actual version has more parts and all previous parts matched, it's still supported
+			break
+		}
+		aPart, err := strconv.Atoi(aParts[i])
+		if err != nil {
+			logging.Error(fmt.Sprintf("failed to parse glibc version part: %s", aParts[i]))
+			return false
+		}
+		ePart, err := strconv.Atoi(eParts[i])
+		if err != nil {
+			logging.Error(fmt.Sprintf("failed to parse expected glibc version part: %s", eParts[i]))
+			return false
+		}
+		if aPart < ePart {
+			logging.Error(fmt.Sprintf("glibc version %s is lower than required %s", version, minSupportedGlibcVersion))
+			return false
+		}
+	}
+
+	return true
+}
+
+func getGlibcVersion() (string, error) {
+	output := shell.Run("ldd", "--version")
+	if !output.SucceededOrLog() {
+		return "", fmt.Errorf("failed to run ldd command: %w", output.Error)
+	}
+
+	// The first line of the output contains the version information
+	lines := output.StdoutString()
+	if len(lines) == 0 {
+		return "", fmt.Errorf("no output from ldd command")
+	}
+
+	// Extract the version number from the first line
+	var version string
+	_, err := fmt.Sscanf(lines, "ldd (GNU libc) %s", &version)
+	if err != nil {
+		logging.Error(fmt.Sprintf("failed to parse glibc version from output: %s", lines))
+		return "", fmt.Errorf("failed to parse glibc version: %w", err)
+	}
+
+	return version, nil
+}
