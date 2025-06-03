yugabyte/yugabyte-db

commit f8950ffa4f4f913ca20d6652d622d5003eac6ef4

[#25807] DocDB: Support various metrics in vector indexes
Summary:
PgVector and usearch support different vector distance functions.
PgVector supports: Squared Euclidean/L2, Inner product, Cosine and L1 distance functions.
usearch also supports: Squared Euclidean/L2, Inner product, Cosine, along with 7 other distance functions. It, however, does not support L1.

So both of them supports:
1) L2Squared
2) Cosine
3) InnerProduct

This diff provides support for all those metrics in ybhnsw index access method.
Jira: DB-15107

Test Plan: PgVectorIndexTest.Cosine/*, PgVectorIndexTest.L2/*, PgVectorIndexTest.InnerProduct/*

Reviewers: tnayak, aleksandr.ponomarenko

Reviewed By: tnayak, aleksandr.ponomarenko

Subscribers: yql, ybase

Tags: #jenkins-ready

Differential Revision: https://phorge.dev.yugabyte.com/D41586

src/postgres/src/backend/utils/misc/pg_yb_utils.c
@@ -6129,7 +6129,8 @@ YbIndexSetNewRelfileNode(Relation indexRel, Oid newRelfileNodeId,
 				   InvalidOid /* colocation ID */ ,
 				   indexRel->rd_rel->reltablespace,
 				   newRelfileNodeId,
-				   YbGetRelfileNodeId(indexRel));
+				   YbGetRelfileNodeId(indexRel),
+				   NULL /* opclassOids */ );
 
 	table_close(indexedRel, ShareLock);
 

src/postgres/third-party-extensions/pgvector/src/ybvector/ybvectorwrite.c
@@ -69,16 +69,12 @@ void
 bindVectorIndexOptions(YbcPgStatement handle,
 					   IndexInfo *indexInfo,
 					   TupleDesc indexTupleDesc,
-					   YbcPgVectorIdxType ybpg_idx_type)
+					   YbcPgVectorIdxType ybpg_idx_type,
+					   YbcPgVectorDistType dist_type)
 {
 	YbcPgVectorIdxOptions options;
 	options.idx_type = ybpg_idx_type;
-
-	/*
-	 * Hardcoded for now.
-	 * TODO(tanuj): Pass down distance info from the used distance opclass.
-	 */
-	options.dist_type = YB_VEC_DIST_L2;
+	options.dist_type = dist_type;
 
 	/* We only support indexes with one vector attribute for now. */
 	Assert(indexTupleDesc->natts == 1);

src/postgres/src/backend/access/yb_access/yb_lsm.c
@@ -718,7 +718,8 @@ static void
 ybcinbindschema(YbcPgStatement handle,
 				struct IndexInfo *indexInfo,
 				TupleDesc indexTupleDesc,
-				int16 *coloptions)
+				int16 *coloptions,
+				Oid *opclassOids)
 {
 	YBCBindCreateIndexColumns(handle,
 							  indexInfo,

src/postgres/src/backend/access/ybgin/ybginutil.c
@@ -104,7 +104,8 @@ void
 ybginbindschema(YbcPgStatement handle,
 				struct IndexInfo *indexInfo,
 				TupleDesc indexTupleDesc,
-				int16 *coloptions)
+				int16 *coloptions,
+				Oid *objectClassId)
 {
 	YBCBindCreateIndexColumns(handle,
 							  indexInfo,

src/postgres/src/backend/catalog/index.c
@@ -1040,7 +1040,8 @@ index_create(Relation heapRelation,
 					   colocationId,
 					   tableSpaceId,
 					   YbGetRelfileNodeId(indexRelation),
-					   InvalidOid /* oldRelfileNodeId */ );
+					   InvalidOid /* oldRelfileNodeId */ ,
+					   classObjectId);
 	}
 
 	/*

src/postgres/src/backend/commands/yb_cmds.c
@@ -1226,7 +1226,8 @@ YBCCreateIndex(const char *indexName,
 			   Oid colocationId,
 			   Oid tablespaceId,
 			   Oid indexRelfileNodeId,
-			   Oid oldRelfileNodeId)
+			   Oid oldRelfileNodeId,
+			   Oid *opclassOids)
 {
 	Oid			namespaceId = RelationGetNamespace(rel);
 	char	   *db_name = get_database_name(YBCGetDatabaseOid(rel));
@@ -1266,7 +1267,8 @@ YBCCreateIndex(const char *indexName,
 														true);
 
 	Assert(amroutine != NULL && amroutine->yb_ambindschema != NULL);
-	amroutine->yb_ambindschema(handle, indexInfo, indexTupleDesc, coloptions);
+	amroutine->yb_ambindschema(handle, indexInfo, indexTupleDesc, coloptions,
+							   opclassOids);
 
 	/* Handle SPLIT statement, if present */
 	if (split_options)

src/postgres/src/include/access/amapi.h
@@ -231,7 +231,8 @@ typedef int64 (*yb_amgetbitmap_function) (IndexScanDesc scan,
 typedef void (*yb_ambindschema_function) (YbcPgStatement handle,
 										  struct IndexInfo *indexInfo,
 										  TupleDesc indexTupleDesc,
-										  int16 *coloptions);
+										  int16 *coloptions,
+										  Oid *opclassOids);
 
 /* end index scan */
 typedef void (*amendscan_function) (IndexScanDesc scan);

src/postgres/src/include/access/ybgin.h
@@ -51,7 +51,8 @@ extern bool ybginvalidate(Oid opclassoid);
 extern void ybginbindschema(YbcPgStatement handle,
 							struct IndexInfo *indexInfo,
 							TupleDesc indexTupleDesc,
-							int16 *coloptions);
+							int16 *coloptions,
+							Oid *opclassIds);
 extern IndexScanDesc ybginbeginscan(Relation rel, int nkeys, int norderbys);
 extern void ybginrescan(IndexScanDesc scan, ScanKey scankey, int nscankeys,
 						ScanKey orderbys, int norderbys);

commit 9401fdcbb78be7cc76fa742886074256b0e8267f

[#26879] YSQL: Fix PG segfault during rewrite on tables with vector indexes
Summary:
The segfault happens because a null pointer is passed for the opclassOids argument to YBCCreateIndex, and subsequently to ybhnswbindcolumnschema. This issue was introduced in commit f8950ffa4f4f913ca20d6652d622d5003eac6ef4.
Fix the segfault by properly constructing and passing the operator class oids.

Specific code changes:
- pg_yb_utils.c: retrieve the operator class oids from the sys cache (pg_index.indclass) in YbIndexSetNewRelfileNode, and pass it to YBCCreateIndex
- ybvectorwrite.c: use YbGetRelfileNodeId(index) instead of the index's oid in ybvectorcopartitionedbuild.

Jira: DB-16293

Test Plan:
Note: rewrites on tables with vector indexes are still flaky (occasionally cause a tserver FATAL at a later stage) due to a different issue, so this revision doesn't add any automated tests.

Manual test:
```
yugabyte=# CREATE EXTENSION vector;
CREATE EXTENSION
yugabyte=# CREATE TABLE IF NOT EXISTS ybarticles (id int, content_vector vector(1536));
CREATE TABLE
yugabyte=# CREATE INDEX ON ybarticles USING ybhnsw (content_vector vector_l2_ops);
CREATE INDEX
yugabyte=# TRUNCATE TABLE ybarticles;
TRUNCATE TABLE
yugabyte=# SELECT * FROM ybarticles;
 id | content_vector
----+----------------
(0 rows)
```

Reviewers: myang, sergei

Reviewed By: myang

Subscribers: yql

Differential Revision: https://phorge.dev.yugabyte.com/D44458

src/postgres/src/backend/utils/misc/pg_yb_utils.c
@@ -7122,10 +7122,13 @@ YbIndexSetNewRelfileNode(Relation indexRel, Oid newRelfileNodeId,
 						 bool yb_copy_split_options)
 {
 	bool		isNull;
+	HeapTuple   indexTuple;
 	HeapTuple	tuple;
 	Datum		reloptions = (Datum) 0;
 	Relation	indexedRel;
 	IndexInfo  *indexInfo;
+	oidvector  *indclass = NULL;
+	Datum       indclassDatum;
 
 	tuple = SearchSysCache1(RELOID,
 							ObjectIdGetDatum(RelationGetRelid(indexRel)));
@@ -7142,6 +7145,19 @@ YbIndexSetNewRelfileNode(Relation indexRel, Oid newRelfileNodeId,
 	indexInfo = BuildIndexInfo(indexRel);
 
 	YbGetTableProperties(indexRel);
+
+	indexTuple = SearchSysCache1(INDEXRELID,
+								 ObjectIdGetDatum(RelationGetRelid(indexRel)));
+	if (!HeapTupleIsValid(indexTuple))
+		elog(ERROR, "cache lookup failed for index %u",
+			 RelationGetRelid(indexRel));
+
+	indclassDatum = SysCacheGetAttr(INDEXRELID, indexTuple,
+									Anum_pg_index_indclass, &isNull);
+	if (!isNull)
+		indclass = (oidvector *) DatumGetPointer(indclassDatum);
+	ReleaseSysCache(indexTuple);
+
 	YBCCreateIndex(RelationGetRelationName(indexRel),
 				   indexInfo,
 				   RelationGetDescr(indexRel),
@@ -7157,7 +7173,7 @@ YbIndexSetNewRelfileNode(Relation indexRel, Oid newRelfileNodeId,
 				   indexRel->rd_rel->reltablespace,
 				   newRelfileNodeId,
 				   YbGetRelfileNodeId(indexRel),
-				   NULL /* opclassOids */ );
+				   indclass ? indclass->values : NULL);
 
 	table_close(indexedRel, ShareLock);
 

src/postgres/third-party-extensions/pgvector/src/ybvector/ybvectorwrite.c
@@ -457,7 +457,7 @@ IndexBuildResult *
 ybvectorcopartitionedbuild(Relation heap, Relation index, struct IndexInfo *indexInfo)
 {
 	HandleYBStatus(YBCPgWaitVectorIndexReady(
-		YBCGetDatabaseOid(index), index->rd_id));
+		YBCGetDatabaseOid(index), YbGetRelfileNodeId(index)));
 
 	IndexBuildResult *result = palloc0(sizeof(IndexBuildResult));
 