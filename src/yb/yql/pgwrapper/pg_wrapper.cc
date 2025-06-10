629ec561f94769ac7ecd99bf33f6ccf8d897894c
Hal Takahara <8877300+mtakahar@users.noreply.github.com>
2025-06-10 08:51:59 -0400

    [BACKPORT 2024.2][#27495] YSQL: add ysql_yb_enable_cbo tserver flag
    Summary:
    Add tserver GFlag counter part of yb_enable_cbo, a GUC parameter introduced by #26868.
    Jira: DB-17048
    
    Original commit: 006c7f38b160a297c3f547f26e4e87d2d62affd4 / D44518
    
    Test Plan: Manual test with both valid and invalid values, jenkins
    
    Reviewers: hsunder, mihnea
    
    Reviewed By: hsunder
    
    Subscribers: yql
    
    Differential Revision: https://phorge.dev.yugabyte.com/D44686

--- src/yb/yql/pgwrapper/pg_wrapper.cc
+++ src/yb/yql/pgwrapper/pg_wrapper.cc
@@ -249,6 +249,10 @@ DEFINE_RUNTIME_PG_PREVIEW_FLAG(bool, yb_enable_optimizer_statistics, false,
 DEFINE_RUNTIME_PG_PREVIEW_FLAG(bool, yb_enable_base_scans_cost_model, false,
     "Enable cost model enhancements");
 
+DEFINE_RUNTIME_PG_FLAG(string, yb_enable_cbo, "legacy_mode",
+    "YSQL cost-based optimizer mode. Allowed values are 'legacy_mode', 'legacy_stats_mode', "
+    "'off', and 'on'");
+
 DEFINE_RUNTIME_PG_FLAG(uint64, yb_fetch_row_limit, 1024,
     "Maximum number of rows to fetch per scan.");
 
@@ -474,6 +478,10 @@ DEFINE_validator(ysql_pg_conf_csv, &ValidateConfCsv);
 
 DEFINE_validator(ysql_hba_conf_csv, &ValidateConfCsv);
 DEFINE_validator(ysql_ident_conf_csv, &ValidateConfCsv);
+
+// Keep the value list in sync with `yb_cost_model_options` in `guc.c`.
+DEFINE_validator(ysql_yb_enable_cbo,
+    FLAG_IN_SET_VALIDATOR("off", "on", "legacy_mode", "legacy_stats_mode"));
 
 namespace {
 // Append any Pg gFlag with non default value, or non-promoted AutoFlag
