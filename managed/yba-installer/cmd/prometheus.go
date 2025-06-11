e016c7add44e2b5d635bcf1a5f20cab0373a0aae
Muthu Chidambaram <mchidambaram@yugabyte.com>
2025-06-11 18:14:33 +0000

    [BACKPORT 2025.1][PLAT-17717] Improving yba-ctl upgrade rollback
    Summary:
    Original commit: 9df82f3cf2f2bdcb37fa57bd977f3793cfdf5432 / D44557
    This diff handles a couple of issues recently seen with rollback, primarily related to filesystem loops and restore failing because postgres is not up. Removes the old CreateSymlink function and replaced with Symlink which properly handles duplicate symlinks. Also reordered the rollback steps to reconfigure first so that the new services are stopped and old services are started before we attempt a restore. We also save off the yba-ctl.yml and yba_installer.state in case any migrations may have changed them, we can restore them and the reconfigure runs properly with an old yba-ctl.
    
    There is still a difficulty with yb_platform_backup.sh as we have to use the old script since we don't know when in the rollback process the new script is available to us. Also, we don't want to rely on the backup script being backwards compatible so it is safer to backup/restore the old YBA with the old script. We may want to add a test that will catch any backup script arguments that are added but not handled in the rollback feature, or change the backup script to ignore any extraneous arguments so we don't have to have version checks in code that pass certain arguments.
    
    Test Plan:
    test upgrade
    have failures in upgrade at specific times (after new postgres and prometheus fail to come up) and ensure rollback still works
    
    Reviewers: dshubin
    
    Reviewed By: dshubin
    
    Subscribers: yugaware
    
    Differential Revision: https://phorge.dev.yugabyte.com/D44723

--- managed/yba-installer/cmd/prometheus.go
+++ managed/yba-installer/cmd/prometheus.go
@@ -458,17 +458,10 @@ func (prom Prometheus) createPrometheusSymlinks() error {
 		}
 	}
 
-	links := []struct {
-		pkgDir, linkDir, binary string
-	}{
-		{promPkg, prom.PromDir, "prometheus"},
-		{promPkg, prom.PromDir, "promtool"},
-		{promPkg, prom.PromDir, "consoles"},
-		{promPkg, prom.PromDir, "console_libraries"},
-	}
-	for _, link := range links {
-		if err := common.CreateSymlink(link.pkgDir, link.linkDir, link.binary); err != nil {
-			log.Error("failed to create symlink for " + link.binary + ": " + err.Error())
+	binaries := []string{"prometheus", "promtool", "consoles", "console_libraries"}
+	for _, binary := range binaries {
+		if err := common.Symlink(fmt.Sprintf("%s/%s", promPkg, binary), fmt.Sprintf("%s/%s", prom.PromDir, binary)); err != nil {
+			log.Error("failed to create symlink for " + binary + ": " + err.Error())
 			return err
 		}
 	}
@@ -486,8 +479,8 @@ func (prom Prometheus) createPrometheusSymlinks() error {
 func (prom Prometheus) createDataSymlinks() error {
 	if common.HasSudoAccess() {
 		// for root the log file is in /var/log in case of SELinux
-		if err := common.CreateSymlink(prom.LogDir,
-			filepath.Join(common.GetBaseInstall(), "data/logs"), "prometheus.log"); err != nil {
+		if err := common.Symlink(fmt.Sprintf("%s/prometheus.log", prom.LogDir),
+			fmt.Sprintf("%s/prometheus.log", filepath.Join(common.GetBaseInstall(), "data/logs"))); err != nil {
 			return err
 		}
 	}
