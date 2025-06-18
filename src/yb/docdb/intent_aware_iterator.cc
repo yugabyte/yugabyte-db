1d80005a21157bd42050615aeca130ff3ff140ef
Sergei Politov <spolitov@gmail.com>
2025-06-18 11:21:32 +0300

    [BACKPORT 2024.2.3] [#27641] DocDB: Fix seek key invalidation when Seek is invoked from SeekToLatestDocKeyInternal
    Summary:
    In `IntentAwareIterator::SeekToLatestDocKeyInternal`, the iterator attempts to position itself on the latest version of the current key.
    
       - In some scenarios the method retrieves the current key from the underlying regular DB iterator as a `Slice` (`iter_.key()`).
       - This `Slice` points to internal memory managed by the underlying iterator.
       - The method performs a `Seek(iter_.key())` that consists of two phases: seek on regular DB and seek on intents DB.
       - Seek on regular DB could invalidate memory referenced by the `Slice`.
       - In this scenario Seek on intents DB is invoked with invalid Slice.
       - As result intents DB iterator could be positioned on a wrong key.
       - It could cause wrong query results returned to the user.
    
    Important note that internal memory invalidation happens in scenario where key+doc_hybrid_time overflows key buffer in `DBIter`.
    This buffer is used only when entry is stored on disk, when entry is stored in MemTable this buffer is not used and direct reference to MemTable entry is used.
    Since we seek for key that is already in regular DB, the overflow could happen only if key was updated and new doc_hybrid_time is encoded using more bytes than original one.
    For instance it happens when old entry (current position) was written w/o transaction and doc_hybrid_time has 0 write time which is encoded as 1 byte.
    While updated entry was written as a part of transaction with multiple records, and has write time which encoded using 2 bytes.
    So it is important that encoded original entry key+doc_hybrid_time is encoded as 64 bytes, while updated entry key+doc_hybrid_time is encoded as 65 bytes for overflow to happen.
    
    Overflow is necessary for key invalidation, since we seek to the same key as the current position. Simple update of the key buffer with new entry results in situation where key part remains the same, while doc_hybrid_time is updated. Since we use only key part for `Seek`, the slice content remain the same.
    
    Fixed by copying key to special buffer when invoking `Seek` from `SeekToLatestDocKeyInternal`.
    Original commit: bc0b7637977756a9a6c91374f1ecca9ccf6839ae / D44784
    Jira: DB-17228
    
    Test Plan: PgSingleTServerTest.IterKeyInvalidationDuringBackwardScan
    
    Reviewers: arybochkin, timur
    
    Reviewed By: arybochkin
    
    Subscribers: yql, ybase, patnaik.balivada
    
    Tags: #jenkins-ready
    
    Differential Revision: https://phorge.dev.yugabyte.com/D44856

--- src/yb/docdb/intent_aware_iterator.cc
+++ src/yb/docdb/intent_aware_iterator.cc
@@ -249,11 +249,17 @@ void IntentAwareIterator::Seek(Slice key, Full full) {
 
   SeekTriggered();
 
+#ifndef NDEBUG
+  KeyBuffer original_key(key);
+#endif
   SkipFutureRecords<Direction::kForward>(ROCKSDB_SEEK(&iter_, key));
   if (intent_iter_.Initialized()) {
     if (!SetIntentUpperbound()) {
       return;
     }
+#ifndef NDEBUG
+    CHECK_EQ(original_key.AsSlice(), key);
+#endif
     if (full) {
       seek_buffer_.Assign(key, StrongWriteSuffix(key));
       key = seek_buffer_.AsSlice();
@@ -664,8 +670,9 @@ void IntentAwareIterator::SeekToLatestSubDocKeyInternal() {
   if (!HandleStatus(doc_ht)) {
     return;
   }
-  subdockey_slice.remove_suffix(1);
-  Seek(subdockey_slice);
+  // subdockey_slice could reference iter_.key(), which is invalidated during Seek.
+  KeyBuffer subdockey(subdockey_slice.WithoutSuffix(1));
+  Seek(subdockey.AsSlice());
 }
 
 void IntentAwareIterator::SeekToLatestDocKeyInternal() {
@@ -676,7 +683,9 @@ void IntentAwareIterator::SeekToLatestDocKeyInternal() {
   if (!HandleStatus(dockey_size)) {
     return;
   }
-  Seek(Slice(subdockey_slice.data(), *dockey_size));
+  // subdockey_slice could reference iter_.key(), which is invalidated during Seek.
+  KeyBuffer subdockey(subdockey_slice.Prefix(*dockey_size));
+  Seek(subdockey.AsSlice());
 }
 
 void IntentAwareIterator::Revalidate() {
