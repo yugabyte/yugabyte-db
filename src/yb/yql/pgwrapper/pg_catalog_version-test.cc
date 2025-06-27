30cebd1ede8898ebaeb6c7628741bf2b32344fe4
Minghui Yang <myang@yugabyte.com>
2025-06-27 16:45:00 +0000

    [BACKPORT 2025.1][#27822] YSQL: improve TabletServer::SetTserverCatalogMessageList to avoid full refresh
    Summary:
    In one of the runs of `SqlCrossDBLoadWithDDL` test I found an unexpected full
    catalog cache refresh caused by the commit
    a260932fe7aba6653b17812c7a8f8b6b6b2e4633.
    
    (1) PG backend 713239 is on version 360
    (2) It made a RPC which returned "schema version mismatch"
    (3) In `YBPrepareCacheRefreshIfNeeded`, it reads the latest master catalog
    version 361, which has not yet propagated via heartbeat to this tserver yet
    (4) PG backend 713239 calls `YbWaitForSharedCatalogVersionToCatchup` to wait for
    version 361 to propagate to show up in shared memory
    (5) Another PG backend on the same node executed a DDL that incremented the catalog version to
    362, due to the change in commit a260932fe7aba6653b17812c7a8f8b6b6b2e4633, it
    sets the new version 362 in local tserver, causing 362 to show up in shared
    memory before 361 arrives
    (6) PG backend 713239 breaks out of `YbWaitForSharedCatalogVersionToCatchup`
    because 362 > 361
    (7) PG backend is ready to do catalog refresh, it calls
    `YBCGetTserverCatalogMessageLists` which makes a RPC to local tserver asking for
    invalidation messages of version 361 and 362.
    (8) The tserver has not received the version 361 yet, even though it has version
    362. So the request cannot be fulfilled.
    (9) PG backend 713239 performs a full catalog cache refresh.
    
    To improve this situation, I made a change to
    `TabletServer::SetTserverCatalogMessageList` so that if there is a gap between
    the max existing catalog version and the new catalog version, then we do not set
    the new catalog version and return status TryAgain. At the PG side, the DDL
    session calls `YbWaitForSharedCatalogVersionToCatchup(new_version)` to wait for
    the usual heartbeat framework to propagate the `new_version` and its messages
    back to this tserver. In this way step (6) will not break out of the wait loop
    until 361 has propagated back. Then it will be able to do incremental catalog
    cache refresh.
    
    Also made a small logging improvement to shorten a long log line.
    
    Added a new unit test that would fail due to the above event sequence before the fix.
    Jira: DB-17417
    
    Original commit: 8cfffc90d6e1f6533bece1df8d9558a076a8a412 / D45099
    
    Test Plan:
    Repeat the following test steps a few times until seeing the desired logs that
    indicating the scenario described in the summary has happened.
    
    (1) Start a RF-3 local dev vm cluster
    ```
    ./bin/yb-ctl create --rf 3
    
    ```
    (2) Run the `SqlCrossDBLoadWithDDL` test:
    ```
    /usr/lib/jvm/java-17-openjdk-17.0.15.0.6-2.el8.x86_64/bin/java -jar $HOME/code/yb-stress-test/tools/sample-app/target/yb-stress-sample-apps.jar --workload SqlCrossDBLoadWithDDL --num_of_tables_in_db 1 --uuid be8ef4cb-ec72-4679-a488-5af1eb848d39 --uuid_marker be8ef4cb-ec72-4679-a488-5af1eb848d39 --num_writes -1 --num_reads -1 --num_threads_write 3 --num_threads_read 3 --num_unique_keys 2000000000000000 --num_value_columns 30 --default_postgres_database postgres --use_datatypes true --nodes 127.0.0.1:5433,127.0.0.2:5433,127.0.0.3:5433 --username yugabyte --per_db_catalog_mode true --batch_size 3 --num_of_non_colocated_databases 1 --num_of_colocated_databases 0  --num_of_parallel_ddls 1 --run_time 3600
    ```
    
    Observed logs:
    ```
    postgresql-2025-07-02_010724.log:2025-07-02 01:36:05.996 UTC [4081300] LOG:  failed to set catalog message list: new catalog version 1225 has a gap from 1223
    postgresql-2025-07-02_014019.log:2025-07-02 01:42:11.856 UTC [4121605] LOG:  failed to set catalog message list: new catalog version 1487 has a gap from 1485
    ```
    No full refresh is done.
    
    Also manually performed tpcc perf test locally with auto-analyze on/off, there
    was no regression observed in terms of increased latency percentage, with or
    without this diff.
    
    Reviewers: kfranz, sanketh, mihnea
    
    Reviewed By: sanketh
    
    Subscribers: yql
    
    Differential Revision: https://phorge.dev.yugabyte.com/D45195

--- src/yb/yql/pgwrapper/pg_catalog_version-test.cc
+++ src/yb/yql/pgwrapper/pg_catalog_version-test.cc
@@ -558,7 +558,6 @@ class PgCatalogVersionTest : public LibPqTestBase {
 
     int count = 0;
     for (const auto& metric : json_metrics) {
-      // Should see one full refresh.
       if (metric.name.find("CatCacheRefresh") != std::string::npos) {
         ++count;
         LOG(INFO) << "CatCacheRefresh count: " << metric.value;
@@ -568,7 +567,6 @@ class PgCatalogVersionTest : public LibPqTestBase {
           ASSERT_EQ(metric.value, num_full_refreshes);
         }
       }
-      // Should not see any incremental refresh.
       if (metric.name.find("CatCacheDeltaRefresh") != std::string::npos) {
         ++count;
         LOG(INFO) << "CatCacheDeltaRefresh count: " << metric.value;
@@ -3028,6 +3026,78 @@ TEST_F(PgCatalogVersionTest, InvalMessageMinimalRetention) {
   VerifyCatCacheRefreshMetricsHelper(
       0 /* num_full_refreshes */, 1 /* num_delta_refreshes */,
       std::make_pair(false, true) /* at_least */);
+}
+
+// https://github.com/yugabyte/yugabyte-db/issues/27822
+TEST_F(PgCatalogVersionTest, InvalMessageWaitOnVersionGap) {
+  RestartClusterWithInvalMessageEnabled(
+      { "--heartbeat_interval_ms=10000",
+        "--ysql_pg_conf_csv=log_statement=all" });
+  // Create a test table.
+  auto conn = ASSERT_RESULT(ConnectToDB(kYugabyteDatabase));
+  ASSERT_OK(conn.Execute("CREATE TABLE test_table(id int)"));
+  ASSERT_OK(conn.Execute("INSERT INTO test_table VALUES(1)"));
+
+  // Execute a pair of DDLs to get some initial catalog version/inval messages
+  // propagated to all tservers.
+  ASSERT_OK(conn.Execute("ANALYZE"));
+  ASSERT_OK(conn.Execute("ANALYZE"));
+  SleepFor(12s);
+
+  // conn1 connects to node 1
+  auto conn1 = ASSERT_RESULT(ConnectToDB(kYugabyteDatabase));
+  auto v = ASSERT_RESULT(GetCatalogVersion(&conn1));
+  ASSERT_EQ(v, 3);
+  // At this time, conn1's local catalog version should be 3.
+  auto result = ASSERT_RESULT(conn1.FetchAllAsString("SELECT id FROM test_table"));
+  ASSERT_EQ(result, "1");
+
+  std::atomic<bool> conn2_executed_ddl = false;
+  TestThreadHolder thread_holder;
+  thread_holder.AddThreadFunctor([this, &conn2_executed_ddl, &stop = thread_holder.stop_flag()] {
+    // conn2 connects to node 2
+    pg_ts = cluster_->tablet_server(1);
+    auto conn2 = ASSERT_RESULT(ConnectToDB(kYugabyteDatabase));
+    // Execute a DDL on conn2 to cause catalog version bump to v+1.
+    ASSERT_OK(conn2.Execute("ALTER TABLE test_table ADD COLUMN c2 INT"));
+    conn2_executed_ddl = true;
+    while (!stop.load(std::memory_order_acquire)) { }
+  });
+  thread_holder.AddThreadFunctor([this, &conn2_executed_ddl, &stop = thread_holder.stop_flag()] {
+    // Ensure conn3 sees catalog version v+1.
+    while (!conn2_executed_ddl.load(std::memory_order_acquire)) {}
+
+    // Start conn3 connects to node 1 before the new catalog version v+1 has propagated to node 1.
+    pg_ts = cluster_->tablet_server(0);
+    auto conn3 = ASSERT_RESULT(ConnectToDB(kYugabyteDatabase));
+    // Execute a DDL on conn3 to cause catalog version bump again to new catalog version v+2.
+    // This DDL should not set version v+2 in tserver at node 1 because tserver 1 has
+    // only seen catalog version 1, the missing version v+1 is yet to propagate to tserver 1
+    // via heartbeat response. Setting new catalog version v+2 on tserver 1 would have cause
+    // a gap between version 1 and version v+2, which can lead to the next query on conn1
+    // to trigger a full catalog cache refresh because it would need catalog versions v+1 and
+    // v+2 and their invalidation messages in order to do incremental catalog cache refresh.
+    // After fixing GHI 27822, the DDL on conn3 will not set catalog version v+2 in tserver 1,
+    // instead it waits for version v+2 to propagate to tserver 1.
+
+    // Let the next query on conn1 starts first.
+    SleepFor(100ms);
+
+    ASSERT_OK(conn3.Execute("ALTER TABLE test_table DROP COLUMN c2"));
+    while (!stop.load(std::memory_order_acquire)) { }
+  });
+
+  // Ensure conn1 sees new table schema.
+  while (!conn2_executed_ddl.load(std::memory_order_acquire)) {}
+
+  // Execute query on conn1 again.
+  result = ASSERT_RESULT(conn1.FetchAllAsString("SELECT id FROM test_table"));
+  ASSERT_EQ(result, "1");
+  thread_holder.Stop();
+
+  // Verify that the incremental catalog cache refresh happened on conn3.
+  // Before the fix of GHI 27822, there would be a full catalog cache refresh.
+  VerifyCatCacheRefreshMetricsHelper(0 /* num_full_refreshes */, 1 /* num_delta_refreshes */);
 }
 
 } // namespace pgwrapper
