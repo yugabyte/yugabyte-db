47f6797b59737d117641cdd313073a534e7b1d8a
Basava <basava726.k@gmail.com>
2025-06-30 17:36:30 -0500

    [BACKPORT 2025.1][#27810] DocDB: Fix potential high memory consumption issue for pg_locks
    Summary:
    Original commit: 3960a250194701604ffbf3b8318262695d76b7d7 / D45028
    In one of the customer envs, we saw `pg_locks` controbuting to high memory with the following stack traces.
    ```
    Estimated bytes: 22817013760, estimated count: 9, sampled bytes: 22817013760, sampled count: 9, stack:
    ;;tcmalloc::tcmalloc_internal::SampleifyAllocation<>()
    ;;tcmalloc::tcmalloc_internal::slow_alloc_large<>()
    ;;yb::tablet::Tablet::GetLockStatus()
    ;;yb::tserver::TabletServiceImpl::GetLockStatus()
    
    Estimated bytes: 1421082624, estimated count: 43368, sampled bytes: 43581440, sampled count: 1330, stack:
    ;;tcmalloc::tcmalloc_internal::SampleifyAllocation<>()
    ;;tcmalloc::tcmalloc_internal::slow_alloc_small<>()
    ;;rocksdb::UncompressBlockContents()
    ;;rocksdb::ReadBlockContents()
    ;;rocksdb::block_based_table::ReadBlockFromFile()
    ;;rocksdb::BlockBasedTable::ReadBlockFromFileAndMaybePutToCache()
    ;;rocksdb::BlockBasedTable::BlockEntryIteratorState::NewSecondaryIterator()
    ;;rocksdb::(anonymous namespace)::TwoLevelIterator::InitDataBlock()
    ;;rocksdb::(anonymous namespace)::TwoLevelIterator::Seek()
    ;;rocksdb::MergingIteratorBase<>::Seek()
    ;;rocksdb::DBIter::Seek()
    ;;yb::tablet::Tablet::GetLockStatus()
    ;;yb::tserver::TabletServiceImpl::GetLockStatus()
    
    Estimated bytes: 1112997888, estimated count: 33966, sampled bytes: 34177024, sampled count: 1043, stack:
    ;;tcmalloc::tcmalloc_internal::SampleifyAllocation<>()
    ;;tcmalloc::tcmalloc_internal::slow_alloc_small<>()
    ;;rocksdb::UncompressBlockContents()
    ;;rocksdb::ReadBlockContents()
    ;;rocksdb::block_based_table::ReadBlockFromFile()
    ;;rocksdb::BlockBasedTable::ReadBlockFromFileAndMaybePutToCache()
    ;;rocksdb::BlockBasedTable::BlockEntryIteratorState::NewSecondaryIterator()
    ;;rocksdb::(anonymous namespace)::TwoLevelIterator::InitDataBlock()
    ;;rocksdb::(anonymous namespace)::TwoLevelIterator::DoSkipEmptyDa
    taBlocksForward();;rocksdb::MergingIteratorBase<>::Next()
    ;;rocksdb::DBIter::Next();;yb::tablet::Tablet::GetLockStatus()
    ;;yb::tserver::TabletServiceImpl::GetLockStatus()
    ```
    
    Stacks 2 & 3 can be avoided by explicitly setting iterator read options to not fill the cache. Cause for stack 1 is still not clear. It could have been from the local vector - `txn_intent_keys` -  where we store the value section of the interested reverse index keys from intentsdb. Note that this part doesn't respect `max_txn_locks_per_tablet` set in the request and reads all intents of the txn. So it is better we limit reading of intents to `max_txn_locks_per_tablet` per txn as well, since we anyways don't populate records corresponding to the additional ones in the response.
    
    This revision addresses the issue by:
    1. Explicitly setting the interator read options to not cache the contents in block cache
    2. Limiting the number of intents records we read per txn
    Jira: DB-17405
    
    Test Plan: ./yb_build.sh --cxx-test='TEST_F(PgGetLockStatusTest, TestGetLockStatusLimitNumTxnLocks) {'
    
    Reviewers: yyan, rthallam
    
    Reviewed By: yyan
    
    Subscribers: ybase
    
    Differential Revision: https://phorge.dev.yugabyte.com/D45121

--- src/yb/tablet/tablet.cc
+++ src/yb/tablet/tablet.cc
@@ -4964,6 +4964,7 @@ Status Tablet::GetLockStatus(const std::map<TransactionId, SubtxnSet>& transacti
 
   TransactionLockInfoManager lock_info_manager(tablet_lock_info);
   rocksdb::ReadOptions read_options;
+  read_options.fill_cache = false;
   auto intent_iter = std::unique_ptr<rocksdb::Iterator>(intents_db_->NewIterator(read_options));
   intent_iter->SeekToFirst();
   // It could happen that the tablet gets a lock status request with the transactions field unset,
@@ -5014,12 +5015,15 @@ Status Tablet::GetLockStatus(const std::map<TransactionId, SubtxnSet>& transacti
       }
 
       // Scan the transaction's corresponding reverse index section.
-      while (intent_iter->Valid() && intent_iter->key().compare_prefix(reverse_key) == 0) {
+      uint32_t txn_intents_count = 0;
+      while (intent_iter->Valid() && intent_iter->key().compare_prefix(reverse_key) == 0 &&
+             (!max_txn_locks_per_tablet || txn_intents_count <= max_txn_locks_per_tablet)) {
         DCHECK_EQ(intent_iter->key()[0], KeyEntryTypeAsChar::kTransactionId);
         // We should only consider intents whose value is within the tablet's key bounds.
         // Else, we would observe duplicate results in case of tablet split.
         if (key_bounds_.IsWithinBounds(intent_iter->value())) {
           txn_intent_keys.emplace_back(intent_iter->value());
+          ++txn_intents_count;
         }
         intent_iter->Next();
       }
