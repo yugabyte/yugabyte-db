c0c036452b5e6d95bd68ea84c16d2409b4b998ab
Hal Takahara <8877300+mtakahar@users.noreply.github.com>
2025-07-09 11:25:49 -0400

    [BACKPORT 2025.1][#26864] YSQL: add GUC_EXPLAIN to plan-influencing yb GUC parameters
    Summary:
    Add GUC_EXPLAIN flag to YB-specific GUC parameters affecting query planning, so modified values are shown in EXPLAIN output with the SETTINGS option.
    
    Method of Identification:
    1. Extract the GUC parameters from `guc.c` where:
      * Name starts with `yb_`
      * Doesn't have `GUC_EXPLAIN`
    2. From the extracted list, remove the ones that is not referenced in any of those files:
      * src/postgres/src/include/optimizer/*.h
      * src/postgres/src/backend/optimizer/*/*.c
      * src/postgres/src/backend/access/yb_access/yb_scan.c
      * src/postgres/src/backend/access/yb_access/yb_lsm.c
      * src/postgres/src/backend/commands/explain.c
    3. Eye-ball the list and exclude the ones that won't affect planning.
    
    The finalists for `GUC_EXPLAIN`:
    ```
    yb_bnl_batch_size
    yb_bnl_optimize_first_batch
    yb_bypass_cond_recheck
    yb_enable_base_scans_cost_model
    yb_enable_batchednl
    yb_enable_bitmapscan
    yb_enable_distinct_pushdown
    yb_enable_geolocation_costing
    yb_enable_optimizer_statistics
    yb_enable_parallel_append
    yb_fetch_row_limit
    yb_fetch_size_limit
    yb_network_fetch_cost
    yb_parallel_range_rows
    yb_parallel_range_size
    yb_prefer_bnl
    yb_pushdown_is_not_null (hidden)
    yb_pushdown_strict_inequality (hidden)
    ```
    
    Used in the files listed above but excluded because they do not impact the plan:
    ```
    yb_enable_memory_tracking
    yb_enable_planner_trace
    yb_explain_hide_non_deterministic_fields
    yb_explicit_row_locking_batch_size
    yb_hinted_uids
    yb_lock_pk_single_rpc
    ```
    Jira: DB-16279
    
    Original commit: 62d4809fed0adf80542ea3b68d286ae33d0f49d1 / D45159
    
    Test Plan: ./yb_build.sh release --java-test 'org.yb.pgsql.TestPgRegressPlanner'
    
    Reviewers: mihnea, gkukreja, william.mckenna
    
    Reviewed By: gkukreja
    
    Subscribers: yql
    
    Differential Revision: https://phorge.dev.yugabyte.com/D45296

--- src/postgres/src/test/regress/sql/yb.orig.optimizer_guc.sql
+++ src/postgres/src/test/regress/sql/yb.orig.optimizer_guc.sql
@@ -1,3 +1,7 @@
+--
+-- test yb_enable_cbo and legacy counterpart value sync
+--
+
 reset yb_enable_cbo;
 reset yb_enable_base_scans_cost_model;
 reset yb_enable_optimizer_statistics;
@@ -35,6 +39,7 @@ show yb_enable_optimizer_statistics;
 
 
 -- turn on/off old parameters
+
 set yb_enable_base_scans_cost_model = off;
 show yb_enable_cbo;
 show yb_enable_base_scans_cost_model;
@@ -78,5 +83,61 @@ show yb_enable_cbo;
 
 
 -- error
+
 set yb_enable_cbo = oui;
 
+
+--
+-- test GUC_EXPLAIN flag
+--
+
+reset all;
+
+explain (settings, costs off)
+select * from pg_class;
+
+set yb_enable_cbo = on;
+
+explain (settings, costs off)
+select * from pg_class;
+
+set yb_enable_cbo = legacy_stats_mode;
+
+explain (settings, costs off)
+select * from pg_class;
+
+set yb_bnl_optimize_first_batch = off;
+set yb_prefer_bnl = off;
+set yb_enable_batchednl = off;
+set yb_enable_geolocation_costing = off;
+set yb_pushdown_strict_inequality = off;
+set yb_pushdown_is_not_null = off;
+set yb_enable_distinct_pushdown = off;
+set yb_bypass_cond_recheck = off;
+set yb_enable_parallel_append = on;
+set yb_enable_bitmapscan = on;
+set yb_enable_base_scans_cost_model = on;
+set yb_enable_optimizer_statistics = on;
+set yb_bnl_batch_size = 1;
+set yb_fetch_row_limit = 0;
+set yb_fetch_size_limit = '4GB';
+set yb_parallel_range_rows = 10;
+set yb_parallel_range_size = '1kB';
+set yb_network_fetch_cost = 100;
+
+explain (settings, costs off)
+select * from pg_class;
+
+
+-- reset all then try a few yb_ parameters that won't affect plans
+
+reset all;
+
+explain (settings, costs off)
+select * from pg_class;
+
+set yb_enable_memory_tracking = off;
+set yb_enable_planner_trace = on;
+
+explain (settings, costs off)
+select * from pg_class;
