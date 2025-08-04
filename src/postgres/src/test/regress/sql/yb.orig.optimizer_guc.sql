2eb87bbf7b79e6cff7b8301142c0f5a1697aaecd
Hal Takahara <8877300+mtakahar@users.noreply.github.com>
2025-08-04 13:08:38 -0400

    [#28156] YSQL: Fix incorrect yb_enable_cbo value when legacy GUCs are both ON
    Summary:
    `yb_enable_cbo` was introduced in 4dbb628f538b435863ee1f6de482776a3da8efab to replace the legacy GUCs `yb_enable_base_scans_cost_model` and `yb_enable_optimizer_statistics`.
    
    This commit fixes an issue where, if both legacy GUCs were ON, `yb_enable_cbo` was incorrectly set to `legacy_stats_mode` when `yb_enable_base_scans_cost_model` was enabled first. It should have been set to `on`.
    Jira: DB-17803
    
    Test Plan: ./yb_build.sh release --java-test 'org.yb.pgsql.TestPgRegressPlanner'
    
    Reviewers: mihnea, gkukreja
    
    Reviewed By: gkukreja
    
    Subscribers: yql
    
    Differential Revision: https://phorge.dev.yugabyte.com/D45795

--- src/postgres/src/test/regress/sql/yb.orig.optimizer_guc.sql
+++ src/postgres/src/test/regress/sql/yb.orig.optimizer_guc.sql
@@ -2,68 +2,114 @@
 -- test yb_enable_cbo and legacy counterpart value sync
 --
 
+set client_min_messages = warning;
+drop function if exists check_optimizer_guc;
+
+create function check_optimizer_guc() returns text
+language plpgsql as
+$$
+declare
+    cbo text;
+    bscm text;
+    stats text;
+    vals text;
+    valid_vals text[] := '{"(on, on, on)"}'::text[] ||
+               		 '{"(on, on, off)"}'::text[] ||
+                         '{"(legacy_mode, off, off)"}'::text[] ||
+                         '{"(off, off, off)"}'::text[] || /* legacy ignore stats */
+                         '{"(legacy_stats_mode, off, on)"}'::text[];
+begin
+    execute 'show yb_enable_cbo' into cbo;
+    execute 'show yb_enable_base_scans_cost_model' into bscm;
+    execute 'show yb_enable_optimizer_statistics' into stats;
+    vals := '(' || cbo || ', ' || bscm || ', ' || stats || ')';
+    if vals = any(valid_vals) then
+	return vals;
+    else
+	return '*** Invalid value combination: ' || vals;
+    end if;
+end;
+$$;
+
+
 reset yb_enable_cbo;
 reset yb_enable_base_scans_cost_model;
 reset yb_enable_optimizer_statistics;
 
-show yb_enable_cbo;
-show yb_enable_base_scans_cost_model;
-show yb_enable_optimizer_statistics;
+select check_optimizer_guc() "(cbo, bscm, stats)";
 
 -- change yb_enable_cbo
 
 set yb_enable_cbo = on;
-show yb_enable_cbo;
-show yb_enable_base_scans_cost_model;
-show yb_enable_optimizer_statistics;
-
-set yb_enable_cbo = off;
-show yb_enable_cbo;
-show yb_enable_base_scans_cost_model;
-show yb_enable_optimizer_statistics;
-
-set yb_enable_cbo = legacy_mode;
-show yb_enable_cbo;
-show yb_enable_base_scans_cost_model;
-show yb_enable_optimizer_statistics;
+select check_optimizer_guc() "(cbo, bscm, stats)";
 
 set yb_enable_cbo = legacy_stats_mode;
-show yb_enable_cbo;
-show yb_enable_base_scans_cost_model;
-show yb_enable_optimizer_statistics;
+select check_optimizer_guc() "(cbo, bscm, stats)";
+
+set yb_enable_cbo = on;
+select check_optimizer_guc() "(cbo, bscm, stats)";
+
+set yb_enable_cbo = legacy_mode;
+select check_optimizer_guc() "(cbo, bscm, stats)";
+
+set yb_enable_cbo = on;
+select check_optimizer_guc() "(cbo, bscm, stats)";
 
 set yb_enable_cbo = off;
-show yb_enable_cbo;
-show yb_enable_base_scans_cost_model;
-show yb_enable_optimizer_statistics;
+select check_optimizer_guc() "(cbo, bscm, stats)";
+
+set yb_enable_cbo = legacy_mode;
+select check_optimizer_guc() "(cbo, bscm, stats)";
+
+set yb_enable_cbo = off;
+select check_optimizer_guc() "(cbo, bscm, stats)";
+
+set yb_enable_cbo = legacy_stats_mode;
+select check_optimizer_guc() "(cbo, bscm, stats)";
+
+set yb_enable_cbo = legacy_mode;
+select check_optimizer_guc() "(cbo, bscm, stats)";
+
+set yb_enable_cbo = legacy_stats_mode;
+select check_optimizer_guc() "(cbo, bscm, stats)";
+
+set yb_enable_cbo = off;
+select check_optimizer_guc() "(cbo, bscm, stats)";
 
 
 -- turn on/off old parameters
 
-set yb_enable_base_scans_cost_model = off;
-show yb_enable_cbo;
-show yb_enable_base_scans_cost_model;
-show yb_enable_optimizer_statistics;
-set yb_enable_optimizer_statistics = off;
-show yb_enable_cbo;
-show yb_enable_base_scans_cost_model;
-show yb_enable_optimizer_statistics;
+-- (bscm, stats) off, off -> stats = on
 set yb_enable_optimizer_statistics = on;
-show yb_enable_cbo;
-show yb_enable_base_scans_cost_model;
-show yb_enable_optimizer_statistics;
+select check_optimizer_guc() "(cbo, bscm, stats)";
+
+-- (bscm, stats) off, on -> bscm = on
 set yb_enable_base_scans_cost_model = on;
-show yb_enable_cbo;
-show yb_enable_base_scans_cost_model;
-show yb_enable_optimizer_statistics;
+select check_optimizer_guc() "(cbo, bscm, stats)";
+
+-- (bscm, stats) on, on -> bscm = off
 set yb_enable_base_scans_cost_model = off;
-show yb_enable_cbo;
-show yb_enable_base_scans_cost_model;
-show yb_enable_optimizer_statistics;
+select check_optimizer_guc() "(cbo, bscm, stats)";
+
+-- (bscm, stats) off, on -> stats = off
 set yb_enable_optimizer_statistics = off;
-show yb_enable_cbo;
-show yb_enable_base_scans_cost_model;
-show yb_enable_optimizer_statistics;
+select check_optimizer_guc() "(cbo, bscm, stats)";
+
+-- (bscm, stats) off, off -> bscm = on
+set yb_enable_base_scans_cost_model = on;
+select check_optimizer_guc() "(cbo, bscm, stats)";
+
+-- (bscm, stats) on, off -> stats = on
+set yb_enable_optimizer_statistics = on;
+select check_optimizer_guc() "(cbo, bscm, stats)";
+
+-- (bscm, stats) on, on -> stats = off
+set yb_enable_optimizer_statistics = off;
+select check_optimizer_guc() "(cbo, bscm, stats)";
+
+-- (bscm, stats) on, off -> bscm = off
+set yb_enable_base_scans_cost_model = off;
+select check_optimizer_guc() "(cbo, bscm, stats)";
 
 
 -- boolean aliases
