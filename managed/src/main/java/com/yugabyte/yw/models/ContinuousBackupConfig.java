e3ecd2606573da6a77e46c23da70b08f4759ecb9
Jethro Mak <88681329+Jethro-M@users.noreply.github.com>
2025-08-29 22:17:09 -0400

    [PLAT-18449]  Fix NPE for continuous backups when lastBackup is null
    Summary:
    The lastBackup field is declared as a primitive long type, but in
    the db it is a nullable column. When we try to query and hit a
    record where last_backup is null, we get a NPE "Cannot
    invoke "java.lang.Long.longValue()" because "o" is null".
    
    In `ContinuousBackupMapper.java` we're already storing
    `lastBackup` into a `Long` and then checking for null, this
    diff just updates the type in `ContinuousBackupConfig.java`.
    
    Test Plan:
    Create a continuous backup configuration successfully.
    Issue a successful get request for the backup configuration prior to
    any backups being performed (so lastbackup is still `null`).
    {F388497}
    
    Reviewers: muthu
    
    Reviewed By: muthu
    
    Differential Revision: https://phorge.dev.yugabyte.com/D46400

--- managed/src/main/java/com/yugabyte/yw/models/ContinuousBackupConfig.java
+++ managed/src/main/java/com/yugabyte/yw/models/ContinuousBackupConfig.java
@@ -58,13 +58,13 @@ public class ContinuousBackupConfig extends Model {
   private String storageLocation;
 
   @ApiModelProperty(value = "the last time a successful backup occurred")
-  private long lastBackup;
+  private Long lastBackup;
 
   @Transactional
   public static ContinuousBackupConfig create(
-      UUID uuid, long frequency, TimeUnit timeUnit, int numBackups, String backupDir) {
+      UUID storageConfigUUID, long frequency, TimeUnit timeUnit, int numBackups, String backupDir) {
     ContinuousBackupConfig cbConfig = new ContinuousBackupConfig();
-    cbConfig.storageConfigUUID = uuid;
+    cbConfig.storageConfigUUID = storageConfigUUID;
     cbConfig.frequency = frequency;
     cbConfig.frequencyTimeUnit = timeUnit;
     cbConfig.numBackupsToRetain = numBackups;
