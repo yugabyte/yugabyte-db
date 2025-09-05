yugabyte/yugabyte-db

commit 0dd2f62eb8aa51be520c69f6dfae8276b26d14cc

[#28348] YSQL: delete inval messages for dropped database
Summary:
When a database is dropped, we delete its row from pg_yb_catalog_version table. However currently we
do not delete the invalidation messages associated with the dropped database from the table
pg_yb_invalidation_messages. This causes garbage invalidation messages left over in the table
pg_yb_invalidation_messages table. Overtime it can cause reading the table to become slower and
slower after creating and dropped many databases.

Note that if there is a global-impact DDL that increments the catalog version, then as part of the
execution of the function `yb_increment_all_db_catalog_versions_with_inval_messages` we do
have a chance to delete old messages irrespective of db_oid.

```
      DELETE FROM pg_yb_invalidation_messages WHERE
        message_time < extract(epoch FROM now())::BIGINT - expiration_sec;
```

I made a change to delete the invalidation messages associated with db_oid for a dropped database.
A new unit test is added that would fail before the fix.
Jira: DB-18033

Test Plan: ./yb_build.sh release --cxx-test pg_catalog_version-test --gtest_filter PgCatalogVersionTest.InvalMessageDropDatabase

Reviewers: kfranz, sanketh, #db-approvers

Reviewed By: kfranz, #db-approvers

Subscribers: yql

Differential Revision: https://phorge.dev.yugabyte.com/D46258

src/postgres/src/backend/catalog/yb_catalog/yb_catalog_version.c
@@ -23,8 +23,10 @@
 #include "catalog/pg_proc.h"
 #include "catalog/pg_type.h"
 #include "catalog/pg_yb_catalog_version.h"
+#include "catalog/pg_yb_invalidation_messages.h"
 #include "catalog/schemapg.h"
 #include "catalog/yb_catalog_version.h"
+#include "executor/spi.h"
 #include "executor/ybExpr.h"
 #include "executor/ybModifyTable.h"
 #include "miscadmin.h"
@@ -760,6 +762,57 @@ YbCreateMasterDBCatalogVersionTableEntry(Oid db_oid)
 }
 
 void
+YbDeleteMasterDBInvalidationMessagesTableEntries(Oid db_oid)
+{
+	/*
+	 * Connect to SPI manager
+	 */
+	if (SPI_connect() != SPI_OK_CONNECT)
+		elog(ERROR, "SPI_connect failed");
+	char		query[100];
+
+	sprintf(query, "DELETE FROM pg_catalog.pg_yb_invalidation_messages WHERE db_oid = %u", db_oid);
+	SPIPlanPtr	plan = SPI_prepare(query, 0, NULL);
+
+	if (plan == NULL)
+		elog(ERROR, "SPI_prepare failed for \"%s\"", query);
+
+	bool		saved_yb_is_calling_internal_sql_for_ddl = yb_is_calling_internal_sql_for_ddl;
+	Oid			save_userid;
+	int			save_sec_context;
+
+	GetUserIdAndSecContext(&save_userid, &save_sec_context);
+	SetUserIdAndSecContext(BOOTSTRAP_SUPERUSERID,
+						   SECURITY_RESTRICTED_OPERATION);
+	yb_is_calling_internal_sql_for_ddl = true;
+	PG_TRY();
+	{
+		int			spirc = SPI_execute_plan(plan, NULL, NULL, false, 0);
+
+		yb_is_calling_internal_sql_for_ddl = saved_yb_is_calling_internal_sql_for_ddl;
+		SetUserIdAndSecContext(save_userid, save_sec_context);
+		if (spirc != SPI_OK_DELETE)
+			elog(ERROR, "SPI_execute_plan failed for \"%s\"", query);
+		ereport((*YBCGetGFlags()->log_ysql_catalog_versions ? LOG : DEBUG1),
+				(errmsg("%s: deleted %lu invalidation messages for database %u",
+						__func__, SPI_processed, db_oid)));
+	}
+	PG_CATCH();
+	{
+		yb_is_calling_internal_sql_for_ddl = saved_yb_is_calling_internal_sql_for_ddl;
+		SetUserIdAndSecContext(save_userid, save_sec_context);
+		PG_RE_THROW();
+	}
+	PG_END_TRY();
+
+	/*
+	 * Disconnect from SPI manager
+	 */
+	if (SPI_finish() != SPI_OK_FINISH)
+		elog(ERROR, "SPI_finish failed");
+}
+
+void
 YbDeleteMasterDBCatalogVersionTableEntry(Oid db_oid)
 {
 	Assert(YbGetCatalogVersionType() == CATALOG_VERSION_CATALOG_TABLE);
@@ -798,6 +851,14 @@ YbDeleteMasterDBCatalogVersionTableEntry(Oid db_oid)
 	Assert(rows_affected_count == 1);
 
 	RelationClose(rel);
+
+	/*
+	 * When invalidation messages are enabled, we also need to delete the
+	 * invalidation messages stored in pg_yb_invalidation_messages table
+	 * for db_oid.
+	 */
+	if (YbIsInvalidationMessageEnabled())
+		YbDeleteMasterDBInvalidationMessagesTableEntries(db_oid);
 }
 
 YbCatalogVersionType

src/postgres/src/include/catalog/yb_catalog_version.h
@@ -50,6 +50,8 @@ extern void YbCreateMasterDBCatalogVersionTableEntry(Oid db_oid);
 /* Send a request to delete the master catalog version for the given database. */
 extern void YbDeleteMasterDBCatalogVersionTableEntry(Oid db_oid);
 
+extern void YbDeleteMasterDBInvalidationMessagesTableEntries(Oid db_oid);
+
 /* Annotate an DML request if it changes the catalog data (if needed). */
 extern bool YbMarkStatementIfCatalogVersionIncrement(YbcPgStatement ybc_stmt,
 													 Relation rel);

src/yb/yql/pgwrapper/pg_catalog_version-test.cc
@@ -3239,6 +3239,29 @@ TEST_F(PgCatalogVersionTest, InvalMessageDeltaTableLoad) {
   }
 }
 
+TEST_F(PgCatalogVersionTest, InvalMessageDropDatabase) {
+  auto conn = CHECK_RESULT(Connect());
+  ASSERT_OK(conn.Execute("CREATE DATABASE test_db"));
+  conn = ASSERT_RESULT(ConnectToDBAsUser("test_db", "yugabyte"));
+  ASSERT_OK(conn.Execute("CREATE TABLE test_table(id INT)"));
+
+  // Each alter inserts a new row into pg_yb_invalidation_messages for test_db.
+  ASSERT_OK(conn.Execute("ALTER TABLE test_table ADD COLUMN val TEXT"));
+  ASSERT_OK(conn.Execute("ALTER TABLE test_table DROP COLUMN val"));
+
+  const auto test_db_oid = ASSERT_RESULT(GetDatabaseOid(&conn, "test_db"));
+  const auto query =
+      Format("SELECT COUNT(*) FROM pg_yb_invalidation_messages WHERE db_oid = $0", test_db_oid);
+  auto message_count  = ASSERT_RESULT(conn.FetchRow<PGUint64>(query));
+  ASSERT_EQ(message_count, 2);
+  conn = CHECK_RESULT(Connect());
+  // After dropping the database, its associated invalidation messages should also be
+  // deleted from pg_yb_invalidation_messages.
+  ASSERT_OK(conn.Execute("DROP DATABASE test_db"));
+  message_count = ASSERT_RESULT(conn.FetchRow<PGUint64>(query));
+  ASSERT_EQ(message_count, 0);
+}
+
 class PgCatalogVersionConnManagerTest
     : public PgCatalogVersionTest,
       public ::testing::WithParamInterface<bool> {

commit 5f51edbd3833fe10e8688f73c16bd0ab11671bbf

[#28348] YSQL: Fix bug in delete inval messages for dropped database
Summary:
To reproduce the bug:

Create a RF-1 cluster using 2.25.1.0-b85 initdb snapshot (which did not have the table
pg_catalog.pg_yb_invalidation_messages):

```
./bin/yb-ctl create --rf 1 --master_flags initial_sys_catalog_snapshot_path=$HOME/tmp/yugabyte-2.25.1.0/share/initial_sys_catalog_snapshot
```

Run the following ysqlsh session:

```
yugabyte=# create database db1;
CREATE DATABASE
yugabyte=# drop database db1;
ERROR:  relation "pg_catalog.pg_yb_invalidation_messages" does not exist
LINE 1: DELETE FROM pg_catalog.pg_yb_invalidation_messages WHERE db_...
                    ^
QUERY:  DELETE FROM pg_catalog.pg_yb_invalidation_messages WHERE db_oid = 16384
```

This is a recent regression bug introduced by 0dd2f62eb8aa51be520c69f6dfae8276b26d14cc.

I made a change to only delete the invalidation messages associated with db_oid for a dropped
database when the table `pg_catalog.pg_yb_invalidation_messages` exists.
Jira: DB-18033

Test Plan:
Manual test
create a RF-1 cluster using 2.25.1.0 initdb snapshot

```
./bin/yb-ctl create --rf 1 --master_flags initial_sys_catalog_snapshot_path=/net/dev-server-myang/share/tmp/yugabyte-2.25.1.0/share/initial_sys_catalog_snapshot

```

Run the above ysqlsh session, no more error:
```
yugabyte=# create database db1;
CREATE DATABASE
yugabyte=# drop database db1;
DROP DATABASE
yugabyte=#
```

Reviewers: kfranz, sanketh

Reviewed By: kfranz

Subscribers: yql

Differential Revision: https://phorge.dev.yugabyte.com/D46534

src/postgres/src/backend/catalog/yb_catalog/yb_catalog_version.c
@@ -857,7 +857,7 @@ YbDeleteMasterDBCatalogVersionTableEntry(Oid db_oid)
 	 * invalidation messages stored in pg_yb_invalidation_messages table
 	 * for db_oid.
 	 */
-	if (YbIsInvalidationMessageEnabled())
+	if (YbIsInvalidationMessageEnabled() && YbInvalidationMessagesTableExists())
 		YbDeleteMasterDBInvalidationMessagesTableEntries(db_oid);
 }
 