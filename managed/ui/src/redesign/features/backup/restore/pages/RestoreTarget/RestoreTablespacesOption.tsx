85f38623c3b53dd02f5bfdddc6cefdf9e502ec66
kkannan <kkannan@yugabyte.com>
2025-09-06 08:51:06 +0530

    [PLAT-18423][Platform][UI] Allow Restore if conflicting tablespaces is found
    Summary:
    Previously, we blocked the restore process if conflicting tablespaces were detected. We continue to block restores if unsupported tablespaces are found.
    
    The current scenarios are as follows:
    
      - No conflicting tablespaces and no unsupported tablespaces: **No Warning Banner**
      - Conflicting tablespaces but no unsupported tablespaces: **Warning Banner. send errorIfTablespacesExists as false**
      - No conflicting tablespaces but unsupported tablespaces present: **Disable restore tablespaces**
      - Both conflicting and unsupported tablespaces present: **Disable restore tablespaces**
      - No tablespaces found: **No Warning Banner**
    
    Test Plan:
    Tested manually
    
    PITR disabled restore modal
    {F390679}
    {F390678}
    
    PITR enabled restore modal
    {F390676}
    {F390677}
    
    Reviewers: lsangappa
    
    Reviewed By: lsangappa
    
    Differential Revision: https://phorge.dev.yugabyte.com/D46352

--- managed/ui/src/redesign/features/backup/restore/pages/RestoreTarget/RestoreTablespacesOption.tsx
+++ managed/ui/src/redesign/features/backup/restore/pages/RestoreTarget/RestoreTablespacesOption.tsx
@@ -10,24 +10,40 @@
 import { FC } from 'react';
 import { keys } from 'lodash';
 import { useFormContext } from 'react-hook-form';
-import { useToggle } from 'react-use';
 import { Trans, useTranslation } from 'react-i18next';
 import { makeStyles } from '@material-ui/core';
-import { GetRestoreContext } from '../../RestoreUtils';
-import { YBCheckbox } from '../../../../../components';
-import { RestoreTablespaceNotSupported } from './RestoreTablespaceNotSupported';
+import { GetRestoreContext, getUnSupportedTableSpaceConfig } from '../../RestoreUtils';
+import { AlertVariant, YBAlert, YBCheckbox } from '../../../../../components';
+
 import { RestoreFormModel } from '../../models/RestoreFormModel';
+import { TablespaceUnSupported } from './TablespaceUnSupported';
 
 const useStyles = makeStyles((theme) => ({
   helperText: {
     marginLeft: '35px',
     color: theme.palette.ybacolors.textDarkGray,
     fontSize: '12px'
+  },
+  conflictingTablespace: {
+    marginLeft: '34px',
+    marginTop: '5px'
+  },
+  unSupportedTablespace: {
+    marginLeft: '34px',
+    marginTop: '8px'
+  },
+  link: {
+    color: theme.palette.grey[700],
+    textDecoration: 'underline',
+    cursor: 'pointer',
+    '&:hover, &:focus': {
+      color: theme.palette.grey[700],
+      textDecoration: 'underline'
+    }
   }
 }));
 const RestoreTablespacesOption: FC = () => {
   const [{ preflightResponse }] = GetRestoreContext();
-  const [errMsgModalVisible, toggleErrMsgModal] = useToggle(false);
   const { watch, setValue } = useFormContext<RestoreFormModel>();
   const { t } = useTranslation('translation', {
     keyPrefix: 'backup.restore.target'
@@ -51,14 +67,14 @@ const RestoreTablespacesOption: FC = () => {
     return null;
   }
 
-  const hasUnsupportedTablespaceConfig = storageLocationsKeys.some((location) => {
-    const tablespaceResponse =
-      preflightResponse.perLocationBackupInfoMap[location].tablespaceResponse;
-    return (
-      tablespaceResponse.conflictingTablespaces?.length !== 0 ||
-      tablespaceResponse.unsupportedTablespaces?.length !== 0
-    );
-  });
+  const hasUnsupportedTablespaces = getUnSupportedTableSpaceConfig(
+    preflightResponse,
+    'unsupportedTablespaces'
+  );
+  const hasConflictingTablespaces = getUnSupportedTableSpaceConfig(
+    preflightResponse,
+    'conflictingTablespaces'
+  );
 
   return (
     <div>
@@ -67,20 +83,44 @@ const RestoreTablespacesOption: FC = () => {
         name="target.useTablespaces"
         checked={useTablespaces}
         onChange={(event) => {
-          if (event.target.checked && hasUnsupportedTablespaceConfig) {
-            toggleErrMsgModal(true);
-          } else {
-            setValue('target.useTablespaces', event.target.checked);
-          }
+          setValue('target.useTablespaces', event.target.checked);
         }}
+        disabled={hasUnsupportedTablespaces}
       />
-      <div className={classes.helperText}>
-        <Trans i18nKey="restoreTablespacesHelpText" components={{ b: <b /> }} t={t} />
-      </div>
-      <RestoreTablespaceNotSupported
-        visible={errMsgModalVisible}
-        onHide={() => toggleErrMsgModal(false)}
-      />
+      {!hasUnsupportedTablespaces && (
+        <div className={classes.helperText}>
+          <Trans i18nKey="restoreTablespacesHelpText" components={{ b: <b /> }} t={t} />
+        </div>
+      )}
+      {hasUnsupportedTablespaces && (
+        <div className={classes.unSupportedTablespace}>
+          <TablespaceUnSupported loggingID={preflightResponse.loggingID} />
+        </div>
+      )}
+      {useTablespaces && !hasUnsupportedTablespaces && hasConflictingTablespaces && (
+        <YBAlert
+          text={
+            <Trans
+              i18nKey="tablespaceConflictResolution"
+              t={t}
+              components={{
+                a: (
+                  <a
+                    className={classes.link}
+                    href={`/logs?queryRegex=${preflightResponse.loggingID}`}
+                    target="_blank"
+                    rel="noreferrer"
+                  />
+                ),
+                b: <b />
+              }}
+            />
+          }
+          open
+          variant={AlertVariant.Warning}
+          className={classes.conflictingTablespace}
+        />
+      )}
     </div>
   );
 };
