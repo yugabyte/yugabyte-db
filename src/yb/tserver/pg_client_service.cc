yugabyte/yugabyte-db

commit 7f56068aed54258770f565511f1d86841f228aad

[#15061] docdb: Fail C++ integration tests when test harness force kills a yb process
Summary:
This diff changes the external mini cluster used in integration tests so it tracks whether it kills a yb process forcefully. If so, it fails the test. This is a first step in fixing #15061.
Jira: DB-4289

Test Plan: Existing tests.

Reviewers: hsunder, amitanand, sergei, bkolagani

Reviewed By: bkolagani

Subscribers: bkolagani, yql, rthallam, ybase, slingam

Differential Revision: https://phorge.dev.yugabyte.com/D45640

src/yb/tserver/pg_client_service.cc
@@ -234,11 +234,11 @@ class LockablePgClientSession {
     return status;
   }
 
-  void StartShutdown() {
+  void StartShutdown(bool pg_service_shutting_down) {
     if (exchange_runnable_) {
       exchange_runnable_->StartShutdown();
     }
-    session_.StartShutdown();
+    session_.StartShutdown(pg_service_shutting_down);
   }
 
   bool ReadyToShutdown() const {
@@ -586,29 +586,40 @@ class PgClientServiceImpl::Impl : public SessionProvider {
     shared_mem_pool_.Start(messenger->scheduler());
   }
 
-  ~Impl() override {
-    cdc_state_table_.reset();
+  void Shutdown() {
     std::vector<SessionInfoPtr> sessions;
     {
       std::lock_guard lock(mutex_);
+      if (shutting_down_) {
+        return;
+      }
+      shutting_down_ = true;
       sessions.reserve(sessions_.size());
       for (const auto& session : sessions_) {
         sessions.push_back(session);
       }
     }
+    cdc_state_table_.reset();
     for (const auto& session : sessions) {
-      session->session().StartShutdown();
+      session->session().StartShutdown(/* pg_service_shutting_down */ true);
     }
     for (const auto& session : sessions) {
       session->session().CompleteShutdown();
     }
     sessions.clear();
-    check_expired_sessions_.Shutdown();
+    {
+      std::lock_guard lock(mutex_);
+      check_expired_sessions_.Shutdown();
+    }
     check_object_id_allocators_.Shutdown();
     check_ysql_lease_.Shutdown();
     if (exchange_thread_pool_) {
       exchange_thread_pool_->Shutdown();
     }
+  }
+
+  ~Impl() override {
+    Shutdown();
   }
 
   uint64_t lease_epoch() EXCLUDES(mutex_) {
@@ -667,6 +678,9 @@ class PgClientServiceImpl::Impl : public SessionProvider {
     });
 
     std::lock_guard lock(mutex_);
+    if (shutting_down_) {
+      return STATUS(ShutdownInProgress, "PG client service shutting down");
+    }
     auto it = sessions_.insert(std::move(session_info)).first;
     session_expiration_queue_.emplace((**it).session().expiration(), session_id);
     return Status::OK();
@@ -2183,7 +2197,7 @@ class PgClientServiceImpl::Impl : public SessionProvider {
     }
     std::vector<SessionInfoPtr> not_ready_sessions;
     for (const auto& session : expired_sessions) {
-      session->session().StartShutdown();
+      session->session().StartShutdown(/* pg_service_shutting_donw= */ false);
       txn_snapshot_manager_.UnregisterAll(session->id());
     }
     AtomicFlagSleepMs(&FLAGS_TEST_delay_before_complete_expired_pg_sessions_shutdown_ms);
@@ -2721,6 +2735,7 @@ class PgClientServiceImpl::Impl : public SessionProvider {
   CoarseTimePoint lease_expiry_time_ GUARDED_BY(mutex_);
   bool ysql_lease_is_live_ GUARDED_BY(mutex_) {false};
   uint64_t lease_epoch_ GUARDED_BY(mutex_) = 0;
+  bool shutting_down_ GUARDED_BY(mutex_) = false;
 };
 
 PgClientServiceImpl::PgClientServiceImpl(
@@ -2770,6 +2785,8 @@ YSQLLeaseInfo PgClientServiceImpl::GetYSQLLeaseInfo() const {
 }
 
 size_t PgClientServiceImpl::TEST_SessionsCount() { return impl_->TEST_SessionsCount(); }
+
+void PgClientServiceImpl::Shutdown() { impl_->Shutdown(); }
 
 #define YB_PG_CLIENT_METHOD_DEFINE(r, data, method) \
 void PgClientServiceImpl::method( \

java/yb-cdc/src/test/java/org/yb/cdc/common/CDCBaseClass.java
@@ -130,7 +130,6 @@ public class CDCBaseClass extends BaseMiniClusterTest {
     flagMap.put("ysql_enable_packed_row", "false");
 
     flagMap.put("cdc_enable_implicit_checkpointing", "true");
-
     return flagMap;
   }
 

java/yb-client/src/test/java/org/yb/minicluster/MiniYBDaemon.java
@@ -349,6 +349,7 @@ public class MiniYBDaemon {
     try {
       process.getInputStream().close();
     } catch (IOException ex) {
+      LOG.warn("Exception when trying to close process stdin for process " + this, ex);
     }
 
     // Manually cleanup left behind YB Controller subprocesses if any.

src/yb/client/client-internal.cc
@@ -641,7 +641,7 @@ Status YBClient::Data::WaitForCreateTableToFinish(YBClient* client,
                                                   CoarseTimePoint deadline,
                                                   const uint32_t max_jitter_ms,
                                                   const uint32_t init_exponent) {
-  return RetryFunc(
+  return RetryUntilShutdown(
       deadline, "Waiting on Create Table to be completed", "Timed out waiting for Table Creation",
       std::bind(
           &YBClient::Data::IsCreateTableInProgress, this, client, table_name, table_id, _1, _2),
@@ -769,7 +769,7 @@ Status YBClient::Data::IsDeleteTableInProgress(YBClient* client,
 Status YBClient::Data::WaitForDeleteTableToFinish(YBClient* client,
                                                   const std::string& table_id,
                                                   CoarseTimePoint deadline) {
-  return RetryFunc(
+  return RetryUntilShutdown(
       deadline, "Waiting on Delete Table to be completed", "Timed out waiting for Table Deletion",
       std::bind(&YBClient::Data::IsDeleteTableInProgress, this, client, table_id, _1, _2));
 }
@@ -805,7 +805,6 @@ Status YBClient::Data::IsTruncateTableInProgress(YBClient* client,
   DCHECK_ONLY_NOTNULL(truncate_in_progress);
   IsTruncateTableDoneRequestPB req;
   IsTruncateTableDoneResponsePB resp;
-
   req.set_table_id(table_id);
   RETURN_NOT_OK(SyncLeaderMasterRpc(
       deadline, req, &resp, "IsTruncateTableDone",
@@ -818,7 +817,7 @@ Status YBClient::Data::IsTruncateTableInProgress(YBClient* client,
 Status YBClient::Data::WaitForTruncateTableToFinish(YBClient* client,
                                                     const std::string& table_id,
                                                     CoarseTimePoint deadline) {
-  return RetryFunc(
+  return RetryUntilShutdown(
       deadline, "Waiting on Truncate Table to be completed",
       "Timed out waiting for Table Truncation",
       std::bind(&YBClient::Data::IsTruncateTableInProgress, this, client, table_id, _1, _2));
@@ -1003,7 +1002,6 @@ Status YBClient::Data::IsBackfillIndexInProgress(YBClient* client,
                                                  CoarseTimePoint deadline,
                                                  bool* backfill_in_progress) {
   DCHECK_ONLY_NOTNULL(backfill_in_progress);
-
   YBTableInfo yb_table_info;
   RETURN_NOT_OK(GetTableSchema(client,
                                table_id,
@@ -1027,7 +1025,7 @@ Status YBClient::Data::WaitForBackfillIndexToFinish(
     const TableId& table_id,
     const TableId& index_id,
     CoarseTimePoint deadline) {
-  return RetryFunc(
+  return RetryUntilShutdown(
       deadline,
       "Waiting on Backfill Index to be completed",
       "Timed out waiting for Backfill Index",
@@ -1136,7 +1134,7 @@ Status YBClient::Data::WaitForCreateNamespaceToFinish(
     const boost::optional<YQLDatabase>& database_type,
     const std::string& namespace_id,
     CoarseTimePoint deadline) {
-  return RetryFunc(
+  return RetryUntilShutdown(
       deadline,
       "Waiting on Create Namespace to be completed",
       "Timed out waiting for Namespace Creation",
@@ -1186,7 +1184,7 @@ Status YBClient::Data::WaitForDeleteNamespaceToFinish(YBClient* client,
     const boost::optional<YQLDatabase>& database_type,
     const std::string& namespace_id,
     CoarseTimePoint deadline) {
-  return RetryFunc(
+  return RetryUntilShutdown(
       deadline,
       "Waiting on Delete Namespace to be completed",
       "Timed out waiting for Namespace Deletion",
@@ -1234,7 +1232,7 @@ Status YBClient::Data::IsCloneNamespaceInProgress(
 Status YBClient::Data::WaitForCloneNamespaceToFinish(
     YBClient* client, const std::string& source_namespace_id, uint32_t clone_seq_no,
     CoarseTimePoint deadline) {
-  return RetryFunc(
+  return RetryUntilShutdown(
       deadline, "Waiting on Clone Namespace to be completed",
       "Timed out waiting for Namespace Cloning",
       std::bind(
@@ -1278,7 +1276,7 @@ Status YBClient::Data::WaitForAlterTableToFinish(YBClient* client,
                                                  const YBTableName& alter_name,
                                                  const string table_id,
                                                  CoarseTimePoint deadline) {
-  return RetryFunc(
+  return RetryUntilShutdown(
       deadline, "Waiting on Alter Table to be completed", "Timed out waiting for AlterTable",
       std::bind(&YBClient::Data::IsAlterTableInProgress, this, client,
               alter_name, table_id, _1, _2));
@@ -1357,7 +1355,7 @@ Status YBClient::Data::IsFlushTableInProgress(YBClient* client,
 Status YBClient::Data::WaitForFlushTableToFinish(YBClient* client,
                                                  const FlushRequestId& flush_id,
                                                  const CoarseTimePoint deadline) {
-  return RetryFunc(
+  return RetryUntilShutdown(
       deadline, "Waiting for FlushTables to be completed", "Timed out waiting for FlushTables",
       std::bind(&YBClient::Data::IsFlushTableInProgress, this, client, flush_id, _1, _2));
 }
@@ -2535,7 +2533,7 @@ Result<IndexPermissions> YBClient::Data::WaitUntilIndexPermissionsAtLeast(
     const CoarseDuration max_wait) {
   const bool retry_on_not_found = (target_index_permissions != INDEX_PERM_NOT_USED);
   IndexPermissions actual_index_permissions = INDEX_PERM_NOT_USED;
-  RETURN_NOT_OK(RetryFunc(
+  RETURN_NOT_OK(RetryUntilShutdown(
       deadline,
       "Waiting for index to have desired permissions",
       "Timed out waiting for proper index permissions",
@@ -2564,7 +2562,7 @@ Result<IndexPermissions> YBClient::Data::WaitUntilIndexPermissionsAtLeast(
   const bool retry_on_not_found = (target_index_permissions != INDEX_PERM_NOT_USED);
   IndexPermissions actual_index_permissions = INDEX_PERM_NOT_USED;
   YBTableInfo yb_index_info;
-  RETURN_NOT_OK(RetryFunc(
+  RETURN_NOT_OK(RetryUntilShutdown(
       deadline,
       "Waiting for index table schema",
       "Timed out waiting for index table schema",
@@ -2581,9 +2579,8 @@ Result<IndexPermissions> YBClient::Data::WaitUntilIndexPermissionsAtLeast(
         return Status::OK();
       },
       max_wait));
-  RETURN_NOT_OK(RetryFunc(
-      deadline,
-      "Waiting for index to have desired permissions",
+  RETURN_NOT_OK(RetryUntilShutdown(
+      deadline, "Waiting for index to have desired permissions",
       "Timed out waiting for proper index permissions",
       [&](CoarseTimePoint deadline, bool* retry) -> Status {
         Result<IndexPermissions> result = GetIndexPermissions(
@@ -2958,12 +2955,11 @@ Status YBClient::Data::SetReplicationInfo(
     bool* retry) {
   // If retry was not set, we'll wrap around in a retryable function.
   if (!retry) {
-    return RetryFunc(
+    return RetryUntilShutdown(
         deadline, "Other clients changed the config. Retrying.",
         "Timed out retrying the config change. Probably too many concurrent attempts.",
         std::bind(&YBClient::Data::SetReplicationInfo, this, client, replication_info, _1, _2));
   }
-
   // Get the current config.
   GetMasterClusterConfigRequestPB get_req;
   GetMasterClusterConfigResponsePB get_resp;
@@ -3074,7 +3070,7 @@ Status YBClient::Data::IsYsqlDdlVerificationInProgress(
 Status YBClient::Data::WaitForDdlVerificationToFinish(
     const TransactionMetadata& txn,
     CoarseTimePoint deadline) {
-  return RetryFunc(
+  return RetryUntilShutdown(
       deadline,
       Format("Waiting on YSQL DDL Verification for $0 to be completed", txn.transaction_id),
       Format("Timed out on YSQL DDL Verification for $0 to be completed", txn.transaction_id),
@@ -3225,6 +3221,10 @@ void YBClient::Data::Shutdown() {
   }
 }
 
+bool YBClient::Data::Closing() {
+  return closing_.load();
+}
+
 bool YBClient::Data::IsMultiMaster() {
   std::lock_guard l(master_server_addrs_lock_);
   if (full_master_server_addrs_.size() > 1) {
@@ -3252,5 +3252,21 @@ bool YBClient::Data::IsMultiMaster() {
   return status.ok() && (addrs.size() > 1);
 }
 
-} // namespace client
-} // namespace yb
+Status YBClient::Data::RetryUntilShutdown(
+    CoarseTimePoint deadline, const std::string& retry_msg, const std::string& timeout_msg,
+    const std::function<Status(CoarseTimePoint, bool*)>& func, const CoarseDuration max_wait,
+    const uint32_t max_jitter_ms, const uint32_t init_exponent) {
+  return RetryFunc(
+      deadline, retry_msg, timeout_msg,
+      [func, this](CoarseTimePoint deadline, bool* retry) {
+        if (retry != nullptr && Closing()) {
+          *retry = false;
+          return STATUS(ShutdownInProgress, "Client is shutting down");
+        }
+        return func(deadline, retry);
+      },
+      max_wait, max_jitter_ms, init_exponent);
+}
+
+}  // namespace client
+}  // namespace yb

src/yb/client/client-internal.h
@@ -528,6 +528,8 @@ class YBClient::Data {
 
   void Shutdown();
 
+  bool Closing();
+
   void DoSetMasterServerProxy(
       CoarseTimePoint deadline, bool skip_resolution, bool wait_for_leader_election);
   Result<server::MasterAddresses> ParseMasterAddresses(const Status& reinit_status);
@@ -627,6 +629,15 @@ class YBClient::Data {
   Status FlushTablesHelper(YBClient* client,
                            const CoarseTimePoint deadline,
                            const master::FlushTablesRequestPB& req);
+
+  Status RetryUntilShutdown(
+    CoarseTimePoint deadline,
+    const std::string& retry_msg,
+    const std::string& timeout_msg,
+    const std::function<Status(CoarseTimePoint, bool*)>& func,
+    const CoarseDuration max_wait = kDefaultMaxRetryWait,
+    const uint32_t max_jitter_ms = CoarseBackoffWaiter::kDefaultMaxJitterMs,
+    const uint32_t init_exponent = CoarseBackoffWaiter::kDefaultInitExponent);
 
   DISALLOW_COPY_AND_ASSIGN(Data);
 };

src/yb/client/client_master_rpc.cc
@@ -94,6 +94,12 @@ void ClientMasterRpcBase::Finished(const Status& status) {
     return;
   }
 
+  if (client_data_->Closing()) {
+    auto retained_self = client_data_->rpcs_.Unregister(&retained_self_);
+    ProcessResponse(new_status);
+    return;
+  }
+
   if (new_status.ok() && !resp_status.ok()) {
     master::MasterError master_error(resp_status);
     if (master_error == master::MasterErrorPB::NOT_THE_LEADER ||

src/yb/client/meta_data_cache.cc
@@ -159,7 +159,9 @@ YBMetaDataCache::YBMetaDataCache(
   }
 }
 
-YBMetaDataCache::~YBMetaDataCache() = default;
+YBMetaDataCache::~YBMetaDataCache() {
+  Shutdown();
+}
 
 void YBMetaDataCache::GetTableAsync(
     const YBTableName& table_name, const GetTableAsyncCallback& callback) {
@@ -378,6 +380,16 @@ Status YBMetaDataCache::HasTablePermission(const NamespaceName& keyspace_name,
                            CacheCheckMode::NO_RETRY);
   }
   return s;
+}
+
+void YBMetaDataCache::Shutdown() {
+  bool expected = false;
+  if (!shutting_down_.compare_exchange_strong(expected, true)) {
+    return;
+  }
+  if (permissions_cache_) {
+    permissions_cache_->Shutdown();
+  }
 }
 
 } // namespace client

src/yb/client/meta_data_cache.h
@@ -132,6 +132,8 @@ class YBMetaDataCache {
       const PermissionType permission,
       const CacheCheckMode check_mode =  CacheCheckMode::RETRY);
 
+  void Shutdown();
+
  private:
   friend struct YBMetaDataCacheEntry;
 
@@ -173,6 +175,7 @@ class YBMetaDataCache {
   YBTypeMap cached_types_ GUARDED_BY(cached_types_mutex_);
 
   MemTrackerPtr mem_tracker_;
+  std::atomic_bool shutting_down_{false};
 };
 
 } // namespace client

commit 04f0ca5c4d90e51659469a874212313f235c1d1b

[#28597] docdb: fix tserver shutdown deadlock introduced by #15061
Summary:
7f56068 introduced a deadlock in the pg client service shutdown path. The `CheckExpiredSessions` callback, run by the `check_expired_sessions_` scheduler, can deadlock with the shutdown thread calling `PgClientServiceImpl::Impl::Shutdown` when the shutdown thread calls `check_expired_sessions_::Shutdown` while holding `PgClientServiceImpl::Impl::mutex_` inside `PgClientServiceImpl::Impl::Shutdown`. The issue is `CheckExpiredSessions` tries to acquires `PgClientServiceImpl::Impl::mutex_` and the scheduler's `Shutdown` method blocks waiting for all inflight tasks to complete, so the two threads can deadlock on `PgClientServiceImpl::Impl::mutex_`.

This diff removes lock guard annotation from `check_expired_sessions_` so `check_expired_sessions_::Shutdown` can be called without holding the pg client service lock.
Jira: DB-18293, DB-4289

Test Plan:
```
/yb_build.sh --cxx-test='TEST_F(PgAdvisoryLockTest, SessionLockDeadlockWithRowLocks) {' -n 60 --tp 1 --stop-at-failure
```

Reviewers: bkolagani, sergei

Reviewed By: bkolagani

Subscribers: ybase, rthallam, yql

Differential Revision: https://phorge.dev.yugabyte.com/D46753

src/yb/tserver/pg_client_service.cc
@@ -620,12 +620,16 @@ class PgClientServiceImpl::Impl : public SessionProvider {
       session->session().CompleteShutdown();
     }
     sessions.clear();
-    {
-      std::lock_guard lock(mutex_);
-      check_expired_sessions_.Shutdown();
+    auto schedulers = {
+        std::reference_wrapper(check_expired_sessions_),
+        std::reference_wrapper(check_object_id_allocators_),
+        std::reference_wrapper(check_ysql_lease_)};
+    for (auto& task : schedulers) {
+      task.get().StartShutdown();
     }
-    check_object_id_allocators_.Shutdown();
-    check_ysql_lease_.Shutdown();
+    for (auto& task : schedulers) {
+      task.get().CompleteShutdown();
+    }
     if (exchange_thread_pool_) {
       exchange_thread_pool_->Shutdown();
     }
@@ -2722,7 +2726,7 @@ class PgClientServiceImpl::Impl : public SessionProvider {
 
   std::atomic<int64_t> session_serial_no_{0};
 
-  rpc::ScheduledTaskTracker check_expired_sessions_ GUARDED_BY(mutex_);
+  rpc::ScheduledTaskTracker check_expired_sessions_;
   CoarseTimePoint check_expired_sessions_time_ GUARDED_BY(mutex_);
   rpc::ScheduledTaskTracker check_object_id_allocators_;
   rpc::ScheduledTaskTracker check_ysql_lease_;