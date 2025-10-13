yugabyte/yugabyte-db

commit e01d18cce6760f7c9d4fd5b5336a9ceea4584ecd

[#6218] YSQL:  Handle master-failover during index backfill
Summary:
 - Move the index to DO_BACKFILL permission when pg_backend asks the master to start backfill. This will ensure that if the master fails after that, then the newly elected master can restart the backfill process.
 - update tests to ensure that backfill completes successfully, after a master failover during backfill.
 - use b2a to escape binary strings to make rewrite_test_log.py happy. With the sed replacement being done in our test scripts, printing binary characters may cause the test to abort due to sed not being able to accept the lines. Specifically, backfilled_until was being printed in binary. Use b2a to make it printable.
 - Allow "schema version mismatch" errors during `BACKFILL INDEX` to be handled transparently by doing retries.
   Note: There is some debate/discussion with jason regarding whether or not this should be done -- the counter point being that the whole "create index" should act like one transaction and  schema version/catalog version mismatches are not retried within one transaction. This however is not the case with how things are implemented today. each `BACKFILL INDEX` picks ups the schema version to use independently. Thus a retry is no different than this RPC being issued "a little later" in time.  //Once Table level locks are used by default, this becomes a non-issue.//
Jira: DB-2052

Test Plan: yb_build.sh fastdebug  --cxx-test pg_index_backfill-test --gtest_filter *MasterLeaderStepdown*

Reviewers: jason

Reviewed By: jason

Subscribers: yql, ybase

Differential Revision: https://phorge.dev.yugabyte.com/D19732

src/yb/master/backfill_index.cc
@@ -126,6 +126,7 @@ namespace master {
 
 using namespace std::literals;
 using server::MonitoredTaskState;
+using strings::b2a_hex;
 using strings::Substitute;
 using tserver::TabletServerErrorPB;
 
@@ -425,10 +426,18 @@ IndexPermissions NextPermission(IndexPermissions perm) {
 
 Status MultiStageAlterTable::LaunchNextTableInfoVersionIfNecessary(
     CatalogManager* catalog_manager, const scoped_refptr<TableInfo>& indexed_table,
-    uint32_t current_version, const LeaderEpoch& epoch, bool respect_backfill_deferrals) {
-  DVLOG(3) << __PRETTY_FUNCTION__ << " " << AsString(*indexed_table);
+    uint32_t current_version, const LeaderEpoch& epoch, bool respect_backfill_deferrals,
+    bool update_ysql_to_backfill) {
+  DVLOG(3) << __PRETTY_FUNCTION__ << " "
+           << yb::Format(
+                  "$0, version: $1, respect_deferrals: $2, update_ysql_to_backfill: $3",
+                  yb::ToString(*indexed_table), current_version, respect_backfill_deferrals,
+                  update_ysql_to_backfill);
 
   const bool is_ysql_table = (indexed_table->GetTableType() == TableType::PGSQL_TABLE_TYPE);
+  // For YSQL, master won't automatically move the index permission to DO_BACKFILL unless
+  // postgres calls CatalogManager::BackfillIndex() because postgres drives permission changes.
+  const bool update_to_backfill = (!is_ysql_table || update_ysql_to_backfill);
   const bool defer_backfill = !is_ysql_table && GetAtomicFlag(&FLAGS_defer_index_backfill);
   const bool is_backfilling = indexed_table->IsBackfilling();
 
@@ -461,16 +470,13 @@ Status MultiStageAlterTable::LaunchNextTableInfoVersionIfNecessary(
         }
       } else if (idx_pb.index_permissions() == INDEX_PERM_INDEX_UNUSED) {
         indexes_to_delete.emplace_back(idx_pb);
-      // For YSQL, there should never be indexes to update from master side because postgres drives
-      // permission changes.
-      } else if (idx_pb.index_permissions() != INDEX_PERM_READ_WRITE_AND_DELETE && !is_ysql_table) {
+      } else if (
+          idx_pb.index_permissions() != INDEX_PERM_READ_WRITE_AND_DELETE && update_to_backfill) {
         indexes_to_update.emplace(idx_pb.table_id(), NextPermission(idx_pb.index_permissions()));
       }
     }
 
-    // TODO(#6218): Do we really not want to continue backfill
-    // across master failovers for YSQL?
-    if (!is_ysql_table && !is_backfilling && l.data().pb.backfill_jobs_size() > 0) {
+    if (!is_backfilling && l.data().pb.backfill_jobs_size() > 0) {
       // If a backfill job was started for a set of indexes and then the leader
       // fails over, we should be careful that we are restarting the backfill job
       // with the same set of indexes.
@@ -504,24 +510,20 @@ Status MultiStageAlterTable::LaunchNextTableInfoVersionIfNecessary(
   }
 
   // For YSQL online schema migration of indexes, instead of master driving the schema changes,
-  // postgres will drive it.  Postgres will use three of the DocDB index permissions:
+  // postgres will drive it.  Postgres will use four of the DocDB index permissions:
   //
   // - INDEX_PERM_WRITE_AND_DELETE (set from the start)
+  // - INDEX_PERM_DO_BACKFILL (set by master, when postgres initiates BackfillIndex)
   // - INDEX_PERM_READ_WRITE_AND_DELETE (set by master)
   // - INDEX_PERM_WRITE_AND_DELETE_WHILE_REMOVING (set by master)
   //
   // This changes how we treat indexes_to_foo:
   //
-  // - indexes_to_update should always be empty because we never want master to set index
-  //   permissions.
+  // - indexes_to_update: used for moving from WRITE_AND_DELETE to DO_BACKFILL.
   // - indexes_to_delete is impossible to be nonempty, and, in the future, when we do use
   //   INDEX_PERM_INDEX_UNUSED, we want to use some other delete trigger that makes sure no
   //   transactions are left using the index.  Prepare for that by doing nothing when nonempty.
-  // - indexes_to_backfill is impossible to be nonempty, but, in the future, we want to set
-  //   INDEX_PERM_DO_BACKFILL so that backfill resumes on master leader changes.  Prepare for that
-  //   by handling indexes_to_backfill like for YCQL.
-  //
-  // TODO(jason): when using INDEX_PERM_DO_BACKFILL, update this comment (issue #6218).
+  // - indexes_to_backfill: used to launch StartBackfillingData once the index ready to backfill.
 
   if (!indexes_to_update.empty()) {
     VLOG(1) << "Updating index permissions for " << yb::ToString(indexes_to_update) << " on "
@@ -1272,8 +1274,7 @@ BackfillTablet::BackfillTablet(
     }
   }
   if (!backfilled_until_.empty()) {
-    VLOG_WITH_PREFIX(1) << " resuming backfill from "
-                        << yb::ToString(backfilled_until_);
+    VLOG_WITH_PREFIX(1) << " resuming backfill from " << b2a_hex(backfilled_until_);
   } else if (done()) {
     VLOG_WITH_PREFIX(1) << " backfill already done.";
   } else {
@@ -1293,7 +1294,7 @@ Status BackfillTablet::LaunchNextChunkOrDone() {
     VLOG_WITH_PREFIX(1) << "is done";
     return backfill_table_->Done(Status::OK(), /* failed_indexes */ {});
   } else if (!backfill_table_->done()) {
-    VLOG_WITH_PREFIX(2) << "Launching next chunk from " << backfilled_until_;
+    VLOG_WITH_PREFIX(2) << "Launching next chunk from " << b2a_hex(backfilled_until_);
     auto chunk = std::make_shared<BackfillChunk>(shared_from_this(),
                                                  backfilled_until_,
                                                  backfill_table_->epoch());
@@ -1324,7 +1325,7 @@ Status BackfillTablet::UpdateBackfilledUntil(
     const string& backfilled_until, const uint64_t number_rows_processed) {
   backfilled_until_ = backfilled_until;
   VLOG_WITH_PREFIX(2) << "Done backfilling the tablet " << yb::ToString(tablet_) << " until "
-                      << yb::ToString(backfilled_until_);
+                      << b2a_hex(backfilled_until_);
   {
     auto l = tablet_->LockForWrite();
     for (const auto& idx_id : backfill_table_->indexes_to_build()) {

src/yb/yql/pgwrapper/pg_index_backfill-test.cc
@@ -861,9 +861,9 @@ TEST_P(PgIndexBackfillTest, Nonconcurrent) {
       kIndexName,
       kTableName));
 
-  // If the index used backfill, it would have incremented the table schema version by two or three:
+  // If the index used backfill, it would have incremented the table schema version by three:
   // - add index info with INDEX_PERM_DELETE_ONLY
-  // - update to INDEX_PERM_DO_BACKFILL (as part of issue #6218)
+  // - update to INDEX_PERM_DO_BACKFILL
   // - update to INDEX_PERM_READ_WRITE_AND_DELETE
   // If the index did not use backfill, it would have incremented the table schema version by one:
   // - add index info with no DocDB permission (default INDEX_PERM_READ_WRITE_AND_DELETE)
@@ -928,12 +928,11 @@ TEST_P(PgIndexBackfillTestSimultaneously, CreateIndexSimultaneously) {
     if (status.ok()) {
       num_ok++;
       LOG(INFO) << "got ok status";
-      // Success index creations do two schema changes:
+      // Success index creations do three schema changes:
       // - add index with INDEX_PERM_WRITE_AND_DELETE
+      // - transition to INDEX_PERM_DO_BACKFILL, then
       // - transition to success INDEX_PERM_READ_WRITE_AND_DELETE
-      // TODO(jason): change this when closing #6218 because DO_BACKFILL permission will add another
-      // schema version.
-      expected_schema_version += 2;
+      expected_schema_version += 3;
     } else {
       ASSERT_TRUE(status.IsNetworkError()) << status;
       const std::string msg = status.message().ToBuffer();
@@ -1922,7 +1921,7 @@ class PgIndexBackfillMultiMaster : public PgIndexBackfillFastClientTimeout {
 
 INSTANTIATE_TEST_CASE_P(, PgIndexBackfillMultiMaster, ::testing::Bool());
 
-// Make sure that master leader change during backfill causes the index to not become public and
+// Make sure that master leader change during backfill causes the index backfill to continue and
 // doesn't cause any weird hangups or other issues.  Simulate the following:
 //   Session A                                    Session B
 //   --------------------------                   ----------------------
@@ -1932,7 +1931,6 @@ INSTANTIATE_TEST_CASE_P(, PgIndexBackfillMultiMaster, ::testing::Bool());
 //   - backfill
 //     - get safe time for read
 //                                                master leader stepdown
-// TODO(jason): update this test when handling master leader changes during backfill (issue #6218).
 TEST_P(PgIndexBackfillMultiMaster, MasterLeaderStepdown) {
   ASSERT_OK(conn_->ExecuteFormat("CREATE TABLE $0 (i int)", kTableName));
 
@@ -1940,12 +1938,13 @@ TEST_P(PgIndexBackfillMultiMaster, MasterLeaderStepdown) {
   thread_holder_.AddThreadFunctor([this] {
     LOG(INFO) << "Begin create thread";
     PGConn create_conn = ASSERT_RESULT(ConnectToDB(kDatabaseName));
-    // The CREATE INDEX should get master leader change during backfill so that its
-    // WaitUntilIndexPermissionsAtLeast call starts querying the new leader.  Since the new leader
-    // will be inactive at the WRITE_AND_DELETE docdb permission, it will wait until the deadline,
-    // which is set to 30s.
-    Status status = create_conn.ExecuteFormat("CREATE INDEX $0 ON $1 (i)", kIndexName, kTableName);
-    ASSERT_TRUE(HasClientTimedOut(status)) << status;
+    // The CREATE INDEX should get master leader change during backfill. We expect
+    // that the new master will continue the backfill, and it should succeed.
+    ASSERT_OK(create_conn.ExecuteFormat("CREATE INDEX $0 ON $1 (i)", kIndexName, kTableName));
+    ASSERT_TRUE(ASSERT_RESULT(IsAtTargetIndexStateFlags(
+        kIndexName, IndexStateFlags{
+                        IndexStateFlag::kIndIsLive, IndexStateFlag::kIndIsReady,
+                        IndexStateFlag::kIndIsValid})));
   });
   thread_holder_.AddThreadFunctor([this] {
     LOG(INFO) << "Begin master leader stepdown thread";

src/yb/master/backfill_index.h
@@ -65,7 +65,8 @@ class MultiStageAlterTable {
   // INDEX_PERM_DELETE_ONLY -> INDEX_PERM_WRITE_AND_DELETE -> BACKFILL
   static Status LaunchNextTableInfoVersionIfNecessary(
       CatalogManager* mgr, const scoped_refptr<TableInfo>& Info, uint32_t current_version,
-      const LeaderEpoch& epoch, bool respect_backfill_deferrals = true);
+      const LeaderEpoch& epoch, bool respect_backfill_deferrals = true,
+      bool update_ysql_to_backfill = false);
 
   // Clears the fully_applied_* state for the given table and optionally sets it to RUNNING.
   // If the version has changed and does not match the expected version no
@@ -90,7 +91,7 @@ class MultiStageAlterTable {
       const LeaderEpoch& epoch,
       boost::optional<uint32_t> current_version = boost::none);
 
-  // TODO(jason): make this private when closing issue #6218.
+ private:
   // Start Index Backfill process/step for the specified table/index.
   static Status
   StartBackfillingData(CatalogManager *catalog_manager,

src/yb/master/catalog_manager.cc
@@ -5996,6 +5996,7 @@ Status CatalogManager::BackfillIndex(
       !ysql_manager_->IsMajorUpgradeInProgress(), InternalError,
       "Attempting to backfill index during a major YSQL upgrade");
   const TableIdentifierPB& index_table_identifier = req->index_identifier();
+  VLOG(1) << __func__ << " for " << yb::ToString(index_table_identifier);
 
   scoped_refptr<TableInfo> index_table = VERIFY_RESULT(FindTable(index_table_identifier));
 
@@ -6020,10 +6021,13 @@ Status CatalogManager::BackfillIndex(
                   index_table_identifier.ShortDebugString());
   }
 
-  // TODO(jason): when ready to use INDEX_PERM_DO_BACKFILL for resuming backfill across master
-  // leader changes, replace the following (issue #6218).
+  uint32_t current_version;
+  {
+    auto l = indexed_table->LockForRead();
+    current_version = l->pb.version();
+  }
 
-  // Collect index_info_pb.
+  // Validate that the index is at the correct permission.
   IndexInfoPB index_info_pb;
   indexed_table->GetIndexInfo(index_table->id()).ToPB(&index_info_pb);
   if (index_info_pb.index_permissions() != INDEX_PERM_WRITE_AND_DELETE) {
@@ -6036,8 +6040,9 @@ Status CatalogManager::BackfillIndex(
             IndexPermissions_Name(index_info_pb.index_permissions())));
   }
 
-  return MultiStageAlterTable::StartBackfillingData(
-      this, indexed_table, {index_info_pb}, boost::none, epoch);
+  return MultiStageAlterTable::LaunchNextTableInfoVersionIfNecessary(
+      this, indexed_table, current_version, epoch, /* respect deferrals for backfill */ false,
+      /* update ysql to backfill */ true);
 }
 
 Status CatalogManager::GetBackfillJobs(

src/yb/tablet/tablet.cc
@@ -2925,8 +2925,13 @@ Result<PgsqlBackfillSpecPB> QueryPostgresToDoBackfill(
   auto result = conn->Fetch(query);
   if (!result.ok()) {
     const auto libpq_error_msg = AuxilaryMessage(result.status()).value();
-    LOG(WARNING) << "libpq query \"" << query << "\" returned "
-                 << result.status() << ": " << libpq_error_msg;
+    LOG(WARNING) << "libpq query \"" << query << "\" returned " << result.status() << ": "
+                 << libpq_error_msg;
+    // The 2 spaces after ERROR: is necessary to match the error message.
+    constexpr auto kSchemaMismatchSubstring = "ERROR:  schema version mismatch";
+    if (libpq_error_msg.starts_with(kSchemaMismatchSubstring)) {
+      return STATUS(TryAgain, libpq_error_msg);
+    }
     return STATUS(IllegalState, libpq_error_msg);
   }
   auto& res = result.get();

src/yb/tserver/tablet_service.cc
@@ -872,16 +872,8 @@ void TabletServiceAdminImpl::BackfillIndex(
 
       IndexInfoPB idx_info_pb;
       index_info->ToPB(&idx_info_pb);
-      if (!is_pg_table) {
-        all_at_backfill &=
-            idx_info_pb.index_permissions() == IndexPermissions::INDEX_PERM_DO_BACKFILL;
-      } else {
-        // YSQL tables don't use all the docdb permissions, so use this approximation.
-        // TODO(jason): change this back to being like YCQL once we bring the docdb permission
-        // DO_BACKFILL back (issue #6218).
-        all_at_backfill &=
-            idx_info_pb.index_permissions() == IndexPermissions::INDEX_PERM_WRITE_AND_DELETE;
-      }
+      all_at_backfill &=
+          idx_info_pb.index_permissions() == IndexPermissions::INDEX_PERM_DO_BACKFILL;
       all_past_backfill &=
           idx_info_pb.index_permissions() > IndexPermissions::INDEX_PERM_DO_BACKFILL;
     } else {
@@ -981,7 +973,7 @@ void TabletServiceAdminImpl::BackfillIndex(
   }
   DVLOG(1) << "Tablet " << tablet.peer->tablet_id() << " backfilled indexes "
            << yb::ToString(index_ids) << " and got " << backfill_status
-           << " backfilled until : " << backfilled_until;
+           << " backfilled until : " << b2a_hex(backfilled_until);
 
   resp->set_backfilled_until(backfilled_until);
   resp->set_propagated_hybrid_time(server_->Clock()->Now().ToUint64());

commit b6efa55e7247da85f50ab77ed4d1151e49fd9101

[#28849] YSQL: Fix OBJECT_NOT_FOUND regression bug
Summary:
The symptom of this bug:
```
yugabyte=# create table foo(id int, id2 int);
CREATE TABLE
yugabyte=# insert into foo values (1, 1), (2, 2);
INSERT 0 2
yugabyte=# insert into foo values (1, 3), (2, 4);
INSERT 0 2
yugabyte=# create unique index concurrently id_idx on foo(id); -- id_idx is invalid
ERROR:  ERROR:  duplicate key value violates unique constraint "id_idx"
yugabyte=# create unique index concurrently id_idx2 on foo(id2); -- moves id_idx permission from INDEX_PERM_WRITE_AND_DELETE_WHILE_REMOVING to INDEX_PERM_DELETE_ONLY_WHILE_REMOVING
CREATE INDEX
yugabyte=# create unique index concurrently id_idx3 on foo(id2); -- moves id_idx permission from INDEX_PERM_DELETE_ONLY_WHILE_REMOVING to INDEX_PERM_INDEX_UNUSED, which causes master to delete the docdb table for id_idx
CREATE INDEX
yugabyte=# insert into foo values (5, 5); -- OBJECT_NOT_FOUND error
ERROR:  Table with identifier 000034d4000030008000000000004003 not found: OBJECT_NOT_FOUND
```

This bug did not exist in 2024.2.4.1. The regression was introduced by commit
e01d18cce6760f7c9d4fd5b5336a9ceea4584ecd.

In function `MultiStageAlterTable::LaunchNextTableInfoVersionIfNecessary`:

Before the commit:

```
 466       } else if (idx_pb.index_permissions() != INDEX_PERM_READ_WRITE_AND_DELETE && !is_ysql_table) {
 467         indexes_to_update.emplace(idx_pb.table_id(), NextPermission(idx_pb.index_permissions()));
 468       }
```

After the commit:

```
 472       } else if (
 473           idx_pb.index_permissions() != INDEX_PERM_READ_WRITE_AND_DELETE && update_to_backfill) {
 474         indexes_to_update.emplace(idx_pb.table_id(), NextPermission(idx_pb.index_permissions()));
 475       }
```

We were not updating YSQL index permission before, now we do.

So when processing id_idx2, we will come to line 473 for id_idx as well, that will move its
permission state to the next one (`NextPermission`). Then when processing id_idx3, we come to line

src/yb/master/backfill_index.cc
@@ -436,7 +436,6 @@ Status MultiStageAlterTable::LaunchNextTableInfoVersionIfNecessary(
   const bool is_ysql_table = (indexed_table->GetTableType() == TableType::PGSQL_TABLE_TYPE);
   // For YSQL, master won't automatically move the index permission to DO_BACKFILL unless
   // postgres calls CatalogManager::BackfillIndex() because postgres drives permission changes.
-  const bool update_to_backfill = (!is_ysql_table || update_ysql_to_backfill);
   const bool defer_backfill = !is_ysql_table && GetAtomicFlag(&FLAGS_defer_index_backfill);
   const bool is_backfilling = indexed_table->IsBackfilling();
 
@@ -469,8 +468,11 @@ Status MultiStageAlterTable::LaunchNextTableInfoVersionIfNecessary(
         }
       } else if (idx_pb.index_permissions() == INDEX_PERM_INDEX_UNUSED) {
         indexes_to_delete.emplace_back(idx_pb);
-      } else if (
-          idx_pb.index_permissions() != INDEX_PERM_READ_WRITE_AND_DELETE && update_to_backfill) {
+      } else if (!is_ysql_table && idx_pb.index_permissions() != INDEX_PERM_READ_WRITE_AND_DELETE) {
+        indexes_to_update.emplace(idx_pb.table_id(), NextPermission(idx_pb.index_permissions()));
+      } else if (update_ysql_to_backfill &&
+                 idx_pb.index_permissions() != INDEX_PERM_READ_WRITE_AND_DELETE &&
+                 idx_pb.index_permissions() != INDEX_PERM_WRITE_AND_DELETE_WHILE_REMOVING) {
         indexes_to_update.emplace(idx_pb.table_id(), NextPermission(idx_pb.index_permissions()));
       }
     }

src/yb/yql/pgwrapper/pg_index_backfill-test.cc
@@ -1782,6 +1782,70 @@ class PgIndexBackfillClientDeadline : public PgIndexBackfillBlockDoBackfill {
   }
 };
 
+// https://github.com/yugabyte/yugabyte-db/issues/28849
+TEST_P(PgIndexBackfillTest, MultipleIndexesFirstOneInvalid) {
+  auto conn = CHECK_RESULT(Connect());
+  ASSERT_OK(conn.Execute("CREATE table foo(id int, id2 int)"));
+  // Insert values so that id contain duplicate values, id2 contais unique values.
+  ASSERT_OK(conn.Execute("INSERT INTO foo values (1, 1), (2, 2)"));
+  ASSERT_OK(conn.Execute("INSERT INTO foo values (1, 3), (2, 4)"));
+  // Do CONCURRENTLY to trigger the multi-stage index creation.
+  auto status = conn.Execute("CREATE UNIQUE INDEX CONCURRENTLY id_idx ON foo(id)");
+  ASSERT_TRUE(status.IsNetworkError()) << status;
+  ASSERT_STR_CONTAINS(status.ToString(), "duplicate key value violates unique constraint");
+
+  // Before fixing 28849, this moved the permission of id_idx from
+  // INDEX_PERM_WRITE_AND_DELETE_WHILE_REMOVING to INDEX_PERM_DELETE_ONLY_WHILE_REMOVING
+  ASSERT_OK(conn.Execute("CREATE UNIQUE INDEX CONCURRENTLY id_idx2 ON foo(id2)"));
+
+  // Before fixing 28849, this moved the permission of id_idx from
+  // INDEX_PERM_DELETE_ONLY_WHILE_REMOVING to INDEX_PERM_INDEX_UNUSED, INDEX_PERM_INDEX_UNUSED
+  // caused the docdb table for id_idx to be deleted.
+  ASSERT_OK(conn.Execute("CREATE UNIQUE INDEX CONCURRENTLY id_idx3 ON foo(id2)"));
+
+  // As a result, this INSERT failed with OBJECT_NOT_FOUND error.
+  ASSERT_OK(conn.Execute("INSERT INTO foo VALUES (5, 5)"));
+  status = conn.Execute("INSERT INTO foo VALUES (5, 6)");
+  // Although id_idx is in an invalid state, it still enforces that new values inserted
+  // must be unique according to id_idx, id_idx2 and id_idx3. We have already inserted
+  // a new value 5 for id, a second insertion of 5 for id causes id_idx to detect
+  // unique constraint violation.
+  ASSERT_TRUE(status.IsNetworkError()) << status;
+  ASSERT_STR_CONTAINS(status.ToString(), "duplicate key value violates unique constraint");
+
+  auto client = ASSERT_RESULT(cluster_->CreateClient());
+  auto count_indexes_fn = [&client]() -> int {
+    auto tables = CHECK_RESULT(client->ListTables());
+    int count = 0;
+    for (const auto& table : tables) {
+      if (table.namespace_name() != kDatabaseName)
+        continue;
+      const auto& table_name = table.table_name();
+      if (table_name == "id_idx" || table_name == "id_idx2" || table_name == "id_idx3") {
+        count++;
+      }
+    }
+    return count;
+  };
+
+  // Make sure that the indexes exists in DocDB metadata.
+  ASSERT_EQ(count_indexes_fn(), 3);
+
+  // Make sure drop index works.
+  ASSERT_OK(conn.Execute("DROP INDEX id_idx"));
+  ASSERT_OK(conn.Execute("DROP INDEX id_idx2"));
+  ASSERT_OK(conn.Execute("DROP INDEX id_idx3"));
+
+  // Make sure that the index is gone.
+  // Check postgres metadata.
+  auto value = ASSERT_RESULT(conn_->FetchRow<PGUint64>(
+      "SELECT COUNT(*) FROM pg_class WHERE relname like 'id_idx%'"));
+  ASSERT_EQ(value, 0);
+
+  // Check DocDB metadata.
+  ASSERT_EQ(count_indexes_fn(), 0);
+}
+
 INSTANTIATE_TEST_CASE_P(, PgIndexBackfillClientDeadline, ::testing::Bool());
 
 // Make sure that the postgres timeout when waiting for backfill to finish causes the index to not