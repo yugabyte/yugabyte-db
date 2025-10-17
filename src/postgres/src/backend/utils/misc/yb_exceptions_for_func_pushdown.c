a28f3dde99a08ee147b64600134a70df7d0976b3
Fizaa Luthra <fizaaluthra@gmail.com>
2025-10-17 11:25:17 -0400

    [#28997] YSQL: Pushdown filters that use SQL Value Functions
    Summary:
    SQL Value Functions that are all stable (have the same value throughout the transaction) and parameterless, but they are not pushed down as storage filters today.
    
    These functions are:
    
    timezone aware:
    - CURRENT_TIMESTAMP
    - CURRENT_TIMESTAMP(N)
    - CURRENT_TIME
    - CURRENT_TIME(N)
    
    timezone un-aware:
    - LOCALTIMESTAMP
    - LOCALTIMESTAMP(N)
    - LOCALTIME
    - LOCALTIME(N)
    
    date:
    - CURRENT_DATE
    
    Note: all of the above functions internally work with the current transaction time.
    
    other:
    - CURRENT_ROLE
    - CURRENT_USER
    - USER
    - SESSION_USER
    - CURRENT_CATALOG,
    - CURRENT_SCHEMA
    
    This diff enables pushdown for these functions by evaluating them to a constant at execution time.
    
    Specific code changes:
    - In `yb_pushdown_walker`, mark `T_SQLValueFunction` node types as pushable.
    - In `yb_expr_instantiate_exprs_mutator`, evaluate `SQLValueFunction` nodes to a constant.
    
    Note: For the timestamp/date functions, we will still have to do per-row filtering in YSQL for mixed comparisons between timezone aware and timezone un-aware types. Example: comparing a timezone column with CURRENT_TIMESTAMP, comparing timestamptz column with LOCALTIMESTAMP.
    Jira: DB-18743
    
    Test Plan: ./yb_build.sh --java-test 'org.yb.pgsql.TestPgRegressDml'
    
    Reviewers: amartsinchyk
    
    Reviewed By: amartsinchyk
    
    Subscribers: yql
    
    Differential Revision: https://phorge.dev.yugabyte.com/D46783

--- src/postgres/src/backend/utils/misc/yb_exceptions_for_func_pushdown.c
+++ src/postgres/src/backend/utils/misc/yb_exceptions_for_func_pushdown.c
@@ -24,9 +24,10 @@
  *-------------------------------------------------------------------------
  */
 
-#include "c.h"
+#include "postgres.h"
 
 #include "catalog/pg_operator_d.h"
+#include "nodes/primnodes.h"
 #include "utils/fmgroids.h"
 
 const uint32 yb_funcs_safe_for_pushdown[] = {
@@ -230,9 +231,22 @@ const uint32 yb_pushdown_funcs_to_constify[] = {
 	F_NOW
 };
 
+const SQLValueFunctionOp yb_pushdown_sqlvaluefunctions[] = {
+	SVFOP_CURRENT_TIMESTAMP,
+	SVFOP_CURRENT_TIMESTAMP_N,
+	SVFOP_CURRENT_TIME,
+	SVFOP_CURRENT_TIME_N,
+	SVFOP_CURRENT_DATE,
+	SVFOP_LOCALTIME,
+	SVFOP_LOCALTIME_N,
+	SVFOP_LOCALTIMESTAMP,
+	SVFOP_LOCALTIMESTAMP_N,
+};
+
 #define DEFINE_ARRAY_SIZE(array) const int array##_count = lengthof(array)
 
 DEFINE_ARRAY_SIZE(yb_funcs_safe_for_pushdown);
 DEFINE_ARRAY_SIZE(yb_funcs_unsafe_for_pushdown);
 DEFINE_ARRAY_SIZE(yb_funcs_safe_for_mixed_mode_pushdown);
 DEFINE_ARRAY_SIZE(yb_pushdown_funcs_to_constify);
+DEFINE_ARRAY_SIZE(yb_pushdown_sqlvaluefunctions);
