f107ed5789e9145e756bab211dd573dd5673597c
Minghui Yang <myang@yugabyte.com>
2025-10-23 00:56:34 +0000

    [BACKPORT 2025.2][#29001] YSQL: Improve error message when alter the primary key index owner
    Summary:
    From the GHI:
    
    ```
    yugabyte=# create table foo(id int, primary key(id));
    CREATE TABLE
    yugabyte=# \d foo
                    Table "public.foo"
     Column |  Type   | Collation | Nullable | Default
    --------+---------+-----------+----------+---------
     id     | integer |           | not null |
    Indexes:
        "foo_pkey" PRIMARY KEY, lsm (id HASH)
    
    yugabyte=# alter index foo_pkey OWNER TO test_role_4;
    ERROR:  Table with identifier 000033cf000030008000000000004003 not found: OBJECT_NOT_FOUND
    ```
    
    This is an error that is specific in yugabyte because in yugabyte there is no separate index table
    for the primary key. I made a fix to issue an improved error message:
    
    ```
    yugabyte=# create table foo(id int, primary key(id));
    CREATE TABLE
    yugabyte=# alter index foo_pkey OWNER TO test_role_4;
    ERROR:  cannot alter the primary key index "foo_pkey" owner directly
    HINT:  Alter the table instead.
    ```
    
    Note that for temp table, we continue to follow PG's error messages. I
    found that PG's error message for temp table is issued in the work flow
    after YB issued the AlterTable RPC request to DocDB which would have already
    caused the OBJECT_NOT_FOUND error. So YB has to report error prior to
    sending AlterTable RPC. In particular, OBJECT_NOT_FOUND was hit before
    ATExecChangeOwner is invoked which would have been a better place
    to report the error.
    
    In native PG, it issued a WARNING for temp table, but there is no error
    for a regular table:
    ```
    postgres=# create temp table foo(id int, primary key(id));
    CREATE TABLE
    postgres=# alter index foo_pkey OWNER TO yugabyte;
    WARNING:  cannot change owner of index "foo_pkey"
    HINT:  Change the ownership of the index's table, instead.
    ALTER INDEX
    ```
    
    ```
    postgres=# create table foo(id int, primary key(id));
    CREATE TABLE
    postgres=# alter index foo_pkey OWNER TO postgres;
    ALTER INDEX
    ```
    
    YB is already issuing ERROR on a similar case that is also YB restriction:
    ```
    yugabyte=# ALTER INDEX foo_pkey SET TABLESPACE fast_storage;
    ERROR:  cannot set tablespace for primary key index
    ```
    Jira: DB-18750
    
    Original commit: b498bf89bc77d94bdc7a2a132cb05ec4f2a9750d / D47714
    
    Test Plan: ./yb_build.sh release --java-test org.yb.pgsql.TestPgRegressTable
    
    Reviewers: fizaa, sanketh, #db-approvers
    
    Reviewed By: fizaa, #db-approvers
    
    Subscribers: yql
    
    Differential Revision: https://phorge.dev.yugabyte.com/D47735

--- src/postgres/src/backend/tcop/utility.c
+++ src/postgres/src/backend/tcop/utility.c
@@ -1166,6 +1166,47 @@ standard_ProcessUtility(PlannedStmt *pstmt,
 }
 
 /*
+ * Disallow ALTER INDEX primary_key change owner because there
+ * is no separate index table for the primary key in Yugabyte.
+ */
+static void
+YbCheckAlterPrimaryIndex(const AlterTableStmt *atstmt, Oid relid)
+{
+	char *disallowed_type = NULL;
+	ListCell   *cell;
+	/*
+	 * Find the first disallowed command type, currently only AT_ChangeOwner.
+	 */
+	foreach(cell, atstmt->cmds)
+	{
+		AlterTableCmd *cmd = (AlterTableCmd *) lfirst(cell);
+
+		if (cmd->subtype == AT_ChangeOwner)
+		{
+			disallowed_type = " owner ";
+			break;
+		}
+	}
+
+	if (!disallowed_type)
+		return;
+
+	Relation rel = RelationIdGetRelation(relid);
+	if (!rel)
+		return;
+
+	bool is_primary_key = rel->rd_index && rel->rd_index->indisprimary;
+	bool is_temp = rel->rd_rel->relpersistence == RELPERSISTENCE_TEMP;
+	RelationClose(rel);
+	if (is_primary_key && !is_temp)
+		ereport(ERROR,
+				(errcode(ERRCODE_INVALID_TABLE_DEFINITION),
+				 errmsg("cannot alter the primary key index \"%s\"%sdirectly",
+						atstmt->relation->relname, disallowed_type),
+				 errhint("Alter the table instead.")));
+}
+
+/*
  * The "Slow" variant of ProcessUtility should only receive statements
  * supported by the event triggers facility.  Therefore, we always
  * perform the trigger support calls if the context allows it.
@@ -1386,6 +1427,9 @@ ProcessUtilitySlow(ParseState *pstate,
 
 					if (OidIsValid(relid))
 					{
+						if (IsYugaByteEnabled() && !YBCIsInitDbModeEnvVarSet())
+							YbCheckAlterPrimaryIndex(atstmt, relid);
+
 						AlterTableUtilityContext atcontext;
 
 						/* Set up info needed for recursive callbacks ... */
