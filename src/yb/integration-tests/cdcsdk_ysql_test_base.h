72ecfe448c684942af79b9fef3ad82058a253eae
Sumukh-Phalgaonkar <sumukhphalgaonkar@gmail.com>
2025-10-29 19:01:27 +0530

    [BACKPORT 2025.1.1][#29136] CDC: Fix issue with virtual wal restart after DDL
    Summary:
    ##### Backport Description
    No merge conflicts were encountered.
    
    ##### Original Description
    We do not generate LSN for DDL records. As a result there is no direct acknowledgement received by the virtual WAL for these records. Before this change, a DDL was considered acknowledged if the transaction shipped just before the DDL was acknowledged. This was not by design. This buggy behaviour resulted in movement of explicit checkpoint forward on tablets such that the the explicit checkpoint would now point to records with commit time higher than the restart time.
    
    Upon restart, the walsender would fetch schema as of the restart time but would receive records with commit time higher than the DDL, thus failing to decode records due to schema mismatch.
    
    This revision fixes this issue by doing the following:
      - As soon as a DDL is encountered we store its commit time in the class variable `last_seen_ddl_commit_time_`.
      - We suspend sending explicit checkpoint in the subsequent GetChanges requests until the restart time is strictly greater than the `last_seen_ddl_commit_time_`. The "strictly greater" check here is because walsender sends the RELATION message corresponding to a DDL with the next DML it encounters. Receiving an acknowledgement for the next DML indicates that now we can move the explicit checkpoint beyond the DDL.
      - If multiple DDLs are encountered, then sending of explicit checkpoint is suspended till the restart time moves beyond the commit time of the last (latest) DDL. This is fine since we do not expect to see too many continuous DDLs in a typical system.
    Jira: DB-18900
    
    Original commit: 921b898d84ed91196011e8139735ced59ee7c225 / D47822
    
    Test Plan:
    Jenkins: test regex: .*(CDC|ReplicationSlot).*
    
    Following new tests have been added:
    ```
    ./yb_build.sh --java-test 'org.yb.pgsql.TestPgReplicationSlot#testRestartAfterDdl'
    ./yb_build.sh --cxx-test integration-tests_cdcsdk_consumption_consistent_changes-test --gtest_filter CDCSDKConsumptionConsistentChangesTest.TestExplcictCheckpointMovementAfterDDLWithActivityPostDDL
    ./yb_build.sh --cxx-test integration-tests_cdcsdk_consumption_consistent_changes-test --gtest_filter CDCSDKConsumptionConsistentChangesTest.TestExplcictCheckpointMovementAfterDDLWithNoActivityPostDDL
    ./yb_build.sh --cxx-test integration-tests_cdcsdk_consumption_consistent_changes-test --gtest_filter CDCSDKConsumptionConsistentChangesTest.TestExplcictCheckpointMovementAfterMultipleDDL
    ```
    
    Reviewers: skumar, asrinivasan
    
    Reviewed By: asrinivasan
    
    Subscribers: ycdcxcluster
    
    Tags: #jenkins-ready
    
    Differential Revision: https://phorge.dev.yugabyte.com/D47841

--- src/yb/integration-tests/cdcsdk_ysql_test_base.h
+++ src/yb/integration-tests/cdcsdk_ysql_test_base.h
@@ -139,6 +139,7 @@ DECLARE_int32(retryable_request_timeout_secs);
 DECLARE_bool(save_index_into_wal_segments);
 DECLARE_bool(TEST_skip_process_apply);
 DECLARE_bool(TEST_ysql_yb_enable_implicit_dynamic_tables_logical_replication);
+DECLARE_bool(cdcsdk_update_restart_time_when_nothing_to_stream);
 
 namespace yb {
 
@@ -653,6 +654,9 @@ class CDCSDKYsqlTest : public CDCSDKTestBase {
           "Tablets in cdc_state for the stream doesnt match the expected set");
 
   Result<int> GetStateTableRowCount();
+
+  Result<OpId> GetCheckpointFromStateTable(
+      const xrepl::StreamId& stream_id, const TabletId& tablet_id);
 
   Status VerifyStateTableAndStreamMetadataEntriesCount(
       const xrepl::StreamId& stream_id, const size_t& state_table_entries,
