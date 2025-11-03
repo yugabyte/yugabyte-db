57b01eb3e00baedbefaa09faf00d13c5077f5593
Daniel Shubin <dshubin@yugabyte.com>
2025-11-03 21:40:51 +0000

    [BACKPORT 2025.2][PLAT-18765] Support python 12 and 13 in YBA
    Summary:
    Add newer python version support
    Original commit: eb471a189138b3f89a47354969e75c39c3ac8ec8 / D47862
    
    Test Plan: itest
    
    Reviewers: anijhawan, nbhatia, nsingh
    
    Reviewed By: anijhawan, nsingh
    
    Subscribers: nikhil, yugaware
    
    Differential Revision: https://phorge.dev.yugabyte.com/D47978

--- managed/yba-installer/pkg/preflight/checks/python.go
+++ managed/yba-installer/pkg/preflight/checks/python.go
@@ -17,7 +17,12 @@ import (
 // Python checks to ensure the correct version of python exists
 var Python = &pythonCheck{"python", false}
 
-var pythonBinaryNames = []string{"python3.11", "python3.10", "python3"}
+var pythonBinaryNames = []string{
+	"python3",
+	"python3.10",
+	"python3.11",
+	"python3.12",
+	"python3.13"}
 
 var pythonVersionRegex = regexp.MustCompile(`Python (\d+)\.(\d+)`)
 
@@ -58,14 +63,15 @@ func (p pythonCheck) Execute() Result {
 		majorVersion, _ := strconv.Atoi(match[1])
 		minorVersion, _ := strconv.Atoi(match[2])
 		// Allowing python 3.10 or 3.11, as defined by LINUX_PLATFORMS in common.sh
-		if majorVersion == 3 && minorVersion >= 10 && minorVersion <= 11 {
+		if majorVersion == 3 && minorVersion >= 10 && minorVersion <= 13 {
 			log.Info("System meets Python installation requirements with version " + outputTrimmed)
 			return res
 		}
 		log.Warn("Found " + outputTrimmed + " on system but is not allowed Python version.")
 	}
 
-	res.Error = fmt.Errorf("System does not meet Python requirements. Please install Python 3.10 or 3.11.")
+	res.Error = fmt.Errorf("System does not meet Python requirements. Please install Python " +
+		"3.10, 3.11, 3.12, or 3.13.")
 	res.Status = StatusCritical
 	return res
 }
