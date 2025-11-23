4475c2f116350da1fab5f9d8da494179aa70b1ed
Zachary Drudi <zdrudi@yugabyte.com>
2025-11-23 12:50:23 -0500

    [#29454] ysqlconnmanager: fix race between ysql lease and conn manager
    Summary:
    With the ysql lease enabled, tservers do not accept pg connections until after they have acquired a ysql lease from the master leader. So tests that do not wait for long enough before trying to create a pg connection may fail. This diff fixes such failures for the ysql connection manager java tests.
    
    These tests became flaky after 3aaba7b3c3a46d2bc3cea8d3885e2e0964fac91e. However this race was introduced with the ysql lease work, for example 53146f9da4877244f931761920a5159a5b8da412 which enabled the lease by default.
    
    Test Plan: existing tests
    
    Reviewers: hsunder, sergei
    
    Reviewed By: hsunder, sergei
    
    Subscribers: rthallam, ybase
    
    Differential Revision: https://phorge.dev.yugabyte.com/D48492

--- java/yb-ysql-conn-mgr/src/test/java/org/yb/ysqlconnmgr/TestUnixSocketConnections.java
+++ java/yb-ysql-conn-mgr/src/test/java/org/yb/ysqlconnmgr/TestUnixSocketConnections.java
@@ -16,39 +16,41 @@ package org.yb.ysqlconnmgr;
 import static org.yb.AssertionWrappers.*;
 
 import java.sql.Connection;
-import java.sql.Statement;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.Map;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.yb.minicluster.MiniYBClusterBuilder;
+import org.yb.pgsql.ConnectionBuilder;
 import org.yb.pgsql.ConnectionEndpoint;
 
 @RunWith(value = YBTestRunnerYsqlConnMgr.class)
 public class TestUnixSocketConnections extends BaseYsqlConnMgr {
 
-    @Override
-    protected void customizeMiniClusterBuilder(MiniYBClusterBuilder builder) {
-        super.customizeMiniClusterBuilder(builder);
-        builder.addCommonTServerFlag("ysql_enable_auth", "true");
-        builder.addCommonTServerFlag("ysql_conn_mgr_password", "wrong pass");
-    }
+  @Override
+  protected void customizeMiniClusterBuilder(MiniYBClusterBuilder builder) {
+    super.customizeMiniClusterBuilder(builder);
+    builder.addCommonTServerFlag("ysql_enable_auth", "true");
+    builder.addCommonTServerFlag("ysql_conn_mgr_password", "wrong pass");
+  }
 
-    // TODO(mkumar) GH #19622 Enable the support for restarting Mini Cluster
-    // in Ysql Conn Mgr Tests
-    @Test
-    public void testUnixSocketConnections() throws Exception {
-        try (Connection conn = getConnectionBuilder()
-                    .withConnectionEndpoint(ConnectionEndpoint.YSQL_CONN_MGR)
-                    .withUser("yugabyte")
-                    .withPassword("yugabyte")
-                    .connect();)
-        {
-        } catch (Exception e) {
-            LOG.error("Got an unexpected error while creating a connection: ", e);
-            fail("connection faced an unexpected issue");
-        }
+  @Override
+  public ConnectionBuilder connectionBuilderForVerification(ConnectionBuilder builder) {
+    return builder.withUser("yugabyte").withPassword("yugabyte");
+  }
+
+  // TODO(mkumar) GH #19622 Enable the support for restarting Mini Cluster
+  // in Ysql Conn Mgr Tests
+  @Test
+  public void testUnixSocketConnections() throws Exception {
+    try (Connection conn = getConnectionBuilder()
+         .withConnectionEndpoint(ConnectionEndpoint.YSQL_CONN_MGR)
+         .withUser("yugabyte")
+         .withPassword("yugabyte")
+         .connect();)
+      {
+      } catch (Exception e) {
+      LOG.error("Got an unexpected error while creating a connection: ", e);
+      fail("connection faced an unexpected issue");
     }
+  }
 }
