yugabyte/yugabyte-db

commit 89d841944e07217a125db64a2b1bb0457cfdb77e

[BACKPORT 2025.2] [#28968] DocDB: Allow creating thread failure when shared memory is used for postgres queries
Summary:
We could use shared memory to receive queries from postgres in TServer (on by default in 2024.2).
In this case we use a separate thread per postgres connection.
In 2024.2 and before when creating such thread fails the whole TServer process crashes (in 2024).
In 2025.1 and after creating such thread silently ignored and postgres connection becomes unusable.

This diff adds logic to correctly handle thread creation failure (i.e. don't ignore in newer releases) and fallback to RPC mechanism when thread was not started (suitable for all releases).
Jira: DB-18711
Original commit: 9e22b13fc76a4bf51454da1a24308eecd655966f / D47537

Test Plan: PgSharedMemTest.ThreadStartFailure

Reviewers: esheng, rthallam, #db-approvers

Reviewed By: rthallam, #db-approvers

Subscribers: svc_phabricator, ybase, yql

Tags: #jenkins-ready

Differential Revision: https://phorge.dev.yugabyte.com/D47823

src/yb/tserver/pg_client_service.cc
@@ -217,7 +217,7 @@ class LockablePgClientSession {
         lifetime_(lifetime), expiration_(NewExpiration()) {
   }
 
-  Status StartExchange(const std::string& instance_id, ThreadPool& thread_pool) {
+  Status StartExchange(const std::string& instance_id, YBThreadPool& thread_pool) {
     shared_mem_manager_ = VERIFY_RESULT(PgSessionSharedMemoryManager::Make(
         instance_id, id(), Create::kTrue));
     session_.SetupSharedObjectLocking(shared_mem_manager_.object_locking_data());
@@ -667,12 +667,12 @@ class PgClientServiceImpl::Impl : public SessionProvider {
     resp->set_session_id(session_id);
     if (FLAGS_pg_client_use_shared_memory) {
       std::call_once(exchange_thread_pool_once_flag_, [this] {
-        CHECK_OK(ThreadPoolBuilder("shmem_exchange")
-                     .set_min_threads(1)
-                     .unlimited_threads()
-                     .set_idle_timeout(FLAGS_shmem_exchange_idle_timeout_ms * 1ms)
-                     .set_max_queue_size(0)
-                     .Build(&exchange_thread_pool_));
+        exchange_thread_pool_ = std::make_unique<YBThreadPool>(ThreadPoolOptions {
+          .name = "shmem_exchange",
+          .max_workers = ThreadPoolOptions::kUnlimitedWorkersWithoutQueue,
+          .min_workers = 1,
+          .idle_timeout = MonoDelta::FromMilliseconds(FLAGS_shmem_exchange_idle_timeout_ms),
+        });
       });
       auto status = session_info->session().StartExchange(instance_id_, *exchange_thread_pool_);
       if (status.ok()) {
@@ -2558,7 +2558,7 @@ class PgClientServiceImpl::Impl : public SessionProvider {
   }
 
   Result<SessionInfoPtr> GetSessionInfo(uint64_t session_id) {
-    DCHECK_NE(session_id, 0);
+    RSTATUS_DCHECK_NE(session_id, static_cast<uint64_t>(0), InvalidArgument, "Bad session id");
     SharedLock lock(mutex_);
     auto it = sessions_.find(session_id);
     if (PREDICT_FALSE(it == sessions_.end())) {
@@ -2749,7 +2749,7 @@ class PgClientServiceImpl::Impl : public SessionProvider {
 
   const std::string instance_id_;
   std::once_flag exchange_thread_pool_once_flag_;
-  std::unique_ptr<ThreadPool> exchange_thread_pool_;
+  std::unique_ptr<YBThreadPool> exchange_thread_pool_;
 
   PgSharedMemoryPool shared_mem_pool_;
 

src/yb/tserver/tserver_shared_mem.cc
@@ -465,10 +465,16 @@ SharedExchangeRunnable::SharedExchangeRunnable(
     : exchange_(exchange), session_id_(session_id), listener_(listener) {
 }
 
-Status SharedExchangeRunnable::Start(ThreadPool& thread_pool) {
-  RETURN_NOT_OK(thread_pool.Submit(shared_from_this()));
-  stop_latch_.Reset(1);
+Status SharedExchangeRunnable::Start(YBThreadPool& thread_pool) {
+  stop_future_ = stop_promise_.get_future();
+  if (!thread_pool.Enqueue(this)) {
+    return stop_future_.get();
+  }
   return Status::OK();
+}
+
+void SharedExchangeRunnable::Done(const Status& status) {
+  stop_promise_.set_value(status);
 }
 
 void SharedExchangeRunnable::Run() {
@@ -483,7 +489,6 @@ void SharedExchangeRunnable::Run() {
     }
     listener_(*query_size);
   }
-  stop_latch_.CountDown();
 }
 
 SharedExchangeRunnable::~SharedExchangeRunnable() {
@@ -492,13 +497,19 @@ SharedExchangeRunnable::~SharedExchangeRunnable() {
 }
 
 void SharedExchangeRunnable::StartShutdown() {
-  if (stop_latch_.count()) {
+  if (stop_future_.valid()) {
     exchange_.SignalStop();
   }
 }
 
 void SharedExchangeRunnable::CompleteShutdown() {
-  stop_latch_.Wait();
+  if (stop_future_.valid()) {
+    stop_future_.get();
+  }
+}
+
+bool SharedExchangeRunnable::ReadyToShutdown() const {
+  return !stop_future_.valid() || stop_future_.wait_for(0s) == std::future_status::ready;
 }
 
 class PgSessionSharedMemoryManager::Impl {

src/yb/tserver/tserver_shared_mem.h
@@ -247,7 +247,7 @@ class SharedExchange {
 using SharedExchangeListener = std::function<void(size_t)>;
 
 class SharedExchangeRunnable :
-    public Runnable, public std::enable_shared_from_this<SharedExchangeRunnable> {
+    public ThreadPoolTask, public std::enable_shared_from_this<SharedExchangeRunnable> {
  public:
   SharedExchangeRunnable(
       SharedExchange& exchange, uint64_t session_id, const SharedExchangeListener& listener);
@@ -258,22 +258,22 @@ class SharedExchangeRunnable :
     return exchange_;
   }
 
-  bool ReadyToShutdown() const {
-    return stop_latch_.count() == 0;
-  }
+  bool ReadyToShutdown() const;
 
-  Status Start(ThreadPool& thread_pool);
+  Status Start(YBThreadPool& thread_pool);
 
   void StartShutdown();
   void CompleteShutdown();
 
  private:
   void Run() override;
+  void Done(const Status& status) override;
 
   SharedExchange& exchange_;
   uint64_t session_id_;
   SharedExchangeListener listener_;
-  CountDownLatch stop_latch_{0};
+  std::promise<Status> stop_promise_;
+  std::shared_future<Status> stop_future_;
 };
 
 struct SharedExchangeMessage {

src/yb/util/thread_pool.cc
@@ -21,6 +21,7 @@
 
 #include <boost/intrusive/list.hpp>
 
+#include "yb/util/debug-util.h"
 #include "yb/util/flags.h"
 #include "yb/util/lockfree.h"
 #include "yb/util/scope_exit.h"
@@ -35,6 +36,8 @@ DEFINE_NON_RUNTIME_uint64(default_idle_timeout_ms, 15000,
     "Default RPC YBThreadPool idle timeout value in milliseconds");
 
 namespace yb {
+
+bool TEST_fail_to_create_second_thread_in_thread_pool_without_queue = false;
 
 namespace {
 
@@ -280,7 +283,7 @@ class YBThreadPool::Impl {
       : share_(std::move(options)) {
     LOG(INFO) << "Starting thread pool " << share_.options.ToString();
     for (size_t index = 0; index != options.min_workers; ++index) {
-      if (!TryStartNewWorker(nullptr, /* persistent = */ true)) {
+      if (!TryStartNewWorker(nullptr, /* persistent = */ true).ok()) {
         break;
       }
     }
@@ -308,9 +311,16 @@ class YBThreadPool::Impl {
       return true;
     }
 
-    if (TryStartNewWorker(task, /* persistent= */ false)) {
-      --adding_;
-      return true;
+    {
+      auto start_worker_status = TryStartNewWorker(task, /* persistent= */ false);
+      if (start_worker_status.ok()) {
+        --adding_;
+        return true;
+      } else if (share_.options.max_workers == ThreadPoolOptions::kUnlimitedWorkersWithoutQueue) {
+        task->Done(start_worker_status);
+        --adding_;
+        return false;
+      }
     }
 
     // We can get here in 3 cases:
@@ -324,7 +334,7 @@ class YBThreadPool::Impl {
     return true;
   }
 
-  bool TryStartNewWorker(ThreadPoolTask* task, bool persistent) EXCLUDES(mutex_) {
+  Status TryStartNewWorker(ThreadPoolTask* task, bool persistent) EXCLUDES(mutex_) {
     Worker* worker = nullptr;
     if (share_.num_workers++ < share_.options.max_workers) {
       std::lock_guard lock(mutex_);
@@ -333,13 +343,20 @@ class YBThreadPool::Impl {
         workers_.push_back(*(worker = new_worker.release()));
       }
     }
+    Status status;
     if (worker) {
-      auto status = worker->Start(++worker_counter_, task);
+      if (TEST_fail_to_create_second_thread_in_thread_pool_without_queue &&
+          share_.options.max_workers == ThreadPoolOptions::kUnlimitedWorkersWithoutQueue &&
+          worker_counter_ != 0) {
+        status = STATUS_FORMAT(RuntimeError, "TEST: Artificial start thread failure");
+      } else {
+        status = worker->Start(++worker_counter_, task);
+      }
       if (status.ok()) {
         if (task && share_.options.metrics.queue_time_us_stats) {
           share_.options.metrics.queue_time_us_stats->Increment(0);
         }
-        return true;
+        return Status::OK();
       }
       bool empty;
       {
@@ -356,9 +373,13 @@ class YBThreadPool::Impl {
       } else {
         LOG_WITH_PREFIX(WARNING) << "Unable to start worker: " << status;
       }
+    } else {
+      static const Status kReachedWorkersLimitStatus = STATUS(
+          RuntimeError, "Reached workers limit");
+      status = kReachedWorkersLimitStatus;
     }
     --share_.num_workers;
-    return false;
+    return status;
   }
 
   // Returns true if we found worker that will pick up this task, false otherwise.

src/yb/util/thread_pool.h
@@ -142,6 +142,7 @@ struct ThreadPoolOptions {
   }
 
   static constexpr auto kUnlimitedWorkers = std::numeric_limits<decltype(max_workers)>::max();
+  static constexpr auto kUnlimitedWorkersWithoutQueue = kUnlimitedWorkers - 1;
 };
 
 // An object that can enqueue/submit tasks, e.g. a thread pool, a sub-pool, or a strand.

src/yb/yql/pggate/pg_client.cc
@@ -560,7 +560,10 @@ class PgClient::Impl : public BigDataFetcher {
     proxy_->HeartbeatAsync(
         req, &heartbeat_resp_, PrepareHeartbeatController(),
         [this, create] {
-      auto status = ResponseStatus(heartbeat_resp_);
+      auto status = heartbeat_controller_.status();
+      if (status.ok()) {
+        status = ResponseStatus(heartbeat_resp_);;
+      }
       if (create) {
         if (!status.ok()) {
           create_session_promise_.set_value(status);

src/yb/yql/pgwrapper/pg_shared_mem-test.cc
@@ -38,6 +38,13 @@ DECLARE_int32(TEST_transactional_read_delay_ms);
 DECLARE_uint64(big_shared_memory_segment_expiration_time_ms);
 DECLARE_uint64(big_shared_memory_segment_session_expiration_time_ms);
 
+
+namespace yb {
+
+extern bool TEST_fail_to_create_second_thread_in_thread_pool_without_queue;
+
+}
+
 namespace yb::pgwrapper {
 
 class PgSharedMemTest : public PgMiniTestBase {
@@ -72,16 +79,29 @@ class PgSharedMemTest : public PgMiniTestBase {
   void OverrideMiniClusterOptions(MiniClusterOptions* options) override {
     options->wait_for_pg = false;
   }
+
+  void TestSimple();
 };
 
-TEST_F(PgSharedMemTest, Simple) {
+void PgSharedMemTest::TestSimple() {
   auto conn = ASSERT_RESULT(Connect());
 
   ASSERT_OK(conn.Execute("CREATE TABLE t (key INT PRIMARY KEY, value TEXT)"));
   ASSERT_OK(conn.Execute("INSERT INTO t (key, value) VALUES (1, 'hello')"));
 
-  auto value = ASSERT_RESULT(conn.FetchRow<std::string>("SELECT value FROM t WHERE key = 1"));
+  auto conn2 = ASSERT_RESULT(Connect());
+  auto value = ASSERT_RESULT(conn2.FetchRow<std::string>("SELECT value FROM t WHERE key = 1"));
   ASSERT_EQ(value, "hello");
+}
+
+TEST_F(PgSharedMemTest, Simple) {
+  TestSimple();
+}
+
+TEST_F(PgSharedMemTest, ThreadStartFailure) {
+  TEST_fail_to_create_second_thread_in_thread_pool_without_queue = true;
+
+  TestSimple();
 }
 
 TEST_F(PgSharedMemTest, Restart) {

commit 6e3e7193c189bd383fded53caf92521bef6e1c0e

[#29242] YSQL: Exit process on pggate heartbeat failure during init
Summary:
D47823 / 89d841944e07217a125db64a2b1bb0457cfdb77e introduced the handling of errors in the heartbeat RPC between pggate and its local tserver.
A new backend acquires a session ID from its local tserver as part of its first heartbeat during the initialization of PgClient. This session ID is used in all future communication between this backend and the tserver.
Therefore, if a backend fails to acquire a session ID due to a failure of its first heartbeat, no meaningful work can be done by the backend and it must exit itself.
Currently, a `CHECK_OK` asserts that the first heartbeat succeeds during the initialization of PgClient. When the first heartbeat fails, the `CHECK_OK` causes the backend to abort itself, resulting in a core dump.

This revision reworks the error handling for the failure of the first heartbeat by:
 - Removing the `CHECK_OK` macro.
 - Propagating the error as a `Status` up to Postgres.
 - `FATAL`ing the process in Postgres which does not produce a core dump by default.

Transient network failures are not rare. Skipping the production of core dumps results in lower noise in QA stress tests and fewer support engagements in customer clusters (as alerts are raised when cores are produced).

Test Plan:
Run the following test:
```
./yb_build.sh --cxx-test pgwrapper_pg_mini-test --gtest-filter 'PgHeartbeatFailureTest.MockTransientHeartbeatFailure'
```

Reviewers: schandra, amartsinchyk, mihnea, jason, #db-approvers

Reviewed By: jason, #db-approvers

Subscribers: rthallam, svc_phabricator, dmitry, ybase, smishra, yql

Tags: #jenkins-ready

Differential Revision: https://phorge.dev.yugabyte.com/D48477

src/postgres/src/backend/utils/misc/pg_yb_utils.c
@@ -1154,8 +1154,10 @@ YBInitPostgresBackend(const char *program_name, const YbcPgInitPostgresInfo *ini
 			.shared_data = &MyProc->yb_shared_data
 		};
 
-		YBCInitPgGate(YbGetTypeTable(), &callbacks,
-					  init_info ? init_info : &default_init_info, &ash_config);
+		HandleYBStatusAtErrorLevel(YBCInitPgGate(YbGetTypeTable(),
+												 &callbacks,
+												 init_info ? init_info : &default_init_info,
+												 &ash_config), FATAL);
 		YBCInstallTxnDdlHook();
 
 		/*

src/yb/tserver/pg_client_service.cc
@@ -3046,6 +3046,18 @@ PgClientServiceMockImpl::Handle PgClientServiceMockImpl::SetMock(
   return Handle{std::move(mock)};
 }
 
+void PgClientServiceMockImpl::UnsetMock(const std::string& method) {
+  std::lock_guard lock(mutex_);
+  auto it = mocks_.find(method);
+  if (it == mocks_.end()) {
+    LOG(WARNING) << "No mock found for method: " << method;
+    return;
+  }
+
+  LOG(INFO) << "Resetting mock for method: " << method;
+  mocks_.erase(it);
+}
+
 Result<bool> PgClientServiceMockImpl::DispatchMock(
     const std::string& method, const void* req, void* resp, rpc::RpcContext* context) {
   SharedFunctor mock;

src/yb/tserver/pg_client_service.h
@@ -203,6 +203,8 @@ class PgClientServiceMockImpl : public PgClientServiceIf {
     return STATUS(NotSupported, "Mocking PollVectorIndexReady is not supported");
   }
 
+  void UnsetMock(const std::string& method);
+
  private:
   PgClientServiceIf* impl_;
   std::unordered_map<std::string, SharedFunctor::weak_type> mocks_;

src/yb/yql/pggate/pggate.cc
@@ -688,15 +688,19 @@ PgApiImpl::PgApiImpl(
   std::memcpy(ash_config.top_level_node_id, tserver_shared_object_->tserver_uuid(), kUuidSize);
   wait_state_ = ash::WaitStateInfo::CreateIfAshIsEnabled<ash::PgWaitStateInfo>(ash_config);
   ash::WaitStateInfo::SetCurrentWaitState(wait_state_);
+}
 
-  CHECK_OK(interrupter_->Start());
-  CHECK_OK(clock_->Init());
+Status PgApiImpl::StartPgApi(const YbcPgInitPostgresInfo& init_postgres_info) {
+  RETURN_NOT_OK(interrupter_->Start());
+  RETURN_NOT_OK(clock_->Init());
 
-  CHECK_OK(pg_client_.Start(
+  RETURN_NOT_OK(pg_client_.Start(
       proxy_cache_.get(), &messenger_holder_.messenger->scheduler(),
       *tserver_shared_object_,
       init_postgres_info.parallel_leader_session_id
           ? std::optional(*init_postgres_info.parallel_leader_session_id) : std::nullopt));
+
+  return Status::OK();
 }
 
 PgApiImpl::~PgApiImpl() {

src/yb/yql/pggate/pggate.h
@@ -132,6 +132,8 @@ class PgApiImpl {
   void ResetCatalogReadTime();
   [[nodiscard]] ReadHybridTime GetCatalogReadTime() const;
 
+  Status StartPgApi(const YbcPgInitPostgresInfo& init_postgres_info);
+
   // Initialize a session to process statements that come from the same client connection.
   void InitSession(YbcPgExecStatsState& session_stats, bool is_binary_upgrade);
 

src/yb/yql/pggate/ybc_pggate.cc
@@ -171,7 +171,7 @@ inline std::optional<Bound> MakeBound(YbcPgBoundType type, uint16_t value) {
   return Bound{.value = value, .is_inclusive = (type == YB_YQL_BOUND_VALID_INCLUSIVE)};
 }
 
-void InitPgGateImpl(
+Status InitPgGateImpl(
     YbcPgTypeEntities type_entities, const YbcPgCallbacks& pg_callbacks,
     YbcPgAshConfig& ash_config, const YbcPgInitPostgresInfo& init_postgres_info) {
   // TODO: We should get rid of hybrid clock usage in YSQL backend processes (see #16034).
@@ -192,8 +192,10 @@ void InitPgGateImpl(
 
   pgapi_shutdown_done.exchange(false);
   pgapi = new PgApiImpl(type_entities, pg_callbacks, init_postgres_info, ash_config);
+  RETURN_NOT_OK(pgapi->StartPgApi(init_postgres_info));
 
   VLOG(1) << "PgGate open";
+  return Status::OK();
 }
 
 Status PgInitSessionImpl(YbcPgExecStatsState& session_stats, bool is_binary_upgrade) {
@@ -497,13 +499,12 @@ YbcPgTabletsDescriptor MakeYbcPgTabletsDescriptor(const tablet::TabletStatusPB& 
 
 extern "C" {
 
-void YBCInitPgGate(
+YbcStatus YBCInitPgGate(
     YbcPgTypeEntities type_entities, const YbcPgCallbacks *pg_callbacks,
     const YbcPgInitPostgresInfo *init_postgres_info, YbcPgAshConfig *ash_config) {
-  CHECK_OK(WithMaskedYsqlSignals(
+  return ToYBCStatus(WithMaskedYsqlSignals(
       [&type_entities, pg_callbacks, init_postgres_info, ash_config]() -> Status {
-        InitPgGateImpl(type_entities, *pg_callbacks, *ash_config, *init_postgres_info);
-        return Status::OK();
+        return InitPgGateImpl(type_entities, *pg_callbacks, *ash_config, *init_postgres_info);
   }));
 }
 

src/yb/yql/pggate/ybc_pggate.h
@@ -45,7 +45,7 @@ typedef struct {
 
 // This must be called exactly once to initialize the YB/PostgreSQL gateway API before any other
 // functions in this API are called.
-void YBCInitPgGate(
+YbcStatus YBCInitPgGate(
     YbcPgTypeEntities type_entities, const YbcPgCallbacks* pg_callbacks,
     const YbcPgInitPostgresInfo *init_postgres_info, YbcPgAshConfig* ash_config);
 

src/yb/yql/pgwrapper/pg_mini-test.cc
@@ -2990,6 +2990,86 @@ TEST_P(PgRecursiveAbortTest, MockAbortFailure) {
   ASSERT_EQ(conn.ConnStatus(), CONNECTION_BAD);
 }
 
+class PgHeartbeatFailureTest : public PgMiniTestSingleNode {
+ public:
+  void SetUp() override {
+    ANNOTATE_UNPROTECTED_WRITE(FLAGS_TEST_enable_pg_client_mock) = true;
+    PgMiniTest::SetUp();
+  }
+
+  template <class F>
+  tserver::PgClientServiceMockImpl::Handle MockHeartbeat(const F& mock) {
+    auto* client = cluster_->mini_tablet_server(0)->server()->TEST_GetPgClientServiceMock();
+    return client->MockHeartbeat(mock);
+  }
+
+  void UnsetHeartbeatMock() {
+    auto* client = cluster_->mini_tablet_server(0)->server()->TEST_GetPgClientServiceMock();
+    client->UnsetMock("Heartbeat");
+  }
+};
+
+static MonoDelta kHeartbeatFailureDuration = MonoDelta::FromSeconds(5);
+static CoarseTimePoint kFailureStart = CoarseTimePoint();
+
+Status MockHeartbeatFailure(
+    const yb::tserver::PgHeartbeatRequestPB* req,
+    yb::tserver::PgHeartbeatResponsePB* resp, yb::rpc::RpcContext* context) {
+  LOG(INFO) << "Heartbeat called for session: " << req->session_id();
+  if (kFailureStart == CoarseTimePoint()) {
+    kFailureStart = CoarseMonoClock::Now();
+  }
+
+  if (CoarseMonoClock::Now() - kFailureStart < kHeartbeatFailureDuration) {
+    return STATUS(NetworkError, "Mocking network failure on Heartbeat");
+  }
+
+  // Do not set the session ID. The client should create a new session.
+  return Status::OK();
+}
+
+TEST_F(PgHeartbeatFailureTest, MockTransientHeartbeatFailure) {
+  PGConn conn = ASSERT_RESULT(Connect());
+  auto _ = MockHeartbeat(MockHeartbeatFailure);
+
+  TestThreadHolder thread_holder;
+  thread_holder.AddThreadFunctor([this] {
+    SleepFor(5s);
+    UnsetHeartbeatMock();
+  });
+
+  uint nfailures = 0;
+  ASSERT_OK(WaitFor([this, &nfailures]() {
+    auto result = ConnectToDB(std::string() /* dbname */, 1 /* timeout */ );
+    if (!result.ok()) {
+      nfailures++;
+      return false;
+    }
+
+    // Validate that once a heartbeat is successful, the connection can service
+    // queries.
+    PGConn conn2 = std::move(*result);
+    auto status = conn2.Execute("CREATE TABLE t1 (k INT)");
+    if (!status.ok()) {
+      nfailures++;
+      return false;
+    }
+
+    return true;
+  }, 10s, "Transient failure timeout", 1s));
+
+  thread_holder.JoinAll();
+  // Validate that at least one new connection experienced heartbeat failure.
+  ASSERT_GT(nfailures, 0);
+
+  // The old connection should still work as it received a valid session ID from
+  // the tserver before the mock was installed. Validate that it can service
+  // queries.
+  ASSERT_OK(conn.Execute("INSERT INTO t1 VALUES (1)"));
+  auto nrows = ASSERT_RESULT(conn.FetchRow<PGUint64>("SELECT COUNT(*) FROM t1"));
+  ASSERT_EQ(nrows, 1);
+}
+
 TEST_F(PgMiniTest, KillPGInTheMiddleOfBatcherOperation) {
   const std::string kTableName = "test_table";
   const auto kQuery = Format("SELECT * FROM $0", kTableName);