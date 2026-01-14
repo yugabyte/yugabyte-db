yugabyte/yugabyte-db

commit edbc6876e29e1fd4a7d1f9e9291c563eaed88df5

[#17289][#19781][#19800] YSQL: Error handling in Ysql Connection Manager in authentication passthrough
Summary:
This diff handles the scenario of an auth failure in Ysql Connection Manager. Two important issues related to this scenario were, the client getting a wrong error message and the control connection getting broken.
This diff also refactors the auth passthrough code.

**Old behaviour**
- Whenever the authentication fails, a FATAL or ERROR packet is sent by the pg_backend
- Backend connection gets destroyed whenever the pg_backend sends a FATAL or ERROR packet.
- If Odyssey receives a FATAL or ERROR packet it considers the authentication to be failed.

**New behaviour**
- Instead of sending a FATAL or ERROR packet, pg_backend will send a FORWARDASFATAL + WARNING packet.
- Here  ForwardAsFatal packet is used to identify that the next WARNING packet needs to be treated as a FATAL packet.
- ForwardAsFatal is needed so that Odyssey doesn't declare a connection as failed for any WARNING packet.
- Once Odyssey receives a ForwardAsFatal packet it forwards the next WARNING packet as a FATAL packet.
- Previously these WARNING packets would have come as an ERROR packet and were forwarded.

Changes for broken physical connection:
- pg_backend closes the connection after sending an ERROR or FATAL packet.
- In this diff, changes are made to declare the physical connection as offline when an ERROR/FATAL packet is received.
- Also wherever possible, the use of ERROR/FATAL packet is replaced with the use of ForwardAsFatal + WARNING packet.

**Upgrade/Rollback safety** :

There is no communication across nodes. So there is no issue of one node running an older version while the other nodes are on the newer version. So we do not need an auto flag.
Jira: DB-6514, DB-8675, DB-8713

Test Plan:
Trigger Jenkins
1. Test for broken physical connection:
```
./yb_build.sh --java-test org.yb.ysqlconnmgr.TestCorrectErrorMessage
```

2. Test for checking the error message:
```

src/odyssey/sources/yb_auth_passthrough.c
@@ -1,0 +1,506 @@
+/*-------------------------------------------------------------------------
+ *
+ * yb_auth_passthrough.c
+ * Utilities for Ysql Connection Manager/Yugabyte (Postgres layer) integration
+ * that have to be defined on the Ysql Connection Manager (Odyssey) side.
+ *
+ * Copyright (c) YugaByteDB, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License.  You may obtain a copy
+ * of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ *
+ * IDENTIFICATION
+ *	  sources/yb_auth_passthrough.c
+ *
+ *-------------------------------------------------------------------------
+ */
+
+#include <kiwi.h>
+#include <machinarium.h>
+#include <odyssey.h>
+
+#define YB_SHMEM_KEY_FORMAT "shmkey="
+#define CONTEXT_AUTH_PASSTHROUGH "yb auth passthrough"
+
+enum YB_CLI_AUTH_STATUS {
+	YB_CLI_AUTH_FAILED,
+	YB_CLI_AUTH_SUCCESS,
+	YB_CLI_AUTH_PROGRESS
+};
+
+/*
+ * Forward a "WARNING" packet as a "FATAL" packet to the client.
+ * WARNING packets are of type KIWI_BE_NOTICE_RESPONSE.
+ */
+static int yb_forward_fatal_msg(od_server_t *client,
+				machine_msg_t *warning_packet)
+{
+	int rc = 0;
+	kiwi_be_type_t type = *(char *)machine_msg_data(warning_packet);
+	kiwi_fe_error_t fatal_pkt;
+
+	if (type != KIWI_BE_NOTICE_RESPONSE)
+		goto errmsg;
+
+	rc = kiwi_fe_read_notice(machine_msg_data(warning_packet),
+				 machine_msg_size(warning_packet), &fatal_pkt);
+	if (rc == -1)
+		goto errmsg;
+
+	od_frontend_fatal_forward(client, fatal_pkt.code, fatal_pkt.message);
+	return 0;
+
+errmsg:
+	od_frontend_fatal(
+		client, KIWI_PROTOCOL_VIOLATION,
+		"Error while forwarding the FATAL packet comming from the pg_backend.");
+	return -1;
+}
+
+static void yb_control_connection_failed(od_server_t *server,
+					 od_instance_t *instance)
+{
+	od_debug(&instance->logger, CONTEXT_AUTH_PASSTHROUGH, NULL, server,
+		 "Control connection failed during the auth passthrough");
+
+	/*
+	 * GH issue #19781 This connection should not be kept in the pool,
+	 * mark it as offile to avoide the "Broken physical connection error".
+	 */
+	server->offline = 1;
+}
+
+static int yb_server_write_auth_passthroug_request_pkt(od_client_t *client,
+						       od_server_t *server,
+						       od_instance_t *instance)
+{
+	int rc = -1;
+	machine_msg_t *msg;
+	char client_address[128];
+	od_getpeername(client->io.io, client_address, sizeof(client_address), 1,
+		       0);
+
+	msg = kiwi_fe_write_authentication(NULL, client->startup.user.value,
+					   client->startup.database.value,
+					   client_address);
+
+	/* Send `Auth Passthrough Request` packet. */
+	rc = od_write(&server->io, msg);
+	if (rc == -1) {
+		yb_control_connection_failed(server, instance);
+		od_frontend_fatal(
+			client, KIWI_PROTOCOL_VIOLATION,
+			"Unable to send auth passthrough request, broken control connection");
+		return -1;
+	}
+
+	od_debug(
+		&instance->logger, CONTEXT_AUTH_PASSTHROUGH, client, server,
+		"Starting Auth Passthrough, sent 'Auth Passthrough Request' packet");
+
+	return 0;
+}
+
+static machine_msg_t *yb_read_auth_pkt_from_server(od_client_t *client,
+						   od_server_t *server,
+						   od_instance_t *instance)
+{
+	machine_msg_t *msg = NULL;
+
+	msg = od_read(&server->io, UINT32_MAX);
+	if (msg == NULL) {
+		if (!machine_timedout()) {
+			od_error(&instance->logger, CONTEXT_AUTH_PASSTHROUGH,
+				 server->client, server,
+				 "read error from server: %s",
+				 od_io_error(&server->io));
+			return NULL;
+		}
+	}
+
+	return msg;
+}
+
+/*
+ * In this scenario, pg_backend is expecting a packet from the client.
+ * Since the client exited before sending any packet, we need to make sure that
+ * pg_backend exits from the read loop.
+ *
+ * Send an empty password response packet leading to an auth failure
+ * at database side.
+ *
+ * This case is observed when connecting to the database via ysqlsh.
+ */
+
+static void yb_client_exit_mid_passthrough(od_server_t *server,
+					   od_instance_t *instance)
+{
+	machine_msg_t *msg = kiwi_fe_write_password(NULL, NULL, 0);
+	if (od_write(&server->io, msg) == -1) {
+		od_error(&instance->logger, CONTEXT_AUTH_PASSTHROUGH, NULL,
+			 server, "write error in sever: %s",
+			 od_io_error(&server->io));
+		server->offline = 1;
+	}
+}
+
+static int yb_forward_auth_pkt_client_to_server(od_client_t *client,
+						od_server_t *server,
+						od_instance_t *instance)
+{
+	machine_msg_t *msg;
+	kiwi_fe_type_t type;
+	int rc = -1;
+
+	/* Wait for password response packet from the client. */
+	while (true) {
+		msg = od_read(&client->io, UINT32_MAX);
+		if (msg == NULL) {
+			od_error(&instance->logger, CONTEXT_AUTH_PASSTHROUGH,
+				 client, NULL, "read error in middleware: %s",
+				 od_io_error(&client->io));
+			yb_client_exit_mid_passthrough(server, instance);
+			return -1;
+		}
+
+		type = *(char *)machine_msg_data(msg);
+
+		/*
+		 * Packet type `KIWI_FE_PASSWORD_MESSAGE` is used by client
+		 * to respond to the server packet for:
+		 * 		GSSAPI, SSPI and password response messages
+		 */
+		if (type == KIWI_FE_PASSWORD_MESSAGE)
+			break;
+		machine_msg_free(msg);
+	}
+
+	/* Forward the password response packet to the database. */
+	rc = od_write(&server->io, msg);
+	if (rc == -1) {
+		od_error(
+			&instance->logger, CONTEXT_AUTH_PASSTHROUGH, client,
+			server,
+			"Unable to forward the password response to the server");
+		return -1;
+	}
+
+	od_debug(&instance->logger, CONTEXT_AUTH_PASSTHROUGH, client, server,
+		 "Forwaded the password response to the server");
+	return 0;
+}
+
+/* Write a packet on the client socket */
+static int yb_client_write_pkt(od_client_t *client, od_server_t *server,
+			       od_instance_t *instance, machine_msg_t *msg,
+			       enum YB_CLI_AUTH_STATUS progress)
+{
+	int rc = od_write(&client->io, msg);
+	if (rc == -1) {
+		od_error(&instance->logger, CONTEXT_AUTH_PASSTHROUGH, client,
+			 NULL, "write error in middleware: %s",
+			 od_io_error(&client->io));
+
+		if (progress == YB_CLI_AUTH_PROGRESS) {
+			/*
+			 * pg_backend will be expecting a packet,
+			 * send an empty password packet.
+			 */
+			yb_client_exit_mid_passthrough(client->server,
+						       instance);
+		}
+
+		/* Since the client socket is closed, we need not send any fatal packet */
+		return -1;
+	}
+
+	return rc;
+}
+
+/*
+ * Read the authentication packets from the server,
+ * process it and forward it to the client.
+ */
+static enum YB_CLI_AUTH_STATUS
+yb_forward_auth_pkt_server_to_client(od_client_t *client, od_server_t *server,
+				     od_instance_t *instance)
+{
+	kiwi_be_type_t type;
+	machine_msg_t *msg = NULL;
+	int rc = -1;
+	int auth_pkt_type = 0;
+	enum YB_CLI_AUTH_STATUS progress = YB_CLI_AUTH_PROGRESS;
+
+	/* Forward all the packets comming from the server until
+	 * we receive a "AUTH" pkt */
+	while (true) {
+		msg = yb_read_auth_pkt_from_server(client, server, instance);
+
+		if (msg == NULL) {
+			server->offline = 1;
+			return YB_CLI_AUTH_FAILED;
+		}
+
+		type = *(char *)machine_msg_data(msg);
+		od_debug(&instance->logger, CONTEXT_AUTH_PASSTHROUGH,
+			 server->client, server,
+			 "Got %s packet from the server",
+			 kiwi_be_type_to_string(type));
+
+		/*
+		 * Here, expected packet can be of the following type
+		 * 		1. KIWI_BE_AUTHENTICATION
+		 * 		2. KIWI_BE_NOTICE_RESPONSE
+		 * 		3. KIWI_BE_ERROR_RESPONSE
+		 * 		4. YB_KIWI_BE_FATAL_FOR_LOGICAL_CONNECTION
+		 * For any other packet we need to report an error.
+		 */
+		switch (type) {
+		case KIWI_BE_AUTHENTICATION:
+			/*
+			 * If the packet is of type:
+			 * 		1. AUTHOK --> forward it and mark the client authenticated.
+			 * 		2. AUTHFAILED --> forward the next notice packet as the FATAL
+			 */
+			auth_pkt_type = yb_kiwi_fe_auth_packet_type(
+				machine_msg_data(msg), machine_msg_size(msg));
+
+			if (auth_pkt_type < 0) {
+				od_error(
+					&instance->logger,
+					CONTEXT_AUTH_PASSTHROUGH, NULL, server,
+					"failed to parse authentication message");
+				yb_control_connection_failed(server, instance);
+				od_frontend_fatal(
+					client, KIWI_PROTOCOL_VIOLATION,
+					"Unable to read the control connection packet");
+				return YB_CLI_AUTH_FAILED;
+			}
+
+			if (auth_pkt_type == 0) /* AUTHOK pkt */
+				progress = YB_CLI_AUTH_SUCCESS;
+			else
+				progress = YB_CLI_AUTH_PROGRESS;
+
+			rc = yb_client_write_pkt(client, server, instance, msg,
+						 progress);
+			if (rc == -1) {
+				return YB_CLI_AUTH_FAILED;
+			}
+
+			return progress;
+
+		case KIWI_BE_NOTICE_RESPONSE:
+			/* Simply forward the packet */
+			rc = yb_client_write_pkt(client, server, instance, msg,
+						 progress);
+			if (rc == -1) {
+				return YB_CLI_AUTH_FAILED;
+			}
+
+			/* Wait in the loop for the Auth packet */
+			continue;
+
+		case YB_KIWI_BE_FATAL_FOR_LOGICAL_CONNECTION:
+			yb_handle_fatalforlogicalconnection_pkt(client, server);
+
+			/* No need to wait for the Auth packet */
+			return YB_CLI_AUTH_FAILED;
+
+		case KIWI_BE_ERROR_RESPONSE:
+		default:
+			progress = YB_CLI_AUTH_FAILED;
+			rc = yb_client_write_pkt(client, server, instance, msg,
+						 progress);
+
+			od_debug(&instance->logger, CONTEXT_AUTH_PASSTHROUGH,
+				 client, server,
+				 "Received an unexpected error message");
+			server->offline = 1;
+			return progress;
+		}
+	}
+}
+
+/*
+ * Handle the forwarding of authentication packets in Ysql Connection Manager.
+ * Any packet related to the auth will be forwarded to the client in this function.
+ *
+ * Incase the client gets authenticated but there is a failure post authentication
+ * and before sending the READY_FOR_QUERY packet, client will receive a
+ * AuthOK message followed by a FATAL packet.
+ */
+static int yb_route_auth_packets(od_server_t *server, od_client_t *client)
+{
+	od_instance_t *instance = server->global->instance;
+	enum YB_CLI_AUTH_STATUS status;
+
+	yb_server_write_auth_passthroug_request_pkt(client, server, instance);
+
+	while (true) {
+		status = yb_forward_auth_pkt_server_to_client(client, server,
+							      instance);
+		switch (status) {
+		case YB_CLI_AUTH_FAILED:
+			return -1;
+		case YB_CLI_AUTH_SUCCESS:
+			return 0;
+		case YB_CLI_AUTH_PROGRESS:
+			break;
+		}
+
+		if (yb_forward_auth_pkt_client_to_server(client, server,
+							 instance) < 0)
+			return -1;
+	}
+}
+
+static bool yb_pkt_contains_client_id(char *data)
+{
+	if (data != NULL) {
+		return strncmp(data, YB_SHMEM_KEY_FORMAT,
+			       strlen(YB_SHMEM_KEY_FORMAT)) == 0;
+	}
+
+	return false;
+}
+
+static int yb_read_client_id_from_notice_pkt(od_client_t *client,
+					     od_server_t *server,
+					     od_instance_t *instance,
+					     machine_msg_t *msg)
+{
+	kiwi_fe_error_t hint;
+	int rc = -1;
+
+	/* Received a NOTICE packet, it can be the HINT containing the client id */
+	if (kiwi_fe_read_notice(machine_msg_data(msg), machine_msg_size(msg),
+				&hint) == -1) {
+		od_debug(&instance->logger, "read clientid", client, server,
+			 "failed to parse error message from server");
+		return -1;
+	}
+
+	if (yb_pkt_contains_client_id(hint.hint)) {
+		assert(client->client_id == 0);
+		client->client_id =
+			atoi(hint.hint + strlen(YB_SHMEM_KEY_FORMAT));
+		return 0;
+	}
+	return -1;
+}
+
+int yb_auth_frontend_passthrough(od_client_t *client, od_server_t *server)
+{
+	od_global_t *global = client->global;
+	kiwi_var_t *user = &client->startup.user;
+	od_instance_t *instance = global->instance;
+	od_router_t *router = global->router;
+	kiwi_be_type_t type;
+	machine_msg_t *msg;
+	int rc = -1;
+	int rc_auth = -1;
+
+	rc = yb_route_auth_packets(server, client);
+	rc_auth = rc;
+
+	/* Wait till the `READY_FOR_QUERY` packet is received. */
+	while (true) {
+		msg = od_read(&server->io, UINT32_MAX);
+		if (msg == NULL) {
+			if (!machine_timedout()) {
+				od_error(&instance->logger,
+					 CONTEXT_AUTH_PASSTHROUGH,
+					 server->client, server,
+					 "read error from server: %s",
+					 od_io_error(&server->io));
+				return -1;
+			}
+		}
+
+		type = *(char *)machine_msg_data(msg);
+		od_debug(&instance->logger, CONTEXT_AUTH_PASSTHROUGH,
+			 server->client, server,
+			 "Got %s packet from the pg_backend",
+			 kiwi_be_type_to_string(type));
+
+		switch (type) {
+		case KIWI_BE_READY_FOR_QUERY:
+			if (client->client_id == 0) {
+				od_frontend_fatal(
+					client, KIWI_PROTOCOL_VIOLATION,
+					"Unable to allocate the shared memory segment");
+				return -1;
+			}
+
+			return rc_auth;
+
+		case YB_KIWI_BE_FATAL_FOR_LOGICAL_CONNECTION:
+			yb_handle_fatalforlogicalconnection_pkt(client, server);
+			rc_auth = -1;
+			continue;
+
+		case KIWI_BE_NOTICE_RESPONSE:
+			if (yb_read_client_id_from_notice_pkt(
+				    client, server, instance, msg) < 0) {
+				/*
+				 * The notice packet does not contains any client id and
+				 * thus it is required to forward this notice packet to the client
+				 */
+				rc = od_write(&client->io, msg);
+				if (rc < 0)
+					rc_auth = -1;
+
+				continue;
+			}
+
+			break;
+
+		case KIWI_BE_ERROR_RESPONSE:
+			/* Physical connection is broken, no need to wait for readyForQuery pkt */
+			server->offline = 1;
+		default:
+			return -1;
+		}
+	}
+}
+
+/*
+ * Handle FatalForLogicalConnection packet.
+ * The immediate next WARNING packet should be treated as a FATAL packet,
+ * for the client.
+ * Forward this packet as a FATAL and close the client connection.
+ */
+void yb_handle_fatalforlogicalconnection_pkt(od_client_t *client,
+					     od_server_t *server)
+{
+	od_instance_t *instance = client->global->instance;
+	machine_msg_t *msg;
+
+	msg = od_read(&server->io, UINT32_MAX);
+	if (msg == NULL) {
+		if (!machine_timedout()) {
+			od_error(&instance->logger,
+				 "handling fatalforlogicalconnection pkt",
+				 server->client, server,
+				 "read error from server: %s",
+				 od_io_error(&server->io));
+
+			od_frontend_fatal(
+				client, KIWI_PROTOCOL_VIOLATION,
+				"Error while reading the FATAL message from the pg_backend.");
+			return;
+		}
+	}
+
+	yb_forward_fatal_msg(client, msg);
+}

java/yb-ysql-conn-mgr/src/test/java/org/yb/ysqlconnmgr/BaseYsqlConnMgr.java
@@ -30,11 +30,16 @@ public class BaseYsqlConnMgr extends BaseMiniClusterTest {
   protected static final int NUM_TSERVER = 3;
   private static final String DEFAULT_PG_USER = "yugabyte";
 
+  protected String getHbaConf()
+  {
+    return "host all all all trust, local all yugabyte trust";
+  }
+
   @Override
   protected void customizeMiniClusterBuilder(MiniYBClusterBuilder builder) {
     Map<String, String> additionalTserverFlags = new HashMap<String, String>() {
       {
-          put("ysql_hba_conf_csv", "host all all all trust, local all yugabyte trust");
+          put("ysql_hba_conf_csv", getHbaConf());
           put("ysql_conn_mgr_use_unix_conn", "true");
       }
     };

java/yb-ysql-conn-mgr/src/test/java/org/yb/ysqlconnmgr/TestCorrectErrorMessage.java
@@ -1,0 +1,111 @@
+// Copyright (c) YugabyteDB, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+// in compliance with the License. You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software distributed under the License
+// is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+// or implied. See the License for the specific language governing permissions and limitations
+// under the License.
+//
+
+package org.yb.ysqlconnmgr;
+
+import java.sql.Connection;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.yb.minicluster.MiniYBClusterBuilder;
+import org.yb.pgsql.ConnectionEndpoint;
+import static org.yb.AssertionWrappers.assertTrue;
+import static org.yb.AssertionWrappers.fail;
+
+@RunWith(value = YBTestRunnerYsqlConnMgr.class)
+public class TestCorrectErrorMessage extends BaseYsqlConnMgr {
+  private static final String CUSTOM_PG_HBA_CONFIG =
+      "host    yugabyte    yugabyte   all   md5, " +
+      "host    wrong_db    yugabyte    all    trust, " +
+      "host    all    yugabyte     all    md5, " +
+      "host    all    wrong_user   all   trust, " +
+      "host    all    all          all    trust, " +
+      "local   all    all   trust";
+
+  private static final String WRONG_USER_ERR_STRING = "role \"wrong_user\" does not exist";
+  private static final String WRONG_DB_ERR_STRING = "database \"wrong_db\" does not exist";
+
+  @Override
+  protected String getHbaConf() {
+    return CUSTOM_PG_HBA_CONFIG;
+  }
+
+  @Override
+  protected void customizeMiniClusterBuilder(MiniYBClusterBuilder builder) {
+    super.customizeMiniClusterBuilder(builder);
+
+    // At max there should be only one connection in the control connection pool.
+    // pool size of control connection = ysql_max_connections / 10
+    builder.addCommonTServerFlag("ysql_max_connections", "15");
+  }
+
+  public void checkBrokenControlConnection() throws Exception {
+    try (Connection connection =
+        getConnectionBuilder().withConnectionEndpoint(ConnectionEndpoint.YSQL_CONN_MGR)
+                              .withUser("yugabyte")
+                              .withPassword("yugabyte")
+                              .connect()) {
+    };
+  }
+
+  // GH #19781: If the authentication fails, the control connection gets broken and next time when
+  // any client attempts making connection
+  // "broken server connection" error message is received.
+  // This test ensures that client can retry making connection without getting "broken server
+  // connection" error message.
+  @Test
+  public void testNoBrokenConnAfterAuthFailed() throws Exception {
+    try {
+      getConnectionBuilder().withConnectionEndpoint(ConnectionEndpoint.YSQL_CONN_MGR)
+                            .withDatabase("yugabyte")
+                            .withUser("yugabyte")
+                            .withPassword("wrong password")
+                            .connect();
+      fail("Did not expected to login with a wrong password");
+    } catch (Exception e) {
+      LOG.info("Got an expected error message", e);
+    }
+
+    checkBrokenControlConnection();
+  }
+
+  @Test
+  public void testWrongUserErrMsg() throws Exception {
+    try (Connection connection =
+        getConnectionBuilder().withConnectionEndpoint(ConnectionEndpoint.YSQL_CONN_MGR)
+                              .withUser("wrong_user")
+                              .withPassword("yugabyte")
+                              .connect()) {
+      fail("Did not expected to login with a wrong username");
+    } catch (Exception e) {
+      assertTrue("Got wrong error message", e.getMessage().contains(WRONG_USER_ERR_STRING));
+    }
+
+    checkBrokenControlConnection();
+  }
+
+  @Test
+  public void testWrongDbErrMsg() throws Exception {
+    try (Connection connection =
+        getConnectionBuilder().withConnectionEndpoint(ConnectionEndpoint.YSQL_CONN_MGR)
+                              .withUser("yugabyte")
+                              .withDatabase("wrong_db")
+                              .connect()) {
+      fail("Did not expected to login with a wrong db");
+    } catch (Exception e) {
+      /* Match error message */
+      assertTrue("Wrong error message", e.getMessage().contains(WRONG_DB_ERR_STRING));
+    }
+
+    checkBrokenControlConnection();
+  }
+}

src/odyssey/sources/CMakeLists.txt
@@ -53,7 +53,8 @@ set(od_src
     hashmap.c
     hba.c
     hba_reader.c
-    hba_rule.c)
+    hba_rule.c
+    yb_auth_passthrough.c)
 
 if (PAM_FOUND)
         list(APPEND od_src pam.c)

commit e068052d57eb26f6b62ef807cf101688185f0139

[#29477] YSQL: Conn mgr - Fix segfault in AP mid-auth error handling
Summary:
This diff fixes a potential segfault in an error handling code path of Auth Passthrough (AP) mode of connection manager.

The diff changes an arg passed to the function `yb_client_exit_mid_passthrough()` when a client exits during the authentication procedure in .
Earlier, `client->server` was passed, which is uninitialised in this code path in AP. Instead, we pass the `server` var provided to the enclosing function.

This is an AP-specific bug and is hit only if AP is enabled in a very specific scenario - a client closes the connection after sending the Auth Passthrough request packet but before authentication is complete.

The bug was introduced in commit https://github.com/yugabyte/yugabyte-db/commit/edbc6876e29e1fd4a7d1f9e9291c563eaed88df5

Test Plan: Jenkins: all tests

Reviewers: skumar, mkumar, arpit.saxena, asrinivasan

Reviewed By: arpit.saxena

Subscribers: yql

Differential Revision: https://phorge.dev.yugabyte.com/D49602

src/odyssey/sources/yb_auth_passthrough.c
@@ -313,8 +313,7 @@ static int yb_client_write_pkt(od_client_t *client, od_server_t *server,
 			 * pg_backend will be expecting a packet,
 			 * send an empty password packet.
 			 */
-			yb_client_exit_mid_passthrough(client->server,
-						       instance);
+			yb_client_exit_mid_passthrough(server, instance);
 		}
 
 		/* Since the client socket is closed, we need not send any fatal packet */