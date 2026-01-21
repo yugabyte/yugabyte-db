e76de73f11607a55b34b2989c92465268c095458
Yury Shchetinin <yshchetinin@yugabyte.com>
2026-01-21 19:00:00 +0300

    [PLAT-19503] Add prechecks-only flow to node operations
    Summary: Added ability to run only prechecks for node operations (like we do for upgrades)
    
    Test Plan: manually run API call with runOnlyPrechecks=true
    
    Reviewers: rmadhavan
    
    Reviewed By: rmadhavan
    
    Subscribers: yugaware
    
    Differential Revision: https://phorge.dev.yugabyte.com/D49792

--- managed/src/main/java/com/yugabyte/yw/forms/NodeActionFormData.java
+++ managed/src/main/java/com/yugabyte/yw/forms/NodeActionFormData.java
@@ -1,6 +1,7 @@
 package com.yugabyte.yw.forms;
 
 import com.yugabyte.yw.common.NodeActionType;
+import com.yugabyte.yw.models.common.YbaApi;
 import io.swagger.annotations.ApiModelProperty;
 import javax.validation.constraints.NotNull;
 import lombok.Data;
@@ -20,4 +21,8 @@ public class NodeActionFormData {
 
   @ApiModelProperty(value = "Should ignore master unavailability and proceed with the node action")
   private boolean force = false;
+
+  @ApiModelProperty(value = "YbaApi Internal. Run only prechecks during task run")
+  @YbaApi(visibility = YbaApi.YbaApiVisibility.INTERNAL, sinceYBAVersion = "2.29.0.0")
+  private boolean runOnlyPrechecks = false;
 }
