b48072b2672665e37b4cc28dbdbb6f65e6522664
Anusha S18l <3714727+anushas@users.noreply.github.com>
2026-02-05 17:24:47 +0000

    [BACKPORT 2025.2][#30088] DocDB: fix the lock order inversion in AbortTableCreation
    Summary:
    Problem:
    AbortTableCreation() is called from CreateTable() on certain failures.
    At the time it is called, the COW lock on the newly created table
    mutable metadata (for index, also the COW lock on the indexed table)
    is already held - acquired in CreateTableInfo(). But the catalog manager
    mutex is released by the time AbortTableCreation() is called.
    AbortTableCreation() re-acquires the catalog manager mutex before
    doing the cleanup. This results in lock order inversion.
    
    Solution:
    This commit fixes this by releasing the COW lock in AbortTableCreation()
    first before re-acquiring the catalog manager mutex again.
    It uses a test flag to fake the abort for testing.
    
    Original commit: bd21091bd73e84763042515a0dd19e0379001520 / D50020
    
    Test Plan:
    A new testcase that exercises the abort codepath.
    - ./yb_build.sh release --cxx-test master_create_abort-test (ran in loop).
    - ./yb_build.sh tsan --cxx-test master_create_abort-test
    
    Reviewers: zdrudi, hsunder, jhe
    
    Reviewed By: jhe
    
    Subscribers: ybase, yql
    
    Differential Revision: https://phorge.dev.yugabyte.com/D50210

--- src/yb/master/catalog_manager.cc
+++ src/yb/master/catalog_manager.cc
@@ -248,6 +248,8 @@ DEFINE_test_flag(bool, get_ysql_catalog_version_from_sys_catalog, false,
                  "Whether catalog manager should get the ysql catalog version "
                  "from the sys_catalog.");
 
+DEFINE_test_flag(uint32, abort_create_table, 0,
+    "Abort the creation of a table at a specified point in code.");
 
 // TODO: should this be a test flag?
 DEFINE_RUNTIME_int32(catalog_manager_inject_latency_in_delete_table_ms, 0,
@@ -2293,7 +2295,8 @@ void CatalogManager::CompleteShutdown() {
 Status CatalogManager::AbortTableCreation(TableInfo* table,
                                           const TabletInfos& tablets,
                                           const Status& s,
-                                          CreateTableResponsePB* resp) {
+                                          CreateTableResponsePB* resp,
+                                          TableInfoWithWriteLock* indexed_table) {
   LOG(WARNING) << s;
 
   const TableId table_id = table->id();
@@ -2312,20 +2315,34 @@ Status CatalogManager::AbortTableCreation(TableInfo* table,
   // all tasks, as (by definition) no tasks may be pending against a
   // table that has failed to successfully create.
   table->CloseAndWaitForAllTasksToAbort();
+
+  // For an index, must release the indexed_table COW lock
+  // that was acquired earlier. The indexed_table lock may be
+  // already released depending on the error causing the abort.
+  // Unlock() api checks for that internally.
+  // Note that the two locks are acquired based on the table_id
+  // order to avoid deadlocks. But can be released in any order
+  // w/o affecting correctness.
+  if (indexed_table && *indexed_table) {
+    indexed_table->lock.Unlock();
+  }
+
+  // Call AbortMutation() manually to release the COW lock.
+  // Callers are assumed to have done some changes and
+  // called StartMutation() earlier.
+  for (const auto& tablet : tablets) {
+    tablet->mutable_metadata()->AbortMutation();
+  }
+  table->mutable_metadata()->AbortMutation();
+
+  // Remove the metadata from the catalog manager
   {
     LockGuard lock(mutex_);
-
-    // Call AbortMutation() manually, as otherwise the lock won't be released.
-    for (const auto& tablet : tablets) {
-      tablet->mutable_metadata()->AbortMutation();
-    }
-    table->mutable_metadata()->AbortMutation();
     auto tablet_map_checkout = tablet_map_.CheckOut();
     for (const TabletId& tablet_id_to_erase : tablet_ids_to_erase) {
       CHECK_EQ(tablet_map_checkout->erase(tablet_id_to_erase), 1)
           << "Unable to erase tablet " << tablet_id_to_erase << " from tablet map.";
     }
-
     auto table_map_checkout = tables_.CheckOut();
     table_names_map_.erase({table_namespace_id, table_name});  // Not present if PGSQL table.
     CHECK_EQ(table_map_checkout->Erase(table_id), 1)
@@ -2337,6 +2354,23 @@ Status CatalogManager::AbortTableCreation(TableInfo* table,
   }
   return CheckIfNoLongerLeaderAndSetupError(s, resp);
 }
+
+namespace {
+
+// Helper for TEST_abort_create_table: if flag matches and name has test prefix, return
+// non-OK status. Caller should call AbortTableCreation if result is not OK.
+Status TEST_MaybeFakeAbortTableCreation(
+    uint32_t test_abort_case, const std::string& table_name) {
+  if (FLAGS_TEST_abort_create_table == test_abort_case &&
+      table_name.starts_with("test_create_abort_")) {
+    return STATUS(InternalError,
+        "TEST: Aborting due to FLAGS_TEST_abort_create_table (case" +
+            std::to_string(test_abort_case) + ")");
+  }
+  return Status::OK();
+}
+
+}  // namespace
 
 Result<ReplicationInfoPB> CatalogManager::GetTableReplicationInfo(
   const ReplicationInfoPB& table_replication_info,
@@ -3780,7 +3814,8 @@ Status CatalogManager::CompleteCreateYsqlSysTable(
   if (PREDICT_FALSE(!s.ok())) {
     return AbortTableCreation(
         data.table.get(), {},
-        s.CloneAndPrepend("An error occurred while inserting to sys-tablets: "), &data.resp);
+        s.CloneAndPrepend("An error occurred while inserting to sys-tablets: "),
+          &data.resp, nullptr /* indexed_table */);
   }
   TRACE("Wrote table to system table");
 
@@ -3846,7 +3881,8 @@ Status CatalogManager::CreateYsqlSysTable(
     if (PREDICT_FALSE(!s.ok())) {
       return AbortTableCreation(
           data.table.get(), {},
-          s.CloneAndPrepend("An error occurred while inserting to sys-tablets: "), &data.resp);
+          s.CloneAndPrepend("An error occurred while inserting to sys-tablets: "),
+            &data.resp, nullptr /* indexed_table */);
     }
     // TODO(zdrudi): to handle the new format for the sys tablet, set the child table's parent table
     // id and add to the tablet's in-memory list of hosted table ids.
@@ -4720,11 +4756,19 @@ Status CatalogManager::CreateTable(const CreateTableRequestPB* orig_req,
     }
   }
 
+  // if test flag to abort create table is set and this is to create
+  // test table, fake abort the table creation.
+  // case 1: fakes the case where the Upsert failed.
+  s = TEST_MaybeFakeAbortTableCreation(1, req.name());
+  if (PREDICT_FALSE(!s.ok())) {
+    return AbortTableCreation(table.get(), tablets, s, resp, &indexed_table);
+  }
+
   s = sys_catalog_->Upsert(epoch, table, tablets);
   if (PREDICT_FALSE(!s.ok())) {
     return AbortTableCreation(
         table.get(), tablets, s.CloneAndPrepend("An error occurred while inserting to sys-tablets"),
-        resp);
+        resp, &indexed_table);
   }
   VLOG(3) << "SysTablesEntryPB after CreateTable: " << table->metadata().dirty().pb.DebugString();
   TRACE("Wrote table and tablets to system table");
@@ -4742,11 +4786,31 @@ Status CatalogManager::CreateTable(const CreateTableRequestPB* orig_req,
         index_info.set_index_permissions(INDEX_PERM_DELETE_ONLY);
       }
     }
+
+    // if test flag to abort create table is set and this is to create
+    // test table, fake abort the table creation.
+    // case 2: fakes the case where Upsert was successful but
+    // AddIndexInfoToTable failed while still holding the COW lock
+    // on the indexed table.
+    s = TEST_MaybeFakeAbortTableCreation(2, req.name());
+    if (PREDICT_FALSE(!s.ok())) {
+      return AbortTableCreation(table.get(), tablets, s, resp, &indexed_table);
+    }
+
     s = AddIndexInfoToTable(indexed_table, index_info, epoch, resp);
     if (PREDICT_FALSE(!s.ok())) {
       return AbortTableCreation(
           table.get(), tablets, s.CloneAndPrepend("An error occurred while inserting index info"),
-          resp);
+          resp, &indexed_table);
+    }
+    // if test flag to abort create table is set and this is to create
+    // test table, fake abort the table creation.
+    // case 3: fakes the case where AddIndexInfoToTable failed after
+    // committing indexed table's in-memory state & releasing
+    // the COW lock on the indexed table.
+    s = TEST_MaybeFakeAbortTableCreation(3, req.name());
+    if (PREDICT_FALSE(!s.ok())) {
+      return AbortTableCreation(table.get(), tablets, s, resp, &indexed_table);
     }
   }
 
