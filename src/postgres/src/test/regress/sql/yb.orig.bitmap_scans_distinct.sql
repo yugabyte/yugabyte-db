5954b4260bfb4b715bb25785c2edeb59a05b11fa
Jason Kim <git@jasonk.me>
2026-02-06 18:31:28 -0800

    [#30236] YSQL: use explainrun for bitmap_scans regress tests
    Summary:
    Following up on commit 24d84a82997f3abbec1803973680c72045b8bc99, use the
    yb_commands/explainrun.sql helper script to deduplicate queries for
    regress tests under the yb_bitmap_scans_schedule schedule, which is run
    by the TestPgRegressYbBitmapScans Java test.
    
    Test Plan:
    On Almalinux 8:
    
        ./yb_build.sh fastdebug --gcc13 daemons initdb \
          --no-ybc --skip-pg-parquet --no-odyssey \
          --java-test TestPgRegressYbBitmapScans
    
    Close: #30236
    
    Reviewers: patnaik.balivada
    
    Reviewed By: patnaik.balivada
    
    Subscribers: yql
    
    Differential Revision: https://phorge.dev.yugabyte.com/D50265

--- src/postgres/src/test/regress/sql/yb.orig.bitmap_scans_distinct.sql
+++ src/postgres/src/test/regress/sql/yb.orig.bitmap_scans_distinct.sql
@@ -1,6 +1,11 @@
 --
 -- test distinct bitmap scans (distinct pushdown is not supported by bitmap scans)
 --
+\getenv abs_srcdir PG_ABS_SRCDIR
+\set filename :abs_srcdir '/yb_commands/explainrun.sql'
+\i :filename
+\set explain 'EXPLAIN (ANALYZE, COSTS OFF, SUMMARY OFF)'
+
 SET yb_enable_bitmapscan = true;
 
 CREATE TABLE test_distinct (r1 INT, r2 INT, r3 INT, v INT, PRIMARY KEY(r1 ASC, r2 ASC, r3 ASC)) SPLIT AT VALUES ((1, 1, 500));
@@ -11,51 +16,39 @@ INSERT INTO test_distinct (SELECT 2, i%3, i, i/3 FROM GENERATE_SERIES(1, 1000) A
 SET yb_enable_distinct_pushdown = true;
 SET enable_bitmapscan = false;
 
-EXPLAIN (ANALYZE, COSTS OFF, SUMMARY OFF)
-SELECT DISTINCT r1 FROM test_distinct WHERE r1 < 2 ORDER BY r1;
-SELECT DISTINCT r1 FROM test_distinct WHERE r1 < 2 ORDER BY r1;
-EXPLAIN (ANALYZE, COSTS OFF, SUMMARY OFF)
-SELECT DISTINCT r1, r2 FROM test_distinct WHERE r1 < 2 OR r2 < 3 ORDER BY r1, r2;
-SELECT DISTINCT r1, r2 FROM test_distinct WHERE r1 < 2 OR r2 < 3 ORDER BY r1, r2;
-EXPLAIN (ANALYZE, COSTS OFF, SUMMARY OFF)
-SELECT DISTINCT r1, r2 FROM test_distinct WHERE r1 < 2 OR r2 < 3 OR r3 < 2 ORDER BY r1, r2;
-SELECT DISTINCT r1, r2 FROM test_distinct WHERE r1 < 2 OR r2 < 3 OR r3 < 2 ORDER BY r1, r2;
+\set query 'SELECT DISTINCT r1 FROM test_distinct WHERE r1 < 2 ORDER BY r1'
+:explain1run1
+\set query 'SELECT DISTINCT r1, r2 FROM test_distinct WHERE r1 < 2 OR r2 < 3 ORDER BY r1, r2'
+:explain1run1
+\set query 'SELECT DISTINCT r1, r2 FROM test_distinct WHERE r1 < 2 OR r2 < 3 OR r3 < 2 ORDER BY r1, r2'
+:explain1run1
 
 RESET enable_bitmapscan;
 
-/*+ BitmapScan(test_distinct) */ EXPLAIN (ANALYZE, COSTS OFF, SUMMARY OFF)
-SELECT DISTINCT r1 FROM test_distinct WHERE r1 < 2 ORDER BY r1;
-/*+ BitmapScan(test_distinct) */
-SELECT DISTINCT r1 FROM test_distinct WHERE r1 < 2 ORDER BY r1;
-/*+ BitmapScan(test_distinct) */ EXPLAIN (ANALYZE, COSTS OFF, SUMMARY OFF)
-SELECT DISTINCT r1, r2 FROM test_distinct WHERE r1 < 2 OR r2 < 3 ORDER BY r1, r2;
-/*+ BitmapScan(test_distinct) */
-SELECT DISTINCT r1, r2 FROM test_distinct WHERE r1 < 2 OR r2 < 3 ORDER BY r1, r2;
-/*+ BitmapScan(test_distinct) */ EXPLAIN (ANALYZE, COSTS OFF, SUMMARY OFF)
-SELECT DISTINCT r1, r2 FROM test_distinct WHERE r1 < 2 OR r2 < 3 OR r3 < 2 ORDER BY r1, r2;
-/*+ BitmapScan(test_distinct) */
-SELECT DISTINCT r1, r2 FROM test_distinct WHERE r1 < 2 OR r2 < 3 OR r3 < 2 ORDER BY r1, r2;
+\set hint1 '/*+ BitmapScan(test_distinct) */'
+\set query 'SELECT DISTINCT r1 FROM test_distinct WHERE r1 < 2 ORDER BY r1'
+:explain1run1
+\set query 'SELECT DISTINCT r1, r2 FROM test_distinct WHERE r1 < 2 OR r2 < 3 ORDER BY r1, r2'
+:explain1run1
+\set query 'SELECT DISTINCT r1, r2 FROM test_distinct WHERE r1 < 2 OR r2 < 3 OR r3 < 2 ORDER BY r1, r2'
+:explain1run1
 
 SET yb_enable_distinct_pushdown TO false;
 SET enable_bitmapscan = false;
 
-EXPLAIN (ANALYZE, COSTS OFF, SUMMARY OFF)
-SELECT DISTINCT r1 FROM test_distinct WHERE r1 < 2 ORDER BY r1;
-SELECT DISTINCT r1 FROM test_distinct WHERE r1 < 2 ORDER BY r1;
-EXPLAIN (ANALYZE, COSTS OFF, SUMMARY OFF)
-SELECT DISTINCT r1, r2 FROM test_distinct WHERE r1 < 2 OR r2 < 3 ORDER BY r1, r2;
-SELECT DISTINCT r1, r2 FROM test_distinct WHERE r1 < 2 OR r2 < 3 ORDER BY r1, r2;
+\set hint1
+\set query 'SELECT DISTINCT r1 FROM test_distinct WHERE r1 < 2 ORDER BY r1'
+:explain1run1
+\set query 'SELECT DISTINCT r1, r2 FROM test_distinct WHERE r1 < 2 OR r2 < 3 ORDER BY r1, r2'
+:explain1run1
 
 RESET enable_bitmapscan;
 
-/*+ BitmapScan(test_distinct) */ EXPLAIN (ANALYZE, COSTS OFF, SUMMARY OFF)
-SELECT DISTINCT r1 FROM test_distinct WHERE r1 < 2 ORDER BY r1;
-/*+ BitmapScan(test_distinct) */
-SELECT DISTINCT r1 FROM test_distinct WHERE r1 < 2 ORDER BY r1;
-/*+ BitmapScan(test_distinct) */ EXPLAIN (ANALYZE, COSTS OFF, SUMMARY OFF)
-SELECT DISTINCT r1, r2 FROM test_distinct WHERE r1 < 2 OR r2 < 3 ORDER BY r1, r2;
-/*+ BitmapScan(test_distinct) */
-SELECT DISTINCT r1, r2 FROM test_distinct WHERE r1 < 2 OR r2 < 3 ORDER BY r1, r2;
+\set hint1 '/*+ BitmapScan(test_distinct) */'
+\set query 'SELECT DISTINCT r1 FROM test_distinct WHERE r1 < 2 ORDER BY r1'
+:explain1run1
+\set query 'SELECT DISTINCT r1, r2 FROM test_distinct WHERE r1 < 2 OR r2 < 3 ORDER BY r1, r2'
+:explain1run1
 
 RESET yb_enable_distinct_pushdown;
 DROP TABLE test_distinct;
