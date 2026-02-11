yugabyte/yugabyte-db

commit 44a0bfb01cc0678246350160782898b80cc3ded5

[#29112] YSQL: Fix GUC correctness and error handling flaws in Auth Passthrough
Summary:
This revision implements authentication passthrough mechanism in connection manager ensuring correctness while keeping latency low.

The primary goal of this revision is to fix certain correctness issues in the current implementation of Auth Passthrough, specifially GUC handling (applying correct defaults, reporting them to connection manager), and properly handling errors that might occur during authentication (ensuring db checks are done, not crashing the backend on common auth failures, correctly resetting the pg backend after auth in case of both success and failure).

A summary from the design document is appended below. The final behaviour of Auth Passthrough mode after this revision is described here.

 - AB: Authentication Backend (current default mode that spawns a fresh control backend ("authentication backend") each time an authentication request arrives)
 - AP: Authentication Passthrough (the mode where we reuse control backends for authentication requests, avoiding backend-spawn time and resource costs)
 - Control Backend: A postgres backend process spawned for the purpose of authentication

**Proposal**
To fix the latency issues with AB and avoid staleness/incorrect state issues with original AP, we employ the original AP approach of reusing control backends across authentications, but discard the idea of caching parameters from a separately spawned “transient backend”.

**Goals**
The targets for this design proposal are
 - To maintain GUC (and other, like cat cache) state correctness for new connections
 - To reduce authentication latency (down single digit milliseconds)

**Idea**

src/postgres/src/backend/libpq/auth.c
@@ -61,17 +61,6 @@ static void auth_failed(Port *port, int status, const char *logdetail,
 static char *recv_password_packet(Port *port);
 static void set_authn_id(Port *port, const char *id);
 
-/*
- * GH #19781: FATAL or ERROR packet leads to a broken physical connection. Therefore,
- * in auth passthrough, instead of FATAL/ERROR packet, WARNING packet along with
- * FatalForLogicalConnection packet is used.
- */
-static int
-YbAuthFailedErrorLevel(const bool auth_passthrough)
-{
-	return (YbIsClientYsqlConnMgr() && auth_passthrough == true) ? WARNING : FATAL;
-}
-
 /*----------------------------------------------------------------
  * Password-based authentication methods (password, md5, and scram-sha-256)
  *----------------------------------------------------------------
@@ -284,6 +273,18 @@ auth_failed(Port *port, int status, const char *logdetail, bool yb_role_is_locke
 	char	   *cdetail;
 	int			errcode_return = ERRCODE_INVALID_AUTHORIZATION_SPECIFICATION;
 
+	bool		yb_is_auth_passthrough = YbIsAuthPassthroughInProgress(port);
+
+	/*
+	 * YB: When using Auth Passthrough mode of connection manager, mark the
+	 * current auth attempt as failed so that the control backend knows to abort
+	 * auth and reset to its base state. The fact that we are in a call to
+	 * `auth_failed()` is sufficient to conclude that auth has failed.
+	 */
+
+	if (yb_is_auth_passthrough)
+		port->yb_has_auth_passthrough_failed = true;
+
 	/*
 	 * If we failed due to EOF from client, just quit; there's no point in
 	 * trying to send a message to the client, and not much point in logging
@@ -293,9 +294,22 @@ auth_failed(Port *port, int status, const char *logdetail, bool yb_role_is_locke
 	 * send.  We'll get a useless log entry for every psql connection under
 	 * password auth, even if it's perfectly successful, if we log STATUS_EOF
 	 * events.)
+	 *
+	 * YB: When conn mgr is enabled and in auth passthrough mode, avoid calling
+	 * proc_exit here, instead returning a failure result via
+	 * port->yb_has_auth_passthrough_failed.
 	 */
 	if (status == STATUS_EOF)
-		proc_exit(0);
+	{
+		if (yb_is_auth_passthrough)
+		{
+			return;
+		}
+		else
+		{
+			proc_exit(0);
+		}
+	}
 
 	switch (port->hba->auth_method)
 	{
@@ -366,10 +380,10 @@ auth_failed(Port *port, int status, const char *logdetail, bool yb_role_is_locke
 	else
 		logdetail = cdetail;
 
-	if (port->yb_is_auth_passthrough_req)
+	if (yb_is_auth_passthrough)
 		YbSendFatalForLogicalConnectionPacket();
 
-	ereport(YbAuthFailedErrorLevel(port->yb_is_auth_passthrough_req),
+	ereport(YbAuthFailedErrorLevel(yb_is_auth_passthrough),
 			(errcode(errcode_return),
 			 (yb_role_is_locked_out ?
 			  errmsg("role \"%s\" is locked. Contact your database administrator.",
@@ -378,6 +392,11 @@ auth_failed(Port *port, int status, const char *logdetail, bool yb_role_is_locke
 			 (logdetail ? errdetail_log("%s", logdetail) : 0)));
 
 	/* doesn't return */
+	/*
+	 * YB: This function does in fact return when the Auth Passthrough mode of
+	 * Connection Manager is enabled. The relevant codepath changes for this
+	 * change in behaviour are all under the `yb_ai_auth_passthrough` flag.
+	 */
 }
 
 
@@ -429,6 +448,13 @@ set_authn_id(Port *port, const char *id)
 /*
  * Client authentication starts here.  If there is an error, this
  * function does not return and the backend process is terminated.
+ *
+ * YB: This function *does* return in case connection manager is active and this
+ * is a control backend being used for authentication with auth passthrough mode
+ * enabled.
+ * If auth fails, `port->yb_has_auth_passthrough_failed` is used to signal
+ * authentication failure. This is set in the call to `auth_failed()`. Else it
+ * must be set manually where auth_failed is not called before returning.
  */
 void
 ClientAuthentication(Port *port)
@@ -436,7 +462,7 @@ ClientAuthentication(Port *port)
 	int			status = STATUS_ERROR;
 	const char *logdetail = NULL;
 
-	bool		yb_auth_passthrough = port->yb_is_auth_passthrough_req;
+	bool		yb_auth_passthrough = YbIsAuthPassthroughInProgress(port);
 
 	if (yb_auth_passthrough)
 	{
@@ -446,6 +472,8 @@ ClientAuthentication(Port *port)
 		/*
 		 * Connections from Ysql Connection Manager will never be of type
 		 * replication.
+		 * TODO (vikram.damle) (#29113): Need to handle logical replication
+		 * connection request that might now be coming via conn mgr.
 		 */
 		Assert(am_walsender == false);
 	}
@@ -487,9 +515,10 @@ ClientAuthentication(Port *port)
 			 * authentication type of a client, if authentication type is cert,
 			 * a FATAL packet is sent back to the Ysql Connection Manager.
 			 */
-			ereport(FATAL,
-					(errcode(ERRCODE_INVALID_AUTHORIZATION_SPECIFICATION),
-					 errmsg("cert authentication is not supported with connection manager")));
+			auth_failed(port, status,
+						"cert authentication is not supported with connection "
+						"manager",
+						false);
 			return;
 		}
 
@@ -547,6 +576,11 @@ ClientAuthentication(Port *port)
 #endif
 					_("no encryption");
 
+				/*
+				 * YB TODO (vikram.damle) (#29113):
+				 * When adding support for replication auth in auth passthrough,
+				 * need to handle this fatal err. Here and in uaImplicitReject.
+				 */
 				if (am_walsender && !am_db_walsender)
 					ereport(FATAL,
 							(errcode(ERRCODE_INVALID_AUTHORIZATION_SPECIFICATION),
@@ -557,7 +591,10 @@ ClientAuthentication(Port *port)
 				else
 				{
 					if (yb_auth_passthrough)
+					{
 						YbSendFatalForLogicalConnectionPacket();
+						port->yb_has_auth_passthrough_failed = true;
+					}
 
 					ereport(YbAuthFailedErrorLevel(yb_auth_passthrough),
 							(errcode(ERRCODE_INVALID_AUTHORIZATION_SPECIFICATION),
@@ -631,7 +668,10 @@ ClientAuthentication(Port *port)
 				else
 				{
 					if (yb_auth_passthrough)
+					{
 						YbSendFatalForLogicalConnectionPacket();
+						port->yb_has_auth_passthrough_failed = true;
+					}
 
 					ereport(YbAuthFailedErrorLevel(yb_auth_passthrough),
 							(errcode(ERRCODE_INVALID_AUTHORIZATION_SPECIFICATION),
@@ -705,12 +745,13 @@ ClientAuthentication(Port *port)
 #ifdef HAVE_UNIX_SOCKETS
 			Assert(IsYugaByteEnabled());
 
-			if (YbIsClientYsqlConnMgr() && port->yb_is_auth_passthrough_req)
+			if (YbIsAuthPassthroughInProgress(port))
 			{
-				YbSendFatalForLogicalConnectionPacket();
-				elog(WARNING,
-					 "YbTserverKey authentication is not supported "
-					 " in auth passthrough");
+				auth_failed(port, status,
+							"YbTserverKey authentication is not supported "
+							"in auth passthrough",
+							false);
+				return;
 			}
 
 			status = CheckYbTserverKeyAuth(port, &logdetail);
@@ -806,9 +847,6 @@ ClientAuthentication(Port *port)
 			if (roleid != InvalidOid)
 				YbResetFailedAttemptsIfAllowed(roleid);
 			sendAuthRequest(port, AUTH_REQ_OK, NULL, 0);
-
-			if (YbIsClientYsqlConnMgr() && yb_auth_passthrough)
-				YbCreateClientId();
 		}
 		else
 		{
@@ -824,9 +862,6 @@ ClientAuthentication(Port *port)
 	if (status == STATUS_OK)
 	{
 		sendAuthRequest(port, AUTH_REQ_OK, NULL, 0);
-
-		if (YbIsClientYsqlConnMgr() && yb_auth_passthrough)
-			YbCreateClientId();
 	}
 	else
 		auth_failed(port, status, logdetail, false /* yb_role_is_locked_out */ );
@@ -921,11 +956,31 @@ recv_password_packet(Port *port)
 	 * this check here, to prevent an empty password from being used with
 	 * authentication methods that check the password against an external
 	 * system, like PAM, LDAP and RADIUS.
+	 *
+	 * YB: In the case of auth passthrough mode of connection manager, we want
+	 * to avoid terminating the backend process if possible. In this case, we
+	 * forward a FATAL packet through conn mgr to the client and abort auth, but
+	 * do not push an ERROR level log and kill the backend (using WARNING
+	 * instead).
 	 */
 	if (buf.len == 1)
-		ereport(ERROR,
-				(errcode(ERRCODE_INVALID_PASSWORD),
-				 errmsg("empty password returned by client")));
+	{
+		if (YbIsAuthPassthroughInProgress(port))
+		{
+			YbSendFatalForLogicalConnectionPacket();
+			ereport(YbAuthFailedErrorLevel(true /* auth_passthrough */ ),
+					(errcode(ERRCODE_INVALID_PASSWORD),
+					 errmsg("empty password returned by client")));
+
+			return NULL;		/* YB: Added return NULL as per above comment */
+		}
+		else
+		{
+			ereport(ERROR,
+					(errcode(ERRCODE_INVALID_PASSWORD),
+					 errmsg("empty password returned by client")));
+		}
+	}
 
 	/* Do not echo password to logs, for security. */
 	elog(DEBUG5, "received password packet");

src/postgres/src/backend/postmaster/postmaster.c
@@ -2092,6 +2092,17 @@ initMasks(fd_set *rmask)
 	return maxsock + 1;
 }
 
+/*
+ * YB: This function is a wrapper around ProcessStartupPacket, lifting it from
+ * `static` to `extern` as it is required in postgres.c for the Authentication
+ * Passthrough mode of Connection Manager, while making minimal changes to
+ * upstream-owned code.
+ */
+int
+YbProcessStartupPacket(Port *port, bool ssl_done, bool gss_done)
+{
+	return ProcessStartupPacket(port, ssl_done, gss_done);
+}
 
 /*
  * Read a client's startup packet and do something according to it.
@@ -2130,6 +2141,7 @@ ProcessStartupPacket(Port *port, bool ssl_done, bool gss_done)
 	char		yb_logical_conn_type = 'U'; /* Unencrypted */
 	bool		yb_logical_conn_type_provided = false;
 	bool		yb_auto_analyze_backend = false;
+	bool		yb_is_auth_via_conn_mgr = false;
 
 	pq_startmsgread();
 
@@ -2494,15 +2506,19 @@ retry1:
 			SendNegotiateProtocolVersion(unrecognized_protocol_options);
 	}
 
+	yb_is_auth_via_conn_mgr = yb_is_auth_backend ||
+		port->yb_is_auth_passthrough_req;
+
 	if (YBIsEnabledInPostgresEnvVar())
 	{
 		if (yb_auth_backend_remote_host != NULL)
 		{
-			if (!yb_is_auth_backend)
+			if (!yb_is_auth_via_conn_mgr)
 				ereport(FATAL,
 						(errcode(ERRCODE_PROTOCOL_VIOLATION),
-						 errmsg("yb_auth_remote_host must only be provided when"
-								" yb_authonly is true")));
+						 errmsg("yb_auth_remote_host must only be provided "
+								"when yb_authonly is true or in an auth passthrough "
+								"'A' request packet")));
 
 			/*
 			 * HARD Code connection type between client and ysql_conn_mgr to
@@ -2521,7 +2537,7 @@ retry1:
 
 		if (yb_logical_conn_type_provided)
 		{
-			if (!yb_is_auth_backend)
+			if (!yb_is_auth_via_conn_mgr)
 				ereport(FATAL,
 						(errcode(ERRCODE_PROTOCOL_VIOLATION),
 						 errmsg("yb_logical_conn_type must only be provided "
@@ -4828,6 +4844,14 @@ BackendInitialize(Port *port)
 	/* set these to empty in case they are needed before we set them up */
 	port->remote_host = "";
 	port->remote_port = "";
+
+	/*
+	 * YB: Initialize custom vars to avoid issue in control/auth backend startup
+	 */
+	port->yb_is_auth_passthrough_req = false;
+	port->yb_has_auth_passthrough_failed = false;
+	port->yb_is_tserver_auth_method = false;
+	port->yb_is_ssl_enabled_in_logical_conn = false;
 
 	/*
 	 * Initialize libpq and enable reporting of ereport errors to the client.

src/postgres/src/backend/tcop/postgres.c
@@ -253,7 +253,7 @@ static void enable_statement_timeout(void);
 static void disable_statement_timeout(void);
 
 static void yb_start_xact_command_internal(bool yb_skip_read_committed_internal_savepoint);
-
+static void yb_abort_xact_command(void);
 
 /* ----------------------------------------------------------------
  *		routines to obtain user input
@@ -3040,6 +3040,35 @@ finish_xact_command(void)
 	}
 }
 
+/*
+ * YB: Use this function to cleanly abort a txn, similar to finish_xact_command.
+ * Used in the Authentication Passthrough mode of Connection Manager to reset
+ * control backend state.
+ */
+static void
+yb_abort_xact_command(void)
+{
+	/* cancel active statement timeout after each command */
+	disable_statement_timeout();
+
+	if (xact_started)
+	{
+		AbortCurrentTransaction();
+
+#ifdef MEMORY_CONTEXT_CHECKING
+		/* Check all memory contexts that weren't freed during commit */
+		/* (those that were, were checked before being deleted) */
+		MemoryContextCheck(TopMemoryContext);
+#endif
+
+#ifdef SHOW_MEMORY_STATS
+		/* Print mem stats after each commit for leak tracking */
+		MemoryContextStats(TopMemoryContext);
+#endif
+
+		xact_started = false;
+	}
+}
 
 /*
  * Convenience routines for checking whether a statement is one of the
@@ -7154,7 +7183,19 @@ PostgresMain(const char *dbname, const char *username)
 				 */
 				break;
 
-			case 'A':			/* YB: Auth Passthrough Request */
+			case 'A':
+				/*
+				 * YB: Auth Passthrough Request.
+				 * We should have a `startup packet` sent from Connection
+				 * manager queued up to be read in the socket buffer as well.
+				 * This is to mimic normal startup in order to allow reusing as
+				 * much of the original authentication code path as possible.
+				 * Authentication wire protocol after this should look exactly
+				 * like normal authentication, barring the values/existence of
+				 * certain fields in the startup (and subsequent) packets. The
+				 * packet types themselves should match the regular pg startup
+				 * wire protocol.
+				 */
 				if (YbIsClientYsqlConnMgr())
 				{
 					/*
@@ -7173,14 +7214,15 @@ PostgresMain(const char *dbname, const char *username)
 					char	   *host = MyProcPort->remote_host;
 					const char *authn_id = MyProcPort->authn_id;
 					sa_family_t conn_type = MyProcPort->raddr.addr.ss_family;
+					List	   *guc_options = MyProcPort->guc_options;
+					char	   *cmdline_options = MyProcPort->cmdline_options;
 
-					/* Update the Port details with the new context. */
-					MyProcPort->user_name =
-						(char *) pq_getmsgstring(&input_message);
-					MyProcPort->database_name =
-						(char *) pq_getmsgstring(&input_message);
-					MyProcPort->remote_host =
-						(char *) pq_getmsgstring(&input_message);
+					/*
+					 * Clear guc_options sent in startup packet for
+					 * control_connection_client/db
+					 */
+					MyProcPort->guc_options = NIL;
+					MyProcPort->cmdline_options = NULL;
 
 					/*
 					 * This will be set when authenticating and needs to be
@@ -7194,8 +7236,6 @@ PostgresMain(const char *dbname, const char *username)
 					 * authentication
 					 */
 					MyProcPort->raddr.addr.ss_family = AF_INET;
-					MyProcPort->yb_is_ssl_enabled_in_logical_conn =
-						pq_getmsgbyte(&input_message) == 'E' ? true : false;
 
 					/* Update the `remote_host` */
 					struct sockaddr_in *ip_address_1;
@@ -7205,19 +7245,69 @@ PostgresMain(const char *dbname, const char *username)
 					inet_pton(AF_INET, MyProcPort->remote_host,
 							  &(ip_address_1->sin_addr));
 					MyProcPort->yb_is_auth_passthrough_req = true;
+					MyProcPort->yb_has_auth_passthrough_failed = false;
 
 					/* Start authentication */
-					start_xact_command();
-					ClientAuthentication(MyProcPort);
-					finish_xact_command();
+					{
+						start_xact_command();
+						/*
+						 * Parse input to populate MyProcPort with new client
+						 * context. Also parse GUC vars. We pass true for the
+						 * ssl_done and gss_done args as this negotiation
+						 * between conn mgr and the control backend is already
+						 * done during control backend startup.
+						 */
+						YbProcessStartupPacket(MyProcPort,
+											   true /* ssl_done */ ,
+											   true /* gss_done */ );
+
+						/*
+						 * Set up a timeout in case a buggy or malicious client
+						 * fails to respond during authentication.  Since we're
+						 * inside a transaction and might do database access, we
+						 * have to use the statement_timeout infrastructure.
+						 */
+						enable_timeout_after(STATEMENT_TIMEOUT,
+											 AuthenticationTimeout * 1000);
+
+						ClientAuthentication(MyProcPort);
+
+						/*
+						 * Done with authentication.  Disable the timeout, and
+						 * log if needed.
+						 * TODO (vikram.damle):
+						 * Add connection logging (cf postinit.c:284) and update
+						 * YbGetAuthorizedConnections as done in
+						 * `PerformaAuthentication()`.
+						 */
+						disable_timeout(STATEMENT_TIMEOUT, false);
+
+						/*
+						 * Skip these steps if authentication failed. Normally,
+						 * the backend would just close by passing an ERROR
+						 * level log to ereport, but we pass a WARNING level log
+						 * instead, to avoid closing the control backend. Thus,
+						 * the subsequent steps need to be manually skipped.
+						 */
+						if (!MyProcPort->yb_has_auth_passthrough_failed)
+						{
+							if (YbCreateClientId() == 0)
+								YbAuthPassthroughSetupGUCAndReport();
+						}
+
+						yb_abort_xact_command();
+					}
 
 					/* Place back the old context */
 					MyProcPort->yb_is_auth_passthrough_req = false;
+					MyProcPort->yb_has_auth_passthrough_failed = false;
 					MyProcPort->yb_is_ssl_enabled_in_logical_conn = false;
 					MyProcPort->user_name = user_name;
 					MyProcPort->database_name = db_name;
 					MyProcPort->remote_host = host;
 					MyProcPort->raddr.addr.ss_family = conn_type;
+					MyProcPort->guc_options = guc_options;
+					MyProcPort->cmdline_options = cmdline_options;
 					inet_pton(AF_INET, MyProcPort->remote_host,
 							  &(ip_address_1->sin_addr));
 

commit cc95c8d045ea20dcf28403b4180206cf812636ea

[#30275] YSQL: Connection Manager - Remove mem leak in auth passthrough auth loop
Summary:
This diff fixes a memory leak introduced in
[44a0bfb01cc0678246350160782898b80cc3ded5](https://github.com/yugabyte/yugabyte-db/commit/44a0bfb01cc0678246350160782898b80cc3ded5).
The memory leak was due to the functions `ProcessStartupPacket()` and `set_authn_id()` explicitly
allocating memory for client details in the `TopMemoryContext`. These functions are expected to
be called only during backend startup and the parsed client details are expected to last throughtout
the lifetime of the backend.

However, Auth Passthrough violates this assumption as it uses the same backend to authenticate
multiple clients. The client details parsed from the StartupPacket are not expected to be persistent
after the auth attempt is complete. While other allocations happen in the associated txn
MemoryContext during auth, the above two locations explicitly allocate in the top context, leading to
the leak.

This revision addresses the issue by conditionally using the txn MemoryContext in these two
functions when an Auth Passthrough attempt is in progress.

Test Plan:
Jenkins: all tests, urgent
The memory leak resolution was tested manually by examining memory dumps from lldb after running an
auth-heavy work-load for 30 min. StartupPacket-related data is seen leaking before the fix but not
after the fix.

Reviewers: arpit.saxena, skumar, mkumar, asrinivasan, aagrawal, stiwary

Reviewed By: stiwary

Subscribers: yql

Differential Revision: https://phorge.dev.yugabyte.com/D50359

src/postgres/src/backend/libpq/auth.c
@@ -432,7 +432,18 @@ set_authn_id(Port *port, const char *id)
 							   port->authn_id, id)));
 	}
 
-	port->authn_id = MemoryContextStrdup(TopMemoryContext, id);
+	/*
+	 * YB: When handling an incoming auth request in Auth Passthrough mode,
+	 * the authn_id of the incoming client is not required after auth finishes.
+	 * Thus there is no need to store it in TopMemoryContext; and allocating in
+	 * TopMemoryContext here leads to a memory leak in this scenario (requiring
+	 * an explicit pfree elsewhere). So, we continue allocating in the txn
+	 * MemoryContext spawned specifically for Auth Passthrough auth attempts.
+	 */
+	if (YbIsAuthPassthroughInProgress(port))
+		port->authn_id = pstrdup(id);
+	else
+		port->authn_id = MemoryContextStrdup(TopMemoryContext, id);
 
 	if (Log_connections)
 	{

src/postgres/src/backend/postmaster/postmaster.c
@@ -2345,8 +2345,18 @@ retry1:
 	 * running backend (even after PostmasterContext is destroyed).  We need
 	 * not worry about leaking this storage on failure, since we aren't in the
 	 * postmaster process anymore.
+	 *
+	 * YB: When in auth passthrough mode, we reuse this ProcessStartupPacket
+	 * func to handle client startup packets. The specific details of the client
+	 * are not required after authentication is over. Thus, there is no need to
+	 * store startup data in TopMemoryContext; and allocating in
+	 * TopMemoryContext here leads to a memory leak in this scenario (requiring
+	 * explicit pfree's elsewhere). So, we continue allocating in the txn
+	 * MemoryContext (currently active) spawned specifically for Auth
+	 * Passthrough auth attempts.
 	 */
-	oldcontext = MemoryContextSwitchTo(TopMemoryContext);
+	if (!YbIsAuthPassthroughInProgress(port))
+		oldcontext = MemoryContextSwitchTo(TopMemoryContext);
 
 	/* Handle protocol version 3 startup packet */
 	{
@@ -2605,8 +2615,11 @@ retry1:
 
 	/*
 	 * Done putting stuff in TopMemoryContext.
+	 * YB: No context switch required if Auth Passthrough is in progress.
+	 * See above.
 	 */
-	MemoryContextSwitchTo(oldcontext);
+	if (!YbIsAuthPassthroughInProgress(port))
+		MemoryContextSwitchTo(oldcontext);
 
 	/*
 	 * If we're going to reject the connection due to database state, say so

src/postgres/src/backend/tcop/postgres.c
@@ -7327,6 +7327,12 @@ PostgresMain(const char *dbname, const char *username)
 						yb_abort_xact_command();
 					}
 
+					/*
+					 * NOTE: We don't need to free previous
+					 * MyProcPort fields since they should be allocated in the
+					 * transaction MemoryContext which has been free'd now
+					 */
+
 					/* Place back the old context */
 					MyProcPort->yb_is_auth_passthrough_req = false;
 					MyProcPort->yb_has_auth_passthrough_failed = false;
@@ -7340,11 +7346,6 @@ PostgresMain(const char *dbname, const char *username)
 					inet_pton(AF_INET, MyProcPort->remote_host,
 							  &(ip_address_1->sin_addr));
 
-					/*
-					 * NOTE: We don't need to free previous
-					 * MyProcPort->authn_id since it was allocated in the
-					 * transaction MemoryContext which has been free'd now
-					 */
 					MyProcPort->authn_id = authn_id;
 
 					send_ready_for_query = true;