8b1654c8bbfb6a3eebd017e56c311e7cd50f4594
Arpan Agrawal <arpan3495@gmail.com>
2026-02-27 13:53:17 +0530

    [BACKPORT 2025.2][#1872] YSQL: Refactor/enhance replication code for LISTEN/NOTIFY
    Summary:
    This revision refactors/enhances the replication slot-related APIs so that these can be used for LISTEN/NOTIFY. These include:
    
    - YBCCreateReplicationSlot(): instead of creating the replication slot in the current database, it takes the database oid as an arg.
    - YBCGetReplicationSlot(): add 'if_exists' arg. When this is true, the function doesn't throw an error if the slot doesn't exist; it just returns false.
    - YBCDropReplicationSlot(): add 'if_exists' arg. When this is true, the function doesn't throw an error if the slot doesn't exist.
    - YBGetHeapTuplesForRecord(): Promote this function to the public interface by moving its declaration to the header file.
    - Split YBCReadRecord() into two publicly exposed functions:
       - YBCReadRecord() that just fetches and returns the record. Called by YBXLogReadRecord(). It will also be called by LISTEN/NOTIFY.
       - YBXLogReadRecord() that calls the above and does additional XLogReaderState state management. Called by StartLogicalReplication().
    - ReplicationSlotCreate(): Extract out the YB replication slot creation logic into a new function, YbReplicationSlotCreate(), which will also be called by LISTEN/NOTIFY.
    - ReplicationSlotDrop(): add yb_force and yb_if_exits options.
    
    Original commit: 61929514ce7ffd49ab1e9d147bd84aa65bd8c58b / D49986
    
    Test Plan: Jenkins
    
    Reviewers: sumukh.phalgaonkar, stiwary
    
    Reviewed By: sumukh.phalgaonkar
    
    Subscribers: yql
    
    Tags: #jenkins-ready
    
    Differential Revision: https://phorge.dev.yugabyte.com/D50839

--- src/postgres/src/backend/replication/slotfuncs.c
+++ src/postgres/src/backend/replication/slotfuncs.c
@@ -332,7 +332,8 @@ pg_drop_replication_slot(PG_FUNCTION_ARGS)
 
 	CheckSlotRequirements();
 
-	ReplicationSlotDrop(NameStr(*name), true);
+	ReplicationSlotDrop(NameStr(*name), true, /* yb_force = */ false,
+						/* yb_if_exists = */ false);
 
 	PG_RETURN_VOID();
 }
