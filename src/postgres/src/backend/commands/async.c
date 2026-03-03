yugabyte/yugabyte-db

commit feeb96b138f60708a1bde077384ef85c049657a8

[#1872] YSQL: Support for LISTEN/NOTIFY
Summary:
This revision adds support for the following commands to YB:
- `LISTEN channel`: registers the current session as a listener on the given notification channel.
- `NOTIFY channel [, payload ]`: sends a notification to each client application that has previously executed a LISTEN for the specified channel name in the current database.
- `UNLISTEN { channel | * }`: cancels the registration (if any) of the current session as a listener on the specified notification channel name. The special wildcard `*` cancels all listener registrations for the current session.

=== PG Design ===
- PG has a 'central queue' that stores the undelivered notifications from all the channels across all the databases.
- All notifying backends write the committed notifications to the central queue in the order of their commit time. They also signal the listening backends about the new notification(s).
- On receiving the signal, each listening backend reads the new notification(s) from the queue. It ignores the notifications that do not match its database or its listening channels. It maintains a pointer to the last read notification, which is used to periodically GC the delivered notifications from the queue.

=== YB Design ===
In Yugabyte, the notifying and the listening backends can reside in different nodes. So, just writing and reading to/from the central queue present in the local tserver does not work. So, in YB:
- On a NOTIFY, insert the notifications to the 'pg_yb_notifications' table (created by 77482fb) in a transactional fashion.
- Stream the changes of this table to every tserver that has one or more listening backends using CDC. CDC guarantees that only the committed notifications are streamed, and they are ordered by the commit time.

src/postgres/src/backend/utils/misc/pg_yb_utils.c
@@ -143,6 +143,8 @@ static uint64_t yb_new_catalog_version = YB_CATCACHE_VERSION_UNINITIALIZED;
 static uint64_t yb_logical_client_cache_version = YB_CATCACHE_VERSION_UNINITIALIZED;
 static bool yb_need_invalidate_all_table_cache = false;
 
+static Oid	yb_system_db_oid_cache = InvalidOid;
+
 static bool YbHasDdlMadeChanges();
 static int YbGetNumCreateFunctionStmts();
 static int YbGetNumRollbackToSavepointStmts();
@@ -2102,7 +2104,8 @@ YBCGetSchemaName(Oid schemaoid)
 Oid
 YBCGetDatabaseOid(Relation rel)
 {
-	return YBCGetDatabaseOidFromShared(rel->rd_rel->relisshared);
+	return YBCGetDatabaseOidFromShared(rel->rd_rel->relisshared,
+									   rel->belongs_to_yb_system_db);
 }
 
 Oid
@@ -2112,13 +2115,24 @@ YBCGetDatabaseOidByRelid(Oid relid)
 	bool		relisshared = relation->rd_rel->relisshared;
 
 	RelationClose(relation);
-	return YBCGetDatabaseOidFromShared(relisshared);
+	return YBCGetDatabaseOidFromShared(relisshared,
+									   relation->belongs_to_yb_system_db);
 }
 
 Oid
-YBCGetDatabaseOidFromShared(bool relisshared)
+YbSystemDbOid()
 {
-	return relisshared ? Template1DbOid : MyDatabaseId;
+	if (yb_system_db_oid_cache == InvalidOid)
+		yb_system_db_oid_cache = get_database_oid(YbSystemDbName, true);
+	return yb_system_db_oid_cache;
+}
+
+Oid
+YBCGetDatabaseOidFromShared(bool relisshared, bool belongs_to_yb_system_db)
+{
+	Assert(!relisshared || !belongs_to_yb_system_db);
+	return relisshared ? Template1DbOid :
+		(belongs_to_yb_system_db ? YbSystemDbOid() : MyDatabaseId);
 }
 
 void

java/yb-pgsql/src/test/java/org/yb/pgsql/TestPgRegressPgAsync.java
@@ -1,0 +1,116 @@
+// Copyright (c) YugabyteDB, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+// in compliance with the License.  You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software distributed under the License
+// is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+// or implied.  See the License for the specific language governing permissions and limitations
+// under the License.
+//
+package org.yb.pgsql;
+
+import java.util.Map;
+import java.sql.Connection;
+import java.sql.Statement;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import org.yb.YBTestRunner;
+import org.yb.client.TestUtils;
+import org.yb.util.Timeouts;
+
+/**
+ * Runs the pg_regress test suite on YB code.
+ */
+@RunWith(value=YBTestRunner.class)
+public class TestPgRegressPgAsync extends BasePgRegressTestPorted {
+
+  private void waitForObect(Connection conn, String stmt) throws Exception {
+    Statement statement = conn.createStatement();
+    TestUtils.waitFor(() -> {
+      Row row = getSingleRow(statement, stmt);
+      return row.getInt(0) == 1;
+    }, Timeouts.adjustTimeoutSecForBuildType(120 * 1000));
+  }
+
+  // Wait for master to create yb_system database.
+  private void waitForYbSystemDB() throws Exception {
+    waitForObect(connection,
+        "SELECT" +
+            "  CASE" +
+            "      WHEN EXISTS (" +
+            "          SELECT 1 FROM pg_database WHERE datname = 'yb_system'" +
+            "      )" +
+            "      THEN 1" +
+            "      ELSE 0" +
+            "  END");
+  }
+
+  // Wait for master to create pg_yb_notifications table.
+  private void waitForNotificationsTable() throws Exception {
+    waitForYbSystemDB();
+    Connection conn = getConnectionBuilder().withDatabase("yb_system").connect();
+    waitForObect(conn,
+        " SELECT" +
+            "    CASE" +
+            "        WHEN EXISTS (" +
+            "            SELECT 1 FROM pg_class" +
+            "            WHERE relname = 'pg_yb_notifications'" +
+            "              AND relkind = 'r'" +
+            "              AND relnamespace = 2200" +
+            "        )" +
+            "        THEN 1" +
+            "        ELSE 0" +
+            "    END ");
+  }
+
+  @Override
+  public int getTestMethodTimeoutSec() {
+    return 1800;
+  }
+
+  @Override
+  protected Map<String, String> getTServerFlags() {
+    Map<String, String> flagMap = super.getTServerFlags();
+    flagMap.put("TEST_ysql_yb_enable_listen_notify", "true");
+    return flagMap;
+  }
+
+  @Override
+  protected Map<String, String> getMasterFlags() {
+    Map<String, String> flagMap = super.getMasterFlags();
+    flagMap.put("TEST_ysql_yb_enable_listen_notify", "true");
+    return flagMap;
+  }
+
+  @Test
+  public void schedule() throws Exception {
+    waitForNotificationsTable();
+    runPgRegressTest("yb_pg_async_schedule");
+  }
+
+  @Test
+  public void testIsolationPgRegress() throws Exception {
+    waitForNotificationsTable();
+    runPgRegressTest(
+      PgRegressBuilder.PG_ISOLATION_REGRESS_DIR /* inputDir */,
+      "yb_pg_async_isolation_schedule",
+      0 /* maxRuntimeMillis */, PgRegressBuilder.PG_ISOLATION_REGRESS_EXECUTABLE);
+  }
+
+  @Override
+  protected Map<String, String> getPgRegressEnvVars() {
+    Map<String, String> envs = super.getPgRegressEnvVars();
+
+    // In YB, it takes longer to deliver the notifications. In order to match the
+    // PG's expected output for isolation test async-notify, introduce a sleep in
+    // isolationtester.c after completing execution of each step and before
+    // checking for notifications.
+    envs.put("YB_ISOLATION_TEST_WAIT_FOR_NOTIFS_MS", "500");
+    return envs;
+  }
+}

src/postgres/src/backend/commands/yb_cmds.c
@@ -602,7 +602,9 @@ YBCCreateTable(CreateStmt *stmt, char *tableName, char relkind, TupleDesc desc,
 	YbcPgStatement handle = NULL;
 	ListCell   *listptr;
 	bool		is_shared_relation = tablespaceId == GLOBALTABLESPACE_OID;
-	Oid			databaseId = YBCGetDatabaseOidFromShared(is_shared_relation);
+	Oid			databaseId =
+		YBCGetDatabaseOidFromShared(is_shared_relation,
+									/* belongs_to_yb_system_db= */ false);
 	bool		is_matview = relkind == RELKIND_MATVIEW;
 	bool		is_colocated_tables_with_tablespace_enabled =
 		*YBCGetGFlags()->ysql_enable_colocated_tables_with_tablespaces;

src/postgres/src/backend/parser/gram.y
@@ -1131,12 +1131,12 @@ stmt:
 			| ImportForeignSchemaStmt { parser_ybc_beta_feature(@1, "foreign data wrapper", false); }
 			| IndexStmt
 			| InsertStmt
-			| ListenStmt { parser_ybc_warn_ignored(@1, "LISTEN", 1872); }
+			| ListenStmt
 			| RefreshMatViewStmt
 			| LoadStmt { parser_ybc_not_support(@1, "This statement"); }
 			| LockStmt
 			| MergeStmt { parser_ybc_not_support(@1, "This statement"); }
-			| NotifyStmt { parser_ybc_warn_ignored(@1, "NOTIFY", 1872); }
+			| NotifyStmt
 			| PrepareStmt
 			| ReassignOwnedStmt
 			| ReindexStmt
@@ -1151,7 +1151,7 @@ stmt:
 			| SelectStmt
 			| TransactionStmt
 			| TruncateStmt
-			| UnlistenStmt { parser_ybc_warn_ignored(@1, "UNLISTEN", 1872); }
+			| UnlistenStmt
 			| UpdateStmt
 			| VacuumStmt
 			| VariableResetStmt

src/postgres/src/backend/postmaster/bgworker.c
@@ -37,6 +37,7 @@
 #include "utils/timeout.h"
 
 /* YB includes */
+#include "commands/async.h"
 #include "yb_ash.h"
 #include "yb_query_diagnostics.h"
 
@@ -141,6 +142,9 @@ static const struct
 	},
 	{
 		"YbQueryDiagnosticsDatabaseConnectionWorkerMain", YbQueryDiagnosticsDatabaseConnectionWorkerMain
+	},
+	{
+		"YbNotifsPollerMain", YbNotifsPollerMain
 	}
 };
 
@@ -1320,4 +1324,10 @@ GetBackgroundWorkerTypeByPid(pid_t pid)
 		return NULL;
 
 	return result;
+}
+
+size_t
+YbBackgroundWorkerHandleSize()
+{
+	return sizeof(BackgroundWorkerHandle);
 }

src/postgres/src/backend/utils/cache/relcache.c
@@ -631,6 +631,9 @@ AllocateRelationDesc(Form_pg_class relp)
 	/* which we mark as a reference-counted tupdesc */
 	relation->rd_att->tdrefcount = 1;
 
+	/* YB: See struct field's comment for explanation. */
+	relation->belongs_to_yb_system_db = false;
+
 	MemoryContextSwitchTo(oldcxt);
 
 	return relation;
@@ -4164,6 +4167,9 @@ formrdesc(const char *relationName, Oid relationReltype,
 	 */
 	RelationCacheInsert(relation, false);
 
+	/* YB: See struct field's comment for explanation. */
+	relation->belongs_to_yb_system_db = false;
+
 	/* It's fully valid */
 	relation->rd_isvalid = true;
 }
@@ -5933,6 +5939,9 @@ RelationBuildLocalRelation(const char *relname,
 	 * can't do this before storing relid in it.
 	 */
 	EOXactListAdd(rel);
+
+	/* YB: See struct field's comment for explanation. */
+	rel->belongs_to_yb_system_db = false;
 
 	/* It's fully valid */
 	rel->rd_isvalid = true;

src/postgres/src/backend/utils/init/globals.c
@@ -175,3 +175,7 @@ int64		VacuumPageDirty = 0;
 
 int			VacuumCostBalance = 0;	/* working state for vacuum */
 bool		VacuumCostActive = false;
+
+char        *YbSystemDbName = "yb_system";
+char        *PgYbNotificationsTableName = "pg_yb_notifications";
+char        *PgYbNotificationsPublicationName = "pg_yb_notifications_publication";

commit ff1686ba6ba98661f62763563eaba270d9d1c9d9

[#1872] YSQL: Misc. LISTEN/NOTIFY bug fixes
Summary:
This revision fixes two bugs introduced by feeb96b138f60708a1bde077384ef85c049657a8 / D46148:
  # ybRecordToAsyncQueueEntry(): Incorrectly used VARSIZE_ANY() to get the length of "data_datum" (which is a bytea datum representing the value of pg_yb_notifications.data column). VARSIZE_ANY_EXHDR() should be used instead, as it excludes the header size.
  # YBCGetDatabaseOidByRelid(): Incorrectly read Relation object's belongs_to_yb_system_db field after calling RelationClose().
  # Also fix a code comment typo.

These bugs were identified by Cursor Bugbot.

Test Plan:
It is non-trivial to write tests for these bugs that fail before the fix and pass after.

For issue #1, manually verified the 'datalen' value by logging it:
```
diff --git a/src/postgres/src/backend/commands/async.c b/src/postgres/src/backend/commands/async.c
index 6ecc3973d1..f3c503ba4f 100644
--- a/src/postgres/src/backend/commands/async.c
+++ b/src/postgres/src/backend/commands/async.c
@@ -3012,6 +3012,7 @@ ybRecordToAsyncQueueEntry(const YbcPgRowMessage *record,
 	Assert(!isnull);
 	const void *data = DatumGetPointer(data_datum);
 	size_t		datalen = VARSIZE_ANY_EXHDR(data);
+	elog(LOG, "datalen %lu", datalen);

 	memcpy(qe->data, VARDATA_ANY(data), datalen);

```

Reviewers: jason, stiwary

Reviewed By: jason

Subscribers: yql

Differential Revision: https://phorge.dev.yugabyte.com/D50865

src/postgres/src/backend/commands/async.c
@@ -3011,12 +3011,12 @@ ybRecordToAsyncQueueEntry(const YbcPgRowMessage *record,
 
 	Assert(!isnull);
 	const void *data = DatumGetPointer(data_datum);
-	size_t		datalen = VARSIZE_ANY(data);
+	size_t		datalen = VARSIZE_ANY_EXHDR(data);
 
 	memcpy(qe->data, VARDATA_ANY(data), datalen);
 
 	/*
-	 * Both AsyncQueueEntryEmptySize amd datalen count the two null terminators
+	 * Both AsyncQueueEntryEmptySize and datalen count the two null terminators
 	 * in the 'data' field. Adjust to avoid double counting them.
 	 */
 	int			entryLength = AsyncQueueEntryEmptySize + datalen - 2;

src/postgres/src/backend/utils/misc/pg_yb_utils.c
@@ -2118,10 +2118,11 @@ YBCGetDatabaseOidByRelid(Oid relid)
 {
 	Relation	relation = RelationIdGetRelation(relid);
 	bool		relisshared = relation->rd_rel->relisshared;
+	bool		belongs_to_yb_system_db = relation->belongs_to_yb_system_db;
 
 	RelationClose(relation);
 	return YBCGetDatabaseOidFromShared(relisshared,
-									   relation->belongs_to_yb_system_db);
+									   belongs_to_yb_system_db);
 }
 
 Oid