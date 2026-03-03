f8a82c19ad7a1d4fe6a04286cc1be3d243c95d77
Amitanand Aiyer <amitanandaiyer@users.noreply.github.com>
2026-03-03 09:26:37 +0530

    [BACKPORT 2025.1][#30332,#30312] Docdb: Duplicate value check during ysql create-unique-index should respect IntraWriteId
    Summary:
    Original commit: 412858cfb6cf880d4f3f05ce0a1f2dd673b32a30 / D50490
    Writes backfilling into a ysql unique-index need to check the immediate older and newer entries
    to ensure that uniqueness is not violated.
    
    While checking for newer entries, HasDuplicateUniqueIndexValueBackward calculates the
    FindOldestOverwrittenTimestamp and uses it to read the closest value to check if it matches
    with the row-being inserted (i.e. either it is a delete, or the rows have the same value).
    
    This check should respect DocHybridTime, which includes HybridTime and IntraWriteId.
    Code currently uses HybridTime, which is incorrect, as it does not respect the order of
    writes within the transaction that is adjacent to the row being inserted.
    
    Test Plan: yb_build.sh release --cxx-test pg_index_backfill-test --gtest_filter *DeleteAndInsertSameValueInTxn*
    
    Reviewers: #db-approvers, timur, arybochkin, jason, kfranz, smishra
    
    Reviewed By: kfranz
    
    Subscribers: ybase, yql, svc_phabricator
    
    Differential Revision: https://phorge.dev.yugabyte.com/D50875

--- src/yb/docdb/intent_aware_iterator.h
+++ src/yb/docdb/intent_aware_iterator.h
@@ -139,11 +139,12 @@ class IntentAwareIterator final : public IntentAwareIteratorIf {
       Slice* result_value = nullptr);
 
   // Finds the oldest record for a particular key that is larger than the
-  // specified min_hybrid_time, returns the overwrite time.
+  // specified min_hybrid_time, returns the overwrite time as a DocHybridTime
+  // (including write_id).
   // This record may not be a full record, but instead a merge record (e.g. a
   // TTL row).
-  // Returns HybridTime::kInvalid if no such record was found.
-  Result<HybridTime> FindOldestRecord(Slice key_without_ht, HybridTime min_hybrid_time);
+  // Returns DocHybridTime::kInvalid if no such record was found.
+  Result<DocHybridTime> FindOldestRecord(Slice key_without_ht, HybridTime min_hybrid_time);
 
   void UpdateFilterKey(Slice user_key_for_filter);
 
