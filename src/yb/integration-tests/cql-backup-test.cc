yugabyte/yugabyte-db

commit dc01d397a2dda8bc359592300cafe928f9ed54b4

[#28766] DocDB: Fix a race between alter table and create snapshot
Summary:
Imagine the following scenario on a table `t(k int primary key)`:

1. `ALTER TABLE t ADD COLUMN v INT` command is issued to a pg backend. The command starts executing and is not yet committed in the following steps.
2. pg_backend writes the changes related to this DDL to the pg catalog as intents (not committed yet).
3. pg_backend sends an AlterTable RPC to the master leader.
4. Master leader executed the alter, which means saving the new schema of the DocDB table to the sys.catalog (this is committed) and creating a `YsqlDdlTxnVerifierState` object to track whether the transaction is committed or not from pg.
5. pg_backend commits the transaction, which means the writes to the pg catalog are now committed.
6. pg_backend notifies the master leader that the transaction is committed, which means the master will roll forward and clear the `YsqlDdlTxnVerifierState`.

If a create snapshot RPC arrives after step 4 but before step 5 and picks a `snapshot_hybrid_time` between 4 and 5, then the snapshot will contain the new DocDB schema for table t (that includes column `v`). However, running ysql_dump as of `snapshot_ht` will return the Create table t with the old schema (without column `v`). This is because the new column is not yet committed from pg side.

This leads to inconsistency between the ysql_dump output and the DocDB schema in the snapshot metadata. Consequently, the restore will fail as at restore, we are comparing the DocDB schema of the table exported from the backup side with the DocDB schema created by executing the ysql_dump. Restore was failing if these 2 schemas were not identical.

src/yb/master/catalog_manager_ext.cc
@@ -66,6 +66,7 @@
 #include "yb/master/tablet_split_manager.h"
 #include "yb/master/ts_manager.h"
 #include "yb/master/xcluster_consumer_registry_service.h"
+#include "yb/master/ysql_ddl_verification_task.h"
 #include "yb/master/ysql/ysql_manager_if.h"
 #include "yb/master/ysql_tablegroup_manager.h"
 
@@ -990,6 +991,12 @@ Status CatalogManager::ImportSnapshotPreprocess(
           if (data.old_table_id.empty()) {
             data.old_table_id = entry.id();
             data.table_entry_pb = VERIFY_RESULT(ParseFromSlice<SysTablesEntryPB>(entry.data()));
+            // Mark tables undergoing DDL at backup time to skip schema validation at import.
+            if (data.table_entry_pb.ysql_ddl_txn_verifier_state_size() > 0) {
+            data.validate_schema = false;
+            LOG_WITH_FUNC(INFO) << "Marking table " << data.old_table_id
+                                << " to skip schema validation due to DDL verifier state.";
+            }
             if (backup_entry.has_pg_schema_name()) {
               data.pg_schema_name = backup_entry.pg_schema_name();
             }
@@ -2264,9 +2271,10 @@ Status CatalogManager::ImportTableEntry(
     // Additionally, for indexes, we compare the column ids as we expect them to be
     // preserved.
     const vector<ColumnId>& column_ids = schema.column_ids();
-    if (!persisted_schema.Equals(schema, comparator)
-        || persisted_schema.column_ids().size() != column_ids.size()
-        || (table->is_index() && persisted_schema.column_ids() != column_ids)) {
+    if (table_data->validate_schema &&
+        (!persisted_schema.Equals(schema, comparator) ||
+         persisted_schema.column_ids().size() != column_ids.size() ||
+         (table->is_index() && persisted_schema.column_ids() != column_ids))) {
       const string msg = Format(
           "Invalid created $0 table '$1' in namespace id $2: schema={$3}, expected={$4}",
           TableType_Name(meta.table_type()), meta.name(), new_namespace_id,
@@ -2402,10 +2410,16 @@ Status CatalogManager::ImportTableEntry(
     // 1- Clone case: bump it to current schema version of source table + 1. This ensures that the
     // current schema version is greater than all schema versions that might exist in the snapshot
     // used for clone.
-    // 2- Restoring a backup case: bump the schema version to the schema version of SysTableEntryPB
-    // found in the snapshotInfo if the latter is greater. This is because it is guaranteed that the
-    // schema version found in snapshotInfo is the maximum schema version that can be found in the
-    // snapshot at backup creation time.
+    // 2- Restoring a backup case: bump the schema version to 1 + the schema version of
+    // SysTableEntryPB found in the SnapshotInfoPB if the latter is greater. This is because it is
+    // guaranteed that the schema version found in snapshotInfo is the maximum schema version that
+    // can be found in the snapshot at backup creation time. The one extra schema version bump is
+    // used to avoid any conflict with the snapshot's older schema packings at tserver side.
+    // The last version is used for the committed schema on the master of the restore side
+    // The semantics are as follows: At tserver, all schema packings coming from the snapshot
+    // will be used in tablet-meta and the last schema will have the correct committed schema
+    // created at restore side as part of executing the SQL dump. The last schema is send from the
+    // master to the tservers during ImportSnapshot.
     if (is_clone) {
       // The Source table should be found as we are cloning from it.
       TRACE("Looking up source table");
@@ -2421,8 +2435,8 @@ Status CatalogManager::ImportTableEntry(
       } else {
         schema_version = source_table_lock->pb.version() + 1;
       }
-    } else if (meta.version() > table->LockForRead()->pb.version()) {
-      schema_version = meta.version();
+    } else if (meta.version() >= table->LockForRead()->pb.version()) {
+      schema_version = meta.version() + 1;
     }
 
     if (schema_version) {

src/yb/master/catalog_entity_info.h
@@ -90,6 +90,10 @@ struct ExternalTableSnapshotData {
   // tables' schemas and schema versions are added to the target tablet's superblock when applying
   // the clone_op.
   std::optional<int> new_table_schema_version = std::nullopt;
+  // When false, skip old DocDB vs new DocDB schema equality checks at import time for this table.
+  // This is set to false for YSQL tables that were undergoing DDL verification at backup time
+  // (i.e. the SysTablesEntryPB has ysql_ddl_txn_verifier_state).
+  bool validate_schema = true;
 };
 using ExternalTableSnapshotDataMap = std::unordered_map<TableId, ExternalTableSnapshotData>;
 

src/yb/master/catalog_manager.cc
@@ -7746,6 +7746,7 @@ Status CatalogManager::AlterTable(const AlterTableRequestPB* req,
   lock.unlock();
 
   TEST_SYNC_POINT("YBBackupTestWithColocationParam::AlterTableDocDBTableCommitted");
+  TEST_SYNC_POINT("YBBackupTestWithColocationParam::ContinueAlterTable");
   if (PREDICT_FALSE(FLAGS_TEST_fail_alter_table_after_commit)) {
     return STATUS(IllegalState, "Injected failure after in-memory commit");
   }

src/yb/tablet/tablet_metadata.cc
@@ -338,11 +338,13 @@ Status TableInfo::MergeSchemaPackings(
   // the latest schema.
   const dockv::SchemaPacking& latest_packing = VERIFY_RESULT(
       doc_read_context->schema_packing_storage.GetPacking(schema_version));
-  LOG_IF_WITH_PREFIX(DFATAL,
-                     !latest_packing.SchemaContainsPacking(table_type, doc_read_context->schema()))
-      << "After merging schema packings during restore, latest schema does not"
-      << " have the same packing as the corresponding latest packing for table "
-      << table_id;
+  LOG_IF_WITH_PREFIX(
+      DFATAL, !latest_packing.SchemaContainsPacking(table_type, doc_read_context->schema()))
+      << Format(
+             "After merging schema packings during restore, latest schema does not have the same"
+             "packing as the corresponding latest packing for table $0, latest packing: $1, "
+             "current schema: $2",
+             table_id, latest_packing.ToString(), doc_read_context->schema().ToString());
   return Status::OK();
 }
 

src/yb/tools/yb-backup/yb-backup-during-ddl-test.cc
@@ -14,11 +14,15 @@
 #include <future>
 #include <memory>
 #include <string>
+#include <tuple>
 
 #include "yb/common/colocated_util.h"
 #include "yb/common/transaction.h"
 
 #include "yb/gutil/strings/join.h"
+
+#include "yb/client/client-test-util.h"
+#include "yb/client/table_info.h"
 
 #include "yb/master/master.h"
 #include "yb/master/master_backup.proxy.h"
@@ -31,6 +35,8 @@
 
 #include "yb/util/backoff_waiter.h"
 #include "yb/util/countdown_latch.h"
+#include "yb/util/env.h"
+#include "yb/util/path_util.h"
 #include "yb/util/sync_point.h"
 #include "yb/util/test_thread_holder.h"
 #include "yb/util/test_util.h"
@@ -38,6 +44,7 @@
 
 #include "yb/yql/pgwrapper/pg_mini_test_base.h"
 
+using yb::client::GetTableIdByTableName;
 using yb::client::Snapshots;
 using yb::client::SnapshotTestUtil;
 using yb::client::YBTableName;
@@ -50,11 +57,12 @@ namespace yb {
 namespace tools {
 
 YB_DEFINE_ENUM(YsqlColocationConfig, (kNotColocated)(kDBColocated));
-class YBBackupTestWithColocationParam : public pgwrapper::PgMiniTestBase,
-                                        public YBBackupTestBase,
-                                        public ::testing::WithParamInterface<YsqlColocationConfig> {
+
+// Base class for backup during DDL tests - contains all common functionality
+// that doesn't depend on test parameters.
+class YBBackupDuringDdl : public pgwrapper::PgMiniTestBase, public YBBackupTestBase {
  public:
-  void SetUp() override {
+  void SetUp() {
     ANNOTATE_UNPROTECTED_WRITE(FLAGS_TEST_enable_sync_points) = true;
     // We need the following to be able to run yb-controller with MiniCluster.
     ANNOTATE_UNPROTECTED_WRITE(FLAGS_TEST_use_custom_varz) = true;
@@ -65,7 +73,6 @@ class YBBackupTestWithColocationParam : public pgwrapper::PgMiniTestBase,
     if (UseYbController()) {
       CHECK_OK(cluster_->StartYbControllerServers());
     }
-    CreateDatabase(kBackupSourceDbName, GetParam());
     snapshot_util_ = std::make_unique<SnapshotTestUtil>();
     snapshot_util_->SetProxy(&client_->proxy_cache());
     snapshot_util_->SetCluster(cluster_.get());
@@ -97,6 +104,35 @@ class YBBackupTestWithColocationParam : public pgwrapper::PgMiniTestBase,
     YsqlshRunner ysqlsh_runner =
         VERIFY_RESULT(YsqlshRunner::GetYsqlshRunner(cluster_->YsqlHostport()));
     return ysqlsh_runner.ExecuteSqlScript(sql_script, "ysql_dump" /* tmp_file_prefix */);
+  }
+
+  // Log the contents of a directory recursively for debugging backup artifacts.
+  void LogBackupDirContents(const std::string& dir) {
+    LOG(INFO) << "Listing backup directory contents: " << dir;
+    auto* env = Env::Default();
+    if (!env->DirExists(dir)) {
+      LOG(INFO) << "Directory does not exist: " << dir;
+      return;
+    }
+    Status s = env->Walk(
+        dir, Env::PRE_ORDER,
+        [&](Env::FileType type, const std::string& dirname, const std::string& basename) -> Status {
+          const std::string full = JoinPathSegments(dirname, basename);
+          if (type == Env::DIRECTORY_TYPE) {
+            LOG(INFO) << "dir  " << full;
+          } else {
+            auto size = env->GetFileSize(full);
+            if (size.ok()) {
+              LOG(INFO) << "file " << full << " (" << *size << " bytes)";
+            } else {
+              LOG(INFO) << "file " << full;
+            }
+          }
+          return Status::OK();
+        });
+    if (!s.ok()) {
+      LOG(WARNING) << "Failed to walk directory " << dir << ": " << s.ToString();
+    }
   }
 
   Status AreTablesIncludedInSnapshot(
@@ -185,9 +221,20 @@ class YBBackupTestWithColocationParam : public pgwrapper::PgMiniTestBase,
     return future;
   }
 
+ protected:
   std::unique_ptr<SnapshotTestUtil> snapshot_util_;
   const std::string kBackupSourceDbName = "backup_source_db";
   const std::string kRestoreTargetDbName = "restore_target_db";
+};
+
+// Parametrized test class for colocation-only tests
+class YBBackupTestWithColocationParam : public YBBackupDuringDdl,
+                                        public ::testing::WithParamInterface<YsqlColocationConfig> {
+ public:
+  void SetUp() override {
+    YBBackupDuringDdl::SetUp();
+    CreateDatabase(kBackupSourceDbName, GetParam());
+  }
 };
 
 INSTANTIATE_TEST_CASE_P(
@@ -214,6 +261,7 @@ TEST_P(YBBackupTestWithColocationParam, TestRestorePreserveRelfilenode) {
       {"--backup_location", backup_dir, "--keyspace", Format("ysql.$0", kBackupSourceDbName),
        "create"},
       cluster_.get()));
+  LogBackupDirContents(backup_dir);
   ASSERT_OK(conn.ExecuteFormat("INSERT INTO mytbl (k, v) VALUES (999, 'foo')"));
   ASSERT_OK(RunBackupCommand(
       {"--backup_location", backup_dir, "--keyspace", Format("ysql.$0", kRestoreTargetDbName),
@@ -534,6 +582,194 @@ TEST_P(YBBackupTestWithColocationParam, CleanNamespaceAnchoringAfterFailedSnapsh
         return list_tables_resp.tables_size() == 1;
       },
       30s, "Wait for dropped table to be hard deleted"));
+  LOG(INFO) << "Test finished: " << CURRENT_TEST_CASE_AND_TEST_NAME_STR();
+}
+
+// Base class for Backup creation during ALTER TABLE tests.
+// Test that import_snapshot restores the correct DocDB table schema in case a snapshot is created
+// in the middle of alter table DDL.
+// Timeline of the test:
+// Main Thread                    Master Leader
+//   |                                 |
+//   |1.Create test_table              |
+//   |2.Setup sync points              |
+//   |3.Start AlterTable               |
+//   |4.Create a Backup and            |
+//   |  start CreateSnapshot           |
+//   |                                 |5.Wait until new DocDB table schema is committed
+//   |                                 |6.Select snapshot_ht
+//   |                                 |7.Collect tables (including test_table)
+//   |8.continue AlterTable, commit on pg|
+//   |9.Run ysql_dump as of snapshot_ht|
+//   |10.Wait for snapshot completion  |
+//   |11.Export snapshot               |
+//   |12.Restore the database          |
+//   |13.Verify restore is successful  |
+//   |14.Assert test_table has old     |
+//   |   schema (before the ALTER)     |
+
+class YBBackupDuringAlterTable : public YBBackupDuringDdl,
+                                 public ::testing::WithParamInterface<YsqlColocationConfig> {
+ public:
+  void SetUp() override {
+    YBBackupDuringDdl::SetUp();
+    CreateDatabase(kBackupSourceDbName, GetParam());
+  }
+
+  // Runs the DDL query during backup creation and returns the restored table info and connection.
+  Result<std::pair<client::YBTableInfo, pgwrapper::PGConn>> RunDDLDuringBackup(
+      const std::string& table_name, const std::string& alter_query) {
+    std::vector<SyncPoint::Dependency> dependencies;
+    dependencies.push_back(
+        {"YBBackupTestWithColocationParam::AlterTableDocDBTableCommitted",
+         "YBBackupTestWithColocationParam::ContinueSnapshotCreation"});
+    dependencies.push_back(
+        {"YBBackupTestWithColocationParam::CreateSnapshotEntriesCollected",
+         "YBBackupTestWithColocationParam::ContinueAlterTable"});
+    yb::SyncPoint::GetInstance()->LoadDependency(dependencies);
+    yb::SyncPoint::GetInstance()->EnableProcessing();
+
+    // Run the ALTER TABLE DDL while the backup is being created.
+    // Creating the snapshot will wait until the new schema is committed in DocDB but not in
+    // pg catalog.
+    // The ALTER is committed in pg catalog after the master leader selects the snapshot_hybrid_time
+    // and collects the entries included in the snapshot.
+    // CreateSnapshot will include the new schema as it should be part of the committed DocDB
+    // schema. However, it will also contain the old schema as it might be needed in case of DDL
+    // failure to rollback to the old schema.
+    auto conn = VERIFY_RESULT(ConnectToDB(kBackupSourceDbName));
+    TestThreadHolder thread_holder;
+    thread_holder.AddThreadFunctor([&] {
+      LOG(INFO) << Format("Started Altering table: $0 with query: $1", table_name, alter_query);
+      ASSERT_OK(conn.ExecuteFormat(alter_query));
+    });
+
+    const string backup_dir = GetTempDir("backup");
+    RETURN_NOT_OK(RunBackupCommand(
+        {"--backup_location", backup_dir, "--keyspace", Format("ysql.$0", kBackupSourceDbName),
+         "create"},
+        cluster_.get()));
+    thread_holder.JoinAll();
+
+    LogBackupDirContents(backup_dir);
+    RETURN_NOT_OK(RunBackupCommand(
+        {"--backup_location", backup_dir, "--keyspace", Format("ysql.$0", kRestoreTargetDbName),
+         "restore"},
+        cluster_.get()));
+
+    // Get the restored table info.
+    auto client = VERIFY_RESULT(cluster_->CreateClient());
+    auto table_id =
+        VERIFY_RESULT(GetTableIdByTableName(client.get(), kRestoreTargetDbName, table_name));
+    auto table_info =
+        VERIFY_RESULT(client->GetYBTableInfoById(table_id, false /* include_hidden */));
+    LOG(INFO) << "Table info: " << table_info.schema.ToString();
+
+    auto restored_conn = VERIFY_RESULT(ConnectToDB(kRestoreTargetDbName));
+    return std::make_pair(std::move(table_info), std::move(restored_conn));
+  }
+};
+
+INSTANTIATE_TEST_CASE_P(
+    AlterTable, YBBackupDuringAlterTable,
+    ::testing::Values(YsqlColocationConfig::kNotColocated, YsqlColocationConfig::kDBColocated));
+
+TEST_P(YBBackupDuringAlterTable, AddColumn) {
+  if (!UseYbController()) {
+    GTEST_SKIP() << "Test requires YBC";
+  }
+  auto conn = ASSERT_RESULT(ConnectToDB(kBackupSourceDbName));
+  const std::string table_name = "test_table";
+
+  // Set up table with initial schema
+  ASSERT_OK(conn.ExecuteFormat("CREATE TABLE $0 (k INT PRIMARY KEY)", table_name));
+  ASSERT_OK(conn.ExecuteFormat("INSERT INTO $0 (k) VALUES (1)", table_name));
+  // Add and remove some columns so that we have multiple schema packings
+  ASSERT_OK(conn.ExecuteFormat("ALTER TABLE $0 ADD COLUMN c1 INT", table_name));
+  ASSERT_OK(conn.ExecuteFormat("INSERT INTO $0 (k,c1) VALUES (2, 2)", table_name));
+  ASSERT_OK(conn.ExecuteFormat("ALTER TABLE $0 DROP COLUMN c1", table_name));
+  // Add and remove c2
+  ASSERT_OK(conn.ExecuteFormat("ALTER TABLE $0 ADD COLUMN c2 INT", table_name));
+  ASSERT_OK(conn.ExecuteFormat("INSERT INTO $0 (k,c2) VALUES (3, 3)", table_name));
+  ASSERT_OK(conn.ExecuteFormat("ALTER TABLE $0 DROP COLUMN c2", table_name));
+  // Add column c3 and keep it. This is to test different column_ids in backup/restore side for
+  // the same column name. i.e c3 will have column_id= [1,3] in the [backup,restore] side.
+  ASSERT_OK(conn.ExecuteFormat("ALTER TABLE $0 ADD COLUMN c3 INT", table_name));
+  ASSERT_OK(conn.ExecuteFormat("INSERT INTO $0 (k,c3) VALUES (4, 4)", table_name));
+
+  // Run the ALTER TABLE DDL during backup
+  auto [table_info, restored_conn] = ASSERT_RESULT(
+      RunDDLDuringBackup(table_name, Format("ALTER TABLE $0 ADD COLUMN v TEXT", table_name)));
+
+  // Verify that the restored table has the old schema (before the ALTER).
+  // Should have old schema: just k and c3 columns
+  ASSERT_EQ(table_info.schema.columns().size(), 2);
+  ASSERT_EQ(table_info.schema.columns()[0].name(), "k");
+  ASSERT_EQ(table_info.schema.columns()[1].name(), "c3");
+  auto result =
+      ASSERT_RESULT(restored_conn.FetchRows<int32_t>(Format("SELECT k FROM $0", table_name)));
+  ASSERT_EQ(result.size(), 4);
+  // Try to add a new column with the name v and verify it works successfully
+  ASSERT_OK(restored_conn.ExecuteFormat("ALTER TABLE $0 ADD COLUMN v TEXT", table_name));
+
+  LOG(INFO) << "Test finished: " << CURRENT_TEST_CASE_AND_TEST_NAME_STR();
+}
+
+TEST_P(YBBackupDuringAlterTable, DropColumn) {
+  if (!UseYbController()) {
+    GTEST_SKIP() << "Test requires YBC";
+  }
+  auto conn = ASSERT_RESULT(ConnectToDB(kBackupSourceDbName));
+  const std::string table_name = "test_table";
+
+  // Set up table with initial schema
+  ASSERT_OK(conn.ExecuteFormat("CREATE TABLE $0 (k INT PRIMARY KEY, v TEXT)", table_name));
+  ASSERT_OK(conn.ExecuteFormat("INSERT INTO $0 (k, v) VALUES (1, 'test')", table_name));
+
+  // Run the ALTER TABLE DDL during backup
+  auto [table_info, restored_conn] = ASSERT_RESULT(
+      RunDDLDuringBackup(table_name, Format("ALTER TABLE $0 DROP COLUMN v", table_name)));
+
+  // Verify that the restored table has the old schema (before the ALTER).
+  // Should have old schema: both k and v columns
+  ASSERT_EQ(table_info.schema.columns().size(), 2);
+  ASSERT_EQ(table_info.schema.columns()[0].name(), "k");
+  ASSERT_EQ(table_info.schema.columns()[1].name(), "v");
+  auto result = ASSERT_RESULT(
+      (restored_conn.FetchRows<int32_t, std::string>(Format("SELECT k, v FROM $0", table_name))));
+  ASSERT_EQ(result.size(), 1);
+  ASSERT_EQ(std::get<0>(result[0]), 1);
+  ASSERT_EQ(std::get<1>(result[0]), "test");
+
+  LOG(INFO) << "Test finished: " << CURRENT_TEST_CASE_AND_TEST_NAME_STR();
+}
+
+TEST_P(YBBackupDuringAlterTable, RenameColumn) {
+  if (!UseYbController()) {
+    GTEST_SKIP() << "Test requires YBC";
+  }
+  auto conn = ASSERT_RESULT(ConnectToDB(kBackupSourceDbName));
+  const std::string table_name = "test_table";
+
+  // Set up table with initial schema
+  ASSERT_OK(conn.ExecuteFormat("CREATE TABLE $0 (k INT PRIMARY KEY, v TEXT)", table_name));
+  ASSERT_OK(conn.ExecuteFormat("INSERT INTO $0 (k, v) VALUES (1, 'test')", table_name));
+
+  // Run the ALTER TABLE DDL during backup
+  auto [table_info, restored_conn] = ASSERT_RESULT(
+      RunDDLDuringBackup(table_name, Format("ALTER TABLE $0 RENAME COLUMN v TO v2", table_name)));
+
+  // Verify that the restored table has the old schema (before the ALTER).
+  // Should have old schema: k and v (not v2)
+  ASSERT_EQ(table_info.schema.columns().size(), 2);
+  ASSERT_EQ(table_info.schema.columns()[0].name(), "k");
+  ASSERT_EQ(table_info.schema.columns()[1].name(), "v");
+  auto result = ASSERT_RESULT(
+      (restored_conn.FetchRows<int32_t, std::string>(Format("SELECT k, v FROM $0", table_name))));
+  ASSERT_EQ(result.size(), 1);
+  ASSERT_EQ(std::get<0>(result[0]), 1);
+  ASSERT_EQ(std::get<1>(result[0]), "test");
+
   LOG(INFO) << "Test finished: " << CURRENT_TEST_CASE_AND_TEST_NAME_STR();
 }
 

commit b7327bd55175c3fcd480ad97042c992106e6fed6

[#29789] DocDB: Fix restore regression for YCQL unique index
Summary:
dc01d39 introduced a regression in YCQL backup/restore when the backed-up table has a unique index. During ImportSnapshot, the schema version bumping logic was bumping the schema version to 1 + schema version from the backup side. This was correct for YSQL tables, but unintentionally affects CQL index tables. Since CQL index tables always have schema version 0, CQL unique index writes implicitly use schema version 0 (the field is not set in the protobuf write request).
After the version bump, the tserver holds version 1 for the index table, causing every unique index write to fail with:
```schema version mismatch for table <id>: expected 1, got 0.```
The clone path in ImportTableEntry already had a guard that skips the version bump for CQL index tables, but the restore path did not.
The diff fixes the issue by extracting the CQL index guard into a single check before the clone/restore branch, so both paths are protected. If the table is a CQL index (YQL_TABLE_TYPE and is an index), we assert that meta.version() == 0 and skip the version bump entirely. The clone and restore version-bump logic for non-CQL-index tables is unchanged.

Test Plan: ./yb_build.sh --cxx-test cql-backup-test --gtest_filter CqlBackupTest.TestBackupRestoreWithUniqueIndex

Reviewers: asrivastava, zdrudi

Reviewed By: zdrudi

Subscribers: ybase, hsunder

Differential Revision: https://phorge.dev.yugabyte.com/D50960

src/yb/master/catalog_manager_ext.cc
@@ -2399,36 +2399,31 @@ Status CatalogManager::ImportTableEntry(
       notify_ts_for_schema_change = true;
     }
 
-    // Bump up the current schema version of the target table as follows:
-    // 1- Clone case: bump it to current schema version of source table + 1. This ensures that the
-    // current schema version is greater than all schema versions that might exist in the snapshot
-    // used for clone.
-    // 2- Restoring a backup case: bump the schema version to 1 + the schema version of
-    // SysTableEntryPB found in the SnapshotInfoPB if the latter is greater. This is because it is
-    // guaranteed that the schema version found in snapshotInfo is the maximum schema version that
-    // can be found in the snapshot at backup creation time. The one extra schema version bump is
-    // used to avoid any conflict with the snapshot's older schema packings at tserver side.
-    // The last version is used for the committed schema on the master of the restore side
-    // The semantics are as follows: At tserver, all schema packings coming from the snapshot
-    // will be used in tablet-meta and the last schema will have the correct committed schema
-    // created at restore side as part of executing the SQL dump. The last schema is send from the
-    // master to the tservers during ImportSnapshot.
-    if (is_clone) {
-      // The Source table should be found as we are cloning from it.
+    // Bump up the current schema version of the target table
+    // CQL index tables always have schema version 0 because we do not support dropping or
+    // renaming columns on CQL indexes. CQL index writes depend on this because they implicitly
+    // use a schema_version of 0 (by not setting the field in the protobuf write request). This is
+    // checked against the table schema_version when applying the write. Therefore we must never
+    // bump the schema version for CQL index tables.
+    if (meta.table_type() == TableType::YQL_TABLE_TYPE && table_data->is_index()) {
+      SCHECK_EQ(meta.version(), 0, IllegalState, "CQL index table should have version 0");
+    } else if (is_clone) {
+      // Bump the schema version to 1 + the current schema version of source table. This ensures
+      // that the current schema version is greater than all schema versions that might exist in the
+      //  snapshot used for clone.
       TRACE("Looking up source table");
       TableInfoPtr source_table = VERIFY_RESULT(FindTableById(table_data->old_table_id));
       auto source_table_lock = source_table->LockForRead();
-      if (source_table_lock->table_type() == TableType::YQL_TABLE_TYPE &&
-          source_table_lock->is_index()) {
-        // CQL index tables as of November 2024 always have schema version 0 because we do not
-        // support dropping or renaming columns yet. CQL index deletes depend on this because they
-        // implicitly use a schema_version of 0 (by not setting the field in the protobuf write
-        // request). This is checked against the table schema_version when applying the write.
-        SCHECK_EQ(meta.version() == 0, true, IllegalState, "CQL index table should have version 0");
-      } else {
-        schema_version = source_table_lock->pb.version() + 1;
-      }
+      schema_version = source_table_lock->pb.version() + 1;
     } else if (meta.version() >= table->LockForRead()->pb.version()) {
+      // Restoring a backup: bump the schema version to 1 + the schema version of SysTableEntryPB
+      // found in the SnapshotInfoPB if the latter is >= the current version. It is guaranteed that
+      // the schema version in snapshotInfo is the maximum version that can be found in the snapshot
+      // at backup time. The extra bump avoids conflicts with the snapshot's older schema packings
+      // at tserver side. At the tserver, all schema packings from the snapshot will be used in
+      // tablet-meta and the last schema will have the correct committed schema created at restore
+      // side as part of executing the SQL dump. The last schema is sent from master to tservers
+      // during ImportSnapshot.
       schema_version = meta.version() + 1;
     }
 

src/yb/integration-tests/cql-backup-test.cc
@@ -284,4 +284,30 @@ TEST_F(CqlBackupTest, YB_DISABLE_TEST_IN_SANITIZERS(TestFailedImportSnapshotKeep
   LOG(INFO) << "Test finished: " << CURRENT_TEST_CASE_AND_TEST_NAME_STR();
 }
 
+TEST_F(CqlBackupTest, YB_DISABLE_TEST_IN_SANITIZERS(TestBackupRestoreWithUniqueIndex)) {
+  cql("CREATE TABLE test_tbl (key INT PRIMARY KEY, value INT) "
+      "WITH TRANSACTIONS = {'enabled' : true}");
+  cql("CREATE UNIQUE INDEX test_idx ON test_tbl (value)");
+  cql("INSERT INTO test_tbl (key, value) VALUES (1, 100)");
+  ASSERT_EQ(1, getRowCount());
+
+  ASSERT_OK(RunBackupCommand(
+      {"--backup_location", backup_dir_, "--keyspace", kCqlTestKeyspace, "create"}));
+
+  cql("DROP TABLE test_tbl");
+
+  ASSERT_OK(RunBackupCommand(
+      {"--backup_location", backup_dir_, "--keyspace", kCqlTestKeyspace, "restore"}));
+
+  ASSERT_EQ(1, getRowCount());
+
+  // This INSERT exercises the unique index write path. Unique index writes enforce strict
+  // schema version checks, so this will fail with "schema version mismatch" if the index
+  // table's version was incorrectly bumped from 0 to 1 during ImportSnapshot.
+  cql("INSERT INTO test_tbl (key, value) VALUES (2, 200)");
+
+  ASSERT_EQ(2, getRowCount());
+  LOG(INFO) << "Test finished: " << CURRENT_TEST_CASE_AND_TEST_NAME_STR();
+}
+
 }  // namespace yb