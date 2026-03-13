cfe3d77cd5a13852ebce5efb14402035a9ce2025
Huapeng Yuan <huapeng.yuan@yugabyte.com>
2026-03-13 19:43:48 +0000

    [#30691] DocDB: Fix some failing unit tests involving Serializable
    Summary: After the [change](https://github.com/yugabyte/yugabyte-db/commit/23bf208dcbacff6466c1cab84ab8e4837748a619), a strong lock is taken on tablet level for serializable when skip prefix lock is enabled. It causes more conflict, some test cases need to be fixed.
    
    Test Plan:
    ybd release --java-test 'org.yb.pgsql.TestOneOrTwoAdmins#testOneOrTwoAdmins'
    ybd release --java-test 'org.yb.pgsql.TestPgForeignKey#testInsertConcurrencySerializableIsolation'
    ybd release --java-test 'org.yb.pgsql.TestPgRegressIsolationWithoutWaitQueues#testPgRegress'
    ybd release --java-test 'org.yb.pgsql.TestPgRegressWaitQueues#testPgRegress'
    ybd release --cxx-test pg_misc_conflicts-test
    
    Reviewers: pjain
    
    Reviewed By: pjain
    
    Subscribers: ybase, yql
    
    Differential Revision: https://phorge.dev.yugabyte.com/D51223

--- src/yb/yql/pgwrapper/pg_misc_conflicts-test.cc
+++ src/yb/yql/pgwrapper/pg_misc_conflicts-test.cc
@@ -13,6 +13,7 @@
 
 #include <string>
 #include <string_view>
+#include <tuple>
 #include <utility>
 
 #include <gtest/gtest.h>
@@ -31,6 +32,7 @@
 #include "yb/yql/pgwrapper/pg_test_utils.h"
 
 DECLARE_bool(skip_prefix_locks);
+DECLARE_bool(ysql_enable_packed_row);
 
 using namespace std::literals;
 
@@ -38,6 +40,7 @@ namespace yb::pgwrapper {
 namespace {
 
 YB_DEFINE_ENUM(ExplicitRowLock, (kNoLock)(kForUpdate)(kForNoKeyUpdate)(kForShare)(kForKeyShare));
+YB_DEFINE_ENUM(SkipPrefixLocks, (kFalse)(kTrue));
 
 template<class T>
 std::string TestParamToString(const testing::TestParamInfo<T>& param_info) {
@@ -50,27 +53,42 @@ Status SetExplicitRowLockingBatchSize(PGConn& conn, size_t value) {
 
 } // namespace
 
-class PgMiscConflictsTest : public PgMiniTestBase {
+class PgMiscConflictsTestBase : public PgMiniTestBase {
  protected:
   void SetUp() override {
-    // disable skip_prefix_locks because serializable is being used.
-    // TODO: enhance this with skip_prefix_locks=true
-    ANNOTATE_UNPROTECTED_WRITE(FLAGS_skip_prefix_locks) = false;
+    auto mode = GetSkipPrefixLocks();
+    ANNOTATE_UNPROTECTED_WRITE(FLAGS_skip_prefix_locks) = mode == SkipPrefixLocks::kTrue;
+    ANNOTATE_UNPROTECTED_WRITE(FLAGS_ysql_enable_packed_row) = true;
     EnableFailOnConflict();
     PgMiniTestBase::SetUp();
   }
+
+  virtual SkipPrefixLocks GetSkipPrefixLocks() const = 0;
 
   size_t NumTabletServers() override {
     return 1;
   }
 };
 
-// Test checks conflict detection on colocated table in case of explicit row lock
-TEST_F(PgMiscConflictsTest, TablegroupRowLock) {
+// Parameterized on SkipPrefixLocks so every test automatically runs in both modes.
+class PgMiscConflictsTest : public PgMiscConflictsTestBase,
+                            public testing::WithParamInterface<SkipPrefixLocks> {
+ protected:
+  SkipPrefixLocks GetSkipPrefixLocks() const override { return GetParam(); }
+};
+
+INSTANTIATE_TEST_SUITE_P(
+    , PgMiscConflictsTest,
+    testing::ValuesIn(kSkipPrefixLocksArray),
+    TestParamToString<SkipPrefixLocks>);
+
+TEST_P(PgMiscConflictsTest, TablegroupRowLock) {
   constexpr auto* kTable = "tbl";
 
   auto conn = ASSERT_RESULT(Connect());
   auto aux_conn = ASSERT_RESULT(Connect());
+  ASSERT_OK(conn.ExecuteFormat("DROP TABLE IF EXISTS $0", kTable));
+  ASSERT_OK(conn.Execute("DROP TABLEGROUP IF EXISTS tg"));
   ASSERT_OK(conn.Execute("CREATE TABLEGROUP tg"));
   ASSERT_OK(conn.ExecuteFormat("CREATE TABLE $0(k INT, v INT) TABLEGROUP tg", kTable));
   ASSERT_OK(conn.ExecuteFormat("INSERT INTO $0 VALUES(1, 11)", kTable));
@@ -87,14 +105,16 @@ TEST_F(PgMiscConflictsTest, TablegroupRowLock) {
   ASSERT_OK(conn.CommitTransaction());
 }
 
-// Test checks conflict detection on colocated table with FK
-TEST_F(PgMiscConflictsTest, TablegroupFKDelete) {
+TEST_P(PgMiscConflictsTest, TablegroupFKDelete) {
   constexpr auto* kRefTable = "ref_tbl";
   constexpr auto* kTable = "tbl";
 
   auto conn = ASSERT_RESULT(SetHighPriTxn(Connect()));
   auto aux_conn = ASSERT_RESULT(SetLowPriTxn(Connect()));
 
+  ASSERT_OK(conn.ExecuteFormat("DROP TABLE IF EXISTS $0", kTable));
+  ASSERT_OK(conn.ExecuteFormat("DROP TABLE IF EXISTS $0", kRefTable));
+  ASSERT_OK(conn.Execute("DROP TABLEGROUP IF EXISTS tg"));
   ASSERT_OK(conn.Execute("CREATE TABLEGROUP tg"));
   ASSERT_OK(conn.ExecuteFormat(
       "CREATE TABLE $0(k INT PRIMARY KEY, v INT) TABLEGROUP tg", kRefTable));
@@ -115,23 +135,19 @@ TEST_F(PgMiscConflictsTest, TablegroupFKDelete) {
 
 // Test checks absence of conflict with concurrent update in case only key columns are read by the
 // query and all key columns are specified.
-TEST_F(PgMiscConflictsTest, SerializableIsolationWeakReadIntent) {
+TEST_P(PgMiscConflictsTest, SerializableIsolationWeakReadIntent) {
+  const bool skip_prefix_locks = ANNOTATE_UNPROTECTED_READ(FLAGS_skip_prefix_locks);
   constexpr auto* kTable = "tbl";
 
   auto conn = ASSERT_RESULT(SetHighPriTxn(Connect()));
   auto aux_conn = ASSERT_RESULT(SetLowPriTxn(Connect()));
 
+  ASSERT_OK(conn.ExecuteFormat("DROP TABLE IF EXISTS $0", kTable));
   ASSERT_OK(conn.ExecuteFormat("CREATE TABLE $0 (k INT PRIMARY KEY, v INT)", kTable));
   ASSERT_OK(conn.ExecuteFormat("INSERT INTO $0 VALUES(1, 10)", kTable));
 
   ASSERT_OK(conn.StartTransaction(IsolationLevel::SERIALIZABLE_ISOLATION));
   ASSERT_OK(conn.ExecuteFormat("UPDATE $0 SET v = v + 1 WHERE k = 1", kTable));
-
-  ASSERT_OK(aux_conn.StartTransaction(IsolationLevel::SERIALIZABLE_ISOLATION));
-  // No conflict. Because only key column is read and StrongRead intent is created on
-  // kLivenessColumn of k = 1 row
-  ASSERT_OK(aux_conn.FetchFormat("SELECT k FROM $0 WHERE k = 1", kTable));
-  ASSERT_OK(aux_conn.CommitTransaction());
 
   auto serialize_access_conflict_checker = [&aux_conn](auto&&... args) {
     ASSERT_OK(aux_conn.StartTransaction(IsolationLevel::SERIALIZABLE_ISOLATION));
@@ -139,6 +155,16 @@ TEST_F(PgMiscConflictsTest, SerializableIsolationWeakReadIntent) {
     ASSERT_TRUE(IsSerializeAccessError(status)) << status;
     ASSERT_OK(aux_conn.RollbackTransaction());
   };
+
+  ASSERT_OK(aux_conn.StartTransaction(IsolationLevel::SERIALIZABLE_ISOLATION));
+  if (skip_prefix_locks) {
+    serialize_access_conflict_checker("SELECT k FROM $0 WHERE k = 1", kTable);
+  } else {
+    // No conflict. Because only key column is read and StrongRead intent is created on
+    // kLivenessColumn of k = 1 row
+    ASSERT_OK(aux_conn.FetchFormat("SELECT k FROM $0 WHERE k = 1", kTable));
+    ASSERT_OK(aux_conn.CommitTransaction());
+  }
 
   // Conflict. Because not only key columns are read and StrongRead intent is created on
   // entire k = 1 row
@@ -153,24 +179,34 @@ TEST_F(PgMiscConflictsTest, SerializableIsolationWeakReadIntent) {
   ASSERT_OK(conn.CommitTransaction());
 }
 
-class PgRowLockTest : public PgMiscConflictsTest,
-                      public testing::WithParamInterface<ExplicitRowLock> {
+using PgRowLockTestParam = std::tuple<SkipPrefixLocks, ExplicitRowLock>;
+
+namespace {
+
+std::string PgRowLockTestParamToString(const testing::TestParamInfo<PgRowLockTestParam>& info) {
+  return Format("$0_$1", std::get<0>(info.param), std::get<1>(info.param));
+}
+
+} // namespace
+
+class PgRowLockTest : public PgMiscConflictsTestBase,
+                      public testing::WithParamInterface<PgRowLockTestParam> {
  protected:
-  void SetUp() override {
-    PgMiscConflictsTest::SetUp();
-    auto conn = ASSERT_RESULT(Connect());
-    ASSERT_OK(conn.ExecuteFormat("CREATE TABLE $0 (k INT PRIMARY KEY, v INT)", kTable));
-    ASSERT_OK(conn.ExecuteFormat("INSERT INTO $0 VALUES (1, 10)", kTable));
-  }
+  SkipPrefixLocks GetSkipPrefixLocks() const override { return std::get<0>(GetParam()); }
 
   // Test checks that that serializable read locks are taken even when an explicit row locking
   // clause is specified.
   void DoSerializableTxnUpdate() {
+    auto setup_conn = ASSERT_RESULT(Connect());
+    ASSERT_OK(setup_conn.ExecuteFormat("DROP TABLE IF EXISTS $0", kTable));
+    ASSERT_OK(setup_conn.ExecuteFormat("CREATE TABLE $0 (k INT PRIMARY KEY, v INT)", kTable));
+    ASSERT_OK(setup_conn.ExecuteFormat("INSERT INTO $0 VALUES (1, 10)", kTable));
+
     auto conn = ASSERT_RESULT(SetHighPriTxn(Connect()));
     auto aux_conn = ASSERT_RESULT(SetLowPriTxn(Connect()));
     ASSERT_OK(conn.StartTransaction(IsolationLevel::SERIALIZABLE_ISOLATION));
     ASSERT_OK(conn.FetchFormat(
-        "SELECT * FROM $0 WHERE k = 1 $1", kTable, ToQueryString(GetParam())));
+        "SELECT * FROM $0 WHERE k = 1 $1", kTable, ToQueryString(GetRowLock())));
     ASSERT_OK(aux_conn.StartTransaction(IsolationLevel::SERIALIZABLE_ISOLATION));
     const auto status = aux_conn.ExecuteFormat("UPDATE $0 SET v = 100 WHERE k = 1", kTable);
     ASSERT_TRUE(IsSerializeAccessError(status)) << "Unexpected status " << status;
@@ -181,6 +217,8 @@ class PgRowLockTest : public PgMiscConflictsTest,
   static constexpr auto kTable = "tbl"sv;
 
  private:
+  ExplicitRowLock GetRowLock() const { return std::get<1>(GetParam()); }
+
   std::string_view ToQueryString(ExplicitRowLock row_lock) {
     switch(row_lock) {
       case ExplicitRowLock::kNoLock: return std::string_view();
@@ -193,17 +231,21 @@ class PgRowLockTest : public PgMiscConflictsTest,
   }
 };
 
-INSTANTIATE_TEST_CASE_P(
+INSTANTIATE_TEST_SUITE_P(
     PgMiscConflictsTest, PgRowLockTest,
-    testing::ValuesIn(kExplicitRowLockArray), TestParamToString<ExplicitRowLock>);
+    testing::Combine(
+        testing::ValuesIn(kSkipPrefixLocksArray),
+        testing::ValuesIn(kExplicitRowLockArray)),
+    PgRowLockTestParamToString);
 
 TEST_P(PgRowLockTest, SerializableTxnUpdate) {
   DoSerializableTxnUpdate();
 }
 
-TEST_F(PgMiscConflictsTest, DifferentColumnsConcurrentUpdate) {
+TEST_P(PgMiscConflictsTest, DifferentColumnsConcurrentUpdate) {
   auto conn = ASSERT_RESULT(SetHighPriTxn(Connect()));
   auto aux_conn = ASSERT_RESULT(SetLowPriTxn(Connect()));
+  ASSERT_OK(conn.Execute("DROP TABLE IF EXISTS t"));
   ASSERT_OK(conn.Execute("CREATE TABLE t (k INT PRIMARY KEY, v1 INT, v2 INT)"));
   ASSERT_OK(conn.Execute("INSERT INTO t VALUES(1, 1, 1)"));
   ASSERT_OK(conn.StartTransaction(IsolationLevel::SNAPSHOT_ISOLATION));
@@ -216,27 +258,25 @@ TEST_F(PgMiscConflictsTest, DifferentColumnsConcurrentUpdate) {
 
 class PgExplicitRowLockingTest : public PgMiscConflictsTest {
  protected:
-  void SetUp() override {
-    PgMiscConflictsTest::SetUp();
-    CHECK_OK(Prepare());
-  }
-
-  Status CreateKVTableWithValues(std::string_view name) {
-    RETURN_NOT_OK(conn_->ExecuteFormat("CREATE TABLE $0 (k INT PRIMARY KEY, v INT)", name));
-    return conn_->ExecuteFormat("INSERT INTO $0 SELECT s, s FROM generate_series(1, 3) AS s", name);
+  Status CreateKVTableWithValues(PGConn& conn, std::string_view name) {
+    RETURN_NOT_OK(conn.ExecuteFormat("DROP TABLE IF EXISTS $0", name));
+    RETURN_NOT_OK(conn.ExecuteFormat("CREATE TABLE $0 (k INT PRIMARY KEY, v INT)", name));
+    return conn.ExecuteFormat("INSERT INTO $0 SELECT s, s FROM generate_series(1, 3) AS s", name);
   }
 
   // Helper function to check that error message in batched and non batched mode matches.
-  Status CheckRowLockErrorMessage(std::string_view table_name, const std::string& lock_query) {
+  Status CheckRowLockErrorMessage(
+      PGConn& conn, PGConn& low_pri_conn,
+      std::string_view table_name, const std::string& lock_query) {
     static const auto kErrorPattern = "could not obtain lock on row in relation \"$0\""s;
 
     auto row_locker =
-        [&conn = *low_pri_conn_, &lock_query](size_t batch_size) mutable -> Result<std::string> {
-          RETURN_NOT_OK(SetExplicitRowLockingBatchSize(conn, batch_size));
-          return TryLockRowWithFailure(conn, lock_query);
+        [&low_pri_conn, &lock_query](size_t batch_size) mutable -> Result<std::string> {
+          RETURN_NOT_OK(SetExplicitRowLockingBatchSize(low_pri_conn, batch_size));
+          return TryLockRowWithFailure(low_pri_conn, lock_query);
         };
-    RETURN_NOT_OK(conn_->StartTransaction(IsolationLevel::READ_COMMITTED));
-    RETURN_NOT_OK(conn_->Fetch(Format("SELECT * FROM $0 WHERE k = 1 FOR UPDATE", table_name)));
+    RETURN_NOT_OK(conn.StartTransaction(IsolationLevel::READ_COMMITTED));
+    RETURN_NOT_OK(conn.Fetch(Format("SELECT * FROM $0 WHERE k = 1 FOR UPDATE", table_name)));
 
     const auto non_batched_mode_error_msg = VERIFY_RESULT(row_locker(/*batch_size =*/1));
     const auto expected_substring = Format(kErrorPattern, table_name);
@@ -246,14 +286,11 @@ class PgExplicitRowLockingTest : public PgMiscConflictsTest {
         batched_mode_error_msg, non_batched_mode_error_msg, IllegalState,
         "Error messages in batched and non-batched mode expected to be equal");
 
-    return conn_->CommitTransaction();
+    return conn.CommitTransaction();
   }
 
   static constexpr auto kTableName_1 = "very_very_long_name_1"sv;
   static constexpr auto kTableName_2 = "very_very_long_name_2"sv;
-
-  std::optional<PGConn> conn_;
-  std::optional<PGConn> low_pri_conn_;
 
  private:
   static Result<std::string> TryLockRowWithFailure(PGConn& conn, const std::string& lock_query) {
@@ -263,37 +300,38 @@ class PgExplicitRowLockingTest : public PgMiscConflictsTest {
     RETURN_NOT_OK(conn.RollbackTransaction());
     return AuxilaryMessage(status).value();
   }
-
-  Status Prepare() {
-    DCHECK(!conn_ && !low_pri_conn_);
-    conn_.emplace(VERIFY_RESULT(SetHighPriTxn(Connect())));
-    low_pri_conn_.emplace(VERIFY_RESULT(SetLowPriTxn(Connect())));
-    return CreateKVTableWithValues(kTableName_1);
-  }
-
 };
+
+INSTANTIATE_TEST_SUITE_P(
+    , PgExplicitRowLockingTest,
+    testing::ValuesIn(kSkipPrefixLocksArray),
+    TestParamToString<SkipPrefixLocks>);
 
 // The test checks error messages in case of using batched and non batched mode for explicit
 // row locking match and both contains expected table name.
-TEST_F_EX(
-    PgMiscConflictsTest, ExplicitRowLockingSingleTableErrorMessage, PgExplicitRowLockingTest) {
-  constexpr auto kTableName = kTableName_1;
+TEST_P(PgExplicitRowLockingTest, ExplicitRowLockingSingleTableErrorMessage) {
+  auto conn = ASSERT_RESULT(SetHighPriTxn(Connect()));
+  auto low_pri_conn = ASSERT_RESULT(SetLowPriTxn(Connect()));
+  ASSERT_OK(CreateKVTableWithValues(conn, kTableName_1));
 
   ASSERT_OK(CheckRowLockErrorMessage(
-      kTableName, Format("SELECT * FROM $0 WHERE k = 1 FOR SHARE NOWAIT", kTableName)));
+      conn, low_pri_conn, kTableName_1,
+      Format("SELECT * FROM $0 WHERE k = 1 FOR SHARE NOWAIT", kTableName_1)));
 }
 
 // The test checks error messages in case of using batched and non batched mode for explicit
 // row locking match and both contains expected table name.
-TEST_F_EX(
-    PgMiscConflictsTest, ExplicitRowLockingMultipleTablesErrorMessage, PgExplicitRowLockingTest) {
-  ASSERT_OK(CreateKVTableWithValues(kTableName_2));
+TEST_P(PgExplicitRowLockingTest, ExplicitRowLockingMultipleTablesErrorMessage) {
+  auto conn = ASSERT_RESULT(SetHighPriTxn(Connect()));
+  auto low_pri_conn = ASSERT_RESULT(SetLowPriTxn(Connect()));
+  ASSERT_OK(CreateKVTableWithValues(conn, kTableName_1));
+  ASSERT_OK(CreateKVTableWithValues(conn, kTableName_2));
 
   const auto lock_query = Format(
       "SELECT * FROM $0 INNER JOIN $1 ON $0.k = $1.k WHERE $0.k = 1 FOR SHARE NOWAIT",
       kTableName_1, kTableName_2);
-  ASSERT_OK(CheckRowLockErrorMessage(kTableName_1, lock_query));
-  ASSERT_OK(CheckRowLockErrorMessage(kTableName_2, lock_query));
+  ASSERT_OK(CheckRowLockErrorMessage(conn, low_pri_conn, kTableName_1, lock_query));
+  ASSERT_OK(CheckRowLockErrorMessage(conn, low_pri_conn, kTableName_2, lock_query));
 }
 
 } // namespace yb::pgwrapper
