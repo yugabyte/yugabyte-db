11996518e583c9e82c468d740bbceaae4b84df98
Sergei Politov <spolitov@gmail.com>
2026-03-23 16:24:49 +0300

    [BACKPORT 2025.2][#30240] YSQL: Introduce PGConn::CopyFromStdin method
    Summary:
    === Backport Notes (master --> 2025.2) ===
     - Minor adjacency conflicts in libpq_utils.cc and libpq_utils.h due to
       absence of bee33af716dc5728668b412c4adc2d67e9e9d204 in 2025.2. Resolve
       by retaining 2025.2 side of changes.
     - The import of f7bba4dad5ba92a1d9dcfc959421ccb19435931e introduced a new test failure
       `PgLibPqTest.BulkCopy` in debug/ASAN builds which is fixed by commit 621e2be274ea7b79357470a9c4f3dffd1a5176c7.
    
    This revision backports the following fixes.
    Original commits:
     - f7bba4dad5ba92a1d9dcfc959421ccb19435931e / D50470
     - 621e2be274ea7b79357470a9c4f3dffd1a5176c7 / D51439
    
    === f7bba4dad5ba92a1d9dcfc959421ccb19435931e / D50470 ===
    As part of simplification `PGConn` interface this diff introduces the `CopyFromStdin` method which substitues the following ones:
    -  CopyBegin
    -  CopyEnd
    -  CopyStartRow
    -  CopyPutInt16
    -  CopyPutInt32
    -  CopyPutInt64
    -  CopyPut
    -  CopyPutString
    
    The `CopyFromStdin` simplifies the `COPY FROM STDIN`usage because of the following:
    - caller code no need to vary about number of columns and keep them in sync with actual number of specified columns
    - `PGConn` has no intermediate state like before between `CopyStart` / `CopyEnd`
    - caller code no need to write PostgreSQL query
    
    Old usage:
    
    ```
      ASSERT_OK(conn_->CopyBegin(Format("COPY $0 FROM STDIN WITH BINARY", kTableName)));
      for (int i = 6; i <= 9; ++i) {
        conn_->CopyStartRow(2 /* number of columns */);
        conn_->CopyPutInt32(i);
        conn_->CopyPutString(i < 8 ? "" : "not default");
      }
      ASSERT_OK(conn_->CopyEnd());
    ```
    
    New usage
    
    ```
      ASSERT_OK(conn_->CopyFromStdin(
          kTableName,
          [](PGConn::RowMaker<int32_t, std::string_view>& row) {
            for (int i = 6; i <= 9; ++i) {
              row(i, i < 8 ? "" : "not default");
            }
          }));
    ```
    
    **Note**:
    This diff introduces new helper class `CopyController` in the `libpq_utils::internal` namespace. This class is implemented in such a way to use forward declaration only (in header file). This is done to simplify future ridding off `#include "libpq-fe.h"` from header.
    
    === 621e2be274ea7b79357470a9c4f3dffd1a5176c7 / D51439 ===
    The bug was introduced by f7bba4dad5ba92a1d9dcfc959421ccb19435931e / D50470.
    CopyController receives std::array by value and stores pointers to its contents.
    
    Fixed by passing fixed size std::span instead of std::array
    
    Test Plan: Jenkins
    
    Reviewers: jason, sergei, myang
    
    Reviewed By: myang
    
    Subscribers: yql
    
    Tags: #jenkins-ready
    
    Differential Revision: https://phorge.dev.yugabyte.com/D53312

--- src/yb/yql/pgwrapper/pg_mini-test.cc
+++ src/yb/yql/pgwrapper/pg_mini-test.cc
@@ -1023,14 +1023,13 @@ TEST_F_EX(PgMiniTest, BulkCopyWithRestart, PgMiniSmallWriteBufferTest) {
     });
 
     while (!stop.load(std::memory_order_acquire) && key < kBatchSize * kTotalBatches) {
-      ASSERT_OK(connection.CopyBegin(Format("COPY $0 FROM STDIN WITH BINARY", kTableName)));
-      for (int j = 0; j != kBatchSize; ++j) {
-        connection.CopyStartRow(2);
-        connection.CopyPutInt32(++key);
-        connection.CopyPutString(RandomHumanReadableString(kValueSize));
-      }
-
-      ASSERT_OK(connection.CopyEnd());
+      ASSERT_OK(connection.CopyFromStdin(
+          kTableName,
+          [&key](PGConn::RowMaker<int32_t, std::string_view>& row) {
+            for (int j = 0; j != kBatchSize; ++j) {
+              row(++key, RandomHumanReadableString(kValueSize));
+            }
+          }));
     }
   });
 
@@ -2593,14 +2592,13 @@ TEST_F_EX(PgMiniTest, DISABLED_ReadsDuringRBS, PgMiniStreamCompressionTest) {
 
   auto conn = ASSERT_RESULT(Connect());
   ASSERT_OK(conn.Execute("CREATE TABLE t (key INT PRIMARY KEY, value BYTEA) SPLIT INTO 1 TABLETS"));
-  ASSERT_OK(conn.CopyBegin("COPY t FROM STDIN WITH BINARY"));
-  for (auto key : Range(kNumRows)) {
-    conn.CopyStartRow(2);
-    conn.CopyPutInt32(key);
-    conn.CopyPutString(RandomString(kValueSize));
-  }
-  ASSERT_OK(conn.CopyEnd());
-
+  ASSERT_OK(conn.CopyFromStdin(
+      "t",
+      [](PGConn::RowMaker<int32_t, std::string_view>& row) {
+        for (auto key : Range(kNumRows)) {
+          row(key, RandomString(kValueSize));
+        }
+      }));
   FlushAndCompactTablets();
 
   LOG(INFO) << "Rows: " << ASSERT_RESULT(conn.FetchAllAsString("SELECT key FROM t"));
@@ -3398,6 +3396,15 @@ $procedure$
 
   thread_holder.WaitAndStop(kWaitTime);
   ASSERT_GT(num_read_restarts, 0);
+}
+
+Status FetchMatrix(PGConn& conn, const std::string& command, int rows, int cols) {
+  auto res = VERIFY_RESULT(conn.FetchRows<RowAsString>(command));
+  SCHECK_EQ(res.size(), rows, RuntimeError, "Unexpected number of rows");
+  if (!res.empty()) {
+    SCHECK_EQ(res.front().size(), cols, RuntimeError, "Unexpected number of cols");
+  }
+  return Status::OK();
 }
 
 }  // namespace yb::pgwrapper
