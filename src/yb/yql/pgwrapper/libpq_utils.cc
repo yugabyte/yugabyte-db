yugabyte/yugabyte-db

commit f7bba4dad5ba92a1d9dcfc959421ccb19435931e

[#30240] YSQL: Introduce PGConn::CopyFromStdin method
Summary:
As part of simplification `PGConn` interface this diff introduces the `CopyFromStdin` method which substitues the following ones:
-  CopyBegin
-  CopyEnd
-  CopyStartRow
-  CopyPutInt16
-  CopyPutInt32
-  CopyPutInt64
-  CopyPut
-  CopyPutString

The `CopyFromStdin` simplifies the `COPY FROM STDIN`usage because of the following:
- caller code no need to vary about number of columns and keep them in sync with actual number of specified columns
- `PGConn` has no intermediate state like before between `CopyStart` / `CopyEnd`
- caller code no need to write PostgreSQL query

Old usage:

```
  ASSERT_OK(conn_->CopyBegin(Format("COPY $0 FROM STDIN WITH BINARY", kTableName)));
  for (int i = 6; i <= 9; ++i) {
    conn_->CopyStartRow(2 /* number of columns */);
    conn_->CopyPutInt32(i);
    conn_->CopyPutString(i < 8 ? "" : "not default");
  }
  ASSERT_OK(conn_->CopyEnd());
```

New usage

```
  ASSERT_OK(conn_->CopyFromStdin(
      kTableName,
      [](PGConn::RowMaker<int32_t, std::string_view>& row) {
        for (int i = 6; i <= 9; ++i) {
          row(i, i < 8 ? "" : "not default");
        }
      }));
```

**Note**:
This diff introduces new helper class `CopyController` in the `libpq_utils::internal` namespace. This class is implemented in such a way to use forward declaration only (in header file). This is done to simplify future ridding off `#include "libpq-fe.h"` from header.

Test Plan: Jenkins

Reviewers: jason, sergei, myang

Reviewed By: myang

Subscribers: yql

Tags: #jenkins-ready

Differential Revision: https://phorge.dev.yugabyte.com/D50470

src/yb/yql/pgwrapper/libpq_utils.cc
@@ -14,8 +14,8 @@
 #include "yb/yql/pgwrapper/libpq_utils.h"
 
 #include <chrono>
-#include <string>
-#include <utility>
+#include <concepts>
+#include <span>
 
 #include <boost/algorithm/string/replace.hpp>
 #include <boost/preprocessor/seq/for_each.hpp>
@@ -319,48 +319,263 @@ bool IsValidType(Oid pg_type) {
   return IsValidType<typename T::Type>(pg_type);
 }
 
+Result<PGResultPtr> CheckResult(PGResultPtr src, const std::string& command) {
+  auto* result = src.get();
+  const auto status = PQresultStatus(result);
+  switch(status) {
+    case ExecStatusType::PGRES_TUPLES_OK: [[fallthrough]];
+    case ExecStatusType::PGRES_COPY_IN:
+      return src;
+
+    case ExecStatusType::PGRES_COPY_BOTH:        [[fallthrough]];
+    case ExecStatusType::PGRES_COPY_OUT:         [[fallthrough]];
+    case ExecStatusType::PGRES_COMMAND_OK:       [[fallthrough]];
+    case ExecStatusType::PGRES_EMPTY_QUERY:      [[fallthrough]];
+    case ExecStatusType::PGRES_BAD_RESPONSE:     [[fallthrough]];
+    case ExecStatusType::PGRES_NONFATAL_ERROR:   [[fallthrough]];
+    case ExecStatusType::PGRES_FATAL_ERROR:      [[fallthrough]];
+    case ExecStatusType::PGRES_SINGLE_TUPLE:     [[fallthrough]];
+    case ExecStatusType::PGRES_PIPELINE_SYNC:    [[fallthrough]];
+    case ExecStatusType::PGRES_PIPELINE_ABORTED:
+      break;
+  }
+
+  const auto msg = GetPQErrorMessage(result);
+  return STATUS(
+      NetworkError,
+      Format("Fetch '$0' failed: $1, message: $2", command, status, msg),
+      Slice() /* msg2 */,
+      PgsqlError(GetSqlState(result))).CloneAndAddErrorCode(AuxilaryMessage(msg));
+}
+
 }  // namespace
+
+namespace libpq_utils::internal {
+
+template<BasePGType T>
+Status GetValueHelper<T>::CheckType(const PGresult* result, int column) {
+  const auto pg_type = PQftype(result, column);
+  SCHECK(
+      IsValidType<T>(pg_type), Corruption,
+      "Unexpected type $0 of column #$1 ('$2')", pg_type, column, PQfname(result, column));
+  return Status::OK();
+}
+
+template<BasePGType T>
+GetValueResult<T> GetValueHelper<T>::Get(const PGresult* result, int row, int column) {
+  SCHECK(
+      (!PQgetisnull(result, row, column)),
+      Corruption, "Unexpected NULL value at row: $0, column: $1", row, column);
+
+  if constexpr (IsPGNonNeg<T>) {
+    const auto value = VERIFY_RESULT(GetValueHelper<typename T::Type>::Get(result, row, column));
+    SCHECK_GE(value, 0, Corruption, "Bad narrow cast");
+    return value;
+  } else if constexpr (IsPGFloatType<T>) { // NOLINT
+    using FloatType = typename FloatTraits<T>::FloatType;
+    using IntType = typename FloatTraits<T>::IntType;
+    static_assert(sizeof(FloatType) == sizeof(IntType), "Wrong sizes");
+    const auto value = VERIFY_RESULT(GetValueImpl<IntType>(result, row, column));
+    return *pointer_cast<const FloatType*>(&value);
+  } else if constexpr (IsPGIntType<T>) {
+    return GetValueImpl<std::make_unsigned_t<T>>(result, row, column);
+  } else if constexpr (std::is_same_v<T, MonoDelta>) { // NOLINT
+    return MonoDelta::FromMicroseconds(
+        VERIFY_RESULT(GetValueHelper<int64_t>::Get(result, row, column)));
+  } else if constexpr (std::is_same_v<T, JsonDocument>) { // NOLINT
+    JsonDocument doc;
+    RETURN_NOT_OK(doc.Parse(VERIFY_RESULT(GetValueHelper<std::string>::Get(result, row, column))));
+    return doc;
+  } else {
+    return GetValueImpl<typename PGTypeTraits<T>::ReturnType>(result, row, column);
+  }
+}
+
+template struct GetValueHelper<int16_t>;
+template struct GetValueHelper<int32_t>;
+template struct GetValueHelper<int64_t>;
+template struct GetValueHelper<PGUint16>;
+template struct GetValueHelper<PGUint32>;
+template struct GetValueHelper<PGUint64>;
+template struct GetValueHelper<float>;
+template struct GetValueHelper<double>;
+template struct GetValueHelper<bool>;
+template struct GetValueHelper<std::string>;
+template struct GetValueHelper<char>;
+template struct GetValueHelper<PGOid>;
+template struct GetValueHelper<Uuid>;
+template struct GetValueHelper<MonoDelta>;
+template struct GetValueHelper<JsonDocument>;
+
+FetchHelper<RowAsString>::RowsResult FetchHelper<RowAsString>::FetchRows(
+    Result<PGResultPtr>&& source) {
+  auto result = VERIFY_RESULT(std::move(source));
+  RowsType rows;
+  auto num_rows = PQntuples(result.get());
+  rows.reserve(num_rows);
+  for (decltype(num_rows) row = 0; row < num_rows; ++row) {
+    rows.push_back(VERIFY_RESULT(RowToString(result.get(), row, DefaultColumnSeparator())));
+  }
+  return rows;
+}
+
+class CopyStoreInt {
+  static void DoStore(uint16_t value, char* buf) { BigEndian::Store16(buf, value); }
+  static void DoStore(uint32_t value, char* buf) { BigEndian::Store32(buf, value); }
+  static void DoStore(uint64_t value, char* buf) { BigEndian::Store64(buf, value); }
+
+ public:
+  static constexpr size_t kMaxSize = sizeof(uint64_t);
+
+  template<class T>
+  requires(std::integral<T> && requires (T t) { DoStore(std::make_unsigned_t<T>{}, nullptr); })
+  static char* Store(T value, char* buf) {
+    using UT = std::make_unsigned_t<T>;
+    static_assert(sizeof(UT) <= kMaxSize);
+    DoStore(static_cast<UT>(value), buf);
+    return buf + sizeof(UT);
+  }
+};
+
+template<class T>
+concept CopyProtocolInt = requires (const T& t) { CopyStoreInt::Store(t, nullptr); };
+
+class CopyController {
+ public:
+  template<size_t N>
+  CopyController(
+      PGconn* conn, std::array<char, N> buffer, std::string_view table, CopyOptions options)
+      : conn_(conn), buffer_(buffer), pos_(&buffer_.front()) {
+    static_assert(N >= kNumOfAlwaysAvailableBytes);
+    const auto command = Format(
+        "COPY $0 FROM STDIN WITH (FORMAT BINARY$1)",
+        table,
+        options.rows_per_txn
+            ? Format(", ROWS_PER_TRANSACTION $0", options.rows_per_txn)
+            : std::string());
+    status_ = ResultToStatus(CheckResult(
+        PGResultPtr(PQexecParams(conn_, command.c_str(), 0, nullptr, nullptr, nullptr, nullptr, 0)),
+        command));
+    Write("PGCOPY\n\xff\r\n\0\0\0\0\0\0\0\0\0"sv);
+  }
+
+  Status Finalize() {
+    Write(static_cast<uint16_t>(0xFFFF));
+    RETURN_NOT_OK(DoFlush());
+    const auto res = PQputCopyEnd(conn_, nullptr);
+    SCHECK(res > 0, NetworkError, "Put copy end failed: $0", res);
+
+    PGResultPtr result(PQgetResult(conn_));
+    const auto status = PQresultStatus(result.get());
+    switch(status) {
+      case ExecStatusType::PGRES_COPY_BOTH:  [[fallthrough]];
+      case ExecStatusType::PGRES_COPY_IN:    [[fallthrough]];
+      case ExecStatusType::PGRES_COPY_OUT:   [[fallthrough]];
+      case ExecStatusType::PGRES_COMMAND_OK:
+        return Status::OK();
+
+      case ExecStatusType::PGRES_EMPTY_QUERY:      [[fallthrough]];
+      case ExecStatusType::PGRES_TUPLES_OK:        [[fallthrough]];
+      case ExecStatusType::PGRES_BAD_RESPONSE:     [[fallthrough]];
+      case ExecStatusType::PGRES_NONFATAL_ERROR:   [[fallthrough]];
+      case ExecStatusType::PGRES_FATAL_ERROR:      [[fallthrough]];
+      case ExecStatusType::PGRES_SINGLE_TUPLE:     [[fallthrough]];
+      case ExecStatusType::PGRES_PIPELINE_SYNC:    [[fallthrough]];
+      case ExecStatusType::PGRES_PIPELINE_ABORTED:
+        break;
+    }
+    return STATUS_FORMAT(
+        NetworkError,
+        "Copy end failed, status: $0, message: $1", status, GetPQErrorMessage(result.get()));
+  }
+
+ private:
+  friend class RowMakerImpl;
+
+  static constexpr size_t kNumOfAlwaysAvailableBytes = CopyStoreInt::kMaxSize * 2;
+
+  template<CopyProtocolInt T, CopyProtocolInt... Args>
+  void Write(T v1, Args... v2) {
+    static_assert(sizeof...(v2) < 2);
+    if (!status_.ok()) {
+      return;
+    }
+    DCHECK_GE(BytesLeft(), kNumOfAlwaysAvailableBytes);
+    pos_ = CopyStoreInt::Store(v1, pos_);
+    if constexpr (sizeof...(v2) == 1) {
+      pos_ = CopyStoreInt::Store(v2..., pos_);
+    }
+    Flush();
+  }
+
+  void Write(std::span<const char> buf) {
+    for (const auto* p = buf.data(), *end = buf.data() + buf.size(); status_.ok();) {
+      const auto count = std::min(BytesLeft(), static_cast<size_t>(end - p));
+      memcpy(pos_, p, count);
+      p += count;
+      pos_ += count;
+      if (p == end) {
+        break;
+      }
+      Flush();
+    }
+    Flush();
+  }
+
+  size_t BytesLeft() const {
+    return &buffer_.back() - pos_ + 1;
+  }
+
+  void Flush() {
+    if (status_.ok()) {
+      status_ = DoFlush(/* force_flush = */ false);
+    }
+  }
+
+  Status DoFlush(bool force_flush = true) {
+    const auto* begin = buffer_.data();
+    const auto len = pos_ - begin;
+    if ((len > 0 && force_flush) || len + kNumOfAlwaysAvailableBytes > buffer_.size()) {
+      pos_ = &buffer_.front();
+      const auto res = PQputCopyData(conn_, begin, narrow_cast<int>(len));
+      SCHECK(res >= 0, NetworkError, "Put copy data failed: $0", res);
+    }
+    return Status::OK();
+  }
+
+  PGconn* conn_;
+  std::span<char> buffer_;
+  char* pos_;
+  Status status_{Status::OK()};
+};
+
+template<class T>
+auto FieldSize(T value) {
+  return static_cast<uint32_t>(value);
+}
+
+void RowMakerImpl::Put(int32_t value) {
+  impl_.Write(FieldSize(4), value);
+}
+
+void RowMakerImpl::Put(int64_t value) {
+  impl_.Write(FieldSize(8), value);
+}
+
+void RowMakerImpl::Put(std::string_view value) {
+  impl_.Write(FieldSize(value.size()));
+  impl_.Write(value);
+}
+
+void RowMakerImpl::StartRow(uint16_t columns) {
+  impl_.Write(columns);
+}
+
+} // namespace libpq_utils::internal
 
 void PGConnClose::operator()(PGconn* conn) const {
   PQfinish(conn);
 }
-
-struct PGConn::CopyData {
-  static constexpr size_t kBufferSize = 2048;
-
-  Status error;
-  char * pos;
-  char buffer[kBufferSize];
-
-  void Start() {
-    pos = buffer;
-    error = Status::OK();
-  }
-
-  void WriteUInt16(uint16_t value) {
-    BigEndian::Store16(pos, value);
-    pos += 2;
-  }
-
-  void WriteUInt32(uint32_t value) {
-    BigEndian::Store32(pos, value);
-    pos += 4;
-  }
-
-  void WriteUInt64(uint64_t value) {
-    BigEndian::Store64(pos, value);
-    pos += 8;
-  }
-
-  void Write(const char* value, size_t len) {
-    memcpy(pos, value, len);
-    pos += len;
-  }
-
-  size_t left() const {
-    return buffer + kBufferSize - pos;
-  }
-};
 
 Result<PGConn> PGConn::Connect(const std::string& conn_str,
                                CoarseTimePoint deadline,
@@ -467,19 +682,6 @@ ConnStatusType PGConn::ConnStatus() {
 bool PGConn::IsBusy() {
   static constexpr int kIsBusy = 1;
   return PQisBusy(impl_.get()) == kIsBusy;
-}
-
-Result<PGResultPtr> CheckResult(PGResultPtr result, const std::string& command) {
-  auto status = PQresultStatus(result.get());
-  if (ExecStatusType::PGRES_TUPLES_OK != status && ExecStatusType::PGRES_COPY_IN != status) {
-    auto msg = GetPQErrorMessage(result.get());
-    return STATUS(NetworkError,
-                  Format("Fetch '$0' failed: $1, message: $2",
-                         command, status, msg),
-                  Slice() /* msg2 */,
-                  PgsqlError(GetSqlState(result.get()))).CloneAndAddErrorCode(AuxilaryMessage(msg));
-  }
-  return result;
 }
 
 Result<PGResultPtr> PGConn::Fetch(const std::string& command) {
@@ -595,121 +797,14 @@ Result<bool> PGConn::HasScanType(const std::string& query, const std::string exp
   return false;
 }
 
-Status PGConn::CopyBegin(const std::string& command) {
-  auto result = VERIFY_RESULT(CheckResult(
-      PGResultPtr(
-          PQexecParams(impl_.get(), command.c_str(), 0, nullptr, nullptr, nullptr, nullptr, 0)),
-      command));
-
-  if (!copy_data_) {
-    copy_data_.reset(new CopyData);
+Status PGConn::DoCopyFromStdin(
+    std::string_view table, const CopyOptions& options, const ControllerReceiver& receiver) {
+  if (!copy_buffer_) {
+    copy_buffer_ = std::make_unique<CopyBuffer>();
   }
-  copy_data_->Start();
-
-  static const char prefix[] = "PGCOPY\n\xff\r\n\0\0\0\0\0\0\0\0\0";
-  copy_data_->Write(prefix, sizeof(prefix) - 1);
-
-  return Status::OK();
-}
-
-bool PGConn::CopyEnsureBuffer(size_t len) {
-  if (!copy_data_->error.ok()) {
-    return false;
-  }
-  if (copy_data_->left() < len) {
-    return CopyFlushBuffer();
-  }
-  return true;
-}
-
-void PGConn::CopyStartRow(int16_t columns) {
-  if (!CopyEnsureBuffer(2)) {
-    return;
-  }
-  copy_data_->WriteUInt16(columns);
-}
-
-bool PGConn::CopyFlushBuffer() {
-  if (!copy_data_->error.ok()) {
-    return false;
-  }
-  ptrdiff_t len = copy_data_->pos - copy_data_->buffer;
-  if (len) {
-    int res = PQputCopyData(impl_.get(), copy_data_->buffer, narrow_cast<int>(len));
-    if (res < 0) {
-      copy_data_->error = STATUS_FORMAT(NetworkError, "Put copy data failed: $0", res);
-      return false;
-    }
-  }
-  copy_data_->Start();
-  return true;
-}
-
-void PGConn::CopyPutInt16(int16_t value) {
-  if (!CopyEnsureBuffer(6)) {
-    return;
-  }
-  copy_data_->WriteUInt32(2);
-  copy_data_->WriteUInt16(value);
-}
-
-void PGConn::CopyPutInt32(int32_t value) {
-  if (!CopyEnsureBuffer(8)) {
-    return;
-  }
-  copy_data_->WriteUInt32(4);
-  copy_data_->WriteUInt32(value);
-}
-
-void PGConn::CopyPutInt64(int64_t value) {
-  if (!CopyEnsureBuffer(12)) {
-    return;
-  }
-  copy_data_->WriteUInt32(8);
-  copy_data_->WriteUInt64(value);
-}
-
-void PGConn::CopyPut(const char* value, size_t len) {
-  if (!CopyEnsureBuffer(4)) {
-    return;
-  }
-  copy_data_->WriteUInt32(static_cast<uint32_t>(len));
-  for (;;) {
-    size_t left = copy_data_->left();
-    if (copy_data_->left() < len) {
-      copy_data_->Write(value, left);
-      value += left;
-      len -= left;
-      if (!CopyFlushBuffer()) {
-        return;
-      }
-    } else {
-      copy_data_->Write(value, len);
-      break;
-    }
-  }
-}
-
-Result<PGResultPtr> PGConn::CopyEnd() {
-  if (CopyEnsureBuffer(2)) {
-    copy_data_->WriteUInt16(static_cast<uint16_t>(-1));
-  }
-  if (!CopyFlushBuffer()) {
-    return copy_data_->error;
-  }
-  int res = PQputCopyEnd(impl_.get(), nullptr);
-  if (res <= 0) {
-    return STATUS_FORMAT(NetworkError, "Put copy end failed: $0", res);
-  }
-
-  PGResultPtr result(PQgetResult(impl_.get()));
-  auto status = PQresultStatus(result.get());
-  if (status == ExecStatusType::PGRES_COPY_BOTH || status == ExecStatusType::PGRES_COPY_IN ||
-      status == ExecStatusType::PGRES_COPY_OUT || status == ExecStatusType::PGRES_COMMAND_OK) {
-    return result;
-  }
-  auto msg = GetPQErrorMessage(result.get());
-  return STATUS_FORMAT(NetworkError, "Copy end failed, status: $0, message: $1", status, msg);
+  libpq_utils::internal::CopyController controller{impl_.get(), *copy_buffer_, table, options};
+  receiver(controller);
+  return controller.Finalize();
 }
 
 Result<std::string> ToString(const PGresult* result, int row, int column) {
@@ -886,74 +981,4 @@ PGConnBuilder CreateInternalPGConnBuilder(
        .yb_auto_analyze = yb_auto_analyze});
 }
 
-namespace libpq_utils::internal {
-
-template<BasePGType T>
-Status GetValueHelper<T>::CheckType(const PGresult* result, int column) {
-  const auto pg_type = PQftype(result, column);
-  SCHECK(
-      IsValidType<T>(pg_type), Corruption,
-      "Unexpected type $0 of column #$1 ('$2')", pg_type, column, PQfname(result, column));
-  return Status::OK();
-}
-
-template<BasePGType T>
-GetValueResult<T> GetValueHelper<T>::Get(const PGresult* result, int row, int column) {
-  SCHECK(
-      (!PQgetisnull(result, row, column)),
-      Corruption, "Unexpected NULL value at row: $0, column: $1", row, column);
-
-  if constexpr (IsPGNonNeg<T>) {
-    const auto value = VERIFY_RESULT(GetValueHelper<typename T::Type>::Get(result, row, column));
-    SCHECK_GE(value, 0, Corruption, "Bad narrow cast");
-    return value;
-  } else if constexpr (IsPGFloatType<T>) { // NOLINT
-    using FloatType = typename FloatTraits<T>::FloatType;
-    using IntType = typename FloatTraits<T>::IntType;
-    static_assert(sizeof(FloatType) == sizeof(IntType), "Wrong sizes");
-    const auto value = VERIFY_RESULT(GetValueImpl<IntType>(result, row, column));
-    return *pointer_cast<const FloatType*>(&value);
-  } else if constexpr (IsPGIntType<T>) {
-    return GetValueImpl<std::make_unsigned_t<T>>(result, row, column);
-  } else if constexpr (std::is_same_v<T, MonoDelta>) { // NOLINT
-    return MonoDelta::FromMicroseconds(
-        VERIFY_RESULT(GetValueHelper<int64_t>::Get(result, row, column)));
-  } else if constexpr (std::is_same_v<T, JsonDocument>) { // NOLINT
-    JsonDocument doc;
-    RETURN_NOT_OK(doc.Parse(VERIFY_RESULT(GetValueHelper<std::string>::Get(result, row, column))));
-    return doc;
-  } else {
-    return GetValueImpl<typename PGTypeTraits<T>::ReturnType>(result, row, column);
-  }
-}
-
-template struct GetValueHelper<int16_t>;
-template struct GetValueHelper<int32_t>;
-template struct GetValueHelper<int64_t>;
-template struct GetValueHelper<PGUint16>;
-template struct GetValueHelper<PGUint32>;
-template struct GetValueHelper<PGUint64>;
-template struct GetValueHelper<float>;
-template struct GetValueHelper<double>;
-template struct GetValueHelper<bool>;
-template struct GetValueHelper<std::string>;
-template struct GetValueHelper<char>;
-template struct GetValueHelper<PGOid>;
-template struct GetValueHelper<Uuid>;
-template struct GetValueHelper<MonoDelta>;
-template struct GetValueHelper<JsonDocument>;
-
-FetchHelper<RowAsString>::RowsResult FetchHelper<RowAsString>::FetchRows(
-    Result<PGResultPtr>&& source) {
-  auto result = VERIFY_RESULT(std::move(source));
-  RowsType rows;
-  auto num_rows = PQntuples(result.get());
-  rows.reserve(num_rows);
-  for (decltype(num_rows) row = 0; row < num_rows; ++row) {
-    rows.push_back(VERIFY_RESULT(RowToString(result.get(), row, DefaultColumnSeparator())));
-  }
-  return rows;
-}
-
-} // namespace libpq_utils::internal
 } // namespace yb::pgwrapper

src/yb/yql/pgwrapper/libpq_utils.h
@@ -13,9 +13,11 @@
 
 #pragma once
 
+#include <array>
 #include <memory>
 #include <optional>
 #include <string>
+#include <string_view>
 #include <tuple>
 #include <type_traits>
 #include <utility>
@@ -27,6 +29,7 @@
 
 #include "yb/util/format.h"
 #include "yb/util/json_document.h"
+#include "yb/util/lw_function.h"
 #include "yb/util/monotime.h"
 #include "yb/util/net/net_fwd.h"
 #include "yb/util/result.h"
@@ -250,6 +253,55 @@ class FetchHelper<RowAsString> {
   static RowsResult FetchRows(Result<PGResultPtr>&& source);
 };
 
+struct CopyOptions {
+  std::optional<size_t> rows_per_txn{};
+};
+
+class CopyController;
+
+class RowMakerImpl {
+ public:
+  explicit RowMakerImpl(CopyController& impl) : impl_(impl) {}
+
+  template<class... Args>
+  void operator()(const Args&... args) {
+    StartRow(sizeof...(Args));
+    DoPut(args...);
+  }
+
+ private:
+  template<class T, class... Tail>
+  void DoPut(const T& v, const Tail&... tail) {
+    Put(v);
+    if constexpr (sizeof...(Tail) > 0) {
+      DoPut(tail...);
+    }
+  }
+
+  void Put(int32_t value);
+  void Put(int64_t value);
+  void Put(std::string_view value);
+  void StartRow(uint16_t columns);
+
+  CopyController& impl_;
+};
+
+template<class... Args>
+class RowMaker {
+ public:
+  explicit RowMaker(CopyController& controller) : impl_(controller) {}
+
+  void operator()(const Args&... args) { impl_(args...); }
+
+ private:
+  RowMakerImpl impl_;
+};
+
+template<class T, class... Args>
+auto MakeRowMaker(CopyController& controller, void (T::*)(RowMaker<Args...>&) const) {
+  return RowMaker<Args...>{controller};
+}
+
 } // namespace libpq_utils::internal
 
 class PGConn {
@@ -335,31 +387,35 @@ class PGConn {
   Result<bool> HasIndexScan(const std::string& query);
   Result<bool> HasScanType(const std::string& query, const std::string expected_scan_type);
 
-  Status CopyBegin(const std::string& command);
-  Result<PGResultPtr> CopyEnd();
+  template<class... Args>
+  using RowMaker = libpq_utils::internal::RowMaker<Args...>;
+  using CopyOptions = libpq_utils::internal::CopyOptions;
+  using CopyController = libpq_utils::internal::CopyController;
 
-  void CopyStartRow(int16_t columns);
-
-  void CopyPutInt16(int16_t value);
-  void CopyPutInt32(int32_t value);
-  void CopyPutInt64(int64_t value);
-  void CopyPut(const char* value, size_t len);
-
-  void CopyPutString(const std::string& value) {
-    CopyPut(value.c_str(), value.length());
+  template<class RowProvider, class... Args>
+  Status CopyFromStdin(
+      std::string_view table, const RowProvider& row_provider, const CopyOptions& options = {}) {
+    return DoCopyFromStdin(
+        table, options,
+        make_lw_function([&row_provider](CopyController& controller) {
+          auto maker = MakeRowMaker(controller, &RowProvider::operator());
+          row_provider(maker);
+        }));
   }
 
  private:
-  struct CopyData;
+  using ControllerReceiver = LWFunction<void(CopyController&)>;
+
+  Status DoCopyFromStdin(
+      std::string_view table, const CopyOptions& options, const ControllerReceiver& receiver);
 
   PGConn(PGConnPtr ptr, bool simple_query_protocol);
 
-  bool CopyEnsureBuffer(size_t len);
-  bool CopyFlushBuffer();
+  using CopyBuffer = std::array<char, 2048>;
 
   PGConnPtr impl_;
   bool simple_query_protocol_;
-  std::unique_ptr<CopyData> copy_data_;
+  std::unique_ptr<CopyBuffer> copy_buffer_;
 };
 
 // Settings to pass to PGConnBuilder.

src/yb/yql/pgwrapper/pg_alter_add_column_default-test.cc
@@ -101,19 +101,20 @@ TEST_P(PgAddColumnDefaultTest, AddColumnDefaultCopy) {
   // COPY new rows into table.
   // Insert the following rows:
   // (4), (5), (6, null), (7, null), (8, "not default"), (9, "not default")
-  ASSERT_OK(conn_->CopyBegin(Format("COPY $0(t) FROM STDIN WITH BINARY", kTableName)));
-  for (int i = 4; i <= 5; ++i) {
-    conn_->CopyStartRow(1 /* number of columns */);
-    conn_->CopyPutInt32(i);
-  }
-  ASSERT_OK(conn_->CopyEnd());
-  ASSERT_OK(conn_->CopyBegin(Format("COPY $0 FROM STDIN WITH BINARY", kTableName)));
-  for (int i = 6; i <= 9; ++i) {
-    conn_->CopyStartRow(2 /* number of columns */);
-    conn_->CopyPutInt32(i);
-    conn_->CopyPutString(i < 8 ? "" : "not default");
-  }
-  ASSERT_OK(conn_->CopyEnd());
+  ASSERT_OK(conn_->CopyFromStdin(
+      Format("$0(t)", kTableName),
+      [](PGConn::RowMaker<int32_t>& row) {
+        for (int i = 4; i <= 5; ++i) {
+          row(i);
+        }
+      }));
+  ASSERT_OK(conn_->CopyFromStdin(
+      kTableName,
+      [](PGConn::RowMaker<int32_t, std::string_view>& row) {
+        for (int i = 6; i <= 9; ++i) {
+          row(i, i < 8 ? "" : "not default");
+        }
+      }));
 
   auto table_content_checker = [&conn = this->conn_]() -> Status {
     auto rows = VERIFY_RESULT((conn->FetchRows<int32_t, std::string>(

src/yb/yql/pgwrapper/pg_copy-test.cc
@@ -80,23 +80,21 @@ TEST_F(PgCopyTest, TestRetriesAreDisabledForCopy) {
   ANNOTATE_UNPROTECTED_WRITE(FLAGS_TEST_respond_write_with_abort_probability) = 0.2;
   const auto kRowsPerTransaction = 2;
   const auto kNumRows = 20;
-  ASSERT_OK(conn.CopyBegin(
-      Format("COPY $0 FROM STDIN WITH (FORMAT BINARY, ROWS_PER_TRANSACTION $1)",
-          kTable, kRowsPerTransaction)));
-  for (int i = 0; i < kNumRows; ++i) {
-    LOG(INFO) << "Inserting row: " << i;
-    conn.CopyStartRow(2 /* number of columns */);
-    conn.CopyPutInt32(i);
-    conn.CopyPutInt32(i);
-  }
-  auto result = conn.CopyEnd();
+  auto status = conn.CopyFromStdin(
+      kTable,
+      [](PGConn::RowMaker<int32_t, int32_t>& row) {
+        for (int i = 0; i < kNumRows; ++i) {
+          row(i, i);
+        }
+      },
+      {.rows_per_txn = kRowsPerTransaction});
 
   // If retries were allowed for COPY, we would face the "COPY file signature not recognized" error
   // when the query layer tries to perform the first retry.
-  if (!result.ok()) {
-    LOG(INFO) << "Status: " << result.status().ToString();
+  if (!status.ok()) {
+    LOG(INFO) << "Status: " << status;
     ASSERT_STR_CONTAINS(
-        result.status().message().ToString(), "Transaction expired or aborted by a conflict");
+        status.message().ToString(), "Transaction expired or aborted by a conflict");
   }
 
   // Ensure that a prefix of rows are visible i.e., without any holes. Just in case there was a

src/yb/yql/pgwrapper/pg_index-test.cc
@@ -155,15 +155,14 @@ TEST_F(PgIndexTest, RandomIndexScan) {
       });
   std::shuffle(rows.begin(), rows.end(), rng);
   for (size_t i = 0; i != kNumChunks; ++i) {
-    ASSERT_OK(conn_->CopyBegin("COPY test FROM STDIN WITH BINARY"));
-    for (size_t j = 0; j != kRowsPerChunk; ++j) {
-      conn_->CopyStartRow(3);
-      auto [key, value] = rows[i * kRowsPerChunk + j];
-      conn_->CopyPutInt64(key);
-      conn_->CopyPutInt64(value);
-      conn_->CopyPutInt64(-key);
-    }
-    ASSERT_OK(conn_->CopyEnd());
+    ASSERT_OK(conn_->CopyFromStdin(
+        "test",
+        [&rows, i](PGConn::RowMaker<int64_t, int64_t, int64_t>& row) {
+          for (size_t j = 0; j != kRowsPerChunk; ++j) {
+            const auto& [key, value] = rows[i * kRowsPerChunk + j];
+            row(key, value, -key);
+          }
+        }));
     ASSERT_OK(WaitForAllIntentsApplied(cluster_.get()));
     ASSERT_OK(cluster_->FlushTablets());
   }

commit 621e2be274ea7b79357470a9c4f3dffd1a5176c7

[#30782] DocDB: Fix COPY implementation in libpq_utils
Summary:
The bug was introduced by f7bba4dad5ba92a1d9dcfc959421ccb19435931e / D50470.
CopyController receives std::array by value and stores pointers to its contents.

Fixed by passing fixed size std::span instead of std::array

Test Plan: ./yb_build.sh asan --cxx-test pgwrapper_pg_alter_add_column_default-test --gtest_filter 'AddColumnDefaultTest/PgAddColumnDefaultTest.AddColumnDefaultCopy/0'

Reviewers: dmitry, myang, #db-approvers

Reviewed By: dmitry, myang

Subscribers: svc_phabricator, yql, ybase

Tags: #jenkins-ready

Differential Revision: https://phorge.dev.yugabyte.com/D51439

src/yb/yql/pgwrapper/libpq_utils.cc
@@ -444,12 +444,14 @@ template<class T>
 concept CopyProtocolInt = requires (const T& t) { CopyStoreInt::Store(t, nullptr); };
 
 class CopyController {
+  static constexpr size_t kNumOfAlwaysAvailableBytes = CopyStoreInt::kMaxSize * 2;
+
  public:
   template<size_t N>
+  requires(N > kNumOfAlwaysAvailableBytes)
   CopyController(
-      PGconn* conn, std::array<char, N> buffer, std::string_view table, CopyOptions options)
+      PGconn* conn, std::span<char, N> buffer, std::string_view table, CopyOptions options)
       : conn_(conn), buffer_(buffer), pos_(&buffer_.front()) {
-    static_assert(N >= kNumOfAlwaysAvailableBytes);
     const auto command = Format(
         "COPY $0 FROM STDIN WITH (FORMAT BINARY$1)",
         table,
@@ -494,8 +496,6 @@ class CopyController {
 
  private:
   friend class RowMakerImpl;
-
-  static constexpr size_t kNumOfAlwaysAvailableBytes = CopyStoreInt::kMaxSize * 2;
 
   template<CopyProtocolInt T, CopyProtocolInt... Args>
   void Write(T v1, Args... v2) {
@@ -811,7 +811,8 @@ Status PGConn::DoCopyFromStdin(
   if (!copy_buffer_) {
     copy_buffer_ = std::make_unique<CopyBuffer>();
   }
-  libpq_utils::internal::CopyController controller{impl_.get(), *copy_buffer_, table, options};
+  libpq_utils::internal::CopyController controller{
+      impl_.get(), std::span{*copy_buffer_}, table, options};
   receiver(controller);
   return controller.Finalize();
 }