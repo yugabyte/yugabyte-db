cfc6f569c67dc2b06b8f833cf9a287208887b1d6
Hamidreza Zare <hzare@yugabyte.com>
2026-04-01 14:14:58 -0400

    [BACKPORT 2025.1][PLAT-20031][xCluster] Increase the timeout for pause xCluster RPC
    Summary:
    For yb-client in YBA, we have two timeout configs, AdminOperationTimeoutMs and OperationTimeoutMs . The first has a default value of 2 minutes and the second has a default value of 30 seconds. From the yb-client java implementation, the min value of the two is used for the `SetUniverseReplicationEnabled` RPC.
    
    The internal testing shows 2 minutes is a more appropriate timeout for this RPC. This diff adds a runtime config for this and set the default value to 2 minutes.
    
    Original commit: ff307ca0507344ef704f9d44224728abc249165e / D51016
    
    Test Plan: - tested pause/enable xCluster config works as expected.
    
    Reviewers: amindrov, spothuraju, jmak, anijhawan, nbhatia
    
    Reviewed By: jmak
    
    Subscribers: yugaware
    
    Differential Revision: https://phorge.dev.yugabyte.com/D51705

--- managed/src/main/java/com/yugabyte/yw/common/config/UniverseConfKeys.java
+++ managed/src/main/java/com/yugabyte/yw/common/config/UniverseConfKeys.java
@@ -1172,6 +1172,15 @@ public class UniverseConfKeys extends RuntimeConfigKeysModule {
               + " fails.",
           ConfDataType.DurationType,
           ImmutableList.of(ConfKeyTags.PUBLIC));
+  public static final ConfKeyInfo<Duration> xclusterPauseRpcTimeout =
+      new ConfKeyInfo<>(
+          "yb.xcluster.pause_rpc_timeout",
+          ScopeType.UNIVERSE,
+          "Timeout for xCluster Pause RPC call",
+          "Timeout for xCluster Pause RPC call; if the SetUniverseReplicationEnabled RPC times out,"
+              + " you can increase this runtime config to alleviate the issue",
+          ConfDataType.DurationType,
+          ImmutableList.of(ConfKeyTags.PUBLIC));
   public static final ConfKeyInfo<Duration>
       xclusterDbScopedDeleteReplicationOnSourceTimeoutDuringFailover =
           new ConfKeyInfo<>(
