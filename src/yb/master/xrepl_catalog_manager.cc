a454a270ec2a93d84c55661c0aa168ef4011cf24
Devansh Singhal <devansh5398@gmail.com>
2026-04-02 13:12:05 +0530

    [BACKPORT 2025.1][#30773] CDC: Fixing bug of having cdc state table entry for ineligible table
    Summary:
    #### Backport Description
    
    - This version doesn't have https://phorge.dev.yugabyte.com/D48349 which supports streaming of tables without primary key. As a result of this, removed the test `TestNoEntryAddedInCDCStateTableForIneligibleTable` since that was applicable when few streams allow tables without primary key while others don't.
    - Need to set `set_pgsql_proxy_bind_address = true` in `CDCSDKTestBase::SetUpWithParams()` in the introduced test `TestNoEntryAddedInCDCStateTableForIndex` to ensures the flag `FLAGS_pgsql_proxy_bind_address` is properly configured before the cluster starts, allowing the tserver to connect to Postgres via the correct unix socket path.
    - Also, the function `IsTableEligibleForCDCSDKStream()` only takes 3 parameters in current version. So, made changes to it calls accordingly.
    
    #### Original Description
    
    ###### Code changes summary
    If a namespace already have a cdcsdk logical replication stream and when a index (or any non eligible table wrt the existing logical replication stream) is created in the same namespace, the current workflow erroneously populates the cdc state table entry for this ineligible table.
    This doesn’t affect the UPAM or cdc service as such. However, the retention barriers gets placed on the new tablets of this created table.
    UPAM won’t be able to move ahead retention barriers of these entries. The reason is that the newly created ineligible table is not present in stream metadata and thus UPAM keep getting `Unable to obtain before image / replica identity information for tablet` error in its iteration.
    
    Thus although the retention barriers will get released once they go stale, but we are erroneously holding the resources for some time. Also, the cdc state table for such table will never get cleaned up (unless there a tablet split on it, which cleans up the parent tablet entry).
    
    This revision is to correct the logic of not adding the cdc state table entry for such tables at the first place.
    
    ###### Upgrade/Rollback safety
    Doesn't change any existing proto. Also since during upgrade the DDLs are anyways restricted, the diff won't affect upgrades.
    
    ###### Considerations for colocated tables
    Since the cdc_state table will have the entry for existing tablet-stream pair, creating a new colocated table in the same tablet won't add the new entry to cdc_state table.
    
    ###### Compatibility with logical and gRPC streams
    The bug occurs only for logical streams. So the revision fixes only for the same.
    
    ###### Considerations for connector
    NA
    
    Jira: DB-20667 [link](https://yugabyte.atlassian.net/browse/DB-20667)
    
    Original commit: 05d1e497bc39619b9ef3acce59526198972ee838 / D51423
    
    Test Plan: yb_build.sh --cxx-test integration-tests_cdcsdk_ysql-test --gtest_filter CDCSDKYsqlTest.TestNoEntryAddedInCDCStateTableForIndex
    
    Reviewers: asrinivasan, sumukh.phalgaonkar, skumar, xCluster, hsunder, #db-approvers
    
    Reviewed By: sumukh.phalgaonkar, skumar, #db-approvers
    
    Subscribers: ybase, ycdcxcluster
    
    Tags: #jenkins-ready
    
    Differential Revision: https://phorge.dev.yugabyte.com/D51747

--- src/yb/master/xrepl_catalog_manager.cc
+++ src/yb/master/xrepl_catalog_manager.cc
@@ -1540,7 +1540,8 @@ Status CatalogManager::PopulateCDCStateTableOnNewTableCreation(
     SharedLock lock(mutex_);
     for (const auto& entry : cdc_stream_map_) {
       const auto& stream_info = entry.second;
-      if (stream_info->IsCDCSDKStream() && stream_info->namespace_id() == namespace_id) {
+      if (stream_info->IsCDCSDKStream() && stream_info->namespace_id() == namespace_id &&
+          IsTableEligibleForCDCSDKStream(table, table->LockForRead(), /*check_schema=*/true)) {
         streams.emplace_back(stream_info);
       }
     }
