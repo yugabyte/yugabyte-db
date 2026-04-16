77d20c5d70140a2899a5fc72b271f07cbf18e31a
Basava <basava726.k@gmail.com>
2026-04-16 03:25:26 +0000

    [BACKPORT 2025.2][#31099] DocDB: Fix regressed test PgCatalogVersionConnManagerTest.TestConnectionManagerBoundedStaleness
    Summary:
    Test `PgCatalogVersionConnManagerTest.TestConnectionManagerBoundedStaleness` regressed in release mode (as object locking is enabled in release) as part of https://phorge.dev.yugabyte.com/D51035, since catalog cache is force refreshed in-line on DDL commit despite `TEST_tserver_disable_catalog_refresh_on_heartbeat` being set. This revision fixes the test by changing the test logic to expect the auth with old password to fail when object locking is enabled, that is - connection is rejected with stale password.
    
    Original commit: 9e462b6b6d646f419fc36721d977ee1f444eceae / D52057
    Original commit: 0b7859431ede2ab2291c988a0e782d468f92a192 / D52106
    
    Test Plan:
    Jenkins
    
    ./yb_duild.sh release --cxx-test pgwrapper_pg_catalog_version-test --gtest_filter 'PgCatalogVersionConnManagerTest.TestConnectionManagerBoundedStaleness/1'
    
    Reviewers: myang, sergei
    
    Reviewed By: myang
    
    Subscribers: yql, ybase
    
    Differential Revision: https://phorge.dev.yugabyte.com/D52065

--- src/yb/yql/pgwrapper/pg_catalog_version-test.cc
+++ src/yb/yql/pgwrapper/pg_catalog_version-test.cc
@@ -3544,9 +3544,17 @@ TEST_P(PgCatalogVersionConnManagerTest,
   pg_ts = cluster_->tablet_server(0);
   if (enable_ysql_conn_mgr) {
     setenv("PGPASSWORD", "old_password", /*overwrite=*/true);
-    // Verify the old password still works because we have set the gflag
-    // --TEST_tserver_disable_catalog_refresh_on_heartbeat=true.
-    ASSERT_RESULT(ConnectToDBAsUser("yugabyte", "test_user"));
+    if (IsObjectLockingEnabled()) {
+      // With object locking enabled, the password authentication should fail as
+      // the DDL commit codepath would force a catalog refresh on all tservers
+      // despite TEST_tserver_disable_catalog_refresh_on_heartbeat being set.
+      ASSERT_NOK_STR_CONTAINS(ConnectToDBAsUser("yugabyte", "test_user"),
+          "password authentication failed for user \"test_user\"");
+    } else {
+      // Verify the old password still works because we have set the gflag
+      // --TEST_tserver_disable_catalog_refresh_on_heartbeat=true.
+      ASSERT_RESULT(ConnectToDBAsUser("yugabyte", "test_user"));
+    }
 
     // Wait for the stale cache in tserver expires.
     SleepFor(1ms * stale_cache_bound_ms);
