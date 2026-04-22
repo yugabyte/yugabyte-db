784d496c357c42d5f764a113cb23695fce52bb8c
Jason Kim <git@jasonk.me>
2026-04-22 16:35:47 -0700

    [#31266] YSQL: parameterize regress test yb.orig.dml_derived_equalities
    Summary:
    Use parameterized_query regress test framework to deduplicate queries in
    the yb.orig.dml_derived_equalities regress test.  Notable call-outs:
    
    - There is a discrepancy with a comment saying "Baseline BNL" and the
      explain showing "Nested Loop" rather than "YB Batched Nested Loop".
      Fix hints to properly use NL/BNL for these queries.
    - Query under comment "Full join with WHERE on both sides" was only
      EXPLAINed and not run.  This sticks out as different from the queries
      around it, which do both EXPLAIN and run.  Conform this query to do
      both EXPLAIN and run.
    - Remove useless join_collapse_limit GUC setting: it doesn't impact the
      output.
    - Some queries use "ORDER BY" but omit that in the EXPLAIN version of
      the same query.  Ensure both have "ORDER BY".
    - Some EXPLAIN queries use a pg_hint_plan hint, but the non-EXPLAIN
      version of the same query doesn't.  Ensure both have the hint.
    - Many queries put the pg_hint_plan hint after "SELECT" and before "*".
      Change the position of the hint to before "SELECT".
    
    Test Plan:
    On Almalinux 8:
    
        ./yb_build.sh fastdebug --gcc13 daemons initdb \
          --no-ybc --skip-pg-parquet --no-odyssey \
          --java-test TestPgRegressDml --tp 1 -n 20
    
    Close: #31266
    Jenkins: test regex: TestPgRegressDml
    
    Reviewers: fizaa
    
    Reviewed By: fizaa
    
    Subscribers: yql
    
    Differential Revision: https://phorge.dev.yugabyte.com/D52345

--- src/postgres/src/test/regress/sql/yb.orig.dml_derived_equalities.sql
+++ src/postgres/src/test/regress/sql/yb.orig.dml_derived_equalities.sql
@@ -1,3 +1,10 @@
+\getenv abs_srcdir PG_ABS_SRCDIR
+\set filename :abs_srcdir '/yb_commands/parameterized_query.sql'
+\i :filename
+\set P1 ':explain'
+\set P2
+\set explain 'EXPLAIN (COSTS OFF)'
+
 -- Enable CBO
 SET yb_enable_optimizer_statistics = true;
 SET yb_enable_base_scans_cost_model = true;
@@ -23,21 +30,27 @@ INSERT INTO orders VALUES
 ANALYZE orders;
 
 -- Multi-column bucket (bucket depends on user_id AND order_id)
-EXPLAIN (COSTS OFF) SELECT * FROM orders WHERE user_id = 100 AND order_id = 1;
-SELECT * FROM orders WHERE user_id = 100 AND order_id = 1;
+\set query ':P SELECT * FROM orders WHERE user_id = 100 AND order_id = 1;'
+\i :iter_P2
+
+\set hint '/*+ IndexScan(orders orders_created_idx) */'
 
 -- Expression index
-EXPLAIN (COSTS OFF) SELECT /*+ IndexScan(orders orders_created_idx) */ * FROM orders WHERE created_at = '2024-01-02'::timestamptz;
-SELECT * FROM orders WHERE created_at = '2024-01-02'::timestamptz;
+SELECT $$
+:P :hint SELECT * FROM orders WHERE created_at = '2024-01-02'::timestamptz;
+$$ AS query \gset
+\i :iter_P2
 
 -- Stable expression
-EXPLAIN (COSTS OFF) SELECT /*+ IndexScan(orders orders_created_idx) */ * FROM orders WHERE created_at = date_trunc('day', '2024-01-02 15:30:12.789'::timestamptz);
-SELECT * FROM orders WHERE created_at = date_trunc('day', '2024-01-02 15:30:12.789'::timestamptz);
+SELECT $$
+:P :hint SELECT * FROM orders WHERE created_at = date_trunc('day', '2024-01-02 15:30:12.789'::timestamptz);
+$$ AS query \gset
+\i :iter_P2
 
 -- Parameterized query
 PREPARE order_query AS SELECT * FROM orders WHERE user_id = $1 AND order_id = $2;
-EXPLAIN (COSTS OFF) EXECUTE order_query(100, 2);
-EXECUTE order_query(100, 2);
+\set query ':P EXECUTE order_query(100, 2);'
+\i :iter_P2
 
 DROP TABLE orders;
 
@@ -54,10 +67,7 @@ CREATE TABLE orders_no_bucket (
     order_id int,
     user_id int
 );
-
 CREATE INDEX orders_no_bucket_user_id_idx ON orders_no_bucket(user_id ASC);
-
-INSERT INTO orders_no_bucket VALUES (1, 50), (2, 50), (3, 60), (4, 70);
 
 -- Table 2: With generated column in PRIMARY KEY
 CREATE TABLE orders_gen (
@@ -67,84 +77,73 @@ CREATE TABLE orders_gen (
     PRIMARY KEY (bucket_id ASC, user_id ASC, order_id ASC)
 );
 
-INSERT INTO orders_gen VALUES (1, 50), (2, 50), (3, 60), (4, 70);
-
 -- Table 3: With expression index
 CREATE TABLE orders_expr (
     order_id int,
     user_id int
 );
-
 CREATE INDEX orders_expr_idx ON orders_expr((yb_hash_code(user_id) % 3) ASC, user_id ASC);
 
-INSERT INTO orders_expr VALUES (1, 50), (2, 50), (3, 60), (4, 70);
+\set R1 'orders_no_bucket'
+\set R2 'orders_gen'
+\set R3 'orders_expr'
+\set query 'INSERT INTO :R VALUES (1, 50), (2, 50), (3, 60), (4, 70);'
+\i :iter_R3
 
 ANALYZE users, orders_no_bucket, orders_gen, orders_expr;
 
-SET yb_prefer_bnl = false;
-SET yb_enable_batchednl = false;
-
--- Baseline NL
-EXPLAIN (COSTS OFF) SELECT /*+ NestLoop(u o) IndexScan(o orders_no_bucket_user_id_idx) */ * FROM users u JOIN orders_no_bucket o ON o.user_id = u.id;
-
--- NL with generated column (should show bucket condition)
-EXPLAIN (COSTS OFF) SELECT /*+ NestLoop(u o) IndexScan(o) */ * FROM users u JOIN orders_gen o ON o.user_id = u.id;
-
--- NL with expression index (should show bucket condition)
-EXPLAIN (COSTS OFF) SELECT /*+ NestLoop(u o) IndexScan(o orders_expr_idx) */ * FROM users u JOIN orders_expr o ON o.user_id = u.id;
-
-SET yb_prefer_bnl = true;
-SET yb_enable_batchednl = true;
-
--- Baseline BNL
-EXPLAIN (COSTS OFF) SELECT /*+ NestLoop(u o) IndexScan(o orders_no_bucket_user_id_idx) */ * FROM users u JOIN orders_no_bucket o ON o.user_id = u.id;
-
--- BNL with generated column (should show bucket condition)
-EXPLAIN (COSTS OFF) SELECT /*+ NestLoop(u o) IndexScan(o) */ * FROM users u JOIN orders_gen o ON o.user_id = u.id;
+\set Q1 '/*+ Set(yb_prefer_bnl off) NestLoop(u o) IndexScan(o) */'
+\set Q2 '/*+ YbBatchedNL(u o) IndexScan(o) */'
+\set Qnext :iter_R3
+\set query ':explain :Q SELECT * FROM users u JOIN :R o ON o.user_id = u.id;'
 
 -- TODO: BNL is not supported when the index clause is on an expression column
--- This will pick NL
-EXPLAIN (COSTS OFF) SELECT /*+ NestLoop(u o) IndexScan(o orders_expr_idx) */ * FROM users u JOIN orders_expr o ON o.user_id = u.id;
+\i :iter_Q2
+
+\set hint '/*+ NestLoop(u o) IndexScan(o) */'
+\set R1 'u.id = 50'
+\set R2 'u.id IN (50, 60, 70)'
 
 -- With WHERE clause filter
-EXPLAIN (COSTS OFF) SELECT /*+ NestLoop(u o) IndexScan(o) */ * FROM users u JOIN orders_gen o ON o.user_id = u.id WHERE u.id = 50;
-EXPLAIN (COSTS OFF) SELECT /*+ NestLoop(u o) IndexScan(o) */ * FROM users u JOIN orders_gen o ON o.user_id = u.id WHERE u.id IN (50, 60, 70);
+\set query ':explain :hint SELECT * FROM users u JOIN orders_gen o ON o.user_id = u.id WHERE :R;'
+\i :iter_R2
 
 -- Condition in ON clause
-EXPLAIN (COSTS OFF) SELECT /*+ NestLoop(u o) IndexScan(o) */ * FROM users u JOIN orders_gen o ON o.user_id = u.id AND u.id = 50;
-EXPLAIN (COSTS OFF) SELECT /*+ NestLoop(u o) IndexScan(o) */ * FROM users u JOIN orders_gen o ON o.user_id = u.id AND u.id IN (50, 60, 70);
+\set query ':explain :hint SELECT * FROM users u JOIN orders_gen o ON o.user_id = u.id AND :R;'
+\i :iter_R2
 
 -- Equality conditions in outer joins
 
 -- Left join with WHERE on non-nullable side
-EXPLAIN (COSTS OFF) SELECT /*+ NestLoop(u o) IndexScan(o) */ * FROM users u LEFT JOIN orders_gen o ON o.user_id = u.id WHERE u.id = 80;
-SELECT * FROM users u LEFT JOIN orders_gen o ON o.user_id = u.id WHERE u.id = 80 ORDER BY u.id, o.order_id;
+\set query ':P :hint SELECT * FROM users u LEFT JOIN orders_gen o ON o.user_id = u.id WHERE u.id = 80 ORDER BY u.id, o.order_id;'
+\i :iter_P2
 
 -- Left join with WHERE on nullable side
-EXPLAIN (COSTS OFF) SELECT /*+ NestLoop(u o) IndexScan(o) */ * FROM users u LEFT JOIN orders_gen o ON o.user_id = u.id WHERE o.user_id = 50;
-SELECT * FROM users u LEFT JOIN orders_gen o ON o.user_id = u.id WHERE o.user_id = 50 ORDER BY u.id, o.order_id;
+\set query ':P :hint SELECT * FROM users u LEFT JOIN orders_gen o ON o.user_id = u.id WHERE o.user_id = 50 ORDER BY u.id, o.order_id;'
+\i :iter_P2
 
 -- Left join with WHERE in ON clause
-EXPLAIN (COSTS OFF) SELECT /*+ NestLoop(u o) IndexScan(o) */ * FROM users u LEFT JOIN orders_gen o ON o.user_id = u.id AND o.user_id = 50;
-SELECT * FROM users u LEFT JOIN orders_gen o ON o.user_id = u.id AND o.user_id = 50 ORDER BY u.id, o.order_id;
+\set query ':P :hint SELECT * FROM users u LEFT JOIN orders_gen o ON o.user_id = u.id AND o.user_id = 50 ORDER BY u.id, o.order_id;'
+\i :iter_P2
 
 -- Left join with expression index
-EXPLAIN (COSTS OFF) SELECT /*+ NestLoop(u o) IndexScan(o orders_expr_idx) */ * FROM users u LEFT JOIN orders_expr o ON o.user_id = u.id WHERE u.id = 50;
-SELECT * FROM users u LEFT JOIN orders_expr o ON o.user_id = u.id WHERE u.id = 50 ORDER BY u.id, o.order_id;
+\set query ':P :hint SELECT * FROM users u LEFT JOIN orders_expr o ON o.user_id = u.id WHERE u.id = 50 ORDER BY u.id, o.order_id;'
+\i :iter_P2
 
 -- Full join with WHERE on either side
-EXPLAIN (COSTS OFF) SELECT /*+ NestLoop(u o) IndexScan(o) */ * FROM users u FULL JOIN orders_gen o ON o.user_id = u.id WHERE u.id = 50;
-SELECT * FROM users u FULL JOIN orders_gen o ON o.user_id = u.id WHERE u.id = 50 ORDER BY u.id, o.order_id;
+\set query ':P :hint SELECT * FROM users u FULL JOIN orders_gen o ON o.user_id = u.id WHERE u.id = 50 ORDER BY u.id, o.order_id;'
+\i :iter_P2
 
-EXPLAIN (COSTS OFF) SELECT /*+ NestLoop(u o) IndexScan(o) */ * FROM users u FULL JOIN orders_gen o ON o.user_id = u.id WHERE o.user_id = 60;
-SELECT * FROM users u FULL JOIN orders_gen o ON o.user_id = u.id WHERE o.user_id = 60 ORDER BY u.id, o.order_id;
+\set query ':P :hint SELECT * FROM users u FULL JOIN orders_gen o ON o.user_id = u.id WHERE o.user_id = 60 ORDER BY u.id, o.order_id;'
+\i :iter_P2
 
 -- Full join with WHERE on both sides
-EXPLAIN (COSTS OFF) SELECT /*+ NestLoop(u o) IndexScan(o) */ * FROM users u FULL JOIN orders_gen o ON o.user_id = u.id WHERE u.id = 50 AND o.user_id = 50;
+\set query ':P :hint SELECT * FROM users u FULL JOIN orders_gen o ON o.user_id = u.id WHERE u.id = 50 AND o.user_id = 50 ORDER BY u.id, o.order_id;'
+\i :iter_P2
 
 -- Full join with expression index
-EXPLAIN (COSTS OFF) SELECT /*+ NestLoop(u o) IndexScan(o orders_expr_idx) */ * FROM users u FULL JOIN orders_expr o ON o.user_id = u.id WHERE u.id = 50;
-SELECT * FROM users u FULL JOIN orders_expr o ON o.user_id = u.id WHERE u.id = 50 ORDER BY u.id, o.order_id;
+\set query ':P :hint SELECT * FROM users u FULL JOIN orders_expr o ON o.user_id = u.id WHERE u.id = 50 ORDER BY u.id, o.order_id;'
+\i :iter_P2
 
 -- Multi-way joins
 CREATE TABLE t1 (x int);
@@ -152,18 +151,18 @@ CREATE TABLE t2 (x int, bucket_id int GENERATED ALWAYS AS (yb_hash_code(x) % 5) 
                  PRIMARY KEY (bucket_id, x));
 CREATE TABLE t3 (x int);
 
-INSERT INTO t1 SELECT generate_series(1, 1000);
-INSERT INTO t2 SELECT generate_series(1, 1000);
-INSERT INTO t3 SELECT generate_series(1, 1000);
+\set R1 't1'
+\set R2 't2'
+\set R3 't3'
+\set query 'INSERT INTO :R SELECT generate_series(1, 1000);'
+\i :iter_R3
 
 ANALYZE t1, t2, t3;
 SET enable_hashjoin = false;
 SET enable_mergejoin = false;
 
-EXPLAIN (COSTS OFF) SELECT * FROM t1 JOIN t2 ON t2.x = t1.x JOIN t3 ON t3.x = t1.x;
-SET join_collapse_limit = 1;
-EXPLAIN (COSTS OFF) SELECT * FROM t3 JOIN t2 ON t2.x = t3.x JOIN t1 ON t1.x = t3.x;
-RESET join_collapse_limit;
+:explain SELECT * FROM t1 JOIN t2 ON t2.x = t1.x JOIN t3 ON t3.x = t1.x;
+:explain SELECT * FROM t3 JOIN t2 ON t2.x = t3.x JOIN t1 ON t1.x = t3.x;
 
 -- Cleanup
 DROP TABLE users;
