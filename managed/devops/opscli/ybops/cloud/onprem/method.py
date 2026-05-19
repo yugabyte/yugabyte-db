d0302aa7872e613f513a195799b642c163ebf570
Naorem Khogendro Singh <nsingh@yugabyte.com>
2026-05-19 11:32:57 -0700

    [BACKPORT 2026.1][PLAT-20971] Ansible 9.x -> 10.x upgrade in 2026.1 breaks onprem nodes with Python 3.6
    Summary:
    This is a fix directly in 2026.1 as master does not have this code.
    
    Remove this unnecessary ansible execution to unblock 2026.1
    
    Test Plan: Failed itests must pass.
    
    Reviewers: anijhawan, skhilar, ayush.kushwaha, nbhatia
    
    Reviewed By: ayush.kushwaha
    
    Subscribers: nikhil, yugaware
    
    Differential Revision: https://phorge.dev.yugabyte.com/D53402

--- managed/devops/opscli/ybops/cloud/onprem/method.py
+++ managed/devops/opscli/ybops/cloud/onprem/method.py
@@ -304,12 +304,6 @@ class OnPremPrecheckInstanceMethod(AbstractInstancesMethod):
             scp_result = copy_to_tmp(self.extra_vars, get_datafile_path('preflight_checks.sh'),
                                      remote_tmp_dir=args.remote_tmp_dir)
             results["SSH Connection"] = scp_result == 0
-            sudo_pass_file = '{}/.yb_sudo_pass.sh'.format(args.remote_tmp_dir)
-            self.extra_vars['sudo_pass_file'] = sudo_pass_file
-            ansible_status = self.cloud.setup_ansible(args).run("send_sudo_pass.yml",
-                                                                self.extra_vars, host_info,
-                                                                print_output=False)
-            results["Try Ansible Command"] = ansible_status == 0
             ports_to_check = ",".join([str(p) for p in [args.master_http_port,
                                                         args.master_rpc_port,
                                                         args.tserver_http_port,
@@ -325,10 +319,10 @@ class OnPremPrecheckInstanceMethod(AbstractInstancesMethod):
                                                         args.node_exporter_http_port]
                                       if p is not None])
             cmd = "{}/preflight_checks.sh --type {} --yb_home_dir {} --mount_points {} " \
-                "--ports_to_check {} --sudo_pass_file {} --tmp_dir {} --cleanup".format(
+                "--ports_to_check {} --tmp_dir {} --cleanup".format(
                     args.remote_tmp_dir, args.precheck_type, args.yb_home_dir,
                     self.cloud.get_mount_points_csv(args),
-                    ports_to_check, sudo_pass_file, args.remote_tmp_dir)
+                    ports_to_check, args.remote_tmp_dir)
             if args.install_node_exporter:
                 cmd += " --install_node_exporter"
             if args.air_gap:
