9e4dbe4dc95735588de19149ba114aaaadd57860
Zachary Drudi <zdrudi@yugabyte.com>
2026-05-19 18:39:13 -0400

    [BACKPORT 2025.2][#31646] docdb: Skip clone test under tsan as per other clone unit tests
    Summary:
    Cloning takes several minutes under TSAN. Most clone tests are disabled under TSAN. This diff also disables the test YbAdminSnapshotScheduleTest.ColocatedCloneWithMultipleTablespaces on TSAN.
    
    Original commit: 000c67a36f25f42241f2331253ac0a0a4cedbbd6 / D53176
    
    Test Plan:
    ```
    ./yb_build.sh asan daemons initdb && ./yb_build.sh asan --cxx-test yb-admin-snapshot-schedule-test --cxx-test-filter-re yb-admin-snapshot-schedule-test --gtest_filter 'YbAdminSnapshotScheduleTest.ColocatedCloneWithMultipleTablespaces' -n 20 --stop-at-failure
    ./yb_build.sh tsan daemons initdb && ./yb_build.sh tsan --cxx-test yb-admin-snapshot-schedule-test --cxx-test-filter-re yb-admin-snapshot-schedule-test --gtest_filter 'YbAdminSnapshotScheduleTest.ColocatedCloneWithMultipleTablespaces'
    ```
    
    Reviewers: mhaddad, mlillibridge
    
    Reviewed By: mhaddad
    
    Subscribers: ybase, jason
    
    Differential Revision: https://phorge.dev.yugabyte.com/D53423

--- src/yb/tools/yb-admin-snapshot-schedule-test.cc
+++ src/yb/tools/yb-admin-snapshot-schedule-test.cc
@@ -6285,6 +6285,11 @@ TEST_F_EX(
 // single default colocated parent.
 class YbAdminCloneColocationTablespaceTest : public YbAdminSnapshotScheduleTestWithLBYsql {
  public:
+  void SetUp() override {
+    YB_SKIP_TEST_IN_TSAN();
+    YbAdminSnapshotScheduleTestWithLBYsql::SetUp();
+  }
+
   void UpdateMiniClusterOptions(ExternalMiniClusterOptions* opts) override {
     YbAdminSnapshotScheduleTestWithLBYsql::UpdateMiniClusterOptions(opts);
     opts->extra_master_flags.emplace_back("--ysql_enable_colocated_tables_with_tablespaces=true");
