e8317f4531f083dbdd35c494c792e2417e7d96e0
Aman <anijhawan@yugabyte.com.>
2026-05-19 11:15:18 -0700

    [PLAT-20973] Fix Incorrect CQL API validation for multi tenancy  enablement
    Summary: [PLAT-20973] Fix Incorrect CQL API validation for MT enablement
    
    Test Plan:
    unit test
    phabricator
    
    Reviewers: #yba-api-review!, nsingh
    
    Reviewed By: nsingh
    
    Subscribers: nsingh, yugaware
    
    Differential Revision: https://phorge.dev.yugabyte.com/D53404

--- managed/src/main/java/com/yugabyte/yw/controllers/handlers/UniverseCRUDHandler.java
+++ managed/src/main/java/com/yugabyte/yw/controllers/handlers/UniverseCRUDHandler.java
@@ -1046,14 +1046,8 @@ public class UniverseCRUDHandler {
             confGetter
                 .getCustomerConf(customer)
                 .getBoolean(UniverseConfKeys.multitenancySkipYcqlPrecheck.getKey());
-        if (skipYcqlPrecheck && !userIntent.enableYSQL) {
-          throw new PlatformServiceException(
-              BAD_REQUEST, "YSQL API should be enabled to enable multi-tenancy");
-        } else if (!(userIntent.enableYSQL && !userIntent.enableYCQL)) {
-          throw new PlatformServiceException(
-              BAD_REQUEST,
-              "YSQL API should be enabled and YCQL api disabled to enable multi-tenancy");
-        }
+        validateMultiTenancyApiConfig(
+            userIntent.enableYSQL, userIntent.enableYCQL, skipYcqlPrecheck);
         for (Cluster cluster : taskParams.clusters) {
           if (cluster.userIntent.providerType == Common.CloudType.kubernetes) {
             throw new PlatformServiceException(
@@ -1920,6 +1914,18 @@ public class UniverseCRUDHandler {
         "Node to node encrypt setting");
     checkEquals(
         c -> c.userIntent.assignPublicIP, primaryCluster, cluster, "Assign public IP setting");
+  }
+
+  static void validateMultiTenancyApiConfig(
+      boolean enableYSQL, boolean enableYCQL, boolean skipYcqlPrecheck) {
+    if (!enableYSQL) {
+      throw new PlatformServiceException(
+          BAD_REQUEST, "YSQL API should be enabled to enable multi-tenancy");
+    }
+    if (!skipYcqlPrecheck && enableYCQL) {
+      throw new PlatformServiceException(
+          BAD_REQUEST, "YCQL API should be disabled to enable multi-tenancy");
+    }
   }
 
   private static void checkEquals(
