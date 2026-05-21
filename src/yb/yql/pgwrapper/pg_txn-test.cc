4c84ea6a02cbd2ae94bb6a717715596ff41faec1
Sergei Politov <spolitov@gmail.com>
2026-05-21 14:23:51 +0300

    [BACKPORT 2025.1][#28504] DocDB: Fix PgTxnTest.FlushLargeTransaction in ASAN
    Summary:
    Disable load balancing in PgTxnTest.FlushLargeTransaction to prevent
    interference with the file-count and compaction expectations the test
    relies on. Without this, the test could flake in ASAN with mismatched
    SST file name_ids.
    
    Original commit: e35fa7690410cab344fcc538cf56b8eb5c7f8285 / D53266
    
    Test Plan: ./yb_build.sh asan --gtest_filter PgTxnTest.FlushLargeTransaction -n 100 -- -p 20
    
    Reviewers: bkolagani
    
    Reviewed By: bkolagani
    
    Subscribers: yql, ybase
    
    Tags: #jenkins-ready
    
    Differential Revision: https://phorge.dev.yugabyte.com/D53517

--- src/yb/yql/pgwrapper/pg_txn-test.cc
+++ src/yb/yql/pgwrapper/pg_txn-test.cc
@@ -35,6 +35,7 @@ using namespace std::literals;
 
 DECLARE_bool(TEST_fail_in_apply_if_no_metadata);
 DECLARE_bool(TEST_running_test);
+DECLARE_bool(enable_load_balancing);
 DECLARE_bool(enable_wait_queues);
 DECLARE_bool(ysql_enable_packed_row);
 DECLARE_bool(yb_enable_read_committed_isolation);
@@ -619,6 +620,7 @@ TEST_F(PgTxnTest, FlushLargeTransaction) {
   ANNOTATE_UNPROTECTED_WRITE(FLAGS_rocksdb_universal_compaction_always_include_size_threshold) = 0;
   ANNOTATE_UNPROTECTED_WRITE(FLAGS_timestamp_history_retention_interval_sec) = 0;
   ANNOTATE_UNPROTECTED_WRITE(FLAGS_rocksdb_universal_compaction_min_merge_width) = 2;
+  ANNOTATE_UNPROTECTED_WRITE(FLAGS_enable_load_balancing) = false;
 
   constexpr auto kTxnRows = 15;
   constexpr auto kValueLen = 16_KB;
