b0bfc625344d955d0e62d447fdc92420d2d027b1
Basava <basava726.k@gmail.com>
2026-06-03 17:04:10 +0000

    [BACKPORT 2026.1][#31594] DocDB: Table Locks: Fix false deadlock caused by re-use of session level txn.
    Summary:
    In one of the itests, we saw create index statements running into false deadlocks. It is due to reuse of the session level transaction. The flow is something as below
    ```
    s1                                                                                  s2
    $create index idx1 on test(v1);
     - phase1, acquires session object lock on relation test in the end
       associated with session_txn1
     - phase2                                                                           $analyze test;
                                                                                         - waits on session_txn1
     - phase3, releases session object lock on relation test
                                                                                         - unblocks ... analyze in progress
    $create index idx1 on test(v1);
     - ddl txn tries acquiring conflicting object lock on relation test
       leads to ddl_txn2 -> analyze_txn
    ```
    
    The above leads to a deadlock. Since the session level txn has a inherent dependency onto the host txn, we end up with a false cycle
    ```
    session_txn1 -> ddl_txn2 -> analyze_txn -> session_txn1
    ```
    This is due to the earlier edge `analyze_txn -> session_txn1` not being pruned. The wait-for edges in YB are pruned either when the corresponding subtxn rollsback or the txn itself isn't active anymore.
    
    This revision fixes the above false deadlock issue by associating the session object locks from different transactions to different subtxns. In specific,
    - when acquiring a session level object lock, if one doesn't already exist, bump up the active subtxn and acquire the session object locks against it (stored in `subtxn_with_session_object_locks_`).
    - when acquiring a session level object lock, if an active subtxn with session locks already exists, acquire them against the same subtxn
    - on release all session locks, rollback to `subtxn_with_session_object_locks_` and bump the active subtxn id
    
    The same session level txn is also used for acquiring session advisory locks, and they need to honored despite the above logic of rolling back specific subtxns. The above logic takes care of this since we increment active subtxn when acquiring a session object lock for the first time, ensuring that all active advisory locks associated with the current subtxn (before the increment) would still remain active. Additionally an `SCHECK` is introduced on the acquire session advisory lock path which ensures that we don't serve any session advisory lock requests when the session level txn is holding active session object locks. This is necessary as release all session object locks rollsback the subtxn. This SCHECK itself is expected to never fail since session advisory locks are user level and cannot be issue in the middle of execution of a DDL (like CREATE INDEX) which use session object locks.
    
    **Upgrade/Downgrade safety**
    Added new field to the proto message which is used for ysql <-> local tserver communication alone. No upgrade/downgrade impact. Additionally, the usage of the feature (table locking) is disabled by default in all existing major releases.
    
    Original commit: f37604b92442062f18f1f39e1525e86a67925dc5 / D53287
    
    Test Plan:
    Jenkins
    
    ./yb_build.sh --cxx-test pg_object_locks-test --gtest_filter PgObjectLocksTest.ConsecutiveCreateIndexDontDeadlock
    
    Reviewers: amitanand
    
    Reviewed By: amitanand
    
    Subscribers: ybase, yql
    
    Differential Revision: https://phorge.dev.yugabyte.com/D53949

--- src/yb/client/transaction.h
+++ src/yb/client/transaction.h
@@ -187,6 +187,10 @@ class YBTransaction : public std::enable_shared_from_this<YBTransaction> {
 
   std::optional<SubTransactionMetadataPB> GetSubTransactionMetadataPB() const;
 
+  SubTransactionId IncrementAndGetSubTransactionId();
+
+  SubTransactionId GetActiveSubTransactionId() const;
+
   Status SetPgTxnStart(int64_t pg_txn_start_us, bool using_table_locks);
 
   Status RollbackToSubTransaction(SubTransactionId id, CoarseTimePoint deadline);
