7f6373ae281a3c616dcc6574237f9cb6260f7a63
Yury Shchetinin <yshchetinin@yugabyte.com>
2026-06-23 13:10:00 +0300

    [PLAT-21355] Fix replica distribution logic
    Summary:
    Previous replica distribution logic check was not taking in account number of nodes in AZ, which could lead to incorrect replica distribution
    (since DB cannot track the number of nodes in AZ in cluster configuration). We should not have number of replicas in zone greater than the number of nodes in zone.
    
    Also added replicas check to chaos test.
    
    Test Plan: sbt test
    
    Reviewers: nsingh, anijhawan
    
    Reviewed By: anijhawan
    
    Subscribers: yugaware
    
    Differential Revision: https://phorge.dev.yugabyte.com/D54851

--- managed/src/main/java/com/yugabyte/yw/common/PlacementInfoUtil.java
+++ managed/src/main/java/com/yugabyte/yw/common/PlacementInfoUtil.java
@@ -58,6 +58,7 @@ import java.util.Set;
 import java.util.SortedSet;
 import java.util.TreeSet;
 import java.util.UUID;
+import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.function.Consumer;
 import java.util.function.Function;
@@ -628,8 +629,12 @@ public class PlacementInfoUtil {
     applyDedicatedModeChanges(universe, cluster, taskParams);
 
     LOG.info("Set of nodes after node configure: {}.", taskParams.nodeDetailsSet);
-    checkAndSetPerAZRF(
-        cluster.placementInfo, cluster.userIntent.replicationFactor, defaultRegionUUID, false);
+    if (clusterOpType == ClusterOperationType.CREATE) {
+      setPerAZRF(cluster.placementInfo, cluster.userIntent.replicationFactor, defaultRegionUUID);
+    } else {
+      checkAndSetPerAZRF(
+          cluster.placementInfo, cluster.userIntent.replicationFactor, defaultRegionUUID, false);
+    }
     LOG.info("Final Placement info: {}.", cluster.placementInfo);
 
     finalSanityCheckConfigure(cluster, taskParams.getNodesInCluster(cluster.uuid));
@@ -1128,6 +1133,7 @@ public class PlacementInfoUtil {
     Set<String> zeroZones = new HashSet<>();
     Set<String> incorrectlyPlacedReplicas = new HashSet<>();
     AtomicInteger totalReplicas = new AtomicInteger();
+    AtomicBoolean result = new AtomicBoolean(true);
     placementInfo
         .azInfoStream()
         .forEach(
@@ -1135,11 +1141,27 @@ public class PlacementInfoUtil {
               PlacementAZ az = azInfo.placementAZ;
               zoneCount.incrementAndGet();
               if (az.replicationFactor < 0) {
+                String message = "Cannot have negative number of replicas: " + az.replicationFactor;
+                LOG.error(message);
                 if (throwInIncorrect) {
-                  throw new IllegalArgumentException(
-                      "Cannot have negative number of replicas: " + az.replicationFactor);
+                  throw new IllegalArgumentException(message);
                 } else {
-                  az.replicationFactor = 0;
+                  result.set(false);
+                }
+              }
+              if (az.replicationFactor > az.numNodesInAZ) {
+                String message =
+                    "Cannot have number of replicas "
+                        + az.replicationFactor
+                        + " greater than the number of nodes "
+                        + az.numNodesInAZ
+                        + " in "
+                        + az.name;
+                LOG.error(message);
+                if (throwInIncorrect) {
+                  throw new IllegalArgumentException(message);
+                } else {
+                  result.set(false);
                 }
               }
               if (az.replicationFactor == 0) {
@@ -1150,13 +1172,16 @@ public class PlacementInfoUtil {
               }
               totalReplicas.addAndGet(az.replicationFactor);
             });
+    if (!result.get()) {
+      return false;
+    }
     // Allowing replicas only in default region (if exist)
     if (!incorrectlyPlacedReplicas.isEmpty()) {
       String message =
           "Incorrectly placed replicas in zones: "
               + incorrectlyPlacedReplicas
               + ", should be in default region only";
-      LOG.debug(message);
+      LOG.error(message);
       if (throwInIncorrect) {
         throw new IllegalStateException(message);
       }
@@ -1168,7 +1193,7 @@ public class PlacementInfoUtil {
     }
     if (!zeroZones.isEmpty() && zoneCount.get() <= rf) {
       String message = "Some zones " + zeroZones + " has zero replicas";
-      LOG.debug(message);
+      LOG.error(message);
       if (throwInIncorrect) {
         throw new IllegalStateException(message);
       }
@@ -1177,7 +1202,7 @@ public class PlacementInfoUtil {
     if (totalReplicas.get() != rf) {
       String message =
           "Illegal number of replicas: current " + totalReplicas.get() + " but should be " + rf;
-      LOG.debug(message);
+      LOG.error(message);
       if (throwInIncorrect) {
         throw new IllegalStateException(message);
       }
@@ -1984,7 +2009,6 @@ public class PlacementInfoUtil {
       LOG.error("{}. PlacementAZ={}, nodesAZ={}", msg, placementAZToNodeMap, nodesAZToNodeMap);
       throw new IllegalStateException(msg);
     }
-
     if (cluster.userIntent.providerType == CloudType.kubernetes) {
       return;
     }
@@ -3090,11 +3114,18 @@ public class PlacementInfoUtil {
    */
   public static UUID getDefaultRegion(UniverseDefinitionTaskParams taskParams) {
     for (Cluster cluster : taskParams.clusters) {
-      if (cluster.clusterType == ClusterType.PRIMARY
-          && cluster.placementInfo != null
-          && !CollectionUtils.isEmpty(cluster.placementInfo.cloudList)) {
-        return cluster.placementInfo.cloudList.get(0).defaultRegion;
+      if (cluster.clusterType == ClusterType.PRIMARY) {
+        return getDefaultRegion(cluster);
       }
+    }
+    return null;
+  }
+
+  public static UUID getDefaultRegion(Cluster cluster) {
+    if (cluster.clusterType == ClusterType.PRIMARY
+        && cluster.placementInfo != null
+        && !CollectionUtils.isEmpty(cluster.placementInfo.cloudList)) {
+      return cluster.placementInfo.cloudList.get(0).defaultRegion;
     }
     return null;
   }
