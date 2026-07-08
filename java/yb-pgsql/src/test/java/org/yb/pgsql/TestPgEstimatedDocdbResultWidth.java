711083a5f08cde0b219296bf6e862537af52d431
Gaurav Kukreja <gaurav@gauravk.in>
2026-07-08 14:55:30 +0200

    [BACKPORT 2024.2][#32517] YSQL: Fix incorrect ybctid estimation and out-of-bounds array access in cost model
    Summary:
    An incorrect array index access was found in YugabyteDB cost model for Index
    Scan, which can lead to incorrect estimation of ybctid width and/or out of
    bounds array access.
    
    This occurs during estimation of the width of ybctids and can lead to incorrect
    estimation of the ybctid width. Specifically, it occurs when a secondary index
    has an expression as a key. In this case, the cost model reads attributes
    to check if the column has variable width, in which case one byte needs to be
    added to the ybctid width to account for double null termination of variable
    length keys in the ybctid. However, the cost model incorrectly accesses the
    attributes of the next key.
    
    For each expression key in the index, this can lead to an error of 1 byte in
    ybctid width estimation. If the expression key is the last key in the index,
    this can lead to an out of bounds array access. This does not cause memory
    corruption because the out of bound memory is not being written to. However, it
    can potentially lead to segmentation faults if it tries to access unallocated
    memory.
    
    Original commit: cbee44ebff8ebf16937380b46917fbb9c44094b3 / D55337
    
    Test Plan: Jenkins
    
    Reviewers: fizaa
    
    Reviewed By: fizaa
    
    Subscribers: yql
    
    Differential Revision: https://phorge.dev.yugabyte.com/D55394

--- java/yb-pgsql/src/test/java/org/yb/pgsql/TestPgEstimatedDocdbResultWidth.java
+++ java/yb-pgsql/src/test/java/org/yb/pgsql/TestPgEstimatedDocdbResultWidth.java
@@ -606,13 +606,13 @@ public class TestPgEstimatedDocdbResultWidth extends BasePgSQLTest {
         "test_table", 33);
       testDocdbResultWidhEstimationHelper(stmt,
         "/*+ IndexOnlyScan(test_table test_index_1) */ SELECT 0 FROM test_table",
-        "test_table", 178);
+        "test_table", 177);
       testDocdbResultWidhEstimationHelper(stmt,
         "/*+ IndexOnlyScan(test_table test_index_2) */ SELECT 0 FROM test_table",
         "test_table", 113);
       testDocdbResultWidhEstimationHelper(stmt,
         "/*+ IndexOnlyScan(test_table test_index_3) */ SELECT 0 FROM test_table",
-        "test_table", 129);
+        "test_table", 130);
 
       stmt.execute("CREATE INDEX test_index_4 ON test_table " +
         "((sqrt(v1)::text || v3), (sqrt(v2)::text || v4))");
@@ -620,14 +620,14 @@ public class TestPgEstimatedDocdbResultWidth extends BasePgSQLTest {
       // Without running ANALYZE, the result width is incorrect for test_index_4
       testDocdbResultWidhEstimationHelper(stmt,
         "/*+ IndexOnlyScan(test_table test_index_4) */ SELECT 0 FROM test_table",
-        "test_table", 48);
+        "test_table", 49);
 
       stmt.execute("ANALYZE test_table");
 
       // After running ANALYZE, result width is updated for test_index_4
       testDocdbResultWidhEstimationHelper(stmt,
         "/*+ IndexOnlyScan(test_table test_index_4) */ SELECT 0 FROM test_table",
-        "test_table", 140);
+        "test_table", 141);
     }
   }
 
