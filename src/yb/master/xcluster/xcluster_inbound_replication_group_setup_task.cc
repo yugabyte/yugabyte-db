c49de526743ca2d5c30805ac06d4e213b2ee28dd
yusong-yan <yusongyan3@gmail.com>
2026-07-21 20:01:19 +0000

    [BACKPORT 2026.1][#32780] xCluster: Fix use after free of producer_map in xCluster setup task
    Summary:
    Original commit: 4804201f08942fd9ff113dee5f3733cfa3638b65 / D55939
    Issue:
    In `SetupReplicationGroup`, a reference to the cluster config `producer_map` is extracted from the write lock's dirty state. When the lock is committed, the dirty state is swapped out and freed, leaving the reference dangling. It is then accessed later in SyncConsumerReplicationStatusMap. The crash happens when a concurrent allocation overwrites that freed memory in the window between the commit and the access.
    
    Fix: Call `SyncConsumerReplicationStatusMap` before committing the cluster config lock, so the `producer_map` reference is still valid. Moving cluster config commit to later step won't cause any lock issue or ordering issue,
    
    Test Plan:
    Jenkins
    Also reran the failing QA test on this diff without errors
    
    Reviewers: jhe, xCluster, hsunder
    
    Reviewed By: jhe
    
    Subscribers: ybase
    
    Differential Revision: https://phorge.dev.yugabyte.com/D56366

--- src/yb/master/xcluster/xcluster_inbound_replication_group_setup_task.cc
+++ src/yb/master/xcluster/xcluster_inbound_replication_group_setup_task.cc
@@ -526,8 +526,6 @@ Status XClusterInboundReplicationGroupSetupTask::SetupReplicationGroup() {
   LOG_WITH_PREFIX(INFO) << "Replication Map: "
                         << cluster_config_producer_map.at(original_id.ToString()).DebugString();
 
-  cluster_config_l.Commit();
-
   if (universe_lock) {
     universe_lock->Commit();
   } else {
@@ -546,6 +544,8 @@ Status XClusterInboundReplicationGroupSetupTask::SetupReplicationGroup() {
   }
 
   xcluster_manager_.SyncConsumerReplicationStatusMap(original_id, cluster_config_producer_map);
+
+  cluster_config_l.Commit();
 
   xcluster_manager_.CreateXClusterSafeTimeTableAndStartService();
 
