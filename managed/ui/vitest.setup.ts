e73dba0d29ab541c7b316cfdcf69f36f05894443
Aleksandr Malyshev <amalyshev@yugabyte.com>
2026-07-23 23:50:12 +0300

    [BACKPORT 2026.1][PLAT-20544] Make sure PA embedded UI works for SSO users
    Summary:
    Currently, PA Collector UI, embedded in YBA, sends requests directly to PA API.
    This approach is using X-AUTH-TOKEN header to perform authentication and basic RBAC.
    PA UI sends the above token to PA API, and PA API calls YBA back with this token to check access to particular universe.
    Now, the issue is - we have SSO users, which don't have any auth token associated with them.
    And we need PA UI to work for such users as well.
    
    The options I considered were:
    1. Create auth token for SSO users as well and use that for authentication.
    2. Configure SSO for PA collector as well.
    3. Reverse proxy in YBA - so that all the PA API calls are going through YBA and YBA permorms both authn and authz.
    
    This diff implement option #3, as options #1 and #2 doe not cover authz properly. In our flows we don't create PA user for each YBA user (and we son't want that, untimately) - so only YBA can properly perform authz.)
    
    Original diff: https://phorge.dev.yugabyte.com/D55145
    
    Test Plan:
    Tested PA UI locally, made sure it works properly.
    COnfigured SSO. Made sure PA collector UI still works as expected.
    
    Reviewers: #yba-api-review!, aravind.ramarathinam
    
    Reviewed By: aravind.ramarathinam
    
    Subscribers: yugaware
    
    Differential Revision: https://phorge.dev.yugabyte.com/D55488

--- managed/ui/vitest.setup.ts
+++ managed/ui/vitest.setup.ts
@@ -5,7 +5,13 @@ import { server } from './src/mocks/server';
 
 // @yugabytedb/perf-advisor-ui depends on vis-timeline/standalone which has broken ESM
 // (directory imports without file extensions). Mock it globally to avoid the chain.
-vi.mock('@yugabytedb/perf-advisor-ui', () => ({}));
+// Stub any named exports that YBA modules invoke at import time (side-effect calls)
+// so the mock doesn't blow up before the test body runs. Passive named imports
+// (types, constants) can still be undefined and pass through vitest untouched.
+vi.mock('@yugabytedb/perf-advisor-ui', () => ({
+  // perfAdvisorHeaders.ts registers a CSRF header hook at module load time.
+  setPerfAdvisorCustomHeaders: () => {}
+}));
 
 beforeAll(() => server.listen({ onUnhandledRequest: 'warn' }));
 afterEach(() => server.resetHandlers());
