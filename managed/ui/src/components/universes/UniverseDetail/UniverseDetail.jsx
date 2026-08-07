ed72c2ef2aa90e04bd62aa8bbe122f93acbb89d0
Jethro Mak <88681329+Jethro-M@users.noreply.github.com>
2026-08-07 17:02:30 -0400

    [BACKPORT 2026.1][PLAT-21936] Rename reprovision nodes action
    Summary:
    Simple rename of a universe action to remove mostly internal reference of
    `YNP` (Yugabyte Node Provisioner).
    
    No functional changes here.
    
    Original commit: None / D56719
    
    Test Plan:
    Verified the text lables were changed and there were no impacts on the form logic.
    {F541563}
    {F541564}
    
    Reviewers: nsingh, skhilar
    
    Reviewed By: nsingh
    
    Subscribers: yugaware
    
    Differential Revision: https://phorge.dev.yugabyte.com/D56720

--- managed/ui/src/components/universes/UniverseDetail/UniverseDetail.jsx
+++ managed/ui/src/components/universes/UniverseDetail/UniverseDetail.jsx
@@ -1657,7 +1657,7 @@ class UniverseDetail extends Component {
                             onClick={showReprovisionNodesWithYnpModal}
                           >
                             <YBLabelWithIcon icon="fa fa-refresh">
-                              Reprovision Nodes with YNP
+                              Reprovision Universe Nodes
                             </YBLabelWithIcon>
                           </YBMenuItem>
                         </RbacValidator>
