7948269fd4e5cb126eb07e41fe0281508c6de92e
kkannan <kkannan@yugabyte.com>
2026-08-13 15:37:08 +0530

    [PLAT-22009][Default]- Make the provider selection smart when possible
    Summary:
    Previously, we selected AWS as the default provider, even when the account did not have any AWS provider configured. This was cumbersome for users who had only GCP or another provider configured, as they had to manually select their provider every time.
    
    With this diff, we have introduced smarter provider selection. The logic is as follows:
    
    ### A. Account has only one provider type configured
    
    For example, Azure only.
    
    * In the **Create Universe** wizard, that provider is selected by default.
    * The corresponding provider card appears first in the row.
    
    ### B. Account has multiple provider types configured
    
    For example, AWS and GCP.
    
    * In the **Create Universe** wizard, one of the configured providers is selected by default.
    * Providers are prioritized alphabetically. In this example, AWS is selected by default because it comes first alphabetically.
    * The selected provider's card appears first in the row.
    * If the user switches from AWS to GCP, the provider card order remains unchanged.
    
    ### C. Account has no providers configured
    
    * AWS is selected by default, maintaining the existing behavior.
    
    Test Plan: tested manually
    
    Reviewers: lsangappa
    
    Reviewed By: lsangappa
    
    Subscribers: yugaware
    
    Differential Revision: https://phorge.dev.yugabyte.com/D56946

--- managed/ui/src/redesign/features-v2/universe/create-universe/fields/provider/ProviderSelect.tsx
+++ managed/ui/src/redesign/features-v2/universe/create-universe/fields/provider/ProviderSelect.tsx
@@ -1,5 +1,6 @@
-import { ReactElement } from 'react';
+import { ReactElement, useMemo } from 'react';
 import { Control, FieldValues, Path, useFormContext } from 'react-hook-form';
+import { useQuery } from 'react-query';
 import {
   AWS_CLOUD_OPTION,
   AZURE_CLOUD_OPTION,
@@ -10,6 +11,9 @@ import {
   YBCloudSelectField
 } from '@yugabyte-ui-library/core';
 import { CloudType } from '../../../../../features/universe/universe-form/utils/dto';
+import { api, QUERY_KEY } from '../../../../../features/universe/universe-form/utils/api';
+import { ProviderStatus } from '../../../../../../components/configRedesign/providerRedesign/constants';
+import { YBProvider } from '../../../../../../components/configRedesign/providerRedesign/types';
 import { useEffectOnce } from 'react-use';
 
 interface CloudFieldProps<T> {
@@ -17,24 +21,45 @@ interface CloudFieldProps<T> {
   label: string;
 }
 
+const BASE_CLOUD_OPTIONS = [
+  { ...AWS_CLOUD_OPTION, value: CloudType.aws },
+  { ...GCP_CLOUD_OPTION, value: CloudType.gcp },
+  { ...AZURE_CLOUD_OPTION, value: CloudType.azu },
+  { ...K8S_CLOUD_OPTION, value: CloudType.kubernetes },
+  { ...ON_PREM_CLOUD_OPTION, value: CloudType.onprem },
+  { ...OCI_CLOUD_OPTION, value: CloudType.oci }
+];
+
 export const CloudField = <T,>({ name, label }: CloudFieldProps<T>): ReactElement => {
   const { control, getValues, setValue } =
     useFormContext<T extends FieldValues ? T : FieldValues>();
-  const clouds = [
-    {...AWS_CLOUD_OPTION, value: CloudType.aws},
-    {...GCP_CLOUD_OPTION, value: CloudType.gcp},
-    {
-      ...AZURE_CLOUD_OPTION,
-      value: CloudType.azu
-    },
-    {...K8S_CLOUD_OPTION, value: CloudType.kubernetes},
-    {...ON_PREM_CLOUD_OPTION, value: CloudType.onprem},
-    {...OCI_CLOUD_OPTION, value: CloudType.oci},
-  ];
+  const fieldName = name as Path<T extends FieldValues ? T : FieldValues>;
+
+  const { data: providers = [] } = useQuery(QUERY_KEY.getProvidersList, api.getProvidersList);
+
+  const configuredCodes = useMemo(() => {
+    return new Set(
+      (providers as YBProvider[])
+        .filter((provider) => provider.usabilityState === ProviderStatus.READY)
+        .map((provider) => provider.code as string)
+    );
+  }, [providers]);
+
+  const clouds = useMemo(() => {
+    const configured = BASE_CLOUD_OPTIONS.filter((option) =>
+      configuredCodes.has(option.value)
+    ).sort((a, b) => a.label.localeCompare(b.label));
+    const unconfigured = BASE_CLOUD_OPTIONS.filter(
+      (option) => !configuredCodes.has(option.value)
+    );
+    return [...configured, ...unconfigured];
+  }, [configuredCodes]);
 
   useEffectOnce(() => {
-    if (!getValues(name as Path<T extends FieldValues ? T : FieldValues>)) {
-      setValue(name as Path<T extends FieldValues ? T : FieldValues>, CloudType.aws as any);
+    if (!getValues(fieldName)) {
+      const defaultCloud =
+        configuredCodes.size > 0 ? clouds[0].value : CloudType.aws;
+      setValue(fieldName, defaultCloud as any);
     }
   });
 
@@ -44,9 +69,9 @@ export const CloudField = <T,>({ name, label }: CloudFieldProps<T>): ReactElemen
       label={label}
       options={clouds as any}
       control={control as unknown as Control<FieldValues>}
-      value={getValues(name as Path<T extends FieldValues ? T : FieldValues>)}
+      value={getValues(fieldName)}
       onChange={(value: FieldValues) => {
-        setValue(name as Path<T extends FieldValues ? T : FieldValues>, value as any);
+        setValue(fieldName, value as any);
       }}
     />
   );
