e863b6a347315bee67a0e57c8f64e023ee5e0892
Aashay Mokadam <48543070+aashay-m@users.noreply.github.com>
2026-08-17 14:44:41 -0700

    [BACKPORT 2.31.0.4263][CLOUDGA-35490] YBA: Demote high-volume debug logs to TRACE (#33321)
    ## Summary
    Backport of f6eb0192ed to 2.31.0.4263.
    
    Original commit: f6eb0192ed / D56888
    
    [CLOUDGA-35490] YBA: Demote high-volume debug logs to TRACE
    
    ## Test plan
    None / trivial (log level change only)
    
    [CLOUDGA-35490]: https://yugabyte.atlassian.net/browse/CLOUDGA-35490

--- managed/src/main/java/com/yugabyte/yw/forms/UniverseDefinitionTaskParams.java
+++ managed/src/main/java/com/yugabyte/yw/forms/UniverseDefinitionTaskParams.java
@@ -1868,10 +1868,12 @@ public class UniverseDefinitionTaskParams extends UniverseTaskParams {
       OverridenDetails overridenDetails =
           getOverridenDetails(UniverseTaskBase.ServerType.TSERVER, azUUID);
       if (overridenDetails.getDeviceInfo() != null) {
-        log.debug(
-            "Getting overriden device info {} for az {}",
-            Json.toJson(overridenDetails.getDeviceInfo()),
-            azUUID);
+        if (log.isTraceEnabled()) {
+          log.trace(
+              "Getting overriden device info {} for az {}",
+              Json.toJson(overridenDetails.getDeviceInfo()),
+              azUUID);
+        }
         return mergeDeviceInfos(deviceInfo, overridenDetails.getDeviceInfo());
       }
       return deviceInfo;
@@ -1887,9 +1889,9 @@ public class UniverseDefinitionTaskParams extends UniverseTaskParams {
       }
       JsonNode original = Json.toJson(deviceInfo);
       JsonNode overriden = Json.toJson(overridenDeviceInfo);
-      log.debug("Merging device info {} with {}", original, overriden);
+      log.trace("Merging device info {} with {}", original, overriden);
       CommonUtils.deepMerge(original, overriden, true);
-      log.debug("Device info after merging {}", original);
+      log.trace("Device info after merging {}", original);
       return Json.fromJson(original, DeviceInfo.class);
     }
 
