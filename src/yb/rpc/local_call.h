6926ee1ed7c8fdc2eb3546c721847929f3c3f058
Ella Baron <ella.baron@shopify.com>
2026-08-31 06:56:43 -0700

    dist-trace: drop a redundant doc comment on otel_span_context
    Review feedback on #33112.

--- src/yb/rpc/local_call.h
+++ src/yb/rpc/local_call.h
@@ -52,8 +52,6 @@ class LocalOutboundCall : public OutboundCall {
     return req_;
   }
 
-  // Span context of this outbound call's trace span, to parent the matching local inbound span
-  // (local calls carry no wire header). nullopt when tracing is off.
   std::optional<opentelemetry::trace::SpanContext> otel_span_context() const {
     if (otel_span_) {
       return otel_span_->GetContext();
