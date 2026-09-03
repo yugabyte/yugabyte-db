e5049e141537b33cea6207798beebd74c3a24e4b
Naorem Khogendro Singh <nsingh@yugabyte.com>
2026-09-03 11:52:04 -0700

    [PLAT-22329] Add comparator for cloudList in placementInfo for proper delta generation
    Summary: Minor fix to have a generic UUID comparator as many nested ones are on UUID as the key.
    
    Test Plan:
    Tested manually - trival.
    
    Looks good with this change.
    
    ```
     "placementInfo": {
              "cloudList": [
                {
                  "uuid": "ed205728-046b-46f7-b6f9-c22bdd334296",
                  "code": "aws",
                  "regionList": [
                    {
                      "uuid": "11032ecf-4117-4932-a157-f4867e317b53",
                      "code": "us-west-2",
                      "name": "US West (Oregon)",
                      "azList": [
                        {
                          "uuid": "472c3d7a-00e7-4fcc-8b9b-227aa02afd9c",
                          "name": "us-west-2a",
                          "replicationFactor": 1,
                          "subnet": "subnet-6553f513",
                          "secondarySubnet": null,
                          "numNodesInAZ": {
                            "$oldValue": 1,
                            "$newValue": 2,
                            "$deltaType": "REPLACE"
                          },
                          "isAffinitized": true,
    ```
    
    Reviewers: vbansal, skhilar
    
    Reviewed By: vbansal, skhilar
    
    Subscribers: yugaware
    
    Differential Revision: https://phorge.dev.yugabyte.com/D57831

--- managed/src/main/java/com/yugabyte/yw/common/NodeDetailsArrayComparator.java
+++ managed/src/main/java/com/yugabyte/yw/common/NodeDetailsArrayComparator.java
@@ -3,6 +3,7 @@
 package com.yugabyte.yw.common;
 
 import com.fasterxml.jackson.databind.JsonNode;
+import org.apache.commons.lang3.StringUtils;
 
 /**
  * Array element comparator for universe definition JSON that matches {@code nodeDetailsSet} by
@@ -17,8 +18,13 @@ public class NodeDetailsArrayComparator implements DeltaEvaluator.ArrayElementCo
     if (path != null && path.endsWith("nodeDetailsSet")) {
       return Integer.compare(nodeIdx(node1), nodeIdx(node2));
     }
-    if (path != null && path.endsWith("clusters")) {
-      return uuid(node1).compareTo(uuid(node2));
+    if (node1.isObject() && node2.isObject()) {
+      // This covers clusters[].regions, clusters[].userIntent.regionList etc.
+      String uuid1 = uuid(node1);
+      String uuid2 = uuid(node2);
+      if (StringUtils.isNotBlank(uuid1) && StringUtils.isNotBlank(uuid2)) {
+        return uuid1.compareTo(uuid2);
+      }
     }
     return Integer.compare(node1.hashCode(), node2.hashCode());
   }
