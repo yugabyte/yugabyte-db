a530c1e024a9ab115bb96f86eb69a578ae75b3dd
kkannan <kkannan@yugabyte.com>
2026-09-08 13:27:04 +0530

    [BACKPORT 2026.1][PLAT-22353]Create universe button takes to old ui
    Summary: backport of this parent [[ https://phorge.dev.yugabyte.com/D57938 | diff ]]
    
    Test Plan: Followed the test plan from the parent diff.
    
    Reviewers: lsangappa
    
    Reviewed By: lsangappa
    
    Subscribers: yugaware
    
    Differential Revision: https://phorge.dev.yugabyte.com/D57946

--- managed/ui/src/components/advanced/EditConfig.tsx
+++ managed/ui/src/components/advanced/EditConfig.tsx
@@ -60,7 +60,7 @@ export const EditConfig: FC<EditConfigData> = ({
       configData.type === 'Boolean' ? configData.configValue === 'true' : configData.configValue
   };
   const isEditUniverseV2Config =
-    configData.configKey === RuntimeConfigKey.ENABLE_V2_EDIT_UNIVERSE_UI;
+    configData.configKey === RuntimeConfigKey.ENABLE_NEW_UNIVERSE_EXPERIENCE_FOR_ALL_USERS;
 
   const handleSubmit = async (
     values: EditConfigDataValues,
