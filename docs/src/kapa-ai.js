2c1c45192d3f5a79e440ea71bb962c4defd3bbcd
Sami Ahmed Siddiqui <sami.siddiqui@yasglobal.com>
2026-09-08 09:19:50 +0500

    Fix kapa button display issue

--- docs/src/kapa-ai.js
+++ docs/src/kapa-ai.js
@@ -1,46 +1,46 @@
-(function() {
-  window.addEventListener('load', function () {
-    const activeGroups = window.OnetrustActiveGroups || '';
-    if (activeGroups.indexOf('C0002') > -1) {
-      const askAiTop = document.querySelector('.ask-ai-top');
-      const askAiTopSpan = document.querySelector('.ask-ai-top > span');
-      const kapaButton = document.querySelector('.yb-kapa-button');
+(function () {
+	window.addEventListener('load', function () {
+		const activeGroups = window.OnetrustActiveGroups || '';
+		if (activeGroups.indexOf('C0002') > -1) {
+			const askAiTop = document.querySelector('.ask-ai-top');
+			const askAiTopSpan = document.querySelector('.ask-ai-top > span');
+			const kapaButton = document.querySelector('.yb-kapa-button');
 
-      if (askAiTopSpan) {
-        askAiTopSpan.addEventListener('click', function () {
-          if (window.Kapa) {
-            window.Kapa.open({
-              mode: 'ai',
-            });
-          }
-        });
-      }
+			if (askAiTopSpan) {
+				askAiTopSpan.addEventListener('click', function () {
+					if (window.Kapa) {
+						window.Kapa.open({
+							mode: 'ai',
+						});
+					}
+				});
+			}
 
-      if (askAiTop) {
-        askAiTop.classList.remove('hidden');
-      }
+			if (askAiTop) {
+				askAiTop.classList.remove('hidden');
+			}
 
-      if (kapaButton) {
-        kapaButton.classList.remove('block');
-      }
-    }
+			if (kapaButton) {
+				kapaButton.classList.remove('hidden');
+			}
+		}
 
-    let clearbitAnonymousId = window.browserCookieUtils.getCookie('cb_anonymous_id');
-    if (clearbitAnonymousId && clearbitAnonymousId !== 'null') {
-      clearbitAnonymousId = clearbitAnonymousId.replace(/%22/g, '').replace(/"/g, '');
-      const kapaUser = {
-        uniqueClientId: clearbitAnonymousId,
-      };
+		let clearbitAnonymousId = window.browserCookieUtils.getCookie('cb_anonymous_id');
+		if (clearbitAnonymousId && clearbitAnonymousId !== 'null') {
+			clearbitAnonymousId = clearbitAnonymousId.replace(/%22/g, '').replace(/"/g, '');
+			const kapaUser = {
+				uniqueClientId: clearbitAnonymousId,
+			};
 
-      let clearbitUserId = window.browserCookieUtils.getCookie('cb_user_id');
-      if (clearbitUserId && clearbitUserId !== 'null') {
-        clearbitUserId = clearbitUserId.replace(/%22/g, '').replace(/"/g, '');
-        kapaUser.email = clearbitUserId;
-      }
+			let clearbitUserId = window.browserCookieUtils.getCookie('cb_user_id');
+			if (clearbitUserId && clearbitUserId !== 'null') {
+				clearbitUserId = clearbitUserId.replace(/%22/g, '').replace(/"/g, '');
+				kapaUser.email = clearbitUserId;
+			}
 
-      window.kapaSettings = {
-        user: kapaUser,
-      };
-    }
-  });
+			window.kapaSettings = {
+				user: kapaUser,
+			};
+		}
+	});
 })();
