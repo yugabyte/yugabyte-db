a1e9b837ce07cad2d12733e7a9ad54345a6e1fac
Naorem Khogendro Singh <nsingh@yugabyte.com>
2026-09-09 16:30:39 -0700

    [PLAT-22386] ReprovisionUniverseNodes must blacklist tablet leaders before stopping tserver
    Summary:
    Before stopping, tablet leaders must be moved out to avoid leaderless for a few seconds. This is similar to PLAT-22380.
    
    Also, this enables state tracking as in VMImageUpgrade.
    
    Test Plan:
    UT added. Itests must pass.
    
    Manual Test
    
    ```
    2026-09-10T01:32:54.867Z  [info] e041a1dd-99db-4d45-ae60-c1c44b895d56 ProvisionUniverseNodes.java:156 [TaskPool-0] com.yugabyte.yw.commissioner.tasks.ProvisionUniverseNodes Skipping node yb-admin-nsingh-test-universe1-n3 because it has already been provisioned in this task run.
    ```
    
    Reviewers: skhilar, yshchetinin
    
    Reviewed By: skhilar, yshchetinin
    
    Subscribers: yugaware
    
    Differential Revision: https://phorge.dev.yugabyte.com/D58032

--- managed/src/main/java/com/yugabyte/yw/commissioner/tasks/ProvisionUniverseNodes.java
+++ managed/src/main/java/com/yugabyte/yw/commissioner/tasks/ProvisionUniverseNodes.java
@@ -4,11 +4,13 @@ package com.yugabyte.yw.commissioner.tasks;
 
 import static play.mvc.Http.Status.BAD_REQUEST;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import com.yugabyte.yw.commissioner.BaseTaskDependencies;
 import com.yugabyte.yw.commissioner.ITask.Abortable;
 import com.yugabyte.yw.commissioner.ITask.Retryable;
 import com.yugabyte.yw.commissioner.UpgradeTaskBase;
 import com.yugabyte.yw.commissioner.UserTaskDetails.SubTaskGroupType;
+import com.yugabyte.yw.commissioner.tasks.upgrade.VMImageUpgrade.RuntimeInfo;
 import com.yugabyte.yw.common.PlatformServiceException;
 import com.yugabyte.yw.common.Util;
 import com.yugabyte.yw.forms.ProvisionUniverseNodesParams;
@@ -23,6 +25,8 @@ import java.util.LinkedHashSet;
 import java.util.List;
 import java.util.Set;
 import java.util.TreeSet;
+import java.util.UUID;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 import javax.inject.Inject;
@@ -33,10 +37,17 @@ import org.apache.commons.collections4.CollectionUtils;
 @Abortable
 @Retryable
 public class ProvisionUniverseNodes extends UpgradeTaskBase {
+  private volatile RuntimeInfo runtimeInfo;
 
   @Inject
   protected ProvisionUniverseNodes(BaseTaskDependencies baseTaskDependencies) {
     super(baseTaskDependencies);
+  }
+
+  /** Task runtime progress info. */
+  public static class RuntimeInfo {
+    @JsonProperty("provisionedNodeUuids")
+    Set<UUID> provisionedNodeUuids = ConcurrentHashMap.newKeySet();
   }
 
   @Override
@@ -126,6 +137,8 @@ public class ProvisionUniverseNodes extends UpgradeTaskBase {
   protected void createPrecheckTasks(Universe universe) {
     super.createPrecheckTasks(universe);
     addBasicPrecheckTasks();
+    // Load the runtime info for this task run.
+    runtimeInfo = getRuntimeInfo(RuntimeInfo.class);
   }
 
   @Override
@@ -139,6 +152,12 @@ public class ProvisionUniverseNodes extends UpgradeTaskBase {
           taskParams().clusters = universe.getUniverseDetails().clusters;
 
           for (NodeDetails node : nodeSet) {
+            if (runtimeInfo.provisionedNodeUuids.contains(node.nodeUuid)) {
+              log.info(
+                  "Skipping node {} because it has already been provisioned in this task run.",
+                  node.nodeName);
+              continue;
+            }
             Set<ServerType> processTypes = new LinkedHashSet<>();
             if (node.isMaster) {
               processTypes.add(ServerType.MASTER);
@@ -164,11 +183,14 @@ public class ProvisionUniverseNodes extends UpgradeTaskBase {
             // control, re-issuing the stop would fail. Gating on the node state and marking the
             // node Stopped before re-provisioning keeps this idempotent across retries.
             if (node.state == NodeState.Live) {
-              for (ServerType processType : processTypes) {
-                createServerControlTask(node, processType, "stop")
-                    .setSubTaskGroupType(SubTaskGroupType.StoppingNodeProcesses);
-              }
-
+              // This also blacklist tablet leaders on the node before stopping the tserver process.
+              stopProcessesOnNodes(
+                  singleNode,
+                  processTypes,
+                  false /* remove master from quorum */,
+                  false /* deconfigure */,
+                  false /* flushTablets */,
+                  SubTaskGroupType.StoppingNodeProcesses);
               // Intentionally short-lived: it flips to Reprovisioning right below. Persisting
               // Stopped here is the point at which the node leaves Live, so a retry that fails
               // anywhere in the re-provisioning that follows will skip the stop above.
@@ -179,8 +201,7 @@ public class ProvisionUniverseNodes extends UpgradeTaskBase {
             createSetNodeStateTasks(singleNode, getNodeState())
                 .setSubTaskGroupType(getTaskSubGroupType());
 
-            createSetupYNPTask(universe, singleNode)
-                .setSubTaskGroupType(SubTaskGroupType.Provisioning);
+            createSetupYNPTask(universe, singleNode).setSubTaskGroupType(getTaskSubGroupType());
             createYNPProvisioningTask(
                     universe,
                     singleNode,
@@ -188,12 +209,11 @@ public class ProvisionUniverseNodes extends UpgradeTaskBase {
                       p.isSoftwarePresent = true;
                       p.isDataPresent = true;
                     })
-                .setSubTaskGroupType(SubTaskGroupType.Provisioning);
+                .setSubTaskGroupType(getTaskSubGroupType());
             createInstallNodeAgentTasks(universe, singleNode)
                 .setSubTaskGroupType(SubTaskGroupType.InstallingSoftware);
             createWaitForNodeAgentTasks(singleNode)
                 .setSubTaskGroupType(SubTaskGroupType.InstallingSoftware);
-
             for (ServerType processType : processTypes) {
               if (processType.equals(ServerType.CONTROLLER)) {
                 createStartYbcTasks(singleNode)
@@ -213,16 +233,22 @@ public class ProvisionUniverseNodes extends UpgradeTaskBase {
                     .setSubTaskGroupType(SubTaskGroupType.StartingNodeProcesses);
               }
             }
-
             createWaitForKeyInMemoryTasks(singleNode)
                 .setSubTaskGroupType(SubTaskGroupType.StartingNodeProcesses);
-
+            if (processTypes.contains(ServerType.TSERVER)) {
+              removeFromLeaderBlackListIfAvailable(
+                  singleNode, SubTaskGroupType.StartingNodeProcesses);
+            }
             createSetNodeStateTasks(singleNode, NodeState.Live)
-                .setSubTaskGroupType(getTaskSubGroupType());
+                .setSubTaskGroupType(SubTaskGroupType.StartingNodeProcesses)
+                .setAfterGroupRunListener(
+                    g ->
+                        updateRuntimeInfo(
+                            RuntimeInfo.class,
+                            info -> info.provisionedNodeUuids.add(singleNode.get(0).nodeUuid)));
           }
-
           createUpdateUniverseFieldsTask(u -> u.getUniverseDetails().installNodeAgent = false)
-              .setSubTaskGroupType(SubTaskGroupType.InstallingSoftware);
+              .setSubTaskGroupType(SubTaskGroupType.Configuring);
         });
   }
 }
