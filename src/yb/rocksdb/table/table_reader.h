db3ec741d5881c5a965969ca48579a3c4717e7cf
Hideaki Kimura <hideaki.kimura@yugabyte.com>
2026-09-15 08:11:35 -0700

    Revert "[BACKPORT 2025.2][#31746] DocDB: Unify filesystem abstractions between yb_util and rocksdb libraries (#33834)"
    This reverts commit 1c2c584ca09c607d7430b45d0295338d807e8262.

--- src/yb/rocksdb/table/table_reader.h
+++ src/yb/rocksdb/table/table_reader.h
@@ -41,6 +41,7 @@ class GetContext;
 class InternalIterator;
 class Iterator;
 class RandomAccessFileReader;
+class WritableFile;
 
 // A Table is a sorted map from strings to strings.  Tables are
 // immutable and persistent.  A Table may be safely accessed from
