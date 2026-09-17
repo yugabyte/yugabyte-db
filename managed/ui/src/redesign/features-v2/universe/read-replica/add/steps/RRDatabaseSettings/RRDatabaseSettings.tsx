9ee0df9384fc47f9cfecf6083087b7b48f6a25fb
Lingeshwar <lingesh100@gmail.com>
2026-09-17 11:24:57 +0530

    [BACKPORT 2026.1.2][PLAT-22114][PLAT-22531] - fix : Mutliline csv gflags broken in New UI and other bugs
    Summary: Backport of [[ https://phorge.dev.yugabyte.com/D58226 | https://phorge.dev.yugabyte.com/D58226 ]]
    
    Test Plan: Tested manually
    
    Reviewers: kkannan
    
    Reviewed By: kkannan
    
    Subscribers: ui, yugaware
    
    Differential Revision: https://phorge.dev.yugabyte.com/D58274

--- managed/ui/src/redesign/features-v2/universe/read-replica/add/steps/RRDatabaseSettings/RRDatabaseSettings.tsx
+++ managed/ui/src/redesign/features-v2/universe/read-replica/add/steps/RRDatabaseSettings/RRDatabaseSettings.tsx
@@ -13,16 +13,26 @@ import {
 import { CustomizeRRFlagField, CUSTOMIZE_RR_FLAG_FIELD } from './CustomizeRRFlagField';
 import { GFlagsFieldNew } from '@app/redesign/features/universe/universe-form/form/fields/GflagsField/GflagsFieldNew';
 import { getDBVersion } from '../../AddReadReplicaUtils';
+import { useRuntimeConfigValues } from '@app/redesign/features-v2/universe/create-universe/helpers/utils';
+import { getClusterByType } from '@app/redesign/features-v2/universe/edit-universe/EditUniverseUtils';
+import { GFLAGS_FIELD } from '@app/redesign/features-v2/universe/create-universe/fields/FieldNames';
+import { RunTimeConfigEntry } from '@app/redesign/features/universe/universe-form/utils/dto';
+import { RuntimeConfigKey } from '@app/redesign/helpers/constants';
+import {
+  ClusterGFlagsAllOfGflagGroupsItem,
+  ClusterSpecClusterType
+} from '@app/v2/api/yugabyteDBAnywhereV2APIs.schemas';
 
 const { Box } = mui;
 
 export const RRDatabaseSettings = forwardRef<StepsRef>((_, forwardRef) => {
   const [
-    { activeStep, databaseSettings, universeData },
+    { databaseSettings, universeData },
     { moveToNextPage, moveToPreviousPage, saveDatabaseSettings }
-  ] = (useContext(AddRRContext) as unknown) as AddRRContextMethods;
+  ] = useContext(AddRRContext) as unknown as AddRRContextMethods;
 
   const { t } = useTranslation('translation', { keyPrefix: 'readReplica.addRR' });
+  const { runtimeConfigs } = useRuntimeConfigValues();
   const methods = useForm<RRDatabaseSettingsProps>({
     defaultValues: databaseSettings,
     mode: 'onChange'
@@ -31,6 +41,18 @@ export const RRDatabaseSettings = forwardRef<StepsRef>((_, forwardRef) => {
   const { control, watch } = methods;
 
   const customizeFlagValue = watch(CUSTOMIZE_RR_FLAG_FIELD);
+
+  const isGFlagMultilineConfEnabled =
+    runtimeConfigs?.configEntries?.find(
+      (c: RunTimeConfigEntry) => c.key === RuntimeConfigKey.IS_GFLAG_MULTILINE_ENABLED
+    )?.value === 'true';
+
+  const primaryCluster = universeData
+    ? getClusterByType(universeData, ClusterSpecClusterType.PRIMARY)
+    : undefined;
+  const isPGSupported = !!primaryCluster?.gflags?.gflag_groups?.includes(
+    ClusterGFlagsAllOfGflagGroupsItem.ENHANCED_POSTGRES_COMPATIBILITY
+  );
 
   useImperativeHandle(
     forwardRef,
@@ -59,12 +81,12 @@ export const RRDatabaseSettings = forwardRef<StepsRef>((_, forwardRef) => {
             {universeData && customizeFlagValue && (
               <GFlagsFieldNew
                 control={control}
-                fieldPath={'gFlags'}
+                fieldPath={GFLAGS_FIELD}
                 dbVersion={getDBVersion(universeData)}
                 isReadReplica={true}
                 editMode={false}
-                isGFlagMultilineConfEnabled={false}
-                isPGSupported={false}
+                isGFlagMultilineConfEnabled={isGFlagMultilineConfEnabled}
+                isPGSupported={isPGSupported}
                 isReadOnly={false}
               />
             )}
