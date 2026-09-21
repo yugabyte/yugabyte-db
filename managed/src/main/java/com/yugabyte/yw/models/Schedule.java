29855f55970f990fe6f344b27903d4d7fc224779
Daniel Shubin <dshubin@yugabyte.com>
2026-09-21 21:28:06 +0000

    [PLAT-21098] V2 api schedule response include useRoles
    The v2 schedule response will now include if the backup is to be taken
    with or without ysql roles

--- managed/src/main/java/com/yugabyte/yw/models/Schedule.java
+++ managed/src/main/java/com/yugabyte/yw/models/Schedule.java
@@ -958,6 +958,7 @@ public class Schedule extends Model {
             .expiryTimeUnit(params.expiryTimeUnit)
             .parallelism(params.parallelism)
             .pointInTimeRestoreEnabled(params.enablePointInTimeRestore)
+            .useRoles(params.getUseRoles)
             .build();
     return backupInfo;
   }
