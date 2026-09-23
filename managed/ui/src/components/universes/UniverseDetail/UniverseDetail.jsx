5e9e7f4b55ccf35cbae3320e804c6a0bc74a818e
Aleksandr Malyshev <amalyshev@yugabyte.com>
2026-09-23 20:30:11 +0300

    [BACKPORT 2026.1.2] [PLAT-22659] YBA: Use + icon for the Enable Perf Advisor Collector menu item
    Summary:
    Original diff: https://phorge.dev.yugabyte.com/D58566
    
    The universe Actions > More menu showed a trash icon for "Enable Perf Advisor Collector", which reads as a destructive action. Enable now uses a + icon, matching the Add button on the Perf Advisor Endpoints list; Disable keeps the trash icon.
    
    Test Plan: Cherry-picked cleanly onto 2026.1.2. Prettier on the changed hunk.
    
    Reviewers: sarthak.naithani
    
    Reviewed By: sarthak.naithani
    
    Subscribers: yugaware
    
    Differential Revision: https://phorge.dev.yugabyte.com/D58573

--- managed/ui/src/components/universes/UniverseDetail/UniverseDetail.jsx
+++ managed/ui/src/components/universes/UniverseDetail/UniverseDetail.jsx
@@ -1860,7 +1860,13 @@ class UniverseDetail extends Component {
                             disabled={isPerfAdvisorActionDisabled}
                             onClick={showEnablePerfAdvisorModal}
                           >
-                            <YBLabelWithIcon icon="fa fa-trash-o fa-fw">
+                            <YBLabelWithIcon
+                              icon={
+                                isUniverseRegisteredToPa
+                                  ? 'fa fa-trash-o fa-fw'
+                                  : 'fa fa-plus fa-fw'
+                              }
+                            >
                               {isUniverseRegisteredToPa
                                 ? 'Disable Perf Advisor Collector'
                                 : 'Enable Perf Advisor Collector'}
